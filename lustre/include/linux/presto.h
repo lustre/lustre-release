2ce63e7c1278fa87883b3060a7a0c4d98b5ccd15
pschwan <pschwan>
1999-06-05 19:02:00 +0000

    A couple days of changes--much improvement.

--- lustre/include/linux/presto.h
+++ lustre/include/linux/presto.h
@@ -1,17 +1,6 @@
 #ifndef __PRESTO_H_
 #define __PRESTO_H_ 1
 
-/* super.c */
-#if 0
-extern struct super_block * ext2_read_super (struct super_block * sb, 
-					     void * data,
-					     int silent);
-#endif
-extern struct file_system_type presto_fs_type;
-extern int init_presto_fs(void);
-
-int presto_ispresto(struct inode *);
-
 struct bottomfs {
 	struct super_operations *bottom_sops;
 
@@ -26,137 +15,8 @@ struct bottomfs {
 };
 extern struct bottomfs *the_bottom;
 
-/* inode.c */
-void presto_read_inode(struct inode *inode);
-int presto_notify_change(struct dentry *de, struct iattr *iattr);
-
-/* dcache.c */
-extern struct dentry_operations presto_dentry_operations;
-
-/* dir.c */
-extern struct inode_operations presto_dir_iops;
-extern struct inode_operations presto_file_iops;
-extern struct inode_operations presto_sym_iops;
-extern struct file_operations presto_dir_fops;
-extern struct file_operations presto_file_fops;
-extern struct file_operations presto_sym_fops;
-
-void presto_setup_file_ops(struct inode *);
-void presto_setup_symlink_ops(struct inode *);
-
-int presto_lookup(struct inode * dir, struct dentry *dentry);
-int presto_dir_open(struct inode *inode, struct file *file);
-int presto_file_open(struct inode *inode, struct file *file);
-int presto_file_release(struct inode *inode, struct file *file);
-
-int presto_mkdir(struct inode *inode, struct dentry *, int mode);
-int presto_create(struct inode *inode, struct dentry *, int mode);
-int presto_unlink(struct inode *inode, struct dentry *);
-int presto_rmdir(struct inode *inode, struct dentry *);
-int presto_symlink(struct inode *, struct dentry *, const char *);
-int presto_rename(struct inode *old_dir, struct dentry *old_dentry,
-		  struct inode *new_dir, struct dentry *new_dentry);
-
-int lento_journal(char *page);
-
-/* intermezzo.c */
-#define PRESTO_ATTR		0x00000001 /* attributes cached */
-#define PRESTO_DATA		0x00000002 /* data cached */
-#define PRESTO_VOLROOT          0x00000004 /* this dentry is root of volume */
-#define PRESTO_HASPERMIT        0x00000008 /* we have a permit to WB */
-
-#define EISVOLROOT              0x2001
-
-int presto_chk(struct dentry *dentry, int flag);
-void presto_set(struct dentry *dentry, int flag);
-void presto_permit(struct inode *);
-int presto_mark_dentry(const char *path, int and_bits, int or_bits);
-
-/* journal.c */
-
-#define JOURNAL_PAGE  PAGE_SIZE
-
-
-#define PRESTO_CREATE  1
-#define PRESTO_MKDIR   2
-#define PRESTO_UNLINK  3
-#define PRESTO_RMDIR   4
-#define PRESTO_CLOSE   5
-#define PRESTO_SYMLINK 6
-#define PRESTO_RENAME  7
-#define PRESTO_SETATTR 8
-#define PRESTO_LINK    9
-
-void journal_create(struct inode *dirinode, struct inode *fileinode, int len, const char *name, int mode);
-void journal_symlink(struct inode *inode, int len, const char *name, const char *target);
-void journal_mkdir(struct inode *inode, int len, const char *name, int mode);
-void journal_unlink(struct inode *inode, int len, const char *name);
-void journal_rmdir(struct inode *inode, int len, const char *name);
-void journal_rename(struct inode *old_dir, struct inode *old_file,
-		    struct inode *new_dir, int new_len, const char *new_name);
-void journal_setattr(struct inode *, int uid, int gid, int fsize, int atime,
-		     int mtime, int ctime, int mode, unsigned int flags,
-		     unsigned int valid);
-void journal_close(struct inode *inode);
-void journal_link(struct inode *src, struct inode *tpdiri, 
-		  struct inode *ti, int len, const char *name);
-void journal_fetch(void);
-
 /* sysctl.c */
 void presto_sysctl_init(void);
 void presto_sysctl_clean(void);
-
-/* global variables */
-extern int presto_debug;
-extern int presto_print_entry;
-
-/* debugging masks */
-#define D_SUPER     1   /* print results returned by Venus */ 
-#define D_INODE     2   /* print entry and exit into procedure */
-#define D_FILE      4   
-#define D_CACHE     8   /* cache debugging */
-#define D_MALLOC    16  /* print malloc, de-alloc information */
-#define D_JOURNAL   32
-#define D_UPCALL    64  /* up and downcall debugging */
-#define D_PSDEV    128  
-#define D_PIOCTL   256
-#define D_SPECIAL  512
-#define D_TIMING  1024
-#define D_DOWNCALL 2048
- 
-#if 0
-#define CDEBUG(mask, format, a...)                                \
-  do {                                                            \
-  if (presto_debug & mask) {                                        \
-    printk("(%s,l. %d): ",  __FUNCTION__, __LINE__);              \
-    printk(format, ## a); }                                       \
-} while (0) ;                            
-#else
-#define CDEBUG(mask, format, a...) ;
-#endif
-
-#define ENTRY    \
-    if(presto_print_entry) printk("Process %d entered %s\n",current->pid,__FUNCTION__)
-
-#define EXIT    \
-    if(presto_print_entry) printk("Process %d leaving %s\n",current->pid,__FUNCTION__)
-
-
-#define PRESTO_ALLOC(ptr, cast, size)                                       \
-do {                                                                      \
-    if (size <= 4096) {                                                    \
-        ptr = (cast)kmalloc((unsigned long) size, GFP_KERNEL);            \
-                CDEBUG(D_MALLOC, "kmalloced: %x at %x.\n", (int) size, (int) ptr);\
-     }  else {                                                             \
-        ptr = (cast)vmalloc((unsigned long) size);                        \
-	CDEBUG(D_MALLOC, "vmalloced: %x at %x.\n", (int) size, (int) ptr);}\
-    if (ptr == 0) {                                                       \
-        printk("kernel malloc returns 0 at %s:%d\n", __FILE__, __LINE__);  \
-    }                                                                     \
-    memset( ptr, 0, size );                                                   \
-} while (0)
-
-
-#define PRESTO_FREE(ptr,size) do {if (size <= 4096) { kfree_s((ptr), (size)); CDEBUG(D_MALLOC, "kfreed: %x at %x.\n", (int) size, (int) ptr); } else { vfree((ptr)); CDEBUG(D_MALLOC, "vfreed: %x at %x.\n", (int) size, (int) ptr);} } while (0)
 
 #endif
--- lustre/include/linux/sym_obd.h
+++ lustre/include/linux/sym_obd.h
@@ -9,22 +9,99 @@
 /*
  * Debug code
  */
+/* global variables */
+extern int obd_debug_level;
+extern int obd_print_entry;
+
+/* debugging masks */
+#define D_PSDEV       1 /* debug information from psdev.c */
+#define D_UNUSED1     2
+#define D_UNUSED2     4
+#define D_UNUSED3     8
+#define D_UNUSED4    16
+#define D_UNUSED5    32
+#define D_UNUSED6    64
+#define D_MALLOC    128 /* print malloc, free information */
+#define D_CACHE     256 /* cache-related items */
+#define D_INFO      512 /* general information, especially from interface.c */
+#define D_IOCTL    1024 /* ioctl related information */
+#define D_BLOCKS   2048 /* ext2 block allocation */
+ 
 #ifdef SYM_OBD_DEBUG
-#	define obd_debug(f, a...)	{ \
-					printk ("SYM OBD DEBUG (%s, %d): %s:", \
-						__FILE__, __LINE__, __FUNCTION__); \
-				  	printk (f, ## a); \
-					}
-#else
-#	define obd_debug(f, a...)	/**/
-#endif
+#define CDEBUG(mask, format, a...)					\
+        do {								\
+	if (obd_debug_level & mask) {					\
+		printk("(%s,l. %d): ",  __FUNCTION__, __LINE__);	\
+		printk(format, ## a); }					\
+	} while (0);
+
+#define ENTRY								      \
+        if (obd_print_entry)						      \
+                printk("Process %d entered %s\n", current->pid, __FUNCTION__)
+
+#define EXIT								      \
+        if (obd_print_entry)						      \
+                printk("Process %d leaving %s\n", current->pid, __FUNCTION__)
+
+#else /* SYM_OBD_DEBUG */
+
+#       define CDEBUG(mask, format, a...) ;
+#       define ENTRY ;
+#       define EXIT ;
+
+#endif /* SYM_OBD_DEBUG */
+
+
+
+#define OBD_ALLOC(ptr, cast, size)					\
+do {									\
+	if (size <= 4096) {						\
+		ptr = (cast)kmalloc((unsigned long) size, GFP_KERNEL);	\
+                CDEBUG(D_MALLOC, "kmalloced: %x at %x.\n",		\
+		       (int) size, (int) ptr);				\
+	} else {							\
+		ptr = (cast)vmalloc((unsigned long) size);		\
+		CDEBUG(D_MALLOC, "vmalloced: %x at %x.\n",		\
+		       (int) size, (int) ptr);				\
+	}								\
+	if (ptr == 0) {							\
+		printk("kernel malloc returns 0 at %s:%d\n",		\
+		       __FILE__, __LINE__);				\
+	}								\
+	memset(ptr, 0, size);						\
+} while (0)
+
+#define OBD_FREE(ptr,size)				\
+do {							\
+	if (size <= 4096) {				\
+		kfree_s((ptr), (size));			\
+		CDEBUG(D_MALLOC, "kfreed: %x at %x.\n",	\
+		       (int) size, (int) ptr);		\
+	} else {					\
+		vfree((ptr));				\
+		CDEBUG(D_MALLOC, "vfreed: %x at %x.\n",	\
+		       (int) size, (int) ptr);		\
+	}						\
+} while (0)
+
 
 /*
  * ioctl commands
  */
+struct ioc_prealloc {
+	long alloc; /* user sets it to the number of inodes requesting to be
+		     * preallocated.  kernel sets it to the actual number of
+		     * succesfully preallocate inodes */
+	long inodes[32]; /* actual inode numbers */
+};
+
 #define OBD_IOC_CREATE                 _IOR('f', 3, long)
 #define OBD_IOC_SETUP                  _IOW('f', 4, long)
 #define OBD_IOC_SYNC                   _IOR('f', 5, long)
+#define OBD_IOC_DESTROY                _IOW('f', 6, long)
+
+#define OBD_IOC_DEC_USE_COUNT          _IO('f', 8)
+
 
 /* balloc.c */
 int obd_new_block (const struct inode * inode, unsigned long goal,
@@ -33,9 +110,23 @@ void obd_free_blocks (const struct inode * inode, unsigned long block,
                       unsigned long count);
 unsigned long obd_count_free_blocks (struct super_block * sb);
 int ext2_group_sparse(int group);
+struct ext2_group_desc * ext2_get_group_desc(struct super_block * sb,
+                                            unsigned int block_group,
+                                            struct buffer_head ** bh);
+
+
+/* bitmap.c */
+unsigned long ext2_count_free(struct buffer_head * map, unsigned int numchars);
 
 /* fsync.c */
-int obd_sync_file(struct file * file, struct dentry *dentry);
+extern int obd_sync_file(struct file * file, struct dentry *dentry);
+
+/* ialloc.c */
+extern void ext2_free_inode (struct inode * inode);
+extern struct inode * ext2_new_inode (const struct inode * dir, int mode,
+				     int * err);
+extern unsigned long ext2_count_free_inodes (struct super_block * sb);
+extern void ext2_check_inodes_bitmap (struct super_block * sb);
 
 /* inode.c */
 void obd_read_inode (struct inode * inode);
@@ -50,11 +141,8 @@ struct buffer_head * obd_getblk (struct inode * inode, long block,
                                  int create, int * err);
 
 /* interface.c */
-extern struct inode * obd_inode_new (int inode_hint, int * err);
-extern void obd_inode_destroy (struct inode * inode);
-extern unsigned long obd_count_free_inodes (struct super_block * sb);
-extern void obd_check_inodes_bitmap (struct super_block * sb);
-unsigned long obd_count_free_inodes (struct super_block * sb);
+extern int obd_create (struct super_block * sb, int inode_hint, int * err);
+extern void obd_unlink (struct inode * inode);
 
 /* ioctl.c */
 int obd_ioctl (struct inode * inode, struct file * filp, unsigned int cmd,
@@ -68,6 +156,10 @@ extern void obd_warning (struct super_block *, const char *, const char *, ...)
 int obd_remount (struct super_block * sb, int * flags, char * data);
 struct super_block * obd_read_super (struct super_block * sb, void * data,
 				     int silent);
+
+/* sysctl.c */
+extern void obd_sysctl_init (void);
+extern void obd_sysctl_clean (void);
 
 /* truncate.c */
 void obd_truncate (struct inode * inode);
