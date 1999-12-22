69948e57f0aec156b56c98237b7b3a5b58f26115
adilger <adilger>
1999-12-22 15:49:29 +0000

    demos/obdtest.sh: added new script

---
+++ lustre/demos/obdtest.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Utility script to create an OBD snapshot.  If an existing filesystem is
+# not already mounted on /mnt/obd, we call the basic OBD setup script to
+# create and mount a filesystem for us.
+OBDDIR="`dirname $0`/.."
+[ "$OBDDIR" = "" ] && OBDDIR=".."
+. $OBDDIR/demos/config.sh
+
+[ ! -d $MNTOBD/lost+found ] && $OBDDIR/demos/obdsetup.sh
+
+echo "yesterday" > $MNTOBD/hello	# create a file
+echo "testing" > $MNTOBD/goodbye	# create a file
+ln -s hello $MNTOBD/link		# create a link
+cd $MNTOBD ; touch a b c ; cd -		# create a file
