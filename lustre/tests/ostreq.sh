99f73e65d9b7730f7e1bf78a6a14c3e32db8fcc9
braam <braam>
2002-01-03 18:39:07 +0000

    omitted test script

---
+++ lustre/tests/ostreq.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+R=/r
+
+insmod /lib/modules/2.4.17/kernel/drivers/block/loop.o
+dd if=/dev/zero of=/tmp/fs bs=1024 count=10000
+mke2fs -F /tmp/fs
+losetup /dev/loop/0 /tmp/fs
+
+
+insmod $R/usr/src/obd/class/obdclass.o 
+insmod $R/usr/src/obd/ext2obd/obdext2.o
+insmod $R/usr/src/obd/ost/ost.o
+insmod $R/usr/src/obd/osc/osc.o
+mknod /dev/obd c 10 241
+
+$R/usr/src/obd/utils/obdctl <<EOF
+device 0
+attach obdext2
+setup /dev/loop/0
+device 1
+attach ost
+setup 0
+quit
+EOF
+
+
+
+
