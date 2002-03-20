52b625220e61a0a5bac9604c9260f29766717c81
adilger <adilger>
2002-03-20 20:47:57 +0000

    Add "llrmount.sh" script, which does the same setup as llmount.sh, but does not create new filesystems.  It uses the existing filesystems from a previous invocation of llmount.sh.  This is useful for persistency testing or testing recoverability.

---
+++ lustre/tests/llrmount.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+SRCDIR="`dirname $0`"
+. $SRCDIR/common.sh
+
+setup
+
+$PTLCTL <<EOF
+mynid localhost
+setup tcp
+connect localhost 1234
+add_uuid self
+add_uuid mds
+add_uuid ost
+quit
+EOF
+
+old_fs ext2 /tmp/ost 80000
+OST=$LOOPDEV
+MDSFS=ext2
+old_fs ${MDSFS} /tmp/mds 10000
+MDS=$LOOPDEV
+
+echo 0xffffffff > /proc/sys/portals/debug
+
+$OBDCTL <<EOF
+device 0
+attach mds
+setup ${MDS} ${MDSFS}
+device 1
+attach obdext2
+setup ${OST}
+device 2
+attach ost
+setup 1
+device 3
+attach osc
+setup -1
+quit
+EOF
+
+mount -t lustre_light -o device=3 none /mnt/obd
