3c7325df09895db8ba3dd083e5660d3c0b5e7c15
adilger <adilger>
2002-03-22 18:57:56 +0000

    Update ostreq.sh script to use new helper functions.

--- lustre/tests/ostreq.sh
+++ lustre/tests/ostreq.sh
@@ -5,20 +5,9 @@ SRCDIR="`dirname $0`"
 
 SERVER=localhost
 
-mknod /dev/portals c 10 240
+setup
 
-insmod $R/usr/src/portals/linux/oslib/portals.o || exit -1
-insmod $R/usr/src/portals/linux/socknal/ksocknal.o || exit -1
-
-$R/usr/src/portals/linux/utils/acceptor 1234 &
-
-insmod $R/usr/src/obd/class/obdclass.o || exit -1
-insmod $R/usr/src/obd/rpc/ptlrpc.o || exit -1
-insmod $R/usr/src/obd/ext2obd/obdext2.o || exit -1
-insmod $R/usr/src/obd/ost/ost.o || exit -1
-insmod $R/usr/src/obd/osc/osc.o || exit -1
-
-$R/usr/src/portals/linux/utils/ptlctl <<EOF
+$PTLCTL <<EOF
 mynid localhost
 setup tcp
 connect $SERVER 1234
@@ -28,21 +17,13 @@ quit
 EOF
 
 
-dd if=/dev/zero of=/tmp/fs bs=1024 count=10000
-mke2fs -F /tmp/fs
-losetup ${LOOP}0 /tmp/fs || exit -1
+tmp_fs ext2 /tmp/fs 10000
+OBD=${LOOPDEV}
 
-echo 4095 > /proc/sys/obd/debug
-echo 4095 > /proc/sys/obd/trace
-
-mknod /dev/obd c 10 241
-
-$R/usr/src/portals/linux/utils/debugctl modules > $R/tmp/ogdb
-
-$R/usr/src/obd/utils/obdctl <<EOF
+$OBDCTL <<EOF
 device 0
 attach obdext2
-setup ${LOOP}0
+setup ${OBD}
 device 1
 attach ost
 setup 0
