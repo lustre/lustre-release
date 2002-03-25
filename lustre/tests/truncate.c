99ce67fa5579e70ff2275bf5c66f8534d7ca72f1
pschwan <pschwan>
2002-03-25 01:55:49 +0000

    - build truncate with LARGEFILE64_SOURCE - truncate's second argument is now in KB

--- lustre/tests/truncate.c
+++ lustre/tests/truncate.c
@@ -6,16 +6,17 @@
 
 int main(int argc, char **argv)
 {
-	unsigned long off;
+	unsigned long long off;
 	int err;
 
 	if (argc != 3) { 
-		printf("usage %s file offset\n", argv[0]); 
+		printf("usage %s file offset (kilobytes)\n", argv[0]); 
 		return 1;
 	}
 
 	off = strtoul(argv[2], NULL, 0);
-	err = truncate(argv[1], off); 
+	off *= 1024;
+	err = truncate64(argv[1], off); 
 	if ( err ) { 
 		printf("Error truncating %s: %s\n", argv[1], strerror(errno));
 	}
