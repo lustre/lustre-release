e42db7cb749e52fa08bcedfe1aa3aac001f71d30
cvs2svn <admin@example.com>
2002-05-08 07:07:44 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.50'.

--- lustre/tests/llext3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SRCDIR="`dirname $0`/"
-. $SRCDIR/common.sh
-
-export DEBUG_WAIT=yes
-. $SRCDIR/llsetup.sh $SRCDIR/net-local.cfg $SRCDIR/client-mount.cfg $SRCDIR/mds.cfg $SRCDIR/obdext2.cfg
-
-debug_client_on
