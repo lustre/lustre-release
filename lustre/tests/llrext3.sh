f5df803dc637a1d0a796ab03e98462398b53a9c4
adilger <adilger>
2002-05-09 20:19:59 +0000

    Exit on setup error.

--- lustre/tests/llrext3.sh
+++ lustre/tests/llrext3.sh
@@ -4,6 +4,7 @@ SRCDIR="`dirname $0`/"
 . $SRCDIR/common.sh
 
 export DEBUG_WAIT=yes
-. $SRCDIR/llrsetup.sh $SRCDIR/net-local.cfg $SRCDIR/client-mount.cfg $SRCDIR/mds.cfg $SRCDIR/obdext2.cfg
+. $SRCDIR/llrsetup.sh $SRCDIR/net-local.cfg $SRCDIR/client-mount.cfg $SRCDIR/mds.cfg $SRCDIR/obdext2.cfg || exit 2
 
 debug_client_on
+#debug_client_off
