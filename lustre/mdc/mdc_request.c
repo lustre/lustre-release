b546794d8894e7089bac94560578c9076cda965f
shaver <shaver>
2002-07-10 20:31:23 +0000

    mdc_connect needs to use a level of LUSTRE_CONN_NEW. (I broke mounting on the trunk before, this fixes it.)

--- lustre/mdc/mdc_request.c
+++ lustre/mdc/mdc_request.c
@@ -747,6 +747,7 @@ static int mdc_connect(struct lustre_handle *conn, struct obd_device *obd)
         if (!request)
                 GOTO(out_disco, -ENOMEM);
 
+        request->rq_level = LUSTRE_CONN_NEW;
         request->rq_replen = lustre_msg_size(0, NULL);
 
         rc = ptlrpc_queue_wait(request);
