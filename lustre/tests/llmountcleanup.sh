8502061ebedaed8e6fc8168b82955c67b879ab88
cvs2svn <admin@example.com>
2002-08-14 00:29:46 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.768'.

--- lustre/tests/llmountcleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SRCDIR="`dirname $0`/"
-. $SRCDIR/common.sh
-
-. $SRCDIR/llcleanup.sh $SRCDIR/net-local.cfg $SRCDIR/mds.cfg $SRCDIR/obdfilter.cfg $SRCDIR/client-mount.cfg $SRCDIR/ldlm.cfg || exit 2
