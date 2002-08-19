5fdf17222e3c4fbd5bbf1d86d6156225abad7333
rread <rread>
2002-08-19 22:16:56 +0000

    - generate some simple configs

---
+++ lustre/tests/local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+config=local.xml
+LMC=../utils/lmc
+
+# create nodes
+${LMC} -o $config --node localhost --net uml1 tcp 
+
+# configure mds server
+${LMC} -m $config --format  --node localhost --mds mds1 /tmp/mds1 50000
+
+# configure ost
+${LMC} -m $config --format --node localhost --ost /tmp/ost1 100000
+
+# create client config
+${LMC} -m $config --node localhost --mtpt /mnt/lustre mds1 OSC_localhost
---
+++ lustre/tests/lov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+config=lov.xml
+LMC=../utils/lmc
+
+echo "FIXME: autoformat is no by default, edit $config to change"
+
+# create nodes
+${LMC} -o $config --node localhost --net uml1 tcp 
+
+# configure mds server
+${LMC} -m $config --format --node localhost --mds mds1 /tmp/mds1 50000
+
+# configure ost
+${LMC} -m $config --lov lov1 mds1 4096 0 0
+${LMC} -m $config --format --node localhost --lov lov1 --ost /tmp/ost1 100000
+${LMC} -m $config --format --node localhost --lov lov1 --ost /tmp/ost2 100000
+
+# create client config
+${LMC} -m $config  --node localhost --mtpt /mnt/lustre mds1 lov1
