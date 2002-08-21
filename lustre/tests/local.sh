1ab1f42e241d3b789d971a2f06193f5512ea210a
braam <braam>
2002-08-21 03:53:09 +0000

    - put Robert's file back...

--- lustre/tests/local.sh
+++ lustre/tests/local.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-config=uml.xml
+config=local.xml
 LMC=../utils/lmc
 
-echo "FIXME: autoformat is no by default, edit $config to change"
-
 # create nodes
-${LMC} -o $config --node localhost --net localhost tcp 
+${LMC} -o $config --node localhost --net uml1 tcp 
 
 # configure mds server
-${LMC} -m $config  --node localhost --mds mds1 /tmp/mds1 50000
+${LMC} -m $config --format  --node localhost --mds mds1 /tmp/mds1 50000
 
 # configure ost
-${LMC} -m $config  --node localhost --ost /tmp/ost1 100000
-# is this needed?
-# ${LMC} -m $config  --node localhost --mdc MDC_mds1
+${LMC} -m $config --format --node localhost --ost /tmp/ost1 100000
 
 # create client config
-${LMC} -m $config  --node localhost --mtpt /mnt/lustre mds1 ost1
-
+${LMC} -m $config --node localhost --mtpt /mnt/lustre mds1 OSC_localhost
