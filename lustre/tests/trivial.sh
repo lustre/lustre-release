9e641ad15150b19eb8bbed819bf2eb6843c4d9be
adilger <adilger>
2002-09-07 06:47:07 +0000

    Invoke shell scripts via "sh" because CVS doesn't keep file perms well. Add some extra basic checks after the cleanup.

--- lustre/tests/trivial.sh
+++ lustre/tests/trivial.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Simple test of mount and unmount
-llsetup.sh obdecho.cfg net-local.cfg client-echo.cfg || exit 1
+sh llsetup.sh obdecho.cfg net-local.cfg client-echo.cfg || exit 1
 # FIXME: Scan logs for any unusual things (unbalanced allocations, errors)
-llcleanup.sh obdecho.cfg net-local.cfg client-echo.cfg
+sh llcleanup.sh obdecho.cfg net-local.cfg client-echo.cfg
+OBD_LEAK=`dmesg | awk '/obd memory leaked/ { print $7 }'`
+[ "$OBD_LEAK" != "0" ] && echo "OBD memory leak: $OBD_LEAK bytes" && ERR=1
+NAL_LEAK=`dmesg | awk '/NAL unloaded/ { print $7 }'
+[ "$NAL_LEAK" != "0)" ] && echo "Portals memory leak: $NAL_LEAK" && ERR=1
+/sbin/lsmod | grep -q portals && "Portals module still loaded" && ERR=1
+exit $ERR
