cf3ca60dae10d518b02f7bc6d5a1fae97e88d9e1
adilger <adilger>
2002-09-08 09:10:03 +0000

    Disable the lprocfs deregistration, now that I've disabled the registration.

--- lustre/obdclass/class_obd.c
+++ lustre/obdclass/class_obd.c
@@ -655,9 +655,11 @@ static int obd_class_ioctl (struct inode * inode, struct file * filp,
                         GOTO(out, err=-EBUSY);
                 }
 
+                /*
                 if (lprocfs_dereg_dev(obd) != LPROCFS_SUCCESS) {
                         CERROR("Could not remove /proc entry\n");
                 }
+                */
                 if (obd->obd_name) {
                         OBD_FREE(obd->obd_name, strlen(obd->obd_name)+1);
                         obd->obd_name = NULL;
