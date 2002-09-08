2acc8c4db7fb688debc33efda6ca29cc6f785f82
adilger <adilger>
2002-09-08 01:18:08 +0000

    Updated test_brw user program.  Checks args better, uses a long long for the iteration count, handles default args properly.

--- lustre/tests/test_brw.c
+++ lustre/tests/test_brw.c
@@ -14,11 +14,15 @@
 
 #define BLOCKSIZE 4096
 #define CERROR(fmt, arg...) fprintf(stderr, fmt, ## arg)
+#ifndef __u64
 #define __u64 long long
-#define LASSERT(v) do {} while(0)
 #define HTON__u64(v) (v)
+#endif
+
+#ifndef LPU64
 #define LPU64 "%Lu"
 #define LPX64 "%Lx"
+#endif
 
 #define READ  1
 #define WRITE 2
@@ -26,8 +30,6 @@
 #define LPDS sizeof(__u64)
 int page_debug_setup(void *addr, int len, __u64 off, __u64 id)
 {
-        LASSERT(addr);
-
         off = HTON__u64(off);
         id = HTON__u64(id);
         memcpy(addr, (char *)&off, LPDS);
@@ -40,12 +42,10 @@ int page_debug_setup(void *addr, int len, __u64 off, __u64 id)
         return 0;
 }
 
-int page_debug_check(char *who, void *addr, int end, __u64 off, __u64 id)
+int page_debug_check(char *who, void *addr, int size, __u64 off, __u64 id)
 {
         __u64 ne_off;
         int err = 0;
-
-        LASSERT(addr);
 
         ne_off = HTON__u64(off);
         id = HTON__u64(id);
@@ -60,7 +60,7 @@ int page_debug_check(char *who, void *addr, int end, __u64 off, __u64 id)
                 err = -EINVAL;
         }
 
-        addr += end - LPDS - LPDS;
+        addr += size - LPDS - LPDS;
         if (memcmp(addr, (char *)&ne_off, LPDS)) {
                 CERROR("%s: for offset "LPU64" end off: "LPX64" != "LPX64"\n",
                        who, off, *(__u64 *)addr, ne_off);
@@ -78,7 +78,8 @@ int page_debug_check(char *who, void *addr, int end, __u64 off, __u64 id)
 
 void usage(char *prog)
 {
-	fprintf(stderr, "usage: %s file count [[d]{r|w|rw} [pages_per_vec [objid]]]\n",
+	fprintf(stderr,
+		"usage: %s file count [[d]{r|w|rw} [pages_per_vec [objid]]]\n",
 		prog);
 	exit(1);
 }
@@ -87,47 +88,62 @@ int main(int argc, char **argv)
 {
         int fd;
         char *buf;
-        long pg_vec, count;
-	long len;
-	long long end, offset;
+	long long count, last, offset;
+	long pg_vec, len;
 	long long objid = 3;
-	int flags = O_RDWR | O_CREAT;
+	int flags = 0;
 	int cmd = 0;
+	char *end;
         int rc;
 
         if (argc < 3 || argc > 6)
 		usage(argv[0]);
 
-        count = strtoul(argv[2], 0, 0);
+        count = strtoull(argv[2], &end, 0);
+	if (*end) {
+		fprintf(stderr, "%s: invalid count '%s'\n", argv[0], argv[2]);
+		usage(argv[0]);
+	}
 	if (argc >= 4) {
 		if (strchr(argv[3], 'r')) {
-			cmd |= READ;
-			printf("reading\n");
+			cmd = READ;
+			flags = O_RDONLY;
 		}
 		if (strchr(argv[3], 'w')) {
 			cmd |= WRITE;
-			printf("writing\n");
+			flags = O_RDWR | O_CREAT;
 		}
 		if (strchr(argv[3], 'd')) {
 			flags |= O_DIRECT;
-			printf("directing\n");
+		}
+		if (!cmd)
+			usage(argv[0]);
+	} else {
+		cmd = READ | WRITE;
+		flags = O_RDWR | O_CREAT | O_DIRECT;
+	}
+
+	if (argc >= 5) {
+		pg_vec = strtoul(argv[4], &end, 0);
+		if (*end) {
+			fprintf(stderr, "%s: invalid pages_per_vec '%s'\n",
+				argv[0], argv[4]);
+			usage(argv[0]);
 		}
 	}
-	if (!cmd)
-		usage(argv[0]);
-	printf("cmd = %x, flags = %x\n", cmd, flags);
-
-	if (argc >= 5)
-		pg_vec = strtoul(argv[4], 0, 0);
 	len = pg_vec * BLOCKSIZE;
-	end = (long long)count * len;
+	last = (long long)count * len;
 
 	if (argc >= 6) {
-		objid = strtoull(argv[5], 0, 0);
-		printf("objid %s = 0x%Lx\n", argv[5], objid);
+		objid = strtoull(argv[5], &end, 0);
+		if (*end) {
+			fprintf(stderr, "%s: invalid objid '%s'\n",
+				argv[0], argv[5]);
+			usage(argv[0]);
+		}
 	}
 
-        printf("%s: %s on %s(objid 0x"LPX64") for %ldx%ld pages \n",
+        printf("%s: %s on %s(objid 0x"LPX64") for "LPU64"x%ld pages \n",
 	       argv[0], flags & O_DIRECT ? "directio" : "i/o",
 	       argv[1], objid, count, pg_vec);
 
@@ -138,14 +154,14 @@ int main(int argc, char **argv)
                 return 2;
         }
 
-        fd = open(argv[1], flags);
+        fd = open(argv[1], flags | O_LARGEFILE);
         if (fd == -1) {
-                fprintf(stderr, "%s: cannot open %s:  %s\n", argv[1],
-			argv[0], strerror(errno));
+                fprintf(stderr, "%s: cannot open %s:  %s\n", argv[0],
+			argv[1], strerror(errno));
                 return 3;
         }
 
-	for (offset = 0; offset < end && cmd & WRITE; offset += len) {
+	for (offset = 0; offset < last && cmd & WRITE; offset += len) {
 		int i;
 
 		for (i = 0; i < len; i += BLOCKSIZE)
@@ -172,7 +188,7 @@ int main(int argc, char **argv)
                 return 5;
         }
 
-	for (offset = 0; offset < end && cmd && READ; offset += len) {
+	for (offset = 0; offset < last && cmd && READ; offset += len) {
 		int i;
 
 		rc = read(fd, buf, len);
