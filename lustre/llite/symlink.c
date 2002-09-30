603d52bf911038c5196f65beb36c5339d2951479
pschwan <pschwan>
2002-09-30 03:01:43 +0000

    b=614622 All filesystems need to implement their own follow_link method, and we weren't. So we never followed a symlink.  Ever.
    This file doesn't bear even the slightest relation to ext2/symlink.c anymore,
    so I removed the old copyright notice.

--- lustre/llite/symlink.c
+++ lustre/llite/symlink.c
@@ -1,71 +1,99 @@
 /* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
  * vim:expandtab:shiftwidth=8:tabstop=8:
- *  linux/fs/ext2/symlink.c
  *
- * This code is issued under the GNU General Public License.
- * See the file COPYING in this distribution
+ *  Copyright (c) 2002 Cluster File Systems, Inc.
  *
- * Copyright (C) 1992, 1993, 1994, 1995
- * Remy Card (card@masi.ibp.fr)
- * Laboratoire MASI - Institut Blaise Pascal
- * Universite Pierre et Marie Curie (Paris VI)
+ *   This file is part of Lustre, http://www.lustre.org.
  *
- *  Copyright (C) 1991, 1992  Linus Torvalds
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
  *
- *  ext2 symlink handling code
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- * Modified for OBDFS:
- *  Copyright (C) 1999 Seagate Technology Inc. (author: braam@stelias.com)
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
-#include <linux/fs.h>
-#include <linux/mm.h>
-#include <linux/stat.h>
-#include <linux/locks.h>
 
 #define DEBUG_SUBSYSTEM S_LLITE
 
-#include <linux/obd_support.h> /* for ENTRY and EXIT only */
 #include <linux/lustre_lite.h>
+
+static int ll_readlink_internal(struct inode *inode,
+                                struct ptlrpc_request **request, char **symname)
+{
+        struct ll_inode_info *lli = ll_i2info(inode);
+        struct ll_sb_info *sbi = ll_i2sbi(inode);
+        int rc, len = inode->i_size + 1;
+
+        *request = NULL;
+
+        if (lli->lli_symlink_name) {
+                *symname = lli->lli_symlink_name;
+                CDEBUG(D_INODE, "using cached symlink %s\n", *symname);
+                RETURN(0);
+        }
+
+        rc = mdc_getattr(&sbi->ll_mdc_conn, inode->i_ino, S_IFLNK,
+                         OBD_MD_LINKNAME, len, request);
+        if (rc) {
+                CERROR("inode "LPD64": rc = %d\n", inode->i_ino, rc);
+                RETURN(rc);
+        }
+
+        *symname = lustre_msg_buf((*request)->rq_repmsg, 1);
+
+        OBD_ALLOC(lli->lli_symlink_name, len);
+        /* do not return an error if we cannot cache the symlink locally */
+        if (lli->lli_symlink_name)
+                memcpy(lli->lli_symlink_name, *symname, len);
+
+        RETURN(0);
+}
 
 static int ll_readlink(struct dentry *dentry, char *buffer, int buflen)
 {
-        struct ll_sb_info *sbi = ll_i2sbi(dentry->d_inode);
-        struct ptlrpc_request *request = NULL;
         struct inode *inode = dentry->d_inode;
         struct ll_inode_info *lli = ll_i2info(inode);
-        int len = inode->i_size + 1;
+        struct ptlrpc_request *request;
         char *symname;
         int rc;
         ENTRY;
 
         /* on symlinks lli_open_sem protects lli_symlink_name allocation/data */
         down(&lli->lli_open_sem);
-        if (lli->lli_symlink_name) {
-                symname = lli->lli_symlink_name;
-                CDEBUG(D_INODE, "using cached symlink %s\n", symname);
-                GOTO(out_readlink, rc = 0);
-        }
+        rc = ll_readlink_internal(inode, &request, &symname);
+        if (rc)
+                GOTO(out, rc);
 
-        rc = mdc_getattr(&sbi->ll_mdc_conn, inode->i_ino, S_IFLNK,
-                         OBD_MD_LINKNAME, len, &request);
-
-        if (rc) {
-                CERROR("inode %d readlink: rc = %ld\n", rc, inode->i_ino);
-                GOTO(out_readlink_sem, rc);
-        }
-
-        symname = lustre_msg_buf(request->rq_repmsg, 1);
-
-        OBD_ALLOC(lli->lli_symlink_name, len);
-        /* do not return an error if we cannot cache the symlink locally */
-        if (lli->lli_symlink_name)
-                memcpy(lli->lli_symlink_name, symname, len);
-
-out_readlink:
         rc = vfs_readlink(dentry, buffer, buflen, symname);
+ out:
+        up(&lli->lli_open_sem);
+        ptlrpc_free_req(request);
 
-out_readlink_sem:
+        RETURN(rc);
+}
+
+static int ll_follow_link(struct dentry *dentry, struct nameidata *nd)
+{
+        struct inode *inode = dentry->d_inode;
+        struct ll_inode_info *lli = ll_i2info(inode);
+        struct ptlrpc_request *request;
+        char *symname;
+        int rc;
+        ENTRY;
+
+        down(&lli->lli_open_sem);
+        rc = ll_readlink_internal(inode, &request, &symname);
+        if (rc)
+                GOTO(out, rc);
+
+	rc = vfs_follow_link(nd, symname);
+ out:
         up(&lli->lli_open_sem);
         ptlrpc_free_req(request);
 
@@ -75,5 +103,6 @@ out_readlink_sem:
 extern int ll_setattr(struct dentry *de, struct iattr *attr);
 struct inode_operations ll_fast_symlink_inode_operations = {
         readlink:       ll_readlink,
-        setattr:        ll_setattr
+        setattr:        ll_setattr,
+        follow_link:    ll_follow_link
 };
