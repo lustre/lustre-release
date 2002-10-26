ef7f79de0061e39fa7f20caee5b0c65220c5163f
pschwan <pschwan>
2002-10-26 18:54:22 +0000

    Oops, one more.

--- lustre/obdfilter/filter.c
+++ lustre/obdfilter/filter.c
@@ -384,7 +384,7 @@ static struct file *filter_obj_open(struct obd_export *export,
                 atomic_inc(&fdd->fdd_open_count);
         } else {
                 atomic_set(&fdd->fdd_open_count, 1);
-                spin_lock_init(&fdd->fo_fddlock);
+                spin_lock_init(&filter->fo_fddlock);
                 fdd->fdd_flags = 0;
                 /* If this is racy, then we can use {cmp}xchg and atomic_add */
                 file->f_dentry->d_fsdata = fdd;
