a15fe9d88d5277f502c481252f469fabc71d2c42
adilger <adilger>
2002-10-28 17:40:28 +0000

    Avoid CVS permission problems.

--- lustre/tests/llrmount.sh
+++ lustre/tests/llrmount.sh
@@ -7,7 +7,7 @@ config=$NAME.xml
 mkconfig=./$NAME.sh
 
 if [ ! -f $config -o $mkconfig -nt $config ]; then
-   $mkconfig $config || exit 1
+   sh $mkconfig $config || exit 1
 fi
 
 ${LCONF} --gdb $config || exit 2
