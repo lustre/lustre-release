2a951fd0943153d30bdc30d7645ea9be4591b426
adilger <adilger>
2002-11-12 18:50:44 +0000

    Widesweeping change to support different-sized LOV MD data (b=307,345, and partially 314).  The goal is to remove all knowledge of MD data from llite and the MDS, and keep it solely inside OSC and LOV.  The change is not entirely complete (for enh 314) in that we still pass "struct lov_stripe_md" and "struct lov_mds_md" structs around, but that was mostly for sanity to avoid getting the two screwed up during this large amount of change.
    OSC passes runtests, and LOV has a _huge_ bug in it that dies after a
    single file copy+cleanup, so that's why it's on a branch.
    
    This adds two new OBD API methods (I'm not totally stuck on the names yet):
    - o_packmd to alloc/pack/free data for wire transport and storage on disk
    - o_unpackmd to alloc/pack/free data for in-memory and storage on disk
    
    There are helper functions obd_{alloc,size,free}_{mem,wire}md() which call
    the above two OBD API methods with the correct parameters to just allocate
    or size or free an MD.  This totally isolates anything outside LOV/OSC from
    knowing anything of the internals of the MD data struct, and lays the
    groundwork for LOV stacking in the future.
    
    The user-space LOV striping setup has also changed slightly to use the
    "MDS/wire/disk" MD format for communication instead of the in-memory
    MD format, and the lovstripe.c test, utils/lfind.c and new utils/lstripe.c
    program (formerly makestripe.c from Kevin Harlan) have been updated to use
    this new interface.  The lstripe.c utility allows easy creation of files
    with a specific striping pattern.

---
+++ lustre/lov/lov_pack.c
@@ -0,0 +1,178 @@
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ *
+ *  Copyright (C) 2002 Cluster File Systems, Inc. <adilger@clusterfs.com>
+ *
+ *   This file is part of Lustre, http://www.lustre.org.
+ *
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * (Un)packing of OST/MDS requests
+ *
+ */
+
+#define DEBUG_SUBSYSTEM S_LLITE
+
+#include <linux/lustre_net.h>
+#include <linux/obd.h>
+#include <linux/obd_lov.h>
+#include <linux/obd_support.h>
+
+void lov_packdesc(struct lov_desc *ld)
+{
+        ld->ld_tgt_count = HTON__u32(ld->ld_tgt_count);
+        ld->ld_default_stripe_count = HTON__u32(ld->ld_default_stripe_count);
+        ld->ld_default_stripe_size = HTON__u32(ld->ld_default_stripe_size);
+        ld->ld_pattern = HTON__u32(ld->ld_pattern);
+}
+
+void lov_unpackdesc(struct lov_desc *ld)
+{
+        ld->ld_tgt_count = NTOH__u32(ld->ld_tgt_count);
+        ld->ld_default_stripe_count = HTON__u32(ld->ld_default_stripe_count);
+        ld->ld_default_stripe_size = HTON__u32(ld->ld_default_stripe_size);
+        ld->ld_pattern = HTON__u32(ld->ld_pattern);
+}
+
+/* Pack LOV object metadata for shipment to the MDS.
+ *
+ * XXX In the future, this will be enhanced to get the EA size from the
+ *     underlying OSC device(s) to get their EA sizes so we can stack
+ *     LOVs properly.  For now lov_mds_md_size() just assumes one obd_id
+ *     per stripe.
+ */
+int lov_packmd(struct lustre_handle *conn, struct lov_mds_md **lmmp,
+               struct lov_stripe_md *lsm)
+{
+        struct obd_device *obd = class_conn2obd(conn);
+        struct lov_obd *lov = &obd->u.lov;
+        struct lov_oinfo *loi;
+        struct lov_mds_md *lmm;
+        int ost_count = lov->desc.ld_tgt_count;
+        int stripe_count = ost_count;
+        int lmm_size;
+        int i;
+
+        if (lsm)
+                stripe_count = lsm->lsm_stripe_count;
+
+        /* XXX LOV STACKING call into osc for sizes */
+        lmm_size = lov_mds_md_size(stripe_count);
+
+        if (!lmmp)
+                RETURN(lmm_size);
+
+        if (*lmmp && !lsm) {
+                /* endianness */
+                stripe_count = ((*lmmp)->lmm_stripe_count);
+                OBD_FREE(*lmmp, lov_stripe_md_size(stripe_count));
+                *lmmp = NULL;
+                RETURN(0);
+        }
+
+        if (!*lmmp) {
+                OBD_ALLOC(*lmmp, lmm_size);
+                if (!*lmmp)
+                        RETURN(-ENOMEM);
+        }
+        lmm->lmm_stripe_count = (stripe_count);
+        if (!lsm)
+                RETURN(lmm_size);
+        lmm = *lmmp;
+
+        /* XXX endianness */
+        lmm->lmm_magic = (lsm->lsm_magic);
+        lmm->lmm_object_id = (lsm->lsm_object_id);
+        lmm->lmm_stripe_size = (lsm->lsm_stripe_size);
+        lmm->lmm_stripe_pattern = (lsm->lsm_stripe_pattern);
+        lmm->lmm_stripe_offset = (lsm->lsm_stripe_offset);
+
+        /* Only fill in the object ids which we are actually using.
+         * Assumes lmm_objects is otherwise zero-filled. */
+        for (i = 0, loi = lsm->lsm_oinfo; i < stripe_count; i++, loi++)
+                /* XXX call down to osc_packmd() to do the packing */
+                lmm->lmm_objects[loi->loi_ost_idx].l_object_id = (loi->loi_id);
+
+        return lmm_size;
+}
+
+int lov_unpackmd(struct lustre_handle *conn, struct lov_stripe_md **lsmp,
+                 struct lov_mds_md *lmm)
+{
+        struct obd_device *obd = class_conn2obd(conn);
+        struct lov_obd *lov = &obd->u.lov;
+        struct lov_stripe_md *lsm;
+        struct lov_oinfo *loi;
+        int ost_count = lov->desc.ld_active_tgt_count;
+        int ost_offset = 0;
+        int stripe_count = 0;
+        int lsm_size;
+        int i;
+
+        if (lmm)
+                /* endianness */
+                stripe_count = (lmm->lmm_stripe_count);
+
+        if (!stripe_count)
+                stripe_count = lov->desc.ld_default_stripe_count;
+        if (!stripe_count || stripe_count > ost_count)
+                stripe_count = ost_count;
+
+        /* XXX LOV STACKING call into osc for sizes */
+        lsm_size = lov_stripe_md_size(stripe_count);
+
+        if (!lsmp)
+                RETURN(lsm_size);
+
+        if (*lsmp && !lmm) {
+                stripe_count = (*lsmp)->lsm_stripe_count;
+                OBD_FREE(*lsmp, lov_stripe_md_size(stripe_count));
+                *lsmp = NULL;
+                RETURN(0);
+        }
+
+        if (!*lsmp) {
+                OBD_ALLOC(*lsmp, lsm_size);
+                if (!*lsmp)
+                        RETURN(-ENOMEM);
+        }
+
+        lsm->lsm_stripe_count = stripe_count;
+        if (!lmm)
+                RETURN(lsm_size);
+
+        lsm = *lsmp;
+
+        /* XXX endianness */
+        ost_offset = lsm->lsm_stripe_offset = (lmm->lmm_stripe_offset);
+        lsm->lsm_magic = (lmm->lmm_magic);
+        lsm->lsm_object_id = (lmm->lmm_object_id);
+        lsm->lsm_stripe_size = (lmm->lmm_stripe_size);
+        lsm->lsm_stripe_pattern = (lmm->lmm_stripe_pattern);
+
+        for (i = 0, loi = lsm->lsm_oinfo; i < ost_count; i++, ost_offset++) {
+                ost_offset %= ost_count;
+
+                if (!lmm->lmm_objects[ost_offset].l_object_id)
+                        continue;
+
+                LASSERT(loi - lsm->lsm_oinfo < stripe_count);
+                /* XXX LOV STACKING call down to osc_unpackmd() */
+                loi->loi_id = (lmm->lmm_objects[ost_offset].l_object_id);
+                loi->loi_ost_idx = ost_offset;
+                loi++;
+        }
+
+        return lsm_size;
+}
