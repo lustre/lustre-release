4c865c3cb8965e9274478e0c52a038f81726cc01
shaver <shaver>
2002-11-25 18:25:52 +0000

    HEAD->b_recovery merge.

--- lustre/tests/mcr.sh
+++ lustre/tests/mcr.sh
@@ -12,7 +12,6 @@ ROUTER=dev5
 CLIENT_LO=dev2
 CLIENT_HI=dev25
 
-PORT=988
 TCPBUF=1048576
  
 
@@ -29,14 +28,14 @@ h2ip () {
 # Client node
 ${LMC} --node client --net '*' elan || exit 1
 # Router node
-${LMC} --router --node $ROUTER --tcpbuf $TCPBUF --net `h2ip $ROUTER`  tcp $PORT || exit 1
+${LMC} --router --node $ROUTER --tcpbuf $TCPBUF --net `h2ip $ROUTER`  tcp || exit 1
 ${LMC} --node $ROUTER --net `h2elan $ROUTER` elan|| exit 1
 ${LMC} --node $ROUTER --route elan `h2elan $ROUTER` `h2elan $CLIENT_LO` `h2elan $CLIENT_HI` || exit 2
 
 for s in $SERVERS
  do
    # server node
-   ${LMC} --node $s --tcpbuf $TCPBUF --net $s tcp $PORT || exit 1
+   ${LMC} --node $s --tcpbuf $TCPBUF --net $s tcp || exit 1
    # route to server
    ${LMC} --node $ROUTER --route tcp `h2ip $ROUTER` $s || exit 2
    # the device on the server
