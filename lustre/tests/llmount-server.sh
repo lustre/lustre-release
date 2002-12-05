64bb3a24b02ffc6f620d77a358dfdc66e17a847b
cvs2svn <admin@example.com>
2002-12-05 08:42:38 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.18.4'.

--- lustre/tests/llmount-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SRCDIR="`dirname $0`/"
-. $SRCDIR/common.sh
-
-export DEBUG_WAIT=yes
-. $SRCDIR/llsetup.sh $SRCDIR/net-server.cfg $SRCDIR/ldlm.cfg $SRCDIR/mds.cfg $SRCDIR/obdfilter.cfg || exit 2
-
-debug_client_on
-#debug_client_off
