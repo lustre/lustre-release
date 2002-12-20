05d4d3d3cdbeae2eec7481e5fa8ce9efac83ba90
pschwan <pschwan>
2002-12-20 19:07:23 +0000

    merge HEAD into b_md

--- lustre/tests/mount2fs.sh
+++ lustre/tests/mount2fs.sh
@@ -20,23 +20,23 @@ OSTNODE=uml2
 CLIENT=uml3
 
 # create nodes
-${LMC} -o $config --node $MDSNODE --net $MDSNODE tcp || exit 1
-${LMC} -m $config --node $OSTNODE --net $OSTNODE tcp || exit 2
-${LMC} -m $config --node $CLIENT --net $CLIENT tcp || exit 3
+${LMC} -o $config --add net --node $MDSNODE --nid $MDSNODE --nettype tcp || exit 1
+${LMC} -m $config --add net --node $OSTNODE --nid $OSTNODE --nettype tcp || exit 2
+${LMC} -m $config --add net --node $CLIENT --nid $CLIENT --nettype tcp || exit 3
 
 # configure mds server
-${LMC} -m $config --format --node $MDSNODE --mds mds1 $MDSDEV $MDSSIZE ||exit 10
-${LMC} -m $config --format --node $MDSNODE --mds mds2 $MDSDEV2 $MDSSIZE ||exit 10
+${LMC} -m $config --format --add mds --node $MDSNODE --mds mds1 --dev $MDSDEV --size $MDSSIZE ||exit 10
+${LMC} -m $config --format --add mds --node $MDSNODE --mds mds2 --dev $MDSDEV2 --size $MDSSIZE ||exit 10
 
 # configure ost
-${LMC} -m $config  --lov lov1 mds1 65536 0 0 || exit 20
-${LMC} -m $config  --lov lov2 mds2 65536 0 0 || exit 20
-${LMC} -m $config --node $OSTNODE --lov lov1 --ost $OSTDEV1 $OSTSIZE || exit 21
-${LMC} -m $config --node $OSTNODE --lov lov2 --ost $OSTDEV2 $OSTSIZE || exit 22
+${LMC} -m $config --add lov --lov lov1 --mds mds1 --stripe_sz 65536 --stripe_cnt 0 --stripe_pattern 0 || exit 20
+${LMC} -m $config --add lov --lov lov2 --mds mds2 --stripe_sz 65536 --stripe_cnt 0 --stripe_pattern 0 || exit 20
+${LMC} -m $config --add ost --node $OSTNODE --lov lov1 --dev $OSTDEV1 --size $OSTSIZE || exit 21
+${LMC} -m $config --add ost --node $OSTNODE --lov lov2 --dev $OSTDEV2 --size $OSTSIZE || exit 22
 
 # create client config
-${LMC} -m $config  --node $CLIENT --mtpt /mnt/lustre mds1 lov1 || exit 30
-${LMC} -m $config  --node $CLIENT --mtpt /mnt/lustre2 mds2 lov2 || exit 30
+${LMC} -m $config --add mtpt --node $CLIENT --path /mnt/lustre --mds mds1 --lov lov1 || exit 30
+${LMC} -m $config --add mtpt --node $CLIENT --path /mnt/lustre2 --mds mds2 --lov lov2 || exit 30
 
 
 
