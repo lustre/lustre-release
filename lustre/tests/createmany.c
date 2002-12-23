f21ed2dc107853983785323f8adfa3ad3dcdeb0e
pschwan <pschwan>
2002-12-23 04:59:00 +0000

    createmany supports -o to open, -m to mcreate

--- lustre/tests/createmany.c
+++ lustre/tests/createmany.c
@@ -12,29 +12,55 @@ int main(int argc, char ** argv)
 {
         int i, rc, count;
         char filename[4096];
+        int do_open;
+        long int start, last;
 
-        if (argc < 3) {
-                printf("Usage %s filenamebase count\n", argv[0]);
+        if (argc < 4) {
+                printf("Usage %s <-o|-m> filenamebase count\n", argv[0]);
                 return 1;
         }
 
-        if (strlen(argv[1]) > 4080) {
+        if (strcmp(argv[1], "-o") == 0) {
+                do_open = 1;
+        } else if (strcmp(argv[1], "-m") == 0) {
+                do_open = 0;
+        } else {
+                printf("Usage %s <-o|-m> filenamebase count\n", argv[0]);
+                return 1;
+        }
+
+        if (strlen(argv[2]) > 4080) {
                 printf("name too long\n");
                 return 1;
         }
 
-        count = strtoul(argv[2], NULL, 0);
+        count = strtoul(argv[3], NULL, 0);
 
-        for (i=0 ; i < count ; i++) {
-                sprintf(filename, "%s-%d", argv[1], i);
-                rc = mknod(filename, S_IFREG| 0444, 0);
-                if (rc) {
-                        printf("mknod(%s) error: %s\n",
-                               filename, strerror(errno));
-                        break;
+        start = last = time(0);
+
+        for (i = 0; i < count; i++) {
+                sprintf(filename, "%s-%d", argv[2], i);
+                if (do_open) {
+                        rc = open(filename, O_CREAT|O_RDWR, 0644);
+                        if (rc < 0) {
+                                printf("open(%s) error: %s\n", filename,
+                                       strerror(errno));
+                                break;
+                        }
+                        close(rc);
+                } else {
+                        rc = mknod(filename, S_IFREG| 0444, 0);
+                        if (rc) {
+                                printf("mknod(%s) error: %s\n",
+                                       filename, strerror(errno));
+                                break;
+                        }
                 }
-		if ((i % 10000) == 0)
-		    printf(" - created %d (time %ld)\n", i, time(0));
+		if ((i % 10000) == 0) {
+                        printf(" - created %d (time %ld ; total %ld ; last %ld)\n",
+                               i, time(0), time(0) - start, last);
+                        last = time(0);
+                }
         }
         return rc;
 }
