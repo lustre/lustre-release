70bb12b49d98172b9a5cd449533ec8a25c60c34a
adilger <adilger>
2003-01-09 23:49:42 +0000

    Merge of HEAD into b_intel. Some fixes along the way - whitespace cleanup, etc.
    Also made obd_statfs_self() _way_ more efficient, which is pretty crucial
    given that it is called once for almost every entry in lprocfs.

--- lustre/utils/lstripe.c
+++ lustre/utils/lstripe.c
@@ -17,28 +17,11 @@
 
 void usage(char *pgm)
 {
-	fprintf(stderr, "\nIncorrect parameters!  Correct usage:\n\n" );
-	fprintf(stderr, "%s <output filename> <stripe size> <OST #> <stripe #>\n", pgm);
+	fprintf(stderr, "usage: %s <filename> <stripe size> <start stripe> <stripe count>\n", pgm);
 
-	fprintf(stderr, "\n\nArgument explanations:\n---------------------\n\n");
-	fprintf(stderr, "<output filename> = the full name and path of the output file to create\n");
-	fprintf(stderr, "<stripe size> = the number of bytes to have in each stripe.\n");
-	fprintf(stderr, "<OST #> = the OST number to start the striping on.\n");
-	fprintf(stderr, "<stripe #> = the number of stripes to use.\n");
-
-	fprintf(stderr, "\n\nExamples:\n---------\n\n");
-
-	fprintf(stderr, "%s /mnt/lustre/ost1 131072 0 1\n", pgm);
-	fprintf(stderr, "\t\tcreates a file only on ost1.\n\n");
-
-	fprintf(stderr, "%s /mnt/lustre/ost2 131072 1 1\n", pgm);
-	fprintf(stderr, "\t\tcreates a file only on ost2.\n\n");
-
-	fprintf(stderr, "%s /mnt/lustre/ost1and2 131072 0 2\n", pgm);
-	fprintf(stderr, "\t\tcreates a 128k file with 2 stripes, on ost1 and ost2.\n");
-
-	fprintf(stderr, "%s /mnt/lustre/ost1and2 131072 1 2\n", pgm);
-	fprintf(stderr, "\t\tcreates a 128k file with 2 stripes, on ost2 and ost1.\n");
+	fprintf(stderr, "\tstripe size: number of bytes in each stripe\n");
+	fprintf(stderr, "\tstripe start: OST index which holds first stripe\n");
+	fprintf(stderr, "\tstripe count: number of OSTs to stripe over\n");
 }
 
 int create_file(char *name, long stripe_size, int stripe_offset,
@@ -49,7 +32,6 @@ int create_file(char *name, long stripe_size, int stripe_offset,
 
 	/*  Initialize IOCTL striping pattern structure  */
 	a_striping.lmm_magic = LOV_MAGIC;
-	a_striping.lmm_stripe_pattern = 0;
 	a_striping.lmm_stripe_size = stripe_size;
 	a_striping.lmm_stripe_offset = stripe_offset;
 	a_striping.lmm_stripe_count = stripe_count;
