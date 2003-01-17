d3bf1bb56d4202e71b4459a60fe6852886bd1f5b
rread <rread>
2003-01-17 01:07:11 +0000

    * it bothers me to no end that lconf needs the portals  directory for cleanup

--- lustre/tests/llrmount.sh
+++ lustre/tests/llrmount.sh
@@ -6,8 +6,12 @@ NAME=${NAME:-local}
 config=$NAME.xml
 mkconfig=$NAME.sh
 
+if [ "$PORTALS" ]; then
+  portals_opt="--portals=$PORTALS"
+fi
+
 if [ ! -f $config -o $mkconfig -nt $config ]; then
    sh $mkconfig $config || exit 1
 fi
 
-${LCONF} --gdb $config || exit 2
+${LCONF} $portals_opt --gdb $config || exit 2
