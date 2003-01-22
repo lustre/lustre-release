a8497f9050d172c40ad77f69ed5029ec1ee1fe3c
pschwan <pschwan>
2003-01-22 17:42:30 +0000

    use the second create.pl mode in acceptance-metadata

--- lustre/tests/acceptance-metadata-single.sh
+++ lustre/tests/acceptance-metadata-single.sh
@@ -16,6 +16,10 @@ echo "create.pl, 1 mount, 1 thread, 10 ops, debug on"
 perl create.pl -- $MNT -1 10
 echo "create.pl, 1 mount, 1 thread, 100 ops, debug on"
 perl create.pl --silent -- $MNT -1 100
+echo "create.pl --mcreate=0, 1 mount, 1 thread, 10 ops, debug on"
+perl create.pl --mcreate=0 -- $MNT -1 10
+echo "create.pl --mcreate=0, 1 mount, 1 thread, 100 ops, debug on"
+perl create.pl --mcreate=0 --silent -- $MNT -1 100
 echo "rename.pl, 1 mount, 1 thread, 10 ops, debug on"
 perl rename.pl $MNT 10
 echo "rename.pl, 1 mount, 1 thread, 100 ops, debug on"
@@ -24,6 +28,8 @@ perl rename.pl --silent $MNT 100
 debug_client_off
 echo "create.pl, 1 mount, 1 thread, 1000 ops, debug off"
 perl create.pl --silent -- $MNT -1 1000
+echo "create.pl --mcreate=0, 1 mount, 1 thread, 1000 ops, debug off"
+perl create.pl --silent --mcreate=0 -- $MNT -1 1000
 echo "rename.pl, 1 mount, 1 thread, 1000 ops, debug off"
 perl rename.pl --silent $MNT 1000
 
@@ -31,6 +37,10 @@ debug_client_on
 echo "create.pl, 1 mount, 2 threads, 100 ops, debug on"
 perl create.pl --silent -- $MNT -1 100 &
 perl create.pl --silent -- $MNT -1 100 &
+wait
+echo "create.pl --mcreate=0, 1 mount, 2 threads, 100 ops, debug on"
+perl create.pl --silent --mcreate=0 -- $MNT -1 100 &
+perl create.pl --silent --mcreate=0 -- $MNT -1 100 &
 wait
 echo "rename.pl, 1 mount, 2 thread, 1000 ops, debug on"
 perl rename.pl --silent $MNT 1000 &
@@ -42,6 +52,10 @@ echo "create.pl, 1 mount, 2 threads, 2000 ops, debug off"
 perl create.pl --silent -- $MNT -1 2000 &
 perl create.pl --silent -- $MNT -1 2000 &
 wait
+echo "create.pl --mcreate=0, 1 mount, 2 threads, 2000 ops, debug off"
+perl create.pl --silent --mcreate=0 -- $MNT -1 2000 &
+perl create.pl --silent --mcreate=0 -- $MNT -1 2000 &
+wait
 echo "rename.pl, 1 mount, 2 threads, 2000 ops, debug off"
 perl rename.pl --silent $MNT 2000 &
 perl rename.pl --silent $MNT 2000 &
@@ -51,6 +65,11 @@ debug_client_on
 echo "create.pl, 1 mount, 4 threads, 100 ops, debug on"
 for i in `seq 1 4`; do
   perl create.pl --silent -- $MNT -1 100 &
+done
+wait
+echo "create.pl --mcreate=0, 1 mount, 4 threads, 100 ops, debug on"
+for i in `seq 1 4`; do
+  perl create.pl --silent --mcreate=0 -- $MNT -1 100 &
 done
 wait
 echo "rename.pl, 1 mount, 4 threads, 2000 ops, debug on"
@@ -65,6 +84,11 @@ for i in `seq 1 4`; do
   perl create.pl --silent -- $MNT -1 2000 &
 done
 wait
+echo "create.pl --mcreate=0, 1 mount, 4 threads, 2000 ops, debug off"
+for i in `seq 1 4`; do
+  perl create.pl --silent --mcreate=0 -- $MNT -1 2000 &
+done
+wait
 echo "rename.pl, 1 mount, 4 threads, 2000 ops, debug off"
 for i in `seq 1 4`; do
   perl rename.pl --silent $MNT 2000 &
@@ -72,9 +96,14 @@ done
 wait
 
 debug_client_on
-echo "create.pl, 1 mount, 8 threads, 100 ops, debug on"
+echo "create.pl, 1 mount, 8 threads, 500 ops, debug on"
 for i in `seq 1 8`; do
-  perl create.pl --silent -- $MNT -1 100 &
+  perl create.pl --silent -- $MNT -1 500 &
+done
+wait
+echo "create.pl --mcreate=0, 1 mount, 8 threads, 500 ops, debug on"
+for i in `seq 1 8`; do
+  perl create.pl --silent --mcreate=0 -- $MNT -1 500 &
 done
 wait
 echo "rename.pl, 1 mount, 8 threads, 2000 ops, debug on"
@@ -87,6 +116,11 @@ debug_client_off
 echo "create.pl, 1 mount, 8 threads, 2000 ops, debug off"
 for i in `seq 1 8`; do
   perl create.pl --silent -- $MNT -1 2000 &
+done
+wait
+echo "create.pl --mcreate=0, 1 mount, 8 threads, 2000 ops, debug off"
+for i in `seq 1 8`; do
+  perl create.pl --silent --mcreate=0 -- $MNT -1 2000 &
 done
 wait
 echo "rename.pl, 1 mount, 8 threads, 2000 ops, debug off"
