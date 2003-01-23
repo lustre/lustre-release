3bcf827ff0d047ccd89723322d5897f5d954f99e
zab <zab>
2003-01-23 19:09:24 +0000

    - add fs_sink filter method which blocks until the backing filter is synced.   ext* uses ext*_force_commit, reiser is unimplemented - add /proc/sys/lustre/filter_sync_on_commit tunable, disabled by default.   when enabled it calls the filter sync after each filter_commitrw for write

--- lustre/obdclass/fsfilt_ext3.c
+++ lustre/obdclass/fsfilt_ext3.c
@@ -271,6 +271,11 @@ static int fsfilt_ext3_statfs(struct super_block *sb, struct statfs *sfs)
         return rc;
 }
 
+static int fsfilt_ext3_sync(struct super_block *sb)
+{
+        return ext3_force_commit(sb);
+}
+
 static struct fsfilt_operations fsfilt_ext3_ops = {
         fs_type:                "ext3",
         fs_owner:               THIS_MODULE,
@@ -283,6 +288,7 @@ static struct fsfilt_operations fsfilt_ext3_ops = {
         fs_journal_data:        fsfilt_ext3_journal_data,
         fs_set_last_rcvd:       fsfilt_ext3_set_last_rcvd,
         fs_statfs:              fsfilt_ext3_statfs,
+        fs_sync:                fsfilt_ext3_sync,
 };
 
 static int __init fsfilt_ext3_init(void)
