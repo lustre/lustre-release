cb5c7a346370feca08f854e90f616b8c8891efae
rread <rread>
2003-01-30 22:25:56 +0000

    - give echo_client a fighting chance

--- lustre/tests/echo.sh
+++ lustre/tests/echo.sh
@@ -32,11 +32,11 @@ $LMC --add net --node $SERVER --nid $SERVER --nettype $NET || exit 2
 if (($LOV)); then
     $LMC --add mds --node $SERVER --mds mds1 --dev $MDSDEV --size $MDSSIZE || exit 10
     $LMC --add lov --lov lov1 --mds mds1 --stripe_sz $STRIPE_BYTES --stripe_cnt $STRIPES_PER_OBJ --stripe_pattern 0 || exit 11
-    $LMC --add ost --node $SERVER --lov lov1 --obdtype=obdecho || exit 12
-    $LMC --add ost --node $SERVER --lov lov1 --obdtype=obdecho || exit 13
+    $LMC --add ost --node $SERVER --lov lov1 --osdtype=obdecho || exit 12
+    $LMC --add ost --node $SERVER --lov lov1 --osdtype=obdecho || exit 13
     OBD_NAME=lov1
 else
-    $LMC --add ost --obd obd1 --node $SERVER --obdtype=obdecho || exit 12
+    $LMC --add ost --ost obd1 --node $SERVER --osdtype=obdecho || exit 12
     OBD_NAME=obd1
 fi
 
@@ -45,5 +45,5 @@ if [ "$SERVER" != "$CLIENT" ]; then
    $LMC --add net --node $CLIENT --nid $CLIENT --nettype $NET || exit 2
 fi
 
-$LMC --add echo_client --node $CLIENT --obd ${OBD_NAME} || exit 3
+$LMC --add echo_client --node $CLIENT --ost ${OBD_NAME} || exit 3
 
