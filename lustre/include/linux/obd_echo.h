3c82342308c3a32c8b0586021c06ccbe3a71edc6
eeb <eeb>
2003-02-05 17:45:07 +0000

    *  Added lock enqueue/cancel ioctl interface to echo_client, with proper    teardown on (possibly unexpected) client exit.
    *  added '-l' flag to utils/obdio, so that it locks the extent it writes,
       then reads back.
    
    *  took 'filter' prefix off /proc stats interface in filter and copied it
       into the echo OBD; obdstats takes obd type name parameter as well as
       optional repeat interval.

--- lustre/include/linux/obd_echo.h
+++ lustre/include/linux/obd_echo.h
@@ -13,6 +13,8 @@
 struct ec_object
 {
 	struct list_head       eco_obj_chain;
+	struct list_head       eco_lockhandles;
+	struct obd_device     *eco_device;
 	int                    eco_refcount;
 	int                    eco_deleted;
 	obd_id                 eco_id;
@@ -24,6 +26,22 @@ struct ec_open_object
 	struct list_head       ecoo_exp_chain;
 	struct ec_object      *ecoo_object;
 	struct obdo            ecoo_oa;
+};
+
+struct ec_lockhandle
+{
+	struct list_head       eclh_obj_chain;
+	struct lustre_handle   eclh_handle;
+};
+
+struct ec_lock
+{
+	struct list_head       ecl_exp_chain;
+	struct lustre_handle   ecl_handle;
+	struct ldlm_extent     ecl_extent;
+	__u32                  ecl_mode;
+	struct ec_object      *ecl_object;
+	__u64                  ecl_cookie;
 };
 
 #endif
