dbac4beff5d302d427439ddcdfd048a4ca7b0111
zab <zab>
2003-02-25 01:08:38 +0000

    - bring b_devel changes into b_io in preparation for file size fixes

--- lustre/include/linux/obd_filter.h
+++ lustre/include/linux/obd_filter.h
@@ -39,7 +39,7 @@
 struct filter_server_data {
         __u8  fsd_uuid[37];        /* server UUID */
         __u8  fsd_uuid_padding[3]; /* unused */
-        __u64 fsd_last_objid;      /* last completed transaction ID */
+        __u64 fsd_last_objid;      /* last created object ID */
         __u64 fsd_last_rcvd;       /* last completed transaction ID */
         __u64 fsd_mount_count;     /* FILTER incarnation number */
         __u8  fsd_padding[FILTER_LR_SERVER_SIZE - 64]; /*  */
@@ -54,6 +54,10 @@ struct filter_client_data {
         __u64 fcd_last_xid;        /* client RPC xid for the last transaction */
         __u8  fcd_padding[FILTER_LR_CLIENT_SIZE - 64]; 
 };
+
+#ifndef OBD_FILTER_SAN_DEVICENAME
+#define OBD_FILTER_SAN_DEVICENAME "sanobdfilter"
+#endif
 
 /* In-memory access to client data from OST struct */
 struct filter_export_data {
