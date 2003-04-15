504f897d289a14c5ace079cdf13dc53c5a6c5b5e
rread <rread>
2003-04-15 21:46:42 +0000

    - add verison to config, so lconf can make sure the xml/ldap was created   by a compatible lmc.
    - recreate clients if remounting

--- lustre/utils/Lustre/__init__.py
+++ lustre/utils/Lustre/__init__.py
@@ -3,3 +3,5 @@ __all__ = ["lustredb"]
 from lustredb import LustreDB, LustreDB_XML, LustreDB_LDAP
 from error import LconfError, OptionError
 from cmdline import Options
+
+CONFIG_VERSION="2003041401"
