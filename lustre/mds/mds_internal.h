4411a84952edb1c381e05fe223d70cc3438fddff
pschwan <pschwan>
2003-04-27 17:40:04 +0000

    b=1178 - Preparations for removing unused "addr" from lustre_handle - Some mild internal API consistency fixups, before I copied this API   to three other places that need handles

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -1,3 +1,3 @@
-struct mds_file_data *mds_new_mfd(void);
+struct mds_file_data *mds_mfd_new(void);
 void mds_mfd_put(struct mds_file_data *mfd);
-void mds_destroy_mfd(struct mds_file_data *mfd);
+void mds_mfd_destroy(struct mds_file_data *mfd);
