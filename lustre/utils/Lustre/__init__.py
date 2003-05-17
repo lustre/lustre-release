b5550b7542ad202a37e360fa739255429f9890e5
braam <braam>
2003-05-17 21:18:08 +0000

    - remainder of the b_devel into b_open merge.

--- lustre/utils/Lustre/__init__.py
+++ lustre/utils/Lustre/__init__.py
@@ -4,4 +4,4 @@ from lustredb import LustreDB, LustreDB_XML, LustreDB_LDAP
 from error import LconfError, OptionError
 from cmdline import Options
 
-CONFIG_VERSION="2003041401"
+CONFIG_VERSION="2003051401"
