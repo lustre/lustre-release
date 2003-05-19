445b0a1dab128787214afeba474c7626605cb359
pschwan <pschwan>
2003-05-19 06:29:35 +0000

    merge b_ad (cleaned up a bit) into b_devel; entirely 2.5 compat fixes

--- lustre/lib/client.c
+++ lustre/lib/client.c
@@ -153,13 +153,10 @@ static kdev_t path2dev(char *path)
 {
         struct dentry *dentry;
         struct nameidata nd;
-        kdev_t dev = 0;
-
-        if (!path_init(path, LOOKUP_FOLLOW, &nd))
-                return 0;
-
-        if (path_walk(path, &nd))
-                return 0;
+        kdev_t dev;
+        KDEVT_VAL(dev, 0);
+        if (path_lookup(path, LOOKUP_FOLLOW, &nd))
+                return dev;
 
         dentry = nd.dentry;
         if (dentry->d_inode && !is_bad_inode(dentry->d_inode) &&
