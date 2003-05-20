f8a2f58d7344b3a85f778bf17600f8df930f55c9
zab <zab>
2003-05-20 01:56:16 +0000

    - include the portals ping client/server in the 2.5 build - default to the socknal in the ping server

---
+++ lnet/tests/Makefile.mk
@@ -0,0 +1,9 @@
+# Copyright (C) 2001  Cluster File Systems, Inc.
+#
+# This code is issued under the GNU General Public License.
+# See the file COPYING in this distribution
+
+include $(obj)/../Kernelenv
+
+obj-y += ping_cli.o
+obj-y += ping_srv.o
