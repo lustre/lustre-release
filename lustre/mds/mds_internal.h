d64b39d243745b85d148309f4f08157ad494dfec
adilger <adilger>
2003-05-21 06:25:00 +0000

    Remove bogus local function prototypes that were causing me grief. Fix header mess as necessary to be able to compile. Most of MDS unlink cookie generation complete. Remove some local variable declarations that are only used in debug messages   that appeared when compiling with CDEBUG() as a no-op (reduce stack usage).

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -1,3 +1,17 @@
+static inline struct mds_obd *mds_req2mds(struct ptlrpc_request *req)
+{
+        return &req->rq_export->exp_obd->u.mds;
+}
+
+/* mds/handler.c */
 struct mds_file_data *mds_mfd_new(void);
 void mds_mfd_put(struct mds_file_data *mfd);
 void mds_mfd_destroy(struct mds_file_data *mfd);
+
+/* mds/mds_reint.c */
+void mds_last_rcvd_cb(struct obd_device *obd, __u64 last_rcvd,
+                      void *cb_data, int error);
+int mds_finish_transno(struct mds_obd *mds, struct inode *inode, void *handle,
+                       struct ptlrpc_request *req, int rc, __u32 op_data,
+                       fsfilt_cb_t cb_fn, void *cb_data);
+
