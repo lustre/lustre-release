29edcffaa88eb0fc54e9d7d202ee429a211e9b1c
zab <zab>
2003-05-22 07:51:48 +0000

    - N instances of lib objs will collide in the final link, lets try building   it explicitly.. - get obdecho/lib compiling, make headway into obdclass - I'm tired of adding Makefile.mk entries to configure.in - lproc's awesome code duplication was saddening the final link, just call me   cap'n static

--- lustre/Makefile.mk
+++ lustre/Makefile.mk
@@ -1,4 +1,10 @@
-include fs/lustre/portals/Kernelenv
+include $(src)/portals/Kernelenv
+
+# for scripts/version_tag.pl
+LINUX = @LINUX@
 
 obj-y += portals/
+obj-y += obdclass/
+obj-y += obdecho/
 obj-y += mds/
+obj-y += lib/
--- lustre/mds/Makefile.mk
+++ lustre/mds/Makefile.mk
@@ -6,14 +6,15 @@
 include $(src)/../portals/Kernelenv
 
 obj-y += mds.o
+mds-objs := mds_lov.o handler.o mds_reint.o mds_fs.o lproc_mds.o mds_open.o
 
-martians := mds_updates.c simple.c target.c
-
-$(addprefix $(obj)/, $(martians)): $(obj)/%: $(src)/../lib/%
-	@rm -f $@
-	ln -s ../lib/$* $@
-
-mds-objs := mds_lov.o handler.o mds_reint.o mds_fs.o lproc_mds.o mds_open.o \
-		$(patsubst %.c, %.o, $(martians))
-
-clean-files := $(martians)
+#martians := mds_updates.c simple.c target.c
+#
+#$(addprefix $(obj)/, $(martians)): $(obj)/%: $(src)/../lib/%
+#	@rm -f $@
+#	ln -s ../lib/$* $@
+#
+#mds-objs := mds_lov.o handler.o mds_reint.o mds_fs.o lproc_mds.o mds_open.o \
+#		$(patsubst %.c, %.o, $(martians))
+#
+#clean-files := $(martians)
