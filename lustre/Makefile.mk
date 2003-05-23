03b641dfea5708c3fef1eb769097ae67d4f1b8aa
zab <zab>
2003-05-23 21:34:00 +0000

    - add mdc, obdfilter, and lov to the mix - static out more duplicate lprocfs symbols, xprocfs too - move lustre code to the 2.5 d_hash hlist_ api and have compat map it back to   2.4's list_heads - ->bmap now enjoys sector_t

--- lustre/Makefile.mk
+++ lustre/Makefile.mk
@@ -9,10 +9,14 @@ obj-y += obdclass/
 obj-y += lib/
 obj-y += ptlrpc/
 obj-y += ldlm/
-#obj-y += mds/
+obj-y += obdfilter/
+obj-y += mdc/
+obj-y += mds/
 obj-y += obdecho/
 obj-y += osc/
 obj-y += ost/
+obj-y += lov/
+#obj-y += llite/
 
 # portals needs to be before utils/, which pulls in ptlctl objects
 obj-m += utils/
