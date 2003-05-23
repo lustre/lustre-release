eb5d4096f8d42ff394917f933eed5155e48cb471
zab <zab>
2003-05-23 00:22:26 +0000

    -  this seems to pass runregression-net.sh.. - bring the osc and ost into the mix for the benefit of echo.sh - get lprocfs building into the kernel by not defining the same 3 symbols in   every lprocfs object (sigh) - compat out path_lookup, we may need to EXPORT it for 2.5 later - s/uint flags/ulong flags/ in ptlrpc (eep)

--- lustre/osc/Makefile.mk
+++ lustre/osc/Makefile.mk
@@ -1,8 +1,9 @@
-# Copyright (C) 2001  Cluster File Systems, Inc.
+# Copyright (C) 2003  Cluster File Systems, Inc.
 #
 # This code is issued under the GNU General Public License.
 # See the file COPYING in this distribution
 
 include $(src)/../portals/Kernelenv
 
-obj-y += mds_updates.o obd_pack.o simple.o client.o target.o
+obj-y += osc.o
+osc-objs := osc_request.o lproc_osc.o
