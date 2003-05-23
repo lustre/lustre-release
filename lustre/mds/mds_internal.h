e85f5dcf129822ac0016ffba952dd3206a87f698
adilger <adilger>
2003-05-23 07:03:13 +0000

    Merge b_devel -> b_orphan.   DEVEL_ORPHAN_UPDATE_PARENT_20030522 -> DEVEL_ORPHAN_UPDATE_PARENT_20030522

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -9,8 +9,8 @@ void mds_mfd_put(struct mds_file_data *mfd);
 void mds_mfd_destroy(struct mds_file_data *mfd);
 
 /* mds/mds_reint.c */
-void mds_last_rcvd_cb(struct obd_device *obd, __u64 last_rcvd,
-                      void *cb_data, int error);
+void mds_commit_cb(struct obd_device *obd, __u64 last_rcvd,
+		   void *cb_data, int error);
 int mds_finish_transno(struct mds_obd *mds, struct inode *inode, void *handle,
                        struct ptlrpc_request *req, int rc, __u32 op_data,
                        fsfilt_cb_t cb_fn, void *cb_data);
