17e625eb0dfcf01d65d1ce55187b37b9605d2a39
adilger <adilger>
2003-05-25 18:17:54 +0000

    Mostly working full cookie create-send-cancel cycle for MDS unlinks. MDS now establishes a connection (LOV only, getting UUID from LOVDESC pending   bug 1280 resolution). We cannot clean up as a result of this (MDS->LOV connection).  Might be   possible to fix by changing cleanup order so we do llite/mdc/mds cleanup   before lov/osc/ost cleanup.

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -3,6 +3,14 @@ static inline struct mds_obd *mds_req2mds(struct ptlrpc_request *req)
         return &req->rq_export->exp_obd->u.mds;
 }
 
+/* mds/mds_fs.c */
+struct llog_handle *mds_log_create(struct obd_device *obd);
+int mds_log_close(struct llog_handle *cathandle, struct llog_handle *loghandle);
+struct llog_handle *mds_log_open(struct obd_device *obd,
+                                 struct llog_cookie *logcookie);
+struct llog_handle *mds_get_catalog(struct obd_device *obd);
+void mds_put_catalog(struct llog_handle *cathandle);
+
 /* mds/handler.c */
 struct mds_file_data *mds_mfd_new(void);
 void mds_mfd_put(struct mds_file_data *mfd);
@@ -14,4 +22,5 @@ void mds_commit_cb(struct obd_device *obd, __u64 last_rcvd,
 int mds_finish_transno(struct mds_obd *mds, struct inode *inode, void *handle,
                        struct ptlrpc_request *req, int rc, __u32 op_data,
                        fsfilt_cb_t cb_fn, void *cb_data);
+
 
