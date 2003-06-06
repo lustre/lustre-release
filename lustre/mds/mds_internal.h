6e40b85e06f8286a5018ec441f65cbf8a7fc06b9
adilger <adilger>
2003-06-06 21:34:09 +0000

    Fix lock ordering deadlock on the MDS when clients are connecting at the same time that regular MDS operations are ongoing.

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -4,6 +4,11 @@ void mds_mfd_destroy(struct mds_file_data *mfd);
 int mds_update_unpack(struct ptlrpc_request *, int offset,
                       struct mds_update_record *);
 
+/* mds/mds_fs.c */
+int mds_client_add(struct obd_device *obd, struct mds_export_data *med,
+                   int cl_off);
+int mds_client_free(struct obd_export *exp);
+
 #ifdef __KERNEL__
 void mds_pack_inode2fid(struct ll_fid *fid, struct inode *inode);
 void mds_pack_inode2body(struct mds_body *body, struct inode *inode);
