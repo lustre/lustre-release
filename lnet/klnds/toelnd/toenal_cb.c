2719ec3ccf15950a9740a9fb6affce8c1b13c73c
zab <zab>
2003-06-27 21:06:52 +0000

    - include version.h before using KERNEL_VERSION - have ll_ts2u64 in 2.4 also take a pointer to the time in question - ifdef out some 2.5 cache paths so 2.4 isn't sad - fix up remenants of llwp's move to a properly allocated structure - add some missing break;s to the end of default: blocks

--- lnet/klnds/toelnd/toenal_cb.c
+++ lnet/klnds/toelnd/toenal_cb.c
@@ -894,6 +894,7 @@ ktoenal_process_receive (ksock_conn_t *conn, long *irq_flags)
                                 spin_lock_irqsave (&ktoenal_data.ksnd_sched_lock, *irq_flags);
                                 goto get_fmb;   /* => go get a fwd msg buffer */
                         default:
+                                break;
                         }
                         /* Not Reached */
                         LBUG ();
@@ -935,6 +936,7 @@ ktoenal_process_receive (ksock_conn_t *conn, long *irq_flags)
                 goto out;                       /* (later) */
 
         default:
+                break;
         }
 
         /* Not Reached */
