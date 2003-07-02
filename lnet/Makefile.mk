8a0394fab12e0b6598d01be5b01fe08ae82fc543
cvs2svn <admin@example.com>
2003-07-02 17:25:44 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.4.5.4'.

--- lnet/Makefile.mk
+++
@@ -1,6 +0,0 @@
-include fs/lustre/portals/Kernelenv
-
-obj-y += portals/
-obj-y += libcfs/
-obj-y += knals/
-obj-y += router/
--- lustre/llite/llite_internal.h
+++
@@ -1,2 +0,0 @@
-int ll_mdc_cancel_unused(struct lustre_handle *conn, struct inode *inode,
-                         int flags, void *opaque);
