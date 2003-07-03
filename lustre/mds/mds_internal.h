8d55483b7c26838e5ded2d0725f1ebb1fa7e1126
pschwan <pschwan>
2003-07-03 19:55:01 +0000

    Merge b_devel into b_orphan (20030703 update tag)

--- lustre/mds/mds_internal.h
+++ lustre/mds/mds_internal.h
@@ -22,9 +22,7 @@ void mds_mfd_destroy(struct mds_file_data *mfd);
 void mds_commit_cb(struct obd_device *obd, __u64 last_rcvd,
 		   void *cb_data, int error);
 int mds_finish_transno(struct mds_obd *mds, struct inode *inode, void *handle,
-                       struct ptlrpc_request *req, int rc, __u32 op_data,
-                       fsfilt_cb_t cb_fn, void *cb_data);
-
+                       struct ptlrpc_request *req, int rc, __u32 op_data);
 
 /* mds/mds_lib.c */
 int mds_update_unpack(struct ptlrpc_request *, int offset,
