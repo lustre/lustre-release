f380b06aff8b139beb5125b265b76b5ad717bf43
adilger <adilger>
2003-07-07 21:32:46 +0000

    Move some function declarations into llite_internal.h.

--- lustre/llite/llite_internal.h
+++ lustre/llite/llite_internal.h
@@ -12,9 +12,23 @@
 
 struct lustre_handle;
 struct lov_stripe_md;
+struct ll_sb_info;
 
+/* llite/commit_callback.c */
+int ll_commitcbd_setup(struct ll_sb_info *);
+int ll_commitcbd_cleanup(struct ll_sb_info *);
+
+/* lproc_llite.c */
+int lprocfs_register_mountpoint(struct proc_dir_entry *parent,
+                                struct super_block *sb, char *osc, char *mdc);
+void lprocfs_unregister_mountpoint(struct ll_sb_info *sbi);
+
+/* llite/namei.c */
+int ll_it_open_error(int phase, struct lookup_intent *it);
 int ll_mdc_cancel_unused(struct lustre_handle *conn, struct inode *inode,
                          int flags, void *opaque);
+
+/* llite/rw.c */
 int ll_rd_dirty_pages(char *page, char **start, off_t off, int count,
                       int *eof, void *data);
 int ll_rd_max_dirty_pages(char *page, char **start, off_t off, int count,
@@ -25,5 +39,9 @@ int ll_clear_dirty_pages(struct lustre_handle *conn, struct lov_stripe_md *lsm,
                          unsigned long start, unsigned long end);
 int ll_mark_dirty_page(struct lustre_handle *conn, struct lov_stripe_md *lsm,
                        unsigned long index);
+
+/* llite/super.c */
+int ll_inode_setattr(struct inode *inode, struct iattr *attr, int do_trunc);
+int ll_setattr(struct dentry *de, struct iattr *attr);
 
 #endif /* LLITE_INTERNAL_H */
