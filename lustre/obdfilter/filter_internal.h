bf7e023a386b312c9afb29ec17461c53ee47d00f
shaver <shaver>
2003-07-11 10:20:57 +0000

    b_devel -> b_unify merge.  Doesn't build with vanilla-2.4.20 at least, but has the Lustre bits (especially mtime) merged correctly as far as we can tell.
    Some of the dentry_operations bits don't line up well, though the 2.4.20 patch
    is very similar to the 2.4.18-chaos65 one in the relevant areas (dcache.h).
    Might just be some more bits to bring over from b_ad?

--- lustre/obdfilter/filter_internal.h
+++ lustre/obdfilter/filter_internal.h
@@ -1,6 +1,14 @@
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ *
+ * Copyright (C) 2003 Cluster File Systems, Inc.
+ *
+ * This code is issued under the GNU General Public License.
+ * See the file COPYING in this distribution
+ */
+
 #ifndef _FILTER_INTERNAL_H
 #define _FILTER_INTERNAL_H
-
 
 #ifdef __KERNEL__
 # include <linux/spinlock.h>
@@ -16,10 +24,17 @@
 # define OBD_FILTER_SAN_DEVICENAME "sanobdfilter"
 #endif
 
+#define LAST_RCVD "last_rcvd"
+#define INIT_OBJID 2
+
 #define FILTER_LR_SERVER_SIZE    512
 
 #define FILTER_LR_CLIENT_START   8192
 #define FILTER_LR_CLIENT_SIZE    128
+
+/* This limit is arbitrary, but for now we fit it in 1 page (32k clients) */
+#define FILTER_LR_MAX_CLIENTS (PAGE_SIZE * 8)
+#define FILTER_LR_MAX_CLIENT_WORDS (FILTER_LR_MAX_CLIENTS/sizeof(unsigned long))
 
 #define FILTER_SUBDIR_COUNT      32            /* set to zero for no subdirs */
 
@@ -76,6 +91,11 @@ struct filter_dentry_data {
 #define FILTER_DENTRY_MAGIC 0x9efba101
 #define FILTER_FLAG_DESTROY 0x0001      /* destroy dentry on last file close */
 
+/* Limit the returned fields marked valid to those that we actually might set */
+#define FILTER_VALID_FLAGS (OBD_MD_FLTYPE | OBD_MD_FLMODE | OBD_MD_FLGENER  |\
+                            OBD_MD_FLSIZE | OBD_MD_FLBLOCKS | OBD_MD_FLBLKSZ|\
+                            OBD_MD_FLATIME | OBD_MD_FLMTIME | OBD_MD_FLCTIME)
+
 enum {
         LPROC_FILTER_READ_BYTES = 0,
         LPROC_FILTER_WRITE_BYTES = 1,
@@ -90,6 +110,10 @@ struct dentry *filter_parent_lock(struct obd_device *, obd_mode mode,
 void f_dput(struct dentry *);
 struct dentry *filter_fid2dentry(struct obd_device *, struct dentry *dir,
                                  obd_mode mode, obd_id id);
+struct dentry *__filter_oa2dentry(struct obd_device *obd,struct obdo *oa,
+                                  char *what);
+#define filter_oa2dentry(obd, oa) __filter_oa2dentry(obd, oa, __FUNCTION__)
+
 int filter_finish_transno(struct obd_export *, struct obd_trans_info *, int rc);
 __u64 filter_next_id(struct filter_obd *);
 int filter_update_server_data(struct file *, struct filter_server_data *);
@@ -100,11 +124,12 @@ int filter_common_setup(struct obd_device *, obd_count len, void *buf,
 int filter_preprw(int cmd, struct obd_export *, struct obdo *, int objcount,
                   struct obd_ioobj *, int niocount, struct niobuf_remote *,
                   struct niobuf_local *, struct obd_trans_info *);
-int filter_commitrw(int cmd, struct obd_export *, int objcount,
+int filter_commitrw(int cmd, struct obd_export *, struct obdo *, int objcount,
                     struct obd_ioobj *, int niocount, struct niobuf_local *,
                     struct obd_trans_info *);
-int filter_brw(int cmd, struct lustre_handle *, struct lov_stripe_md *,
-               obd_count oa_bufs, struct brw_page *, struct obd_trans_info *);
+int filter_brw(int cmd, struct lustre_handle *, struct obdo *,
+	       struct lov_stripe_md *, obd_count oa_bufs, struct brw_page *,
+	       struct obd_trans_info *);
 
 /* filter_log.c */
 int filter_log_cancel(struct lustre_handle *, struct lov_stripe_md *,
@@ -116,7 +141,7 @@ int filter_log_op_orphan(struct llog_handle *cathandle, obd_id oid,
 
 /* filter_san.c */
 int filter_san_setup(struct obd_device *obd, obd_count len, void *buf);
-int filter_san_preprw(int cmd, struct lustre_handle *, int objcount,
+int filter_san_preprw(int cmd, struct obd_export *, struct obdo *, int objcount,
                       struct obd_ioobj *, int niocount, struct niobuf_remote *);
 
 #endif
