5691e8bbf8f53d7314989bc21a824a23c2b344d7
braam <braam>
2003-07-14 23:42:19 +0000

    - merge b_unify into b_ad.  This makes b_ad almost ready to be landed   on b_unify and we may just do that next.

--- lustre/include/linux/lustre_mgmt.h
+++ lustre/include/linux/lustre_mgmt.h
@@ -10,7 +10,7 @@
 #ifndef LUSTRE_MGMT_H
 #define LUSTRE_MGMT_H
 
-#define LUSTRE_MGMTCLI_NAME "mgmtcli"
+#define LUSTRE_MGMTCLI_NAME "mgmt_cli"
 
 /* For the convenience and type-safety of inter_module_getters. */
 
