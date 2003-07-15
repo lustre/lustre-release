f3b7b1ca0bef503330fac87f9e9f6b19e7b55d53
braam <braam>
2003-07-15 03:45:05 +0000

    - a collection of minor changes to bring b_ad down onto b_unify - the 2.5 kernel patches from b_ad will follow shortly.

--- lnet/include/linux/portals_compat25.h
+++ lnet/include/linux/portals_compat25.h
@@ -1,15 +1,33 @@
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)) || defined(CONFIG_RH_2_4_20)
-# define SIGNAL_MASK_LOCK(task, flags)					\
-  spin_lock_irqsave(&task->sighand->siglock, flags)
-# define SIGNAL_MASK_UNLOCK(task, flags)				\
-  spin_unlock_irqrestore(&task->sighand->siglock, flags)
-# define RECALC_SIGPENDING         recalc_sigpending()
-#else
-# define SIGNAL_MASK_LOCK(task, flags)					\
-  spin_lock_irqsave(&task->sigmask_lock, flags)
-# define SIGNAL_MASK_UNLOCK(task, flags)				\
-  spin_unlock_irqrestore(&task->sigmask_lock, flags)
-# define RECALC_SIGPENDING         recalc_sigpending(current)
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ */
+#ifndef _KERNEL_COMPAT_H
+#define _KERNEL_COMPAT_H
+
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)
+
+#define SIGNAL_MASK_LOCK(task, flags)         \
+        spin_lock_irqsave( &task->sighand->siglock, flags)
+#define SIGNAL_MASK_UNLOCK(task, flags)       \
+        spin_unlock_irqrestore(&task->sighand->siglock, flags)
+#define USERMODEHELPER(path, argv, envp)       \
+        call_usermodehelper(path, argv, envp, 1)
+#define RECALC_SIGPENDING       recalc_sigpending()
+#define CURRENT_SECONDS         get_seconds()
+
+#else 
+        /* 2.4.. */
+
+#define SIGNAL_MASK_LOCK(task, flags)         \
+        spin_lock_irqsave(&task->sigmask_lock, flags)
+#define SIGNAL_MASK_UNLOCK(task, flags)       \
+        spin_unlock_irqrestore(&task->sigmask_lock, flags)
+#define USERMODEHELPER(path, argv, envp)       \
+        call_usermodehelper(path, argv, envp)
+#define RECALC_SIGPENDING         recalc_sigpending(current)
+#define CURRENT_SECONDS         CURRENT_TIME
+
 #endif
 
 #if defined(__arch_um__) && (LINUX_VERSION_CODE < KERNEL_VERSION(2,4,20))
@@ -22,3 +40,5 @@
 # define THREAD_NAME(comm, fmt, a...)                                   \
 	sprintf(comm, fmt, ## a)
 #endif
+
+#endif /* _KERNEL_COMPAT_H */
