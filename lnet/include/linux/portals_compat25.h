a94dbb74fd78b681c23f1cc1fee6fe365d6a2c98
phil <phil>
2003-07-22 14:00:03 +0000

    Fix small merge damage in archdep.m4, remove tabs in portals_compat25.h

--- lnet/include/linux/portals_compat25.h
+++ lnet/include/linux/portals_compat25.h
@@ -20,37 +20,37 @@
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)) || defined(CONFIG_RH_2_4_20)
 
-# define SIGNAL_MASK_LOCK(task, flags)					\
+# define SIGNAL_MASK_LOCK(task, flags)                                  \
   spin_lock_irqsave(&task->sighand->siglock, flags)
-# define SIGNAL_MASK_UNLOCK(task, flags)				\
+# define SIGNAL_MASK_UNLOCK(task, flags)                                \
   spin_unlock_irqrestore(&task->sighand->siglock, flags)
-#define USERMODEHELPER(path, argv, envp)                                \
-        call_usermodehelper(path, argv, envp, 1)
+# define USERMODEHELPER(path, argv, envp)                               \
+  call_usermodehelper(path, argv, envp, 1)
 # define RECALC_SIGPENDING         recalc_sigpending()
 # define CURRENT_SECONDS           get_seconds()
 
 #else /* 2.4.x */
 
-# define SIGNAL_MASK_LOCK(task, flags)					\
+# define SIGNAL_MASK_LOCK(task, flags)                                  \
   spin_lock_irqsave(&task->sigmask_lock, flags)
-# define SIGNAL_MASK_UNLOCK(task, flags)				\
+# define SIGNAL_MASK_UNLOCK(task, flags)                                \
   spin_unlock_irqrestore(&task->sigmask_lock, flags)
-#define USERMODEHELPER(path, argv, envp)                                \
-        call_usermodehelper(path, argv, envp)
+# define USERMODEHELPER(path, argv, envp)                               \
+  call_usermodehelper(path, argv, envp)
 # define RECALC_SIGPENDING         recalc_sigpending(current)
 # define CURRENT_SECONDS           CURRENT_TIME
 
 #endif
 
 #if defined(__arch_um__) && (LINUX_VERSION_CODE < KERNEL_VERSION(2,4,20))
-# define THREAD_NAME(comm, fmt, a...)					\
-	sprintf(comm, fmt "|%d", ## a, current->thread.extern_pid)
+# define THREAD_NAME(comm, fmt, a...)                                   \
+        sprintf(comm, fmt "|%d", ## a, current->thread.extern_pid)
 #elif defined(__arch_um__) && (LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0))
-# define THREAD_NAME(comm, fmt, a...)					\
+# define THREAD_NAME(comm, fmt, a...)                                   \
         sprintf(comm, fmt "|%d", ## a, current->thread.mode.tt.extern_pid)
 #else
 # define THREAD_NAME(comm, fmt, a...)                                   \
-	sprintf(comm, fmt, ## a)
+        sprintf(comm, fmt, ## a)
 #endif
 
 #endif /* _PORTALS_COMPAT_H */
