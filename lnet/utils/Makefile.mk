3ce2f7b700d20b1f3ba560fefa3e8a6519e12891
cvs2svn <admin@example.com>
2003-07-30 04:13:57 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.2.5.14'.

--- lnet/utils/Makefile.mk
+++
@@ -1,6 +0,0 @@
-include $(src)/../Kernelenv
-
-host-progs := acceptor ptlctl
-always := $(host-progs)
-
-ptlctl-objs := ptlctl.o $(PTLCTLOBJS)
--- lustre/lib/Makefile.mk
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2001  Cluster File Systems, Inc.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-
-include $(src)/../portals/Kernelenv
-
-obj-y += mds_updates.o obd_pack.o simple.o client.o target.o
