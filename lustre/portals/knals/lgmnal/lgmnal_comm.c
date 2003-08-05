c39da0c8b593d91ccf458a520ec2d0fd704a4bd9
phil <phil>
2003-08-05 15:58:40 +0000

    invoking section 3 of the GNU LGPL, to instead apply the terms of the GPL

--- lustre/portals/knals/lgmnal/lgmnal_comm.c
+++ lustre/portals/knals/lgmnal/lgmnal_comm.c
@@ -1,23 +1,23 @@
 /* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
-  * vim:expandtab:shiftwidth=8:tabstop=8:
-  *
-  *  Copyright (c) 2003 Los Alamos National Laboratory (LANL)
-  *
-  *   This file is part of Lustre, http://www.lustre.org/
-  *
-  *   This file is free software; you can redistribute it and/or
-  *   modify it under the terms of version 2.1 of the GNU Lesser General
-  *   Public License as published by the Free Software Foundation.
-  *
-  *   Lustre is distributed in the hope that it will be useful,
-  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *   GNU Lesser General Public License for more details.
-  *
-  *   You should have received a copy of the GNU Lesser General Public
-  *   License along with Portals; if not, write to the Free Software
-  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-  */
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ *
+ *  Copyright (c) 2003 Los Alamos National Laboratory (LANL)
+ *
+ *   This file is part of Lustre, http://www.lustre.org/
+ *
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 
 /*
