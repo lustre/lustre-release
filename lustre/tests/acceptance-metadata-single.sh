5befbf374d8d3eea8b3b85d16bf2cb9688bc8ada
phil <phil>
2003-08-14 05:55:21 +0000

    merge b_filterio into b_llpio; b_filterio soon to be deleted, given that b_llpio is a superset

--- lustre/tests/acceptance-metadata-single.sh
+++ lustre/tests/acceptance-metadata-single.sh
@@ -10,6 +10,20 @@ SRCDIR="`dirname $0`"
 CREATE=$SRCDIR/create.pl
 RENAME=$SRCDIR/rename.pl
 
+TIME=${TIME:-/usr/bin/time}
+
+display_elapsed_time() {
+    PREVIOUS_TS=$CURRENT_TS
+    CURRENT_TS=`date +%s`
+    BLOCK_ELAPSED=`expr $CURRENT_TS - $PREVIOUS_TS`
+    TOTAL_ELAPSED=`expr $CURRENT_TS - $START_TS`
+
+    echo " "
+    echo "Elapsed time (block): ${BLOCK_ELAPSED} seconds"
+    echo "Elapsed time (TOTAL): ${TOTAL_ELAPSED} seconds"
+    echo " "
+}    
+
 debug_client_on()
 {
 	echo -1 > /proc/sys/portals/debug
@@ -22,80 +36,118 @@ debug_client_off()
 
 MNT=${MNT:-/mnt/lustre}
 
+# Get our initial timestamps.
+START_TS=`date +%s`
+CURRENT_TS=$START_TS
+PREVIOUS_TS=$START_TS
+
 debug_client_on
 echo "create.pl, 1 mount, 1 thread, 10 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=10
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=10
 echo "create.pl, 1 mount, 1 thread, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --silent
 echo "create.pl --mcreate=0, 1 mount, 1 thread, 10 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=10 --use_mcreate=0
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=10 --use_mcreate=0
 echo "create.pl --mcreate=0, 1 mount, 1 thread, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 1 thread, 10 ops, debug on"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=10
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=10
 echo "rename.pl, 1 mount, 1 thread, 100 ops, debug on"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=100 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=100 --silent
+
+display_elapsed_time
 
 debug_client_off
 echo "create.pl, 1 mount, 1 thread, 1000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --silent
 echo "create.pl --mcreate=0, 1 mount, 1 thread, 1000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 1 thread, 1000 ops, debug off"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --silent
+
+display_elapsed_time
 
 debug_client_on
 echo "create.pl, 1 mount, 2 threads, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=2 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=2 --silent
 echo "create.pl --mcreate=0, 1 mount, 2 threads, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=2 --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=2 --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 2 thread, 1000 ops, debug on"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --num_threads=2 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=1000 --num_threads=2 --silent
+
+display_elapsed_time
 
 debug_client_off
 echo "create.pl, 1 mount, 2 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --silent
 echo "create.pl --mcreate=0, 1 mount, 2 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --use_mcreate=0  --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --use_mcreate=0  --silent
 wait
 echo "rename.pl, 1 mount, 2 threads, 2000 ops, debug off"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=2 --silent
+
+display_elapsed_time
 
 debug_client_on
 echo "create.pl, 1 mount, 4 threads, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=4 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=4 --silent
 echo "create.pl --mcreate=0, 1 mount, 4 threads, 100 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=4 --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=100 --num_threads=4 --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 4 threads, 2000 ops, debug on"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4 --silent
+
+display_elapsed_time
 
 debug_client_off
 echo "create.pl, 1 mount, 4 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4  --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4  --silent
 echo "create.pl --mcreate=0, 1 mount, 4 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4  --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4  --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 4 threads, 2000 ops, debug off"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=4 --silent
+
+display_elapsed_time
 
 debug_client_on
 echo "create.pl, 1 mount, 8 threads, 500 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=500 --num_threads=8  --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=500 --num_threads=8  --silent
 echo "create.pl --mcreate=0, 1 mount, 8 threads, 500 ops, debug on"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=500 --num_threads=8  --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=500 --num_threads=8  --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 8 threads, 2000 ops, debug on"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8 --silent
+
+display_elapsed_time
 
 debug_client_off
 echo "create.pl, 1 mount, 8 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8  --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8  --silent
 echo "create.pl --mcreate=0, 1 mount, 8 threads, 2000 ops, debug off"
-perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8  --use_mcreate=0 --silent
+$TIME perl $CREATE --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8  --use_mcreate=0 --silent
 echo "rename.pl, 1 mount, 8 threads, 2000 ops, debug off"
-perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8 --silent
+$TIME perl $RENAME --mountpt=${MNT} --num_mounts=-1 --iterations=2000 --num_threads=8 --silent
 
-sh rundbench 1
-sh rundbench 2
-sh rundbench 4
-sh rundbench 8
-sh rundbench 16
-sh rundbench 32
+display_elapsed_time
+
+$TIME sh rundbench 1
+
+display_elapsed_time
+
+$TIME sh rundbench 2
+
+display_elapsed_time
+
+$TIME sh rundbench 4
+
+display_elapsed_time
+
+$TIME sh rundbench 8
+
+display_elapsed_time
+
+$TIME sh rundbench 16
+
+display_elapsed_time
+
+$TIME sh rundbench 32
+
+display_elapsed_time
