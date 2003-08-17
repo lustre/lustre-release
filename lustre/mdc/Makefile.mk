a82f4373a28259bc52412a2bbf6bf323de89be35
cvs2svn <admin@example.com>
2003-08-17 06:38:30 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.10.2.2'.

--- lustre/mdc/Makefile.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2003  Cluster File Systems, Inc.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-
-include $(src)/../portals/Kernelenv
-
-obj-y += mdc.o
-mdc-objs := mdc_request.o mdc_reint.o lproc_mdc.o mdc_lib.o
