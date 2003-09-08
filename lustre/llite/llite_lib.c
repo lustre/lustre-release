33f931dac9d2999ed2812685ccddc4a4e098f9e6
adilger <adilger>
2003-09-08 20:14:23 +0000

    Don't fail assertion if lock has waiters. Was apparently fixed in the client and development branches, but not HEAD. b=1815

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -671,7 +671,7 @@ int ll_setattr_raw(struct inode *inode, struct iattr *attr)
                  * pages.  This last one is especially bad for racing
                  * o_append users on other nodes. */
                 /* bug 1639: avoid write/truncate i_sem/DLM deadlock */
-                LASSERT(atomic_read(&inode->i_sem.count) == 0);
+                LASSERT(atomic_read(&inode->i_sem.count) <= 0);
                 up(&inode->i_sem);
                 rc = ll_extent_lock_no_validate(NULL, inode, lsm, LCK_PW,
                                                  &extent, &lockh);
