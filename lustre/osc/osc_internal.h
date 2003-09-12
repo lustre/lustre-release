bb50d36b7c81ba2437805432419d717b04b08bb7
phil <phil>
2003-09-12 19:13:47 +0000

    b=1807 - backport oscc_create rewrite from b_llp_hp - send osc_creates to a new OST thread, so that time-sensitive   creations will not have to compete with billions of read/write requests

--- lustre/osc/osc_internal.h
+++ lustre/osc/osc_internal.h
@@ -2,8 +2,6 @@ int osc_create(struct obd_export *exp, struct obdo *oa,
 	       struct lov_stripe_md **ea, struct obd_trans_info *oti);
 int osc_real_create(struct obd_export *exp, struct obdo *oa,
 	       struct lov_stripe_md **ea, struct obd_trans_info *oti);
-int osccd_setup(void);
-int osccd_cleanup(void);
 void oscc_init(struct lustre_handle *exph);
 
 int lproc_osc_attach_seqstat(struct obd_device *dev);
