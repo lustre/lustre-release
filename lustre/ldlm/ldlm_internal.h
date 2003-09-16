2a2e0bd48f18287b768a9bb448c0fe7ff820ac51
dmilos <dmilos>
2003-09-16 22:41:12 +0000

    Merge latest b_llp_hp changes onto b_flock

--- lustre/ldlm/ldlm_internal.h
+++ lustre/ldlm/ldlm_internal.h
@@ -1,1 +1,10 @@
+/* ldlm_request.c */
 int ldlm_cancel_lru(struct ldlm_namespace *ns);
+
+/* ldlm_lock.c */
+void ldlm_grant_lock(struct ldlm_lock *lock, void *data, int datalen,
+		     int run_ast);
+struct ldlm_lock *
+ldlm_lock_create(struct ldlm_namespace *ns,
+                 struct lustre_handle *parent_lock_handle, struct ldlm_res_id,
+                 __u32 type, ldlm_mode_t, ldlm_blocking_callback, void *data);
