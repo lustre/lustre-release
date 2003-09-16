595a4a5ffa48b101e9174ec98733e85ae9d7e6c3
phil <phil>
2003-09-16 18:23:24 +0000

    Followup cleanup commit to the last one: move the setting of the completion ast to ldlm_lock_create, where the blocking AST is set.

--- lustre/ldlm/ldlm_internal.h
+++ lustre/ldlm/ldlm_internal.h
@@ -7,4 +7,7 @@ void ldlm_grant_lock(struct ldlm_lock *lock, void *data, int datalen,
 struct ldlm_lock *
 ldlm_lock_create(struct ldlm_namespace *ns,
                  struct lustre_handle *parent_lock_handle, struct ldlm_res_id,
-                 __u32 type, ldlm_mode_t, ldlm_blocking_callback, void *data);
+                 __u32 type, ldlm_mode_t, ldlm_blocking_callback,
+                 ldlm_completion_callback, void *data);
+ldlm_error_t ldlm_lock_enqueue(struct ldlm_namespace *, struct ldlm_lock **,
+                               void *cookie, int cookie_len, int *flags);
