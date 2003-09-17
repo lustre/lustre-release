c0323b31c71069c3958b939ce5ade8ea166263d4
mfrey <mfrey>
2003-09-17 11:25:25 +0000

    update from b_llp_hp

--- lustre/mdc/mdc_lib.c
+++ lustre/mdc/mdc_lib.c
@@ -30,7 +30,7 @@
 #include "mdc_internal.h"
 
 void mdc_readdir_pack(struct ptlrpc_request *req, __u64 offset, __u32 size,
-                      obd_id ino, int type)
+                      struct ll_fid *mdc_fid)
 {
         struct mds_body *b;
 
@@ -38,8 +38,7 @@ void mdc_readdir_pack(struct ptlrpc_request *req, __u64 offset, __u32 size,
         b->fsuid = current->fsuid;
         b->fsgid = current->fsgid;
         b->capability = current->cap_effective;
-        b->fid1.id = ino;
-        b->fid1.f_type = type;
+        b->fid1 = *mdc_fid;
         b->size = offset;                       /* !! */
         b->suppgid = -1;
         b->nlink = size;                        /* !! */
@@ -94,7 +93,7 @@ void mdc_create_pack(struct ptlrpc_request *req, int offset,
 void mdc_open_pack(struct ptlrpc_request *req, int offset,
                    struct mdc_op_data *op_data,
                    __u32 mode, __u64 rdev, __u64 time,
-                   __u32 flags, const void *data, int datalen)
+                   __u32 flags, const void *lmm, int lmmlen)
 {
         struct mds_rec_create *rec;
         char *tmp;
@@ -120,9 +119,10 @@ void mdc_open_pack(struct ptlrpc_request *req, int offset,
                 LOGL0(op_data->name, op_data->namelen, tmp);
         }
 
-        if (data) {
-                tmp = lustre_msg_buf(req->rq_reqmsg, offset + 2, datalen);
-                memcpy (tmp, data, datalen);
+        if (lmm) {
+                rec->cr_flags |= MDS_OPEN_HAS_EA;
+                tmp = lustre_msg_buf(req->rq_reqmsg, offset + 2, lmmlen);
+                memcpy (tmp, lmm, lmmlen);
         }
 }
 
