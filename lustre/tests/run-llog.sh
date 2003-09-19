7a28a4510dfc908b4fd354d28eb31927357d6713
rread <rread>
2003-09-19 21:28:47 +0000

    * use records that have a tail for the simple record tests. * change verify_handle to scan the bitmap to deterime the correct last_idx and active rec count. * test 4 now uses verify_handle, and fails the last_idx check after writing one rec.
    * run-llog.sh now tells lctl to ignore_errors, so it can cleanup
    after a failed test.

--- lustre/tests/run-llog.sh
+++ lustre/tests/run-llog.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 insmod ../obdclass/llog_test.o
 ../utils/lctl modules > /r/tmp/ogdb-localhost.localdomain
 echo "NOW reload debugging syms.."
 
+# Using ignore_errors will allow lctl to cleanup even if the test
+# fails.
 ../utils/lctl <<EOF
+ignore_errors
 newdev
 attach llog_test llt_name llt_uuid
 setup mds1
