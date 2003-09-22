c1c99e96eac359921e9e9a108c7d13cf428487dd
dmilos <dmilos>
2003-09-22 14:24:54 +0000

    Move policy specific processing function prototypes to ldlm_internal.h.

--- lustre/ldlm/ldlm_internal.h
+++ lustre/ldlm/ldlm_internal.h
@@ -11,3 +11,15 @@ ldlm_lock_create(struct ldlm_namespace *ns,
                  ldlm_completion_callback, void *data);
 ldlm_error_t ldlm_lock_enqueue(struct ldlm_namespace *, struct ldlm_lock **,
                                void *cookie, int cookie_len, int *flags);
+
+/* ldlm_plain.c */
+int ldlm_process_plain_lock(struct ldlm_lock *lock, int *flags, int first_enq,
+                            ldlm_error_t *err);
+
+/* ldlm_extent.c */
+int ldlm_process_extent_lock(struct ldlm_lock *lock, int *flags, int first_enq,
+                             ldlm_error_t *err);
+
+/* ldlm_flock.c */
+int ldlm_process_flock_lock(struct ldlm_lock *lock, int *flags, int first_enq,
+                            ldlm_error_t *err);
