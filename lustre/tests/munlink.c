4c2a14efd87e8c4238cf9950779636f096cbb91e
dmilos <dmilos>
2003-09-22 15:01:18 +0000

    Land latest b_llp_hp changes onto b_flock.

--- lustre/tests/munlink.c
+++ lustre/tests/munlink.c
@@ -8,16 +8,17 @@
 
 int main(int argc, char ** argv)
 {
-        int rc;
+        int rc, i;
 
         if (argc < 2) { 
                 printf("Usage %s filename\n", argv[0]);
                 return 1;
         }
 
-        rc = unlink(argv[1]);
-        if (rc) { 
-                printf("unlink(%s) error: %s\n", argv[1], strerror(errno));
+	for (i = 1; i < argc; i++) {
+        	rc = unlink(argv[i]);
+        	if (rc)
+                	printf("unlink(%s) error: %s\n", argv[i], strerror(errno));
         }
         return rc;
 } 
