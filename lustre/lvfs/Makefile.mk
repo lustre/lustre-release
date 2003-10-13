1b1d91aeecc8997b2ece2caa2dd697e170d9a306
wangdi <wangdi>
2003-10-13 10:28:59 +0000

    remove redundant fsfilt_ext3.o in Makefile.mk

--- lustre/lvfs/Makefile.mk
+++ lustre/lvfs/Makefile.mk
@@ -1,4 +1,4 @@
 include $(src)/../portals/Kernelenv
 
 obj-y += lvfs.o fsfilt_ext3.o 
-lvfs-objs := fsfilt.o  fsfilt_ext3.o lvfs_common.o  lvfs_linux.o 
+lvfs-objs := fsfilt.o  lvfs_common.o  lvfs_linux.o 
