6ef1bb6e43809b760b558a0fe2e4cd12e2834aa2
rread <rread>
2003-10-15 23:55:22 +0000

    b=2111 r=peter
    - most of this is Peter patch from 2111, including making sure
      obd_log_exp is set once.
    - my integration with 0conf
    - add an obd method "obd_pre_cleanup" so the log export can be
      disconnected, and allow the device to be fully cleaned up.

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -587,6 +587,7 @@ int lprocfs_alloc_obd_stats(struct obd_device *obd, unsigned num_private_stats)
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, config);
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, setup);
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, postsetup);
+        LPROCFS_OBD_OP_INIT(num_private_stats, stats, pre_cleanup);
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, cleanup);
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, postrecov);
         LPROCFS_OBD_OP_INIT(num_private_stats, stats, connect);
