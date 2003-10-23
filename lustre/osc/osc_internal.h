276a88117d46dc933cdc438d9f992e49c5da85ac
zab <zab>
2003-10-23 16:28:19 +0000

    b=2010 This time, actually initialize the OSC's maximum number of pages allowed in each RPC to the nice value that we calculate, rather than just setting the tunable max.  We export the math from portals so that client_obd_setup() can use it to initialize the OSC from its summer home in ptlrpc/.

--- lustre/osc/osc_internal.h
+++ lustre/osc/osc_internal.h
@@ -12,7 +12,7 @@
 
 /* bug 1578: negotiate BRW_MAX_SIZE with the OST, instead of hard-coding it */
 #define OSC_BRW_MAX_SIZE PTL_MTU
-#define OSC_BRW_MAX_IOV min_t(int, PTL_MD_MAX_IOV, OSC_BRW_MAX_SIZE/PAGE_SIZE)
+#define OSC_BRW_MAX_IOV PTL_MD_MAX_PAGES
 
 #define OAP_MAGIC 8675309
 
