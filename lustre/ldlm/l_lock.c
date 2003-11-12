9dfe893d2f00d77821e050a03abcb694d3f0c3e0
ericm <ericm>
2003-11-12 11:03:09 +0000

    merge b_devel to b_eq: 20031112 for robert's final zeroconf code.

--- lustre/ldlm/l_lock.c
+++ lustre/ldlm/l_lock.c
@@ -113,3 +113,18 @@ int l_has_lock(struct lustre_lock *lock)
                 CDEBUG(D_INFO, "lock_depth: %d\n", depth);
         return owner;
 }
+
+#include <linux/lustre_version.h>
+void l_check_no_ns_lock(struct ldlm_namespace *ns)
+{
+        static long next_msg;
+
+        if (l_has_lock(&ns->ns_lock) && time_after(jiffies, next_msg)) {
+                CERROR("namespace %s lock held during RPCs; tell phil\n",
+                       ns->ns_name);
+#if (LUSTRE_KERNEL_VERSION >= 30)
+                CERROR(portals_debug_dumpstack());
+#endif
+                next_msg = jiffies + 60 * HZ;
+        }
+}
