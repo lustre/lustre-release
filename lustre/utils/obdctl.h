ad22993a06ea81dbd9649d19199162263c6ce904
rread <rread>
2003-11-15 01:17:56 +0000

    b=2250
    create lctl commands set_lustre_upcall and set_timeout, so they
    can be saved as part of the 0conf log.

--- lustre/utils/obdctl.h
+++ lustre/utils/obdctl.h
@@ -95,6 +95,8 @@ int jt_lcfg_del_uuid(int argc, char **argv);
 int jt_lcfg_lov_setup(int argc, char **argv);
 int jt_lcfg_mount_option(int argc, char **argv);
 int jt_lcfg_del_mount_option(int argc, char **argv);
+int jt_lcfg_set_timeout(int argc, char **argv);
+int jt_lcfg_set_lustre_upcall(int argc, char **argv);
 
 int obd_add_uuid(char *uuid, ptl_nid_t nid, int nal);
 
