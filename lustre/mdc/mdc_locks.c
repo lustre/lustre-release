dbad65a2fabba78c5eaffc8a3cf64582334eea90
phil <phil>
2004-01-01 22:27:57 +0000

    - add an AST which returns a snapshot of the LVB (glimpse) - add llite and LOV code to respond to glimpse ASTs with the   stripe-adjusted size of the inode - add a policy function on the filter; this policy either grants a [0,-1] PR lock, if no conflicts, or performs a glimpse and returns that LVB - send LVB in completion AST - add lvb_swabber pointer to ldlm_lock and enqueue functions - remove never-used parent_lock_handle from enqueue functions - ldlm_intent_policy renamed to mds_intent_policy - filter_lvbo_update can now update LVB from the network message or   disk inode, as required

--- lustre/mdc/mdc_locks.c
+++ lustre/mdc/mdc_locks.c
@@ -284,9 +284,10 @@ int mdc_enqueue(struct obd_export *exp,
         }
 
         mdc_get_rpc_lock(obddev->u.cli.cl_rpc_lock, it);
-        rc = ldlm_cli_enqueue(exp, req, obddev->obd_namespace, NULL, res_id,
+        rc = ldlm_cli_enqueue(exp, req, obddev->obd_namespace, res_id,
                               lock_type, NULL, 0, lock_mode, &flags,
-                              cb_completion, cb_blocking, cb_data, 0, lockh);
+                              cb_completion, cb_blocking, NULL, cb_data, 0,
+                              NULL, lockh);
         mdc_put_rpc_lock(obddev->u.cli.cl_rpc_lock, it);
 
         /* Similarly, if we're going to replay this request, we don't want to
