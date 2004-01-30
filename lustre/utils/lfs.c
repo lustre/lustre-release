7a49011d2fe9d5dbada5ad6c1bed4f8ee2b1f617
shaocheng <shaocheng>
2004-01-30 22:26:42 +0000

    add functionality to set/get stripe info on directories.  - add "lfs dsetstripe" to set dir's stripe info  - using the existing "lfs find" to retrieve stripe info  - files created under a directory will use the directory's stripe info as default  - bugs: mkdir dir1; lfs dsetstripe dir1; cd dir1. can't enter it. the reason is that mdc_req2lustre_md will get the stripe info for dir1, which can not pass lov_verify_lmm due to different size of lov_mds_md returned.

--- lustre/utils/lfs.c
+++ lustre/utils/lfs.c
@@ -40,12 +40,15 @@
 
 extern int op_create_file(char *name, long stripe_size, int stripe_offset,
                 int stripe_count);
+extern int op_setstripe_dir(char *path, long stripe_size, int stripe_offset,
+                int stripe_count);
 extern int op_find(char *path, struct obd_uuid *obduuid, int recursive,
                 int verbose, int quiet);
 extern int op_check(int type_num, char **obd_type_p, char *dir);
 extern int op_catinfo(char *dir, char *keyword, char *node_name);
 
 /* all functions */
+static int lfs_dsetstripe(int argc, char **argv);
 static int lfs_setstripe(int argc, char **argv);
 static int lfs_find(int argc, char **argv);
 static int lfs_getstripe(int argc, char **argv);
@@ -58,6 +61,12 @@ command_t cmdlist[] = {
         {"setstripe", lfs_setstripe, 0,
          "To create a new file with a specific striping pattern.\n"
          "usage: setstripe <filename> <stripe size> <stripe start> <stripe count>\n"
+         "\tstripe size:  Number of bytes in each stripe (0 default)\n"
+         "\tstripe start: OST index of first stripe (-1 default)\n"
+         "\tstripe count: Number of OSTs to stripe over (0 default)"},
+        {"dsetstripe", lfs_dsetstripe, 0,
+         "To set a specific striping pattern for an existing directory.\n"
+         "usage: dsetstripe <dirname> <stripe size> <stripe start> <stripe count>\n"
          "\tstripe size:  Number of bytes in each stripe (0 default)\n"
          "\tstripe start: OST index of first stripe (-1 default)\n"
          "\tstripe count: Number of OSTs to stripe over (0 default)"},
@@ -123,6 +132,47 @@ static int lfs_setstripe(int argc, char **argv)
                                 argv[0]);
 
         return result;
+}
+
+static int lfs_dsetstripe(int argc, char **argv)
+{
+        int result;
+        long st_size;
+        int  st_offset, st_count;
+        char *end;
+
+        if (argc != 5)
+                return CMD_HELP;
+
+        // get the stripe size
+        st_size = strtoul(argv[2], &end, 0);
+        if (*end != '\0') {
+                fprintf(stderr, "error: %s: bad stripe size '%s'\n",
+                                argv[0], argv[2]);
+                return CMD_HELP;
+        }
+        // get the stripe offset
+        st_offset = strtoul(argv[3], &end, 0);
+        if (*end != '\0') {
+                fprintf(stderr, "error: %s: bad stripe offset '%s'\n",
+                                argv[0], argv[3]);
+                return CMD_HELP;
+        }
+        // get the stripe count
+        st_count = strtoul(argv[4], &end, 0);
+        if (*end != '\0') {
+                fprintf(stderr, "error: %s: bad stripe count '%s'\n",
+                                argv[0], argv[4]);
+                return CMD_HELP;
+        }
+
+        result = op_setstripe_dir(argv[1], st_size, st_offset, st_count);
+        if (result)
+                fprintf(stderr, "error: %s: create stripe file failed\n",
+                                argv[0]);
+
+        return result;
+
 }
 
 static int lfs_find(int argc, char **argv)
