ac67f0188d5d8bb84e8fd2044e6ce6e8efdf0341
adilger <adilger>
2004-02-09 22:04:21 +0000

    Update b_smallfix from HEAD (20040209_1433) (merge of b_recovery)

--- lustre/smfs/inode.c
+++ lustre/smfs/inode.c
@@ -1,7 +1,5 @@
 /*
- *  fs/snap/snap.c
- *
- *  A snap shot file system.
+ *  smfs/inode.c
  *
  */
 
@@ -31,6 +29,7 @@ void duplicate_inode(struct inode *cache_inode, struct inode *inode)
 					 * size */  
 	inode->i_blocks = cache_inode->i_blocks;
 	inode->i_version = cache_inode->i_version;
+	inode->i_state = cache_inode->i_state;
 }
 static void smfs_read_inode(struct inode *inode)
 {
@@ -74,14 +73,202 @@ static void smfs_clear_inode(struct inode *inode)
 	
 	cache_sb = S2CSB(inode->i_sb);
 	cache_inode = I2CI(inode);
-	clear_inode(cache_inode);
+
+	duplicate_inode(inode, cache_inode); 
+	
+	if (cache_sb->s_op->clear_inode)
+		cache_sb->s_op->clear_inode(cache_inode);
+
+	//clear_inode(cache_inode);
+	duplicate_inode(inode, cache_inode);
+	
 	return;	
 }
+static void smfs_delete_inode(struct inode *inode)
+{
+	struct inode *cache_inode;
+	struct super_block *cache_sb;
 
+	ENTRY;
+	cache_inode = I2CI(inode);
+	cache_sb = S2CSB(inode->i_sb);
+
+	if (!cache_inode || !cache_sb)
+		return;
+		
+	duplicate_inode(inode, cache_inode); 
+	
+	list_del(&cache_inode->i_hash);
+        INIT_LIST_HEAD(&cache_inode->i_hash);
+        list_del(&cache_inode->i_list);
+        INIT_LIST_HEAD(&cache_inode->i_list);
+    	
+	if (cache_inode->i_data.nrpages)
+        	truncate_inode_pages(&cache_inode->i_data, 0);
+	
+	if (cache_sb->s_op->delete_inode)
+		cache_sb->s_op->delete_inode(cache_inode);
+
+	duplicate_inode(cache_inode, inode); 
+	
+	return;
+}
+static void smfs_write_inode(struct inode *inode, int wait)
+{
+	struct inode *cache_inode;
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_inode = I2CI(inode);
+	cache_sb = S2CSB(inode->i_sb);
+
+	if (!cache_inode || !cache_sb)
+		return;
+		
+	if (cache_sb->s_op->write_inode)
+		cache_sb->s_op->write_inode(cache_inode, wait);
+
+	duplicate_inode(cache_inode, inode); 
+	
+	return;
+}
+static void smfs_dirty_inode(struct inode *inode)
+{
+	struct inode *cache_inode;
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_inode = I2CI(inode);
+	cache_sb = S2CSB(inode->i_sb);
+
+	if (!cache_inode || !cache_sb)
+		return;
+		
+	if (cache_sb->s_op->dirty_inode)
+		cache_sb->s_op->dirty_inode(cache_inode);
+
+	duplicate_inode(cache_inode, inode); 
+	return;
+}
+
+static void smfs_put_inode(struct inode *inode)
+{
+	struct inode *cache_inode;
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_inode = I2CI(inode);
+	cache_sb = S2CSB(inode->i_sb);
+
+	if (!cache_inode || !cache_sb)
+		return;
+		
+	if (cache_sb->s_op->put_inode)
+		cache_sb->s_op->put_inode(cache_inode);
+
+	return;
+}
+
+static void smfs_write_super(struct super_block *sb)
+{
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_sb = S2CSB(sb);
+
+	if (!cache_sb)
+		return;
+		
+	if (cache_sb->s_op->write_super)
+		cache_sb->s_op->write_super(cache_sb);
+
+	duplicate_sb(cache_sb, sb);
+	return;
+}
+
+static void smfs_write_super_lockfs(struct super_block *sb)
+{
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_sb = S2CSB(sb);
+
+	if (!cache_sb)
+		return;
+		
+	if (cache_sb->s_op->write_super_lockfs)
+		cache_sb->s_op->write_super_lockfs(cache_sb);
+
+	duplicate_sb(cache_sb, sb);
+	return;
+}
+
+static void smfs_unlockfs(struct super_block *sb)
+{
+	struct super_block *cache_sb;
+
+	ENTRY;
+	cache_sb = S2CSB(sb);
+
+	if (!cache_sb)
+		return;
+		
+	if (cache_sb->s_op->unlockfs)
+		cache_sb->s_op->unlockfs(cache_sb);
+
+	duplicate_sb(cache_sb, sb);
+	return;
+}
+static int smfs_statfs(struct super_block * sb, struct statfs * buf) 
+{
+	struct super_block *cache_sb;
+	int	rc = 0;
+
+	ENTRY;
+	cache_sb = S2CSB(sb);
+
+	if (!cache_sb)
+		return;
+		
+	if (cache_sb->s_op->statfs)
+		rc = cache_sb->s_op->statfs(cache_sb, buf);
+
+	duplicate_sb(cache_sb, sb);
+	
+	return rc;
+}
+static int smfs_remount(struct super_block * sb, int * flags, char * data)
+{
+	struct super_block *cache_sb;
+	int    rc = 0;
+
+	ENTRY;
+	cache_sb = S2CSB(sb);
+
+	if (!cache_sb)
+		RETURN(-EINVAL);
+		
+	if (cache_sb->s_op->remount_fs)
+		rc = cache_sb->s_op->remount_fs(cache_sb, flags, data);
+
+	duplicate_sb(cache_sb, sb);
+	RETURN(rc);
+}
 struct super_operations smfs_super_ops = {
 	read_inode:	smfs_read_inode,
-	clear_inode:	smfs_clear_inode,
+	//clear_inode:	smfs_clear_inode,
 	put_super:	smfs_put_super,
+	delete_inode:	smfs_delete_inode,
+        write_inode:	smfs_write_inode,
+        dirty_inode:    smfs_dirty_inode,       /* BKL not held.  We take it */
+        put_inode:      smfs_put_inode,         /* BKL not held.  Don't need */
+
+        write_super:    smfs_write_super,       /* BKL held */
+        write_super_lockfs: smfs_write_super_lockfs, /* BKL not held. Take it */
+        unlockfs:       smfs_unlockfs,          /* BKL not held.  We take it */
+        statfs:         smfs_statfs,            /* BKL held */
+        remount_fs:     smfs_remount,           /* BKL held */
+
 };
 
 
