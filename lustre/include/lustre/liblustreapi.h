311ff4a10e9a6f77f9d2e0b2716517e6aca03b8c
shaocheng <shaocheng>
2004-02-10 09:44:54 +0000

    - last commit failed to add and remove some files listed below.

---
+++ lustre/include/lustre/liblustreapi.h
@@ -0,0 +1,41 @@
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
+ * vim:expandtab:shiftwidth=8:tabstop=8:
+ *
+ *  Copyright (C) 2002 Cluster File Systems, Inc.
+ *   Author: Peter J. Braam <braam@clusterfs.com>
+ *   Author: Phil Schwan <phil@clusterfs.com>
+ *   Author: Brian Behlendorf <behlendorf1@llnl.gov> 
+ *   Author: Robert Read <rread@clusterfs.com> 
+ *
+ *   This file is part of Lustre, http://www.lustre.org.
+ *
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+#ifndef _LIBLUSTREAPI_H_
+#define _LIBLUSTREAPI_H_
+
+/* liblustreapi.c */
+int op_create_file(char *name, long stripe_size, int stripe_offset,
+         int stripe_count);
+int op_setstripe_dir(char *path, long stripe_size, int stripe_offset,
+         int stripe_count);
+int op_find(char *path, struct obd_uuid *obduuid, int recursive,
+         int verbose, int quiet);
+int op_check(int type_num, char **obd_type_p, char *dir);
+int op_catinfo(char *dir, char *keyword, char *node_name);
+
+
+
+#endif
