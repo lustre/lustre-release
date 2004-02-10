42a807b66b280a7a25cdba76984fab18c0533aa8
adilger <adilger>
2004-02-10 20:19:28 +0000

    Add land[12].sh and add some extra checks to merge[12].sh.

--- lustre/scripts/merge1.sh
+++ lustre/scripts/merge1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e 
 
+CONFLICTS=cvs-merge-conflicts
 CVS=cvs
 
 if [ -f .mergeinfo ] ; then
@@ -7,8 +8,8 @@ if [ -f .mergeinfo ] ; then
     exit 
 fi
 
-if [ -f merge-conflicts ] ; then
-    echo "cvs-merge-conflicts exists - clean up first"
+if [ -f $CONFLICTS ] ; then
+    echo "$CONFLICTS exists - clean up first"
     exit 
 fi
 
@@ -18,17 +19,26 @@ if [ $# != 2 ]; then
 fi
 
 parent=$1
-PARENT=`echo $parent | tr '[a-z]' '[A-Z]'`
+PARENT=`echo $parent | sed -e "s/^b_//" | tr "[a-z]" "[A-Z]"`
 child=$2
-CHILD=`echo $child | tr '[a-z]' '[A-Z]'`
+CHILD=`echo $child | sed -e "s/^b_//" | tr "[a-z]" "[A-Z]"`
 date=`date +%Y%m%d_%H%M`
 module=lustre
 
-if [ $parent != "HEAD" ]; then
-  parent="b_$parent"
-fi
-if [ $child != "HEAD" ]; then
-  child="b_$child"
+case $parent in
+  HEAD) : ;;
+  b_*|b1*) : ;;
+  *) parent="b_$parent" ;;
+esac
+case $child in
+  HEAD) : ;;
+  b_*|b1*) : ;;
+  *) child="b_$child"
+esac
+
+if [ "$child" != "HEAD" -a "`cat CVS/Tag`" != "T$child" ]; then
+	echo "This script must be run within the $child branch"
+	exit 1
 fi
 
 cat << EOF > .mergeinfo
@@ -38,24 +48,26 @@ child=$child
 CHILD=$CHILD
 date=$date
 module=$module
+CONFLICTS=$CONFLICTS
 EOF
 
-echo PARENT $PARENT parent $parent CHILD $CHILD child $child date $date
+echo PARENT: $PARENT parent: $parent CHILD: $CHILD child: $child date: $date
 
-echo -n "tagging $parent as ${PARENT}_${CHILD}_UPDATE_PARENT_$date ...."
+echo -n "tagging $parent as '${PARENT}_${CHILD}_UPDATE_PARENT_$date' ...."
 $CVS rtag -r $parent ${PARENT}_${CHILD}_UPDATE_PARENT_$date $module
 echo "done"
-echo -n "tagging $child as ${PARENT}_${CHILD}_UPDATE_CHILD_$date ...."
+echo -n "tagging $child as '${PARENT}_${CHILD}_UPDATE_CHILD_$date' ...."
 $CVS rtag -r $child ${PARENT}_${CHILD}_UPDATE_CHILD_$date $module
 echo "done"
 echo "Updating: -j ${CHILD}_BASE -j ${PARENT}_${CHILD}_UPDATE_PARENT_$date ...."
 $CVS update -j ${CHILD}_BASE -j ${PARENT}_${CHILD}_UPDATE_PARENT_$date -dP
 echo "done"
-echo -n "Recording conflicts in cvs-merge-conflicts ..."
-if $CVS update | grep '^C' > cvs-merge-conflicts; then
+echo -n "Recording conflicts in $CONFLICTS ..."
+if $CVS update | grep '^C' > $CONFLICTS; then
     echo "Conflicts found, fix before committing."
-    cat cvs-merge-conflicts
+    cat $CONFLICTS
 else 
     echo "No conflicts found"
+    rm -f $CONFLICTS
 fi
 echo "Test, commit and then run merge2.sh (no arguments)"
