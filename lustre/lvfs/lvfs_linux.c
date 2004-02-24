3d22da61cd56abc0d9fcbc759e20f852606d345d
adilger <adilger>
2004-02-24 01:11:32 +0000

    Update b_smallfix from HEAD (20040223_1758) (mostly b_size landing).

--- lustre/lvfs/lvfs_linux.c
+++ lustre/lvfs/lvfs_linux.c
@@ -370,8 +370,6 @@ EXPORT_SYMBOL(l_readdir);
 EXPORT_SYMBOL(obd_memory);
 EXPORT_SYMBOL(obd_memmax);
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0))
-
 static int __init lvfs_linux_init(void)
 {
         RETURN(0);
@@ -395,10 +393,3 @@ MODULE_LICENSE("GPL");
 
 module_init(lvfs_linux_init);
 module_exit(lvfs_linux_exit);
-
-#else
-
-#warning "lvfs_linux_init() and fsfilt_ext3_exit() aren't called on 2.6. MUST be fixed"
-
-
-#endif
