5c0404058bf3bb313426ec11e93071989f6b835b
adilger <adilger>
2004-03-08 17:12:40 +0000

    Fix munlink messages to not make it appear like there was an unlink failure. b=2167

--- lustre/tests/munlink.c
+++ lustre/tests/munlink.c
@@ -13,16 +13,23 @@ int main(int argc, char ** argv)
 {
         int rc, i;
 
-        if (argc < 2) { 
-                printf("Usage %s filename\n", argv[0]);
+        if (argc < 2) {
+                printf("Usage %s filename {filename ...}\n", argv[0]);
                 return 1;
         }
 
         for (i = 1; i < argc; i++) {
-        	rc = unlink(argv[i]);
-        	if (rc)
-                	printf("unlink(%s) error: %s\n", argv[i],
-                               strerror(errno));
+                rc = unlink(argv[i]);
+                if (rc) {
+                        printf("unlink(%s): %s ", argv[i], strerror(errno));
+                        rc = access(argv[i], F_OK);
+                        if (rc && errno == ENOENT)
+                                printf("(unlinked anyways)\n");
+                        else if (rc == 0)
+                                printf("(still exists)\n");
+                        else
+                                printf("(%s looking up)\n", strerror(errno));
+                }
         }
         return rc;
-} 
+}
