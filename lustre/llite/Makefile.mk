ea234b443a4062341c858c099817485a4e1ba73c
adilger <adilger>
2004-03-18 06:18:45 +0000

    Remove useless and unused llite/sysctl.c file.

--- lustre/llite/Makefile.mk
+++ lustre/llite/Makefile.mk
@@ -7,5 +7,5 @@ include $(src)/../portals/Kernelenv
 
 obj-y += llite.o
 llite-objs := llite_lib.o dcache.o super.o rw.o \
-	super25.o file.o dir.o sysctl.o symlink.o namei.o lproc_llite.o \
+	super25.o file.o dir.o symlink.o namei.o lproc_llite.o \
 	rw26.o llite_nfs.o llite_close.o
