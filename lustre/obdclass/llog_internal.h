754d0867f6d180eaf828761a6fc94bf0811e537d
tianying <tianying>
2004-04-29 04:20:44 +0000

    fix smfs_lookup, and add LASSERT to llog_lvfs_create

--- lustre/obdclass/llog_internal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LLOG_INTERNAL_H__
-#define __LLOG_INTERNAL_H__
-
-int llog_put_cat_list(struct obd_device *obd, struct obd_device *disk_obd,
-                      char *name, int count, struct llog_catid *idarray);
-int llog_cat_id2handle(struct llog_handle *cathandle, struct llog_handle **res,
-                       struct llog_logid *logid);
-#endif
