b9d655e5c04392fd6b8141bc3897c00bc27b5eec
cvs2svn <admin@example.com>
2004-06-02 15:06:29 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.3.258'.

--- lustre/utils/Lustre/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["lustredb"]
-
-from lustredb import LustreDB, LustreDB_XML, LustreDB_LDAP
-from error import LconfError, OptionError
-from cmdline import Options
-
-CONFIG_VERSION="2003070801"
