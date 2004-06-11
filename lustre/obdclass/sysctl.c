4e65b400c6ead977cdb0a6395f2ea2da1bb7d8a7
adilger <adilger>
2004-06-11 01:47:21 +0000

    Land b1_4_smallfix onto b1_4 (20040610_1356) (excluding Jacob's recent commit) - balance journal closure when 2.6 filter write fails (3401) - clean kiobufs before and after use (3485) - strip trailing '/'s before comparing paths with /proc/mounts (3486) - remove assertions to work around "in-flight rpcs" recovery bug (3063) - change init script to fail more clearly if not run as root (1528) - fix ns_lock/i_sem lock ordering deadlock for kms update (3477) - don't do DNS lookups on NIDs too small for IP addresses (3442) - dynamic ptlrpc request buffer allocation (2102) - set MDS/OST threads to umask 0 to not clobber client modes (3359) - servers can dump a log evicting a client - lustre.dump_on_timeout=1

--- lustre/obdclass/sysctl.c
+++ lustre/obdclass/sysctl.c
@@ -50,9 +50,11 @@ struct ctl_table_header *obd_table_header = NULL;
 enum {
         OBD_FAIL_LOC = 1,       /* control test failures instrumentation */
         OBD_TIMEOUT,            /* RPC timeout before recovery/intr */
+        OBD_DUMP_ON_TIMEOUT,    /* dump kernel debug log upon eviction */
         OBD_UPCALL,             /* path to recovery upcall */
         OBD_MEMUSED,            /* bytes currently OBD_ALLOCated */
         OBD_SYNCFILTER,         /* XXX temporary, as we play with sync osts.. */
+        OBD_LDLM_TIMEOUT,       /* LDLM timeout for ASTs before client eviction */
 };
 
 int proc_fail_loc(ctl_table *table, int write, struct file *filp,
@@ -63,6 +65,8 @@ static ctl_table obd_table[] = {
                 &proc_dointvec},
         {OBD_TIMEOUT, "timeout", &obd_timeout, sizeof(int), 0644, NULL,
                 &proc_fail_loc},
+        {OBD_DUMP_ON_TIMEOUT, "dump_on_timeout", &obd_dump_on_timeout,
+                sizeof(int), 0644, NULL, &proc_dointvec},
         /* XXX need to lock so we avoid update races with recovery upcall! */
         {OBD_UPCALL, "upcall", obd_lustre_upcall, 128, 0644, NULL,
                 &proc_dostring, &sysctl_string },
@@ -70,6 +74,8 @@ static ctl_table obd_table[] = {
                 sizeof(int), 0644, NULL, &proc_dointvec},
         {OBD_SYNCFILTER, "filter_sync_on_commit", &obd_sync_filter, sizeof(int),
                 0644, NULL, &proc_dointvec},
+        {OBD_LDLM_TIMEOUT, "ldlm_timeout", &ldlm_timeout, sizeof(int), 0644,
+                NULL, &proc_dointvec},
         { 0 }
 };
 
