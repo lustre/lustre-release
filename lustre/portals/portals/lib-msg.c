3bd6bf342ae3cca61671b7819e30d1a9cfabf462
adilger <adilger>
2004-07-20 16:10:41 +0000

    Land b1_4_smallfix onto b1_4 (20040720_0138) - don't hold llog sem during network request (3652) - update server last transno after client disconnects (2525) - replace config semaphore with spinlock (3306) - don't allow multiple threads in OSC recovery(3812) - don't close LustreDB during write_conf until it is done (3860) - fix typo in lconf for_each_profile (3821) - allow dumping logs from multiple threads at one time (3820) - drop dentry ref in ext3_add_link from open_connect_dentry (3266) - free recovery state on server during a forced cleanup (3571) - unregister_reply for resent reqs (3063) - loop back devices mounting and status check on 2.6 (3563) - fix resource-creation race that can provoke i_size == 0 (3513) - don't try to use bad inodes returned from MDS/OST fs lookup (3688) - more debugging for page-accounting assertion (3746) - return -ENOENT instead of asserting if ost getattr+unlink race (3558) - avoid deadlock after precreation failure (3758) - fix race and lock order deadlock in orphan handling (3450, 3750) - add /proc/.../recovery_status to obdfilter (3428) - lightweight CDEBUG infrastructure, debug daemon (3668) - change default OSC RPC parameters to be better on small clusters - turn off OST read cache for files smaller than 32MB

--- lustre/portals/portals/lib-msg.c
+++ lustre/portals/portals/lib-msg.c
@@ -91,16 +91,16 @@ lib_finalize (lib_nal_t *nal, void *private, lib_msg_t *msg, ptl_err_t status)
                 LASSERT(msg->ev.type == PTL_EVENT_PUT_END);
 
                 memset (&ack, 0, sizeof (ack));
-                ack.type     = HTON__u32 (PTL_MSG_ACK);
-                ack.dest_nid = HTON__u64 (msg->ev.initiator.nid);
-                ack.dest_pid = HTON__u32 (msg->ev.initiator.pid);
-                ack.src_nid  = HTON__u64 (nal->libnal_ni.ni_pid.nid);
-                ack.src_pid  = HTON__u32 (nal->libnal_ni.ni_pid.pid);
+                ack.type     = cpu_to_le32(PTL_MSG_ACK);
+                ack.dest_nid = cpu_to_le64(msg->ev.initiator.nid);
+                ack.dest_pid = cpu_to_le32(msg->ev.initiator.pid);
+                ack.src_nid  = cpu_to_le64(nal->libnal_ni.ni_pid.nid);
+                ack.src_pid  = cpu_to_le32(nal->libnal_ni.ni_pid.pid);
                 ack.payload_length = 0;
 
                 ack.msg.ack.dst_wmd = msg->ack_wmd;
                 ack.msg.ack.match_bits = msg->ev.match_bits;
-                ack.msg.ack.mlength = HTON__u32 (msg->ev.mlength);
+                ack.msg.ack.mlength = cpu_to_le32(msg->ev.mlength);
 
                 rc = lib_send (nal, private, NULL, &ack, PTL_MSG_ACK,
                                msg->ev.initiator.nid, msg->ev.initiator.pid, 
