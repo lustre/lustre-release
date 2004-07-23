c6960bf831621b0a97fc386dfdf6a0f52a7ae777
adilger <adilger>
2004-07-23 06:52:08 +0000

    Merge b1_4_smallfix from b1_4 (20040722_1843) (mballoc) - ext3 exents and multi-block allocation (3024) - service time statistics in /proc - minor fixes to liblustre build (3317)

--- lustre/include/linux/lprocfs_status.h
+++ lustre/include/linux/lprocfs_status.h
@@ -258,6 +258,11 @@ void lprocfs_oh_tally(struct obd_histogram *oh, unsigned int value);
 void lprocfs_oh_tally_log2(struct obd_histogram *oh, unsigned int value);
 void lprocfs_oh_clear(struct obd_histogram *oh);
 unsigned long lprocfs_oh_sum(struct obd_histogram *oh);
+struct obd_service_time;
+void lprocfs_stime_record(struct obd_service_time *stime, 
+                          struct timeval *large, struct timeval *small);
+unsigned long lprocfs_stime_avg_ms(struct obd_service_time *stime);
+unsigned long lprocfs_stime_avg_us(struct obd_service_time *stime);
 
 /* lprocfs_status.c: counter read/write functions */
 extern int lprocfs_counter_read(char *page, char **start, off_t off,
@@ -277,6 +282,10 @@ static inline void lprocfs_counter_incr(struct lprocfs_stats *stats,
 static inline void lprocfs_counter_init(struct lprocfs_stats *stats,
                                         int index, unsigned conf,
                                         const char *name, const char *units)
+{ return; }
+struct obd_service_time;
+static inline void lprocfs_stime_record(struct obd_service_time *stime, 
+                          struct timeval *large, struct timeval *small)
 { return; }
 
 static inline struct lprocfs_stats* lprocfs_alloc_stats(unsigned int num)
