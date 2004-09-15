7ea6a9289d0b5309bce6c257aeb7e32951e852bb
ccooper <ccooper>
2004-09-15 21:53:09 +0000

    r=adilger,nic
    - landing b1_4_smallfix

--- lustre/include/linux/lvfs.h
+++ lustre/include/linux/lvfs.h
@@ -1,10 +1,26 @@
 /* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
  * vim:expandtab:shiftwidth=8:tabstop=8:
  *
- * Copyright (C) 2004  Cluster File Systems, Inc.
+ *  Copyright (C) 2001, 2002, 2003, 2004 Cluster File Systems, Inc.
  *
- * This code is issued under the GNU General Public License.
- * See the file COPYING in this distribution
+ *  Author: <braam@clusterfs.com>
+ *
+ *   This file is part of Lustre, http://www.lustre.org.
+ *
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * lustre VFS/process permission interface
  */
 
 #ifndef __LVFS_H__
@@ -12,11 +28,11 @@
 
 #include <linux/kp30.h>
 
-#define LL_FID_NAMELEN  (16 + 1 + 8 + 1)
+#define LL_FID_NAMELEN (16 + 1 + 8 + 1)
 
 #if defined __KERNEL__
 #include <linux/lvfs_linux.h>
-#endif 
+#endif
 
 #ifdef LIBLUSTRE
 #include <lvfs_user_fs.h>
@@ -106,6 +122,16 @@ static inline struct dentry *ll_lookup_one_len(const char *fid_name,
         down(&dparent->d_inode->i_sem);
         dchild = lookup_one_len(fid_name, dparent, fid_namelen);
         up(&dparent->d_inode->i_sem);
+
+        if (IS_ERR(dchild) || dchild->d_inode == NULL)
+                return dchild;
+
+        if (is_bad_inode(dchild->d_inode)) {
+                CERROR("bad inode returned %lu/%u\n",
+                       dchild->d_inode->i_ino, dchild->d_inode->i_generation);
+                dput(dchild);
+                dchild = ERR_PTR(-ENOENT);
+        }
 
         return dchild;
 }
