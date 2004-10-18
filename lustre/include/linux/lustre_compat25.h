a9552c22aa94755c64929428a66adfbcffb99fd7
ccooper <ccooper>
2004-10-18 23:28:27 +0000

    - include mm_inline on 2.4 kernels (required for page_count)

--- lustre/include/linux/lustre_compat25.h
+++ lustre/include/linux/lustre_compat25.h
@@ -119,6 +119,10 @@ static inline int cleanup_group_info(void)
 
 #else /* 2.4.. */
 
+#ifdef HAVE_MM_INLINE 
+#include <linux/mm_inline.h> 
+#endif
+
 #define ll_vfs_create(a,b,c,d)              vfs_create(a,b,c)
 #define ll_permission(inode,mask,nd)        permission(inode,mask)
 #define ILOOKUP(sb, ino, test, data)        ilookup4(sb, ino, test, data);
