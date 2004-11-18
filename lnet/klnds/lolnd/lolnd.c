39f984661391eeb60cf1076a1a1b2243b93d623b
eeb <eeb>
2004-11-18 17:52:55 +0000

    *   Fixed lonal copyright + unused vars

--- lnet/klnds/lolnd/lolnd.c
+++ lnet/klnds/lolnd/lolnd.c
@@ -1,32 +1,29 @@
-/*
- * Copyright (C) 2002 Cluster File Systems, Inc.
- *   Author: Eric Barton <eric@bartonsoftware.com>
+/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
+ * vim:expandtab:shiftwidth=8:tabstop=8:
  *
- * Copyright (C) 2002, Lawrence Livermore National Labs (LLNL)
- * W. Marcus Miller - Based on ksocknal
+ *  Copyright (C) 2004 Cluster File Systems, Inc.
  *
- * This file is part of Portals, http://www.sf.net/projects/lustre/
+ *   This file is part of Lustre, http://www.lustre.org.
  *
- * Portals is free software; you can redistribute it and/or
- * modify it under the terms of version 2 of the GNU General Public
- * License as published by the Free Software Foundation.
+ *   Lustre is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License as published by the Free Software Foundation.
  *
- * Portals is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *   Lustre is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Portals; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Lustre; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #include "lonal.h"
 
 nal_t			klonal_api;
 klonal_data_t		klonal_data;
-ptl_handle_ni_t        klonal_ni;
+ptl_handle_ni_t         klonal_ni;
 
 
 int
@@ -49,8 +46,6 @@ klonal_cmd (struct portals_cfg *pcfg, void *private)
 static void
 klonal_shutdown(nal_t *nal)
 {
-	unsigned long flags;
-
 	/* NB The first ref was this module! */
 	if (nal->nal_refct != 0)
 		return;
@@ -90,7 +85,6 @@ klonal_startup (nal_t *nal, ptl_pid_t requested_pid,
 		ptl_ni_limits_t *actual_limits)
 {
 	int               rc;
-	int               i;
 	ptl_process_id_t  my_process_id;
 	int               pkmem = atomic_read(&portal_kmemory);
 
