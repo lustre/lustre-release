dface99a7ff57fd17e86bc4d54a8c4b1dac900d4
cvs2svn <admin@example.com>
2005-02-04 18:50:01 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.100'.

--- lustre/tests/lsnap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-MDSDEV=smfs OSTDEV=smfs FSTYPE=smfs MDS_MOUNT_OPTS="kml,snap" OST_MOUNT_OPTS="kml,snap" \
-OSTSIZE=100000 MDSSIZE=50000 MDS_BACKFSTYPE=ext3 OST_BACKFSTYPE=ext3 \
-MDS_BACKDEV=/tmp/mds1-$(hostname) OST_BACKDEV=/tmp/ost1-$(hostname) sh llmount.sh
