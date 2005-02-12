2cf76d01116451a107966af72e7421d7a675be49
phil <phil>
2005-02-12 06:23:38 +0000

    I keep finding these trees that I have, with little uncommitted copyright changes, and wondering how that happened.

--- lnet/lnet/api-errno.c
+++ lnet/lnet/api-errno.c
@@ -5,6 +5,7 @@
  * Instantiate the string table of errors
  *
  *   This file is part of Lustre, http://www.sf.net/projects/lustre/
+ *   This file is not subject to copyright protection.
  */
 
 /* If you change these, you must update the number table in portals/errno.h */
--- lnet/lnet/lib-pid.c
+++ lnet/lnet/lib-pid.c
@@ -1,36 +1,13 @@
 /* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
  * vim:expandtab:shiftwidth=8:tabstop=8:
  *
- * lib/lib-pid.c
- *
- * Process identification routines
- * Copyright (C) 2001-2003 Cluster File Systems, Inc.
- *
  *   This file is part of Lustre, http://www.lustre.org
- *
- *   Lustre is free software; you can redistribute it and/or
- *   modify it under the terms of version 2 of the GNU General Public
- *   License as published by the Free Software Foundation.
- *
- *   Lustre is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Lustre; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   This file is not subject to copyright protection.
  */
 
 /* This should be removed.  The NAL should have the PID information */
 #define DEBUG_SUBSYSTEM S_PORTALS
 
-#if defined (__KERNEL__)
-extern int getpid(void);
-#else
-#       include <stdio.h>
-#       include <unistd.h>
-#endif
 #include <portals/lib-p30.h>
 
 int
