2c33759f4b3c28208109e850f441014cd7427fc9
nic <nic>
2005-03-04 02:11:54 +0000

    clean up MOUNT grep

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -167,10 +167,10 @@ pass() {
 	echo PASS $@
 }
 
-MOUNT="`cat /proc/mounts | egrep "lustre|lustre_lite" | awk '{print $2}'`"
+MOUNT="`cat /proc/mounts | grep "lustre" | awk '{print $2}'`"
 if [ -z "$MOUNT" ]; then
 	sh llmount.sh
-	MOUNT="`cat /proc/mounts | egrep "lustre|lustre_lite" | awk '{print $2}'`"
+	MOUNT="`cat /proc/mounts | grep "lustre" | awk '{print $2}'`"
 	[ -z "$MOUNT" ] && error "NAME=$NAME not mounted"
 	I_MOUNTED=yes
 fi
