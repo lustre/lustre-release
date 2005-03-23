85be04a556acc0d0cd83de7579c81d73a8f449db
adilger <adilger>
2005-03-23 18:54:34 +0000

    Branch: HEAD Move clearpatches.sh and confirmpatches.sh into build from lustre-core.

---
+++ build/confirmpatches.sh
@@ -0,0 +1,11 @@
+SERIESPATH=./series
+PATCHESPATH=./patches
+for SERIES in `ls $SERIESPATH | grep -v CVS` ; do
+	#echo $SERIES
+	for PATCH in `cat $SERIESPATH/$SERIES`; do
+		#echo $PATCH
+		if [ ! `find $PATCHESPATH -name $PATCH` ]; then
+			echo "$SERIESPATH/$SERIES: patch $PATCH was not found !"
+		fi
+	done
+done
