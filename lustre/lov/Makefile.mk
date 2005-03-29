cc86a34105e8eae3aba1a43c246fdfb546fae856
jacob <jacob>
2005-03-29 04:55:56 +0000

    r=phil
    these haven't been used in years and years.

--- lustre/lov/Makefile.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2003  Cluster File Systems, Inc.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-
-include $(src)/../portals/Kernelenv
-
-obj-y += lov.o
-lov-objs := lov_obd.o lov_pack.o lproc_lov.o lov_log.o lov_offset.o lov_merge.o lov_request.o lov_qos.o
