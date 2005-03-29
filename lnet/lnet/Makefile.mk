0b149ce38ca8f81876279647e1c9ea08eddc03c3
jacob <jacob>
2005-03-29 05:27:18 +0000

    r=phil
    remove unused files.

--- lnet/lnet/Makefile.mk
+++
@@ -1,7 +0,0 @@
-include $(src)/../Kernelenv
-
-obj-y += portals.o
-portals-objs    := 	lib-eq.o lib-init.o lib-md.o lib-me.o \
-			lib-move.o lib-msg.o lib-ni.o lib-pid.o \
-			api-errno.o api-ni.o api-wrap.o \
-			module.o
--- lustre/lmv/Makefile.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2003  Cluster File Systems, Inc.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-
-include $(src)/../portals/Kernelenv
-
-obj-y += lmv.o
-lmv-objs := lmv_obd.o lmv_intent.o lmv_objmgr.o lproc_lmv.o
