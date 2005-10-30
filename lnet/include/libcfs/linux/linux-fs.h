2b3c9210b3daa02a3d165d58f1925fa4625ec3a7
lsy <lsy>
2005-10-30 08:12:13 +0000

    revert quota file write. fsfilt_write_record will ignore page cache, which will lead to error for quota functions in kernel.

--- lnet/include/libcfs/linux/linux-fs.h
+++ lnet/include/libcfs/linux/linux-fs.h
@@ -65,6 +65,7 @@ typedef struct file_lock cfs_flock_t;
 #define CFS_FLOCK_END(fl)                   ((fl)->fl_end)
 #define CFS_FLOCK_SET_END(fl, end)          do { (fl)->fl_end = (end); } while(0)
 
+ssize_t cfs_user_write (cfs_file_t *filp, const char *buf, size_t count, loff_t *offset);
 #endif
 
 #endif
