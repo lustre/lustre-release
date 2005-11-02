a062886418831584559ad0419f6177ae0abdd9cc
lsy <lsy>
2005-11-02 12:19:35 +0000

    initialize size in cfs_user_write. rename lfs_quotachog to lfs_quotachown.

--- lnet/libcfs/linux/linux-fs.c
+++ lnet/libcfs/linux/linux-fs.c
@@ -32,7 +32,7 @@ ssize_t
 cfs_user_write (cfs_file_t *filp, const char *buf, size_t count, loff_t *offset)
 {
 	mm_segment_t fs;
-	ssize_t size;
+	ssize_t size = 0;
 
 	fs = get_fs();
 	set_fs(KERNEL_DS);
