c8521aa1599ce9130c1739b5a16f599f72a0328a
cliffw <cliffw>
2006-04-03 15:55:06 +0000

    Removed per Peter Bojanic. Will return if the code can be fixed and tested.

--- lustre-iokit/obdsurvey/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(name='lustre-iokit',
-      py_modules=['lustre_obdsurveylib'],
-      scripts=['lustre_obdsurvey.py'],
-      data_files=[
-              ('/usr/share/lustre-iokit', ['README', 'LICENSE'])
-              ]
-      )
