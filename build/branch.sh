ec54526780f06eb596829eb6a2e40da46c70d983
adilger <adilger>
2006-05-04 08:04:02 +0000

    Branch b1_4 Per Eric's request - remove portals from the build tree, as he is testing with Cray portals directly, when testing the ptllnd. r=eeb

--- build/branch.sh
+++ build/branch.sh
@@ -19,7 +19,7 @@ fi
 module=$(basename $(<$dir/CVS/Repository))
 
 if [ "$module" = "lustre" ]; then
-    echo >&2 "${progname}: You probably want to branch lustre or portals."
+    echo >&2 "${progname}: You probably want to branch lustre or lnet."
     echo >&2 "${progname}: Try using ${0} $parent $child lustre"
     exit 1
 fi
