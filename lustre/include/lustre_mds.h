4e5fd08371c7cadef39f69d598230fd7c9a6b198
ericm <ericm>
2006-05-30 20:48:22 +0000

    branch: b1_5 xattr_acl_size() instead of acl_xattr_size()

--- lustre/include/lustre_mds.h
+++ lustre/include/lustre_mds.h
@@ -88,21 +88,19 @@ struct mds_file_data {
 
 /* ACL */
 #ifdef CONFIG_FS_POSIX_ACL
-#ifdef CONFIG_FS_POSIX_ACL
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,16))
-#define MDS_XATTR_NAME_ACL_ACCESS POSIX_ACL_XATTR_ACCESS
-#define mds_xattr_acl_size(entry) posix_acl_xattr_size(entry)
-#else
-#define MDS_XATTR_NAME_ACL_ACCESS XATTR_NAME_ACL_ACCESS
-#define mds_xattr_acl_size(entry) acl_xattr_size(entry)
-#endif
-#endif
+# if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,16))
+#  define MDS_XATTR_NAME_ACL_ACCESS POSIX_ACL_XATTR_ACCESS
+#  define mds_xattr_acl_size(entry) posix_acl_xattr_size(entry)
+# else
+#  define MDS_XATTR_NAME_ACL_ACCESS XATTR_NAME_ACL_ACCESS
+#  define mds_xattr_acl_size(entry) xattr_acl_size(entry)
+# endif
 
-#define LUSTRE_POSIX_ACL_MAX_ENTRIES    (32)
-#define LUSTRE_POSIX_ACL_MAX_SIZE       \
+# define LUSTRE_POSIX_ACL_MAX_ENTRIES   (32)
+# define LUSTRE_POSIX_ACL_MAX_SIZE      \
                 (mds_xattr_acl_size(LUSTRE_POSIX_ACL_MAX_ENTRIES))
 #else
-#define LUSTRE_POSIX_ACL_MAX_SIZE       0
+# define LUSTRE_POSIX_ACL_MAX_SIZE      0
 #endif
 
 /* mds/mds_reint.c */
