2d9446c0244c10d63a62cebbb97b13f835af53c2
yury <yury>
2006-10-09 12:21:43 +0000

    - fixed to sanity-lmv.sh, we can't use find for that due to "Value is too big"

--- lustre/tests/sanity-lmv.sh
+++ lustre/tests/sanity-lmv.sh
@@ -239,7 +239,7 @@ run_test 1a " remove splitted dir ============================="
 test_1b() {
 	mkdir $DIR/1b0 || error
 	createmany -o $DIR/1b0/f 4000
-	find $DIR/1b0 -type f | xargs rm -f
+	for file in $DIR/1b0/*; do rm -f $file; done
 	NUM=`ls $DIR/1b0 | wc -l`
 	if [ $NUM -ne 0 ] ; then
 		echo "dir must be empty"
@@ -269,7 +269,7 @@ run_test 1b " remove splitted dir ============================="
 test_1c() {
 	mkdir $DIR/1b1 || error
 	createmany -o $DIR/1b1/f 4000
-	find $DIR/1b1 -type f | xargs rm -f
+	for file in $DIR/1b1/*; do rm -f $file; done
 	NUM=`ls $DIR/1b1 | wc -l`
 	if [ $NUM -ne 0 ] ; then
 		echo "dir must be empty"
