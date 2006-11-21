6fe48f1dab5ea52309461c5308907032a890cd7b
brian <brian>
2006-11-21 02:16:01 +0000

    Change the key for objects to their name. Support "--lmv <lmvname>" for CMD.  Using --lmv is how LMVs were built with lmc in 1.4 and since we are trying to source lmc, we should simply use the same syntax. find_obj()->find_objs() now returns all objects that match. So callers need to expect lists and deal with them.  In a lot of cases a caller can *know* that the list returned will be only one item if the lmc file is contructed correctly. Support specification of multiple MDTs on the same MDS using different devices. For this, the mountpoint of the MDT needs to use the fsname to make it unique. The MDTs and OSTs also need to be created with fsnames. Use the --node <name> from the lmc mtpt object as the fsname.

--- lustre/scripts/lmc2csv.pl
+++ lustre/scripts/lmc2csv.pl
@@ -49,10 +49,11 @@ sub add_net {
 sub add_mds {
     my $mds = {};
     $mds->{"node"} = get_arg_val("node", \@_);
-    $mds->{"mds"} = get_arg_val("mds", \@_);
+    $mds->{"name"} = get_arg_val("mds", \@_);
     $mds->{"fstype"} = get_arg_val("fstype", \@_);
     $mds->{"dev"} = get_arg_val("dev", \@_);
     $mds->{"size"} = get_arg_val("size", \@_);
+    $mds->{"lmv"} = get_arg_val("lmv", \@_);
     if ($#_ > 0) {
         print STDERR "Unknown arguments to \"--add mds\": @_\n";
         exit(1);
@@ -62,8 +63,9 @@ sub add_mds {
 
 sub add_lov {
     my $lov = {};
-    $lov->{"lov"} = get_arg_val("lov", \@_);
+    $lov->{"name"} = get_arg_val("lov", \@_);
     $lov->{"mds"} = get_arg_val("mds", \@_);
+    $lov->{"lmv"} = get_arg_val("lmv", \@_);
     $lov->{"stripe_sz"} = get_arg_val("stripe_sz", \@_);
     $lov->{"stripe_cnt"} = get_arg_val("stripe_cnt", \@_);
     $lov->{"stripe_pattern"} = get_arg_val("stripe_pattern", \@_);
@@ -77,7 +79,7 @@ sub add_lov {
 sub add_ost {
     my $ost = {};
     $ost->{"node"} = get_arg_val("node", \@_);
-    $ost->{"ost"} = get_arg_val("ost", \@_);
+    $ost->{"name"} = get_arg_val("ost", \@_);
     $ost->{"fstype"} = get_arg_val("fstype", \@_);
     $ost->{"dev"} = get_arg_val("dev", \@_);
     $ost->{"size"} = get_arg_val("size", \@_);
@@ -98,6 +100,7 @@ sub add_mtpt {
     $mtpt->{"path"} = get_arg_val("path", \@_);
     $mtpt->{"mds"} = get_arg_val("mds", \@_);
     $mtpt->{"lov"} = get_arg_val("lov", \@_);
+    $mtpt->{"lmv"} = get_arg_val("lmv", \@_);
     if ($#_ > 0) {
         print STDERR "Unknown arguments to \"--add mtpt\": @_\n";
         exit(1);
@@ -107,17 +110,20 @@ sub add_mtpt {
 
 no strict 'refs';
 
-sub find_obj {
+sub find_objs {
     my $type = shift;
     my $key = shift;
     my $value = shift;
     my @objs = @_;
 
+    my @found_objs;
     foreach my $obj (@objs) {
         if ($obj->{$key} eq $value) {
-            return $obj;
+            push(@found_objs, $obj);
         }
     }
+
+    return @found_objs;
 }
 
 sub lnet_options {
@@ -162,16 +168,66 @@ while(<>) {
 
 # link lovs to mdses
 foreach my $lov (@{$objs{"lov"}}) {
-    my $mds = find_obj("mds", "mds", $lov->{"mds"}, @{$objs{"mds"}});
-    $mds->{"lov"} = $lov;
+    foreach my $mds (find_objs("mds", "name", $lov->{"mds"}, @{$objs{"mds"}})) {
+        if ($mds) {
+            $mds->{"lov"} = $lov;
+        }
+    }
+    # try via lmvs as well
+    foreach my $mds (find_objs("mds", "lmv", $lov->{"lmv"}, @{$objs{"mds"}})) {
+        if ($mds) {
+            $mds->{"lov"} = $lov;
+        }
+    }
 }
+
+# create lmvs and link them to mdses
+foreach my $mds (@{$objs{"mds"}}) {
+    my $lmv;
+    my @lmvs = find_objs("lmv", "name", $mds->{"lmv"}, @{$objs{"lmv"}});
+    if ($#lmvs < 0) {
+        $lmv = {};
+        $lmv->{"name"} = $mds->{"lmv"};
+        push(@{$objs{"lmv"}}, $lmv);
+    } else {
+        $lmv = pop(@lmvs);
+    }
+    $mds->{"lmv"} = $lmv;
+}
+
+# link mtpts to lovs and lmvs or mdses
+foreach my $mtpt (@{$objs{"mtpt"}}) {
+    foreach my $mds (find_objs("mds", "name", $mtpt->{"mds"}, @{$objs{"mds"}})) {
+        if ($mds) {
+            $mds->{"mtpt"} = $mtpt;
+        }
+    }
+    foreach my $lmv (find_objs("lmv", "name", $mtpt->{"lmv"}, @{$objs{"lmv"}})) {
+        if ($lmv) {
+            $lmv->{"mtpt"} = $mtpt;
+        }
+    }
+    foreach my $lov (find_objs("lov", "name", $mtpt->{"lov"}, @{$objs{"lov"}})) {
+        if ($lov) {
+            $lov->{"mtpt"} = $mtpt;
+        }
+    }
+}
+
 # XXX could find failover pairs of osts and mdts here and link them to
 # one another and then fill in their details in the csv generators below
 my $COUNT = 1;
 foreach my $mds (@{$objs{"mds"}}) {
     # find the net for this node
-    my $net = find_obj("net", "node", $mds->{"node"}, @{$objs{"net"}});
+    my @nets = find_objs("net", "node", $mds->{"node"}, @{$objs{"net"}});
+    my $lmv = $mds->{"lmv"};
     my $lov = $mds->{"lov"};
+    my $mtpt;
+    if ($lmv) {
+        $mtpt = $mds->{"lmv"}->{"mtpt"};
+    } else {
+        $mtpt = $mds->{"mtpt"};
+    }
     my $mkfs_options="";
     if (defined($lov->{"stripe_sz"})) {
         $mkfs_options .= "lov.stripesize=" . $lov->{"stripe_sz"} . " ";
@@ -187,24 +243,31 @@ foreach my $mds (@{$objs{"mds"}}) {
         $mkfs_options = " --param=\"$mkfs_options\"";
     }
 
+    my $mount_point = "$MOUNTPT/" . $mds->{"name"};
+    if (defined($mtpt->{"node"})) {
+        $mkfs_options .= " --fsname=\"" . $mtpt->{"node"} . "\" ";
+        $mount_point .= "_" . $mtpt->{"node"};
+    }
+    $mkfs_options =~ s/\s*$//;
+
     if ($COUNT == 1) {
         # mgs/mdt
-        printf "%s,%s,%s,$MOUNTPT/%s,mgs|mdt,,,,--device-size=%s --noformat%s,,noauto\n", 
+        printf "%s,%s,%s,%s,mgs|mdt,,,,--device-size=%s --noformat%s,,noauto\n", 
         $mds->{"node"},
-        lnet_options($net),
+        lnet_options($nets[0]),
         $mds->{"dev"},
-        $mds->{"mds"},
+        $mount_point,
         $mds->{"size"},
         $mkfs_options;
 
-        push(@mgses, $net->{"nid"});
+        push(@mgses, $nets[0]->{"nid"});
     } else {
         # mdt
-        printf "%s,%s,%s,$MOUNTPT/%s,mdt,,\"%s\",,--device-size=%s --noformat%s,,noauto\n",
+        printf "%s,%s,%s,%s,mdt,,\"%s\",,--device-size=%s --noformat%s,,noauto\n",
         $mds->{"node"},
-        lnet_options($net),
+        lnet_options($nets[0]),
         $mds->{"dev"},
-        $mds->{"mds"},
+        $mount_point,
         join(",", @mgses),
         $mds->{"size"},
         $mkfs_options;
@@ -213,7 +276,6 @@ foreach my $mds (@{$objs{"mds"}}) {
 }
 
 foreach my $ost (@{$objs{"ost"}}) {
-    # find the net for this node
     my $mount_opts="noauto";
     if (defined($ost->{"mountfsoptions"})) {
         $mount_opts .= "," . $ost->{"mountfsoptions"};
@@ -230,12 +292,21 @@ foreach my $ost (@{$objs{"ost"}}) {
         $mkfs_options = " --param=\"$mkfs_options\"";
     }
     
-    my $net = find_obj("net", "node", $ost->{"node"}, @{$objs{"net"}});
-    printf "%s,%s,%s,$MOUNTPT/%s,ost,,\"%s\",,--device-size=%s --noformat%s,,\"%s\"\n", 
+    $ost->{"lov"} = (find_objs("lov", "name", $ost->{"lov"}, @{$objs{"lov"}}))[0];
+    my $mount_point = "$MOUNTPT/" . $ost->{"name"}, 
+    my $mtpt = $ost->{"lov"}->{"mtpt"};
+    if (defined($mtpt->{"node"})) {
+        $mkfs_options .= " --fsname=\"" . $mtpt->{"node"} . "\" ";
+        $mount_point .= "_" . $mtpt->{"node"};
+    }
+    $mkfs_options =~ s/\s*$//;
+    # find the net for this node
+    my @nets = find_objs("net", "node", $ost->{"node"}, @{$objs{"net"}});
+    printf "%s,%s,%s,%s,ost,,\"%s\",,--device-size=%s --noformat%s,,\"%s\"\n", 
     $ost->{"node"},
-    lnet_options($net),
+    lnet_options($nets[0]),
     $ost->{"dev"},
-    $ost->{"ost"},
+    $mount_point,
     join(",", @mgses),
     $ost->{"size"},
     $mkfs_options,
