e88b0824d834a6ed5c464d2d9484bd51faf8dc7e
cvs2svn <admin@example.com>
2007-02-10 00:05:21 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.9.46'.

--- lustre/tests/lsmfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# this is framework that sets env for SMFS before exec scripts
-FSTYPE=smfs MDS_MOUNT_OPTS="audit" OST_MOUNT_OPTS="audit" \
-MDS_BACKFSTYPE=ldiskfs OST_BACKFSTYPE=ldiskfs \
-MDSDEV=/tmp/mds1-$(hostname) OSTDEV=/tmp/ost1-$(hostname) sh $1
--- lustre/tests/recovery-small-upcall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-LUSTRE=`dirname $0`/..
-PATH=$LUSTRE/utils:$PATH
-lctl --device %$3 recover || logger -p kern.info recovery failed: $@
