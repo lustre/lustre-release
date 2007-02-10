4a047f546ef4408f526c78fb4a972c8914fd852f
cvs2svn <admin@example.com>
2007-02-10 00:05:08 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.19.48'.

--- lustre/include/linux/obd_lmv.h
+++
@@ -1,8 +0,0 @@
-/* -*- mode: c; c-basic-offset: 8; indent-tabs-mode: nil; -*-
- * vim:expandtab:shiftwidth=8:tabstop=8:
- */
-
-#ifndef _OBD_LMV_H__
-#define _OBD_LMV_H__
-
-#endif
--- lustre/tests/llmount-upcall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LUSTRE=`dirname $0`/..
-exec >> /tmp/recovery-`hostname`.log
-exec 2>&1
-
-$LUSTRE/utils/lconf --recover --verbose --tgt_uuid $2 --client_uuid $3 --conn_uuid $4 $LUSTRE/tests/local.xml
