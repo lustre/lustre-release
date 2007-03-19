79e6f0e38a1c532e069246f79cc5ebe1189eb4e9
nathan <nathan>
2007-03-19 23:11:10 +0000

    b=11013 This is a potential null pointer dereference. Caught by HEAD's conf_sanity test 29

--- lustre/ptlrpc/connection.c
+++ lustre/ptlrpc/connection.c
@@ -135,13 +135,15 @@ out_conn:
 int ptlrpc_put_connection(struct ptlrpc_connection *c)
 {
         int rc = 0;
-        lnet_process_id_t peer = c->c_peer;
+        lnet_process_id_t peer;
         ENTRY;
 
         if (c == NULL) {
                 CERROR("NULL connection\n");
                 RETURN(0);
         }
+
+        peer = c->c_peer;
 
         CDEBUG (D_INFO, "connection=%p refcount %d to %s\n",
                 c, atomic_read(&c->c_refcount) - 1, 
