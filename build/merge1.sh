a5debac55d7b148b99f0660e8865683c088ab6bf
scjody <scjody>
2007-04-24 01:12:07 +0000

    Branch HEAD
    Run these with "bash" not "sh" since they use bash-only features disabled
    when newer versions of bash are invoked as "sh".

--- build/merge1.sh
+++ build/merge1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e 
+#!/bin/bash -e 
 
 CONFLICTS=cvs-merge-conflicts
 CVS="cvs -z3"
--- build/merge2.sh
+++ build/merge2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e 
+#!/bin/bash -e 
 
 if [ ! -f .mergeinfo ] ; then
     echo ".mergeinfo doesn't exist - exit"
