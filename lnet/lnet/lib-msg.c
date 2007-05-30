977211ac514d75a3fd1af73f9655783ada7fe309
isaac <isaac>
2007-05-30 08:35:45 +0000

    i=eeb -   should set the_lnet.ln_finalizing before completing messages.

--- lnet/lnet/lib-msg.c
+++ lnet/lnet/lib-msg.c
@@ -199,6 +199,8 @@ lnet_finalize (lnet_ni_t *ni, lnet_msg_t *msg, int status)
 #else
         if (the_lnet.ln_finalizing)
                 goto out;
+
+        the_lnet.ln_finalizing = 1;
 #endif
 
         while (!list_empty(&the_lnet.ln_finalizeq)) {
