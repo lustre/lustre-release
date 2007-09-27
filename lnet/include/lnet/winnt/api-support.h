d9da04073e33569a596a09f113a7e3a41bb5da16
cvs2svn <admin@example.com>
2007-09-27 02:30:25 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.22.16'.

--- lnet/include/lnet/winnt/api-support.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WINNT_API_SUPPORT_H__
-#define __WINNT_API_SUPPORT_H__
-
-#ifndef __LNET_API_SUPPORT_H__
-#error Do not #include this file directly. #include <lnet/api-support.h> instead
-#endif
-
-
-#endif
