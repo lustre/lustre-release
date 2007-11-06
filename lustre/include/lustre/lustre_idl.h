67d8e01d2455677606046f50bcf1b4a07316b804
bwzhou <bwzhou>
2007-11-06 10:02:59 +0000

    Branch HEAD b=13933 i=adilger i=johann
    Set an upper limit for stripe count

--- lustre/include/lustre/lustre_idl.h
+++ lustre/include/lustre/lustre_idl.h
@@ -1462,6 +1462,7 @@ enum seq_op {
 
 #define LOV_MIN_STRIPE_SIZE 65536   /* maximum PAGE_SIZE (ia64), power of 2 */
 #define LOV_MAX_STRIPE_COUNT  160   /* until bug 4424 is fixed */
+#define LOV_V1_INSANE_STRIPE_COUNT 65532 /* maximum stripe count bz13933 */
 
 #define LOV_MAX_UUID_BUFFER_SIZE  8192
 /* The size of the buffer the lov/mdc reserves for the
