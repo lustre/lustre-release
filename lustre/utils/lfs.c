ad8a16dc6763acf1e86f28faaa3818724f9c1b10
johann <johann>
2007-11-23 08:42:57 +0000

    Branch b1_6 b=14022 i=tianzy i=andrew
    By default, enable/disable quotas for both user & group.

--- lustre/utils/lfs.c
+++ lustre/utils/lfs.c
@@ -1188,6 +1188,8 @@ static int lfs_quotaon(int argc, char **argv)
 
         if (qctl.qc_type)
                 qctl.qc_type--;
+        else /* by default, enable quota for both user & group */
+                qctl.qc_type = 0x02;
 
         if (argc == optind)
                 return CMD_HELP;
@@ -1235,6 +1237,8 @@ static int lfs_quotaoff(int argc, char **argv)
 
         if (qctl.qc_type)
                 qctl.qc_type--;
+        else /* by default, disable quota for both user & group */
+                qctl.qc_type = 0x02;
 
         if (argc == optind)
                 return CMD_HELP;
