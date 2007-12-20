b8c9384b7b8d604996144c886de09323172ef90b
fanyong <fanyong>
2007-12-20 01:48:24 +0000

    Branch HEAD b=13709 i=chen zheng
    enable conf-sanity test_22.

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -16,8 +16,8 @@ MOUNTCONFSKIP="10 11 12 13 13b 14 15"
 # bug number for skipped test: 13739 
 HEAD_EXCEPT="                  32a 32b "
 
-# bug number for skipped test:                                  13709 10510 12743
-ALWAYS_EXCEPT=" $CONF_SANITY_EXCEPT $MOUNTCONFSKIP $HEAD_EXCEPT 22    23    36"
+# bug number for skipped test:                                  10510 12743
+ALWAYS_EXCEPT=" $CONF_SANITY_EXCEPT $MOUNTCONFSKIP $HEAD_EXCEPT 23    36"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 #					             
