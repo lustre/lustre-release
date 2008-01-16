c7d0c3ae63b9543509212f85be4c5a349480c1cd
grev <grev>
2008-01-16 19:34:23 +0000

    b=14620 i=Adilger
    t-f have_modules fn; force log fn to unload modules if it has loaded them

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -120,6 +120,10 @@ case `uname -r` in
     *) EXT=".ko"; USE_QUOTA=yes;;
 esac
 
+have_modules () {
+    lsmod | grep -q lnet
+}
+
 load_module() {
     module=$1
     shift
@@ -138,11 +142,8 @@ load_modules() {
         # use modprobe
     return 0
     fi
-    if [ "$HAVE_MODULES" = true ]; then
-    # we already loaded
-        return 0
-    fi
-    HAVE_MODULES=true
+    # we already loaded ?
+    have_modules && return 0
 
     echo Loading modules from $LUSTRE
     load_module ../lnet/libcfs/libcfs
@@ -233,7 +234,6 @@ unload_modules() {
         wait_for_lnet || return 3
     fi
     fi
-    HAVE_MODULES=false
 
     LEAK_LUSTRE=$(dmesg | tail -n 30 | grep "obd mem.*leaked" || true)
     LEAK_PORTALS=$(dmesg | tail -n 20 | grep "Portals memory leaked" || true)
@@ -1070,7 +1070,9 @@ equals_msg() {
 
 log() {
     echo "$*"
-    lsmod | grep lnet > /dev/null || load_modules
+
+    local HAVE_MODULES=""
+    lsmod | grep lnet > /dev/null || { load_modules && HAVE_MODULES="yes"; }
 
     local MSG="$*"
     # Get rif of '
@@ -1083,6 +1085,8 @@ log() {
     for NODE in $NODES; do
         do_node $NODE $LCTL mark "$MSG" 2> /dev/null || true
     done
+
+    [ -z "$HAVE_MODULES" ] || unload_modules
 }
 
 trace() {
