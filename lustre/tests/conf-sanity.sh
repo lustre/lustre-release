400368f765c0c89f83d6299b90d54d8c54e5ae43
bwzhou <bwzhou>
2008-01-28 05:24:20 +0000

    Branch b1_6 b=14413 r=shadow, johann
    make leak-finder work correctly with LUSTRE and LNET debug messages

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -1528,5 +1528,14 @@ test_38() { # bug 14222
 
 run_test 38 "MDS recreates missing lov_objid file from OST data"
 
+test_39() { #bug 14413
+        PTLDEBUG=malloc
+        setup
+        cleanup
+        perl $SRCDIR/leak_finder.pl $TMP/debug 2>&1 | egrep "*** Leak:" && error
+}
+
+run_test 39 "leak_finder recognizes both LUSTRE and LNET malloc messages"
+
 equals_msg `basename $0`: test complete
 [ -f "$TESTSUITELOG" ] && cat $TESTSUITELOG || true
