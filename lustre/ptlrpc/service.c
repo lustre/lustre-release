04908214a55e6f9d658c5d2bc3b4975b290979f3
robert.read <robert.read>
2008-03-31 18:31:43 +0000

    Branch b1_6
    Change #if to #ifdef to make gcc 3.x happy.
    
    b=15327
    i=oleg
    i=walter

--- lustre/ptlrpc/service.c
+++ lustre/ptlrpc/service.c
@@ -42,7 +42,7 @@ unsigned int at_min = 0;
 CFS_MODULE_PARM(at_min, "i", int, 0644,
                 "Adaptive timeout minimum (sec)");
 
-#if HAVE_AT_SUPPORT
+#ifdef HAVE_AT_SUPPORT
 unsigned int at_max = 600;
 #else
 unsigned int at_max = 0;
