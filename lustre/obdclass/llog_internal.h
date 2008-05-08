d6ca89d0c50b7904538be3cb96653081572c2672
shadow <shadow>
2008-05-08 11:54:07 +0000

    rollback bug 15575, because at any run acc-sm i hit LustreError: 29518:0:(lvfs_linux.c:194:pop_ctxt()) ASSERTION(current->fs->pwd == new_ctx->pwd) after run-tests.

--- lustre/obdclass/llog_internal.h
+++ lustre/obdclass/llog_internal.h
@@ -1,17 +1,6 @@
 #ifndef __LLOG_INTERNAL_H__
 #define __LLOG_INTERNAL_H__
 
-#include <lustre_log.h>
-
-struct llog_process_info {
-        struct llog_handle *lpi_loghandle;
-        llog_cb_t           lpi_cb;
-        void               *lpi_cbdata;
-        void               *lpi_catdata;
-        int                 lpi_rc;
-        struct completion   lpi_completion;
-};
-
 int llog_put_cat_list(struct obd_device *obd, struct obd_device *disk_obd,
                       char *name, int count, struct llog_catid *idarray);
 int llog_cat_id2handle(struct llog_handle *cathandle, struct llog_handle **res,
