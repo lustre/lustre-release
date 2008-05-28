b2f4fd040713bea3ad3d3f2540048c0ee8e07def
brian <brian>
2008-05-28 19:17:16 +0000

    b=15908 i=johann i=zhenyu.xu:
    Small message correction.

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -197,7 +197,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
                 err = lprocfs_register_mountpoint(proc_lustre_fs_root, sb,
                                                   dt, md);
                 if (err < 0)
-                        CERROR("could not register mount in /proc/lustre");
+                        CERROR("could not register mount in /proc/fs/lustre\n");
         }
 
         /* indicate the features supported by this client */
