4efb28221b21c3891de23aa4bf9972b67a03ae5a
adilger <adilger>
2008-05-28 23:39:49 +0000

    Branch HEAD Add updated racer.sh script i=oleg.drokin

---
+++ lustre/tests/racer/file_create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=$1
+MAX=$2
+MAX_MB=256
+
+create() {
+    SIZE=$(($RANDOM*MAX_MB/32))
+    echo "file_create: SIZE=$SIZE"
+    dd if=/dev/zero of=$DIR/$file bs=1k count=$SIZE
+}
+
+while /bin/true ; do 
+    file=$(($RANDOM%$MAX))
+    create 2> /dev/null
+done
+
