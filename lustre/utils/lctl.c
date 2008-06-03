39a7c4ac2df4a3800ea1853af06e03d2c6d4a6fb
komaln <komaln>
2008-06-03 06:11:51 +0000

    b=14471 r=nathan, andreas
    To add -N option in lctl get_param which will print only path names.

--- lustre/utils/lctl.c
+++ lustre/utils/lctl.c
@@ -120,9 +120,10 @@ command_t cmdlist[] = {
         {"local_param", jt_lcfg_param, 0, "set a temporary, local param\n"
          "usage: local_param <target.keyword=val> ...\n"},
         {"get_param", jt_lcfg_getparam, 0, "get the Lustre or LNET parameter\n"
-         "usage: get_param [-n] path/to/param/file \n"
+         "usage: get_param [-n | -N] path/to/param/file \n"
          "Get the value of Lustre or LNET parameter from the specified path\n"
-         "Use '-n' to disable printing of the key name when printing values."},
+         "Use '-n' to disable printing of the key name when printing values.\n"
+         "Use '-N' to print only path names and not the values."},
         {"set_param", jt_lcfg_setparam, 0, "set the Lustre or LNET parameter\n"
          "usage: set_param [-n] path/to/param/file value\n"
          "Set the value of the Lustre or LNET parameter at the specified path\n"
