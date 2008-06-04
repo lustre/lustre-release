08cfd2c83ad5d3282447d6f9295446dc0c3bad7c
cvs2svn <admin@example.com>
2008-06-04 04:18:31 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.71.30.17.2.1.2'.

--- lustre/scripts/maketags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (C) 2001  Cluster File Systems, Inc.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-set -vx
-rm -f TAGS ; find . -name '*.h' -or -name '*.c' | xargs etags
-rm -f ctags; find . -name '*.h' -or -name '*.c' | xargs ctags
