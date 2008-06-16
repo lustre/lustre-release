f0e1e89f816518d7313dfb0f124317f2bcc38b16
yangsheng <yangsheng>
2008-06-16 09:41:49 +0000

    Branch b1_6 b=15592 i=oleg, johann
    Correctly handle the FS_RENAME_DOES_D_MOVE flag in Lustre.

--- lustre/obdclass/obd_mount.c
+++ lustre/obdclass/obd_mount.c
@@ -2014,7 +2014,8 @@ struct file_system_type lustre_fs_type = {
         .name         = "lustre",
         .get_sb       = lustre_get_sb,
         .kill_sb      = lustre_kill_super,
-        .fs_flags     = FS_BINARY_MOUNTDATA | FS_REQUIRES_DEV,
+        .fs_flags     = FS_BINARY_MOUNTDATA | FS_REQUIRES_DEV |
+                        LL_RENAME_DOES_D_MOVE,
 };
 
 #else
