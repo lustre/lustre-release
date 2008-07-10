ae310f0e4196cca5753a334d938ffa2fa288ac08
fanyong <fanyong>
2008-07-10 07:32:02 +0000

    Branch b1_8 b=15927 i=johann i=huanghua
    (1) replace ptlrpcd with the statahead thread to interpret the async statahead RPC callback.
    (2) update from HEAD for dropping unnecessary "mi_exp".

--- lustre/include/lustre_mds.h
+++ lustre/include/lustre_mds.h
@@ -237,7 +237,6 @@ typedef int (* md_enqueue_cb_t)(struct obd_export *exp,
                                 int rc);
 
 struct md_enqueue_info {
-        struct obd_export      *mi_exp;
         struct mdc_op_data      mi_data;
         struct lookup_intent    mi_it;
         struct lustre_handle    mi_lockh;
@@ -245,11 +244,6 @@ struct md_enqueue_info {
         md_enqueue_cb_t         mi_cb;
         unsigned int            mi_generation;
         void                   *mi_cbdata;
-};
-
-struct mdc_enqueue_args {
-        struct md_enqueue_info   *ma_mi;
-        struct ldlm_enqueue_info *ma_ei;
 };
 
 #endif
