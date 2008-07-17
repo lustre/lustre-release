3aca6527eab8013a4f3772c5475ae93b1d10fed1
huanghua <huanghua>
2008-07-17 14:43:29 +0000

    Branch b1_8_gate b=15993
    a=17865
    i=adilger,nikita
    
    a=17883
    i=adilger
    
    - remove common EA (in downgraded mode) if it exists, and its test case.

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -154,6 +154,7 @@ extern unsigned int obd_alloc_fail_rate;
 #define OBD_FAIL_MDS_CLOSE_NET_REP       0x13b
 #define OBD_FAIL_MDS_BLOCK_QUOTA_REQ     0x13c
 #define OBD_FAIL_MDS_DROP_QUOTA_REQ      0x13d
+#define OBD_FAIL_MDS_REMOVE_COMMON_EA    0x13e
 
 #define OBD_FAIL_OST                     0x200
 #define OBD_FAIL_OST_CONNECT_NET         0x201
