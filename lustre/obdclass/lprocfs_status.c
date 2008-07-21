89458765ec46e178127ff44671c62b8d7855d32e
bobijam <bobijam>
2008-07-21 01:16:02 +0000

    Branch HEAD b=16266 o=komal i=adilger, bobijam
    add missing LPROCFS_ENTRY/EXIT markers in HEAD.

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -58,6 +58,7 @@ struct proc_dir_entry *lprocfs_srch(struct proc_dir_entry *head,
 
         if (head == NULL)
                 return NULL;
+        LPROCFS_ENTRY();
 
         temp = head->subdir;
         while (temp != NULL) {
@@ -66,6 +67,7 @@ struct proc_dir_entry *lprocfs_srch(struct proc_dir_entry *head,
 
                 temp = temp->next;
         }
+        LPROCFS_EXIT();
         return NULL;
 }
 
@@ -309,6 +311,7 @@ void lprocfs_remove(struct proc_dir_entry **rooth)
 
         parent = root->parent;
         LASSERT(parent != NULL);
+        LPROCFS_ENTRY(); /* search vs remove race */
 
         while (1) {
                 while (temp->subdir != NULL)
@@ -332,6 +335,7 @@ void lprocfs_remove(struct proc_dir_entry **rooth)
                 if (temp == parent)
                         break;
         }
+        LPROCFS_EXIT();
 }
 
 void lprocfs_remove_proc_entry(const char *name, struct proc_dir_entry *parent)
