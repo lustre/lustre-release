f7254512a04c462571c37bc3c7fffc83c4447498
kalpak <kalpak>
2008-08-07 09:32:37 +0000

    b=16098
    Add URL for GPLv2 license in copyright headers

--- lnet/libcfs/linux/linux-module.c
+++ lnet/libcfs/linux/linux-module.c
@@ -16,8 +16,8 @@
  * in the LICENSE file that accompanied this code).
  *
  * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see [sun.com URL with a
- * copy of GPLv2].
+ * version 2 along with this program; If not, see
+ * http://www.sun.com/software/products/lustre/docs/GPLv2.pdf
  *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
@@ -183,5 +183,3 @@ cfs_psdev_t libcfs_dev = {
 	"lnet",
 	&libcfs_fops
 };
-
-
