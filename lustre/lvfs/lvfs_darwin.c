37fd65ed7ca4fd0bd18bd8aebd9ff136ba6fc986
kalpak <kalpak>
2008-08-07 09:37:50 +0000

    b=16098
    Add URL for GPLv2 license in copyright headers

--- lustre/lvfs/lvfs_darwin.c
+++ lustre/lvfs/lvfs_darwin.c
@@ -16,8 +16,8 @@
  * in the LICENSE file that accompanied this code).
  *
  * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see [sun.com URL with a
- * copy of GPLv2].
+ * version 2 along with this program; If not, see
+ * http://www.sun.com/software/products/lustre/docs/GPLv2.pdf
  *
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
@@ -78,4 +78,3 @@ static void __exit lvfs_exit(void)
 }
 
 cfs_module(lvfs, "1.0.0", lvfs_init, lvfs_exit);
-
