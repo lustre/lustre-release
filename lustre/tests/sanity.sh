5f6986cbb9b6915e0e41ecb03f226e457fd50c91
kalpak <kalpak>
2008-08-09 05:52:29 +0000

    b=10555 i=kalpak (patch by rread)
    fix the lines while skip fiemap tests

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -5152,7 +5152,7 @@ run_test 129 "test directory size limit ========================"
 
 test_130a() {
 	filefrag_op=$(filefrag -e 2>&1 | grep "invalid option")
-	[ -n "$filefrag_op" ] && skip '"filefrag does not support FIEMAP" && return'
+	[ -n "$filefrag_op" ] && skip "filefrag does not support FIEMAP" && return
 
 	local fm_file=$DIR/$tfile
 	lfs setstripe -s 65536 -c 1 $fm_file || error "setstripe failed on $fm_file"
@@ -5189,7 +5189,7 @@ test_130b() {
 	[ "$OSTCOUNT" -lt "2" ] && skip "skipping FIEMAP on 2-stripe file test" && return
 
 	filefrag_op=$(filefrag -e 2>&1 | grep "invalid option")
-	[ -n "$filefrag_op" ] && skip '"filefrag does not support FIEMAP" && return'
+	[ -n "$filefrag_op" ] && skip "filefrag does not support FIEMAP" && return
 
 	local fm_file=$DIR/$tfile
 	lfs setstripe -s 65536 -c 2 $fm_file || error "setstripe failed on $fm_file"
@@ -5232,7 +5232,7 @@ test_130c() {
 	[ "$OSTCOUNT" -lt "2" ] && skip "skipping FIEMAP on 2-stripe file with hole test" && return
 
 	filefrag_op=$(filefrag -e 2>&1 | grep "invalid option")
-	[ -n "$filefrag_op" ] && skip '"filefrag does not support FIEMAP" && return'
+	[ -n "$filefrag_op" ] && skip "filefrag does not support FIEMAP" && return
 
 	local fm_file=$DIR/$tfile
 	lfs setstripe -s 65536 -c 2 $fm_file || error "setstripe failed on $fm_file"
@@ -5280,7 +5280,7 @@ test_130d() {
 	[ "$OSTCOUNT" -lt "3" ] && skip "skipping FIEMAP on N-stripe file test" && return
 
 	filefrag_op=$(filefrag -e 2>&1 | grep "invalid option")
-	[ -n "$filefrag_op" ] && skip '"filefrag does not support FIEMAP" && return'
+	[ -n "$filefrag_op" ] && skip "filefrag does not support FIEMAP" && return
 
 	local fm_file=$DIR/$tfile
 	lfs setstripe -s 65536 -c $OSTCOUNT $fm_file || error "setstripe failed on $fm_file"
@@ -5323,7 +5323,7 @@ test_130e() {
 	[ "$OSTCOUNT" -lt "2" ] && skip "skipping continuation FIEMAP test" && return
 
 	filefrag_op=$(filefrag -e 2>&1 | grep "invalid option")
-	[ -n "$filefrag_op" ] && skip '"filefrag does not support FIEMAP" && return'
+	[ -n "$filefrag_op" ] && skip "filefrag does not support FIEMAP" && return
 
 	local fm_file=$DIR/$tfile
 	lfs setstripe -s 65536 -c 2 $fm_file || error "setstripe failed on $fm_file"
