f384b022ed047e4ed4ed12f96f67268253fede5b
bobijam <bobijam>
2008-08-15 16:51:54 +0000

    Branch b1_8_gate b=16495 o=green i=adilger, shadow
    Assert on freeing replayable request.

--- lustre/ptlrpc/client.c
+++ lustre/ptlrpc/client.c
@@ -1480,6 +1480,7 @@ static void __ptlrpc_free_req(struct ptlrpc_request *request, int locked)
         LASSERTF(request->rq_rqbd == NULL, "req %p\n",request);/* client-side */
         LASSERTF(list_empty(&request->rq_list), "req %p\n", request);
         LASSERTF(list_empty(&request->rq_set_chain), "req %p\n", request);
+        LASSERTF(!request->rq_replay, "req %p\n", request);
 
         /* We must take it off the imp_replay_list first.  Otherwise, we'll set
          * request->rq_reqmsg to NULL while osc_close is dereferencing it. */
