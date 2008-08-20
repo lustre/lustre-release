c98a7d9fa94520280d2bbd1e7287ae808b43a5a9
adilger <adilger>
2008-08-20 09:02:14 +0000

    Branch b1_8 Revert patch landed directly to b1_8 but not b1_8_gate. b=15812

--- lustre/ldlm/ldlm_lock.c
+++ lustre/ldlm/ldlm_lock.c
@@ -193,7 +193,6 @@ void ldlm_lock_add_to_lru_nolock(struct ldlm_lock *lock)
         struct ldlm_namespace *ns = lock->l_resource->lr_namespace;
         lock->l_last_used = cfs_time_current();
         LASSERT(list_empty(&lock->l_lru));
-        LASSERT(lock->l_resource->lr_type != LDLM_FLOCK);
         list_add_tail(&lock->l_lru, &ns->ns_unused_list);
         LASSERT(ns->ns_nr_unused >= 0);
         ns->ns_nr_unused++;
