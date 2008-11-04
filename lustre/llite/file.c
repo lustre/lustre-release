c3ed50209cac38632cd68135518b92a9c1751529
tianzy <tianzy>
2008-11-04 07:28:40 +0000

    Branch b1_8 Replace LBUG with RETURN(-EINVAL) to avoid crashing b=5135 i=adilger i=johann

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -2924,7 +2924,7 @@ int ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
                 break;
         default:
                 CERROR("unknown fcntl lock type: %d\n", file_lock->fl_type);
-                LBUG();
+                RETURN (-EINVAL);
         }
 
         switch (cmd) {
@@ -2951,7 +2951,7 @@ int ll_file_flock(struct file *file, int cmd, struct file_lock *file_lock)
                 break;
         default:
                 CERROR("unknown fcntl lock command: %d\n", cmd);
-                LBUG();
+                RETURN (-EINVAL);
         }
 
         CDEBUG(D_DLMTRACE, "inode=%lu, pid=%u, flags=%#x, mode=%u, "
