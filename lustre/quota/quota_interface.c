fa939887cf85fd9aaed726ea60cb74e3f3d88018
yangsheng <yangsheng>
2008-11-11 07:44:30 +0000

    Branch b1_8_gate b=17153 i=adilger, robert.read
    Remove 2.4 check.
    Author: tappro
    CVSooi=<inspected_by>

--- lustre/quota/quota_interface.c
+++ lustre/quota/quota_interface.c
@@ -45,14 +45,10 @@
 # include <linux/init.h>
 # include <linux/fs.h>
 # include <linux/jbd.h>
-# if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0))
 #  include <linux/smp_lock.h>
 #  include <linux/buffer_head.h>
 #  include <linux/workqueue.h>
 #  include <linux/mount.h>
-# else
-#  include <linux/locks.h>
-# endif
 #else /* __KERNEL__ */
 # include <liblustre.h>
 #endif
