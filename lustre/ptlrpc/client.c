da755b5de047bbc045c97ff50bc325529ff8261d
yury <yury>
2008-11-12 19:16:03 +0000

    b=17310   r=shadow,vitaly   - correct check for phase in ptlrpc_expired_set() and couple of other places.

--- lustre/ptlrpc/client.c
+++ lustre/ptlrpc/client.c
@@ -1408,8 +1408,7 @@ int ptlrpc_expired_set(void *data)
                         list_entry(tmp, struct ptlrpc_request, rq_set_chain);
 
                 /* Request in-flight? */
-                if (!((req->rq_phase & 
-                       (RQ_PHASE_RPC | RQ_PHASE_UNREGISTERING) &&
+                if (!((req->rq_phase == RQ_PHASE_RPC &&
                        !req->rq_waiting && !req->rq_resend) ||
                       (req->rq_phase == RQ_PHASE_BULK)))
                         continue;
@@ -1472,19 +1471,10 @@ int ptlrpc_set_next_timeout(struct ptlrpc_request_set *set)
                 req = list_entry(tmp, struct ptlrpc_request, rq_set_chain);
 
                 /* request in-flight? */
-                if (!(((req->rq_phase & 
-                        (RQ_PHASE_RPC | RQ_PHASE_UNREGISTERING)) && 
-                      !req->rq_waiting) ||
+                if (!(((req->rq_phase == RQ_PHASE_RPC) && !req->rq_waiting) ||
                       (req->rq_phase == RQ_PHASE_BULK) ||
                       (req->rq_phase == RQ_PHASE_NEW)))
                         continue;
-
-                /* Check those waiting for long reply unlink every one 
-                 * second. */
-                if (req->rq_phase == RQ_PHASE_UNREGISTERING) {
-                        timeout = 1;
-                        break;
-                }
 
                 /* Already timed out. */
                 if (req->rq_timedout)
