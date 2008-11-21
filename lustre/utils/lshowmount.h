241651b49b4b455c2122b31e766edadb09f64c04
yangsheng <yangsheng>
2008-11-21 15:33:30 +0000

    Brancb b1_8_gate b=16208
    i=adilger, johann
    
    Add utility for showing mounted hosts
    Author: Herb Wartens(LLNL)

---
+++ lustre/utils/lshowmount.h
@@ -0,0 +1,17 @@
+#ifndef __LSHOWMOUNT_H
+#define __LSHOWMOUNT_H
+
+#define PROC_DIR_MGS          "/proc/fs/lustre/mgs"
+#define PROC_DIR_MDS          "/proc/fs/lustre/mds"
+#define PROC_DIR_OST          "/proc/fs/lustre/obdfilter"
+#define PROC_EXPORTS          "exports"
+#define PROC_NID              "nid"
+#define NID_MAX               1024
+#define LNET_NETWORK_TYPE_MAX 32
+#define NETWORK_MAX           128
+
+#endif
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ lustre/utils/thread.c
@@ -0,0 +1,50 @@
+/*****************************************************************************
+ *  $Id: thread.c,v 1.1.4.2 2008/11/21 15:33:30 yangsheng Exp $
+ *****************************************************************************
+ *  Copyright (C) 2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Chris Dunlap <cdunlap@llnl.gov>.
+ *
+ *  This file is from LSD-Tools, the LLNL Software Development Toolbox.
+ *
+ *  LSD-Tools is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  LSD-Tools is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with LSD-Tools; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *****************************************************************************/
+
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+#include "thread.h"
+
+
+#if WITH_PTHREADS
+#ifndef NDEBUG
+int
+lsd_mutex_is_locked (pthread_mutex_t *mutex)
+{
+/*  Returns true if the mutex is locked; o/w, returns false.
+ */
+    int rc;
+
+    assert (mutex != NULL);
+    rc = pthread_mutex_trylock (mutex);
+    return (rc == EBUSY ? 1 : 0);
+}
+#endif /* !NDEBUG */
+#endif /* WITH_PTHREADS */
