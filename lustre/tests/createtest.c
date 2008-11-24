2b76ef11a2aac1e6bb1d0824fae859d22b8a17c6
adilger <adilger>
2008-11-24 21:30:35 +0000

    Branch HEAD Fix filename formatting to include space for sticky bit.

--- lustre/tests/createtest.c
+++ lustre/tests/createtest.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
 		int mode = i | 0644;
 		int rc;
 
-		sprintf(name, "%s-mknod%06o", argv[1], mode);
+		sprintf(name, "%s-mknod%07o", argv[1], mode);
 		rc = mknod(name, mode, 0x1234);
 		switch (i) {
 		case 0:
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
 		int rc;
 
 		mode = i | 0644;
-		sprintf(name, "%s-creat%06o", argv[1], mode);
+		sprintf(name, "%s-creat%07o", argv[1], mode);
 		fd = open(name, O_CREAT|O_RDONLY, mode);
 		if (fd < 0) {
 			fprintf(stderr, "%s: ERROR creat %s: %s\n",
