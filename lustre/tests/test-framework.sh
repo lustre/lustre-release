f16855c5895da04d005151f16f00fa046164717a
grev <grev>
2008-12-03 17:35:49 +0000

    b=16551 i=Adilger conf-sanity test_32* fix

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -681,6 +681,7 @@ wait_exit_ST () {
 
     local WAIT=0
     local INTERVAL=1
+    local running
     # conf-sanity 31 takes a long time cleanup
     while [ $WAIT -lt 300 ]; do
         running=$(do_facet ${facet} "lsmod | grep lnet > /dev/null && lctl dl | grep ' ST '") || true
@@ -891,6 +892,8 @@ declare -fx h2o2ib
 
 facet_host() {
     local facet=$1
+
+    [ "$facet" == client ] && echo -n $HOSTNAME && return
     varname=${facet}_HOST
     if [ -z "${!varname}" ]; then
         if [ "${facet:0:3}" == "ost" ]; then
