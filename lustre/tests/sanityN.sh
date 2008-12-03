4a2d30371431f50477f4cd985e51b92c2c50b84d
vitaly <vitaly>
2008-12-03 17:47:35 +0000

    Branch HEAD b=17748 i=grev i=adilger
    the sanityN test issue is fixed

--- lustre/tests/sanityN.sh
+++ lustre/tests/sanityN.sh
@@ -780,12 +780,12 @@ run_test 33a "commit on sharing, cross crete/delete, 2 clients, benchmark"
 # End commit on sharing tests
 
 test_34() { #16129
+        local OPER
+        local lock_in
+        local lock_out
         for OPER in notimeout timeout ; do
                 rm $DIR1/$tfile 2>/dev/null
-                lock_in=0;
-                for f in `lctl get_param -n ldlm/namespaces/*/lock_timeouts`; do
-                        lock_in=$(($lock_in + $f))
-                done
+                lock_in=$(do_nodes $(osts_nodes) "lctl get_param -n ldlm.namespaces.filter-*.lock_timeouts" | calc_sum)
                 if [ $OPER == "timeout" ] ; then
                         for j in `seq $OSTCOUNT`; do
                                 #define OBD_FAIL_PTLRPC_HPREQ_TIMEOUT    0x511
@@ -808,10 +808,7 @@ test_34() { #16129
                 dd of=/dev/null if=$DIR2/$tfile > /dev/null 2>&1
                 # wait for a lock timeout
                 sleep 4
-                lock_out=0
-                for f in `lctl get_param -n ldlm/namespaces/*/lock_timeouts`; do
-                        lock_out=$(($lock_out + $f))
-                done
+                lock_out=$(do_nodes $(osts_nodes) "lctl get_param -n ldlm.namespaces.filter-*.lock_timeouts" | calc_sum)
                 if [ $OPER == "timeout" ] ; then 
                         if [ $lock_in == $lock_out ]; then
                                 error "no lock timeout happened"
