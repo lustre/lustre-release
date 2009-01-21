43ece30252ec49df6634f727d175d78510a4984e
girish <girish>
2009-01-21 18:39:04 +0000

    b=16895 i=adilger (o=johann) i=girish
    don't LASSERT on read-only device

--- lustre/obdfilter/filter.c
+++ lustre/obdfilter/filter.c
@@ -1763,7 +1763,11 @@ int filter_common_setup(struct obd_device *obd, obd_count len, void *buf,
         if (rc != 0)
                 GOTO(err_ops, rc);
 
-        LASSERT(!lvfs_check_rdonly(lvfs_sbdev(mnt->mnt_sb)));
+        if (lvfs_check_rdonly(lvfs_sbdev(mnt->mnt_sb))) {
+                CERROR("%s: Underlying device is marked as read-only. "
+                       "Setup failed\n", obd->obd_name);
+                GOTO(err_ops, rc = -EROFS);
+        }
 
         /* failover is the default */
         obd->obd_replayable = 1;
