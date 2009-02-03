774caffea3375cf40cb003d0f095b255fb766df3
grev <grev>
2009-02-03 13:34:16 +0000

    b=17899 i=Johann test_53* fix: rm dirs used at start

--- lustre/tests/replay-single.sh
+++ lustre/tests/replay-single.sh
@@ -1034,6 +1034,8 @@ test_53a() {
 run_test 53a "|X| close request while two MDC requests in flight"
 
 test_53b() {
+    rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
     mkdir -p $DIR/$tdir-1
     mkdir -p $DIR/$tdir-2
     multiop $DIR/$tdir-1/f O_c &
@@ -1063,6 +1065,8 @@ test_53b() {
 run_test 53b "|X| open request while two MDC requests in flight"
 
 test_53c() {
+    rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
     mkdir -p $DIR/${tdir}-1
     mkdir -p $DIR/${tdir}-2
     multiop $DIR/${tdir}-1/f O_c &
@@ -1094,6 +1098,8 @@ test_53c() {
 run_test 53c "|X| open request and close request while two MDC requests in flight"
 
 test_53d() {
+    rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
     mkdir -p $DIR/${tdir}-1
     mkdir -p $DIR/${tdir}-2
     multiop $DIR/${tdir}-1/f O_c &
@@ -1121,6 +1127,8 @@ test_53d() {
 run_test 53d "|X| close reply while two MDC requests in flight"
 
 test_53e() {
+    rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
     mkdir -p $DIR/$tdir-1
     mkdir -p $DIR/$tdir-2
     multiop $DIR/$tdir-1/f O_c &
@@ -1150,6 +1158,8 @@ test_53e() {
 run_test 53e "|X| open reply while two MDC requests in flight"
 
 test_53f() {
+        rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
         mkdir -p $DIR/${tdir}-1
         mkdir -p $DIR/${tdir}-2
         multiop $DIR/${tdir}-1/f O_c &
@@ -1181,6 +1191,8 @@ test_53f() {
 run_test 53f "|X| open reply and close reply while two MDC requests in flight"
 
 test_53g() {
+        rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
         mkdir -p $DIR/${tdir}-1
         mkdir -p $DIR/${tdir}-2
         multiop $DIR/${tdir}-1/f O_c &
@@ -1212,6 +1224,8 @@ test_53g() {
 run_test 53g "|X| drop open reply and close request while close and open are both in flight"
 
 test_53h() {
+    rm -rf $DIR/${tdir}-1 $DIR/${tdir}-2
+
     mkdir -p $DIR/${tdir}-1
     mkdir -p $DIR/${tdir}-2
     multiop $DIR/${tdir}-1/f O_c &
