c00464ae851915c45ad9e24ab50f6cbe0d40747f
pravins <pravins>
2009-02-08 17:24:31 +0000

    b=15134 i=adilger i=h.huang 	interop patch for lu_dirent attribute handling.

--- lnet/include/libcfs/linux/linux-fs.h
+++ lnet/include/libcfs/linux/linux-fs.h
@@ -94,5 +94,6 @@ typedef struct file_lock cfs_flock_t;
 ssize_t cfs_user_write (cfs_file_t *filp, const char *buf, size_t count, loff_t *offset);
 
 #endif
+#define CFS_IFTODT(type)   (((type) & 0170000) >> 12)
 
 #endif
