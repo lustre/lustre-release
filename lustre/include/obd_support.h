f77b8242413f1a2afa7ab816e7087e97efe75567
johann <johann>
2009-02-09 14:59:19 +0000

    Branch b_release_1_8_0 i=johann (patch from Tappro) i=shadow
    Return only valid cookies, empty slot for failed ones.
    This fixes a memory corruption causing random oops.

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -177,6 +177,7 @@ extern unsigned int obd_alloc_fail_rate;
 #define OBD_FAIL_MDS_DROP_QUOTA_REQ      0x13d
 #define OBD_FAIL_MDS_REMOVE_COMMON_EA    0x13e
 #define OBD_FAIL_MDS_ALLOW_COMMON_EA_SETTING   0x13f
+#define OBD_FAIL_MDS_FAIL_LOV_LOG_ADD    0x140
 
 #define OBD_FAIL_OST                     0x200
 #define OBD_FAIL_OST_CONNECT_NET         0x201
