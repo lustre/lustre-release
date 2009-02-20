fda66cfccb4c29c647fa57d6c67b77627f63b352
grev <grev>
2009-02-20 21:18:22 +0000

    b=18443 i=Adilger remove obsolete tests scripts

--- lustre/tests/set_dates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-[ `hostname` = "uml1" ] && date --set="Mar 14 20:21:41 MDT 1973" #101010101
-[ `hostname` = "uml2" ] && date --set="May 26 22:43:22 MDT 1976" #202020202
-[ `hostname` = "uml3" ] && date --set="Aug  9 01:05:03 MDT 1979" #303030303
