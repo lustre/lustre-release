839ee2dab40cfcdd09a5301801168dc135ef0816
grev <grev>
2009-02-25 16:57:23 +0000

    b=18468 o=Adilger i=Oleg.Drokin i=grev make racer harder

--- lustre/tests/racer/file_rename.sh
+++ lustre/tests/racer/file_rename.sh
@@ -4,7 +4,7 @@ DIR=$1
 MAX=$2
 
 while /bin/true ; do 
-    file=$(($RANDOM%$MAX))
-    new_file=$((($file + 1)%$MAX))
+    file=$((RANDOM % MAX))
+    new_file=$((RANDOM % MAX))
     mv $DIR/$file $DIR/$new_file 2> /dev/null
 done
--- lustre/tests/racer/file_rm.sh
+++ lustre/tests/racer/file_rm.sh
@@ -4,7 +4,7 @@ DIR=$1
 MAX=$2
 
 while /bin/true ; do 
-    file=$(($RANDOM%$MAX))
+    file=$((RANDOM % MAX))
     rm -rf $DIR/$file 2> /dev/null
     sleep 1
 done
