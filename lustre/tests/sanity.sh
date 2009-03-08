9b96e4fe7a04e53464393cc5cdb69d150cf7000f
girish <girish>
2009-03-08 08:14:07 +0000

    b=17846 i=adilger i=h.huang
    changes to regression test after landing 17846 Lustre patch. make sure all files are not extracted on OST id 0

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -4059,6 +4059,8 @@ test_102c() {
 run_test 102c "non-root getfattr/setfattr for lustre.lov EAs ==========="
 
 compare_stripe_info1() {
+	local stripe_index_all_zero=1
+
 	for num in 1 2 3 4
 	do
  		for count in `seq 1 $STRIPE_COUNT`
@@ -4075,11 +4077,13 @@ compare_stripe_info1() {
 					error "$file: different stripe count $stripe_count, expected $count" && return
 				fi
 				if [ $stripe_index -ne 0 ]; then
- 					error "$file: different stripe offset $stripe_index, expected 0" && return
+				       stripe_index_all_zero=0
 				fi
 			done
 		done
 	done
+	[ $stripe_index_all_zero -eq 1 ] && error "all files are being extracted starting from OST index 0"
+	return 0
 }
 
 compare_stripe_info2() {
