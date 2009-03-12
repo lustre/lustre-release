7b706473020364634f7e105dbbd594d0d88cc8d6
grev <grev>
2009-03-12 16:58:14 +0000

    b=18761 i=Oleg.Drokin declare array fix

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -549,7 +549,7 @@ start_client_load() {
 }
 
 start_client_loads () {
-    local clients=(${1//,/ })
+    local -a clients=(${1//,/ })
     local numloads=${#CLIENT_LOADS[@]}
     local testnum
 
@@ -1954,7 +1954,7 @@ get_random_entry () {
 
     rnodes=${rnodes//,/ }
 
-    local nodes=($rnodes)
+    local -a nodes=($rnodes)
     local num=${#nodes[@]} 
     local i=$((RANDOM * num * 2 / 65536))
 
