8b9bcb0cfe1829808b0733da8b2f131d316a5f4e
bobijam <bobijam>
2009-04-24 02:18:16 +0000

    Branch b1_8 b=18683 i=joahnn i=sheng.yang
    If no options are given, tunefs.lustre/mkfs.lustre just print info.

--- lustre/utils/mkfs_lustre.c
+++ lustre/utils/mkfs_lustre.c
@@ -1094,7 +1094,7 @@ static char *convert_hostnames(char *s1)
                 sep = *s2;
                 *s2 = '\0';
                 nid = libcfs_str2nid(s1);
-                
+
                 if (nid == LNET_NID_ANY) {
                         fprintf(stderr, "%s: Can't parse NID '%s'\n", progname, s1);
                         free(converted);
@@ -1109,7 +1109,7 @@ static char *convert_hostnames(char *s1)
                         free(converted);
                         return NULL;
                 }
-                                        
+
                 c += snprintf(c, left, "%s%c", libcfs_nid2str(nid), sep);
                 left = converted + MAXNIDSTR - c;
                 s1 = s2 + 1;
@@ -1337,6 +1337,10 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
                 fprintf(stderr, "Bad argument: %s\n", argv[optind]);
                 return EINVAL;
         }
+
+        /* single argument: <device> */
+        if (argc == 2)
+                ++print_only;
 
 #ifndef TUNEFS
         if (mop->mo_ldd.ldd_flags & LDD_F_SV_TYPE_MDT && 0 == upcall) {
