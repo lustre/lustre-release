20e0f133659e3bde63951695b8abf022d3c33e51
robert.read <robert.read>
2009-06-09 22:34:54 +0000

    Branch HEAD b=19577
    Change error_exit calls to error so the rest of the tests can run.

--- lustre/tests/sanity-quota.sh
+++ lustre/tests/sanity-quota.sh
@@ -301,7 +301,7 @@ test_1_sub() {
         etime=`date +%s`
         delta=$((etime - stime))
         rate=$((BLK_SZ * LIMIT / 2 / delta / 1024))
-        [ $rate -gt 1024 ] || error_exit "SLOW IO for $TSTUSR (user): $rate KB/sec"
+        [ $rate -gt 1024 ] || error "SLOW IO for $TSTUSR (user): $rate KB/sec"
         log "    Done"
         log "    Write out of block quota ..."
 	# this time maybe cache write,  ignore it's failure
@@ -337,7 +337,7 @@ test_1_sub() {
         etime=`date +%s`
         delta=$((etime - stime))
         rate=$((BLK_SZ * LIMIT / 2 / delta / 1024))
-        [ $rate -gt 1024 ] || error_exit "SLOW IO for $TSTUSR (group): $rate KB/sec"
+        [ $rate -gt 1024 ] || error "SLOW IO for $TSTUSR (group): $rate KB/sec"
         log "    Done"
         log "    Write out of block quota ..."
 	# this time maybe cache write, ignore it's failure
