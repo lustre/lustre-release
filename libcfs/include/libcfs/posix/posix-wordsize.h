b18bbb8903b3bd09c274850a6d319e989bbe1fdd
pravins <pravins>
2009-08-26 09:41:27 +0000

    b=20339
    reverting patch as Andreas suggested this can break child branch from HEAD which are
    not in sync with lustre-core/lustre.
    once most of child branch are updated and there is no user of LPSZ we can land it.

--- libcfs/include/libcfs/posix/posix-wordsize.h
+++ libcfs/include/libcfs/posix/posix-wordsize.h
@@ -131,6 +131,18 @@ typedef struct {
 # define LPF64 "l"
 #endif
 
+#ifdef HAVE_SIZE_T_LONG
+# define LPSZ  "%lu"
+#else
+# define LPSZ  "%u"
+#endif
+
+#ifdef HAVE_SSIZE_T_LONG
+# define LPSSZ "%ld"
+#else
+# define LPSSZ "%d"
+#endif
+
 #ifndef LPU64
 # error "No word size defined"
 #endif
