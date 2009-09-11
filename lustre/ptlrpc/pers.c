f2e32d0fec82dffdd5e8e7974cee3da0e8c3105a
bobijam <bobijam>
2009-09-11 01:31:07 +0000

    Branch b_release_1_8_1 b=20560 i=johann i=alexander.zarochentsev
    Description: File checksum failures with OST read cache on
    Details    : Clear page cache's uptodate bit when its contents are cleared.

--- lustre/ptlrpc/pers.c
+++ lustre/ptlrpc/pers.c
@@ -76,9 +76,10 @@ void ptlrpc_add_bulk_page(struct ptlrpc_bulk_desc *desc, cfs_page_t *page,
 void ptl_rpc_wipe_bulk_pages(struct ptlrpc_bulk_desc *desc)
 {
         int i;
-        
+
         for (i = 0; i < desc->bd_iov_count ; i++) {
                 lnet_kiov_t *kiov = &desc->bd_iov[i];
+                ClearPageUptodate(kiov->kiov_page);
                 memset(cfs_kmap(kiov->kiov_page)+kiov->kiov_offset, 0xab,
                        kiov->kiov_len);
                 cfs_kunmap(kiov->kiov_page);
@@ -95,7 +96,7 @@ void ptlrpc_fill_bulk_md(lnet_md_t *md, struct ptlrpc_bulk_desc *desc)
                 md->length = desc->bd_iov[0].iov_len;
                 return;
         }
-        
+
         md->options |= LNET_MD_IOVEC;
         md->start = &desc->bd_iov[0];
         md->length = desc->bd_iov_count;
@@ -103,7 +104,7 @@ void ptlrpc_fill_bulk_md(lnet_md_t *md, struct ptlrpc_bulk_desc *desc)
 
 static int can_merge_iovs(lnet_md_iovec_t *existing, lnet_md_iovec_t *candidate)
 {
-        if (existing->iov_base + existing->iov_len == candidate->iov_base) 
+        if (existing->iov_base + existing->iov_len == candidate->iov_base)
                 return 1;
 #if 0
         /* Enable this section to provide earlier evidence of fragmented bulk */
@@ -114,7 +115,7 @@ static int can_merge_iovs(lnet_md_iovec_t *existing, lnet_md_iovec_t *candidate)
         return 0;
 }
 
-void ptlrpc_add_bulk_page(struct ptlrpc_bulk_desc *desc, cfs_page_t *page, 
+void ptlrpc_add_bulk_page(struct ptlrpc_bulk_desc *desc, cfs_page_t *page,
                           int pageoffset, int len)
 {
         lnet_md_iovec_t *iov = &desc->bd_iov[desc->bd_iov_count];
