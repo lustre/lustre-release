8ef31d8cecdd96db66d545fbbbb6474de586eb96
yangsheng <yangsheng>
2009-12-03 06:20:14 +0000

    Branch HEAD b=21375
    i=robert, johann
    
    Cleanup code.

--- lustre/obdclass/obd_mount.c
+++ lustre/obdclass/obd_mount.c
@@ -2124,7 +2124,7 @@ struct file_system_type lustre_fs_type = {
         .get_sb       = lustre_get_sb,
         .kill_sb      = lustre_kill_super,
         .fs_flags     = FS_BINARY_MOUNTDATA | FS_REQUIRES_DEV |
-#ifdef HAVE_LINUX_FIEMAP_H
+#ifdef FS_HAS_FIEMAP
                         FS_HAS_FIEMAP |
 #endif
                         LL_RENAME_DOES_D_MOVE,
