3185bb951c8aa008282d5b2bdad4ea73b392e51f
Andrew Perepechko <andrew.perepechko@sun.com>
2009-12-18 16:59:12 +0300

    b=21489 mtime could be spoiled by a write callback
    i=Johann Lombardi
    i=Vitaly Fertman
    
    write callback with some stale mtime could "undo" a later setattr

--- lustre/ost/ost_handler.c
+++ lustre/ost/ost_handler.c
@@ -1121,6 +1121,13 @@ static int ost_brw_write(struct ptlrpc_request *req, struct obd_trans_info *oti)
                  * has timed out the request already */
                 no_reply = 1;
 
+        /*
+         * Disable sending mtime back to the client. If the client locked the
+         * whole object, then it has already updated the mtime on its side,
+         * otherwise it will have to glimpse anyway (see bug 21489, comment 32)
+         */
+        repbody->oa.o_valid &= ~(OBD_MD_FLMTIME | OBD_MD_FLATIME);
+
         if (unlikely(client_cksum != server_cksum && rc == 0)) {
                 int  new_cksum = ost_checksum_bulk(desc, OST_WRITE, cksum_type);
                 char *msg;
