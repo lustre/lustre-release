433133a5f7be74c404c7b40d038b2f0b161ffab3
Andrew Perepechko <Andrew.Perepechko@Sun.COM>
2010-01-11 17:45:48 +0300

    b=21670 use the correct fmt specifier for *printf in check_cur_qunit
    i=Dmitry Zogin
    i=Sheng Yang

--- lustre/quota/quota_context.c
+++ lustre/quota/quota_context.c
@@ -349,7 +349,7 @@ check_cur_qunit(struct obd_device *obd,
                 quota_compute_lqs(qdata, lqs, 1, (ret == 1) ? 1 : 0);
 
         CDEBUG(D_QUOTA, "type: %c, limit: "LPU64", usage: "LPU64
-               ", pending_write: "LPU64", record: "LPD64
+               ", pending_write: "LPU64", record: %lld"
                ", qunit_sz: %lu, tune_sz: %lu, ret: %d.\n",
                QDATA_IS_BLK(qdata) ? 'b' : 'i', limit, usage, pending_write,
                record, qunit_sz, tune_sz, ret);
