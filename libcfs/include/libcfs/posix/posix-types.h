2606b783041ef0d126f2a7377378e434d2df5436
yangsheng <Sheng.Yang@Sun.COM>
2010-01-20 10:41:39 -0800

    b=18790 Add config check to avoid type conflict.
    i=panda
    i=yangsheng

--- libcfs/include/libcfs/posix/posix-types.h
+++ libcfs/include/libcfs/posix/posix-types.h
@@ -54,14 +54,26 @@ typedef umode_t cfs_umode_t;
  * header files exported to user space
  */
 
+#ifndef HAVE___S8
 typedef __signed__ char __s8;
+#endif
+#ifndef HAVE___U8
 typedef unsigned char __u8;
+#endif
 
+#ifndef HAVE___S16
 typedef __signed__ short __s16;
+#endif
+#ifndef HAVE___U16
 typedef unsigned short __u16;
+#endif
 
+#ifndef HAVE___S32
 typedef __signed__ int __s32;
+#endif
+#ifndef HAVE___U32
 typedef unsigned int __u32;
+#endif
 
 #ifndef HAVE___S64
 typedef __signed__ long long __s64;
