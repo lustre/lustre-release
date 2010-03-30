fc2e06325690234c31ccb9a5bfc570bd17641a6f
Elena Gryaznova <grev@sun.com>
2010-03-30 11:59:02 -0700

    b=21927 test_61 fix
    i=Mikhail.Pershin
    
    improve awk pattern to get objid correctly

--- lustre/tests/recovery-small.sh
+++ lustre/tests/recovery-small.sh
@@ -1090,12 +1090,12 @@ test_61()
 
 	replay_barrier $SINGLEMDS
 	createmany -o $DIR/$tdir/$tfile-%d 10 
-	local oid=`do_facet ost1 "lctl get_param -n obdfilter.*OST0000.last_id"`
+	local oid=`do_facet ost1 "lctl get_param -n obdfilter.${ost1_svc}.last_id"`
 
 	fail_abort $SINGLEMDS
 	
 	touch $DIR/$tdir/$tfile
-	local id=`$LFS getstripe $DIR/$tdir/$tfile |awk '$2 ~ /^[1-9]+/ {print $2}'`
+	local id=`$LFS getstripe $DIR/$tdir/$tfile |awk '($1 ~ 0 && $2 ~ /^[1-9]+/) {print $2}'`
 	[ $id -le $oid ] && error "the orphan objid was reused, failed"
 
 	# Cleanup
