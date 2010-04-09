b98c2e2a463f4a9a5d548a9f20c9027f067a9709
Alexander.Zarochentsev <Alexander.Zarochentsev@sun.com>
2010-04-08 18:24:01 -0700

    b=22359 fix t_id for the recovery thread
    arbitrary uninialized value in the recovery thread t_id
    was the reason of filter_iobuf_get()) ASSERTION(thread_id <
    filter->fo_iobuf_count) failure.
    
    i=tappro
    i=andrew.perepechko

--- lustre/ldlm/ldlm_lib.c
+++ lustre/ldlm/ldlm_lib.c
@@ -1725,6 +1725,7 @@ static int target_recovery_thread(void *arg)
                 RETURN(rc);
 
         thread->t_env = &env;
+        thread->t_id = -1; /* force filter_iobuf_get/put to use local buffers */
         env.le_ctx.lc_thread = thread;
 
         CERROR("%s: started recovery thread pid %d\n", obd->obd_name,
