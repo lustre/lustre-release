7fe7ae101a1cc742db804544e5b1ded852e05502
Elena Gryaznova <grev@sun.com>
2010-06-24 19:45:15 +0400

    b=23014 sgpdd-survey acc-sm integration
    i=Andreas.Dilger

---
+++ lustre/tests/sgpdd-survey.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#set -x
+set -e
+
+LUSTRE=${LUSTRE:-`dirname $0`/..}
+. $LUSTRE/tests/test-framework.sh
+init_test_env $@
+
+# QE uses the following parameters:
+# size=128 crghi=16 thrhi=32
+crghi=${crghi:-2}
+thrhi=${thrhi:-16} 
+size=${size:-1024}
+
+. ${CONFIG:=$LUSTRE/tests/cfg/$NAME.sh}
+
+[ "$SLOW" = no ] && { crghi=2; thrhi=2; }
+
+# Skip these tests
+ALWAYS_EXCEPT="$SGPDD_SURVEY_EXCEPT"
+
+SGPDDSURVEY=${OBDSURVEY:-$(which sgpdd-survey)}
+
+build_test_filter
+
+init_facets_vars
+
+cleanupall
+
+run_sgpdd_host () {
+    local host=$1
+    local devs=$2
+
+    local params="size=$size crghi=$crghi thrhi=$thrhi"
+    do_rpc_nodes $host run_sgpdd $devs "$params"
+}
+
+test_1 () {
+    local facet=mds
+
+    local host=$(facet_host $facet)
+    local dev=${facet}_dev
+    echo "=== $facet === $host === ${!dev} ==="
+    run_sgpdd_host $host ${!dev}
+}
+run_test 1 "sgpdd-survey, mds, scsidevs"
+
+test_2 () {
+    local facet
+
+    local osts=$(get_facets OST)
+    
+    local ostshosts
+    for facet in ${osts//,/ }; do
+        local host=$(facet_host $facet)
+        local dev=${facet}_dev
+        local var=${host}_devs
+        eval ${var}=$(expand_list ${!var} ${!dev})
+	ostshosts=$(expand_list $ostshosts $host)
+    done
+
+    for host in ${ostshosts//,/ }; do
+	var=${host}_devs
+        echo "=== osts === $host === ${!var} ==="
+        local scsidevs=${!var}
+        run_sgpdd_host $host ${scsidevs}
+    done
+}
+run_test 2 "sgpdd-survey, osts, scsidevs"
+
+equals_msg `basename $0`: test complete, cleaning up
+[ -f "$TESTSUITELOG" ] && cat $TESTSUITELOG || true
--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -129,6 +129,7 @@ init_test_env() {
     fi
     export LST=${LST:-"$LUSTRE/../lnet/utils/lst"}
     [ ! -f "$LST" ] && export LST=$(which lst)
+    export SGPDDSURVEY=${OBDSURVEY:-$(which sgpdd-survey)}
     export MDSRATE=${MDSRATE:-"$LUSTRE/tests/mpi/mdsrate"}
     [ ! -f "$MDSRATE" ] && export MDSRATE=$(which mdsrate 2> /dev/null)
     if ! echo $PATH | grep -q $LUSTRE/tests/racer; then
@@ -3327,3 +3328,18 @@ duplicate_mdt_files() {
     done
     do_umount
 }
+
+run_sgpdd () {
+    local devs=${1//,/ }
+    shift
+    local params=$@
+    local rslt=$TMP/sgpdd_survey
+
+    # sgpdd-survey cleanups ${rslt}.* files
+
+    local cmd="rslt=$rslt $params scsidevs=\"$devs\" $SGPDDSURVEY"
+    echo + $cmd
+    eval $cmd
+    cat ${rslt}.detail
+}
+
