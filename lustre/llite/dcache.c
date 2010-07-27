ec367431d95133af03c9f6ee4f911d0c0706810d
Landen <landen@sun.com>
2010-07-27 15:11:04 +0400

    b=20433 we should recycle dentries and inodes if only cancelling locks existing
    i=green
    i=adilger

--- lustre/llite/dcache.c
+++ lustre/llite/dcache.c
@@ -116,6 +116,8 @@ int ll_dcompare(struct dentry *parent, struct qstr *d_name, struct qstr *name)
 
 static inline int return_if_equal(struct ldlm_lock *lock, void *data)
 {
+        if (lock->l_flags & LDLM_FL_CANCELING)
+                return LDLM_ITER_CONTINUE;
         return LDLM_ITER_STOP;
 }
 
