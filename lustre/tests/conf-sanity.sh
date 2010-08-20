6b812da2abae558472147a1f1a8690429b393af5
Andrew Perepechko <Andrew.Perepechko@sun.com>
2010-08-20 13:15:25 +0200

    b=21174 allow quotacheck over OSTs with sparse indices
    i=Elena Gryznova

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -2249,6 +2249,7 @@ test_56() {
 	start_ost
 	start_ost2 || error "Unable to start second ost"
 	mount_client $MOUNT || error "Unable to mount client"
+	[ -n "$ENABLE_QUOTA" ] && { $LFS quotacheck -ug $MOUNT || error "quotacheck has failed" ;
 
 	stopall
 	reformat
