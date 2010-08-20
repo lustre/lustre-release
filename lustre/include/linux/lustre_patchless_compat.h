7c27d9a67ada42a70b638ca1701dc298142934cc
Mikhail Pershin <tappro@sun.com>
2010-08-20 23:25:26 +0400

    b=22610 a truncate_complete_page fix
    truncate_complete_page implementation for the patchless
    client could arbitrarily unset PG_Uptodate flag for a
    page being kicked from the page cache, an uptodate check
    right after a readpage call in filemap_fault could fail
    because of that as though the page read had been unsuccessful.
    
    i=Oleg Drokin
    i=Johann Lombardi

--- lustre/include/linux/lustre_patchless_compat.h
+++ lustre/include/linux/lustre_patchless_compat.h
@@ -87,7 +87,6 @@ truncate_complete_page(struct address_space *mapping, struct page *page)
 #else
         clear_page_dirty(page);
 #endif
-        ClearPageUptodate(page);
         ClearPageMappedToDisk(page);
         ll_remove_from_page_cache(page);
         page_cache_release(page);       /* pagecache ref */
