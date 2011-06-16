c453ccb0f82fcaa5f537620990c4cc90a769b210
Jinshan Xiong <jay@whamcloud.com>
2011-06-15 22:30:27 -0700

    LU-395: obd_zombie_barrier is not barrier actually
    a hotfix for avoiding deadlock:
    sys_umount -> class_detach -> obd_zombie_impexp_cull -> filter_cleanup
    -> obd_zombie_barrier, but the zombies_count will only be decreased after
    obd_zombie_impexp_cull, so this process is waiting for itself, this is a
    deadlock.
    
    Signed-off-by: Jinshan Xiong <jay@whamcloud.com>
    Change-Id: Id29deab4c85a7570ec59b2a20f70ccfae9440f01
    Reviewed-on: http://review.whamcloud.com/945
    Reviewed-by: Mikhail Pershin <tappro@whamcloud.com>
    Tested-by: Hudson
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/obdclass/obd_config.c
+++ lustre/obdclass/obd_config.c
@@ -545,10 +545,6 @@ int class_detach(struct obd_device *obd, struct lustre_cfg *lcfg)
                obd->obd_name, obd->obd_uuid.uuid);
 
         class_decref(obd, "attach", obd);
-
-        /* not strictly necessary, but cleans up eagerly */
-        obd_zombie_impexp_cull();
-
         RETURN(0);
 }
 
