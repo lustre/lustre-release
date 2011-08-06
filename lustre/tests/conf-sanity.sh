f68c7642d140c3ad87af5c155a6eb8207585ca16
Jinshan Xiong <jay@whamcloud.com>
2011-08-05 19:32:22 -0700

    LU-573: conf-sanity test_22 failed with 41
    Make sure recovery on OST is finished before trying to create file
    
    Change-Id: I4a36685a5cd9c55de729906bff50c29b1108c931
    Signed-off-by: Jinshan Xiong <jay@whamcloud.com>
    Reviewed-on: http://review.whamcloud.com/1192
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -735,6 +735,7 @@ test_21d() {
         stop_mgs
         #writeconf to remove all ost2 traces for subsequent tests
         writeconf
+        start_mgs
 }
 run_test 21d "start mgs then ost and then mds"
 
@@ -764,6 +765,8 @@ test_22() {
 		sleep $((TIMEOUT + TIMEOUT + TIMEOUT))
 	fi
 	mount_client $MOUNT
+        wait_osc_import_state mds ost FULL
+        wait_osc_import_state client ost FULL
 	check_mount || return 41
 	pass
 
