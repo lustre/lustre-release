a88ec6990b4bbdcf6c8481374ef8ecb98cb93cf0
Lai Siyao <laisiyao@whamcloud.com>
2011-11-28 22:24:08 -0800

    LU-878 test: pdirop fail timeout too soon
    OBD_FAIL_MDS_PDO_LOCK timeout 10 seconds, but our autotest runs on
    hypervisor, if sanityn test_40b can't finish in 10 seconds, it will
    fail. Increase timeout to 15 should be able to fix this issue.
    
    Signed-off-by: Lai Siyao <laisiyao@whamcloud.com>
    Change-Id: I85de77b949efc65f439eab64b2d6b709560df772
    Reviewed-on: http://review.whamcloud.com/1751
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Niu Yawei <niu@whamcloud.com>
    Reviewed-by: Yu Jian <yujian@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -2239,7 +2239,7 @@ int mdt_object_lock(struct mdt_thread_info *info, struct mdt_object *o,
         else if (unlikely(OBD_FAIL_PRECHECK(OBD_FAIL_MDS_PDO_LOCK)) &&
                  lh->mlh_pdo_hash != 0 &&
                  (lh->mlh_reg_mode == LCK_PW || lh->mlh_reg_mode == LCK_EX)) {
-                OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_PDO_LOCK, 10);
+                OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_PDO_LOCK, 15);
         }
 
         RETURN(rc);
