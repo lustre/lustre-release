81d9117b65c74b149557b09d5e6df4f271e1f5df
Alex Zhuravlev <bzzz@whamcloud.com>
2011-12-12 20:11:08 +0300

    LU-911 defs: XATTR_NAME_FID defition
    to be used by OFD later to store back pointer
    to MDS object referencing this OST object.
    
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Change-Id: I5c7b4838efa9064eedf6c530d9eb54bed9e602fb
    Signed-off-by: Mikhail Pershin <tappro@whamcloud.com>
    Reviewed-on: http://review.whamcloud.com/1839
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/include/lustre/lustre_idl.h
+++ lustre/include/lustre/lustre_idl.h
@@ -1380,6 +1380,7 @@ struct lov_mds_md_v1 {            /* LOV EA mds/wire data (little-endian) */
 #define XATTR_NAME_LMA          "trusted.lma"
 #define XATTR_NAME_LMV          "trusted.lmv"
 #define XATTR_NAME_LINK         "trusted.link"
+#define XATTR_NAME_FID          "trusted.fid"
 #define XATTR_NAME_VERSION      "trusted.version"
 
 
