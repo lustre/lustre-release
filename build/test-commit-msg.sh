9fd0df600f0556877174ff31d8da3a232e37d815
Andreas Dilger <adilger@whamcloud.com>
2012-01-29 00:20:45 -0700

    LU-1053 build: allow deleted/chmod in commit-msg
    Allow files that were deleted or chmod when verifying a commit
    message that contains a diff generated with "commit -v".  The
    checks added for excluding the diff were previously too strict.
    Add a test case for each of these situations, as well as a case
    for a commit message that contains "diff" at the start of a line
    in the middle of a commit message.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I74fe98682342e445da7fe64fd42e91df4564500c
    Reviewed-on: http://review.whamcloud.com/2053
    Reviewed-by: Bruce Korb <bruce_korb@xyratex.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- build/test-commit-msg.sh
+++ build/test-commit-msg.sh
@@ -15,15 +15,15 @@ trap "rm -f $TEMPFILE COMMIT*" EXIT
 
 test $# -eq 0 && set -- ${progdir}/commit.*
 
+export FAIL=""
 readonly report_fmt='%-20s %s\n'
-for f
-do
+for f; do
         case "$f" in
-        ( *.orig | *.rej ) continue ;;
+	( *.orig | *.rej ) continue ;;
         esac
+
         cp $f $TEMPFILE
-        results=$(exec 2>&1
-                ${SHELL:-sh} $progdir/commit-msg $TEMPFILE)
+        results=$(exec 2>&1 ${SHELL:-sh} $progdir/commit-msg $TEMPFILE)
         case $'\n'"$results" in
         ( *$'\nerror:'* ) OK=0 ;;
         ( * ) OK=1 ;;
@@ -32,10 +32,15 @@ do
         f=$(basename $f)
         case $OK${f#*commit.} in
         1ok*)   printf "$report_fmt" $f: "PASS (was allowed)" ;;
-        0ok*)   printf "$report_fmt" $f: "FAIL (not allowed)" ;;
+        0ok*)   printf "$report_fmt" $f: "FAIL (not allowed)"; FAIL="$FAIL $f";;
         0*)     printf "$report_fmt" $f: "PASS (found error)" ;;
-        *)      printf "$report_fmt" $f: "FAIL (no error)"    ;;
+        *)      printf "$report_fmt" $f: "FAIL (no error)"   ; FAIL="$FAIL $f";;
         esac
 done
+
+if [ -n "$FAIL" ]; then
+	echo -e "\nerror: commit-msg test(s) failed!" 1>&2
+	echo "   $FAIL"
+fi
 
 rm -f $TEMPFILE $TEMPFILE.*
