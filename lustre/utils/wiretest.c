91dbd0b99e4d3fb6fe6a870cbf7297b5a362f05f
wangdi <di.wang@whamcloud.com>
2012-02-22 10:44:35 -0800

    LU-1081 fid: define special FID for BFL
    The sequence of LUSTRE_BFL_FID is in conflict with
    FID_SEQ_OST_MDT1, so reserve a new sequence for this FID.
    
    Also it defines a new FID sequence(FID_SEQ_SPECIAL) for
    later special purpose.
    
    Move some special dot_lustre oid to lustre_idl.h, so avoid
    oid conflict in future.
    
    Signed-off-by: Wang Di <di.wang@whamcloud.com>
    Change-Id: I556b31c706cc04c610c909f83e59f0cb9a821535
    Reviewed-on: http://review.whamcloud.com/2180
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>

--- lustre/utils/wiretest.c
+++ lustre/utils/wiretest.c
@@ -505,6 +505,13 @@ void lustre_assert_wire_constants(void)
         LASSERTF(FID_SEQ_LOV_DEFAULT == 0xffffffffffffffffULL, "found 0x%.16llxULL\n",
                  (long long)FID_SEQ_LOV_DEFAULT);
 
+        LASSERTF(FID_OID_SPECIAL_BFL == 0x00000001UL, "found 0x%.8xUL\n",
+                 (unsigned)FID_OID_SPECIAL_BFL);
+        LASSERTF(FID_OID_DOT_LUSTRE == 0x00000001UL, "found 0x%.8xUL\n",
+                 (unsigned)FID_OID_DOT_LUSTRE);
+        LASSERTF(FID_OID_DOT_LUSTRE_OBF == 0x00000002UL, "found 0x%.8xUL\n",
+                 (unsigned)FID_OID_DOT_LUSTRE_OBF);
+
         /* Checks for struct lu_dirent */
         LASSERTF((int)sizeof(struct lu_dirent) == 32, "found %lld\n",
                  (long long)(int)sizeof(struct lu_dirent));
