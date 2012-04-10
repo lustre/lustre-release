7cc542fd4c26ccb117ceb13a47ac8ced3107b9b3
Yevheniy Demchenko <zheka@uvt.cz>
2012-04-10 22:01:14 +0200

    LU-549 llite: Improve statfs performance if selinux is disabled
    Even if selinux is disabled, client still tries to get selinux
    attributes from MDS. As xattrs are not yet cached, this significantly
    slows down xattr heavy operations like ls -l. This patch forces
    to return -EOPNOTSUPP on the client side if selinux is disabled.
    It speeds up ls -l 25% for cold-cache case and 50% for hot-cache
    case.
    
    Signed-off-by: Yevheniy Demchenko <zheka@uvt.cz>
    Change-Id: I5e416093bba4126e5fcad62d8c0a2963c1866386
    Reviewed-on: http://review.whamcloud.com/2503
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Fan Yong <yong.fan@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/llite/xattr.c
+++ lustre/llite/xattr.c
@@ -40,6 +40,9 @@
 #include <linux/sched.h>
 #include <linux/mm.h>
 #include <linux/smp_lock.h>
+#ifdef HAVE_SELINUX_IS_ENABLED
+#include <linux/selinux.h>
+#endif
 
 #define DEBUG_SUBSYSTEM S_LLITE
 
@@ -95,6 +98,8 @@ int xattr_type_filter(struct ll_sb_info *sbi, int xattr_type)
            !(sbi->ll_flags & LL_SBI_ACL))
                 return -EOPNOTSUPP;
 
+        if (xattr_type == XATTR_SECURITY_T && !selinux_is_enabled())
+                return -EOPNOTSUPP;
         if (xattr_type == XATTR_USER_T && !(sbi->ll_flags & LL_SBI_USER_XATTR))
                 return -EOPNOTSUPP;
         if (xattr_type == XATTR_TRUSTED_T && !cfs_capable(CFS_CAP_SYS_ADMIN))
