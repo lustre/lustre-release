ac1473289379785baf8da8b590da28a0baeb2503
Minh Diep <mdiep@whamcloud.com>
2012-04-20 11:57:37 -0700

    LU-814 test: automated NFS over lustre testing
    Provide setup nfs within auster framework
    Note: this change includes LU-1134, LU-1213
    
    Signed-off-by: Minh Diep <mdiep@whamcloud.com>
    Change-Id: If28a237a23cd448c7d8b9a772a4b8951d94697ef
    Reviewed-on: http://review.whamcloud.com/2593
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Bob Glossman <bogl@whamcloud.com>
    Tested-by: Bob Glossman <bogl@whamcloud.com>
    Reviewed-by: Yu Jian <yujian@whamcloud.com>
    Reviewed-by: Johann Lombardi <johann@whamcloud.com>

---
+++ lustre/tests/parallel-scale-nfsv3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#set -vx
+
+LUSTRE=${LUSTRE:-$(cd $(dirname $0)/..; echo $PWD)}
+. $LUSTRE/tests/test-framework.sh
+init_test_env $@
+
+sh $LUSTRE/tests/parallel-scale-nfs.sh 3
---
+++ lustre/tests/setup-nfs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# vim:expandtab:shiftwidth=4:softtabstop=4:tabstop=4:
+#set -x
+EXPORT_OPTS=${EXPORT_OPTS:-"rw,async,no_root_squash"}
+
+setup_nfs() {
+    local NFS_VER=${1}
+    local MNTPNT=${2}
+    local LUSTRE_CLIENT=${3}
+    local NFS_CLIENTS=${4}
+    local export_opts_v=$EXPORT_OPTS
+
+    echo "Exporting Lustre filesystem..."
+
+    if [ "$NFS_VER" = "4" ]; then
+        export_opts_v="$EXPORT_OPTS,fsid=0"
+        do_nodes $LUSTRE_CLIENT "mkdir -p /var/lib/nfs/v4recovery"
+    fi
+
+    do_nodes $LUSTRE_CLIENT,$NFS_CLIENTS "grep -q rpc_pipefs' ' /proc/mounts ||\
+        { mkdir -p /var/lib/nfs/rpc_pipefs && \
+        mount -t rpc_pipefs sunrpc /var/lib/nfs/rpc_pipefs; }" || return 1
+    sleep 5
+
+    do_nodes $LUSTRE_CLIENT "service nfs restart" || return 1
+
+    do_nodes $NFS_CLIENTS "service rpcidmapd restart" || return 1
+
+    do_nodes $LUSTRE_CLIENT "exportfs -o $export_opts_v *:$MNTPNT \
+        && exportfs -v" || return 1
+
+    echo -e "\nMounting NFS clients (version $NFS_VER)..."
+
+    do_nodes $NFS_CLIENTS "mkdir -p $MNTPNT" || return 1
+    if [ "$NFS_VER" = "4" ]; then
+        do_nodes $NFS_CLIENTS \
+            "mount -t nfs$NFS_VER -o async $LUSTRE_CLIENT:/ $MNTPNT" || return 1
+    else
+        do_nodes $NFS_CLIENTS \
+            "mount -t nfs -o nfsvers=$NFS_VER,async \
+                $LUSTRE_CLIENT:$MNTPNT $MNTPNT" || return 1
+    fi
+    return 0
+}
+
+cleanup_nfs() {
+    local MNTPNT=${1}
+    local LUSTRE_CLIENT=${2}
+    local NFS_CLIENTS=${3}
+
+    echo -e "\nUnmounting NFS clients..."
+    do_nodes $NFS_CLIENTS "umount -f $MNTPNT" || return 1
+
+    echo -e "\nUnexporting Lustre filesystem..."
+    do_nodes $NFS_CLIENTS "service rpcidmapd stop" || return 1
+
+    do_nodes $LUSTRE_CLIENT "service nfs stop" || return 1
+
+    do_nodes $LUSTRE_CLIENT "exportfs -u *:$MNTPNT" || return 1
+
+    do_nodes $LUSTRE_CLIENT "exportfs -v"
+}
