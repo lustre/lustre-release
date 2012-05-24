98f973b5b2ce7dd5bcccdf126f39e59c376eaca2
Oleg Drokin <green@whamcloud.com>
2012-05-24 00:40:21 -0400

    LU-957 revert OI scrub patch
    The patch interactions with lut changes from LU-911
    broke mdt mounting.
    
    Revert "LU-957 scrub: Ancillary work for LFSCK/OI scrub"
    
    This reverts commit eeb7850ac3eae797a3cc3fee1a18725674d4d95f.
    
    Change-Id: Ic9a1d3e3da1e6d97d5db30b7d08656e68e4a0ba6
    Signed-off-by: Oleg Drokin <green@whamcloud.com>

--- lustre/osd-ldiskfs/osd_compat.c
+++ lustre/osd-ldiskfs/osd_compat.c
@@ -559,7 +559,7 @@ int osd_compat_spec_lookup(struct osd_thread_info *info,
 
         name = oid2name(fid_oid(fid));
         if (name == NULL || strlen(name) == 0)
-		RETURN(-ENOENT);
+                return -ERESTART;
 
         dentry = ll_lookup_one_len(name, osd_sb(osd)->s_root, strlen(name));
         if (!IS_ERR(dentry)) {
