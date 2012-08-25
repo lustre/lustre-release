a7a4fd2fa117497764e62d8bf28f4a0a37263aa2
Liu Xuezhao <xuezhao.liu@emc.com>
2012-08-25 16:19:20 +0800

    LU-812 llite: O_LOV_DELAY_CREATE conflicts with FMODE_NONOTIFY
    Kernel 2.6.36 introduces FMODE_NONOTIFY and defines it as 0x1000000
    (commit 12ed2e36c98aec6c41559222e311f4aa15d254b6), this causes the
    confliction with O_LOV_DELAY_CREATE which is now defined as octal
    0100000000.
    This confliction causes kernel remove this flag in build_open_flags,
    then "lfs setstripe" a new file will fail with -EEXIST(stripe
    already set).
    
    This patch changes O_LOV_DELAY_CREATE to 0120000000(0x1400000)
    to make it works for new kernel and compatible with old version
    statically linked binary.
    
    Another change is starting to build dynamic linked version of
    liblustreapi.so.
    
    Signed-off-by: Liu Xuezhao <xuezhao.liu@emc.com>
    Change-Id: I643d3734cfdc2f45099fc6be663a370c2dcc8ea3
    Reviewed-on: http://review.whamcloud.com/3779
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Iurii Golovach <Iurii_Golovach@xyratex.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>

--- lustre/include/lustre/lustre_user.h
+++ lustre/include/lustre/lustre_user.h
@@ -171,7 +171,13 @@ struct obd_statfs {
 
 #define MAX_OBD_NAME 128 /* If this changes, a NEW ioctl must be added */
 
-#define O_LOV_DELAY_CREATE 0100000000  /* hopefully this does not conflict */
+/* Hopefully O_LOV_DELAY_CREATE does not conflict with standard O_xxx flags.
+ * Previously it was defined as 0100000000 and conflicts with FMODE_NONOTIFY
+ * which was added since kernel 2.6.36, so we redefine it as 020000000.
+ * To be compatible with old version's statically linked binary, finally we
+ * define it as (020000000 | 0100000000).
+ * */
+#define O_LOV_DELAY_CREATE      0120000000
 
 #define LL_FILE_IGNORE_LOCK     0x00000001
 #define LL_FILE_GROUP_LOCKED    0x00000002
