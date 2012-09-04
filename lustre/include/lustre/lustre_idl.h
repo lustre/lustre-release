448732e41ec3e62d8c618847138d1e4922ac19e8
Johann Lombardi <johann@whamcloud.com>
2012-09-04 09:52:00 +0200

    LU-1789 protocol: reserve connect flag for lightweight conn
    Reserve connection flag for lightweight connection support.
    Although this feature will never be supported on 1.8, it still avoids
    flag conflicts.
    
    Signed-off-by: Johann Lombardi <johann@whamcloud.com>
    Change-Id: I1b943579d8c46cce0d70a5df45d35874bf63b29b
    Reviewed-on: http://review.whamcloud.com/3852

--- lustre/include/lustre/lustre_idl.h
+++ lustre/include/lustre/lustre_idl.h
@@ -371,6 +371,9 @@ extern void lustre_swab_ptlrpc_body(struct ptlrpc_body *pb, int msgsize);
                                                   * write RPC error properly */
 #define OBD_CONNECT_GRANT_PARAM 0x100000000000ULL/* extra grant params used for
                                                   * finer space reservation */
+#define OBD_CONNECT_NANOSEC_TIME 0x200000000000ULL /* nanosecond timestamps */
+#define OBD_CONNECT_LVB_TYPE     0x400000000000ULL /* variable type of LVB */
+#define OBD_CONNECT_LIGHTWEIGHT 0x1000000000000ULL /* lightweight connection */
 /* XXX README XXX:
  * Please DO NOT add flag values here before first ensuring that this same
  * flag value is not in use on some other branch.  Please clear any such
