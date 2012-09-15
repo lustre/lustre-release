3402e6c6517cc15f159b19bf4462279285cd83f2
Wang Di <di.wang@whamcloud.com>
2012-09-15 07:34:15 -0700

    LU-1571 mdt: Do not update xid for open replay req
    Do not update last_xid for open replay req,
    otherwise the following resend(after replay)
    can not be matched with correct xid.
    
    Remove unnecessary mti_transo zero check in
    mdt_empty_transno.
    
    Signed-off-by: wang di <di.wang@whamcloud.com>
    Change-Id: I2a05f3ac05b301ae31641a1dc51f8c4eed96427d
    Reviewed-on: http://review.whamcloud.com/3195
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>

--- lustre/tests/recovery-small.sh
+++ lustre/tests/recovery-small.sh
@@ -1542,6 +1542,29 @@ test_106() { # LU-1789
 }
 run_test 106 "lightweight connection support"
 
+test_107 () {
+	local CLIENT_PID
+	local close_pid
+
+	mkdir -p $DIR/$tdir
+	# OBD_FAIL_MDS_REINT_NET_REP   0x119
+	do_facet $SINGLEMDS lctl set_param fail_loc=0x119
+	multiop $DIR/$tdir D_c &
+	close_pid=$!
+	mkdir $DIR/$tdir/dir_106 &
+	CLIENT_PID=$!
+	do_facet $SINGLEMDS lctl set_param fail_loc=0
+	fail $SINGLEMDS
+
+	wait $CLIENT_PID || rc=$?
+	checkstat -t dir $DIR/$tdir/dir_106 || return 1
+
+	kill -USR1 $close_pid
+	wait $close_pid || return 2
+
+	return $rc
+}
+run_test 107 "drop reint reply, then restart MDT"
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status
