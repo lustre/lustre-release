4d75e156df75ff1fe95ca2f1f60e1b2681449e16
Alexander.Boyko <alexander_boyko@xyratex.com>
2012-09-17 13:26:01 +0400

    LU-1757 brw: added OBD short io connect flag
    To prevent collisions with any future flags needed in features written
    against this branch.
    
    Signed-off-by: Alexander Boyko <alexander_boyko@xyratex.com>
    Change-Id: I797ca7d9ac66c4932d8b6e8a85ced9c271fda076
    Reviewed-on: http://review.whamcloud.com/3890
    Tested-by: Hudson
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Johann Lombardi <johann@whamcloud.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/ptlrpc/wiretest.c
+++ lustre/ptlrpc/wiretest.c
@@ -1077,6 +1077,8 @@ void lustre_assert_wire_constants(void)
                  OBD_CONNECT_GRANT_PARAM);
         LASSERTF(OBD_CONNECT_LIGHTWEIGHT == 0x1000000000000ULL, "found 0x%.16llxULL\n",
                  OBD_CONNECT_LIGHTWEIGHT);
+	LASSERTF(OBD_CONNECT_SHORTIO == 0x2000000000000ULL, "found 0x%.16llxULL\n",
+		 OBD_CONNECT_SHORTIO);
         LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
                  (unsigned)OBD_CKSUM_CRC32);
         LASSERTF(OBD_CKSUM_ADLER == 0x00000002UL, "found 0x%.8xUL\n",
