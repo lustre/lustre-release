147839e23d49551ba61319b0812a3ef7e36cc80a
Andreas Dilger <adilger@whamcloud.com>
2012-10-03 16:18:54 -0600

    LU-2083 build: install git commit hooks automatically
    Install the Lustre Git commit hooks into .git/hooks/ by default when
    autogen.sh is run, so that they are present when patches are being
    committed.  This avoids the relatively common case where a new tree
    is checked out by new or experienced developers and is missing the
    commit hooks when patches are being submitted.
    
    While the commit hooks are sure to be installed for in any tree that
    was built, this isn't a guarantee that the hooks will be installed in
    every tree that has a commit, but it is very likely to be the case.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I6a15420fb7a35b790c1e816c67e20a8004500c1e
    Reviewed-on: http://review.whamcloud.com/4175
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Yu Jian <yujian@whamcloud.com>
    Reviewed-by: Wei Liu <wei3.liu@intel.com>
    Reviewed-by: Bruce Korb <bruce_korb@xyratex.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# install Lustre Git commit hooks by default - LU-2083
+for HOOK in commit-msg prepare-commit-msg; do
+	[ -e .git/hooks/$HOOK ] || ln -sf ../build/$HOOK .git/hooks/
+done
+
 exec bash build/autogen.sh $@ 
+
