ea941366d550f8cfefb108ebc3354d08e1bd71bd
Andreas Dilger <adilger@whamcloud.com>
2012-12-03 00:02:26 -0700

    LU-20 build: fix confirmpatches, remove old patches
    Update both confirmpatches.sh and clearpatches.sh scripts to take
    a command line argument of the patch directory to check.  Add
    options "-h" for help, and "-v" for verbose.
    
    Update confirmpatches.sh to ignore comment lines in patch series.
    
    Update clearpatches.sh to have a "-d" option to delete the patches
    it found to be unused.
    
    Remove unused kernel/ldiskfs patches found from running clearpatches.
    
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: Id5a1de13930f1c428a13d6816e68db40ce61cab0
    Reviewed-on: http://review.whamcloud.com/4755
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Christopher J. Morrone <chris.morrone.llnl@gmail.com>
    Reviewed-by: Yang Sheng <yang.sheng@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- build/clearpatches.sh
+++ build/clearpatches.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+PROG=$(basename $0)
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
+[ "$1" = "-d" ] && shift && DELETE="git rm" || DELETE="echo"
+[ "$1" = "-v" ] && shift && VERBOSE="echo" || VERBOSE=":"
+
+[ "$1" ] && BASEDIR="$1"
 BASEDIR=${BASEDIR:-lustre/kernel_patches}
 SERIESPATH=${SERIESPATH:-$BASEDIR/series}
-PATCHESPATH=${PATCHESPATH:-$BASEDIR/patches}
-NOUSEPATH=${NOUSEPATH:-$BASEDIR/unused}
+PATCHPATH=${PATCHPATH:-$BASEDIR/patches}
 
-#mkdir -p $NOUSEPATH
-for PATCH in `ls $PATCHESPATH | grep -v CVS` ; do
-	#echo $PATCH
- 	if ! grep -rq $PATCH $SERIESPATH ; then
-		echo "$PATCH"
-	  	#mv $PATCHESPATH/$PATCH $NOUSEPATH
+[ ! -d "$BASEDIR" ] && echo "$PROG: missing base directory '$BASEDIR'" && exit 1
+[ ! -d "$SERIESPATH" ] && echo "$PROG: missing series '$SERIESPATH'" && exit 2
+[ ! -d "$PATCHPATH" ] && echo "$PROG: missing patches '$PATCHPATH'" && exit 3
+
+for PATCH in $(ls $PATCHPATH | egrep -v "CVS|~$|.orig|.rej"); do
+	$VERBOSE $PATCH
+ 	if ! grep -q $PATCH $SERIESPATH/*.series ; then
+	  	$DELETE $PATCHPATH/$PATCH
 	fi
 done
--- build/confirmpatches.sh
+++ build/confirmpatches.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+PROG=$(basename $0)
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
+[ "$1" = "-v" ] && shift && VERBOSE="echo" || VERBOSE=":"
+
 BASEDIR=${BASEDIR:-lustre/kernel_patches}
 SERIESPATH=${SERIESPATH:-$BASEDIR/series}
-PATCHESPATH=${PATCHESPATH:-$BASEDIR/patches}
-for SERIES in `ls $SERIESPATH | egrep -v "CVS|~$|.orig"` ; do
-	#echo $SERIES
-	for PATCH in `cat $SERIESPATH/$SERIES`; do
-		#echo $PATCH
-		if [ ! `find $PATCHESPATH -name $PATCH` ]; then
-			echo "$SERIESPATH/$SERIES: patch $PATCH was not found !"
+PATCHPATH=${PATCHPATH:-$BASEDIR/patches}
+
+[ ! -d "$BASEDIR" ] && echo "$PROG: missing base directory '$BASEDIR'" && exit 1
+[ ! -d "$SERIESPATH" ] && echo "$PROG: missing series '$SERIESPATH'" && exit 2
+[ ! -d "$PATCHPATH" ] && echo "$PROG: missing patches '$PATCHPATH'" && exit 3
+
+for SERIES in $(ls $SERIESPATH | egrep -v "CVS|~$|.orig") ; do
+	$VERBOSE "series: $SERIES"
+	for PATCH in $(grep -v "^#" $SERIESPATH/$SERIES); do
+		$VERBOSE $PATCH
+		if [ ! $(find $PATCHPATH -name $PATCH) ]; then
+			echo "$SERIESPATH/$SERIES: patch '$PATCH' not found!"
 		fi
 	done
 done
