34867abf42f22d6c435e9e15e9497f8095059b04
Niu Yawei <niu@whamcloud.com>
2013-01-04 04:35:07 -0500

    LU-2543 llite: wait osc cleanup when put super
    Since osc is now accessing sbi->ll_cache, we'd wait till all OSCs
    are gone when put super.
    
    Signed-off-by: Niu Yawei <niu@whamcloud.com>
    Change-Id: I5d09b29cda28d4b596f67bdf8da2ce5e7082a38e
    Reviewed-on: http://review.whamcloud.com/4951
    Reviewed-by: Bobi Jam <bobijam@gmail.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -595,6 +595,8 @@ out_lock_cn_cb:
 out_dt:
         obd_disconnect(sbi->ll_dt_exp);
         sbi->ll_dt_exp = NULL;
+	/* Make sure all OScs are gone, since cl_cache is accessing sbi. */
+	obd_zombie_barrier();
 out_md_fid:
         obd_fid_fini(sbi->ll_md_exp);
 out_md:
@@ -687,6 +689,9 @@ void client_common_put_super(struct super_block *sb)
         obd_fid_fini(sbi->ll_dt_exp);
         obd_disconnect(sbi->ll_dt_exp);
         sbi->ll_dt_exp = NULL;
+	/* wait till all OSCs are gone, since cl_cache is accessing sbi.
+	 * see LU-2543. */
+	obd_zombie_barrier();
 
         lprocfs_unregister_mountpoint(sbi);
 
