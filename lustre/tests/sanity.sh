97354fbe39ec40ef720115fe5a3687c3cda7b8ae
Hongchao Zhang <hongchao.zhang@intel.com>
2013-01-20 14:53:56 +0800

    LU-1940 test: use default resend count for 118c
    in sanity, the 118c could fail with the small resend count
    for SLOW=no case, using the default value instead.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>
    Change-Id: I3e5ecc6af2eba560f82a80eb75223f8ab299f397
    Reviewed-on: http://review.whamcloud.com/4694
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -6465,6 +6465,7 @@ test_117() # bug 10891
 }
 run_test 117 "verify fsfilt_extend =========="
 
+NO_SLOW_RESENDCOUNT=4
 export OLD_RESENDCOUNT=""
 set_resend_count () {
 	local PROC_RESENDCOUNT="osc.${FSNAME}-OST*-osc-*.resend_count"
@@ -6473,7 +6474,8 @@ set_resend_count () {
 	echo resend_count is set to $(lctl get_param -n $PROC_RESENDCOUNT)
 }
 
-[ "$SLOW" = "no" ] && set_resend_count 4 # for reduce test_118* time (bug 14842)
+# for reduce test_118* time (b=14842)
+[ "$SLOW" = "no" ] && set_resend_count $NO_SLOW_RESENDCOUNT
 
 # Reset async IO behavior after error case
 reset_async() {
@@ -6543,6 +6545,10 @@ run_test 118b "Reclaim dirty pages on fatal error =========="
 test_118c()
 {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
+
+	# for 118c, restore the original resend count, LU-1940
+	[ "$SLOW" = "no" ] && [ -n "$OLD_RESENDCOUNT" ] &&
+				set_resend_count $OLD_RESENDCOUNT
 	remote_ost_nodsh && skip "remote OST with nodsh" && return
 
 	reset_async
@@ -6584,6 +6590,9 @@ test_118c()
 	return 0
 }
 run_test 118c "Fsync blocks on EROFS until dirty pages are flushed =========="
+
+# continue to use small resend count to reduce test_118* time (b=14842)
+[ "$SLOW" = "no" ] && set_resend_count $NO_SLOW_RESENDCOUNT
 
 test_118d()
 {
