0be1c87fe2d4ffddaca9e568cc137518b7368b2d
Andreas Dilger <andreas.dilger@intel.com>
2013-02-13 17:04:37 -0800

    LU-2523 tests: disable racer setstripe until fixed
    Disable the use of "lfs setstripe" in racer file_create.sh until
    the LU-2523 and LU-2789 are fixed, so that racer can be added to
    the "review" test workload instead of only in the "full" workload.
    
    Test-Parameters: testlist=racer,racer,racer
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I8b0814ef7864efd817ff585f715183fbf73ebbe5
    Reviewed-on: http://review.whamcloud.com/5424
    Reviewed-by: John Hammond <johnlockwoodhammond@gmail.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/racer/file_create.sh
+++ lustre/tests/racer/file_create.sh
@@ -4,13 +4,15 @@ DIR=$1
 MAX=$2
 MAX_MB=256
 
+# disable until LU-2523 and LU-2789 are fixed
+[ -z "$RACER_SETSTRIPE" ] && RACER_SETSTRIPE=false
 OSTCOUNT=${OSTCOUNT:-$(lfs df $DIR 2> /dev/null | grep -c OST)}
 
 while /bin/true ; do 
 	file=$((RANDOM % MAX))
 	SIZE=$((RANDOM * MAX_MB / 32))
 	echo "file_create: FILE=$DIR/$file SIZE=$SIZE"
-	[ $OSTCOUNT -gt 0 ] &&
+	[ $OSTCOUNT -gt 0 ] && $RACER_SETSTRIPE &&
 		lfs setstripe -c $((RANDOM % OSTCOUNT)) $DIR/$file 2> /dev/null
 	dd if=/dev/zero of=$DIR/$file bs=1k count=$SIZE 2> /dev/null
 done
