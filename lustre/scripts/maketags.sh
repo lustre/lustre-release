6b3e12122122cefab463ed854ab233ee57d9f187
Andreas Dilger <andreas.dilger@intel.com>
2013-05-01 10:08:02 -0600

    LU-1538 utils: remove obsolete scripts
    A number of obsolete test, build, and helper scripts are removed,
    because they are no longer useful.
    
      lustre/doc/chbar.sh
      lustre/doc/postbar
      lustre/doc/tex2pdf
      lustre/scripts/bdev-io-survey.sh
      lustre/scripts/dodiff.sh
      lustre/scripts/maketags.sh
      lustre/tests/sanity-nano.sh
      lustre/utils/loadmod_all.sh
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I109133aadfc75f9812259e560710dc70036e3dd6
    Reviewed-on: http://review.whamcloud.com/6227
    Tested-by: Hudson
    Reviewed-by: Emoly Liu <emoly.liu@intel.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Reviewed-by: Prakash Surya <surya1@llnl.gov>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/scripts/maketags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (c) 1999, 2010, Oracle and/or its affiliates. All rights reserved.
-#
-# This code is issued under the GNU General Public License.
-# See the file COPYING in this distribution
-set -vx
-rm -f TAGS ; find . -name '*.h' -or -name '*.c' | xargs etags
-rm -f ctags; find . -name '*.h' -or -name '*.c' | xargs ctags
