d90e0d48989e61109fc05cc07022c3282df8b23c
John L. Hammond <john.hammond@intel.com>
2013-05-15 12:42:47 -0500

    LU-3348 test: use correct old path in racer's file_link.sh
    In racer's file_link.sh the old path was not prefixed by the directory
    used by racer. Since file_link.sh does not run out of this directory
    no links were created. Fix this in by supplying the correct old path.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: Ifcbcc3d50c70faed7a99fa95217b08c8efe35791
    Reviewed-on: http://review.whamcloud.com/6351
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Jian Yu <jian.yu@intel.com>

--- lustre/tests/racer/file_link.sh
+++ lustre/tests/racer/file_link.sh
@@ -3,8 +3,8 @@
 DIR=$1
 MAX=$2
 
-while /bin/true ; do 
-    file=$((RANDOM % $MAX))
-    new_file=$((RANDOM % MAX))
-    ln $file $DIR/$new_file 2> /dev/null
+while true; do
+	old_path=$DIR/$((RANDOM % MAX))
+	new_path=$DIR/$((RANDOM % MAX))
+	ln $old_path $new_path 2> /dev/null
 done
