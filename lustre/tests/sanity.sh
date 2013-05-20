fd7466439be3039cb27f723c57be73317b5e48ba
Jian Yu <jian.yu@intel.com>
2013-05-20 13:33:58 +0800

    LU-2598 tests: check nr_local in ofd_preprw()
    This patch checks the number of bulk I/O RPC pages with
    PTLRPC_MAX_BRW_PAGES in ofd_preprw() to avoid
    LASSERT(iobuf->dr_npages < iobuf->dr_max_pages) occurring
    while larger I/O size is specified.
    
    The patch also fixes echo_client_prep_commit() to reuse
    the env context so as to avoid LASSERT(info->fti_exp == NULL)
    occurring while the bulk I/O size is larger than
    PTLRPC_MAX_BRW_SIZE.
    
    The patch also improves obdfilter-survey to handle the
    case while interoprating with old server.
    
    Test-Parameters: envdefinitions=SLOW=yes,ENABLE_QUOTA=yes \
    testlist=obdfilter-survey
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: Ifa7b720da1e3fc752f85bcfa6968eb16f93af2fa
    Reviewed-on: http://review.whamcloud.com/6394
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -9541,6 +9541,7 @@ cleanup_obdecho_osc () {
 obdecho_test() {
         local OBD=$1
         local node=$2
+	local pages=${3:-64}
         local rc=0
         local id
         do_facet $node "$LCTL attach echo_client ec ec_uuid" || rc=1
@@ -9553,8 +9554,8 @@ obdecho_test() {
         echo "New object id is $id"
 	[ $rc -eq 0 ] && { do_facet $node "$LCTL --device ec getattr $id" ||
 			   rc=4; }
-	[ $rc -eq 0 ] && { do_facet $node "$LCTL --device ec test_brw 10 w v 64 $id" ||
-			   rc=4; }
+	[ $rc -eq 0 ] && { do_facet $node "$LCTL --device ec "		       \
+			   "test_brw 10 w v $pages $id" || rc=4; }
 	[ $rc -eq 0 ] && { do_facet $node "$LCTL --device ec destroy $id 1" ||
 			   rc=4; }
 	[ $rc -eq 0 -o $rc -gt 2 ] && { do_facet $node "$LCTL --device ec "    \
@@ -9606,6 +9607,32 @@ test_180b() {
 	return $rc
 }
 run_test 180b "test obdecho directly on obdfilter"
+
+test_180c() { # LU-2598
+	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
+	[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.4.0) ]] &&
+		skip "Need MDS version at least 2.4.0" && return
+
+	local rc=0
+	local rmmod_remote=false
+	local pages=16384 # 64MB bulk I/O RPC size
+	local target
+
+	do_rpc_nodes $(facet_active_host ost1) load_module obdecho/obdecho &&
+		rmmod_remote=true || error "failed to load module obdecho"
+
+	target=$(do_facet ost1 $LCTL dl | awk '/obdfilter/ {print $4}'|head -1)
+	if [[ -n $target ]]; then
+		obdecho_test "$target" ost1 "$pages" ||
+			rc=${PIPESTATUS[0]}
+	else
+		echo "there is no obdfilter target on ost1"
+		rc=2
+	fi
+	$rmmod_remote && do_facet ost1 "rmmod obdecho" || true
+	return $rc
+}
+run_test 180c "test huge bulk I/O size on obdfilter, don't LASSERT"
 
 test_181() { # bug 22177
 	test_mkdir -p $DIR/$tdir || error "creating dir $DIR/$tdir"
