a9c1d872bdfc8a8b7ecd43b3d2081a183c7bda7e
Bruce Korb <bruce_korb@xyratex.com>
2013-07-11 11:16:06 -0700

    LU-3568 contrib: ignore initial comments
    Sometimes, git likes to insert "git status" commentary
    at the top of an auto-generated commit message starter
    file. If the committer carelessly neglects to remove
    the stuff, the script thinks the message has a missing
    commit summary line.
    
    * contrib/tests/test-commit-msg.sh: pass through xtrace setting
    * contrib/tests/commit.ok_commit: This message with cruft
    * contrib/commit-msg: use a flag instead of a line number to see
      if we still need to process the summary line.
    
    Signed-off-by: Bruce Korb <bruce_korb@xyratex.com>
    Reviewed-by: Cheng Shao <cheng_shao@xyratex.com>
    Xyratex-bug-id: MRP-1160
    Change-Id: I46e2951f2e28cbbf53bf2e96e420ddcea7c0d991
    Reviewed-on: http://review.whamcloud.com/6921
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- contrib/git-hooks/tests/test-commit-msg.sh
+++ contrib/git-hooks/tests/test-commit-msg.sh
@@ -4,43 +4,57 @@ program=$0
 progdir=$(\cd $(dirname $0) >/dev/null && pwd)
 
 die() {
-        exec 1>&2
-        echo "$program fatal error:  $*"
-        exit 1
-}
+	echo "$program fatal error:  $*"
+	exit 1
+} 1>&2
 
 TEMPFILE=$(mktemp ${TMPDIR:-.}/commit-XXXXXX)
 test -f "$TEMPFILE" || die "mktemp fails"
 trap "rm -f $TEMPFILE COMMIT*" EXIT
+
+# Pass through xtrace setting to commit-msg script
+#
+SHELL=${SHELL:-sh}
+shopt -qo xtrace && SHELL+=' -x'
 
 test $# -eq 0 && set -- ${progdir}/commit.*
 
 export FAIL=""
 readonly report_fmt='%-20s %s\n'
 for f; do
-        case "$f" in
+	case "$f" in
 	( *.orig | *.rej ) continue ;;
-        esac
+	esac
 
-        cp $f $TEMPFILE
-        results=$(exec 2>&1 ${SHELL:-sh} $progdir/../commit-msg $TEMPFILE)
-        case $'\n'"$results" in
-        ( *$'\nerror:'* ) OK=0 ;;
-        ( * ) OK=1 ;;
-        esac
+	cp $f $TEMPFILE
+	results=$(exec 2>&1 ${SHELL} $progdir/../commit-msg $TEMPFILE)
+	case $'\n'"$results" in
+	( *$'\nerror:'* ) OK=0 ;;
+	( * ) OK=1 ;;
+	esac
 
-        f=$(basename $f)
-        case $OK${f#*commit.} in
-        1ok*)   printf "$report_fmt" $f: "PASS (was allowed)" ;;
-        0ok*)   printf "$report_fmt" $f: "FAIL (not allowed)"; FAIL="$FAIL $f";;
-        0*)     printf "$report_fmt" $f: "PASS (found error)" ;;
-        *)      printf "$report_fmt" $f: "FAIL (no error)"   ; FAIL="$FAIL $f";;
-        esac
+	f=$(basename $f)
+	case $OK${f#*commit.} in
+	1ok*)	printf "$report_fmt" $f: "PASS (was allowed)" ;;
+	0ok*)	printf "$report_fmt" $f: "FAIL (not allowed)"; FAIL="$FAIL $f";;
+	0*)	printf "$report_fmt" $f: "PASS (found error)" ;;
+	*)	printf "$report_fmt" $f: "FAIL (no error)"   ; FAIL="$FAIL $f";;
+	esac
 done
 
 if [ -n "$FAIL" ]; then
 	echo -e "\nerror: commit-msg test(s) failed!" 1>&2
-	echo "   $FAIL"
+	echo "	 $FAIL"
 fi
 
 rm -f $TEMPFILE $TEMPFILE.*
+
+## Local Variables:
+## Mode: shell-script
+## sh-basic-offset:          8
+## sh-indent-after-do:       8
+## sh-indentation:           8
+## sh-indent-for-case-label: 0
+## sh-indent-for-case-alt:   8
+## indent-tabs-mode:         t
+## End:
