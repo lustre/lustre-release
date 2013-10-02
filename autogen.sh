5020ca0aa28feb0874549fde955e82ccf70c4b82
Christopher J. Morrone <morrone2@llnl.gov>
2013-10-02 16:14:03 -0700

    LU-4052 build: simplify autogen.sh
    The concept of conditional subdirectories dates back to the days
    when we had Lustre spread across multiple CVS repositories.  For
    years now we have used git, and the tree is entirely available
    at all times.
    
    Therefore we can simplify autogen.sh a bit more.
    
    Change-Id: I3d7c8e5e93b57c492bb61554a546ceb6d90b2ffe
    Signed-off-by: Christopher J. Morrone <morrone2@llnl.gov>
    Reviewed-on: http://review.whamcloud.com/7840
    Tested-by: Hudson
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Minh Diep <minh.diep@intel.com>
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -2,54 +2,17 @@
 
 # NOTE: Please avoid bashisms (bash specific syntax) in this script
 
-# die a horrible death.  All output goes to stderr.
-#
-die()
-{
-	echo "bootstrap failure:  $*"
-	echo Aborting
-	exit 1
-} 1>&2
+set -e
 
-run_cmd()
-{
-	echo -n "Running $*"
-	eval "$@" || die "command exited with code $?"
-	echo
-}
-
-echo "Checking for a complete tree..."
-REQUIRED_DIRS="libcfs lnet lustre"
-OPTIONAL_DIRS="snmp portals"
-CONFIGURE_DIRS="libsysio"
-
-for dir in $REQUIRED_DIRS ; do
-	test -d "$dir" || \
-		die "Your tree seems to be missing $dir.
-Please read README.lustrecvs for details."
-
+for dir in libcfs lnet lustre snmp ; do
 	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $PWD/$dir/autoconf"
 done
-# optional directories for Lustre
-for dir in $OPTIONAL_DIRS; do
-	if [ -d "$dir" ] ; then
-		ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $PWD/$dir/autoconf"
-	fi
-done
 
-PWD_SAVE=$PWD
+aclocal -I $PWD/config $ACLOCAL_FLAGS
+autoheader
+automake -a -c
+autoconf
 
-run_cmd "aclocal -I $PWD/config $ACLOCAL_FLAGS"
-run_cmd "autoheader"
-run_cmd "automake -a -c"
-run_cmd autoconf
-
-# bootstrap in these directories
-for dir in $CONFIGURE_DIRS; do
-	if [ -d $dir ] ; then
-		cd $dir
-		echo "bootstrapping in $dir..."
-		run_cmd "sh autogen.sh"
-	fi
-	cd $PWD_SAVE
-done
+cd libsysio
+echo "bootstrapping in libsysio..."
+sh autogen.sh
