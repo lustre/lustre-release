28fefece16fbc2cf98000e3d709be7648cb8237e
Fan Yong <fan.yong@intel.com>
2013-10-23 14:19:03 +0800

    LU-4058 tests: skip sanity-scrub test_15 for 2.4.1 or older
    Lustre-2.4.1 or older does not support dry-run mode OI scrub yet.
    
    Test-Parameters: mdsjob=lustre-b2_4 ossjob=lustre-b2_4 \
    mdsbuildno=54 ossbuildno=54 testlist=sanity-scrub
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: I1a97f850ab39d7d42ef7ed6e191ce66b3b093415
    Reviewed-on: http://review.whamcloud.com/7868
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/tests/sanity-scrub.sh
+++ lustre/tests/sanity-scrub.sh
@@ -42,6 +42,13 @@ check_and_setup_lustre
 [[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.3.90) ]] &&
 	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 1a"
 
+[[ $(lustre_version_code $SINGLEMDS) -le $(version_code 2.4.1) ]] &&
+	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 15"
+
+[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.4.90) ]] &&
+[[ $(lustre_version_code $SINGLEMDS) -ge $(version_code 2.4.50) ]] &&
+	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 15"
+
 [[ $(lustre_version_code ost1) -lt $(version_code 2.4.50) ]] &&
 	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 11 12 13 14"
 
