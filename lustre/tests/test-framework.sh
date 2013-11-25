1c30099de6d74e038cd3d269c2a64b8eeb3c91ca
Jian Yu <jian.yu@intel.com>
2013-11-25 14:12:22 +0800

    LU-3738 tests: improve posix.sh to support BASELINE_FS=zfs
    This patch improves posix.sh to support BASELINE_FS=zfs. It also
    adds create_zpool(), create_zfs() and destroy_zpool() common
    functions into test-framework.sh.
    
    Test-Parameters: envdefinitions=SLOW=yes \
    mdtfilesystemtype=zfs mdsfilesystemtype=zfs ostfilesystemtype=zfs \
    ostcount=2 testlist=posix
    
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: Ib8b3cf988bd7066a84a6f9f50ccc159ffeab6e0e
    Reviewed-on: http://review.whamcloud.com/8323
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -887,6 +887,33 @@ zpool_name() {
 }
 
 #
+# Create ZFS storage pool.
+#
+create_zpool() {
+	local facet=$1
+	local poolname=$2
+	local vdev=$3
+	shift 3
+	local opts=${@:-"-o cachefile=none"}
+
+	do_facet $facet "$ZPOOL list -H $poolname >/dev/null 2>&1 ||
+		$ZPOOL create -f $opts $poolname $vdev"
+}
+
+#
+# Create ZFS file system.
+#
+create_zfs() {
+	local facet=$1
+	local dataset=$2
+	shift 2
+	local opts=${@:-"-o mountpoint=legacy"}
+
+	do_facet $facet "$ZFS list -H $dataset >/dev/null 2>&1 ||
+		$ZFS create $opts $dataset"
+}
+
+#
 # Export ZFS storage pool.
 # Before exporting the pool, all datasets within the pool should be unmounted.
 #
@@ -902,6 +929,22 @@ export_zpool() {
 		do_facet $facet "! $ZPOOL list -H $poolname >/dev/null 2>&1 ||
 			grep -q ^$poolname/ /proc/mounts ||
 			$ZPOOL export $opts $poolname"
+	fi
+}
+
+#
+# Destroy ZFS storage pool.
+# Destroy the given pool and free up any devices for other use. This command
+# tries to unmount any active datasets before destroying the pool.
+# -f    Force any active datasets contained within the pool to be unmounted.
+#
+destroy_zpool() {
+	local facet=$1
+	local poolname=${2:-$(zpool_name $facet)}
+
+	if [[ -n "$poolname" ]]; then
+		do_facet $facet "! $ZPOOL list -H $poolname >/dev/null 2>&1 ||
+			$ZPOOL destroy -f $poolname"
 	fi
 }
 
