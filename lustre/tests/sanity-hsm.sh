72f32e4c8d2769d1947690e59f8a7b8605b259ca
Nathaniel Clark <nathaniel.l.clark@intel.com>
2013-12-06 16:26:59 -0500

    LU-3700 tests: skip sanity-hsm/51b for ZFS
    EXCEPT sanity-hsm/51b until a full fix can be found for this issue so
    that review-zfs can be enabled.
    
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: If6b7fb0d6c5ed20c8fb7ce898311f2b3d53a2916
    Reviewed-on: http://review.whamcloud.com/8503
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Faccini Bruno <bruno.faccini@intel.com>

--- lustre/tests/sanity-hsm.sh
+++ lustre/tests/sanity-hsm.sh
@@ -34,6 +34,10 @@ FAIL_ON_ERROR=false
 [ $MDSCOUNT -gt 9 ] &&
 	error "script cannot handle more than 9 MDTs, please fix" && exit
 
+[ $(facet_fstype $SINGLEMDS) = "zfs" ] &&
+# bug number for skipped test:        LU-3700
+	ALWAYS_EXCEPT="$ALWAYS_EXCEPT 51b"
+
 check_and_setup_lustre
 
 if [[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.4.53) ]]; then
