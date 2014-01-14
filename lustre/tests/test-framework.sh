db1b3bec6ee1d24d56969543539c1b1f91e8bc6a
Mikhail Pershin <mike.pershin@intel.com>
2014-01-14 18:58:33 +0400

    LU-4480 tests: set fail_val in sanity test_36f
    The OBD_FAIL_OST_BRW_PAUSE_BULK needs fail_val to be passed
    as amount of seconds to wait or 0 for default value.
    That was not set to any of those values in test_36f.
    
    Patch set fail_val=0 along with fail_loc in test_36f
    
    Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
    Change-Id: I3966a6c8a7f07fd6232a2f70329eb34cfdd95cee
    Reviewed-on: http://review.whamcloud.com/8842
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -4757,7 +4757,8 @@ check_mds() {
 
 reset_fail_loc () {
     echo -n "Resetting fail_loc on all nodes..."
-    do_nodes $(comma_list $(nodes_list)) "lctl set_param -n fail_loc=0 2>/dev/null || true"
+    do_nodes $(comma_list $(nodes_list)) "lctl set_param -n fail_loc=0 \
+	    fail_val=0 2>/dev/null || true"
     echo done.
 }
 
