7fd6681466dbc8007032f836808e077fc1fa244c
wangdi <di.wang@whamcloud.com>
2014-01-17 02:13:54 -0800

    LU-1750 ofd: always update last_id.
    Always update last_id during orphan cleanup, even though
    the orphan might be cleanup by the preious request.
    
    Change-Id: I824c97b29b5e03906e66f27e044876cf097ce534
    Signed-off-by: Wang Di <di.wang@intel.com>
    Reviewed-on: http://review.whamcloud.com/4331
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Hudson
    Tested-by: Maloo <whamcloud.maloo@gmail.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Niu Yawei <yawei.niu@intel.com>
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/ofd/ofd_obd.c
+++ lustre/ofd/ofd_obd.c
@@ -1230,6 +1230,7 @@ int ofd_create(const struct lu_env *env, struct obd_export *exp,
 			CERROR("%s:["LPU64"] destroys_in_progress already"
 			       " cleared\n", exp->exp_obd->obd_name,
 			       ostid_seq(&oa->o_oi));
+			ostid_set_id(&oa->o_oi, ofd_seq_last_oid(oseq));
 			GOTO(out, rc = 0);
 		}
 		diff = ostid_id(&oa->o_oi) - ofd_seq_last_oid(oseq);
