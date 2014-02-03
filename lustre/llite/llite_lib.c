96f482b509867bb70cc26e94e60903df82b09f30
Dmitry Eremin <dmitry.eremin@intel.com>
2014-02-03 20:07:27 +0400

    LU-4257 clio: replace semaphore with mutex
    According https://www.kernel.org/doc/Documentation/mutex-design.txt:
     - the mutex subsystem is slightly faster and has better scalability
       for contended workloads. In terms of 'ops per CPU cycle', the
       semaphore kernel performed 551 ops/sec per 1% of CPU time used,
       while the mutex kernel performed 3825 ops/sec per 1% of CPU time
       used - it was 6.9 times more efficient.
     - there are no fastpath tradeoffs, the mutex fastpath is just as
       tight as the semaphore fastpath. On x86, the locking fastpath is
       2 instructions.
     - 'struct mutex' semantics are well-defined and are enforced if
       CONFIG_DEBUG_MUTEXES is turned on. Semaphores on the other hand
       have virtually no debugging code or instrumentation.
    
    One more benefit of mutex is optimistic spinning. It try to spin for
    acquisition when there are no pending waiters and the lock owner is
    currently running on a (different) CPU. The rationale is that if the
    lock owner is running, it is likely to release the lock soon.
    
    This significantly reduce amount of context switches when locked
    region is small and we have high contention.
    
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Change-Id: Ic13179ab7e5ee628f550e5dfd251af31ac6d4211
    Reviewed-on: http://review.whamcloud.com/9095
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -1001,8 +1001,7 @@ void ll_lli_init(struct ll_inode_info *lli)
 		spin_lock_init(&lli->lli_sa_lock);
 		lli->lli_opendir_pid = 0;
 	} else {
-		sema_init(&lli->lli_size_sem, 1);
-		lli->lli_size_sem_owner = NULL;
+		mutex_init(&lli->lli_size_mutex);
 		lli->lli_symlink_name = NULL;
 		init_rwsem(&lli->lli_trunc_sem);
 		mutex_init(&lli->lli_write_mutex);
@@ -1950,25 +1949,20 @@ int ll_statfs(struct dentry *de, struct kstatfs *sfs)
 
 void ll_inode_size_lock(struct inode *inode)
 {
-        struct ll_inode_info *lli;
+	struct ll_inode_info *lli;
 
-        LASSERT(!S_ISDIR(inode->i_mode));
+	LASSERT(!S_ISDIR(inode->i_mode));
 
-        lli = ll_i2info(inode);
-        LASSERT(lli->lli_size_sem_owner != current);
-	down(&lli->lli_size_sem);
-        LASSERT(lli->lli_size_sem_owner == NULL);
-        lli->lli_size_sem_owner = current;
+	lli = ll_i2info(inode);
+	mutex_lock(&lli->lli_size_mutex);
 }
 
 void ll_inode_size_unlock(struct inode *inode)
 {
-        struct ll_inode_info *lli;
+	struct ll_inode_info *lli;
 
-        lli = ll_i2info(inode);
-        LASSERT(lli->lli_size_sem_owner == current);
-        lli->lli_size_sem_owner = NULL;
-	up(&lli->lli_size_sem);
+	lli = ll_i2info(inode);
+	mutex_unlock(&lli->lli_size_mutex);
 }
 
 void ll_update_inode(struct inode *inode, struct lustre_md *md)
