81e27655e4c9a04b04b2c704bf57754566feaa49
Oleg Drokin <oleg.drokin@intel.com>
2014-02-07 14:08:12 +0000

    Revert "LU-1778 libcfs: add a service that prints a nidlist"
    Whoops, this patch broke build: http://build.whamcloud.com/job/lustre-master/arch=x86_64,build_type=client,distro=ubuntu1004,ib_stack=inkernel/1879/changes
    
    So I am reverting it.
    
    This reverts commit 874f67c06da8304a194df5fc0dd5a2c61937076c.
    
    Change-Id: Ieb36ba5c909bc3731dc4a925d89773be89ab64ec
    Reviewed-on: http://review.whamcloud.com/9178
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Tested-by: Oleg Drokin <oleg.drokin@intel.com>

--- libcfs/libcfs/nidstrings.c
+++ libcfs/libcfs/nidstrings.c
@@ -103,10 +103,6 @@ static void libcfs_hexnum_addr2str(__u32 addr, char *str);
 static int  libcfs_num_str2addr(const char *str, int nob, __u32 *addr);
 static int  libcfs_num_parse(char *str, int len, struct list_head *list);
 static int  libcfs_num_match(__u32 addr, struct list_head *list);
-static int  libcfs_num_addr_range_print(char *buffer, int count,
-					struct list_head *list);
-static int  libcfs_ip_addr_range_print(char *buffer, int count,
-				       struct list_head *list);
 
 struct netstrfns {
 	int	 nf_type;
@@ -116,8 +112,6 @@ struct netstrfns {
 	int	(*nf_str2addr)(const char *str, int nob, __u32 *addr);
 	int	(*nf_parse_addrlist)(char *str, int len,
 					struct list_head *list);
-	int	(*nf_print_addrlist)(char *buffer, int count,
-				     struct list_head *list);
 	int	(*nf_match_addr)(__u32 addr, struct list_head *list);
 };
 
@@ -128,7 +122,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_decnum_addr2str,
 	 /* .nf_str2addr  */  libcfs_lo_str2addr,
 	 /* .nf_parse_addr*/  libcfs_num_parse,
-	 /* .nf_print_addrlist*/  libcfs_num_addr_range_print,
 	 /* .nf_match_addr*/  libcfs_num_match},
 	{/* .nf_type      */  SOCKLND,
 	 /* .nf_name      */  "tcp",
@@ -136,7 +129,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  O2IBLND,
 	 /* .nf_name      */  "o2ib",
@@ -144,7 +136,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  CIBLND,
 	 /* .nf_name      */  "cib",
@@ -152,7 +143,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  OPENIBLND,
 	 /* .nf_name      */  "openib",
@@ -160,7 +150,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  IIBLND,
 	 /* .nf_name      */  "iib",
@@ -168,7 +157,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  VIBLND,
 	 /* .nf_name      */  "vib",
@@ -176,7 +164,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
 	{/* .nf_type      */  RALND,
 	 /* .nf_name      */  "ra",
@@ -184,7 +171,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
         {/* .nf_type      */  QSWLND,
          /* .nf_name      */  "elan",
@@ -192,7 +178,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
          /* .nf_addr2str  */  libcfs_decnum_addr2str,
          /* .nf_str2addr  */  libcfs_num_str2addr,
          /* .nf_parse_addrlist*/  libcfs_num_parse,
-	 /* .nf_print_addrlist*/  libcfs_num_addr_range_print,
          /* .nf_match_addr*/  libcfs_num_match},
         {/* .nf_type      */  GMLND,
          /* .nf_name      */  "gm",
@@ -200,7 +185,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
          /* .nf_addr2str  */  libcfs_hexnum_addr2str,
          /* .nf_str2addr  */  libcfs_num_str2addr,
          /* .nf_parse_addrlist*/  libcfs_num_parse,
-	 /* .nf_print_addrlist*/  libcfs_num_addr_range_print,
          /* .nf_match_addr*/  libcfs_num_match},
 	{/* .nf_type      */  MXLND,
 	 /* .nf_name      */  "mx",
@@ -208,7 +192,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
 	 /* .nf_addr2str  */  libcfs_ip_addr2str,
 	 /* .nf_str2addr  */  libcfs_ip_str2addr,
 	 /* .nf_parse_addrlist*/  cfs_ip_addr_parse,
-	 /* .nf_print_addrlist*/  libcfs_ip_addr_range_print,
 	 /* .nf_match_addr*/  cfs_ip_addr_match},
         {/* .nf_type      */  PTLLND,
          /* .nf_name      */  "ptl",
@@ -216,7 +199,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
          /* .nf_addr2str  */  libcfs_decnum_addr2str,
          /* .nf_str2addr  */  libcfs_num_str2addr,
          /* .nf_parse_addrlist*/  libcfs_num_parse,
-	 /* .nf_print_addrlist*/  libcfs_num_addr_range_print,
          /* .nf_match_addr*/  libcfs_num_match},
         {/* .nf_type      */  GNILND,
          /* .nf_name      */  "gni",
@@ -224,7 +206,6 @@ static struct netstrfns  libcfs_netstrfns[] = {
          /* .nf_addr2str  */  libcfs_decnum_addr2str,
          /* .nf_str2addr  */  libcfs_num_str2addr,
          /* .nf_parse_addrlist*/  libcfs_num_parse,
-	 /* .nf_print_addrlist*/  libcfs_num_addr_range_print,
          /* .nf_match_addr*/  libcfs_num_match},
 	{/* .nf_type      */  GNIIPLND,
 	 /* .nf_name      */  "gip",
@@ -677,8 +658,8 @@ libcfs_num_parse(char *str, int len, struct list_head *list)
  * Allocates struct addrrange and links to \a nidrange via
  * (nidrange::nr_addrranges)
  *
- * \retval 0 if \a src parses to '*' | \<ipaddr_range\> | \<cfs_expr_list\>
- * \retval -errno otherwise
+ * \retval 1 if \a src parses to '*' | \<ipaddr_range\> | \<cfs_expr_list\>
+ * \retval 0 otherwise
  */
 static int
 parse_addrange(const struct cfs_lstr *src, struct nidrange *nidrange)
@@ -687,12 +668,12 @@ parse_addrange(const struct cfs_lstr *src, struct nidrange *nidrange)
 
 	if (src->ls_len == 1 && src->ls_str[0] == '*') {
 		nidrange->nr_all = 1;
-		return 0;
+		return 1;
 	}
 
 	LIBCFS_ALLOC(addrrange, sizeof(struct addrrange));
 	if (addrrange == NULL)
-		return -ENOMEM;
+		return 0;
 	list_add_tail(&addrrange->ar_link, &nidrange->nr_addrranges);
 	INIT_LIST_HEAD(&addrrange->ar_numaddr_ranges);
 
@@ -922,110 +903,6 @@ int cfs_match_nid(lnet_nid_t nid, struct list_head *nidlist)
 	RETURN(0);
 }
 
-static int
-libcfs_num_addr_range_print(char *buffer, int count, struct list_head *list)
-{
-	int i = 0, j = 0;
-	struct cfs_expr_list *el;
-
-	list_for_each_entry(el, list, el_link) {
-		LASSERT(j++ < 1);
-		i += cfs_expr_list_print(buffer + i, count - i, el);
-	}
-	return i;
-}
-
-static int
-libcfs_ip_addr_range_print(char *buffer, int count, struct list_head *list)
-{
-	int i = 0, j = 0;
-	struct cfs_expr_list *el;
-
-	list_for_each_entry(el, list, el_link) {
-		LASSERT(j++ < 4);
-		if (i != 0)
-			i += cfs_snprintf(buffer + i, count - i, ".");
-		i += cfs_expr_list_print(buffer + i, count - i, el);
-	}
-	return i;
-}
-
-
-/**
- * Print the network part of the nidrange \a nr into the specified \a buffer.
- *
- * \retval number of characters written
- */
-static int
-cfs_print_network(char *buffer, int count, struct nidrange *nr)
-{
-	struct netstrfns *nf = nr->nr_netstrfns;
-
-	if (nr->nr_netnum == 0)
-		return cfs_snprintf(buffer, count, "@%s", nf->nf_name);
-	else
-		return cfs_snprintf(buffer, count, "@%s%u",
-				    nf->nf_name, nr->nr_netnum);
-}
-
-
-/**
- * Print a list of addrrange (\a addrranges) into the specified \a buffer.
- * At max \a count characters can be printed into \a buffer.
- *
- * \retval number of characters written
- */
-static int
-cfs_print_addrranges(char *buffer, int count, struct list_head *addrranges,
-		     struct nidrange *nr)
-{
-	int i = 0;
-	struct addrrange *ar;
-	struct netstrfns *nf = nr->nr_netstrfns;
-
-	list_for_each_entry(ar, addrranges, ar_link) {
-		if (i != 0)
-			i += cfs_snprintf(buffer + i, count - i, " ");
-		i += nf->nf_print_addrlist(buffer + i, count - i,
-					   &ar->ar_numaddr_ranges);
-		i += cfs_print_network(buffer + i, count - i, nr);
-	}
-	return i;
-}
-
-
-/**
- * Print a list of nidranges (\a nidlist) into the specified \a buffer.
- * At max \a count characters can be printed into \a buffer.
- * Nidranges are separated by a space character.
- *
- * \retval number of characters written
- */
-int cfs_print_nidlist(char *buffer, int count, struct list_head *nidlist)
-{
-	int i = 0;
-	struct nidrange *nr;
-	ENTRY;
-
-	if (count <= 0)
-		RETURN(0);
-
-	list_for_each_entry(nr, nidlist, nr_link) {
-		if (i != 0)
-			i += cfs_snprintf(buffer + i, count - i, " ");
-
-		if (nr->nr_all != 0) {
-			LASSERT(list_empty(&nr->nr_addrranges));
-			i += cfs_snprintf(buffer + i, count - i, "*");
-			i += cfs_print_network(buffer + i, count - i, nr);
-		} else {
-			i += cfs_print_addrranges(buffer + i, count - i,
-						  &nr->nr_addrranges, nr);
-		}
-	}
-	RETURN(i);
-}
-
 #ifdef __KERNEL__
 
 EXPORT_SYMBOL(libcfs_isknown_lnd);
@@ -1040,7 +917,6 @@ EXPORT_SYMBOL(libcfs_id2str);
 EXPORT_SYMBOL(libcfs_str2anynid);
 EXPORT_SYMBOL(cfs_free_nidlist);
 EXPORT_SYMBOL(cfs_parse_nidlist);
-EXPORT_SYMBOL(cfs_print_nidlist);
 EXPORT_SYMBOL(cfs_match_nid);
 
 #endif
