e2033661f5b078d1056d1ea7c395f12131e2db65
Emoly Liu <emoly.liu@intel.com>
2014-03-03 03:06:56 +0800

    LU-4167 tests: update version check to conf-sanity test_32
    This patch updates version check codes to skip running LFSCK on an
    OST for versions >= 2.5.0 in conf-sanity.sh test_32.
    
    Test-Parameters: testlist=conf-sanity envdefinitions=SLOW=yes, ONLY=32 ossjob=lustre-b2_4 mdsjob=lustre-b2_4 ossbuildno=71 mdsbuildno=71 clientdistro=el6 ossdistro=el6 mdsdistro=el6 clientarch=x86_64 ossarch=x86_64 mdsarch=x86_64
    Signed-off-by: Emoly Liu <emoly.liu@intel.com>
    Change-Id: Ibc0c34a0c15f4882a0dd06dae75d1f1fe10b6f16
    Reviewed-on: http://review.whamcloud.com/8132
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -1623,7 +1623,7 @@ t32_test() {
 	echo "    Arch: $img_arch"
 
 	local version=$(version_code $img_commit)
-	[[ $version -gt $(version_code 2.4.0) ]] && ff_convert="no"
+	[[ $version -ge $(version_code 2.5.0) ]] && ff_convert="no"
 
 	if [ $fstype == "zfs" ]; then
 		# import pool first
