87b61133b627032c12ab4759ea1f96cbc4e6719e
Jinshan Xiong <jinshan.xiong@intel.com>
2014-03-05 22:45:47 -0800

    LU-4591 lov: cancel ungranted sub lock
    When the top lock is canceled due to error, we should cancel
    ungranted sub lock otherwise the sublock state is undefined.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I5c1df5220ce77ff434463d6443135c625e79ea04
    Reviewed-on: http://review.whamcloud.com/9524
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/lov/lov_lock.c
+++ lustre/lov/lov_lock.c
@@ -689,6 +689,7 @@ static int lov_lock_unuse(const struct lu_env *env,
 				lov_sublock_release(env, lck, i, 0, 0);
 				break;
 			default:
+				cl_lock_cancel(subenv->lse_env, sublock);
 				lov_sublock_release(env, lck, i, 1, 0);
 				break;
 			}
@@ -745,6 +746,7 @@ static void lov_lock_cancel(const struct lu_env *env,
                                 lov_sublock_release(env, lck, i, 0, 0);
                                 break;
                         default:
+				cl_lock_cancel(subenv->lse_env, sublock);
                                 lov_sublock_release(env, lck, i, 1, 0);
                                 break;
                         }
