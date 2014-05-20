58c2a357202ba83d26ba9d8f1d9c093e3272e9a0
Hongchao Zhang <hongchao.zhang@intel.com>
2014-05-20 11:38:52 +0800

    LU-3326 tests: sync after file deleted in run_*.sh
    In run_*.sh, call sync to flush any dirty pages to OST
    if MDT has not sent the object destroy request to it,
    which will shrink the available disk space gotten before
    running the tests and cause -EIO error.
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>
    Change-Id: Ia246122a5c3471b7cbea9cdaa7fad2ea4db856c5
    Test-Parameters: alwaysuploadlogs \
    envdefinitions=SLOW=yes,ENABLE_QUOTA=yes,REQFAIL=100,DURATION=28800 \
    clientdistro=el6 ossdistro=el6 mdsdistro=el6 clientarch=x86_64 \
    ossarch=x86_64 mdsarch=x86_64 clientcount=4 osscount=2 mdscount=2 \
    mdssizegb=10 ostsizegb=10 austeroptions=-R failover=true iscsi=1 \
    testlist=recovery-mds-scale
    Reviewed-on: http://review.whamcloud.com/8071
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/run_iozone.sh
+++ lustre/tests/run_iozone.sh
@@ -28,32 +28,36 @@ TESTDIR=$MOUNT/d0.iozone-$(hostname)
 
 CONTINUE=true
 while [ ! -e "$END_RUN_FILE" ] && $CONTINUE; do
-    echoerr "$(date +'%F %H:%M:%S'): iozone run starting"
-    mkdir -p $TESTDIR
-    cd $TESTDIR
-    iozone -a -M -R -V 0xab -g 100M -q 512k -i0 -i1 -f $TESTDIR/iozone-file 1>$LOG &
-    load_pid=$!
-    wait $load_pid
-    if [ ${PIPESTATUS[0]} -eq 0 ]; then
-        echoerr "$(date +'%F %H:%M:%S'): iozone succeeded"
-        cd $TMP
-        rm -rf $TESTDIR
-        if [ -d $TESTDIR ]; then
-            echoerr "$(date +'%F %H:%M:%S'): failed to remove $TESTDIR"
-            echo $(hostname) >> $END_RUN_FILE
-            CONTINUE=false
-        fi
-        echoerr "$(date +'%F %H:%M:%S'): iozone run finished"
-    else
-        echoerr "$(date +'%F %H:%M:%S'): iozone failed"
-        if [ -z "$ERRORS_OK" ]; then
-            echo $(hostname) >> $END_RUN_FILE
-        fi
-        if [ $BREAK_ON_ERROR ]; then
-            # break
-            CONTINUE=false
-        fi
-    fi
+	echoerr "$(date +'%F %H:%M:%S'): iozone run starting"
+	mkdir -p $TESTDIR
+	cd $TESTDIR
+
+	sync
+	iozone -a -M -R -V 0xab -g 100M -q 512k -i0 -i1 \
+					-f $TESTDIR/iozone-file 1>$LOG &
+	load_pid=$!
+	wait $load_pid
+	if [ ${PIPESTATUS[0]} -eq 0 ]; then
+		echoerr "$(date +'%F %H:%M:%S'): iozone succeeded"
+		cd $TMP
+		rm -rf $TESTDIR
+		if [ -d $TESTDIR ]; then
+			echoerr "$(date +'%F %H:%M:%S'): failed to remove \
+								 $TESTDIR"
+			echo $(hostname) >> $END_RUN_FILE
+			CONTINUE=false
+		fi
+		echoerr "$(date +'%F %H:%M:%S'): iozone run finished"
+	else
+		echoerr "$(date +'%F %H:%M:%S'): iozone failed"
+		if [ -z "$ERRORS_OK" ]; then
+			echo $(hostname) >> $END_RUN_FILE
+		fi
+		if [ $BREAK_ON_ERROR ]; then
+			# break
+			CONTINUE=false
+		fi
+	fi
 done
 
 echoerr "$(date +'%F %H:%M:%S'): iozone run exiting"
