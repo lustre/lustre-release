bdc5bb52c55470cf8020933f80e327c397810603
Hongchao Zhang <hongchao.zhang@intel.com>
2014-05-22 12:26:25 +0800

    LU-4933 osc: Automatically increase the max_dirty_mb
    When RPC size or the max RPCs in flight is increased, the actual
    limit might be max_dirty_mb. This patch automatically increases
    the max_dirty_mb value at connection time and when the related
    values are tuned manually by proc file system.
    
    this patch also changes the unit of "cl_dirty" and "cl_dirty_max"
    in client_obd from byte to page.
    
    Change-Id: Ieaea7660caa8008365b38f548b4ecba4027e080b
    Signed-off-by: Li Xi <lixi@ddn.com>
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>
    Reviewed-on: http://review.whamcloud.com/10446
    Tested-by: Jenkins
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- lustre/osc/lproc_osc.c
+++ lustre/osc/lproc_osc.c
@@ -109,12 +109,13 @@ static ssize_t osc_max_rpcs_in_flight_seq_write(struct file *file,
         if (pool && val > cli->cl_max_rpcs_in_flight)
                 pool->prp_populate(pool, val-cli->cl_max_rpcs_in_flight);
 
-        client_obd_list_lock(&cli->cl_loi_list_lock);
-        cli->cl_max_rpcs_in_flight = val;
-        client_obd_list_unlock(&cli->cl_loi_list_lock);
+	client_obd_list_lock(&cli->cl_loi_list_lock);
+	cli->cl_max_rpcs_in_flight = val;
+	client_adjust_max_dirty(cli);
+	client_obd_list_unlock(&cli->cl_loi_list_lock);
 
-        LPROCFS_CLIMP_EXIT(dev);
-        return count;
+	LPROCFS_CLIMP_EXIT(dev);
+	return count;
 }
 LPROC_SEQ_FOPS(osc_max_rpcs_in_flight);
 
@@ -125,11 +126,11 @@ static int osc_max_dirty_mb_seq_show(struct seq_file *m, void *v)
         long val;
         int mult;
 
-        client_obd_list_lock(&cli->cl_loi_list_lock);
-        val = cli->cl_dirty_max;
-        client_obd_list_unlock(&cli->cl_loi_list_lock);
+	client_obd_list_lock(&cli->cl_loi_list_lock);
+	val = cli->cl_dirty_max_pages;
+	client_obd_list_unlock(&cli->cl_loi_list_lock);
 
-	mult = 1 << 20;
+	mult = 1 << (20 - PAGE_CACHE_SHIFT);
 	return lprocfs_seq_read_frac_helper(m, val, mult);
 }
 
@@ -151,7 +152,7 @@ static ssize_t osc_max_dirty_mb_seq_write(struct file *file, const char *buffer,
 		return -ERANGE;
 
 	client_obd_list_lock(&cli->cl_loi_list_lock);
-	cli->cl_dirty_max = (obd_count)(pages_number << PAGE_CACHE_SHIFT);
+	cli->cl_dirty_max_pages = pages_number;
 	osc_wake_cache_waiters(cli);
 	client_obd_list_unlock(&cli->cl_loi_list_lock);
 
@@ -226,7 +227,7 @@ static int osc_cur_dirty_bytes_seq_show(struct seq_file *m, void *v)
 	int rc;
 
 	client_obd_list_lock(&cli->cl_loi_list_lock);
-	rc = seq_printf(m, "%lu\n", cli->cl_dirty);
+	rc = seq_printf(m, "%lu\n", cli->cl_dirty_pages << PAGE_CACHE_SHIFT);
 	client_obd_list_unlock(&cli->cl_loi_list_lock);
 	return rc;
 }
@@ -511,6 +512,7 @@ static ssize_t osc_obd_max_pages_per_rpc_seq_write(struct file *file,
 	}
 	client_obd_list_lock(&cli->cl_loi_list_lock);
 	cli->cl_max_pages_per_rpc = val;
+	client_adjust_max_dirty(cli);
 	client_obd_list_unlock(&cli->cl_loi_list_lock);
 
 	LPROCFS_CLIMP_EXIT(dev);
