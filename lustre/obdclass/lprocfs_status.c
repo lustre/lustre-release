a66c72c37140e17f600f9cab8f8f3b09374a96c4
Stephen Champion <schamp@sgi.com>
2014-05-30 17:35:45 -0700

    LU-5123 procfs: u64 helper does not respect multiplier
    lprocfs_write_frac_u64_helper should apply the multiplier argument to
    the value if units are not specified by the user.  It has been
    overriding the multiplier with '1' regardless.
    
    Signed-off-by: Stephen Champion <schamp@sgi.com>
    Change-Id: Iaacbb6923122479ba1ba4a5ee0088c670245d081
    Reviewed-on: http://review.whamcloud.com/10535
    Tested-by: Jenkins
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: James Simmons <uja.ornl@gmail.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Keith Mannthey <keith.mannthey@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/obdclass/lprocfs_status.c
+++ lustre/obdclass/lprocfs_status.c
@@ -2487,7 +2487,7 @@ int lprocfs_write_frac_u64_helper(const char *buffer, unsigned long count,
 		}
 	}
         /* Specified units override the multiplier */
-        if (units)
+	if (units > 1)
                 mult = mult < 0 ? -units : units;
 
         frac *= mult;
