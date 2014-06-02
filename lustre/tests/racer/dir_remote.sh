2f55405983bc78a195c10ce888747df63ee4617b
Patrick Farrell <paf@cray.com>
2014-06-02 10:24:03 -0500

    LU-5093 test: dir_remote.sh change stripe count
    dir_remote.sh currently creates only directories with
    $MDTCOUNT stripes. This patch makes it create directories
    with from 1 to $MDTCOUNT stripes, which also creates
    non-striped remote directories (stripe_count = 1).
    
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Change-Id: I8334661b40620c1e0ecc03dd29756fb607e3e3ca
    Reviewed-on: http://review.whamcloud.com/10390
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/tests/racer/dir_remote.sh
+++ lustre/tests/racer/dir_remote.sh
@@ -8,8 +8,11 @@ while /bin/true ; do
 	remote_dir=$((RANDOM % MAX))
 	file=$((RANDOM % MAX))
 	mdt_idx=$((RANDOM % MDTCOUNT))
+	# stripe_count in range [1,MDTCOUNT]
+	# lfs mkdir treats stripe_count 0 and 1 the same
+	stripe_count=$(((RANDOM % MDTCOUNT) + 1))
 	mkdir -p $DIR 2> /dev/null
-	$LFS mkdir -i$mdt_idx -c$MDTCOUNT $DIR/$remote_dir 2> /dev/null
+	$LFS mkdir -i$mdt_idx -c$stripe_count $DIR/$remote_dir 2> /dev/null
 	touch $DIR/$remote_dir/$file 2> /dev/null
 	$LFS getdirstripe $DIR/$remote_dir > /dev/null 2>&1
 done
