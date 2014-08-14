7e8275b1669d5df047790f6ca2e03ba203634b11
Wu Libin <lwu@ddn.com>
2014-08-14 14:27:21 -0700

    LU-5128 obdclass: fix a race in recovery
    in "class_export_recovery_cleanup", the check of the flag
    "exp->exp_req_replay_needed" should be protected by "exp_lock".
    
    Lustre-commit: 5a7e4480fc519c8df4ca0b11e6d5239956316bd3
    Lustre-change: http://review.whamcloud.com/10849
    
    Signed-off-by: Hongchao Zhang <hongchao.zhang@intel.com>
    Signed-off-by: Wu Libin <lwu@ddn.com>
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Change-Id: Ib3c980101f400ce1b9cb0b3d2a4418fbec31b94b
    Reviewed-on: http://review.whamcloud.com/11102
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/obdclass/genops.c
+++ lustre/obdclass/genops.c
@@ -1171,21 +1171,20 @@ void class_export_recovery_cleanup(struct obd_export *exp)
 	}
 	spin_unlock(&obd->obd_recovery_task_lock);
 	/** Cleanup req replay fields */
+
+	spin_lock(&exp->exp_lock);
 	if (exp->exp_req_replay_needed) {
-		spin_lock(&exp->exp_lock);
 		exp->exp_req_replay_needed = 0;
-		spin_unlock(&exp->exp_lock);
 		LASSERT(cfs_atomic_read(&obd->obd_req_replay_clients));
 		cfs_atomic_dec(&obd->obd_req_replay_clients);
 	}
 	/** Cleanup lock replay data */
 	if (exp->exp_lock_replay_needed) {
-		spin_lock(&exp->exp_lock);
 		exp->exp_lock_replay_needed = 0;
-		spin_unlock(&exp->exp_lock);
 		LASSERT(cfs_atomic_read(&obd->obd_lock_replay_clients));
 		cfs_atomic_dec(&obd->obd_lock_replay_clients);
 	}
+	spin_unlock(&exp->exp_lock);
 }
 
 /* This function removes 1-3 references from the export:
