26de2a56803ce20f6ae21dac650d598e4335f247
James Nunez <james.a.nunez@intel.com>
2015-02-13 10:12:30 -0700

    LU-2828 test: Remove tests from ALWAYS_EXCEPT list
    conf-sanity tests 59 and 64 were added to the ALWAYS_EXCEPT list
    in commit b2c829b7be757cd2bc523ab0d2857a77eeb7a349 for LU-2469.
    
    Commit 1e7845ecbe5f3e8ac1aa0d3e345e6cf6cf6f0543, for LU-2828, resolves
    the cause of the conf-sanity test 59 and 64 failures.
    
    conf-sanity test 59 and 64 need to be removed from the except list.
    
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Change-Id: I4b70485f91e0096c2e4387ebcdc95cf5720a7e16
    Reviewed-on: http://review.whamcloud.com/13757
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Faccini Bruno <bruno.faccini@intel.com>
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -4,8 +4,8 @@ set -e
 
 ONLY=${ONLY:-"$*"}
 
-# bug number for skipped test:     LU-2828
-ALWAYS_EXCEPT="$CONF_SANITY_EXCEPT 59 64"
+# bug number for skipped test:
+ALWAYS_EXCEPT="$CONF_SANITY_EXCEPT"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 is_sles11()						# LU-2181
