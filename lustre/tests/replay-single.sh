43af9a3481e980e267a94c9f466ff2b300f619d9
James Nunez <james.a.nunez@intel.com>
2015-03-23 16:46:19 -0600

    LU-6319 tests: Clean up replay-single ALWAYS_EXCEPT list
    Several tests in replay-single were added to the ALWAYS_EXCEPT list
    when a bug was making the test fail repeatedly. For tests 33a, 33b,
    and 62, the tickets were closed, but the tests were never
    re-activated.
    
    Remove replay-single tests 33a, 33b, and 62 from the
    ALWAYS_EXCEPT list.
    
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Change-Id: I872af58ca6fb7b362b4ffe146132d98262a280a4
    Reviewed-on: http://review.whamcloud.com/14149
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Wei Liu <wei3.liu@intel.com>
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/replay-single.sh
+++ lustre/tests/replay-single.sh
@@ -21,8 +21,9 @@ GRANT_CHECK_LIST=${GRANT_CHECK_LIST:-""}
 require_dsh_mds || exit 0
 
 # Skip these tests
-# bug number:  17466 18857      LU-1473
-ALWAYS_EXCEPT="61d   33a 33b    62	$REPLAY_SINGLE_EXCEPT"
+# bug number for skipped tests: b=17466/LU-472
+ALWAYS_EXCEPT="                 61d	$REPLAY_SINGLE_EXCEPT"
+# UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 #                                                  63 min  7 min  AT AT AT AT"
 [ "$SLOW" = "no" ] && EXCEPT_SLOW="1 2 3 4 6 12 16 44a      44b    65 66 67 68"
