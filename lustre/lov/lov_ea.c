7f75a4d5b955072c426f1723ff86bb7d67f55f19
Bobi Jam <bobijam.xu@intel.com>
2015-06-20 11:07:39 +0800

    LU-6744 lov: init LOV stripe type beforehand
    When lu_object_alloc() reaches to LOV object init, we need initialize
    its stripe type beforehand, so that if something wrong in the conf
    buffer, the object chain need to be traversed to free what has been
    allocated, with LOV object type be set as LLT_EMPTY, and when the LOV
    part is reached, it won't panic without knowing what stripe type it
    is.
    
    This patch also improves debug messages in lsm_unpackmd_common(), and
    does not return error if the LOV device is still processing config
    log while trying to verify a layout buffer.
    
    Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
    Change-Id: Ic814f89ebc960cc4c04835141d915f86c35ca39d
    Reviewed-on: http://review.whamcloud.com/15362
    Tested-by: Jenkins
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

--- lustre/lov/lov_ea.c
+++ lustre/lov/lov_ea.c
@@ -181,17 +181,20 @@ static int lsm_unpackmd_common(struct lov_obd *lov,
 		if (lov_oinfo_is_dummy(loi))
 			continue;
 
-		if (loi->loi_ost_idx >= lov->desc.ld_tgt_count) {
-			CERROR("OST index %d more than OST count %d\n",
+		if (loi->loi_ost_idx >= lov->desc.ld_tgt_count &&
+		    !lov2obd(lov)->obd_process_conf) {
+			CERROR("%s: OST index %d more than OST count %d\n",
+			       (char*)lov->desc.ld_uuid.uuid,
 			       loi->loi_ost_idx, lov->desc.ld_tgt_count);
 			lov_dump_lmm_v1(D_WARNING, lmm);
 			return -EINVAL;
 		}
 
-		if (!lov->lov_tgts[loi->loi_ost_idx]) {
-			CERROR("OST index %d missing\n", loi->loi_ost_idx);
+		if (lov->lov_tgts[loi->loi_ost_idx] == NULL) {
+			CERROR("%s: OST index %d missing\n",
+			       (char*)lov->desc.ld_uuid.uuid, loi->loi_ost_idx);
 			lov_dump_lmm_v1(D_WARNING, lmm);
-			return -EINVAL;
+			continue;
 		}
 
 		stripe_maxbytes = min_t(loff_t, stripe_maxbytes,
