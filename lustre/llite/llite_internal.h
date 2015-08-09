be3c67ff1f55930f3c0871b2584c610805edb3d4
Oleg Drokin <oleg.drokin@intel.com>
2015-08-08 20:02:06 -0400

    LU-6972 llite: get rid of unused ll_super_blocks list
    ll_super_blocks became unused quite a while ago with switch
    to the new CLIO code.
    So this patch removes the list, ll_sb_lock spinlock that guards it
    and superblock info ll_list linkage.
    
    Change-Id: If0af4f4c05f45feaedc54ca57f0d1106ad7cabe3
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: http://review.whamcloud.com/15922
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>

--- lustre/llite/llite_internal.h
+++ lustre/llite/llite_internal.h
@@ -509,7 +509,6 @@ struct lustre_client_ocd {
 };
 
 struct ll_sb_info {
-	struct list_head		  ll_list;
 	/* this protects pglist and ra_info.  It isn't safe to
 	 * grab from interrupt contexts */
 	spinlock_t		  ll_lock;
--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -63,9 +63,6 @@
 
 struct kmem_cache *ll_file_data_slab;
 
-static struct list_head ll_super_blocks = LIST_HEAD_INIT(ll_super_blocks);
-static DEFINE_SPINLOCK(ll_sb_lock);
-
 #ifndef log2
 #define log2(n) ffz(~(n))
 #endif
@@ -113,10 +110,6 @@ static struct ll_sb_info *ll_init_sbi(void)
         class_uuid_unparse(uuid, &sbi->ll_sb_uuid);
         CDEBUG(D_CONFIG, "generated uuid: %s\n", sbi->ll_sb_uuid.uuid);
 
-	spin_lock(&ll_sb_lock);
-	list_add_tail(&sbi->ll_list, &ll_super_blocks);
-	spin_unlock(&ll_sb_lock);
-
         sbi->ll_flags |= LL_SBI_VERBOSE;
 #ifdef ENABLE_CHECKSUM
         sbi->ll_flags |= LL_SBI_CHECKSUM;
@@ -156,9 +149,6 @@ static void ll_free_sbi(struct super_block *sb)
 	ENTRY;
 
 	if (sbi != NULL) {
-		spin_lock(&ll_sb_lock);
-		list_del(&sbi->ll_list);
-		spin_unlock(&ll_sb_lock);
 		if (!list_empty(&sbi->ll_squash.rsi_nosquash_nids))
 			cfs_free_nidlist(&sbi->ll_squash.rsi_nosquash_nids);
 		if (sbi->ll_cache != NULL) {
