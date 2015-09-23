82cbfd77f33bc33ea047407dfaecf4b04d44930a
Mikhail Pershin <mike.pershin@intel.com>
2015-09-23 12:09:27 -0700

    LU-7012 osp: don't use OSP when import is deactivated
    Unset opd_imp_connected flag upon IMP_EVENT_INACTIVE event,
    it will stop any llog processing by that device until import
    will be activated again.
    
    Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
    Change-Id: Ie219e536c216130f428ba933d11842511692c95b
    Reviewed-on: http://review.whamcloud.com/16937
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Jian Yu <jian.yu@intel.com>
    Tested-by: Jenkins
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/osp/osp_dev.c
+++ lustre/osp/osp_dev.c
@@ -1586,6 +1586,7 @@ static int osp_import_event(struct obd_device *obd, struct obd_import *imp,
 		break;
 	case IMP_EVENT_INACTIVE:
 		d->opd_imp_active = 0;
+		d->opd_imp_connected = 0;
 		if (d->opd_connect_mdt)
 			break;
 
