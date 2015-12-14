07146e09be0478b4f8293ed7065b2722c85b1659
Jeremy Filizetti <jeremy.filizetti@gmail.com>
2015-12-13 23:39:11 -0500

    LU-3289 osp: osp_precreate_thread does not check for errors
    If servers do not have a valid key for GSS the call to
    osp_statfs_update() can fail.  This patch breaks the loop in the
    event of a failure from osp_statfs_update().
    
    Signed-off-by: Jeremy Filizetti <jeremy.filizetti@gmail.com>
    Change-Id: I576cff86d285c4263e8d98cccb9b3c6943af6f75
    Reviewed-on: http://review.whamcloud.com/17598
    Tested-by: Jenkins
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/osp/osp_precreate.c
+++ lustre/osp/osp_precreate.c
@@ -1084,6 +1084,8 @@ static int osp_precreate_thread(void *_arg)
 	struct osp_device	*d = _arg;
 	struct ptlrpc_thread	*thread = &d->opd_pre_thread;
 	struct l_wait_info	 lwi = { 0 };
+	struct l_wait_info	 lwi2 = LWI_TIMEOUT(cfs_time_seconds(5),
+						    back_to_sleep, NULL);
 	struct lu_env		 env;
 	int			 rc;
 
@@ -1137,7 +1139,11 @@ static int osp_precreate_thread(void *_arg)
 			continue;
 		}
 
-		osp_statfs_update(d);
+		if (osp_statfs_update(d)) {
+			l_wait_event(d->opd_pre_waitq,
+				     !osp_precreate_running(d), &lwi2);
+			continue;
+		}
 
 		/*
 		 * Clean up orphans or recreate missing objects.
@@ -1164,7 +1170,8 @@ static int osp_precreate_thread(void *_arg)
 				break;
 
 			if (osp_statfs_need_update(d))
-				osp_statfs_update(d);
+				if (osp_statfs_update(d))
+					break;
 
 			/* To avoid handling different seq in precreate/orphan
 			 * cleanup, it will hold precreate until current seq is
