c683c00408bc56bb68ab44d8bd21eaceb75fe86a
James Simmons <uja.ornl@yahoo.com>
2015-12-23 12:45:45 -0500

    LU-6401 uapi: change uapi_kernelcomm.h to LGPLv2.1 license
    The header uapi_kernelcomm.h need their license to be
    changed so it can be used by LGPL library HSM code.
    
    Change-Id: I4ea4fd3017e9e8e286ad9d13ff6b6979cf71359b
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: http://review.whamcloud.com/17720
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Nathan Rutman <nathan.rutman@seagate.com>
    Reviewed-by: Henri Doreau <henri.doreau@cea.fr>
    Reviewed-by: Frank Zago <fzago@cray.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/include/uapi_kernelcomm.h
+++ lustre/include/uapi_kernelcomm.h
@@ -1,23 +1,22 @@
 /*
- * GPL HEADER START
+ * LGPL HEADER START
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
  *
- * GPL HEADER END
+ * LGPL HEADER END
  */
 /*
  * Copyright (c) 2009, 2010, Oracle and/or its affiliates. All rights reserved.
@@ -28,7 +27,7 @@
 /*
  * This file is part of Lustre, http://www.lustre.org/
  *
- * Author: Nathan Rutman <nathan.rutman@sun.com>
+ * Author: Nathan Rutman <nathan.rutman@seagate.com>
  *
  * Kernel <-> userspace communication routines.
  * The definitions below are used in the kernel and userspace.
