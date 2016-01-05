ad77bcfd14b662ef86c71f812b11cfc203a4543f
James Simmons <uja.ornl@yahoo.com>
2016-01-04 21:55:07 -0500

    LU-6613 lnet: change lib-dlc.h to LGPLv2.1 license
    Change the lib-dlc.h header to the proper LGPLv2.1 license
    
    Change-Id: Ic152d0722ec775d57cf7702058e6772ce2f5ff8a
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: http://review.whamcloud.com/17697
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lnet/include/lnet/lib-dlc.h
+++ lnet/include/lnet/lib-dlc.h
@@ -3,12 +3,10 @@
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Copyright (c) 2014, Intel Corporation.
- *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,8 +18,12 @@
  *
  * LGPL HEADER END
  *
- * Contributers:
- *   Amir Shehata
+ */
+/*
+ * Copyright (c) 2014, Intel Corporation.
+ */
+/*
+ * Author: Amir Shehata <amir.shehata@intel.com>
  */
 
 #ifndef LNET_DLC_H
