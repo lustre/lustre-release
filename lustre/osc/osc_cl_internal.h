f73c9fe598c60558282ec23a124ed41cbfee1558
Dmitry Eremin <dmitry.eremin@intel.com>
2016-03-29 20:37:02 +0300

    LU-7258 osc: fix signed one bit field
    Bit field 'oi_lockless' and 'oi_is_active' has one bit and is signed.
    
    Change-Id: Ib2fb92faf29ced057438a090dad382258eb9fc40
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-on: http://review.whamcloud.com/19196
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Frank Zago <fzago@cray.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/osc/osc_cl_internal.h
+++ lustre/osc/osc_cl_internal.h
@@ -65,7 +65,7 @@ struct osc_io {
 	/** super class */
 	struct cl_io_slice oi_cl;
 	/** true if this io is lockless. */
-	int                oi_lockless:1,
+	unsigned int	   oi_lockless:1,
 	/** true if this io is counted as active IO */
 			   oi_is_active:1;
 	/** how many LRU pages are reserved for this IO */
