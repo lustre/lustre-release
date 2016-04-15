f5c7fec23cb26219d959290a4a311119747cc609
Doug Oucharek <doug.s.oucharek@intel.com>
2016-04-15 11:37:54 -0700

    LU-8022 lnet: Don't access NULL NI on failure path
    In kiblnd_passive_connect(), if we are failing the connection
    attempt because we cannot find a valid NI (we have a NULL NI),
    we were coring after the "goto fail" because the failure
    path was assuming non-NULL NI.
    
    This patch ensures we don't dereference a NULL NI on that
    failure path.
    
    Signed-off-by: Doug Oucharek <doug.s.oucharek@intel.com>
    Change-Id: I4f3deb78b4d3e9d81551ba915ceee852ec3b0792
    Reviewed-on: http://review.whamcloud.com/19614
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Matt Ezell <ezellma@ornl.gov>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -2513,12 +2513,14 @@ kiblnd_passive_connect(struct rdma_cm_id *cmid, void *priv, int priv_nob)
         return 0;
 
  failed:
-        if (ni != NULL)
-                lnet_ni_decref(ni);
+	if (ni != NULL) {
+		lnet_ni_decref(ni);
+		rej.ibr_cp.ibcp_queue_depth =
+			kiblnd_msg_queue_size(version, ni);
+		rej.ibr_cp.ibcp_max_frags   = kiblnd_rdma_frags(version, ni);
+	}
 
 	rej.ibr_version = version;
-	rej.ibr_cp.ibcp_queue_depth = kiblnd_msg_queue_size(version, ni);
-	rej.ibr_cp.ibcp_max_frags   = kiblnd_rdma_frags(version, ni);
 	kiblnd_reject(cmid, &rej);
 
 	return -ECONNREFUSED;
