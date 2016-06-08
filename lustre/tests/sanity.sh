d6fd48fe7357a8056159e48442ffff5a2fd2d59d
Saurabh Tandan <saurabh.tandan@intel.com>
2016-06-08 16:45:19 -0700

    LU-2805 tests: sanity -Remove test_184c from Always Except
    For sanity.sh removing test_184c from the ALWAYS_EXCEPT
    list.
    
    Test-Parameters: trivial mdtfilesystemtype=zfs ostfilesystemtype=zfs mdsfilesystemtype=zfs envdefinitions=SLOW=yes testlist=sanity,sanity,sanity,sanity
    Signed-off-by: Saurabh Tandan <saurabh.tandan@intel.com>
    Change-Id: Iba0b5b0ff2613e94c1cc90921face828605d05cb
    Reviewed-on: http://review.whamcloud.com/20104
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: James Nunez <james.a.nunez@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -79,8 +79,8 @@ init_logging
 [ "$SLOW" = "no" ] && EXCEPT_SLOW="24D 27m 64b 68 71 115 300o"
 
 if [ $(facet_fstype $SINGLEMDS) = "zfs" ]; then
-	# bug number for skipped test: LU-4536 LU-1957 LU-2805
-	ALWAYS_EXCEPT="$ALWAYS_EXCEPT  65ic    180     184c"
+	# bug number for skipped test: LU-4536 LU-1957
+	ALWAYS_EXCEPT="$ALWAYS_EXCEPT  65ic    180"
 	#                                               4   13    (min)"
 	[ "$SLOW" = "no" ] && EXCEPT_SLOW="$EXCEPT_SLOW 51b 51ba"
 fi
