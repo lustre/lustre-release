9d8469694738288fcd246a1e9f22c47918a32c58
Dmitry Eremin <dmitry.eremin@intel.com>
2016-07-06 14:31:08 +0300

    LU-8372 llite: Fix suspicious dereference of pointer 'vma->vm_file'
    Remove useless LASSERT(vma->vm_file) because of if it's NULL it
    will crash early in file_inode(vma->vm_file).
    
    Test-Parameters: trivial
    
    Change-Id: I309127fae03a77020883a0e190c4fa6ea341dfee
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-on: http://review.whamcloud.com/21171
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/llite/llite_mmap.c
+++ lustre/llite/llite_mmap.c
@@ -448,7 +448,6 @@ static void ll_vm_open(struct vm_area_struct * vma)
 	struct vvp_object *vob = cl_inode2vvp(inode);
 
 	ENTRY;
-	LASSERT(vma->vm_file);
 	LASSERT(atomic_read(&vob->vob_mmap_cnt) >= 0);
 	atomic_inc(&vob->vob_mmap_cnt);
 	EXIT;
@@ -463,7 +462,6 @@ static void ll_vm_close(struct vm_area_struct *vma)
 	struct vvp_object *vob   = cl_inode2vvp(inode);
 
 	ENTRY;
-	LASSERT(vma->vm_file);
 	atomic_dec(&vob->vob_mmap_cnt);
 	LASSERT(atomic_read(&vob->vob_mmap_cnt) >= 0);
 	EXIT;
