fa722ccf04fe6c386628ea5a575165c173643800
Oleg Drokin <oleg.drokin@intel.com>
2016-12-25 16:09:07 -0500

    LU-8972 tests: Disable conf-sanity test 101
    It causes a lot of failures for apparently no good reason.
    
    Change-Id: I0e29c3274b18eece1249e79bed2b58edc493836f
    Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: https://review.whamcloud.com/24517
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -4,8 +4,8 @@ set -e
 
 ONLY=${ONLY:-"$*"}
 
-# bug number for skipped test:
-ALWAYS_EXCEPT="$CONF_SANITY_EXCEPT"
+# bug number for skipped test:  LU-8972
+ALWAYS_EXCEPT="$CONF_SANITY_EXCEPT 101"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 is_sles11()						# LU-2181
