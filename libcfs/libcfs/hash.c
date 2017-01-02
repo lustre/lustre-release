8a13ffb4a1eafff18cb891fdc812afaf09136f29
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-01-01 19:19:16 -0500

    LU-4423 libcfs: Use swap() in cfs_hash_bd_order()
    Use swap() function instead of using a temporary variable for swapping
    two variables.
    
    The Coccinelle semantic patch used to make this change is as follows:
    //<smpl>
    @@
    type T;
    T a,b,c;
    @@
    - a = b;
    - b = c;
    - c = a;
    + swap(b, c);
    //<smpl>
    
    Signed-off-by: Amitoj Kaur Chawla <amitoj1606@gmail.com>
    Change-Id: I19aa52fe4e05fed2e03c1d8515731a5ce01b3d09
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-on: https://review.whamcloud.com/24576
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>

--- libcfs/libcfs/hash.c
+++ libcfs/libcfs/hash.c
@@ -798,12 +798,8 @@ cfs_hash_bd_order(struct cfs_hash_bd *bd1, struct cfs_hash_bd *bd2)
         if (rc == 0) {
                 bd2->bd_bucket = NULL;
 
-        } else if (rc > 0) { /* swab bd1 and bd2 */
-		struct cfs_hash_bd tmp;
-
-                tmp = *bd2;
-                *bd2 = *bd1;
-                *bd1 = tmp;
+	} else if (rc > 0) {
+		swap(*bd1, *bd2); /* swab bd1 and bd2 */
         }
 }
 
