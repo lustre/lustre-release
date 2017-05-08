9b74a75ecbdccb084eb6a747dfda78c6fb7ef176
Jinshan Xiong <jinshan.xiong@intel.com>
2017-05-08 13:22:13 -0700

    LU-9771 lov: pack lsm_flags from layout
    Pack lsm_flags from compount layout.
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I39cb0fe319c1665ef9d12ff704931c4583d730ff
    Reviewed-on: https://review.whamcloud.com/29081
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Lai Siyao <lai.siyao@intel.com>

--- lustre/lov/lov_internal.h
+++ lustre/lov/lov_internal.h
@@ -80,8 +80,9 @@ struct lov_stripe_md {
 	struct ost_id	lsm_oi;
 	u32		lsm_magic;
 	u32		lsm_layout_gen;
-	u32		lsm_entry_count;
+	u16		lsm_flags;
 	bool		lsm_is_released;
+	u32		lsm_entry_count;
 	struct lov_stripe_md_entry *lsm_entries[];
 };
 
