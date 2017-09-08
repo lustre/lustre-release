38c78e21ca7abe4bd6e6500d88a692e7a56a7712
dilip krishnagiri <dilipx.krishnagiri@intel.com>
2017-09-08 12:30:57 -0600

    LU-9963 test: add parallel-scale tests to ALWAYS_EXCEPT
    add the following parallel-scale tests
    
    parallel_grouplock :
           test_parallel_grouplock: test failed to respond and timed out
    Associated Jira ticket LU-9429 is in open state.
    to ALWAYS_EXCEPT list.
    
    Test-Parameters: trivial testlist=parallel-scale
    
    Change-Id: I25709af1ab49a30498a89e5369521582c5ab6cf8
    Signed-off-by: dilip krishnagiri <dilipx.krishnagiri@intel.com>
    Reviewed-on: https://review.whamcloud.com/28914
    Tested-by: Jenkins
    Reviewed-by: James Nunez <james.a.nunez@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Casper <jamesx.casper@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/parallel-scale.sh
+++ lustre/tests/parallel-scale.sh
@@ -8,8 +8,8 @@ init_test_env $@
 . ${CONFIG:=$LUSTRE/tests/cfg/$NAME.sh}
 init_logging
 
-#              bug 20670
-ALWAYS_EXCEPT="$PARALLEL_SCALE_EXCEPT"
+# bug number for skipped test:  LU-9429
+     ALWAYS_EXCEPT="            parallel_grouplock  $PARALLEL_SCALE_EXCEPT "
 
 if [ $(facet_fstype $SINGLEMDS) = zfs -o $(facet_fstype "ost1") = zfs ]; then
 	ZFSSLOW=$SLOW
