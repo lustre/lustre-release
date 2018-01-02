d645c722d73c8c4226ab5695d37a541d39bb0a64
James Nunez <james.a.nunez@intel.com>
2018-01-02 15:56:11 -0700

    LU-10406 tests: stop running sanity-lfsck 31c
    sanity-lfsck is failing about half the time it is run.
    Add sanity-lfsck test 31c to the ALWAYS_EXCEPT list until
    we understand and fix the issue.
    
    Test-Parameters: trivial
    Signed-off-by: James Nunez <james.a.nunez@intel.com>
    Change-Id: Iace77e1b1ea59971548497063974628be9e70733
    Reviewed-on: https://review.whamcloud.com/30694
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/tests/sanity-lfsck.sh
+++ lustre/tests/sanity-lfsck.sh
@@ -8,8 +8,8 @@ set -e
 
 ONLY=${ONLY:-"$*"}
 
-#Bug number for excepting test
-ALWAYS_EXCEPT="$SANITY_LFSCK_EXCEPT"
+#Bug number for excepting test      LU-10406
+ALWAYS_EXCEPT="$SANITY_LFSCK_EXCEPT 31c"
 
 [ "$SLOW" = "no" ] && EXCEPT_SLOW=""
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
