3281d5d57cec9d6deaa50cb4d9ec9509e3d03507
Nathaniel Clark <nathaniel.l.clark@intel.com>
2018-02-28 17:18:09 -0500

    LU-9551 utils: add l_tunedisk to fix disk tunings
    This adds l_tunedisk utility to utilize osd_tune_lustre call for
    mount_utils.h.  This can be called from udev.
    This adds a udev rule to fix disk tunings.
    This in some ways duplicates LU-9132, which sets this value at mount
    time, but if a multipath component is removed then re-added, the
    multipath's max_sectors_kb will not propgate to the newly added device
    and this now will cause an error for I/Os that would violate this.
    
    Test-Parameters: trivial
    Lustre-change: https://review.whamcloud.com/31464
    Lustre-commit: 892280742a2b6347df1464379b3ed223b2961ed4
    
    Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
    Change-Id: I35330ebe75552d71b71212f9fae00cfdcc028ea1
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Signed-off-by: Minh Diep <minh.diep@intel.com>
    Reviewed-on: https://review.whamcloud.com/31951
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

---
+++ lustre/utils/l_tunedisk.c
@@ -0,0 +1,81 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2018, Intel Corporation.
+ */
+
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mount_utils.h"
+int	verbose;
+char	*progname;
+
+
+int main(int argc, char *const argv[])
+{
+	struct mount_opts mop = {
+		.mo_max_sectors_kb = -1
+	};
+	char real_path[PATH_MAX] = {'\0'};
+	unsigned int mount_type;
+	int ret;
+
+	verbose = 0;
+	progname = strrchr(argv[0], '/');
+	progname = progname ? progname + 1 : argv[0];
+
+	ret = osd_init();
+	if (ret != 0) {
+		vprint("%s: osd_init() failed to initialize: %d\n",
+		       progname, ret);
+		return ret;
+	}
+
+	/* device is last arg */
+	mop.mo_usource = argv[argc - 1];
+
+	mop.mo_source = realpath(mop.mo_usource, real_path);
+	if (mop.mo_source == NULL) {
+		vprint("%s: No realpath for %s\n", progname, mop.mo_usource);
+		goto out;
+	}
+
+	/* Check whether the disk has already been formatted by mkfs.lustre */
+	ret = osd_is_lustre(mop.mo_source, &mount_type);
+	if (ret == 0)
+		goto out;
+
+	ret = osd_tune_lustre(mop.mo_source, &mop);
+
+out:
+	osd_fini();
+	return ret;
+}
