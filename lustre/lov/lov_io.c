lustre/lustre-release

commit fcd45488711ac7341eef2a1bed9c7a8021c57436

LU-5683 clio: add CIT_DATA_VERSION
Add a new cl_io type CIT_DATA_VERSION to get file data version. Remove
the old data version code (cl_object_obd_info_get() and
cl_object_data_version()). Add test_37() to sanity-hsm.sh to ensure
that data version is computed under the correct layout.

Signed-off-by: John L. Hammond <john.hammond@intel.com>
Change-Id: If371c8e8c18e91d476f94de2e0170199252c088d
Reviewed-on: http://review.whamcloud.com/14649
Tested-by: Jenkins
Reviewed-by: Henri Doreau <henri.doreau@cea.fr>
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/lov/lov_io.c
@@ -106,6 +106,12 @@ static void lov_io_sub_inherit(struct cl_io *io, struct lov_io *lio,
                 }
                 break;
         }
+	case CIT_DATA_VERSION: {
+		io->u.ci_data_version.dv_data_version = 0;
+		io->u.ci_data_version.dv_flags =
+			parent->u.ci_data_version.dv_flags;
+		break;
+	}
         case CIT_FAULT: {
                 struct cl_object *obj = parent->ci_obj;
                 loff_t off = cl_offset(obj, parent->u.ci_fault.ft_index);
@@ -349,6 +355,11 @@ static int lov_io_slice_init(struct lov_io *lio,
                 lio->lis_endpos = OBD_OBJECT_EOF;
                 break;
 
+	case CIT_DATA_VERSION:
+		lio->lis_pos = 0;
+		lio->lis_endpos = OBD_OBJECT_EOF;
+		break;
+
         case CIT_FAULT: {
                 pgoff_t index = io->u.ci_fault.ft_index;
                 lio->lis_pos = cl_offset(io->ci_obj, index);
@@ -553,6 +564,27 @@ static void lov_io_end(const struct lu_env *env, const struct cl_io_slice *ios)
 
         rc = lov_io_call(env, cl2lov_io(env, ios), lov_io_end_wrapper);
         LASSERT(rc == 0);
+}
+
+static void
+lov_io_data_version_end(const struct lu_env *env, const struct cl_io_slice *ios)
+{
+	struct lov_io *lio = cl2lov_io(env, ios);
+	struct cl_io *parent = lio->lis_cl.cis_io;
+	struct lov_io_sub *sub;
+
+	ENTRY;
+	list_for_each_entry(sub, &lio->lis_active, sub_linkage) {
+		lov_io_end_wrapper(env, sub->sub_io);
+
+		parent->u.ci_data_version.dv_data_version +=
+			sub->sub_io->u.ci_data_version.dv_data_version;
+
+		if (parent->ci_result == 0)
+			parent->ci_result = sub->sub_io->ci_result;
+	}
+
+	EXIT;
 }
 
 static void lov_io_iter_fini(const struct lu_env *env,
@@ -865,6 +897,15 @@ static const struct cl_io_operations lov_io_ops = {
                         .cio_start     = lov_io_start,
                         .cio_end       = lov_io_end
                 },
+		[CIT_DATA_VERSION] = {
+			.cio_fini	= lov_io_fini,
+			.cio_iter_init	= lov_io_iter_init,
+			.cio_iter_fini	= lov_io_iter_fini,
+			.cio_lock	= lov_io_lock,
+			.cio_unlock	= lov_io_unlock,
+			.cio_start	= lov_io_start,
+			.cio_end	= lov_io_data_version_end,
+		},
                 [CIT_FAULT] = {
                         .cio_fini      = lov_io_fini,
                         .cio_iter_init = lov_io_iter_init,
@@ -1010,6 +1051,7 @@ int lov_io_init_empty(const struct lu_env *env, struct cl_object *obj,
 		break;
 	case CIT_FSYNC:
 	case CIT_SETATTR:
+	case CIT_DATA_VERSION:
 		result = +1;
 		break;
 	case CIT_WRITE:
@@ -1046,6 +1088,7 @@ int lov_io_init_released(const struct lu_env *env, struct cl_object *obj,
 		LASSERTF(0, "invalid type %d\n", io->ci_type);
 	case CIT_MISC:
 	case CIT_FSYNC:
+	case CIT_DATA_VERSION:
 		result = 1;
 		break;
 	case CIT_SETATTR:

lustre/include/cl_object.h
@@ -435,17 +435,6 @@ struct cl_object_operations {
 			  struct ll_fiemap_info_key *fmkey,
 			  struct fiemap *fiemap, size_t *buflen);
 	/**
-	 * Get attributes of the object from server. (top->bottom)
-	 */
-	int (*coo_obd_info_get)(const struct lu_env *env, struct cl_object *obj,
-				struct obd_info *oinfo,
-				struct ptlrpc_request_set *set);
-	/**
-	 * Get data version of the object. (top->bottom)
-	 */
-	int (*coo_data_version)(const struct lu_env *env, struct cl_object *obj,
-				__u64 *version, int flags);
-	/**
 	 * Get layout and generation of the object.
 	 */
 	int (*coo_layout_get)(const struct lu_env *env, struct cl_object *obj,
@@ -1418,6 +1407,8 @@ enum cl_io_type {
         CIT_WRITE,
         /** truncate, utime system calls */
         CIT_SETATTR,
+	/** get data version */
+	CIT_DATA_VERSION,
         /**
          * page fault handling
          */
@@ -1820,6 +1811,10 @@ struct cl_io {
 			const struct lu_fid	*sa_parent_fid;
 			struct obd_capa		*sa_capa;
 		} ci_setattr;
+		struct cl_data_version_io {
+			u64 dv_data_version;
+			int dv_flags;
+		} ci_data_version;
                 struct cl_fault_io {
                         /** page index within file. */
                         pgoff_t         ft_index;
@@ -2229,11 +2224,6 @@ int cl_object_find_cbdata(const struct lu_env *env, struct cl_object *obj,
 int cl_object_fiemap(const struct lu_env *env, struct cl_object *obj,
 		     struct ll_fiemap_info_key *fmkey, struct fiemap *fiemap,
 		     size_t *buflen);
-int cl_object_obd_info_get(const struct lu_env *env, struct cl_object *obj,
-			   struct obd_info *oinfo,
-			   struct ptlrpc_request_set *set);
-int cl_object_data_version(const struct lu_env *env, struct cl_object *obj,
-			   __u64 *version, int flags);
 int cl_object_layout_get(const struct lu_env *env, struct cl_object *obj,
 			 struct cl_layout *cl);
 loff_t cl_object_maxbytes(struct cl_object *obj);

lustre/include/obd.h
@@ -145,8 +145,6 @@ struct obd_info {
            - while setattr, the flags used for distinguish punch operation
          */
 	__u64                   oi_flags;
-        /* lsm data specific for every OSC. */
-        struct lov_stripe_md   *oi_md;
         /* obdo data specific for every OSC, if needed at all. */
         struct obdo            *oi_oa;
         /* statfs data specific for every OSC, if needed at all. */

lustre/llite/file.c
@@ -1807,13 +1807,16 @@ error:
  */
 int ll_data_version(struct inode *inode, __u64 *data_version, int flags)
 {
-	struct lu_env	*env;
-	int		refcheck;
-	int		rc;
+	struct cl_object *obj = ll_i2info(inode)->lli_clob;
+	struct lu_env *env;
+	struct cl_io *io;
+	int refcheck;
+	int result;
+
 	ENTRY;
 
 	/* If no file object initialized, we consider its version is 0. */
-	if (ll_i2info(inode)->lli_clob == NULL) {
+	if (obj == NULL) {
 		*data_version = 0;
 		RETURN(0);
 	}
@@ -1822,10 +1825,27 @@ int ll_data_version(struct inode *inode, __u64 *data_version, int flags)
 	if (IS_ERR(env))
 		RETURN(PTR_ERR(env));
 
-	rc = cl_object_data_version(env, ll_i2info(inode)->lli_clob,
-				    data_version, flags);
+	io = vvp_env_thread_io(env);
+	io->ci_obj = obj;
+	io->u.ci_data_version.dv_data_version = 0;
+	io->u.ci_data_version.dv_flags = flags;
+
+restart:
+	if (cl_io_init(env, io, CIT_DATA_VERSION, io->ci_obj) == 0)
+		result = cl_io_loop(env, io);
+	else
+		result = io->ci_result;
+
+	*data_version = io->u.ci_data_version.dv_data_version;
+
+	cl_io_fini(env, io);
+
+	if (unlikely(io->ci_need_restart))
+		goto restart;
+
 	cl_env_put(env, &refcheck);
-	RETURN(rc);
+
+	RETURN(result);
 }
 
 /*

lustre/lov/lov_internal.h
@@ -127,8 +127,6 @@ static inline void lov_put_reqset(struct lov_request_set *set)
         (char *)((lv)->lov_tgts[index]->ltd_uuid.uuid)
 
 /* lov_merge.c */
-void lov_merge_attrs(struct obdo *tgt, struct obdo *src, u64 valid,
-		     struct lov_stripe_md *lsm, int stripeno, int *set);
 int lov_merge_lvb_kms(struct lov_stripe_md *lsm,
                       struct ost_lvb *lvb, __u64 *kms_place);
 
@@ -150,12 +148,7 @@ void lov_set_add_req(struct lov_request *req, struct lov_request_set *set);
 int lov_set_finished(struct lov_request_set *set, int idempotent);
 void lov_update_set(struct lov_request_set *set,
                     struct lov_request *req, int rc);
-int lov_update_common_set(struct lov_request_set *set,
-                          struct lov_request *req, int rc);
 int lov_check_and_wait_active(struct lov_obd *lov, int ost_idx);
-int lov_prep_getattr_set(struct obd_export *exp, struct obd_info *oinfo,
-                         struct lov_request_set **reqset);
-int lov_fini_getattr_set(struct lov_request_set *set);
 int lov_prep_statfs_set(struct obd_device *obd, struct obd_info *oinfo,
                         struct lov_request_set **reqset);
 void lov_update_statfs(struct obd_statfs *osfs, struct obd_statfs *lov_sfs,
@@ -181,7 +174,6 @@ int lov_process_config_base(struct obd_device *obd, struct lustre_cfg *lcfg,
                             __u32 *indexp, int *genp);
 int lov_del_target(struct obd_device *obd, __u32 index,
                    struct obd_uuid *uuidp, int gen);
-int lov_getattr_interpret(struct ptlrpc_request_set *rqset, void *data, int rc);
 
 /* lov_pack.c */
 ssize_t lov_lsm_pack(const struct lov_stripe_md *lsm, void *buf,

lustre/lov/lov_merge.c
@@ -110,53 +110,3 @@ int lov_merge_lvb_kms(struct lov_stripe_md *lsm,
         lvb->lvb_ctime = current_ctime;
         RETURN(rc);
 }
-
-void lov_merge_attrs(struct obdo *tgt, struct obdo *src, u64 valid,
-		     struct lov_stripe_md *lsm, int stripeno, int *set)
-{
-	valid &= src->o_valid;
-
-	if (*set != 0) {
-		tgt->o_valid &= valid;
-		if (valid & OBD_MD_FLSIZE) {
-			/* this handles sparse files properly */
-			u64 lov_size;
-
-			lov_size = lov_stripe_size(lsm, src->o_size, stripeno);
-			if (lov_size > tgt->o_size)
-				tgt->o_size = lov_size;
-		}
-		if (valid & OBD_MD_FLBLOCKS)
-			tgt->o_blocks += src->o_blocks;
-		if (valid & OBD_MD_FLBLKSZ)
-			tgt->o_blksize += src->o_blksize;
-		if (valid & OBD_MD_FLCTIME && tgt->o_ctime < src->o_ctime)
-			tgt->o_ctime = src->o_ctime;
-		if (valid & OBD_MD_FLMTIME && tgt->o_mtime < src->o_mtime)
-			tgt->o_mtime = src->o_mtime;
-		if (valid & OBD_MD_FLDATAVERSION)
-			tgt->o_data_version += src->o_data_version;
-
-		/* handle flags */
-		if (valid & OBD_MD_FLFLAGS)
-			tgt->o_flags &= src->o_flags;
-		else
-			tgt->o_flags = 0;
-	} else {
-		memcpy(tgt, src, sizeof(*tgt));
-		tgt->o_oi = lsm->lsm_oi;
-		tgt->o_valid = valid;
-		if (valid & OBD_MD_FLSIZE)
-			tgt->o_size = lov_stripe_size(lsm, src->o_size,
-						      stripeno);
-		tgt->o_flags = 0;
-		if (valid & OBD_MD_FLFLAGS)
-			tgt->o_flags = src->o_flags;
-	}
-
-	/* data_version needs to be valid on all stripes to be correct! */
-	if (!(valid & OBD_MD_FLDATAVERSION))
-		tgt->o_valid &= ~OBD_MD_FLDATAVERSION;
-
-	*set += 1;
-}

lustre/lov/lov_obd.c
@@ -1029,19 +1029,6 @@ do {                                                                            
                  "%p->lsm_magic=%x\n", (lsmp), (lsmp)->lsm_magic);              \
 } while (0)
 
-int lov_getattr_interpret(struct ptlrpc_request_set *rqset, void *data, int rc)
-{
-	struct lov_request_set *lovset = (struct lov_request_set *)data;
-	int err;
-	ENTRY;
-
-	/* don't do attribute merge if this aysnc op failed */
-	if (rc)
-		atomic_set(&lovset->set_completes, 0);
-	err = lov_fini_getattr_set(lovset);
-	RETURN(rc ? rc : err);
-}
-
 int lov_statfs_interpret(struct ptlrpc_request_set *rqset, void *data, int rc)
 {
 	struct lov_request_set *lovset = (struct lov_request_set *)data;

lustre/lov/lov_object.c
@@ -1430,130 +1430,6 @@ out:
 	return rc;
 }
 
-static int lov_dispatch_obd_info_get(const struct lu_env *env,
-				     struct cl_object *obj,
-				     struct obd_info *oinfo,
-				     struct ptlrpc_request_set *set)
-{
-	struct cl_object	*subobj = NULL;
-	struct lov_obd		*lov = lu2lov_dev(obj->co_lu.lo_dev)->ld_lov;
-	struct lov_request_set	*lovset;
-	struct list_head	*pos;
-	struct lov_request	*req;
-	int			rc;
-	int			rc2;
-	ENTRY;
-
-	rc = lov_prep_getattr_set(lov2obd(lov)->obd_self_export, oinfo,
-				  &lovset);
-	if (rc != 0)
-		RETURN(rc);
-
-	CDEBUG(D_INFO, "objid "DOSTID": %ux%u byte stripes.\n",
-	       POSTID(&oinfo->oi_md->lsm_oi),
-	       oinfo->oi_md->lsm_stripe_count,
-	       oinfo->oi_md->lsm_stripe_size);
-
-	list_for_each(pos, &lovset->set_list) {
-		req = list_entry(pos, struct lov_request, rq_link);
-
-		CDEBUG(D_INFO, "objid "DOSTID"[%d] has subobj "DOSTID" at idx"
-		       "%u\n", POSTID(&oinfo->oi_oa->o_oi), req->rq_stripe,
-		       POSTID(&req->rq_oi.oi_oa->o_oi), req->rq_idx);
-		subobj = lov_find_subobj(env, cl2lov(obj), oinfo->oi_md,
-					 req->rq_stripe);
-		if (IS_ERR(subobj))
-			GOTO(errout, rc = PTR_ERR(subobj));
-
-		rc = cl_object_obd_info_get(env, subobj, &req->rq_oi, set);
-		cl_object_put(env, subobj);
-		if (rc != 0) {
-			CERROR("%s: getattr objid "DOSTID" subobj"
-			       DOSTID" on OST idx %d: rc = %d\n",
-			       lov2obd(lov)->obd_name,
-			       POSTID(&oinfo->oi_oa->o_oi),
-			       POSTID(&req->rq_oi.oi_oa->o_oi),
-			       req->rq_idx, rc);
-			GOTO(errout, rc);
-		}
-	}
-
-	if (!list_empty(&set->set_requests)) {
-		LASSERT(rc == 0);
-		LASSERT(set->set_interpret == NULL);
-		set->set_interpret = lov_getattr_interpret;
-		set->set_arg = lovset;
-		GOTO(out, rc);
-	}
-errout:
-	if (rc)
-		atomic_set(&lovset->set_completes, 0);
-	rc2 = lov_fini_getattr_set(lovset);
-	rc = rc != 0 ? rc : rc2;
-out:
-	RETURN(rc);
-}
-
-static int lov_object_data_version(const struct lu_env *env,
-				   struct cl_object *obj, __u64 *data_version,
-				   int flags)
-{
-	struct ptlrpc_request_set	*set;
-	struct obd_info			oinfo =  { { { 0 } } };
-	struct obdo			*obdo = NULL;
-	struct lov_stripe_md		*lsm;
-	int				rc;
-	ENTRY;
-
-	lsm = lov_lsm_addref(cl2lov(obj));
-	if (!lsm_has_objects(lsm)) {
-		/* If no stripe, we consider version is 0. */
-		*data_version = 0;
-		GOTO(out, rc = 0);
-	}
-
-	OBD_ALLOC_PTR(obdo);
-	if (obdo == NULL)
-		GOTO(out, rc = -ENOMEM);
-
-	oinfo.oi_md = lsm;
-	oinfo.oi_oa = obdo;
-	obdo->o_oi = lsm->lsm_oi;
-	obdo->o_mode = S_IFREG;
-	obdo->o_valid = OBD_MD_FLID | OBD_MD_FLGROUP | OBD_MD_FLTYPE;
-	if (flags & (LL_DV_RD_FLUSH | LL_DV_WR_FLUSH)) {
-		obdo->o_valid |= OBD_MD_FLFLAGS;
-		obdo->o_flags |= OBD_FL_SRVLOCK;
-		if (flags & LL_DV_WR_FLUSH)
-			obdo->o_flags |= OBD_FL_FLUSH;
-	}
-
-	set = ptlrpc_prep_set();
-	if (set == NULL)
-		GOTO(out_obdo, rc = -ENOMEM);
-
-	rc = lov_dispatch_obd_info_get(env, obj, &oinfo, set);
-	if (rc == 0)
-		rc = ptlrpc_set_wait(set);
-	ptlrpc_set_destroy(set);
-	if (rc == 0) {
-		oinfo.oi_oa->o_valid &= OBD_MD_FLDATAVERSION | OBD_MD_FLFLAGS;
-		if (flags & LL_DV_WR_FLUSH &&
-		    !(oinfo.oi_oa->o_valid & OBD_MD_FLFLAGS &&
-		      oinfo.oi_oa->o_flags & OBD_FL_FLUSH))
-			rc = -EOPNOTSUPP;
-		else if (!(obdo->o_valid & OBD_MD_FLDATAVERSION))
-			rc = -EOPNOTSUPP;
-		else
-			*data_version = obdo->o_data_version;
-	}
-out_obdo:
-	OBD_FREE_PTR(obdo);
-out:
-	lov_lsm_put(lsm);
-	RETURN(rc);
-}
-
 static int lov_object_getstripe(const struct lu_env *env, struct cl_object *obj,
 				struct lov_user_md __user *lum)
 {
@@ -1640,7 +1516,6 @@ static const struct cl_object_operations lov_ops = {
 	.coo_maxbytes     = lov_object_maxbytes,
 	.coo_find_cbdata  = lov_object_find_cbdata,
 	.coo_fiemap       = lov_object_fiemap,
-	.coo_data_version = lov_object_data_version,
 };
 
 static const struct lu_object_operations lov_lu_obj_ops = {

commit 893e5a99e766ab97e95217b7b201b9d293bed6e0

LU-4423 lov: use correct env in lov_io_data_version_end()
lov - the logical object volume manager - is responsible for
striping data across multiple volumes.

So when it is given a request, it creates one or more
sub-requests, one for each target volume.  Each sub_io
request has a sub_env environment which it operates in.

When lov_io_data_version_end() calls lov_io_end_wrapper() to
wait for and close off a sub_io, it passes the wrong
environment.

This causes an LINVRNT() to fail in cl2osc_io(), and may
cause other problems.

This patch changes the call to use ->sub_env, much like
other code in the same file.

Change-Id: Id120929f4189196232d18103007e45ba89195fff
Fixes: fcd45488711a (LU-5683 clio: add CIT_DATA_VERSION)
Signed-off-by: NeilBrown <neilb@suse.com>
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/31418
Reviewed-by: Jinshan Xiong <jinshan.xiong@gmail.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/lov/lov_io.c
@@ -956,7 +956,7 @@ lov_io_data_version_end(const struct lu_env *env, const struct cl_io_slice *ios)
 	list_for_each_entry(sub, &lio->lis_active, sub_linkage) {
 		struct cl_data_version_io *sdv = &sub->sub_io.u.ci_data_version;
 
-		lov_io_end_wrapper(env, &sub->sub_io);
+		lov_io_end_wrapper(sub->sub_env, &sub->sub_io);
 
 		pdv->dv_data_version += sdv->dv_data_version;
 		if (pdv->dv_layout_version > sdv->dv_layout_version)