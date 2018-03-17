lustre/lustre-release

commit 0100ab268c3120aa84847a88a2493988f38dee6b

LU-8066 obd: final pieces for sysfs/debugfs support.
This patch puts in place the basics needed for debugfs.
It also creates class_setup_tunables so sysfs kobject
creation is handled for both obd_devices and llite. Add a
special LDEBUGFS_FOPS_WR_ONLY since often in this case
i_private is not set so any attempt to call PDE_DATA(inode)
will cause it to crash. Make lprocfs_obd_setup select either
debugfs or procfs but not both.

Handle the special symlinks needed for both debugfs
and sysfs with the server case. For lod we need to
create "lov" and osp we create "osc" for both sysfs
and debugfs. Handle the complex case of when a node
is both a server and client. For debugfs we can take
advantage of d_lookup() and for sysfs kset_find_obj()
to avoid special access to struct obd_type. This also
places the burden on the server lod/osp modules instead
of the client lov/osc modules.

Change-Id: I87090859db4da2300ab9e2aa3c23cb3773276103
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/28108
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Reviewed-by: Ben Evans <bevans@cray.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/obdecho/echo_client.c
@@ -3071,7 +3071,7 @@ static int __init obdecho_init(void)
         if (rc != 0)
                 goto failed_0;
 
-	rc = class_register_type(&echo_obd_ops, NULL, true, NULL,
+	rc = class_register_type(&echo_obd_ops, NULL, false, NULL,
 				 LUSTRE_ECHO_NAME, NULL);
 	if (rc != 0)
 		goto failed_1;
@@ -3079,8 +3079,8 @@ static int __init obdecho_init(void)
 
 	rc = lu_kmem_init(echo_caches);
 	if (rc == 0) {
-		rc = class_register_type(&echo_client_obd_ops, NULL, true, NULL,
-					 LUSTRE_ECHO_CLIENT_NAME,
+		rc = class_register_type(&echo_client_obd_ops, NULL, false,
+					 NULL, LUSTRE_ECHO_CLIENT_NAME,
 					 &echo_device_type);
 		if (rc)
 			lu_kmem_fini(echo_caches);

lustre/autoconf/lustre-core.m4
@@ -1593,6 +1593,24 @@ truncate_pagecache_old_size, [
 ]) # LC_OLDSIZE_TRUNCATE_PAGECACHE
 
 #
+# LC_PTR_ERR_OR_ZERO
+#
+# For some reason SLES11SP4 is missing the PTR_ERR_OR_ZERO macro
+# It was added to linux kernel 3.12
+#
+AC_DEFUN([LC_PTR_ERR_OR_ZERO_MISSING], [
+LB_CHECK_COMPILE([if 'PTR_ERR_OR_ZERO' is missing],
+is_err_or_null, [
+	#include <linux/err.h>
+],[
+	if (PTR_ERR_OR_ZERO(NULL)) return 0;
+],[
+	AC_DEFINE(HAVE_PTR_ERR_OR_ZERO, 1,
+		['PTR_ERR_OR_ZERO' exist])
+])
+]) # LC_PTR_ERR_OR_ZERO_MISSING
+
+#
 # LC_HAVE_DENTRY_D_U_D_ALIAS
 #
 # 3.11 kernel moved d_alias to the union d_u in struct dentry
@@ -2836,6 +2854,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 3.12
 	LC_OLDSIZE_TRUNCATE_PAGECACHE
+	LC_PTR_ERR_OR_ZERO_MISSING
 	LC_KIOCB_KI_LEFT
 
 	# 3.13

lustre/include/lprocfs_status.h
@@ -490,10 +490,14 @@ extern int lprocfs_add_clear_entry(struct obd_device * obd,
 #ifdef HAVE_SERVER_SUPPORT
 extern int lprocfs_exp_setup(struct obd_export *exp, lnet_nid_t *peer_nid);
 extern int lprocfs_exp_cleanup(struct obd_export *exp);
+struct dentry *ldebugfs_add_symlink(const char *name, const char *target,
+				    const char *format, ...);
 #else
 static inline int lprocfs_exp_cleanup(struct obd_export *exp)
 { return 0; }
 #endif
+struct dentry *ldebugfs_add_simple(struct dentry *root, char *name, void *data,
+				   const struct file_operations *fops);
 extern struct proc_dir_entry *
 lprocfs_add_simple(struct proc_dir_entry *root, char *name,
 		   void *data, const struct file_operations *fops);
@@ -553,16 +557,14 @@ static inline int LPROCFS_ENTRY_CHECK(struct inode *inode)
 { return 0; }
 #endif
 
-extern int lprocfs_obd_setup(struct obd_device *dev, bool uuid_only);
+extern int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only);
 extern int lprocfs_obd_cleanup(struct obd_device *obd);
 #ifdef HAVE_SERVER_SUPPORT
 extern const struct file_operations lprocfs_evict_client_fops;
 #endif
 
-extern int ldebugfs_seq_create(struct dentry *parent, const char *name,
-			       umode_t mode,
-			       const struct file_operations *seq_fops,
-			       void *data);
+int ldebugfs_seq_create(struct dentry *parent, const char *name, umode_t mode,
+			const struct file_operations *seq_fops, void *data);
 extern int lprocfs_seq_create(struct proc_dir_entry *parent, const char *name,
 			      mode_t mode,
 			      const struct file_operations *seq_fops,
@@ -577,6 +579,7 @@ extern int lprocfs_uuid_seq_show(struct seq_file *m, void *data);
 extern int lprocfs_name_seq_show(struct seq_file *m, void *data);
 extern int lprocfs_server_uuid_seq_show(struct seq_file *m, void *data);
 extern int lprocfs_conn_uuid_seq_show(struct seq_file *m, void *data);
+ssize_t conn_uuid_show(struct kobject *kobj, struct attribute *attr, char *buf);
 extern int lprocfs_import_seq_show(struct seq_file *m, void *data);
 extern int lprocfs_state_seq_show(struct seq_file *m, void *data);
 extern int lprocfs_connect_flags_seq_show(struct seq_file *m, void *data);
@@ -595,6 +598,8 @@ extern ssize_t
 lprocfs_evict_client_seq_write(struct file *file, const char __user *buffer,
 				size_t count, loff_t *off);
 #endif
+ssize_t ping_store(struct kobject *kobj, struct attribute *attr,
+		   const char *buffer, size_t count);
 extern ssize_t
 lprocfs_ping_seq_write(struct file *file, const char __user *buffer,
 		       size_t count, loff_t *off);
@@ -658,10 +663,75 @@ extern int lprocfs_seq_release(struct inode *, struct file *);
 #define LPROCFS_CLIMP_EXIT(obd)                 \
 	up_read(&(obd)->u.cli.cl_sem);
 
+/* write the name##_seq_show function, call LDEBUGFS_SEQ_FOPS_RO for read-only
+ * debugfs entries; otherwise, you will define name##_seq_write function also
+ * for a read-write debugfs entry, and then call LDEBUGFS_SEQ_FOPS instead.
+ * Finally, call ldebugfs_seq_create(obd, filename, 0444, &name#_fops, data);
+ */
+#define __LDEBUGFS_SEQ_FOPS(name, custom_seq_write)			\
+static int name##_single_open(struct inode *inode, struct file *file)	\
+{									\
+	return single_open(file, name##_seq_show, inode->i_private);	\
+}									\
+static const struct file_operations name##_fops = {			\
+	.owner	 = THIS_MODULE,						\
+	.open	 = name##_single_open,					\
+	.read	 = seq_read,						\
+	.write	 = custom_seq_write,					\
+	.llseek	 = seq_lseek,						\
+	.release = single_release,					\
+}
+
+#define LDEBUGFS_SEQ_FOPS_RO(name)	__LDEBUGFS_SEQ_FOPS(name, NULL)
+#define LDEBUGFS_SEQ_FOPS(name)		__LDEBUGFS_SEQ_FOPS(name, \
+							    name##_seq_write)
+
+#define LDEBUGFS_SEQ_FOPS_RO_TYPE(name, type)				\
+	static int name##_##type##_seq_show(struct seq_file *m, void *v)\
+	{								\
+		return lprocfs_##type##_seq_show(m, m->private);	\
+	}								\
+	LDEBUGFS_SEQ_FOPS_RO(name##_##type)
+
+#define LDEBUGFS_SEQ_FOPS_RW_TYPE(name, type)				\
+	static int name##_##type##_seq_show(struct seq_file *m, void *v)\
+	{								\
+		return lprocfs_##type##_seq_show(m, m->private);	\
+	}								\
+	static ssize_t name##_##type##_seq_write(struct file *file,	\
+			const char __user *buffer, size_t count,	\
+			loff_t *off)					\
+	{								\
+		struct seq_file *seq = file->private_data;		\
+		return ldebugfs_##type##_seq_write(file, buffer, count,	\
+						   seq->private);	\
+	}								\
+	LDEBUGFS_SEQ_FOPS(name##_##type);
+
+#define LDEBUGFS_FOPS_WR_ONLY(name, type)				\
+	static ssize_t name##_##type##_write(struct file *file,		\
+			const char __user *buffer, size_t count,	\
+			loff_t *off)					\
+	{								\
+		return ldebugfs_##type##_seq_write(file, buffer, count,	\
+						   off);		\
+	}								\
+	static int name##_##type##_open(struct inode *inode,		\
+					struct file *file)		\
+	{								\
+		return single_open(file, NULL, inode->i_private);	\
+	}								\
+	static const struct file_operations name##_##type##_fops = {	\
+		.open	 = name##_##type##_open,			\
+		.write	 = name##_##type##_write,			\
+		.release = single_release,				\
+	};
+
 /* write the name##_seq_show function, call LPROC_SEQ_FOPS_RO for read-only
-  proc entries; otherwise, you will define name##_seq_write function also for
-  a read-write proc entry, and then call LPROC_SEQ_SEQ instead. Finally,
-  call lprocfs_obd_seq_create(obd, filename, 0444, &name#_fops, data); */
+ * proc entries; otherwise, you will define name##_seq_write function also for
+ * a read-write proc entry, and then call LPROC_SEQ_FOPS instead. Finally,
+ * call ldebugfs_obd_seq_create(obd, filename, 0444, &name#_fops, data);
+ */
 #define __LPROC_SEQ_FOPS(name, custom_seq_write)			\
 static int name##_single_open(struct inode *inode, struct file *file)	\
 {									\

lustre/include/lustre_compat.h
@@ -392,6 +392,16 @@ static inline void truncate_inode_pages_final(struct address_space *map)
 }
 #endif
 
+#ifndef HAVE_PTR_ERR_OR_ZERO
+static inline int __must_check PTR_ERR_OR_ZERO(__force const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
+}
+#endif
+
 #ifndef SIZE_MAX
 #define SIZE_MAX	(~(size_t)0)
 #endif

lustre/include/obd.h
@@ -102,13 +102,15 @@ struct obd_type {
 	struct md_ops		*typ_md_ops;
 	struct proc_dir_entry	*typ_procroot;
 	struct proc_dir_entry	*typ_procsym;
-	__u32			 typ_sym_filter;
+	struct dentry		*typ_debugfs_entry;
+#ifdef HAVE_SERVER_SUPPORT
+	bool			 typ_sym_filter;
+#endif
 	char			*typ_name;
 	int			 typ_refcnt;
 	struct lu_device_type	*typ_lu;
 	spinlock_t		 obd_type_lock;
-	struct kobject		 typ_kobj;
-	struct completion	 typ_kobj_unregister;
+	struct kobject		*typ_kobj;
 };
 
 struct brw_page {
@@ -704,6 +706,7 @@ struct obd_device {
 	unsigned int			 obd_md_cntr_base;
 	struct lprocfs_stats		*obd_md_stats;
 
+	struct dentry			*obd_debugfs_entry;
 	struct proc_dir_entry	*obd_proc_entry;
 	struct proc_dir_entry	*obd_proc_exports_entry;
 	struct proc_dir_entry	*obd_svc_procroot;

lustre/include/obd_class.h
@@ -32,7 +32,7 @@
 #ifndef __CLASS_OBD_H
 #define __CLASS_OBD_H
 
-
+#include <linux/kobject.h>
 #include <obd_support.h>
 #include <lustre_import.h>
 #include <lustre_net.h>
@@ -63,6 +63,7 @@ struct lu_device_type;
 
 /* genops.c */
 struct obd_export *class_conn2export(struct lustre_handle *);
+struct kobject *class_setup_tunables(const char *name);
 int class_register_type(struct obd_ops *, struct md_ops *, bool enable_proc,
 			struct lprocfs_vars *module_vars,
 			const char *nm, struct lu_device_type *ldt);

lustre/lod/lod_dev.c
@@ -2057,9 +2057,14 @@ static struct obd_ops lod_obd_device_ops = {
 	.o_pool_del     = lod_pool_del,
 };
 
+static struct obd_type sym;
+
 static int __init lod_init(void)
 {
+	struct dentry *symlink;
 	struct obd_type	*type;
+	struct kobject *kobj;
+	struct qstr dname;
 	int rc;
 
 	rc = lu_kmem_init(lod_caches);
@@ -2073,10 +2078,42 @@ static int __init lod_init(void)
 		return rc;
 	}
 
-	/* create "lov" entry in procfs for compatibility purposes */
+	/* create "lov" entry for compatibility purposes */
+	dname.name = "lov";
+	dname.len = strlen(dname.name);
+	dname.hash = ll_full_name_hash(debugfs_lustre_root, dname.name,
+				       dname.len);
+	symlink = d_lookup(debugfs_lustre_root, &dname);
+	if (!symlink) {
+		symlink = debugfs_create_dir(dname.name, debugfs_lustre_root);
+		if (IS_ERR_OR_NULL(symlink)) {
+			rc = symlink ? PTR_ERR(symlink) : -ENOMEM;
+			GOTO(no_lov, rc);
+		}
+		sym.typ_debugfs_entry = symlink;
+	} else {
+		dput(symlink);
+	}
+
+	kobj = kset_find_obj(lustre_kset, dname.name);
+	if (kobj) {
+		kobject_put(kobj);
+		goto try_proc;
+	}
+
+	kobj = class_setup_tunables(dname.name);
+	if (IS_ERR(kobj)) {
+		rc = PTR_ERR(kobj);
+		if (sym.typ_debugfs_entry)
+			ldebugfs_remove(&sym.typ_debugfs_entry);
+		GOTO(no_lov, rc);
+	}
+	sym.typ_kobj = kobj;
+
+try_proc:
 	type = class_search_type(LUSTRE_LOV_NAME);
 	if (type != NULL && type->typ_procroot != NULL)
-		return rc;
+		GOTO(no_lov, rc);
 
 	type = class_search_type(LUSTRE_LOD_NAME);
 	type->typ_procsym = lprocfs_register("lov", proc_lustre_root,
@@ -2086,11 +2123,14 @@ static int __init lod_init(void)
 		       (int)PTR_ERR(type->typ_procsym));
 		type->typ_procsym = NULL;
 	}
+no_lov:
 	return rc;
 }
 
 static void __exit lod_exit(void)
 {
+	ldebugfs_remove(&sym.typ_debugfs_entry);
+	kobject_put(sym.typ_kobj);
 	class_unregister_type(LUSTRE_LOD_NAME);
 	lu_kmem_fini(lod_caches);
 }

lustre/lod/lod_internal.h
@@ -167,7 +167,6 @@ struct lod_device {
 	struct dt_device      lod_dt_dev;
 	struct obd_export    *lod_child_exp;
 	struct dt_device     *lod_child;
-	struct proc_dir_entry *lod_proc_entry;
 	struct lprocfs_stats *lod_stats;
 	spinlock_t	      lod_connects_lock;
 	int		      lod_connects;
@@ -211,6 +210,7 @@ struct lod_device {
 	enum lustre_sec_part   lod_sp_me;
 
 	struct proc_dir_entry *lod_symlink;
+	struct dentry	       *lod_debugfs;
 
 	/* ROOT object, used to fetch FS default striping */
 	struct lod_object      *lod_md_root;

commit af94e4b7afd3fa724d161a5127dc9a89d898e73c

LU-10804 echo: allow echo server to setup procfs
Restore lprocfs init for echo server. It is still using
procfs for stats.

Fixes: 0100ab268c3120aa84847a88a2493988f38dee6b
Test-Parameters: trivial envdefinitions=SLOW=yes testlist=obdfilter-survey osscount=1 ostcount=4 mdscount=1 mdtcount=1
Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
Change-Id: I7a1bf7de3d7c3202e6da7545da63979555ce6624
Reviewed-on: https://review.whamcloud.com/31664
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Fan Yong <fan.yong@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Reviewed-by: Elena Gryaznova <c17455@cray.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>

lustre/obdecho/echo_client.c
@@ -3071,7 +3071,7 @@ static int __init obdecho_init(void)
         if (rc != 0)
                 goto failed_0;
 
-	rc = class_register_type(&echo_obd_ops, NULL, false, NULL,
+	rc = class_register_type(&echo_obd_ops, NULL, true, NULL,
 				 LUSTRE_ECHO_NAME, NULL);
 	if (rc != 0)
 		goto failed_1;