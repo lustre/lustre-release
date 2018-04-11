f19699186d29ba8561d67b4457e98c018ea26317
Mike Marciniszyn <mike.marciniszyn@intel.com>
2018-04-11 05:20:05 -0700

    LU-10560 libcfs: Fix inconsistent declaration for cfs_kernel_write()
    The header file buffer should be const void * to match the
    implementation.
    
    Test-Parameters: trivial
    
    Change-Id: I3a09b84e4c3dbe39870be04ae98c13e9e6221a6d
    Fixes: b9a32054600a8d63948cced361191aa6ae7ea8f2
    Signed-off-by: Mike Marciniszyn <mike.marciniszyn@intel.com>
    Reviewed-on: https://review.whamcloud.com/31948
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- libcfs/include/libcfs/linux/linux-misc.h
+++ libcfs/include/libcfs/linux/linux-misc.h
@@ -117,7 +117,8 @@ int cfs_get_environ(const char *key, char *value, int *val_len);
 #define wait_queue_entry_t wait_queue_t
 #endif
 
-int cfs_kernel_write(struct file *filp, char *buf, size_t count, loff_t *pos);
+int cfs_kernel_write(struct file *filp, const void *buf, size_t count,
+		     loff_t *pos);
 
 /*
  * For RHEL6 struct kernel_parm_ops doesn't exist. Also
