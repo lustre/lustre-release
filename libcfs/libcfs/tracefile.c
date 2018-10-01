lustre/lustre-release

commit fdb5d3d508edb6d76b6fcc1f23d5d9649211ab64

LU-9019 libcfs: remove the remaining cfs_time wrappers
In the lustre code various small bits are left of the libcfs time
wrappers. Remove all the remaining wrappers except for the inline
function cfs_time_seconds(). For cfs_time_seconds() we have to
move to nsec_to_jiffies() since msecs_to_jiffies() has become an
inline function in jiffies.h which means HZ can be different on
the installation node verses what the target node is configured
for. Since nsec_to_jiffies() is a normal built in kernel function
we can avoid the LU-5443 issues. For cfs_duration_secs() we use
the internal kernel function jiffies_to_msec() since only the
newest kernels have jiffies_to_nsec() and that function is just
a wrapper around jiffies_to_msec(). All the reset of the libcfs
time abstractions are gone.

Change-Id: I166973071304f1d55a15b1e21fcfbe434ff58199
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/31068
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Ben Evans <bevans@cray.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

libcfs/libcfs/tracefile.c
@@ -46,7 +46,6 @@
 #include <linux/pagemap.h>
 #include <linux/uaccess.h>
 #include <libcfs/linux/linux-fs.h>
-#include <libcfs/linux/linux-misc.h>
 #include <libcfs/libcfs.h>
 
 /* XXX move things up to the top, comment */
@@ -395,34 +394,34 @@ console:
                 return 1;
         }
 
-        if (cdls != NULL) {
-                if (libcfs_console_ratelimit &&
-                    cdls->cdls_next != 0 &&     /* not first time ever */
-                    !cfs_time_after(cfs_time_current(), cdls->cdls_next)) {
-                        /* skipping a console message */
-                        cdls->cdls_count++;
-                        if (tcd != NULL)
-                                cfs_trace_put_tcd(tcd);
-                        return 1;
-                }
+	if (cdls != NULL) {
+		if (libcfs_console_ratelimit &&
+		    cdls->cdls_next != 0 &&	/* not first time ever */
+		    time_after(jiffies, cdls->cdls_next)) {
+			/* skipping a console message */
+			cdls->cdls_count++;
+			if (tcd != NULL)
+				cfs_trace_put_tcd(tcd);
+			return 1;
+		}
 
-                if (cfs_time_after(cfs_time_current(), cdls->cdls_next +
-                                                       libcfs_console_max_delay
-                                                       + cfs_time_seconds(10))) {
-                        /* last timeout was a long time ago */
-                        cdls->cdls_delay /= libcfs_console_backoff * 4;
-                } else {
-                        cdls->cdls_delay *= libcfs_console_backoff;
-                }
+		if (time_after(jiffies, cdls->cdls_next +
+					libcfs_console_max_delay +
+					cfs_time_seconds(10))) {
+			/* last timeout was a long time ago */
+			cdls->cdls_delay /= libcfs_console_backoff * 4;
+		} else {
+			cdls->cdls_delay *= libcfs_console_backoff;
+		}
 
 		if (cdls->cdls_delay < libcfs_console_min_delay)
 			cdls->cdls_delay = libcfs_console_min_delay;
 		else if (cdls->cdls_delay > libcfs_console_max_delay)
 			cdls->cdls_delay = libcfs_console_max_delay;
 
-                /* ensure cdls_next is never zero after it's been seen */
-                cdls->cdls_next = (cfs_time_current() + cdls->cdls_delay) | 1;
-        }
+		/* ensure cdls_next is never zero after it's been seen */
+		cdls->cdls_next = (jiffies + cdls->cdls_delay) | 1;
+	}
 
         if (tcd != NULL) {
                 cfs_print_to_console(&header, mask, string_buf, needed, file,

libcfs/include/libcfs/Makefile.am
@@ -16,6 +16,5 @@ EXTRA_DIST = \
 	libcfs_prim.h \
 	libcfs_private.h \
 	libcfs_string.h \
-	libcfs_time.h \
 	libcfs_workitem.h \
 	libcfs_ptask.h

libcfs/include/libcfs/libcfs.h
@@ -33,13 +33,18 @@
 #ifndef __LIBCFS_LIBCFS_H__
 #define __LIBCFS_LIBCFS_H__
 
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <libcfs/linux/linux-misc.h>
+#include <libcfs/linux/linux-time.h>
+
 #include <uapi/linux/lnet/libcfs_ioctl.h>
 #include <libcfs/libcfs_debug.h>
 #include <libcfs/libcfs_private.h>
 #include <libcfs/bitmap.h>
 #include <libcfs/libcfs_cpu.h>
 #include <libcfs/libcfs_prim.h>
-#include <libcfs/libcfs_time.h>
 #include <libcfs/libcfs_string.h>
 #include <libcfs/libcfs_workitem.h>
 #include <libcfs/libcfs_hash.h>

libcfs/include/libcfs/libcfs_time.h
@@ -1,72 +1,0 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
-/*
- * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
- * Use is subject to license terms.
- */
-/*
- * This file is part of Lustre, http://www.lustre.org/
- * Lustre is a trademark of Sun Microsystems, Inc.
- *
- * libcfs/include/libcfs/libcfs_time.h
- *
- * Time functions.
- *
- */
-
-#ifndef __LIBCFS_TIME_H__
-#define __LIBCFS_TIME_H__
-
-#include <libcfs/linux/linux-time.h>
-
-/*
- * generic time manipulation functions.
- */
-
-static inline cfs_time_t cfs_time_add(cfs_time_t t, cfs_duration_t d)
-{
-        return (cfs_time_t)(t + d);
-}
-
-static inline cfs_duration_t cfs_time_sub(cfs_time_t t1, cfs_time_t t2)
-{
-        return (cfs_time_t)(t1 - t2);
-}
-
-static inline int cfs_time_after(cfs_time_t t1, cfs_time_t t2)
-{
-        return cfs_time_before(t2, t1);
-}
-
-static inline int cfs_time_aftereq(cfs_time_t t1, cfs_time_t t2)
-{
-        return cfs_time_beforeq(t2, t1);
-}
-
-static inline cfs_time_t cfs_time_shift(int seconds)
-{
-        return cfs_time_add(cfs_time_current(), cfs_time_seconds(seconds));
-}
-
-#define CFS_TICK	1
-
-#endif

libcfs/include/libcfs/linux/linux-time.h
@@ -40,24 +40,6 @@
 #define __LIBCFS_LINUX_LINUX_TIME_H__
 
 /* Portable time API */
-
-/*
- * Platform provides three opaque data-types:
- *
- *  cfs_time_t        represents point in time. This is internal kernel
- *                    time rather than "wall clock". This time bears no
- *                    relation to gettimeofday().
- *
- *  cfs_duration_t    represents time interval with resolution of internal
- *                    platform clock
- *
- *  cfs_time_t     cfs_time_current(void);
- *  cfs_time_t     cfs_time_add    (cfs_time_t, cfs_duration_t);
- *  cfs_duration_t cfs_time_sub    (cfs_time_t, cfs_time_t);
- *
- *  time_t         cfs_duration_sec (cfs_duration_t);
- */
-
 #include <linux/hrtimer.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -71,10 +53,6 @@
 /*
  * Generic kernel stuff
  */
-
-typedef unsigned long cfs_time_t;      /* jiffies */
-typedef long cfs_duration_t;
-
 #ifndef HAVE_TIMESPEC64
 
 typedef __s64 time64_t;
@@ -214,34 +192,9 @@ static inline ktime_t timespec64_to_ktime(struct timespec64 ts)
 }
 #endif
 
-static inline int cfs_time_before(cfs_time_t t1, cfs_time_t t2)
+static inline unsigned long cfs_time_seconds(time64_t seconds)
 {
-        return time_before(t1, t2);
-}
-
-static inline int cfs_time_beforeq(cfs_time_t t1, cfs_time_t t2)
-{
-        return time_before_eq(t1, t2);
-}
-
-static inline cfs_time_t cfs_time_current(void)
-{
-        return jiffies;
-}
-
-static inline time_t cfs_time_current_sec(void)
-{
-	return get_seconds();
-}
-
-static inline cfs_duration_t cfs_time_seconds(int seconds)
-{
-	return ((cfs_duration_t)seconds) * msecs_to_jiffies(MSEC_PER_SEC);
-}
-
-static inline time_t cfs_duration_sec(cfs_duration_t d)
-{
-	return d / msecs_to_jiffies(MSEC_PER_SEC);
+	return nsecs_to_jiffies(seconds * NSEC_PER_SEC);
 }
 
 #endif /* __LIBCFS_LINUX_LINUX_TIME_H__ */

libcfs/libcfs/debug.c
@@ -39,7 +39,6 @@
 
 #include <linux/ctype.h>
 #include <linux/kthread.h>
-#include <libcfs/linux/linux-misc.h>
 #include <libcfs/libcfs.h>
 #include "tracefile.h"
 
@@ -148,7 +147,7 @@ static int param_get_delay(char *buffer, cfs_kernel_param_arg_t *kp)
 {
 	unsigned int d = *(unsigned int *)kp->arg;
 
-	return sprintf(buffer, "%u", (unsigned int)cfs_duration_sec(d * 100));
+	return sprintf(buffer, "%lu", jiffies_to_msecs(d * 10) / MSEC_PER_SEC);
 }
 
 unsigned int libcfs_console_max_delay;

libcfs/libcfs/linux/linux-prim.c
@@ -41,11 +41,12 @@
 #include <linux/sched/mm.h>
 #endif
 #include <linux/uaccess.h>
-#include <libcfs/libcfs.h>
 
 #if defined(CONFIG_KGDB)
 #include <asm/kgdb.h>
 #endif
+
+#include <libcfs/linux/linux-time.h>
 
 #ifndef HAVE_KTIME_GET_TS64
 void ktime_get_ts64(struct timespec64 *ts)

libcfs/libcfs/module.c
@@ -53,7 +53,6 @@
 
 #include <libcfs/libcfs.h>
 #include <libcfs/libcfs_crypto.h>
-#include <libcfs/linux/linux-misc.h>
 #include <lnet/lib-lnet.h>
 #include "tracefile.h"
 

commit 4037c1462730c120af00bd0e95b08fe0669e0271

LU-11373 libcfs: fix wrong check in libcfs_debug_vmsg2()
Logic here is we skip output if time is before @cdls_next
reach and increase @cdls_count. however we did it in the
opposite way:

1)libcfs_debug_vmsg2() is called for a long time, that means
current check succeed, we skip print messages and return, we
will skip all messages later too..

2)libcfs_debug_vmsg2() is called frequently, current check
fail every time, message will be bumped out always. the
worst case is we never skip any messages.

Also fix test case to cover this later which is from Andreas:

The test_60a() llog test is being run on the MGS, while the
check in test_60b() to confirm that CDEBUG_LIMIT() works properly
is being run on the client.  There has been a breakage in
CDEBUG_LIMIT() that this test failed to catch, now we need to
track it down.

Change test_60b to dump the dmesg logs on the MGS.

Fixes: fdb5d3d50
Test-parameters: trivial
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Change-Id: Idb2ecfe5ddadc72d8931e6623b171b9c773ebbe5
Reviewed-on: https://review.whamcloud.com/33154
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>

libcfs/libcfs/tracefile.c
@@ -397,7 +397,7 @@ console:
 	if (cdls != NULL) {
 		if (libcfs_console_ratelimit &&
 		    cdls->cdls_next != 0 &&	/* not first time ever */
-		    time_after(jiffies, cdls->cdls_next)) {
+		    time_before(jiffies, cdls->cdls_next)) {
 			/* skipping a console message */
 			cdls->cdls_count++;
 			if (tcd != NULL)

lustre/tests/sanity.sh
@@ -6281,18 +6281,19 @@ test_60b() { # bug 6411
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 
 	dmesg > $DIR/$tfile
-	LLOG_COUNT=$(dmesg | awk "/$TEST60_HEAD/ { marker = 1; from_marker = 0; }
-				/llog.test/ {
-					if (marker)
-						from_marker++
-					from_begin++
-				}
-				END {
-					if (marker)
-						print from_marker
-					else
-						print from_begin
-				}")
+	LLOG_COUNT=$(do_facet mgs dmesg |
+		     awk "/$TEST60_HEAD/ { marker = 1; from_marker = 0; }
+			  /llog_[a-z]*.c:[0-9]/ {
+				if (marker)
+					from_marker++
+				from_begin++
+			  }
+			  END {
+				if (marker)
+					print from_marker
+				else
+					print from_begin
+			  }")
 	[[ $LLOG_COUNT -gt 100 ]] &&
 		error "CDEBUG_LIMIT not limiting messages ($LLOG_COUNT)" || true
 }