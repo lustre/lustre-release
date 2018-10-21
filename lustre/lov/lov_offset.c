6e4885ea71f32f5e75b2ebbe9d1692e05ca2caa2
Arshad Hussain <arshad.super@gmail.com>
2018-10-22 00:59:38 +0530

    LU-6142 lov: Fix style issues for lov_offset.c
    This patch fixes issues reported by checkpatch for file
    lustre/lov/lov_offset.c
    
    Change-Id: I0aeeb161bef9247f11a91563f82bce48150001d9
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Reviewed-on: https://review.whamcloud.com/33539
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/lov/lov_offset.c
+++ lustre/lov/lov_offset.c
@@ -58,10 +58,11 @@ u64 lov_stripe_size(struct lov_stripe_md *lsm, int index, u64 ost_size,
 	unsigned long stripe_size;
 	loff_t swidth;
 	loff_t lov_size;
-        ENTRY;
 
-        if (ost_size == 0)
-                RETURN(0);
+	ENTRY;
+
+	if (ost_size == 0)
+		RETURN(0);
 
 	swidth = stripe_width(lsm, index);
 
@@ -72,7 +73,7 @@ u64 lov_stripe_size(struct lov_stripe_md *lsm, int index, u64 ost_size,
 	else
 		lov_size = (ost_size - 1) * swidth + (stripeno + 1) * ssize;
 
-        RETURN(lov_size);
+	RETURN(lov_size);
 }
 
 /**
@@ -89,7 +90,8 @@ pgoff_t lov_stripe_pgoff(struct lov_stripe_md *lsm, int index,
 	return offset >> PAGE_SHIFT;
 }
 
-/* we have an offset in file backed by an lov and want to find out where
+/*
+ * we have an offset in file backed by an lov and want to find out where
  * that offset lands in our given stripe of the file.  for the easy
  * case where the offset is within the stripe, we just have to scale the
  * offset down to make it relative to the stripe instead of the lov.
@@ -136,7 +138,8 @@ pgoff_t lov_stripe_pgoff(struct lov_stripe_md *lsm, int index,
  * this function returns < 0 when the offset was "before" the stripe and
  * was moved forward to the start of the stripe in question;  0 when it
  * falls in the stripe and no shifting was done; > 0 when the offset
- * was outside the stripe and was pulled back to its final byte. */
+ * was outside the stripe and was pulled back to its final byte.
+ */
 int lov_stripe_offset(struct lov_stripe_md *lsm, int index, loff_t lov_off,
 		      int stripeno, loff_t *obdoff)
 {
@@ -157,23 +160,24 @@ int lov_stripe_offset(struct lov_stripe_md *lsm, int index, loff_t lov_off,
 	stripe_off = lov_do_div64(lov_off, swidth);
 
 	this_stripe = (loff_t)stripeno * ssize;
-        if (stripe_off < this_stripe) {
-                stripe_off = 0;
-                ret = -1;
-        } else {
-                stripe_off -= this_stripe;
+	if (stripe_off < this_stripe) {
+		stripe_off = 0;
+		ret = -1;
+	} else {
+		stripe_off -= this_stripe;
 
-                if (stripe_off >= ssize) {
-                        stripe_off = ssize;
-                        ret = 1;
-                }
-        }
+		if (stripe_off >= ssize) {
+			stripe_off = ssize;
+			ret = 1;
+		}
+	}
 
-        *obdoff = lov_off * ssize + stripe_off;
-        return ret;
+	*obdoff = lov_off * ssize + stripe_off;
+	return ret;
 }
 
-/* Given a whole-file size and a stripe number, give the file size which
+/*
+ * Given a whole-file size and a stripe number, give the file size which
  * corresponds to the individual object of that stripe.
  *
  * This behaves basically in the same was as lov_stripe_offset, except that
@@ -209,35 +213,37 @@ loff_t lov_size_to_stripe(struct lov_stripe_md *lsm, int index, u64 file_size,
 	stripe_off = lov_do_div64(file_size, swidth);
 
 	this_stripe = (loff_t)stripeno * ssize;
-        if (stripe_off < this_stripe) {
-                /* Move to end of previous stripe, or zero */
-                if (file_size > 0) {
-                        file_size--;
-                        stripe_off = ssize;
-                } else {
-                        stripe_off = 0;
-                }
-        } else {
-                stripe_off -= this_stripe;
+	if (stripe_off < this_stripe) {
+		/* Move to end of previous stripe, or zero */
+		if (file_size > 0) {
+			file_size--;
+			stripe_off = ssize;
+		} else {
+			stripe_off = 0;
+		}
+	} else {
+		stripe_off -= this_stripe;
 
-                if (stripe_off >= ssize) {
-                        /* Clamp to end of this stripe */
-                        stripe_off = ssize;
-                }
-        }
+		if (stripe_off >= ssize) {
+			/* Clamp to end of this stripe */
+			stripe_off = ssize;
+		}
+	}
 
-        return (file_size * ssize + stripe_off);
+	return (file_size * ssize + stripe_off);
 }
 
-/* given an extent in an lov and a stripe, calculate the extent of the stripe
+/*
+ * given an extent in an lov and a stripe, calculate the extent of the stripe
  * that is contained within the lov extent.  this returns true if the given
- * stripe does intersect with the lov extent. */
+ * stripe does intersect with the lov extent.
+ */
 int lov_stripe_intersects(struct lov_stripe_md *lsm, int index, int stripeno,
 			  struct lu_extent *ext, u64 *obd_start, u64 *obd_end)
 {
 	struct lov_stripe_md_entry *entry = lsm->lsm_entries[index];
 	u64 start, end;
-        int start_side, end_side;
+	int start_side, end_side;
 
 	if (!lu_extent_is_overlapped(ext, &entry->lsme_extent))
 			return 0;
@@ -253,24 +259,28 @@ int lov_stripe_intersects(struct lov_stripe_md *lsm, int index, int stripeno,
 	CDEBUG(D_INODE, "[%lld->%lld] -> [(%d) %lld->%lld (%d)]\n",
 		start, end, start_side, *obd_start, *obd_end, end_side);
 
-        /* this stripe doesn't intersect the file extent when neither
-         * start or the end intersected the stripe and obd_start and
-         * obd_end got rounded up to the save value. */
-        if (start_side != 0 && end_side != 0 && *obd_start == *obd_end)
-                return 0;
+	/*
+	 * this stripe doesn't intersect the file extent when neither
+	 * start or the end intersected the stripe and obd_start and
+	 * obd_end got rounded up to the save value.
+	 */
+	if (start_side != 0 && end_side != 0 && *obd_start == *obd_end)
+		return 0;
 
-        /* as mentioned in the lov_stripe_offset commentary, end
-         * might have been shifted in the wrong direction.  This
-         * happens when an end offset is before the stripe when viewed
-         * through the "mod stripe size" math. we detect it being shifted
-         * in the wrong direction and touch it up.
-         * interestingly, this can't underflow since end must be > start
-         * if we passed through the previous check.
-         * (should we assert for that somewhere?) */
-        if (end_side != 0)
-                (*obd_end)--;
+	/*
+	 * as mentioned in the lov_stripe_offset commentary, end
+	 * might have been shifted in the wrong direction.  This
+	 * happens when an end offset is before the stripe when viewed
+	 * through the "mod stripe size" math. we detect it being shifted
+	 * in the wrong direction and touch it up.
+	 * interestingly, this can't underflow since end must be > start
+	 * if we passed through the previous check.
+	 * (should we assert for that somewhere?)
+	 */
+	if (end_side != 0)
+		(*obd_end)--;
 
-        return 1;
+	return 1;
 }
 
 /* compute which stripe number "lov_off" will be written into */
