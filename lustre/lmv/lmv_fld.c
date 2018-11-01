72ee63625055d94cf7819586f097c9d9a07b7b3b
Arshad Hussain <arshad.super@gmail.com>
2018-11-01 19:53:45 +0530

    LU-6142 lmv: Fix style issues for lmv_fld.c
    This patch fixes issues reported by checkpatch for file
    lustre/lmv/lmv_fld.c
    
    Change-Id: I98a5bdca61eff990370a7b2518b54e7f4137d120
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Reviewed-on: https://review.whamcloud.com/33566
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Tested-by: Jenkins
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/lmv/lmv_fld.c
+++ lustre/lmv/lmv_fld.c
@@ -52,32 +52,37 @@ int lmv_fld_lookup(struct lmv_obd *lmv, const struct lu_fid *fid, u32 *mds)
 {
 	struct obd_device *obd = lmv2obd_dev(lmv);
 	int rc;
+
 	ENTRY;
 
-	/* FIXME: Currently ZFS still use local seq for ROOT unfortunately, and
-	 * this fid_is_local check should be removed once LU-2240 is fixed */
+	/*
+	 * FIXME: Currently ZFS still use local seq for ROOT unfortunately, and
+	 * this fid_is_local check should be removed once LU-2240 is fixed
+	 */
 	if (!fid_is_sane(fid) || !(fid_seq_in_fldb(fid_seq(fid)) ||
 				   fid_seq_is_local_file(fid_seq(fid)))) {
-		CERROR("%s: invalid FID "DFID"\n", obd->obd_name, PFID(fid));
-		RETURN(-EINVAL);
+		rc = -EINVAL;
+		CERROR("%s: invalid FID "DFID": rc = %d\n", obd->obd_name,
+		       PFID(fid), rc);
+		RETURN(rc);
 	}
 
 	rc = fld_client_lookup(&lmv->lmv_fld, fid_seq(fid), mds,
-                               LU_SEQ_RANGE_MDT, NULL);
-        if (rc) {
-		CERROR("Error while looking for mds number. Seq %#llx"
-                       ", err = %d\n", fid_seq(fid), rc);
-                RETURN(rc);
-        }
+			       LU_SEQ_RANGE_MDT, NULL);
+	if (rc) {
+		CERROR("%s: Error while looking for mds number. Seq %#llx: rc = %d\n",
+		       obd->obd_name, fid_seq(fid), rc);
+		RETURN(rc);
+	}
 
-        CDEBUG(D_INODE, "FLD lookup got mds #%x for fid="DFID"\n",
-               *mds, PFID(fid));
+	CDEBUG(D_INODE, "FLD lookup got mds #%x for fid="DFID"\n",
+	       *mds, PFID(fid));
 
-        if (*mds >= lmv->desc.ld_tgt_count) {
-                CERROR("FLD lookup got invalid mds #%x (max: %x) "
-                       "for fid="DFID"\n", *mds, lmv->desc.ld_tgt_count,
-                       PFID(fid));
-                rc = -EINVAL;
-        }
-        RETURN(rc);
+	if (*mds >= lmv->desc.ld_tgt_count) {
+		rc = -EINVAL;
+		CERROR("%s: FLD lookup got invalid mds #%x (max: %x) for fid="DFID": rc = %d\n",
+		       obd->obd_name, *mds, lmv->desc.ld_tgt_count, PFID(fid),
+		       rc);
+	}
+	RETURN(rc);
 }
