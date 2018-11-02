lustre/lustre-release

commit 34acfbc2bfe502d18c12ba35771bde7c4a0f7906

LU-6401 uapi: fix up lustre_ostid.h and lustre_fid.h
Several inline functions in the header lustre_ostid.h
are using debug macros instead of returning proper errors.
Remove the debug macros and properly handle the returned
error codes. Place both UAPI headers lustre_fid.h and
lustre_ostid.h into the uapi directory.

Change-Id: Ic32afd05850b5bf02fb8de655cb1971eeb52a321
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/24569
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Mike Pershin <mike.pershin@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
Tested-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/ofd/ofd_dev.c
@@ -1526,8 +1526,7 @@ done:
 		rc = ofd_seq_last_oid_write(env, ofd, oseq);
 	} else {
 		/* don't reuse orphan object, return last used objid */
-		ostid_set_id(oi, last);
-		rc = 0;
+		rc = ostid_set_id(oi, last);
 	}
 
 	GOTO(out_put, rc);
@@ -1624,16 +1623,17 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 		if (!oseq->os_destroys_in_progress) {
 			CERROR("%s:[%llu] destroys_in_progress already"
 			       " cleared\n", ofd_name(ofd), seq);
-			ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq));
-			GOTO(out, rc = 0);
+			rc = ostid_set_id(&rep_oa->o_oi,
+					  ofd_seq_last_oid(oseq));
+			GOTO(out, rc);
 		}
 		diff = oid - ofd_seq_last_oid(oseq);
 		CDEBUG(D_HA, "ofd_last_id() = %llu -> diff = %d\n",
 			ofd_seq_last_oid(oseq), diff);
 		if (-diff > OST_MAX_PRECREATE) {
 			/* Let MDS know that we are so far ahead. */
-			ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq) + 1);
-			rc = 0;
+			rc = ostid_set_id(&rep_oa->o_oi,
+					  ofd_seq_last_oid(oseq) + 1);
 		} else if (diff < 0) {
 			rc = ofd_orphans_destroy(tsi->tsi_env, exp,
 						 ofd, rep_oa);
@@ -1781,7 +1781,7 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 			granted = 0;
 		}
 
-		ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq));
+		rc = ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq));
 	}
 	EXIT;
 	ofd_counter_incr(exp, LPROC_OFD_STATS_CREATE,

lustre/osp/osp_precreate.c
@@ -494,16 +494,17 @@ static int osp_precreate_fids(const struct lu_env *env, struct osp_device *osp,
 	if (fid_is_idif(fid)) {
 		struct lu_fid	*last_fid;
 		struct ost_id	*oi = &osi->osi_oi;
+		int rc;
 
 		spin_lock(&osp->opd_pre_lock);
 		last_fid = &osp->opd_pre_last_created_fid;
 		fid_to_ostid(last_fid, oi);
 		end = min(ostid_id(oi) + *grow, IDIF_MAX_OID);
 		*grow = end - ostid_id(oi);
-		ostid_set_id(oi, ostid_id(oi) + *grow);
+		rc = ostid_set_id(oi, ostid_id(oi) + *grow);
 		spin_unlock(&osp->opd_pre_lock);
 
-		if (*grow == 0)
+		if (*grow == 0 || rc)
 			return 1;
 
 		ostid_to_fid(fid, oi, osp->opd_index);

lustre/include/lustre/Makefile.am
@@ -41,10 +41,8 @@ EXTRA_DIST = libiam.h \
 	liblustreapi.h \
 	ll_fiemap.h \
 	lustre_errno.h \
-	lustre_fid.h \
 	lustre_idl.h \
 	lustre_lfsck_user.h \
-	lustre_ostid.h \
 	lustre_user.h \
 	lustreapi.h \
 	lustre_barrier_user.h

lustre/include/lustre_fid.h
@@ -149,9 +149,9 @@
  */
 
 #include <libcfs/libcfs.h>
-#include <lustre/lustre_fid.h>
+#include <uapi/linux/lustre_fid.h>
 #include <lustre/lustre_idl.h>
-#include <lustre/lustre_ostid.h>
+#include <uapi/linux/lustre_ostid.h>
 
 struct lu_env;
 struct lu_site;
@@ -323,6 +323,53 @@ static inline int fid_seq_in_fldb(__u64 seq)
 {
 	return fid_seq_is_igif(seq) || fid_seq_is_norm(seq) ||
 	       fid_seq_is_root(seq) || fid_seq_is_dot(seq);
+}
+
+static inline void ost_layout_cpu_to_le(struct ost_layout *dst,
+					const struct ost_layout *src)
+{
+	dst->ol_stripe_size = __cpu_to_le32(src->ol_stripe_size);
+	dst->ol_stripe_count = __cpu_to_le32(src->ol_stripe_count);
+	dst->ol_comp_start = __cpu_to_le64(src->ol_comp_start);
+	dst->ol_comp_end = __cpu_to_le64(src->ol_comp_end);
+	dst->ol_comp_id = __cpu_to_le32(src->ol_comp_id);
+}
+
+static inline void ost_layout_le_to_cpu(struct ost_layout *dst,
+					const struct ost_layout *src)
+{
+	dst->ol_stripe_size = __le32_to_cpu(src->ol_stripe_size);
+	dst->ol_stripe_count = __le32_to_cpu(src->ol_stripe_count);
+	dst->ol_comp_start = __le64_to_cpu(src->ol_comp_start);
+	dst->ol_comp_end = __le64_to_cpu(src->ol_comp_end);
+	dst->ol_comp_id = __le32_to_cpu(src->ol_comp_id);
+}
+
+/* Both filter_fid_*cpu* functions not currently used */
+static inline void filter_fid_cpu_to_le(struct filter_fid *dst,
+					const struct filter_fid *src, int size)
+{
+	fid_cpu_to_le(&dst->ff_parent, &src->ff_parent);
+
+	if (size < sizeof(struct filter_fid))
+		memset(&dst->ff_layout, 0, sizeof(dst->ff_layout));
+	else
+		ost_layout_cpu_to_le(&dst->ff_layout, &src->ff_layout);
+
+	/* XXX: Add more if filter_fid is enlarged in the future. */
+}
+
+static inline void filter_fid_le_to_cpu(struct filter_fid *dst,
+					const struct filter_fid *src, int size)
+{
+	fid_le_to_cpu(&dst->ff_parent, &src->ff_parent);
+
+	if (size < sizeof(struct filter_fid))
+		memset(&dst->ff_layout, 0, sizeof(dst->ff_layout));
+	else
+		ost_layout_le_to_cpu(&dst->ff_layout, &src->ff_layout);
+
+	/* XXX: Add more if filter_fid is enlarged in the future. */
 }
 
 static inline void lu_last_id_fid(struct lu_fid *fid, __u64 seq, __u32 ost_idx)
@@ -658,6 +705,50 @@ static inline bool ostid_res_name_eq(const struct ost_id *oi,
 	}
 }
 
+/**
+ * Note: we need check oi_seq to decide where to set oi_id,
+ * so oi_seq should always be set ahead of oi_id.
+ */
+static inline int ostid_set_id(struct ost_id *oi, __u64 oid)
+{
+	if (fid_seq_is_mdt0(oi->oi.oi_seq)) {
+		if (oid >= IDIF_MAX_OID)
+			return -E2BIG;
+		oi->oi.oi_id = oid;
+	} else if (fid_is_idif(&oi->oi_fid)) {
+		if (oid >= IDIF_MAX_OID)
+			return -E2BIG;
+		oi->oi_fid.f_seq = fid_idif_seq(oid,
+						fid_idif_ost_idx(&oi->oi_fid));
+		oi->oi_fid.f_oid = oid;
+		oi->oi_fid.f_ver = oid >> 48;
+	} else {
+		if (oid >= OBIF_MAX_OID)
+			return -E2BIG;
+		oi->oi_fid.f_oid = oid;
+	}
+	return 0;
+}
+
+/* pack any OST FID into an ostid (id/seq) for the wire/disk */
+static inline int fid_to_ostid(const struct lu_fid *fid, struct ost_id *ostid)
+{
+	int rc = 0;
+
+	if (fid_seq_is_igif(fid->f_seq))
+		return -EBADF;
+
+	if (fid_is_idif(fid)) {
+		ostid_set_seq_mdt0(ostid);
+		rc = ostid_set_id(ostid, fid_idif_id(fid_seq(fid),
+				  fid_oid(fid), fid_ver(fid)));
+	} else {
+		ostid->oi_fid = *fid;
+	}
+
+	return rc;
+}
+
 /* The same as osc_build_res_name() */
 static inline void ost_fid_build_resid(const struct lu_fid *fid,
 				       struct ldlm_res_id *resname)
@@ -680,8 +771,12 @@ static inline void ost_fid_from_resid(struct lu_fid *fid,
 	if (fid_seq_is_mdt0(name->name[LUSTRE_RES_ID_VER_OID_OFF])) {
 		/* old resid */
 		struct ost_id oi;
+
 		ostid_set_seq(&oi, name->name[LUSTRE_RES_ID_VER_OID_OFF]);
-		ostid_set_id(&oi, name->name[LUSTRE_RES_ID_SEQ_OFF]);
+		if (ostid_set_id(&oi, name->name[LUSTRE_RES_ID_SEQ_OFF])) {
+			CERROR("Bad %llu to set " DOSTID "\n",
+			       name->name[LUSTRE_RES_ID_SEQ_OFF], POSTID(&oi));
+		}
 		ostid_to_fid(fid, &oi, ost_idx);
 	} else {
 		/* new resid */

lustre/include/lustre_log_user.h
@@ -38,7 +38,7 @@
 #ifndef _LUSTRE_LOG_USER_H
 #define _LUSTRE_LOG_USER_H
 
-#include <lustre/lustre_fid.h>
+#include <uapi/linux/lustre_fid.h>
 
 /*  Lustre logs use FIDs constructed from oi_id and oi_seq directly,
  *  without attempting to use the IGIF and IDIF ranges as is done

lustre/include/uapi/linux/Makefile.am
@@ -31,4 +31,6 @@
 
 EXTRA_DIST = \
 	lustre_disk.h \
-	lustre_ioctl.h
+	lustre_fid.h \
+	lustre_ioctl.h \
+	lustre_ostid.h

lustre/include/uapi/linux/lustre_fid.h
@@ -34,8 +34,8 @@
  * of three parts: sequence, Object ID, and version.
  *
  */
-#ifndef _LUSTRE_LUSTRE_FID_H_
-#define _LUSTRE_LUSTRE_FID_H_
+#ifndef _UAPI_LUSTRE_FID_H_
+#define _UAPI_LUSTRE_FID_H_
 
 #include <lustre/lustre_idl.h>
 
@@ -322,52 +322,6 @@ static inline void fid_be_to_cpu(struct lu_fid *dst, const struct lu_fid *src)
 	dst->f_seq = __be64_to_cpu(fid_seq(src));
 	dst->f_oid = __be32_to_cpu(fid_oid(src));
 	dst->f_ver = __be32_to_cpu(fid_ver(src));
-}
-
-static inline void ost_layout_cpu_to_le(struct ost_layout *dst,
-					const struct ost_layout *src)
-{
-	dst->ol_stripe_size = __cpu_to_le32(src->ol_stripe_size);
-	dst->ol_stripe_count = __cpu_to_le32(src->ol_stripe_count);
-	dst->ol_comp_start = __cpu_to_le64(src->ol_comp_start);
-	dst->ol_comp_end = __cpu_to_le64(src->ol_comp_end);
-	dst->ol_comp_id = __cpu_to_le32(src->ol_comp_id);
-}
-
-static inline void ost_layout_le_to_cpu(struct ost_layout *dst,
-					const struct ost_layout *src)
-{
-	dst->ol_stripe_size = __le32_to_cpu(src->ol_stripe_size);
-	dst->ol_stripe_count = __le32_to_cpu(src->ol_stripe_count);
-	dst->ol_comp_start = __le64_to_cpu(src->ol_comp_start);
-	dst->ol_comp_end = __le64_to_cpu(src->ol_comp_end);
-	dst->ol_comp_id = __le32_to_cpu(src->ol_comp_id);
-}
-
-static inline void filter_fid_cpu_to_le(struct filter_fid *dst,
-					const struct filter_fid *src, int size)
-{
-	fid_cpu_to_le(&dst->ff_parent, &src->ff_parent);
-
-	if (size < sizeof(struct filter_fid))
-		memset(&dst->ff_layout, 0, sizeof(dst->ff_layout));
-	else
-		ost_layout_cpu_to_le(&dst->ff_layout, &src->ff_layout);
-
-	/* XXX: Add more if filter_fid is enlarged in the future. */
-}
-
-static inline void filter_fid_le_to_cpu(struct filter_fid *dst,
-					const struct filter_fid *src, int size)
-{
-	fid_le_to_cpu(&dst->ff_parent, &src->ff_parent);
-
-	if (size < sizeof(struct filter_fid))
-		memset(&dst->ff_layout, 0, sizeof(dst->ff_layout));
-	else
-		ost_layout_le_to_cpu(&dst->ff_layout, &src->ff_layout);
-
-	/* XXX: Add more if filter_fid is enlarged in the future. */
 }
 
 static inline bool fid_is_sane(const struct lu_fid *fid)

lustre/include/uapi/linux/lustre_ostid.h
@@ -31,11 +31,18 @@
  * Define ost_id  associated functions
  */
 
-#ifndef _LUSTRE_OSTID_H_
-#define _LUSTRE_OSTID_H_
+#ifndef _UAPI_LUSTRE_OSTID_H_
+#define _UAPI_LUSTRE_OSTID_H_
 
-#include <libcfs/libcfs.h>
-#include <lustre/lustre_fid.h>
+/*
+ * This is due to us being out of kernel and the way the OpenSFS branch
+ * handles CFLAGS. Upstream will just have linux/lustre_fid.h
+ */
+#ifdef __KERNEL__
+#include <uapi/linux/lustre_fid.h>
+#else
+#include <linux/lustre_fid.h>
+#endif
 
 static inline __u64 lmm_oi_id(const struct ost_id *oi)
 {
@@ -133,39 +140,6 @@ static inline void ostid_set_seq_llog(struct ost_id *oi)
 	ostid_set_seq(oi, FID_SEQ_LLOG);
 }
 
-/**
- * Note: we need check oi_seq to decide where to set oi_id,
- * so oi_seq should always be set ahead of oi_id.
- */
-static inline void ostid_set_id(struct ost_id *oi, __u64 oid)
-{
-	if (fid_seq_is_mdt0(oi->oi.oi_seq)) {
-		if (oid >= IDIF_MAX_OID) {
-			CERROR("Bad %llu to set "DOSTID"\n",
-				(unsigned long long)oid, POSTID(oi));
-			return;
-		}
-		oi->oi.oi_id = oid;
-	} else if (fid_is_idif(&oi->oi_fid)) {
-		if (oid >= IDIF_MAX_OID) {
-			CERROR("Bad %llu to set "DOSTID"\n",
-				(unsigned long long)oid, POSTID(oi));
-			return;
-		}
-		oi->oi_fid.f_seq = fid_idif_seq(oid,
-						fid_idif_ost_idx(&oi->oi_fid));
-		oi->oi_fid.f_oid = oid;
-		oi->oi_fid.f_ver = oid >> 48;
-	} else {
-		if (oid > OBIF_MAX_OID) {
-			CERROR("Bad %llu to set "DOSTID"\n",
-				(unsigned long long)oid, POSTID(oi));
-			return;
-		}
-		oi->oi_fid.f_oid = oid;
-	}
-}
-
 static inline void ostid_cpu_to_le(const struct ost_id *src_oi,
 				   struct ost_id *dst_oi)
 {
@@ -186,25 +160,6 @@ static inline void ostid_le_to_cpu(const struct ost_id *src_oi,
 	} else {
 		fid_le_to_cpu(&dst_oi->oi_fid, &src_oi->oi_fid);
 	}
-}
-
-/* pack any OST FID into an ostid (id/seq) for the wire/disk */
-static inline int fid_to_ostid(const struct lu_fid *fid, struct ost_id *ostid)
-{
-	if (fid_seq_is_igif(fid->f_seq)) {
-		CERROR("bad IGIF, "DFID"\n", PFID(fid));
-		return -EBADF;
-	}
-
-	if (fid_is_idif(fid)) {
-		ostid_set_seq_mdt0(ostid);
-		ostid_set_id(ostid, fid_idif_id(fid_seq(fid), fid_oid(fid),
-						fid_ver(fid)));
-	} else {
-		ostid->oi_fid = *fid;
-	}
-
-	return 0;
 }
 
 /**
@@ -251,11 +206,8 @@ static inline int ostid_to_fid(struct lu_fid *fid, const struct ost_id *ostid,
 {
 	__u64 seq = ostid_seq(ostid);
 
-	if (ost_idx > 0xffff) {
-		CERROR("bad ost_idx, "DOSTID" ost_idx:%u\n", POSTID(ostid),
-		       ost_idx);
+	if (ost_idx > 0xffff)
 		return -EBADF;
-	}
 
 	if (fid_seq_is_mdt0(seq)) {
 		__u64 oid = ostid_id(ostid);
@@ -266,11 +218,9 @@ static inline int ostid_to_fid(struct lu_fid *fid, const struct ost_id *ostid,
 		 * been in production for years.  This can handle create rates
 		 * of 1M objects/s/OST for 9 years, or combinations thereof.
 		 */
-		if (oid >= IDIF_MAX_OID) {
-			CERROR("bad MDT0 id(1), "DOSTID" ost_idx:%u\n",
-			       POSTID(ostid), ost_idx);
+		if (oid >= IDIF_MAX_OID)
 			return -EBADF;
-		}
+
 		fid->f_seq = fid_idif_seq(oid, ost_idx);
 		/* truncate to 32 bits by assignment */
 		fid->f_oid = oid;
@@ -282,14 +232,12 @@ static inline int ostid_to_fid(struct lu_fid *fid, const struct ost_id *ostid,
 		 * maps legacy OST objects into the FID namespace.  In both
 		 * cases, we just pass the FID through, no conversion needed.
 		 */
-		if (ostid->oi_fid.f_ver) {
-			CERROR("bad MDT0 id(2), "DOSTID" ost_idx:%u\n",
-			       POSTID(ostid), ost_idx);
+		if (ostid->oi_fid.f_ver)
 			return -EBADF;
-		}
+
 		*fid = ostid->oi_fid;
 	}
 
 	return 0;
 }
-#endif
+#endif /* _UAPI_LUSTRE_OSTID_H_ */

commit 1f97bb8e7236971aaf3029fe3699db9baf721da1

LU-11536 ofd: ofd_create_hdl may return 0 in case of ENOSPC
ostid_set_id rewrites ofd_precreate_objects result after
"LU-6401 uapi: fix up lustre_ostid.h and lustre_fid.h".
This breakes the logic of osp_precreate_reserve() causing
osp_precreate_send() to return ESTALE instead of ENOSPC
when OST can't precreate objects.
osp_precreate_send() returns ESTALE because the result of
create is 0 while last created fid on OST is still the same
with local last_id:

fs1-OST0001-osc-MDT0000: precreate fid [0x100010000:0x571607f:0x0] <
local used fid [0x100010000:0x571607f:0x0]: rc = -116
fs1-OST0001-osc-MDT0000: precreate failed opd_pre_status -116
fs1-OST0001-osc-MDT0000: cannot precreate objects: rc = -116

Change-Id: I4dc057c201253cab14e63c1f06bd5b0d56b5ad2d
Signed-off-by: Sergey Cheremencev <c17829@cray.com>
Fixes: 34acfbc2bfe502d18c12ba35771bde7c4a0f7906
Reviewed-on: https://es-gerrit.dev.cray.com/153462
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Artem Blagodarenko <c17828@cray.com>
Tested-by: Alexander Lezhoev <c17454@cray.com>
Reviewed-on: https://review.whamcloud.com/33390
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_dev.c
@@ -1621,6 +1621,7 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 		u64 next_id;
 		int created = 0;
 		int count;
+		int rc2;
 
 		if (!(oa->o_valid & OBD_MD_FLFLAGS) ||
 		    !(oa->o_flags & OBD_FL_DELORPHAN)) {
@@ -1708,7 +1709,8 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 			granted = 0;
 		}
 
-		rc = ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq));
+		rc2 = ostid_set_id(&rep_oa->o_oi, ofd_seq_last_oid(oseq));
+		rc = rc ? : rc2;
 	}
 	EXIT;
 	ofd_counter_incr(exp, LPROC_OFD_STATS_CREATE,

lustre/osp/osp_precreate.c
@@ -665,7 +665,7 @@ static int osp_precreate_send(const struct lu_env *env, struct osp_device *d)
 
 ready:
 	if (osp_fid_diff(fid, &d->opd_pre_used_fid) <= 0) {
-		CERROR("%s: precreate fid "DFID" < local used fid "DFID
+		CERROR("%s: precreate fid "DFID" <= local used fid "DFID
 		       ": rc = %d\n", d->opd_obd->obd_name,
 		       PFID(fid), PFID(&d->opd_pre_used_fid), -ESTALE);
 		GOTO(out_req, rc = -ESTALE);