lustre/lustre-release

commit b500d5193360711a6c6b07497f34e61cc590cf19

LU-10018 protocol: MDT as a statfs proxy
MDT can act as a proxy for statfs data. this should
make df faster (RTT vs RTT*(#MDTs+1)) and enable
idling connections so that clients don't connect to
each OST just to report statfs data. the protocol
has been changing slightly to let MDT differentiate
self and aggregated statfs.

also, obd_statfs has got a new field "granted" where
OST reports how much space has been granted to the
requesting MDT so that space can be added to available
space.

client's NID is used to distribute MDS_STATFS among
MDTS.

Change-Id: I59e03cb5abf809ae8820f874ec51dd2b74e1806c
Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Reviewed-on: https://review.whamcloud.com/29136
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd_class.h
@@ -47,6 +47,7 @@
                                          * obd_osfs_age */
 #define OBD_STATFS_FOR_MDT0	0x0004	/* The statfs is only for retrieving
 					 * information from MDT0. */
+#define OBD_STATFS_SUM		0x0008	/* get aggregated statfs from MDT */
 
 extern rwlock_t obd_dev_lock;
 
@@ -1081,7 +1082,10 @@ static inline int obd_statfs(const struct lu_env *env, struct obd_export *exp,
 
 	CDEBUG(D_SUPER, "osfs %lld, max_age %lld\n",
                obd->obd_osfs_age, max_age);
-	if (obd->obd_osfs_age < max_age) {
+	/* ignore cache if aggregated isn't expected */
+	if (obd->obd_osfs_age < max_age ||
+	    ((obd->obd_osfs.os_state & OS_STATE_SUM) &&
+	     !(flags & OBD_STATFS_SUM))) {
                 rc = OBP(obd, statfs)(env, exp, osfs, max_age, flags);
                 if (rc == 0) {
 			spin_lock(&obd->obd_osfs_lock);

lustre/llite/llite_lib.c
@@ -227,7 +227,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 
 	data->ocd_connect_flags2 = OBD_CONNECT2_FLR |
 				   OBD_CONNECT2_LOCK_CONVERT |
-				   OBD_CONNECT2_DIR_MIGRATE;
+				   OBD_CONNECT2_DIR_MIGRATE |
+				   OBD_CONNECT2_SUM_STATFS;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
         if (sbi->ll_flags & LL_SBI_LRU_RESIZE)
@@ -1813,6 +1814,9 @@ int ll_statfs_internal(struct ll_sb_info *sbi, struct obd_statfs *osfs,
 	CDEBUG(D_SUPER, "MDC blocks %llu/%llu objects %llu/%llu\n",
                osfs->os_bavail, osfs->os_blocks, osfs->os_ffree,osfs->os_files);
 
+	if (osfs->os_state & OS_STATE_SUM)
+		GOTO(out, rc);
+
         if (sbi->ll_flags & LL_SBI_LAZYSTATFS)
                 flags |= OBD_STATFS_NODELAY;
 
@@ -1841,6 +1845,7 @@ int ll_statfs_internal(struct ll_sb_info *sbi, struct obd_statfs *osfs,
                 osfs->os_ffree = obd_osfs.os_ffree;
         }
 
+out:
         RETURN(rc);
 }
 int ll_statfs(struct dentry *de, struct kstatfs *sfs)
@@ -1854,7 +1859,7 @@ int ll_statfs(struct dentry *de, struct kstatfs *sfs)
         ll_stats_ops_tally(ll_s2sbi(sb), LPROC_LL_STAFS, 1);
 
 	/* Some amount of caching on the client is allowed */
-	rc = ll_statfs_internal(ll_s2sbi(sb), &osfs, 0);
+	rc = ll_statfs_internal(ll_s2sbi(sb), &osfs, OBD_STATFS_SUM);
 	if (rc)
 		return rc;
 

lustre/lmv/lmv_obd.c
@@ -1335,6 +1335,32 @@ out:
 	RETURN(rc);
 }
 
+static int lmv_select_statfs_mdt(struct lmv_obd *lmv, __u32 flags)
+{
+	int i;
+
+	if (flags & OBD_STATFS_FOR_MDT0)
+		return 0;
+
+	if (lmv->lmv_statfs_start || lmv->desc.ld_tgt_count == 1)
+		return lmv->lmv_statfs_start;
+
+	/* choose initial MDT for this client */
+	for (i = 0;; i++) {
+		struct lnet_process_id lnet_id;
+		if (LNetGetId(i, &lnet_id) == -ENOENT)
+			break;
+
+		if (LNET_NETTYP(LNET_NIDNET(lnet_id.nid)) != LOLND) {
+			lmv->lmv_statfs_start =
+				lnet_id.nid % lmv->desc.ld_tgt_count;
+			break;
+		}
+	}
+
+	return lmv->lmv_statfs_start;
+}
+
 static int lmv_statfs(const struct lu_env *env, struct obd_export *exp,
 		      struct obd_statfs *osfs, time64_t max_age, __u32 flags)
 {
@@ -1342,42 +1368,52 @@ static int lmv_statfs(const struct lu_env *env, struct obd_export *exp,
 	struct lmv_obd		*lmv = &obd->u.lmv;
 	struct obd_statfs	*temp;
 	int			 rc = 0;
-	__u32			 i;
+	__u32			 i, idx;
 	ENTRY;
 
         OBD_ALLOC(temp, sizeof(*temp));
         if (temp == NULL)
                 RETURN(-ENOMEM);
 
-        for (i = 0; i < lmv->desc.ld_tgt_count; i++) {
-		if (lmv->tgts[i] == NULL || lmv->tgts[i]->ltd_exp == NULL)
+	/* distribute statfs among MDTs */
+	idx = lmv_select_statfs_mdt(lmv, flags);
+
+	for (i = 0; i < lmv->desc.ld_tgt_count; i++, idx++) {
+		idx = idx % lmv->desc.ld_tgt_count;
+		if (lmv->tgts[idx] == NULL || lmv->tgts[idx]->ltd_exp == NULL)
 			continue;
 
-		rc = obd_statfs(env, lmv->tgts[i]->ltd_exp, temp,
+		rc = obd_statfs(env, lmv->tgts[idx]->ltd_exp, temp,
 				max_age, flags);
 		if (rc) {
 			CERROR("can't stat MDS #%d (%s), error %d\n", i,
-			       lmv->tgts[i]->ltd_exp->exp_obd->obd_name,
+			       lmv->tgts[idx]->ltd_exp->exp_obd->obd_name,
 			       rc);
 			GOTO(out_free_temp, rc);
 		}
 
+		if (temp->os_state & OS_STATE_SUM ||
+		    flags == OBD_STATFS_FOR_MDT0) {
+			/* reset to the last aggregated values
+			 * and don't sum with non-aggrated data */
+			/* If the statfs is from mount, it needs to retrieve
+			 * necessary information from MDT0. i.e. mount does
+			 * not need the merged osfs from all of MDT. Also
+			 * clients can be mounted as long as MDT0 is in
+			 * service */
+			*osfs = *temp;
+			break;
+		}
+
 		if (i == 0) {
 			*osfs = *temp;
-			/* If the statfs is from mount, it will needs
-			 * retrieve necessary information from MDT0.
-			 * i.e. mount does not need the merged osfs
-			 * from all of MDT.
-			 * And also clients can be mounted as long as
-			 * MDT0 is in service*/
-			if (flags & OBD_STATFS_FOR_MDT0)
-				GOTO(out_free_temp, rc);
-                } else {
-                        osfs->os_bavail += temp->os_bavail;
-                        osfs->os_blocks += temp->os_blocks;
-                        osfs->os_ffree += temp->os_ffree;
-                        osfs->os_files += temp->os_files;
-                }
+		} else {
+			osfs->os_bavail += temp->os_bavail;
+			osfs->os_blocks += temp->os_blocks;
+			osfs->os_ffree += temp->os_ffree;
+			osfs->os_files += temp->os_files;
+			osfs->os_granted += temp->os_granted;
+		}
         }
 
         EXIT;

lustre/lod/lod_dev.c
@@ -1263,6 +1263,30 @@ static int lod_root_get(const struct lu_env *env,
 	return dt_root_get(env, dt2lod_dev(dev)->lod_child, f);
 }
 
+static void lod_statfs_sum(struct obd_statfs *sfs,
+			     struct obd_statfs *ost_sfs, int *bs)
+{
+	while (ost_sfs->os_bsize < *bs) {
+		*bs >>= 1;
+		sfs->os_bsize >>= 1;
+		sfs->os_bavail <<= 1;
+		sfs->os_blocks <<= 1;
+		sfs->os_bfree <<= 1;
+		sfs->os_granted <<= 1;
+	}
+	while (ost_sfs->os_bsize > *bs) {
+		ost_sfs->os_bsize >>= 1;
+		ost_sfs->os_bavail <<= 1;
+		ost_sfs->os_blocks <<= 1;
+		ost_sfs->os_bfree <<= 1;
+		ost_sfs->os_granted <<= 1;
+	}
+	sfs->os_bavail += ost_sfs->os_bavail;
+	sfs->os_blocks += ost_sfs->os_blocks;
+	sfs->os_bfree += ost_sfs->os_bfree;
+	sfs->os_granted += ost_sfs->os_granted;
+}
+
 /**
  * Implementation of dt_device_operations::dt_statfs() for LOD
  *
@@ -1271,7 +1295,73 @@ static int lod_root_get(const struct lu_env *env,
 static int lod_statfs(const struct lu_env *env,
 		      struct dt_device *dev, struct obd_statfs *sfs)
 {
-	return dt_statfs(env, dt2lod_dev(dev)->lod_child, sfs);
+	struct lod_device   *lod = dt2lod_dev(dev);
+	struct lod_ost_desc *ost;
+	struct lod_mdt_desc *mdt;
+	struct obd_statfs    ost_sfs;
+	int i, rc, bs;
+	bool mdtonly;
+
+	rc = dt_statfs(env, dt2lod_dev(dev)->lod_child, sfs);
+	if (rc)
+		GOTO(out, rc);
+
+	bs = sfs->os_bsize;
+
+	sfs->os_bavail = 0;
+	sfs->os_blocks = 0;
+	sfs->os_bfree = 0;
+	sfs->os_granted = 0;
+
+	lod_getref(&lod->lod_mdt_descs);
+	lod_foreach_mdt(lod, i) {
+		mdt = MDT_TGT(lod, i);
+		LASSERT(mdt && mdt->ltd_mdt);
+		rc = dt_statfs(env, mdt->ltd_mdt, &ost_sfs);
+		/* ignore errors */
+		if (rc)
+			continue;
+		sfs->os_files += ost_sfs.os_files;
+		sfs->os_ffree += ost_sfs.os_ffree;
+		lod_statfs_sum(sfs, &ost_sfs, &bs);
+	}
+	lod_putref(lod, &lod->lod_mdt_descs);
+
+	/* at some point we can check whether DoM is enabled and
+	 * decide how to account MDT space. for simplicity let's
+	 * just fallback to pre-DoM policy if any OST is alive */
+	mdtonly = true;
+
+	lod_getref(&lod->lod_ost_descs);
+	lod_foreach_ost(lod, i) {
+		ost = OST_TGT(lod, i);
+		LASSERT(ost && ost->ltd_ost);
+		rc = dt_statfs(env, ost->ltd_ost, &ost_sfs);
+		/* ignore errors */
+		if (rc || ost_sfs.os_bsize == 0)
+			continue;
+		if (mdtonly) {
+			/* if only MDTs and DoM report MDT space,
+			 * otherwise only OST space */
+			sfs->os_bavail = 0;
+			sfs->os_blocks = 0;
+			sfs->os_bfree = 0;
+			sfs->os_granted = 0;
+			mdtonly = false;
+		}
+		ost_sfs.os_bavail += ost_sfs.os_granted;
+		lod_statfs_sum(sfs, &ost_sfs, &bs);
+		LASSERTF(bs == ost_sfs.os_bsize, "%d != %d\n",
+			(int)sfs->os_bsize, (int)ost_sfs.os_bsize);
+	}
+	lod_putref(lod, &lod->lod_ost_descs);
+	sfs->os_state |= OS_STATE_SUM;
+
+	/* a single successful statfs should be enough */
+	rc = 0;
+
+out:
+	RETURN(rc);
 }
 
 /**

lustre/tests/sanity.sh
@@ -10756,13 +10756,19 @@ test_133b() {
 		ls -l ${testdir}/${tfile} > /dev/null|| error "ls failed"
 		check_stats $SINGLEMDS "getattr" 1
 	fi
+	rm -rf $DIR/${tdir}
+
+	# when DNE is enabled, MDT uses STATFS RPC to ping other targets
+	# so the check below is not reliable
+	[ $MDSCOUNT -eq 1 ] || return 0
+
 	# Sleep to avoid a cached response.
 	#define OBD_STATFS_CACHE_SECONDS 1
 	sleep 2
 	$LFS df || error "lfs failed"
 	check_stats $SINGLEMDS "statfs" 1
 
-	rm -rf $DIR/${tdir}
+	return 0
 }
 run_test 133b "Verifying extra MDT stats =================================="
 

lustre/include/md_object.h
@@ -648,6 +648,14 @@ static inline int mdo_unlink(const struct lu_env *env,
 	return p->mo_dir_ops->mdo_unlink(env, p, c, lname, ma, no_name);
 }
 
+static inline int mdo_statfs(const struct lu_env *env,
+			     struct md_device *m,
+			     struct obd_statfs *sfs)
+{
+	LASSERT(m->md_ops->mdo_statfs);
+	return m->md_ops->mdo_statfs(env, m, sfs);
+}
+
 /**
  * Used in MDD/OUT layer for object lock rule
  **/

lustre/include/obd.h
@@ -446,6 +446,8 @@ struct lmv_obd {
 
 	__u32			tgts_size; /* size of tgts array */
 	struct lmv_tgt_desc	**tgts;
+	int			lmv_statfs_start;
+
 
 	struct obd_connect_data	conn_data;
 	struct kobject		*lmv_tgts_kobj;

lustre/include/obd_support.h
@@ -179,7 +179,9 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_MDS_GET_ROOT_NET	 0x11b
 #define OBD_FAIL_MDS_GET_ROOT_PACK	 0x11c
 #define OBD_FAIL_MDS_STATFS_PACK         0x11d
+#define OBD_FAIL_MDS_STATFS_SUM_PACK     0x11d
 #define OBD_FAIL_MDS_STATFS_NET          0x11e
+#define OBD_FAIL_MDS_STATFS_SUM_NET      0x11e
 #define OBD_FAIL_MDS_GETATTR_NAME_NET    0x11f
 #define OBD_FAIL_MDS_PIN_NET             0x120
 #define OBD_FAIL_MDS_UNPIN_NET           0x121

commit a829595add808d0fb09bab525c500d0aa6955883

LU-11721 lod: limit statfs ffree if less than OST ffree
If the OSTs report fewer total free objects than the MDTs, then
use the free files count reported by the OSTs, since it represents
the minimum number of files that can be created in the filesystem
(creating more may be possible, but this depends on other factors).
This has always been what ll_statfs_internal() reports, but the
statfs aggregation via the MDT missed this step in lod_statfs().

Fix a minor defect in sanity test_418() that would let it loop
forever until the test was killed due to timeout if the "df -i"
and "lfs df -i" output did not converge.

Fixes: b500d5193360 ("LU-10018 protocol: MDT as a statfs proxy")
Fixes: 263e80f4572b ("LU-11721 tests: wait for statfs to update ...")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Id8d7b7edfd854f1ec30bfbbb85f04b0c973ebbe5
Reviewed-on: https://review.whamcloud.com/34167
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Nikitas Angelinas <nangelinas@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd_class.h
@@ -1020,8 +1020,9 @@ static inline int obd_statfs_async(struct obd_export *exp,
 
 	obd = exp->exp_obd;
 	if (!obd->obd_type || !obd->obd_type->typ_dt_ops->o_statfs) {
-		CERROR("%s: no %s operation\n", obd->obd_name, __func__);
-		RETURN(-EOPNOTSUPP);
+		rc = -EOPNOTSUPP;
+		CERROR("%s: no statfs operation: rc = %d\n", obd->obd_name, rc);
+		RETURN(rc);
 	}
 
 	CDEBUG(D_SUPER, "%s: age %lld, max_age %lld\n",

lustre/llite/llite_lib.c
@@ -1797,57 +1797,54 @@ int ll_setattr(struct dentry *de, struct iattr *attr)
 int ll_statfs_internal(struct ll_sb_info *sbi, struct obd_statfs *osfs,
 		       u32 flags)
 {
-	struct obd_statfs obd_osfs;
+	struct obd_statfs obd_osfs = { 0 };
 	time64_t max_age;
 	int rc;
 
 	ENTRY;
 	max_age = ktime_get_seconds() - OBD_STATFS_CACHE_SECONDS;
 
-        rc = obd_statfs(NULL, sbi->ll_md_exp, osfs, max_age, flags);
-        if (rc) {
-                CERROR("md_statfs fails: rc = %d\n", rc);
-                RETURN(rc);
-        }
+	rc = obd_statfs(NULL, sbi->ll_md_exp, osfs, max_age, flags);
+	if (rc)
+		RETURN(rc);
 
 	osfs->os_type = LL_SUPER_MAGIC;
 
 	CDEBUG(D_SUPER, "MDC blocks %llu/%llu objects %llu/%llu\n",
-               osfs->os_bavail, osfs->os_blocks, osfs->os_ffree,osfs->os_files);
+	      osfs->os_bavail, osfs->os_blocks, osfs->os_ffree, osfs->os_files);
 
 	if (osfs->os_state & OS_STATE_SUM)
 		GOTO(out, rc);
 
-        if (sbi->ll_flags & LL_SBI_LAZYSTATFS)
-                flags |= OBD_STATFS_NODELAY;
+	if (sbi->ll_flags & LL_SBI_LAZYSTATFS)
+		flags |= OBD_STATFS_NODELAY;
 
 	rc = obd_statfs(NULL, sbi->ll_dt_exp, &obd_osfs, max_age, flags);
-        if (rc) {
-                CERROR("obd_statfs fails: rc = %d\n", rc);
-                RETURN(rc);
-        }
+	if (rc) /* Possibly a filesystem with no OSTs.  Report MDT totals. */
+		GOTO(out, rc = 0);
 
 	CDEBUG(D_SUPER, "OSC blocks %llu/%llu objects %llu/%llu\n",
-               obd_osfs.os_bavail, obd_osfs.os_blocks, obd_osfs.os_ffree,
-               obd_osfs.os_files);
+	       obd_osfs.os_bavail, obd_osfs.os_blocks, obd_osfs.os_ffree,
+	       obd_osfs.os_files);
 
-        osfs->os_bsize = obd_osfs.os_bsize;
-        osfs->os_blocks = obd_osfs.os_blocks;
-        osfs->os_bfree = obd_osfs.os_bfree;
-        osfs->os_bavail = obd_osfs.os_bavail;
+	osfs->os_bsize = obd_osfs.os_bsize;
+	osfs->os_blocks = obd_osfs.os_blocks;
+	osfs->os_bfree = obd_osfs.os_bfree;
+	osfs->os_bavail = obd_osfs.os_bavail;
 
-        /* If we don't have as many objects free on the OST as inodes
-         * on the MDS, we reduce the total number of inodes to
-         * compensate, so that the "inodes in use" number is correct.
-         */
-        if (obd_osfs.os_ffree < osfs->os_ffree) {
-                osfs->os_files = (osfs->os_files - osfs->os_ffree) +
-                        obd_osfs.os_ffree;
-                osfs->os_ffree = obd_osfs.os_ffree;
-        }
+	/* If we have _some_ OSTs, but don't have as many free objects on the
+	 * OSTs as inodes on the MDTs, reduce the reported number of inodes
+	 * to compensate, so that the "inodes in use" number is correct.
+	 * This should be kept in sync with lod_statfs() behaviour.
+	 */
+	if (obd_osfs.os_files && obd_osfs.os_ffree < osfs->os_ffree) {
+		osfs->os_files = (osfs->os_files - osfs->os_ffree) +
+				 obd_osfs.os_ffree;
+		osfs->os_ffree = obd_osfs.os_ffree;
+	}
 
 out:
-        RETURN(rc);
+	RETURN(rc);
 }
 int ll_statfs(struct dentry *de, struct kstatfs *sfs)
 {

lustre/lmv/lmv_obd.c
@@ -1410,8 +1410,8 @@ static int lmv_statfs(const struct lu_env *env, struct obd_export *exp,
 		rc = obd_statfs(env, lmv->tgts[idx]->ltd_exp, temp,
 				max_age, flags);
 		if (rc) {
-			CERROR("can't stat MDS #%d (%s), error %d\n", i,
-			       lmv->tgts[idx]->ltd_exp->exp_obd->obd_name,
+			CERROR("%s: can't stat MDS #%d: rc = %d\n",
+			       lmv->tgts[idx]->ltd_exp->exp_obd->obd_name, i,
 			       rc);
 			GOTO(out_free_temp, rc);
 		}

lustre/lod/lod_dev.c
@@ -1327,8 +1327,9 @@ static int lod_statfs(const struct lu_env *env,
 	struct lod_ost_desc *ost;
 	struct lod_mdt_desc *mdt;
 	struct obd_statfs ost_sfs;
+	u64 ost_files = 0;
+	u64 ost_ffree = 0;
 	int i, rc, bs;
-	bool mdtonly;
 
 	rc = dt_statfs(env, dt2lod_dev(dev)->lod_child, sfs);
 	if (rc)
@@ -1360,8 +1361,6 @@ static int lod_statfs(const struct lu_env *env,
 	 * decide how to account MDT space. for simplicity let's
 	 * just fallback to pre-DoM policy if any OST is alive
 	 */
-	mdtonly = true;
-
 	lod_getref(&lod->lod_ost_descs);
 	lod_foreach_ost(lod, i) {
 		ost = OST_TGT(lod, i);
@@ -1370,17 +1369,18 @@ static int lod_statfs(const struct lu_env *env,
 		/* ignore errors */
 		if (rc || ost_sfs.os_bsize == 0)
 			continue;
-		if (mdtonly) {
+		if (!ost_files) {
 			/*
-			 * if only MDTs and DoM report MDT space,
-			 * otherwise only OST space
+			 * if only MDTs with DoM then report only MDT blocks,
+			 * otherwise show only OST blocks, and DoM is "free"
 			 */
 			sfs->os_bavail = 0;
 			sfs->os_blocks = 0;
 			sfs->os_bfree = 0;
 			sfs->os_granted = 0;
-			mdtonly = false;
 		}
+		ost_files += sfs->os_files;
+		ost_ffree += sfs->os_ffree;
 		ost_sfs.os_bavail += ost_sfs.os_granted;
 		lod_statfs_sum(sfs, &ost_sfs, &bs);
 		LASSERTF(bs == ost_sfs.os_bsize, "%d != %d\n",
@@ -1388,6 +1388,16 @@ static int lod_statfs(const struct lu_env *env,
 	}
 	lod_putref(lod, &lod->lod_ost_descs);
 	sfs->os_state |= OS_STATE_SUM;
+
+	/* If we have _some_ OSTs, but don't have as many free objects on the
+	 * OSTs as inodes on the MDTs, reduce the reported number of inodes
+	 * to compensate, so that the "inodes in use" number is correct.
+	 * This should be kept in sync with ll_statfs_internal().
+	 */
+	if (ost_files && ost_ffree < sfs->os_ffree) {
+		sfs->os_files = (sfs->os_files - sfs->os_ffree) + ost_ffree;
+		sfs->os_ffree = ost_ffree;
+	}
 
 	/* a single successful statfs should be enough */
 	rc = 0;

lustre/tests/sanity.sh
@@ -19525,40 +19525,40 @@ check_lfs_df() {
 	local inodes
 	local df_out
 	local lfs_df_out
-	local tries=100
-	local count=0
+	local count
 	local passed=false
 
 	# blocks or inodes
 	[ "$1" == "blocks" ] && inodes= || inodes="-i"
 
-	while (( count < tries )); do
+	for count in {1..100}; do
 		cancel_lru_locks
 		sync; sleep 0.2
 
 		# read the lines of interest
-		df_out=($(df $inodes $dir | tail -n +2)) ||
+		df_out=($(df -P $inodes $dir | tail -n +2)) ||
 			error "df $inodes $dir | tail -n +2 failed"
 		lfs_df_out=($($LFS df $inodes $dir | grep summary:)) ||
 			error "lfs df $inodes $dir | grep summary: failed"
 
 		# skip first substrings of each output as they are different
-		# <NID>:/<fsname for df, filesystem_summary: for lfs df
-		df_out=(${df_out[@]:1})
-		lfs_df_out=(${lfs_df_out[@]:1})
-
+		# "<NID>:/<fsname>" for df, "filesystem_summary:" for lfs df
 		# compare the two outputs
 		passed=true
-
-		for i in {0..4}; do
+		for i in {1..5}; do
 			[ "${df_out[i]}" != "${lfs_df_out[i]}" ] && passed=false
 		done
 		$passed && break
 	done
 
-	$passed || error "df and lfs df $1 output mismatch: "	\
-			 "df ${inodes}: ${df_out[*]}, "		\
-			 "lfs df ${inodes}: ${lfs_df_out[*]}"
+	if ! $passed; then
+		df -P $inodes $dir
+		echo
+		lfs df $inodes $dir
+		error "df and lfs df $1 output mismatch: "	\
+		      "df ${inodes}: ${df_out[*]}, "		\
+		      "lfs df ${inodes}: ${lfs_df_out[*]}"
+	fi
 }
 
 test_418() {