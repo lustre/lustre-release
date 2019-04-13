lustre/lustre-release

commit 39f63cf54c624d89439b5b473035c1afe35e10fa

LU-4017 quota: add setting/getting project id function
Extend Attr RPC to support project ID attribute, new
ioctl is introduced to get/set project id, it is kept
same ioctl number as VFS, you could use:

lsattr -p <file>
chattr -p <file>

Redefine LL_MAXQUOTAS as 3, change
@qsd_acct_failed into qqi, otherwise osd-zfs
could not use USR/GRP quota normally.

Remove osd_quota_migration(), it only output
some messages in case upgrading from 1.8 which
give us extra efforts for project quota, remove
it indead.

also fix a typo bug in osp_attr_set().

Change-Id: I4b537be9efa29fda730592b6d5e41c6b60fb6c0d
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/26202
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
Tested-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -1955,6 +1955,9 @@ static void osd_object_delete(const struct lu_env *env, struct lu_object *l)
 
 			qi->lqi_id.qid_uid = gid;
 			qsd_op_adjust(env, qsd, &qi->lqi_id, GRPQUOTA);
+
+			qi->lqi_id.qid_uid = i_projid_read(inode);
+			qsd_op_adjust(env, qsd, &qi->lqi_id, PRJQUOTA);
 		}
         }
 }
@@ -4351,10 +4354,6 @@ static int osd_index_try(const struct lu_env *env, struct dt_object *dt,
                         result = -ENOTDIR;
         }
         LINVRNT(osd_invariant(obj));
-
-	if (result == 0 && feat == &dt_quota_glb_features &&
-	    fid_seq(lu_object_fid(&dt->do_lu)) == FID_SEQ_QUOTA_GLB)
-		result = osd_quota_migration(env, dt);
 
         return result;
 }

lustre/contrib/wireshark/packet-lustre.c
@@ -549,7 +549,7 @@ static int hf_lustre_mdt_rec_setattr_sa_ctime = -1;
 static int hf_lustre_mdt_rec_setattr_sa_attr_flags = -1;
 static int hf_lustre_mdt_rec_setattr_sa_mode = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_2 = -1;
-static int hf_lustre_mdt_rec_setattr_sa_padding_3 = -1;
+static int hf_lustre_mdt_rec_setattr_sa_projid = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_4 = -1;
 static int hf_lustre_mdt_rec_setattr_sa_padding_5 = -1;
 
@@ -3198,7 +3198,7 @@ lustre_dissect_struct_mdt_body(tvbuff_t *tvb _U_, int offset _U_,
 /* IDL: 	uint32 sa_attr_flags; */
 /* IDL: 	uint32 sa_mode; */
 /* IDL: 	uint32 sa_padding_2; */
-/* IDL: 	uint32 sa_padding_3; */
+/* IDL: 	uint32 sa_projid; */
 /* IDL: 	uint32 sa_padding_4; */
 /* IDL: 	uint32 sa_padding_5; */
 /* IDL: } */
@@ -3393,9 +3393,9 @@ lustre_dissect_element_mdt_rec_setattr_sa_padding_2(tvbuff_t *tvb _U_, int offse
 }
 
 static int
-lustre_dissect_element_mdt_rec_setattr_sa_padding_3(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_)
+lustre_dissect_element_mdt_rec_setattr_sa_projid(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_)
 {
-  offset=dissect_uint32(tvb, offset, pinfo, tree,  hf_lustre_mdt_rec_setattr_sa_padding_3);
+  offset=dissect_uint32(tvb, offset, pinfo, tree,  hf_lustre_mdt_rec_setattr_sa_projid);
 
   return offset;
 }
@@ -3479,7 +3479,7 @@ lustre_dissect_struct_mdt_rec_setattr(tvbuff_t *tvb _U_, int offset _U_, packet_
 
   offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_2(tvb, offset, pinfo, tree);
 
-  offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_3(tvb, offset, pinfo, tree);
+  offset=lustre_dissect_element_mdt_rec_setattr_sa_projid(tvb, offset, pinfo, tree);
 
   offset=lustre_dissect_element_mdt_rec_setattr_sa_padding_4(tvb, offset, pinfo, tree);
 
@@ -9909,8 +9909,8 @@ void proto_register_dcerpc_lustre(void)
       { "Sa Mode", "lustre.mdt_rec_setattr.sa_mode", FT_UINT32, BASE_OCT, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_2,
       { "Sa Padding 2", "lustre.mdt_rec_setattr.sa_padding_2", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
-    { &hf_lustre_mdt_rec_setattr_sa_padding_3,
-      { "Sa Padding 3", "lustre.mdt_rec_setattr.sa_padding_3", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
+    { &hf_lustre_mdt_rec_setattr_sa_projid,
+      { "Sa Padding 3", "lustre.mdt_rec_setattr.sa_projid", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_4,
       { "Sa Padding 4", "lustre.mdt_rec_setattr.sa_padding_4", FT_UINT32, BASE_DEC, NULL, 0, "", HFILL }},
     { &hf_lustre_mdt_rec_setattr_sa_padding_5,

lustre/include/cl_object.h
@@ -166,6 +166,9 @@ struct cl_attr {
 
 	/* nlink of the directory */
 	__u64  cat_nlink;
+
+	/* Project identifier for quota purpose. */
+	__u32  cat_projid;
 };
 
 /**
@@ -179,7 +182,8 @@ enum cl_attr_valid {
         CAT_CTIME  = 1 << 5,
         CAT_BLOCKS = 1 << 6,
         CAT_UID    = 1 << 7,
-        CAT_GID    = 1 << 8
+	CAT_GID    = 1 << 8,
+	CAT_PROJID = 1 << 9
 };
 
 /**

lustre/include/lustre/lustre_idl.h
@@ -1744,7 +1744,7 @@ struct mdt_rec_setattr {
         __u32           sa_attr_flags;
         __u32           sa_mode;
 	__u32           sa_bias;      /* some operation flags */
-        __u32           sa_padding_3;
+	__u32		sa_projid;
         __u32           sa_padding_4;
         __u32           sa_padding_5;
 };

lustre/include/lustre/lustre_user.h
@@ -65,7 +65,7 @@
  * Should be removed as soon as system header is updated.
  */
 #undef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #undef INITQFNAMES
 #define INITQFNAMES { \
     "user",	/* USRQUOTA */ \
@@ -361,6 +361,25 @@ struct ll_futimes_3 {
 #define LL_IOC_FID2MDTIDX		_IOWR('f', 248, struct lu_fid)
 #define LL_IOC_GETPARENT		_IOWR('f', 249, struct getparent)
 #define LL_IOC_LADVISE			_IOR('f', 250, struct llapi_lu_ladvise)
+
+#ifdef	FS_IOC_FSGETXATTR
+#define LL_IOC_FSGETXATTR		FS_IOC_FSGETXATTR
+#define LL_IOC_FSSETXATTR		FS_IOC_FSSETXATTR
+#else
+/*
+ * Structure for FS_IOC_FSGETXATTR and FS_IOC_FSSETXATTR.
+*/
+struct fsxattr {
+	__u32           fsx_xflags;     /* xflags field value (get/set) */
+	__u32           fsx_extsize;    /* extsize field value (get/set)*/
+	__u32           fsx_nextents;   /* nextents field value (get)   */
+	__u32           fsx_projid;     /* project identifier (get/set) */
+	unsigned char   fsx_pad[12];
+};
+#define LL_IOC_FSGETXATTR		_IOR('X', 31, struct fsxattr)
+#define LL_IOC_FSSETXATTR		_IOW('X', 32, struct fsxattr)
+#endif
+
 
 /* Lease types for use as arg and return of LL_IOC_{GET,SET}_LEASE ioctl. */
 enum ll_lease_type {

lustre/include/lustre/lustreapi.h
@@ -43,7 +43,7 @@
 #include <lustre/lustre_user.h>
 
 #ifndef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #endif
 
 extern bool liblustreapi_initialized;

lustre/include/lustre_quota.h
@@ -48,7 +48,7 @@
 #endif
 
 #ifndef LL_MAXQUOTAS
-#define LL_MAXQUOTAS 2
+#define LL_MAXQUOTAS 3
 #endif
 
 struct lquota_id_info;

lustre/include/obd.h
@@ -836,6 +836,7 @@ struct md_op_data {
 
 	/* default stripe offset */
 	__u32			op_default_stripe_offset;
+	__u32			op_projid;
 };
 
 struct md_callback {

commit 91ca866862aaa89218370996d9ed9c668611e069

LU-12160 osd-ldiskfs: use-after-free in osd_object_delete()
store a local copy of projid to avoid use-after-free.

Fixes: 39f63cf54c62 ("LU-4017 quota: add setting/getting project id function")

Change-Id: I60e19de3485cae3df1cc2e8aae6eeed4b5de3a11
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34596
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Jenkins
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -2094,8 +2094,9 @@ static void osd_object_delete(const struct lu_env *env, struct lu_object *l)
 	osd_index_fini(obj);
 	if (inode != NULL) {
 		struct qsd_instance *qsd = osd_def_qsd(osd_obj2dev(obj));
-		qid_t		     uid = i_uid_read(inode);
-		qid_t		     gid = i_gid_read(inode);
+		qid_t uid = i_uid_read(inode);
+		qid_t gid = i_gid_read(inode);
+		__u64 projid = i_projid_read(inode);
 
 		obj->oo_inode = NULL;
 		iput(inode);
@@ -2110,7 +2111,7 @@ static void osd_object_delete(const struct lu_env *env, struct lu_object *l)
 			qi->lqi_id.qid_uid = gid;
 			qsd_op_adjust(env, qsd, &qi->lqi_id, GRPQUOTA);
 
-			qi->lqi_id.qid_uid = i_projid_read(inode);
+			qi->lqi_id.qid_uid = projid;
 			qsd_op_adjust(env, qsd, &qi->lqi_id, PRJQUOTA);
 		}
 	}