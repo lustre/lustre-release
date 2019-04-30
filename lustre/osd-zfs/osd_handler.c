lustre/lustre-release

commit 0fedb017c12629d145fa0577451d43adc757eb36

LU-9890 osd-zfs: dmu_objset_own/disown changes
ZFS 0.8.0 will introduce ZFS encryption. The interfaces
to 'dmu_objset_own' and 'dmu_objset_disown' have changed.
Add configure checks to determine which versions of these
functions are available and call them appropriately.

Signed-off-by: Giuseppe Di Natale <dinatale2@llnl.gov>
Test-Parameters: trivial ostfilesystemtype=zfs mdtfilesystemtype=zfs testlist=sanity
Change-Id: Ide1a712858770e373404445b06596130a574d85b
Reviewed-on: https://review.whamcloud.com/28593
Tested-by: Jenkins
Reviewed-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

lustre/osd-zfs/osd_handler.c
@@ -850,9 +850,10 @@ static int osd_objset_open(struct osd_device *o)
 	int		rc;
 	ENTRY;
 
-	rc = -dmu_objset_own(o->od_mntdev, DMU_OST_ZFS,
+	rc = -osd_dmu_objset_own(o->od_mntdev, DMU_OST_ZFS,
 			     o->od_dt_dev.dd_rdonly ? B_TRUE : B_FALSE,
-			     o, &o->od_os);
+			     B_FALSE, o, &o->od_os);
+
 	if (rc) {
 		CERROR("%s: can't open %s\n", o->od_svname, o->od_mntdev);
 		o->od_os = NULL;
@@ -916,7 +917,7 @@ static int osd_objset_open(struct osd_device *o)
 
 out:
 	if (rc != 0 && o->od_os != NULL) {
-		dmu_objset_disown(o->od_os, o);
+		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
 		o->od_os = NULL;
 	}
 
@@ -1107,7 +1108,7 @@ static int osd_mount(const struct lu_env *env,
 	osd_unlinked_drain(env, o);
 err:
 	if (rc && o->od_os) {
-		dmu_objset_disown(o->od_os, o);
+		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
 		o->od_os = NULL;
 	}
 
@@ -1147,8 +1148,7 @@ static void osd_umount(const struct lu_env *env, struct osd_device *o)
 			txg_wait_synced(dmu_objset_pool(o->od_os), 0ULL);
 
 		/* close the object set */
-		dmu_objset_disown(o->od_os, o);
-
+		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
 		o->od_os = NULL;
 	}
 

config/lustre-build-zfs.m4
@@ -628,6 +628,33 @@ your distribution.
 			AC_DEFINE(HAVE_DMU_READ_BY_DNODE, 1,
 				[Have dmu_read_by_dnode() in ZFS])
 		])
+		dnl #
+		dnl # ZFS 0.8.x changes dmu_objset_own for encryption
+		dnl #
+		LB_CHECK_COMPILE([if ZFS has 'dmu_objset_own' with 6 args],
+		dmu_objset_own, [
+			#include <sys/dmu_objset.h>
+		],[
+			objset_t *os = NULL;
+			dmu_objset_type_t type = DMU_OST_ANY;
+			dmu_objset_own(NULL, type, B_FALSE, B_FALSE, FTAG, &os);
+		],[
+			AC_DEFINE(HAVE_DMU_OBJSET_OWN_6ARG, 1,
+				[Have dmu_objset_own() with 6 args])
+		])
+		dnl #
+		dnl # ZFS 0.8.x changes dmu_objset_disown for encryption
+		dnl #
+		LB_CHECK_COMPILE([if ZFS has 'dmu_objset_disown' with 3 args],
+		dmu_objset_disown, [
+			#include <sys/dmu_objset.h>
+		],[
+			objset_t *os = NULL;
+			dmu_objset_disown(os, B_FALSE, FTAG);
+		],[
+			AC_DEFINE(HAVE_DMU_OBJSET_DISOWN_3ARG, 1,
+				[Have dmu_objset_disown() with 3 args])
+		])
 	])
 
 	AM_CONDITIONAL(ZFS_ENABLED, [test "x$enable_zfs" = xyes])

lustre/osd-zfs/osd_internal.h
@@ -880,4 +880,20 @@ static inline int osd_dmu_read(struct osd_device *osd, dnode_t *dn,
 #endif
 }
 
+#ifdef HAVE_DMU_OBJSET_OWN_6ARG
+#define osd_dmu_objset_own(name, type, ronly, decrypt, tag, os)	\
+	dmu_objset_own((name), (type), (ronly), (decrypt), (tag), (os))
+#else
+#define osd_dmu_objset_own(name, type, ronly, decrypt, tag, os)	\
+	dmu_objset_own((name), (type), (ronly), (tag), (os))
+#endif
+
+#ifdef HAVE_DMU_OBJSET_DISOWN_3ARG
+#define osd_dmu_objset_disown(os, decrypt, tag)	\
+	dmu_objset_disown((os), (decrypt), (tag))
+#else
+#define osd_dmu_objset_disown(os, decrypt, tag)	\
+	dmu_objset_disown((os), (tag))
+#endif
+
 #endif /* _OSD_INTERNAL_H */

commit 3d43658fc36e5821d8b094c1c2365e9520dbe9fe

LU-11845 osd-zfs: Support encrypted ZFS datasets
Call zfs::dmu_objset_own and zfs::dmu_objset_disown with
decrypt=B_TRUE

This is called the same way as in zfs modules.

Fixes: 0fedb017c1 ("LU-9890 osd-zfs: dmu_objset_own/disown changes")
Test-Parameters: envdefinitions=ZFS_MKFS_OPTS="encryption=on -o keylocation=file:///etc/adjtime -o keyformat=passphrase" testlist=sanity fstype=zfs
Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
Change-Id: I1d9bc1a579ac26706a9f6cc5a0d52649ce005228
Reviewed-on: https://review.whamcloud.com/33999
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osd-zfs/osd_handler.c
@@ -904,7 +904,7 @@ static int osd_objset_open(struct osd_device *o)
 
 	rc = -osd_dmu_objset_own(o->od_mntdev, DMU_OST_ZFS,
 			     o->od_dt_dev.dd_rdonly ? B_TRUE : B_FALSE,
-			     B_FALSE, o, &o->od_os);
+			     B_TRUE, o, &o->od_os);
 
 	if (rc) {
 		CERROR("%s: can't open %s\n", o->od_svname, o->od_mntdev);
@@ -969,7 +969,7 @@ static int osd_objset_open(struct osd_device *o)
 
 out:
 	if (rc != 0 && o->od_os != NULL) {
-		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
+		osd_dmu_objset_disown(o->od_os, B_TRUE, o);
 		o->od_os = NULL;
 	}
 
@@ -1202,7 +1202,7 @@ static int osd_mount(const struct lu_env *env,
 	osd_unlinked_drain(env, o);
 err:
 	if (rc && o->od_os) {
-		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
+		osd_dmu_objset_disown(o->od_os, B_TRUE, o);
 		o->od_os = NULL;
 	}
 
@@ -1249,7 +1249,7 @@ static void osd_umount(const struct lu_env *env, struct osd_device *o)
 			txg_wait_synced(dmu_objset_pool(o->od_os), 0ULL);
 
 		/* close the object set */
-		osd_dmu_objset_disown(o->od_os, B_FALSE, o);
+		osd_dmu_objset_disown(o->od_os, B_TRUE, o);
 		o->od_os = NULL;
 	}
 