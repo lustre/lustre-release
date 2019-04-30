lustre/lustre-release

commit f90abfdc961debae069804307dcbc883b50c137c

LU-169 lov: add generation number to LOV EA
This patch shrinks lov_mds_md_v*::lmm_stripe_count to 16 bits and use
the remaining 16 bits to store a generation number for the layout.
This generation will be used in conjunction the layout lock to allow
clients to detect when the file layout has changed.
The layout generation starts at 0 and will be bumped each time the
file layout is altered.
For backward compatibility, the layout generation is set to 0 when the
layout is sent to a client that does not support
OBD_CONNECT_LAYOUTLOCK.

This patch also stores OBD_INCOMPAT_LMM_VER in the MDS last_rcvd file
to prevent older versions of Lustre that cannot deal with a 16-bit
lmm_stripe from mounting the filesystem and exporting layouts to older
clients without setting the layout to 0.  This flag will be set in the
last_rcvd file only once we start modifying file layouts, but at least
the current version should not be confused by a 16-bit stripe count
and a non-zero generation.

Signed-off-by: Jacques-Charles Lafoucriere <jacques-charles.lafoucriere@cea.fr>
Change-Id: Ic40083227057eba565287d1a10890875b8a96c13
Signed-off-by: Johann Lombardi <johann@whamcloud.com>
Reviewed-on: http://review.whamcloud.com/1866
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/lustre/lustre_user.h
@@ -200,6 +200,7 @@ struct lov_user_ost_data_v1 {     /* per-stripe data structure */
 } __attribute__((packed));
 
 #define lov_user_md lov_user_md_v1
+#define lmm_stripe_offset u.lum_stripe_offset
 struct lov_user_md_v1 {           /* LOV EA user data (host-endian) */
         __u32 lmm_magic;          /* magic number = LOV_USER_MAGIC_V1 */
         __u32 lmm_pattern;        /* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */
@@ -207,7 +208,12 @@ struct lov_user_md_v1 {           /* LOV EA user data (host-endian) */
         __u64 lmm_object_seq;     /* LOV object seq */
         __u32 lmm_stripe_size;    /* size of stripe in bytes */
         __u16 lmm_stripe_count;   /* num stripes in use for this object */
-        __u16 lmm_stripe_offset;  /* starting stripe offset in lmm_objects */
+        union {
+                __u16 lum_stripe_offset;  /* starting stripe offset in
+                                           * lmm_objects, use when writing */
+                __u16 lum_layout_gen;     /* layout generation number
+                                           * used when reading */
+        } u;
         struct lov_user_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 } __attribute__((packed,  __may_alias__));
 
@@ -218,7 +224,12 @@ struct lov_user_md_v3 {           /* LOV EA user data (host-endian) */
         __u64 lmm_object_seq;     /* LOV object seq */
         __u32 lmm_stripe_size;    /* size of stripe in bytes */
         __u16 lmm_stripe_count;   /* num stripes in use for this object */
-        __u16 lmm_stripe_offset;  /* starting stripe offset in lmm_objects */
+        union {
+                __u16 lum_stripe_offset;  /* starting stripe offset in
+                                           * lmm_objects, use when writing */
+                __u16 lum_layout_gen;     /* layout generation number
+                                           * used when reading */
+        } u;
         char  lmm_pool_name[LOV_MAXPOOLNAME]; /* pool name */
         struct lov_user_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 } __attribute__((packed));

lustre/cmm/cmm_object.c
@@ -594,6 +594,17 @@ static int cml_unlink(const struct lu_env *env, struct md_object *mo_p,
         RETURN(rc);
 }
 
+/** Call mdo_lum_lmm_cmp() on next layer */
+static int cml_lum_lmm_cmp(const struct lu_env *env, struct md_object *mo_c,
+                           const struct md_op_spec *spec, struct md_attr *ma)
+{
+        int rc;
+        ENTRY;
+
+        rc = mdo_lum_lmm_cmp(env, md_object_next(mo_c), spec, ma);
+        RETURN(rc);
+}
+
 /**
  * \ingroup cmm
  * Get mode of object.
@@ -880,6 +891,7 @@ static const struct md_dir_operations cml_dir_ops = {
         .mdo_create      = cml_create,
         .mdo_link        = cml_link,
         .mdo_unlink      = cml_unlink,
+        .mdo_lum_lmm_cmp = cml_lum_lmm_cmp,
         .mdo_name_insert = cml_name_insert,
         .mdo_rename      = cml_rename,
         .mdo_rename_tgt  = cml_rename_tgt,
@@ -1122,6 +1134,12 @@ static int cmr_file_lock(const struct lu_env *env, struct md_object *mo,
 
 static int cmr_file_unlock(const struct lu_env *env, struct md_object *mo,
                            struct lov_mds_md *lmm, struct lustre_handle *lockh)
+{
+        return -EREMOTE;
+}
+
+static int cmr_lum_lmm_cmp(const struct lu_env *env, struct md_object *mo_c,
+                           const struct md_op_spec *spec, struct md_attr *ma)
 {
         return -EREMOTE;
 }
@@ -1527,6 +1545,7 @@ static const struct md_dir_operations cmr_dir_ops = {
         .mdo_create      = cmr_create,
         .mdo_link        = cmr_link,
         .mdo_unlink      = cmr_unlink,
+        .mdo_lum_lmm_cmp = cmr_lum_lmm_cmp,
         .mdo_rename      = cmr_rename,
         .mdo_rename_tgt  = cmr_rename_tgt
 };

lustre/include/lustre/liblustreapi.h
@@ -100,14 +100,15 @@ extern int llapi_file_get_stripe(const char *path, struct lov_user_md *lum);
 #define HAVE_LLAPI_FILE_LOOKUP
 extern int llapi_file_lookup(int dirfd, const char *name);
 
-#define VERBOSE_COUNT   0x1
-#define VERBOSE_SIZE    0x2
-#define VERBOSE_OFFSET  0x4
-#define VERBOSE_POOL    0x8
-#define VERBOSE_DETAIL  0x10
-#define VERBOSE_OBJID   0x20
-#define VERBOSE_ALL     (VERBOSE_COUNT | VERBOSE_SIZE | VERBOSE_OFFSET | \
-                         VERBOSE_POOL | VERBOSE_OBJID)
+#define VERBOSE_COUNT      0x1
+#define VERBOSE_SIZE       0x2
+#define VERBOSE_OFFSET     0x4
+#define VERBOSE_POOL       0x8
+#define VERBOSE_DETAIL     0x10
+#define VERBOSE_OBJID      0x20
+#define VERBOSE_GENERATION 0x40
+#define VERBOSE_ALL        (VERBOSE_COUNT | VERBOSE_SIZE | VERBOSE_OFFSET | \
+                            VERBOSE_POOL | VERBOSE_OBJID | VERBOSE_GENERATION)
 
 struct find_param {
         unsigned int maxdepth;

lustre/include/lustre/lustre_idl.h
@@ -1317,7 +1317,9 @@ struct lov_mds_md_v1 {            /* LOV EA mds/wire data (little-endian) */
         __u64 lmm_object_id;      /* LOV object ID */
         __u64 lmm_object_seq;     /* LOV object seq number */
         __u32 lmm_stripe_size;    /* size of stripe in bytes */
-        __u32 lmm_stripe_count;   /* num stripes in use for this object */
+        /* lmm_stripe_count used to be __u32 */
+        __u16 lmm_stripe_count;   /* num stripes in use for this object */
+        __u16 lmm_layout_gen;     /* layout generation number */
         struct lov_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 };
 
@@ -1346,7 +1348,9 @@ struct lov_mds_md_v3 {            /* LOV EA mds/wire data (little-endian) */
         __u64 lmm_object_id;      /* LOV object ID */
         __u64 lmm_object_seq;     /* LOV object seq number */
         __u32 lmm_stripe_size;    /* size of stripe in bytes */
-        __u32 lmm_stripe_count;   /* num stripes in use for this object */
+        /* lmm_stripe_count used to be __u32 */
+        __u16 lmm_stripe_count;   /* num stripes in use for this object */
+        __u16 lmm_layout_gen;     /* layout generation number */
         char  lmm_pool_name[LOV_MAXPOOLNAME]; /* must be 32bit aligned */
         struct lov_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 };
@@ -2143,6 +2147,7 @@ enum seq_op {
  *
  * (max buffer size - lov+rpc header) / sizeof(struct lov_ost_data_v1) */
 #define LOV_MAX_STRIPE_COUNT 2000  /* ((12 * 4096 - 256) / 24) */
+#define LOV_ALL_STRIPES       0xffff /* only valid for directories */
 #define LOV_V1_INSANE_STRIPE_COUNT 65532 /* maximum stripe count bz13933 */
 
 #define LOV_MAX_UUID_BUFFER_SIZE  8192

lustre/include/lustre_disk.h
@@ -274,7 +274,12 @@ struct lustre_mount_data {
 #define OBD_INCOMPAT_IAM_DIR    0x00000040
 /** LMA attribute contains per-inode incompatible flags */
 #define OBD_INCOMPAT_LMA        0x00000080
-
+/** lmm_stripe_count has been shrunk from __u32 to __u16 and the remaining 16
+ * bits are now used to store a generation. Once we start changing the layout
+ * and bumping the generation, old versions expecting a 32-bit lmm_stripe_count
+ * will be confused by interpreting stripe_count | gen << 16 as the actual
+ * stripe count */
+#define OBD_INCOMPAT_LMM_VER    0x00000100
 
 /* Data stored per server at the head of the last_rcvd file.  In le32 order.
    This should be common to filter_internal.h, lustre_mds.h */

lustre/include/md_object.h
@@ -127,7 +127,7 @@ enum ma_valid {
         MA_LMV       = (1 << 4),
         MA_ACL_DEF   = (1 << 5),
         MA_LOV_DEF   = (1 << 6),
-/* (Layout lock will used #7 here) */
+        MA_LAY_GEN   = (1 << 7),
         MA_HSM       = (1 << 8),
         MA_SOM       = (1 << 9),
         MA_PFID      = (1 << 10)
@@ -170,18 +170,19 @@ struct md_attr {
         __u64                   ma_need;
         __u64                   ma_attr_flags;
         struct lu_attr          ma_attr;
-        struct lov_mds_md      *ma_lmm;
-        int                     ma_lmm_size;
-        struct lmv_stripe_md   *ma_lmv;
-        int                     ma_lmv_size;
-        void                   *ma_acl;
-        int                     ma_acl_size;
-        struct llog_cookie     *ma_cookie;
-        int                     ma_cookie_size;
-        struct lustre_capa     *ma_capa;
-        struct md_hsm           ma_hsm;
-        struct md_som_data     *ma_som;
         struct lu_fid           ma_pfid;
+        struct md_hsm           ma_hsm;
+        struct lov_mds_md      *ma_lmm;
+        struct lmv_stripe_md   *ma_lmv;
+        void                   *ma_acl;
+        struct llog_cookie     *ma_cookie;
+        struct lustre_capa     *ma_capa;
+        struct md_som_data     *ma_som;
+        int                     ma_lmm_size;
+        int                     ma_lmv_size;
+        int                     ma_acl_size;
+        int                     ma_cookie_size;
+        __u16                   ma_layout_gen;
 };
 
 /** Additional parameters for create */
@@ -326,6 +327,13 @@ struct md_dir_operations {
         int (*mdo_unlink)(const struct lu_env *env, struct md_object *pobj,
                           struct md_object *cobj, const struct lu_name *lname,
                           struct md_attr *ma);
+
+        /** This method is used to compare a requested layout to an existing
+         * layout (struct lov_mds_md_v1/3 vs struct lov_mds_md_v1/3) */
+        int (*mdo_lum_lmm_cmp)(const struct lu_env *env,
+                               struct md_object *cobj,
+                               const struct md_op_spec *spec,
+                               struct md_attr *ma);
 
         /** partial ops for cross-ref case */
         int (*mdo_name_insert)(const struct lu_env *env,
@@ -850,6 +858,15 @@ static inline int mdo_unlink(const struct lu_env *env,
 {
         LASSERT(c->mo_dir_ops->mdo_unlink);
         return c->mo_dir_ops->mdo_unlink(env, p, c, lname, ma);
+}
+
+static inline int mdo_lum_lmm_cmp(const struct lu_env *env,
+                                  struct md_object *c,
+                                  const struct md_op_spec *spec,
+                                  struct md_attr *ma)
+{
+        LASSERT(c->mo_dir_ops->mdo_lum_lmm_cmp);
+        return c->mo_dir_ops->mdo_lum_lmm_cmp(env, c, spec, ma);
 }
 
 static inline int mdo_name_insert(const struct lu_env *env,

lustre/include/obd.h
@@ -144,7 +144,8 @@ struct lov_stripe_md {
                 __u32 lw_magic;
                 __u32 lw_stripe_size;      /* size of the stripe */
                 __u32 lw_pattern;          /* striping pattern (RAID0, RAID1) */
-                unsigned lw_stripe_count;  /* number of objects being striped over */
+                __u16 lw_stripe_count;  /* number of objects being striped over */
+                __u16 lw_layout_gen;       /* generation of the layout */
                 char  lw_pool_name[LOV_MAXPOOLNAME]; /* pool name */
         } lsm_wire;
 
@@ -154,6 +155,7 @@ struct lov_stripe_md {
 #define lsm_object_id    lsm_wire.lw_object_id
 #define lsm_object_seq   lsm_wire.lw_object_seq
 #define lsm_magic        lsm_wire.lw_magic
+#define lsm_layout_gen   lsm_wire.lw_layout_gen
 #define lsm_stripe_size  lsm_wire.lw_stripe_size
 #define lsm_pattern      lsm_wire.lw_pattern
 #define lsm_stripe_count lsm_wire.lw_stripe_count
@@ -203,6 +205,57 @@ static inline int lov_stripe_md_cmp(struct lov_stripe_md *m1,
          * allocation.
          */
         return memcmp(&m1->lsm_wire, &m2->lsm_wire, sizeof m1->lsm_wire);
+}
+
+static inline int lov_lum_lsm_cmp(struct lov_user_md *lum,
+                                  struct lov_stripe_md  *lsm)
+{
+        if (lsm->lsm_magic != lum->lmm_magic)
+                return 1;
+        if ((lsm->lsm_stripe_count != 0) && (lum->lmm_stripe_count != 0) &&
+            (lsm->lsm_stripe_count != lum->lmm_stripe_count))
+                return 2;
+        if ((lsm->lsm_stripe_size != 0) && (lum->lmm_stripe_size != 0) &&
+            (lsm->lsm_stripe_size != lum->lmm_stripe_size))
+                return 3;
+        if ((lsm->lsm_pattern != 0) && (lum->lmm_pattern != 0) &&
+            (lsm->lsm_pattern != lum->lmm_pattern))
+                return 4;
+        if ((lsm->lsm_magic == LOV_MAGIC_V3) &&
+            (strncmp(lsm->lsm_pool_name,
+                     ((struct lov_user_md_v3 *)lum)->lmm_pool_name,
+                     LOV_MAXPOOLNAME) != 0))
+                return 5;
+        return 0;
+}
+
+static inline int lov_lum_swab_if_needed(struct lov_user_md_v3 *lumv3,
+                                         int *lmm_magic,
+                                         struct lov_user_md *lum)
+{
+        if (lum && cfs_copy_from_user(lumv3, lum,sizeof(struct lov_user_md_v1)))
+                return -EFAULT;
+
+        *lmm_magic = lumv3->lmm_magic;
+
+        if (*lmm_magic == __swab32(LOV_USER_MAGIC_V1)) {
+                lustre_swab_lov_user_md_v1((struct lov_user_md_v1 *)lumv3);
+                *lmm_magic = LOV_USER_MAGIC_V1;
+        } else if (*lmm_magic == LOV_USER_MAGIC_V3) {
+                if (lum && cfs_copy_from_user(lumv3, lum, sizeof(*lumv3)))
+                        return -EFAULT;
+        } else if (*lmm_magic == __swab32(LOV_USER_MAGIC_V3)) {
+                if (lum && cfs_copy_from_user(lumv3, lum, sizeof(*lumv3)))
+                        return -EFAULT;
+                lustre_swab_lov_user_md_v3(lumv3);
+                *lmm_magic = LOV_USER_MAGIC_V3;
+        } else if (*lmm_magic != LOV_USER_MAGIC_V1) {
+                CDEBUG(D_IOCTL,
+                       "bad userland LOV MAGIC: %#08x != %#08x nor %#08x\n",
+                       *lmm_magic, LOV_USER_MAGIC_V1, LOV_USER_MAGIC_V3);
+                       return -EINVAL;
+        }
+        return 0;
 }
 
 void lov_stripe_lock(struct lov_stripe_md *md);
@@ -1587,7 +1640,7 @@ struct lsm_operations {
         void (*lsm_stripe_by_offset)(struct lov_stripe_md *, int *, obd_off *,
                                      obd_off *);
         int (*lsm_lmm_verify) (struct lov_mds_md *lmm, int lmm_bytes,
-                               int *stripe_count);
+                               __u16 *stripe_count);
         int (*lsm_unpackmd) (struct lov_obd *lov, struct lov_stripe_md *lsm,
                              struct lov_mds_md *lmm);
 };

lustre/include/obd_lov.h
@@ -37,12 +37,12 @@
 #ifndef _OBD_LOV_H__
 #define _OBD_LOV_H__
 
-static inline int lov_stripe_md_size(int stripes)
+static inline int lov_stripe_md_size(__u16 stripes)
 {
         return sizeof(struct lov_stripe_md) + stripes*sizeof(struct lov_oinfo*);
 }
 
-static inline __u32 lov_mds_md_size(int stripes, __u32 lmm_magic)
+static inline __u32 lov_mds_md_size(__u16 stripes, __u32 lmm_magic)
 {
         if (lmm_magic == LOV_MAGIC_V3)
                 return sizeof(struct lov_mds_md_v3) +

commit c872afa36ff5de5910f0f524f7a487982fa0c776

LU-12040 mdc: reset lmm->lmm_stripe_offset in mdc_save_lovea
In order to prepare for replay lmm->lmm_stripe_offset (which contains
layout generation) has to be set to -1 (LOV_OFFSET_DEFAULT) in order
to not confuse lod_verify_v1v3

Fixes: f90abfdc96 ("LU-169 lov: add generation number to LOV EA")
Fixes: 89693927f0 ("LU-8998 lod: accomodate to composite layout")
Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Cray-bug-id: LUS-7008
Change-Id: I911d3c659b6c11cc8847f0517062dd8e4df89dff
Reviewed-on: https://review.whamcloud.com/34371
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -556,6 +556,8 @@ struct fsxattr {
 #define LOV_PATTERN_F_RELEASED	0x80000000 /* HSM released file */
 #define LOV_PATTERN_DEFAULT	0xffffffff
 
+#define LOV_OFFSET_DEFAULT      ((__u16)-1)
+
 static inline bool lov_pattern_supported(__u32 pattern)
 {
 	return (pattern & ~LOV_PATTERN_F_RELEASED) == LOV_PATTERN_RAID0 ||

lustre/lod/lod_internal.h
@@ -54,8 +54,6 @@
 #define LMVEA_DELETE_VALUES(count, offset)				\
 	((count) == 0 && (offset) == (typeof(offset))(-1))
 
-#define LOV_OFFSET_DEFAULT		((__u16)-1)
-
 struct lod_qos_rr {
 	spinlock_t		 lqr_alloc;	/* protect allocation index */
 	__u32			 lqr_start_idx;	/* start index of new inode */

lustre/mdc/mdc_lib.c
@@ -416,7 +416,8 @@ void mdc_setattr_pack(struct ptlrpc_request *req, struct md_op_data *op_data,
 		lum->lmm_magic = cpu_to_le32(LOV_USER_MAGIC_V1);
 		lum->lmm_stripe_size = 0;
 		lum->lmm_stripe_count = 0;
-		lum->lmm_stripe_offset = (typeof(lum->lmm_stripe_offset))(-1);
+		lum->lmm_stripe_offset =
+		  (typeof(lum->lmm_stripe_offset))LOV_OFFSET_DEFAULT;
 	} else {
 		memcpy(lum, ea, ealen);
 	}

lustre/mdc/mdc_locks.c
@@ -220,7 +220,7 @@ int mdc_save_lovea(struct ptlrpc_request *req,
 		   void *data, u32 size)
 {
 	struct req_capsule *pill = &req->rq_pill;
-	void *lmm;
+	struct lov_user_md *lmm;
 	int rc = 0;
 
 	if (req_capsule_get_size(pill, field, RCL_CLIENT) < size) {
@@ -237,8 +237,12 @@ int mdc_save_lovea(struct ptlrpc_request *req,
 
 	req_capsule_set_size(pill, field, RCL_CLIENT, size);
 	lmm = req_capsule_client_get(pill, field);
-	if (lmm)
+	if (lmm) {
 		memcpy(lmm, data, size);
+		/* overwrite layout generation returned from the MDS */
+		lmm->lmm_stripe_offset =
+		  (typeof(lmm->lmm_stripe_offset))LOV_OFFSET_DEFAULT;
+	}
 
 	return rc;
 }

lustre/tests/replay-single.sh
@@ -4915,6 +4915,25 @@ test_133() {
 }
 run_test 133 "check resend of ongoing requests for lwp during failover"
 
+test_134() {
+	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs" && return 0
+
+	pool_add pool_134
+	pool_add_targets pool_134 1 1
+
+	mkdir $DIR/$tdir
+	$LFS setstripe -p pool_134 $DIR/$tdir
+
+	replay_barrier mds1
+
+	touch $DIR/$tdir/$tfile
+
+	fail mds1
+
+	[ -f $DIR/$tdir/$tfile ] || error "file does not exist"
+}
+run_test 134 "replay creation of a file created in a pool"
+
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status