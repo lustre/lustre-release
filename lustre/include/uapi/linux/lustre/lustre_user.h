lustre/lustre-release

commit ae723cf8161f7af15fb7c2541fa7d4131a816af9

LU-10602 llite: add file heat support
File heat is a special attribute fo files/objects which reflects
the access frequency of the files/objects.
File heat is mainly desinged for cache management. Caches like
PCC can use file heat to determine which files to be removed from
the cache or which files to fetch into cache.
This patch adds file heat support on llite level.

Signed-off-by: Li Xi <lixi@ddn.com>
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I168fc657f0c859311e5114191b60047646909be0
Reviewed-on: https://review.whamcloud.com/34399
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -477,6 +477,8 @@ struct ll_ioc_lease_id {
 #define LL_IOC_FID2MDTIDX		_IOWR('f', 248, struct lu_fid)
 #define LL_IOC_GETPARENT		_IOWR('f', 249, struct getparent)
 #define LL_IOC_LADVISE			_IOR('f', 250, struct llapi_lu_ladvise)
+#define LL_IOC_HEAT_GET			_IOWR('f', 251, struct lu_heat)
+#define LL_IOC_HEAT_SET			_IOW('f', 252, long)
 
 #ifndef	FS_IOC_FSGETXATTR
 /*
@@ -2184,6 +2186,36 @@ enum lockahead_results {
 	LLA_RESULT_SENT = 0,
 	LLA_RESULT_DIFFERENT,
 	LLA_RESULT_SAME,
+};
+
+enum lu_heat_flag_bit {
+	LU_HEAT_FLAG_BIT_INVALID = 0,
+	LU_HEAT_FLAG_BIT_OFF,
+	LU_HEAT_FLAG_BIT_CLEAR,
+};
+
+#define LU_HEAT_FLAG_CLEAR	(1 << LU_HEAT_FLAG_BIT_CLEAR)
+#define LU_HEAT_FLAG_OFF	(1 << LU_HEAT_FLAG_BIT_OFF)
+
+enum obd_heat_type {
+	OBD_HEAT_READSAMPLE	= 0,
+	OBD_HEAT_WRITESAMPLE	= 1,
+	OBD_HEAT_READBYTE	= 2,
+	OBD_HEAT_WRITEBYTE	= 3,
+	OBD_HEAT_COUNT
+};
+
+#define LU_HEAT_NAMES {					\
+	[OBD_HEAT_READSAMPLE]	= "readsample",		\
+	[OBD_HEAT_WRITESAMPLE]	= "writesample",	\
+	[OBD_HEAT_READBYTE]	= "readbyte",		\
+	[OBD_HEAT_WRITEBYTE]	= "writebyte",		\
+}
+
+struct lu_heat {
+	__u32 lh_count;
+	__u32 lh_flags;
+	__u64 lh_heat[0];
 };
 
 #if defined(__cplusplus)

lustre/llite/file.c
@@ -1369,6 +1369,37 @@ static void ll_io_init(struct cl_io *io, struct file *file, enum cl_io_type iot)
 	ll_io_set_mirror(io, file);
 }
 
+static void ll_heat_add(struct inode *inode, enum cl_io_type iot,
+			__u64 count)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	enum obd_heat_type sample_type;
+	enum obd_heat_type iobyte_type;
+	__u64 now = ktime_get_real_seconds();
+
+	if (!ll_sbi_has_file_heat(sbi) ||
+	    lli->lli_heat_flags & LU_HEAT_FLAG_OFF)
+		return;
+
+	if (iot == CIT_READ) {
+		sample_type = OBD_HEAT_READSAMPLE;
+		iobyte_type = OBD_HEAT_READBYTE;
+	} else if (iot == CIT_WRITE) {
+		sample_type = OBD_HEAT_WRITESAMPLE;
+		iobyte_type = OBD_HEAT_WRITEBYTE;
+	} else {
+		return;
+	}
+
+	spin_lock(&lli->lli_heat_lock);
+	obd_heat_add(&lli->lli_heat_instances[sample_type], now, 1,
+		     sbi->ll_heat_decay_weight, sbi->ll_heat_period_second);
+	obd_heat_add(&lli->lli_heat_instances[iobyte_type], now, count,
+		     sbi->ll_heat_decay_weight, sbi->ll_heat_period_second);
+	spin_unlock(&lli->lli_heat_lock);
+}
+
 static ssize_t
 ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 		   struct file *file, enum cl_io_type iot,
@@ -1499,6 +1530,8 @@ out:
 	}
 
 	CDEBUG(D_VFSTRACE, "iot: %d, result: %zd\n", iot, result);
+	if (result > 0)
+		ll_heat_add(inode, iot, result);
 
 	RETURN(result > 0 ? result : rc);
 }
@@ -1559,9 +1592,11 @@ ll_do_fast_read(struct kiocb *iocb, struct iov_iter *iter)
 	if (result == -ENODATA)
 		result = 0;
 
-	if (result > 0)
+	if (result > 0) {
+		ll_heat_add(file_inode(iocb->ki_filp), CIT_READ, result);
 		ll_stats_ops_tally(ll_i2sbi(file_inode(iocb->ki_filp)),
 				LPROC_LL_READ_BYTES, result);
+	}
 
 	return result;
 }
@@ -1649,6 +1684,7 @@ static ssize_t ll_do_tiny_write(struct kiocb *iocb, struct iov_iter *iter)
 		result = 0;
 
 	if (result > 0) {
+		ll_heat_add(inode, CIT_WRITE, result);
 		ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_WRITE_BYTES,
 				   result);
 		ll_file_set_flag(ll_i2info(inode), LLIF_DATA_MODIFIED);
@@ -3238,6 +3274,41 @@ static long ll_file_set_lease(struct file *file, struct ll_ioc_lease *ioc,
 	RETURN(rc);
 }
 
+static void ll_heat_get(struct inode *inode, struct lu_heat *heat)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	__u64 now = ktime_get_real_seconds();
+	int i;
+
+	spin_lock(&lli->lli_heat_lock);
+	heat->lh_flags = lli->lli_heat_flags;
+	for (i = 0; i < heat->lh_count; i++)
+		heat->lh_heat[i] = obd_heat_get(&lli->lli_heat_instances[i],
+						now, sbi->ll_heat_decay_weight,
+						sbi->ll_heat_period_second);
+	spin_unlock(&lli->lli_heat_lock);
+}
+
+static int ll_heat_set(struct inode *inode, __u64 flags)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+	int rc = 0;
+
+	spin_lock(&lli->lli_heat_lock);
+	if (flags & LU_HEAT_FLAG_CLEAR)
+		obd_heat_clear(lli->lli_heat_instances, OBD_HEAT_COUNT);
+
+	if (flags & LU_HEAT_FLAG_OFF)
+		lli->lli_heat_flags |= LU_HEAT_FLAG_OFF;
+	else
+		lli->lli_heat_flags &= ~LU_HEAT_FLAG_OFF;
+
+	spin_unlock(&lli->lli_heat_lock);
+
+	RETURN(rc);
+}
+
 static long
 ll_file_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -3621,6 +3692,37 @@ out_ladvise:
 		RETURN(ll_ioctl_fssetxattr(inode, cmd, arg));
 	case BLKSSZGET:
 		RETURN(put_user(PAGE_SIZE, (int __user *)arg));
+	case LL_IOC_HEAT_GET: {
+		struct lu_heat uheat;
+		struct lu_heat *heat;
+		int size;
+
+		if (copy_from_user(&uheat, (void __user *)arg, sizeof(uheat)))
+			RETURN(-EFAULT);
+
+		if (uheat.lh_count > OBD_HEAT_COUNT)
+			uheat.lh_count = OBD_HEAT_COUNT;
+
+		size = offsetof(typeof(uheat), lh_heat[uheat.lh_count]);
+		OBD_ALLOC(heat, size);
+		if (heat == NULL)
+			RETURN(-ENOMEM);
+
+		heat->lh_count = uheat.lh_count;
+		ll_heat_get(inode, heat);
+		rc = copy_to_user((char __user *)arg, heat, size);
+		OBD_FREE(heat, size);
+		RETURN(rc ? -EFAULT : 0);
+	}
+	case LL_IOC_HEAT_SET: {
+		__u64 flags;
+
+		if (copy_from_user(&flags, (void __user *)arg, sizeof(flags)))
+			RETURN(-EFAULT);
+
+		rc = ll_heat_set(inode, flags);
+		RETURN(rc);
+	}
 	default:
 		RETURN(obd_iocontrol(cmd, ll_i2dtexp(inode), 0, NULL,
 				     (void __user *)arg));

lustre/utils/lfs.c
@@ -119,6 +119,8 @@ static int lfs_swap_layouts(int argc, char **argv);
 static int lfs_mv(int argc, char **argv);
 static int lfs_ladvise(int argc, char **argv);
 static int lfs_getsom(int argc, char **argv);
+static int lfs_heat_get(int argc, char **argv);
+static int lfs_heat_set(int argc, char **argv);
 static int lfs_mirror(int argc, char **argv);
 static int lfs_mirror_list_commands(int argc, char **argv);
 static int lfs_list_commands(int argc, char **argv);
@@ -605,6 +607,15 @@ command_t cmdlist[] = {
 	 "\t-s: Only show the size value of the SOM data for a given file\n"
 	 "\t-b: Only show the blocks value of the SOM data for a given file\n"
 	 "\t-f: Only show the flags value of the SOM data for a given file\n"},
+	{"heat_get", lfs_heat_get, 0,
+	 "To get heat of files.\n"
+	 "usage: heat_get <file> ...\n"},
+	{"heat_set", lfs_heat_set, 0,
+	 "To set heat flags of files.\n"
+	 "usage: heat_set [--clear|-c] [--off|-o] [--on|-O] <file> ...\n"
+	 "\t--clear|-c:	Clear file heat for given files\n"
+	 "\t--off|-o:	Turn off file heat for given files\n"
+	 "\t--on|-O:	Turn on file heat for given files\n"},
 	{"help", Parser_help, 0, "help"},
 	{"exit", Parser_quit, 0, "quit"},
 	{"quit", Parser_quit, 0, "quit"},
@@ -7890,6 +7901,131 @@ static int lfs_ladvise(int argc, char **argv)
 			goto next;
 		}
 
+next:
+		if (rc == 0 && rc2 < 0)
+			rc = rc2;
+	}
+	return rc;
+}
+
+
+static const char *const heat_names[] = LU_HEAT_NAMES;
+
+static int lfs_heat_get(int argc, char **argv)
+{
+	struct lu_heat	*heat;
+	int		 rc = 0, rc2;
+	char		*path;
+	int		 fd;
+	int		 i;
+
+	if (argc <= 1)
+		return CMD_HELP;
+
+	heat = calloc(sizeof(*heat) + sizeof(__u64) * OBD_HEAT_COUNT, 1);
+	if (!heat) {
+		fprintf(stderr, "%s: memory allocation failed\n", argv[0]);
+		return -ENOMEM;
+	}
+
+	optind = 1;
+	while (optind < argc) {
+		path = argv[optind++];
+
+		fd = open(path, O_RDONLY);
+		if (fd < 0) {
+			fprintf(stderr, "%s: cannot open file '%s': %s\n",
+				argv[0], path, strerror(errno));
+			rc2 = -errno;
+			goto next;
+		}
+
+		heat->lh_count = OBD_HEAT_COUNT;
+		rc2 = llapi_heat_get(fd, heat);
+		close(fd);
+		if (rc2 < 0) {
+			fprintf(stderr, "%s: cannot get heat of file '%s'"
+				": %s\n", argv[0], path, strerror(errno));
+			goto next;
+		}
+
+		printf("flags: %x\n", heat->lh_flags);
+		for (i = 0; i < heat->lh_count; i++)
+			printf("%s: %llu\n", heat_names[i], heat->lh_heat[i]);
+next:
+		if (rc == 0 && rc2 < 0)
+			rc = rc2;
+	}
+
+	free(heat);
+	return rc;
+}
+
+static int lfs_heat_set(int argc, char **argv)
+{
+	struct option	 long_opts[] = {
+		{"clear", no_argument, 0, 'c'},
+		{"off", no_argument, 0, 'o'},
+		{"on", no_argument, 0, 'O'},
+		{0, 0, 0, 0}
+	};
+	char		 short_opts[] = "coO";
+	int		 rc = 0, rc2;
+	char		*path;
+	int		 fd;
+	__u64		 flags = 0;
+	int		 c;
+
+	if (argc <= 1)
+		return CMD_HELP;
+
+	optind = 0;
+	while ((c = getopt_long(argc, argv, short_opts,
+				long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'c':
+			flags |= LU_HEAT_FLAG_CLEAR;
+			break;
+		case 'o':
+			flags |= LU_HEAT_FLAG_CLEAR;
+			flags |= LU_HEAT_FLAG_OFF;
+			break;
+		case 'O':
+			flags &= ~LU_HEAT_FLAG_OFF;
+			break;
+		case '?':
+			return CMD_HELP;
+		default:
+			fprintf(stderr, "%s: option '%s' unrecognized\n",
+				argv[0], argv[optind - 1]);
+			return CMD_HELP;
+		}
+	}
+
+	if (argc <= optind) {
+		fprintf(stderr, "%s: please give one or more file names\n",
+			argv[0]);
+		return CMD_HELP;
+	}
+
+	while (optind < argc) {
+		path = argv[optind++];
+
+		fd = open(path, O_RDONLY);
+		if (fd < 0) {
+			fprintf(stderr, "%s: cannot open file '%s': %s\n",
+				argv[0], path, strerror(errno));
+			rc2 = -errno;
+			goto next;
+		}
+
+		rc2 = llapi_heat_set(fd, flags);
+		close(fd);
+		if (rc2 < 0) {
+			fprintf(stderr, "%s: cannot setflags heat of file '%s'"
+				": %s\n", argv[0], path, strerror(errno));
+			goto next;
+		}
 next:
 		if (rc == 0 && rc2 < 0)
 			rc = rc2;

lustre/doc/Makefile.am
@@ -42,6 +42,7 @@ MANFILES =					\
 	lfs-find.1				\
 	lfs-getstripe.1				\
 	lfs-getdirstripe.1			\
+	lfs-heat.1				\
 	lfs-hsm.1				\
 	lfs-ladvise.1				\
 	lfs_migrate.1				\
@@ -66,6 +67,8 @@ MANFILES =					\
 	llapi_file_open.3			\
 	llapi_group_lock.3			\
 	llapi_group_unlock.3			\
+	llapi_heat_get.3			\
+	llapi_heat_set.3			\
 	llapi_hsm_action_begin.3		\
 	llapi_hsm_action_end.3			\
 	llapi_hsm_action_get_dfid.3		\

lustre/doc/lfs-heat.1
@@ -1,0 +1,69 @@
+.TH lfs-heat 1 "Feb. 09, 2019" Lustre "Lustre utility"
+.SH NAME
+.B lfs heat_command
+lfs commands used to interact with file heat features
+.SH SYNOPSIS
+.B lfs heat_get|heat_set
+.IR \fR<\fIFILE \fR...>
+.br
+.SH DESCRIPTION
+These are a set of lfs commands used to interact with Lustre file heat feature.
+Currently file heat is only stored in memory with file inode, it might be reset
+to be zero at any time with the release of the inode due to memory reclaim.
+.TP
+.B lfs heat_get  \fR<\fIFILE \fR...>
+Get file heat on file list.
+.TP
+.B lfs heat_set [\fB--clear\fR|\fB-c\fR] [\fB--off\fR|\fB-o\fR] [\fB--on\fR|\fB-O\fR] \fR<\fIFILE \fR...>
+Set provided file heat flags on file list.
+.SH OPTIONS
+.TP
+.BR --clear | -c
+Clear file heat on given files.
+.TP
+.BR --off | -o
+Turn off file heat on given files.
+.TP
+.BR --on | -O
+Turn on file heat on given files.
+.SH EXAMPLES
+.TP
+Turn on file heat support for the Lustre filesystem:
+.B $ lctl set_param llite.$FSNAME*.file_heat=1
+.TP
+Trun off file heat support for the Lustre filesystem:
+.B $ lctl set_param llite.$FSNAME*.file_heat=0
+.TP
+Display current file heat for foo:
+.B $ lfs heat_get /mnt/lustre/foo
+.br
+flags: 0
+.br
+readsample: 0
+.br
+writesample: 16
+.br
+readbyte: 0
+.br
+writebyte: 16777216
+.br
+
+.TP
+Clear the file heat for foo:
+.B $ lfs heat_set -c /mnt/lustre/foo
+.TP
+Turn off file heat for foo:
+.B $ lfs heat_set -o /mnt/lustre/foo
+.TP
+Turn on file heat for foo:
+.B $ lfs heat_set -O /mnt/lustre/foo
+.SH AUTHOR
+The
+.B lfs heat
+command is part of the
+.BR Lustre (7)
+filesystem.
+
+.SH SEE ALSO
+.BR lfs (1)
+.BR lustre (7)

lustre/doc/llapi_heat_get.3
@@ -1,0 +1,65 @@
+.TH llapi_heat_get 3 "2019 Feb 09" "Lustre User API"
+.SH NAME
+llapi_heat_get, llapi_heat_set \- get and clear heat for a file
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_heat_get(int " fd ", struct lu_heat *" heat ");"
+
+.BI "int llapi_heat_set(int " fd ", __u64 " flags ");"
+.fi
+.SH DESCRIPTION
+.PP
+The function
+.B llapi_heat_get()
+returns file access frequency information on the file descriptor
+.BR fd .
+Information is returned in the
+.I heat
+argument which should already be allocated,  which is a
+.B lu_heat
+data structure, which contains the following fields:
+.nf
+.LP
+struct lu_heat {
+	__u32 lh_heat_count;
+	__u32 lh_padding1;
+	__u64 lh_heat[0];
+};
+.fi
+The function
+.B llapi_heat_set()
+mainly clears the heat information on the file descriptor
+.I fd
+according to the parameter
+.I flags
+which gives options for file heat, currently one of:
+.TP
+LU_HEAT_FLAG_CLEAR
+Clear the heat information for a given file.
+.TP
+LU_HEAT_FLAG_OFF
+Turn off the file heat support for a given file.
+
+.SH RETURN VALUES
+.LP
+.B llapi_heat_get()
+and
+.B llapi_heat_set()
+return 0 on success or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM -ENOMEM
+Insufficient memory to complete operation.
+.TP
+.SM -EFAULT
+Memory region is not properly mapped.
+.TP
+.SM -EINVAL
+One or more invalid arguments are given.
+.TP
+.SM EOPNOTSUPP
+File heat operation is not supported.
+.SH "SEE ALSO"
+.BR lustreapi (7)

lustre/doc/llapi_heat_set.3
@@ -1,0 +1,1 @@
+.so man3/llapi_heat_get.3

lustre/include/lustre/lustreapi.h
@@ -1008,6 +1008,9 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count);
 int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst,
 		       off_t pos, size_t count);
 
+int llapi_heat_get(int fd, struct lu_heat *heat);
+int llapi_heat_set(int fd, __u64 flags);
+
 /** @} llapi */
 
 #if defined(__cplusplus)

commit ac1f97a881019b78ce3289885461259d280562b3

LU-10602 utils: fix file heat support
Change the LL_IOC_HEAT_SET ioctl number assignment to reduce the
number of different values used, since we are running out.  Use
a __u64 as the IOC struct argument instead of a "long" since that
is what is actually passed, and it avoids being CPU-dependent.

Move the LU_HEAT_FLAG_* values into an enum to avoid a generic
"flags" argument in the code.  This makes it clear what is passed.

Clean up code style for lfs_heat_get() and lfs_heat_set().

Fixes: ae723cf8161f ("LU-10602 llite: add file heat support")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: If06212d2d62d085a2104cf54ae9a10e512eb2efd
Reviewed-on: https://review.whamcloud.com/34757
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Jenkins
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -479,7 +479,7 @@ struct ll_ioc_lease_id {
 #define LL_IOC_GETPARENT		_IOWR('f', 249, struct getparent)
 #define LL_IOC_LADVISE			_IOR('f', 250, struct llapi_lu_ladvise)
 #define LL_IOC_HEAT_GET			_IOWR('f', 251, struct lu_heat)
-#define LL_IOC_HEAT_SET			_IOW('f', 252, long)
+#define LL_IOC_HEAT_SET			_IOW('f', 251, __u64)
 
 #ifndef	FS_IOC_FSGETXATTR
 /*
@@ -2240,8 +2240,10 @@ enum lu_heat_flag_bit {
 	LU_HEAT_FLAG_BIT_CLEAR,
 };
 
-#define LU_HEAT_FLAG_CLEAR	(1 << LU_HEAT_FLAG_BIT_CLEAR)
-#define LU_HEAT_FLAG_OFF	(1 << LU_HEAT_FLAG_BIT_OFF)
+enum lu_heat_flag {
+	LU_HEAT_FLAG_OFF	= 1ULL << LU_HEAT_FLAG_BIT_OFF,
+	LU_HEAT_FLAG_CLEAR	= 1ULL << LU_HEAT_FLAG_BIT_CLEAR,
+};
 
 enum obd_heat_type {
 	OBD_HEAT_READSAMPLE	= 0,

lustre/llite/file.c
@@ -3301,7 +3301,7 @@ static void ll_heat_get(struct inode *inode, struct lu_heat *heat)
 	spin_unlock(&lli->lli_heat_lock);
 }
 
-static int ll_heat_set(struct inode *inode, __u64 flags)
+static int ll_heat_set(struct inode *inode, enum lu_heat_flag flags)
 {
 	struct ll_inode_info *lli = ll_i2info(inode);
 	int rc = 0;

lustre/utils/lfs.c
@@ -8209,16 +8209,15 @@ next:
 	return rc;
 }
 
-
 static const char *const heat_names[] = LU_HEAT_NAMES;
 
 static int lfs_heat_get(int argc, char **argv)
 {
-	struct lu_heat	*heat;
-	int		 rc = 0, rc2;
-	char		*path;
-	int		 fd;
-	int		 i;
+	struct lu_heat *heat;
+	int rc = 0, rc2;
+	char *path;
+	int fd;
+	int i;
 
 	if (argc <= 1)
 		return CMD_HELP;
@@ -8264,25 +8263,22 @@ next:
 
 static int lfs_heat_set(int argc, char **argv)
 {
-	struct option	 long_opts[] = {
-		{"clear", no_argument, 0, 'c'},
-		{"off", no_argument, 0, 'o'},
-		{"on", no_argument, 0, 'O'},
-		{0, 0, 0, 0}
-	};
-	char		 short_opts[] = "coO";
-	int		 rc = 0, rc2;
-	char		*path;
-	int		 fd;
-	__u64		 flags = 0;
-	int		 c;
+	struct option long_opts[] = {
+	{ .val = 'c',	.name = "clear",	.has_arg = no_argument },
+	{ .val = 'o',	.name = "off",		.has_arg = no_argument },
+	{ .val = 'O',	.name = "on",		.has_arg = no_argument },
+	{ .name = NULL } };
+	enum lu_heat_flag flags = 0;
+	int rc = 0, rc2;
+	char *path;
+	int fd;
+	int c;
 
 	if (argc <= 1)
 		return CMD_HELP;
 
 	optind = 0;
-	while ((c = getopt_long(argc, argv, short_opts,
-				long_opts, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "coO", long_opts, NULL)) != -1) {
 		switch (c) {
 		case 'c':
 			flags |= LU_HEAT_FLAG_CLEAR;