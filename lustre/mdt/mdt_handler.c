lustre/lustre-release

commit fca35f74f9ec5c5ed77e774f3e3209d9df057a01

LU-9193 security: return security context for metadata ops
Security layer needs to fetch security context of files/dirs
upon metadata ops like lookup, getattr, open, truncate, and
layout, for its own purpose and control checks.
Retrieving the security context consists in a getxattr operation
at the file system level. The fact that the requested metadata
operation and the getxattr are not atomic can create a window
for a dead-lock situation where, based on some access patterns,
all MDT service threads can become stuck waiting for lookup lock
to be released and thus unable to serve getxattr for security context.
Another problem is that sending an additional getxattr request for
every metadata op hurts performance.

This patch introduces a way to get atomicity by having
the MDT return security context upon granted lock reply,
sparing the client an additional getxattr request.

Test-Parameters: serverbuildno=62488 serverjob=lustre-reviews testlist=sanity,sanity-selinux clientselinux
Test-Parameters: clientbuildno=4033 clientjob=lustre-reviews-patchless testlist=sanity,sanity-selinux clientselinux
Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Iaaf4d93f8d3bf31b5a2c23e7db36b3cb3feb31ba
Reviewed-on: https://review.whamcloud.com/26831
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -1622,9 +1622,11 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 	struct lu_name         *lname = NULL;
 	struct mdt_lock_handle *lhp = NULL;
 	struct ldlm_lock       *lock;
+	struct req_capsule *pill = info->mti_pill;
 	__u64 try_bits = 0;
 	bool is_resent;
 	int ma_need = 0;
+	bool deal_with_dom = false;
 	int rc;
 
 	ENTRY;
@@ -1681,18 +1683,20 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 		if (unlikely(rc != 0))
 			mdt_object_unlock(info, child, lhc, 1);
 
-                RETURN(rc);
-        }
+		mdt_pack_secctx_in_reply(info, child);
+
+		RETURN(rc);
+	}
 
 	lname = &info->mti_name;
-	mdt_name_unpack(info->mti_pill, &RMF_NAME, lname, MNF_FIX_ANON);
+	mdt_name_unpack(pill, &RMF_NAME, lname, MNF_FIX_ANON);
 
 	if (lu_name_is_valid(lname)) {
 		CDEBUG(D_INODE, "getattr with lock for "DFID"/"DNAME", "
 		       "ldlm_rep = %p\n", PFID(mdt_object_fid(parent)),
 		       PNAME(lname), ldlm_rep);
 	} else {
-		reqbody = req_capsule_client_get(info->mti_pill, &RMF_MDT_BODY);
+		reqbody = req_capsule_client_get(pill, &RMF_MDT_BODY);
 		if (unlikely(reqbody == NULL))
 			RETURN(err_serious(-EPROTO));
 
@@ -1845,16 +1849,17 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 				child_bits &= ~MDS_INODELOCK_UPDATE;
 			rc = mdt_object_lock(info, child, lhc, child_bits);
 		}
-                if (unlikely(rc != 0))
-                        GOTO(out_child, rc);
-        }
+		if (unlikely(rc != 0))
+			GOTO(out_child, rc);
+	}
 
-        lock = ldlm_handle2lock(&lhc->mlh_reg_lh);
+	lock = ldlm_handle2lock(&lhc->mlh_reg_lh);
 
-        /* finally, we can get attr for child. */
-        rc = mdt_getattr_internal(info, child, ma_need);
-        if (unlikely(rc != 0)) {
-                mdt_object_unlock(info, child, lhc, 1);
+	/* finally, we can get attr for child. */
+	rc = mdt_getattr_internal(info, child, ma_need);
+	if (unlikely(rc != 0)) {
+		mdt_object_unlock(info, child, lhc, 1);
+		GOTO(out_lock, rc);
 	} else if (lock) {
 		/* Debugging code. */
 		LDLM_DEBUG(lock, "Returning lock to client");
@@ -1866,31 +1871,32 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 
 		if (S_ISREG(lu_object_attr(&child->mot_obj)) &&
 		    mdt_object_exists(child) && !mdt_object_remote(child) &&
-		    child != parent) {
-			LDLM_LOCK_PUT(lock);
-			mdt_object_put(info->mti_env, child);
-			/* NB: call the mdt_pack_size2body always after
-			 * mdt_object_put(), that is why this special
-			 * exit path is used. */
-			rc = mdt_pack_size2body(info, child_fid,
-						&lhc->mlh_reg_lh);
-			if (rc != 0 && child_bits & MDS_INODELOCK_DOM) {
-				/* DOM lock was taken in advance but this is
-				 * not DoM file. Drop the lock. */
-				lock_res_and_lock(lock);
-				ldlm_inodebits_drop(lock, MDS_INODELOCK_DOM);
-				unlock_res_and_lock(lock);
-			}
-
-			GOTO(out_parent, rc = 0);
-		}
+		    child != parent)
+			deal_with_dom = true;
 	}
+
+	mdt_pack_secctx_in_reply(info, child);
+
+out_lock:
 	if (lock)
 		LDLM_LOCK_PUT(lock);
 
 	EXIT;
 out_child:
 	mdt_object_put(info->mti_env, child);
+	if (deal_with_dom) {
+		rc = mdt_pack_size2body(info, child_fid,
+					&lhc->mlh_reg_lh);
+		if (rc != 0 && child_bits & MDS_INODELOCK_DOM) {
+			/* DOM lock was taken in advance but this is
+			 * not DoM file. Drop the lock.
+			 */
+			lock_res_and_lock(lock);
+			ldlm_inodebits_drop(lock, MDS_INODELOCK_DOM);
+			unlock_res_and_lock(lock);
+		}
+		rc = 0;
+	}
 out_parent:
 	if (lhp)
 		mdt_object_unlock(info, parent, lhp, 1);
@@ -2096,6 +2102,28 @@ static inline bool mdt_is_readonly_open(struct mdt_thread_info *info, __u32 op)
 	     !(info->mti_spec.sp_cr_flags & (MDS_FMODE_WRITE | MDS_OPEN_CREAT));
 }
 
+static void mdt_preset_secctx_size(struct mdt_thread_info *info)
+{
+	struct req_capsule *pill = info->mti_pill;
+
+	if (req_capsule_has_field(pill, &RMF_FILE_SECCTX,
+				  RCL_SERVER) &&
+	    req_capsule_has_field(pill, &RMF_FILE_SECCTX_NAME,
+				  RCL_CLIENT)) {
+		if (req_capsule_get_size(pill, &RMF_FILE_SECCTX_NAME,
+					 RCL_CLIENT) != 0) {
+			/* pre-set size in server part with max size */
+			req_capsule_set_size(pill, &RMF_FILE_SECCTX,
+					     RCL_SERVER,
+					     info->mti_mdt->mdt_max_ea_size);
+		} else {
+			req_capsule_set_size(pill, &RMF_FILE_SECCTX,
+					     RCL_SERVER, 0);
+		}
+	}
+
+}
+
 static int mdt_reint_internal(struct mdt_thread_info *info,
                               struct mdt_lock_handle *lhc,
                               __u32 op)
@@ -2124,7 +2152,7 @@ static int mdt_reint_internal(struct mdt_thread_info *info,
 				     DEF_REP_MD_SIZE);
 
 	/* llog cookies are always 0, the field is kept for compatibility */
-        if (req_capsule_has_field(pill, &RMF_LOGCOOKIES, RCL_SERVER))
+	if (req_capsule_has_field(pill, &RMF_LOGCOOKIES, RCL_SERVER))
 		req_capsule_set_size(pill, &RMF_LOGCOOKIES, RCL_SERVER, 0);
 
 	/* Set ACL reply buffer size as LUSTRE_POSIX_ACL_MAX_SIZE_OLD
@@ -2134,33 +2162,35 @@ static int mdt_reint_internal(struct mdt_thread_info *info,
 		req_capsule_set_size(pill, &RMF_ACL, RCL_SERVER,
 				     LUSTRE_POSIX_ACL_MAX_SIZE_OLD);
 
-        rc = req_capsule_server_pack(pill);
-        if (rc != 0) {
-                CERROR("Can't pack response, rc %d\n", rc);
-                RETURN(err_serious(rc));
-        }
+	mdt_preset_secctx_size(info);
 
-        if (req_capsule_has_field(pill, &RMF_MDT_BODY, RCL_SERVER)) {
-                repbody = req_capsule_server_get(pill, &RMF_MDT_BODY);
-                LASSERT(repbody);
+	rc = req_capsule_server_pack(pill);
+	if (rc != 0) {
+		CERROR("Can't pack response, rc %d\n", rc);
+		RETURN(err_serious(rc));
+	}
+
+	if (req_capsule_has_field(pill, &RMF_MDT_BODY, RCL_SERVER)) {
+		repbody = req_capsule_server_get(pill, &RMF_MDT_BODY);
+		LASSERT(repbody);
 		repbody->mbo_eadatasize = 0;
 		repbody->mbo_aclsize = 0;
-        }
+	}
 
-        OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_REINT_DELAY, 10);
+	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_REINT_DELAY, 10);
 
-        /* for replay no cookkie / lmm need, because client have this already */
-        if (info->mti_spec.no_create)
-                if (req_capsule_has_field(pill, &RMF_MDT_MD, RCL_SERVER))
-                        req_capsule_set_size(pill, &RMF_MDT_MD, RCL_SERVER, 0);
+	/* for replay no cookkie / lmm need, because client have this already */
+	if (info->mti_spec.no_create)
+		if (req_capsule_has_field(pill, &RMF_MDT_MD, RCL_SERVER))
+			req_capsule_set_size(pill, &RMF_MDT_MD, RCL_SERVER, 0);
 
-        rc = mdt_init_ucred_reint(info);
-        if (rc)
-                GOTO(out_shrink, rc);
+	rc = mdt_init_ucred_reint(info);
+	if (rc)
+		GOTO(out_shrink, rc);
 
-        rc = mdt_fix_attr_ucred(info, op);
-        if (rc != 0)
-                GOTO(out_ucred, rc = err_serious(rc));
+	rc = mdt_fix_attr_ucred(info, op);
+	if (rc != 0)
+		GOTO(out_ucred, rc = err_serious(rc));
 
 	rc = mdt_check_resent(info, mdt_reconstruct, lhc);
 	if (rc < 0) {
@@ -2168,12 +2198,12 @@ static int mdt_reint_internal(struct mdt_thread_info *info,
 	} else if (rc == 1) {
 		DEBUG_REQ(D_INODE, mdt_info_req(info), "resent opt.");
 		rc = lustre_msg_get_status(mdt_info_req(info)->rq_repmsg);
-                GOTO(out_ucred, rc);
-        }
-        rc = mdt_reint_rec(info, lhc);
-        EXIT;
+		GOTO(out_ucred, rc);
+	}
+	rc = mdt_reint_rec(info, lhc);
+	EXIT;
 out_ucred:
-        mdt_exit_ucred(info);
+	mdt_exit_ucred(info);
 out_shrink:
 	mdt_client_compatibility(info);
 
@@ -3364,7 +3394,12 @@ static int mdt_unpack_req_pack_rep(struct mdt_thread_info *info,
 			req_capsule_set_size(pill, &RMF_ACL, RCL_SERVER,
 					     LUSTRE_POSIX_ACL_MAX_SIZE_OLD);
 
+		mdt_preset_secctx_size(info);
+
 		rc = req_capsule_server_pack(pill);
+		if (rc)
+			CWARN("%s: cannot pack response: rc = %d\n",
+				      mdt_obd_name(info->mti_mdt), rc);
 	}
 	RETURN(rc);
 }

lustre/mdt/mdt_lib.c
@@ -1720,3 +1720,45 @@ int mdt_reint_unpack(struct mdt_thread_info *info, __u32 op)
         }
         RETURN(rc);
 }
+
+void mdt_pack_secctx_in_reply(struct mdt_thread_info *info,
+			      struct mdt_object *child)
+{
+	char *secctx_name;
+	struct lu_buf *buffer;
+	struct mdt_body *repbody;
+	struct req_capsule *pill = info->mti_pill;
+	int rc;
+
+	if (req_capsule_has_field(pill, &RMF_FILE_SECCTX, RCL_SERVER) &&
+	    req_capsule_get_size(pill, &RMF_FILE_SECCTX, RCL_SERVER) != 0) {
+		secctx_name =
+			req_capsule_client_get(pill, &RMF_FILE_SECCTX_NAME);
+		buffer = &info->mti_buf;
+
+		/* fill reply buffer with security context now */
+		buffer->lb_len = req_capsule_get_size(pill, &RMF_FILE_SECCTX,
+						      RCL_SERVER);
+		buffer->lb_buf = req_capsule_server_get(info->mti_pill,
+							&RMF_FILE_SECCTX);
+		rc = mo_xattr_get(info->mti_env, mdt_object_child(child),
+				  buffer, secctx_name);
+		if (rc >= 0) {
+			CDEBUG(D_SEC,
+			       "found security context of size %d for "DFID"\n",
+			       rc, PFID(mdt_object_fid(child)));
+
+			repbody = req_capsule_server_get(pill, &RMF_MDT_BODY);
+			repbody->mbo_valid |= OBD_MD_SECCTX;
+			if (rc < buffer->lb_len)
+				req_capsule_shrink(pill, &RMF_FILE_SECCTX, rc,
+						   RCL_SERVER);
+		} else {
+			CDEBUG(D_SEC,
+			     "security context not found for "DFID": rc = %d\n",
+			     PFID(mdt_object_fid(child)), rc);
+			req_capsule_shrink(pill, &RMF_FILE_SECCTX, 0,
+					   RCL_SERVER);
+		}
+	}
+}

lustre/autoconf/lustre-core.m4
@@ -1497,6 +1497,21 @@ security_dentry_init_security, [
 ]) # LC_HAVE_SECURITY_DENTRY_INIT_SECURITY
 
 #
+# 3.10 exports security_inode_listsecurity
+#
+AC_DEFUN([LC_HAVE_SECURITY_INODE_LISTSECURITY], [
+LB_CHECK_COMPILE([if security_inode_listsecurity() is available/exported],
+security_inode_listsecurity, [
+	#include <linux/security.h>
+],[
+	security_inode_listsecurity(NULL, NULL, 0);
+],[
+	AC_DEFINE(HAVE_SECURITY_INODE_LISTSECURITY, 1,
+		  [security_inode_listsecurity() is available/exported])
+])
+]) # LC_HAVE_SECURITY_INODE_LISTSECURITY
+
+#
 # LC_INVALIDATE_RANGE
 #
 # 3.11 invalidatepage requires the length of the range to invalidate
@@ -3157,6 +3172,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_HAVE_PROC_REMOVE
 	LC_HAVE_PROJECT_QUOTA
 	LC_HAVE_SECURITY_DENTRY_INIT_SECURITY
+	LC_HAVE_SECURITY_INODE_LISTSECURITY
 
 	# 3.11
 	LC_INVALIDATE_RANGE

lustre/include/obd.h
@@ -882,8 +882,9 @@ struct md_op_data {
 	__u64			op_data_version;
 	struct lustre_handle	op_lease_handle;
 
-	/* File security context, for creates. */
+	/* File security context, for creates/metadata ops */
 	const char	       *op_file_secctx_name;
+	__u32			op_file_secctx_name_size;
 	void		       *op_file_secctx;
 	__u32			op_file_secctx_size;
 

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1291,6 +1291,7 @@ lov_mds_md_max_stripe_count(size_t buf_size, __u32 lmm_magic)
 #define OBD_MD_DEFAULT_MEA   (0x0040000000000000ULL) /* default MEA */
 #define OBD_MD_FLOSTLAYOUT   (0x0080000000000000ULL) /* contain ost_layout */
 #define OBD_MD_FLPROJID      (0x0100000000000000ULL) /* project ID */
+#define OBD_MD_SECCTX        (0x0200000000000000ULL) /* embed security xattr */
 
 #define OBD_MD_FLALLQUOTA (OBD_MD_FLUSRQUOTA | \
 			   OBD_MD_FLGRPQUOTA | \

lustre/llite/llite_internal.h
@@ -320,6 +320,9 @@ int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
 int ll_inode_init_security(struct dentry *dentry, struct inode *inode,
 			   struct inode *dir);
 
+int ll_listsecurity(struct inode *inode, char *secctx_name,
+		    size_t secctx_name_size);
+
 /*
  * Locking to guarantee consistency of non-atomic updates to long long i_size,
  * consistency between file size and KMS.

lustre/llite/namei.c
@@ -601,7 +601,8 @@ struct dentry *ll_splice_alias(struct inode *inode, struct dentry *de)
 
 static int ll_lookup_it_finish(struct ptlrpc_request *request,
 			       struct lookup_intent *it,
-			       struct inode *parent, struct dentry **de)
+			       struct inode *parent, struct dentry **de,
+			       void *secctx, __u32 secctxlen)
 {
 	struct inode		 *inode = NULL;
 	__u64			  bits = 0;
@@ -614,6 +615,10 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 	CDEBUG(D_DENTRY, "it %p it_disposition %x\n", it,
 	       it->it_disposition);
 	if (!it_disposition(it, DISP_LOOKUP_NEG)) {
+		struct req_capsule *pill = &request->rq_pill;
+		struct mdt_body *body = req_capsule_server_get(pill,
+							       &RMF_MDT_BODY);
+
 		rc = ll_prep_inode(&inode, request, (*de)->d_sb, it);
 		if (rc)
 			RETURN(rc);
@@ -632,6 +637,33 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 		 * ll_glimpse_size or some equivalent themselves anyway.
 		 * Also see bug 7198.
 		 */
+
+		/* If security context was returned by MDT, put it in
+		 * inode now to save an extra getxattr from security hooks,
+		 * and avoid deadlock.
+		 */
+		if (body->mbo_valid & OBD_MD_SECCTX) {
+			secctx = req_capsule_server_get(pill, &RMF_FILE_SECCTX);
+			secctxlen = req_capsule_get_size(pill,
+							   &RMF_FILE_SECCTX,
+							   RCL_SERVER);
+
+			if (secctxlen)
+				CDEBUG(D_SEC, "server returned security context"
+				       " for "DFID"\n",
+				       PFID(ll_inode2fid(inode)));
+		}
+
+		if (secctx != NULL && secctxlen != 0) {
+			inode_lock(inode);
+			rc = security_inode_notifysecctx(inode, secctx,
+							 secctxlen);
+			inode_unlock(inode);
+			if (rc)
+				CWARN("cannot set security context for "
+				      DFID": rc = %d\n",
+				      PFID(ll_inode2fid(inode)), rc);
+		}
 	}
 
 	/* Only hash *de if it is unhashed (new dentry).
@@ -692,9 +724,11 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 	struct dentry *save = dentry, *retval;
 	struct ptlrpc_request *req = NULL;
 	struct md_op_data *op_data = NULL;
-        __u32 opc;
-        int rc;
-        ENTRY;
+	__u32 opc;
+	int rc;
+	char secctx_name[XATTR_NAME_MAX + 1];
+
+	ENTRY;
 
         if (dentry->d_name.len > ll_i2sbi(parent)->ll_namelen)
                 RETURN(ERR_PTR(-ENAMETOOLONG));
@@ -746,6 +780,28 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 			*secctx = op_data->op_file_secctx;
 		if (secctxlen != NULL)
 			*secctxlen = op_data->op_file_secctx_size;
+	} else {
+		if (secctx != NULL)
+			*secctx = NULL;
+		if (secctxlen != NULL)
+			*secctxlen = 0;
+	}
+
+	/* ask for security context upon intent */
+	if (it->it_op & (IT_LOOKUP | IT_GETATTR | IT_OPEN)) {
+		/* get name of security xattr to request to server */
+		rc = ll_listsecurity(parent, secctx_name,
+				     sizeof(secctx_name));
+		if (rc < 0) {
+			CDEBUG(D_SEC, "cannot get security xattr name for "
+			       DFID": rc = %d\n",
+			       PFID(ll_inode2fid(parent)), rc);
+		} else if (rc > 0) {
+			op_data->op_file_secctx_name = secctx_name;
+			op_data->op_file_secctx_name_size = rc;
+			CDEBUG(D_SEC, "'%.*s' is security xattr for "DFID"\n",
+			       rc, secctx_name, PFID(ll_inode2fid(parent)));
+		}
 	}
 
 	rc = md_intent_lock(ll_i2mdexp(parent), op_data, it, &req,
@@ -781,7 +837,9 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 
 	/* dir layout may change */
 	ll_unlock_md_op_lsm(op_data);
-	rc = ll_lookup_it_finish(req, it, parent, &dentry);
+	rc = ll_lookup_it_finish(req, it, parent, &dentry,
+				 secctx != NULL ? *secctx : NULL,
+				 secctxlen != NULL ? *secctxlen : 0);
         if (rc != 0) {
                 ll_intent_release(it);
                 GOTO(out, retval = ERR_PTR(rc));

lustre/llite/xattr_security.c
@@ -187,3 +187,36 @@ out_free:
 	return err;
 }
 #endif /* HAVE_SECURITY_IINITSEC_CALLBACK */
+
+/**
+ * Get security context xattr name used by policy.
+ *
+ * \retval >= 0     length of xattr name
+ * \retval < 0      failure to get security context xattr name
+ */
+int
+ll_listsecurity(struct inode *inode, char *secctx_name, size_t secctx_name_size)
+{
+	int rc;
+
+	if (!selinux_is_enabled())
+		return 0;
+
+#ifdef HAVE_SECURITY_INODE_LISTSECURITY
+	rc = security_inode_listsecurity(inode, secctx_name, secctx_name_size);
+	if (rc >= secctx_name_size)
+		rc = -ERANGE;
+	else if (rc >= 0)
+		secctx_name[rc] = '\0';
+	return rc;
+#else /* !HAVE_SECURITY_INODE_LISTSECURITY */
+	rc = sizeof(XATTR_NAME_SELINUX);
+	if (secctx_name && rc < secctx_name_size) {
+		memcpy(secctx_name, XATTR_NAME_SELINUX, rc);
+		secctx_name[rc] = '\0';
+	} else {
+		rc = -ERANGE;
+	}
+	return rc;
+#endif /* HAVE_SECURITY_INODE_LISTSECURITY */
+}

commit cb61ed93f8563c26b6a6db396478fe54f8dc42cb

LU-12212 mdt: fix SECCTX reply buffer handling
LU-9193 changes for inline SECCTX in reply may cause often
resends and reconnects in some loads, e.g. dbench runs.
That is caused by missed buffer shrink when SECCTX is not
used.

Patch fo the following:
- shrink SECCTX buffer if it is not used
- in mdt_getattr_name_lock() fill SECCTX buffer a bit earlier
  for simpler handling DoM size attributes, also move
  LDLM_LOCK_PUT() at the end of block to don't use 'lock'
  after LDLM_LOCK_PUT()

Fixes: fca35f74f9ec ("LU-9193 security: return security context for metadata ops")
Test-Parameters: clientselinux testlist=sanity envdefinitions=EXCEPT=103a
Test-Parameters: mdscount=2 mdtcount=4 clientselinux testlist=recovery-small,sanity-selinux
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I9beffd06f76c3bd8e826ba4ab0ce70ac3f57951c
Reviewed-on: https://review.whamcloud.com/34734
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -1626,7 +1626,6 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 	__u64 try_bits = 0;
 	bool is_resent;
 	int ma_need = 0;
-	bool deal_with_dom = false;
 	int rc;
 
 	ENTRY;
@@ -1853,14 +1852,17 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 			GOTO(out_child, rc);
 	}
 
-	lock = ldlm_handle2lock(&lhc->mlh_reg_lh);
-
 	/* finally, we can get attr for child. */
 	rc = mdt_getattr_internal(info, child, ma_need);
 	if (unlikely(rc != 0)) {
 		mdt_object_unlock(info, child, lhc, 1);
-		GOTO(out_lock, rc);
-	} else if (lock) {
+		GOTO(out_child, rc);
+	}
+
+	mdt_pack_secctx_in_reply(info, child);
+
+	lock = ldlm_handle2lock(&lhc->mlh_reg_lh);
+	if (lock) {
 		/* Debugging code. */
 		LDLM_DEBUG(lock, "Returning lock to client");
 		LASSERTF(fid_res_name_eq(mdt_object_fid(child),
@@ -1871,32 +1873,27 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 
 		if (S_ISREG(lu_object_attr(&child->mot_obj)) &&
 		    mdt_object_exists(child) && !mdt_object_remote(child) &&
-		    child != parent)
-			deal_with_dom = true;
-	}
-
-	mdt_pack_secctx_in_reply(info, child);
-
-out_lock:
-	if (lock)
+		    child != parent) {
+			mdt_object_put(info->mti_env, child);
+			rc = mdt_pack_size2body(info, child_fid,
+						&lhc->mlh_reg_lh);
+			if (rc != 0 && child_bits & MDS_INODELOCK_DOM) {
+				/* DOM lock was taken in advance but this is
+				 * not DoM file. Drop the lock.
+				 */
+				lock_res_and_lock(lock);
+				ldlm_inodebits_drop(lock, MDS_INODELOCK_DOM);
+				unlock_res_and_lock(lock);
+			}
+			LDLM_LOCK_PUT(lock);
+			GOTO(out_parent, rc = 0);
+		}
 		LDLM_LOCK_PUT(lock);
+	}
 
 	EXIT;
 out_child:
 	mdt_object_put(info->mti_env, child);
-	if (deal_with_dom) {
-		rc = mdt_pack_size2body(info, child_fid,
-					&lhc->mlh_reg_lh);
-		if (rc != 0 && child_bits & MDS_INODELOCK_DOM) {
-			/* DOM lock was taken in advance but this is
-			 * not DoM file. Drop the lock.
-			 */
-			lock_res_and_lock(lock);
-			ldlm_inodebits_drop(lock, MDS_INODELOCK_DOM);
-			unlock_res_and_lock(lock);
-		}
-		rc = 0;
-	}
 out_parent:
 	if (lhp)
 		mdt_object_unlock(info, parent, lhp, 1);

lustre/mdt/mdt_lib.c
@@ -790,6 +790,12 @@ int mdt_fix_reply(struct mdt_thread_info *info)
 		req_capsule_shrink(pill, &RMF_LOGCOOKIES, acl_size, RCL_SERVER);
 	}
 
+	/* Shrink optional SECCTX buffer if it is not used */
+	if (req_capsule_has_field(pill, &RMF_FILE_SECCTX, RCL_SERVER) &&
+	    req_capsule_get_size(pill, &RMF_FILE_SECCTX, RCL_SERVER) != 0 &&
+	    !(body->mbo_valid & OBD_MD_SECCTX))
+		req_capsule_shrink(pill, &RMF_FILE_SECCTX, 0, RCL_SERVER);
+
 	/*
 	 * Some more field should be shrinked if needed.
 	 * This should be done by those who added fields to reply message.