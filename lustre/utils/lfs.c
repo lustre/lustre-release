lustre/lustre-release

commit 591a9b4cebc510ff51f0fdb944e5a81f08fdaf62

LU-9846 lod: Add overstriping support
Each stripe in a shared file in Lustre corresponds to a
single LDLM extent locking domain and also to a single
object on disk (and in the OSS page cache).  LDLM locks are
extent locks, but there are still significant issues with
false sharing with multiple writers.  On-disk file systems
also have per-object performance limitations for both read
and write.

The LDLM limitation means it is best to have a single
writer per stripe, but modern OSTs can be faster than a
single client, so this restricts maximum performance unless
special methods are used (eg, Lustre lock ahead).

The on disk file system limitations mean that even if LDLM
locking is not an issue (read and not write, or lockahead),
OST performance in a shared file is still limited by having
only one object per OST.

These limitations make it impossible to get the full
performance of a modern Lustre FS with a single shared
file.

This patch makes it possible to have >1 stripe on a given
OST in each layout component.  This is known as
overstriping.  It works exactly like a normally striped
file, and is largely transparent to users.

By raising the object count per OST, this avoids the single
object limits, and by creating more stripes, also avoids
the "single effective writer per stripe" LDLM limitation.

However, it is only desirable in some situations, so users
must request it with a special setstripe command:

lfs setstripe -C [count] [file]

Users can also access overstriping using the standard '-o'
option to manually select OSTs:

lfs setstripe -o [ost_indices] [file]

Overstriping also makes it easy to test layout size limits,so we add a
test for that.

Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I14bb94b05642b3542a965e84fda4615b997a4dea
Reviewed-on: https://review.whamcloud.com/28425
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>

lustre/utils/lfs.c
@@ -164,6 +164,7 @@ static inline int lfs_mirror_split(int argc, char **argv)
 #define SSM_CMD_COMMON(cmd) \
 	"usage: "cmd" [--component-end|-E <comp_end>]\n"		\
 	"                 [--stripe-count|-c <stripe_count>]\n"		\
+	"                 [--overstripe-count|-C <stripe_count>]\n"	\
 	"                 [--stripe-index|-i <start_ost_idx>]\n"	\
 	"                 [--stripe-size|-S <stripe_size>]\n"		\
 	"                 [--layout|-L <pattern>]\n"			\
@@ -174,6 +175,9 @@ static inline int lfs_mirror_split(int argc, char **argv)
 
 #define SSM_HELP_COMMON \
 	"\tstripe_count: Number of OSTs to stripe over (0=fs default, -1 all)\n" \
+	"\t		 Using -C instead of -c allows overstriping, which\n" \
+	 "\t		 will place more than one stripe per OST if\n" \
+	 "\t		 stripe_count is greater than the number of OSTs\n" \
 	"\tstart_ost_idx: OST index of first stripe (-1=default round robin)\n"\
 	"\tstripe_size:  Number of bytes on each OST (0=fs default)\n" \
 	"\t              Can be specified with K, M or G (for KB, MB, GB\n" \
@@ -569,6 +573,7 @@ command_t cmdlist[] = {
 	 "layout\nto another (may be not safe with concurent writes).\n"
 	 "usage: migrate  "
 	 "[--stripe-count|-c] <stripe_count>\n"
+	 "[--overstripe-count|-C] <stripe_count>\n"
 	 "		[--stripe-index|-i] <start_ost_index>\n"
 	 "		[--stripe-size|-S] <stripe_size>\n"
 	 "		[--pool|-p] <pool_name>\n"
@@ -578,6 +583,9 @@ command_t cmdlist[] = {
 	 "		[--non-direct|-D]\n"
 	 "		<file|directory>\n"
 	 "\tstripe_count:     number of OSTs to stripe a file over\n"
+	 "\t		  Using -C instead of -c allows overstriping, which\n"
+	 "\t		  will place more than one stripe per OST if\n"
+	 "\t		  stripe_count is greater than the number of OSTs\n"
 	 "\tstripe_ost_index: index of the first OST to stripe a file over\n"
 	 "\tstripe_size:      number of bytes to store before moving to the next OST\n"
 	 "\tpool_name:        name of the predefined pool of OSTs\n"
@@ -1951,20 +1959,23 @@ free_layout:
  * indices and ranges, for example "1,2-4,7". Add the indices into the
  * \a tgts array and remove duplicates.
  *
- * \param[out] tgts    array to store indices in
- * \param[in] size     size of \a tgts array
- * \param[in] offset   starting index in \a tgts
- * \param[in] arg      string containing OST index list
+ * \param[out] tgts		array to store indices in
+ * \param[in] size		size of \a tgts array
+ * \param[in] offset		starting index in \a tgts
+ * \param[in] arg		string containing OST index list
+ * \param[in/out] overstriping	index list may contain duplicates
  *
  * \retval positive    number of indices in \a tgts
  * \retval -EINVAL     unable to parse \a arg
  */
-static int parse_targets(__u32 *tgts, int size, int offset, char *arg)
+static int parse_targets(__u32 *tgts, int size, int offset, char *arg,
+			 unsigned long long *pattern)
 {
 	int rc;
 	int nr = offset;
 	int slots = size - offset;
 	char *ptr = NULL;
+	bool overstriped = false;
 	bool end_of_loop;
 
 	if (arg == NULL)
@@ -1972,8 +1983,8 @@ static int parse_targets(__u32 *tgts, int size, int offset, char *arg)
 
 	end_of_loop = false;
 	while (!end_of_loop) {
-		int start_index;
-		int end_index;
+		int start_index = 0;
+		int end_index = 0;
 		int i;
 		char *endptr = NULL;
 
@@ -2004,14 +2015,21 @@ static int parse_targets(__u32 *tgts, int size, int offset, char *arg)
 
 			/* remove duplicate */
 			for (j = 0; j < offset; j++) {
-				if (tgts[j] == i)
-					break;
+				if (tgts[j] == i && pattern &&
+				    *pattern == LLAPI_LAYOUT_OVERSTRIPING)
+					overstriped = true;
+				else if (tgts[j] == i)
+					return -EINVAL;
 			}
-			if (j == offset) { /* no duplicate */
+
+			j = offset;
+
+			if (j == offset) { /* check complete */
 				tgts[nr++] = i;
 				--slots;
 			}
 		}
+
 		if (slots == 0 && i < end_index)
 			break;
 
@@ -2022,6 +2040,9 @@ static int parse_targets(__u32 *tgts, int size, int offset, char *arg)
 	}
 	if (!end_of_loop && ptr != NULL)
 		*ptr = ',';
+
+	if (!overstriped && pattern)
+		*pattern = LLAPI_LAYOUT_DEFAULT;
 
 	return rc < 0 ? rc : nr;
 }
@@ -2201,6 +2222,13 @@ static int comp_args_to_layout(struct llapi_layout **composite,
 		}
 		/* Data-on-MDT component has always single stripe up to end */
 		lsa->lsa_stripe_size = lsa->lsa_comp_end;
+	} else if (lsa->lsa_pattern == LLAPI_LAYOUT_OVERSTRIPING) {
+		rc = llapi_layout_pattern_set(layout, lsa->lsa_pattern);
+		if (rc) {
+			fprintf(stderr, "Set stripe pattern %#llx failed. %s\n",
+				lsa->lsa_pattern, strerror(errno));
+			return rc;
+		}
 	}
 
 	rc = llapi_layout_stripe_size_set(layout, lsa->lsa_stripe_size);
@@ -2326,6 +2354,10 @@ static int build_layout_from_yaml_node(struct cYAML *node,
 				} else if (!strcmp(string, "pattern")) {
 					if (!strcmp(node->cy_valuestring, "mdt"))
 						lsa->lsa_pattern = LLAPI_LAYOUT_MDT;
+					if (!strcmp(node->cy_valuestring,
+						    "raid0,overstriped"))
+						lsa->lsa_pattern =
+							LLAPI_LAYOUT_OVERSTRIPING;
 				} else if (!strcmp(string, "lcme_flags")) {
 					rc = comp_str2flags(node->cy_valuestring,
 							    &lsa->lsa_comp_flags,
@@ -2679,7 +2711,8 @@ static int lfs_setstripe_internal(int argc, char **argv,
 	{ .val = 'c',	.name = "stripe-count",	.has_arg = required_argument},
 	{ .val = 'c',	.name = "stripe_count",	.has_arg = required_argument},
 	{ .val = 'c',	.name = "mdt-count",	.has_arg = required_argument},
-/* find	{ .val = 'C',	.name = "ctime",	.has_arg = required_argument }*/
+	{ .val = 'C',	.name = "overstripe-count",
+						.has_arg = required_argument},
 	{ .val = 'd',	.name = "delete",	.has_arg = no_argument},
 	{ .val = 'd',	.name = "destroy",	.has_arg = no_argument},
 	/* --non-direct is only valid in migrate mode */
@@ -2735,7 +2768,7 @@ static int lfs_setstripe_internal(int argc, char **argv,
 	snprintf(cmd, sizeof(cmd), "%s %s", progname, argv[0]);
 	progname = cmd;
 	while ((c = getopt_long(argc, argv,
-				"bc:dDE:f:H:i:I:m:N::no:p:L:s:S:vx:y:",
+				"bc:C:dDE:f:H:i:I:m:N::no:p:L:s:S:vx:y:",
 				long_opts, NULL)) >= 0) {
 		switch (c) {
 		case 0:
@@ -2866,6 +2899,9 @@ static int lfs_setstripe_internal(int argc, char **argv,
 			}
 			migration_block = true;
 			break;
+		case 'C':
+			lsa.lsa_pattern = LLAPI_LAYOUT_OVERSTRIPING;
+			/* fall through */
 		case 'c':
 			lsa.lsa_stripe_count = strtoul(optarg, &end, 0);
 			if (*end != '\0') {
@@ -3021,7 +3057,7 @@ static int lfs_setstripe_internal(int argc, char **argv,
 			migrate_mdt_mode = true;
 			lsa.lsa_nr_tgts = parse_targets(tgts,
 						sizeof(tgts) / sizeof(__u32),
-						lsa.lsa_nr_tgts, optarg);
+						lsa.lsa_nr_tgts, optarg, NULL);
 			if (lsa.lsa_nr_tgts < 0) {
 				fprintf(stderr,
 					"%s %s: invalid MDT target(s) '%s'\n",
@@ -3090,9 +3126,15 @@ static int lfs_setstripe_internal(int argc, char **argv,
 				fprintf(stderr, "warning: '--ost-list' is "
 					"deprecated, use '--ost' instead\n");
 #endif
+			/* -o allows overstriping, and must note it because
+			 * parse_targets is shared with MDT striping, which
+			 * does not allow duplicates
+			 */
+			lsa.lsa_pattern = LLAPI_LAYOUT_OVERSTRIPING;
 			lsa.lsa_nr_tgts = parse_targets(tgts,
 						sizeof(tgts) / sizeof(__u32),
-						lsa.lsa_nr_tgts, optarg);
+						lsa.lsa_nr_tgts, optarg,
+						&lsa.lsa_pattern);
 			if (lsa.lsa_nr_tgts < 0) {
 				fprintf(stderr,
 					"%s %s: invalid OST target(s) '%s'\n",
@@ -3395,6 +3437,14 @@ static int lfs_setstripe_internal(int argc, char **argv,
 			param->lsp_stripe_offset = -1;
 		else
 			param->lsp_stripe_offset = lsa.lsa_stripe_off;
+		param->lsp_stripe_pattern =
+				llapi_pattern_to_lov(lsa.lsa_pattern);
+		if (param->lsp_stripe_pattern == EINVAL) {
+			fprintf(stderr, "error: %s: invalid stripe pattern\n",
+				argv[0]);
+			free(param);
+			goto usage_error;
+		}
 		param->lsp_pool = lsa.lsa_pool_name;
 		param->lsp_is_specific = false;
 		if (lsa.lsa_nr_tgts > 0) {
@@ -3656,6 +3706,8 @@ static int name2layout(__u32 *layout, char *name)
 			*layout |= LOV_PATTERN_RAID0;
 		else if (strcmp(layout_name, "mdt") == 0)
 			*layout |= LOV_PATTERN_MDT;
+		else if (strcmp(layout_name, "overstriping") == 0)
+			*layout |= LOV_PATTERN_OVERSTRIPING;
 		else
 			return -1;
 	}
@@ -5253,15 +5305,17 @@ static int lfs_setdirstripe(int argc, char **argv)
 					"%s %s: warning: '--index' deprecated, use '--mdt-index' instead\n",
 					progname, argv[0]);
 #endif
+			lsa.lsa_pattern = LLAPI_LAYOUT_OVERSTRIPING;
 			lsa.lsa_nr_tgts = parse_targets(mdts,
 						sizeof(mdts) / sizeof(__u32),
-						lsa.lsa_nr_tgts, optarg);
+						lsa.lsa_nr_tgts, optarg, NULL);
 			if (lsa.lsa_nr_tgts < 0) {
 				fprintf(stderr,
 					"%s %s: invalid MDT target(s) '%s'\n",
 					progname, argv[0], optarg);
 				return CMD_HELP;
 			}
+			lsa.lsa_pattern = 0;
 
 			lsa.lsa_tgts = mdts;
 			if (lsa.lsa_stripe_off == LLAPI_LAYOUT_DEFAULT)

lustre/doc/lfs-setstripe.1
@@ -146,6 +146,12 @@ The number of OSTs to stripe a file over. \fB0 \fRmeans to use the
 filesystem-wide default stripe count (default 1), and \fB-1 \fRmeans to stripe
 over all available OSTs.
 .TP
+.B -C\fR, \fB--overstripe-count \fR<\fIstripe_count\fR>
+The number of stripes to create, creating > 1 stripe per OST if count exceeds
+the number of OSTs in the file system. \fB0 \fRmeans to use the filesystem-wide
+default stripe count (default 1), and \fB-1 \fRmeans to stripe over all
+available OSTs.
+.TP
 .B -S\fR, \fB--stripe-size \fR<\fIstripe_size\fR>
 The number of bytes to store on each OST before moving to the next OST. A
 stripe size of

lustre/include/lustre/lustreapi.h
@@ -126,6 +126,8 @@ struct llapi_stripe_param {
 
 #define lsp_tgts	lsp_osts
 
+__u32 llapi_pattern_to_lov(uint64_t pattern);
+
 int llapi_file_open_param(const char *name, int flags, mode_t mode,
 			  const struct llapi_stripe_param *param);
 int llapi_file_create_foreign(const char *name, mode_t mode, __u32 type,
@@ -656,8 +658,9 @@ int llapi_layout_merge(struct llapi_layout **dst_layout,
  * stored using RAID0.  That is, data will be split evenly and without
  * redundancy across all OSTs in the layout.
  */
-#define LLAPI_LAYOUT_RAID0	0ULL
-#define LLAPI_LAYOUT_MDT	2ULL
+#define LLAPI_LAYOUT_RAID0		0ULL
+#define LLAPI_LAYOUT_MDT		2ULL
+#define LLAPI_LAYOUT_OVERSTRIPING	4ULL
 
 /**
 * The layout includes a specific set of OSTs on which to allocate.

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -892,6 +892,7 @@ struct ptlrpc_body_v2 {
 #define MDT_CONNECT_SUPPORTED2 (OBD_CONNECT2_FILE_SECCTX | \
 				OBD_CONNECT2_DIR_MIGRATE | \
 				OBD_CONNECT2_SUM_STATFS | \
+				OBD_CONNECT2_OVERSTRIPING | \
 				OBD_CONNECT2_FLR |\
 				OBD_CONNECT2_LOCK_CONVERT | \
 				OBD_CONNECT2_ARCHIVE_ID_ARRAY | \

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -547,11 +547,11 @@ struct fsxattr {
 #define LMV_USER_MAGIC_V0	0x0CD20CD0    /* old default lmv magic*/
 #define LMV_USER_MAGIC_SPECIFIC	0x0CD40CD0
 
-#define LOV_PATTERN_NONE	0x000
-#define LOV_PATTERN_RAID0	0x001
-#define LOV_PATTERN_RAID1	0x002
-#define LOV_PATTERN_MDT		0x100
-#define LOV_PATTERN_CMOBD	0x200
+#define LOV_PATTERN_NONE		0x000
+#define LOV_PATTERN_RAID0		0x001
+#define LOV_PATTERN_RAID1		0x002
+#define LOV_PATTERN_MDT			0x100
+#define LOV_PATTERN_OVERSTRIPING	0x200
 
 #define LOV_PATTERN_F_MASK	0xffff0000
 #define LOV_PATTERN_F_HOLE	0x40000000 /* there is hole in LOV EA */
@@ -563,7 +563,20 @@ struct fsxattr {
 static inline bool lov_pattern_supported(__u32 pattern)
 {
 	return (pattern & ~LOV_PATTERN_F_RELEASED) == LOV_PATTERN_RAID0 ||
+	       (pattern & ~LOV_PATTERN_F_RELEASED) ==
+			(LOV_PATTERN_RAID0 | LOV_PATTERN_OVERSTRIPING) ||
 	       (pattern & ~LOV_PATTERN_F_RELEASED) == LOV_PATTERN_MDT;
+}
+
+/* RELEASED and MDT patterns are not valid in many places, so rather than
+ * having many extra checks on lov_pattern_supported, we have this separate
+ * check for non-released, non-DOM components
+ */
+static inline bool lov_pattern_supported_normal_comp(__u32 pattern)
+{
+	return pattern == LOV_PATTERN_RAID0 ||
+	       pattern == (LOV_PATTERN_RAID0 | LOV_PATTERN_OVERSTRIPING);
+
 }
 
 #define LOV_MAXPOOLNAME 15
@@ -581,7 +594,7 @@ static inline bool lov_pattern_supported(__u32 pattern)
  * allocation that is sufficient for the current generation of systems.
  *
  * (max buffer size - lov+rpc header) / sizeof(struct lov_ost_data_v1) */
-#define LOV_MAX_STRIPE_COUNT 2000  /* ((12 * 4096 - 256) / 24) */
+#define LOV_MAX_STRIPE_COUNT 2000  /* ~((12 * 4096 - 256) / 24) */
 #define LOV_ALL_STRIPES       0xffff /* only valid for directories */
 #define LOV_V1_INSANE_STRIPE_COUNT 65532 /* maximum stripe count bz13933 */
 

lustre/lfsck/lfsck_layout.c
@@ -368,7 +368,7 @@ static int lfsck_layout_verify_header_v1v3(struct dt_object *obj,
 	}
 #endif
 
-	if (lov_pattern(pattern) != LOV_PATTERN_RAID0) {
+	if (!lov_pattern_supported_normal_comp(lov_pattern(pattern))) {
 		CDEBUG(D_LFSCK, "Unsupported LOV EA pattern %u for the file "
 		       DFID" in the component %x\n",
 		       pattern, PFID(lfsck_dto2fid(obj)), comp_id);

lustre/llite/llite_lib.c
@@ -223,6 +223,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 
 	data->ocd_connect_flags2 = OBD_CONNECT2_DIR_MIGRATE |
 				   OBD_CONNECT2_SUM_STATFS |
+				   OBD_CONNECT2_OVERSTRIPING |
 				   OBD_CONNECT2_FLR |
 				   OBD_CONNECT2_LOCK_CONVERT |
 				   OBD_CONNECT2_ARCHIVE_ID_ARRAY |

lustre/lod/lod_internal.h
@@ -764,7 +764,7 @@ __u16 lod_comp_entry_stripe_count(struct lod_object *lo,
 				  struct lod_layout_component *entry,
 				  bool is_dir);
 __u16 lod_get_stripe_count(struct lod_device *lod, struct lod_object *lo,
-			   __u16 stripe_count);
+			   __u16 stripe_count, bool overstriping);
 void lod_qos_statfs_update(const struct lu_env *env, struct lod_device *lod);
 
 /* lproc_lod.c */

commit 44405d4d6f7c0a77d540d6a114050ccabd0a4e9f

LU-9846 utils: hash may be overridden in 'lfs setdirstripe'
lfs_setdirstripe() may override 'hash' if '-H hash' is specified
before '-i', since LMV doesn't support OVERSTRIPING, this support
can be ignored.

Fixes: 591a9b4cebc5 ("LU-9846 lod: Add overstriping support")

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: If6db03d2d4f6d208da19ae064fde1d851f01beb4
Reviewed-on: https://review.whamcloud.com/35095
Tested-by: Jenkins
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/lfs.c
@@ -5324,7 +5324,6 @@ static int lfs_setdirstripe(int argc, char **argv)
 					"%s %s: warning: '--index' deprecated, use '--mdt-index' instead\n",
 					progname, argv[0]);
 #endif
-			lsa.lsa_pattern = LLAPI_LAYOUT_OVERSTRIPING;
 			lsa.lsa_nr_tgts = parse_targets(mdts,
 						sizeof(mdts) / sizeof(__u32),
 						lsa.lsa_nr_tgts, optarg, NULL);
@@ -5334,7 +5333,6 @@ static int lfs_setdirstripe(int argc, char **argv)
 					progname, argv[0], optarg);
 				return CMD_HELP;
 			}
-			lsa.lsa_pattern = 0;
 
 			lsa.lsa_tgts = mdts;
 			if (lsa.lsa_stripe_off == LLAPI_LAYOUT_DEFAULT)
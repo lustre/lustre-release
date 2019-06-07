44405d4d6f7c0a77d540d6a114050ccabd0a4e9f
Lai Siyao <lai.siyao@whamcloud.com>
2019-06-07 14:20:13 +0800

    LU-9846 utils: hash may be overridden in 'lfs setdirstripe'
    lfs_setdirstripe() may override 'hash' if '-H hash' is specified
    before '-i', since LMV doesn't support OVERSTRIPING, this support
    can be ignored.
    
    Fixes: 591a9b4cebc5 ("LU-9846 lod: Add overstriping support")
    
    Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
    Change-Id: If6db03d2d4f6d208da19ae064fde1d851f01beb4
    Reviewed-on: https://review.whamcloud.com/35095
    Tested-by: Jenkins
    Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/utils/lfs.c
+++ lustre/utils/lfs.c
@@ -5324,7 +5324,6 @@ static int lfs_setdirstripe(int argc, char **argv)
 					"%s %s: warning: '--index' deprecated, use '--mdt-index' instead\n",
 					progname, argv[0]);
 #endif
-			lsa.lsa_pattern = LLAPI_LAYOUT_OVERSTRIPING;
 			lsa.lsa_nr_tgts = parse_targets(mdts,
 						sizeof(mdts) / sizeof(__u32),
 						lsa.lsa_nr_tgts, optarg, NULL);
@@ -5334,7 +5333,6 @@ static int lfs_setdirstripe(int argc, char **argv)
 					progname, argv[0], optarg);
 				return CMD_HELP;
 			}
-			lsa.lsa_pattern = 0;
 
 			lsa.lsa_tgts = mdts;
 			if (lsa.lsa_stripe_off == LLAPI_LAYOUT_DEFAULT)
