da2ada921cb8ac9506a751f212dd61838f42e947
James Simmons <uja.ornl@yahoo.com>
2019-06-13 21:52:06 -0700

    LU-11742 test: have libtool execute the test binaries
    With the move to libtools the ability to run all the lustre
    utilities form the source tree was lost. To work around this
    the libtool -no-install flag was used to prevent the creation
    of the libtool wrappers. While this worked to restore the
    source tree sand box development new package breakage is showing.
    This is due to the rpath being hard coded into the utilies when
    -no-install is used and some platforms disable fixed rpaths.
    
    A very similar problem exist for people who want to use gdb to
    debug their projects application. gdb does not work on libtool
    wrappers as well so the recommended approach to this type of
    problem is to use the libtool execute command. This command
    allows the execution of an external non project binary, like
    gdb, with the projects real binary application. Apply this
    approach to the lustre test suite so commands like kill can
    be used to shutdown lustre utilies that are not installed into
    the testing environment.
    
    Lustre-change: https://review.whamcloud.com/33947
    Lustre-commit: f9e5224fbb60bb8b44753b7be10cb06108627f89
    
    Change-Id: I74112f7250f1c43313d868c0edc7c8815d373002
    Signed-off-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Chris Horn <hornc@cray.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/34583
    Tested-by: Jenkins
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Yang Sheng <ys@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanity-hsm.sh
+++ lustre/tests/sanity-hsm.sh
@@ -115,7 +115,6 @@ init_agt_vars() {
 	export HSMTOOL_UPDATE_INTERVAL=${HSMTOOL_UPDATE_INTERVAL:=""}
 	export HSMTOOL_EVENT_FIFO=${HSMTOOL_EVENT_FIFO:=""}
 	export HSMTOOL_TESTDIR
-	export HSMTOOL_BASE=$(basename "$HSMTOOL" | cut -f1 -d" ")
 
 	HSM_ARCHIVE_NUMBER=2
 
@@ -151,14 +150,14 @@ get_mdt_devices() {
 
 search_copytools() {
 	local hosts=${1:-$(facet_active_host $SINGLEAGT)}
-	do_nodesv $hosts "pgrep -x $HSMTOOL_BASE"
+	do_nodesv $hosts "libtool execute pgrep -x $HSMTOOL"
 }
 
 kill_copytools() {
 	local hosts=${1:-$(facet_active_host $SINGLEAGT)}
 
 	echo "Killing existing copytools on $hosts"
-	do_nodesv $hosts "killall -q $HSMTOOL_BASE" || true
+	do_nodesv $hosts "libtool execute killall -q $HSMTOOL" || true
 	copytool_continue "$hosts"
 }
 
@@ -289,7 +288,7 @@ __lhsmtool_setup()
 	cmd+=" \"$mountpoint\""
 
 	echo "Starting copytool $facet on $(facet_host $facet)"
-	stack_trap "do_facet $facet pkill -x '$HSMTOOL_BASE' || true" EXIT
+	stack_trap "do_facet $facet libtool execute pkill -x '$HSMTOOL' || true" EXIT
 	do_facet $facet "$cmd < /dev/null > \"$(copytool_logfile $facet)\" 2>&1"
 }
 
@@ -401,15 +400,15 @@ copytool_suspend() {
 	local agents=${1:-$(facet_active_host $SINGLEAGT)}
 
 	stack_trap \
-		"do_nodesv $agents pkill -CONT -x '$HSMTOOL_BASE' || true" EXIT
-	do_nodesv $agents "pkill -STOP -x $HSMTOOL_BASE" || return 0
+		"do_nodesv $agents libtool execute pkill -CONT -x '$HSMTOOL' || true" EXIT
+	do_nodesv $agents "libtool execute pkill -STOP -x $HSMTOOL" || return 0
 	echo "Copytool is suspended on $agents"
 }
 
 copytool_continue() {
 	local agents=${1:-$(facet_active_host $SINGLEAGT)}
 
-	do_nodesv $agents "pkill -CONT -x $HSMTOOL_BASE" || return 0
+	do_nodesv $agents "libtool execute pkill -CONT -x $HSMTOOL" || return 0
 	echo "Copytool is continued on $agents"
 }
 
@@ -854,7 +853,7 @@ get_agent_uuid() {
 
 	# Lustre mount-point is mandatory and last parameter on
 	# copytool cmd-line.
-	local mntpnt=$(do_rpc_nodes $agent ps -C $HSMTOOL_BASE -o args= |
+	local mntpnt=$(do_rpc_nodes $agent libtool execute ps -C $HSMTOOL -o args= |
 		       awk '{print $NF}')
 	[ -n "$mntpnt" ] || error "Found no Agent or with no mount-point "\
 				  "parameter"
