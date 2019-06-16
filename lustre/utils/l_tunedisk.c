lustre/lustre-release

commit 892280742a2b6347df1464379b3ed223b2961ed4

LU-9551 utils: add l_tunedisk to fix disk tunings
This adds l_tunedisk utility to utilize osd_tune_lustre call for
mount_utils.h.  This can be called from udev.
This adds a udev rule to fix disk tunings.
This in some ways duplicates LU-9132, which sets this value at mount
time, but if a multipath component is removed then re-added, the
multipath's max_sectors_kb will not propgate to the newly added device
and this now will cause an error for I/Os that would violate this.

Test-Parameters: trivial
Signed-off-by: Nathaniel Clark <nathaniel.l.clark@intel.com>
Change-Id: I35330ebe75552d71b71212f9fae00cfdcc028ea1
Reviewed-on: https://review.whamcloud.com/31464
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Bob Glossman <bob.glossman@intel.com>

lustre/utils/l_tunedisk.c
@@ -1,0 +1,81 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2018, Intel Corporation.
+ */
+
+
+#if HAVE_CONFIG_H
+#  include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mount_utils.h"
+int	verbose;
+char	*progname;
+
+
+int main(int argc, char *const argv[])
+{
+	struct mount_opts mop = {
+		.mo_max_sectors_kb = -1
+	};
+	char real_path[PATH_MAX] = {'\0'};
+	unsigned int mount_type;
+	int ret;
+
+	verbose = 0;
+	progname = strrchr(argv[0], '/');
+	progname = progname ? progname + 1 : argv[0];
+
+	ret = osd_init();
+	if (ret != 0) {
+		vprint("%s: osd_init() failed to initialize: %d\n",
+		       progname, ret);
+		return ret;
+	}
+
+	/* device is last arg */
+	mop.mo_usource = argv[argc - 1];
+
+	mop.mo_source = realpath(mop.mo_usource, real_path);
+	if (mop.mo_source == NULL) {
+		vprint("%s: No realpath for %s\n", progname, mop.mo_usource);
+		goto out;
+	}
+
+	/* Check whether the disk has already been formatted by mkfs.lustre */
+	ret = osd_is_lustre(mop.mo_source, &mount_type);
+	if (ret == 0)
+		goto out;
+
+	ret = osd_tune_lustre(mop.mo_source, &mop);
+
+out:
+	osd_fini();
+	return ret;
+}

lustre/conf/99-lustre.rules
@@ -1,3 +1,7 @@
 KERNEL=="obd", MODE="0666"
+
+# Ensure block devices re-added to the system allow for large writes (LU-9551)
+ACTION=="add|change", SUBSYSTEM=="block", RUN+="/usr/sbin/l_tunedisk /dev/%k"
+
 # set sysfs values on client
 SUBSYSTEM=="lustre", ACTION=="change", ENV{PARAM}=="?*", RUN+="/usr/sbin/lctl set_param $env{PARAM}=$env{SETTING}"

lustre/utils/.gitignore
@@ -24,3 +24,4 @@
 /ll_decode_linkea
 /lhsmd_posix
 /lhsmtool_posix
+/l_tunedisk

lustre/utils/Makefile.am
@@ -34,7 +34,7 @@ endif # TESTS
 
 if SERVER
 sbin_PROGRAMS += mkfs.lustre tunefs.lustre llverdev lr_reader lshowmount \
-		 ll_decode_filter_fid llog_reader
+		 ll_decode_filter_fid llog_reader l_tunedisk
 endif
 if LIBPTHREAD
 sbin_PROGRAMS += lhsmtool_posix
@@ -187,6 +187,11 @@ tunefs_lustre_SOURCES = mkfs_lustre.c mount_utils.c mount_utils.h $(GSSSRC)
 tunefs_lustre_CPPFLAGS := -DTUNEFS ${MNTMODCFLAGS}
 tunefs_lustre_LDFLAGS  := ${MNTMODLDFLAGS}
 tunefs_lustre_LDADD    := $(mkfs_lustre_LDADD)
+
+l_tunedisk_SOURCES = l_tunedisk.c mount_utils.c mount_utils.h $(GSSSRC)
+l_tunedisk_CPPFLAGS := ${MNTMODCFLAGS}
+l_tunedisk_LDFLAGS  := ${MNTMODLDFLAGS}
+l_tunedisk_LDADD    := $(mount_lustre_LDADD)
 
 l_getidentity_SOURCES = l_getidentity.c
 l_getidentity_LDADD := $(top_builddir)/libcfs/libcfs/libcfs.la

commit 9cb4f810164efa5f058dc7605fb1835ea51b0a92

LU-12387 utils: Read existing ldd data in l_tunedisk
Read the lustre_disk_data from the device passed to l_tunedisk, so
we can determine whether the device is an MGT or MDT and thus skip
the tuning of the device.

Fixes: 892280742a2b ("LU-9551 utils: add l_tunedisk to fix disk tunings")
Fixes: 2f8d7b4679de ("LU-11736 utils: don't set max_sectors_kb on MDT/MGT")
Cray-bug-id: LUS-7358
Signed-off-by: Chris Horn <hornc@cray.com>
Change-Id: I193fe008d5777b0e83f2be9a500eaffb1d3ca615
Reviewed-on: https://review.whamcloud.com/35066
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Gu Zheng <gzheng@ddn.com>
Reviewed-by: Nathaniel Clark <nclark@whamcloud.com>

lustre/utils/l_tunedisk.c
@@ -44,6 +44,8 @@ int main(int argc, char *const argv[])
 	struct mount_opts mop = {
 		.mo_max_sectors_kb = -1
 	};
+	struct lustre_disk_data *ldd = &mop.mo_ldd;
+
 	char real_path[PATH_MAX] = {'\0'};
 	unsigned int mount_type;
 	int ret;
@@ -72,6 +74,15 @@ int main(int argc, char *const argv[])
 	ret = osd_is_lustre(mop.mo_source, &mount_type);
 	if (ret == 0)
 		goto out;
+
+	ldd->ldd_mount_type = mount_type;
+
+	ret = osd_read_ldd(mop.mo_source, ldd);
+	if (ret != 0) {
+		fprintf(stderr, "Failed to read previous Lustre data from %s "
+			"(%d)\n", mop.mo_source, ret);
+		goto out;
+	}
 
 	ret = osd_tune_lustre(mop.mo_source, &mop);
 