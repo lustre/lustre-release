lustre/lustre-release

commit aa82cc83612dbd4c7d05fc101b98e8660f1373db

LU-12034 obdclass: put all service's env on the list
to be able to lookup by current thread where it's too
complicated to pass env by argument.

this version has stats to see slow/fast lookups. so, in sanity-benchmark
there were 172850 fast lookups (from per-cpu cache) and 27228 slow lookups
(from rhashtable). going to see the ration in autotest's reports.

Fixes: 2339e1b3b690 ("LU-11483 ldlm ofd_lvbo_init() and mdt_lvbo_fill() create env")
Fixes: e02cb40761ff ("LU-11164 ldlm: pass env to lvbo methods")

Change-Id: Ia760e10fa5c68e7a18284e4726d215b330fc0eed
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34566
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/obdclass/lu_object.c
@@ -1882,6 +1882,101 @@ int lu_env_refill_by_tags(struct lu_env *env, __u32 ctags,
 }
 EXPORT_SYMBOL(lu_env_refill_by_tags);
 
+
+struct lu_env_item {
+	struct task_struct *lei_task;	/* rhashtable key */
+	struct rhash_head lei_linkage;
+	struct lu_env *lei_env;
+};
+
+static const struct rhashtable_params lu_env_rhash_params = {
+	.key_len     = sizeof(struct task_struct *),
+	.key_offset  = offsetof(struct lu_env_item, lei_task),
+	.head_offset = offsetof(struct lu_env_item, lei_linkage),
+    };
+
+struct rhashtable lu_env_rhash;
+
+struct lu_env_percpu {
+	struct task_struct *lep_task;
+	struct lu_env *lep_env ____cacheline_aligned_in_smp;
+};
+
+static struct lu_env_percpu lu_env_percpu[NR_CPUS];
+
+int lu_env_add(struct lu_env *env)
+{
+	struct lu_env_item *lei, *old;
+
+	LASSERT(env);
+
+	OBD_ALLOC_PTR(lei);
+	if (!lei)
+		return -ENOMEM;
+
+	lei->lei_task = current;
+	lei->lei_env = env;
+
+	old = rhashtable_lookup_get_insert_fast(&lu_env_rhash,
+						&lei->lei_linkage,
+						lu_env_rhash_params);
+	LASSERT(!old);
+
+	return 0;
+}
+EXPORT_SYMBOL(lu_env_add);
+
+void lu_env_remove(struct lu_env *env)
+{
+	struct lu_env_item *lei;
+	const void *task = current;
+	int i;
+
+	for_each_possible_cpu(i) {
+		if (lu_env_percpu[i].lep_env == env) {
+			LASSERT(lu_env_percpu[i].lep_task == task);
+			lu_env_percpu[i].lep_task = NULL;
+			lu_env_percpu[i].lep_env = NULL;
+		}
+	}
+
+	rcu_read_lock();
+	lei = rhashtable_lookup_fast(&lu_env_rhash, &task,
+				     lu_env_rhash_params);
+	if (lei && rhashtable_remove_fast(&lu_env_rhash, &lei->lei_linkage,
+					  lu_env_rhash_params) == 0)
+		OBD_FREE_PTR(lei);
+	rcu_read_unlock();
+}
+EXPORT_SYMBOL(lu_env_remove);
+
+struct lu_env *lu_env_find(void)
+{
+	struct lu_env *env = NULL;
+	struct lu_env_item *lei;
+	const void *task = current;
+	int i = get_cpu();
+
+	if (lu_env_percpu[i].lep_task == current) {
+		env = lu_env_percpu[i].lep_env;
+		put_cpu();
+		LASSERT(env);
+		return env;
+	}
+
+	lei = rhashtable_lookup_fast(&lu_env_rhash, &task,
+				     lu_env_rhash_params);
+	if (lei) {
+		env = lei->lei_env;
+		lu_env_percpu[i].lep_task = current;
+		lu_env_percpu[i].lep_env = env;
+	}
+	put_cpu();
+
+	return env;
+}
+EXPORT_SYMBOL(lu_env_find);
+
 static struct shrinker *lu_site_shrinker;
 
 typedef struct lu_site_stats{
@@ -2097,7 +2192,7 @@ void lu_context_keys_dump(void)
  */
 int lu_global_init(void)
 {
-        int result;
+	int result;
 	DEF_SHRINKER_VAR(shvar, lu_cache_shrink,
 			 lu_cache_shrink_count, lu_cache_shrink_scan);
 
@@ -2132,6 +2227,8 @@ int lu_global_init(void)
         if (lu_site_shrinker == NULL)
                 return -ENOMEM;
 
+	result = rhashtable_init(&lu_env_rhash, &lu_env_rhash_params);
+
         return result;
 }
 
@@ -2154,6 +2251,8 @@ void lu_global_fini(void)
 	down_write(&lu_sites_guard);
         lu_env_fini(&lu_shrink_env);
 	up_write(&lu_sites_guard);
+
+	rhashtable_destroy(&lu_env_rhash);
 
         lu_ref_global_fini();
 }

lustre/include/lu_object.h
@@ -1250,6 +1250,10 @@ void lu_env_fini  (struct lu_env *env);
 int  lu_env_refill(struct lu_env *env);
 int  lu_env_refill_by_tags(struct lu_env *env, __u32 ctags, __u32 stags);
 
+struct lu_env *lu_env_find(void);
+int lu_env_add(struct lu_env *env);
+void lu_env_remove(struct lu_env *env);
+
 /** @} lu_context */
 
 /**

lustre/include/lustre_dlm.h
@@ -293,16 +293,14 @@ typedef int (*ldlm_cancel_cbt)(struct ldlm_lock *lock);
  * of ldlm_[res_]lvbo_[init,update,fill]() functions.
  */
 struct ldlm_valblock_ops {
-	int (*lvbo_init)(const struct lu_env *env, struct ldlm_resource *res);
-	int (*lvbo_update)(const struct lu_env *env, struct ldlm_resource *res,
-			   struct ldlm_lock *lock, struct ptlrpc_request *r,
-			   int increase);
+	int (*lvbo_init)(struct ldlm_resource *res);
+	int (*lvbo_update)(struct ldlm_resource *res, struct ldlm_lock *lock,
+			   struct ptlrpc_request *r, int increase);
 	int (*lvbo_free)(struct ldlm_resource *res);
 	/* Return size of lvb data appropriate RPC size can be reserved */
 	int (*lvbo_size)(struct ldlm_lock *lock);
 	/* Called to fill in lvb data to RPC buffer @buf */
-	int (*lvbo_fill)(const struct lu_env *env, struct ldlm_lock *lock,
-			 void *buf, int *buflen);
+	int (*lvbo_fill)(struct ldlm_lock *lock, void *buf, int *buflen);
 };
 
 /**
@@ -1089,8 +1087,7 @@ ldlm_lock_to_ns_at(struct ldlm_lock *lock)
         return &lock->l_resource->lr_ns_bucket->nsb_at_estimate;
 }
 
-static inline int ldlm_lvbo_init(const struct lu_env *env,
-				 struct ldlm_resource *res)
+static inline int ldlm_lvbo_init(struct ldlm_resource *res)
 {
 	struct ldlm_namespace *ns = ldlm_res_to_ns(res);
 	int rc = 0;
@@ -1105,7 +1102,7 @@ static inline int ldlm_lvbo_init(const struct lu_env *env,
 		mutex_unlock(&res->lr_lvb_mutex);
 		return 0;
 	}
-	rc = ns->ns_lvbo->lvbo_init(env, res);
+	rc = ns->ns_lvbo->lvbo_init(res);
 	if (rc < 0) {
 		CDEBUG(D_DLMTRACE, "lvbo_init failed for resource : rc = %d\n",
 		       rc);
@@ -1131,8 +1128,7 @@ static inline int ldlm_lvbo_size(struct ldlm_lock *lock)
 	return 0;
 }
 
-static inline int ldlm_lvbo_fill(const struct lu_env *env,
-				 struct ldlm_lock *lock, void *buf, int *len)
+static inline int ldlm_lvbo_fill(struct ldlm_lock *lock, void *buf, int *len)
 {
 	struct ldlm_namespace *ns = ldlm_lock_to_ns(lock);
 	int rc;
@@ -1140,13 +1136,13 @@ static inline int ldlm_lvbo_fill(const struct lu_env *env,
 	if (ns->ns_lvbo != NULL) {
 		LASSERT(ns->ns_lvbo->lvbo_fill != NULL);
 		/* init lvb now if not already */
-		rc = ldlm_lvbo_init(env, lock->l_resource);
+		rc = ldlm_lvbo_init(lock->l_resource);
 		if (rc < 0) {
 			CERROR("lock %p: delayed lvb init failed (rc %d)",
 			       lock, rc);
 			return rc;
 		}
-		return ns->ns_lvbo->lvbo_fill(env, lock, buf, len);
+		return ns->ns_lvbo->lvbo_fill(lock, buf, len);
 	}
 	return 0;
 }
@@ -1408,8 +1404,7 @@ ldlm_handle2lock_long(const struct lustre_handle *h, __u64 flags)
  * Update Lock Value Block Operations (LVBO) on a resource taking into account
  * data from request \a r
  */
-static inline int ldlm_lvbo_update(const struct lu_env *env,
-				   struct ldlm_resource *res,
+static inline int ldlm_lvbo_update(struct ldlm_resource *res,
 				   struct ldlm_lock *lock,
 				   struct ptlrpc_request *req, int increase)
 {
@@ -1417,24 +1412,23 @@ static inline int ldlm_lvbo_update(const struct lu_env *env,
 	int rc;
 
 	/* delayed lvb init may be required */
-	rc = ldlm_lvbo_init(env, res);
+	rc = ldlm_lvbo_init(res);
 	if (rc < 0) {
 		CERROR("delayed lvb init failed (rc %d)\n", rc);
 		return rc;
 	}
 
 	if (ns->ns_lvbo && ns->ns_lvbo->lvbo_update)
-		return ns->ns_lvbo->lvbo_update(env, res, lock, req, increase);
+		return ns->ns_lvbo->lvbo_update(res, lock, req, increase);
 
 	return 0;
 }
 
-static inline int ldlm_res_lvbo_update(const struct lu_env *env,
-				       struct ldlm_resource *res,
+static inline int ldlm_res_lvbo_update(struct ldlm_resource *res,
 				       struct ptlrpc_request *req,
 				       int increase)
 {
-	return ldlm_lvbo_update(env, res, NULL, req, increase);
+	return ldlm_lvbo_update(res, NULL, req, increase);
 }
 
 int ldlm_error2errno(enum ldlm_error error);

lustre/include/obd_class.h
@@ -576,12 +576,19 @@ static inline int obd_precleanup(struct obd_device *obd)
 	ENTRY;
 
 	if (ldt != NULL && d != NULL) {
-		struct lu_env env;
+		struct lu_env *env = lu_env_find();
+		struct lu_env _env;
 
-		rc = lu_env_init(&env, ldt->ldt_ctx_tags);
-		if (rc == 0) {
-			ldt->ldt_ops->ldto_device_fini(&env, d);
-			lu_env_fini(&env);
+		if (!env) {
+			env = &_env;
+			rc = lu_env_init(env, ldt->ldt_ctx_tags);
+			LASSERT(rc == 0);
+			lu_env_add(env);
+		}
+		ldt->ldt_ops->ldto_device_fini(env, d);
+		if (env == &_env) {
+			lu_env_remove(env);
+			lu_env_fini(env);
 		}
 	}
 

lustre/ldlm/ldlm_lib.c
@@ -2470,18 +2470,16 @@ static int target_recovery_thread(void *arg)
         if (thread == NULL)
                 RETURN(-ENOMEM);
 
-        OBD_ALLOC_PTR(env);
-        if (env == NULL) {
-                OBD_FREE_PTR(thread);
-                RETURN(-ENOMEM);
-        }
+	OBD_ALLOC_PTR(env);
+	if (env == NULL)
+		GOTO(out_thread, rc = -ENOMEM);
+	rc = lu_env_add(env);
+	if (rc)
+		GOTO(out_env, rc);
 
         rc = lu_context_init(&env->le_ctx, LCT_MD_THREAD | LCT_DT_THREAD);
-        if (rc) {
-                OBD_FREE_PTR(thread);
-                OBD_FREE_PTR(env);
-                RETURN(rc);
-        }
+	if (rc)
+		GOTO(out_env_remove, rc);
 
 	thread->t_env = env;
 	thread->t_id = -1; /* force filter_iobuf_get/put to use local buffers */
@@ -2572,8 +2570,12 @@ static int target_recovery_thread(void *arg)
 	complete(&trd->trd_finishing);
 
 	tgt_io_thread_done(thread);
-	OBD_FREE_PTR(thread);
+out_env_remove:
+	lu_env_remove(env);
+out_env:
 	OBD_FREE_PTR(env);
+out_thread:
+	OBD_FREE_PTR(thread);
 	RETURN(rc);
 }
 

lustre/ldlm/ldlm_lock.c
@@ -2493,7 +2493,7 @@ static void ldlm_cancel_lock_for_export(struct obd_export *exp,
 
 	res = ldlm_resource_getref(lock->l_resource);
 
-	ldlm_lvbo_update(ecl->ecl_env, res, lock, NULL, 1);
+	ldlm_lvbo_update(res, lock, NULL, 1);
 	ldlm_lock_cancel(lock);
 	if (!exp->exp_obd->obd_stopping)
 		ldlm_reprocess_all(res);

lustre/ldlm/ldlm_lockd.c
@@ -663,8 +663,7 @@ static void ldlm_failed_ast(struct ldlm_lock *lock, int rc,
 /**
  * Perform lock cleanup if AST reply came with error.
  */
-static int ldlm_handle_ast_error(const struct lu_env *env,
-				 struct ldlm_lock *lock,
+static int ldlm_handle_ast_error(struct ldlm_lock *lock,
 				 struct ptlrpc_request *req, int rc,
 				 const char *ast_type)
 {
@@ -726,7 +725,7 @@ static int ldlm_handle_ast_error(const struct lu_env *env,
 			 * see b=23174
 			 */
 			ldlm_resource_getref(res);
-			ldlm_lvbo_update(env, res, lock, NULL, 1);
+			ldlm_lvbo_update(res, lock, NULL, 1);
 			ldlm_resource_putref(res);
 		}
 		ldlm_lock_cancel(lock);
@@ -759,28 +758,25 @@ static int ldlm_cb_interpret(const struct lu_env *env,
 		 *   -ELDLM_NO_LOCK_DATA when inode is cleared. LU-274
 		 */
 		if (unlikely(arg->gl_interpret_reply)) {
-			rc = arg->gl_interpret_reply(env, req, args, rc);
+			rc = arg->gl_interpret_reply(NULL, req, args, rc);
 		} else if (rc == -ELDLM_NO_LOCK_DATA) {
 			LDLM_DEBUG(lock,
 				   "lost race - client has a lock but no inode");
-			ldlm_lvbo_update(env, lock->l_resource, lock, NULL, 1);
+			ldlm_lvbo_update(lock->l_resource, lock, NULL, 1);
 		} else if (rc != 0) {
-			rc = ldlm_handle_ast_error(env, lock, req,
-						   rc, "glimpse");
+			rc = ldlm_handle_ast_error(lock, req, rc, "glimpse");
 		} else {
-			rc = ldlm_lvbo_update(env, lock->l_resource,
+			rc = ldlm_lvbo_update(lock->l_resource,
 					      lock, req, 1);
 		}
 		break;
 	case LDLM_BL_CALLBACK:
 		if (rc != 0)
-			rc = ldlm_handle_ast_error(env, lock, req,
-						   rc, "blocking");
+			rc = ldlm_handle_ast_error(lock, req, rc, "blocking");
 		break;
 	case LDLM_CP_CALLBACK:
 		if (rc != 0)
-			rc = ldlm_handle_ast_error(env, lock, req,
-						   rc, "completion");
+			rc = ldlm_handle_ast_error(lock, req, rc, "completion");
 		break;
 	default:
 		LDLM_ERROR(lock, "invalid opcode for lock callback %d",
@@ -1037,12 +1033,7 @@ int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data)
 	ldlm_lock2desc(lock, &body->lock_desc);
 	if (lvb_len > 0) {
 		void *lvb = req_capsule_client_get(&req->rq_pill, &RMF_DLM_LVB);
-		const struct lu_env *env = NULL;
-
-		if (req->rq_svc_thread)
-			env = req->rq_svc_thread->t_env;
-
-		lvb_len = ldlm_lvbo_fill(env, lock, lvb, &lvb_len);
+		lvb_len = ldlm_lvbo_fill(lock, lvb, &lvb_len);
 		if (lvb_len < 0) {
 			/*
 			 * We still need to send the RPC to wake up the blocked
@@ -1322,7 +1313,7 @@ int ldlm_handle_enqueue0(struct ldlm_namespace *ns,
 	res = lock->l_resource;
 	if (!(flags & LDLM_FL_REPLAY)) {
 		/* non-replayed lock, delayed lvb init may need to be done */
-		rc = ldlm_lvbo_init(env, res);
+		rc = ldlm_lvbo_init(res);
 		if (rc < 0) {
 			LDLM_DEBUG(lock, "delayed lvb init failed (rc %d)", rc);
 			GOTO(out, rc);
@@ -1500,7 +1491,7 @@ retry:
 			if ((buflen > 0) && !(flags & LDLM_FL_REPLAY)) {
 				int rc2;
 
-				rc2 = ldlm_lvbo_fill(env, lock, buf, &buflen);
+				rc2 = ldlm_lvbo_fill(lock, buf, &buflen);
 				if (rc2 >= 0) {
 					req_capsule_shrink(&req->rq_pill,
 							   &RMF_DLM_LVB,
@@ -1682,7 +1673,6 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 			const struct ldlm_request *dlm_req,
 			int first, enum lustre_at_flags flags)
 {
-	const struct lu_env *env = req->rq_svc_thread->t_env;
 	struct ldlm_resource *res, *pres = NULL;
 	struct ldlm_lock *lock;
 	int i, count, done = 0;
@@ -1733,7 +1723,7 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 				LDLM_RESOURCE_ADDREF(res);
 
 				if (!ldlm_is_discard_data(lock))
-					ldlm_lvbo_update(env, res, lock,
+					ldlm_lvbo_update(res, lock,
 							 NULL, 1);
 			}
 			pres = res;
@@ -2826,10 +2816,22 @@ static int ldlm_bl_thread_exports(struct ldlm_bl_pool *blp,
  */
 static int ldlm_bl_thread_main(void *arg)
 {
+	struct lu_env *env;
 	struct ldlm_bl_pool *blp;
 	struct ldlm_bl_thread_data *bltd = arg;
+	int rc;
 
 	ENTRY;
+
+	OBD_ALLOC_PTR(env);
+	if (!env)
+		RETURN(-ENOMEM);
+	rc = lu_env_init(env, LCT_DT_THREAD);
+	if (rc)
+		GOTO(out_env, rc);
+	rc = lu_env_add(env);
+	if (rc)
+		GOTO(out_env_fini, rc);
 
 	blp = bltd->bltd_blp;
 
@@ -2875,7 +2877,13 @@ static int ldlm_bl_thread_main(void *arg)
 
 	atomic_dec(&blp->blp_num_threads);
 	complete(&blp->blp_comp);
-	RETURN(0);
+
+	lu_env_remove(env);
+out_env_fini:
+	lu_env_fini(env);
+out_env:
+	OBD_FREE_PTR(env);
+	RETURN(rc);
 }
 
 

lustre/ldlm/ldlm_request.c
@@ -469,7 +469,7 @@ int ldlm_cli_enqueue_local(const struct lu_env *env,
 	if (IS_ERR(lock))
 		GOTO(out_nolock, err = PTR_ERR(lock));
 
-	err = ldlm_lvbo_init(env, lock->l_resource);
+	err = ldlm_lvbo_init(lock->l_resource);
 	if (err < 0) {
 		LDLM_ERROR(lock, "delayed lvb init failed (rc %d)", err);
 		ldlm_lock_destroy_nolock(lock);

commit 87306c22e4b977356f4857d5f750447639d89c26

LU-12491 obdclass: use RCU to release lu_env_item
as rhashtable_lookup_fast() is lockless and can
find just released objects.

Fixes: aa82cc8361 ("obdclass: put all service's env on the list")
Change-Id: I6ed8ccc5bb5b192eed90b55103d11b822ec90692
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/35038
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/lu_object.c
@@ -1886,6 +1886,7 @@ struct lu_env_item {
 	struct task_struct *lei_task;	/* rhashtable key */
 	struct rhash_head lei_linkage;
 	struct lu_env *lei_env;
+	struct rcu_head lei_rcu_head;
 };
 
 static const struct rhashtable_params lu_env_rhash_params = {
@@ -1925,6 +1926,14 @@ int lu_env_add(struct lu_env *env)
 }
 EXPORT_SYMBOL(lu_env_add);
 
+static void lu_env_item_free(struct rcu_head *head)
+{
+	struct lu_env_item *lei;
+
+	lei = container_of(head, struct lu_env_item, lei_rcu_head);
+	OBD_FREE_PTR(lei);
+}
+
 void lu_env_remove(struct lu_env *env)
 {
 	struct lu_env_item *lei;
@@ -1939,13 +1948,11 @@ void lu_env_remove(struct lu_env *env)
 		}
 	}
 
-	rcu_read_lock();
 	lei = rhashtable_lookup_fast(&lu_env_rhash, &task,
 				     lu_env_rhash_params);
 	if (lei && rhashtable_remove_fast(&lu_env_rhash, &lei->lei_linkage,
 					  lu_env_rhash_params) == 0)
-		OBD_FREE_PTR(lei);
-	rcu_read_unlock();
+		call_rcu(&lei->lei_rcu_head, lu_env_item_free);
 }
 EXPORT_SYMBOL(lu_env_remove);
 