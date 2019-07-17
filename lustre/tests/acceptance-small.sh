ca3c77b8424961e82db642c748e1078ae07e9d97
Patrick Farrell <pfarrell@whamcloud.com>
2019-07-17 15:59:02 -0400

    LU-12560 tests: Use full path for test-groups
    Acceptance-small doesn't use the full path when accessing
    the test-groups directory, so it will only run when
    executed from the directory containing test-groups.
    
    Correct it to use the full path, so it can be run from any
    directory.
    
    Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
    Change-Id: I788b2993e8b9a5e83e18940aae503fb21ade9cdc
    Reviewed-on: https://review.whamcloud.com/35541
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Nunez <jnunez@whamcloud.com>

--- lustre/tests/acceptance-small.sh
+++ lustre/tests/acceptance-small.sh
@@ -4,7 +4,7 @@
 #	if no tests are specified, they are taken from test-groups/regression
 #	if {TEST_NAME}=no is set, that test script is skipped
 DEFAULT_SUITES="${@:-$ACC_SM_ONLY}"
-DEFAULT_SUITES="${DEFAULT_SUITES:-$(cat test-groups/regression)}"
+DEFAULT_SUITES="${DEFAULT_SUITES:-$(cat $LUSTRE/tests/test-groups/regression)}"
 for SUB in $DEFAULT_SUITES; do
 	ENV=$(echo $SUB | tr "[:lower:]-" "[:upper:]_")
 	[ "$(eval echo \$$ENV)" = "no" ] && continue
