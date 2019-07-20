lustre/lustre-release

commit 68635c3d9b3113621b93fd989f1a3f8f064385b9

LU-11963 osd: Add nonrotational flag to statfs
It is potentially useful for the MDS and userspace to
know whether or not an OST is using non-rotational media.

Add a flag to obd_statfs that reflects this.

Users can override this parameter in proc.

ZFS does not currently make this information available to
Lustre, so default to rotational and allow users to
override.

Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: Iac2b54c5d8cc1eb79cdace764e93578c7b058661
Reviewed-on: https://review.whamcloud.com/34235
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -132,6 +132,7 @@ enum obd_statfs_state {
 	OS_STATE_ENOSPC		= 0x00000020, /**< not enough free space */
 	OS_STATE_ENOINO		= 0x00000040, /**< not enough inodes */
 	OS_STATE_SUM		= 0x00000100, /**< aggregated for all tagrets */
+	OS_STATE_NONROT		= 0x00000200, /**< non-rotational device */
 };
 
 /** filesystem statistics/attributes for target device */

lustre/utils/lfs.c
@@ -4884,6 +4884,30 @@ static inline int obd_statfs_ratio(const struct obd_statfs *st, bool inodes)
 	return (ratio - (int)ratio) > 0 ? (int)(ratio + 1) : (int)ratio;
 }
 
+/* This is only used to reflect various problem states for lfs df, so we only
+ * translate the flags reflecting those states.
+ */
+static char obd_statfs_state_names[] = {
+	[OS_STATE_DEGRADED]	= 'D',
+	[OS_STATE_READONLY]	= 'R',
+	[OS_STATE_NOPRECREATE]	= 'N',
+	[OS_STATE_ENOSPC]	= 'S',
+	[OS_STATE_ENOINO]	= 'I',
+};
+
+static char obd_statfs_state2char(int s)
+{
+	/* Not an error state, do not print here  */
+	if (s == OS_STATE_NONROT)
+		return '\0';
+	/* Unknown name */
+	if (s > ARRAY_SIZE(obd_statfs_state_names)/sizeof(char) ||
+	    obd_statfs_state_names[s] == 0)
+		return '?';
+
+	return obd_statfs_state_names[s];
+}
+
 static int showdf(char *mntdir, struct obd_statfs *stat,
 		  char *uuid, enum mntdf_flags flags,
 		  char *type, int index, int rc)
@@ -4957,21 +4981,16 @@ static int showdf(char *mntdir, struct obd_statfs *stat,
 			printf("[%s:%d]", type, index);
 
 		if (stat->os_state) {
-			/*
-			 * Each character represents the matching
-			 * OS_STATE_* bit.
-			 */
-			const char state_names[] = "DRSI";
-			__u32	   state;
-			__u32	   i;
+			uint32_t state;
+			uint32_t i;
 
 			printf(" ");
-			for (i = 0, state = stat->os_state;
-			     state && i < sizeof(state_names); i++) {
-				if (!(state & (1 << i)))
+			for (i = 0, state = stat->os_state; state != 0; i++) {
+				uint32_t mask = 1 << i;
+				if (!(state & mask))
 					continue;
-				printf("%c", state_names[i]);
-				state ^= 1 << i;
+				printf("%c", obd_statfs_state2char(mask));
+				state &= ~mask;
 			}
 		}
 

lustre/osd-ldiskfs/osd_handler.c
@@ -2198,6 +2198,9 @@ int osd_statfs(const struct lu_env *env, struct dt_device *d,
 	statfs_pack(sfs, ksfs);
 	if (unlikely(sb->s_flags & MS_RDONLY))
 		sfs->os_state |= OS_STATE_READONLY;
+
+	sfs->os_state |= osd->od_nonrotational ? OS_STATE_NONROT : 0;
+
 	if (ldiskfs_has_feature_extents(sb))
 		sfs->os_maxbytes = sb->s_maxbytes;
 	else
@@ -7757,6 +7760,7 @@ static int osd_device_init0(const struct lu_env *env,
 	o->od_read_cache = 1;
 	o->od_writethrough_cache = 1;
 	o->od_readcache_max_filesize = OSD_MAX_CACHE_SIZE;
+
 	o->od_auto_scrub_interval = AS_DEFAULT;
 
 	cplen = strlcpy(o->od_svname, lustre_cfg_string(cfg, 4),
@@ -7777,6 +7781,10 @@ static int osd_device_init0(const struct lu_env *env,
 	rc = osd_mount(env, o, cfg);
 	if (rc != 0)
 		GOTO(out, rc);
+
+	/* Can only check block device after mount */
+	o->od_nonrotational =
+		blk_queue_nonrot(bdev_get_queue(osd_sb(o)->s_bdev));
 
 	rc = osd_obj_map_init(env, o);
 	if (rc != 0)

lustre/osd-ldiskfs/osd_internal.h
@@ -267,7 +267,9 @@ struct osd_device {
 				  od_check_ff:1,
 				  od_is_ost:1,
 				  od_in_init:1,
-				  od_index_in_idif:1;
+				  od_index_in_idif:1,
+	/* Other flags */
+				  od_nonrotational:1;
 
 	__s64			  od_auto_scrub_interval;
 	__u32			  od_dirent_journal;

lustre/osd-ldiskfs/osd_lproc.c
@@ -353,6 +353,43 @@ ssize_t force_sync_store(struct kobject *kobj, struct attribute *attr,
 }
 LUSTRE_WO_ATTR(force_sync);
 
+static ssize_t nonrotational_show(struct kobject *kobj, struct attribute *attr,
+				  char *buf)
+{
+	struct dt_device *dt = container_of(kobj, struct dt_device,
+					    dd_kobj);
+	struct osd_device *osd = osd_dt_dev(dt);
+
+	LASSERT(osd);
+	if (unlikely(!osd->od_mnt))
+		return -EINPROGRESS;
+
+	return sprintf(buf, "%u\n", osd->od_nonrotational);
+}
+
+static ssize_t nonrotational_store(struct kobject *kobj,
+				   struct attribute *attr, const char *buffer,
+				   size_t count)
+{
+	struct dt_device *dt = container_of(kobj, struct dt_device,
+					    dd_kobj);
+	struct osd_device *osd = osd_dt_dev(dt);
+	bool val;
+	int rc;
+
+	LASSERT(osd);
+	if (unlikely(!osd->od_mnt))
+		return -EINPROGRESS;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	osd->od_nonrotational = val;
+	return count;
+}
+LUSTRE_RW_ATTR(nonrotational);
+
 static ssize_t pdo_show(struct kobject *kobj, struct attribute *attr,
 			char *buf)
 {
@@ -668,6 +705,7 @@ static struct attribute *ldiskfs_attrs[] = {
 	&lustre_attr_fstype.attr,
 	&lustre_attr_mntdev.attr,
 	&lustre_attr_force_sync.attr,
+	&lustre_attr_nonrotational.attr,
 	&lustre_attr_index_backup.attr,
 	&lustre_attr_auto_scrub.attr,
 	&lustre_attr_pdo.attr,

lustre/osd-zfs/osd_handler.c
@@ -554,16 +554,23 @@ static int osd_objset_statfs(struct osd_device *osd, struct obd_statfs *osfs)
 int osd_statfs(const struct lu_env *env, struct dt_device *d,
 	       struct obd_statfs *osfs)
 {
-	int		   rc;
+	struct osd_device *osd = osd_dt_dev(d);
+	int		  rc;
 	ENTRY;
 
-	rc = osd_objset_statfs(osd_dt_dev(d), osfs);
+	rc = osd_objset_statfs(osd, osfs);
 	if (unlikely(rc != 0))
 		RETURN(rc);
 
 	osfs->os_bavail -= min_t(u64,
 				 OSD_GRANT_FOR_LOCAL_OIDS / osfs->os_bsize,
 				 osfs->os_bavail);
+
+	/* ZFS does not support reporting nonrotional status yet, so return
+	 * flag only if user has set nonrotational.
+	 */
+	osfs->os_state |= osd->od_nonrotational ? OS_STATE_NONROT : 0;
+
 	RETURN(0);
 }
 
@@ -1274,6 +1281,11 @@ static int osd_device_init0(const struct lu_env *env,
 	sema_init(&o->od_otable_sem, 1);
 	INIT_LIST_HEAD(&o->od_ios_list);
 	o->od_auto_scrub_interval = AS_DEFAULT;
+
+	/* ZFS does not support reporting nonrotional status yet, so this flag
+	 * is only set if explicitly set by the user.
+	 */
+	o->od_nonrotational = 0;
 
 out:
 	RETURN(rc);

lustre/osd-zfs/osd_internal.h
@@ -347,7 +347,8 @@ struct osd_device {
 				 od_xattr_in_sa:1,
 				 od_is_ost:1,
 				 od_in_init:1,
-				 od_posix_acl:1;
+				 od_posix_acl:1,
+				 od_nonrotational:1;
 	unsigned int		 od_dnsize;
 	int			 od_index_backup_stop;
 

lustre/osd-zfs/osd_lproc.c
@@ -295,6 +295,43 @@ ssize_t force_sync_store(struct kobject *kobj, struct attribute *attr,
 }
 LUSTRE_WO_ATTR(force_sync);
 
+static ssize_t nonrotational_show(struct kobject *kobj, struct attribute *attr,
+				  char *buf)
+{
+	struct dt_device *dt = container_of(kobj, struct dt_device,
+					    dd_kobj);
+	struct osd_device *osd = osd_dt_dev(dt);
+
+	LASSERT(osd);
+	if (!osd->od_os)
+		return -EINPROGRESS;
+
+	return sprintf(buf, "%u\n", osd->od_nonrotational);
+}
+
+static ssize_t nonrotational_store(struct kobject *kobj,
+				   struct attribute *attr, const char *buffer,
+				   size_t count)
+{
+	struct dt_device *dt = container_of(kobj, struct dt_device,
+					    dd_kobj);
+	struct osd_device *osd = osd_dt_dev(dt);
+	bool val;
+	int rc;
+
+	LASSERT(osd);
+	if (!osd->od_os)
+		return -EINPROGRESS;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	osd->od_nonrotational = val;
+	return count;
+}
+LUSTRE_RW_ATTR(nonrotational);
+
 static ssize_t index_backup_show(struct kobject *kobj, struct attribute *attr,
 				 char *buf)
 {
@@ -373,6 +410,7 @@ static struct attribute *zfs_attrs[] = {
 	&lustre_attr_fstype.attr,
 	&lustre_attr_mntdev.attr,
 	&lustre_attr_force_sync.attr,
+	&lustre_attr_nonrotational.attr,
 	&lustre_attr_index_backup.attr,
 	&lustre_attr_auto_scrub.attr,
 	NULL,

commit e4d92a8a08acbdca6634decd4deb9fe5678ad7ba

LU-12501 utils: fix 'lfs df' printing loop
If the OS_STATE_NONROT flag is set for a device, the showdf() state
printing loop will spin endlessly because this bit is not printed,
so it is never cleared from the loop's state mask.

Declaring the obd_statfs_state_names[] array indexed by OS_STATE_*
flags also is problematic because the array will double in size as
new binary flags are added (already OS_STATE_NONROT results in an
array size of 0x200 = 512 entries).  Instead, declare a struct that
is indexed linearly and stores the OS_STATE_* flag in a field,
along with the name and whether the flag indicates a problem state.

The flag printing loop can iterate over the array of flags instead
of the os_state bits, which clarifies the for-loop iteration and is
equally efficient.

This also allows printing informational flags with "lfs df -v" so
that OS_STATE_NONROT and similar flags can be visible to users.

Fixes: 68635c3d9b3 ("LU-11963 osd: Add nonrotational flag to statfs")
Change-Id: Ib62e949ca56d691c4699d5f2d9439c42643ebbe5
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/35456
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -126,9 +126,11 @@ typedef struct stat     lstat_t;
 #define LUSTRE_FIEMAP_FLAGS_COMPAT (FIEMAP_FLAG_SYNC | FIEMAP_FLAG_DEVICE_ORDER)
 
 enum obd_statfs_state {
-	OS_STATE_DEGRADED       = 0x00000001, /**< RAID degraded/rebuilding */
-	OS_STATE_READONLY       = 0x00000002, /**< filesystem is read-only */
-	OS_STATE_NOPRECREATE    = 0x00000004, /**< no object precreation */
+	OS_STATE_DEGRADED	= 0x00000001, /**< RAID degraded/rebuilding */
+	OS_STATE_READONLY	= 0x00000002, /**< filesystem is read-only */
+	OS_STATE_NOPRECREATE	= 0x00000004, /**< no object precreation */
+	OS_STATE_UNUSED1	= 0x00000008, /**< obsolete 1.6, was EROFS=30 */
+	OS_STATE_UNUSED2	= 0x00000010, /**< obsolete 1.6, was EROFS=30 */
 	OS_STATE_ENOSPC		= 0x00000020, /**< not enough free space */
 	OS_STATE_ENOINO		= 0x00000040, /**< not enough inodes */
 	OS_STATE_SUM		= 0x00000100, /**< aggregated for all tagrets */

lustre/utils/lfs.c
@@ -4965,26 +4965,26 @@ static inline int obd_statfs_ratio(const struct obd_statfs *st, bool inodes)
 	return (ratio - (int)ratio) > 0 ? (int)(ratio + 1) : (int)ratio;
 }
 
-/* This is only used to reflect various problem states for lfs df, so we only
- * translate the flags reflecting those states.
+/* This is to identify various problem states for "lfs df" if .osn_err = true,
+ * so only show flags reflecting those states by default. Informational states
+ * are only shown with "-v" and use lower-case names to distinguish them.
+ * UNUSED[12] were for "EROFS = 30" until 1.6 but are now available for use.
  */
-static char obd_statfs_state_names[] = {
-	[OS_STATE_DEGRADED]	= 'D',
-	[OS_STATE_READONLY]	= 'R',
-	[OS_STATE_NOPRECREATE]	= 'N',
-	[OS_STATE_ENOSPC]	= 'S',
-	[OS_STATE_ENOINO]	= 'I',
+static struct obd_statfs_state_names {
+	enum obd_statfs_state	osn_state;
+	const char		osn_name;
+	bool			osn_err;
+} oss_names[] = {
+	{ .osn_state = OS_STATE_DEGRADED,    .osn_name = 'D', .osn_err = true },
+	{ .osn_state = OS_STATE_READONLY,    .osn_name = 'R', .osn_err = true },
+	{ .osn_state = OS_STATE_NOPRECREATE, .osn_name = 'N', .osn_err = true },
+	{ .osn_state = OS_STATE_UNUSED1,     .osn_name = '?', .osn_err = true },
+	{ .osn_state = OS_STATE_UNUSED2,     .osn_name = '?', .osn_err = true },
+	{ .osn_state = OS_STATE_ENOSPC,	     .osn_name = 'S', .osn_err = true },
+	{ .osn_state = OS_STATE_ENOINO,	     .osn_name = 'I', .osn_err = true },
+	{ .osn_state = OS_STATE_SUM,	     .osn_name = 'a', /* aggregate */ },
+	{ .osn_state = OS_STATE_NONROT,      .osn_name = 'f', /* flash */     },
 };
-
-static char obd_statfs_state2char(int s)
-{
-	/* Unknown name */
-	if (s > ARRAY_SIZE(obd_statfs_state_names)/sizeof(char) ||
-	    obd_statfs_state_names[s] == 0)
-		return '?';
-
-	return obd_statfs_state_names[s];
-}
 
 static int showdf(char *mntdir, struct obd_statfs *stat,
 		  char *uuid, enum mntdf_flags flags,
@@ -5059,16 +5059,14 @@ static int showdf(char *mntdir, struct obd_statfs *stat,
 			printf("[%s:%d]", type, index);
 
 		if (stat->os_state) {
-			uint32_t state;
 			uint32_t i;
 
 			printf(" ");
-			for (i = 0, state = stat->os_state; state != 0; i++) {
-				uint32_t mask = 1 << i;
-				if (!(state & mask) || mask == OS_STATE_NONROT)
-					continue;
-				printf("%c", obd_statfs_state2char(mask));
-				state &= ~mask;
+			for (i = 0; i < ARRAY_SIZE(oss_names); i++) {
+				if (oss_names[i].osn_state & stat->os_state &&
+				    (oss_names[i].osn_err ||
+				     flags & MNTDF_VERBOSE))
+					printf("%c", oss_names[i].osn_name);
 			}
 		}
 