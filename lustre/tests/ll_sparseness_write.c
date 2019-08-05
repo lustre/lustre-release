5e53741c6c0f2d0473f23c11c2c9e168df05f429
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 05:17:25 +0530

    LU-6142 tests: Fix style issues for ll_sparseness_write.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/ll_sparseness_write.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: Icf1b9488f75791cd859e975c21341e67b160992c
    Reviewed-on: https://review.whamcloud.com/35821
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/ll_sparseness_write.c
+++ lustre/tests/ll_sparseness_write.c
@@ -41,48 +41,49 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUFSIZE (1024*1024)   
+#define BUFSIZE (1024 * 1024)
 
-/* Function: pwrite character '+' to <filename> at <offset> (man pwrite)
+/*
+ * Function: pwrite character '+' to <filename> at <offset> (man pwrite)
  * Return:   0 success
- *           1 failure */
+ *           1 failure
+ */
 int main(int argc, char**argv)
 {
-        int p_size;
-        unsigned int offset;
-        char *filename;
-        int fd;
-        char buf[] = "+++";
-        char *end;
+	int p_size;
+	unsigned int offset;
+	char *filename;
+	int fd;
+	char buf[] = "+++";
+	char *end;
 
-        if(argc != 3) {
-                fprintf(stderr, "Usage: %s <filename> <offset>(KB)\n", argv[0]);
-                exit(1);
-        }
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <filename> <offset>(KB)\n", argv[0]);
+		exit(1);
+	}
 
-        filename = argv[1];
-        offset = strtoul(argv[2], &end, 10);
-        if (*end) {
-                fprintf(stderr, "<offset> parameter should be integer\n");
-                exit(1);
-        }
+	filename = argv[1];
+	offset = strtoul(argv[2], &end, 10);
+	if (*end) {
+		fprintf(stderr, "<offset> parameter should be integer\n");
+		exit(1);
+	}
 
-        fd = open(filename, O_CREAT|O_RDWR, 0644);
-        if (fd == -1) {
-                fprintf(stderr, "Opening %s fails (%s)\n", 
-                        filename, strerror(errno));
-                return 1;
-        }
+	fd = open(filename, O_CREAT|O_RDWR, 0644);
+	if (fd == -1) {
+		fprintf(stderr, "Opening %s fails (%s)\n",
+			filename, strerror(errno));
+		return 1;
+	}
 
-        /* write the character '+' at offset */
-        p_size = pwrite(fd, buf, 1, offset);
-        if (p_size != 1) {
-                fprintf(stderr, "pwrite %s fails (%s)\n", 
-                        filename, strerror(errno));
-                close(fd);
-                return 1;
-        }
-                
-        close(fd);
-        return 0;
+	/* write the character '+' at offset */
+	p_size = pwrite(fd, buf, 1, offset);
+	if (p_size != 1) {
+		fprintf(stderr, "pwrite %s fails (%s)\n",
+			filename, strerror(errno));
+		close(fd);
+		return 1;
+	}
+	close(fd);
+	return 0;
 }
