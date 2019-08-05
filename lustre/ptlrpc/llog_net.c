0bc376734441a6da0c4f1aced680703da7ff79a0
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 01:48:44 +0530

    LU-6142 ptlrpc: Fix style issues for llog_net.c
    This patch fixes issues reported by checkpatch
    for file lustre/ptlrpc/llog_net.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: Ieee60565e5332a1546999e6c8edaffc833074271
    Reviewed-on: https://review.whamcloud.com/35823
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/ptlrpc/llog_net.c
+++ lustre/ptlrpc/llog_net.c
@@ -48,20 +48,21 @@
 
 int llog_initiator_connect(struct llog_ctxt *ctxt)
 {
-        struct obd_import *new_imp;
-        ENTRY;
+	struct obd_import *new_imp;
 
-        LASSERT(ctxt);
-        new_imp = ctxt->loc_obd->u.cli.cl_import;
-        LASSERTF(ctxt->loc_imp == NULL || ctxt->loc_imp == new_imp,
-                 "%p - %p\n", ctxt->loc_imp, new_imp);
+	ENTRY;
+
+	LASSERT(ctxt);
+	new_imp = ctxt->loc_obd->u.cli.cl_import;
+	LASSERTF(!ctxt->loc_imp || ctxt->loc_imp == new_imp,
+		 "%p - %p\n", ctxt->loc_imp, new_imp);
 	mutex_lock(&ctxt->loc_mutex);
-        if (ctxt->loc_imp != new_imp) {
-                if (ctxt->loc_imp)
-                        class_import_put(ctxt->loc_imp);
-                ctxt->loc_imp = class_import_get(new_imp);
-        }
+	if (ctxt->loc_imp != new_imp) {
+		if (ctxt->loc_imp)
+			class_import_put(ctxt->loc_imp);
+		ctxt->loc_imp = class_import_get(new_imp);
+	}
 	mutex_unlock(&ctxt->loc_mutex);
-        RETURN(0);
+	RETURN(0);
 }
 EXPORT_SYMBOL(llog_initiator_connect);
