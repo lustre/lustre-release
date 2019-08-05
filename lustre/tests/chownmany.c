24e0869af02d3b9cf3da1cd82ae94c2df82bc01b
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 03:25:40 +0530

    LU-6142 tests: Fix style issues for chownmany.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/chownmany.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: I57a89193c683c12881215394fa417ab0aefa004f
    Reviewed-on: https://review.whamcloud.com/35815
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/chownmany.c
+++ lustre/tests/chownmany.c
@@ -44,34 +44,34 @@ void usage(char *prog)
 	printf("       %s owner filenamefmt start count\n", prog);
 }
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
-        int i, rc = 0, mask = 0;
-        char format[4096], *fmt;
-        char filename[4096];
-        long start, last;
+	int i, rc = 0, mask = 0;
+	char format[4096], *fmt;
+	char filename[4096];
+	long start, last;
 	long begin = 0, count;
 
-        if (argc < 4 || argc > 5) {
+	if (argc < 4 || argc > 5) {
 		usage(argv[0]);
-                return 1;
-        }
+		return 1;
+	}
 
-        mask = strtol(argv[1], NULL, 0);
+	mask = strtol(argv[1], NULL, 0);
 
-        if (strlen(argv[2]) > 4080) {
-                printf("name too long\n");
-                return 1;
-        }
+	if (strlen(argv[2]) > 4080) {
+		printf("name too long\n");
+		return 1;
+	}
 
-        start = last = time(0);
+	start = last = time(0);
 
 	if (argc == 4) {
 		count = strtol(argv[3], NULL, 0);
 		if (count < 1) {
-                        printf("count must be at least one\n");
-                        return 1;
-                }
+			printf("count must be at least one\n");
+			return 1;
+		}
 	} else {
 		begin = strtol(argv[3], NULL, 0);
 		count = strtol(argv[4], NULL, 0);
@@ -79,28 +79,27 @@ int main(int argc, char ** argv)
 
 	if (strchr(argv[2], '%')) {
 		fmt = argv[2];
-        } else {
+	} else {
 		sprintf(format, "%s%%d", argv[2]);
 		fmt = format;
 	}
-        for (i = 0; i < count; i++, begin++) {
-                sprintf(filename, fmt, begin);
+	for (i = 0; i < count; i++, begin++) {
+		sprintf(filename, fmt, begin);
 		rc = chown(filename, mask, -1);
-                if (rc) {
-                        printf("chown (%s) error: %s\n",
-                               filename, strerror(errno));
-                        rc = errno;
-                        break;
-                }
-                if ((i % 10000) == 0) {
-                        printf(" - chowned %d (time %ld ; total %ld ; last "
-                               "%ld)\n", i, time(0), time(0) - start,
-                               time(0) - last);
-                        last = time(0);
-                }
-        }
-        printf("total: %d chowns in %ld seconds: %f chowns/second\n", i,
-               time(0) - start, ((float)i / (time(0) - start)));
+		if (rc) {
+			printf("chown (%s) error: %s\n",
+			       filename, strerror(errno));
+			rc = errno;
+			break;
+		}
+		if ((i % 10000) == 0) {
+			printf(" - chowned %d (time %ld ; total %ld ; last %ld)\n", i, time(0), time(0) - start,
+			       time(0) - last);
+			last = time(0);
+		}
+	}
+	printf("total: %d chowns in %ld seconds: %f chowns/second\n", i,
+	       time(0) - start, ((float)i / (time(0) - start)));
 
-        return rc;
+	return rc;
 }
