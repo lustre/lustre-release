7c852b8fd359a81addb8d68cbc8ebcd5e8782a45
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 17:53:42 +0530

    LU-6142 tests: Fix style issues for multifstat.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/multifstat.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: Ief81a0fb80c5863ee92b5e6e602924e8ebdd0031
    Reviewed-on: https://review.whamcloud.com/35905
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/multifstat.c
+++ lustre/tests/multifstat.c
@@ -38,62 +38,61 @@
 
 int main(int argc, char **argv)
 {
-        int fd1, fd2;
-        struct stat st1, st2;
+	int fd1, fd2;
+	struct stat st1, st2;
 
-        if (argc != 3) {
-                printf("Usage %s file1 file2\n", argv[0]);
-                return 1;
-        }
+	if (argc != 3) {
+		printf("Usage %s file1 file2\n", argv[0]);
+		return 1;
+	}
 
+	fd1 = open(argv[1], O_CREAT | O_RDWR, 0666);
+	if (fd1 == -1) {
+		printf("Error opening %s: %s\n", argv[1], strerror(errno));
+		return errno;
+	}
 
-        fd1 = open(argv[1], O_CREAT| O_RDWR, 0666);
-        if (fd1 == -1) {
-                printf("Error opening %s: %s\n", argv[1], strerror(errno));
-                return errno;
-        }
+	fd2 = open(argv[2], O_RDONLY);
+	if (fd2 == -1) {
+		printf("Error opening %s: %s\n", argv[2], strerror(errno));
+		return errno;
+	}
 
-        fd2 = open(argv[2], O_RDONLY);
-        if (fd2 == -1) {
-                printf("Error opening %s: %s\n", argv[2], strerror(errno));
-                return errno;
-        }
+	sleep(1);
 
-        sleep(1);
+	if (write(fd1, "hello", strlen("hello")) != strlen("hello")) {
+		printf("Error writing: %s\n", strerror(errno));
+		return errno;
+	}
 
-        if ( write(fd1, "hello", strlen("hello")) != strlen("hello")) {
-                printf("Error writing: %s\n", strerror(errno));
-                return errno;
-        }
+	if (fstat(fd1, &st1)) {
+		printf("Error statting %s: %s\n", argv[1], strerror(errno));
+		return errno;
+	}
 
-        if ( fstat(fd1, &st1) ) {
-                printf("Error statting %s: %s\n", argv[1], strerror(errno));
-                return errno;
-        }
+	if (fstat(fd2, &st2)) {
+		printf("Error statting %s: %s\n", argv[2], strerror(errno));
+		return errno;
+	}
 
-        if ( fstat(fd2, &st2) ) {
-                printf("Error statting %s: %s\n", argv[2], strerror(errno));
-                return errno;
-        }
-
-        if ( st1.st_size != st2.st_size ) {
-                printf("Sizes don't match %lu, %lu\n",
-                       (unsigned long)st1.st_size,
+	if (st1.st_size != st2.st_size) {
+		printf("Sizes don't match %lu, %lu\n",
+		       (unsigned long)st1.st_size,
 		       (unsigned long)st2.st_size);
-                return 1;
-        }
+		return 1;
+	}
 
-        if ( st1.st_mtime != st2.st_mtime ) {
-                printf("Mtimes don't match %ld, %ld\n",
-                       st1.st_mtime, st2.st_mtime);
-                return 1;
-        }
+	if (st1.st_mtime != st2.st_mtime) {
+		printf("Mtimes don't match %ld, %ld\n",
+		       st1.st_mtime, st2.st_mtime);
+		return 1;
+	}
 
-        if ( st1.st_blocks != st2.st_blocks ) {
-                printf("Blocks don't match %ld, %ld\n",
-                       (long)st1.st_blocks, (long)st2.st_blocks);
-                return 1;
-        }
+	if (st1.st_blocks != st2.st_blocks) {
+		printf("Blocks don't match %ld, %ld\n",
+		       (long)st1.st_blocks, (long)st2.st_blocks);
+		return 1;
+	}
 
-        return 0;
+	return 0;
 }
