0732254bc8b7351f1e168c04d5124f16bc762c64
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 18:35:51 +0530

    LU-6142 tests: Fix style issues for opendirunlink.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/opendirunlink.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: I245b37c93e53313c96a5f69d11d41bc93b3844e2
    Reviewed-on: https://review.whamcloud.com/35909
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/opendirunlink.c
+++ lustre/tests/opendirunlink.c
@@ -45,106 +45,95 @@
 
 int main(int argc, char **argv)
 {
-        char *dname1, *dname2;
-        int fddir1, fddir2, rc;
-        //DIR *dp;
-        struct stat st1, st2;
+	char *dname1, *dname2;
+	int fddir1, fddir2, rc;
+	struct stat st1, st2;
 
-        if (argc < 2 || argc > 3) {
-                fprintf(stderr, "usage: %s dirname1 [dirname2]\n", argv[0]);
-                exit(1);
-        }
+	if (argc < 2 || argc > 3) {
+		fprintf(stderr, "usage: %s dirname1 [dirname2]\n", argv[0]);
+		exit(1);
+	}
 
-        dname1 = argv[1];
-        if (argc == 3)
-                dname2 = argv[2];
-        else
-                dname2 = argv[1];
+	dname1 = argv[1];
+	if (argc == 3)
+		dname2 = argv[2];
+	else
+		dname2 = argv[1];
 
-        //create the directory
-        fprintf(stderr, "creating directory %s\n", dname1);
-        rc = mkdir(dname1, 0744);
-        if (rc == -1) {
-                fprintf(stderr, "creating %s fails: %s\n", 
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* create the directory */
+	fprintf(stderr, "creating directory %s\n", dname1);
+	rc = mkdir(dname1, 0744);
+	if (rc == -1) {
+		fprintf(stderr, "creating %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        // open the dir again
-        fprintf(stderr, "opening directory\n");
-        fddir1 = open(dname1, O_RDONLY | O_DIRECTORY);
-        if (fddir1 == -1) {
-                fprintf(stderr, "open %s fails: %s\n",
-                        dname1, strerror(errno));
-                exit(2);
-        }
-        
-        // doesn't matter if the two dirs are the same??
-        fddir2 = open(dname2, O_RDONLY | O_DIRECTORY);
-        if (fddir2 == -1) {
-                fprintf(stderr, "open %s fails: %s\n",
-                        dname2, strerror(errno));
-                exit(3);
-        }
-        
-        // another method
-/*        
-        if ( (dp = opendir(dname2)) == NULL) {
-                fprintf(stderr, "opendir() %s\n", strerror(errno));
-                exit(1);
-        }
-        fddir = dirfd(dp);
-*/
+	/* open the dir again */
+	fprintf(stderr, "opening directory\n");
+	fddir1 = open(dname1, O_RDONLY | O_DIRECTORY);
+	if (fddir1 == -1) {
+		fprintf(stderr, "open %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(2);
+	}
 
-        // delete the dir
-        fprintf (stderr, "unlinking %s\n", dname1);
-        rc = rmdir(dname1);
-        if (rc) {
-                fprintf(stderr, "unlink %s error: %s\n", 
-                        dname1, strerror(errno));
-                exit(4);
-        }
+	/* doesn't matter if the two dirs are the same?? */
+	fddir2 = open(dname2, O_RDONLY | O_DIRECTORY);
+	if (fddir2 == -1) {
+		fprintf(stderr, "open %s fails: %s\n",
+			dname2, strerror(errno));
+		exit(3);
+	}
 
-        if (access(dname2, F_OK) == 0){
-                fprintf(stderr, "%s still exists\n", dname2);
-                exit(5);
-        }
+	/* delete the dir */
+	fprintf(stderr, "unlinking %s\n", dname1);
+	rc = rmdir(dname1);
+	if (rc) {
+		fprintf(stderr, "unlink %s error: %s\n",
+			dname1, strerror(errno));
+		exit(4);
+	}
 
-        if (access(dname1, F_OK) == 0){
-                fprintf(stderr, "%s still exists\n", dname1);
-                exit(6);
-        }
+	if (access(dname2, F_OK) == 0) {
+		fprintf(stderr, "%s still exists\n", dname2);
+		exit(5);
+	}
 
-        // fchmod the dir
-        rc = fchmod (fddir1, 0777);
-        if(rc == -1)
-        {
-                fprintf(stderr, "fchmod unlinked dir fails %s\n", 
-                        strerror(errno));
-                exit(7);
-        }
-                
-        // fstat two dirs to check if they are the same
-        rc = fstat(fddir1, &st1);
-        if(rc == -1)
-        {
-                fprintf(stderr, "fstat unlinked dir %s fails %s\n", 
-                        dname1, strerror(errno));
-                exit(8);
-        }
+	if (access(dname1, F_OK) == 0) {
+		fprintf(stderr, "%s still exists\n", dname1);
+		exit(6);
+	}
 
-        rc = fstat(fddir2, &st2);
-        if (rc == -1) {
-                fprintf(stderr, "fstat dir %s fails %s\n",
-                        dname2, strerror(errno));
-                exit(9);
-        }
+	/* fchmod the dir */
+	rc = fchmod(fddir1, 0777);
+	if (rc == -1) {
+		fprintf(stderr, "fchmod unlinked dir fails %s\n",
+			strerror(errno));
+		exit(7);
+	}
 
-        if (st1.st_mode != st2.st_mode) {  // can we do this?
-                fprintf(stderr, "fstat different value on %s and %s\n",                                 dname1, dname2);
-                exit(10);
-        }        
+	/* fstat two dirs to check if they are the same */
+	rc = fstat(fddir1, &st1);
+	if (rc == -1) {
+		fprintf(stderr, "fstat unlinked dir %s fails %s\n",
+			dname1, strerror(errno));
+		exit(8);
+	}
 
-        fprintf(stderr, "Ok, everything goes well.\n");
-        return 0;
+	rc = fstat(fddir2, &st2);
+	if (rc == -1) {
+		fprintf(stderr, "fstat dir %s fails %s\n",
+			dname2, strerror(errno));
+		exit(9);
+	}
+
+	if (st1.st_mode != st2.st_mode) {  /* can we do this? */
+		fprintf(stderr, "fstat different value on %s and %s\n",
+			dname1, dname2);
+		exit(10);
+	}
+
+	fprintf(stderr, "Ok, everything goes well.\n");
+	return 0;
 }
