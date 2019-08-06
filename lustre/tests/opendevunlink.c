4e86479d2ae7f5a966d0fe96735f91e0b8ec3032
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 18:19:59 +0530

    LU-6142 tests: Fix style issues for opendevunlink.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/opendevunlink.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: I2f488b77610429e7c17ac3975a66aab271b27409
    Reviewed-on: https://review.whamcloud.com/35910
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/opendevunlink.c
+++ lustre/tests/opendevunlink.c
@@ -44,90 +44,89 @@
 
 int main(int argc, char **argv)
 {
-        char *dname1, *dname2;
-        int fddev1, fddev2, rc;
-        //DIR *dp;
-        struct stat st1, st2;
+	char *dname1, *dname2;
+	int fddev1, fddev2, rc;
+	struct stat st1, st2;
 
-        if (argc < 2 || argc > 3) {
-                fprintf(stderr, "usage: %s filename1 [filename2]\n", argv[0]);
-                exit(1);
-        }
+	if (argc < 2 || argc > 3) {
+		fprintf(stderr, "usage: %s filename1 [filename2]\n", argv[0]);
+		exit(1);
+	}
 
-        dname1 = argv[1];
-        if (argc == 3)
-                dname2 = argv[2];
-        else
-                dname2 = argv[1];
+	dname1 = argv[1];
+	if (argc == 3)
+		dname2 = argv[2];
+	else
+		dname2 = argv[1];
 
-        //create the special file (right now only test on pipe)
-        fprintf(stderr, "creating special file %s\n", dname1);
-        rc = mknod(dname1, 0777|S_IFIFO, 0);
-        if (rc == -1) {
-                fprintf(stderr, "creating %s fails: %s\n",
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* create the special file (right now only test on pipe) */
+	fprintf(stderr, "creating special file %s\n", dname1);
+	rc = mknod(dname1, 0777 | S_IFIFO, 0);
+	if (rc == -1) {
+		fprintf(stderr, "creating %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        // open the special file again
-        fprintf(stderr, "opening file\n");
-        fddev1 = open(dname1, O_RDONLY | O_NONBLOCK);
-        if (fddev1 == -1) {
-                fprintf(stderr, "open %s fails: %s\n",
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* open the special file again */
+	fprintf(stderr, "opening file\n");
+	fddev1 = open(dname1, O_RDONLY | O_NONBLOCK);
+	if (fddev1 == -1) {
+		fprintf(stderr, "open %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        // doesn't matter if the two dirs are the same??
-        fddev2 = open(dname2, O_RDONLY | O_NONBLOCK);
-        if (fddev2 == -1) {
-                fprintf(stderr, "open %s fails: %s\n",
-                        dname2, strerror(errno));
-                exit(1);
-        }
+	/* doesn't matter if the two dirs are the same?? */
+	fddev2 = open(dname2, O_RDONLY | O_NONBLOCK);
+	if (fddev2 == -1) {
+		fprintf(stderr, "open %s fails: %s\n",
+			dname2, strerror(errno));
+		exit(1);
+	}
 
-        // delete the special file
-        fprintf (stderr, "unlinking %s\n", dname1);
-        rc = unlink(dname1);
-        if (rc) {
-                fprintf(stderr, "unlink %s error: %s\n",
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* delete the special file */
+	fprintf(stderr, "unlinking %s\n", dname1);
+	rc = unlink(dname1);
+	if (rc) {
+		fprintf(stderr, "unlink %s error: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        if (access(dname2, F_OK) == 0) {
-                fprintf(stderr, "%s still exists\n", dname2);
-                exit(1);
-        }
+	if (access(dname2, F_OK) == 0) {
+		fprintf(stderr, "%s still exists\n", dname2);
+		exit(1);
+	}
 
-        if (access(dname1, F_OK) == 0) {
-                fprintf(stderr, "%s still exists\n", dname1);
-                exit(1);
-        }
+	if (access(dname1, F_OK) == 0) {
+		fprintf(stderr, "%s still exists\n", dname1);
+		exit(1);
+	}
 
-        // fchmod one special file
-        rc = fchmod (fddev1, 0777);
-        if (rc == -1) {
-                fprintf(stderr, "fchmod unlinked special file %s fails: %s\n",
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* fchmod one special file */
+	rc = fchmod(fddev1, 0777);
+	if (rc == -1) {
+		fprintf(stderr, "fchmod unlinked special file %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        // fstat two files to check if they are the same
-        rc = fstat(fddev1, &st1);
-        if (rc == -1) {
-                fprintf(stderr, "fstat unlinked special file %s fails: %s\n",
-                        dname1, strerror(errno));
-                exit(1);
-        }
+	/* fstat two files to check if they are the same */
+	rc = fstat(fddev1, &st1);
+	if (rc == -1) {
+		fprintf(stderr, "fstat unlinked special file %s fails: %s\n",
+			dname1, strerror(errno));
+		exit(1);
+	}
 
-        rc = fstat(fddev2, &st2);
-        if (rc == -1) {
-                fprintf(stderr, "fstat file %s fails: %s\n",
-                        dname2, strerror(errno));
-                exit(1);
-        }
+	rc = fstat(fddev2, &st2);
+	if (rc == -1) {
+		fprintf(stderr, "fstat file %s fails: %s\n",
+			dname2, strerror(errno));
+		exit(1);
+	}
 
-        fprintf(stderr, "Ok, everything goes well.\n");
-        return 0;
+	fprintf(stderr, "Ok, everything goes well.\n");
+	return 0;
 }
