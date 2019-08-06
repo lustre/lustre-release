fe2300b2fb91cc321412a6e53335848741ea30cd
Arshad Hussain <arshad.super@gmail.com>
2019-08-06 15:59:11 +0530

    LU-6142 tests: Fix style issues for mkdirmany.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/mkdirmany.c
    
    Test-Parameters: trivial
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: I79a86ddf661c3149b1de67a29b329809dd87fbec
    Reviewed-on: https://review.whamcloud.com/35903
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Reviewed-by: Ben Evans <bevans@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/mkdirmany.c
+++ lustre/tests/mkdirmany.c
@@ -40,31 +40,31 @@
 
 int main(int argc, char ** argv)
 {
-        int i, rc = 0, count;
-        char dirname[4096];
+	int i, rc = 0, count;
+	char dirname[4096];
 
-        if (argc < 3) {
-                printf("Usage %s dirnamebase count\n", argv[0]);
-                return 1;
-        }
+	if (argc < 3) {
+		printf("Usage %s dirnamebase count\n", argv[0]);
+		return 1;
+	}
 
-        if (strlen(argv[1]) > 4080) {
-                printf("name too long\n");
-                return 1;
-        }
+	if (strlen(argv[1]) > 4080) {
+		printf("name too long\n");
+		return 1;
+	}
 
-        count = strtoul(argv[2], NULL, 0);
+	count = strtoul(argv[2], NULL, 0);
 
-        for (i = 0; i < count; i++) {
-                sprintf(dirname, "%s-%d", argv[1], i);
-                rc = mkdir(dirname, 0444);
-                if (rc) {
-                        printf("mkdir(%s) error: %s\n",
-                               dirname, strerror(errno));
-                        break;
-                }
+	for (i = 0; i < count; i++) {
+		sprintf(dirname, "%s-%d", argv[1], i);
+		rc = mkdir(dirname, 0444);
+		if (rc) {
+			printf("mkdir(%s) error: %s\n",
+			       dirname, strerror(errno));
+			break;
+		}
 		if ((i % 10000) == 0)
 		    printf(" - created %d (time %ld)\n", i, time(0));
-        }
-        return rc;
+	}
+	return rc;
 }
