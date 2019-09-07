lustre/lustre-release

commit 6bc3237477069c8ddc70deee6e0d7cbbbdf92a08

LU-9897 build: use pkgconf for detecting libblkid
Currently we have extra complexity to determine where packages are
installed. This approach is also fragile if packages are not
installed in the standard location. This can be handled in a much
cleaner way with pkgconfig. Use pkgconf to discover libblkid.

Test-Parameters: trivial

Change-Id: I5241c2c38b1114d8f061ee5f2a4cb0c677ce2c71
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/35531
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Nathaniel Clark <nclark@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -3261,7 +3261,9 @@ AC_CHECK_HEADERS([netdb.h endian.h])
 AC_CHECK_FUNCS([gethostbyname])
 
 # lustre/utils/llverdev.c
-AC_CHECK_HEADERS([blkid/blkid.h])
+AS_IF([test "x$enable_dist" = xno], [
+	PKG_CHECK_MODULES(blkid, [blkid])
+])
 
 # lustre/utils/llverfs.c
 AC_CHECK_HEADERS([ext2fs/ext2fs.h])
@@ -3278,6 +3280,10 @@ AS_IF([test "$enable_dist" = "no"], [
 ])
 
 SELINUX=""
+AS_IF([test "x$enable_dist" = xno], [
+	PKG_CHECK_MODULES(blkid, [blkid])
+])
+
 AC_CHECK_LIB([selinux], [is_selinux_enabled],
 	[AC_CHECK_HEADERS([selinux/selinux.h],
 			[SELINUX="-lselinux"
@@ -3373,7 +3379,6 @@ AM_CONDITIONAL(MPITESTS, test x$enable_mpitests = xyes, Build MPI Tests)
 AM_CONDITIONAL(CLIENT, test x$enable_client = xyes)
 AM_CONDITIONAL(SERVER, test x$enable_server = xyes)
 AM_CONDITIONAL(SPLIT, test x$enable_split = xyes)
-AM_CONDITIONAL(BLKID, test x$ac_cv_header_blkid_blkid_h = xyes)
 AM_CONDITIONAL(EXT2FS_DEVEL, test x$ac_cv_header_ext2fs_ext2fs_h = xyes)
 AM_CONDITIONAL(GSS, test x$enable_gss = xyes)
 AM_CONDITIONAL(GSS_KEYRING, test x$enable_gss_keyring = xyes)

lustre/utils/Makefile.am
@@ -89,15 +89,8 @@ E2PLIB =
 EXT2FSLIB =
 endif
 
-if BLKID
-BLKIDLIB = -lblkid -luuid
-else
-BLKIDLIB =
-endif
-
 llverfs_LDADD := $(EXT2FSLIB) $(E2PLIB)
-
-llverdev_LDADD := $(EXT2FSLIB) $(BLKIDLIB)
+llverdev_LDADD := $(EXT2FSLIB) $(blkid_LIBS)
 
 liblustreapi_la_SOURCES = liblustreapi.c liblustreapi_hsm.c \
 			  liblustreapi_nodemap.c lustreapi_internal.h \

lustre.spec.in
@@ -146,13 +146,18 @@ BuildRoot: %{_tmppath}/lustre-%{version}-root
 Requires: %{requires_kmod_name} = %{requires_kmod_version} zlib
 Requires: %{requires_yaml_name}
 BuildRequires: libtool libyaml-devel zlib-devel
+%if %{_vendor}=="redhat"
+BuildRequires: pkgconfig
+%else
+BuildRequires: pkg-config
+%endif
 %if %{with servers}
 Requires: lustre-osd
 Requires: lustre-osd-mount
 Obsoletes: lustre-client < %{version}
 Provides: lustre-client = %{version}-%{release}
 %endif
-# GSS requires this: BuildRequires: pkgconfig, libgssapi-devel >= 0.10
+# GSS requires this: BuildRequires: libgssapi-devel >= 0.10
 %if %{_vendor}=="redhat" || %{_vendor}=="fedora"
 #suse don't support selinux
 BuildRequires: libselinux-devel

commit fe0e096567ba148d5690316d2bad2226c0cf70bf

LU-9897 utils: remove dependency on libblkid
There was a weak dependency in llverdev on libblkid in order to
find the size of the underlying block device.  However, in the
absence of libblkid there is fallback code that can find the
block device size, so we don't really need libblkid at all.

Similarly, llverdev had a weak dependency on libext2fs to check
if the block device was currently in use, but all kernels since
v2.6.34-rc4-22-g6b4517a7913a allow using open(O_EXCL) to ensure
a block device is not mounted, so there is no need for llverdev
to depend on libext2fs for this.

Remove the conditional code depending on the external libraries
from llverdev and the corresponding configuration checks, which
were causing problems in various build environments.

Test-Parameters: trivial testlist=conf-sanity
Fixes: 6bc323747706 ("LU-9897 build: use pkgconf for detecting libblkid")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I90d2f0c99115ce07168c185dec6628be813ebbe5
Reviewed-on: https://review.whamcloud.com/36010
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -3199,11 +3199,6 @@ LC_MDS_MAX_THREADS
 AC_CHECK_HEADERS([netdb.h endian.h])
 AC_CHECK_FUNCS([gethostbyname])
 
-# lustre/utils/llverdev.c
-AS_IF([test "x$enable_dist" = xno], [
-	PKG_CHECK_MODULES(blkid, [blkid])
-])
-
 # lustre/utils/llverfs.c
 AC_CHECK_HEADERS([ext2fs/ext2fs.h])
 
@@ -3219,9 +3214,6 @@ AS_IF([test "$enable_dist" = "no"], [
 ])
 
 SELINUX=""
-AS_IF([test "x$enable_dist" = xno], [
-	PKG_CHECK_MODULES(blkid, [blkid])
-])
 
 AC_CHECK_LIB([selinux], [is_selinux_enabled],
 	[AC_CHECK_HEADERS([selinux/selinux.h],

lustre/utils/Makefile.am
@@ -90,7 +90,6 @@ EXT2FSLIB =
 endif
 
 llverfs_LDADD := $(EXT2FSLIB) $(E2PLIB)
-llverdev_LDADD := $(EXT2FSLIB) $(blkid_LIBS)
 
 liblustreapi_la_SOURCES = liblustreapi.c liblustreapi_hsm.c \
 			  liblustreapi_nodemap.c lustreapi_internal.h \

lustre/utils/llverdev.c
@@ -84,10 +84,6 @@
 #include <sys/time.h>
 #include <gnu/stubs.h>
 
-#ifdef HAVE_EXT2FS_EXT2FS_H
-#  include <ext2fs/ext2fs.h>
-#endif
-
 #define ONE_MB (1024 * 1024)
 #define ONE_GB (1024 * 1024 * 1024)
 #define HALF_MB (ONE_MB / 2)
@@ -157,23 +153,8 @@ void usage(int status)
  */
 static int open_dev(const char *devname, int mode)
 {
-	int     fd;
-#ifdef HAVE_EXT2FS_EXT2FS_H
-	int     mount_flags;
-	char    mountpt[80] = "";
+	int fd;
 
-	if (ext2fs_check_mount_point(devname, &mount_flags, mountpt,
-				     sizeof(mountpt))) {
-		fprintf(stderr, "%s: ext2fs_check_mount_point failed:%s",
-			progname, strerror(errno));
-		exit(1);
-	}
-	if (mount_flags & EXT2_MF_MOUNTED){
-		fprintf(stderr, "%s: %s is already mounted\n", progname,
-			devname);
-		exit(1);
-	}
-#endif
 	fd = open(devname, mode | O_EXCL | O_LARGEFILE);
 	if (fd < 0) {
 		fprintf(stderr, "%s: Open failed: %s",progname,strerror(errno));
@@ -182,25 +163,13 @@ static int open_dev(const char *devname, int mode)
 	return fd;
 }
 
-#ifdef HAVE_BLKID_BLKID_H
-#include <blkid/blkid.h>
-#endif
 /*
  * sizeof_dev: Returns size of device in bytes
  */
-static loff_t sizeof_dev(int fd)
+static size_t sizeof_dev(int fd)
 {
-	loff_t numbytes;
+	size_t numbytes;
 
-#ifdef HAVE_BLKID_BLKID_H
-	numbytes = blkid_get_dev_size(fd);
-	if (numbytes <= 0) {
-		fprintf(stderr, "%s: blkid_get_dev_size(%s) failed",
-			progname, devname);
-		return 1;
-	}
-	goto out;
-#else
 # if defined BLKGETSIZE64	/* in sys/mount.h */
 	if (ioctl(fd, BLKGETSIZE64, &numbytes) >= 0)
 		goto out;
@@ -226,7 +195,6 @@ static loff_t sizeof_dev(int fd)
 	fprintf(stderr, "%s: unable to determine size of %s\n",
 		progname, devname);
 	return 0;
-#endif
 
 out:
 	if (verbose)
@@ -583,10 +551,10 @@ int main(int argc, char **argv)
 	if (!force && writeoption) {
 		printf("%s: permanently overwrite all data on %s (yes/no)? ",
 		       progname, devname);
-                if (scanf("%3s", yesno) == EOF && ferror(stdin)) {
-                        perror("reading from stdin");
-                        return -1;
-                }
+		if (scanf("%3s", yesno) == EOF && ferror(stdin)) {
+			perror("reading from stdin");
+			return -1;
+		}
 		if (!(strcasecmp("yes", yesno) || strcasecmp("y", yesno))) {
 			printf("Not continuing due to '%s' response", yesno);
 			return 0;