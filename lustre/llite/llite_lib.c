lustre/lustre-release

commit 0c2cc920370e5dcf99e141610020d75082d2898c

LU-9019 libcfs: avoid using HZ and msecs_to_jiffies()
HZ is a constant selected with the configuration of the kernel
and msecs_to_jiffies() is a inline function in jiffies.h. Because
we are out of tree that means prebuilt lustre packages could be
installed on a node with different values which impact the
behavior of the file system. This was addressed earlier but
regression have crept back in. Fix up all those instances by
replacing msec_to_jiffies() and HZ with cfs_time_seconds() which
translates seconds to jiffies with nsec_to_jiffies(). Add to
spelling.txt to avoid new regressions.

Change-Id: Ie13efe83774db498bb5475ed47a057bbd42d47bf
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/35520
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -810,7 +810,7 @@ void ll_kill_super(struct super_block *sb)
 		/* wait running statahead threads to quit */
 		while (atomic_read(&sbi->ll_sa_running) > 0) {
 			set_current_state(TASK_UNINTERRUPTIBLE);
-			schedule_timeout(msecs_to_jiffies(MSEC_PER_SEC >> 3));
+			schedule_timeout(cfs_time_seconds(1 >> 3));
 		}
 	}
 

lustre/mgc/mgc_request.c
@@ -641,9 +641,9 @@ static int mgc_requeue_thread(void *data)
 		/* Always wait a few seconds to allow the server who
 		   caused the lock revocation to finish its setup, plus some
 		   random so everyone doesn't try to reconnect at once. */
-		to = msecs_to_jiffies(MGC_TIMEOUT_MIN_SECONDS * MSEC_PER_SEC);
+		to = cfs_time_seconds(MGC_TIMEOUT_MIN_SECONDS);
 		/* rand is centi-seconds */
-		to += msecs_to_jiffies(rand * MSEC_PER_SEC / 100);
+		to += cfs_time_seconds(rand) / 100;
 		lwi = LWI_TIMEOUT(to, NULL, NULL);
 		l_wait_event(rq_waitq, rq_state & (RQ_STOP | RQ_PRECLEANUP),
 			     &lwi);

libcfs/libcfs/fail.c
@@ -130,7 +130,7 @@ int __cfs_fail_timeout_set(__u32 id, __u32 value, int ms, int set)
 		CERROR("cfs_fail_timeout id %x sleeping for %dms\n", id, ms);
 		while (ktime_before(ktime_get(), till)) {
 			set_current_state(TASK_UNINTERRUPTIBLE);
-			schedule_timeout(msecs_to_jiffies(1000) / 10);
+			schedule_timeout(cfs_time_seconds(1) / 10);
 			set_current_state(TASK_RUNNING);
 			if (!cfs_fail_loc) {
 				CERROR("cfs_fail_timeout interrupted\n");

libcfs/libcfs/linux/linux-crypto.c
@@ -350,7 +350,7 @@ static void cfs_crypto_performance_test(enum cfs_crypto_hash_alg hash_alg)
 	memset(buf, 0xAD, PAGE_SIZE);
 	kunmap(page);
 
-	for (start = jiffies, end = start + msecs_to_jiffies(MSEC_PER_SEC / 4),
+	for (start = jiffies, end = start + cfs_time_seconds(1) / 4,
 	     bcount = 0; time_before(jiffies, end) && err == 0; bcount++) {
 		struct ahash_request *req;
 		int i;

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -3617,7 +3617,7 @@ kiblnd_connd (void *arg)
 					     kiblnd_data.kib_peer_hash_size;
 			}
 
-			deadline += msecs_to_jiffies(p * MSEC_PER_SEC);
+			deadline += cfs_time_seconds(p);
 			spin_lock_irqsave(lock, flags);
 		}
 

lnet/lnet/api-ni.c
@@ -3917,9 +3917,9 @@ LNetCtl(unsigned int cmd, void *arg)
 		/* If timeout is negative then set default of 3 minutes */
 		if (((s32)data->ioc_u32[1] <= 0) ||
 		    data->ioc_u32[1] > (DEFAULT_PEER_TIMEOUT * MSEC_PER_SEC))
-			timeout = msecs_to_jiffies(DEFAULT_PEER_TIMEOUT * MSEC_PER_SEC);
+			timeout = cfs_time_seconds(DEFAULT_PEER_TIMEOUT);
 		else
-			timeout = msecs_to_jiffies(data->ioc_u32[1]);
+			timeout = nsecs_to_jiffies(data->ioc_u32[1] * NSEC_PER_MSEC);
 
 		rc = lnet_ping(id, timeout, data->ioc_pbuf1,
 			       data->ioc_plen1 / sizeof(struct lnet_process_id));
@@ -3939,9 +3939,9 @@ LNetCtl(unsigned int cmd, void *arg)
 		/* If timeout is negative then set default of 3 minutes */
 		if (((s32)ping->op_param) <= 0 ||
 		    ping->op_param > (DEFAULT_PEER_TIMEOUT * MSEC_PER_SEC))
-			timeout = msecs_to_jiffies(DEFAULT_PEER_TIMEOUT * MSEC_PER_SEC);
+			timeout = cfs_time_seconds(DEFAULT_PEER_TIMEOUT);
 		else
-			timeout = msecs_to_jiffies(ping->op_param);
+			timeout = nsecs_to_jiffies(ping->op_param * NSEC_PER_MSEC);
 
 		rc = lnet_ping(ping->ping_id, timeout,
 			       ping->ping_buf,
@@ -4087,7 +4087,7 @@ static int lnet_ping(struct lnet_process_id id, signed long timeout,
 	int which;
 	int unlinked = 0;
 	int replied = 0;
-	const signed long a_long_time = msecs_to_jiffies(60 * MSEC_PER_SEC);
+	const signed long a_long_time = cfs_time_seconds(60);
 	struct lnet_ping_buffer *pbuf;
 	struct lnet_process_id tmpid;
 	int i;

lnet/lnet/lib-socket.c
@@ -68,7 +68,7 @@ int
 lnet_sock_write(struct socket *sock, void *buffer, int nob, int timeout)
 {
 	int		rc;
-	long		jiffies_left = timeout * msecs_to_jiffies(MSEC_PER_SEC);
+	long		jiffies_left = cfs_time_seconds(timeout);
 	unsigned long	then;
 	struct timeval	tv;
 
@@ -87,15 +87,7 @@ lnet_sock_write(struct socket *sock, void *buffer, int nob, int timeout)
 
 		if (timeout != 0) {
 			/* Set send timeout to remaining time */
-			tv = (struct timeval) {
-				.tv_sec = jiffies_left /
-					  msecs_to_jiffies(MSEC_PER_SEC),
-				.tv_usec = ((jiffies_left %
-					     msecs_to_jiffies(MSEC_PER_SEC)) *
-					     USEC_PER_SEC) /
-					     msecs_to_jiffies(MSEC_PER_SEC)
-			};
-
+			jiffies_to_timeval(jiffies_left, &tv);
 			rc = kernel_setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO,
 					       (char *)&tv, sizeof(tv));
 			if (rc != 0) {
@@ -135,7 +127,7 @@ int
 lnet_sock_read(struct socket *sock, void *buffer, int nob, int timeout)
 {
 	int		rc;
-	long		jiffies_left = timeout * msecs_to_jiffies(MSEC_PER_SEC);
+	long		jiffies_left = cfs_time_seconds(timeout);
 	unsigned long	then;
 	struct timeval	tv;
 
@@ -152,13 +144,7 @@ lnet_sock_read(struct socket *sock, void *buffer, int nob, int timeout)
 		};
 
 		/* Set receive timeout to remaining time */
-		tv = (struct timeval) {
-			.tv_sec = jiffies_left / msecs_to_jiffies(MSEC_PER_SEC),
-			.tv_usec = ((jiffies_left %
-					msecs_to_jiffies(MSEC_PER_SEC)) *
-					USEC_PER_SEC) /
-					msecs_to_jiffies(MSEC_PER_SEC)
-		};
+		jiffies_to_timeval(jiffies_left, &tv);
 		rc = kernel_setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO,
 				       (char *)&tv, sizeof(tv));
 		if (rc != 0) {

lustre/ldlm/ldlm_lib.c
@@ -2144,7 +2144,7 @@ repeat:
 
 	while (wait_event_timeout(obd->obd_next_transno_waitq,
 				  check_routine(lut),
-				  msecs_to_jiffies(60 * MSEC_PER_SEC)) == 0)
+				  cfs_time_seconds(60)) == 0)
 		; /* wait indefinitely for event, but don't trigger watchdog */
 
 	if (obd->obd_abort_recovery) {

commit 6fbfb01be3c45c60c418ca37280d2378f91bc565

LU-9019 llite: fix timeout to not be zero
The timeout in ll_kill_super() is intended to be
1/8 of a second, while it loops waiting on other
threads to finish.  However, this was incorrectly
calculated to always be 0 (infinite wait) due to
integer division.

Instead, convert seconds to jiffies before division.

Fixes: 0c2cc920370e ("LU-9019 libcfs: avoid using HZ and msecs_to_jiffies()")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I33efbfc2d6c0a9c4ae6404c1974c7593a72540e5
Reviewed-on: https://review.whamcloud.com/35992
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -809,7 +809,7 @@ void ll_kill_super(struct super_block *sb)
 		/* wait running statahead threads to quit */
 		while (atomic_read(&sbi->ll_sa_running) > 0) {
 			set_current_state(TASK_UNINTERRUPTIBLE);
-			schedule_timeout(cfs_time_seconds(1 >> 3));
+			schedule_timeout(cfs_time_seconds(1) >> 3);
 		}
 	}
 

lustre/mgc/mgc_request.c
@@ -641,10 +641,9 @@ static int mgc_requeue_thread(void *data)
 		/* Always wait a few seconds to allow the server who
 		   caused the lock revocation to finish its setup, plus some
 		   random so everyone doesn't try to reconnect at once. */
-		to = cfs_time_seconds(MGC_TIMEOUT_MIN_SECONDS);
+		to = cfs_time_seconds(MGC_TIMEOUT_MIN_SECONDS * 100 + rand);
 		/* rand is centi-seconds */
-		to += cfs_time_seconds(rand) / 100;
-		lwi = LWI_TIMEOUT(to, NULL, NULL);
+		lwi = LWI_TIMEOUT(to / 100, NULL, NULL);
 		l_wait_event(rq_waitq, rq_state & (RQ_STOP | RQ_PRECLEANUP),
 			     &lwi);
 