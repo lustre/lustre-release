lustre/lustre-release

commit d3090bb2b4860e997730e90426e11fc51ee27c0c

LU-11213 lod: share object alloc QoS code with LMV
Move object alloc QoS code to obdclass, so that LMV and LOD
can share the same code.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I451a43fa9a254ec709b2acd43538fdcba0be4a88
Reviewed-on: https://review.whamcloud.com/35219
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/lu_qos.c
@@ -209,3 +209,307 @@ u64 lu_prandom_u64_max(u64 ep_ro)
 	return rand;
 }
 EXPORT_SYMBOL(lu_prandom_u64_max);
+
+static inline __u64 tgt_statfs_bavail(struct lu_tgt_desc *tgt)
+{
+	struct obd_statfs *statfs = &tgt->ltd_statfs;
+
+	return statfs->os_bavail * statfs->os_bsize;
+}
+
+static inline __u64 tgt_statfs_iavail(struct lu_tgt_desc *tgt)
+{
+	return tgt->ltd_statfs.os_ffree;
+}
+
+/**
+ * Calculate penalties per-tgt and per-server
+ *
+ * Re-calculate penalties when the configuration changes, active targets
+ * change and after statfs refresh (all these are reflected by lq_dirty flag).
+ * On every tgt and server: decay the penalty by half for every 8x the update
+ * interval that the device has been idle. That gives lots of time for the
+ * statfs information to be updated (which the penalty is only a proxy for),
+ * and avoids penalizing server/tgt under light load.
+ * See lqos_calc_weight() for how penalties are factored into the weight.
+ *
+ * \param[in] qos		lu_qos
+ * \param[in] ltd		lu_tgt_descs
+ * \param[in] active_tgt_nr	active tgt number
+ * \param[in] maxage		qos max age
+ * \param[in] is_mdt		MDT will count inode usage
+ *
+ * \retval 0		on success
+ * \retval -EAGAIN	the number of tgt isn't enough or all tgt spaces are
+ *			almost the same
+ */
+int lqos_calc_penalties(struct lu_qos *qos, struct lu_tgt_descs *ltd,
+			__u32 active_tgt_nr, __u32 maxage, bool is_mdt)
+{
+	struct lu_tgt_desc *tgt;
+	struct lu_svr_qos *svr;
+	__u64 ba_max, ba_min, ba;
+	__u64 ia_max, ia_min, ia = 1;
+	__u32 num_active;
+	int prio_wide;
+	time64_t now, age;
+	int rc;
+
+	ENTRY;
+
+	if (!qos->lq_dirty)
+		GOTO(out, rc = 0);
+
+	num_active = active_tgt_nr - 1;
+	if (num_active < 1)
+		GOTO(out, rc = -EAGAIN);
+
+	/* find bavail on each server */
+	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
+		svr->lsq_bavail = 0;
+		/* if inode is not counted, set to 1 to ignore */
+		svr->lsq_iavail = is_mdt ? 0 : 1;
+	}
+	qos->lq_active_svr_count = 0;
+
+	/*
+	 * How badly user wants to select targets "widely" (not recently chosen
+	 * and not on recent MDS's).  As opposed to "freely" (free space avail.)
+	 * 0-256
+	 */
+	prio_wide = 256 - qos->lq_prio_free;
+
+	ba_min = (__u64)(-1);
+	ba_max = 0;
+	ia_min = (__u64)(-1);
+	ia_max = 0;
+	now = ktime_get_real_seconds();
+
+	/* Calculate server penalty per object */
+	ltd_foreach_tgt(ltd, tgt) {
+		if (!tgt->ltd_active)
+			continue;
+
+		/* when inode is counted, bavail >> 16 to avoid overflow */
+		ba = tgt_statfs_bavail(tgt);
+		if (is_mdt)
+			ba >>= 16;
+		else
+			ba >>= 8;
+		if (!ba)
+			continue;
+
+		ba_min = min(ba, ba_min);
+		ba_max = max(ba, ba_max);
+
+		/* Count the number of usable servers */
+		if (tgt->ltd_qos.ltq_svr->lsq_bavail == 0)
+			qos->lq_active_svr_count++;
+		tgt->ltd_qos.ltq_svr->lsq_bavail += ba;
+
+		if (is_mdt) {
+			/* iavail >> 8 to avoid overflow */
+			ia = tgt_statfs_iavail(tgt) >> 8;
+			if (!ia)
+				continue;
+
+			ia_min = min(ia, ia_min);
+			ia_max = max(ia, ia_max);
+
+			tgt->ltd_qos.ltq_svr->lsq_iavail += ia;
+		}
+
+		/*
+		 * per-tgt penalty is
+		 * prio * bavail * iavail / (num_tgt - 1) / 2
+		 */
+		tgt->ltd_qos.ltq_penalty_per_obj = prio_wide * ba * ia;
+		do_div(tgt->ltd_qos.ltq_penalty_per_obj, num_active);
+		tgt->ltd_qos.ltq_penalty_per_obj >>= 1;
+
+		age = (now - tgt->ltd_qos.ltq_used) >> 3;
+		if (qos->lq_reset || age > 32 * maxage)
+			tgt->ltd_qos.ltq_penalty = 0;
+		else if (age > maxage)
+			/* Decay tgt penalty. */
+			tgt->ltd_qos.ltq_penalty >>= (age / maxage);
+	}
+
+	num_active = qos->lq_active_svr_count - 1;
+	if (num_active < 1) {
+		/*
+		 * If there's only 1 server, we can't penalize it, so instead
+		 * we have to double the tgt penalty
+		 */
+		num_active = 1;
+		ltd_foreach_tgt(ltd, tgt) {
+			if (!tgt->ltd_active)
+				continue;
+
+			tgt->ltd_qos.ltq_penalty_per_obj <<= 1;
+		}
+	}
+
+	/*
+	 * Per-server penalty is
+	 * prio * bavail * iavail / server_tgts / (num_svr - 1) / 2
+	 */
+	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
+		ba = svr->lsq_bavail;
+		ia = svr->lsq_iavail;
+		svr->lsq_penalty_per_obj = prio_wide * ba  * ia;
+		do_div(ba, svr->lsq_tgt_count * num_active);
+		svr->lsq_penalty_per_obj >>= 1;
+
+		age = (now - svr->lsq_used) >> 3;
+		if (qos->lq_reset || age > 32 * maxage)
+			svr->lsq_penalty = 0;
+		else if (age > maxage)
+			/* Decay server penalty. */
+			svr->lsq_penalty >>= age / maxage;
+	}
+
+	qos->lq_dirty = 0;
+	qos->lq_reset = 0;
+
+	/*
+	 * If each tgt has almost same free space, do rr allocation for better
+	 * creation performance
+	 */
+	qos->lq_same_space = 0;
+	if ((ba_max * (256 - qos->lq_threshold_rr)) >> 8 < ba_min &&
+	    (ia_max * (256 - qos->lq_threshold_rr)) >> 8 < ia_min) {
+		qos->lq_same_space = 1;
+		/* Reset weights for the next time we enter qos mode */
+		qos->lq_reset = 1;
+	}
+	rc = 0;
+
+out:
+	if (!rc && qos->lq_same_space)
+		RETURN(-EAGAIN);
+
+	RETURN(rc);
+}
+EXPORT_SYMBOL(lqos_calc_penalties);
+
+bool lqos_is_usable(struct lu_qos *qos, __u32 active_tgt_nr)
+{
+	if (!qos->lq_dirty && qos->lq_same_space)
+		return false;
+
+	if (active_tgt_nr < 2)
+		return false;
+
+	return true;
+}
+EXPORT_SYMBOL(lqos_is_usable);
+
+/**
+ * Calculate weight for a given tgt.
+ *
+ * The final tgt weight is bavail >> 16 * iavail >> 8 minus the tgt and server
+ * penalties.  See lqos_calc_ppts() for how penalties are calculated.
+ *
+ * \param[in] tgt	target descriptor
+ */
+void lqos_calc_weight(struct lu_tgt_desc *tgt)
+{
+	struct lu_tgt_qos *ltq = &tgt->ltd_qos;
+	__u64 temp, temp2;
+
+	temp = (tgt_statfs_bavail(tgt) >> 16) * (tgt_statfs_iavail(tgt) >> 8);
+	temp2 = ltq->ltq_penalty + ltq->ltq_svr->lsq_penalty;
+	if (temp < temp2)
+		ltq->ltq_weight = 0;
+	else
+		ltq->ltq_weight = temp - temp2;
+}
+EXPORT_SYMBOL(lqos_calc_weight);
+
+/**
+ * Re-calculate weights.
+ *
+ * The function is called when some target was used for a new object. In
+ * this case we should re-calculate all the weights to keep new allocations
+ * balanced well.
+ *
+ * \param[in] qos		lu_qos
+ * \param[in] ltd		lu_tgt_descs
+ * \param[in] tgt		target where a new object was placed
+ * \param[in] active_tgt_nr	active tgt number
+ * \param[out] total_wt	new total weight for the pool
+ *
+ * \retval		0
+ */
+int lqos_recalc_weight(struct lu_qos *qos, struct lu_tgt_descs *ltd,
+		       struct lu_tgt_desc *tgt, __u32 active_tgt_nr,
+		       __u64 *total_wt)
+{
+	struct lu_tgt_qos *ltq;
+	struct lu_svr_qos *svr;
+
+	ENTRY;
+
+	ltq = &tgt->ltd_qos;
+	LASSERT(ltq);
+
+	/* Don't allocate on this device anymore, until the next alloc_qos */
+	ltq->ltq_usable = 0;
+
+	svr = ltq->ltq_svr;
+
+	/*
+	 * Decay old penalty by half (we're adding max penalty, and don't
+	 * want it to run away.)
+	 */
+	ltq->ltq_penalty >>= 1;
+	svr->lsq_penalty >>= 1;
+
+	/* mark the server and tgt as recently used */
+	ltq->ltq_used = svr->lsq_used = ktime_get_real_seconds();
+
+	/* Set max penalties for this tgt and server */
+	ltq->ltq_penalty += ltq->ltq_penalty_per_obj * active_tgt_nr;
+	svr->lsq_penalty += svr->lsq_penalty_per_obj * active_tgt_nr;
+
+	/* Decrease all MDS penalties */
+	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
+		if (svr->lsq_penalty < svr->lsq_penalty_per_obj)
+			svr->lsq_penalty = 0;
+		else
+			svr->lsq_penalty -= svr->lsq_penalty_per_obj;
+	}
+
+	*total_wt = 0;
+	/* Decrease all tgt penalties */
+	ltd_foreach_tgt(ltd, tgt) {
+		if (!tgt->ltd_active)
+			continue;
+
+		if (ltq->ltq_penalty < ltq->ltq_penalty_per_obj)
+			ltq->ltq_penalty = 0;
+		else
+			ltq->ltq_penalty -= ltq->ltq_penalty_per_obj;
+
+		lqos_calc_weight(tgt);
+
+		/* Recalc the total weight of usable osts */
+		if (ltq->ltq_usable)
+			*total_wt += ltq->ltq_weight;
+
+		CDEBUG(D_OTHER, "recalc tgt %d usable=%d avail=%llu"
+			  " tgtppo=%llu tgtp=%llu svrppo=%llu"
+			  " svrp=%llu wt=%llu\n",
+			  tgt->ltd_index, ltq->ltq_usable,
+			  tgt_statfs_bavail(tgt) >> 10,
+			  ltq->ltq_penalty_per_obj >> 10,
+			  ltq->ltq_penalty >> 10,
+			  ltq->ltq_svr->lsq_penalty_per_obj >> 10,
+			  ltq->ltq_svr->lsq_penalty >> 10,
+			  ltq->ltq_weight >> 10);
+	}
+
+	RETURN(0);
+}
+EXPORT_SYMBOL(lqos_recalc_weight);

lustre/include/lu_object.h
@@ -1513,6 +1513,13 @@ struct lu_qos {
 void lu_qos_rr_init(struct lu_qos_rr *lqr);
 int lqos_add_tgt(struct lu_qos *qos, struct lu_tgt_desc *ltd);
 int lqos_del_tgt(struct lu_qos *qos, struct lu_tgt_desc *ltd);
+bool lqos_is_usable(struct lu_qos *qos, __u32 active_tgt_nr);
+int lqos_calc_penalties(struct lu_qos *qos, struct lu_tgt_descs *ltd,
+			__u32 active_tgt_nr, __u32 maxage, bool is_mdt);
+void lqos_calc_weight(struct lu_tgt_desc *tgt);
+int lqos_recalc_weight(struct lu_qos *qos, struct lu_tgt_descs *ltd,
+		       struct lu_tgt_desc *tgt, __u32 active_tgt_nr,
+		       __u64 *total_wt);
 u64 lu_prandom_u64_max(u64 ep_ro);
 
 int lu_tgt_descs_init(struct lu_tgt_descs *ltd);

lustre/lmv/Makefile.in
@@ -1,4 +1,4 @@
 MODULES := lmv
-lmv-objs := lmv_obd.o lmv_intent.o lmv_fld.o lproc_lmv.o lmv_qos.o
+lmv-objs := lmv_obd.o lmv_intent.o lmv_fld.o lproc_lmv.o
 
 @INCLUDE_RULES@

lustre/lmv/lmv_internal.h
@@ -219,10 +219,6 @@ static inline bool lmv_dir_retry_check_update(struct md_op_data *op_data)
 struct lmv_tgt_desc *lmv_locate_tgt(struct lmv_obd *lmv,
 				    struct md_op_data *op_data);
 
-/* lmv_qos.c */
-struct lu_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv, __u32 *mdt);
-struct lu_tgt_desc *lmv_locate_tgt_rr(struct lmv_obd *lmv, __u32 *mdt);
-
 /* lproc_lmv.c */
 int lmv_tunables_init(struct obd_device *obd);
 

lustre/lmv/lmv_obd.c
@@ -1555,6 +1555,91 @@ static int lmv_close(struct obd_export *exp, struct md_op_data *op_data,
 	RETURN(rc);
 }
 
+static struct lu_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv, __u32 *mdt)
+{
+	struct lu_tgt_desc *tgt;
+	__u64 total_weight = 0;
+	__u64 cur_weight = 0;
+	__u64 rand;
+	int rc;
+
+	ENTRY;
+
+	if (!lqos_is_usable(&lmv->lmv_qos, lmv->desc.ld_active_tgt_count))
+		RETURN(ERR_PTR(-EAGAIN));
+
+	down_write(&lmv->lmv_qos.lq_rw_sem);
+
+	if (!lqos_is_usable(&lmv->lmv_qos, lmv->desc.ld_active_tgt_count))
+		GOTO(unlock, tgt = ERR_PTR(-EAGAIN));
+
+	rc = lqos_calc_penalties(&lmv->lmv_qos, &lmv->lmv_mdt_descs,
+				 lmv->desc.ld_active_tgt_count,
+				 lmv->desc.ld_qos_maxage, true);
+	if (rc)
+		GOTO(unlock, tgt = ERR_PTR(rc));
+
+	lmv_foreach_tgt(lmv, tgt) {
+		tgt->ltd_qos.ltq_usable = 0;
+		if (!tgt->ltd_exp || !tgt->ltd_active)
+			continue;
+
+		tgt->ltd_qos.ltq_usable = 1;
+		lqos_calc_weight(tgt);
+		total_weight += tgt->ltd_qos.ltq_weight;
+	}
+
+	rand = lu_prandom_u64_max(total_weight);
+
+	lmv_foreach_connected_tgt(lmv, tgt) {
+		if (!tgt->ltd_qos.ltq_usable)
+			continue;
+
+		cur_weight += tgt->ltd_qos.ltq_weight;
+		if (cur_weight < rand)
+			continue;
+
+		*mdt = tgt->ltd_index;
+		lqos_recalc_weight(&lmv->lmv_qos, &lmv->lmv_mdt_descs, tgt,
+				   lmv->desc.ld_active_tgt_count,
+				   &total_weight);
+		GOTO(unlock, rc = 0);
+	}
+
+	/* no proper target found */
+	GOTO(unlock, tgt = ERR_PTR(-EAGAIN));
+unlock:
+	up_write(&lmv->lmv_qos.lq_rw_sem);
+
+	return tgt;
+}
+
+static struct lu_tgt_desc *lmv_locate_tgt_rr(struct lmv_obd *lmv, __u32 *mdt)
+{
+	struct lu_tgt_desc *tgt;
+	int i;
+	int index;
+
+	ENTRY;
+
+	spin_lock(&lmv->lmv_qos.lq_rr.lqr_alloc);
+	for (i = 0; i < lmv->desc.ld_tgt_count; i++) {
+		index = (i + lmv->lmv_qos_rr_index) % lmv->desc.ld_tgt_count;
+		tgt = lmv_tgt(lmv, index);
+		if (!tgt || !tgt->ltd_exp || !tgt->ltd_active)
+			continue;
+
+		*mdt = tgt->ltd_index;
+		lmv->lmv_qos_rr_index = (*mdt + 1) % lmv->desc.ld_tgt_count;
+		spin_unlock(&lmv->lmv_qos.lq_rr.lqr_alloc);
+
+		RETURN(tgt);
+	}
+	spin_unlock(&lmv->lmv_qos.lq_rr.lqr_alloc);
+
+	RETURN(ERR_PTR(-ENODEV));
+}
+
 static struct lmv_tgt_desc *
 lmv_locate_tgt_by_name(struct lmv_obd *lmv, struct lmv_stripe_md *lsm,
 		       const char *name, int namelen, struct lu_fid *fid,

lustre/lmv/lmv_qos.c
@@ -1,413 +1,0 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
-/*
- * This file is part of Lustre, http://www.lustre.org/
- *
- * lustre/lmv/lmv_qos.c
- *
- * LMV QoS.
- * These are the only exported functions, they provide some generic
- * infrastructure for object allocation QoS
- *
- */
-
-#define DEBUG_SUBSYSTEM S_LMV
-
-#include <asm/div64.h>
-#include <linux/random.h>
-
-#include <libcfs/libcfs.h>
-#include <uapi/linux/lustre/lustre_idl.h>
-#include <lustre_swab.h>
-#include <obd_class.h>
-
-#include "lmv_internal.h"
-
-static inline __u64 tgt_statfs_bavail(struct lu_tgt_desc *tgt)
-{
-	struct obd_statfs *statfs = &tgt->ltd_statfs;
-
-	return statfs->os_bavail * statfs->os_bsize;
-}
-
-static inline __u64 tgt_statfs_iavail(struct lu_tgt_desc *tgt)
-{
-	return tgt->ltd_statfs.os_ffree;
-}
-
-/**
- * Calculate penalties per-tgt and per-server
- *
- * Re-calculate penalties when the configuration changes, active targets
- * change and after statfs refresh (all these are reflected by lq_dirty flag).
- * On every MDT and MDS: decay the penalty by half for every 8x the update
- * interval that the device has been idle. That gives lots of time for the
- * statfs information to be updated (which the penalty is only a proxy for),
- * and avoids penalizing MDS/MDTs under light load.
- * See lmv_qos_calc_weight() for how penalties are factored into the weight.
- *
- * \param[in] lmv	LMV device
- *
- * \retval 0		on success
- * \retval -EAGAIN	the number of MDTs isn't enough or all MDT spaces are
- *			almost the same
- */
-static int lmv_qos_calc_ppts(struct lmv_obd *lmv)
-{
-	struct lu_qos *qos = &lmv->lmv_qos;
-	struct lu_tgt_desc *tgt;
-	struct lu_svr_qos *svr;
-	__u64 ba_max, ba_min, ba;
-	__u64 ia_max, ia_min, ia;
-	__u32 num_active;
-	int prio_wide;
-	time64_t now, age;
-	__u32 maxage = lmv->desc.ld_qos_maxage;
-	int rc;
-
-	ENTRY;
-
-	if (!qos->lq_dirty)
-		GOTO(out, rc = 0);
-
-	num_active = lmv->desc.ld_active_tgt_count;
-	if (num_active < 2)
-		GOTO(out, rc = -EAGAIN);
-
-	/* find bavail on each server */
-	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
-		svr->lsq_bavail = 0;
-		svr->lsq_iavail = 0;
-	}
-	qos->lq_active_svr_count = 0;
-
-	/*
-	 * How badly user wants to select targets "widely" (not recently chosen
-	 * and not on recent MDS's).  As opposed to "freely" (free space avail.)
-	 * 0-256
-	 */
-	prio_wide = 256 - qos->lq_prio_free;
-
-	ba_min = (__u64)(-1);
-	ba_max = 0;
-	ia_min = (__u64)(-1);
-	ia_max = 0;
-	now = ktime_get_real_seconds();
-
-	/* Calculate server penalty per object */
-	lmv_foreach_tgt(lmv, tgt) {
-		if (!tgt->ltd_exp || !tgt->ltd_active)
-			continue;
-
-		/* bavail >> 16 to avoid overflow */
-		ba = tgt_statfs_bavail(tgt) >> 16;
-		if (!ba)
-			continue;
-
-		ba_min = min(ba, ba_min);
-		ba_max = max(ba, ba_max);
-
-		/* iavail >> 8 to avoid overflow */
-		ia = tgt_statfs_iavail(tgt) >> 8;
-		if (!ia)
-			continue;
-
-		ia_min = min(ia, ia_min);
-		ia_max = max(ia, ia_max);
-
-		/* Count the number of usable MDS's */
-		if (tgt->ltd_qos.ltq_svr->lsq_bavail == 0)
-			qos->lq_active_svr_count++;
-		tgt->ltd_qos.ltq_svr->lsq_bavail += ba;
-		tgt->ltd_qos.ltq_svr->lsq_iavail += ia;
-
-		/*
-		 * per-MDT penalty is
-		 * prio * bavail * iavail / (num_tgt - 1) / 2
-		 */
-		tgt->ltd_qos.ltq_penalty_per_obj = prio_wide * ba * ia;
-		do_div(tgt->ltd_qos.ltq_penalty_per_obj, num_active - 1);
-		tgt->ltd_qos.ltq_penalty_per_obj >>= 1;
-
-		age = (now - tgt->ltd_qos.ltq_used) >> 3;
-		if (qos->lq_reset || age > 32 * maxage)
-			tgt->ltd_qos.ltq_penalty = 0;
-		else if (age > maxage)
-			/* Decay tgt penalty. */
-			tgt->ltd_qos.ltq_penalty >>= (age / maxage);
-	}
-
-	num_active = qos->lq_active_svr_count;
-	if (num_active < 2) {
-		/*
-		 * If there's only 1 MDS, we can't penalize it, so instead
-		 * we have to double the MDT penalty
-		 */
-		num_active = 2;
-		lmv_foreach_tgt(lmv, tgt) {
-			if (!tgt->ltd_exp || !tgt->ltd_active)
-				continue;
-
-			tgt->ltd_qos.ltq_penalty_per_obj <<= 1;
-		}
-	}
-
-	/*
-	 * Per-MDS penalty is
-	 * prio * bavail * iavail / server_tgts / (num_svr - 1) / 2
-	 */
-	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
-		ba = svr->lsq_bavail;
-		ia = svr->lsq_iavail;
-		svr->lsq_penalty_per_obj = prio_wide * ba  * ia;
-		do_div(ba, svr->lsq_tgt_count * (num_active - 1));
-		svr->lsq_penalty_per_obj >>= 1;
-
-		age = (now - svr->lsq_used) >> 3;
-		if (qos->lq_reset || age > 32 * maxage)
-			svr->lsq_penalty = 0;
-		else if (age > maxage)
-			/* Decay server penalty. */
-			svr->lsq_penalty >>= age / maxage;
-	}
-
-	qos->lq_dirty = 0;
-	qos->lq_reset = 0;
-
-	/*
-	 * If each MDT has almost same free space, do rr allocation for better
-	 * creation performance
-	 */
-	qos->lq_same_space = 0;
-	if ((ba_max * (256 - qos->lq_threshold_rr)) >> 8 < ba_min &&
-	    (ia_max * (256 - qos->lq_threshold_rr)) >> 8 < ia_min) {
-		qos->lq_same_space = 1;
-		/* Reset weights for the next time we enter qos mode */
-		qos->lq_reset = 1;
-	}
-	rc = 0;
-
-out:
-	if (!rc && qos->lq_same_space)
-		RETURN(-EAGAIN);
-
-	RETURN(rc);
-}
-
-static inline bool lmv_qos_is_usable(struct lmv_obd *lmv)
-{
-	if (!lmv->lmv_qos.lq_dirty && lmv->lmv_qos.lq_same_space)
-		return false;
-
-	if (lmv->desc.ld_active_tgt_count < 2)
-		return false;
-
-	return true;
-}
-
-/**
- * Calculate weight for a given MDT.
- *
- * The final MDT weight is bavail >> 16 * iavail >> 8 minus the MDT and MDS
- * penalties.  See lmv_qos_calc_ppts() for how penalties are calculated.
- *
- * \param[in] tgt	MDT target descriptor
- */
-static void lmv_qos_calc_weight(struct lu_tgt_desc *tgt)
-{
-	struct lu_tgt_qos *ltq = &tgt->ltd_qos;
-	__u64 temp, temp2;
-
-	temp = (tgt_statfs_bavail(tgt) >> 16) * (tgt_statfs_iavail(tgt) >> 8);
-	temp2 = ltq->ltq_penalty + ltq->ltq_svr->lsq_penalty;
-	if (temp < temp2)
-		ltq->ltq_weight = 0;
-	else
-		ltq->ltq_weight = temp - temp2;
-}
-
-/**
- * Re-calculate weights.
- *
- * The function is called when some target was used for a new object. In
- * this case we should re-calculate all the weights to keep new allocations
- * balanced well.
- *
- * \param[in] lmv	LMV device
- * \param[in] tgt	target where a new object was placed
- * \param[out] total_wt	new total weight for the pool
- *
- * \retval		0
- */
-static int lmv_qos_used(struct lmv_obd *lmv, struct lu_tgt_desc *tgt,
-			__u64 *total_wt)
-{
-	struct lu_tgt_qos *ltq;
-	struct lu_svr_qos *svr;
-
-	ENTRY;
-
-	ltq = &tgt->ltd_qos;
-	LASSERT(ltq);
-
-	/* Don't allocate on this device anymore, until the next alloc_qos */
-	ltq->ltq_usable = 0;
-
-	svr = ltq->ltq_svr;
-
-	/*
-	 * Decay old penalty by half (we're adding max penalty, and don't
-	 * want it to run away.)
-	 */
-	ltq->ltq_penalty >>= 1;
-	svr->lsq_penalty >>= 1;
-
-	/* mark the MDS and MDT as recently used */
-	ltq->ltq_used = svr->lsq_used = ktime_get_real_seconds();
-
-	/* Set max penalties for this MDT and MDS */
-	ltq->ltq_penalty += ltq->ltq_penalty_per_obj *
-			    lmv->desc.ld_active_tgt_count;
-	svr->lsq_penalty += svr->lsq_penalty_per_obj *
-		lmv->lmv_qos.lq_active_svr_count;
-
-	/* Decrease all MDS penalties */
-	list_for_each_entry(svr, &lmv->lmv_qos.lq_svr_list, lsq_svr_list) {
-		if (svr->lsq_penalty < svr->lsq_penalty_per_obj)
-			svr->lsq_penalty = 0;
-		else
-			svr->lsq_penalty -= svr->lsq_penalty_per_obj;
-	}
-
-	*total_wt = 0;
-	/* Decrease all MDT penalties */
-	lmv_foreach_tgt(lmv, tgt) {
-		if (!tgt->ltd_exp || !tgt->ltd_active)
-			continue;
-
-		if (ltq->ltq_penalty < ltq->ltq_penalty_per_obj)
-			ltq->ltq_penalty = 0;
-		else
-			ltq->ltq_penalty -= ltq->ltq_penalty_per_obj;
-
-		lmv_qos_calc_weight(tgt);
-
-		/* Recalc the total weight of usable osts */
-		if (ltq->ltq_usable)
-			*total_wt += ltq->ltq_weight;
-
-		CDEBUG(D_OTHER, "recalc tgt %d usable=%d avail=%llu"
-			  " tgtppo=%llu tgtp=%llu svrppo=%llu"
-			  " svrp=%llu wt=%llu\n",
-			  tgt->ltd_index, ltq->ltq_usable,
-			  tgt_statfs_bavail(tgt) >> 10,
-			  ltq->ltq_penalty_per_obj >> 10,
-			  ltq->ltq_penalty >> 10,
-			  ltq->ltq_svr->lsq_penalty_per_obj >> 10,
-			  ltq->ltq_svr->lsq_penalty >> 10,
-			  ltq->ltq_weight >> 10);
-	}
-
-	RETURN(0);
-}
-
-struct lu_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv, __u32 *mdt)
-{
-	struct lu_tgt_desc *tgt;
-	__u64 total_weight = 0;
-	__u64 cur_weight = 0;
-	__u64 rand;
-	int rc;
-
-	ENTRY;
-
-	if (!lmv_qos_is_usable(lmv))
-		RETURN(ERR_PTR(-EAGAIN));
-
-	down_write(&lmv->lmv_qos.lq_rw_sem);
-
-	if (!lmv_qos_is_usable(lmv))
-		GOTO(unlock, tgt = ERR_PTR(-EAGAIN));
-
-	rc = lmv_qos_calc_ppts(lmv);
-	if (rc)
-		GOTO(unlock, tgt = ERR_PTR(rc));
-
-	lmv_foreach_tgt(lmv, tgt) {
-		tgt->ltd_qos.ltq_usable = 0;
-		if (!tgt->ltd_exp || !tgt->ltd_active)
-			continue;
-
-		tgt->ltd_qos.ltq_usable = 1;
-		lmv_qos_calc_weight(tgt);
-		total_weight += tgt->ltd_qos.ltq_weight;
-	}
-
-	rand = lu_prandom_u64_max(total_weight);
-
-	lmv_foreach_tgt(lmv, tgt) {
-		if (!tgt->ltd_qos.ltq_usable)
-			continue;
-
-		cur_weight += tgt->ltd_qos.ltq_weight;
-		if (cur_weight < rand)
-			continue;
-
-		*mdt = tgt->ltd_index;
-		lmv_qos_used(lmv, tgt, &total_weight);
-		GOTO(unlock, rc = 0);
-	}
-
-	/* no proper target found */
-	GOTO(unlock, tgt = ERR_PTR(-EAGAIN));
-unlock:
-	up_write(&lmv->lmv_qos.lq_rw_sem);
-
-	return tgt;
-}
-
-struct lu_tgt_desc *lmv_locate_tgt_rr(struct lmv_obd *lmv, __u32 *mdt)
-{
-	struct lu_tgt_desc *tgt;
-	int i;
-
-	ENTRY;
-
-	spin_lock(&lmv->lmv_qos.lq_rr.lqr_alloc);
-	for (i = 0; i < lmv->desc.ld_tgt_count; i++) {
-		tgt = lmv_tgt(lmv,
-			(i + lmv->lmv_qos_rr_index) % lmv->desc.ld_tgt_count);
-		if (!tgt || !tgt->ltd_exp || !tgt->ltd_active)
-			continue;
-
-		*mdt = tgt->ltd_index;
-		lmv->lmv_qos_rr_index =
-			(i + lmv->lmv_qos_rr_index + 1) %
-			lmv->desc.ld_tgt_count;
-		spin_unlock(&lmv->lmv_qos.lq_rr.lqr_alloc);
-
-		RETURN(tgt);
-	}
-	spin_unlock(&lmv->lmv_qos.lq_rr.lqr_alloc);
-
-	RETURN(ERR_PTR(-ENODEV));
-}

commit 9130d05de4e2b610095169b6f49a149c66444ee2

LU-12495 obdclass: qos penalties miscalculated
In lqos_calc_penalties(), the penalty_per_obj is miscalculated.

Also improve sanity test_413b: take both blocks and inodes into
account to make the test more robost.

Fixes: d3090bb ("LU-11213 lod: share object alloc QoS code with LMV")
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ie965fc3bfa3e303c27f93a6e1a428cc4a90f8548
Reviewed-on: https://review.whamcloud.com/36269
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/lu_qos.c
@@ -323,7 +323,7 @@ int lqos_calc_penalties(struct lu_qos *qos, struct lu_tgt_descs *ltd,
 		 * per-tgt penalty is
 		 * prio * bavail * iavail / (num_tgt - 1) / 2
 		 */
-		tgt->ltd_qos.ltq_penalty_per_obj = prio_wide * ba * ia;
+		tgt->ltd_qos.ltq_penalty_per_obj = prio_wide * ba * ia >> 8;
 		do_div(tgt->ltd_qos.ltq_penalty_per_obj, num_active);
 		tgt->ltd_qos.ltq_penalty_per_obj >>= 1;
 
@@ -357,7 +357,7 @@ int lqos_calc_penalties(struct lu_qos *qos, struct lu_tgt_descs *ltd,
 	list_for_each_entry(svr, &qos->lq_svr_list, lsq_svr_list) {
 		ba = svr->lsq_bavail;
 		ia = svr->lsq_iavail;
-		svr->lsq_penalty_per_obj = prio_wide * ba  * ia;
+		svr->lsq_penalty_per_obj = prio_wide * ba  * ia >> 8;
 		do_div(ba, svr->lsq_tgt_count * num_active);
 		svr->lsq_penalty_per_obj >>= 1;
 

lustre/tests/sanity.sh
@@ -20792,57 +20792,56 @@ test_413b() {
 	sleep_maxage
 
 	local ffree
+	local bavail
 	local max
 	local min
 	local max_index
 	local min_index
+	local tmp
 
-	ffree=($(lctl get_param -n mdc.*[mM][dD][cC]-[^M]*.filesfree | uniq))
-	echo "MDT filesfree available: ${ffree[@]}"
-	max=${ffree[0]}
-	min=${ffree[0]}
+	ffree=($(lctl get_param -n mdc.*[mM][dD][cC]-[^M]*.filesfree))
+	bavail=($(lctl get_param -n mdc.*[mM][dD][cC]-[^M]*.kbytesavail))
+	bsize=$(lctl get_param -n mdc.*MDT0000*.blocksize)
+
+	max=$(((${ffree[0]} >> 8) * (${bavail[0]} * bsize >> 16)))
+	min=$(((${ffree[0]} >> 8) * (${bavail[0]} * bsize >> 16)))
 	max_index=0
 	min_index=0
-	for ((i = 0; i < ${#ffree[@]}; i++)); do
-		if [[ ${ffree[i]} -gt $max ]]; then
-			max=${ffree[i]}
+	for ((i = 1; i < ${#ffree[@]}; i++)); do
+		tmp=$(((${ffree[i]} >> 8) * (${bavail[i]} * bsize >> 16)))
+		if [ $tmp -gt $max ]; then
+			max=$tmp
 			max_index=$i
 		fi
-		if [[ ${ffree[i]} -lt $min ]]; then
-			min=${ffree[i]}
+		if [ $tmp -lt $min ]; then
+			min=$tmp
 			min_index=$i
 		fi
 	done
-	echo "Min free files: MDT$min_index: $min"
-	echo "Max free files: MDT$max_index: $max"
 
-	[ $min -eq 0 ] && skip "no free files in MDT$min_index"
-	[ $min -gt 10000000 ] && skip "too much free files in MDT$min_index"
+	[ ${ffree[min_index]} -eq 0 ] &&
+		skip "no free files in MDT$min_index"
+	[ ${ffree[min_index]} -gt 100000000 ] &&
+		skip "too much free files in MDT$min_index"
 
 	# Check if we need to generate uneven MDTs
-	test_mkdir -i $min_index -c 1 -p $DIR/$tdir-MDT$min_index
-	local threshold=10
-	local diff=$((max - min))
-	local diff2=$((diff * 100 / min))
+	local threshold=50
+	local diff=$(((max - min ) * 100 / min))
+	local value="$(generate_string 1024)"
+	local i
 
-	echo -n "Check for uneven MDTs: "
-	echo -n "diff=$diff files ($diff2%) must be > $threshold% ..."
-
-	if [ $diff2 -gt $threshold ]; then
-		echo "ok"
-		echo "Don't need to fill MDT$min_index"
-	else
-		# generate uneven MDTs, create till 25% diff
-		echo "no"
-		diff2=$((threshold - diff2))
-		diff=$((min * diff2 / 100))
+	while [ $diff -lt $threshold ]; do
+		# generate uneven MDTs, create till $threshold% diff
+		echo -n "weight diff=$diff% must be > $threshold% ..."
+		count=$((${ffree[min_index]} / 10))
 		# 50 sec per 10000 files in vm
-		[ $diff -gt 40000 ] && [ "$SLOW" = "no" ] &&
-			skip "$diff files to create"
-		echo "Fill $diff2% diff in MDT$min_index with $diff files"
-		local i
-		local value="$(generate_string 1024)"
-		for i in $(seq $diff); do
+		[ $count -gt 40000 ] && [ "$SLOW" = "no" ] &&
+			skip "$count files to create"
+		echo "Fill MDT$min_index with $count files"
+		[ -d $DIR/$tdir-MDT$min_index ] ||
+			$LFS mkdir -i $min_index $DIR/$tdir-MDT$min_index ||
+			error "mkdir $tdir-MDT$min_index failed"
+		for i in $(seq $count); do
 			$OPENFILE -f O_CREAT:O_LOV_DELAY_CREATE \
 				$DIR/$tdir-MDT$min_index/f$i > /dev/null ||
 				error "create f$i failed"
@@ -20850,31 +20849,43 @@ test_413b() {
 				$DIR/$tdir-MDT$min_index/f$i ||
 				error "setfattr f$i failed"
 		done
-	fi
 
-	min=$((100 *MDSCOUNT))
-	max=0
+		ffree=($(lctl get_param -n mdc.*[mM][dD][cC]-*.filesfree))
+		bavail=($(lctl get_param -n mdc.*[mM][dD][cC]-*.kbytesavail))
+		max=$(((${ffree[max_index]} >> 8) * \
+			(${bavail[max_index]} * bsize >> 16)))
+		min=$(((${ffree[min_index]} >> 8) * \
+			(${bavail[min_index]} * bsize >> 16)))
+		diff=$(((max - min) * 100 / min))
+	done
+
+	echo "MDT filesfree available: ${ffree[@]}"
+	echo "MDT blocks available: ${bavail[@]}"
+	echo "weight diff=$diff%"
 
 	echo "mkdir with balanced space usage"
 	$LCTL set_param lmv.*.qos_prio_free=100
 	for i in $(seq $((100 * MDSCOUNT))); do
 		mkdir $DIR/$tdir/subdir$i || error "mkdir subdir$i failed"
 	done
+
 	for i in $(seq $MDSCOUNT); do
 		count=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$((i - 1))$ |
 			wc -w)
 		echo "$count directories created on MDT$((i - 1))"
-		[ $min -gt $count ] && min=$count
-		[ $max -lt $count ] && max=$count
 	done
-	[ $((max - min)) -gt $MDSCOUNT ] ||
+
+	max=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$max_index$ | wc -l)
+	min=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$min_index$ | wc -l)
+
+	[ $((max - min)) -lt 10 ] &&
 		error "subdirs shouldn't be evenly distributed"
 
 	which getfattr > /dev/null 2>&1 || skip_env "no getfattr command"
 
 	$LFS setdirstripe -D -d $DIR/$tdir || error "setdirstripe -d failed"
-	getfattr -n trusted.dmv $DIR/$tdir && error "default dir layout exists"
-	true
+	getfattr -n trusted.dmv $DIR/$tdir &&
+		error "default dir layout exists" || true
 }
 run_test 413b "mkdir with balanced space usage"
 