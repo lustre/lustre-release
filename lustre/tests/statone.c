3faa90830b5d43b4cbeca7fbe22a42d3bf72d72c
Arshad Hussain <arshad.super@gmail.com>
2019-09-29 22:47:06 +0530

    LU-6142 tests: Fix style issues for statone.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/statone.c
    
    Test-Parameters: trivial testlist=recovery-small
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: Idb38488116c471b8f6dbe767eaada2dc328b3d7a
    Reviewed-on: https://review.whamcloud.com/36387
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Ben Evans <bevans@cray.com>

--- lustre/tests/statone.c
+++ lustre/tests/statone.c
@@ -38,46 +38,45 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <lustre/lustreapi.h>
 
 int main(int argc, char **argv)
 {
-    char parent[4096], *base, *name, *t;
-    int fd, offset, rc;
+	char parent[4096], *base, *name, *t;
+	int fd, offset, rc;
 
-    if (argc != 2) {
-        printf("usage: %s filename\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2) {
+		printf("usage: %s filename\n", argv[0]);
+		return 1;
+	}
 
-    base = argv[1];
-    t = strrchr(base, '/');
-    if (!t) {
-        strcpy(parent, ".");
-        offset = -1;
-    } else {
-        strncpy(parent, base, t - base);
-        offset = t - base - 1;
-	parent[t - base] = 0;
-    }
+	base = argv[1];
+	t = strrchr(base, '/');
+	if (!t) {
+		strcpy(parent, ".");
+		offset = -1;
+	} else {
+		strncpy(parent, base, t - base);
+		offset = t - base - 1;
+		parent[t - base] = 0;
+	}
 
-    fd = open(parent, O_RDONLY);
-    if (fd < 0) {
-        printf("open(%s) error: %s\n", parent, strerror(errno));
-        exit(errno);
-    }
+	fd = open(parent, O_RDONLY);
+	if (fd < 0) {
+		printf("open(%s) error: %s\n", parent, strerror(errno));
+		exit(errno);
+	}
 
-    name = base;
-    if (offset >= 0)
-	name += offset + 2;
+	name = base;
+	if (offset >= 0)
+		name += offset + 2;
 
-    rc = llapi_file_lookup(fd, name);
-    if (rc < 0) {
-	printf("llapi_file_lookup (%s/%s) error: %s\n", parent,
-	       name, strerror(errno));
-        exit(errno);
-    }
+	rc = llapi_file_lookup(fd, name);
+	if (rc < 0) {
+		printf("llapi_file_lookup (%s/%s) error: %s\n", parent,
+		       name, strerror(errno));
+		exit(errno);
+	}
 
-    return 0;
+	return 0;
 }
