208424d0dde935a23e2a7bcb6863588c115e0da8
Arshad Hussain <arshad.super@gmail.com>
2019-09-29 22:33:43 +0530

    LU-6142 tests: Fix style issues for test_brw.c
    This patch fixes issues reported by checkpatch
    for file lustre/tests/test_brw.c
    
    Test-Parameters: trivial testlist=sanityn,recovery-small,recovery-single,lnet-selftest
    Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
    Change-Id: I888fa9289839dbdf6970685395ae17f4d6a28d44
    Reviewed-on: https://review.whamcloud.com/36388
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Ben Evans <bevans@cray.com>

--- lustre/tests/test_brw.c
+++ lustre/tests/test_brw.c
@@ -55,210 +55,213 @@ int block_debug_setup(void *addr, int len, uint64_t off, uint64_t id)
 {
 	off = htole64(off);
 	id = htole64(id);
-        memcpy(addr, (char *)&off, LPDS);
-        memcpy(addr + LPDS, (char *)&id, LPDS);
+	memcpy(addr, (char *)&off, LPDS);
+	memcpy(addr + LPDS, (char *)&id, LPDS);
 
-        addr += len - LPDS - LPDS;
-        memcpy(addr, (char *)&off, LPDS);
-        memcpy(addr + LPDS, (char *)&id, LPDS);
+	addr += len - LPDS - LPDS;
+	memcpy(addr, (char *)&off, LPDS);
+	memcpy(addr + LPDS, (char *)&id, LPDS);
 
-        return 0;
+	return 0;
 }
 
-int block_debug_check(char *who, void *addr, int size, uint64_t off, uint64_t id)
+int block_debug_check(char *who, void *addr, int size, uint64_t off,
+		      uint64_t id)
 {
 	uint64_t ne_off;
-        int err = 0;
+	int err = 0;
 
 	ne_off = le64toh(off);
 	id = le64toh(id);
-        if (memcmp(addr, (char *)&ne_off, LPDS)) {
+	if (memcmp(addr, (char *)&ne_off, LPDS)) {
 		fprintf(stderr, "%s: for offset %"PRIu64" off: %"PRIx64" != %"PRIx64"\n",
 			who, off, *(uint64_t *)addr, ne_off);
-                err = -EINVAL;
-        }
-        if (memcmp(addr + LPDS, (char *)&id, LPDS)) {
+		err = -EINVAL;
+	}
+	if (memcmp(addr + LPDS, (char *)&id, LPDS)) {
 		fprintf(stderr, "%s: for offset %"PRIu64" id: %"PRIx64" != %"PRIx64"\n",
 			who, off, *(uint64_t *)(addr + LPDS), id);
-                err = -EINVAL;
-        }
+		err = -EINVAL;
+	}
 
-        addr += size - LPDS - LPDS;
-        if (memcmp(addr, (char *)&ne_off, LPDS)) {
+	addr += size - LPDS - LPDS;
+	if (memcmp(addr, (char *)&ne_off, LPDS)) {
 		fprintf(stderr, "%s: for offset %"PRIu64" end off: %"PRIx64" != %"PRIx64"\n",
 			who, off, *(uint64_t *)addr, ne_off);
-                err = -EINVAL;
-        }
-        if (memcmp(addr + LPDS, (char *)&id, LPDS)) {
+		err = -EINVAL;
+	}
+	if (memcmp(addr + LPDS, (char *)&id, LPDS)) {
 		fprintf(stderr, "%s: for offset %"PRIu64" end id: %"PRIx64" != %"PRIx64"\n",
 			who, off, *(uint64_t *)(addr + LPDS), id);
-                err = -EINVAL;
-        }
+		err = -EINVAL;
+	}
 
-        return err;
+	return err;
 }
+
 #undef LPDS
 
 void usage(char *prog)
 {
-        fprintf(stderr,
-                "usage: %s file count [[d]{r|w|rw} [pages_per_vec [objid]]]\n",
-                prog);
-        exit(1);
+	fprintf(stderr,
+		"usage: %s file count [[d]{r|w|rw} [pages_per_vec [objid]]]\n",
+		prog);
+	exit(1);
 }
 
 int main(int argc, char **argv)
 {
-        int fd;
-        char *buf;
+	int fd;
+	char *buf;
 	long long count, last;
-        long pg_vec, len;
+	long pg_vec, len;
 	uint64_t objid, offset;
-        struct stat st;
-        int flags = 0;
-        int cmd = 0;
-        char *end;
-        int rc;
+	struct stat st;
+	int flags = 0;
+	int cmd = 0;
+	char *end;
+	int rc;
 
-        if (argc < 3 || argc > 6)
-                usage(argv[0]);
+	if (argc < 3 || argc > 6)
+		usage(argv[0]);
 
-        count = strtoull(argv[2], &end, 0);
-        if (*end) {
-                fprintf(stderr, "%s: invalid count '%s'\n", argv[0], argv[2]);
-                usage(argv[0]);
-        }
-        if (argc >= 4) {
-                if (strchr(argv[3], 'r')) {
-                        cmd = READ;
-                        flags = O_RDONLY;
-                }
-                if (strchr(argv[3], 'w')) {
-                        cmd |= WRITE;
-                        flags = O_RDWR | O_CREAT;
-                }
-                if (strchr(argv[3], 'd')) {
+	count = strtoull(argv[2], &end, 0);
+	if (*end) {
+		fprintf(stderr, "%s: invalid count '%s'\n", argv[0], argv[2]);
+		usage(argv[0]);
+	}
+	if (argc >= 4) {
+		if (strchr(argv[3], 'r')) {
+			cmd = READ;
+			flags = O_RDONLY;
+		}
+		if (strchr(argv[3], 'w')) {
+			cmd |= WRITE;
+			flags = O_RDWR | O_CREAT;
+		}
+		if (strchr(argv[3], 'd')) {
 #ifdef O_DIRECT
-                        flags |= O_DIRECT;
+			flags |= O_DIRECT;
 #else
-                        fprintf(stderr,
-                                "%s: O_DIRECT not supported in this build\n",
-                                argv[0]);
-                        exit(1);
+			fprintf(stderr,
+				"%s: O_DIRECT not supported in this build\n",
+				argv[0]);
+			exit(1);
 #endif
-                }
-                if (!cmd)
-                        usage(argv[0]);
-        } else {
-                cmd = READ | WRITE;
-                flags = O_RDWR | O_CREAT;
+		}
+		if (!cmd)
+			usage(argv[0]);
+	} else {
+		cmd = READ | WRITE;
+		flags = O_RDWR | O_CREAT;
 #ifdef O_DIRECT
-                flags |= O_DIRECT;
+		flags |= O_DIRECT;
 #else
-                fprintf(stderr, "%s: warning: not setting O_DIRECT\n",
-                        argv[0]);
+		fprintf(stderr, "%s: warning: not setting O_DIRECT\n",
+			argv[0]);
 #endif
-        }
+	}
 
-        if (argc >= 5) {
-                pg_vec = strtoul(argv[4], &end, 0);
-                if (*end) {
-                        fprintf(stderr, "%s: invalid pages_per_vec '%s'\n",
-                                argv[0], argv[4]);
-                        usage(argv[0]);
-                }
-        } else {
-                pg_vec = 16;
-        }
+	if (argc >= 5) {
+		pg_vec = strtoul(argv[4], &end, 0);
+		if (*end) {
+			fprintf(stderr, "%s: invalid pages_per_vec '%s'\n",
+				argv[0], argv[4]);
+			usage(argv[0]);
+		}
+	} else {
+		pg_vec = 16;
+	}
 
-        if (argc >= 6) {
-                objid = strtoull(argv[5], &end, 0);
-                if (*end) {
-                        fprintf(stderr, "%s: invalid objid '%s'\n",
-                                argv[0], argv[5]);
-                        usage(argv[0]);
-                }
-        } else {
-                objid = 3;
-        }
+	if (argc >= 6) {
+		objid = strtoull(argv[5], &end, 0);
+		if (*end) {
+			fprintf(stderr, "%s: invalid objid '%s'\n",
+				argv[0], argv[5]);
+			usage(argv[0]);
+		}
+	} else {
+		objid = 3;
+	}
 
-	printf("%s: %s on %s(objid %"PRIx64") for %llux%ld pages \n",
-               argv[0],
+	printf("%s: %s on %s(objid %"PRIx64") for %llux%ld pages\n",
+	       argv[0],
 #ifdef O_DIRECT
-               flags & O_DIRECT ? "directio" : "i/o",
+	       flags & O_DIRECT ? "directio" : "i/o",
 #else
-               "i/o",
+	       "i/o",
 #endif
-               argv[1], objid, count, pg_vec);
+	       argv[1], objid, count, pg_vec);
 
-        fd = open(argv[1], flags | O_LARGEFILE);
-        if (fd == -1) {
-                fprintf(stderr, "%s: cannot open %s:  %s\n", argv[0],
-                        argv[1], strerror(errno));
-                return 3;
-        }
+	fd = open(argv[1], flags | O_LARGEFILE);
+	if (fd == -1) {
+		fprintf(stderr, "%s: cannot open %s:  %s\n", argv[0],
+			argv[1], strerror(errno));
+		return 3;
+	}
 
-        rc = fstat(fd, &st);
-        if (rc < 0) {
-                fprintf(stderr, "%s: cannot stat %s: %s\n", argv[0],
-                        argv[1], strerror(errno));
-                return 4;
-        }
+	rc = fstat(fd, &st);
+	if (rc < 0) {
+		fprintf(stderr, "%s: cannot stat %s: %s\n", argv[0],
+			argv[1], strerror(errno));
+		return 4;
+	}
 
-        len = pg_vec * st.st_blksize;
-        last = (long long)count * len;
+	len = pg_vec * st.st_blksize;
+	last = (long long)count * len;
 
-        buf = mmap(0, len, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, 0, 0);
-        if (buf == MAP_FAILED) {
-                fprintf(stderr, "%s: no buffer memory %s\n",
-                        argv[0], strerror(errno));
-                return 2;
-        }
+	buf = mmap(0, len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON,
+		   0, 0);
+	if (buf == MAP_FAILED) {
+		fprintf(stderr, "%s: no buffer memory %s\n",
+			argv[0], strerror(errno));
+		return 2;
+	}
 
-        for (offset = 0; offset < last && cmd & WRITE; offset += len) {
+	for (offset = 0; offset < last && cmd & WRITE; offset += len) {
 		unsigned int i;
 
-                for (i = 0; i < len; i += st.st_blksize)
+		for (i = 0; i < len; i += st.st_blksize)
 			block_debug_setup(buf + i, st.st_blksize,
-                                          offset + i, objid);
+					  offset + i, objid);
 
-                rc = write(fd, buf, len);
+		rc = write(fd, buf, len);
 
-                for (i = 0; i < len; i += st.st_blksize) {
-                        if (block_debug_check("write", buf + i, st.st_blksize,
-                                              offset + i, objid))
-                                return 10;
-                }
+		for (i = 0; i < len; i += st.st_blksize) {
+			if (block_debug_check("write", buf + i, st.st_blksize,
+					      offset + i, objid))
+				return 10;
+		}
 
-                if (rc != len) {
-                        fprintf(stderr, "%s: write error: %s, rc %d != %ld\n",
-                                argv[0], strerror(errno), rc, len);
-                        return 4;
-                }
-        }
+		if (rc != len) {
+			fprintf(stderr, "%s: write error: %s, rc %d != %ld\n",
+				argv[0], strerror(errno), rc, len);
+			return 4;
+		}
+	}
 
-        if (lseek(fd, 0, SEEK_SET) != 0) {
-                fprintf(stderr, "%s: cannot seek %s\n",
-                        argv[0], strerror(errno));
-                return 5;
-        }
+	if (lseek(fd, 0, SEEK_SET) != 0) {
+		fprintf(stderr, "%s: cannot seek %s\n",
+			argv[0], strerror(errno));
+		return 5;
+	}
 
-        for (offset = 0; offset < last && cmd & READ; offset += len) {
-                int i;
+	for (offset = 0; offset < last && cmd & READ; offset += len) {
+		int i;
 
-                rc = read(fd, buf, len);
-                if (rc != len) {
-                        fprintf(stderr, "%s: read error: %s, rc %d != %ld\n",
-                                argv[0], strerror(errno), rc, len);
-                        return 6;
-                }
+		rc = read(fd, buf, len);
+		if (rc != len) {
+			fprintf(stderr, "%s: read error: %s, rc %d != %ld\n",
+				argv[0], strerror(errno), rc, len);
+			return 6;
+		}
 
-                for (i = 0; i < len; i += st.st_blksize) {
-                        if (block_debug_check("read", buf + i, st.st_blksize,
-                                              offset + i, objid))
-                                return 11;
-                }
-        }
+		for (i = 0; i < len; i += st.st_blksize) {
+			if (block_debug_check("read", buf + i, st.st_blksize,
+					      offset + i, objid))
+				return 11;
+		}
+	}
 
-        return 0;
+	return 0;
 }
