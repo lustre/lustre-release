75582dbeced0a905bd81530c5a10e2dea4e53bd2
James Simmons <jsimmons@infradead.org>
2019-10-18 09:31:00 -0400

    LU-12803 libcfs: bump module version
    The linux client version of libcfs is further ahead in its
    cleanup so its module version is higher. While this is the
    case it does prevent the OpenSFS version of libcfs from
    loading and since OpenSFS is current ahead of the linux
    client we prefere to use it at this time. Lets just increase
    the OpenSFS libcfs module to be just slightly ahead of the
    linux client.
    
    Test-Parameters: trivial
    
    Lustre-change: https://review.whamcloud.com/36488
    Lustre-commit: 4b25d733342bc6f3a424ecfb0db80f1c175a8986
    
    Change-Id: Ie57d93529bf25d908099f7dab06d2960f9923d58
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Signed-off-by: Minh Diep <mdiep@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/36642
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- libcfs/include/libcfs/libcfs.h
+++ libcfs/include/libcfs/libcfs.h
@@ -52,7 +52,7 @@
 #include <libcfs/libcfs_fail.h>
 #include "curproc.h"
 
-#define LIBCFS_VERSION	"0.5.0"
+#define LIBCFS_VERSION	"0.7.1"
 
 #define PO2_ROUNDUP_TYPED(x, po2, type) (-(-(type)(x) & -(type)(po2)))
 #define LOWEST_BIT_SET(x) ((x) & ~((x) - 1))
