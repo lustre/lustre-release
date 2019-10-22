lustre/lustre-release

commit 3ec712bd183a859a7bb09280b8a5a1776ec5e2c2

LU-11868 osd: Set max ea size to XATTR_SIZE_MAX
Lustre currently limits EA size to either ~1 MiB (ldiskfs)
or 32K (ZFS).  VFS has its own limit, XATTR_SIZE_MAX,
which we must respect to interoperate correctly with
userspace tools like tar, getattr, and the getxattr()
syscall.

Set this as the new max EA size for both ldiskfs and ZFS.

(The current 32K on ZFS is too small for
LOV_MAX_STRIPE_COUNT [2000] files, so needs to be raised
regardless.)

In order to use this correctly, we have to use the real ea
size on the client.  The previous code for maximum ea size
on the client (KEY_MAX_EASIZE, llite.max_easize) used a
calculated value based on number of targets.

With one exception, the mdc code already uses the default
ea size rather than the max.  Default ea size adjusts
automatically to the largest size sent by the server.

The exception is the open code, which uses the max so it
never has to resend a layout request.  This patch changes
it to use default, which means that the first time a very
widely striped file is opened, the open will be resent.

Add limit checks on client & server so the xattr size limit
is honored.

Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I4da62691f30fa276d20959810116cf558cccc515
Reviewed-on: https://review.whamcloud.com/34058
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/conf-sanity.sh
@@ -3494,11 +3494,11 @@ test_48() { # bz-17636 LU-7473
 	then
 		count=28	# hard coded of RPC protocol
 	elif [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
-		count=4000	# max_num 4091 max_ea_size = 32768
+		count=4000	# max_num 4091 max_ea_size = ~65536
 	elif ! large_xattr_enabled; then
 		count=450	# max_num 497 max_ea_size = 4012
 	else
-		count=4500	# max_num 8187 max_ea_size = 1048492
+		count=4500	# max_num 8187 max_ea_size = 65452
 				# not create too much (>5000) to save test time
 	fi
 
@@ -5847,8 +5847,23 @@ test_81() { # LU-4665
 
 	# Check max_easize.
 	local max_easize=$($LCTL get_param -n llite.*.max_easize)
-	[[ $max_easize -eq 128 ]] ||
-		error "max_easize is $max_easize, should be 128 bytes"
+	if [ $MDS1_VERSION -lt $(version_code 2.12.51) ]
+	then
+		[[ $max_easize -eq 128 ]] ||
+			error "max_easize is $max_easize, should be 128 bytes"
+	else
+		# LU-11868
+		# 4012 is 4096 - ldiskfs ea overhead
+		[[ $max_easize -ge 4012 ]] ||
+		error "max_easize is $max_easize, should be at least 4012 bytes"
+
+		# 65452 is XATTR_SIZE_MAX - ldiskfs ea overhead
+		if large_xattr_enabled;
+		then
+			[[ $max_easize -ge 65452 ]] ||
+			error "max_easize is $max_easize, should be at least 65452 bytes"
+		fi
+	fi
 
 	restore_ostindex
 }

lustre/tests/sanity.sh
@@ -8355,11 +8355,19 @@ grow_xattr() {
 	local file=$DIR/$tfile
 	local value="$(generate_string $xsize)"
 	local xbig=trusted.big
+	local toobig=$2
 
 	touch $file
 	log "save $xbig on $file"
-	setfattr -n $xbig -v $value $file ||
-		error "saving $xbig on $file failed"
+	if [ -z "$toobig" ]
+	then
+		setfattr -n $xbig -v $value $file ||
+			error "saving $xbig on $file failed"
+	else
+		setfattr -n $xbig -v $value $file &&
+			error "saving $xbig on $file succeeded"
+		return 0
+	fi
 
 	local orig=$(get_xattr_value $xbig $file)
 	[[ "$orig" != "$value" ]] && error "$xbig different after saving $xbig"
@@ -8390,7 +8398,12 @@ run_test 102h "grow xattr from inside inode to external block"
 test_102ha() {
 	large_xattr_enabled || skip_env "ea_inode feature disabled"
 
+	echo "setting xattr of max xattr size: $(max_xattr_size)"
 	grow_xattr $(max_xattr_size)
+
+	echo "setting xattr of > max xattr size: $(max_xattr_size) + 10"
+	echo "This should fail:"
+	grow_xattr $(($(max_xattr_size) + 10)) 1
 }
 run_test 102ha "grow xattr from inside inode to external inode"
 

lustre/tests/test-framework.sh
@@ -8202,8 +8202,7 @@ max_xattr_size() {
     local size
 
     if large_xattr_enabled; then
-        # include/linux/limits.h: #define XATTR_SIZE_MAX 65536
-        size=65536
+	size=$($LCTL get_param -n llite.*.max_easize)
     else
         local mds_dev=$(mdsdevname ${SINGLEMDS//mds/})
         local block_size=$(get_block_size $SINGLEMDS $mds_dev)

lustre/include/obd.h
@@ -151,7 +151,15 @@ enum {
  * vmalloc(). Excessive use of vmalloc() may cause spinlock contention
  * on the MDS.
  */
-#define OBD_MAX_DEFAULT_EA_SIZE		4096
+#define OBD_MAX_DEFAULT_EA_SIZE	4096
+
+/*
+ * Lustre can handle larger xattrs internally, but we must respect the Linux
+ * VFS limitation or tools like tar cannot interact with Lustre volumes
+ * correctly.
+ */
+#define OBD_MAX_EA_SIZE		XATTR_SIZE_MAX
+
 
 enum obd_cl_sem_lock_class {
 	OBD_CLI_SEM_NORMAL,

lustre/llite/llite_lib.c
@@ -677,11 +677,15 @@ int ll_get_max_mdsize(struct ll_sb_info *sbi, int *lmmsize)
 		RETURN(rc);
 	}
 
+	CDEBUG(D_INFO, "max LOV ea size: %d\n", *lmmsize);
+
 	size = sizeof(int);
 	rc = obd_get_info(NULL, sbi->ll_md_exp, sizeof(KEY_MAX_EASIZE),
 			  KEY_MAX_EASIZE, &size, lmmsize);
 	if (rc)
 		CERROR("Get max mdsize error rc %d\n", rc);
+
+	CDEBUG(D_INFO, "max LMV ea size: %d\n", *lmmsize);
 
 	RETURN(rc);
 }

lustre/lov/lov_obd.c
@@ -1163,10 +1163,7 @@ static int lov_get_info(const struct lu_env *env, struct obd_export *exp,
 	lov_tgts_getref(obddev);
 
 	if (KEY_IS(KEY_MAX_EASIZE)) {
-		u32 max_stripe_count = min_t(u32, ld->ld_active_tgt_count,
-					     LOV_MAX_STRIPE_COUNT);
-
-		*((u32 *)val) = lov_mds_md_size(max_stripe_count, LOV_MAGIC_V3);
+		*((u32 *)val) = exp->exp_connect_data.ocd_max_easize;
 	} else if (KEY_IS(KEY_DEFAULT_EASIZE)) {
 		u32 def_stripe_count = min_t(u32, ld->ld_default_stripe_count,
 					     LOV_MAX_STRIPE_COUNT);

lustre/mdc/mdc_locks.c
@@ -256,6 +256,7 @@ mdc_intent_open_pack(struct obd_export *exp, struct lookup_intent *it,
 	struct ldlm_intent	*lit;
 	const void		*lmm = op_data->op_data;
 	__u32			 lmmsize = op_data->op_data_size;
+	__u32			 mdt_md_capsule_size;
 	struct list_head	 cancels = LIST_HEAD_INIT(cancels);
 	int			 count = 0;
 	enum ldlm_mode		 mode;
@@ -263,6 +264,8 @@ mdc_intent_open_pack(struct obd_export *exp, struct lookup_intent *it,
 	int repsize, repsize_estimate;
 
 	ENTRY;
+
+	mdt_md_capsule_size = obddev->u.cli.cl_default_mds_easize;
 
 	it->it_create_mode = (it->it_create_mode & ~S_IFMT) | S_IFREG;
 
@@ -352,7 +355,7 @@ mdc_intent_open_pack(struct obd_export *exp, struct lookup_intent *it,
                       lmmsize);
 
 	req_capsule_set_size(&req->rq_pill, &RMF_MDT_MD, RCL_SERVER,
-			     obddev->u.cli.cl_max_mds_easize);
+			     mdt_md_capsule_size);
 	req_capsule_set_size(&req->rq_pill, &RMF_ACL, RCL_SERVER, acl_bufsize);
 
 	if (!(it->it_op & IT_CREAT) && it->it_op & IT_OPEN &&
@@ -391,7 +394,7 @@ mdc_intent_open_pack(struct obd_export *exp, struct lookup_intent *it,
 				      lustre_msg_early_size());
 	/* Estimate free space for DoM files in repbuf */
 	repsize_estimate = repsize - (req->rq_replen -
-			   obddev->u.cli.cl_max_mds_easize +
+			   mdt_md_capsule_size +
 			   sizeof(struct lov_comp_md_v1) +
 			   sizeof(struct lov_comp_md_entry_v1) +
 			   lov_mds_md_size(0, LOV_MAGIC_V3));
@@ -546,10 +549,7 @@ mdc_intent_getattr_pack(struct obd_export *exp, struct lookup_intent *it,
 	lit = req_capsule_client_get(&req->rq_pill, &RMF_LDLM_INTENT);
 	lit->opc = (__u64)it->it_op;
 
-	if (obddev->u.cli.cl_default_mds_easize > 0)
-		easize = obddev->u.cli.cl_default_mds_easize;
-	else
-		easize = obddev->u.cli.cl_max_mds_easize;
+	easize = obddev->u.cli.cl_default_mds_easize;
 
 	/* pack the intended request */
 	mdc_getattr_pack(req, valid, it->it_flags, op_data, easize);

lustre/mdt/mdt_lib.c
@@ -1162,6 +1162,7 @@ static int mdt_setattr_unpack(struct mdt_thread_info *info)
                 rr->rr_eadata = req_capsule_client_get(pill, &RMF_EADATA);
                 rr->rr_eadatalen = req_capsule_get_size(pill, &RMF_EADATA,
                                                         RCL_CLIENT);
+
 		if (rr->rr_eadatalen > 0) {
 			const struct lmv_user_md	*lum;
 
@@ -1503,6 +1504,7 @@ static int mdt_migrate_unpack(struct mdt_thread_info *info)
 			rr->rr_eadatalen = req_capsule_get_size(pill,
 								&RMF_EADATA,
 								RCL_CLIENT);
+
 			if (rr->rr_eadatalen > 0) {
 				rr->rr_eadata = req_capsule_client_get(pill,
 								&RMF_EADATA);
@@ -1591,6 +1593,7 @@ static int mdt_open_unpack(struct mdt_thread_info *info)
         if (req_capsule_field_present(pill, &RMF_EADATA, RCL_CLIENT)) {
                 rr->rr_eadatalen = req_capsule_get_size(pill, &RMF_EADATA,
                                                         RCL_CLIENT);
+
                 if (rr->rr_eadatalen > 0) {
                         rr->rr_eadata = req_capsule_client_get(pill,
                                                                &RMF_EADATA);
@@ -1662,6 +1665,10 @@ static int mdt_setxattr_unpack(struct mdt_thread_info *info)
         if (req_capsule_field_present(pill, &RMF_EADATA, RCL_CLIENT)) {
                 rr->rr_eadatalen = req_capsule_get_size(pill, &RMF_EADATA,
                                                         RCL_CLIENT);
+
+		if (rr->rr_eadatalen > info->mti_mdt->mdt_max_ea_size)
+			RETURN(-E2BIG);
+
                 if (rr->rr_eadatalen > 0) {
                         rr->rr_eadata = req_capsule_client_get(pill,
                                                                &RMF_EADATA);

commit 84097792f56c38265b6eb0417a47c557f81f9461

LU-12784 llite: limit max xattr size by kernel value
Limit the maximum xattr size returned to userspace from the MDS to
what the currently-running kernel supports (XATTR_SIZE_MAX=65536
bytes typically).  While it is possible a Lustre backing filesystem
may store larger xattrs than this, it wouldn't be possible for users
to access a larger xattr via kernel xattr interfaces.

This fixes interop problems when newer clients and tests are running
against older servers:

  sanity.sh: line 8946: /usr/bin/setfattr: Argument list too long

Skip subtests for new features in 2.13 so 2.12 interop testing passes.

Fix test-framework.sh::large_xattr_enabled() to return true for ZFS.
Fix test-framework.sh::max_xattr_size() to return the actual value
returned from the MDS rather than computing it locally.

Fixes: 3ec712bd183 ("LU-11868 osd: Set max ea size to XATTR_SIZE_MAX")
Test-Parameters: trivial serverversion=2.12 testlist=sanity
Test-Parameters: serverversion=2.12 testlist=conf-sanity envdefinitions=ONLY=81
Test-Parameters: testlist=sanity-pfl,replay-single
Test-Parameters: testlist=conf-sanity envdefinitions=ONLY=48,61,81
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I14232809b13886efa8f11a50ecc35e78f316810d
Reviewed-on: https://review.whamcloud.com/36240
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/llite/lproc_llite.c
@@ -918,7 +918,9 @@ static ssize_t max_easize_show(struct kobject *kobj,
 	if (rc)
 		return rc;
 
-	return sprintf(buf, "%u\n", ealen);
+	/* Limit xattr size returned to userspace based on kernel maximum */
+	return snprintf(buf, PAGE_SIZE, "%u\n",
+			ealen > XATTR_SIZE_MAX ? XATTR_SIZE_MAX : ealen);
 }
 LUSTRE_RO_ATTR(max_easize);
 
@@ -946,7 +948,9 @@ static ssize_t default_easize_show(struct kobject *kobj,
 	if (rc)
 		return rc;
 
-	return sprintf(buf, "%u\n", ealen);
+	/* Limit xattr size returned to userspace based on kernel maximum */
+	return snprintf(buf, PAGE_SIZE, "%u\n",
+			ealen > XATTR_SIZE_MAX ? XATTR_SIZE_MAX : ealen);
 }
 
 /**

lustre/tests/conf-sanity.sh
@@ -3506,16 +3506,13 @@ test_48() { # bz-17636 LU-7473
 	# LOV EA, and so on. These EA will use some EA space that is shared by
 	# ACL entries. So here we only check some reasonable ACL entries count,
 	# instead of the max number that is calculated from the max_ea_size.
-	if [ "$MDS1_VERSION" -lt $(version_code 2.8.57) ];
-	then
+	if [ "$MDS1_VERSION" -lt $(version_code 2.8.57) ]; then
 		count=28	# hard coded of RPC protocol
-	elif [ "$mds1_FSTYPE" != ldiskfs ]; then
-		count=4000	# max_num 4091 max_ea_size = ~65536
-	elif ! large_xattr_enabled; then
-		count=450	# max_num 497 max_ea_size = 4012
-	else
+	elif large_xattr_enabled; then
 		count=4500	# max_num 8187 max_ea_size = 65452
-				# not create too much (>5000) to save test time
+				# not create too many (4500) to save test time
+	else
+		count=450	# max_num 497 max_ea_size = 4012
 	fi
 
 	echo "It is expected to hold at least $count ACL entries"
@@ -5859,22 +5856,15 @@ test_81() { # LU-4665
 
 	# Check max_easize.
 	local max_easize=$($LCTL get_param -n llite.*.max_easize)
-	if [ $MDS1_VERSION -lt $(version_code 2.12.51) ]
-	then
-		[[ $max_easize -eq 128 ]] ||
-			error "max_easize is $max_easize, should be 128 bytes"
+	# 65452 is XATTR_SIZE_MAX less ldiskfs ea overhead
+	if large_xattr_enabled; then
+		[[ $max_easize -ge 65452 ]] ||
+			error "max_easize is $max_easize, should be at least 65452 bytes"
 	else
 		# LU-11868
-		# 4012 is 4096 - ldiskfs ea overhead
+		# 4012 is 4096 less ldiskfs ea overhead
 		[[ $max_easize -ge 4012 ]] ||
-		error "max_easize is $max_easize, should be at least 4012 bytes"
-
-		# 65452 is XATTR_SIZE_MAX - ldiskfs ea overhead
-		if large_xattr_enabled;
-		then
-			[[ $max_easize -ge 65452 ]] ||
-			error "max_easize is $max_easize, should be at least 65452 bytes"
-		fi
+			error "max_easize is $max_easize, should be at least 4012 bytes"
 	fi
 
 	restore_ostindex

lustre/tests/sanity-flr.sh
@@ -2628,7 +2628,7 @@ run_test 204c "FLR write/stale/resync test with component removal"
 test_204d() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
 	[ "$MDS1_VERSION" -lt $(version_code $SEL_VER) ] &&
-		skip "skipped for lustre < $SEL_VERSION"
+		skip "skipped for lustre < $SEL_VER"
 
 	local comp_file=$DIR/$tdir/$tfile
 	local found=""
@@ -2697,7 +2697,7 @@ run_test 204d "FLR write/stale/resync sel test with repeated comp"
 test_204e() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
 	[ "$MDS1_VERSION" -lt $(version_code $SEL_VER) ] &&
-		skip "skipped for lustre < $SEL_VERSION"
+		skip "skipped for lustre < $SEL_VER"
 
 	local comp_file=$DIR/$tdir/$tfile
 	local found=""
@@ -2778,7 +2778,7 @@ run_test 204e "FLR write/stale/resync sel test with repeated comp"
 test_204f() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
 	[ "$MDS1_VERSION" -lt $(version_code $SEL_VER) ] &&
-		skip "skipped for lustre < $SEL_VERSION"
+		skip "skipped for lustre < $SEL_VER"
 
 	local comp_file=$DIR/$tdir/$tfile
 	local found=""

lustre/tests/sanity.sh
@@ -2468,11 +2468,11 @@ test_27D() {
 	[ $MDS1_VERSION -lt $(version_code 2.9.55) ] ||
 		[ $CLIENT_VERSION -lt $(version_code 2.9.55) ] &&
 			skip27D+=" -s 30,31"
-	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code $SEL_VER) ] &&
-		skip27D+="-s 32"
 	[[ ! $($LCTL get_param mdc.*.import) =~ connect_flags.*overstriping ||
 	  $OSTCOUNT -ge $(($LOV_MAX_STRIPE_COUNT / 2)) ]] &&
 		skip27D+=" -s 32,33"
+	[[ $MDS_VERSION -lt $(version_code $SEL_VER) ]] &&
+		skip27D+=" -s 34"
 	llapi_layout_test -d$DIR/$tdir -p$POOL -o$OSTCOUNT $skip27D ||
 		error "llapi_layout_test failed"
 
@@ -2612,6 +2612,8 @@ run_test 27H "Set specific OSTs stripe"
 test_27I() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 	[[ $OSTCOUNT -lt 2 ]] && skip_env "needs >= 2 OSTs"
+	[[ $MDS1_VERSION -gt $(version_code 2.12.52) ]] ||
+		skip "Need MDS version newer than 2.12.52"
 	local pool=$TESTNAME
 	local ostrange="1 1 1"
 
@@ -5857,6 +5859,8 @@ test_56r() {
 run_test 56r "check lfs find -size works"
 
 test_56ra() {
+	[[ $MDS1_VERSION -gt $(version_code 2.12.58) ]] ||
+		skip "MDS < 2.12.58 doesn't return LSOM data"
 	local dir=$DIR/$tdir
 
 	[[ $OSC == "mdc" ]] && skip "DoM files" && return
@@ -22144,6 +22148,8 @@ run_test 809 "Verify no SOM xattr store for DoM-only files"
 test_810() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 	$GSS && skip_env "could not run with gss"
+	[[ $OST1_VERSION -gt $(version_code 2.12.58) ]] ||
+		skip "OST < 2.12.58 doesn't align checksum"
 
 	set_checksums 1
 	stack_trap "set_checksums $ORIG_CSUM" EXIT

lustre/tests/test-framework.sh
@@ -8475,7 +8475,7 @@ get_block_size() {
 # ldiskfs xattrs over one block in size.  Allow both the historical
 # Lustre feature name (large_xattr) and the upstream name (ea_inode).
 large_xattr_enabled() {
-	[[ $(facet_fstype $SINGLEMDS) == zfs ]] && return 0
+	[[ $(facet_fstype $SINGLEMDS) == zfs ]] && return 1
 
 	local mds_dev=$(mdsdevname ${SINGLEMDS//mds/})
 
@@ -8486,20 +8486,7 @@ large_xattr_enabled() {
 
 # Get the maximum xattr size supported by the filesystem.
 max_xattr_size() {
-    local size
-
-    if large_xattr_enabled; then
-	size=$($LCTL get_param -n llite.*.max_easize)
-    else
-        local mds_dev=$(mdsdevname ${SINGLEMDS//mds/})
-        local block_size=$(get_block_size $SINGLEMDS $mds_dev)
-
-        # maximum xattr size = size of block - size of header -
-        #                      size of 1 entry - 4 null bytes
-        size=$((block_size - 32 - 32 - 4))
-    fi
-
-    echo $size
+	$LCTL get_param -n llite.*.max_easize
 }
 
 # Dump the value of the named xattr from a file.
@@ -10262,8 +10249,8 @@ verify_comp_at_zero() {
 		error "No component starting at zero(!)"
 }
 
-#TODO: This version is a placeholder, to be replaced before final commit
-SEL_VER="2.12.52"
+# version after which Self-Extending Layouts are available
+SEL_VER="2.12.55"
 
 sel_layout_sanity() {
 	local file=$1