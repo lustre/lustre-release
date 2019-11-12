lustre/lustre-release

commit e41bdca755954beeb47ee6653ed9e140b7338e96

LU-11071 build: Add server build support for Ubuntu 18.04
This enables server build for Ubuntu 18.04 LTS, the ldiskfs
patches are based on Gael's 4.12 support,
they apply to kernel versions 4.15.0-20.21 to 4.15.0-23.25

There's also a small fix to make dpkg happy when installing
lustre packages which requires lustre-client-utils.

Test-Parameters: clientdistro=ubuntu1604 trivial
Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Signed-off-by: Gael Delbary <gael.delbary@cea.fr>
Change-Id: I65e1a5ee0d17115f23ba071ff1ab23b4fb22e78f
Reviewed-on: https://review.whamcloud.com/32613
Tested-by: Jenkins
Reviewed-by: Quentin Bouget <quentin.bouget@cea.fr>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2537,6 +2537,24 @@ generic_write_sync_2args, [
 ]) # LC_GENERIC_WRITE_SYNC_2ARGS
 
 #
+# LC_FOP_ITERATE_SHARED
+#
+# Kernel version 4.7 adds iterate_shared method to file_operations
+#
+AC_DEFUN([LC_FOP_ITERATE_SHARED], [
+LB_CHECK_COMPILE([if 'file_operations' has 'iterate_shared'],
+fop_iterate_shared, [
+	#include <linux/fs.h>
+],[
+	struct file_operations fop;
+	fop.iterate_shared = NULL;
+],[
+	AC_DEFINE(HAVE_FOP_ITERATE_SHARED, 1,
+		[file_operations has iterate_shared])
+])
+]) # LC_FOP_ITERATE_SHARED
+
+#
 # LC_FOPS_ITERATE_SHARED
 #
 # 4.7 commit ae05327a00fd47c34dfe25294b359a3f3fef96e8

autoMakefile.am
@@ -282,8 +282,11 @@ debs: undef.h
 	if test "x@ENABLEO2IB@" = "xno"; then \
 		export IB_OPTIONS="--with-o2ib=no"; \
 	else \
-		if test -n "@O2IBPATH@"; then \
+		if test "x@ENABLEO2IB@" != "xyes" && \
+		   test -n "@O2IBPATH@"; then \
 			export IB_OPTIONS="--with-o2ib=@O2IBPATH@"; \
+		else \
+			export IB_OPTIONS="--with-o2ib=yes"; \
 		fi; \
 	fi; \
 	export KSRC_TREE=$(LINUX) && \

config/lustre-build-ldiskfs.m4
@@ -56,23 +56,33 @@ AS_IF([test x$RHEL_KERNEL = xyes], [
 	)], [LDISKFS_SERIES="4.4-sles12sp3.series"],
             [LDISKFS_SERIES="4.4-sles12sp3.series"])
 ], [test x$UBUNTU_KERNEL = xyes], [
-	AS_VERSION_COMPARE([$LINUXRELEASE],[4.4.0],
-		[],
-		[
-			KPLEV=$(echo $LINUXRELEASE | sed -n 's/.*-\([0-9]\+\).*/\1/p')
-			AS_IF(
-				[test -z "$KPLEV"], [
-					AC_MSG_WARN([Failed to determine Kernel patch level. Assume latest.])
-					LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"
-				],
-				[test $KPLEV -ge 73], [LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"],
-				[test $KPLEV -ge 62], [LDISKFS_SERIES="4.4.0-62-ubuntu14+16.series"],
-				[test $KPLEV -ge 49], [LDISKFS_SERIES="4.4.0-49-ubuntu14+16.series"],
-				[LDISKFS_SERIES="4.4.0-45-ubuntu14+16.series"]
-			)
-		],
-		[LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"]
-	)
+	AS_VERSION_COMPARE([$LINUXRELEASE],[4.15.0],[
+	AS_VERSION_COMPARE([$LINUXRELEASE],[4.4.0], [],
+	[
+		KPLEV=$(echo $LINUXRELEASE | sed -n 's/.*-\([0-9]\+\).*/\1/p')
+		AS_IF(
+			[test -z "$KPLEV"], [
+				AC_MSG_WARN([Failed to determine Kernel patch level. Assume latest.])
+				LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"
+			],
+			[test $KPLEV -ge 73], [LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"],
+			[test $KPLEV -ge 62], [LDISKFS_SERIES="4.4.0-62-ubuntu14+16.series"],
+			[test $KPLEV -ge 49], [LDISKFS_SERIES="4.4.0-49-ubuntu14+16.series"],
+			[LDISKFS_SERIES="4.4.0-45-ubuntu14+16.series"]
+		)
+	],
+	[LDISKFS_SERIES="4.4.0-73-ubuntu14+16.series"])],
+	[
+		KPLEV=$(echo $LINUXRELEASE | sed -n 's/.*-\([0-9]\+\).*/\1/p')
+		AS_IF(
+			[test -z "$KPLEV"], [
+				AC_MSG_WARN([Failed to determine Kernel patch level. Assume latest.])
+				LDISKFS_SERIES="4.15.0-20-ubuntu18.series"
+			],
+			[test $KPLEV -ge 20], [LDISKFS_SERIES="4.15.0-20-ubuntu18.series"]
+		)
+	],
+	[LDISKFS_SERIES="4.15.0-20-ubuntu18.series"])
 ])
 ])
 AS_IF([test -z "$LDISKFS_SERIES"],

config/lustre-build-linux.m4
@@ -210,11 +210,13 @@ AC_DEFUN([LB_ARG_CANON_PATH], [
 # Find paths for linux, handling kernel-source rpms
 #
 AC_DEFUN([LB_LINUX_PATH], [
-for DEFAULT_LINUX in /lib/modules/$(uname -r)/{source,build} /usr/src/linux $(find /usr/src/kernels/ -maxdepth 1 -name @<:@0-9@:>@\* | xargs -r ls -d | tail -n 1); do
+for DEFAULT_LINUX in /usr/src/linux-source-* /lib/modules/$(uname -r)/{source,build} /usr/src/linux $(find /usr/src/kernels/ -maxdepth 1 -name @<:@0-9@:>@\* | xargs -r ls -d | tail -n 1); do
 	AS_IF([readlink -q -e $DEFAULT_LINUX >/dev/null], [break])
 done
 if test "$DEFAULT_LINUX" = "/lib/modules/$(uname -r)/source"; then
 	PATHS="/lib/modules/$(uname -r)/build"
+else
+	PATHS="/usr/src/linux-headers-$(uname -r)"
 fi
 PATHS+=" $DEFAULT_LINUX"
 for DEFAULT_LINUX_OBJ in $PATHS; do

config/lustre-build-zfs.m4
@@ -15,8 +15,8 @@ dnl #
 dnl #                       * /var/lib/dkms/zfs/${VERSION}/source
 dnl #                       * /usr/src/zfs-${VERSION}/${LINUXRELEASE}
 dnl #                       * /usr/src/zfs-${VERSION}
-dnl #                       * ../spl/
-dnl #                       * $LINUX
+dnl #                       * ../zfs/
+dnl #                       * $LINUX/zfs
 dnl #
 dnl # --with-zfs-devel=path
 dnl #                 - User provided directory where zfs development headers
@@ -46,7 +46,7 @@ dnl #                   * /var/lib/dkms/spl/${VERSION}/source
 dnl #                   * /usr/src/spl-${VERSION}/${LINUXRELEASE}
 dnl #                   * /usr/src/spl-${VERSION}
 dnl #                   * ../spl/
-dnl #                   * $LINUX
+dnl #                   * $LINUX/spl
 dnl #
 dnl # --with-spl=path - Enable spl support and use the spl headers in the
 dnl #                   provided path.  No autodetection is performed.
@@ -76,7 +76,7 @@ AC_DEFUN([LB_SPL], [
 	splsrc1="/usr/src/spl-${splver}/${LINUXRELEASE}"
 	splsrc2="/usr/src/spl-${splver}"
 	splsrc3="../spl/"
-	splsrc4="$LINUX"
+	splsrc4="$LINUX/spl"
 
 	AC_MSG_CHECKING([spl source directory])
 	AS_IF([test -z "${splsrc}"], [
@@ -194,7 +194,7 @@ AC_DEFUN([LB_ZFS], [
 	zfssrc1="/usr/src/zfs-${zfsver}/${LINUXRELEASE}"
 	zfssrc2="/usr/src/zfs-${zfsver}"
 	zfssrc3="../zfs/"
-	zfssrc4="$LINUX"
+	zfssrc4="$LINUX/zfs"
 
 	AC_MSG_CHECKING([zfs source directory])
 	AS_IF([test -z "${zfssrc}"], [

debian/control
@@ -4,19 +4,19 @@ Priority: optional
 Maintainer: Brian J. Murrell <brian.murrell@intel.com>
 Uploaders: Brian J. Murrell <brian.murrell@intel.com>
 Standards-Version: 3.8.3
-Build-Depends: module-assistant, libreadline-dev, debhelper (>=7.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync
-Homepage: https://wiki.hpdd.intel.com/
-Vcs-Git: git://git.hpdd.intel.com/fs/lustre-release.git
+Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync
+Homepage: https://wiki.whamcloud.com/
+Vcs-Git: git://git.whamcloud.com/fs/lustre-release.git
 
 Package: lustre-source
 Section: admin
 Architecture: all
 Priority: optional
-Depends: module-assistant, bzip2, debhelper (>= 7.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch
+Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
 Description: source for Lustre filesystem client kernel modules
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package contains the module source. The client kernel modules
  can be built for kernels 2.6.32+ with the use of module-assistant
@@ -29,8 +29,8 @@ Priority: optional
 Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp30, zlib1g, perl
 Description: Userspace utilities for the Lustre filesystem (client)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of userspace utilities for
  accessing Lustre filesystems from a client. If you need server utils,
@@ -41,13 +41,13 @@ Section: utils
 Architecture: i386 powerpc ppc64el amd64 ia64 arm64
 Priority: optional
 Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp30, zlib1g, perl
-Provides: lustre-server-utils, lustre-client-utils
+Provides: lustre-server-utils, lustre-client-utils (= ${binary:Version})
 Conflicts: lustre-client-utils
 Replaces: lustre-client-utils
 Description: Userspace utilities for the Lustre filesystem (server)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of userspace utilities for
  accessing and maintaining Lustre filesystems from a server.
@@ -61,8 +61,8 @@ Priority: optional
 Depends: lustre-client-utils (= ${binary:Version}), python, perl, sg3-utils
 Description: Collection of benchmark tools for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a collection of benchmark tools
 
@@ -73,8 +73,8 @@ Priority: optional
 Depends: lustre-iokit (= ${binary:Version}), attr, rsync, perl, lsof, mpi-default-bin
 Description: Test suite for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of test utilities for the Lustre filesystem.
 
@@ -85,7 +85,7 @@ Architecture: i386 powerpc ppc64el amd64 ia64 arm64
 Depends: lustre-client-utils (= ${binary:Version})
 Description: Development files for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides development libraries for the Lustre filesystem.

debian/control.main
@@ -4,19 +4,19 @@ Priority: optional
 Maintainer: Brian J. Murrell <brian.murrell@intel.com>
 Uploaders: Brian J. Murrell <brian.murrell@intel.com>
 Standards-Version: 3.8.3
-Build-Depends: module-assistant, libreadline-dev, debhelper (>=7.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync
-Homepage: https://wiki.hpdd.intel.com/
-Vcs-Git: git://git.hpdd.intel.com/fs/lustre-release.git
+Build-Depends: module-assistant, libreadline-dev, debhelper (>=9.0.0), dpatch, automake (>=1.7) | automake1.7 | automake1.8 | automake1.9, pkg-config, libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, bzip2, quilt, linux-headers-generic | linux-headers | linux-headers-amd64, rsync
+Homepage: https://wiki.whamcloud.com/
+Vcs-Git: git://git.whamcloud.com/fs/lustre-release.git
 
 Package: lustre-source
 Section: admin
 Architecture: all
 Priority: optional
-Depends: module-assistant, bzip2, debhelper (>= 7.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch
+Depends: module-assistant, bzip2, debhelper (>= 9.0.0), libtool, libyaml-dev, libselinux-dev, libsnmp-dev, mpi-default-dev, dpatch, pkg-config
 Description: source for Lustre filesystem client kernel modules
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package contains the module source. The client kernel modules
  can be built for kernels 2.6.32+ with the use of module-assistant
@@ -29,8 +29,8 @@ Priority: optional
 Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp30, zlib1g, perl
 Description: Userspace utilities for the Lustre filesystem (client)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of userspace utilities for
  accessing Lustre filesystems from a client. If you need server utils,
@@ -41,13 +41,13 @@ Section: utils
 Architecture: i386 powerpc ppc64el amd64 ia64 arm64
 Priority: optional
 Depends: ${shlibs:Depends}, ${misc:Depends}, libyaml-0-2, libselinux1, libsnmp30, zlib1g, perl
-Provides: lustre-server-utils, lustre-client-utils
+Provides: lustre-server-utils, lustre-client-utils (= ${binary:Version})
 Conflicts: lustre-client-utils
 Replaces: lustre-client-utils
 Description: Userspace utilities for the Lustre filesystem (server)
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of userspace utilities for
  accessing and maintaining Lustre filesystems from a server.
@@ -61,8 +61,8 @@ Priority: optional
 Depends: lustre-client-utils (= ${binary:Version}), python, perl, sg3-utils
 Description: Collection of benchmark tools for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a collection of benchmark tools
 
@@ -73,8 +73,8 @@ Priority: optional
 Depends: lustre-iokit (= ${binary:Version}), attr, rsync, perl, lsof, mpi-default-bin
 Description: Test suite for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides a number of test utilities for the Lustre filesystem.
 
@@ -85,7 +85,7 @@ Architecture: i386 powerpc ppc64el amd64 ia64 arm64
 Depends: lustre-client-utils (= ${binary:Version})
 Description: Development files for the Lustre filesystem
  Lustre is a scalable, secure, robust, highly-available cluster file system.
- This release is maintained by Intel Corporation and available from
- https://wiki.hpdd.intel.com/
+ This release is maintained by Whamcloud and available from
+ https://wiki.whamcloud.com/
  .
  This package provides development libraries for the Lustre filesystem.

debian/control.modules.in
@@ -4,7 +4,7 @@ Priority: optional
 Maintainer: Brian J. Murrell <brian.murrell@intel.com>
 Uploaders: Brian J. Murrell <brian.murrell@intel.com>
 Standards-Version: 3.7.2
-Build-Depends: debhelper (>= 7.0.0), bzip2
+Build-Depends: debhelper (>= 9.0.0), bzip2
 
 Package: lustre-client-modules-_KVERS_
 Architecture: any

commit 6e26d2d715ee65079682f7823f986f61f80eb07c

LU-12137 llite: use ->iterate_shared() for readdir
Use the ->iterate_shared() method for readdir in llite, which has
been available since kernel commit v4.6-rc3-29-g6192269.

Remove duplicate autoconf check for the "iterate_shared" method.

Fixes: e41bdca7559 ("LU-11071 build: Add server build support for Ubuntu 18.04")
Test-Parameters: clientdistro=sles12sp3
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I4dcec7c8ce1a5dbcc1e0ebc74ac47d1b7a4cab07
Reviewed-on: https://review.whamcloud.com/34556
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2305,7 +2305,7 @@ generic_write_sync_2args, [
 #
 # LC_FOP_ITERATE_SHARED
 #
-# Kernel version 4.7 adds iterate_shared method to file_operations
+# Kernel v4.6-rc3-29-g6192269 adds iterate_shared method to file_operations
 #
 AC_DEFUN([LC_FOP_ITERATE_SHARED], [
 LB_CHECK_COMPILE([if 'file_operations' has 'iterate_shared'],
@@ -2319,29 +2319,6 @@ fop_iterate_shared, [
 		[file_operations has iterate_shared])
 ])
 ]) # LC_FOP_ITERATE_SHARED
-
-#
-# LC_FOPS_ITERATE_SHARED
-#
-# 4.7 commit ae05327a00fd47c34dfe25294b359a3f3fef96e8
-# ext4: switch to ->iterate_shared()
-# this replaces ext4_dir_operations iterate with iterate_shared.
-# dir_relaxed_shared() was also added in this commit, so we can
-# use that function to verify that the ext4_dir_operations is using
-# iterate_shared.
-#
-AC_DEFUN([LC_FOPS_ITERATE_SHARED], [
-LB_CHECK_COMPILE([if ext4_dir_operations uses iterate_shared],
-iterate_shared, [
-	#include <linux/fs.h>
-],[
-	((struct file_operations *)0)->iterate_shared(NULL, NULL);
-	dir_relax_shared(NULL);
-],[
-	AC_DEFINE(HAVE_ITERATE_SHARED, 1,
-		['iterate_shared' is available])
-])
-]) # LC_FOPS_ITERATE_SHARED
 
 #
 # LC_HAVE_POSIX_ACL_VALID_USER_NS
@@ -3102,7 +3079,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_D_IN_LOOKUP
 	LC_DIRECTIO_2ARGS
 	LC_GENERIC_WRITE_SYNC_2ARGS
-	LC_FOPS_ITERATE_SHARED
+	LC_FOP_ITERATE_SHARED
 
 	# 4.8
 	LC_HAVE_POSIX_ACL_VALID_USER_NS

lustre/include/lustre_compat.h
@@ -243,6 +243,10 @@ static inline struct dentry *d_make_root(struct inode *root)
 }
 #endif
 
+#ifndef HAVE_FOP_ITERATE_SHARED
+#define iterate_shared iterate
+#endif
+
 #ifdef HAVE_DIRTY_INODE_HAS_FLAG
 # define ll_dirty_inode(inode, flag)	(inode)->i_sb->s_op->dirty_inode((inode), flag)
 #else

lustre/llite/dir.c
@@ -2189,7 +2189,7 @@ const struct file_operations ll_dir_operations = {
 	.release	= ll_dir_release,
 	.read		= generic_read_dir,
 #ifdef HAVE_DIR_CONTEXT
-	.iterate	= ll_iterate,
+	.iterate_shared	= ll_iterate,
 #else
 	.readdir	= ll_readdir,
 #endif

lustre/osd-ldiskfs/osd_handler.c
@@ -1023,11 +1023,7 @@ again:
 		oclb.oclb_items = 0;
 #ifdef HAVE_DIR_CONTEXT
 		oclb.ctx.pos = filp->f_pos;
-#ifdef HAVE_ITERATE_SHARED
 		rc = fops->iterate_shared(filp, &oclb.ctx);
-#else
-		rc = fops->iterate(filp, &oclb.ctx);
-#endif
 		filp->f_pos = oclb.ctx.pos;
 #else
 		rc = fops->readdir(filp, &oclb, osd_stripe_dir_filldir);
@@ -6631,11 +6627,7 @@ static int osd_ldiskfs_it_fill(const struct lu_env *env,
 
 #ifdef HAVE_DIR_CONTEXT
 	buf.ctx.pos = filp->f_pos;
-#ifdef HAVE_ITERATE_SHARED
 	rc = inode->i_fop->iterate_shared(filp, &buf.ctx);
-#else
-	rc = inode->i_fop->iterate(filp, &buf.ctx);
-#endif
 	filp->f_pos = buf.ctx.pos;
 #else
 	rc = inode->i_fop->readdir(filp, &buf, osd_ldiskfs_filldir);

lustre/osd-ldiskfs/osd_scrub.c
@@ -2282,11 +2282,7 @@ osd_ios_general_scan(struct osd_thread_info *info, struct osd_device *dev,
 		buf.oifb_items = 0;
 #ifdef HAVE_DIR_CONTEXT
 		buf.ctx.pos = filp->f_pos;
-#ifdef HAVE_ITERATE_SHARED
 		rc = fops->iterate_shared(filp, &buf.ctx);
-#else
-		rc = fops->iterate(filp, &buf.ctx);
-#endif
 		filp->f_pos = buf.ctx.pos;
 #else
 		rc = fops->readdir(filp, &buf, filldir);