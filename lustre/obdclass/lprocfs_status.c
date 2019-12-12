lustre/lustre-release

commit e5810126b3fb488a3fed37e085e3ca4ae585324c

LU-11359 mdt: fix mdt_dom_discard_data() timeouts
The mdt_dom_discard_data() issues new lock to cause data
discard for all conflicting client locks. This was done in
context of unlink RPC processing and may cause it to be stuck
waiting for client to cancel their locks leading to cascading
timeouts for any other locks waiting on the same resource and
parent directory.

Patch skips discard lock waiting in the current context by
using own CP callback for that which doesn't wait for blocking
locks. They will be finished later by LDLM and cleaned up in
that completion callback. So current thread just makes sure
discard locks are taken and BL ASTs are sent but doesnt't wait
for lock granting and that fixes the original problem.

At the same time that opens window for race with data being
flushed on client, so it is possible that new IO from client
will happen on just unlinked object causing error message and
it is not possible to distinguish that case from other
possibly critical situations. To solve that the unlinked object
is pinned in memory while until discard lock is granted.
Therefore, such objects can be easily distinguished as stale one
and any IO against it can be just silently ignored.

Older clients are not fully compatible with async DoM discard so
patch adds also new connection flag ASYNC_DISCARD to distinguish
old clients and use old blocking discard for then.

Lustre-change: https://review.whamcloud.com/34071
Lustre-commit: 9c028e74c2202a8a481557c4cb22225734aaf19f

Test-Parameters: testlist=racer,racer,racer
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I419677af43c33e365a246fe12205b506209deace
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Signed-off-by: Minh Diep <mdiep@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/35197
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Stephan Thiell <sthiell@stanford.edu>

lustre/obdclass/lprocfs_status.c
@@ -785,6 +785,7 @@ static const char *obd_connect_names[] = {
 	"unknown",		/* 0x200 */
 	"selinux_policy",	/* 0x400 */
 	"lsom",			/* 0x800 */
+	"async_discard",	/* 0x4000 */
 	NULL
 };
 

lustre/include/lustre_dlm.h
@@ -1427,6 +1427,8 @@ static inline int ldlm_res_lvbo_update(struct ldlm_resource *res,
 	return ldlm_lvbo_update(res, NULL, req, increase);
 }
 
+int is_granted_or_cancelled_nolock(struct ldlm_lock *lock);
+
 int ldlm_error2errno(enum ldlm_error error);
 enum ldlm_error ldlm_errno2error(int err_no); /* don't call it `errno': this
 					       * confuses user-space. */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -835,6 +835,7 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_ARCHIVE_ID_ARRAY	0x100ULL /* store HSM archive_id in array */
 #define OBD_CONNECT2_SELINUX_POLICY	0x400ULL /* has client SELinux policy */
 #define OBD_CONNECT2_LSOM		0x800ULL /* LSOM support */
+#define OBD_CONNECT2_ASYNC_DISCARD	0x4000ULL /* support async DoM data discard */
 
 /* XXX README XXX:
  * Please DO NOT add flag values here before first ensuring that this same
@@ -891,7 +892,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_DIR_MIGRATE | \
 				OBD_CONNECT2_ARCHIVE_ID_ARRAY | \
 				OBD_CONNECT2_SELINUX_POLICY | \
-				OBD_CONNECT2_LSOM)
+				OBD_CONNECT2_LSOM | \
+				OBD_CONNECT2_ASYNC_DISCARD)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \

lustre/ldlm/ldlm_internal.h
@@ -354,16 +354,13 @@ ldlm_add_var(struct lprocfs_vars *vars, struct dentry *debugfs_entry,
 
 static inline int is_granted_or_cancelled(struct ldlm_lock *lock)
 {
-        int ret = 0;
+	int ret = 0;
 
-        lock_res_and_lock(lock);
-	if (ldlm_is_granted(lock) && !ldlm_is_cp_reqd(lock))
-		ret = 1;
-	else if (ldlm_is_failed(lock) || ldlm_is_cancel(lock))
-                ret = 1;
-        unlock_res_and_lock(lock);
+	lock_res_and_lock(lock);
+	ret = is_granted_or_cancelled_nolock(lock);
+	unlock_res_and_lock(lock);
 
-        return ret;
+	return ret;
 }
 
 static inline bool is_bl_done(struct ldlm_lock *lock)

lustre/ldlm/ldlm_request.c
@@ -149,6 +149,19 @@ int ldlm_expired_completion_wait(void *data)
         RETURN(0);
 }
 
+int is_granted_or_cancelled_nolock(struct ldlm_lock *lock)
+{
+	int ret = 0;
+
+	check_res_locked(lock->l_resource);
+	if (ldlm_is_granted(lock) && !ldlm_is_cp_reqd(lock))
+		ret = 1;
+	else if (ldlm_is_failed(lock) || ldlm_is_cancel(lock))
+		ret = 1;
+	return ret;
+}
+EXPORT_SYMBOL(is_granted_or_cancelled_nolock);
+
 /**
  * Calculate the Completion timeout (covering enqueue, BL AST, data flush,
  * lock cancel, and their replies). Used for lock completion timeout on the

lustre/llite/llite_lib.c
@@ -223,7 +223,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				   OBD_CONNECT2_DIR_MIGRATE |
 				   OBD_CONNECT2_SUM_STATFS |
 				   OBD_CONNECT2_ARCHIVE_ID_ARRAY |
-				   OBD_CONNECT2_LSOM;
+				   OBD_CONNECT2_LSOM |
+				   OBD_CONNECT2_ASYNC_DISCARD;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
         if (sbi->ll_flags & LL_SBI_LRU_RESIZE)
@@ -2099,11 +2100,17 @@ void ll_delete_inode(struct inode *inode)
 	unsigned long nrpages;
 	ENTRY;
 
-	if (S_ISREG(inode->i_mode) && lli->lli_clob != NULL)
+	if (S_ISREG(inode->i_mode) && lli->lli_clob != NULL) {
 		/* It is last chance to write out dirty pages,
-		 * otherwise we may lose data while umount */
-		cl_sync_file_range(inode, 0, OBD_OBJECT_EOF, CL_FSYNC_LOCAL, 1);
-
+		 * otherwise we may lose data while umount.
+		 *
+		 * If i_nlink is 0 then just discard data. This is safe because
+		 * local inode gets i_nlink 0 from server only for the last
+		 * unlink, so that file is not opened somewhere else
+		 */
+		cl_sync_file_range(inode, 0, OBD_OBJECT_EOF, inode->i_nlink ?
+				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1);
+	}
 	truncate_inode_pages_final(mapping);
 
 	/* Workaround for LU-118: Note nrpages may not be totally updated when

lustre/llite/namei.c
@@ -233,8 +233,20 @@ void ll_lock_cancel_bits(struct ldlm_lock *lock, __u64 to_cancel)
 	__u64 bits = to_cancel;
 	int rc;
 
-	if (inode == NULL)
-		return;
+	ENTRY;
+
+	if (!inode) {
+		/* That means the inode is evicted most likely and may cause
+		 * the skipping of lock cleanups below, so print the message
+		 * about that in log.
+		 */
+		if (lock->l_resource->lr_lvb_inode)
+			LDLM_DEBUG(lock,
+				   "can't take inode for the lock (%sevicted)\n",
+				   lock->l_resource->lr_lvb_inode->i_state &
+				   I_FREEING ? "" : "not ");
+		RETURN_EXIT;
+	}
 
 	if (!fid_res_name_eq(ll_inode2fid(inode),
 			     &lock->l_resource->lr_name)) {
@@ -372,6 +384,7 @@ void ll_lock_cancel_bits(struct ldlm_lock *lock, __u64 to_cancel)
 		ll_invalidate_aliases(inode);
 
 	iput(inode);
+	RETURN_EXIT;
 }
 
 /* Check if the given lock may be downgraded instead of canceling and

lustre/mdt/mdt_internal.h
@@ -1291,8 +1291,7 @@ int mdt_brw_enqueue(struct mdt_thread_info *info, struct ldlm_namespace *ns,
 		    struct ldlm_lock **lockp, __u64 flags);
 int mdt_dom_read_on_open(struct mdt_thread_info *mti, struct mdt_device *mdt,
 			 struct lustre_handle *lh);
-void mdt_dom_discard_data(struct mdt_thread_info *info,
-			  const struct lu_fid *fid);
+void mdt_dom_discard_data(struct mdt_thread_info *info, struct mdt_object *mo);
 int mdt_dom_disk_lvbo_update(const struct lu_env *env, struct mdt_object *mo,
 			     struct ldlm_resource *res, bool increase_only);
 void mdt_dom_obj_lvb_update(const struct lu_env *env, struct mdt_object *mo,

commit b66efa7d500f94d755f318d43804f0f3eb883835

LU-12935 obdclass: fix import connect flag printing
The obd_connect_names[] array holds strings for the OBD_CONNECT_*
and obd_CONNECT2_* flag names.  It is positional, so every flag
bit needs a corresponding field in the array.

The "async_discard" feature was backported to b2_12, but the two
earlier features "pcc" and (now removed) "plain_layout" were not
backported.  Add in strings for those features, and fill in some
earlier "unknown" flag names as well

Fixes: e5810126b3fb ("LU-11359 mdt: fix mdt_dom_discard_data() timeouts")
Test-Parameters: trivial
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I883d236262805361be3f48c533d781878f9494fa
Reviewed-on: https://review.whamcloud.com/36881
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.super@gmail.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: Stephan Thiell <sthiell@stanford.edu>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/lprocfs_status.c
@@ -776,15 +776,17 @@ static const char *obd_connect_names[] = {
 	"file_secctx",	/* 0x01 */
 	"lockaheadv2",	/* 0x02 */
 	"dir_migrate",	/* 0x04 */
-	"unknown",	/* 0x08 */
-	"unknown",	/* 0x10 */
+	"sum_statfs",	/* 0x08 */
+	"overstriping",	/* 0x10 */
 	"flr",		/* 0x20 */
 	"wbc",		/* 0x40 */
 	"lock_convert",  /* 0x80 */
 	"archive_id_array",	/* 0x100 */
-	"unknown",		/* 0x200 */
+	"increasing_xid",	/* 0x200 */
 	"selinux_policy",	/* 0x400 */
 	"lsom",			/* 0x800 */
+	"pcc",			/* 0x1000 */
+	"unknown",		/* 0x2000 */
 	"async_discard",	/* 0x4000 */
 	NULL
 };