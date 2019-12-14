lustre/lustre-release

commit b11be372c21dcaa7a6e16f65cf4dd226ea39cadb

LU-9019 lnet: move ping and delay injection to time64_t
Migrate away from jiffies for the pinger to time_64_t to one make
it clear its for time keeping and secondly to ensure the behavior
is consistent across any platform. Besides the lnet pinger code
move the lnet dely injection code to time64_t as well.

Change-Id: If363523893fc1dcce4eaa866501946edd6558751
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/30658
Tested-by: Jenkins
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Doug Oucharek <dougso@me.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lnet/lnet/net_fault.c
@@ -57,9 +57,9 @@ struct lnet_drop_rule {
 	/**
 	 * seconds to drop the next message, it's exclusive with dr_drop_at
 	 */
-	cfs_time_t		dr_drop_time;
+	time64_t		dr_drop_time;
 	/** baseline to caculate dr_drop_time */
-	cfs_time_t		dr_time_base;
+	time64_t		dr_time_base;
 	/** statistic of dropped messages */
 	struct lnet_fault_stat	dr_stat;
 };
@@ -170,9 +170,9 @@ lnet_drop_rule_add(struct lnet_fault_attr *attr)
 
 	rule->dr_attr = *attr;
 	if (attr->u.drop.da_interval != 0) {
-		rule->dr_time_base = cfs_time_shift(attr->u.drop.da_interval);
-		rule->dr_drop_time = cfs_time_shift(cfs_rand() %
-						    attr->u.drop.da_interval);
+		rule->dr_time_base = ktime_get_seconds() + attr->u.drop.da_interval;
+		rule->dr_drop_time = ktime_get_seconds() +
+				     cfs_rand() % attr->u.drop.da_interval;
 	} else {
 		rule->dr_drop_at = cfs_rand() % attr->u.drop.da_rate;
 	}
@@ -283,10 +283,9 @@ lnet_drop_rule_reset(void)
 		if (attr->u.drop.da_rate != 0) {
 			rule->dr_drop_at = cfs_rand() % attr->u.drop.da_rate;
 		} else {
-			rule->dr_drop_time = cfs_time_shift(cfs_rand() %
-						attr->u.drop.da_interval);
-			rule->dr_time_base = cfs_time_shift(attr->u.drop.
-								  da_interval);
+			rule->dr_drop_time = ktime_get_seconds() +
+					     cfs_rand() % attr->u.drop.da_interval;
+			rule->dr_time_base = ktime_get_seconds() + attr->u.drop.da_interval;
 		}
 		spin_unlock(&rule->dr_lock);
 	}
@@ -312,21 +311,19 @@ drop_rule_match(struct lnet_drop_rule *rule, lnet_nid_t src,
 	/* match this rule, check drop rate now */
 	spin_lock(&rule->dr_lock);
 	if (rule->dr_drop_time != 0) { /* time based drop */
-		cfs_time_t now = cfs_time_current();
+		time64_t now = ktime_get_seconds();
 
 		rule->dr_stat.fs_count++;
-		drop = cfs_time_aftereq(now, rule->dr_drop_time);
+		drop = now >= rule->dr_drop_time;
 		if (drop) {
-			if (cfs_time_after(now, rule->dr_time_base))
+			if (now > rule->dr_time_base)
 				rule->dr_time_base = now;
 
 			rule->dr_drop_time = rule->dr_time_base +
-					     cfs_time_seconds(cfs_rand() %
-						attr->u.drop.da_interval);
-			rule->dr_time_base += cfs_time_seconds(attr->u.drop.
-							       da_interval);
+					     cfs_rand() % attr->u.drop.da_interval;
+			rule->dr_time_base += attr->u.drop.da_interval;
 
-			CDEBUG(D_NET, "Drop Rule %s->%s: next drop : %ld\n",
+			CDEBUG(D_NET, "Drop Rule %s->%s: next drop : %lld\n",
 			       libcfs_nid2str(attr->fa_src),
 			       libcfs_nid2str(attr->fa_dst),
 			       rule->dr_drop_time);
@@ -412,9 +409,9 @@ struct lnet_delay_rule {
 	/**
 	 * seconds to delay the next message, it's exclusive with dl_delay_at
 	 */
-	cfs_time_t		dl_delay_time;
+	time64_t		dl_delay_time;
 	/** baseline to caculate dl_delay_time */
-	cfs_time_t		dl_time_base;
+	time64_t		dl_time_base;
 	/** jiffies to send the next delayed message */
 	unsigned long		dl_msg_send;
 	/** delayed message list */
@@ -443,13 +440,6 @@ struct delay_daemon_data {
 };
 
 static struct delay_daemon_data	delay_dd;
-
-static cfs_time_t
-round_timeout(cfs_time_t timeout)
-{
-	return cfs_time_seconds((unsigned int)
-			cfs_duration_sec(cfs_time_sub(timeout, 0)) + 1);
-}
 
 static void
 delay_rule_decref(struct lnet_delay_rule *rule)
@@ -481,21 +471,19 @@ delay_rule_match(struct lnet_delay_rule *rule, lnet_nid_t src,
 	/* match this rule, check delay rate now */
 	spin_lock(&rule->dl_lock);
 	if (rule->dl_delay_time != 0) { /* time based delay */
-		cfs_time_t now = cfs_time_current();
+		time64_t now = ktime_get_seconds();
 
 		rule->dl_stat.fs_count++;
-		delay = cfs_time_aftereq(now, rule->dl_delay_time);
+		delay = now >= rule->dl_delay_time;
 		if (delay) {
-			if (cfs_time_after(now, rule->dl_time_base))
+			if (now > rule->dl_time_base)
 				rule->dl_time_base = now;
 
 			rule->dl_delay_time = rule->dl_time_base +
-					     cfs_time_seconds(cfs_rand() %
-						attr->u.delay.la_interval);
-			rule->dl_time_base += cfs_time_seconds(attr->u.delay.
-							       la_interval);
+					      cfs_rand() % attr->u.delay.la_interval;
+			rule->dl_time_base += attr->u.delay.la_interval;
 
-			CDEBUG(D_NET, "Delay Rule %s->%s: next delay : %ld\n",
+			CDEBUG(D_NET, "Delay Rule %s->%s: next delay : %lld\n",
 			       libcfs_nid2str(attr->fa_src),
 			       libcfs_nid2str(attr->fa_dst),
 			       rule->dl_delay_time);
@@ -526,8 +514,7 @@ delay_rule_match(struct lnet_delay_rule *rule, lnet_nid_t src,
 	rule->dl_stat.u.delay.ls_delayed++;
 
 	list_add_tail(&msg->msg_list, &rule->dl_msg_list);
-	msg->msg_delay_send = round_timeout(
-			cfs_time_shift(attr->u.delay.la_latency));
+	msg->msg_delay_send = ktime_get_seconds() + attr->u.delay.la_latency;
 	if (rule->dl_msg_send == -1) {
 		rule->dl_msg_send = msg->msg_delay_send;
 		mod_timer(&rule->dl_timer, rule->dl_msg_send);
@@ -574,9 +561,9 @@ delayed_msg_check(struct lnet_delay_rule *rule, bool all,
 {
 	struct lnet_msg *msg;
 	struct lnet_msg *tmp;
-	unsigned long	 now = cfs_time_current();
+	time64_t now = ktime_get_seconds();
 
-	if (!all && rule->dl_msg_send > now)
+	if (!all && cfs_time_seconds(rule->dl_msg_send) > now)
 		return;
 
 	spin_lock(&rule->dl_lock);
@@ -783,9 +770,10 @@ lnet_delay_rule_add(struct lnet_fault_attr *attr)
 
 	rule->dl_attr = *attr;
 	if (attr->u.delay.la_interval != 0) {
-		rule->dl_time_base = cfs_time_shift(attr->u.delay.la_interval);
-		rule->dl_delay_time = cfs_time_shift(cfs_rand() %
-						     attr->u.delay.la_interval);
+		rule->dl_time_base = ktime_get_seconds() +
+				     attr->u.delay.la_interval;
+		rule->dl_delay_time = ktime_get_seconds() +
+				      cfs_rand() % attr->u.delay.la_interval;
 	} else {
 		rule->dl_delay_at = cfs_rand() % attr->u.delay.la_rate;
 	}
@@ -936,10 +924,10 @@ lnet_delay_rule_reset(void)
 		if (attr->u.delay.la_rate != 0) {
 			rule->dl_delay_at = cfs_rand() % attr->u.delay.la_rate;
 		} else {
-			rule->dl_delay_time = cfs_time_shift(cfs_rand() %
-						attr->u.delay.la_interval);
-			rule->dl_time_base = cfs_time_shift(attr->u.delay.
-								  la_interval);
+			rule->dl_delay_time = ktime_get_seconds() +
+					      cfs_rand() % attr->u.delay.la_interval;
+			rule->dl_time_base = ktime_get_seconds() +
+					     attr->u.delay.la_interval;
 		}
 		spin_unlock(&rule->dl_lock);
 	}

lnet/include/lnet/lib-lnet.h
@@ -537,9 +537,9 @@ extern unsigned int lnet_peer_discovery_disabled;
 extern int portal_rotor;
 
 int lnet_notify(struct lnet_ni *ni, lnet_nid_t peer, int alive,
-		cfs_time_t when);
+		time64_t when);
 void lnet_notify_locked(struct lnet_peer_ni *lp, int notifylnd, int alive,
-			cfs_time_t when);
+			time64_t when);
 int lnet_add_route(__u32 net, __u32 hops, lnet_nid_t gateway_nid,
 		   unsigned int priority);
 int lnet_check_routes(void);
@@ -939,7 +939,8 @@ lnet_is_peer_healthy_locked(struct lnet_peer *peer)
 static inline void
 lnet_peer_set_alive(struct lnet_peer_ni *lp)
 {
-	lp->lpni_last_alive = lp->lpni_last_query = cfs_time_current();
+	lp->lpni_last_alive = ktime_get_seconds();
+	lp->lpni_last_query = lp->lpni_last_alive;
 	if (!lp->lpni_alive)
 		lnet_notify_locked(lp, 0, 1, lp->lpni_last_alive);
 }

lnet/include/lnet/lib-types.h
@@ -258,7 +258,7 @@ struct lnet_lnd {
 	void (*lnd_notify)(struct lnet_ni *ni, lnet_nid_t peer, int alive);
 
 	/* query of peer aliveness */
-	void (*lnd_query)(struct lnet_ni *ni, lnet_nid_t peer, cfs_time_t *when);
+	void (*lnd_query)(struct lnet_ni *ni, lnet_nid_t peer, time64_t *when);
 
 	/* accept a new connection */
 	int (*lnd_accept)(struct lnet_ni *ni, struct socket *sock);
@@ -390,7 +390,7 @@ struct lnet_ni {
 	int			**ni_refs;
 
 	/* when I was last alive */
-	long			ni_last_alive;
+	time64_t		ni_last_alive;
 
 	/* pointer to parent network */
 	struct lnet_net		*ni_net;
@@ -498,15 +498,15 @@ struct lnet_peer_ni {
 	/* # times router went dead<->alive. Protected with lpni_lock */
 	int			lpni_alive_count;
 	/* time of last aliveness news */
-	cfs_time_t		lpni_timestamp;
+	time64_t		lpni_timestamp;
 	/* time of last ping attempt */
-	cfs_time_t		lpni_ping_timestamp;
+	time64_t		lpni_ping_timestamp;
 	/* != 0 if ping reply expected */
-	cfs_time_t		lpni_ping_deadline;
+	time64_t		lpni_ping_deadline;
 	/* when I was last alive */
-	cfs_time_t		lpni_last_alive;
+	time64_t		lpni_last_alive;
 	/* when lpni_ni was queried last time */
-	cfs_time_t		lpni_last_query;
+	time64_t		lpni_last_query;
 	/* network peer is on */
 	struct lnet_net		*lpni_net;
 	/* peer's NID */

lnet/klnds/gnilnd/gnilnd.c
@@ -1920,7 +1920,7 @@ kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 }
 
 void
-kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
+kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when)
 {
 	kgn_net_t               *net = ni->ni_data;
 	kgn_tx_t                *tx;
@@ -1939,7 +1939,7 @@ kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
 		/* LIE if in a quiesce - we will update the timeouts after,
 		 * but we don't want sends failing during it */
 		if (kgnilnd_data.kgn_quiesce_trigger) {
-			*when = jiffies;
+			*when = ktime_get_seconds();
 			read_unlock(&kgnilnd_data.kgn_peer_conn_lock);
 			GOTO(out, 0);
 		}
@@ -1955,7 +1955,7 @@ kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
 			 * - if it was, we lie to LNet because we believe a TX would complete
 			 * on reconnect */
 			if (kgnilnd_conn_clean_errno(peer->gnp_last_errno)) {
-				*when = jiffies;
+				*when = ktime_get_seconds();
 			}
 			/* we still want to fire a TX and new conn in this case */
 		} else {
@@ -1985,7 +1985,7 @@ kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
 		kgnilnd_launch_tx(tx, net, &id);
 	}
 out:
-	CDEBUG(D_NETTRACE, "peer 0x%p->%s when %lu\n", peer,
+	CDEBUG(D_NETTRACE, "peer 0x%p->%s when %lld\n", peer,
 	       libcfs_nid2str(nid), *when);
 	EXIT;
 }

lnet/klnds/gnilnd/gnilnd.h
@@ -1778,7 +1778,7 @@ void kgnilnd_unmap_fma_blocks(kgn_device_t *device);
 void kgnilnd_free_phys_fmablk(kgn_device_t *device);
 
 int kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg);
-void kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when);
+void kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when);
 int kgnilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg);
 int kgnilnd_eager_recv(struct lnet_ni *ni, void *private,
 			struct lnet_msg *lntmsg, void **new_private);

lnet/klnds/o2iblnd/o2iblnd.c
@@ -1216,13 +1216,13 @@ kiblnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 }
 
 static void
-kiblnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
+kiblnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when)
 {
-	cfs_time_t	last_alive = 0;
-	cfs_time_t	now = cfs_time_current();
-	rwlock_t	*glock = &kiblnd_data.kib_global_lock;
-	kib_peer_ni_t	*peer_ni;
-	unsigned long	flags;
+	time64_t last_alive = 0;
+	time64_t now = ktime_get_seconds();
+	rwlock_t *glock = &kiblnd_data.kib_global_lock;
+	kib_peer_ni_t *peer_ni;
+	unsigned long flags;
 
 	read_lock_irqsave(glock, flags);
 
@@ -1240,9 +1240,9 @@ kiblnd_query(struct lnet_ni *ni, lnet_nid_t nid, cfs_time_t *when)
 	if (peer_ni == NULL)
 		kiblnd_launch_tx(ni, NULL, nid);
 
-	CDEBUG(D_NET, "peer_ni %s %p, alive %ld secs ago\n",
+	CDEBUG(D_NET, "peer_ni %s %p, alive %lld secs ago\n",
 	       libcfs_nid2str(nid), peer_ni,
-	       last_alive ? cfs_duration_sec(now - last_alive) : -1);
+	       last_alive ? now - last_alive : -1);
 	return;
 }
 

lnet/klnds/o2iblnd/o2iblnd.h
@@ -754,7 +754,7 @@ typedef struct kib_peer
 	/* incarnation of peer_ni */
 	__u64			ibp_incarnation;
 	/* when (in jiffies) I was last alive */
-	cfs_time_t		ibp_last_alive;
+	time64_t		ibp_last_alive;
 	/* # users */
 	atomic_t		ibp_refcount;
 	/* version of peer_ni */

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1941,8 +1941,8 @@ kiblnd_thread_fini (void)
 static void
 kiblnd_peer_alive (kib_peer_ni_t *peer_ni)
 {
-	/* This is racy, but everyone's only writing cfs_time_current() */
-	peer_ni->ibp_last_alive = cfs_time_current();
+	/* This is racy, but everyone's only writing ktime_get_seconds() */
+	peer_ni->ibp_last_alive = ktime_get_seconds();
 	smp_mb();
 }
 
@@ -1950,7 +1950,7 @@ static void
 kiblnd_peer_notify (kib_peer_ni_t *peer_ni)
 {
         int           error = 0;
-        cfs_time_t    last_alive = 0;
+	time64_t last_alive = 0;
         unsigned long flags;
 
 	read_lock_irqsave(&kiblnd_data.kib_global_lock, flags);
@@ -3314,11 +3314,10 @@ kiblnd_check_conns (int idx)
 			}
 
 			if (timedout) {
-				CERROR("Timed out RDMA with %s (%lu): "
+				CERROR("Timed out RDMA with %s (%lld): "
 				       "c: %u, oc: %u, rc: %u\n",
 				       libcfs_nid2str(peer_ni->ibp_nid),
-				       cfs_duration_sec(cfs_time_current() -
-							peer_ni->ibp_last_alive),
+				       ktime_get_seconds() - peer_ni->ibp_last_alive,
 				       conn->ibc_credits,
 				       conn->ibc_outstanding_credits,
 				       conn->ibc_reserved_credits);

commit e150810faa5b22382b2213225478c3763195438b

LU-12931 timers: correctly offset mod_timer.
During a high level code review of the lustre time code it was
discovered that some of the mod_timer() calles was missing
adding the current jiffies value to the timeout that converted
to jiffies from seconds. Add this proper offset.

Fixes: b11be372c21d ("LU-9019 lnet: move ping an delay injection to time64_t")
Fixes: e920b3681451 ("LU-9019 ldlm: migrate the rest of the code to 64 bit time")
Change-Id: Ie4be14946032308610aff2fe72d15d4d70773da1
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/36688
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/net_fault.c
@@ -487,8 +487,8 @@ struct lnet_delay_rule {
 	time64_t		dl_delay_time;
 	/** baseline to caculate dl_delay_time */
 	time64_t		dl_time_base;
-	/** jiffies to send the next delayed message */
-	unsigned long		dl_msg_send;
+	/** seconds until we send the next delayed message */
+	time64_t		dl_msg_send;
 	/** delayed message list */
 	struct list_head	dl_msg_list;
 	/** statistic of delayed messages */
@@ -593,7 +593,8 @@ delay_rule_match(struct lnet_delay_rule *rule, lnet_nid_t src,
 	msg->msg_delay_send = ktime_get_seconds() + attr->u.delay.la_latency;
 	if (rule->dl_msg_send == -1) {
 		rule->dl_msg_send = msg->msg_delay_send;
-		mod_timer(&rule->dl_timer, rule->dl_msg_send);
+		mod_timer(&rule->dl_timer,
+			  jiffies + cfs_time_seconds(rule->dl_msg_send));
 	}
 
 	spin_unlock(&rule->dl_lock);
@@ -661,7 +662,8 @@ delayed_msg_check(struct lnet_delay_rule *rule, bool all,
 		msg = list_entry(rule->dl_msg_list.next,
 				 struct lnet_msg, msg_list);
 		rule->dl_msg_send = msg->msg_delay_send;
-		mod_timer(&rule->dl_timer, rule->dl_msg_send);
+		mod_timer(&rule->dl_timer,
+			  jiffies + cfs_time_seconds(rule->dl_msg_send));
 	}
 	spin_unlock(&rule->dl_lock);
 }

lustre/ldlm/ldlm_lockd.c
@@ -349,12 +349,15 @@ static void waiting_locks_callback(TIMER_DATA_TYPE unused)
 	 * left.
 	 */
 	if (!list_empty(&waiting_locks_list)) {
-		unsigned long timeout_jiffies;
+		time64_t now = ktime_get_seconds();
+		time_t delta = 0;
 
 		lock = list_entry(waiting_locks_list.next, struct ldlm_lock,
 				  l_pending_chain);
-		timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
-		mod_timer(&waiting_locks_timer, timeout_jiffies);
+		if (lock->l_callback_timeout - now > 0)
+			delta = lock->l_callback_timeout - now;
+		mod_timer(&waiting_locks_timer,
+			  jiffies + cfs_time_seconds(delta));
 	}
 	spin_unlock_bh(&waiting_locks_spinlock);
 }
@@ -373,8 +376,10 @@ static void waiting_locks_callback(TIMER_DATA_TYPE unused)
  */
 static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds)
 {
-	unsigned long timeout_jiffies;
-	time64_t timeout;
+	unsigned long timeout_jiffies = jiffies;
+	time64_t now = ktime_get_seconds();
+	time64_t deadline;
+	time_t timeout;
 
 	if (!list_empty(&lock->l_pending_chain))
 		return 0;
@@ -383,11 +388,13 @@ static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds)
 	    OBD_FAIL_CHECK(OBD_FAIL_PTLRPC_HPREQ_TIMEOUT))
 		seconds = 1;
 
-	timeout = ktime_get_seconds() + seconds;
-	if (likely(timeout > lock->l_callback_timeout))
-		lock->l_callback_timeout = timeout;
+	deadline = now + seconds;
+	if (likely(deadline > lock->l_callback_timeout))
+		lock->l_callback_timeout = deadline;
 
-	timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
+	timeout = clamp_t(time_t, lock->l_callback_timeout - now,
+			  0, seconds);
+	timeout_jiffies += cfs_time_seconds(timeout);
 
 	if (time_before(timeout_jiffies, waiting_locks_timer.expires) ||
 	    !timer_pending(&waiting_locks_timer))
@@ -504,12 +511,17 @@ static int __ldlm_del_waiting_lock(struct ldlm_lock *lock)
 			/* No more, just cancel. */
 			del_timer(&waiting_locks_timer);
 		} else {
+			time64_t now = ktime_get_seconds();
 			struct ldlm_lock *next;
+			time_t delta = 0;
 
 			next = list_entry(list_next, struct ldlm_lock,
 					  l_pending_chain);
+			if (next->l_callback_timeout - now > 0)
+				delta = lock->l_callback_timeout - now;
+
 			mod_timer(&waiting_locks_timer,
-				  cfs_time_seconds(next->l_callback_timeout));
+				  jiffies + cfs_time_seconds(delta));
 		}
 	}
 	list_del_init(&lock->l_pending_chain);