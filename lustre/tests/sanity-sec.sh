lustre/lustre-release

commit a21c13d4df4bea1bec0f5804136740ed53d5a57f

LU-8602 gss: Properly port gss to newer crypto api.
In newer kernels the old crypto_hash_* was removed which
requires the gss layer to now user crypto_ahash_*. Pieces
of the port were done incorrectly which this patch addresses.
In the process cleanup some cases where data was leaked
in the case of the sg_tables.

Address the limitation of the gss keys in that they are hard
coded to u16 variables that are not guaranteed to be constant
and it will prove to be challenge in the future to add new crypto
algorithms.
Keep u16 representation of hash and crypto algorithms in Shared-Keys
as they are stored on disk, to not break compatibility with already
existing keys. But pass in to kernel space the name string
of the crypto algorithm we want to use.

Also deal with timer_setup and key.usage new format introduced in
newer kernels.

Test-Parameters: testlist=sanity-sec envdefinitions=SHARED_KEY=true
Change-Id: I90ef58104a1955ce12603173964a6878f60b601e
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-on: https://review.whamcloud.com/28309
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/gss/gss_sk_mech.c
@@ -61,14 +61,14 @@
 #define SK_IV_REV_START (1ULL << 63)
 
 struct sk_ctx {
-	__u16			sc_hmac;
-	__u16			sc_crypt;
-	__u32			sc_expire;
-	__u32			sc_host_random;
-	__u32			sc_peer_random;
-	atomic64_t		sc_iv;
-	rawobj_t		sc_hmac_key;
-	struct gss_keyblock	sc_session_kb;
+	enum cfs_crypto_crypt_alg sc_crypt;
+	enum cfs_crypto_hash_alg  sc_hmac;
+	__u32			  sc_expire;
+	__u32			  sc_host_random;
+	__u32			  sc_peer_random;
+	atomic64_t		  sc_iv;
+	rawobj_t		  sc_hmac_key;
+	struct gss_keyblock	  sc_session_kb;
 };
 
 struct sk_hdr {
@@ -85,24 +85,6 @@ struct sk_wire {
 	rawobj_t		skw_header;
 	rawobj_t		skw_cipher;
 	rawobj_t		skw_hmac;
-};
-
-static struct sk_crypt_type sk_crypt_types[] = {
-	[SK_CRYPT_AES256_CTR] = {
-		.sct_name = "ctr(aes)",
-		.sct_bytes = 32,
-	},
-};
-
-static struct sk_hmac_type sk_hmac_types[] = {
-	[SK_HMAC_SHA256] = {
-		.sht_name = "hmac(sha256)",
-		.sht_bytes = 32,
-	},
-	[SK_HMAC_SHA512] = {
-		.sht_name = "hmac(sha512)",
-		.sht_bytes = 64,
-	},
 };
 
 static inline unsigned long sk_block_mask(unsigned long len, int blocksize)
@@ -147,22 +129,18 @@ void sk_construct_rfc3686_iv(__u8 *iv, __u32 nonce, __u64 partial_iv)
 	memcpy(iv, &ctr, sizeof(ctr));
 }
 
-static int sk_init_keys(struct sk_ctx *skc)
-{
-	return gss_keyblock_init(&skc->sc_session_kb,
-				 sk_crypt_types[skc->sc_crypt].sct_name, 0);
-}
-
 static int sk_fill_context(rawobj_t *inbuf, struct sk_ctx *skc)
 {
 	char *ptr = inbuf->data;
 	char *end = inbuf->data + inbuf->len;
-	__u32 tmp;
+	char sk_hmac[CRYPTO_MAX_ALG_NAME];
+	char sk_crypt[CRYPTO_MAX_ALG_NAME];
+	u32 tmp;
 
 	/* see sk_serialize_kctx() for format from userspace side */
 	/*  1. Version */
 	if (gss_get_bytes(&ptr, end, &tmp, sizeof(tmp))) {
-		CERROR("Failed to read shared key interface version");
+		CERROR("Failed to read shared key interface version\n");
 		return -1;
 	}
 	if (tmp != SK_INTERFACE_VERSION) {
@@ -171,28 +149,34 @@ static int sk_fill_context(rawobj_t *inbuf, struct sk_ctx *skc)
 	}
 
 	/* 2. HMAC type */
-	if (gss_get_bytes(&ptr, end, &skc->sc_hmac, sizeof(skc->sc_hmac))) {
-		CERROR("Failed to read HMAC algorithm type");
+	if (gss_get_bytes(&ptr, end, &sk_hmac, sizeof(sk_hmac))) {
+		CERROR("Failed to read HMAC algorithm type\n");
 		return -1;
 	}
-	if (skc->sc_hmac <= SK_HMAC_EMPTY || skc->sc_hmac >= SK_HMAC_MAX) {
-		CERROR("Invalid hmac type: %d\n", skc->sc_hmac);
+
+	skc->sc_hmac = cfs_crypto_hash_alg(sk_hmac);
+	if (skc->sc_hmac != CFS_HASH_ALG_NULL &&
+	    skc->sc_hmac != CFS_HASH_ALG_SHA256 &&
+	    skc->sc_hmac != CFS_HASH_ALG_SHA512) {
+		CERROR("Invalid hmac type: %s\n", sk_hmac);
 		return -1;
 	}
 
 	/* 3. crypt type */
-	if (gss_get_bytes(&ptr, end, &skc->sc_crypt, sizeof(skc->sc_crypt))) {
-		CERROR("Failed to read crypt algorithm type");
+	if (gss_get_bytes(&ptr, end, &sk_crypt, sizeof(sk_crypt))) {
+		CERROR("Failed to read crypt algorithm type\n");
 		return -1;
 	}
-	if (skc->sc_crypt <= SK_CRYPT_EMPTY || skc->sc_crypt >= SK_CRYPT_MAX) {
-		CERROR("Invalid crypt type: %d\n", skc->sc_crypt);
+
+	skc->sc_crypt = cfs_crypto_crypt_alg(sk_crypt);
+	if (skc->sc_crypt == CFS_CRYPT_ALG_UNKNOWN) {
+		CERROR("Invalid crypt type: %s\n", sk_crypt);
 		return -1;
 	}
 
 	/* 4. expiration time */
 	if (gss_get_bytes(&ptr, end, &tmp, sizeof(tmp))) {
-		CERROR("Failed to read context expiration time");
+		CERROR("Failed to read context expiration time\n");
 		return -1;
 	}
 	skc->sc_expire = tmp + ktime_get_real_seconds();
@@ -200,20 +184,20 @@ static int sk_fill_context(rawobj_t *inbuf, struct sk_ctx *skc)
 	/* 5. host random is used as nonce for encryption */
 	if (gss_get_bytes(&ptr, end, &skc->sc_host_random,
 			  sizeof(skc->sc_host_random))) {
-		CERROR("Failed to read host random ");
+		CERROR("Failed to read host random\n");
 		return -1;
 	}
 
 	/* 6. peer random is used as nonce for decryption */
 	if (gss_get_bytes(&ptr, end, &skc->sc_peer_random,
 			  sizeof(skc->sc_peer_random))) {
-		CERROR("Failed to read peer random ");
+		CERROR("Failed to read peer random\n");
 		return -1;
 	}
 
 	/* 7. HMAC key */
 	if (gss_get_rawobj(&ptr, end, &skc->sc_hmac_key)) {
-		CERROR("Failed to read HMAC key");
+		CERROR("Failed to read HMAC key\n");
 		return -1;
 	}
 	if (skc->sc_hmac_key.len <= SK_MIN_SIZE) {
@@ -224,7 +208,7 @@ static int sk_fill_context(rawobj_t *inbuf, struct sk_ctx *skc)
 
 	/* 8. Session key, can be empty if not using privacy mode */
 	if (gss_get_rawobj(&ptr, end, &skc->sc_session_kb.kb_key)) {
-		CERROR("Failed to read session key");
+		CERROR("Failed to read session key\n");
 		return -1;
 	}
 
@@ -262,7 +246,8 @@ __u32 gss_import_sec_context_sk(rawobj_t *inbuf, struct gss_ctx *gss_context)
 	/* Only privacy mode needs to initialize keys */
 	if (skc->sc_session_kb.kb_key.len > 0) {
 		privacy = true;
-		if (sk_init_keys(skc))
+		if (gss_keyblock_init(&skc->sc_session_kb,
+				      cfs_crypto_crypt_name(skc->sc_crypt), 0))
 			goto out_err;
 	}
 
@@ -303,7 +288,9 @@ __u32 gss_copy_reverse_context_sk(struct gss_ctx *gss_context_old,
 
 	/* Only privacy mode needs to initialize keys */
 	if (skc_new->sc_session_kb.kb_key.len > 0)
-		if (sk_init_keys(skc_new))
+		if (gss_keyblock_init(&skc_new->sc_session_kb,
+				      cfs_crypto_crypt_name(skc_new->sc_crypt),
+				      0))
 			goto out_err;
 
 	gss_context_new->internal_ctx_id = skc_new;
@@ -327,24 +314,26 @@ __u32 gss_inquire_context_sk(struct gss_ctx *gss_context,
 }
 
 static
-__u32 sk_make_hmac(char *alg_name, rawobj_t *key, int msg_count, rawobj_t *msgs,
-		   int iov_count, lnet_kiov_t *iovs, rawobj_t *token)
+u32 sk_make_hmac(enum cfs_crypto_hash_alg algo, rawobj_t *key, int msg_count,
+		 rawobj_t *msgs, int iov_count, lnet_kiov_t *iovs,
+		 rawobj_t *token)
 {
-	struct crypto_hash *tfm;
-	int rc;
+	struct cfs_crypto_hash_desc *desc;
+	int rc2, rc;
 
-	tfm = crypto_alloc_hash(alg_name, 0, 0);
-	if (IS_ERR(tfm))
-		return GSS_S_FAILURE;
+	desc = cfs_crypto_hash_init(algo, key->data, key->len);
+	if (IS_ERR(desc)) {
+		rc = PTR_ERR(desc);
+		goto out_init_failed;
+	}
 
-	rc = GSS_S_FAILURE;
-	LASSERT(token->len >= crypto_hash_digestsize(tfm));
-	if (!gss_digest_hmac(tfm, key, NULL, msg_count, msgs, iov_count, iovs,
-			    token))
-		rc = GSS_S_COMPLETE;
-
-	crypto_free_hash(tfm);
-	return rc;
+	rc2 = gss_digest_hash(desc, NULL, msg_count, msgs, iov_count, iovs,
+			      token);
+	rc = cfs_crypto_hash_final(desc, key->data, &key->len);
+	if (!rc && rc2)
+		rc = rc2;
+out_init_failed:
+	return rc ? GSS_S_FAILURE : GSS_S_COMPLETE;
 }
 
 static
@@ -356,20 +345,22 @@ __u32 gss_get_mic_sk(struct gss_ctx *gss_context,
 		     rawobj_t *token)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	return sk_make_hmac(sk_hmac_types[skc->sc_hmac].sht_name,
+
+	return sk_make_hmac(skc->sc_hmac,
 			    &skc->sc_hmac_key, message_count, messages,
 			    iov_count, iovs, token);
 }
 
 static
-__u32 sk_verify_hmac(struct sk_hmac_type *sht, rawobj_t *key, int message_count,
-			 rawobj_t *messages, int iov_count, lnet_kiov_t *iovs,
-			 rawobj_t *token)
+u32 sk_verify_hmac(enum cfs_crypto_hash_alg algo, rawobj_t *key,
+		   int message_count, rawobj_t *messages,
+		   int iov_count, lnet_kiov_t *iovs,
+		   rawobj_t *token)
 {
 	rawobj_t checksum = RAWOBJ_EMPTY;
 	__u32 rc = GSS_S_FAILURE;
 
-	checksum.len = sht->sht_bytes;
+	checksum.len = cfs_crypto_hash_digestsize(algo);
 	if (token->len < checksum.len) {
 		CDEBUG(D_SEC, "Token received too short, expected %d "
 		       "received %d\n", token->len, checksum.len);
@@ -380,8 +371,8 @@ __u32 sk_verify_hmac(struct sk_hmac_type *sht, rawobj_t *key, int message_count,
 	if (!checksum.data)
 		return rc;
 
-	if (sk_make_hmac(sht->sht_name, key, message_count, messages,
-			 iov_count, iovs, &checksum)) {
+	if (sk_make_hmac(algo, key, message_count,
+			 messages, iov_count, iovs, &checksum)) {
 		CDEBUG(D_SEC, "Failed to create checksum to validate\n");
 		goto cleanup;
 	}
@@ -404,23 +395,20 @@ cleanup:
  * to decrypt up to the number of bytes actually specified from the sender
  * (bd_nob) otherwise the calulated HMAC will be incorrect. */
 static
-__u32 sk_verify_bulk_hmac(struct sk_hmac_type *sht, rawobj_t *key,
-			  int msgcnt, rawobj_t *msgs, int iovcnt,
-			  lnet_kiov_t *iovs, int iov_bytes, rawobj_t *token)
+u32 sk_verify_bulk_hmac(enum cfs_crypto_hash_alg sc_hmac, rawobj_t *key,
+			int msgcnt, rawobj_t *msgs, int iovcnt,
+			lnet_kiov_t *iovs, int iov_bytes, rawobj_t *token)
 {
+	struct cfs_crypto_hash_desc *desc;
 	rawobj_t checksum = RAWOBJ_EMPTY;
-	struct crypto_hash *tfm;
-	struct hash_desc desc = {
-		.tfm = NULL,
-		.flags = 0,
-	};
+	struct ahash_request *req;
 	struct scatterlist sg[1];
+	int rc = GSS_S_FAILURE;
 	struct sg_table sgt;
 	int bytes;
 	int i;
-	int rc = GSS_S_FAILURE;
 
-	checksum.len = sht->sht_bytes;
+	checksum.len = cfs_crypto_hash_digestsize(sc_hmac);
 	if (token->len < checksum.len) {
 		CDEBUG(D_SEC, "Token received too short, expected %d "
 		       "received %d\n", token->len, checksum.len);
@@ -431,31 +419,21 @@ __u32 sk_verify_bulk_hmac(struct sk_hmac_type *sht, rawobj_t *key,
 	if (!checksum.data)
 		return rc;
 
-	tfm = crypto_alloc_hash(sht->sht_name, 0, 0);
-	if (IS_ERR(tfm))
+	desc = cfs_crypto_hash_init(sc_hmac, key->data, key->len);
+	if (IS_ERR(desc))
 		goto cleanup;
 
-	desc.tfm = tfm;
-
-	LASSERT(token->len >= crypto_hash_digestsize(tfm));
-
-	rc = crypto_hash_setkey(tfm, key->data, key->len);
-	if (rc)
-		goto hash_cleanup;
-
-	rc = crypto_hash_init(&desc);
-	if (rc)
-		goto hash_cleanup;
-
+	req = (struct ahash_request *) desc;
 	for (i = 0; i < msgcnt; i++) {
-		if (msgs[i].len == 0)
+		if (!msgs[i].len)
 			continue;
 
 		rc = gss_setup_sgtable(&sgt, sg, msgs[i].data, msgs[i].len);
 		if (rc != 0)
 			goto hash_cleanup;
 
-		rc = crypto_hash_update(&desc, sg, msgs[i].len);
+		ahash_request_set_crypt(req, sg, NULL, msgs[i].len);
+		rc = crypto_ahash_update(req);
 		if (rc) {
 			gss_teardown_sgtable(&sgt);
 			goto hash_cleanup;
@@ -474,12 +452,11 @@ __u32 sk_verify_bulk_hmac(struct sk_hmac_type *sht, rawobj_t *key,
 		sg_init_table(sg, 1);
 		sg_set_page(&sg[0], iovs[i].kiov_page, bytes,
 			    iovs[i].kiov_offset);
-		rc = crypto_hash_update(&desc, sg, bytes);
+		ahash_request_set_crypt(req, sg, NULL, bytes);
+		rc = crypto_ahash_update(req);
 		if (rc)
 			goto hash_cleanup;
 	}
-
-	crypto_hash_final(&desc, checksum.data);
 
 	if (memcmp(token->data, checksum.data, checksum.len)) {
 		rc = GSS_S_BAD_SIG;
@@ -489,7 +466,7 @@ __u32 sk_verify_bulk_hmac(struct sk_hmac_type *sht, rawobj_t *key,
 	rc = GSS_S_COMPLETE;
 
 hash_cleanup:
-	crypto_free_hash(tfm);
+	cfs_crypto_hash_final(desc, checksum.data, &checksum.len);
 
 cleanup:
 	OBD_FREE_LARGE(checksum.data, checksum.len);
@@ -506,7 +483,8 @@ __u32 gss_verify_mic_sk(struct gss_ctx *gss_context,
 			rawobj_t *token)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	return sk_verify_hmac(&sk_hmac_types[skc->sc_hmac], &skc->sc_hmac_key,
+
+	return sk_verify_hmac(skc->sc_hmac, &skc->sc_hmac_key,
 			      message_count, messages, iov_count, iovs, token);
 }
 
@@ -516,7 +494,7 @@ __u32 gss_wrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 		    rawobj_t *token)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	struct sk_hmac_type *sht = &sk_hmac_types[skc->sc_hmac];
+	size_t sht_bytes = cfs_crypto_hash_digestsize(skc->sc_hmac);
 	struct sk_wire skw;
 	struct sk_hdr skh;
 	rawobj_t msgbufs[3];
@@ -540,7 +518,7 @@ __u32 gss_wrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 
 	sk_construct_rfc3686_iv(local_iv, skc->sc_host_random, skh.skh_iv);
 	skw.skw_cipher.data = skw.skw_header.data + skw.skw_header.len;
-	skw.skw_cipher.len = token->len - skw.skw_header.len - sht->sht_bytes;
+	skw.skw_cipher.len = token->len - skw.skw_header.len - sht_bytes;
 	if (gss_crypt_rawobjs(skc->sc_session_kb.kb_tfm, local_iv, 1, message,
 			      &skw.skw_cipher, 1))
 		return GSS_S_FAILURE;
@@ -551,9 +529,9 @@ __u32 gss_wrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 	msgbufs[2] = skw.skw_cipher;
 
 	skw.skw_hmac.data = skw.skw_cipher.data + skw.skw_cipher.len;
-	skw.skw_hmac.len = sht->sht_bytes;
-	if (sk_make_hmac(sht->sht_name, &skc->sc_hmac_key, 3, msgbufs, 0,
-			 NULL, &skw.skw_hmac))
+	skw.skw_hmac.len = sht_bytes;
+	if (sk_make_hmac(skc->sc_hmac, &skc->sc_hmac_key,
+			 3, msgbufs, 0, NULL, &skw.skw_hmac))
 		return GSS_S_FAILURE;
 
 	token->len = skw.skw_header.len + skw.skw_cipher.len + skw.skw_hmac.len;
@@ -566,7 +544,7 @@ __u32 gss_unwrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 		      rawobj_t *token, rawobj_t *message)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	struct sk_hmac_type *sht = &sk_hmac_types[skc->sc_hmac];
+	size_t sht_bytes = cfs_crypto_hash_digestsize(skc->sc_hmac);
 	struct sk_wire skw;
 	struct sk_hdr *skh;
 	rawobj_t msgbufs[3];
@@ -576,15 +554,15 @@ __u32 gss_unwrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 
 	LASSERT(skc->sc_session_kb.kb_tfm);
 
-	if (token->len < sizeof(skh) + sht->sht_bytes)
+	if (token->len < sizeof(skh) + sht_bytes)
 		return GSS_S_DEFECTIVE_TOKEN;
 
 	skw.skw_header.data = token->data;
 	skw.skw_header.len = sizeof(struct sk_hdr);
 	skw.skw_cipher.data = skw.skw_header.data + skw.skw_header.len;
-	skw.skw_cipher.len = token->len - skw.skw_header.len - sht->sht_bytes;
+	skw.skw_cipher.len = token->len - skw.skw_header.len - sht_bytes;
 	skw.skw_hmac.data = skw.skw_cipher.data + skw.skw_cipher.len;
-	skw.skw_hmac.len = sht->sht_bytes;
+	skw.skw_hmac.len = sht_bytes;
 
 	blocksize = crypto_blkcipher_blocksize(skc->sc_session_kb.kb_tfm);
 	if (skw.skw_cipher.len % blocksize != 0)
@@ -599,8 +577,8 @@ __u32 gss_unwrap_sk(struct gss_ctx *gss_context, rawobj_t *gss_header,
 	msgbufs[0] = skw.skw_header;
 	msgbufs[1] = *gss_header;
 	msgbufs[2] = skw.skw_cipher;
-	rc = sk_verify_hmac(sht, &skc->sc_hmac_key, 3, msgbufs, 0, NULL,
-			    &skw.skw_hmac);
+	rc = sk_verify_hmac(skc->sc_hmac, &skc->sc_hmac_key, 3, msgbufs,
+			    0, NULL, &skw.skw_hmac);
 	if (rc)
 		return rc;
 
@@ -809,7 +787,7 @@ __u32 gss_wrap_bulk_sk(struct gss_ctx *gss_context,
 		       int adj_nob)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	struct sk_hmac_type *sht = &sk_hmac_types[skc->sc_hmac];
+	size_t sht_bytes = cfs_crypto_hash_digestsize(skc->sc_hmac);
 	struct sk_wire skw;
 	struct sk_hdr skh;
 	__u8 local_iv[SK_IV_SIZE];
@@ -826,14 +804,14 @@ __u32 gss_wrap_bulk_sk(struct gss_ctx *gss_context,
 
 	sk_construct_rfc3686_iv(local_iv, skc->sc_host_random, skh.skh_iv);
 	skw.skw_cipher.data = skw.skw_header.data + skw.skw_header.len;
-	skw.skw_cipher.len = token->len - skw.skw_header.len - sht->sht_bytes;
+	skw.skw_cipher.len = token->len - skw.skw_header.len - sht_bytes;
 	if (sk_encrypt_bulk(skc->sc_session_kb.kb_tfm, local_iv,
 			    desc, &skw.skw_cipher, adj_nob))
 		return GSS_S_FAILURE;
 
 	skw.skw_hmac.data = skw.skw_cipher.data + skw.skw_cipher.len;
-	skw.skw_hmac.len = sht->sht_bytes;
-	if (sk_make_hmac(sht->sht_name, &skc->sc_hmac_key, 1, &skw.skw_cipher,
+	skw.skw_hmac.len = sht_bytes;
+	if (sk_make_hmac(skc->sc_hmac, &skc->sc_hmac_key, 1, &skw.skw_cipher,
 			 desc->bd_iov_count, GET_ENC_KIOV(desc), &skw.skw_hmac))
 		return GSS_S_FAILURE;
 
@@ -846,7 +824,7 @@ __u32 gss_unwrap_bulk_sk(struct gss_ctx *gss_context,
 			   rawobj_t *token, int adj_nob)
 {
 	struct sk_ctx *skc = gss_context->internal_ctx_id;
-	struct sk_hmac_type *sht = &sk_hmac_types[skc->sc_hmac];
+	size_t sht_bytes = cfs_crypto_hash_digestsize(skc->sc_hmac);
 	struct sk_wire skw;
 	struct sk_hdr *skh;
 	__u8 local_iv[SK_IV_SIZE];
@@ -854,25 +832,25 @@ __u32 gss_unwrap_bulk_sk(struct gss_ctx *gss_context,
 
 	LASSERT(skc->sc_session_kb.kb_tfm);
 
-	if (token->len < sizeof(skh) + sht->sht_bytes)
+	if (token->len < sizeof(skh) + sht_bytes)
 		return GSS_S_DEFECTIVE_TOKEN;
 
 	skw.skw_header.data = token->data;
 	skw.skw_header.len = sizeof(struct sk_hdr);
 	skw.skw_cipher.data = skw.skw_header.data + skw.skw_header.len;
-	skw.skw_cipher.len = token->len - skw.skw_header.len - sht->sht_bytes;
+	skw.skw_cipher.len = token->len - skw.skw_header.len - sht_bytes;
 	skw.skw_hmac.data = skw.skw_cipher.data + skw.skw_cipher.len;
-	skw.skw_hmac.len = sht->sht_bytes;
+	skw.skw_hmac.len = sht_bytes;
 
 	skh = (struct sk_hdr *)skw.skw_header.data;
 	rc = sk_verify_header(skh);
 	if (rc != GSS_S_COMPLETE)
 		return rc;
 
-	rc = sk_verify_bulk_hmac(&sk_hmac_types[skc->sc_hmac],
-				 &skc->sc_hmac_key, 1, &skw.skw_cipher,
-				 desc->bd_iov_count, GET_ENC_KIOV(desc),
-				 desc->bd_nob, &skw.skw_hmac);
+	rc = sk_verify_bulk_hmac(skc->sc_hmac, &skc->sc_hmac_key, 1,
+				 &skw.skw_cipher, desc->bd_iov_count,
+				 GET_ENC_KIOV(desc), desc->bd_nob,
+				 &skw.skw_hmac);
 	if (rc)
 		return rc;
 

libcfs/include/libcfs/libcfs_crypto.h
@@ -38,6 +38,12 @@ struct cfs_crypto_hash_type {
 	unsigned int    cht_size;       /**< hash digest size */
 };
 
+struct cfs_crypto_crypt_type {
+	char	       *cct_name;	  /**< crypto algorithm name, equal to
+					   * format name for crypto api */
+	unsigned int    cct_size;         /**< crypto key size */
+};
+
 enum cfs_crypto_hash_alg {
 	CFS_HASH_ALG_NULL	= 0,
 	CFS_HASH_ALG_ADLER32,
@@ -52,6 +58,13 @@ enum cfs_crypto_hash_alg {
 	CFS_HASH_ALG_MAX,
 	CFS_HASH_ALG_SPEED_MAX = CFS_HASH_ALG_MD5,
 	CFS_HASH_ALG_UNKNOWN	= 0xff
+};
+
+enum cfs_crypto_crypt_alg {
+	CFS_CRYPT_ALG_NULL	= 0,
+	CFS_CRYPT_ALG_AES256_CTR,
+	CFS_CRYPT_ALG_MAX,
+	CFS_CRYPT_ALG_UNKNOWN	= 0xff
 };
 
 static struct cfs_crypto_hash_type hash_types[] = {
@@ -104,6 +117,17 @@ static struct cfs_crypto_hash_type hash_types[] = {
 		.cht_name	= NULL,
 		.cht_key	= 0,
 		.cht_size	= 64
+	}
+};
+
+static struct cfs_crypto_crypt_type crypt_types[] = {
+	[CFS_CRYPT_ALG_NULL] = {
+		.cct_name	= "null",
+		.cct_size	= 0
+	},
+	[CFS_CRYPT_ALG_AES256_CTR] = {
+		.cct_name	= "ctr(aes)",
+		.cct_size	= 32
 	}
 };
 
@@ -186,6 +210,86 @@ static inline unsigned char cfs_crypto_hash_alg(const char *algname)
 			return hash_alg;
 
 	return CFS_HASH_ALG_UNKNOWN;
+}
+
+/**
+ * Return crypt algorithm information for the specified algorithm identifier
+ *
+ * Crypt information includes algorithm name, key size.
+ *
+ * \retval		cfs_crypto_crupt_type for valid ID (CFS_CRYPT_ALG_*)
+ * \retval		NULL for unknown algorithm identifier
+ */
+static inline const struct
+cfs_crypto_crypt_type *cfs_crypto_crypt_type(
+	enum cfs_crypto_crypt_alg crypt_alg)
+{
+	struct cfs_crypto_crypt_type *ct;
+
+	if (crypt_alg < CFS_CRYPT_ALG_MAX) {
+		ct = &crypt_types[crypt_alg];
+		if (ct->cct_name != NULL)
+			return ct;
+	}
+	return NULL;
+}
+
+/**
+ * Return crypt name for crypt algorithm identifier
+ *
+ * \param[in] crypt_alg	crypt alrgorithm id (CFS_CRYPT_ALG_*)
+ *
+ * \retval		string name of known crypt algorithm
+ * \retval		"unknown" if hash algorithm is unknown
+ */
+static inline const
+char *cfs_crypto_crypt_name(enum cfs_crypto_crypt_alg crypt_alg)
+{
+	const struct cfs_crypto_crypt_type *ct;
+
+	ct = cfs_crypto_crypt_type(crypt_alg);
+	if (ct)
+		return ct->cct_name;
+
+	return "unknown";
+}
+
+
+/**
+ * Return key size for crypto algorithm type
+ *
+ * \param[in] crypt_alg	crypt alrgorithm id (CFS_CRYPT_ALG_*)
+ *
+ * \retval		crypt algorithm key size in bytes
+ * \retval		0 if crypt algorithm type is unknown
+ */
+static inline
+unsigned int cfs_crypto_crypt_keysize(enum cfs_crypto_crypt_alg crypt_alg)
+{
+	const struct cfs_crypto_crypt_type *ct;
+
+	ct = cfs_crypto_crypt_type(crypt_alg);
+	if (ct != NULL)
+		return ct->cct_size;
+
+	return 0;
+}
+
+/**
+ * Find crypto algorithm ID for the specified algorithm name
+ *
+ * \retval		crypto algorithm ID for valid ID (CFS_CRYPT_ALG_*)
+ * \retval		CFS_CRYPT_ALG_UNKNOWN for unknown algorithm name
+ */
+static inline unsigned char cfs_crypto_crypt_alg(const char *algname)
+{
+	enum cfs_crypto_crypt_alg crypt_alg;
+
+	for (crypt_alg = 0; crypt_alg < CFS_CRYPT_ALG_MAX; crypt_alg++)
+		if (strcmp(crypt_types[crypt_alg].cct_name, algname) == 0)
+			return crypt_alg;
+
+	return CFS_CRYPT_ALG_UNKNOWN;
 }
 
 int cfs_crypto_hash_digest(enum cfs_crypto_hash_alg hash_alg,

commit dcdf060342e7d69b64171840cf9475bf65d036ea

LU-12894 sec: fix checksum for skpi
Compute checkum on message before actually comparing
it to hmac value.

Add test to exercise all SSK flavors.
Make sure zconf_mount does include skpath mount option if SSK or
Kerberos is in use.

Fixes: a21c13d4df ("LU-8602 gss: Properly port gss to newer crypto api.")
Test-Parameters: envdefinitions=SHARED_KEY=true testlist=sanity-sec
Test-Parameters: envdefinitions=SHARED_KEY=true,SK_FLAVOR=skn testlist=sanity,recovery-small
Test-Parameters: envdefinitions=SHARED_KEY=true,SK_FLAVOR=ska testlist=sanity,recovery-small
Test-Parameters: envdefinitions=SHARED_KEY=true,SK_FLAVOR=ski testlist=sanity,recovery-small
Test-Parameters: envdefinitions=SHARED_KEY=true,SK_FLAVOR=skpi testlist=sanity,recovery-small
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7bcc3618c1824a0f0ca73219c7ac0ccc8405b946
Reviewed-on: https://review.whamcloud.com/36604
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Nunez <jnunez@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/gss/gss_sk_mech.c
@@ -409,7 +409,7 @@ u32 sk_verify_bulk_hmac(enum cfs_crypto_hash_alg sc_hmac, rawobj_t *key,
 	rawobj_t checksum = RAWOBJ_EMPTY;
 	struct ahash_request *req;
 	struct scatterlist sg[1];
-	int rc = GSS_S_FAILURE;
+	int rc = 0;
 	struct sg_table sgt;
 	int bytes;
 	int i;
@@ -423,11 +423,13 @@ u32 sk_verify_bulk_hmac(enum cfs_crypto_hash_alg sc_hmac, rawobj_t *key,
 
 	OBD_ALLOC_LARGE(checksum.data, checksum.len);
 	if (!checksum.data)
-		return rc;
+		return GSS_S_FAILURE;
 
 	req = cfs_crypto_hash_init(sc_hmac, key->data, key->len);
-	if (IS_ERR(req))
+	if (IS_ERR(req)) {
+		rc = GSS_S_FAILURE;
 		goto cleanup;
+	}
 
 	for (i = 0; i < msgcnt; i++) {
 		if (!msgs[i].len)
@@ -463,15 +465,15 @@ u32 sk_verify_bulk_hmac(enum cfs_crypto_hash_alg sc_hmac, rawobj_t *key,
 			goto hash_cleanup;
 	}
 
-	if (memcmp(token->data, checksum.data, checksum.len)) {
-		rc = GSS_S_BAD_SIG;
-		goto hash_cleanup;
-	}
-
-	rc = GSS_S_COMPLETE;
-
 hash_cleanup:
 	cfs_crypto_hash_final(req, checksum.data, &checksum.len);
+	if (rc)
+		goto cleanup;
+
+	if (memcmp(token->data, checksum.data, checksum.len))
+		rc = GSS_S_BAD_SIG;
+	else
+		rc = GSS_S_COMPLETE;
 
 cleanup:
 	OBD_FREE_LARGE(checksum.data, checksum.len);

lustre/tests/sanity-sec.sh
@@ -2147,22 +2147,26 @@ test_29() {
 	touch $DIR/$tdir/$tfile || error "touch"
 	zconf_umount_clients ${clients_arr[0]} $MOUNT ||
 		error "unable to umount clients"
-	keyctl show | awk '/lustre/ { print $1 }' |
-		xargs -IX keyctl unlink X
+	do_node ${clients_arr[0]} "keyctl show |
+		awk '/lustre/ { print \\\$1 }' | xargs -IX keyctl unlink X"
 	OLD_SK_PATH=$SK_PATH
 	export SK_PATH=/dev/null
 	if zconf_mount_clients ${clients_arr[0]} $MOUNT; then
 		export SK_PATH=$OLD_SK_PATH
-		if [ -e $DIR/$tdir/$tfile ]; then
+		do_node ${clients_arr[0]} "ls $DIR/$tdir/$tfile"
+		if [ $? -eq 0 ]; then
 			error "able to mount and read without key"
 		else
 			error "able to mount without key"
 		fi
 	else
 		export SK_PATH=$OLD_SK_PATH
-		keyctl show | awk '/lustre/ { print $1 }' |
-			xargs -IX keyctl unlink X
+		do_node ${clients_arr[0]} "keyctl show |
+			awk '/lustre/ { print \\\$1 }' |
+			xargs -IX keyctl unlink X"
 	fi
+	zconf_mount_clients ${clients_arr[0]} $MOUNT ||
+		error "unable to mount clients"
 }
 run_test 29 "check for missing shared key"
 
@@ -2178,17 +2182,19 @@ test_30() {
 	zconf_umount_clients ${clients_arr[0]} $MOUNT ||
 		error "unable to umount clients"
 	# unload keys from ring
-	keyctl show | awk '/lustre/ { print $1 }' |
-		xargs -IX keyctl unlink X
+	do_node ${clients_arr[0]} "keyctl show |
+		awk '/lustre/ { print \\\$1 }' | xargs -IX keyctl unlink X"
 	# invalidate the key with bogus filesystem name
-	lgss_sk -w $SK_PATH/$FSNAME-bogus.key -f $FSNAME.bogus \
-		-t client -d /dev/urandom || error "lgss_sk failed (1)"
+	do_node ${clients_arr[0]} "lgss_sk -w $SK_PATH/$FSNAME-bogus.key \
+		-f $FSNAME.bogus -t client -d /dev/urandom" ||
+		error "lgss_sk failed (1)"
 	do_facet $SINGLEMDS lfs flushctx || error "could not run flushctx"
 	OLD_SK_PATH=$SK_PATH
 	export SK_PATH=$SK_PATH/$FSNAME-bogus.key
 	if zconf_mount_clients ${clients_arr[0]} $MOUNT; then
 		SK_PATH=$OLD_SK_PATH
-		if [ -a $DIR/$tdir/$tdir.out ]; then
+		do_node ${clients_arr[0]} "ls $DIR/$tdir/$tdir.out"
+		if [ $? -eq 0 ]; then
 			error "mount and read file with invalid key"
 		else
 			error "mount with invalid key"
@@ -2197,8 +2203,45 @@ test_30() {
 	SK_PATH=$OLD_SK_PATH
 	zconf_umount_clients ${clients_arr[0]} $MOUNT ||
 		error "unable to umount clients"
+	zconf_mount_clients ${clients_arr[0]} $MOUNT ||
+		error "unable to mount clients"
 }
 run_test 30 "check for invalid shared key"
+
+basic_ios() {
+	local flvr=$1
+
+	mkdir -p $DIR/$tdir || error "mkdir $flvr"
+	touch $DIR/$tdir/f0 || error "touch $flvr"
+	ls $DIR/$tdir || error "ls $flvr"
+	dd if=/dev/zero of=$DIR/$tdir/f0 conv=fsync bs=1M count=10 \
+		>& /dev/null || error "dd $flvr"
+	rm -f $DIR/$tdir/f0 || error "rm $flvr"
+	rmdir $DIR/$tdir || error "rmdir $flvr"
+
+	sync ; sync
+	echo 3 > /proc/sys/vm/drop_caches
+}
+
+test_30b() {
+	local save_flvr=$SK_FLAVOR
+
+	if ! $SHARED_KEY; then
+		skip "need shared key feature for this test"
+	fi
+
+	stack_trap restore_to_default_flavor EXIT
+
+	for flvr in skn ska ski skpi; do
+		# set flavor
+		SK_FLAVOR=$flvr
+		restore_to_default_flavor || error "cannot set $flvr flavor"
+		SK_FLAVOR=$save_flvr
+
+		basic_ios $flvr
+	done
+}
+run_test 30b "basic test of all different SSK flavors"
 
 cleanup_31() {
 	# unmount client

lustre/tests/test-framework.sh
@@ -2259,6 +2259,11 @@ zconf_mount() {
 		exit 1
 	fi
 
+	if $GSS_SK; then
+		# update mount option with skpath
+		opts=$(add_sk_mntflag $opts)
+	fi
+
 	echo "Starting client: $client: $flags $opts $device $mnt"
 	do_node $client mkdir -p $mnt
 	if [ -n "$FILESET" -a -z "$SKIP_FILESET" ];then