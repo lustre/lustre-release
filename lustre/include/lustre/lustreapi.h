lustre/lustre-release

commit 2a4821b836c82764ff13dbf32ba2386c9d3dca8e

LU-12159 utils: improve lfs getname functionality
Add "-n" and "-i" options to lfs getname to allow printing only
the fsname or instance ID of the filesystem(s).

Split out the documentation to a separate lfs-getname.1 man page.

Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ie132513325b6630fc5103a89b469271ba7392cb2
Reviewed-on: https://review.whamcloud.com/34595
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Ben Evans <bevans@cray.com>
Reviewed-by: Arshad Hussain <arshad.super@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/doc/lfs-getname.1
@@ -1,0 +1,43 @@
+.TH LFS-GETSOM 1 2018-10-04 "Lustre" "lustre Utilities"
+.SH NAME
+lfs getname \- retrieve the filesystem name and instance ID
+.SH SYNOPSIS
+.B lfs getname
+.RB [ --help | -h "] [" --instance | -i "] [" --fsname | -n ]
+.RI [ pathname ...]
+.SH DESCRIPTION
+Print the filesystem name, instance ID, and mountpoint for all
+Lustre mount points on the local client. If one or more
+.I pathname
+arguments are provided, then only print the Lustre filesystem name
+and instance ID for these mount points.
+.SH OPTIONS
+.TP
+.BR --help | -h
+Print the usage message.
+.TP
+.BR --fsname | -n
+Print only the Lustre filesystem name for each filesystem.  If
+.B -n
+is used with a single
+.I pathname
+then the filesystem name is printed without the mountpoint.
+.TP
+.BR --instance | -i
+Only show the mount instance ID for each filesystem.  If
+.B -i
+is used with a single
+.I pathname
+then the instance ID is printed without the mountpoint.
+.SH EXAMPLES
+.TP
+.B $ lfs getname
+myth-ffff880428703400 /myth
+.br
+testfs-ffff937009271000 /mnt/testfs
+.TP
+.B $ lfs getname -n /mnt/testfs
+testfs
+.SH SEE ALSO
+.BR lfs (1),
+.BR lustre (7)

lustre/utils/lfs.c
@@ -413,26 +413,26 @@ command_t cmdlist[] = {
 	 "\thashtype:	hash type of the striped directory.\n"
 	 "\t		fnv_1a_64 FNV-1a hash algorithm\n"
 	 "\t		all_char  sum of characters % MDT_COUNT\n"},
-        {"check", lfs_check, 0,
+	{"check", lfs_check, 0,
 	 "Display the status of MGTs, MDTs or OSTs (as specified in the command)\n"
 	 "or all the servers (MGTs, MDTs and OSTs).\n"
 	 "usage: check <mgts|osts|mdts|all>"},
-        {"osts", lfs_osts, 0, "list OSTs connected to client "
-         "[for specified path only]\n" "usage: osts [path]"},
-        {"mdts", lfs_mdts, 0, "list MDTs connected to client "
-         "[for specified path only]\n" "usage: mdts [path]"},
-        {"df", lfs_df, 0,
-         "report filesystem disk space usage or inodes usage"
-         "of each MDS and all OSDs or a batch belonging to a specific pool .\n"
-         "Usage: df [-i] [-h] [--lazy|-l] [--pool|-p <fsname>[.<pool>] [path]"},
-        {"getname", lfs_getname, 0, "list instances and specified mount points "
-         "[for specified path only]\n"
-         "Usage: getname [-h]|[path ...] "},
+	{"osts", lfs_osts, 0, "list OSTs connected to client "
+	 "[for specified path only]\n" "usage: osts [path]"},
+	{"mdts", lfs_mdts, 0, "list MDTs connected to client "
+	 "[for specified path only]\n" "usage: mdts [path]"},
+	{"df", lfs_df, 0,
+	 "report filesystem disk space usage or inodes usage "
+	 "of each MDS and all OSDs or a batch belonging to a specific pool.\n"
+	 "Usage: df [-i] [-h] [--lazy|-l] [--pool|-p <fsname>[.<pool>] [path]"},
+	{"getname", lfs_getname, 0,
+	 "list instances and specified mount points [for specified path only]\n"
+	 "Usage: getname [--help|-h] [--instance|-i] [--fsname|-n] [path ...]"},
 #ifdef HAVE_SYS_QUOTA_H
-        {"setquota", lfs_setquota, 0, "Set filesystem quotas.\n"
+	{"setquota", lfs_setquota, 0, "Set filesystem quotas.\n"
 	 "usage: setquota <-u|-g|-p> <uname>|<uid>|<gname>|<gid>|<projid>\n"
-         "                -b <block-softlimit> -B <block-hardlimit>\n"
-         "                -i <inode-softlimit> -I <inode-hardlimit> <filesystem>\n"
+	 "                -b <block-softlimit> -B <block-hardlimit>\n"
+	 "                -i <inode-softlimit> -I <inode-hardlimit> <filesystem>\n"
 	 "       setquota <-u|--user|-g|--group|-p|--projid> <uname>|<uid>|<gname>|<gid>|<projid>\n"
          "                [--block-softlimit <block-softlimit>]\n"
          "                [--block-hardlimit <block-hardlimit>]\n"
@@ -5442,43 +5442,104 @@ static int lfs_df(int argc, char **argv)
 	return rc;
 }
 
+static int print_instance(const char *mntdir, char *fsname, size_t fsnamelen,
+			  bool opt_instance, bool opt_fsname, bool opt_mntdir)
+{
+	char *buf = fsname;
+
+	/* llapi_search_mounts() fills "fsname", but that is not called if
+	 * explicit paths are specified on the command-line
+	 */
+	if (opt_instance || (opt_fsname && fsname[0] == '\0')) {
+		int rc = llapi_getname(mntdir, fsname, fsnamelen);
+
+		if (rc < 0) {
+			fprintf(stderr, "cannot get instance for '%s': %s\n",
+				mntdir, strerror(-rc));
+			return rc;
+		}
+		buf = fsname;
+		if (!opt_instance) {
+			/* print only the fsname name */
+			buf = strchr(fsname, '-');
+			if (buf)
+				*buf = '\0';
+			buf = fsname;
+		} else if (!opt_fsname) {
+			/* print only the instance name */
+			buf = strchr(fsname, '-');
+			if (buf)
+				buf++;
+			else
+				buf = fsname;
+		}
+	} else if (opt_fsname) {
+		/* print only the fsname */
+		buf = fsname;
+	}
+
+	printf("%s %s\n", buf, opt_mntdir ? mntdir : "");
+
+	return 0;
+}
+
 static int lfs_getname(int argc, char **argv)
 {
-        char mntdir[PATH_MAX] = "", path[PATH_MAX] = "", fsname[PATH_MAX] = "";
-        int rc = 0, index = 0, c;
-        char buf[sizeof(struct obd_uuid)];
+	struct option long_opts[] = {
+	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
+	{ .val = 'i',	.name = "instance",	.has_arg = no_argument },
+	{ .val = 'n',	.name = "fsname",	.has_arg = no_argument },
+	{ .name = NULL} };
+	bool opt_instance = false, opt_fsname = false;
+	char fsname[PATH_MAX] = "";
+	int rc = 0, rc2, c;
 
-        while ((c = getopt(argc, argv, "h")) != -1)
-                return CMD_HELP;
+	while ((c = getopt_long(argc, argv, "hin", long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'i':
+			opt_instance = true;
+			break;
+		case 'n':
+			opt_fsname = true;
+			break;
+		case 'h':
+		default:
+			return CMD_HELP;
+		}
+	}
 
-        if (optind == argc) { /* no paths specified, get all paths. */
-                while (!llapi_search_mounts(path, index++, mntdir, fsname)) {
-                        rc = llapi_getname(mntdir, buf, sizeof(buf));
-                        if (rc < 0) {
-                                fprintf(stderr,
-                                        "cannot get name for `%s': %s\n",
-                                        mntdir, strerror(-rc));
-                                break;
-                        }
+	/* If neither option is given, print both instance and fsname */
+	if (!opt_instance && !opt_fsname)
+		opt_instance = opt_fsname = true;
 
-                        printf("%s %s\n", buf, mntdir);
+	if (optind == argc) { /* no paths specified, get all paths. */
+		char mntdir[PATH_MAX] = "", path[PATH_MAX] = "";
+		int index = 0;
 
-                        path[0] = fsname[0] = mntdir[0] = 0;
-                }
-        } else { /* paths specified, only attempt to search these. */
-                for (; optind < argc; optind++) {
-                        rc = llapi_getname(argv[optind], buf, sizeof(buf));
-                        if (rc < 0) {
-                                fprintf(stderr,
-                                        "cannot get name for `%s': %s\n",
-                                        argv[optind], strerror(-rc));
-                                break;
-                        }
+		while (!llapi_search_mounts(path, index++, mntdir, fsname)) {
+			rc2 = print_instance(mntdir, fsname, sizeof(fsname),
+					     opt_instance, opt_fsname, true);
+			if (!rc)
+				rc = rc2;
+			path[0] = fsname[0] = mntdir[0] = '\0';
+		}
+	} else { /* paths specified, only attempt to search these. */
+		bool opt_mntdir;
 
-                        printf("%s %s\n", buf, argv[optind]);
-                }
-        }
-        return rc;
+		/* if only one path is given, print only requested info */
+		opt_mntdir = argc - optind > 1 || (opt_instance == opt_fsname);
+
+		for (; optind < argc; optind++) {
+			rc2 = print_instance(argv[optind], fsname,
+					     sizeof(fsname), opt_instance,
+					     opt_fsname, opt_mntdir);
+			if (!rc)
+				rc = rc2;
+			fsname[0] = '\0';
+		}
+	}
+
+	return rc;
 }
 
 static int lfs_check(int argc, char **argv)

lustre/doc/lfs.1
@@ -44,7 +44,9 @@ lfs \- client utility for Lustre-specific file layout and other attributes
       [[\fB!\fR] \fB--uid\fR|\fB-u\fR|\fB--user\fR|\fB-U
 \fR<\fIuname\fR>|<\fIuid\fR>]
 .br
-.B lfs getname [-h]|[path ...]
+.B lfs getname
+.RB [ --help | -h "] [" --instance | -i "] [" --fsname | -n "] ["
+.IR path ...]
 .br
 .B lfs getstripe
 [\fB--component-count\fR|\fB--comp-count\fR]
@@ -184,12 +186,6 @@ data has not been changed during an archive operation or before a release
 operation, and by OST migration, primarily for verifying that file data has not
 been changed during a data copy, when done in non-blocking mode.
 .TP
-.B getname [-h]|[path ...]
-Report all the Lustre mount points and the corresponding Lustre filesystem
-instance. If one or more \fBpath\fR entries are provided, then only the
-Lustre instance for these mount points is returned. If the path given is not on
-a Lustre instance 'No such device' is reported.
-.TP
 .B osts
 .RB [ path ]
 List all the OSTs for all mounted filesystems. If a \fBpath\fR is provided
@@ -312,8 +308,9 @@ The lfs command is part of the Lustre filesystem.
 .BR lctl (8),
 .BR lfs-df (1),
 .BR lfs-find (1),
-.BR lfs-getstripe (1),
 .BR lfs-getdirstripe (1),
+.BR lfs-getname (1),
+.BR lfs-getstripe (1),
 .BR lfs-hsm (1),
 .BR lfs-mkdir (1),
 .BR lfs-migrate (1),

commit 00d14521ca1cd38a5fff82e1639ad3cc944ee13d

LU-12521 llapi: add separate fsname and instance API
The llapi_getname() function returns the combined fsname and client
instance as one string, which is fine when using the entire string,
but the output cannot be safely parsed into separate fsname and
instance strings in all cases.

Introduce new llapi_get_fsname() and llapi_get_instance() functions
that return only the fsname and instance strings, since the source
string returned from the kernel can be unambiguously separated before
it is returned in a combined string via llapi_getname().

Fix the lfs_getname() '-n' and '-i' options to use the new routines
rather than parsing the output from llapi_getname().

Add man pages for these functions.

Fixes: 2a4821b836c8 ("LU-12159 utils: improve lfs getname functionality")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Iaf5846a0ae147a428f66ec8a1d0251e7e12540e5
Reviewed-on: https://review.whamcloud.com/35451
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/doc/Makefile.am
@@ -85,10 +85,14 @@ MANFILES =					\
 	llapi_file_create_foreign.3		\
 	llapi_file_get_stripe.3			\
 	llapi_file_open.3			\
+	llapi_get_fsname.3			\
+	llapi_get_fsname_instance.3		\
+	llapi_get_instance.3			\
 	llapi_get_lum_dir.3			\
 	llapi_get_lum_dir_fd.3			\
 	llapi_get_lum_file.3			\
-	llapi_get_lum_file_fd.3		\
+	llapi_get_lum_file_fd.3			\
+	llapi_getname.3				\
 	llapi_group_lock.3			\
 	llapi_group_unlock.3			\
 	llapi_heat_get.3			\

lustre/doc/lfs-getname.1
@@ -30,14 +30,20 @@ is used with a single
 .I pathname
 then the instance ID is printed without the mountpoint.
 .SH EXAMPLES
-.TP
 .B $ lfs getname
+.br
 myth-ffff880428703400 /myth
 .br
 testfs-ffff937009271000 /mnt/testfs
-.TP
+.P
 .B $ lfs getname -n /mnt/testfs
+.br
 testfs
+.P
+.B $ lfs getname -i /mnt/testfs
+.br
+ffff937009271000
 .SH SEE ALSO
 .BR lfs (1),
+.BR llapi_getname (3),
 .BR lustre (7)

lustre/doc/llapi_get_fsname.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_fsname_instance.3

lustre/doc/llapi_get_fsname_instance.3
@@ -1,0 +1,97 @@
+.TH lustreapi 3 "2019 Jul 10" "Lustre User API"
+.SH NAME
+llapi_get_fsname_instance - get the client filesystem name and instance
+.br
+llapi_get_fsname - get the filesystem fsname from path
+.br
+llapi_get_instance - get the client filesystem instance from path
+.br
+llapi_getname - get the client filesystem identifier from path
+.SH SYNOPSIS
+.nf
+.B #include <sys/types.h>
+.B #include <sys/stat.h>
+.B #include <fcntl.h>
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_getname(const char *" path ", char *" name ", size_t " namelen );
+.sp
+.BI "int llapi_get_fsname_instance(const char *" path ", char *" fsname ,
+.BI					size_t " fsname_len" ", char *" instance ,
+.BI					size_t " instance_len" );
+.sp
+.BI "int llapi_get_fsname(const char *" path ", char *" fsname ,
+.BI					size_t " fsname_len" );
+.sp
+.BI "int llapi_get_instance(const char *" path ", char *" instance ,
+.BI					size_t " instance_len" );
+.SH DESCRIPTION
+.LP
+.B llapi_get_fsname_instance()
+retrieves the filesystem fsname and instance for the specified
+.I path
+and stores them in the
+.I fsname
+and
+.I instance
+buffers respectively, each of which can respectively hold at least
+.I fsname_len
+(at least
+.B LUSTRE_MAXFSNAME
+bytes), and
+.I instance_len
+(at least
+.B LUSTRE_MAXINSTANCE
+bytes).
+If only the fsname or instance identifier is needed, then the respective
+.BR llapi_get_fsname (3)
+or
+.BR llapi_get_instance (3)
+function can be used.  The
+.BR llapi_getname (3)
+function returns the combined
+.IR fsname - instance
+string.
+.P
+The filesystem
+.I fsname
+is a short name like
+.RB ' testfs '
+or
+.RB ' lfs2g '
+used to identify the filesystem on the clients and servers for mounting
+and configuration.  The
+.I instance
+is a client-unique temporary identifier like
+.RB ' ffff8803c9c0f000 '
+that differentiates multiple mounts of the same filesystem on the same
+client.  The instance is not guaranteed to be unique between different clients,
+nor is it expected to be the same for repeated mounts on the same client.
+.P
+The filesystem identifier is used in client-side parameters, like
+.BI mdc. fsname -MDT0000-mdc- instance .max_rpcs_in_flight
+for MDC and OSC devices, or
+.BI llite. fsname - instance .max_cached_mb
+and similar for llite tunable values and statistics.
+.SH RETURN VALUES
+.TP
+.B 0
+on success, or
+.TP
+-ve
+error code on failure and sets errno appropriately.
+.SH ERRORS
+.TP 15
+.SM EINVAL
+Invalid filesystem information found.
+.TP
+.SM ENAMETOOLONG
+The
+.IR fsname_len ,
+.IR instance_len ,
+or
+.I namelen
+buffer length was not large enough to hold the string.
+.SH SEE ALSO
+.BR lfs-getname (1),
+.BR lustreapi (7)

lustre/doc/llapi_get_instance.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_fsname_instance.3

lustre/doc/llapi_getname.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_fsname_instance.3

lustre/include/lustre/lustreapi.h
@@ -404,7 +404,11 @@ int llapi_parse_size(const char *optarg, unsigned long long *size,
 int llapi_search_mounts(const char *pathname, int index, char *mntdir,
 			char *fsname);
 int llapi_search_fsname(const char *pathname, char *fsname);
-int llapi_getname(const char *path, char *buf, size_t size);
+int llapi_get_fsname_instance(const char *path, char *fsname, size_t fsname_len,
+			      char *instance, size_t instance_len);
+int llapi_get_instance(const char *path, char *instance, size_t instance_len);
+int llapi_get_fsname(const char *path, char *fsname, size_t fsname_len);
+int llapi_getname(const char *path, char *name, size_t namelen);
 int llapi_search_fileset(const char *pathname, char *fileset);
 
 int llapi_search_rootpath(char *pathname, const char *fsname);

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1146,6 +1146,7 @@ static inline char *obd_uuid2str(const struct obd_uuid *uuid)
 }
 
 #define LUSTRE_MAXFSNAME 8
+#define LUSTRE_MAXINSTANCE 16
 
 /* Extract fsname from uuid (or target name) of a target
    e.g. (myfs-OST0007_UUID -> myfs)

lustre/llite/llite_lib.c
@@ -1060,7 +1060,7 @@ int ll_fill_super(struct super_block *sb)
 	char	*profilenm = get_profile_name(sb);
 	struct config_llog_instance *cfg;
 	/* %p for void* in printf needs 16+2 characters: 0xffffffffffffffff */
-	const int instlen = 16 + 2;
+	const int instlen = LUSTRE_MAXINSTANCE + 2;
 	unsigned long cfg_instance = ll_get_cfg_instance(sb);
 	char name[MAX_STRING_SIZE];
 	int md_len = 0;

lustre/obdclass/obd_config.c
@@ -1766,7 +1766,8 @@ int class_config_llog_handler(const struct lu_env *env,
 		if (cfg->cfg_instance &&
 		    lcfg->lcfg_command != LCFG_SPTLRPC_CONF &&
 		    LUSTRE_CFG_BUFLEN(lcfg, 0) > 0) {
-			inst_len = LUSTRE_CFG_BUFLEN(lcfg, 0) + 16 + 4;
+			inst_len = LUSTRE_CFG_BUFLEN(lcfg, 0) +
+				LUSTRE_MAXINSTANCE + 4;
 			OBD_ALLOC(inst_name, inst_len);
 			if (!inst_name)
 				GOTO(out, rc = -ENOMEM);

lustre/utils/lfs.c
@@ -6203,43 +6203,33 @@ static int lfs_df(int argc, char **argv)
 	return rc;
 }
 
-static int print_instance(const char *mntdir, char *fsname, size_t fsnamelen,
+static int print_instance(const char *mntdir, char *buf, size_t buflen,
 			  bool opt_instance, bool opt_fsname, bool opt_mntdir)
 {
-	char *buf = fsname;
+	int rc = 0;
 
-	/* llapi_search_mounts() fills "fsname", but that is not called if
-	 * explicit paths are specified on the command-line
-	 */
-	if (opt_instance || (opt_fsname && fsname[0] == '\0')) {
-		int rc = llapi_getname(mntdir, fsname, fsnamelen);
-
-		if (rc < 0) {
-			fprintf(stderr, "cannot get instance for '%s': %s\n",
-				mntdir, strerror(-rc));
-			return rc;
-		}
-		buf = fsname;
-		if (!opt_instance) {
-			/* print only the fsname name */
-			buf = strchr(fsname, '-');
-			if (buf)
-				*buf = '\0';
-			buf = fsname;
-		} else if (!opt_fsname) {
-			/* print only the instance name */
-			buf = strchr(fsname, '-');
-			if (buf)
-				buf++;
-			else
-				buf = fsname;
-		}
+	if (opt_fsname == opt_instance) { /* both true or both false */
+		rc = llapi_getname(mntdir, buf, buflen);
 	} else if (opt_fsname) {
-		/* print only the fsname */
-		buf = fsname;
+		/* llapi_search_mounts() fills @buf with fsname, but that is not
+		 * called if explicit paths are specified on the command-line
+		 */
+		if (buf[0] == '\0')
+			rc = llapi_get_fsname(mntdir, buf, buflen);
+	} else /* if (opt_instance) */ {
+		rc = llapi_get_instance(mntdir, buf, buflen);
 	}
 
-	printf("%s %s\n", buf, opt_mntdir ? mntdir : "");
+	if (rc < 0) {
+		fprintf(stderr, "cannot get instance for '%s': %s\n",
+			mntdir, strerror(-rc));
+		return rc;
+	}
+
+	if (opt_mntdir)
+		printf("%s %s\n", buf, mntdir);
+	else
+		printf("%s\n", buf);
 
 	return 0;
 }
@@ -6268,10 +6258,6 @@ static int lfs_getname(int argc, char **argv)
 			return CMD_HELP;
 		}
 	}
-
-	/* If neither option is given, print both instance and fsname */
-	if (!opt_instance && !opt_fsname)
-		opt_instance = opt_fsname = true;
 
 	if (optind == argc) { /* no paths specified, get all paths. */
 		char mntdir[PATH_MAX] = "", path[PATH_MAX] = "";

lustre/utils/liblustreapi.c
@@ -1475,40 +1475,6 @@ int llapi_search_rootpath(char *pathname, const char *fsname)
 	return get_root_path(WANT_PATH, (char *)fsname, NULL, pathname, -1);
 }
 
-int llapi_getname(const char *path, char *buf, size_t size)
-{
-	struct obd_uuid uuid_buf;
-	char *uuid = uuid_buf.uuid;
-	char *cfg_instance;
-	int rc, len, fsname_len;
-
-	memset(&uuid_buf, 0, sizeof(uuid_buf));
-	rc = llapi_file_get_lov_uuid(path, &uuid_buf);
-	if (rc)
-		return rc;
-
-	/*
-	 * We want to turn testfs-clilov-ffff88002738bc00 into
-	 * testfs-ffff88002738bc00 in a portable way that doesn't depend
-	 * on what is after "-clilov-" as it may change in the future.
-	 * Unfortunately, the "fsname" part may contain a dash, so we
-	 * can't just skip to the first dash, and the "instance" may be a
-	 * UUID in the future, so we can't necessarily go to the last dash.
-	 */
-	cfg_instance = strstr(uuid, "-clilov-");
-	if (!cfg_instance)
-		return -EINVAL;
-
-	fsname_len = cfg_instance - uuid;
-	cfg_instance += strlen("-clilov-");
-	len = snprintf(buf, size, "%.*s-%s", fsname_len, uuid, cfg_instance);
-
-	if (len >= size)
-		rc = -ENAMETOOLONG;
-
-	return rc;
-}
-
 /**
  * Get the list of pool members.
  * \param poolname    string of format \<fsname\>.\<poolname\>

lustre/utils/liblustreapi_util.c
@@ -311,3 +311,79 @@ retry_open:
 
 	return rc ? -errno : 0;
 }
+
+int llapi_get_fsname_instance(const char *path, char *fsname, size_t fsname_len,
+			      char *instance, size_t instance_len)
+{
+	struct obd_uuid uuid_buf;
+	char *uuid = uuid_buf.uuid;
+	char *ptr;
+	int rc;
+
+	memset(&uuid_buf, 0, sizeof(uuid_buf));
+	rc = llapi_file_get_lov_uuid(path, &uuid_buf);
+	if (rc)
+		return rc;
+
+	/*
+	 * We want to turn fs-foo-clilov-ffff88002738bc00 into 'fs-foo' and
+	 * 'ffff88002738bc00' in a portable way that doesn't depend on what is
+	 * after "-clilov-" as it may change to a UUID string in the future.
+	 * Unfortunately, the "fsname" part may contain a dash, so we can't
+	 * just skip to the first dash, and if the "instance" is a UUID in the
+	 * future we can't necessarily go to the last dash either.
+	 */
+	ptr = strstr(uuid, "-clilov-");
+	if (!ptr || (!fsname && !instance)) {
+		rc = -EINVAL;
+		goto out;
+	}
+
+	*ptr = '\0';
+	ptr += strlen("-clilov-");
+	if (instance) {
+		snprintf(instance, instance_len, "%s", ptr);
+		if (strlen(ptr) >= instance_len)
+			rc = -ENAMETOOLONG;
+	}
+
+	if (fsname) {
+		snprintf(fsname, fsname_len, "%s", uuid);
+		if (strlen(uuid) >= fsname_len)
+			rc = -ENAMETOOLONG;
+	}
+
+out:
+	errno = -rc;
+	return rc;
+}
+
+int llapi_getname(const char *path, char *name, size_t namelen)
+{
+	char fsname[16];
+	char instance[40];
+	int rc;
+
+	rc = llapi_get_fsname_instance(path, fsname, sizeof(fsname),
+				       instance, sizeof(instance));
+	if (rc)
+		return rc;
+
+	snprintf(name, namelen, "%s-%s", fsname, instance);
+	if (strlen(fsname) + 1 + strlen(instance) >= namelen) {
+		rc = -ENAMETOOLONG;
+		errno = -rc;
+	}
+
+	return rc;
+}
+
+int llapi_get_instance(const char *path, char *instance, size_t instance_len)
+{
+	return llapi_get_fsname_instance(path, NULL, 0, instance, instance_len);
+}
+
+int llapi_get_fsname(const char *path, char *fsname, size_t fsname_len)
+{
+	return llapi_get_fsname_instance(path, fsname, fsname_len, NULL, 0);
+}