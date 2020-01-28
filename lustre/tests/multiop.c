lustre/lustre-release

commit 02b5a407081c88090f7237ef464a6f6c74139f67

LU-11681 lmv: disable remote file statahead
Remote file statahead is not supported, because such file needs
two RPCs to fetch both LOOKUP and GETATTR lock, on LOOKUP success
we only know file FID, thus can't prepare an inode correctly.

Disable this to avoid noise messages and confusion.

Update sanity.sh test_60g.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ied99dbce320f678ffd94c461bef9d67e107e673c
Reviewed-on: https://review.whamcloud.com/33930
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -3468,27 +3468,32 @@ int lmv_intent_getattr_async(struct obd_export *exp,
 	struct md_op_data *op_data = &minfo->mi_data;
 	struct obd_device *obd = exp->exp_obd;
 	struct lmv_obd *lmv = &obd->u.lmv;
-	struct lmv_tgt_desc *tgt = NULL;
+	struct lmv_tgt_desc *ptgt;
+	struct lmv_tgt_desc *ctgt;
 	int rc;
+
 	ENTRY;
 
 	if (!fid_is_sane(&op_data->op_fid2))
 		RETURN(-EINVAL);
 
-	tgt = lmv_find_target(lmv, &op_data->op_fid1);
-	if (IS_ERR(tgt))
-		RETURN(PTR_ERR(tgt));
+	ptgt = lmv_locate_tgt(lmv, op_data);
+	if (IS_ERR(ptgt))
+		RETURN(PTR_ERR(ptgt));
+
+	ctgt = lmv_find_target(lmv, &op_data->op_fid2);
+	if (IS_ERR(ctgt))
+		RETURN(PTR_ERR(ctgt));
 
 	/*
-	 * no special handle for remote dir, which needs to fetch both LOOKUP
-	 * lock on parent, and then UPDATE lock on child MDT, which makes all
-	 * complicated because this is done async. So only LOOKUP lock is
-	 * fetched for remote dir, but considering remote dir is rare case,
-	 * and not supporting it in statahead won't cause any issue, just leave
-	 * it as is.
+	 * remote object needs two RPCs to lookup and getattr, considering the
+	 * complexity don't support statahead for now.
 	 */
+	if (ctgt != ptgt)
+		RETURN(-EREMOTE);
 
-	rc = md_intent_getattr_async(tgt->ltd_exp, minfo);
+	rc = md_intent_getattr_async(ptgt->ltd_exp, minfo);
+
 	RETURN(rc);
 }
 

lustre/tests/sanity.sh
@@ -6928,13 +6928,16 @@ run_test 60e "no space while new llog is being created"
 
 test_60g() {
 	local pid
+	local i
 
 	test_mkdir -c $MDSCOUNT $DIR/$tdir
-	$LFS setdirstripe -D -i -1 -c $MDSCOUNT $DIR/$tdir
 
 	(
 		local index=0
 		while true; do
+			$LFS setdirstripe -i $(($index % $MDSCOUNT)) \
+				-c $MDSCOUNT $DIR/$tdir/subdir$index \
+				2>/dev/null
 			mkdir $DIR/$tdir/subdir$index 2>/dev/null
 			rmdir $DIR/$tdir/subdir$index 2>/dev/null
 			index=$((index + 1))
@@ -6943,16 +6946,34 @@ test_60g() {
 
 	pid=$!
 
-	for i in $(seq 100); do 
+	for i in {0..100}; do
 		# define OBD_FAIL_OSD_TXN_START    0x19a
-		do_facet mds1 lctl set_param fail_loc=0x8000019a
+		local index=$((i % MDSCOUNT + 1))
+
+		do_facet mds$index $LCTL set_param fail_loc=0x8000019a \
+			> /dev/null
 		usleep 100
 	done
 
 	kill -9 $pid
 
+	for i in $(seq $MDSCOUNT); do
+		do_facet mds$i $LCTL set_param fail_loc=0 > /dev/null
+	done
+
 	mkdir $DIR/$tdir/new || error "mkdir failed"
 	rmdir $DIR/$tdir/new || error "rmdir failed"
+
+	do_facet mds1 $LCTL lfsck_start -M $(facet_svc mds1) -A -C \
+		-t namespace
+	for i in $(seq $MDSCOUNT); do
+		wait_update_facet mds$i "$LCTL get_param -n \
+			mdd.$(facet_svc mds$i).lfsck_namespace |
+			awk '/^status/ { print \\\$2 }'" "completed"
+	done
+
+	ls -R $DIR/$tdir || error "ls failed"
+	rm -rf $DIR/$tdir || error "rmdir failed"
 }
 run_test 60g "transaction abort won't cause MDT hung"
 

commit 68330379b01cb6bf9b24235a80a4666d24c0e343

LU-13099 lmv: disable statahead for remote objects
Statahead for remote objects is supposed to be disabled by
LU-11681 lmv: disable remote file statahead.

However due to typo it is not and statahead for remote objects is
accompanied by warnings like:
  ll_set_inode()) Can not initialize inode .. without object type..
  ll_prep_inode()) new_inode -fatal: rc -12

Fix the typo.

Test to illustrate the issue is added.

Fixes: 02b5a407081c ("LU-11681 lmv: disable remote file statahead")

Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Cray-bug-id: LUS-8262
Change-Id: I8055b6373fb7b9777fa888dcb09384213822a59f
Reviewed-on: https://review.whamcloud.com/37089
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -3436,7 +3436,7 @@ int lmv_intent_getattr_async(struct obd_export *exp,
 	if (IS_ERR(ptgt))
 		RETURN(PTR_ERR(ptgt));
 
-	ctgt = lmv_fid2tgt(lmv, &op_data->op_fid1);
+	ctgt = lmv_fid2tgt(lmv, &op_data->op_fid2);
 	if (IS_ERR(ctgt))
 		RETURN(PTR_ERR(ctgt));
 

lustre/tests/multiop.c
@@ -52,6 +52,7 @@
 #include <semaphore.h>
 #include <time.h>
 #include <err.h>
+#include <dirent.h>
 
 #include <lustre/lustreapi.h>
 
@@ -78,9 +79,9 @@ char usage[] =
 "	 E[+|-] get lease. +/-: expect lease to (not) exist\n"
 "	 f  statfs\n"
 "	 F  print FID\n"
-"	 H[num] create HSM released file with num stripes\n"
 "	 G gid get grouplock\n"
 "	 g gid put grouplock\n"
+"	 H[num] create HSM released file with num stripes\n"
 "	 K  link path to filename\n"
 "	 L  link\n"
 "	 l  symlink filename to path\n"
@@ -91,6 +92,7 @@ char usage[] =
 "	 o  open(O_RDONLY)\n"
 "	 O  open(O_CREAT|O_RDWR)\n"
 "	 p  print return value of last command\n"
+"	 Q  open filename (should be dir), stat first entry to init statahead"
 "	 r[num] read [optional length]\n"
 "	 R  reference entire mmap-ed region\n"
 "	 s  stat\n"
@@ -201,6 +203,35 @@ int get_flags(char *data, int *rflags)
 
 	*rflags = flags;
 	return size;
+}
+
+static int statahead(char *dname)
+{
+	DIR *d;
+	struct dirent *dent;
+	struct stat st;
+	char *buf;
+	int rc;
+
+	rc = 0;
+	d = opendir(dname);
+	if (!d)
+		return errno;
+	dent = readdir(d);
+	if (!dent) {
+		rc = errno;
+		goto out_closedir;
+	}
+	if (asprintf(&buf, "%s/%s", dname, dent->d_name) == -1) {
+		rc = errno;
+		goto out_closedir;
+	}
+	if (stat(buf, &st))
+		rc = errno;
+	free(buf);
+out_closedir:
+	closedir(d);
+	return rc;
 }
 
 #define POP_ARG() (pop_arg(argc, argv))
@@ -545,6 +576,13 @@ int main(int argc, char **argv)
 			break;
 		case 'p':
 			printf("%lld\n", last_rc);
+			break;
+		case 'Q':
+			save_errno = statahead(fname);
+			if (save_errno) {
+				perror("statahead");
+				exit(save_errno);
+			}
 			break;
 		case 'r':
 			len = atoi(commands + 1);

lustre/tests/sanity.sh
@@ -11162,6 +11162,27 @@ test_123b () { # statahead(bug 15027)
 }
 run_test 123b "not panic with network error in statahead enqueue (bug 15027)"
 
+test_123c() {
+	[[ $MDSCOUNT -lt 2 ]] && skip_env "needs >= 2 MDTs"
+
+	test_mkdir -i 0 -c 1 $DIR/$tdir.0
+	test_mkdir -i 1 -c 1 $DIR/$tdir.1
+	touch $DIR/$tdir.1/{1..3}
+	mv $DIR/$tdir.1/{1..3} $DIR/$tdir.0
+
+	remount_client $MOUNT
+
+	$MULTIOP $DIR/$tdir.0 Q
+
+	# let statahead to complete
+	ls -l $DIR/$tdir.0 > /dev/null
+
+	testid=$(echo $TESTNAME | tr '_' ' ')
+	dmesg | tac | sed "/$testid/,$ d" | grep "Can not initialize inode" &&
+		error "statahead warning" || true
+}
+run_test 123c "Can not initialize inode warning on DNE statahead"
+
 test_124a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 	$LCTL get_param -n mdc.*.connect_flags | grep -q lru_resize ||

lustre/tests/test-framework.sh
@@ -6427,7 +6427,7 @@ run_one() {
 	check_node_health
 	check_dmesg_for_errors || error "Error in dmesg detected"
 	if [ "$PARALLEL" != "yes" ]; then
-		ps auxww | grep -v grep | grep -q multiop &&
+		ps auxww | grep -v grep | grep -q "multiop " &&
 					error "multiop still running"
 	fi
 	unset TESTNAME