7599dd3d20d6bb4ee89634c5a76730481ca62470
Alexander Boyko <c17825@cray.com>
2020-02-03 04:24:40 -0500

    LU-12593 osd: up i_append_sem during errors
    There is a potential leak of i_append_sem during errors for
    buffer head read and ldiskfs_joural_get_write_access() at
    osd_ldiskfs_write_record().
    The patch adds up(i_append_sem) for errors paths.
    
    Fixes: f832a7dc33c6 ("LU-12593 osd: zeroing a freshly allocated block buffer")
    Signed-off-by: Alexander Boyko <c17825@cray.com>
    Change-Id: I245d0c45af03519c66b75731e5d57f42de41fe95
    Reviewed-on: https://review.whamcloud.com/37406
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/osd-ldiskfs/osd_io.c
+++ lustre/osd-ldiskfs/osd_io.c
@@ -1696,7 +1696,7 @@ static int osd_ldiskfs_write_record(struct dt_object *dt, void *buf,
         int                 boffs;
         int                 dirty_inode = 0;
 	struct ldiskfs_inode_info *ei = LDISKFS_I(inode);
-	bool create, sparse;
+	bool create, sparse, sync = false;
 
 	if (write_NUL) {
 		/*
@@ -1714,7 +1714,6 @@ static int osd_ldiskfs_write_record(struct dt_object *dt, void *buf,
 
 	while (bufsize > 0) {
 		int credits = handle->h_buffer_credits;
-		bool sync;
 		unsigned long last_block = (new_size == 0) ? 0 :
 					   (new_size - 1) >> inode->i_blkbits;
 
@@ -1751,8 +1750,10 @@ static int osd_ldiskfs_write_record(struct dt_object *dt, void *buf,
 			bh = __ldiskfs_bread(handle, inode, block, flags);
 			create = true;
 		} else {
-			if (sync)
+			if (sync) {
 				up(&ei->i_append_sem);
+				sync = false;
+			}
 			create = false;
 		}
 		if (IS_ERR_OR_NULL(bh)) {
@@ -1781,8 +1782,10 @@ static int osd_ldiskfs_write_record(struct dt_object *dt, void *buf,
 			 boffs, size, (unsigned long)bh->b_size);
 		if (create) {
 			memset(bh->b_data, 0, bh->b_size);
-			if (sync)
+			if (sync) {
 				up(&ei->i_append_sem);
+				sync = false;
+			}
 		}
 		memcpy(bh->b_data + boffs, buf, size);
 		err = ldiskfs_handle_dirty_metadata(handle, NULL, bh);
@@ -1795,8 +1798,11 @@ static int osd_ldiskfs_write_record(struct dt_object *dt, void *buf,
                 bufsize -= size;
                 buf += size;
         }
-        if (bh)
-                brelse(bh);
+	if (sync)
+		up(&ei->i_append_sem);
+
+	if (bh)
+		brelse(bh);
 
 	if (write_NUL)
 		--new_size;
