lustre/lustre-release

commit 23773b32bfe15fccc9c029d722dc1bd5b0144779

LU-11444 ptlrpc: resend may corrupt the data
Late resend if arrives much later than another modification RPC
which has been already handled on this slot, may be still applied
and therefore overrides the last one

Send RPCs from client in increasing order for each tag
and check it on server to check late resend.

A slot can be reused by a client after kill while
the server continue to rely on it.

Add flag for such obsolete requests, here we trust the
client and perform xid check for all in progress requests.

Cray-bug-id: LUS-6272, LUS-7277, LUS-7339
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Change-Id: I97806577cf979f49a75379ffc55947cc3dcd02b1
Reviewed-on: https://review.whamcloud.com/35114
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -3924,7 +3924,7 @@ void mdt_intent_fixup_resent(struct mdt_thread_info *info,
 	 * If the xid matches, then we know this is a resent request, and allow
 	 * it. (It's probably an OPEN, for which we don't send a lock.
 	 */
-	if (req_can_reconstruct(req, NULL))
+	if (req_can_reconstruct(req, NULL) == 1)
 		return;
 
         /*
@@ -6314,6 +6314,11 @@ static int mdt_init_export(struct obd_export *exp)
 	exp->exp_connecting = 1;
 	spin_unlock(&exp->exp_lock);
 
+	OBD_ALLOC(exp->exp_used_slots,
+		  BITS_TO_LONGS(OBD_MAX_RIF_MAX) * sizeof(long));
+	if (exp->exp_used_slots == NULL)
+		RETURN(-ENOMEM);
+
         /* self-export doesn't need client data and ldlm initialization */
         if (unlikely(obd_uuid_equals(&exp->exp_obd->obd_uuid,
                                      &exp->exp_client_uuid)))
@@ -6332,6 +6337,10 @@ static int mdt_init_export(struct obd_export *exp)
 err_free:
 	tgt_client_free(exp);
 err:
+	OBD_FREE(exp->exp_used_slots,
+		 BITS_TO_LONGS(OBD_MAX_RIF_MAX) * sizeof(long));
+	exp->exp_used_slots = NULL;
+
 	CERROR("%s: Failed to initialize export: rc = %d\n",
 	       exp->exp_obd->obd_name, rc);
 	return rc;
@@ -6342,6 +6351,10 @@ static int mdt_destroy_export(struct obd_export *exp)
         ENTRY;
 
         target_destroy_export(exp);
+	if (exp->exp_used_slots)
+		OBD_FREE(exp->exp_used_slots,
+			 BITS_TO_LONGS(OBD_MAX_RIF_MAX) * sizeof(long));
+
         /* destroy can be called from failed obd_setup, so
          * checking uuid is safer than obd_self_export */
         if (unlikely(obd_uuid_equals(&exp->exp_obd->obd_uuid,

lustre/include/lu_target.h
@@ -455,7 +455,7 @@ void tgt_register_lfsck_query(int (*query)(const struct lu_env *,
 					   struct lfsck_request *,
 					   struct lfsck_reply *,
 					   struct lfsck_query *));
-bool req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
 
 extern struct tgt_handler tgt_sec_ctx_handlers[];
 extern struct tgt_handler tgt_lfsck_handlers[];
@@ -493,9 +493,10 @@ int tgt_client_new(const struct lu_env *env, struct obd_export *exp);
 int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg,
 			   int sync);
 int tgt_reply_data_init(const struct lu_env *env, struct lu_target *tgt);
-bool tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
 int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 		       struct tg_export_data *ted, struct tg_reply_data *trd,
+		       struct ptlrpc_request *req,
 		       struct thandle *th, bool update_lrd_file);
 struct tg_reply_data *tgt_lookup_reply_by_xid(struct tg_export_data *ted,
 					       __u64 xid);

lustre/include/lustre_export.h
@@ -307,7 +307,8 @@ struct obd_export {
 	/** highest XID received by export client that has no
 	 * unreceived lower-numbered XID
 	 */
-	__u64			  exp_last_xid;
+	__u64			exp_last_xid;
+	long			*exp_used_slots;
 };
 
 #define exp_target_data u.eu_target_data

lustre/include/lustre_mdc.h
@@ -73,6 +73,7 @@ static inline void mdc_get_mod_rpc_slot(struct ptlrpc_request *req,
 	opc = lustre_msg_get_opc(req->rq_reqmsg);
 	tag = obd_get_mod_rpc_slot(cli, opc, it);
 	lustre_msg_set_tag(req->rq_reqmsg, tag);
+	ptlrpc_reassign_next_xid(req);
 }
 
 static inline void mdc_put_mod_rpc_slot(struct ptlrpc_request *req,

lustre/include/lustre_net.h
@@ -999,7 +999,8 @@ struct ptlrpc_request {
 	unsigned int
 		rq_hp:1,		/**< high priority RPC */
 		rq_at_linked:1,		/**< link into service's srv_at_array */
-		rq_packed_final:1;	/**< packed final reply */
+		rq_packed_final:1,	/**< packed final reply */
+		rq_obsolete:1;		/* aborted by a signal on a client */
 	/** @} */
 
 	/** one of RQ_PHASE_* */
@@ -2169,6 +2170,7 @@ void ptlrpc_retain_replayable_request(struct ptlrpc_request *req,
 __u64 ptlrpc_next_xid(void);
 __u64 ptlrpc_sample_next_xid(void);
 __u64 ptlrpc_req_xid(struct ptlrpc_request *request);
+void ptlrpc_reassign_next_xid(struct ptlrpc_request *req);
 
 /* Set of routines to run a function in ptlrpcd context */
 void *ptlrpcd_alloc_work(struct obd_import *imp,

lustre/include/obd_support.h
@@ -450,6 +450,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_PTLRPC_LONG_BOTH_UNLINK 0x51c
 #define OBD_FAIL_PTLRPC_CLIENT_BULK_CB3  0x520
 #define OBD_FAIL_PTLRPC_BULK_ATTACH      0x521
+#define OBD_FAIL_PTLRPC_RESEND_RACE	 0x525
 #define OBD_FAIL_PTLRPC_ROUND_XID	 0x530
 #define OBD_FAIL_PTLRPC_CONNECT_RACE	 0x531
 

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -896,6 +896,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_FLR |\
 				OBD_CONNECT2_LOCK_CONVERT | \
 				OBD_CONNECT2_ARCHIVE_ID_ARRAY | \
+				OBD_CONNECT2_INC_XID | \
 				OBD_CONNECT2_SELINUX_POLICY | \
 				OBD_CONNECT2_LSOM | \
 				OBD_CONNECT2_ASYNC_DISCARD | \
@@ -920,7 +921,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT_GRANT_PARAM | \
 				OBD_CONNECT_SHORTIO | OBD_CONNECT_FLAGS2)
 
-#define OST_CONNECT_SUPPORTED2 OBD_CONNECT2_LOCKAHEAD
+#define OST_CONNECT_SUPPORTED2 (OBD_CONNECT2_LOCKAHEAD | OBD_CONNECT2_INC_XID)
 
 #define ECHO_CONNECT_SUPPORTED (OBD_CONNECT_FID)
 #define ECHO_CONNECT_SUPPORTED2 0

lustre/llite/llite_lib.c
@@ -255,6 +255,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				   OBD_CONNECT2_FLR |
 				   OBD_CONNECT2_LOCK_CONVERT |
 				   OBD_CONNECT2_ARCHIVE_ID_ARRAY |
+				   OBD_CONNECT2_INC_XID |
 				   OBD_CONNECT2_LSOM |
 				   OBD_CONNECT2_ASYNC_DISCARD |
 				   OBD_CONNECT2_PCC;
@@ -464,7 +465,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 	data->ocd_connect_flags |= OBD_CONNECT_LOCKAHEAD_OLD;
 #endif
 
-	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD;
+	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD |
+				   OBD_CONNECT2_INC_XID;
 
 	if (!OBD_FAIL_CHECK(OBD_FAIL_OSC_CONNECT_GRANT_PARAM))
 		data->ocd_connect_flags |= OBD_CONNECT_GRANT_PARAM;

commit 5e11d744becb81e79930b23ea0f998c7e7f07c08

LU-13165 mdt: MSG_RESENT can be improperly cleared.
req_can_reconstruct() can return -EPROTO, it means that
original request was processed and reply was received.

Change-Id: I06ba9aa24821f414777d38e9ca606652b172e92c
Fixes: 23773b32bf ("LU-11444 ptlrpc: resend may corrupt the data")
Cray-bug-id: LUS-7972
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-on: https://review.whamcloud.com/37296
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -3943,7 +3943,7 @@ void mdt_intent_fixup_resent(struct mdt_thread_info *info,
 	 * If the xid matches, then we know this is a resent request, and allow
 	 * it. (It's probably an OPEN, for which we don't send a lock.
 	 */
-	if (req_can_reconstruct(req, NULL) == 1)
+	if (req_can_reconstruct(req, NULL) != 0)
 		return;
 
         /*