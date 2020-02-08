lustre/lustre-release

commit 8e498d3f23ea9bcbef524153c6613f93a6229431

LU-11300 lnet: peer aliveness
Peer NI aliveness is now solely dependent on the health
infrastructure. With the addition of router_sensitivity_percentage,
peer NI is considered dead if its health drops below the percentage
specified of the total health. Setting the percentage to 100% means
that a peer_ni is considered dead if it's interface is less than
fully healthy.

Removed obsolete code that queries the peer NI every second since
the health infrastructure introduces the recovery mechanism which
is designed to recover the health of peer NIs.

Test-Parameters: forbuildonly
Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Change-Id: I506060fbb66c74295808891b689d7d634dc69284
Reviewed-on: https://review.whamcloud.com/33186
Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
Reviewed-by: Chris Horn <hornc@cray.com>
Tested-by: Jenkins

lnet/include/lnet/lib-types.h
@@ -569,8 +569,6 @@ struct lnet_peer_ni {
 	int			lpni_minrtrcredits;
 	/* bytes queued for sending */
 	long			lpni_txqnob;
-	/* alive/dead? */
-	bool			lpni_alive;
 	/* notification outstanding? */
 	bool			lpni_notify;
 	/* outstanding notification for LND? */

lnet/include/lnet/lib-lnet.h
@@ -955,15 +955,6 @@ lnet_find_peer_net_locked(struct lnet_peer *peer, __u32 net_id)
 	return NULL;
 }
 
-static inline void
-lnet_peer_set_alive(struct lnet_peer_ni *lp)
-{
-	lp->lpni_last_alive = ktime_get_seconds();
-	lp->lpni_last_query = lp->lpni_last_alive;
-	if (!lp->lpni_alive)
-		lnet_notify_locked(lp, 0, 1, lp->lpni_last_alive);
-}
-
 static inline bool
 lnet_peer_is_multi_rail(struct lnet_peer *lp)
 {
@@ -1000,6 +991,22 @@ lnet_peer_needs_push(struct lnet_peer *lp)
 	if (lp->lp_node_seqno < atomic_read(&the_lnet.ln_ping_target_seqno))
 		return true;
 	return false;
+}
+
+/*
+ * A peer is alive if it satisfies the following two conditions:
+ *  1. peer health >= LNET_MAX_HEALTH_VALUE * router_sensitivity_percentage
+ *  2. the cached NI status received when we discover the peer is UP
+ */
+static inline bool
+lnet_is_peer_ni_alive(struct lnet_peer_ni *lpni)
+{
+	bool halive = false;
+
+	halive = (atomic_read(&lpni->lpni_healthv) >=
+		 (LNET_MAX_HEALTH_VALUE * router_sensitivity_percentage / 100));
+
+	return halive && lpni->lpni_ns_status == LNET_NI_STATUS_UP;
 }
 
 static inline void

lnet/lnet/lib-move.c
@@ -794,87 +794,14 @@ lnet_ni_eager_recv(struct lnet_ni *ni, struct lnet_msg *msg)
 	return rc;
 }
 
-/*
- * This function can be called from two paths:
- *	1. when sending a message
- *	2. when decommiting a message (lnet_msg_decommit_tx())
- * In both these cases the peer_ni should have it's reference count
- * acquired by the caller and therefore it is safe to drop the spin
- * lock before calling lnd_query()
- */
-static void
-lnet_ni_query_locked(struct lnet_ni *ni, struct lnet_peer_ni *lp)
-{
-	time64_t last_alive = 0;
-	int cpt = lnet_cpt_of_nid_locked(lp->lpni_nid, ni);
-
-	LASSERT(lnet_peer_aliveness_enabled(lp));
-	LASSERT(ni->ni_net->net_lnd->lnd_query != NULL);
-
-	lnet_net_unlock(cpt);
-	(ni->ni_net->net_lnd->lnd_query)(ni, lp->lpni_nid, &last_alive);
-	lnet_net_lock(cpt);
-
-	lp->lpni_last_query = ktime_get_seconds();
-
-	if (last_alive != 0) /* NI has updated timestamp */
-		lp->lpni_last_alive = last_alive;
-}
-
-/* NB: always called with lnet_net_lock held */
-static inline int
-lnet_peer_is_alive(struct lnet_peer_ni *lp, time64_t now)
-{
-	int alive;
-	time64_t deadline;
-
-	LASSERT (lnet_peer_aliveness_enabled(lp));
-
-	/*
-	 * Trust lnet_notify() if it has more recent aliveness news, but
-	 * ignore the initial assumed death (see lnet_peers_start_down()).
-	 */
-	spin_lock(&lp->lpni_lock);
-	if (!lp->lpni_alive && lp->lpni_alive_count > 0 &&
-	    lp->lpni_timestamp >= lp->lpni_last_alive) {
-		spin_unlock(&lp->lpni_lock);
-		return 0;
-	}
-
-	deadline = lp->lpni_last_alive +
-		   lp->lpni_net->net_tunables.lct_peer_timeout;
-	alive = deadline > now;
-
-	/*
-	 * Update obsolete lp_alive except for routers assumed to be dead
-	 * initially, because router checker would update aliveness in this
-	 * case, and moreover lpni_last_alive at peer creation is assumed.
-	 */
-	if (alive && !lp->lpni_alive &&
-	    !(lnet_isrouter(lp) && lp->lpni_alive_count == 0)) {
-		spin_unlock(&lp->lpni_lock);
-		lnet_notify_locked(lp, 0, 1, lp->lpni_last_alive);
-	} else {
-		spin_unlock(&lp->lpni_lock);
-	}
-
-	return alive;
-}
-
-
 /* NB: returns 1 when alive, 0 when dead, negative when error;
  *     may drop the lnet_net_lock */
 static int
-lnet_peer_alive_locked(struct lnet_ni *ni, struct lnet_peer_ni *lp,
+lnet_peer_alive_locked(struct lnet_ni *ni, struct lnet_peer_ni *lpni,
 		       struct lnet_msg *msg)
 {
-	time64_t now = ktime_get_seconds();
-
-	if (!lnet_peer_aliveness_enabled(lp))
+	if (!lnet_peer_aliveness_enabled(lpni))
 		return -ENODEV;
-
-	if (lnet_peer_is_alive(lp, now))
-		return 1;
 
 	/*
 	 * If we're resending a message, let's attempt to send it even if
@@ -883,36 +810,16 @@ lnet_peer_alive_locked(struct lnet_ni *ni, struct lnet_peer_ni *lp,
 	if (msg->msg_retry_count > 0)
 		return 1;
 
-	/*
-	 * Peer appears dead, but we should avoid frequent NI queries (at
-	 * most once per lnet_queryinterval seconds).
-	 */
-	if (lp->lpni_last_query != 0) {
-		static const int lnet_queryinterval = 1;
-		time64_t next_query;
-
-		next_query = lp->lpni_last_query + lnet_queryinterval;
-
-		if (now < next_query) {
-			if (lp->lpni_alive)
-				CWARN("Unexpected aliveness of peer %s: "
-				      "%lld < %lld (%d/%d)\n",
-				      libcfs_nid2str(lp->lpni_nid),
-				      now, next_query,
-				      lnet_queryinterval,
-				      lp->lpni_net->net_tunables.lct_peer_timeout);
-			return 0;
-		}
-	}
-
-	/* query NI for latest aliveness news */
-	lnet_ni_query_locked(ni, lp);
-
-	if (lnet_peer_is_alive(lp, now))
+	/* try and send recovery messages irregardless */
+	if (msg->msg_recovery)
 		return 1;
 
-	lnet_notify_locked(lp, 0, 0, lp->lpni_last_alive);
-	return 0;
+	/* always send any responses */
+	if (msg->msg_type == LNET_MSG_ACK ||
+	    msg->msg_type == LNET_MSG_REPLY)
+		return 1;
+
+	return lnet_is_peer_ni_alive(lpni);
 }
 
 /**
@@ -4458,18 +4365,12 @@ lnet_parse(struct lnet_ni *ni, struct lnet_hdr *hdr, lnet_nid_t from_nid,
 	/* Multi-Rail: Primary NID of source. */
 	msg->msg_initiator = lnet_peer_primary_nid_locked(src_nid);
 
-	if (lnet_isrouter(msg->msg_rxpeer)) {
-		lnet_peer_set_alive(msg->msg_rxpeer);
-		if (avoid_asym_router_failure &&
-		    LNET_NIDNET(src_nid) != LNET_NIDNET(from_nid)) {
-			/* received a remote message from router, update
-			 * remote NI status on this router.
-			 * NB: multi-hop routed message will be ignored.
-			 */
-			lnet_router_ni_update_locked(msg->msg_rxpeer,
-						     LNET_NIDNET(src_nid));
-		}
-	}
+	/*
+	 * mark the status of this lpni as UP since we received a message
+	 * from it. The ping response reports back the ns_status which is
+	 * marked on the remote as up or down and we cache it here.
+	 */
+	msg->msg_rxpeer->lpni_ns_status = LNET_NI_STATUS_UP;
 
 	lnet_msg_commit(msg, cpt);
 

lnet/lnet/peer.c
@@ -3309,7 +3309,7 @@ lnet_debug_peer(lnet_nid_t nid)
 	}
 
 	if (lnet_isrouter(lp) || lnet_peer_aliveness_enabled(lp))
-		aliveness = lp->lpni_alive ? "up" : "down";
+		aliveness = (lnet_is_peer_ni_alive(lp)) ? "up" : "down";
 
 	CDEBUG(D_WARNING, "%-24s %4d %5s %5d %5d %5d %5d %5d %ld\n",
 	       libcfs_nid2str(lp->lpni_nid), atomic_read(&lp->lpni_refcount),
@@ -3365,7 +3365,7 @@ int lnet_get_peer_ni_info(__u32 peer_index, __u64 *nid,
 			if (lnet_isrouter(lp) ||
 				lnet_peer_aliveness_enabled(lp))
 				snprintf(aliveness, LNET_MAX_STR_LEN,
-					 lp->lpni_alive ? "up" : "down");
+					 lnet_is_peer_ni_alive(lp) ? "up" : "down");
 
 			*nid = lp->lpni_nid;
 			*refcount = atomic_read(&lp->lpni_refcount);
@@ -3452,7 +3452,7 @@ int lnet_get_peer_info(struct lnet_ioctl_peer_cfg *cfg, void __user *bulk)
 		if (lnet_isrouter(lpni) ||
 			lnet_peer_aliveness_enabled(lpni))
 			snprintf(lpni_info->cr_aliveness, LNET_MAX_STR_LEN,
-				lpni->lpni_alive ? "up" : "down");
+				lnet_is_peer_ni_alive(lpni) ? "up" : "down");
 
 		lpni_info->cr_refcount = atomic_read(&lpni->lpni_refcount);
 		lpni_info->cr_ni_peer_tx_credits = (lpni->lpni_net != NULL) ?

lnet/lnet/router.c
@@ -170,8 +170,10 @@ lnet_notify_locked(struct lnet_peer_ni *lp, int notifylnd, int alive,
 
 	lp->lpni_timestamp = when; /* update timestamp */
 
-	if (lp->lpni_alive_count != 0 &&          /* got old news */
-	    (!lp->lpni_alive) == (!alive)) {      /* new date for old news */
+	/* got old news */
+	if (lp->lpni_alive_count != 0 &&
+	    /* new date for old news */
+	    (!lnet_is_peer_ni_alive(lp)) == (!alive)) {
 		spin_unlock(&lp->lpni_lock);
 		CDEBUG(D_NET, "Old news\n");
 		return;
@@ -180,10 +182,9 @@ lnet_notify_locked(struct lnet_peer_ni *lp, int notifylnd, int alive,
 	/* Flag that notification is outstanding */
 
 	lp->lpni_alive_count++;
-	lp->lpni_alive = (alive) ? 1 : 0;
 	lp->lpni_notify = 1;
 	lp->lpni_notifylnd = notifylnd;
-	if (lp->lpni_alive)
+	if (lnet_is_peer_ni_alive(lp))
 		lp->lpni_ping_feats = LNET_PING_FEAT_INVAL; /* reset */
 
 	spin_unlock(&lp->lpni_lock);
@@ -218,7 +219,7 @@ lnet_ni_notify_locked(struct lnet_ni *ni, struct lnet_peer_ni *lp)
 	 * lnet_notify_locked().
 	 */
 	while (lp->lpni_notify) {
-		alive     = lp->lpni_alive;
+		alive     = lnet_is_peer_ni_alive(lp);
 		notifylnd = lp->lpni_notifylnd;
 
 		lp->lpni_notifylnd = 0;

lnet/lnet/router_proc.c
@@ -536,7 +536,8 @@ proc_lnet_peers(struct ctl_table *table, int write, void __user *buffer,
 
 			if (lnet_isrouter(peer) ||
 			    lnet_peer_aliveness_enabled(peer))
-				aliveness = peer->lpni_alive ? "up" : "down";
+				aliveness = lnet_is_peer_ni_alive(peer) ?
+					"up" : "down";
 
 			if (lnet_peer_aliveness_enabled(peer)) {
 				time64_t now = ktime_get_seconds();

commit 0d816af574b7063c0ce339b67d2066b229d20f59

LU-11300 lnet: remove lnd_query interface.
The ->lnd_query interface is completely unused, and has been since
commit 8e498d3f23ea ("LU-11300 lnet: peer aliveness")

So remove all mention of it.

Fixes: 8e498d3f23ea ("LU-11300 lnet: peer aliveness")
Test-Parameters: trivial
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Iff11652283b371519cf31bf66b9ba08e024d3193
Reviewed-on: https://review.whamcloud.com/37337
Reviewed-by: Chris Horn <hornc@cray.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-types.h
@@ -297,9 +297,6 @@ struct lnet_lnd {
 	/* notification of peer down */
 	void (*lnd_notify_peer_down)(lnet_nid_t peer);
 
-	/* query of peer aliveness */
-	void (*lnd_query)(struct lnet_ni *ni, lnet_nid_t peer, time64_t *when);
-
 	/* accept a new connection */
 	int (*lnd_accept)(struct lnet_ni *ni, struct socket *sock);
 };

lnet/klnds/gnilnd/gnilnd.c
@@ -33,7 +33,6 @@ const struct lnet_lnd the_kgnilnd = {
 	.lnd_send       = kgnilnd_send,
 	.lnd_recv       = kgnilnd_recv,
 	.lnd_eager_recv = kgnilnd_eager_recv,
-	.lnd_query      = kgnilnd_query,
 };
 
 kgn_data_t      kgnilnd_data;
@@ -1920,77 +1919,6 @@ kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 	}
 
 	return rc;
-}
-
-void
-kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when)
-{
-	kgn_net_t               *net = ni->ni_data;
-	kgn_tx_t                *tx;
-	kgn_peer_t              *peer = NULL;
-	kgn_conn_t              *conn = NULL;
-	struct lnet_process_id       id = {
-		.nid = nid,
-		.pid = LNET_PID_LUSTRE,
-	};
-	ENTRY;
-
-	/* I expect to find him, so only take a read lock */
-	read_lock(&kgnilnd_data.kgn_peer_conn_lock);
-	peer = kgnilnd_find_peer_locked(nid);
-	if (peer != NULL) {
-		/* LIE if in a quiesce - we will update the timeouts after,
-		 * but we don't want sends failing during it */
-		if (kgnilnd_data.kgn_quiesce_trigger) {
-			*when = ktime_get_seconds();
-			read_unlock(&kgnilnd_data.kgn_peer_conn_lock);
-			GOTO(out, 0);
-		}
-
-		/* Update to best guess, might refine on later checks */
-		*when = peer->gnp_last_alive;
-
-		/* we have a peer, how about a conn? */
-		conn = kgnilnd_find_conn_locked(peer);
-
-		if (conn == NULL)  {
-			/* if there is no conn, check peer last errno to see if clean disconnect
-			 * - if it was, we lie to LNet because we believe a TX would complete
-			 * on reconnect */
-			if (kgnilnd_conn_clean_errno(peer->gnp_last_errno)) {
-				*when = ktime_get_seconds();
-			}
-			/* we still want to fire a TX and new conn in this case */
-		} else {
-			/* gnp_last_alive is valid, run for the hills */
-			read_unlock(&kgnilnd_data.kgn_peer_conn_lock);
-			GOTO(out, 0);
-		}
-	}
-	/* if we get here, either we have no peer or no conn for him, so fire off
-	 * new TX to trigger conn setup */
-	read_unlock(&kgnilnd_data.kgn_peer_conn_lock);
-
-	/* if we couldn't find him, we'll fire up a TX and get connected -
-	 * if we don't do this, after ni_peer_timeout, LNet will declare him dead.
-	 * So really we treat kgnilnd_query as a bit of a 'connect now' type
-	 * event because it'll only do this when it wants to send
-	 *
-	 * Use a real TX for this to get the proper gnp_tx_queue behavior, etc
-	 * normally we'd use kgnilnd_send_ctlmsg for this, but we don't really
-	 * care that this goes out quickly since we already know we need a new conn
-	 * formed */
-	if (CFS_FAIL_CHECK(CFS_FAIL_GNI_NOOP_SEND))
-		return;
-
-	tx = kgnilnd_new_tx_msg(GNILND_MSG_NOOP, ni->ni_nid);
-	if (tx != NULL) {
-		kgnilnd_launch_tx(tx, net, &id);
-	}
-out:
-	CDEBUG(D_NETTRACE, "peer 0x%p->%s when %lld\n", peer,
-	       libcfs_nid2str(nid), *when);
-	EXIT;
 }
 
 int

lnet/klnds/gnilnd/gnilnd.h
@@ -1782,7 +1782,6 @@ void kgnilnd_unmap_fma_blocks(kgn_device_t *device);
 void kgnilnd_free_phys_fmablk(kgn_device_t *device);
 
 int kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg);
-void kgnilnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when);
 int kgnilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg);
 int kgnilnd_eager_recv(struct lnet_ni *ni, void *private,
 			struct lnet_msg *lntmsg, void **new_private);

lnet/klnds/o2iblnd/o2iblnd.c
@@ -1224,36 +1224,6 @@ kiblnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 }
 
 static void
-kiblnd_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when)
-{
-	time64_t last_alive = 0;
-	time64_t now = ktime_get_seconds();
-	rwlock_t *glock = &kiblnd_data.kib_global_lock;
-	struct kib_peer_ni *peer_ni;
-	unsigned long flags;
-
-	read_lock_irqsave(glock, flags);
-
-	peer_ni = kiblnd_find_peer_locked(ni, nid);
-	if (peer_ni != NULL)
-		last_alive = peer_ni->ibp_last_alive;
-
-	read_unlock_irqrestore(glock, flags);
-
-	if (last_alive != 0)
-		*when = last_alive;
-
-	/* peer_ni is not persistent in hash, trigger peer_ni creation
-	 * and connection establishment with a NULL tx */
-	if (peer_ni == NULL)
-		kiblnd_launch_tx(ni, NULL, nid);
-
-	CDEBUG(D_NET, "peer_ni %s %p, alive %lld secs ago\n",
-	       libcfs_nid2str(nid), peer_ni,
-	       last_alive ? now - last_alive : -1);
-}
-
-static void
 kiblnd_free_pages(struct kib_pages *p)
 {
 	int	npages = p->ibp_npages;
@@ -3460,7 +3430,6 @@ static const struct lnet_lnd the_o2iblnd = {
 	.lnd_startup	= kiblnd_startup,
 	.lnd_shutdown	= kiblnd_shutdown,
 	.lnd_ctl	= kiblnd_ctl,
-	.lnd_query	= kiblnd_query,
 	.lnd_send	= kiblnd_send,
 	.lnd_recv	= kiblnd_recv,
 };

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -2751,11 +2751,12 @@ kiblnd_check_reconnect(struct kib_conn *conn, int version,
 	}
 
 	write_lock_irqsave(glock, flags);
-        /* retry connection if it's still needed and no other connection
-         * attempts (active or passive) are in progress
-         * NB: reconnect is still needed even when ibp_tx_queue is
-         * empty if ibp_version != version because reconnect may be
-         * initiated by kiblnd_query() */
+	/* retry connection if it's still needed and no other connection
+	 * attempts (active or passive) are in progress
+	 * NB: reconnect is still needed even when ibp_tx_queue is
+	 * empty if ibp_version != version because reconnect may be
+	 * initiated.
+	 */
 	reconnect = (!list_empty(&peer_ni->ibp_tx_queue) ||
 		     peer_ni->ibp_version != version) &&
 		    peer_ni->ibp_connecting &&

lnet/klnds/socklnd/socklnd.c
@@ -1777,69 +1777,6 @@ ksocknal_notify_gw_down(lnet_nid_t gw_nid)
 	 * if we have autroutes, and these connect on demand. */
 }
 
-void
-ksocknal_query(struct lnet_ni *ni, lnet_nid_t nid, time64_t *when)
-{
-	int connect = 1;
-	time64_t last_alive = 0;
-	time64_t now = ktime_get_seconds();
-	struct ksock_peer_ni *peer_ni = NULL;
-	rwlock_t *glock = &ksocknal_data.ksnd_global_lock;
-	struct lnet_process_id id = {
-		.nid = nid,
-		.pid = LNET_PID_LUSTRE,
-	};
-
-	read_lock(glock);
-
-	peer_ni = ksocknal_find_peer_locked(ni, id);
-	if (peer_ni != NULL) {
-		struct list_head *tmp;
-		struct ksock_conn *conn;
-		int bufnob;
-
-		list_for_each(tmp, &peer_ni->ksnp_conns) {
-			conn = list_entry(tmp, struct ksock_conn, ksnc_list);
-			bufnob = conn->ksnc_sock->sk->sk_wmem_queued;
-
-			if (bufnob < conn->ksnc_tx_bufnob) {
-				/* something got ACKed */
-				conn->ksnc_tx_deadline = ktime_get_seconds() +
-							 lnet_get_lnd_timeout();
-                                peer_ni->ksnp_last_alive = now;
-                                conn->ksnc_tx_bufnob = bufnob;
-                        }
-                }
-
-                last_alive = peer_ni->ksnp_last_alive;
-                if (ksocknal_find_connectable_route_locked(peer_ni) == NULL)
-                        connect = 0;
-        }
-
-	read_unlock(glock);
-
-        if (last_alive != 0)
-		*when = last_alive;
-
-	CDEBUG(D_NET, "peer_ni %s %p, alive %lld secs ago, connect %d\n",
-               libcfs_nid2str(nid), peer_ni,
-	       last_alive ? now - last_alive : -1,
-               connect);
-
-        if (!connect)
-                return;
-
-        ksocknal_add_peer(ni, id, LNET_NIDADDR(nid), lnet_acceptor_port());
-
-	write_lock_bh(glock);
-
-        peer_ni = ksocknal_find_peer_locked(ni, id);
-        if (peer_ni != NULL)
-                ksocknal_launch_all_connections_locked(peer_ni);
-
-	write_unlock_bh(glock);
-}
-
 static void
 ksocknal_push_peer(struct ksock_peer_ni *peer_ni)
 {
@@ -2768,7 +2705,6 @@ static const struct lnet_lnd the_ksocklnd = {
 	.lnd_send		= ksocknal_send,
 	.lnd_recv		= ksocknal_recv,
 	.lnd_notify_peer_down	= ksocknal_notify_gw_down,
-	.lnd_query		= ksocknal_query,
 	.lnd_accept		= ksocknal_accept,
 };
 

lnet/lnet/api-ni.c
@@ -2386,9 +2386,6 @@ lnet_startup_lndnet(struct lnet_net *net, struct lnet_lnd_tunables *tun)
 		if (rc < 0)
 			goto failed1;
 
-		LASSERT(ni->ni_net->net_tunables.lct_peer_timeout <= 0 ||
-			ni->ni_net->net_lnd->lnd_query != NULL);
-
 		lnet_ni_addref(ni);
 		list_add_tail(&ni->ni_netlist, &local_ni_list);
 