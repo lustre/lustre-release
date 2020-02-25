lustre/lustre-release

commit f32fbf189fab57202810a9a47343a14c6c3ead95

LU-13004 osp: use KIOV in osp_prep_update_req
Convert osp_prep_update_req to use a BULK_BUF_KIOV
rather than a BULK_BUF_KVEC descriptor.

This is a step towards remove KIOV support.

Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I2fdf84d73ba2d34c678b6eb6a8bbd323a761dfe4
Reviewed-on: https://review.whamcloud.com/36828
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osp/osp_trans.c
@@ -347,6 +347,7 @@ int osp_prep_update_req(const struct lu_env *env, struct obd_import *imp,
 	struct out_update_header	*ouh;
 	struct out_update_buffer	*oub;
 	__u32				buf_count = 0;
+	int				page_count = 0;
 	int				repsize = 0;
 	struct object_update_reply	*reply;
 	int				rc, i;
@@ -418,13 +419,14 @@ int osp_prep_update_req(const struct lu_env *env, struct obd_import *imp,
 	list_for_each_entry(ours, &our->our_req_list, ours_list) {
 		oub->oub_size = ours->ours_req_size;
 		oub++;
+		page_count += round_up(ours->ours_req_size, PAGE_SIZE) + 1;
 	}
 
 	req->rq_bulk_write = 1;
-	desc = ptlrpc_prep_bulk_imp(req, buf_count,
+	desc = ptlrpc_prep_bulk_imp(req, page_count,
 		MD_MAX_BRW_SIZE >> LNET_MTU_BITS,
-		PTLRPC_BULK_GET_SOURCE | PTLRPC_BULK_BUF_KVEC,
-		MDS_BULK_PORTAL, &ptlrpc_bulk_kvec_ops);
+		PTLRPC_BULK_GET_SOURCE | PTLRPC_BULK_BUF_KIOV,
+		MDS_BULK_PORTAL, &ptlrpc_bulk_kiov_nopin_ops);
 	if (desc == NULL)
 		GOTO(out_req, rc = -ENOMEM);
 

commit 5f04a9d6d6a4709382f8981c8cc56d526cefa2dc

LU-13004 osp: use correct page count in osp_prep_update_req
A fix that went into patchset 3 of
 https://review.whamcloud.com/#/c/36828/3
disappeared in patchset 5.
We should restore it.

Specifically, 'page_count' should be a count of pages,
but it is currently a count of the bytes in all the pages.

Fixes: f32fbf189fab ("LU-13004 osp: use KIOV in osp_prep_update_req")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Ic8dcdac414d16b4f2c1c6e0367d496de7e0a8cff
Reviewed-on: https://review.whamcloud.com/37587
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osp/osp_trans.c
@@ -419,7 +419,8 @@ int osp_prep_update_req(const struct lu_env *env, struct obd_import *imp,
 	list_for_each_entry(ours, &our->our_req_list, ours_list) {
 		oub->oub_size = ours->ours_req_size;
 		oub++;
-		page_count += round_up(ours->ours_req_size, PAGE_SIZE) + 1;
+		/* First *and* last might be partial pages, hence +1 */
+		page_count += DIV_ROUND_UP(ours->ours_req_size, PAGE_SIZE) + 1;
 	}
 
 	req->rq_bulk_write = 1;