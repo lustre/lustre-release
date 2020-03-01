lustre/lustre-release

commit adb5aca3d67380078c4bd35c74651a7b9f848f4d

LU-8066 llite: Move all remaining procfs entries to debugfs
This moves all remaining procfs handling in llite layer to debugfs.

This is a modified version of

Linux-commit : ae7c0f4833a65b7648cceaf1a60503a89e057f0f

Change-Id: Id5c411d21a660a17a015ca9976b857e6b088c28a
Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
Signed-off-by: Oleg Drokin <oleg.drokin@intel.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/32517
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Ben Evans <bevans@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -499,16 +499,16 @@ struct lustre_client_ocd {
 struct ll_sb_info {
 	/* this protects pglist and ra_info.  It isn't safe to
 	 * grab from interrupt contexts */
-	spinlock_t		  ll_lock;
-	spinlock_t		  ll_pp_extent_lock; /* pp_extent entry*/
-	spinlock_t		  ll_process_lock; /* ll_rw_process_info */
-        struct obd_uuid           ll_sb_uuid;
-        struct obd_export        *ll_md_exp;
-        struct obd_export        *ll_dt_exp;
-	struct obd_device	 *ll_md_obd;
-	struct obd_device	 *ll_dt_obd;
-        struct proc_dir_entry*    ll_proc_root;
-        struct lu_fid             ll_root_fid; /* root object fid */
+	spinlock_t		 ll_lock;
+	spinlock_t		 ll_pp_extent_lock; /* pp_extent entry*/
+	spinlock_t		 ll_process_lock; /* ll_rw_process_info */
+	struct obd_uuid		 ll_sb_uuid;
+	struct obd_export	*ll_md_exp;
+	struct obd_export	*ll_dt_exp;
+	struct obd_device	*ll_md_obd;
+	struct obd_device	*ll_dt_obd;
+	struct dentry		*ll_debugfs_entry;
+	struct lu_fid		 ll_root_fid; /* root object fid */
 
         int                       ll_flags;
 	unsigned int		  ll_umounting:1,
@@ -684,7 +684,6 @@ struct ll_file_data {
 	__u32 fd_layout_version;
 };
 
-extern struct proc_dir_entry *proc_lustre_fs_root;
 void llite_tunables_unregister(void);
 int llite_tunables_register(void);
 
@@ -739,17 +738,9 @@ int cl_get_grouplock(struct cl_object *obj, unsigned long gid, int nonblock,
 void cl_put_grouplock(struct ll_grouplock *lg);
 
 /* llite/lproc_llite.c */
-#ifdef CONFIG_PROC_FS
-void ll_stats_ops_tally(struct ll_sb_info *sbi, int op, int count);
-extern struct lprocfs_vars lprocfs_llite_obd_vars[];
-#else
-static inline int ll_debugs_register_super(struct super_block *sb,
-					   const char *name)
-{ return 0; }
-static void ll_stats_ops_tally(struct ll_sb_info *sbi, int op, int count) {}
-#endif
 int ll_debugfs_register_super(struct super_block *sb, const char *name);
 void ll_debugfs_unregister_super(struct super_block *sb);
+void ll_stats_ops_tally(struct ll_sb_info *sbi, int op, int count);
 
 enum {
 	LPROC_LL_DIRTY_HITS,

lustre/llite/llite_lib.c
@@ -296,7 +296,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 	 * can make sure the client can be mounted as long as MDT0 is
 	 * avaible */
 	err = obd_statfs(NULL, sbi->ll_md_exp, osfs,
-			ktime_get_seconds() -OBD_STATFS_CACHE_SECONDS,
+			ktime_get_seconds() - OBD_STATFS_CACHE_SECONDS,
 			OBD_STATFS_FOR_MDT0);
 	if (err)
 		GOTO(out_md_fid, err);
@@ -431,8 +431,9 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 
 	/* OBD_CONNECT_CKSUM should always be set, even if checksums are
 	 * disabled by default, because it can still be enabled on the
-	 * fly via /proc. As a consequence, we still need to come to an
-	 * agreement on the supported algorithms at connect time */
+	 * fly via /sys. As a consequence, we still need to come to an
+	 * agreement on the supported algorithms at connect time
+	 */
 	data->ocd_connect_flags |= OBD_CONNECT_CKSUM;
 
 	if (OBD_FAIL_CHECK(OBD_FAIL_OSC_CKSUM_ADLER_ONLY))
@@ -1029,20 +1030,20 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 
 	OBD_ALLOC_PTR(cfg);
 	if (cfg == NULL)
-		GOTO(out_free, err = -ENOMEM);
+		GOTO(out_free_cfg, err = -ENOMEM);
 
 	/* client additional sb info */
 	lsi->lsi_llsbi = sbi = ll_init_sbi();
 	if (!sbi)
-		GOTO(out_free, err = -ENOMEM);
+		GOTO(out_free_cfg, err = -ENOMEM);
 
 	err = ll_options(lsi->lsi_lmd->lmd_opts, sbi);
 	if (err)
-		GOTO(out_free, err);
+		GOTO(out_free_cfg, err);
 
 	err = super_setup_bdi_name(sb, "lustre-%p", sb);
 	if (err)
-		GOTO(out_free, err);
+		GOTO(out_free_cfg, err);
 
 #ifndef HAVE_DCACHE_LOCK
 	/* kernel >= 2.6.38 store dentry operations in sb->s_d_op. */
@@ -1069,8 +1070,9 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	}
 
 	/* Generate a string unique to this super, in case some joker tries
-	   to mount the same fs at two mount points.
-	   Use the address of the super itself.*/
+	 * to mount the same fs at two mount points.
+	 * Use the address of the super itself.
+	 */
 	cfg->cfg_instance = sb;
 	cfg->cfg_uuid = lsi->lsi_llsbi->ll_sb_uuid;
 	cfg->cfg_callback = class_config_llog_handler;
@@ -1078,7 +1080,7 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	/* set up client obds */
 	err = lustre_process_log(sb, profilenm, cfg);
 	if (err < 0)
-		GOTO(out_proc, err);
+		GOTO(out_debugfs, err);
 
 	/* Profile set with LCFG_MOUNTOPT so we can find our mdc and osc obds */
 	lprof = class_get_profile(profilenm);
@@ -1086,7 +1088,7 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 		LCONSOLE_ERROR_MSG(0x156, "The client profile '%s' could not be"
 				   " read from the MGS.  Does that filesystem "
 				   "exist?\n", profilenm);
-		GOTO(out_proc, err = -EINVAL);
+		GOTO(out_debugfs, err = -EINVAL);
 	}
 	CDEBUG(D_CONFIG, "Found profile %s: mdc=%s osc=%s\n", profilenm,
 	       lprof->lp_md, lprof->lp_dt);
@@ -1094,34 +1096,38 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	dt_len = strlen(lprof->lp_dt) + instlen + 2;
 	OBD_ALLOC(dt, dt_len);
 	if (!dt)
-		GOTO(out_proc, err = -ENOMEM);
+		GOTO(out_profile, err = -ENOMEM);
 	snprintf(dt, dt_len - 1, "%s-%p", lprof->lp_dt, cfg->cfg_instance);
 
 	md_len = strlen(lprof->lp_md) + instlen + 2;
 	OBD_ALLOC(md, md_len);
 	if (!md)
-		GOTO(out_proc, err = -ENOMEM);
+		GOTO(out_free_dt, err = -ENOMEM);
 	snprintf(md, md_len - 1, "%s-%p", lprof->lp_md, cfg->cfg_instance);
 
 	/* connections, registrations, sb setup */
 	err = client_common_fill_super(sb, md, dt, mnt);
 	if (err < 0)
-		GOTO(out_proc, err);
+		GOTO(out_free_md, err);
 
 	sbi->ll_client_common_fill_super_succeeded = 1;
 
-out_proc:
-	if (err < 0)
-		ll_debugfs_unregister_super(sb);
-out_free:
+out_free_md:
 	if (md)
 		OBD_FREE(md, md_len);
+out_free_dt:
 	if (dt)
 		OBD_FREE(dt, dt_len);
-	if (lprof != NULL)
+out_profile:
+	if (lprof)
 		class_put_profile(lprof);
+out_debugfs:
+	if (err < 0)
+		ll_debugfs_unregister_super(sb);
+out_free_cfg:
 	if (cfg)
 		OBD_FREE_PTR(cfg);
+
 	if (err)
 		ll_put_super(sb);
 	else if (sbi->ll_flags & LL_SBI_VERBOSE)

commit 59a3d7237df82ac2ce14b6779577584163f9122b

LU-12043 llite: move tunable params to sysfs_memparse()
Move the max_read_ahead_* tunables from debugfs to sysfs, since
they follow the one-value-per-file rule and should be visible to
regular users.

Rename the functions and constants from *readahead* to *read_ahead*
or *READ_AHEAD* to match the tunable names from procfs.

Deprecate usage of llprocfs_str_with_units_to_s64(), lu_str_to_s64(),
llprocfs_str_with_units_to_u64(), and lu_str_to_u64(), and instead
use sysfs_memparse() to parse sizes in the few remaining places
where they are used.  A separate patch will remove those functions.

Minor fix to the "lctl set_param" man page.

Fixes: adb5aca3d673 ("LU-8066 llite: Move all remaining procfs entries to debugfs")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I2cdf5f8f0aeca458ed1989366102c33ae83ebbe5
Reviewed-on: https://review.whamcloud.com/34849
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/doc/lctl-set_param.8
@@ -40,9 +40,9 @@ Set parameters from
 instead of from the command-line.  The contents of
 .I filename
 is YAML format, created as an output from
-.BR "lctl --device MGS llog_print " < \fIfsname >- client
-fR or any other valid llog configuration log as listed by
-.B lctl --device MGS llog_catlist
+.BR  ' "lctl --device MGS llog_print " < \fIfsname\fR >- client '
+or any other valid llog configuration log as listed by
+.RB ' "lctl --device MGS llog_catlist" '
 .TP
 .B -n
 Disable printing of the parameter name after setting it.

lustre/llite/llite_internal.h
@@ -452,11 +452,11 @@ static inline struct pcc_inode *ll_i2pcci(struct inode *inode)
 /* default to use at least 16M for fast read if possible */
 #define RA_REMAIN_WINDOW_MIN			MiB_TO_PAGES(16UL)
 
-/* default to about 64M of readahead on a given system. */
-#define SBI_DEFAULT_READAHEAD_MAX		MiB_TO_PAGES(64UL)
+/* default readahead on a given system. */
+#define SBI_DEFAULT_READ_AHEAD_MAX		MiB_TO_PAGES(64UL)
 
-/* default to read-ahead full files smaller than 2MB on the second read */
-#define SBI_DEFAULT_READAHEAD_WHOLE_MAX		MiB_TO_PAGES(2UL)
+/* default read-ahead full files smaller than limit on the second read */
+#define SBI_DEFAULT_READ_AHEAD_WHOLE_MAX	MiB_TO_PAGES(2UL)
 
 enum ra_stat {
         RA_STAT_HIT = 0,

lustre/llite/llite_lib.c
@@ -113,7 +113,7 @@ static struct ll_sb_info *ll_init_sbi(void)
 		GOTO(out_destroy_ra, rc = -ENOMEM);
 
 	sbi->ll_ra_info.ra_max_pages_per_file = min(pages / 32,
-					   SBI_DEFAULT_READAHEAD_MAX);
+						    SBI_DEFAULT_READ_AHEAD_MAX);
 	sbi->ll_ra_info.ra_async_pages_per_file_threshold =
 				sbi->ll_ra_info.ra_max_pages_per_file;
 	sbi->ll_ra_info.ra_max_pages = sbi->ll_ra_info.ra_max_pages_per_file;
@@ -516,7 +516,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 	/* Don't change value if it was specified in the config log */
 	if (sbi->ll_ra_info.ra_max_read_ahead_whole_pages == -1) {
 		sbi->ll_ra_info.ra_max_read_ahead_whole_pages =
-			max_t(unsigned long, SBI_DEFAULT_READAHEAD_WHOLE_MAX,
+			max_t(unsigned long, SBI_DEFAULT_READ_AHEAD_WHOLE_MAX,
 			      (data->ocd_brw_size >> PAGE_SHIFT));
 		if (sbi->ll_ra_info.ra_max_read_ahead_whole_pages >
 		    sbi->ll_ra_info.ra_max_pages_per_file)

lustre/llite/lproc_llite.c
@@ -321,40 +321,41 @@ static int ll_site_stats_seq_show(struct seq_file *m, void *v)
 
 LDEBUGFS_SEQ_FOPS_RO(ll_site_stats);
 
-static int ll_max_readahead_mb_seq_show(struct seq_file *m, void *v)
+static ssize_t max_read_ahead_mb_show(struct kobject *kobj,
+				      struct attribute *attr, char *buf)
 {
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
 	unsigned long ra_max_mb;
 
 	spin_lock(&sbi->ll_lock);
 	ra_max_mb = PAGES_TO_MiB(sbi->ll_ra_info.ra_max_pages);
 	spin_unlock(&sbi->ll_lock);
 
-	seq_printf(m, "%lu\n", ra_max_mb);
-	return 0;
+	return snprintf(buf, PAGE_SIZE, "%lu\n", ra_max_mb);
 }
 
-static ssize_t
-ll_max_readahead_mb_seq_write(struct file *file, const char __user *buffer,
-			      size_t count, loff_t *off)
+static ssize_t max_read_ahead_mb_store(struct kobject *kobj,
+				       struct attribute *attr,
+				       const char *buffer, size_t count)
 {
-	struct seq_file *m = file->private_data;
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
-	s64 ra_max_mb, pages_number;
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	u64 ra_max_mb, pages_number;
 	int rc;
 
-	rc = lprocfs_str_with_units_to_s64(buffer, count, &ra_max_mb, 'M');
+	rc = sysfs_memparse(buffer, count, &ra_max_mb, "MiB");
 	if (rc)
 		return rc;
 
 	pages_number = round_up(ra_max_mb, 1024 * 1024) >> PAGE_SHIFT;
-	if (pages_number < 0 || pages_number > cfs_totalram_pages() / 2) {
+	CDEBUG(D_INFO, "%s: set max_read_ahead_mb=%llu (%llu pages)\n",
+	       sbi->ll_fsname, PAGES_TO_MiB(pages_number), pages_number);
+	if (pages_number > cfs_totalram_pages() / 2) {
 		/* 1/2 of RAM */
-		CERROR("%s: can't set max_readahead_mb=%llu > %luMB\n",
+		CERROR("%s: cannot set max_read_ahead_mb=%llu > totalram/2=%luMB\n",
 		       sbi->ll_fsname, PAGES_TO_MiB(pages_number),
-		       PAGES_TO_MiB(cfs_totalram_pages()));
+		       PAGES_TO_MiB(cfs_totalram_pages() / 2));
 		return -ERANGE;
 	}
 
@@ -364,42 +365,40 @@ ll_max_readahead_mb_seq_write(struct file *file, const char __user *buffer,
 
 	return count;
 }
+LUSTRE_RW_ATTR(max_read_ahead_mb);
 
-LDEBUGFS_SEQ_FOPS(ll_max_readahead_mb);
-
-static int ll_max_readahead_per_file_mb_seq_show(struct seq_file *m, void *v)
+static ssize_t max_read_ahead_per_file_mb_show(struct kobject *kobj,
+					       struct attribute *attr,
+					       char *buf)
 {
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
 	unsigned long ra_max_file_mb;
 
 	spin_lock(&sbi->ll_lock);
 	ra_max_file_mb = PAGES_TO_MiB(sbi->ll_ra_info.ra_max_pages_per_file);
 	spin_unlock(&sbi->ll_lock);
 
-	seq_printf(m, "%lu\n", ra_max_file_mb);
-	return 0;
+	return snprintf(buf, PAGE_SIZE, "%lu\n", ra_max_file_mb);
 }
 
-static ssize_t
-ll_max_readahead_per_file_mb_seq_write(struct file *file,
-				       const char __user *buffer,
-				       size_t count, loff_t *off)
+static ssize_t max_read_ahead_per_file_mb_store(struct kobject *kobj,
+						struct attribute *attr,
+						const char *buffer,
+						size_t count)
 {
-	struct seq_file *m = file->private_data;
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
-	s64 ra_max_file_mb, pages_number;
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	u64 ra_max_file_mb, pages_number;
 	int rc;
 
-	rc = lprocfs_str_with_units_to_s64(buffer, count, &ra_max_file_mb,
-					   'M');
+	rc = sysfs_memparse(buffer, count, &ra_max_file_mb, "MiB");
 	if (rc)
 		return rc;
 
 	pages_number = round_up(ra_max_file_mb, 1024 * 1024) >> PAGE_SHIFT;
-	if (pages_number < 0 || pages_number > sbi->ll_ra_info.ra_max_pages) {
-		CERROR("%s: can't set max_readahead_per_file_mb=%llu > max_read_ahead_mb=%lu\n",
+	if (pages_number > sbi->ll_ra_info.ra_max_pages) {
+		CERROR("%s: cannot set max_read_ahead_per_file_mb=%llu > max_read_ahead_mb=%lu\n",
 		       sbi->ll_fsname, PAGES_TO_MiB(pages_number),
 		       PAGES_TO_MiB(sbi->ll_ra_info.ra_max_pages));
 		return -ERANGE;
@@ -411,36 +410,32 @@ ll_max_readahead_per_file_mb_seq_write(struct file *file,
 
 	return count;
 }
+LUSTRE_RW_ATTR(max_read_ahead_per_file_mb);
 
-LDEBUGFS_SEQ_FOPS(ll_max_readahead_per_file_mb);
-
-static int ll_max_read_ahead_whole_mb_seq_show(struct seq_file *m, void *v)
+static ssize_t max_read_ahead_whole_mb_show(struct kobject *kobj,
+					    struct attribute *attr, char *buf)
 {
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
 	unsigned long ra_max_whole_mb;
 
 	spin_lock(&sbi->ll_lock);
 	ra_max_whole_mb = PAGES_TO_MiB(sbi->ll_ra_info.ra_max_read_ahead_whole_pages);
 	spin_unlock(&sbi->ll_lock);
 
-	seq_printf(m, "%lu\n", ra_max_whole_mb);
-	return 0;
+	return snprintf(buf, PAGE_SIZE, "%lu\n", ra_max_whole_mb);
 }
 
-static ssize_t
-ll_max_read_ahead_whole_mb_seq_write(struct file *file,
-				     const char __user *buffer,
-				     size_t count, loff_t *off)
+static ssize_t max_read_ahead_whole_mb_store(struct kobject *kobj,
+					     struct attribute *attr,
+					     const char *buffer, size_t count)
 {
-	struct seq_file *m = file->private_data;
-	struct super_block *sb = m->private;
-	struct ll_sb_info *sbi = ll_s2sbi(sb);
-	s64 ra_max_whole_mb, pages_number;
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	u64 ra_max_whole_mb, pages_number;
 	int rc;
 
-	rc = lprocfs_str_with_units_to_s64(buffer, count, &ra_max_whole_mb,
-					   'M');
+	rc = sysfs_memparse(buffer, count, &ra_max_whole_mb, "MiB");
 	if (rc)
 		return rc;
 
@@ -448,11 +443,11 @@ ll_max_read_ahead_whole_mb_seq_write(struct file *file,
 	/* Cap this at the current max readahead window size, the readahead
 	 * algorithm does this anyway so it's pointless to set it larger.
 	 */
-	if (pages_number < 0 ||
-	    pages_number > sbi->ll_ra_info.ra_max_pages_per_file) {
-		CERROR("%s: can't set max_read_ahead_whole_mb=%llu > max_read_ahead_per_file_mb=%lu\n",
+	if (pages_number > sbi->ll_ra_info.ra_max_pages_per_file) {
+		CERROR("%s: cannot set max_read_ahead_whole_mb=%llu > max_read_ahead_per_file_mb=%lu\n",
 		       sbi->ll_fsname, PAGES_TO_MiB(pages_number),
 		       PAGES_TO_MiB(sbi->ll_ra_info.ra_max_pages_per_file));
+
 		return -ERANGE;
 	}
 
@@ -462,8 +457,7 @@ ll_max_read_ahead_whole_mb_seq_write(struct file *file,
 
 	return count;
 }
-
-LDEBUGFS_SEQ_FOPS(ll_max_read_ahead_whole_mb);
+LUSTRE_RW_ATTR(max_read_ahead_whole_mb);
 
 static int ll_max_cached_mb_seq_show(struct seq_file *m, void *v)
 {
@@ -500,9 +494,9 @@ static ssize_t ll_max_cached_mb_seq_write(struct file *file,
 	long diff = 0;
 	long nrpages = 0;
 	__u16 refcheck;
-	__s64 pages_number;
+	u64 pages_number;
 	int rc;
-	char kernbuf[128];
+	char kernbuf[128], *ptr;
 
 	ENTRY;
 	if (count >= sizeof(kernbuf))
@@ -510,11 +504,10 @@ static ssize_t ll_max_cached_mb_seq_write(struct file *file,
 
 	if (copy_from_user(kernbuf, buffer, count))
 		RETURN(-EFAULT);
-	kernbuf[count] = 0;
+	kernbuf[count] = '\0';
 
-	buffer += lprocfs_find_named_value(kernbuf, "max_cached_mb:", &count) -
-		  kernbuf;
-	rc = lprocfs_str_with_units_to_s64(buffer, count, &pages_number, 'M');
+	ptr = lprocfs_find_named_value(kernbuf, "max_cached_mb:", &count);
+	rc = sysfs_memparse(ptr, count, &pages_number, "MiB");
 	if (rc)
 		RETURN(rc);
 
@@ -595,7 +588,6 @@ out:
 	}
 	return rc;
 }
-
 LDEBUGFS_SEQ_FOPS(ll_max_cached_mb);
 
 static ssize_t checksums_show(struct kobject *kobj, struct attribute *attr,
@@ -1100,14 +1092,14 @@ static ssize_t max_read_ahead_async_active_show(struct kobject *kobj,
 }
 
 static ssize_t max_read_ahead_async_active_store(struct kobject *kobj,
-						struct attribute *attr,
-						const char *buffer,
-						size_t count)
+						 struct attribute *attr,
+						 const char *buffer,
+						 size_t count)
 {
-	unsigned int val;
-	int rc;
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
+	unsigned int val;
+	int rc;
 
 	rc = kstrtouint(buffer, 10, &val);
 	if (rc)
@@ -1121,7 +1113,9 @@ static ssize_t max_read_ahead_async_active_store(struct kobject *kobj,
 		return -ERANGE;
 	}
 
+	spin_lock(&sbi->ll_lock);
 	sbi->ll_ra_info.ra_async_max_active = val;
+	spin_unlock(&sbi->ll_lock);
 	workqueue_set_max_active(sbi->ll_ra_info.ll_readahead_wq, val);
 
 	return count;
@@ -1466,12 +1460,6 @@ LPROC_SEQ_FOPS(ll_pcc);
 struct lprocfs_vars lprocfs_llite_obd_vars[] = {
 	{ .name	=	"site",
 	  .fops	=	&ll_site_stats_fops			},
-	{ .name =	"max_read_ahead_mb",
-	  .fops =	&ll_max_readahead_mb_fops		},
-	{ .name =	"max_read_ahead_per_file_mb",
-	  .fops =	&ll_max_readahead_per_file_mb_fops	},
-	{ .name =	"max_read_ahead_whole_mb",
-	  .fops =	&ll_max_read_ahead_whole_mb_fops	},
 	{ .name	=	"max_cached_mb",
 	  .fops	=	&ll_max_cached_mb_fops			},
 	{ .name	=	"statahead_stats",
@@ -1504,6 +1492,11 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_uuid.attr,
 	&lustre_attr_checksums.attr,
 	&lustre_attr_checksum_pages.attr,
+	&lustre_attr_max_read_ahead_mb.attr,
+	&lustre_attr_max_read_ahead_per_file_mb.attr,
+	&lustre_attr_max_read_ahead_whole_mb.attr,
+	&lustre_attr_max_read_ahead_async_active.attr,
+	&lustre_attr_read_ahead_async_file_threshold_mb.attr,
 	&lustre_attr_stats_track_pid.attr,
 	&lustre_attr_stats_track_ppid.attr,
 	&lustre_attr_stats_track_gid.attr,
@@ -1520,8 +1513,6 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_file_heat.attr,
 	&lustre_attr_heat_decay_percentage.attr,
 	&lustre_attr_heat_period_second.attr,
-	&lustre_attr_max_read_ahead_async_active.attr,
-	&lustre_attr_read_ahead_async_file_threshold_mb.attr,
 	NULL,
 };
 

lustre/obdclass/lprocfs_status.c
@@ -2360,13 +2360,20 @@ ssize_t lprocfs_obd_max_pages_per_rpc_seq_write(struct file *file,
 	struct obd_import *imp;
 	struct obd_connect_data *ocd;
 	int chunk_mask, rc;
-	s64 val;
+	char kernbuf[22];
+	u64 val;
 
-	rc = lprocfs_str_with_units_to_s64(buffer, count, &val, '1');
+	if (count > sizeof(kernbuf) - 1)
+		return -EINVAL;
+
+	if (copy_from_user(kernbuf, buffer, count))
+		return -EFAULT;
+
+	kernbuf[count] = '\0';
+
+	rc = sysfs_memparse(kernbuf, count, &val, "B");
 	if (rc)
 		return rc;
-	if (val < 0)
-		return -ERANGE;
 
 	/* if the max_pages is specified in bytes, convert to pages */
 	if (val >= ONE_MB_BRW_SIZE)
@@ -2416,21 +2423,16 @@ ssize_t short_io_bytes_store(struct kobject *kobj, struct attribute *attr,
 	struct obd_device *dev = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
 	struct client_obd *cli = &dev->u.cli;
-	char kernbuf[32];
-	s64 val;
+	u64 val;
 	int rc;
 
-	if (count >= sizeof(kernbuf))
-		return -EINVAL;
-
-	memcpy(kernbuf, buffer, count);
-	kernbuf[count] = '\0';
-	rc = lu_str_to_s64(kernbuf, count, &val, '1');
-	if (rc)
-		GOTO(out, rc);
-
-	if (val == -1)
+	if (strcmp(buffer, "-1") == 0) {
 		val = OBD_DEF_SHORT_IO_BYTES;
+	} else {
+		rc = sysfs_memparse(buffer, count, &val, "B");
+		if (rc)
+			GOTO(out, rc);
+	}
 
 	if (val && (val < MIN_SHORT_IO_BYTES || val > LNET_MTU))
 		GOTO(out, rc = -ERANGE);

lustre/tests/sanity.sh
@@ -8932,11 +8932,6 @@ test_101c() {
 }
 run_test 101c "check stripe_size aligned read-ahead ================="
 
-set_read_ahead() {
-	$LCTL get_param -n llite.*.max_read_ahead_mb | head -n 1
-	$LCTL set_param -n llite.*.max_read_ahead_mb $1 > /dev/null 2>&1
-}
-
 test_101d() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 
@@ -8956,7 +8951,10 @@ test_101d() {
 	cancel_lru_locks osc
 
 	echo Disable read-ahead
-	local old_READAHEAD=$(set_read_ahead 0)
+	local old_RA=$($LCTL get_param -n llite.*.max_read_ahead_mb | head -n 1)
+	$LCTL set_param -n llite.*.max_read_ahead_mb=0
+	stack_trap "$LCTL set_param -n llite.*.max_read_ahead_mb $old_RA" EXIT
+	$LCTL get_param -n llite.*.max_read_ahead_mb
 
 	echo Reading the test file $file with read-ahead disabled
 	local raOFF=$(do_and_time "dd if=$file of=/dev/null bs=1M count=$sz_MB")
@@ -8964,7 +8962,7 @@ test_101d() {
 	echo Cancel LRU locks on lustre client to flush the client cache
 	cancel_lru_locks osc
 	echo Enable read-ahead with ${ra_MB}MB
-	set_read_ahead $ra_MB
+	$LCTL set_param -n llite.*.max_read_ahead_mb=$ra_MB
 
 	echo Reading the test file $file with read-ahead enabled
 	local raON=$(do_and_time "dd if=$file of=/dev/null bs=1M count=$sz_MB")
@@ -8972,7 +8970,6 @@ test_101d() {
 	echo "read-ahead disabled time read $raOFF"
 	echo "read-ahead enabled  time read $raON"
 
-	set_read_ahead $old_READAHEAD
 	rm -f $file
 	wait_delete_completed
 
@@ -16196,8 +16193,7 @@ test_224c() { # LU-6441
 	save_writethrough $p
 	set_cache writethrough on
 
-	local pages_per_rpc=$($LCTL get_param \
-				osc.*.max_pages_per_rpc)
+	local pages_per_rpc=$($LCTL get_param osc.*.max_pages_per_rpc)
 	local at_max=$($LCTL get_param -n at_max)
 	local timeout=$($LCTL get_param -n timeout)
 	local test_at="at_max"