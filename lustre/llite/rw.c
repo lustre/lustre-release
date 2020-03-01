lustre/lustre-release

commit cfbeae97d736152e5bdf8f3e759a260f6ee903d8

LU-12043 llite: extend readahead locks for striped file
Currently cl_io_read_ahead() can not return locks
that cross stripe boundary at one time, thus readahead
will stop because of this reason.

This is really bad, as we will stop readahead every
time we hit stripe boundary, for example default stripe
size is 1M, this could hurt performances very much
especially with async readahead introduced.

So try to use existed locks aggressivly if there is no
lock contention, otherwise lock should be not
less than requested extent.

Change-Id: I8b2dcd0e80138ea530272cab6a665981aa00cca8
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/35438
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw.c
@@ -364,6 +364,17 @@ ll_read_ahead_pages(const struct lu_env *env, struct cl_io *io,
 				if (rc < 0)
 					break;
 
+				/* Do not shrink the ria_end at any case until
+				 * the minimum end of current read is covered.
+				 * And only shrink the ria_end if the matched
+				 * LDLM lock doesn't cover more. */
+				if (page_idx > ra.cra_end ||
+				    (ra.cra_contention &&
+				     page_idx > ria->ria_end_min)) {
+					ria->ria_end = ra.cra_end;
+					break;
+				}
+
 				CDEBUG(D_READA, "idx: %lu, ra: %lu, rpc: %lu\n",
 				       page_idx, ra.cra_end, ra.cra_rpc_size);
 				LASSERTF(ra.cra_end >= page_idx,
@@ -380,8 +391,6 @@ ll_read_ahead_pages(const struct lu_env *env, struct cl_io *io,
 					ria->ria_end = end - 1;
 				if (ria->ria_end < ria->ria_end_min)
 					ria->ria_end = ria->ria_end_min;
-				if (ria->ria_end > ra.cra_end)
-					ria->ria_end = ra.cra_end;
 			}
 			if (page_idx > ria->ria_end)
 				break;

lustre/include/cl_object.h
@@ -1484,6 +1484,8 @@ struct cl_read_ahead {
 	void    (*cra_release)(const struct lu_env *env, void *cbdata);
 	/* Callback data for cra_release routine */
 	void	*cra_cbdata;
+	/* whether lock is in contention */
+	bool	cra_contention;
 };
 
 static inline void cl_read_ahead_release(const struct lu_env *env,

lustre/osc/osc_io.c
@@ -91,6 +91,8 @@ static int osc_io_read_ahead(const struct lu_env *env,
 				       dlmlock->l_policy_data.l_extent.end);
 		ra->cra_release = osc_read_ahead_release;
 		ra->cra_cbdata = dlmlock;
+		if (ra->cra_end != CL_PAGE_EOF)
+			ra->cra_contention = true;
 		result = 0;
 	}
 

commit 347ec7b87c766613d43aa340c3a1e6fa3cdcf044

LU-13293 llite: don't abort readahead too aggressively
We will stop readahead if we have covered @ria_end_idx_min
but lock contention case happen, this could cause a problem
with small read with SSF mode.

Because even lock contention happen, but every client could
potentially have a large consecutive pages to read, if this
is a 4K read, it will end up with a lot small read.

To fix this problem, at least allow readahead continue with
one stripe size, this is exact behavior before the commit

Without Patch:
Max Write: 13082.37 MiB/sec (13717.85 MB/sec)
Max Read:  854.17 MiB/sec (895.67 MB/sec)

With Patch:
Max Write: 12448.90 MiB/sec (13053.61 MB/sec)
Max Read: 23921.73 MiB/sec (25083.75 MB/sec)

Fixes: cfbeae9 ("LU-12043 llite: extend readahead locks for striped file")
Change-Id: I59963592f6dbe6babd746cd01441f4a99a8cafcb
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/37697
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw.c
@@ -373,19 +373,30 @@ ll_read_ahead_pages(const struct lu_env *env, struct cl_io *io,
 			if (ra.cra_end_idx == 0 || ra.cra_end_idx < page_idx) {
 				pgoff_t end_idx;
 
+				/*
+				 * Do not shrink ria_end_idx at any case until
+				 * the minimum end of current read is covered.
+				 *
+				 * Do not extend read lock accross stripe if
+				 * lock contention detected.
+				 */
+				if (ra.cra_contention &&
+				    page_idx > ria->ria_end_idx_min) {
+					ria->ria_end_idx = *ra_end;
+					break;
+				}
+
 				cl_read_ahead_release(env, &ra);
 
 				rc = cl_io_read_ahead(env, io, page_idx, &ra);
 				if (rc < 0)
 					break;
 
-				/* Do not shrink ria_end_idx at any case until
-				 * the minimum end of current read is covered.
-				 * And only shrink ria_end_idx if the matched
-				 * LDLM lock doesn't cover more. */
-				if (page_idx > ra.cra_end_idx ||
-				    (ra.cra_contention &&
-				     page_idx > ria->ria_end_idx_min)) {
+				 /*
+				  * Only shrink ria_end_idx if the matched
+				  * LDLM lock doesn't cover more.
+				  */
+				if (page_idx > ra.cra_end_idx) {
 					ria->ria_end_idx = ra.cra_end_idx;
 					break;
 				}