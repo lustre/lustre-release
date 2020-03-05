lustre/lustre-release

commit f64e9f19f68e3983024520bd80122f12367a235a

LU-12977 ldiskfs: properly take inode_lock() for truncates
Originally Lustre grabbed the inode_lock() but this lead to
deadlocks as described in LU-6446 and LU-4252. The recent work
of LU-10048 changed the truncate code so that it is called
asynchronously from the main transactions. This should avoid
lock ordering issues. It should be safe to take the
inode_lock() around ldiskfs_truncate() and remove the WARN().

Test-Parameters: fstype=ldiskfs testlist=racer

Change-Id: Id7b6d05d054ab041980e946989aa1effae5c7111
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37116
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -2160,7 +2160,9 @@ void osd_execute_truncate(struct osd_object *obj)
 		return;
 	}
 
+	inode_lock(inode);
 	ldiskfs_truncate(inode);
+	inode_unlock(inode);
 
 	/*
 	 * For a partial-page truncate, flush the page to disk immediately to

ldiskfs/kernel_patches/patches/rhel7/ext4-remove-truncate-warning.patch
@@ -1,39 +1,0 @@
-There will cause a deadlock if invoke ext4_truncate with i_mutex locked
-in lustre. Since lustre has own lock to provide protect so we don't
-need this check at all. 
-
-Index: linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/inode.c
-===================================================================
---- linux-3.10.0-229.1.2.fc21.x86_64.orig/fs/ext4/inode.c
-+++ linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/inode.c
-@@ -3644,8 +3644,6 @@ void ext4_truncate(struct inode *inode)
- 	 * or it completely new indode. In those cases we might not
- 	 * have i_mutex locked because it's not necessary.
- 	 */
--	if (!(inode->i_state & (I_NEW|I_FREEING)))
--		WARN_ON(!mutex_is_locked(&inode->i_mutex));
- 	trace_ext4_truncate_enter(inode);
- 
- 	if (!ext4_can_truncate(inode))
-Index: linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/namei.c
-===================================================================
---- linux-3.10.0-229.1.2.fc21.x86_64.orig/fs/ext4/namei.c
-+++ linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/namei.c
-@@ -3163,8 +3163,6 @@ int ext4_orphan_add(handle_t *handle, st
- 	if (!sbi->s_journal || is_bad_inode(inode))
- 		return 0;
- 
--	WARN_ON_ONCE(!(inode->i_state & (I_NEW | I_FREEING)) &&
--		     !mutex_is_locked(&inode->i_mutex));
- 	/*
- 	 * Exit early if inode already is on orphan list. This is a big speedup
- 	 * since we don't have to contend on the global s_orphan_lock.
-@@ -3245,8 +3243,6 @@ int ext4_orphan_del(handle_t *handle, st
- 	if (!sbi->s_journal && !(sbi->s_mount_state & EXT4_ORPHAN_FS))
- 		return 0;
- 
--	WARN_ON_ONCE(!(inode->i_state & (I_NEW | I_FREEING)) &&
--		     !mutex_is_locked(&inode->i_mutex));
- 	/* Do this quick check before taking global s_orphan_lock. */
- 	if (list_empty(&ei->i_orphan))
- 		return 0;

ldiskfs/kernel_patches/patches/sles12/ext4-remove-truncate-warning.patch
@@ -1,17 +1,0 @@
-There will cause a deadlock if invoke ext4_truncate with i_mutex locked
-in lustre. Since lustre has own lock to provide protect so we don't
-need this check at all. 
-
-Index: linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/inode.c
-===================================================================
---- linux-3.10.0-229.1.2.fc21.x86_64.orig/fs/ext4/inode.c
-+++ linux-3.10.0-229.1.2.fc21.x86_64/fs/ext4/inode.c
-@@ -3644,8 +3644,6 @@ void ext4_truncate(struct inode *inode)
- 	 * or it completely new indode. In those cases we might not
- 	 * have i_mutex locked because it's not necessary.
- 	 */
--	if (!(inode->i_state & (I_NEW|I_FREEING)))
--		WARN_ON(!mutex_is_locked(&inode->i_mutex));
- 	trace_ext4_truncate_enter(inode);
- 
- 	if (!ext4_can_truncate(inode))

ldiskfs/kernel_patches/patches/ubuntu18/ext4-remove-truncate-warning.patch
@@ -1,39 +1,0 @@
-There will cause a deadlock if invoke ext4_truncate with i_mutex locked
-in lustre. Since lustre has own lock to provide protect so we don't
-need this check at all.
-
-Index: linux-4.15.0/fs/ext4/inode.c
-===================================================================
---- linux-4.15.0.orig/fs/ext4/inode.c
-+++ linux-4.15.0/fs/ext4/inode.c
-@@ -4398,8 +4398,6 @@ int ext4_truncate(struct inode *inode)
- 	 * or it's a completely new inode. In those cases we might not
- 	 * have i_mutex locked because it's not necessary.
- 	 */
--	if (!(inode->i_state & (I_NEW|I_FREEING)))
--		WARN_ON(!inode_is_locked(inode));
- 	trace_ext4_truncate_enter(inode);
- 
- 	if (!ext4_can_truncate(inode))
-Index: linux-4.15.0/fs/ext4/namei.c
-===================================================================
---- linux-4.15.0.orig/fs/ext4/namei.c
-+++ linux-4.15.0/fs/ext4/namei.c
-@@ -3370,8 +3370,6 @@ int ext4_orphan_add(handle_t *handle, st
- 	if (!sbi->s_journal || is_bad_inode(inode))
- 		return 0;
- 
--	WARN_ON_ONCE(!(inode->i_state & (I_NEW | I_FREEING)) &&
--		     !inode_is_locked(inode));
- 	/*
- 	 * Exit early if inode already is on orphan list. This is a big speedup
- 	 * since we don't have to contend on the global s_orphan_lock.
-@@ -3452,8 +3450,6 @@ int ext4_orphan_del(handle_t *handle, st
- 	if (!sbi->s_journal && !(sbi->s_mount_state & EXT4_ORPHAN_FS))
- 		return 0;
- 
--	WARN_ON_ONCE(!(inode->i_state & (I_NEW | I_FREEING)) &&
--		     !inode_is_locked(inode));
- 	/* Do this quick check before taking global s_orphan_lock. */
- 	if (list_empty(&ei->i_orphan))
- 		return 0;

ldiskfs/kernel_patches/series/ldiskfs-3.10-rhel7.2.series
@@ -15,7 +15,6 @@ rhel7/ext4-nocmtime.patch
 rhel7/ext4-large-dir.patch
 rhel7.2/ext4-pdirop.patch
 rhel7/ext4-max-dir-size.patch
-rhel7/ext4-remove-truncate-warning.patch
 rhel7/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
 rhel7/ext4-give-warning-with-dir-htree-growing.patch
 rhel7/ext4-mmp-brelse.patch

ldiskfs/kernel_patches/series/ldiskfs-3.10-rhel7.3.series
@@ -15,7 +15,6 @@ rhel7/ext4-nocmtime.patch
 rhel7/ext4-large-dir.patch
 rhel7.2/ext4-pdirop.patch
 rhel7/ext4-max-dir-size.patch
-rhel7/ext4-remove-truncate-warning.patch
 rhel7.3/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
 rhel7/ext4-give-warning-with-dir-htree-growing.patch
 rhel7/ext4-mmp-brelse.patch

ldiskfs/kernel_patches/series/ldiskfs-3.10-rhel7.4.series
@@ -15,7 +15,6 @@ rhel7/ext4-nocmtime.patch
 rhel7.4/ext4-large-dir.patch
 rhel7.4/ext4-pdirop.patch
 rhel7/ext4-max-dir-size.patch
-rhel7/ext4-remove-truncate-warning.patch
 rhel7.3/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
 rhel7/ext4-give-warning-with-dir-htree-growing.patch
 rhel7/ext4-mmp-brelse.patch

ldiskfs/kernel_patches/series/ldiskfs-3.10-rhel7.5.series
@@ -15,7 +15,6 @@ rhel7/ext4-nocmtime.patch
 rhel7.4/ext4-large-dir.patch
 rhel7.4/ext4-pdirop.patch
 rhel7/ext4-max-dir-size.patch
-rhel7/ext4-remove-truncate-warning.patch
 rhel7.3/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
 rhel7/ext4-give-warning-with-dir-htree-growing.patch
 rhel7/ext4-mmp-brelse.patch

commit 9016356a288cb3365ba69c4f287406aa7ebdf97a

LU-13234: osd-ldiskfs: hold inode mutex for ldiskfs_orphan_add()
See following warning:

/home/green/git/lustre-release/ldiskfs/namei.c:3310 ldiskfs_orphan_add+0x11e/0x2a0 [ldiskfs]
...
Call Trace:
[<ffffffff817d1711>] dump_stack+0x19/0x1b
[<ffffffff8108ba58>] __warn+0xd8/0x100
[<ffffffff8108bb9d>] warn_slowpath_null+0x1d/0x20
[<ffffffffa0adbfce>] ldiskfs_orphan_add+0x11e/0x2a0 [ldiskfs]
[<ffffffffa0bb3b73>] osd_punch+0x2a3/0x6c0 [osd_ldiskfs]
[<ffffffffa0692541>] ? tgt_fmd_put+0x41/0x120 [ptlrpc]
[<ffffffffa0e8b0a8>] ofd_object_punch+0x7e8/0xce0 [ofd]
[<ffffffffa0e7a5d3>] ofd_punch_hdl+0x4f3/0xa70 [ofd]
[<ffffffffa0673f55>] tgt_request_handle+0x965/0x1620 [ptlrpc]
[<ffffffffa0229dde>] ? libcfs_nid2str_r+0xfe/0x130 [lnet]
[<ffffffffa0616f60>] ptlrpc_server_handle_request+0x250/0xb10 [ptlrpc]
[<ffffffff810c6941>] ? __wake_up_common_lock+0x91/0xc0
[<ffffffff810c6250>] ? sched_feat_set+0xf0/0xf0
[<ffffffffa061b1c0>] ptlrpc_main+0xcb0/0x1cb0 [ptlrpc]
[<ffffffff810c665d>] ? finish_task_switch+0x5d/0x1b0
[<ffffffffa061a510>] ? ptlrpc_register_service+0xff0/0xff0 [ptlrpc]
[<ffffffff810b8254>] kthread+0xe4/0xf0
[<ffffffff810b8170>] ? kthread_create_on_node+0x140/0x140
[<ffffffff817e5ddd>] ret_from_fork_nospec_begin+0x7/0x21
[<ffffffff810b8170>] ? kthread_create_on_node+0x140/0x140

Hold mutex lock for ldiskfs_orphan_add() to eliminate warning.

Fixes: f64e9f19f68e ("LU-12977 ldiskfs: properly take inode_lock() for truncates")
Change-Id: I810c3c170649b3c96d98f227a480bb1092f2386c
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/37528
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -1948,10 +1948,12 @@ static int osd_punch(const struct lu_env *env, struct dt_object *dt,
 		GOTO(out, rc);
 	}
 
+	inode_lock(inode);
 	/* add to orphan list to ensure truncate completion
 	 * if this transaction succeed. ldiskfs_truncate()
 	 * will take the inode out of the list */
 	rc = ldiskfs_orphan_add(oh->ot_handle, inode);
+	inode_unlock(inode);
 	if (rc != 0)
 		GOTO(out, rc);
 