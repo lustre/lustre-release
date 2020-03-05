lustre/lustre-release

commit 85a12c6c8d7a6d40fc81c73f9b900475b58e3e98

LU-12828 ldlm: FLOCK request can be processed twice
Original request can be processed after resend
request, so it can create a lock on MDT without
client lock or unlock other lock.

Make flock enqueue to use modify RPC slot.

Change-Id: Icfee202fe2e389beda1116f78f8b933c7ea182fb
Cray-bug-id: LUS-5739
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Signed-off-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-on: https://review.whamcloud.com/36340
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdc/mdc_locks.c
@@ -860,6 +860,16 @@ out_lock:
 	RETURN(rc);
 }
 
+static inline bool mdc_skip_mod_rpc_slot(const struct lookup_intent *it)
+{
+	if (it != NULL &&
+	    (it->it_op == IT_GETATTR || it->it_op == IT_LOOKUP ||
+	     it->it_op == IT_READDIR ||
+	     (it->it_op == IT_LAYOUT && !(it->it_flags & MDS_FMODE_WRITE))))
+		return true;
+	return false;
+}
+
 /* We always reserve enough space in the reply packet for a stripe MD, because
  * we don't know in advance the file type. */
 static int mdc_enqueue_base(struct obd_export *exp,
@@ -871,7 +881,7 @@ static int mdc_enqueue_base(struct obd_export *exp,
 			    __u64 extra_lock_flags)
 {
 	struct obd_device *obddev = class_exp2obd(exp);
-	struct ptlrpc_request *req = NULL;
+	struct ptlrpc_request *req;
 	__u64 flags, saved_flags = extra_lock_flags;
 	struct ldlm_res_id res_id;
 	static const union ldlm_policy_data lookup_policy = {
@@ -922,6 +932,7 @@ resend:
 		LASSERTF(einfo->ei_type == LDLM_FLOCK, "lock type %d\n",
 			 einfo->ei_type);
 		res_id.name[3] = LDLM_FLOCK;
+		req = ldlm_enqueue_pack(exp, 0);
 	} else if (it->it_op & IT_OPEN) {
 		req = mdc_intent_open_pack(exp, it, op_data, acl_bufsize);
 	} else if (it->it_op & (IT_GETATTR | IT_LOOKUP)) {
@@ -949,20 +960,7 @@ resend:
 		req->rq_sent = ktime_get_real_seconds() + resends;
         }
 
-	/* It is important to obtain modify RPC slot first (if applicable), so
-	 * that threads that are waiting for a modify RPC slot are not polluting
-	 * our rpcs in flight counter.
-	 * We do not do flock request limiting, though */
-	if (it) {
-		mdc_get_mod_rpc_slot(req, it);
-		rc = obd_get_request_slot(&obddev->u.cli);
-		if (rc != 0) {
-			mdc_put_mod_rpc_slot(req, it);
-			mdc_clear_replay_flag(req, 0);
-			ptlrpc_req_finished(req);
-			RETURN(rc);
-		}
-	}
+	einfo->ei_enq_slot = !mdc_skip_mod_rpc_slot(it);
 
 	/* With Data-on-MDT the glimpse callback is needed too.
 	 * It is set here in advance but not in mdc_finish_enqueue()
@@ -973,6 +971,7 @@ resend:
 
 	rc = ldlm_cli_enqueue(exp, &req, einfo, &res_id, policy, &flags, NULL,
 			      0, lvb_type, lockh, 0);
+
 	if (!it) {
 		/* For flock requests we immediatelly return without further
 		   delay and let caller deal with the rest, since rest of
@@ -986,11 +985,9 @@ resend:
 		    (einfo->ei_type == LDLM_FLOCK) &&
 		    (einfo->ei_mode == LCK_NL))
 			goto resend;
+		ptlrpc_req_finished(req);
 		RETURN(rc);
 	}
-
-	obd_put_request_slot(&obddev->u.cli);
-	mdc_put_mod_rpc_slot(req, it);
 
 	if (rc < 0) {
 		CDEBUG(D_INFO,
@@ -1335,19 +1332,15 @@ static int mdc_intent_getattr_async_interpret(const struct lu_env *env,
 	struct obd_export *exp = ga->ga_exp;
 	struct md_enqueue_info *minfo = ga->ga_minfo;
 	struct ldlm_enqueue_info *einfo = &minfo->mi_einfo;
-	struct lookup_intent *it;
-	struct lustre_handle *lockh;
-	struct obd_device *obddev;
-	struct ldlm_reply *lockrep;
-	__u64 flags = LDLM_FL_HAS_INTENT;
+	struct lookup_intent     *it;
+	struct lustre_handle     *lockh;
+	struct ldlm_reply	 *lockrep;
+	__u64                     flags = LDLM_FL_HAS_INTENT;
 	ENTRY;
 
         it    = &minfo->mi_it;
         lockh = &minfo->mi_lockh;
 
-        obddev = class_exp2obd(exp);
-
-	obd_put_request_slot(&obddev->u.cli);
         if (OBD_FAIL_CHECK(OBD_FAIL_MDC_GETATTR_ENQUEUE))
                 rc = -ETIMEDOUT;
 
@@ -1384,7 +1377,6 @@ int mdc_intent_getattr_async(struct obd_export *exp,
 	struct lookup_intent    *it = &minfo->mi_it;
 	struct ptlrpc_request   *req;
 	struct mdc_getattr_args *ga;
-	struct obd_device       *obddev = class_exp2obd(exp);
 	struct ldlm_res_id       res_id;
 	union ldlm_policy_data policy = {
 				.l_inodebits = { MDS_INODELOCK_LOOKUP |
@@ -1405,12 +1397,6 @@ int mdc_intent_getattr_async(struct obd_export *exp,
 	if (IS_ERR(req))
 		RETURN(PTR_ERR(req));
 
-	rc = obd_get_request_slot(&obddev->u.cli);
-	if (rc != 0) {
-		ptlrpc_req_finished(req);
-		RETURN(rc);
-	}
-
 	/* With Data-on-MDT the glimpse callback is needed too.
 	 * It is set here in advance but not in mdc_finish_enqueue()
 	 * to avoid possible races. It is safe to have glimpse handler
@@ -1421,7 +1407,6 @@ int mdc_intent_getattr_async(struct obd_export *exp,
 	rc = ldlm_cli_enqueue(exp, &req, &minfo->mi_einfo, &res_id, &policy,
 			      &flags, NULL, 0, LVB_T_NONE, &minfo->mi_lockh, 1);
 	if (rc < 0) {
-		obd_put_request_slot(&obddev->u.cli);
 		ptlrpc_req_finished(req);
 		RETURN(rc);
 	}

lustre/include/lu_target.h
@@ -220,6 +220,8 @@ struct lu_target {
 #define LUT_REPLY_SLOTS_PER_CHUNK (1<<20)
 #define LUT_REPLY_SLOTS_MAX_CHUNKS 16
 
+#define TRD_INDEX_MEMORY -1
+
 /**
  * Target reply data
  */
@@ -494,10 +496,10 @@ int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg,
 			   int sync);
 int tgt_reply_data_init(const struct lu_env *env, struct lu_target *tgt);
 int tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
-int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
-		       struct tg_export_data *ted, struct tg_reply_data *trd,
-		       struct ptlrpc_request *req,
-		       struct thandle *th, bool update_lrd_file);
+int tgt_mk_reply_data(const struct lu_env *env, struct lu_target *tgt,
+		      struct tg_export_data *ted, struct ptlrpc_request *req,
+		      __u64 opdata, struct thandle *th, bool write_update,
+		      __u64 transno);
 struct tg_reply_data *tgt_lookup_reply_by_xid(struct tg_export_data *ted,
 					       __u64 xid);
 int tgt_tunables_init(struct lu_target *lut);

lustre/include/lustre_dlm.h
@@ -1200,6 +1200,7 @@ struct ldlm_enqueue_info {
 	void		*ei_namespace;	/** lock namespace **/
 	u64		ei_inodebits;	/** lock inode bits **/
 	unsigned int	ei_enq_slave:1;	/** whether enqueue slave stripes */
+	unsigned int	ei_enq_slot:1;	/** whether acquire rpc slot */
 };
 
 #define ei_res_id	ei_cb_gl

lustre/include/lustre_mdc.h
@@ -63,32 +63,6 @@ struct obd_export;
 struct ptlrpc_request;
 struct obd_device;
 
-static inline void mdc_get_mod_rpc_slot(struct ptlrpc_request *req,
-					struct lookup_intent *it)
-{
-	struct client_obd *cli = &req->rq_import->imp_obd->u.cli;
-	__u32 opc;
-	__u16 tag;
-
-	opc = lustre_msg_get_opc(req->rq_reqmsg);
-	tag = obd_get_mod_rpc_slot(cli, opc, it);
-	lustre_msg_set_tag(req->rq_reqmsg, tag);
-	ptlrpc_reassign_next_xid(req);
-}
-
-static inline void mdc_put_mod_rpc_slot(struct ptlrpc_request *req,
-					struct lookup_intent *it)
-{
-	struct client_obd *cli = &req->rq_import->imp_obd->u.cli;
-	__u32 opc;
-	__u16 tag;
-
-	opc = lustre_msg_get_opc(req->rq_reqmsg);
-	tag = lustre_msg_get_tag(req->rq_reqmsg);
-	obd_put_mod_rpc_slot(cli, opc, it, tag);
-}
-
-
 /**
  * Update the maximum possible easize.
  *

lustre/include/lustre_net.h
@@ -2167,7 +2167,8 @@ void ptlrpc_retain_replayable_request(struct ptlrpc_request *req,
 __u64 ptlrpc_next_xid(void);
 __u64 ptlrpc_sample_next_xid(void);
 __u64 ptlrpc_req_xid(struct ptlrpc_request *request);
-void ptlrpc_reassign_next_xid(struct ptlrpc_request *req);
+void ptlrpc_get_mod_rpc_slot(struct ptlrpc_request *req);
+void ptlrpc_put_mod_rpc_slot(struct ptlrpc_request *req);
 
 /* Set of routines to run a function in ptlrpcd context */
 void *ptlrpcd_alloc_work(struct obd_import *imp,

lustre/include/obd_class.h
@@ -129,10 +129,8 @@ __u16 obd_get_max_mod_rpcs_in_flight(struct client_obd *cli);
 int obd_set_max_mod_rpcs_in_flight(struct client_obd *cli, __u16 max);
 int obd_mod_rpc_stats_seq_show(struct client_obd *cli, struct seq_file *seq);
 
-__u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc,
-			   struct lookup_intent *it);
-void obd_put_mod_rpc_slot(struct client_obd *cli, __u32 opc,
-			  struct lookup_intent *it, __u16 tag);
+__u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc);
+void obd_put_mod_rpc_slot(struct client_obd *cli, __u32 opc, __u16 tag);
 
 struct llog_handle;
 struct llog_rec_hdr;

lustre/ldlm/ldlm_lock.c
@@ -1759,6 +1759,9 @@ enum ldlm_error ldlm_lock_enqueue(const struct lu_env *env,
 	int local = ns_is_client(ldlm_res_to_ns(res));
 	enum ldlm_error rc = ELDLM_OK;
 	struct ldlm_interval *node = NULL;
+#ifdef HAVE_SERVER_SUPPORT
+	bool reconstruct = false;
+#endif
 	ENTRY;
 
         /* policies are not executed on the client or during replay */
@@ -1813,6 +1816,19 @@ enum ldlm_error ldlm_lock_enqueue(const struct lu_env *env,
 	 * this lock in the future. - jay */
 	if (!local && (*flags & LDLM_FL_REPLAY) && res->lr_type == LDLM_EXTENT)
 		OBD_SLAB_ALLOC_PTR_GFP(node, ldlm_interval_slab, GFP_NOFS);
+
+#ifdef HAVE_SERVER_SUPPORT
+	reconstruct = !local && res->lr_type == LDLM_FLOCK &&
+		      !(*flags & LDLM_FL_TEST_LOCK);
+	if (reconstruct) {
+		rc = req_can_reconstruct(cookie, NULL);
+		if (rc != 0) {
+			if (rc == 1)
+				rc = 0;
+			RETURN(rc);
+		}
+	}
+#endif
 
 	lock_res_and_lock(lock);
 	if (local && ldlm_is_granted(lock)) {
@@ -1887,6 +1903,16 @@ enum ldlm_error ldlm_lock_enqueue(const struct lu_env *env,
 
 out:
         unlock_res_and_lock(lock);
+
+#ifdef HAVE_SERVER_SUPPORT
+	if (reconstruct) {
+		struct ptlrpc_request *req = cookie;
+
+		tgt_mk_reply_data(NULL, NULL,
+				  &req->rq_export->exp_target_data,
+				  req, 0, NULL, false, 0);
+	}
+#endif
         if (node)
                 OBD_SLAB_FREE(node, ldlm_interval_slab, sizeof(*node));
         return rc;

lustre/ldlm/ldlm_lockd.c
@@ -1349,18 +1349,11 @@ int ldlm_handle_enqueue0(struct ldlm_namespace *ns,
 		lock->l_req_extent = lock->l_policy_data.l_extent;
 
 existing_lock:
-	if (flags & LDLM_FL_HAS_INTENT) {
-		/*
-		 * In this case, the reply buffer is allocated deep in
-		 * local_lock_enqueue by the policy function.
-		 */
-		cookie = req;
-	} else {
-		/*
-		 * based on the assumption that lvb size never changes during
+	cookie = req;
+	if (!(flags & LDLM_FL_HAS_INTENT)) {
+		/* based on the assumption that lvb size never changes during
 		 * resource life time otherwise it need resource->lr_lock's
-		 * protection
-		 */
+		 * protection */
 		req_capsule_set_size(&req->rq_pill, &RMF_DLM_LVB,
 				     RCL_SERVER, ldlm_lvbo_size(lock));
 

commit 00ef8d08a9edc88f8b50d5761a8f96583fc39591

LU-13229 ldlm: unlock request memory leak
resending an unlock request can caused an memory leak as new request
will allocated on resend. Lets finish a request before resend.

Fixes: 85a12c6c8d7 ("LU-12828 ldlm: FLOCK request can be processed twice")
Cray-bug-id: LUS-8447
Signed-off-by: Alexey Lyashkov <c17817@cray.com>
Change-Id: I15dfe0388d1305c8eb5be18b19b0ffc687454ef1
Reviewed-on: https://review.whamcloud.com/37670
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdc/mdc_locks.c
@@ -987,11 +987,11 @@ resend:
 		   can not rely on caller and this mainly for F_UNLCKs
 		   (explicits or automatically generated by Kernel to clean
 		   current FLocks upon exit) that can't be trashed */
+		ptlrpc_req_finished(req);
 		if (((rc == -EINTR) || (rc == -ETIMEDOUT)) &&
 		    (einfo->ei_type == LDLM_FLOCK) &&
 		    (einfo->ei_mode == LCK_NL))
 			goto resend;
-		ptlrpc_req_finished(req);
 		RETURN(rc);
 	}
 