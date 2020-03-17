lustre/lustre-release

commit c1fad6a9a583b395c23ea1a102f1b67c50640f6f

LU-10467 ptlrpc: convert waiting in sptlrpc_req_refresh_ctx()
The l_wait_event call in sptlrpc_req_refresh_ctx() is somewhat complex
as it is passed both an on_timeout and on_signal handler, and
on_timeout doesn't return a constant value.

The net effect is to wait for the timeout with signals blocked.  Then,
if the condition still isn't true, run the on_timeout handler and if
that returns zero, wait again - indefinitely this time - and allow
some signals.  If a signal was received, call the on_signal handler.

This is fairly straight forward to write out in C, as shown in the
patch.

Change-Id: I7f9cfb8a8ff234bed4045ab21b53d018337cd615
Signed-off-by: Mr NeilBrown <neilb@suse.com>
Reviewed-on: https://review.whamcloud.com/35987
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/sec.c
@@ -631,9 +631,8 @@ int ctx_check_refresh(struct ptlrpc_cli_ctx *ctx)
 }
 
 static
-int ctx_refresh_timeout(void *data)
+int ctx_refresh_timeout(struct ptlrpc_request *req)
 {
-	struct ptlrpc_request *req = data;
 	int rc;
 
 	/* conn_cnt is needed in expire_one_request */
@@ -653,9 +652,8 @@ int ctx_refresh_timeout(void *data)
 }
 
 static
-void ctx_refresh_interrupt(void *data)
+void ctx_refresh_interrupt(struct ptlrpc_request *req)
 {
-	struct ptlrpc_request *req = data;
 
 	spin_lock(&req->rq_lock);
 	req->rq_intr = 1;
@@ -689,7 +687,6 @@ int sptlrpc_req_refresh_ctx(struct ptlrpc_request *req, long timeout)
 {
 	struct ptlrpc_cli_ctx *ctx = req->rq_cli_ctx;
 	struct ptlrpc_sec *sec;
-	struct l_wait_info lwi;
 	int rc;
 
 	ENTRY;
@@ -823,10 +820,18 @@ again:
 	req->rq_restart = 0;
 	spin_unlock(&req->rq_lock);
 
-	lwi = LWI_TIMEOUT_INTR(cfs_time_seconds(timeout),
-			       ctx_refresh_timeout,
-			       ctx_refresh_interrupt, req);
-	rc = l_wait_event(req->rq_reply_waitq, ctx_check_refresh(ctx), &lwi);
+	if (wait_event_idle_timeout(req->rq_reply_waitq,
+				    ctx_check_refresh(ctx),
+				    cfs_time_seconds(timeout)) == 0) {
+		rc = -ETIMEDOUT;
+		if (!ctx_refresh_timeout(req) &&
+		    l_wait_event_abortable(req->rq_reply_waitq,
+					   ctx_check_refresh(ctx))
+		    == -ERESTARTSYS) {
+			rc = -EINTR;
+			ctx_refresh_interrupt(req);
+		}
+	}
 
 	/*
 	 * following cases could lead us here:

commit 0b09d826149f4baadce305df63396bf86eb20cf7

LU-13216 ptlrpc: sptlrpc_req_refresh_ctx's timeout semantic
sptlrpc_req_refresh_ctx() is only called with two possible timeouts
in the current code: either we do not wait at all, or we wait
indefinitely.
So change semantic of sptlrpc_req_refresh_ctx()'s timeout parameter,
to be more consistent with wait* primitives. timeout == 0 means
'do not wait at all', and timeout == MAX_SCHEDULE_TIMEOUT means
'wait indefinitely'. Other timeout values are unsupported.

Consequently, remove code that handles the case of a non zero and
non infinite timeout. And remove the unsed ctx_refresh_timeout()
callback.
It solves a problem in the current code, because in case of indefinite
wait, sptlrpc_req_refresh_ctx() is not supposed to try to refresh the
request, so it must not call the ctx_refresh_timeout() callback.

Fixes: c1fad6a9a5 ("LU-10467 ptlrpc: convert waiting in sptlrpc_req_refresh_ctx()")
Test-Parameters: trivial envdefinitions=SHARED_KEY=true testlist=sanity-sec
Test-Parameters: envdefinitions=SHARED_KEY=true testlist=sanity-sec
Test-Parameters: envdefinitions=SHARED_KEY=true testlist=sanity-sec
Test-Parameters: envdefinitions=SHARED_KEY=true testlist=sanity-sec
Test-Parameters: envdefinitions=SHARED_KEY=true testlist=sanity-sec
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I1b20457c86863d57f97f4d998f7c5fa8ec928b31
Reviewed-on: https://review.whamcloud.com/37473
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/client.c
@@ -1673,7 +1673,7 @@ static int ptlrpc_send_new_req(struct ptlrpc_request *req)
 
 	lustre_msg_set_status(req->rq_reqmsg, current->pid);
 
-	rc = sptlrpc_req_refresh_ctx(req, -1);
+	rc = sptlrpc_req_refresh_ctx(req, 0);
 	if (rc) {
 		if (req->rq_err) {
 			req->rq_status = rc;
@@ -1990,7 +1990,7 @@ int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
 				 * rq_wait_ctx is only touched by ptlrpcd,
 				 * so no lock is needed here.
 				 */
-				status = sptlrpc_req_refresh_ctx(req, -1);
+				status = sptlrpc_req_refresh_ctx(req, 0);
 				if (status) {
 					if (req->rq_err) {
 						req->rq_status = status;

lustre/ptlrpc/sec.c
@@ -628,27 +628,6 @@ int ctx_check_refresh(struct ptlrpc_cli_ctx *ctx)
 }
 
 static
-int ctx_refresh_timeout(struct ptlrpc_request *req)
-{
-	int rc;
-
-	/* conn_cnt is needed in expire_one_request */
-	lustre_msg_set_conn_cnt(req->rq_reqmsg, req->rq_import->imp_conn_cnt);
-
-	rc = ptlrpc_expire_one_request(req, 1);
-	/*
-	 * if we started recovery, we should mark this ctx dead; otherwise
-	 * in case of lgssd died nobody would retire this ctx, following
-	 * connecting will still find the same ctx thus cause deadlock.
-	 * there's an assumption that expire time of the request should be
-	 * later than the context refresh expire time.
-	 */
-	if (rc == 0)
-		req->rq_cli_ctx->cc_ops->die(req->rq_cli_ctx, 0);
-	return rc;
-}
-
-static
 void ctx_refresh_interrupt(struct ptlrpc_request *req)
 {
 
@@ -669,9 +648,9 @@ void req_off_ctx_list(struct ptlrpc_request *req, struct ptlrpc_cli_ctx *ctx)
 /**
  * To refresh the context of \req, if it's not up-to-date.
  * \param timeout
- * - < 0: don't wait
- * - = 0: wait until success or fatal error occur
- * - > 0: timeout value (in seconds)
+ * - == 0: do not wait
+ * - == MAX_SCHEDULE_TIMEOUT: wait indefinitely
+ * - > 0: not supported
  *
  * The status of the context could be subject to be changed by other threads
  * at any time. We allow this race, but once we return with 0, the caller will
@@ -692,6 +671,11 @@ int sptlrpc_req_refresh_ctx(struct ptlrpc_request *req, long timeout)
 
 	if (req->rq_ctx_init || req->rq_ctx_fini)
 		RETURN(0);
+
+	if (timeout != 0 && timeout != MAX_SCHEDULE_TIMEOUT) {
+		CERROR("req %p: invalid timeout %lu\n", req, timeout);
+		RETURN(-EINVAL);
+	}
 
 	/*
 	 * during the process a request's context might change type even
@@ -805,7 +789,7 @@ again:
 		list_add(&req->rq_ctx_chain, &ctx->cc_req_list);
 	spin_unlock(&ctx->cc_lock);
 
-	if (timeout < 0)
+	if (timeout == 0)
 		RETURN(-EWOULDBLOCK);
 
 	/* Clear any flags that may be present from previous sends */
@@ -817,17 +801,13 @@ again:
 	req->rq_restart = 0;
 	spin_unlock(&req->rq_lock);
 
-	if (wait_event_idle_timeout(req->rq_reply_waitq,
-				    ctx_check_refresh(ctx),
-				    cfs_time_seconds(timeout)) == 0) {
-		rc = -ETIMEDOUT;
-		if (!ctx_refresh_timeout(req) &&
-		    l_wait_event_abortable(req->rq_reply_waitq,
-					   ctx_check_refresh(ctx))
-		    == -ERESTARTSYS) {
-			rc = -EINTR;
-			ctx_refresh_interrupt(req);
-		}
+	/* by now we know that timeout value is MAX_SCHEDULE_TIMEOUT,
+	 * so wait indefinitely with non-fatal signals blocked
+	 */
+	if (l_wait_event_abortable(req->rq_reply_waitq,
+				   ctx_check_refresh(ctx)) == -ERESTARTSYS) {
+		rc = -EINTR;
+		ctx_refresh_interrupt(req);
 	}
 
 	/*
@@ -981,7 +961,7 @@ int sptlrpc_import_check_ctx(struct obd_import *imp)
 	req->rq_flvr = sec->ps_flvr;
 	req->rq_cli_ctx = ctx;
 
-	rc = sptlrpc_req_refresh_ctx(req, 0);
+	rc = sptlrpc_req_refresh_ctx(req, MAX_SCHEDULE_TIMEOUT);
 	LASSERT(list_empty(&req->rq_ctx_chain));
 	sptlrpc_cli_ctx_put(req->rq_cli_ctx, 1);
 	ptlrpc_request_cache_free(req);