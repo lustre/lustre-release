lustre/lustre-release

commit 8fbef5ee761920faa3c84f142f56934d6266a409

LU-12043 llite: make sure readahead cover current read
When doing readahead, @ria_end_min is used to indicate
how far we are expected to read to cover current
read.

update @ria_end_min unconditionally with IO end.
also @ria_end_min is closed interval which should be
calculated as start + count - 1;

Change-Id: If7f8da44da31623a73b363d5a18c1ec8b54da745
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/35215
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw.c
@@ -677,16 +677,8 @@ static int ll_readahead(const struct lu_env *env, struct cl_io *io,
 
 	/* at least to extend the readahead window to cover current read */
 	if (!hit && vio->vui_ra_valid &&
-	    vio->vui_ra_start + vio->vui_ra_count > ria->ria_start) {
-		unsigned long remainder;
-
-		/* to the end of current read window. */
-		mlen = vio->vui_ra_start + vio->vui_ra_count - ria->ria_start;
-		/* trim to RPC boundary */
-		ras_align(ras, ria->ria_start, &remainder);
-		mlen = min(mlen, ras->ras_rpc_size - remainder);
-		ria->ria_end_min = ria->ria_start + mlen;
-	}
+	    vio->vui_ra_start + vio->vui_ra_count > ria->ria_start)
+		ria->ria_end_min = vio->vui_ra_start + vio->vui_ra_count - 1;
 
 	ria->ria_reserved = ll_ra_count_get(ll_i2sbi(inode), ria, len, mlen);
 	if (ria->ria_reserved < len)

lustre/tests/sanity.sh
@@ -8617,6 +8617,26 @@ test_101g() {
 }
 run_test 101g "Big bulk(4/16 MiB) readahead"
 
+test_101h() {
+	$LFS setstripe -i 0 -c 1 $DIR/$tfile
+
+	dd if=/dev/zero of=$DIR/$tfile bs=1M count=70 ||
+		error "dd 70M file failed"
+	echo Cancel LRU locks on lustre client to flush the client cache
+	cancel_lru_locks osc
+
+	echo "Reset readahead stats"
+	$LCTL set_param -n llite.*.read_ahead_stats 0
+
+	echo "Read 10M of data but cross 64M bundary"
+	dd if=$DIR/$tfile of=/dev/null bs=10M skip=6 count=1
+	local miss=$($LCTL get_param -n llite.*.read_ahead_stats |
+			get_named_value 'misses' | cut -d" " -f1 | calc_total)
+	[ $miss -eq 1 ] || error "expected miss 1 but got $miss"
+	rm -f $p $DIR/$tfile
+}
+run_test 101h "Readahead should cover current read window"
+
 setup_test102() {
 	test_mkdir $DIR/$tdir
 	chown $RUNAS_ID $DIR/$tdir

commit 777b04a09356afe425e9dc3dd3de385109990b52

LU-13386 llite: allow current readahead to exceed reservation
ll_ra_count_get() is designed to allow over reservation for performance
reasons, this could happen that multiple threads consuming a small
readahead window limit, it will fallback to small 4K read which hurts
performances.

Fixes: 8fbef5e("LU-12043 llite: make sure readahead cover current read")
Change-Id: Ied4ee0ffcd55c568c70f7d3074e810cd0034ef04
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/38047
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw.c
@@ -729,9 +729,12 @@ static int ll_readahead(const struct lu_env *env, struct cl_io *io,
 
 	/* at least to extend the readahead window to cover current read */
 	if (!hit && vio->vui_ra_valid &&
-	    vio->vui_ra_start_idx + vio->vui_ra_pages > ria->ria_start_idx)
+	    vio->vui_ra_start_idx + vio->vui_ra_pages > ria->ria_start_idx) {
 		ria->ria_end_idx_min =
 			vio->vui_ra_start_idx + vio->vui_ra_pages - 1;
+		pages_min = vio->vui_ra_start_idx + vio->vui_ra_pages -
+				ria->ria_start_idx;
+	}
 
 	ria->ria_reserved = ll_ra_count_get(ll_i2sbi(inode), ria, pages,
 					    pages_min);

lustre/tests/sanity.sh
@@ -9273,6 +9273,30 @@ test_101h() {
 }
 run_test 101h "Readahead should cover current read window"
 
+test_101i() {
+	dd if=/dev/zero of=$DIR/$tfile bs=1M count=10 ||
+		error "dd 10M file failed"
+
+	local max_per_file_mb=$($LCTL get_param -n \
+		llite.*.max_read_ahead_per_file_mb 2>/dev/null)
+	cancel_lru_locks osc
+	stack_trap "$LCTL set_param llite.*.max_read_ahead_per_file_mb=$max_per_file_mb"
+	$LCTL set_param llite.*.max_read_ahead_per_file_mb=1 ||
+		error "set max_read_ahead_per_file_mb to 1 failed"
+
+	echo "Reset readahead stats"
+	$LCTL set_param llite.*.read_ahead_stats=0
+
+	dd if=$DIR/$tfile of=/dev/null bs=2M
+
+	$LCTL get_param llite.*.read_ahead_stats
+	local miss=$($LCTL get_param -n llite.*.read_ahead_stats |
+		     awk '/misses/ { print $2 }')
+	[ $miss -eq 5 ] || error "expected misses 5 but got $miss"
+	rm -f $DIR/$tfile
+}
+run_test 101i "allow current readahead to exceed reservation"
+
 setup_test102() {
 	test_mkdir $DIR/$tdir
 	chown $RUNAS_ID $DIR/$tdir