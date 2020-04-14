lustre/lustre-release

commit 1bdc4fd0594e8948623bde18e7b5d691104d8808

LU-6307 obdclass: distinguish MGC/MDT connection properly
In the 5f8847bca12afb798de600299356ed2e3655a53e, we introduced the
version checking for the MDT-MDT connection. But there is a corner
that the MGC will set OBD_CONNECT_MNE_SWAB (that is defined as the
same as OBD_CONNECT_MDS_MDS) in the connection flags for Imperative
Recovery interoperability issues with MGS. So the server needs to
know whether the connection is really from another MDT or from the
MGC via checking OBD_CONNECT_FID (that is not set for the MGC-MGS
connection).

Test-Parameters: envdefinitions=ONLY=105 clientjob=lustre-b2_6 clientbuildno=19 testlist=recovery-small
Signed-off-by: Fan Yong <fan.yong@intel.com>
Change-Id: I9cee743d5474702b77adbb8c3dedd6c19faef15f
Reviewed-on: http://review.whamcloud.com/13927
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Lai Siyao <lai.siyao@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/ldlm/ldlm_lib.c
@@ -902,10 +902,14 @@ int target_handle_connect(struct ptlrpc_request *req)
 			     OBD_CONNECT_LIGHTWEIGHT) != 0;
 
 		/* OBD_CONNECT_MNE_SWAB is defined as OBD_CONNECT_MDS_MDS
-		 * for Imperative Recovery connection from MGC to MGS. */
+		 * for Imperative Recovery connection from MGC to MGS.
+		 *
+		 * Via check OBD_CONNECT_FID, we can distinguish whether
+		 * the OBD_CONNECT_MDS_MDS/OBD_CONNECT_MNE_SWAB is from
+		 * MGC or MDT. */
 		if (!lw_client &&
 		    (data->ocd_connect_flags & OBD_CONNECT_MDS_MDS) &&
-		    !(data->ocd_connect_flags & OBD_CONNECT_IMP_RECOV) &&
+		    (data->ocd_connect_flags & OBD_CONNECT_FID) &&
 		    (data->ocd_connect_flags & OBD_CONNECT_VERSION)) {
 			__u32 major = OBD_OCD_VERSION_MAJOR(data->ocd_version);
 			__u32 minor = OBD_OCD_VERSION_MINOR(data->ocd_version);

lustre/ptlrpc/import.c
@@ -1016,7 +1016,7 @@ static int ptlrpc_connect_interpret(const struct lu_env *env,
 
 	if (!(imp->imp_connect_flags_orig & OBD_CONNECT_LIGHTWEIGHT) &&
 	    (imp->imp_connect_flags_orig & OBD_CONNECT_MDS_MDS) &&
-	    !(imp->imp_connect_flags_orig & OBD_CONNECT_IMP_RECOV) &&
+	    (imp->imp_connect_flags_orig & OBD_CONNECT_FID) &&
 	    (ocd->ocd_connect_flags & OBD_CONNECT_VERSION)) {
 		__u32 major = OBD_OCD_VERSION_MAJOR(ocd->ocd_version);
 		__u32 minor = OBD_OCD_VERSION_MINOR(ocd->ocd_version);

commit 3fe77a129e131014ff654bde616a62a1e243e322

LU-13356 client: don't use OBD_CONNECT_MNE_SWAB
OBD_CONNECT_MNE_SWAB is equal to OBD_CONNECT_MDS_MDS, and
it was used at MGC client in past for mne swabbing during interop.
Right now it is interpreted at MGS like OBD_CONNECT_MDS_MDS and skip
these clients from eviction and lock canceling after timeout.

Fixes: 1bdc4fd0594e ("LU-6307 obdclass: distinguish MGC/MDT connection properly")
Test-Parameters: testlist=runtests clientversion=2.12 envdefinitions=MDS_MOUNT_OPTS="-orw",OST_MOUNT_OPTS="-orw"
Test-Parameters: testlist=runtests serverversion=2.12 envdefinitions=MDS_MOUNT_OPTS="-orw",OST_MOUNT_OPTS="-orw"
Test-Parameters: testlist=runtests clientversion=2.10 clientdistro=el7.6 envdefinitions=MDS_MOUNT_OPTS="-orw",OST_MOUNT_OPTS="-orw"
Test-Parameters: testlist=runtests serverversion=2.10 serverdistro=el7.6 envdefinitions=MDS_MOUNT_OPTS="-orw",OST_MOUNT_OPTS="-orw"
Signed-off-by: Alexander Boyko <c17825@cray.com>
Cray-bug-id: LUS-8484
Change-Id: I4f8ddeb1808cfaee7507e0efcdefa24040cfcbb6
Reviewed-on: https://review.whamcloud.com/37880
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Artem Blagodarenko <c17828@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -849,12 +849,6 @@ struct ptlrpc_body_v2 {
  * to reserve the flag for future use.
  */
 
-/* The MNE_SWAB flag is overloading the MDS_MDS bit only for the MGS
- * connection.  It is a temporary bug fix for Imperative Recovery interop
- * between 2.2 and 2.3 x86/ppc nodes, and can be removed when interop for
- * 2.2 clients/servers is no longer needed.  LU-1252/LU-1644. */
-#define OBD_CONNECT_MNE_SWAB		 OBD_CONNECT_MDS_MDS
-
 #define OCD_HAS_FLAG(ocd, flg)  \
         (!!((ocd)->ocd_connect_flags & OBD_CONNECT_##flg))
 
@@ -929,7 +923,7 @@ struct ptlrpc_body_v2 {
 
 #define MGS_CONNECT_SUPPORTED  (OBD_CONNECT_VERSION | OBD_CONNECT_AT | \
 				OBD_CONNECT_FULL20 | OBD_CONNECT_IMP_RECOV | \
-				OBD_CONNECT_MNE_SWAB | OBD_CONNECT_PINGLESS |\
+				OBD_CONNECT_PINGLESS |\
 				OBD_CONNECT_BULK_MBITS | OBD_CONNECT_BARRIER)
 
 #define MGS_CONNECT_SUPPORTED2 0

lustre/ldlm/ldlm_lib.c
@@ -1138,8 +1138,8 @@ int target_handle_connect(struct ptlrpc_request *req)
 				OBD_CONNECT_MDS_MDS) != 0;
 
 		/*
-		 * OBD_CONNECT_MNE_SWAB is defined as OBD_CONNECT_MDS_MDS
-		 * for Imperative Recovery connection from MGC to MGS.
+		 * OBD_CONNECT_MNE_SWAB is removed at 2.14
+		 * Checking OBD_CONNECT_FID can be removed in the future.
 		 *
 		 * Via check OBD_CONNECT_FID, we can distinguish whether
 		 * the OBD_CONNECT_MDS_MDS/OBD_CONNECT_MNE_SWAB is from

lustre/mgs/mgs_llog.c
@@ -1513,23 +1513,21 @@ static int only_mgs_is_running(struct obd_device *mgs_obd)
 		/* skip self export */
 		if (exp == mgs_obd->obd_self_export)
 			continue;
-		if (exp_connect_flags(exp) & OBD_CONNECT_MDS_MDS)
-			continue;
 
 		++num_exports;
 
-		CERROR("%s: node %s still connected during replace_nids "
-		       "connect_flags:%llx\n",
-		       mgs_obd->obd_name,
-		       libcfs_nid2str(exp->exp_nid_stats->nid),
-		       exp_connect_flags(exp));
-
+		if (num_exports > 1)
+			CERROR("%s: node %s still connected during replace_nids connect_flags:%llx\n",
+			       mgs_obd->obd_name,
+			       libcfs_nid2str(exp->exp_nid_stats->nid),
+			       exp_connect_flags(exp));
 	}
 	spin_unlock(&mgs_obd->obd_dev_lock);
 
-	/* osd, MGS and MGC + self_export
-	   (wc -l /proc/fs/lustre/devices <= 2) && (non self exports == 0) */
-	return (num_devices <= 3) && (num_exports == 0);
+	/* osd, MGS and MGC + MGC export (nosvc starts MGC)
+	 *  (wc -l /proc/fs/lustre/devices <= 3) && (non self exports == 1)
+	 */
+	return (num_devices <= 3) && (num_exports <= 1);
 }
 
 static int name_create_mdt(char **logname, char *fsname, int mdt_idx)

lustre/obdclass/obd_mount.c
@@ -469,10 +469,6 @@ int lustre_start_mgc(struct super_block *sb)
 				  OBD_CONNECT_LVB_TYPE |
 				  OBD_CONNECT_BULK_MBITS | OBD_CONNECT_BARRIER;
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 0, 53, 0)
-	data->ocd_connect_flags |= OBD_CONNECT_MNE_SWAB;
-#endif
-
 	if (lmd_is_client(lsi->lsi_lmd) &&
 	    lsi->lsi_lmd->lmd_flags & LMD_FLG_NOIR)
 		data->ocd_connect_flags &= ~OBD_CONNECT_IMP_RECOV;