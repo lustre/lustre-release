lustre/lustre-release

commit 8bd04b4e57663e02e9384727eca4df84d99edb3c

LU-12722 target: disable recovery for local clients
when client is running on a server node, then the local
services can't rely on that client in the contex of
recovery - such a client dies with the node, can't replay
requests and states and then the restarting server has to
wait till recovery expires which doesn't make any sense.

so the servers should recogize local clients and exclude
them from recovery (i.e. don't make them part of last_rcvd).

for the purpose of local testing a special mount option
"local_recov" has been added to {MDS|OST}_MOUNT_OPTS in
tests/cfg/local.sh to save local testing when everyting
is running within a single node.

Signed-off-by: Alexey Zhuravlev <bzzz@whamcloud.com>
Change-Id: I4cb906c44c1192933f7d77dc782160e426e9efde
Reviewed-on: https://review.whamcloud.com/36025
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/target/tgt_lastrcvd.c
@@ -944,6 +944,25 @@ static int tgt_last_commit_cb_add(struct thandle *th, struct lu_target *tgt,
 	return rc ? rc : exp->exp_need_sync;
 }
 
+static int tgt_is_local_client(const struct lu_env *env,
+				      struct obd_export *exp)
+{
+	struct lu_target	*tgt = class_exp2tgt(exp);
+	struct tgt_session_info *tsi = tgt_ses_info(env);
+	struct ptlrpc_request	*req = tgt_ses_req(tsi);
+
+	if (tgt->lut_local_recovery)
+		return 0;
+	if (!req)
+		return 0;
+	if (!LNetIsPeerLocal(req->rq_peer.nid))
+		return 0;
+	if (exp_connect_flags(exp) & OBD_CONNECT_MDS)
+		return 0;
+
+	return 1;
+}
+
 /**
  * Add new client to the last_rcvd upon new connection.
  *
@@ -964,6 +983,13 @@ int tgt_client_new(const struct lu_env *env, struct obd_export *exp)
 
 	if (exp_connect_flags(exp) & OBD_CONNECT_LIGHTWEIGHT)
 		RETURN(0);
+
+	if (tgt_is_local_client(env, exp)) {
+		LCONSOLE_WARN("%s: local client %s w/o recovery\n",
+			      exp->exp_obd->obd_name, ted->ted_lcd->lcd_uuid);
+		exp->exp_no_recovery = 1;
+		RETURN(0);
+	}
 
 	/* the bitmap operations can handle cl_idx > sizeof(long) * 8, so
 	 * there's no need for extra complication here
@@ -1092,7 +1118,8 @@ int tgt_client_del(const struct lu_env *env, struct obd_export *exp)
 	/* XXX if lcd_uuid were a real obd_uuid, I could use obd_uuid_equals */
 	if (!strcmp((char *)ted->ted_lcd->lcd_uuid,
 		    (char *)tgt->lut_obd->obd_uuid.uuid) ||
-	    exp_connect_flags(exp) & OBD_CONNECT_LIGHTWEIGHT)
+	    exp_connect_flags(exp) & OBD_CONNECT_LIGHTWEIGHT ||
+	    exp->exp_no_recovery)
 		RETURN(0);
 
 	/* Slot may be not yet assigned, use case is race between Client

lustre/include/lu_target.h
@@ -171,7 +171,9 @@ struct lu_target {
 	unsigned int		 lut_syncjournal:1,
 				 lut_sync_lock_cancel:2,
 				 /* e.g. OST node */
-				 lut_no_reconstruct:1;
+				 lut_no_reconstruct:1,
+				 /* enforce recovery for local clients */
+				 lut_local_recovery:1;
 	/** last_rcvd file */
 	struct dt_object	*lut_last_rcvd;
 	/* transaction callbacks */

lustre/include/lustre_disk.h
@@ -107,6 +107,7 @@ struct lustre_mount_data {
 #define LMD_FLG_HSM		0x4000	/* Start coordinator */
 #define LMD_FLG_DEV_RDONLY	0x8000	/* discard modification quitely */
 #define LMD_FLG_NO_PRECREATE	0x10000	/* do not allow OST object creation */
+#define LMD_FLG_LOCAL_RECOV	0x20000 /* force recovery for local clients */
 
 #define lmd_is_client(x) ((x)->lmd_flags & LMD_FLG_CLIENT)
 

lustre/include/lustre_export.h
@@ -277,7 +277,9 @@ struct obd_export {
 				exp_need_mne_swab:1,
 				/* The export already got final replay ping
 				 * request. */
-				exp_replay_done:1;
+				exp_replay_done:1,
+				/* local client with recovery disabled */
+				exp_no_recovery:1;
 	/* also protected by exp_lock */
 	enum lustre_sec_part	exp_sp_peer;
 	struct sptlrpc_flavor	exp_flvr;		/* current */

lustre/mdt/mdt_handler.c
@@ -5413,12 +5413,12 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 		RETURN(-EFAULT);
 	} else {
 		lsi = s2lsi(lmi->lmi_sb);
+		LASSERT(lsi->lsi_lmd);
 		/* CMD is supported only in IAM mode */
 		LASSERT(num);
 		node_id = simple_strtol(num, NULL, 10);
 		obd->u.obt.obt_magic = OBT_MAGIC;
-		if (lsi->lsi_lmd != NULL &&
-		    lsi->lsi_lmd->lmd_flags & LMD_FLG_SKIP_LFSCK)
+		if (lsi->lsi_lmd->lmd_flags & LMD_FLG_SKIP_LFSCK)
 			m->mdt_skip_lfsck = 1;
 	}
 
@@ -5594,6 +5594,9 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	 * value appropriately. */
 	if (ldlm_timeout == LDLM_TIMEOUT_DEFAULT)
 		ldlm_timeout = MDS_LDLM_TIMEOUT_DEFAULT;
+
+	if ((lsi->lsi_lmd->lmd_flags & LMD_FLG_LOCAL_RECOV))
+		m->mdt_lut.lut_local_recovery = 1;
 
 	RETURN(0);
 err_procfs:

lustre/mdt/mdt_lproc.c
@@ -531,6 +531,34 @@ static ssize_t commit_on_sharing_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(commit_on_sharing);
 
+static ssize_t local_recovery_show(struct kobject *kobj,
+				      struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n",
+			 obd->u.obt.obt_lut->lut_local_recovery);
+}
+
+static ssize_t local_recovery_store(struct kobject *kobj,
+				       struct attribute *attr,
+				       const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	bool val;
+	int rc;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	obd->u.obt.obt_lut->lut_local_recovery = !!val;
+	return count;
+}
+LUSTRE_RW_ATTR(local_recovery);
+
 static int mdt_root_squash_seq_show(struct seq_file *m, void *data)
 {
 	struct obd_device *obd = m->private;
@@ -1085,6 +1113,7 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_enable_dir_migration.attr,
 	&lustre_attr_enable_remote_rename.attr,
 	&lustre_attr_commit_on_sharing.attr,
+	&lustre_attr_local_recovery.attr,
 	&lustre_attr_async_commit_count.attr,
 	&lustre_attr_sync_count.attr,
 	&lustre_attr_dom_lock.attr,

lustre/obdclass/lprocfs_status_server.c
@@ -167,6 +167,7 @@ static int obd_export_flags2str(struct obd_export *exp, struct seq_file *m)
 	flag2str(exp, in_recovery);
 	flag2str(exp, disconnected);
 	flag2str(exp, connecting);
+	flag2str(exp, no_recovery);
 
 	return 0;
 }

lustre/obdclass/obd_mount.c
@@ -1454,6 +1454,9 @@ static int lmd_parse(char *options, struct lustre_mount_data *lmd)
 			strlcat(lmd->lmd_params, " ", LMD_PARAMS_MAXLEN);
 			s3 = s1 + 6 + length;
 			clear++;
+		} else if (strncmp(s1, "localrecov", 10) == 0) {
+			lmd->lmd_flags |= LMD_FLG_LOCAL_RECOV;
+			clear++;
 		} else if (strncmp(s1, "osd=", 4) == 0) {
 			rc = lmd_parse_string(&lmd->lmd_osd_type, s1 + 4);
 			if (rc)

commit 6682b74280ed778f8668f942c808eb70ed7bc67f

LU-13402 target: never exclude MDT/OST from last_rcvd
as it's supposed to be a mandatory part of recovery process

Fixes: 8bd04b4e5766 ("LU-12722 target: disable recovery for local clients")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I5a41c500df04dc9eeecd3b94f69e4d2722e63410
Reviewed-on: https://review.whamcloud.com/38138
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/target/tgt_lastrcvd.c
@@ -951,13 +951,14 @@ static int tgt_is_local_client(const struct lu_env *env,
 	struct tgt_session_info *tsi = tgt_ses_info(env);
 	struct ptlrpc_request	*req = tgt_ses_req(tsi);
 
+	if (exp_connect_flags(exp) & OBD_CONNECT_MDS ||
+	    exp_connect_flags(exp) & OBD_CONNECT_MDS_MDS)
+		return 0;
 	if (tgt->lut_local_recovery)
 		return 0;
 	if (!req)
 		return 0;
 	if (!LNetIsPeerLocal(req->rq_peer.nid))
-		return 0;
-	if (exp_connect_flags(exp) & OBD_CONNECT_MDS)
 		return 0;
 
 	return 1;