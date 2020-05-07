lustre/lustre-release

commit 8bd04b4e57663e02e9384727eca4df84d99edb3c

LU-12722 target: disable recovery for local clients
when client is running on a server node, then the local
services can't rely on that client in the contex of
recovery - such a client dies with the node, can't replay
requests and states and then the restarting server has to
wait till recovery expires which doesn't make any sense.

so the servers should recogize local clients and exclude
them from recovery (i.e. don't make them part of last_rcvd).

for the purpose of local testing a special mount option
"local_recov" has been added to {MDS|OST}_MOUNT_OPTS in
tests/cfg/local.sh to save local testing when everyting
is running within a single node.

Signed-off-by: Alexey Zhuravlev <bzzz@whamcloud.com>
Change-Id: I4cb906c44c1192933f7d77dc782160e426e9efde
Reviewed-on: https://review.whamcloud.com/36025
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/lprocfs_status_server.c
@@ -167,6 +167,7 @@ static int obd_export_flags2str(struct obd_export *exp, struct seq_file *m)
 	flag2str(exp, in_recovery);
 	flag2str(exp, disconnected);
 	flag2str(exp, connecting);
+	flag2str(exp, no_recovery);
 
 	return 0;
 }

lustre/tests/recovery-small.sh
@@ -2262,13 +2262,15 @@ test_110k() {
 
 #define OBD_FAIL_FLD_QUERY_REQ 0x1103
 	do_facet mds2 lctl set_param fail_loc=0x1103
-	start mds2 $(mdsdevname 2) -o abort_recovery ||
+	local OPTS="$MDS_MOUNT_OPTS -o abort_recovery"
+	start mds2 $(mdsdevname 2) $OPTS ||
 		error "start MDS with abort_recovery should succeed"
 	do_facet mds2 lctl set_param fail_loc=0
 
 	# cleanup
 	stop mds2 || error "cleanup: stop mds2 failed"
-	start mds2 $(mdsdevname 2) || error "cleanup: start mds2 failed"
+	start mds2 $(mdsdevname 2) $MDS_MOUNT_OPTS ||
+		error "cleanup: start mds2 failed"
 	zconf_mount $(hostname) $MOUNT || error "cleanup: mount failed"
 	client_up || error "post-failover df failed"
 }
@@ -2283,10 +2285,10 @@ test_111 ()
 #define OBD_FAIL_MDS_CHANGELOG_INIT 0x151
 	do_facet $SINGLEMDS lctl set_param fail_loc=0x151
 	stop $SINGLEMDS || error "stop MDS failed"
-	start $SINGLEMDS $(mdsdevname ${SINGLEMDS//mds/}) &&
+	start $SINGLEMDS $(mdsdevname ${SINGLEMDS//mds/}) $MDS_MOUNT_OPTS &&
 		error "start MDS should fail"
 	do_facet $SINGLEMDS lctl set_param fail_loc=0
-	start $SINGLEMDS $(mdsdevname ${SINGLEMDS//mds/}) ||
+	start $SINGLEMDS $(mdsdevname ${SINGLEMDS//mds/}) $MDS_MOUNT_OPTS ||
 		error "start MDS failed"
 }
 run_test 111 "mdd setup fail should not cause umount oops"
@@ -2912,6 +2914,63 @@ test_139() {
 	start $SINGLEMDS $mdt_dev $MDS_MOUNT_OPTS || error "Fail to start MDT"
 }
 run_test 139 "corrupted catid won't cause crash"
+
+test_140a() {
+	[ $MDS1_VERSION -lt $(version_code 2.12.58) ] &&
+		skip "Need MDS version at least 2.13.50"
+
+	slr=$(do_facet mds1 \
+		$LCTL get_param -n mdt.$FSNAME-MDT0000.local_recovery)
+	stack_trap "do_facet mds1 $LCTL set_param \
+		mdt.*.local_recovery=$slr" EXIT
+
+	# disable recovery for local clients
+	# so local clients should be marked with no_recovery flag
+	do_facet mds1 $LCTL set_param mdt.*.local_recovery=0
+	mount_mds_client
+
+	local cnt
+	cnt=$(do_facet mds1 $LCTL get_param "mdt.*.exports.*.export" |
+		grep export_flags.*no_recovery | wc -l)
+	echo "$cnt clients with recovery disabled"
+	umount_mds_client
+	[ $cnt -eq 0 ] && error "no clients with recovery disabled"
+
+	# enable recovery for local clients
+	# so no local clients should be marked with no_recovery flag
+	do_facet mds1 $LCTL set_param mdt.*.local_recovery=1
+	mount_mds_client
+
+	cnt=$(do_facet mds1 $LCTL get_param "mdt.*.exports.*.export" |
+		grep export_flags.*no_recovery | wc -l)
+	echo "$cnt clients with recovery disabled"
+	umount_mds_client
+	[ $cnt -eq 0 ] || error "$cnt clients with recovery disabled"
+}
+run_test 140a "local mount is flagged properly"
+
+test_140b() {
+	[ $MDS1_VERSION -lt $(version_code 2.12.58) ] &&
+		skip "Need MDS version at least 2.13.50"
+
+	slr=$(do_facet mds1 \
+		$LCTL get_param -n mdt.$FSNAME-MDT0000.local_recovery)
+	stack_trap "do_facet mds1 $LCTL set_param \
+		mdt.*.local_recovery=$slr" EXIT
+
+	# disable recovery for local clients
+	do_facet mds1 $LCTL set_param mdt.*.local_recovery=0
+
+	mount_mds_client
+	replay_barrier mds1
+	umount_mds_client
+	local before=$SECONDS
+	fail mds1
+	local after=$SECONDS
+	(( $after-$before < $TIMEOUT*2 )) ||
+		error "recovery took too long" $((after-bsfore)) $TIMEOUT
+}
+run_test 140b "local mount is excluded from recovery"
 
 complete $SECONDS
 check_and_cleanup_lustre

lustre/tests/test-framework.sh
@@ -2331,6 +2331,22 @@ zconf_umount() {
 	fi
 }
 
+# Mount the file system on the MDS
+mount_mds_client() {
+	local mds_HOST=${SINGLEMDS}_HOST
+	echo $mds_HOST
+	do_facet $SINGLEMDS "mkdir -p $MOUNT2"
+	zconf_mount $mds1_HOST $MOUNT2 $MOUNT_OPTS ||
+		error "unable to mount $MOUNT2 on MDS"
+}
+
+# Unmount the file system on the MDS
+umount_mds_client() {
+	local mds_HOST=${SINGLEMDS}_HOST
+	zconf_umount $mds1_HOST $MOUNT2
+	do_facet $SINGLEMDS "rm -rf $MOUNT2"
+}
+
 # nodes is comma list
 sanity_mount_check_nodes () {
     local nodes=$1
@@ -8559,7 +8575,7 @@ mds_backup_restore() {
 	local rcmd="do_facet $facet"
 	local metaea=${TMP}/backup_restore.ea
 	local metadata=${TMP}/backup_restore.tgz
-	local opts=${MDS_MOUNT_OPTS}
+	local opts=${MDS_MOUNT_FS_OPTS}
 	local svc=${facet}_svc
 
 	if ! ${rcmd} test -b ${devname}; then
@@ -8616,7 +8632,7 @@ mds_remove_ois() {
 	local devname=$(mdsdevname $(facet_number $facet))
 	local mntpt=$(facet_mntpt brpt)
 	local rcmd="do_facet $facet"
-	local opts=${MDS_MOUNT_OPTS}
+	local opts=${MDS_MOUNT_FS_OPTS}
 
 	if ! ${rcmd} test -b ${devname}; then
 		opts=$(csa_add "$opts" -o loop)

lustre/include/lu_target.h
@@ -171,7 +171,9 @@ struct lu_target {
 	unsigned int		 lut_syncjournal:1,
 				 lut_sync_lock_cancel:2,
 				 /* e.g. OST node */
-				 lut_no_reconstruct:1;
+				 lut_no_reconstruct:1,
+				 /* enforce recovery for local clients */
+				 lut_local_recovery:1;
 	/** last_rcvd file */
 	struct dt_object	*lut_last_rcvd;
 	/* transaction callbacks */

lustre/include/lustre_disk.h
@@ -107,6 +107,7 @@ struct lustre_mount_data {
 #define LMD_FLG_HSM		0x4000	/* Start coordinator */
 #define LMD_FLG_DEV_RDONLY	0x8000	/* discard modification quitely */
 #define LMD_FLG_NO_PRECREATE	0x10000	/* do not allow OST object creation */
+#define LMD_FLG_LOCAL_RECOV	0x20000 /* force recovery for local clients */
 
 #define lmd_is_client(x) ((x)->lmd_flags & LMD_FLG_CLIENT)
 

lustre/include/lustre_export.h
@@ -277,7 +277,9 @@ struct obd_export {
 				exp_need_mne_swab:1,
 				/* The export already got final replay ping
 				 * request. */
-				exp_replay_done:1;
+				exp_replay_done:1,
+				/* local client with recovery disabled */
+				exp_no_recovery:1;
 	/* also protected by exp_lock */
 	enum lustre_sec_part	exp_sp_peer;
 	struct sptlrpc_flavor	exp_flvr;		/* current */

lustre/mdt/mdt_handler.c
@@ -5413,12 +5413,12 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 		RETURN(-EFAULT);
 	} else {
 		lsi = s2lsi(lmi->lmi_sb);
+		LASSERT(lsi->lsi_lmd);
 		/* CMD is supported only in IAM mode */
 		LASSERT(num);
 		node_id = simple_strtol(num, NULL, 10);
 		obd->u.obt.obt_magic = OBT_MAGIC;
-		if (lsi->lsi_lmd != NULL &&
-		    lsi->lsi_lmd->lmd_flags & LMD_FLG_SKIP_LFSCK)
+		if (lsi->lsi_lmd->lmd_flags & LMD_FLG_SKIP_LFSCK)
 			m->mdt_skip_lfsck = 1;
 	}
 
@@ -5594,6 +5594,9 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	 * value appropriately. */
 	if (ldlm_timeout == LDLM_TIMEOUT_DEFAULT)
 		ldlm_timeout = MDS_LDLM_TIMEOUT_DEFAULT;
+
+	if ((lsi->lsi_lmd->lmd_flags & LMD_FLG_LOCAL_RECOV))
+		m->mdt_lut.lut_local_recovery = 1;
 
 	RETURN(0);
 err_procfs:

lustre/mdt/mdt_lproc.c
@@ -531,6 +531,34 @@ static ssize_t commit_on_sharing_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(commit_on_sharing);
 
+static ssize_t local_recovery_show(struct kobject *kobj,
+				      struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n",
+			 obd->u.obt.obt_lut->lut_local_recovery);
+}
+
+static ssize_t local_recovery_store(struct kobject *kobj,
+				       struct attribute *attr,
+				       const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	bool val;
+	int rc;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	obd->u.obt.obt_lut->lut_local_recovery = !!val;
+	return count;
+}
+LUSTRE_RW_ATTR(local_recovery);
+
 static int mdt_root_squash_seq_show(struct seq_file *m, void *data)
 {
 	struct obd_device *obd = m->private;
@@ -1085,6 +1113,7 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_enable_dir_migration.attr,
 	&lustre_attr_enable_remote_rename.attr,
 	&lustre_attr_commit_on_sharing.attr,
+	&lustre_attr_local_recovery.attr,
 	&lustre_attr_async_commit_count.attr,
 	&lustre_attr_sync_count.attr,
 	&lustre_attr_dom_lock.attr,

commit 814bb394843434883a94fe6432cd8c656035a3e1

LU-13449 tgt: fix recovery timer comparisons
The obd_recovery_start time was changed to use a monotonic kernel
clock via ktime_get_seconds(), which is a relative kernel time in
newer kernels.  However, it was still being compared with wallclock
time in some places, which lead to incorrect calculations.

Always use the monotonic clock within the kernel, but convert the
times to wallclock time when they are printed to userspace.

Fix the added recovery-small test_140b to use the actual recovery time
rather than the entire failover time, since that may increased too
much by operations unrelated to the MDS recovery.

Fixes: 8bd04b4e5766 ("LU-12722 target: disable recovery for local clients")
Fixes: 06408a4ef381 ("LU-12769 recovery: use monotonic timer")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ibe866463cbad81010e91f630a1088990f8a48664
Reviewed-on: https://review.whamcloud.com/38366
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lib.c
@@ -2766,7 +2766,7 @@ static enum hrtimer_restart target_recovery_expired(struct hrtimer *timer)
 	CDEBUG(D_HA,
 	       "%s: recovery timed out; %d clients are still in recovery after %llu seconds (%d clients connected)\n",
 	       obd->obd_name, atomic_read(&obd->obd_lock_replay_clients),
-	       ktime_get_real_seconds() - obd->obd_recovery_start,
+	       ktime_get_seconds() - obd->obd_recovery_start,
 	       atomic_read(&obd->obd_connected_clients));
 
 	obd->obd_recovery_expired = 1;

lustre/obdclass/lprocfs_status_server.c
@@ -716,11 +716,12 @@ int lprocfs_recovery_status_seq_show(struct seq_file *m, void *data)
 	if (obd->obd_recovering == 0) {
 		seq_printf(m, "COMPLETE\n");
 		seq_printf(m, "recovery_start: %lld\n",
-			   (s64)obd->obd_recovery_start);
+			   (s64)ktime_get_real_seconds() -
+			   (ktime_get_seconds() - obd->obd_recovery_start));
 		seq_printf(m, "recovery_duration: %lld\n",
 			   obd->obd_recovery_end ?
 			   obd->obd_recovery_end - obd->obd_recovery_start :
-			   ktime_get_real_seconds() - obd->obd_recovery_start);
+			   ktime_get_seconds() - obd->obd_recovery_start);
 		/* Number of clients that have completed recovery */
 		seq_printf(m, "completed_clients: %d/%d\n",
 			   atomic_read(&obd->obd_max_recoverable_clients) -
@@ -751,9 +752,11 @@ int lprocfs_recovery_status_seq_show(struct seq_file *m, void *data)
 			seq_printf(m, "non-ready MDTs: %s\n",
 				   buf ? buf : "unknown (not enough RAM)");
 			seq_printf(m, "recovery_start: %lld\n",
-				   (s64)obd->obd_recovery_start);
+				   (s64)ktime_get_real_seconds() -
+				   (ktime_get_seconds() -
+				    obd->obd_recovery_start));
 			seq_printf(m, "time_waited: %lld\n",
-				   (s64)(ktime_get_real_seconds() -
+				   (s64)(ktime_get_seconds() -
 					 obd->obd_recovery_start));
 		}
 
@@ -771,14 +774,15 @@ int lprocfs_recovery_status_seq_show(struct seq_file *m, void *data)
 	}
 
 	seq_printf(m, "RECOVERING\n");
-	seq_printf(m, "recovery_start: %lld\n", (s64)obd->obd_recovery_start);
+	seq_printf(m, "recovery_start: %lld\n", (s64)ktime_get_real_seconds() -
+		   (ktime_get_seconds() - obd->obd_recovery_start));
 	seq_printf(m, "time_remaining: %lld\n",
-		   ktime_get_real_seconds() >=
+		   ktime_get_seconds() >=
 		   obd->obd_recovery_start +
 		   obd->obd_recovery_timeout ? 0 :
 		   (s64)(obd->obd_recovery_start +
 			 obd->obd_recovery_timeout -
-			 ktime_get_real_seconds()));
+			 ktime_get_seconds()));
 	seq_printf(m, "connected_clients: %d/%d\n",
 		   atomic_read(&obd->obd_connected_clients),
 		   atomic_read(&obd->obd_max_recoverable_clients));

lustre/tests/recovery-small.sh
@@ -2970,11 +2970,12 @@ test_140b() {
 	mount_mds_client
 	replay_barrier mds1
 	umount_mds_client
-	local before=$SECONDS
 	fail mds1
-	local after=$SECONDS
-	(( $after-$before < $TIMEOUT*2 )) ||
-		error "recovery took too long" $((after-bsfore)) $TIMEOUT
+	local recovery=$(do_facet mds1 dmesg |
+			 awk -F: '/Recovery over after/ { print $4 }' |
+			 cut -d, -f1 | tail -1)
+	(( $recovery < $TIMEOUT*2 )) ||
+		error "recovery took too long $recovery > $((TIMEOUT * 2))"
 }
 run_test 140b "local mount is excluded from recovery"
 

lustre/tests/test-framework.sh
@@ -2361,7 +2361,6 @@ zconf_umount() {
 mount_mds_client() {
 	local mds_HOST=${SINGLEMDS}_HOST
 	echo $mds_HOST
-	do_facet $SINGLEMDS "mkdir -p $MOUNT2"
 	zconf_mount $mds1_HOST $MOUNT2 $MOUNT_OPTS ||
 		error "unable to mount $MOUNT2 on MDS"
 }
@@ -2370,7 +2369,7 @@ mount_mds_client() {
 umount_mds_client() {
 	local mds_HOST=${SINGLEMDS}_HOST
 	zconf_umount $mds1_HOST $MOUNT2
-	do_facet $SINGLEMDS "rm -rf $MOUNT2"
+	do_facet $SINGLEMDS "rmdir $MOUNT2"
 }
 
 # nodes is comma list