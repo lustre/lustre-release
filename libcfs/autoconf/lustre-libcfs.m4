lustre/lustre-release

commit 742897a967cff5be53c447d14b17ae405c2b31f2

LU-13274 uapi: make lnet UAPI headers C99 compliant
Attempting to compile strict C99 user land applications or
libraries with the Lustre UAPI headers will fail. These same
errors can be seen by enabling CONFIG_UAPI_HEADER_TEST as well.
Update the LNet UAPI headers to be compilable with -std=c99.
Add LNet test covering UAPI header handling.

Test-Parameters: trivial testlist=sanity-lnet

Change-Id: Ic1c30ed2d4d9d1d7f11b657d3817a05161367e13
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37971
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -5,7 +5,6 @@
 #
 AC_DEFUN([LC_CONFIG_SRCDIR], [
 AC_CONFIG_SRCDIR([lustre/obdclass/obdo.c])
-libcfs_is_module="yes"
 ldiskfs_is_ext4="yes"
 ])
 

lnet/utils/Makefile.am
@@ -44,6 +44,7 @@ routerstat_SOURCES = routerstat.c
 routerstat_LDADD = $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la
 
 lst_SOURCES = lst.c
+lst_CFLAGS = -fPIC -D_LINUX_TIME_H -D_GNU_SOURCE
 lst_LDADD = $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la \
 	    $(LIBEFENCE)
 

lnet/utils/lnetconfig/Makefile.am
@@ -29,10 +29,9 @@
 lib_LTLIBRARIES = liblnetconfig.la
 
 liblnetconfig_la_SOURCES  = liblnetconfig.c liblnetconfig.h \
-			    liblnetconfig_lnd.c liblnd.h cyaml.c \
-			    $(top_builddir)/lnet/include/cyaml.h
+			    liblnetconfig_lnd.c liblnd.h cyaml.c cyaml.h
 liblnetconfig_la_CPPFLAGS = -D_LARGEFILE64_SOURCE=1 -D_FILE_OFFSET_BITS=64 \
-			    -DLUSTRE_UTILS=1 -I$(top_builddir)/lnet/include
+			    -DLUSTRE_UTILS=1 -fPIC
 liblnetconfig_la_LDFLAGS = -L$(top_builddir)/libcfs/libcfs -lyaml -lm \
 			   $(LIBREADLINE) -version-info 4:0:0
 liblnetconfig_la_LIBADD = $(top_builddir)/libcfs/libcfs/libcfs.la

lustre/utils/Makefile.am
@@ -67,6 +67,7 @@ lctl_LDADD :=  liblustreapi.la $(PTHREAD_LIBS) -lyaml
 lctl_DEPENDENCIES := liblustreapi.la
 
 lfs_SOURCES = lfs.c lfs_project.c lfs_project.h
+lfs_CFLAGS := -fPIC -I $(top_builddir)/lnet/utils
 lfs_LDADD := liblustreapi.la -lz
 lfs_LDADD += $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la
 lfs_DEPENDENCIES := liblustreapi.la

Makefile.in
@@ -1,4 +1,4 @@
-obj-m += @LDISKFS_SUBDIR@/ @LIBCFS_SUBDIR@/ lnet/
+obj-m += @LDISKFS_SUBDIR@/ libcfs/ lnet/
 obj-m += lustre/
 
 @INCLUDE_RULES@

autoMakefile.am
@@ -1,15 +1,15 @@
 SUBDIRS := @LDISKFS_SUBDIR@ \
 	. \
 	@LUSTREIOKIT_SUBDIR@ \
-	@LIBCFS_SUBDIR@ \
 	@SNMP_SUBDIR@ \
+	libcfs \
 	lnet \
 	lustre
 
 DIST_SUBDIRS := ldiskfs \
 	lustre-iokit \
-	@LIBCFS_SUBDIR@ \
 	@SNMP_DIST_SUBDIR@ \
+	libcfs \
 	lnet \
 	lustre \
 	config

config/lustre-build.m4
@@ -103,19 +103,6 @@ AC_SUBST(LUSTREIOKIT_SUBDIR)
 AM_CONDITIONAL([BUILD_LUSTREIOKIT], [test "x$enable_iokit" = xyes])
 ]) # LB_PATH_LUSTREIOKIT
 
-# Define no libcfs by default.
-AC_DEFUN([LB_LIBCFS_DIR], [
-AS_IF([test "x$libcfs_is_module" = xyes], [
-		LIBCFS_INCLUDE_DIR="libcfs/include"
-		LIBCFS_SUBDIR="libcfs"
-	], [
-		LIBCFS_INCLUDE_DIR="lnet/include"
-		LIBCFS_SUBDIR=""
-	])
-AC_SUBST(LIBCFS_INCLUDE_DIR)
-AC_SUBST(LIBCFS_SUBDIR)
-]) # LB_LIBCFS_DIR
-
 #
 # LB_LIBMOUNT
 #
@@ -329,13 +316,14 @@ AS_IF([test $target_cpu = powerpc64], [
 	CC="$CC -m64"
 ])
 
-CPPFLAGS="-I$PWD/$LIBCFS_INCLUDE_DIR -I$PWD/lnet/include -I$PWD/lnet/include/uapi -I$PWD/lustre/include -I$PWD/lustre/include/uapi $CPPFLAGS"
+# UAPI headers, libcfs/include for util headers, lustre/include for liblustreapi and friends
+CPPFLAGS="-I$PWD/libcfs/include -I$PWD/lnet/include/uapi -I$PWD/lustre/include -I$PWD/lustre/include/uapi $CPPFLAGS"
 
 CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
 
-# everyone builds against lnet and lustre
-EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/$LIBCFS_INCLUDE_DIR -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include"
+# everyone builds against lnet and lustre kernel headers
+EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include"
 AC_SUBST(EXTRA_KCFLAGS)
 ]) # LB_PROG_CC
 
@@ -608,8 +596,6 @@ LB_CONFIG_DIST
 
 LB_DOWNSTREAM_RELEASE
 LB_USES_DPKG
-
-LB_LIBCFS_DIR
 
 LB_INCLUDE_RULES
 

libcfs/include/libcfs/util/ioctl.h
@@ -43,7 +43,7 @@
 /* Sparse annotation. */
 #define __user
 
-#include <uapi/linux/lnet/libcfs_ioctl.h>
+#include <linux/lnet/libcfs_ioctl.h>
 
 #define LIBCFS_IOC_INIT(data)					\
 do {								\

commit 6e1c48c4b7224f28b2aebcb372dc1156f09268f6

LU-9859 libcfs: handle missing refcount.h
Recently the OpenSFS tree was modified to make backported headers to
appear like native Linux kernel headers to avoid ifdef handling. One
missed is refcount.h. Rename linux-refcount.h to refcount.h to make
it look like the native kernel version. Change EXTRA_KCFLAGS to not
include the Lustre include directories since it contains the fake
kernel headers in the autoconf test. Also remove stray
LC_REPLACE_EXCEPTIONAL_ENTRIES.

Change-Id: Ia5de6dab0837db419a6b02b3b2a6ddeb6bb62f03
Test-Parameters: trivial
Fixes: 742897a967cf ("LU-13274 uapi: make lnet UAPI headers C99 compliant")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/38390
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/autoconf/lustre-libcfs.m4
@@ -733,9 +733,13 @@ cpu_hotplug_state_machine, [
 # introduced refcount_t which is atomic_t plus over flow guards.
 #
 AC_DEFUN([LIBCFS_REFCOUNT_T], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
 LB_CHECK_LINUX_HEADER([linux/refcount.h], [
 	AC_DEFINE(HAVE_REFCOUNT_T, 1,
-		[refcount_t is supported])])
+		[refcount_t is supported])
+])
+EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_REFCOUNT_T
 
 #
@@ -839,9 +843,13 @@ uuid_t, [
 # introduce processor.h
 #
 AC_DEFUN([LIBCFS_HAVE_PROCESSOR_HEADER], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
 LB_CHECK_LINUX_HEADER([linux/processor.h], [
 	AC_DEFINE(HAVE_PROCESSOR_H, 1,
-		[processor.h is present])])
+		[processor.h is present])
+])
+EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_HAVE_PROCESSOR_HEADER
 
 #

libcfs/include/libcfs/linux/Makefile.am
@@ -1,3 +1,3 @@
 EXTRA_DIST = linux-misc.h linux-fs.h linux-mem.h linux-time.h linux-cpu.h \
 	     linux-list.h linux-hash.h linux-uuid.h linux-wait.h \
-	     linux-refcount.h processor.h
+	     refcount.h processor.h

lustre/autoconf/lustre-core.m4
@@ -2349,7 +2349,6 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC___XA_SET_MARK
 	LC_UAPI_LINUX_MOUNT_H
 	LC_HAVE_SUNRPC_CACHE_HASH_LOCK_IS_A_SPINLOCK
-	LC_REPLACE_EXCEPTIONAL_ENTRIES
 
 	# 5.1
 	LC_HAS_LINUX_SELINUX_ENABLED