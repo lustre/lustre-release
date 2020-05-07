lustre/lustre-release

commit 23773b32bfe15fccc9c029d722dc1bd5b0144779

LU-11444 ptlrpc: resend may corrupt the data
Late resend if arrives much later than another modification RPC
which has been already handled on this slot, may be still applied
and therefore overrides the last one

Send RPCs from client in increasing order for each tag
and check it on server to check late resend.

A slot can be reused by a client after kill while
the server continue to rely on it.

Add flag for such obsolete requests, here we trust the
client and perform xid check for all in progress requests.

Cray-bug-id: LUS-6272, LUS-7277, LUS-7339
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Change-Id: I97806577cf979f49a75379ffc55947cc3dcd02b1
Reviewed-on: https://review.whamcloud.com/35114
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/target/tgt_lastrcvd.c
@@ -1145,8 +1145,30 @@ int tgt_client_del(const struct lu_env *env, struct obd_export *exp)
 }
 EXPORT_SYMBOL(tgt_client_del);
 
+static void tgt_clean_by_tag(struct obd_export *exp, __u64 xid, __u16 tag)
+{
+	struct tg_export_data	*ted = &exp->exp_target_data;
+	struct lu_target	*lut = class_exp2tgt(exp);
+	struct tg_reply_data	*trd, *tmp;
+
+	if (tag == 0)
+		return;
+
+	list_for_each_entry_safe(trd, tmp, &ted->ted_reply_list, trd_list) {
+		if (trd->trd_tag != tag)
+			continue;
+
+		LASSERT(ergo(tgt_is_increasing_xid_client(exp),
+			     trd->trd_reply.lrd_xid <= xid));
+
+		ted->ted_release_tag++;
+		tgt_release_reply_data(lut, ted, trd);
+	}
+}
+
 int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 		       struct tg_export_data *ted, struct tg_reply_data *trd,
+		       struct ptlrpc_request *req,
 		       struct thandle *th, bool update_lrd_file)
 {
 	struct lsd_reply_data	*lrd;
@@ -1183,6 +1205,19 @@ int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 	}
 	/* add reply data to target export's reply list */
 	mutex_lock(&ted->ted_lcd_lock);
+	if (req != NULL) {
+		int exclude = tgt_is_increasing_xid_client(req->rq_export) ?
+			      MSG_REPLAY : MSG_REPLAY|MSG_RESENT;
+
+		if (req->rq_obsolete) {
+			mutex_unlock(&ted->ted_lcd_lock);
+			RETURN(-EALREADY);
+		}
+
+		if (!(lustre_msg_get_flags(req->rq_reqmsg) & exclude))
+			tgt_clean_by_tag(req->rq_export, req->rq_xid,
+					 trd->trd_tag);
+	}
 	list_add(&trd->trd_list, &ted->ted_reply_list);
 	ted->ted_reply_cnt++;
 	if (ted->ted_reply_cnt > ted->ted_reply_max)
@@ -1192,7 +1227,8 @@ int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 	CDEBUG(D_TRACE, "add reply %p: xid %llu, transno %llu, "
 	       "tag %hu, client gen %u, slot idx %d\n",
 	       trd, lrd->lrd_xid, lrd->lrd_transno,
-	       trd->trd_tag, lrd->lrd_client_gen, i);
+	       trd->trd_tag, lrd->lrd_client_gen, trd->trd_index);
+
 	RETURN(0);
 }
 EXPORT_SYMBOL(tgt_add_reply_data);
@@ -1323,7 +1359,8 @@ static int tgt_last_rcvd_update(const struct lu_env *env, struct lu_target *tgt,
 			trd->trd_pre_versions[3] = pre_versions[3];
 		}
 
-		rc = tgt_add_reply_data(env, tgt, ted, trd, th, write_update);
+		rc = tgt_add_reply_data(env, tgt, ted, trd, req,
+					th, write_update);
 		if (rc < 0)
 			OBD_FREE_PTR(trd);
 		return rc;
@@ -2044,43 +2081,70 @@ out:
 	return rc;
 }
 
-struct tg_reply_data *tgt_lookup_reply_by_xid(struct tg_export_data *ted,
-					      __u64 xid)
+static int tgt_check_lookup_req(struct ptlrpc_request *req, int lookup,
+				struct tg_reply_data *trd)
 {
-	struct tg_reply_data	*found = NULL;
-	struct tg_reply_data	*reply;
+	struct tg_export_data *ted = &req->rq_export->exp_target_data;
+	struct lu_target *lut = class_exp2tgt(req->rq_export);
+	__u16 tag = lustre_msg_get_tag(req->rq_reqmsg);
+	int rc = 0;
+	struct tg_reply_data *reply;
+	bool check_increasing;
 
-	mutex_lock(&ted->ted_lcd_lock);
+	if (tag == 0)
+		return 0;
+
+	check_increasing = tgt_is_increasing_xid_client(req->rq_export) &&
+			   !(lustre_msg_get_flags(req->rq_reqmsg) & MSG_REPLAY);
+	if (!lookup && !check_increasing)
+		return 0;
+
 	list_for_each_entry(reply, &ted->ted_reply_list, trd_list) {
-		if (reply->trd_reply.lrd_xid == xid) {
-			found = reply;
+		if (lookup && reply->trd_reply.lrd_xid == req->rq_xid) {
+			rc = 1;
+			if (trd != NULL)
+				*trd = *reply;
+			break;
+		} else if (check_increasing && reply->trd_tag == tag &&
+			   reply->trd_reply.lrd_xid > req->rq_xid) {
+			rc = -EPROTO;
+			CERROR("%s: busy tag=%u req_xid=%llu, trd=%p: xid=%llu transno=%llu client_gen=%u slot_idx=%d: rc = %d\n",
+			       tgt_name(lut), tag, req->rq_xid, trd,
+			       reply->trd_reply.lrd_xid,
+			       reply->trd_reply.lrd_transno,
+			       reply->trd_reply.lrd_client_gen,
+			       reply->trd_index, rc);
 			break;
 		}
 	}
-	mutex_unlock(&ted->ted_lcd_lock);
-	return found;
+
+	return rc;
 }
-EXPORT_SYMBOL(tgt_lookup_reply_by_xid);
 
 /* Look for a reply data matching specified request @req
  * A copy is returned in @trd if the pointer is not NULL
  */
-bool tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd)
+int tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd)
 {
-	struct tg_export_data	*ted = &req->rq_export->exp_target_data;
-	struct tg_reply_data	*reply;
-	bool			 found = false;
+	struct tg_export_data *ted = &req->rq_export->exp_target_data;
+	int found = 0;
+	bool not_replay = !(lustre_msg_get_flags(req->rq_reqmsg) & MSG_REPLAY);
 
-	reply = tgt_lookup_reply_by_xid(ted, req->rq_xid);
-	if (reply != NULL) {
-		found = true;
-		if (trd != NULL)
-			*trd = *reply;
+	mutex_lock(&ted->ted_lcd_lock);
+	if (not_replay && req->rq_xid <= req->rq_export->exp_last_xid) {
+		/* A check for the last_xid is needed here in case there is
+		 * no reply data is left in the list. It may happen if another
+		 * RPC on another slot increased the last_xid between our
+		 * process_req_last_xid & tgt_lookup_reply calls */
+		found = -EPROTO;
+	} else {
+		found = tgt_check_lookup_req(req, 1, trd);
 	}
+	mutex_unlock(&ted->ted_lcd_lock);
 
-	CDEBUG(D_TRACE, "%s: lookup reply xid %llu, found %d\n",
-	       tgt_name(class_exp2tgt(req->rq_export)), req->rq_xid,
-	       found ? 1 : 0);
+	CDEBUG(D_TRACE, "%s: lookup reply xid %llu, found %d last_xid %llu\n",
+	       tgt_name(class_exp2tgt(req->rq_export)), req->rq_xid, found,
+	       req->rq_export->exp_last_xid);
 
 	return found;
 }
@@ -2092,37 +2156,19 @@ int tgt_handle_received_xid(struct obd_export *exp, __u64 rcvd_xid)
 	struct lu_target	*lut = class_exp2tgt(exp);
 	struct tg_reply_data	*trd, *tmp;
 
-	mutex_lock(&ted->ted_lcd_lock);
+
 	list_for_each_entry_safe(trd, tmp, &ted->ted_reply_list, trd_list) {
 		if (trd->trd_reply.lrd_xid > rcvd_xid)
 			continue;
 		ted->ted_release_xid++;
 		tgt_release_reply_data(lut, ted, trd);
 	}
-	mutex_unlock(&ted->ted_lcd_lock);
 
 	return 0;
 }
 
-int tgt_handle_tag(struct obd_export *exp, __u16 tag)
+int tgt_handle_tag(struct ptlrpc_request *req)
 {
-	struct tg_export_data	*ted = &exp->exp_target_data;
-	struct lu_target	*lut = class_exp2tgt(exp);
-	struct tg_reply_data	*trd, *tmp;
-
-	if (tag == 0)
-		return 0;
-
-	mutex_lock(&ted->ted_lcd_lock);
-	list_for_each_entry_safe(trd, tmp, &ted->ted_reply_list, trd_list) {
-		if (trd->trd_tag != tag)
-			continue;
-		ted->ted_release_tag++;
-		tgt_release_reply_data(lut, ted, trd);
-		break;
-	}
-	mutex_unlock(&ted->ted_lcd_lock);
-
-	return 0;
+	return tgt_check_lookup_req(req, 0, NULL);
 }
 

lustre/include/lu_target.h
@@ -455,7 +455,7 @@ void tgt_register_lfsck_query(int (*query)(const struct lu_env *,
 					   struct lfsck_request *,
 					   struct lfsck_reply *,
 					   struct lfsck_query *));
-bool req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
 
 extern struct tgt_handler tgt_sec_ctx_handlers[];
 extern struct tgt_handler tgt_lfsck_handlers[];
@@ -493,9 +493,10 @@ int tgt_client_new(const struct lu_env *env, struct obd_export *exp);
 int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg,
 			   int sync);
 int tgt_reply_data_init(const struct lu_env *env, struct lu_target *tgt);
-bool tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
 int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 		       struct tg_export_data *ted, struct tg_reply_data *trd,
+		       struct ptlrpc_request *req,
 		       struct thandle *th, bool update_lrd_file);
 struct tg_reply_data *tgt_lookup_reply_by_xid(struct tg_export_data *ted,
 					       __u64 xid);

lustre/include/lustre_export.h
@@ -307,7 +307,8 @@ struct obd_export {
 	/** highest XID received by export client that has no
 	 * unreceived lower-numbered XID
 	 */
-	__u64			  exp_last_xid;
+	__u64			exp_last_xid;
+	long			*exp_used_slots;
 };
 
 #define exp_target_data u.eu_target_data

lustre/include/lustre_mdc.h
@@ -73,6 +73,7 @@ static inline void mdc_get_mod_rpc_slot(struct ptlrpc_request *req,
 	opc = lustre_msg_get_opc(req->rq_reqmsg);
 	tag = obd_get_mod_rpc_slot(cli, opc, it);
 	lustre_msg_set_tag(req->rq_reqmsg, tag);
+	ptlrpc_reassign_next_xid(req);
 }
 
 static inline void mdc_put_mod_rpc_slot(struct ptlrpc_request *req,

lustre/include/lustre_net.h
@@ -999,7 +999,8 @@ struct ptlrpc_request {
 	unsigned int
 		rq_hp:1,		/**< high priority RPC */
 		rq_at_linked:1,		/**< link into service's srv_at_array */
-		rq_packed_final:1;	/**< packed final reply */
+		rq_packed_final:1,	/**< packed final reply */
+		rq_obsolete:1;		/* aborted by a signal on a client */
 	/** @} */
 
 	/** one of RQ_PHASE_* */
@@ -2169,6 +2170,7 @@ void ptlrpc_retain_replayable_request(struct ptlrpc_request *req,
 __u64 ptlrpc_next_xid(void);
 __u64 ptlrpc_sample_next_xid(void);
 __u64 ptlrpc_req_xid(struct ptlrpc_request *request);
+void ptlrpc_reassign_next_xid(struct ptlrpc_request *req);
 
 /* Set of routines to run a function in ptlrpcd context */
 void *ptlrpcd_alloc_work(struct obd_import *imp,

lustre/include/obd_support.h
@@ -450,6 +450,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_PTLRPC_LONG_BOTH_UNLINK 0x51c
 #define OBD_FAIL_PTLRPC_CLIENT_BULK_CB3  0x520
 #define OBD_FAIL_PTLRPC_BULK_ATTACH      0x521
+#define OBD_FAIL_PTLRPC_RESEND_RACE	 0x525
 #define OBD_FAIL_PTLRPC_ROUND_XID	 0x530
 #define OBD_FAIL_PTLRPC_CONNECT_RACE	 0x531
 

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -896,6 +896,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_FLR |\
 				OBD_CONNECT2_LOCK_CONVERT | \
 				OBD_CONNECT2_ARCHIVE_ID_ARRAY | \
+				OBD_CONNECT2_INC_XID | \
 				OBD_CONNECT2_SELINUX_POLICY | \
 				OBD_CONNECT2_LSOM | \
 				OBD_CONNECT2_ASYNC_DISCARD | \
@@ -920,7 +921,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT_GRANT_PARAM | \
 				OBD_CONNECT_SHORTIO | OBD_CONNECT_FLAGS2)
 
-#define OST_CONNECT_SUPPORTED2 OBD_CONNECT2_LOCKAHEAD
+#define OST_CONNECT_SUPPORTED2 (OBD_CONNECT2_LOCKAHEAD | OBD_CONNECT2_INC_XID)
 
 #define ECHO_CONNECT_SUPPORTED (OBD_CONNECT_FID)
 #define ECHO_CONNECT_SUPPORTED2 0

lustre/llite/llite_lib.c
@@ -255,6 +255,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				   OBD_CONNECT2_FLR |
 				   OBD_CONNECT2_LOCK_CONVERT |
 				   OBD_CONNECT2_ARCHIVE_ID_ARRAY |
+				   OBD_CONNECT2_INC_XID |
 				   OBD_CONNECT2_LSOM |
 				   OBD_CONNECT2_ASYNC_DISCARD |
 				   OBD_CONNECT2_PCC;
@@ -464,7 +465,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 	data->ocd_connect_flags |= OBD_CONNECT_LOCKAHEAD_OLD;
 #endif
 
-	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD;
+	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD |
+				   OBD_CONNECT2_INC_XID;
 
 	if (!OBD_FAIL_CHECK(OBD_FAIL_OSC_CONNECT_GRANT_PARAM))
 		data->ocd_connect_flags |= OBD_CONNECT_GRANT_PARAM;

commit e87e30460b18e73d1dbf2627e59485ae3d670e60

LU-13389 tgt: not rollback obsolete rq
Restoring old pre-LU-11444 behavior in
last_rcvd/reply_data update, do not generate
a error for obsolete requests. Othewise tx_end
tries to rollback the operation and does it partially
leaving inodes unattached and changelog records not cleared;
Fixing reply_data slot leak on error.

Fixes: 23773b3 ("LU-11444 ptlrpc: resend may corrupt the data")
HPE-bug-id: LUS-8590
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I76c2de7b820da0239ce2f0a9d17b7dedadc0519b
Reviewed-on: https://review.whamcloud.com/38221
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/target/tgt_lastrcvd.c
@@ -1200,6 +1200,7 @@ static int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 {
 	struct lsd_reply_data	*lrd;
 	int	i;
+	int	rc;
 
 	lrd = &trd->trd_reply;
 	/* update export last transno */
@@ -1220,7 +1221,6 @@ static int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 
 		if (update_lrd_file) {
 			loff_t	off;
-			int	rc;
 
 			/* write reply data to disk */
 			off = sizeof(struct lsd_reply_header) + sizeof(*lrd) * i;
@@ -1228,7 +1228,7 @@ static int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 			if (unlikely(rc != 0)) {
 				CERROR("%s: can't update %s file: rc = %d\n",
 				       tgt_name(tgt), REPLY_DATA, rc);
-				RETURN(rc);
+				GOTO(free_slot, rc);
 			}
 		}
 	} else {
@@ -1242,8 +1242,12 @@ static int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 			      MSG_REPLAY : MSG_REPLAY|MSG_RESENT;
 
 		if (req->rq_obsolete) {
+			CDEBUG(D_INFO,
+			       "drop reply data update for obsolete req xid=%llu,"
+			       "transno=%llu, tag=%hu\n", req->rq_xid,
+			       lrd->lrd_transno, trd->trd_tag);
 			mutex_unlock(&ted->ted_lcd_lock);
-			RETURN(-EALREADY);
+			GOTO(free_slot, rc = -EBADR);
 		}
 
 		if (!(lustre_msg_get_flags(req->rq_reqmsg) & exclude))
@@ -1262,6 +1266,11 @@ static int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 	       trd->trd_tag, lrd->lrd_client_gen, trd->trd_index);
 
 	RETURN(0);
+
+free_slot:
+	if (tgt != NULL)
+		tgt_clear_reply_slot(tgt, trd->trd_index);
+	return rc;
 }
 
 int tgt_mk_reply_data(const struct lu_env *env,
@@ -1315,8 +1324,11 @@ int tgt_mk_reply_data(const struct lu_env *env,
 
 	rc = tgt_add_reply_data(env, tgt, ted, trd, req,
 				th, write_update);
-	if (rc < 0)
+	if (rc < 0) {
 		OBD_FREE_PTR(trd);
+		if (rc == -EBADR)
+			rc = 0;
+	}
 	return rc;
 
 }