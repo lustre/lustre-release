lustre/lustre-release

commit 604c266a175b72500ef99793652b64ed4f842b2c

LU-11803 obd: replace class_uuid with linux kernel version.
We can replace the lustre custom class_uuid_t with the linux
kernels uuid handling.

Change-Id: I9a59b0b6027ccb95994a87f3a5dcdf80a8a56480
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/33916
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Ben Evans <bevans@cray.com>
Tested-by: Jenkins
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Gu Zheng <gzheng@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -37,9 +37,11 @@
 #define DEBUG_SUBSYSTEM S_LLITE
 
 #include <linux/module.h>
+#include <linux/random.h>
 #include <linux/statfs.h>
 #include <linux/time.h>
 #include <linux/types.h>
+#include <libcfs/linux/linux-uuid.h>
 #include <linux/version.h>
 #include <linux/mm.h>
 #include <linux/user_namespace.h>
@@ -71,7 +73,6 @@ static struct ll_sb_info *ll_init_sbi(void)
 	unsigned long pages;
 	unsigned long lru_page_max;
 	struct sysinfo si;
-	class_uuid_t uuid;
 	int i;
 	ENTRY;
 
@@ -100,10 +101,6 @@ static struct ll_sb_info *ll_init_sbi(void)
 					   SBI_DEFAULT_READAHEAD_MAX);
 	sbi->ll_ra_info.ra_max_pages = sbi->ll_ra_info.ra_max_pages_per_file;
 	sbi->ll_ra_info.ra_max_read_ahead_whole_pages = -1;
-
-        ll_generate_random_uuid(uuid);
-        class_uuid_unparse(uuid, &sbi->ll_sb_uuid);
-        CDEBUG(D_CONFIG, "generated uuid: %s\n", sbi->ll_sb_uuid.uuid);
 
         sbi->ll_flags |= LL_SBI_VERBOSE;
 #ifdef ENABLE_CHECKSUM
@@ -1031,6 +1028,7 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	char name[MAX_STRING_SIZE];
 	int md_len = 0;
 	int dt_len = 0;
+	uuid_t uuid;
 	char *ptr;
 	int len;
 	int err;
@@ -1055,14 +1053,16 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	if (err)
 		GOTO(out_free_cfg, err);
 
-	err = super_setup_bdi_name(sb, "lustre-%016lx", cfg_instance);
-	if (err)
-		GOTO(out_free_cfg, err);
-
 #ifndef HAVE_DCACHE_LOCK
 	/* kernel >= 2.6.38 store dentry operations in sb->s_d_op. */
 	sb->s_d_op = &ll_d_ops;
 #endif
+	/* UUID handling */
+	generate_random_uuid(uuid.b);
+	snprintf(sbi->ll_sb_uuid.uuid, UUID_SIZE, "%pU", uuid.b);
+
+	CDEBUG(D_CONFIG, "llite sb uuid: %s\n", sbi->ll_sb_uuid.uuid);
+
 	/* Get fsname */
 	len = strlen(profilenm);
 	ptr = strrchr(profilenm, '-');
@@ -1083,8 +1083,12 @@ int ll_fill_super(struct super_block *sb, struct vfsmount *mnt)
 	sbi->ll_fsname[len] = '\0';
 
 	/* Mount info */
-	snprintf(name, MAX_STRING_SIZE, "%.*s-%016lx", len,
+	snprintf(name, sizeof(name), "%.*s-%016lx", len,
 		 profilenm, cfg_instance);
+
+	err = super_setup_bdi_name(sb, "%s", name);
+	if (err)
+		GOTO(out_free_cfg, err);
 
 	/* Call ll_debugfs_register_super() before lustre_process_log()
 	 * so that "llite.*.*" params can be processed correctly.

lustre/obdclass/obd_mount.c
@@ -43,6 +43,8 @@
 
 #include <obd.h>
 #include <obd_class.h>
+#include <linux/random.h>
+#include <libcfs/linux/linux-uuid.h>
 #include <linux/version.h>
 #include <lustre_log.h>
 #include <lustre_disk.h>
@@ -222,7 +224,7 @@ int lustre_start_mgc(struct super_block *sb)
 	struct obd_device *obd;
 	struct obd_export *exp;
 	struct obd_uuid *uuid = NULL;
-	class_uuid_t uuidc;
+	uuid_t uuidc;
 	lnet_nid_t nid;
 	char nidstr[LNET_NIDSTR_SIZE];
 	char *mgcname = NULL, *niduuid = NULL, *mgssec = NULL;
@@ -405,8 +407,8 @@ int lustre_start_mgc(struct super_block *sb)
 	if (uuid == NULL)
 		GOTO(out_free, rc = -ENOMEM);
 
-	ll_generate_random_uuid(uuidc);
-	class_uuid_unparse(uuidc, uuid);
+	generate_random_uuid(uuidc.b);
+	snprintf(uuid->uuid, UUID_SIZE, "%pU", uuidc.b);
 
 	/* Start the MGC */
 	rc = lustre_start_simple(mgcname, LUSTRE_MGC_NAME,

lustre/osd-ldiskfs/osd_scrub.c
@@ -2669,8 +2669,10 @@ int osd_scrub_setup(const struct lu_env *env, struct osd_device *dev)
 				       "failed to allocate RAM for report\n",
 				       osd_dev2name(dev));
 			} else {
-				class_uuid_unparse(sf->sf_uuid, old_uuid);
-				class_uuid_unparse(es->s_uuid, new_uuid);
+				snprintf(old_uuid->uuid, UUID_SIZE, "%pU",
+					 sf->sf_uuid);
+				snprintf(new_uuid->uuid, UUID_SIZE, "%pU",
+					 es->s_uuid);
 				CDEBUG(D_LFSCK, "%s: UUID has been changed "
 				       "from %s to %s\n", osd_dev2name(dev),
 				       old_uuid->uuid, new_uuid->uuid);

lustre/osd-zfs/osd_scrub.c
@@ -1468,10 +1468,11 @@ int osd_scrub_setup(const struct lu_env *env, struct osd_device *dev)
 				       "failed to allocate RAM for report\n",
 				       osd_name(dev));
 			} else {
-				class_uuid_unparse(sf->sf_uuid, old_uuid);
-				class_uuid_unparse(dev->od_uuid, new_uuid);
-				CDEBUG(D_LFSCK, "%s: UUID has been changed "
-				       "from %s to %s\n", osd_name(dev),
+				snprintf(old_uuid->uuid, UUID_SIZE, "%pU", sf->sf_uuid);
+				snprintf(new_uuid->uuid, UUID_SIZE, "%pU", dev->od_uuid);
+				CDEBUG(D_LFSCK,
+				       "%s: UUID has been changed from %s to %s\n",
+				       osd_name(dev),
 				       old_uuid->uuid, new_uuid->uuid);
 			}
 			scrub_file_reset(scrub, dev->od_uuid, SF_INCONSISTENT);

libcfs/autoconf/lustre-libcfs.m4
@@ -888,6 +888,23 @@ rhashtable_lookup_get_insert_fast, [
 ]) # LIBCFS_RHASHTABLE_LOOKUP_GET_INSERT_FAST
 
 #
+# Kernel version 4.12-rc3 f9727a17db9bab71ddae91f74f11a8a2f9a0ece6
+# renamed uuid_be to uuid_t
+#
+AC_DEFUN([LIBCFS_UUID_T], [
+LB_CHECK_COMPILE([if 'uuid_t' exist],
+uuid_t, [
+	#include <linux/uuid.h>
+],[
+	uuid_t uuid;
+
+	memset(uuid.b, 0, 16);
+],[
+	AC_DEFINE(HAVE_UUID_T, 1, ['uuid_t' exist])
+])
+]) # LIBCFS_UUID_T
+
+#
 # LIBCFS_WAIT_QUEUE_ENTRY
 #
 # Kernel version 4.13 ac6424b981bce1c4bc55675c6ce11bfe1bbfa64f
@@ -1053,6 +1070,8 @@ LIBCFS_HOTPLUG_STATE_MACHINE
 # 4.11
 LIBCFS_RHASHTABLE_LOOKUP_GET_INSERT_FAST
 LIBCFS_SCHED_HEADERS
+# 4.12
+LIBCFS_UUID_T
 # 4.13
 LIBCFS_WAIT_QUEUE_ENTRY
 # 4.14

libcfs/include/libcfs/linux/Makefile.am
@@ -1,2 +1,2 @@
 EXTRA_DIST = linux-misc.h linux-fs.h linux-mem.h linux-time.h linux-cpu.h \
-	     linux-list.h linux-hash.h linux-crypto.h
+	     linux-list.h linux-hash.h linux-uuid.h linux-crypto.h

libcfs/include/libcfs/linux/linux-uuid.h
@@ -1,0 +1,64 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Copyright (c) 2011, 2017, Intel Corporation.
+ */
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ * Lustre is a trademark of Sun Microsystems, Inc.
+ */
+
+#ifndef __LIBCFS_LINUX_UUID_H__
+#define __LIBCFS_LINUX_UUID_H__
+
+#include <linux/uuid.h>
+
+#define UUID_SIZE 16
+
+/*
+ * The original linux UUID code had uuid_be and uuid_le.
+ * Later uuid_le was changed to guid_t and uuid_be
+ * to uuid_t. See for details kernel commit:
+ *
+ * f9727a17db9bab71ddae91f74f11a8a2f9a0ece6
+ */
+#ifndef HAVE_UUID_T
+typedef struct {
+	__u8 b[UUID_SIZE];
+} uuid_t;
+
+static inline void uuid_copy(uuid_t *dst, uuid_t *src)
+{
+	memcpy(dst, src, sizeof(uuid_t));
+}
+
+static inline bool uuid_equal(const uuid_t *u1, const uuid_t *u2)
+{
+	return memcmp(u1, u2, sizeof(uuid_t)) == 0;
+}
+
+#endif
+
+#endif /* __LIBCFS_LINUX_UUID_H__ */

lustre/include/lustre_scrub.h
@@ -33,6 +33,7 @@
 #ifndef _LUSTRE_SCRUB_H
 # define _LUSTRE_SCRUB_H
 
+#include <libcfs/linux/linux-uuid.h>
 #include <dt_object.h>
 #include <lustre_net.h>
 

commit 9abdf96e56e239b81102c8a43dc2f0524f2be90a

LU-13499 obd: fix printing of client connection UUID
The client connection UUID sent to the servers (ASCII format) was
being truncated to only 16 bytes in size, like '595f3c6a-20ae-4'
instead of a full UUID like '18ae0f9a-4b09-4599-8ced-0f2126eab425'.

This was caused by using UUID_SIZE to limit the size of the "%pU"
string printed to avoid overflowing the target buffer, but in fact
UUID_SIZE is the size of the binary uuid_t (16 bytes) instead of
the size of struct obd_uuid (40 bytes) where the ASCII version of
the UUID is stored.

Fix this to use sizeof(target) rather than an external constant,
which is exactly why sizeof(target) should always be used.  The
usage in osd_scrub.c is not actually broken, but it is still
better to use sizeof(target) to avoid future inconsistencies.

Fixes: 604c266a175b7 ("LU-11803 obd: replace class_uuid with linux kernel version")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I05325646cd632a09997d6632a483909629ce7057
Reviewed-on: https://review.whamcloud.com/38443
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.super@gmail.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -1104,7 +1104,7 @@ int ll_fill_super(struct super_block *sb)
 
 	/* UUID handling */
 	generate_random_uuid(uuid.b);
-	snprintf(sbi->ll_sb_uuid.uuid, UUID_SIZE, "%pU", uuid.b);
+	snprintf(sbi->ll_sb_uuid.uuid, sizeof(sbi->ll_sb_uuid), "%pU", uuid.b);
 
 	CDEBUG(D_CONFIG, "llite sb uuid: %s\n", sbi->ll_sb_uuid.uuid);
 

lustre/obdclass/obd_mount.c
@@ -410,7 +410,7 @@ int lustre_start_mgc(struct super_block *sb)
 		GOTO(out_free, rc = -ENOMEM);
 
 	generate_random_uuid(uuidc.b);
-	snprintf(uuid->uuid, UUID_SIZE, "%pU", uuidc.b);
+	snprintf(uuid->uuid, sizeof(*uuid), "%pU", uuidc.b);
 
 	/* Start the MGC */
 	rc = lustre_start_simple(mgcname, LUSTRE_MGC_NAME,

lustre/osd-ldiskfs/osd_scrub.c
@@ -2623,7 +2623,7 @@ int osd_scrub_setup(const struct lu_env *env, struct osd_device *dev)
 		RETURN(obj ? PTR_ERR(obj) : -ENOENT);
 
 #ifndef HAVE_S_UUID_AS_UUID_T
-	memcpy(dev->od_uuid.b, sb->s_uuid, UUID_SIZE);
+	memcpy(dev->od_uuid.b, sb->s_uuid, sizeof(dev->od_uuid));
 #else
 	uuid_copy(&dev->od_uuid, &sb->s_uuid);
 #endif