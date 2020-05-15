d25381cc073044f26f7e0eb0461a3526610b0d52
James Simmons <jsimmons@infradead.org>
2020-05-15 12:54:39 -0400

    LU-6142 libcfs: fix tab and alignment issues for libcfs_string.c
    Resolve non-code changes for checkpatch errors.
    
    Test-Parameters: trivial
    Change-Id: Ie2848a83fdf58853ec32c4b188bcae1bcf4afa89
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Reviewed-on: https://review.whamcloud.com/38623
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.super@gmail.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- libcfs/libcfs/libcfs_string.c
+++ libcfs/libcfs/libcfs_string.c
@@ -69,40 +69,41 @@ EXPORT_SYMBOL(cfs_strrstr);
 
 /* Convert a text string to a bitmask */
 int cfs_str2mask(const char *str, const char *(*bit2str)(int bit),
-                 int *oldmask, int minmask, int allmask)
+		 int *oldmask, int minmask, int allmask)
 {
-        const char *debugstr;
-        char op = 0;
-        int newmask = minmask, i, len, found = 0;
-        ENTRY;
+	const char *debugstr;
+	char op = 0;
+	int newmask = minmask, i, len, found = 0;
 
-        /* <str> must be a list of tokens separated by whitespace
-         * and optionally an operator ('+' or '-').  If an operator
-         * appears first in <str>, '*oldmask' is used as the starting point
-         * (relative), otherwise minmask is used (absolute).  An operator
-         * applies to all following tokens up to the next operator. */
-        while (*str != 0) {
-                while (isspace(*str))
-                        str++;
-                if (*str == 0)
-                        break;
-                if (*str == '+' || *str == '-') {
-                        op = *str++;
-                        if (!found)
-                                /* only if first token is relative */
-                                newmask = *oldmask;
-                        while (isspace(*str))
-                                str++;
-                        if (*str == 0)          /* trailing op */
-                                return -EINVAL;
-                }
+	ENTRY;
+	/* <str> must be a list of tokens separated by whitespace
+	 * and optionally an operator ('+' or '-').  If an operator
+	 * appears first in <str>, '*oldmask' is used as the starting point
+	 * (relative), otherwise minmask is used (absolute).  An operator
+	 * applies to all following tokens up to the next operator.
+	 */
+	while (*str != 0) {
+		while (isspace(*str))
+			str++;
+		if (*str == 0)
+			break;
+		if (*str == '+' || *str == '-') {
+			op = *str++;
+			if (!found)
+				/* only if first token is relative */
+				newmask = *oldmask;
+			while (isspace(*str))
+				str++;
+			if (*str == 0)		/* trailing op */
+				return -EINVAL;
+		}
 
-                /* find token length */
-                for (len = 0; str[len] != 0 && !isspace(str[len]) &&
-                      str[len] != '+' && str[len] != '-'; len++);
+		/* find token length */
+		for (len = 0; str[len] != 0 && !isspace(str[len]) &&
+		     str[len] != '+' && str[len] != '-'; len++);
 
-                /* match token */
-                found = 0;
+		/* match token */
+		found = 0;
 		for (i = 0; i < 32; i++) {
 			debugstr = bit2str(i);
 			if (debugstr != NULL &&
@@ -116,52 +117,52 @@ int cfs_str2mask(const char *str, const char *(*bit2str)(int bit),
 				break;
 			}
 		}
-                if (!found && len == 3 &&
-                    (strncasecmp(str, "ALL", len) == 0)) {
-                        if (op == '-')
-                                newmask = minmask;
-                        else
-                                newmask = allmask;
-                        found = 1;
-                }
-                if (!found) {
-                        CWARN("unknown mask '%.*s'.\n"
-                              "mask usage: [+|-]<all|type> ...\n", len, str);
-                        return -EINVAL;
-                }
-                str += len;
-        }
+		if (!found && len == 3 &&
+		    (strncasecmp(str, "ALL", len) == 0)) {
+			if (op == '-')
+				newmask = minmask;
+			else
+				newmask = allmask;
+			found = 1;
+		}
+		if (!found) {
+			CWARN("unknown mask '%.*s'.\n"
+			      "mask usage: [+|-]<all|type> ...\n", len, str);
+			return -EINVAL;
+		}
+		str += len;
+	}
 
-        *oldmask = newmask;
-        return 0;
+	*oldmask = newmask;
+	return 0;
 }
 EXPORT_SYMBOL(cfs_str2mask);
 
 /* get the first string out of @str */
 char *cfs_firststr(char *str, size_t size)
 {
-        size_t i = 0;
-        char  *end;
+	size_t i = 0;
+	char *end;
 
-        /* trim leading spaces */
-        while (i < size && *str && isspace(*str)) {
-                ++i;
-                ++str;
-        }
+	/* trim leading spaces */
+	while (i < size && *str && isspace(*str)) {
+		++i;
+		++str;
+	}
 
-        /* string with all spaces */
-        if (*str == '\0')
-                goto out;
+	/* string with all spaces */
+	if (*str == '\0')
+		goto out;
 
-        end = str;
-        while (i < size && *end != '\0' && !isspace(*end)) {
-                ++i;
-                ++end;
-        }
+	end = str;
+	while (i < size && *end != '\0' && !isspace(*end)) {
+		++i;
+		++end;
+	}
 
-        *end= '\0';
+	*end = '\0';
 out:
-        return str;
+	return str;
 }
 EXPORT_SYMBOL(cfs_firststr);
 
@@ -296,8 +297,8 @@ static int
 cfs_range_expr_parse(struct cfs_lstr *src, unsigned min, unsigned max,
 		     int bracketed, struct cfs_range_expr **expr)
 {
-	struct cfs_range_expr	*re;
-	struct cfs_lstr		tok;
+	struct cfs_range_expr *re;
+	struct cfs_lstr	tok;
 
 	LIBCFS_ALLOC(re, sizeof(*re));
 	if (re == NULL)
@@ -347,11 +348,11 @@ cfs_range_expr_parse(struct cfs_lstr *src, unsigned min, unsigned max,
 		}
 	}
 
- out:
+out:
 	*expr = re;
 	return 0;
 
- failed:
+failed:
 	LIBCFS_FREE(re, sizeof(*re));
 	return -EINVAL;
 }
@@ -378,11 +379,11 @@ cfs_range_expr_print(char *buffer, int count, struct cfs_range_expr *expr,
 		i = scnprintf(buffer, count, "%u", expr->re_lo);
 	else if (expr->re_stride == 1)
 		i = scnprintf(buffer, count, "%s%u-%u%s",
-				  s, expr->re_lo, expr->re_hi, e);
+			      s, expr->re_lo, expr->re_hi, e);
 	else
 		i = scnprintf(buffer, count, "%s%u-%u/%u%s",
-				  s, expr->re_lo, expr->re_hi,
-				  expr->re_stride, e);
+			      s, expr->re_lo, expr->re_hi,
+			      expr->re_stride, e);
 	return i;
 }
 
@@ -432,7 +433,7 @@ EXPORT_SYMBOL(cfs_expr_list_print);
 int
 cfs_expr_list_match(__u32 value, struct cfs_expr_list *expr_list)
 {
-	struct cfs_range_expr	*expr;
+	struct cfs_range_expr *expr;
 
 	list_for_each_entry(expr, &expr_list->el_exprs, re_link) {
 		if (value >= expr->re_lo && value <= expr->re_hi &&
@@ -454,10 +455,10 @@ EXPORT_SYMBOL(cfs_expr_list_match);
 int
 cfs_expr_list_values(struct cfs_expr_list *expr_list, int max, __u32 **valpp)
 {
-	struct cfs_range_expr	*expr;
-	__u32			*val;
-	int			count = 0;
-	int			i;
+	struct cfs_range_expr *expr;
+	__u32 *val;
+	int count = 0;
+	int i;
 
 	list_for_each_entry(expr, &expr_list->el_exprs, re_link) {
 		for (i = expr->re_lo; i <= expr->re_hi; i++) {
@@ -533,10 +534,10 @@ int
 cfs_expr_list_parse(char *str, int len, unsigned min, unsigned max,
 		    struct cfs_expr_list **elpp)
 {
-	struct cfs_expr_list	*expr_list;
-	struct cfs_range_expr	*expr;
-	struct cfs_lstr		src;
-	int			rc;
+	struct cfs_expr_list *expr_list;
+	struct cfs_range_expr *expr;
+	struct cfs_lstr	src;
+	int rc;
 
 	LIBCFS_ALLOC(expr_list, sizeof(*expr_list));
 	if (expr_list == NULL)
@@ -565,15 +566,12 @@ cfs_expr_list_parse(char *str, int len, unsigned min, unsigned max,
 			if (rc != 0)
 				break;
 
-			list_add_tail(&expr->re_link,
-					  &expr_list->el_exprs);
+			list_add_tail(&expr->re_link, &expr_list->el_exprs);
 		}
 	} else {
 		rc = cfs_range_expr_parse(&src, min, max, 0, &expr);
-		if (rc == 0) {
-			list_add_tail(&expr->re_link,
-					  &expr_list->el_exprs);
-		}
+		if (rc == 0)
+			list_add_tail(&expr->re_link, &expr_list->el_exprs);
 	}
 
 	if (rc != 0)
