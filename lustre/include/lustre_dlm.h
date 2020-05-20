lustre/lustre-release

commit e920be6814512b1aa8696ea36d697d3b698c13e8

LU-9019 ldlm: migrate the rest of the code to 64 bit time
Replace the last cfs_time_current_sec() to avoid the overflow
issues in 2038 with ktime_get_real_seconds(). Reduce the jiffies
usage to the bare minimum which is useage for mod_timer() and
schedule_timeout(). This makes the ldlm totally 64 bit time
compliant.

Change-Id: Iaee92c17d51fdfc55bd26e9e813e30a6ce794856
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/29295
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: John L. Hammond <john.hammond@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/include/lustre_dlm.h
@@ -438,14 +438,14 @@ struct ldlm_namespace {
 	 * This allows the client to start caching negative dentries
 	 * for a directory and may save an RPC for a later stat.
 	 */
-	unsigned int		ns_ctime_age_limit;
+	time64_t		ns_ctime_age_limit;
 
 	/**
 	 * Used to rate-limit ldlm_namespace_dump calls.
 	 * \see ldlm_namespace_dump. Increased by 10 seconds every time
 	 * it is called.
 	 */
-	cfs_time_t		ns_next_dump;
+	time64_t		ns_next_dump;
 
 	/** "policy" function that does actual lock conflict determination */
 	ldlm_res_policy		ns_policy;
@@ -483,7 +483,7 @@ struct ldlm_namespace {
 	 * The resources in this namespace remember contended state during
 	 * \a ns_contention_time, in seconds.
 	 */
-	unsigned		ns_contention_time;
+	time64_t		ns_contention_time;
 
 	/**
 	 * Limit size of contended extent locks, in bytes.
@@ -871,7 +871,7 @@ struct ldlm_lock {
 	 * under this lock.
 	 * \see ost_rw_prolong_locks
 	 */
-	cfs_time_t		l_callback_timeout;
+	time64_t		l_callback_timeout;
 
 	/** Local PID of process which created this lock. */
 	__u32			l_pid;
@@ -980,8 +980,9 @@ struct ldlm_resource {
 	union {
 		/**
 		 * When the resource was considered as contended,
-		 * used only on server side. */
-		cfs_time_t	lr_contention_time;
+		 * used only on server side.
+		 */
+		time64_t	lr_contention_time;
 		/**
 		 * Associated inode, used only on client side.
 		 */
@@ -1263,7 +1264,7 @@ struct ldlm_prolong_args {
 	struct ldlm_res_id	lpa_resid;
 	struct ldlm_extent	lpa_extent;
 	enum ldlm_mode		lpa_mode;
-	int			lpa_timeout;
+	time64_t		lpa_timeout;
 	int			lpa_locks_cnt;
 	int			lpa_blocks_cnt;
 };
@@ -1312,10 +1313,10 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 /** @} ldlm_handlers */
 
 void ldlm_revoke_export_locks(struct obd_export *exp);
-unsigned int ldlm_bl_timeout(struct ldlm_lock *lock);
+time64_t ldlm_bl_timeout(struct ldlm_lock *lock);
 #endif
 int ldlm_del_waiting_lock(struct ldlm_lock *lock);
-int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, int timeout);
+int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, time64_t timeout);
 int ldlm_get_ref(void);
 void ldlm_put_ref(void);
 int ldlm_init_export(struct obd_export *exp);

lustre/ldlm/ldlm_lockd.c
@@ -64,18 +64,16 @@ struct kset *ldlm_svc_kset;
 
 static struct ldlm_state *ldlm_state;
 
-static inline cfs_time_t round_timeout(cfs_time_t timeout)
+/* timeout for initial callback (AST) reply (bz10399)
+ * Due to having to send a 32 bit time value over the
+ * wire return it as time_t instead of time64_t
+ */
+static inline time_t ldlm_get_rq_timeout(void)
 {
-        return cfs_time_seconds((int)cfs_duration_sec(cfs_time_sub(timeout, 0)) + 1);
-}
+	/* Non-AT value */
+	time_t timeout = min(ldlm_timeout, obd_timeout / 3);
 
-/* timeout for initial callback (AST) reply (bz10399) */
-static inline unsigned int ldlm_get_rq_timeout(void)
-{
-        /* Non-AT value */
-        unsigned int timeout = min(ldlm_timeout, obd_timeout / 3);
-
-        return timeout < 1 ? 1 : timeout;
+	return timeout < 1 ? 1 : timeout;
 }
 
 struct ldlm_bl_pool {
@@ -259,7 +257,7 @@ static int expired_lock_main(void *arg)
 }
 
 static int ldlm_add_waiting_lock(struct ldlm_lock *lock);
-static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, int seconds);
+static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds);
 
 /**
  * Check if there is a request in the export request list
@@ -296,11 +294,10 @@ static void waiting_locks_callback(unsigned long unused)
 	spin_lock_bh(&waiting_locks_spinlock);
 	while (!list_empty(&waiting_locks_list)) {
 		lock = list_entry(waiting_locks_list.next, struct ldlm_lock,
-                                      l_pending_chain);
-                if (cfs_time_after(lock->l_callback_timeout,
-                                   cfs_time_current()) ||
-                    (lock->l_req_mode == LCK_GROUP))
-                        break;
+				  l_pending_chain);
+		if (lock->l_callback_timeout > ktime_get_seconds() ||
+		    lock->l_req_mode == LCK_GROUP)
+			break;
 
                 /* Check if we need to prolong timeout */
                 if (!OBD_FAIL_CHECK(OBD_FAIL_PTLRPC_HPREQ_TIMEOUT) &&
@@ -348,17 +345,18 @@ static void waiting_locks_callback(unsigned long unused)
 		wake_up(&expired_lock_wait_queue);
 	}
 
-        /*
-         * Make sure the timer will fire again if we have any locks
-         * left.
-         */
+	/*
+	 * Make sure the timer will fire again if we have any locks
+	 * left.
+	 */
 	if (!list_empty(&waiting_locks_list)) {
-                cfs_time_t timeout_rounded;
+		unsigned long timeout_jiffies;
+
 		lock = list_entry(waiting_locks_list.next, struct ldlm_lock,
-                                      l_pending_chain);
-                timeout_rounded = (cfs_time_t)round_timeout(lock->l_callback_timeout);
-		mod_timer(&waiting_locks_timer, timeout_rounded);
-        }
+				  l_pending_chain);
+		timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
+		mod_timer(&waiting_locks_timer, timeout_jiffies);
+	}
 	spin_unlock_bh(&waiting_locks_spinlock);
 }
 
@@ -374,10 +372,10 @@ static void waiting_locks_callback(unsigned long unused)
  *
  * Called with the namespace lock held.
  */
-static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, int seconds)
+static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds)
 {
-        cfs_time_t timeout;
-        cfs_time_t timeout_rounded;
+	unsigned long timeout_jiffies;
+	time64_t timeout;
 
 	if (!list_empty(&lock->l_pending_chain))
                 return 0;
@@ -386,21 +384,22 @@ static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, int seconds)
             OBD_FAIL_CHECK(OBD_FAIL_PTLRPC_HPREQ_TIMEOUT))
                 seconds = 1;
 
-        timeout = cfs_time_shift(seconds);
-        if (likely(cfs_time_after(timeout, lock->l_callback_timeout)))
+	timeout = ktime_get_seconds() + seconds;
+	if (likely(timeout > lock->l_callback_timeout))
                 lock->l_callback_timeout = timeout;
 
-        timeout_rounded = round_timeout(lock->l_callback_timeout);
+	timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
 
-	if (cfs_time_before(timeout_rounded, waiting_locks_timer.expires) ||
-	    !timer_pending(&waiting_locks_timer)) {
-		mod_timer(&waiting_locks_timer, timeout_rounded);
-        }
-        /* if the new lock has a shorter timeout than something earlier on
-           the list, we'll wait the longer amount of time; no big deal. */
-        /* FIFO */
+	if (time_before(timeout_jiffies, waiting_locks_timer.expires) ||
+	    !timer_pending(&waiting_locks_timer))
+		mod_timer(&waiting_locks_timer, timeout_jiffies);
+
+	/* if the new lock has a shorter timeout than something earlier on
+	 * the list, we'll wait the longer amount of time; no big deal.
+	 */
+	/* FIFO */
 	list_add_tail(&lock->l_pending_chain, &waiting_locks_list);
-        return 1;
+	return 1;
 }
 
 static void ldlm_add_blocked_lock(struct ldlm_lock *lock)
@@ -427,8 +426,8 @@ static void ldlm_add_blocked_lock(struct ldlm_lock *lock)
 
 static int ldlm_add_waiting_lock(struct ldlm_lock *lock)
 {
+	time64_t timeout = ldlm_bl_timeout(lock);
 	int ret;
-	int timeout = ldlm_bl_timeout(lock);
 
 	/* NB: must be called with hold of lock_res_and_lock() */
 	LASSERT(ldlm_is_res_locked(lock));
@@ -447,12 +446,12 @@ static int ldlm_add_waiting_lock(struct ldlm_lock *lock)
 	}
 
 	if (ldlm_is_destroyed(lock)) {
-		static cfs_time_t next;
+		static time64_t next;
 
 		spin_unlock_bh(&waiting_locks_spinlock);
 		LDLM_ERROR(lock, "not waiting on destroyed lock (bug 5653)");
-		if (cfs_time_after(cfs_time_current(), next)) {
-			next = cfs_time_shift(14400);
+		if (ktime_get_seconds() > next) {
+			next = ktime_get_seconds() + 14400;
 			libcfs_debug_dumpstack(NULL);
 		}
 		return 0;
@@ -471,7 +470,7 @@ static int ldlm_add_waiting_lock(struct ldlm_lock *lock)
 	if (ret)
 		ldlm_add_blocked_lock(lock);
 
-	LDLM_DEBUG(lock, "%sadding to wait list(timeout: %d, AT: %s)",
+	LDLM_DEBUG(lock, "%sadding to wait list(timeout: %lld, AT: %s)",
 		   ret == 0 ? "not re-" : "", timeout,
 		   AT_OFF ? "off" : "on");
 	return ret;
@@ -501,10 +500,11 @@ static int __ldlm_del_waiting_lock(struct ldlm_lock *lock)
 			del_timer(&waiting_locks_timer);
                 } else {
                         struct ldlm_lock *next;
+
 			next = list_entry(list_next, struct ldlm_lock,
-                                              l_pending_chain);
+					  l_pending_chain);
 			mod_timer(&waiting_locks_timer,
-				  round_timeout(next->l_callback_timeout));
+				  cfs_time_seconds(next->l_callback_timeout));
                 }
         }
 	list_del_init(&lock->l_pending_chain);
@@ -547,7 +547,7 @@ int ldlm_del_waiting_lock(struct ldlm_lock *lock)
  *
  * Called with namespace lock held.
  */
-int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, int timeout)
+int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, time64_t timeout)
 {
 	if (lock->l_export == NULL) {
 		/* We don't have a "waiting locks list" on clients. */
@@ -587,7 +587,7 @@ int ldlm_del_waiting_lock(struct ldlm_lock *lock)
         RETURN(0);
 }
 
-int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, int timeout)
+int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, time64_t timeout)
 {
         RETURN(0);
 }
@@ -605,9 +605,9 @@ int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, int timeout)
  *
  * \retval            timeout in seconds to wait for the client reply
  */
-unsigned int ldlm_bl_timeout(struct ldlm_lock *lock)
+time64_t ldlm_bl_timeout(struct ldlm_lock *lock)
 {
-	unsigned int timeout;
+	time64_t timeout;
 
 	if (AT_OFF)
 		return obd_timeout / 2;
@@ -617,7 +617,7 @@ unsigned int ldlm_bl_timeout(struct ldlm_lock *lock)
 	 * It would be nice to have some kind of "early reply" mechanism for
 	 * lock callbacks too... */
 	timeout = at_get(&lock->l_export->exp_bl_lock_at);
-	return max(timeout + (timeout >> 1), ldlm_enqueue_min);
+	return max(timeout + (timeout >> 1), (time64_t)ldlm_enqueue_min);
 }
 EXPORT_SYMBOL(ldlm_bl_timeout);
 
@@ -773,8 +773,8 @@ static int ldlm_cb_interpret(const struct lu_env *env,
 
 static void ldlm_update_resend(struct ptlrpc_request *req, void *data)
 {
-	struct ldlm_cb_async_args *ca   = data;
-	struct ldlm_lock          *lock = ca->ca_lock;
+	struct ldlm_cb_async_args *ca = data;
+	struct ldlm_lock *lock = ca->ca_lock;
 
 	ldlm_refresh_waiting_lock(lock, ldlm_bl_timeout(lock));
 }
@@ -1773,7 +1773,8 @@ static void ldlm_handle_cp_callback(struct ptlrpc_request *req,
 
 	INIT_LIST_HEAD(&ast_list);
 	if (OBD_FAIL_CHECK(OBD_FAIL_LDLM_CANCEL_BL_CB_RACE)) {
-		int to = cfs_time_seconds(1);
+		long to = cfs_time_seconds(1);
+
 		while (to > 0) {
 			set_current_state(TASK_INTERRUPTIBLE);
 			schedule_timeout(to);

lustre/ldlm/ldlm_resource.c
@@ -409,7 +409,6 @@ static ssize_t lru_max_age_store(struct kobject *kobj, struct attribute *attr,
 	int scale = NSEC_PER_MSEC;
 	unsigned long long tmp;
 	char *buf;
-	int err;
 
 	/* Did the user ask in seconds or milliseconds. Default is in ms */
 	buf = strstr(buffer, "ms");
@@ -422,8 +421,7 @@ static ssize_t lru_max_age_store(struct kobject *kobj, struct attribute *attr,
 	if (buf)
 		*buf = '\0';
 
-	err = kstrtoull(buffer, 10, &tmp);
-	if (err != 0)
+	if (kstrtoull(buffer, 10, &tmp))
 		return -EINVAL;
 
 	ns->ns_max_age = ktime_set(0, tmp * scale);
@@ -471,7 +469,7 @@ static ssize_t ctime_age_limit_show(struct kobject *kobj,
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
 
-	return sprintf(buf, "%u\n", ns->ns_ctime_age_limit);
+	return sprintf(buf, "%llu\n", ns->ns_ctime_age_limit);
 }
 
 static ssize_t ctime_age_limit_store(struct kobject *kobj,
@@ -480,11 +478,9 @@ static ssize_t ctime_age_limit_store(struct kobject *kobj,
 {
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
-	unsigned long tmp;
-	int err;
+	unsigned long long tmp;
 
-	err = kstrtoul(buffer, 10, &tmp);
-	if (err != 0)
+	if (kstrtoull(buffer, 10, &tmp))
 		return -EINVAL;
 
 	ns->ns_ctime_age_limit = tmp;
@@ -537,7 +533,7 @@ static ssize_t contention_seconds_show(struct kobject *kobj,
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
 
-	return sprintf(buf, "%u\n", ns->ns_contention_time);
+	return sprintf(buf, "%llu\n", ns->ns_contention_time);
 }
 
 static ssize_t contention_seconds_store(struct kobject *kobj,
@@ -546,11 +542,9 @@ static ssize_t contention_seconds_store(struct kobject *kobj,
 {
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
-	unsigned long tmp;
-	int err;
+	unsigned long long tmp;
 
-	err = kstrtoul(buffer, 10, &tmp);
-	if (err != 0)
+	if (kstrtoull(buffer, 10, &tmp))
 		return -EINVAL;
 
 	ns->ns_contention_time = tmp;
@@ -1654,14 +1648,14 @@ void ldlm_namespace_dump(int level, struct ldlm_namespace *ns)
 	       ldlm_ns_name(ns), atomic_read(&ns->ns_bref),
 	       ns_is_client(ns) ? "client" : "server");
 
-	if (cfs_time_before(cfs_time_current(), ns->ns_next_dump))
+	if (ktime_get_seconds() < ns->ns_next_dump)
 		return;
 
 	cfs_hash_for_each_nolock(ns->ns_rs_hash,
 				 ldlm_res_hash_dump,
 				 (void *)(unsigned long)level, 0);
 	spin_lock(&ns->ns_lock);
-	ns->ns_next_dump = cfs_time_shift(10);
+	ns->ns_next_dump = ktime_get_seconds() + 10;
 	spin_unlock(&ns->ns_lock);
 }
 

lustre/include/lustre_net.h
@@ -784,9 +784,9 @@ struct ptlrpc_cli_req {
 	/** For bulk requests on client only: bulk descriptor */
 	struct ptlrpc_bulk_desc		*cr_bulk;
 	/** optional time limit for send attempts */
-	cfs_duration_t			 cr_delay_limit;
+	time64_t			 cr_delay_limit;
 	/** time request was first queued */
-	cfs_time_t			 cr_queued_time;
+	time64_t			 cr_queued_time;
 	/** request sent in nanoseconds */
 	ktime_t				 cr_sent_ns;
 	/** time for request really sent out */
@@ -1115,8 +1115,9 @@ struct ptlrpc_request {
 	/**
 	 * service time estimate (secs)
 	 * If the request is not served by this time, it is marked as timed out.
+	 * Do not change to time64_t since this is transmitted over the wire.
 	 */
-	int				 rq_timeout;
+	time_t				 rq_timeout;
 	/**
 	 * when request/reply sent (secs), or time when request should be sent
 	 */
@@ -2592,11 +2593,8 @@ static inline int ptlrpc_req_get_repsize(struct ptlrpc_request *req)
 static inline int ptlrpc_send_limit_expired(struct ptlrpc_request *req)
 {
         if (req->rq_delay_limit != 0 &&
-            cfs_time_before(cfs_time_add(req->rq_queued_time,
-                                         cfs_time_seconds(req->rq_delay_limit)),
-                            cfs_time_current())) {
+	    req->rq_queued_time + req->rq_delay_limit < ktime_get_seconds())
                 return 1;
-        }
         return 0;
 }
 

lustre/ldlm/ldlm_extent.c
@@ -311,7 +311,7 @@ static void ldlm_extent_policy(struct ldlm_resource *res,
 static int ldlm_check_contention(struct ldlm_lock *lock, int contended_locks)
 {
 	struct ldlm_resource *res = lock->l_resource;
-	cfs_time_t now = cfs_time_current();
+	time64_t now = ktime_get_seconds();
 
 	if (OBD_FAIL_CHECK(OBD_FAIL_LDLM_SET_CONTENTION))
 		return 1;
@@ -319,8 +319,9 @@ static int ldlm_check_contention(struct ldlm_lock *lock, int contended_locks)
 	CDEBUG(D_DLMTRACE, "contended locks = %d\n", contended_locks);
 	if (contended_locks > ldlm_res_to_ns(res)->ns_contended_locks)
 		res->lr_contention_time = now;
-	return cfs_time_before(now, cfs_time_add(res->lr_contention_time,
-		cfs_time_seconds(ldlm_res_to_ns(res)->ns_contention_time)));
+
+	return now < res->lr_contention_time +
+		     ldlm_res_to_ns(res)->ns_contention_time;
 }
 
 struct ldlm_extent_compat_args {
@@ -675,7 +676,7 @@ destroylock:
 void ldlm_lock_prolong_one(struct ldlm_lock *lock,
 			   struct ldlm_prolong_args *arg)
 {
-	int timeout;
+	time64_t timeout;
 
 	if (arg->lpa_export != lock->l_export ||
 	    lock->l_flags & LDLM_FL_DESTROYED)
@@ -693,7 +694,7 @@ void ldlm_lock_prolong_one(struct ldlm_lock *lock,
 	 */
 	timeout = arg->lpa_timeout + (ldlm_bl_timeout(lock) >> 1);
 
-	LDLM_DEBUG(lock, "refreshed to %ds.\n", timeout);
+	LDLM_DEBUG(lock, "refreshed to %llds.\n", timeout);
 
 	arg->lpa_blocks_cnt++;
 

commit 3108bbb0b8485484bc1ac8ba5bac4276fc11c7c3

LU-12931 ldlm: use proper units for timeouts
Use timeout_t for ns_ctime_age_limit since this is a relative time
and not an absolute time.

Use ktime_t for ns_dirty_age_limit internally, even though the user
interface is in seconds, since this is frequenty used together with
other ktime_t values in the kernel.

Fixes: e920be681451 ("LU-9019 ldlm: migrate the rest of the code to 64 bit time")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Idb5ece834e56a95cd781cb871b1b7c20bf3ebbe5
Reviewed-on: https://review.whamcloud.com/38365
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_dlm.h
@@ -435,7 +435,9 @@ struct ldlm_namespace {
 	 */
 	unsigned int		ns_max_unused;
 
-	/** Maximum allowed age (last used time) for locks in the LRU */
+	/** Maximum allowed age (last used time) for locks in the LRU.  Set in
+	 * seconds from userspace, but stored in ns to avoid repeat conversions.
+	 */
 	ktime_t			ns_max_age;
 
 	/**
@@ -444,19 +446,20 @@ struct ldlm_namespace {
 	 */
 	unsigned int		ns_timeouts;
 	/**
-	 * Number of seconds since the file change time after which the
-	 * MDT will return an UPDATE lock along with a LOOKUP lock.
+	 * Number of seconds since the file change time after which
+	 * the MDT will return an UPDATE lock along with a LOOKUP lock.
 	 * This allows the client to start caching negative dentries
 	 * for a directory and may save an RPC for a later stat.
 	 */
-	time64_t		ns_ctime_age_limit;
+	timeout_t		ns_ctime_age_limit;
 	/**
-	 * Number of seconds since the lock was last used. The client may
-	 * cancel the lock limited by this age and flush related data if
-	 * any other client shows interest in it doing glimpse request.
-	 * This allows to cache stat data locally for such files early.
+	 * Number of (nano)seconds since the lock was last used. The client
+	 * may cancel the lock older than this age and flush related data if
+	 * another client shows interest in this lock by doing glimpse request.
+	 * This allows to cache stat data locally for such files early. Set in
+	 * seconds from userspace, but stored in ns to avoid repeat conversions.
 	 */
-	time64_t		ns_dirty_age_limit;
+	ktime_t			ns_dirty_age_limit;
 	/**
 	 * Used to rate-limit ldlm_namespace_dump calls.
 	 * \see ldlm_namespace_dump. Increased by 10 seconds every time

lustre/ldlm/ldlm_lockd.c
@@ -2083,8 +2083,7 @@ static void ldlm_handle_gl_callback(struct ptlrpc_request *req,
 	if (lock->l_granted_mode == LCK_PW &&
 	    !lock->l_readers && !lock->l_writers &&
 	    ktime_after(ktime_get(),
-			ktime_add(lock->l_last_used,
-				  ktime_set(ns->ns_dirty_age_limit, 0)))) {
+			ktime_add(lock->l_last_used, ns->ns_dirty_age_limit))) {
 		unlock_res_and_lock(lock);
 
 		/* For MDS glimpse it is always DOM lock, set corresponding

lustre/ldlm/ldlm_resource.c
@@ -458,7 +458,8 @@ static ssize_t dirty_age_limit_show(struct kobject *kobj,
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
 
-	return sprintf(buf, "%llu\n", ns->ns_dirty_age_limit);
+	return snprintf(buf, PAGE_SIZE, "%llu\n",
+			ktime_divns(ns->ns_dirty_age_limit, NSEC_PER_SEC));
 }
 
 static ssize_t dirty_age_limit_store(struct kobject *kobj,
@@ -472,7 +473,7 @@ static ssize_t dirty_age_limit_store(struct kobject *kobj,
 	if (kstrtoull(buffer, 10, &tmp))
 		return -EINVAL;
 
-	ns->ns_dirty_age_limit = tmp;
+	ns->ns_dirty_age_limit = ktime_set(tmp, 0);
 
 	return count;
 }
@@ -485,7 +486,7 @@ static ssize_t ctime_age_limit_show(struct kobject *kobj,
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
 
-	return sprintf(buf, "%llu\n", ns->ns_ctime_age_limit);
+	return snprintf(buf, PAGE_SIZE, "%u\n", ns->ns_ctime_age_limit);
 }
 
 static ssize_t ctime_age_limit_store(struct kobject *kobj,
@@ -494,9 +495,9 @@ static ssize_t ctime_age_limit_store(struct kobject *kobj,
 {
 	struct ldlm_namespace *ns = container_of(kobj, struct ldlm_namespace,
 						 ns_kobj);
-	unsigned long long tmp;
+	unsigned long tmp;
 
-	if (kstrtoull(buffer, 10, &tmp))
+	if (kstrtoul(buffer, 10, &tmp))
 		return -EINVAL;
 
 	ns->ns_ctime_age_limit = tmp;
@@ -915,7 +916,7 @@ struct ldlm_namespace *ldlm_namespace_new(struct obd_device *obd, char *name,
 	ns->ns_max_unused         = LDLM_DEFAULT_LRU_SIZE;
 	ns->ns_max_age            = ktime_set(LDLM_DEFAULT_MAX_ALIVE, 0);
 	ns->ns_ctime_age_limit    = LDLM_CTIME_AGE_LIMIT;
-	ns->ns_dirty_age_limit    = LDLM_DIRTY_AGE_LIMIT;
+	ns->ns_dirty_age_limit    = ktime_set(LDLM_DIRTY_AGE_LIMIT, 0);
 	ns->ns_timeouts           = 0;
 	ns->ns_orig_connect_flags = 0;
 	ns->ns_connect_flags      = 0;

lustre/llite/namei.c
@@ -416,8 +416,7 @@ int ll_md_need_convert(struct ldlm_lock *lock)
 	/* is lock is too old to be converted? */
 	lock_res_and_lock(lock);
 	if (ktime_after(ktime_get(),
-			ktime_add(lock->l_last_used,
-				  ktime_set(ns->ns_dirty_age_limit, 0)))) {
+			ktime_add(lock->l_last_used, ns->ns_dirty_age_limit))) {
 		unlock_res_and_lock(lock);
 		return 0;
 	}