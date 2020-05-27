lustre/lustre-release

commit b1f6f3becedc93004773eb1242d838827815743d

LU-9679 libcfs: Add CFS_ALLOC_PTR_ARRAY and free
Following the pattern of CFS_ALLOC_PTR() and the kernel
pattern of kmalloc_array(), add
  CFS_ALLOC_PTR_ARRAY()
and
  CFS_FREE_PTR_ARRAY()

which allocate and free arrays given a pointer to
the array, and a number of elements

This makes code easier to read and could be a step
toward using the kernel's hardened alloc_array interfaces,
which insure the multiplication doesn't overflow.

Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I54c919bd9ce22fbc72715c3da16f8c29e7135ccc
Reviewed-on: https://review.whamcloud.com/36975
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lnet/lnet/api-ni.c
@@ -622,7 +622,7 @@ lnet_create_remote_nets_table(void)
 
 	LASSERT(the_lnet.ln_remote_nets_hash == NULL);
 	LASSERT(the_lnet.ln_remote_nets_hbits > 0);
-	LIBCFS_ALLOC(hash, LNET_REMOTE_NETS_HASH_SIZE * sizeof(*hash));
+	CFS_ALLOC_PTR_ARRAY(hash, LNET_REMOTE_NETS_HASH_SIZE);
 	if (hash == NULL) {
 		CERROR("Failed to create remote nets hash table\n");
 		return -ENOMEM;
@@ -645,9 +645,8 @@ lnet_destroy_remote_nets_table(void)
 	for (i = 0; i < LNET_REMOTE_NETS_HASH_SIZE; i++)
 		LASSERT(list_empty(&the_lnet.ln_remote_nets_hash[i]));
 
-	LIBCFS_FREE(the_lnet.ln_remote_nets_hash,
-		    LNET_REMOTE_NETS_HASH_SIZE *
-		    sizeof(the_lnet.ln_remote_nets_hash[0]));
+	CFS_FREE_PTR_ARRAY(the_lnet.ln_remote_nets_hash,
+			   LNET_REMOTE_NETS_HASH_SIZE);
 	the_lnet.ln_remote_nets_hash = NULL;
 }
 
@@ -997,8 +996,7 @@ lnet_res_container_cleanup(struct lnet_res_container *rec)
 	}
 
 	if (rec->rec_lh_hash != NULL) {
-		LIBCFS_FREE(rec->rec_lh_hash,
-			    LNET_LH_HASH_SIZE * sizeof(rec->rec_lh_hash[0]));
+		CFS_FREE_PTR_ARRAY(rec->rec_lh_hash, LNET_LH_HASH_SIZE);
 		rec->rec_lh_hash = NULL;
 	}
 
@@ -4282,7 +4280,6 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 	int i;
 	int rc;
 	int max_intf = lnet_interfaces_max;
-	size_t buf_size;
 
 	if (n_ids <= 0 ||
 	    id.nid == LNET_NID_ANY)
@@ -4298,9 +4295,7 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 	if (n_ids > max_intf)
 		n_ids = max_intf;
 
-	buf_size = n_ids * sizeof(*buf);
-
-	LIBCFS_ALLOC(buf, buf_size);
+	CFS_ALLOC_PTR_ARRAY(buf, n_ids);
 	if (!buf)
 		return -ENOMEM;
 
@@ -4353,7 +4348,7 @@ out_decref:
 out:
 	lnet_net_unlock(cpt);
 
-	LIBCFS_FREE(buf, buf_size);
+	CFS_FREE_PTR_ARRAY(buf, n_ids);
 
 	return rc;
 }

libcfs/include/libcfs/libcfs_private.h
@@ -343,8 +343,13 @@ do {                                                            \
 #define LASSERT_ATOMIC_ZERO(a)                  LASSERT_ATOMIC_EQ(a, 0)
 #define LASSERT_ATOMIC_POS(a)                   LASSERT_ATOMIC_GT(a, 0)
 
-#define CFS_ALLOC_PTR(ptr)      LIBCFS_ALLOC(ptr, sizeof (*(ptr)));
-#define CFS_FREE_PTR(ptr)       LIBCFS_FREE(ptr, sizeof (*(ptr)));
+#define CFS_ALLOC_PTR(ptr)      LIBCFS_ALLOC(ptr, sizeof(*(ptr)));
+#define CFS_ALLOC_PTR_ARRAY(ptr, count)			\
+	LIBCFS_ALLOC(ptr, (count) * sizeof(*(ptr)))
+
+#define CFS_FREE_PTR(ptr)       LIBCFS_FREE(ptr, sizeof(*(ptr)));
+#define CFS_FREE_PTR_ARRAY(ptr, count)			\
+	LIBCFS_FREE(ptr, (count) * sizeof(*(ptr)))
 
 /* implication */
 #define ergo(a, b) (!(a) || (b))

libcfs/libcfs/libcfs_cpu.c
@@ -86,23 +86,21 @@ struct cfs_cpt_table *cfs_cpt_table_alloc(int ncpt)
 	if (!cptab->ctb_nodemask)
 		goto failed_alloc_nodemask;
 
-	LIBCFS_ALLOC(cptab->ctb_cpu2cpt,
-		     nr_cpu_ids * sizeof(cptab->ctb_cpu2cpt[0]));
+	CFS_ALLOC_PTR_ARRAY(cptab->ctb_cpu2cpt, nr_cpu_ids);
 	if (!cptab->ctb_cpu2cpt)
 		goto failed_alloc_cpu2cpt;
 
 	memset(cptab->ctb_cpu2cpt, -1,
 	       nr_cpu_ids * sizeof(cptab->ctb_cpu2cpt[0]));
 
-	LIBCFS_ALLOC(cptab->ctb_node2cpt,
-		     nr_node_ids * sizeof(cptab->ctb_node2cpt[0]));
+	CFS_ALLOC_PTR_ARRAY(cptab->ctb_node2cpt, nr_node_ids);
 	if (!cptab->ctb_node2cpt)
 		goto failed_alloc_node2cpt;
 
 	memset(cptab->ctb_node2cpt, -1,
 	       nr_node_ids * sizeof(cptab->ctb_node2cpt[0]));
 
-	LIBCFS_ALLOC(cptab->ctb_parts, ncpt * sizeof(cptab->ctb_parts[0]));
+	CFS_ALLOC_PTR_ARRAY(cptab->ctb_parts, ncpt);
 	if (!cptab->ctb_parts)
 		goto failed_alloc_ctb_parts;
 
@@ -118,8 +116,7 @@ struct cfs_cpt_table *cfs_cpt_table_alloc(int ncpt)
 		if (!part->cpt_nodemask)
 			goto failed_setting_ctb_parts;
 
-		LIBCFS_ALLOC(part->cpt_distance,
-			     cptab->ctb_nparts * sizeof(part->cpt_distance[0]));
+		CFS_ALLOC_PTR_ARRAY(part->cpt_distance, cptab->ctb_nparts);
 		if (!part->cpt_distance)
 			goto failed_setting_ctb_parts;
 
@@ -141,26 +138,22 @@ failed_setting_ctb_parts:
 		free_cpumask_var(part->cpt_cpumask);
 
 		if (part->cpt_distance) {
-			LIBCFS_FREE(part->cpt_distance,
-				cptab->ctb_nparts *
-					sizeof(part->cpt_distance[0]));
+			CFS_FREE_PTR_ARRAY(part->cpt_distance,
+					   cptab->ctb_nparts);
 		}
 	}
 
-	if (cptab->ctb_parts) {
-		LIBCFS_FREE(cptab->ctb_parts,
-			    cptab->ctb_nparts * sizeof(cptab->ctb_parts[0]));
-	}
+	if (cptab->ctb_parts)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_parts, cptab->ctb_nparts);
+
 failed_alloc_ctb_parts:
-	if (cptab->ctb_node2cpt) {
-		LIBCFS_FREE(cptab->ctb_node2cpt,
-			    nr_node_ids * sizeof(cptab->ctb_node2cpt[0]));
-	}
+	if (cptab->ctb_node2cpt)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_node2cpt, nr_node_ids);
+
 failed_alloc_node2cpt:
-	if (cptab->ctb_cpu2cpt) {
-		LIBCFS_FREE(cptab->ctb_cpu2cpt,
-			    nr_cpu_ids * sizeof(cptab->ctb_cpu2cpt[0]));
-	}
+	if (cptab->ctb_cpu2cpt)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_cpu2cpt, nr_cpu_ids);
+
 failed_alloc_cpu2cpt:
 	if (cptab->ctb_nodemask)
 		LIBCFS_FREE(cptab->ctb_nodemask, sizeof(*cptab->ctb_nodemask));
@@ -176,15 +169,11 @@ void cfs_cpt_table_free(struct cfs_cpt_table *cptab)
 {
 	int i;
 
-	if (cptab->ctb_cpu2cpt) {
-		LIBCFS_FREE(cptab->ctb_cpu2cpt,
-			    nr_cpu_ids * sizeof(cptab->ctb_cpu2cpt[0]));
-	}
+	if (cptab->ctb_cpu2cpt)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_cpu2cpt, nr_cpu_ids);
 
-	if (cptab->ctb_node2cpt) {
-		LIBCFS_FREE(cptab->ctb_node2cpt,
-			    nr_node_ids * sizeof(cptab->ctb_node2cpt[0]));
-	}
+	if (cptab->ctb_node2cpt)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_node2cpt, nr_node_ids);
 
 	for (i = 0; cptab->ctb_parts && i < cptab->ctb_nparts; i++) {
 		struct cfs_cpu_partition *part = &cptab->ctb_parts[i];
@@ -196,17 +185,13 @@ void cfs_cpt_table_free(struct cfs_cpt_table *cptab)
 
 		free_cpumask_var(part->cpt_cpumask);
 
-		if (part->cpt_distance) {
-			LIBCFS_FREE(part->cpt_distance,
-				cptab->ctb_nparts *
-					sizeof(part->cpt_distance[0]));
-		}
+		if (part->cpt_distance)
+			CFS_FREE_PTR_ARRAY(part->cpt_distance,
+					   cptab->ctb_nparts);
 	}
 
-	if (cptab->ctb_parts) {
-		LIBCFS_FREE(cptab->ctb_parts,
-			    cptab->ctb_nparts * sizeof(cptab->ctb_parts[0]));
-	}
+	if (cptab->ctb_parts)
+		CFS_FREE_PTR_ARRAY(cptab->ctb_parts, cptab->ctb_nparts);
 
 	if (cptab->ctb_nodemask)
 		LIBCFS_FREE(cptab->ctb_nodemask, sizeof(*cptab->ctb_nodemask));

libcfs/libcfs/libcfs_string.c
@@ -475,7 +475,7 @@ cfs_expr_list_values(struct cfs_expr_list *expr_list, int max, __u32 **valpp)
 		return -EINVAL;
 	}
 
-	LIBCFS_ALLOC(val, sizeof(val[0]) * count);
+	CFS_ALLOC_PTR_ARRAY(val, count);
 	if (val == NULL)
 		return -ENOMEM;
 
@@ -498,7 +498,7 @@ cfs_expr_list_values_free(__u32 *values, int num)
 	/* This array is allocated by LIBCFS_ALLOC(), so it shouldn't be freed
 	 * by OBD_FREE() if it's called by module other than libcfs & LNet,
 	 * otherwise we will see fake memory leak */
-	LIBCFS_FREE(values, num * sizeof(values[0]));
+	CFS_FREE_PTR_ARRAY(values, num);
 }
 EXPORT_SYMBOL(cfs_expr_list_values_free);
 

lnet/klnds/gnilnd/gnilnd.c
@@ -576,7 +576,7 @@ kgnilnd_peer_notify(kgn_peer_t *peer, int error, int alive)
 			return;
 		}
 
-		LIBCFS_ALLOC(nets, nnets * sizeof(*nets));
+		CFS_ALLOC_PTR_ARRAY(nets, nnets);
 
 		if (nets == NULL) {
 			up_read(&kgnilnd_data.kgn_net_rw_sem);
@@ -613,7 +613,7 @@ kgnilnd_peer_notify(kgn_peer_t *peer, int error, int alive)
 			kgnilnd_net_decref(net);
 		}
 
-		LIBCFS_FREE(nets, nnets * sizeof(*nets));
+		CFS_FREE_PTR_ARRAY(nets, nnets);
 	}
 }
 
@@ -2192,8 +2192,8 @@ int kgnilnd_base_startup(void)
 		init_rwsem(&dev->gnd_conn_sem);
 
 		/* alloc & setup nid based dgram table */
-		LIBCFS_ALLOC(dev->gnd_dgrams,
-			    sizeof(struct list_head) * *kgnilnd_tunables.kgn_peer_hash_size);
+		CFS_ALLOC_PTR_ARRAY(dev->gnd_dgrams,
+				    *kgnilnd_tunables.kgn_peer_hash_size);
 
 		if (dev->gnd_dgrams == NULL)
 			GOTO(failed, rc = -ENOMEM);
@@ -2236,8 +2236,8 @@ int kgnilnd_base_startup(void)
 
 	rwlock_init(&kgnilnd_data.kgn_peer_conn_lock);
 
-	LIBCFS_ALLOC(kgnilnd_data.kgn_peers,
-		    sizeof(struct list_head) * *kgnilnd_tunables.kgn_peer_hash_size);
+	CFS_ALLOC_PTR_ARRAY(kgnilnd_data.kgn_peers,
+			    *kgnilnd_tunables.kgn_peer_hash_size);
 
 	if (kgnilnd_data.kgn_peers == NULL)
 		GOTO(failed, rc = -ENOMEM);
@@ -2246,8 +2246,8 @@ int kgnilnd_base_startup(void)
 		INIT_LIST_HEAD(&kgnilnd_data.kgn_peers[i]);
 	}
 
-	LIBCFS_ALLOC(kgnilnd_data.kgn_conns,
-		    sizeof(struct list_head) * *kgnilnd_tunables.kgn_peer_hash_size);
+	CFS_ALLOC_PTR_ARRAY(kgnilnd_data.kgn_conns,
+			    *kgnilnd_tunables.kgn_peer_hash_size);
 
 	if (kgnilnd_data.kgn_conns == NULL)
 		GOTO(failed, rc = -ENOMEM);
@@ -2256,8 +2256,8 @@ int kgnilnd_base_startup(void)
 		INIT_LIST_HEAD(&kgnilnd_data.kgn_conns[i]);
 	}
 
-	LIBCFS_ALLOC(kgnilnd_data.kgn_nets,
-		    sizeof(struct list_head) * *kgnilnd_tunables.kgn_net_hash_size);
+	CFS_ALLOC_PTR_ARRAY(kgnilnd_data.kgn_nets,
+			    *kgnilnd_tunables.kgn_net_hash_size);
 
 	if (kgnilnd_data.kgn_nets == NULL)
 		GOTO(failed, rc = -ENOMEM);
@@ -2524,9 +2524,8 @@ kgnilnd_base_shutdown(void)
 		for (i = 0; i < *kgnilnd_tunables.kgn_peer_hash_size; i++)
 			LASSERT(list_empty(&kgnilnd_data.kgn_peers[i]));
 
-		LIBCFS_FREE(kgnilnd_data.kgn_peers,
-			    sizeof (struct list_head) *
-			    *kgnilnd_tunables.kgn_peer_hash_size);
+		CFS_FREE_PTRE_ARRAT(kgnilnd_data.kgn_peers,
+				    *kgnilnd_tunables.kgn_peer_hash_size);
 	}
 
 	down_write(&kgnilnd_data.kgn_net_rw_sem);
@@ -2534,9 +2533,8 @@ kgnilnd_base_shutdown(void)
 		for (i = 0; i < *kgnilnd_tunables.kgn_net_hash_size; i++)
 			LASSERT(list_empty(&kgnilnd_data.kgn_nets[i]));
 
-		LIBCFS_FREE(kgnilnd_data.kgn_nets,
-			    sizeof (struct list_head) *
-			    *kgnilnd_tunables.kgn_net_hash_size);
+		CFS_FREE_PTRE_ARRAY(kgnilnd_data.kgn_nets,
+				    *kgnilnd_tunables.kgn_net_hash_size);
 	}
 	up_write(&kgnilnd_data.kgn_net_rw_sem);
 
@@ -2547,9 +2545,8 @@ kgnilnd_base_shutdown(void)
 		for (i = 0; i < *kgnilnd_tunables.kgn_peer_hash_size; i++)
 			LASSERT(list_empty(&kgnilnd_data.kgn_conns[i]));
 
-		LIBCFS_FREE(kgnilnd_data.kgn_conns,
-			    sizeof (struct list_head) *
-			    *kgnilnd_tunables.kgn_peer_hash_size);
+		CFS_FREE_PTR_ARRAY(kgnilnd_data.kgn_conns,
+				   *kgnilnd_tunables.kgn_peer_hash_size);
 	}
 
 	for (i = 0; i < kgnilnd_data.kgn_ndevs; i++) {
@@ -2560,12 +2557,12 @@ kgnilnd_base_shutdown(void)
 			"dgrams left %d\n", atomic_read(&dev->gnd_ndgrams));
 
 		if (dev->gnd_dgrams != NULL) {
-			for (i = 0; i < *kgnilnd_tunables.kgn_peer_hash_size; i++)
+			for (i = 0; i < *kgnilnd_tunables.kgn_peer_hash_size;
+			     i++)
 				LASSERT(list_empty(&dev->gnd_dgrams[i]));
 
-			LIBCFS_FREE(dev->gnd_dgrams,
-				    sizeof (struct list_head) *
-				    *kgnilnd_tunables.kgn_peer_hash_size);
+			CFS_FREE_PTR_ARRAY(dev->gnd_dgrams,
+					   *kgnilnd_tunables.kgn_peer_hash_size);
 		}
 
 		kgnilnd_free_phys_fmablk(dev);

lnet/klnds/gnilnd/gnilnd_conn.c
@@ -190,7 +190,7 @@ kgnilnd_alloc_fmablk(kgn_device_t *device, int use_phys)
 	}
 
 	/* allocate just enough space for the bits to track the mailboxes */
-	LIBCFS_ALLOC(fma_blk->gnm_bit_array, BITS_TO_LONGS(num_mbox) * sizeof(unsigned long));
+	CFS_ALLOC_PTR_ARRAY(fma_blk->gnm_bit_array, BITS_TO_LONGS(num_mbox));
 	if (fma_blk->gnm_bit_array == NULL) {
 		CNETERR("could not allocate mailbox bitmask, %lu bytes for %d mbox\n",
 		       sizeof(unsigned long) * BITS_TO_LONGS(num_mbox), num_mbox);
@@ -199,8 +199,10 @@ kgnilnd_alloc_fmablk(kgn_device_t *device, int use_phys)
 	}
 	bitmap_zero(fma_blk->gnm_bit_array, num_mbox);
 
-	/* now that the num_mbox is set based on allocation type, get debug info setup */
-	LIBCFS_ALLOC(fma_blk->gnm_mbox_info, sizeof(kgn_mbox_info_t) * num_mbox);
+	/* now that the num_mbox is set based on allocation type, get debug
+	 * info setup
+	 * */
+	CFS_ALLOC_PTR_ARRAY(fma_blk->gnm_mbox_info, num_mbox);
 	if (fma_blk->gnm_mbox_info == NULL) {
 		CNETERR("could not allocate mailbox debug, %lu bytes for %d mbox\n",
 		       sizeof(kgn_mbox_info_t) * num_mbox, num_mbox);
@@ -238,9 +240,9 @@ kgnilnd_alloc_fmablk(kgn_device_t *device, int use_phys)
 	return 0;
 
 free_info:
-	LIBCFS_FREE(fma_blk->gnm_mbox_info, sizeof(kgn_mbox_info_t)*num_mbox);
+	CFS_FREE_PTR_ARRAY(fma_blk->gnm_mbox_info, num_mbox);
 free_bit:
-	LIBCFS_FREE(fma_blk->gnm_bit_array, BITS_TO_LONGS(num_mbox) * sizeof (unsigned long));
+	CFS_FREE_PTR_ARRAY(fma_blk->gnm_bit_array, BITS_TO_LONGS(num_mbox));
 free_blk:
 	if (fma_blk->gnm_state == GNILND_FMABLK_VIRT) {
 		kgnilnd_vfree(fma_blk->gnm_block, fma_blk->gnm_blk_size);
@@ -353,8 +355,9 @@ kgnilnd_free_fmablk_locked(kgn_device_t *dev, kgn_fma_memblock_t *fma_blk)
 
 	list_del(&fma_blk->gnm_bufflist);
 
-	LIBCFS_FREE(fma_blk->gnm_mbox_info, sizeof(kgn_mbox_info_t)*fma_blk->gnm_num_mboxs);
-	LIBCFS_FREE(fma_blk->gnm_bit_array, BITS_TO_LONGS(fma_blk->gnm_num_mboxs) * sizeof (unsigned long));
+	CFS_FREE_PTR_ARRAY(fma_blk->gnm_mbox_info, fma_blk->gnm_num_mboxs);
+	CFS_FREE_PTR_ARRAY(fma_blk->gnm_bit_array,
+			   BITS_TO_LONGS(fma_blk->gnm_num_mboxs));
 	LIBCFS_FREE(fma_blk, sizeof(kgn_fma_memblock_t));
 }
 

lnet/klnds/gnilnd/gnilnd_proc.c
@@ -48,8 +48,8 @@ _kgnilnd_proc_run_cksum_test(int caseno, int nloops, int nob)
 	__u16                    cksum, cksum2;
 	__u64                    mbytes;
 
-	LIBCFS_ALLOC(src, LNET_MAX_IOV * sizeof(lnet_kiov_t));
-	LIBCFS_ALLOC(dest, LNET_MAX_IOV * sizeof(lnet_kiov_t));
+	CFS_ALLOC_PTR_ARRAY(src, LNET_MAX_IOV);
+	CFS_ALLOC_PTR_ARRAY(dest, LNET_MAX_IOV);
 
 	if (src == NULL || dest == NULL) {
 		CERROR("couldn't allocate iovs\n");
@@ -150,9 +150,9 @@ unwind:
 	}
 
 	if (src != NULL)
-		LIBCFS_FREE(src, LNET_MAX_IOV * sizeof(lnet_kiov_t));
+		CFS_FREE_PTR_ARRAY(src, LNET_MAX_IOV);
 	if (dest != NULL)
-		LIBCFS_FREE(dest, LNET_MAX_IOV * sizeof(lnet_kiov_t));
+		CFS_FREE_PTR_ARRAY(dest, LNET_MAX_IOV);
 	return rc;
 }
 

lnet/klnds/o2iblnd/o2iblnd.c
@@ -1050,10 +1050,8 @@ kiblnd_destroy_conn(struct kib_conn *conn)
 	if (conn->ibc_rx_pages != NULL)
 		kiblnd_unmap_rx_descs(conn);
 
-	if (conn->ibc_rxs != NULL) {
-		LIBCFS_FREE(conn->ibc_rxs,
-			    IBLND_RX_MSGS(conn) * sizeof(struct kib_rx));
-	}
+	if (conn->ibc_rxs != NULL)
+		CFS_FREE_PTR_ARRAY(conn->ibc_rxs, IBLND_RX_MSGS(conn));
 
 	if (conn->ibc_connvars != NULL)
 		LIBCFS_FREE(conn->ibc_connvars, sizeof(*conn->ibc_connvars));
@@ -2228,37 +2226,32 @@ kiblnd_destroy_tx_pool(struct kib_pool *pool)
         if (tpo->tpo_tx_descs == NULL)
                 goto out;
 
-        for (i = 0; i < pool->po_size; i++) {
+	for (i = 0; i < pool->po_size; i++) {
 		struct kib_tx *tx = &tpo->tpo_tx_descs[i];
 		int	  wrq_sge = *kiblnd_tunables.kib_wrq_sge;
 
 		list_del(&tx->tx_list);
-                if (tx->tx_pages != NULL)
-                        LIBCFS_FREE(tx->tx_pages,
-                                    LNET_MAX_IOV *
-                                    sizeof(*tx->tx_pages));
-                if (tx->tx_frags != NULL)
-                        LIBCFS_FREE(tx->tx_frags,
-				    (1 + IBLND_MAX_RDMA_FRAGS) *
-				    sizeof(*tx->tx_frags));
-                if (tx->tx_wrq != NULL)
-                        LIBCFS_FREE(tx->tx_wrq,
-                                    (1 + IBLND_MAX_RDMA_FRAGS) *
-                                    sizeof(*tx->tx_wrq));
+		if (tx->tx_pages != NULL)
+			CFS_FREE_PTR_ARRAY(tx->tx_pages, LNET_MAX_IOV);
+		if (tx->tx_frags != NULL)
+			CFS_FREE_PTR_ARRAY(tx->tx_frags,
+					   (1 + IBLND_MAX_RDMA_FRAGS));
+		if (tx->tx_wrq != NULL)
+			CFS_FREE_PTR_ARRAY(tx->tx_wrq,
+					   (1 + IBLND_MAX_RDMA_FRAGS));
 		if (tx->tx_sge != NULL)
-			LIBCFS_FREE(tx->tx_sge,
-				    (1 + IBLND_MAX_RDMA_FRAGS) * wrq_sge *
-				    sizeof(*tx->tx_sge));
-                if (tx->tx_rd != NULL)
-                        LIBCFS_FREE(tx->tx_rd,
+			CFS_FREE_PTR_ARRAY(tx->tx_sge,
+					   (1 + IBLND_MAX_RDMA_FRAGS) *
+					   wrq_sge);
+		if (tx->tx_rd != NULL)
+			LIBCFS_FREE(tx->tx_rd,
 				    offsetof(struct kib_rdma_desc,
-                                             rd_frags[IBLND_MAX_RDMA_FRAGS]));
-        }
+					     rd_frags[IBLND_MAX_RDMA_FRAGS]));
+	}
 
-        LIBCFS_FREE(tpo->tpo_tx_descs,
-		    pool->po_size * sizeof(struct kib_tx));
+	CFS_FREE_PTR_ARRAY(tpo->tpo_tx_descs, pool->po_size);
 out:
-        kiblnd_fini_pool(pool);
+	kiblnd_fini_pool(pool);
 	CFS_FREE_PTR(tpo);
 }
 
@@ -2995,11 +2988,9 @@ kiblnd_base_shutdown(void)
                 break;
         }
 
-	if (kiblnd_data.kib_peers != NULL) {
-		LIBCFS_FREE(kiblnd_data.kib_peers,
-			    sizeof(struct list_head) *
-			    kiblnd_data.kib_peer_hash_size);
-	}
+	if (kiblnd_data.kib_peers)
+		CFS_FREE_PTR_ARRAY(kiblnd_data.kib_peers,
+				   kiblnd_data.kib_peer_hash_size);
 
 	if (kiblnd_data.kib_scheds != NULL)
 		cfs_percpt_free(kiblnd_data.kib_scheds);
@@ -3104,9 +3095,8 @@ kiblnd_base_startup(struct net *ns)
 	INIT_LIST_HEAD(&kiblnd_data.kib_failed_devs);
 
 	kiblnd_data.kib_peer_hash_size = IBLND_PEER_HASH_SIZE;
-	LIBCFS_ALLOC(kiblnd_data.kib_peers,
-		     sizeof(struct list_head) *
-		     kiblnd_data.kib_peer_hash_size);
+	CFS_ALLOC_PTR_ARRAY(kiblnd_data.kib_peers,
+			    kiblnd_data.kib_peer_hash_size);
 	if (kiblnd_data.kib_peers == NULL)
 		goto failed;
 

commit 45722de60a8fb5fee86a07ce5e3861b31dcedf59

LU-9679 lnet: tidy lnet_discover and fix mem accounting bug.
A recent patch introduce a memory accounting bug because "n_ids"
can change between the ALLOC call and the FREE call.

With this patch we fix that by ensuring n_ids doesn't change - the
current change is not needed.
Also:
 - discard 'max_intf' var.  It is always exactly lnet_interfaces_max,
   so just use that directly.
 - only copy back the number of interfaces found
 - report the number of interfaces actually copied.
 - Move the copy_to_user until after all locks and references are
   dropped so there is no need to re-take any locks.

Test-Parameters: trivial
Fixes: b1f6f3becedc ("LU-9679 libcfs: Add CFS_ALLOC_PTR_ARRAY and free")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Ib807ca4ce5235b28e7ae11d90e1942aff4454cfc
Reviewed-on: https://review.whamcloud.com/38644
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -4257,7 +4257,6 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 	int cpt;
 	int i;
 	int rc;
-	int max_intf = lnet_interfaces_max;
 
 	if (n_ids <= 0 ||
 	    id.nid == LNET_NID_ANY)
@@ -4267,11 +4266,11 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 		id.pid = LNET_PID_LUSTRE;
 
 	/*
-	 * if the user buffer has more space than the max_intf
-	 * then only fill it up to max_intf
+	 * If the user buffer has more space than the lnet_interfaces_max,
+	 * then only fill it up to lnet_interfaces_max.
 	 */
-	if (n_ids > max_intf)
-		n_ids = max_intf;
+	if (n_ids > lnet_interfaces_max)
+		n_ids = lnet_interfaces_max;
 
 	CFS_ALLOC_PTR_ARRAY(buf, n_ids);
 	if (!buf)
@@ -4299,11 +4298,6 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 	if (rc)
 		goto out_decref;
 
-	/* Peer may have changed. */
-	lp = lpni->lpni_peer_net->lpn_peer;
-	if (lp->lp_nnis < n_ids)
-		n_ids = lp->lp_nnis;
-
 	i = 0;
 	p = NULL;
 	while ((p = lnet_get_next_peer_ni_locked(lp, NULL, p)) != NULL) {
@@ -4312,20 +4306,16 @@ lnet_discover(struct lnet_process_id id, __u32 force,
 		if (++i >= n_ids)
 			break;
 	}
+	rc = i;
 
-	lnet_net_unlock(cpt);
-
-	rc = -EFAULT;
-	if (copy_to_user(ids, buf, n_ids * sizeof(*buf)))
-		goto out_relock;
-	rc = n_ids;
-out_relock:
-	lnet_net_lock(cpt);
 out_decref:
 	lnet_peer_ni_decref_locked(lpni);
 out:
 	lnet_net_unlock(cpt);
 
+	if (rc >= 0)
+		if (copy_to_user(ids, buf, rc * sizeof(*buf)))
+			rc = -EFAULT;
 	CFS_FREE_PTR_ARRAY(buf, n_ids);
 
 	return rc;