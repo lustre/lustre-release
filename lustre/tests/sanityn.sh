051a2b9629f9542850cb7c79cfe06c5eac511569
Sebastien Buisson <sbuisson@ddn.com>
2020-06-03 11:31:20 +0200

    LU-13628 tests: add sanityn test_106 to ALWAYS_EXCEPT
    sanityn test_106 fails on CentOS 8 and Ubuntu 18, and is skipped
    on all other distros because of lack of support for statx.
    
    Test-Parameters: trivial
    Test-Parameters: clientdistro=el8.1 testlist=sanityn
    Test-Parameters: clientdistro=ubuntu1804 testlist=sanityn
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Change-Id: I370585138bbd05d1e4ea8f323c74659145fe7dec
    Reviewed-on: https://review.whamcloud.com/38815
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -18,8 +18,8 @@ init_test_env $@
 init_logging
 
 ALWAYS_EXCEPT="$SANITYN_EXCEPT "
-# bug number for skipped test:  LU-7105
-ALWAYS_EXCEPT+="                28 "
+# bug number for skipped test:  LU-7105 LU-13628
+ALWAYS_EXCEPT+="                28      106"
 # UPDATE THE COMMENT ABOVE WITH BUG NUMBERS WHEN CHANGING ALWAYS_EXCEPT!
 
 # skip tests for PPC until they are fixed
