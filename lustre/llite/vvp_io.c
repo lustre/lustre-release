lustre/lustre-release

commit 742897a967cff5be53c447d14b17ae405c2b31f2

LU-13274 uapi: make lnet UAPI headers C99 compliant
Attempting to compile strict C99 user land applications or
libraries with the Lustre UAPI headers will fail. These same
errors can be seen by enabling CONFIG_UAPI_HEADER_TEST as well.
Update the LNet UAPI headers to be compilable with -std=c99.
Add LNet test covering UAPI header handling.

Test-Parameters: trivial testlist=sanity-lnet

Change-Id: Ic1c30ed2d4d9d1d7f11b657d3817a05161367e13
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37971
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/Makefile.am
@@ -67,6 +67,7 @@ lctl_LDADD :=  liblustreapi.la $(PTHREAD_LIBS) -lyaml
 lctl_DEPENDENCIES := liblustreapi.la
 
 lfs_SOURCES = lfs.c lfs_project.c lfs_project.h
+lfs_CFLAGS := -fPIC -I $(top_builddir)/lnet/utils
 lfs_LDADD := liblustreapi.la -lz
 lfs_LDADD += $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la
 lfs_DEPENDENCIES := liblustreapi.la

lnet/utils/Makefile.am
@@ -44,6 +44,7 @@ routerstat_SOURCES = routerstat.c
 routerstat_LDADD = $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la
 
 lst_SOURCES = lst.c
+lst_CFLAGS = -fPIC -D_LINUX_TIME_H -D_GNU_SOURCE
 lst_LDADD = $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la \
 	    $(LIBEFENCE)
 

lnet/utils/lnetconfig/Makefile.am
@@ -29,10 +29,9 @@
 lib_LTLIBRARIES = liblnetconfig.la
 
 liblnetconfig_la_SOURCES  = liblnetconfig.c liblnetconfig.h \
-			    liblnetconfig_lnd.c liblnd.h cyaml.c \
-			    $(top_builddir)/lnet/include/cyaml.h
+			    liblnetconfig_lnd.c liblnd.h cyaml.c cyaml.h
 liblnetconfig_la_CPPFLAGS = -D_LARGEFILE64_SOURCE=1 -D_FILE_OFFSET_BITS=64 \
-			    -DLUSTRE_UTILS=1 -I$(top_builddir)/lnet/include
+			    -DLUSTRE_UTILS=1 -fPIC
 liblnetconfig_la_LDFLAGS = -L$(top_builddir)/libcfs/libcfs -lyaml -lm \
 			   $(LIBREADLINE) -version-info 4:0:0
 liblnetconfig_la_LIBADD = $(top_builddir)/libcfs/libcfs/libcfs.la

Makefile.in
@@ -1,4 +1,4 @@
-obj-m += @LDISKFS_SUBDIR@/ @LIBCFS_SUBDIR@/ lnet/
+obj-m += @LDISKFS_SUBDIR@/ libcfs/ lnet/
 obj-m += lustre/
 
 @INCLUDE_RULES@

autoMakefile.am
@@ -1,15 +1,15 @@
 SUBDIRS := @LDISKFS_SUBDIR@ \
 	. \
 	@LUSTREIOKIT_SUBDIR@ \
-	@LIBCFS_SUBDIR@ \
 	@SNMP_SUBDIR@ \
+	libcfs \
 	lnet \
 	lustre
 
 DIST_SUBDIRS := ldiskfs \
 	lustre-iokit \
-	@LIBCFS_SUBDIR@ \
 	@SNMP_DIST_SUBDIR@ \
+	libcfs \
 	lnet \
 	lustre \
 	config

config/lustre-build.m4
@@ -103,19 +103,6 @@ AC_SUBST(LUSTREIOKIT_SUBDIR)
 AM_CONDITIONAL([BUILD_LUSTREIOKIT], [test "x$enable_iokit" = xyes])
 ]) # LB_PATH_LUSTREIOKIT
 
-# Define no libcfs by default.
-AC_DEFUN([LB_LIBCFS_DIR], [
-AS_IF([test "x$libcfs_is_module" = xyes], [
-		LIBCFS_INCLUDE_DIR="libcfs/include"
-		LIBCFS_SUBDIR="libcfs"
-	], [
-		LIBCFS_INCLUDE_DIR="lnet/include"
-		LIBCFS_SUBDIR=""
-	])
-AC_SUBST(LIBCFS_INCLUDE_DIR)
-AC_SUBST(LIBCFS_SUBDIR)
-]) # LB_LIBCFS_DIR
-
 #
 # LB_LIBMOUNT
 #
@@ -329,13 +316,14 @@ AS_IF([test $target_cpu = powerpc64], [
 	CC="$CC -m64"
 ])
 
-CPPFLAGS="-I$PWD/$LIBCFS_INCLUDE_DIR -I$PWD/lnet/include -I$PWD/lnet/include/uapi -I$PWD/lustre/include -I$PWD/lustre/include/uapi $CPPFLAGS"
+# UAPI headers, libcfs/include for util headers, lustre/include for liblustreapi and friends
+CPPFLAGS="-I$PWD/libcfs/include -I$PWD/lnet/include/uapi -I$PWD/lustre/include -I$PWD/lustre/include/uapi $CPPFLAGS"
 
 CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
 
-# everyone builds against lnet and lustre
-EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/$LIBCFS_INCLUDE_DIR -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include"
+# everyone builds against lnet and lustre kernel headers
+EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include"
 AC_SUBST(EXTRA_KCFLAGS)
 ]) # LB_PROG_CC
 
@@ -608,8 +596,6 @@ LB_CONFIG_DIST
 
 LB_DOWNSTREAM_RELEASE
 LB_USES_DPKG
-
-LB_LIBCFS_DIR
 
 LB_INCLUDE_RULES
 

libcfs/include/libcfs/util/ioctl.h
@@ -43,7 +43,7 @@
 /* Sparse annotation. */
 #define __user
 
-#include <uapi/linux/lnet/libcfs_ioctl.h>
+#include <linux/lnet/libcfs_ioctl.h>
 
 #define LIBCFS_IOC_INIT(data)					\
 do {								\

lnet/include/uapi/linux/lnet/libcfs_ioctl.h
@@ -40,6 +40,13 @@
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
+/*
+ * sparse kernel source annotations
+ */
+#ifndef __user
+#define __user
+#endif
+
 #define LIBCFS_IOCTL_VERSION 0x0001000a
 #define LIBCFS_IOCTL_VERSION2 0x0001000b
 

commit 57bb3024613831d5d7f8f085bb0c7ebc33796790

LU-13673 misc: quiet compiler warning on armv7l
Add AM_CFLAGS when building lfs.c and lfs_project.c.

Avoid overflow in jobid_get_from_environ() and lu_prandom_u64_max().

Quiet printk() warning for mismatched type of size_t variables
by using %z modifier for those variables.

Fixes: 742897a967cf ("LU-13274 uapi: make lnet UAPI headers C99 compliant")
Fixes: df21a3b9eb01 ("LU-12790 obdclass: print jobid error message properly")
Fixes: 645b72c5c058 ("LU-12495 obdclass: generate random u64 max correctly")
Test-Parameters: trivial
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ib424497d1543fd7840d630facf3210b6fa1344c7
Reviewed-on: https://review.whamcloud.com/38927
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/vvp_io.c
@@ -829,7 +829,7 @@ static int vvp_io_read_start(const struct lu_env *env,
 		GOTO(out, result);
 
 	LU_OBJECT_HEADER(D_INODE, env, &obj->co_lu,
-			 "Read ino %lu, %lu bytes, offset %lld, size %llu\n",
+			 "Read ino %lu, %zu bytes, offset %lld, size %llu\n",
 			 inode->i_ino, cnt, pos, i_size_read(inode));
 
 	/* turn off the kernel's read-ahead */
@@ -1309,9 +1309,8 @@ static int vvp_io_write_start(const struct lu_env *env,
 		}
 	}
 	if (vio->vui_iocb->ki_pos != (pos + io->ci_nob - nob)) {
-		CDEBUG(D_VFSTRACE, "%s: write position mismatch: "
-		       "ki_pos %lld vs. pos %lld, written %ld, commit %ld "
-		       "rc %ld\n",
+		CDEBUG(D_VFSTRACE,
+		       "%s: write position mismatch: ki_pos %lld vs. pos %lld, written %zd, commit %zd: rc = %zd\n",
 		       file_dentry(file)->d_name.name,
 		       vio->vui_iocb->ki_pos, pos + io->ci_nob - nob,
 		       written, io->ci_nob - nob, result);

lustre/obdclass/jobid.c
@@ -238,7 +238,7 @@ int jobid_get_from_environ(char *jobid_var, char *jobid, int *jobid_len)
 		if (unlikely(ktime_to_ns(printed) == 0 ||
 			     ktime_after(ktime_get(),
 					 ktime_add_ns(printed,
-						      3600*24*NSEC_PER_SEC)))) {
+					     3600ULL * 24 * NSEC_PER_SEC)))) {
 			LCONSOLE_WARN("jobid: '%s' value too large (%d)\n",
 				      obd_jobid_var, *jobid_len);
 			printed = ktime_get();

lustre/obdclass/lu_tgt_descs.c
@@ -64,7 +64,7 @@ u64 lu_prandom_u64_max(u64 ep_ro)
 		 * 32 bits (truncated to the upper limit, if needed)
 		 */
 		if (ep_ro > 0xffffffffULL)
-			rand = prandom_u32_max((u32)(ep_ro >> 32)) << 32;
+			rand = (u64)prandom_u32_max((u32)(ep_ro >> 32)) << 32;
 
 		if (rand == (ep_ro & 0xffffffff00000000ULL))
 			rand |= prandom_u32_max((u32)ep_ro);

lustre/utils/Makefile.am
@@ -68,7 +68,7 @@ lctl_LDADD :=  liblustreapi.la $(PTHREAD_LIBS) -lyaml
 lctl_DEPENDENCIES := liblustreapi.la
 
 lfs_SOURCES = lfs.c lfs_project.c lfs_project.h
-lfs_CFLAGS := -fPIC -I $(top_builddir)/lnet/utils
+lfs_CFLAGS := -fPIC $(AM_CFLAGS) -I $(top_builddir)/lnet/utils
 lfs_LDADD := liblustreapi.la -lz
 lfs_LDADD += $(top_builddir)/lnet/utils/lnetconfig/liblnetconfig.la
 lfs_DEPENDENCIES := liblustreapi.la