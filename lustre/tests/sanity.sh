lustre/lustre-release

commit 9fe4b52ad2ffadf125d9b5c78bb2ff9a01725707

LU-1030 osc: new IO engine implementation
New IO engine to manage dirty pages with osc_extent.

Osc_extent is a data structure to manage a series of contiguous
blocks; however, the pages in an extent is not required to be
contiguous. An extent must be written out in one RPC.

The purpose of introducing extents are:
1. make grants work for extent-based OSD;
2. form better IO by picking up contiguous pages to compose RPC;
3. reimplement ll_writepages() with CIT_FSYNC.

Signed-off-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
Change-Id: I3ef619c1f07eefd201236ab55e5fd858791d41e0
Reviewed-on: http://review.whamcloud.com/2270
Reviewed-by: Johann Lombardi <johann@whamcloud.com>
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/tests/sanity.sh
@@ -3032,6 +3032,29 @@ test_48e() { # bug 4134
 }
 run_test 48e "Access to recreated parent subdir (should return errors)"
 
+test_49() { # LU-1030
+	# get ost1 size - lustre-OST0000
+	ost1_size=$(do_facet ost1 lfs df |grep ${ost1_svc} |awk '{print $4}')
+	# write 800M at maximum
+	[ $ost1_size -gt 819200 ] && ost1_size=819200
+
+	lfs setstripe -c 1 -i 0 $DIR/$tfile
+	dd if=/dev/zero of=$DIR/$tfile bs=4k count=$((ost1_size >> 2)) &
+	local dd_pid=$!
+
+	# change max_pages_per_rpc while writing the file
+	local osc1_mppc=osc.$(get_osc_import_name client ost1).max_pages_per_rpc
+	local orig_mppc=`$LCTL get_param -n $osc1_mppc`
+	# loop until dd process exits
+	while ps ax -opid | grep -q $dd_pid; do
+		$LCTL set_param $osc1_mppc=$((RANDOM % 256 + 1))
+		sleep $((RANDOM % 5 + 1))
+	done
+	# restore original max_pages_per_rpc
+	$LCTL set_param $osc1_mppc=$orig_mppc
+}
+run_test 49 "Change max_pages_per_rpc won't break osc extent"
+
 test_50() {
 	# bug 1485
 	mkdir $DIR/d50

libcfs/include/libcfs/linux/libcfs.h
@@ -62,6 +62,7 @@
 #include <linux/types.h>
 #include <asm/timex.h>
 #include <linux/sched.h> /* THREAD_SIZE */
+#include <linux/rbtree.h>
 
 #define CFS_THREAD_SIZE   THREAD_SIZE
 #define LUSTRE_TRACE_SIZE (THREAD_SIZE >> 5)

libcfs/include/libcfs/posix/libcfs.h
@@ -415,6 +415,78 @@ static inline void radix_tree_preload_end(void)
 {
 }
 
+/***************************************************************************
+ *
+ * Linux kernel red black tree emulation.
+ *
+ ***************************************************************************/
+struct rb_node {
+	unsigned long  rb_parent_color;
+#define	RB_RED		0
+#define	RB_BLACK	1
+	struct rb_node *rb_right;
+	struct rb_node *rb_left;
+};
+
+struct rb_root {
+	struct rb_node *rb_node;
+};
+
+
+#define rb_parent(r)   ((struct rb_node *)((r)->rb_parent_color & ~3))
+#define rb_color(r)   ((r)->rb_parent_color & 1)
+#define rb_is_red(r)   (!rb_color(r))
+#define rb_is_black(r) rb_color(r)
+#define rb_set_red(r)  do { (r)->rb_parent_color &= ~1; } while (0)
+#define rb_set_black(r)  do { (r)->rb_parent_color |= 1; } while (0)
+
+static inline void rb_set_parent(struct rb_node *rb, struct rb_node *p)
+{
+	rb->rb_parent_color = (rb->rb_parent_color & 3) | (unsigned long)p;
+}
+static inline void rb_set_color(struct rb_node *rb, int color)
+{
+	rb->rb_parent_color = (rb->rb_parent_color & ~1) | color;
+}
+
+#define RB_ROOT	((struct rb_root) { NULL, })
+#define rb_entry(ptr, type, member) container_of(ptr, type, member)
+
+#define RB_EMPTY_ROOT(root)	((root)->rb_node == NULL)
+#define RB_EMPTY_NODE(node)	(rb_parent(node) == node)
+#define RB_CLEAR_NODE(node)	(rb_set_parent(node, node))
+
+static inline void rb_init_node(struct rb_node *rb)
+{
+	rb->rb_parent_color = 0;
+	rb->rb_right = NULL;
+	rb->rb_left = NULL;
+	RB_CLEAR_NODE(rb);
+}
+
+extern void rb_insert_color(struct rb_node *, struct rb_root *);
+extern void rb_erase(struct rb_node *, struct rb_root *);
+
+/* Find logical next and previous nodes in a tree */
+extern struct rb_node *rb_next(const struct rb_node *);
+extern struct rb_node *rb_prev(const struct rb_node *);
+extern struct rb_node *rb_first(const struct rb_root *);
+extern struct rb_node *rb_last(const struct rb_root *);
+static inline void rb_link_node(struct rb_node *node, struct rb_node *parent,
+				struct rb_node **rb_link)
+{
+	node->rb_parent_color = (unsigned long)parent;
+	node->rb_left = node->rb_right = NULL;
+
+	*rb_link = node;
+}
+
+/***************************************************************************
+ *
+ * End of Linux kernel red black tree emulation.
+ *
+ ***************************************************************************/
+
 typedef ssize_t (*read_actor_t)();
 
 #define CFS_IFSHIFT 12

libcfs/libcfs/autoMakefile.am
@@ -44,7 +44,8 @@ if LIBLUSTRE
 noinst_LIBRARIES= libcfs.a
 libcfs_a_SOURCES= posix/posix-debug.c user-prim.c user-lock.c user-tcpip.c \
 		  prng.c user-bitops.c user-mem.c hash.c kernel_user_comm.c \
-		  workitem.c fail.c libcfs_cpu.c libcfs_mem.c libcfs_lock.c
+		  workitem.c fail.c libcfs_cpu.c libcfs_mem.c libcfs_lock.c \
+		  posix/rbtree.c
 libcfs_a_CPPFLAGS = $(LLCPPFLAGS)
 libcfs_a_CFLAGS = $(LLCFLAGS)
 endif
@@ -70,7 +71,7 @@ nodist_libcfs_SOURCES := darwin/darwin-sync.c darwin/darwin-mem.c	\
 	darwin/darwin-debug.c darwin/darwin-proc.c 			\
 	darwin/darwin-tracefile.c darwin/darwin-module.c 		\
 	posix/posix-debug.c module.c tracefile.c nidstrings.c watchdog.c \
-	kernel_user_comm.c hash.c
+	kernel_user_comm.c hash.c posix/rbtree.c
 
 libcfs_CFLAGS := $(EXTRA_KCFLAGS)
 libcfs_LDFLAGS := $(EXTRA_KLDFLAGS)

libcfs/libcfs/posix/rbtree.c
@@ -1,0 +1,333 @@
+/*
+  Red Black Trees
+  (C) 1999  Andrea Arcangeli <andrea@suse.de>
+  (C) 2002  David Woodhouse <dwmw2@infradead.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  linux/lib/rbtree.c
+*/
+
+#include <libcfs/libcfs.h>
+
+static void __rb_rotate_left(struct rb_node *node, struct rb_root *root)
+{
+	struct rb_node *right = node->rb_right;
+	struct rb_node *parent = rb_parent(node);
+
+	node->rb_right = right->rb_left;
+	if (node->rb_right != NULL)
+		rb_set_parent(right->rb_left, node);
+	right->rb_left = node;
+
+	rb_set_parent(right, parent);
+
+	if (parent) {
+		if (node == parent->rb_left)
+			parent->rb_left = right;
+		else
+			parent->rb_right = right;
+	} else
+		root->rb_node = right;
+	rb_set_parent(node, right);
+}
+
+static void __rb_rotate_right(struct rb_node *node, struct rb_root *root)
+{
+	struct rb_node *left = node->rb_left;
+	struct rb_node *parent = rb_parent(node);
+
+	node->rb_left = left->rb_right;
+	if (node->rb_left != NULL)
+		rb_set_parent(left->rb_right, node);
+	left->rb_right = node;
+
+	rb_set_parent(left, parent);
+
+	if (parent) {
+		if (node == parent->rb_right)
+			parent->rb_right = left;
+		else
+			parent->rb_left = left;
+	} else
+		root->rb_node = left;
+	rb_set_parent(node, left);
+}
+
+void rb_insert_color(struct rb_node *node, struct rb_root *root)
+{
+	struct rb_node *parent, *gparent;
+
+	while ((parent = rb_parent(node)) != NULL && rb_is_red(parent)) {
+		gparent = rb_parent(parent);
+
+		if (parent == gparent->rb_left) {
+			register struct rb_node *uncle = gparent->rb_right;
+			if (uncle && rb_is_red(uncle)) {
+				rb_set_black(uncle);
+				rb_set_black(parent);
+				rb_set_red(gparent);
+				node = gparent;
+				continue;
+			}
+
+			if (parent->rb_right == node) {
+				register struct rb_node *tmp;
+				__rb_rotate_left(parent, root);
+				tmp = parent;
+				parent = node;
+				node = tmp;
+			}
+
+			rb_set_black(parent);
+			rb_set_red(gparent);
+			__rb_rotate_right(gparent, root);
+		} else {
+			register struct rb_node *uncle = gparent->rb_left;
+			if (uncle && rb_is_red(uncle)) {
+				rb_set_black(uncle);
+				rb_set_black(parent);
+				rb_set_red(gparent);
+				node = gparent;
+				continue;
+			}
+
+			if (parent->rb_left == node) {
+				register struct rb_node *tmp;
+				__rb_rotate_right(parent, root);
+				tmp = parent;
+				parent = node;
+				node = tmp;
+			}
+
+			rb_set_black(parent);
+			rb_set_red(gparent);
+			__rb_rotate_left(gparent, root);
+		}
+	}
+
+	rb_set_black(root->rb_node);
+}
+
+static void __rb_erase_color(struct rb_node *node, struct rb_node *parent,
+			     struct rb_root *root)
+{
+	struct rb_node *ptr;
+
+	while ((!node || rb_is_black(node)) && node != root->rb_node) {
+		if (parent->rb_left == node) {
+			ptr = parent->rb_right;
+			if (rb_is_red(ptr)) {
+				rb_set_black(ptr);
+				rb_set_red(parent);
+				__rb_rotate_left(parent, root);
+				ptr = parent->rb_right;
+			}
+			if ((!ptr->rb_left || rb_is_black(ptr->rb_left)) &&
+			    (!ptr->rb_right || rb_is_black(ptr->rb_right))) {
+				rb_set_red(ptr);
+				node = parent;
+				parent = rb_parent(node);
+			} else {
+				if (!ptr->rb_right ||
+				    rb_is_black(ptr->rb_right)) {
+					rb_set_black(ptr->rb_left);
+					rb_set_red(ptr);
+					__rb_rotate_right(ptr, root);
+					ptr = parent->rb_right;
+				}
+				rb_set_color(ptr, rb_color(parent));
+				rb_set_black(parent);
+				rb_set_black(ptr->rb_right);
+				__rb_rotate_left(parent, root);
+				node = root->rb_node;
+				break;
+			}
+		} else {
+			ptr = parent->rb_left;
+			if (rb_is_red(ptr)) {
+				rb_set_black(ptr);
+				rb_set_red(parent);
+				__rb_rotate_right(parent, root);
+				ptr = parent->rb_left;
+			}
+			if ((!ptr->rb_left || rb_is_black(ptr->rb_left)) &&
+			    (!ptr->rb_right || rb_is_black(ptr->rb_right))) {
+				rb_set_red(ptr);
+				node = parent;
+				parent = rb_parent(node);
+			} else {
+				if (!ptr->rb_left ||
+				    rb_is_black(ptr->rb_left)) {
+					rb_set_black(ptr->rb_right);
+					rb_set_red(ptr);
+					__rb_rotate_left(ptr, root);
+					ptr = parent->rb_left;
+				}
+				rb_set_color(ptr, rb_color(parent));
+				rb_set_black(parent);
+				rb_set_black(ptr->rb_left);
+				__rb_rotate_right(parent, root);
+				node = root->rb_node;
+				break;
+			}
+		}
+	}
+	if (node)
+		rb_set_black(node);
+}
+
+void rb_erase(struct rb_node *node, struct rb_root *root)
+{
+	struct rb_node *child, *parent;
+	int color;
+
+	if (!node->rb_left)
+		child = node->rb_right;
+	else if (!node->rb_right)
+		child = node->rb_left;
+	else {
+		struct rb_node *old = node, *left;
+
+		node = node->rb_right;
+		while ((left = node->rb_left) != NULL)
+			node = left;
+
+		if (rb_parent(old)) {
+			if (rb_parent(old)->rb_left == old)
+				rb_parent(old)->rb_left = node;
+			else
+				rb_parent(old)->rb_right = node;
+		} else
+			root->rb_node = node;
+
+		child = node->rb_right;
+		parent = rb_parent(node);
+		color = rb_color(node);
+
+		if (parent == old) {
+			parent = node;
+		} else {
+			if (child)
+				rb_set_parent(child, parent);
+			parent->rb_left = child;
+
+			node->rb_right = old->rb_right;
+			rb_set_parent(old->rb_right, node);
+		}
+
+		node->rb_parent_color = old->rb_parent_color;
+		node->rb_left = old->rb_left;
+		rb_set_parent(old->rb_left, node);
+
+		goto color;
+	}
+
+	parent = rb_parent(node);
+	color = rb_color(node);
+
+	if (child)
+		rb_set_parent(child, parent);
+	if (parent) {
+		if (parent->rb_left == node)
+			parent->rb_left = child;
+		else
+			parent->rb_right = child;
+	} else
+		root->rb_node = child;
+
+ color:
+	if (color == RB_BLACK)
+		__rb_erase_color(child, parent, root);
+}
+
+/*
+ * This function returns the first node (in sort order) of the tree.
+ */
+struct rb_node *rb_first(const struct rb_root *root)
+{
+	struct rb_node	*n;
+
+	n = root->rb_node;
+	if (!n)
+		return NULL;
+	while (n->rb_left)
+		n = n->rb_left;
+	return n;
+}
+
+struct rb_node *rb_last(const struct rb_root *root)
+{
+	struct rb_node	*n;
+
+	n = root->rb_node;
+	if (!n)
+		return NULL;
+	while (n->rb_right)
+		n = n->rb_right;
+	return n;
+}
+
+struct rb_node *rb_next(const struct rb_node *node)
+{
+	struct rb_node *parent;
+
+	if (rb_parent(node) == node)
+		return NULL;
+
+	/* If we have a right-hand child, go down and then left as far
+	   as we can. */
+	if (node->rb_right) {
+		node = node->rb_right;
+		while (node->rb_left)
+			node = node->rb_left;
+		return (struct rb_node *)node;
+	}
+
+	/* No right-hand children.  Everything down and left is
+	   smaller than us, so any 'next' node must be in the general
+	   direction of our parent. Go up the tree; any time the
+	   ancestor is a right-hand child of its parent, keep going
+	   up. First time it's a left-hand child of its parent, said
+	   parent is our 'next' node. */
+	while ((parent = rb_parent(node)) && node == parent->rb_right)
+		node = parent;
+
+	return parent;
+}
+
+struct rb_node *rb_prev(const struct rb_node *node)
+{
+	struct rb_node *parent;
+
+	if (rb_parent(node) == node)
+		return NULL;
+
+	/* If we have a left-hand child, go down and then right as far
+	   as we can. */
+	if (node->rb_left) {
+		node = node->rb_left;
+		while (node->rb_right)
+			node = node->rb_right;
+		return (struct rb_node *)node;
+	}
+
+	/* No left-hand children. Go up till we find an ancestor which
+	   is a right-hand child of its parent */
+	while ((parent = rb_parent(node)) && node == parent->rb_left)
+		node = parent;
+
+	return parent;
+}

commit 05f326a7988a7a0d6954d1b0d318315526209ae6

LU-12687 osc: consume grants for direct I/O
New IO engine implementation lost consuming grants by direct I/O
writes. That led to early emergence of out of space condition during
direct I/O. The below illustrates the problem:
  # OSTSIZE=100000 sh llmount.sh
  # dd if=/dev/zero of=/mnt/lustre/file bs=4k count=100 oflag=direct
  dd: error writing ‘/mnt/lustre/file’: No space left on device

Consume grants for direct I/O.

Try to consume grants in osc_queue_sync_pages() when it is called for
pages which are being writted in direct i/o.

Tests are added to verify grant consumption in buffered and direct i/o
and to verify direct i/o overwrite when ost is full.
The overwrite test is for ldiskfs only as zfs is unable to overwrite
when it is full.

Fixes: 9fe4b52ad2 ("LU-1030 osc: new IO engine implementation")
Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Change-Id: I9a199452c564e8e8ad02f79231e8481166f3666e
Cray-bug-id: LUS-7036
Reviewed-on: https://review.whamcloud.com/35896
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>

lustre/include/obd_support.h
@@ -487,6 +487,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_TGT_RECOVERY_REQ_RACE	 0x721
 #define OBD_FAIL_TGT_REPLY_DATA_RACE	 0x722
 #define OBD_FAIL_TGT_RECOVERY_CONNECT    0x724
+#define OBD_FAIL_TGT_NO_GRANT		 0x725
 
 #define OBD_FAIL_MDC_REVALIDATE_PAUSE    0x800
 #define OBD_FAIL_MDC_ENQUEUE_PAUSE       0x801

lustre/osc/osc_cache.c
@@ -2610,6 +2610,28 @@ int osc_queue_sync_pages(const struct lu_env *env, const struct cl_io *io,
 	ext->oe_srvlock = !!(brw_flags & OBD_BRW_SRVLOCK);
 	ext->oe_ndelay = !!(brw_flags & OBD_BRW_NDELAY);
 	ext->oe_dio = !!(brw_flags & OBD_BRW_NOCACHE);
+	if (ext->oe_dio && !ext->oe_rw) { /* direct io write */
+		int grants;
+		int ppc;
+
+		ppc = 1 << (cli->cl_chunkbits - PAGE_SHIFT);
+		grants = cli->cl_grant_extent_tax;
+		grants += (1 << cli->cl_chunkbits) *
+			((page_count + ppc - 1) / ppc);
+
+		spin_lock(&cli->cl_loi_list_lock);
+		if (osc_reserve_grant(cli, grants) == 0) {
+			list_for_each_entry(oap, list, oap_pending_item) {
+				osc_consume_write_grant(cli,
+							&oap->oap_brw_page);
+				atomic_long_inc(&obd_dirty_pages);
+			}
+			osc_unreserve_grant_nolock(cli, grants, 0);
+			ext->oe_grants = grants;
+		}
+		spin_unlock(&cli->cl_loi_list_lock);
+	}
+
 	ext->oe_is_rdma_only = !!(brw_flags & OBD_BRW_RDMA_ONLY);
 	ext->oe_nr_pages = page_count;
 	ext->oe_mppr = mppr;

lustre/target/tgt_grant.c
@@ -902,6 +902,9 @@ static long tgt_grant_alloc(struct obd_export *exp, u64 curgrant,
 
 	ENTRY;
 
+	if (OBD_FAIL_CHECK(OBD_FAIL_TGT_NO_GRANT))
+		RETURN(0);
+
 	/* When tgd_grant_compat_disable is set, we don't grant any space to
 	 * clients not supporting OBD_CONNECT_GRANT_PARAM.
 	 * Otherwise, space granted to such a client is inflated since it

lustre/tests/conf-sanity.sh
@@ -140,6 +140,9 @@ start_mds() {
 	for num in $(seq $MDSCOUNT); do
 		start_mdt $num $@ || return 94
 	done
+	for num in $(seq $MDSCOUNT); do
+		wait_clients_import_state ${CLIENTS:-$HOSTNAME} mds${num} FULL
+	done
 }
 
 start_mgsmds() {
@@ -165,6 +168,7 @@ stop_mgs() {
 start_ost() {
 	echo "start ost1 service on `facet_active_host ost1`"
 	start ost1 $(ostdevname 1) $OST_MOUNT_OPTS $@ || return 95
+	wait_clients_import_state ${CLIENTS:-$HOSTNAME} ost1 FULL
 }
 
 stop_ost() {
@@ -176,6 +180,7 @@ stop_ost() {
 start_ost2() {
 	echo "start ost2 service on `facet_active_host ost2`"
 	start ost2 $(ostdevname 2) $OST_MOUNT_OPTS $@ || return 92
+	wait_clients_import_state ${CLIENTS:-$HOSTNAME} ost2 FULL
 }
 
 stop_ost2() {
@@ -9052,6 +9057,35 @@ test_126() {
 	start mds1 $(mdsdevname 1) $MDS_MOUNT_OPTS
 }
 run_test 126 "mount in parallel shouldn't cause a crash"
+
+test_127() {
+	[[ "$ost1_FSTYPE" == ldiskfs ]] || skip "ldiskfs only test"
+
+	cleanup
+	setup
+	zconf_umount_clients $RCLIENTS $MOUNT
+
+	wait_osp_active ost ${FSNAME}-OST0000 0 1
+	local osc_tgt="$FSNAME-OST0000-osc-$($LFS getname -i $DIR)"
+	local avail1=($($LCTL get_param -n osc.${osc_tgt}.kbytesavail))
+
+	$LFS setstripe -i 0 $DIR/$tfile || error "failed creating $DIR/$tfile"
+	dd if=/dev/zero of=$DIR/$tfile bs=1M oflag=direct || true
+
+	local avail2=($($LCTL get_param -n osc.${osc_tgt}.kbytesavail))
+
+	if ((avail2 * 100 / avail1 > 1)); then
+		lfs df $DIR
+		ls -l $DIR/$tfile
+		error "more than 1% space left: before=$avail1 after=$avail2"
+	fi
+
+	local mbs=$(($(stat -c %s $DIR/$tfile) / (1024 * 1024)))
+
+	dd if=/dev/zero of=$DIR/$tfile bs=1M count=$mbs conv=notrunc \
+		oflag=direct || error "overwrite failed"
+}
+run_test 127 "direct io overwrite on full ost"
 
 if ! combined_mgs_mds ; then
 	stop mgs

lustre/tests/sanity.sh
@@ -7839,18 +7839,25 @@ test_64c() {
 }
 run_test 64c "verify grant shrink"
 
+import_param() {
+	local tgt=$1
+	local param=$2
+
+	$LCTL get_param osc.$tgt.import | awk "/$param/ { print \$2 }"
+}
+
 # this does exactly what osc_request.c:osc_announce_cached() does in
 # order to calculate max amount of grants to ask from server
 want_grant() {
 	local tgt=$1
 
-	local nrpages=$($LCTL get_param -n osc.${tgt}.max_pages_per_rpc)
-	local rpc_in_flight=$($LCTL get_param -n osc.${tgt}.max_rpcs_in_flight)
+	local nrpages=$($LCTL get_param -n osc.$tgt.max_pages_per_rpc)
+	local rpc_in_flight=$($LCTL get_param -n osc.$tgt.max_rpcs_in_flight)
 
-	((rpc_in_flight ++));
+	((rpc_in_flight++));
 	nrpages=$((nrpages * rpc_in_flight))
 
-	local dirty_max_pages=$($LCTL get_param -n osc.${tgt}.max_dirty_mb)
+	local dirty_max_pages=$($LCTL get_param -n osc.$tgt.max_dirty_mb)
 
 	dirty_max_pages=$((dirty_max_pages * 1024 * 1024 / PAGE_SIZE))
 
@@ -7858,13 +7865,11 @@ want_grant() {
 	local undirty=$((nrpages * PAGE_SIZE))
 
 	local max_extent_pages
-	max_extent_pages=$($LCTL get_param osc.${tgt}.import |
-	    grep grant_max_extent_size | awk '{print $2}')
+	max_extent_pages=$(import_param $tgt grant_max_extent_size)
 	max_extent_pages=$((max_extent_pages / PAGE_SIZE))
 	local nrextents=$(((nrpages + max_extent_pages - 1) / max_extent_pages))
 	local grant_extent_tax
-	grant_extent_tax=$($LCTL get_param osc.${tgt}.import |
-	    grep grant_extent_tax | awk '{print $2}')
+	grant_extent_tax=$(import_param $tgt grant_extent_tax)
 
 	undirty=$((undirty + nrextents * grant_extent_tax))
 
@@ -7878,55 +7883,170 @@ grant_chunk() {
 	local max_brw_size
 	local grant_extent_tax
 
-	max_brw_size=$($LCTL get_param osc.${tgt}.import |
-	    grep max_brw_size | awk '{print $2}')
+	max_brw_size=$(import_param $tgt max_brw_size)
 
-	grant_extent_tax=$($LCTL get_param osc.${tgt}.import |
-	    grep grant_extent_tax | awk '{print $2}')
+	grant_extent_tax=$(import_param $tgt grant_extent_tax)
 
 	echo $(((max_brw_size + grant_extent_tax) * 2))
 }
 
 test_64d() {
-	[ $OST1_VERSION -lt $(version_code 2.10.56) ] &&
+	[ $OST1_VERSION -ge $(version_code 2.10.56) ] ||
 		skip "OST < 2.10.55 doesn't limit grants enough"
 
-	local tgt=$($LCTL dl | grep "0000-osc-[^mM]" | awk '{print $4}')
-	local file=$DIR/$tfile
+	local tgt=$($LCTL dl | awk '/OST0000-osc-[^mM]/ { print $4 }')
 
-	[[ $($LCTL get_param osc.${tgt}.import |
-	     grep "connect_flags:.*grant_param") ]] ||
+	[[ "$($LCTL get_param osc.${tgt}.import)" =~ "grant_param" ]] ||
 		skip "no grant_param connect flag"
 
-	local olddebug=$($LCTL get_param -n debug 2> /dev/null)
+	local olddebug="$($LCTL get_param -n debug 2> /dev/null)"
 
-	$LCTL set_param debug="$OLDDEBUG" 2> /dev/null || true
+	$LCTL set_param -n -n debug="$OLDDEBUG" || true
+	stack_trap "$LCTL set_param -n debug='$olddebug'" EXIT
+
 
 	local max_cur_granted=$(($(want_grant $tgt) + $(grant_chunk $tgt)))
-	stack_trap "rm -f $file" EXIT
+	stack_trap "rm -f $DIR/$tfile && wait_delete_completed" EXIT
 
-	$LFS setstripe $file -i 0 -c 1
-	dd if=/dev/zero of=$file bs=1M count=1000 &
+	$LFS setstripe $DIR/$tfile -i 0 -c 1
+	dd if=/dev/zero of=$DIR/$tfile bs=1M count=1000 &
 	ddpid=$!
 
-	while true
-	do
-		local cur_grant=$($LCTL get_param -n osc.${tgt}.cur_grant_bytes)
-		if [[ $cur_grant -gt $max_cur_granted ]]
-		then
+	while kill -0 $ddpid; do
+		local cur_grant=$($LCTL get_param -n osc.$tgt.cur_grant_bytes)
+
+		if [[ $cur_grant -gt $max_cur_granted ]]; then
 			kill $ddpid
 			error "cur_grant $cur_grant > $max_cur_granted"
 		fi
-		kill -0 $ddpid
-		[[ $? -ne 0 ]] && break;
-		sleep 2
-	done
 
-	rm -f $DIR/$tfile
-	wait_delete_completed
-	$LCTL set_param debug="$olddebug" 2> /dev/null || true
+		sleep 1
+	done
 }
 run_test 64d "check grant limit exceed"
+
+check_grants() {
+	local tgt=$1
+	local expected=$2
+	local msg=$3
+	local cur_grants=$($LCTL get_param -n osc.$tgt.cur_grant_bytes)
+
+	((cur_grants == expected)) ||
+		error "$msg: grants mismatch: $cur_grants, expected $expected"
+}
+
+round_up_p2() {
+	echo $((($1 + $2 - 1) & ~($2 - 1)))
+}
+
+test_64e() {
+	[ $PARALLEL == "yes" ] && skip "skip parallel run"
+	[ $OST1_VERSION -ge $(version_code 2.11.56) ] ||
+		skip "Need OSS version at least 2.11.56"
+
+	# Remount client to reset grant
+	remount_client $MOUNT || error "failed to remount client"
+	local osc_tgt="$FSNAME-OST0000-osc-$($LFS getname -i $DIR)"
+
+	local init_grants=$(import_param $osc_tgt initial_grant)
+
+	check_grants $osc_tgt $init_grants "init grants"
+
+	local extent_tax=$(import_param $osc_tgt grant_extent_tax)
+	local max_brw_size=$(import_param $osc_tgt max_brw_size)
+	local gbs=$(import_param $osc_tgt grant_block_size)
+
+	# write random number of bytes from max_brw_size / 4 to max_brw_size
+	local write_bytes=$(shuf -i $((max_brw_size / 4))-$max_brw_size -n 1)
+	# align for direct io
+	write_bytes=$(round_up_p2 $write_bytes PAGE_SIZE)
+	# round to grant consumption unit
+	local wb_round_up=$(round_up_p2 $write_bytes gbs)
+
+	local grants=$((wb_round_up + extent_tax))
+
+	$LFS setstripe -c 1 -i 0 $DIR/$tfile  || error "lfs setstripe failed"
+
+	# define OBD_FAIL_TGT_NO_GRANT 0x725
+	# make the server not grant more back
+	do_facet ost1 $LCTL set_param fail_loc=0x725
+	dd if=/dev/zero of=$DIR/$tfile bs=$write_bytes count=1 oflag=direct
+
+	do_facet ost1 $LCTL set_param fail_loc=0
+
+	check_grants $osc_tgt $((init_grants - grants)) "dio w/o grant alloc"
+
+	rm -f $DIR/$tfile || error "rm failed"
+
+	# Remount client to reset grant
+	remount_client $MOUNT || error "failed to remount client"
+	osc_tgt="$FSNAME-OST0000-osc-$($LFS getname -i $DIR)"
+
+	$LFS setstripe -c 1 -i 0 $DIR/$tfile || error "lfs setstripe failed"
+
+	# define OBD_FAIL_TGT_NO_GRANT 0x725
+	# make the server not grant more back
+	do_facet ost1 $LCTL set_param fail_loc=0x725
+	$MULTIOP $DIR/$tfile "oO_WRONLY:w${write_bytes}yc"
+	do_facet ost1 $LCTL set_param fail_loc=0
+
+	check_grants $osc_tgt $((init_grants - grants)) "buf io w/o grant alloc"
+}
+run_test 64e "check grant consumption (no grant allocation)"
+
+test_64f() {
+	[ $PARALLEL == "yes" ] && skip "skip parallel run"
+
+	# Remount client to reset grant
+	remount_client $MOUNT || error "failed to remount client"
+	local osc_tgt="$FSNAME-OST0000-osc-$($LFS getname -i $DIR)"
+
+	local init_grants=$(import_param $osc_tgt initial_grant)
+	local extent_tax=$(import_param $osc_tgt grant_extent_tax)
+	local max_brw_size=$(import_param $osc_tgt max_brw_size)
+	local gbs=$(import_param $osc_tgt grant_block_size)
+	local chunk=$(grant_chunk $osc_tgt)
+
+	# write random number of bytes from max_brw_size / 4 to max_brw_size
+	local write_bytes=$(shuf -i $((max_brw_size / 4))-$max_brw_size -n 1)
+	# align for direct io
+	write_bytes=$(round_up_p2 $write_bytes PAGE_SIZE)
+	# round to grant consumption unit
+	local wb_round_up=$(round_up_p2 $write_bytes gbs)
+
+	local grants=$((wb_round_up + extent_tax))
+
+	$LFS setstripe -c 1 -i 0 $DIR/$tfile || error "lfs setstripe failed"
+	dd if=/dev/zero of=$DIR/$tfile bs=$write_bytes count=1 oflag=direct ||
+		error "error writing to $DIR/$tfile"
+
+	check_grants $osc_tgt $((init_grants - grants + chunk)) \
+		"direct io with grant allocation"
+
+	rm -f $DIR/$tfile || error "rm failed"
+
+	# Remount client to reset grant
+	remount_client $MOUNT || error "failed to remount client"
+	osc_tgt="$FSNAME-OST0000-osc-$($LFS getname -i $DIR)"
+
+	$LFS setstripe -c 1 -i 0 $DIR/$tfile || error "lfs setstripe failed"
+
+	local cmd="oO_WRONLY:w${write_bytes}_yc"
+
+	$MULTIOP $DIR/$tfile $cmd &
+	MULTIPID=$!
+	sleep 1
+
+	check_grants $osc_tgt $((init_grants - grants)) \
+		"buffered io, not write rpc"
+
+	kill -USR1 $MULTIPID
+	wait
+
+	check_grants $osc_tgt $((init_grants - grants + chunk)) \
+		"buffered io, one RPC"
+}
+run_test 64f "check grant consumption (with grant allocation)"
 
 # bug 1414 - set/get directories' stripe info
 test_65a() {