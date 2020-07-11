lustre/lustre-release

commit 9d42660e173e5b9a2683f412fc8a756094c6f1b4

LU-11986 lnet: properly cleanup lnet debugfs files
The function lnet_router_debugfs_remove() is suppose to cleanup
the lnet specific debugfs files but that is not happening at all.
Change lnet_remove_debugfs() from doing the final debugfs lnet
and libcfs cleanup to doing specific debugfs file removal. We
can make libcfs module unloading to directly finish the entire
libcfs and debugfs tree removal instead. With this change we can
make lnet_router_debugfs_fini() call lnet_remove_debugfs().

Lustre-change: https://review.whamcloud.com/34669/
Lustre-commit: 8cb7ccf54e2d34010bea9a71fe0eff708545ab78

Change-Id: I9e314e7efde806073b621166ff2e1b344e550875
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Signed-off-by: Minh Diep <mdiep@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34692
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sonia Sharma <sharmaso@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/module.c
@@ -548,8 +548,7 @@ static const struct file_operations *lnet_debugfs_fops_select(umode_t mode)
 	return &lnet_debugfs_file_operations_rw;
 }
 
-void lnet_insert_debugfs(struct ctl_table *table,
-			 const struct lnet_debugfs_symlink_def *symlinks)
+void lnet_insert_debugfs(struct ctl_table *table)
 {
 	if (!lnet_debugfs_root)
 		lnet_debugfs_root = debugfs_create_dir("lnet", NULL);
@@ -565,19 +564,29 @@ void lnet_insert_debugfs(struct ctl_table *table,
 		debugfs_create_file(table->procname, table->mode,
 				    lnet_debugfs_root, table,
 				    lnet_debugfs_fops_select(table->mode));
+}
+EXPORT_SYMBOL_GPL(lnet_insert_debugfs);
 
+static void lnet_insert_debugfs_links(
+		const struct lnet_debugfs_symlink_def *symlinks)
+{
 	for (; symlinks && symlinks->name; symlinks++)
 		debugfs_create_symlink(symlinks->name, lnet_debugfs_root,
 				       symlinks->target);
 }
-EXPORT_SYMBOL_GPL(lnet_insert_debugfs);
 
-static void lnet_remove_debugfs(void)
+void lnet_remove_debugfs(struct ctl_table *table)
 {
-	debugfs_remove_recursive(lnet_debugfs_root);
+	for (; table && table->procname; table++) {
+		struct qstr dname = QSTR_INIT(table->procname,
+					      strlen(table->procname));
+		struct dentry *dentry;
 
-	lnet_debugfs_root = NULL;
+		dentry = d_hash_and_lookup(lnet_debugfs_root, &dname);
+		debugfs_remove(dentry);
+	}
 }
+EXPORT_SYMBOL_GPL(lnet_remove_debugfs);
 
 static int __init libcfs_init(void)
 {
@@ -620,7 +629,9 @@ static int __init libcfs_init(void)
 		goto cleanup_wi;
 	}
 
-	lnet_insert_debugfs(lnet_table, lnet_debugfs_symlinks);
+	lnet_insert_debugfs(lnet_table);
+	if (!IS_ERR_OR_NULL(lnet_debugfs_root))
+		lnet_insert_debugfs_links(lnet_debugfs_symlinks);
 
 	CDEBUG (D_OTHER, "portals setup OK\n");
 	return 0;
@@ -639,7 +650,9 @@ static void __exit libcfs_exit(void)
 {
 	int rc;
 
-	lnet_remove_debugfs();
+	/* Remove everthing */
+	debugfs_remove_recursive(lnet_debugfs_root);
+	lnet_debugfs_root = NULL;
 
 	CDEBUG(D_MALLOC, "before Portals cleanup: kmem %d\n",
 	       atomic_read(&libcfs_kmemory));

libcfs/include/libcfs/libcfs.h
@@ -139,8 +139,8 @@ struct lnet_debugfs_symlink_def {
 	const char *target;
 };
 
-void lnet_insert_debugfs(struct ctl_table *table,
-			 const struct lnet_debugfs_symlink_def *symlinks);
+void lnet_insert_debugfs(struct ctl_table *table);
+void lnet_remove_debugfs(struct ctl_table *table);
 
 /* helper for sysctl handlers */
 int lprocfs_call_handler(void *data, int write, loff_t *ppos,

lnet/lnet/api-ni.c
@@ -2657,7 +2657,7 @@ LNetNIFini()
 
 		lnet_fault_fini();
 
-		lnet_router_debugfs_init();
+		lnet_router_debugfs_fini();
 		lnet_peer_discovery_stop();
 		lnet_push_target_fini();
 		lnet_monitor_thr_stop();

lnet/lnet/router_proc.c
@@ -957,5 +957,10 @@ static struct ctl_table lnet_table[] = {
 
 void lnet_router_debugfs_init(void)
 {
-	lnet_insert_debugfs(lnet_table, NULL);
+	lnet_insert_debugfs(lnet_table);
+}
+
+void lnet_router_debugfs_fini(void)
+{
+	lnet_remove_debugfs(lnet_table);
 }

commit 7f821c9382c39fd16156593569737df27dfb0467

LU-11986 libcfs: lnet_remove_debugfs() compat for RHEL6
Unloading libcfs module on RHEL 6.10 Lustre client with
kernel 2.6.32-754.24.3 hit kernel panic issue. The issue
doesn't exist in Lustre b2_10 where RHEL 6.10 is supported
and debugfs_remove_recursive() is called directly from
lnet_remove_debugfs(). This patch adds compat changes to
lnet_remove_debugfs() to resolve the issue.

Fixes: 9d42660e173e ("LU-11986 lnet: properly cleanup lnet debugfs files")
Fixes: ae93a9f21752 ("LU-11986 libcfs: add compat for d_hash_and_lookup()")
Test-Parameters: trivial
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Change-Id: Ib63a40afe8926f56cd1d2873975855c226098418
Reviewed-on: https://review.whamcloud.com/38716
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/module.c
@@ -575,33 +575,14 @@ static void lnet_insert_debugfs_links(
 				       symlinks->target);
 }
 
-#ifndef HAVE_D_HASH_AND_LOOKUP
-/**
- * d_hash_and_lookup - hash the qstr then search for a dentry
- * @dir: Directory to search in
- * @name: qstr of name we wish to find
- *
- * On lookup failure NULL is returned; on bad name - ERR_PTR(-error)
- */
-struct dentry *d_hash_and_lookup(struct dentry *dir, struct qstr *name)
-{
-	/*
-	 * Check for a fs-specific hash function. Note that we must
-	 * calculate the standard hash first, as the d_op->d_hash()
-	 * routine may choose to leave the hash value unchanged.
-	 */
-	name->hash = full_name_hash(name->name, name->len);
-	if (dir->d_op && dir->d_op->d_hash) {
-		int err = dir->d_op->d_hash(dir, name);
-		if (unlikely(err < 0))
-			return ERR_PTR(err);
-	}
-	return d_lookup(dir, name);
-}
-#endif
-
 void lnet_remove_debugfs(struct ctl_table *table)
 {
+#ifndef HAVE_D_HASH_AND_LOOKUP
+	debugfs_remove_recursive(lnet_debugfs_root);
+	lnet_debugfs_root = NULL;
+	return;
+#endif
+
 	for (; table && table->procname; table++) {
 		struct qstr dname = QSTR_INIT(table->procname,
 					      strlen(table->procname));
@@ -679,8 +660,10 @@ static void __exit libcfs_exit(void)
 	int rc;
 
 	/* Remove everthing */
-	debugfs_remove_recursive(lnet_debugfs_root);
-	lnet_debugfs_root = NULL;
+	if (lnet_debugfs_root) {
+		debugfs_remove_recursive(lnet_debugfs_root);
+		lnet_debugfs_root = NULL;
+	}
 
 	CDEBUG(D_MALLOC, "before Portals cleanup: kmem %d\n",
 	       atomic_read(&libcfs_kmemory));