lustre/lustre-release

commit f9a2da63abab5b8b687842166a0b5b5e434ad441

LU-13437 mdt: don't fetch LOOKUP lock for remote object
Pack parent FID in getattr by FID, which will be used to check whether
child is remote object on parent. The helper function is called
mdt_is_remote_object(). NB, directory shard is not treated as remote
object, because if so, client needs to revalidate shards when dir is
accessed, which will hurt performance much.

For getattr by FID, if object is remote file on parent, don't fetch
LOOKUP lock, otherwise client may see stale dir entries.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Id181ecc053579ee394080381a82706334503ced0
Reviewed-on: https://review.whamcloud.com/38561
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -4665,8 +4665,8 @@ static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 	       PFID(ll_inode2fid(inode)), inode, dentry->d_name.name);
 
 	/* Call getattr by fid, so do not provide name at all. */
-	op_data = ll_prep_md_op_data(NULL, inode, inode, NULL, 0, 0,
-				     LUSTRE_OPC_ANY, NULL);
+	op_data = ll_prep_md_op_data(NULL, dentry->d_parent->d_inode, inode,
+				     NULL, 0, 0, LUSTRE_OPC_ANY, NULL);
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
@@ -4712,7 +4712,7 @@ static int ll_merge_md_attr(struct inode *inode)
 		RETURN(0);
 
 	down_read(&lli->lli_lsm_sem);
-	rc = md_merge_attr(ll_i2mdexp(inode), ll_i2info(inode)->lli_lsm_md,
+	rc = md_merge_attr(ll_i2mdexp(inode), &lli->lli_fid, lli->lli_lsm_md,
 			   &attr, ll_md_blocking_ast);
 	up_read(&lli->lli_lsm_sem);
 	if (rc != 0)

lustre/llite/llite_lib.c
@@ -1599,8 +1599,8 @@ static int ll_update_lsm_md(struct inode *inode, struct lustre_md *md)
 		GOTO(unlock, rc = -ENOMEM);
 
 	/* validate the lsm */
-	rc = md_merge_attr(ll_i2mdexp(inode), lli->lli_lsm_md, attr,
-			   ll_md_blocking_ast);
+	rc = md_merge_attr(ll_i2mdexp(inode), &lli->lli_fid, lli->lli_lsm_md,
+			   attr, ll_md_blocking_ast);
 	if (!rc) {
 		if (md->body->mbo_valid & OBD_MD_FLNLINK)
 			md->body->mbo_nlink = attr->cat_nlink;

lustre/lmv/lmv_intent.c
@@ -152,6 +152,7 @@ out:
 }
 
 int lmv_revalidate_slaves(struct obd_export *exp,
+			  const struct lu_fid *pfid,
 			  const struct lmv_stripe_md *lsm,
 			  ldlm_blocking_callback cb_blocking,
 			  int extra_lock_flags)
@@ -198,7 +199,7 @@ int lmv_revalidate_slaves(struct obd_export *exp,
 		 * which is not needed here.
 		 */
 		memset(op_data, 0, sizeof(*op_data));
-		op_data->op_fid1 = fid;
+		op_data->op_fid1 = *pfid;
 		op_data->op_fid2 = fid;
 
 		tgt = lmv_tgt(lmv, lsm->lsm_md_oinfo[i].lmo_mds);
@@ -439,12 +440,17 @@ lmv_intent_lookup(struct obd_export *exp, struct md_op_data *op_data,
 	}
 
 retry:
-	tgt = lmv_locate_tgt(lmv, op_data);
+	if (op_data->op_name) {
+		tgt = lmv_locate_tgt(lmv, op_data);
+		if (!fid_is_sane(&op_data->op_fid2))
+			fid_zero(&op_data->op_fid2);
+	} else if (fid_is_sane(&op_data->op_fid2)) {
+		tgt = lmv_fid2tgt(lmv, &op_data->op_fid2);
+	} else {
+		tgt = lmv_fid2tgt(lmv, &op_data->op_fid1);
+	}
 	if (IS_ERR(tgt))
 		RETURN(PTR_ERR(tgt));
-
-	if (!fid_is_sane(&op_data->op_fid2))
-		fid_zero(&op_data->op_fid2);
 
 	CDEBUG(D_INODE, "LOOKUP_INTENT with fid1="DFID", fid2="DFID
 	       ", name='%s' -> mds #%u\n",
@@ -463,7 +469,8 @@ retry:
 		/* If RPC happens, lsm information will be revalidated
 		 * during update_inode process (see ll_update_lsm_md) */
 		if (lmv_dir_striped(op_data->op_mea2)) {
-			rc = lmv_revalidate_slaves(exp, op_data->op_mea2,
+			rc = lmv_revalidate_slaves(exp, &op_data->op_fid2,
+						   op_data->op_mea2,
 						   cb_blocking,
 						   extra_lock_flags);
 			if (rc != 0)

lustre/include/obd.h
@@ -1152,7 +1152,7 @@ struct md_ops {
 
 	int (*m_free_lustre_md)(struct obd_export *, struct lustre_md *);
 
-	int (*m_merge_attr)(struct obd_export *,
+	int (*m_merge_attr)(struct obd_export *, const struct lu_fid *fid,
 			    const struct lmv_stripe_md *lsm,
 			    struct cl_attr *attr, ldlm_blocking_callback);
 

lustre/include/obd_class.h
@@ -1591,6 +1591,7 @@ static inline int md_free_lustre_md(struct obd_export *exp,
 }
 
 static inline int md_merge_attr(struct obd_export *exp,
+				const struct lu_fid *fid,
 				const struct lmv_stripe_md *lsm,
 				struct cl_attr *attr,
 				ldlm_blocking_callback cb)
@@ -1601,7 +1602,7 @@ static inline int md_merge_attr(struct obd_export *exp,
 	if (rc)
 		return rc;
 
-	return MDP(exp->exp_obd, merge_attr)(exp, lsm, attr, cb);
+	return MDP(exp->exp_obd, merge_attr)(exp, fid, lsm, attr, cb);
 }
 
 static inline int md_setxattr(struct obd_export *exp, const struct lu_fid *fid,

lustre/lmv/lmv_internal.h
@@ -53,6 +53,7 @@ int lmv_fid_alloc(const struct lu_env *env, struct obd_export *exp,
 		  struct lu_fid *fid, struct md_op_data *op_data);
 
 int lmv_revalidate_slaves(struct obd_export *exp,
+			  const struct lu_fid *pfid,
 			  const struct lmv_stripe_md *lsm,
 			  ldlm_blocking_callback cb_blocking,
 			  int extra_lock_flags);

lustre/lmv/lmv_obd.c
@@ -3552,6 +3552,7 @@ int lmv_quotactl(struct obd_device *unused, struct obd_export *exp,
 }
 
 static int lmv_merge_attr(struct obd_export *exp,
+			  const struct lu_fid *fid,
 			  const struct lmv_stripe_md *lsm,
 			  struct cl_attr *attr,
 			  ldlm_blocking_callback cb_blocking)
@@ -3562,7 +3563,7 @@ static int lmv_merge_attr(struct obd_export *exp,
 	if (!lmv_dir_striped(lsm))
 		return 0;
 
-	rc = lmv_revalidate_slaves(exp, lsm, cb_blocking, 0);
+	rc = lmv_revalidate_slaves(exp, fid, lsm, cb_blocking, 0);
 	if (rc < 0)
 		return rc;
 

lustre/mdt/mdt_handler.c
@@ -1838,14 +1838,14 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
                                  __u64 child_bits,
                                  struct ldlm_reply *ldlm_rep)
 {
-	struct ptlrpc_request  *req = mdt_info_req(info);
-	struct mdt_body        *reqbody = NULL;
-	struct mdt_object      *parent = info->mti_object;
-	struct mdt_object      *child;
-	struct lu_fid          *child_fid = &info->mti_tmp_fid1;
-	struct lu_name         *lname = NULL;
+	struct ptlrpc_request *req = mdt_info_req(info);
+	struct mdt_body *reqbody = NULL;
+	struct mdt_object *parent = info->mti_object;
+	struct mdt_object *child = NULL;
+	struct lu_fid *child_fid = &info->mti_tmp_fid1;
+	struct lu_name *lname = NULL;
 	struct mdt_lock_handle *lhp = NULL;
-	struct ldlm_lock       *lock;
+	struct ldlm_lock *lock;
 	struct req_capsule *pill = info->mti_pill;
 	__u64 try_bits = 0;
 	bool is_resent;
@@ -1918,6 +1918,13 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 	mdt_name_unpack(pill, &RMF_NAME, lname, MNF_FIX_ANON);
 
 	if (lu_name_is_valid(lname)) {
+		if (mdt_object_remote(parent)) {
+			CERROR("%s: parent "DFID" is on remote target\n",
+			       mdt_obd_name(info->mti_mdt),
+			       PFID(mdt_object_fid(parent)));
+			RETURN(-EPROTO);
+		}
+
 		CDEBUG(D_INODE, "getattr with lock for "DFID"/"DNAME", "
 		       "ldlm_rep = %p\n", PFID(mdt_object_fid(parent)),
 		       PNAME(lname), ldlm_rep);
@@ -1927,9 +1934,32 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 			RETURN(err_serious(-EPROTO));
 
 		*child_fid = reqbody->mbo_fid2;
-
 		if (unlikely(!fid_is_sane(child_fid)))
 			RETURN(err_serious(-EINVAL));
+
+		if (lu_fid_eq(mdt_object_fid(parent), child_fid)) {
+			mdt_object_get(info->mti_env, parent);
+			child = parent;
+		} else {
+			child = mdt_object_find(info->mti_env, info->mti_mdt,
+						child_fid);
+			if (IS_ERR(child))
+				RETURN(PTR_ERR(child));
+		}
+
+		if (mdt_object_remote(child)) {
+			CERROR("%s: child "DFID" is on remote target\n",
+			       mdt_obd_name(info->mti_mdt),
+			       PFID(mdt_object_fid(child)));
+			GOTO(out_child, rc = -EPROTO);
+		}
+
+		/* don't fetch LOOKUP lock if it's remote object */
+		rc = mdt_is_remote_object(info, parent, child);
+		if (rc < 0)
+			GOTO(out_child, rc);
+		if (rc)
+			child_bits &= ~MDS_INODELOCK_LOOKUP;
 
 		CDEBUG(D_INODE, "getattr with lock for "DFID"/"DFID", "
 		       "ldlm_rep = %p\n",
@@ -1943,14 +1973,7 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 		LU_OBJECT_DEBUG(D_INODE, info->mti_env,
 				&parent->mot_obj,
 				"Parent doesn't exist!");
-		RETURN(-ESTALE);
-	}
-
-	if (mdt_object_remote(parent)) {
-		CERROR("%s: parent "DFID" is on remote target\n",
-		       mdt_obd_name(info->mti_mdt),
-		       PFID(mdt_object_fid(parent)));
-		RETURN(-EIO);
+		GOTO(out_child, rc = -ESTALE);
 	}
 
 	if (lu_name_is_valid(lname)) {
@@ -1984,30 +2007,18 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 			mdt_set_disposition(info, ldlm_rep, DISP_LOOKUP_NEG);
 
 		if (rc != 0)
-			GOTO(out_parent, rc);
+			GOTO(unlock_parent, rc);
+
+		child = mdt_object_find(info->mti_env, info->mti_mdt,
+					child_fid);
+		if (unlikely(IS_ERR(child)))
+			GOTO(unlock_parent, rc = PTR_ERR(child));
 	}
 
 	mdt_set_disposition(info, ldlm_rep, DISP_LOOKUP_POS);
 
-	/*
-	 *step 3: find the child object by fid & lock it.
-	 *        regardless if it is local or remote.
-	 *
-	 *Note: LU-3240 (commit 762f2114d282a98ebfa4dbbeea9298a8088ad24e)
-	 *	set parent dir fid the same as child fid in getattr by fid case
-	 *	we should not lu_object_find() the object again, could lead
-	 *	to hung if there is a concurrent unlink destroyed the object.
-	 */
-	if (lu_fid_eq(mdt_object_fid(parent), child_fid)) {
-		mdt_object_get(info->mti_env, parent);
-		child = parent;
-	} else {
-		child = mdt_object_find(info->mti_env, info->mti_mdt,
-					child_fid);
-	}
-
-	if (unlikely(IS_ERR(child)))
-		GOTO(out_parent, rc = PTR_ERR(child));
+	/* step 3: lock child regardless if it is local or remote. */
+	LASSERT(child);
 
 	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RESEND, obd_timeout * 2);
 	if (!mdt_object_exists(child)) {
@@ -2116,15 +2127,16 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 				unlock_res_and_lock(lock);
 			}
 			LDLM_LOCK_PUT(lock);
-			GOTO(out_parent, rc = 0);
+			GOTO(unlock_parent, rc = 0);
 		}
 		LDLM_LOCK_PUT(lock);
 	}
 
 	EXIT;
 out_child:
-	mdt_object_put(info->mti_env, child);
-out_parent:
+	if (child)
+		mdt_object_put(info->mti_env, child);
+unlock_parent:
 	if (lhp)
 		mdt_object_unlock(info, parent, lhp, 1);
 	return rc;

commit 5f2c44bf626b178503c1c4d2d85c40bae087ff4f

LU-13437 llite: pack parent FID in getattr
Pack parent FID in getattr request if OBD_CONNECT2_GETATTR_PFID is
enabled, otherwise fill it with target FID for backward compatibility.

Fixes: f9a2da63 ("LU-13437 mdt: don't fetch LOOKUP lock for remot...")
Test-Parameters: clientversion=2.12 testlist=sanity env=SANITY_EXCEPT="27M 151 156"
Test-Parameters: serverversion=2.12 testlist=sanity env=SANITY_EXCEPT="56 165 205b"
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Idcf8388b65dee1f0a09a53b240ce8303f3c6ff75
Reviewed-on: https://review.whamcloud.com/39290
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -906,7 +906,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_ASYNC_DISCARD | \
 				OBD_CONNECT2_PCC | \
 				OBD_CONNECT2_CRUSH | \
-				OBD_CONNECT2_ENCRYPT)
+				OBD_CONNECT2_ENCRYPT | \
+				OBD_CONNECT2_GETATTR_PFID)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \

lustre/llite/file.c
@@ -4695,6 +4695,7 @@ static int ll_inode_revalidate_fini(struct inode *inode, int rc)
 
 static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 {
+	struct inode *parent;
 	struct inode *inode = dentry->d_inode;
 	struct obd_export *exp = ll_i2mdexp(inode);
 	struct lookup_intent oit = {
@@ -4708,9 +4709,14 @@ static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p),name=%s\n",
 	       PFID(ll_inode2fid(inode)), inode, dentry->d_name.name);
 
+	if (exp_connect_flags2(exp) & OBD_CONNECT2_GETATTR_PFID)
+		parent = dentry->d_parent->d_inode;
+	else
+		parent = inode;
+
 	/* Call getattr by fid, so do not provide name at all. */
-	op_data = ll_prep_md_op_data(NULL, dentry->d_parent->d_inode, inode,
-				     NULL, 0, 0, LUSTRE_OPC_ANY, NULL);
+	op_data = ll_prep_md_op_data(NULL, parent, inode, NULL, 0, 0,
+				     LUSTRE_OPC_ANY, NULL);
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 

lustre/llite/llite_lib.c
@@ -276,7 +276,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 				   OBD_CONNECT2_LSOM |
 				   OBD_CONNECT2_ASYNC_DISCARD |
 				   OBD_CONNECT2_PCC |
-				   OBD_CONNECT2_CRUSH;
+				   OBD_CONNECT2_CRUSH |
+				   OBD_CONNECT2_GETATTR_PFID;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
         if (sbi->ll_flags & LL_SBI_LRU_RESIZE)

lustre/lmv/lmv_intent.c
@@ -199,7 +199,10 @@ int lmv_revalidate_slaves(struct obd_export *exp,
 		 * which is not needed here.
 		 */
 		memset(op_data, 0, sizeof(*op_data));
-		op_data->op_fid1 = *pfid;
+		if (exp_connect_flags2(exp) & OBD_CONNECT2_GETATTR_PFID)
+			op_data->op_fid1 = *pfid;
+		else
+			op_data->op_fid1 = fid;
 		op_data->op_fid2 = fid;
 
 		tgt = lmv_tgt(lmv, lsm->lsm_md_oinfo[i].lmo_mds);