lustre/lustre-release

commit cd8fb1e8d300c0622200d6b25d187d8d0472e79f

LU-13597 ofd: add more information to job_stats
Request processing times/latency and basic IO size information
is added to the job_stats output. This allows monitoring per-job
request processing performance.
Except read_bytes and write_bytes in bytes units, all the others
use "usecs" units and show min/max/sum values. What's more, two
new counters for read and write time are added to calculate
bandwidth. The output format is like:
write_bytes: { samples: 1, unit: bytes, min: x, max: x, sum: x,
sumsq: x}

sanity.sh test_205b is modified to verify this patch.

Signed-off-by: Emoly Liu <emoly@whamcloud.com>
Change-Id: I7a5b77ca0ba464f6330a4bc56735c7762e167019
Reviewed-on: https://review.whamcloud.com/38816
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/lproc_llite.c
@@ -1537,18 +1537,14 @@ static struct kobj_type sbi_ktype = {
 	.release        = sbi_kobj_release,
 };
 
-#define LPROCFS_TYPE_LATENCY \
-	(LPROCFS_TYPE_USEC | LPROCFS_CNTR_AVGMINMAX | LPROCFS_CNTR_STDDEV)
 static const struct llite_file_opcode {
 	__u32		opcode;
 	__u32		type;
 	const char	*opname;
 } llite_opcode_table[LPROC_LL_FILE_OPCODES] = {
 	/* file operation */
-	{ LPROC_LL_READ_BYTES,	LPROCFS_CNTR_AVGMINMAX | LPROCFS_TYPE_BYTES,
-		"read_bytes" },
-	{ LPROC_LL_WRITE_BYTES,	LPROCFS_CNTR_AVGMINMAX | LPROCFS_TYPE_BYTES,
-		"write_bytes" },
+	{ LPROC_LL_READ_BYTES,	LPROCFS_TYPE_BYTES_FULL, "read_bytes" },
+	{ LPROC_LL_WRITE_BYTES,	LPROCFS_TYPE_BYTES_FULL, "write_bytes" },
 	{ LPROC_LL_READ,	LPROCFS_TYPE_LATENCY,	"read" },
 	{ LPROC_LL_WRITE,	LPROCFS_TYPE_LATENCY,	"write" },
 	{ LPROC_LL_IOCTL,	LPROCFS_TYPE_REQS,	"ioctl" },

lustre/tests/sanity.sh
@@ -16353,16 +16353,20 @@ test_205a() { # Job stats
 }
 run_test 205a "Verify job stats"
 
-# LU-13117
+# LU-13117, LU-13597
 test_205b() {
+	job_stats="mdt.*.job_stats"
+	$LCTL set_param $job_stats=clear
 	$LCTL set_param jobid_var=USER jobid_name="%e.%u"
 	env -i USERTESTJOBSTATS=foolish touch $DIR/$tfile.1
-	do_facet $SINGLEMDS $LCTL get_param mdt.*.job_stats |
-		grep job_id: | grep foolish &&
+	do_facet $SINGLEMDS $LCTL get_param $job_stats |
+		grep "job_id:.*foolish" &&
 			error "Unexpected jobid found"
-	true
+	do_facet $SINGLEMDS $LCTL get_param $job_stats |
+		grep "open:.*min.*max.*sum" ||
+			error "wrong job_stats format found"
 }
-run_test 205b "Verify job stats jobid parsing"
+run_test 205b "Verify job stats jobid and output format"
 
 # LU-1480, LU-1773 and LU-1657
 test_206() {

lustre/include/lprocfs_status.h
@@ -154,8 +154,14 @@ enum {
 	LPROCFS_TYPE_BYTES		= 0x0200,
 	LPROCFS_TYPE_PAGES		= 0x0400,
 	LPROCFS_TYPE_USEC		= 0x0800,
-};
 
+	LPROCFS_TYPE_LATENCY		= LPROCFS_TYPE_USEC |
+					  LPROCFS_CNTR_AVGMINMAX |
+					  LPROCFS_CNTR_STDDEV,
+	LPROCFS_TYPE_BYTES_FULL		= LPROCFS_TYPE_BYTES |
+					  LPROCFS_CNTR_AVGMINMAX |
+					  LPROCFS_CNTR_STDDEV,
+};
 #define LC_MIN_INIT ((~(__u64)0) >> 1)
 
 struct lprocfs_counter_header {
@@ -395,7 +401,8 @@ struct obd_histogram;
 #define JOBSTATS_NODELOCAL		"nodelocal"
 #define JOBSTATS_SESSION		"session"
 
-typedef void (*cntr_init_callback)(struct lprocfs_stats *stats);
+typedef void (*cntr_init_callback)(struct lprocfs_stats *stats,
+				   unsigned int offset);
 
 struct obd_job_stats {
 	struct cfs_hash	       *ojs_hash;	/* hash of jobids */

lustre/mdt/mdt_fs.c
@@ -78,7 +78,7 @@ int mdt_export_stats_init(struct obd_device *obd, struct obd_export *exp,
 	if (stats->nid_stats == NULL)
 		RETURN(-ENOMEM);
 
-	mdt_stats_counter_init(stats->nid_stats);
+	mdt_stats_counter_init(stats->nid_stats, 0);
 
 	rc = lprocfs_register_stats(stats->nid_proc, "stats", stats->nid_stats);
 	if (rc != 0) {

lustre/mdt/mdt_handler.c
@@ -415,6 +415,7 @@ static int mdt_statfs(struct tgt_session_info *tsi)
 	struct obd_statfs *osfs;
 	struct mdt_body *reqbody = NULL;
 	struct mdt_statfs_cache *msf;
+	ktime_t kstart = ktime_get();
 	int rc;
 
 	ENTRY;
@@ -504,7 +505,8 @@ static int mdt_statfs(struct tgt_session_info *tsi)
 		osfs->os_bsize = 1 << COMPAT_BSIZE_SHIFT;
 	}
 	if (rc == 0)
-		mdt_counter_incr(req, LPROC_MDT_STATFS);
+		mdt_counter_incr(req, LPROC_MDT_STATFS,
+				 ktime_us_delta(ktime_get(), kstart));
 out:
 	mdt_thread_info_fini(info);
 	RETURN(rc);
@@ -1295,6 +1297,7 @@ static int mdt_getattr_internal(struct mdt_thread_info *info,
 	struct mdt_body *repbody;
 	struct lu_buf *buffer = &info->mti_buf;
 	struct obd_export *exp = info->mti_exp;
+	ktime_t kstart = ktime_get();
 	int rc;
 
 	ENTRY;
@@ -1533,7 +1536,8 @@ static int mdt_getattr_internal(struct mdt_thread_info *info,
 
 out:
 	if (rc == 0)
-		mdt_counter_incr(req, LPROC_MDT_GETATTR);
+		mdt_counter_incr(req, LPROC_MDT_GETATTR,
+				 ktime_us_delta(ktime_get(), kstart));
 
 	RETURN(rc);
 }
@@ -2783,6 +2787,7 @@ static int mdt_sync(struct tgt_session_info *tsi)
 	struct ptlrpc_request	*req = tgt_ses_req(tsi);
 	struct req_capsule	*pill = tsi->tsi_pill;
 	struct mdt_body		*body;
+	ktime_t			 kstart = ktime_get();
 	int			 rc;
 
 	ENTRY;
@@ -2819,7 +2824,8 @@ static int mdt_sync(struct tgt_session_info *tsi)
 		mdt_thread_info_fini(info);
 	}
 	if (rc == 0)
-		mdt_counter_incr(req, LPROC_MDT_SYNC);
+		mdt_counter_incr(req, LPROC_MDT_SYNC,
+				 ktime_us_delta(ktime_get(), kstart));
 
 	RETURN(rc);
 }

lustre/mdt/mdt_internal.h
@@ -1298,8 +1298,8 @@ enum mdt_stat_idx {
 	LPROC_MDT_LAST,
 };
 
-void mdt_counter_incr(struct ptlrpc_request *req, int opcode);
-void mdt_stats_counter_init(struct lprocfs_stats *stats);
+void mdt_counter_incr(struct ptlrpc_request *req, int opcode, long amount);
+void mdt_stats_counter_init(struct lprocfs_stats *stats, unsigned int offset);
 int mdt_tunables_init(struct mdt_device *mdt, const char *name);
 void mdt_tunables_fini(struct mdt_device *mdt);
 
@@ -1309,7 +1309,8 @@ int lprocfs_mdt_open_files_seq_open(struct inode *inode,
 void mdt_rename_counter_tally(struct mdt_thread_info *info,
 			      struct mdt_device *mdt,
 			      struct ptlrpc_request *req,
-			      struct mdt_object *src, struct mdt_object *tgt);
+			      struct mdt_object *src, struct mdt_object *tgt,
+			      long count);
 
 static inline struct obd_device *mdt2obd_dev(const struct mdt_device *mdt)
 {

lustre/mdt/mdt_io.c
@@ -35,12 +35,6 @@
 
 /* functions below are stubs for now, they will be implemented with
  * grant support on MDT */
-static inline void mdt_io_counter_incr(struct obd_export *exp, int opcode,
-				       char *jobid, long amount)
-{
-	return;
-}
-
 static inline void mdt_dom_read_lock(struct mdt_object *mo)
 {
 	down_read(&mo->mot_dom_sem);
@@ -364,6 +358,8 @@ static int mdt_preprw_read(const struct lu_env *env, struct obd_export *exp,
 			   struct niobuf_remote *rnb, int *nr_local,
 			   struct niobuf_local *lnb, char *jobid)
 {
+	struct tgt_session_info *tsi = tgt_ses_info(env);
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
 	struct dt_object *dob;
 	int i, j, rc, tot_bytes = 0;
 	int maxlnb = *nr_local;
@@ -422,7 +418,7 @@ static int mdt_preprw_read(const struct lu_env *env, struct obd_export *exp,
 	if (unlikely(rc))
 		GOTO(buf_put, rc);
 
-	mdt_io_counter_incr(exp, LPROC_MDT_IO_READ, jobid, tot_bytes);
+	mdt_counter_incr(req, LPROC_MDT_IO_READ, tot_bytes);
 	RETURN(0);
 buf_put:
 	dt_bufs_put(env, dob, lnb, *nr_local);
@@ -437,6 +433,8 @@ static int mdt_preprw_write(const struct lu_env *env, struct obd_export *exp,
 			    struct niobuf_remote *rnb, int *nr_local,
 			    struct niobuf_local *lnb, char *jobid)
 {
+	struct tgt_session_info *tsi = tgt_ses_info(env);
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
 	struct dt_object *dob;
 	int i, j, k, rc = 0, tot_bytes = 0;
 	int maxlnb = *nr_local;
@@ -496,7 +494,7 @@ static int mdt_preprw_write(const struct lu_env *env, struct obd_export *exp,
 	if (likely(rc))
 		GOTO(err, rc);
 
-	mdt_io_counter_incr(exp, LPROC_MDT_IO_WRITE, jobid, tot_bytes);
+	mdt_counter_incr(req, LPROC_MDT_IO_WRITE, tot_bytes);
 	RETURN(0);
 err:
 	dt_bufs_put(env, dob, lnb, *nr_local);
@@ -849,6 +847,7 @@ stop:
 int mdt_punch_hdl(struct tgt_session_info *tsi)
 {
 	const struct obdo *oa = &tsi->tsi_ost_body->oa;
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
 	struct ost_body *repbody;
 	struct mdt_thread_info *info;
 	struct lu_attr *la;
@@ -859,6 +858,7 @@ int mdt_punch_hdl(struct tgt_session_info *tsi)
 	struct dt_object *dob;
 	__u64 flags = 0;
 	struct lustre_handle lh = { 0, };
+	ktime_t kstart = ktime_get();
 	__u64 start, end;
 	int rc;
 	bool srvlock;
@@ -942,8 +942,8 @@ int mdt_punch_hdl(struct tgt_session_info *tsi)
 		GOTO(out_put, rc);
 
 	mdt_dom_obj_lvb_update(tsi->tsi_env, mo, false);
-	mdt_io_counter_incr(tsi->tsi_exp, LPROC_MDT_IO_PUNCH,
-			    tsi->tsi_jobid, 1);
+	mdt_counter_incr(req, LPROC_MDT_IO_PUNCH,
+			 ktime_us_delta(ktime_get(), kstart));
 	EXIT;
 out_put:
 	lu_object_put(tsi->tsi_env, &mo->mot_obj);

lustre/mdt/mdt_lproc.c
@@ -165,7 +165,7 @@ void mdt_rename_counter_tally(struct mdt_thread_info *info,
 			      struct mdt_device *mdt,
 			      struct ptlrpc_request *req,
 			      struct mdt_object *src,
-			      struct mdt_object *tgt)
+			      struct mdt_object *tgt, long count)
 {
         struct md_attr *ma = &info->mti_attr;
         struct rename_stats *rstats = &mdt->mdt_rename_stats;
@@ -181,13 +181,13 @@ void mdt_rename_counter_tally(struct mdt_thread_info *info,
         }
 
         if (src == tgt) {
-		mdt_counter_incr(req, LPROC_MDT_SAMEDIR_RENAME);
+		mdt_counter_incr(req, LPROC_MDT_SAMEDIR_RENAME, count);
                 lprocfs_oh_tally_log2(&rstats->hist[RENAME_SAMEDIR_SIZE],
                                       (unsigned int)ma->ma_attr.la_size);
                 return;
         }
 
-	mdt_counter_incr(req, LPROC_MDT_CROSSDIR_RENAME);
+	mdt_counter_incr(req, LPROC_MDT_CROSSDIR_RENAME, count);
         lprocfs_oh_tally_log2(&rstats->hist[RENAME_CROSSDIR_SRC_SIZE],
                               (unsigned int)ma->ma_attr.la_size);
 
@@ -1346,20 +1346,21 @@ int lprocfs_mdt_open_files_seq_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-void mdt_counter_incr(struct ptlrpc_request *req, int opcode)
+void mdt_counter_incr(struct ptlrpc_request *req, int opcode, long amount)
 {
 	struct obd_export *exp = req->rq_export;
 
 	if (exp->exp_obd && exp->exp_obd->obd_md_stats)
-		lprocfs_counter_incr(exp->exp_obd->obd_md_stats,
-				     opcode + LPROC_MD_LAST_OPC);
+		lprocfs_counter_add(exp->exp_obd->obd_md_stats,
+				    opcode + LPROC_MD_LAST_OPC, amount);
 	if (exp->exp_nid_stats && exp->exp_nid_stats->nid_stats != NULL)
-		lprocfs_counter_incr(exp->exp_nid_stats->nid_stats, opcode);
+		lprocfs_counter_add(exp->exp_nid_stats->nid_stats, opcode,
+				    amount);
 	if (exp->exp_obd && exp->exp_obd->u.obt.obt_jobstats.ojs_hash &&
 	    (exp_connect_flags(exp) & OBD_CONNECT_JOBSTATS))
 		lprocfs_job_stats_log(exp->exp_obd,
 				      lustre_msg_get_jobid(req->rq_reqmsg),
-				      opcode, 1);
+				      opcode, amount);
 }
 
 static const char * const mdt_stats[] = {
@@ -1385,19 +1386,24 @@ static const char * const mdt_stats[] = {
 	[LPROC_MDT_MIGRATE]		= "migrate",
 };
 
-void mdt_stats_counter_init(struct lprocfs_stats *stats)
+void mdt_stats_counter_init(struct lprocfs_stats *stats, unsigned int offset)
 {
-	int idx;
+	int array_size = ARRAY_SIZE(mdt_stats);
+	int oidx; /* obd_md_stats index */
+	int midx; /* mdt_stats index */
 
-	LASSERT(stats && stats->ls_num >= ARRAY_SIZE(mdt_stats));
+	LASSERT(stats && stats->ls_num >= offset + array_size);
 
-	for (idx = 0; idx < ARRAY_SIZE(mdt_stats); idx++) {
-		int flags = 0;
-
-		if (idx == LPROC_MDT_IO_WRITE || idx == LPROC_MDT_IO_READ)
-			flags = LPROCFS_CNTR_AVGMINMAX;
-
-		lprocfs_counter_init(stats, idx, flags, mdt_stats[idx], "reqs");
+	for (midx = 0; midx < array_size; midx++) {
+		oidx = midx + offset;
+		if (midx == LPROC_MDT_IO_READ || midx == LPROC_MDT_IO_WRITE)
+			lprocfs_counter_init(stats, oidx,
+					     LPROCFS_TYPE_BYTES_FULL,
+					     mdt_stats[midx], "bytes");
+		else
+			lprocfs_counter_init(stats, oidx,
+					     LPROCFS_TYPE_LATENCY,
+					     mdt_stats[midx], "usecs");
 	}
 }
 
@@ -1405,7 +1411,6 @@ int mdt_tunables_init(struct mdt_device *mdt, const char *name)
 {
 	struct obd_device *obd = mdt2obd_dev(mdt);
 	int rc;
-	int i;
 
 	ENTRY;
 	LASSERT(name != NULL);
@@ -1444,17 +1449,7 @@ int mdt_tunables_init(struct mdt_device *mdt, const char *name)
 		return rc;
 
 	/* add additional MDT md_stats after the default ones */
-	for (i = 0; i < ARRAY_SIZE(mdt_stats); i++) {
-		int idx = i + LPROC_MD_LAST_OPC;
-		int flags = 0;
-
-		if (idx == LPROC_MDT_IO_WRITE || idx == LPROC_MDT_IO_READ)
-			flags = LPROCFS_CNTR_AVGMINMAX;
-
-		lprocfs_counter_init(obd->obd_md_stats, idx, flags,
-				     mdt_stats[i], "reqs");
-	}
-
+	mdt_stats_counter_init(obd->obd_md_stats, LPROC_MD_LAST_OPC);
 	rc = lprocfs_job_stats_init(obd, ARRAY_SIZE(mdt_stats),
 				    mdt_stats_counter_init);
 

commit 8a1334626ec2f3dba954d5a7e2162eb3fe966c47

LU-13733 llite: report client stats sumsq
Commit cd8fb tries to account sumsq for every client operation, but
lprocfs_counter_init() did not init them properly, also add a test
case to verify new format of client stats.

Fixes: cd8fb1e8d300 ("LU-13597 ofd: add more information to job_stats")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Change-Id: I4c7fc4c3958fdab757a9755cc851836971a4b700
Reviewed-on: https://review.whamcloud.com/39223
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/lproc_llite.c
@@ -1656,19 +1656,16 @@ int ll_debugfs_register_super(struct super_block *sb, const char *name)
 	/* do counter init */
 	for (id = 0; id < LPROC_LL_FILE_OPCODES; id++) {
 		u32 type = llite_opcode_table[id].type;
-		void *ptr = NULL;
+		void *ptr = "unknown";
 
 		if (type & LPROCFS_TYPE_REQS)
 			ptr = "reqs";
 		else if (type & LPROCFS_TYPE_BYTES)
 			ptr = "bytes";
-		else if (type & LPROCFS_TYPE_PAGES)
-			ptr = "pages";
 		else if (type & LPROCFS_TYPE_USEC)
 			ptr = "usec";
 		lprocfs_counter_init(sbi->ll_stats,
-				     llite_opcode_table[id].opcode,
-				     (type & LPROCFS_CNTR_AVGMINMAX),
+				     llite_opcode_table[id].opcode, type,
 				     llite_opcode_table[id].opname, ptr);
 	}
 

lustre/tests/sanity.sh
@@ -16492,6 +16492,17 @@ test_205b() {
 }
 run_test 205b "Verify job stats jobid and output format"
 
+# LU-13733
+test_205c() {
+	$LCTL set_param llite.*.stats=0
+	dd if=/dev/zero of=$DIR/$tfile.1 bs=4k count=1
+	$LCTL get_param llite.*.stats
+	$LCTL get_param llite.*.stats | grep \
+		"write_bytes *1 samples \[bytes\] 4096 4096 4096 16777216" ||
+			error "wrong client stats format found"
+}
+run_test 205c "Verify client stats format"
+
 # LU-1480, LU-1773 and LU-1657
 test_206() {
 	mkdir -p $DIR/$tdir