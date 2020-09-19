lustre/lustre-release

commit 40d91eafe257fb407d27c54cd2f7ae9961672f60

LU-12275 sec: atomicity of encryption context getting/setting
Encryption layer needs to set an encryption context on files and dirs
that are encrypted. This context is stored as an extended attribute,
that then needs to be fetched upon metadata ops like lookup, getattr,
open, truncate, and layout.

With this patch we send encryption context to the MDT along with
create RPCs. This closes the insecure window between creation and
setting of the encryption context, and saves a setxattr request.

This patch also introduces a way to have the MDT return encryption
context upon granted lock reply, making the encryption context
retrieval atomic, and sparing the client an additional getxattr
request.

Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=ldiskfs mdscount=2 mdtcount=4
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=zfs mdscount=2 mdtcount=4
Test-Parameters: clientversion=2.12 env=SANITY_EXCEPT="27M 56ra 151 156 802"
Test-Parameters: serverversion=2.12 env=SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Test-Parameters: serverversion=2.12 clientdistro=el8.1 env=SANITYN_EXCEPT=106,SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I45599cdff13d5587103aff6edd699abcda6cb8f4
Reviewed-on: https://review.whamcloud.com/38430
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_internal.h
@@ -600,8 +600,18 @@ static inline int mdo_declare_xattr_set(const struct lu_env *env,
                                         const char *name,
                                         int fl, struct thandle *handle)
 {
-        struct dt_object *next = mdd_object_child(obj);
-        return dt_declare_xattr_set(env, next, buf, name, fl, handle);
+	struct dt_object *next = mdd_object_child(obj);
+	int rc;
+
+	rc = dt_declare_xattr_set(env, next, buf, name, fl, handle);
+	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+		struct lu_attr la = { 0 };
+
+		la.la_valid = LA_FLAGS;
+		la.la_flags = LUSTRE_ENCRYPT_FL;
+		rc = dt_declare_attr_set(env, next, &la, handle);
+	}
+	return rc;
 }
 
 static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
@@ -609,11 +619,20 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 				int fl, struct thandle *handle)
 {
 	struct dt_object *next = mdd_object_child(obj);
+	int rc;
 
 	if (!mdd_object_exists(obj))
 		return -ENOENT;
 
-	return dt_xattr_set(env, next, buf, name, fl, handle);
+	rc = dt_xattr_set(env, next, buf, name, fl, handle);
+	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+		struct lu_attr la = { 0 };
+
+		la.la_valid = LA_FLAGS;
+		la.la_flags = LUSTRE_ENCRYPT_FL;
+		rc = dt_attr_set(env, next, &la, handle);
+	}
+	return rc;
 }
 
 static inline int mdo_declare_xattr_del(const struct lu_env *env,

lustre/tests/sanity-sec.sh
@@ -3533,6 +3533,61 @@ test_48b() {
 }
 run_test 48b "encrypted file: concurrent truncate"
 
+trace_cmd() {
+	local cmd="$@"
+	local xattr_name="security.c"
+
+	sync ; sync ; echo 3 > /proc/sys/vm/drop_caches
+	$LCTL set_param debug=+info
+	$LCTL clear
+
+	echo $cmd
+	eval $cmd
+
+	$LCTL dk | grep -E "get xattr '${xattr_name}'|get xattrs"
+	[ $? -ne 0 ] || error "get xattr event was triggered"
+}
+
+test_49() {
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	stack_trap cleanup_for_enc_tests EXIT
+	setup_for_enc_tests
+
+	local dirname=$DIR/$tdir/subdir
+
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+	trace_cmd $LFS setdirstripe -i 1 $dirname/d2
+	trace_cmd $LFS migrate -m 0 $dirname/d2
+
+	$LFS setdirstripe -i 1 -c 1 $dirname/d3
+	dirname=$dirname/d3/subdir
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+}
+run_test 49 "Avoid getxattr for encryption context"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {

lustre/include/lustre_crypto.h
@@ -30,6 +30,9 @@
 #define _LUSTRE_CRYPTO_H_
 
 struct ll_sb_info;
+int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
+		    bool preload);
+void llcrypt_free_ctx(void *encctx, __u32 size);
 bool ll_sbi_has_test_dummy_encryption(struct ll_sb_info *sbi);
 bool ll_sbi_has_encrypt(struct ll_sb_info *sbi);
 void ll_sbi_set_encrypt(struct ll_sb_info *sbi, bool set);

lustre/include/lustre_export.h
@@ -475,6 +475,11 @@ static inline int exp_connect_sepol(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_SELINUX_POLICY);
 }
 
+static inline int exp_connect_encrypt(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_req_layout.h
@@ -59,7 +59,7 @@ enum req_location {
 };
 
 /* Maximal number of fields (buffers) in a request message. */
-#define REQ_MAX_FIELD_NR 11
+#define REQ_MAX_FIELD_NR 12
 
 struct req_capsule {
         struct ptlrpc_request   *rc_req;
@@ -260,6 +260,7 @@ extern struct req_msg_field RMF_CLOSE_DATA;
 extern struct req_msg_field RMF_FILE_SECCTX_NAME;
 extern struct req_msg_field RMF_FILE_SECCTX;
 extern struct req_msg_field RMF_FID_ARRAY;
+extern struct req_msg_field RMF_FILE_ENCCTX;
 
 /*
  * connection handle received in MDS_CONNECT request.

lustre/include/md_object.h
@@ -160,6 +160,10 @@ struct md_op_spec {
 	void		*sp_cr_file_secctx; /* xattr value */
 	size_t		 sp_cr_file_secctx_size; /* xattr value size */
 
+	/* File encryption context for creates. */
+	void		*sp_cr_file_encctx; /* enc ctx value */
+	size_t		 sp_cr_file_encctx_size; /* enc ctx size */
+
 	/* Archive ID used for auto PCC attach when create newly files. */
 	__u32		 sp_archive_id;
 

lustre/include/obd.h
@@ -914,6 +914,10 @@ struct md_op_data {
 	void		       *op_file_secctx;
 	__u32			op_file_secctx_size;
 
+	/* File encryption context, for creates/metadata ops */
+	void		       *op_file_encctx;
+	__u32			op_file_encctx_size;
+
 	__u32			op_projid;
 
 	/* Used by readdir */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1318,11 +1318,10 @@ lov_mds_md_max_stripe_count(__kernel_size_t buf_size, __u32 lmm_magic)
 #define OBD_MD_FLOSTLAYOUT   (0x0080000000000000ULL) /* contain ost_layout */
 #define OBD_MD_FLPROJID      (0x0100000000000000ULL) /* project ID */
 #define OBD_MD_SECCTX        (0x0200000000000000ULL) /* embed security xattr */
-
 #define OBD_MD_FLLAZYSIZE    (0x0400000000000000ULL) /* Lazy size */
 #define OBD_MD_FLLAZYBLOCKS  (0x0800000000000000ULL) /* Lazy blocks */
-
 #define OBD_MD_FLBTIME       (0x1000000000000000ULL) /* birth time */
+#define OBD_MD_ENCCTX        (0x2000000000000000ULL) /* embed encryption ctx */
 
 #define OBD_MD_FLALLQUOTA (OBD_MD_FLUSRQUOTA | \
 			   OBD_MD_FLGRPQUOTA | \

commit e8f74fb0f5c9306ee5a099133799e03e09ca8e47

LU-12275 sec: verify dir is empty when setting enc policy
When setting an encryption policy on a directory that we want to
be encrypted, make sure it is empty.
Otherwise return -ENOTEMPTY from server to client doing the ioctl.

Also add test to exercise the ioctl interface to get/set encryption
policies on directories, thanks to the fscrypt userspace tool.
And test the ability to lock an encrypted directory.

Fixes: 40d91eafe257 ("LU-12275 sec: atomicity of encryption context getting/setting")
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 52 53 54" clientdistro=el8.1 fstype=ldiskfs mdscount=2 mdtcount=4
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 52 53 54" clientdistro=el8.1 fstype=zfs mdscount=2 mdtcount=4
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Iabffd24800314b12908f505d7e66492be7292a16
Reviewed-on: https://review.whamcloud.com/39617
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_internal.h
@@ -620,19 +620,23 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 				int fl, struct thandle *handle)
 {
 	struct dt_object *next = mdd_object_child(obj);
-	int rc;
+	int rc = 0;
 
 	if (!mdd_object_exists(obj))
 		return -ENOENT;
 
-	rc = dt_xattr_set(env, next, buf, name, fl, handle);
-	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+	if ((strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) &&
+	    (!S_ISDIR(mdd_object_type(obj)) ||
+	     (rc = mdd_dir_is_empty(env, obj)) == 0)) {
 		struct lu_attr la = { 0 };
 
 		la.la_valid = LA_FLAGS;
 		la.la_flags = LUSTRE_ENCRYPT_FL;
 		rc = dt_attr_set(env, next, &la, handle);
 	}
+	if (rc >= 0)
+		rc = dt_xattr_set(env, next, buf, name, fl, handle);
+
 	return rc;
 }
 

lustre/tests/sanity-sec.sh
@@ -3967,6 +3967,81 @@ test_53() {
 }
 run_test 53 "Mixed PAGE_SIZE clients"
 
+test_54() {
+	local testdir=$DIR/$tdir/$ID0
+	local testfile=$testdir/$tfile
+	local testfile2=$testdir/${tfile}2
+	local tmpfile=$TMP/${tfile}.tmp
+	local resfile=$TMP/${tfile}.res
+
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	which fscrypt || skip "This test needs fscrypt userspace tool"
+
+	fscrypt setup --force --verbose || error "fscrypt global setup failed"
+	sed -i 's/\(.*\)policy_version\(.*\):\(.*\)\"[0-9]*\"\(.*\)/\1policy_version\2:\3"2"\4/' \
+		/etc/fscrypt.conf
+	fscrypt setup --verbose $MOUNT || error "fscrypt setup $MOUNT failed"
+	mkdir -p $testdir
+	chown -R $ID0:$ID0 $testdir
+
+	echo -e 'mypass\nmypass' | su - $USER0 -c "fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector $testdir" ||
+		error "fscrypt encrypt failed"
+
+	echo -e 'mypass\nmypass' | su - $USER0 -c "fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector2 $testdir" &&
+		error "second fscrypt encrypt should have failed"
+
+	mkdir -p ${testdir}2 || error "mkdir ${testdir}2 failed"
+	touch ${testdir}2/f || error "mkdir ${testdir}2/f failed"
+	cancel_lru_locks
+
+	echo -e 'mypass\nmypass' | fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector3 ${testdir}2 &&
+		error "fscrypt encrypt on non-empty dir should have failed"
+
+	$RUNAS dd if=/dev/urandom of=$testfile bs=127 count=1 conv=fsync ||
+		error "write to encrypted file $testfile failed"
+	cp $testfile $tmpfile
+	$RUNAS dd if=/dev/urandom of=$testfile2 bs=127 count=1 conv=fsync ||
+		error "write to encrypted file $testfile2 failed"
+
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (1)"
+
+	$RUNAS hexdump -C $testfile &&
+		error "reading $testfile should have failed without key"
+
+	echo mypass | $RUNAS fscrypt unlock --verbose $testdir ||
+		error "fscrypt unlock $testdir failed (1)"
+
+	$RUNAS cat $testfile > $resfile ||
+		error "reading $testfile failed"
+
+	cmp -bl $tmpfile $resfile || error "file read differs from file written"
+
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (2)"
+
+	$RUNAS hexdump -C $testfile2 &&
+		error "reading $testfile2 should have failed without key"
+
+	echo mypass | $RUNAS fscrypt unlock --verbose $testdir ||
+		error "fscrypt unlock $testdir failed (2)"
+
+	rm -rf $testdir/*
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (3)"
+
+	rm -f $tmpfile $resfile
+}
+run_test 54 "Encryption policies with fscrypt"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {