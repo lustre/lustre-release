5da99051e58b9e9079b66a275d6c47e1e109eee5
Oleg Drokin <green@whamcloud.com>
2020-10-23 02:56:04 -0400

    LU-14069 ldlm: Fix unbounded OBD_FAIL_LDLM_CANCEL_BL_CB_RACE wait
    in ldlm_handle_cp_callback the while loop is clearly supposed
    to be limited by the "to" value of 1 second, but is not.
    Seems to have been broken by all the Solaris porting in HEAD
    all the way back in 2008.
    Restore the to assignment to make it not hang indefinitely.
    
    Change-Id: I449bfd7f585ab7db475fb3fd4cbbd876126ff789
    Fixes: adde80ff ("Land b_head_libcfs onto HEAD")
    Signed-off-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/40375
    Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

--- lustre/ldlm/ldlm_lockd.c
+++ lustre/ldlm/ldlm_lockd.c
@@ -1927,7 +1927,7 @@ static int ldlm_handle_cp_callback(struct ptlrpc_request *req,
 		ldlm_callback_reply(req, 0);
 
 		while (to > 0) {
-			schedule_timeout_interruptible(to);
+			to = schedule_timeout_interruptible(to);
 			if (ldlm_is_granted(lock) ||
 			    ldlm_is_destroyed(lock))
 				break;
