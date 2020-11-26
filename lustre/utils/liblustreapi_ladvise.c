lustre/lustre-release

commit e14246641c04c9e3004043f58f469532223d06d6

LU-4931 ladvise: Add feature of giving file access advices
The fadvise() system call provided by Linux kernel enables
applications to give advices or hints about how a file will be
accessed. However, It is only a client side mechanism which is
not enough for distributed file systems like Lustre, because in
order to tune system-wide cache or read-ahead policies, servers
need to understand the advices too.

This patch adds a new feature named ladvise which provides new
APIs and utils to give advices about the access pattern of Lustre
files with the purpose of performance improvement. It is similar
to Linux fadvise() system call, except it forwards the advices
directly from Lustre client to server. The server side codes will
apply appropriate read-ahead and caching techniques for the
corresponding files.

A typical workload for ladvise is e.g. a bunch of different
clients are doing small random reads of a file, so prefetching
pages into OSS cache with big linear reads before the random IO
is a net benefit. Fetching all that data into each client cache
with fadvise() may not be, due to much more data being sent to
the client.

Signed-off-by: Li Xi <lixi@ddn.com>
Change-Id: I6de14ed87199949963360d393897ff79826c9819
Reviewed-on: http://review.whamcloud.com/10029
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/utils/liblustreapi_ladvise.c
@@ -1,0 +1,85 @@
+/*
+ * LGPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * (C) Copyright (c) 2015, DataDirect Networks Inc, all rights reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * LGPL version 2.1 or (at your discretion) any later version.
+ * LGPL version 2.1 accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * LGPL HEADER END
+ */
+/*
+ * lustre/utils/liblustreapi_ladvise.c
+ *
+ * lustreapi library for ladvise
+ *
+ * Author: Li Xi <lixi@ddn.com>
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+#include <lustre/lustreapi.h>
+#include <lustre/lustre_idl.h>
+#include "lustreapi_internal.h"
+
+/*
+ * Give file access advices
+ *
+ * \param fd       File to give advice on.
+ * \param ladvise  Advice to give.
+ *
+ * \retval 0 on success.
+ * \retval -errno on failure.
+ */
+int llapi_ladvise(int fd, unsigned long long flags, int num_advise,
+		  struct lu_ladvise *ladvise)
+{
+	int rc;
+	struct ladvise_hdr *ladvise_hdr;
+
+	if (num_advise < 1 || num_advise >= LAH_COUNT_MAX) {
+		errno = EINVAL;
+		llapi_error(LLAPI_MSG_ERROR, -EINVAL,
+			    "bad advice number %d", num_advise);
+		return -1;
+	}
+
+	ladvise_hdr = calloc(1, offsetof(typeof(*ladvise_hdr),
+			     lah_advise[num_advise]));
+	if (ladvise_hdr == NULL) {
+		errno = ENOMEM;
+		llapi_error(LLAPI_MSG_ERROR, -ENOMEM, "not enough memory");
+		return -1;
+	}
+	ladvise_hdr->lah_magic = LADVISE_MAGIC;
+	ladvise_hdr->lah_count = num_advise;
+	ladvise_hdr->lah_flags = flags & LF_MASK;
+	memcpy(ladvise_hdr->lah_advise, ladvise, sizeof(*ladvise) * num_advise);
+
+	rc = ioctl(fd, LL_IOC_LADVISE, ladvise_hdr);
+	if (rc < 0) {
+		llapi_error(LLAPI_MSG_ERROR, -errno, "cannot give advice");
+		return -1;
+	}
+	return 0;
+}
+

lustre/contrib/wireshark/packet-lustre.c
@@ -181,6 +181,7 @@ typedef enum {
   OST_QUOTACHECK = 18,
   OST_QUOTACTL   = 19,
   OST_QUOTA_ADJUST_QUNIT = 20,
+  OST_LADVISE = 21,
   OST_LAST_OPC
 } ost_cmd_t ;
 
@@ -1022,6 +1023,11 @@ static int hf_lustre_ldlm_intent_opc_pin      = -1;
 static int hf_lustre_llog_hdr_llh_flag_zap_when_empty = -1;
 static int hf_lustre_llog_hdr_llh_flag_is_cat = -1;
 static int hf_lustre_llog_hdr_llh_flag_is_play = -1;
+static int hf_lustre_lu_ladvise = -1;
+static int hf_lustre_lu_ladvise_lla_start = -1;
+static int hf_lustre_lu_ladvise_lla_end = -1;
+static int hf_lustre_lu_ladvise_lla_advice = -1;
+static int hf_lustre_lu_ladvise_lla_padding = -1;
 /* --------------------------------------------------------------------*/
 
 
@@ -1145,7 +1151,9 @@ const value_string lustre_op_codes[] = {
   {17 , "OST_SET_INFO"},
   {18 , "OST_QUOTACHECK"},
   {19 , "OST_QUOTACTL"},
-  {20 , "OST_LAST_OPC"},
+  {20 , "OST_QUOTA_ADJUST_QUNIT"},
+  {21 , "OST_LADVISE"},
+  {22 , "OST_LAST_OPC"},
   /*MDS Opcodes*/
   {33 , "MDS_GETATTR"},
   {34 , "MDS_GETATTR_NAME"},
@@ -8381,6 +8389,83 @@ lustre_dissect_generic_connect(tvbuff_t *tvb _U_, int offset _U_, packet_info *p
   return offset;
 }
 
+int
+lustre_dissect_struct_capa(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_,
+			   proto_tree *parent_tree _U_, int hf_index _U_, guint32 buf_num)
+{
+  proto_item *item = NULL;
+  proto_tree *tree = NULL;
+  int old_offset;
+  int data_len;
+
+  old_offset=offset;
+
+  data_len = LUSTRE_BUFFER_LEN(buf_num);
+  if (data_len == 0)
+    return offset;
+
+  if (parent_tree) {
+    item = proto_tree_add_item(parent_tree, hf_index, tvb, offset, -1, TRUE);
+    tree = proto_item_add_subtree(item, ett_lustre_capa);
+  }
+
+  offset=lustre_dissect_struct_lu_fid(tvb,offset,pinfo,tree,hf_lustre_capa_fid);
+
+  offset=lustre_dissect_element_capa_opc(tvb,offset,pinfo,tree);
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_capa_uid);
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_capa_gid);
+
+  offset=lustre_dissect_element_capa_flags(tvb,offset,pinfo,tree);
+
+  offset=dissect_uint32(tvb,offset,pinfo,tree,hf_lustre_capa_keyid);
+
+  offset=dissect_uint32(tvb,offset,pinfo,tree,hf_lustre_capa_timeout);
+
+  offset=dissect_uint32(tvb,offset,pinfo,tree,hf_lustre_capa_expiry);
+
+  offset=lustre_dissect_struct_hmac(tvb,offset,pinfo,tree, hf_lustre_capa_hmac, 64);
+
+  if (item)
+    proto_item_set_len(item, offset-old_offset);
+  return offset;
+}
+
+
+int
+lustre_dissect_struct_lu_ladvise(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_,
+				 proto_tree *parent_tree _U_, int hf_index _U_, guint32 buf_num)
+{
+  proto_item *item = NULL;
+  proto_tree *tree = NULL;
+  int old_offset;
+  int data_len;
+
+  old_offset=offset;
+
+  data_len = LUSTRE_BUFFER_LEN(buf_num);
+  if (data_len == 0)
+    return offset;
+
+  if (parent_tree) {
+    item = proto_tree_add_item(parent_tree, hf_index, tvb, offset, -1, TRUE);
+    tree = proto_item_add_subtree(item, ett_lustre_ladvise);
+  }
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_lu_ladvise_lla_start);
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_lu_ladvise_lla_end);
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_lu_ladvise_lla_advice);
+
+  offset=dissect_uint64(tvb,offset,pinfo,tree,hf_lustre_lu_ladvise_lla_padding);
+
+  if (item)
+    proto_item_set_len(item, offset-old_offset);
+  return offset;
+}
+
 static int
 lustre_ost_opcode_process(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree * tree _U_, guint32 opc _U_, guint32 pb_type)
 {
@@ -8486,6 +8571,14 @@ lustre_ost_opcode_process(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo 
     case OST_QUOTA_ADJUST_QUNIT:
       /* [quota_adjust_qunit] in both case ? */
       offset=lustre_dissect_struct_quota_adjust_qunit(tvb, offset, pinfo, tree, hf_lustre_quota_adjust_qunit) ;
+    case OST_LADVISE:
+      /*[ost_body] in both case */
+      offset=lustre_dissect_struct_ost_body(tvb, offset, pinfo, tree, hf_lustre_ost_body) ;
+      if (pb_type == PTL_RPC_MSG_REQUEST) {
+	  offset=lustre_dissect_struct_capa(tvb, offset, pinfo, tree, hf_lustre_capa, LUSTRE_REQ_REC_OFF+1);
+	  offset=lustre_dissect_struct_lu_ladvise(tvb, offset, pinfo, tree, hf_lustre_lu_ladvise, LUSTRE_REQ_REC_OFF+2);
+      }
+      break;
   };
   return offset;
 }
@@ -11034,6 +11127,7 @@ void proto_register_dcerpc_lustre(void)
     &ett_lustre_mdt_ioepoch,
     &ett_lustre_capa,
     &ett_lustre_acl,
+    &ett_lustre_ladvise,
 	};
 
   proto_lustre = proto_register_protocol("Lustre", "lustre", "lustre");

lustre/doc/Makefile.am
@@ -58,7 +58,7 @@ MANFILES = lustre.7 lfs.1 mount.lustre.8 lctl.8 lnetctl.8 \
 	llapi_hsm_copytool_recv.3 llapi_hsm_action_end.3 \
 	llapi_hsm_action_progress.3 llapi_hsm_action_get_dfid.3 \
 	llapi_hsm_action_get_fd.3 lustreapi.7 llapi_hsm_action_begin.3 \
-	llapi_hsm_copytool_register.3
+	llapi_hsm_copytool_register.3 lfs-ladvise.1 llapi_ladvise.3
 
 SERVER_MANFILES = mkfs.lustre.8 tunefs.lustre.8
 

lustre/doc/lfs-ladvise.1
@@ -1,0 +1,58 @@
+.TH LFS-LADVISE 1 2015-11-30 "Lustre" "Lustre Utilities"
+.SH NAME
+lfs ladvise \- give file access advices or hints to server.
+.SH SYNOPSIS
+.br
+.B lfs ladvise [--advice|-a ADVICE ] [--background|-b]
+        \fB[--start|-s START[kMGT]]
+        \fB{[--end|-e END[kMGT]] | [--length|-l LENGTH[kMGT]]}
+        \fB<FILE> ...\fR
+.br
+.SH DESCRIPTION
+Give file access advices or hints to Lustre server side, usually OSS. This lfs
+utility is simlar to Linux fadvise() system call, except it can forward the
+advices from Lustre clients to servers.
+.SH OPTIONS
+.TP
+\fB\-a\fR, \fB\-\-advice\fR=\fIADVICE\fR
+Give advice or hint of type \fIADVICE\fR.
+.TP
+\fB\-b\fR, \fB\-\-background
+Enable the advices to be sent and handled asynchronously.
+.TP
+\fB\-s\fR, \fB\-\-start\fR=\fISTART_OFFSET\fR
+File range starts from \fISTART_OFFSET\fR.
+.TP
+\fB\-e\fR, \fB\-\-end\fR=\fIEND_OFFSET\fR
+File range ends at (not including) \fIEND_OFFSET\fR.
+This option may not be specified at the same time as the -l option.
+.TP
+\fB\-l\fR, \fB\-\-length\fR=\fILENGTH\fR
+File range has length of \fILENGTH\fR. This option may not be specified at the
+same time as the -e option.
+.SH NOTE
+.PP
+Typically, the "lfs ladvise" forwards the advice to Lustre servers without
+guaranteeing what and when servers will react to the advice. Actions may or
+may not triggered when the advices are recieved, depending on the type of the
+advice as well as the real-time decision of the affected server-side
+components.
+
+A typical usage of ladvise is to enable applications and users with external
+knowledge to intervene in server-side cache management. For example, if a
+bunch of different clients are doing small random reads of a file, prefetching
+pages into OSS cache with big linear reads before the random IO is a net
+benefit. Fetching all that data into each client cache with fadvise() may not
+be, due to much more data being sent to the client.
+
+The main difference between Linux fadvise() system call and ladvise is that
+fadvise() is only a client side mechanism that does not pass the advice to the
+filesystem, while ladvise can send advices or hints to Lustre server sides.
+
+.SH AVAILABILITY
+The lfs ladvise command is part of the Lustre filesystem.
+.SH SEE ALSO
+.BR lfs (1),
+.BR fadvise (2),
+.BR llapi_ladvise (3),
+.BR lustre (7)

lustre/doc/llapi_ladvise.3
@@ -1,0 +1,69 @@
+.TH llapi_ladvise 3 "2015 Dec 15" "Lustre User API"
+.SH NAME
+llapi_ladvise \- give IO advices/hints on a Lustre file to the server
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_ladvise(int " fd ", unsigned long long " flags ", int " num_advise ", struct lu_ladvise *" ladvise ");"
+.sp
+.fi
+.SH DESCRIPTION
+.LP
+.B llapi_ladvise()
+gives advices to a file specified by the file descriptor
+.IR fd .
+.IR ladvise
+points to an array that contains the information of the advices. The number
+of advices is
+.IR num_advise ,
+which should be smaller than
+.B MAX_NUM_LADVISE.
+Each of these advises is given in
+.I lu_ladvise
+structure, which contains the following fields:
+.PP
+.in +4n
+.nf
+struct lu_ladvise {
+    __u64 lla_advice;  /* Advice type */
+    __u64 lla_start;   /* Start offset of file */
+    __u64 lla_end;     /* End offset of file(not included) */
+    __u64 lla_padding; /* Not used yet */
+};
+.fi
+.in
+.LP
+In addition, zero or more flags can be
+.RI bitwise-or 'd
+in
+.IR flags .
+The full list of file creation flags and file status flags is as follows:
+.TP
+.B LF_ASYNC
+Client return to userspace immediately after submitting ladvise RPCs, leaving
+server threads to handle the advices asynchronously.
+.PP
+.SH RETURN VALUES
+.PP
+.B llapi_ladvise()
+return 0 on success, or -1 if an error occurred (in which case, errno is set
+appropriately).
+.SH ERRORS
+.TP 15
+.SM ENOMEM
+Insufficient memory to complete operation.
+.TP
+.SM EINVAL
+One or more invalid arguments are given.
+.TP
+.SM EFAULT
+memory region pointed by
+.I ladvise
+is not properly mapped.
+.TP
+.SM ENOTSUPP
+Advice type is not supported.
+.SH "SEE ALSO"
+.BR lfs-ladvise (1),
+.BR liblustreapi (7)

lustre/include/cl_object.h
@@ -1395,6 +1395,11 @@ enum cl_io_type {
          * cl_io_loop() is never called for it.
          */
         CIT_MISC,
+	/**
+	 * ladvise handling
+	 * To give advice about access of a file
+	 */
+	CIT_LADVISE,
         CIT_OP_NR
 };
 
@@ -1727,7 +1732,6 @@ struct cl_io_rw_common {
         int         crw_nonblock;
 };
 
-
 /**
  * State for io.
  *
@@ -1799,6 +1803,14 @@ struct cl_io {
 			/* how many pages were written/discarded */
 			unsigned int       fi_nr_written;
 		} ci_fsync;
+		struct cl_ladvise_io {
+			__u64			 li_start;
+			__u64			 li_end;
+			/** file system level fid */
+			struct lu_fid		*li_fid;
+			enum lu_ladvise_type	 li_advice;
+			__u64			 li_flags;
+		} ci_ladvise;
         } u;
         struct cl_2queue     ci_queue;
         size_t               ci_nob;

lustre/include/dt_object.h
@@ -1318,6 +1318,27 @@ struct dt_body_operations {
 			   __u64 start,
 			   __u64 end,
 			   struct thandle *th);
+	/**
+	 * Give advices on specified region in an object.
+	 *
+	 * This method is used to give advices about access pattern on an
+	 * given region of the object. The disk filesystem understands
+	 * the advices and tunes cache/read-ahead policies.
+	 *
+	 * \param[in] env	execution environment for this thread
+	 * \param[in] dt	object
+	 * \param[in] start	the start of the region affected
+	 * \param[in] end	the end of the region affected
+	 * \param[in] advice	advice type
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int   (*dbo_ladvise)(const struct lu_env *env,
+			     struct dt_object *dt,
+			     __u64 start,
+			     __u64 end,
+			     enum lu_ladvise_type advice);
 };
 
 /**
@@ -2391,6 +2412,15 @@ static inline int dt_punch(const struct lu_env *env, struct dt_object *dt,
         LASSERT(dt->do_body_ops);
         LASSERT(dt->do_body_ops->dbo_punch);
 	return dt->do_body_ops->dbo_punch(env, dt, start, end, th);
+}
+
+static inline int dt_ladvise(const struct lu_env *env, struct dt_object *dt,
+			     __u64 start, __u64 end, int advice)
+{
+	LASSERT(dt);
+	LASSERT(dt->do_body_ops);
+	LASSERT(dt->do_body_ops->dbo_ladvise);
+	return dt->do_body_ops->dbo_ladvise(env, dt, start, end, advice);
 }
 
 static inline int dt_fiemap_get(const struct lu_env *env, struct dt_object *d,

lustre/include/lustre/lustre_idl.h
@@ -1415,7 +1415,8 @@ typedef enum {
 	OST_QUOTACHECK = 18, /* not used since 2.4 */
         OST_QUOTACTL   = 19,
 	OST_QUOTA_ADJUST_QUNIT = 20, /* not used since 2.4 */
-        OST_LAST_OPC
+	OST_LADVISE    = 21,
+	OST_LAST_OPC /* must be < 33 to avoid MDS_GETATTR */
 } ost_cmd_t;
 #define OST_FIRST_OPC  OST_REPLY
 

commit 50ce29a3b59a8c2984a928ea062e0b415739724d

LU-13891 utils: fix memory leak in llapi_ladvise()
A buffer allocated in llapi_ladvise() for ioctl() is never
released. This patch ensures the buffer is properly freed.

Fixes: e14246641c04 ("LU-4931 ladvise: Add feature of giving file access advices")
Signed-off-by: Jean-Yves VET <jyvet@ddn.com>
Change-Id: I0761e161074ae3029218473ec951670fdbbd33bd
Reviewed-on: https://review.whamcloud.com/39610
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/liblustreapi_ladvise.c
@@ -78,7 +78,9 @@ int llapi_ladvise(int fd, unsigned long long flags, int num_advise,
 	rc = ioctl(fd, LL_IOC_LADVISE, ladvise_hdr);
 	if (rc < 0) {
 		llapi_error(LLAPI_MSG_ERROR, -errno, "cannot give advice");
-		return -1;
+		goto out;
+	} else {
+		rc = 0;
 	}
 
 	/* Copy results back in to caller provided structs */
@@ -92,6 +94,8 @@ int llapi_ladvise(int fd, unsigned long long flags, int num_advise,
 					ladvise_iter->lla_lockahead_result;
 	}
 
-	return 0;
+out:
+	free(ladvise_hdr);
+	return rc;
 }
 