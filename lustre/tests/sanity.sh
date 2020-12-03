lustre/lustre-release

commit c1d0a355a6a64ec97c9f56c38ba036e5e50cd8c4

LU-12624 lod: alloc dir stripes by QoS
Similar to file OST object allocation, introduce directory stripe
allocation by space usage, but they don't share the same code because
of the many differences between them: file has mirrors, PFL, object
precreation; while for directory, the first stripe is always on the
same MDT where its master object is on. The changes include:
* add lod_mdt_alloc_qos() to allocate stripes by space/inode usage.
* add lod_mdt_alloc_rr() to allocate stripes round-robin.
* add lod_mdt_alloc_specific() to allocate stripes in the old way.
* add sysfs support for lmv_desc field in LOD structure, and move
  those remain in procfs to sysfs.

This patch also changes LMV QoS code:
* mkdir by QoS if user mkdir by command 'lfs mkdir -i -1 ...', or the
  parent directory default LMV starting MDT index is -1.
* with the above change, 'space' hash flag is useless, remove all
  related code.
* previously 'lfs mkdir -i -1' QoS code is in lfs_setdirstripe(),
  but now it's done in LMV, remove the old code.

Update sanity 413a 413b to support QoS mkdir of both plain and
striped directories.

Update lfs-setdirstripe man to reflect the changes.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I8f5f8e46faae68ffd9a49a4ac1d450e951e979c5
Reviewed-on: https://review.whamcloud.com/35825
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_object.c
@@ -1945,74 +1945,73 @@ out:
 	RETURN(rc);
 }
 
-static int lod_prep_md_striped_create(const struct lu_env *env,
-				      struct dt_object *dt,
-				      struct lu_attr *attr,
-				      const struct lmv_user_md_v1 *lum,
-				      struct dt_object_format *dof,
-				      struct thandle *th)
+/**
+ * Allocate a striping on a predefined set of MDTs.
+ *
+ * Allocates new striping using the MDT index range provided by the data from
+ * the lum_obejcts contained in the lmv_user_md passed to this method if
+ * \a is_specific is true; or allocates new layout starting from MDT index in
+ * lo->ldo_dir_stripe_offset. The exact order of MDTs is not important and
+ * varies depending on MDT status. The number of stripes needed and stripe
+ * offset are taken from the object. If that number cannot be met, then the
+ * function returns an error and then it's the caller's responsibility to
+ * release the stripes allocated. All the internal structures are protected,
+ * but no concurrent allocation is allowed on the same objects.
+ *
+ * \param[in] env		execution environment for this thread
+ * \param[in] lo		LOD object
+ * \param[out] stripes		striping created
+ * \param[out] mdt_indices	MDT indices of striping created
+ * \param[in] is_specific	true if the MDTs are provided by lum; false if
+ *				only the starting MDT index is provided
+ *
+ * \retval positive	stripes allocated, including the first stripe allocated
+ *			outside
+ * \retval negative	errno on failure
+ */
+static int lod_mdt_alloc_specific(const struct lu_env *env,
+				  struct lod_object *lo,
+				  struct dt_object **stripes,
+				  __u32 *mdt_indices, bool is_specific)
 {
 	struct lod_thread_info  *info = lod_env_info(env);
-	struct lod_device	*lod = lu2lod_dev(dt->do_lu.lo_dev);
-	struct lod_tgt_descs	*ltd = &lod->lod_mdt_descs;
-	struct lod_object	*lo = lod_dt_obj(dt);
-	struct dt_object	**stripe;
-	__u32			stripe_count;
-	int			*idx_array;
-	__u32			master_index;
-	int			rc = 0;
-	__u32			i;
-	__u32			j;
-	bool			is_specific = false;
-	ENTRY;
+	struct lod_device *lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
+	struct lu_tgt_descs *ltd = &lod->lod_mdt_descs;
+	struct lu_tgt_desc *tgt = NULL;
+	struct lu_object_conf conf = { .loc_flags = LOC_F_NEW };
+	struct dt_device *tgt_dt = NULL;
+	struct lu_fid fid = { 0 };
+	struct dt_object *dto;
+	u32 master_index;
+	u32 stripe_count = lo->ldo_dir_stripe_count;
+	int stripe_idx = 1;
+	int j;
+	int idx;
+	int rc;
 
-	/* The lum has been verifed in lod_verify_md_striping */
-	LASSERT(le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC ||
-		le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC_SPECIFIC);
-
-	stripe_count = lo->ldo_dir_stripe_count;
-
-	OBD_ALLOC(idx_array, sizeof(idx_array[0]) * stripe_count);
-	if (idx_array == NULL)
-		RETURN(-ENOMEM);
-
-	OBD_ALLOC(stripe, sizeof(stripe[0]) * stripe_count);
-	if (stripe == NULL)
-		GOTO(out_free, rc = -ENOMEM);
-
-	/* Start index must be the master MDT */
 	master_index = lu_site2seq(lod2lu_dev(lod)->ld_site)->ss_node_id;
-	idx_array[0] = master_index;
-	if (le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC_SPECIFIC) {
-		is_specific = true;
-		for (i = 1; i < stripe_count; i++)
-			idx_array[i] = le32_to_cpu(lum->lum_objects[i].lum_mds);
-	}
+	if (stripe_count > 1)
+		/* Set the start index for the 2nd stripe allocation */
+		mdt_indices[1] = (mdt_indices[0] + 1) %
+					(lod->lod_remote_mdt_count + 1);
 
-	for (i = 0; i < stripe_count; i++) {
-		struct lod_tgt_desc	*tgt = NULL;
-		struct dt_object	*dto;
-		struct lu_fid		fid = { 0 };
-		int			idx;
-		struct lu_object_conf	conf = { 0 };
-		struct dt_device	*tgt_dt = NULL;
-
+	for (; stripe_idx < stripe_count; stripe_idx++) {
 		/* Try to find next avaible target */
-		idx = idx_array[i];
+		idx = mdt_indices[stripe_idx];
 		for (j = 0; j < lod->lod_remote_mdt_count;
 		     j++, idx = (idx + 1) % (lod->lod_remote_mdt_count + 1)) {
 			bool already_allocated = false;
 			__u32 k;
 
 			CDEBUG(D_INFO, "try idx %d, mdt cnt %u, allocated %u\n",
-			       idx, lod->lod_remote_mdt_count + 1, i);
+			       idx, lod->lod_remote_mdt_count + 1, stripe_idx);
 
 			if (likely(!is_specific &&
 				   !OBD_FAIL_CHECK(OBD_FAIL_LARGE_STRIPE))) {
 				/* check whether the idx already exists
 				 * in current allocated array */
-				for (k = 0; k < i; k++) {
-					if (idx_array[k] == idx) {
+				for (k = 0; k < stripe_idx; k++) {
+					if (mdt_indices[k] == idx) {
 						already_allocated = true;
 						break;
 					}
@@ -2033,29 +2032,25 @@ static int lod_prep_md_striped_create(const struct lu_env *env,
 				rc = obd_fid_alloc(env, lod->lod_child_exp,
 						   &fid, NULL);
 				if (rc < 0)
-					GOTO(out_put, rc);
+					continue;
 				tgt_dt = lod->lod_child;
 				break;
 			}
 
 			/* check the status of the OSP */
 			tgt = LTD_TGT(ltd, idx);
-			if (tgt == NULL)
+			if (!tgt)
 				continue;
 
 			tgt_dt = tgt->ltd_tgt;
 			rc = dt_statfs(env, tgt_dt, &info->lti_osfs);
-			if (rc) {
+			if (rc)
 				/* this OSP doesn't feel well */
-				rc = 0;
 				continue;
-			}
 
 			rc = obd_fid_alloc(env, tgt->ltd_exp, &fid, NULL);
-			if (rc < 0) {
-				rc = 0;
+			if (rc < 0)
 				continue;
-			}
 
 			break;
 		}
@@ -2063,15 +2058,16 @@ static int lod_prep_md_striped_create(const struct lu_env *env,
 		/* Can not allocate more stripes */
 		if (j == lod->lod_remote_mdt_count) {
 			CDEBUG(D_INFO, "%s: require stripes %u only get %d\n",
-			       lod2obd(lod)->obd_name, stripe_count, i);
+			       lod2obd(lod)->obd_name, stripe_count,
+			       stripe_idx);
 			break;
 		}
 
 		CDEBUG(D_INFO, "Get idx %d, for stripe %d "DFID"\n",
-		       idx, i, PFID(&fid));
-		idx_array[i] = idx;
+		       idx, stripe_idx, PFID(&fid));
+		mdt_indices[stripe_idx] = idx;
 		/* Set the start index for next stripe allocation */
-		if (!is_specific && i < stripe_count - 1) {
+		if (!is_specific && stripe_idx < stripe_count - 1) {
 			/*
 			 * for large dir test, put all other slaves on one
 			 * remote MDT, otherwise we may save too many local
@@ -2079,7 +2075,7 @@ static int lod_prep_md_striped_create(const struct lu_env *env,
 			 */
 			if (unlikely(OBD_FAIL_CHECK(OBD_FAIL_LARGE_STRIPE)))
 				idx = master_index;
-			idx_array[i + 1] = (idx + 1) %
+			mdt_indices[stripe_idx + 1] = (idx + 1) %
 					   (lod->lod_remote_mdt_count + 1);
 		}
 		/* tgt_dt and fid must be ready after search avaible OSP
@@ -2088,47 +2084,124 @@ static int lod_prep_md_striped_create(const struct lu_env *env,
 		LASSERT(fid_is_sane(&fid));
 
 		/* fail a remote stripe FID allocation */
-		if (i && OBD_FAIL_CHECK(OBD_FAIL_MDS_STRIPE_FID))
+		if (stripe_idx && OBD_FAIL_CHECK(OBD_FAIL_MDS_STRIPE_FID))
 			continue;
 
-		conf.loc_flags = LOC_F_NEW;
 		dto = dt_locate_at(env, tgt_dt, &fid,
-				   dt->do_lu.lo_dev->ld_site->ls_top_dev,
-				   &conf);
-		if (IS_ERR(dto))
-			GOTO(out_put, rc = PTR_ERR(dto));
-		stripe[i] = dto;
+				  lo->ldo_obj.do_lu.lo_dev->ld_site->ls_top_dev,
+				  &conf);
+		if (IS_ERR(dto)) {
+			rc = PTR_ERR(dto);
+			goto error;
+		}
+
+		stripes[stripe_idx] = dto;
 	}
 
+	return stripe_idx;
+
+error:
+	for (j = 1; j < stripe_idx; j++) {
+		LASSERT(stripes[j] != NULL);
+		dt_object_put(env, stripes[j]);
+		stripes[j] = NULL;
+	}
+	return rc;
+}
+
+static int lod_prep_md_striped_create(const struct lu_env *env,
+				      struct dt_object *dt,
+				      struct lu_attr *attr,
+				      const struct lmv_user_md_v1 *lum,
+				      struct dt_object_format *dof,
+				      struct thandle *th)
+{
+	struct lod_device *lod = lu2lod_dev(dt->do_lu.lo_dev);
+	struct lod_object *lo = lod_dt_obj(dt);
+	struct dt_object **stripes;
+	struct lu_object_conf conf = { .loc_flags = LOC_F_NEW };
+	struct lu_fid fid = { 0 };
+	__u32 stripe_count;
+	int i;
+	int rc = 0;
+
+	ENTRY;
+
+	/* The lum has been verifed in lod_verify_md_striping */
+	LASSERT(le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC ||
+		le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC_SPECIFIC);
+
+	stripe_count = lo->ldo_dir_stripe_count;
+
+	OBD_ALLOC(stripes, sizeof(stripes[0]) * stripe_count);
+	if (!stripes)
+		RETURN(-ENOMEM);
+
+	/* Allocate the first stripe locally */
+	rc = obd_fid_alloc(env, lod->lod_child_exp, &fid, NULL);
+	if (rc < 0)
+		GOTO(out, rc);
+
+	stripes[0] = dt_locate_at(env, lod->lod_child, &fid,
+				  dt->do_lu.lo_dev->ld_site->ls_top_dev, &conf);
+	if (IS_ERR(stripes[0]))
+		GOTO(out, rc = PTR_ERR(stripes[0]));
+
+	if (lo->ldo_dir_stripe_offset == LMV_OFFSET_DEFAULT) {
+		lod_qos_statfs_update(env, lod, &lod->lod_mdt_descs);
+		rc = lod_mdt_alloc_qos(env, lo, stripes);
+		if (rc == -EAGAIN)
+			rc = lod_mdt_alloc_rr(env, lo, stripes);
+	} else {
+		int *idx_array;
+		bool is_specific = false;
+
+		OBD_ALLOC(idx_array, sizeof(idx_array[0]) * stripe_count);
+		if (!idx_array)
+			GOTO(out, rc = -ENOMEM);
+
+		if (le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC_SPECIFIC) {
+			is_specific = true;
+			for (i = 0; i < stripe_count; i++)
+				idx_array[i] =
+				       le32_to_cpu(lum->lum_objects[i].lum_mds);
+		}
+
+		/* stripe 0 is local */
+		idx_array[0] =
+			lu_site2seq(lod2lu_dev(lod)->ld_site)->ss_node_id;
+		rc = lod_mdt_alloc_specific(env, lo, stripes, idx_array,
+					    is_specific);
+		OBD_FREE(idx_array, sizeof(idx_array[0]) * stripe_count);
+	}
+
+	if (rc < 0)
+		GOTO(out, rc);
+
+	LASSERT(rc > 0);
+
 	lo->ldo_dir_striped = 1;
-	lo->ldo_stripe = stripe;
-	lo->ldo_dir_stripe_count = i;
+	lo->ldo_stripe = stripes;
+	lo->ldo_dir_stripe_count = rc;
 	lo->ldo_dir_stripes_allocated = stripe_count;
 	smp_mb();
 	lo->ldo_dir_stripe_loaded = 1;
 
-	if (lo->ldo_dir_stripe_count == 0)
-		GOTO(out_put, rc = -ENOSPC);
-
 	rc = lod_dir_declare_create_stripes(env, dt, attr, dof, th);
-	if (rc != 0)
-		GOTO(out_put, rc);
-
-out_put:
-	if (rc < 0) {
-		for (i = 0; i < stripe_count; i++)
-			if (stripe[i] != NULL)
-				dt_object_put(env, stripe[i]);
-		OBD_FREE(stripe, sizeof(stripe[0]) * stripe_count);
-		lo->ldo_dir_stripe_count = 0;
-		lo->ldo_dir_stripes_allocated = 0;
-		lo->ldo_stripe = NULL;
-	}
-
-out_free:
-	OBD_FREE(idx_array, sizeof(idx_array[0]) * stripe_count);
+	if (rc < 0)
+		lod_striping_free(env, lo);
 
 	RETURN(rc);
+
+out:
+	LASSERT(rc < 0);
+	if (!IS_ERR_OR_NULL(stripes[0]))
+		dt_object_put(env, stripes[0]);
+	for (i = 1; i < stripe_count; i++)
+		LASSERT(!stripes[i]);
+	OBD_FREE(stripes, sizeof(stripes[0]) * stripe_count);
+
+	return rc;
 }
 
 /**
@@ -3770,8 +3843,7 @@ static int lod_xattr_set_default_lmv_on_dir(const struct lu_env *env,
 
 	if (LMVEA_DELETE_VALUES((le32_to_cpu(lum->lum_stripe_count)),
 				 le32_to_cpu(lum->lum_stripe_offset)) &&
-	    le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC &&
-	    !(le32_to_cpu(lum->lum_hash_type) & LMV_HASH_FLAG_SPACE)) {
+	    le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC) {
 		rc = lod_xattr_del_internal(env, dt, name, th);
 		if (rc == -ENODATA)
 			rc = 0;
@@ -5136,8 +5208,7 @@ static void lod_striping_from_default(struct lod_object *lo,
 			lo->ldo_dir_stripe_offset =
 				lds->lds_dir_def_stripe_offset;
 		if (lo->ldo_dir_hash_type == 0)
-			lo->ldo_dir_hash_type = lds->lds_dir_def_hash_type &
-						~LMV_HASH_FLAG_SPACE;
+			lo->ldo_dir_hash_type = lds->lds_dir_def_hash_type;
 
 		CDEBUG(D_LAYOUT, "striping from default dir: count:%hu, "
 		       "offset:%u, hash_type:%u\n",
@@ -5593,7 +5664,7 @@ static inline int dt_object_qos_mkdir(const struct lu_env *env,
 		return -EINVAL;
 
 	lmu = info->lti_ea_store;
-	return !!(le32_to_cpu(lmu->lum_hash_type) & LMV_HASH_FLAG_SPACE);
+	return le32_to_cpu(lmu->lum_stripe_offset) == LMV_OFFSET_DEFAULT;
 }
 
 /**
@@ -5659,19 +5730,22 @@ static int lod_declare_create(const struct lu_env *env, struct dt_object *dt,
 			if (OBD_FAIL_CHECK(OBD_FAIL_MDS_STALE_DIR_LAYOUT))
 				GOTO(out, rc = -EREMOTE);
 
-			if (lo->ldo_dir_stripe_offset == -1) {
+			if (lo->ldo_dir_stripe_offset == LMV_OFFSET_DEFAULT) {
+				struct lod_default_striping *lds;
+
+				lds = lo->ldo_def_striping;
 				/*
-				 * child and parent should be in the same MDT,
-				 * but if parent has plain layout, it's allowed.
+				 * child and parent should be on the same MDT,
+				 * but if parent has default LMV, and the start
+				 * MDT offset is -1, it's allowed. This check
+				 * is not necessary after 2.12.22 because client
+				 * follows this already, but old client may not.
 				 */
 				if (hint->dah_parent &&
-				    dt_object_remote(hint->dah_parent)) {
-					rc = dt_object_qos_mkdir(env,
-						       lo->ldo_obj.do_lu.lo_dev,
-						       hint->dah_parent);
-					if (rc <= 0)
-						GOTO(out, rc ? rc : -EREMOTE);
-				}
+				    dt_object_remote(hint->dah_parent) && lds &&
+				    lds->lds_dir_def_stripe_offset !=
+				    LMV_OFFSET_DEFAULT)
+					GOTO(out, rc = -EREMOTE);
 			} else if (lo->ldo_dir_stripe_offset !=
 				   ss->ss_node_id) {
 				struct lod_device *lod;
@@ -7178,7 +7252,7 @@ static int lod_primary_pick(const struct lu_env *env, struct lod_object *lo,
 	 * This algo can be revised later after knowing the topology of
 	 * cluster.
 	 */
-	lod_qos_statfs_update(env, lod);
+	lod_qos_statfs_update(env, lod, &lod->lod_ost_descs);
 	for (i = 0; i < lo->ldo_mirror_count; i++) {
 		bool ost_avail = true;
 		int index = (i + seq) % lo->ldo_mirror_count;

lustre/tests/sanity.sh
@@ -20750,87 +20750,86 @@ test_412() {
 }
 run_test 412 "mkdir on specific MDTs"
 
-test_413a() {
-	[ $MDSCOUNT -lt 2 ] &&
-		skip "We need at least 2 MDTs for this test"
+test_qos_mkdir() {
+	local mkdir_cmd=$1
+	local stripe_count=$2
+	local mdts=$(comma_list $(mdts_nodes))
 
-	if [ $(lustre_version_code mds1) -lt $(version_code 2.10.55) ]; then
-		skip "Need server version at least 2.10.55"
-	fi
-
-	mkdir $DIR/$tdir || error "mkdir failed"
-
-	# find MDT that is the most full
-	local max=$($LFS df | grep MDT |
-		awk 'BEGIN { a=0 }
-			{ sub("%", "", $5)
-			  if (0+$5 >= a)
-			  {
-				a = $5
-				b = $6
-			  }
-			}
-		     END { split(b, c, ":")
-			   sub("]", "", c[2])
-			   print c[2]
-			 }')
-
-	for i in $(seq $((MDSCOUNT - 1))); do
-		$LFS mkdir -c $i $DIR/$tdir/d$i ||
-			error "mkdir d$i failed"
-		$LFS getdirstripe $DIR/$tdir/d$i
-		local stripe_index=$($LFS getdirstripe -i $DIR/$tdir/d$i)
-		[ $stripe_index -ne $max ] ||
-			error "don't expect $max"
-	done
-}
-run_test 413a "mkdir on less full MDTs"
-
-test_413b() {
-	[ $MDSCOUNT -lt 2 ] &&
-		skip "We need at least 2 MDTs for this test"
-
-	[ $MDS1_VERSION -lt $(version_code 2.12.52) ] &&
-		skip "Need server version at least 2.12.52"
-
-	mkdir $DIR/$tdir || error "mkdir failed"
-	$LFS setdirstripe -D -i -1 -H space $DIR/$tdir ||
-		error "setdirstripe failed"
-
-	local qos_prio_free
-	local qos_threshold_rr
+	local testdir
+	local lmv_qos_prio_free
+	local lmv_qos_threshold_rr
+	local lmv_qos_maxage
+	local lod_qos_prio_free
+	local lod_qos_threshold_rr
+	local lod_qos_maxage
 	local count
+	local i
 
-	qos_prio_free=$($LCTL get_param -n lmv.*.qos_prio_free | head -n1)
-	qos_prio_free=${qos_prio_free%%%}
-	qos_threshold_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr | head -n1)
-	qos_threshold_rr=${qos_threshold_rr%%%}
-	qos_maxage=$($LCTL get_param -n lmv.*.qos_maxage)
+	lmv_qos_prio_free=$($LCTL get_param -n lmv.*.qos_prio_free | head -n1)
+	lmv_qos_prio_free=${lmv_qos_prio_free%%%}
+	lmv_qos_threshold_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr |
+		head -n1)
+	lmv_qos_threshold_rr=${lmv_qos_threshold_rr%%%}
+	lmv_qos_maxage=$($LCTL get_param -n lmv.*.qos_maxage)
+	stack_trap "$LCTL set_param \
+		lmv.*.qos_prio_free=$lmv_qos_prio_free > /dev/null" EXIT
+	stack_trap "$LCTL set_param \
+		lmv.*.qos_threshold_rr=$lmv_qos_threshold_rr > /dev/null" EXIT
+	stack_trap "$LCTL set_param \
+		lmv.*.qos_maxage=$lmv_qos_maxage > /dev/null" EXIT
 
-	stack_trap "$LCTL set_param lmv.*.qos_prio_free=$qos_prio_free" EXIT
-	stack_trap "$LCTL set_param lmv.*.qos_threshold_rr=$qos_threshold_rr" \
+	lod_qos_prio_free=$(do_facet mds1 $LCTL get_param -n \
+		lod.lustre-MDT0000-mdtlov.mdt_qos_prio_free | head -n1)
+	lod_qos_prio_free=${lod_qos_prio_free%%%}
+	lod_qos_threshold_rr=$(do_facet mds1 $LCTL get_param -n \
+		lod.lustre-MDT0000-mdtlov.mdt_qos_thresholdrr | head -n1)
+	lod_qos_threshold_rr=${lod_qos_threshold_rr%%%}
+	lod_qos_maxage=$(do_facet mds1 $LCTL get_param -n \
+		lod.lustre-MDT0000-mdtlov.qos_maxage | awk '{ print $1 }')
+	stack_trap "do_nodes $mdts $LCTL set_param \
+		lod.*.mdt_qos_prio_free=$lod_qos_prio_free > /dev/null" EXIT
+	stack_trap "do_nodes $mdts $LCTL set_param \
+		lod.*.mdt_qos_thresholdrr=$lod_qos_threshold_rr > /dev/null" \
 		EXIT
-	stack_trap "$LCTL set_param lmv.*.qos_maxage=$qos_maxage" EXIT
+	stack_trap "do_nodes $mdts $LCTL set_param \
+		lod.*.mdt_qos_maxage=$lod_qos_maxage > /dev/null" EXIT
 
-	echo "mkdir with roundrobin"
+	echo
+	echo "Mkdir (stripe_count $stripe_count) roundrobin:"
 
-	$LCTL set_param lmv.*.qos_threshold_rr=100
+	$LCTL set_param lmv.*.qos_threshold_rr=100 > /dev/null
+	do_nodes $mdts $LCTL set_param lod.*.mdt_qos_thresholdrr=100 > /dev/null
+
+	testdir=$DIR/$tdir-s$stripe_count/rr
+
 	for i in $(seq $((100 * MDSCOUNT))); do
-		mkdir $DIR/$tdir/subdir$i || error "mkdir subdir$i failed"
+		eval $mkdir_cmd $testdir/subdir$i ||
+			error "$mkdir_cmd subdir$i failed"
 	done
+
 	for i in $(seq $MDSCOUNT); do
-		count=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$((i - 1))$ |
-			wc -w)
+		count=$($LFS getdirstripe -i $testdir/* |
+				grep ^$((i - 1))$ | wc -l)
 		echo "$count directories created on MDT$((i - 1))"
 		[ $count -eq 100 ] || error "subdirs are not evenly distributed"
+
+		if [ $stripe_count -gt 1 ]; then
+			count=$($LFS getdirstripe $testdir/* |
+				grep -P "^\s+$((i - 1))\t" | wc -l)
+			echo "$count stripes created on MDT$((i - 1))"
+			# deviation should < 5% of average
+			[ $count -lt $((95 * stripe_count)) ] ||
+			[ $count -gt $((105 * stripe_count)) ] &&
+				error "stripes are not evenly distributed"
+		fi
 	done
 
-	rm -rf $DIR/$tdir/*
+	$LCTL set_param lmv.*.qos_threshold_rr=$lmv_qos_threshold_rr > /dev/null
+	do_nodes $mdts $LCTL set_param \
+		lod.*.mdt_qos_thresholdrr=$lod_qos_threshold_rr > /dev/null
 
-	$LCTL set_param lmv.*.qos_threshold_rr=$qos_threshold_rr
-	# Shorten statfs result age, so that it can be updated in time
-	$LCTL set_param lmv.*.qos_maxage=1
-	sleep_maxage
+	echo
+	echo "Check for uneven MDTs: "
 
 	local ffree
 	local bavail
@@ -20867,9 +20866,8 @@ test_413b() {
 
 	# Check if we need to generate uneven MDTs
 	local threshold=50
-	local diff=$(((max - min ) * 100 / min))
+	local diff=$(((max - min) * 100 / min))
 	local value="$(generate_string 1024)"
-	local i
 
 	while [ $diff -lt $threshold ]; do
 		# generate uneven MDTs, create till $threshold% diff
@@ -20884,11 +20882,11 @@ test_413b() {
 			error "mkdir $tdir-MDT$min_index failed"
 		for i in $(seq $count); do
 			$OPENFILE -f O_CREAT:O_LOV_DELAY_CREATE \
-				$DIR/$tdir-MDT$min_index/f$i > /dev/null ||
-				error "create f$i failed"
+				$DIR/$tdir-MDT$min_index/f$j_$i > /dev/null ||
+				error "create f$j_$i failed"
 			setfattr -n user.413b -v $value \
-				$DIR/$tdir-MDT$min_index/f$i ||
-				error "setfattr f$i failed"
+				$DIR/$tdir-MDT$min_index/f$j_$i ||
+				error "setfattr f$j_$i failed"
 		done
 
 		ffree=($(lctl get_param -n mdc.*[mM][dD][cC]-*.filesfree))
@@ -20904,31 +20902,95 @@ test_413b() {
 	echo "MDT blocks available: ${bavail[@]}"
 	echo "weight diff=$diff%"
 
-	echo "mkdir with balanced space usage"
-	$LCTL set_param lmv.*.qos_prio_free=100
+	echo
+	echo "Mkdir (stripe_count $stripe_count) with balanced space usage:"
+
+	$LCTL set_param lmv.*.qos_prio_free=100 > /dev/null
+	do_nodes $mdts $LCTL set_param lod.*.mdt_qos_prio_free=100 > /dev/null
+	# decrease statfs age, so that it can be updated in time
+	$LCTL set_param lmv.*.qos_maxage=1 > /dev/null
+	do_nodes $mdts $LCTL set_param lod.*.mdt_qos_maxage=1 > /dev/null
+
+	sleep 1
+
+	testdir=$DIR/$tdir-s$stripe_count/qos
+
 	for i in $(seq $((100 * MDSCOUNT))); do
-		mkdir $DIR/$tdir/subdir$i || error "mkdir subdir$i failed"
+		eval $mkdir_cmd $testdir/subdir$i ||
+			error "$mkdir_cmd subdir$i failed"
 	done
 
 	for i in $(seq $MDSCOUNT); do
-		count=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$((i - 1))$ |
-			wc -w)
+		count=$($LFS getdirstripe -i $testdir/* | grep ^$((i - 1))$ |
+			wc -l)
 		echo "$count directories created on MDT$((i - 1))"
+
+		if [ $stripe_count -gt 1 ]; then
+			count=$($LFS getdirstripe $testdir/* |
+				grep -P "^\s+$((i - 1))\t" | wc -l)
+			echo "$count stripes created on MDT$((i - 1))"
+		fi
 	done
 
-	max=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$max_index$ | wc -l)
-	min=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$min_index$ | wc -l)
+	max=$($LFS getdirstripe -i $testdir/* | grep ^$max_index$ | wc -l)
+	min=$($LFS getdirstripe -i $testdir/* | grep ^$min_index$ | wc -l)
 
+	# D-value should > 10% of averge
 	[ $((max - min)) -lt 10 ] &&
 		error "subdirs shouldn't be evenly distributed"
 
-	which getfattr > /dev/null 2>&1 || skip_env "no getfattr command"
-
-	$LFS setdirstripe -D -d $DIR/$tdir || error "setdirstripe -d failed"
-	getfattr -n trusted.dmv $DIR/$tdir &&
-		error "default dir layout exists" || true
+	# ditto
+	if [ $stripe_count -gt 1 ]; then
+		max=$($LFS getdirstripe $testdir/* |
+			grep -P "^\s+$max_index\t" | wc -l)
+		min=$($LFS getdirstripe $testdir/* |
+			grep -P "^\s+$min_index\t" | wc -l)
+		[ $((max - min)) -le $((10 * stripe_count)) ] &&
+			error "stripes shouldn't be evenly distributed"|| true
+	fi
 }
-run_test 413b "mkdir with balanced space usage"
+
+test_413a() {
+	[ $MDSCOUNT -lt 2 ] &&
+		skip "We need at least 2 MDTs for this test"
+
+	[ $MDS1_VERSION -lt $(version_code 2.12.52) ] &&
+		skip "Need server version at least 2.12.52"
+
+	local stripe_count
+
+	for stripe_count in $(seq 1 $((MDSCOUNT - 1))); do
+		mkdir $DIR/$tdir-s$stripe_count || error "mkdir failed"
+		mkdir $DIR/$tdir-s$stripe_count/rr || error "mkdir failed"
+		mkdir $DIR/$tdir-s$stripe_count/qos || error "mkdir failed"
+		test_qos_mkdir "$LFS mkdir -c $stripe_count" $stripe_count
+	done
+}
+run_test 413a "QoS mkdir with 'lfs mkdir -i -1'"
+
+test_413b() {
+	[ $MDSCOUNT -lt 2 ] &&
+		skip "We need at least 2 MDTs for this test"
+
+	[ $MDS1_VERSION -lt $(version_code 2.12.52) ] &&
+		skip "Need server version at least 2.12.52"
+
+	local stripe_count
+
+	for stripe_count in $(seq 1 $((MDSCOUNT - 1))); do
+		mkdir $DIR/$tdir-s$stripe_count || error "mkdir failed"
+		mkdir $DIR/$tdir-s$stripe_count/rr || error "mkdir failed"
+		mkdir $DIR/$tdir-s$stripe_count/qos || error "mkdir failed"
+		$LFS setdirstripe -D -c $stripe_count \
+			$DIR/$tdir-s$stripe_count/rr ||
+			error "setdirstripe failed"
+		$LFS setdirstripe -D -c $stripe_count \
+			$DIR/$tdir-s$stripe_count/qos ||
+			error "setdirstripe failed"
+		test_qos_mkdir "mkdir" $stripe_count
+	done
+}
+run_test 413b "QoS mkdir under dir whose default LMV starting MDT offset is -1"
 
 test_414() {
 #define OBD_FAIL_PTLRPC_BULK_ATTACH      0x521

lustre/doc/lfs-setdirstripe.1
@@ -44,13 +44,6 @@ Fowler-Noll-Vo (FNV-1a) hash algorithm.  This provides
 reasonably uniform, but not cryptographically strong,
 hashing of the filename. (default)
 .TP
-.B space
-This can only be set on plain directory default striping.
-Newly created sub-directories will be distributed on all
-MDTs by MDT space usage. Note that this is suggested to
-be set on lustre ROOT, so that all sub-directories under
-lustre ROOT are distributed among all MDTs.
-.TP
 .B all_char
 Sum of ASCII characters modulo number of MDTs. This
 provides weak hashing of the filename, and is suitable

lustre/include/lprocfs_status.h
@@ -614,6 +614,7 @@ extern ssize_t
 lprocfs_pinger_recov_seq_write(struct file *file, const char __user *buffer,
 			       size_t count, loff_t *off);
 
+int lu_str_to_s64(char *buffer, unsigned long count, __s64 *val, char defunit);
 extern int lprocfs_str_with_units_to_s64(const char __user *buffer,
 					 unsigned long count, __s64 *val,
 					 char defunit);

lustre/include/lustre_lmv.h
@@ -54,12 +54,6 @@ struct lmv_stripe_md {
 	struct lmv_oinfo lsm_md_oinfo[0];
 };
 
-static inline bool lmv_is_known_hash_type(__u32 type)
-{
-	return (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_FNV_1A_64 ||
-	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_ALL_CHARS;
-}
-
 static inline bool lmv_dir_striped(const struct lmv_stripe_md *lsm)
 {
 	return lsm && lsm->lsm_md_magic == LMV_MAGIC;
@@ -86,12 +80,6 @@ static inline bool lmv_dir_bad_hash(const struct lmv_stripe_md *lsm)
 		return false;
 
 	return !lmv_is_known_hash_type(lsm->lsm_md_hash_type);
-}
-
-/* NB, this is checking directory default LMV */
-static inline bool lmv_dir_qos_mkdir(const struct lmv_stripe_md *lsm)
-{
-	return lsm && (lsm->lsm_md_hash_type & LMV_HASH_FLAG_SPACE);
 }
 
 static inline bool

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -712,6 +712,7 @@ struct fsxattr {
 #define LOV_PATTERN_DEFAULT	0xffffffff
 
 #define LOV_OFFSET_DEFAULT      ((__u16)-1)
+#define LMV_OFFSET_DEFAULT      ((__u32)-1)
 
 static inline bool lov_pattern_supported(__u32 pattern)
 {
@@ -1001,10 +1002,11 @@ enum lmv_hash_type {
  * might be interpreted differently with different flags. */
 #define LMV_HASH_TYPE_MASK 0x0000ffff
 
-/* once this is set on a plain directory default layout, newly created
- * subdirectories will be distributed on all MDTs by space usage.
- */
-#define LMV_HASH_FLAG_SPACE	0x08000000
+static inline bool lmv_is_known_hash_type(__u32 type)
+{
+	return (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_FNV_1A_64 ||
+	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_ALL_CHARS;
+}
 
 /* The striped directory has ever lost its master LMV EA, then LFSCK
  * re-generated it. This flag is used to indicate such case. It is an

lustre/lmv/lmv_intent.c
@@ -305,22 +305,10 @@ static int lmv_intent_open(struct obd_export *exp, struct md_op_data *op_data,
 				/*
 				 * open(O_CREAT | O_EXCL) needs to check
 				 * existing name, which should be done on both
-				 * old and new layout, to avoid creating new
-				 * file under old layout, check old layout on
+				 * old and new layout, check old layout on
 				 * client side.
 				 */
-				tgt = lmv_locate_tgt(lmv, op_data);
-				if (IS_ERR(tgt))
-					RETURN(PTR_ERR(tgt));
-
-				rc = md_getattr_name(tgt->ltd_exp, op_data,
-						     reqp);
-				if (!rc) {
-					ptlrpc_req_finished(*reqp);
-					*reqp = NULL;
-					RETURN(-EEXIST);
-				}
-
+				rc = lmv_migrate_existence_check(lmv, op_data);
 				if (rc != -ENOENT)
 					RETURN(rc);
 

commit e7d91d9b8cf291d638857b6bd904469b46d1273a

LU-12960 lod: don't set index for 2nd stripe if specific
When MDTs are specific, don't set index for the second stripe
allocation, otherwise it's not created on specific MDT.

Add sanity 31q.

Fixes: c1d0a355a6 ("LU-12624 lod: alloc dir stripes by QoS")
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I415be0fa7c6e702ec72dcaac88bba55290463d44
Reviewed-on: https://review.whamcloud.com/36735
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_object.c
@@ -2075,7 +2075,7 @@ static int lod_mdt_alloc_specific(const struct lu_env *env,
 	int rc;
 
 	master_index = lu_site2seq(lod2lu_dev(lod)->ld_site)->ss_node_id;
-	if (stripe_count > 1)
+	if (!is_specific && stripe_count > 1)
 		/* Set the start index for the 2nd stripe allocation */
 		mdt_indices[1] = (mdt_indices[0] + 1) %
 					(lod->lod_remote_mdt_count + 1);

lustre/tests/sanity.sh
@@ -3347,6 +3347,21 @@ test_31p() {
 }
 run_test 31p "remove of open striped directory"
 
+test_31q() {
+	[ $MDSCOUNT -lt 3 ] && skip_env "needs >= 3 MDTs"
+
+	$LFS mkdir -i 3,1 $DIR/$tdir || error "mkdir failed"
+	index=$($LFS getdirstripe -i $DIR/$tdir)
+	[ $index -eq 3 ] || error "first stripe index $index != 3"
+	index=$($LFS getdirstripe $DIR/$tdir | tail -1 | awk '{print $1}')
+	[ $index -eq 1 ] || error "second stripe index $index != 1"
+
+	# when "-c <stripe_count>" is set, the number of MDTs specified after
+	# "-i" should equal to the stripe count
+	$LFS mkdir -i 3,1 -c 3 $DIR/$tdir.2 && error "mkdir should fail" || true
+}
+run_test 31q "create striped directory on specific MDTs"
+
 cleanup_test32_mount() {
 	local rc=0
 	trap 0