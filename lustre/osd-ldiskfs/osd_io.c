lustre/lustre-release

commit cf48a2d23bca685e1076e45370c5d3763c9a9952

LU-13839 kernel: RHEL 8.3 server support
This patch makes changes to support RHEL 8.3 release with
kernel 4.18.0-240.1.1.el8_3 for Lustre server.

Test-Parameters: trivial fstype=ldiskfs \
clientdistro=el8.3 serverdistro=el8.3 testlist=sanity

Test-Parameters: trivial fstype=zfs \
clientdistro=el8.3 serverdistro=el8.3 testlist=sanity

Change-Id: I2aa2efaf86b5f244fcb1dd95be1d0bfe04e8d245
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/40729
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -931,7 +931,23 @@ static int osd_chunk_trans_blocks(struct inode *inode, int nrblocks)
 	return ret;
 }
 
-static int osd_extend_trans(handle_t *handle, int needed)
+#ifdef HAVE_LDISKFS_JOURNAL_ENSURE_CREDITS
+static int osd_extend_trans(handle_t *handle, int needed,
+			    struct inode *inode)
+{
+	return  __ldiskfs_journal_ensure_credits(handle, needed, needed,
+		ldiskfs_trans_default_revoke_credits(inode->i_sb));
+}
+
+static int osd_extend_restart_trans(handle_t *handle, int needed,
+				    struct inode *inode)
+{
+	return ldiskfs_journal_ensure_credits(handle, needed,
+		ldiskfs_trans_default_revoke_credits(inode->i_sb));
+}
+#else
+static int osd_extend_trans(handle_t *handle, int needed,
+			    struct inode *inode)
 {
 	if (ldiskfs_handle_has_enough_credits(handle, needed))
 		return 0;
@@ -940,16 +956,18 @@ static int osd_extend_trans(handle_t *handle, int needed)
 				      needed - handle->h_buffer_credits);
 }
 
-static int osd_extend_restart_trans(handle_t *handle, int needed)
+static int osd_extend_restart_trans(handle_t *handle, int needed,
+				    struct inode *inode)
 {
 
-	int rc = osd_extend_trans(handle, needed);
+	int rc = osd_extend_trans(handle, needed, inode);
 
 	if (rc <= 0)
 		return rc;
 
 	return ldiskfs_journal_restart(handle, needed);
 }
+#endif /* HAVE_LDISKFS_JOURNAL_ENSURE_CREDITS */
 
 static int osd_ldiskfs_map_write(struct inode *inode, struct osd_iobuf *iobuf,
 				 struct osd_device *osd, sector_t start_blocks,
@@ -1054,7 +1072,7 @@ cont_map:
 			 * credits to insert 1 extent into extent tree.
 			 */
 			credits = osd_chunk_trans_blocks(inode, blen);
-			rc = osd_extend_trans(handle, credits);
+			rc = osd_extend_trans(handle, credits, inode);
 			if (rc < 0)
 				GOTO(cleanup, rc);
 			/*
@@ -1069,7 +1087,12 @@ cont_map:
 					count, &disk_size, user_size);
 				if (rc)
 					GOTO(cleanup, rc);
+#ifdef HAVE_LDISKFS_JOURNAL_ENSURE_CREDITS
+				rc = ldiskfs_journal_restart(handle, credits,
+					ldiskfs_trans_default_revoke_credits(inode->i_sb));
+#else
 				rc = ldiskfs_journal_restart(handle, credits);
+#endif
 				if (rc)
 					GOTO(cleanup, rc);
 				start_blocks += count;
@@ -1473,7 +1496,8 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 			handle_t *handle = ldiskfs_journal_current_handle();
 
 			LASSERT(handle != NULL);
-			rc = osd_extend_restart_trans(handle, save_credits);
+			rc = osd_extend_restart_trans(handle, save_credits,
+						      inode);
 		}
 	} else {
 		/* no pages to write, no transno is needed */
@@ -2178,7 +2202,7 @@ static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
 		}
 
 		/* TODO: quota check */
-		rc = osd_extend_restart_trans(handle, credits);
+		rc = osd_extend_restart_trans(handle, credits, inode);
 		if (rc)
 			break;
 
@@ -2210,11 +2234,11 @@ static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
 	}
 
 out:
-	inode_unlock(inode);
-
 	/* extand credits if needed for operations such as attribute set */
 	if (rc >= 0)
-		rc = osd_extend_restart_trans(handle, save_credits);
+		rc = osd_extend_restart_trans(handle, save_credits, inode);
+
+	inode_unlock(inode);
 
 	RETURN(rc);
 }

config/lustre-build-ldiskfs.m4
@@ -13,6 +13,7 @@ esac
 AS_IF([test -z "$LDISKFS_SERIES"], [
 AS_IF([test x$RHEL_KERNEL = xyes], [
 	case $RHEL_RELEASE_NO in
+	83)     LDISKFS_SERIES="4.18-rhel8.3.series"    ;;
 	82)     LDISKFS_SERIES="4.18-rhel8.2.series"    ;;
 	81)     LDISKFS_SERIES="4.18-rhel8.1.series"    ;;
 	80)     LDISKFS_SERIES="4.18-rhel8.series"      ;;
@@ -226,6 +227,31 @@ ext4_i_crypt_info, [
 ]) # LB_EXT4_HAVE_I_CRYPT_INFO
 
 #
+# LB_LDISKFS_JOURNAL_ENSURE_CREDITS
+#
+# kernel 4.18.0-240.1.1.el8 and
+# kernel 5.4 commit a413036791d040e33badcc634453a4d0c0705499
+#
+# ext4_journal_ensure_credits was introduced to ensure given handle
+# has at least requested amount of credits available, and possibly
+# restarting transaction if needed.
+#
+AC_DEFUN([LB_LDISKFS_JOURNAL_ENSURE_CREDITS], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if 'ext4_journal_ensure_credits' exists],
+ext4_journal_ensure_credits, [
+	#include "$EXT4_SRC_DIR/ext4_jbd2.h"
+],[
+	ext4_journal_ensure_credits(NULL, 0, 0);
+],[
+	AC_DEFINE(HAVE_LDISKFS_JOURNAL_ENSURE_CREDITS, 1,
+		['ext4_journal_ensure_credits' exists])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LB_LDISKFS_JOURNAL_ENSURE_CREDITS
+
+#
 # LB_LDISKFS_IGET_HAS_FLAGS_ARG
 #
 # kernel 4.19 commit 8a363970d1dc38c4ec4ad575c862f776f468d057
@@ -417,6 +443,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 	LB_EXT4_BREAD_4ARGS
 	LB_EXT4_HAVE_INFO_DQUOT
 	LB_EXT4_HAVE_I_CRYPT_INFO
+	LB_LDISKFS_JOURNAL_ENSURE_CREDITS
 	LB_LDISKFS_IGET_HAS_FLAGS_ARG
 	LB_LDISKFS_FIND_ENTRY_LOCKED_EXISTS
 	LB_LDISKFSFS_DIRHASH_WANTS_DIR

contrib/lbuild/lbuild-rhel8
@@ -111,6 +111,7 @@ kernel_srpm_location() {
 	local kdir
 
 	case $lnxrel in
+		*8_3) kdir="8.3.2011" ;;
 		*8_2) kdir="8.2.2004" ;;
 		*8_1) kdir="8.1.1911" ;;
 		*8_0) kdir="8.0.1905" ;;

ldiskfs/kernel_patches/patches/rhel8.3/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
@@ -1,0 +1,262 @@
+Since we could skip corrupt block groups, this patch
+use ext4_warning() intead of ext4_error() to make FS not
+emount RO in default
+
+Index: linux-4.18.0-80.1.2.el8_0/fs/ext4/balloc.c
+===================================================================
+--- linux-4.18.0-80.1.2.el8_0.orig/fs/ext4/balloc.c
++++ linux-4.18.0-80.1.2.el8_0/fs/ext4/balloc.c
+@@ -373,7 +373,7 @@ static int ext4_validate_block_bitmap(st
+ 	if (unlikely(!ext4_block_bitmap_csum_verify(sb, block_group,
+ 			desc, bh))) {
+ 		ext4_unlock_group(sb, block_group);
+-		ext4_error(sb, "bg %u: bad block bitmap checksum", block_group);
++		ext4_warning(sb, "bg %u: bad block bitmap checksum", block_group);
+ 		ext4_mark_group_bitmap_corrupted(sb, block_group,
+ 					EXT4_GROUP_INFO_BBITMAP_CORRUPT);
+ 		return -EFSBADCRC;
+@@ -381,8 +381,8 @@ static int ext4_validate_block_bitmap(st
+ 	blk = ext4_valid_block_bitmap(sb, desc, block_group, bh);
+ 	if (unlikely(blk != 0)) {
+ 		ext4_unlock_group(sb, block_group);
+-		ext4_error(sb, "bg %u: block %llu: invalid block bitmap",
+-			   block_group, blk);
++		ext4_warning(sb, "bg %u: block %llu: invalid block bitmap",
++			     block_group, blk);
+ 		ext4_mark_group_bitmap_corrupted(sb, block_group,
+ 					EXT4_GROUP_INFO_BBITMAP_CORRUPT);
+ 		return -EFSCORRUPTED;
+@@ -458,8 +458,8 @@ ext4_read_block_bitmap_nowait(struct sup
+ 		ext4_unlock_group(sb, block_group);
+ 		unlock_buffer(bh);
+ 		if (err) {
+-			ext4_error(sb, "Failed to init block bitmap for group "
+-				   "%u: %d", block_group, err);
++			ext4_warning(sb, "Failed to init block bitmap for group "
++				     "%u: %d", block_group, err);
+ 			goto out;
+ 		}
+ 		goto verify;
+Index: linux-4.18.0-80.1.2.el8_0/fs/ext4/ialloc.c
+===================================================================
+--- linux-4.18.0-80.1.2.el8_0.orig/fs/ext4/ialloc.c
++++ linux-4.18.0-80.1.2.el8_0/fs/ext4/ialloc.c
+@@ -96,8 +96,8 @@ static int ext4_validate_inode_bitmap(st
+ 	if (!ext4_inode_bitmap_csum_verify(sb, block_group, desc, bh,
+ 					   EXT4_INODES_PER_GROUP(sb) / 8)) {
+ 		ext4_unlock_group(sb, block_group);
+-		ext4_error(sb, "Corrupt inode bitmap - block_group = %u, "
+-			   "inode_bitmap = %llu", block_group, blk);
++		ext4_warning(sb, "Corrupt inode bitmap - block_group = %u, "
++			     "inode_bitmap = %llu", block_group, blk);
+ 		ext4_mark_group_bitmap_corrupted(sb, block_group,
+ 					EXT4_GROUP_INFO_IBITMAP_CORRUPT);
+ 		return -EFSBADCRC;
+@@ -346,7 +346,7 @@ out:
+ 		if (!fatal)
+ 			fatal = err;
+ 	} else {
+-		ext4_error(sb, "bit already cleared for inode %lu", ino);
++		ext4_warning(sb, "bit already cleared for inode %lu", ino);
+ 		ext4_mark_group_bitmap_corrupted(sb, block_group,
+ 					EXT4_GROUP_INFO_IBITMAP_CORRUPT);
+ 	}
+Index: linux-4.18.0-240.1.1.el8/fs/ext4/mballoc.c
+===================================================================
+--- linux-4.18.0-240.1.1.el8.orig/fs/ext4/mballoc.c
++++ linux-4.18.0-240.1.1.el8/fs/ext4/mballoc.c
+@@ -741,10 +741,14 @@ int ext4_mb_generate_buddy(struct super_
+ 	grp->bb_fragments = fragments;
+ 
+ 	if (free != grp->bb_free) {
+-		ext4_grp_locked_error(sb, group, 0, 0,
+-				      "block bitmap and bg descriptor "
+-				      "inconsistent: %u vs %u free clusters",
+-				      free, grp->bb_free);
++		struct ext4_group_desc *gdp;
++		gdp = ext4_get_group_desc(sb, group, NULL);
++		ext4_warning(sb, "group %lu: block bitmap and bg descriptor "
++			     "inconsistent: %u vs %u free clusters "
++			     "%u in gd, %lu pa's",
++			     (long unsigned int)group, free, grp->bb_free,
++			     ext4_free_group_clusters(sb, gdp),
++			     grp->bb_prealloc_nr);
+ 		/*
+ 		 * If we intend to continue, we consider group descriptor
+ 		 * corrupt and update bb_free using bitmap value
+@@ -1107,7 +1111,7 @@ ext4_mb_load_buddy_gfp(struct super_bloc
+ 	int block;
+ 	int pnum;
+ 	int poff;
+-	struct page *page;
++	struct page *page = NULL;
+ 	int ret;
+ 	struct ext4_group_info *grp;
+ 	struct ext4_sb_info *sbi = EXT4_SB(sb);
+@@ -1133,7 +1137,7 @@ ext4_mb_load_buddy_gfp(struct super_bloc
+ 		 */
+ 		ret = ext4_mb_init_group(sb, group, gfp);
+ 		if (ret)
+-			return ret;
++			goto err;
+ 	}
+ 
+ 	/*
+@@ -1236,6 +1240,7 @@ err:
+ 		put_page(e4b->bd_buddy_page);
+ 	e4b->bd_buddy = NULL;
+ 	e4b->bd_bitmap = NULL;
++	ext4_warning(sb, "Error loading buddy information for %u", group);
+ 	return ret;
+ }
+ 
+@@ -3675,9 +3680,11 @@ int ext4_mb_check_ondisk_bitmap(struct s
+ 	}
+ 
+ 	if (free != free_in_gdp) {
+-		ext4_error(sb, "on-disk bitmap for group %d"
++		ext4_warning(sb, "on-disk bitmap for group %d"
+ 			"corrupted: %u blocks free in bitmap, %u - in gd\n",
+ 			group, free, free_in_gdp);
++		ext4_mark_group_bitmap_corrupted(sb, group,
++					EXT4_GROUP_INFO_BBITMAP_CORRUPT);
+ 		return -EIO;
+ 	}
+ 	return 0;
+@@ -4037,16 +4044,8 @@ ext4_mb_release_inode_pa(struct ext4_bud
+ 	/* "free < pa->pa_free" means we maybe double alloc the same blocks,
+ 	 * otherwise maybe leave some free blocks unavailable, no need to BUG.*/
+ 	if ((free > pa->pa_free && !pa->pa_error) || (free < pa->pa_free)) {
+-		ext4_error(sb, "pa free mismatch: [pa %p] "
+-				"[phy %lu] [logic %lu] [len %u] [free %u] "
+-				"[error %u] [inode %lu] [freed %u]", pa,
+-				(unsigned long)pa->pa_pstart,
+-				(unsigned long)pa->pa_lstart,
+-				(unsigned)pa->pa_len, (unsigned)pa->pa_free,
+-				(unsigned)pa->pa_error, pa->pa_inode->i_ino,
+-				free);
+ 		ext4_grp_locked_error(sb, group, 0, 0, "free %u, pa_free %u",
+-					free, pa->pa_free);
++				      free, pa->pa_free);
+ 		/*
+ 		 * pa is already deleted so we use the value obtained
+ 		 * from the bitmap and continue.
+@@ -4108,16 +4107,11 @@ ext4_mb_discard_group_preallocations(str
+ 	bitmap_bh = ext4_read_block_bitmap(sb, group);
+ 	if (IS_ERR(bitmap_bh)) {
+ 		err = PTR_ERR(bitmap_bh);
+-		ext4_error_err(sb, -err,
+-			       "Error %d reading block bitmap for %u",
+-			       err, group);
+ 		return 0;
+ 	}
+ 
+ 	err = ext4_mb_load_buddy(sb, group, &e4b);
+ 	if (err) {
+-		ext4_warning(sb, "Error %d loading buddy information for %u",
+-			     err, group);
+ 		put_bh(bitmap_bh);
+ 		return 0;
+ 	}
+@@ -4278,17 +4272,12 @@ repeat:
+ 
+ 		err = ext4_mb_load_buddy_gfp(sb, group, &e4b,
+ 					     GFP_NOFS|__GFP_NOFAIL);
+-		if (err) {
+-			ext4_error_err(sb, -err, "Error %d loading buddy information for %u",
+-				       err, group);
++		if (err)
+ 			return;
+-		}
+ 
+ 		bitmap_bh = ext4_read_block_bitmap(sb, group);
+ 		if (IS_ERR(bitmap_bh)) {
+ 			err = PTR_ERR(bitmap_bh);
+-			ext4_error_err(sb, -err, "Error %d reading block bitmap for %u",
+-				       err, group);
+ 			ext4_mb_unload_buddy(&e4b);
+ 			continue;
+ 		}
+@@ -4550,11 +4539,8 @@ ext4_mb_discard_lg_preallocations(struct
+ 		group = ext4_get_group_number(sb, pa->pa_pstart);
+ 		err = ext4_mb_load_buddy_gfp(sb, group, &e4b,
+ 					     GFP_NOFS|__GFP_NOFAIL);
+-		if (err) {
+-			ext4_error_err(sb, -err, "Error %d loading buddy information for %u",
+-				       err, group);
++		if (err)
+ 			continue;
+-		}
+ 		ext4_lock_group(sb, group);
+ 		list_del(&pa->pa_group_list);
+ 		ext4_get_group_info(sb, group)->bb_prealloc_nr--;
+@@ -4807,7 +4793,7 @@ errout:
+ 			 * been updated or not when fail case. So can
+ 			 * not revert pa_free back, just mark pa_error*/
+ 			pa->pa_error++;
+-			ext4_error(sb,
++			ext4_warning(sb,
+ 				"Updating bitmap error: [err %d] "
+ 				"[pa %p] [phy %lu] [logic %lu] "
+ 				"[len %u] [free %u] [error %u] "
+@@ -4818,6 +4804,7 @@ errout:
+ 				(unsigned)pa->pa_free,
+ 				(unsigned)pa->pa_error,
+ 				pa->pa_inode ? pa->pa_inode->i_ino : 0);
++			ext4_mark_group_bitmap_corrupted(sb, 0, 0);
+ 		}
+ 	}
+ 	ext4_mb_release_context(ac);
+@@ -5103,7 +5090,7 @@ do_more:
+ 	err = ext4_mb_load_buddy_gfp(sb, block_group, &e4b,
+ 				     GFP_NOFS|__GFP_NOFAIL);
+ 	if (err)
+-		goto error_return;
++		goto error_brelse;
+ 
+ 	/*
+ 	 * We need to make sure we don't reuse the freed block until after the
+@@ -5194,8 +5181,9 @@ do_more:
+ 		goto do_more;
+ 	}
+ error_return:
+-	brelse(bitmap_bh);
+ 	ext4_std_error(sb, err);
++error_brelse:
++	brelse(bitmap_bh);
+ 	return;
+ }
+ 
+@@ -5295,7 +5283,7 @@ int ext4_group_add_blocks(handle_t *hand
+ 
+ 	err = ext4_mb_load_buddy(sb, block_group, &e4b);
+ 	if (err)
+-		goto error_return;
++		goto error_brelse;
+ 
+ 	/*
+ 	 * need to update group_info->bb_free and bitmap
+@@ -5334,8 +5322,9 @@ int ext4_group_add_blocks(handle_t *hand
+ 		err = ret;
+ 
+ error_return:
+-	brelse(bitmap_bh);
+ 	ext4_std_error(sb, err);
++error_brelse:
++	brelse(bitmap_bh);
+ 	return err;
+ }
+ 
+@@ -5410,11 +5399,8 @@ ext4_trim_all_free(struct super_block *s
+ 	trace_ext4_trim_all_free(sb, group, start, max);
+ 
+ 	ret = ext4_mb_load_buddy(sb, group, &e4b);
+-	if (ret) {
+-		ext4_warning(sb, "Error %d loading buddy information for %u",
+-			     ret, group);
++	if (ret)
+ 		return ret;
+-	}
+ 	bitmap = e4b.bd_bitmap;
+ 
+ 	ext4_lock_group(sb, group);

ldiskfs/kernel_patches/patches/rhel8.3/ext4-mballoc-extra-checks.patch
@@ -1,0 +1,301 @@
+Index: linux-4.18.0-80.1.2.el8_0/fs/ext4/ext4.h
+===================================================================
+--- linux-4.18.0-80.1.2.el8_0.orig/fs/ext4/ext4.h
++++ linux-4.18.0-80.1.2.el8_0/fs/ext4/ext4.h
+@@ -2881,6 +2881,7 @@ struct ext4_group_info {
+ 	ext4_grpblk_t	bb_fragments;	/* nr of freespace fragments */
+ 	ext4_grpblk_t	bb_largest_free_order;/* order of largest frag in BG */
+ 	struct          list_head bb_prealloc_list;
++	unsigned long   bb_prealloc_nr;
+ #ifdef DOUBLE_CHECK
+ 	void            *bb_bitmap;
+ #endif
+Index: linux-4.18.0-240.1.1.el8/fs/ext4/mballoc.c
+===================================================================
+--- linux-4.18.0-240.1.1.el8.orig/fs/ext4/mballoc.c
++++ linux-4.18.0-240.1.1.el8/fs/ext4/mballoc.c
+@@ -352,7 +352,7 @@ static const char * const ext4_groupinfo
+ 	"ext4_groupinfo_64k", "ext4_groupinfo_128k"
+ };
+ 
+-static void ext4_mb_generate_from_pa(struct super_block *sb, void *bitmap,
++static int ext4_mb_generate_from_pa(struct super_block *sb, void *bitmap,
+ 					ext4_group_t group);
+ static void ext4_mb_generate_from_freelist(struct super_block *sb, void *bitmap,
+ 						ext4_group_t group);
+@@ -708,7 +708,7 @@ mb_set_largest_free_order(struct super_b
+ }
+ 
+ static noinline_for_stack
+-void ext4_mb_generate_buddy(struct super_block *sb,
++int ext4_mb_generate_buddy(struct super_block *sb,
+ 				void *buddy, void *bitmap, ext4_group_t group)
+ {
+ 	struct ext4_group_info *grp = ext4_get_group_info(sb, group);
+@@ -752,6 +752,7 @@ void ext4_mb_generate_buddy(struct super
+ 		grp->bb_free = free;
+ 		ext4_mark_group_bitmap_corrupted(sb, group,
+ 					EXT4_GROUP_INFO_BBITMAP_CORRUPT);
++		return -EIO;
+ 	}
+ 	mb_set_largest_free_order(sb, grp);
+ 
+@@ -762,6 +763,8 @@ void ext4_mb_generate_buddy(struct super
+ 	sbi->s_mb_buddies_generated++;
+ 	sbi->s_mb_generation_time += period;
+ 	spin_unlock(&sbi->s_bal_lock);
++
++	return 0;
+ }
+ 
+ static void mb_regenerate_buddy(struct ext4_buddy *e4b)
+@@ -882,7 +885,7 @@ static int ext4_mb_init_cache(struct pag
+ 	}
+ 
+ 	first_block = page->index * blocks_per_page;
+-	for (i = 0; i < blocks_per_page; i++) {
++	for (i = 0; i < blocks_per_page && err == 0; i++) {
+ 		group = (first_block + i) >> 1;
+ 		if (group >= ngroups)
+ 			break;
+@@ -926,7 +929,7 @@ static int ext4_mb_init_cache(struct pag
+ 			ext4_lock_group(sb, group);
+ 			/* init the buddy */
+ 			memset(data, 0xff, blocksize);
+-			ext4_mb_generate_buddy(sb, data, incore, group);
++			err = ext4_mb_generate_buddy(sb, data, incore, group);
+ 			ext4_unlock_group(sb, group);
+ 			incore = NULL;
+ 		} else {
+@@ -941,7 +944,7 @@ static int ext4_mb_init_cache(struct pag
+ 			memcpy(data, bitmap, blocksize);
+ 
+ 			/* mark all preallocated blks used in in-core bitmap */
+-			ext4_mb_generate_from_pa(sb, data, group);
++			err = ext4_mb_generate_from_pa(sb, data, group);
+ 			ext4_mb_generate_from_freelist(sb, data, group);
+ 			ext4_unlock_group(sb, group);
+ 
+@@ -951,7 +954,8 @@ static int ext4_mb_init_cache(struct pag
+ 			incore = data;
+ 		}
+ 	}
+-	SetPageUptodate(page);
++	if (likely(err == 0))
++		SetPageUptodate(page);
+ 
+ out:
+ 	if (bh) {
+@@ -2280,9 +2284,11 @@ static void *ext4_mb_seq_groups_next(str
+ static int ext4_mb_seq_groups_show(struct seq_file *seq, void *v)
+ {
+ 	struct super_block *sb = PDE_DATA(file_inode(seq->file));
++	struct ext4_group_desc *gdp;
+ 	ext4_group_t group = (ext4_group_t) ((unsigned long) v);
+ 	int i;
+ 	int err, buddy_loaded = 0;
++	int free = 0;
+ 	struct ext4_buddy e4b;
+ 	struct ext4_group_info *grinfo;
+ 	unsigned char blocksize_bits = min_t(unsigned char,
+@@ -2295,7 +2301,7 @@ static int ext4_mb_seq_groups_show(struc
+ 
+ 	group--;
+ 	if (group == 0)
+-		seq_puts(seq, "#group: free  frags first ["
++		seq_puts(seq, "#group: bfree gfree frags first pa    ["
+ 			      " 2^0   2^1   2^2   2^3   2^4   2^5   2^6  "
+ 			      " 2^7   2^8   2^9   2^10  2^11  2^12  2^13  ]\n");
+ 
+@@ -2313,13 +2319,19 @@ static int ext4_mb_seq_groups_show(struc
+ 		buddy_loaded = 1;
+ 	}
+ 
++	gdp = ext4_get_group_desc(sb, group, NULL);
++	if (gdp != NULL)
++		free = ext4_free_group_clusters(sb, gdp);
++
+ 	memcpy(&sg, ext4_get_group_info(sb, group), i);
+ 
+ 	if (buddy_loaded)
+ 		ext4_mb_unload_buddy(&e4b);
+ 
+-	seq_printf(seq, "#%-5u: %-5u %-5u %-5u [", group, sg.info.bb_free,
+-			sg.info.bb_fragments, sg.info.bb_first_free);
++	seq_printf(seq, "#%-5lu: %-5u %-5u %-5u %-5u %-5lu [",
++			(long unsigned int)group, sg.info.bb_free, free,
++			sg.info.bb_fragments, sg.info.bb_first_free,
++			sg.info.bb_prealloc_nr);
+ 	for (i = 0; i <= 13; i++)
+ 		seq_printf(seq, " %-5u", i <= blocksize_bits + 1 ?
+ 				sg.info.bb_counters[i] : 0);
+@@ -3635,22 +3647,71 @@ static void ext4_mb_generate_from_freeli
+ }
+ 
+ /*
++ * check free blocks in bitmap match free block in group descriptor
++ * do this before taking preallocated blocks into account to be able
++ * to detect on-disk corruptions. The group lock should be hold by the
++ * caller.
++ */
++int ext4_mb_check_ondisk_bitmap(struct super_block *sb, void *bitmap,
++				struct ext4_group_desc *gdp, int group)
++{
++	unsigned short max = EXT4_CLUSTERS_PER_GROUP(sb);
++	unsigned short i, first, free = 0;
++	unsigned short free_in_gdp = ext4_free_group_clusters(sb, gdp);
++
++	if (free_in_gdp == 0 && gdp->bg_flags & cpu_to_le16(EXT4_BG_BLOCK_UNINIT))
++		return 0;
++
++	i = mb_find_next_zero_bit(bitmap, max, 0);
++
++	while (i < max) {
++		first = i;
++		i = mb_find_next_bit(bitmap, max, i);
++		if (i > max)
++			i = max;
++		free += i - first;
++		if (i < max)
++			i = mb_find_next_zero_bit(bitmap, max, i);
++	}
++
++	if (free != free_in_gdp) {
++		ext4_error(sb, "on-disk bitmap for group %d"
++			"corrupted: %u blocks free in bitmap, %u - in gd\n",
++			group, free, free_in_gdp);
++		return -EIO;
++	}
++	return 0;
++}
++
++/*
+  * the function goes through all preallocation in this group and marks them
+  * used in in-core bitmap. buddy must be generated from this bitmap
+  * Need to be called with ext4 group lock held
+  */
+ static noinline_for_stack
+-void ext4_mb_generate_from_pa(struct super_block *sb, void *bitmap,
++int ext4_mb_generate_from_pa(struct super_block *sb, void *bitmap,
+ 					ext4_group_t group)
+ {
+ 	struct ext4_group_info *grp = ext4_get_group_info(sb, group);
+ 	struct ext4_prealloc_space *pa;
++	struct ext4_group_desc *gdp;
+ 	struct list_head *cur;
+ 	ext4_group_t groupnr;
+ 	ext4_grpblk_t start;
+ 	int preallocated = 0;
++	int skip = 0, count = 0;
++	int err;
+ 	int len;
+ 
++	gdp = ext4_get_group_desc(sb, group, NULL);
++	if (gdp == NULL)
++		return -EIO;
++
++	/* before applying preallocations, check bitmap consistency */
++	err = ext4_mb_check_ondisk_bitmap(sb, bitmap, gdp, group);
++	if (err)
++		return err;
++
+ 	/* all form of preallocation discards first load group,
+ 	 * so the only competing code is preallocation use.
+ 	 * we don't need any locking here
+@@ -3666,13 +3727,23 @@ void ext4_mb_generate_from_pa(struct sup
+ 					     &groupnr, &start);
+ 		len = pa->pa_len;
+ 		spin_unlock(&pa->pa_lock);
+-		if (unlikely(len == 0))
++		if (unlikely(len == 0)) {
++			skip++;
+ 			continue;
++		}
+ 		BUG_ON(groupnr != group);
+ 		ext4_set_bits(bitmap, start, len);
+ 		preallocated += len;
++		count++;
++	}
++	if (count + skip != grp->bb_prealloc_nr) {
++		ext4_error(sb, "lost preallocations: "
++			   "count %d, bb_prealloc_nr %lu, skip %d\n",
++			   count, grp->bb_prealloc_nr, skip);
++		return -EIO;
+ 	}
+ 	mb_debug(1, "preallocated %u for group %u\n", preallocated, group);
++	return 0;
+ }
+ 
+ static void ext4_mb_pa_callback(struct rcu_head *head)
+@@ -3736,6 +3807,7 @@ static void ext4_mb_put_pa(struct ext4_a
+ 	 */
+ 	ext4_lock_group(sb, grp);
+ 	list_del(&pa->pa_group_list);
++	ext4_get_group_info(sb, grp)->bb_prealloc_nr--;
+ 	ext4_unlock_group(sb, grp);
+ 
+ 	spin_lock(pa->pa_obj_lock);
+@@ -3830,6 +3902,7 @@ ext4_mb_new_inode_pa(struct ext4_allocat
+ 
+ 	ext4_lock_group(sb, ac->ac_b_ex.fe_group);
+ 	list_add(&pa->pa_group_list, &grp->bb_prealloc_list);
++	grp->bb_prealloc_nr++;
+ 	ext4_unlock_group(sb, ac->ac_b_ex.fe_group);
+ 
+ 	spin_lock(pa->pa_obj_lock);
+@@ -3891,6 +3964,7 @@ ext4_mb_new_group_pa(struct ext4_allocat
+ 
+ 	ext4_lock_group(sb, ac->ac_b_ex.fe_group);
+ 	list_add(&pa->pa_group_list, &grp->bb_prealloc_list);
++	grp->bb_prealloc_nr++;
+ 	ext4_unlock_group(sb, ac->ac_b_ex.fe_group);
+ 
+ 	/*
+@@ -4063,6 +4137,8 @@ repeat:
+ 
+ 		spin_unlock(&pa->pa_lock);
+ 
++		BUG_ON(grp->bb_prealloc_nr == 0);
++		grp->bb_prealloc_nr--;
+ 		list_del(&pa->pa_group_list);
+ 		list_add(&pa->u.pa_tmp_list, &list);
+ 	}
+@@ -4193,7 +4269,7 @@ repeat:
+ 		if (err) {
+ 			ext4_error_err(sb, -err, "Error %d loading buddy information for %u",
+ 				       err, group);
+-			continue;
++			return;
+ 		}
+ 
+ 		bitmap_bh = ext4_read_block_bitmap(sb, group);
+@@ -4206,6 +4282,8 @@ repeat:
+ 		}
+ 
+ 		ext4_lock_group(sb, group);
++		BUG_ON(e4b.bd_info->bb_prealloc_nr == 0);
++		e4b.bd_info->bb_prealloc_nr--;
+ 		list_del(&pa->pa_group_list);
+ 		ext4_mb_release_inode_pa(&e4b, bitmap_bh, pa);
+ 		ext4_unlock_group(sb, group);
+@@ -4467,6 +4545,7 @@ ext4_mb_discard_lg_preallocations(struct
+ 		}
+ 		ext4_lock_group(sb, group);
+ 		list_del(&pa->pa_group_list);
++		ext4_get_group_info(sb, group)->bb_prealloc_nr--;
+ 		ext4_mb_release_group_pa(&e4b, pa);
+ 		ext4_unlock_group(sb, group);
+ 
+Index: linux-4.18.0-80.1.2.el8_0/fs/ext4/mballoc.h
+===================================================================
+--- linux-4.18.0-80.1.2.el8_0.orig/fs/ext4/mballoc.h
++++ linux-4.18.0-80.1.2.el8_0/fs/ext4/mballoc.h
+@@ -70,7 +70,7 @@ do {									\
+ /*
+  * for which requests use 2^N search using buddies
+  */
+-#define MB_DEFAULT_ORDER2_REQS		2
++#define MB_DEFAULT_ORDER2_REQS		8
+ 
+ /*
+  * default group prealloc size 512 blocks

commit 4aa17923f08bea425a20961cb6eaa72ad9af38c1

LU-14205 osd-ldiskfs: fix return of osd_extend_restart_trans()
ldiskfs_journal_ensure_credits() could possibly return 1 if
transaction restarted, Lustre osd expect it return 0 or negative.

Fixes: cf48a2d23b ("LU-13839 kernel: RHEL 8.3 server support")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Change-Id: I51ff0bd50575902b66529cbd71ff14710c202d03
Reviewed-on: https://review.whamcloud.com/40989
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -942,8 +942,15 @@ static int osd_extend_trans(handle_t *handle, int needed,
 static int osd_extend_restart_trans(handle_t *handle, int needed,
 				    struct inode *inode)
 {
-	return ldiskfs_journal_ensure_credits(handle, needed,
+	int rc;
+
+	rc = ldiskfs_journal_ensure_credits(handle, needed,
 		ldiskfs_trans_default_revoke_credits(inode->i_sb));
+	/* this means journal has been restarted */
+	if (rc > 0)
+		rc = 0;
+
+	return rc;
 }
 #else
 static int osd_extend_trans(handle_t *handle, int needed,