lustre/lustre-release

commit 48457868a02ae5a0407f304c9028f7e80e7fb574

LU-3606 fallocate: Implement fallocate preallocate operation
This patch adds fallocate(2) preallocate operation support
for Lustre. fallocate(2) method of the inode_operations or
file_operations is implemented and transported to the OSTs
to interface with the underlying OSD's fallocate(2) code.

In a saperate patch, a new RPC, OST_FALLOCATE has been
added and reserved for space preallocation. The fallocate
functionality (prealloc) in CLIO has been multiplexed
with CIT_SETATTR. (https://review.whamcloud.com/37277)

Lustre fsx(File system exerciser) is updated in a saperate
patch to handle fallocate calls. (https://review.whamcloud.com/37277)

Only fallocate preallocate operation is supported by this
patch for now. Other operations like, FALLOC_FL_PUNCH
(deallocate), FALLOC_FL_ZERO_RANGE, FALLOC_FL_COLLAPSE_RANGE
and FALLOC_FL_INSPECT_RANGE is not supported by this patch
and will be addressed by a separate patch.

ZFS operation is not supported by this patch. ZFS fallocate(2)
will be addressed by patch (https://review.whamcloud.com/36506/)

New test case under sanity is added to verify fallocate
call.

Test-Parameters: fstype=ldiskfs testlist=sanity,sanityn,sanity-dom
Signed-off-by: Swapnil Pimpale <spimpale@ddn.com>
Signed-off-by: Li Xi <lixi@ddn.com>
Signed-off-by: Abrarahmed Momin <abrar.momin@gmail.com>
Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
Change-Id: I03f27d356616fbf3a3ab8e6309af26c00434d81b
Reviewed-on: https://review.whamcloud.com/9275
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -1864,6 +1864,69 @@ static ssize_t osd_write(const struct lu_env *env, struct dt_object *dt,
 	return result;
 }
 
+static int osd_declare_fallocate(const struct lu_env *env,
+				 struct dt_object *dt, struct thandle *th)
+{
+	struct osd_thandle *oh;
+	struct inode *inode;
+	int rc;
+	ENTRY;
+
+	LASSERT(th);
+	oh = container_of(th, struct osd_thandle, ot_super);
+
+	osd_trans_declare_op(env, oh, OSD_OT_PREALLOC,
+			     osd_dto_credits_noquota[DTO_WRITE_BLOCK]);
+	inode = osd_dt_obj(dt)->oo_inode;
+	LASSERT(inode);
+
+	rc = osd_declare_inode_qid(env, i_uid_read(inode), i_gid_read(inode),
+				   i_projid_read(inode), 0, oh, osd_dt_obj(dt),
+				   NULL, OSD_QID_BLK);
+	RETURN(rc);
+}
+
+static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
+			 __u64 start, __u64 end, int mode, struct thandle *th)
+{
+	struct osd_object *obj = osd_dt_obj(dt);
+	struct inode *inode = obj->oo_inode;
+	int rc = 0;
+	struct osd_thread_info *info = osd_oti_get(env);
+	struct dentry *dentry = &info->oti_obj_dentry;
+	struct file *file = &info->oti_file;
+
+	ENTRY;
+	/*
+	 * Only mode == 0 (which is standard prealloc) is supported now.
+	 * Rest of mode options is not supported yet.
+	 */
+	if (mode & ~FALLOC_FL_KEEP_SIZE)
+		RETURN(-EOPNOTSUPP);
+
+	LASSERT(dt_object_exists(dt));
+	LASSERT(osd_invariant(obj));
+	LASSERT(inode != NULL);
+	dquot_initialize(inode);
+
+	LASSERT(th);
+
+	osd_trans_exec_op(env, th, OSD_OT_PREALLOC);
+
+	/*
+	 * Because f_op->fallocate() does not have an inode arg
+	 */
+	dentry->d_inode = inode;
+	dentry->d_sb = inode->i_sb;
+	file->f_path.dentry = dentry;
+	file->f_mapping = inode->i_mapping;
+	file->f_op = inode->i_fop;
+	file->f_inode = inode;
+	rc = file->f_op->fallocate(file, mode, start, end - start);
+
+	RETURN(rc);
+}
+
 static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
                              __u64 start, __u64 end, struct thandle *th)
 {
@@ -1911,7 +1974,6 @@ static int osd_punch(const struct lu_env *env, struct dt_object *dt,
 	bool grow = false;
 	ENTRY;
 
-	LASSERT(end == OBD_OBJECT_EOF);
 	LASSERT(dt_object_exists(dt));
 	LASSERT(osd_invariant(obj));
 	LASSERT(inode != NULL);
@@ -2086,6 +2148,8 @@ const struct dt_body_operations osd_body_ops = {
 	.dbo_punch			= osd_punch,
 	.dbo_fiemap_get			= osd_fiemap_get,
 	.dbo_ladvise			= osd_ladvise,
+	.dbo_declare_fallocate		= osd_declare_fallocate,
+	.dbo_fallocate			= osd_fallocate,
 };
 
 /**

lustre/osd-zfs/osd_io.c
@@ -1119,6 +1119,32 @@ static int osd_ladvise(const struct lu_env *env, struct dt_object *dt,
 	RETURN(rc);
 }
 
+static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
+			 __u64 start, __u64 end, int mode, struct thandle *th)
+{
+	int rc = -EOPNOTSUPP;
+	ENTRY;
+
+	 /*
+	  * space preallocation is not supported for ZFS
+	  * Returns -EOPNOTSUPP for now
+	  */
+	RETURN(rc);
+}
+
+static int osd_declare_fallocate(const struct lu_env *env,
+				 struct dt_object *dt, struct thandle *th)
+{
+	int rc = -EOPNOTSUPP;
+	ENTRY;
+
+	 /*
+	  * space preallocation is not supported for ZFS
+	  * Returns -EOPNOTSUPP for now
+	  */
+	RETURN(rc);
+}
+
 struct dt_body_operations osd_body_ops = {
 	.dbo_read			= osd_read,
 	.dbo_declare_write		= osd_declare_write,
@@ -1132,6 +1158,8 @@ struct dt_body_operations osd_body_ops = {
 	.dbo_declare_punch		= osd_declare_punch,
 	.dbo_punch			= osd_punch,
 	.dbo_ladvise			= osd_ladvise,
+	.dbo_declare_fallocate		= osd_declare_fallocate,
+	.dbo_fallocate			= osd_fallocate,
 };
 
 struct dt_body_operations osd_body_scrub_ops = {

lustre/include/cl_object.h
@@ -1756,6 +1756,31 @@ struct cl_io_rw_common {
 	size_t	crw_count;
 	int	crw_nonblock;
 };
+enum cl_setattr_subtype {
+	/** regular setattr **/
+	CL_SETATTR_REG = 1,
+	/** truncate(2) **/
+	CL_SETATTR_TRUNC,
+	/** fallocate(2) - mode preallocate **/
+	CL_SETATTR_FALLOCATE
+};
+
+struct cl_io_range {
+	loff_t cir_pos;
+	size_t cir_count;
+};
+
+struct cl_io_pt {
+	struct cl_io_pt *cip_next;
+	struct kiocb cip_iocb;
+	struct iov_iter cip_iter;
+	struct file *cip_file;
+	enum cl_io_type cip_iot;
+	unsigned int cip_need_restart:1;
+	loff_t cip_pos;
+	size_t cip_count;
+	ssize_t cip_result;
+};
 
 /**
  * State for io.
@@ -1804,6 +1829,14 @@ struct cl_io {
 			int			 sa_stripe_index;
 			struct ost_layout	 sa_layout;
 			const struct lu_fid	*sa_parent_fid;
+			/* SETATTR interface is used for regular setattr, */
+			/* truncate(2) and fallocate(2) subtypes */
+			enum cl_setattr_subtype	 sa_subtype;
+			/* The following are used for fallocate(2) */
+			int			 sa_falloc_mode;
+			loff_t			 sa_falloc_offset;
+			loff_t			 sa_falloc_len;
+			loff_t			 sa_falloc_end;
 		} ci_setattr;
 		struct cl_data_version_io {
 			u64 dv_data_version;
@@ -2379,7 +2412,14 @@ static inline int cl_io_is_mkwrite(const struct cl_io *io)
 static inline int cl_io_is_trunc(const struct cl_io *io)
 {
 	return io->ci_type == CIT_SETATTR &&
-		(io->u.ci_setattr.sa_avalid & ATTR_SIZE);
+		(io->u.ci_setattr.sa_avalid & ATTR_SIZE) &&
+		(io->u.ci_setattr.sa_subtype != CL_SETATTR_FALLOCATE);
+}
+
+static inline int cl_io_is_fallocate(const struct cl_io *io)
+{
+	return (io->ci_type == CIT_SETATTR) &&
+	       (io->u.ci_setattr.sa_subtype == CL_SETATTR_FALLOCATE);
 }
 
 struct cl_io *cl_io_top(struct cl_io *io);

lustre/include/dt_object.h
@@ -1413,6 +1413,39 @@ struct dt_body_operations {
 			     __u64 start,
 			     __u64 end,
 			     enum lu_ladvise_type advice);
+
+	/**
+	 * Declare intention to preallocate space for an object
+	 *
+	 * \param[in] env	execution environment for this thread
+	 * \param[in] dt	object
+	 * \param[in] th	transaction handle
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int (*dbo_declare_fallocate)(const struct lu_env *env,
+				    struct dt_object *dt,
+				    struct thandle *th);
+	/**
+	 * Allocate specified region for an object
+	 *
+	 * \param[in] env	execution environment for this thread
+	 * \param[in] dt	object
+	 * \param[in] start	the start of the region to allocate
+	 * \param[in] end	the end of the region to allocate
+	 * \param[in] mode	fallocate mode
+	 * \param[in] th	transaction handle
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int (*dbo_fallocate)(const struct lu_env *env,
+			    struct dt_object *dt,
+			    __u64 start,
+			    __u64 end,
+			    int mode,
+			    struct thandle *th);
 };
 
 /**
@@ -2516,6 +2549,29 @@ static inline int dt_ladvise(const struct lu_env *env, struct dt_object *dt,
 	LASSERT(dt->do_body_ops);
 	LASSERT(dt->do_body_ops->dbo_ladvise);
 	return dt->do_body_ops->dbo_ladvise(env, dt, start, end, advice);
+}
+
+static inline int dt_declare_falloc(const struct lu_env *env,
+				      struct dt_object *dt, struct thandle *th)
+{
+	LASSERT(dt);
+	if (!dt->do_body_ops)
+		return -EOPNOTSUPP;
+	LASSERT(dt->do_body_ops);
+	LASSERT(dt->do_body_ops->dbo_declare_fallocate);
+	return dt->do_body_ops->dbo_declare_fallocate(env, dt, th);
+}
+
+static inline int dt_falloc(const struct lu_env *env, struct dt_object *dt,
+			      __u64 start, __u64 end, int mode,
+			      struct thandle *th)
+{
+	LASSERT(dt);
+	if (!dt->do_body_ops)
+		return -EOPNOTSUPP;
+	LASSERT(dt->do_body_ops);
+	LASSERT(dt->do_body_ops->dbo_fallocate);
+	return dt->do_body_ops->dbo_fallocate(env, dt, start, end, mode, th);
 }
 
 static inline int dt_fiemap_get(const struct lu_env *env, struct dt_object *d,

lustre/include/lustre_req_layout.h
@@ -197,6 +197,7 @@ extern struct req_format RQF_OST_GETATTR;
 extern struct req_format RQF_OST_SETATTR;
 extern struct req_format RQF_OST_CREATE;
 extern struct req_format RQF_OST_PUNCH;
+extern struct req_format RQF_OST_FALLOCATE;
 extern struct req_format RQF_OST_SYNC;
 extern struct req_format RQF_OST_DESTROY;
 extern struct req_format RQF_OST_BRW_READ;

lustre/include/obd_support.h
@@ -344,15 +344,16 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_OST_DELAY_TRANS	 0x246
 #define OBD_FAIL_OST_PREPARE_DELAY	 0x247
 #define OBD_FAIL_OST_2BIG_NIOBUF	 0x248
+#define OBD_FAIL_OST_FALLOCATE_NET	 0x249
 
 #define OBD_FAIL_LDLM                    0x300
 #define OBD_FAIL_LDLM_NAMESPACE_NEW      0x301
-#define OBD_FAIL_LDLM_ENQUEUE_NET			0x302
-#define OBD_FAIL_LDLM_CONVERT_NET			0x303
-#define OBD_FAIL_LDLM_CANCEL_NET			0x304
-#define OBD_FAIL_LDLM_BL_CALLBACK_NET			0x305
-#define OBD_FAIL_LDLM_CP_CALLBACK_NET			0x306
-#define OBD_FAIL_LDLM_GL_CALLBACK_NET			0x307
+#define OBD_FAIL_LDLM_ENQUEUE_NET	 0x302
+#define OBD_FAIL_LDLM_CONVERT_NET	 0x303
+#define OBD_FAIL_LDLM_CANCEL_NET	 0x304
+#define OBD_FAIL_LDLM_BL_CALLBACK_NET	 0x305
+#define OBD_FAIL_LDLM_CP_CALLBACK_NET	 0x306
+#define OBD_FAIL_LDLM_GL_CALLBACK_NET	 0x307
 #define OBD_FAIL_LDLM_ENQUEUE_EXTENT_ERR 0x308
 #define OBD_FAIL_LDLM_ENQUEUE_INTENT_ERR 0x309
 #define OBD_FAIL_LDLM_CREATE_RESOURCE    0x30a

lustre/llite/file.c
@@ -43,6 +43,7 @@
 #include <linux/sched.h>
 #include <linux/user_namespace.h>
 #include <linux/uidgid.h>
+#include <linux/falloc.h>
 
 #include <uapi/linux/lustre/lustre_ioctl.h>
 #include <lustre_swab.h>
@@ -4780,6 +4781,84 @@ int ll_getattr(struct vfsmount *mnt, struct dentry *de, struct kstat *stat)
 	return ll_getattr_dentry(de, stat);
 }
 
+int cl_falloc(struct inode *inode, int mode, loff_t offset, loff_t len)
+{
+	struct lu_env *env;
+	struct cl_io *io;
+	__u16 refcheck;
+	int rc; loff_t sa_falloc_end;
+	loff_t size = i_size_read(inode);
+
+	ENTRY;
+
+	env = cl_env_get(&refcheck);
+	if (IS_ERR(env))
+		RETURN(PTR_ERR(env));
+
+	io = vvp_env_thread_io(env);
+	io->ci_obj = ll_i2info(inode)->lli_clob;
+	io->ci_verify_layout = 1;
+	io->u.ci_setattr.sa_parent_fid = lu_object_fid(&io->ci_obj->co_lu);
+	io->u.ci_setattr.sa_falloc_mode = mode;
+	io->u.ci_setattr.sa_falloc_offset = offset;
+	io->u.ci_setattr.sa_falloc_len = len;
+	io->u.ci_setattr.sa_falloc_end = io->u.ci_setattr.sa_falloc_offset +
+		io->u.ci_setattr.sa_falloc_len;
+	io->u.ci_setattr.sa_subtype = CL_SETATTR_FALLOCATE;
+	sa_falloc_end = io->u.ci_setattr.sa_falloc_end;
+	if (sa_falloc_end > size) {
+		/* Check new size against VFS/VM file size limit and rlimit */
+		rc = inode_newsize_ok(inode, sa_falloc_end);
+		if (rc)
+			goto out;
+		if (sa_falloc_end > ll_file_maxbytes(inode)) {
+			CDEBUG(D_INODE, "file size too large %llu > %llu\n",
+			       (unsigned long long)(sa_falloc_end),
+			       ll_file_maxbytes(inode));
+			rc = -EFBIG;
+			goto out;
+		}
+		io->u.ci_setattr.sa_attr.lvb_size = sa_falloc_end;
+		if (!(mode & FALLOC_FL_KEEP_SIZE))
+			io->u.ci_setattr.sa_avalid |= ATTR_SIZE;
+	} else {
+		io->u.ci_setattr.sa_attr.lvb_size = size;
+	}
+
+again:
+	if (cl_io_init(env, io, CIT_SETATTR, io->ci_obj) == 0)
+		rc = cl_io_loop(env, io);
+	else
+		rc = io->ci_result;
+
+	cl_io_fini(env, io);
+	if (unlikely(io->ci_need_restart))
+		goto again;
+
+out:
+	cl_env_put(env, &refcheck);
+	RETURN(rc);
+}
+
+long ll_fallocate(struct file *filp, int mode, loff_t offset, loff_t len)
+{
+	struct inode *inode = filp->f_path.dentry->d_inode;
+	int rc;
+
+	/*
+	 * Only mode == 0 (which is standard prealloc) is supported now.
+	 * Punch is not supported yet.
+	 */
+	if (mode & ~FALLOC_FL_KEEP_SIZE)
+		RETURN(-EOPNOTSUPP);
+
+	ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_FALLOCATE, 1);
+
+	rc = cl_falloc(inode, mode, offset, len);
+
+	RETURN(rc);
+}
+
 static int ll_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
 		     __u64 start, __u64 len)
 {
@@ -4902,19 +4981,22 @@ int ll_inode_permission(struct inode *inode, int mask)
 	cfs_cap_t cap;
 	bool squash_id = false;
 	ktime_t kstart = ktime_get();
+
 	ENTRY;
 
 	if (mask & MAY_NOT_BLOCK)
 		return -ECHILD;
 
-       /* as root inode are NOT getting validated in lookup operation,
-        * need to do it before permission check. */
+	/*
+	 * as root inode are NOT getting validated in lookup operation,
+	 * need to do it before permission check.
+	 */
 
-        if (inode == inode->i_sb->s_root->d_inode) {
+	if (inode == inode->i_sb->s_root->d_inode) {
 		rc = ll_inode_revalidate(inode->i_sb->s_root, IT_LOOKUP);
-                if (rc)
-                        RETURN(rc);
-        }
+		if (rc)
+			RETURN(rc);
+	}
 
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p), inode mode %x mask %o\n",
 	       PFID(ll_inode2fid(inode)), inode, inode->i_mode, mask);
@@ -4983,7 +5065,8 @@ struct file_operations ll_file_operations = {
 	.llseek		= ll_file_seek,
 	.splice_read	= ll_file_splice_read,
 	.fsync		= ll_fsync,
-	.flush		= ll_flush
+	.flush		= ll_flush,
+	.fallocate	= ll_fallocate,
 };
 
 struct file_operations ll_file_operations_flock = {
@@ -5009,7 +5092,8 @@ struct file_operations ll_file_operations_flock = {
 	.fsync		= ll_fsync,
 	.flush		= ll_flush,
 	.flock		= ll_file_flock,
-	.lock		= ll_file_flock
+	.lock		= ll_file_flock,
+	.fallocate	= ll_fallocate,
 };
 
 /* These are for -o noflock - to return ENOSYS on flock calls */
@@ -5036,7 +5120,8 @@ struct file_operations ll_file_operations_noflock = {
 	.fsync		= ll_fsync,
 	.flush		= ll_flush,
 	.flock		= ll_file_noflock,
-	.lock		= ll_file_noflock
+	.lock		= ll_file_noflock,
+	.fallocate	= ll_fallocate,
 };
 
 struct inode_operations ll_file_inode_operations = {

lustre/llite/lcommon_cl.c
@@ -72,8 +72,8 @@ int cl_setattr_ost(struct cl_object *obj, const struct iattr *attr,
 {
 	struct lu_env *env;
 	struct cl_io  *io;
-	int            result;
-	__u16          refcheck;
+	int result;
+	__u16 refcheck;
 
 	ENTRY;
 
@@ -93,7 +93,8 @@ int cl_setattr_ost(struct cl_object *obj, const struct iattr *attr,
 	io->u.ci_setattr.sa_avalid = attr->ia_valid;
 	io->u.ci_setattr.sa_xvalid = xvalid;
 	io->u.ci_setattr.sa_parent_fid = lu_object_fid(&obj->co_lu);
-
+	if (attr->ia_valid & ATTR_SIZE)
+		io->u.ci_setattr.sa_subtype = CL_SETATTR_TRUNC;
 again:
 	if (attr->ia_valid & ATTR_FILE)
 		ll_io_set_mirror(io, attr->ia_file);
@@ -102,8 +103,10 @@ again:
 		struct vvp_io *vio = vvp_env_io(env);
 
 		if (attr->ia_valid & ATTR_FILE)
-			/* populate the file descriptor for ftruncate to honor
-			 * group lock - see LU-787 */
+			/*
+			 * populate the file descriptor for ftruncate to honor
+			 * group lock - see LU-787
+			 */
 			vio->vui_fd = attr->ia_file->private_data;
 
 		result = cl_io_loop(env, io);

commit 72617588ac8cb2e3e5a7b8e5ebc201cab524d938

LU-14286 osd-ldiskfs: fallocate() should zero new blocks
The ldiskfs osd_fallocate() does not correctly zero the
allocated blocks. This is not noticed during autotest and
local developer testing because the underlying storage is
zero-filled due to using a sparse backing file and will
usually read back as zero. Also, the fsx data correctness
test was not properly detecting fallocate() support, and
as a result this behavior was not detected properly.

'fsx' uses test_fallocate() to determine if fallocate()
along with its various FLAGS is enabled and could be
called. It was found that test_fallocate() always returned
false as a bug. Resulting is fallocate being skipped always
for all fsx runs.

This patches fixes test_fallocate() to properly determine
if fallocate calls could be supported or not and return
appropriately and correctly (1 or 0) true/false.

After patch:
~~~~~~~~~~~
$ MOUNT_2="yes" bash ./lustre/tests/llmount.sh

$ lfs setstripe -c -1 /mnt/lustre/f110

$ fsx -c 50 -p 100 -N 2500 -S 0 -l 10485760 /mnt/lustre*/f110
Chance of close/open is 1 in 50
Seed set to 4481
fd 0: /mnt/lustre/f110
fd 1: /mnt/lustre2/f110
fsx: test_fallocate: does not support fallocate mode 0x3, disabling!
fsx: test_fallocate: does not support fallocate mode 0x10, disabling!
skipping zero size read
:
fallocating to largest ever: 0x30ce
:
1900[0] 1609444486.316868 fallocate  0x7f6c thru 0xab3a (0x2bcf bytes)
:
2300[1] 1609444487.241910 fallocate  0x0697 thru 0xed8e (0xe6f8 bytes)
:
All operations completed A-OK!
$

Test-Parameters: trivial testlist=sanity,sanityn,sanity-dom env=COUNT=10000
Fixes: 48457868a02a ("LU-3606 fallocate: Implement fallocate preallocate operation")
Fixes: 853d180121a6 ("LU-3606 fsx: Add fallocate operation to fsx")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Idbb092db6bd09cfda129f6077dcbfae4f4c9d0d0
Reviewed-on: https://review.whamcloud.com/41119
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -2218,7 +2218,8 @@ static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
 	boff = start >> inode->i_blkbits;
 	blen = (ALIGN(end, 1 << inode->i_blkbits) >> inode->i_blkbits) - boff;
 
-	flags = LDISKFS_GET_BLOCKS_CREATE;
+	/* Create and Write zeros to new extents */
+	flags = LDISKFS_GET_BLOCKS_CREATE_ZERO;
 	if (mode & FALLOC_FL_KEEP_SIZE)
 		flags |= LDISKFS_GET_BLOCKS_KEEP_SIZE;
 

lustre/tests/fsx.c
@@ -1530,12 +1530,13 @@ test_fallocate(int mode)
 			if (!quiet)
 				warn("%s: filesystem does not support fallocate mode 0x%x, disabling!",
 				     __func__, mode);
+		} else {
+			ret = 1;
 		}
 
-		if (ret == 0 && ftruncate(fd, 0) == -1) {
-			ret = 1;
+		/* Call truncate only when fallocate succeeds */
+		if (ret == 1 && ftruncate(fd, 0) == -1)
 			warn("ftruncate to 0 size failed");
-		}
 	}
 	return ret;
 }