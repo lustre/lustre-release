cadc77a9ba5ca428786c6790cc4e3a496efd5488
Hongchao Zhang <hongchao@whamcloud.com>
2021-01-30 04:51:43 +0800

    LU-14299 test: sleep to enable quota acquire again
    sanity-quota test_61 fails with incorrect quota exceeded
    errors because quota acquire will be disabled for 5 seconds
    after edquot flag is set.  The test should introduce some
    delay between the test of over quota and normal one.
    
    Lustre-change: https://review.whamcloud.com/41389
    Lustre-commit: 430e3f01ef2dc83ed317cf2b97be8a2ad50d9f13
    
    Test-Parameters: trivial fstype=zfs testlist=sanity-quota env=ONLY=61,ONLY_REPEAT=20
    Fixes: 530881fe4ee20 ("LU-7816 quota: add default quota setting support")
    Change-Id: I8040ba960f32cf01cb7cee3a77c06ad4bd732f0e
    Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Sergey Cheremencev <sergey.cheremencev@hpe.com>
    Signed-off-by: Minh Diep <mdiep@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47618
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/tests/sanity-quota.sh
+++ lustre/tests/sanity-quota.sh
@@ -3314,6 +3314,10 @@ test_default_quota() {
 	fi
 
 	log "Increase default quota"
+
+	# LU-4505: sleep 5 seconds to enable quota acquire
+	sleep 5
+
 	# increase default quota
 	$LFS setquota $qdtype $qs $((LIMIT*3)) $qh $((LIMIT*3)) $DIR ||
 		error "set default quota failed"
@@ -3349,6 +3353,10 @@ test_default_quota() {
 	fi
 
 	log "Set to use default quota again"
+
+	# LU-4505: sleep 5 seconds to enable quota acquire
+	sleep 5
+
 	$LFS setquota $qtype $qid -d $DIR ||
 		error "set $qid to use default quota failed"
 
