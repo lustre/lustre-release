lustre/lustre-release

commit 6350af100c204a4db794e7472951ab72c3bc20ef

LU-3437 mdd: Fix ACL/def_ACL during object creation.
In mdd_acl_init, after getting default ACL which will
be converted to ACL, then later(in mdd_create) the
acl_buf is re-used to set both default_ACL and ACL,
which is definitely wrong.

And also the remote directory creation does not use
correct ACL/default ACL.

This patch use different buf for def ACL and ACL to avoid
confusion.

Signed-off-by: wang di <di.wang@intel.com>
Change-Id: I30177bc55e4c6b4d916ccf118ab3485f3394fa87
Reviewed-on: http://review.whamcloud.com/6552
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/mdd/mdd_dir.c
@@ -1849,10 +1849,11 @@ static int mdd_declare_create(const struct lu_env *env, struct mdd_device *mdd,
 			      struct mdd_object *p, struct mdd_object *c,
 			      const struct lu_name *name,
 			      struct lu_attr *attr,
-			      int got_def_acl,
 			      struct thandle *handle,
 			      const struct md_op_spec *spec,
-			      struct linkea_data *ldata)
+			      struct linkea_data *ldata,
+			      struct lu_buf *def_acl_buf,
+			      struct lu_buf *acl_buf)
 {
 	int rc;
 
@@ -1861,19 +1862,16 @@ static int mdd_declare_create(const struct lu_env *env, struct mdd_device *mdd,
                 GOTO(out, rc);
 
 #ifdef CONFIG_FS_POSIX_ACL
-	if (got_def_acl > 0) {
-		struct lu_buf *acl_buf;
-
-		acl_buf = mdd_buf_get(env, NULL, got_def_acl);
+	if (def_acl_buf->lb_len > 0 && S_ISDIR(attr->la_mode)) {
 		/* if dir, then can inherit default ACl */
-		if (S_ISDIR(attr->la_mode)) {
-			rc = mdo_declare_xattr_set(env, c, acl_buf,
-						   XATTR_NAME_ACL_DEFAULT,
-						   0, handle);
-			if (rc)
-				GOTO(out, rc);
-		}
+		rc = mdo_declare_xattr_set(env, c, def_acl_buf,
+					   XATTR_NAME_ACL_DEFAULT,
+					   0, handle);
+		if (rc)
+			GOTO(out, rc);
+	}
 
+	if (acl_buf->lb_len > 0) {
 		rc = mdo_declare_attr_set(env, c, attr, handle);
 		if (rc)
 			GOTO(out, rc);
@@ -1938,27 +1936,31 @@ out:
 }
 
 static int mdd_acl_init(const struct lu_env *env, struct mdd_object *pobj,
-			struct lu_attr *la, struct lu_buf *acl_buf,
-			int *got_def_acl, int *reset_acl)
+			struct lu_attr *la, struct lu_buf *def_acl_buf,
+			struct lu_buf *acl_buf)
 {
 	int	rc;
 	ENTRY;
 
-	if (S_ISLNK(la->la_mode))
+	if (S_ISLNK(la->la_mode)) {
+		acl_buf->lb_len = 0;
+		def_acl_buf->lb_len = 0;
 		RETURN(0);
+	}
 
 	mdd_read_lock(env, pobj, MOR_TGT_PARENT);
-	rc = mdo_xattr_get(env, pobj, acl_buf,
+	rc = mdo_xattr_get(env, pobj, def_acl_buf,
 			   XATTR_NAME_ACL_DEFAULT, BYPASS_CAPA);
 	mdd_read_unlock(env, pobj);
 	if (rc > 0) {
-		/* If there are default ACL, fix mode by default ACL */
-		*got_def_acl = rc;
+		/* If there are default ACL, fix mode/ACL by default ACL */
+		def_acl_buf->lb_len = rc;
+		LASSERT(def_acl_buf->lb_len <= acl_buf->lb_len);
+		memcpy(acl_buf->lb_buf, def_acl_buf->lb_buf, rc);
 		acl_buf->lb_len = rc;
 		rc = __mdd_fix_mode_acl(env, acl_buf, &la->la_mode);
 		if (rc < 0)
 			RETURN(rc);
-		*reset_acl = rc;
 	} else if (rc == -ENODATA || rc == -EOPNOTSUPP) {
 		/* If there are no default ACL, fix mode by mask */
 		struct lu_ucred *uc = lu_ucred(env);
@@ -1968,6 +1970,8 @@ static int mdd_acl_init(const struct lu_env *env, struct mdd_object *pobj,
 		if (unlikely(uc != NULL))
 			la->la_mode &= ~uc->uc_umask;
 		rc = 0;
+		acl_buf->lb_len = 0;
+		def_acl_buf->lb_len = 0;
 	}
 
 	RETURN(rc);
@@ -1989,12 +1993,11 @@ static int mdd_create(const struct lu_env *env, struct md_object *pobj,
 	struct thandle		*handle;
 	struct lu_attr		*pattr = &info->mti_pattr;
 	struct lu_buf		acl_buf;
+	struct lu_buf		def_acl_buf;
 	struct linkea_data	*ldata = &info->mti_link_data;
 	struct dynlock_handle	*dlh;
 	const char		*name = lname->ln_name;
 	int			 rc, created = 0, initialized = 0, inserted = 0;
-	int			 got_def_acl = 0;
-	int			 reset_acl = 0;
 	ENTRY;
 
         /*
@@ -2047,8 +2050,9 @@ static int mdd_create(const struct lu_env *env, struct md_object *pobj,
 
 	acl_buf.lb_buf = info->mti_xattr_buf;
 	acl_buf.lb_len = sizeof(info->mti_xattr_buf);
-	rc = mdd_acl_init(env, mdd_pobj, attr, &acl_buf, &got_def_acl,
-			  &reset_acl);
+	def_acl_buf.lb_buf = info->mti_key;
+	def_acl_buf.lb_len = sizeof(info->mti_key);
+	rc = mdd_acl_init(env, mdd_pobj, attr, &def_acl_buf, &acl_buf);
 	if (rc < 0)
 		GOTO(out_free, rc);
 
@@ -2061,8 +2065,9 @@ static int mdd_create(const struct lu_env *env, struct md_object *pobj,
 	memset(ldata, 0, sizeof(*ldata));
 	mdd_linkea_prepare(env, son, NULL, NULL, mdd_object_fid(mdd_pobj),
 			   lname, 1, 0, ldata);
+
 	rc = mdd_declare_create(env, mdd, mdd_pobj, son, lname, attr,
-				got_def_acl, handle, spec, ldata);
+				handle, spec, ldata, &def_acl_buf, &acl_buf);
         if (rc)
                 GOTO(out_stop, rc);
 
@@ -2084,34 +2089,24 @@ static int mdd_create(const struct lu_env *env, struct md_object *pobj,
 	created = 1;
 
 #ifdef CONFIG_FS_POSIX_ACL
-	if (got_def_acl) {
+	if (def_acl_buf.lb_len > 0 && S_ISDIR(attr->la_mode)) {
 		/* set default acl */
-		if (S_ISDIR(attr->la_mode)) {
-			LASSERTF(acl_buf.lb_len  == got_def_acl,
-				 "invalid acl_buf: %p:%d got_def %d\n",
-				 acl_buf.lb_buf, (int)acl_buf.lb_len,
-				 got_def_acl);
-			rc = mdo_xattr_set(env, son, &acl_buf,
-					   XATTR_NAME_ACL_DEFAULT, 0,
-					   handle, BYPASS_CAPA);
-			if (rc) {
-				mdd_write_unlock(env, son);
-				GOTO(cleanup, rc);
-			}
+		rc = mdo_xattr_set(env, son, &def_acl_buf,
+				   XATTR_NAME_ACL_DEFAULT, 0,
+				   handle, BYPASS_CAPA);
+		if (rc) {
+			mdd_write_unlock(env, son);
+			GOTO(cleanup, rc);
 		}
-
-		/* set its own acl */
-		if (reset_acl) {
-			LASSERTF(acl_buf.lb_buf != NULL && acl_buf.lb_len != 0,
-				 "invalid acl_buf %p:%d\n", acl_buf.lb_buf,
-				 (int)acl_buf.lb_len);
-			rc = mdo_xattr_set(env, son, &acl_buf,
-					   XATTR_NAME_ACL_ACCESS,
-					   0, handle, BYPASS_CAPA);
-			if (rc) {
-				mdd_write_unlock(env, son);
-				GOTO(cleanup, rc);
-			}
+	}
+	/* set its own acl */
+	if (acl_buf.lb_len > 0) {
+		rc = mdo_xattr_set(env, son, &acl_buf,
+				   XATTR_NAME_ACL_ACCESS,
+				   0, handle, BYPASS_CAPA);
+		if (rc) {
+			mdd_write_unlock(env, son);
+			GOTO(cleanup, rc);
 		}
 	}
 #endif

commit f3d03bc38a3afdef83635d578ee0b2ffdd985685

LU-14430 mdd: fix inheritance of big default ACLs
If the number of default ACLs in directory is more than 31, then
mdd_acl_init() fails to inherit them for a newly created file.
This limitation is caused by using a fixed-size def_acl_buf buffer
in the mdd_create()->mdd_acl_init() call chain. Instead, the
default ACL buffer should be increased when it is needed.

Patch adds check for -ERANGE after mdd_acl_init(), reallocates
default ACL buffer with required size and calls mdd_acl_init()
again. Thus big default ACL are processed as expected.

Fixes: 6350af100c20 ("LU-3437 mdd: Fix ACL/def_ACL during object creation")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I700da90c09f824955fcb8dc7ca0bc2f581f916a0
Reviewed-on: https://review.whamcloud.com/41494
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -124,7 +124,7 @@ int mdd_lookup(const struct lu_env *env,
 }
 
 /** Read the link EA into a temp buffer.
- * Uses the mdd_thread_info::mti_big_buf since it is generally large.
+ * Uses the mdd_thread_info::mti_link_buf since it is generally large.
  * A pointer to the buffer is stored in \a ldata::ld_buf.
  *
  * \retval 0 or error
@@ -1254,7 +1254,7 @@ static inline int mdd_links_del(const struct lu_env *env,
 /** Read the link EA into a temp buffer.
  * Uses the name_buf since it is generally large.
  * \retval IS_ERR err
- * \retval ptr to \a lu_buf (always \a mti_big_buf)
+ * \retval ptr to \a lu_buf (always \a mti_link_buf)
  */
 struct lu_buf *mdd_links_get(const struct lu_env *env,
 			     struct mdd_object *mdd_obj)
@@ -2303,6 +2303,7 @@ static int mdd_acl_init(const struct lu_env *env, struct mdd_object *pobj,
 			struct lu_buf *acl_buf)
 {
 	int	rc;
+
 	ENTRY;
 
 	if (S_ISLNK(la->la_mode)) {
@@ -2572,22 +2573,23 @@ int mdd_create(const struct lu_env *env, struct md_object *pobj,
 		      const struct lu_name *lname, struct md_object *child,
 		      struct md_op_spec *spec, struct md_attr *ma)
 {
-	struct mdd_thread_info	*info = mdd_env_info(env);
-	struct lu_attr		*la = &info->mti_la_for_fix;
-	struct mdd_object	*mdd_pobj = md2mdd_obj(pobj);
-	struct mdd_object	*son = md2mdd_obj(child);
-	struct mdd_device	*mdd = mdo2mdd(pobj);
-	struct lu_attr		*attr = &ma->ma_attr;
-	struct thandle		*handle;
-	struct lu_attr		*pattr = &info->mti_pattr;
-	struct lu_buf		acl_buf;
-	struct lu_buf		def_acl_buf;
-	struct lu_buf		hsm_buf;
-	struct linkea_data	*ldata = &info->mti_link_data;
-	const char		*name = lname->ln_name;
+	struct mdd_thread_info *info = mdd_env_info(env);
+	struct lu_attr *la = &info->mti_la_for_fix;
+	struct mdd_object *mdd_pobj = md2mdd_obj(pobj);
+	struct mdd_object *son = md2mdd_obj(child);
+	struct mdd_device *mdd = mdo2mdd(pobj);
+	struct lu_attr *attr = &ma->ma_attr;
+	struct thandle *handle;
+	struct lu_attr *pattr = &info->mti_pattr;
+	struct lu_buf acl_buf;
+	struct lu_buf def_acl_buf;
+	struct lu_buf hsm_buf;
+	struct linkea_data *ldata = &info->mti_link_data;
+	const char *name = lname->ln_name;
 	struct dt_allocation_hint *hint = &mdd_env_info(env)->mti_hint;
-	int			 rc;
-	int			 rc2;
+	int acl_size = LUSTRE_POSIX_ACL_MAX_SIZE_OLD;
+	int rc, rc2;
+
 	ENTRY;
 
 	rc = mdd_la_get(env, mdd_pobj, pattr);
@@ -2606,13 +2608,25 @@ int mdd_create(const struct lu_env *env, struct md_object *pobj,
 	if (IS_ERR(handle))
 		GOTO(out_free, rc = PTR_ERR(handle));
 
-	lu_buf_check_and_alloc(&info->mti_xattr_buf,
-			min_t(unsigned int, mdd->mdd_dt_conf.ddp_max_ea_size,
-			      XATTR_SIZE_MAX));
-	acl_buf = info->mti_xattr_buf;
-	def_acl_buf.lb_buf = info->mti_key;
-	def_acl_buf.lb_len = sizeof(info->mti_key);
+use_bigger_buffer:
+	acl_buf = *lu_buf_check_and_alloc(&info->mti_xattr_buf, acl_size);
+	if (!acl_buf.lb_buf)
+		GOTO(out_stop, rc = -ENOMEM);
+	/* mti_big_buf is also used down below in mdd_changelog_ns_store(),
+	 * but def_acl_buf is finished with it before then
+	 */
+	def_acl_buf = *lu_buf_check_and_alloc(&info->mti_big_buf, acl_size);
+	if (!def_acl_buf.lb_buf)
+		GOTO(out_stop, rc = -ENOMEM);
+
 	rc = mdd_acl_init(env, mdd_pobj, attr, &def_acl_buf, &acl_buf);
+	if (unlikely(rc == -ERANGE &&
+		     acl_size == LUSTRE_POSIX_ACL_MAX_SIZE_OLD)) {
+		/* use maximum-sized xattr buffer for too-big default ACL */
+		acl_size = min_t(unsigned int, mdd->mdd_dt_conf.ddp_max_ea_size,
+				 XATTR_SIZE_MAX);
+		goto use_bigger_buffer;
+	}
 	if (rc < 0)
 		GOTO(out_stop, rc);
 

lustre/tests/sanity.sh
@@ -10501,6 +10501,21 @@ test_103c() {
 }
 run_test 103c "'cp -rp' won't set empty acl"
 
+test_103e() {
+	(( $MDS1_VERSION >= $(version_code 2.13.59) )) ||
+		skip "MDS needs to be at least 2.13.59"
+
+	mkdir -p $DIR/$tdir
+	# one default ACL will be created for the file owner
+	for U in {2..256}; do
+		setfacl -m default:user:$U:rwx $DIR/$tdir
+		numacl=$(getfacl $DIR/$tdir |& grep -c "default:user")
+		touch $DIR/$tdir/$tfile.$U ||
+			error "failed to create $tfile.$U with $numacl ACLs"
+	done
+}
+run_test 103e "inheritance of big amount of default ACLs"
+
 test_104a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 