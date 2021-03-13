lustre/lustre-release

commit e8f74fb0f5c9306ee5a099133799e03e09ca8e47

LU-12275 sec: verify dir is empty when setting enc policy
When setting an encryption policy on a directory that we want to
be encrypted, make sure it is empty.
Otherwise return -ENOTEMPTY from server to client doing the ioctl.

Also add test to exercise the ioctl interface to get/set encryption
policies on directories, thanks to the fscrypt userspace tool.
And test the ability to lock an encrypted directory.

Fixes: 40d91eafe257 ("LU-12275 sec: atomicity of encryption context getting/setting")
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 52 53 54" clientdistro=el8.1 fstype=ldiskfs mdscount=2 mdtcount=4
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 52 53 54" clientdistro=el8.1 fstype=zfs mdscount=2 mdtcount=4
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Iabffd24800314b12908f505d7e66492be7292a16
Reviewed-on: https://review.whamcloud.com/39617
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_internal.h
@@ -620,19 +620,23 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 				int fl, struct thandle *handle)
 {
 	struct dt_object *next = mdd_object_child(obj);
-	int rc;
+	int rc = 0;
 
 	if (!mdd_object_exists(obj))
 		return -ENOENT;
 
-	rc = dt_xattr_set(env, next, buf, name, fl, handle);
-	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+	if ((strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) &&
+	    (!S_ISDIR(mdd_object_type(obj)) ||
+	     (rc = mdd_dir_is_empty(env, obj)) == 0)) {
 		struct lu_attr la = { 0 };
 
 		la.la_valid = LA_FLAGS;
 		la.la_flags = LUSTRE_ENCRYPT_FL;
 		rc = dt_attr_set(env, next, &la, handle);
 	}
+	if (rc >= 0)
+		rc = dt_xattr_set(env, next, buf, name, fl, handle);
+
 	return rc;
 }
 

lustre/tests/sanity-sec.sh
@@ -3967,6 +3967,81 @@ test_53() {
 }
 run_test 53 "Mixed PAGE_SIZE clients"
 
+test_54() {
+	local testdir=$DIR/$tdir/$ID0
+	local testfile=$testdir/$tfile
+	local testfile2=$testdir/${tfile}2
+	local tmpfile=$TMP/${tfile}.tmp
+	local resfile=$TMP/${tfile}.res
+
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	which fscrypt || skip "This test needs fscrypt userspace tool"
+
+	fscrypt setup --force --verbose || error "fscrypt global setup failed"
+	sed -i 's/\(.*\)policy_version\(.*\):\(.*\)\"[0-9]*\"\(.*\)/\1policy_version\2:\3"2"\4/' \
+		/etc/fscrypt.conf
+	fscrypt setup --verbose $MOUNT || error "fscrypt setup $MOUNT failed"
+	mkdir -p $testdir
+	chown -R $ID0:$ID0 $testdir
+
+	echo -e 'mypass\nmypass' | su - $USER0 -c "fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector $testdir" ||
+		error "fscrypt encrypt failed"
+
+	echo -e 'mypass\nmypass' | su - $USER0 -c "fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector2 $testdir" &&
+		error "second fscrypt encrypt should have failed"
+
+	mkdir -p ${testdir}2 || error "mkdir ${testdir}2 failed"
+	touch ${testdir}2/f || error "mkdir ${testdir}2/f failed"
+	cancel_lru_locks
+
+	echo -e 'mypass\nmypass' | fscrypt encrypt --verbose \
+		--source=custom_passphrase --name=protector3 ${testdir}2 &&
+		error "fscrypt encrypt on non-empty dir should have failed"
+
+	$RUNAS dd if=/dev/urandom of=$testfile bs=127 count=1 conv=fsync ||
+		error "write to encrypted file $testfile failed"
+	cp $testfile $tmpfile
+	$RUNAS dd if=/dev/urandom of=$testfile2 bs=127 count=1 conv=fsync ||
+		error "write to encrypted file $testfile2 failed"
+
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (1)"
+
+	$RUNAS hexdump -C $testfile &&
+		error "reading $testfile should have failed without key"
+
+	echo mypass | $RUNAS fscrypt unlock --verbose $testdir ||
+		error "fscrypt unlock $testdir failed (1)"
+
+	$RUNAS cat $testfile > $resfile ||
+		error "reading $testfile failed"
+
+	cmp -bl $tmpfile $resfile || error "file read differs from file written"
+
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (2)"
+
+	$RUNAS hexdump -C $testfile2 &&
+		error "reading $testfile2 should have failed without key"
+
+	echo mypass | $RUNAS fscrypt unlock --verbose $testdir ||
+		error "fscrypt unlock $testdir failed (2)"
+
+	rm -rf $testdir/*
+	$RUNAS fscrypt lock --verbose $testdir ||
+		error "fscrypt lock $testdir failed (3)"
+
+	rm -f $tmpfile $resfile
+}
+run_test 54 "Encryption policies with fscrypt"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {

commit 67c4cffac6dbd30ce30e1d3132b65d4e4a374dda

LU-14401 sec: fix migrate for encrypted dir
When setting an encryption policy on a directory that we want to
be encrypted, we need to make sure it is empty.
But, in some cases, setting the LL_XATTR_NAME_ENCRYPTION_CONTEXT xattr
should be allowed on non-empty directories, for instance when a
directory is migrated across MDTs into new shard directories.
Also, it is required for the encrpytion key to be available on the
client when migrating a directory so that the filenames can be
properly rehashed for the new MDT directory shard.
And, in any case, we need to prevent explicit setting of
LL_XATTR_NAME_ENCRYPTION_CONTEXT xattr outside of encryption policy
definition.

Update sanity-sec test_49 to test migration of non-empty encrypted
directory, and add sanity-sec test_57 to test security.c protection.

Fixes: e8f74fb0f5 ("LU-12275 sec: verify dir is empty when setting enc policy")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I2466ea35a871c6c07bdcf9fba7191485e855e655
Reviewed-on: https://review.whamcloud.com/41413
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/crypto.c
@@ -105,6 +105,7 @@ static int ll_set_context(struct inode *inode, const void *ctx, size_t len,
 		return -EPERM;
 
 	dentry = (struct dentry *)fs_data;
+	ll_file_set_flag(ll_i2info(inode), LLIF_SET_ENC_CTX);
 	rc = ll_vfs_setxattr(dentry, inode, LL_XATTR_NAME_ENCRYPTION_CONTEXT,
 			     ctx, len, XATTR_CREATE);
 	if (rc)

lustre/llite/file.c
@@ -4558,6 +4558,17 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	if (is_root_inode(child_inode))
 		GOTO(out_iput, rc = -EINVAL);
 
+	if (IS_ENCRYPTED(child_inode)) {
+		rc = llcrypt_get_encryption_info(child_inode);
+		if (rc)
+			GOTO(out_iput, rc);
+		if (!llcrypt_has_encryption_key(child_inode)) {
+			CDEBUG(D_SEC, "no enc key for "DFID"\n",
+			       PFID(ll_inode2fid(child_inode)));
+			GOTO(out_iput, rc = -ENOKEY);
+		}
+	}
+
 	op_data = ll_prep_md_op_data(NULL, parent, NULL, name, namelen,
 				     child_inode->i_mode, LUSTRE_OPC_ANY, NULL);
 	if (IS_ERR(op_data))

lustre/llite/llite_internal.h
@@ -406,6 +406,8 @@ enum ll_file_flags {
 	LLIF_UPDATE_ATIME	= 4,
 	/* foreign file/dir can be unlinked unconditionnaly */
 	LLIF_FOREIGN_REMOVABLE	= 5,
+	/* setting encryption context in progress */
+	LLIF_SET_ENC_CTX	= 6,
 
 };
 

lustre/llite/xattr.c
@@ -148,6 +148,17 @@ static int ll_xattr_set_common(const struct xattr_handler *handler,
 			RETURN(-EPERM);
 	}
 
+	/* Setting LL_XATTR_NAME_ENCRYPTION_CONTEXT xattr is only allowed
+	 * when defining an encryption policy on a directory, ie when it
+	 * comes from ll_set_context().
+	 * When new files/dirs are created in an encrypted dir, the xattr
+	 * is set directly in the create request.
+	 */
+	if (handler->flags == XATTR_SECURITY_T &&
+	    !strcmp(name, "c") &&
+	    !ll_file_test_and_clear_flag(ll_i2info(inode), LLIF_SET_ENC_CTX))
+		RETURN(-EPERM);
+
 	fullname = kasprintf(GFP_KERNEL, "%s%s", xattr_prefix(handler), name);
 	if (!fullname)
 		RETURN(-ENOMEM);

lustre/mdd/mdd_internal.h
@@ -630,8 +630,19 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 	if (!mdd_object_exists(obj))
 		return -ENOENT;
 
+	/* If we are about to set the LL_XATTR_NAME_ENCRYPTION_CONTEXT
+	 * xattr, it means the file/dir is encrypted. In that case we want
+	 * to set the LUSTRE_ENCRYPT_FL flag as well: it will be stored
+	 * into the LMA, making it more efficient to recognise we are
+	 * dealing with an encrypted file/dir, as LMA info is cached upon
+	 * object init.
+	 * However, marking a dir as encrypted is only possible if it is
+	 * being created or migrated (LU_XATTR_CREATE flag not set), or
+	 * if it is empty.
+	 */
 	if ((strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) &&
 	    (!S_ISDIR(mdd_object_type(obj)) ||
+	     !(fl & LU_XATTR_CREATE) ||
 	     (rc = mdd_dir_is_empty(env, obj)) == 0)) {
 		struct lu_attr la = { 0 };
 

lustre/tests/sanity-sec.sh
@@ -3575,12 +3575,13 @@ trace_cmd() {
 	local cmd="$@"
 	local xattr_name="security.c"
 
-	cancel_lru_locks osc ; cancel_lru_locks mdc
+	cancel_lru_locks
 	$LCTL set_param debug=+info
 	$LCTL clear
 
 	echo $cmd
 	eval $cmd
+	[ $? -eq 0 ] || error "$cmd failed"
 
 	$LCTL dk | grep -E "get xattr '${xattr_name}'|get xattrs"
 	[ $? -ne 0 ] || error "get xattr event was triggered"
@@ -3608,21 +3609,29 @@ test_49() {
 	trace_cmd $TRUNCATE $dirname/f1 10240
 	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
 	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
-	trace_cmd $LFS setdirstripe -i 1 $dirname/d2
-	trace_cmd $LFS migrate -m 0 $dirname/d2
 
-	$LFS setdirstripe -i 1 -c 1 $dirname/d3
-	dirname=$dirname/d3/subdir
-	mkdir $dirname
+	if [[ $MDSCOUNT -gt 1 ]]; then
+		trace_cmd $LFS setdirstripe -i 1 $dirname/d2
+		trace_cmd $LFS migrate -m 0 $dirname/d2
+		touch $dirname/d2/subf
+		# migrate a non-empty encrypted dir
+		trace_cmd $LFS migrate -m 1 $dirname/d2
 
-	trace_cmd stat $dirname
-	trace_cmd touch $dirname/f1
-	trace_cmd stat $dirname/f1
-	trace_cmd cat $dirname/f1
-	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
-	trace_cmd $TRUNCATE $dirname/f1 10240
-	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
-	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+		$LFS setdirstripe -i 1 -c 1 $dirname/d3
+		dirname=$dirname/d3/subdir
+		mkdir $dirname
+
+		trace_cmd stat $dirname
+		trace_cmd touch $dirname/f1
+		trace_cmd stat $dirname/f1
+		trace_cmd cat $dirname/f1
+		dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+		trace_cmd $TRUNCATE $dirname/f1 10240
+		trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+		trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+	else
+		skip_noexit "2nd part needs >= 2 MDTs"
+	fi
 }
 run_test 49 "Avoid getxattr for encryption context"
 
@@ -4169,6 +4178,41 @@ test_56() {
 		error "filefrag $testfile does not show encoded flag"
 }
 run_test 56 "FIEMAP on encrypted file"
+
+test_57() {
+	local testdir=$DIR/$tdir/mytestdir
+	local testfile=$DIR/$tdir/$tfile
+
+	[[ $(facet_fstype ost1) == zfs ]] && skip "skip ZFS backend"
+
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	mkdir $DIR/$tdir
+	mkdir $testdir
+	setfattr -n security.c -v myval $testdir &&
+		error "setting xattr on $testdir should have failed (1)"
+	touch $testfile
+	setfattr -n security.c -v myval $testfile &&
+		error "setting xattr on $testfile should have failed (1)"
+
+	rm -rf $DIR/$tdir
+
+	stack_trap cleanup_for_enc_tests EXIT
+	setup_for_enc_tests
+
+	mkdir $testdir
+	setfattr -n security.c -v myval $testdir &&
+		error "setting xattr on $testdir should have failed (2)"
+	touch $testfile
+	setfattr -n security.c -v myval $testfile &&
+		error "setting xattr on $testfile should have failed (2)"
+	return 0
+}
+run_test 57 "security.c xattr protection"
 
 log "cleanup: ======================================================"
 