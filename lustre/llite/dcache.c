lustre/lustre-release

commit 15d44e787e17ff57fc1fb5a6c9ae568cdaab6e34

LU-12682 llite: fake symlink type of foreign file/dir
This patch implements a "fake symlink" specific usage of
"foreign" LOV/LMV format. It basically allows these
particular type of foreign files/dirs to behave as a
symlink from VFS point of view, by allowing to construct
a relative path from the LOV/LMV foreign content, to
complement it with a prefix, and then to expose it to
the VFS as a symlink destination. The default/internal
mechanism simply takes the full foreign free string as
the relative path, and for more complex internal formats
an upcall has been implemented to provide format's
details (presently just in terms of constant strings
and substrings positions in EA, but this can be enhanced)
to llite layer.
Using this feature, instead of real symlinks or user EA,
will permit to benefit from the special features (lock,
prefetch, caches) already implemented to handle both
LOV/LMV EAs.

Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: Id3c262e3b042447aa09aad25f682ff02787b350d
Reviewed-on: https://review.whamcloud.com/35856
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Ben Evans <beevans@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/dcache.c
@@ -294,12 +294,22 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 		return 1;
 
 	/* Symlink - always valid as long as the dentry was found */
+	/* only special case is to prevent ELOOP error from VFS during open
+	 * of a foreign symlink file/dir with O_NOFOLLOW, like it happens for
+	 * real symlinks. This will allow to open foreign symlink file/dir
+	 * for get[dir]stripe/unlock ioctl()s.
+	 */
 #ifdef HAVE_IOP_GET_LINK
-	if (dentry->d_inode && dentry->d_inode->i_op->get_link)
+	if (dentry->d_inode && dentry->d_inode->i_op->get_link) {
 #else
-	if (dentry->d_inode && dentry->d_inode->i_op->follow_link)
+	if (dentry->d_inode && dentry->d_inode->i_op->follow_link) {
 #endif
-		return 1;
+		if (!S_ISLNK(dentry->d_inode->i_mode) &&
+		    !(lookup_flags & LOOKUP_FOLLOW))
+			return 0;
+		else
+			return 1;
+	}
 
 	/*
 	 * VFS warns us that this is the second go around and previous

lustre/doc/Makefile.am
@@ -178,7 +178,8 @@ LIBMAN = 					\
 	llapi_rmfid.3				\
 	llapi_search_mdt.3			\
 	llapi_search_ost.3			\
-	llapi_search_tgt.3
+	llapi_search_tgt.3			\
+	llapi_unlink_foreign.3
 
 SERVER_MANFILES =				\
 	lctl-barrier.8				\

lustre/doc/lfs-find.1
@@ -119,7 +119,7 @@ File has a foreign (non-Lustre/free format) layout and is of the given
 if specified.  Presently only
 .B none
 or
-.B daos
+.B symlink
 are defined types, though 32-bit numeric types can also be used.
 .TP
 .BR --gid | -g
@@ -365,9 +365,9 @@ Recursively list all mirrored files that have more than 2 mirrors.
 .B $ lfs find ! --mirror-state=ro /mnt/lustre
 Recursively list all out-of-sync mirrored files.
 .TP
-.B $ lfs find ! --foreign=daos /mnt/lustre
+.B $ lfs find ! --foreign=symlink /mnt/lustre
 Recursively list all but foreign files/dirs of
-.B daos
+.B symlink
 type.
 .SH BUGS
 The

lustre/doc/lfs-setdirstripe.1
@@ -94,9 +94,10 @@ Create a directory with a foreign (non-Lustre/free format, see
 option) striping. Where
 .BR type
 specifies a known foreign type (like
-.BR none ,
-.BR daos )
-or a 32-bit numeric type.
+.BR none
+,
+.BR symlink
+, ...) or a 32-bit numeric type.
 .TP
 .BR \-\-flags =\fI<hex>\fR
 Specify a numeric bitmask of type-specific layout flags for the foreign layout.
@@ -149,12 +150,12 @@ This creates a directory striped on two MDTs, whose first stripe is on
 (MDT index 1), and whose hash type is
 .BR all_char .
 .TP
-.B $ lfs mkdir --foreign=daos --xattr PUUID:CUUID /mnt/lustre/dir1
+.B $ lfs mkdir --foreign=symlink --xattr PUUID/CUUID /mnt/lustre/dir1
 This creates
 .B dir1
 with foreign (non-lustre/free format)
-.B PUUID:CUUID
-striping/LMV EA.
+.B PUUID/CUUID
+striping/LMV EA value (symlink type).
 .SH AVAILABILITY
 The
 .B lfs setdirstripe

lustre/doc/lfs-setstripe.1
@@ -133,7 +133,7 @@ file.
 Create a new
 .I file
 with a foreign/non-lustre layout of type
-.I foreign_type \fR(\fBnone\fR, \fBdaos\fR, ...)
+.I foreign_type \fR(\fBnone\fR, \fBsymlink\fR, ...)
 with flags
 .I hex
 and a free-format layout value of
@@ -264,7 +264,7 @@ options).
 Current known types are
 .BR none
 and
-.BR daos
+.BR symlink
 , default is
 .BR none
 .
@@ -533,12 +533,12 @@ which can be created with the
 .B lfs getstripe --yaml
 command.
 .TP
-.B lfs setstripe --foreign=daos --flags=0xda08 \
+.B lfs setstripe --foreign=symlink --flags=0xda08 \
 	--xattr=PUUID:CUUID /mnt/lustre/file1
 This creates foreign
 .BR file1
 of type
-.BR daos
+.BR symlink
 with non-lustre/free-format
 .BR PUUID:CUUID
 layout/LOV EA and flags

lustre/doc/lfs.1
@@ -127,6 +127,9 @@ lfs \- client utility for Lustre-specific file layout and other attributes
 .br
 .B lfs setstripe --yaml=\fR<\fIyaml_template_file\fR> <\fIfilename\fR>
 .br
+.B lfs unlink_foreign
+.IR path \fR[\fIpath\fR ...]
+.br
 .B lfs --version
 .br
 .B lfs --list-commands
@@ -219,6 +222,9 @@ See lfs-migrate(1).
 .TP
 .B setstripe
 See lfs-setstripe(1).
+.TP
+.B unlink_foreign
+Remove the foreign files/dirs that are prevented to be using regular unlink/rmdir commands/syscalls. Works also for regular files/dirs.
 .TP
 .B --version
 Output the build version of the lfs utility. Use "lctl lustre_build_version" to get the version of the Lustre kernel modules

lustre/doc/llapi_file_create_foreign.3
@@ -73,13 +73,13 @@ int main(int argc, char *argv[])
         if (argc != 2)
                 return -1;
 
-        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_DAOS,
-				       0xda08, "PUUID@CUUID");
+        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_SYMLINK,
+				       0xda05, "PUUID/CUUID");
         if (rc < 0) {
                 fprintf(stderr, "file creation has failed, %s\\n", strerror(-rc));
                 return -1;
         }
-        printf("foreign file %s has been created with 'PUUID@CUUID' layout!\\n",
+        printf("foreign file %s has been created with 'PUUID/CUUID' layout!\\n",
                argv[1]);
         return 0;
 }

lustre/doc/llapi_unlink_foreign.3
@@ -1,0 +1,80 @@
+.TH lustreapi 3 "2009 Jul 10" The Lustre user application interface library
+.SH NAME
+llapi_unlink_foreign \- unlink file/dir with foreign layout on a Lustre filesystem
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_unlink_foreign(char *" name ");"
+.sp
+.fi
+.SH DESCRIPTION
+.LP
+.B llapi_unlink_foreign(\|)
+will allow to unlink a file/dir of
+.I name
+with foreign LOV/LMV, that would be prevented to be using regular unlink/rmdir
+command/syscall.
+It also works for regular file/dir.
+.SH RETURN VALUES
+.LP
+.B llapi_unlink_foreign(\|)
+return:
+.TP
+=0
+on success.
+.TP
+<0
+on failure, the absolute value is an error code.
+.SH ERRORS
+.TP 15
+.SM ENOENT
+.I name
+does not exist.
+.TP
+.SM ENOTTY
+.I name
+may not point to a Lustre filesystem.
+.SH "EXAMPLE"
+.nf
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <lustre/lustreapi.h>
+
+int main(int argc, char **argv)
+{
+	char *foreign = NULL;
+	int c, rc;
+
+	while ((c = getopt(argc, argv, "hf:")) != -1) {
+		switch (c) {
+		case 'f':
+			foreign = optarg;
+			break;
+		case 'h':
+		default:
+			fprintf(stderr,
+				"Usage: %s [-f <foreign file/dir pathname>]\n",
+				argv[0]);
+			exit(0);
+			break;
+		}
+	}
+
+	if (foreign == NULL) {
+		fprintf(stderr, "a foreign file/dir pathname must be provided\n");
+		exit(0);
+	}
+
+	rc = llapi_unlink_foreign(foreign);
+	if (rc < 0)
+		fprintf(stderr, "llapi_unlink_foreign() error: %d\n", rc);
+
+	return rc;
+}
+.fi
+.SH "SEE ALSO"
+.BR lustre (7),
+.BR lustreapi (7)

commit 36b1e4c4142f8a7251aa3b6b1ec9ecce4799114f

LU-6142 llite: use d_is_symlink to test if dentry is a symlink
Using d_is_symlink() is preferred to testing ->get_link or
->follow_link.

A recent patch made this work for foreign files/dirs by making sure
the entry type in d_flags is correct, so we can simplify the code in
ll_revalidate_dentry().

Fixes: 15d44e787e17 ("LU-12682 llite: fake symlink type of foreign file/dir")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Ie4c33ae1fb9a660ccbd50e2c70b6cde65cc9b990
Reviewed-on: https://review.whamcloud.com/41770
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/dcache.c
@@ -295,11 +295,7 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 	 * real symlinks. This will allow to open foreign symlink file/dir
 	 * for get[dir]stripe/unlock ioctl()s.
 	 */
-#ifdef HAVE_IOP_GET_LINK
-	if (dentry->d_inode && dentry->d_inode->i_op->get_link) {
-#else
-	if (dentry->d_inode && dentry->d_inode->i_op->follow_link) {
-#endif
+	if (d_is_symlink(dentry)) {
 		if (!S_ISLNK(dentry->d_inode->i_mode) &&
 		    !(lookup_flags & LOOKUP_FOLLOW))
 			return 0;