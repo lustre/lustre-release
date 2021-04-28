lustre/lustre-release

commit d7249d9d70ac0fcfa665ece78634b495bc9a22cd

LU-13783 libcfs: provide fallback kallsyms_lookup_name()
Since Linux 5.7, kallsyms_lookup_name() is no longer exported, so we
cannot rely on it.

So test for this, and when not available provide a fallback which just
returns NULL.

As this was the only way to access apply_workqueue_attrs() in recent
kernels, we need to cope with the absence of that function.

Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I09cc00047ec163a9395c5acd415505a8586e4e99
Reviewed-on: https://review.whamcloud.com/40826
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

libcfs/include/libcfs/linux/linux-misc.h
@@ -146,4 +146,11 @@ void cfs_arch_init(void);
 #define sizeof_field(type, member)	FIELD_SIZEOF(type, member)
 #endif
 
+#ifndef HAVE_KALLSYMS_LOOKUP_NAME
+static inline void *kallsyms_lookup_name(char *func)
+{
+	return NULL;
+}
+#endif
+
 #endif /* __LIBCFS_LINUX_MISC_H__ */

libcfs/libcfs/linux/linux-prim.c
@@ -109,7 +109,9 @@ static int (*cfs_apply_workqueue_attrs_t)(struct workqueue_struct *wq,
 int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
 			      const struct workqueue_attrs *attrs)
 {
-	return cfs_apply_workqueue_attrs_t(wq, attrs);
+	if (cfs_apply_workqueue_attrs_t)
+		return cfs_apply_workqueue_attrs_t(wq, attrs);
+	return 0;
 }
 EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
 

libcfs/autoconf/lustre-libcfs.m4
@@ -1290,6 +1290,17 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_CACHE_DETAIL_WRITERS
 
 #
+# LIBCFS_KALLSYMS_LOOKUP
+#
+# kernel v5.6-11591-g0bd476e6c671
+# kallsyms: unexport kallsyms_lookup_name() and kallsyms_on_each_symbol()
+AC_DEFUN([LIBCFS_KALLSYMS_LOOKUP], [
+LB_CHECK_EXPORT([kallsyms_lookup_name], [kernel/kallsyms.c],
+	[AC_DEFINE(HAVE_KALLSYMS_LOOKUP_NAME, 1,
+		[kallsyms_lookup_name is exported by kernel])])
+]) # LIBCFS_KALLSYMS_LOOKUP
+
+#
 # LIBCFS_VMALLOC_2ARGS
 #
 # kernel v5.8-rc1~201^2~19
@@ -1475,6 +1486,8 @@ LIBCFS_LOOKUP_USER_KEY
 LIBCFS_FORCE_SIG_WITH_TASK
 LIBCFS_CACHE_DETAIL_WRITERS
 LIBCFS_HAVE_NR_UNSTABLE_NFS
+# 5.7
+LIBCFS_KALLSYMS_LOOKUP
 # 5.8
 LIBCFS_HAVE_MMAP_LOCK
 LIBCFS_KERNEL_SETSOCKOPT

commit 783002035ae9612b5b0aa80f2342a2ee9e81c374

LU-13783 libcfs: add cfs_kallsyms_lookup_name()
The inline kallsyms_lookup_name() added by
commit d7249d9d70a caused the following failures:

libcfs/include/libcfs/linux/linux-misc.h:150:21:
error: conflicting types for ‘kallsyms_lookup_name’
  150 | static inline void *kallsyms_lookup_name(char *func)
      |                     ^~~~~~~~~~~~~~~~~~~~

include/linux/kallsyms.h:76:15:
note: previous declaration of ‘kallsyms_lookup_name’ was here
   76 | unsigned long kallsyms_lookup_name(const char *name);
      |               ^~~~~~~~~~~~~~~~~~~~

This patch removes the inline kallsyms_lookup_name() definition
from linux-misc.h and adds cfs_kallsyms_lookup_name() to wrap
kallsyms_lookup_name() if it is exported or return NULL in case of
kallsyms_lookup_name() is not exported.

Fixes: d7249d9d70a ("LU-13783 libcfs: provide fallback kallsyms_lookup_name()")
Change-Id: I4b2d4499948a8586b48db68484491ec76c3a609d
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/43296
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/include/libcfs/linux/linux-misc.h
@@ -40,6 +40,7 @@
 #include <linux/mutex.h>
 #include <linux/user_namespace.h>
 #include <linux/uio.h>
+#include <linux/kallsyms.h>
 
 #ifndef HAVE_IOV_ITER_TYPE
 #ifdef HAVE_IOV_ITER_HAS_TYPE_MEMBER
@@ -145,8 +146,13 @@ void cfs_arch_init(void);
 #define sizeof_field(type, member)	FIELD_SIZEOF(type, member)
 #endif
 
-#ifndef HAVE_KALLSYMS_LOOKUP_NAME
-static inline void *kallsyms_lookup_name(char *func)
+#ifdef HAVE_KALLSYMS_LOOKUP_NAME
+static inline void *cfs_kallsyms_lookup_name(const char *name)
+{
+	return (void *)kallsyms_lookup_name(name);
+}
+#else
+static inline void *cfs_kallsyms_lookup_name(const char *name)
 {
 	return NULL;
 }

libcfs/libcfs/debug.c
@@ -515,7 +515,7 @@ void __init cfs_debug_init(void)
 {
 #ifdef CONFIG_ARCH_STACKWALK
 	task_dump_stack = (void *)
-			kallsyms_lookup_name("stack_trace_save_tsk");
+			cfs_kallsyms_lookup_name("stack_trace_save_tsk");
 
 #endif
 }

libcfs/libcfs/linux/linux-prim.c
@@ -31,7 +31,6 @@
 
 #define DEBUG_SUBSYSTEM S_LNET
 
-#include <linux/kallsyms.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
@@ -47,6 +46,7 @@
 
 #include <libcfs/linux/linux-time.h>
 #include <libcfs/linux/linux-wait.h>
+#include <libcfs/linux/linux-misc.h>
 
 #ifndef HAVE_KTIME_GET_TS64
 void ktime_get_ts64(struct timespec64 *ts)
@@ -124,10 +124,10 @@ void __init cfs_arch_init(void)
 	wait_bit_init();
 #endif
 	cfs_apply_workqueue_attrs_t =
-		(void *)kallsyms_lookup_name("apply_workqueue_attrs");
+		(void *)cfs_kallsyms_lookup_name("apply_workqueue_attrs");
 #ifndef HAVE_XARRAY_SUPPORT
 	radix_tree_node_cachep =
-		(void *)kallsyms_lookup_name("radix_tree_node_cachep");
+		(void *)cfs_kallsyms_lookup_name("radix_tree_node_cachep");
 #endif
 }
 

lustre/llite/vvp_dev.c
@@ -291,8 +291,7 @@ int vvp_global_init(void)
 	 * Kernel v5.2-5678-gac1c3e4 no longer exports account_page_dirtied
 	 */
 	vvp_account_page_dirtied = (void *)
-		kallsyms_lookup_name("account_page_dirtied");
-	BUG_ON(!vvp_account_page_dirtied);
+		cfs_kallsyms_lookup_name("account_page_dirtied");
 #endif
 
 	return 0;

lustre/llite/vvp_io.c
@@ -959,7 +959,8 @@ static inline void ll_account_page_dirtied(struct page *page,
 #elif defined(HAVE_ACCOUNT_PAGE_DIRTIED_EXPORT)
 	account_page_dirtied(page, mapping);
 #else
-	vvp_account_page_dirtied(page, mapping);
+	if (vvp_account_page_dirtied)
+		vvp_account_page_dirtied(page, mapping);
 #endif
 }
 

lustre/osd-ldiskfs/osd_handler.c
@@ -8406,7 +8406,7 @@ static int __init osd_init(void)
 
 #ifdef CONFIG_KALLSYMS
 	priv_security_file_alloc =
-		(void *)kallsyms_lookup_name("security_file_alloc");
+		(void *)cfs_kallsyms_lookup_name("security_file_alloc");
 #endif
 
 	rc = class_register_type(&osd_obd_device_ops, NULL, true,