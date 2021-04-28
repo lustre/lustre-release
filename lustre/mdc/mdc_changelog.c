lustre/lustre-release

commit 66172e3274ca3187edd519dd87a81a478a6d7259

LU-13238 ofd: add OFD access logs
Add access logs to OFD layer. BRW RPC handlers will record accesss to
an in memory circular buffer which may be read in userspace through
character devices (/dev/lustre-access-log/$FSNAME-OSTxxxx). A control
device (/dev/lustre-access-log/control) is added to facilitate device
discovery. A utility (ofd_access_log_reader) to discover and consume
access logs is included.

Signed-off-by: John L. Hammond <jhammond@whamcloud.com>
Change-Id: I76b78cc5075ee01f9b234e96e7a22a1bdcf2f755
Reviewed-on: https://review.whamcloud.com/37552
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Gu Zheng <gzheng@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_access_log.c
@@ -1,0 +1,625 @@
+#include <linux/cdev.h>
+#include <linux/circ_buf.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+#include <uapi/linux/lustre/lustre_idl.h>
+#include <uapi/linux/lustre/lustre_access_log.h>
+#include "ofd_internal.h"
+
+/* OFD access logs: OST (OFD) RPC handlers log accesses by FID and
+ * PFID which are read from userspace through character device files
+ * (/dev/lustre-access-log/scratch-OST0000). Accesses are described by
+ * struct ofd_access_entry_v1. The char device implements read()
+ * (blocking and nonblocking) and poll(), along with an ioctl that
+ * returns diagnostic information on an oal device.
+ *
+ * A control device (/dev/lustre-access-log/control) supports an ioctl()
+ * plus poll() method to for oal discovery. See uses of
+ * oal_control_event_count and oal_control_wait_queue for details.
+ *
+ * oal log size and entry size are restricted to powers of 2 to
+ * support circ_buf methods. See Documentation/core-api/circular-buffers.rst
+ * in the linux tree for more information.
+ *
+ * The associated struct device (*oal_device) owns the oal. The
+ * release() method of oal_device frees the oal and releases its
+ * minor. This may seem slightly more complicated than necessary but
+ * it allows the OST to be unmounted while the oal still has open file
+ * descriptors.
+ */
+
+enum {
+	OAL_DEV_COUNT = 1 << MINORBITS,
+};
+
+struct ofd_access_log {
+	char oal_name[128]; /* lustre-OST0000 */
+	struct device oal_device;
+	struct cdev oal_cdev;
+	struct circ_buf oal_circ;
+	wait_queue_head_t oal_read_wait_queue;
+	spinlock_t oal_read_lock;
+	spinlock_t oal_write_lock;
+	unsigned int oal_drop_count;
+	unsigned int oal_is_closed;
+	unsigned int oal_log_size;
+	unsigned int oal_entry_size;
+};
+
+static atomic_t oal_control_event_count = ATOMIC_INIT(0);
+static DECLARE_WAIT_QUEUE_HEAD(oal_control_wait_queue);
+
+static struct class *oal_log_class;
+static unsigned int oal_log_major;
+static DEFINE_IDR(oal_log_minor_idr); /* TODO Use ida instead. */
+static DEFINE_SPINLOCK(oal_log_minor_lock);
+
+bool ofd_access_log_size_is_valid(unsigned int size)
+{
+	const unsigned int size_min = 2 * sizeof(struct ofd_access_entry_v1);
+	const unsigned int size_max = 1U << 30;
+
+	if (size == 0)
+		return true;
+
+	return is_power_of_2(size) && size_min <= size && size <= size_max;
+}
+
+static void oal_control_event_inc(void)
+{
+	atomic_inc(&oal_control_event_count);
+	wake_up(&oal_control_wait_queue);
+}
+
+static int oal_log_minor_alloc(int *pminor)
+{
+	void *OAL_LOG_MINOR_ALLOCED = (void *)-1;
+	int minor;
+
+	idr_preload(GFP_KERNEL);
+	spin_lock(&oal_log_minor_lock);
+	minor = idr_alloc(&oal_log_minor_idr, OAL_LOG_MINOR_ALLOCED, 0,
+			OAL_DEV_COUNT, GFP_NOWAIT);
+	spin_unlock(&oal_log_minor_lock);
+	idr_preload_end();
+
+	if (minor < 0)
+		return minor;
+
+	*pminor = minor;
+
+	return 0;
+}
+
+static void oal_log_minor_free(int minor)
+{
+	spin_lock(&oal_log_minor_lock);
+	idr_remove(&oal_log_minor_idr, minor);
+	spin_unlock(&oal_log_minor_lock);
+}
+
+static bool oal_is_empty(struct ofd_access_log *oal)
+{
+	return CIRC_CNT(oal->oal_circ.head,
+			oal->oal_circ.tail,
+			oal->oal_log_size) < oal->oal_entry_size;
+}
+
+static ssize_t oal_write_entry(struct ofd_access_log *oal,
+			const void *entry, size_t entry_size)
+{
+	struct circ_buf *circ = &oal->oal_circ;
+	unsigned int head;
+	unsigned int tail;
+	ssize_t rc;
+
+	if (entry_size != oal->oal_entry_size)
+		return -EINVAL;
+
+	spin_lock(&oal->oal_write_lock);
+	head = circ->head;
+	tail = READ_ONCE(circ->tail);
+
+	/* CIRC_SPACE() return space available, 0..oal_log_size -
+	 * 1. It always leaves one free char, since a completely full
+	 * buffer would have head == tail, which is the same as empty. */
+	if (CIRC_SPACE(head, tail, oal->oal_log_size) < oal->oal_entry_size) {
+		oal->oal_drop_count++;
+		rc = -EAGAIN;
+		goto out_write_lock;
+	}
+
+	memcpy(&circ->buf[head], entry, entry_size);
+	rc = entry_size;
+
+	/* Ensure the entry is stored before we update the head. */
+	smp_store_release(&circ->head,
+			(head + oal->oal_entry_size) & (oal->oal_log_size - 1));
+
+	wake_up(&oal->oal_read_wait_queue);
+out_write_lock:
+	spin_unlock(&oal->oal_write_lock);
+
+	return rc;
+}
+
+/* Read one entry from the log and return its size. Non-blocking.
+ * When the log is empty we return -EAGAIN if the OST is still mounted
+ * and 0 otherwise.
+ */
+static ssize_t oal_read_entry(struct ofd_access_log *oal,
+			void *entry_buf, size_t entry_buf_size)
+{
+	struct circ_buf *circ = &oal->oal_circ;
+	unsigned int head;
+	unsigned int tail;
+	ssize_t rc;
+
+	/* XXX This method may silently truncate entries when
+	 * entry_buf_size is less than oal_entry_size. But that's OK
+	 * because you know what you are doing. */
+	spin_lock(&oal->oal_read_lock);
+
+	/* Memory barrier usage follows circular-buffers.txt. */
+	head = smp_load_acquire(&circ->head);
+	tail = circ->tail;
+
+	if (!CIRC_CNT(head, tail, oal->oal_log_size)) {
+		rc = oal->oal_is_closed ? 0 : -EAGAIN;
+		goto out_read_lock;
+	}
+
+	BUG_ON(CIRC_CNT(head, tail, oal->oal_log_size) < oal->oal_entry_size);
+
+	/* Read index before reading contents at that index. */
+	smp_read_barrier_depends();
+
+	/* Extract one entry from the buffer. */
+	rc = min_t(size_t, oal->oal_entry_size, entry_buf_size);
+	memcpy(entry_buf, &circ->buf[tail], rc);
+
+	/* Memory barrier usage follows circular-buffers.txt. */
+	smp_store_release(&circ->tail,
+			(tail + oal->oal_entry_size) & (oal->oal_log_size - 1));
+
+out_read_lock:
+	spin_unlock(&oal->oal_read_lock);
+
+	return rc;
+}
+
+static int oal_file_open(struct inode *inode, struct file *filp)
+{
+	filp->private_data = container_of(inode->i_cdev,
+					struct ofd_access_log, oal_cdev);
+
+	return nonseekable_open(inode, filp);
+}
+
+/* User buffer size must be a multiple of ofd access entry size. */
+static ssize_t oal_file_read(struct file *filp, char __user *buf, size_t count,
+			loff_t *ppos)
+{
+	struct ofd_access_log *oal = filp->private_data;
+	void *entry;
+	size_t size = 0;
+	int rc = 0;
+
+	if (!count)
+		return 0;
+
+	if (count & (oal->oal_entry_size - 1))
+		return -EINVAL;
+
+	entry = kzalloc(oal->oal_entry_size, GFP_KERNEL);
+	if (!entry)
+		return -ENOMEM;
+
+	while (size < count) {
+		rc = oal_read_entry(oal, entry, oal->oal_entry_size);
+		if (rc == -EAGAIN) {
+			if (filp->f_flags & O_NONBLOCK)
+				break;
+
+			rc = wait_event_interruptible(oal->oal_read_wait_queue,
+				!oal_is_empty(oal) || oal->oal_is_closed);
+			if (rc)
+				break;
+		} else if (rc <= 0) {
+			break; /* cloed or error */
+		} else {
+			if (copy_to_user(buf, entry, oal->oal_entry_size)) {
+				rc = -EFAULT;
+				break;
+			}
+
+			buf += oal->oal_entry_size;
+			size += oal->oal_entry_size;
+		}
+	}
+
+	kfree(entry);
+
+	return size ? size : rc;
+}
+
+/* Included for test purposes. User buffer size must be a multiple of
+ * ofd access entry size. */
+static ssize_t oal_file_write(struct file *filp, const char __user *buf,
+			size_t count, loff_t *ppos)
+{
+	struct ofd_access_log *oal = filp->private_data;
+	void *entry;
+	size_t size = 0;
+	ssize_t rc = 0;
+
+	if (!count)
+		return 0;
+
+	if (count & (oal->oal_entry_size - 1))
+		return -EINVAL;
+
+	entry = kzalloc(oal->oal_entry_size, GFP_KERNEL);
+	if (!entry)
+		return -ENOMEM;
+
+	while (size < count) {
+		if (copy_from_user(entry, buf, oal->oal_entry_size)) {
+			rc = -EFAULT;
+			break;
+		}
+
+		rc = oal_write_entry(oal, entry, oal->oal_entry_size);
+		if (rc <= 0)
+			break;
+
+		buf += oal->oal_entry_size;
+		size += oal->oal_entry_size;
+	}
+
+	kfree(entry);
+
+	return size > 0 ? size : rc;
+}
+
+unsigned int oal_file_poll(struct file *filp, struct poll_table_struct *wait)
+{
+	struct ofd_access_log *oal = filp->private_data;
+	unsigned int mask = 0;
+
+	poll_wait(filp, &oal->oal_read_wait_queue, wait);
+
+	spin_lock(&oal->oal_read_lock);
+
+	if (!oal_is_empty(oal) || oal->oal_is_closed)
+		mask |= POLLIN;
+
+	spin_unlock(&oal->oal_read_lock);
+
+	return mask;
+}
+
+static long oal_ioctl_info(struct ofd_access_log *oal, unsigned long arg)
+{
+	struct lustre_access_log_info_v1 __user *lali;
+	u32 entry_count = CIRC_CNT(oal->oal_circ.head,
+				oal->oal_circ.tail,
+				oal->oal_log_size) / oal->oal_entry_size;
+	u32 entry_space = CIRC_SPACE(oal->oal_circ.head,
+				oal->oal_circ.tail,
+				oal->oal_log_size) / oal->oal_entry_size;
+
+	lali = (struct lustre_access_log_info_v1 __user *)arg;
+	BUILD_BUG_ON(sizeof(lali->lali_name) != sizeof(oal->oal_name));
+
+	if (put_user(LUSTRE_ACCESS_LOG_VERSION_1, &lali->lali_version))
+		return -EFAULT;
+
+	if (put_user(LUSTRE_ACCESS_LOG_TYPE_OFD, &lali->lali_type))
+		return -EFAULT;
+
+	if (copy_to_user(lali->lali_name, oal->oal_name, sizeof(oal->oal_name)))
+		return -EFAULT;
+
+	if (put_user(oal->oal_log_size, &lali->lali_log_size))
+		return -EFAULT;
+
+	if (put_user(oal->oal_entry_size, &lali->lali_entry_size))
+		return -EFAULT;
+
+	if (put_user(oal->oal_circ.head, &lali->_lali_head))
+		return -EFAULT;
+
+	if (put_user(oal->oal_circ.tail, &lali->_lali_tail))
+		return -EFAULT;
+
+	if (put_user(entry_space, &lali->_lali_entry_space))
+		return -EFAULT;
+
+	if (put_user(entry_count, &lali->_lali_entry_count))
+		return -EFAULT;
+
+	if (put_user(oal->oal_drop_count, &lali->_lali_drop_count))
+		return -EFAULT;
+
+	if (put_user(oal->oal_is_closed, &lali->_lali_is_closed))
+		return -EFAULT;
+
+	return 0;
+}
+
+static long oal_file_ioctl(struct file *filp, unsigned int cmd,
+			unsigned long arg)
+{
+	struct ofd_access_log *oal = filp->private_data;
+
+	switch (cmd) {
+	case LUSTRE_ACCESS_LOG_IOCTL_VERSION:
+		return LUSTRE_ACCESS_LOG_VERSION_1;
+	case LUSTRE_ACCESS_LOG_IOCTL_INFO:
+		return oal_ioctl_info(oal, arg);
+	default:
+		return -ENOTTY;
+	}
+}
+
+static const struct file_operations oal_fops = {
+	.owner = THIS_MODULE,
+	.open = &oal_file_open,
+	.unlocked_ioctl = &oal_file_ioctl,
+	.read = &oal_file_read,
+	.write = &oal_file_write,
+	.poll = &oal_file_poll,
+	.llseek = &no_llseek,
+};
+
+static void oal_device_release(struct device *dev)
+{
+	struct ofd_access_log *oal = dev_get_drvdata(dev);
+
+	oal_log_minor_free(MINOR(oal->oal_device.devt));
+	vfree(oal->oal_circ.buf);
+	kfree(oal);
+}
+
+struct ofd_access_log *ofd_access_log_create(const char *ofd_name, size_t size)
+{
+	const size_t entry_size = sizeof(struct ofd_access_entry_v1);
+	struct ofd_access_log *oal;
+	int minor;
+	int rc;
+
+	BUILD_BUG_ON(sizeof(oal->oal_name) != MAX_OBD_NAME);
+	BUILD_BUG_ON(!is_power_of_2(entry_size));
+
+	if (!size)
+		return NULL;
+
+	if (!is_power_of_2(size) || (size & (entry_size - 1)) ||
+	    (unsigned int)size != size)
+		return ERR_PTR(-EINVAL);
+
+	oal = kzalloc(sizeof(*oal), GFP_KERNEL);
+	if (!oal)
+		return ERR_PTR(-ENOMEM);
+
+	strlcpy(oal->oal_name, ofd_name, sizeof(oal->oal_name));
+	oal->oal_log_size = size;
+	oal->oal_entry_size = entry_size;
+	spin_lock_init(&oal->oal_write_lock);
+	spin_lock_init(&oal->oal_read_lock);
+	init_waitqueue_head(&oal->oal_read_wait_queue);
+
+	oal->oal_circ.buf = vmalloc(oal->oal_log_size);
+	if (!oal->oal_circ.buf) {
+		rc = -ENOMEM;
+		goto out_free;
+	}
+
+	rc = oal_log_minor_alloc(&minor);
+	if (rc < 0)
+		goto out_free;
+
+	device_initialize(&oal->oal_device);
+	oal->oal_device.devt = MKDEV(oal_log_major, minor);
+	oal->oal_device.class = oal_log_class;
+	oal->oal_device.release = &oal_device_release;
+	dev_set_drvdata(&oal->oal_device, oal);
+	rc = dev_set_name(&oal->oal_device,
+			"%s!%s", LUSTRE_ACCESS_LOG_DIR_NAME, oal->oal_name);
+	if (rc < 0)
+		goto out_minor;
+
+	cdev_init(&oal->oal_cdev, &oal_fops);
+	oal->oal_cdev.owner = THIS_MODULE;
+	rc = cdev_device_add(&oal->oal_cdev, &oal->oal_device);
+	if (rc < 0)
+		goto out_device_name;
+
+	oal_control_event_inc();
+
+	return oal;
+
+out_device_name:
+	kfree_const(oal->oal_device.kobj.name);
+out_minor:
+	oal_log_minor_free(minor);
+out_free:
+	vfree(oal->oal_circ.buf);
+	kfree(oal);
+
+	return ERR_PTR(rc);
+}
+
+void ofd_access(struct ofd_device *m,
+		const struct lu_fid *parent_fid,
+		__u64 begin, __u64 end,
+		unsigned int size,
+		unsigned int segment_count,
+		int rw)
+{
+	unsigned int flags = (rw == READ) ? OFD_ACCESS_READ : OFD_ACCESS_WRITE;
+
+	if (m->ofd_access_log && (flags & m->ofd_access_log_mask)) {
+		struct ofd_access_entry_v1 oae = {
+			.oae_parent_fid = *parent_fid,
+			.oae_begin = begin,
+			.oae_end = end,
+			.oae_time = ktime_get_real_seconds(),
+			.oae_size = size,
+			.oae_segment_count = segment_count,
+			.oae_flags = flags,
+		};
+
+		oal_write_entry(m->ofd_access_log, &oae, sizeof(oae));
+	}
+}
+
+/* Called on OST umount to:
+ * - Close the write end of the oal. The wakes any tasks sleeping in
+ *   read or poll and makes all reads return zero once the log
+ *   becomes empty.
+ * - Delete the associated stuct device and cdev, preventing new
+ *   opens. Existing opens retain a reference on the oal through
+ *   their reference on oal_device.
+ * The oal will be freed when the last open file handle is closed. */
+void ofd_access_log_delete(struct ofd_access_log *oal)
+{
+	if (!oal)
+		return;
+
+	oal->oal_is_closed = 1;
+	wake_up_all(&oal->oal_read_wait_queue);
+	cdev_device_del(&oal->oal_cdev, &oal->oal_device);
+}
+
+/* private_data for control device file. */
+struct oal_control_file {
+	int ccf_event_count;
+};
+
+/* Control file usage:
+ * Open /dev/lustre-access-log/control.
+ * while (1)
+ *   Poll for readable on control FD.
+ *   Call ioctl(FD, LUSTRE_ACCESS_LOG_IOCTL_PRESCAN) to fetch event count.
+ *   Scan /dev/ or /sys/class/... for new devices.
+ */
+static int oal_control_file_open(struct inode *inode, struct file *filp)
+{
+	struct oal_control_file *ccf;
+	int rc;
+
+	rc = nonseekable_open(inode, filp);
+	if (rc)
+		return rc;
+
+	/* ccf->ccf_event_count = 0 on open */
+	ccf = kzalloc(sizeof(*ccf), GFP_KERNEL);
+	if (!ccf)
+		return -ENOMEM;
+
+	filp->private_data = ccf;
+
+	return 0;
+}
+
+static int oal_control_file_release(struct inode *inode, struct file *filp)
+{
+	kfree(filp->private_data);
+	return 0;
+}
+
+static unsigned int oal_control_file_poll(struct file *filp, poll_table *wait)
+{
+	struct oal_control_file *ccf = filp->private_data;
+	unsigned int mask = 0;
+
+	poll_wait(filp, &oal_control_wait_queue, wait);
+
+	if (atomic_read(&oal_control_event_count) != ccf->ccf_event_count)
+		mask |= POLLIN;
+
+	return mask;
+}
+
+static long oal_control_file_ioctl(struct file *filp, unsigned int cmd,
+				unsigned long arg)
+{
+	struct oal_control_file *ccf = filp->private_data;
+
+	switch (cmd) {
+	case LUSTRE_ACCESS_LOG_IOCTL_VERSION:
+		return LUSTRE_ACCESS_LOG_VERSION_1;
+	case LUSTRE_ACCESS_LOG_IOCTL_MAJOR:
+		return oal_log_major;
+	case LUSTRE_ACCESS_LOG_IOCTL_PRESCAN:
+		ccf->ccf_event_count = atomic_read(&oal_control_event_count);
+		return 0;
+	default:
+		return -ENOTTY;
+	}
+}
+
+static const struct file_operations oal_control_fops = {
+	.owner = THIS_MODULE,
+	.open = &oal_control_file_open,
+	.release = &oal_control_file_release,
+	.poll = &oal_control_file_poll,
+	.unlocked_ioctl = &oal_control_file_ioctl,
+	.llseek = &noop_llseek,
+};
+
+static struct miscdevice oal_control_misc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = LUSTRE_ACCESS_LOG_DIR_NAME"!control",
+	.fops = &oal_control_fops,
+};
+
+int ofd_access_log_module_init(void)
+{
+	dev_t dev;
+	int rc;
+
+	BUILD_BUG_ON(!is_power_of_2(sizeof(struct ofd_access_entry_v1)));
+
+	rc = misc_register(&oal_control_misc);
+	if (rc)
+		return rc;
+
+	rc = alloc_chrdev_region(&dev, 0, OAL_DEV_COUNT,
+				LUSTRE_ACCESS_LOG_DIR_NAME);
+	if (rc)
+		goto out_oal_control_misc;
+
+	oal_log_major = MAJOR(dev);
+
+	oal_log_class = class_create(THIS_MODULE, LUSTRE_ACCESS_LOG_DIR_NAME);
+	if (IS_ERR(oal_log_class)) {
+		rc = PTR_ERR(oal_log_class);
+		goto out_dev;
+	}
+
+	return 0;
+out_dev:
+	unregister_chrdev_region(dev, OAL_DEV_COUNT);
+out_oal_control_misc:
+	misc_deregister(&oal_control_misc);
+
+	return rc;
+}
+
+void ofd_access_log_module_exit(void)
+{
+	class_destroy(oal_log_class);
+	unregister_chrdev_region(MKDEV(oal_log_major, 0), OAL_DEV_COUNT);
+	idr_destroy(&oal_log_minor_idr);
+	misc_deregister(&oal_control_misc);
+}

lustre/include/uapi/linux/lustre/Makefile.am
@@ -44,6 +44,7 @@ lustreinclude_HEADERS = \
 
 if SERVER
 lustreinclude_HEADERS += \
+	lustre_access_log.h \
 	lustre_barrier_user.h \
 	lustre_disk.h \
 	lustre_lfsck_user.h \
@@ -51,6 +52,7 @@ lustreinclude_HEADERS += \
 endif
 
 EXTRA_DIST = \
+	lustre_access_log.h \
 	lustre_barrier_user.h \
 	lustre_cfg.h \
 	lustre_disk.h \

lustre/include/uapi/linux/lustre/lustre_access_log.h
@@ -1,0 +1,79 @@
+#ifndef _LUSTRE_ACCESS_LOG_H
+# define _LUSTRE_ACCESS_LOG_H
+
+#include <linux/types.h>
+#include <asm/ioctl.h>
+/*
+ * This is due to us being out of kernel and the way the OpenSFS branch
+ * handles CFLAGS.
+ */
+#ifdef __KERNEL__
+# include <uapi/linux/lustre/lustre_user.h>
+#else
+# include <linux/lustre/lustre_user.h>
+#endif
+
+enum ofd_access_flags {
+	OFD_ACCESS_READ = 0x1,
+	OFD_ACCESS_WRITE = 0x2,
+};
+
+struct ofd_access_entry_v1 {
+	struct lu_fid	oae_parent_fid; /* 16 */
+	__u64		oae_begin; /* 24 */
+	__u64		oae_end; /* 32 */
+	__u64		oae_time; /* 40 */
+	__u32		oae_size; /* 44 */
+	__u32		oae_segment_count; /* 48 */
+	__u32		oae_flags; /* 52 enum ofd_access_flags */
+	__u32		oae_reserved1; /* 56 */
+	__u32		oae_reserved2; /* 60 */
+	__u32		oae_reserved3; /* 64 */
+};
+
+/* The name of the subdirectory of devtmpfs (/dev) containing the
+ * control and access log char devices. */
+#define LUSTRE_ACCESS_LOG_DIR_NAME "lustre-access-log"
+
+enum {
+	LUSTRE_ACCESS_LOG_VERSION_1 = 0x00010000,
+	LUSTRE_ACCESS_LOG_TYPE_OFD = 0x1,
+	LUSTRE_ACCESS_LOG_NAME_SIZE = 128,
+};
+
+struct lustre_access_log_info_v1 {
+	__u32	lali_version; /* LUSTRE_ACCESS_LOG_VERSION_1 */
+	__u32	lali_type; /* LUSTRE_ACCESS_LOG_TYPE_OFD */
+	char	lali_name[LUSTRE_ACCESS_LOG_NAME_SIZE]; /* obd_name */
+	__u32	lali_log_size;
+	__u32	lali_entry_size;
+	/* Underscore prefixed members are intended for test and debug
+	 * purposes only. */
+	__u32	_lali_head;
+	__u32	_lali_tail;
+	__u32	_lali_entry_space;
+	__u32	_lali_entry_count;
+	__u32	_lali_drop_count;
+	__u32	_lali_is_closed;
+};
+
+enum {
+	/* /dev/lustre-access-log/control ioctl: return lustre access log
+	 * interface version. */
+	LUSTRE_ACCESS_LOG_IOCTL_VERSION = _IO('O', 0x81),
+
+	/* /dev/lustre-access-log/control ioctl: return device major
+	 * used for access log devices. (The major is dynamically
+	 * allocated during ofd module initialization. */
+	LUSTRE_ACCESS_LOG_IOCTL_MAJOR = _IO('O', 0x82),
+
+	/* /dev/lustre-access-log/control ioctl: get global control event
+	 * count and store it into file private_data. */
+	LUSTRE_ACCESS_LOG_IOCTL_PRESCAN = _IO('O', 0x83),
+
+	/* /dev/lustre-access-log/OBDNAME ioctl: populate struct
+	 * lustre_access_log_info_v1 for the current device. */
+	LUSTRE_ACCESS_LOG_IOCTL_INFO = _IOR('O', 0x84, struct lustre_access_log_info_v1),
+};
+
+#endif /* _LUSTRE_ACCESS_LOG_H */

lustre/ofd/Makefile.in
@@ -1,7 +1,7 @@
 MODULES := ofd
 
 ofd-objs := ofd_dev.o ofd_obd.o ofd_fs.o ofd_trans.o ofd_objects.o ofd_io.o
-ofd-objs += lproc_ofd.o ofd_dlm.o ofd_lvb.o
+ofd-objs += lproc_ofd.o ofd_dlm.o ofd_lvb.o ofd_access_log.o
 
 EXTRA_DIST = $(ofd-objs:%.o=%.c) ofd_internal.h
 

lustre/ofd/lproc_ofd.c
@@ -45,6 +45,7 @@
 #include <lprocfs_status.h>
 #include <linux/seq_file.h>
 #include <lustre_lfsck.h>
+#include <uapi/linux/lustre/lustre_access_log.h>
 
 #include "ofd_internal.h"
 
@@ -687,6 +688,105 @@ ofd_lfsck_verify_pfid_seq_write(struct file *file, const char __user *buffer,
 
 LPROC_SEQ_FOPS(ofd_lfsck_verify_pfid);
 
+static ssize_t access_log_mask_show(struct kobject *kobj,
+			struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+
+	return snprintf(buf, PAGE_SIZE, "%s%s%s\n",
+		(ofd->ofd_access_log_mask == 0) ? "0" : "",
+		(ofd->ofd_access_log_mask & OFD_ACCESS_READ) ? "r" : "",
+		(ofd->ofd_access_log_mask & OFD_ACCESS_WRITE) ? "w" : "");
+}
+
+static ssize_t access_log_mask_store(struct kobject *kobj,
+				     struct attribute *attr,
+				     const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+	unsigned int mask = 0;
+	size_t i;
+
+	for (i = 0; i < count; i++) {
+		switch (tolower(buffer[i])) {
+		case '0':
+			break;
+		case 'r':
+			mask |= OFD_ACCESS_READ;
+			break;
+		case 'w':
+			mask |= OFD_ACCESS_WRITE;
+			break;
+		default:
+			return -EINVAL;
+		}
+	}
+
+	ofd->ofd_access_log_mask = mask;
+
+	return count;
+}
+LUSTRE_RW_ATTR(access_log_mask);
+
+static ssize_t access_log_size_show(struct kobject *kobj,
+				    struct attribute *attr, char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", ofd->ofd_access_log_size);
+}
+
+static ssize_t access_log_size_store(struct kobject *kobj,
+				     struct attribute *attr,
+				     const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+	struct ofd_access_log *oal;
+	unsigned int size;
+	ssize_t rc;
+
+	rc = kstrtouint(buffer, 0, &size);
+	if (rc < 0)
+		return rc;
+
+	if (!ofd_access_log_size_is_valid(size))
+		return -EINVAL;
+
+	/* The size of the ofd_access_log cannot be changed after it
+	 * has been created.
+	 */
+	if (ofd->ofd_access_log_size == size)
+		return count;
+
+	oal = ofd_access_log_create(obd->obd_name, size);
+	if (IS_ERR(oal))
+		return PTR_ERR(oal);
+
+	spin_lock(&ofd->ofd_flags_lock);
+	if (ofd->ofd_access_log != NULL) {
+		rc = -EBUSY;
+	} else {
+		ofd->ofd_access_log = oal;
+		ofd->ofd_access_log_size = size;
+		oal = NULL;
+		rc = count;
+	}
+	spin_unlock(&ofd->ofd_flags_lock);
+
+	ofd_access_log_delete(oal);
+
+	return rc;
+}
+LUSTRE_RW_ATTR(access_log_size);
+
 static int ofd_site_stats_seq_show(struct seq_file *m, void *data)
 {
 	struct obd_device *obd = m->private;
@@ -981,6 +1081,8 @@ static struct attribute *ofd_attrs[] = {
 #endif
 	&lustre_attr_soft_sync_limit.attr,
 	&lustre_attr_lfsck_speed_limit.attr,
+	&lustre_attr_access_log_mask.attr,
+	&lustre_attr_access_log_size.attr,
 	&lustre_attr_job_cleanup_interval.attr,
 	&lustre_attr_checksum_t10pi_enforce.attr,
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)

lustre/ofd/ofd_dev.c
@@ -2845,6 +2845,8 @@ static int ofd_init0(const struct lu_env *env, struct ofd_device *m,
 	INIT_LIST_HEAD(&m->ofd_inconsistency_list);
 	spin_lock_init(&m->ofd_inconsistency_lock);
 
+	m->ofd_access_log_mask = -1; /* Log all accesses if enabled. */
+
 	spin_lock_init(&m->ofd_batch_lock);
 	init_rwsem(&m->ofd_lastid_rwsem);
 
@@ -3015,6 +3017,9 @@ static void ofd_fini(const struct lu_env *env, struct ofd_device *m)
 		d->ld_obd->obd_namespace = m->ofd_namespace = NULL;
 	}
 
+	ofd_access_log_delete(m->ofd_access_log);
+	m->ofd_access_log = NULL;
+
 	ofd_stack_fini(env, m, &m->ofd_dt_dev.dd_lu_dev);
 
 	LASSERT(atomic_read(&d->ld_ref) == 0);
@@ -3130,13 +3135,28 @@ static struct lu_device_type ofd_device_type = {
  */
 static int __init ofd_init(void)
 {
-	int				rc;
+	int rc;
 
 	rc = lu_kmem_init(ofd_caches);
 	if (rc)
 		return rc;
+
+	rc = ofd_access_log_module_init();
+	if (rc)
+		goto out_caches;
+
 	rc = class_register_type(&ofd_obd_ops, NULL, true, NULL,
 				 LUSTRE_OST_NAME, &ofd_device_type);
+	if (rc)
+		goto out_ofd_access_log;
+
+	return 0;
+
+out_ofd_access_log:
+	ofd_access_log_module_exit();
+out_caches:
+	lu_kmem_fini(ofd_caches);
+
 	return rc;
 }
 
@@ -3148,8 +3168,9 @@ static int __init ofd_init(void)
  */
 static void __exit ofd_exit(void)
 {
-	lu_kmem_fini(ofd_caches);
 	class_unregister_type(LUSTRE_OST_NAME);
+	ofd_access_log_module_exit();
+	lu_kmem_fini(ofd_caches);
 }
 
 MODULE_AUTHOR("OpenSFS, Inc. <http://www.lustre.org/>");

lustre/ofd/ofd_internal.h
@@ -120,6 +120,10 @@ struct ofd_device {
 	__u64			 ofd_inconsistency_self_detected;
 	__u64			 ofd_inconsistency_self_repaired;
 
+	struct ofd_access_log	*ofd_access_log;
+	unsigned int		 ofd_access_log_size;
+	unsigned int		 ofd_access_log_mask;
+
 	struct list_head	ofd_seq_list;
 	rwlock_t		ofd_seq_list_lock;
 	int			ofd_seq_count;
@@ -288,6 +292,18 @@ struct ofd_thread_info {
 
 extern void target_recovery_fini(struct obd_device *obd);
 extern void target_recovery_init(struct lu_target *lut, svc_handler_t handler);
+
+/* ofd_access_log.c */
+bool ofd_access_log_size_is_valid(unsigned int size);
+int ofd_access_log_module_init(void);
+void ofd_access_log_module_exit(void);
+
+struct ofd_access_log;
+struct ofd_access_log *ofd_access_log_create(const char *ofd_name, size_t size);
+void ofd_access_log_delete(struct ofd_access_log *oal);
+void ofd_access(struct ofd_device *m,
+		const struct lu_fid *parent_fid, __u64 begin, __u64 end,
+		unsigned int size, unsigned int segment_count, int rw);
 
 /* ofd_dev.c */
 extern struct lu_context_key ofd_thread_key;

commit 3589a3141a4b9f94887b3ac5d6202233b06b8996

LU-6142 mdc: include linux/idr.h for referenced code
Include the <linux/idr.h> header in files that references IDR
functionality.  Don't depend on its indirect inclusion elsewhere.

Test-Parameters: trivial
Fixes: 66172e3274ca ("LU-13238 ofd: add OFD access logs")
Fixes: d0423abc1adc ("LU-12506 changelog: support large number of MDT")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Icdd03e15d31eabc4a1363d1757fc4db7723ebbe5
Reviewed-on: https://review.whamcloud.com/43346
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdc/mdc_changelog.c
@@ -35,6 +35,7 @@
 #include <linux/poll.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/idr.h>
 
 #include <lustre_log.h>
 #include <uapi/linux/lustre/lustre_ioctl.h>

lustre/ofd/ofd_access_log.c
@@ -2,6 +2,7 @@
 #include <linux/circ_buf.h>
 #include <linux/device.h>
 #include <linux/fs.h>
+#include <linux/idr.h>
 #include <linux/kernel.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>