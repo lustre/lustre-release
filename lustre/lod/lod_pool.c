lustre/lustre-release

commit 13cd0f9f667c6e138a8cb235d4920f8b749cb154

LU-13344 libcfs: Abstract proc_fs with proc_ops
Linux 5.6 introduces proc_ops with v5.5-8862-gd56c0d45f0e2
proc: decouple proc from VFS with "struct proc_ops"

Map proc_ops and it's members to file_operations and
the appropriate members for older kernels.

One remaining 'PROC_OWNER()' macro is left to deal with
proc_ops being unable to sensibly map the owner member.

Test-Parameters: trivial
HPE-bug-id: LUS-8589
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I3d8940a91b331c4f6bb31a9432194cc082c9cecd
Reviewed-on: https://review.whamcloud.com/37873
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_pool.c
@@ -59,6 +59,7 @@
 
 #include <libcfs/libcfs.h>
 #include <libcfs/linux/linux-hash.h>
+#include <libcfs/linux/linux-fs.h>
 #include <obd.h>
 #include "lod_internal.h"
 
@@ -317,7 +318,7 @@ static int pool_proc_open(struct inode *inode, struct file *file)
 	return rc;
 }
 
-const static struct file_operations pool_proc_operations = {
+const static struct proc_ops pool_proc_operations = {
 	.open		= pool_proc_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,

lustre/ptlrpc/nodemap_lproc.c
@@ -682,21 +682,21 @@ LPROC_SEQ_FOPS_RO(nodemap_map_mode);
 LPROC_SEQ_FOPS_RO(nodemap_audit_mode);
 LPROC_SEQ_FOPS_RO(nodemap_forbid_encryption);
 
-static const struct file_operations nodemap_ranges_fops = {
+static const struct proc_ops nodemap_ranges_fops = {
 	.open			= nodemap_ranges_open,
 	.read			= seq_read,
 	.llseek			= seq_lseek,
 	.release		= single_release
 };
 
-static const struct file_operations nodemap_idmap_fops = {
+static const struct proc_ops nodemap_idmap_fops = {
 	.open			= nodemap_idmap_open,
 	.read			= seq_read,
 	.llseek			= seq_lseek,
 	.release		= single_release
 };
 
-static const struct file_operations nodemap_exports_fops = {
+static const struct proc_ops nodemap_exports_fops = {
 	.open			= nodemap_exports_open,
 	.read			= seq_read,
 	.llseek			= seq_lseek,

.gitignore
@@ -42,6 +42,7 @@
 *.so
 *.swp
 *.unsigned
+_lpb
 00[0-9][0-9]-*.patch
 autoMakefile
 autoMakefile.in

libcfs/autoconf/lustre-libcfs.m4
@@ -1301,6 +1301,32 @@ LB_CHECK_EXPORT([kallsyms_lookup_name], [kernel/kallsyms.c],
 ]) # LIBCFS_KALLSYMS_LOOKUP
 
 #
+# LIBCFS_HAVE_PROC_OPS
+#
+# v5.5-8862-gd56c0d45f0e2
+# proc: decouple proc from VFS with "struct proc_ops"
+#
+AC_DEFUN([LIBCFS_SRC_HAVE_PROC_OPS], [
+	LB2_LINUX_TEST_SRC([proc_ops], [
+		#include <linux/proc_fs.h>
+
+		static struct proc_ops *my_proc;
+	],[
+		my_proc->proc_lseek = NULL;
+	],[-Werror])
+])
+AC_DEFUN([LIBCFS_HAVE_PROC_OPS], [
+	AC_MSG_CHECKING([if struct proc_ops exists])
+	LB2_LINUX_TEST_RESULT([proc_ops], [
+		AC_DEFINE(HAVE_PROC_OPS, 1,
+			[struct proc_ops exists])
+		AC_MSG_RESULT(yes)
+	],[
+		AC_MSG_RESULT(no)
+	])
+]) # LIBCFS_HAVE_PROC_OPS
+
+#
 # LIBCFS_VMALLOC_2ARGS
 #
 # kernel v5.8-rc1~201^2~19
@@ -1398,8 +1424,17 @@ kfree_sensitive_exists, [
 EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_HAVE_NR_UNSTABLE_NFS
 
-AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [] )
-AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [])
+AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
+	LIBCFS_SRC_HAVE_PROC_OPS
+
+	AC_MSG_CHECKING([for available kernel interfaces to libcfs])
+	LB2_LINUX_TEST_COMPILE_ALL([libcfs])
+	AC_MSG_RESULT([done])
+])
+AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
+	# 5.6
+	LIBCFS_HAVE_PROC_OPS
+])
 
 #
 # LIBCFS_PROG_LINUX

libcfs/include/libcfs/linux/linux-fs.h
@@ -60,4 +60,21 @@ static inline struct dentry *file_dentry(const struct file *file)
 #define DTTOIF(dirtype)		((dirtype) << S_DT_SHIFT)
 #endif
 
+#ifdef HAVE_PROC_OPS
+#define PROC_OWNER(_fn)
+#else
+#define proc_ops file_operations
+#define PROC_OWNER(_owner)		.owner = (_owner),
+#define proc_open			open
+#define proc_read			read
+#define proc_write			write
+#define proc_lseek			llseek
+#define proc_release			release
+#define proc_poll			poll
+#define proc_ioctl			unlocked_ioctl
+#define proc_compat_ioctl		compat_ioctl
+#define proc_mmap			mmap
+#define proc_get_unmapped_area		get_unmapped_area
+#endif
+
 #endif

lustre/include/lprocfs_status.h
@@ -46,6 +46,7 @@
 #include <linux/seq_file.h>
 
 #include <libcfs/libcfs.h>
+#include <libcfs/linux/linux-fs.h>
 #include <uapi/linux/lustre/lustre_idl.h>
 
 /*
@@ -55,7 +56,7 @@
  */
 struct lprocfs_vars {
 	const char			*name;
-	const struct file_operations	*fops;
+	const struct proc_ops		*fops;
 	void				*data;
 	/** /proc file mode. */
 	mode_t				 proc_mode;
@@ -521,7 +522,7 @@ static inline int lprocfs_exp_cleanup(struct obd_export *exp)
 #endif
 extern struct proc_dir_entry *
 lprocfs_add_simple(struct proc_dir_entry *root, char *name,
-		   void *data, const struct file_operations *fops);
+		   void *data, const struct proc_ops *ops);
 extern struct proc_dir_entry *
 lprocfs_add_symlink(const char *name, struct proc_dir_entry *parent,
                     const char *format, ...);
@@ -557,12 +558,10 @@ extern int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only);
 extern int lprocfs_obd_cleanup(struct obd_device *obd);
 
 extern int lprocfs_seq_create(struct proc_dir_entry *parent, const char *name,
-			      mode_t mode,
-			      const struct file_operations *seq_fops,
+			      mode_t mode, const struct proc_ops *seq_fops,
 			      void *data);
 extern int lprocfs_obd_seq_create(struct obd_device *obd, const char *name,
-				  mode_t mode,
-				  const struct file_operations *seq_fops,
+				  mode_t mode, const struct proc_ops *seq_fops,
 				  void *data);
 
 /* Generic callbacks */
@@ -746,13 +745,13 @@ static int name##_single_open(struct inode *inode, struct file *file)	\
 			   inode->i_private ? inode->i_private :	\
 					      PDE_DATA(inode));		\
 }									\
-static const struct file_operations name##_fops = {			\
-	.owner	 = THIS_MODULE,						\
-	.open	 = name##_single_open,					\
-	.read	 = seq_read,						\
-	.write	 = custom_seq_write,					\
-	.llseek	 = seq_lseek,						\
-	.release = lprocfs_single_release,				\
+static const struct proc_ops name##_fops = {				\
+	PROC_OWNER(THIS_MODULE)						\
+	.proc_open		= name##_single_open,			\
+	.proc_read		= seq_read,				\
+	.proc_write		= custom_seq_write,			\
+	.proc_lseek		= seq_lseek,				\
+	.proc_release		= lprocfs_single_release,		\
 }
 
 #define LPROC_SEQ_FOPS_RO(name)		__LPROC_SEQ_FOPS(name, NULL)
@@ -793,10 +792,10 @@ static const struct file_operations name##_fops = {			\
 				   inode->i_private ? inode->i_private : \
 				   PDE_DATA(inode));			\
 	}								\
-	static const struct file_operations name##_##type##_fops = {	\
-		.open	 = name##_##type##_open,			\
-		.write	 = name##_##type##_write,			\
-		.release = lprocfs_single_release,			\
+	static const struct proc_ops name##_##type##_fops = {		\
+		.proc_open	= name##_##type##_open,			\
+		.proc_write	= name##_##type##_write,		\
+		.proc_release	= lprocfs_single_release,		\
 	};
 
 struct lustre_attr {

lustre/lmv/lproc_lmv.c
@@ -249,12 +249,12 @@ static int lmv_target_seq_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static const struct file_operations lmv_proc_target_fops = {
-        .owner                = THIS_MODULE,
-        .open                 = lmv_target_seq_open,
-        .read                 = seq_read,
-        .llseek               = seq_lseek,
-        .release              = seq_release,
+static const struct proc_ops lmv_proc_target_fops = {
+	PROC_OWNER(THIS_MODULE)
+	.proc_open	= lmv_target_seq_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= seq_release,
 };
 #endif /* CONFIG_PROC_FS */
 

lustre/lod/lproc_lod.c
@@ -1015,20 +1015,20 @@ static ssize_t mdt_hash_store(struct kobject *kobj, struct attribute *attr,
 }
 LUSTRE_RW_ATTR(mdt_hash);
 
-static const struct file_operations lod_proc_mdt_fops = {
-	.owner   = THIS_MODULE,
-	.open    = lod_mdts_seq_open,
-	.read    = seq_read,
-	.llseek  = seq_lseek,
-	.release = lprocfs_seq_release,
+static const struct proc_ops lod_proc_mdt_fops = {
+	PROC_OWNER(THIS_MODULE)
+	.proc_open	= lod_mdts_seq_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= lprocfs_seq_release,
 };
 
-static const struct file_operations lod_proc_target_fops = {
-	.owner   = THIS_MODULE,
-	.open    = lod_osts_seq_open,
-	.read    = seq_read,
-	.llseek  = seq_lseek,
-	.release = lprocfs_seq_release,
+static struct proc_ops lod_proc_target_fops = {
+	PROC_OWNER(THIS_MODULE)
+	.proc_open	= lod_osts_seq_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= lprocfs_seq_release,
 };
 
 static struct attribute *lod_attrs[] = {

commit d106dfc1458702865118e73bfcdfc2ec2676a7d6

LU-13783 procfs: fix improper prop_ops fields
The lod pool and nodemap proc_ops missed renaming the fields to
start with .proc_*. On newer distros like Ubuntu 20.04 HWE you
get the following compile error:

lustre-release/lustre/ptlrpc/nodemap_lproc.c:686:3: error: ‘const struct proc_ops’ has no member named ‘open’
  686 |  .open   = nodemap_ranges_open,

Test-Parameters: trivial
Fixes: 13cd0f9f667 ("LU-13344 libcfs: Abstract proc_fs with proc_ops")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I5fff7519a801f585690d468255f7ca6c73adcc90
Reviewed-on: https://review.whamcloud.com/43880
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_pool.c
@@ -318,10 +318,10 @@ static int pool_proc_open(struct inode *inode, struct file *file)
 }
 
 const static struct proc_ops pool_proc_operations = {
-	.open		= pool_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= seq_release,
+	.proc_open	= pool_proc_open,
+	.proc_read	= seq_read,
+	.proc_lseek	= seq_lseek,
+	.proc_release	= seq_release,
 };
 
 /**

lustre/ptlrpc/nodemap_lproc.c
@@ -683,24 +683,24 @@ LPROC_SEQ_FOPS_RO(nodemap_audit_mode);
 LPROC_SEQ_FOPS_RO(nodemap_forbid_encryption);
 
 static const struct proc_ops nodemap_ranges_fops = {
-	.open			= nodemap_ranges_open,
-	.read			= seq_read,
-	.llseek			= seq_lseek,
-	.release		= single_release
+	.proc_open		= nodemap_ranges_open,
+	.proc_read		= seq_read,
+	.proc_lseek		= seq_lseek,
+	.proc_release		= single_release
 };
 
 static const struct proc_ops nodemap_idmap_fops = {
-	.open			= nodemap_idmap_open,
-	.read			= seq_read,
-	.llseek			= seq_lseek,
-	.release		= single_release
+	.proc_open		= nodemap_idmap_open,
+	.proc_read		= seq_read,
+	.proc_lseek		= seq_lseek,
+	.proc_release		= single_release
 };
 
 static const struct proc_ops nodemap_exports_fops = {
-	.open			= nodemap_exports_open,
-	.read			= seq_read,
-	.llseek			= seq_lseek,
-	.release		= single_release
+	.proc_open		= nodemap_exports_open,
+	.proc_read		= seq_read,
+	.proc_lseek		= seq_lseek,
+	.proc_release		= single_release
 };
 
 static struct lprocfs_vars lprocfs_nodemap_vars[] = {