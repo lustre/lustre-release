lustre/lustre-release

commit 701cc249594eae08f6e762eff74183e768c2cee3

LU-13533 utils: ext4lazyinit should be disabled
lazyinit gets more than 24H on typical OST installation
and produces writes inside read.  This influences to benchmark
tests that are usually executed just after cluster installation
is complete. Testing shows, disabling that feature adds ~30 sec
to formating OST drive so that is not a noticeable time during
install.

Explicitly send lazy_itable_init and lazy_journal_init in
conf_sanity 116 to avoid out of disk space.

HPE-bug-id: LUS-3358
Signed-off-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: I95bfa43d11cd67c890b036aa0b71fae4c1eea37d
Reviewed-on: https://review.whamcloud.com/38534
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/libmount_utils_ldiskfs.c
@@ -882,9 +882,17 @@ int ldiskfs_make_lustre(struct mkfs_opts *mop)
 	}
 
 	/* Avoid zeroing out the full journal - speeds up mkfs */
-	if (is_e2fsprogs_feature_supp("-E lazy_journal_init"))
+	if (is_e2fsprogs_feature_supp("-E lazy_journal_init=0")) {
 		append_unique(start, ext_opts ? "," : " -E ",
-			      "lazy_journal_init", NULL, maxbuflen);
+			      "lazy_journal_init=0", NULL, maxbuflen);
+		ext_opts = 1;
+	}
+	if (is_e2fsprogs_feature_supp("-E lazy_itable_init=0")) {
+		append_unique(start, ext_opts ? "," : "-E",
+			    "lazy_itable_init=0", NULL, maxbuflen);
+		ext_opts = 1;
+	}
+
 	/* end handle -E mkfs options */
 
 	/* Allow reformat of full devices (as opposed to partitions).

lustre/tests/conf-sanity.sh
@@ -8559,7 +8559,10 @@ test_116() {
 
 	MDSSIZE=$old_mdssize
 	MDSISIZE=$old_mdsisize
-	do_facet $SINGLEMDS $MKFS $opts17t $tmpmnt/$mdtimg ||
+
+	do_facet $SINGLEMDS $MKFS \
+		${opts17t/-E /-E lazy_itable_init,lazy_journal_init,} \
+		$tmpmnt/$mdtimg ||
 		error "failed to mkfs for $tmpmnt/$mdtimg"
 
 	do_facet $SINGLEMDS $TUNE2FS -l $tmpmnt/$mdtimg |

commit a81c093a935c62b9e4586ae930aab7439948d538

LU-14767 utils: mkfs.lustre allow lazy_itable_init=1
When "lazy_itable_init=0" was added to the mke2fs options the call
to append_unique() to see whether "lazy_itable_init" was already
listed in the mke2fs options was incorrect. It checks to see if
"lazy_itable_init=0" is already present in the options, and doesn't
match "lazy_itable_init=1" if it was specified on the command-line.

Separate the key and value passed to append_unique() so that it can
check if any form of the key is present in the existing options.

Test-Parameters: trivial testlist=conf-sanity
Fixes: 701cc249594e ("LU-13533 utils: ext4lazyinit should be disabled")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ic7a6dbb81f004dd35f0f1c5f5ddec0fb363ebbe5
Reviewed-on: https://review.whamcloud.com/44019
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/libmount_utils_ldiskfs.c
@@ -886,12 +886,12 @@ int ldiskfs_make_lustre(struct mkfs_opts *mop)
 	/* Avoid zeroing out the full journal - speeds up mkfs */
 	if (is_e2fsprogs_feature_supp("-E lazy_journal_init=0")) {
 		append_unique(start, ext_opts ? "," : " -E ",
-			      "lazy_journal_init=0", NULL, maxbuflen);
+			      "lazy_journal_init", "0", maxbuflen);
 		ext_opts = 1;
 	}
 	if (is_e2fsprogs_feature_supp("-E lazy_itable_init=0")) {
 		append_unique(start, ext_opts ? "," : "-E",
-			    "lazy_itable_init=0", NULL, maxbuflen);
+			    "lazy_itable_init", "0", maxbuflen);
 		ext_opts = 1;
 	}
 