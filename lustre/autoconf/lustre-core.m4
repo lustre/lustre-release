lustre/lustre-release

commit 84e12028be9a13a5f702ce04adfd55d94d8a96ef

LU-9859 libcfs: add support for Xarray
In the late 4.X kernel cycle Xarrays were introduced with the
goal of replacing the radix tree for the page cache. It is
highly optimized for densely packed data which is the case for
several items in Lustre such as the static array for the obd
devices and quota ids. This patch provides Xarray support for
kernels that lack. The current verison of Xarray back ported
is from the 5.4-rc2 kernel.

Change-Id: I54f9046f50a353e1cd4271c0b97207062bbf3898
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37391
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -1967,41 +1967,25 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_INODE_TIMESPEC64
 
 #
-# LC_XA_IS_VALUE
-# kernel 4.19-rc6 commit 3159f943aafdbacb2f94c38fdaadabf2bbde2a14
-# xarray: Replace exceptional entries
-# adds xa_is_value
-#
-AC_DEFUN([LC_XA_IS_VALUE], [
-LB_CHECK_COMPILE([xa_is_value exist],
-xa_is_value, [
-	#include <linux/xarray.h>
-],[
-	xa_is_value(NULL);
-],[
-	AC_DEFINE(HAVE_XA_IS_VALUE, 1, [xa_is_value exist])
-])
-]) # LC_XA_IS_VALUE
-
-#
 # LC___XA_SET_MARK
 #
 # kernel 4.20 commit v4.19-rc5-248-g9b89a0355144
-# xarray: Add XArray marks
+# xarray: Add XArray marks - replaced radix_tree_tag_set
 #
 AC_DEFUN([LC___XA_SET_MARK], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
 LB_CHECK_COMPILE([if '__xa_set_mark' exists],
 __xa_set_mark, [
-	#include <linux/xarray.h>
 	#include <linux/fs.h>
+	#include <linux/radix-tree.h>
 ],[
-	struct xarray *xa = NULL;
-
-	__xa_set_mark(xa, 0, PAGECACHE_TAG_DIRTY);
+	radix_tree_tag_set(NULL, 0, PAGECACHE_TAG_DIRTY);
 ],[
-	AC_DEFINE(HAVE___XA_SET_MARK, 1,
+	AC_DEFINE(HAVE_RADIX_TREE_TAG_SET, 1,
 		[__xa_set_mark exists])
 ])
+EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC___XA_SET_MARK
 
 #
@@ -2343,7 +2327,6 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 4.18
 	LC_INODE_TIMESPEC64
-	LC_XA_IS_VALUE
 
 	# 4.20
 	LC___XA_SET_MARK

libcfs/autoconf/lustre-libcfs.m4
@@ -1069,6 +1069,28 @@ clear_and_wake_up_bit, [
 ]) # LIBCFS_CLEAR_AND_WAKE_UP_BIT
 
 #
+# LIBCFS_XARRAY_SUPPORT
+#
+# 4.19-rc5 kernel commit 3159f943aafdbacb2f94c38fdaadabf2bbde2a14
+# replaced the radix tree implementation with Xarrays. This change
+# introduced functionaly needed for general Xarray support
+#
+AC_DEFUN([LIBCFS_XARRAY_SUPPORT], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if page cache uses Xarray],
+xarray_support, [
+	#include <linux/radix-tree.h>
+],[
+	radix_tree_exceptional_entry(NULL);
+],[
+	AC_DEFINE(HAVE_RADIX_TREE_EXCEPTIONAL_ENTRY, 1,
+		[kernel lacks 'xa_is_value'])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LIBCFS_XARRAY_SUPPORT
+
+#
 # LIBCFS_HAVE_IOV_ITER_TYPE
 #
 # kernel 4.20 commit 00e23707442a75b404392cef1405ab4fd498de6b
@@ -1290,6 +1312,8 @@ LIBCFS_TIMER_SETUP
 LIBCFS_WAIT_VAR_EVENT
 # 4.17
 LIBCFS_CLEAR_AND_WAKE_UP_BIT
+# 4.19
+LIBCFS_XARRAY_SUPPORT
 # 4.20
 LIBCFS_HAVE_IOV_ITER_TYPE
 # 5.0

libcfs/include/libcfs/linux/Makefile.am
@@ -1,3 +1,3 @@
 EXTRA_DIST = linux-misc.h linux-fs.h linux-mem.h linux-time.h linux-cpu.h \
 	     linux-list.h linux-hash.h linux-uuid.h linux-wait.h \
-	     refcount.h processor.h
+	     refcount.h processor.h xarray.h

libcfs/libcfs/Makefile.in
@@ -4,6 +4,7 @@ libcfs-linux-objs := linux-prim.o
 libcfs-linux-objs += linux-curproc.o
 libcfs-linux-objs += linux-hash.o
 libcfs-linux-objs += linux-wait.o
+libcfs-linux-objs += xarray.o
 
 libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
 libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o bio.o

libcfs/libcfs/linux/Makefile.am
@@ -1,4 +1,5 @@
 EXTRA_DIST = linux-prim.c \
 	     linux-curproc.c \
 	     linux-hash.c \
-	     linux-wait.c
+	     linux-wait.c \
+	     xarray.c

libcfs/libcfs/linux/linux-prim.c
@@ -114,7 +114,7 @@ int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
 }
 EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
 
-struct kmem_cache (*cfs_radix_tree_node_cachep);
+struct kmem_cache (*radix_tree_node_cachep);
 
 void __init cfs_arch_init(void)
 {
@@ -123,6 +123,8 @@ void __init cfs_arch_init(void)
 #endif
 	cfs_apply_workqueue_attrs_t =
 		(void *)kallsyms_lookup_name("apply_workqueue_attrs");
+	radix_tree_node_cachep =
+		(void *)kallsyms_lookup_name("radix_tree_node_cachep");
 }
 
 int cfs_kernel_write(struct file *filp, const void *buf, size_t count,

commit 88bb292426cc6e398f530a06d3c5852760c8edfa

LU-14817 build: __xa_set_mark is not checked anymore
LC__XA_SET_MARK does not check for __xa_set_mark anymore after
LU-9859, however the result variable still exists and its value
has changed from 'no' to 'yes'.

Test-Parameters: trivial
Fixes: 84e12028be ("LU-9859 libcfs: add support for Xarray")
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I24fffe7f2727b1d892ec3cabfc6e65ae8f68e024
Reviewed-on: https://review.whamcloud.com/44138
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2186,26 +2186,26 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_INODE_TIMESPEC64
 
 #
-# LC___XA_SET_MARK
+# LC_RADIX_TREE_TAG_SET
 #
 # kernel 4.20 commit v4.19-rc5-248-g9b89a0355144
 # xarray: Add XArray marks - replaced radix_tree_tag_set
 #
-AC_DEFUN([LC___XA_SET_MARK], [
+AC_DEFUN([LC_RADIX_TREE_TAG_SET], [
 tmp_flags="$EXTRA_KCFLAGS"
 EXTRA_KCFLAGS="-Werror"
-LB_CHECK_COMPILE([if '__xa_set_mark' exists],
-__xa_set_mark, [
+LB_CHECK_COMPILE([if 'radix_tree_tag_set' exists],
+radix_tree_tag_set, [
 	#include <linux/fs.h>
 	#include <linux/radix-tree.h>
 ],[
 	radix_tree_tag_set(NULL, 0, PAGECACHE_TAG_DIRTY);
 ],[
 	AC_DEFINE(HAVE_RADIX_TREE_TAG_SET, 1,
-		[__xa_set_mark exists])
+		[radix_tree_tag_set exists])
 ])
 EXTRA_KCFLAGS="$tmp_flags"
-]) # LC___XA_SET_MARK
+]) # LC_RADIX_TREE_TAG_SET
 
 #
 # LC_UAPI_LINUX_MOUNT_H
@@ -2561,7 +2561,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_INODE_TIMESPEC64
 
 	# 4.20
-	LC___XA_SET_MARK
+	LC_RADIX_TREE_TAG_SET
 	LC_UAPI_LINUX_MOUNT_H
 	LC_HAVE_SUNRPC_CACHE_HASH_LOCK_IS_A_SPINLOCK
 