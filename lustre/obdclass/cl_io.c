lustre/lustre-release

commit dfe2d225b86d4215cbd09e863e8de7547f0d4dae

LU-13799 clio: Implement real list splice
Lustre's list_splice is actually just a slightly
depressing list_for_each; let's use a real list_splice.

This saves significant time in AIO/DIO page submission,
getting a several % performance boost.

This patch reduces i/o time in ms/GiB by:
Write: 16 ms/GiB
Read: 14 ms/GiB

Totals:
Write: 220 ms/GiB
Read: 209 ms/GiB

mpirun -np 1  $IOR -w -r -t 64M -b 64G -o ./iorfile --posix.odirect

With previous patches in series:
write     4326 MiB/s
read      4587 MiB/s

With this patch:
write     4647 MiB/s
read      4888 MiB/s

Signed-off-by: Patrick Farrell <farr0186@gmail.com>
Change-Id: Icfd4a3d9dd6f162b011b402a1c88d7dae53eff40
Reviewed-on: https://review.whamcloud.com/39439
Reviewed-by: Wang Shilong <wshilong@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/obdclass/cl_io.c
@@ -918,15 +918,24 @@ EXPORT_SYMBOL(cl_page_list_move_head);
 /**
  * splice the cl_page_list, just as list head does
  */
-void cl_page_list_splice(struct cl_page_list *list, struct cl_page_list *head)
+void cl_page_list_splice(struct cl_page_list *src, struct cl_page_list *dst)
 {
+#ifdef USE_LU_REF
 	struct cl_page *page;
 	struct cl_page *tmp;
 
 
 	ENTRY;
 	cl_page_list_for_each_safe(page, tmp, list)
-		cl_page_list_move(head, list, page);
+		lu_ref_set_at(&page->cp_reference, &page->cp_queue_ref,
+			      "queue", src, dst);
+#else
+	ENTRY;
+#endif
+	dst->pl_nr += src->pl_nr;
+	src->pl_nr = 0;
+	list_splice_tail_init(&src->pl_pages, &dst->pl_pages);
+
 	EXIT;
 }
 EXPORT_SYMBOL(cl_page_list_splice);

commit 6b319185659104bd65a837fe2b3cbe550d83c650

LU-8066 obdclass: move lu_ref to debugfs
A special procfs file is created for lu_ref debugging. Lets move
this to debugfs where it belongs.

Also fixed a missed USE_LU_REF due to landing order as well as
a build fix.

Fixes: dfe2d225b86 ("LU-13799 clio: Implement real list splice")
Change-Id: I33646a87adfcabc5a5f214832953b2444e7aaf0a
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/44311
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/cl_io.c
@@ -922,13 +922,12 @@ EXPORT_SYMBOL(cl_page_list_move_head);
  */
 void cl_page_list_splice(struct cl_page_list *src, struct cl_page_list *dst)
 {
-#ifdef USE_LU_REF
+#ifdef CONFIG_LUSTRE_DEBUG_LU_REF
 	struct cl_page *page;
 	struct cl_page *tmp;
 
-
 	ENTRY;
-	cl_page_list_for_each_safe(page, tmp, list)
+	cl_page_list_for_each_safe(page, tmp, src)
 		lu_ref_set_at(&page->cp_reference, &page->cp_queue_ref,
 			      "queue", src, dst);
 #else

lustre/obdclass/lu_ref.c
@@ -285,8 +285,6 @@ void lu_ref_del_at(struct lu_ref *ref, struct lu_ref_link *link,
 }
 EXPORT_SYMBOL(lu_ref_del_at);
 
-#ifdef CONFIG_PROC_FS
-
 static void *lu_ref_seq_start(struct seq_file *seq, loff_t *pos)
 {
 	struct lu_ref *ref = seq->private;
@@ -411,8 +409,6 @@ static const struct file_operations lu_ref_dump_fops = {
 	.release	= lu_ref_seq_release
 };
 
-#endif /* CONFIG_PROC_FS */
-
 int lu_ref_global_init(void)
 {
 	int result;
@@ -421,24 +417,20 @@ int lu_ref_global_init(void)
 	       "lu_ref tracking is enabled. Performance isn't.\n");
 
 	result = lu_kmem_init(lu_ref_caches);
+	if (result)
+		return result;
 
-#ifdef CONFIG_PROC_FS
-	if (result == 0) {
-		result = lprocfs_seq_create(proc_lustre_root, "lu_refs",
-					    0444, &lu_ref_dump_fops, NULL);
-		if (result)
-			lu_kmem_fini(lu_ref_caches);
-	}
-#endif /* CONFIG_PROC_FS */
+	debugfs_create_file("lu_refs", 0444, debugfs_lustre_root,
+			    NULL, &lu_ref_dump_fops);
 
 	return result;
 }
 
 void lu_ref_global_fini(void)
 {
-#ifdef CONFIG_PROC_FS
-	lprocfs_remove_proc_entry("lu_refs", proc_lustre_root);
-#endif /* CONFIG_PROC_FS */
+	/* debugfs file gets cleaned up by debugfs_remove_recursive on
+	 * debugfs_lustre_root
+	 */
 	lu_kmem_fini(lu_ref_caches);
 }
 