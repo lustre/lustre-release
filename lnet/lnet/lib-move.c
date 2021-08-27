a18c4a16246e6185919eda805eca52772bbc3efe
Chris Horn <chris.horn@hpe.com>
2021-08-27 16:29:09 -0500

    LU-13575 lnet: Ensure round robin selection of local NIs
    Use the net sequence number to set the NI sequence number to ensure
    round robin selection of NIs on each net.
    
    Test-Parameters: trivial
    HPE-bug-id: LUS-10349
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I6ce0b088fcad6312186e6fbad4ab14283aee55eb
    Reviewed-on: https://review.whamcloud.com/45003
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lnet/lnet/lib-move.c
+++ lnet/lnet/lib-move.c
@@ -1839,8 +1839,8 @@ lnet_handle_send(struct lnet_send_data *sd)
 	 */
 	best_lpni->lpni_seq++;
 	best_lpni->lpni_peer_net->lpn_seq++;
-	best_ni->ni_seq++;
 	best_ni->ni_net->net_seq++;
+	best_ni->ni_seq = best_ni->ni_net->net_seq;
 
 	CDEBUG(D_NET, "%s NI seq info: [%d:%d:%d:%u] %s LPNI seq info [%d:%d:%d:%u]\n",
 	       libcfs_nidstr(&best_ni->ni_nid),
