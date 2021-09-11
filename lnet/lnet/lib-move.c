lustre/lustre-release

commit fa8b4e6357c53ea457ef6624b0b19bece0b0fdde

LU-7734 lnet: peer/peer_ni handling adjustments
A peer can be added by specifying a list of NIDs
	The first NID shall be used as the primary NID. The rest of
	the NIDs will be added under the primary NID

A peer can be added by explicitly specifying the key NID, and then
by adding a set of other NIDs, all done through one API call

If a key NID already exists, but it's not an MR NI, then adding that
Key NID from DLC shall convert that NI to an MR NI

If a key NID already exists, and it is an MR NI, then re-adding the
Key NID shall have no effect

if a Key NID already exists as part of another peer, then adding that
NID as part of another peer or as primary shall fail

if a NID is being added to a peer NI and that NID is a non-MR, then
that NID is moved under the peer and is made to be MR capable

if a NID is being added to a peer and that NID is an MR NID and part
of another peer, then the operation shall fail

if a NID is being added to a peer and it is already part of that Peer
then the operation is a no-op.

Moreover, the code is structured to consider the addition of Dynamic
Discovery in later patches.

Signed-off-by: Amir Shehata <amir.shehata@intel.com>
Change-Id: I71f740192a31ae00f83014ca3e9e06b61ae4ecd5
Reviewed-on: http://review.whamcloud.com/20531

lnet/lnet/lib-move.c
@@ -1336,10 +1336,10 @@ again:
 	lpni = NULL;
 	seq = lnet_get_dlc_seq_locked();
 
-	rc = lnet_find_or_create_peer_locked(dst_nid, cpt, &peer);
-	if (rc != 0) {
+	peer = lnet_find_or_create_peer_locked(dst_nid, cpt);
+	if (IS_ERR(peer)) {
 		lnet_net_unlock(cpt);
-		return rc;
+		return PTR_ERR(peer);
 	}
 
 	/* If peer is not healthy then can not send anything to it */
@@ -1547,13 +1547,6 @@ set_ni:
 		}
 	}
 	/*
-	 * Now that we selected the NI to use increment its sequence
-	 * number so the Round Robin algorithm will detect that it has
-	 * been used and pick the next NI.
-	 */
-	best_ni->ni_seq++;
-
-	/*
 	 * if the peer is not MR capable, then we should always send to it
 	 * using the first NI in the NET we determined.
 	 */
@@ -1566,6 +1559,13 @@ set_ni:
 			libcfs_nid2str(dst_nid));
 		return -EINVAL;
 	}
+
+	/*
+	 * Now that we selected the NI to use increment its sequence
+	 * number so the Round Robin algorithm will detect that it has
+	 * been used and pick the next NI.
+	 */
+	best_ni->ni_seq++;
 
 	if (routing)
 		goto send;
@@ -1633,7 +1633,7 @@ pick_peer:
 		}
 
 		CDEBUG(D_NET, "Best route to %s via %s for %s %d\n",
-			libcfs_nid2str(lpni->lpni_nid),
+			libcfs_nid2str(dst_nid),
 			libcfs_nid2str(best_gw->lpni_nid),
 			lnet_msgtyp2str(msg->msg_type), msg->msg_len);
 
@@ -2247,8 +2247,9 @@ lnet_parse(lnet_ni_t *ni, lnet_hdr_t *hdr, lnet_nid_t from_nid,
 	lnet_pid_t     dest_pid;
 	lnet_nid_t     dest_nid;
 	lnet_nid_t     src_nid;
-	__u32	       payload_length;
-	__u32	       type;
+	struct lnet_peer_ni *lpni;
+	__u32          payload_length;
+	__u32          type;
 
 	LASSERT (!in_interrupt ());
 
@@ -2410,19 +2411,20 @@ lnet_parse(lnet_ni_t *ni, lnet_hdr_t *hdr, lnet_nid_t from_nid,
 	msg->msg_initiator = lnet_peer_primary_nid(src_nid);
 
 	lnet_net_lock(cpt);
-	rc = lnet_nid2peerni_locked(&msg->msg_rxpeer, from_nid, cpt);
-	if (rc != 0) {
+	lpni = lnet_nid2peerni_locked(from_nid, cpt);
+	if (IS_ERR(lpni)) {
 		lnet_net_unlock(cpt);
 		CERROR("%s, src %s: Dropping %s "
-		       "(error %d looking up sender)\n",
+		       "(error %ld looking up sender)\n",
 		       libcfs_nid2str(from_nid), libcfs_nid2str(src_nid),
-		       lnet_msgtyp2str(type), rc);
+		       lnet_msgtyp2str(type), PTR_ERR(lpni));
 		lnet_msg_free(msg);
 		if (rc == -ESHUTDOWN)
 			/* We are shutting down.  Don't do anything more */
 			return 0;
 		goto drop;
 	}
+	msg->msg_rxpeer = lpni;
 	msg->msg_rxni = ni;
 	lnet_ni_addref_locked(ni, cpt);
 

lnet/include/lnet/lib-lnet.h
@@ -801,13 +801,12 @@ inline __u32 lnet_get_numa_range(void);
 struct lnet_peer_ni *lnet_get_next_peer_ni_locked(struct lnet_peer *peer,
 						  struct lnet_peer_net *peer_net,
 						  struct lnet_peer_ni *prev);
-int lnet_find_or_create_peer_locked(lnet_nid_t dst_nid, int cpt,
-				    struct lnet_peer **peer);
-int lnet_nid2peerni_locked(struct lnet_peer_ni **lpp, lnet_nid_t nid, int cpt);
+struct lnet_peer *lnet_find_or_create_peer_locked(lnet_nid_t dst_nid, int cpt);
+struct lnet_peer_ni *lnet_nid2peerni_locked(lnet_nid_t nid, int cpt);
 struct lnet_peer_ni *lnet_find_peer_ni_locked(lnet_nid_t nid);
 void lnet_peer_net_added(struct lnet_net *net);
 lnet_nid_t lnet_peer_primary_nid(lnet_nid_t nid);
-void lnet_peer_tables_cleanup(lnet_ni_t *ni);
+void lnet_peer_tables_cleanup(struct lnet_net *net);
 void lnet_peer_uninit(void);
 int lnet_peer_tables_create(void);
 void lnet_debug_peer(lnet_nid_t nid);
@@ -818,7 +817,7 @@ bool lnet_peer_is_ni_pref_locked(struct lnet_peer_ni *lpni,
 int lnet_add_peer_ni_to_peer(lnet_nid_t key_nid, lnet_nid_t nid, bool mr);
 int lnet_del_peer_ni_from_peer(lnet_nid_t key_nid, lnet_nid_t nid);
 int lnet_get_peer_info(__u32 idx, lnet_nid_t *primary_nid, lnet_nid_t *nid,
-		       struct lnet_peer_ni_credit_info *peer_ni_info,
+		       bool *mr, struct lnet_peer_ni_credit_info *peer_ni_info,
 		       struct lnet_ioctl_element_stats *peer_ni_stats);
 int lnet_get_peer_ni_info(__u32 peer_index, __u64 *nid,
 			  char alivness[LNET_MAX_STR_LEN],

lnet/include/lnet/lib-types.h
@@ -302,9 +302,9 @@ struct lnet_net {
 	/* chain on the ln_nets */
 	struct list_head	net_list;
 
-	/* net ID, which is compoed of
+	/* net ID, which is composed of
 	 * (net_type << 16) | net_num.
-	 * net_type can be one of the enumarated types defined in
+	 * net_type can be one of the enumerated types defined in
 	 * lnet/include/lnet/nidstr.h */
 	__u32			net_id;
 
@@ -548,11 +548,11 @@ struct lnet_peer_net {
 /* peer hash table */
 struct lnet_peer_table {
 	int			pt_version;	/* /proc validity stamp */
-	int			pt_number;	/* # peers extant */
-	int			pt_zombies;	/* # zombies to go to deathrow
-						 * (and not there yet) */
-	struct list_head	pt_deathrow;	/* zombie peers */
+	atomic_t		pt_number;	/* # peers extant */
 	struct list_head	*pt_hash;	/* NID->peer hash */
+	struct list_head	pt_zombie_list;	/* zombie peers */
+	int			pt_zombies;	/* # zombie peers */
+	spinlock_t		pt_zombie_lock;	/* protect list and count */
 };
 
 /* peer aliveness is enabled only on routers for peers in a network where the

lnet/lnet/api-ni.c
@@ -1298,9 +1298,6 @@ lnet_shutdown_lndni(struct lnet_ni *ni)
 	for (i = 0; i < the_lnet.ln_nportals; i++)
 		lnet_clear_lazy_portal(ni, i, "Shutting down NI");
 
-	/* Do peer table cleanup for this ni */
-	lnet_peer_tables_cleanup(ni);
-
 	lnet_net_lock(LNET_LOCK_EX);
 	lnet_clear_zombies_nis_locked(net);
 	lnet_net_unlock(LNET_LOCK_EX);
@@ -1325,6 +1322,12 @@ lnet_shutdown_lndnet(struct lnet_net *net)
 		lnet_net_lock(LNET_LOCK_EX);
 	}
 
+	lnet_net_unlock(LNET_LOCK_EX);
+
+	/* Do peer table cleanup for this net */
+	lnet_peer_tables_cleanup(net);
+
+	lnet_net_lock(LNET_LOCK_EX);
 	/*
 	 * decrement ref count on lnd only when the entire network goes
 	 * away
@@ -2658,13 +2661,16 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (config->cfg_hdr.ioc_len < sizeof(*config))
 			return -EINVAL;
 
-		return lnet_get_route(config->cfg_count,
-				      &config->cfg_net,
-				      &config->cfg_config_u.cfg_route.rtr_hop,
-				      &config->cfg_nid,
-				      &config->cfg_config_u.cfg_route.rtr_flags,
-				      &config->cfg_config_u.cfg_route.
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_route(config->cfg_count,
+				    &config->cfg_net,
+				    &config->cfg_config_u.cfg_route.rtr_hop,
+				    &config->cfg_nid,
+				    &config->cfg_config_u.cfg_route.rtr_flags,
+				    &config->cfg_config_u.cfg_route.
 					rtr_priority);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 
 	case IOC_LIBCFS_GET_LOCAL_NI: {
 		struct lnet_ioctl_config_ni *cfg_ni;
@@ -2686,7 +2692,10 @@ LNetCtl(unsigned int cmd, void *arg)
 		tun_size = cfg_ni->lic_cfg_hdr.ioc_len - sizeof(*cfg_ni) -
 			sizeof(*stats);
 
-		return lnet_get_ni_config(cfg_ni, tun, stats, tun_size);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_ni_config(cfg_ni, tun, stats, tun_size);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_GET_NET: {
@@ -2697,7 +2706,10 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (config->cfg_hdr.ioc_len < total)
 			return -EINVAL;
 
-		return lnet_get_net_config(config);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_net_config(config);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_GET_LNET_STATS:
@@ -2707,7 +2719,9 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (lnet_stats->st_hdr.ioc_len < sizeof(*lnet_stats))
 			return -EINVAL;
 
+		mutex_lock(&the_lnet.ln_api_mutex);
 		lnet_counters_get(&lnet_stats->st_cntrs);
+		mutex_unlock(&the_lnet.ln_api_mutex);
 		return 0;
 	}
 
@@ -2748,7 +2762,9 @@ LNetCtl(unsigned int cmd, void *arg)
 		numa = arg;
 		if (numa->nr_hdr.ioc_len != sizeof(*numa))
 			return -EINVAL;
+		mutex_lock(&the_lnet.ln_api_mutex);
 		lnet_numa_range = numa->nr_range;
+		mutex_unlock(&the_lnet.ln_api_mutex);
 		return 0;
 	}
 
@@ -2771,7 +2787,11 @@ LNetCtl(unsigned int cmd, void *arg)
 			return -EINVAL;
 
 		pool_cfg = (struct lnet_ioctl_pool_cfg *)config->cfg_bulk;
-		return lnet_get_rtr_pool_cfg(config->cfg_count, pool_cfg);
+
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_rtr_pool_cfg(config->cfg_count, pool_cfg);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_ADD_PEER_NI: {
@@ -2780,9 +2800,13 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (cfg->prcfg_hdr.ioc_len < sizeof(*cfg))
 			return -EINVAL;
 
-		return lnet_add_peer_ni_to_peer(cfg->prcfg_key_nid,
-						cfg->prcfg_cfg_nid,
-						cfg->prcfg_mr);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		lnet_incr_dlc_seq();
+		rc = lnet_add_peer_ni_to_peer(cfg->prcfg_key_nid,
+					      cfg->prcfg_cfg_nid,
+					      cfg->prcfg_mr);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_DEL_PEER_NI: {
@@ -2791,8 +2815,12 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (cfg->prcfg_hdr.ioc_len < sizeof(*cfg))
 			return -EINVAL;
 
-		return lnet_del_peer_ni_from_peer(cfg->prcfg_key_nid,
-						  cfg->prcfg_cfg_nid);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		lnet_incr_dlc_seq();
+		rc = lnet_del_peer_ni_from_peer(cfg->prcfg_key_nid,
+						cfg->prcfg_cfg_nid);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_GET_PEER_INFO: {
@@ -2801,7 +2829,8 @@ LNetCtl(unsigned int cmd, void *arg)
 		if (peer_info->pr_hdr.ioc_len < sizeof(*peer_info))
 			return -EINVAL;
 
-		return lnet_get_peer_ni_info(
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_peer_ni_info(
 		   peer_info->pr_count,
 		   &peer_info->pr_nid,
 		   peer_info->pr_lnd_u.pr_peer_credits.cr_aliveness,
@@ -2812,6 +2841,8 @@ LNetCtl(unsigned int cmd, void *arg)
 		   &peer_info->pr_lnd_u.pr_peer_credits.cr_peer_rtr_credits,
 		   &peer_info->pr_lnd_u.pr_peer_credits.cr_peer_min_rtr_credits,
 		   &peer_info->pr_lnd_u.pr_peer_credits.cr_peer_tx_qnob);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_GET_PEER_NI: {
@@ -2828,9 +2859,12 @@ LNetCtl(unsigned int cmd, void *arg)
 		lpni_stats = (struct lnet_ioctl_element_stats *)
 			     (cfg->prcfg_bulk + sizeof(*lpni_cri));
 
-		return lnet_get_peer_info(cfg->prcfg_idx, &cfg->prcfg_key_nid,
-					  &cfg->prcfg_cfg_nid, lpni_cri,
-					  lpni_stats);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		rc = lnet_get_peer_info(cfg->prcfg_idx, &cfg->prcfg_key_nid,
+					&cfg->prcfg_cfg_nid, &cfg->prcfg_mr,
+					lpni_cri, lpni_stats);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return rc;
 	}
 
 	case IOC_LIBCFS_NOTIFY_ROUTER: {

lnet/lnet/router.c
@@ -302,6 +302,7 @@ lnet_add_route(__u32 net, __u32 hops, lnet_nid_t gateway,
 	lnet_remotenet_t	*rnet2;
 	lnet_route_t		*route;
 	lnet_ni_t		*ni;
+	struct lnet_peer_ni	*lpni;
 	int			add_route;
 	int			rc;
 
@@ -340,13 +341,14 @@ lnet_add_route(__u32 net, __u32 hops, lnet_nid_t gateway,
 
 	lnet_net_lock(LNET_LOCK_EX);
 
-	rc = lnet_nid2peerni_locked(&route->lr_gateway, gateway, LNET_LOCK_EX);
-	if (rc != 0) {
+	lpni = lnet_nid2peerni_locked(gateway, LNET_LOCK_EX);
+	if (IS_ERR(lpni)) {
 		lnet_net_unlock(LNET_LOCK_EX);
 
 		LIBCFS_FREE(route, sizeof(*route));
 		LIBCFS_FREE(rnet, sizeof(*rnet));
 
+		rc = PTR_ERR(lpni);
 		if (rc == -EHOSTUNREACH) /* gateway is not on a local net. */
 			return rc;	 /* ignore the route entry */
 		CERROR("Error %d creating route %s %d %s\n", rc,
@@ -354,7 +356,7 @@ lnet_add_route(__u32 net, __u32 hops, lnet_nid_t gateway,
 			libcfs_nid2str(gateway));
 		return rc;
 	}
-
+	route->lr_gateway = lpni;
 	LASSERT(!the_lnet.ln_shutdown);
 
 	rnet2 = lnet_find_rnet_locked(net);

lnet/utils/lnetconfig/liblnetconfig.c
@@ -169,6 +169,90 @@ void lustre_lnet_init_nw_descr(struct lnet_dlc_network_descr *nw_descr)
 	}
 }
 
+int lustre_lnet_parse_nids(char *nids, char **array, int size,
+			   char ***out_array)
+{
+	int num_nids = 0;
+	char *comma = nids, *cur, *entry;
+	char **new_array;
+	int i, len, start = 0, finish = 0;
+
+	if (nids == NULL || strlen(nids) == 0)
+		return size;
+
+	/* count the number or new nids, by counting the number of commas */
+	while (comma) {
+		comma = strchr(comma, ',');
+		if (comma) {
+			comma++;
+			num_nids++;
+		} else {
+			num_nids++;
+		}
+	}
+
+	/*
+	 * if the array is not NULL allocate a large enough array to house
+	 * the old and new entries
+	 */
+	new_array = calloc(sizeof(char*),
+			   (size > 0) ? size + num_nids : num_nids);
+
+	if (!new_array)
+		goto failed;
+
+	/* parse our the new nids and add them to the tail of the array */
+	comma = nids;
+	cur = nids;
+	start = (size > 0) ? size: 0;
+	finish = (size > 0) ? size + num_nids : num_nids;
+	for (i = start; i < finish; i++) {
+		comma = strchr(comma, ',');
+		if (!comma)
+			/*
+			 * the length of the string to be parsed out is
+			 * from cur to end of string. So it's good enough
+			 * to strlen(cur)
+			 */
+			len = strlen(cur) + 1;
+		else
+			/* length of the string is comma - cur */
+			len = (comma - cur) + 1;
+
+		entry = calloc(1, len);
+		if (!entry) {
+			finish = i > 0 ? i - 1: 0;
+			goto failed;
+		}
+		strncpy(entry, cur, len - 1);
+		entry[len] = '\0';
+		new_array[i] = entry;
+		if (comma) {
+			comma++;
+			cur = comma;
+		}
+	}
+
+	/* add the old entries in the array and delete the old array*/
+	for (i = 0; i < size; i++)
+		new_array[i] = array[i];
+
+	if (array)
+		free(array);
+
+	*out_array = new_array;
+
+	return finish;
+
+failed:
+	for (i = start; i < finish; i++)
+		free(new_array[i]);
+	if (new_array)
+		free(new_array);
+
+	return size;
+}
+
 /*
  * format expected:
  *	<intf>[<expr>], <intf>[<expr>],..
@@ -279,41 +363,33 @@ int lustre_lnet_config_ni_system(bool up, bool load_ni_from_mod,
 	return rc;
 }
 
-static lnet_nid_t *allocate_create_nid_array(char **nids, char *err_str)
+static lnet_nid_t *allocate_create_nid_array(char **nids, __u32 num_nids,
+					     char *err_str)
 {
 	lnet_nid_t *array = NULL;
-	int idx = 0;
+	__u32 i;
 
 	if (!nids) {
 		snprintf(err_str, LNET_MAX_STR_LEN, "no NIDs to add");
 		return NULL;
 	}
 
-	/* count the size of the array */
-	while (nids[idx] != NULL)
-		idx++;
-
-	array = calloc(sizeof(*array) * idx + 1, 1);
+	array = calloc(sizeof(*array) * num_nids, 1);
 	if (array == NULL) {
 		snprintf(err_str, LNET_MAX_STR_LEN, "out of memory");
 		return NULL;
 	}
 
-	idx = 0;
-	while (nids[idx] != NULL) {
-		array[idx] = libcfs_str2nid(nids[idx]);
-		if (array[idx] == LNET_NID_ANY) {
+	for (i = 0; i < num_nids; i++) {
+		array[i] = libcfs_str2nid(nids[i]);
+		if (array[i] == LNET_NID_ANY) {
 			free(array);
 			snprintf(err_str, LNET_MAX_STR_LEN,
 				 "bad NID: '%s'",
-				 nids[idx]);
+				 nids[i]);
 			return NULL;
 		}
-		idx++;
 	}
-
-	/* identify last entry */
-	array[idx] = LNET_NID_ANY;
 
 	return array;
 }
@@ -339,17 +415,18 @@ static int dispatch_peer_ni_cmd(lnet_nid_t knid, lnet_nid_t nid, __u32 cmd,
 	return rc;
 }
 
-int lustre_lnet_config_peer_nid(char *knid, char **nid, bool mr, int seq_no,
-				struct cYAML **err_rc)
+int lustre_lnet_config_peer_nid(char *knid, char **nid, int num_nids,
+				bool mr, int seq_no, struct cYAML **err_rc)
 {
 	struct lnet_ioctl_peer_cfg data;
 	lnet_nid_t key_nid = LNET_NID_ANY;
 	int rc = LUSTRE_CFG_RC_NO_ERR;
 	int idx = 0;
+	bool nid0_used = false;
 	char err_str[LNET_MAX_STR_LEN] = {0};
-	lnet_nid_t *nids = allocate_create_nid_array(nid, err_str);
+	lnet_nid_t *nids = allocate_create_nid_array(nid, num_nids, err_str);
 
-	if (knid != NULL) {
+	if (knid) {
 		key_nid = libcfs_str2nid(knid);
 		if (key_nid == LNET_NID_ANY) {
 			snprintf(err_str, sizeof(err_str),
@@ -358,7 +435,7 @@ int lustre_lnet_config_peer_nid(char *knid, char **nid, bool mr, int seq_no,
 			rc = LUSTRE_CFG_RC_MISSING_PARAM;
 			goto out;
 		}
-	} else if (nids[0] == LNET_NID_ANY) {
+	} else if (!nids || nids[0] == LNET_NID_ANY) {
 		snprintf(err_str, sizeof(err_str),
 			 "no NIDs provided for configuration");
 		rc = LUSTRE_CFG_RC_MISSING_PARAM;
@@ -371,14 +448,27 @@ int lustre_lnet_config_peer_nid(char *knid, char **nid, bool mr, int seq_no,
 
 	LIBCFS_IOC_INIT_V2(data, prcfg_hdr);
 	data.prcfg_mr = mr;
-	if (nids[0] == LNET_NID_ANY) {
-		rc = dispatch_peer_ni_cmd(LNET_NID_ANY, key_nid,
-					  IOC_LIBCFS_ADD_PEER_NI,
-					  &data, err_str, "add");
-		goto out;
+
+	/*
+	 * if key_nid is not specified use the first nid in the list of
+	 * nids provided as the key_nid. NOTE: on entering 'if' we must
+	 * have at least 1 NID
+	 */
+	if (key_nid == LNET_NID_ANY) {
+		nid0_used = true;
+		key_nid = nids[0];
 	}
 
-	while (nids[idx] != LNET_NID_ANY) {
+	/* Create the key_nid first */
+	rc = dispatch_peer_ni_cmd(key_nid, LNET_NID_ANY,
+				  IOC_LIBCFS_ADD_PEER_NI,
+				  &data, err_str, "add");
+
+	if (rc != 0)
+		goto out;
+
+	/* add the rest of the nids to the key nid if any are available */
+	for (idx = nid0_used ? 1 : 0 ; nids && idx < num_nids; idx++) {
 		/*
 		 * If key_nid is not provided then the first nid in the
 		 * list becomes the key_nid. First time round the loop use
@@ -391,11 +481,6 @@ int lustre_lnet_config_peer_nid(char *knid, char **nid, bool mr, int seq_no,
 
 		if (rc != 0)
 			goto out;
-
-		if (idx == 0 && key_nid == LNET_NID_ANY)
-			key_nid = nids[0];
-
-		idx++;
 	}
 
 out:
@@ -405,15 +490,15 @@ out:
 	return rc;
 }
 
-int lustre_lnet_del_peer_nid(char *knid, char **nid, int seq_no,
-			     struct cYAML **err_rc)
+int lustre_lnet_del_peer_nid(char *knid, char **nid, int num_nids,
+			     int seq_no, struct cYAML **err_rc)
 {
 	struct lnet_ioctl_peer_cfg data;
 	lnet_nid_t key_nid;
 	int rc = LUSTRE_CFG_RC_NO_ERR;
 	int idx = 0;
 	char err_str[LNET_MAX_STR_LEN] = {0};
-	lnet_nid_t *nids = allocate_create_nid_array(nid, err_str);
+	lnet_nid_t *nids = allocate_create_nid_array(nid, num_nids, err_str);
 
 	if (knid == NULL) {
 		snprintf(err_str, sizeof(err_str),
@@ -434,22 +519,20 @@ int lustre_lnet_del_peer_nid(char *knid, char **nid, int seq_no,
 	snprintf(err_str, sizeof(err_str), "\"Success\"");
 
 	LIBCFS_IOC_INIT_V2(data, prcfg_hdr);
-	if (nids[0] == LNET_NID_ANY) {
+	if (!nids || nids[0] == LNET_NID_ANY) {
 		rc = dispatch_peer_ni_cmd(key_nid, LNET_NID_ANY,
 					  IOC_LIBCFS_DEL_PEER_NI,
 					  &data, err_str, "del");
 		goto out;
 	}
 
-	while (nids[idx] != LNET_NID_ANY) {
+	for (idx = 0; nids && idx < num_nids; idx++) {
 		rc = dispatch_peer_ni_cmd(key_nid, nids[idx],
 					  IOC_LIBCFS_DEL_PEER_NI, &data,
 					  err_str, "del");
 
 		if (rc != 0)
 			goto out;
-
-		idx++;
 	}
 
 out:
@@ -1906,8 +1989,8 @@ out:
 	return rc;
 }
 
-int lustre_lnet_show_peer(char *knid, int seq_no, struct cYAML **show_rc,
-			  struct cYAML **err_rc)
+int lustre_lnet_show_peer(char *knid, int detail, int seq_no,
+			  struct cYAML **show_rc, struct cYAML **err_rc)
 {
 	struct lnet_ioctl_peer_cfg *peer_info;
 	struct lnet_peer_ni_credit_info *lpni_cri;
@@ -1981,6 +2064,11 @@ int lustre_lnet_show_peer(char *knid, int seq_no, struct cYAML **show_rc,
 							libcfs_nid2str(pnid))
 				    == NULL)
 					goto out;
+				if (cYAML_create_string(peer, "Multi-Rail",
+							peer_info->prcfg_mr ?
+							"True" : "False")
+				    == NULL)
+					goto out;
 				tmp = cYAML_create_seq(peer, "peer ni");
 				if (tmp == NULL)
 					goto out;
@@ -2005,9 +2093,8 @@ int lustre_lnet_show_peer(char *knid, int seq_no, struct cYAML **show_rc,
 			    == NULL)
 				goto out;
 
-			if (cYAML_create_number(peer_ni, "refcount",
-						lpni_cri->cr_refcount) == NULL)
-				goto out;
+			if (!detail)
+				continue;
 
 			if (cYAML_create_number(peer_ni, "max_ni_tx_credits",
 						lpni_cri->cr_ni_peer_tx_credits)
@@ -2047,6 +2134,10 @@ int lustre_lnet_show_peer(char *knid, int seq_no, struct cYAML **show_rc,
 			if (cYAML_create_number(peer_ni, "drop_count",
 						lpni_stats->drop_count)
 			    == NULL)
+				goto out;
+
+			if (cYAML_create_number(peer_ni, "refcount",
+						lpni_cri->cr_refcount) == NULL)
 				goto out;
 		}
 
@@ -2282,10 +2373,11 @@ static void yaml_free_string_array(char **array, int num)
 
 	for (i = 0; i < num; i++) {
 		if (*sub_array != NULL)
-			free(sub_array);
+			free(*sub_array);
 		sub_array++;
 	}
-	free(array);
+	if (array)
+		free(array);
 }
 
 /*
@@ -2644,46 +2736,40 @@ static int handle_yaml_del_ni(struct cYAML *tree, struct cYAML **show_rc,
 
 static int yaml_copy_peer_nids(struct cYAML *tree, char ***nidsppp)
 {
-	struct cYAML *nids_entry = NULL, *child;
+	struct cYAML *nids_entry = NULL, *child = NULL, *entry = NULL;
 	char **nids = NULL;
 	int num = 0, rc = LUSTRE_CFG_RC_NO_ERR;
 
-	nids_entry = cYAML_get_object_item(tree, "nids");
-	if (nids_entry != NULL) {
-		/* count */
-		child = nids_entry->cy_child;
-		while (child != NULL) {
+	nids_entry = cYAML_get_object_item(tree, "peer ni");
+	if (cYAML_is_sequence(nids_entry)) {
+		while (cYAML_get_next_seq_item(nids_entry, &child))
 			num++;
-			child = child->cy_next;
-		}
-
-		if (num == 0)
-			return LUSTRE_CFG_RC_MISSING_PARAM;
-
-		nids = calloc(sizeof(*nids) * num, 1);
-		if (nids == NULL)
-			return LUSTRE_CFG_RC_OUT_OF_MEM;
-
-		/* now grab all the nids */
-		child = nids_entry->cy_child;
-		num = 0;
-		while (child != NULL) {
-			nids[num] = calloc(strlen(child->cy_valuestring) + 1,
-					   1);
-			if (nids[num] == NULL) {
-				rc = LUSTRE_CFG_RC_OUT_OF_MEM;
-				goto failed;
-			}
-			strncpy(nids[num], child->cy_valuestring,
-				strlen(child->cy_valuestring));
-			child = child->cy_next;
-			num++;
-		}
-		rc = num;
-	} else {
-		rc = LUSTRE_CFG_RC_MISSING_PARAM;
-		goto failed;
 	}
+
+	if (num == 0)
+		return LUSTRE_CFG_RC_MISSING_PARAM;
+
+	nids = calloc(sizeof(*nids) * num, 1);
+	if (nids == NULL)
+		return LUSTRE_CFG_RC_OUT_OF_MEM;
+
+	/* now grab all the nids */
+	num = 0;
+	child = NULL;
+	while (cYAML_get_next_seq_item(nids_entry, &child)) {
+		entry = cYAML_get_object_item(child, "nid");
+		if (!entry)
+			continue;
+		nids[num] = calloc(strlen(entry->cy_valuestring) + 1, 1);
+		if (!nids[num]) {
+			rc = LUSTRE_CFG_RC_OUT_OF_MEM;
+			goto failed;
+		}
+		strncpy(nids[num], entry->cy_valuestring,
+			strlen(entry->cy_valuestring));
+		num++;
+	}
+	rc = num;
 
 	*nidsppp = nids;
 	return rc;
@@ -2707,11 +2793,11 @@ static int handle_yaml_config_peer(struct cYAML *tree, struct cYAML **show_rc,
 		return num;
 
 	seq_no = cYAML_get_object_item(tree, "seq_no");
-	key_nid = cYAML_get_object_item(tree, "key_nid");
+	key_nid = cYAML_get_object_item(tree, "primary nid");
 	non_mr = cYAML_get_object_item(tree, "non_mr");
 
 	rc = lustre_lnet_config_peer_nid((key_nid) ? key_nid->cy_valuestring : NULL,
-					 nids,
+					 nids, num,
 					 (non_mr) ? false : true,
 					 (seq_no) ? seq_no->cy_valueint : -1,
 					 err_rc);
@@ -2732,10 +2818,10 @@ static int handle_yaml_del_peer(struct cYAML *tree, struct cYAML **show_rc,
 		return num;
 
 	seq_no = cYAML_get_object_item(tree, "seq_no");
-	key_nid = cYAML_get_object_item(tree, "key_nid");
+	key_nid = cYAML_get_object_item(tree, "primary nid");
 
 	rc = lustre_lnet_del_peer_nid((key_nid) ? key_nid->cy_valuestring : NULL,
-				      nids,
+				      nids, num,
 				      (seq_no) ? seq_no->cy_valueint : -1,
 				      err_rc);
 
@@ -2870,12 +2956,14 @@ static int handle_yaml_show_routing(struct cYAML *tree, struct cYAML **show_rc,
 static int handle_yaml_show_credits(struct cYAML *tree, struct cYAML **show_rc,
 				    struct cYAML **err_rc)
 {
-	struct cYAML *seq_no, *key_nid;
+	struct cYAML *seq_no, *key_nid, *detail;
 
 	seq_no = cYAML_get_object_item(tree, "seq_no");
+	detail = cYAML_get_object_item(tree, "detail");
 	key_nid = cYAML_get_object_item(tree, "key_nid");
 
 	return lustre_lnet_show_peer((key_nid) ? key_nid->cy_valuestring : NULL,
+				     (detail) ? detail->cy_valueint : 0,
 				     (seq_no) ? seq_no->cy_valueint : -1,
 				     show_rc, err_rc);
 }
@@ -2891,6 +2979,41 @@ static int handle_yaml_show_stats(struct cYAML *tree, struct cYAML **show_rc,
 				      show_rc, err_rc);
 }
 
+static int handle_yaml_config_numa(struct cYAML *tree, struct cYAML **show_rc,
+				  struct cYAML **err_rc)
+{
+	struct cYAML *seq_no, *range;
+
+	seq_no = cYAML_get_object_item(tree, "seq_no");
+	range = cYAML_get_object_item(tree, "range");
+
+	return lustre_lnet_config_numa_range(range ? range->cy_valueint : -1,
+					     seq_no ? seq_no->cy_valueint : -1,
+					     err_rc);
+}
+
+static int handle_yaml_del_numa(struct cYAML *tree, struct cYAML **show_rc,
+			       struct cYAML **err_rc)
+{
+	struct cYAML *seq_no;
+
+	seq_no = cYAML_get_object_item(tree, "seq_no");
+
+	return lustre_lnet_config_numa_range(0, seq_no ? seq_no->cy_valueint : -1,
+					     err_rc);
+}
+
+static int handle_yaml_show_numa(struct cYAML *tree, struct cYAML **show_rc,
+				struct cYAML **err_rc)
+{
+	struct cYAML *seq_no;
+
+	seq_no = cYAML_get_object_item(tree, "seq_no");
+
+	return lustre_lnet_show_numa_range(seq_no ? seq_no->cy_valueint : -1,
+					   show_rc, err_rc);
+}
+
 struct lookup_cmd_hdlr_tbl {
 	char *name;
 	cmd_handler_t cb;
@@ -2903,6 +3026,7 @@ static struct lookup_cmd_hdlr_tbl lookup_config_tbl[] = {
 	{"peer", handle_yaml_config_peer},
 	{"routing", handle_yaml_config_routing},
 	{"buffers", handle_yaml_config_buffers},
+	{"numa", handle_yaml_config_numa},
 	{NULL, NULL}
 };
 
@@ -2911,6 +3035,7 @@ static struct lookup_cmd_hdlr_tbl lookup_del_tbl[] = {
 	{"net", handle_yaml_del_ni},
 	{"peer", handle_yaml_del_peer},
 	{"routing", handle_yaml_del_routing},
+	{"numa", handle_yaml_del_numa},
 	{NULL, NULL}
 };
 
@@ -2921,6 +3046,7 @@ static struct lookup_cmd_hdlr_tbl lookup_show_tbl[] = {
 	{"routing", handle_yaml_show_routing},
 	{"credits", handle_yaml_show_credits},
 	{"statistics", handle_yaml_show_stats},
+	{"numa", handle_yaml_show_numa},
 	{NULL, NULL}
 };
 

commit cce82630cbf2c7badbbdd16a8ca9c8c0065ded13

LU-14962 lnet: Check for -ESHUTDOWN in lnet_parse
The fix for LU-8106, http://review.whamcloud.com/19993, no longer
works because rc does not have the return value from
lnet_nid2peerni_locked(). Use PTR_ERR to get the return value and
restore the LU-8106 fix.

HPE-bug-id: LUS-10333
Fixes: fa8b4e6357 ("LU-7734 lnet: peer/peer_ni handling adjustments")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I9cc2bc2d6e675d38cf06d99c524bdd95110bf0e9
Reviewed-on: https://review.whamcloud.com/44743
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/lib-move.c
@@ -4659,10 +4659,10 @@ lnet_parse(struct lnet_ni *ni, struct lnet_hdr *hdr, lnet_nid_t from_nid,
 				      cpt);
 	if (IS_ERR(lpni)) {
 		lnet_net_unlock(cpt);
-		CERROR("%s, src %s: Dropping %s "
-		       "(error %ld looking up sender)\n",
+		rc = PTR_ERR(lpni);
+		CERROR("%s, src %s: Dropping %s (error %d looking up sender)\n",
 		       libcfs_nid2str(from_nid), libcfs_nid2str(src_nid),
-		       lnet_msgtyp2str(type), PTR_ERR(lpni));
+		       lnet_msgtyp2str(type), rc);
 		lnet_msg_free(msg);
 		if (rc == -ESHUTDOWN)
 			/* We are shutting down.  Don't do anything more */