lustre/lustre-release

commit d62efba975d27d98f2d50f14d49dda53d2355d0c

LU-8837 utils: make tools lightweight for lustre clients
Currently the lustre tools are built for the server target even
for the case of clients rpms. Rework the tools so server specific
code is not built for the client only case. The UAPI headers
lustre_log_user.h, lustre_barrier_user.h, and lustre_disk.h are
server specific and are not available in the upstream client.
So make our tools not to depend on those headers in the client
case. With this change it is simple step to make our tools
buildable against upstream. Last move user land specific code
from lustre_disk.h to mount_utils.h. Don't install server UAPI
headers on client targets.

Change-Id: I6b2b780ac0e7338a085ee6a8747097f0097efca1
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/28315
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Tested-by: Jenkins
Reviewed-by: John L. Hammond <john.hammond@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -114,36 +114,9 @@ enum ldd_mount_type {
 	LDD_MT_LAST
 };
 
-/* On-disk configuration file. In host-endian order. */
-struct lustre_disk_data {
-	__u32 ldd_magic;
-	__u32 ldd_feature_compat;	/* compatible feature flags */
-	__u32 ldd_feature_rocompat;	/* read-only compatible feature flags */
-	__u32 ldd_feature_incompat;	/* incompatible feature flags */
-
-	__u32 ldd_config_ver;		/* config rewrite count - not used */
-	__u32 ldd_flags;		/* LDD_SV_TYPE */
-	__u32 ldd_svindex;		/* server index (0001), must match
-					 * svname
-					 */
-	__u32 ldd_mount_type;		/* target fs type LDD_MT_* */
-	char  ldd_fsname[64];		/* filesystem this server is part of,
-					 * MTI_NAME_MAXLEN
-					 */
-	char  ldd_svname[64];		/* this server's name (lustre-mdt0001)*/
-	__u8  ldd_uuid[40];		/* server UUID (COMPAT_146) */
-
-	char  ldd_userdata[1024 - 200];	/* arbitrary user string '200' */
-	__u8  ldd_padding[4096 - 1024];	/* 1024 */
-	char  ldd_mount_opts[4096];	/* target fs mount opts '4096' */
-	char  ldd_params[4096];		/* key=value pairs '8192' */
-};
-
 /****************** last_rcvd file *********************/
 
 #define LR_EXPIRE_INTERVALS 16	/**< number of intervals to track transno */
-#define ENOENT_VERSION 1	/** 'virtual' version of non-existent object */
-
 #define LR_SERVER_SIZE	512
 #define LR_CLIENT_START	8192
 #define LR_CLIENT_SIZE	128

lustre/utils/mount_utils.h
@@ -55,8 +55,10 @@
 #include <sys/stat.h>
 
 #include <libcfs/util/list.h>
-#include <linux/lustre/lustre_disk.h>
 #include <linux/lustre/lustre_param.h>
+#ifdef HAVE_SERVER_SUPPORT
+#include <linux/lustre/lustre_disk.h>
+#endif
 
 extern char *progname;
 extern int verbose;
@@ -81,6 +83,32 @@ extern int failover;
 /* Maximum length of on-disk parameters in the form key=<value> */
 #define PARAM_MAX		4096
 
+#ifdef HAVE_SERVER_SUPPORT
+/* On-disk configuration file. In host-endian order. */
+struct lustre_disk_data {
+	__u32 ldd_magic;
+	__u32 ldd_feature_compat;	/* compatible feature flags */
+	__u32 ldd_feature_rocompat;	/* read-only compatible feature flags */
+	__u32 ldd_feature_incompat;	/* incompatible feature flags */
+
+	__u32 ldd_config_ver;		/* config rewrite count - not used */
+	__u32 ldd_flags;		/* LDD_SV_TYPE */
+	__u32 ldd_svindex;		/* server index (0001), must match
+					 * svname
+					 */
+	__u32 ldd_mount_type;		/* target fs type LDD_MT_* */
+	char  ldd_fsname[64];		/* filesystem this server is part of,
+					 * MTI_NAME_MAXLEN
+					 */
+	char  ldd_svname[64];		/* this server's name (lustre-mdt0001)*/
+	__u8  ldd_uuid[40];		/* server UUID (COMPAT_146) */
+
+	char  ldd_userdata[1024 - 200];	/* arbitrary user string '200' */
+	__u8  ldd_padding[4096 - 1024];	/* 1024 */
+	char  ldd_mount_opts[4096];	/* target fs mount opts '4096' */
+	char  ldd_params[4096];		/* key=value pairs '8192' */
+};
+
 /* used to describe the options to format the lustre disk, not persistent */
 struct mkfs_opts {
 	struct lustre_disk_data	mo_ldd; /* to be written in MOUNT_DATA_FILE */
@@ -94,10 +122,13 @@ struct mkfs_opts {
 	int	mo_flags;
 	int	mo_mgs_failnodes;
 };
+#endif
 
 /* used to describe the options to mount the lustre disk */
 struct mount_opts {
+#ifdef HAVE_SERVER_SUPPORT
 	struct lustre_disk_data	 mo_ldd;
+#endif
 	char	*mo_orig_options;
 	char	*mo_usource;		/* user-specified mount device */
 	char	*mo_source;		/* our mount device name */
@@ -115,6 +146,7 @@ struct mount_opts {
 	int	 mo_max_sectors_kb;
 };
 
+#ifdef HAVE_SERVER_SUPPORT
 int get_mountdata(char *, struct lustre_disk_data *);
 
 static inline char *mt_str(enum ldd_mount_type mt)
@@ -142,6 +174,7 @@ static inline char *mt_type(enum ldd_mount_type mt)
 	};
 	return mount_type_string[mt];
 }
+#endif /* HAVE_SERVER_SUPPORT */
 
 #define MT_STR(data)   mt_str((data)->ldd_mount_type)
 
@@ -168,6 +201,7 @@ int update_mtab_entry(char *spec, char *mtpt, char *type, char *opts,
 int update_utab_entry(struct mount_opts *mop);
 int check_mountfsoptions(char *mountopts, char *wanted_mountopts);
 void trim_mountfsoptions(char *s);
+#ifdef HAVE_SERVER_SUPPORT
 __u64 get_device_size(char* device);
 int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
 			 const char *oldname);
@@ -220,6 +254,8 @@ struct module_backfs_ops ldiskfs_ops;
 
 struct module_backfs_ops *load_backfs_module(enum ldd_mount_type mount_type);
 void unload_backfs_ops(struct module_backfs_ops *ops);
+#endif
+
 #ifdef HAVE_OPENSSL_SSK
 int load_shared_keys(struct mount_opts *mop);
 #else

lustre/include/uapi/linux/lustre/Makefile.am
@@ -31,20 +31,24 @@
 
 lustreincludedir = /usr/include/linux/lustre
 lustreinclude_HEADERS = \
-	lustre_barrier_user.h \
 	lustre_cfg.h \
-	lustre_disk.h \
 	lustre_fid.h \
 	lustre_fiemap.h \
 	lustre_idl.h \
 	lustre_ioctl.h \
 	lustre_kernelcomm.h \
-	lustre_lfsck_user.h \
-	lustre_log_user.h \
 	lustre_ostid.h \
 	lustre_param.h \
 	lustre_user.h \
 	lustre_ver.h
+
+if SERVER
+lustreinclude_HEADERS += \
+	lustre_barrier_user.h \
+	lustre_disk.h \
+	lustre_lfsck_user.h \
+	lustre_log_user.h
+endif
 
 EXTRA_DIST = \
 	lustre_barrier_user.h \

lustre/mdt/mdt_internal.h
@@ -264,6 +264,8 @@ struct mdt_device {
 #define MDT_SERVICE_WATCHDOG_FACTOR	(2)
 #define MDT_COS_DEFAULT         (0)
 
+#define ENOENT_VERSION 1	/** 'virtual' version of non-existent object */
+
 struct mdt_object {
 	struct lu_object_header	mot_header;
 	struct lu_object	mot_obj;

lustre/utils/Makefile.am
@@ -51,7 +51,10 @@ endif
 noinst_LIBRARIES = liblustreapitmp.a
 endif # UTILS
 
-lctl_SOURCES = lustre_lfsck.c portals.c debug.c obd.c lustre_cfg.c lctl.c obdctl.h lsnapshot.c
+lctl_SOURCES = portals.c debug.c obd.c lustre_cfg.c lctl.c obdctl.h
+if SERVER
+lctl_SOURCES += lustre_lfsck.c lsnapshot.c
+endif
 lctl_LDADD :=  liblustreapi.a $(LIBCFS) $(LIBREADLINE) $(PTHREAD_LIBS)
 lctl_DEPENDENCIES := $(LIBCFS) liblustreapi.a
 

lustre/utils/lctl.c
@@ -245,6 +245,7 @@ command_t cmdlist[] = {
 	 "list pools and pools members\n"
 	 "usage: pool_list  <fsname>[.<poolname>] | <pathname>"},
 
+#ifdef HAVE_SERVER_SUPPORT
 	/* Barrier commands */
 	{"===  Barrier ==", NULL, 0, "barrier management"},
 	{"barrier_freeze", jt_barrier_freeze, 0,
@@ -297,7 +298,7 @@ command_t cmdlist[] = {
 	 "usage: snapshot_umount <-F | --fsname fsname> [-h | --help]\n"
 	 "			 <-n | --name ssname>\n"
 	 "			 [-r | --rsh remote_shell]"},
-
+#endif /* HAVE_SERVER_SUPPORT */
 	/* Nodemap commands */
 	{"=== Nodemap ===", NULL, 0, "nodemap management"},
 	{"nodemap_activate", jt_nodemap_activate, 0,
@@ -451,7 +452,7 @@ command_t cmdlist[] = {
 	 "get the version of an object on servers\n"
 	 "usage: getobjversion <fid>\n"
 	 "	 getobjversion -i <id> -g <group>"},
-
+#ifdef HAVE_SERVER_SUPPORT
 	/* LFSCK commands */
 	{"==== LFSCK ====", NULL, 0, "LFSCK"},
 	{"lfsck_start", jt_lfsck_start, 0, "start LFSCK\n"
@@ -471,7 +472,7 @@ command_t cmdlist[] = {
 	 "usage: lfsck_query [-M | --device MDT_device] [-h | --help]\n"
 	 "		     [-t | --type lfsck_type[,lfsck_type...]]\n"
 	 "		     [-w | --wait]"},
-
+#endif /* HAVE_SERVER_SUPPORT */
 	{"==== obsolete (DANGEROUS) ====", NULL, 0, "obsolete (DANGEROUS)"},
 	/* some test scripts still use these */
 	{"cfg_device", jt_obd_device, 0,

lustre/utils/mount_lustre.c
@@ -347,6 +347,7 @@ out_options:
 	return rc;
 }
 
+#ifdef HAVE_SERVER_SUPPORT
 /* Add mgsnids from ldd params */
 static int add_mgsnids(struct mount_opts *mop, char *options,
 		       const char *params, size_t options_len)
@@ -580,6 +581,7 @@ static int parse_ldd(char *source, struct mount_opts *mop,
 
 	return rc;
 }
+#endif /* HAVE_SERVER_SUPPORT */
 
 static void set_defaults(struct mount_opts *mop)
 {
@@ -701,6 +703,7 @@ static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
 	return 0;
 }
 
+#ifdef HAVE_SERVER_SUPPORT
 /* change label from <fsname>:<index> to
  * <fsname>-<index> to indicate the device has
  * been registered. only if the label is
@@ -729,6 +732,7 @@ static void label_lustre(struct mount_opts *mop)
 		}
 	}
 }
+#endif /* HAVE_SERVER_SUPPORT */
 
 int main(int argc, char *const argv[])
 {
@@ -818,6 +822,7 @@ int main(int argc, char *const argv[])
 
 	client = (strstr(mop.mo_usource, ":/") != NULL);
 	if (!client) {
+#ifdef HAVE_SERVER_SUPPORT
 		rc = osd_init();
 		if (rc)
 			goto out_options;
@@ -825,6 +830,10 @@ int main(int argc, char *const argv[])
 		rc = parse_ldd(mop.mo_source, &mop, options, maxopt_len);
 		if (rc)
 			goto out_osd;
+#else
+		rc = -EINVAL;
+		goto out_options;
+#endif
 	}
 
 	/* In Linux 2.4, the target device doesn't get passed to any of our
@@ -837,13 +846,14 @@ int main(int argc, char *const argv[])
 		printf("mounting device %s at %s, flags=%#x options=%s\n",
 		       mop.mo_source, mop.mo_target, flags, options);
 
+#ifdef HAVE_SERVER_SUPPORT
 	if (!client && osd_tune_lustre(mop.mo_source, &mop)) {
 		if (verbose)
 			fprintf(stderr, "%s: unable to set tunables for %s"
 					" (may cause reduced IO performance)\n",
 					argv[0], mop.mo_source);
 	}
-
+#endif
 #ifdef HAVE_GSS
 	if (mop.mo_skpath[0] != '\0') {
 		/* Treat shared key failures as fatal */
@@ -864,10 +874,7 @@ int main(int argc, char *const argv[])
 		for (i = 0, rc = -EAGAIN; i <= mop.mo_retry && rc != 0; i++) {
 			rc = mount(mop.mo_source, mop.mo_target, "lustre",
 				   flags, (void *)options);
-			if (rc == 0) {
-				if (!client)
-					label_lustre(&mop);
-			} else {
+			if (rc != 0) {
                                 if (verbose) {
                                         fprintf(stderr, "%s: mount %s at %s "
                                                 "failed: %s retries left: "
@@ -884,6 +891,11 @@ int main(int argc, char *const argv[])
 				} else {
 					rc = errno;
 				}
+#ifdef HAVE_SERVER_SUPPORT
+			} else {
+				if (!client)
+					label_lustre(&mop);
+#endif
 			}
 		}
 	}
@@ -965,9 +977,10 @@ int main(int argc, char *const argv[])
 	}
 
 out_osd:
+#ifdef HAVE_SERVER_SUPPORT
 	if (!client)
 		osd_fini();
-
+#endif
 out_options:
 	free(options);
 

lustre/utils/mount_utils.c
@@ -68,7 +68,9 @@ extern int verbose;
 #define vprint(fmt, arg...) if (verbose > 0) printf(fmt, ##arg)
 #define verrprint(fmt, arg...) if (verbose >= 0) fprintf(stderr, fmt, ##arg)
 
+#ifdef HAVE_SERVER_SUPPORT
 static struct module_backfs_ops *backfs_ops[LDD_MT_LAST];
+#endif
 
 void fatal(void)
 {
@@ -117,6 +119,7 @@ int run_command(char *cmd, int cmdsz)
         return rc;
 }
 
+#ifdef HAVE_SERVER_SUPPORT
 int add_param(char *buf, char *key, char *val)
 {
 	int end = sizeof(((struct lustre_disk_data *)0)->ldd_params);
@@ -193,6 +196,7 @@ int append_param(char *buf, char *key, char *val, char sep)
 
 	return add_param(buf, key, str);
 }
+#endif
 
 char *strscat(char *dst, char *src, int buflen)
 {
@@ -395,6 +399,7 @@ void trim_mountfsoptions(char *s)
 		*p-- = '\0';
 }
 
+#ifdef HAVE_SERVER_SUPPORT
 /* Setup a file in the first unused loop_device */
 int loop_setup(struct mkfs_opts *mop)
 {
@@ -895,6 +900,7 @@ __u64 get_device_size(char* device)
 	/* return value in KB */
 	return size >> 10;
 }
+#endif
 
 int file_create(char *path, __u64 size)
 {
@@ -941,7 +947,7 @@ int file_create(char *path, __u64 size)
 	return 0;
 }
 
-
+#ifdef HAVE_SERVER_SUPPORT
 struct lustre_cfg_entry {
 	struct list_head lce_list;
 	char		 lce_name[0];
@@ -1130,6 +1136,7 @@ out:
 
 	return ret;
 }
+#endif /* HAVE_SERVER_SUPPORT */
 
 #ifdef HAVE_GSS
 #ifdef HAVE_OPENSSL_SSK

commit b5ddb7dab5fca41b493212002406034925770942

LU-8837 utils: move lustre_disk_data back to lustre_disk.h
This patch moves struct lustre_disk_data from mount_utils.h
back to lustre_disk.h so that it can be used in other codes
without including mount_utils.h.

Fixes: d62efba975d2 ("LU-8837 utils: make tools lightweight for lustre clients")
Change-Id: I589da2710e3cbe7d93a59928143f2b5cac955e6e
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/44829
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -70,6 +70,31 @@
 #define INDEX_BACKUP_DIR	"index_backup"
 #define MDT_ORPHAN_DIR		"PENDING"
 
+/* On-disk configuration file. In host-endian order. */
+struct lustre_disk_data {
+	__u32 ldd_magic;
+	__u32 ldd_feature_compat;	/* compatible feature flags */
+	__u32 ldd_feature_rocompat;	/* read-only compatible feature flags */
+	__u32 ldd_feature_incompat;	/* incompatible feature flags */
+
+	__u32 ldd_config_ver;		/* config rewrite count - not used */
+	__u32 ldd_flags;		/* LDD_SV_TYPE */
+	__u32 ldd_svindex;		/* server index (0001), must match
+					 * svname
+					 */
+	__u32 ldd_mount_type;		/* target fs type LDD_MT_* */
+	char  ldd_fsname[64];		/* filesystem this server is part of,
+					 * MTI_NAME_MAXLEN
+					 */
+	char  ldd_svname[64];		/* this server's name (lustre-mdt0001)*/
+	__u8  ldd_uuid[40];		/* server UUID (COMPAT_146) */
+
+	char  ldd_userdata[1024 - 200];	/* arbitrary user string '200' */
+	__u8  ldd_padding[4096 - 1024];	/* 1024 */
+	char  ldd_mount_opts[4096];	/* target fs mount opts '4096' */
+	char  ldd_params[4096];		/* key=value pairs '8192' */
+};
+
 /****************** persistent mount data *********************/
 
 #define LDD_F_SV_TYPE_MDT	0x0001

lustre/utils/mount_utils.h
@@ -83,31 +83,6 @@ extern int failover;
 #define PARAM_MAX		4096
 
 #ifdef HAVE_SERVER_SUPPORT
-/* On-disk configuration file. In host-endian order. */
-struct lustre_disk_data {
-	__u32 ldd_magic;
-	__u32 ldd_feature_compat;	/* compatible feature flags */
-	__u32 ldd_feature_rocompat;	/* read-only compatible feature flags */
-	__u32 ldd_feature_incompat;	/* incompatible feature flags */
-
-	__u32 ldd_config_ver;		/* config rewrite count - not used */
-	__u32 ldd_flags;		/* LDD_SV_TYPE */
-	__u32 ldd_svindex;		/* server index (0001), must match
-					 * svname
-					 */
-	__u32 ldd_mount_type;		/* target fs type LDD_MT_* */
-	char  ldd_fsname[64];		/* filesystem this server is part of,
-					 * MTI_NAME_MAXLEN
-					 */
-	char  ldd_svname[64];		/* this server's name (lustre-mdt0001)*/
-	__u8  ldd_uuid[40];		/* server UUID (COMPAT_146) */
-
-	char  ldd_userdata[1024 - 200];	/* arbitrary user string '200' */
-	__u8  ldd_padding[4096 - 1024];	/* 1024 */
-	char  ldd_mount_opts[4096];	/* target fs mount opts '4096' */
-	char  ldd_params[4096];		/* key=value pairs '8192' */
-};
-
 /* used to describe the options to format the lustre disk, not persistent */
 struct mkfs_opts {
 	struct lustre_disk_data	mo_ldd; /* to be written in MOUNT_DATA_FILE */