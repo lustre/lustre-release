lustre/lustre-release

commit 1158386ac9c6a638f791f62e47a7513b2322772c

LU-14629 sec: forbid file rename from enc to unencrypted dir
fscrypt allows renaming an encrypted file from an encrypted directory
into an unencrypted directory. But it leaves the file encrypted,
sitting in an unencrypted directory, which can lead to unexpected
issues.
So just prevent this kind of rename, and adapt sanity-sec test_47
accordingly.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I38e17caa4786c1c8d80a363a826a5aa298eb0980
Reviewed-on: https://review.whamcloud.com/43404
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -2804,6 +2804,15 @@ static int mdd_rename_sanity_check(const struct lu_env *env,
 	    (pattr->la_projid != tpattr->la_projid)))
 		RETURN(-EXDEV);
 
+	/* we prevent an encrypted file from being renamed
+	 * into an unencrypted dir
+	 */
+	if ((cattr->la_valid & LA_FLAGS &&
+	     cattr->la_flags & LUSTRE_ENCRYPT_FL) &&
+	    !(tpattr->la_valid & LA_FLAGS &&
+	      tpattr->la_flags & LUSTRE_ENCRYPT_FL))
+		RETURN(-EXDEV);
+
 	rc = mdd_may_delete(env, src_pobj, pattr, sobj, cattr, NULL, 1, 0);
 	if (rc)
 		RETURN(rc);

lustre/llite/namei.c
@@ -1874,6 +1874,11 @@ static int ll_rename(struct inode *src, struct dentry *src_dchild,
 #endif
 	if (err)
 		RETURN(err);
+	/* we prevent an encrypted file from being renamed
+	 * into an unencrypted dir
+	 */
+	if (IS_ENCRYPTED(src) && !IS_ENCRYPTED(tgt))
+		RETURN(-EXDEV);
 
 	if (src_dchild->d_inode)
 		mode = src_dchild->d_inode->i_mode;

lustre/tests/sanity-sec.sh
@@ -3404,8 +3404,9 @@ test_47() {
 		error "link from encrypted to unencrypted dir should succeed"
 	rm -f $tmpfile
 
-	mrename $testfile2 $tmpfile ||
-		error "rename from encrypted to unencrypted dir should succeed"
+	mrename $testfile2 $tmpfile &&
+		error "rename from encrypted to unencrypted dir should fail"
+	touch $tmpfile
 
 	dd if=/dev/zero of=$testfile bs=512K count=1
 	mkdir $DIR/$tdir/mydir

commit 3db8e31797535570e5f2e99f6c8471b49d395efe

LU-14629 sec: do not block rename of topmost encrypted dir
We intentionally forbid file and directory rename from encrypted to
unencrypted directory. But we must not block rename of the topmost
encrypted directory.

Fixes: 1158386ac9 ("LU-14629 sec: forbid file rename from enc to unencrypted dir")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I480a24b2b0327e1d9104f216da54720e4f351636
Reviewed-on: https://review.whamcloud.com/45054
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -2822,8 +2822,8 @@ static int mdd_rename_sanity_check(const struct lu_env *env,
 	/* we prevent an encrypted file from being renamed
 	 * into an unencrypted dir
 	 */
-	if ((cattr->la_valid & LA_FLAGS &&
-	     cattr->la_flags & LUSTRE_ENCRYPT_FL) &&
+	if ((pattr->la_valid & LA_FLAGS &&
+	     pattr->la_flags & LUSTRE_ENCRYPT_FL) &&
 	    !(tpattr->la_valid & LA_FLAGS &&
 	      tpattr->la_flags & LUSTRE_ENCRYPT_FL))
 		RETURN(-EXDEV);