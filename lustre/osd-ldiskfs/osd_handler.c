lustre/lustre-release

commit f5967b06aac5bb7c065e526358995ad127c47cb8

LU-14734 osd-ldiskfs: enable large_dir automatically
Enable the large_dir feature automatically at mount time for
filesystems that do not have it enabled already.  Otherwise,
the REMOTE_PARENT_DIR may overflow if there are many remote
entries created, or for object directories on very large OSTs.
It isn't really needed on a dedicated MGS filesystem.

Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I1c4ead26b09d60567ad12945d7b366b53475cebb
Reviewed-on: https://review.whamcloud.com/43931
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -7756,6 +7756,20 @@ static void osd_umount(const struct lu_env *env, struct osd_device *o)
 	EXIT;
 }
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 2, 53, 0)
+# ifndef LDISKFS_HAS_INCOMPAT_FEATURE
+/* Newer kernels provide the ldiskfs_set_feature_largedir() wrapper already,
+ * which calls ldiskfs_update_dynamic_rev() to update ancient filesystems.
+ * All ldiskfs filesystems are already v2, so it is a no-op and unnecessary.
+ * This avoids maintaining patches to export this otherwise-useless function.
+ */
+void ldiskfs_update_dynamic_rev(struct super_block *sb)
+{
+	/* do nothing */
+}
+# endif
+#endif
+
 static int osd_mount(const struct lu_env *env,
 		     struct osd_device *o, struct lustre_cfg *cfg)
 {
@@ -7900,6 +7914,7 @@ static int osd_mount(const struct lu_env *env,
 		GOTO(out_mnt, rc = -EINVAL);
 	}
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 2, 53, 0)
 #ifdef LDISKFS_MOUNT_DIRDATA
 	if (ldiskfs_has_feature_dirdata(o->od_mnt->mnt_sb))
 		LDISKFS_SB(osd_sb(o))->s_mount_opt |= LDISKFS_MOUNT_DIRDATA;
@@ -7908,6 +7923,15 @@ static int osd_mount(const struct lu_env *env,
 		      "enabling the dirdata feature. If you do not want to "
 		      "downgrade to Lustre-1.x again, you can enable it via "
 		      "'tune2fs -O dirdata device'\n", name, dev);
+#endif
+	/* enable large_dir on MDTs to avoid REMOTE_PARENT_DIR overflow,
+	 * and on very large OSTs to avoid object directory overflow */
+	if (unlikely(!ldiskfs_has_feature_largedir(o->od_mnt->mnt_sb) &&
+		     !strstr(name, "MGS"))) {
+		ldiskfs_set_feature_largedir(o->od_mnt->mnt_sb);
+		LCONSOLE_INFO("%s: enable 'large_dir' feature on device '%s'\n",
+			      name, dev);
+	}
 #endif
 	inode = osd_sb(o)->s_root->d_inode;
 	lu_local_obj_fid(fid, OSD_FS_ROOT_OID);

lustre/osd-ldiskfs/osd_internal.h
@@ -1549,6 +1549,12 @@ osd_index_backup(const struct lu_env *env, struct osd_device *osd, bool backup)
 # define ldiskfs_has_feature_dirdata(sb) \
 	LDISKFS_HAS_INCOMPAT_FEATURE(sb, LDISKFS_FEATURE_INCOMPAT_DIRDATA)
 # endif
+# ifdef LDISKFS_FEATURE_INCOMPAT_LARGEDIR
+# define ldiskfs_has_feature_largedir(sb) \
+	LDISKFS_HAS_INCOMPAT_FEATURE(sb, LDISKFS_FEATURE_INCOMPAT_LARGEDIR)
+# define ldiskfs_set_feature_largedir(sb) \
+	LDISKFS_SET_INCOMPAT_FEATURE(sb, LDISKFS_FEATURE_INCOMPAT_LARGEDIR)
+# endif
 # ifdef LDISKFS_FEATURE_COMPAT_HAS_JOURNAL
 # define ldiskfs_has_feature_journal(sb) \
 	LDISKFS_HAS_COMPAT_FEATURE(sb, LDISKFS_FEATURE_COMPAT_HAS_JOURNAL)

commit 2a24b6ec67da9224e1cb6226166cde3a9c95431d

LU-14734 ldiskfs: improve message for large_dir
Make it more clear that the large_dir feature has already been
enabled, rather than making the admin think that they need to
enable the feature themselves.

Test-Parameters: trivial
Fixes: f5967b06aac5 ("LU-14734 osd-ldiskfs: enable large_dir automatically")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ica59d3370148ed277d3541c05be065c4638daf8d
Reviewed-on: https://review.whamcloud.com/45046
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -8036,7 +8036,7 @@ static int osd_mount(const struct lu_env *env,
 	if (unlikely(!ldiskfs_has_feature_largedir(o->od_mnt->mnt_sb) &&
 		     !strstr(name, "MGS"))) {
 		ldiskfs_set_feature_largedir(o->od_mnt->mnt_sb);
-		LCONSOLE_INFO("%s: enable 'large_dir' feature on device '%s'\n",
+		LCONSOLE_INFO("%s: enabled 'large_dir' feature on device %s\n",
 			      name, dev);
 	}
 #endif