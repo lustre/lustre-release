lustre/lustre-release

commit 7df7347b7b188e7168e094304fd6d2d985f7f274

LU-12967 ofd: restore sync_on_lock_cancel tunable
The "ofd.*.sync_on_lock_cancel" tunable was inadvertently replaced
during procfs->sysfs changes in 2.12 with "sync_lock_cancel".  Restore
the "sync_on_lock_cancel" tunable since it has existed since the 2.0
release and is definitely in use with several systems.

It isn't just a matter of restoring the old tunable name, since the
"mdt.*.sync_lock_cancel" name is also used since 2.8 and the code for
the two tunables was recently consolidated in the server target code.

Instead, keep the common "sync_lock_cancel" tunable name, add backward
compatibility for "sync_on_lock_cancel" for a number of releases, and
print a deprecation warning if the old name is used.

Fix up sanity.sh test_80 to check for both the old and new names,
but only if we actually need to change this tunable for ZFS, along
with minor test script style cleanups.

Fixes: 7059644e9ad3 ("LU-8066 ofd: migrate from proc to sysfs")
Change-Id: Iffe65f6268d94075c71b96d42fe60ef11ac39448
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/36748
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/ofd/lproc_ofd.c
@@ -409,8 +409,32 @@ ofd_brw_size_seq_write(struct file *file, const char __user *buffer,
 
 	return count;
 }
-
 LPROC_SEQ_FOPS(ofd_brw_size);
+
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 16, 53, 0)
+static bool sync_on_lock_cancel_warned;
+static ssize_t sync_on_lock_cancel_show(struct kobject *kobj,
+					struct attribute *attr, char *buf)
+{
+	if (!sync_on_lock_cancel_warned) {
+		sync_on_lock_cancel_warned = true;
+		pr_info("ofd: 'obdfilter.*.sync_on_lock_cancel' is deprecated, use 'obdfilter.*.sync_lock_cancel' instead\n");
+	}
+	return sync_lock_cancel_show(kobj, attr, buf);
+}
+
+static ssize_t sync_on_lock_cancel_store(struct kobject *kobj,
+					 struct attribute *attr,
+					 const char *buffer, size_t count)
+{
+	if (!sync_on_lock_cancel_warned) {
+		sync_on_lock_cancel_warned = true;
+		pr_info("ofd: 'obdfilter.*.sync_on_lock_cancel' is deprecated, use 'obdfilter.*.sync_lock_cancel' instead\n");
+	}
+	return sync_lock_cancel_store(kobj, attr, buffer, count);
+}
+LUSTRE_RW_ATTR(sync_on_lock_cancel);
+#endif
 
 /**
  * Show the limit of soft sync RPCs.
@@ -892,6 +916,9 @@ static struct attribute *ofd_attrs[] = {
 	&lustre_attr_fstype.attr,
 	&lustre_attr_no_precreate.attr,
 	&lustre_attr_sync_journal.attr,
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 16, 53, 0)
+	&lustre_attr_sync_on_lock_cancel.attr,
+#endif
 	&lustre_attr_soft_sync_limit.attr,
 	&lustre_attr_lfsck_speed_limit.attr,
 	&lustre_attr_job_cleanup_interval.attr,

lustre/include/lu_target.h
@@ -544,6 +544,12 @@ ssize_t grant_compat_disable_show(struct kobject *kobj, struct attribute *attr,
 ssize_t grant_compat_disable_store(struct kobject *kobj,
 				   struct attribute *attr,
 				   const char *buffer, size_t count);
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 16, 53, 0)
+ssize_t sync_lock_cancel_show(struct kobject *kobj,
+			      struct attribute *attr, char *buf);
+ssize_t sync_lock_cancel_store(struct kobject *kobj, struct attribute *attr,
+			       const char *buffer, size_t count);
+#endif
 
 /* FMD */
 void tgt_fmd_update(struct obd_export *exp, const struct lu_fid *fid,

lustre/include/obd.h
@@ -135,7 +135,7 @@ struct timeout_item {
 #define OSC_MAX_DIRTY_MB_MAX	2048     /* arbitrary, but < MAX_LONG bytes */
 #define OSC_DEFAULT_RESENDS	10
 
-/* possible values for fo_sync_lock_cancel */
+/* possible values for lut_sync_lock_cancel */
 enum {
         NEVER_SYNC_ON_CANCEL = 0,
         BLOCKING_SYNC_ON_CANCEL = 1,

lustre/target/tgt_main.c
@@ -53,8 +53,8 @@ static char *sync_on_cancel_states[] = {"never",
  * \retval		0 and buffer filled with data on success
  * \retval		negative value on error
  */
-static ssize_t sync_lock_cancel_show(struct kobject *kobj,
-				     struct attribute *attr, char *buf)
+ssize_t sync_lock_cancel_show(struct kobject *kobj,
+			      struct attribute *attr, char *buf)
 {
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
@@ -63,6 +63,7 @@ static ssize_t sync_lock_cancel_show(struct kobject *kobj,
 	return sprintf(buf, "%s\n",
 		       sync_on_cancel_states[tgt->lut_sync_lock_cancel]);
 }
+EXPORT_SYMBOL(sync_lock_cancel_show);
 
 /**
  * Change policy for handling dirty data under a lock being cancelled.
@@ -85,9 +86,8 @@ static ssize_t sync_lock_cancel_show(struct kobject *kobj,
  * \retval		\a count on success
  * \retval		negative value on error
  */
-static ssize_t sync_lock_cancel_store(struct kobject *kobj,
-				      struct attribute *attr,
-				      const char *buffer, size_t count)
+ssize_t sync_lock_cancel_store(struct kobject *kobj, struct attribute *attr,
+			       const char *buffer, size_t count)
 {
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
@@ -120,6 +120,7 @@ static ssize_t sync_lock_cancel_store(struct kobject *kobj,
 	spin_unlock(&tgt->lut_flags_lock);
 	return count;
 }
+EXPORT_SYMBOL(sync_lock_cancel_store);
 LUSTRE_RW_ATTR(sync_lock_cancel);
 
 /**

lustre/tests/sanity.sh
@@ -8586,31 +8586,35 @@ test_80() { # bug 10718
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 
 	# relax strong synchronous semantics for slow backends like ZFS
-	local soc="obdfilter.*.sync_on_lock_cancel"
-	local soc_old=$(do_facet ost1 lctl get_param -n $soc | head -n1)
-	local hosts=
-	if [ "$soc_old" != "never" ] &&
-		[ "$ost1_FSTYPE" != "ldiskfs" ]; then
-			hosts=$(for host in $(seq -f "ost%g" 1 $OSTCOUNT); do
-				facet_active_host $host; done | sort -u)
-			do_nodes $hosts lctl set_param $soc=never
+	if [ "$ost1_FSTYPE" != "ldiskfs" ]; then
+		local soc="obdfilter.*.sync_lock_cancel"
+		local save=$(do_facet ost1 $LCTL get_param -n $soc | head -n1)
+
+		# "sync_on_lock_cancel" was broken by v2_11_55_0-26-g7059644e9a
+		if [ -z "$save" ]; then
+			soc="obdfilter.*.sync_on_lock_cancel"
+			save=$(do_facet ost1 $LCTL get_param -n $soc | head -n1)
+		fi
+
+		if [ "$save" != "never" ]; then
+			local hosts=$(comma_list $(osts_nodes))
+
+			do_nodes $hosts $LCTL set_param $soc=never
+			stack_trap "do_nodes $hosts $LCTL set_param $soc=$save"
+		fi
 	fi
 
-        dd if=/dev/zero of=$DIR/$tfile bs=1M count=1 seek=1M
-        sync; sleep 1; sync
-        local BEFORE=`date +%s`
-        cancel_lru_locks osc
-        local AFTER=`date +%s`
-        local DIFF=$((AFTER-BEFORE))
-        if [ $DIFF -gt 1 ] ; then
-                error "elapsed for 1M@1T = $DIFF"
-        fi
+	dd if=/dev/zero of=$DIR/$tfile bs=1M count=1 seek=1M
+	sync; sleep 1; sync
+	local before=$(date +%s)
+	cancel_lru_locks osc
+	local after=$(date +%s)
+	local diff=$((after - before))
+	[ $diff -le 1 ] || error "elapsed for 1M@1T = $diff"
 
-        [ -n "$hosts" ] && do_nodes $hosts lctl set_param $soc=$soc_old
-
-        rm -f $DIR/$tfile
+	rm -f $DIR/$tfile
 }
-run_test 80 "Page eviction is equally fast at high offsets too  ===="
+run_test 80 "Page eviction is equally fast at high offsets too"
 
 test_81a() { # LU-456
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"

commit 3919a282ca4481f2bd2143b747ac1e46000c64d5

LU-15106 ofd: quiet deprecated param warning
There are a number of obdfilter parameter files that report a
warning even when they are read, which is confusing for users
if there is a tool that is scraping all available parameters:

    # lctl get_param obdfilter.*.*
    ofd: 'obdfilter.*.read_cache_enabled' is deprecated,
         use 'osd-*.read_cache_enabled' instead
    ofd: 'obdfilter.*.readcache_max_filesize' is deprecated,
         use 'osd-*.readcache_max_filesize' instead
    ofd: 'obdfilter.*.sync_on_lock_cancel' is deprecated,
         use 'obdfilter.*.sync_lock_cancel' instead
    ofd: 'obdfilter.*.writethrough_cache_enabled' is deprecated,
         use 'osd-*.writethrough_cache_enabled' instead

It should only print a message if the parameters are actually written.
Also fix the messages to reference the correct parameter names.

Most of these parameter links were added in 2.4 with the addition of
osd-ldiskfs.  However, the deprecation warnings were only added in
2.12.53 and slated for removal in 2.15, but were not backported to
2.12 LTS, and there hasn't been an LTS release since then, so it is
better bump removal so the upcoming 2.15 LTS release includes them.

Fix the test scripts to only use the new parameter names, to avoid
spurious warning messages.  We don't test interop with 2.3 anymore.

Test-Parameters: trivial
Fixes: 7df7347b7b18 ("LU-12967 ofd: restore sync_on_lock_cancel tunable")
Fixes: 493cd8088388 ("LU-8066 osd: migrate from proc to sysfs")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ie548e5b6af5463959fb4774e31996097373ebbe5
Reviewed-on: https://review.whamcloud.com/45246
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/lproc_ofd.c
@@ -510,14 +510,9 @@ LPROC_SEQ_FOPS_RO(ofd_checksum_type);
 
 
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 16, 53, 0)
-static bool sync_on_lock_cancel_warned;
 static ssize_t sync_on_lock_cancel_show(struct kobject *kobj,
 					struct attribute *attr, char *buf)
 {
-	if (!sync_on_lock_cancel_warned) {
-		sync_on_lock_cancel_warned = true;
-		pr_info("ofd: 'obdfilter.*.sync_on_lock_cancel' is deprecated, use 'obdfilter.*.sync_lock_cancel' instead\n");
-	}
 	return sync_lock_cancel_show(kobj, attr, buf);
 }
 
@@ -525,6 +520,8 @@ static ssize_t sync_on_lock_cancel_store(struct kobject *kobj,
 					 struct attribute *attr,
 					 const char *buffer, size_t count)
 {
+	static bool sync_on_lock_cancel_warned;
+
 	if (!sync_on_lock_cancel_warned) {
 		sync_on_lock_cancel_warned = true;
 		pr_info("ofd: 'obdfilter.*.sync_on_lock_cancel' is deprecated, use 'obdfilter.*.sync_lock_cancel' instead\n");
@@ -896,11 +893,7 @@ static ssize_t checksum_t10pi_enforce_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(checksum_t10pi_enforce);
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)
-static bool max_file_warned;
-static bool rd_cache_warned;
-static bool wr_cache_warned;
-
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 53, 0)
 static ssize_t read_cache_enable_show(struct kobject *kobj,
 				      struct attribute *attr,
 				      char *buf)
@@ -908,11 +901,6 @@ static ssize_t read_cache_enable_show(struct kobject *kobj,
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-
-	if (!rd_cache_warned) {
-		rd_cache_warned = true;
-		pr_info("ofd: 'obdfilter.*.read_cache_enabled' is deprecated, use 'osd-*.read_cache_enabled' instead\n");
-	}
 
 	if (!ofd->ofd_read_cache_enable)
 		return -EOPNOTSUPP;
@@ -928,10 +916,11 @@ static ssize_t read_cache_enable_store(struct kobject *kobj,
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+	static bool rd_cache_warned;
 
 	if (!rd_cache_warned) {
 		rd_cache_warned = true;
-		pr_info("ofd: 'obdfilter.*.read_cache_enabled' is deprecated, use 'osd-*.read_cache_enabled' instead\n");
+		pr_info("ofd: 'obdfilter.*.read_cache_enable' is deprecated, use 'osd-*.*.read_cache_enable' instead\n");
 	}
 
 	if (!ofd->ofd_read_cache_enable)
@@ -950,11 +939,6 @@ static ssize_t readcache_max_filesize_show(struct kobject *kobj,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
 
-	if (!max_file_warned) {
-		max_file_warned = true;
-		pr_info("ofd: 'obdfilter.*.readcache_max_filesize' is deprecated, use 'osd-*.readcache_max_filesize' instead\n");
-	}
-
 	if (!ofd->ofd_read_cache_max_filesize)
 		return -EOPNOTSUPP;
 
@@ -969,10 +953,11 @@ static ssize_t readcache_max_filesize_store(struct kobject *kobj,
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+	static bool max_file_warned;
 
 	if (!max_file_warned) {
 		max_file_warned = true;
-		pr_info("ofd: 'obdfilter.*.readcache_max_filesize' is deprecated, use 'osd-*.readcache_max_filesize' instead\n");
+		pr_info("ofd: 'obdfilter.*.readcache_max_filesize' is deprecated, use 'osd-*.*.readcache_max_filesize' instead\n");
 	}
 
 	if (!ofd->ofd_read_cache_max_filesize)
@@ -992,11 +977,6 @@ static ssize_t writethrough_cache_enable_show(struct kobject *kobj,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
 
-	if (!wr_cache_warned) {
-		wr_cache_warned = true;
-		pr_info("ofd: 'obdfilter.*.writethrough_cache_enabled' is deprecated, use 'osd-*.writethrough_cache_enabled' instead\n");
-	}
-
 	if (!ofd->ofd_write_cache_enable)
 		return -EOPNOTSUPP;
 
@@ -1011,6 +991,12 @@ static ssize_t writethrough_cache_enable_store(struct kobject *kobj,
 	struct obd_device *obd = container_of(kobj, struct obd_device,
 					      obd_kset.kobj);
 	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
+	static bool wr_cache_warned;
+
+	if (!wr_cache_warned) {
+		wr_cache_warned = true;
+		pr_info("ofd: 'obdfilter.*.writethrough_cache_enable' is deprecated, use 'osd-*.*.writethrough_cache_enable' instead\n");
+	}
 
 	if (!ofd->ofd_write_cache_enable)
 		return -EOPNOTSUPP;
@@ -1133,7 +1119,7 @@ static struct attribute *ofd_attrs[] = {
 	&lustre_attr_access_log_size.attr,
 	&lustre_attr_job_cleanup_interval.attr,
 	&lustre_attr_checksum_t10pi_enforce.attr,
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 53, 0)
 	&lustre_attr_read_cache_enable.attr,
 	&lustre_attr_readcache_max_filesize.attr,
 	&lustre_attr_writethrough_cache_enable.attr,

lustre/tests/test-framework.sh
@@ -1566,8 +1566,7 @@ get_osd_param() {
 	local device=${2:-$FSNAME-OST*}
 	local name=$3
 
-	do_nodes $nodes "$LCTL get_param -n obdfilter.$device.$name \
-		osd-*.$device.$name 2>&1" | grep -v 'error:'
+	do_nodes $nodes "$LCTL get_param -n osd-*.$device.$name"
 }
 
 set_osd_param() {
@@ -1576,8 +1575,7 @@ set_osd_param() {
 	local name=$3
 	local value=$4
 
-	do_nodes $nodes "$LCTL set_param -n obdfilter.$device.$name=$value \
-		osd-*.$device.$name=$value 2>&1" | grep -v 'error:'
+	do_nodes $nodes "$LCTL set_param -n osd-*.$device.$name=$value"
 }
 
 set_debug_size () {