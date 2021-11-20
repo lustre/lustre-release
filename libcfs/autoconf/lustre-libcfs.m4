lustre/lustre-release

commit f6df31a163806c38c734add346ed7cbfe427cce4

LU-10467 lustre: add wait_event macros suitable for upstream
This patch adds three sorts of wait_event macros.

1/ wait_event_idle_* which are available upstream, but not
   in older kernels.
   if TASK_NOLOAD is not available, we use TASK_UNINTERRUPTIBLE,
   and block all interrupts.

   We cannot use ___wait_cond_timeout() as it changed signature
   in 3.13. so we define our own ___wait_cond_timeout1().

2/ wait_event_idle_exclusive_lifo() and
   wait_event_idle_exclusive_lifo_timeout()
    which might be accepted upstream if we can make a strong case

   prepare_to_wait_event() doesn't support this directly, but
   as it won't relink a wait_entry that is already linked, it
   is sufficient to link to the head of the queue before calling
   prepare_to_wait_event().

3/ l_wait_event_abortable
   l_wait_event_abortable_timeout
   l_wait_event_abortable_exclusive
    which are unlikely to be accepted upstream, but match the general
    approach of upstream wait_event macros, and are useful
    to lustre.
    Possibly some or all of these should become wait_event_killable_*
    LUSTRE_FATAL_SIGS is moved over to linux-wait.h.

___wait_event() and related macros are copied from upstream linux,
and modified slightly to work across all supported kernels.

Test-Parameters: trivial
Signed-off-by: Mr NeilBrown <neilb@suse.com>
Change-Id: I2d260fc159dbe5b1a3cc7a26e4aeedf30150d85a
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Reviewed-on: https://review.whamcloud.com/35962
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/include/libcfs/linux/linux-wait.h
@@ -2,6 +2,8 @@
 #ifndef __LIBCFS_LINUX_WAIT_BIT_H
 #define __LIBCFS_LINUX_WAIT_BIT_H
 
+/* Make sure we can see if we have TASK_NOLOAD */
+#include <linux/sched.h>
 /*
  * Linux wait-bit related types and methods:
  */
@@ -30,6 +32,18 @@ struct wait_bit_queue_entry {
 extern long prepare_to_wait_event(wait_queue_head_t *wq_head,
 				  wait_queue_entry_t *wq_entry, int state);
 #endif
+
+/* ___wait_cond_timeout changed number of args in v3.12-rc1-78-g35a2af94c7ce
+ * so let's define our own ___wait_cond_timeout1
+ */
+
+#define ___wait_cond_timeout1(condition)				\
+({									\
+	bool __cond = (condition);					\
+	if (__cond && !__ret)						\
+		__ret = 1;						\
+	__cond || !__ret;						\
+})
 
 #ifndef HAVE_CLEAR_AND_WAKE_UP_BIT
 /**
@@ -110,7 +124,7 @@ do {									\
 })
 
 #define __wait_var_event_timeout(var, condition, timeout)		\
-	___wait_var_event(var, ___wait_cond_timeout(condition),		\
+	___wait_var_event(var, ___wait_cond_timeout1(condition),	\
 			  TASK_UNINTERRUPTIBLE, 0, timeout,		\
 			  __ret = schedule_timeout(__ret))
 
@@ -118,10 +132,423 @@ do {									\
 ({									\
 	long __ret = timeout;						\
 	might_sleep();							\
-	if (!___wait_cond_timeout(condition))				\
+	if (!___wait_cond_timeout1(condition))				\
 		__ret = __wait_var_event_timeout(var, condition, timeout); \
 	__ret;								\
 })
 #endif /* ! HAVE_WAIT_VAR_EVENT */
+
+/*
+ * prepare_to_wait_event() does not support an exclusive
+ * lifo wait.
+ * However it will not relink the wait_queue_entry if
+ * it is already linked.  So we link to the head of the
+ * queue here, and it will stay there.
+ */
+static inline void prepare_to_wait_exclusive_head(
+	wait_queue_head_t *waitq, wait_queue_entry_t *link)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&(waitq->lock), flags);
+#ifdef HAVE_WAIT_QUEUE_ENTRY_LIST
+	if (list_empty(&link->entry))
+#else
+	if (list_empty(&link->task_list))
+#endif
+		__add_wait_queue_exclusive(waitq, link);
+	spin_unlock_irqrestore(&((waitq)->lock), flags);
+}
+
+#ifndef ___wait_event
+/*
+ * The below macro ___wait_event() has an explicit shadow of the __ret
+ * variable when used from the wait_event_*() macros.
+ *
+ * This is so that both can use the ___wait_cond_timeout1() construct
+ * to wrap the condition.
+ *
+ * The type inconsistency of the wait_event_*() __ret variable is also
+ * on purpose; we use long where we can return timeout values and int
+ * otherwise.
+ */
+
+#define ___wait_event(wq_head, condition, state, exclusive, ret, cmd)	\
+({									\
+	__label__ __out;						\
+	wait_queue_entry_ __wq_entry;					\
+	long __ret = ret;	/* explicit shadow */			\
+									\
+	init_wait(&__wq_entry);						\
+	if (exclusive)							\
+		__wq_entry.flags = WQ_FLAG_EXCLUSIVE			\
+	for (;;) {							\
+		long __int = prepare_to_wait_event(&wq_head,		\
+						  &__wq_entry, state);	\
+									\
+		if (condition)						\
+			break;						\
+									\
+		if (___wait_is_interruptible(state) && __int) {		\
+			__ret = __int;					\
+			goto __out;					\
+		}							\
+									\
+		cmd;							\
+	}								\
+	finish_wait(&wq_head, &__wq_entry);				\
+__out:	__ret;								\
+})
+#endif
+
+#ifndef TASK_NOLOAD
+
+#define ___wait_event_idle(wq_head, condition, exclusive, ret, cmd)	\
+({									\
+	wait_queue_entry_t __wq_entry;					\
+	long __ret = ret;	/* explicit shadow */			\
+	sigset_t __blocked;						\
+									\
+	__blocked = cfs_block_sigsinv(0);				\
+	init_wait(&__wq_entry);						\
+	if (exclusive)							\
+		__wq_entry.flags = WQ_FLAG_EXCLUSIVE;			\
+	for (;;) {							\
+		prepare_to_wait_event(&wq_head,				\
+				   &__wq_entry,				\
+				   TASK_INTERRUPTIBLE);			\
+									\
+		if (condition)						\
+			break;						\
+		/* See justification in __l_wait_event */		\
+		if (signal_pending(current))				\
+			cfs_clear_sigpending();				\
+									\
+		cmd;							\
+	}								\
+	finish_wait(&wq_head, &__wq_entry);				\
+	cfs_restore_sigs(__blocked);					\
+	__ret;								\
+})
+
+#define wait_event_idle(wq_head, condition)				\
+do {									\
+	might_sleep();							\
+	if (!(condition))						\
+		___wait_event_idle(wq_head, condition, 0, 0, schedule());\
+} while (0)
+
+#define wait_event_idle_exclusive(wq_head, condition)			\
+do {									\
+	might_sleep();							\
+	if (!(condition))						\
+		___wait_event_idle(wq_head, condition, 1, 0, schedule());\
+} while (0)
+
+#define __wait_event_idle_exclusive_timeout(wq_head, condition, timeout)\
+	___wait_event_idle(wq_head, ___wait_cond_timeout1(condition),	\
+			   1, timeout,					\
+			   __ret = schedule_timeout(__ret))
+
+#define wait_event_idle_exclusive_timeout(wq_head, condition, timeout)	\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_exclusive_timeout(		\
+			wq_head, condition, timeout);			\
+	__ret;								\
+})
+
+#define __wait_event_idle_exclusive_timeout_cmd(wq_head, condition,	\
+						timeout, cmd1, cmd2)	\
+	___wait_event_idle(wq_head, ___wait_cond_timeout1(condition),	\
+			   1, timeout,					\
+			   cmd1; __ret = schedule_timeout(__ret); cmd2)
+
+#define wait_event_idle_exclusive_timeout_cmd(wq_head, condition, timeout,\
+					      cmd1, cmd2)		\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_exclusive_timeout_cmd(	\
+			wq_head, condition, timeout, cmd1, cmd2);	\
+	__ret;								\
+})
+
+#define __wait_event_idle_timeout(wq_head, condition, timeout)		\
+	___wait_event_idle(wq_head, ___wait_cond_timeout1(condition),	\
+			   0, timeout,					\
+			   __ret = schedule_timeout(__ret))
+
+#define wait_event_idle_timeout(wq_head, condition, timeout)		\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_timeout(wq_head, condition,	\
+						  timeout);		\
+	__ret;								\
+})
+
+#else /* TASK_IDLE */
+#ifndef wait_event_idle
+/**
+ * wait_event_idle - wait for a condition without contributing to system load
+ * @wq_head: the waitqueue to wait on
+ * @condition: a C expression for the event to wait for
+ *
+ * The process is put to sleep (TASK_IDLE) until the
+ * @condition evaluates to true.
+ * The @condition is checked each time the waitqueue @wq_head is woken up.
+ *
+ * wake_up() has to be called after changing any variable that could
+ * change the result of the wait condition.
+ *
+ */
+#define wait_event_idle(wq_head, condition)				\
+do {									\
+	might_sleep();							\
+	if (!(condition))						\
+		___wait_event(wq_head, condition, TASK_IDLE, 0, 0,	\
+			      schedule());				\
+} while (0)
+#endif
+#ifndef wait_event_idle_exclusive
+/**
+ * wait_event_idle_exclusive - wait for a condition without contributing to
+ *               system load
+ * @wq_head: the waitqueue to wait on
+ * @condition: a C expression for the event to wait for
+ *
+ * The process is put to sleep (TASK_IDLE) until the
+ * @condition evaluates to true.
+ * The @condition is checked each time the waitqueue @wq_head is woken up.
+ *
+ * The process is put on the wait queue with an WQ_FLAG_EXCLUSIVE flag
+ * set thus if other processes wait on the same list, when this
+ * process is woken further processes are not considered.
+ *
+ * wake_up() has to be called after changing any variable that could
+ * change the result of the wait condition.
+ *
+ */
+#define wait_event_idle_exclusive(wq_head, condition)			\
+do {									\
+	might_sleep();							\
+	if (!(condition))						\
+		___wait_event(wq_head, condition, TASK_IDLE, 1, 0,	\
+			      schedule());				\
+} while (0)
+#endif
+#ifndef wait_event_idle_exclusive_timeout
+/**
+ * wait_event_idle_exclusive_timeout - sleep without load until a condition
+ *                       becomes true or a timeout elapses
+ * @wq_head: the waitqueue to wait on
+ * @condition: a C expression for the event to wait for
+ * @timeout: timeout, in jiffies
+ *
+ * The process is put to sleep (TASK_IDLE) until the
+ * @condition evaluates to true. The @condition is checked each time
+ * the waitqueue @wq_head is woken up.
+ *
+ * The process is put on the wait queue with an WQ_FLAG_EXCLUSIVE flag
+ * set thus if other processes wait on the same list, when this
+ * process is woken further processes are not considered.
+ *
+ * wake_up() has to be called after changing any variable that could
+ * change the result of the wait condition.
+ *
+ * Returns:
+ * 0 if the @condition evaluated to %false after the @timeout elapsed,
+ * 1 if the @condition evaluated to %true after the @timeout elapsed,
+ * or the remaining jiffies (at least 1) if the @condition evaluated
+ * to %true before the @timeout elapsed.
+ */
+#define wait_event_idle_exclusive_timeout(wq_head, condition, timeout)	\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_exclusive_timeout(wq_head,	\
+							    condition,	\
+							    timeout);	\
+	__ret;								\
+})
+#endif
+#ifndef wait_event_idle_exclusive_timeout_cmd
+#define __wait_event_idle_exclusive_timeout_cmd(wq_head, condition,	\
+						timeout, cmd1, cmd2)	\
+	___wait_event(wq_head, ___wait_cond_timeout1(condition),	\
+		      TASK_IDLE, 1, timeout,				\
+		      cmd1; __ret = schedule_timeout(__ret); cmd2)
+
+#define wait_event_idle_exclusive_timeout_cmd(wq_head, condition, timeout,\
+					      cmd1, cmd2)		\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_exclusive_timeout_cmd(	\
+			wq_head, condition, timeout, cmd1, cmd2);	\
+	__ret;								\
+})
+#endif
+
+#ifndef wait_event_idle_timeout
+
+#define __wait_event_idle_timeout(wq_head, condition, timeout)		\
+	___wait_event(wq_head, ___wait_cond_timeout1(condition),	\
+		      TASK_IDLE, 0, timeout,				\
+		      __ret = schedule_timeout(__ret))
+
+/**
+ * wait_event_idle_timeout - sleep without load until a condition becomes
+ *                           true or a timeout elapses
+ * @wq_head: the waitqueue to wait on
+ * @condition: a C expression for the event to wait for
+ * @timeout: timeout, in jiffies
+ *
+ * The process is put to sleep (TASK_IDLE) until the
+ * @condition evaluates to true. The @condition is checked each time
+ * the waitqueue @wq_head is woken up.
+ *
+ * wake_up() has to be called after changing any variable that could
+ * change the result of the wait condition.
+ *
+ * Returns:
+ * 0 if the @condition evaluated to %false after the @timeout elapsed,
+ * 1 if the @condition evaluated to %true after the @timeout elapsed,
+ * or the remaining jiffies (at least 1) if the @condition evaluated
+ * to %true before the @timeout elapsed.
+ */
+#define wait_event_idle_timeout(wq_head, condition, timeout)		\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_timeout(wq_head, condition,	\
+						  timeout);		\
+	__ret;								\
+})
+#endif
+#endif /* TASK_IDLE */
+
+/* ___wait_event_lifo is used for lifo exclusive 'idle' waits */
+#ifdef TASK_NOLOAD
+
+#define ___wait_event_lifo(wq_head, condition, ret, cmd)		\
+({									\
+	wait_queue_entry_t	 __wq_entry;				\
+	long __ret = ret;	/* explicit shadow */			\
+									\
+	init_wait(&__wq_entry);						\
+	__wq_entry.flags =  WQ_FLAG_EXCLUSIVE;				\
+	for (;;) {							\
+		prepare_to_wait_exclusive_head(&wq_head, &__wq_entry);	\
+		prepare_to_wait_event(&wq_head, &__wq_entry, TASK_IDLE);\
+									\
+		if (condition)						\
+			break;						\
+									\
+		cmd;							\
+	}								\
+	finish_wait(&wq_head, &__wq_entry);				\
+	__ret;								\
+})
+#else
+#define ___wait_event_lifo(wq_head, condition, ret, cmd)		\
+({									\
+	wait_queue_entry_t __wq_entry;					\
+	long __ret = ret;	/* explicit shadow */			\
+	sigset_t __blocked;						\
+									\
+	__blocked = cfs_block_sigsinv(0);				\
+	init_wait(&__wq_entry);						\
+	__wq_entry.flags = WQ_FLAG_EXCLUSIVE;				\
+	for (;;) {							\
+		prepare_to_wait_exclusive_head(&wq_head, &__wq_entry);	\
+		prepare_to_wait_event(&wq_head, &__wq_entry,		\
+				      TASK_INTERRUPTIBLE);		\
+									\
+		if (condition)						\
+			break;						\
+		/* See justification in __l_wait_event */		\
+		if (signal_pending(current))				\
+			cfs_clear_sigpending();				\
+									\
+		cmd;							\
+	}								\
+	cfs_restore_sigs(__blocked);					\
+	finish_wait(&wq_head, &__wq_entry);				\
+	__ret;								\
+})
+#endif
+
+#define wait_event_idle_exclusive_lifo(wq_head, condition)		\
+do {									\
+	might_sleep();							\
+	if (!(condition))						\
+		___wait_event_lifo(wq_head, condition, 0, schedule());	\
+} while (0)
+
+#define __wait_event_idle_lifo_timeout(wq_head, condition, timeout)	\
+	___wait_event_lifo(wq_head, ___wait_cond_timeout1(condition),	\
+			   timeout,					\
+			   __ret = schedule_timeout(__ret))
+
+#define wait_event_idle_exclusive_lifo_timeout(wq_head, condition, timeout)\
+({									\
+	long __ret = timeout;						\
+	might_sleep();							\
+	if (!___wait_cond_timeout1(condition))				\
+		__ret = __wait_event_idle_lifo_timeout(wq_head,		\
+						       condition,	\
+						       timeout);	\
+	__ret;								\
+})
+
+/* l_wait_event_abortable() is a bit like wait_event_killable()
+ * except there is a fixed set of signals which will abort:
+ * LUSTRE_FATAL_SIGS
+ */
+#define LUSTRE_FATAL_SIGS					 \
+	(sigmask(SIGKILL) | sigmask(SIGINT) | sigmask(SIGTERM) | \
+	 sigmask(SIGQUIT) | sigmask(SIGALRM))
+
+#define l_wait_event_abortable(wq, condition)				\
+({									\
+	sigset_t __new_blocked, __old_blocked;				\
+	int __ret = 0;							\
+	siginitsetinv(&__new_blocked, LUSTRE_FATAL_SIGS);		\
+	sigprocmask(SIG_BLOCK, &__new_blocked, &__old_blocked);		\
+	__ret = wait_event_interruptible(wq, condition);		\
+	sigprocmask(SIG_SETMASK, &__old_blocked, NULL);			\
+	__ret;								\
+})
+
+#define l_wait_event_abortable_timeout(wq, condition, timeout)		\
+({									\
+	sigset_t __new_blocked, __old_blocked;				\
+	int __ret = 0;							\
+	siginitsetinv(&__new_blocked, LUSTRE_FATAL_SIGS);		\
+	sigprocmask(SIG_BLOCK, &__new_blocked, &__old_blocked);		\
+	__ret = wait_event_interruptible_timeout(wq, condition, timeout);\
+	sigprocmask(SIG_SETMASK, &__old_blocked, NULL);			\
+	__ret;								\
+})
+
+#define l_wait_event_abortable_exclusive(wq, condition)			\
+({									\
+	sigset_t __new_blocked, __old_blocked;				\
+	int __ret = 0;							\
+	siginitsetinv(&__new_blocked, LUSTRE_FATAL_SIGS);		\
+	sigprocmask(SIG_BLOCK, &__new_blocked, &__old_blocked);		\
+	__ret = wait_event_interruptible_exclusive(wq, condition);	\
+	sigprocmask(SIG_SETMASK, &__old_blocked, NULL);			\
+	__ret;								\
+})
 
 #endif /* __LICBFS_LINUX_WAIT_BIT_H */

lustre/include/lustre_lib.h
@@ -213,10 +213,6 @@ struct l_wait_info {
 
 #define LWI_INTR(cb, data)  LWI_TIMEOUT_INTR(0, NULL, cb, data)
 
-#define LUSTRE_FATAL_SIGS					 \
-	(sigmask(SIGKILL) | sigmask(SIGINT) | sigmask(SIGTERM) | \
-	 sigmask(SIGQUIT) | sigmask(SIGALRM))
-
 /**
  * wait_queue_t of Linux (version < 2.6.34) is a FIFO list for exclusively
  * waiting threads, which is not always desirable because all threads will

commit 1abd574b8203676951f676e979c14605752dd0b1

LU-14706 libcfs: ___wait_event() update for older kernels
A couple of wait issues fail to build correctly on older
3.0 kernels

Fixes: f6df31a163 ("LU-10467 lustre: add wait_event macros suitable for upstream")
Fixes: d05427a785 ("LU-10428 lnet: call event handlers without res_lock")
HPE-bug-id: LUS-9975
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I275fbf79c4472575d867075a3e3ebd3d6ec1cdfa
Reviewed-on: https://review.whamcloud.com/43785
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/autoconf/lustre-libcfs.m4
@@ -1017,6 +1017,30 @@ wait_queue_task_list, [
 ]) # LIBCFS_WAIT_QUEUE_TASK_LIST_RENAME
 
 #
+# LIBCFS_WAIT_BIT_QUEUE_ENTRY_EXISTS
+#
+# Kernel version v4.12-rc6-23-g76c85ddc4695
+# sched/wait: Standardize wait_bit_queue naming
+#
+# renamed struct wait_bit_queue  => wait_bit_queue_entry
+#
+AC_DEFUN([LIBCFS_WAIT_BIT_QUEUE_ENTRY_EXISTS], [
+LB_CHECK_COMPILE([if struct wait_bit_queue_entry exists],
+struct_wait_bit_queue_entry_exists, [
+	#include <linux/wait.h>
+	#if HAVE_WAIT_BIT_HEADER_H
+		#include <linux/wait_bit.h>
+	#endif
+],[
+	struct wait_bit_queue_entry entry;
+	memset(&entry, 0, sizeof(entry));
+],[
+	AC_DEFINE(HAVE_WAIT_BIT_QUEUE_ENTRY, 1,
+		[if struct wait_bit_queue_entry exists])
+])
+]) # LIBCFS_WAIT_BIT_QUEUE_ENTRY_EXISTS
+
+#
 # LIBCFS_NLA_STRDUP
 #
 # Kernel version 4.13-rc1 commit 2cf0c8b3e6942ecafe6ebb1a6d0328a81641bf39
@@ -1821,6 +1845,7 @@ LIBCFS_UUID_T
 # 4.13
 LIBCFS_NLA_STRDUP
 LIBCFS_WAIT_QUEUE_ENTRY
+LIBCFS_WAIT_BIT_QUEUE_ENTRY_EXISTS
 # 4.14
 LIBCFS_DEFINE_TIMER
 LIBCFS_NEW_KERNEL_WRITE

libcfs/include/libcfs/linux/linux-wait.h
@@ -190,12 +190,12 @@ static inline void prepare_to_wait_exclusive_head(
 #define ___wait_event(wq_head, condition, state, exclusive, ret, cmd)	\
 ({									\
 	__label__ __out;						\
-	wait_queue_entry_ __wq_entry;					\
+	wait_queue_entry_t __wq_entry;					\
 	long __ret = ret;	/* explicit shadow */			\
 									\
 	init_wait(&__wq_entry);						\
 	if (exclusive)							\
-		__wq_entry.flags = WQ_FLAG_EXCLUSIVE			\
+		__wq_entry.flags = WQ_FLAG_EXCLUSIVE;			\
 	for (;;) {							\
 		long __int = prepare_to_wait_event(&wq_head,		\
 						  &__wq_entry, state);	\

lnet/include/lnet/lib-lnet.h
@@ -264,7 +264,7 @@ lnet_ni_set_status(struct lnet_ni *ni, __u32 status)
 static inline void lnet_md_wait_handling(struct lnet_libmd *md, int cpt)
 {
 	wait_queue_head_t *wq = __var_waitqueue(md);
-#ifdef HAVE_WAIT_QUEUE_ENTRY
+#if defined(HAVE_WAIT_BIT_QUEUE_ENTRY) || !defined(HAVE_WAIT_VAR_EVENT)
 	struct wait_bit_queue_entry entry;
 	wait_queue_entry_t *wqe = &entry.wq_entry;
 #else