lustre/lustre-release

commit 48457868a02ae5a0407f304c9028f7e80e7fb574

LU-3606 fallocate: Implement fallocate preallocate operation
This patch adds fallocate(2) preallocate operation support
for Lustre. fallocate(2) method of the inode_operations or
file_operations is implemented and transported to the OSTs
to interface with the underlying OSD's fallocate(2) code.

In a saperate patch, a new RPC, OST_FALLOCATE has been
added and reserved for space preallocation. The fallocate
functionality (prealloc) in CLIO has been multiplexed
with CIT_SETATTR. (https://review.whamcloud.com/37277)

Lustre fsx(File system exerciser) is updated in a saperate
patch to handle fallocate calls. (https://review.whamcloud.com/37277)

Only fallocate preallocate operation is supported by this
patch for now. Other operations like, FALLOC_FL_PUNCH
(deallocate), FALLOC_FL_ZERO_RANGE, FALLOC_FL_COLLAPSE_RANGE
and FALLOC_FL_INSPECT_RANGE is not supported by this patch
and will be addressed by a separate patch.

ZFS operation is not supported by this patch. ZFS fallocate(2)
will be addressed by patch (https://review.whamcloud.com/36506/)

New test case under sanity is added to verify fallocate
call.

Test-Parameters: fstype=ldiskfs testlist=sanity,sanityn,sanity-dom
Signed-off-by: Swapnil Pimpale <spimpale@ddn.com>
Signed-off-by: Li Xi <lixi@ddn.com>
Signed-off-by: Abrarahmed Momin <abrar.momin@gmail.com>
Signed-off-by: Arshad Hussain <arshad.super@gmail.com>
Change-Id: I03f27d356616fbf3a3ab8e6309af26c00434d81b
Reviewed-on: https://review.whamcloud.com/9275
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/cl_object.h
@@ -1756,6 +1756,31 @@ struct cl_io_rw_common {
 	size_t	crw_count;
 	int	crw_nonblock;
 };
+enum cl_setattr_subtype {
+	/** regular setattr **/
+	CL_SETATTR_REG = 1,
+	/** truncate(2) **/
+	CL_SETATTR_TRUNC,
+	/** fallocate(2) - mode preallocate **/
+	CL_SETATTR_FALLOCATE
+};
+
+struct cl_io_range {
+	loff_t cir_pos;
+	size_t cir_count;
+};
+
+struct cl_io_pt {
+	struct cl_io_pt *cip_next;
+	struct kiocb cip_iocb;
+	struct iov_iter cip_iter;
+	struct file *cip_file;
+	enum cl_io_type cip_iot;
+	unsigned int cip_need_restart:1;
+	loff_t cip_pos;
+	size_t cip_count;
+	ssize_t cip_result;
+};
 
 /**
  * State for io.
@@ -1804,6 +1829,14 @@ struct cl_io {
 			int			 sa_stripe_index;
 			struct ost_layout	 sa_layout;
 			const struct lu_fid	*sa_parent_fid;
+			/* SETATTR interface is used for regular setattr, */
+			/* truncate(2) and fallocate(2) subtypes */
+			enum cl_setattr_subtype	 sa_subtype;
+			/* The following are used for fallocate(2) */
+			int			 sa_falloc_mode;
+			loff_t			 sa_falloc_offset;
+			loff_t			 sa_falloc_len;
+			loff_t			 sa_falloc_end;
 		} ci_setattr;
 		struct cl_data_version_io {
 			u64 dv_data_version;
@@ -2379,7 +2412,14 @@ static inline int cl_io_is_mkwrite(const struct cl_io *io)
 static inline int cl_io_is_trunc(const struct cl_io *io)
 {
 	return io->ci_type == CIT_SETATTR &&
-		(io->u.ci_setattr.sa_avalid & ATTR_SIZE);
+		(io->u.ci_setattr.sa_avalid & ATTR_SIZE) &&
+		(io->u.ci_setattr.sa_subtype != CL_SETATTR_FALLOCATE);
+}
+
+static inline int cl_io_is_fallocate(const struct cl_io *io)
+{
+	return (io->ci_type == CIT_SETATTR) &&
+	       (io->u.ci_setattr.sa_subtype == CL_SETATTR_FALLOCATE);
 }
 
 struct cl_io *cl_io_top(struct cl_io *io);

lustre/llite/file.c
@@ -43,6 +43,7 @@
 #include <linux/sched.h>
 #include <linux/user_namespace.h>
 #include <linux/uidgid.h>
+#include <linux/falloc.h>
 
 #include <uapi/linux/lustre/lustre_ioctl.h>
 #include <lustre_swab.h>
@@ -4780,6 +4781,84 @@ int ll_getattr(struct vfsmount *mnt, struct dentry *de, struct kstat *stat)
 	return ll_getattr_dentry(de, stat);
 }
 
+int cl_falloc(struct inode *inode, int mode, loff_t offset, loff_t len)
+{
+	struct lu_env *env;
+	struct cl_io *io;
+	__u16 refcheck;
+	int rc; loff_t sa_falloc_end;
+	loff_t size = i_size_read(inode);
+
+	ENTRY;
+
+	env = cl_env_get(&refcheck);
+	if (IS_ERR(env))
+		RETURN(PTR_ERR(env));
+
+	io = vvp_env_thread_io(env);
+	io->ci_obj = ll_i2info(inode)->lli_clob;
+	io->ci_verify_layout = 1;
+	io->u.ci_setattr.sa_parent_fid = lu_object_fid(&io->ci_obj->co_lu);
+	io->u.ci_setattr.sa_falloc_mode = mode;
+	io->u.ci_setattr.sa_falloc_offset = offset;
+	io->u.ci_setattr.sa_falloc_len = len;
+	io->u.ci_setattr.sa_falloc_end = io->u.ci_setattr.sa_falloc_offset +
+		io->u.ci_setattr.sa_falloc_len;
+	io->u.ci_setattr.sa_subtype = CL_SETATTR_FALLOCATE;
+	sa_falloc_end = io->u.ci_setattr.sa_falloc_end;
+	if (sa_falloc_end > size) {
+		/* Check new size against VFS/VM file size limit and rlimit */
+		rc = inode_newsize_ok(inode, sa_falloc_end);
+		if (rc)
+			goto out;
+		if (sa_falloc_end > ll_file_maxbytes(inode)) {
+			CDEBUG(D_INODE, "file size too large %llu > %llu\n",
+			       (unsigned long long)(sa_falloc_end),
+			       ll_file_maxbytes(inode));
+			rc = -EFBIG;
+			goto out;
+		}
+		io->u.ci_setattr.sa_attr.lvb_size = sa_falloc_end;
+		if (!(mode & FALLOC_FL_KEEP_SIZE))
+			io->u.ci_setattr.sa_avalid |= ATTR_SIZE;
+	} else {
+		io->u.ci_setattr.sa_attr.lvb_size = size;
+	}
+
+again:
+	if (cl_io_init(env, io, CIT_SETATTR, io->ci_obj) == 0)
+		rc = cl_io_loop(env, io);
+	else
+		rc = io->ci_result;
+
+	cl_io_fini(env, io);
+	if (unlikely(io->ci_need_restart))
+		goto again;
+
+out:
+	cl_env_put(env, &refcheck);
+	RETURN(rc);
+}
+
+long ll_fallocate(struct file *filp, int mode, loff_t offset, loff_t len)
+{
+	struct inode *inode = filp->f_path.dentry->d_inode;
+	int rc;
+
+	/*
+	 * Only mode == 0 (which is standard prealloc) is supported now.
+	 * Punch is not supported yet.
+	 */
+	if (mode & ~FALLOC_FL_KEEP_SIZE)
+		RETURN(-EOPNOTSUPP);
+
+	ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_FALLOCATE, 1);
+
+	rc = cl_falloc(inode, mode, offset, len);
+
+	RETURN(rc);
+}
+
 static int ll_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
 		     __u64 start, __u64 len)
 {
@@ -4902,19 +4981,22 @@ int ll_inode_permission(struct inode *inode, int mask)
 	cfs_cap_t cap;
 	bool squash_id = false;
 	ktime_t kstart = ktime_get();
+
 	ENTRY;
 
 	if (mask & MAY_NOT_BLOCK)
 		return -ECHILD;
 
-       /* as root inode are NOT getting validated in lookup operation,
-        * need to do it before permission check. */
+	/*
+	 * as root inode are NOT getting validated in lookup operation,
+	 * need to do it before permission check.
+	 */
 
-        if (inode == inode->i_sb->s_root->d_inode) {
+	if (inode == inode->i_sb->s_root->d_inode) {
 		rc = ll_inode_revalidate(inode->i_sb->s_root, IT_LOOKUP);
-                if (rc)
-                        RETURN(rc);
-        }
+		if (rc)
+			RETURN(rc);
+	}
 
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p), inode mode %x mask %o\n",
 	       PFID(ll_inode2fid(inode)), inode, inode->i_mode, mask);
@@ -4983,7 +5065,8 @@ struct file_operations ll_file_operations = {
 	.llseek		= ll_file_seek,
 	.splice_read	= ll_file_splice_read,
 	.fsync		= ll_fsync,
-	.flush		= ll_flush
+	.flush		= ll_flush,
+	.fallocate	= ll_fallocate,
 };
 
 struct file_operations ll_file_operations_flock = {
@@ -5009,7 +5092,8 @@ struct file_operations ll_file_operations_flock = {
 	.fsync		= ll_fsync,
 	.flush		= ll_flush,
 	.flock		= ll_file_flock,
-	.lock		= ll_file_flock
+	.lock		= ll_file_flock,
+	.fallocate	= ll_fallocate,
 };
 
 /* These are for -o noflock - to return ENOSYS on flock calls */
@@ -5036,7 +5120,8 @@ struct file_operations ll_file_operations_noflock = {
 	.fsync		= ll_fsync,
 	.flush		= ll_flush,
 	.flock		= ll_file_noflock,
-	.lock		= ll_file_noflock
+	.lock		= ll_file_noflock,
+	.fallocate	= ll_fallocate,
 };
 
 struct inode_operations ll_file_inode_operations = {

lustre/lov/lov_io.c
@@ -497,11 +497,16 @@ static int lov_io_slice_init(struct lov_io *lio,
 		break;
 
 	case CIT_SETATTR:
-		if (cl_io_is_trunc(io))
+		if (cl_io_is_fallocate(io)) {
+			lio->lis_pos = io->u.ci_setattr.sa_falloc_offset;
+			lio->lis_endpos = io->u.ci_setattr.sa_falloc_end;
+		} else if (cl_io_is_trunc(io)) {
 			lio->lis_pos = io->u.ci_setattr.sa_attr.lvb_size;
-		else
+			lio->lis_endpos = OBD_OBJECT_EOF;
+		} else {
 			lio->lis_pos = 0;
-		lio->lis_endpos = OBD_OBJECT_EOF;
+			lio->lis_endpos = OBD_OBJECT_EOF;
+		}
 		break;
 
 	case CIT_DATA_VERSION:
@@ -653,15 +658,24 @@ static void lov_io_sub_inherit(struct lov_io_sub *sub, struct lov_io *lio,
 			parent->u.ci_setattr.sa_attr_flags;
 		io->u.ci_setattr.sa_avalid = parent->u.ci_setattr.sa_avalid;
 		io->u.ci_setattr.sa_xvalid = parent->u.ci_setattr.sa_xvalid;
+		io->u.ci_setattr.sa_falloc_mode =
+			parent->u.ci_setattr.sa_falloc_mode;
 		io->u.ci_setattr.sa_stripe_index = stripe;
 		io->u.ci_setattr.sa_parent_fid =
 					parent->u.ci_setattr.sa_parent_fid;
+		/* For SETATTR(fallocate) pass the subtype to lower IO */
+		io->u.ci_setattr.sa_subtype = parent->u.ci_setattr.sa_subtype;
 		if (cl_io_is_trunc(io)) {
 			loff_t new_size = parent->u.ci_setattr.sa_attr.lvb_size;
 
 			new_size = lov_size_to_stripe(lsm, index, new_size,
 						      stripe);
 			io->u.ci_setattr.sa_attr.lvb_size = new_size;
+		} else if (cl_io_is_fallocate(io)) {
+			io->u.ci_setattr.sa_falloc_offset = start;
+			io->u.ci_setattr.sa_falloc_end = end;
+			io->u.ci_setattr.sa_attr.lvb_size =
+				parent->u.ci_setattr.sa_attr.lvb_size;
 		}
 		lov_lsm2layout(lsm, lsm->lsm_entries[index],
 			       &io->u.ci_setattr.sa_layout);
@@ -1534,8 +1548,11 @@ int lov_io_init_released(const struct lu_env *env, struct cl_object *obj,
 		 * - in open, for open O_TRUNC
 		 * - in setattr, for truncate
 		 */
-		/* the truncate is for size > 0 so triggers a restore */
-		if (cl_io_is_trunc(io)) {
+		/*
+		 * the truncate is for size > 0 so triggers a restore,
+		 * also trigger a restore for prealloc/punch
+		 */
+		if (cl_io_is_trunc(io) || cl_io_is_fallocate(io)) {
 			io->ci_restore_needed = 1;
 			result = -ENODATA;
 		} else

lustre/ofd/ofd_dev.c
@@ -77,6 +77,7 @@
 #include <lustre_quota.h>
 #include <lustre_nodemap.h>
 #include <lustre_log.h>
+#include <linux/falloc.h>
 
 #include "ofd_internal.h"
 
@@ -1929,6 +1930,114 @@ put:
 }
 
 /**
+ * OFD request handler for OST_FALLOCATE RPC.
+ *
+ * This is part of request processing. Validate request fields,
+ * preallocate the given OFD object and pack reply.
+ *
+ * \param[in] tsi	target session environment for this request
+ *
+ * \retval		0 if successful
+ * \retval		negative value on error
+ */
+static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
+{
+	struct obdo *oa = &tsi->tsi_ost_body->oa;
+	struct ost_body *repbody;
+	struct ofd_thread_info *info = tsi2ofd_info(tsi);
+	struct ldlm_namespace *ns = tsi->tsi_tgt->lut_obd->obd_namespace;
+	struct ldlm_resource *res;
+	struct ofd_object *fo;
+	__u64 flags = 0;
+	struct lustre_handle lh = { 0, };
+	int rc, mode;
+	__u64 start, end;
+	bool srvlock;
+
+	repbody = req_capsule_server_get(tsi->tsi_pill, &RMF_OST_BODY);
+	if (repbody == NULL)
+		RETURN(err_serious(-ENOMEM));
+
+	/*
+	 * fallocate start and end are passed in o_size, o_blocks
+	 * on the wire.
+	 */
+	start = oa->o_size;
+	end = oa->o_blocks;
+	mode = oa->o_falloc_mode;
+	/*
+	 * Only mode == 0 (which is standard prealloc) is supported now.
+	 * Punch is not supported yet.
+	 */
+	if (mode & ~FALLOC_FL_KEEP_SIZE)
+		RETURN(-EOPNOTSUPP);
+
+	repbody->oa.o_oi = oa->o_oi;
+	repbody->oa.o_valid = OBD_MD_FLID;
+
+	srvlock = oa->o_valid & OBD_MD_FLFLAGS &&
+		  oa->o_flags & OBD_FL_SRVLOCK;
+
+	if (srvlock) {
+		rc = tgt_extent_lock(tsi->tsi_env, ns, &tsi->tsi_resid,
+				     start, end, &lh, LCK_PW, &flags);
+		if (rc != 0)
+			RETURN(rc);
+	}
+
+	fo = ofd_object_find_exists(tsi->tsi_env, ofd_exp(tsi->tsi_exp),
+				    &tsi->tsi_fid);
+	if (IS_ERR(fo))
+		GOTO(out, rc = PTR_ERR(fo));
+
+	la_from_obdo(&info->fti_attr, oa,
+		     OBD_MD_FLMTIME | OBD_MD_FLATIME | OBD_MD_FLCTIME);
+
+	rc = ofd_object_fallocate(tsi->tsi_env, fo, start, end, mode,
+				 &info->fti_attr, oa);
+	if (rc)
+		GOTO(out_put, rc);
+
+	rc = ofd_attr_get(tsi->tsi_env, fo, &info->fti_attr);
+	if (rc == 0)
+		obdo_from_la(&repbody->oa, &info->fti_attr,
+			     OFD_VALID_FLAGS);
+	else
+		rc = 0;
+
+	ofd_counter_incr(tsi->tsi_exp, LPROC_OFD_STATS_PREALLOC,
+			 tsi->tsi_jobid, 1);
+
+	EXIT;
+out_put:
+	ofd_object_put(tsi->tsi_env, fo);
+out:
+	if (srvlock)
+		tgt_extent_unlock(&lh, LCK_PW);
+	if (rc == 0) {
+		res = ldlm_resource_get(ns, NULL, &tsi->tsi_resid,
+					LDLM_EXTENT, 0);
+		if (!IS_ERR(res)) {
+			struct ost_lvb *res_lvb;
+
+			ldlm_res_lvbo_update(res, NULL, 0);
+			res_lvb = res->lr_lvb_data;
+			/* Blocks */
+			repbody->oa.o_valid |= OBD_MD_FLBLOCKS;
+			repbody->oa.o_blocks = res_lvb->lvb_blocks;
+			/* Size */
+			repbody->oa.o_valid |= OBD_MD_FLSIZE;
+			repbody->oa.o_size = res_lvb->lvb_size;
+
+			ldlm_resource_putref(res);
+		}
+	}
+
+	RETURN(rc);
+}
+
+
+/**
  * OFD request handler for OST_PUNCH RPC.
  *
  * This is part of request processing. Validate request fields,
@@ -2711,6 +2820,7 @@ TGT_OST_HDL_HP(HAS_BODY | HAS_REPLY | IS_MUTABLE,
 TGT_OST_HDL(HAS_BODY | HAS_REPLY,	OST_SYNC,	ofd_sync_hdl),
 TGT_OST_HDL(HAS_REPLY,	OST_QUOTACTL,	ofd_quotactl),
 TGT_OST_HDL(HAS_BODY | HAS_REPLY, OST_LADVISE,	ofd_ladvise_hdl),
+TGT_OST_HDL(HAS_BODY | HAS_REPLY | IS_MUTABLE, OST_FALLOCATE, ofd_fallocate_hdl)
 };
 
 static struct tgt_opc_slice ofd_common_slice[] = {

lustre/osc/osc_io.c
@@ -40,6 +40,7 @@
 #include <lustre_obdo.h>
 #include <lustre_osc.h>
 #include <linux/pagevec.h>
+#include <linux/falloc.h>
 
 #include "osc_internal.h"
 
@@ -540,23 +541,30 @@ static void osc_trunc_check(const struct lu_env *env, struct cl_io *io,
 static int osc_io_setattr_start(const struct lu_env *env,
                                 const struct cl_io_slice *slice)
 {
-        struct cl_io            *io     = slice->cis_io;
-        struct osc_io           *oio    = cl2osc_io(env, slice);
-        struct cl_object        *obj    = slice->cis_obj;
-        struct lov_oinfo        *loi    = cl2osc(obj)->oo_oinfo;
-        struct cl_attr          *attr   = &osc_env_info(env)->oti_attr;
-        struct obdo             *oa     = &oio->oi_oa;
+	struct cl_io *io = slice->cis_io;
+	struct osc_io *oio = cl2osc_io(env, slice);
+	struct cl_object *obj = slice->cis_obj;
+	struct lov_oinfo *loi = cl2osc(obj)->oo_oinfo;
+	struct cl_attr *attr = &osc_env_info(env)->oti_attr;
+	struct obdo *oa = &oio->oi_oa;
 	struct osc_async_cbargs *cbargs = &oio->oi_cbarg;
-	__u64                    size   = io->u.ci_setattr.sa_attr.lvb_size;
 	unsigned int ia_avalid = io->u.ci_setattr.sa_avalid;
 	enum op_xvalid ia_xvalid = io->u.ci_setattr.sa_xvalid;
 	int result = 0;
+	__u64 size = io->u.ci_setattr.sa_attr.lvb_size;
+	__u64 end = OBD_OBJECT_EOF;
+	bool io_is_falloc = false;
 
 	ENTRY;
 	/* truncate cache dirty pages first */
-	if (cl_io_is_trunc(io))
+	if (cl_io_is_trunc(io)) {
 		result = osc_cache_truncate_start(env, cl2osc(obj), size,
 						  &oio->oi_trunc);
+	} else if (cl_io_is_fallocate(io)) {
+		io_is_falloc = true;
+		size = io->u.ci_setattr.sa_falloc_offset;
+		end = io->u.ci_setattr.sa_falloc_end;
+	}
 
 	if (result == 0 && oio->oi_lockless == 0) {
 		cl_object_attr_lock(obj);
@@ -608,9 +616,15 @@ static int osc_io_setattr_start(const struct lu_env *env,
 			oa->o_mtime = attr->cat_mtime;
 		}
 		if (ia_avalid & ATTR_SIZE) {
-			oa->o_size = size;
-			oa->o_blocks = OBD_OBJECT_EOF;
-			oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
+			if (io_is_falloc) {
+				oa->o_size = size;
+				oa->o_blocks = end;
+				oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
+			} else {
+				oa->o_size = size;
+				oa->o_blocks = OBD_OBJECT_EOF;
+				oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
+			}
 
 			if (oio->oi_lockless) {
 				oa->o_flags = OBD_FL_SRVLOCK;
@@ -633,14 +647,20 @@ static int osc_io_setattr_start(const struct lu_env *env,
 
 		init_completion(&cbargs->opc_sync);
 
-		if (ia_avalid & ATTR_SIZE)
+		if (io_is_falloc) {
+			int falloc_mode = io->u.ci_setattr.sa_falloc_mode;
+
+			result = osc_fallocate_base(osc_export(cl2osc(obj)),
+						    oa, osc_async_upcall,
+						    cbargs, falloc_mode);
+		} else if (ia_avalid & ATTR_SIZE) {
 			result = osc_punch_send(osc_export(cl2osc(obj)),
 						oa, osc_async_upcall, cbargs);
-		else
+		} else {
 			result = osc_setattr_async(osc_export(cl2osc(obj)),
 						   oa, osc_async_upcall,
 						   cbargs, PTLRPCD_SET);
-
+		}
 		cbargs->opc_rpc_sent = result == 0;
 	}
 
@@ -670,6 +690,7 @@ void osc_io_setattr_end(const struct lu_env *env,
 			struct osc_device *osd = lu2osc_dev(obj->co_lu.lo_dev);
 
 			LASSERT(cl_io_is_trunc(io));
+			LASSERT(cl_io_is_trunc(io) || cl_io_is_fallocate(io));
 			/* XXX: Need a lock. */
 			osd->od_stats.os_lockless_truncates++;
 		}
@@ -688,6 +709,25 @@ void osc_io_setattr_end(const struct lu_env *env,
 		osc_trunc_check(env, io, oio, size);
 		osc_cache_truncate_end(env, oio->oi_trunc);
 		oio->oi_trunc = NULL;
+	}
+
+	if (cl_io_is_fallocate(io)) {
+		cl_object_attr_lock(obj);
+
+		/* update blocks */
+		if (oa->o_valid & OBD_MD_FLBLOCKS) {
+			attr->cat_blocks = oa->o_blocks;
+			cl_valid |= CAT_BLOCKS;
+		}
+
+		/* update size */
+		if (oa->o_valid & OBD_MD_FLSIZE) {
+			attr->cat_size = oa->o_size;
+			cl_valid |= CAT_SIZE;
+		}
+
+		cl_object_attr_update(env, obj, attr, cl_valid);
+		cl_object_attr_unlock(obj);
 	}
 }
 EXPORT_SYMBOL(osc_io_setattr_end);

lustre/include/dt_object.h
@@ -1413,6 +1413,39 @@ struct dt_body_operations {
 			     __u64 start,
 			     __u64 end,
 			     enum lu_ladvise_type advice);
+
+	/**
+	 * Declare intention to preallocate space for an object
+	 *
+	 * \param[in] env	execution environment for this thread
+	 * \param[in] dt	object
+	 * \param[in] th	transaction handle
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int (*dbo_declare_fallocate)(const struct lu_env *env,
+				    struct dt_object *dt,
+				    struct thandle *th);
+	/**
+	 * Allocate specified region for an object
+	 *
+	 * \param[in] env	execution environment for this thread
+	 * \param[in] dt	object
+	 * \param[in] start	the start of the region to allocate
+	 * \param[in] end	the end of the region to allocate
+	 * \param[in] mode	fallocate mode
+	 * \param[in] th	transaction handle
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int (*dbo_fallocate)(const struct lu_env *env,
+			    struct dt_object *dt,
+			    __u64 start,
+			    __u64 end,
+			    int mode,
+			    struct thandle *th);
 };
 
 /**
@@ -2516,6 +2549,29 @@ static inline int dt_ladvise(const struct lu_env *env, struct dt_object *dt,
 	LASSERT(dt->do_body_ops);
 	LASSERT(dt->do_body_ops->dbo_ladvise);
 	return dt->do_body_ops->dbo_ladvise(env, dt, start, end, advice);
+}
+
+static inline int dt_declare_falloc(const struct lu_env *env,
+				      struct dt_object *dt, struct thandle *th)
+{
+	LASSERT(dt);
+	if (!dt->do_body_ops)
+		return -EOPNOTSUPP;
+	LASSERT(dt->do_body_ops);
+	LASSERT(dt->do_body_ops->dbo_declare_fallocate);
+	return dt->do_body_ops->dbo_declare_fallocate(env, dt, th);
+}
+
+static inline int dt_falloc(const struct lu_env *env, struct dt_object *dt,
+			      __u64 start, __u64 end, int mode,
+			      struct thandle *th)
+{
+	LASSERT(dt);
+	if (!dt->do_body_ops)
+		return -EOPNOTSUPP;
+	LASSERT(dt->do_body_ops);
+	LASSERT(dt->do_body_ops->dbo_fallocate);
+	return dt->do_body_ops->dbo_fallocate(env, dt, start, end, mode, th);
 }
 
 static inline int dt_fiemap_get(const struct lu_env *env, struct dt_object *d,

lustre/include/lustre_req_layout.h
@@ -197,6 +197,7 @@ extern struct req_format RQF_OST_GETATTR;
 extern struct req_format RQF_OST_SETATTR;
 extern struct req_format RQF_OST_CREATE;
 extern struct req_format RQF_OST_PUNCH;
+extern struct req_format RQF_OST_FALLOCATE;
 extern struct req_format RQF_OST_SYNC;
 extern struct req_format RQF_OST_DESTROY;
 extern struct req_format RQF_OST_BRW_READ;

lustre/include/obd_support.h
@@ -344,15 +344,16 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_OST_DELAY_TRANS	 0x246
 #define OBD_FAIL_OST_PREPARE_DELAY	 0x247
 #define OBD_FAIL_OST_2BIG_NIOBUF	 0x248
+#define OBD_FAIL_OST_FALLOCATE_NET	 0x249
 
 #define OBD_FAIL_LDLM                    0x300
 #define OBD_FAIL_LDLM_NAMESPACE_NEW      0x301
-#define OBD_FAIL_LDLM_ENQUEUE_NET			0x302
-#define OBD_FAIL_LDLM_CONVERT_NET			0x303
-#define OBD_FAIL_LDLM_CANCEL_NET			0x304
-#define OBD_FAIL_LDLM_BL_CALLBACK_NET			0x305
-#define OBD_FAIL_LDLM_CP_CALLBACK_NET			0x306
-#define OBD_FAIL_LDLM_GL_CALLBACK_NET			0x307
+#define OBD_FAIL_LDLM_ENQUEUE_NET	 0x302
+#define OBD_FAIL_LDLM_CONVERT_NET	 0x303
+#define OBD_FAIL_LDLM_CANCEL_NET	 0x304
+#define OBD_FAIL_LDLM_BL_CALLBACK_NET	 0x305
+#define OBD_FAIL_LDLM_CP_CALLBACK_NET	 0x306
+#define OBD_FAIL_LDLM_GL_CALLBACK_NET	 0x307
 #define OBD_FAIL_LDLM_ENQUEUE_EXTENT_ERR 0x308
 #define OBD_FAIL_LDLM_ENQUEUE_INTENT_ERR 0x309
 #define OBD_FAIL_LDLM_CREATE_RESOURCE    0x30a

commit 789038c97ae1072874dcd6afcc9cd78e9c885368

LU-15167 quota: fallocate send UID/GID for quota
Calling fallocate() on a newly created file did not account quota
usage properly because the OST object did not have a UID/GID
assigned yet. Update the fallocate code in the OSC to always send
the file UID/GID/PROJID to the OST so that the object ownership
can be updated before space is allocated.

Test-case: sanity-quota/78 added

Fixes: 48457868a02a ("LU-3606 fallocate: Implement fallocate preallocate operation")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I86d80a7f415a80100f7d2fb5f417cf47bf5b2900
Reviewed-on: https://review.whamcloud.com/45475
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/cl_object.h
@@ -1870,6 +1870,8 @@ struct cl_io {
 			int			 sa_falloc_mode;
 			loff_t			 sa_falloc_offset;
 			loff_t			 sa_falloc_end;
+			uid_t			 sa_falloc_uid;
+			gid_t			 sa_falloc_gid;
 		} ci_setattr;
 		struct cl_data_version_io {
 			u64 dv_data_version;

lustre/llite/file.c
@@ -5360,6 +5360,14 @@ int cl_falloc(struct file *file, struct inode *inode, int mode, loff_t offset,
 	io->u.ci_setattr.sa_falloc_offset = offset;
 	io->u.ci_setattr.sa_falloc_end = offset + len;
 	io->u.ci_setattr.sa_subtype = CL_SETATTR_FALLOCATE;
+
+	CDEBUG(D_INODE, "UID %u GID %u\n",
+	       from_kuid(&init_user_ns, inode->i_uid),
+	       from_kgid(&init_user_ns, inode->i_gid));
+
+	io->u.ci_setattr.sa_falloc_uid = from_kuid(&init_user_ns, inode->i_uid);
+	io->u.ci_setattr.sa_falloc_gid = from_kgid(&init_user_ns, inode->i_gid);
+
 	if (io->u.ci_setattr.sa_falloc_end > size) {
 		loff_t newsize = io->u.ci_setattr.sa_falloc_end;
 

lustre/lov/lov_io.c
@@ -689,6 +689,10 @@ static void lov_io_sub_inherit(struct lov_io_sub *sub, struct lov_io *lio,
 		if (cl_io_is_fallocate(io)) {
 			io->u.ci_setattr.sa_falloc_offset = start;
 			io->u.ci_setattr.sa_falloc_end = end;
+			io->u.ci_setattr.sa_falloc_uid =
+				parent->u.ci_setattr.sa_falloc_uid;
+			io->u.ci_setattr.sa_falloc_gid =
+				parent->u.ci_setattr.sa_falloc_gid;
 		}
 		if (cl_io_is_trunc(io)) {
 			loff_t new_size = parent->u.ci_setattr.sa_attr.lvb_size;

lustre/ofd/ofd_dev.c
@@ -1961,6 +1961,7 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 	struct ldlm_resource *res;
 	struct ofd_object *fo;
 	__u64 flags = 0;
+	__u64 valid;
 	struct lustre_handle lh = { 0, };
 	int rc, mode;
 	__u64 start, end;
@@ -2017,8 +2018,9 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 	if (IS_ERR(fo))
 		GOTO(out, rc = PTR_ERR(fo));
 
-	la_from_obdo(&info->fti_attr, oa,
-		     OBD_MD_FLMTIME | OBD_MD_FLATIME | OBD_MD_FLCTIME);
+	valid = OBD_MD_FLUID | OBD_MD_FLGID | OBD_MD_FLPROJID |
+		OBD_MD_FLATIME | OBD_MD_FLMTIME | OBD_MD_FLCTIME;
+	la_from_obdo(&info->fti_attr, oa, valid);
 
 	rc = ofd_object_fallocate(tsi->tsi_env, fo, start, end, mode,
 				 &info->fti_attr, oa);
@@ -2027,8 +2029,7 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 
 	rc = ofd_attr_get(tsi->tsi_env, fo, &info->fti_attr);
 	if (rc == 0)
-		obdo_from_la(&repbody->oa, &info->fti_attr,
-			     OFD_VALID_FLAGS);
+		obdo_from_la(&repbody->oa, &info->fti_attr, OFD_VALID_FLAGS);
 	else
 		rc = 0;
 

lustre/osc/osc_io.c
@@ -678,7 +678,13 @@ static int osc_io_setattr_start(const struct lu_env *env,
 
 			oa->o_size = io->u.ci_setattr.sa_falloc_offset;
 			oa->o_blocks = io->u.ci_setattr.sa_falloc_end;
-			oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
+			oa->o_uid = io->u.ci_setattr.sa_falloc_uid;
+			oa->o_gid = io->u.ci_setattr.sa_falloc_gid;
+			oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS |
+				OBD_MD_FLUID | OBD_MD_FLGID;
+
+			CDEBUG(D_INODE, "size %llu blocks %llu uid %u gid %u\n",
+			       oa->o_size, oa->o_blocks, oa->o_uid, oa->o_gid);
 			result = osc_fallocate_base(osc_export(cl2osc(obj)),
 						    oa, osc_async_upcall,
 						    cbargs, falloc_mode);

lustre/tests/sanity-quota.sh
@@ -4993,6 +4993,48 @@ test_77()
 }
 run_test 77 "lfs setquota should fail in Lustre mount with 'ro'"
 
+test_78()
+{
+	(( $OST1_VERSION >= $(version_code 2.14.55) )) ||
+		skip "need OST at least 2.14.55"
+	check_set_fallocate_or_skip
+
+	setup_quota_test || error "setup quota failed with $?"
+	stack_trap cleanup_quota_test
+
+	# enable ost quota
+	set_ost_qtype $QTYPE || error "enable ost quota failed"
+
+	mkdir -p $DIR/$tdir || error "failed to create $tdir"
+	chown $TSTUSR $DIR/$tdir || error "failed to chown $tdir"
+
+	# setup quota limit
+	$LFS setquota -u $TSTUSR -b25M -B25M $DIR/$tdir ||
+		error "lfs setquota failed"
+
+	# call fallocate
+	runas -u $TSTUSR -g $TSTUSR fallocate -l 204800 $DIR/$tdir/$tfile
+
+	kbytes=$(lfs quota -u $TSTUSR $DIR |
+		awk -v pattern=$DIR 'match($0, pattern) {printf $2}')
+	echo "kbytes returned:$kbytes"
+
+	# For file size of 204800. We should be having roughly 200 kbytes
+	# returned. Anything alarmingly low (50 taken as arbitrary value)
+	# would bail out this TC. Also this also avoids $kbytes of 0
+	# to be used in calculation below.
+	(( $kbytes > 50 )) ||
+		error "fallocate did not use quota. kbytes returned:$kbytes"
+
+	local expect_lo=$(($kbytes * 95 / 100)) # 5% below
+	local expect_hi=$(($kbytes * 105 / 100)) # 5% above
+
+	# Verify kbytes is 200 (204800/1024). With a permited  5% drift
+	(( $kbytes >= $expect_lo && $kbytes <= $expect_hi )) ||
+		error "fallocate did not use quota correctly"
+}
+run_test 78 "Check fallocate increase quota usage"
+
 quota_fini()
 {
 	do_nodes $(comma_list $(nodes_list)) \