lustre/lustre-release

commit 6b183927e19715d093c80a35ebc42a1cda5e70e2

LU-14553 changelog: eliminate mdd_changelog_clear warning
When handling a changelog_clear request, the user may specify a
range of indices which do not exist.  Similarly, the user may
specify a changelog user which does not exist.  Neither indicates
a problem within Lustre that justifies a a console warning.

Change those cases to CDEBUG.

Test-Parameters: trivial
Signed-off-by: Olaf Faaland <faaland1@llnl.gov>
Change-Id: I64bab12ef4978c4bf7139f5f36a39f9b109616fb
Reviewed-on: https://review.whamcloud.com/43125
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_device.c
@@ -1817,7 +1817,11 @@ static int mdd_changelog_clear(const struct lu_env *env,
 			      mdd_changelog_clear_cb, (void *)&mcuc,
 			      0, 0);
 
-	if (rc < 0) {
+	if (rc == -EINVAL) {
+		CDEBUG(D_IOCTL, "%s: No changelog recnum <= %llu to clear\n",
+		       mdd2obd_dev(mdd)->obd_name, (unsigned long long) endrec);
+		RETURN(-EINVAL);
+	} else if (rc < 0) {
 		CWARN("%s: Failure to clear the changelog for user %d: %d\n",
 		      mdd2obd_dev(mdd)->obd_name, id, rc);
 	} else if (mcuc.mcuc_flush) {
@@ -1832,7 +1836,7 @@ static int mdd_changelog_clear(const struct lu_env *env,
 						      mcuc.mcuc_minrec);
 		}
 	} else {
-		CWARN("%s: No entry for user %d\n",
+		CDEBUG(D_IOCTL, "%s: No entry for user %d\n",
 		      mdd2obd_dev(mdd)->obd_name, id);
 		rc = -ENOENT;
 	}

commit d083c93c6fd9251d6637d33029049b1d27d2a20a

LU-15357 mdd: fix changelog context leak
The mdd_changelog_clear() shouldn't skip llog_ctxt_put()
in case of error.

Fixes: 6b183927e1 (LU-14553 changelog: eliminate mdd_changelog_clear warning)
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I9c9aa3ce0d11e8f67470b450d007f2a1081644c6
Reviewed-on: https://review.whamcloud.com/45831
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_device.c
@@ -2142,48 +2142,54 @@ static int mdd_changelog_clear(const struct lu_env *env,
 
 	ctxt = llog_get_context(mdd2obd_dev(mdd),
 				LLOG_CHANGELOG_USER_ORIG_CTXT);
-	if (ctxt == NULL ||
-	    (ctxt->loc_handle->lgh_hdr->llh_flags & LLOG_F_IS_CAT) == 0)
+	if (!ctxt)
+		RETURN(-ENXIO);
+
+	if (!(ctxt->loc_handle->lgh_hdr->llh_flags & LLOG_F_IS_CAT))
 		GOTO(out, rc = -ENXIO);
 
 	rc = llog_cat_process(env, ctxt->loc_handle, mdd_changelog_clear_cb,
 			      &mcuc, 0, 0);
 	if (rc == -EINVAL) {
-		CDEBUG(D_IOCTL, "%s: No changelog recnum <= %llu to clear\n",
+		CDEBUG(D_IOCTL, "%s: no changelog recnum <= %llu to clear\n",
 		       mdd2obd_dev(mdd)->obd_name, (unsigned long long)endrec);
-		RETURN(-EINVAL);
-	} else if (rc < 0) {
+		GOTO(out, rc);
+	}
+
+	if (rc < 0) {
 		CWARN("%s: can't clear the changelog for user %s: rc = %d\n",
 		      mdd2obd_dev(mdd)->obd_name, mcuc.mcuc_name, rc);
-	} else if (mcuc.mcuc_flush) {
-		CDEBUG(D_IOCTL,
-		       "%s: purge changelog user %s entries up to %llu\n",
-		       mdd2obd_dev(mdd)->obd_name, mcuc.mcuc_name,
-		       mcuc.mcuc_minrec);
-		rc = mdd_changelog_llog_cancel(env, mdd, mcuc.mcuc_minrec);
-		if (!rc) {
-			spin_lock(&mdd->mdd_cl.mc_user_lock);
-			mdd->mdd_cl.mc_minrec = mcuc.mcuc_minrec;
-			mdd->mdd_cl.mc_mintime = mcuc.mcuc_mintime;
-			spin_unlock(&mdd->mdd_cl.mc_user_lock);
-		}
-	} else {
-		CDEBUG(D_IOCTL, "%s: No entry for user %d\n",
-		      mdd2obd_dev(mdd)->obd_name, id);
-		rc = -ENOENT;
+		GOTO(out, rc);
 	}
+
+	if (!mcuc.mcuc_flush) {
+		CDEBUG(D_IOCTL, "%s: no entry for user %d\n",
+		       mdd2obd_dev(mdd)->obd_name, id);
+		GOTO(out, rc = -ENOENT);
+	}
+
+	CDEBUG(D_IOCTL, "%s: purge changelog user %s entries up to %llu\n",
+	       mdd2obd_dev(mdd)->obd_name, mcuc.mcuc_name, mcuc.mcuc_minrec);
+
+	rc = mdd_changelog_llog_cancel(env, mdd, mcuc.mcuc_minrec);
+	if (rc)
+		GOTO(out, rc);
+
+	spin_lock(&mdd->mdd_cl.mc_user_lock);
+	mdd->mdd_cl.mc_minrec = mcuc.mcuc_minrec;
+	mdd->mdd_cl.mc_mintime = mcuc.mcuc_mintime;
+	spin_unlock(&mdd->mdd_cl.mc_user_lock);
 
 	EXIT;
 out:
-	if (ctxt != NULL)
-		llog_ctxt_put(ctxt);
+	llog_ctxt_put(ctxt);
 
 	return rc;
 }
 
 static int mdd_changelog_user_deregister(const struct lu_env *env,
-				       struct mdd_device *mdd, int *id,
-				       const char *name)
+					 struct mdd_device *mdd, int *id,
+					 const char *name)
 {
 	struct llog_ctxt *ctxt;
 	struct mdd_changelog_name_check_data mcnc = {