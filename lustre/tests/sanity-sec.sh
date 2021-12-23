lustre/lustre-release

commit 4d38566a004f6a636c37ec0c86f053be9b905bd7

LU-13717 sec: filename encryption
On client side, call the appropriate llcrypt primitives from llite,
to proceed with filename encryption before sending requests to servers
and filename decryption upon request receipt.
Note we need specific overlay functions to handle encoding and
decoding of encrypted filenames, as we do not want server side to deal
with binary names before they reach the backend file system layer.

On server side, mainly the OSD layer, we need to know the encryption
status of files being processed.
If an object belongs to an encrypted file, the filename has been
encoded by the client because it is binary, so it needs to be decoded
before being handed over to the backend file system layer.
And conversely, the filename of an encrypted file has to be encoded
before being sent over the wire.
Note server side is osd-ldiskfs only for now.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7ac9047f5a046b8bc63afdbbb1f28e78aa5c8c7e
Reviewed-on: https://review.whamcloud.com/43390
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/llite/llite_lib.c
@@ -3104,6 +3104,9 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 				      __u32 mode, enum md_op_code opc,
 				      void *data)
 {
+	struct llcrypt_name fname = { 0 };
+	int rc;
+
 	LASSERT(i1 != NULL);
 
 	if (name == NULL) {
@@ -3128,7 +3131,6 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 
 	ll_i2gids(op_data->op_suppgids, i1, i2);
 	op_data->op_fid1 = *ll_inode2fid(i1);
-	op_data->op_code = opc;
 
 	if (S_ISDIR(i1->i_mode)) {
 		down_read_non_owner(&ll_i2info(i1)->lli_lsm_sem);
@@ -3160,8 +3162,46 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 	if (ll_need_32bit_api(ll_i2sbi(i1)))
 		op_data->op_cli_flags |= CLI_API32;
 
-	op_data->op_name = name;
-	op_data->op_namelen = namelen;
+	if (opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_CREATE) {
+		/* In case of lookup, ll_setup_filename() has already been
+		 * called in ll_lookup_it(), so just take provided name.
+		 */
+		fname.disk_name.name = (unsigned char *)name;
+		fname.disk_name.len = namelen;
+	} else if (name && namelen) {
+		struct qstr dname = QSTR_INIT(name, namelen);
+		struct inode *dir;
+		int lookup;
+
+		if (!S_ISDIR(i1->i_mode) && i2 && S_ISDIR(i2->i_mode)) {
+			/* special case when called from ll_link() */
+			dir = i2;
+			lookup = 0;
+		} else {
+			dir = i1;
+			lookup = (int)(opc == LUSTRE_OPC_ANY);
+		}
+		rc = ll_setup_filename(dir, &dname, lookup, &fname);
+		if (rc) {
+			ll_finish_md_op_data(op_data);
+			return ERR_PTR(rc);
+		}
+		if (fname.disk_name.name &&
+		    fname.disk_name.name != (unsigned char *)name)
+			/* op_data->op_name must be freed after use */
+			op_data->op_flags |= MF_OPNAME_KMALLOCED;
+	}
+
+	/* In fact LUSTRE_OPC_LOOKUP, LUSTRE_OPC_OPEN, LUSTRE_OPC_MIGR
+	 * are LUSTRE_OPC_ANY
+	 */
+	if (opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_OPEN ||
+	    opc == LUSTRE_OPC_MIGR)
+		op_data->op_code = LUSTRE_OPC_ANY;
+	else
+		op_data->op_code = opc;
+	op_data->op_name = fname.disk_name.name;
+	op_data->op_namelen = fname.disk_name.len;
 	op_data->op_mode = mode;
 	op_data->op_mod_time = ktime_get_real_seconds();
 	op_data->op_fsuid = from_kuid(&init_user_ns, current_fsuid());
@@ -3182,6 +3222,11 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
 	ll_unlock_md_op_lsm(op_data);
 	ll_security_release_secctx(op_data->op_file_secctx,
 				   op_data->op_file_secctx_size);
+	if (op_data->op_flags & MF_OPNAME_KMALLOCED)
+		/* allocated via ll_setup_filename called
+		 * from ll_prep_md_op_data
+		 */
+		kfree(op_data->op_name);
 	llcrypt_free_ctx(op_data->op_file_encctx, op_data->op_file_encctx_size);
 	OBD_FREE_PTR(op_data);
 }

lustre/mdd/mdd_dir.c
@@ -55,11 +55,16 @@ static struct lu_name lname_dotdot = {
 };
 
 static inline int
-mdd_name_check(struct mdd_device *m, const struct lu_name *ln)
+mdd_name_check(const struct lu_env *env, struct mdd_device *m,
+	       const struct lu_name *ln)
 {
+	struct mdd_thread_info *info = mdd_env_info(env);
+	bool enc = info->mdi_pattr.la_valid & LA_FLAGS &&
+		info->mdi_pattr.la_flags & LUSTRE_ENCRYPT_FL;
+
 	if (!lu_name_is_valid(ln))
 		return -EINVAL;
-	else if (ln->ln_namelen > m->mdd_dt_conf.ddp_max_name_len)
+	else if (!enc && ln->ln_namelen > m->mdd_dt_conf.ddp_max_name_len)
 		return -ENAMETOOLONG;
 	else
 		return 0;
@@ -628,7 +633,7 @@ static int mdd_link_sanity_check(const struct lu_env *env,
                 RETURN(-ESTALE);
 
         /* Local ops, no lookup before link, check filename length here. */
-	rc = mdd_name_check(m, lname);
+	rc = mdd_name_check(env, m, lname);
 	if (rc < 0)
 		RETURN(rc);
 
@@ -2109,7 +2114,7 @@ static int mdd_create_sanity_check(const struct lu_env *env,
 		cattr->la_valid |= LA_PROJID;
 	}
 
-	rc = mdd_name_check(m, lname);
+	rc = mdd_name_check(env, m, lname);
 	if (rc < 0)
 		RETURN(rc);
 
@@ -3068,7 +3073,7 @@ static int mdd_rename(const struct lu_env *env,
 	if (rc)
 		GOTO(out_pending, rc);
 
-	rc = mdd_name_check(mdd, ltname);
+	rc = mdd_name_check(env, mdd, ltname);
 	if (rc < 0)
 		GOTO(out_pending, rc);
 
@@ -3515,11 +3520,9 @@ static int mdd_update_link(const struct lu_env *env,
 
 	ENTRY;
 
-	LASSERT(lu_name_is_valid(lname));
-
 	/* ignore tobj */
 	if (lu_fid_eq(tpfid, fid) && tname->ln_namelen == lname->ln_namelen &&
-	    !strncmp(tname->ln_name, lname->ln_name, lname->ln_namelen))
+	    !memcmp(tname->ln_name, lname->ln_name, lname->ln_namelen))
 		RETURN(0);
 
 	CDEBUG(D_INFO, "update "DFID"/"DNAME":"DFID"\n",
@@ -3602,7 +3605,7 @@ static int mdd_is_link_on_source_mdt(const struct lu_env *env,
 
 	/* ignore tobj */
 	if (lu_fid_eq(tpfid, fid) && tname->ln_namelen == lname->ln_namelen &&
-	    !strcmp(tname->ln_name, lname->ln_name))
+	    !memcmp(tname->ln_name, lname->ln_name, lname->ln_namelen))
 		return 0;
 
 	rc = mdd_fld_lookup(env, mdd, fid, &link_mdt_index);

lustre/tests/sanity-sec.sh
@@ -3252,12 +3252,11 @@ run_test 45 "encrypted file access semantics: MMAP"
 test_46() {
 	local testdir=$DIR/$tdir/mydir
 	local testfile=$testdir/myfile
+	local testdir2=$DIR/$tdir/mydir2
+	local testfile2=$testdir/myfile2
 	local lsfile=$TMP/lsfile
 	local scrambleddir
 	local scrambledfile
-
-	local testfile2=$DIR/$tdir/${tfile}.2
-	local tmpfile=$DIR/junk
 
 	$LCTL get_param mdc.*.import | grep -q client_encryption ||
 		skip "client encryption not supported"
@@ -3268,24 +3267,32 @@ test_46() {
 	stack_trap cleanup_for_enc_tests EXIT
 	setup_for_enc_tests
 
-	touch $DIR/onefile
 	touch $DIR/$tdir/$tfile
 	mkdir $testdir
 	echo test > $testfile
+	echo othertest > $testfile2
 	sync ; echo 3 > /proc/sys/vm/drop_caches
 
 	# remove fscrypt key from keyring
 	keyctl revoke $(keyctl show | awk '$7 ~ "^fscrypt:" {print $1}')
 	keyctl reap
+	cancel_lru_locks
 
 	scrambleddir=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type d)
-	ls -1 $scrambleddir > $lsfile || error "ls $testdir failed"
+	ls -1 $scrambleddir > $lsfile || error "ls $testdir failed (1)"
 
 	scrambledfile=$scrambleddir/$(head -n 1 $lsfile)
-	stat $scrambledfile || error "stat $scrambledfile failed"
+	stat $scrambledfile || error "stat $scrambledfile failed (1)"
 	rm -f $lsfile
 
-	cat $scrambledfile && error "cat $scrambledfile should have failed"
+	cat $scrambledfile && error "cat $scrambledfile should have failed (1)"
+	rm -f $scrambledfile || error "rm $scrambledfile failed (1)"
+
+	ls -1 $scrambleddir > $lsfile || error "ls $testdir failed (2)"
+	scrambledfile=$scrambleddir/$(head -n 1 $lsfile)
+	stat $scrambledfile || error "stat $scrambledfile failed (2)"
+	rm -f $lsfile
+	cat $scrambledfile && error "cat $scrambledfile should have failed (2)"
 
 	touch $scrambleddir/otherfile &&
 		error "touch otherfile should have failed"
@@ -3294,10 +3301,10 @@ test_46() {
 		error "mkdir otherdir should have failed"
 	ls -d $scrambleddir/otherdir && error "otherdir should not exist"
 
-	rm -f $scrambledfile || error "rm $scrambledfile failed"
+	ls -R $DIR
+	rm -f $scrambledfile || error "rm $scrambledfile failed (2)"
 	rmdir $scrambleddir || error "rmdir $scrambleddir failed"
-
-	rm -f $DIR/onefile
+	ls -R $DIR
 }
 run_test 46 "encrypted file access semantics without key"
 
@@ -3322,7 +3329,7 @@ test_47() {
 		error "rename from unencrypted to encrypted dir should fail"
 
 	ln $tmpfile $testfile &&
-		error "link from unencrypted to encrypted dir should fail"
+		error "link from encrypted to unencrypted dir should fail"
 
 	cp $tmpfile $testfile ||
 		error "cp from unencrypted to encrypted dir should succeed"
@@ -3336,7 +3343,7 @@ test_47() {
 	rm -f $testfile
 
 	ln $testfile2 $tmpfile ||
-		error "link from encrypted to unencrypted dir should succeed"
+		error "link from unencrypted to encrypted dir should succeed"
 	rm -f $tmpfile
 
 	# check we are limited in the number of hard links
@@ -3345,9 +3352,11 @@ test_47() {
 		ln $testfile2 ${testfile}_$i || break
 	done
 	[ $i -lt 160 ] || error "hard link $i should fail"
+	rm -f ${testfile}_*
 
 	mrename $testfile2 $tmpfile &&
 		error "rename from encrypted to unencrypted dir should fail"
+	rm -f $testfile2
 	touch $tmpfile
 
 	dd if=/dev/zero of=$testfile bs=512K count=1
@@ -3357,6 +3366,7 @@ test_47() {
 	# remove fscrypt key from keyring
 	keyctl revoke $(keyctl show | awk '$7 ~ "^fscrypt:" {print $1}')
 	keyctl reap
+	cancel_lru_locks
 
 	scrambleddir=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type d)
 	scrambledfile=$(find $DIR/$tdir/ -maxdepth 1 -type f)
@@ -4038,8 +4048,9 @@ test_54() {
 	local filecount=$($RUNAS find $testdir -type f | wc -l)
 	[ $filecount -eq 3 ] || error "found $filecount files"
 
-	$RUNAS hexdump -C $testfile &&
-		error "reading $testfile should have failed without key"
+	scrambledfiles=( $(find $testdir/ -maxdepth 1 -type f) )
+	$RUNAS hexdump -C ${scrambledfiles[0]} &&
+		error "reading ${scrambledfiles[0]} should fail without key"
 
 	$RUNAS touch ${testfile}.nokey &&
 		error "touch ${testfile}.nokey should have failed without key"
@@ -4055,8 +4066,8 @@ test_54() {
 	$RUNAS fscrypt lock --verbose $testdir ||
 		error "fscrypt lock $testdir failed (2)"
 
-	$RUNAS hexdump -C $testfile2 &&
-		error "reading $testfile2 should have failed without key"
+	$RUNAS hexdump -C ${scrambledfiles[1]} &&
+		error "reading ${scrambledfiles[1]} should fail without key"
 
 	echo mypass | $RUNAS fscrypt unlock --verbose $testdir ||
 		error "fscrypt unlock $testdir failed (2)"

lustre/include/obd.h
@@ -840,6 +840,7 @@ enum md_op_flags {
 	MF_GETATTR_BY_FID	= BIT(5),
 	MF_QOS_MKDIR		= BIT(6),
 	MF_RR_MKDIR		= BIT(7),
+	MF_OPNAME_KMALLOCED	= BIT(8),
 };
 
 enum md_cli_flags {
@@ -857,6 +858,9 @@ enum md_op_code {
 	LUSTRE_OPC_MKNOD,
 	LUSTRE_OPC_CREATE,
 	LUSTRE_OPC_ANY,
+	LUSTRE_OPC_LOOKUP,
+	LUSTRE_OPC_OPEN,
+	LUSTRE_OPC_MIGR,
 };
 
 /**

lustre/llite/crypto.c
@@ -29,6 +29,7 @@
 #include "llite_internal.h"
 
 #ifdef HAVE_LUSTRE_CRYPTO
+#include <libcfs/libcfs_crypto.h>
 
 static int ll_get_context(struct inode *inode, void *ctx, size_t len)
 {
@@ -158,6 +159,150 @@ static bool ll_empty_dir(struct inode *inode)
 	return true;
 }
 
+/**
+ * ll_setup_filename() - overlay to llcrypt_setup_filename
+ * @dir: the directory that will be searched
+ * @iname: the user-provided filename being searched for
+ * @lookup: 1 if we're allowed to proceed without the key because it's
+ *	->lookup() or we're finding the dir_entry for deletion; 0 if we cannot
+ *	proceed without the key because we're going to create the dir_entry.
+ * @fname: the filename information to be filled in
+ *
+ * This overlay function is necessary to properly encode @fname after
+ * encryption, as it will be sent over the wire.
+ */
+int ll_setup_filename(struct inode *dir, const struct qstr *iname,
+		      int lookup, struct llcrypt_name *fname)
+{
+	int rc;
+
+	rc = llcrypt_setup_filename(dir, iname, lookup, fname);
+	if (rc)
+		return rc;
+
+	if (IS_ENCRYPTED(dir) &&
+	    !name_is_dot_or_dotdot(fname->disk_name.name,
+				   fname->disk_name.len)) {
+		int presented_len = critical_chars(fname->disk_name.name,
+						   fname->disk_name.len);
+		char *buf;
+
+		buf = kmalloc(presented_len + 1, GFP_NOFS);
+		if (!buf) {
+			rc = -ENOMEM;
+			goto out_free;
+		}
+
+		if (presented_len == fname->disk_name.len)
+			memcpy(buf, fname->disk_name.name, presented_len);
+		else
+			critical_encode(fname->disk_name.name,
+					fname->disk_name.len, buf);
+		buf[presented_len] = '\0';
+		kfree(fname->crypto_buf.name);
+		fname->crypto_buf.name = buf;
+		fname->crypto_buf.len = presented_len;
+		fname->disk_name.name = fname->crypto_buf.name;
+		fname->disk_name.len = fname->crypto_buf.len;
+	}
+
+	return rc;
+
+out_free:
+	llcrypt_free_filename(fname);
+	return rc;
+}
+
+/**
+ * ll_fname_disk_to_usr() - overlay to llcrypt_fname_disk_to_usr
+ * @inode: the inode to convert name
+ * @hash: major hash for inode
+ * @minor_hash: minor hash for inode
+ * @iname: the user-provided filename needing conversion
+ * @oname: the filename information to be filled in
+ *
+ * The caller must have allocated sufficient memory for the @oname string.
+ *
+ * This overlay function is necessary to properly decode @iname before
+ * decryption, as it comes from the wire.
+ */
+int ll_fname_disk_to_usr(struct inode *inode,
+			 u32 hash, u32 minor_hash,
+			 struct llcrypt_str *iname, struct llcrypt_str *oname)
+{
+	struct llcrypt_str lltr = LLTR_INIT(iname->name, iname->len);
+	char *buf = NULL;
+	int rc;
+
+	if (IS_ENCRYPTED(inode) &&
+	    !name_is_dot_or_dotdot(lltr.name, lltr.len) &&
+	    strnchr(lltr.name, lltr.len, '=')) {
+		/* Only proceed to critical decode if
+		 * iname contains espace char '='.
+		 */
+		int len = lltr.len;
+
+		buf = kmalloc(len, GFP_NOFS);
+		if (!buf)
+			return -ENOMEM;
+
+		len = critical_decode(lltr.name, len, buf);
+		lltr.name = buf;
+		lltr.len = len;
+	}
+
+	rc = llcrypt_fname_disk_to_usr(inode, hash, minor_hash, &lltr, oname);
+
+	kfree(buf);
+
+	return rc;
+}
+
+/* Copied from llcrypt_d_revalidate, as it is not exported */
+/*
+ * Validate dentries in encrypted directories to make sure we aren't potentially
+ * caching stale dentries after a key has been added.
+ */
+int ll_revalidate_d_crypto(struct dentry *dentry, unsigned int flags)
+{
+	struct dentry *dir;
+	int err;
+	int valid;
+
+	/*
+	 * Plaintext names are always valid, since llcrypt doesn't support
+	 * reverting to ciphertext names without evicting the directory's inode
+	 * -- which implies eviction of the dentries in the directory.
+	 */
+	if (!(dentry->d_flags & DCACHE_ENCRYPTED_NAME))
+		return 1;
+
+	/*
+	 * Ciphertext name; valid if the directory's key is still unavailable.
+	 *
+	 * Although llcrypt forbids rename() on ciphertext names, we still must
+	 * use dget_parent() here rather than use ->d_parent directly.  That's
+	 * because a corrupted fs image may contain directory hard links, which
+	 * the VFS handles by moving the directory's dentry tree in the dcache
+	 * each time ->lookup() finds the directory and it already has a dentry
+	 * elsewhere.  Thus ->d_parent can be changing, and we must safely grab
+	 * a reference to some ->d_parent to prevent it from being freed.
+	 */
+
+	if (flags & LOOKUP_RCU)
+		return -ECHILD;
+
+	dir = dget_parent(dentry);
+	err = llcrypt_get_encryption_info(d_inode(dir));
+	valid = !llcrypt_has_encryption_key(d_inode(dir));
+	dput(dir);
+
+	if (err < 0)
+		return err;
+
+	return valid;
+}
+
 const struct llcrypt_operations lustre_cryptops = {
 	.key_prefix		= "lustre:",
 	.get_context		= ll_get_context,
@@ -189,6 +334,24 @@ bool ll_sbi_has_encrypt(struct ll_sb_info *sbi)
 
 void ll_sbi_set_encrypt(struct ll_sb_info *sbi, bool set)
 {
+}
+
+int ll_setup_filename(struct inode *dir, const struct qstr *iname,
+		      int lookup, struct llcrypt_name *fname)
+{
+	return llcrypt_setup_filename(dir, iname, lookup, fname);
+}
+
+int ll_fname_disk_to_usr(struct inode *inode,
+			 u32 hash, u32 minor_hash,
+			 struct llcrypt_str *iname, struct llcrypt_str *oname)
+{
+	return llcrypt_fname_disk_to_usr(inode, hash, minor_hash, iname, oname);
+}
+
+int ll_revalidate_d_crypto(struct dentry *dentry, unsigned int flags)
+{
+	return 1;
 }
 #endif
 

lustre/llite/dcache.c
@@ -293,9 +293,14 @@ static int ll_revalidate_dentry(struct dentry *dentry,
 				unsigned int lookup_flags)
 {
 	struct inode *dir = dentry->d_parent->d_inode;
+	int rc;
 
 	CDEBUG(D_VFSTRACE, "VFS Op:name=%s, flags=%u\n",
 	       dentry->d_name.name, lookup_flags);
+
+	rc = ll_revalidate_d_crypto(dentry, lookup_flags);
+	if (rc != 1)
+		return rc;
 
 	/* If this is intermediate component path lookup and we were able to get
 	 * to this dentry, then its lock has not been revoked and the

lustre/llite/dir.c
@@ -55,6 +55,7 @@
 #include <lustre_fid.h>
 #include <lustre_kernelcomm.h>
 #include <lustre_swab.h>
+#include <libcfs/libcfs_crypto.h>
 
 #include "llite_internal.h"
 
@@ -186,14 +187,21 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 		void *cookie, filldir_t filldir)
 {
 #endif
-	struct ll_sb_info    *sbi        = ll_i2sbi(inode);
-	__u64                 pos        = *ppos;
-	bool                  is_api32 = ll_need_32bit_api(sbi);
-	bool                  is_hash64 = sbi->ll_flags & LL_SBI_64BIT_HASH;
-	struct page          *page;
-	bool                  done = false;
-	int                   rc = 0;
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	__u64 pos = *ppos;
+	bool is_api32 = ll_need_32bit_api(sbi);
+	bool is_hash64 = sbi->ll_flags & LL_SBI_64BIT_HASH;
+	struct page *page;
+	bool done = false;
+	struct llcrypt_str lltr = LLTR_INIT(NULL, 0);
+	int rc = 0;
 	ENTRY;
+
+	if (IS_ENCRYPTED(inode)) {
+		rc = llcrypt_fname_alloc_buffer(inode, NAME_MAX, &lltr);
+		if (rc < 0)
+			RETURN(rc);
+	}
 
 	page = ll_get_dir_page(inode, op_data, pos);
 
@@ -238,9 +246,31 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 			 * for 'filldir()' must be part of the 'ent'. */
 #ifdef HAVE_DIR_CONTEXT
 			ctx->pos = lhash;
-			done = !dir_emit(ctx, ent->lde_name, namelen, ino,
-					 type);
+			if (!IS_ENCRYPTED(inode)) {
+				done = !dir_emit(ctx, ent->lde_name, namelen,
+						 ino, type);
+			} else {
+				/* Directory is encrypted */
+				int save_len = lltr.len;
+				struct llcrypt_str de_name =
+					LLTR_INIT(ent->lde_name, namelen);
+
+				rc = ll_fname_disk_to_usr(inode, 0, 0, &de_name,
+							  &lltr);
+				de_name = lltr;
+				lltr.len = save_len;
+				if (rc) {
+					done = 1;
+					break;
+				}
+				done = !dir_emit(ctx, de_name.name, de_name.len,
+						 ino, type);
+			}
 #else
+			/* HAVE_DIR_CONTEXT is defined from kernel 3.11, whereas
+			 * IS_ENCRYPTED is brought by kernel 4.14.
+			 * So there is no need to handle encryption case here.
+			 */
 			done = filldir(cookie, ent->lde_name, namelen, lhash,
 				       ino, type);
 #endif
@@ -277,6 +307,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 #else
 	*ppos = pos;
 #endif
+	llcrypt_fname_free_buffer(&lltr);
 	RETURN(rc);
 }
 
@@ -307,6 +338,12 @@ static int ll_readdir(struct file *filp, void *cookie, filldir_t filldir)
 	       "VFS Op:inode="DFID"(%p) pos/size%lu/%llu 32bit_api %d\n",
 	       PFID(ll_inode2fid(inode)),
 	       inode, (unsigned long)pos, i_size_read(inode), api32);
+
+	if (IS_ENCRYPTED(inode)) {
+		rc = llcrypt_get_encryption_info(inode);
+		if (rc && rc != -ENOKEY)
+			GOTO(out, rc);
+	}
 
 	if (pos == MDS_DIR_END_OFF)
 		/*

lustre/llite/file.c
@@ -628,7 +628,7 @@ retry:
 	}
 
 	op_data = ll_prep_md_op_data(NULL, parent->d_inode, de->d_inode,
-				     name, len, 0, LUSTRE_OPC_ANY, NULL);
+				     name, len, 0, LUSTRE_OPC_OPEN, NULL);
 	if (IS_ERR(op_data)) {
 		kfree(name);
 		RETURN(PTR_ERR(op_data));
@@ -2283,7 +2283,7 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
                              struct ptlrpc_request **request)
 {
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
-	struct mdt_body *body;
+	struct mdt_body  *body;
 	struct lov_mds_md *lmm = NULL;
 	struct ptlrpc_request *req = NULL;
 	struct md_op_data *op_data;
@@ -2305,8 +2305,8 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 	rc = md_getattr_name(sbi->ll_md_exp, op_data, &req);
 	ll_finish_md_op_data(op_data);
 	if (rc < 0) {
-		CDEBUG(D_INFO, "md_getattr_name failed "
-		       "on %s: rc %d\n", filename, rc);
+		CDEBUG(D_INFO, "md_getattr_name failed on %s: rc %d\n",
+		       filename, rc);
 		GOTO(out, rc);
 	}
 
@@ -4734,10 +4734,10 @@ int ll_get_fid_by_name(struct inode *parent, const char *name,
 		       int namelen, struct lu_fid *fid,
 		       struct inode **inode)
 {
-	struct md_op_data	*op_data = NULL;
-	struct mdt_body		*body;
-	struct ptlrpc_request	*req;
-	int			rc;
+	struct md_op_data *op_data = NULL;
+	struct mdt_body *body;
+	struct ptlrpc_request *req;
+	int rc;
 	ENTRY;
 
 	op_data = ll_prep_md_op_data(NULL, parent, NULL, name, namelen, 0,
@@ -4845,7 +4845,8 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	}
 
 	op_data = ll_prep_md_op_data(NULL, parent, NULL, name, namelen,
-				     child_inode->i_mode, LUSTRE_OPC_ANY, NULL);
+				     child_inode->i_mode, LUSTRE_OPC_MIGR,
+				     NULL);
 	if (IS_ERR(op_data))
 		GOTO(out_iput, rc = PTR_ERR(op_data));
 
@@ -4886,8 +4887,9 @@ again:
 		spin_unlock(&och->och_mod->mod_open_req->rq_lock);
 	}
 
-	rc = md_rename(ll_i2sbi(parent)->ll_md_exp, op_data, name, namelen,
-		       name, namelen, &request);
+	rc = md_rename(ll_i2sbi(parent)->ll_md_exp, op_data,
+		       op_data->op_name, op_data->op_namelen,
+		       op_data->op_name, op_data->op_namelen, &request);
 	if (rc == 0) {
 		LASSERT(request != NULL);
 		ll_update_times(request, parent);

commit 75414af6bf310244d38284958ecf037d61936726

LU-13717 sec: fix handling of encrypted file with long name
The ciphertext representation of the name of an encrypted file or
directory can be up to 256 bytes of binary data, if the cleartext
name is up to NAME_MAX. But then this ciphertext is encoded via
critical_encode() before being sent to servers. Once encoded, the
length can exceed NAME_MAX because of the escaped critical
characters.
So make sure ll_prep_md_op_data() accepts those too long encoded names
if it is called for lookup or create of an encrypted file or
directory. In the other cases, the 'name' taken as input is the plain
text version, so it must conform to the NAME_MAX limit.

When carrying out operations on an encrypted file with long name, we
manipulate a digested form whose hash needs to be matched against the
content of the LinkEA. The name found in the LinkEA is not NUL
terminated, so this aspect must be taken care of.

Fixes: 4d38566a00 ("LU-13717 sec: filename encryption")
Fixes: ed4a625d88 ("LU-13717 sec: filename encryption - digest support")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I4b0e51eee5e549ab56292fe0fec3c1be1b487fc7
Reviewed-on: https://review.whamcloud.com/45163
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -3221,7 +3221,9 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 		if (namelen != 0)
 			return ERR_PTR(-EINVAL);
 	} else {
-		if (namelen > ll_i2sbi(i1)->ll_namelen)
+		if ((!IS_ENCRYPTED(i1) ||
+		     (opc != LUSTRE_OPC_LOOKUP && opc != LUSTRE_OPC_CREATE)) &&
+		    namelen > ll_i2sbi(i1)->ll_namelen)
 			return ERR_PTR(-ENAMETOOLONG);
 
 		/* "/" is not valid name, but it's allowed */

lustre/mdd/mdd_dir.c
@@ -1717,7 +1717,7 @@ static int mdd_unlink(const struct lu_env *env, struct md_object *pobj,
 		      struct md_object *cobj, const struct lu_name *lname,
 		      struct md_attr *ma, int no_name)
 {
-	const char *name = lname->ln_name;
+	char *name = (char *)lname->ln_name;
 	struct lu_attr *pattr = MDD_ENV_VAR(env, pattr);
 	struct lu_attr *cattr = MDD_ENV_VAR(env, cattr);
 	struct lu_attr *la = &mdd_env_info(env)->mdi_la_for_fix;
@@ -1775,6 +1775,16 @@ static int mdd_unlink(const struct lu_env *env, struct md_object *pobj,
 
 	if (likely(mdd_cobj != NULL))
 		mdd_write_lock(env, mdd_cobj, DT_TGT_CHILD);
+
+	if (lname->ln_name[lname->ln_namelen] != '\0') {
+		/* lname->ln_name is not necessarily NUL terminated */
+		name = kmalloc(lname->ln_namelen + 1, GFP_NOFS);
+		if (!name)
+			GOTO(cleanup, rc = -ENOMEM);
+
+		memcpy(name, lname->ln_name, lname->ln_namelen);
+		name[lname->ln_namelen] = '\0';
+	}
 
 	if (likely(no_name == 0) && !OBD_FAIL_CHECK(OBD_FAIL_LFSCK_DANGLING2)) {
 		rc = __mdd_index_delete(env, mdd_pobj, name, is_dir, handle);
@@ -1850,6 +1860,9 @@ static int mdd_unlink(const struct lu_env *env, struct md_object *pobj,
 
 	EXIT;
 cleanup:
+	if (name != lname->ln_name)
+		kfree(name);
+
 	if (likely(mdd_cobj != NULL))
 		mdd_write_unlock(env, mdd_cobj);
 

lustre/tests/sanity-sec.sh
@@ -3374,6 +3374,11 @@ test_46() {
 	local testfile=$testdir/myfile
 	local testdir2=$DIR/$tdir/mydirwithaveryverylongnametotestcodebehaviour0
 	local testfile2=$testdir/myfilewithaveryverylongnametotestcodebehaviour0
+	# testdir3, testfile3, testhl3 and testsl3 names are 255 bytes long
+	local testdir3=$testdir2/dir_abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz012345678
+	local testfile3=$testdir2/file_abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz01234567
+	local testhl3=$testdir2/hl_abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789
+	local testsl3=$testdir2/sl_abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789
 	local lsfile=$TMP/lsfile
 	local scrambleddir
 	local scrambledfile
@@ -3396,6 +3401,14 @@ test_46() {
 	else
 		mkdir $testdir2
 	fi
+	if [ "$mds1_FSTYPE" = ldiskfs ]; then
+		# For now, restrict this part of the test to ldiskfs backend,
+		# as osd-zfs does not support 255 byte-long encrypted names.
+		mkdir $testdir3 || error "cannot mkdir $testdir3"
+		touch $testfile3 || error "cannot touch $testfile3"
+		ln $testfile3 $testhl3 || error "cannot ln $testhl3"
+		ln -s $testfile3 $testsl3 || error "cannot ln $testsl3"
+	fi
 	sync ; echo 3 > /proc/sys/vm/drop_caches
 
 	# remove fscrypt key from keyring
@@ -3406,6 +3419,10 @@ test_46() {
 	# this is $testdir2
 	scrambleddir=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type d| grep _)
 	stat $scrambleddir || error "stat $scrambleddir failed"
+	if [ "$mds1_FSTYPE" = ldiskfs ]; then
+		stat $scrambleddir/* || error "cannot stat in $scrambleddir"
+		rm -rf $scrambleddir/* || error "cannot clean in $scrambleddir"
+	fi
 	rmdir $scrambleddir || error "rmdir $scrambleddir failed"
 
 	scrambleddir=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type d)