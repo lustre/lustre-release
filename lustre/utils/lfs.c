lustre/lustre-release

commit 11aa7f8704c490b011f60f234c3ac9929ce76948

LU-11367 som: integrate LSOM with lfs find
The patch integrates LSOM functionality with lfs find so that it
is possible to use LSOM functionality directly on the client. The
MDS fills in the mbo_size and mbo_blocks fields from the LSOM
xattr, if the actual size/blocks are not available, and then set
new OBD_MD_FLLSIZE and OBD_MD_FLLBLOCKS flags in the reply so that
the client knows these fields are valid.

The lfs find command adds "-l|--lazy" option to allow the use of
LSOM data from the MDS.

Add a new version of ioctl(LL_IOC_MDC_GETINFO) call that also returns
valid flags from the MDS RPC to userspace in struct lov_user_mds_data
so that it is possible to determine whether the size and blocks are
returned by the call.  The old LL_IOC_MDC_GETINFO ioctl number is
renamed to LL_IOC_MDC_GETINFO_OLD and is binary compatible, but
newly-compiled applications will use the new struct lov_user_mds_data.

New llapi interfaces llapi_get_lum_file(), llapi_get_lum_dir(),
llapi_get_lum_file_fd(), llapi_get_lum_dir_fd() are added to fetch
valid stat() attributes and LOV info to the user.

Signed-off-by: Qian Yingjin <qian@ddn.com>
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I21dfae7c2633dead5d83b438ec340fea4d3ebbe5
Reviewed-on: https://review.whamcloud.com/35167
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>

lustre/utils/lfs.c
@@ -3078,6 +3078,7 @@ static int lfs_setstripe_internal(int argc, char **argv,
 	{ .val = 'i',	.name = "stripe_index",	.has_arg = required_argument},
 	{ .val = 'I',	.name = "comp-id",	.has_arg = required_argument},
 	{ .val = 'I',	.name = "component-id",	.has_arg = required_argument},
+/* find { .val = 'l',	.name = "lazy",		.has_arg = no_argument }, */
 	{ .val = 'L',	.name = "layout",	.has_arg = required_argument },
 	{ .val = 'm',	.name = "mdt",		.has_arg = required_argument},
 	{ .val = 'm',	.name = "mdt-index",	.has_arg = required_argument},
@@ -4135,6 +4136,7 @@ static int lfs_find(int argc, char **argv)
 	{ .val = 'i',	.name = "stripe-index",	.has_arg = required_argument },
 	{ .val = 'i',	.name = "stripe_index",	.has_arg = required_argument },
 /* getstripe { .val = 'I', .name = "comp-id",	.has_arg = required_argument }*/
+	{ .val = 'l',	.name = "lazy",		.has_arg = no_argument },
 	{ .val = 'L',	.name = "layout",	.has_arg = required_argument },
 	{ .val = 'm',	.name = "mdt",		.has_arg = required_argument },
 	{ .val = 'm',	.name = "mdt-index",	.has_arg = required_argument },
@@ -4430,6 +4432,9 @@ static int lfs_find(int argc, char **argv)
 			param.fp_check_hash_type = 1;
 			param.fp_exclude_hash_type = !!neg_opt;
 			break;
+		case 'l':
+			param.fp_lazy = 1;
+			break;
 		case 'L':
 			ret = name2layout(&param.fp_layout, optarg);
 			if (ret)
@@ -4706,22 +4711,21 @@ err_free:
 			param.fp_check_ext_size = 1;
 			param.fp_exclude_ext_size = !!neg_opt;
 			break;
+		default:
+			ret = CMD_HELP;
+			goto err;
+		};
+	}
 
-                default:
-                        ret = CMD_HELP;
-                        goto err;
-                };
-        }
-
-        if (pathstart == -1) {
-                fprintf(stderr, "error: %s: no filename|pathname\n",
-                        argv[0]);
-                ret = CMD_HELP;
-                goto err;
-        } else if (pathend == -1) {
-                /* no options */
-                pathend = argc;
-        }
+	if (pathstart == -1) {
+		fprintf(stderr, "error: %s: no filename|pathname\n",
+			argv[0]);
+		ret = CMD_HELP;
+		goto err;
+	} else if (pathend == -1) {
+		/* no options */
+		pathend = argc;
+	}
 
 	do {
 		rc = llapi_find(argv[pathstart], &param);
@@ -4782,6 +4786,7 @@ static int lfs_getstripe_internal(int argc, char **argv,
 	{ .val = 'i',	.name = "stripe_index",	.has_arg = no_argument },
 	{ .val = 'I',	.name = "comp-id",	.has_arg = optional_argument },
 	{ .val = 'I',	.name = "component-id",	.has_arg = optional_argument },
+/* find { .val = 'l',	.name = "lazy",		.has_arg = no_argument }, */
 	{ .val = 'L',	.name = "layout",	.has_arg = no_argument },
 	{ .val = 'm',	.name = "mdt",		.has_arg = no_argument },
 	{ .val = 'm',	.name = "mdt-index",	.has_arg = no_argument },
@@ -5635,6 +5640,7 @@ static int lfs_setdirstripe(int argc, char **argv)
 	{ .val = 'i',	.name = "mdt-index",	.has_arg = required_argument },
 	{ .val = 'i',	.name = "mdt",		.has_arg = required_argument },
 #else
+/* find { .val = 'l',	.name = "lazy",		.has_arg = no_argument }, */
 	{ .val = 'm',	.name = "mdt-index",	.has_arg = required_argument },
 	{ .val = 'm',	.name = "mdt",		.has_arg = required_argument },
 #endif

lustre/doc/lfs-find.1
@@ -14,8 +14,11 @@ lfs-find \- Lustre client utility to list files with specific attributes
       [[\fB!\fR] \fB--gid\fR|\fB-g\fR|\fB--group\fR|\fB-G\fR <\fIgname\fR>|<\fIgid\fR>]
       [[\fB!\fR] \fB--layout\fR|\fB-L mdt\fR,\fBraid0\fR,\fBreleased\fR]
       [[\fB!\fR] \fB--foreign\fR [<\fItype\fR>]]
-[\fB--maxdepth\fR|\fB-D\fI n\fR]
-      [[\fB!\fR] \fB--mdt\fR|\fB--mdt-index\fR|\fB-m\fR <\fIuuid\fR|\fIindex\fR,...>]
+[[\fB!\fR] \fB--gid\fR|\fB-g\fR|\fB--group\fR|\fB-G\fR <\fIgname\fR>|<\fIgid\fR>]
+      [[\fB!\fR] \fB--layout\fR|\fB-L mdt\fR,\fBraid0\fR,\fBreleased\fR]
+[\fB--lazy\fR]
+      [\fB--maxdepth\fR|\fB-D\fI n\fR]
+[[\fB!\fR] \fB--mdt\fR|\fB--mdt-index\fR|\fB-m\fR <\fIuuid\fR|\fIindex\fR,...>]
       [[\fB!\fR] \fB--mdt-count\fR|\fB-T\fR [\fB+-\fR]\fIn\fR]
 [[\fB!\fR] \fB--mdt-hash\fR|\fB-H <\fIhashtype\fR>]
       [[\fB!\fR] \fB--mirror-count|\fB-N\fR [\fB+-\fR]\fIn\fR]
@@ -24,7 +27,6 @@ lfs-find \- Lustre client utility to list files with specific attributes
 [[\fB!\fR] \fB--name\fR|\fB-n <\fIpattern\fR>]
       [[\fB!\fR] \fB--ost\fR|\fB-O\fR <\fIindex\fR,...>]
 [[\fB!\fR] \fB--pool\fR <\fIpool\fR>]
-[[\fB!\fR] \fB--foreign\fR [<\fItype\fR>]]
 [\fB--print\fR|\fB-P\fR]
       [\fB--print0\fR|\fB-0\fR]
 [[\fB!\fR] \fB--projid\fR |<\fIprojid\fR>]
@@ -104,6 +106,15 @@ local on the network to clients.
 .BR --component-start | --comp-start
 The file has component start offset \fIn\fR (in bytes) for any component.
 .TP
+.BR --foreign
+File has a foreign (non-Lustre/free format) layout and is of the given
+.IR type ,
+if specified.  Presently only
+.B none
+or
+.B daos
+are defined types, though 32-bit numeric types can also be used.
+.TP
 .BR --gid | -g
 File has specified numeric group ID.
 .TP
@@ -124,10 +135,8 @@ HSM-archived files that are not resident in the filesystem.
 Files that have the first data component on an MDT.
 .RE
 .TP
-.BR --foreign[=<type>]
-File is foreign (non-Lustre layout) and is of type if specified.
-Presently only none or daos are defined types.
-.RE
+.BR --lazy
+Use file size and blocks from MDT, if available, to avoid extra RPCs.
 .TP
 .BR --maxdepth
 Limits find to decend at most \fIn\fR levels of directory tree.
@@ -199,16 +208,6 @@ and replacement using
 Layout was created with the specified
 .I pool
 name.  For composite files, this may match the pool of any component.
-.TP
-.BR --foreign[=<type>]
-Directory striping format is foreign (non-Lustre/free format) and is of type
-.BR <type>
-if specified. Presently only
-.BR none
-or
-.BR daos
-are defined types , though 32-bit numeric types can also be used.
-.TP
 .BR --print | -P
 Prints the file or directory name to standard output if it matches
 all specified parameters, one file per line with a trailing linefeed.

lustre/doc/llapi_get_lum_dir.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_lum_file_fd.3

lustre/doc/llapi_get_lum_dir_fd.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_lum_file_fd.3

lustre/doc/llapi_get_lum_file.3
@@ -1,0 +1,1 @@
+.so man3/llapi_get_lum_file_fd.3

lustre/doc/llapi_get_lum_file_fd.3
@@ -1,0 +1,164 @@
+.TH llapi_get_lum_file_fd 3 "2019 July 15" "Lustre User API"
+.SH NAME
+llapi_get_lum_file_fd, llapi_get_lum_dir_fd, llapi_get_lum_file,
+llapi_get_lum_dir \- get valid file attrbutes and LOV stripe information to
+the user.
+
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_get_lum_file_fd(int " dir_fd ", const char *" fname ",
+.BI "                          __u64 *" valid ", lstatx_t *" statx ",
+.BI "                          struct lov_user_md *" lum ",
+.BI "                          size_t " lumsize ");"
+.PP
+.BI "int llapi_get_lum_dir_fd(int " dir_fd ", __u64 *" valid ",
+.BI "                        lstatx_t *" statx ", struct lov_user_md *" lum ",
+.BI "                        size_t " lumsize ");"
+.PP
+.BI "int llapi_get_lum_file(const char *" path ", __u64 *" valid ",
+.BI "                       lstatx_t *" statx ", struct lov_user_md *" lum ",
+.BI "                       size_t " lumsize ");"
+.PP
+.BI "int llapi_get_lum_dir(const char *" path ", __u64 *" valid ",
+.BI "                      lstatx_t *" statx ", struct lov_user_md *" lum ",
+.BI "                      size_t " lumsize ");"
+.fi
+.SH DESCRIPTION
+.PP
+The function
+.BR llapi_get_lum_file_fd() ,
+.BR llapi_get_lum_dir_fd() ,
+.BR llapi_get_lum_file() ,
+and
+.BR llapi_get_lum_dir()
+returns file attributes and LOV stripe information by an ioctl on the dir.
+.BR dir_fd
+is a file descriptor that refers to a directory. For function
+.BR llapi_get_lum_file_fd() ,
+if
+.BR fname
+is provided, it is a relative pathname that is interpreted relative to the
+directory referred to by
+.BR dir_fd .
+
+The function
+.BR llapi_get_lum_dir_fd() ,
+.BR llapi_get_lum_file() ,
+and
+.BR llapi_get_lum_dir()
+can be used for applications that are only processing a single file or directory
+referred to by
+.BR dir_fd
+or
+.BR path .
+
+The function
+.BR llapi_get_lum_file_fd()
+can be used for applications that process lots of files. It avoids opening,
+locking, and closing each file on the client if that is not needed. Multiple
+calls of this llapi can be done on the parent dir referenced by
+.B dir_fd
+with a single open for all files in that directory, and it also does not
+pollute the client dcache with millions of dentries when traversing a large
+filesystem.
+
+The stripe information for the target file is returned to the user in the
+.B struct lov_user_md
+pointed to by
+.BR lum
+if it is not a NULL pointer. The file attribute information for the target file
+is returned in the
+.BR lstatx_t
+structure pointed to by
+.BR statx
+if it is not a NULL pointer.
+The output argument
+.BR valid
+contains the flags to indicate which fields in the returned file attributes
+have been valid filled in.
+
+The valid flags are as follows:
+.TP
+OBD_MD_FLSIZE
+The returned file size is known strictly correct.
+.TP
+OBD_MD_FLBLOCKS
+The returned file blocks is known strictly correct.
+.TP
+OBD_MD_FLLAZYSIZE
+The returned file size is approximate, may never have been strictly correct.
+.TP
+OBD_MD_FLLAZYBLOCKS
+The returned file blocks is approximate, may never have been strictly correct.
+.TP
+OBD_MD_FLID
+The returned object ID is valid.
+.TP
+OBD_MD_FLATIME
+The returned access time is valid.
+.TP
+OBD_MD_FLMTIME
+The returned data modification time is valid.
+.TP
+OBD_MD_FLCTIME
+The returned change time is valid.
+.TP
+OBD_MD_FLBLKSZ
+The returned block size is valid.
+.TP
+OBD_MD_FLMODE
+The returned access bits (mode & ~S_IFMT) is valid.
+.TP
+OBD_MD_FLTYPE
+The returned object type (mode & S_IFMT) is valid.
+.TP
+OBD_MD_FLUID
+The returned user ID is vaild.
+.TP
+OBD_MD_FLGID
+The returned group ID is valid.
+.TP
+OBD_MD_FLFLAGS
+The returned flags word is valid.
+.TP
+OBD_MD_FLNLINK
+The returned link count is valid.
+.TP
+OBD_MD_FLRDEV
+The returned device number is valid.
+.SH RETURN VALUES
+.LP
+.BR llapi_get_lum_file_fd() ,
+.BR llapi_get_lum_dir_fd() ,
+.BR llapi_get_lum_file() ,
+and
+.BR llapi_get_lum_dir()
+return 0 on success or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM -ENOMEM
+Insufficient memory to complete operation.
+.TP
+.SM -EFAULT
+Memory region is not properly mapped.
+.TP
+.SM -EINVAL
+One or more invalid arguments are given.
+.TP
+.SM -EOPNOTSUPP
+The operation is not supported.
+.TP
+.SM -ENOTTY
+File does not reside on a Lustre filesystem.
+.TP
+.SM -ENOENT
+.I path
+does not exist.
+.TP
+.SM -EBADF
+.I dir_fd
+is not a valid open file descriptor.
+.SH "SEE ALSO"
+.BR lustreapi (7)

lustre/include/lustre/lustreapi.h
@@ -273,7 +273,8 @@ struct find_param {
 				 fp_check_foreign:1,
 				 fp_exclude_foreign:1,
 				 fp_check_ext_size:1, /* extension size */
-				 fp_exclude_ext_size:1;
+				 fp_exclude_ext_size:1,
+				 fp_lazy:1;
 
 	enum llapi_layout_verbose fp_verbose;
 	int			 fp_quiet;
@@ -403,6 +404,16 @@ int llapi_fid2path(const char *device, const char *fidstr, char *path,
 int llapi_path2fid(const char *path, struct lu_fid *fid);
 int llapi_get_mdt_index_by_fid(int fd, const struct lu_fid *fid,
 			       int *mdt_index);
+int llapi_get_lum_file(const char *path, __u64 *valid, lstatx_t *statx,
+		       struct lov_user_md *lum, size_t lumsize);
+int llapi_get_lum_dir(const char *path, __u64 *valid, lstatx_t *statx,
+		      struct lov_user_md *lum, size_t lumsize);
+int llapi_get_lum_file_fd(int dir_fd, const char *fname, __u64 *valid,
+			  lstatx_t *statx, struct lov_user_md *lum,
+			  size_t lumsize);
+int llapi_get_lum_dir_fd(int dir_fd, __u64 *valid, lstatx_t *statx,
+			 struct lov_user_md *lum, size_t lumsize);
+
 int llapi_fd2fid(int fd, struct lu_fid *fid);
 /* get FID of parent dir + the related name of entry in this parent dir */
 int llapi_path2parent(const char *path, unsigned int linkno,

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1309,6 +1309,9 @@ lov_mds_md_max_stripe_count(size_t buf_size, __u32 lmm_magic)
 #define OBD_MD_FLPROJID      (0x0100000000000000ULL) /* project ID */
 #define OBD_MD_SECCTX        (0x0200000000000000ULL) /* embed security xattr */
 
+#define OBD_MD_FLLAZYSIZE    (0x0400000000000000ULL) /* Lazy size */
+#define OBD_MD_FLLAZYBLOCKS  (0x0800000000000000ULL) /* Lazy blocks */
+
 #define OBD_MD_FLALLQUOTA (OBD_MD_FLUSRQUOTA | \
 			   OBD_MD_FLGRPQUOTA | \
 			   OBD_MD_FLPRJQUOTA)

commit e37adf512c6b08a3c008da051d76f68ad63d508c

LU-930 utils: add --lazy option to 'lfs find' usage
The usage message for "lfs find" does not show the "--lazy"
option to allow checking LSOM data from the MDT instead of
getting size and blocks from the OSTs.

Also sort the options in the "lfs find" usage to be roughly
in alphabetical order.

Test-Parameters: trivial
Fixes: 11aa7f8704c4 ("LU-11367 som: integrate LSOM with lfs find")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Iafea18b2d74d889bb258f2be4b3af0f9203ebbe5
Reviewed-on: https://review.whamcloud.com/45816
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -399,32 +399,29 @@ command_t cmdlist[] = {
 	{"find", lfs_find, 0,
 	 "find files matching given attributes recursively in directory tree.\n"
 	 "usage: find <directory|filename> ...\n"
-	 "     [[!] --atime|-A [+-]N[smhdwy]] [[!] --ctime|-C [+-]N[smhdwy]]\n"
-	 "     [[!] --mtime|-M [+-]N[smhdwy]]\n"
-	 "     [[!] --btime|--Btime|-B [+-]N[smhdwy]] [--help|-h]\n"
-	 "     [[!] --newer[XY] <reference>] [[!] --blocks|-b N]\n"
-	 "     [--maxdepth|-D N] [[!] --mdt-index|--mdt|-m <uuid|index,...>]\n"
-	 "     [[!] --name|-n <pattern>] [[!] --ost|-O <uuid|index,...>]\n"
-	 "     [[!] --perm [/-]mode] [[!] --pool <pool>] [--print|-P]\n"
-	 "     [--print0|-0] [[!] --projid <projid>]\n"
-	 "     [[!] --size|-s [+-]N[bkMGTPE]]\n"
-	 "     [[!] --stripe-count|-c [+-]<stripes>]\n"
-	 "     [[!] --stripe-index|-i <index,...>]\n"
-	 "     [[!] --stripe-size|-S [+-]N[kMGT]] [[!] --type|-t <filetype>]\n"
-	 "     [[!] --extension-size|--ext-size|-z [+-]N[kMGT]]\n"
-	 "     [[!] --gid|-g|--group|-G <gid>|<gname>]\n"
-	 "     [[!] --uid|-u|--user|-U <uid>|<uname>]\n"
-	 "     [[!] --layout|-L released,raid0,mdt]\n"
-	 "     [[!] --foreign[=<foreign_type>]]\n"
-	 "     [[!] --component-count [+-]<comp_cnt>]\n"
+	 "     [[!] --atime|-A [+-]N[smhdwy]] [[!] --btime|-B [+-]N[smhdwy]]\n"
+	 "     [[!] --ctime|-C [+-]N[smhdwy]] [[!] --mtime|-M [+-]N[smhdwy]]\n"
+	 "     [[!] --blocks|-b N] [[!] --component-count [+-]<comp_cnt>]\n"
 	 "     [[!] --component-start [+-]N[kMGTPE]]\n"
 	 "     [[!] --component-end|-E [+-]N[kMGTPE]]\n"
 	 "     [[!] --component-flags {init,stale,prefer,offline,nosync,extension}]\n"
+	 "     [[!] --extension-size|--ext-size|-z [+-]N[kMGT]]\n"
+	 "     [[!] --foreign[=<foreign_type>]]\n"
+	 "     [[!] --gid|-g|--group|-G <gid>|<gname>] [--help|-h]\n"
+	 "     [[!] --layout|-L released,raid0,mdt] [--lazy]\n"
+	 "     [--maxdepth|-D N] [[!] --mdt-count|-T [+-]<stripes>]\n"
+	 "     [[!] --mdt-hash|-H <[^][blm],[^]fnv_1a_64,all_char,crush,...>\n"
+	 "     [[!] --mdt-index|--mdt|-m <uuid|index,...>]\n"
 	 "     [[!] --mirror-count|-N [+-]<n>]\n"
 	 "     [[!] --mirror-state <[^]state>]\n"
-	 "     [[!] --mdt-count|-T [+-]<stripes>]\n"
-	 "     [[!] --mdt-hash|-H <[^][blm],[^]fnv_1a_64,all_char,crush,...>\n"
-	 "     [[!] --mdt-index|-m <uuid|index,...>]\n"
+	 "     [[!] --name|-n <pattern>] [[!] --newer[XY] <reference>]\n"
+	 "     [[!] --ost|-O <uuid|index,...>] [[!] --perm [/-]mode]\n"
+	 "     [[!] --pool <pool>] [--print|-P] [--print0|-0]\n"
+	 "     [[!] --projid <projid>] [[!] --size|-s [+-]N[bkMGTPE]]\n"
+	 "     [[!] --stripe-count|-c [+-]<stripes>]\n"
+	 "     [[!] --stripe-index|-i <index,...>]\n"
+	 "     [[!] --stripe-size|-S [+-]N[kMGT]] [[!] --type|-t <filetype>]\n"
+	 "     [[!] --uid|-u|--user|-U <uid>|<uname>]\n"
 	 "\t !: used before an option indicates 'NOT' requested attribute\n"
 	 "\t -: used before a value indicates less than requested value\n"
 	 "\t +: used before a value indicates more than requested value\n"
@@ -4896,6 +4893,8 @@ static int lfs_find(int argc, char **argv)
 /* find	{ .val = 'o'	.name = "or", .has_arg = no_argument }, like find(1) */
 	{ .val = 'O',	.name = "obd",		.has_arg = required_argument },
 	{ .val = 'O',	.name = "ost",		.has_arg = required_argument },
+	{ .val = LFS_FIND_PERM,
+			.name = "perm",		.has_arg = required_argument },
 	/* no short option for pool yet, can be 'p' after 2.18 */
 	{ .val = LFS_POOL_OPT,
 			.name = "pool",		.has_arg = required_argument },
@@ -4910,8 +4909,6 @@ static int lfs_find(int argc, char **argv)
 	{ .val = 'S',	.name = "stripe-size",	.has_arg = required_argument },
 	{ .val = 'S',	.name = "stripe_size",	.has_arg = required_argument },
 	{ .val = 't',	.name = "type",		.has_arg = required_argument },
-	{ .val = LFS_FIND_PERM,
-			.name = "perm",		.has_arg = required_argument },
 	{ .val = 'T',	.name = "mdt-count",	.has_arg = required_argument },
 	{ .val = 'u',	.name = "uid",		.has_arg = required_argument },
 	{ .val = 'U',	.name = "user",		.has_arg = required_argument },