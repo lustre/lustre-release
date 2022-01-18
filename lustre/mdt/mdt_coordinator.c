lustre/lustre-release

commit a558006b83dfe32798cce644aa888c37e805d50b

LU-13920 hsm: process hsm_actions only after mdd setup
There is no guarantee that MDD setup is finished
at the moment when coordinator is started by
config params processing. If MDD setup is not finished,
hsm actions llog is not inited(mdd_hsm_actions_llog_init).
Hence hsm_pending_restore will not be called, i.e.
RESTORE requests will be sent to agents without taken
layout locks. I believe it may cause different problems.
I faced at least a kernel panic when llog includes
RESTORE request in ARS_WAITING that hasn't bee sent to
agent before failover. And the 2nd one RESTORE request
to the same fid was resent after recovery. Finally
agent handles to RESTORE to the same FID in parallel
that resulted in a panic with following bt:

BUG: unable to handle kernel NULL pointer dereference at 0000000000000040
IP: [<ffffffffc0b03bec>] thandle_get_sub_by_dt+0x14c/0x420 [ptlrpc]
...
[<ffffffffc1202732>] lod_sub_get_thandle+0x2f2/0x400 [lod]
[<ffffffffc1205021>] lod_sub_declare_xattr_set+0x61/0x300 [lod]
[<ffffffffc11db0d5>] lod_obj_stripe_replace_parent_fid_cb+0x245/0x450 [lod]
[<ffffffffc11eae0e>] lod_obj_for_each_stripe+0x11e/0x2d0 [lod]
[<ffffffffc11ebfe2>] lod_replace_parent_fid+0x2a2/0x390 [lod]
[<ffffffffc11dae90>] ? lod_attr_get+0x110/0x110 [lod]
[<ffffffffc11f8faf>] lod_declare_xattr_set+0x24f/0xf70 [lod]
[<ffffffffc077b251>] ? lprocfs_counter_sub+0xc1/0x130 [obdclass]
[<ffffffffc1091ff4>] mdo_declare_xattr_set+0x74/0x2b0 [mdd]
[<ffffffffc077b129>] ? lprocfs_counter_add+0xf9/0x160 [obdclass]
[<ffffffffc0fa2f7b>] ? osd_trans_create+0xbb/0x620 [osd_ldiskfs]
[<ffffffffc1094903>] mdd_declare_xattr_set+0x33/0x70 [mdd]
[<ffffffffc1094b4e>] mdd_object_pfid_replace+0x7e/0x1e0 [mdd]
[<ffffffffc109c2c6>] mdd_swap_layouts+0xa76/0x1dc0 [mdd]
[<ffffffffc10a5e1a>] ? mdd_trans_stop+0x3a/0x174 [mdd]
[<ffffffffc114b489>] hsm_cdt_request_completed.isra.14+0xc89/0xf50 [mdt]
[<ffffffffc077b129>] ? lprocfs_counter_add+0xf9/0x160 [obdclass]

lustre/mdt/mdt_coordinator.c
@@ -1071,11 +1071,12 @@ int  mdt_hsm_cdt_fini(struct mdt_device *mdt)
  */
 static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 {
-	struct coordinator	*cdt = &mdt->mdt_coordinator;
+	struct coordinator *cdt = &mdt->mdt_coordinator;
 	struct mdt_thread_info *cdt_mti;
-	int			 rc;
-	void			*ptr;
-	struct task_struct	*task;
+	unsigned int i = 0;
+	int rc;
+	void *ptr;
+	struct task_struct *task;
 	ENTRY;
 
 	/* functions defined but not yet used
@@ -1103,6 +1104,14 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 	cdt->cdt_user_request_mask = (1UL << HSMA_RESTORE);
 	cdt->cdt_group_request_mask = (1UL << HSMA_RESTORE);
 	cdt->cdt_other_request_mask = (1UL << HSMA_RESTORE);
+
+	/* wait until MDD initialize hsm actions llog */
+	while (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state) && i < obd_timeout) {
+		schedule_timeout_interruptible(cfs_time_seconds(1));
+		i++;
+	}
+	if (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state))
+		CWARN("%s: trying to init HSM before MDD\n", mdt_obd_name(mdt));
 
 	/* to avoid deadlock when start is made through sysfs
 	 * sysfs entries are created by the coordinator thread

commit e26d7cc3992252e5fce5a51aee716f933b04c13a

LU-14399 hsm: process hsm_actions in coordinator
Wait for mdd setup in a separate thread to don't block mount.
The patch adds conf-sanity_131 to verify the fix.

Change-Id: Ifd0e8969d7ed4f8944ab61ab0e0ebe2655bad003
Fixes: a558006b ("LU-13920 hsm: process hsm_actions only after mdd setup")
HPE-bug-id: LUS-9750
Signed-off-by: Sergey Cheremencev <sergey.cheremencev@hpe.com>
Reviewed-on: https://review.whamcloud.com/41445
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_coordinator.c
@@ -549,7 +549,31 @@ static int set_cdt_state(struct coordinator *cdt, enum cdt_states new_state)
 	return rc;
 }
 
+static int mdt_hsm_pending_restore(struct mdt_thread_info *mti);
 
+static void cdt_start_pending_restore(struct mdt_device *mdt,
+				      struct coordinator *cdt)
+{
+	struct mdt_thread_info *cdt_mti;
+	unsigned int i = 0;
+	int rc;
+
+	/* wait until MDD initialize hsm actions llog */
+	while (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state) && i < obd_timeout) {
+		schedule_timeout_interruptible(cfs_time_seconds(1));
+		i++;
+	}
+	if (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state))
+		CWARN("%s: trying to init HSM before MDD\n", mdt_obd_name(mdt));
+
+	/* set up list of started restore requests */
+	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
+	rc = mdt_hsm_pending_restore(cdt_mti);
+	if (rc)
+		CERROR("%s: cannot take the layout locks needed for registered restore: %d\n",
+		       mdt_obd_name(mdt), rc);
+
+}
 
 /**
  * coordinator thread
@@ -579,6 +603,7 @@ static int mdt_coordinator(void *data)
 
 	/* Inform mdt_hsm_cdt_start(). */
 	wake_up(&cdt->cdt_waitq);
+	cdt_start_pending_restore(mdt, cdt);
 
 	while (1) {
 		int i;
@@ -1103,7 +1128,6 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 {
 	struct coordinator *cdt = &mdt->mdt_coordinator;
 	struct mdt_thread_info *cdt_mti;
-	unsigned int i = 0;
 	int rc;
 	void *ptr;
 	struct task_struct *task;
@@ -1135,28 +1159,13 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 	cdt->cdt_group_request_mask = (1UL << HSMA_RESTORE);
 	cdt->cdt_other_request_mask = (1UL << HSMA_RESTORE);
 
-	/* wait until MDD initialize hsm actions llog */
-	while (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state) && i < obd_timeout) {
-		schedule_timeout_interruptible(cfs_time_seconds(1));
-		i++;
-	}
-	if (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state))
-		CWARN("%s: trying to init HSM before MDD\n", mdt_obd_name(mdt));
-
 	/* to avoid deadlock when start is made through sysfs
 	 * sysfs entries are created by the coordinator thread
 	 */
-	/* set up list of started restore requests */
-	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
-	rc = mdt_hsm_pending_restore(cdt_mti);
-	if (rc)
-		CERROR("%s: cannot take the layout locks needed"
-		       " for registered restore: %d\n",
-		       mdt_obd_name(mdt), rc);
-
 	if (mdt->mdt_bottom->dd_rdonly)
 		RETURN(0);
 
+	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
 	task = kthread_run(mdt_coordinator, cdt_mti, "hsm_cdtr");
 	if (IS_ERR(task)) {
 		rc = PTR_ERR(task);

lustre/tests/conf-sanity.sh
@@ -9397,6 +9397,28 @@ test_131() {
 }
 run_test 131 "MDT backup restore with project ID"
 
+test_132() {
+	local err_cnt
+	local err_cnt2
+
+	reformat
+	combined_mgs_mds || start_mgs || error "unable to start MGS"
+	start_mdt 1 || error "unable to start mdt1"
+
+	err_cnt=$(do_facet mds1 dmesg | grep -c "cannot take the layout locks")
+	stop_mdt 1 || error "stop mdt1 failed"
+
+	[ "$mds1_FSTYPE" == zfs ] && import_zpool mds1
+	do_facet mds1 $TUNEFS --param mdt.hsm_control=enabled $(mdsdevname 1) ||
+		error "tunefs failed"
+	start_mdt 1 || error "cannot start mdt1"
+
+	err_cnt2=$(do_facet mds1 dmesg | grep -c "cannot take the layout locks")
+	[ $err_cnt -eq $err_cnt2 ] || error "Can not take the layout lock"
+	stop_mdt 1 || error "stop mdt1 failed"
+}
+run_test 132 "hsm_actions processed after failover"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi