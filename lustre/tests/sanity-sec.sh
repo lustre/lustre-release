lustre/lustre-release

commit 40d91eafe257fb407d27c54cd2f7ae9961672f60

LU-12275 sec: atomicity of encryption context getting/setting
Encryption layer needs to set an encryption context on files and dirs
that are encrypted. This context is stored as an extended attribute,
that then needs to be fetched upon metadata ops like lookup, getattr,
open, truncate, and layout.

With this patch we send encryption context to the MDT along with
create RPCs. This closes the insecure window between creation and
setting of the encryption context, and saves a setxattr request.

This patch also introduces a way to have the MDT return encryption
context upon granted lock reply, making the encryption context
retrieval atomic, and sparing the client an additional getxattr
request.

Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=ldiskfs mdscount=2 mdtcount=4
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=zfs mdscount=2 mdtcount=4
Test-Parameters: clientversion=2.12 env=SANITY_EXCEPT="27M 56ra 151 156 802"
Test-Parameters: serverversion=2.12 env=SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Test-Parameters: serverversion=2.12 clientdistro=el8.1 env=SANITYN_EXCEPT=106,SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I45599cdff13d5587103aff6edd699abcda6cb8f4
Reviewed-on: https://review.whamcloud.com/38430
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -2966,7 +2966,8 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
 	ll_unlock_md_op_lsm(op_data);
 	security_release_secctx(op_data->op_file_secctx,
 				op_data->op_file_secctx_size);
-        OBD_FREE_PTR(op_data);
+	llcrypt_free_ctx(op_data->op_file_encctx, op_data->op_file_encctx_size);
+	OBD_FREE_PTR(op_data);
 }
 
 int ll_show_options(struct seq_file *seq, struct dentry *dentry)

lustre/mdc/mdc_request.c
@@ -455,7 +455,9 @@ static int mdc_getxattr(struct obd_export *exp, const struct lu_fid *fid,
 	LASSERT(obd_md_valid == OBD_MD_FLXATTR ||
 		obd_md_valid == OBD_MD_FLXATTRLS);
 
-	/* The below message is checked in sanity-selinux.sh test_20d */
+	/* Message below is checked in sanity-selinux test_20d
+	 * and sanity-sec test_49
+	 */
 	CDEBUG(D_INFO, "%s: get xattr '%s' for "DFID"\n",
 	       exp->exp_obd->obd_name, name, PFID(fid));
 	rc = mdc_xattr_common(exp, &RQF_MDS_GETXATTR, fid, MDS_GETXATTR,

lustre/mdt/mdt_handler.c
@@ -1913,6 +1913,12 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 		}
 
 		rc = mdt_pack_secctx_in_reply(info, child);
+		if (unlikely(rc)) {
+			mdt_object_unlock(info, child, lhc, 1);
+			RETURN(rc);
+		}
+
+		rc = mdt_pack_encctx_in_reply(info, child);
 		if (unlikely(rc))
 			mdt_object_unlock(info, child, lhc, 1);
 		RETURN(rc);
@@ -2102,6 +2108,12 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 	}
 
 	rc = mdt_pack_secctx_in_reply(info, child);
+	if (unlikely(rc)) {
+		mdt_object_unlock(info, child, lhc, 1);
+		GOTO(out_child, rc);
+	}
+
+	rc = mdt_pack_encctx_in_reply(info, child);
 	if (unlikely(rc)) {
 		mdt_object_unlock(info, child, lhc, 1);
 		GOTO(out_child, rc);
@@ -2569,17 +2581,27 @@ static void mdt_preset_secctx_size(struct mdt_thread_info *info)
 	    req_capsule_has_field(pill, &RMF_FILE_SECCTX_NAME,
 				  RCL_CLIENT)) {
 		if (req_capsule_get_size(pill, &RMF_FILE_SECCTX_NAME,
-					 RCL_CLIENT) != 0) {
+					 RCL_CLIENT) != 0)
 			/* pre-set size in server part with max size */
 			req_capsule_set_size(pill, &RMF_FILE_SECCTX,
 					     RCL_SERVER,
-					     info->mti_mdt->mdt_max_ea_size);
-		} else {
+					     OBD_MAX_DEFAULT_EA_SIZE);
+		else
 			req_capsule_set_size(pill, &RMF_FILE_SECCTX,
 					     RCL_SERVER, 0);
-		}
 	}
+}
 
+static void mdt_preset_encctx_size(struct mdt_thread_info *info)
+{
+	struct req_capsule *pill = info->mti_pill;
+
+	if (req_capsule_has_field(pill, &RMF_FILE_ENCCTX,
+				  RCL_SERVER))
+		/* pre-set size in server part with max size */
+		req_capsule_set_size(pill, &RMF_FILE_ENCCTX,
+				     RCL_SERVER,
+				     info->mti_mdt->mdt_max_mdsize);
 }
 
 static int mdt_reint_internal(struct mdt_thread_info *info,
@@ -2621,6 +2643,7 @@ static int mdt_reint_internal(struct mdt_thread_info *info,
 				     LUSTRE_POSIX_ACL_MAX_SIZE_OLD);
 
 	mdt_preset_secctx_size(info);
+	mdt_preset_encctx_size(info);
 
 	rc = req_capsule_server_pack(pill);
 	if (rc != 0) {
@@ -3872,6 +3895,7 @@ static int mdt_unpack_req_pack_rep(struct mdt_thread_info *info,
 					     LUSTRE_POSIX_ACL_MAX_SIZE_OLD);
 
 		mdt_preset_secctx_size(info);
+		mdt_preset_encctx_size(info);
 
 		rc = req_capsule_server_pack(pill);
 		if (rc)

lustre/ptlrpc/layout.c
@@ -213,7 +213,8 @@ static const struct req_msg_field *mds_reint_create_acl_client[] = {
 	&RMF_DLM_REQ,
 	&RMF_FILE_SECCTX_NAME,
 	&RMF_FILE_SECCTX,
-	&RMF_SELINUX_POL
+	&RMF_SELINUX_POL,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *mds_reint_create_sym_client[] = {
@@ -225,7 +226,8 @@ static const struct req_msg_field *mds_reint_create_sym_client[] = {
 	&RMF_DLM_REQ,
 	&RMF_FILE_SECCTX_NAME,
 	&RMF_FILE_SECCTX,
-	&RMF_SELINUX_POL
+	&RMF_SELINUX_POL,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *mds_reint_open_client[] = {
@@ -237,7 +239,8 @@ static const struct req_msg_field *mds_reint_open_client[] = {
 	&RMF_EADATA,
 	&RMF_FILE_SECCTX_NAME,
 	&RMF_FILE_SECCTX,
-	&RMF_SELINUX_POL
+	&RMF_SELINUX_POL,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *mds_reint_open_server[] = {
@@ -467,7 +470,8 @@ static const struct req_msg_field *ldlm_intent_open_server[] = {
 	&RMF_CAPA1,
 	&RMF_CAPA2,
 	&RMF_NIOBUF_INLINE,
-	&RMF_FILE_SECCTX
+	&RMF_FILE_SECCTX,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *ldlm_intent_getattr_client[] = {
@@ -488,7 +492,8 @@ static const struct req_msg_field *ldlm_intent_getattr_server[] = {
 	&RMF_ACL,
 	&RMF_CAPA1,
 	&RMF_FILE_SECCTX,
-	&RMF_DEFAULT_MDT_MD
+	&RMF_DEFAULT_MDT_MD,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *ldlm_intent_create_client[] = {
@@ -501,7 +506,8 @@ static const struct req_msg_field *ldlm_intent_create_client[] = {
 	&RMF_EADATA,
 	&RMF_FILE_SECCTX_NAME,
 	&RMF_FILE_SECCTX,
-	&RMF_SELINUX_POL
+	&RMF_SELINUX_POL,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *ldlm_intent_open_client[] = {
@@ -515,7 +521,8 @@ static const struct req_msg_field *ldlm_intent_open_client[] = {
 	&RMF_EADATA,
 	&RMF_FILE_SECCTX_NAME,
 	&RMF_FILE_SECCTX,
-	&RMF_SELINUX_POL
+	&RMF_SELINUX_POL,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *ldlm_intent_getxattr_client[] = {
@@ -1064,6 +1071,10 @@ EXPORT_SYMBOL(RMF_FILE_SECCTX_NAME);
 struct req_msg_field RMF_FILE_SECCTX =
 	DEFINE_MSGF("file_secctx", RMF_F_NO_SIZE_CHECK, -1, NULL, NULL);
 EXPORT_SYMBOL(RMF_FILE_SECCTX);
+
+struct req_msg_field RMF_FILE_ENCCTX =
+	DEFINE_MSGF("file_encctx", RMF_F_NO_SIZE_CHECK, -1, NULL, NULL);
+EXPORT_SYMBOL(RMF_FILE_ENCCTX);
 
 struct req_msg_field RMF_LLOGD_BODY =
         DEFINE_MSGF("llogd_body", 0,

lustre/tests/sanity-sec.sh
@@ -3533,6 +3533,61 @@ test_48b() {
 }
 run_test 48b "encrypted file: concurrent truncate"
 
+trace_cmd() {
+	local cmd="$@"
+	local xattr_name="security.c"
+
+	sync ; sync ; echo 3 > /proc/sys/vm/drop_caches
+	$LCTL set_param debug=+info
+	$LCTL clear
+
+	echo $cmd
+	eval $cmd
+
+	$LCTL dk | grep -E "get xattr '${xattr_name}'|get xattrs"
+	[ $? -ne 0 ] || error "get xattr event was triggered"
+}
+
+test_49() {
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	stack_trap cleanup_for_enc_tests EXIT
+	setup_for_enc_tests
+
+	local dirname=$DIR/$tdir/subdir
+
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+	trace_cmd $LFS setdirstripe -i 1 $dirname/d2
+	trace_cmd $LFS migrate -m 0 $dirname/d2
+
+	$LFS setdirstripe -i 1 -c 1 $dirname/d3
+	dirname=$dirname/d3/subdir
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+}
+run_test 49 "Avoid getxattr for encryption context"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {

lustre/include/lustre_crypto.h
@@ -30,6 +30,9 @@
 #define _LUSTRE_CRYPTO_H_
 
 struct ll_sb_info;
+int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
+		    bool preload);
+void llcrypt_free_ctx(void *encctx, __u32 size);
 bool ll_sbi_has_test_dummy_encryption(struct ll_sb_info *sbi);
 bool ll_sbi_has_encrypt(struct ll_sb_info *sbi);
 void ll_sbi_set_encrypt(struct ll_sb_info *sbi, bool set);

lustre/include/lustre_export.h
@@ -475,6 +475,11 @@ static inline int exp_connect_sepol(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_SELINUX_POLICY);
 }
 
+static inline int exp_connect_encrypt(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_req_layout.h
@@ -59,7 +59,7 @@ enum req_location {
 };
 
 /* Maximal number of fields (buffers) in a request message. */
-#define REQ_MAX_FIELD_NR 11
+#define REQ_MAX_FIELD_NR 12
 
 struct req_capsule {
         struct ptlrpc_request   *rc_req;
@@ -260,6 +260,7 @@ extern struct req_msg_field RMF_CLOSE_DATA;
 extern struct req_msg_field RMF_FILE_SECCTX_NAME;
 extern struct req_msg_field RMF_FILE_SECCTX;
 extern struct req_msg_field RMF_FID_ARRAY;
+extern struct req_msg_field RMF_FILE_ENCCTX;
 
 /*
  * connection handle received in MDS_CONNECT request.

commit faf057b46bc770a1a69cacd59e65a40a4b18b9fd

LU-15176 sec: allow subdir mount of encrypted dir
In case of sub-directory mount of an encrypted directory, we need to
retrieve the encryption context of the root inode of the filesystem.
This is done by making the MDT return this upon getattr reply.

Also add sanity-sec test_60 to exercise this capability.

Fixes: 40d91eafe2 ("LU-12275 sec: atomicity of encryption context getting/setting")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Ic7a273813533f2904225011b247cdfe995ce9be8
Reviewed-on: https://review.whamcloud.com/45407
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -274,6 +274,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 	u64 valid;
 	int size, err, checksum;
 	bool api32;
+	void *encctx;
+	int encctxlen;
 
 	ENTRY;
 	sbi->ll_md_obd = class_name2obd(md);
@@ -642,7 +644,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 
 	/* make root inode
 	 * XXX: move this to after cbd setup? */
-	valid = OBD_MD_FLGETATTR | OBD_MD_FLBLOCKS | OBD_MD_FLMODEASIZE;
+	valid = OBD_MD_FLGETATTR | OBD_MD_FLBLOCKS | OBD_MD_FLMODEASIZE |
+		OBD_MD_ENCCTX;
 	if (test_bit(LL_SBI_ACL, sbi->ll_flags))
 		valid |= OBD_MD_FLACL;
 
@@ -656,6 +659,13 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 
 	err = md_getattr(sbi->ll_md_exp, op_data, &request);
 
+	/* We need enc ctx info, so reset it in op_data to
+	 * prevent it from being freed.
+	 */
+	encctx = op_data->op_file_encctx;
+	encctxlen = op_data->op_file_encctx_size;
+	op_data->op_file_encctx = NULL;
+	op_data->op_file_encctx_size = 0;
 	OBD_FREE_PTR(op_data);
 	if (err) {
 		CERROR("%s: md_getattr failed for root: rc = %d\n",
@@ -675,7 +685,6 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 	api32 = test_bit(LL_SBI_32BIT_API, sbi->ll_flags);
 	root = ll_iget(sb, cl_fid_build_ino(&sbi->ll_root_fid, api32), &lmd);
 	md_free_lustre_md(sbi->ll_md_exp, &lmd);
-	ptlrpc_req_finished(request);
 
 	if (IS_ERR(root)) {
 		lmd_clear_acl(&lmd);
@@ -683,8 +692,21 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 		root = NULL;
 		CERROR("%s: bad ll_iget() for root: rc = %d\n",
 		       sbi->ll_fsname, err);
+		ptlrpc_req_finished(request);
 		GOTO(out_root, err);
 	}
+
+	if (encctxlen) {
+		CDEBUG(D_SEC,
+		       "server returned encryption ctx for root inode "DFID"\n",
+		       PFID(&sbi->ll_root_fid));
+		err = ll_set_encflags(root, encctx, encctxlen, true);
+		if (err)
+			CWARN("%s: cannot set enc ctx for "DFID": rc = %d\n",
+			      sbi->ll_fsname,
+			      PFID(&sbi->ll_root_fid), err);
+	}
+	ptlrpc_req_finished(request);
 
 	checksum = test_bit(LL_SBI_CHECKSUM, sbi->ll_flags);
 	if (sbi->ll_checksum_set) {
@@ -3276,9 +3298,11 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 	if (ll_need_32bit_api(ll_i2sbi(i1)))
 		op_data->op_cli_flags |= CLI_API32;
 
-	if (opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_CREATE) {
+	if ((i2 && is_root_inode(i2)) ||
+	    opc == LUSTRE_OPC_LOOKUP || opc == LUSTRE_OPC_CREATE) {
 		/* In case of lookup, ll_setup_filename() has already been
 		 * called in ll_lookup_it(), so just take provided name.
+		 * Also take provided name if we are dealing with root inode.
 		 */
 		fname.disk_name.name = (unsigned char *)name;
 		fname.disk_name.len = namelen;

lustre/mdc/mdc_dev.c
@@ -1290,6 +1290,7 @@ static int mdc_io_data_version_start(const struct lu_env *env,
 
 	req_capsule_set_size(&req->rq_pill, &RMF_ACL, RCL_SERVER, 0);
 	req_capsule_set_size(&req->rq_pill, &RMF_MDT_MD, RCL_SERVER, 0);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX, RCL_SERVER, 0);
 	ptlrpc_request_set_replen(req);
 
 	req->rq_interpret_reply = mdc_data_version_interpret;

lustre/mdc/mdc_request.c
@@ -161,7 +161,8 @@ out:
  * layouts.  --umka
  */
 static int mdc_getattr_common(struct obd_export *exp,
-                              struct ptlrpc_request *req)
+			      struct ptlrpc_request *req,
+			      struct md_op_data *op_data)
 {
         struct req_capsule *pill = &req->rq_pill;
         struct mdt_body    *body;
@@ -189,6 +190,18 @@ static int mdc_getattr_common(struct obd_export *exp,
 			RETURN(-EPROTO);
 	}
 
+	/* If encryption context was returned by MDT, put it in op_data
+	 * so that caller can set it on inode and save an extra getxattr.
+	 */
+	if (op_data && op_data->op_valid & OBD_MD_ENCCTX &&
+	    body->mbo_valid & OBD_MD_ENCCTX) {
+		op_data->op_file_encctx =
+			req_capsule_server_get(pill, &RMF_FILE_ENCCTX);
+		op_data->op_file_encctx_size =
+			req_capsule_get_size(pill, &RMF_FILE_ENCCTX,
+					     RCL_SERVER);
+	}
+
         RETURN(0);
 }
 
@@ -207,6 +220,7 @@ static int mdc_getattr(struct obd_export *exp, struct md_op_data *op_data,
 		       struct ptlrpc_request **request)
 {
 	struct ptlrpc_request *req;
+	struct obd_device *obd = class_exp2obd(exp);
 	struct obd_import *imp = class_exp2cliimp(exp);
 	__u32 acl_bufsize = LUSTRE_POSIX_ACL_MAX_SIZE_OLD;
 	int rc;
@@ -238,9 +252,16 @@ again:
 	req_capsule_set_size(&req->rq_pill, &RMF_ACL, RCL_SERVER, acl_bufsize);
 	req_capsule_set_size(&req->rq_pill, &RMF_MDT_MD, RCL_SERVER,
 			     op_data->op_mode);
+	if (exp_connect_encrypt(exp) && op_data->op_valid & OBD_MD_ENCCTX)
+		req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX,
+				     RCL_SERVER,
+				     obd->u.cli.cl_max_mds_easize);
+	else
+		req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX,
+				     RCL_SERVER, 0);
 	ptlrpc_request_set_replen(req);
 
-	rc = mdc_getattr_common(exp, req);
+	rc = mdc_getattr_common(exp, req, op_data);
 	if (rc) {
 		if (rc == -ERANGE) {
 			acl_bufsize = min_t(__u32,
@@ -294,6 +315,7 @@ again:
 	req_capsule_set_size(&req->rq_pill, &RMF_MDT_MD, RCL_SERVER,
 			     op_data->op_mode);
 	req_capsule_set_size(&req->rq_pill, &RMF_ACL, RCL_SERVER, acl_bufsize);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX, RCL_SERVER, 0);
 	ptlrpc_request_set_replen(req);
 	if (op_data->op_bias & MDS_FID_OP) {
 		struct mdt_body *b = req_capsule_client_get(&req->rq_pill,
@@ -305,7 +327,7 @@ again:
 		}
 	}
 
-	rc = mdc_getattr_common(exp, req);
+	rc = mdc_getattr_common(exp, req, NULL);
 	if (rc) {
 		if (rc == -ERANGE) {
 			acl_bufsize = min_t(__u32,

lustre/mdt/mdt_handler.c
@@ -1321,6 +1321,20 @@ out:
 	RETURN(rc);
 }
 
+static void mdt_preset_encctx_size(struct mdt_thread_info *info)
+{
+	struct req_capsule *pill = info->mti_pill;
+
+	ENTRY;
+	if (req_capsule_has_field(pill, &RMF_FILE_ENCCTX,
+				  RCL_SERVER))
+		/* pre-set size in server part with max size */
+		req_capsule_set_size(pill, &RMF_FILE_ENCCTX,
+				     RCL_SERVER,
+				     info->mti_mdt->mdt_max_mdsize);
+	EXIT;
+}
+
 static int mdt_getattr_internal(struct mdt_thread_info *info,
 				struct mdt_object *o, int ma_need)
 {
@@ -1641,6 +1655,7 @@ static int mdt_getattr(struct tgt_session_info *tsi)
 	 * enlarge the buffer when necessary. */
 	req_capsule_set_size(pill, &RMF_ACL, RCL_SERVER,
 			     LUSTRE_POSIX_ACL_MAX_SIZE_OLD);
+	mdt_preset_encctx_size(info);
 
 	rc = req_capsule_server_pack(pill);
 	if (unlikely(rc != 0))
@@ -1658,6 +1673,10 @@ static int mdt_getattr(struct tgt_session_info *tsi)
 	info->mti_cross_ref = !!(reqbody->mbo_valid & OBD_MD_FLCROSSREF);
 
 	rc = mdt_getattr_internal(info, obj, 0);
+	if (unlikely(rc))
+		GOTO(out_shrink, rc);
+
+	rc = mdt_pack_encctx_in_reply(info, obj);
 	EXIT;
 out_shrink:
 	mdt_client_compatibility(info);
@@ -2866,18 +2885,6 @@ static void mdt_preset_secctx_size(struct mdt_thread_info *info)
 			req_capsule_set_size(pill, &RMF_FILE_SECCTX,
 					     RCL_SERVER, 0);
 	}
-}
-
-static void mdt_preset_encctx_size(struct mdt_thread_info *info)
-{
-	struct req_capsule *pill = info->mti_pill;
-
-	if (req_capsule_has_field(pill, &RMF_FILE_ENCCTX,
-				  RCL_SERVER))
-		/* pre-set size in server part with max size */
-		req_capsule_set_size(pill, &RMF_FILE_ENCCTX,
-				     RCL_SERVER,
-				     info->mti_mdt->mdt_max_mdsize);
 }
 
 static int mdt_reint_internal(struct mdt_thread_info *info,

lustre/mdt/mdt_io.c
@@ -1586,6 +1586,7 @@ int mdt_data_version_get(struct tgt_session_info *tsi)
 
 	req_capsule_set_size(tsi->tsi_pill, &RMF_MDT_MD, RCL_SERVER, 0);
 	req_capsule_set_size(tsi->tsi_pill, &RMF_ACL, RCL_SERVER, 0);
+	req_capsule_set_size(tsi->tsi_pill, &RMF_FILE_ENCCTX, RCL_SERVER, 0);
 	rc = req_capsule_server_pack(tsi->tsi_pill);
 	if (unlikely(rc != 0))
 		RETURN(err_serious(rc));

lustre/ptlrpc/layout.c
@@ -565,12 +565,13 @@ static const struct req_msg_field *mds_getxattr_server[] = {
 };
 
 static const struct req_msg_field *mds_getattr_server[] = {
-        &RMF_PTLRPC_BODY,
-        &RMF_MDT_BODY,
-        &RMF_MDT_MD,
-        &RMF_ACL,
-        &RMF_CAPA1,
-        &RMF_CAPA2
+	&RMF_PTLRPC_BODY,
+	&RMF_MDT_BODY,
+	&RMF_MDT_MD,
+	&RMF_ACL,
+	&RMF_CAPA1,
+	&RMF_CAPA2,
+	&RMF_FILE_ENCCTX,
 };
 
 static const struct req_msg_field *mds_setattr_server[] = {

lustre/target/tgt_handler.c
@@ -458,6 +458,10 @@ static int tgt_handle_request0(struct tgt_session_info *tsi,
 					  body->oa.o_flags & OBD_FL_SHORT_IO) ?
 					 remote_nb[0].rnb_len : 0);
 		}
+		if (req_capsule_has_field(tsi->tsi_pill, &RMF_FILE_ENCCTX,
+					  RCL_SERVER))
+			req_capsule_set_size(tsi->tsi_pill, &RMF_FILE_ENCCTX,
+					     RCL_SERVER, 0);
 
 		rc = req_capsule_server_pack(tsi->tsi_pill);
 	}

lustre/tests/sanity-sec.sh
@@ -2712,6 +2712,19 @@ setup_dummy_key() {
 	echo -n -e "${key}" | keyctl padd logon fscrypt:4242424242424242 @s
 }
 
+insert_enc_key() {
+	cancel_lru_locks
+	sync ; echo 3 > /proc/sys/vm/drop_caches
+	setup_dummy_key
+}
+
+remove_enc_key() {
+	cancel_lru_locks
+	sync ; echo 3 > /proc/sys/vm/drop_caches
+	keyctl revoke $(keyctl show | awk '$7 ~ "^fscrypt:" {print $1}')
+	keyctl reap
+}
+
 setup_for_enc_tests() {
 	# remount client with test_dummy_encryption option
 	if is_mounted $MOUNT; then
@@ -4535,18 +4548,6 @@ test_58() {
 }
 run_test 58 "access to enc file's xattrs"
 
-insert_enc_key() {
-	sync ; echo 3 > /proc/sys/vm/drop_caches
-	setup_dummy_key
-}
-
-remove_env_key() {
-	cancel_lru_locks
-	sync ; echo 3 > /proc/sys/vm/drop_caches
-	keyctl revoke $(keyctl show | awk '$7 ~ "^fscrypt:" {print $1}')
-	keyctl reap
-}
-
 verify_mirror() {
 	local mirror1=$TMP/$tfile.mirror1
 	local mirror2=$TMP/$tfile.mirror2
@@ -4597,7 +4598,7 @@ test_59a() {
 	$LFS getstripe $testfile
 
 	# now, without the key
-	remove_env_key
+	remove_enc_key
 	scrambledfile=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type f)
 	$LFS mirror resync $scrambledfile ||
 		error "could not resync mirror"
@@ -4645,7 +4646,7 @@ test_59b() {
 	$LFS getstripe $testfile
 
 	# now, without the key
-	remove_env_key
+	remove_enc_key
 	scrambledfile=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type f)
 	$LFS migrate -i1 $scrambledfile ||
 		error "migrate $scrambledfile failed"
@@ -4660,7 +4661,7 @@ test_59b() {
 		error "migrated file is corrupted"
 
 	# now, without the key
-	remove_env_key
+	remove_enc_key
 	$LFS mirror extend -N -i0 $scrambledfile ||
 		error "mirror extend $scrambledfile failed (1)"
 	$LFS getstripe $scrambledfile
@@ -4686,7 +4687,7 @@ test_59b() {
 	verify_mirror $testfile $tmpfile
 
 	# now, without the key
-	remove_env_key
+	remove_enc_key
 	$LFS mirror split --mirror-id 1 -d $scrambledfile ||
 		error "mirror split file $scrambledfile failed (1)"
 	$LFS getstripe $scrambledfile
@@ -4725,7 +4726,7 @@ test_59c() {
 	echo b > $dirname/subf
 
 	# now, without the key
-	remove_env_key
+	remove_enc_key
 	scrambleddir=$(find $DIR/$tdir/ -maxdepth 1 -mindepth 1 -type d)
 
 	# migrate a non-empty encrypted dir
@@ -4737,6 +4738,53 @@ test_59c() {
 		error "migrate $scrambleddir between MDTs failed (2)"
 }
 run_test 59c "MDT migrate of encrypted files without key"
+
+test_60() {
+	local testdir=$DIR/$tdir/mytestdir
+	local testfile=$DIR/$tdir/$tfile
+
+	(( $MDS1_VERSION > $(version_code 2.14.53) )) ||
+		skip "Need MDS version at least 2.14.53"
+
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	stack_trap cleanup_for_enc_tests EXIT
+	setup_for_enc_tests
+
+	echo a > $DIR/$tdir/file1
+	mkdir $DIR/$tdir/subdir
+	echo b > $DIR/$tdir/subdir/subfile1
+
+	remove_enc_key
+	# unmount client completely
+	umount_client $MOUNT || error "umount $MOUNT failed"
+	if is_mounted $MOUNT2; then
+		umount_client $MOUNT2 || error "umount $MOUNT2 failed"
+	fi
+
+	# remount client with subdirectory mount
+	export FILESET=/$tdir
+	mount_client $MOUNT ${MOUNT_OPTS} || error "remount failed"
+	if [ "$MOUNT_2" ]; then
+		mount_client $MOUNT2 ${MOUNT_OPTS} || error "remount failed"
+	fi
+	export FILESET=""
+
+	ls -Rl $DIR || error "ls -Rl $DIR failed (1)"
+
+	# now, with the key
+	insert_enc_key
+
+	ls -Rl $DIR || error "ls -Rl $DIR failed (2)"
+	cat $DIR/file1 || error "cat $DIR/$tdir/file1 failed"
+	cat $DIR/subdir/subfile1 ||
+		error "cat $DIR/$tdir/subdir/subfile1 failed"
+}
+run_test 60 "Subdirmount of encrypted dir"
 
 log "cleanup: ======================================================"
 