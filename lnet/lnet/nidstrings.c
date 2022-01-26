lustre/lustre-release

commit 82a17076f880770a267746eafc9d956998189531

LU-10391 lnet: introduce struct lnet_nid
LNet nids are currently limited to 4-bytes for addresses.
This excludes the use of IPv6.

In order to support IPv6, introduce 'struct lnet_nid' which can hold
up to 128bit address and is extensible, and deprecate 'lnet_nid_t'.
lnet_nid_it will eventually be removed.  Where lnet_nid_t is often
passed around by value, 'struct lnet_nid' will normally be passed
around by reference as it is over twice as large.

The net_type field, which currently has value up to 16, is now limited
to 0-254 with 255 being used as a wildcard.  The most significant byte
is now a size field which gives the size of the whole nid minus 8.  So
zero is correct for current nids with 4-byte addresses.

Where we still need to use 4-byte-address nids, we will use names
containing "nid4".  So "nid4" is a lnet_nid_t when "nid" is a struct
lnet_nid.  lnet_nid_to_nid4 converts a 'struct lnet_nid' to an
lnet_nid_t.

While lnet_nid_t is stored and often transmitted in host-endian format
(and possibly byte-swapped on receipt), 'struct lnet_nid' is always
stored in network-byte-order (i.e.  big-endian).  This is more common
approach for network addresses.

In this first instance, 'struct lnet_nid' is used for ni_nid in
'struct lnet_ni', and related support functions.

In particular libcfs_nidstr() is introduced which parallels
libcfs_nid2str(), but takes 'struct lnet_nid'.

In cases were we need to have similar functions for old and new style
nid, the new function is introduced with a slightly different name,
such as libcfs_nid2str above, or LNET_NID_NET (like LNET_NIDNET).
It will be confusing having both, but the plan is to remove the old
names as soon as practical.

Test-Parameters: trivial
Test-Parameters: serverversion=2.12 serverdistro=el7.9 testlist=runtests
Test-Parameters: clientversion=2.12 testlist=runtests
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I4dcf1bab856621915b6535958d77cdde89105d96
Reviewed-on: https://review.whamcloud.com/42100

lnet/lnet/nidstrings.c
@@ -910,6 +910,46 @@ libcfs_nid2str_r(lnet_nid_t nid, char *buf, size_t buf_size)
 }
 EXPORT_SYMBOL(libcfs_nid2str_r);
 
+char *
+libcfs_nidstr_r(const struct lnet_nid *nid, char *buf, size_t buf_size)
+{
+	__u32 nnum = be16_to_cpu(nid->nid_num);
+	__u32 lnd  = nid->nid_type;
+	struct netstrfns *nf;
+
+	if (LNET_NID_IS_ANY(nid)) {
+		strncpy(buf, "<?>", buf_size);
+		buf[buf_size - 1] = '\0';
+		return buf;
+	}
+
+	nf = libcfs_lnd2netstrfns(lnd);
+	if (nf && nid_is_nid4(nid)) {
+		size_t addr_len;
+
+		nf->nf_addr2str(ntohl(nid->nid_addr[0]), buf, buf_size);
+		addr_len = strlen(buf);
+		if (nnum == 0)
+			snprintf(buf + addr_len, buf_size - addr_len, "@%s",
+				 nf->nf_name);
+		else
+			snprintf(buf + addr_len, buf_size - addr_len, "@%s%u",
+				 nf->nf_name, nnum);
+	} else {
+		int l = 0;
+		int words = DIV_ROUND_UP(NID_ADDR_BYTES(nid), 4);
+		int i;
+
+		for (i = 0; i < words && i < 4; i++)
+			l = snprintf(buf+l, buf_size-l, "%s%x",
+				     i ? ":" : "", ntohl(nid->nid_addr[i]));
+		snprintf(buf+l, buf_size-l, "@<%u:%u>", lnd, nnum);
+	}
+
+	return buf;
+}
+EXPORT_SYMBOL(libcfs_nidstr_r);
+
 static struct netstrfns *
 libcfs_str2net_internal(const char *str, __u32 *net)
 {

lnet/include/lnet/lib-lnet.h
@@ -226,7 +226,7 @@ __must_hold(&ni->ni_lock)
 
 	if (ni->ni_status && ni->ni_status->ns_status != status) {
 		CDEBUG(D_NET, "ni %s status changed from %#x to %#x\n",
-		       libcfs_nid2str(ni->ni_nid),
+		       libcfs_nidstr(&ni->ni_nid),
 		       ni->ni_status->ns_status, status);
 		ni->ni_status->ns_status = status;
 		update = true;
@@ -239,7 +239,7 @@ static inline unsigned int
 lnet_ni_get_status_locked(struct lnet_ni *ni)
 __must_hold(&ni->ni_lock)
 {
-	if (ni->ni_nid == LNET_NID_LO_0)
+	if (nid_is_lo0(&ni->ni_nid))
 		return LNET_NI_STATUS_UP;
 	else if (atomic_read(&ni->ni_fatal_error_on))
 		return LNET_NI_STATUS_DOWN;

lnet/include/lnet/lib-types.h
@@ -455,7 +455,7 @@ struct lnet_ni {
 	__u32			*ni_cpts;
 
 	/* interface's NID */
-	lnet_nid_t		ni_nid;
+	struct lnet_nid		ni_nid;
 
 	/* instance-specific data */
 	void			*ni_data;

lnet/include/uapi/linux/lnet/lnet-idl.h
@@ -39,17 +39,41 @@
  * These are sent in sender's byte order (i.e. receiver flips).
  */
 
-/**
- * Address of an end-point in an LNet network.
+/** Address of an end-point in an LNet network.
  *
  * A node can have multiple end-points and hence multiple addresses.
  * An LNet network can be a simple network (e.g. tcp0) or a network of
  * LNet networks connected by LNet routers. Therefore an end-point address
  * has two parts: network ID, and address within a network.
+ * The most-significant-byte in this format is always 0.  A larger value
+ * would imply a larger nid with a larger address.
  *
  * \see LNET_NIDNET, LNET_NIDADDR, and LNET_MKNID.
  */
 typedef __u64 lnet_nid_t;
+
+/*
+ * Address of LNet end-point in extended form
+ *
+ * To support addresses larger than 32bits we have
+ * an extended nid which supports up to 128 bits
+ * of address and is extensible.
+ * If nid_size is 0, then the nid can be stored in an lnet_nid_t,
+ * and the first 8 bytes of the 'struct lnet_nid' are identical to
+ * the lnet_nid_t in big-endian format.
+ * If nid_type == 0xff, then all other fields should be ignored
+ * and this is an ANY wildcard address.  In particular, the nid_size
+ * can be 0xff without making the address too big to fit.
+ */
+struct lnet_nid {
+	__u8	nid_size;	/* total bytes - 8 */
+	__u8	nid_type;
+	__be16	nid_num;
+	__be32	nid_addr[4];
+} __attribute__((packed));
+
+#define NID_BYTES(nid)		((nid)->nid_size + 8)
+#define NID_ADDR_BYTES(nid)	((nid)->nid_size + 4)
 
 /**
  * ID of a process in a node. Shortened as PID to distinguish from

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -32,6 +32,9 @@
 #ifndef __UAPI_LNET_TYPES_H__
 #define __UAPI_LNET_TYPES_H__
 
+#include <linux/string.h>
+#include <asm/byteorder.h>
+
 /** \addtogroup lnet
  * @{ */
 
@@ -51,6 +54,15 @@
 #define LNET_NID_ANY	  ((lnet_nid_t) -1)
 /** wildcard PID that matches any lnet_pid_t */
 #define LNET_PID_ANY	  ((lnet_pid_t) -1)
+
+static inline int LNET_NID_IS_ANY(const struct lnet_nid *nid)
+{
+	/* A NULL pointer can be used to mean "ANY" */
+	return !nid || nid->nid_type == 0xFF;
+}
+
+#define LNET_ANY_NID ((struct lnet_nid)			\
+		      {0xFF, 0xFF, ~0, {~0, ~0, ~0, ~0} })
 
 #define LNET_PID_RESERVED 0xf0000000 /* reserved bits in PID */
 #define LNET_PID_USERFLAG 0x80000000 /* set in userspace peers */
@@ -81,7 +93,7 @@ static inline __u32 LNET_NETNUM(__u32 net)
 
 static inline __u32 LNET_NETTYP(__u32 net)
 {
-	return (net >> 16) & 0xffff;
+	return (net >> 16) & 0xff;
 }
 
 static inline __u32 LNET_MKNET(__u32 type, __u32 num)
@@ -93,6 +105,58 @@ static inline __u32 LNET_MKNET(__u32 type, __u32 num)
 #define LNET_NID_LO_0 LNET_MKNID(LNET_MKNET(LOLND, 0), 0)
 
 #define LNET_NET_ANY LNET_NIDNET(LNET_NID_ANY)
+
+static inline int nid_is_nid4(const struct lnet_nid *nid)
+{
+	return NID_ADDR_BYTES(nid) == 4;
+}
+
+/* LOLND may not be defined yet, so we cannot use an inline */
+#define nid_is_lo0(__nid)						\
+	((__nid)->nid_type == LOLND &&					\
+	 nid_is_nid4(__nid) &&						\
+	 (__nid)->nid_num == 0 &&					\
+	 (__nid)->nid_addr[0] == 0)
+
+static inline __u32 LNET_NID_NET(const struct lnet_nid *nid)
+{
+	return LNET_MKNET(nid->nid_type, __be16_to_cpu(nid->nid_num));
+}
+
+static inline void lnet_nid4_to_nid(lnet_nid_t nid4, struct lnet_nid *nid)
+{
+	if (nid4 == LNET_NID_ANY) {
+		/* equal to setting to LNET_ANY_NID */
+		memset(nid, 0xff, sizeof(*nid));
+		return;
+	}
+
+	nid->nid_size = 0;
+	nid->nid_type = LNET_NETTYP(LNET_NIDNET(nid4));
+	nid->nid_num = __cpu_to_be16(LNET_NETNUM(LNET_NIDNET(nid4)));
+	nid->nid_addr[0] = __cpu_to_be32(LNET_NIDADDR(nid4));
+	nid->nid_addr[1] = nid->nid_addr[2] = nid->nid_addr[3] = 0;
+}
+
+static inline lnet_nid_t lnet_nid_to_nid4(const struct lnet_nid *nid)
+{
+	if (LNET_NID_IS_ANY(nid))
+		return LNET_NID_ANY;
+
+	return LNET_MKNID(LNET_NID_NET(nid), __be32_to_cpu(nid->nid_addr[0]));
+}
+
+static inline int nid_same(const struct lnet_nid *n1,
+			    const struct lnet_nid *n2)
+{
+	return n1->nid_size == n2->nid_size &&
+		n1->nid_type == n2->nid_type &&
+		n1->nid_num == n2->nid_num &&
+		n1->nid_addr[0] == n2->nid_addr[0] &&
+		n1->nid_addr[1] == n2->nid_addr[1] &&
+		n1->nid_addr[2] == n2->nid_addr[2] &&
+		n1->nid_addr[3] == n2->nid_addr[3];
+}
 
 struct lnet_counters_health {
 	__u32	lch_rst_alloc;

lnet/include/uapi/linux/lnet/nidstr.h
@@ -59,7 +59,7 @@ enum {
 struct list_head;
 
 #define LNET_NIDSTR_COUNT 1024	/* # of nidstrings */
-#define LNET_NIDSTR_SIZE  32	/* size of each one (see below for usage) */
+#define LNET_NIDSTR_SIZE  64	/* size of each one (see below for usage) */
 
 /* support decl needed by both kernel and user space */
 char *libcfs_next_nidstring(void);
@@ -83,6 +83,13 @@ static inline char *libcfs_nid2str(lnet_nid_t nid)
 {
 	return libcfs_nid2str_r(nid, libcfs_next_nidstring(),
 				LNET_NIDSTR_SIZE);
+}
+char *libcfs_nidstr_r(const struct lnet_nid *nid,
+			char *buf, __kernel_size_t buf_size);
+static inline char *libcfs_nidstr(const struct lnet_nid *nid)
+{
+	return libcfs_nidstr_r(nid, libcfs_next_nidstring(),
+				 LNET_NIDSTR_SIZE);
 }
 __u32 libcfs_str2net(const char *str);
 lnet_nid_t libcfs_str2nid(const char *str);

lnet/klnds/gnilnd/gnilnd.c
@@ -91,7 +91,8 @@ kgnilnd_close_stale_conns_locked(kgn_peer_t *peer, kgn_conn_t *newconn)
 	int                 loopback;
 	int                 count = 0;
 
-	loopback = peer->gnp_nid == peer->gnp_net->gnn_ni->ni_nid;
+	loopback = (peer->gnp_nid ==
+		    lnet_nid_to_nid4(&peer->gnp_net->gnn_ni->ni_nid));
 
 	list_for_each_entry_safe(conn, cnxt, &peer->gnp_conns, gnc_list) {
 		if (conn->gnc_state != GNILND_CONN_ESTABLISHED)
@@ -161,7 +162,8 @@ kgnilnd_conn_isdup_locked(kgn_peer_t *peer, kgn_conn_t *newconn)
 	int               loopback;
 	ENTRY;
 
-	loopback = peer->gnp_nid == peer->gnp_net->gnn_ni->ni_nid;
+	loopback = (peer->gnp_nid ==
+		    lnet_nid_to_nid4(&peer->gnp_net->gnn_ni->ni_nid));
 
 	list_for_each_entry(conn, &peer->gnp_conns, gnc_list) {
 		CDEBUG(D_NET, "checking conn 0x%p for peer %s"
@@ -594,8 +596,9 @@ kgnilnd_peer_notify(kgn_peer_t *peer, int error, int alive)
 
 			net = nets[i];
 
-			peer_nid = kgnilnd_lnd2lnetnid(net->gnn_ni->ni_nid,
-								 peer->gnp_nid);
+			peer_nid = kgnilnd_lnd2lnetnid(
+				lnet_nid_to_nid4(&net->gnn_ni->ni_nid),
+				peer->gnp_nid);
 
 			CDEBUG(D_NET, "peer 0x%p->%s last_alive %lld (%llds ago)\n",
 				peer, libcfs_nid2str(peer_nid), peer->gnp_last_alive,
@@ -1726,7 +1729,7 @@ kgnilnd_report_node_state(lnet_nid_t nid, int down)
 		/* The nid passed in does not yet contain the net portion.
 		 * Let's build it up now
 		 */
-		nid = LNET_MKNID(LNET_NIDNET(net->gnn_ni->ni_nid), nid);
+		nid = LNET_MKNID(LNET_NID_NET(&net->gnn_ni->ni_nid), nid);
 		rc = kgnilnd_add_peer(net, nid, &new_peer);
 
 		if (rc) {
@@ -1807,7 +1810,8 @@ kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 		 * LNET assumes a conn and peer per net, the LNET_MKNID/LNET_NIDADDR allows us to let Lnet see what it
 		 * wants to see instead of the underlying network that is being used to send the data
 		 */
-		data->ioc_nid    = LNET_MKNID(LNET_NIDNET(ni->ni_nid), LNET_NIDADDR(nid));
+		data->ioc_nid    = LNET_MKNID(LNET_NID_NET(&ni->ni_nid),
+					      LNET_NIDADDR(nid));
 		data->ioc_flags  = peer_connecting;
 		data->ioc_count  = peer_refcount;
 
@@ -1858,7 +1862,8 @@ kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 			/* LNET_MKNID is used to build the correct address based on what LNET wants to see instead of
 			 * the generic connection that is used to send the data
 			 */
-			data->ioc_nid    = LNET_MKNID(LNET_NIDNET(ni->ni_nid), LNET_NIDADDR(conn->gnc_peer->gnp_nid));
+			data->ioc_nid	 = LNET_MKNID(LNET_NID_NET(&ni->ni_nid),
+						      LNET_NIDADDR(conn->gnc_peer->gnp_nid));
 			data->ioc_u32[0] = conn->gnc_device->gnd_id;
 			kgnilnd_conn_decref(conn);
 		}
@@ -1881,12 +1886,12 @@ kgnilnd_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
 	}
 	case IOC_LIBCFS_REGISTER_MYNID: {
 		/* Ignore if this is a noop */
-		if (data->ioc_nid == ni->ni_nid) {
+		if (data->ioc_nid == lnet_nid_to_nid4(&ni->ni_nid)) {
 			rc = 0;
 		} else {
 			CERROR("obsolete IOC_LIBCFS_REGISTER_MYNID: %s(%s)\n",
 			       libcfs_nid2str(data->ioc_nid),
-			       libcfs_nid2str(ni->ni_nid));
+			       libcfs_nidstr(&ni->ni_nid));
 			rc = -EINVAL;
 		}
 		break;
@@ -2640,9 +2645,9 @@ kgnilnd_startup(struct lnet_ni *ni)
 	atomic_set(&net->gnn_refcount, 1);
 
 	/* if we have multiple devices, spread the nets around */
-	net->gnn_netnum = LNET_NETNUM(LNET_NIDNET(ni->ni_nid));
+	net->gnn_netnum = LNET_NETNUM(LNET_NID_NET(&ni->ni_nid));
 
-	devno = LNET_NIDNET(ni->ni_nid) % GNILND_MAXDEVS;
+	devno = LNET_NID_NET(&ni->ni_nid) % GNILND_MAXDEVS;
 	net->gnn_dev = &kgnilnd_data.kgn_devices[devno];
 
 	/* allocate a 'dummy' cdm for datagram use. We can only have a single
@@ -2653,10 +2658,10 @@ kgnilnd_startup(struct lnet_ni *ni)
 	/* the instance id for the cdm is the NETNUM offset by MAXDEVS -
 	 * ensuring we'll have a unique id */
 
-
-	ni->ni_nid = LNET_MKNID(LNET_NIDNET(ni->ni_nid), net->gnn_dev->gnd_nid);
+	ni->ni_nid.nid_addr[0] =
+		cpu_to_be32(LNET_NIDADDR(net->gnn_dev->gnd_nid));
 	CDEBUG(D_NET, "adding net %p nid=%s on dev %d \n",
-		net, libcfs_nid2str(ni->ni_nid), net->gnn_dev->gnd_id);
+		net, libcfs_nidstr(&ni->ni_nid), net->gnn_dev->gnd_id);
 	/* until the gnn_list is set, we need to cleanup ourselves as
 	 * kgnilnd_shutdown is just gonna get confused */
 

lnet/klnds/gnilnd/gnilnd.h
@@ -1169,20 +1169,20 @@ do {									\
 		wake_up_var(&kgnilnd_data);				\
 }while (0)
 
-#define kgnilnd_net_addref(net)                                                 \
-do {                                                                            \
-	int     val = atomic_inc_return(&net->gnn_refcount);                    \
-	LASSERTF(val > 1, "net %p refcount %d\n", net, val);                    \
-	CDEBUG(D_NETTRACE, "net %p->%s++ (%d)\n", net,                          \
-		libcfs_nid2str(net->gnn_ni->ni_nid), val);                      \
+#define kgnilnd_net_addref(net)						\
+do {									\
+	int     val = atomic_inc_return(&net->gnn_refcount);		\
+	LASSERTF(val > 1, "net %p refcount %d\n", net, val);		\
+	CDEBUG(D_NETTRACE, "net %p->%s++ (%d)\n", net,			\
+		libcfs_nidstr(&net->gnn_ni->ni_nid), val);		\
 } while (0)
 
-#define kgnilnd_net_decref(net)                                                 \
-do {                                                                            \
-	int     val = atomic_dec_return(&net->gnn_refcount);                    \
-	LASSERTF(val >= 0, "net %p refcount %d\n", net, val);                   \
-	CDEBUG(D_NETTRACE, "net %p->%s-- (%d)\n", net,                          \
-	       libcfs_nid2str(net->gnn_ni->ni_nid), val);                       \
+#define kgnilnd_net_decref(net)						\
+do {									\
+	int     val = atomic_dec_return(&net->gnn_refcount);		\
+	LASSERTF(val >= 0, "net %p refcount %d\n", net, val);		\
+	CDEBUG(D_NETTRACE, "net %p->%s-- (%d)\n", net,			\
+	       libcfs_nidstr(&net->gnn_ni->ni_nid), val);		\
 } while (0)
 
 #define kgnilnd_peer_addref(peer)                                               \
@@ -1376,7 +1376,7 @@ kgnilnd_check_purgatory_conn(kgn_conn_t *conn)
 
 	if (conn->gnc_peer) {
 		loopback = conn->gnc_peer->gnp_nid ==
-		       conn->gnc_peer->gnp_net->gnn_ni->ni_nid;
+			lnet_nid_to_nid4(&conn->gnc_peer->gnp_net->gnn_ni->ni_nid);
 	} else {
 		/* short circuit - a conn that didn't complete
 		 * setup never needs a purgatory hold */
@@ -1744,8 +1744,11 @@ kgnilnd_find_net(lnet_nid_t nid, kgn_net_t **netp)
 		return -ESHUTDOWN;
 	}
 
-	list_for_each_entry(net, kgnilnd_netnum2netlist(LNET_NETNUM(LNET_NIDNET(nid))), gnn_list) {
-		if (!net->gnn_shutdown && LNET_NIDNET(net->gnn_ni->ni_nid) == LNET_NIDNET(nid)) {
+	list_for_each_entry(net,
+			    kgnilnd_netnum2netlist(LNET_NETNUM(LNET_NIDNET(nid))),
+			    gnn_list) {
+		if (!net->gnn_shutdown &&
+		    LNET_NID_NET(&net->gnn_ni->ni_nid) == LNET_NIDNET(nid)) {
 			kgnilnd_net_addref(net);
 			up_read(&kgnilnd_data.kgn_net_rw_sem);
 			*netp = net;

commit 8ed370864c4747281dab22f3fafceb5310c1cd53

LU-10391 lnet: Fix NULL-deref in lnet_nidstr_r()
It is valid to pass NULL as the nid for lnet_nidstr_r() - it indicate
"any" nid.  LNET_NID_IS_ANY() tests for this and the function exits
early.

However, 'lnd' is assigned from "nid->nid_type" and 'nnum' from
"nid->nid_num", causing a NULL-pointer dereference.

So move these assignments later.

Fixes: 82a17076f880 ("LU-10391 lnet: introduce struct lnet_nid")
Test-Parameters: trivial
Test-Parameters: serverversion=2.12 serverdistro=el7.9 testlist=runtests
Test-Parameters: clientversion=2.12 testlist=runtests
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Ie29dd4d0ef7fac0f11c1ece714278a7dd9860602
Reviewed-on: https://review.whamcloud.com/44838
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/nidstrings.c
@@ -973,8 +973,8 @@ EXPORT_SYMBOL(libcfs_nid2str_r);
 char *
 libcfs_nidstr_r(const struct lnet_nid *nid, char *buf, size_t buf_size)
 {
-	__u32 nnum = be16_to_cpu(nid->nid_num);
-	__u32 lnd  = nid->nid_type;
+	__u32 nnum;
+	__u32 lnd;
 	struct netstrfns *nf;
 
 	if (LNET_NID_IS_ANY(nid)) {
@@ -983,6 +983,8 @@ libcfs_nidstr_r(const struct lnet_nid *nid, char *buf, size_t buf_size)
 		return buf;
 	}
 
+	nnum = be16_to_cpu(nid->nid_num);
+	lnd = nid->nid_type;
 	nf = libcfs_lnd2netstrfns(lnd);
 	if (nf) {
 		size_t addr_len;