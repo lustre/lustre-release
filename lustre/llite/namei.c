lustre/lustre-release

commit 55ca00c3d1cd8635258ccbda27ee3f0f9b2966a8

LU-11213 ptlrpc: intent_getattr fetches default LMV
Intent_getattr fetches default LMV, and caches it on client,
which will be used in subdir creation.

* Add RMF_DEFAULT_MDT_MD in intent_getattr reply.
* Save default LMV in ll_inode_info->lli_default_lsm_md, and
  replace lli_def_stripe_offset with it.
* take LOOKUP lock on default LMV setting to let client update
  cached default LMV.
* improve mdt_object_striped() to read from bottom device
  to avoid reading stripe FIDs.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Idb369db2c514a9c5108390f70d9284b3a87d26db
Reviewed-on: https://review.whamcloud.com/34802
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/namei.c
@@ -256,8 +256,6 @@ void ll_lock_cancel_bits(struct ldlm_lock *lock, __u64 to_cancel)
 	}
 
 	if (bits & MDS_INODELOCK_XATTR) {
-		if (S_ISDIR(inode->i_mode))
-			ll_i2info(inode)->lli_def_stripe_offset = -1;
 		ll_xattr_cache_destroy(inode);
 		bits &= ~MDS_INODELOCK_XATTR;
 	}
@@ -1271,13 +1269,11 @@ again:
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
 			cfs_curproc_cap_pack(), rdev, &request);
-	if (err < 0 && err != -EREMOTE)
-		GOTO(err_exit, err);
-
-	/* If the client doesn't know where to create a subdirectory (or
-	 * in case of a race that sends the RPC to the wrong MDS), the
-	 * MDS will return -EREMOTE and the client will fetch the layout
-	 * of the directory, then create the directory on the right MDT. */
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 58, 0)
+	/*
+	 * server < 2.12.58 doesn't pack default LMV in intent_getattr reply,
+	 * fetch default LMV here.
+	 */
 	if (unlikely(err == -EREMOTE)) {
 		struct ll_inode_info	*lli = ll_i2info(dir);
 		struct lmv_user_md	*lum;
@@ -1289,24 +1285,54 @@ again:
 
 		err2 = ll_dir_getstripe(dir, (void **)&lum, &lumsize, &request,
 					OBD_MD_DEFAULT_MEA);
+		ll_finish_md_op_data(op_data);
+		op_data = NULL;
 		if (err2 == 0) {
-			/* Update stripe_offset and retry */
-			lli->lli_def_stripe_offset = lum->lum_stripe_offset;
-		} else if (err2 == -ENODATA &&
-			   lli->lli_def_stripe_offset != -1) {
-			/* If there are no default stripe EA on the MDT, but the
+			struct lustre_md md = { NULL };
+
+			md.body = req_capsule_server_get(&request->rq_pill,
+							 &RMF_MDT_BODY);
+			if (!md.body)
+				GOTO(err_exit, err = -EPROTO);
+
+			OBD_ALLOC_PTR(md.default_lmv);
+			if (!md.default_lmv)
+				GOTO(err_exit, err = -ENOMEM);
+
+			md.default_lmv->lsm_md_magic = lum->lum_magic;
+			md.default_lmv->lsm_md_stripe_count =
+				lum->lum_stripe_count;
+			md.default_lmv->lsm_md_master_mdt_index =
+				lum->lum_stripe_offset;
+			md.default_lmv->lsm_md_hash_type = lum->lum_hash_type;
+
+			err = ll_update_inode(dir, &md);
+			md_free_lustre_md(sbi->ll_md_exp, &md);
+			if (err)
+				GOTO(err_exit, err);
+		} else if (err2 == -ENODATA && lli->lli_default_lsm_md) {
+			/*
+			 * If there are no default stripe EA on the MDT, but the
 			 * client has default stripe, then it probably means
-			 * default stripe EA has just been deleted. */
-			lli->lli_def_stripe_offset = -1;
+			 * default stripe EA has just been deleted.
+			 */
+			down_write(&lli->lli_lsm_sem);
+			if (lli->lli_default_lsm_md)
+				OBD_FREE_PTR(lli->lli_default_lsm_md);
+			lli->lli_default_lsm_md = NULL;
+			up_write(&lli->lli_lsm_sem);
 		} else {
 			GOTO(err_exit, err);
 		}
 
 		ptlrpc_req_finished(request);
 		request = NULL;
-		ll_finish_md_op_data(op_data);
 		goto again;
 	}
+#endif
+
+	if (err < 0)
+		GOTO(err_exit, err);
 
 	ll_update_times(request, dir);
 

lustre/include/lustre_lmv.h
@@ -74,10 +74,12 @@ lsm_md_eq(const struct lmv_stripe_md *lsm1, const struct lmv_stripe_md *lsm2)
 		      lsm2->lsm_md_pool_name) != 0)
 		return false;
 
-	for (idx = 0; idx < lsm1->lsm_md_stripe_count; idx++) {
-		if (!lu_fid_eq(&lsm1->lsm_md_oinfo[idx].lmo_fid,
-			       &lsm2->lsm_md_oinfo[idx].lmo_fid))
-			return false;
+	if (lsm1->lsm_md_magic == LMV_MAGIC_V1) {
+		for (idx = 0; idx < lsm1->lsm_md_stripe_count; idx++) {
+			if (!lu_fid_eq(&lsm1->lsm_md_oinfo[idx].lmo_fid,
+				       &lsm2->lsm_md_oinfo[idx].lmo_fid))
+				return false;
+		}
 	}
 
 	return true;
@@ -93,6 +95,9 @@ static inline void lsm_md_dump(int mask, const struct lmv_stripe_md *lsm)
 		lsm->lsm_md_master_mdt_index, lsm->lsm_md_hash_type,
 		lsm->lsm_md_layout_version, lsm->lsm_md_migrate_offset,
 		lsm->lsm_md_migrate_hash, lsm->lsm_md_pool_name);
+
+	if (lsm->lsm_md_magic != LMV_MAGIC_V1)
+		return;
 
 	for (i = 0; i < lsm->lsm_md_stripe_count; i++)
 		CDEBUG(mask, "stripe[%d] "DFID"\n",

lustre/include/lustre_req_layout.h
@@ -269,6 +269,7 @@ extern struct req_msg_field RMF_DLM_GL_DESC;
 extern struct req_msg_field RMF_LDLM_INTENT;
 extern struct req_msg_field RMF_LAYOUT_INTENT;
 extern struct req_msg_field RMF_MDT_MD;
+extern struct req_msg_field RMF_DEFAULT_MDT_MD;
 extern struct req_msg_field RMF_REC_REINT;
 extern struct req_msg_field RMF_EADATA;
 extern struct req_msg_field RMF_EAVALS;

lustre/include/md_object.h
@@ -130,9 +130,11 @@ struct md_attr {
 	struct md_som		 ma_som;
 	struct lov_mds_md	*ma_lmm;
 	union lmv_mds_md	*ma_lmv;
+	struct lmv_user_md	*ma_default_lmv;
 	void			*ma_acl;
 	int			 ma_lmm_size;
 	int			 ma_lmv_size;
+	int			 ma_default_lmv_size;
 	int			 ma_acl_size;
 };
 

lustre/include/obd.h
@@ -838,6 +838,14 @@ enum md_cli_flags {
 	CLI_MIGRATE     = 1 << 4,
 };
 
+enum md_op_code {
+	LUSTRE_OPC_MKDIR	= 0,
+	LUSTRE_OPC_SYMLINK	= 1,
+	LUSTRE_OPC_MKNOD	= 2,
+	LUSTRE_OPC_CREATE	= 3,
+	LUSTRE_OPC_ANY		= 5,
+};
+
 /**
  * GETXATTR is not included as only a couple of fields in the reply body
  * is filled, but not FID which is needed for common intent handling in
@@ -855,6 +863,7 @@ struct md_op_data {
 	struct lu_fid		op_fid4; /* to the operation locks. */
 	u32			op_mds;  /* what mds server open will go to */
 	__u32			op_mode;
+	enum md_op_code		op_code;
 	struct lustre_handle	op_open_handle;
 	s64			op_mod_time;
 	const char		*op_name;
@@ -863,6 +872,7 @@ struct md_op_data {
 	struct rw_semaphore	*op_mea2_sem;
 	struct lmv_stripe_md	*op_mea1;
 	struct lmv_stripe_md	*op_mea2;
+	struct lmv_stripe_md	*op_default_mea1;	/* default LMV */
 	__u32			op_suppgids[2];
 	__u32			op_fsuid;
 	__u32			op_fsgid;
@@ -895,9 +905,6 @@ struct md_op_data {
 	__u32			op_file_secctx_name_size;
 	void		       *op_file_secctx;
 	__u32			op_file_secctx_size;
-
-	/* default stripe offset */
-	__u32			op_default_stripe_offset;
 
 	__u32			op_projid;
 
@@ -1041,6 +1048,7 @@ struct lustre_md {
 		struct lmv_stripe_md    *lmv;
 		struct lmv_foreign_md   *lfm;
 	};
+	struct lmv_stripe_md    *default_lmv;
 #ifdef CONFIG_FS_POSIX_ACL
 	struct posix_acl        *posix_acl;
 #endif

lustre/llite/llite_internal.h
@@ -169,12 +169,8 @@ struct ll_inode_info {
 			struct rw_semaphore		lli_lsm_sem;
 			/* directory stripe information */
 			struct lmv_stripe_md		*lli_lsm_md;
-			/* default directory stripe offset.  This is extracted
-			 * from the "dmv" xattr in order to decide which MDT to
-			 * create a subdirectory on.  The MDS itself fetches
-			 * "dmv" and gets the rest of the default layout itself
-			 * (count, hash, etc). */
-			__u32				lli_def_stripe_offset;
+			/* directory default LMV */
+			struct lmv_stripe_md		*lli_default_lsm_md;
 		};
 
 		/* for non-directory */
@@ -984,19 +980,12 @@ int ll_get_max_mdsize(struct ll_sb_info *sbi, int *max_mdsize);
 int ll_get_default_mdsize(struct ll_sb_info *sbi, int *default_mdsize);
 int ll_set_default_mdsize(struct ll_sb_info *sbi, int default_mdsize);
 
-enum {
-	LUSTRE_OPC_MKDIR	= 0,
-	LUSTRE_OPC_SYMLINK	= 1,
-	LUSTRE_OPC_MKNOD	= 2,
-	LUSTRE_OPC_CREATE	= 3,
-	LUSTRE_OPC_ANY		= 5,
-};
-
 void ll_unlock_md_op_lsm(struct md_op_data *op_data);
 struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 				      struct inode *i1, struct inode *i2,
 				      const char *name, size_t namelen,
-				      __u32 mode, __u32 opc, void *data);
+				      __u32 mode, enum md_op_code opc,
+				      void *data);
 void ll_finish_md_op_data(struct md_op_data *op_data);
 int ll_get_obd_name(struct inode *inode, unsigned int cmd, unsigned long arg);
 void ll_compute_rootsquash_state(struct ll_sb_info *sbi);

lustre/llite/llite_lib.c
@@ -965,7 +965,6 @@ void ll_lli_init(struct ll_inode_info *lli)
 		spin_lock_init(&lli->lli_sa_lock);
 		lli->lli_opendir_pid = 0;
 		lli->lli_sa_enabled = 0;
-		lli->lli_def_stripe_offset = -1;
 		init_rwsem(&lli->lli_lsm_sem);
 	} else {
 		mutex_init(&lli->lli_size_mutex);
@@ -1291,9 +1290,14 @@ void ll_dir_clear_lsm_md(struct inode *inode)
 
 	LASSERT(S_ISDIR(inode->i_mode));
 
-	if (lli->lli_lsm_md != NULL) {
+	if (lli->lli_lsm_md) {
 		lmv_free_memmd(lli->lli_lsm_md);
 		lli->lli_lsm_md = NULL;
+	}
+
+	if (lli->lli_default_lsm_md) {
+		lmv_free_memmd(lli->lli_default_lsm_md);
+		lli->lli_default_lsm_md = NULL;
 	}
 }
 
@@ -1396,6 +1400,46 @@ static int ll_init_lsm_md(struct inode *inode, struct lustre_md *md)
 	return 0;
 }
 
+static void ll_update_default_lsm_md(struct inode *inode, struct lustre_md *md)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+
+	if (!md->default_lmv) {
+		/* clear default lsm */
+		if (lli->lli_default_lsm_md) {
+			down_write(&lli->lli_lsm_sem);
+			if (lli->lli_default_lsm_md) {
+				lmv_free_memmd(lli->lli_default_lsm_md);
+				lli->lli_default_lsm_md = NULL;
+			}
+			up_write(&lli->lli_lsm_sem);
+		}
+	} else if (lli->lli_default_lsm_md) {
+		/* update default lsm if it changes */
+		down_read(&lli->lli_lsm_sem);
+		if (lli->lli_default_lsm_md &&
+		    !lsm_md_eq(lli->lli_default_lsm_md, md->default_lmv)) {
+			up_read(&lli->lli_lsm_sem);
+			down_write(&lli->lli_lsm_sem);
+			if (lli->lli_default_lsm_md)
+				lmv_free_memmd(lli->lli_default_lsm_md);
+			lli->lli_default_lsm_md = md->default_lmv;
+			lsm_md_dump(D_INODE, md->default_lmv);
+			md->default_lmv = NULL;
+			up_write(&lli->lli_lsm_sem);
+		} else {
+			up_read(&lli->lli_lsm_sem);
+		}
+	} else {
+		/* init default lsm */
+		down_write(&lli->lli_lsm_sem);
+		lli->lli_default_lsm_md = md->default_lmv;
+		lsm_md_dump(D_INODE, md->default_lmv);
+		md->default_lmv = NULL;
+		up_write(&lli->lli_lsm_sem);
+	}
+}
+
 static int ll_update_lsm_md(struct inode *inode, struct lustre_md *md)
 {
 	struct ll_inode_info *lli = ll_i2info(inode);
@@ -1407,6 +1451,10 @@ static int ll_update_lsm_md(struct inode *inode, struct lustre_md *md)
 	LASSERT(S_ISDIR(inode->i_mode));
 	CDEBUG(D_INODE, "update lsm %p of "DFID"\n", lli->lli_lsm_md,
 	       PFID(ll_inode2fid(inode)));
+
+	/* update default LMV */
+	if (md->default_lmv)
+		ll_update_default_lsm_md(inode, md);
 
 	/*
 	 * no striped information from request, lustre_md from req does not
@@ -2422,7 +2470,9 @@ int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 {
 	struct ll_sb_info *sbi = NULL;
 	struct lustre_md md = { NULL };
+	bool default_lmv_deleted = false;
 	int rc;
+
 	ENTRY;
 
 	LASSERT(*inode || sb);
@@ -2431,6 +2481,15 @@ int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 			      sbi->ll_md_exp, &md);
 	if (rc != 0)
 		GOTO(out, rc);
+
+	/*
+	 * clear default_lmv only if intent_getattr reply doesn't contain it.
+	 * but it needs to be done after iget, check this early because
+	 * ll_update_lsm_md() may change md.
+	 */
+	if (it && (it->it_op & (IT_LOOKUP | IT_GETATTR)) &&
+	    S_ISDIR(md.body->mbo_mode) && !md.default_lmv)
+		default_lmv_deleted = true;
 
 	if (*inode) {
 		rc = ll_update_inode(*inode, &md);
@@ -2494,6 +2553,9 @@ int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 		}
 		LDLM_LOCK_PUT(lock);
 	}
+
+	if (default_lmv_deleted)
+		ll_update_default_lsm_md(*inode, &md);
 
 	GOTO(out, rc = 0);
 
@@ -2571,7 +2633,8 @@ void ll_unlock_md_op_lsm(struct md_op_data *op_data)
 struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 				      struct inode *i1, struct inode *i2,
 				      const char *name, size_t namelen,
-				      __u32 mode, __u32 opc, void *data)
+				      __u32 mode, enum md_op_code opc,
+				      void *data)
 {
 	LASSERT(i1 != NULL);
 
@@ -2595,15 +2658,13 @@ struct md_op_data *ll_prep_md_op_data(struct md_op_data *op_data,
 
 	ll_i2gids(op_data->op_suppgids, i1, i2);
 	op_data->op_fid1 = *ll_inode2fid(i1);
-	op_data->op_default_stripe_offset = -1;
+	op_data->op_code = opc;
 
 	if (S_ISDIR(i1->i_mode)) {
 		down_read(&ll_i2info(i1)->lli_lsm_sem);
 		op_data->op_mea1_sem = &ll_i2info(i1)->lli_lsm_sem;
 		op_data->op_mea1 = ll_i2info(i1)->lli_lsm_md;
-		if (opc == LUSTRE_OPC_MKDIR)
-			op_data->op_default_stripe_offset =
-				   ll_i2info(i1)->lli_def_stripe_offset;
+		op_data->op_default_mea1 = ll_i2info(i1)->lli_default_lsm_md;
 	}
 
 	if (i2) {

lustre/lmv/lmv_obd.c
@@ -1187,13 +1187,12 @@ static int lmv_placement_policy(struct obd_device *obd,
 	    le32_to_cpu(lum->lum_magic != LMV_MAGIC_FOREIGN) &&
 	    le32_to_cpu(lum->lum_stripe_offset) != (__u32)-1) {
 		*mds = le32_to_cpu(lum->lum_stripe_offset);
-	} else if (op_data->op_default_stripe_offset != (__u32)-1) {
-		*mds = op_data->op_default_stripe_offset;
+	} else if (op_data->op_code == LUSTRE_OPC_MKDIR &&
+		   op_data->op_default_mea1 &&
+		   op_data->op_default_mea1->lsm_md_master_mdt_index !=
+			 (__u32)-1) {
+		*mds = op_data->op_default_mea1->lsm_md_master_mdt_index;
 		op_data->op_mds = *mds;
-		/* Correct the stripe offset in lum */
-		if (lum != NULL &&
-		    le32_to_cpu(lum->lum_magic != LMV_MAGIC_FOREIGN))
-			lum->lum_stripe_offset = cpu_to_le32(*mds);
 	} else {
 		*mds = op_data->op_mds;
 	}
@@ -3029,6 +3028,18 @@ static int lmv_unpack_md_v1(struct obd_export *exp, struct lmv_stripe_md *lsm,
 	RETURN(rc);
 }
 
+static inline int lmv_unpack_user_md(struct obd_export *exp,
+				     struct lmv_stripe_md *lsm,
+				     const struct lmv_user_md *lmu)
+{
+	lsm->lsm_md_magic = le32_to_cpu(lmu->lum_magic);
+	lsm->lsm_md_stripe_count = le32_to_cpu(lmu->lum_stripe_count);
+	lsm->lsm_md_master_mdt_index = le32_to_cpu(lmu->lum_stripe_offset);
+	lsm->lsm_md_hash_type = le32_to_cpu(lmu->lum_hash_type);
+
+	return 0;
+}
+
 static int lmv_unpackmd(struct obd_export *exp, struct lmv_stripe_md **lsmp,
 			const union lmv_mds_md *lmm, size_t lmm_size)
 {
@@ -3055,11 +3066,15 @@ static int lmv_unpackmd(struct obd_export *exp, struct lmv_stripe_md **lsmp,
 			RETURN(0);
 		}
 
-		for (i = 0; i < lsm->lsm_md_stripe_count; i++) {
-			if (lsm->lsm_md_oinfo[i].lmo_root)
-				iput(lsm->lsm_md_oinfo[i].lmo_root);
+		if (lsm->lsm_md_magic == LMV_MAGIC) {
+			for (i = 0; i < lsm->lsm_md_stripe_count; i++) {
+				if (lsm->lsm_md_oinfo[i].lmo_root)
+					iput(lsm->lsm_md_oinfo[i].lmo_root);
+			}
+			lsm_size = lmv_stripe_md_size(lsm->lsm_md_stripe_count);
+		} else {
+			lsm_size = lmv_stripe_md_size(0);
 		}
-		lsm_size = lmv_stripe_md_size(lsm->lsm_md_stripe_count);
 		OBD_FREE(lsm, lsm_size);
 		*lsmp = NULL;
 		RETURN(0);
@@ -3116,6 +3131,9 @@ static int lmv_unpackmd(struct obd_export *exp, struct lmv_stripe_md **lsmp,
 	switch (le32_to_cpu(lmm->lmv_magic)) {
 	case LMV_MAGIC_V1:
 		rc = lmv_unpack_md_v1(exp, lsm, &lmm->lmv_md_v1);
+		break;
+	case LMV_USER_MAGIC:
+		rc = lmv_unpack_user_md(exp, lsm, &lmm->lmv_user_md);
 		break;
 	default:
 		CERROR("%s: unrecognized magic %x\n", exp->exp_obd->obd_name,
@@ -3243,6 +3261,10 @@ int lmv_free_lustre_md(struct obd_export *exp, struct lustre_md *md)
 	struct lmv_tgt_desc	*tgt = lmv->tgts[0];
 	ENTRY;
 
+	if (md->default_lmv) {
+		lmv_free_memmd(md->default_lmv);
+		md->default_lmv = NULL;
+	}
 	if (md->lmv != NULL) {
 		lmv_free_memmd(md->lmv);
 		md->lmv = NULL;

commit 1ce2fee3156858e132dbc1c90f72a32f526de988

LU-15456 llite: deadlock in ll_new_node()
ll_new_node() will call ll_dir_getstripe() to fetch parent default
LMV if md_create() returns -EREMOTE, it should call
ll_finish_md_op_data() before calling ll_dir_getstripe() because
the latter will lock lli_lsm_sem again, which will deadlock.

Fixes: 55ca00c3d1cd863 ("LU-11213 ptlrpc: intent_getattr fetches default LMV")
Test-Parameters: mdscount=2 mdtcount=4 testlist=racer,racer,racer
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ib858bae19ff88533fe487583c27d544026aafa3f
Reviewed-on: https://review.whamcloud.com/46157
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/llite/namei.c
@@ -1659,11 +1659,11 @@ again:
 
 		ptlrpc_req_finished(request);
 		request = NULL;
+		ll_finish_md_op_data(op_data);
+		op_data = NULL;
 
 		err2 = ll_dir_getstripe(dir, (void **)&lum, &lumsize, &request,
 					OBD_MD_DEFAULT_MEA);
-		ll_finish_md_op_data(op_data);
-		op_data = NULL;
 		if (err2 == 0) {
 			struct lustre_md md = { NULL };
 