lustre/lustre-release

commit d76cc65d5d68ed3e04bfbd9b7527f64ab0ee0ca7

LU-12125 mds: allow parallel regular file rename
Allow rename of non-directory files in the same directory to be done
in parallel, by only taking the DLM lock on the parent FID, without
also locking the global LUSTRE_BFL_FID (Big Filesystem Lock).

Older clients may not send the renamed file mode in mds_rec_rename.
In this case, the LUSTRE_BFL_FID lock will still be taken, and is not
worse than before parallel rename was allowed.

Similarly, if (for whatever reason) there is a mix of MDS versions
running in the same filesystem, at worst older MDSes will continue to
unnecessarily lock LUSTRE_BFL_FID before doing the file rename.

If MDT0000 is on an older MDS, but newer MDSes are doing renames of
non-directories, the newer MDSes will *not* lock LUSTRE_BFL_FID first,
but there will still be proper serialization from the parent directory
FID lock for other renames affecting the parent and the source/target
entries.  That MDT0000 is unaware of the rename is the whole point.

In case of a race, where the file mode sent by the client is stale,
this is also not a concern, because the file mode is rechecked later
under lock and the rename fails if the source and target mode differ.

Test-Parameters: testlist=racer env=DURATION=3600
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: If330b53eb6db46e40f50fd7834a83e80db3ebbe5
Reviewed-on: https://review.whamcloud.com/41186
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_reint.c
@@ -2604,11 +2604,22 @@ static int mdt_reint_rename(struct mdt_thread_info *info,
 		    mdt_object_remote(msrcdir))
 			GOTO(out_put_tgtdir, rc = -EXDEV);
 
-		rc = mdt_rename_lock(info, &rename_lh);
-		if (rc != 0) {
-			CERROR("%s: can't lock FS for rename: rc = %d\n",
-			       mdt_obd_name(mdt), rc);
-			GOTO(out_put_tgtdir, rc);
+		/* This may be further relaxed in the future for different
+		 * source and target parents. Start with only same-directory
+		 * renames of non-directory files for simplicity and because
+		 * this is by far the most common use case.
+		 */
+		if (msrcdir != mtgtdir || S_ISDIR(ma->ma_attr.la_mode)) {
+			rc = mdt_rename_lock(info, &rename_lh);
+			if (rc != 0) {
+				CERROR("%s: cannot lock for rename: rc = %d\n",
+				       mdt_obd_name(mdt), rc);
+				GOTO(out_put_tgtdir, rc);
+			}
+		} else {
+			CDEBUG(D_INFO, "%s: samedir rename "DFID"/"DNAME"\n",
+			       mdt_obd_name(mdt), PFID(rr->rr_fid1),
+			       PNAME(&rr->rr_name));
 		}
 	}
 

commit 82ec537d8b4cc9261828f4efe6b03d8d33f38432

LU-15285 mdt: fix same-dir racing rename deadlock
With LU-12125 lifting the BFL for same directory rename,
a deadlock possibility opens up since we lock source and target
of rename in the source-target order, if there are two renames
racing to rename arguments in reverse order:
mv a b &
mv b a

a lock inversion happens and a deadlock has been observed.

To avert this - instill additional order requirement:
lower PDO hash value is to be locked ahead of the higher one.

Fixes: d76cc65d5d68 ("LU-12125 mds: allow parallel regular file rename")
Fixes: b50bb830f92e ("LU-3538 dne: Commit-on-Sharing for DNE")
Fixes: 9f1711f3d7d1 ("LU-12081 mdt: rename shouldn't PDO lock if parent is remote")
Change-Id: I88dd3aebb394ea40e97e6029d6dcc161116f982e
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/45676
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>

lustre/mdt/mdt_reint.c
@@ -2615,6 +2615,43 @@ static int mdt_rename_source_lock(struct mdt_thread_info *info,
 	return rc;
 }
 
+/* Helper function for mdt_reint_rename so we don't need to opencode
+ * two different order lockings
+ */
+static int mdt_lock_two_dirs(struct mdt_thread_info *info,
+			     struct mdt_object *mfirstdir,
+			     struct mdt_lock_handle *lh_firstdirp,
+			     struct mdt_object *mseconddir,
+			     struct mdt_lock_handle *lh_seconddirp,
+			     bool cos_incompat)
+{
+	int rc;
+
+	rc = mdt_object_lock_save(info, mfirstdir, lh_firstdirp, 0,
+				  cos_incompat);
+	if (rc)
+		return rc;
+
+	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RENAME, 5);
+
+	if (mfirstdir != mseconddir) {
+		rc = mdt_object_lock_save(info, mseconddir, lh_seconddirp, 1,
+					  cos_incompat);
+	} else if (!mdt_object_remote(mseconddir) &&
+		   lh_firstdirp->mlh_pdo_hash !=
+		   lh_seconddirp->mlh_pdo_hash) {
+		rc = mdt_pdir_hash_lock(info, lh_seconddirp, mseconddir,
+					MDS_INODELOCK_UPDATE,
+					cos_incompat);
+		OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_PDO_LOCK2, 10);
+	}
+
+	if (rc != 0)
+		mdt_object_unlock(info, mfirstdir, lh_firstdirp, rc);
+
+	return rc;
+}
+
 /*
  * VBR: rename versions in reply: 0 - srcdir parent; 1 - tgtdir parent;
  * 2 - srcdir child; 3 - tgtdir child.
@@ -2714,7 +2751,6 @@ static int mdt_reint_rename(struct mdt_thread_info *info,
 	rc = mdt_rename_determine_lock_order(info, msrcdir, mtgtdir);
 	if (rc < 0)
 		GOTO(out_unlock_rename, rc);
-
 	reverse = rc;
 
 	/* source needs to be looked up after locking source parent, otherwise
@@ -2737,44 +2773,24 @@ relock:
 	mdt_lock_pdo_init(lh_srcdirp, LCK_PW, &rr->rr_name);
 	mdt_lock_pdo_init(lh_tgtdirp, LCK_PW, &rr->rr_tgt_name);
 
-	if (reverse) {
-		rc = mdt_object_lock_save(info, mtgtdir, lh_tgtdirp, 1,
-					  cos_incompat);
-		if (rc)
-			GOTO(out_unlock_rename, rc);
+	/* In case of same dir local rename we must sort by the hash,
+	 * otherwise a lock deadlock is possible when renaming
+	 * a to b and b to a at the same time LU-15285
+	 */
+	if (!mdt_object_remote(mtgtdir) && mtgtdir == msrcdir)
+		reverse = lh_srcdirp->mlh_pdo_hash > lh_tgtdirp->mlh_pdo_hash;
+	if (unlikely(OBD_FAIL_PRECHECK(OBD_FAIL_MDS_PDO_LOCK)))
+		reverse = 0;
 
-		OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RENAME, 5);
+	if (reverse)
+		rc = mdt_lock_two_dirs(info, mtgtdir, lh_tgtdirp, msrcdir,
+				       lh_srcdirp, cos_incompat);
+	else
+		rc = mdt_lock_two_dirs(info, msrcdir, lh_srcdirp, mtgtdir,
+				       lh_tgtdirp, cos_incompat);
 
-		rc = mdt_object_lock_save(info, msrcdir, lh_srcdirp, 0,
-					  cos_incompat);
-		if (rc != 0) {
-			mdt_object_unlock(info, mtgtdir, lh_tgtdirp, rc);
-			GOTO(out_unlock_rename, rc);
-		}
-	} else {
-		rc = mdt_object_lock_save(info, msrcdir, lh_srcdirp, 0,
-					  cos_incompat);
-		if (rc)
-			GOTO(out_unlock_rename, rc);
-
-		OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RENAME, 5);
-
-		if (mtgtdir != msrcdir) {
-			rc = mdt_object_lock_save(info, mtgtdir, lh_tgtdirp, 1,
-						  cos_incompat);
-		} else if (!mdt_object_remote(mtgtdir) &&
-			   lh_srcdirp->mlh_pdo_hash !=
-			   lh_tgtdirp->mlh_pdo_hash) {
-			rc = mdt_pdir_hash_lock(info, lh_tgtdirp, mtgtdir,
-						MDS_INODELOCK_UPDATE,
-						cos_incompat);
-			OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_PDO_LOCK2, 10);
-		}
-		if (rc != 0) {
-			mdt_object_unlock(info, msrcdir, lh_srcdirp, rc);
-			GOTO(out_unlock_rename, rc);
-		}
-	}
+	if (rc != 0)
+		GOTO(out_unlock_rename, rc);
 
 	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RENAME4, 5);
 	OBD_FAIL_TIMEOUT(OBD_FAIL_MDS_RENAME2, 5);