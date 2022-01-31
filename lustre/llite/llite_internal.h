lustre/lustre-release

commit 624a3ac233938153e889ae81f639cf46c0a8312b

LU-921 llite: warning in case of discarding dirty pages
when a client is evicted, dirty pages may get silently discarded,
the caller of successful write(2) will not know that the data he
wrote have been discarded due to eviction before it can be flushed
to the OSS.

Signed-off-by: Hongchao Zhang <hongchao.zhang@whamcloud.com>
Change-Id: Iecfbf096548ff08cdd6064d53ad8c688343fcddc
Reviewed-on: http://review.whamcloud.com/1908
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -819,6 +819,7 @@ void ll_umount_begin(struct super_block *sb);
 #endif
 int ll_remount_fs(struct super_block *sb, int *flags, char *data);
 int ll_show_options(struct seq_file *seq, struct vfsmount *vfs);
+void ll_dirty_page_discard_warn(cfs_page_t *page, int ioret);
 int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
                   struct super_block *);
 void lustre_dump_dentry(struct dentry *, int recur);

lustre/llite/llite_lib.c
@@ -2407,3 +2407,48 @@ char *ll_get_fsname(struct super_block *sb, char *buf, int buflen)
 
 	return buf;
 }
+
+static char* ll_d_path(struct dentry *dentry, char *buf, int bufsize)
+{
+	char *path = NULL;
+
+#ifdef HAVE_FS_STRUCT_USE_PATH
+	struct path p;
+
+	p.dentry = dentry;
+	p.mnt = current->fs->root.mnt;
+	path_get(&p);
+	path = d_path(&p, buf, bufsize);
+	path_put(&p);
+#else
+	path = d_path(dentry, current->fs->rootmnt, buf, bufsize);
+#endif
+
+	return path;
+}
+
+void ll_dirty_page_discard_warn(cfs_page_t *page, int ioret)
+{
+	char *buf, *path = NULL;
+	struct dentry *dentry = NULL;
+	struct ccc_object *obj = cl_inode2ccc(page->mapping->host);
+
+	buf = (char *)__get_free_page(GFP_KERNEL);
+	if (buf != NULL) {
+		dentry = d_find_alias(page->mapping->host);
+		if (dentry != NULL)
+			path = ll_d_path(dentry, buf, PAGE_SIZE);
+	}
+
+	CWARN("%s: dirty page discard: %s/fid: "DFID"/%s may get corrupted "
+	      "(rc %d)\n", ll_get_fsname(page->mapping->host->i_sb, NULL, 0),
+	      s2lsi(page->mapping->host->i_sb)->lsi_lmd->lmd_dev,
+	      PFID(&obj->cob_header.coh_lu.loh_fid),
+	      (path && !IS_ERR(path)) ? path : "", ioret);
+
+	if (dentry != NULL)
+		dput(dentry);
+
+	if (buf != NULL)
+		free_page((unsigned long)buf);
+}

lustre/llite/vvp_page.c
@@ -245,15 +245,24 @@ static int vvp_page_prep_write(const struct lu_env *env,
  */
 static void vvp_vmpage_error(struct inode *inode, cfs_page_t *vmpage, int ioret)
 {
-        if (ioret == 0)
-                ClearPageError(vmpage);
-        else if (ioret != -EINTR) {
-                SetPageError(vmpage);
-                if (ioret == -ENOSPC)
-                        set_bit(AS_ENOSPC, &inode->i_mapping->flags);
-                else
-                        set_bit(AS_EIO, &inode->i_mapping->flags);
-        }
+	struct ccc_object *obj = cl_inode2ccc(inode);
+
+	if (ioret == 0) {
+		ClearPageError(vmpage);
+		obj->cob_discard_page_warned = 0;
+	} else {
+		SetPageError(vmpage);
+		if (ioret == -ENOSPC)
+			set_bit(AS_ENOSPC, &inode->i_mapping->flags);
+		else
+			set_bit(AS_EIO, &inode->i_mapping->flags);
+
+		if ((ioret == -ESHUTDOWN || ioret == -EINTR) &&
+		     obj->cob_discard_page_warned == 0) {
+			obj->cob_discard_page_warned = 1;
+			ll_dirty_page_discard_warn(vmpage, ioret);
+		}
+	}
 }
 
 static void vvp_page_completion_read(const struct lu_env *env,

lustre/include/lclient.h
@@ -204,6 +204,18 @@ struct ccc_object {
          * \see ll_vm_open(), ll_vm_close().
          */
         cfs_atomic_t            cob_mmap_cnt;
+
+	/**
+	 * various flags
+	 * cob_discard_page_warned
+	 *     if pages belonging to this object are discarded when a client
+	 * is evicted, some debug info will be printed, this flag will be set
+	 * during processing the first discarded page, then avoid flooding
+	 * debug message for lots of discarded pages.
+	 *
+	 * \see ll_dirty_page_discard_warn.
+	 */
+	int			cob_discard_page_warned:1;
 };
 
 /**

lustre/tests/recovery-small.sh
@@ -690,7 +690,7 @@ test_23() { #b=4561
 }
 run_test 23 "client hang when close a file after mds crash"
 
-test_24() { # bug 11710 details correct fsync() behavior
+test_24a() { # bug 11710 details correct fsync() behavior
 	remote_ost_nodsh && skip "remote OST with nodsh" && return 0
 
 	mkdir -p $DIR/$tdir
@@ -706,7 +706,7 @@ test_24() { # bug 11710 details correct fsync() behavior
 	client_reconnect
 	[ $rc -eq 0 ] && error_ignore 5494 "multiop didn't fail fsync: rc $rc" || true
 }
-run_test 24 "fsync error (should return error)"
+run_test 24a "fsync error (should return error)"
 
 wait_client_evicted () {
 	local facet=$1
@@ -714,9 +714,42 @@ wait_client_evicted () {
 	local varsvc=${facet}_svc
 
 	wait_update $(facet_active_host $facet) \
-                "lctl get_param -n *.${!varsvc}.num_exports | cut -d' ' -f2" \
-                $((exports - 1)) $3
+		"lctl get_param -n *.${!varsvc}.num_exports | cut -d' ' -f2" \
+		$((exports - 1)) $3
 }
+
+test_24b() {
+	remote_ost_nodsh && skip "remote OST with nodsh" && return 0
+
+	dmesg -c
+	mkdir -p $DIR/$tdir
+	lfs setstripe $DIR/$tdir -s 0 -i 0 -c 1
+	cancel_lru_locks osc
+	multiop_bg_pause $DIR/$tdir/$tfile-1 Ow8192_yc ||
+		error "mulitop Ow8192_yc failed"
+
+	MULTI_PID1=$!
+	multiop_bg_pause $DIR/$tdir/$tfile-2 Ow8192_c ||
+		error "mulitop Ow8192_c failed"
+
+	MULTI_PID2=$!
+	ost_evict_client
+
+	kill -USR1 $MULTI_PID1
+	wait $MULTI_PID1
+	rc1=$?
+	kill -USR1 $MULTI_PID2
+	wait $MULTI_PID2
+	rc2=$?
+	lctl set_param fail_loc=0x0
+	client_reconnect
+	[ $rc1 -eq 0 -o $rc2 -eq 0 ] &&
+	error_ignore "multiop didn't fail fsync: $rc1 or close: $rc2" || true
+
+	dmesg | grep "dirty page discard:" || \
+		error "no discarded dirty page found!"
+}
+run_test 24b "test dirty page discard due to client eviction"
 
 test_26a() {      # was test_26 bug 5921 - evict dead exports by pinger
 # this test can only run from a client on a separate node.

commit a1d75780ba19cfca53cbacf0d38e8d7df540b209

LU-15340 llite: Delay dput in ll_dirty_page_discard_warn
Otherwise we can be final dput and need to wait for pages
to clear which is bad because this is called from ptlrpcd
that is not supposed to block esp. for network traffic as
it can cause livelocks if it happens to be needed to kill
the very same RPC we are waiting on.

Additionally pass in the inode from IO since the page
we are using might come from directio and that is
probably not even a valid inode.

Fixes: 624a3ac23393 ("LU-921 llite: warning in case of discarding dirty pages")
Change-Id: Ie2f1a34047145202c11a4e1a0b18b2e01d9e4601
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/45784
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>

lustre/llite/llite_internal.h
@@ -1228,7 +1228,7 @@ int ll_flush_ctx(struct inode *inode);
 void ll_umount_begin(struct super_block *sb);
 int ll_remount_fs(struct super_block *sb, int *flags, char *data);
 int ll_show_options(struct seq_file *seq, struct dentry *dentry);
-void ll_dirty_page_discard_warn(struct page *page, int ioret);
+void ll_dirty_page_discard_warn(struct inode *inode, int ioret);
 int ll_prep_inode(struct inode **inode, struct req_capsule *pill,
 		  struct super_block *sb, struct lookup_intent *it);
 int ll_obd_statfs(struct inode *inode, void __user *arg);

lustre/llite/llite_lib.c
@@ -3447,6 +3447,23 @@ int ll_get_obd_name(struct inode *inode, unsigned int cmd, unsigned long arg)
 	RETURN(0);
 }
 
+struct dname_buf {
+	struct work_struct db_work;
+	struct dentry *db_dentry;
+	/* Let's hope the path is not too long, 32 bytes for the work struct
+	 * on my kernel
+	 */
+	char buf[PAGE_SIZE - sizeof(struct work_struct) - sizeof(void *)];
+};
+
+static void ll_dput_later(struct work_struct *work)
+{
+	struct dname_buf *db = container_of(work, struct dname_buf, db_work);
+
+	dput(db->db_dentry);
+	free_page((unsigned long)db);
+}
+
 static char* ll_d_path(struct dentry *dentry, char *buf, int bufsize)
 {
 	char *path = NULL;
@@ -3461,33 +3478,43 @@ static char* ll_d_path(struct dentry *dentry, char *buf, int bufsize)
 	return path;
 }
 
-void ll_dirty_page_discard_warn(struct page *page, int ioret)
+void ll_dirty_page_discard_warn(struct inode *inode, int ioret)
 {
-	char *buf, *path = NULL;
+	struct dname_buf *db;
+	char  *path = NULL;
 	struct dentry *dentry = NULL;
-	struct inode *inode = page->mapping->host;
 
 	/* this can be called inside spin lock so use GFP_ATOMIC. */
-	buf = (char *)__get_free_page(GFP_ATOMIC);
-	if (buf != NULL) {
-		dentry = d_find_alias(page->mapping->host);
+	db = (struct dname_buf *)__get_free_page(GFP_ATOMIC);
+	if (db != NULL) {
+
+		dentry = d_find_alias(inode);
 		if (dentry != NULL)
-			path = ll_d_path(dentry, buf, PAGE_SIZE);
+			path = ll_d_path(dentry, db->buf, sizeof(db->buf));
 	}
 
 	/* The below message is checked in recovery-small.sh test_24b */
 	CDEBUG(D_WARNING,
 	       "%s: dirty page discard: %s/fid: "DFID"/%s may get corrupted "
 	       "(rc %d)\n", ll_i2sbi(inode)->ll_fsname,
-	       s2lsi(page->mapping->host->i_sb)->lsi_lmd->lmd_dev,
+	       s2lsi(inode->i_sb)->lsi_lmd->lmd_dev,
 	       PFID(ll_inode2fid(inode)),
 	       (path && !IS_ERR(path)) ? path : "", ioret);
 
-	if (dentry != NULL)
-		dput(dentry);
-
-	if (buf != NULL)
-		free_page((unsigned long)buf);
+	if (dentry != NULL) {
+		/* We cannot dput here since if we happen to be the last holder
+		 * then we can end up waiting for page evictions that
+		 * in turn wait for RPCs that need this instance of ptlrpcd
+		 * (callng brw_interpret->*page_completion*->vmpage_error->here)
+		 * LU-15340
+		 */
+		INIT_WORK(&db->db_work, ll_dput_later);
+		db->db_dentry = dentry;
+		schedule_work(&db->db_work);
+	} else {
+		if (db != NULL)
+			free_page((unsigned long)db);
+	}
 }
 
 ssize_t ll_copy_user_md(const struct lov_user_md __user *md,

lustre/llite/vvp_page.c
@@ -254,7 +254,7 @@ static void vvp_vmpage_error(struct inode *inode, struct page *vmpage,
 		if ((ioret == -ESHUTDOWN || ioret == -EINTR ||
 		     ioret == -EIO) && obj->vob_discard_page_warned == 0) {
 			obj->vob_discard_page_warned = 1;
-			ll_dirty_page_discard_warn(vmpage, ioret);
+			ll_dirty_page_discard_warn(inode, ioret);
 		}
 	}
 }