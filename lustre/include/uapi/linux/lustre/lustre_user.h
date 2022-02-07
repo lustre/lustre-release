lustre/lustre-release

commit 0a1cf8da806962d663f23ed813764e4011a36ee7

LU-11025 dne: introduce new directory hash type: "crush"
Current directory striping strategy is fixed striping, i.e., it
calls (hash(filename) % stripe_count) to decide stripe for file.
The problem with this approach is that if stripe_count changes,
most of the files will need to be relocated between MDTs. This
makes directory split/merge quite expensive.

This patch introduces consistent hash striping strategy:
it calls (hash(filename) % LMV_CRUSH_PG_COUNT) to locate PG_ID
(placement group index), and then calls
crush_hash(PG_ID, stripe_index) to get a straw for each stripe,
and the stripe with the highest staw will be used to place this
file.

As we can see, it uses the CRUSH algorithm, but it only uses it
to map placement group pseudo-randomly among all stripes, while
doesn't use it to choose MDTs if MDT is not specified. The latter
is done by MDT object QoS allocation in LMV and LOD (LMV decides
the starting stripe MDT, while LOD decides the rest stripes).

This implementation contains below changes:
* new hash type "crush", and lmv_name_to_stripe_index() will take
  care of it in mapping name to stripe index.
* add "mdt_hash" sysfs tunable for LOD, which will be the default LMV
  hash type used in mkdir.
* if 'lfs setdirstripe' doesn't set hash type, server will choose
  the default LMV hash type.
* place temp file on MDT where target is: map temporary file created
  by rsync or mpifileutils dstripe on the same MDT where target is
  located, so later rename won't make target a remote entry. And the
  same for backup file ends with .bak, .sav, .orig or ~.

Compatibility:
* if server doesn't support OBD_CONNECT2_CRUSH, client will switch
  to "fnv_1a_64" hash type if "crush" is specified.
* if client doesn't know "crush" hash type, it will try all stripes
  in lookup.
* client will set hash type to LMV_HASH_TYPE_UNKNOWN if setdirstripe
  doesn't set it explicitly, and server will use the default hash

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -990,6 +990,7 @@ enum lmv_hash_type {
 	LMV_HASH_TYPE_UNKNOWN	= 0,	/* 0 is reserved for testing purpose */
 	LMV_HASH_TYPE_ALL_CHARS = 1,
 	LMV_HASH_TYPE_FNV_1A_64 = 2,
+	LMV_HASH_TYPE_CRUSH	= 3,
 	LMV_HASH_TYPE_MAX,
 };
 
@@ -997,9 +998,7 @@ enum lmv_hash_type {
 
 #define LMV_HASH_NAME_ALL_CHARS	"all_char"
 #define LMV_HASH_NAME_FNV_1A_64	"fnv_1a_64"
-
-/* not real hash type, but exposed to user as "space" hash type */
-#define LMV_HASH_NAME_SPACE	"space"
+#define LMV_HASH_NAME_CRUSH	"crush"
 
 /* Right now only the lower part(0-16bits) of lmv_hash_type is being used,
  * and the higher part will be the flag to indicate the status of object,
@@ -1010,16 +1009,17 @@ enum lmv_hash_type {
 static inline bool lmv_is_known_hash_type(__u32 type)
 {
 	return (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_FNV_1A_64 ||
-	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_ALL_CHARS;
+	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_ALL_CHARS ||
+	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_CRUSH;
 }
 
 /* The striped directory has ever lost its master LMV EA, then LFSCK
  * re-generated it. This flag is used to indicate such case. It is an
  * on-disk flag. */
-#define LMV_HASH_FLAG_LOST_LMV	0x10000000
+#define LMV_HASH_FLAG_LOST_LMV		0x10000000
 
-#define LMV_HASH_FLAG_BAD_TYPE	0x20000000
-#define LMV_HASH_FLAG_MIGRATION	0x80000000
+#define LMV_HASH_FLAG_BAD_TYPE		0x20000000
+#define LMV_HASH_FLAG_MIGRATION		0x80000000
 
 extern char *mdt_hash_name[LMV_HASH_TYPE_MAX];
 

lustre/llite/dir.c
@@ -464,7 +464,7 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 	int err;
 	ENTRY;
 
-	if (unlikely(!lmv_magic_supported(lump->lum_magic)))
+	if (unlikely(!lmv_user_magic_supported(lump->lum_magic)))
 		RETURN(-EINVAL);
 
 	if (lump->lum_magic != LMV_MAGIC_FOREIGN) {
@@ -489,7 +489,26 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 	    !OBD_FAIL_CHECK(OBD_FAIL_LLITE_NO_CHECK_DEAD))
 		RETURN(-ENOENT);
 
-	if (unlikely(!lmv_magic_supported(cpu_to_le32(lump->lum_magic))))
+	if (!(exp_connect_flags2(sbi->ll_md_exp) & OBD_CONNECT2_CRUSH)) {
+		if ((lump->lum_hash_type & LMV_HASH_TYPE_MASK) ==
+		     LMV_HASH_TYPE_CRUSH) {
+			/* if server doesn't support 'crush' hash type,
+			 * switch to fnv_1a_64.
+			 */
+			lump->lum_hash_type &= ~LMV_HASH_TYPE_MASK;
+			lump->lum_hash_type |= LMV_HASH_TYPE_FNV_1A_64;
+		} else if ((lump->lum_hash_type & LMV_HASH_TYPE_MASK) ==
+		     LMV_HASH_TYPE_UNKNOWN) {
+			/* from 2.14 MDT will choose default hash type if client
+			 * doesn't set a valid one, while old server doesn't
+			 * handle it.
+			 */
+			lump->lum_hash_type &= ~LMV_HASH_TYPE_MASK;
+			lump->lum_hash_type |= LMV_HASH_TYPE_DEFAULT;
+		}
+	}
+
+	if (unlikely(!lmv_user_magic_supported(cpu_to_le32(lump->lum_magic))))
 		lustre_swab_lmv_user_md(lump);
 
 	if (!IS_POSIXACL(parent) || !exp_connect_umask(ll_i2mdexp(parent)))
@@ -1473,7 +1492,7 @@ out_free:
 		lum = (struct lmv_user_md *)data->ioc_inlbuf2;
 		lumlen = data->ioc_inllen2;
 
-		if (!lmv_magic_supported(lum->lum_magic)) {
+		if (!lmv_user_magic_supported(lum->lum_magic)) {
 			CERROR("%s: wrong lum magic %x : rc = %d\n", filename,
 			       lum->lum_magic, -EINVAL);
 			GOTO(lmv_out_free, rc = -EINVAL);

lustre/doc/lfs-migrate.1
@@ -99,6 +99,11 @@ Use
 for the new layout.
 .RS 1.2i
 .TP
+.B crush
+CRUSH hash algorithm.  This is a consistent hash
+algorithm, so minimum sub files need to relocate
+during directory restripe.
+.TP
 .B fnv_1a_64
 Fowler-Noll-Vo (FNV-1a) hash algorithm.  This provides
 reasonably uniform, but not cryptographically strong,

lustre/doc/lfs-setdirstripe.1
@@ -29,15 +29,20 @@ Use the MDT whose index is
 as the master/starting MDT for the directory. If multiple
 .I MDT_INDEX
 values are given, then the stripes will be allocated on the specified
-MDT indices.  If index -1 (default) is used, it will randomly pick
-.I COUNT
-less full MDTs.
+MDT indices.  If index -1 (default) is used, it will prefer to select
+.B COUNT
+MDTs proportional to the free space and inodes on each.
 .TP
 .BR \-H ", " \-\-mdt-hash =\fIHASH_TYPE\fR
 Use
 .I hash_type
 for the striped directory.
 .RS 1.2i
+.TP
+.B crush
+CRUSH hash algorithm.  This is a consistent hash
+algorithm, so minimum sub files need to relocate
+during directory restripe.
 .TP
 .B fnv_1a_64
 Fowler-Noll-Vo (FNV-1a) hash algorithm.  This provides

lustre/include/lu_object.h
@@ -38,6 +38,7 @@
 #include <uapi/linux/lustre/lustre_idl.h>
 #include <lu_ref.h>
 #include <linux/percpu_counter.h>
+#include <linux/ctype.h>
 
 struct seq_file;
 struct proc_dir_entry;
@@ -1313,6 +1314,66 @@ static inline bool name_is_dot_or_dotdot(const char *name, int namelen)
 static inline bool lu_name_is_dot_or_dotdot(const struct lu_name *lname)
 {
 	return name_is_dot_or_dotdot(lname->ln_name, lname->ln_namelen);
+}
+
+static inline bool lu_name_is_temp_file(const char *name, int namelen,
+					bool dot_prefix, int suffixlen)
+{
+	int lower = 0;
+	int upper = 0;
+	int digit = 0;
+	int len = suffixlen;
+
+	if (dot_prefix && name[0] != '.')
+		return false;
+
+	if (namelen < dot_prefix + suffixlen + 2 ||
+	    name[namelen - suffixlen - 1] != '.')
+		return false;
+
+	while (len) {
+		lower += islower(name[namelen - len]);
+		upper += isupper(name[namelen - len]);
+		digit += isdigit(name[namelen - len]);
+		len--;
+	}
+	/* mktemp() filename suffixes will have a mix of upper- and lower-case
+	 * letters and/or numbers, not all numbers, or all upper or lower-case.
+	 * About 0.07% of randomly-generated names will slip through,
+	 * but this avoids 99.93% of cross-MDT renames for those files.
+	 */
+	if (digit >= suffixlen - 2 || upper == suffixlen || lower == suffixlen)
+		return false;
+
+	return true;
+}
+
+static inline bool lu_name_is_backup_file(const char *name, int namelen,
+					  int *suffixlen)
+{
+	if (namelen > 1 &&
+	    name[namelen - 2] != '.' && name[namelen - 1] == '~') {
+		if (suffixlen)
+			*suffixlen = 1;
+		return true;
+	}
+
+	if (namelen > 4 && name[namelen - 4] == '.' &&
+	    (!strncasecmp(name + namelen - 3, "bak", 3) ||
+	     !strncasecmp(name + namelen - 3, "sav", 3))) {
+		if (suffixlen)
+			*suffixlen = 4;
+		return true;
+	}
+
+	if (namelen > 5 && name[namelen - 5] == '.' &&
+	    !strncasecmp(name + namelen - 4, "orig", 4)) {
+		if (suffixlen)
+			*suffixlen = 5;
+		return true;
+	}
+
+	return false;
 }
 
 static inline bool lu_name_is_valid_len(const char *name, size_t name_len)

lustre/include/lustre_lmv.h
@@ -79,6 +79,9 @@ static inline bool lmv_dir_bad_hash(const struct lmv_stripe_md *lsm)
 	    lsm->lsm_md_stripe_count - lsm->lsm_md_migrate_offset <= 1)
 		return false;
 
+	if (lsm->lsm_md_hash_type & LMV_HASH_FLAG_BAD_TYPE)
+		return true;
+
 	return !lmv_is_known_hash_type(lsm->lsm_md_hash_type);
 }
 
@@ -193,40 +196,140 @@ lmv_hash_fnv1a(unsigned int count, const char *name, int namelen)
 	return do_div(hash, count);
 }
 
-static inline int lmv_name_to_stripe_index(__u32 lmv_hash_type,
+/*
+ * Robert Jenkins' function for mixing 32-bit values
+ * http://burtleburtle.net/bob/hash/evahash.html
+ * a, b = random bits, c = input and output
+ *
+ * Mixing inputs to generate an evenly distributed hash.
+ */
+#define crush_hashmix(a, b, c)				\
+do {							\
+	a = a - b;  a = a - c;  a = a ^ (c >> 13);	\
+	b = b - c;  b = b - a;  b = b ^ (a << 8);	\
+	c = c - a;  c = c - b;  c = c ^ (b >> 13);	\
+	a = a - b;  a = a - c;  a = a ^ (c >> 12);	\
+	b = b - c;  b = b - a;  b = b ^ (a << 16);	\
+	c = c - a;  c = c - b;  c = c ^ (b >> 5);	\
+	a = a - b;  a = a - c;  a = a ^ (c >> 3);	\
+	b = b - c;  b = b - a;  b = b ^ (a << 10);	\
+	c = c - a;  c = c - b;  c = c ^ (b >> 15);	\
+} while (0)
+
+#define crush_hash_seed 1315423911
+
+static inline __u32 crush_hash(__u32 a, __u32 b)
+{
+	__u32 hash = crush_hash_seed ^ a ^ b;
+	__u32 x = 231232;
+	__u32 y = 1232;
+
+	crush_hashmix(a, b, hash);
+	crush_hashmix(x, a, hash);
+	crush_hashmix(b, y, hash);
+
+	return hash;
+}
+
+/* refer to https://github.com/ceph/ceph/blob/master/src/crush/hash.c and
+ * https://www.ssrc.ucsc.edu/Papers/weil-sc06.pdf for details of CRUSH
+ * algorithm.
+ */
+static inline unsigned int
+lmv_hash_crush(unsigned int count, const char *name, int namelen)
+{
+	unsigned long long straw;
+	unsigned long long highest_straw = 0;
+	unsigned int pg_id;
+	unsigned int idx = 0;
+	int i;
+
+	/* put temp and backup file on the same MDT where target is located.
+	 * temporary file naming rule:
+	 * 1. rsync: .<target>.XXXXXX
+	 * 2. dstripe: <target>.XXXXXXXX
+	 */
+	if (lu_name_is_temp_file(name, namelen, true, 6)) {
+		name++;
+		namelen -= 8;
+	} else if (lu_name_is_temp_file(name, namelen, false, 8)) {
+		namelen -= 9;
+	} else if (lu_name_is_backup_file(name, namelen, &i)) {
+		LASSERT(i < namelen);
+		namelen -= i;
+	}
+
+	pg_id = lmv_hash_fnv1a(LMV_CRUSH_PG_COUNT, name, namelen);
+
+	/* distribute PG among all stripes pseudo-randomly, so they are almost
+	 * evenly distributed, and when stripe count changes, only (delta /
+	 * total) sub files need to be moved, herein 'delta' is added or removed
+	 * stripe count, 'total' is total stripe count before change for
+	 * removal, or count after change for addition.
+	 */
+	for (i = 0; i < count; i++) {
+		straw = crush_hash(pg_id, i);
+		if (straw > highest_straw) {
+			highest_straw = straw;
+			idx = i;
+		}
+	}
+	LASSERT(idx < count);
+
+	return idx;
+}
+
+static inline int lmv_name_to_stripe_index(__u32 hash_type,
 					   unsigned int stripe_count,
 					   const char *name, int namelen)
 {
-	int idx;
+	unsigned int idx;
 
 	LASSERT(namelen > 0);
+	LASSERT(stripe_count > 0);
 
-	if (stripe_count <= 1)
+	if (stripe_count == 1)
 		return 0;
 
-	switch (lmv_hash_type & LMV_HASH_TYPE_MASK) {
+	switch (hash_type & LMV_HASH_TYPE_MASK) {
 	case LMV_HASH_TYPE_ALL_CHARS:
 		idx = lmv_hash_all_chars(stripe_count, name, namelen);
 		break;
 	case LMV_HASH_TYPE_FNV_1A_64:
 		idx = lmv_hash_fnv1a(stripe_count, name, namelen);
 		break;
-	default:
-		idx = -EBADFD;
+	case LMV_HASH_TYPE_CRUSH:
+		idx = lmv_hash_crush(stripe_count, name, namelen);
 		break;
+	default:
+		return -EBADFD;
 	}
 
 	CDEBUG(D_INFO, "name %.*s hash_type %#x idx %d/%u\n", namelen, name,
-	       lmv_hash_type, idx, stripe_count);
+	       hash_type, idx, stripe_count);
 
 	return idx;
 }
 
-static inline bool lmv_magic_supported(__u32 lum_magic)
+static inline bool lmv_user_magic_supported(__u32 lum_magic)
 {
 	return lum_magic == LMV_USER_MAGIC ||
 	       lum_magic == LMV_USER_MAGIC_SPECIFIC ||
 	       lum_magic == LMV_MAGIC_FOREIGN;
+}
+
+static inline bool lmv_is_sane(const struct lmv_mds_md_v1 *lmv)
+{
+	if (le32_to_cpu(lmv->lmv_magic) != LMV_MAGIC_V1)
+		return false;
+
+	if (le32_to_cpu(lmv->lmv_stripe_count) == 0)
+		return false;
+
+	if (!lmv_is_known_hash_type(lmv->lmv_hash_type))
+		return false;
+
+	return true;
 }
 
 #endif

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -900,7 +900,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_SELINUX_POLICY | \
 				OBD_CONNECT2_LSOM | \
 				OBD_CONNECT2_ASYNC_DISCARD | \
-				OBD_CONNECT2_PCC)
+				OBD_CONNECT2_PCC | \
+				OBD_CONNECT2_CRUSH)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \
@@ -2266,6 +2267,9 @@ static inline __u64 lustre_hash_fnv_1a_64(const void *buf, __kernel_size_t size)
 
 	return hash;
 }
+
+/* CRUSH placement group count */
+#define LMV_CRUSH_PG_COUNT	4096
 
 union lmv_mds_md {
 	__u32			 lmv_magic;

lustre/llite/llite_lib.c
@@ -269,7 +269,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 				   OBD_CONNECT2_INC_XID |
 				   OBD_CONNECT2_LSOM |
 				   OBD_CONNECT2_ASYNC_DISCARD |
-				   OBD_CONNECT2_PCC;
+				   OBD_CONNECT2_PCC |
+				   OBD_CONNECT2_CRUSH;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
         if (sbi->ll_flags & LL_SBI_LRU_RESIZE)

commit 51c6d596d4f3e82decb037c02e8b5d095e0a4962

LU-15502 llite: set default LMV hash type with 2.12 MDS
If default LMV hash type is CRUSH, or unset, it should be converted
to fnv_16_64, because 2.12 MDS doesn't understand this.

Fix LMV_HASH_FLAG_KNOWN to match actual known flags.

Test-Parameters: testlist=sanity env=ONLY=300 mdtcount=2 serverversion=2.12 serverdistro=el7.9
Fixes: 0a1cf8da8069 ("LU-11025 dne: introduce new directory hash type: "crush")
Fixes: bb60caa1c6e7 ("LU-14459 lmv: change default hash type to crush")
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ie2ad5a456040dcd01bc2c5ab96db52bf944abbd2
Reviewed-on: https://review.whamcloud.com/46378
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1042,7 +1042,7 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 #define LMV_HASH_FLAG_LAYOUT_CHANGE	\
 	(LMV_HASH_FLAG_MIGRATION | LMV_HASH_FLAG_SPLIT | LMV_HASH_FLAG_MERGE)
 
-#define LMV_HASH_FLAG_KNOWN		0xfe000000
+#define LMV_HASH_FLAG_KNOWN		0xbe000000
 
 /* both SPLIT and MIGRATION are set for directory split */
 static inline bool lmv_hash_is_splitting(__u32 hash)

lustre/llite/dir.c
@@ -496,7 +496,7 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 		enum lmv_hash_type type = lump->lum_hash_type &
 					  LMV_HASH_TYPE_MASK;
 
-		if (type == LMV_HASH_TYPE_CRUSH ||
+		if (type >= LMV_HASH_TYPE_CRUSH ||
 		    type == LMV_HASH_TYPE_UNKNOWN)
 			lump->lum_hash_type = (lump->lum_hash_type ^ type) |
 					      LMV_HASH_TYPE_FNV_1A_64;
@@ -620,12 +620,29 @@ int ll_dir_setstripe(struct inode *inode, struct lov_user_md *lump,
 		case LOV_USER_MAGIC_COMP_V1:
 			lum_size = ((struct lov_comp_md_v1 *)lump)->lcm_size;
 			break;
-		case LMV_USER_MAGIC:
-			if (lump->lmm_magic != cpu_to_le32(LMV_USER_MAGIC))
-				lustre_swab_lmv_user_md(
-					(struct lmv_user_md *)lump);
-			lum_size = sizeof(struct lmv_user_md);
+		case LMV_USER_MAGIC: {
+			struct lmv_user_md *lmv = (struct lmv_user_md *)lump;
+
+			/* MDS < 2.14 doesn't support 'crush' hash type, and
+			 * cannot handle unknown hash if client doesn't set a
+			 * valid one. switch to fnv_1a_64.
+			 */
+			if (!(exp_connect_flags2(sbi->ll_md_exp) &
+			      OBD_CONNECT2_CRUSH)) {
+				enum lmv_hash_type type = lmv->lum_hash_type &
+							  LMV_HASH_TYPE_MASK;
+
+				if (type >= LMV_HASH_TYPE_CRUSH ||
+				    type == LMV_HASH_TYPE_UNKNOWN)
+					lmv->lum_hash_type =
+						(lmv->lum_hash_type ^ type) |
+						LMV_HASH_TYPE_FNV_1A_64;
+			}
+			if (lmv->lum_magic != cpu_to_le32(LMV_USER_MAGIC))
+				lustre_swab_lmv_user_md(lmv);
+			lum_size = sizeof(*lmv);
 			break;
+		}
 		case LOV_USER_MAGIC_SPECIFIC: {
 			struct lov_user_md_v3 *v3 =
 				(struct lov_user_md_v3 *)lump;