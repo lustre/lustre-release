lustre/lustre-release

commit 80447caf980699fd1b0118e8f7c11e48aead04ce

LU-14926 utils: print unlink and setattr recs in llog_reader
Enhance llog_reader to print unlink and setattr llog records
correctly.

HPE-bug-id: LUS-10220
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I7b44f65c976459d143521185a807939524f67fa2
Reviewed-on: https://review.whamcloud.com/44591
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/llog_reader.c
@@ -916,6 +916,48 @@ void print_update_rec(struct llog_update_record *lur)
 
 }
 
+static void print_unlink_rec(struct llog_unlink_rec *lur)
+{
+	printf("unlink record id:0x%x target %llx:%x:%x\n",
+		__le32_to_cpu(lur->lur_hdr.lrh_id),
+		__le64_to_cpu(lur->lur_oid),
+		__le32_to_cpu(lur->lur_oseq),
+		__le32_to_cpu(lur->lur_count));
+}
+
+static void print_unlink64_rec(struct llog_unlink64_rec *lur)
+{
+	printf("unlink64 record id:0x%x target "DFID"\n",
+		__le32_to_cpu(lur->lur_hdr.lrh_id),
+		PFID(&lur->lur_fid));
+}
+
+static void print_setattr64_rec(struct llog_setattr64_rec *lsr)
+{
+	printf("setattr64 record id:0x%x target "DFID" valid %llx uid %u:%u gid %u:%u\n",
+		__le32_to_cpu(lsr->lsr_hdr.lrh_id),
+		PFID(&lsr->lsr_oi.oi_fid),
+		__le64_to_cpu(lsr->lsr_valid),
+		__le32_to_cpu(lsr->lsr_uid_h),
+		__le32_to_cpu(lsr->lsr_uid),
+		__le32_to_cpu(lsr->lsr_gid_h),
+		__le32_to_cpu(lsr->lsr_gid));
+}
+
+static void print_setattr64_rec_v2(struct llog_setattr64_rec_v2 *lsr)
+{
+	printf("setattr64 v2 record id:0x%x target "DFID" valid %llx uid %u:%u gid %u:%u prj %u\n",
+		__le32_to_cpu(lsr->lsr_hdr.lrh_id),
+		PFID(&lsr->lsr_oi.oi_fid),
+		__le64_to_cpu(lsr->lsr_valid),
+		__le32_to_cpu(lsr->lsr_uid_h),
+		__le32_to_cpu(lsr->lsr_uid),
+		__le32_to_cpu(lsr->lsr_gid_h),
+		__le32_to_cpu(lsr->lsr_gid),
+		__le32_to_cpu(lsr->lsr_projid));
+}
+
+
 static void print_records(struct llog_rec_hdr **recs,
 			  int rec_number, int is_ext)
 {
@@ -964,6 +1006,22 @@ static void print_records(struct llog_rec_hdr **recs,
 			break;
 		case UPDATE_REC:
 			print_update_rec((struct llog_update_record *)recs[i]);
+			break;
+		case MDS_UNLINK_REC:
+			print_unlink_rec((struct llog_unlink_rec *)recs[i]);
+			break;
+		case MDS_UNLINK64_REC:
+			print_unlink64_rec((struct llog_unlink64_rec *)recs[i]);
+			break;
+		case MDS_SETATTR64_REC:
+			if (__le32_to_cpu(recs[i]->lrh_len) >
+				sizeof(struct llog_setattr64_rec)) {
+				print_setattr64_rec_v2(
+				  (struct llog_setattr64_rec_v2 *)recs[i]);
+			} else {
+				print_setattr64_rec(
+					(struct llog_setattr64_rec *)recs[i]);
+			}
 			break;
 		default:
 			printf("unknown type %x\n", lopt);

commit 131f559c5a241f15b8a91009968dfbd9a5dcddeb

LU-15584 utils: ppc64le __le64_to_cpu type mismatch
Cast values returned by __le64_to_cpu to
long long unsigned int. This is to match print format
strings that use %llx. This mismatch was resulting in a
build failure for ppc64le.

Build log message:
llog_reader.c:921:42: error: format '%llx' expects
argument of type 'long long unsigned int', but
argument 3 has type 'long unsigned int'

Fixes: 80447caf980 LU-14926 utils: print unlink and setattr recs in llog_reader
Signed-off-by: Gian-Carlo DeFazio <defazio1@llnl.gov>
Change-Id: I939b94626d2707b6ff644324c5c2798218331c4d
Reviewed-on: https://review.whamcloud.com/46588
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/llog_reader.c
@@ -920,7 +920,7 @@ static void print_unlink_rec(struct llog_unlink_rec *lur)
 {
 	printf("unlink record id:0x%x target %llx:%x:%x\n",
 		__le32_to_cpu(lur->lur_hdr.lrh_id),
-		__le64_to_cpu(lur->lur_oid),
+		(unsigned long long)__le64_to_cpu(lur->lur_oid),
 		__le32_to_cpu(lur->lur_oseq),
 		__le32_to_cpu(lur->lur_count));
 }
@@ -937,7 +937,7 @@ static void print_setattr64_rec(struct llog_setattr64_rec *lsr)
 	printf("setattr64 record id:0x%x target "DFID" valid %llx uid %u:%u gid %u:%u\n",
 		__le32_to_cpu(lsr->lsr_hdr.lrh_id),
 		PFID(&lsr->lsr_oi.oi_fid),
-		__le64_to_cpu(lsr->lsr_valid),
+		(unsigned long long)__le64_to_cpu(lsr->lsr_valid),
 		__le32_to_cpu(lsr->lsr_uid_h),
 		__le32_to_cpu(lsr->lsr_uid),
 		__le32_to_cpu(lsr->lsr_gid_h),
@@ -949,7 +949,7 @@ static void print_setattr64_rec_v2(struct llog_setattr64_rec_v2 *lsr)
 	printf("setattr64 v2 record id:0x%x target "DFID" valid %llx uid %u:%u gid %u:%u prj %u\n",
 		__le32_to_cpu(lsr->lsr_hdr.lrh_id),
 		PFID(&lsr->lsr_oi.oi_fid),
-		__le64_to_cpu(lsr->lsr_valid),
+		(unsigned long long)__le64_to_cpu(lsr->lsr_valid),
 		__le32_to_cpu(lsr->lsr_uid_h),
 		__le32_to_cpu(lsr->lsr_uid),
 		__le32_to_cpu(lsr->lsr_gid_h),