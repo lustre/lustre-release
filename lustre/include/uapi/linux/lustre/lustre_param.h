7b8449e8f9ed310376a191ec8dfa2f1cb6233377
Sebastien Buisson <sbuisson@ddn.com>
2022-03-08 12:07:47 +0100

    LU-15625 sec: fix set_param -P for root squash
    lctl set_param -P needs to be fixed for root squash in order
    to properly propagate to clients.
    On server (MDT) side, the paramaters are mdt.*.*squash*.
    On client side, the parameters are llite.*.*squash*.
    So if the setting on MGS side is done as
    set_param -P *.*.root_squash and
    *.*.nosquash_nids, it works fine. But if it is done as
    set_param -P mdt.*.root_squash, this setting is not applied to
    client side.
    The solution is to have the settings translated into
    *.*.root_squash and *.*.nosquash_nids to make both sides happy.
    
    Also, to ensure client vs. server consistency, prevent
    set_param -P llite.*.root_squash.
    
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Change-Id: I8984c9d9024d7a053e548ed15d1321b281d9940f
    Reviewed-on: https://review.whamcloud.com/46739
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/include/uapi/linux/lustre/lustre_param.h
+++ lustre/include/uapi/linux/lustre/lustre_param.h
@@ -68,6 +68,8 @@
 #define PARAM_ACTIVE               "active="           /* activate/deactivate */
 #define PARAM_NETWORK              "network="          /* bind on nid */
 #define PARAM_ID_UPCALL		"identity_upcall="  /* identity upcall */
+#define PARAM_ROOTSQUASH	   "root_squash="      /* root squash */
+#define PARAM_NOSQUASHNIDS	   "nosquash_nids="    /* no squash nids */
 
 /* Prefixes for parameters handled by obd's proc methods (XXX_process_config) */
 #define PARAM_OST		"ost."
