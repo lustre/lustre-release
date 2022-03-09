0882c73b1d367ec133c2514d875e439fc9c14bc9
Arshad Hussain <arshad.hussain@aeoncomputing.com>
2022-03-09 12:57:25 +0530

    LU-15626 tests: Fix "error" reported by shellcheck for sanity-dom
    This patch fixes "error" issues reported by shellcheck
    for file lustre/tests/sanity-dom.sh
    
    Test-Parameters: trivial testlist=sanity-dom
    Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Change-Id: I185622dcb30abbc43a2118cbfaa9c643f8ce52bc
    Reviewed-on: https://review.whamcloud.com/46796
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanity-dom.sh
+++ lustre/tests/sanity-dom.sh
@@ -10,7 +10,7 @@ ONLY=${ONLY:-"$*"}
 
 LUSTRE=${LUSTRE:-$(dirname $0)/..}
 . $LUSTRE/tests/test-framework.sh
-init_test_env $@
+init_test_env "$@"
 init_logging
 
 # bug number for skipped test:
@@ -222,4 +222,4 @@ complete $SECONDS
 check_and_cleanup_lustre
 declare -a logs=($ONLY)
 logs=("${logs[@]/#/$TMP/}")
-exit_status "$(echo ${logs[@]/%/.log})"
+exit_status "$(echo "${logs[@]/%/.log}")"
