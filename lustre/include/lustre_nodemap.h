lustre/lustre-release

commit 8a770616a5ad21360ecba63c3643cadd245a2a50

LU-14797 sec: add projid to nodemap
Add the ability to create id maps of a new type, projid. This also
requires adding a new value to map_mode, projid_only. Finally, a new
property named squash_projid is used to map all project ID to a
default one.
Update lctl man pages to mention these additions.
Update sanity-sec test_12 and test_15 to exercise projid mapping and
squash_projid property.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I63eba8b0d33feaa7ece8c1788cb587fcb330357a
Reviewed-on: https://review.whamcloud.com/44108
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_nodemap.h
@@ -44,6 +44,7 @@
 enum nodemap_id_type {
 	NODEMAP_UID,
 	NODEMAP_GID,
+	NODEMAP_PROJID,
 };
 
 enum nodemap_tree_type {
@@ -52,9 +53,13 @@ enum nodemap_tree_type {
 };
 
 enum nodemap_mapping_modes {
-	NODEMAP_MAP_BOTH,
-	NODEMAP_MAP_UID_ONLY,
-	NODEMAP_MAP_GID_ONLY,
+	NODEMAP_MAP_UID		= 0x01,
+	NODEMAP_MAP_GID		= 0x02,
+	NODEMAP_MAP_BOTH	= 0x03, /* for compatibility */
+	NODEMAP_MAP_PROJID	= 0x04,
+	NODEMAP_MAP_ALL		= NODEMAP_MAP_UID |
+				  NODEMAP_MAP_GID |
+				  NODEMAP_MAP_PROJID,
 };
 
 struct nodemap_pde {
@@ -75,10 +80,11 @@ struct lu_nodemap {
 	bool			 nmf_trust_client_ids:1,
 				 nmf_deny_unknown:1,
 				 nmf_allow_root_access:1,
-				 nmf_map_uid_only:1,
-				 nmf_map_gid_only:1,
 				 nmf_enable_audit:1,
 				 nmf_forbid_encryption:1;
+	/* bitmap for mapping type */
+	enum nodemap_mapping_modes
+				nmf_map_mode;
 	/* unique ID set by MGS */
 	unsigned int		 nm_id;
 	/* nodemap ref counter */
@@ -87,6 +93,8 @@ struct lu_nodemap {
 	uid_t			 nm_squash_uid;
 	/* GID to squash unmapped GIDs */
 	gid_t			 nm_squash_gid;
+	/* PROJID to squash unmapped PROJIDs */
+	projid_t		 nm_squash_projid;
 	/* NID range list */
 	struct list_head	 nm_ranges;
 	/* lock for idmap red/black trees */
@@ -99,6 +107,10 @@ struct lu_nodemap {
 	struct rb_root		 nm_fs_to_client_gidmap;
 	/* GID map keyed by remote UID */
 	struct rb_root		 nm_client_to_fs_gidmap;
+	/* PROJID map keyed by local UID */
+	struct rb_root		 nm_fs_to_client_projidmap;
+	/* PROJID map keyed by remote UID */
+	struct rb_root		 nm_client_to_fs_projidmap;
 	/* attached client members of this nodemap */
 	struct mutex		 nm_member_list_lock;
 	struct list_head	 nm_member_list;
@@ -136,9 +148,11 @@ int nodemap_del_range(const char *name, const lnet_nid_t nid[2]);
 int nodemap_set_allow_root(const char *name, bool allow_root);
 int nodemap_set_trust_client_ids(const char *name, bool trust_client_ids);
 int nodemap_set_deny_unknown(const char *name, bool deny_unknown);
-int nodemap_set_mapping_mode(const char *name, enum nodemap_mapping_modes mode);
+int nodemap_set_mapping_mode(const char *name,
+			     enum nodemap_mapping_modes map_mode);
 int nodemap_set_squash_uid(const char *name, uid_t uid);
 int nodemap_set_squash_gid(const char *name, gid_t gid);
+int nodemap_set_squash_projid(const char *name, projid_t projid);
 int nodemap_set_audit_mode(const char *name, bool enable_audit);
 int nodemap_set_forbid_encryption(const char *name, bool forbid_encryption);
 bool nodemap_can_setquota(const struct lu_nodemap *nodemap);

lustre/ptlrpc/nodemap_storage.c
@@ -72,10 +72,11 @@ enum nm_flag_shifts {
 	NM_FL_ALLOW_ROOT_ACCESS = 0x1,
 	NM_FL_TRUST_CLIENT_IDS = 0x2,
 	NM_FL_DENY_UNKNOWN = 0x4,
-	NM_FL_MAP_UID_ONLY = 0x8,
-	NM_FL_MAP_GID_ONLY = 0x10,
+	NM_FL_MAP_UID = 0x8,
+	NM_FL_MAP_GID = 0x10,
 	NM_FL_ENABLE_AUDIT = 0x20,
 	NM_FL_FORBID_ENCRYPT = 0x40,
+	NM_FL_MAP_PROJID = 0x80,
 };
 
 static void nodemap_cluster_key_init(struct nodemap_key *nk, unsigned int nm_id)
@@ -93,6 +94,7 @@ static void nodemap_cluster_rec_init(union nodemap_rec *nr,
 	strncpy(nr->ncr.ncr_name, nodemap->nm_name, sizeof(nr->ncr.ncr_name));
 	nr->ncr.ncr_squash_uid = cpu_to_le32(nodemap->nm_squash_uid);
 	nr->ncr.ncr_squash_gid = cpu_to_le32(nodemap->nm_squash_gid);
+	nr->ncr.ncr_squash_projid = cpu_to_le32(nodemap->nm_squash_projid);
 	nr->ncr.ncr_flags = cpu_to_le32(
 		(nodemap->nmf_trust_client_ids ?
 			NM_FL_TRUST_CLIENT_IDS : 0) |
@@ -100,10 +102,12 @@ static void nodemap_cluster_rec_init(union nodemap_rec *nr,
 			NM_FL_ALLOW_ROOT_ACCESS : 0) |
 		(nodemap->nmf_deny_unknown ?
 			NM_FL_DENY_UNKNOWN : 0) |
-		(nodemap->nmf_map_uid_only ?
-			NM_FL_MAP_UID_ONLY : 0) |
-		(nodemap->nmf_map_gid_only ?
-			NM_FL_MAP_GID_ONLY : 0) |
+		(nodemap->nmf_map_mode & NODEMAP_MAP_UID ?
+			NM_FL_MAP_UID : 0) |
+		(nodemap->nmf_map_mode & NODEMAP_MAP_GID ?
+			NM_FL_MAP_GID : 0) |
+		(nodemap->nmf_map_mode & NODEMAP_MAP_PROJID ?
+			NM_FL_MAP_PROJID : 0) |
 		(nodemap->nmf_enable_audit ?
 			NM_FL_ENABLE_AUDIT : 0) |
 		(nodemap->nmf_forbid_encryption ?
@@ -118,8 +122,12 @@ static void nodemap_idmap_key_init(struct nodemap_key *nk, unsigned int nm_id,
 
 	if (id_type == NODEMAP_UID)
 		idx_type = NODEMAP_UIDMAP_IDX;
-	else
+	else if (id_type == NODEMAP_GID)
 		idx_type = NODEMAP_GIDMAP_IDX;
+	else if (id_type == NODEMAP_PROJID)
+		idx_type = NODEMAP_PROJIDMAP_IDX;
+	else
+		idx_type = NODEMAP_EMPTY_IDX;
 
 	nk->nk_nodemap_id = cpu_to_le32(nm_idx_set_type(nm_id, idx_type));
 	nk->nk_id_client = cpu_to_le32(id_client);
@@ -476,6 +484,17 @@ int nodemap_idx_nodemap_del(const struct lu_nodemap *nodemap)
 			rc = rc2;
 	}
 
+	root = nodemap->nm_client_to_fs_projidmap;
+	nm_rbtree_postorder_for_each_entry_safe(idmap, temp, &root,
+						id_client_to_fs) {
+		nodemap_idmap_key_init(&nk, nodemap->nm_id, NODEMAP_PROJID,
+				       idmap->id_client);
+		rc2 = nodemap_idx_delete(&env, nodemap_mgs_ncf->ncf_obj,
+					 &nk, NULL);
+		if (rc2 < 0)
+			rc = rc2;
+	}
+
 	list_for_each_entry_safe(range, range_temp, &nodemap->nm_ranges,
 				 rn_list) {
 		nodemap_range_key_init(&nk, nodemap->nm_id, range->rn_id);
@@ -685,7 +704,7 @@ static int nodemap_process_keyrec(struct nodemap_config *config,
 
 	/* find the correct nodemap in the load list */
 	if (type == NODEMAP_RANGE_IDX || type == NODEMAP_UIDMAP_IDX ||
-	    type == NODEMAP_GIDMAP_IDX) {
+	    type == NODEMAP_GIDMAP_IDX || type == NODEMAP_PROJIDMAP_IDX) {
 		struct lu_nodemap *tmp = NULL;
 
 		nodemap = *recent_nodemap;
@@ -746,6 +765,8 @@ static int nodemap_process_keyrec(struct nodemap_config *config,
 				le32_to_cpu(rec->ncr.ncr_squash_uid);
 		nodemap->nm_squash_gid =
 				le32_to_cpu(rec->ncr.ncr_squash_gid);
+		nodemap->nm_squash_projid =
+			le32_to_cpu(rec->ncr.ncr_squash_projid);
 
 		flags = le32_to_cpu(rec->ncr.ncr_flags);
 		nodemap->nmf_allow_root_access =
@@ -754,10 +775,12 @@ static int nodemap_process_keyrec(struct nodemap_config *config,
 					flags & NM_FL_TRUST_CLIENT_IDS;
 		nodemap->nmf_deny_unknown =
 					flags & NM_FL_DENY_UNKNOWN;
-		nodemap->nmf_map_uid_only =
-					flags & NM_FL_MAP_UID_ONLY;
-		nodemap->nmf_map_gid_only =
-					flags & NM_FL_MAP_GID_ONLY;
+		nodemap->nmf_map_mode = (flags & NM_FL_MAP_UID ?
+					 NODEMAP_MAP_UID : 0) |
+					(flags & NM_FL_MAP_GID ?
+					 NODEMAP_MAP_GID : 0) |
+					(flags & NM_FL_MAP_PROJID ?
+					 NODEMAP_MAP_PROJID : 0);
 		nodemap->nmf_enable_audit =
 					flags & NM_FL_ENABLE_AUDIT;
 		nodemap->nmf_forbid_encryption =
@@ -795,13 +818,18 @@ static int nodemap_process_keyrec(struct nodemap_config *config,
 		break;
 	case NODEMAP_UIDMAP_IDX:
 	case NODEMAP_GIDMAP_IDX:
+	case NODEMAP_PROJIDMAP_IDX:
 		map[0] = le32_to_cpu(key->nk_id_client);
 		map[1] = le32_to_cpu(rec->nir.nir_id_fs);
 
 		if (type == NODEMAP_UIDMAP_IDX)
 			id_type = NODEMAP_UID;
-		else
+		else if (type == NODEMAP_GIDMAP_IDX)
 			id_type = NODEMAP_GID;
+		else if (type == NODEMAP_PROJIDMAP_IDX)
+			id_type = NODEMAP_PROJID;
+		else
+			GOTO(out, rc = -EINVAL);
 
 		rc = nodemap_add_idmap_helper(nodemap, id_type, map);
 		if (rc != 0)
@@ -1049,6 +1077,18 @@ struct dt_object *nodemap_save_config_cache(const struct lu_env *env,
 		nm_rbtree_postorder_for_each_entry_safe(idmap, id_tmp, &root,
 							id_client_to_fs) {
 			nodemap_idmap_key_init(&nk, nodemap->nm_id, NODEMAP_GID,
+					       idmap->id_client);
+			nodemap_idmap_rec_init(&nr, idmap->id_fs);
+			rc2 = nodemap_idx_insert(env, o, &nk, &nr);
+			if (rc2 < 0)
+				rc = rc2;
+		}
+
+		root = nodemap->nm_client_to_fs_projidmap;
+		nm_rbtree_postorder_for_each_entry_safe(idmap, id_tmp, &root,
+							id_client_to_fs) {
+			nodemap_idmap_key_init(&nk, nodemap->nm_id,
+					       NODEMAP_PROJID,
 					       idmap->id_client);
 			nodemap_idmap_rec_init(&nr, idmap->id_fs);
 			rc2 = nodemap_idx_insert(env, o, &nk, &nr);

lustre/doc/lctl-nodemap-add-idmap.8
@@ -1,23 +1,23 @@
 .TH LCTL-NODEMAP_ADD_IDMAP 8 "2015-01-20" Lustre "configuration utilities"
 .SH NAME
-lctl-nodemap_add_idmap \- define a range of NIDs for a nodemap
+lctl-nodemap_add_idmap \- define ID mappings for a nodemap
 .SH SYNOPSIS
 .br
-.B lctl nodemap_add_idmap <--name name> <--idtype {uid|gid}>
-<--idmap clientid:fsid>
+.B lctl nodemap_add_idmap <--name name> <--idtype {uid|gid|projid}>
+.B <--idmap clientid:fsid>
 .br
 .SH DESCRIPTION
-.B nodemap_add_idmap adds an identity mapping to a nodemap. Clients that are
-members of the given nodemap will have the identities of their users mapped
-accordingly.
+.B nodemap_add_idmap
+adds an identity mapping to a nodemap. Clients that are members of the given
+nodemap will have the identities of their users mapped accordingly.
 
 .SH OPTIONS
 .I name
 is the name of the nodemap that this idmap should be added to.
 
 .I idtype
-is either "uid" or "gid" depending on if it is a user ID or group ID that is to
-be mapped.
+is either "uid" or "gid" or "projid" depending on if it is a user ID or group
+ID or project ID that is to be mapped.
 
 .I idmap
 is the identity to map, and what it should be mapped to. The first digit is the
@@ -28,6 +28,7 @@ that it should map to on the Lustre filesystem.
 .nf
 # lctl nodemap_add_idmap --name remotesite --idtype uid --idmap 2001:1001
 # lctl nodemap_add_idmap --name remotesite --idtype gid --idmap 2002:1002
+# lctl nodemap_add_idmap --name remotesite --idtype projid --idmap 33:1
 .fi
 
 .SH AVAILABILITY

lustre/doc/lctl-nodemap-del-idmap.8
@@ -3,22 +3,21 @@
 lctl-nodemap_del_idmap \- delete an existing idmap from a nodemap
 .SH SYNOPSIS
 .br
-.B lctl nodemap_del_idmap <--name name> <--idtype [uid|gid]>
-<--idmap clientid:fsid>
+.B lctl nodemap_del_idmap <--name name> <--idtype {uid|gid|projid}>
+.B <--idmap clientid:fsid>
 .br
 .SH DESCRIPTION
 .B nodemap_del_idmap
-deletes an idmap from a nodemap. Users or groups in the nodemap with that ID
-will be squashed, if the trusted flag is not enabled.
+deletes an idmap from a nodemap. Users or groups or projects in the nodemap with
+that ID will be squashed, if the trusted flag is not enabled.
 
 .SH OPTIONS
 .I name
 is the name of the nodemap that this idmap should be deleted from.
 
-
 .I idtype
-is either "uid" or "gid" depending on if it is a user or group mapping that is
-to be removed.
+is either "uid" or "gid" or "projid" depending on if it is a user or group or
+project ID mapping that is to be removed.
 
 .I idmap
 is the identity map to delete.
@@ -27,6 +26,7 @@ is the identity map to delete.
 .nf
 # lctl nodemap_del_idmap --name remotesite --idtype uid --idmap 2001:1001
 # lctl nodemap_del_idmap --name remotesite --idtype gid --idmap 2002:1002
+# lctl nodemap_del_idmap --name remotesite --idtype projid --idmap 33:1
 .fi
 
 .SH AVAILABILITY

lustre/doc/lctl-nodemap-modify.8
@@ -14,9 +14,9 @@ modifies a property of the given nodemap.
 .I nodemap_name
 is the name of the nodemap to modify
 
-
 .I property_name
 is one of the following properties:
+.RS 0.3i
 .PP
 admin
 .RS 4
@@ -42,6 +42,11 @@ squash_gid
 Defaults to 99. The group ID that unknown groups (if not trusted) and root (if not admin) should be mapped to.
 .RE
 .PP
+squash_projid
+.RS 4
+Defaults to 99. The project ID that unknown projects (if not trusted) should be mapped to.
+.RE
+.PP
 deny_unknown
 .RS 4
 Defaults to off. If set to on then unknown (squashed) users will be denied
@@ -62,7 +67,15 @@ activated or not.
 The reason not to record file system events from given clients is to prevent
 some nodes (e.g. backup, HSM agent nodes) from flooding the Changelogs.
 .RE
+.PP
+map_mode
+.RS 4
+Defaults to all, which means the nodemap maps UIDs, GIDs, and PROJIDs.
+Other possible values (multiple can be specified, comma separated) are uid to
+map UIDs, gid to map GIDs, both to map UIDs and GIDs, and projid to map PROJIDs.
+.RE
 
+.RE
 .I value
 is the value to set for the property. Should be 0 or 1 for admin and trusted.
 
@@ -70,6 +83,7 @@ is the value to set for the property. Should be 0 or 1 for admin and trusted.
 .nf
 # lctl nodemap_modify --name remotesite --property trusted --value 1
 # lctl nodemap_modify --name remotesite --property admin --value 1
+# lctl nodemap_modify --name remotesite --property map_mode --value uid_only
 # lctl nodemap_modify --name otherremotesite --property squash_uid --value 101
 .fi
 

lustre/include/uapi/linux/lustre/lustre_cfg.h
@@ -118,6 +118,8 @@ enum lcfg_command_type {
 						 *  id mapping
 						 */
 	LCFG_NODEMAP_ADMIN	  = 0x00ce049, /**< allow cluster to use id 0 */
+	LCFG_NODEMAP_ADD_PROJIDMAP	  = 0x00ce04a, /**< add a projidmap */
+	LCFG_NODEMAP_DEL_PROJIDMAP	  = 0x00ce04b, /**< delete a projidmap */
 	LCFG_NODEMAP_TRUSTED	  = 0x00ce050, /**< trust a clusters ids */
 	LCFG_NODEMAP_SQUASH_UID	  = 0x00ce051, /**< default map uid */
 	LCFG_NODEMAP_SQUASH_GID	  = 0x00ce052, /**< default map gid */
@@ -137,6 +139,7 @@ enum lcfg_command_type {
 	LCFG_NODEMAP_AUDIT_MODE	  = 0x00ce05a, /**< set the audit mode */
 	LCFG_NODEMAP_SET_SEPOL	  = 0x00ce05b, /**< set SELinux policy */
 	LCFG_NODEMAP_FORBID_ENCRYPT	= 0x00ce05c, /**< forbid encryption */
+	LCFG_NODEMAP_SQUASH_PROJID	= 0x00ce05d, /**< default map projid */
 };
 
 struct lustre_cfg_bufs {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3586,7 +3586,7 @@ struct nodemap_cluster_rec {
 	char	ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
 	__u8	ncr_flags;
 	__u16	ncr_padding1;
-	__u32	ncr_padding2;
+	__u32	ncr_squash_projid;
 	__u32	ncr_squash_uid;
 	__u32	ncr_squash_gid;
 };

lustre/mgs/mgs_handler.c
@@ -814,8 +814,12 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 		nid = libcfs_str2nid(nidstr);
 		if (strcmp(idtype_str, "uid") == 0)
 			idtype = NODEMAP_UID;
-		else
+		else if (strcmp(idtype_str, "gid") == 0)
 			idtype = NODEMAP_GID;
+		else if (strcmp(idtype_str, "projid") == 0)
+			idtype = NODEMAP_PROJID;
+		else
+			GOTO(out_lcfg, rc = -EINVAL);
 
 		rc = kstrtoul(client_idstr, 10, &client_id);
 		if (rc != 0)
@@ -839,6 +843,8 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 	case LCFG_NODEMAP_DEL_UIDMAP:
 	case LCFG_NODEMAP_ADD_GIDMAP:
 	case LCFG_NODEMAP_DEL_GIDMAP:
+	case LCFG_NODEMAP_ADD_PROJIDMAP:
+	case LCFG_NODEMAP_DEL_PROJIDMAP:
 	case LCFG_NODEMAP_SET_FILESET:
 	case LCFG_NODEMAP_SET_SEPOL:
 		if (lcfg->lcfg_bufcount != 3)
@@ -852,6 +858,7 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 	case LCFG_NODEMAP_DENY_UNKNOWN:
 	case LCFG_NODEMAP_SQUASH_UID:
 	case LCFG_NODEMAP_SQUASH_GID:
+	case LCFG_NODEMAP_SQUASH_PROJID:
 	case LCFG_NODEMAP_MAP_MODE:
 	case LCFG_NODEMAP_AUDIT_MODE:
 	case LCFG_NODEMAP_FORBID_ENCRYPT:

commit 4d93fd79e8b05de1065fc60abce9b0d052a92627

LU-15661 nodemap: fix map mode value for 'both'
The patch that introduced the ability to map project IDs with
nodemap changed the value used for the "map both uid and gid"
case, from 0 to 3.
This poses a problem in case of upgrade from a previous Lustre
version, so re-introduce the value 0 as NODEMAP_MAP_BOTH_LEGACY.

Change-Id: I1f605de9c97faff32411da5052e8782a60645767
Fixes: 8a770616a5 ("LU-14797 sec: add projid to nodemap")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-on: https://review.whamcloud.com/46870
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_nodemap.h
@@ -53,6 +53,7 @@ enum nodemap_tree_type {
 };
 
 enum nodemap_mapping_modes {
+	NODEMAP_MAP_BOTH_LEGACY	= 0x0,  /* for compatibility */
 	NODEMAP_MAP_UID		= 0x01,
 	NODEMAP_MAP_GID		= 0x02,
 	NODEMAP_MAP_BOTH	= 0x03, /* for compatibility */

lustre/ptlrpc/nodemap_storage.c
@@ -781,6 +781,8 @@ static int nodemap_process_keyrec(struct nodemap_config *config,
 					 NODEMAP_MAP_GID : 0) |
 					(flags & NM_FL_MAP_PROJID ?
 					 NODEMAP_MAP_PROJID : 0);
+		if (nodemap->nmf_map_mode == NODEMAP_MAP_BOTH_LEGACY)
+			nodemap->nmf_map_mode = NODEMAP_MAP_BOTH;
 		nodemap->nmf_enable_audit =
 					flags & NM_FL_ENABLE_AUDIT;
 		nodemap->nmf_forbid_encryption =