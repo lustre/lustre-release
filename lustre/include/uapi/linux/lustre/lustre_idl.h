lustre/lustre-release

commit 2f496148c31dd435681e595129834cecdaec82dc

LU-15551 ofd: Return EOPNOTSUPP instead of EPROTO
Modify server to return -EOPNOTSUPP instead of
-EPROTO for unsupported fallocate modes

Test-Parameters: serverversion=2.14.0 testlist=sanity env=ONLY=150
Test-Parameters: serverversion=2.14.0 testlist=sanity-flr env=ONLY=50
Test-Parameters: serverversion=2.14.0 testlist=ost-pools env=ONLY="29 31"
Test-Parameters: serverversion=2.14.0 testlist=sanity-benchmark env=ONLY=fsx
Test-Parameters: serverversion=2.14.0 testlist=sanity-dom env=ONLY=fsx
Test-Parameters: serverversion=2.14.0 testlist=sanityn env=ONLY=16
Fixes: 7462e8cad730 ("LU-14160 fallocate: Add punch mode to fallocate")
Signed-off-by: arshad.hussain@aeoncomputing.com
Change-Id: Id203c0b9abbdd674af33f1f78e81ae7fe105e90f
Reviewed-on: https://review.whamcloud.com/46516
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_dev.c
@@ -1987,10 +1987,12 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 	/*
 	 * fallocate start and end are passed in o_size, o_blocks
 	 * on the wire.
+	 * Return -EOPNOTSUPP to also handle older clients not
+	 * supporting newer server modes
 	 */
 	if ((oa->o_valid & (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)) !=
 	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS))
-		RETURN(err_serious(-EPROTO));
+		RETURN(-EOPNOTSUPP);
 
 	start = oa->o_size;
 	end = oa->o_blocks;

lustre/tests/sanity-flr.sh
@@ -2720,8 +2720,8 @@ test_50c() {
 
 	if [[ "$FSTYPE" == "ldiskfs" ]]; then
 		# ZFS does not support fallocate for now
-		fallocate -p -o 1MiB -l 1MiB $tf ||
-			error "punch hole in $tf failed"
+		out=$(fallocate -p -o 1MiB -l 1MiB $tf 2>&1) ||
+			skip_eopnotsupp "$out|punch hole in $tf failed"
 		verify_flr_state $tf "wp"
 	fi
 
@@ -2763,7 +2763,7 @@ test_50d() {
 	elif [[ ! $prt =~ "unsupported" ]]; then
 		error "punch hole in $file failed: $prt"
 	else
-		skip "Fallocate punch is not supported"
+		skip "Fallocate punch is not supported: $prt"
 	fi
 
 	echo " ** verify sparseness"

lustre/tests/sanity.sh
@@ -14647,7 +14647,7 @@ test_150b() {
 
 	touch $DIR/$tfile
 	stack_trap "rm -f $DIR/$tfile; wait_delete_completed"
-	check_fallocate $DIR/$tfile || error "fallocate failed"
+	check_fallocate $DIR/$tfile || skip_eopnotsupp "fallocate failed"
 }
 run_test 150b "Verify fallocate (prealloc) functionality"
 
@@ -14697,8 +14697,7 @@ test_150c() {
 
 	$LFS setstripe -E1M $striping -E16M -c3 -Eeof -c 4 $DIR/$tfile ||
 		error "Create $DIR/$tfile failed"
-	fallocate -l $((1048576 * 512)) $DIR/$tfile ||
-			error "fallocate failed"
+	fallocate -l $((1048576 * 512)) $DIR/$tfile || error "fallocate failed"
 	bytes=$(($(stat -c '%b * %B' $DIR/$tfile)))
 	want=$((512 * 1048576))
 
@@ -14802,8 +14801,8 @@ test_150f() {
 		error "dd failed for bs 4096 and count 5"
 
 	# Call fallocate with punch range which is within the file range
-	fallocate -p --offset 4096 -l $length $DIR/$tfile ||
-		error "fallocate failed: offset 4096 and length $length"
+	out=$(fallocate -p --offset 4096 -l $length $DIR/$tfile 2>&1) ||
+		skip_eopnotsupp "$out|fallocate: offset 4096 and length $length"
 	# client must see changes immediately after fallocate
 	size=$(stat -c '%s' $DIR/$tfile)
 	blocks=$(stat -c '%b' $DIR/$tfile)
@@ -14833,8 +14832,8 @@ test_150f() {
 	want_blocks_after=40  # 512 sized blocks
 
 	# Punch overlaps two blocks and less than blocksize
-	fallocate -p --offset 4000 -l 3000 $DIR/$tfile ||
-		error "fallocate failed: offset 4000 length 3000"
+	out=$(fallocate -p --offset 4000 -l 3000 $DIR/$tfile 2>&1) ||
+		skip_eopnotsupp "$out|fallocate: offset 4000 length 3000"
 	size=$(stat -c '%s' $DIR/$tfile)
 	blocks=$(stat -c '%b' $DIR/$tfile)
 
@@ -14918,8 +14917,8 @@ test_150g() {
 	# Call fallocate to punch all except 2 blocks. We leave the
 	# first and the last block
 	echo "fallocate -p --offset $BS -l $punch_size $DIR/$tfile"
-	fallocate -p --offset $BS -l $punch_size $DIR/$tfile ||
-		error "fallocate failed: offset $BS length $punch_size"
+	out=$(fallocate -p --offset $BS -l $punch_size $DIR/$tfile 2>&1) ||
+		skip_eopnotsupp "$out|fallocate: offset $BS length $punch_size"
 
 	size_after=$(stat -c '%s' $DIR/$tfile)
 	blocks_after=$(stat -c '%b' $DIR/$tfile)

lustre/tests/test-framework.sh
@@ -6451,6 +6451,19 @@ skip() {
 	exit 0
 }
 
+#
+# For interop testing treate EOPNOTSUPP as success
+# and skip
+#
+skip_eopnotsupp() {
+	local retstr=$@
+
+	echo $retstr | awk -F'|' '{print $1}' |
+		grep -E unsupported\|"(Operation not supported)"
+	(( $? == 0 )) || error "$retstr"
+	skip $retstr
+}
+
 build_test_filter() {
 	EXCEPT="$EXCEPT $(testslist_filter)"
 

commit 79053592966792a21912390b9db09b1ab1d61e2a

LU-15748 osc: fallocate interop for 2.14 clients
fallocate() start and end are passed in o_size and o_blocks
on the wire.  Clients 2.15.0 and newer should always set
the OBD_MD_FLSIZE and OBD_MD_FLBLOCKS valid flags, but some
older client versions did not.  We permit older clients to
not set these flags, checking their version by proxy using
the missing OBD_CONNECT_TRUNCLOCK to imply 2.14.0 or older.

Test-Parameters: serverversion=2.14.0 testlist=sanity env=SANITY_EXCEPT="64h 103e"
Fixes: 2f496148c31d ("LU-15551 Return EOPNOTSUPP instead of EPROTO")
Fixes: 163870abfb7c ("LU-14382 mdt: implement fallocate in MDC/MDT")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I1ea47854f40d54297bceb03ad32b24737efa4ae7
Reviewed-on: https://review.whamcloud.com/47098
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -753,8 +753,10 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT_ACL                  0x80ULL /*access control lists */
 #define OBD_CONNECT_XATTR               0x100ULL /*client use extended attr */
 #define OBD_CONNECT_LARGE_ACL		0x200ULL /* more than 32 ACL entries */
-/* was OBD_CONNECT_TRUNCLOCK           0x400ULL *locks on server for punch */
-#define OBD_CONNECT_TRANSNO             0x800ULL /*replay sends init transno */
+/* was OBD_CONNECT_TRUNCLOCK		0x400ULL *locks on server for punch */
+/* temporary reuse until 2.21.53 to indicate pre-2.15 client, see LU-15478 */
+#define OBD_CONNECT_OLD_FALLOC		0x400ULL /* missing o_valid flags */
+#define OBD_CONNECT_TRANSNO		0x800ULL /*replay sends init transno */
 #define OBD_CONNECT_IBITS	       0x1000ULL /* not checked in 2.11+ */
 #define OBD_CONNECT_BARRIER	       0x2000ULL /* write barrier. Resevered to
 						  * avoid use on client.

lustre/mdt/mdt_io.c
@@ -950,15 +950,33 @@ int mdt_fallocate_hdl(struct tgt_session_info *tsi)
 		RETURN(err_serious(-ENOMEM));
 
 	/*
+	 * fallocate() start and end are passed in o_size and o_blocks
+	 * on the wire.  Clients 2.15.0 and newer should always set
+	 * the OBD_MD_FLSIZE and OBD_MD_FLBLOCKS valid flags, but some
+	 * older client versions did not.  We permit older clients to
+	 * not set these flags, checking their version by proxy using
+	 * the lack of OBD_CONNECT_TRUNCLOCK to imply 2.14.0 and older.
+	 *
+	 * Return -EOPNOTSUPP to also work with older clients not
+	 * supporting newer server modes.
+	 *
 	 * fallocate start and end are passed in o_size, o_blocks
 	 * on the wire.
 	 */
 	if ((oa->o_valid & (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)) !=
-	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS))
-		RETURN(err_serious(-EPROTO));
+	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 21, 53, 0)
+	    && (tgt_conn_flags(tsi) & OBD_CONNECT_OLD_FALLOC)
+#endif
+	    )
+		RETURN(-EOPNOTSUPP);
 
 	start = oa->o_size;
 	end = oa->o_blocks;
+	/* client should already limit len >= 0 */
+	if (start >= end)
+		RETURN(-EINVAL);
+
 	mode = oa->o_falloc_mode;
 
 	CDEBUG(D_INODE,

lustre/ofd/ofd_dev.c
@@ -1985,17 +1985,30 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 		RETURN(err_serious(-ENOMEM));
 
 	/*
-	 * fallocate start and end are passed in o_size, o_blocks
-	 * on the wire.
-	 * Return -EOPNOTSUPP to also handle older clients not
-	 * supporting newer server modes
+	 * fallocate() start and end are passed in o_size and o_blocks
+	 * on the wire.  Clients 2.15.0 and newer should always set
+	 * the OBD_MD_FLSIZE and OBD_MD_FLBLOCKS valid flags, but some
+	 * older client versions did not.  We permit older clients to
+	 * not set these flags, checking their version by proxy using
+	 * the lack of OBD_CONNECT_TRUNCLOCK to imply 2.14.0 and older.
+	 *
+	 * Return -EOPNOTSUPP to also work with older clients not
+	 * supporting newer server modes.
 	 */
 	if ((oa->o_valid & (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)) !=
-	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS))
+	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 21, 53, 0)
+	    && (tgt_conn_flags(tsi) & OBD_CONNECT_OLD_FALLOC)
+#endif
+	    )
 		RETURN(-EOPNOTSUPP);
 
 	start = oa->o_size;
 	end = oa->o_blocks;
+	/* client should already limit len >= 0 */
+	if (start >= end)
+		RETURN(-EINVAL);
+
 	mode = oa->o_falloc_mode;
 	/*
 	 * mode == 0 (which is standard prealloc) and PUNCH is supported