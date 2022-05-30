lustre/lustre-release

commit b384ea39e593cda1ac4d6fb8b955d0c7d1a1f67b

LU-14480 pool: wrong usage with ost list
When the OST list is given on setstripe, it should have a priority
over the pool. Also, we check only for the 1st OST if it is in the
pool at the creation time, what worked well in past with -c and
works even with -C, but not with the OST list when some of the OSTs
are out of the pool.

Make the --pool and --ost options mutualy exclusive.
Drop the pool inheritance if the OST list is given.

Signed-off-by: Vitaly Fertman <c17818@cray.com>
Change-Id: I94a7fe97391f1185392f986f78ab1a372238972a
Reviewed-on: https://es-gerrit.dev.cray.com/158198
HPE-bug-id: LUS-9579
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>
Reviewed-on: https://review.whamcloud.com/41815
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andriy Skulysh <askulysh@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_qos.c
@@ -2242,9 +2242,13 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 		}
 
 		pool_name = NULL;
+		if (def_pool[0] != '\0')
+			pool_name = def_pool;
+
 		if (v1->lmm_magic == LOV_USER_MAGIC_V3 ||
 		    v1->lmm_magic == LOV_USER_MAGIC_SPECIFIC) {
 			v3 = (struct lov_user_md_v3 *)v1;
+
 			if (v3->lmm_pool_name[0] != '\0')
 				pool_name = v3->lmm_pool_name;
 
@@ -2252,11 +2256,10 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 				rc = lod_comp_copy_ost_lists(lod_comp, v3);
 				if (rc)
 					GOTO(free_comp, rc);
+
+				pool_name = NULL;
 			}
 		}
-
-		if (pool_name == NULL && def_pool[0] != '\0')
-			pool_name = def_pool;
 
 		if (v1->lmm_pattern == 0)
 			v1->lmm_pattern = LOV_PATTERN_RAID0;

lustre/tests/sanity-flr.sh
@@ -378,7 +378,6 @@ test_0b() {
 	verify_comp_attr stripe-size $tf ${ids[1]} 4194304
 	verify_comp_attr stripe-count $tf ${ids[1]} 2
 	verify_comp_attr stripe-index $tf ${ids[1]} 2
-	verify_comp_attr pool $tf ${ids[1]} flash
 
 	# verify component ${ids[2]}
 	verify_comp_attr stripe-size $tf ${ids[2]} 16777216
@@ -450,7 +449,6 @@ test_0c() {
 		verify_comp_attr_with_default stripe-size $tf ${ids[$i]}
 		verify_comp_attr stripe-count $tf ${ids[$i]} 2
 		verify_comp_attr stripe-index $tf ${ids[$i]} 1
-		verify_comp_attr pool $tf ${ids[$i]} flash
 		verify_comp_extent $tf ${ids[$i]} 0 4194304
 	done
 
@@ -566,13 +564,11 @@ test_0e() {
 	verify_comp_attr stripe-size $tf ${ids[0]} 33554432
 	verify_comp_attr stripe-count $tf ${ids[0]} 3
 	verify_comp_attr stripe-index $tf ${ids[0]} 1
-	verify_comp_attr pool $tf ${ids[0]} ssd
 
 	# verify component ${ids[1]}
 	verify_comp_attr stripe-size $tf ${ids[1]} 4194304
 	verify_comp_attr stripe-count $tf ${ids[1]} 2
 	verify_comp_attr stripe-index $tf ${ids[1]} 2
-	verify_comp_attr pool $tf ${ids[1]} flash
 
 	# verify component ${ids[2]}
 	verify_comp_attr stripe-size $tf ${ids[2]} 16777216
@@ -644,7 +640,6 @@ test_0f() {
 		verify_comp_attr_with_default stripe-size $tf ${ids[$i]}
 		verify_comp_attr stripe-count $tf ${ids[$i]} 2
 		verify_comp_attr stripe-index $tf ${ids[$i]} 1
-		verify_comp_attr pool $tf ${ids[$i]} flash
 		verify_comp_extent $tf ${ids[$i]} 0 4194304
 	done
 

commit 06dd5a4638dd36640b146d4388c09a322873760b

LU-15658 lod: ost list and pool name conflict
If the OST list is given on setstripe with the -o option, the pool is
unconditionally dropped even if all the OSTs are in. Let the pool stay
in this case.

Also, if the start index given on setstripe with the -i option is out
of the pool, make it similar to the -o option - drop the pool.

HPE-bug-id: LUS-10868
Fixes: b384ea39e5 ("LU-14480 pool: wrong usage with ost list")
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I718c237e273689048eb74044eea73de6c212395e
Reviewed-on: https://review.whamcloud.com/46967
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_qos.c
@@ -2093,6 +2093,59 @@ unlock:
 	RETURN(rc);
 }
 
+static void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name,
+			     struct lov_user_md_v1 *v1)
+{
+	struct lod_device *d = lu2lod_dev(lod2lu_obj(lo)->lo_dev);
+	struct lod_layout_component *lod_comp;
+	struct pool_desc *pool = NULL;
+	int j, rc = 0;
+
+	/* In the function below, .hs_keycmp resolves to
+	 * pool_hashkey_keycmp() */
+	/* coverity[overrun-buffer-val] */
+	if (pool_name)
+		pool = lod_find_pool(d, pool_name);
+
+	if (pool) {
+		lod_comp = &lo->ldo_comp_entries[pos];
+		if (lod_comp->llc_stripe_offset != LOV_OFFSET_DEFAULT) {
+			if (v1->lmm_magic == LOV_USER_MAGIC_SPECIFIC) {
+				struct lov_user_md_v3 *v3;
+
+				v3 = (struct lov_user_md_v3 *)v1;
+				for (j = 0; j < v3->lmm_stripe_count; j++) {
+					__u32 num;
+
+					num = lod_comp->llc_ostlist.op_array[j];
+					rc = lod_check_index_in_pool(num, pool);
+					if (rc)
+						break;
+				}
+			} else {
+				rc = lod_check_index_in_pool(
+					lod_comp->llc_stripe_offset, pool);
+			}
+			if (rc < 0) {
+				CDEBUG(D_LAYOUT, "%s: index %u is not in the "
+				       "pool %s, dropping the pool\n",
+				       lod2obd(d)->obd_name,
+				       lod_comp->llc_stripe_offset,
+				       pool_name);
+				pool_name = NULL;
+			}
+		}
+
+		if (lod_comp->llc_stripe_count > pool_tgt_count(pool) &&
+		    !(lod_comp->llc_pattern & LOV_PATTERN_OVERSTRIPING))
+			lod_comp->llc_stripe_count = pool_tgt_count(pool);
+
+		lod_pool_putref(pool);
+	}
+
+	lod_obj_set_pool(lo, pos, pool_name);
+}
+
 /**
  * Parse suggested striping configuration.
  *
@@ -2229,7 +2282,6 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 	LASSERT(lo->ldo_comp_entries);
 
 	for (i = 0; i < comp_cnt; i++) {
-		struct pool_desc	*pool;
 		struct lu_extent	*ext;
 		char	*pool_name;
 
@@ -2260,8 +2312,6 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 				rc = lod_comp_copy_ost_lists(lod_comp, v3);
 				if (rc)
 					GOTO(free_comp, rc);
-
-				pool_name = NULL;
 			}
 		}
 
@@ -2294,35 +2344,7 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 		}
 
 		lod_comp->llc_stripe_offset = v1->lmm_stripe_offset;
-		lod_obj_set_pool(lo, i, pool_name);
-
-		if (pool_name == NULL)
-			continue;
-
-		/* In the function below, .hs_keycmp resolves to
-		 * pool_hashkey_keycmp() */
-		/* coverity[overrun-buffer-val] */
-		pool = lod_find_pool(d, pool_name);
-		if (pool == NULL)
-			continue;
-
-		if (lod_comp->llc_stripe_offset != LOV_OFFSET_DEFAULT) {
-			rc = lod_check_index_in_pool(
-					lod_comp->llc_stripe_offset, pool);
-			if (rc < 0) {
-				lod_pool_putref(pool);
-				CDEBUG(D_LAYOUT, "%s: invalid offset, %u\n",
-				       lod2obd(d)->obd_name,
-				       lod_comp->llc_stripe_offset);
-				GOTO(free_comp, rc = -EINVAL);
-			}
-		}
-
-		if (lod_comp->llc_stripe_count > pool_tgt_count(pool) &&
-		    !(lod_comp->llc_pattern & LOV_PATTERN_OVERSTRIPING))
-			lod_comp->llc_stripe_count = pool_tgt_count(pool);
-
-		lod_pool_putref(pool);
+		lod_qos_set_pool(lo, i, pool_name, v1);
 	}
 
 	RETURN(0);