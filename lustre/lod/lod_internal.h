lustre/lustre-release

commit 6a20bdcc608bc2b933774b9f34ec25395e920a54

LU-11376 lov: new foreign LOV format
This patch introduces a new layout/LOV format in order to
allow to specify an arbitrary external reference for a file
in Lustre namespace.
The new LOV format is made of {newmagic, length, type, flags,
string[length]} to be as flexible as possible.
Foreign file can be created by using the open(O_LOV_DELAY_CREATE) +
ioctl(LL_IOC_LOV_SETSTRIPE) operations and it can only be and remain
an empty file until removed.
A new API method llapi_file_create_foreign() has been introduced
and "lfs [[get,set]stripe,find" modified to understand new layout.
The idea behind this is to provide Lustre namespace support and
layout prefetch/caching under layout protection, for user/external
usage.

Code has been added for lfsck to handle foreign files, and
a new sub-test has been added in sanity-lfsck in order to verify
if does not break foreign file and that reverse is also true.

Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: I5d9c0642fe8e7009c30918bfa946cac7c00c9af8
Reviewed-on: https://review.whamcloud.com/33755
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>

lustre/lod/lod_internal.h
@@ -306,7 +306,8 @@ struct lod_object {
 			struct lod_mirror_entry	*ldo_mirrors;
 			__u32		ldo_is_composite:1,
 					ldo_flr_state:2,
-					ldo_comp_cached:1;
+					ldo_comp_cached:1,
+					ldo_is_foreign:1;
 		};
 		/* directory stripe (LMV) */
 		struct {
@@ -331,10 +332,19 @@ struct lod_object {
 			struct lod_default_striping	*ldo_def_striping;
 		};
 	};
-	/* file stripe (LOV) */
-	struct lod_layout_component	*ldo_comp_entries;
-	/* slave stripes of striped directory (LMV) */
-	struct dt_object		**ldo_stripe;
+	union {
+		struct {
+			/* foreign/raw format LOV */
+			char				*ldo_foreign_lov;
+			size_t				 ldo_foreign_lov_size;
+		};
+		struct {
+			/* file stripe (LOV) */
+			struct lod_layout_component	*ldo_comp_entries;
+			/* slave stripes of striped directory (LMV) */
+			struct dt_object		**ldo_stripe;
+		};
+	};
 };
 
 #define lod_foreach_mirror_comp(comp, lo, mirror_idx)                      \
@@ -518,6 +528,9 @@ static inline bool lod_obj_is_striped(struct dt_object *dt)
 
 	if (S_ISDIR(dt->do_lu.lo_header->loh_attr))
 		return lo->ldo_dir_stripe_count != 0;
+
+	if (lo->ldo_is_foreign)
+		return false;
 
 	for (i = 0; i < lo->ldo_comp_cnt; i++) {
 		if (lo->ldo_comp_entries[i].llc_stripe == NULL)
@@ -763,6 +776,8 @@ int lod_declare_striped_create(const struct lu_env *env, struct dt_object *dt,
 int lod_striped_create(const struct lu_env *env, struct dt_object *dt,
 			struct lu_attr *attr, struct dt_object_format *dof,
 			struct thandle *th);
+int lod_alloc_foreign_lov(struct lod_object *lo, size_t size);
+void lod_free_foreign_lov(struct lod_object *lo);
 void lod_striping_free_nolock(const struct lu_env *env, struct lod_object *lo);
 void lod_striping_free(const struct lu_env *env, struct lod_object *lo);
 

lustre/lod/lod_lov.c
@@ -945,7 +945,7 @@ int lod_generate_lovea(const struct lu_env *env, struct lod_object *lo,
 	struct lov_comp_md_v1 *lcm;
 	struct lod_layout_component *comp_entries;
 	__u16 comp_cnt, mirror_cnt;
-	bool is_composite;
+	bool is_composite, is_foreign = false;
 	int i, rc = 0, offset;
 	ENTRY;
 
@@ -960,9 +960,27 @@ int lod_generate_lovea(const struct lu_env *env, struct lod_object *lo,
 		mirror_cnt = lo->ldo_mirror_count;
 		comp_entries = lo->ldo_comp_entries;
 		is_composite = lo->ldo_is_composite;
+		is_foreign = lo->ldo_is_foreign;
 	}
 
 	LASSERT(lmm_size != NULL);
+
+	if (is_foreign) {
+		struct lov_foreign_md *lfm;
+
+		lfm = (struct lov_foreign_md *)lmm;
+		memcpy(lfm, lo->ldo_foreign_lov, lo->ldo_foreign_lov_size);
+		/* need to store little-endian */
+		if (cpu_to_le32(LOV_MAGIC_FOREIGN) != LOV_MAGIC_FOREIGN) {
+			__swab32s(&lfm->lfm_magic);
+			__swab32s(&lfm->lfm_length);
+			__swab32s(&lfm->lfm_type);
+			__swab32s(&lfm->lfm_flags);
+		}
+		*lmm_size = lo->ldo_foreign_lov_size;
+		RETURN(0);
+	}
+
 	LASSERT(comp_cnt != 0 && comp_entries != NULL);
 
 	if (!is_composite) {
@@ -1237,6 +1255,7 @@ int lod_parse_striping(const struct lu_env *env, struct lod_object *lo,
 {
 	struct lov_mds_md_v1	*lmm;
 	struct lov_comp_md_v1	*comp_v1 = NULL;
+	struct lov_foreign_md	*foreign = NULL;
 	struct lov_ost_data_v1	*objs;
 	__u32	magic, pattern;
 	int	i, j, rc = 0;
@@ -1253,10 +1272,13 @@ int lod_parse_striping(const struct lu_env *env, struct lod_object *lo,
 	magic = le32_to_cpu(lmm->lmm_magic);
 
 	if (magic != LOV_MAGIC_V1 && magic != LOV_MAGIC_V3 &&
-	    magic != LOV_MAGIC_COMP_V1)
+	    magic != LOV_MAGIC_COMP_V1 && magic != LOV_MAGIC_FOREIGN)
 		GOTO(out, rc = -EINVAL);
 
-	lod_free_comp_entries(lo);
+	if (lo->ldo_is_foreign)
+		lod_free_foreign_lov(lo);
+	else
+		lod_free_comp_entries(lo);
 
 	if (magic == LOV_MAGIC_COMP_V1) {
 		comp_v1 = (struct lov_comp_md_v1 *)lmm;
@@ -1268,6 +1290,25 @@ int lod_parse_striping(const struct lu_env *env, struct lod_object *lo,
 		lo->ldo_flr_state = le16_to_cpu(comp_v1->lcm_flags) &
 					LCM_FL_FLR_MASK;
 		mirror_cnt = le16_to_cpu(comp_v1->lcm_mirror_count) + 1;
+	} else if (magic == LOV_MAGIC_FOREIGN) {
+		size_t length;
+
+		foreign = (struct lov_foreign_md *)buf->lb_buf;
+		length = offsetof(typeof(*foreign), lfm_value);
+		if (buf->lb_len < length ||
+		    buf->lb_len < (length + le32_to_cpu(foreign->lfm_length))) {
+			CDEBUG(D_LAYOUT,
+			       "buf len %zu too small for lov_foreign_md\n",
+			       buf->lb_len);
+			GOTO(out, rc = -EINVAL);
+		}
+
+		/* just cache foreign LOV EA raw */
+		rc = lod_alloc_foreign_lov(lo, length);
+		if (rc)
+			GOTO(out, rc);
+		memcpy(lo->ldo_foreign_lov, buf->lb_buf, length);
+		GOTO(out, rc);
 	} else {
 		comp_cnt = 1;
 		lo->ldo_layout_gen = le16_to_cpu(lmm->lmm_layout_gen);
@@ -1810,7 +1851,40 @@ int lod_verify_striping(struct lod_device *d, struct lod_object *lo,
 	int     rc = 0;
 	ENTRY;
 
+	if (buf->lb_len < sizeof(lum->lmm_magic)) {
+		CDEBUG(D_LAYOUT, "invalid buf len %zu\n", buf->lb_len);
+		RETURN(-EINVAL);
+	}
+
 	lum = buf->lb_buf;
+
+	magic = le32_to_cpu(lum->lmm_magic) & ~LOV_MAGIC_DEFINED;
+	/* treat foreign LOV EA/object case first
+	 * XXX is it expected to try setting again a foreign?
+	 * XXX should we care about different current vs new layouts ?
+	 */
+	if (unlikely(magic == LOV_USER_MAGIC_FOREIGN)) {
+		struct lov_foreign_md *lfm = buf->lb_buf;
+
+		if (buf->lb_len < offsetof(typeof(*lfm), lfm_value)) {
+			CDEBUG(D_LAYOUT,
+			       "buf len %zu < min lov_foreign_md size (%zu)\n",
+			       buf->lb_len, offsetof(typeof(*lfm),
+			       lfm_value));
+			RETURN(-EINVAL);
+		}
+
+		if (foreign_size_le(lfm) > buf->lb_len) {
+			CDEBUG(D_LAYOUT,
+			       "buf len %zu < this lov_foreign_md size (%zu)\n",
+			       buf->lb_len, foreign_size_le(lfm));
+			RETURN(-EINVAL);
+		}
+		/* Don't do anything with foreign layouts */
+		RETURN(0);
+	}
+
+	/* normal LOV/layout cases */
 
 	if (buf->lb_len < sizeof(*lum)) {
 		CDEBUG(D_LAYOUT, "buf len %zu too small for lov_user_md\n",
@@ -1818,7 +1892,6 @@ int lod_verify_striping(struct lod_device *d, struct lod_object *lo,
 		RETURN(-EINVAL);
 	}
 
-	magic = le32_to_cpu(lum->lmm_magic) & ~LOV_MAGIC_DEFINED;
 	if (magic != LOV_USER_MAGIC_V1 &&
 	    magic != LOV_USER_MAGIC_V3 &&
 	    magic != LOV_USER_MAGIC_SPECIFIC &&

lustre/lod/lod_object.c
@@ -2578,7 +2578,7 @@ static int lod_comp_md_size(struct lod_object *lo, bool is_dir)
 	int magic, size = 0, i;
 	struct lod_layout_component *comp_entries;
 	__u16 comp_cnt;
-	bool is_composite;
+	bool is_composite, is_foreign = false;
 
 	if (is_dir) {
 		comp_cnt = lo->ldo_def_striping->lds_def_comp_cnt;
@@ -2589,8 +2589,11 @@ static int lod_comp_md_size(struct lod_object *lo, bool is_dir)
 		comp_cnt = lo->ldo_comp_cnt;
 		comp_entries = lo->ldo_comp_entries;
 		is_composite = lo->ldo_is_composite;
+		is_foreign = lo->ldo_is_foreign;
 	}
 
+	if (is_foreign)
+		return lo->ldo_foreign_lov_size;
 
 	LASSERT(comp_cnt != 0 && comp_entries != NULL);
 	if (is_composite) {
@@ -3940,7 +3943,7 @@ static int lod_generate_and_set_lovea(const struct lu_env *env,
 
 	LASSERT(lo);
 
-	if (lo->ldo_comp_cnt == 0) {
+	if (lo->ldo_comp_cnt == 0 && !lo->ldo_is_foreign) {
 		lod_striping_free(env, lo);
 		rc = lod_sub_xattr_del(env, next, XATTR_NAME_LOV, th);
 		RETURN(rc);
@@ -5256,7 +5259,8 @@ int lod_striped_create(const struct lu_env *env, struct dt_object *dt,
 	int	rc = 0, i, j;
 	ENTRY;
 
-	LASSERT(lo->ldo_comp_cnt != 0 && lo->ldo_comp_entries != NULL);
+	LASSERT((lo->ldo_comp_cnt != 0 && lo->ldo_comp_entries != NULL) ||
+		lo->ldo_is_foreign);
 
 	mirror_id = 0; /* non-flr file's mirror_id is 0 */
 	if (lo->ldo_mirror_count > 1) {
@@ -6819,6 +6823,41 @@ static int lod_object_init(const struct lu_env *env, struct lu_object *lo,
 
 /**
  *
+ * Alloc cached foreign LOV
+ *
+ * \param[in] lo        object
+ * \param[in] size      size of foreign LOV
+ *
+ * \retval		0 on success
+ * \retval		negative if failed
+ */
+int lod_alloc_foreign_lov(struct lod_object *lo, size_t size)
+{
+	OBD_ALLOC_LARGE(lo->ldo_foreign_lov, size);
+	if (lo->ldo_foreign_lov == NULL)
+		return -ENOMEM;
+	lo->ldo_foreign_lov_size = size;
+	lo->ldo_is_foreign = 1;
+	return 0;
+}
+
+/**
+ *
+ * Free cached foreign LOV
+ *
+ * \param[in] lo        object
+ */
+void lod_free_foreign_lov(struct lod_object *lo)
+{
+	if (lo->ldo_foreign_lov != NULL)
+		OBD_FREE_LARGE(lo->ldo_foreign_lov, lo->ldo_foreign_lov_size);
+	lo->ldo_foreign_lov = NULL;
+	lo->ldo_foreign_lov_size = 0;
+	lo->ldo_is_foreign = 0;
+}
+
+/**
+ *
  * Release resources associated with striping.
  *
  * If the object is striped (regular or directory), then release
@@ -6832,7 +6871,10 @@ void lod_striping_free_nolock(const struct lu_env *env, struct lod_object *lo)
 	struct lod_layout_component *lod_comp;
 	int i, j;
 
-	if (lo->ldo_stripe != NULL) {
+	if (unlikely(lo->ldo_is_foreign)) {
+		lod_free_foreign_lov(lo);
+		lo->ldo_comp_cached = 0;
+	} else if (lo->ldo_stripe != NULL) {
 		LASSERT(lo->ldo_comp_entries == NULL);
 		LASSERT(lo->ldo_dir_stripes_allocated > 0);
 

lustre/doc/Makefile.am
@@ -63,6 +63,7 @@ MANFILES =					\
 	llapi_create_volatile_param.3		\
 	llapi_fd2parent.3			\
 	llapi_file_create.3			\
+	llapi_file_create_foreign.3		\
 	llapi_file_get_stripe.3			\
 	llapi_file_open.3			\
 	llapi_group_lock.3			\

lustre/doc/lfs-find.1
@@ -12,6 +12,7 @@ lfs-find \- Lustre client utility to list files with specific attributes
       [[\fB!\fR] \fB--component-start|\fB--comp-start\fR [\fB+-\fR]\fIn\fR[\fBKMGTPE\fR]]
       [[\fB!\fR] \fB--gid\fR|\fB-g\fR|\fB--group\fR|\fB-G\fR <\fIgname\fR>|<\fIgid\fR>]
       [[\fB!\fR] \fB--layout\fR|\fB-L mdt\fR,\fBraid0\fR,\fBreleased\fR]
+      [[\fB!\fR] \fB--foreign\fR [<\fItype\fR>]]
 [\fB--maxdepth\fR|\fB-D\fI n\fR]
       [[\fB!\fR] \fB--mdt\fR|\fB--mdt-index\fR|\fB-m\fR <\fIuuid\fR|\fIindex\fR,...>]
       [[\fB!\fR] \fB--mdt-count\fR|\fB-T\fR [\fB+-\fR]\fIn\fR]
@@ -107,7 +108,7 @@ File has specified numeric group ID.
 .BR --group | -G
 File belongs to specified group, numeric group ID allowed.
 .TP
-.BR --layout
+.BR --layout | -L
 File has a layout of the given type, one of:
 .RS 1.2i
 .TP
@@ -119,6 +120,11 @@ HSM-archived files that are not resident in the filesystem.
 .TP
 .B mdt
 Files that have the first data component on an MDT.
+.RE
+.TP
+.BR --foreign[=<type>]
+File is foreign (non-Lustre layout) and is of type if specified.
+Presently only none or daos are defined types.
 .RE
 .TP
 .BR --maxdepth
@@ -290,6 +296,9 @@ Recursively list all mirrored files that have more than 2 mirrors.
 .TP
 .B $ lfs find ! --mirror-state=ro /mnt/lustre
 Recursively list all out-of-sync mirrored files.
+.TP
+.B $ lfs find ! --foreign=daos /mnt/lustre
+Recursively list all but foreign files of daos type.
 .SH BUGS
 The
 .B lfs find

lustre/doc/lfs-setstripe.1
@@ -23,6 +23,9 @@ lfs setstripe \- set striping pattern of a file or directory default
 .B lfs setstripe --yaml=\fR<\fIyaml_template_file.lyl\fR> <\fIfile\fR>
 .br
 .B lfs setstripe --copy=\fR<\fIsource_template_file\fR> <\fIfile\fR>
+.br
+.B lfs setstripe --foreign[=\fR<\fIforeign_type\fR>\fB] \
+[--flags=\fR<\fIhex\fR>\fB] --xattr=\fR<\fIlayout_string\fR> <\fIfile\fR>
 .SH DESCRIPTION
 The
 .B lfs setstripe
@@ -123,6 +126,18 @@ This is similar to the
 option but avoids the need for the intermediate
 .B .lyl
 file.
+.TP
+.B lfs setstripe --foreign[=\fR<\fIforeign_type\fR>\fB] \
+[--flags=\fR<\fIhex\fR>\fB] --xattr=\fR<\fIlayout_string\fR> <\fIfile\fR>
+.br
+Create a new
+.I file
+with a foreign/non-lustre layout of type
+.I foreign_type \fR(\fBnone\fR, \fBdaos\fR, ...)
+with flags
+.I hex
+and a free-format layout value of
+.I layout_string.
 .SH STRIPE_OPTIONS
 The various OST stripe related options are listed and explained below:
 .TP
@@ -231,6 +246,29 @@ or
 .BR pool_name=none
 (since Lustre 2.11) to force a component to inherit the pool from the parent
 or root directory instead of the previous component.
+.TP
+.B --foreign \fR[<\fIforeign_type\fR>]
+file layout is non-lustre/free-format and of type <
+.IR foreign_type
+>, if specified (see also
+.IR --xattr
+and
+.IR --flags
+options).
+Current known types are
+.BR none
+and
+.BR daos
+, default is
+.BR none
+.
+.B -x\fR, \fB--xattr \fR<\fIlayout_string\fR>
+Mandatory non-lustre/free-format layout/LOV EA content for
+.I foreign
+file.
+.TP
+.B -F\fR, \fB--flags \fR<\fIhex\fR>
+Optional bitmap of flags for foreign type.
 .SH COMPONENT_OPTIONS
 The various component related options are listed and explained below.  The
 .B --component-*
@@ -436,6 +474,18 @@ file
 which can be created with the
 .B lfs getstripe --yaml
 command.
+.TP
+.B lfs setstripe --foreign=daos --flags=0xda08 \
+	--xattr=PUUID:CUUID /mnt/lustre/file1
+This creates foreign
+.BR file1
+of type
+.BR daos
+with non-lustre/free-format
+.BR PUUID:CUUID
+layout/LOV EA and flags
+.BR 0xda08
+.
 .SH SEE ALSO
 .BR lctl (1),
 .BR lfs (1),

lustre/doc/llapi_file_create_foreign.3
@@ -1,0 +1,91 @@
+.TH lustreapi 3 "2009 Jul 10" The Lustre user application interface library
+.SH NAME
+llapi_file_create_foreign \- create a file with foreign layout on a Lustre filesystem
+.SH SYNOPSIS
+.nf
+.B #include <sys/types.h>
+.B #include <sys/stat.h>
+.B #include <fcntl.h>
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_file_create_foreign(const char *" name ", mode_t " mode ","
+.BI "                    __u32 " type ", __u32 " flags ", char *" foreign_lov );
+.sp
+.fi
+.SH DESCRIPTION
+.LP
+.B llapi_file_create_foreign(\|)
+creates a file with
+.I foreign_lov
+free format layout and
+.I mode
+and
+.I name
+on a Lustre filesystem, followed by file close.
+.TP 15
+.I mode
+specifies the permission bits.
+Refer to
+.BR open(2)
+man page for a detailed description.
+.TP 15
+.I type
+specifies a type of foreign layout.
+.TP 15
+.I flags
+specifies any flags for foreign layout.
+.TP 15
+.I foreign_lov
+free-format string to be set as file layout/LOV EA.
+.SH RETURN VALUES
+.LP
+.B llapi_file_create_foreign(\|)
+return:
+.TP
+>=0
+on success, for
+.B llapi_file_open
+the return value is a file descriptor.
+.TP
+<0
+on failure, the absolute value is an error code.
+.SH ERRORS
+.TP 15
+.SM EEXIST
+.I name
+already exists.
+.TP
+.SM ENOTTY
+.I name
+may not point to a Lustre filesystem.
+.SH "EXAMPLE"
+.nf
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <lustre/lustreapi.h>
+int main(int argc, char *argv[])
+{
+        int rc;
+
+        if (argc != 2)
+                return -1;
+
+        rc = llapi_file_create_foreign(argv[1], 0600, LOV_FOREIGN_TYPE_DAOS,
+				       0xda08, "PUUID@CUUID");
+        if (rc < 0) {
+                fprintf(stderr, "file creation has failed, %s\\n", strerror(-rc));
+                return -1;
+        }
+        printf("foreign file %s has been created with 'PUUID@CUUID' layout!\\n",
+               argv[1]);
+        return 0;
+}
+.fi
+.SH "SEE ALSO"
+.BR llapi_file_open (3)
+.BR lustre (7),
+.BR lustreapi (7)
+.BR open (2),

lustre/include/lustre/lustreapi.h
@@ -128,6 +128,8 @@ struct llapi_stripe_param {
 
 int llapi_file_open_param(const char *name, int flags, mode_t mode,
 			  const struct llapi_stripe_param *param);
+int llapi_file_create_foreign(const char *name, mode_t mode, __u32 type,
+			      __u32 flags, char *foreign_lov);
 int llapi_file_create(const char *name, unsigned long long stripe_size,
 		      int stripe_offset, int stripe_count, int stripe_pattern);
 int llapi_file_open(const char *name, int flags, int mode,
@@ -263,7 +265,9 @@ struct find_param {
 				 fp_exclude_hash_type:1,
 				 fp_yaml:1,	/* output layout in YAML */
 				 fp_check_blocks:1,
-				 fp_exclude_blocks:1;
+				 fp_exclude_blocks:1,
+				 fp_check_foreign:1,
+				 fp_exclude_foreign:1;
 
 	enum llapi_layout_verbose fp_verbose;
 	int			 fp_quiet;
@@ -321,6 +325,7 @@ struct find_param {
 	unsigned int		 fp_depth;
 	unsigned int		 fp_hash_type;
 	unsigned int		 fp_time_margin; /* time margin in seconds */
+	__u32			 fp_foreign_type;
 };
 
 int llapi_ostlist(char *path, struct find_param *param);

commit 7a9c9ccabe93f2d96c80e90f8cbb786faca74835

LU-15278 lod: distinguish DIR/REGULAR lod_object members
In lod_striping_free_nolock(), we need to distinguish lod_object
type, since DIR/REGULAR lod_object structure share the same memory
region, it could accidently free some unintended memory if it treat
DIR lod_object as REGULAR one, or vice versa.

Fixes: 6a20bdcc608b ("LU-11376 lov: new foreign LOV format")
Fixes: fdad38781ccc ("LU-11376 lmv: new foreign LMV format")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I2d4c563725b35f7a75f0f1fbf9c1d35b1799eff4
Reviewed-on: https://review.whamcloud.com/45710
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_internal.h
@@ -246,6 +246,8 @@ struct lod_object {
 	/* common fields for both files and directories */
 	struct dt_object		ldo_obj;
 	struct mutex			ldo_layout_mutex;
+					/* foreign file/directory */
+	__u32				ldo_is_foreign:1;
 	union {
 		/* file stripe (LOV) */
 		struct {
@@ -259,8 +261,7 @@ struct lod_object {
 			struct lod_mirror_entry	*ldo_mirrors;
 			__u32		ldo_is_composite:1,
 					ldo_flr_state:4,
-					ldo_comp_cached:1,
-					ldo_is_foreign:1;
+					ldo_comp_cached:1;
 		};
 		/* directory stripe (LMV) */
 		struct {
@@ -277,9 +278,7 @@ struct lod_object {
 			__u32		ldo_dir_slave_stripe:1,
 					ldo_dir_striped:1,
 					/* the stripe has been loaded */
-					ldo_dir_stripe_loaded:1,
-					/* foreign directory */
-					ldo_dir_is_foreign;
+					ldo_dir_stripe_loaded:1;
 			/*
 			 * This default LMV is parent default LMV, which will be
 			 * used in child creation, and it's not cached, so this

lustre/lod/lod_lov.c
@@ -584,6 +584,7 @@ int lod_alloc_comp_entries(struct lod_object *lo,
 	}
 
 	lo->ldo_comp_cnt = comp_count;
+	lo->ldo_is_foreign = 0;
 	return 0;
 }
 
@@ -1512,7 +1513,7 @@ int lod_striping_load(const struct lu_env *env, struct lod_object *lo)
 				info->lti_ea_store_size = 0;
 
 				lo->ldo_dir_stripe_loaded = 1;
-				lo->ldo_dir_is_foreign = 1;
+				lo->ldo_is_foreign = 1;
 				GOTO(unlock, rc = 0);
 			}
 		}

lustre/lod/lod_object.c
@@ -1868,6 +1868,7 @@ int lod_parse_dir_striping(const struct lu_env *env, struct lod_object *lo,
 	}
 out:
 	lo->ldo_stripe = stripe;
+	lo->ldo_is_foreign = 0;
 	lo->ldo_dir_stripe_count = le32_to_cpu(lmv1->lmv_stripe_count);
 	lo->ldo_dir_stripes_allocated = le32_to_cpu(lmv1->lmv_stripe_count);
 	lo->ldo_dir_layout_version = le32_to_cpu(lmv1->lmv_layout_version);
@@ -2300,6 +2301,41 @@ out:
 
 /**
  *
+ * Alloc cached foreign LOV
+ *
+ * \param[in] lo        object
+ * \param[in] size      size of foreign LOV
+ *
+ * \retval		0 on success
+ * \retval		negative if failed
+ */
+int lod_alloc_foreign_lov(struct lod_object *lo, size_t size)
+{
+	OBD_ALLOC_LARGE(lo->ldo_foreign_lov, size);
+	if (lo->ldo_foreign_lov == NULL)
+		return -ENOMEM;
+	lo->ldo_foreign_lov_size = size;
+	lo->ldo_is_foreign = 1;
+	return 0;
+}
+
+/**
+ *
+ * Free cached foreign LOV
+ *
+ * \param[in] lo        object
+ */
+void lod_free_foreign_lov(struct lod_object *lo)
+{
+	if (lo->ldo_foreign_lov != NULL)
+		OBD_FREE_LARGE(lo->ldo_foreign_lov, lo->ldo_foreign_lov_size);
+	lo->ldo_foreign_lov = NULL;
+	lo->ldo_foreign_lov_size = 0;
+	lo->ldo_is_foreign = 0;
+}
+
+/**
+ *
  * Alloc cached foreign LMV
  *
  * \param[in] lo        object
@@ -2314,9 +2350,24 @@ int lod_alloc_foreign_lmv(struct lod_object *lo, size_t size)
 	if (lo->ldo_foreign_lmv == NULL)
 		return -ENOMEM;
 	lo->ldo_foreign_lmv_size = size;
-	lo->ldo_dir_is_foreign = 1;
+	lo->ldo_is_foreign = 1;
 
 	return 0;
+}
+
+/**
+ *
+ * Free cached foreign LMV
+ *
+ * \param[in] lo        object
+ */
+void lod_free_foreign_lmv(struct lod_object *lo)
+{
+	if (lo->ldo_foreign_lmv != NULL)
+		OBD_FREE_LARGE(lo->ldo_foreign_lmv, lo->ldo_foreign_lmv_size);
+	lo->ldo_foreign_lmv = NULL;
+	lo->ldo_foreign_lmv_size = 0;
+	lo->ldo_is_foreign = 0;
 }
 
 /**
@@ -2357,7 +2408,7 @@ static int lod_declare_xattr_set_lmv(const struct lu_env *env,
 	       (int)le32_to_cpu(lum->lum_stripe_offset));
 
 	if (lo->ldo_dir_stripe_count == 0) {
-		if (lo->ldo_dir_is_foreign) {
+		if (lo->ldo_is_foreign) {
 			rc = lod_alloc_foreign_lmv(lo, lum_buf->lb_len);
 			if (rc != 0)
 				GOTO(out, rc);
@@ -4231,7 +4282,7 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 	/* The stripes are supposed to be allocated in declare phase,
 	 * if there are no stripes being allocated, it will skip */
 	if (lo->ldo_dir_stripe_count == 0) {
-		if (lo->ldo_dir_is_foreign) {
+		if (lo->ldo_is_foreign) {
 			rc = lod_sub_xattr_set(env, dt_object_child(dt), buf,
 					       XATTR_NAME_LMV, fl, th);
 			if (rc != 0)
@@ -4463,7 +4514,7 @@ static int lod_dir_striping_create_internal(const struct lu_env *env,
 							       lmu, dof, th);
 			}
 		} else {
-			if (lo->ldo_dir_is_foreign) {
+			if (lo->ldo_is_foreign) {
 				LASSERT(lo->ldo_foreign_lmv != NULL &&
 					lo->ldo_foreign_lmv_size > 0);
 				info->lti_buf.lb_buf = lo->ldo_foreign_lmv;
@@ -5650,7 +5701,7 @@ static void lod_ah_init(const struct lu_env *env,
 		 */
 		if (ah->dah_eadata != NULL && ah->dah_eadata_len != 0 &&
 		    le32_to_cpu(lum1->lum_magic) == LMV_MAGIC_FOREIGN) {
-			lc->ldo_dir_is_foreign = true;
+			lc->ldo_is_foreign = true;
 			/* keep stripe_count 0 and stripe_offset -1 */
 			CDEBUG(D_INFO, "no default striping for foreign dir\n");
 			RETURN_EXIT;
@@ -8346,6 +8397,7 @@ static int lod_dir_declare_layout_attach(const struct lu_env *env,
 		OBD_FREE_PTR_ARRAY(lo->ldo_stripe,
 				   lo->ldo_dir_stripes_allocated);
 	lo->ldo_stripe = stripes;
+	lo->ldo_is_foreign = 0;
 	lo->ldo_dir_migrate_offset = lo->ldo_dir_stripe_count;
 	lo->ldo_dir_migrate_hash = le32_to_cpu(lmv->lmv_hash_type);
 	lo->ldo_dir_stripe_count += stripe_count;
@@ -8590,6 +8642,7 @@ static int lod_dir_declare_layout_split(const struct lu_env *env,
 	OBD_FREE(lo->ldo_stripe,
 		 sizeof(*stripes) * lo->ldo_dir_stripes_allocated);
 	lo->ldo_stripe = stripes;
+	lo->ldo_is_foreign = 0;
 	lo->ldo_dir_striped = 1;
 	lo->ldo_dir_stripe_count = rc;
 	lo->ldo_dir_stripes_allocated = stripe_count;
@@ -9059,56 +9112,6 @@ static int lod_object_init(const struct lu_env *env, struct lu_object *lo,
 
 /**
  *
- * Alloc cached foreign LOV
- *
- * \param[in] lo        object
- * \param[in] size      size of foreign LOV
- *
- * \retval		0 on success
- * \retval		negative if failed
- */
-int lod_alloc_foreign_lov(struct lod_object *lo, size_t size)
-{
-	OBD_ALLOC_LARGE(lo->ldo_foreign_lov, size);
-	if (lo->ldo_foreign_lov == NULL)
-		return -ENOMEM;
-	lo->ldo_foreign_lov_size = size;
-	lo->ldo_is_foreign = 1;
-	return 0;
-}
-
-/**
- *
- * Free cached foreign LOV
- *
- * \param[in] lo        object
- */
-void lod_free_foreign_lov(struct lod_object *lo)
-{
-	if (lo->ldo_foreign_lov != NULL)
-		OBD_FREE_LARGE(lo->ldo_foreign_lov, lo->ldo_foreign_lov_size);
-	lo->ldo_foreign_lov = NULL;
-	lo->ldo_foreign_lov_size = 0;
-	lo->ldo_is_foreign = 0;
-}
-
-/**
- *
- * Free cached foreign LMV
- *
- * \param[in] lo        object
- */
-void lod_free_foreign_lmv(struct lod_object *lo)
-{
-	if (lo->ldo_foreign_lmv != NULL)
-		OBD_FREE_LARGE(lo->ldo_foreign_lmv, lo->ldo_foreign_lmv_size);
-	lo->ldo_foreign_lmv = NULL;
-	lo->ldo_foreign_lmv_size = 0;
-	lo->ldo_dir_is_foreign = 0;
-}
-
-/**
- *
  * Release resources associated with striping.
  *
  * If the object is striped (regular or directory), then release
@@ -9120,14 +9123,17 @@ void lod_free_foreign_lmv(struct lod_object *lo)
 void lod_striping_free_nolock(const struct lu_env *env, struct lod_object *lo)
 {
 	struct lod_layout_component *lod_comp;
+	__u32 obj_attr = lo->ldo_obj.do_lu.lo_header->loh_attr;
 	int i, j;
 
 	if (unlikely(lo->ldo_is_foreign)) {
-		lod_free_foreign_lov(lo);
-		lo->ldo_comp_cached = 0;
-	} else if (unlikely(lo->ldo_dir_is_foreign)) {
-		lod_free_foreign_lmv(lo);
-		lo->ldo_dir_stripe_loaded = 0;
+		if (S_ISREG(obj_attr)) {
+			lod_free_foreign_lov(lo);
+			lo->ldo_comp_cached = 0;
+		} else if (S_ISDIR(obj_attr)) {
+			lod_free_foreign_lmv(lo);
+			lo->ldo_dir_stripe_loaded = 0;
+		}
 	} else if (lo->ldo_stripe != NULL) {
 		LASSERT(lo->ldo_comp_entries == NULL);
 		LASSERT(lo->ldo_dir_stripes_allocated > 0);