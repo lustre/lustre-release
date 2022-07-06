lustre/lustre-release

commit bb7c82f13e7a01891edcdf0626c6fb91a240e56e

LU-15795 kernel: RHEL 8.6 server support
This patch makes changes to support RHEL 8.6 release
with kernel 4.18.0-372.9.1.el8 for Lustre server.

Test-Parameters: trivial fstype=ldiskfs \
clientdistro=el8.6 serverdistro=el8.6 testlist=sanity

Test-Parameters: trivial fstype=zfs \
clientdistro=el8.6 serverdistro=el8.6 testlist=sanity

Change-Id: I86407171b40d40a05379085444eee79e2ac272b3
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/47192
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2602,6 +2602,29 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_HAVE_USER_NAMESPACE_ARG
 
 #
+# LC_TASK_STRUCT_HAS_NEW_STATE
+#
+# kernel 5.13 commit 2f064a59a11ff9bc22e52e9678bc601404c7cb34
+# Change the type and name of task_struct::state. Drop the volatile and
+# shrink it to an 'unsigned int'. Rename it in order to find all uses
+# such that we can use READ_ONCE/WRITE_ONCE as appropriate.
+#
+AC_DEFUN([LC_TASK_STRUCT_HAS_NEW_STATE], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if 'struct task_struct' has a '__state' member],
+task_struct_state, [
+	#include <linux/sched.h>
+],[
+	((struct task_struct *)NULL)->__state = 0;
+],[
+	AC_DEFINE(__state, state,
+		['struct task_struct' has a '__state' member])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LC_TASK_STRUCT_HAS_NEW_STATE
+
+#
 # LC_HAVE_GET_ACL_RCU_ARG
 #
 # kernel 5.15 commit 0cad6246621b5887d5b33fea84219d2a71f2f99a
@@ -2821,6 +2844,9 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 5.12
 	LC_HAVE_USER_NAMESPACE_ARG
+
+	# 5.13
+	LC_TASK_STRUCT_HAS_NEW_STATE
 
 	# 5.15
 	LC_HAVE_GET_ACL_RCU_ARG

config/lustre-build-ldiskfs.m4
@@ -13,6 +13,7 @@ esac
 AS_IF([test -z "$LDISKFS_SERIES"], [
 AS_IF([test x$RHEL_KERNEL = xyes], [
 	case $RHEL_RELEASE_NO in
+	86)     LDISKFS_SERIES="4.18-rhel8.6.series"    ;;
 	85)     LDISKFS_SERIES="4.18-rhel8.5.series"    ;;
 	84)     LDISKFS_SERIES="4.18-rhel8.4.series"    ;;
 	83)     LDISKFS_SERIES="4.18-rhel8.3.series"    ;;

ldiskfs/kernel_patches/series/ldiskfs-4.18-rhel8.6.series
@@ -1,0 +1,33 @@
+rhel8.1/ext4-inode-version.patch
+suse15/ext4-lookup-dotdot.patch
+suse15/ext4-print-inum-in-htree-warning.patch
+rhel8/ext4-prealloc.patch
+ubuntu18/ext4-osd-iop-common.patch
+rhel8.3/ext4-misc.patch
+rhel8.3/ext4-mballoc-extra-checks.patch
+ubuntu18/ext4-hash-indexed-dir-dotdot-update.patch
+rhel8.1/ext4-kill-dx-root.patch
+rhel7.6/ext4-mballoc-pa-free-mismatch.patch
+rhel8.4/ext4-data-in-dirent.patch
+rhel8/ext4-nocmtime.patch
+base/ext4-htree-lock.patch
+rhel8.4/ext4-pdirop.patch
+rhel8/ext4-max-dir-size.patch
+rhel8.3/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
+ubuntu18/ext4-give-warning-with-dir-htree-growing.patch
+ubuntu18/ext4-jcb-optimization.patch
+rhel8.2/ext4-attach-jinode-in-writepages.patch
+rhel8/ext4-dont-check-before-replay.patch
+rhel7.6/ext4-use-GFP_NOFS-in-ext4_inode_attach_jinode.patch
+rhel7.6/ext4-export-orphan-add.patch
+rhel8/ext4-export-mb-stream-allocator-variables.patch
+rhel8/ext4-simple-blockalloc.patch
+rhel8/ext4-mballoc-skip-uninit-groups-cr0.patch
+rhel8.1/ext4-mballoc-prefetch.patch
+rhel8.3/ext4-xattr-disable-credits-check.patch
+base/ext4-no-max-dir-size-limit-for-iam-objects.patch
+rhel8/ext4-ialloc-uid-gid-and-pass-owner-down.patch
+base/ext4-projid-xattrs.patch
+rhel8.5/ext4-enc-flag.patch
+base/ext4-delayed-iput.patch
+rhel8.5/ext4-filename-encode.patch

lustre/kernel_patches/series/4.18-rhel8.6.series
@@ -1,0 +1,2 @@
+block-integrity-allow-optional-integrity-functions-rhel8.patch
+block-pass-bio-into-integrity_processing_fn-rhel8.patch

lustre/kernel_patches/targets/4.18-rhel8.6.target.in
@@ -2,7 +2,7 @@ lnxmaj="4.18.0"
 lnxrel="372.9.1.el8"
 
 KERNEL_SRPM=kernel-${lnxmaj}-${lnxrel}.src.rpm
-SERIES=""
+SERIES=4.18-rhel8.6.series
 EXTRA_VERSION=${lnxrel}_lustre.@VERSION@
 LUSTRE_VERSION=@VERSION@
 

lustre/kernel_patches/which_patch
@@ -23,5 +23,6 @@ PATCH SERIES FOR SERVER KERNELS:
 4.18-rhel8.3.series     4.18.0-240.22.1.el8  (RHEL 8.3)
 4.18-rhel8.4.series     4.18.0-305.25.1.el8  (RHEL 8.4)
 4.18-rhel8.5.series     4.18.0-348.23.1.el8  (RHEL 8.5)
+4.18-rhel8.6.series     4.18.0-372.9.1.el8   (RHEL 8.6)
 
 See lustre/ChangeLog for supported client kernel versions.

commit f4c65e01a2e58b2bae770770590494d85ef636f7

LU-15973 build: remove AC_DEFINE(__state, state, ...)
RHEL 8.6 build failed with MLNX_OFED 5.6-2.0.9.0 as follows:

error: 'struct task_struct' has no member named 'state'; did you mean '__state'?
 #define __state state
                 ^~~~~

The failure was introduced by
commit bb7c82f13e7a01891edcdf0626c6fb91a240e56e and a proper
way to resolve the original issue was in
commit c04adbcd76725a360f411f09c63df785bf7db426. So, let's
remove the improper way AC_DEFINE(__state, state, ...).

Test-Parameters: trivial fstype=ldiskfs \
clientdistro=el8.6 serverdistro=el8.6 testlist=sanity

Test-Parameters: trivial fstype=zfs \
clientdistro=el8.6 serverdistro=el8.6 testlist=sanity

Change-Id: Icbc897cf5870352311262af8bd59ec24ea9d7301
Fixes: bb7c82f13e7 ("LU-15795 kernel: RHEL 8.6 server support")
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/47799
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Minh Diep <mdiep@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2602,29 +2602,6 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_HAVE_USER_NAMESPACE_ARG
 
 #
-# LC_TASK_STRUCT_HAS_NEW_STATE
-#
-# kernel 5.13 commit 2f064a59a11ff9bc22e52e9678bc601404c7cb34
-# Change the type and name of task_struct::state. Drop the volatile and
-# shrink it to an 'unsigned int'. Rename it in order to find all uses
-# such that we can use READ_ONCE/WRITE_ONCE as appropriate.
-#
-AC_DEFUN([LC_TASK_STRUCT_HAS_NEW_STATE], [
-tmp_flags="$EXTRA_KCFLAGS"
-EXTRA_KCFLAGS="-Werror"
-LB_CHECK_COMPILE([if 'struct task_struct' has a '__state' member],
-task_struct_state, [
-	#include <linux/sched.h>
-],[
-	((struct task_struct *)NULL)->__state = 0;
-],[
-	AC_DEFINE(__state, state,
-		['struct task_struct' has a '__state' member])
-])
-EXTRA_KCFLAGS="$tmp_flags"
-]) # LC_TASK_STRUCT_HAS_NEW_STATE
-
-#
 # LC_HAVE_GET_ACL_RCU_ARG
 #
 # kernel 5.15 commit 0cad6246621b5887d5b33fea84219d2a71f2f99a
@@ -2844,9 +2821,6 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 5.12
 	LC_HAVE_USER_NAMESPACE_ARG
-
-	# 5.13
-	LC_TASK_STRUCT_HAS_NEW_STATE
 
 	# 5.15
 	LC_HAVE_GET_ACL_RCU_ARG