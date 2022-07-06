lustre/lustre-release

commit e6d1968fbbad5971b60e6c09a02d121c31d37d59

LU-13903 build: Move GLIBC/openssl checks to where needed.
Two config checks on glibs support:
	LC_GLIBC_SUPPORT_FHANDLES
	LC_GLIBC_SUPPORT_COPY_FILE_RANGE
and two on openssl support:
	LC_OPENSSL_SSK
	LC_OPENSSL_GETSEPOL

are currently only run when modules are being built.
The FHANDLES test is needed when building tests.
The COPY_FILE_RANGE test is needed when building
utils as are the OPENSSL checks

So move the calls to these tests to a more appropriate place, so that
  ./configure --disable-modules --disable-server
can run correctly.

Test-Parameters: trivial
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: Id7801112cd53601b3d560119784cbd062bf9610e
Reviewed-on: https://review.whamcloud.com/39653
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

config/lustre-build.m4
@@ -663,8 +663,14 @@ LC_CONFIG_CRYPTO
 # Tests depends from utils (multiop from liblustreapi)
 AS_IF([test "x$enable_utils" = xno], [enable_tests="no"])
 
+AS_IF([test "x$enable_utils" = xyes], [
+	LC_GLIBC_SUPPORT_COPY_FILE_RANGE
+	LC_OPENSSL_SSK
+	LC_OPENSSL_GETSEPOL
+])
 AS_IF([test "x$enable_tests" = xyes], [
 	LC_HAVE_LIBAIO
+	LC_GLIBC_SUPPORT_FHANDLES
 ])
 
 LIBCFS_CONFIG_CDEBUG

lustre/autoconf/lustre-core.m4
@@ -2359,11 +2359,6 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_CONFIG_FHANDLE
 	LC_CONFIG_GSS
 
-	LC_GLIBC_SUPPORT_FHANDLES
-	LC_GLIBC_SUPPORT_COPY_FILE_RANGE
-	LC_OPENSSL_SSK
-	LC_OPENSSL_GETSEPOL
-
 	# 3.10
 	LC_HAVE_PROJECT_QUOTA
 

commit 85a6eebeca1e1efb5bb9c3e737ab1978f8aca741

LU-15652 build: On Debian detect -common kernel headers
Check for a matching /usr/src/linux-headers-<ver>-common/
and update the --with-linux argument accordingly.

Also move LC_GLIBC_SUPPORT_COPY_FILE_RANGE outside
of utils as this also breaks the dkms build on Debian
with 'static' follows 'non static' declairation of
copy_file_range.

Fixes: e6d1968fbbad ("LU-13903 build: Move GLIBC/openssl checks to where needed")
HPE-bug-id: LUS-10826
Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I6e4f0b27eba6c5b07cda14f064e57aa9c93ae3cc
Reviewed-on: https://review.whamcloud.com/46836
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-linux.m4
@@ -262,7 +262,6 @@ AC_ARG_WITH([linux],
 	DEFAULT_LINUX_OBJ=$LINUX],
 	[LINUX=$DEFAULT_LINUX])
 AC_MSG_RESULT([$LINUX])
-AC_SUBST(LINUX)
 
 # -------- check for linux --------
 LB_CHECK_FILE([$LINUX], [],
@@ -276,8 +275,21 @@ AC_ARG_WITH([linux-obj],
 	[LB_ARG_CANON_PATH([linux-obj], [LINUX_OBJ])],
 	[LINUX_OBJ=$DEFAULT_LINUX_OBJ])
 AC_MSG_RESULT([$LINUX_OBJ])
-AC_SUBST(LINUX_OBJ)
 
+## -------- with linux should point to <kernel>-common on Debian
+AS_IF([test ${LINUX} == ${LINUX_OBJ} -a ${LINUX} == $(realpath ${LINUX})],[
+	this_arch=$(realpath ${LINUX} | sed 's/-/\n/g' | tail -1)
+	linux_headers_common=$(realpath ${LINUX}|sed "s/-${this_arch}\$/-common/g")
+	AS_IF([test "${this_arch}" != common],[
+		_cah="${linux_headers_common}/include/linux/compiler_attributes.h"
+		_cgh="${linux_headers_common}/include/linux/compiler-gcc.h"
+		AS_IF([test -f "${_cah}" -o -f "${_cgh}"],[
+			AC_MSG_WARN([Setting LINUX to ${linux_headers_common} was ${LINUX}])
+			LINUX=${linux_headers_common}])
+		])
+	])
+AC_SUBST(LINUX)
+AC_SUBST(LINUX_OBJ)
 # -------- check for .config --------
 AC_ARG_WITH([linux-config],
 	[AS_HELP_STRING([--with-linux-config=path],

config/lustre-build.m4
@@ -676,12 +676,12 @@ LC_CONFIG_CLIENT
 LB_CONFIG_MPITESTS
 LB_CONFIG_SERVERS
 LC_CONFIG_CRYPTO
+LC_GLIBC_SUPPORT_COPY_FILE_RANGE
 
 # Tests depends from utils (multiop from liblustreapi)
 AS_IF([test "x$enable_utils" = xno], [enable_tests="no"])
 
 AS_IF([test "x$enable_utils" = xyes], [
-	LC_GLIBC_SUPPORT_COPY_FILE_RANGE
 	LC_OPENSSL_SSK
 	LC_OPENSSL_GETSEPOL
 	LC_FID2PATH_ANON_UNION