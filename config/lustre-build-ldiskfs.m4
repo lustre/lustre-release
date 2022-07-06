lustre/lustre-release

commit c93a3e5b15b72afc1eeb3f7754c15e078d098769

LU-14195 ldiskfs: update patches for Linux 5.10
Mostly simple conflicts due to code movement, however:

ext4-data-in-dirent.patch now needs to patch fs/ext4/fast-commit.c as
well as ext4_init_new_dir() is used in that file. Since fast commit
can break recovery we prevent mounting with this option.

Test-Parameters: trivial
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I59b10fdb6bb606b193472e3045ab7d9b1d0d36b5
Reviewed-on: https://review.whamcloud.com/40913
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-ldiskfs.m4
@@ -136,7 +136,11 @@ AS_IF([test -z "$LDISKFS_SERIES"],
 	    [AS_VERSION_COMPARE([$LINUXRELEASE],[5.9.0],
 	      [LDISKFS_SERIES="5.8.0-ml.series"],  # lt
 	      [LDISKFS_SERIES="5.9.0-ml.series"],  # eq
-	      [LDISKFS_SERIES="5.9.0-ml.series"],  # gt
+	      [AS_VERSION_COMPARE([$LINUXRELEASE],[5.10.0],
+	        [LDISKFS_SERIES="5.9.0-ml.series"],  # lt
+	        [LDISKFS_SERIES="5.10.0-ml.series"],  # eq
+	        [LDISKFS_SERIES="5.10.0-ml.series"],  # gt
+	      )]
 	    )]
 	  )]
 		)])

lustre/osd-ldiskfs/osd_internal.h
@@ -1555,7 +1555,14 @@ osd_index_backup(const struct lu_env *env, struct osd_device *osd, bool backup)
 # define ldiskfs_has_feature_project(sb) \
 	LDISKFS_HAS_RO_COMPAT_FEATURE(sb, LDISKFS_FEATURE_RO_COMPAT_PROJECT)
 # endif
+# ifdef LDISKFS_FEATURE_COMPAT_FAST_COMMIT
+# define ldiskfs_has_feature_fast_commit(sb) \
+	LDISKFS_HAS_INCOMPAT_FEATURE(sb, LDISKFS_FEATURE_COMPAT_FAST_COMMIT)
+# endif
+#endif
 
+#ifndef ldiskfs_has_feature_fast_commit
+#define ldiskfs_has_feature_fast_commit(sb)  false
 #endif
 
 int osd_trunc_lock(struct osd_object *obj, struct osd_thandle *oh,

ldiskfs/kernel_patches/patches/linux-5.10/ext4-attach-jinode-in-writepages.patch
@@ -1,0 +1,51 @@
+From 01da8ce642e08594db95d940b3352ad7ee153b09 Mon Sep 17 00:00:00 2001
+From: Shaun Tancheff <stancheff@cray.com>
+Date: Tue, 6 Aug 2019 17:11:57 -0500
+Subject: [PATCH] + linux-5.3/ext4-attach-jinode-in-writepages
+
+---
+ fs/ext4/ext4.h  |    1 +
+ fs/ext4/inode.c |    8 ++++++++
+ 2 files changed, 9 insertions(+)
+
+--- a/fs/ext4/ext4.h
++++ b/fs/ext4/ext4.h
+@@ -2972,6 +2972,7 @@ extern void ext4_mb_mark_bb(struct super
+ 		       int len, int state);
+ 
+ /* inode.c */
++#define HAVE_LDISKFS_INFO_JINODE
+ void ext4_inode_csum_set(struct inode *inode, struct ext4_inode *raw,
+ 			 struct ext4_inode_info *ei);
+ int ext4_inode_is_fast_symlink(struct inode *inode);
+--- a/fs/ext4/inode.c
++++ b/fs/ext4/inode.c
+@@ -723,6 +723,10 @@ out_sem:
+ 				(loff_t)map->m_lblk << inode->i_blkbits;
+ 			loff_t length = (loff_t)map->m_len << inode->i_blkbits;
+ 
++			ret = ext4_inode_attach_jinode(inode);
++			if (ret)
++				return ret;
++
+ 			if (flags & EXT4_GET_BLOCKS_IO_SUBMIT)
+ 				ret = ext4_jbd2_inode_add_wait(handle, inode,
+ 						start_byte, length);
+@@ -2725,6 +2729,9 @@ static int ext4_writepages(struct addres
+ 		mpd.last_page = wbc->range_end >> PAGE_SHIFT;
+ 	}
+ 
++	ret = ext4_inode_attach_jinode(inode);
++	if (ret)
++		goto out_writepages;
+ 	mpd.inode = inode;
+ 	mpd.wbc = wbc;
+ 	ext4_io_submit_init(&mpd.io_submit, wbc);
+@@ -4156,6 +4163,7 @@ int ext4_inode_attach_jinode(struct inod
+ 		jbd2_free_inode(jinode);
+ 	return 0;
+ }
++EXPORT_SYMBOL(ext4_inode_attach_jinode);
+ 
+ /*
+  * ext4_truncate()

ldiskfs/kernel_patches/patches/linux-5.10/ext4-give-warning-with-dir-htree-growing.patch
@@ -1,0 +1,155 @@
+---
+ fs/ext4/ext4.h  |    1 
+ fs/ext4/namei.c |   68 ++++++++++++++++++++++++++++++++++++++++++++++++++++++--
+ fs/ext4/super.c |    2 +
+ fs/ext4/sysfs.c |    2 +
+ 4 files changed, 71 insertions(+), 2 deletions(-)
+
+--- a/fs/ext4/ext4.h
++++ b/fs/ext4/ext4.h
+@@ -1547,6 +1547,7 @@ struct ext4_sb_info {
+ 	unsigned int s_mb_group_prealloc;
+ 	unsigned int s_mb_max_inode_prealloc;
+ 	unsigned int s_max_dir_size_kb;
++	unsigned long s_warning_dir_size;
+ 	/* where last allocation was done - for stream allocation */
+ 	unsigned long s_mb_last_group;
+ 	unsigned long s_mb_last_start;
+--- a/fs/ext4/namei.c
++++ b/fs/ext4/namei.c
+@@ -775,12 +775,20 @@ struct ext4_dir_lock_data {
+ #define ext4_htree_lock_data(l)	((struct ext4_dir_lock_data *)(l)->lk_private)
+ #define ext4_find_entry(dir, name, dirent, inline) \
+ 			ext4_find_entry_locked(dir, name, dirent, inline, NULL)
+-#define ext4_add_entry(handle, dentry, inode) \
+-			ext4_add_entry_locked(handle, dentry, inode, NULL)
+ 
+ /* NB: ext4_lblk_t is 32 bits so we use high bits to identify invalid blk */
+ #define EXT4_HTREE_NODE_CHANGED	(0xcafeULL << 32)
+ 
++inline int ext4_add_entry(handle_t *handle, struct dentry *dentry,
++			  struct inode *inode)
++{
++	int ret = ext4_add_entry_locked(handle, dentry, inode, NULL);
++
++	if (ret == -ENOBUFS)
++		ret = 0;
++	return ret;
++}
++
+ static void ext4_htree_event_cb(void *target, void *event)
+ {
+ 	u64 *block = (u64 *)target;
+@@ -2657,6 +2665,54 @@ out:
+ 	return err;
+ }
+ 
++static unsigned long __ext4_max_dir_size(struct dx_frame *frames,
++			       struct dx_frame *frame, struct inode *dir)
++{
++	unsigned long max_dir_size;
++
++	if (EXT4_SB(dir->i_sb)->s_max_dir_size_kb) {
++		max_dir_size = EXT4_SB(dir->i_sb)->s_max_dir_size_kb << 10;
++	} else {
++		max_dir_size = EXT4_BLOCK_SIZE(dir->i_sb);
++		while (frame >= frames) {
++			max_dir_size *= dx_get_limit(frame->entries);
++			if (frame == frames)
++				break;
++			frame--;
++		}
++		/* use 75% of max dir size in average */
++		max_dir_size = max_dir_size / 4 * 3;
++	}
++	return max_dir_size;
++}
++
++/*
++ * With hash tree growing, it is easy to hit ENOSPC, but it is hard
++ * to predict when it will happen. let's give administrators warning
++ * when reaching 3/5 and 2/3 of limit
++ */
++static inline bool dir_size_in_warning_range(struct dx_frame *frames,
++					     struct dx_frame *frame,
++					     struct inode *dir)
++{
++	unsigned long size1, size2;
++	struct super_block *sb = dir->i_sb;
++
++	if (unlikely(!EXT4_SB(sb)->s_warning_dir_size))
++		EXT4_SB(sb)->s_warning_dir_size =
++			__ext4_max_dir_size(frames, frame, dir);
++
++	size1 = EXT4_SB(sb)->s_warning_dir_size / 16 * 10;
++	size1 = size1 & ~(EXT4_BLOCK_SIZE(sb) - 1);
++	size2 = EXT4_SB(sb)->s_warning_dir_size / 16 * 11;
++	size2 = size2 & ~(EXT4_BLOCK_SIZE(sb) - 1);
++	if (in_range(dir->i_size, size1, EXT4_BLOCK_SIZE(sb)) ||
++	    in_range(dir->i_size, size2, EXT4_BLOCK_SIZE(sb)))
++		return true;
++
++	return false;
++}
++
+ /*
+  *	ext4_add_entry()
+  *
+@@ -2796,6 +2852,7 @@ static int ext4_dx_add_entry(handle_t *h
+ 	struct ext4_dir_entry_2 *de;
+ 	int restart;
+ 	int err;
++	bool ret_warn = false;
+ 
+ again:
+ 	restart = 0;
+@@ -2824,6 +2881,11 @@ again:
+ 	/* Block full, should compress but for now just split */
+ 	dxtrace(printk(KERN_DEBUG "using %u of %u node entries\n",
+ 		       dx_get_count(entries), dx_get_limit(entries)));
++
++	if (frame - frames + 1 >= ext4_dir_htree_level(sb) ||
++	    EXT4_SB(sb)->s_warning_dir_size)
++		ret_warn = dir_size_in_warning_range(frames, frame, dir);
++
+ 	/* Need to split index? */
+ 	if (dx_get_count(entries) == dx_get_limit(entries)) {
+ 		ext4_lblk_t newblock;
+@@ -2987,6 +3049,8 @@ cleanup:
+ 	 */
+ 	if (restart && err == 0)
+ 		goto again;
++	if (err == 0 && ret_warn)
++		err = -ENOBUFS;
+ 	return err;
+ }
+ 
+--- a/fs/ext4/super.c
++++ b/fs/ext4/super.c
+@@ -2243,6 +2243,8 @@ static int handle_mount_opt(struct super
+ 		sbi->s_li_wait_mult = arg;
+ 	} else if (token == Opt_max_dir_size_kb) {
+ 		sbi->s_max_dir_size_kb = arg;
++		/* reset s_warning_dir_size and make it re-calculated */
++		sbi->s_warning_dir_size = 0;
+ #ifdef CONFIG_EXT4_DEBUG
+ 	} else if (token == Opt_fc_debug_max_replay) {
+ 		sbi->s_fc_debug_max_replay = arg;
+--- a/fs/ext4/sysfs.c
++++ b/fs/ext4/sysfs.c
+@@ -214,6 +214,7 @@ EXT4_ATTR_OFFSET(inode_readahead_blks, 0
+ EXT4_RW_ATTR_SBI_UI(inode_goal, s_inode_goal);
+ EXT4_RW_ATTR_SBI_UI(max_dir_size, s_max_dir_size_kb);
+ EXT4_RW_ATTR_SBI_UI(max_dir_size_kb, s_max_dir_size_kb);
++EXT4_RW_ATTR_SBI_UI(warning_dir_size, s_warning_dir_size);
+ EXT4_RW_ATTR_SBI_UI(mb_stats, s_mb_stats);
+ EXT4_RW_ATTR_SBI_UI(mb_max_to_scan, s_mb_max_to_scan);
+ EXT4_RW_ATTR_SBI_UI(mb_min_to_scan, s_mb_min_to_scan);
+@@ -264,6 +265,7 @@ static struct attribute *ext4_attrs[] =
+ 	ATTR_LIST(inode_goal),
+ 	ATTR_LIST(max_dir_size),
+ 	ATTR_LIST(max_dir_size_kb),
++	ATTR_LIST(warning_dir_size),
+ 	ATTR_LIST(mb_stats),
+ 	ATTR_LIST(mb_max_to_scan),
+ 	ATTR_LIST(mb_min_to_scan),

ldiskfs/kernel_patches/patches/linux-5.10/ext4-misc.patch
@@ -1,0 +1,193 @@
+---
+ fs/ext4/ext4.h   |   29 ++++++++++++++++++++++-
+ fs/ext4/ialloc.c |    3 ++-
+ fs/ext4/inode.c  |   15 +++++++++++++++
+ fs/ext4/namei.c  |    9 ++++++---
+ fs/ext4/super.c  |   10 ++--------
+ 5 files changed, 47 insertions(+), 13 deletions(-)
+
+diff -ur a/fs/ext4/ext4.h b/fs/ext4/ext4.h
+--- a/fs/ext4/ext4.h	2021-12-09 07:30:00.134965109 -0700
++++ b/fs/ext4/ext4.h	2021-12-09 07:32:16.038650421 -0700
+@@ -1849,6 +1849,8 @@
+ 
+ #define NEXT_ORPHAN(inode) EXT4_I(inode)->i_dtime
+ 
++#define JOURNAL_START_HAS_3ARGS	1
++
+ /*
+  * Codes for operating systems
+  */
+@@ -2089,7 +2091,21 @@
+ 
+ EXTN_FEATURE_FUNCS(2)
+ EXTN_FEATURE_FUNCS(3)
+-EXTN_FEATURE_FUNCS(4)
++static inline bool ext4_has_unknown_ext4_compat_features(struct super_block *sb)
++{
++	return ((EXT4_SB(sb)->s_es->s_feature_compat &
++		cpu_to_le32(~EXT4_FEATURE_COMPAT_SUPP)) != 0);
++}
++static inline bool ext4_has_unknown_ext4_ro_compat_features(struct super_block *sb)
++{
++	return ((EXT4_SB(sb)->s_es->s_feature_ro_compat &
++		cpu_to_le32(~EXT4_FEATURE_RO_COMPAT_SUPP)) != 0);
++}
++static inline bool ext4_has_unknown_ext4_incompat_features(struct super_block *sb)
++{
++	return ((EXT4_SB(sb)->s_es->s_feature_incompat &
++		cpu_to_le32(~EXT4_FEATURE_INCOMPAT_SUPP)) != 0);
++}
+ 
+ static inline bool ext4_has_compat_features(struct super_block *sb)
+ {
+@@ -3554,6 +3575,11 @@
+ #define EXT_MAX_BLOCKS	0xffffffff
+ 
+ extern void ext4_ext_tree_init(handle_t *handle, struct inode *inode);
++extern struct buffer_head *ext4_read_inode_bitmap(struct super_block *sb,
++						  ext4_group_t block_group);
++extern struct buffer_head *ext4_append(handle_t *handle,
++				       struct inode *inode,
++				       ext4_lblk_t *block);
+ extern int ext4_ext_index_trans_blocks(struct inode *inode, int extents);
+ extern int ext4_ext_map_blocks(handle_t *handle, struct inode *inode,
+ 			       struct ext4_map_blocks *map, int flags);
+diff -ur a/fs/ext4/ialloc.c b/fs/ext4/ialloc.c
+--- a/fs/ext4/ialloc.c	2021-12-09 07:30:00.086965299 -0700
++++ b/fs/ext4/ialloc.c	2021-12-09 07:30:47.418778395 -0700
+@@ -120,7 +120,7 @@
+  *
+  * Return buffer_head of bitmap on success, or an ERR_PTR on error.
+  */
+-static struct buffer_head *
++struct buffer_head *
+ ext4_read_inode_bitmap(struct super_block *sb, ext4_group_t block_group)
+ {
+ 	struct ext4_group_desc *desc;
+@@ -215,6 +215,7 @@
+ 	put_bh(bh);
+ 	return ERR_PTR(err);
+ }
++EXPORT_SYMBOL(ext4_read_inode_bitmap);
+ 
+ /*
+  * NOTE! When we get the inode, we're the only people
+diff -ur a/fs/ext4/inode.c b/fs/ext4/inode.c
+--- a/fs/ext4/inode.c	2021-12-09 07:30:00.126965141 -0700
++++ b/fs/ext4/inode.c	2021-12-09 07:30:47.422778379 -0700
+@@ -6197,3 +6197,19 @@
+ 
+ 	return ret;
+ }
++EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_truncate);
++EXPORT_SYMBOL(ext4_iget);
++EXPORT_SYMBOL(ext4_bread);
++EXPORT_SYMBOL(ext4_itable_unused_count);
++EXPORT_SYMBOL(ext4_force_commit);
++EXPORT_SYMBOL(__ext4_mark_inode_dirty);
++EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_write_access);
++EXPORT_SYMBOL(__ext4_journal_start_sb);
++EXPORT_SYMBOL(__ext4_journal_stop);
++EXPORT_SYMBOL(__ext4_handle_dirty_metadata);
++EXPORT_SYMBOL(__ext4_std_error);
++EXPORT_SYMBOL(ext4fs_dirhash);
++EXPORT_SYMBOL(ext4_get_inode_loc);
++EXPORT_SYMBOL(__ext4_journal_ensure_credits);
+diff -ur a/fs/ext4/namei.c b/fs/ext4/namei.c
+--- a/fs/ext4/namei.c	2021-12-09 07:30:00.138965094 -0700
++++ b/fs/ext4/namei.c	2021-12-09 07:30:47.426778363 -0700
+@@ -50,7 +50,7 @@
+ #define NAMEI_RA_BLOCKS  4
+ #define NAMEI_RA_SIZE	     (NAMEI_RA_CHUNKS * NAMEI_RA_BLOCKS)
+ 
+-static struct buffer_head *ext4_append(handle_t *handle,
++struct buffer_head *ext4_append(handle_t *handle,
+ 					struct inode *inode,
+ 					ext4_lblk_t *block)
+ {
+@@ -181,6 +181,7 @@
+ 	}
+ 	return bh;
+ }
++EXPORT_SYMBOL(ext4_append);
+ 
+ #ifndef assert
+ #define assert(test) J_ASSERT(test)
+@@ -2575,23 +2576,25 @@
+  * for checking S_ISDIR(inode) (since the INODE_INDEX feature will not be set
+  * on regular files) and to avoid creating huge/slow non-HTREE directories.
+  */
+-static void ext4_inc_count(struct inode *inode)
++void ext4_inc_count(struct inode *inode)
+ {
+ 	inc_nlink(inode);
+ 	if (is_dx(inode) &&
+ 	    (inode->i_nlink > EXT4_LINK_MAX || inode->i_nlink == 2))
+ 		set_nlink(inode, 1);
+ }
++EXPORT_SYMBOL(ext4_inc_count);
+ 
+ /*
+  * If a directory had nlink == 1, then we should let it be 1. This indicates
+  * directory has >EXT4_LINK_MAX subdirs.
+  */
+-static void ext4_dec_count(struct inode *inode)
++void ext4_dec_count(struct inode *inode)
+ {
+ 	if (!S_ISDIR(inode->i_mode) || inode->i_nlink > 2)
+ 		drop_nlink(inode);
+ }
++EXPORT_SYMBOL(ext4_dec_count);
+ 
+ 
+ /*
+diff -ur a/fs/ext4/super.c b/fs/ext4/super.c
+--- a/fs/ext4/super.c	2021-12-09 07:30:00.050965441 -0700
++++ b/fs/ext4/super.c	2021-12-09 07:30:47.426778363 -0700
+@@ -435,7 +435,7 @@
+ 		return;
+ 	es->s_state |= cpu_to_le16(EXT4_ERROR_FS);
+ 	ext4_update_tstamp(es, s_last_error_time);
+-	strncpy(es->s_last_error_func, func, sizeof(es->s_last_error_func));
++	strlcpy(es->s_last_error_func, func, sizeof(es->s_last_error_func));
+ 	es->s_last_error_line = cpu_to_le32(line);
+ 	es->s_last_error_ino = cpu_to_le32(ino);
+ 	es->s_last_error_block = cpu_to_le64(block);
+@@ -496,7 +496,7 @@
+ 	if (!es->s_first_error_time) {
+ 		es->s_first_error_time = es->s_last_error_time;
+ 		es->s_first_error_time_hi = es->s_last_error_time_hi;
+-		strncpy(es->s_first_error_func, func,
++		strlcpy(es->s_first_error_func, func,
+ 			sizeof(es->s_first_error_func));
+ 		es->s_first_error_line = cpu_to_le32(line);
+ 		es->s_first_error_ino = es->s_last_error_ino;
+@@ -6683,16 +6683,12 @@
+ 	if (err)
+ 		goto out05;
+ 
+-	register_as_ext3();
+-	register_as_ext2();
+ 	err = register_filesystem(&ext4_fs_type);
+ 	if (err)
+ 		goto out;
+ 
+ 	return 0;
+ out:
+-	unregister_as_ext2();
+-	unregister_as_ext3();
+ out05:
+ 	destroy_inodecache();
+ out1:
+@@ -6716,8 +6712,6 @@
+ static void __exit ext4_exit_fs(void)
+ {
+ 	ext4_destroy_lazyinit_thread();
+-	unregister_as_ext2();
+-	unregister_as_ext3();
+ 	unregister_filesystem(&ext4_fs_type);
+ 	destroy_inodecache();
+ 	ext4_exit_mballoc();

ldiskfs/kernel_patches/series/ldiskfs-5.10.0-ml.series
@@ -1,0 +1,29 @@
+rhel8/ext4-inode-version.patch
+linux-5.4/ext4-lookup-dotdot.patch
+suse15/ext4-print-inum-in-htree-warning.patch
+linux-5.8/ext4-prealloc.patch
+ubuntu18/ext4-osd-iop-common.patch
+linux-5.10/ext4-misc.patch
+linux-5.8/ext4-mballoc-extra-checks.patch
+linux-5.4/ext4-hash-indexed-dir-dotdot-update.patch
+linux-5.8/ext4-kill-dx-root.patch
+linux-5.8/ext4-mballoc-pa-free-mismatch.patch
+linux-5.10/ext4-data-in-dirent.patch
+rhel8/ext4-nocmtime.patch
+base/ext4-htree-lock.patch
+linux-5.10/ext4-pdirop.patch
+linux-5.8/ext4-max-dir-size.patch
+linux-5.8/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
+linux-5.10/ext4-give-warning-with-dir-htree-growing.patch
+ubuntu18/ext4-jcb-optimization.patch
+linux-5.10/ext4-attach-jinode-in-writepages.patch
+rhel8/ext4-dont-check-before-replay.patch
+rhel7.6/ext4-use-GFP_NOFS-in-ext4_inode_attach_jinode.patch
+rhel7.6/ext4-export-orphan-add.patch
+linux-5.8/ext4-export-mb-stream-allocator-variables.patch
+ubuntu19/ext4-iget-with-flags.patch
+linux-5.4/export-ext4fs-dirhash-helper.patch
+linux-5.9/ext4-simple-blockalloc.patch
+rhel8.3/ext4-xattr-disable-credits-check.patch
+linux-5.8/ext4-no-max-dir-size-limit-for-iam-objects.patch
+rhel8/ext4-ialloc-uid-gid-and-pass-owner-down.patch

commit ed80c3f64317a87ed10919cedbf2d65b36abcdcb

LU-15703 ldiskfs: Disable unused fast commit buffer
Linux commit v5.9-rc7-39-g6866d7b3f2bb
    ext4 / jbd2: add fast commit initialization

Disable journal fast commit buffer via a mount option because it is
not used by lustre's ldiskfs since it will break recovery.

Linux commit v5.10-rc2-9-gede7dc7fa0af
    jbd2: rename j_maxlen to j_total_len and add
          jbd2_journal_max_txn_bufs

Change osd_transaction_size to use jbd2_journal_get_max_txn_bufs
and provide a jbd2_journal_get_max_txn_bufs when it is not
provided.

Test-Parameters: trivial
HPE-bug-id: LUS-10858
Fixes: c93a3e5b15 ("LU-14195 ldiskfs: update patches for Linux 5.10")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I9bffc3559a8bbce9d4c1c2b6692cb8518f3f991a
Reviewed-on: https://review.whamcloud.com/46943
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

config/lustre-build-ldiskfs.m4
@@ -487,6 +487,28 @@ AC_DEFUN([LB_EXT4_INC_DEC_COUNT_2ARGS], [
 ]) # LB_EXT4_INC_DEC_COUNT_2ARGS
 
 #
+# LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS
+# Linux commit v5.10-rc2-9-gede7dc7fa0af
+#  jbd2: rename j_maxlen to j_total_len and add jbd2_journal_max_txn_bufs
+#
+AC_DEFUN([LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if jbd2_journal_get_max_txn_bufs is available],
+jbd2_journal_get_max_txn_bufs, [
+	#include <linux/jbd2.h>
+],[
+	journal_t *journal = NULL;
+	int x = jbd2_journal_get_max_txn_bufs(journal);
+	(void)x;
+],[
+	AC_DEFINE(HAVE_JBD2_JOURNAL_GET_MAX_TXN_BUFS, 1,
+		[if jbd2_journal_get_max_txn_bufs is available])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS
+
+#
 # LB_CONFIG_LDISKFS
 #
 AC_DEFUN([LB_CONFIG_LDISKFS], [
@@ -541,6 +563,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 	LB_JBD2_H_TOTAL_CREDITS
 	LB_EXT4_GET_BLOCKS_KEEP_SIZE
 	LB_EXT4_INC_DEC_COUNT_2ARGS
+	LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS
 	AC_DEFINE(CONFIG_LDISKFS_FS_POSIX_ACL, 1, [posix acls for ldiskfs])
 	AC_DEFINE(CONFIG_LDISKFS_FS_SECURITY, 1, [fs security for ldiskfs])
 	AC_DEFINE(CONFIG_LDISKFS_FS_XATTR, 1, [extened attributes for ldiskfs])

lustre/osd-ldiskfs/osd_internal.h
@@ -958,6 +958,11 @@ static inline void i_projid_write(struct inode *inode, __u32 projid)
 # define osd_attach_jinode(inode) 0
 #endif /* HAVE_LDISKFS_INFO_JINODE */
 
+#ifndef HAVE_JBD2_JOURNAL_GET_MAX_TXN_BUFS
+#define jbd2_journal_get_max_txn_bufs(jrnl) \
+	(jrnl->j_max_transaction_buffers)
+#endif
+
 #ifdef LDISKFS_HT_MISC
 # define osd_journal_start_sb(sb, type, nblock) \
 		ldiskfs_journal_start_sb(sb, type, nblock)
@@ -986,7 +991,7 @@ static inline struct buffer_head *osd_ldiskfs_append(handle_t *handle,
 # define osd_journal_start(inode, type, nblocks) \
 		ldiskfs_journal_start(inode, type, nblocks)
 # define osd_transaction_size(dev) \
-		(osd_journal(dev)->j_max_transaction_buffers / 2)
+		(jbd2_journal_get_max_txn_bufs(osd_journal(dev)) / 2)
 #else /* ! defined LDISKFS_HT_MISC */
 # define LDISKFS_HT_MISC	0
 # define osd_journal_start_sb(sb, type, nblock) \
@@ -1018,7 +1023,7 @@ static inline struct buffer_head *osd_ldiskfs_append(handle_t *handle,
 # define osd_journal_start(inode, type, nblocks) \
 		ldiskfs_journal_start(inode, nblocks)
 # define osd_transaction_size(dev) \
-		(osd_journal(dev)->j_max_transaction_buffers)
+		jbd2_journal_get_max_txn_bufs(osd_journal(dev))
 #endif /* LDISKFS_HT_MISC */
 
 #ifndef HAVE___LDISKFS_FIND_ENTRY

lustre/utils/libmount_utils_ldiskfs.c
@@ -540,6 +540,10 @@ static int enable_default_ext4_features(struct mkfs_opts *mop, char *anchor,
 	if (IS_MDT(&mop->mo_ldd) && is_e2fsprogs_feature_supp("-O large_dir"))
 		append_unique(anchor, ",", "large_dir", NULL, maxbuflen);
 
+	/* Disable fast_commit since it breaks ldiskfs transactions ordering */
+	if (is_e2fsprogs_feature_supp("fast_commit"))
+		append_unique(anchor, ",", "^fast_commit", NULL, maxbuflen);
+
 	/* Cluster inode/block bitmaps and inode table for more efficient IO.
 	 * Align the flex groups on a 1MB boundary for better performance. */
 	/* This -O feature needs to go last, since it adds the "-G" option. */