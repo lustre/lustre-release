lustre/lustre-release

commit 35679a730bf0b7a8d4ce84cadc3ecc7c289ef491

LU-10958 ofd: data corruption due to RPC reordering
Without read-only cache, it is possible that a client
resends a BRW RPC, receives a reply from the original
BRW RPC, modifies the same data and sends a new BRW
RPC, however, because of RPC reordering stale data
gets to disk.

Let's use range locking to protect against this race.

Change-Id: I35cbf95594601eacfc5f108b14e4c447962b0bbf
Signed-off-by: Andrew Perepechko <c17827@cray.com>
Cray-bug-id: LUS-5578,LUS-8943
Reviewed-on: https://review.whamcloud.com/32281
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_dev.c
@@ -545,6 +545,7 @@ static struct lu_object *ofd_object_alloc(const struct lu_env *env,
 		lu_object_init(o, h, d);
 		lu_object_add_top(h, o);
 		o->lo_ops = &ofd_obj_ops;
+		range_lock_tree_init(&of->ofo_write_tree);
 		RETURN(o);
 	} else {
 		RETURN(NULL);

lustre/ofd/ofd_internal.h
@@ -38,6 +38,7 @@
 #include <dt_object.h>
 #include <md_object.h>
 #include <lustre_fid.h>
+#include <range_lock.h>
 
 #define OFD_INIT_OBJID	0
 #define OFD_PRECREATE_BATCH_DEFAULT (OBJ_SUBDIR_COUNT * 4)
@@ -188,6 +189,7 @@ struct ofd_object {
 	time64_t		ofo_atime_ondisk;
 	unsigned int		ofo_pfid_checking:1,
 				ofo_pfid_verified:1;
+	struct range_lock_tree	ofo_write_tree;
 };
 
 static inline struct ofd_object *ofd_obj(struct lu_object *o)
@@ -286,6 +288,8 @@ struct ofd_thread_info {
 		struct lfsck_req_local	 fti_lrl;
 		struct obd_connect_data	 fti_ocd;
 	};
+	struct range_lock		 fti_write_range;
+	unsigned			 fti_range_locked:1;
 };
 
 extern void target_recovery_fini(struct obd_device *obd);

lustre/ofd/ofd_io.c
@@ -688,6 +688,7 @@ static int ofd_preprw_write(const struct lu_env *env, struct obd_export *exp,
 	int maxlnb = *nr_local;
 	__u64 begin, end;
 	ktime_t kstart = ktime_get();
+	struct range_lock *range = &ofd_info(env)->fti_write_range;
 
 	ENTRY;
 	LASSERT(env != NULL);
@@ -841,6 +842,26 @@ static int ofd_preprw_write(const struct lu_env *env, struct obd_export *exp,
 		tot_bytes,
 		obj->ioo_bufcnt,
 		WRITE);
+
+	/*
+	 * Reordering precautions: make sure that request processing that
+	 * was able to receive its bulk data should not get reordered with
+	 * overlapping BRW requests, e.g.
+	 *  1) BRW1 sent, bulk data received, but disk I/O delayed
+	 *  2) BRW1 resent and fully processed
+	 *  3) the page was unlocked on the client and its writeback bit reset
+	 *  4) BRW2 sent and fully processed
+	 *  5) BRW1 processing wakes up and writes stale data to disk
+	 * If on step 1 bulk data was not received, client resend will invalidate
+	 * its bulk descriptor and the RPC will be dropped due to failed bulk
+	 * transfer, which is just fine.
+	 */
+	range_lock_init(range,
+			rnb[0].rnb_offset,
+			rnb[obj->ioo_bufcnt - 1].rnb_offset +
+			rnb[obj->ioo_bufcnt - 1].rnb_len - 1);
+	range_lock(&fo->ofo_write_tree, range);
+	ofd_info(env)->fti_range_locked = 1;
 
 	ofd_counter_incr(exp, LPROC_OFD_STATS_WRITE_BYTES, jobid, tot_bytes);
 	ofd_counter_incr(exp, LPROC_OFD_STATS_WRITE, jobid,
@@ -1220,6 +1241,7 @@ ofd_commitrw_write(const struct lu_env *env, struct obd_export *exp,
 	bool soft_sync = false;
 	bool cb_registered = false;
 	bool fake_write = false;
+	struct range_lock *range = &ofd_info(env)->fti_write_range;
 
 	ENTRY;
 
@@ -1384,6 +1406,10 @@ out_stop:
 		dt_commit_async(env, ofd->ofd_osd);
 
 out:
+	if (info->fti_range_locked) {
+		range_unlock(&fo->ofo_write_tree, range);
+		info->fti_range_locked = 0;
+	}
 	dt_bufs_put(env, o, lnb, niocount);
 	ofd_object_put(env, fo);
 	if (granted > 0)

lustre/include/Makefile.am
@@ -95,6 +95,7 @@ EXTRA_DIST = \
 	obd_support.h \
 	obd_target.h \
 	obj_update.h \
+	range_lock.h \
 	upcall_cache.h \
 	lustre_kernelcomm.h \
 	seq_range.h

lustre/ldlm/Makefile.am
@@ -40,4 +40,4 @@ MOSTLYCLEANFILES := @MOSTLYCLEANFILES@
 EXTRA_DIST = ldlm_extent.c ldlm_flock.c ldlm_internal.h ldlm_lib.c \
 	ldlm_lock.c ldlm_lockd.c ldlm_plain.c ldlm_request.c	     \
 	ldlm_resource.c l_lock.c ldlm_inodebits.c ldlm_pool.c 	     \
-	interval_tree.c ldlm_reclaim.c
+	ldlm_reclaim.c

lustre/llite/Makefile.in
@@ -7,9 +7,9 @@ lustre-objs += glimpse.o
 lustre-objs += lcommon_cl.o
 lustre-objs += lcommon_misc.o
 lustre-objs += vvp_dev.o vvp_page.o vvp_io.o vvp_object.o
-lustre-objs += range_lock.o pcc.o crypto.o
+lustre-objs += pcc.o crypto.o
 
 EXTRA_DIST := $(lustre-objs:.o=.c) xattr.c rw26.c super25.c
-EXTRA_DIST += llite_internal.h vvp_internal.h range_lock.h pcc.h
+EXTRA_DIST += llite_internal.h vvp_internal.h pcc.h
 
 @INCLUDE_RULES@

lustre/llite/llite_internal.h
@@ -46,9 +46,9 @@
 #include <linux/aio.h>
 #include <lustre_compat.h>
 #include <lustre_crypto.h>
+#include <range_lock.h>
 
 #include "vvp_internal.h"
-#include "range_lock.h"
 #include "pcc.h"
 
 #ifndef FMODE_EXEC

commit a21ce928aa641cbfacb9dde74e4be1cd1b658fb0

LU-15894 ofd: revert range locking in ofd
After commit 301d76a711 (LU-14876), range locking is no longer needed
in ofd, because 301d76a711 itself prevents the original data corruption
fixed by range locking. At the same time, range locking in ofd adds
unnecessary overhead, we can even see serialization under specific load.

This patch reverts range locking but keeps recovery-small test 148
to test the original corruption scenario case.

Change-Id: Ic795bcfb1e249c4927f66b6bad456f5511819861
Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
HPE-bug-id: LUS-9890
Fixes: 35679a730 ("LU-10958 ofd: data corruption due to RPC reordering")
Reviewed-on: https://review.whamcloud.com/47466
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_dev.c
@@ -544,7 +544,6 @@ static struct lu_object *ofd_object_alloc(const struct lu_env *env,
 		lu_object_init(o, h, d);
 		lu_object_add_top(h, o);
 		o->lo_ops = &ofd_obj_ops;
-		range_lock_tree_init(&of->ofo_write_tree);
 		RETURN(o);
 	} else {
 		RETURN(NULL);

lustre/ofd/ofd_internal.h
@@ -37,7 +37,6 @@
 #include <dt_object.h>
 #include <md_object.h>
 #include <lustre_fid.h>
-#include <range_lock.h>
 
 #define OFD_INIT_OBJID	0
 #define OFD_PRECREATE_BATCH_DEFAULT (OBJ_SUBDIR_COUNT * 4)
@@ -188,7 +187,6 @@ struct ofd_object {
 	time64_t		ofo_atime_ondisk;
 	unsigned int		ofo_pfid_checking:1,
 				ofo_pfid_verified:1;
-	struct range_lock_tree	ofo_write_tree;
 };
 
 static inline struct ofd_object *ofd_obj(struct lu_object *o)
@@ -287,8 +285,6 @@ struct ofd_thread_info {
 		struct lfsck_req_local	 fti_lrl;
 		struct obd_connect_data	 fti_ocd;
 	};
-	struct range_lock		 fti_write_range;
-	unsigned			 fti_range_locked:1;
 };
 
 extern void target_recovery_fini(struct obd_device *obd);

lustre/ofd/ofd_io.c
@@ -689,7 +689,6 @@ static int ofd_preprw_write(const struct lu_env *env, struct obd_export *exp,
 	enum dt_bufs_type dbt = DT_BUFS_TYPE_WRITE;
 	int maxlnb = *nr_local;
 	__u64 begin, end;
-	struct range_lock *range = &ofd_info(env)->fti_write_range;
 
 	ENTRY;
 	LASSERT(env != NULL);
@@ -841,26 +840,6 @@ static int ofd_preprw_write(const struct lu_env *env, struct obd_export *exp,
 		tot_bytes,
 		obj->ioo_bufcnt,
 		WRITE);
-
-	/*
-	 * Reordering precautions: make sure that request processing that
-	 * was able to receive its bulk data should not get reordered with
-	 * overlapping BRW requests, e.g.
-	 *  1) BRW1 sent, bulk data received, but disk I/O delayed
-	 *  2) BRW1 resent and fully processed
-	 *  3) the page was unlocked on the client and its writeback bit reset
-	 *  4) BRW2 sent and fully processed
-	 *  5) BRW1 processing wakes up and writes stale data to disk
-	 * If on step 1 bulk data was not received, client resend will invalidate
-	 * its bulk descriptor and the RPC will be dropped due to failed bulk
-	 * transfer, which is just fine.
-	 */
-	range_lock_init(range,
-			rnb[0].rnb_offset,
-			rnb[obj->ioo_bufcnt - 1].rnb_offset +
-			rnb[obj->ioo_bufcnt - 1].rnb_len - 1);
-	range_lock(&fo->ofo_write_tree, range);
-	ofd_info(env)->fti_range_locked = 1;
 
 	RETURN(0);
 
@@ -1234,7 +1213,6 @@ ofd_commitrw_write(const struct lu_env *env, struct obd_export *exp,
 	bool soft_sync = false;
 	bool cb_registered = false;
 	bool fake_write = false;
-	struct range_lock *range = &ofd_info(env)->fti_write_range;
 
 	ENTRY;
 
@@ -1399,10 +1377,6 @@ out_stop:
 		dt_commit_async(env, ofd->ofd_osd);
 
 out:
-	if (info->fti_range_locked) {
-		range_unlock(&fo->ofo_write_tree, range);
-		info->fti_range_locked = 0;
-	}
 	dt_bufs_put(env, o, lnb, niocount);
 	ofd_object_put(env, fo);
 	if (granted > 0)