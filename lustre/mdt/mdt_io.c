lustre/lustre-release

commit bbfdc7c1670c92747a8f98d39e1e43dc39e59e30

LU-14739 quota: fix quota with root squash enabled
This patch tries to fix several problems:

1. OSD will ignore quota if IO comes from client
cache or root, however since following change:

LU-12687 osc: consume grants for direct I/O

DIO now consumes grant too, following check for
sync IO is wrong now:

(lnb[i].lnb_flags & (OBD_BRW_FROM_GRANT | OBD_BRW_SYNC))
        == OBD_BRW_FROM_GRANT)

This wass originally added to support 1.8 client, it is
going to be 2.15 now, so let's remove this broken check.

2. Server side will clear OBD_BRW_NOQUOTA if root squash
is enabled, this will revert fixes from:

"LU-13228 clio: mmap write when overquota"

We need to separate @ci_noquota and @oi_cap_sys_resource cases,
introduce a new flag OBD_BRW_SYS_RESOURCE, and extend test_75
to cover this case.

3. LU-14739 missed case that DoM quota should be considered
as well.

4. If EDQUOT is returned for root, we check the new root squash
flag OBD_FL_ROOT_SQUASH from server side. If this flag is not set,
we bypass quota for root, otherwise all root writes become sync
writes.

5. Fix a leftover problem with LU-9671 for DOM

Fixes: a4fbe7341baf12 ("LU-14739 quota: nodemap squashed root cannot bypass quota")
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Signed-off-by: Wang Shilong <wangshilong1991@gmail.com>
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I3fd23da7d56acb5b485540333208e5d5b0b48023
Reviewed-on: https://review.whamcloud.com/44347
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_io.c
@@ -32,6 +32,7 @@
 
 #include <dt_object.h>
 #include <linux/falloc.h>
+#include <lustre_nodemap.h>
 
 #include "mdt_internal.h"
 
@@ -760,10 +761,35 @@ int mdt_obd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 	struct lu_attr *la = &info->mti_attr.ma_attr;
 	__u64 valid;
 	int rc = 0;
+	int root_squash = 0;
 
 	LASSERT(mo);
 
 	if (cmd == OBD_BRW_WRITE) {
+		struct lu_nodemap *nodemap;
+		__u32 mapped_uid, mapped_gid;
+
+		nodemap = nodemap_get_from_exp(exp);
+		if (IS_ERR(nodemap))
+			RETURN(PTR_ERR(nodemap));
+		mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
+					    NODEMAP_FS_TO_CLIENT,
+					    oa->o_uid);
+		mapped_gid = nodemap_map_id(nodemap, NODEMAP_GID,
+					    NODEMAP_FS_TO_CLIENT,
+					    oa->o_gid);
+		if (!IS_ERR_OR_NULL(nodemap)) {
+			/* do not bypass quota enforcement if squashed uid */
+			if (unlikely(mapped_uid == nodemap->nm_squash_uid)) {
+				int idx;
+
+				for (idx = 0; idx < npages; idx++)
+					lnb[idx].lnb_flags &=
+						~OBD_BRW_SYS_RESOURCE;
+				root_squash = 1;
+			}
+			nodemap_putref(nodemap);
+		}
 		/* Don't update timestamps if this write is older than a
 		 * setattr which modifies the timestamps. b=10150 */
 
@@ -812,9 +838,18 @@ int mdt_obd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 					oa->o_flags = OBD_FL_NO_PRJQUOTA;
 			}
 
+			if (root_squash)
+				oa->o_flags |= OBD_FL_ROOT_SQUASH;
+
 			oa->o_valid |= OBD_MD_FLFLAGS | OBD_MD_FLUSRQUOTA |
 				       OBD_MD_FLGRPQUOTA | OBD_MD_FLPRJQUOTA;
 		}
+		/* Convert back to client IDs. LU-9671.
+		 * nodemap_get_from_exp() may fail due to nodemap deactivated,
+		 * server ID will be returned back to client in that case.
+		 */
+		oa->o_uid = mapped_uid;
+		oa->o_gid = mapped_gid;
 	} else if (cmd == OBD_BRW_READ) {
 		/* If oa != NULL then mdt_preprw_read updated the inode
 		 * atime and we should update the lvb so that other glimpses

lustre/ofd/ofd_io.c
@@ -1456,6 +1456,7 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 	struct ldlm_resource *rs = NULL;
 	__u64 valid;
 	int rc = 0;
+	int root_squash = 0;
 
 	LASSERT(npages > 0);
 
@@ -1464,6 +1465,8 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 		__u32 mapped_uid, mapped_gid;
 
 		nodemap = nodemap_get_from_exp(exp);
+		if (IS_ERR(nodemap))
+			RETURN(PTR_ERR(nodemap));
 		mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
 					    NODEMAP_FS_TO_CLIENT,
 					    oa->o_uid);
@@ -1477,7 +1480,9 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 				int idx;
 
 				for (idx = 0; idx < npages; idx++)
-					lnb[idx].lnb_flags &= ~OBD_BRW_NOQUOTA;
+					lnb[idx].lnb_flags &=
+						~OBD_BRW_SYS_RESOURCE;
+				root_squash = 1;
 			}
 			nodemap_putref(nodemap);
 		}
@@ -1520,6 +1525,9 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 				else
 					oa->o_flags = OBD_FL_NO_PRJQUOTA;
 			}
+
+			if (root_squash)
+				oa->o_flags |= OBD_FL_ROOT_SQUASH;
 
 			oa->o_valid |= OBD_MD_FLFLAGS;
 			oa->o_valid |= OBD_MD_FLALLQUOTA;

lustre/include/obd.h
@@ -239,6 +239,8 @@ struct client_obd {
 	struct list_head	cl_grant_chain;
 	time64_t		cl_grant_shrink_interval; /* seconds */
 
+	int			cl_root_squash; /* if root squash enabled*/
+
 	/* A chunk is an optimal size used by osc_extent to determine
 	 * the extent size. A chunk is max(PAGE_SIZE, OST block size) */
 	int			cl_chunkbits;

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1099,6 +1099,7 @@ enum obdo_flags {
         OBD_FL_NOSPC_BLK    = 0x00100000, /* no more block space on OST */
 	OBD_FL_FLUSH	    = 0x00200000, /* flush pages on the OST */
 	OBD_FL_SHORT_IO	    = 0x00400000, /* short io request */
+	OBD_FL_ROOT_SQUASH  = 0x00800000, /* root squash */
 	/* OBD_FL_LOCAL_MASK = 0xF0000000, was local-only flags until 2.10 */
 
 	/*
@@ -1380,7 +1381,7 @@ struct hsm_state_set {
 #define OBD_BRW_GRANTED         0x40 /* the ost manages this */
 /* OBD_BRW_NOCACHE is currently neither set nor tested */
 #define OBD_BRW_NOCACHE         0x80 /* this page is a part of non-cached IO */
-#define OBD_BRW_NOQUOTA        0x100
+#define OBD_BRW_NOQUOTA        0x100 /* do not enforce quota */
 #define OBD_BRW_SRVLOCK        0x200 /* Client holds no lock over this page */
 #define OBD_BRW_ASYNC          0x400 /* Server may delay commit to disk */
 #define OBD_BRW_MEMALLOC       0x800 /* Client runs in the "kswapd" context */
@@ -1392,6 +1393,7 @@ struct hsm_state_set {
 				      * it to sync quickly */
 #define OBD_BRW_OVER_PRJQUOTA 0x8000 /* Running out of project quota */
 #define OBD_BRW_RDMA_ONLY    0x20000 /* RPC contains RDMA-only pages*/
+#define OBD_BRW_SYS_RESOURCE 0x40000 /* page has CAP_SYS_RESOURCE */
 
 #define OBD_BRW_OVER_ALLQUOTA (OBD_BRW_OVER_USRQUOTA | \
 			       OBD_BRW_OVER_GRPQUOTA | \

lustre/osc/osc_cache.c
@@ -2304,9 +2304,14 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 
 	/* Set the OBD_BRW_SRVLOCK before the page is queued. */
 	brw_flags |= ops->ops_srvlock ? OBD_BRW_SRVLOCK : 0;
-	if (oio->oi_cap_sys_resource || io->ci_noquota) {
+	if (io->ci_noquota) {
 		brw_flags |= OBD_BRW_NOQUOTA;
 		cmd |= OBD_BRW_NOQUOTA;
+	}
+
+	if (oio->oi_cap_sys_resource) {
+		brw_flags |= OBD_BRW_SYS_RESOURCE;
+		cmd |= OBD_BRW_SYS_RESOURCE;
 	}
 
 	/* check if the file's owner/group is over quota */
@@ -2325,8 +2330,20 @@ int osc_queue_async_io(const struct lu_env *env, struct cl_io *io,
 		qid[USRQUOTA] = attr->cat_uid;
 		qid[GRPQUOTA] = attr->cat_gid;
 		qid[PRJQUOTA] = attr->cat_projid;
-		if (rc == 0 && osc_quota_chkdq(cli, qid) == -EDQUOT)
-			rc = -EDQUOT;
+		/*
+		 * if EDQUOT returned for root, we double check
+		 * if root squash enabled or not updated from server side.
+		 * without root squash, we should bypass quota for root.
+		 */
+		if (rc == 0 && osc_quota_chkdq(cli, qid) == -EDQUOT) {
+			if (oio->oi_cap_sys_resource &&
+			    !cli->cl_root_squash) {
+				io->ci_noquota = 1;
+				rc = 0;
+			} else {
+				rc = -EDQUOT;
+			}
+		}
 		if (rc)
 			RETURN(rc);
 	}

lustre/osc/osc_page.c
@@ -317,8 +317,8 @@ void osc_page_submit(const struct lu_env *env, struct osc_page *opg,
 	oap->oap_brw_flags = OBD_BRW_SYNC | brw_flags;
 
 	if (oio->oi_cap_sys_resource) {
-		oap->oap_brw_flags |= OBD_BRW_NOQUOTA;
-		oap->oap_cmd |= OBD_BRW_NOQUOTA;
+		oap->oap_brw_flags |= OBD_BRW_SYS_RESOURCE;
+		oap->oap_cmd |= OBD_BRW_SYS_RESOURCE;
 	}
 
 	opg->ops_submit_time = submit_time;

lustre/osc/osc_quota.c
@@ -108,6 +108,7 @@ int osc_quota_setdq(struct client_obd *cli, __u64 xid, const unsigned int qid[],
 		RETURN(0);
 
 	mutex_lock(&cli->cl_quota_mutex);
+	cli->cl_root_squash = !!(flags & OBD_FL_ROOT_SQUASH);
 	/* still mark the quots is running out for the old request, because it
 	 * could be processed after the new request at OST, the side effect is
 	 * the following request will be processed synchronously, but it will

lustre/osc/osc_request.c
@@ -1158,7 +1158,8 @@ static inline int can_merge_pages(struct brw_page *p1, struct brw_page *p2)
         if (p1->flag != p2->flag) {
 		unsigned mask = ~(OBD_BRW_FROM_GRANT | OBD_BRW_NOCACHE |
 				  OBD_BRW_SYNC       | OBD_BRW_ASYNC   |
-				  OBD_BRW_NOQUOTA    | OBD_BRW_SOFT_SYNC);
+				  OBD_BRW_NOQUOTA    | OBD_BRW_SOFT_SYNC |
+				  OBD_BRW_SYS_RESOURCE);
 
                 /* warn if we try to combine flags that we don't know to be
                  * safe to combine */
@@ -1974,8 +1975,8 @@ static int osc_brw_fini_request(struct ptlrpc_request *req, int rc)
 		       "setdq for [%u %u %u] with valid %#llx, flags %x\n",
 		       body->oa.o_uid, body->oa.o_gid, body->oa.o_projid,
 		       body->oa.o_valid, body->oa.o_flags);
-		       osc_quota_setdq(cli, req->rq_xid, qid, body->oa.o_valid,
-				       body->oa.o_flags);
+		osc_quota_setdq(cli, req->rq_xid, qid, body->oa.o_valid,
+				body->oa.o_flags);
 	}
 
 	osc_update_grant(cli, body);
@@ -3512,6 +3513,7 @@ int osc_setup_common(struct obd_device *obd, struct lustre_cfg *lcfg)
 		GOTO(out_ptlrpcd_work, rc);
 
 	cli->cl_grant_shrink_interval = GRANT_SHRINK_INTERVAL;
+	cli->cl_root_squash = 0;
 	osc_update_next_shrink(cli);
 
 	RETURN(rc);

commit ee4b50278e4471b7c64e619e13699c560502b81e

LU-15993 ofd: don't leak pages if nodemap fails
ofd_commitrw() shouldn't exit w/o calling ofd_commitrw_write(),
otherwise the pages taken in ofd_preprw() are leaked.

same in mdt_obd_commitrw()

Fixes: bbfdc7c167 ("LU-14739 quota: fix quota with root squash enabled")

Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Icd60c7ab80c5a7b65603d7da0d2e83872dc6b97f
Reviewed-on: https://review.whamcloud.com/47873
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_io.c
@@ -761,18 +761,24 @@ int mdt_obd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 		struct lu_nodemap *nodemap;
 		__u32 mapped_uid, mapped_gid, mapped_projid;
 
+		mapped_uid = oa->o_uid;
+		mapped_gid = oa->o_gid;
+		mapped_projid = oa->o_projid;
 		nodemap = nodemap_get_from_exp(exp);
-		if (IS_ERR(nodemap))
-			RETURN(PTR_ERR(nodemap));
-		mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
-					    NODEMAP_FS_TO_CLIENT,
-					    oa->o_uid);
-		mapped_gid = nodemap_map_id(nodemap, NODEMAP_GID,
-					    NODEMAP_FS_TO_CLIENT,
-					    oa->o_gid);
-		mapped_projid = nodemap_map_id(nodemap, NODEMAP_PROJID,
-					       NODEMAP_FS_TO_CLIENT,
-					       oa->o_projid);
+		if (!IS_ERR(nodemap)) {
+			mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
+						    NODEMAP_FS_TO_CLIENT,
+						    oa->o_uid);
+			mapped_gid = nodemap_map_id(nodemap, NODEMAP_GID,
+						    NODEMAP_FS_TO_CLIENT,
+						    oa->o_gid);
+			mapped_projid = nodemap_map_id(nodemap, NODEMAP_PROJID,
+						       NODEMAP_FS_TO_CLIENT,
+						       oa->o_projid);
+		} else if (old_rc == 0) {
+			old_rc = PTR_ERR(nodemap);
+		}
+
 		if (!IS_ERR_OR_NULL(nodemap)) {
 			/* do not bypass quota enforcement if squashed uid */
 			if (unlikely(mapped_uid == nodemap->nm_squash_uid)) {

lustre/ofd/ofd_io.c
@@ -1443,18 +1443,23 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 		ofd_counter_incr(exp, LPROC_OFD_STATS_WRITE, jobid,
 				 ktime_us_delta(ktime_get(), kstart));
 
+		mapped_uid = oa->o_uid;
+		mapped_gid = oa->o_gid;
+		mapped_projid = oa->o_projid;
 		nodemap = nodemap_get_from_exp(exp);
-		if (IS_ERR(nodemap))
-			RETURN(PTR_ERR(nodemap));
-		mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
-					    NODEMAP_FS_TO_CLIENT,
-					    oa->o_uid);
-		mapped_gid = nodemap_map_id(nodemap, NODEMAP_GID,
-					    NODEMAP_FS_TO_CLIENT,
-					    oa->o_gid);
-		mapped_projid = nodemap_map_id(nodemap, NODEMAP_PROJID,
-					       NODEMAP_FS_TO_CLIENT,
-					       oa->o_projid);
+		if (!IS_ERR(nodemap)) {
+			mapped_uid = nodemap_map_id(nodemap, NODEMAP_UID,
+						    NODEMAP_FS_TO_CLIENT,
+						    oa->o_uid);
+			mapped_gid = nodemap_map_id(nodemap, NODEMAP_GID,
+						    NODEMAP_FS_TO_CLIENT,
+						    oa->o_gid);
+			mapped_projid = nodemap_map_id(nodemap, NODEMAP_PROJID,
+						       NODEMAP_FS_TO_CLIENT,
+						       oa->o_projid);
+		} else if (old_rc == 0) {
+			old_rc = PTR_ERR(nodemap);
+		}
 
 		if (!IS_ERR_OR_NULL(nodemap)) {
 			/* do not bypass quota enforcement if squashed uid */

lustre/osd-ldiskfs/osd_handler.c
@@ -7762,6 +7762,8 @@ static void osd_key_exit(const struct lu_context *ctx,
 	LASSERT(info->oti_r_locks == 0);
 	LASSERT(info->oti_w_locks == 0);
 	LASSERT(info->oti_txns    == 0);
+	LASSERTF(info->oti_dio_pages_used == 0, "%d\n",
+		 info->oti_dio_pages_used);
 }
 
 /* type constructor/destructor: osd_type_init, osd_type_fini */