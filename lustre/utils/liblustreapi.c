lustre/lustre-release

commit 5604a6d270b8be13a8aacd72a105fc72b5e16976

LU-14975 dne: dir migration in non-recursive mode
Add an option "-d|--directory" option for "lfs migrate -m" to
migrate specified directory only, which is similar to "ls -d".

Add sanity 230w.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ib97949e3840a3b49f7074b16e259582a9bf16e3b
Reviewed-on: https://review.whamcloud.com/44802
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi.c
@@ -5474,6 +5474,9 @@ static int cb_migrate_mdt_init(char *path, int p, int *dp,
 	data.ioc_inlbuf2 = (char *)lmu;
 	data.ioc_inllen2 = lmv_user_md_size(lmu->lum_stripe_count,
 					    lmu->lum_magic);
+	/* reach bottom? */
+	if (param->fp_depth == param->fp_max_depth)
+		data.ioc_type = MDS_MIGRATE_NSONLY;
 	ret = llapi_ioctl_pack(&data, &rawbuf, sizeof(raw));
 	if (ret != 0) {
 		llapi_error(LLAPI_MSG_ERROR, ret,
@@ -5515,6 +5518,11 @@ migrate:
 	}
 
 out:
+	/* Do not get down anymore? */
+	if (param->fp_depth == param->fp_max_depth)
+		ret = 1;
+	param->fp_depth++;
+
 	if (dp != NULL) {
 		/*
 		 * If the directory is being migration, we need

lustre/tests/sanity.sh
@@ -20389,6 +20389,24 @@ test_230v()
 }
 run_test 230v "subdir migrated to the MDT where its parent is located"
 
+test_230w() {
+	(( MDSCOUNT > 1 )) || skip "needs >= 2 MDTs"
+	(( MDS1_VERSION >= $(version_code 2.14.53) )) ||
+		skip "Need MDS version at least 2.14.53"
+
+	mkdir -p $DIR/$tdir/sub || error "mkdir failed"
+
+	$LFS migrate -m 1 -c $MDSCOUNT -d $DIR/$tdir ||
+		error "migrate failed"
+
+	(( $($LFS getdirstripe -c $DIR/$tdir) == MDSCOUNT )) ||
+		error "$tdir stripe count mismatch"
+
+	(( $($LFS getdirstripe -c $DIR/$tdir/sub) == 0 )) ||
+		error "$tdir/sub is striped"
+}
+run_test 230w "non-recursive mode dir migration"
+
 test_231a()
 {
 	# For simplicity this test assumes that max_pages_per_rpc

lustre/doc/lfs-migrate.1
@@ -8,7 +8,7 @@ lfs migrate \- migrate files or directories between MDTs or OSTs.
 .IR FILE " ..."
 .br
 .B lfs migrate -m \fISTART_MDT_INDEX
-.RB [ -cHv ]
+.RB [ -cdHv ]
 .I DIRECTORY
 .br
 .SH DESCRIPTION
@@ -124,6 +124,11 @@ values are specified in a comma-separated list, then the number of specified
 values must match
 .IR COUNT .
 .TP
+.BR -d , --directory
+Only migrate the specified \fIDIRECTORY\fR and the non-directory inodes that are
+directly located within it.
+Similar to '\fBls -d\fR' and '\fBlfs getstripe -d\fR'.
+.TP
 .BR -H , --mdt-hash=\fIHASH_TYPE\fR
 Use
 .I HASH_TYPE
@@ -192,6 +197,11 @@ Recursively move the subdirectories and inodes contained in directory
 from its current MDT to MDT0000 and MDT0002.  The
 .B testremote
 directory and all of its subdirectories will be striped across both MDTs.
+.TP
+.B $ lfs migrate -m 0,2 -d ./testremote
+Move ./testremote and the first level of sub files from their current MDT
+to the MDT with index 0 and 2. Different from above case, the layout of
+subdirectories under ./testremote won't be changed.
 .TP
 .B # lfs setstripe -E 256M -c 1 -E 16G -c 4 -E eof -c 40 topdir
 Set a default PFL layout (without any DoM component) on the directory

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1955,6 +1955,8 @@ enum mds_op_bias {
 	/* setstripe create only, don't restripe if target exists */
 	MDS_SETSTRIPE_CREATE	= 1 << 21,
 	MDS_FID_OP		= 1 << 22,
+	/* migrate dirent only */
+	MDS_MIGRATE_NSONLY	= 1 << 23,
 };
 
 #define MDS_CLOSE_INTENT (MDS_HSM_RELEASE | MDS_CLOSE_LAYOUT_SWAP |         \

lustre/llite/dir.c
@@ -2144,6 +2144,7 @@ out_hur:
 		int len;
 		char *filename;
 		int namelen = 0;
+		__u32 flags;
 		int rc;
 
 		rc = obd_ioctl_getdata(&data, &len, (void __user *)arg);
@@ -2156,6 +2157,7 @@ out_hur:
 
 		filename = data->ioc_inlbuf1;
 		namelen = data->ioc_inllen1;
+		flags = data->ioc_type;
 
 		if (namelen < 1 || namelen != strlen(filename) + 1) {
 			CDEBUG(D_INFO, "IOC_MDC_LOOKUP missing filename\n");
@@ -2171,7 +2173,7 @@ out_hur:
 			GOTO(migrate_free, rc);
 		}
 
-		rc = ll_migrate(inode, file, lum, filename);
+		rc = ll_migrate(inode, file, lum, filename, flags);
 migrate_free:
 		OBD_FREE_LARGE(data, len);
 

lustre/llite/file.c
@@ -4790,7 +4790,7 @@ out_req:
 }
 
 int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
-	       const char *name)
+	       const char *name, __u32 flags)
 {
 	struct dentry *dchild = NULL;
 	struct inode *child_inode = NULL;
@@ -4887,6 +4887,11 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	op_data->op_cli_flags |= CLI_MIGRATE | CLI_SET_MEA;
 	op_data->op_data = lum;
 	op_data->op_data_size = lumlen;
+
+	/* migrate dirent only for subdirs if MDS_MIGRATE_NSONLY set */
+	if (S_ISDIR(child_inode->i_mode) && (flags & MDS_MIGRATE_NSONLY) &&
+	    lmv_dir_layout_changing(ll_i2info(parent)->lli_lsm_md))
+		op_data->op_bias |= MDS_MIGRATE_NSONLY;
 
 again:
 	if (S_ISREG(child_inode->i_mode)) {

lustre/llite/llite_internal.h
@@ -1125,7 +1125,7 @@ static inline int ll_inode_flags_to_xflags(int inode_flags)
 }
 
 int ll_migrate(struct inode *parent, struct file *file,
-	       struct lmv_user_md *lum, const char *name);
+	       struct lmv_user_md *lum, const char *name, __u32 flags);
 int ll_get_fid_by_name(struct inode *parent, const char *name,
 		       int namelen, struct lu_fid *fid, struct inode **inode);
 int ll_inode_permission(struct inode *inode, int mask);

lustre/lmv/lmv_obd.c
@@ -2267,6 +2267,11 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 			tp_tgt = lmv_tgt(lmv, oinfo->lmo_mds);
 			if (!tp_tgt)
 				RETURN(-ENODEV);
+
+			/* parent unchanged and update namespace only */
+			if (lu_fid_eq(&op_data->op_fid4, &op_data->op_fid2) &&
+			    op_data->op_bias & MDS_MIGRATE_NSONLY)
+				RETURN(-EALREADY);
 		}
 	} else {
 		sp_tgt = parent_tgt;

commit df3e6b4404496530a2ee65722b4b739cc0278227

LU-14975 utils: non-recursive dir migration fix
If sem_init() doesn't return 0, llapi_semantic_traverse() won't
call sem_fini() in directory traverse, therefore
cb_migrate_mdt_init() shouldn't increase param->fp_depth if it
reaches max depth in non-recursive mode.

Update sanity 230w.

Fixes: 5604a6d270b ("LU-14975 dne: dir migration in non-recursive mode")
Signed-off-by: Lai Syao <lai.siyao@whamcloud.com>
Change-Id: I8814aaae7c267cec51654175f9fa0708f7685a5a
Reviewed-on: https://review.whamcloud.com/47012
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi.c
@@ -6016,7 +6016,8 @@ out:
 	/* Do not get down anymore? */
 	if (param->fp_depth == param->fp_max_depth)
 		ret = 1;
-	param->fp_depth++;
+	else
+		param->fp_depth++;
 
 	if (dp != NULL) {
 		/*

lustre/tests/sanity.sh
@@ -20719,10 +20719,12 @@ run_test 230v "subdir migrated to the MDT where its parent is located"
 
 test_230w() {
 	(( MDSCOUNT > 1 )) || skip "needs >= 2 MDTs"
-	(( MDS1_VERSION >= $(version_code 2.14.53) )) ||
-		skip "Need MDS version at least 2.14.53"
+	(( MDS1_VERSION >= $(version_code 2.15.0) )) ||
+		skip "Need MDS version at least 2.15.0"
 
-	mkdir -p $DIR/$tdir/sub || error "mkdir failed"
+	mkdir -p $DIR/$tdir || error "mkdir $tdir failed"
+	createmany -o $DIR/$tdir/f 10 || error "create files failed"
+	createmany -d $DIR/$tdir/d 10 || error "create dirs failed"
 
 	$LFS migrate -m 1 -c $MDSCOUNT -d $DIR/$tdir ||
 		error "migrate failed"
@@ -20730,8 +20732,10 @@ test_230w() {
 	(( $($LFS getdirstripe -c $DIR/$tdir) == MDSCOUNT )) ||
 		error "$tdir stripe count mismatch"
 
-	(( $($LFS getdirstripe -c $DIR/$tdir/sub) == 0 )) ||
-		error "$tdir/sub is striped"
+	for i in $(seq 0 9); do
+		(( $($LFS getdirstripe -c $DIR/$tdir/d$i) == 0 )) ||
+			error "d$i is striped"
+	done
 }
 run_test 230w "non-recursive mode dir migration"
 