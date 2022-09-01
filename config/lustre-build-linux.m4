lustre/lustre-release

commit a5084c2f2ec851fe888838fb1e90aff8b5ed6fd2

LU-14937 build: re-use config cache in 'make rpms/debs'
Idea is to pass along the value of the -C or --cache-file options from
the initial ./configure to the one launched as part of the rpm or deb
build.
But all the environment variables related cache info must be removed
otherwise the config cache file cannot be reused.

Test-Parameters: trivial
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Iab4ae2815961ba10132d9cb44f82ca58d313e908
Reviewed-on: https://review.whamcloud.com/44659
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build.m4
@@ -628,6 +628,19 @@ AC_SUBST(RPMBUILD_BINARY_ARGS)
 ]) # LB_CONFIG_RPMBUILD_OPTIONS
 
 #
+# LB_CONFIG_CACHE_OPTIONS
+#
+# Propagate config cache option
+#
+AC_DEFUN([LB_CONFIG_CACHE_OPTIONS], [
+CONFIG_CACHE_FILE=
+if test -f "$cache_file"; then
+	CONFIG_CACHE_FILE=$(readlink --canonicalize "$cache_file")
+fi
+AC_SUBST(CONFIG_CACHE_FILE)
+]) # LB_CONFIG_CACHE_OPTIONS
+
+#
 # LB_CONFIGURE
 #
 # main configure steps
@@ -706,6 +719,7 @@ MOSTLYCLEANFILES='.*.cmd .*.flags *.o *.ko *.mod.c .depend .*.1.* Modules.symver
 AC_SUBST(MOSTLYCLEANFILES)
 
 LB_CONFIG_RPMBUILD_OPTIONS
+LB_CONFIG_CACHE_OPTIONS
 
 AC_OUTPUT
 

autoMakefile.am
@@ -208,6 +208,9 @@ dkms-rpms: dkms-rpm
 
 rpms: srpm
 	rpmbuilddir=`mktemp -t -d rpmbuild-@PACKAGE@-$$USER-XXXXXXXX`; \
+	if test "x@CONFIG_CACHE_FILE@" != "x"; then \
+		export CONFIG_CACHE_FILE="@CONFIG_CACHE_FILE@"; \
+	fi; \
 	$(MAKE) $(AM_MAKEFLAGS) \
 		rpmbuilddir="$$rpmbuilddir" rpm-local || exit 1; \
 	$(RPMBUILD) \
@@ -260,6 +263,9 @@ debs: undef.h
 	if test "x@ENABLEO2IB@" != "xno"; then \
 		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} o2ib"; \
 		export O2IB_SRC="@O2IBPATH@"; \
+	fi; \
+	if test "x@CONFIG_CACHE_FILE@" != "x"; then \
+		export CONFIG_CACHE_FILE="@CONFIG_CACHE_FILE@"; \
 	fi; \
 	if test "x@ENABLE_GSS@" = "xyes"; then \
 		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} gss"; \

debian/rules
@@ -199,6 +199,13 @@ configure-stamp: autogen-stamp debian/control.main debian/control.modules.in
 	elif echo "$${DEB_BUILD_PROFILES}" | grep -qw "nocrypto"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --disable-crypto"; \
 	fi; \
+	# remove env variables from config cache built by initial configure,
+	# and create dedicated cache in temporary build directory
+	if [ -f "$${CONFIG_CACHE_FILE}" ]; then \
+		export TMP_CACHE_FILE=$$(mktemp); \
+		sed "/ac_cv_env/d" "$${CONFIG_CACHE_FILE}" > $${TMP_CACHE_FILE}; \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --cache-file=$${TMP_CACHE_FILE}"; \
+	fi; \
 	echo "Final value of EXTRAFLAGS: $${EXTRAFLAGS}"; \
 	( cd $(BUILDDIR) && \
 		$(SRCDIR)/configure --disable-dependency-tracking \
@@ -485,6 +492,13 @@ kdist_config: prep-deb-files patch-stamp
 		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-crypto"; \
 	elif echo "$${DEB_BUILD_PROFILES}" | grep -qw "nocrypto"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --disable-crypto"; \
+	fi; \
+	# remove env variables from config cache built by initial configure,
+	# and create dedicated cache in temporary build directory
+	if [ -f "$${CONFIG_CACHE_FILE}" ]; then \
+		export TMP_CACHE_FILE=$$(mktemp --tmpdir newconfig-XXXXXXXX.cache); \
+		sed "/ac_cv_env/d" "$${CONFIG_CACHE_FILE}" > $${TMP_CACHE_FILE}; \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --cache-file=$${TMP_CACHE_FILE}"; \
 	fi; \
 	echo "Final value of EXTRAFLAGS: $${EXTRAFLAGS}"; \
 	./configure --with-linux=$(KSRC_TREE) \

lustre.spec.in
@@ -453,6 +453,13 @@ if [ -n "$CONFIGURE_ARGS" ]; then
 	# remove --with-kmp-moddir from configure arguments,
 	# it will be set --with-kmp-moddir=%%kmoddir
 	CONFIGURE_ARGS=$(echo $CONFIGURE_ARGS | sed -e 's/"\?--with-kmp-moddir=[^ ][^ ]* \?//')
+	# remove env variables from config cache built by initial configure,
+	# and create dedicated cache in temporary build directory
+	if [ -f "$CONFIG_CACHE_FILE" ]; then
+		TMP_CONFIG_CACHE="$(mktemp --tmpdir newconfig-XXXXXXXX.cache)"
+		sed "/ac_cv_env/d" "$CONFIG_CACHE_FILE" > $TMP_CONFIG_CACHE
+		CONFIGURE_ARGS="$CONFIGURE_ARGS --cache-file=$TMP_CONFIG_CACHE"
+	fi
 fi
 
 # we need to eval "configure" because $CONFIGURE_ARGS could have a quoted

commit 8a7aa8d5902fcffe6c8b56d0c92ee36380a26e88

LU-16058 build: proc_ops check fails with SUBARCH undefined
During configure with config.cache enabled SUBARCH may not
be defined.

Move the definition to a location that must be traversed.

Test-Parameters: trivial
Fixes: a5084c2f2e ("LU-14937 build: re-use config cache in 'make rpms/debs'")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0a7b4de3ecccd41b1c55e8b2df29039517e0c416
Reviewed-on: https://review.whamcloud.com/48101
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-linux.m4
@@ -572,7 +572,6 @@ MODULE_LICENSE("GPL");])
 AC_DEFUN([LB_LINUX_COMPILE_IFELSE],
 [m4_ifvaln([$1], [AC_LANG_CONFTEST([AC_LANG_SOURCE([$1])])])dnl
 rm -f build/conftest.o build/conftest.mod.c build/conftest.ko
-SUBARCH=$(echo $target_cpu | sed -e 's/powerpc.*/powerpc/' -e 's/ppc.*/powerpc/' -e 's/x86_64/x86/' -e 's/i.86/x86/' -e 's/k1om/x86/' -e 's/aarch64.*/arm64/' -e 's/armv7.*/arm/')
 AS_IF([AC_TRY_COMMAND(cp conftest.c build && make -d [$2] LDFLAGS= ${LD:+LD="$LD"} CC="$CC" -f $PWD/build/Makefile LUSTRE_LINUX_CONFIG=$LINUX_CONFIG LINUXINCLUDE="$EXTRA_CHECK_INCLUDE -I$LINUX/arch/$SUBARCH/include -Iinclude -Iarch/$SUBARCH/include/generated -I$LINUX/include -Iinclude2 -I$LINUX/include/uapi -Iinclude/generated -I$LINUX/arch/$SUBARCH/include/uapi -Iarch/$SUBARCH/include/generated/uapi -I$LINUX/include/uapi -Iinclude/generated/uapi ${SPL_OBJ:+-include $SPL_OBJ/spl_config.h} ${ZFS_OBJ:+-include $ZFS_OBJ/zfs_config.h} ${SPL:+-I$SPL/include } ${ZFS:+-I$ZFS -I$ZFS/include -I$ZFS/include/os/linux/kernel -I$ZFS/include/os/linux/spl -I$ZFS/include/os/linux/zfs -I${SPL:-$ZFS/include/spl}} -include $CONFIG_INCLUDE" KBUILD_EXTRA_SYMBOLS="${ZFS_OBJ:+$ZFS_OBJ/Module.symvers} $KBUILD_EXTRA_SYMBOLS" -o tmp_include_depends -o scripts -o include/config/MARKER -C $LINUX_OBJ EXTRA_CFLAGS="-Werror-implicit-function-declaration $EXTRA_KCFLAGS" $MODULE_TARGET=$PWD/build) >/dev/null && AC_TRY_COMMAND([$3])],
 	[$4],
 	[_AC_MSG_LOG_CONFTEST

config/lustre-build.m4
@@ -159,6 +159,9 @@ AS_IF([test "x$enable_modules" = xyes], [
 	AS_IF([test "x$BISON" = "x"], [AC_MSG_ERROR([bison package is required to build kernel modules])])
 	AS_CASE([$target_os],
 		[linux*], [
+			# Ensure SUBARCH is defined
+			SUBARCH=$(echo $target_cpu | sed -e 's/powerpc.*/powerpc/' -e 's/ppc.*/powerpc/' -e 's/x86_64/x86/' -e 's/i.86/x86/' -e 's/k1om/x86/' -e 's/aarch64.*/arm64/' -e 's/armv7.*/arm/')
+
 			# Run serial tests
 			LB_PROG_LINUX
 			LIBCFS_PROG_LINUX
@@ -723,7 +726,7 @@ AC_SUBST(MOSTLYCLEANFILES)
 LB_CONFIG_RPMBUILD_OPTIONS
 LB_CONFIG_CACHE_OPTIONS
 
-AS_IF([test -d $TEST_DIR], [
+AS_IF([test -d $TEST_DIR -a "x${PARALLEL_BUILD_OPT}" != "xdebug"], [
 	AC_MSG_NOTICE([remove temporary parallel configure dir $TEST_DIR])
 	rm -rf $TEST_DIR
 ])