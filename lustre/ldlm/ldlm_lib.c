lustre/lustre-release

commit 9013eb2bb5492b1e8321d0ed534bee566f19627b

LU-9859 libcfs: don't call unshare_fs_struct()
A kthread runs with the same fs_struct as init.
It is only helpful to unshare this if the thread
will change one of the fields in the fs_struct:
 root directory
 current working directory
 umask.

No lustre kthread changes any of these, so there is
no need to call unshare_fs_struct().

Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I7309b6ed184b14a272bad7dc5149ad36281f948e
Reviewed-on: https://review.whamcloud.com/39132
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lib.c
@@ -2641,8 +2641,6 @@ static int target_recovery_thread(void *arg)
 	int rc = 0;
 
 	ENTRY;
-
-	unshare_fs_struct();
 	OBD_ALLOC_PTR(thread);
 	if (thread == NULL)
 		RETURN(-ENOMEM);

lustre/ldlm/ldlm_request.c
@@ -2627,8 +2627,6 @@ static int ldlm_lock_replay_thread(void *data)
 {
 	struct obd_import *imp = data;
 
-	unshare_fs_struct();
-
 	CDEBUG(D_HA, "lock replay thread %s to %s@%s\n",
 	       imp->imp_obd->obd_name, obd2cli_tgt(imp->imp_obd),
 	       imp->imp_connection->c_remote_uuid.uuid);

lustre/obdclass/llog.c
@@ -774,8 +774,6 @@ static int llog_process_thread_daemonize(void *arg)
 	}
 	task_unlock(lpi->lpi_reftask);
 
-	unshare_fs_struct();
-
 	/* client env has no keys, tags is just 0 */
 	rc = lu_env_init(&env, LCT_LOCAL | LCT_MG_THREAD);
 	if (rc)

lustre/ptlrpc/import.c
@@ -1487,9 +1487,6 @@ static int ptlrpc_invalidate_import_thread(void *data)
         struct obd_import *imp = data;
 
         ENTRY;
-
-	unshare_fs_struct();
-
         CDEBUG(D_HA, "thread invalidate import %s to %s@%s\n",
                imp->imp_obd->obd_name, obd2cli_tgt(imp->imp_obd),
                imp->imp_connection->c_remote_uuid.uuid);

lustre/ptlrpc/pinger.c
@@ -578,8 +578,6 @@ static int ping_evictor_main(void *arg)
 	time64_t expire_time;
 
 	ENTRY;
-	unshare_fs_struct();
-
 	CDEBUG(D_HA, "Starting Ping Evictor\n");
 	pet_state = PET_READY;
 	while (1) {

lustre/ptlrpc/ptlrpcd.c
@@ -442,9 +442,6 @@ static int ptlrpcd(void *arg)
 	int				exit = 0;
 
 	ENTRY;
-
-	unshare_fs_struct();
-
 	if (cfs_cpt_bind(cfs_cpt_tab, pc->pc_cpt) != 0)
 		CWARN("Failed to bind %s on CPT %d\n", pc->pc_name, pc->pc_cpt);
 

lustre/ptlrpc/service.c
@@ -2746,7 +2746,6 @@ static int ptlrpc_main(void *arg)
 
 	thread->t_task = current;
 	thread->t_pid = current->pid;
-	unshare_fs_struct();
 
 	if (svc->srv_cpt_bind) {
 		rc = cfs_cpt_bind(svc->srv_cptable, svcpt->scp_cpt);
@@ -2953,8 +2952,6 @@ static int ptlrpc_hr_main(void *arg)
 	OBD_ALLOC_PTR(env);
 	if (env == NULL)
 		RETURN(-ENOMEM);
-
-	unshare_fs_struct();
 
 	rc = cfs_cpt_bind(ptlrpc_hr.hr_cpt_table, hrp->hrp_cpt);
 	if (rc != 0) {

libcfs/include/libcfs/libcfs.h
@@ -86,11 +86,6 @@ static inline int notifier_from_ioctl_errno(int err)
 	return notifier_from_errno(err) | NOTIFY_STOP_MASK;
 }
 
-/*
- * Defined by platform
- */
-int unshare_fs_struct(void);
-
 int libcfs_ioctl_data_adjust(struct libcfs_ioctl_data *data);
 
 #define container_of0(ptr, type, member) container_of_safe(ptr, type, member)

commit c92bdd97d99cc755a187987f3d5963adeb3ea475

LU-16056 libcfs: restore umask handling in kernel threads
This reverts commit 9013eb2bb5 which incorrectly assumes that Lustre
service threads do not modify umask.  A quick grep shows that umask
is modified in osd-ldiskfs __osd_create().

If some other thread sharing the same fs context is modifying umask
in an incompatible way (which includes all Lustre threads after
this patch) then it will occasionally break created file access
permissions for Lustre.

Change-Id: I589b72e4286dc84f4e3f1a0c54fe31aa988e6c18
Fixes: 9013eb2bb5 (LU-9859 libcfs: don't call unshare_fs_struct()")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/48233
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shuichi Ihara <sihara@ddn.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lib.c
@@ -39,6 +39,7 @@
 #define DEBUG_SUBSYSTEM S_LDLM
 
 #include <cl_object.h>
+#include <linux/fs_struct.h>
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
@@ -2716,6 +2717,7 @@ static int target_recovery_thread(void *arg)
 	int rc = 0;
 
 	ENTRY;
+	unshare_fs_struct();
 	OBD_ALLOC_PTR(thread);
 	if (thread == NULL)
 		RETURN(-ENOMEM);

lustre/ldlm/ldlm_request.c
@@ -56,6 +56,7 @@
 
 #define DEBUG_SUBSYSTEM S_LDLM
 
+#include <linux/fs_struct.h>
 #include <lustre_errno.h>
 #include <lustre_dlm.h>
 #include <obd_class.h>
@@ -2609,6 +2610,7 @@ static int ldlm_lock_replay_thread(void *data)
 {
 	struct obd_import *imp = data;
 
+	unshare_fs_struct();
 	CDEBUG(D_HA, "lock replay thread %s to %s@%s\n",
 	       imp->imp_obd->obd_name, obd2cli_tgt(imp->imp_obd),
 	       imp->imp_connection->c_remote_uuid.uuid);

lustre/obdclass/llog.c
@@ -42,6 +42,7 @@
 
 #define DEBUG_SUBSYSTEM S_LOG
 
+#include <linux/fs_struct.h>
 #include <linux/pid_namespace.h>
 #include <linux/kthread.h>
 #include <llog_swab.h>
@@ -836,6 +837,7 @@ static int llog_process_thread_daemonize(void *arg)
 	}
 	task_unlock(lpi->lpi_reftask);
 
+	unshare_fs_struct();
 	/* client env has no keys, tags is just 0 */
 	rc = lu_env_init(&env, LCT_LOCAL | LCT_MG_THREAD);
 	if (rc)

lustre/ptlrpc/import.c
@@ -35,6 +35,7 @@
 
 #define DEBUG_SUBSYSTEM S_RPC
 
+#include <linux/fs_struct.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
 #include <obd_support.h>
@@ -1508,6 +1509,7 @@ static int ptlrpc_invalidate_import_thread(void *data)
 	struct obd_import *imp = data;
 
 	ENTRY;
+	unshare_fs_struct();
 	CDEBUG(D_HA, "thread invalidate import %s to %s@%s\n",
 	       imp->imp_obd->obd_name, obd2cli_tgt(imp->imp_obd),
 	       imp->imp_connection->c_remote_uuid.uuid);

lustre/ptlrpc/pinger.c
@@ -35,6 +35,7 @@
 
 #define DEBUG_SUBSYSTEM S_RPC
 
+#include <linux/fs_struct.h>
 #include <linux/kthread.h>
 #include <linux/workqueue.h>
 #include <obd_support.h>
@@ -465,6 +466,7 @@ static int ping_evictor_main(void *arg)
 	time64_t expire_time;
 
 	ENTRY;
+	unshare_fs_struct();
 	CDEBUG(D_HA, "Starting Ping Evictor\n");
 	pet_state = PET_READY;
 	while (1) {

lustre/ptlrpc/ptlrpcd.c
@@ -50,6 +50,7 @@
 
 #define DEBUG_SUBSYSTEM S_RPC
 
+#include <linux/fs_struct.h>
 #include <linux/kthread.h>
 #include <libcfs/libcfs.h>
 #include <lustre_net.h>
@@ -436,6 +437,7 @@ static int ptlrpcd(void *arg)
 	int				exit = 0;
 
 	ENTRY;
+	unshare_fs_struct();
 	if (cfs_cpt_bind(cfs_cpt_tab, pc->pc_cpt) != 0)
 		CWARN("Failed to bind %s on CPT %d\n", pc->pc_name, pc->pc_cpt);
 

lustre/ptlrpc/service.c
@@ -31,6 +31,7 @@
 
 #define DEBUG_SUBSYSTEM S_RPC
 
+#include <linux/fs_struct.h>
 #include <linux/kthread.h>
 #include <linux/ratelimit.h>
 
@@ -2751,6 +2752,7 @@ static int ptlrpc_main(void *arg)
 	int counter = 0, rc = 0;
 
 	ENTRY;
+	unshare_fs_struct();
 
 	thread->t_task = current;
 	thread->t_pid = current->pid;
@@ -2957,6 +2959,7 @@ static int ptlrpc_hr_main(void *arg)
 	struct lu_env *env;
 	int rc;
 
+	unshare_fs_struct();
 	OBD_ALLOC_PTR(env);
 	if (env == NULL)
 		RETURN(-ENOMEM);