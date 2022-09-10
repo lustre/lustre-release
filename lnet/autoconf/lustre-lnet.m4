lustre/lustre-release

commit 1f4aaefe1aae8eb5c75fff446f4dc377e968677f

LU-15962 build: add in-kernel Module.symvers to symbol path
After building Lustre with in-kernel OFED, installing
ko2iblnd module hit the following errors:

ko2iblnd: disagrees about version of symbol __ib_alloc_pd
ko2iblnd: Unknown symbol __ib_alloc_pd (err -22)
ko2iblnd: disagrees about version of symbol rdma_resolve_addr
ko2iblnd: Unknown symbol rdma_resolve_addr (err -22)

Those exported symbols are contained in in-kernel Module.symvers,
which should be added to the symbol path KBUILD_EXTRA_SYMBOLS.

Test-Parameters: trivial

Change-Id: Ic30caa7079af00a452ea24e7e982a856874af702
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/47699
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/autoconf/lustre-lnet.m4
@@ -206,18 +206,17 @@ AS_IF([test $ENABLEO2IB = "no"], [
 		])
 		# we know at this point that the found OFED source is good
 		O2IB_SYMVER=""
-		if test $ENABLEO2IB = "withpath" -o "x$OFED" = "xyes" ; then
-			if test -f $O2IBPATH/Module.symvers; then
-				O2IB_SYMVER=$O2IBPATH/Module.symvers;
-				break;
-			fi
-			if test -n "$O2IB_SYMVER"; then
-				AC_MSG_NOTICE([adding $O2IB_SYMVER to Symbol Path])
-				EXTRA_SYMBOLS="$EXTRA_SYMBOLS $O2IB_SYMVER"
-				AC_SUBST(EXTRA_SYMBOLS)
-			else
-				AC_MSG_ERROR([an external source tree was, either specified or detected, for o2iblnd however I could not find a $O2IBPATH/Module.symvers there])
-			fi
+		if test -f $O2IBPATH/Module.symvers; then
+			O2IB_SYMVER=$O2IBPATH/Module.symvers
+		elif test "x$SUSE_KERNEL" = "xyes"; then
+			O2IB_SYMVER=$(find ${O2IBPATH}* -name Module.symvers)
+		fi
+		if test -n "$O2IB_SYMVER"; then
+			AC_MSG_NOTICE([adding $O2IB_SYMVER to Symbol Path])
+			EXTRA_SYMBOLS="$EXTRA_SYMBOLS $O2IB_SYMVER"
+			AC_SUBST(EXTRA_SYMBOLS)
+		else
+			AC_MSG_ERROR([an external source tree was, either specified or detected, for o2iblnd however I could not find a $O2IBPATH/Module.symvers there])
 		fi
 
 		LB_CHECK_COMPILE([if Linux kernel has kthread_worker],

commit f3a9921ae4f9c3e48328f2c682e0c7e61221e0d3

LU-16090 build: Module.symvers lookup by flavor on SUSE
When multiple kernel flavors are found we need to select only
the Module.symvers for the flavor that is being built.

HPE-bug-id: LUS-11149
Test-Parameters: trivial
Fixes: 1f4aaefe1aae ("LU-15962 build: add in-kernel Module.symvers to symbol path")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I1c9af91108534d3a67f816077756fded4cd0b653
Reviewed-on: https://review.whamcloud.com/48195
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Tested-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/autoconf/lustre-lnet.m4
@@ -210,6 +210,18 @@ AS_IF([test $ENABLEO2IB = "no"], [
 			O2IB_SYMVER=$O2IBPATH/Module.symvers
 		elif test "x$SUSE_KERNEL" = "xyes"; then
 			O2IB_SYMVER=$(find ${O2IBPATH}* -name Module.symvers)
+			# Select only the current 'flavor' if there is more than 1
+			NUM_AVAIL=$(find ${O2IBPATH}* -name Module.symvers | wc -l)
+			if test ${NUM_AVAIL} -gt 1; then
+				PREFER=$(basename ${LINUX_OBJ})
+				for F in $(find ${O2IBPATH}-obj -name Module.symvers)
+				do
+					maybe=$(echo $F | grep "/${PREFER}")
+					if test "x$maybe" != "x"; then
+						O2IB_SYMVER=$F
+					fi
+				done
+			fi
 		elif test -f $LINUX_OBJ/Module.symvers; then
 			# Debian symvers is in the arch tree
 			O2IB_SYMVER=$LINUX_OBJ/Module.symvers