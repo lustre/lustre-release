lustre/lustre-release

commit e68d496ada38acfe3d3ac73aa164440a00639596

LU-15858 sec: reinstate null encryption for file names
Reinstate null encryption for file names by adding a new llite
parameter named 'enable_filename_encryption', set to 0 by default.
When this parameter is 0, new empty directories configured as
encrypted ignore the filenames_encryption_mode and use
LLCRYPT_MODE_NULL instead, which is a no-op. This LLCRYPT_MODE_NULL
mode is inherited for all subdirectories and files.
When this parameter is 1, new empty directories configured as
encrypted use the normal encryption mode.

To set this parameter globally for all clients, do on the MGS:
mgs# lctl set_param -P llite.*.enable_filename_encryption=0

Also update sanity-sec test_54 to exercise the new parameter
'enable_filename_encryption'.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I9d726ba26cc91a51690d59a81efe3eb98ee2995c
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/47355
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -1194,9 +1194,6 @@ void ll_lli_init(struct ll_inode_info *lli)
 #define MAX_STRING_SIZE 128
 
 #ifndef HAVE_SUPER_SETUP_BDI_NAME
-
-#define LSI_BDI_INITIALIZED	0x00400000
-
 #ifndef HAVE_BDI_CAP_MAP_COPY
 # define BDI_CAP_MAP_COPY	0
 #endif

lustre/llite/lproc_llite.c
@@ -1591,6 +1591,39 @@ static ssize_t ll_nosquash_nids_seq_write(struct file *file,
 
 LDEBUGFS_SEQ_FOPS(ll_nosquash_nids);
 
+static int ll_filename_enc_seq_show(struct seq_file *m, void *v)
+{
+	struct super_block *sb = m->private;
+	struct lustre_sb_info *lsi = s2lsi(sb);
+
+	seq_printf(m, "%u\n", lsi->lsi_flags & LSI_FILENAME_ENC ? 1 : 0);
+	return 0;
+}
+
+static ssize_t ll_filename_enc_seq_write(struct file *file,
+					 const char __user *buffer,
+					 size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct super_block *sb = m->private;
+	struct lustre_sb_info *lsi = s2lsi(sb);
+	bool val;
+	int rc;
+
+	rc = kstrtobool_from_user(buffer, count, &val);
+	if (rc)
+		return rc;
+
+	if (val)
+		lsi->lsi_flags |= LSI_FILENAME_ENC;
+	else
+		lsi->lsi_flags &= ~LSI_FILENAME_ENC;
+
+	return count;
+}
+
+LDEBUGFS_SEQ_FOPS(ll_filename_enc);
+
 static int ll_pcc_seq_show(struct seq_file *m, void *v)
 {
 	struct super_block *sb = m->private;
@@ -1645,6 +1678,8 @@ struct ldebugfs_vars lprocfs_llite_obd_vars[] = {
 	  .fops	=	&ll_nosquash_nids_fops			},
 	{ .name =	"pcc",
 	  .fops =	&ll_pcc_fops,				},
+	{ .name =	"enable_filename_encryption",
+	  .fops =	&ll_filename_enc_fops,			},
 	{ NULL }
 };
 

lustre/tests/sanity-sec.sh
@@ -4269,6 +4269,7 @@ test_54() {
 	local testfile2=$testdir/${tfile}withveryverylongnametoexercisecode
 	local tmpfile=$TMP/${tfile}.tmp
 	local resfile=$TMP/${tfile}.res
+	local nameenc
 	local fid1
 	local fid2
 
@@ -4317,6 +4318,18 @@ test_54() {
 	$RUNAS ls -R $testdir || error "ls -R $testdir failed"
 	local filecount=$($RUNAS find $testdir -type f | wc -l)
 	[ $filecount -eq 3 ] || error "found $filecount files"
+
+	# check enable_filename_encryption default value
+	nameenc=$(lctl get_param -n llite.*.enable_filename_encryption |
+			head -n1)
+	[ $nameenc -eq 0 ] ||
+		error "enable_filename_encryption should be 0 by default"
+
+	# $testfile and $testfile2 should exist because names are not encrypted
+	[ -f $testfile ] ||
+		error "$testfile should exist because name is not encrypted"
+	[ -f $testfile2 ] ||
+		error "$testfile should exist because name is not encrypted"
 
 	scrambledfiles=( $(find $testdir/ -maxdepth 1 -type f) )
 	$RUNAS hexdump -C ${scrambledfiles[0]} &&
@@ -4391,6 +4404,16 @@ test_54() {
 	fid1=$(path2fid $MOUNT/.fscrypt)
 	echo "With FILESET $tdir, .fscrypt FID is $fid1"
 
+	# enable name encryption
+	do_facet mgs $LCTL set_param -P llite.*.enable_filename_encryption=1
+	[ $? -eq 0 ] ||
+		error "set_param -P llite.*.enable_filename_encryption failed"
+
+	wait_update_facet --verbose client \
+	    "$LCTL get_param -n llite.*.enable_filename_encryption | head -n1" \
+	    1 30 ||
+		error "enable_filename_encryption not set on client"
+
 	# encrypt 'vault' dir inside the subdir mount
 	echo -e 'mypass\nmypass' | su - $USER0 -c "fscrypt encrypt --verbose \
 		--source=custom_passphrase --name=protector $testdir" ||
@@ -4402,6 +4425,11 @@ test_54() {
 
 	$RUNAS fscrypt lock --verbose $testdir ||
 		error "fscrypt lock $testdir failed (4)"
+
+	# encfile should actually have its name encrypted
+	[ -f $testdir/encfile ] && error "encfile name should be encrypted"
+	filecount=$(find $testdir -type f | wc -l)
+	[ $filecount -eq 1 ] || error "found $filecount files instead of 1"
 
 	# remount client with encrypted dir as subdirectory mount
 	umount_client $MOUNT || error "umount $MOUNT failed (2)"
@@ -4440,6 +4468,16 @@ test_54() {
 	rm -rf $DIR/$tdir/vault/*
 	$RUNAS fscrypt lock --verbose $DIR/$tdir/vault ||
 		error "fscrypt lock $DIR/$tdir/vault failed (5)"
+
+	# disable name encryption
+	do_facet mgs $LCTL set_param -P llite.*.enable_filename_encryption=0
+	[ $? -eq 0 ] ||
+		error "set_param -P llite.*.enable_filename_encryption failed"
+
+	wait_update_facet --verbose client \
+	    "$LCTL get_param -n llite.*.enable_filename_encryption | head -n1" \
+	    0 30 ||
+		error "enable_filename_encryption not set back to default"
 
 	rm -rf $tmpfile $MOUNT/.fscrypt
 }

libcfs/include/libcfs/crypto/llcrypt.h
@@ -753,7 +753,7 @@ static inline int llcrypt_prepare_symlink(struct inode *dir,
 					  struct llcrypt_str *disk_link)
 {
 	if ((IS_ENCRYPTED(dir) &&
-	     likely(llcrypt_policy_has_filename_enc(dir))) ||
+	     llcrypt_policy_has_filename_enc(dir)) ||
 	    llcrypt_dummy_context_enabled(dir))
 		return __llcrypt_prepare_symlink(dir, len, max_len, disk_link);
 

libcfs/include/libcfs/libcfs_debug.h
@@ -37,6 +37,7 @@
 #ifndef __LIBCFS_DEBUG_H__
 #define __LIBCFS_DEBUG_H__
 
+#include <linux/tty.h>
 #include <linux/limits.h>
 #include <uapi/linux/lnet/libcfs_debug.h>
 
@@ -306,5 +307,22 @@ do {									\
 } while (0)
 
 void cfs_debug_init(void);
+
+static inline void cfs_tty_write_msg(const char *msg)
+{
+	struct tty_struct *tty;
+
+	tty = get_current_tty();
+	if (!tty)
+		return;
+	mutex_lock(&tty->atomic_write_lock);
+	tty_lock(tty);
+	if (tty->ops->write && tty->count > 0)
+		tty->ops->write(tty, msg, strlen(msg));
+	tty_unlock(tty);
+	mutex_unlock(&tty->atomic_write_lock);
+	wake_up_interruptible_poll(&tty->write_wait, POLL_OUT);
+	tty_kref_put(tty);
+}
 
 #endif	/* __LIBCFS_DEBUG_H__ */

libcfs/libcfs/crypto/fname.c
@@ -291,7 +291,7 @@ int llcrypt_fname_disk_to_usr(struct inode *inode,
 		return fname_decrypt(inode, iname, oname);
 	}
 
-	if (unlikely(!llcrypt_policy_has_filename_enc(inode))) {
+	if (!llcrypt_policy_has_filename_enc(inode)) {
 		memcpy(oname->name, iname->name, iname->len);
 		oname->name[iname->len] = '\0';
 		oname->len = iname->len;
@@ -387,7 +387,7 @@ int llcrypt_setup_filename(struct inode *dir, const struct qstr *iname,
 	if (!lookup)
 		return -ENOKEY;
 
-	if (unlikely(!llcrypt_policy_has_filename_enc(dir))) {
+	if (!llcrypt_policy_has_filename_enc(dir)) {
 		fname->disk_name.name = (unsigned char *)iname->name;
 		fname->disk_name.len = iname->len;
 		return 0;

libcfs/libcfs/crypto/hooks.c
@@ -177,7 +177,7 @@ int __llcrypt_encrypt_symlink(struct inode *inode, const char *target,
 	struct llcrypt_symlink_data *sd;
 	unsigned int ciphertext_len;
 
-	if (unlikely(!llcrypt_policy_has_filename_enc(inode)))
+	if (!llcrypt_policy_has_filename_enc(inode))
 		return 0;
 
 	err = llcrypt_require_key(inode);
@@ -269,7 +269,7 @@ const char *llcrypt_get_symlink(struct inode *inode, const void *caddr,
 	 * the ciphertext length, even though this is redundant with i_size.
 	 */
 
-	if (unlikely(!llcrypt_policy_has_filename_enc(inode))) {
+	if (!llcrypt_policy_has_filename_enc(inode)) {
 		cstr.name = (unsigned char *)caddr;
 		cstr.len = strlen(cstr.name);
 

libcfs/libcfs/crypto/policy.c
@@ -307,6 +307,7 @@ int llcrypt_ioctl_set_policy(struct file *filp, const void __user *arg)
 	union llcrypt_policy policy;
 	union llcrypt_policy existing_policy;
 	struct inode *inode = file_inode(filp);
+	struct lustre_sb_info *lsi = s2lsi(inode->i_sb);
 	u8 version;
 	int size;
 	int ret;
@@ -333,6 +334,26 @@ int llcrypt_ioctl_set_policy(struct file *filp, const void __user *arg)
 	if (copy_from_user(&policy, arg, size))
 		return -EFAULT;
 	policy.version = version;
+
+	/* Force file/directory name encryption policy to null if
+	 * LSI_FILENAME_ENC flag is not set on sb.
+	 * This allows enabling filename encryption separately from data
+	 * encryption, and can be useful for interoperability with
+	 * encryption-unaware clients.
+	 */
+	if (!(lsi->lsi_flags & LSI_FILENAME_ENC)) {
+		CWARN("inode %lu: forcing policy filenames_encryption_mode to null\n",
+		      inode->i_ino);
+		cfs_tty_write_msg("\n\nForcing policy filenames_encryption_mode to null.\n\n");
+		switch (policy.version) {
+		case LLCRYPT_POLICY_V1:
+			policy.v1.filenames_encryption_mode = LLCRYPT_MODE_NULL;
+			break;
+		case LLCRYPT_POLICY_V2:
+			policy.v2.filenames_encryption_mode = LLCRYPT_MODE_NULL;
+			break;
+		}
+	}
 
 	if (!inode_owner_or_capable(&init_user_ns, inode))
 		return -EACCES;

commit 4887aff7f10423674939682a2fd56f0490224e6e

LU-15911 enc: null encrypted names is embedded llcrypt only
enable_filename_encryption tunable only makes sense when Lustre client
is built against embedded llcrypt. When built against in-kernel
fscrypt, this tunable is silently ignored, as fscrypt always carries
out file name encryption.

So have the enable_filename_encryption tunable only when Lustre client
is built against embedded llcrypt. Also fix sanity-sec test_54 so that
it works for in-kernel fscrypt.

Fixes: e68d496ada ("LU-15858 sec: reinstate null encryption for file names")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Ibe52feb670a00c9f421907ecd438bcccc62856f0
Reviewed-on: https://review.whamcloud.com/47520
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -1305,12 +1305,15 @@ int ll_fill_super(struct super_block *sb)
 	if (err)
 		GOTO(out_free_cfg, err);
 
+	/* LSI_FILENAME_ENC is only used by embedded llcrypt */
+#ifdef CONFIG_LL_ENCRYPTION
 	if (ll_sb_has_test_dummy_encryption(sb))
 		/* enable filename encryption by default for dummy enc mode */
 		lsi->lsi_flags |= LSI_FILENAME_ENC;
 	else
 		/* filename encryption is disabled by default */
 		lsi->lsi_flags &= ~LSI_FILENAME_ENC;
+#endif
 
 	/* kernel >= 2.6.38 store dentry operations in sb->s_d_op. */
 	sb->s_d_op = &ll_d_ops;

lustre/llite/lproc_llite.c
@@ -1642,6 +1642,7 @@ static ssize_t ll_nosquash_nids_seq_write(struct file *file,
 
 LDEBUGFS_SEQ_FOPS(ll_nosquash_nids);
 
+#ifdef CONFIG_LL_ENCRYPTION
 static int ll_filename_enc_seq_show(struct seq_file *m, void *v)
 {
 	struct super_block *sb = m->private;
@@ -1686,6 +1687,7 @@ static ssize_t ll_filename_enc_seq_write(struct file *file,
 }
 
 LDEBUGFS_SEQ_FOPS(ll_filename_enc);
+#endif /* CONFIG_LL_ENCRYPTION */
 
 static int ll_pcc_seq_show(struct seq_file *m, void *v)
 {
@@ -1741,8 +1743,10 @@ struct ldebugfs_vars lprocfs_llite_obd_vars[] = {
 	  .fops	=	&ll_nosquash_nids_fops			},
 	{ .name =	"pcc",
 	  .fops =	&ll_pcc_fops,				},
+#ifdef CONFIG_LL_ENCRYPTION
 	{ .name =	"enable_filename_encryption",
 	  .fops =	&ll_filename_enc_fops,			},
+#endif
 	{ NULL }
 };
 

lustre/tests/sanity-sec.sh
@@ -4318,10 +4318,12 @@ test_54() {
 
 	which fscrypt || skip "This test needs fscrypt userspace tool"
 
-	fscrypt setup --force --verbose || error "fscrypt global setup failed"
+	yes | fscrypt setup --force --verbose ||
+		error "fscrypt global setup failed"
 	sed -i 's/\(.*\)policy_version\(.*\):\(.*\)\"[0-9]*\"\(.*\)/\1policy_version\2:\3"2"\4/' \
 		/etc/fscrypt.conf
-	fscrypt setup --verbose $MOUNT || error "fscrypt setup $MOUNT failed"
+	yes | fscrypt setup --verbose $MOUNT ||
+		error "fscrypt setup $MOUNT failed"
 	mkdir -p $testdir
 	chown -R $ID0:$ID0 $testdir
 
@@ -4357,9 +4359,12 @@ test_54() {
 	[ $filecount -eq 3 ] || error "found $filecount files"
 
 	# check enable_filename_encryption default value
+	# tunable only available for client built against embedded llcrypt
 	$LCTL get_param mdc.*.connect_flags | grep -q name_encryption &&
 	  nameenc=$(lctl get_param -n llite.*.enable_filename_encryption |
 			head -n1)
+	# If client is built against in-kernel fscrypt, it is not possible
+	# to decide to encrypt file names or not: they are always encrypted.
 	if [ -n "$nameenc" ]; then
 		[ $nameenc -eq 0 ] ||
 		       error "enable_filename_encryption should be 0 by default"
@@ -4437,7 +4442,7 @@ test_54() {
 
 	# setup encryption from inside this subdir mount
 	# the .fscrypt directory is going to be created at the real fs root
-	fscrypt setup --verbose $MOUNT ||
+	yes | fscrypt setup --verbose $MOUNT ||
 		error "fscrypt setup $MOUNT failed (2)"
 	testdir=$MOUNT/vault
 	mkdir $testdir
@@ -4445,7 +4450,7 @@ test_54() {
 	fid1=$(path2fid $MOUNT/.fscrypt)
 	echo "With FILESET $tdir, .fscrypt FID is $fid1"
 
-	# enable name encryption
+	# enable name encryption, only valid if built against embedded llcrypt
 	if [ -n "$nameenc" ]; then
 		do_facet mgs $LCTL set_param -P \
 			llite.*.enable_filename_encryption=1
@@ -4517,7 +4522,7 @@ test_54() {
 	$RUNAS fscrypt lock --verbose $DIR/$tdir/vault ||
 		error "fscrypt lock $DIR/$tdir/vault failed (5)"
 
-	# disable name encryption
+	# disable name encryption, only valid if built against embedded llcrypt
 	if [ -n "$nameenc" ]; then
 		do_facet mgs $LCTL set_param -P \
 			llite.*.enable_filename_encryption=0