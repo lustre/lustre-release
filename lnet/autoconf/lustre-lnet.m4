lustre/lustre-release

commit ec03c9628caeba6fe4dacc4a8b9fe307f1d8af0b

LU-15417 build: find the new path for MOFED 5.5
The path of the mofed header files has change to
/usr/src/ofa_kernel/x86_64/<kernel>,
so we cannot assume it's /usr/src/ofa_kernel/default.

Besides updating lbuild, we also need to update
lustre-lnet.m4 and lustre.spec.in.

Test-Parameters: trivial

Change-Id: Iab42ce9e458f78b0dc0233ac6fd23a1760be5324
Fixes: 94a3f1bfa70 ("LU-15417 build: build MOFED 5.5")
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/46383
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Minh Diep <mdiep@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/autoconf/lustre-lnet.m4
@@ -66,7 +66,9 @@ case $with_o2ib in
 				OFED_INFO="ofed_info"
 				LSPKG="rpm -ql"
 			])
-			O2IBPATHS=$(eval $OFED_INFO | egrep -w 'mlnx-ofed-kernel-dkms|mlnx-ofa_kernel-devel|compat-rdma-devel|kernel-ib-devel|ofa_kernel-devel' | xargs $LSPKG | grep '\(/openib\|/ofa_kernel/default\|/ofa_kernel\)$' | head -n1)
+			O2IBPATHS=$(eval $OFED_INFO |
+				    egrep -w 'mlnx-ofed-kernel-dkms|mlnx-ofa_kernel-devel|compat-rdma-devel|kernel-ib-devel|ofa_kernel-devel' |
+				    xargs $LSPKG | grep -v 'ofa_kernel-' | grep rdma_cm.h | sed 's/\/include\/rdma\/rdma_cm.h//')
 			AS_IF([test -z "$O2IBPATHS"], [
 				AC_MSG_ERROR([
 You seem to have an OFED installed but have not installed it's devel package.

lustre.spec.in
@@ -472,6 +472,18 @@ fi
 %{?_openmpi_load}
 %{?_mpich_load}
 
+%if %{with mofed}
+	o2ib_path=$(find /usr/src/ofa_kernel/ -type d -name ofed_scripts | xargs dirname)
+	if [ -z "$o2ib_path" ]; then
+		echo "ERROR: could not find OFED devel headers"
+		exit 1
+	fi
+	if [ $(echo $o2ib_path | wc -w) -ge 2 ]; then
+		echo "ERROR: multiple OFED versions installed"
+		exit 1
+	fi
+%endif
+
 %eval_configure $CONFIGURE_ARGS \
 	%{?with_lustre_tests:--enable-tests}%{!?with_lustre_tests:--disable-tests} \
 	%{?with_lustre_utils:--enable-utils}%{!?with_lustre_utils:--disable-utils} \
@@ -488,7 +500,7 @@ fi
 	%{!?with_manpages:--disable-manpages} \
 	%{!?with_systemd:--with-systemdsystemunitdir=no} \
 	%{?with_systemd:--with-systemdsystemunitdir=%{_unitdir}} \
-	%{?with_mofed:--with-o2ib=/usr/src/ofa_kernel/default} \
+	%{?with_mofed:--with-o2ib=$o2ib_path} \
 	--with-linux=%{kdir} \
 	--with-linux-obj=%{kobjdir} \
 	--with-kmp-moddir=%{kmoddir}/%{name}

commit 3a7930e63c15b0fbe51ac73db81a1186939115bb

LU-16050 build: replace ofed_info with dpkg/rpm
After installing MLNX_OFED by running mlnxofedinstall command,
mlnx-ofed-kernel-modules package is not listed by ofed_info,
which causes Lustre configure fail as follows:

checking whether to use Compat RDMA... /usr/bin/ofed_info
dpkg-query: error: --listfiles needs at least one package name argument

This patch fixes the above issue by replacing ofed_info with
"dpkg -l" and "rpm -qa" commands to find OFED package.

Test-Parameters: trivial
Fixes: ec03c9628cae ("LU-15417 build: find the new path for MOFED 5.5")
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Change-Id: Ia3c2d6bf10e147ca2761221741eff6f93008556c
Reviewed-on: https://review.whamcloud.com/48047
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Gaurang Tapase <gtapase@ddn.com>
Reviewed-by: Minh Diep <mdiep@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/autoconf/lustre-lnet.m4
@@ -60,15 +60,33 @@ AC_ARG_WITH([o2ib],
 case $with_o2ib in
 	yes)    AS_IF([which ofed_info 2>/dev/null], [
 			AS_IF([test x$uses_dpkg = xyes], [
-				OFED_INFO="ofed_info | awk '{print \[$]2}'"
+				LIST_ALL_PKG="dpkg -l | awk '{print \[$]2}'"
 				LSPKG="dpkg --listfiles"
 			], [
-				OFED_INFO="ofed_info"
+				LIST_ALL_PKG="rpm -qa"
 				LSPKG="rpm -ql"
 			])
-			O2IBPATHS=$(eval $OFED_INFO |
-				    egrep -w 'mlnx-ofed-kernel-dkms|mlnx-ofa_kernel-devel|compat-rdma-devel|kernel-ib-devel|ofa_kernel-devel' |
-				    xargs $LSPKG | grep -v 'ofa_kernel-' | grep rdma_cm.h | sed 's/\/include\/rdma\/rdma_cm.h//')
+
+			O2IBPKG="mlnx-ofed-kernel-dkms"
+			O2IBPKG+="|mlnx-ofed-kernel-modules"
+			O2IBPKG+="|mlnx-ofa_kernel-devel"
+			O2IBPKG+="|compat-rdma-devel"
+			O2IBPKG+="|kernel-ib-devel"
+			O2IBPKG+="|ofa_kernel-devel"
+
+			O2IBDIR="/ofa_kernel"
+			O2IBDIR+="|/ofa_kernel/default"
+			O2IBDIR+="|/openib"
+
+			O2IBDIR_PATH=$(eval $LIST_ALL_PKG |
+				       egrep -w "$O2IBPKG" | xargs $LSPKG |
+				       egrep "${O2IBDIR}$" | head -n1)
+
+			if test -n "$O2IBDIR_PATH"; then
+				O2IBPATHS=$(find $O2IBDIR_PATH -name rdma_cm.h |
+					sed -e 's/\/include\/rdma\/rdma_cm.h//')
+			fi
+
 			AS_IF([test -z "$O2IBPATHS"], [
 				AC_MSG_ERROR([
 You seem to have an OFED installed but have not installed it's devel package.