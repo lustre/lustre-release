lustre/lustre-release

commit 2ad263c602f315a6a4b1c7b545eb17d045cce6b0

LU-1187 utils: add lfs setdirstripe/getdirstripe
1.Add lfs setdirstripe/mkdir/getdirstripe to create
remote directory.

2.Add lfs rm_entry to just remove the entry of remote
directory.

Change-Id: Idaecfbb9dd97e3ae9fbc70e5b902a91a7b2ed18e
Signed-off-by: Wang Di <di.wang@intel.com>
Reviewed-on: http://review.whamcloud.com/4341
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>

lustre/utils/lfs.c
@@ -78,6 +78,9 @@
 static int lfs_setstripe(int argc, char **argv);
 static int lfs_find(int argc, char **argv);
 static int lfs_getstripe(int argc, char **argv);
+static int lfs_getdirstripe(int argc, char **argv);
+static int lfs_setdirstripe(int argc, char **argv);
+static int lfs_rmentry(int argc, char **argv);
 static int lfs_osts(int argc, char **argv);
 static int lfs_mdts(int argc, char **argv);
 static int lfs_df(int argc, char **argv);
@@ -141,6 +144,27 @@ command_t cmdlist[] = {
          "                 [--pool|-p] [--stripe-size|-S] [--directory|-d]\n"
          "                 [--mdt-index|-M] [--recursive|-r] [--raw|-R]\n"
          "                 <directory|filename> ..."},
+	{"setdirstripe", lfs_setdirstripe, 0,
+	 "To create a remote directory on a specified MDT.\n"
+	 "usage: setdirstripe <--index|-i mdt_index> <dir>\n"
+	 "\tmdt_index:    MDT index of first stripe\n"},
+	{"getdirstripe", lfs_getdirstripe, 0,
+	 "To list the striping info for a given directory\n"
+	 "or recursively for all directories in a directory tree.\n"
+	 "usage: getdirstripe [--obd|-O <uuid>] [--quiet|-q] [--verbose|-v]\n"
+	 "		 [--count|-c ] [--index|-i ] [--raw|-R]\n"
+	 "		 [--recursive | -r] <dir> ..."},
+	{"mkdir", lfs_setdirstripe, 0,
+	 "To create a remote directory on a specified MDT. And this can only\n"
+	 "be done on MDT0 by administrator.\n"
+	 "usage: mkdir <--index|-i mdt_index> <dir>\n"
+	 "\tmdt_index:    MDT index of the remote directory.\n"},
+	{"rm_entry", lfs_rmentry, 0,
+	 "To remove the name entry of the remote directory. Note: This\n"
+	 "command will only delete the name entry, i.e. the remote directory\n"
+	 "will become inaccessable after this command. This can only be done\n"
+	 "by the administrator\n"
+	 "usage: rm_entry <dir>\n"},
         {"pool_list", lfs_poollist, 0,
          "List pools or pool OSTs\n"
          "usage: pool_list <fsname>[.<pool>] | <pathname>\n"},
@@ -919,7 +943,8 @@ err:
         return ret;
 }
 
-static int lfs_getstripe(int argc, char **argv)
+static int lfs_getstripe_internal(int argc, char **argv,
+				  struct find_param *param)
 {
         struct option long_opts[] = {
 #if LUSTRE_VERSION >= OBD_OCD_VERSION(2,9,50,0)
@@ -974,127 +999,126 @@ static int lfs_getstripe(int argc, char **argv)
                 {0, 0, 0, 0}
         };
         int c, rc;
-        struct find_param param = { 0 };
 
-        param.maxdepth = 1;
-        optind = 0;
-        while ((c = getopt_long(argc, argv, "cdghiMoO:pqrRsSv",
-                                long_opts, NULL)) != -1) {
-                switch (c) {
-                case 'O':
-                        if (param.obduuid) {
-                                fprintf(stderr,
-                                        "error: %s: only one obduuid allowed",
-                                        argv[0]);
-                                return CMD_HELP;
-                        }
-                        param.obduuid = (struct obd_uuid *)optarg;
-                        break;
-                case 'q':
-                        param.quiet++;
-                        break;
-                case 'd':
-                        param.maxdepth = 0;
-                        break;
-                case 'r':
-                        param.recursive = 1;
-                        break;
-                case 'v':
-                        param.verbose = VERBOSE_ALL | VERBOSE_DETAIL;
-                        break;
-                case 'c':
+	param->maxdepth = 1;
+	optind = 0;
+	while ((c = getopt_long(argc, argv, "cdghiMoO:pqrRsSv",
+				long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'O':
+			if (param->obduuid) {
+				fprintf(stderr,
+					"error: %s: only one obduuid allowed",
+					argv[0]);
+				return CMD_HELP;
+			}
+			param->obduuid = (struct obd_uuid *)optarg;
+			break;
+		case 'q':
+			param->quiet++;
+			break;
+		case 'd':
+			param->maxdepth = 0;
+			break;
+		case 'r':
+			param->recursive = 1;
+			break;
+		case 'v':
+			param->verbose = VERBOSE_ALL | VERBOSE_DETAIL;
+			break;
+		case 'c':
 #if LUSTRE_VERSION >= OBD_OCD_VERSION(2,9,50,0)
 #warning "remove deprecated --count option"
 #elif LUSTRE_VERSION >= OBD_OCD_VERSION(2,6,50,0)
-                        if (strcmp(argv[optind - 1], "--count") == 0)
-                                fprintf(stderr, "warning: '--count' deprecated,"
-                                        " use '--stripe-count' instead\n");
+			if (strcmp(argv[optind - 1], "--count") == 0)
+				fprintf(stderr, "warning: '--count' deprecated,"
+					" use '--stripe-count' instead\n");
 #endif
-                        if (!(param.verbose & VERBOSE_DETAIL)) {
-                                param.verbose |= VERBOSE_COUNT;
-                                param.maxdepth = 0;
-                        }
-                        break;
-                case 's':
+			if (!(param->verbose & VERBOSE_DETAIL)) {
+				param->verbose |= VERBOSE_COUNT;
+				param->maxdepth = 0;
+			}
+			break;
+		case 's':
 #if LUSTRE_VERSION >= OBD_OCD_VERSION(2,9,50,0)
 #warning "remove deprecated --size option"
 #elif LUSTRE_VERSION >= OBD_OCD_VERSION(2,6,50,0)
-                        fprintf(stderr, "warning: '--size|-s' deprecated, "
-                                "use '--stripe-size|-S' instead\n");
+			fprintf(stderr, "warning: '--size|-s' deprecated, "
+				"use '--stripe-size|-S' instead\n");
 #endif
-                case 'S':
-                        if (!(param.verbose & VERBOSE_DETAIL)) {
-                                param.verbose |= VERBOSE_SIZE;
-                                param.maxdepth = 0;
-                        }
-                        break;
-                case 'o':
+		case 'S':
+			if (!(param->verbose & VERBOSE_DETAIL)) {
+				param->verbose |= VERBOSE_SIZE;
+				param->maxdepth = 0;
+			}
+			break;
+		case 'o':
 #if LUSTRE_VERSION >= OBD_OCD_VERSION(2,4,50,0)
-                        fprintf(stderr, "warning: '--offset|-o' deprecated, "
-                                "use '--stripe-index|-i' instead\n");
+			fprintf(stderr, "warning: '--offset|-o' deprecated, "
+				"use '--stripe-index|-i' instead\n");
 #else
-                        if (strcmp(argv[optind - 1], "--offset") == 0)
-                                /* need --stripe-index established first */
-                                fprintf(stderr, "warning: '--offset' deprecated"
-                                        ", use '--index' instead\n");
+			if (strcmp(argv[optind - 1], "--offset") == 0)
+				/* need --stripe-index established first */
+				fprintf(stderr, "warning: '--offset' deprecated"
+					", use '--index' instead\n");
 #endif
-                case 'i':
+		case 'i':
 #if LUSTRE_VERSION >= OBD_OCD_VERSION(2,9,50,0)
 #warning "remove deprecated --offset and --index options"
 #elif LUSTRE_VERSION >= OBD_OCD_VERSION(2,6,50,0)
-                        if (strcmp(argv[optind - 1], "--index") == 0)
-                                fprintf(stderr, "warning: '--index' deprecated"
-                                        ", use '--stripe-index' instead\n");
+			if (strcmp(argv[optind - 1], "--index") == 0)
+				fprintf(stderr, "warning: '--index' deprecated"
+					", use '--stripe-index' instead\n");
 #endif
-                        if (!(param.verbose & VERBOSE_DETAIL)) {
-                                param.verbose |= VERBOSE_OFFSET;
-                                param.maxdepth = 0;
-                        }
-                        break;
-                case 'p':
-                        if (!(param.verbose & VERBOSE_DETAIL)) {
-                                param.verbose |= VERBOSE_POOL;
-                                param.maxdepth = 0;
-                        }
-                        break;
-                case 'g':
-                        if (!(param.verbose & VERBOSE_DETAIL)) {
-                                param.verbose |= VERBOSE_GENERATION;
-                                param.maxdepth = 0;
-                        }
-                        break;
-                case 'M':
-                        if (!(param.verbose & VERBOSE_DETAIL))
-                                param.maxdepth = 0;
-                        param.verbose |= VERBOSE_MDTINDEX;
-                        break;
-                case 'R':
-                        param.raw = 1;
-                        break;
-                default:
-                        return CMD_HELP;
-                }
-        }
+			if (!(param->verbose & VERBOSE_DETAIL)) {
+				param->verbose |= VERBOSE_OFFSET;
+				param->maxdepth = 0;
+			}
+			break;
+		case 'p':
+			if (!(param->verbose & VERBOSE_DETAIL)) {
+				param->verbose |= VERBOSE_POOL;
+				param->maxdepth = 0;
+			}
+			break;
+		case 'g':
+			if (!(param->verbose & VERBOSE_DETAIL)) {
+				param->verbose |= VERBOSE_GENERATION;
+				param->maxdepth = 0;
+			}
+			break;
+		case 'M':
+			if (!(param->verbose & VERBOSE_DETAIL))
+				param->maxdepth = 0;
+			param->verbose |= VERBOSE_MDTINDEX;
+			break;
+		case 'R':
+			param->raw = 1;
+			break;
+		default:
+			return CMD_HELP;
+		}
+	}
 
-        if (optind >= argc)
-                return CMD_HELP;
+	if (optind >= argc)
+		return CMD_HELP;
 
-        if (param.recursive)
-                param.maxdepth = -1;
+	if (param->recursive)
+		param->maxdepth = -1;
 
-        if (!param.verbose)
-                param.verbose = VERBOSE_ALL;
-        if (param.quiet)
-                param.verbose = VERBOSE_OBJID;
+	if (!param->verbose)
+		param->verbose = VERBOSE_ALL;
+	if (param->quiet)
+		param->verbose = VERBOSE_OBJID;
 
-        do {
-                rc = llapi_getstripe(argv[optind], &param);
-        } while (++optind < argc && !rc);
+	do {
+		rc = llapi_getstripe(argv[optind], param);
+	} while (++optind < argc && !rc);
 
-        if (rc)
-                fprintf(stderr, "error: %s failed for %s.\n",
-                        argv[0], argv[optind - 1]);
-        return rc;
+	if (rc)
+		fprintf(stderr, "error: %s failed for %s.\n",
+			argv[0], argv[optind - 1]);
+	return rc;
 }
 
 static int lfs_tgts(int argc, char **argv)
@@ -1133,6 +1157,117 @@ static int lfs_tgts(int argc, char **argv)
         }
 
         return rc;
+}
+
+static int lfs_getstripe(int argc, char **argv)
+{
+	struct find_param param = { 0 };
+	return lfs_getstripe_internal(argc, argv, &param);
+}
+
+/* functions */
+static int lfs_getdirstripe(int argc, char **argv)
+{
+	struct find_param param = { 0 };
+
+	param.get_lmv = 1;
+	return lfs_getstripe_internal(argc, argv, &param);
+}
+
+/* functions */
+static int lfs_setdirstripe(int argc, char **argv)
+{
+	char *dname;
+	int result;
+	int  st_offset, st_count;
+	char *end;
+	int c;
+	char *stripe_off_arg = NULL;
+	int  flags = 0;
+
+	struct option long_opts[] = {
+		{"index",    required_argument, 0, 'i'},
+		{0, 0, 0, 0}
+	};
+
+	st_offset = -1;
+	st_count = 1;
+	optind = 0;
+	while ((c = getopt_long(argc, argv, "i:o",
+				long_opts, NULL)) >= 0) {
+		switch (c) {
+		case 0:
+			/* Long options. */
+			break;
+		case 'i':
+			stripe_off_arg = optarg;
+			break;
+		default:
+			fprintf(stderr, "error: %s: option '%s' "
+					"unrecognized\n",
+					argv[0], argv[optind - 1]);
+			return CMD_HELP;
+		}
+	}
+
+	if (optind == argc) {
+		fprintf(stderr, "error: %s: missing dirname\n",
+			argv[0]);
+		return CMD_HELP;
+	}
+
+	dname = argv[optind];
+	if (stripe_off_arg == NULL) {
+		fprintf(stderr, "error: %s: missing stripe_off.\n",
+			argv[0]);
+		return CMD_HELP;
+	}
+	/* get the stripe offset */
+	st_offset = strtoul(stripe_off_arg, &end, 0);
+	if (*end != '\0') {
+		fprintf(stderr, "error: %s: bad stripe offset '%s'\n",
+			argv[0], stripe_off_arg);
+		return CMD_HELP;
+	}
+	do {
+		result = llapi_dir_create_pool(dname, flags, st_offset,
+					       st_count, 0, NULL);
+		if (result) {
+			fprintf(stderr, "error: %s: create stripe dir '%s' "
+				"failed\n", argv[0], dname);
+			break;
+		}
+		dname = argv[++optind];
+	} while (dname != NULL);
+
+	return result;
+}
+
+/* functions */
+static int lfs_rmentry(int argc, char **argv)
+{
+	char *dname;
+	int   index;
+	int   result = 0;
+
+	if (argc <= 1) {
+		fprintf(stderr, "error: %s: missing dirname\n",
+			argv[0]);
+		return CMD_HELP;
+	}
+
+	index = 1;
+	dname = argv[index];
+	while (dname != NULL) {
+		result = llapi_direntry_remove(dname);
+		if (result) {
+			fprintf(stderr, "error: %s: remove dir entry '%s' "
+				"failed\n", argv[0], dname);
+			break;
+		}
+		dname = argv[++index];
+	}
+	return result;
 }
 
 static int lfs_osts(int argc, char **argv)

lustre/include/lustre/lustre_idl.h
@@ -1878,14 +1878,14 @@ typedef enum {
  */
 
 typedef enum {
-        REINT_SETATTR  = 1,
-        REINT_CREATE   = 2,
-        REINT_LINK     = 3,
-        REINT_UNLINK   = 4,
-        REINT_RENAME   = 5,
-        REINT_OPEN     = 6,
-        REINT_SETXATTR = 7,
-//      REINT_CLOSE    = 8,
+	REINT_SETATTR  = 1,
+	REINT_CREATE   = 2,
+	REINT_LINK     = 3,
+	REINT_UNLINK   = 4,
+	REINT_RENAME   = 5,
+	REINT_OPEN     = 6,
+	REINT_SETXATTR = 7,
+	REINT_RMENTRY  = 8,
 //      REINT_WRITE    = 9,
         REINT_MAX
 } mds_reint_t, mdt_reint_t;

lustre/include/lustre/lustre_user.h
@@ -109,6 +109,40 @@ struct obd_statfs {
         __u32           os_spare9;
 };
 
+/**
+ * File IDentifier.
+ *
+ * FID is a cluster-wide unique identifier of a file or an object (stripe).
+ * FIDs are never reused.
+ **/
+struct lu_fid {
+       /**
+	* FID sequence. Sequence is a unit of migration: all files (objects)
+	* with FIDs from a given sequence are stored on the same server.
+	* Lustre should support 2^64 objects, so even if each sequence
+	* has only a single object we can still enumerate 2^64 objects.
+	**/
+	__u64 f_seq;
+	/* FID number within sequence. */
+	__u32 f_oid;
+	/**
+	 * FID version, used to distinguish different versions (in the sense
+	 * of snapshots, etc.) of the same file system object. Not currently
+	 * used.
+	 **/
+	__u32 f_ver;
+};
+
+struct filter_fid {
+	struct lu_fid   ff_parent;  /* ff_parent.f_ver == file stripe number */
+	__u64	   ff_objid;
+	__u64	   ff_seq;
+};
+
+/* Userspace should treat lu_fid as opaque, and only use the following methods
+ * to print or parse them.  Other functions (e.g. compare, swab) could be moved
+ * here from lustre_idl.h if needed. */
+typedef struct lu_fid lustre_fid;
 
 /*
  * The ioctl naming rules:
@@ -168,6 +202,10 @@ struct obd_statfs {
 						struct hsm_current_action)
 /* see <lustre_lib.h> for ioctl numbers 221-232 */
 
+#define LL_IOC_LMV_SETSTRIPE	    _IOWR('f', 240, struct lmv_user_md)
+#define LL_IOC_LMV_GETSTRIPE	    _IOWR('f', 241, struct lmv_user_md)
+#define LL_IOC_REMOVE_ENTRY	    _IOWR('f', 242, __u64)
+
 #define LL_STATFS_LMV           1
 #define LL_STATFS_LOV           2
 #define LL_STATFS_NODELAY	4
@@ -204,6 +242,9 @@ struct obd_statfs {
 #define LOV_USER_MAGIC    LOV_USER_MAGIC_V1
 #define LOV_USER_MAGIC_JOIN_V1 0x0BD20BD0
 #define LOV_USER_MAGIC_V3 0x0BD30BD0
+
+#define LMV_MAGIC_V1      0x0CD10CD0    /*normal stripe lmv magic */
+#define LMV_USER_MAGIC    0x0CD20CD0    /*default lmv magic*/
 
 #define LOV_PATTERN_RAID0 0x001
 #define LOV_PATTERN_RAID1 0x002
@@ -287,6 +328,41 @@ struct lov_user_mds_data_v3 {
 } __attribute__((packed));
 #endif
 
+/* keep this to be the same size as lov_user_ost_data_v1 */
+struct lmv_user_mds_data {
+	struct lu_fid	lum_fid;
+	__u32		lum_padding;
+	__u32		lum_mds;
+};
+
+/* lum_type */
+enum {
+	LMV_STRIPE_TYPE = 0,
+	LMV_DEFAULT_TYPE = 1,
+};
+
+#define lmv_user_md lmv_user_md_v1
+struct lmv_user_md_v1 {
+	__u32	lum_magic;	 /* must be the first field */
+	__u32	lum_stripe_count;  /* dirstripe count */
+	__u32	lum_stripe_offset; /* MDT idx for default dirstripe */
+	__u32	lum_hash_type;     /* Dir stripe policy */
+	__u32	lum_type;	  /* LMV type: default or normal */
+	__u32	lum_padding1;
+	__u32	lum_padding2;
+	__u32	lum_padding3;
+	char	lum_pool_name[LOV_MAXPOOLNAME];
+	struct	lmv_user_mds_data  lum_objects[0];
+};
+
+static inline int lmv_user_md_size(int stripes, int lmm_magic)
+{
+	return sizeof(struct lmv_user_md) +
+		      stripes * sizeof(struct lmv_user_mds_data);
+}
+
+extern void lustre_swab_lmv_user_md(struct lmv_user_md *lum);
+
 struct ll_recreate_obj {
         __u64 lrc_id;
         __u32 lrc_ost_idx;
@@ -348,41 +424,6 @@ static inline void obd_uuid2fsname(char *buf, char *uuid, int buflen)
         if (p)
            *p = '\0';
 }
-
-/**
- * File IDentifier.
- *
- * FID is a cluster-wide unique identifier of a file or an object (stripe).
- * FIDs are never reused.
- */
-struct lu_fid {
-        /**
-         * FID sequence. Sequence is a unit of migration: all files (objects)
-         * with FIDs from a given sequence are stored on the same server.
-         * Lustre should support 2^64 objects, so even if each sequence
-         * has only a single object we can still enumerate 2^64 objects.
-         */
-        __u64 f_seq;
-        /** FID number within sequence. */
-        __u32 f_oid;
-        /**
-         * FID version, used to distinguish different versions (in the sense
-         * of snapshots, etc.) of the same file system object. Not currently
-         * used.
-         */
-        __u32 f_ver;
-};
-
-struct filter_fid {
-        struct lu_fid   ff_parent;  /* ff_parent.f_ver == file stripe number */
-        __u64           ff_objid;
-        __u64           ff_seq;
-};
-
-/* Userspace should treat lu_fid as opaque, and only use the following methods
-   to print or parse them.  Other functions (e.g. compare, swab) could be moved
-   here from lustre_idl.h if needed. */
-typedef struct lu_fid lustre_fid;
 
 /* printf display format
    e.g. printf("file FID is "DFID"\n", PFID(fid)); */

lustre/include/lustre/lustreapi.h
@@ -163,10 +163,13 @@ struct find_param {
         int                    *mdtindexes;
         int                     file_mdtindex;
 
-        int     lumlen;
-        struct  lov_user_mds_data *lmd;
+	int	lumlen;
+	struct  lov_user_mds_data *lmd;
 
         char poolname[LOV_MAXPOOLNAME + 1];
+
+	int			fp_lmv_count;
+	struct lmv_user_md	*fp_lmv_md;
 
         unsigned long long stripesize;
         unsigned long long stripesize_units;
@@ -186,6 +189,10 @@ extern int llapi_getstripe(char *path, struct find_param *param);
 extern int llapi_find(char *path, struct find_param *param);
 
 extern int llapi_file_fget_mdtidx(int fd, int *mdtidx);
+extern int llapi_dir_create_pool(const char *name, int flags, int stripe_offset,
+				 int stripe_count, int stripe_pattern,
+				 char *poolname);
+int llapi_direntry_remove(char *dname);
 extern int llapi_obd_statfs(char *path, __u32 type, __u32 index,
                      struct obd_statfs *stat_buf,
                      struct obd_uuid *uuid_buf);

lustre/include/obd.h
@@ -1248,21 +1248,32 @@ struct md_op_data {
 
         /* Size-on-MDS epoch and flags. */
         __u64                   op_ioepoch;
-        __u32                   op_flags;
+	__u32                   op_flags;
 
         /* Capa fields */
         struct obd_capa        *op_capa1;
         struct obd_capa        *op_capa2;
 
         /* Various operation flags. */
-        __u32                   op_bias;
+	__u32                   op_bias;
 
         /* Operation type */
-        __u32                   op_opc;
+	__u32                   op_opc;
 
         /* Used by readdir */
-        __u32                   op_npages;
-        __u64                   op_offset;
+	__u64                   op_offset;
+
+	/* Used by readdir */
+	__u32                   op_npages;
+
+	/* used to transfer info between the stacks of MD client
+	 * see enum op_cli_flags */
+	__u32			op_cli_flags;
+};
+
+enum op_cli_flags {
+	CLI_SET_MEA	= 1 << 0,
+	CLI_RM_ENTRY	= 1 << 1,
 };
 
 struct md_enqueue_info;

commit 624e78ae80cd60ed1888eb11cbafd9f0ad55dffd

LU-930 docs: add lfs-rm_entry.8 man page
Add man page for "lfs rm_entry" and alias "lfs rmentry".

Test-Parameters: trivial
Fixes: 2ad263c602 ("LU-1187 utils: add lfs setdirstripe/getdirstripe")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I783f23ec8fd0c75c69bcc78c180a07e54dd0c8a1
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49064
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/doc/Makefile.am
@@ -77,6 +77,8 @@ MANFILES =					\
 	lfs-pcc.1				\
 	lfs-project.1				\
 	lfs-quota.1				\
+	lfs-rm_entry.8				\
+	lfs-rmentry.8				\
 	lfs-rmfid.1				\
 	lfs-setdirstripe.1			\
 	lfs-setquota.1				\

lustre/doc/lfs-rm_entry.8
@@ -1,0 +1,36 @@
+.TH LFS-RMENTRY 8 2017-07-25 "Lustre" "Lustre Utilities"
+.SH NAME
+lfs-rmentry \- remove only filename from directory
+.SH SYNOPSIS
+.B lfs rmentry <FILENAME|DIRECTORY>
+.SH DESCRIPTION
+In cases where an MDT is permanently offline, or if there is an inconsistency
+between MDT directory contents and files (e.g. broken striped directory), the
+.B lfs rmentry
+command removes
+.B only
+the filename entry from a directory, but does not try to delete the inode
+corresponding to the filename.  The MDT inode itself (or a local agent inode
+in the case of DNE remote entries) will still remain on the MDT filesystem,
+and will be reconnected to the internal MDT lost+found directory when
+e2fsck is next run.  Since the MDT inode itself is not deleted, any OST
+objects referenced by this inode will also not be destroyed, so no space
+will be released.
+.SH WARNING
+The main reason to use
+.B lfs rmentry
+is to remove filenames or directory names that are permanently inaccessible,
+in order to allow them to be restored from backup, or to avoid application
+errors when accessing these filenames.  It should not be used to delete files
+that are normally accessible, and can only be run by the root user.
+.SH EXAMPLES
+.TP
+.B lfs rmentry /mnt/testfs/mdt7/newdir
+Remove directory entry
+.B newdir
+on MDT0007 since the remote MDT it is referencing is no longer accessible.
+.SH AUTHOR
+The \fBlfs rmentry\fR command is part of the Lustre filesystem.
+.SH SEE ALSO
+.BR lfs (1),
+.BR lfs-rmfid (1)

lustre/doc/lfs-rmentry.8
@@ -1,0 +1,1 @@
+.so man8/lfs-rm_entry.8

lustre/utils/lfs.c
@@ -388,6 +388,7 @@ command_t cmdlist[] = {
 	 "will become inaccessable after this command. This can only be done\n"
 	 "by the administrator\n"
 	 "usage: rm_entry <dir>\n"},
+	{"rmentry", lfs_rmentry, 0, "remove a dir entry, same as 'rm_entry'\n"},
 	{"unlink_foreign", lfs_unlink_foreign, 0,
 	 "To remove the foreign file/dir.\n"
 	 "Note: This is for files/dirs prevented to be removed using\n"