lustre/lustre-release

commit b59835f8b6c81befee23c5c3c0fafb63e98fbc0c

LU-13903 utils: have liblustreapi support Linux client
Handle two difference between the Linux client and the OpenSFS
branch. The Linux client doesn't support the LL_IOC_REMOVE_ENTRY
since its considered a security risk by the VFS maintainers. The
other difference is that struct getinfo_fid2path doesn't name its
union structure which is typical in the Linux kernel.

Test-Parameters: trivial
Change-Id: Ieb9ea749e8728f33621fd6ec28f78892466ec7a4
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/39207
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Olaf Faaland-LLNL <faaland1@llnl.gov>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build.m4
@@ -344,8 +344,10 @@ AS_IF([test $target_cpu = powerpc64], [
 	CC="$CC -m64"
 ])
 
-# UAPI headers, libcfs/include for util headers, lustre/include for liblustreapi and friends
-CPPFLAGS="-I$PWD/libcfs/include -I$PWD/lnet/utils/ -I$PWD/lnet/include/uapi -I$PWD/lustre/include -I$PWD/lustre/include/uapi $CPPFLAGS"
+# libcfs/include for util headers, lustre/include for liblustreapi and friends
+# UAPI headers from OpenSFS are included if modules support is enabled, otherwise
+# it will use the native kernel implementation.
+CPPFLAGS="-I$PWD/libcfs/include -I$PWD/lnet/utils/ -I$PWD/lustre/include $CPPFLAGS"
 
 CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
@@ -667,6 +669,8 @@ AS_IF([test "x$enable_utils" = xyes], [
 	LC_GLIBC_SUPPORT_COPY_FILE_RANGE
 	LC_OPENSSL_SSK
 	LC_OPENSSL_GETSEPOL
+	LC_FID2PATH_ANON_UNION
+	LC_IOC_REMOVE_ENTRY
 ])
 AS_IF([test "x$enable_tests" = xyes], [
 	LC_HAVE_LIBAIO

lustre/autoconf/lustre-core.m4
@@ -66,6 +66,45 @@ AC_CHECK_FUNCS([copy_file_range],
 ]) # LC_GLIBC_SUPPORT_COPY_FILE_RANGE
 
 #
+# LC_FID2PATH_UNION
+#
+AC_DEFUN([LC_FID2PATH_ANON_UNION], [
+AC_MSG_CHECKING([if 'struct getinfo_fid2path' has anony•mous union])
+AC_COMPILE_IFELSE([AC_LANG_SOURCE([
+	#include <linux/lustre/lustre_idl.h>
+
+	int main(void) {
+		struct getinfo_fid2path gf;
+		struct lu_fid root_fid;
+
+		*gf.gf_root_fid = root_fid;
+		return 0;
+	}
+])],[
+	AC_DEFINE(HAVE_FID2PATH_ANON_UNIONS, 1, [union is unnamed])
+	AC_MSG_RESULT("yes")
+])
+]) # LC_FID2PATH_ANON_UNION
+
+#
+# LC_IOC_REMOVE_ENTRY
+#
+AC_DEFUN([LC_IOC_REMOVE_ENTRY], [
+AC_MSG_CHECKING([if ioctl IOC_REMOVE_ENTRY' is supported])
+AC_COMPILE_IFELSE([AC_LANG_SOURCE([
+	#include <sys/ioctl.h>
+	#include <linux/lustre/lustre_ioctl.h>
+
+	int main(void) {
+		return ioctl(0, LL_IOC_REMOVE_ENTRY, NULL);
+	}
+])],[
+	AC_DEFINE(HAVE_IOC_REMOVE_ENTRY, 1,
+		[IOC_REMOVE_ENTRY ioctl exists])
+])
+]) # LC_IOC_REMOVE_ENTRY
+
+#
 # LC_STACK_SIZE
 #
 # Ensure the stack size is at least 8k in Lustre server (all kernels)

lustre/utils/liblustreapi_util.c
@@ -315,6 +315,7 @@ retry_open:
 
 int llapi_direntry_remove(char *dname)
 {
+#ifdef HAVE_IOC_REMOVE_ENTRY
 	char *dirpath = NULL;
 	char *namepath = NULL;
 	char *dir;
@@ -349,6 +350,9 @@ out:
 	if (fd != -1)
 		close(fd);
 	return rc;
+#else
+	return -ENOTSUP;
+#endif
 }
 
 int llapi_unlink_foreign(char *name)

lustre/utils/liblustreapi_fid.c
@@ -160,6 +160,15 @@ out:
 	return rc;
 }
 
+static inline char *get_gf_path(struct getinfo_fid2path *gf)
+{
+#ifndef HAVE_FID2PATH_ANON_UNIONS
+	return gf->gf_u.gf_path;
+#else
+	return gf->gf_path;
+#endif
+}
+
 int llapi_fid2path_at(int mnt_fd, const struct lu_fid *fid,
 		      char *path_buf, int path_buf_size,
 		      long long *recno, int *linkno)
@@ -188,7 +197,7 @@ int llapi_fid2path_at(int mnt_fd, const struct lu_fid *fid,
 		goto out;
 	}
 
-	rc = copy_strip_dne_path(gf->gf_u.gf_path, path_buf, path_buf_size);
+	rc = copy_strip_dne_path(get_gf_path(gf), path_buf, path_buf_size);
 
 	if (recno != NULL)
 		*recno = gf->gf_recno;

commit efc5c8d4de60d394344506f7cfb188eaf04a4bac

LU-16335 build: remove _GNU_SOURCE dependency in lustre_user.h
The lustre_user.h header uses the non-standard strchrnul() function
in userspace.  This will always leads to LC_IOC_REMOVE_ENTRY configure
check to fail, and in the end "lfs rm_entry" always returns -ENOTSUP.

Implement an alternative approach to avoid external dependencies on
the lustre_user.h header.  Also, LC_IOC_REMOVE_ENTRY is itself
unnecessary, the code can check for LL_IOC_REMOVE_ENTRY directly.

Replace the NFS-specific -ENOTSUP error return code with -EOPNOTSUPP.

Fix the compile test_400[ab] checks to not use "-std=c99" to verify
that the uapi headers are usable without this dependency.

Fixes: b59835f8b6 ("LU-13903 utils: have liblustreapi support Linux client")
Fixes: 7a7309fa84 ("LU-13274 uapi: make lustre UAPI headers C99 compliant")
Fixes: 6331eadbd6 ("LU-15420 uapi: avoid gcc-11 -Werror=stringop-overread")
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: If42743a2148c317b8a9b701ceb5d08bac5149f5f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49328
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-build.m4
@@ -693,7 +693,6 @@ AS_IF([test "x$enable_utils" = xno], [enable_tests="no"])
 AS_IF([test "x$enable_utils" = xyes], [
 	LC_OPENSSL_GETSEPOL
 	LC_FID2PATH_ANON_UNION
-	LC_IOC_REMOVE_ENTRY
 ])
 AS_IF([test "x$enable_tests" = xyes], [
 	LC_HAVE_LIBAIO

lustre/autoconf/lustre-core.m4
@@ -92,30 +92,6 @@ CFLAGS="$saved_flags"
 ]) # LC_FID2PATH_ANON_UNION
 
 #
-# LC_IOC_REMOVE_ENTRY
-#
-AC_DEFUN([LC_IOC_REMOVE_ENTRY], [
-saved_flags="$CFLAGS"
-CFLAGS="$CFLAGS -Werror"
-AC_MSG_CHECKING([if ioctl IOC_REMOVE_ENTRY' is supported])
-AC_COMPILE_IFELSE([AC_LANG_SOURCE([
-	#include <sys/ioctl.h>
-	#include <linux/lustre/lustre_ioctl.h>
-
-	int main(void) {
-		return ioctl(0, LL_IOC_REMOVE_ENTRY, NULL);
-	}
-])],[
-	AC_DEFINE(HAVE_IOC_REMOVE_ENTRY, 1,
-		[IOC_REMOVE_ENTRY ioctl exists])
-	AC_MSG_RESULT([yes])
-],[
-	AC_MSG_RESULT([no])
-])
-CFLAGS="$saved_flags"
-]) # LC_IOC_REMOVE_ENTRY
-
-#
 # LC_STACK_SIZE
 #
 # Ensure the stack size is at least 8k in Lustre server (all kernels)

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -2033,7 +2033,11 @@ static inline char *changelog_rec_name(const struct changelog_rec *rec)
 
 static inline char *changelog_rec_sname(const struct changelog_rec *rec)
 {
-	return strchrnul(changelog_rec_name(rec), '\0') + 1;
+	char *str = changelog_rec_name(rec);
+
+	while (*str != '\0')
+		str++;
+	return str + 1;
 }
 
 static inline __kernel_size_t changelog_rec_snamelen(const struct changelog_rec *rec)

lustre/utils/liblustreapi_util.c
@@ -315,7 +315,7 @@ retry_open:
 
 int llapi_direntry_remove(char *dname)
 {
-#ifdef HAVE_IOC_REMOVE_ENTRY
+#ifdef LL_IOC_REMOVE_ENTRY
 	char *dirpath = NULL;
 	char *namepath = NULL;
 	char *dir;
@@ -351,7 +351,7 @@ out:
 		close(fd);
 	return rc;
 #else
-	return -ENOTSUP;
+	return -EOPNOTSUPP;
 #endif
 }
 

lustre/tests/sanity.sh
@@ -25471,18 +25471,19 @@ test_400a() { # LU-1606, was conf-sanity test_74
 	local prefix=/usr/include/lustre
 	local prog
 
-	# Oleg removes c files in his test rig so test if any c files exist
-	[ -z "$(ls -A $LUSTRE_TESTS_API_DIR)" ] && \
-		skip_env "Needed c test files are missing"
+	# Oleg removes .c files in his test rig so test if any c files exist
+	[[ -n "$(ls -A $LUSTRE_TESTS_API_DIR)" ]] ||
+		skip_env "Needed .c test files are missing"
 
 	if ! [[ -d $prefix ]]; then
 		# Assume we're running in tree and fixup the include path.
-		extra_flags+=" -I$LUSTRE/../lnet/include/uapi -I$LUSTRE/include/uapi -I$LUSTRE/include"
-		extra_flags+=" -L$LUSTRE/utils/.lib"
+		extra_flags+=" -I$LUSTRE/../lnet/include/uapi"
+		extra_flags+=" -I$LUSTRE/include/uapi -I$LUSTRE/include"
+		extra_flags+=" -L$LUSTRE/utils/.libs"
 	fi
 
 	for prog in $LUSTRE_TESTS_API_DIR/*.c; do
-		$CC -Wall -Werror -std=c99 $extra_flags -o $out $prog -llustreapi ||
+		$CC -Wall -Werror $extra_flags -o $out $prog -llustreapi ||
 			error "client api broken"
 	done
 	rm -f $out
@@ -25514,7 +25515,7 @@ test_400b() { # LU-1606, LU-5011
 			continue # lustre_ioctl.h is internal header
 		fi
 
-		$CC -Wall -Werror -std=c99 -include $header -c -x c /dev/null -o $out ||
+		$CC -Wall -Werror -include $header -c -x c /dev/null -o $out ||
 			error "cannot compile '$header'"
 	done
 	rm -f $out