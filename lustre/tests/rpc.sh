9b2e574758a5ef6cfc38152a4f4ee568eaa15153
Timothy Day <timday@amazon.com>
2022-12-30 21:29:59 +0000

    LU-15626 tests: Fix shellcheck error for rpc
    This patch addresses the errors and warnings
    reported by shellcheck for rpc.sh. It also
    breaks up the triple nested subshell for better
    readability.
    
    Signed-off-by: Timothy Day <timday@amazon.com>
    Change-Id: I0d4afa83a6b9d4f825f31896a52dd30319b4bf51
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49535
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

--- lustre/tests/rpc.sh
+++ lustre/tests/rpc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-LUSTRE=${LUSTRE:-$(dirname $0)/..}
+LUSTRE=${LUSTRE:-$(dirname "$0")/..}
 
-if [ ! -f $LUSTRE/tests/rpc.sh ]; then
-	LUSTRE=$(cd $(dirname $(which $0))/..; echo $PWD)
+if [[ ! -f "$LUSTRE/tests/rpc.sh" ]]; then
+	FILE_PATH=$(which "$0")
+	DIRECTORY=$(dirname "$FILE_PATH")
+	LUSTRE=$(dirname "$DIRECTORY")
 fi
 
-. $LUSTRE/tests/test-framework.sh
+. "$LUSTRE/tests/test-framework.sh"
 RPC_MODE=true init_test_env
 
 # Reset the trap on ERR set by the framework.  Noticing this failure is the
 # framework's job.
 trap - ERR
 
-log "$HOSTNAME: executing $@"
+log "$HOSTNAME: executing $*"
 # Execute the command
 "$@"
