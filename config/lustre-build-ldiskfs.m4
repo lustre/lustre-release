lustre/lustre-release

commit 3a83078628a409e464884d73a14a50b988eed962

LU-14409 ldiskfs: Add support for SUSE 5.3.18-24.46.1
Linux-commit: f902b216501094495ff75834035656e8119c537f
ext4: fix bogus warning in ext4_update_dx_flag()

The update breaks the ldiskfs pdirop patch which disables
ext4_update_dx_flag.

SUSE 5.3.18-24.46 can directly use the 5.4.0-66-ubuntu20.series

Test-Parameters: trivial
HPE-bug-id: LUS-9684, LUS-9758
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I9271ee80c05715d7dcec78535cfde1e384ba40e9
Reviewed-on: https://review.whamcloud.com/41473
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-ldiskfs.m4
@@ -48,7 +48,24 @@ AS_IF([test x$RHEL_KERNEL = xyes], [
 		esac
 	]
 	)], [LDISKFS_SERIES="5.4.21-ml.series"],
-	    [LDISKFS_SERIES="5.4.21-ml.series"])
+	    [
+		suse_conf=$LINUX_OBJ/include/generated/uapi/linux/suse_version.h
+		suse_vers=$(awk '[$]2 == "SUSE_VERSION" {print [$]3 }' $suse_conf)
+		suse_patchlevel=$(awk '[$]2 == "SUSE_PATCHLEVEL" {print [$]3 }' $suse_conf)
+		echo "$suse_conf $suse_vers $suse_patchlevel  ${suse_vers}sp$suse_patchlevel" >> /tmp/log-nb
+
+		case ${suse_vers}sp${suse_patchlevel} in # (
+		15sp2 ) LDISKFS_SERIES="5.4.21-ml.series"
+		        grep -A3 ext4_update_dx_flag $LINUX/fs/ext4/ext4.h \
+			  | grep ext4_test_inode_flag
+			if test $? -eq 0; then
+				LDISKFS_SERIES="5.4.0-66-ubuntu20.series"
+			fi
+			;; # (
+		15sp3 ) LDISKFS_SERIES="5.3.18-sles15sp2.series"
+			;;
+		esac
+	    ])
 ], [test x$UBUNTU_KERNEL = xyes], [
 	AS_VERSION_COMPARE([$LINUXRELEASE],[5.8.0],[
 	AS_VERSION_COMPARE([$LINUXRELEASE],[5.4.0],[

commit 374f12ba110921df8479d3e8a702200074951459

LU-14409 ldiskfs: remove stray tracing code
These lines should never have landed :-(

Test-Parameters: trivial
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Fixes: 3a83078628a4 ("LU-14409 ldiskfs: Add support for SUSE 5.3.18-24.46.1")
Change-Id: I7720158605cce81721738a5f6640ccb4e0440b09
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49383
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

config/lustre-build-ldiskfs.m4
@@ -33,7 +33,6 @@ AS_IF([test x$RHEL_KERNEL = xyes], [
 		suse_conf=$LINUX_OBJ/include/generated/uapi/linux/suse_version.h
 		suse_vers=$(awk '[$]2 == "SUSE_VERSION" {print [$]3 }' $suse_conf)
 		suse_patchlevel=$(awk '[$]2 == "SUSE_PATCHLEVEL" {print [$]3 }' $suse_conf)
-		echo "$suse_conf $suse_vers $suse_patchlevel  ${suse_vers}sp$suse_patchlevel" >> /tmp/log-nb
 		case ${suse_vers}sp$suse_patchlevel in # (
 		15sp0 ) LDISKFS_SERIES="4.12-sles15.series"
 			if test ! -f $LINUX/arch/x86/kernel/cpu/hygon.c ; then
@@ -56,8 +55,6 @@ AS_IF([test x$RHEL_KERNEL = xyes], [
 		suse_conf=$LINUX_OBJ/include/generated/uapi/linux/suse_version.h
 		suse_vers=$(awk '[$]2 == "SUSE_VERSION" {print [$]3 }' $suse_conf)
 		suse_patchlevel=$(awk '[$]2 == "SUSE_PATCHLEVEL" {print [$]3 }' $suse_conf)
-		echo "$suse_conf $suse_vers $suse_patchlevel  ${suse_vers}sp$suse_patchlevel" >> /tmp/log-nb
-
 		case ${suse_vers}sp${suse_patchlevel} in # (
 		15sp2 ) LDISKFS_SERIES="5.4.21-ml.series"
 		        grep -A3 ext4_update_dx_flag $LINUX/fs/ext4/ext4.h \