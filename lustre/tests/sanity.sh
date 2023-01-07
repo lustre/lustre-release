lustre/lustre-release

commit ffe259f81cda5b5cd9241362308ec26ebad194b8

LU-13055 changelog: use default mask if server has no mask
When registering a new maskless user and server has no specific
mask set then effective mask to be set to DEFAULT value

Fixes: a15eb4f132 ("LU-13055 mdd: per-user changelog names and mask")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: If799cb5cc29c60cce6ef6c987f2e493145e00e31
Reviewed-on: https://review.whamcloud.com/44404
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanity.sh
@@ -16352,7 +16352,7 @@ test_160o() {
 
 	changelog_register --user test_160o -m unlnk+close+open ||
 		error "changelog_register failed"
-	# drop server mask so it doesn't interfere
+
 	do_facet $SINGLEMDS $LCTL --device $mdt \
 				changelog_register -u "Tt3_-#" &&
 		error "bad symbols in name should fail"
@@ -16442,6 +16442,28 @@ test_160p() {
 		error "found $entry_count changelog entries, expected none"
 }
 run_test 160p "Changelog orphan cleanup with no users"
+
+test_160q() {
+	local mdt="$(facet_svc $SINGLEMDS)"
+	local clu
+
+	[[ $PARALLEL != "yes" ]] || skip "skip parallel run"
+	remote_mds_nodsh && skip "remote MDS with nodsh"
+	[ $MDS1_VERSION -ge $(version_code 2.14.54) ] ||
+		skip "Need MDS version at least 2.14.54"
+
+	# set server mask to minimal value like server init does
+	changelog_chmask "MARK"
+	clu=$(do_facet $SINGLEMDS $LCTL --device $mdt changelog_register -n) ||
+		error "changelog_register failed"
+	# check effective mask again, should be treated as DEFMASK now
+	mask=$(do_facet $SINGLEMDS $LCTL get_param \
+				mdd.$mdt.changelog_current_mask -n)
+	do_facet $SINGLEMDS $LCTL --device $mdt changelog_deregister $clu ||
+		error "changelog_deregister failed"
+	[[ $mask == *"HLINK"* ]] || error "mask is not DEFMASK as expected"
+}
+run_test 160q "changelog effective mask is DEFMASK if not set"
 
 test_161a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"

lustre/mdd/mdd_device.c
@@ -1782,6 +1782,11 @@ static int mdd_changelog_user_register(const struct lu_env *env,
 				  CHANGELOG_MINMASK, CHANGELOG_ALLMASK);
 		if (rc)
 			GOTO(out_users, rc);
+	} else if (mdd->mdd_cl.mc_proc_mask == CHANGELOG_MINMASK) {
+		/* a maskless users means default mask but only if server has
+		 * no specific mask set
+		 */
+		rec->cur_mask = CHANGELOG_DEFMASK;
 	}
 
 	if (name && name[0]) {

commit 34556ca18a18a957a63b53877901896539a53d24

LU-16268 mdd: set effective changelog mask correctly
When changelog mask is changed from MINMASK to a particular
value then recalculation is missed, so effective mask could
stay unchanged against expectations.

Patch adds additional check that old mask is MINMASK or not
to decide if mask recalculation is needed.

Test 160o is extended for that issue.

Fixes: ffe259f81cda ("LU-13055 changelog: use default mask if server has no mask")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ia3c93e19daeb71ff1042ebdb555e918faf89f844
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48961
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/mdd/mdd_lproc.c
@@ -155,11 +155,11 @@ mdd_changelog_mask_seq_write(struct file *file, const char __user *buffer,
 
 	mdd->mdd_cl.mc_proc_mask = newmask;
 
-	/* if mask keeps all bits from oldmask then just extend the current
-	 * mask, otherwise the current mask should be recalculated through
-	 * all user masks.
+	/* if oldmask is not MINMASK and newmask keeps all bits from oldmask
+	 * then just extend the current mask, otherwise the current mask
+	 * should be recalculated through all user masks.
 	 */
-	if ((newmask & oldmask) == oldmask) {
+	if (oldmask != CHANGELOG_MINMASK && (newmask & oldmask) == oldmask) {
 		spin_lock(&mdd->mdd_cl.mc_user_lock);
 		mdd->mdd_cl.mc_current_mask |= newmask;
 		spin_unlock(&mdd->mdd_cl.mc_user_lock);

lustre/tests/sanity.sh
@@ -17160,6 +17160,15 @@ test_160o() {
 				mdd.$mdt.changelog_current_mask -n)
 	[[ $mask == *"HLINK"* ]] || error "mask is not DEFMASK as expected"
 
+	if (( $MDS1_VERSION >= $(version_code 2.15.52) )) ; then
+		# set server mask back to some value
+		changelog_chmask "CLOSE,UNLNK"
+		# check effective mask again, should not remain as DEFMASK
+		mask=$(do_facet $SINGLEMDS $LCTL get_param \
+				mdd.$mdt.changelog_current_mask -n)
+		[[ $mask != *"HLINK"* ]] || error "mask is still DEFMASK"
+	fi
+
 	do_facet $SINGLEMDS $LCTL --device $mdt \
 				changelog_deregister -u test_160o ||
 		error "cannot deregister by name"