lustre/lustre-release

commit 99727c7a1a4f4100361f79bf185cb34b70b67d99

LU-4476 kernel: support process namespace containers
PID namespace was introduce to linux to allow the
migration of containers between hosts. This way running
processes could be migrated to a new machine without
interruption due to pid collisions. User namespace creates
a new enviroment for a exist process where in this new
namespace it will have different uid/gid. Outside that
namespace the default uid/gid. For example a unprivileged
application can create user namespace for itself which has
root privilages. Those privilages don't exist outside of
the created namespace. Both of these changed the traditonal
one to one kernel mapping for pids/gids has changed. Since
userland can now have multiple PID namespaces this means that
each namespace in userland could have the same pid assigned.
This is possible since each namespace will be isolated from
each other. In the case of User namespace the application can
report to the kernel its namespace uid/gid instead of its real
uid/gid. Since the same running kernel is used between all
namespaces on a host the pid/gid data that is pushed into a
kernel context will have to map to a unique pid/gid in kernel
space to avoid collisions. This very similar to Lustre using
FIDs to avoid inode collisions. An example of where the mapping
needs to take place are when pid/gid data is pushed via a ioctl.
Also the internal structures of the kernel store uid/gid in the
kernel internal format. Lastly even with lustre userland uid/gid
namespace being consistant to applications the transmitted
uid/gid will be represented in the kernel differently from
server to server. Lustre wire protocols transmitting uid/gid
must handle these cases.

Change-Id: I207492b9a8a762e43ac2dd8fd1fb1b7397505304
Signed-off-by: James Simmons <uja.ornl@gmail.com>
Reviewed-on: http://review.whamcloud.com/8817
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Yang Sheng <yang.sheng@intel.com>
Reviewed-by: Niu Yawei <yawei.niu@intel.com>

lustre/llite/namei.c
@@ -350,9 +350,9 @@ int ll_md_blocking_ast(struct ldlm_lock *lock, struct ldlm_lock_desc *desc,
 __u32 ll_i2suppgid(struct inode *i)
 {
 	if (in_group_p(i->i_gid))
-		return (__u32)i->i_gid;
+		return (__u32)from_kgid(&init_user_ns, i->i_gid);
 	else
-		return (__u32)(-1);
+		return (__u32) __kgid_val(INVALID_GID);
 }
 
 /* Pack the required supplementary groups into the supplied groups array.
@@ -361,35 +361,15 @@ __u32 ll_i2suppgid(struct inode *i)
  * array in case it might be useful.  Not needed if doing an MDS-side upcall. */
 void ll_i2gids(__u32 *suppgids, struct inode *i1, struct inode *i2)
 {
-#if 0
-        int i;
-#endif
+	LASSERT(i1 != NULL);
+	LASSERT(suppgids != NULL);
 
-        LASSERT(i1 != NULL);
-        LASSERT(suppgids != NULL);
+	suppgids[0] = ll_i2suppgid(i1);
 
-        suppgids[0] = ll_i2suppgid(i1);
-
-        if (i2)
-                suppgids[1] = ll_i2suppgid(i2);
-                else
-                        suppgids[1] = -1;
-
-#if 0
-        for (i = 0; i < current_ngroups; i++) {
-                if (suppgids[0] == -1) {
-                        if (current_groups[i] != suppgids[1])
-                                suppgids[0] = current_groups[i];
-                        continue;
-                }
-                if (suppgids[1] == -1) {
-                        if (current_groups[i] != suppgids[0])
-                                suppgids[1] = current_groups[i];
-                        continue;
-                }
-                break;
-        }
-#endif
+	if (i2)
+		suppgids[1] = ll_i2suppgid(i2);
+	else
+		suppgids[1] = -1;
 }
 
 /*
@@ -948,7 +928,8 @@ static int ll_new_node(struct inode *dir, struct qstr *name,
                 GOTO(err_exit, err = PTR_ERR(op_data));
 
 	err = md_create(sbi->ll_md_exp, op_data, tgt, tgt_len, mode,
-			current_fsuid(), current_fsgid(),
+			from_kuid(&init_user_ns, current_fsuid()),
+			from_kgid(&init_user_ns, current_fsgid()),
 			cfs_curproc_cap_pack(), rdev, &request);
 	ll_finish_md_op_data(op_data);
         if (err)

libcfs/autoconf/lustre-libcfs.m4
@@ -233,6 +233,13 @@ LB_LINUX_TRY_COMPILE([
 ])
 ])
 
+# 3.4 introduced process namespace
+AC_DEFUN([LIBCFS_PROCESS_NAMESPACE],[
+LB_CHECK_LINUX_HEADER([linux/uidgid.h], [
+	AC_DEFINE(HAVE_UIDGID_HEADER, 1, [uidgid.h is present])
+])
+])
+
 #
 # FC18 3.7.2-201 unexport sock_map_fd() change to
 # use sock_alloc_file().
@@ -312,6 +319,8 @@ LIBCFS_DUMP_TRACE_ADDRESS
 LC_SHRINK_CONTROL
 # 3.0
 LIBCFS_STACKTRACE_WARNING
+# 3.4
+LIBCFS_PROCESS_NAMESPACE
 # 3.7
 LIBCFS_SOCK_ALLOC_FILE
 # 3.8

libcfs/include/libcfs/curproc.h
@@ -43,6 +43,68 @@
 #ifndef __LIBCFS_CURPROC_H__
 #define __LIBCFS_CURPROC_H__
 
+#if !defined(HAVE_UIDGID_HEADER) || !defined(__KERNEL__)
+
+typedef uid_t kuid_t;
+typedef gid_t kgid_t;
+
+#define INVALID_UID     -1
+#define INVALID_GID     -1
+
+#ifndef __KERNEL__
+struct user_namespace {
+	unsigned int pad;
+};
+
+extern struct user_namespace init_user_ns;
+#endif
+
+static inline uid_t __kuid_val(kuid_t uid)
+{
+	return uid;
+}
+
+static inline gid_t __kgid_val(kgid_t gid)
+{
+	return gid;
+}
+
+static inline kuid_t make_kuid(struct user_namespace *from, uid_t uid)
+{
+	return uid;
+}
+
+static inline kgid_t make_kgid(struct user_namespace *from, gid_t gid)
+{
+	return gid;
+}
+
+static inline uid_t from_kuid(struct user_namespace *to, kuid_t uid)
+{
+	return uid;
+}
+
+static inline gid_t from_kgid(struct user_namespace *to, kgid_t gid)
+{
+	return gid;
+}
+
+static inline bool uid_eq(kuid_t left, kuid_t right)
+{
+	return left == right;
+}
+
+static inline bool uid_valid(kuid_t uid)
+{
+	return (uid != INVALID_UID);
+}
+
+static inline bool gid_valid(kgid_t gid)
+{
+	return (gid != INVALID_GID);
+}
+#endif
+
 int cfs_get_environ(const char *key, char *value, int *val_len);
 
 typedef __u32 cfs_cap_t;

libcfs/include/libcfs/linux/linux-prim.h
@@ -62,7 +62,10 @@
 #ifdef HAVE_LINUX_RANDOM_H
 #include <linux/random.h>
 #endif
-
+#ifdef HAVE_UIDGID_HEADER
+#include <linux/uidgid.h>
+#endif
+#include <linux/user_namespace.h>
 #include <linux/miscdevice.h>
 #include <libcfs/linux/portals_compat25.h>
 #include <asm/div64.h>

libcfs/libcfs/darwin/darwin-curproc.c
@@ -143,6 +143,9 @@ char  *current_comm(void)
 #endif
 }
 
+struct user_namespace init_user_ns __read_mostly;
+EXPORT_SYMBOL(init_user_ns);
+
 void cfs_cap_raise(cfs_cap_t cap) {}
 void cfs_cap_lower(cfs_cap_t cap) {}
 

libcfs/libcfs/linux/linux-module.c
@@ -137,7 +137,7 @@ static long libcfs_ioctl(struct file *file,
 	struct cfs_psdev_file	 pfile;
 	int    rc = 0;
 
-	if (current_fsuid() != 0)
+	if (!capable(CAP_SYS_ADMIN))
 		return -EACCES;
 
 	if ( _IOC_TYPE(cmd) != IOC_LIBCFS_TYPE ||

libcfs/libcfs/user-prim.c
@@ -245,6 +245,9 @@ void *kthread_run(cfs_thread_t func, void *arg, const char namefmt[], ...)
 }
 #endif
 
+struct user_namespace init_user_ns __read_mostly;
+EXPORT_SYMBOL(init_user_ns);
+
 uid_t current_uid(void)
 {
         return getuid();

libcfs/libcfs/winnt/winnt-curproc.c
@@ -58,6 +58,8 @@ struct task_struct this_task =
       /* journal_info */ NULL
     };
 
+struct user_namespace init_user_ns __read_mostly;
+EXPORT_SYMBOL(init_user_ns);
 
 uid_t  current_uid(void)
 {

commit 557bb0004d7ea1d877a44cccdb3d72b9437e47c5

LU-16438 llite: remove false outdated comment
Old commit 99727c7a1 from Lustre 2.6 changed
ll_i2gids() behavior without updating the function
documentation accordingly. Fix it as this is confusing.

Test-Parameters: trivial
Fixes: 99727c7 ("LU-4476 kernel: support process namespace containers")
Change-Id: Iccc50fe6ac9e02de9bae7fd8f91e3e73ff45e327
Signed-off-by: Aurelien Degremont <degremoa@amazon.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49539
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/namei.c
@@ -534,10 +534,7 @@ __u32 ll_i2suppgid(struct inode *i)
 		return (__u32) __kgid_val(INVALID_GID);
 }
 
-/* Pack the required supplementary groups into the supplied groups array.
- * If we don't need to use the groups from the target inode(s) then we
- * instead pack one or more groups from the user's supplementary group
- * array in case it might be useful.  Not needed if doing an MDS-side upcall. */
+/* Pack the required supplementary groups into the supplied groups array. */
 void ll_i2gids(__u32 *suppgids, struct inode *i1, struct inode *i2)
 {
 	LASSERT(i1 != NULL);