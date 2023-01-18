3b28c0d2727e98425fe45a5add4527cc72a39432
Andreas Dilger <adilger@whamcloud.com>
2023-01-18 12:31:29 -0700

    LU-16452 tests: skip interop recovery-small/144a
    Skip recovery-small test_144a for MDS < 2.15.1
    since the fix and its corresponding test were added there.
    
    Fixes: aa6250b741 ("LU-15724 tests: MDT failover hang reproducer")
    Test-Parameters: trivial testlist=recovery-small env=ONLY=144 serverversion=2.14.0
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I77bfdf55d0218aa9e252f742cc90f1c61216d506
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49681
    Tested-by: Maloo <maloo@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Alex Deiter <alex.deiter@gmail.com>
    Reviewed-by: Sarah Liu <sarah@whamcloud.com>

--- lustre/tests/recovery-small.sh
+++ lustre/tests/recovery-small.sh
@@ -3068,8 +3068,8 @@ test_142() {
 run_test 142 "orphan name stub can be cleaned up in startup"
 
 test_143() {
-	[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.13.00) ] &&
-		skip "Need MDS version at least 2.13.00"
+	(( $MDS1_VERSION >= $(version_code 2.13.0) )) ||
+		skip "Need MDS version at least 2.13.0"
 	[ "$PARALLEL" == "yes" ] && skip "skip parallel run"
 
 	local mntpt=$(facet_mntpt $SINGLEMDS)
@@ -3088,6 +3088,8 @@ run_test 143 "orphan cleanup thread shouldn't be blocked even delete failed"
 test_144a() {
 	[[ $($LCTL get_param mdc.*.import) =~ connect_flags.*overstriping ]] ||
 		skip "server does not support overstriping"
+	(( MDS1_VERSION >= $(version_code 2.15.1) )) ||
+		skip "Need MDS version at least 2.15.1"
 
 	local pids=""
 	local setcount=1000
