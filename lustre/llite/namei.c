lustre/lustre-release

commit 53fa81765750e38f7879ed5092fd729c1bdc8a0f

LU-12514 llite: move client mounting from obdclass to llite
Mounting a lustre client is currently handled
in obdclass, using services from llite.
This requires obdclass to load the llite module
and set up inter-module linkage.

The purpose of this was for common code to support both
client and server mounts.  This isn't really a good idea
and need to go. For lustre servers we already use a
separate filesystem type.

So move the mounting code from obdclass/obd_mount to llite/super25
and remove the inter-module linkages.
Add some EXPORT_SYMBOL() so that llite can access some helpers
that remain in obdclass.

Linux-commit: a989830c88149511ee840356d9c1b34304bac576

Change-Id: Ia33bd55a042f90b178156c745a8072b516f00568
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Reviewed-on: https://review.whamcloud.com/37693
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/super25.c
@@ -32,6 +32,8 @@
 
 #define DEBUG_SUBSYSTEM S_LLITE
 
+#define D_MOUNT (D_SUPER | D_CONFIG/*|D_WARNING */)
+
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/version.h>
@@ -93,6 +95,121 @@ const struct super_operations lustre_super_operations =
 	.remount_fs    = ll_remount_fs,
 	.show_options  = ll_show_options,
 };
+
+/**
+ * This is the entry point for the mount call into Lustre.
+ * This is called when a client is mounted, and this is
+ * where we start setting things up.
+ *
+ * @lmd2data data Mount options (e.g. -o flock,abort_recov)
+ */
+static int lustre_fill_super(struct super_block *sb, void *lmd2_data,
+			     int silent)
+{
+	struct lustre_mount_data *lmd;
+	struct lustre_sb_info *lsi;
+	int rc;
+
+	ENTRY;
+
+	CDEBUG(D_MOUNT|D_VFSTRACE, "VFS Op: sb %p\n", sb);
+
+	lsi = lustre_init_lsi(sb);
+	if (!lsi)
+		RETURN(-ENOMEM);
+	lmd = lsi->lsi_lmd;
+
+	/*
+	 * Disable lockdep during mount, because mount locking patterns are
+	 * 'special'.
+	 */
+	lockdep_off();
+
+	/*
+	 * LU-639: the OBD cleanup of last mount may not finish yet, wait here.
+	 */
+	obd_zombie_barrier();
+
+	/* Figure out the lmd from the mount options */
+	if (lmd_parse(lmd2_data, lmd)) {
+		lustre_put_lsi(sb);
+		GOTO(out, rc = -EINVAL);
+	}
+
+	if (!lmd_is_client(lmd)) {
+#ifdef HAVE_SERVER_SUPPORT
+#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 15, 51, 0)
+		static bool printed;
+
+		if (!printed) {
+			LCONSOLE_WARN("%s: mounting server target with '-t lustre' deprecated, use '-t lustre_tgt'\n",
+				      lmd->lmd_profile);
+			printed = true;
+		}
+#endif
+		rc = server_fill_super(sb);
+#else
+		rc = -ENODEV;
+		CERROR("%s: This is client-side-only module, cannot handle server mount: rc = %d\n",
+		       lmd->lmd_profile, rc);
+		lustre_put_lsi(sb);
+#endif
+		GOTO(out, rc);
+	}
+
+	CDEBUG(D_MOUNT, "Mounting client %s\n", lmd->lmd_profile);
+	rc = lustre_start_mgc(sb);
+	if (rc) {
+		lustre_common_put_super(sb);
+		GOTO(out, rc);
+	}
+	/* Connect and start */
+	rc = ll_fill_super(sb);
+	/* ll_file_super will call lustre_common_put_super on failure,
+	 * which takes care of the module reference.
+	 *
+	 * If error happens in fill_super() call, @lsi will be killed there.
+	 * This is why we do not put it here.
+	 */
+out:
+	if (rc) {
+		CERROR("llite: Unable to mount %s: rc = %d\n",
+		       s2lsi(sb) ? lmd->lmd_dev : "<unknown>", rc);
+	} else {
+		CDEBUG(D_SUPER, "%s: Mount complete\n",
+		       lmd->lmd_dev);
+	}
+	lockdep_on();
+	return rc;
+}
+
+/***************** FS registration ******************/
+static struct dentry *lustre_mount(struct file_system_type *fs_type, int flags,
+				   const char *devname, void *data)
+{
+	return mount_nodev(fs_type, flags, data, lustre_fill_super);
+}
+
+static void lustre_kill_super(struct super_block *sb)
+{
+	struct lustre_sb_info *lsi = s2lsi(sb);
+
+	if (lsi && !IS_SERVER(lsi))
+		ll_kill_super(sb);
+
+	kill_anon_super(sb);
+}
+
+/** Register the "lustre" fs type
+ */
+static struct file_system_type lustre_fs_type = {
+	.owner		= THIS_MODULE,
+	.name		= "lustre",
+	.mount		= lustre_mount,
+	.kill_sb	= lustre_kill_super,
+	.fs_flags	= FS_RENAME_DOES_D_MOVE,
+};
+MODULE_ALIAS_FS("lustre");
 
 static int __init lustre_init(void)
 {
@@ -161,10 +278,14 @@ static int __init lustre_init(void)
 	if (rc != 0)
 		GOTO(out_inode_fini_env, rc);
 
-	lustre_register_super_ops(THIS_MODULE, ll_fill_super, ll_kill_super);
+	rc = register_filesystem(&lustre_fs_type);
+	if (rc)
+		GOTO(out_xattr, rc);
 
 	RETURN(0);
 
+out_xattr:
+	ll_xattr_fini();
 out_inode_fini_env:
 	cl_env_put(cl_inode_fini_env, &cl_inode_fini_refcheck);
 out_vvp:
@@ -180,7 +301,7 @@ out_cache:
 
 static void __exit lustre_exit(void)
 {
-	lustre_register_super_ops(NULL, NULL, NULL);
+	unregister_filesystem(&lustre_fs_type);
 
 	llite_tunables_unregister();
 

lustre.spec.in
@@ -646,7 +646,9 @@ fi
 %{_bindir}/lfs
 %{_bindir}/lfs_migrate
 /sbin/mount.lustre
+%if %{with servers}
 /sbin/mount.lustre_tgt
+%endif
 @BASH_COMPLETION_DIR@
 %endif
 

lustre/include/lustre_disk.h
@@ -350,16 +350,18 @@ int target_name2index(const char *svname, u32 *idx, const char **endptr);
 int lustre_put_lsi(struct super_block *sb);
 int lustre_start_simple(char *obdname, char *type, char *uuid,
 			char *s1, char *s2, char *s3, char *s4);
-int lustre_start_mgc(struct super_block *sb);
 #endif /* HAVE_SERVER_SUPPORT */
 int server_name2fsname(const char *svname, char *fsname, const char **endptr);
 void obdname2fsname(const char *tgt, char *fsname, size_t fslen);
 
-void lustre_register_super_ops(struct module *mod,
-			       int (*cfs)(struct super_block *sb),
-			       void (*ksc)(struct super_block *sb));
+int lustre_start_mgc(struct super_block *sb);
 int lustre_common_put_super(struct super_block *sb);
 
+struct lustre_sb_info *lustre_init_lsi(struct super_block *sb);
+int lustre_put_lsi(struct super_block *sb);
+int lmd_parse(char *options, struct lustre_mount_data *lmd);
+
+/* mgc_request.c */
 int mgc_fsname2resid(char *fsname, struct ldlm_res_id *res_id, int type);
 int mgc_logname2resid(char *fsname, struct ldlm_res_id *res_id, int type);
 

lustre/include/obd_class.h
@@ -1829,9 +1829,9 @@ void lustre_deregister_lwp_item(struct obd_export **exp);
 struct obd_export *lustre_find_lwp_by_index(const char *dev, __u32 idx);
 void lustre_notify_lwp_list(struct obd_export *exp);
 int tgt_name2lwp_name(const char *tgt_name, char *lwp_name, int len, __u32 idx);
+int lustre_tgt_register_fs(void);
+void lustre_tgt_unregister_fs(void);
 #endif /* HAVE_SERVER_SUPPORT */
-int lustre_register_fs(void);
-int lustre_unregister_fs(void);
 int lustre_check_exclusion(struct super_block *sb, char *svname);
 
 /* lustre_peer.c    */

lustre/llite/llite_lib.c
@@ -1428,8 +1428,6 @@ out_no_sbi:
 
 	cl_env_cache_purge(~0);
 
-	module_put(THIS_MODULE);
-
 	EXIT;
 } /* client_put_super */
 

lustre/obdclass/class_obd.c
@@ -717,50 +717,52 @@ static int __init obdclass_init(void)
 	if (err != 0)
 		goto cleanup_lu_global;
 
+	err = llog_info_init();
+	if (err)
+		goto cleanup_cl_global;
+
 #ifdef HAVE_SERVER_SUPPORT
 	err = dt_global_init();
 	if (err != 0)
-		goto cleanup_cl_global;
+		goto cleanup_llog_info;
 
 	err = lu_ucred_global_init();
 	if (err != 0)
 		goto cleanup_dt_global;
-#endif /* HAVE_SERVER_SUPPORT */
 
-	err = llog_info_init();
-	if (err)
-#ifdef HAVE_SERVER_SUPPORT
+	err = lustre_tgt_register_fs();
+	if (err && err != -EBUSY) {
+		/* Don't fail if server code also registers "lustre_tgt" */
+		CERROR("obdclass: register fstype 'lustre_tgt' failed: rc = %d\n",
+		       err);
 		goto cleanup_lu_ucred_global;
-#else /* !HAVE_SERVER_SUPPORT */
-		goto cleanup_cl_global;
+	}
 #endif /* HAVE_SERVER_SUPPORT */
-
-	err = lustre_register_fs();
 
 	/* simulate a late OOM situation now to require all
-	 * alloc'ed/initialized resources to be freed */
+	 * alloc'ed/initialized resources to be freed
+	 */
 	if (OBD_FAIL_CHECK(OBD_FAIL_OBDCLASS_MODULE_LOAD)) {
-		/* fake error but filesystem has been registered */
-		lustre_unregister_fs();
 		/* force error to ensure module will be unloaded/cleaned */
 		err = -ENOMEM;
+		goto cleanup_all;
 	}
-
-	if (err)
-		goto cleanup_llog_info;
-
 	return 0;
 
-cleanup_llog_info:
-	llog_info_fini();
-
+cleanup_all:
 #ifdef HAVE_SERVER_SUPPORT
+	/* fake error but filesystem has been registered */
+	lustre_tgt_unregister_fs();
+
 cleanup_lu_ucred_global:
 	lu_ucred_global_fini();
 
 cleanup_dt_global:
 	dt_global_fini();
+
+cleanup_llog_info:
 #endif /* HAVE_SERVER_SUPPORT */
+	llog_info_fini();
 
 cleanup_cl_global:
 	cl_global_fini();
@@ -826,14 +828,13 @@ static void __exit obdclass_exit(void)
 #endif /* CONFIG_PROC_FS */
 	ENTRY;
 
-	lustre_unregister_fs();
-
 	misc_deregister(&obd_psdev);
-	llog_info_fini();
 #ifdef HAVE_SERVER_SUPPORT
+	lustre_tgt_unregister_fs();
 	lu_ucred_global_fini();
 	dt_global_fini();
 #endif /* HAVE_SERVER_SUPPORT */
+	llog_info_fini();
 	cl_global_fini();
 	lu_global_fini();
 

lustre/obdclass/obd_mount.c
@@ -51,12 +51,6 @@
 #include <uapi/linux/lustre/lustre_param.h>
 #include <libcfs/crypto/llcrypt.h>
 
-static DEFINE_SPINLOCK(client_lock);
-static struct module *client_mod;
-static int (*client_fill_super)(struct super_block *sb);
-
-static void (*kill_super_cb)(struct super_block *sb);
-
 /**************** config llog ********************/
 
 /**
@@ -501,6 +495,7 @@ out_free:
 		OBD_FREE(niduuid, len + 2);
 	RETURN(rc);
 }
+EXPORT_SYMBOL(lustre_start_mgc);
 
 static int lustre_stop_mgc(struct super_block *sb)
 {
@@ -584,7 +579,7 @@ out:
 
 /***************** lustre superblock **************/
 
-static struct lustre_sb_info *lustre_init_lsi(struct super_block *sb)
+struct lustre_sb_info *lustre_init_lsi(struct super_block *sb)
 {
 	struct lustre_sb_info *lsi;
 
@@ -610,6 +605,7 @@ static struct lustre_sb_info *lustre_init_lsi(struct super_block *sb)
 
 	RETURN(lsi);
 }
+EXPORT_SYMBOL(lustre_init_lsi);
 
 static int lustre_free_lsi(struct super_block *sb)
 {
@@ -1296,7 +1292,7 @@ static int lmd_parse_nidlist(char *buf, char **endh)
  * e.g. mount -v -t lustre -o abort_recov uml1:uml2:/lustre-client /mnt/lustre
  * dev is passed as device=uml1:/lustre by mount.lustre_tgt
  */
-static int lmd_parse(char *options, struct lustre_mount_data *lmd)
+int lmd_parse(char *options, struct lustre_mount_data *lmd)
 {
 	char *s1, *s2, *devname = NULL;
 	struct lustre_mount_data *raw = (struct lustre_mount_data *)options;
@@ -1585,15 +1581,17 @@ invalid:
 	CERROR("Bad mount options %s\n", options);
 	RETURN(-EINVAL);
 }
+EXPORT_SYMBOL(lmd_parse);
 
+#ifdef HAVE_SERVER_SUPPORT
 /**
  * This is the entry point for the mount call into Lustre.
- * This is called when a server or client is mounted,
+ * This is called when a server target is mounted,
  * and this is where we start setting things up.
  * @param data Mount options (e.g. -o flock,abort_recov)
  */
-static int lustre_fill_super(struct super_block *sb, void *lmd2_data,
-			     int silent)
+static int lustre_tgt_fill_super(struct super_block *sb, void *lmd2_data,
+				 int silent)
 {
 	struct lustre_mount_data *lmd;
 	struct lustre_sb_info *lsi;
@@ -1626,54 +1624,25 @@ static int lustre_fill_super(struct super_block *sb, void *lmd2_data,
 	}
 
 	if (lmd_is_client(lmd)) {
-		bool have_client = false;
-
-		CDEBUG(D_MOUNT, "Mounting client %s\n", lmd->lmd_profile);
-		if (!client_fill_super)
-			request_module("lustre");
-		spin_lock(&client_lock);
-		if (client_fill_super && try_module_get(client_mod))
-			have_client = true;
-		spin_unlock(&client_lock);
-		if (!have_client) {
-			LCONSOLE_ERROR_MSG(0x165,
-					   "Nothing registered for client mount! Is the 'lustre' module loaded?\n");
-			lustre_put_lsi(sb);
-			rc = -ENODEV;
-		} else {
-			rc = lustre_start_mgc(sb);
-			if (rc) {
-				lustre_common_put_super(sb);
-				GOTO(out, rc);
-			}
-			/* Connect and start */
-			/* (should always be ll_fill_super) */
-			rc = (*client_fill_super)(sb);
-			/* c_f_s will call lustre_common_put_super on failure,
-			 * which takes care of the module reference.
-			 */
-		}
-	} else {
-#ifdef HAVE_SERVER_SUPPORT
-		CDEBUG(D_MOUNT, "Mounting server from %s\n", lmd->lmd_dev);
-		rc = server_fill_super(sb);
-		/*
-		 * s_f_s calls lustre_start_mgc after the mount because we need
-		 * the MGS NIDs which are stored on disk.  Plus, we may
-		 * need to start the MGS first.
-		 */
-		/* s_f_s will call server_put_super on failure */
-#else
-		CERROR("client-side-only module, cannot handle server mount\n");
-		rc = -EINVAL;
-#endif
+		rc = -ENODEV;
+		CERROR("%s: attempting to mount a client with -t lustre_tgt' which is only for server-side mounts: rc = %d\n",
+		       lmd->lmd_dev, rc);
+		lustre_put_lsi(sb);
+		GOTO(out, rc);
 	}
 
+	CDEBUG(D_MOUNT, "Mounting server from %s\n", lmd->lmd_dev);
+	rc = server_fill_super(sb);
 	/*
+	 * server_fill_super calls lustre_start_mgc after the mount
+	 * because we need the MGS NIDs which are stored on disk.
+	 * Plus, we may need to start the MGS first.
+	 *
+	 * server_fill_super will call server_put_super on failure
+	 *
 	 * If error happens in fill_super() call, @lsi will be killed there.
 	 * This is why we do not put it here.
 	 */
-	GOTO(out, rc);
 out:
 	if (rc) {
 		CERROR("Unable to mount %s (%d)\n",
@@ -1686,41 +1655,14 @@ out:
 	return rc;
 }
 
-
-/*
- * We can't call ll_fill_super by name because it lives in a module that
- * must be loaded after this one.
- */
-void lustre_register_super_ops(struct module *mod,
-			       int (*cfs)(struct super_block *sb),
-			       void (*ksc)(struct super_block *sb))
-{
-	spin_lock(&client_lock);
-	client_mod = mod;
-	client_fill_super = cfs;
-	kill_super_cb = ksc;
-	spin_unlock(&client_lock);
-}
-EXPORT_SYMBOL(lustre_register_super_ops);
-
 /***************** FS registration ******************/
-static struct dentry *lustre_mount(struct file_system_type *fs_type, int flags,
-				   const char *devname, void *data)
+static struct dentry *lustre_tgt_mount(struct file_system_type *fs_type,
+				       int flags, const char *devname,
+				       void *data)
 {
-	return mount_nodev(fs_type, flags, data, lustre_fill_super);
+	return mount_nodev(fs_type, flags, data, lustre_tgt_fill_super);
 }
 
-static void lustre_kill_super(struct super_block *sb)
-{
-	struct lustre_sb_info *lsi = s2lsi(sb);
-
-	if (kill_super_cb && lsi && !IS_SERVER(lsi))
-		(*kill_super_cb)(sb);
-
-	kill_anon_super(sb);
-}
-
-#ifdef HAVE_SERVER_SUPPORT
 /* Register the "lustre_tgt" fs type.
  *
  * Right now this isn't any different than the normal "lustre" filesystem
@@ -1731,53 +1673,23 @@ static void lustre_kill_super(struct super_block *sb)
  *
  * The long-term goal is to disentangle the client and server mount code.
  */
-static struct file_system_type lustre_fs_type_tgt = {
+static struct file_system_type lustre_tgt_fstype = {
 	.owner		= THIS_MODULE,
 	.name		= "lustre_tgt",
-	.mount		= lustre_mount,
-	.kill_sb	= lustre_kill_super,
+	.mount		= lustre_tgt_mount,
+	.kill_sb	= kill_anon_super,
 	.fs_flags	= FS_REQUIRES_DEV | FS_RENAME_DOES_D_MOVE,
 };
 MODULE_ALIAS_FS("lustre_tgt");
 
-#define register_filesystem_tgt(fstype)					    \
-do {									    \
-	int _rc;							    \
-									    \
-	_rc = register_filesystem(fstype);				    \
-	if (_rc && _rc != -EBUSY) {					    \
-		/* Don't fail if server code also registers "lustre_tgt" */ \
-		CERROR("obdclass: register fstype '%s' failed: rc = %d\n",  \
-		       (fstype)->name, _rc);				    \
-		return _rc;						    \
-	}								    \
-} while (0)
-#define unregister_filesystem_tgt(fstype) unregister_filesystem(fstype)
-#else
-#define register_filesystem_tgt(fstype)   do {} while (0)
-#define unregister_filesystem_tgt(fstype) do {} while (0)
-#endif
-
-/* Register the "lustre" fs type */
-static struct file_system_type lustre_fs_type = {
-	.owner		= THIS_MODULE,
-	.name		= "lustre",
-	.mount		= lustre_mount,
-	.kill_sb	= lustre_kill_super,
-	.fs_flags	= FS_RENAME_DOES_D_MOVE,
-};
-MODULE_ALIAS_FS("lustre");
-
-int lustre_register_fs(void)
+int lustre_tgt_register_fs(void)
 {
-	register_filesystem_tgt(&lustre_fs_type_tgt);
-
-	return register_filesystem(&lustre_fs_type);
+	return register_filesystem(&lustre_tgt_fstype);
 }
 
-int lustre_unregister_fs(void)
+void lustre_tgt_unregister_fs(void)
 {
-	unregister_filesystem_tgt(&lustre_fs_type_tgt);
-
-	return unregister_filesystem(&lustre_fs_type);
+	unregister_filesystem(&lustre_tgt_fstype);
 }
+
+#endif /* HAVE_SERVER_SUPPORT */

lustre/obdclass/obd_mount_server.c
@@ -2048,6 +2048,7 @@ out_mnt:
 	server_put_super(sb);
 	return rc;
 }
+EXPORT_SYMBOL(server_fill_super);
 
 /*
  * Calculate timeout value for a target.

commit 96edf50039c5d629ed0cb428a9ea724d0ff652c0

LU-16367 misc: remove deprecated code
Remove code that is or will become deprecated in this release based
on the LUSTRE_VERSION_CODE checks.

Fixes: 53fa817657 ("LU-12514 llite: move client mounting from obdclass to llite")
Fixes: 3919a282ca ("LU-15106 ofd: quiet deprecated param warning")
Fixes: 115bba9ffb ("LU-11110 ofd: remove obdfilter.*.* symlinks in few releases")
Fixes: 73f15ad0f1 ("LU-9378 utils: split getstripe and find from lfs.1")
Fixes: 88d8f0f86b ("LU-11891 utils: getstripe use --mdt-index consistently")
Fixes: 78be823f33 ("LU-15218 quota: delete unused quota ID")
Fixes: 0c5fbd80f1 ("LU-5969 lustreapi: replace llapi_get_version()")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Id59371084a102d6d8257c45b55d68077f2ce7057
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49338
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/namei.c
@@ -1623,7 +1623,7 @@ again:
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
 			current_cap(), rdev, &request);
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 58, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 58, 0)
 	/*
 	 * server < 2.12.58 doesn't pack default LMV in intent_getattr reply,
 	 * fetch default LMV here.

lustre/llite/super25.c
@@ -149,7 +149,6 @@ static int lustre_fill_super(struct super_block *sb, void *lmd2_data,
 
 	if (!lmd_is_client(lmd)) {
 #ifdef HAVE_SERVER_SUPPORT
-#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 15, 51, 0)
 		static bool printed;
 
 		if (!printed) {
@@ -157,7 +156,6 @@ static int lustre_fill_super(struct super_block *sb, void *lmd2_data,
 				      lmd->lmd_profile);
 			printed = true;
 		}
-#endif
 		rc = server_fill_super(sb);
 #else
 		rc = -ENODEV;

lustre/ofd/lproc_ofd.c
@@ -893,121 +893,6 @@ static ssize_t checksum_t10pi_enforce_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(checksum_t10pi_enforce);
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 53, 0)
-static ssize_t read_cache_enable_show(struct kobject *kobj,
-				      struct attribute *attr,
-				      char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-
-	if (!ofd->ofd_read_cache_enable)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_show(&ofd->ofd_osd->dd_kobj,
-				ofd->ofd_read_cache_enable, buf);
-}
-
-static ssize_t read_cache_enable_store(struct kobject *kobj,
-				       struct attribute *attr,
-				       const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-	static bool rd_cache_warned;
-
-	if (!rd_cache_warned) {
-		rd_cache_warned = true;
-		pr_info("ofd: 'obdfilter.*.read_cache_enable' is deprecated, use 'osd-*.*.read_cache_enable' instead\n");
-	}
-
-	if (!ofd->ofd_read_cache_enable)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_store(&ofd->ofd_osd->dd_kobj,
-				 ofd->ofd_read_cache_enable, buffer, count);
-}
-LUSTRE_RW_ATTR(read_cache_enable);
-
-static ssize_t readcache_max_filesize_show(struct kobject *kobj,
-					   struct attribute *attr,
-					   char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-
-	if (!ofd->ofd_read_cache_max_filesize)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_show(&ofd->ofd_osd->dd_kobj,
-				ofd->ofd_read_cache_max_filesize, buf);
-}
-
-static ssize_t readcache_max_filesize_store(struct kobject *kobj,
-					    struct attribute *attr,
-					    const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-	static bool max_file_warned;
-
-	if (!max_file_warned) {
-		max_file_warned = true;
-		pr_info("ofd: 'obdfilter.*.readcache_max_filesize' is deprecated, use 'osd-*.*.readcache_max_filesize' instead\n");
-	}
-
-	if (!ofd->ofd_read_cache_max_filesize)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_store(&ofd->ofd_osd->dd_kobj,
-				 ofd->ofd_read_cache_max_filesize,
-				 buffer, count);
-}
-LUSTRE_RW_ATTR(readcache_max_filesize);
-
-static ssize_t writethrough_cache_enable_show(struct kobject *kobj,
-					      struct attribute *attr,
-					      char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-
-	if (!ofd->ofd_write_cache_enable)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_show(&ofd->ofd_osd->dd_kobj,
-				ofd->ofd_write_cache_enable, buf);
-}
-
-static ssize_t writethrough_cache_enable_store(struct kobject *kobj,
-					       struct attribute *attr,
-					       const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct ofd_device *ofd = ofd_dev(obd->obd_lu_dev);
-	static bool wr_cache_warned;
-
-	if (!wr_cache_warned) {
-		wr_cache_warned = true;
-		pr_info("ofd: 'obdfilter.*.writethrough_cache_enable' is deprecated, use 'osd-*.*.writethrough_cache_enable' instead\n");
-	}
-
-	if (!ofd->ofd_write_cache_enable)
-		return -EOPNOTSUPP;
-
-	return lustre_attr_store(&ofd->ofd_osd->dd_kobj,
-				 ofd->ofd_write_cache_enable,
-				 buffer, count);
-}
-LUSTRE_RW_ATTR(writethrough_cache_enable);
-#endif
-
 LPROC_SEQ_FOPS_RO_TYPE(ofd, recovery_status);
 LUSTRE_RW_ATTR(recovery_time_hard);
 LUSTRE_RW_ATTR(recovery_time_soft);
@@ -1122,11 +1007,6 @@ static struct attribute *ofd_attrs[] = {
 	&lustre_attr_access_log_size.attr,
 	&lustre_attr_job_cleanup_interval.attr,
 	&lustre_attr_checksum_t10pi_enforce.attr,
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 53, 0)
-	&lustre_attr_read_cache_enable.attr,
-	&lustre_attr_readcache_max_filesize.attr,
-	&lustre_attr_writethrough_cache_enable.attr,
-#endif
 	NULL,
 };
 

lustre/ofd/ofd_dev.c
@@ -710,55 +710,6 @@ static const struct lu_device_operations ofd_lu_ops = {
 	.ldo_prepare		= ofd_prepare,
 };
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)
-/**
- * Expose OSD statistics to OFD layer.
- *
- * The osd interfaces to the backend file system exposes useful data
- * such as brw_stats and read or write cache states. This same data
- * needs to be exposed into the obdfilter (ofd) layer to maintain
- * backwards compatibility. This function creates the symlinks in the
- * proc layer to enable this.
- *
- * \param[in] ofd	OFD device
- */
-static void ofd_procfs_add_brw_stats_symlink(struct ofd_device *ofd)
-{
-	struct obd_device *obd = ofd_obd(ofd);
-	struct obd_device *osd_obd = ofd->ofd_osd_exp->exp_obd;
-	const struct kobj_type *osd_type;
-	int i;
-
-	osd_type = get_ktype(&ofd->ofd_osd->dd_kobj);
-	for (i = 0; osd_type->default_attrs[i]; i++) {
-		if (strcmp(osd_type->default_attrs[i]->name,
-			   "read_cache_enable") == 0) {
-			ofd->ofd_read_cache_enable =
-				osd_type->default_attrs[i];
-		}
-
-		if (strcmp(osd_type->default_attrs[i]->name,
-			   "readcache_max_filesize") == 0) {
-			ofd->ofd_read_cache_max_filesize =
-				osd_type->default_attrs[i];
-		}
-
-		if (strcmp(osd_type->default_attrs[i]->name,
-			   "writethrough_cache_enable") == 0) {
-			ofd->ofd_write_cache_enable =
-				osd_type->default_attrs[i];
-		}
-	}
-
-	if (obd->obd_proc_entry == NULL)
-		return;
-
-	lprocfs_add_symlink("brw_stats", obd->obd_proc_entry,
-			    "../../%s/%s/brw_stats",
-			    osd_obd->obd_type->typ_name, obd->obd_name);
-}
-#endif
-
 /**
  * Cleanup all procfs entries in OFD.
  *
@@ -3060,10 +3011,6 @@ static int ofd_init0(const struct lu_env *env, struct ofd_device *m,
 		       obd->obd_name, rc);
 		RETURN(rc);
 	}
-
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)
-	ofd_procfs_add_brw_stats_symlink(m);
-#endif
 
 	snprintf(info->fti_u.name, sizeof(info->fti_u.name), "%s-%s",
 		 "filter"/*LUSTRE_OST_NAME*/, obd->obd_uuid.uuid);

lustre/utils/lfs.c
@@ -5545,11 +5545,6 @@ err_free:
 			param.fp_exclude_pool = !!neg_opt;
 			param.fp_check_pool = 1;
 			break;
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 53, 0)
-		case 'p': /* want this for --pool, to match getstripe/find */
-			fprintf(stderr,
-				"warning: -p deprecated, use --print0 or -0\n");
-#endif
 		case '0':
 			param.fp_zero_end = 1;
 			break;
@@ -6799,11 +6794,6 @@ static int lfs_setdirstripe(int argc, char **argv)
 			if (lsa.lsa_stripe_off == LLAPI_LAYOUT_DEFAULT)
 				lsa.lsa_stripe_off = mdts[0];
 			break;
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 15, 53, 0)
-		case 'm':
-			fprintf(stderr,
-				"warning: '-m' is deprecated, use '--mode' or '-o' instead\n");
-#endif
 		case 'o':
 			mode_opt = optarg;
 			break;
@@ -7808,10 +7798,10 @@ quota_type_def:
 			break;
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 0, 53, 0)
 		case 'd':
-#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 15, 53, 0)
-			fprintf(stderr, "'-d' deprecatd, use '-D' or '--default'\n");
-#endif
-			/* falltrrough */
+			fprintf(stderr,
+				"%s setquota: '-d' deprecated, use '-D' or '--default'\n",
+				progname);
+			fallthrough;
 #endif
 		case 'D':
 			use_default = true;

lustre/utils/liblustreapi_util.c
@@ -161,16 +161,15 @@ int llapi_get_version_string(char *version, unsigned int version_size)
  */
 int llapi_get_version(char *buffer, int buffer_size, char **version)
 {
-	int rc;
-#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 8, 53, 0)
 	static bool printed;
+	int rc;
+
 	if (!printed) {
 		fprintf(stderr,
 			"%s deprecated, use llapi_get_version_string()\n",
 			__func__);
 		printed = true;
 	}
-#endif
 
 	rc = llapi_get_version_string(buffer, buffer_size);
 	/* keep old return style for this legacy function */