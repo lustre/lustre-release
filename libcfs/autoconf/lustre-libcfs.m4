lustre/lustre-release

commit 0b406c91d175b6cdbdf472b70a37e618cd66a72d

LU-13642 lnet: modify lnet_inetdev to work with large NIDS
Change li_ipv6 field in struct lnet_inetdev to li_size which
now represents the size of the NID address. This will work
with the GUID of Inifiniband as well. Second change is
to store li_ipaddr always in network format. This will allow
direct comparsion between li_ipaddr and the nid_addr of
struct lnet_nid. We will ensure AF_IB will also be in the
same format as what will be stored in struct lnet_nid.
Implement setup with a NID address for the ko2iblnd LND driver.

Test-Parameters: trivial testlist=sanity-lnet
Change-Id: I7c27edb67263dd5bda4728c536aee266d38a4592
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49525
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/config.c
@@ -1563,9 +1563,9 @@ int lnet_inet_enumerate(struct lnet_inetdev **dev_list, struct net *ns, bool v6)
 
 			ifaces[nip].li_cpt = cpt;
 			ifaces[nip].li_iff_master = !!(flags & IFF_MASTER);
-			ifaces[nip].li_ipv6 = false;
+			ifaces[nip].li_size = sizeof(ifa->ifa_local);
 			ifaces[nip].li_index = dev->ifindex;
-			ifaces[nip].li_ipaddr = ntohl(ifa->ifa_local);
+			ifaces[nip].li_ipaddr = ifa->ifa_local;
 			ifaces[nip].li_netmask = ntohl(ifa->ifa_mask);
 			strlcpy(ifaces[nip].li_name, ifa->ifa_label,
 				sizeof(ifaces[nip].li_name));
@@ -1605,7 +1605,7 @@ int lnet_inet_enumerate(struct lnet_inetdev **dev_list, struct net *ns, bool v6)
 
 			ifaces[nip].li_cpt = cpt;
 			ifaces[nip].li_iff_master = !!(flags & IFF_MASTER);
-			ifaces[nip].li_ipv6 = true;
+			ifaces[nip].li_size = sizeof(struct in6_addr);
 			ifaces[nip].li_index = dev->ifindex;
 			memcpy(ifaces[nip].li_ipv6addr,
 			       &ifa6->addr, sizeof(struct in6_addr));
@@ -1655,16 +1655,14 @@ int lnet_inet_select(struct lnet_ni *ni,
 			/* IP unspecified, use IP of first matching interface */
 			break;
 
-		if (ifaces[if_idx].li_ipv6 &&
-		    nid_is_ipv6(&ni->ni_nid)) {
-			if (memcmp(ni->ni_nid.nid_addr,
-				   ifaces[if_idx].li_ipv6addr,
-				   sizeof(struct in6_addr)) == 0)
-				break;
-		} else if (!ifaces[if_idx].li_ipv6 &&
-			   nid_is_ipv4(&ni->ni_nid)) {
-			if (ni->ni_nid.nid_addr[0] ==
-			    htonl(ifaces[if_idx].li_ipaddr))
+		if (ifaces[if_idx].li_size == NID_ADDR_BYTES(&ni->ni_nid)) {
+			char *addr = (char *)&ifaces[if_idx].li_ipaddr;
+
+			if (ifaces[if_idx].li_size != 4)
+				addr = (char *)ifaces[if_idx].li_ipv6addr;
+
+			if (memcmp(ni->ni_nid.nid_addr, addr,
+				   ifaces[if_idx].li_size) == 0)
 				break;
 		}
 	}
@@ -1673,11 +1671,13 @@ int lnet_inet_select(struct lnet_ni *ni,
 		return if_idx;
 
 	if (ni->ni_interface)
-		CERROR("ksocklnd: failed to find interface %s%s%s\n",
+		CERROR("%s: failed to find interface %s%s%s\n",
+		       libcfs_lnd2modname(ni->ni_nid.nid_type),
 		       ni->ni_interface, addr_set ? "@" : "",
 		       addr_set ? libcfs_nidstr(&ni->ni_nid) : "");
 	else
-		CERROR("ksocklnd: failed to find IP address %s\n",
+		CERROR("%s: failed to find IP address %s\n",
+		       libcfs_lnd2modname(ni->ni_nid.nid_type),
 		       libcfs_nidstr(&ni->ni_nid));
 
 	return -EINVAL;
@@ -1719,7 +1719,7 @@ lnet_parse_ip2nets(const char **networksp, const char *ip2nets)
 	}
 
 	for (i = 0; i < nip; i++)
-		ipaddrs[i] = ifaces[i].li_ipaddr;
+		ipaddrs[i] = ntohl(ifaces[i].li_ipaddr);
 
 	rc = lnet_match_networks(networksp, ip2nets, ipaddrs, nip);
 	if (rc < 0) {

lnet/include/lnet/lib-lnet.h
@@ -908,7 +908,7 @@ struct lnet_inetdev {
 	};
 	u32	li_index;
 	bool	li_iff_master;
-	bool	li_ipv6;
+	u32	li_size;
 	char	li_name[IFNAMSIZ];
 };
 

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -115,16 +115,6 @@ static inline bool nid_is_nid4(const struct lnet_nid *nid)
 	return NID_ADDR_BYTES(nid) == 4;
 }
 
-static inline bool nid_is_ipv4(const struct lnet_nid *nid)
-{
-	return NID_ADDR_BYTES(nid) == 4;
-}
-
-static inline bool nid_is_ipv6(const struct lnet_nid *nid)
-{
-	return NID_ADDR_BYTES(nid) == 16;
-}
-
 /* check for address set */
 static inline bool nid_addr_is_set(const struct lnet_nid *nid)
 {

lnet/klnds/o2iblnd/o2iblnd.c
@@ -3577,8 +3577,7 @@ kiblnd_startup(struct lnet_ni *ni)
 
 	kiblnd_tunables_setup(ni);
 
-	/*
-	 * Multi-Rail wants each secondary
+	/* Multi-Rail wants each secondary
 	 * IP to be treated as an unique 'struct ni' interface.
 	 */
 	if (ni->ni_interface != NULL) {
@@ -3586,6 +3585,9 @@ kiblnd_startup(struct lnet_ni *ni)
 		ifname = ni->ni_interface;
 	} else {
 		ifname = *kiblnd_tunables.kib_default_ipif;
+		rc = libcfs_strnid(&ni->ni_nid, ifname);
+		if (rc < 0 || ni->ni_nid.nid_type != O2IBLND)
+			memset(&ni->ni_nid, 0, sizeof(ni->ni_nid));
 	}
 
 	if (strlen(ifname) >= sizeof(ibdev->ibd_ifname)) {
@@ -3598,12 +3600,13 @@ kiblnd_startup(struct lnet_ni *ni)
 	if (rc < 0)
 		goto failed;
 
-	for (i = 0; i < rc; i++) {
-		if (strcmp(ifname, ifaces[i].li_name) == 0)
-			break;
-	}
+	i = lnet_inet_select(ni, ifaces, rc);
+	if (i < 0)
+		goto failed;
 
-	if (i == rc) {
+	if (nid_addr_is_set(&ni->ni_nid)) {
+		strscpy(ifname, ifaces[i].li_name, sizeof(ifname));
+	} else if (strcmp(ifname, ifaces[i].li_name) != 0) {
 		CERROR("ko2iblnd: No matching interfaces\n");
 		rc = -ENOENT;
 		goto failed;
@@ -3619,7 +3622,7 @@ kiblnd_startup(struct lnet_ni *ni)
 			goto failed;
 		}
 
-		ibdev->ibd_ifip = ifaces[i].li_ipaddr;
+		ibdev->ibd_ifip = ntohl(ifaces[i].li_ipaddr);
 		strlcpy(ibdev->ibd_ifname, ifaces[i].li_name,
 			sizeof(ibdev->ibd_ifname));
 		ibdev->ibd_can_failover = ifaces[i].li_iff_master;

lnet/klnds/socklnd/socklnd.c
@@ -2570,7 +2570,7 @@ ksocknal_startup(struct lnet_ni *ni)
 
 	ni->ni_dev_cpt = ifaces[if_idx].li_cpt;
 	ksi->ksni_index = ifaces[if_idx].li_index;
-	if (ifaces[if_idx].li_ipv6) {
+	if (ifaces[if_idx].li_size == sizeof(struct in6_addr)) {
 		struct sockaddr_in6 *sa;
 		sa = (void *)&ksi->ksni_addr;
 		memset(sa, 0, sizeof(*sa));
@@ -2585,9 +2585,9 @@ ksocknal_startup(struct lnet_ni *ni)
 		sa = (void *)&ksi->ksni_addr;
 		memset(sa, 0, sizeof(*sa));
 		sa->sin_family = AF_INET;
-		sa->sin_addr.s_addr = htonl(ifaces[if_idx].li_ipaddr);
+		sa->sin_addr.s_addr = ifaces[if_idx].li_ipaddr;
 		ksi->ksni_netmask = ifaces[if_idx].li_netmask;
-		ni->ni_nid.nid_size = 4 - 4;
+		ni->ni_nid.nid_size = 0;
 		ni->ni_nid.nid_addr[0] = sa->sin_addr.s_addr;
 	}
 	strlcpy(ksi->ksni_name, ifaces[if_idx].li_name, sizeof(ksi->ksni_name));

commit 7fe7f4ca06b9c8d128f7ba36988e36f8141ed53d

LU-16520 build: Move strscpy to libcfs common header
Ensure strscpy is available to lustre

Test-Parameters: trivial
Fixes: 0b406c91d17 ("LU-13642 lnet: modify lnet_inetdev to work with large NIDS")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0c3673c2aa7e6b61671521a8cabde8a364f7f6f8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49863
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/autoconf/lustre-libcfs.m4
@@ -598,6 +598,29 @@ AC_DEFUN([LIBCFS_KERNEL_PARAM_LOCK],[
 ]) # LIBCFS_KERNEL_PARAM_LOCK
 
 #
+# LIBCFS_STRSCPY_EXISTS
+#
+# Linux commit v4.2-rc1-2-g30035e45753b
+#  string: provide strscpy()
+#
+# If strscpy exists, prefer it over strlcpy
+#
+AC_DEFUN([LIBCFS_SRC_STRSCPY_EXISTS], [
+	LB2_LINUX_TEST_SRC([strscpy_exists], [
+		#include <linux/string.h>
+	],[
+		strscpy((char *)NULL, (const char *)NULL, 0);
+	],[-Werror])
+])
+AC_DEFUN([LIBCFS_STRSCPY_EXISTS], [
+	AC_MSG_CHECKING([if kernel strscpy is available])
+	LB2_LINUX_TEST_RESULT([strscpy_exists], [
+		AC_DEFINE(HAVE_STRSCPY, 1,
+			[kernel strscpy is available])
+	])
+]) # LIBCFS_STRSCPY_EXISTS
+
+#
 # Kernel version 4.2 changed topology_thread_cpumask
 # to topology_sibling_cpumask
 #
@@ -2281,9 +2304,10 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_WAIT_WOKEN
 	# 4.0
 	LIBCFS_SRC_KTIME_MS_DELTA
-	# 4.2
+	# 4.1
 	LIBCFS_SRC_KERNEL_PARAM_LOCK
 	# 4.2
+	LIBCFS_SRC_STRSCPY_EXISTS
 	LIBCFS_SRC_HAVE_TOPOLOGY_SIBLING_CPUMASK
 	# 4.4
 	LIBCFS_SRC_KSTRTOBOOL_FROM_USER
@@ -2425,6 +2449,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	# 4.1
 	LIBCFS_KERNEL_PARAM_LOCK
 	# 4.2
+	LIBCFS_STRSCPY_EXISTS
 	LIBCFS_HAVE_TOPOLOGY_SIBLING_CPUMASK
 	# 4.4
 	LIBCFS_KSTRTOBOOL_FROM_USER

libcfs/include/libcfs/linux/linux-misc.h
@@ -177,6 +177,10 @@ static inline void *cfs_kallsyms_lookup_name(const char *name)
 }
 #endif
 
+#ifndef HAVE_STRSCPY
+#define strscpy(s1, s2, sz)    strlcpy((s1), (s2), (sz))
+#endif
+
 #ifndef HAVE_KOBJ_TYPE_DEFAULT_GROUPS
 #define default_groups			default_attrs
 #define KOBJ_ATTR_GROUPS(_name)		_name##_attrs

lnet/autoconf/lustre-lnet.m4
@@ -724,25 +724,6 @@ AC_SUBST(KFILND)
 AC_SUBST(EXTRA_SYMBOLS)
 ]) # LN_CONFIG_KFILND
 
-# LN_CONFIG_STRSCPY_EXISTS
-#
-# If strscpy exists, prefer it over strlcpy
-#
-AC_DEFUN([LN_CONFIG_STRSCPY_EXISTS], [
-tmp_flags="$EXTRA_KCFLAGS"
-EXTRA_KCFLAGS="-Werror"
-LB_CHECK_COMPILE([kernel strscpy is available],
-strscpy_exists, [
-	#include <linux/string.h>
-],[
-	strscpy((char *)NULL, (const char *)NULL, 0);
-],[
-	AC_DEFINE(HAVE_STRSCPY, 1,
-		[kernel strscpy is available])
-])
-EXTRA_KCFLAGS="$tmp_flags"
-]) # LN_CONFIG_STRSCPY_EXISTS
-
 # LN_CONFIG_SOCK_CREATE_KERN
 #
 # 4.x sock_create_kern() added a first parameter as 'struct net *'
@@ -969,7 +950,6 @@ AC_MSG_NOTICE([LNet kernel checks
 LN_CONFIG_BACKOFF
 LN_CONFIG_O2IB
 LN_CONFIG_GNILND
-LN_CONFIG_STRSCPY_EXISTS
 LN_CONFIG_KFILND
 # 3.10
 LN_EXPORT_KMAP_TO_PAGE

lnet/lnet/config.c
@@ -52,10 +52,6 @@ static int lnet_tbnob = 0;			/* track text buf allocation */
 #define SPACESTR " \t\v\r\n"
 #define DELIMITERS ":()[]"
 
-#ifndef HAVE_STRSCPY
-#define strscpy(s1, s2, sz)	strlcpy((s1), (s2), (sz))
-#endif
-
 static void
 lnet_syntax(const char *name, const char *str, int offset, int width)
 {