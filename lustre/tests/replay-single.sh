lustre/lustre-release

commit 226fd401f9d8bfcd1a71bf264d9baef1e0842441

LU-7660 dne: support fs default stripe
On DNE system slave MDTs should honor fs default stripe, to achieve
this, slave MDT should lock fs root, and cache XATTR_NAME_LOV, below
functionalities are added for this:
* take and cache remote root XATTR lock on slave MDTs.
* add dt_invalidate operation to invalidate OSP attributes cache,
  normally it's called in mdt_object_unlock(), but if we cache this
  lock, it's called upon cross-MDT lock cancel.
* enable OSP attribute cache by default.
* migrate should invalidate remote target explictely if it's newly
  created, because it doesn't take its lock beforehand.
* OSP should cleanup ldlm namespace in device cleanup because there
  may be cached locks.
* add sanity.sh 405 test for this.

Signed-off-by: Lai Siyao <lai.siyao@intel.com>
Change-Id: I90e5f35f3b6a294a2a559c28df267f92ce188b9c
Reviewed-on: http://review.whamcloud.com/19041
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/osp/osp_dev.c
@@ -455,10 +455,11 @@ static void osp_last_used_fini(const struct lu_env *env, struct osp_device *osp)
  */
 static int osp_disconnect(struct osp_device *d)
 {
+	struct obd_device *obd = d->opd_obd;
 	struct obd_import *imp;
 	int rc = 0;
 
-	imp = d->opd_obd->u.cli.cl_import;
+	imp = obd->u.cli.cl_import;
 
 	/* Mark import deactivated now, so we don't try to reconnect if any
 	 * of the cleanup RPCs fails (e.g. ldlm cancel, etc).  We don't
@@ -477,8 +478,7 @@ static int osp_disconnect(struct osp_device *d)
 
 	rc = ptlrpc_disconnect_import(imp, 0);
 	if (rc != 0)
-		CERROR("%s: can't disconnect: rc = %d\n",
-		       d->opd_obd->obd_name, rc);
+		CERROR("%s: can't disconnect: rc = %d\n", obd->obd_name, rc);
 
 	ptlrpc_invalidate_import(imp);
 
@@ -636,6 +636,8 @@ static int osp_process_config(const struct lu_env *env,
 	case LCFG_PRE_CLEANUP:
 		rc = osp_disconnect(d);
 		osp_update_fini(env, d);
+		if (obd->obd_namespace != NULL)
+			ldlm_namespace_free_prior(obd->obd_namespace, NULL, 1);
 		break;
 	case LCFG_CLEANUP:
 		lu_dev_del_linkage(dev->ld_site, dev);

lustre/include/dt_object.h
@@ -1015,6 +1015,20 @@ struct dt_object_operations {
 				struct dt_object *dt,
 				struct ldlm_enqueue_info *einfo,
 				union ldlm_policy_data *policy);
+
+	/**
+	 * Invalidate attribute cache.
+	 *
+	 * This method invalidate attribute cache of the object, which is on OSP
+	 * only.
+	 *
+	 * \param[in] env	execution envionment for this thread
+	 * \param[in] dt	object
+	 *
+	 * \retval 0		on success
+	 * \retval negative	negated errno on error
+	 */
+	int   (*do_invalidate)(const struct lu_env *env, struct dt_object *dt);
 };
 
 /**
@@ -2610,6 +2624,15 @@ static inline int dt_xattr_list(const struct lu_env *env, struct dt_object *dt,
 		return cfs_fail_err;
 
 	return dt->do_ops->do_xattr_list(env, dt, buf);
+}
+
+static inline int dt_invalidate(const struct lu_env *env, struct dt_object *dt)
+{
+	LASSERT(dt);
+	LASSERT(dt->do_ops);
+	LASSERT(dt->do_ops->do_invalidate);
+
+	return dt->do_ops->do_invalidate(env, dt);
 }
 
 static inline int dt_declare_delete(const struct lu_env *env,

lustre/include/md_object.h
@@ -214,6 +214,8 @@ struct md_object_operations {
 				 struct md_object *obj,
 				 struct ldlm_enqueue_info *einfo,
 				 union ldlm_policy_data *policy);
+
+	int (*moo_invalidate)(const struct lu_env *env, struct md_object *obj);
 };
 
 /**
@@ -400,6 +402,12 @@ static inline int mo_xattr_list(const struct lu_env *env,
 {
         LASSERT(m->mo_ops->moo_xattr_list);
         return m->mo_ops->moo_xattr_list(env, m, buf);
+}
+
+static inline int mo_invalidate(const struct lu_env *env, struct md_object *m)
+{
+	LASSERT(m->mo_ops->moo_invalidate);
+	return m->mo_ops->moo_invalidate(env, m);
 }
 
 static inline int mo_swap_layouts(const struct lu_env *env,

lustre/lod/lod_dev.c
@@ -975,6 +975,11 @@ static int lod_process_config(const struct lu_env *env,
 		GOTO(out, rc);
 	}
 	case LCFG_PRE_CLEANUP: {
+		if (lod->lod_md_root != NULL) {
+			lu_object_put(env, &lod->lod_md_root->ldo_obj.do_lu);
+			lod->lod_md_root = NULL;
+		}
+
 		lod_sub_process_config(env, lod, &lod->lod_mdt_descs, lcfg);
 		lod_sub_process_config(env, lod, &lod->lod_ost_descs, lcfg);
 		next = &lod->lod_child->dd_lu_dev;
@@ -1562,7 +1567,7 @@ static int lod_init0(const struct lu_env *env, struct lod_device *lod,
 	if (rc)
 		GOTO(out_pools, rc);
 
-	spin_lock_init(&lod->lod_desc_lock);
+	spin_lock_init(&lod->lod_lock);
 	spin_lock_init(&lod->lod_connects_lock);
 	lod_tgt_desc_init(&lod->lod_mdt_descs);
 	lod_tgt_desc_init(&lod->lod_ost_descs);

lustre/lod/lod_internal.h
@@ -180,8 +180,8 @@ struct lod_device {
 	/* lov settings descriptor storing static information */
 	struct lov_desc	      lod_desc;
 
-	/* use to protect ld_active_tgt_count and all ltd_active */
-	spinlock_t	     lod_desc_lock;
+	/* protect ld_active_tgt_count, ltd_active and lod_md_root */
+	spinlock_t	     lod_lock;
 
 	/* Description of OST */
 	struct lod_tgt_descs  lod_ost_descs;
@@ -210,6 +210,9 @@ struct lod_device {
 	enum lustre_sec_part   lod_sp_me;
 
 	struct proc_dir_entry *lod_symlink;
+
+	/* ROOT object, used to fetch FS default striping */
+	struct lod_object      *lod_md_root;
 };
 
 #define lod_osts	lod_ost_descs.ltd_tgts
@@ -226,58 +229,77 @@ struct lod_device {
 #define ltd_mdt			ltd_tgt
 #define lod_mdt_desc		lod_tgt_desc
 
-struct lod_dir_stripe_info {
-	__u32	ldsi_stripe_offset;
-	__u32	ldsi_def_stripenr;
-	__u32	ldsi_def_stripe_offset;
-	__u32	ldsi_def_hash_type;
-	__u32	ldsi_hash_type;
-
-	unsigned int ldsi_def_striping_set:1,
-		     ldsi_def_striping_cached:1,
-		     ldsi_striped:1;
+struct lod_default_striping {
+	/* default LOV */
+	__u32		lds_def_stripe_size;
+	__u16		lds_def_stripenr;
+	__u16		lds_def_stripe_offset;
+	char		lds_def_pool[LOV_MAXPOOLNAME + 1];
+	/* default LMV */
+	__u32		lds_dir_def_stripenr;
+	__u32		lds_dir_def_stripe_offset;
+	__u32		lds_dir_def_hash_type;
+	/* flags whether default striping is set */
+	__u32		lds_def_striping_set:1,
+			lds_dir_def_striping_set:1;
 };
 
-/*
- * XXX: shrink this structure, currently it's 72bytes on 32bit arch,
- *      so, slab will be allocating 128bytes
- */
 struct lod_object {
-	struct dt_object   ldo_obj;
-
-	/* if object is striped, then the next fields describe stripes */
-	/* For striped directory, ldo_stripenr == slave stripe count */
-	__u16		   ldo_stripenr;
-	__u16		   ldo_layout_gen;
-	__u32		   ldo_stripe_size;
-	__u32		   ldo_pattern;
-	__u16		   ldo_released_stripenr;
-	char		  *ldo_pool;
-	struct dt_object **ldo_stripe;
-	/* to know how much memory to free, ldo_stripenr can be less */
-	/* default striping for directory represented by this object
-	 * is cached in stripenr/stripe_size */
-	unsigned int	   ldo_stripes_allocated:16,
-			   ldo_striping_cached:1,
-			   ldo_def_striping_set:1,
-			   ldo_def_striping_cached:1,
-	/* ldo_dir_slave_stripe indicate this is a slave stripe of
-	 * a striped dir */
-			   ldo_dir_slave_stripe:1;
-	__u32		   ldo_def_stripe_size;
-	__u16		   ldo_def_stripenr;
-	__u16		   ldo_def_stripe_offset;
-	struct lod_dir_stripe_info	*ldo_dir_stripe;
+	struct dt_object			     ldo_obj;
+	union {
+		/* file stripe */
+		struct {
+			/*
+			 * don't change field order, because both file and
+			 * directory use ldo_stripenr/ldo_stripes_allocated
+			 * to access stripe number.
+			 */
+			__u16			     ldo_stripenr;
+			__u16			     ldo_stripes_allocated;
+			__u16			     ldo_layout_gen;
+			__u16			     ldo_released_stripenr;
+			__u32			     ldo_pattern;
+			__u32			     ldo_stripe_size;
+			__u16			     ldo_stripe_offset;
+			char			    *ldo_pool;
+		};
+		/* directory stripe */
+		struct {
+			__u16			     ldo_dir_stripenr;
+			__u16			     ldo_dir_stripes_allocated;
+			__u32			     ldo_dir_stripe_offset;
+			__u32			     ldo_dir_hash_type;
+			__u32			     ldo_dir_slave_stripe:1,
+						     ldo_dir_striped:1;
+			/*
+			 * default striping is not cached, so this field is
+			 * invalid after create, make sure it's used by
+			 * lod_dir_striping_create_internal() only.
+			 */
+			struct lod_default_striping *ldo_def_striping;
+		};
+	};
+	struct dt_object			   **ldo_stripe;
 };
 
-#define ldo_dir_stripe_offset	ldo_dir_stripe->ldsi_stripe_offset
-#define ldo_dir_def_stripenr	ldo_dir_stripe->ldsi_def_stripenr
-#define ldo_dir_hash_type	ldo_dir_stripe->ldsi_hash_type
-#define ldo_dir_def_hash_type	ldo_dir_stripe->ldsi_def_hash_type
-#define ldo_dir_striped		ldo_dir_stripe->ldsi_striped
-#define ldo_dir_def_striping_set	ldo_dir_stripe->ldsi_def_striping_set
-#define ldo_dir_def_striping_cached	ldo_dir_stripe->ldsi_def_striping_cached
-#define ldo_dir_def_stripe_offset	ldo_dir_stripe->ldsi_def_stripe_offset
+static inline int lod_object_set_pool(struct lod_object *lo, const char *pool)
+{
+	int len;
+
+	if (lo->ldo_pool != NULL) {
+		len = strlen(lo->ldo_pool) + 1;
+		OBD_FREE(lo->ldo_pool, len);
+		lo->ldo_pool = NULL;
+	}
+	if (pool != NULL) {
+		len = strlen(pool) + 1;
+		OBD_ALLOC(lo->ldo_pool, len);
+		if (lo->ldo_pool == NULL)
+			return -ENOMEM;
+		strlcpy(lo->ldo_pool, pool, len);
+	}
+	return 0;
+}
 
 struct lod_it {
 	struct dt_object	*lit_obj; /* object from the layer below */
@@ -289,25 +311,28 @@ struct lod_it {
 
 struct lod_thread_info {
 	/* per-thread buffer for LOV EA, may be vmalloc'd */
-	void             *lti_ea_store;
-	__u32             lti_ea_store_size;
+	void			       *lti_ea_store;
+	__u32				lti_ea_store_size;
 	/* per-thread buffer for LMV EA */
-	struct lu_buf     lti_buf;
-	struct ost_id     lti_ostid;
-	struct lu_fid     lti_fid;
-	struct obd_statfs lti_osfs;
-	struct lu_attr    lti_attr;
-	struct lod_it	  lti_it;
-	struct ldlm_res_id lti_res_id;
+	struct lu_buf			lti_buf;
+	struct ost_id			lti_ostid;
+	struct lu_fid			lti_fid;
+	struct obd_statfs		lti_osfs;
+	struct lu_attr			lti_attr;
+	struct lod_it			lti_it;
+	struct ldlm_res_id		lti_res_id;
 	/* used to hold lu_dirent, sizeof(struct lu_dirent) + NAME_MAX */
-	char		  lti_key[sizeof(struct lu_dirent) + NAME_MAX];
-	struct dt_object_format lti_format;
-	struct lu_name	  lti_name;
-	struct lu_buf	  lti_linkea_buf;
-	struct dt_insert_rec lti_dt_rec;
-	struct llog_catid lti_cid;
-	struct llog_cookie lti_cookie;
-	struct lustre_cfg lti_lustre_cfg;
+	char				lti_key[sizeof(struct lu_dirent) +
+						NAME_MAX];
+	struct dt_object_format		lti_format;
+	struct lu_name			lti_name;
+	struct lu_buf			lti_linkea_buf;
+	struct dt_insert_rec		lti_dt_rec;
+	struct llog_catid		lti_cid;
+	struct llog_cookie		lti_cookie;
+	struct lustre_cfg		lti_lustre_cfg;
+	/* used to store parent default striping in create */
+	struct lod_default_striping	lti_def_striping;
 };
 
 extern const struct lu_device_operations lod_lu_ops;
@@ -489,7 +514,6 @@ extern struct dt_object_operations lod_obj_ops;
 extern struct lu_object_operations lod_lu_obj_ops;
 int lod_load_lmv_shards(const struct lu_env *env, struct lod_object *lo,
 			struct lu_buf *buf, bool resize);
-int lod_object_set_pool(struct lod_object *o, char *pool);
 int lod_declare_striped_object(const struct lu_env *env, struct dt_object *dt,
 			       struct lu_attr *attr,
 			       const struct lu_buf *lovea, struct thandle *th);

lustre/lod/lod_lov.c
@@ -960,7 +960,7 @@ int lod_parse_striping(const struct lu_env *env, struct lod_object *lo,
 	if (magic == LOV_MAGIC_V3) {
 		struct lov_mds_md_v3 *v3 = (struct lov_mds_md_v3 *) lmm;
 		objs = &v3->lmm_objects[0];
-		lod_object_set_pool(lo, v3->lmm_pool_name);
+		/* no need to set pool, which is used in create only */
 	} else {
 		objs = &lmm->lmm_objects[0];
 	}
@@ -1041,9 +1041,6 @@ int lod_load_striping_locked(const struct lu_env *env, struct lod_object *lo)
 		 */
 		rc = lod_parse_dir_striping(env, lo, buf);
 	}
-
-	if (rc == 0)
-		lo->ldo_striping_cached = 1;
 out:
 	RETURN(rc);
 }

lustre/lod/lod_qos.c
@@ -209,7 +209,7 @@ static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
 	/* check whether device has changed state (active, inactive) */
 	if (rc != 0 && ost->ltd_active) {
 		/* turned inactive? */
-		spin_lock(&d->lod_desc_lock);
+		spin_lock(&d->lod_lock);
 		if (ost->ltd_active) {
 			ost->ltd_active = 0;
 			if (rc == -ENOTCONN)
@@ -222,13 +222,13 @@ static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
 			CDEBUG(D_CONFIG, "%s: turns inactive\n",
 			       ost->ltd_exp->exp_obd->obd_name);
 		}
-		spin_unlock(&d->lod_desc_lock);
+		spin_unlock(&d->lod_lock);
 	} else if (rc == 0 && ost->ltd_active == 0) {
 		/* turned active? */
 		LASSERTF(d->lod_desc.ld_active_tgt_count < d->lod_ostnr,
 			 "active tgt count %d, ost nr %d\n",
 			 d->lod_desc.ld_active_tgt_count, d->lod_ostnr);
-		spin_lock(&d->lod_desc_lock);
+		spin_lock(&d->lod_lock);
 		if (ost->ltd_active == 0) {
 			ost->ltd_active = 1;
 			ost->ltd_connecting = 0;
@@ -238,7 +238,7 @@ static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
 			CDEBUG(D_CONFIG, "%s: turns active\n",
 			       ost->ltd_exp->exp_obd->obd_name);
 		}
-		spin_unlock(&d->lod_desc_lock);
+		spin_unlock(&d->lod_lock);
 	}
 
 	RETURN(rc);
@@ -1105,7 +1105,7 @@ static int lod_alloc_ost_list(const struct lu_env *env,
 
 	v3 = (struct lov_user_md_v3 *)lum;
 	for (i = 0; i < lo->ldo_stripenr; i++) {
-		if (v3->lmm_objects[i].l_ost_idx == lo->ldo_def_stripe_offset) {
+		if (v3->lmm_objects[i].l_ost_idx == lo->ldo_stripe_offset) {
 			array_idx = i;
 			break;
 		}
@@ -1113,7 +1113,7 @@ static int lod_alloc_ost_list(const struct lu_env *env,
 	if (i == lo->ldo_stripenr) {
 		CDEBUG(D_OTHER,
 		       "%s: start index %d not in the specified list of OSTs\n",
-		       lod2obd(m)->obd_name, lo->ldo_def_stripe_offset);
+		       lod2obd(m)->obd_name, lo->ldo_stripe_offset);
 		RETURN(-EINVAL);
 	}
 
@@ -1161,7 +1161,7 @@ static int lod_alloc_ost_list(const struct lu_env *env,
 /**
  * Allocate a striping on a predefined set of OSTs.
  *
- * Allocates new layout starting from OST index in lo->ldo_def_stripe_offset.
+ * Allocates new layout starting from OST index in lo->ldo_stripe_offset.
  * Full OSTs are not considered. The exact order of OSTs is not important and
  * varies depending on OST status. The allocation procedure prefers the targets
  * with precreated objects ready. The number of stripes needed and stripe
@@ -1217,15 +1217,14 @@ repeat_find:
 	/* search loi_ost_idx in ost array */
 	array_idx = 0;
 	for (i = 0; i < ost_count; i++) {
-		if (osts->op_array[i] == lo->ldo_def_stripe_offset) {
+		if (osts->op_array[i] == lo->ldo_stripe_offset) {
 			array_idx = i;
 			break;
 		}
 	}
 	if (i == ost_count) {
 		CERROR("Start index %d not found in pool '%s'\n",
-		       lo->ldo_def_stripe_offset,
-		       lo->ldo_pool ? lo->ldo_pool : "");
+		       lo->ldo_stripe_offset, lo->ldo_pool ?: "");
 		GOTO(out, rc = -EINVAL);
 	}
 
@@ -1786,7 +1785,7 @@ static int lod_qos_parse_config(const struct lu_env *env,
 	if (v1->lmm_stripe_count > 0)
 		lo->ldo_stripenr = v1->lmm_stripe_count;
 
-	lo->ldo_def_stripe_offset = v1->lmm_stripe_offset;
+	lo->ldo_stripe_offset = v1->lmm_stripe_offset;
 
 	lod_object_set_pool(lo, NULL);
 	if (pool_name != NULL) {
@@ -1797,14 +1796,14 @@ static int lod_qos_parse_config(const struct lu_env *env,
 		/* coverity[overrun-buffer-val] */
 		pool = lod_find_pool(d, pool_name);
 		if (pool != NULL) {
-			if (lo->ldo_def_stripe_offset != LOV_OFFSET_DEFAULT) {
+			if (lo->ldo_stripe_offset != LOV_OFFSET_DEFAULT) {
 				rc = lod_check_index_in_pool(
-					       lo->ldo_def_stripe_offset, pool);
+						lo->ldo_stripe_offset, pool);
 				if (rc < 0) {
 					lod_pool_putref(pool);
 					CERROR("%s: invalid offset, %u\n",
 					       lod2obd(d)->obd_name,
-					       lo->ldo_def_stripe_offset);
+					       lo->ldo_stripe_offset);
 					RETURN(-EINVAL);
 				}
 			}
@@ -1911,7 +1910,7 @@ int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 
 		if (lum != NULL && lum->lmm_magic == LOV_USER_MAGIC_SPECIFIC) {
 			rc = lod_alloc_ost_list(env, lo, stripe, lum, th);
-		} else if (lo->ldo_def_stripe_offset == LOV_OFFSET_DEFAULT) {
+		} else if (lo->ldo_stripe_offset == LOV_OFFSET_DEFAULT) {
 			rc = lod_alloc_qos(env, lo, stripe, flag, th);
 			if (rc == -EAGAIN)
 				rc = lod_alloc_rr(env, lo, stripe, flag, th);

commit eed4d4c7523c26cfc5bc230986d96b2acf152dee

LU-16536 osp: don't cleanup ldlm in precleanup phase
instead do this in cleanup phase so that all OSPs have chance
to abort in-flight RPCs which can block MDT thread holding
LDLM locks.

Fixes: 226fd401f9 ("LU-7660 dne: support fs default stripe")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Ib3714b29c514a7fa938d47717dc36525654407d6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49925
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd_support.h
@@ -691,6 +691,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_OUT_OBJECT_MISS	0x1708
 #define OBD_FAIL_OUT_EIO		0x1709
 #define OBD_FAIL_BUT_UPDATE_NET_REP	0x170a
+#define OBD_FAIL_OUT_DROP_DESTROY	0x170b
 
 /* MIGRATE */
 #define OBD_FAIL_MIGRATE_ENTRIES		0x1801

lustre/osp/osp_dev.c
@@ -662,10 +662,15 @@ static int osp_process_config(const struct lu_env *env,
 	case LCFG_PRE_CLEANUP:
 		rc = osp_disconnect(d);
 		osp_update_fini(env, d);
-		if (obd->obd_namespace != NULL)
-			ldlm_namespace_free_prior(obd->obd_namespace, NULL, 1);
 		break;
 	case LCFG_CLEANUP:
+		/*
+		 * cleanup ldlm so that PRE_CLEANUP phase doesn't block
+		 * awaiting for locks held by MDT threads awaiting for
+		 * all OSPs to interrupt their in-flight RPCs
+		 */
+		if (obd->obd_namespace != NULL)
+			ldlm_namespace_free_prior(obd->obd_namespace, NULL, 1);
 		lu_dev_del_linkage(dev->ld_site, dev);
 		rc = osp_shutdown(env, d);
 		break;

lustre/target/out_handler.c
@@ -589,6 +589,9 @@ static int out_destroy(struct tgt_session_info *tsi)
 			    tti->tti_u.update.tti_update_reply,
 			    tti->tti_u.update.tti_update_reply_index);
 
+	if (OBD_FAIL_CHECK(OBD_FAIL_OUT_DROP_DESTROY))
+		tsi->tsi_pill->rc_req->rq_no_reply = 1;
+
 	RETURN(rc);
 }
 

lustre/tests/replay-single.sh
@@ -4997,6 +4997,29 @@ test_135() {
 }
 run_test 135 "Server failure in lock replay phase"
 
+test_136() {
+	(( $MDSCOUNT >= 3 )) || skip "needs > 2 MDTs"
+	(( MDS1_VERSION >= $(version_code 2.15.53) )) ||
+		skip "need MDS version >= 2.15.53 for LU-16536 fix"
+
+	$LFS mkdir -i0 -c3 $DIR/$tdir || error "can't mkdir"
+	$LFS getdirstripe $DIR/$tdir
+	sync;sync;sync
+
+#define OBD_FAIL_OUT_DROP_DESTROY      0x170b
+	local mdts=$(comma_list $(mdts_nodes))
+	do_nodes $mdts $LCTL set_param fail_loc=0x170b
+	rmdir $DIR/$tdir &
+	sleep 0.5
+	stop mds2
+	stop mds3
+	stop mds1
+	start mds1 $(mdsdevname 1) $MDS_MOUNT_OPTS || error "MDT1 start failed"
+	start mds2 $(mdsdevname 2) $MDS_MOUNT_OPTS || error "MDT2 start failed"
+	start mds3 $(mdsdevname 3) $MDS_MOUNT_OPTS || error "MDT3 star"
+}
+run_test 136 "MDS to disconnect all OSPs first, then cleanup ldlm"
+
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status