lustre/lustre-release

commit c10c6eeb37dd553166367b96369dca25183ace3b

LU-15728 llite: fix relatime support
relatime behavior is properly managed by VFS, however
Lustre also stores acmtime on OST objects and atime
updates for OST objects should honor relatime behavior.

This patch updates 'ci_noatime' feature which was introduced to
properly honor noatime option for OST objects, to also support
'relatime'.
file_is_noatime() code already comes from upstream touch_atime().
Add missing parts from touch_atime() to also support relatime.

It also forces atime to disk on MDD if ondisk atime is older than
ondisk mtime/ctime to match relatime (even if relatime is not enabled)

Add a new test for relatime feature.

Signed-off-by: Aurelien Degremont <degremoa@amazon.com>
Change-Id: I7a26f39841300a60c015944f9e544115b4446ead
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47017
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/mdd/mdd_object.c
@@ -710,6 +710,7 @@ static int mdd_fix_attr(const struct lu_env *env, struct mdd_object *obj,
 		 * close RPCs.
 		 */
 		if (la->la_valid & LA_ATIME &&
+		    oattr->la_atime > max(oattr->la_ctime, oattr->la_mtime) &&
 		    la->la_atime <= (oattr->la_atime +
 				mdd_obj2mdd_dev(obj)->mdd_atime_diff))
 			la->la_valid &= ~LA_ATIME;

lustre/tests/sanity.sh
@@ -5223,6 +5223,62 @@ test_39q() { # LU-8041
 }
 run_test 39q "close won't zero out atime"
 
+test_39s() {
+	local atime0
+	local atime1
+	local atime2
+	local atime3
+	local atime4
+
+	umount_client $MOUNT
+	mount_client $MOUNT relatime
+
+	dd if=/dev/zero of=$DIR/$tfile bs=4096 count=1 status=noxfer conv=fsync
+	atime0=$(stat -c %X $DIR/$tfile)
+
+	# First read updates atime
+	sleep 1
+	cat $DIR/$tfile >/dev/null
+	atime1=$(stat -c %X $DIR/$tfile) # (atime = atime0 + 1)
+
+	# Next reads do not update atime
+	sleep 1
+	cat $DIR/$tfile >/dev/null
+	atime2=$(stat -c %X $DIR/$tfile) # (atime = atime0 + 1)
+
+	# If mtime is greater than atime, atime is updated
+	sleep 1
+	touch -m $DIR/$tfile # (mtime = now)
+	sleep 1
+	cat $DIR/$tfile >/dev/null # (atime is updated because atime < mtime)
+	atime3=$(stat -c %X $DIR/$tfile) # (atime = mtime = atime0 + 3)
+
+	# Next reads do not update atime
+	sleep 1
+	cat $DIR/$tfile >/dev/null
+	atime4=$(stat -c %X $DIR/$tfile)
+
+	# Remount the client to clear 'relatime' option
+	remount_client $MOUNT
+
+	if (( MDS1_VERSION >= $(version_code 2.15.50) )); then
+		# The full test lasted less than default atime_diff
+		# Client was remounted to clear 'relatime' option for next tests
+		# and to confirm atime was written to disk
+		local atime5=$(stat -c %X $DIR/$tfile)
+		(( atime3 == atime5 )) ||
+			error "atime3 $atime3 != atime5 $atime5"
+	fi
+
+	(( atime0 < atime1 )) ||
+		error "atime $atime0 should be smaller than $atime1"
+	(( atime1 == atime2 )) ||
+		error "atime $atime1 was updated to $atime2"
+	(( atime1 < atime3 )) || error "atime1 $atime1 != atime3 $atime3"
+	(( atime3 == atime4 )) || error "atime3 $atime3 != atime4 $atime4"
+}
+run_test 39s "relatime is supported"
+
 test_40() {
 	dd if=/dev/zero of=$DIR/$tfile bs=4096 count=1
 	$RUNAS $OPENFILE -f O_WRONLY:O_TRUNC $DIR/$tfile &&

lustre/llite/file.c
@@ -1529,12 +1529,47 @@ void ll_io_set_mirror(struct cl_io *io, const struct file *file)
 	       file->f_path.dentry->d_name.name, io->ci_designated_mirror);
 }
 
+/*
+ * This is relatime_need_update() from Linux 5.17, which is not exported.
+ */
+static int relatime_need_update(struct vfsmount *mnt, struct inode *inode,
+				struct timespec64 now)
+{
+
+	if (!(mnt->mnt_flags & MNT_RELATIME))
+		return 1;
+	/*
+	 * Is mtime younger than atime? If yes, update atime:
+	 */
+	if (timespec64_compare(&inode->i_mtime, &inode->i_atime) >= 0)
+		return 1;
+	/*
+	 * Is ctime younger than atime? If yes, update atime:
+	 */
+	if (timespec64_compare(&inode->i_ctime, &inode->i_atime) >= 0)
+		return 1;
+
+	/*
+	 * Is the previous atime value older than a day? If yes,
+	 * update atime:
+	 */
+	if ((long)(now.tv_sec - inode->i_atime.tv_sec) >= 24*60*60)
+		return 1;
+	/*
+	 * Good, we can skip the atime update:
+	 */
+	return 0;
+}
+
+/*
+ * Very similar to kernel function: !__atime_needs_update()
+ */
 static bool file_is_noatime(const struct file *file)
 {
-	const struct vfsmount *mnt = file->f_path.mnt;
-	const struct inode *inode = file_inode((struct file *)file);
+	struct vfsmount *mnt = file->f_path.mnt;
+	struct inode *inode = file_inode((struct file *)file);
+	struct timespec64 now;
 
-	/* Adapted from file_accessed() and touch_atime().*/
 	if (file->f_flags & O_NOATIME)
 		return true;
 
@@ -1551,6 +1586,11 @@ static bool file_is_noatime(const struct file *file)
 		return true;
 
 	if ((inode->i_sb->s_flags & SB_NODIRATIME) && S_ISDIR(inode->i_mode))
+		return true;
+
+	now = current_time(inode);
+
+	if (!relatime_need_update(mnt, inode, now))
 		return true;
 
 	return false;

commit 8beeec77c3b426a01e1f10ca51149c7ca7e01b7e

LU-15728 mdd: fix sanity-56oc failure
sanity 56oc starts failing since relatime patch was landed.
'relatime' patch introduced an atime behavior change.  It was forcing
atime uptime to disk on MDD if ondisk atime is older than ondisk
mtime/ctime to match relatime (even if relatime was not enabled).

This was an optimization, trying to have a slightly better atime value
cheaply. This is unfortunately causing regression in sanity-56oc.
Let's remove it for now until we understand that better.

Fixes: c10c6ee ("LU-15728 llite: fix relatime support")
Test-Parameters: testlist=sanity env=ONLY=56oc,ONLY_REPEAT=70
Change-Id: Ieed4d4c7523c26cfc5bc230986d96b2acf152dee
Signed-off-by: Aurelien Degremont <degremoa@amazon.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50009
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_object.c
@@ -651,12 +651,17 @@ static bool is_project_state_change(const struct lu_attr *oattr,
 	return false;
 }
 
-/*
+/**
  * This gives the same functionality as the code between
  * sys_chmod and inode_setattr
  * chown_common and inode_setattr
  * utimes and inode_setattr
  * This API is ported from mds_fix_attr but remove some unnecesssary stuff.
+ *
+ * @param[in]     oattr  Original, current object attributes.
+ * @param[in,out] la     New attributes to be evaluated and potentially
+ *                       modified depending on oattr content and other rules.
+ * @param[in]     ma     md_attr to be evaluated for that object.
  */
 static int mdd_fix_attr(const struct lu_env *env, struct mdd_object *obj,
 			const struct lu_attr *oattr, struct lu_attr *la,
@@ -710,7 +715,6 @@ static int mdd_fix_attr(const struct lu_env *env, struct mdd_object *obj,
 		 * close RPCs.
 		 */
 		if (la->la_valid & LA_ATIME &&
-		    oattr->la_atime > max(oattr->la_ctime, oattr->la_mtime) &&
 		    la->la_atime <= (oattr->la_atime +
 				mdd_obj2mdd_dev(obj)->mdd_atime_diff))
 			la->la_valid &= ~LA_ATIME;

lustre/tests/sanity.sh
@@ -5303,15 +5303,6 @@ test_39s() {
 	# Remount the client to clear 'relatime' option
 	remount_client $MOUNT
 
-	if (( MDS1_VERSION >= $(version_code 2.15.50) )); then
-		# The full test lasted less than default atime_diff
-		# Client was remounted to clear 'relatime' option for next tests
-		# and to confirm atime was written to disk
-		local atime5=$(stat -c %X $DIR/$tfile)
-		(( atime3 == atime5 )) ||
-			error "atime3 $atime3 != atime5 $atime5"
-	fi
-
 	(( atime0 < atime1 )) ||
 		error "atime $atime0 should be smaller than $atime1"
 	(( atime1 == atime2 )) ||