lustre/lustre-release

commit a813e81870096bcfecbe12aeeed8e1b0114cd474

LU-12275 sec: add llcrypt as file encryption library
Include fscrypt sources from Linux 5.4 into libcfs kernel module.
All fscrypt_ prefixes have been replaced with llcrypt_ to avoid
collision. For the exact transformations carried out, please see
script contrib/scripts/fscrypt_inclusion.sh, and patches under
contrib/scripts/crypto_patches/.

This llcrypt library will be built if kernel provides minimum
encryption support, ie IS_ENCRYPTED and S_ENCRYPTED.
If kernel provides support for encryption policies v2 (included in
Linux 5.4), then llcrypt library will not be built, and in-kernel
fscrypt will be used instead.

To be independent as much as possible from the kernel, llcrypt relies
on a new 'lsi_cop' field added to struct lustre_sb_info, to point to
struct llcrypt_operations, and makes use of the new 'lsi_master_keys'
field to store file system keys. llcrypt also uses i_private field on
struct inode to store file-specific enryption info.

Test-Parameters: trivial
Test-Parameters: clientdistro=ubuntu1804 testgroup=review-ldiskfs
Test-Parameters: clientdistro=el8.1 testgroup=review-ldiskfs
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Ide2431b0c3381214aed2e54dd3084132e9545ca8
Reviewed-on: https://review.whamcloud.com/38127
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

contrib/scripts/crypto_patches/0006_bvec_iter_all.patch
@@ -1,0 +1,17 @@
+Linux 5.1 (commit 6dc4f100c175dd0511ae8674786e7c9006cdfbfa) introduces
+bvec_iter_all.
+
+--- a/libcfs/libcfs/crypto/bio.c
++++ b/libcfs/libcfs/crypto/bio.c
+@@ -29,7 +29,11 @@
+ static void __llcrypt_decrypt_bio(struct bio *bio, bool done)
+ {
+ 	struct bio_vec *bv;
++#ifdef HAVE_BVEC_ITER_ALL
+ 	struct bvec_iter_all iter_all;
++#else
++	int iter_all;
++#endif
+ 
+ 	bio_for_each_segment_all(bv, bio, iter_all) {
+ 		struct page *page = bv->bv_page;

libcfs/libcfs/Makefile.in
@@ -5,9 +5,13 @@ libcfs-linux-objs += linux-curproc.o
 libcfs-linux-objs += linux-hash.o
 libcfs-linux-objs += linux-wait.o
 
+libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
+libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o bio.o
+
 default: all
 
 libcfs-linux-objs := $(addprefix linux/,$(libcfs-linux-objs))
+libcfs-crypto-objs := $(addprefix crypto/,$(libcfs-crypto-objs))
 
 libcfs-all-objs := debug.o fail.o module.o tracefile.o \
 		   libcfs_string.o hash.o heap.o \
@@ -17,6 +21,7 @@ libcfs-all-objs := debug.o fail.o module.o tracefile.o \
 		   linux-debug.o linux-tracefile.o
 
 libcfs-objs := $(libcfs-linux-objs) $(libcfs-all-objs)
+@LLCRYPT_TRUE@libcfs-objs += $(libcfs-crypto-objs)
 
 EXTRA_PRE_CFLAGS := -I@LUSTRE@/../libcfs/libcfs
 

libcfs/libcfs/crypto/Makefile.am
@@ -1,0 +1,4 @@
+if LLCRYPT
+EXTRA_DIST = crypto.c fname.c hkdf.c hooks.c keyring.c \
+	keysetup.c keysetup_v1.c policy.c bio.c llcrypt_private.h
+endif

libcfs/libcfs/crypto/bio.c
@@ -1,0 +1,128 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * This contains encryption functions for per-file encryption.
+ *
+ * Copyright (C) 2015, Google, Inc.
+ * Copyright (C) 2015, Motorola Mobility
+ *
+ * Written by Michael Halcrow, 2014.
+ *
+ * Filename encryption additions
+ *	Uday Savagaonkar, 2014
+ * Encryption policy handling additions
+ *	Ildar Muslukhov, 2014
+ * Add llcrypt_pullback_bio_page()
+ *	Jaegeuk Kim, 2015.
+ *
+ * This has not yet undergone a rigorous security audit.
+ *
+ * The usage of AES-XTS should conform to recommendations in NIST
+ * Special Publication 800-38E and IEEE P1619/D16.
+ */
+/*
+ * Linux commit 219d54332a09
+ * tags/v5.4
+ */
+
+#include <linux/pagemap.h>
+#include <linux/module.h>
+#include <linux/bio.h>
+#include <linux/namei.h>
+#include "llcrypt_private.h"
+
+static void __llcrypt_decrypt_bio(struct bio *bio, bool done)
+{
+	struct bio_vec *bv;
+#ifdef HAVE_BVEC_ITER_ALL
+	struct bvec_iter_all iter_all;
+#else
+	int iter_all;
+#endif
+
+	bio_for_each_segment_all(bv, bio, iter_all) {
+		struct page *page = bv->bv_page;
+		int ret = llcrypt_decrypt_pagecache_blocks(page, bv->bv_len,
+							   bv->bv_offset);
+		if (ret)
+			SetPageError(page);
+		else if (done)
+			SetPageUptodate(page);
+		if (done)
+			unlock_page(page);
+	}
+}
+
+void llcrypt_decrypt_bio(struct bio *bio)
+{
+	__llcrypt_decrypt_bio(bio, false);
+}
+EXPORT_SYMBOL(llcrypt_decrypt_bio);
+
+static void completion_pages(struct work_struct *work)
+{
+	struct llcrypt_ctx *ctx = container_of(work, struct llcrypt_ctx, work);
+	struct bio *bio = ctx->bio;
+
+	__llcrypt_decrypt_bio(bio, true);
+	llcrypt_release_ctx(ctx);
+	bio_put(bio);
+}
+
+void llcrypt_enqueue_decrypt_bio(struct llcrypt_ctx *ctx, struct bio *bio)
+{
+	INIT_WORK(&ctx->work, completion_pages);
+	ctx->bio = bio;
+	llcrypt_enqueue_decrypt_work(&ctx->work);
+}
+EXPORT_SYMBOL(llcrypt_enqueue_decrypt_bio);
+
+int llcrypt_zeroout_range(const struct inode *inode, pgoff_t lblk,
+				sector_t pblk, unsigned int len)
+{
+	const unsigned int blockbits = inode->i_blkbits;
+	const unsigned int blocksize = 1 << blockbits;
+	struct page *ciphertext_page;
+	struct bio *bio;
+	int ret, err = 0;
+
+	ciphertext_page = llcrypt_alloc_bounce_page(GFP_NOWAIT);
+	if (!ciphertext_page)
+		return -ENOMEM;
+
+	while (len--) {
+		err = llcrypt_crypt_block(inode, FS_ENCRYPT, lblk,
+					  ZERO_PAGE(0), ciphertext_page,
+					  blocksize, 0, GFP_NOFS);
+		if (err)
+			goto errout;
+
+		bio = bio_alloc(GFP_NOWAIT, 1);
+		if (!bio) {
+			err = -ENOMEM;
+			goto errout;
+		}
+		bio_set_dev(bio, inode->i_sb->s_bdev);
+		bio->bi_iter.bi_sector = pblk << (blockbits - 9);
+		bio_set_op_attrs(bio, REQ_OP_WRITE, 0);
+		ret = bio_add_page(bio, ciphertext_page, blocksize, 0);
+		if (WARN_ON(ret != blocksize)) {
+			/* should never happen! */
+			bio_put(bio);
+			err = -EIO;
+			goto errout;
+		}
+		err = submit_bio_wait(bio);
+		if (err == 0 && bio->bi_status)
+			err = -EIO;
+		bio_put(bio);
+		if (err)
+			goto errout;
+		lblk++;
+		pblk++;
+	}
+	err = 0;
+errout:
+	llcrypt_free_bounce_page(ciphertext_page);
+	return err;
+}
+EXPORT_SYMBOL(llcrypt_zeroout_range);

libcfs/include/Makefile.am
@@ -1,1 +1,1 @@
-SUBDIRS = libcfs 
+SUBDIRS = libcfs uapi

libcfs/include/libcfs/Makefile.am
@@ -1,5 +1,7 @@
 SUBDIRS = linux util
 DIST_SUBDIRS = linux util
+SUBDIRS += crypto
+DIST_SUBDIRS += crypto
 
 libcfsdir = $(includedir)/libcfs
 

libcfs/include/libcfs/crypto/Makefile.am
@@ -1,0 +1,1 @@
+EXTRA_DIST = llcrypt.h

commit d328818a456daf30c20c8df0aa0be9dd2a2b6a9e

LU-12275 sec: remove bio functions in fscrypt compat
Remove libcfs/llibcfs/crypto/bio.c since direct block device access
is not needed for client builds, and the use of stuct bio on the
client adds unnecessary complexity to portability.

Test-Parameters: trivial
Fixes: a813e8187 ("LU-12275 sec: add llcrypt as file encryption library")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I97642dfd85053b9ea4196374f2002ffb6a2540e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50023
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

contrib/scripts/crypto_patches/0006_bvec_iter_all.patch
@@ -1,17 +1,54 @@
-Linux 5.1 (commit 6dc4f100c175dd0511ae8674786e7c9006cdfbfa) introduces
-bvec_iter_all.
-
---- a/libcfs/libcfs/crypto/bio.c
-+++ b/libcfs/libcfs/crypto/bio.c
-@@ -29,7 +29,11 @@
- static void __llcrypt_decrypt_bio(struct bio *bio, bool done)
- {
- 	struct bio_vec *bv;
-+#ifdef HAVE_BVEC_ITER_ALL
- 	struct bvec_iter_all iter_all;
-+#else
-+	int iter_all;
+LU-12275 sec: disable bio functions on client
+    
+diff --git a/libcfs/include/libcfs/crypto/llcrypt.h b/libcfs/include/libcfs/crypto/llcrypt.h
+index b152915fb1..93e1766edc 100644
+--- a/libcfs/include/libcfs/crypto/llcrypt.h
++++ b/libcfs/include/libcfs/crypto/llcrypt.h
+@@ -267,12 +267,14 @@ static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
+ 	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
+ }
+ 
++#ifdef HAVE_SERVER_SUPPORT
+ /* bio.c */
+ extern void llcrypt_decrypt_bio(struct bio *);
+ extern void llcrypt_enqueue_decrypt_bio(struct llcrypt_ctx *ctx,
+ 					struct bio *bio);
+ extern int llcrypt_zeroout_range(const struct inode *, pgoff_t, sector_t,
+ 				 unsigned int);
 +#endif
  
- 	bio_for_each_segment_all(bv, bio, iter_all) {
- 		struct page *page = bv->bv_page;
+ /* hooks.c */
+ extern int llcrypt_file_open(struct inode *inode, struct file *filp);
+@@ -525,6 +527,7 @@ static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
+ 	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
+ }
+ 
++#ifdef HAVE_SERVER_SUPPORT
+ /* bio.c */
+ static inline void llcrypt_decrypt_bio(struct bio *bio)
+ {
+@@ -540,6 +543,7 @@ static inline int llcrypt_zeroout_range(const struct inode *inode, pgoff_t lblk,
+ {
+ 	return -EOPNOTSUPP;
+ }
++#endif
+ 
+ /* hooks.c */
+ 
+diff --git a/libcfs/libcfs/crypto/bio.c b/libcfs/libcfs/crypto/bio.c
+index 78b56cbd36..02ecacff5e 100644
+--- a/libcfs/libcfs/crypto/bio.c
++++ b/libcfs/libcfs/crypto/bio.c
+@@ -24,6 +24,7 @@
+  * tags/v5.4
+  */
+ 
++#ifdef HAVE_SERVER_SUPPORT
+ #include <linux/pagemap.h>
+ #include <linux/module.h>
+ #include <linux/bio.h>
+@@ -126,3 +127,4 @@ errout:
+ 	return err;
+ }
+ EXPORT_SYMBOL(llcrypt_zeroout_range);
++#endif

libcfs/include/libcfs/crypto/llcrypt.h
@@ -267,13 +267,6 @@ static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
 	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
 }
 
-/* bio.c */
-extern void llcrypt_decrypt_bio(struct bio *);
-extern void llcrypt_enqueue_decrypt_bio(struct llcrypt_ctx *ctx,
-					struct bio *bio);
-extern int llcrypt_zeroout_range(const struct inode *, pgoff_t, sector_t,
-				 unsigned int);
-
 /* hooks.c */
 extern int llcrypt_file_open(struct inode *inode, struct file *filp);
 extern int __llcrypt_prepare_link(struct inode *inode, struct inode *dir,
@@ -523,22 +516,6 @@ static inline bool llcrypt_match_name(const struct llcrypt_name *fname,
 	if (de_name_len != fname->disk_name.len)
 		return false;
 	return !memcmp(de_name, fname->disk_name.name, fname->disk_name.len);
-}
-
-/* bio.c */
-static inline void llcrypt_decrypt_bio(struct bio *bio)
-{
-}
-
-static inline void llcrypt_enqueue_decrypt_bio(struct llcrypt_ctx *ctx,
-					       struct bio *bio)
-{
-}
-
-static inline int llcrypt_zeroout_range(const struct inode *inode, pgoff_t lblk,
-					sector_t pblk, unsigned int len)
-{
-	return -EOPNOTSUPP;
 }
 
 /* hooks.c */

libcfs/libcfs/Makefile.in
@@ -8,7 +8,7 @@ libcfs-linux-objs += glob.o
 libcfs-linux-objs += xarray.o
 
 libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
-libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o bio.o
+libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o
 
 default: all
 

libcfs/libcfs/crypto/Makefile.am
@@ -1,4 +1,4 @@
 if LLCRYPT
 EXTRA_DIST = crypto.c fname.c hkdf.c hooks.c keyring.c \
-	keysetup.c keysetup_v1.c policy.c bio.c llcrypt_private.h
+	keysetup.c keysetup_v1.c policy.c llcrypt_private.h
 endif

libcfs/libcfs/crypto/bio.c
@@ -1,128 +1,0 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * This contains encryption functions for per-file encryption.
- *
- * Copyright (C) 2015, Google, Inc.
- * Copyright (C) 2015, Motorola Mobility
- *
- * Written by Michael Halcrow, 2014.
- *
- * Filename encryption additions
- *	Uday Savagaonkar, 2014
- * Encryption policy handling additions
- *	Ildar Muslukhov, 2014
- * Add llcrypt_pullback_bio_page()
- *	Jaegeuk Kim, 2015.
- *
- * This has not yet undergone a rigorous security audit.
- *
- * The usage of AES-XTS should conform to recommendations in NIST
- * Special Publication 800-38E and IEEE P1619/D16.
- */
-/*
- * Linux commit 219d54332a09
- * tags/v5.4
- */
-
-#include <linux/pagemap.h>
-#include <linux/module.h>
-#include <linux/bio.h>
-#include <linux/namei.h>
-#include <lustre_compat.h>
-#include "llcrypt_private.h"
-
-static void __llcrypt_decrypt_bio(struct bio *bio, bool done)
-{
-	struct bio_vec *bv;
-#ifdef HAVE_BVEC_ITER_ALL
-	struct bvec_iter_all iter_all;
-#else
-	int iter_all;
-#endif
-
-	bio_for_each_segment_all(bv, bio, iter_all) {
-		struct page *page = bv->bv_page;
-		int ret = llcrypt_decrypt_pagecache_blocks(page, bv->bv_len,
-							   bv->bv_offset);
-		if (ret)
-			SetPageError(page);
-		else if (done)
-			SetPageUptodate(page);
-		if (done)
-			unlock_page(page);
-	}
-}
-
-void llcrypt_decrypt_bio(struct bio *bio)
-{
-	__llcrypt_decrypt_bio(bio, false);
-}
-EXPORT_SYMBOL(llcrypt_decrypt_bio);
-
-static void completion_pages(struct work_struct *work)
-{
-	struct llcrypt_ctx *ctx = container_of(work, struct llcrypt_ctx, work);
-	struct bio *bio = ctx->bio;
-
-	__llcrypt_decrypt_bio(bio, true);
-	llcrypt_release_ctx(ctx);
-	bio_put(bio);
-}
-
-void llcrypt_enqueue_decrypt_bio(struct llcrypt_ctx *ctx, struct bio *bio)
-{
-	INIT_WORK(&ctx->work, completion_pages);
-	ctx->bio = bio;
-	llcrypt_enqueue_decrypt_work(&ctx->work);
-}
-EXPORT_SYMBOL(llcrypt_enqueue_decrypt_bio);
-
-int llcrypt_zeroout_range(const struct inode *inode, pgoff_t lblk,
-				sector_t pblk, unsigned int len)
-{
-	const unsigned int blockbits = inode->i_blkbits;
-	const unsigned int blocksize = 1 << blockbits;
-	struct page *ciphertext_page;
-	struct bio *bio;
-	int ret, err = 0;
-
-	ciphertext_page = llcrypt_alloc_bounce_page(GFP_NOWAIT);
-	if (!ciphertext_page)
-		return -ENOMEM;
-
-	while (len--) {
-		err = llcrypt_crypt_block(inode, FS_ENCRYPT, lblk,
-					  ZERO_PAGE(0), ciphertext_page,
-					  blocksize, 0, GFP_NOFS);
-		if (err)
-			goto errout;
-
-		bio = cfs_bio_alloc(inode->i_sb->s_bdev, 1, REQ_OP_WRITE,
-				    GFP_NOWAIT);
-		if (!bio) {
-			err = -ENOMEM;
-			goto errout;
-		}
-		bio->bi_iter.bi_sector = pblk << (blockbits - 9);
-		ret = bio_add_page(bio, ciphertext_page, blocksize, 0);
-		if (WARN_ON(ret != blocksize)) {
-			/* should never happen! */
-			bio_put(bio);
-			err = -EIO;
-			goto errout;
-		}
-		err = submit_bio_wait(bio);
-		if (err == 0 && bio->bi_status)
-			err = -EIO;
-		bio_put(bio);
-		if (err)
-			goto errout;
-		lblk++;
-		pblk++;
-	}
-	err = 0;
-errout:
-	llcrypt_free_bounce_page(ciphertext_page);
-	return err;
-}
-EXPORT_SYMBOL(llcrypt_zeroout_range);