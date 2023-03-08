lustre/lustre-release

commit 12604ac4deb242877bac5f12f517c2a5c0c2c72f

LU-16555 obdclass: print more special chars for jobid
Print more YAML compatible special chars for jobid.
Currently they are any of ".@-_:/".

Test-Parameters: trivial
Fixes: 338381574b ("LU-11407 tgt: cleanup job_stats output printing")
Signed-off-by: Lei Feng <flei@whamcloud.com>
Change-Id: Ic3272b73c95b76ad3171cc7a368a18f804b9aa3e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49998
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/obdclass/lprocfs_jobstats.c
@@ -455,8 +455,7 @@ static int lprocfs_jobstats_seq_show(struct seq_file *p, void *v)
 	for (c = job->js_jobid, end = job->js_jobid + sizeof(job->js_jobid);
 	     c < end && *c != '\0';
 	     c++, joblen++) {
-		if (!isalnum(*c) &&
-		    *c != '.' && *c != '@' && *c != '-' && *c != '_') {
+		if (!isalnum(*c) && strchr(".@-_:/", *c) == NULL) {
 			quote = "\"";
 			snprintf(escaped + joblen, sizeof(escaped), "\\x%02X",
 				 (unsigned char)*c);

commit a99eb0210a5741138981e999082c695aef0dffff

LU-16555 obdclass: quote jobid with ':' char
quote jobid with ':' char even though ':' is printed literally.

Fixes: 12604ac4de (LU-16555 obdclass: print more special chars for jobid)
Test-Parameters: trivial
Signed-off-by: Lei Feng <flei@whamcloud.com>
Change-Id: I8bf642180b00f068921598c3a0ea30b4f5b99c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50150
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/obdclass/lprocfs_jobstats.c
@@ -462,6 +462,9 @@ static int lprocfs_jobstats_seq_show(struct seq_file *p, void *v)
 			joblen += 3;
 		} else {
 			escaped[joblen] = *c;
+			/* if jobid has ':', it should be quoted too */
+			if (*c == ':')
+				quote = "\"";
 		}
 	}
 