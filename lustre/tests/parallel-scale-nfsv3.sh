513eb670b01f15104cbeb2909a141d2174dcc874
Andreas Dilger <adilger@whamcloud.com>
2023-03-22 17:42:10 -0600

    LU-16163 tests: skip racer_on_nfs for NFSv3
    This test is continually failing and nobody is available to
    fix it (it may be an NFS bug or a Lustre bug, unsure).
    
    This same test passes on NFSv4 regularly.
    
    Test-Parameters: trivial testlist=parallel-scale-nfsv3
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I7d9ac390c26aa8478dd35457ba20061747c2b92e
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50385
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Alex Deiter <alex.deiter@gmail.com>
    Reviewed-by: Sarah Liu <sarah@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/parallel-scale-nfsv3.sh
+++ lustre/tests/parallel-scale-nfsv3.sh
@@ -4,4 +4,7 @@ LUSTRE=${LUSTRE:-$(dirname $0)/..}
 . $LUSTRE/tests/test-framework.sh
 init_test_env "$@"
 
+
+ALWAYS_EXCEPT="$PARALLEL_SCALE_NFSV3_EXCEPT "
+always_except LU-16163 racer_on_nfs
 $LUSTRE/tests/parallel-scale-nfs.sh 3
