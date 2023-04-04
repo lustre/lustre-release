lustre/lustre-release

commit a114f6b8c52bb97dcad093e5cb32b903ae9f602f

LU-13344 servers: change request timeouts to s32
In the latest kernels time_t has been removed since time_t has
been a 64 bit value just like time64_t so no need for it anymore.
The field sf_run_time is stored on local media and its expected
to be 32 bit in size so change it to s32 explictly. The fields
the MDT coordinator that are currently time_t are a simple change
to s32 since they represent a time period and not a time stamp.
Lastly just update a comment in llog_swab.c since time_t will no
longer exist but we want to point out time in the llog is 32 bits
in size.

Change-Id: I0849fc9798c191d1fed77f7bba1357a209924418
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37950
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_scrub.h
@@ -240,7 +240,7 @@ struct scrub_file {
 	 * to time64_t since this breaks backwards compatibility.
 	 * It shouldn't take more than 136 years to complete :-)
 	 */
-	time_t	sf_run_time;
+	s32	sf_run_time;
 
 	/* How many completed OI scrub ran on the device. */
 	__u32   sf_success_count;

lustre/obdclass/scrub.c
@@ -487,7 +487,7 @@ void scrub_dump(struct seq_file *m, struct lustre_scrub *scrub)
 	} else {
 		if (sf->sf_run_time != 0)
 			speed = div_s64(speed, sf->sf_run_time);
-		seq_printf(m, "run_time: %ld seconds\n"
+		seq_printf(m, "run_time: %d seconds\n"
 			   "average_speed: %lld objects/sec\n"
 			   "real_time_speed: N/A\n"
 			   "current_position: N/A\n",

lustre/mdt/mdt_coordinator.c
@@ -2068,7 +2068,7 @@ ssize_t loop_period_show(struct kobject *kobj, struct attribute *attr,
 	struct coordinator *cdt = container_of(kobj, struct coordinator,
 					       cdt_hsm_kobj);
 
-	return scnprintf(buf, PAGE_SIZE, "%lu\n", cdt->cdt_loop_period);
+	return scnprintf(buf, PAGE_SIZE, "%u\n", cdt->cdt_loop_period);
 }
 
 ssize_t loop_period_store(struct kobject *kobj, struct attribute *attr,
@@ -2096,7 +2096,7 @@ ssize_t grace_delay_show(struct kobject *kobj, struct attribute *attr,
 	struct coordinator *cdt = container_of(kobj, struct coordinator,
 					       cdt_hsm_kobj);
 
-	return scnprintf(buf, PAGE_SIZE, "%lu\n", cdt->cdt_grace_delay);
+	return scnprintf(buf, PAGE_SIZE, "%u\n", cdt->cdt_grace_delay);
 }
 
 ssize_t grace_delay_store(struct kobject *kobj, struct attribute *attr,
@@ -2125,7 +2125,7 @@ ssize_t active_request_timeout_show(struct kobject *kobj,
 	struct coordinator *cdt = container_of(kobj, struct coordinator,
 					       cdt_hsm_kobj);
 
-	return scnprintf(buf, PAGE_SIZE, "%lu\n", cdt->cdt_active_req_timeout);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", cdt->cdt_active_req_timeout);
 }
 
 ssize_t active_request_timeout_store(struct kobject *kobj,

lustre/mdt/mdt_internal.h
@@ -149,10 +149,10 @@ struct coordinator {
 						       * list */
 	struct mutex		 cdt_restore_lock;    /**< protect restore
 						       * list */
-	time_t			 cdt_loop_period;     /**< llog scan period */
-	time_t			 cdt_grace_delay;     /**< request grace
+	s32			 cdt_loop_period;     /**< llog scan period */
+	s32			 cdt_grace_delay;     /**< request grace
 						       * delay */
-	time_t			 cdt_active_req_timeout; /**< request timeout */
+	s32			 cdt_active_req_timeout; /**< request timeout */
 	__u32			 cdt_default_archive_id; /**< archive id used
 						       * when none are
 						       * specified */

lustre/obdclass/llog_swab.c
@@ -446,7 +446,7 @@ void lustre_swab_cfg_marker(struct cfg_marker *marker, int swab, int size)
 		__u32 createtime, canceltime;
 		/*
 		 * There was a problem with the original declaration of
-		 * cfg_marker on 32-bit systems because it used time_t as
+		 * cfg_marker on 32-bit systems because it used 32 time as
 		 * a wire protocol structure, and didn't verify this in
 		 * wirecheck.  We now have to convert the offsets of the
 		 * later fields in order to work on 32- and 64-bit systems.

commit a209505361cf858ec91b0a89992691c5b005ab72

LU-16655 scrub: upgrade scrub_file from 2.12 format
Scrub_file->sf_oi_count has different offsets in Lustre-2.10,
Lustre-2.12, and Lustre-2.15 due to unintended format changes.
Lustre-2.15 reads sf_oi_count from offset of sf_success_count
and may initialize incorrect number of OI files, and not be
able to do FID lookups for existing filesystem objects.

Fixes: a114f6b8c5 ("LU-13344 servers: change request timeouts to s32")
Fixes: 4c2f028a95 ("LU-9019 osd-ldiskfs: migrate to 64 bit time")
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: Id7c8bd555229405d604456c48447f01fd121aca9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50480
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/lustre_scrub.h
@@ -44,6 +44,7 @@
 #define PFID_STRIPE_COUNT_MASK	((1 << PFID_STRIPE_IDX_BITS) - 1)
 
 #define SCRUB_MAGIC_V1			0x4C5FD252
+#define SCRUB_MAGIC_V2			0x4C5FE253
 #define SCRUB_CHECKPOINT_INTERVAL	60
 #define SCRUB_WINDOW_SIZE		1024
 

lustre/obdclass/scrub.c
@@ -119,7 +119,7 @@ void scrub_file_init(struct lustre_scrub *scrub, uuid_t uuid)
 
 	memset(sf, 0, sizeof(*sf));
 	uuid_copy(&sf->sf_uuid, &uuid);
-	sf->sf_magic = SCRUB_MAGIC_V1;
+	sf->sf_magic = SCRUB_MAGIC_V2;
 	sf->sf_status = SS_INIT;
 }
 EXPORT_SYMBOL(scrub_file_init);
@@ -133,6 +133,7 @@ void scrub_file_reset(struct lustre_scrub *scrub, uuid_t uuid, u64 flags)
 	       scrub->os_name, sf->sf_flags, flags);
 
 	uuid_copy(&sf->sf_uuid, &uuid);
+	sf->sf_magic = SCRUB_MAGIC_V2;
 	sf->sf_status = SS_INIT;
 	sf->sf_flags |= flags;
 	sf->sf_flags &= ~SF_AUTO;
@@ -183,9 +184,13 @@ int scrub_file_load(const struct lu_env *env, struct lustre_scrub *scrub)
 	}
 
 	scrub_file_to_cpu(sf, &scrub->os_file_disk);
-	if (sf->sf_magic != SCRUB_MAGIC_V1) {
-		CDEBUG(D_LFSCK, "%s: invalid scrub magic 0x%x != 0x%x\n",
-		       scrub->os_name, sf->sf_magic, SCRUB_MAGIC_V1);
+	if (sf->sf_magic == SCRUB_MAGIC_V1) {
+		CWARN("%s: reset scrub OI count for format change (LU-16655)\n",
+		      scrub->os_name);
+		sf->sf_oi_count = 0;
+	} else if (sf->sf_magic != SCRUB_MAGIC_V2) {
+		CDEBUG(D_LFSCK, "%s: invalid scrub magic %#x, should be %#x\n",
+		       scrub->os_name, sf->sf_magic, SCRUB_MAGIC_V2);
 		return -EFAULT;
 	}
 

lustre/osd-ldiskfs/osd_oi.c
@@ -397,8 +397,9 @@ int osd_oi_init(struct osd_thread_info *info, struct osd_device *osd,
 	int rc;
 	ENTRY;
 
-	if (unlikely(sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0) {
-		LCONSOLE_WARN("%s: Invalid OI count in scrub file %d\n",
+	if (unlikely((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0 ||
+		     sf->sf_oi_count > OSD_OI_FID_NR_MAX)) {
+		LCONSOLE_WARN("%s: invalid OI count %u in scrub file, reset it\n",
 			      osd_dev2name(osd), sf->sf_oi_count);
 		sf->sf_oi_count = 0;
 	}

lustre/osd-zfs/osd_scrub.c
@@ -1380,8 +1380,9 @@ int osd_scrub_setup(const struct lu_env *env, struct osd_device *dev,
 			dirty = true;
 		}
 
-		if ((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0) {
-			LCONSOLE_WARN("%s: invalid oi count %d, set it to %d\n",
+		if (unlikely((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0 ||
+			     sf->sf_oi_count > OSD_OI_FID_NR_MAX)) {
+			LCONSOLE_WARN("%s: invalid OI count %u, reset to %u\n",
 				      osd_name(dev), sf->sf_oi_count,
 				      osd_oi_count);
 			sf->sf_oi_count = osd_oi_count;