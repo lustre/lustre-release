892d726f274c7cd4e505689ad69194ac68dc323b
Alex Deiter <adeiter@tintri.com>
2023-04-07 23:49:23 +0400

    LU-16163 tests: skip racer_on_nfs for NFSv3
    Export ALWAYS_EXCEPT env for child NFS test
    
    Fixes: 513eb670b0 ("LU-16163 tests: skip racer_on_nfs for NFSv3")
    Test-Parameters: trivial testlist=parallel-scale-nfsv3
    Signed-off-by: Alex Deiter <adeiter@tintri.com>
    Change-Id: Ibb4a9916166f13ab9bd2374b33d4313453972276
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50579
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/tests/parallel-scale-nfsv3.sh
+++ lustre/tests/parallel-scale-nfsv3.sh
@@ -4,7 +4,6 @@ LUSTRE=${LUSTRE:-$(dirname $0)/..}
 . $LUSTRE/tests/test-framework.sh
 init_test_env "$@"
 
-
-ALWAYS_EXCEPT="$PARALLEL_SCALE_NFSV3_EXCEPT "
+export ALWAYS_EXCEPT="$PARALLEL_SCALE_NFSV3_EXCEPT "
 always_except LU-16163 racer_on_nfs
 $LUSTRE/tests/parallel-scale-nfs.sh 3
