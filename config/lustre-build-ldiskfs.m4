lustre/lustre-release

commit 068e5f13fb94802ced68712ee11e7f9cb106d0ae

LU-13743 build: Explicitly require encryption support
Linux commit v5.18-rc5-17-gb1241c8eb977
  ext4: move ext4 crypto code to its own file crypto.c

Update the ldiskfs Makefile to exclude crypto.c when
CONFIG_FS_ENCRYPTION is not enabled.

Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ic8a40f3d395286bb52ed20693fd7cc4755b10556
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/39243
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

ldiskfs/Makefile.in
@@ -10,7 +10,11 @@ linux_headers := $(wildcard @LINUX@/include/linux/ext4*.h)
 linux_new_headers := htree_lock.h
 trace_headers := $(wildcard @LINUX@/include/trace/events/ext4*.h)
 
+ifeq ($(CONFIG_FS_ENCRYPTION),)
+backfs_sources := $(filter-out %.mod.c %/inode-test.c %/crypto.c,$(wildcard @EXT4_SRC_DIR@/*.c))
+else
 backfs_sources := $(filter-out %.mod.c %/inode-test.c,$(wildcard @EXT4_SRC_DIR@/*.c))
+endif
 
 new_sources := mmp.c htree_lock.c critical_encode.h
 new_headers :=

commit 71746b6277a59ede2110420f5907fe2dafd9ac2a

LU-16664 build: Debian server fails building crypto.c
When building deb files against a server built without
CONFIG_FS_ENCRYPTION it still attempts to build crypto.c
when it should not.

Test-Parameters: trivial
Fixes: 068e5f13fb ("LU-13743 build: Explicitly require encryption support")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Id1e67daa7b021fdfee49be4eb0beb2b86ca62c39
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50406
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-ldiskfs.m4
@@ -612,18 +612,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 
 	LDISKFS_LINUX_SERIES
 	LDISKFS_AC_PATCH_PROGRAM
-	LB_EXT_FREE_BLOCKS_WITH_BUFFER_HEAD
-	LB_EXT4_JOURNAL_START_3ARGS
-	LB_EXT4_BREAD_4ARGS
-	LB_EXT4_HAVE_INFO_DQUOT
-	LB_EXT4_HAVE_I_CRYPT_INFO
-	LB_LDISKFS_JOURNAL_ENSURE_CREDITS
-	LB_LDISKFS_IGET_HAS_FLAGS_ARG
-	LB_LDISKFS_FIND_ENTRY_LOCKED_EXISTS
-	LB_LDISKFSFS_DIRHASH_WANTS_DIR
-	LB_JBD2_H_TOTAL_CREDITS
 	LB_EXT4_INC_DEC_COUNT_2ARGS
-	LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS
 	LB_EXT4_JOURNAL_GET_WRITE_ACCESS_4A
 	LB_HAVE_INODE_LOCK_SHARED
 	AC_DEFINE(CONFIG_LDISKFS_FS_POSIX_ACL, 1, [posix acls for ldiskfs])
@@ -655,6 +644,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 		LB_SRC_LDISKFSFS_DIRHASH_WANTS_DIR
 		LB_SRC_JBD2_H_TOTAL_CREDITS
 		LB_SRC_JBD2_JOURNAL_GET_MAX_TXN_BUFS
+		LB2_SRC_CHECK_CONFIG_IM([FS_ENCRYPTION])
 
 		LB2_LINUX_TEST_COMPILE_ALL([ldiskfs],
 			[for available ldiskfs ext4 interfaces])
@@ -671,7 +661,11 @@ AS_IF([test x$enable_ldiskfs != xno],[
 		LB_LDISKFSFS_DIRHASH_WANTS_DIR
 		LB_JBD2_H_TOTAL_CREDITS
 		LB_JBD2_JOURNAL_GET_MAX_TXN_BUFS
+		LB2_TEST_CHECK_CONFIG_IM([FS_ENCRYPTION], [
+			EXT4_CRYPTO=],[
+			EXT4_CRYPTO='%/crypto.c'])
 	])
+	AC_SUBST(EXT4_CRYPTO)
 ])
 
 #

ldiskfs/Makefile.in
@@ -10,11 +10,7 @@ linux_headers := $(wildcard @LINUX@/include/linux/ext4*.h)
 linux_new_headers := htree_lock.h
 trace_headers := $(wildcard @LINUX@/include/trace/events/ext4*.h)
 
-ifeq ($(CONFIG_FS_ENCRYPTION),)
-backfs_sources := $(filter-out %.mod.c %/inode-test.c %/crypto.c,$(wildcard @EXT4_SRC_DIR@/*.c))
-else
-backfs_sources := $(filter-out %.mod.c %/inode-test.c,$(wildcard @EXT4_SRC_DIR@/*.c))
-endif
+backfs_sources := $(filter-out %.mod.c %/inode-test.c @EXT4_CRYPTO@,$(wildcard @EXT4_SRC_DIR@/*.c))
 
 new_sources := mmp.c htree_lock.c critical_encode.h encdata.h
 new_headers :=