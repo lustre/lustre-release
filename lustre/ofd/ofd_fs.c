lustre/lustre-release

commit 0ecb2a167c56ffff8e4fcb5cf576fb8c5d9e64fe

LU-11912 ofd: reduce LUSTRE_DATA_SEQ_MAX_WIDTH
Reduce LUSTRE_DATA_SEQ_MAX_WIDTH from ~4B to ~32M
to limit the number of objects under /O/[seq]/d[0..31]
dir on OSTs.
This makes the directories stay optimial for ldiskfs,
to avoid going into the largedir/3-level htree territory.

Remove the hard-coded LUSTRE_DATA_SEQ_MAX_WIDTH checks
in ofd, make them check the seq->lcs_width which is
a tunable set to LUSTRE_DATA_SEQ_MAX_WIDTH by default,
allow the value up to IDIF_MAX_OID if a larger seq width
is needed.

Use the odbo->o_size in the OST_CREATE rpc reply on ofd,
to update osp with the current seq width setting.
osp then uses this seq width to determine when to rollover
to a new seq.

The seq will rollover when the seq width is exhausted,
the default is LUSTRE_DATA_SEQ_MAX_WIDTH.
For seq >= FID_SEQ_NORMAL objects, the upper limit of
seq width is OBIF_MAX_OID,
For IDIF/MDT0 objects, the upper limit is IDIF_MAX_OID.
The seq FID_SEQ_OST_MDT0 will change to a normal seq after the
rollover.

Fix osp_precreate_reserve when the last precreated is the end
of the seq and the osp_objs_precreated can not host all
the requested objects, the mdt thread would stuck:
it wakes up osp precreate thread in a loop for progress,
but osp thread will not try to do anything until the seq
is used up. This can be seen easier when seq->lcs_width is
set to a low number and try to create an overstripe with stripe
number bigger than seq->lcs_width.

Fix the precreate thread spinning when the precreate pool
is at the end of the seq, and is nearly empty.

Change the seq->lcs_width to 16384 for all tests in
test-framework.sh, except a few slow tests to avoid timeouts,
and some overstriping tests creating LOV_MAX_STRIPE_COUNT to
avoid overstriping creating less objects than expected,
when precreate pool is at the end of the seq, and there are
not enough objects.

Fix the problem where seq could still change after
replay_barrier. To achieve this, introduce new fail_loc

lustre/ofd/ofd_fs.c
@@ -419,10 +419,17 @@ struct ofd_seq *ofd_seq_load(const struct lu_env *env, struct ofd_device *ofd,
 
 	if (info->fti_attr.la_size == 0) {
 		/* object is just created, initialize last id */
-		if (OBD_FAIL_CHECK(OBD_FAIL_OFD_SET_OID))
-			ofd_seq_last_oid_set(oseq, 0xffffff00);
-		else
+		if (OBD_FAIL_CHECK(OBD_FAIL_OFD_SET_OID)) {
+			struct seq_server_site *ss = &ofd->ofd_seq_site;
+			struct lu_client_seq *client_seq = ss->ss_client_seq;
+			__u64 seq_width = fid_seq_is_norm(seq) ?
+				min(OBIF_MAX_OID, client_seq->lcs_width) :
+				min(IDIF_MAX_OID, client_seq->lcs_width);
+
+			ofd_seq_last_oid_set(oseq, seq_width & ~0xffULL);
+		} else {
 			ofd_seq_last_oid_set(oseq, OFD_INIT_OBJID);
+		}
 		ofd_seq_last_oid_write(env, ofd, oseq);
 	} else if (info->fti_attr.la_size == sizeof(lastid)) {
 		info->fti_off = 0;

lustre/tests/cfg/local.sh
@@ -48,6 +48,7 @@ OSTCOUNT=${OSTCOUNT:-2}
 OSTDEVBASE=${OSTDEVBASE:-$TMP/${FSNAME}-ost}
 OSTSIZE=${OSTSIZE:-400000}
 OSTOPT=${OSTOPT:-}
+OSTSEQWIDTH=${OSTSEQWIDTH:-0x3fff}
 OST_FS_MKFS_OPTS=${OST_FS_MKFS_OPTS:-}
 OST_MOUNT_OPTS=${OST_MOUNT_OPTS:-}
 OST_MOUNT_FS_OPTS=${OST_MOUNT_FS_OPTS:-}

lustre/tests/replay-dual.sh
@@ -53,6 +53,8 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
 	do_facet $SINGLEMDS "sync; sleep 10; sync; sleep 10; sync"
 fi
 
+force_new_seq mds1
+
 LU482_FAILED=$(mktemp -u $TMP/$TESTSUITE.lu482.XXXXXX)
 test_0a() {
 	echo "Check file is LU482_FAILED=$LU482_FAILED"

lustre/tests/replay-single-lmv.sh
@@ -38,6 +38,8 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
     do_facet $SINGLEMDS sync
 fi
 
+force_new_seq mds1
+
 test_0() {
     replay_barrier mds1
     fail mds1

lustre/tests/replay-single.sh
@@ -40,6 +40,8 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
     do_facet $SINGLEMDS sync
 fi
 
+force_new_seq mds1
+
 test_0a() {	# was test_0
 	mkdir_on_mdt0 $DIR/$tdir || error "mkdir $DIR/$tdir failed"
 	replay_barrier $SINGLEMDS

lustre/tests/sanity-pfl.sh
@@ -40,6 +40,8 @@ check_runas_id $RUNAS_ID $RUNAS_GID $RUNAS
 assert_DIR
 rm -rf $DIR/[Rdfs][0-9]*
 
+force_new_seq mds1
+
 test_0a() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
 
@@ -71,6 +73,8 @@ test_0b() {
 		grep "connect_flags:.*overstriping") ]] ||
 		skip "server does not support overstriping"
 	large_xattr_enabled || skip_env "no large xattr support"
+
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
 
 	local comp_file=$DIR/$tdir/$tfile
 
@@ -114,6 +118,8 @@ test_0c() {
 		skip "skipped for lustre < $SEL_VER"
 
 	large_xattr_enabled || skip_env "no large xattr support"
+
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
 
 	local comp_file=$DIR/$tdir/$tfile
 
@@ -252,6 +258,8 @@ test_1c() {
 		grep "connect_flags:.*overstriping") ]] ||
 		skip "server does not support overstriping"
 	large_xattr_enabled || skip_env "no large xattr support"
+
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
 
 	local comp_file=$DIR/$tdir/$tfile
 	local rw_len=$((3 * 1024 * 1024))	# 3M

lustre/tests/sanity.sh
@@ -2512,6 +2512,8 @@ test_27Cd() {
 	[[ $OSTCOUNT -lt 2 ]] && skip_env "need > 1 OST"
 	large_xattr_enabled || skip_env "ea_inode feature disabled"
 
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+
 	test_mkdir -p $DIR/$tdir
 	local setcount=$LOV_MAX_STRIPE_COUNT
 
@@ -13474,6 +13476,8 @@ run_test 121 "read cancel race ========="
 test_123a_base() { # was test 123, statahead(bug 11401)
 	local lsx="$1"
 
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+
 	SLOWOK=0
 	if ! grep -q "processor.*: 1" /proc/cpuinfo; then
 		log "testing UP system. Performance may be lower than expected."
@@ -15149,6 +15153,8 @@ test_135() {
 	#set only one record at plain llog
 	do_facet $SINGLEMDS $LCTL set_param fail_loc=0x1319 fail_val=1
 
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+
 	#fill already existed plain llog each 64767
 	#wrapping whole catalog
 	createmany -o -u $DIR/$tdir/$tfile- $((64767 * 1))
@@ -15178,6 +15184,8 @@ test_136() {
 	#set only one record at plain llog
 #define OBD_FAIL_CATALOG_FULL_CHECK                0x131a
 	do_facet $SINGLEMDS $LCTL set_param fail_loc=0x131a fail_val=1
+
+	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
 
 	#fill already existed 2 plain llogs each 64767
 	#wrapping whole catalog

lustre/tests/test-framework.sh
@@ -36,6 +36,8 @@ export UMOUNT=${UMOUNT:-"umount -d"}
 export LSNAPSHOT_CONF="/etc/ldev.conf"
 export LSNAPSHOT_LOG="/var/log/lsnapshot.log"
 
+export DATA_SEQ_MAX_WIDTH=0x1ffffff
+
 # sles12 umount has a issue with -d option
 [ -e /etc/SuSE-release ] && grep -w VERSION /etc/SuSE-release | grep -wq 12 && {
 	export UMOUNT="umount"
@@ -2142,6 +2144,10 @@ mount_facet() {
 		do_facet ${facet} \
 			"mkdir -p $mntpt; $MOUNT_CMD $opts $dm_dev $mntpt"
 		RC=${PIPESTATUS[0]}
+		if [[ ${facet} =~ ost ]]; then
+			do_facet ${facet} "$LCTL set_param \
+				seq.cli-$(devicelabel $facet $dm_dev)-super.width=$OSTSEQWIDTH"
+		fi
 	fi
 
 	if [ $RC -ne 0 ]; then
@@ -7108,7 +7114,7 @@ ostname_from_index() {
 }
 
 mdtname_from_index() {
-	local uuid=$(mdtuuid_from_index $1)
+	local uuid=$(mdtuuid_from_index $1 $2)
 	echo ${uuid/_UUID/}
 }
 
@@ -11122,7 +11128,7 @@ consume_precreations() {
 	local extra=${4:-2}
 	local OST=$(ostname_from_index $OSTIDX $dir)
 
-	test_mkdir -p $dir/${OST}
+	mkdir_on_mdt -i $(facet_index $mfacet) $dir/${OST}
 	$LFS setstripe -i $OSTIDX -c 1 ${dir}/${OST}
 
 	# on the mdt's osc
@@ -11169,6 +11175,39 @@ exhaust_all_precreations() {
 		__exhaust_precreations $i $1 -1
 	done
 	sleep_maxage
+}
+
+force_new_seq() {
+	local mfacet=$1
+	local MDTIDX=$(facet_index $mfacet)
+	local MDT=$(mdtname_from_index $MDTIDX $DIR)
+	local i
+
+#define OBD_FAIL_OSP_FORCE_NEW_SEQ		0x210a
+	do_facet $mfacet $LCTL set_param fail_loc=0x210a
+	mkdir_on_mdt -i $MDTIDX $DIR/${MDT}
+	for (( i=0; i < OSTCOUNT; i++ )) ; do
+		# consume preallocated objects, to wake up precreate thread
+		consume_precreations $DIR/${MDT} $mfacet $i
+	done
+	do_facet $mfacet $LCTL set_param fail_loc=0
+	rm -rf $DIR/${MDT}
+}
+
+force_new_seq_all() {
+	local i
+	for (( i=0; i < MDSCOUNT; i++ )) ; do
+		force_new_seq mds$((i + 1))
+	done
+	sleep_maxage
+}
+
+ost_set_temp_seq_width_all() {
+	local osts=$(comma_list $(osts_nodes))
+	local width=$(do_facet ost1 $LCTL get_param -n seq.*OST0000-super.width)
+
+	do_nodes $osts $LCTL set_param seq.*OST*-super.width=$1
+	stack_trap "do_nodes $osts $LCTL set_param seq.*OST*-super.width=$width"
 }
 
 verify_yaml_available() {

commit 2fdb1f8d01b9f55f8270b48edc0e105e40d42f55

LU-11912 tests: SEQ rollover fixes
To avoid changeing SEQ after replay_barrier, we
use force_new_seq when starting the test suites heavily
using replay_barrier, e.g. replay-single.
However when there are fewer OSTs, the default 16384
SEQ width could not last the entire test suite, SEQ
rollover could still happen randomly after replay_barrier.

To overcome this, change the default OSTSEQWIDTH to
65536, and divide by number of OSTs, so the SEQ width is
larger with fewer OSTs. For 8 OSTs, the SEQ width is 16384,
and make sure we don't go under it.

Use force_new_seq_all for the test suites using replay_barrier
on MDTs other than mds1.

Add force_new_seq_all for replay-ost-single, which is using
replay_barrier on OST. If SEQ rollover happens after that,
the SEQ range update on ofd is lost due to replay_barrier,
the next time when we try to allocate a new SEQ will end up
with an old one.

Use force_new_seq_all for the test cases(namely sanity-pfl/0b
0c 1c 16b sanity/27Cd) checking for number of stripes created
with overstriping, to make sure we have enough objects
in the precreate pool.

Test-Parameters: trivial ostcount=4 testlist=replay-single
Test-Parameters: ostcount=2 testlist=replay-single
Test-Parameters: mdtcount=2 testlist=conf-sanity env=ONLY=122a,ONLY_REPEAT=10
Test-Parameters: testlist=sanity,sanity-pfl
Test-Parameters: testlist=sanity-scrub,replay-single,obdfilter-survey,replay-ost-single,large-scale
Fixes: 0ecb2a167c ("LU-11912 ofd: reduce LUSTRE_DATA_SEQ_MAX_WIDTH")
Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Change-Id: Ic65111199f042405d6db8acb729b2cddf91138af
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50478
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_fs.c
@@ -426,7 +426,8 @@ struct ofd_seq *ofd_seq_load(const struct lu_env *env, struct ofd_device *ofd,
 				min(OBIF_MAX_OID, client_seq->lcs_width) :
 				min(IDIF_MAX_OID, client_seq->lcs_width);
 
-			ofd_seq_last_oid_set(oseq, seq_width & ~0xffULL);
+			ofd_seq_last_oid_set(oseq, seq_width > 255 ?
+						seq_width - 255 : seq_width);
 		} else {
 			ofd_seq_last_oid_set(oseq, OFD_INIT_OBJID);
 		}

lustre/tests/cfg/local.sh
@@ -48,7 +48,7 @@ OSTCOUNT=${OSTCOUNT:-2}
 OSTDEVBASE=${OSTDEVBASE:-$TMP/${FSNAME}-ost}
 OSTSIZE=${OSTSIZE:-400000}
 OSTOPT=${OSTOPT:-}
-OSTSEQWIDTH=${OSTSEQWIDTH:-0x3fff}
+OSTSEQWIDTH=${OSTSEQWIDTH:-0x20000}
 OST_FS_MKFS_OPTS=${OST_FS_MKFS_OPTS:-}
 OST_MOUNT_OPTS=${OST_MOUNT_OPTS:-}
 OST_MOUNT_FS_OPTS=${OST_MOUNT_FS_OPTS:-}

lustre/tests/replay-dual.sh
@@ -53,7 +53,7 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
 	do_facet $SINGLEMDS "sync; sleep 10; sync; sleep 10; sync"
 fi
 
-force_new_seq mds1
+force_new_seq_all
 
 LU482_FAILED=$(mktemp -u $TMP/$TESTSUITE.lu482.XXXXXX)
 test_0a() {

lustre/tests/replay-ost-single.sh
@@ -33,6 +33,8 @@ mkdir_on_mdt0 $TDIR
 $LFS setstripe $TDIR -i 0 -c 1
 $LFS getstripe $TDIR
 
+force_new_seq_all
+
 test_0a() {
 	zconf_umount $(hostname) $MOUNT -f
 	# needs to run during initial client->OST connection

lustre/tests/replay-single-lmv.sh
@@ -38,7 +38,7 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
     do_facet $SINGLEMDS sync
 fi
 
-force_new_seq mds1
+force_new_seq_all
 
 test_0() {
     replay_barrier mds1

lustre/tests/replay-single.sh
@@ -43,7 +43,7 @@ if [ $LINUX_VERSION_CODE -lt $(version_code 2.6.33) ]; then
     do_facet $SINGLEMDS sync
 fi
 
-force_new_seq mds1
+force_new_seq_all
 
 test_0a() {	# was test_0
 	mkdir_on_mdt0 $DIR/$tdir || error "mkdir $DIR/$tdir failed"

lustre/tests/sanity-pfl.sh
@@ -74,7 +74,7 @@ test_0b() {
 		skip "server does not support overstriping"
 	large_xattr_enabled || skip_env "no large xattr support"
 
-	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+	force_new_seq_all
 
 	local comp_file=$DIR/$tdir/$tfile
 
@@ -119,7 +119,7 @@ test_0c() {
 
 	large_xattr_enabled || skip_env "no large xattr support"
 
-	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+	force_new_seq_all
 
 	local comp_file=$DIR/$tdir/$tfile
 
@@ -259,7 +259,7 @@ test_1c() {
 		skip "server does not support overstriping"
 	large_xattr_enabled || skip_env "no large xattr support"
 
-	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+	force_new_seq_all
 
 	local comp_file=$DIR/$tdir/$tfile
 	local rw_len=$((3 * 1024 * 1024))	# 3M
@@ -944,6 +944,8 @@ test_16b() {
 	[[ $OSTCOUNT -ge $(($LOV_MAX_STRIPE_COUNT / 2)) ]] &&
 		skip_env "too many osts, skipping"
 	large_xattr_enabled || skip_env "ea_inode feature disabled"
+
+	force_new_seq_all
 
 	local file=$DIR/$tdir/$tfile
 	local dir=$DIR/$tdir/dir

lustre/tests/sanity.sh
@@ -2517,7 +2517,7 @@ test_27Cd() {
 	[[ $OSTCOUNT -lt 2 ]] && skip_env "need > 1 OST"
 	large_xattr_enabled || skip_env "ea_inode feature disabled"
 
-	ost_set_temp_seq_width_all $DATA_SEQ_MAX_WIDTH
+	force_new_seq_all
 
 	test_mkdir -p $DIR/$tdir
 	local setcount=$LOV_MAX_STRIPE_COUNT

lustre/tests/test-framework.sh
@@ -2145,12 +2145,14 @@ mount_facet() {
 	if [ -f $TMP/test-lu482-trigger ]; then
 		RC=2
 	else
+		local seq_width=$(($OSTSEQWIDTH / $OSTCOUNT))
+		(( $seq_width >= 16384 )) || seq_width=16384
 		do_facet ${facet} \
 			"mkdir -p $mntpt; $MOUNT_CMD $opts $dm_dev $mntpt"
 		RC=${PIPESTATUS[0]}
 		if [[ ${facet} =~ ost ]]; then
 			do_facet ${facet} "$LCTL set_param \
-				seq.cli-$(devicelabel $facet $dm_dev)-super.width=$OSTSEQWIDTH"
+				seq.cli-$(devicelabel $facet $dm_dev)-super.width=$seq_width"
 		fi
 	fi
 