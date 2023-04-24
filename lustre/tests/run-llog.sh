14e05ecfa7ff8be474b48979496ab7e431478cec
Timothy Day <timday@amazon.com>
2023-04-24 00:56:48 +0000

    LU-16763 tests: add more logging to run-llog.sh
    Add more logging to run-llog.sh. At the same time, add SPDX text
    and fix some minor shellcheck warnings.
    
    Test-Parameters: trivial testlist=sanity env=ONLY=60a,ONLY_REPEAT=10
    Signed-off-by: Timothy Day <timday@amazon.com>
    Change-Id: I317fac7b872be53a1094022cfcd7d130b4c79c0a
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50719
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/run-llog.sh
+++ lustre/tests/run-llog.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
 
-LUSTRE=${LUSTRE:-$(dirname $0)/..}
-. $LUSTRE/tests/test-framework.sh
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+# lustre/tests/run-llog.sh
+#
+# Script to run the llog_test unit tests
+#
+
+LUSTRE=${LUSTRE:-$(dirname "$0")/..}
+. "$LUSTRE/tests/test-framework.sh"
 init_test_env "$@"
 
 TMP=${TMP:-/tmp}
@@ -11,10 +20,11 @@ MGS=$($LCTL dl | awk '/mgs/ { print $4 }')
 [ -z "$MGS" ] && echo "$0: SKIP: no MGS available, skipping llog test" && exit 0
 
 load_module obdclass/llog_test || exit 1
-$LCTL modules > $TMP/ogdb-$(hostname)
+$LCTL modules > "$TMP/ogdb-$(hostname)"
 echo "NOW reload debugging syms.."
 
 RC=0
+
 # Using ignore_errors will allow lctl to cleanup even if the test fails.
 eval "$LCTL <<-EOF || RC=2
 	attach llog_test llt_name llt_uuid
@@ -23,7 +33,10 @@ eval "$LCTL <<-EOF || RC=2
 	--device llt_name cleanup
 	--device llt_name detach
 EOF"
+
+$LCTL dl
+
 rmmod -v llog_test || RC2=3
-[ $RC -eq 0 -a "$RC2" ] && RC=$RC2
+[ $RC -eq 0 ] && [ "$RC2" ] && RC=$RC2
 
 exit $RC
