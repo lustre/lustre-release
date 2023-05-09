lustre/lustre-release

commit 5e48ffca322c3c72d3b83b0719f245fc6f13c8e4

LU-16524 nodemap: add rbac property to nodemap
Add new rbac property to nodemap. Internally this is a mask of allowed
roles. Externally it defaults to all, which means all roles are
allowed, and it can take the following values (multiple can be
specified, comma separated), with the semantic:
- byfid_ops, to allow operations by FID (e.g. 'lfs rmfid').
- chlg_ops, to allow access to Lustre Changelogs.
- dne_ops, to allow operations related to DNE (e.g. 'lfs mkdir').
- file_perms, to allow modifications of file permissions and owners.
- quota_ops, to allow quota modifications.
Apart from all, any role not explicitly specified is forbidden. And to
forbid all roles, use 'none' value.

Update lctl-nodemap-modify man page to mention this new property.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I4cedf03c75948f4b1e9b55292414ab9110701874
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49873
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_nodemap.h
@@ -36,31 +36,15 @@
 
 #define LUSTRE_NODEMAP_DEFAULT_ID 0
 
-/** enums containing the types of ids contained in a nodemap
- * kept so other modules (mgs, mdt, etc) can define the type
- * of search easily
- */
-
-enum nodemap_id_type {
-	NODEMAP_UID,
-	NODEMAP_GID,
-	NODEMAP_PROJID,
-};
-
-enum nodemap_tree_type {
-	NODEMAP_FS_TO_CLIENT,
-	NODEMAP_CLIENT_TO_FS,
-};
-
-enum nodemap_mapping_modes {
-	NODEMAP_MAP_BOTH_LEGACY	= 0x0,  /* for compatibility */
-	NODEMAP_MAP_UID		= 0x01,
-	NODEMAP_MAP_GID		= 0x02,
-	NODEMAP_MAP_BOTH	= 0x03, /* for compatibility */
-	NODEMAP_MAP_PROJID	= 0x04,
-	NODEMAP_MAP_ALL		= NODEMAP_MAP_UID |
-				  NODEMAP_MAP_GID |
-				  NODEMAP_MAP_PROJID,
+static const struct nodemap_rbac_name {
+	enum nodemap_rbac_roles nrn_mode;
+	const char	       *nrn_name;
+} nodemap_rbac_names[] = {
+	{ NODEMAP_RBAC_FILE_PERMS, "file_perms" },
+	{ NODEMAP_RBAC_DNE_OPS,    "dne_ops"    },
+	{ NODEMAP_RBAC_QUOTA_OPS,  "quota_ops"  },
+	{ NODEMAP_RBAC_BYFID_OPS,  "byfid_ops"  },
+	{ NODEMAP_RBAC_CHLG_OPS,   "chlg_ops"   },
 };
 
 struct nodemap_pde {
@@ -85,8 +69,9 @@ struct lu_nodemap {
 				 nmf_forbid_encryption:1,
 				 nmf_readonly_mount:1;
 	/* bitmap for mapping type */
-	enum nodemap_mapping_modes
-				nmf_map_mode;
+	enum nodemap_mapping_modes nmf_map_mode;
+	/* bitmap for rbac, enum nodemap_rbac_roles */
+	enum nodemap_rbac_roles	 nmf_rbac;
 	/* unique ID set by MGS */
 	unsigned int		 nm_id;
 	/* nodemap ref counter */
@@ -152,6 +137,7 @@ int nodemap_set_trust_client_ids(const char *name, bool trust_client_ids);
 int nodemap_set_deny_unknown(const char *name, bool deny_unknown);
 int nodemap_set_mapping_mode(const char *name,
 			     enum nodemap_mapping_modes map_mode);
+int nodemap_set_rbac(const char *name, enum nodemap_rbac_roles rbac);
 int nodemap_set_squash_uid(const char *name, uid_t uid);
 int nodemap_set_squash_gid(const char *name, gid_t gid);
 int nodemap_set_squash_projid(const char *name, projid_t projid);

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -242,6 +242,97 @@ struct lsd_reply_header {
 	__u8	lrh_pad[sizeof(struct lsd_reply_data_v1) - 12];
 };
 
+/****************** nodemap *********************/
+
+enum nodemap_idx_type {
+	NODEMAP_EMPTY_IDX = 0,		/* index created with blank record */
+	NODEMAP_CLUSTER_IDX = 1,	/* a nodemap cluster of nodes */
+	NODEMAP_RANGE_IDX = 2,		/* nid range assigned to a nm cluster */
+	NODEMAP_UIDMAP_IDX = 3,		/* uid map assigned to a nm cluster */
+	NODEMAP_GIDMAP_IDX = 4,		/* gid map assigned to a nm cluster */
+	NODEMAP_PROJIDMAP_IDX = 5,	/* projid map assigned to nm cluster */
+	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
+};
+
+/* Nodemap records, uses 32 byte record length.
+ * New nodemap config records can be added into NODEMAP_CLUSTER_IDX
+ * with a new nk_cluster_subid value, as long as the records are
+ * kept at 32 bytes in size.  New global config records can be added
+ * into NODEMAP_GLOBAL_IDX with a new nk_global_subid.  This avoids
+ * breaking compatibility.  Do not change the record size.  If a
+ * new ID type or range is needed, a new IDX type should be used.
+ */
+struct nodemap_cluster_rec {
+	char			ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
+	enum nm_flag_bits	ncr_flags:8;
+	enum nm_flag2_bits	ncr_flags2:8;
+	__u8			ncr_padding1;
+	__u32			ncr_squash_projid;
+	__u32			ncr_squash_uid;
+	__u32			ncr_squash_gid;
+};
+
+/* lnet_nid_t is 8 bytes */
+struct nodemap_range_rec {
+	lnet_nid_t	nrr_start_nid;
+	lnet_nid_t	nrr_end_nid;
+	__u64		nrr_padding1;
+	__u64		nrr_padding2;
+};
+
+struct nodemap_id_rec {
+	__u32	nir_id_fs;
+	__u32	nir_padding1;
+	__u64	nir_padding2;
+	__u64	nir_padding3;
+	__u64	nir_padding4;
+};
+
+struct nodemap_global_rec {
+	__u8	ngr_is_active;
+	__u8	ngr_padding1;
+	__u16	ngr_padding2;
+	__u32	ngr_padding3;
+	__u64	ngr_padding4;
+	__u64	ngr_padding5;
+	__u64	ngr_padding6;
+};
+
+struct nodemap_cluster_roles_rec {
+	__u64 ncrr_roles;	/* enum nodemap_rbac_roles */
+	__u64 ncrr_unused1;
+	__u64 ncrr_unused2;
+	__u64 ncrr_unused3;
+};
+
+union nodemap_rec {
+	struct nodemap_cluster_rec ncr;
+	struct nodemap_range_rec nrr;
+	struct nodemap_id_rec nir;
+	struct nodemap_global_rec ngr;
+	struct nodemap_cluster_roles_rec ncrr;
+};
+
+/* sub-keys for records of type NODEMAP_CLUSTER_IDX */
+enum nodemap_cluster_rec_subid {
+	NODEMAP_CLUSTER_REC = 0,   /* nodemap_cluster_rec */
+	NODEMAP_CLUSTER_ROLES = 1, /* nodemap_cluster_roles_rec */
+};
+
+/* first 4 bits of the nodemap_id is the index type */
+struct nodemap_key {
+	__u32 nk_nodemap_id;
+	union {
+		__u32 nk_cluster_subid;
+		__u32 nk_range_id;
+		__u32 nk_id_client;
+		__u32 nk_unused;
+	};
+};
+
+#define NM_TYPE_MASK 0x0FFFFFFF
+#define NM_TYPE_SHIFT 28
+
 /** @} disk */
 
 #endif /* _UAPI_LUSTRE_DISK_H */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3670,6 +3670,8 @@ struct llog_update_record {
 				     SELINUX_POLICY_VER_LEN + \
 				     SELINUX_POLICY_HASH_LEN + 3)
 
+#define LUSTRE_NODEMAP_NAME_LENGTH 16
+
 /* lu_nodemap flags */
 enum nm_flag_bits {
 	NM_FL_ALLOW_ROOT_ACCESS = 0x1,
@@ -3685,49 +3687,45 @@ enum nm_flag2_bits {
 	NM_FL2_READONLY_MOUNT = 0x1,
 };
 
-/* nodemap records, uses 32 byte record length */
-#define LUSTRE_NODEMAP_NAME_LENGTH 16
-struct nodemap_cluster_rec {
-	char			ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
-	enum nm_flag_bits	ncr_flags:8;
-	enum nm_flag2_bits	ncr_flags2:8;
-	__u8			ncr_padding1;
-	__u32			ncr_squash_projid;
-	__u32			ncr_squash_uid;
-	__u32			ncr_squash_gid;
+/** enums containing the types of ids contained in a nodemap
+ * kept so other modules (mgs, mdt, etc) can define the type
+ * of search easily
+ */
+
+enum nodemap_id_type {
+	NODEMAP_UID,
+	NODEMAP_GID,
+	NODEMAP_PROJID,
 };
 
-/* lnet_nid_t is 8 bytes */
-struct nodemap_range_rec {
-	lnet_nid_t	nrr_start_nid;
-	lnet_nid_t	nrr_end_nid;
-	__u64		nrr_padding1;
-	__u64		nrr_padding2;
+enum nodemap_tree_type {
+	NODEMAP_FS_TO_CLIENT,
+	NODEMAP_CLIENT_TO_FS,
 };
 
-struct nodemap_id_rec {
-	__u32	nir_id_fs;
-	__u32	nir_padding1;
-	__u64	nir_padding2;
-	__u64	nir_padding3;
-	__u64	nir_padding4;
+enum nodemap_mapping_modes {
+	NODEMAP_MAP_BOTH_LEGACY	= 0x0,  /* for compatibility */
+	NODEMAP_MAP_UID		= 0x01,
+	NODEMAP_MAP_GID		= 0x02,
+	NODEMAP_MAP_BOTH	= 0x03, /* for compatibility */
+	NODEMAP_MAP_PROJID	= 0x04,
+	NODEMAP_MAP_ALL		= NODEMAP_MAP_UID |
+				  NODEMAP_MAP_GID |
+				  NODEMAP_MAP_PROJID,
 };
 
-struct nodemap_global_rec {
-	__u8	ngr_is_active;
-	__u8	ngr_padding1;
-	__u16	ngr_padding2;
-	__u32	ngr_padding3;
-	__u64	ngr_padding4;
-	__u64	ngr_padding5;
-	__u64	ngr_padding6;
-};
-
-union nodemap_rec {
-	struct nodemap_cluster_rec ncr;
-	struct nodemap_range_rec nrr;
-	struct nodemap_id_rec nir;
-	struct nodemap_global_rec ngr;
+enum nodemap_rbac_roles {
+	NODEMAP_RBAC_FILE_PERMS	= 0x00000001,
+	NODEMAP_RBAC_DNE_OPS	= 0x00000002,
+	NODEMAP_RBAC_QUOTA_OPS	= 0x00000004,
+	NODEMAP_RBAC_BYFID_OPS	= 0x00000008,
+	NODEMAP_RBAC_CHLG_OPS	= 0x00000010,
+	NODEMAP_RBAC_NONE	= (__u32)~(NODEMAP_RBAC_FILE_PERMS |
+					   NODEMAP_RBAC_DNE_OPS    |
+					   NODEMAP_RBAC_QUOTA_OPS  |
+					   NODEMAP_RBAC_BYFID_OPS  |
+					   NODEMAP_RBAC_CHLG_OPS),
+	NODEMAP_RBAC_ALL	= 0xFFFFFFFF, /* future caps ON by default */
 };
 
 /*

commit 5a6725d19d4037026d7cab2442b0c639d1511e5d

LU-16739 uapi: make lustre_disk.h buildable in user land
The rbac work introduced a regression that makes lustre_disk.h
UAPI header no longer buildable in user land. This is causing
sanity test 400b to fail with:

lustre_disk.h:266:18: error: 'LUSTRE_NODEMAP_NAME_LENGTH' undeclared here (not in a function)
  char   ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
                  ^~~~~~~~~~~~~~~~~~~~~~~~~~
lustre_disk.h:267:20: error: 'ncr_flags' is narrower than values of its type [-Werror]
  enum nm_flag_bits ncr_flags:8;
                    ^~~~~~~~~
lustre_disk.h:267:20: error: field 'ncr_flags' has incomplete type
lustre_disk.h:268:21: error: 'ncr_flags2' is narrower than values of its type [-Werror]
  enum nm_flag2_bits ncr_flags2:8;
                     ^~~~~~~~~~
lustre_disk.h:268:21: error: field 'ncr_flags2' has incomplete type
lustre_disk.h:277:2: error: unknown type name 'lnet_nid_t'
  lnet_nid_t nrr_start_nid;
  ^~~~~~~~~~
lustre/lustre_disk.h:278:2: error: unknown type name 'lnet_nid_t'
  lnet_nid_t nrr_end_nid;
  ^~~~~~~~~~

To fix this move several pieces of nodemap handling from lustre_idl.h
to lustre_disk.h.

The git commit 5e6a51787fef20b849682d8c49ec9c2beed5c373 for Linux
kernel version 6.2.0-rc5 made guid_t only available for kernel code.
The only UAPI data structure left is uuid_le. Thankfully MCE requires
this otherwise even uuid_le would be removed. We will need to keep
an eye on this.

Test-Parameters: trivial testlist=sanity envdefinitions=ONLY=400b
Fixes: 5e48ffca322 ("LU-16524 nodemap: add rbac property to nodemap")
Change-Id: I4b962572ec2bf76159a17807c564390ded00d630
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50641
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/include/lustre_nodemap.h
@@ -30,7 +30,7 @@
 #ifndef _LUSTRE_NODEMAP_H
 #define _LUSTRE_NODEMAP_H
 
-#include <uapi/linux/lustre/lustre_idl.h>
+#include <uapi/linux/lustre/lustre_disk.h>
 
 #define LUSTRE_NODEMAP_NAME "nodemap"
 

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -42,6 +42,7 @@
  */
 #include <linux/types.h>
 #include <linux/uuid.h>
+#include <linux/lnet/lnet-types.h> /* for lnet_nid_t */
 
 /****************** on-disk files ********************/
 
@@ -254,6 +255,24 @@ enum nodemap_idx_type {
 	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
 };
 
+#define LUSTRE_NODEMAP_NAME_LENGTH	16
+
+/* lu_nodemap flags */
+enum nm_flag_bits {
+	NM_FL_ALLOW_ROOT_ACCESS = 0x1,
+	NM_FL_TRUST_CLIENT_IDS = 0x2,
+	NM_FL_DENY_UNKNOWN = 0x4,
+	NM_FL_MAP_UID = 0x8,
+	NM_FL_MAP_GID = 0x10,
+	NM_FL_ENABLE_AUDIT = 0x20,
+	NM_FL_FORBID_ENCRYPT = 0x40,
+	NM_FL_MAP_PROJID = 0x80,
+};
+
+enum nm_flag2_bits {
+	NM_FL2_READONLY_MOUNT = 0x1,
+};
+
 /* Nodemap records, uses 32 byte record length.
  * New nodemap config records can be added into NODEMAP_CLUSTER_IDX
  * with a new nk_cluster_subid value, as long as the records are
@@ -388,7 +407,7 @@ enum scrub_param {
 };
 
 struct scrub_file {
-	guid_t	sf_uuid;		    /* 128-bit uuid for volume */
+	uuid_le	sf_uuid;		    /* 128-bit uuid for volume */
 	__u64	sf_flags;		    /* see 'enum scrub_flags' */
 	__u32	sf_magic;		    /* SCRUB_MAGIC_V1/V2 */
 	__u16	sf_status;		    /* see 'enum scrub_status' */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3674,23 +3674,6 @@ struct llog_update_record {
 				     SELINUX_POLICY_VER_LEN + \
 				     SELINUX_POLICY_HASH_LEN + 3)
 
-#define LUSTRE_NODEMAP_NAME_LENGTH 16
-
-/* lu_nodemap flags */
-enum nm_flag_bits {
-	NM_FL_ALLOW_ROOT_ACCESS = 0x1,
-	NM_FL_TRUST_CLIENT_IDS = 0x2,
-	NM_FL_DENY_UNKNOWN = 0x4,
-	NM_FL_MAP_UID = 0x8,
-	NM_FL_MAP_GID = 0x10,
-	NM_FL_ENABLE_AUDIT = 0x20,
-	NM_FL_FORBID_ENCRYPT = 0x40,
-	NM_FL_MAP_PROJID = 0x80,
-};
-enum nm_flag2_bits {
-	NM_FL2_READONLY_MOUNT = 0x1,
-};
-
 /** enums containing the types of ids contained in a nodemap
  * kept so other modules (mgs, mdt, etc) can define the type
  * of search easily

lustre/ptlrpc/nodemap_storage.c
@@ -52,6 +52,7 @@
 #include <linux/types.h>
 #include <uapi/linux/lnet/lnet-types.h>
 #include <uapi/linux/lustre/lustre_idl.h>
+#include <uapi/linux/lustre/lustre_disk.h>
 #include <dt_object.h>
 #include <lu_object.h>
 #include <lustre_net.h>

lustre/utils/gss/sk_utils.h
@@ -34,6 +34,7 @@
 #include <keyutils.h>
 #endif
 #include <linux/lustre/lustre_idl.h>
+#include <linux/lustre/lustre_disk.h>
 #include <openssl/dh.h>
 #include <openssl/evp.h>
 #include <openssl/hmac.h>