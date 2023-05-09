lustre/lustre-release

commit 6b8e97b76c472068e7d6bc792e4f202b2f70ca67

LU-10378 utils: add formatted printf to lfs find
Introduce new --printf option with lfs find utility along with support for
the backslash escapes and format directives given below that allow users to
obtain metadata in formatted style.

List of backslash escapes supported by --printf option:
-------------------------------------
   Description               | Escape
-------------------------------------
   Newline character         | \n
   Tab character             | \t
   Literal backslash         | \\

List of format directives used with --printf option:
----------------------------------------------------------
   Description                                  | Directive
----------------------------------------------------------
   Literal % character                          | %%
   Access time (in ctime format)                | %a
   Access time (in secs since epoch)            | %A@
   File size (in 512B blocks)                   | %b
   Last change time (in ctime format)           | %c
   Last change time (in secs since epoch)       | %C@
   Numeric group ID of file/dir owner           | %G
   File size (in 1K blocks)                     | %k
   File mode (octal)                            | %m
   Path name of file                            | %p
   File size (in bytes)                         | %s
   Modification time (in ctime format)          | %t
   Modification time (in secs since epoch)      | %T@
   Numeric user ID of file/dir owner            | %U
   Birth time (in ctime format)                 | %w
   Birth time (in secs since epoch)             | %W@
   File type					| %y
   Stripe count                                 | %Lc
   Lustre FID					| %LF
   Directory hash type                          | %Lh
   Starting OST (file) or MDT (dir) index       | %Li
   List of all OST (file) or MDT (dir) indices  | %Lo
   OST pool name                                | %Lp
   Numeric project id assigned to file/dir      | %LP

lustre/utils/lfs.c
@@ -416,7 +416,7 @@ command_t cmdlist[] = {
 	 "     [[!] --mirror-state <[^]state>]\n"
 	 "     [[!] --name|-n <pattern>] [[!] --newer[XY] <reference>]\n"
 	 "     [[!] --ost|-O <uuid|index,...>] [[!] --perm [/-]mode]\n"
-	 "     [[!] --pool <pool>] [--print|-P] [--print0|-0]\n"
+	 "     [[!] --pool <pool>] [--print|-P] [--print0|-0] [--printf <format>]\n"
 	 "     [[!] --projid <projid>] [[!] --size|-s [+-]N[bkMGTPE]]\n"
 	 "     [[!] --stripe-count|-c [+-]<stripes>]\n"
 	 "     [[!] --stripe-index|-i <index,...>]\n"
@@ -3349,6 +3349,7 @@ enum {
 	LFS_NEWERXY_OPT,
 	LFS_INHERIT_RR_OPT,
 	LFS_FIND_PERM,
+	LFS_PRINTF_OPT,
 };
 
 #ifndef LCME_USER_MIRROR_FLAGS
@@ -4915,6 +4916,8 @@ static int lfs_find(int argc, char **argv)
 			.name = "pool",		.has_arg = required_argument },
 	{ .val = '0',	.name = "print0",	.has_arg = no_argument },
 	{ .val = 'P',	.name = "print",	.has_arg = no_argument },
+	{ .val = LFS_PRINTF_OPT,
+			.name = "printf",       .has_arg = required_argument },
 	{ .val = LFS_PROJID_OPT,
 			.name = "projid",	.has_arg = required_argument },
 /* getstripe { .val = 'q', .name = "quiet",	.has_arg = no_argument }, */
@@ -4927,11 +4930,10 @@ static int lfs_find(int argc, char **argv)
 	{ .val = 'T',	.name = "mdt-count",	.has_arg = required_argument },
 	{ .val = 'u',	.name = "uid",		.has_arg = required_argument },
 	{ .val = 'U',	.name = "user",		.has_arg = required_argument },
+/* getstripe { .val = 'v', .name = "verbose",	.has_arg = no_argument }, */
 	{ .val = 'z',	.name = "extension-size",
 						.has_arg = required_argument },
 	{ .val = 'z',	.name = "ext-size",	.has_arg = required_argument },
-/* getstripe { .val = 'v', .name = "verbose",	.has_arg = no_argument }, */
-/* getstripe { .val = 'y', .name = "yaml",	.has_arg = no_argument }, */
 	{ .name = NULL } };
 	int optidx = 0;
 	int pathstart = -1;
@@ -4947,7 +4949,7 @@ static int lfs_find(int argc, char **argv)
 
 	/* when getopt_long_only() hits '!' it returns 1, puts "!" in optarg */
 	while ((c = getopt_long_only(argc, argv,
-		"-0A:b:B:c:C:D:E:g:G:hH:i:L:m:M:n:N:O:Ppqrs:S:t:T:u:U:vz:",
+		"-0A:b:B:c:C:D:E:g:G:hH:i:L:m:M:n:N:O:Ppqrs:S:t:T:u:U:z:",
 		long_opts, &optidx)) >= 0) {
 		xtime = NULL;
 		xsign = NULL;
@@ -5537,6 +5539,9 @@ err_free:
 			break;
 		case 'P': /* we always print, this option is a no-op */
 			break;
+		case LFS_PRINTF_OPT:
+			param.fp_format_printf_str = optarg;
+			break;
 		case LFS_PROJID_OPT:
 			rc = name2projid(&param.fp_projid, optarg);
 			if (rc) {
@@ -5691,6 +5696,8 @@ err_free:
 			goto err;
 		}
 	}
+	if (!param.fp_verbose)
+		param.fp_verbose = VERBOSE_DEFAULT;
 
 	if (pathstart == -1) {
 		fprintf(stderr, "error: %s: no filename|pathname\n",

lustre/doc/lfs-find.1
@@ -31,6 +31,7 @@ lfs-find \- Lustre client utility to list files with specific attributes
 [[\fB!\fR] \fB--pool\fR \fIPOOL\fR]
 [\fB--print\fR|\fB-P\fR]
       [\fB--print0\fR|\fB-0\fR]
+[\fB--printf\fR \fIFORMAT\fR]
 [[\fB!\fR] \fB--projid\fR \fIPROJID\fR]
       [[\fB!\fR] \fB--size|\fB-s\fR [\fB-+\fR]\fIn\fR[\fBKMGTPE\fR]]
 [[\fB!\fR] \fB--stripe-count|\fB-c\fR [\fB+-\fR]\fIn\fR]
@@ -290,6 +291,105 @@ parameters, followed by a NUL character.  This is for use together with
 with the
 .B -0
 option to handle filenames with embedded spaces or other special characters.
+.TP
+.BR "--printf \fIformat\fR"
+Print \fIformat\fR to standard output for each matching file, interpreting
+`\' escapes and `%' directives.  Unlike \fB--print\fR, the \fB--printf\fR option
+does not automatically add a newline to the end of the string. The escapes and
+directives are:
+.RS 1.2i
+.TP
+.B \en
+Newline.
+.TP
+.B \et
+Horizontal tab.
+.TP
+.B \e\e
+A literal backslash.
+.TP
+.B %%
+A literal percent sign.
+.TP
+.B %a
+File\'s last access time in the format returned by the C \`ctime\' function.
+.TP
+.B %A@
+File\'s last access time in seconds since Jan. 1, 1970, 00:00 GMT.
+.TP
+.B %b
+The amount of disk space used for the file (in 512-byte blocks).
+.TP
+.B %c
+File\'s last status change time in the format returned by the C \`ctime\' function.
+.TP
+.B %C@
+File\'s last status change time in seconds since Jan. 1, 1970, 00:00 GMT.
+.TP
+.B %G
+File\'s numeric group ID.
+.TP
+.B %m
+File permission bits (in octal).
+.TP
+.B %p
+File's name.
+.TP
+.B %s
+File size in bytes.
+.TP
+.B %t
+File\'s last modification time in the format returned by the C \`ctime\' function.
+.TP
+.B %T@
+File\'s last modification time in seconds since Jan. 1, 1970, 00:00 GMT.
+.TP
+.B %U
+File's numeric user ID.
+.TP
+.B %w
+File\'s birth time in the format returned by the C \`ctime\' function.
+.TP
+.B %W@
+File\'s birth time in seconds since Jan. 1, 1970, 00:00 GMT.
+.TP
+.B %y
+File's type (f=file, d=directory, p=pipe, b=block device, c=character device,
+s=socket l=symbolic link)
+.TP
+Lustre-specific information about a file can be printed using these directives:
+.TP
+.B %Lc
+File\'s stripe count.  For a composite file, this is the stripe count of the last
+instantiated component.
+.TP
+.B %LF
+File Identifier (FID) associated with the file.
+.TP
+.B %Lh
+Directory's hash type (or \`none\' for an unstriped directory).
+.TP
+.B %Li
+File\'s starting OST index (or starting MDT index for a directory).
+For a composite file, this is the starting OST index of the last instantiated
+component.
+.TP
+.B %Lo
+List of all OST/MDT indices associated with a file or directory.
+.TP
+.B %Lp
+File\'s OST pool name.  For a composite file, this is the pool associated
+with the last instantiated component.  (NOTE: This can also be used for
+directories, but since MDT pools are not currently implemented, nothing will
+be printed.)
+.TP
+.B %LP
+Numeric project ID assigned to the file or directory.
+.TP
+.B %LS
+File's stripe size.  For a composite file, this is the stripe size of the last
+instantiated component.
+.RE
 .TP
 .BR --projid
 File has specified numeric project ID.

lustre/include/lustre/lustreapi.h
@@ -384,7 +384,7 @@ struct find_param {
 	unsigned long long	 fp_comp_end;
 	unsigned long long	 fp_comp_end_units;
 	unsigned long long	 fp_mdt_count;
-	unsigned		 fp_projid;
+	unsigned int		 fp_projid;
 	unsigned long long	 fp_blocks;
 	unsigned long long	 fp_blocks_units;
 
@@ -407,6 +407,8 @@ struct find_param {
 	int			 fp_bsign;
 	unsigned int		 fp_hash_inflags;
 	unsigned int		 fp_hash_exflags;
+	/* Print all information (lfs find only) */
+	char			 *fp_format_printf_str;
 };
 
 int llapi_ostlist(char *path, struct find_param *param);
@@ -546,7 +548,8 @@ int llapi_swap_layouts(const char *path1, const char *path2, __u64 dv1,
 
 /* Records received are in extended format now, though most of them are still
  * written in disk in changelog_rec format (to save space and time), it's
- * converted to extended format in the lustre api to ease changelog analysis. */
+ * converted to extended format in the lustre api to ease changelog analysis.
+ */
 #define HAVE_CHANGELOG_EXTEND_REC 1
 
 int llapi_changelog_start(void **priv, enum changelog_send_flag flags,
@@ -822,8 +825,8 @@ int llapi_layout_merge(struct llapi_layout **dst_layout,
 #define LLAPI_LAYOUT_OVERSTRIPING	4ULL
 
 /**
-* The layout includes a specific set of OSTs on which to allocate.
-*/
+ * The layout includes a specific set of OSTs on which to allocate.
+ */
 #define LLAPI_LAYOUT_SPECIFIC	0x2000000000000000ULL
 
 /**

lustre/tests/sanity.sh
@@ -8184,6 +8184,47 @@ test_56da() { # LU-14179
 }
 run_test 56da "test lfs find with long paths"
 
+test_56ea() { #LU-10378
+	local path=$DIR/$tdir
+	local pool=$TESTNAME
+
+	# Create ost pool
+	pool_add $pool || error "pool_add $pool failed"
+	pool_add_targets $pool 0 $((OSTCOUNT - 1)) 1 ||
+		error "adding targets to $pool failed"
+
+	# Set default pool on directory before creating file
+	mkdir $path || error "mkdir $path failed"
+	$LFS setstripe -p $pool $path ||
+		error "set OST pool on $pool failed"
+	touch $path/$tfile || error "touch $path/$tfile failed"
+
+	# Compare basic file attributes from -printf and stat
+	local attr_printf=$($LFS find $path/$tfile -printf "%A@ %T@ %C@ %U %G")
+	local attr_stat=$(stat -c "%X %Y %Z %u %g" $path/$tfile)
+
+	[[ "${attr_printf}" == "${attr_stat}" ]] ||
+		error "Attrs from lfs find and stat don't match"
+
+	# Compare Lustre attributes from lfs find and lfs getstripe
+	local lattr_printf=$($LFS find $path/$tfile -printf "%Lc %LS %Li %Lp")
+	local str_cnt=$($LFS getstripe --stripe-count $path/$tfile)
+	local str_size=$($LFS getstripe --stripe-size $path/$tfile)
+	local str_idx=$($LFS getstripe --stripe-index $path/$tfile)
+	local fpool=$($LFS getstripe --pool $path/$tfile)
+	local lattr_getstr="${str_cnt} ${str_size} ${str_idx} ${fpool}"
+
+	[[ "${lattr_printf}" == "${lattr_getstr}" ]] ||
+		error "Attrs from lfs find and lfs getstripe don't match"
+
+	# Verify behavior for unknown escape/format sequences
+	local esc_printf=$($LFS find $path/$tfile -printf '\\ %% \Q %Q')
+
+	[[ "${esc_printf}" == '\ % \Q %Q' ]] ||
+		error "Escape/format codes don't match"
+}
+run_test 56ea "test lfs find -printf option"
+
 test_57a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 	# note test will not do anything if MDS is not local

commit 75db98cef3df8f9a0e1b6e7a5150f3c332e6167b

LU-16768 lfs: copy optarg string other than using it directly
Copy optarg string for fp_format_printf_str lest it be messed
later.

Fixes: 6b8e97b76c ("LU-10378 utils: add formatted printf to lfs find")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Ib32883d3261ae921adf0fdd7b05bcbf728de7557
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50733
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Rick Mohr <mohrrf@ornl.gov>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -5777,7 +5777,7 @@ err_free:
 		case 'P': /* we always print, this option is a no-op */
 			break;
 		case LFS_PRINTF_OPT:
-			param.fp_format_printf_str = optarg;
+			param.fp_format_printf_str = strdup(optarg);
 			break;
 		case LFS_PROJID_OPT:
 			rc = name2projid(&param.fp_projid, optarg);
@@ -5964,6 +5964,9 @@ err:
 
 	if (param.fp_mdt_uuid && param.fp_num_alloc_mdts)
 		free(param.fp_mdt_uuid);
+
+	if (param.fp_format_printf_str)
+		free(param.fp_format_printf_str);
 
 	return ret;
 }