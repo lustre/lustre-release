63b4c244e8481b4113bb7bd1230bdbfeb6b34973
Patrick Farrell <pfarrell@whamcloud.com>
2023-05-09 14:48:03 -0400

    LU-16649 llite: EIO is possible on a race with page reclaim
    We must clear the 'uptodate' page flag when we delete a
    page from Lustre, or stale reads can occur.  However,
    generic_file_buffered_read requires any pages returned from
    readpage() be uptodate.
    
    So, we must retry reading if page truncation happens in
    parallel with the read.
    
    This implements the same fix as:
    https://review.whamcloud.com/49647
    b4da788a819f82d35b685d6ee7f02809c05ca005
    
    did for the mmap path.
    
    Lustre-change: https://review.whamcloud.com/50344
    Lustre-commit: 1d98e5c32b41e19bb1247958e666bb66e69dbc4c
    
    Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
    Change-Id: Iae0d1eb343f25a0176135347e54c309056c2613a
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50600
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Qian Yingjin <qian@ddn.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/llite/rw26.c
+++ lustre/llite/rw26.c
@@ -172,21 +172,23 @@ static int ll_releasepage(struct page *vmpage, RELEASEPAGE_ARG_TYPE gfp_mask)
 	struct address_space	*mapping;
 	int result = 0;
 
+	ENTRY;
+
 	LASSERT(PageLocked(vmpage));
 	if (PageWriteback(vmpage) || PageDirty(vmpage))
-		return 0;
+		RETURN(0);
 
 	mapping = vmpage->mapping;
 	if (mapping == NULL)
-		return 1;
+		RETURN(1);
 
 	obj = ll_i2info(mapping->host)->lli_clob;
 	if (obj == NULL)
-		return 1;
+		RETURN(1);
 
 	page = cl_vmpage_page(vmpage, obj);
 	if (page == NULL)
-		return 1;
+		RETURN(1);
 
 	env = cl_env_percpu_get();
 	LASSERT(!IS_ERR(env));
@@ -213,7 +215,7 @@ static int ll_releasepage(struct page *vmpage, RELEASEPAGE_ARG_TYPE gfp_mask)
 	cl_page_put(env, page);
 
 	cl_env_percpu_put(env);
-	return result;
+	RETURN(result);
 }
 
 static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
