lustre/lustre-release

commit b1e7be00cb6e1e7b3e35877cf8b8a209e456a2ea

LU-10472 osc: add T10PI support for RPC checksum
T10 Protection Information (T10 PI), previously known as Data
Integrity Field (DIF), is a standard for end-to-end data integrity
validation. T10 PI prevents silent data corruption, ensuring that
incomplete and incorrect data cannot overwrite good data.

Lustre file system already supports RPC level checksum which
validates the data in bulk RPCs when writing/reading data to/from
objects on OSTs. RPC level checksum can detect data corruption that
happens during RPC being transferred over the wire. However, it is
not capable to prevent silent data corruption happening in other
conditions, for example, memory corruption when data is cached in
page cache. And by using the existing checksum mechanism, only
disjoint protection coverage is provided. Thus, in order to provide
end-to-end data protection, T10PI support for Lustre should be added.

In order to provide end-to-end data integrity validation, the T10 PI
checksum of data in a sector need to be calculated on Lustre client
side and validated later on the Lustre OSS side. The T10 protection
information should be sent together with the data in the RPC.
However, in order to avoid significant performance degradation,
instead of sending all original guard tags for all sectors in a bulk
RPC, the existing checksum feature of bulk RPC will be integrated
together with the new T10PI feature.

When OST starts, necessary T10PI information will be extracted from
storage, i.e. the T10PI DIF type and sector size. The DIF type could
be one of TYPE1_IP, TYPE1_CRC, TYPE3_IP and TYPE3_CRC. And sector
size could be either 512 or 4K bytes.

When an OSC is connecting to OST, OSC and OST will negotiate about
the checksum types. New checksum types are added for T10PI support
including OBD_CKSUM_T10IP512, OBD_CKSUM_T10IP4K, OBD_CKSUM_T10CRC512,
and OBD_CKSUM_T10CRC4K. If the OST storage has T10PI suppoort, the
only selectable T10PI checksum type would have the same type with the

lustre/osc/osc_request.c
@@ -1033,6 +1033,103 @@ static inline int can_merge_pages(struct brw_page *p1, struct brw_page *p2)
         return (p1->off + p1->count == p2->off);
 }
 
+static int osc_checksum_bulk_t10pi(const char *obd_name, int nob,
+				   size_t pg_count, struct brw_page **pga,
+				   int opc, obd_dif_csum_fn *fn,
+				   int sector_size,
+				   u32 *check_sum)
+{
+	struct cfs_crypto_hash_desc *hdesc;
+	/* Used Adler as the default checksum type on top of DIF tags */
+	unsigned char cfs_alg = cksum_obd2cfs(OBD_CKSUM_T10_TOP);
+	struct page *__page;
+	unsigned char *buffer;
+	__u16 *guard_start;
+	unsigned int bufsize;
+	int guard_number;
+	int used_number = 0;
+	int used;
+	u32 cksum;
+	int rc = 0;
+	int i = 0;
+
+	LASSERT(pg_count > 0);
+
+	__page = alloc_page(GFP_KERNEL);
+	if (__page == NULL)
+		return -ENOMEM;
+
+	hdesc = cfs_crypto_hash_init(cfs_alg, NULL, 0);
+	if (IS_ERR(hdesc)) {
+		rc = PTR_ERR(hdesc);
+		CERROR("%s: unable to initialize checksum hash %s: rc = %d\n",
+		       obd_name, cfs_crypto_hash_name(cfs_alg), rc);
+		GOTO(out, rc);
+	}
+
+	buffer = kmap(__page);
+	guard_start = (__u16 *)buffer;
+	guard_number = PAGE_SIZE / sizeof(*guard_start);
+	while (nob > 0 && pg_count > 0) {
+		unsigned int count = pga[i]->count > nob ? nob : pga[i]->count;
+
+		/* corrupt the data before we compute the checksum, to
+		 * simulate an OST->client data error */
+		if (unlikely(i == 0 && opc == OST_READ &&
+			     OBD_FAIL_CHECK(OBD_FAIL_OSC_CHECKSUM_RECEIVE))) {
+			unsigned char *ptr = kmap(pga[i]->pg);
+			int off = pga[i]->off & ~PAGE_MASK;
+
+			memcpy(ptr + off, "bad1", min_t(typeof(nob), 4, nob));
+			kunmap(pga[i]->pg);
+		}
+
+		/*
+		 * The left guard number should be able to hold checksums of a
+		 * whole page
+		 */
+		rc = obd_page_dif_generate_buffer(obd_name, pga[i]->pg, 0,
+						  count,
+						  guard_start + used_number,
+						  guard_number - used_number,
+						  &used, sector_size,
+						  fn);
+		if (rc)
+			break;
+
+		used_number += used;
+		if (used_number == guard_number) {
+			cfs_crypto_hash_update_page(hdesc, __page, 0,
+				used_number * sizeof(*guard_start));
+			used_number = 0;
+		}
+
+		nob -= pga[i]->count;
+		pg_count--;
+		i++;
+	}
+	kunmap(__page);
+	if (rc)
+		GOTO(out, rc);
+
+	if (used_number != 0)
+		cfs_crypto_hash_update_page(hdesc, __page, 0,
+			used_number * sizeof(*guard_start));
+
+	bufsize = sizeof(cksum);
+	cfs_crypto_hash_final(hdesc, (unsigned char *)&cksum, &bufsize);
+
+	/* For sending we only compute the wrong checksum instead
+	 * of corrupting the data so it is still correct on a redo */
+	if (opc == OST_WRITE && OBD_FAIL_CHECK(OBD_FAIL_OSC_CHECKSUM_SEND))
+		cksum++;
+
+	*check_sum = cksum;
+out:
+	__free_page(__page);
+	return rc;
+}
+
 static int osc_checksum_bulk(int nob, size_t pg_count,
 			     struct brw_page **pga, int opc,
 			     enum cksum_types cksum_type,
@@ -1087,6 +1184,29 @@ static int osc_checksum_bulk(int nob, size_t pg_count,
 	return 0;
 }
 
+static int osc_checksum_bulk_rw(const char *obd_name,
+				enum cksum_types cksum_type,
+				int nob, size_t pg_count,
+				struct brw_page **pga, int opc,
+				u32 *check_sum)
+{
+	obd_dif_csum_fn *fn = NULL;
+	int sector_size = 0;
+	int rc;
+
+	ENTRY;
+	obd_t10_cksum2dif(cksum_type, &fn, &sector_size);
+
+	if (fn)
+		rc = osc_checksum_bulk_t10pi(obd_name, nob, pg_count, pga,
+					     opc, fn, sector_size, check_sum);
+	else
+		rc = osc_checksum_bulk(nob, pg_count, pga, opc, cksum_type,
+				       check_sum);
+
+	RETURN(rc);
+}
+
 static int
 osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
 		     u32 page_count, struct brw_page **pga,
@@ -1102,6 +1222,7 @@ osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
         struct req_capsule      *pill;
         struct brw_page *pg_prev;
 	void *short_io_buf;
+	const char *obd_name = cli->cl_import->imp_obd->obd_name;
 
         ENTRY;
         if (OBD_FAIL_CHECK(OBD_FAIL_OSC_BRW_PREP_REQ))
@@ -1296,12 +1417,14 @@ no_bulk:
                         if ((body->oa.o_valid & OBD_MD_FLFLAGS) == 0)
                                 body->oa.o_flags = 0;
 
-                        body->oa.o_flags |= cksum_type_pack(cksum_type);
+			body->oa.o_flags |= obd_cksum_type_pack(obd_name,
+								cksum_type);
                         body->oa.o_valid |= OBD_MD_FLCKSUM | OBD_MD_FLFLAGS;
 
-			rc = osc_checksum_bulk(requested_nob, page_count,
-					       pga, OST_WRITE, cksum_type,
-					       &body->oa.o_cksum);
+			rc = osc_checksum_bulk_rw(obd_name, cksum_type,
+						  requested_nob, page_count,
+						  pga, OST_WRITE,
+						  &body->oa.o_cksum);
 			if (rc < 0) {
 				CDEBUG(D_PAGE, "failed to checksum, rc = %d\n",
 				       rc);
@@ -1312,7 +1435,8 @@ no_bulk:
 
                         /* save this in 'oa', too, for later checking */
                         oa->o_valid |= OBD_MD_FLCKSUM | OBD_MD_FLFLAGS;
-                        oa->o_flags |= cksum_type_pack(cksum_type);
+			oa->o_flags |= obd_cksum_type_pack(obd_name,
+							   cksum_type);
                 } else {
                         /* clear out the checksum flag, in case this is a
                          * resend but cl_checksum is no longer set. b=11238 */
@@ -1327,9 +1451,10 @@ no_bulk:
                     !sptlrpc_flavor_has_bulk(&req->rq_flvr)) {
                         if ((body->oa.o_valid & OBD_MD_FLFLAGS) == 0)
                                 body->oa.o_flags = 0;
-                        body->oa.o_flags |= cksum_type_pack(cli->cl_cksum_type);
+			body->oa.o_flags |= obd_cksum_type_pack(obd_name,
+				cli->cl_cksum_type);
                         body->oa.o_valid |= OBD_MD_FLCKSUM | OBD_MD_FLFLAGS;
-                }
+		}
 
 		/* Client cksum has been already copied to wire obdo in previous
 		 * lustre_set_wire_obdo(), and in the case a bulk-read is being
@@ -1426,12 +1551,16 @@ static void dump_all_bulk_pages(struct obdo *oa, __u32 page_count,
 
 static int
 check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
-				__u32 client_cksum, __u32 server_cksum,
-				struct osc_brw_async_args *aa)
+		     __u32 client_cksum, __u32 server_cksum,
+		     struct osc_brw_async_args *aa)
 {
-        __u32 new_cksum;
-        char *msg;
+	const char *obd_name = aa->aa_cli->cl_import->imp_obd->obd_name;
 	enum cksum_types cksum_type;
+	obd_dif_csum_fn *fn = NULL;
+	int sector_size = 0;
+	bool t10pi = false;
+	__u32 new_cksum;
+	char *msg;
 	int rc;
 
         if (server_cksum == client_cksum) {
@@ -1443,15 +1572,50 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 		dump_all_bulk_pages(oa, aa->aa_page_count, aa->aa_ppga,
 				    server_cksum, client_cksum);
 
-	cksum_type = cksum_type_unpack(oa->o_valid & OBD_MD_FLFLAGS ?
-				       oa->o_flags : 0);
-	rc = osc_checksum_bulk(aa->aa_requested_nob, aa->aa_page_count,
-			       aa->aa_ppga, OST_WRITE, cksum_type,
-			       &new_cksum);
+	cksum_type = obd_cksum_type_unpack(oa->o_valid & OBD_MD_FLFLAGS ?
+					   oa->o_flags : 0);
+
+	switch (cksum_type) {
+	case OBD_CKSUM_T10IP512:
+		t10pi = true;
+		fn = obd_dif_ip_fn;
+		sector_size = 512;
+		break;
+	case OBD_CKSUM_T10IP4K:
+		t10pi = true;
+		fn = obd_dif_ip_fn;
+		sector_size = 4096;
+		break;
+	case OBD_CKSUM_T10CRC512:
+		t10pi = true;
+		fn = obd_dif_crc_fn;
+		sector_size = 512;
+		break;
+	case OBD_CKSUM_T10CRC4K:
+		t10pi = true;
+		fn = obd_dif_crc_fn;
+		sector_size = 4096;
+		break;
+	default:
+		break;
+	}
+
+	if (t10pi)
+		rc = osc_checksum_bulk_t10pi(obd_name, aa->aa_requested_nob,
+					     aa->aa_page_count,
+					     aa->aa_ppga,
+					     OST_WRITE,
+					     fn,
+					     sector_size,
+					     &new_cksum);
+	else
+		rc = osc_checksum_bulk(aa->aa_requested_nob, aa->aa_page_count,
+				       aa->aa_ppga, OST_WRITE, cksum_type,
+				       &new_cksum);
 
 	if (rc < 0)
 		msg = "failed to calculate the client write checksum";
-	else if (cksum_type != cksum_type_unpack(aa->aa_oa->o_flags))
+	else if (cksum_type != obd_cksum_type_unpack(aa->aa_oa->o_flags))
                 msg = "the server did not use the checksum type specified in "
                       "the original request - likely a protocol problem";
         else if (new_cksum == server_cksum)
@@ -1467,15 +1631,15 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 			   DFID " object "DOSTID" extent [%llu-%llu], original "
 			   "client csum %x (type %x), server csum %x (type %x),"
 			   " client csum now %x\n",
-			   aa->aa_cli->cl_import->imp_obd->obd_name,
-			   msg, libcfs_nid2str(peer->nid),
+			   obd_name, msg, libcfs_nid2str(peer->nid),
 			   oa->o_valid & OBD_MD_FLFID ? oa->o_parent_seq : (__u64)0,
 			   oa->o_valid & OBD_MD_FLFID ? oa->o_parent_oid : 0,
 			   oa->o_valid & OBD_MD_FLFID ? oa->o_parent_ver : 0,
 			   POSTID(&oa->o_oi), aa->aa_ppga[0]->off,
 			   aa->aa_ppga[aa->aa_page_count - 1]->off +
 				aa->aa_ppga[aa->aa_page_count-1]->count - 1,
-			   client_cksum, cksum_type_unpack(aa->aa_oa->o_flags),
+			   client_cksum,
+			   obd_cksum_type_unpack(aa->aa_oa->o_flags),
 			   server_cksum, cksum_type, new_cksum);
 	return 1;
 }
@@ -1483,11 +1647,12 @@ check_write_checksum(struct obdo *oa, const struct lnet_process_id *peer,
 /* Note rc enters this function as number of bytes transferred */
 static int osc_brw_fini_request(struct ptlrpc_request *req, int rc)
 {
-        struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
+	struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
+	struct client_obd *cli = aa->aa_cli;
+	const char *obd_name = cli->cl_import->imp_obd->obd_name;
 	const struct lnet_process_id *peer =
-                        &req->rq_import->imp_connection->c_peer;
-        struct client_obd *cli = aa->aa_cli;
-        struct ost_body *body;
+		&req->rq_import->imp_connection->c_peer;
+	struct ost_body *body;
 	u32 client_cksum = 0;
         ENTRY;
 
@@ -1607,16 +1772,16 @@ static int osc_brw_fini_request(struct ptlrpc_request *req, int rc)
 		char      *via = "";
 		char      *router = "";
 		enum cksum_types cksum_type;
+		u32 o_flags = body->oa.o_valid & OBD_MD_FLFLAGS ?
+			body->oa.o_flags : 0;
 
-                cksum_type = cksum_type_unpack(body->oa.o_valid &OBD_MD_FLFLAGS?
-                                               body->oa.o_flags : 0);
-		rc = osc_checksum_bulk(rc, aa->aa_page_count, aa->aa_ppga,
-				       OST_READ, cksum_type, &client_cksum);
-		if (rc < 0) {
-			CDEBUG(D_PAGE,
-			       "failed to calculate checksum, rc = %d\n", rc);
+		cksum_type = obd_cksum_type_unpack(o_flags);
+		rc = osc_checksum_bulk_rw(obd_name, cksum_type, rc,
+					  aa->aa_page_count, aa->aa_ppga,
+					  OST_READ, &client_cksum);
+		if (rc < 0)
 			GOTO(out, rc);
-		}
+
 		if (req->rq_bulk != NULL &&
 		    peer->nid != req->rq_bulk->bd_sender) {
 			via = " via ";
@@ -1638,7 +1803,7 @@ static int osc_brw_fini_request(struct ptlrpc_request *req, int rc)
 					   "%s%s%s inode "DFID" object "DOSTID
 					   " extent [%llu-%llu], client %x, "
 					   "server %x, cksum_type %x\n",
-					   req->rq_import->imp_obd->obd_name,
+					   obd_name,
 					   libcfs_nid2str(peer->nid),
 					   via, router,
 					   clbody->oa.o_valid & OBD_MD_FLFID ?

lustre/target/tgt_handler.c
@@ -1885,8 +1885,8 @@ static int check_read_checksum(struct niobuf_local *local_nb, int npages,
 		dump_all_bulk_pages(oa, npages, local_nb, server_cksum,
 				    client_cksum);
 
-	cksum_type = cksum_type_unpack(oa->o_valid & OBD_MD_FLFLAGS ?
-				       oa->o_flags : 0);
+	cksum_type = obd_cksum_type_unpack(oa->o_valid & OBD_MD_FLFLAGS ?
+					   oa->o_flags : 0);
 
 	if (cksum_type != server_cksum_type)
 		msg = "the server may have not used the checksum type specified"
@@ -1937,6 +1937,162 @@ static int tgt_pages2shortio(struct niobuf_local *local, int npages,
 	return copied - size;
 }
 
+static int tgt_checksum_niobuf_t10pi(struct lu_target *tgt,
+				     struct niobuf_local *local_nb,
+				     int npages, int opc,
+				     obd_dif_csum_fn *fn,
+				     int sector_size,
+				     u32 *check_sum)
+{
+	unsigned char cfs_alg = cksum_obd2cfs(OBD_CKSUM_T10_TOP);
+	const char *obd_name = tgt->lut_obd->obd_name;
+	struct cfs_crypto_hash_desc *hdesc;
+	unsigned int bufsize;
+	unsigned char *buffer;
+	struct page *__page;
+	__u16 *guard_start;
+	int guard_number;
+	int used_number = 0;
+	__u32 cksum;
+	int rc = 0;
+	int used;
+	int i;
+
+	__page = alloc_page(GFP_KERNEL);
+	if (__page == NULL)
+		return -ENOMEM;
+
+	hdesc = cfs_crypto_hash_init(cfs_alg, NULL, 0);
+	if (IS_ERR(hdesc)) {
+		CERROR("%s: unable to initialize checksum hash %s\n",
+		       tgt_name(tgt), cfs_crypto_hash_name(cfs_alg));
+		return PTR_ERR(hdesc);
+	}
+
+	buffer = kmap(__page);
+	guard_start = (__u16 *)buffer;
+	guard_number = PAGE_SIZE / sizeof(*guard_start);
+	for (i = 0; i < npages; i++) {
+		/* corrupt the data before we compute the checksum, to
+		 * simulate a client->OST data error */
+		if (i == 0 && opc == OST_WRITE &&
+		    OBD_FAIL_CHECK(OBD_FAIL_OST_CHECKSUM_RECEIVE)) {
+			int off = local_nb[i].lnb_page_offset & ~PAGE_MASK;
+			int len = local_nb[i].lnb_len;
+			struct page *np = tgt_page_to_corrupt;
+
+			if (np) {
+				char *ptr = ll_kmap_atomic(local_nb[i].lnb_page,
+							KM_USER0);
+				char *ptr2 = page_address(np);
+
+				memcpy(ptr2 + off, ptr + off, len);
+				memcpy(ptr2 + off, "bad3", min(4, len));
+				ll_kunmap_atomic(ptr, KM_USER0);
+
+				/* LU-8376 to preserve original index for
+				 * display in dump_all_bulk_pages() */
+				np->index = i;
+
+				cfs_crypto_hash_update_page(hdesc, np, off,
+							    len);
+				continue;
+			} else {
+				CERROR("%s: can't alloc page for corruption\n",
+				       tgt_name(tgt));
+			}
+		}
+
+		/*
+		 * The left guard number should be able to hold checksums of a
+		 * whole page
+		 */
+		rc = obd_page_dif_generate_buffer(obd_name,
+			local_nb[i].lnb_page,
+			local_nb[i].lnb_page_offset & ~PAGE_MASK,
+			local_nb[i].lnb_len, guard_start + used_number,
+			guard_number - used_number, &used, sector_size,
+			fn);
+		if (rc)
+			break;
+
+		used_number += used;
+		if (used_number == guard_number) {
+			cfs_crypto_hash_update_page(hdesc, __page, 0,
+				used_number * sizeof(*guard_start));
+			used_number = 0;
+		}
+
+		 /* corrupt the data after we compute the checksum, to
+		 * simulate an OST->client data error */
+		if (unlikely(i == 0 && opc == OST_READ &&
+			     OBD_FAIL_CHECK(OBD_FAIL_OST_CHECKSUM_SEND))) {
+			int off = local_nb[i].lnb_page_offset & ~PAGE_MASK;
+			int len = local_nb[i].lnb_len;
+			struct page *np = tgt_page_to_corrupt;
+
+			if (np) {
+				char *ptr = ll_kmap_atomic(local_nb[i].lnb_page,
+							KM_USER0);
+				char *ptr2 = page_address(np);
+
+				memcpy(ptr2 + off, ptr + off, len);
+				memcpy(ptr2 + off, "bad4", min(4, len));
+				ll_kunmap_atomic(ptr, KM_USER0);
+
+				/* LU-8376 to preserve original index for
+				 * display in dump_all_bulk_pages() */
+				np->index = i;
+
+				cfs_crypto_hash_update_page(hdesc, np, off,
+							    len);
+				continue;
+			} else {
+				CERROR("%s: can't alloc page for corruption\n",
+				       tgt_name(tgt));
+			}
+		}
+	}
+	kunmap(__page);
+	if (rc)
+		GOTO(out, rc);
+
+	if (used_number != 0)
+		cfs_crypto_hash_update_page(hdesc, __page, 0,
+			used_number * sizeof(*guard_start));
+
+	bufsize = sizeof(cksum);
+	rc = cfs_crypto_hash_final(hdesc, (unsigned char *)&cksum, &bufsize);
+
+	if (rc == 0)
+		*check_sum = cksum;
+out:
+	__free_page(__page);
+	return rc;
+}
+
+static int tgt_checksum_niobuf_rw(struct lu_target *tgt,
+				  enum cksum_types cksum_type,
+				  struct niobuf_local *local_nb,
+				  int npages, int opc, u32 *check_sum)
+{
+	obd_dif_csum_fn *fn = NULL;
+	int sector_size = 0;
+	int rc;
+
+	ENTRY;
+	obd_t10_cksum2dif(cksum_type, &fn, &sector_size);
+
+	if (fn)
+		rc = tgt_checksum_niobuf_t10pi(tgt, local_nb, npages,
+					       opc, fn, sector_size,
+					       check_sum);
+	else
+		rc = tgt_checksum_niobuf(tgt, local_nb, npages, opc,
+					 cksum_type, check_sum);
+	RETURN(rc);
+}
+
 int tgt_brw_read(struct tgt_session_info *tsi)
 {
 	struct ptlrpc_request	*req = tgt_ses_req(tsi);
@@ -1951,6 +2107,7 @@ int tgt_brw_read(struct tgt_session_info *tsi)
 	int			 npages, nob = 0, rc, i, no_reply = 0,
 				 npages_read;
 	struct tgt_thread_big_cache *tbc = req->rq_svc_thread->t_data;
+	const char *obd_name = exp->exp_obd->obd_name;
 
 	ENTRY;
 
@@ -2072,18 +2229,19 @@ int tgt_brw_read(struct tgt_session_info *tsi)
 		rc = -E2BIG;
 
 	if (body->oa.o_valid & OBD_MD_FLCKSUM) {
-		enum cksum_types cksum_type =
-			cksum_type_unpack(body->oa.o_valid & OBD_MD_FLFLAGS ?
-					  body->oa.o_flags : 0);
+		u32 flag = body->oa.o_valid & OBD_MD_FLFLAGS ?
+			   body->oa.o_flags : 0;
+		enum cksum_types cksum_type = obd_cksum_type_unpack(flag);
 
-		repbody->oa.o_flags = cksum_type_pack(cksum_type);
+		repbody->oa.o_flags = obd_cksum_type_pack(obd_name,
+							  cksum_type);
 		repbody->oa.o_valid = OBD_MD_FLCKSUM | OBD_MD_FLFLAGS;
-		rc = tgt_checksum_niobuf(tsi->tsi_tgt, local_nb,
-					 npages_read, OST_READ, cksum_type,
-					 &repbody->oa.o_cksum);
+
+		rc = tgt_checksum_niobuf_rw(tsi->tsi_tgt, cksum_type,
+					    local_nb, npages_read, OST_READ,
+					    &repbody->oa.o_cksum);
 		if (rc < 0)
 			GOTO(out_commitrw, rc);
-
 		CDEBUG(D_PAGE, "checksum at read origin: %x\n",
 		       repbody->oa.o_cksum);
 
@@ -2150,7 +2308,7 @@ out_lock:
 		ptlrpc_req_drop_rs(req);
 		LCONSOLE_WARN("%s: Bulk IO read error with %s (at %s), "
 			      "client will retry: rc %d\n",
-			      exp->exp_obd->obd_name,
+			      obd_name,
 			      obd_uuid2str(&exp->exp_client_uuid),
 			      obd_export_nid2str(exp), rc);
 	}
@@ -2266,6 +2424,7 @@ int tgt_brw_write(struct tgt_session_info *tsi)
 	bool			 no_reply = false, mmap;
 	struct tgt_thread_big_cache *tbc = req->rq_svc_thread->t_data;
 	bool wait_sync = false;
+	const char *obd_name = exp->exp_obd->obd_name;
 
 	ENTRY;
 
@@ -2418,14 +2577,16 @@ skip_transfer:
 		static int cksum_counter;
 
 		if (body->oa.o_valid & OBD_MD_FLFLAGS)
-			cksum_type = cksum_type_unpack(body->oa.o_flags);
+			cksum_type = obd_cksum_type_unpack(body->oa.o_flags);
 
 		repbody->oa.o_valid |= OBD_MD_FLCKSUM | OBD_MD_FLFLAGS;
 		repbody->oa.o_flags &= ~OBD_FL_CKSUM_ALL;
-		repbody->oa.o_flags |= cksum_type_pack(cksum_type);
-		rc = tgt_checksum_niobuf(tsi->tsi_tgt, local_nb,
-					 npages, OST_WRITE, cksum_type,
-					 &repbody->oa.o_cksum);
+		repbody->oa.o_flags |= obd_cksum_type_pack(obd_name,
+							   cksum_type);
+
+		rc = tgt_checksum_niobuf_rw(tsi->tsi_tgt, cksum_type,
+					    local_nb, npages, OST_WRITE,
+					    &repbody->oa.o_cksum);
 		if (rc < 0)
 			GOTO(out_commitrw, rc);
 
@@ -2503,7 +2664,7 @@ out:
 		if (!exp->exp_obd->obd_no_transno)
 			LCONSOLE_WARN("%s: Bulk IO write error with %s (at %s),"
 				      " client will retry: rc = %d\n",
-				      exp->exp_obd->obd_name,
+				      obd_name,
 				      obd_uuid2str(&exp->exp_client_uuid),
 				      obd_export_nid2str(exp), rc);
 	}

libcfs/libcfs/linux/linux-crypto.c
@@ -325,6 +325,9 @@ EXPORT_SYMBOL(cfs_crypto_hash_final);
  * The speed is stored internally in the cfs_crypto_hash_speeds[] array, and
  * is available through the cfs_crypto_hash_speed() function.
  *
+ * This function needs to stay the same as obd_t10_performance_test() so that
+ * the speeds are comparable.
+ *
  * \param[in] hash_alg	hash algorithm id (CFS_HASH_ALG_*)
  * \param[in] buf	data buffer on which to compute the hash
  * \param[in] buf_len	length of \buf on which to compute hash

lustre/autoconf/lustre-core.m4
@@ -1987,6 +1987,23 @@ file_function_iter, [
 ]) # LC_HAVE_FILE_OPERATIONS_READ_WRITE_ITER
 
 #
+# LC_HAVE_INTERVAL_BLK_INTEGRITY
+#
+# 3.17 replace sector_size with interval in struct blk_integrity
+#
+AC_DEFUN([LC_HAVE_INTERVAL_BLK_INTEGRITY], [
+LB_CHECK_COMPILE([if 'blk_integrity.interval' exist],
+interval_blk_integrity, [
+	#include <linux/blkdev.h>
+],[
+	((struct blk_integrity *)0)->interval = 0;
+],[
+	AC_DEFINE(HAVE_INTERVAL_BLK_INTEGRITY, 1,
+		[blk_integrity.interval exist])
+])
+]) # LC_HAVE_INTERVAL_BLK_INTEGRITY
+
+#
 # LC_KEY_MATCH_DATA
 #
 # 3.17	replaces key_type::match with match_preparse
@@ -2239,6 +2256,25 @@ bio_endio, [
 		[bio_endio takes only one argument])
 ])
 ]) # LC_BIO_ENDIO_USES_ONE_ARG
+
+#
+# LC_HAVE_INTERVAL_EXP_BLK_INTEGRITY
+#
+# 4.3 replace interval with interval_exp in 'struct blk_integrity'
+# 'struct blk_integrity_profile' is also added in this version,
+# thus use this to determine whether 'struct blk_integrity' has profile
+#
+AC_DEFUN([LC_HAVE_INTERVAL_EXP_BLK_INTEGRITY], [
+LB_CHECK_COMPILE([if 'blk_integrity.interval_exp' exist],
+blk_integrity_interval_exp, [
+	#include <linux/blkdev.h>
+],[
+	((struct blk_integrity *)0)->interval_exp = 0;
+],[
+	AC_DEFINE(HAVE_INTERVAL_EXP_BLK_INTEGRITY, 1,
+		[blk_integrity.interval_exp exist])
+])
+]) # LC_HAVE_INTERVAL_EXP_BLK_INTEGRITY
 
 #
 # LC_HAVE_LOOP_CTL_GET_FREE
@@ -2952,6 +2988,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_HAVE_FILE_OPERATIONS_READ_WRITE_ITER
 
 	# 3.17
+	LC_HAVE_INTERVAL_BLK_INTEGRITY
 	LC_KEY_MATCH_DATA
 
 	# 3.18
@@ -2979,6 +3016,7 @@ AC_DEFUN([LC_PROG_LINUX], [
 	LC_SYMLINK_OPS_USE_NAMEIDATA
 
 	# 4.3
+	LC_HAVE_INTERVAL_EXP_BLK_INTEGRITY
 	LC_HAVE_CACHE_HEAD_HLIST
 	LC_HAVE_XATTR_HANDLER_SIMPLIFIED
 

lustre/include/dt_object.h
@@ -86,6 +86,8 @@ struct dt_device_param {
 	 * calculation */
 	unsigned int	   ddp_extent_tax;
 	unsigned int	   ddp_brw_size;	/* optimal RPC size */
+	/* T10PI checksum type, zero if not supported */
+	enum cksum_types   ddp_t10_cksum_type;
 };
 
 /**

commit 3999627447c01eebd96c14cc5cf8bba93f89a66b

LU-16712 cksum: fix generating T10PI guard tags for partial brw page
To get better performance, we allocate a page as the buffer for
T10PI guard tags, we fill the buffer going over every page for brw,
when the buffer is considered full, we use
cfs_crypto_hash_update_page() to update the hash and reuse the buffer.

It doesn't work when there's a page in the brw gets clipped, and the
checksum sector size is 512. For a page with PAGE_SIZE of 4096,
and offset at 1024, we will end up with 6 guard tags, and won't have
enough space in the very end of the buffer for a full brw page, which
needs 8.

Work out the number of guard tags for each page, update the
checksum hash and reuse the buffer when needed.

Change-Id: Ic591e63b24534f2a42b670669520895cb35a9546
Fixes: b1e7be00cb ("LU-10472 osc: add T10PI support for RPC checksum")
Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50540
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osc/osc_request.c
@@ -1189,12 +1189,12 @@ static int osc_checksum_bulk_t10pi(const char *obd_name, int nob,
 	struct page *__page;
 	unsigned char *buffer;
 	__be16 *guard_start;
-	unsigned int bufsize;
 	int guard_number;
 	int used_number = 0;
 	int used;
 	u32 cksum;
-	int rc = 0;
+	unsigned int bufsize = sizeof(cksum);
+	int rc = 0, rc2;
 	int i = 0;
 
 	LASSERT(pg_count > 0);
@@ -1219,14 +1219,22 @@ static int osc_checksum_bulk_t10pi(const char *obd_name, int nob,
 	       guard_number, resend, nob, pg_count);
 
 	while (nob > 0 && pg_count > 0) {
+		int off = pga[i]->off & ~PAGE_MASK;
 		unsigned int count = pga[i]->count > nob ? nob : pga[i]->count;
+		int guards_needed = DIV_ROUND_UP(off + count, sector_size) -
+					(off / sector_size);
+
+		if (guards_needed > guard_number - used_number) {
+			cfs_crypto_hash_update_page(req, __page, 0,
+				used_number * sizeof(*guard_start));
+			used_number = 0;
+		}
 
 		/* corrupt the data before we compute the checksum, to
 		 * simulate an OST->client data error */
 		if (unlikely(i == 0 && opc == OST_READ &&
 			     OBD_FAIL_CHECK(OBD_FAIL_OSC_CHECKSUM_RECEIVE))) {
 			unsigned char *ptr = kmap(pga[i]->pg);
-			int off = pga[i]->off & ~PAGE_MASK;
 
 			memcpy(ptr + off, "bad1", min_t(typeof(nob), 4, nob));
 			kunmap(pga[i]->pg);
@@ -1253,33 +1261,31 @@ static int osc_checksum_bulk_t10pi(const char *obd_name, int nob,
 			break;
 
 		used_number += used;
-		if (used_number == guard_number) {
-			cfs_crypto_hash_update_page(req, __page, 0,
-				used_number * sizeof(*guard_start));
-			used_number = 0;
-		}
-
 		nob -= pga[i]->count;
 		pg_count--;
 		i++;
 	}
 	kunmap(__page);
 	if (rc)
-		GOTO(out, rc);
+		GOTO(out_hash, rc);
 
 	if (used_number != 0)
 		cfs_crypto_hash_update_page(req, __page, 0,
 			used_number * sizeof(*guard_start));
 
-	bufsize = sizeof(cksum);
-	cfs_crypto_hash_final(req, (unsigned char *)&cksum, &bufsize);
+out_hash:
+	rc2 = cfs_crypto_hash_final(req, (unsigned char *)&cksum, &bufsize);
+	if (!rc)
+		rc = rc2;
+	if (rc == 0) {
+		/* For sending we only compute the wrong checksum instead
+		 * of corrupting the data so it is still correct on a redo */
+		if (opc == OST_WRITE &&
+				OBD_FAIL_CHECK(OBD_FAIL_OSC_CHECKSUM_SEND))
+			cksum++;
 
-	/* For sending we only compute the wrong checksum instead
-	 * of corrupting the data so it is still correct on a redo */
-	if (opc == OST_WRITE && OBD_FAIL_CHECK(OBD_FAIL_OSC_CHECKSUM_SEND))
-		cksum++;
-
-	*check_sum = cksum;
+		*check_sum = cksum;
+	}
 out:
 	__free_page(__page);
 	return rc;

lustre/target/tgt_handler.c
@@ -2101,13 +2101,21 @@ static int tgt_checksum_niobuf_t10pi(struct lu_target *tgt,
 		CDEBUG(D_PAGE | D_HA, "GRD tags per page = %u\n", guard_number);
 	for (i = 0; i < npages; i++) {
 		bool use_t10_grd;
+		int off = local_nb[i].lnb_page_offset & ~PAGE_MASK;
+		int len = local_nb[i].lnb_len;
+		int guards_needed = DIV_ROUND_UP(off + len, sector_size) -
+					(off / sector_size);
+
+		if (guards_needed > guard_number - used_number) {
+			cfs_crypto_hash_update_page(req, __page, 0,
+				used_number * sizeof(*guard_start));
+			used_number = 0;
+		}
 
 		/* corrupt the data before we compute the checksum, to
 		 * simulate a client->OST data error */
 		if (i == 0 && opc == OST_WRITE &&
 		    OBD_FAIL_CHECK(OBD_FAIL_OST_CHECKSUM_RECEIVE)) {
-			int off = local_nb[i].lnb_page_offset & ~PAGE_MASK;
-			int len = local_nb[i].lnb_len;
 			struct page *np = tgt_page_to_corrupt;
 
 			if (np) {
@@ -2140,7 +2148,7 @@ static int tgt_checksum_niobuf_t10pi(struct lu_target *tgt,
 			      local_nb[i].lnb_len == PAGE_SIZE &&
 			      local_nb[i].lnb_guard_disk;
 		if (use_t10_grd) {
-			used = DIV_ROUND_UP(local_nb[i].lnb_len, sector_size);
+			used = guards_needed;
 			if (used > (guard_number - used_number)) {
 				rc = -E2BIG;
 				CDEBUG(D_PAGE | D_HA,
@@ -2223,18 +2231,11 @@ static int tgt_checksum_niobuf_t10pi(struct lu_target *tgt,
 		}
 
 		used_number += used;
-		if (used_number == guard_number) {
-			cfs_crypto_hash_update_page(req, __page, 0,
-				used_number * sizeof(*guard_start));
-			used_number = 0;
-		}
 
 		 /* corrupt the data after we compute the checksum, to
 		 * simulate an OST->client data error */
 		if (unlikely(i == 0 && opc == OST_READ &&
 			     OBD_FAIL_CHECK(OBD_FAIL_OST_CHECKSUM_SEND))) {
-			int off = local_nb[i].lnb_page_offset & ~PAGE_MASK;
-			int len = local_nb[i].lnb_len;
 			struct page *np = tgt_page_to_corrupt;
 
 			if (np) {