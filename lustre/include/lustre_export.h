lustre/lustre-release

commit 63e17799a369e2ff0b140fd41dc5d7d8656d2bf0

LU-8367 osp: enable replay for precreation request
Lustre has some kind of deadlock between osp_precreate_thread()
and stripe allocation at osp_precreate_reserve(). Stripe allocation
thread allocated objects and sleeps for more objects at
osp_precreate_reserve() in case of OST failover. After reconnection,
osp_precreate_thread() calls osp_precreate_cleanup_orphans() to
synchronize last id and clean-up unused objects, but it waits zero
object reservation(d->opd_pre_reserved). So, no more objects could
be created at OST and no reserved objects could be freed.
This produce slow creates messages and MDT creation threads hang
osp_precreate_reserve()) kjcf05-OST0003-osc-MDT0001: slow creates,
 last=[0x340000400:0x23a4f483:0x0], next=[0x340000400:0x23a4f378:0x0],
 reserved=267, sync_changes=0, sync_rpcs_in_progress=0, status=0
The issue reproduced more often with over stripe feature.

No need to do orphan clean-up phase when MDT supports
resend/replay for precreation request. This behaviour resolves the
osp_precreate_cleanup_orphans() hang and unblocks objects creation.

Force creation logic is added to support reformatted OST with a same
index. It was done during orphan clean-up phase before this.

Sanity tests 27S and 822 become invalid. 27S is based on orphan
clean-up after reconnection, 822 is based on not resendable
OST_CREATE request. These tests are removed.

HPE-bug-id: LUS-10793
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: I21287b51252e573e796fac69ee3df6ac90e28c10
Reviewed-on: https://review.whamcloud.com/46889
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_dev.c
@@ -1665,6 +1665,22 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 		int count;
 		int rc2;
 
+		/* This can happen if a new OST is formatted and installed
+		 * in place of an old one at the same index.  Instead of
+		 * precreating potentially millions of deleted old objects
+		 * (possibly filling the OST), only precreate the last batch.
+		 * LFSCK will eventually clean up any orphans. LU-14 */
+		if (diff > 5 * OST_MAX_PRECREATE) {
+			/* Message below is checked in conf-sanity test_122b */
+			LCONSOLE_WARN("%s: precreate FID "DOSTID" is over %lld higher than LAST_ID "DOSTID", only precreating the last %u objects. OST replaced or reformatted?\n",
+				      ofd_name(ofd), POSTID(&oa->o_oi), diff,
+				      POSTID(&oseq->os_oi),
+				      OST_MAX_PRECREATE);
+			/* From last created */
+			diff = OST_MAX_PRECREATE;
+			ofd_seq_last_oid_set(oseq, ostid_id(&oa->o_oi) - diff);
+		}
+
 		if (!(oa->o_valid & OBD_MD_FLFLAGS) ||
 		    !(oa->o_flags & OBD_FL_DELORPHAN)) {
 			/* don't enforce grant during orphan recovery */
@@ -1681,20 +1697,6 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 			}
 		}
 
-		/* This can happen if a new OST is formatted and installed
-		 * in place of an old one at the same index.  Instead of
-		 * precreating potentially millions of deleted old objects
-		 * (possibly filling the OST), only precreate the last batch.
-		 * LFSCK will eventually clean up any orphans. LU-14 */
-		if (diff > 5 * OST_MAX_PRECREATE) {
-			/* Message below is checked in conf-sanity test_122b */
-			LCONSOLE_WARN("%s: precreate FID "DOSTID" is over %lld higher than LAST_ID "DOSTID", only precreating the last %u objects. OST replaced or reformatted?\n",
-				      ofd_name(ofd), POSTID(&oa->o_oi), diff,
-				      POSTID(&oseq->os_oi),
-				      OST_MAX_PRECREATE / 2);
-			diff = OST_MAX_PRECREATE / 2;
-			ofd_seq_last_oid_set(oseq, ostid_id(&oa->o_oi) - diff);
-		}
 
 		while (diff > 0) {
 			next_id = ofd_seq_last_oid(oseq) + 1;

lustre/ofd/ofd_objects.c
@@ -356,7 +356,7 @@ int ofd_precreate_objects(const struct lu_env *env, struct ofd_device *ofd,
 		}
 	}
 
-	rc = dt_trans_start_local(env, ofd->ofd_osd, th);
+	rc = dt_trans_start(env, ofd->ofd_osd, th);
 	if (rc)
 		GOTO(trans_stop, rc);
 

lustre/osp/osp_precreate.c
@@ -338,8 +338,8 @@ static inline int osp_precreate_near_empty_nolock(const struct lu_env *env,
 
 	/* don't consider new precreation till OST is healty and
 	 * has free space */
-	return ((window - d->opd_pre_reserved < d->opd_pre_create_count / 2) &&
-		(d->opd_pre_status == 0));
+	return ((window - d->opd_pre_reserved < d->opd_pre_create_count / 2 ||
+		 d->opd_force_creation) && (d->opd_pre_status == 0));
 }
 
 /**
@@ -633,9 +633,11 @@ static int osp_precreate_send(const struct lu_env *env, struct osp_device *d)
 	if (req == NULL)
 		RETURN(-ENOMEM);
 	req->rq_request_portal = OST_CREATE_PORTAL;
-	/* we should not resend create request - anyway we will have delorphan
-	 * and kill these objects */
-	req->rq_no_delay = req->rq_no_resend = 1;
+
+	/* Delorphan happens only with a first MDT-OST connect. resend/replay
+	 * handles objects creation on reconnects, no need to do delorhpan
+	 * in this case.
+	 */
 
 	rc = ptlrpc_request_pack(req, LUSTRE_OST_VERSION, OST_CREATE);
 	if (rc) {
@@ -644,7 +646,9 @@ static int osp_precreate_send(const struct lu_env *env, struct osp_device *d)
 	}
 
 	spin_lock(&d->opd_pre_lock);
-	if (d->opd_pre_create_count > d->opd_pre_max_create_count / 2)
+	if (d->opd_force_creation)
+		d->opd_pre_create_count = OST_MIN_PRECREATE;
+	else if (d->opd_pre_create_count > d->opd_pre_max_create_count / 2)
 		d->opd_pre_create_count = d->opd_pre_max_create_count / 2;
 	grow = d->opd_pre_create_count;
 	spin_unlock(&d->opd_pre_lock);
@@ -682,7 +686,6 @@ static int osp_precreate_send(const struct lu_env *env, struct osp_device *d)
 			rc = -ENOTCONN;
 		GOTO(out_req, rc);
 	}
-	LASSERT(req->rq_transno == 0);
 
 	body = req_capsule_server_get(&req->rq_pill, &RMF_OST_BODY);
 	if (body == NULL)
@@ -717,6 +720,7 @@ ready:
 	fid_to_ostid(fid, &body->oa.o_oi);
 
 	d->opd_pre_last_created_fid = *fid;
+	d->opd_force_creation = false;
 	spin_unlock(&d->opd_pre_lock);
 
 	CDEBUG(D_HA, "%s: current precreated pool: "DFID"-"DFID"\n",
@@ -743,12 +747,13 @@ out_req:
  *
  * \param[in] env	LU environment provided by the caller
  * \param[in] d		OSP device
+ * \param[in] update	update or not update last used fid
  *
  * \retval 0		on success
  * \retval negative	negated errno on error
  **/
 static int osp_get_lastfid_from_ost(const struct lu_env *env,
-				    struct osp_device *d)
+				    struct osp_device *d, bool update)
 {
 	struct ptlrpc_request	*req = NULL;
 	struct obd_import	*imp;
@@ -806,9 +811,16 @@ static int osp_get_lastfid_from_ost(const struct lu_env *env,
 
 	/* Only update the last used fid, if the OST has objects for
 	 * this sequence, i.e. fid_oid > 0 */
-	if (fid_oid(last_fid) > 0)
+	if (fid_oid(last_fid) > 0 && update)
 		d->opd_last_used_fid = *last_fid;
 
+	if (fid_oid(last_fid) == 0 &&
+	    fid_seq(last_fid) == fid_seq(&d->opd_last_used_fid)) {
+		/* reformatted OST, it requires creation request
+		 * to recreate objects
+		 */
+		d->opd_force_creation = true;
+	}
 	CDEBUG(D_HA, "%s: Got last_fid "DFID"\n", d->opd_obd->obd_name,
 	       PFID(last_fid));
 
@@ -849,6 +861,15 @@ static int osp_precreate_cleanup_orphans(struct lu_env *env,
 	ENTRY;
 
 	/*
+	 * Do cleanup orphans only with a first connection, after that
+	 * all precreate requests uses resend/replay flags to support OST
+	 * failover/reconnect.
+	 */
+	if (d->opd_cleanup_orphans_done) {
+		rc = osp_get_lastfid_from_ost(env, d, false);
+		RETURN(0);
+	}
+	/*
 	 * wait for local recovery to finish, so we can cleanup orphans
 	 * orphans are all objects since "last used" (assigned), but
 	 * there might be objects reserved and in some cases they won't
@@ -883,7 +904,7 @@ static int osp_precreate_cleanup_orphans(struct lu_env *env,
 	if (fid_oid(&d->opd_last_used_fid) < 2 ||
 	    OBD_FAIL_CHECK(OBD_FAIL_OSP_GET_LAST_FID)) {
 		/* lastfid looks strange... ask OST */
-		rc = osp_get_lastfid_from_ost(env, d);
+		rc = osp_get_lastfid_from_ost(env, d, true);
 		if (rc)
 			GOTO(out, rc);
 	}
@@ -980,6 +1001,7 @@ out:
 		spin_lock(&d->opd_pre_lock);
 		d->opd_pre_recovering = 0;
 		spin_unlock(&d->opd_pre_lock);
+		d->opd_cleanup_orphans_done = true;
 	}
 
 	RETURN(rc);
@@ -1749,6 +1771,8 @@ int osp_init_precreate(struct osp_device *d)
 	d->opd_pre_max_create_count = OST_MAX_PRECREATE;
 	d->opd_reserved_mb_high = 0;
 	d->opd_reserved_mb_low = 0;
+	d->opd_cleanup_orphans_done = false;
+	d->opd_force_creation = false;
 
 	RETURN(0);
 }

lustre/osp/osp_internal.h
@@ -268,6 +268,8 @@ struct osp_device {
 	 */
 	int				opd_reserved_mb_high;
 	int				opd_reserved_mb_low;
+	bool				opd_cleanup_orphans_done;
+	bool				opd_force_creation;
 };
 
 #define opd_pre_used_fid		opd_pre->osp_pre_used_fid

lustre/tests/conf-sanity.sh
@@ -5585,6 +5585,7 @@ test_69() {
 	start_ost || error "OST0 restart failure"
 	wait_osc_import_state mds ost FULL
 
+	sleep $((TIMEOUT/2)) #object recreation requires some time
 	mount_client $MOUNT || error "mount client failed"
 	touch $DIR/$tdir/$tfile-last || error "create file after reformat"
 	local idx=$($LFS getstripe -i $DIR/$tdir/$tfile-last)
@@ -5593,7 +5594,7 @@ test_69() {
 	local iused=$($LFS df -i $MOUNT |
 		awk '/OST0000/ { print $3 }'; exit ${PIPESTATUS[0]})
 	log "On OST0, $iused used inodes rc=$?"
-	[ $iused -ge $((ost_max_pre/2 + 1000)) ] &&
+	[ $iused -ge $((ost_max_pre + 1000)) ] &&
 		error "OST replacement created too many inodes; $iused"
 	cleanup || error "cleanup failed with $?"
 }

lustre/tests/sanity.sh
@@ -3378,31 +3378,6 @@ test_27R() {
 }
 run_test 27R "test max_stripecount limitation when stripe count is set to -1"
 
-test_27S() {
-	(( $MDS1_VERSION >= $(version_code 2.14.54) )) ||
-		skip "Need MDS version at least 2.14.54"
-	[[ "$(facet_host mds1)" != "$(facet_host ost1)" ]] ||
-		skip "needs different host for mdt1 ost1"
-
-	local count=$(precreated_ost_obj_count 0 0)
-
-	echo "precreate count $count"
-	mkdir_on_mdt0 $DIR/$tdir || error "mkdir $tdir failed"
-	$LFS setstripe -i 0 -c 1 $DIR/$tdir || error "setstripe $tdir failed"
-	#define OBD_FAIL_OSP_GET_LAST_FID	0x2109
-	do_facet mds1 $LCTL set_param fail_loc=0x2109
-	#define OBD_FAIL_OST_GET_LAST_FID	0x252
-	do_facet ost1 $LCTL set_param fail_loc=0x252
-	createmany -o $DIR/$tdir/f $count &
-	pid=$!
-	echo "precreate count $(precreated_ost_obj_count 0 0)"
-	do_facet mds1 $LCTL set_param fail_loc=0
-	do_facet ost1 $LCTL set_param fail_loc=0
-	wait $pid || error "createmany failed"
-	echo "precreate count $(precreated_ost_obj_count 0 0)"
-}
-run_test 27S "don't deactivate OSP on network issue"
-
 test_27T() {
 	[ $(facet_host client) == $(facet_host ost1) ] &&
 		skip "need ost1 and client on different nodes"
@@ -28367,34 +28342,6 @@ test_820() {
 	[ $ret -eq 0 ] || error "Failed to copy files to mds$n"
 }
 run_test 820 "update max EA from open intent"
-
-test_822() {
-	local p="$TMP/$TESTSUITE-$TESTNAME.parameters"
-
-	save_lustre_params mds1 \
-		"osp.$FSNAME-OST*-osc-MDT0000.max_create_count" > $p
-	do_facet $SINGLEMDS "$LCTL set_param -n \
-			osp.$FSNAME-OST*MDT0000.max_create_count=0"
-	do_facet $SINGLEMDS "$LCTL set_param -n \
-			osp.$FSNAME-OST0000*MDT0000.max_create_count=20000"
-
-	# wait for statfs update to clear OS_STATFS_NOPRECREATE
-	local maxage=$(do_facet mds1 $LCTL get_param -n \
-		       osp.$FSNAME-OST0000*MDT0000.maxage)
-	sleep $((maxage + 1))
-
-	#define OBD_FAIL_NET_ERROR_RPC          0x532
-	do_facet mds1 "$LCTL set_param fail_loc=0x80000532 fail_val=5"
-
-	stack_trap "restore_lustre_params < $p; rm $p"
-
-	local count=$(do_facet $SINGLEMDS "lctl get_param -n \
-		      osp.$FSNAME-OST0000*MDT0000.create_count")
-	for i in $(seq 1 $count); do
-		touch $DIR/$tfile.${i} || error "touch failed"
-	done
-}
-run_test 822 "test precreate failure"
 
 test_823() {
 	local p="$TMP/$TESTSUITE-$TESTNAME.parameters"

commit 9ee1281060d0a00a9c5d715a9a6d9b99c27123ff

LU-15671 mds: do not send OST_CREATE transno interop
Send OST_CREATE RPCs from the MDS with no_resend and no_delay
when communicating with an old OST that does not support the
OBD_CONNECT2_REPLAY_RESEND.  Likewise, the OST should not reply
to the MDS RPC with rq_transno set, or this will trigger:

   osp_precreate_send() ASSERTION(req->rq_transno == 0) failed

This can be avoided if the MDS is upgraded before the OSS, but
will always be hit if OSS is upgraded first.

After 2.20.53 the MDS/OSS assume that this is always true, since
rolling upgrades are unsupported for larger version differences.

Test-Parameters: testgroup=rolling-upgrade-oss
Fixes: 63e17799a3 ("LU-8367 osp: enable replay for precreation request")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I1ab601a2f55540dd75cf24838f7cdb7f823ed42c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51056
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_export.h
@@ -44,8 +44,9 @@
 #include <linux/rhashtable.h>
 #include <linux/workqueue.h>
 
-#include <lprocfs_status.h>
 #include <uapi/linux/lustre/lustre_idl.h>
+#include <uapi/linux/lustre/lustre_ver.h>
+#include <lprocfs_status.h>
 #include <lustre_dlm.h>
 
 struct mds_client_data;
@@ -499,6 +500,29 @@ static inline int exp_connect_dom_lvb(struct obd_export *exp)
 {
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_DOM_LVB);
 }
+
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 20, 53, 0)
+/* Only needed for interop with older MDS and 2.16+ OSS for rolling upgrade.
+ * This is typically unsupported for long periods, especially between large
+ * large version differences, so assume this is always true in the future
+ * and the OBD_CONNECT2_REPLAY_CREATE flag can be removed/reused in 2.21+.
+ */
+static inline bool exp_connect_replay_create(struct obd_export *exp)
+{
+	return exp_connect_flags2(exp) & OBD_CONNECT2_REPLAY_CREATE;
+}
+
+static inline bool imp_connect_replay_create(struct obd_import *imp)
+{
+	struct obd_connect_data *ocd = &imp->imp_connect_data;
+
+	return (ocd->ocd_connect_flags & OBD_CONNECT_FLAGS2) &&
+		(ocd->ocd_connect_flags2 & OBD_CONNECT2_REPLAY_CREATE);
+}
+#else
+#define exp_connect_replay_create(exp) true
+#define imp_connect_replay_create(exp) true
+#endif
 
 enum {
 	/* archive_ids in array format */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -846,6 +846,8 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_ENCRYPT_NAME	  0x8000000ULL /* name encrypt */
 #define OBD_CONNECT2_DMV_IMP_INHERIT	 0x20000000ULL /* client handle DMV inheritance */
 #define OBD_CONNECT2_ENCRYPT_FID2PATH	 0x40000000ULL /* fid2path enc file */
+/* For MDS+OSS rolling upgrade interop with 2.16+older, ignored after 2.20.53 */
+#define OBD_CONNECT2_REPLAY_CREATE	 0x80000000ULL /* replay OST_CREATE */
 /* XXX README XXX README XXX README XXX README XXX README XXX README XXX
  * Please DO NOT add OBD_CONNECT flags before first ensuring that this value
  * is not in use by some other branch/patch.  Email adilger@whamcloud.com
@@ -937,7 +939,8 @@ struct ptlrpc_body_v2 {
 
 #define OST_CONNECT_SUPPORTED2 (OBD_CONNECT2_LOCKAHEAD | OBD_CONNECT2_INC_XID |\
 				OBD_CONNECT2_ENCRYPT | OBD_CONNECT2_LSEEK |\
-				OBD_CONNECT2_REP_MBITS)
+				OBD_CONNECT2_REP_MBITS |\
+				OBD_CONNECT2_REPLAY_CREATE)
 
 #define ECHO_CONNECT_SUPPORTED (OBD_CONNECT_FID | OBD_CONNECT_FLAGS2)
 #define ECHO_CONNECT_SUPPORTED2 OBD_CONNECT2_REP_MBITS

lustre/lod/lod_lov.c
@@ -175,7 +175,8 @@ int lod_add_device(const struct lu_env *env, struct lod_device *lod,
 	if (data == NULL)
 		GOTO(out_cleanup, rc = -ENOMEM);
 
-	data->ocd_connect_flags = OBD_CONNECT_INDEX | OBD_CONNECT_VERSION;
+	data->ocd_connect_flags = OBD_CONNECT_INDEX | OBD_CONNECT_VERSION |
+				  OBD_CONNECT_FLAGS2;
 	data->ocd_version = LUSTRE_VERSION_CODE;
 	data->ocd_index = index;
 
@@ -196,6 +197,7 @@ int lod_add_device(const struct lu_env *env, struct lod_device *lod,
 					   OBD_CONNECT_PINGLESS |
 					   OBD_CONNECT_LFSCK |
 					   OBD_CONNECT_BULK_MBITS;
+		data->ocd_connect_flags2 = OBD_CONNECT2_REPLAY_CREATE;
 
 		data->ocd_group = tgt_index;
 		ltd = &lod->lod_ost_descs;

lustre/obdclass/lprocfs_status.c
@@ -651,6 +651,7 @@ static const char *const obd_connect_names[] = {
 	"mkdir_replay",			/* 0x10000000 */
 	"dmv_imp_inherit",		/* 0x20000000 */
 	"encryption_fid2path",		/* 0x40000000 */
+	"replay_create",		/* 0x80000000 */
 	NULL
 };
 

lustre/ofd/ofd_dev.c
@@ -1605,6 +1605,7 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 	}
 	if (diff > 0) {
 		time64_t enough_time = ktime_get_seconds() + DISK_TIMEOUT;
+		bool trans_local;
 		u64 next_id;
 		int created = 0;
 		int count;
@@ -1644,7 +1645,7 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 			}
 		}
 
-
+		trans_local = !exp_connect_replay_create(req->rq_export);
 		while (diff > 0) {
 			next_id = ofd_seq_last_oid(oseq) + 1;
 			count = ofd_precreate_batch(ofd, (int)diff);
@@ -1663,7 +1664,8 @@ static int ofd_create_hdl(struct tgt_session_info *tsi)
 			}
 
 			rc = ofd_precreate_objects(tsi->tsi_env, ofd, next_id,
-						   oseq, count, sync_trans);
+						   oseq, count, sync_trans,
+						   trans_local);
 			if (rc > 0) {
 				created += rc;
 				diff -= rc;

lustre/ofd/ofd_internal.h
@@ -382,7 +382,8 @@ int ofd_object_ff_load(const struct lu_env *env, struct ofd_object *fo);
 int ofd_object_ff_update(const struct lu_env *env, struct ofd_object *fo,
 			 const struct obdo *oa, struct filter_fid *ff);
 int ofd_precreate_objects(const struct lu_env *env, struct ofd_device *ofd,
-			  u64 id, struct ofd_seq *oseq, int nr, int sync);
+			  u64 id, struct ofd_seq *oseq, int nr, int sync,
+			  bool trans_local);
 
 static inline void ofd_object_put(const struct lu_env *env,
 				  struct ofd_object *fo)

lustre/ofd/ofd_io.c
@@ -748,7 +748,8 @@ static int ofd_preprw_write(const struct lu_env *env, struct obd_export *exp,
 				int count = ofd_precreate_batch(ofd, diff);
 
 				rc = ofd_precreate_objects(env, ofd, next_id,
-							   oseq, count, sync);
+							   oseq, count, sync,
+							   false);
 				if (rc < 0) {
 					mutex_unlock(&oseq->os_create_lock);
 					ofd_seq_put(env, oseq);

lustre/ofd/ofd_obd.c
@@ -1079,7 +1079,7 @@ static int ofd_echo_create(const struct lu_env *env, struct obd_export *exp,
 	next_id = ofd_seq_last_oid(oseq) + 1;
 	count = ofd_precreate_batch(ofd, (int)diff);
 
-	rc = ofd_precreate_objects(env, ofd, next_id, oseq, count, 0);
+	rc = ofd_precreate_objects(env, ofd, next_id, oseq, count, 0, false);
 	if (rc < 0) {
 		CERROR("%s: unable to precreate: rc = %d\n",
 		       ofd_name(ofd), rc);

lustre/ofd/ofd_objects.c
@@ -41,6 +41,7 @@
 
 #include <dt_object.h>
 #include <lustre_lfsck.h>
+#include <lustre_export.h>
 
 #include "ofd_internal.h"
 
@@ -236,18 +237,20 @@ static int ofd_precreate_cb_add(const struct lu_env *env, struct thandle *th,
  * update the inode. The ctime = 0 case is also handled specially in
  * osd_inode_setattr(). See LU-221, LU-1042 for details.
  *
- * \param[in] env	execution environment
- * \param[in] ofd	OFD device
- * \param[in] id	object ID to start precreation from
- * \param[in] oseq	object sequence
- * \param[in] nr	number of objects to precreate
- * \param[in] sync	synchronous precreation flag
+ * \param[in] env		execution environment
+ * \param[in] ofd		OFD device
+ * \param[in] id		object ID to start precreation from
+ * \param[in] oseq		object sequence
+ * \param[in] nr		number of objects to precreate
+ * \param[in] sync		synchronous precreation flag
+ * \param[in] trans_local	start local transaction
  *
  * \retval		0 if successful
  * \retval		negative value on error
  */
 int ofd_precreate_objects(const struct lu_env *env, struct ofd_device *ofd,
-			  u64 id, struct ofd_seq *oseq, int nr, int sync)
+			  u64 id, struct ofd_seq *oseq, int nr, int sync,
+			  bool trans_local)
 {
 	struct ofd_thread_info	*info = ofd_info(env);
 	struct ofd_object	*fo = NULL;
@@ -359,7 +362,11 @@ int ofd_precreate_objects(const struct lu_env *env, struct ofd_device *ofd,
 		}
 	}
 
-	rc = dt_trans_start(env, ofd->ofd_osd, th);
+	/* Only needed for MDS+OSS rolling upgrade interop with 2.16+older. */
+	if (unlikely(trans_local))
+		rc = dt_trans_start_local(env, ofd->ofd_osd, th);
+	else
+		rc = dt_trans_start(env, ofd->ofd_osd, th);
 	if (rc)
 		GOTO(trans_stop, rc);
 

lustre/osp/osp_precreate.c
@@ -632,6 +632,13 @@ static int osp_precreate_send(const struct lu_env *env, struct osp_device *d)
 		RETURN(-ENOMEM);
 	req->rq_request_portal = OST_CREATE_PORTAL;
 
+	/* We should not resend create request - anyway we will have delorphan
+	 * and kill these objects.
+	 * Only needed for MDS+OSS rolling upgrade interop with 2.16+older.
+	 */
+	if (unlikely(!imp_connect_replay_create(imp)))
+		req->rq_no_delay = req->rq_no_resend = 1;
+
 	/* Delorphan happens only with a first MDT-OST connect. resend/replay
 	 * handles objects creation on reconnects, no need to do delorhpan
 	 * in this case.
@@ -859,7 +866,7 @@ static int osp_precreate_cleanup_orphans(struct lu_env *env,
 	struct osp_thread_info	*osi = osp_env_info(env);
 	struct lu_fid		*last_fid = &osi->osi_fid;
 	struct ptlrpc_request	*req = NULL;
-	struct obd_import	*imp;
+	struct obd_import	*imp = d->opd_obd->u.cli.cl_import;
 	struct ost_body		*body;
 	int			 update_status = 0;
 	int			 rc;
@@ -872,7 +879,7 @@ static int osp_precreate_cleanup_orphans(struct lu_env *env,
 	 * all precreate requests uses resend/replay flags to support OST
 	 * failover/reconnect.
 	 */
-	if (d->opd_cleanup_orphans_done) {
+	if (d->opd_cleanup_orphans_done && imp_connect_replay_create(imp)) {
 		rc = osp_get_lastfid_from_ost(env, d, false);
 		RETURN(0);
 	}

lustre/ptlrpc/wiretest.c
@@ -1265,6 +1265,7 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct obd_connect_data, paddingF));
 	LASSERTF((int)sizeof(((struct obd_connect_data *)0)->paddingF) == 8, "found %lld\n",
 		 (long long)(int)sizeof(((struct obd_connect_data *)0)->paddingF));
+
 	LASSERTF(OBD_CONNECT_RDONLY == 0x1ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT_RDONLY);
 	LASSERTF(OBD_CONNECT_INDEX == 0x2ULL, "found 0x%.16llxULL\n",
@@ -1387,6 +1388,7 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT_OBDOPACK);
 	LASSERTF(OBD_CONNECT_FLAGS2 == 0x8000000000000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT_FLAGS2);
+
 	LASSERTF(OBD_CONNECT2_FILE_SECCTX == 0x1ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_FILE_SECCTX);
 	LASSERTF(OBD_CONNECT2_LOCKAHEAD == 0x2ULL, "found 0x%.16llxULL\n",
@@ -1443,6 +1445,9 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT2_DMV_IMP_INHERIT);
 	LASSERTF(OBD_CONNECT2_ENCRYPT_FID2PATH == 0x40000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_ENCRYPT_FID2PATH);
+	LASSERTF(OBD_CONNECT2_REPLAY_CREATE == 0x80000000ULL, "found 0x%.16llxULL\n",
+		 OBD_CONNECT2_REPLAY_CREATE);
+
 	LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
 		(unsigned)OBD_CKSUM_CRC32);
 	LASSERTF(OBD_CKSUM_ADLER == 0x00000002UL, "found 0x%.8xUL\n",

lustre/utils/wirecheck.c
@@ -591,6 +591,7 @@ check_obd_connect_data(void)
 	CHECK_MEMBER(obd_connect_data, paddingE);
 	CHECK_MEMBER(obd_connect_data, paddingF);
 
+	BLANK_LINE();
 	CHECK_DEFINE_64X(OBD_CONNECT_RDONLY);
 	CHECK_DEFINE_64X(OBD_CONNECT_INDEX);
 	CHECK_DEFINE_64X(OBD_CONNECT_MDS);
@@ -652,6 +653,7 @@ check_obd_connect_data(void)
 	CHECK_DEFINE_64X(OBD_CONNECT_BULK_MBITS);
 	CHECK_DEFINE_64X(OBD_CONNECT_OBDOPACK);
 	CHECK_DEFINE_64X(OBD_CONNECT_FLAGS2);
+	BLANK_LINE();
 	CHECK_DEFINE_64X(OBD_CONNECT2_FILE_SECCTX);
 	CHECK_DEFINE_64X(OBD_CONNECT2_LOCKAHEAD);
 	CHECK_DEFINE_64X(OBD_CONNECT2_DIR_MIGRATE);
@@ -680,7 +682,9 @@ check_obd_connect_data(void)
 	CHECK_DEFINE_64X(OBD_CONNECT2_ENCRYPT_NAME);
 	CHECK_DEFINE_64X(OBD_CONNECT2_DMV_IMP_INHERIT);
 	CHECK_DEFINE_64X(OBD_CONNECT2_ENCRYPT_FID2PATH);
+	CHECK_DEFINE_64X(OBD_CONNECT2_REPLAY_CREATE);
 
+	BLANK_LINE();
 	CHECK_VALUE_X(OBD_CKSUM_CRC32);
 	CHECK_VALUE_X(OBD_CKSUM_ADLER);
 	CHECK_VALUE_X(OBD_CKSUM_CRC32C);

lustre/utils/wiretest.c
@@ -1413,6 +1413,7 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT_OBDOPACK);
 	LASSERTF(OBD_CONNECT_FLAGS2 == 0x8000000000000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT_FLAGS2);
+
 	LASSERTF(OBD_CONNECT2_FILE_SECCTX == 0x1ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_FILE_SECCTX);
 	LASSERTF(OBD_CONNECT2_LOCKAHEAD == 0x2ULL, "found 0x%.16llxULL\n",
@@ -1469,6 +1470,9 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT2_DMV_IMP_INHERIT);
 	LASSERTF(OBD_CONNECT2_ENCRYPT_FID2PATH == 0x40000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_ENCRYPT_FID2PATH);
+	LASSERTF(OBD_CONNECT2_REPLAY_CREATE == 0x80000000ULL, "found 0x%.16llxULL\n",
+		 OBD_CONNECT2_REPLAY_CREATE);
+
 	LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
 		(unsigned)OBD_CKSUM_CRC32);
 	LASSERTF(OBD_CKSUM_ADLER == 0x00000002UL, "found 0x%.8xUL\n",