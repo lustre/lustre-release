lustre/lustre-release

commit bdf7788d19985bb7abf2385add15f1d67f3d01e4

LU-8585 llapi: use open_by_handle_at in llapi_open_by_fid
Reimplement llapi_open_by_fid() to use llapi_fid_to_handle() and
open_by_handle_at(2) rather than using ioctl().  This works for
opens on subdirectory mountpoints, unlike ".lustre/fid/<fid>".

This patch also adds llapi_open_by_fid_at() which is similar to
llapi_open_by_fid() except that it takes an open directory file
descriptor or AT_CWD rather than a path as its first argument.

[AD:
- Move get_root_*() functions over to a new liblustreapi_root.c
  file in expectation of further enhancements to that code.
- Cache an open file handle on the root directory so repeated
  calls to llapi_open_by_fid() and llapi_fid2path() do not need
  to search for and open the same root directory path many times.
- Add man pages for newly-added functions.

  This reduces the system calls for llapi_fid_test significantly:

      original     patched
         14511        4315   total opens
         64807       34067   total syscalls
]

There may still be a need to have a fallback from open_by_handle_at()
to using ".lustre/fid/<FID>" to open the fid (if available), but
that can be added if this initial patch does not test well.  The
open_by_handle_at() method avoids reopening the "fid/" directory
each time (though this fd could also be cached), but it has the
drawback that it reconnects dentries to the root directory each time.

Signed-off-by: Quentin Bouget <quentin.bouget@cea.fr>
Change-Id: I8a4904c996389da2b0894cd9fac639a398607535
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/36603
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi_root.c
@@ -1,0 +1,422 @@
+/*
+ * LGPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 or (at your discretion) any later version.
+ * (LGPL) version 2.1 accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * LGPL HEADER END
+ */
+/*
+ * lustre/utils/liblustreapi_root.c
+ *
+ * lustreapi library for managing the root fd cache for llapi internal use.
+ *
+ * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Copyright (c) 2011, 2017, Intel Corporation.
+ *
+ * Copyright (c) 2018, 2022, Data Direct Networks
+ */
+
+/* for O_DIRECTORY and struct file_handle */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h> /* for dirname() */
+#include <mntent.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h> /* for makedev() */
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <libcfs/util/ioctl.h>
+#include <lustre/lustreapi.h>
+#include <linux/lustre/lustre_fid.h>
+#include "lustreapi_internal.h"
+
+/* could have an array of these for a handful of different paths */
+static struct root_cache {
+	dev_t	dev;
+	char	fsname[PATH_MAX];
+	char	mnt_dir[PATH_MAX];
+	char	nid[MAX_LINE_LEN];
+	int	fd; /* cached fd on filesystem root for internal use only */
+} root_cached = { 0 };
+
+static pthread_rwlock_t root_cached_lock = PTHREAD_RWLOCK_INITIALIZER;
+
+static int get_root_fd(const char *rootpath, int *outfd)
+{
+	int rc = 0;
+	int fd;
+
+	fd = open(rootpath, O_RDONLY | O_DIRECTORY | O_NONBLOCK);
+	if (fd < 0) {
+		rc = -errno;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "cannot open '%s'", rootpath);
+	} else {
+		*outfd = fd;
+	}
+
+	return rc;
+}
+
+static int get_file_dev(const char *path, dev_t *dev)
+{
+#ifdef HAVE_STATX
+	struct statx stx;
+
+	if (!dev)
+		return -EINVAL;
+	if (statx(AT_FDCWD, path, 0, 0, &stx))
+		return -errno;
+	*dev = makedev(stx.stx_dev_major, stx.stx_dev_minor);
+#else
+	struct stat st;
+
+	if (!dev)
+		return -EINVAL;
+	if (stat(path, &st) != 0)
+		return -errno;
+
+	*dev = st.st_dev;
+#endif
+	return 0;
+}
+
+static int get_root_path_fast(int want, char *fsname, int *outfd, char *path,
+			      dev_t *dev, char *nid)
+{
+	int rc = -ENODEV;
+	int fsnamelen;
+	int mntlen;
+
+	if (root_cached.dev == 0)
+		return rc;
+
+	/* hold a write lock on the cache if fd is going to be updated */
+	if ((want & WANT_FD) && outfd && root_cached.fd <= 0)
+		pthread_rwlock_wrlock(&root_cached_lock);
+	else
+		pthread_rwlock_rdlock(&root_cached_lock);
+
+	if (root_cached.dev == 0)
+		goto out_unlock;
+
+	fsnamelen = strlen(root_cached.fsname);
+	mntlen = strlen(root_cached.mnt_dir);
+
+	/* Check the dev for a match, if given */
+	if (!(want & WANT_DEV) && dev && *dev == root_cached.dev) {
+		rc = 0;
+	/* Check the fsname for a match, if given */
+	} else if (!(want & WANT_FSNAME) && fsname &&
+		   strlen(fsname) == fsnamelen &&
+		   (strncmp(root_cached.fsname, fsname, fsnamelen) == 0)) {
+		rc = 0;
+	/* Otherwise find the longest matching path */
+	} else if (path && strlen(path) >= mntlen &&
+		   (strncmp(root_cached.mnt_dir, path, mntlen) == 0) &&
+		   (strlen(path) == mntlen || path[mntlen] == '/')) {
+		rc = 0;
+	}
+
+	if (rc)
+		goto out_unlock;
+
+	if ((want & WANT_FSNAME) && fsname)
+		strcpy(fsname, root_cached.fsname);
+	if ((want & WANT_PATH) && path)
+		strcpy(path, root_cached.mnt_dir);
+	if ((want & WANT_DEV) && dev)
+		*dev = root_cached.dev;
+	if ((want & WANT_FD) && outfd) {
+		if (root_cached.fd > 0) {
+			*outfd = root_cached.fd;
+		} else {
+			rc = get_root_fd(root_cached.mnt_dir, outfd);
+			if (!rc)
+				root_cached.fd = *outfd;
+		}
+	}
+	if ((want & WANT_NID) && nid)
+		strcpy(nid, root_cached.nid);
+out_unlock:
+	pthread_rwlock_unlock(&root_cached_lock);
+
+	return rc;
+}
+
+static int get_root_path_slow(int want, char *fsname, int *outfd, char *path,
+			      int index, dev_t *dev, char *nid)
+{
+	struct mntent mnt;
+	char buf[PATH_MAX];
+	char *ptr, *ptr_end;
+	FILE *fp;
+	int idx = -1, mntlen = 0;
+	int rc = -ENODEV;
+	int fsnamelen = 0;
+	dev_t devmnt = 0;
+
+	/* get the mount point */
+	fp = setmntent(PROC_MOUNTS, "r");
+	if (fp == NULL) {
+		rc = -EIO;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "cannot retrieve filesystem mount point");
+		return rc;
+	}
+	while (getmntent_r(fp, &mnt, buf, sizeof(buf))) {
+
+		if (!llapi_is_lustre_mnt(&mnt))
+			continue;
+
+		idx++;
+		mntlen = strlen(mnt.mnt_dir);
+		ptr = strchr(mnt.mnt_fsname, '/');
+		while (ptr && *ptr == '/')
+			ptr++;
+		/*
+		 * thanks to the call to llapi_is_lustre_mnt() above,
+		 * we are sure that mnt.mnt_fsname contains ":/",
+		 * so ptr should never be NULL
+		 */
+		if (ptr == NULL)
+			continue;
+		ptr_end = ptr;
+		while (*ptr_end != '/' && *ptr_end != '\0')
+			ptr_end++;
+
+		fsnamelen = ptr_end - ptr;
+
+		/* ignore unaccessible filesystem */
+		if (get_file_dev(mnt.mnt_dir, &devmnt))
+			continue;
+
+		if ((want & WANT_INDEX) && idx == index) {
+			rc = 0;
+			break;
+		}
+
+		/* Check the fsname for a match, if given */
+		if (!(want & WANT_FSNAME) && fsname &&
+		    strlen(fsname) == fsnamelen &&
+		    (strncmp(ptr, fsname, fsnamelen) == 0)) {
+			rc = 0;
+			break;
+		}
+
+		/* Check the dev for a match, if given */
+		if (!(want & WANT_DEV) && dev && *dev == devmnt) {
+			rc = 0;
+			break;
+		}
+
+		/* Otherwise find the longest matching path */
+		if (path && strlen(path) >= mntlen &&
+		    (strncmp(mnt.mnt_dir, path, mntlen) == 0) &&
+		    (strlen(path) == mntlen || path[mntlen] == '/')) {
+			rc = 0;
+			break;
+		}
+	}
+
+	if (rc)
+		goto out;
+
+	/* Found it */
+	if (!(want & WANT_INDEX)) {
+		/* Cache the mount point information */
+		pthread_rwlock_wrlock(&root_cached_lock);
+
+		if (root_cached.fd > 0) {
+			close(root_cached.fd);
+			root_cached.fd = 0;
+		}
+		if ((want & WANT_FD) && outfd)
+			rc = get_root_fd(mnt.mnt_dir, &root_cached.fd);
+		strncpy(root_cached.fsname, ptr, fsnamelen);
+		root_cached.fsname[fsnamelen] = '\0';
+		strncpy(root_cached.mnt_dir, mnt.mnt_dir, mntlen);
+		root_cached.mnt_dir[mntlen] = '\0';
+		root_cached.dev = devmnt;
+		ptr_end = strchr(mnt.mnt_fsname, ':');
+		strncpy(root_cached.nid, mnt.mnt_fsname,
+			ptr_end - mnt.mnt_fsname);
+		root_cached.nid[ptr_end - mnt.mnt_fsname] = '\0';
+
+		pthread_rwlock_unlock(&root_cached_lock);
+	}
+
+	if ((want & WANT_FSNAME) && fsname) {
+		strncpy(fsname, ptr, fsnamelen);
+		fsname[fsnamelen] = '\0';
+	}
+	if ((want & WANT_PATH) && path) {
+		strncpy(path, mnt.mnt_dir, mntlen);
+		path[mntlen] = '\0';
+	}
+	if ((want & WANT_DEV) && dev)
+		*dev = devmnt;
+	if ((want & WANT_FD) && outfd) {
+		if (root_cached.fd > 0)
+			*outfd = root_cached.fd;
+		else
+			rc = get_root_fd(mnt.mnt_dir, outfd);
+	}
+	if ((want & WANT_NID) && nid) {
+		ptr_end = strchr(mnt.mnt_fsname, ':');
+		strncpy(nid, mnt.mnt_fsname, ptr_end - mnt.mnt_fsname);
+		nid[ptr_end - mnt.mnt_fsname] = '\0';
+	}
+
+out:
+	endmntent(fp);
+	return rc;
+}
+
+/*
+ * Find the fsname, the full path, and/or an open fd.
+ * Either the fsname or path must not be NULL.
+ *
+ * @outfd is for llapi internal use only, do not return it to the application.
+ */
+int get_root_path(int want, char *fsname, int *outfd, char *path, int index,
+		  dev_t *dev, char *nid)
+{
+	int rc = -ENODEV;
+
+	if (!(want & WANT_INDEX))
+		rc = get_root_path_fast(want, fsname, outfd, path, dev, nid);
+	if (rc)
+		rc = get_root_path_slow(want, fsname, outfd, path, index, dev,
+					nid);
+
+	if (!rc || !(want & WANT_ERROR))
+		goto out_errno;
+
+	if (dev || !(want & WANT_DEV))
+		llapi_err_noerrno(LLAPI_MSG_ERROR,
+				  "'%u/%u' dev not on a mounted Lustre filesystem",
+				  major(*dev), minor(*dev));
+	else
+		llapi_err_noerrno(LLAPI_MSG_ERROR,
+				  "'%s' not on a mounted Lustre filesystem",
+				  (want & WANT_PATH) ? fsname : path);
+out_errno:
+	errno = -rc;
+	return rc;
+}
+/*
+ * search lustre mounts
+ *
+ * Calling this function will return to the user the mount point, mntdir, and
+ * the file system name, fsname, if the user passed a buffer to this routine.
+ *
+ * The user inputs are pathname and index. If the pathname is supplied then
+ * the value of the index will be ignored. The pathname will return data if
+ * the pathname is located on a lustre mount. Index is used to pick which
+ * mount point you want in the case of multiple mounted lustre file systems.
+ * See function lfs_osts in lfs.c for an example of the index use.
+ */
+int llapi_search_mounts(const char *pathname, int index, char *mntdir,
+			char *fsname)
+{
+	int want = WANT_PATH, idx = -1;
+
+	if (!pathname || pathname[0] == '\0') {
+		want |= WANT_INDEX;
+		idx = index;
+	} else {
+		strcpy(mntdir, pathname);
+	}
+
+	if (fsname)
+		want |= WANT_FSNAME;
+	return get_root_path(want, fsname, NULL, mntdir, idx, NULL, NULL);
+}
+
+/* Given a path, find the corresponding Lustre fsname */
+int llapi_search_fsname(const char *pathname, char *fsname)
+{
+	dev_t dev;
+	int rc;
+
+	rc = get_file_dev(pathname, &dev);
+	if (rc) {
+		char tmp[PATH_MAX];
+		char *parent;
+		int len;
+
+		/* file does not exist try the parent */
+		len = readlink(pathname, tmp, PATH_MAX);
+		if (len != -1)
+			tmp[len] = '\0';
+		else
+			strncpy(tmp, pathname, PATH_MAX - 1);
+
+		parent = dirname(tmp);
+		rc = get_file_dev(parent, &dev);
+	}
+
+	if (rc) {
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "cannot resolve path '%s'", pathname);
+		return rc;
+	}
+
+	rc = get_root_path(WANT_FSNAME | WANT_ERROR, fsname, NULL, NULL, -1,
+			   &dev, NULL);
+
+	return rc;
+}
+
+
+int llapi_search_rootpath(char *pathname, const char *fsname)
+{
+	if (!pathname)
+		return -EINVAL;
+
+	/*
+	 * pathname can be used as an argument by get_root_path(),
+	 * clear it for safety
+	 */
+	pathname[0] = 0;
+	return get_root_path(WANT_PATH, (char *)fsname, NULL, pathname, -1,
+			     NULL, NULL);
+}
+
+int llapi_search_rootpath_by_dev(char *pathname, dev_t dev)
+{
+	if (!pathname)
+		return -EINVAL;
+
+	/*
+	 * pathname can be used as an argument by get_root_path(),
+	 * clear it for safety
+	 */
+	pathname[0] = 0;
+	return get_root_path(WANT_PATH, NULL, NULL, pathname, -1, &dev, NULL);
+}
+

lustre/doc/Makefile.am
@@ -101,6 +101,7 @@ LIBMAN = 					\
 	llapi_create_volatile_param.3		\
 	llapi_fd2parent.3			\
 	llapi_fid_parse.3			\
+	llapi_fid_to_handle.3			\
 	llapi_file_create.3			\
 	llapi_file_create_foreign.3		\
 	llapi_file_get_stripe.3			\
@@ -162,6 +163,8 @@ LIBMAN = 					\
 	llapi_layout_stripe_count_set.3		\
 	llapi_layout_stripe_size_get.3		\
 	llapi_layout_stripe_size_set.3		\
+	llapi_open_by_fid.3			\
+	llapi_open_by_fid_at.3			\
 	llapi_param_get_paths.3			\
 	llapi_param_get_value.3			\
 	llapi_path2fid.3			\

lustre/utils/Makefile.am
@@ -109,6 +109,7 @@ liblustreapi_la_SOURCES = liblustreapi.c liblustreapi_hsm.c \
 			  liblustreapi_mirror.c liblustreapi_fid.c \
 			  liblustreapi_ladvise.c liblustreapi_chlg.c \
 			  liblustreapi_heat.c liblustreapi_pcc.c \
+			  liblustreapi_root.c \
 			  liblustreapi_lseek.c liblustreapi_swap.c
 liblustreapi_la_LDFLAGS = $(LIBREADLINE) -version-info 1:0:0 \
 			  -Wl,--version-script=liblustreapi.map

lustre/doc/llapi_fid_to_handle.3
@@ -1,0 +1,48 @@
+.TH llapi_fid_to_handle 3 "2022-11-08" "Lustre User API"
+.SH NAME
+llapi_fid_to_handle \- allocate an NFS handle for File Identifier
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_fid_to_handle(struct file_handle **" handle ", struct lu_fid *" fid ");"
+.fi
+.SH DESCRIPTION
+.PP
+.BR llapi_fid_to_handle()
+allocates an NFS file
+.I handle
+for the File Identifier given by
+.I fid
+for later use by
+.BR open_by_handle_at (2).
+This is similar to the
+.BR name_to_handle_at (2)
+function, and is useful for Lustre-specific tools and interfaces.  While the
+.BR llapi_fid_to_handle (3)
+function does not requre root access itself, the
+.BR open_by_handle_at (2)
+function
+.B does
+require root access since this bypasses all pathname-derived file access
+permissions.
+The memory allocated for
+.I handle
+and returned to the caller must be freed with
+.IR free (2).
+.SH RETURN VALUES
+.LP
+returns 0 on success or a negative errno on failure.
+.SH ERRORS
+.TP 15
+.SM -ENOMEM
+No memory was available for allocating
+.IR handle .
+.SH SEE ALSO
+.BR free (3),
+.BR llapi_fid_parse (3),
+.BR llapi_fid2path (3),
+.BR llapi_path2fid (3),
+.BR lustreapi (7),
+.BR name_to_handle (2)
+.BR open_by_handle_at (2)

lustre/doc/llapi_open_by_fid.3
@@ -1,0 +1,51 @@
+.TH llapi_open_by_fid 3 "2022-11-08" "Lustre User API"
+.SH NAME
+llapi_open_by_fid \- open a file descriptor from a File Identifier
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_open_by_fid(const char *" mntpath ", struct lu_fid *" fid ", int " oflag ");"
+.BI "int llapi_open_by_fid_at(int " dir_fd ", struct lu_fid *" fid ", int " oflag ");"
+.fi
+.SH DESCRIPTION
+.PP
+.BR llapi_open_by_fid()
+opens a file descriptor in the filesystem mountpoint referenced by
+.I path
+for the file identified by
+.IR fid  ,
+using the open file flags in
+.IR oflag
+(as with the normal
+.BR open (2)
+call).
+.PP
+.BR llapi_open_by_fid_at()
+opens a file descriptor on
+.I fid
+in the filesystem referenced by the open file descriptor
+.IR dir_fd .
+This is similar to the
+.IR open_by_handle_at (3)
+system call that can open files by their NFS file handle.  If
+.I dir_fd
+is
+.B AT_CWD
+then the open is performed against the local working directory.
+.SH RETURN VALUES
+.LP
+returns file descriptor number or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM -EINVAL
+An invalid argument was specified.
+.TP 15
+.SM -ENOMEM
+No memory was available during operation.
+.SH SEE ALSO
+.BR llapi_fid_parse (3),
+.BR llapi_fid2path (3),
+.BR llapi_path2fid (3),
+.BR lustreapi (7),
+.BR open_by_handle_at (3)

lustre/doc/llapi_open_by_fid_at.3
@@ -1,0 +1,1 @@
+.so man3/llapi_open_by_fid.3

lustre/include/lustre/lustreapi.h
@@ -513,7 +513,13 @@ int llapi_fd2parent(int fd, unsigned int linkno, struct lu_fid *parent_fid,
 		    char *name, size_t name_size);
 int llapi_rmfid(const char *path, struct fid_array *fa);
 int llapi_chomp_string(char *buf);
-int llapi_open_by_fid(const char *dir, const struct lu_fid *fid,
+
+struct file_handle;
+
+int llapi_handle_to_fid(struct file_handle **handle, const struct lu_fid *fid);
+int llapi_open_by_fid_at(int lustre_fd, const struct lu_fid *fid,
+			 int open_flags);
+int llapi_open_by_fid(const char *lustre_dir, const struct lu_fid *fid,
 		      int open_flags);
 int llapi_get_version_string(char *version, unsigned int version_size);
 /* llapi_get_version() is deprecated, use llapi_get_version_string() instead */

lustre/tests/sanity.sh
@@ -15886,7 +15886,6 @@ run_test 154f "get parent fids by reading link ea"
 
 test_154g()
 {
-	[ -n "$FILESET" ] && skip "SKIP due to FILESET set"
 	[[ $MDS1_VERSION -ge $(version_code 2.6.92) &&
 	   $CLIENT_VERSION -gt $(version_code 2.6.99) ]] ||
 		skip "Need MDS version at least 2.6.92"

commit 9ef1e097d53000233f9ba23319268f467c276173

LU-16747 llapi: fix race in get_root_path_slow()
The patch bdf7788d ("LU-8585 llapi: use open_by_handle_at in
llapi_open_by_fid") caches the Lustre root fd to avoid re-openning
it each time an ioctl() is needed on the fs.

For now, only 1 entry is stored. If a llapi call is performed on
another mountpoint, llapi needs to close the old root fd and open a
new one.

A race condition exists at startup, when root_cached.fd is not
initialized yet. Several threads try to determine root information at
the same time (in get_root_path_slow()). Those threads will close(),
open() and update different "root_cached.fd".
The usage of a closed root fd will return EBADFD (e.g: in
llapi_open_by_fid(), llapi_hsm_request() or llapi_fid2path()).

This patch checks if the fs is the same before updating the root
entry. If so, the root entry (and cached root fd) will not be changed.

Add the regresion test sanityn 85 (llapi_root_test).

Test-Parameters: trivial testlist=sanityn env=ONLY=85,ONLY_REPEAT=20
Test-Parameters: testlist=sanityn
Test-Parameters: testlist=sanity
Fixes: bdf7788d ("LU-8585 llapi: use open_by_handle_at in llapi_open_by_fid")
Signed-off-by: Etienne AUJAMES <eaujames@ddn.com>
Change-Id: I681aac7d5715022e700cdb092db94deaa6bf6a8f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50682
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi_root.c
@@ -250,6 +250,10 @@ static int get_root_path_slow(int want, char *fsname, int *outfd, char *path,
 		/* Cache the mount point information */
 		pthread_rwlock_wrlock(&root_cached_lock);
 
+		/* If the entry matches the saved one -> no update needed */
+		if (strcmp(root_cached.mnt_dir, mnt.mnt_dir) == 0)
+			goto unlock_root_cached;
+
 		if (root_cached.fd > 0) {
 			close(root_cached.fd);
 			root_cached.fd = 0;
@@ -266,6 +270,7 @@ static int get_root_path_slow(int want, char *fsname, int *outfd, char *path,
 			ptr_end - mnt.mnt_fsname);
 		root_cached.nid[ptr_end - mnt.mnt_fsname] = '\0';
 
+unlock_root_cached:
 		pthread_rwlock_unlock(&root_cached_lock);
 	}
 

lustre/tests/.gitignore
@@ -17,6 +17,7 @@
 /llapi_fid_test
 /llapi_hsm_test
 /llapi_layout_test
+/llapi_root_test
 /createdestroy
 /createmany
 /createtest

lustre/tests/Makefile.am
@@ -84,7 +84,7 @@ THETESTS += create_foreign_file parse_foreign_file
 THETESTS += create_foreign_dir parse_foreign_dir
 THETESTS += check_fallocate splice-test lseek_test expand_truncate_test
 THETESTS += foreign_symlink_striping lov_getstripe_old io_uring_probe
-THETESTS += fadvise_dontneed_helper
+THETESTS += fadvise_dontneed_helper llapi_root_test
 
 if LIBAIO
 THETESTS += aiocp
@@ -121,6 +121,7 @@ llapi_layout_test_LDADD = $(LIBLUSTREAPI)
 llapi_hsm_test_LDADD = $(LIBLUSTREAPI)
 group_lock_test_LDADD = $(LIBLUSTREAPI)
 llapi_fid_test_LDADD = $(LIBLUSTREAPI)
+llapi_root_test_LDADD = $(LIBLUSTREAPI) -lpthread
 rw_seq_cst_vs_drop_caches_LDADD = $(PTHREAD_LIBS)
 sendfile_grouplock_LDADD = $(LIBLUSTREAPI)
 swap_lock_test_LDADD = $(LIBLUSTREAPI)

lustre/tests/llapi_root_test.c
@@ -1,0 +1,249 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+
+/*
+ * The purpose of this test is to check Lustre API root fd cache.
+ *
+ * The program will exit as soon as a non zero error code is returned.
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <pthread.h>
+
+
+#include <lustre/lustreapi.h>
+#include <linux/lustre/lustre_idl.h>
+
+#define ERROR(fmt, ...)							\
+	fprintf(stderr, "%s: %s:%d: %s: " fmt "\n",                     \
+		program_invocation_short_name, __FILE__, __LINE__,      \
+		__func__, ## __VA_ARGS__)
+
+#define DIE(fmt, ...)                  \
+	do {			       \
+		ERROR(fmt, ## __VA_ARGS__);	\
+		exit(EXIT_FAILURE);		\
+	} while (0)
+
+#define ASSERTF(cond, fmt, ...)						\
+	do {								\
+		if (!(cond))						\
+			DIE("assertion '%s' failed: "fmt,		\
+			    #cond, ## __VA_ARGS__);			\
+	} while (0)
+
+#define PERFORM(testfn) \
+	do {								\
+		cleanup();						\
+		fprintf(stderr, "Starting test " #testfn " at %lld\n",	\
+			(unsigned long long)time(NULL));		\
+		testfn();						\
+		fprintf(stderr, "Finishing test " #testfn " at %lld\n",	\
+		       (unsigned long long)time(NULL));			\
+		cleanup();						\
+	} while (0)
+
+/* Name of file/directory. Will be set once and will not change. */
+static char *mainpath;  /* path to file on mountpoint 1 */
+static char *mainpath2; /* path to file on mountpoint 2 */
+
+static char mnt_dir[PATH_MAX];	/* Lustre mountpoint 1 */
+static char mnt_dir2[PATH_MAX];	/* Lustre mountpoint 2 */
+static int mnt_fd = -1;
+static int mnt_fd2 = -1;
+
+/* Cleanup our test directory. */
+static void cleanup(void)
+{
+	int rc;
+
+	rc = remove(mainpath);
+	ASSERTF(!rc || errno == ENOENT,
+		"Failed to unlink %s: %s", mainpath, strerror(errno));
+}
+
+#define TEST1_THR_NBR 20
+void *test1_thr(void *arg)
+{
+	char *fidstr = arg;
+	char path[PATH_MAX];
+	long long recno = -1;
+	int linkno = 0;
+	long long rc;
+
+	rc = llapi_fid2path(mnt_dir2, fidstr, path,
+			    sizeof(path), &recno, &linkno);
+
+	return (void *) rc;
+}
+
+/* Race on root cache at startup */
+static void test1(void)
+{
+	static pthread_t thread[TEST1_THR_NBR];
+	int fd, i, iter;
+	long long rc;
+	struct lu_fid fid;
+	char fidstr[FID_LEN + 1];
+
+	fd = creat(mainpath, 00660);
+	ASSERTF(fd >= 0, "creat failed for '%s': %s",
+		mainpath, strerror(errno));
+
+	rc = llapi_fd2fid(fd, &fid);
+	ASSERTF(rc == 0, "llapi_fd2fid failed for '%s': %s",
+		mainpath, strerror(-rc));
+	close(fd);
+
+	snprintf(fidstr, sizeof(fidstr), DFID_NOBRACE, PFID(&fid));
+	for (iter = 0; iter < 100; iter++) {
+		/* reset cache on first mountpoint */
+		fd = llapi_open_by_fid(mnt_dir, &fid, O_RDONLY);
+		ASSERTF(fd >= 0, "llapi_open_by_fid for " DFID_NOBRACE ": %d",
+			PFID(&fid), fd);
+		close(fd);
+
+		/* start threads with llapi_open_by_fid() */
+		for (i = 0; i < TEST1_THR_NBR; i++)
+			pthread_create(&thread[i], NULL, &test1_thr, fidstr);
+
+		for (i = 0; i < TEST1_THR_NBR; i++) {
+			pthread_join(thread[i], (void **) &rc);
+			ASSERTF(rc == 0,
+				"llapi_fid2path for " DFID_NOBRACE " (iter: %d, thr:%d): %s",
+				PFID(&fid), iter, i, strerror(-rc));
+		}
+	}
+}
+
+static void usage(char *prog)
+{
+	fprintf(stderr, "Usage: %s [-h]\n", basename(prog));
+	fprintf(stderr, "or:    %s FILEPATH1 FILEPATH2\n", basename(prog));
+	exit(EXIT_FAILURE);
+}
+
+static void process_args(int argc, char *argv[])
+{
+	/* default mountpoints used */
+	if (argc == 1)
+		return;
+
+	if (argc > 1 && argv[1][0] == '-')
+		usage(argv[0]);
+
+	if (argc <= 2 || argv[1][0] == '\0' || argv[2][0] == '\0')
+		usage(argv[0]);
+
+	mainpath = argv[1];
+	mainpath2 = argv[2];
+}
+
+
+static int fill_default_paths(void)
+{
+	static char tmp1[PATH_MAX] = "/mnt/lustre/llapi_root_test.XXXXXX";
+	static char tmp2[PATH_MAX] = "/mnt/lustre2/";
+	int fd;
+
+	/* default paths needed?*/
+	if (mainpath || mainpath2)
+		return 0;
+
+	fd = mkstemp(tmp1);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to creat %s: %s\n",
+			tmp1, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	close(fd);
+	strcat(tmp2, basename(tmp1));
+
+	mainpath = tmp1;
+	mainpath2 = tmp2;
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	char fsname[8 + 1];
+	char fsname2[8 + 1];
+	int rc;
+
+	process_args(argc, argv);
+	fill_default_paths();
+	atexit(cleanup);
+
+	if (strcmp(basename(mainpath), basename(mainpath2)) != 0 ||
+		   strcmp(mainpath, mainpath2) == 0) {
+		fprintf(stderr, "%s and %s should be the same file on 2 distinct mountpoints\n",
+			mainpath, mainpath2);
+		return EXIT_FAILURE;
+	}
+
+	rc = llapi_search_mounts(mainpath, 0, mnt_dir, fsname);
+	if (rc != 0) {
+		fprintf(stderr, "Error: %s: not a Lustre filesystem\n",
+			mainpath);
+		return EXIT_FAILURE;
+	}
+
+	rc = llapi_search_mounts(mainpath2, 0, mnt_dir2, fsname2);
+	if (rc != 0) {
+		fprintf(stderr, "Error: %s: not a Lustre filesystem\n",
+			mainpath2);
+		return EXIT_FAILURE;
+	}
+
+	if (strcmp(fsname, fsname2) != 0) {
+		fprintf(stderr, "%s and %s are not on the same filesystem (%s, %s)\n",
+			mnt_dir, mnt_dir2, fsname, fsname2);
+		return EXIT_FAILURE;
+	}
+
+	mnt_fd = open(mnt_dir, O_RDONLY|O_DIRECTORY);
+	ASSERTF(mnt_fd >= 0, "cannot open '%s': %s\n", mnt_dir, strerror(errno));
+
+	mnt_fd2 = open(mnt_dir2, O_RDONLY|O_DIRECTORY);
+	ASSERTF(mnt_fd2 >= 0, "cannot open '%s': %s\n", mnt_dir2, strerror(errno));
+
+	fprintf(stderr, "Starting: %s %s %s\n\n",
+		basename(argv[0]), mainpath, mainpath2);
+
+	/* Play nice with Lustre test scripts. Non-line buffered output
+	 * stream under I/O redirection may appear incorrectly.
+	 */
+	setvbuf(stdout, NULL, _IOLBF, 0);
+
+	PERFORM(test1);
+
+	return EXIT_SUCCESS;
+}

lustre/tests/sanityn.sh
@@ -5148,6 +5148,11 @@ test_84() {
 }
 run_test 84 "0-nlink race in lu_object_find()"
 
+test_85() {
+	llapi_root_test $DIR/$tfile $DIR2/$tfile
+}
+run_test 85 "Lustre API root cache race"
+
 test_90() {
 	[ $MDSCOUNT -lt 2 ] && skip "needs >= 2 MDTs" && return
 	local pid1