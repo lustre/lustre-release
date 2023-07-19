lustre/lustre-release

commit cc5594df3e70d1924f34ccdf4c3178654d277ad0

LU-16759 o2ib: MOFED 5.5+ ib_dma_virt_map_sg
MOFED 5.5 fails with:
  ERROR: "ib_dma_virt_map_sg" [.../ko2iblnd.ko] undefined!

See if we have a broken ib_dma_map_sg() and provide
a suitable replacement for the missing functionality.

Test-Parameters: trivial
HPE-bug-id: LUS-11587
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I3b4454fcafe4640c15b13385a0209ed71f51a3d0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50711
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/autoconf/lustre-lnet.m4
@@ -246,7 +246,7 @@ AS_IF([test $ENABLEO2IB = "no"], [
 			O2IB_SYMVER=$LINUX_OBJ/Module.symvers
 		fi
 		if test -n "$O2IB_SYMVER"; then
-			if test ！"$O2IB_SYMVER" -ef "$LINUX_OBJ/Module.symvers"; then
+			if test ! "$O2IB_SYMVER" -ef "$LINUX_OBJ/Module.symvers"; then
 				AC_MSG_NOTICE([adding $O2IB_SYMVER to Symbol Path O2IB])
 				EXTRA_SYMBOLS="$EXTRA_SYMBOLS $O2IB_SYMVER"
 				AC_SUBST(EXTRA_SYMBOLS)
@@ -609,6 +609,34 @@ AS_IF([test $ENABLEO2IB != "no"], [
 		LB2_LINUX_TEST_RESULT([ib_post_send_recv_const], [
 			AC_DEFINE(HAVE_IB_POST_SEND_RECV_CONST, 1,
 				[ib_post_send and ib_post_recv have const parameters])
+		])
+	])
+
+	# MOFED 5.5 fails with:
+	#   ERROR: "ib_dma_virt_map_sg" [.../ko2iblnd.ko] undefined!
+	# See if we have a broken ib_dma_map_sg()
+	AC_DEFUN([LN_SRC_SANE_IB_DMA_MAP_SG], [
+		LB2_LINUX_TEST_SRC([sane_ib_dma_map_sg], [
+			#ifdef HAVE_COMPAT_RDMA
+			#undef PACKAGE_NAME
+			#undef PACKAGE_TARNAME
+			#undef PACKAGE_VERSION
+			#undef PACKAGE_STRING
+			#undef PACKAGE_BUGREPORT
+			#undef PACKAGE_URL
+			#include <linux/compat-2.6.h>
+			#endif
+			#include <rdma/ib_verbs.h>
+		],[
+			ib_dma_map_sg((struct ib_device *)NULL,
+				      (struct scatterlist *)NULL, 1, 0);
+		],[-Werror],[$EXTRA_OFED_CONFIG $EXTRA_OFED_INCLUDE])
+	])
+	AC_DEFUN([LN_SANE_IB_DMA_MAP_SG], [
+		AC_MSG_CHECKING([if ib_dma_map_sg() is sane])
+		LB2_LINUX_TEST_RESULT([sane_ib_dma_map_sg], [
+			AC_DEFINE(HAVE_SANE_IB_DMA_MAP_SG, 1,
+				[ib_dma_map_sg is sane])
 		])
 	])
 

lnet/klnds/o2iblnd/o2iblnd.h
@@ -1078,15 +1078,33 @@ static inline void kiblnd_dma_unmap_single(struct ib_device *dev,
 static inline
 int kiblnd_dma_map_sg(struct kib_hca_dev *hdev, struct kib_tx *tx)
 {
+	struct ib_device *dev = hdev->ibh_ibdev;
 	struct scatterlist *sg = tx->tx_frags;
 	int nents = tx->tx_nfrags;
 	enum dma_data_direction direction = tx->tx_dmadir;
 
 	if (tx->tx_gpu)
-		return lnet_rdma_map_sg_attrs(hdev->ibh_ibdev->dma_device,
-					      sg, nents, direction);
+		return lnet_rdma_map_sg_attrs(dev->dma_device, sg, nents,
+					      direction);
 
-	return ib_dma_map_sg(hdev->ibh_ibdev, sg, nents, direction);
+#ifdef HAVE_SANE_IB_DMA_MAP_SG
+	return ib_dma_map_sg(dev, sg, nents, direction);
+#else
+ #ifdef CONFIG_INFINIBAND_VIRT_DMA
+	if (!dev->dma_device) {
+		struct scatterlist *s;
+		int i;
+
+		/* NOTE: open coded ib_dma_virt_map_sg() */
+		for_each_sg(sg, s, nents, i) {
+			sg_dma_address(s) = (uintptr_t)sg_virt(s);
+			sg_dma_len(s) = s->length;
+		}
+		return nents;
+	}
+ #endif
+	return dma_map_sg_attrs(dev->dma_device, sg, nents, direction, 0);
+#endif
 }
 
 static inline

commit 581db5e89e0d690961e49278a7b50ecce78e5a22

LU-16793 build: Enable compile tests to require <module>.ko
Currently the build tests only demand a kernel api test
create an object (.o).

Cases that have a missing symbol export, directly or
indirectly, will generate an object file and fail to
generate a kernel module (.ko).

Enable tests to select the stricter criteria.

Test-Parameters: trivial
Fixes: cc5594df3e ("LU-16759 o2ib: MOFED 5.5+ ib_dma_virt_map_sg")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Iae481f1287023ea6c2432d147c497fa0a55fd689
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50849
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build-linux.m4
@@ -924,6 +924,7 @@ AC_DEFUN([LB2_LINUX_TEST_SRC], [
 # $1 - *unique* name matching the LB2_LINUX_TEST_SRC macro
 # $2 - run on success (valid .ko generated)
 # $3 - run on failure (unable to compile)
+# $4 - compile only
 #
 AC_DEFUN([LB2_LINUX_TEST_RESULT],[
 	TEST_DIR=${TEST_DIR:-${ac_pwd}/_lpb}
@@ -964,8 +965,13 @@ AC_DEFUN([LB2_LINUX_TEST_RESULT],[
 	# Abort if key does not exist
 	AS_IF([test -f ${O}.tested], [],
 		[AC_MSG_ERROR([*** Compile test for $1 was not run.])])
+	# Default is to expect only the <module>.o be generated.
+	NEED_KO=0
+	# Require the <module>.ko file when "module" is passed
+	AS_IF([test "X'$4'" == "X'module'"], [NEED_KO=1])
 	# If test was compiled and if we got an object ...
-	AS_IF([test -f ${O}.o], [touch ${O}.ko])
+	AS_IF([test ${NEED_KO} -eq 0], [AS_IF([test ! -f ${O}.ko], [AS_IF(
+		[test -f ${O}.o], [touch ${O}.ko])])])
 	# key is valid. Cache should be valid, set the variable
 	AC_CACHE_CHECK([for $1], lb_test,
 		AS_IF([test -f ${O}.ko],

lnet/autoconf/lustre-lnet.m4
@@ -642,7 +642,7 @@ AS_IF([test $ENABLEO2IB != "no"], [
 		LB2_LINUX_TEST_RESULT([sane_ib_dma_map_sg], [
 			AC_DEFINE(HAVE_SANE_IB_DMA_MAP_SG, 1,
 				[ib_dma_map_sg is sane])
-		])
+		],[],[module])
 	])
 
 	#
@@ -803,6 +803,7 @@ AS_IF([test $ENABLEO2IB != "no"], [
 		LN_SRC_O2IB_IB_ALLOC_PD
 		LN_SRC_O2IB_IB_INC_RKEY
 		LN_SRC_O2IB_IB_POST_SEND_CONST
+		LN_SRC_SANE_IB_DMA_MAP_SG
 		LN_SRC_O2IB_IB_DEVICE_OPS_EXISTS
 		LN_SRC_O2IB_IB_SG_DMA_ADDRESS_EXISTS
 		LN_SRC_O2IB_RDMA_REJECT
@@ -823,6 +824,7 @@ AS_IF([test $ENABLEO2IB != "no"], [
 		LN_O2IB_IB_ALLOC_PD
 		LN_O2IB_IB_INC_RKEY
 		LN_O2IB_IB_POST_SEND_CONST
+		LN_SANE_IB_DMA_MAP_SG
 		LN_O2IB_IB_DEVICE_OPS_EXISTS
 		LN_O2IB_IB_SG_DMA_ADDRESS_EXISTS
 		LN_O2IB_RDMA_REJECT