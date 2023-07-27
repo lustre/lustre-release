lustre/lustre-release

commit 61c3b3a9bb848e256845462ffd79b15565cd23ad

LU-16165 sec: retry mechanism for identity cache
Implement a retry mechanism in the identity cache in case the
identity up call times out.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Ib70d3b851a6da3cf66dfed49b03be51da7886d01
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48579
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/upcall_cache.c
@@ -146,6 +146,7 @@ struct upcall_cache_entry *upcall_cache_get_entry(struct upcall_cache *cache,
 						  __u64 key, void *args)
 {
 	struct upcall_cache_entry *entry = NULL, *new = NULL, *next;
+	bool failedacquiring = false;
 	struct list_head *head;
 	wait_queue_entry_t wait;
 	int rc, found;
@@ -231,6 +232,12 @@ find_again:
 			CERROR("acquire for key %llu: error %d\n",
 			       entry->ue_key, rc);
 			put_entry(cache, entry);
+			if (!failedacquiring) {
+				spin_unlock(&cache->uc_lock);
+				failedacquiring = true;
+				new = NULL;
+				goto find_again;
+			}
 			GOTO(out, entry = ERR_PTR(rc));
 		}
 	}

lustre/utils/l_getidentity.c
@@ -408,6 +408,7 @@ int main(int argc, char **argv)
 	glob_t path;
 	unsigned long uid;
 	int fd, rc = -EINVAL, size, maxgroups;
+	bool alreadyfailed = false;
 
 	progname = basename(argv[0]);
 	if (argc != 3) {
@@ -429,10 +430,15 @@ int main(int argc, char **argv)
 		goto out;
 	}
 
+retry:
 	size = offsetof(struct identity_downcall_data, idd_groups[maxgroups]);
 	data = malloc(size);
 	if (!data) {
 		errlog("malloc identity downcall data(%d) failed!\n", size);
+		if (!alreadyfailed) {
+			alreadyfailed = true;
+			goto retry;
+		}
 		rc = -ENOMEM;
 		goto out;
 	}
@@ -475,6 +481,13 @@ downcall:
 	close(fd);
 	if (rc != size) {
 		errlog("partial write ret %d: %s\n", rc, strerror(errno));
+		if (!alreadyfailed) {
+			alreadyfailed = true;
+			cfs_free_param_data(&path);
+			if (data)
+				free(data);
+			goto retry;
+		}
 		rc = -1;
 	} else {
 		rc = 0;

commit 765e59d37dc2b4c85e9bdaea2765c9023226778b

LU-16911 sec: quiet messages from identity upcall retry mech
Do not use CERROR to print messages about failed identity acquire
upcalls. And make a difference between initial attempt before retry,
and final failure.

Fixes: 61c3b3a9bb ("LU-16165 sec: retry mechanism for identity cache")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I35e04ca31b623d6037bb49e4ded4ea96d653f074
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51355
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/upcall_cache.c
@@ -229,15 +229,18 @@ find_again:
 		if (UC_CACHE_IS_ACQUIRING(entry)) {
 			/* we're interrupted or upcall failed in the middle */
 			rc = left > 0 ? -EINTR : -ETIMEDOUT;
-			CERROR("acquire for key %llu: error %d\n",
-			       entry->ue_key, rc);
 			put_entry(cache, entry);
 			if (!failedacquiring) {
 				spin_unlock(&cache->uc_lock);
 				failedacquiring = true;
 				new = NULL;
+				CDEBUG(D_OTHER,
+				       "retry acquire for key %llu (got %d)\n",
+				       entry->ue_key, rc);
 				goto find_again;
 			}
+			CERROR("acquire for key %llu: error %d\n",
+			       entry->ue_key, rc);
 			GOTO(out, entry = ERR_PTR(rc));
 		}
 	}