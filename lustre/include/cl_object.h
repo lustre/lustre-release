lustre/lustre-release

commit 51d62f2122fee14fbb3ff8333b5a830e1181e4e5

LU-16637 llite: call truncate_inode_pages() in inode lock
In some cases vvp_prune()->truncate_inode_pages() is get called
without IO context, we need protect it with inode lock as well.

So we add ll_inode_info::lli_inode_lock_owner and set it according to
vfs lock rules (Documentation/filesystems/Locking or
Documentation/filesystems/locking.rst), so before calling
truncate_inode_pages(), we'd lock the inode if it's not locked in
vfs.

Fixes: ef9be34478 ("LU-16637 llite: call truncate_inode_pages() under inode lock")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I84d7d999a49325810062a9a7337e184d35467820
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50857
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/cl_object.h
@@ -300,6 +300,13 @@ struct cl_layout {
 	bool		cl_is_released;
 };
 
+enum coo_inode_opc {
+	COIO_INODE_LOCK,
+	COIO_INODE_UNLOCK,
+	COIO_SIZE_LOCK,
+	COIO_SIZE_UNLOCK,
+};
+
 /**
  * Operations implemented for each cl object layer.
  *
@@ -427,6 +434,11 @@ struct cl_object_operations {
 	int (*coo_object_flush)(const struct lu_env *env,
 				struct cl_object *obj,
 				struct ldlm_lock *lock);
+	/**
+	 * operate upon inode. Used in LOV to lock/unlock inode from vvp layer.
+	 */
+	int (*coo_inode_ops)(const struct lu_env *env, struct cl_object *obj,
+			     enum coo_inode_opc opc, void *data);
 };
 
 /**
@@ -2139,6 +2151,8 @@ int cl_object_layout_get(const struct lu_env *env, struct cl_object *obj,
 loff_t cl_object_maxbytes(struct cl_object *obj);
 int cl_object_flush(const struct lu_env *env, struct cl_object *obj,
 		    struct ldlm_lock *lock);
+int cl_object_inode_ops(const struct lu_env *env, struct cl_object *obj,
+			enum coo_inode_opc opc, void *data);
 
 
 /**

lustre/llite/file.c
@@ -125,7 +125,8 @@ static void ll_prepare_close(struct inode *inode, struct md_op_data *op_data,
 	op_data->op_open_handle = och->och_open_handle;
 
 	if (och->och_flags & FMODE_WRITE &&
-	    test_and_clear_bit(LLIF_DATA_MODIFIED, &ll_i2info(inode)->lli_flags))
+	    test_and_clear_bit(LLIF_DATA_MODIFIED,
+			       &ll_i2info(inode)->lli_flags))
 		/* For HSM: if inode data has been modified, pack it so that
 		 * MDT can set data dirty flag in the archive. */
 		op_data->op_bias |= MDS_DATA_MODIFIED;
@@ -2201,11 +2202,11 @@ static ssize_t ll_do_tiny_write(struct kiocb *iocb, struct iov_iter *iter)
 		RETURN(0);
 
 	if (unlikely(lock_inode))
-		inode_lock(inode);
+		ll_inode_lock(inode);
 	result = __generic_file_write_iter(iocb, iter);
 
 	if (unlikely(lock_inode))
-		inode_unlock(inode);
+		ll_inode_unlock(inode);
 
 	/* If the page is not already dirty, ll_tiny_write_begin returns
 	 * -ENODATA.  We continue on to normal write.
@@ -3062,9 +3063,9 @@ lookup:
 		if (enckey == 0 || nameenc == 0)
 			continue;
 
-		inode_lock(parent);
+		ll_inode_lock(parent);
 		de = lookup_one_len(p, de_parent, len);
-		inode_unlock(parent);
+		ll_inode_unlock(parent);
 		if (IS_ERR_OR_NULL(de) || !de->d_inode) {
 			dput(de_parent);
 			rc = -ENODATA;
@@ -3479,11 +3480,10 @@ static int ll_hsm_import(struct inode *inode, struct file *file,
 			 ATTR_ATIME | ATTR_ATIME_SET;
 
 	inode_lock(inode);
-
+	/* inode lock owner set in ll_setattr_raw()*/
 	rc = ll_setattr_raw(file_dentry(file), attr, 0, true);
 	if (rc == -ENODATA)
 		rc = 0;
-
 	inode_unlock(inode);
 
 out:
@@ -3532,6 +3532,7 @@ static int ll_file_futimes_3(struct file *file, const struct ll_futimes_3 *lfu)
 		RETURN(-EINVAL);
 
 	inode_lock(inode);
+	/* inode lock owner set in ll_setattr_raw()*/
 	rc = ll_setattr_raw(file_dentry(file), &ia, OP_XVALID_CTIME_SET,
 			    false);
 	inode_unlock(inode);
@@ -3907,10 +3908,10 @@ int ll_ioctl_project(struct file *file, unsigned int cmd, void __user *uarg)
 	/* apply child dentry if name is valid */
 	name_len = strnlen(lu_project.project_name, NAME_MAX);
 	if (name_len > 0 && name_len <= NAME_MAX) {
-		inode_lock(inode);
+		ll_inode_lock(inode);
 		child_dentry = lookup_one_len(lu_project.project_name,
 					      dentry, name_len);
-		inode_unlock(inode);
+		ll_inode_unlock(inode);
 		if (IS_ERR(child_dentry)) {
 			rc = PTR_ERR(child_dentry);
 			goto out;
@@ -5145,7 +5146,7 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	if (IS_ERR(op_data))
 		GOTO(out_iput, rc = PTR_ERR(op_data));
 
-	inode_lock(child_inode);
+	ll_inode_lock(child_inode);
 	op_data->op_fid3 = *ll_inode2fid(child_inode);
 	if (!fid_is_sane(&op_data->op_fid3)) {
 		CERROR("%s: migrate %s, but FID "DFID" is insane\n",
@@ -5226,7 +5227,7 @@ out_close:
 	if (!rc)
 		clear_nlink(child_inode);
 out_unlock:
-	inode_unlock(child_inode);
+	ll_inode_unlock(child_inode);
 	ll_finish_md_op_data(op_data);
 out_iput:
 	iput(child_inode);

lustre/llite/llite_internal.h
@@ -206,7 +206,8 @@ struct ll_inode_info {
 		/* for non-directory */
 		struct {
 			struct mutex		lli_size_mutex;
-			char		       *lli_symlink_name;
+			struct task_struct	*lli_size_lock_owner;
+			char			*lli_symlink_name;
 			struct ll_trunc_sem	lli_trunc_sem;
 			struct range_lock_tree	lli_write_tree;
 			struct mutex		lli_setattr_mutex;
@@ -291,6 +292,8 @@ struct ll_inode_info {
 	struct list_head		lli_xattrs; /* ll_xattr_entry->xe_list */
 	struct list_head		lli_lccs; /* list of ll_cl_context */
 	seqlock_t			lli_page_inv_lock;
+
+	struct task_struct		*lli_inode_lock_owner;
 };
 
 #ifndef HAVE_USER_NAMESPACE_ARG
@@ -434,7 +437,7 @@ static inline void ll_layout_version_set(struct ll_inode_info *lli, __u32 gen)
 	spin_unlock(&lli->lli_layout_lock);
 }
 
-enum ll_file_flags {
+enum ll_inode_flags {
 	/* File data is modified. */
 	LLIF_DATA_MODIFIED      = 0,
 	/* File is being restored */
@@ -447,6 +450,7 @@ enum ll_file_flags {
 	LLIF_UPDATE_ATIME	= 4,
 	/* foreign file/dir can be unlinked unconditionnaly */
 	LLIF_FOREIGN_REMOVABLE	= 5,
+	/* 6 is not used for now */
 	/* Xattr cache is filled */
 	LLIF_XATTR_CACHE_FILLED	= 7,
 
@@ -586,6 +590,35 @@ static inline struct ll_inode_info *ll_i2info(struct inode *inode)
 static inline struct pcc_inode *ll_i2pcci(struct inode *inode)
 {
 	return ll_i2info(inode)->lli_pcc_inode;
+}
+
+static inline void ll_set_inode_lock_owner(struct inode *inode)
+{
+	ll_i2info(inode)->lli_inode_lock_owner = current;
+}
+
+static inline void ll_clear_inode_lock_owner(struct inode *inode)
+{
+	ll_i2info(inode)->lli_inode_lock_owner = NULL;
+}
+
+static inline struct task_struct *ll_get_inode_lock_owner(struct inode *inode)
+{
+	return ll_i2info(inode)->lli_inode_lock_owner;
+}
+
+/* lock inode and set inode lock owener */
+static inline void ll_inode_lock(struct inode *inode)
+{
+	inode_lock(inode);
+	ll_set_inode_lock_owner(inode);
+}
+
+/* clear inode lock owner and unlock it */
+static inline void ll_inode_unlock(struct inode *inode)
+{
+	ll_clear_inode_lock_owner(inode);
+	inode_unlock(inode);
 }
 
 /* default to use at least 16M for fast read if possible */
@@ -1331,7 +1364,7 @@ int ll_update_inode(struct inode *inode, struct lustre_md *md);
 void ll_update_inode_flags(struct inode *inode, unsigned int ext_flags);
 void ll_update_dir_depth_dmv(struct inode *dir, struct dentry *de);
 int ll_read_inode2(struct inode *inode, void *opaque);
-void ll_truncate_inode_pages_final(struct inode *inode, struct cl_io *io);
+void ll_truncate_inode_pages_final(struct inode *inode);
 void ll_delete_inode(struct inode *inode);
 int ll_iocontrol(struct inode *inode, struct file *file,
 		 unsigned int cmd, void __user *uarg);

lustre/llite/llite_lib.c
@@ -1274,6 +1274,7 @@ void ll_lli_init(struct ll_inode_info *lli)
 	/* ll_cl_context initialize */
 	INIT_LIST_HEAD(&lli->lli_lccs);
 	seqlock_init(&lli->lli_page_inv_lock);
+	lli->lli_inode_lock_owner = NULL;
 }
 
 #define MAX_STRING_SIZE 128
@@ -1990,10 +1991,10 @@ static int ll_md_setattr(struct dentry *dentry, struct md_op_data *op_data)
 	 * cache is not cleared yet. */
 	op_data->op_attr.ia_valid &= ~(TIMES_SET_FLAGS | ATTR_SIZE);
 	if (S_ISREG(inode->i_mode))
-		inode_lock(inode);
+		ll_inode_lock(inode);
 	rc = simple_setattr(&init_user_ns, dentry, &op_data->op_attr);
 	if (S_ISREG(inode->i_mode))
-		inode_unlock(inode);
+		ll_inode_unlock(inode);
 	op_data->op_attr.ia_valid = ia_valid;
 
 	rc = ll_update_inode(inode, &md);
@@ -2223,6 +2224,9 @@ int ll_setattr_raw(struct dentry *dentry, struct iattr *attr,
 
 	ENTRY;
 
+	/* VFS has locked the inode before calling this */
+	ll_set_inode_lock_owner(inode);
+
 	CDEBUG(D_VFSTRACE, "%s: setattr inode "DFID"(%p) from %llu to %llu, "
 	       "valid %x, hsm_import %d\n",
 	       ll_i2sbi(inode)->ll_fsname, PFID(&lli->lli_fid),
@@ -2230,29 +2234,29 @@ int ll_setattr_raw(struct dentry *dentry, struct iattr *attr,
 	       hsm_import);
 
 	if (attr->ia_valid & ATTR_SIZE) {
-                /* Check new size against VFS/VM file size limit and rlimit */
-                rc = inode_newsize_ok(inode, attr->ia_size);
-                if (rc)
-                        RETURN(rc);
+		/* Check new size against VFS/VM file size limit and rlimit */
+		rc = inode_newsize_ok(inode, attr->ia_size);
+		if (rc)
+			GOTO(clear, rc);
 
-                /* The maximum Lustre file size is variable, based on the
-                 * OST maximum object size and number of stripes.  This
-                 * needs another check in addition to the VFS check above. */
-                if (attr->ia_size > ll_file_maxbytes(inode)) {
+		/* The maximum Lustre file size is variable, based on the
+		 * OST maximum object size and number of stripes.  This
+		 * needs another check in addition to the VFS check above. */
+		if (attr->ia_size > ll_file_maxbytes(inode)) {
 			CDEBUG(D_INODE,"file "DFID" too large %llu > %llu\n",
-                               PFID(&lli->lli_fid), attr->ia_size,
-                               ll_file_maxbytes(inode));
-                        RETURN(-EFBIG);
-                }
+			       PFID(&lli->lli_fid), attr->ia_size,
+			       ll_file_maxbytes(inode));
+			GOTO(clear, rc = -EFBIG);
+		}
 
-                attr->ia_valid |= ATTR_MTIME | ATTR_CTIME;
-        }
+		attr->ia_valid |= ATTR_MTIME | ATTR_CTIME;
+	}
 
 	/* POSIX: check before ATTR_*TIME_SET set (from inode_change_ok) */
 	if (attr->ia_valid & TIMES_SET_FLAGS) {
 		if ((!uid_eq(current_fsuid(), inode->i_uid)) &&
 		    !capable(CAP_FOWNER))
-			RETURN(-EPERM);
+			GOTO(clear, rc = -EPERM);
 	}
 
 	/* We mark all of the fields "set" so MDS/OST does not re-set them */
@@ -2269,8 +2273,8 @@ int ll_setattr_raw(struct dentry *dentry, struct iattr *attr,
 	if (!(attr->ia_valid & ATTR_MTIME_SET) &&
 	    (attr->ia_valid & ATTR_MTIME)) {
 		attr->ia_mtime = current_time(inode);
-                attr->ia_valid |= ATTR_MTIME_SET;
-        }
+		attr->ia_valid |= ATTR_MTIME_SET;
+	}
 
         if (attr->ia_valid & (ATTR_MTIME | ATTR_CTIME))
 		CDEBUG(D_INODE, "setting mtime %lld, ctime %lld, now = %lld\n",
@@ -2278,7 +2282,7 @@ int ll_setattr_raw(struct dentry *dentry, struct iattr *attr,
 		       ktime_get_real_seconds());
 
 	if (S_ISREG(inode->i_mode))
-		inode_unlock(inode);
+		ll_inode_unlock(inode);
 
 	/* We always do an MDS RPC, even if we're only changing the size;
 	 * only the MDS knows whether truncate() should fail with -ETXTBUSY */
@@ -2452,7 +2456,7 @@ out:
 		ll_finish_md_op_data(op_data);
 
 	if (S_ISREG(inode->i_mode)) {
-		inode_lock(inode);
+		ll_inode_lock(inode);
 		if ((attr->ia_valid & ATTR_SIZE) && !hsm_import)
 			inode_dio_wait(inode);
 		/* Once we've got the i_mutex, it's safe to set the S_NOSEC
@@ -2467,6 +2471,8 @@ out:
 		ll_stats_ops_tally(ll_i2sbi(inode), attr->ia_valid & ATTR_SIZE ?
 					LPROC_LL_TRUNC : LPROC_LL_SETATTR,
 				   ktime_us_delta(ktime_get(), kstart));
+clear:
+	ll_clear_inode_lock_owner(inode);
 
 	RETURN(rc);
 }
@@ -2661,6 +2667,7 @@ void ll_inode_size_lock(struct inode *inode)
 
 	lli = ll_i2info(inode);
 	mutex_lock(&lli->lli_size_mutex);
+	lli->lli_size_lock_owner = current;
 }
 
 void ll_inode_size_unlock(struct inode *inode)
@@ -2668,6 +2675,7 @@ void ll_inode_size_unlock(struct inode *inode)
 	struct ll_inode_info *lli;
 
 	lli = ll_i2info(inode);
+	lli->lli_size_lock_owner = NULL;
 	mutex_unlock(&lli->lli_size_mutex);
 }
 
@@ -2996,14 +3004,16 @@ void ll_update_dir_depth_dmv(struct inode *dir, struct dentry *de)
 		       lli->lli_inherit_depth);
 }
 
-void ll_truncate_inode_pages_final(struct inode *inode, struct cl_io *io)
+void ll_truncate_inode_pages_final(struct inode *inode)
 {
 	struct address_space *mapping = &inode->i_data;
 	unsigned long nrpages;
 	unsigned long flags;
 
-	LASSERTF(io == NULL || inode_is_locked(inode), "io %p (type %d)\n",
-		 io, io ? io->ci_type : 0);
+	LASSERTF((inode->i_state & I_FREEING) || inode_is_locked(inode),
+		 DFID ":inode %p state %#lx, lli_flags %#lx\n",
+		 PFID(ll_inode2fid(inode)), inode, inode->i_state,
+		 ll_i2info(inode)->lli_flags);
 
 	truncate_inode_pages_final(mapping);
 
@@ -3022,11 +3032,11 @@ void ll_truncate_inode_pages_final(struct inode *inode, struct cl_io *io)
 	} /* Workaround end */
 
 	LASSERTF(nrpages == 0, "%s: inode="DFID"(%p) nrpages=%lu "
-		 "io %p (io_type %d), "
+		 "state %#lx, lli_flags %#lx, "
 		 "see https://jira.whamcloud.com/browse/LU-118\n",
 		 ll_i2sbi(inode)->ll_fsname,
 		 PFID(ll_inode2fid(inode)), inode, nrpages,
-		 io, io ? io->ci_type : 0);
+		 inode->i_state, ll_i2info(inode)->lli_flags);
 }
 
 int ll_read_inode2(struct inode *inode, void *opaque)
@@ -3100,7 +3110,7 @@ void ll_delete_inode(struct inode *inode)
 				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1);
 	}
 
-	ll_truncate_inode_pages_final(inode, NULL);
+	ll_truncate_inode_pages_final(inode);
 	ll_clear_inode(inode);
 	clear_inode(inode);
 

lustre/llite/vvp_object.c
@@ -155,7 +155,6 @@ static int vvp_conf_set(const struct lu_env *env, struct cl_object *obj,
 
 static int vvp_prune(const struct lu_env *env, struct cl_object *obj)
 {
-	struct cl_io *io = vvp_env_io(env)->vui_cl.cis_io;
 	struct inode *inode = vvp_object_inode(obj);
 	int rc;
 	ENTRY;
@@ -167,14 +166,16 @@ static int vvp_prune(const struct lu_env *env, struct cl_object *obj)
 		RETURN(rc);
 	}
 
-	if (io != NULL)
-		inode_lock(inode);
+	if (ll_get_inode_lock_owner(inode) != current)
+		/* ask LOV get inode lock then lo_type_guard */
+		RETURN(-EAGAIN);
 
-	ll_truncate_inode_pages_final(inode, io);
+	LASSERTF(inode_is_locked(inode), DFID ":inode %p lli_flags %#lx\n",
+		 PFID(lu_object_fid(&obj->co_lu)), inode,
+		 ll_i2info(inode)->lli_flags);
+
+	ll_truncate_inode_pages_final(inode);
 	mapping_clear_exiting(inode->i_mapping);
-
-	if (io != NULL)
-		inode_unlock(inode);
 
 	RETURN(0);
 }
@@ -230,6 +231,46 @@ static void vvp_req_attr_set(const struct lu_env *env, struct cl_object *obj,
 	memcpy(attr->cra_jobid, &lli->lli_jobid, sizeof(attr->cra_jobid));
 }
 
+static int vvp_inode_ops(const struct lu_env *env, struct cl_object *obj,
+			 enum coo_inode_opc opc, void *data)
+{
+	struct inode *inode = vvp_object_inode(obj);
+	int rc = 0;
+
+	ENTRY;
+	switch (opc) {
+	case COIO_INODE_LOCK:
+		if (ll_get_inode_lock_owner(inode) != current)
+			ll_inode_lock(inode);
+		else
+			rc = -EALREADY;
+		break;
+	case COIO_INODE_UNLOCK:
+		if (ll_get_inode_lock_owner(inode) == current)
+			ll_inode_unlock(inode);
+		else
+			rc = -ENOLCK;
+		break;
+	case COIO_SIZE_LOCK:
+		if (ll_i2info(inode)->lli_size_lock_owner != current)
+			ll_inode_size_lock(inode);
+		else
+			rc = -EALREADY;
+		break;
+	case COIO_SIZE_UNLOCK:
+		if (ll_i2info(inode)->lli_size_lock_owner == current)
+			ll_inode_size_unlock(inode);
+		else
+			rc = -ENOLCK;
+		break;
+	default:
+		rc = -EINVAL;
+		break;
+	}
+
+	RETURN(rc);
+}
+
 static const struct cl_object_operations vvp_ops = {
 	.coo_page_init    = vvp_page_init,
 	.coo_io_init      = vvp_io_init,
@@ -238,7 +279,8 @@ static const struct cl_object_operations vvp_ops = {
 	.coo_conf_set     = vvp_conf_set,
 	.coo_prune        = vvp_prune,
 	.coo_glimpse      = vvp_object_glimpse,
-	.coo_req_attr_set = vvp_req_attr_set
+	.coo_req_attr_set = vvp_req_attr_set,
+	.coo_inode_ops    = vvp_inode_ops,
 };
 
 static int vvp_object_init0(const struct lu_env *env,

lustre/lov/lov_object.c
@@ -309,10 +309,11 @@ static void lov_subobject_kill(const struct lu_env *env, struct lov_object *lov,
 	LASSERT(r0->lo_sub[idx] == NULL);
 }
 
-static void lov_delete_raid0(const struct lu_env *env, struct lov_object *lov,
+static int lov_delete_raid0(const struct lu_env *env, struct lov_object *lov,
 			     struct lov_layout_entry *lle)
 {
 	struct lov_layout_raid0 *r0 = &lle->lle_raid0;
+	int rc;
 
 	ENTRY;
 
@@ -323,7 +324,9 @@ static void lov_delete_raid0(const struct lu_env *env, struct lov_object *lov,
 			struct lovsub_object *los = r0->lo_sub[i];
 
 			if (los != NULL) {
-				cl_object_prune(env, &los->lso_cl);
+				rc = cl_object_prune(env, &los->lso_cl);
+				if (rc)
+					RETURN(rc);
 				/*
 				 * If top-level object is to be evicted from
 				 * the cache, so are its sub-objects.
@@ -333,7 +336,7 @@ static void lov_delete_raid0(const struct lu_env *env, struct lov_object *lov,
 		}
 	}
 
-	EXIT;
+	RETURN(0);
 }
 
 static void lov_fini_raid0(const struct lu_env *env,
@@ -848,6 +851,7 @@ static int lov_delete_composite(const struct lu_env *env,
 				union lov_layout_state *state)
 {
 	struct lov_layout_entry *entry;
+	int rc;
 
 	ENTRY;
 
@@ -858,7 +862,9 @@ static int lov_delete_composite(const struct lu_env *env,
 		if (entry->lle_lsme && lsme_is_foreign(entry->lle_lsme))
 			continue;
 
-		lov_delete_raid0(env, lov, entry);
+		rc = lov_delete_raid0(env, lov, entry);
+		if (rc)
+			RETURN(rc);
 	}
 
 	RETURN(0);
@@ -1360,6 +1366,9 @@ static int lov_conf_set(const struct lu_env *env, struct cl_object *obj,
 	struct lov_stripe_md	*lsm = NULL;
 	struct lov_object	*lov = cl2lov(obj);
 	int			 result = 0;
+	struct cl_object *top = cl_object_top(obj);
+	bool unlock_inode = false;
+	bool lock_inode_size = false;
 	ENTRY;
 
 	if (conf->coc_opc == OBJECT_CONF_SET &&
@@ -1377,6 +1386,7 @@ static int lov_conf_set(const struct lu_env *env, struct cl_object *obj,
 		GOTO(out_lsm, result = 0);
 	}
 
+retry:
 	lov_conf_lock(lov);
 	if (conf->coc_opc == OBJECT_CONF_WAIT) {
 		if (test_bit(LO_LAYOUT_INVALID, &lov->lo_obj_flags) &&
@@ -1425,14 +1435,49 @@ static int lov_conf_set(const struct lu_env *env, struct cl_object *obj,
 	}
 
 	result = lov_layout_change(env, lov, lsm, conf);
-	if (result)
+	if (result) {
+		if (result == -EAGAIN) {
+			/**
+			 * we need unlocked lov conf and get inode lock.
+			 * It's possible we have already taken inode's size
+			 * mutex, so we need keep such lock order, lest deadlock
+			 * happens:
+			 *   inode lock       (ll_inode_lock())
+			 *   inode size lock  (ll_inode_size_lock())
+			 *   lov conf lock    (lov_conf_lock())
+			 *
+			 * e.g.
+			 *   vfs_setxattr                inode locked
+			 *     ll_lov_setstripe_ea_info  inode size locked
+			 *       ll_prep_inode
+			 *         ll_file_inode_init
+			 *           cl_conf_set
+			 *             lov_conf_set      lov conf locked
+			 */
+			lov_conf_unlock(lov);
+			if (cl_object_inode_ops(
+					env, top, COIO_SIZE_UNLOCK, NULL) == 0)
+				lock_inode_size = true;
+
+			/* take lock in order */
+			if (cl_object_inode_ops(
+					env, top, COIO_INODE_LOCK, NULL) == 0)
+				unlock_inode = true;
+			if (lock_inode_size)
+				cl_object_inode_ops(
+					env, top, COIO_SIZE_LOCK, NULL);
+			goto retry;
+		}
 		set_bit(LO_LAYOUT_INVALID, &lov->lo_obj_flags);
-	else
+	} else {
 		clear_bit(LO_LAYOUT_INVALID, &lov->lo_obj_flags);
+	}
 	EXIT;
 
 out:
 	lov_conf_unlock(lov);
+	if (unlock_inode)
+		cl_object_inode_ops(env, top, COIO_INODE_UNLOCK, NULL);
 out_lsm:
 	lov_lsm_put(lsm);
 	CDEBUG(D_INODE, DFID" lo_layout_invalid=%u\n",

lustre/llite/dir.c
@@ -2332,7 +2332,7 @@ static loff_t ll_dir_seek(struct file *file, loff_t offset, int origin)
 	loff_t ret = -EINVAL;
 	ENTRY;
 
-	inode_lock(inode);
+	ll_inode_lock(inode);
 	switch (origin) {
 	case SEEK_SET:
 		break;
@@ -2373,7 +2373,7 @@ static loff_t ll_dir_seek(struct file *file, loff_t offset, int origin)
 	GOTO(out, ret);
 
 out:
-	inode_unlock(inode);
+	ll_inode_unlock(inode);
 	return ret;
 }
 

lustre/llite/llite_nfs.c
@@ -287,14 +287,14 @@ static int ll_get_name(struct dentry *dentry, char *name, struct dentry *child)
 	if (IS_ERR(op_data))
 		GOTO(out, rc = PTR_ERR(op_data));
 
-	inode_lock(dir);
+	ll_inode_lock(dir);
 #ifdef HAVE_DIR_CONTEXT
 	rc = ll_dir_read(dir, &pos, op_data, &lgd.ctx, NULL);
 #else
 	rc = ll_dir_read(dir, &pos, op_data, &lgd, ll_nfs_get_name_filldir,
 			 NULL);
 #endif
-	inode_unlock(dir);
+	ll_inode_unlock(dir);
 	ll_finish_md_op_data(op_data);
 	if (!rc && !lgd.lgd_found)
 		rc = -ENOENT;

commit 8f2c1592c3bbd0351ab3984a88a3eed7075690c8

LU-16958 llite: migrate vs regular ops deadlock
When it need to lock inode in lov_conf_set(), it could have hold
inode's lli_layout_mutex, we need unlock the layout mutex before
taking its inode lock to keep the lock order.

Fixes: 51d62f2122f ("LU-16637 llite: call truncate_inode_pages() in inode lock")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I7ee58039a6d31daefc625ac571a52baf112f8151
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51641
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/cl_object.h
@@ -305,6 +305,8 @@ enum coo_inode_opc {
 	COIO_INODE_UNLOCK,
 	COIO_SIZE_LOCK,
 	COIO_SIZE_UNLOCK,
+	COIO_LAYOUT_LOCK,
+	COIO_LAYOUT_UNLOCK,
 };
 
 /**

lustre/llite/file.c
@@ -6283,6 +6283,7 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 
 	/* take layout lock mutex to enqueue layout lock exclusively. */
 	mutex_lock(&lli->lli_layout_mutex);
+	lli->lli_layout_lock_owner = current;
 
 	while (1) {
 		/* mostly layout lock is caching on the local side, so try to
@@ -6304,6 +6305,7 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 
 	if (rc == 0)
 		*gen = ll_layout_version_get(lli);
+	lli->lli_layout_lock_owner = NULL;
 	mutex_unlock(&lli->lli_layout_mutex);
 
 	RETURN(rc);

lustre/llite/llite_internal.h
@@ -281,6 +281,7 @@ struct ll_inode_info {
 
 	/* mutex to request for layout lock exclusively. */
 	struct mutex			lli_layout_mutex;
+	struct task_struct 		*lli_layout_lock_owner;
 	/* Layout version, protected by lli_layout_lock */
 	__u32				lli_layout_gen;
 	spinlock_t			lli_layout_lock;

lustre/llite/llite_lib.c
@@ -1274,6 +1274,7 @@ void ll_lli_init(struct ll_inode_info *lli)
 		lli->lli_gid = (__u32) -1;
 	}
 	mutex_init(&lli->lli_layout_mutex);
+	lli->lli_layout_lock_owner = NULL;
 	/* ll_cl_context initialize */
 	INIT_LIST_HEAD(&lli->lli_lccs);
 	seqlock_init(&lli->lli_page_inv_lock);

lustre/llite/vvp_object.c
@@ -235,6 +235,7 @@ static int vvp_inode_ops(const struct lu_env *env, struct cl_object *obj,
 			 enum coo_inode_opc opc, void *data)
 {
 	struct inode *inode = vvp_object_inode(obj);
+	struct ll_inode_info *lli = ll_i2info(inode);
 	int rc = 0;
 
 	ENTRY;
@@ -252,16 +253,30 @@ static int vvp_inode_ops(const struct lu_env *env, struct cl_object *obj,
 			rc = -ENOLCK;
 		break;
 	case COIO_SIZE_LOCK:
-		if (ll_i2info(inode)->lli_size_lock_owner != current)
+		if (lli->lli_size_lock_owner != current)
 			ll_inode_size_lock(inode);
 		else
 			rc = -EALREADY;
 		break;
 	case COIO_SIZE_UNLOCK:
-		if (ll_i2info(inode)->lli_size_lock_owner == current)
+		if (lli->lli_size_lock_owner == current)
 			ll_inode_size_unlock(inode);
 		else
 			rc = -ENOLCK;
+		break;
+	case COIO_LAYOUT_LOCK:
+		if (lli->lli_layout_lock_owner != current) {
+			mutex_lock(&lli->lli_layout_mutex);
+			lli->lli_layout_lock_owner = current;
+		}
+		break;
+	case COIO_LAYOUT_UNLOCK:
+		if (lli->lli_layout_lock_owner == current) {
+			lli->lli_layout_lock_owner = NULL;
+			mutex_unlock(&lli->lli_layout_mutex);
+		} else {
+			rc = -ENOLCK;
+		}
 		break;
 	default:
 		rc = -EINVAL;

lustre/lov/lov_object.c
@@ -1369,6 +1369,7 @@ static int lov_conf_set(const struct lu_env *env, struct cl_object *obj,
 	struct cl_object *top = cl_object_top(obj);
 	bool unlock_inode = false;
 	bool lock_inode_size = false;
+	bool lock_layout = false;
 	ENTRY;
 
 	if (conf->coc_opc == OBJECT_CONF_SET &&
@@ -1440,11 +1441,12 @@ retry:
 			/**
 			 * we need unlocked lov conf and get inode lock.
 			 * It's possible we have already taken inode's size
-			 * mutex, so we need keep such lock order, lest deadlock
-			 * happens:
-			 *   inode lock       (ll_inode_lock())
-			 *   inode size lock  (ll_inode_size_lock())
-			 *   lov conf lock    (lov_conf_lock())
+			 * mutex and/or layout mutex, so we need keep such lock
+			 * order, lest deadlock happens:
+			 *   inode lock        (ll_inode_lock())
+			 *   inode size lock   (ll_inode_size_lock())
+			 *   inode layout lock (ll_layout_refresh())
+			 *   lov conf lock     (lov_conf_lock())
 			 *
 			 * e.g.
 			 *   vfs_setxattr                inode locked
@@ -1453,10 +1455,20 @@ retry:
 			 *         ll_file_inode_init
 			 *           cl_conf_set
 			 *             lov_conf_set      lov conf locked
+			 *
+			 *   ll_migrate                  inode locked
+			 *     ...
+			 *       ll_layout_refresh       inode layout locked
+			 *         ll_layout_conf
+			 *           cl_conf_set
+			 *             lov_conf_set      lov conf locked
 			 */
 			lov_conf_unlock(lov);
-			if (cl_object_inode_ops(
-					env, top, COIO_SIZE_UNLOCK, NULL) == 0)
+			if (cl_object_inode_ops(env, top, COIO_LAYOUT_UNLOCK,
+						NULL) == 0)
+				lock_layout = true;
+			if (cl_object_inode_ops(env, top, COIO_SIZE_UNLOCK,
+						NULL) == 0)
 				lock_inode_size = true;
 
 			/* take lock in order */
@@ -1464,8 +1476,11 @@ retry:
 					env, top, COIO_INODE_LOCK, NULL) == 0)
 				unlock_inode = true;
 			if (lock_inode_size)
-				cl_object_inode_ops(
-					env, top, COIO_SIZE_LOCK, NULL);
+				cl_object_inode_ops(env, top, COIO_SIZE_LOCK,
+						    NULL);
+			if (lock_layout)
+				cl_object_inode_ops(env, top, COIO_LAYOUT_LOCK,
+						    NULL);
 			goto retry;
 		}
 		set_bit(LO_LAYOUT_INVALID, &lov->lo_obj_flags);