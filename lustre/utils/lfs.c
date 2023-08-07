lustre/lustre-release

commit 23224e03dc30c89dd449de5a7fe99b0bd3aca495

LU-16587 utils: give lfs migrate a larger buffer
lfs migrate is slow because it mostly uses a small 1MB buffer. Bigify.

[root@kjlmo4n00 16G]# time lfs migrate -S 1M -p flash 16G.1
real	0m25.341s
[root@kjlmo4n00 16G]# time /root/tools/lfs_nzr migrate -S 1M -p flash 16G.1
real	0m6.526s

Signed-off-by: Nathan Rutman <nathan.rutman@hpe.com>
Change-Id: I850ca475fcd0efe2d71d26e4d1544f462c60252a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50118
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/utils/lfs.c
@@ -832,7 +832,7 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 			     long stats_interval_sec, off_t file_size_bytes)
 {
 	struct llapi_layout *layout;
-	size_t buf_size = 4 * 1024 * 1024;
+	size_t buf_size = 64 * 1024 * 1024;
 	void *buf = NULL;
 	off_t pos = 0;
 	off_t data_end = 0;
@@ -850,8 +850,14 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 		uint64_t stripe_size;
 
 		rc = llapi_layout_stripe_size_get(layout, &stripe_size);
-		if (rc == 0)
-			buf_size = stripe_size;
+		if (rc == 0) {
+			/* We like big bufs */
+			if (stripe_size > buf_size)
+				buf_size = stripe_size;
+			else
+				/* Trim to stripe_size multiple */
+				buf_size -= buf_size % stripe_size;
+		}
 
 		llapi_layout_free(layout);
 	}

commit 15b94e08e84095195eee8c83be65c5f3361532e8

LU-16985 utils: adaptive bufsize for mirroring
if bandwidth limit is requested, then change default bufsize to
make I/O rather smooth than like a saw.

Fixes: 23224e03dc ("LU-16587 utils: give lfs migrate a larger buffer")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Ibc8e7d30ded201a4ff3d699530f5c9f8be5ce7f1
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51773
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/lfs.c
@@ -816,7 +816,8 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 			     long stats_interval_sec, off_t file_size_bytes)
 {
 	struct llapi_layout *layout;
-	size_t buf_size = 64 * 1024 * 1024;
+	size_t buf_size = 64 * ONE_MB;
+	uint64_t stripe_size = ONE_MB;
 	void *buf = NULL;
 	off_t pos = 0;
 	off_t data_end = 0;
@@ -831,8 +832,6 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 
 	layout = llapi_layout_get_by_fd(fd_src, 0);
 	if (layout) {
-		uint64_t stripe_size;
-
 		rc = llapi_layout_stripe_size_get(layout, &stripe_size);
 		if (rc == 0) {
 			/* We like big bufs */
@@ -845,6 +844,11 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 
 		llapi_layout_free(layout);
 	}
+
+	/* limit transfer size to what can be sent in one second */
+	if (bandwidth_bytes_sec && bandwidth_bytes_sec < buf_size)
+		buf_size = (bandwidth_bytes_sec + stripe_size - 1) &
+			~(stripe_size - 1);
 
 	/* Use a page-aligned buffer for direct I/O */
 	rc = posix_memalign(&buf, page_size, buf_size);