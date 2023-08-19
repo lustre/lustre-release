lustre/lustre-release

commit 84e12028be9a13a5f702ce04adfd55d94d8a96ef

LU-9859 libcfs: add support for Xarray
In the late 4.X kernel cycle Xarrays were introduced with the
goal of replacing the radix tree for the page cache. It is
highly optimized for densely packed data which is the case for
several items in Lustre such as the static array for the obd
devices and quota ids. This patch provides Xarray support for
kernels that lack. The current verison of Xarray back ported
is from the 5.4-rc2 kernel.

Change-Id: I54f9046f50a353e1cd4271c0b97207062bbf3898
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/37391
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/linux/linux-prim.c
@@ -114,7 +114,7 @@ int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
 }
 EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
 
-struct kmem_cache (*cfs_radix_tree_node_cachep);
+struct kmem_cache (*radix_tree_node_cachep);
 
 void __init cfs_arch_init(void)
 {
@@ -123,6 +123,8 @@ void __init cfs_arch_init(void)
 #endif
 	cfs_apply_workqueue_attrs_t =
 		(void *)kallsyms_lookup_name("apply_workqueue_attrs");
+	radix_tree_node_cachep =
+		(void *)kallsyms_lookup_name("radix_tree_node_cachep");
 }
 
 int cfs_kernel_write(struct file *filp, const void *buf, size_t count,

libcfs/autoconf/lustre-libcfs.m4
@@ -1069,6 +1069,28 @@ clear_and_wake_up_bit, [
 ]) # LIBCFS_CLEAR_AND_WAKE_UP_BIT
 
 #
+# LIBCFS_XARRAY_SUPPORT
+#
+# 4.19-rc5 kernel commit 3159f943aafdbacb2f94c38fdaadabf2bbde2a14
+# replaced the radix tree implementation with Xarrays. This change
+# introduced functionaly needed for general Xarray support
+#
+AC_DEFUN([LIBCFS_XARRAY_SUPPORT], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if page cache uses Xarray],
+xarray_support, [
+	#include <linux/radix-tree.h>
+],[
+	radix_tree_exceptional_entry(NULL);
+],[
+	AC_DEFINE(HAVE_RADIX_TREE_EXCEPTIONAL_ENTRY, 1,
+		[kernel lacks 'xa_is_value'])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LIBCFS_XARRAY_SUPPORT
+
+#
 # LIBCFS_HAVE_IOV_ITER_TYPE
 #
 # kernel 4.20 commit 00e23707442a75b404392cef1405ab4fd498de6b
@@ -1290,6 +1312,8 @@ LIBCFS_TIMER_SETUP
 LIBCFS_WAIT_VAR_EVENT
 # 4.17
 LIBCFS_CLEAR_AND_WAKE_UP_BIT
+# 4.19
+LIBCFS_XARRAY_SUPPORT
 # 4.20
 LIBCFS_HAVE_IOV_ITER_TYPE
 # 5.0

libcfs/include/libcfs/linux/Makefile.am
@@ -1,3 +1,3 @@
 EXTRA_DIST = linux-misc.h linux-fs.h linux-mem.h linux-time.h linux-cpu.h \
 	     linux-list.h linux-hash.h linux-uuid.h linux-wait.h \
-	     refcount.h processor.h
+	     refcount.h processor.h xarray.h

libcfs/libcfs/Makefile.in
@@ -4,6 +4,7 @@ libcfs-linux-objs := linux-prim.o
 libcfs-linux-objs += linux-curproc.o
 libcfs-linux-objs += linux-hash.o
 libcfs-linux-objs += linux-wait.o
+libcfs-linux-objs += xarray.o
 
 libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
 libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o bio.o

libcfs/libcfs/linux/Makefile.am
@@ -1,4 +1,5 @@
 EXTRA_DIST = linux-prim.c \
 	     linux-curproc.c \
 	     linux-hash.c \
-	     linux-wait.c
+	     linux-wait.c \
+	     xarray.c

lustre/autoconf/lustre-core.m4
@@ -1967,41 +1967,25 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_INODE_TIMESPEC64
 
 #
-# LC_XA_IS_VALUE
-# kernel 4.19-rc6 commit 3159f943aafdbacb2f94c38fdaadabf2bbde2a14
-# xarray: Replace exceptional entries
-# adds xa_is_value
-#
-AC_DEFUN([LC_XA_IS_VALUE], [
-LB_CHECK_COMPILE([xa_is_value exist],
-xa_is_value, [
-	#include <linux/xarray.h>
-],[
-	xa_is_value(NULL);
-],[
-	AC_DEFINE(HAVE_XA_IS_VALUE, 1, [xa_is_value exist])
-])
-]) # LC_XA_IS_VALUE
-
-#
 # LC___XA_SET_MARK
 #
 # kernel 4.20 commit v4.19-rc5-248-g9b89a0355144
-# xarray: Add XArray marks
+# xarray: Add XArray marks - replaced radix_tree_tag_set
 #
 AC_DEFUN([LC___XA_SET_MARK], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
 LB_CHECK_COMPILE([if '__xa_set_mark' exists],
 __xa_set_mark, [
-	#include <linux/xarray.h>
 	#include <linux/fs.h>
+	#include <linux/radix-tree.h>
 ],[
-	struct xarray *xa = NULL;
-
-	__xa_set_mark(xa, 0, PAGECACHE_TAG_DIRTY);
+	radix_tree_tag_set(NULL, 0, PAGECACHE_TAG_DIRTY);
 ],[
-	AC_DEFINE(HAVE___XA_SET_MARK, 1,
+	AC_DEFINE(HAVE_RADIX_TREE_TAG_SET, 1,
 		[__xa_set_mark exists])
 ])
+EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC___XA_SET_MARK
 
 #
@@ -2343,7 +2327,6 @@ AC_DEFUN([LC_PROG_LINUX], [
 
 	# 4.18
 	LC_INODE_TIMESPEC64
-	LC_XA_IS_VALUE
 
 	# 4.20
 	LC___XA_SET_MARK

commit 778791dd7da107710c2311935a24cfd7e7a5fd85

LU-8130 libcfs: don't use radix tree for xarray
For newer kernels the radix tree is totally based on Xarray. For Lustre
support for RHEL7 we backported Xarray but it still was using the
radix tree. Their is a mismatch between what the radix tree expects
and using a struct xa_node when allocating and freeing memory. Instead
abandon all use of the radix tree with Xarray. We use our own private
kmem cache which is based on radix tree but it uses xa_node.

Test-Parameters: trivial
Fixes: 84e12028be9a ("LU-9859 libcfs: add support for Xarray")
Change-Id: I87607aa0e55a4aca039f2fef5a76fbff0bedd9b3
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51840
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/linux/linux-prim.c
@@ -49,6 +49,9 @@
 #include <libcfs/linux/linux-time.h>
 #include <libcfs/linux/linux-wait.h>
 #include <libcfs/linux/linux-misc.h>
+#ifndef HAVE_XARRAY_SUPPORT
+#include <libcfs/linux/xarray.h>
+#endif
 
 #ifndef HAVE_KTIME_GET_TS64
 void ktime_get_ts64(struct timespec64 *ts)
@@ -117,7 +120,15 @@ int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
 EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
 
 #ifndef HAVE_XARRAY_SUPPORT
-struct kmem_cache (*radix_tree_node_cachep);
+struct kmem_cache *xarray_cachep;
+
+static void xarray_node_ctor(void *arg)
+{
+	struct xa_node *node = arg;
+
+	memset(node, 0, sizeof(*node));
+	INIT_LIST_HEAD(&node->private_list);
+}
 #endif
 
 void __init cfs_arch_init(void)
@@ -128,8 +139,10 @@ void __init cfs_arch_init(void)
 	cfs_apply_workqueue_attrs_t =
 		(void *)cfs_kallsyms_lookup_name("apply_workqueue_attrs");
 #ifndef HAVE_XARRAY_SUPPORT
-	radix_tree_node_cachep =
-		(void *)cfs_kallsyms_lookup_name("radix_tree_node_cachep");
+	xarray_cachep = kmem_cache_create("xarray_cache",
+					  sizeof(struct xa_node), 0,
+					  SLAB_PANIC | SLAB_RECLAIM_ACCOUNT,
+					  xarray_node_ctor);
 #endif
 }
 

libcfs/libcfs/linux/xarray.c
@@ -15,7 +15,6 @@
 #include <linux/export.h>
 #include <linux/list.h>
 #include <linux/slab.h>
-#include <linux/radix-tree.h>
 #include <libcfs/linux/xarray.h>
 
 /*
@@ -252,18 +251,18 @@ void *xas_load(struct xa_state *xas)
 EXPORT_SYMBOL_GPL(xas_load);
 
 /* Move the radix tree node cache here */
-extern struct kmem_cache *radix_tree_node_cachep;
+extern struct kmem_cache *xarray_cachep;
 
-static inline void tag_clear(struct radix_tree_node *node, unsigned int tag,
+static inline void tag_clear(struct xa_node *node, unsigned int tag,
 			     int offset)
 {
 	__clear_bit(offset, node->tags[tag]);
 }
 
-static void radix_tree_node_rcu_free(struct rcu_head *head)
+static void xarray_node_rcu_free(struct rcu_head *head)
 {
-	struct radix_tree_node *node =
-		container_of(head, struct radix_tree_node, rcu_head);
+	struct xa_node *node =
+		container_of(head, struct xa_node, rcu_head);
 	int i;
 
 	/*
@@ -271,13 +270,13 @@ static void radix_tree_node_rcu_free(struct rcu_head *head)
 	 * can leave us with a non-NULL entry in the first slot, so clear
 	 * that here to make sure.
 	 */
-	for (i = 0; i < RADIX_TREE_MAX_TAGS; i++)
+	for (i = 0; i < XA_MAX_MARKS; i++)
 		tag_clear(node, i, 0);
 
 	node->slots[0] = NULL;
 	node->count = 0;
 
-	kmem_cache_free(radix_tree_node_cachep, node);
+	kmem_cache_free(xarray_cachep, node);
 }
 
 #define XA_RCU_FREE	((struct xarray *)1)
@@ -286,7 +285,7 @@ static void xa_node_free(struct xa_node *node)
 {
 	XA_NODE_BUG_ON(node, !list_empty(&node->private_list));
 	node->array = XA_RCU_FREE;
-	call_rcu(&node->rcu_head, radix_tree_node_rcu_free);
+	call_rcu(&node->rcu_head, xarray_node_rcu_free);
 }
 
 /*
@@ -302,7 +301,7 @@ static void xas_destroy(struct xa_state *xas)
 	if (!node)
 		return;
 	XA_NODE_BUG_ON(node, !list_empty(&node->private_list));
-	kmem_cache_free(radix_tree_node_cachep, node);
+	kmem_cache_free(xarray_cachep, node);
 	xas->xa_alloc = NULL;
 }
 
@@ -334,7 +333,7 @@ bool xas_nomem(struct xa_state *xas, gfp_t gfp)
 	if (xas->xa->xa_flags & XA_FLAGS_ACCOUNT)
 		gfp |= __GFP_ACCOUNT;
 #endif
-	xas->xa_alloc = kmem_cache_alloc(radix_tree_node_cachep, gfp);
+	xas->xa_alloc = kmem_cache_alloc(xarray_cachep, gfp);
 	if (!xas->xa_alloc)
 		return false;
 	XA_NODE_BUG_ON(xas->xa_alloc, !list_empty(&xas->xa_alloc->private_list));
@@ -367,10 +366,10 @@ static bool __xas_nomem(struct xa_state *xas, gfp_t gfp)
 #endif
 	if (gfpflags_allow_blocking(gfp)) {
 		xas_unlock_type(xas, lock_type);
-		xas->xa_alloc = kmem_cache_alloc(radix_tree_node_cachep, gfp);
+		xas->xa_alloc = kmem_cache_alloc(xarray_cachep, gfp);
 		xas_lock_type(xas, lock_type);
 	} else {
-		xas->xa_alloc = kmem_cache_alloc(radix_tree_node_cachep, gfp);
+		xas->xa_alloc = kmem_cache_alloc(xarray_cachep, gfp);
 	}
 	if (!xas->xa_alloc)
 		return false;
@@ -403,7 +402,7 @@ static void *xas_alloc(struct xa_state *xas, unsigned int shift)
 		if (xas->xa->xa_flags & XA_FLAGS_ACCOUNT)
 			gfp |= __GFP_ACCOUNT;
 #endif
-		node = kmem_cache_alloc(radix_tree_node_cachep, gfp);
+		node = kmem_cache_alloc(xarray_cachep, gfp);
 		if (!node) {
 			xas_set_err(xas, -ENOMEM);
 			return NULL;