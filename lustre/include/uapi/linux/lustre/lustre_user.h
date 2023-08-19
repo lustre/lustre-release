lustre/lustre-release

commit fdbf2ffd41fa5660782d5ca8489ec2eb644c8113

LU-14677 sec: no encryption key migrate/extend/resync/split
Allow some layout operations on encrypted files, even when the
encryption key is not available:
- lfs migrate
- lfs mirror extend
- lfs mirror resync
- lfs mirror verify
- lfs mirror split
We allow these access patterns to applications that know what they are
doing, by using the specific flag O_FILE_ENC and O_DIRECT.

Also add sanity-sec test_59a,b,c to exercise these access patterns.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Ieaeee0e5bf7643f18d775fe6daa5e31c2f349f8c
Reviewed-on: https://review.whamcloud.com/44024
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -683,6 +683,10 @@ struct fsxattr {
 #define O_LOV_DELAY_CREATE_MASK	(O_NOCTTY | FASYNC)
 #define O_LOV_DELAY_CREATE		(O_LOV_DELAY_CREATE_1_8 | \
 					 O_LOV_DELAY_CREATE_MASK)
+/* O_FILE_ENC principle is similar to O_LOV_DELAY_CREATE above,
+ * for access to encrypted files without the encryption key.
+ */
+#define O_FILE_ENC		(O_NOCTTY | O_NDELAY)
 
 #define LL_FILE_IGNORE_LOCK     0x00000001
 #define LL_FILE_GROUP_LOCKED    0x00000002

lustre/llite/crypto.c
@@ -131,6 +131,34 @@ static int ll_set_context(struct inode *inode, const void *ctx, size_t len,
 	return ll_set_encflags(inode, (void *)ctx, len, false);
 }
 
+/**
+ * ll_file_open_encrypt() - overlay to llcrypt_file_open
+ * @inode: the inode being opened
+ * @filp: the struct file being set up
+ *
+ * This overlay function is necessary to handle encrypted file open without
+ * the key. We allow this access pattern to applications that know what they
+ * are doing, by using the specific flag O_FILE_ENC.
+ * This flag is only compatible with O_DIRECT IOs, to make sure ciphertext
+ * data is wiped from page cache once IOs are finished.
+ */
+int ll_file_open_encrypt(struct inode *inode, struct file *filp)
+{
+	int rc;
+
+	rc = llcrypt_file_open(inode, filp);
+	if (likely(rc != -ENOKEY))
+		return rc;
+
+	if (rc == -ENOKEY &&
+	    (filp->f_flags & O_FILE_ENC) == O_FILE_ENC &&
+	    filp->f_flags & O_DIRECT)
+		/* allow file open with O_FILE_ENC flag when we have O_DIRECT */
+		rc = 0;
+
+	return rc;
+}
+
 void llcrypt_free_ctx(void *encctx, __u32 size)
 {
 	if (encctx)
@@ -218,6 +246,17 @@ int ll_setup_filename(struct inode *dir, const struct qstr *iname,
 		fid->f_ver = 0;
 	}
 	rc = llcrypt_setup_filename(dir, &dname, lookup, fname);
+	if (rc == -ENOENT && lookup &&
+	    !llcrypt_has_encryption_key(dir) &&
+	    unlikely(filename_is_volatile(iname->name, iname->len, NULL))) {
+		/* For purpose of migration or mirroring without enc key, we
+		 * allow lookup of volatile file without enc context.
+		 */
+		memset(fname, 0, sizeof(struct llcrypt_name));
+		fname->disk_name.name = (unsigned char *)iname->name;
+		fname->disk_name.len = iname->len;
+		rc = 0;
+	}
 	if (rc)
 		return rc;
 
@@ -416,6 +455,11 @@ int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
 		    bool preload)
 {
 	return 0;
+}
+
+int ll_file_open_encrypt(struct inode *inode, struct file *filp)
+{
+	return llcrypt_file_open(inode, filp);
 }
 
 void llcrypt_free_ctx(void *encctx, __u32 size)

lustre/llite/namei.c
@@ -49,7 +49,7 @@
 static int ll_create_it(struct inode *dir, struct dentry *dentry,
 			struct lookup_intent *it,
 			void *secctx, __u32 secctxlen, bool encrypt,
-			void *encctx, __u32 encctxlen);
+			void *encctx, __u32 encctxlen, unsigned int open_flags);
 
 /* called from iget5_locked->find_inode() under inode_lock spinlock */
 static int ll_test_inode(struct inode *inode, void *opaque)
@@ -908,37 +908,20 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 			*secctxlen = 0;
 	}
 	if (it->it_op & IT_CREAT && encrypt) {
-		/* Volatile file name may look like:
-		 * <parent>/LUSTRE_VOLATILE_HDR:<mdt_index>:<random>:fd=<fd>
-		 * where fd is opened descriptor of reference file.
-		 */
 		if (unlikely(filename_is_volatile(dentry->d_name.name,
 						  dentry->d_name.len, NULL))) {
+			/* get encryption context from reference file */
 			int ctx_size = LLCRYPT_ENC_CTX_SIZE;
 			struct lustre_sb_info *lsi;
 			struct file *ref_file;
 			struct inode *ref_inode;
-			char *p, *q, *fd_str;
 			void *ctx;
-			int fd;
 
-			p = strnstr(dentry->d_name.name, ":fd=",
-				    dentry->d_name.len);
-			if (!p || strlen(p + 4) == 0)
-				GOTO(out, retval = ERR_PTR(-EINVAL));
-
-			q = strchrnul(p + 4, ':');
-			fd_str = kstrndup(p + 4, q - p - 4, GFP_NOFS);
-			if (!fd_str)
-				GOTO(out, retval = ERR_PTR(-ENOMEM));
-			rc = kstrtouint(fd_str, 10, &fd);
-			kfree(fd_str);
+			rc = volatile_ref_file(dentry->d_name.name,
+					       dentry->d_name.len,
+					       &ref_file);
 			if (rc)
-				GOTO(inherit, rc = -EINVAL);
-
-			ref_file = fget(fd);
-			if (!ref_file)
-				GOTO(inherit, rc = -EINVAL);
+				GOTO(out, retval = ERR_PTR(rc));
 
 			ref_inode = file_inode(ref_file);
 			if (!ref_inode) {
@@ -984,7 +967,6 @@ getctx:
 				       op_data->op_file_encctx_size);
 				OBD_FREE(ctx, ctx_size);
 			}
-
 		} else {
 inherit:
 			rc = llcrypt_inherit_context(parent, NULL, op_data,
@@ -1252,7 +1234,14 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 		if (rc)
 			GOTO(out_release, rc);
 		if (open_flags & O_CREAT) {
-			if (!llcrypt_has_encryption_key(dir))
+			/* For migration or mirroring without enc key, we still
+			 * need to be able to create a volatile file.
+			 */
+			if (!llcrypt_has_encryption_key(dir) &&
+			    (!filename_is_volatile(dentry->d_name.name,
+						   dentry->d_name.len, NULL) ||
+			    (open_flags & O_FILE_ENC) != O_FILE_ENC ||
+			    !(open_flags & O_DIRECT)))
 				GOTO(out_release, rc = -ENOKEY);
 			encrypt = true;
 		}
@@ -1283,7 +1272,8 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 		if (it_disposition(it, DISP_OPEN_CREATE)) {
 			/* Dentry instantiated in ll_create_it. */
 			rc = ll_create_it(dir, dentry, it, secctx, secctxlen,
-					  encrypt, encctx, encctxlen);
+					  encrypt, encctx, encctxlen,
+					  open_flags);
 			ll_security_release_secctx(secctx, secctxlen);
 			llcrypt_free_ctx(encctx, encctxlen);
 			if (rc) {
@@ -1409,7 +1399,7 @@ static struct inode *ll_create_node(struct inode *dir, struct lookup_intent *it)
 static int ll_create_it(struct inode *dir, struct dentry *dentry,
 			struct lookup_intent *it,
 			void *secctx, __u32 secctxlen, bool encrypt,
-			void *encctx, __u32 encctxlen)
+			void *encctx, __u32 encctxlen, unsigned int open_flags)
 {
 	struct inode *inode;
 	__u64 bits = 0;
@@ -1444,7 +1434,18 @@ static int ll_create_it(struct inode *dir, struct dentry *dentry,
 	d_instantiate(dentry, inode);
 
 	if (encrypt) {
-		rc = ll_set_encflags(inode, encctx, encctxlen, true);
+		bool preload = true;
+
+		/* For migration or mirroring without enc key, we
+		 * create a volatile file without enc context.
+		 */
+		if (!llcrypt_has_encryption_key(dir) &&
+		    filename_is_volatile(dentry->d_name.name,
+					 dentry->d_name.len, NULL) &&
+		    (open_flags & O_FILE_ENC) == O_FILE_ENC &&
+		    open_flags & O_DIRECT)
+			preload = false;
+		rc = ll_set_encflags(inode, encctx, encctxlen, preload);
 		if (rc)
 			RETURN(rc);
 	}

lustre/utils/lfs.c
@@ -624,12 +624,10 @@ static uint32_t check_foreign_type_name(const char *foreign_type_name)
 
 static const char *error_loc = "syserror";
 
-enum {
-	MIGRATION_NONBLOCK	= 0x0001,
-	MIGRATION_MIRROR	= 0x0002,
-	MIGRATION_NONDIRECT	= 0x0004,
-	MIGRATION_VERBOSE	= 0x0008,
-};
+#define MIGRATION_NONBLOCK	LLAPI_MIGRATION_NONBLOCK
+#define MIGRATION_MIRROR	LLAPI_MIGRATION_MIRROR
+#define MIGRATION_NONDIRECT	LLAPI_MIGRATION_NONDIRECT
+#define MIGRATION_VERBOSE	LLAPI_MIGRATION_VERBOSE
 
 static int
 migrate_open_files(const char *name, __u64 migration_flags,
@@ -672,15 +670,24 @@ migrate_open_files(const char *name, __u64 migration_flags,
 		*ptr = '\0';
 	}
 
-	/* open file, direct io */
 	/* even if the file is only read, WR mode is nedeed to allow
 	 * layout swap on fd
 	 */
-	rflags = O_RDWR | O_NOATIME;
+	/* Allow migrating even without the key on encrypted files */
+	rflags = O_RDWR | O_NOATIME | O_FILE_ENC;
 	if (!(migration_flags & MIGRATION_NONDIRECT))
 		rflags |= O_DIRECT;
+source_open:
 	fd = open(name, rflags);
 	if (fd < 0) {
+		/* If encrypted file without the key,
+		 * retry mirror extend in O_DIRECT.
+		 */
+		if (errno == ENOKEY && !(rflags & O_DIRECT) &&
+		    migration_flags & MIGRATION_MIRROR) {
+			rflags |= O_DIRECT;
+			goto source_open;
+		}
 		rc = -errno;
 		error_loc = "cannot open source file";
 		return rc;
@@ -693,9 +700,13 @@ migrate_open_files(const char *name, __u64 migration_flags,
 	}
 
 	do {
-		int open_flags = O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW;
+		int open_flags = O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW |
+			/* Allow migrating without the key on encrypted files */
+			O_FILE_ENC;
 		mode_t open_mode = S_IRUSR | S_IWUSR;
 
+		if (rflags & O_DIRECT)
+			open_flags |= O_DIRECT;
 		random_value = random();
 		rc = snprintf(volatile_file, sizeof(volatile_file),
 			      "%s/%s:%.4X:%.4X:fd=%.2d", parent,
@@ -1831,8 +1842,8 @@ static int mirror_extend_layout(char *name, struct llapi_layout *m_layout,
 		}
 	}
 	llapi_layout_comp_flags_set(m_layout, flags);
-	rc = migrate_open_files(name, MIGRATION_NONDIRECT, NULL, m_layout, &fd,
-				&fdv);
+	rc = migrate_open_files(name, MIGRATION_NONDIRECT | MIGRATION_MIRROR,
+				NULL, m_layout, &fd, &fdv);
 	if (rc < 0)
 		goto out;
 
@@ -2153,7 +2164,15 @@ static int mirror_split(const char *fname, __u32 id, const char *pool,
 		}
 	}
 
-	fd = open(fname, O_RDWR);
+	if (!victim_file && mflags & MF_DESTROY)
+		/* Allow mirror split even without the key on encrypted files,
+		 * and in this case of a 'split -d', open file with O_DIRECT
+		 * (no IOs will be done).
+		 */
+		fd = open(fname, O_RDWR | O_DIRECT | O_FILE_ENC);
+	else
+		fd = open(fname, O_RDWR);
+
 	if (fd < 0) {
 		fprintf(stderr,
 			"error %s: open file '%s' failed: %s\n",
@@ -2237,7 +2256,11 @@ again:
 
 					open_flags = O_RDWR |
 					     (O_LOV_DELAY_CREATE & ~O_ACCMODE) |
-					     O_CREAT | O_EXCL | O_NOFOLLOW;
+					     O_CREAT | O_EXCL | O_NOFOLLOW |
+					     /* O_DIRECT for mirror split -d */
+					     O_DIRECT |
+					     /* Allow split without the key */
+					     O_FILE_ENC;
 					fdv = open(file_path, open_flags,
 						   S_IRUSR | S_IWUSR);
 					if (fdv < 0)
@@ -10582,7 +10605,8 @@ int lfs_mirror_resync_file(const char *fname, struct ll_ioc_lease *ioc,
 		goto error;
 	}
 
-	fd = open(fname, O_DIRECT | O_RDWR);
+	/* Allow mirror resync even without the key on encrypted files */
+	fd = open(fname, O_DIRECT | O_RDWR | O_FILE_ENC);
 	if (fd < 0) {
 		fprintf(stderr, "%s: cannot open '%s': %s.\n",
 			progname, fname, strerror(errno));
@@ -11880,7 +11904,8 @@ int lfs_mirror_verify_file(const char *fname, __u16 *mirror_ids, int ids_nr,
 		goto error;
 	}
 
-	fd = open(fname, O_DIRECT | O_RDONLY);
+	/* Allow mirror verify even without the key on encrypted files */
+	fd = open(fname, O_DIRECT | O_RDONLY | O_FILE_ENC);
 	if (fd < 0) {
 		fprintf(stderr, "%s: cannot open '%s': %s.\n",
 			progname, fname, strerror(errno));

lustre/utils/liblustreapi_layout.c
@@ -1069,15 +1069,27 @@ struct llapi_layout *llapi_layout_get_by_path(const char *path,
 					      enum llapi_layout_get_flags flags)
 {
 	struct llapi_layout *layout = NULL;
+	bool failed = false;
+	int open_flags;
 	int fd;
 	int tmp;
 
 	if (flags & LLAPI_LAYOUT_GET_EXPECTED)
 		return llapi_layout_expected(path);
 
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-		return layout;
+	/* Always get layout in O_DIRECT */
+	/* Allow fetching layout even without the key on encrypted files */
+	open_flags = O_RDONLY | O_DIRECT | O_FILE_ENC;
+do_open:
+	fd = open(path, open_flags);
+	if (fd < 0) {
+		if (errno != EINVAL || failed)
+			return layout;
+		/* EINVAL is because a directory cannot be opened in O_DIRECT */
+		open_flags = O_RDONLY | O_FILE_ENC;
+		failed = true;
+		goto do_open;
+	}
 
 	layout = llapi_layout_get_by_fd(fd, flags);
 	tmp = errno;
@@ -3110,7 +3122,11 @@ do_read:
 		if (comp_array[i].lrc_synced && pos & (page_size - 1)) {
 			rc = llapi_mirror_truncate(fd,
 					comp_array[i].lrc_mirror_id, pos);
-			if (rc < 0)
+			/* Ignore truncate error on encrypted file without the
+			 * key if tried on LUSTRE_ENCRYPTION_UNIT_SIZE boundary.
+			 */
+			if (rc < 0 && (rc != -ENOKEY ||
+				       pos & ~LUSTRE_ENCRYPTION_MASK))
 				comp_array[i].lrc_synced = false;
 		}
 	}

lustre/doc/lfs-migrate.1
@@ -72,6 +72,11 @@ write operations synchronous.  Using the
 .B --non-direct
 option uses buffered read/write operations, which may improve migration
 speed at the cost of more CPU and memory overhead.
+.br
+This option cannot be used on encrypted files when the encryption key is not
+available. It will result in
+.B
+-ENOKEY.
 .TP
 .BR -v , --verbose
 Print each filename as it is migrated.

lustre/doc/lfs-mirror-split.1
@@ -40,10 +40,20 @@ The pool storing a component contained within a mirror.
 .TP
 .BR \-\-destroy\fR|\fB\-d\fR
 This option indicates the split mirror will be destroyed.
+.br
+This option is mandatory on encrypted files when the encryption key is not
+available. Otherwise operation results in
+.B
+-ENOKEY.
 .TP
 .BR \-f\fR\ <\fInew_file\fR>
 This option indicates the layout of the split mirror will be stored into
 <\fInew_file\fR>.
+.br
+This option cannot be used on encrypted files when the encryption key is not
+available. It will result in
+.B
+-ENOKEY.
 .SH EXAMPLES
 .TP
 .B lfs mirror split --mirror-id 1 /mnt/lustre/file1

lustre/include/lustre/lustreapi.h
@@ -148,6 +148,13 @@ struct llapi_stripe_param {
 
 #define lsp_tgts	lsp_osts
 
+enum {
+	LLAPI_MIGRATION_NONBLOCK	= 0x0001,
+	LLAPI_MIGRATION_MIRROR		= 0x0002,
+	LLAPI_MIGRATION_NONDIRECT	= 0x0004,
+	LLAPI_MIGRATION_VERBOSE		= 0x0008,
+};
+
 __u32 llapi_pattern_to_lov(uint64_t pattern);
 
 int llapi_file_open_param(const char *name, int flags, mode_t mode,

commit ac522557b1fe3ea2b7275fa6d5df73691b8d06db

LU-16374 enc: rename O_FILE_ENC to O_CIPHERTEXT
Rename O_FILE_ENC to O_CIPHERTEXT as per discussion in linux-fscrypt
mailing-list.
Also change the flag combination to be:
O_NOCTTY | O_NDELAY | O_DSYNC
to avoid the risk of accidental issues with tar that already opens
files with the 'O_NOCTTY | O_NDELAY' combination.

O_DSYNC does not make much sense for O_RDONLY files, but will force
writes on encrypted restore to be synchronous. With O_DIRECT and large
enough writes (32MB?) that might be OK, but not ideal for small files.

Fixes: fdbf2ffd41 ("LU-14677 sec: no encryption key migrate/extend/resync/split")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I36fed17a413ee690bc445c3e76674ed5fc337de5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51640
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -686,10 +686,10 @@ struct fsxattr {
 #define O_LOV_DELAY_CREATE_MASK	(O_NOCTTY | FASYNC)
 #define O_LOV_DELAY_CREATE		(O_LOV_DELAY_CREATE_1_8 | \
 					 O_LOV_DELAY_CREATE_MASK)
-/* O_FILE_ENC principle is similar to O_LOV_DELAY_CREATE above,
+/* O_CIPHERTEXT principle is similar to O_LOV_DELAY_CREATE above,
  * for access to encrypted files without the encryption key.
  */
-#define O_FILE_ENC		(O_NOCTTY | O_NDELAY)
+#define O_CIPHERTEXT		(O_NOCTTY | O_NDELAY | O_DSYNC)
 
 #define LL_FILE_IGNORE_LOCK     0x00000001
 #define LL_FILE_GROUP_LOCKED    0x00000002

lustre/llite/crypto.c
@@ -126,7 +126,7 @@ static int ll_set_context(struct inode *inode, const void *ctx, size_t len,
  *
  * This overlay function is necessary to handle encrypted file open without
  * the key. We allow this access pattern to applications that know what they
- * are doing, by using the specific flag O_FILE_ENC.
+ * are doing, by using the specific flag O_CIPHERTEXT.
  * This flag is only compatible with O_DIRECT IOs, to make sure ciphertext
  * data is wiped from page cache once IOs are finished.
  */
@@ -139,9 +139,9 @@ int ll_file_open_encrypt(struct inode *inode, struct file *filp)
 		return rc;
 
 	if (rc == -ENOKEY &&
-	    (filp->f_flags & O_FILE_ENC) == O_FILE_ENC &&
+	    (filp->f_flags & O_CIPHERTEXT) == O_CIPHERTEXT &&
 	    filp->f_flags & O_DIRECT)
-		/* allow file open with O_FILE_ENC flag when we have O_DIRECT */
+		/* allow open with O_CIPHERTEXT flag when we have O_DIRECT */
 		rc = 0;
 
 	return rc;

lustre/llite/namei.c
@@ -1247,7 +1247,7 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 			if (!llcrypt_has_encryption_key(dir) &&
 			    (!filename_is_volatile(dentry->d_name.name,
 						   dentry->d_name.len, NULL) ||
-			    (open_flags & O_FILE_ENC) != O_FILE_ENC ||
+			    (open_flags & O_CIPHERTEXT) != O_CIPHERTEXT ||
 			    !(open_flags & O_DIRECT)))
 				GOTO(out_release, rc = -ENOKEY);
 		}
@@ -1450,7 +1450,7 @@ static int ll_create_it(struct inode *dir, struct dentry *dentry,
 		if (!llcrypt_has_encryption_key(dir) &&
 		    filename_is_volatile(dentry->d_name.name,
 					 dentry->d_name.len, NULL) &&
-		    (open_flags & O_FILE_ENC) == O_FILE_ENC &&
+		    (open_flags & O_CIPHERTEXT) == O_CIPHERTEXT &&
 		    open_flags & O_DIRECT)
 			preload = false;
 		rc = ll_set_encflags(inode, encctx, encctxlen, preload);

lustre/utils/lfs.c
@@ -663,7 +663,7 @@ migrate_open_files(const char *name, __u64 migration_flags,
 	 * layout swap on fd
 	 */
 	/* Allow migrating even without the key on encrypted files */
-	rflags = O_RDWR | O_NOATIME | O_FILE_ENC;
+	rflags = O_RDWR | O_NOATIME | O_CIPHERTEXT;
 	if (!(migration_flags & LLAPI_MIGRATION_NONDIRECT))
 		rflags |= O_DIRECT;
 source_open:
@@ -691,7 +691,7 @@ source_open:
 	do {
 		int open_flags = O_WRONLY | O_CREAT | O_EXCL | O_NOFOLLOW |
 			/* Allow migrating without the key on encrypted files */
-			O_FILE_ENC;
+			O_CIPHERTEXT;
 		mode_t open_mode = S_IRUSR | S_IWUSR;
 
 		if (rflags & O_DIRECT)
@@ -2326,7 +2326,7 @@ static int mirror_split(const char *fname, __u32 id, const char *pool,
 		 * and in this case of a 'split -d', open file with O_DIRECT
 		 * (no IOs will be done).
 		 */
-		fd = open(fname, O_RDWR | O_DIRECT | O_FILE_ENC);
+		fd = open(fname, O_RDWR | O_DIRECT | O_CIPHERTEXT);
 	else
 		fd = open(fname, O_RDWR);
 
@@ -2417,7 +2417,7 @@ again:
 					     /* O_DIRECT for mirror split -d */
 					     O_DIRECT |
 					     /* Allow split without the key */
-					     O_FILE_ENC;
+					     O_CIPHERTEXT;
 					fdv = open(file_path, open_flags,
 						   S_IRUSR | S_IWUSR);
 					if (fdv < 0)
@@ -11208,7 +11208,7 @@ int lfs_mirror_resync_file(const char *fname, struct ll_ioc_lease *ioc,
 	}
 
 	/* Allow mirror resync even without the key on encrypted files */
-	fd = open(fname, O_DIRECT | O_RDWR | O_FILE_ENC);
+	fd = open(fname, O_DIRECT | O_RDWR | O_CIPHERTEXT);
 	if (fd < 0) {
 		fprintf(stderr, "%s: cannot open '%s': %s.\n",
 			progname, fname, strerror(errno));
@@ -12507,7 +12507,7 @@ int lfs_mirror_verify_file(const char *fname, __u16 *mirror_ids, int ids_nr,
 	}
 
 	/* Allow mirror verify even without the key on encrypted files */
-	fd = open(fname, O_DIRECT | O_RDONLY | O_FILE_ENC);
+	fd = open(fname, O_DIRECT | O_RDONLY | O_CIPHERTEXT);
 	if (fd < 0) {
 		fprintf(stderr, "%s: cannot open '%s': %s.\n",
 			progname, fname, strerror(errno));

lustre/utils/liblustreapi_layout.c
@@ -1079,14 +1079,14 @@ struct llapi_layout *llapi_layout_get_by_path(const char *path,
 
 	/* Always get layout in O_DIRECT */
 	/* Allow fetching layout even without the key on encrypted files */
-	open_flags = O_RDONLY | O_DIRECT | O_FILE_ENC;
+	open_flags = O_RDONLY | O_DIRECT | O_CIPHERTEXT;
 do_open:
 	fd = open(path, open_flags);
 	if (fd < 0) {
 		if (errno != EINVAL || failed)
 			return layout;
 		/* EINVAL is because a directory cannot be opened in O_DIRECT */
-		open_flags = O_RDONLY | O_FILE_ENC;
+		open_flags = O_RDONLY | O_CIPHERTEXT;
 		failed = true;
 		goto do_open;
 	}