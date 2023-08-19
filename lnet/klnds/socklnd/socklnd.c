lustre/lustre-release

commit a5cbe7883db6d77b82fbd83ad4c662499421d229

LU-12815 socklnd: allow dynamic setting of conns_per_peer
Modify lnetctl and associated code to allow dynamic setting
of conns_per_peer lnd parameter per ni.

The parameter can be set for a specific active nid:
        lnetctl net set --nid 192.168.122.10@tcp --conns-per-peer=4

Or when adding a new net, taking effect on the new nid:
        lnetctl net add --net tcp --if eth0 --conns-per-peer=1

By default, conns_per_peer value specified as the module parameter
shall be used.

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Change-Id: I11625b9ad61f0311c294001a38b7855465491aaf
Reviewed-on: https://review.whamcloud.com/41463
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd.c
@@ -36,6 +36,7 @@
  * Author: Eric Barton <eric@bartonsoftware.com>
  */
 
+#include <linux/ethtool.h>
 #include <linux/inetdevice.h>
 #include "socklnd.h"
 #include <linux/sunrpc/addr.h>
@@ -134,6 +135,7 @@ ksocknal_create_conn_cb(struct sockaddr *addr)
 	conn_cb->ksnr_ctrl_conn_count = 0;
 	conn_cb->ksnr_blki_conn_count = 0;
 	conn_cb->ksnr_blko_conn_count = 0;
+	conn_cb->ksnr_max_conns = 0;
 
 	return conn_cb;
 }
@@ -393,6 +395,19 @@ ksocknal_get_conn_count_by_type(struct ksock_conn_cb *conn_cb,
 	return count;
 }
 
+static unsigned int
+ksocknal_get_conns_per_peer(struct ksock_peer_ni *peer_ni)
+{
+	struct lnet_ni *ni = peer_ni->ksnp_ni;
+	struct lnet_ioctl_config_socklnd_tunables *tunables;
+
+	LASSERT(ni);
+
+	tunables = &ni->ni_lnd_tunables.lnd_tun_u.lnd_sock;
+
+	return tunables->lnd_conns_per_peer;
+}
+
 static void
 ksocknal_incr_conn_count(struct ksock_conn_cb *conn_cb,
 			 int type)
@@ -408,29 +423,25 @@ ksocknal_incr_conn_count(struct ksock_conn_cb *conn_cb,
 		break;
 	case SOCKLND_CONN_BULK_IN:
 		conn_cb->ksnr_blki_conn_count++;
-		if (conn_cb->ksnr_blki_conn_count >=
-		    *ksocknal_tunables.ksnd_conns_per_peer)
+		if (conn_cb->ksnr_blki_conn_count >= conn_cb->ksnr_max_conns)
 			conn_cb->ksnr_connected |= BIT(type);
 		break;
 	case SOCKLND_CONN_BULK_OUT:
 		conn_cb->ksnr_blko_conn_count++;
-		if (conn_cb->ksnr_blko_conn_count >=
-		    *ksocknal_tunables.ksnd_conns_per_peer)
+		if (conn_cb->ksnr_blko_conn_count >= conn_cb->ksnr_max_conns)
 			conn_cb->ksnr_connected |= BIT(type);
 		break;
 	case SOCKLND_CONN_ANY:
-		if (conn_cb->ksnr_conn_count >=
-		    *ksocknal_tunables.ksnd_conns_per_peer)
+		if (conn_cb->ksnr_conn_count >= conn_cb->ksnr_max_conns)
 			conn_cb->ksnr_connected |= BIT(type);
 		break;
 	default:
 		LBUG();
 		break;
-
 	}
 
-	CDEBUG(D_NET, "Add conn type %d, ksnr_connected %x conns_per_peer %d\n",
-	       type, conn_cb->ksnr_connected, *ksocknal_tunables.ksnd_conns_per_peer);
+	CDEBUG(D_NET, "Add conn type %d, ksnr_connected %x ksnr_max_conns %d\n",
+	       type, conn_cb->ksnr_connected, conn_cb->ksnr_max_conns);
 }
 
 static void
@@ -591,6 +602,13 @@ ksocknal_add_peer(struct lnet_ni *ni, struct lnet_process_id id,
 	}
 
 	ksocknal_add_conn_cb_locked(peer_ni, conn_cb);
+
+	/* Remember conns_per_peer setting at the time
+	 * of connection initiation. It will define the
+	 * max number of conns per type for this conn_cb
+	 * while it's in use.
+	 */
+	conn_cb->ksnr_max_conns = ksocknal_get_conns_per_peer(peer_ni);
 
 	write_unlock_bh(&ksocknal_data.ksnd_global_lock);
 
@@ -988,7 +1006,13 @@ ksocknal_create_conn(struct lnet_ni *ni, struct ksock_conn_cb *conn_cb,
 				continue;
 
 			num_dup++;
-			if (num_dup < *ksocknal_tunables.ksnd_conns_per_peer)
+			/* If max conns per type is not registered in conn_cb
+			 * as ksnr_max_conns, use ni's conns_per_peer
+			 */
+			if ((peer_ni->ksnp_conn_cb &&
+			    num_dup < peer_ni->ksnp_conn_cb->ksnr_max_conns) ||
+			    (!peer_ni->ksnp_conn_cb &&
+			    num_dup < ksocknal_get_conns_per_peer(peer_ni)))
 				continue;
 
 			/* Reply on a passive connection attempt so the peer_ni
@@ -1212,7 +1236,7 @@ ksocknal_close_conn_locked(struct ksock_conn *conn, int error)
 		 * of the given type got created
 		 */
 		if (ksocknal_get_conn_count_by_type(conn_cb, conn->ksnc_type) ==
-		    *ksocknal_tunables.ksnd_conns_per_peer)
+		    conn_cb->ksnr_max_conns)
 			LASSERT((conn_cb->ksnr_connected &
 				BIT(conn->ksnc_type)) != 0);
 
@@ -2268,7 +2292,6 @@ int
 ksocknal_startup(struct lnet_ni *ni)
 {
 	struct ksock_net *net;
-	struct lnet_ioctl_config_lnd_cmn_tunables *net_tunables;
 	struct ksock_interface *ksi = NULL;
 	struct lnet_inetdev *ifaces = NULL;
 	struct sockaddr_in *sa;
@@ -2286,27 +2309,8 @@ ksocknal_startup(struct lnet_ni *ni)
 		goto fail_0;
 	net->ksnn_incarnation = ktime_get_real_ns();
 	ni->ni_data = net;
-	net_tunables = &ni->ni_net->net_tunables;
-	if (net_tunables->lct_peer_timeout == -1)
-		net_tunables->lct_peer_timeout =
-			*ksocknal_tunables.ksnd_peertimeout;
 
-	if (net_tunables->lct_max_tx_credits == -1)
-		net_tunables->lct_max_tx_credits =
-			*ksocknal_tunables.ksnd_credits;
-
-	if (net_tunables->lct_peer_tx_credits == -1)
-		net_tunables->lct_peer_tx_credits =
-			*ksocknal_tunables.ksnd_peertxcredits;
-
-	if (net_tunables->lct_peer_tx_credits >
-	    net_tunables->lct_max_tx_credits)
-		net_tunables->lct_peer_tx_credits =
-			net_tunables->lct_max_tx_credits;
-
-	if (net_tunables->lct_peer_rtr_credits == -1)
-		net_tunables->lct_peer_rtr_credits =
-			*ksocknal_tunables.ksnd_peerrtrcredits;
+	ksocknal_tunables_setup(ni);
 
 	rc = lnet_inet_enumerate(&ifaces, ni->ni_net_ns);
 	if (rc < 0)

lnet/include/lnet/lib-lnet.h
@@ -78,6 +78,9 @@ extern struct lnet the_lnet;			/* THE network */
 #define DEFAULT_PEER_CREDITS    8
 #define DEFAULT_CREDITS         256
 
+/* default number of connections per peer */
+#define DEFAULT_CONNS_PER_PEER  1
+
 #ifdef HAVE_KERN_SOCK_GETNAME_2ARGS
 #define lnet_kernel_getpeername(sock, addr, addrlen) \
 		kernel_getpeername(sock, addr)

lnet/include/uapi/linux/lnet/libcfs_ioctl.h
@@ -154,7 +154,8 @@ struct libcfs_ioctl_data {
 #define IOC_LIBCFS_GET_UDSP		   _IOWR(IOC_LIBCFS_TYPE, 108, IOCTL_CONFIG_SIZE)
 #define IOC_LIBCFS_GET_CONST_UDSP_INFO	   _IOWR(IOC_LIBCFS_TYPE, 109, IOCTL_CONFIG_SIZE)
 #define IOC_LIBCFS_RESET_LNET_STATS	   _IOWR(IOC_LIBCFS_TYPE, 110, IOCTL_CONFIG_SIZE)
-#define IOC_LIBCFS_MAX_NR					  110
+#define IOC_LIBCFS_SET_CONNS_PER_PEER	   _IOWR(IOC_LIBCFS_TYPE, 111, IOCTL_CONFIG_SIZE)
+#define IOC_LIBCFS_MAX_NR					  111
 
 extern int libcfs_ioctl_data_adjust(struct libcfs_ioctl_data *data);
 

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -81,9 +81,16 @@ struct lnet_ioctl_config_o2iblnd_tunables {
 	__u16 lnd_ntx;
 };
 
+struct lnet_ioctl_config_socklnd_tunables {
+	__u32 lnd_version;
+	__u16 lnd_conns_per_peer;
+	__u16 lnd_pad;
+};
+
 struct lnet_lnd_tunables {
 	union {
 		struct lnet_ioctl_config_o2iblnd_tunables lnd_o2ib;
+		struct lnet_ioctl_config_socklnd_tunables lnd_sock;
 	} lnd_tun_u;
 };
 
@@ -278,6 +285,13 @@ struct lnet_ioctl_reset_health_cfg {
 	__u16 rh_all:1;
 	__s16 rh_value;
 	lnet_nid_t rh_nid;
+};
+
+struct lnet_ioctl_reset_conns_per_peer_cfg {
+	struct libcfs_ioctl_hdr rcpp_hdr;
+	__u16 rcpp_all:1;
+	__s16 rcpp_value;
+	lnet_nid_t rcpp_nid;
 };
 
 struct lnet_ioctl_recovery_list {

lnet/klnds/socklnd/socklnd.h
@@ -383,7 +383,9 @@ struct ksock_conn_cb {
 	unsigned int		ksnr_blki_conn_count:8;
 	unsigned int		ksnr_blko_conn_count:8;
 	int			ksnr_conn_count;/* total # conns for this cb */
-
+	unsigned int		ksnr_max_conns; /* conns_per_peer at peer
+						 * creation
+						 */
 };
 
 #define SOCKNAL_KEEPALIVE_PING          1       /* cookie for keepalive ping */
@@ -668,6 +670,7 @@ extern int ksocknal_lib_get_conn_tunables(struct ksock_conn *conn, int *txmem,
 					  int *rxmem, int *nagle);
 
 extern int ksocknal_tunables_init(void);
+extern void ksocknal_tunables_setup(struct lnet_ni *ni);
 
 extern void ksocknal_lib_csum_tx(struct ksock_tx *tx);
 

lnet/klnds/socklnd/socklnd_modparams.c
@@ -26,6 +26,8 @@
 #include <asm/hypervisor.h>
 #endif
 
+#define CURRENT_LND_VERSION 1
+
 static int sock_timeout;
 module_param(sock_timeout, int, 0644);
 MODULE_PARM_DESC(sock_timeout, "dead socket timeout (seconds)");
@@ -142,8 +144,8 @@ static unsigned int zc_recv_min_nfrags = 16;
 module_param(zc_recv_min_nfrags, int, 0644);
 MODULE_PARM_DESC(zc_recv_min_nfrags, "minimum # of fragments to enable ZC recv");
 
-static unsigned int conns_per_peer = 1;
-module_param(conns_per_peer, uint, 0444);
+static unsigned int conns_per_peer = DEFAULT_CONNS_PER_PEER;
+module_param(conns_per_peer, uint, 0644);
 MODULE_PARM_DESC(conns_per_peer, "number of connections per peer");
 
 #ifdef SOCKNAL_BACKOFF
@@ -174,9 +176,13 @@ static inline bool is_native_host(void)
 }
 
 struct ksock_tunables ksocknal_tunables;
+static struct lnet_ioctl_config_socklnd_tunables default_tunables;
 
 int ksocknal_tunables_init(void)
 {
+	default_tunables.lnd_version = CURRENT_LND_VERSION;
+	default_tunables.lnd_conns_per_peer = conns_per_peer;
+
 	/* initialize ksocknal_tunables structure */
 	ksocknal_tunables.ksnd_timeout            = &sock_timeout;
 	ksocknal_tunables.ksnd_nscheds		  = &nscheds;
@@ -238,4 +244,47 @@ int ksocknal_tunables_init(void)
 		*ksocknal_tunables.ksnd_zc_min_payload = (16 << 20) + 1;
 
 	return 0;
-};
+}
+
+void ksocknal_tunables_setup(struct lnet_ni *ni)
+{
+	struct lnet_ioctl_config_socklnd_tunables *tunables;
+	struct lnet_ioctl_config_lnd_cmn_tunables *net_tunables;
+
+	/* If no tunables specified, setup default tunables */
+	if (!ni->ni_lnd_tunables_set)
+		memcpy(&ni->ni_lnd_tunables.lnd_tun_u.lnd_sock,
+		       &default_tunables, sizeof(*tunables));
+
+	tunables = &ni->ni_lnd_tunables.lnd_tun_u.lnd_sock;
+
+	/* Current API version */
+	tunables->lnd_version = CURRENT_LND_VERSION;
+
+	net_tunables = &ni->ni_net->net_tunables;
+
+	if (net_tunables->lct_peer_timeout == -1)
+		net_tunables->lct_peer_timeout =
+			*ksocknal_tunables.ksnd_peertimeout;
+
+	if (net_tunables->lct_max_tx_credits == -1)
+		net_tunables->lct_max_tx_credits =
+			*ksocknal_tunables.ksnd_credits;
+
+	if (net_tunables->lct_peer_tx_credits == -1)
+		net_tunables->lct_peer_tx_credits =
+			*ksocknal_tunables.ksnd_peertxcredits;
+
+	if (net_tunables->lct_peer_tx_credits >
+	    net_tunables->lct_max_tx_credits)
+		net_tunables->lct_peer_tx_credits =
+			net_tunables->lct_max_tx_credits;
+
+	if (net_tunables->lct_peer_rtr_credits == -1)
+		net_tunables->lct_peer_rtr_credits =
+			*ksocknal_tunables.ksnd_peerrtrcredits;
+
+	if (!tunables->lnd_conns_per_peer)
+		tunables->lnd_conns_per_peer = (conns_per_peer) ?
+			conns_per_peer : DEFAULT_CONNS_PER_PEER;
+}

lnet/lnet/api-ni.c
@@ -39,7 +39,6 @@
 #ifdef HAVE_SCHED_HEADERS
 #include <linux/sched/signal.h>
 #endif
-
 #include <lnet/udsp.h>
 #include <lnet/lib-lnet.h>
 
@@ -3730,6 +3729,30 @@ lnet_ni_set_healthv(lnet_nid_t nid, int value, bool all)
 	lnet_net_unlock(LNET_LOCK_EX);
 }
 
+static void
+lnet_ni_set_conns_per_peer(lnet_nid_t nid, int value, bool all)
+{
+	struct lnet_net *net;
+	struct lnet_ni *ni;
+
+	lnet_net_lock(LNET_LOCK_EX);
+	list_for_each_entry(net, &the_lnet.ln_nets, net_list) {
+		list_for_each_entry(ni, &net->net_ni_list, ni_netlist) {
+			if (ni->ni_nid != nid && !all)
+				continue;
+			if (LNET_NETTYP(net->net_id) == SOCKLND)
+				ni->ni_lnd_tunables.lnd_tun_u.lnd_sock.lnd_conns_per_peer = value;
+			else if (LNET_NETTYP(net->net_id) == O2IBLND)
+				ni->ni_lnd_tunables.lnd_tun_u.lnd_o2ib.lnd_conns_per_peer = value;
+			if (!all) {
+				lnet_net_unlock(LNET_LOCK_EX);
+				return;
+			}
+		}
+	}
+	lnet_net_unlock(LNET_LOCK_EX);
+}
+
 static int
 lnet_get_local_ni_hstats(struct lnet_ioctl_local_ni_hstats *stats)
 {
@@ -4144,6 +4167,25 @@ LNetCtl(unsigned int cmd, void *arg)
 		else
 			lnet_peer_ni_set_healthv(cfg->rh_nid, value,
 						  cfg->rh_all);
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return 0;
+	}
+
+	case IOC_LIBCFS_SET_CONNS_PER_PEER: {
+		struct lnet_ioctl_reset_conns_per_peer_cfg *cfg = arg;
+		int value;
+
+		if (cfg->rcpp_hdr.ioc_len < sizeof(*cfg))
+			return -EINVAL;
+		if (cfg->rcpp_value < 0)
+			value = 1;
+		else
+			value = cfg->rcpp_value;
+		CDEBUG(D_NET,
+		       "Setting conns_per_peer to %d for %s. all = %d\n",
+		       value, libcfs_nid2str(cfg->rcpp_nid), cfg->rcpp_all);
+		mutex_lock(&the_lnet.ln_api_mutex);
+		lnet_ni_set_conns_per_peer(cfg->rcpp_nid, value, cfg->rcpp_all);
 		mutex_unlock(&the_lnet.ln_api_mutex);
 		return 0;
 	}

lnet/utils/lnetconfig/liblnetconfig.c
@@ -1555,7 +1555,7 @@ int lustre_lnet_config_ni(struct lnet_dlc_network_descr *nw_descr,
 			  struct cfs_expr_list *global_cpts,
 			  char *ip2net,
 			  struct lnet_ioctl_config_lnd_tunables *tunables,
-			  int seq_no, struct cYAML **err_rc)
+			  long int cpp, int seq_no, struct cYAML **err_rc)
 {
 	char *data = NULL;
 	struct lnet_ioctl_config_ni *conf;
@@ -1669,6 +1669,11 @@ int lustre_lnet_config_ni(struct lnet_dlc_network_descr *nw_descr,
 		rc = LUSTRE_CFG_RC_BAD_PARAM;
 		goto out;
 	}
+
+	if (LNET_NETTYP(nw_descr->nw_id) == SOCKLND && (cpp > -1))
+		tunables->lt_tun.lnd_tun_u.lnd_sock.lnd_conns_per_peer = cpp;
+	else if (LNET_NETTYP(nw_descr->nw_id) == O2IBLND && (cpp > -1))
+		tunables->lt_tun.lnd_tun_u.lnd_o2ib.lnd_conns_per_peer = cpp;
 
 	rc = lustre_lnet_intf2nids(nw_descr, &nids, &nnids,
 				   err_str, sizeof(err_str));
@@ -1812,6 +1817,42 @@ lustre_lnet_config_healthv(int value, bool all, lnet_nid_t nid,
 	return rc;
 }
 
+
+static int
+lustre_lnet_config_conns_per_peer(int value, bool all, lnet_nid_t nid,
+				  char *name, int seq_no,
+				  struct cYAML **err_rc)
+{
+	struct lnet_ioctl_reset_conns_per_peer_cfg data;
+	int rc = LUSTRE_CFG_RC_NO_ERR;
+	char err_str[LNET_MAX_STR_LEN] = "\"success\"";
+
+	LIBCFS_IOC_INIT_V2(data, rcpp_hdr);
+	data.rcpp_all = all;
+	data.rcpp_value = value;
+	data.rcpp_nid = nid;
+
+	if (value < 0 || value > 127) {
+		rc = LUSTRE_CFG_RC_BAD_PARAM;
+		snprintf(err_str, sizeof(err_str),
+			 "\"Valid values are: 0-127\"");
+	} else {
+
+		rc = l_ioctl(LNET_DEV_ID,
+			     IOC_LIBCFS_SET_CONNS_PER_PEER, &data);
+		if (rc != 0) {
+			rc = -errno;
+			snprintf(err_str,
+				 sizeof(err_str),
+				 "Can not configure conns_per_peer value: %s",
+				 strerror(errno));
+		}
+	}
+	cYAML_build_error(rc, seq_no, ADD_CMD, name, err_str, err_rc);
+
+	return rc;
+}
+
 int lustre_lnet_config_ni_healthv(int value, bool all, char *ni_nid, int seq_no,
 				  struct cYAML **err_rc)
 {
@@ -1836,6 +1877,20 @@ int lustre_lnet_config_peer_ni_healthv(int value, bool all, char *lpni_nid,
 	return lustre_lnet_config_healthv(value, all, nid,
 					  LNET_HEALTH_TYPE_PEER_NI,
 					  "peer_ni healthv", seq_no, err_rc);
+}
+
+int lustre_lnet_config_ni_conns_per_peer(int value, bool all, char *ni_nid,
+					 int seq_no, struct cYAML **err_rc)
+{
+	lnet_nid_t nid;
+
+	if (ni_nid)
+		nid = libcfs_str2nid(ni_nid);
+	else
+		nid = LNET_NID_ANY;
+	return lustre_lnet_config_conns_per_peer(value, all, nid,
+						 "ni conns_per_peer",
+						 seq_no, err_rc);
 }
 
 static bool
@@ -2272,18 +2327,19 @@ continue_without_msg_stats:
 			if (rc != LUSTRE_CFG_RC_NO_ERR)
 				goto out;
 
-			rc = lustre_ni_show_tunables(tunables, LNET_NETTYP(rc_net),
-						     &lnd->lt_tun);
-			if (rc != LUSTRE_CFG_RC_NO_ERR &&
-			    rc != LUSTRE_CFG_RC_NO_MATCH)
-				goto out;
-
 			if (rc != LUSTRE_CFG_RC_NO_MATCH) {
 				tunables = cYAML_create_object(item,
 							       "lnd tunables");
 				if (tunables == NULL)
 					goto out;
 			}
+
+			rc = lustre_ni_show_tunables(tunables,
+						     LNET_NETTYP(rc_net),
+						     &lnd->lt_tun);
+			if (rc != LUSTRE_CFG_RC_NO_ERR &&
+			    rc != LUSTRE_CFG_RC_NO_MATCH)
+				goto out;
 
 			if (!backup &&
 			    cYAML_create_number(item, "dev cpt",
@@ -4184,10 +4240,9 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 				      LNET_NETTYP(nw_descr.nw_id));
 	seq_no = cYAML_get_object_item(tree, "seq_no");
 
-	rc = lustre_lnet_config_ni(&nw_descr,
-				   global_cpts,
+	rc = lustre_lnet_config_ni(&nw_descr, global_cpts,
 				   (ip2net) ? ip2net->cy_valuestring : NULL,
-				   (found) ? &tunables: NULL,
+				   (found) ? &tunables : NULL, -1,
 				   (seq_no) ? seq_no->cy_valueint : -1,
 				   err_rc);
 

commit 9183fa655af4909623ea259f1b2bb3cbca00b8ba

LU-17021 socklnd: fix late ksnr_max_conns set
ksnr_max_conns was set to the correct value after it was used.

Test-Parameters: trivial
Fixes: a5cbe7883db6 ("LU-12815 socklnd: allow dynamic setting of conns_per_peer")
Signed-off-by: Cyril Bordage <cbordage@whamcloud.com>
Change-Id: I9f2454d915ee1ab27db96f5247028db94965a11f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51890
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/socklnd/socklnd.c
@@ -619,13 +619,13 @@ ksocknal_add_peer(struct lnet_ni *ni, struct lnet_processid *id,
 	if (peer_ni->ksnp_conn_cb) {
 		ksocknal_conn_cb_decref(conn_cb);
 	} else {
-		ksocknal_add_conn_cb_locked(peer_ni, conn_cb);
 		/* Remember conns_per_peer setting at the time
 		 * of connection initiation. It will define the
 		 * max number of conns per type for this conn_cb
 		 * while it's in use.
 		 */
 		conn_cb->ksnr_max_conns = ksocknal_get_conns_per_peer(peer_ni);
+		ksocknal_add_conn_cb_locked(peer_ni, conn_cb);
 	}
 
 	write_unlock_bh(&ksocknal_data.ksnd_global_lock);