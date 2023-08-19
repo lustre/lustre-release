lustre/lustre-release

commit 77eea1985bb1655e58c8b7df00703b4f08b58ec7

LU-3336 lfsck: orphan OST-objects iteration
During the second stage scanning, the LFSCK on the MDT(s) will scan
the orphan OST-objects via OSP level iteration which fetches remote
orphan OST-objects information via OBD_IDX_READ RPC, and shares the
existing framework/functions with others, such as quota.

Implement the sponsor (the master LFSCK engine on the MDT) logic
for the orphan OST-objects iteration.

Implement LFSCK layout rbtree iteration - lfsck_orphan_index_ops,
for slave LFSCK on OST. The lfsck_orphan_index_ops is registered
onto the rbtree object. The incoming OBD_IDX_READ RPC for orphan
OST-object scanning will iterate the rbtree via dt_index_read to
call the registered lfsck_orphan_index_ops.

Others:
1) Speed control during the second-phase scanning.
2) The LFSCK layout trace file (on the MDT) flags should be set
   with LF_INCOMPLETE if LFSCK slave on OST restart or failed.
3) Some code cleanup.

Signed-off-by: Fan Yong <fan.yong@intel.com>
Change-Id: I67d5d870dbf9b80530f4d61ed1a3e5b5df70b1a0
Reviewed-on: http://review.whamcloud.com/8303
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/include/dt_object.h
@@ -212,6 +212,7 @@ enum dt_index_flags {
  */
 extern const struct dt_index_features dt_directory_features;
 extern const struct dt_index_features dt_otable_features;
+extern const struct dt_index_features dt_lfsck_orphan_features;
 extern const struct dt_index_features dt_lfsck_features;
 
 /* index features supported by the accounting objects */

lustre/obdclass/dt_object.c
@@ -567,6 +567,17 @@ EXPORT_SYMBOL(dt_directory_features);
 const struct dt_index_features dt_otable_features;
 EXPORT_SYMBOL(dt_otable_features);
 
+/* lfsck orphan */
+const struct dt_index_features dt_lfsck_orphan_features = {
+	.dif_flags		= 0,
+	.dif_keysize_min	= sizeof(struct lu_fid),
+	.dif_keysize_max	= sizeof(struct lu_fid),
+	.dif_recsize_min	= sizeof(struct lu_orphan_rec),
+	.dif_recsize_max	= sizeof(struct lu_orphan_rec),
+	.dif_ptrsize		= 4
+};
+EXPORT_SYMBOL(dt_lfsck_orphan_features);
+
 /* lfsck */
 const struct dt_index_features dt_lfsck_features = {
 	.dif_flags		= DT_IND_UPDATE,
@@ -630,6 +641,8 @@ static inline const struct dt_index_features *dt_index_feat_select(__u64 seq,
 			/* slave index should be a regular file */
 			return ERR_PTR(-ENOENT);
 		return &dt_quota_slv_features;
+	} else if (seq == FID_SEQ_LAYOUT_RBTREE){
+		return &dt_lfsck_orphan_features;
 	} else if (seq >= FID_SEQ_NORMAL) {
 		/* object is part of the namespace, verify that it is a
 		 * directory */
@@ -870,8 +883,9 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 		 * time being */
 		RETURN(-EOPNOTSUPP);
 
-	if (!fid_is_quota(&ii->ii_fid))
-		/* block access to all local files except quota files */
+	if (!fid_is_quota(&ii->ii_fid) && !fid_is_layout_rbtree(&ii->ii_fid))
+		/* Block access to all local files except quota files and
+		 * layout brtree. */
 		RETURN(-EPERM);
 
 	/* lookup index object subject to the transfer */
@@ -917,13 +931,16 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 		/* key isn't necessarily unique */
 		ii->ii_flags |= II_FL_NONUNQ;
 
-	dt_read_lock(env, obj, 0);
-	/* fetch object version before walking the index */
-	ii->ii_version = dt_version_get(env, obj);
+	if (!fid_is_layout_rbtree(&ii->ii_fid)) {
+		dt_read_lock(env, obj, 0);
+		/* fetch object version before walking the index */
+		ii->ii_version = dt_version_get(env, obj);
+	}
 
 	/* walk the index and fill lu_idxpages with key/record pairs */
 	rc = dt_index_walk(env, obj, rdpg, dt_index_page_build ,ii);
-	dt_read_unlock(env, obj);
+	if (!fid_is_layout_rbtree(&ii->ii_fid))
+		dt_read_unlock(env, obj);
 
 	if (rc == 0) {
 		/* index is empty */

lustre/osp/osp_object.c
@@ -1075,6 +1075,418 @@ int osp_object_destroy(const struct lu_env *env, struct dt_object *dt,
 	RETURN(rc);
 }
 
+struct osp_orphan_it {
+	int			  ooi_pos0;
+	int			  ooi_pos1;
+	int			  ooi_pos2;
+	int			  ooi_total_npages;
+	int			  ooi_valid_npages;
+	unsigned int		  ooi_swab:1;
+	__u64			  ooi_next;
+	struct dt_object	 *ooi_obj;
+	struct lu_orphan_ent	 *ooi_ent;
+	struct page		 *ooi_cur_page;
+	struct lu_idxpage	 *ooi_cur_idxpage;
+	struct page		**ooi_pages;
+};
+
+static int osp_orphan_index_lookup(const struct lu_env *env,
+				   struct dt_object *dt,
+				   struct dt_rec *rec,
+				   const struct dt_key *key,
+				   struct lustre_capa *capa)
+{
+	return -EOPNOTSUPP;
+}
+
+static int osp_orphan_index_declare_insert(const struct lu_env *env,
+					   struct dt_object *dt,
+					   const struct dt_rec *rec,
+					   const struct dt_key *key,
+					   struct thandle *handle)
+{
+	return -EOPNOTSUPP;
+}
+
+static int osp_orphan_index_insert(const struct lu_env *env,
+				   struct dt_object *dt,
+				   const struct dt_rec *rec,
+				   const struct dt_key *key,
+				   struct thandle *handle,
+				   struct lustre_capa *capa,
+				   int ignore_quota)
+{
+	return -EOPNOTSUPP;
+}
+
+static int osp_orphan_index_declare_delete(const struct lu_env *env,
+					   struct dt_object *dt,
+					   const struct dt_key *key,
+					   struct thandle *handle)
+{
+	return -EOPNOTSUPP;
+}
+
+static int osp_orphan_index_delete(const struct lu_env *env,
+				   struct dt_object *dt,
+				   const struct dt_key *key,
+				   struct thandle *handle,
+				   struct lustre_capa *capa)
+{
+	return -EOPNOTSUPP;
+}
+
+static struct dt_it *osp_orphan_it_init(const struct lu_env *env,
+					struct dt_object *dt,
+					__u32 attr,
+					struct lustre_capa *capa)
+{
+	struct osp_orphan_it *it;
+
+	OBD_ALLOC_PTR(it);
+	if (it == NULL)
+		return ERR_PTR(-ENOMEM);
+
+	it->ooi_pos2 = -1;
+	it->ooi_obj = dt;
+
+	return (struct dt_it *)it;
+}
+
+static void osp_orphan_it_fini(const struct lu_env *env,
+			       struct dt_it *di)
+{
+	struct osp_orphan_it	 *it		= (struct osp_orphan_it *)di;
+	struct page		**pages 	= it->ooi_pages;
+	int			  npages	= it->ooi_total_npages;
+	int			  i;
+
+	if (pages != NULL) {
+		for (i = 0; i < npages; i++) {
+			if (pages[i] != NULL) {
+				if (pages[i] == it->ooi_cur_page) {
+					kunmap(pages[i]);
+					it->ooi_cur_page = NULL;
+				}
+				__free_page(pages[i]);
+			}
+		}
+		OBD_FREE(pages, npages * sizeof(*pages));
+	}
+	OBD_FREE_PTR(it);
+}
+
+static int osp_orphan_it_fetch(const struct lu_env *env,
+			       struct osp_orphan_it *it)
+{
+	struct lu_device	 *dev	= it->ooi_obj->do_lu.lo_dev;
+	struct osp_device	 *osp	= lu2osp_dev(dev);
+	struct page		**pages;
+	struct ptlrpc_request	 *req	= NULL;
+	struct ptlrpc_bulk_desc  *desc;
+	struct idx_info 	 *ii;
+	int			  npages;
+	int			  rc;
+	int			  i;
+	ENTRY;
+
+	/* 1MB bulk */
+	npages = min_t(unsigned int, OFD_MAX_BRW_SIZE, 1 << 20);
+	npages /= PAGE_CACHE_SIZE;
+
+	OBD_ALLOC(pages, npages * sizeof(*pages));
+	if (pages == NULL)
+		RETURN(-ENOMEM);
+
+	it->ooi_pages = pages;
+	it->ooi_total_npages = npages;
+	for (i = 0; i < npages; i++) {
+		pages[i] = alloc_page(GFP_IOFS);
+		if (pages[i] == NULL)
+			RETURN(-ENOMEM);
+	}
+
+	req = ptlrpc_request_alloc(osp->opd_obd->u.cli.cl_import,
+				   &RQF_OBD_IDX_READ);
+	if (req == NULL)
+		RETURN(-ENOMEM);
+
+	rc = ptlrpc_request_pack(req, LUSTRE_OBD_VERSION, OBD_IDX_READ);
+	if (rc != 0) {
+		ptlrpc_request_free(req);
+		RETURN(rc);
+	}
+
+	req->rq_request_portal = OST_IDX_PORTAL;
+	ptlrpc_at_set_req_timeout(req);
+
+	desc = ptlrpc_prep_bulk_imp(req, npages, 1, BULK_PUT_SINK,
+				    MDS_BULK_PORTAL);
+	if (desc == NULL) {
+		ptlrpc_request_free(req);
+		RETURN(-ENOMEM);
+	}
+
+	for (i = 0; i < npages; i++)
+		ptlrpc_prep_bulk_page_pin(desc, pages[i], 0, PAGE_CACHE_SIZE);
+
+	ii = req_capsule_client_get(&req->rq_pill, &RMF_IDX_INFO);
+	memset(ii, 0, sizeof(*ii));
+	ii->ii_fid.f_seq = FID_SEQ_LAYOUT_RBTREE;
+	ii->ii_fid.f_oid = osp->opd_index;
+	ii->ii_fid.f_ver = 0;
+	ii->ii_magic = IDX_INFO_MAGIC;
+	ii->ii_flags = II_FL_NOHASH;
+	ii->ii_count = npages * LU_PAGE_COUNT;
+	ii->ii_hash_start = it->ooi_next;
+	ii->ii_attrs =
+		osp->opd_storage->dd_lu_dev.ld_site->ld_seq_site->ss_node_id;
+
+	ptlrpc_request_set_replen(req);
+	rc = ptlrpc_queue_wait(req);
+	if (rc != 0)
+		GOTO(out, rc);
+
+	rc = sptlrpc_cli_unwrap_bulk_read(req, req->rq_bulk,
+					  req->rq_bulk->bd_nob_transferred);
+	if (rc < 0)
+		GOTO(out, rc);
+
+	ii = req_capsule_server_get(&req->rq_pill, &RMF_IDX_INFO);
+	if (ii->ii_magic != IDX_INFO_MAGIC)
+		 GOTO(out, rc = -EPROTO);
+
+	npages = (ii->ii_count + LU_PAGE_COUNT - 1) >>
+		 (PAGE_CACHE_SHIFT - LU_PAGE_SHIFT);
+	if (npages > it->ooi_total_npages) {
+		CERROR("%s: returned more pages than expected, %u > %u\n",
+		       osp->opd_obd->obd_name, npages, it->ooi_total_npages);
+		GOTO(out, rc = -EINVAL);
+	}
+
+	it->ooi_valid_npages = npages;
+	if (ptlrpc_rep_need_swab(req))
+		it->ooi_swab = 1;
+
+	it->ooi_next = ii->ii_hash_end;
+
+	GOTO(out, rc = 0);
+
+out:
+	ptlrpc_req_finished(req);
+
+	return rc;
+}
+
+static int osp_orphan_it_next(const struct lu_env *env,
+			      struct dt_it *di)
+{
+	struct osp_orphan_it	 *it		= (struct osp_orphan_it *)di;
+	struct lu_idxpage	 *idxpage;
+	struct page		**pages;
+	int			  rc;
+	int			  i;
+	ENTRY;
+
+again2:
+	idxpage = it->ooi_cur_idxpage;
+	if (idxpage != NULL) {
+		if (idxpage->lip_nr == 0)
+			RETURN(1);
+
+		it->ooi_pos2++;
+		if (it->ooi_pos2 < idxpage->lip_nr) {
+			it->ooi_ent =
+				(struct lu_orphan_ent *)idxpage->lip_entries +
+				it->ooi_pos2;
+			if (it->ooi_swab)
+				lustre_swab_orphan_ent(it->ooi_ent);
+			RETURN(0);
+		}
+
+		it->ooi_cur_idxpage = NULL;
+		it->ooi_pos1++;
+
+again1:
+		if (it->ooi_pos1 < LU_PAGE_COUNT) {
+			it->ooi_cur_idxpage = (void *)it->ooi_cur_page +
+					      LU_PAGE_SIZE * it->ooi_pos1;
+			if (it->ooi_swab)
+				lustre_swab_lip_header(it->ooi_cur_idxpage);
+			if (it->ooi_cur_idxpage->lip_magic != LIP_MAGIC) {
+				struct osp_device *osp =
+					lu2osp_dev(it->ooi_obj->do_lu.lo_dev);
+
+				CERROR("%s: invalid magic (%x != %x) for page "
+				       "%d/%d while read layout orphan index\n",
+				       osp->opd_obd->obd_name,
+				       it->ooi_cur_idxpage->lip_magic,
+				       LIP_MAGIC, it->ooi_pos0, it->ooi_pos1);
+				/* Skip this lu_page next time. */
+				it->ooi_pos2 = idxpage->lip_nr - 1;
+				RETURN(-EINVAL);
+			}
+			it->ooi_pos2 = -1;
+			goto again2;
+		}
+
+		kunmap(it->ooi_cur_page);
+		it->ooi_cur_page = NULL;
+		it->ooi_pos0++;
+
+again0:
+		pages = it->ooi_pages;
+		if (it->ooi_pos0 < it->ooi_valid_npages) {
+			it->ooi_cur_page = kmap(pages[it->ooi_pos0]);
+			it->ooi_pos1 = 0;
+			goto again1;
+		}
+
+		for (i = 0; i < it->ooi_total_npages; i++) {
+			if (pages[i] != NULL)
+				__free_page(pages[i]);
+		}
+		OBD_FREE(pages, it->ooi_total_npages * sizeof(*pages));
+
+		it->ooi_pos0 = 0;
+		it->ooi_total_npages = 0;
+		it->ooi_valid_npages = 0;
+		it->ooi_swab = 0;
+		it->ooi_ent = NULL;
+		it->ooi_cur_page = NULL;
+		it->ooi_cur_idxpage = NULL;
+		it->ooi_pages = NULL;
+	}
+
+	if (it->ooi_next == II_END_OFF)
+		RETURN(1);
+
+	rc = osp_orphan_it_fetch(env, it);
+	if (rc == 0)
+		goto again0;
+
+	RETURN(rc);
+}
+
+static int osp_orphan_it_get(const struct lu_env *env,
+			     struct dt_it *di,
+			     const struct dt_key *key)
+{
+	return -ENOSYS;
+}
+
+static void osp_orphan_it_put(const struct lu_env *env,
+			      struct dt_it *di)
+{
+}
+
+static struct dt_key *osp_orphan_it_key(const struct lu_env *env,
+					const struct dt_it *di)
+{
+	struct osp_orphan_it	*it  = (struct osp_orphan_it *)di;
+	struct lu_orphan_ent	*ent = it->ooi_ent;
+
+	if (likely(ent != NULL))
+		return (struct dt_key *)(&ent->loe_key);
+
+	return NULL;
+}
+
+static int osp_orphan_it_key_size(const struct lu_env *env,
+				  const struct dt_it *di)
+{
+	return sizeof(struct lu_fid);
+}
+
+static int osp_orphan_it_rec(const struct lu_env *env,
+			     const struct dt_it *di,
+			     struct dt_rec *rec,
+			     __u32 attr)
+{
+	struct osp_orphan_it	*it  = (struct osp_orphan_it *)di;
+	struct lu_orphan_ent	*ent = it->ooi_ent;
+
+	if (likely(ent != NULL)) {
+		*(struct lu_orphan_rec *)rec = ent->loe_rec;
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static __u64 osp_orphan_it_store(const struct lu_env *env,
+				 const struct dt_it *di)
+{
+	struct osp_orphan_it	*it	= (struct osp_orphan_it *)di;
+
+	return it->ooi_next;
+}
+
+/**
+ * \retval	 +1: locate to the exactly position
+ * \retval	  0: cannot locate to the exactly position,
+ *		     call next() to move to a valid position.
+ * \retval	-ve: on error
+ */
+static int osp_orphan_it_load(const struct lu_env *env,
+			      const struct dt_it *di,
+			      __u64 hash)
+{
+	struct osp_orphan_it	*it	= (struct osp_orphan_it *)di;
+	int			 rc;
+
+	it->ooi_next = hash;
+	rc = osp_orphan_it_next(env, (struct dt_it *)di);
+	if (rc == 1)
+		return 0;
+
+	if (rc == 0)
+		return 1;
+
+	return rc;
+}
+
+static int osp_orphan_it_key_rec(const struct lu_env *env,
+				const struct dt_it *di,
+				void *key_rec)
+{
+	return 0;
+}
+
+static const struct dt_index_operations osp_orphan_index_ops = {
+	.dio_lookup		= osp_orphan_index_lookup,
+	.dio_declare_insert	= osp_orphan_index_declare_insert,
+	.dio_insert		= osp_orphan_index_insert,
+	.dio_declare_delete	= osp_orphan_index_declare_delete,
+	.dio_delete		= osp_orphan_index_delete,
+	.dio_it = {
+		.init		= osp_orphan_it_init,
+		.fini		= osp_orphan_it_fini,
+		.next		= osp_orphan_it_next,
+		.get		= osp_orphan_it_get,
+		.put		= osp_orphan_it_put,
+		.key		= osp_orphan_it_key,
+		.key_size	= osp_orphan_it_key_size,
+		.rec		= osp_orphan_it_rec,
+		.store		= osp_orphan_it_store,
+		.load		= osp_orphan_it_load,
+		.key_rec	= osp_orphan_it_key_rec,
+	}
+};
+
+static int osp_index_try(const struct lu_env *env,
+			 struct dt_object *dt,
+			 const struct dt_index_features *feat)
+{
+	if (fid_is_last_id(lu_object_fid(&dt->do_lu))) {
+		dt->do_index_ops = &osp_orphan_index_ops;
+
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
 struct dt_object_operations osp_obj_ops = {
 	.do_declare_attr_get	= osp_declare_attr_get,
 	.do_attr_get		= osp_attr_get,
@@ -1088,6 +1500,7 @@ struct dt_object_operations osp_obj_ops = {
 	.do_create		= osp_object_create,
 	.do_declare_destroy	= osp_declare_object_destroy,
 	.do_destroy		= osp_object_destroy,
+	.do_index_try		= osp_index_try,
 };
 
 static int osp_object_init(const struct lu_env *env, struct lu_object *o,

lustre/fld/fld_handler.c
@@ -250,10 +250,8 @@ int fld_server_lookup(const struct lu_env *env, struct lu_server_fld *fld,
 	rc = fld_name_to_index(fld->lsf_name, &index);
 	if (rc < 0)
 		RETURN(rc);
-	else
-		rc = 0;
 
-	if (index == 0) {
+	if (index == 0 && rc == LDD_F_SV_TYPE_MDT) {
 		/* On server side, all entries should be in cache.
 		 * If we can not find it in cache, just return error */
 		CERROR("%s: Cannot find sequence "LPX64": rc = %d\n",

lustre/include/lustre/lustre_idl.h
@@ -141,6 +141,7 @@
 #define SEQ_DATA_PORTAL                31
 #define SEQ_CONTROLLER_PORTAL          32
 #define MGS_BULK_PORTAL                33
+#define OST_IDX_PORTAL		       34
 
 /* Portal 63 is reserved for the Cray Inc DVS - nic@cray.com, roe@cray.com, n8851@cray.com */
 
@@ -606,6 +607,11 @@ static inline int fid_is_norm(const struct lu_fid *fid)
         return fid_seq_is_norm(fid_seq(fid));
 }
 
+static inline int fid_is_layout_rbtree(const struct lu_fid *fid)
+{
+	return fid_seq(fid) == FID_SEQ_LAYOUT_RBTREE;
+}
+
 /* convert an OST objid into an IDIF FID SEQ number */
 static inline obd_seq fid_idif_seq(obd_id id, __u32 ost_idx)
 {
@@ -952,6 +958,20 @@ static inline void ostid_le_to_cpu(const struct ost_id *src_oi,
 		fid_le_to_cpu(&dst_oi->oi_fid, &src_oi->oi_fid);
 	}
 }
+
+struct lu_orphan_rec {
+	/* The MDT-object's FID referenced by the orphan OST-object */
+	struct lu_fid	lor_fid;
+	__u32		lor_uid;
+	__u32		lor_gid;
+};
+
+struct lu_orphan_ent {
+	/* The orphan OST-object's FID */
+	struct lu_fid		loe_key;
+	struct lu_orphan_rec	loe_rec;
+};
+void lustre_swab_orphan_ent(struct lu_orphan_ent *ent);
 
 /** @} lu_fid */
 

lustre/include/obd_target.h
@@ -78,6 +78,7 @@ struct ost_obd {
 	struct ptlrpc_service	*ost_io_service;
 	struct ptlrpc_service	*ost_seq_service;
 	struct ptlrpc_service	*ost_out_service;
+	struct ptlrpc_service	*ost_idx_service;
 	struct mutex		 ost_health_mutex;
 };
 

lustre/lfsck/lfsck_internal.h
@@ -388,6 +388,7 @@ struct lfsck_component {
 	void			*lc_file_ram;
 	void			*lc_file_disk;
 	void			*lc_data;
+	struct lu_fid		 lc_fid_latest_scanned_phase2;
 
 	/* The time for last checkpoint, jiffies */
 	cfs_time_t		 lc_time_last_checkpoint;
@@ -557,9 +558,14 @@ struct lfsck_thread_info {
 		struct filter_fid	lti_new_pfid;
 	};
 	struct dt_allocation_hint lti_hint;
+	struct lu_orphan_rec	lti_rec;
 };
 
 /* lfsck_lib.c */
+struct lfsck_instance *lfsck_instance_find(struct dt_device *key, bool ref,
+					   bool unlink);
+struct lfsck_component *lfsck_component_find(struct lfsck_instance *lfsck,
+					     __u16 type);
 const char *lfsck_status2names(enum lfsck_status status);
 void lfsck_component_cleanup(const struct lu_env *env,
 			     struct lfsck_component *com);

commit 7576d294582b818b20559138500cf1e58607cfc8

LU-15978 osp: fix striped directory deletion fails for 64K PAGE_SIZE
This fixes the rmdir errors below:
rmdir: failed to remove '/mnt/lustre/d1.sanity/d2': Invalid argument
LustreError: 381691:0:(osp_object.c:1998:osp_it_next_page())
lustre-MDT0000-osp-MDT0001: invalid magic (0 != 8a6d6b6c) for page 0/1
while read layout orphan index.

For 64K PAGE_SIZE, when created an striped directory, e.g. created
with function test_mkdir() defined in test-framework.sh when MDSCOUNT
>= 2, deleting it will fail.

For PAGE_SIZE > LU_PAGE_SIZE, if the end system page fills less than
LU_PAGE_COUNT lu_idxpages, init the header of the remain lu_idxpages.
So that the clients handle this partial filling correctly.

Also make goto labels meaningful and avoid not freeing pages for
lip_nr == 0 in osp_it_next_page().

This patch also fixes wrong page idx for page kunmap in
dt_index_walk().

This server end fix also necessary for the idxpage reading clients
nodemap_process_idx_pages() and qsd_reint_entries(). So this patch also
includes fix for LU-15992: nodemap create and check failed on 64K page
size.

Fixes: 77eea1985bb1 ("LU-3336 lfsck: orphan OST-objects iteration")
Change-Id: I75bd9603c31bed8ea15fdba693677d41affaf61c
Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>
Co-authored-by: Kevin Zhao <kevin.zhao@linaro.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47812
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>

lustre/include/dt_object.h
@@ -2256,6 +2256,8 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 		  void *arg);
 int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 		  struct idx_info *ii, const struct lu_rdpg *rdpg);
+void dt_index_page_adjust(struct page **pages, const u32 npages,
+			  const size_t nlupgs);
 
 static inline struct thandle *dt_trans_create(const struct lu_env *env,
                                               struct dt_device *d)

lustre/obdclass/dt_object.c
@@ -1031,7 +1031,7 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 				/* end of index */
 				break;
 		}
-		kunmap(rdpg->rp_pages[i]);
+		kunmap(rdpg->rp_pages[pageidx]);
 	}
 
 out:
@@ -1129,12 +1129,61 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 		ii->ii_hash_end = II_END_OFF;
 	}
 
+	/*
+	 * For partial lu_idxpage filling of the end system page,
+	 * init the header of the remain lu_idxpages.
+	 */
+	if (rc > 0)
+		dt_index_page_adjust(rdpg->rp_pages, rdpg->rp_npages,
+				     ii->ii_count);
+
 	GOTO(out, rc);
 out:
 	dt_object_put(env, obj);
 	return rc;
 }
 EXPORT_SYMBOL(dt_index_read);
+
+#if PAGE_SIZE > LU_PAGE_SIZE
+/*
+ * For partial lu_idxpage filling of the end system page, init the header of the
+ * remain lu_idxpages. So that the clients handle partial filling correctly.
+ * Current lu_idxpage read clients are osp_it_next_page(),
+ * nodemap_process_idx_pages() and qsd_reint_entries().
+ */
+void dt_index_page_adjust(struct page **pages, const u32 npages,
+			  const size_t nlupgs)
+{
+	u32			nlupgs_mod = nlupgs % LU_PAGE_COUNT;
+	u32			remain_nlupgs;
+	u32			pgidx;
+	struct lu_idxpage      *lip;
+	union lu_page	       *lp;
+	int			i;
+
+	if (nlupgs_mod) {
+		pgidx = nlupgs / LU_PAGE_COUNT;
+		LASSERT(pgidx < npages);
+		lp = kmap(pages[pgidx]);
+		remain_nlupgs = LU_PAGE_COUNT - nlupgs_mod;
+
+		/* initialize the header for the remain lu_pages */
+		for (i = 0, lp += nlupgs_mod; i < remain_nlupgs; i++, lp++) {
+			lip = &lp->lp_idx;
+			memset(lip, 0, LIP_HDR_SIZE);
+			lip->lip_magic = LIP_MAGIC;
+		}
+
+		kunmap(pages[pgidx]);
+	}
+}
+#else
+void dt_index_page_adjust(struct page **pages, const u32 npages,
+			  const size_t nlupgs)
+{
+}
+#endif
+EXPORT_SYMBOL(dt_index_page_adjust);
 
 #ifdef CONFIG_PROC_FS
 int lprocfs_dt_blksize_seq_show(struct seq_file *m, void *v)

lustre/osp/osp_object.c
@@ -1978,21 +1978,22 @@ int osp_it_next_page(const struct lu_env *env, struct dt_it *di)
 	int			i;
 	ENTRY;
 
-again2:
+process_idxpage:
 	idxpage = it->ooi_cur_idxpage;
 	if (idxpage != NULL) {
 		if (idxpage->lip_nr == 0)
-			RETURN(1);
+			goto finish_cur_idxpage;
 
 		if (it->ooi_pos_ent < idxpage->lip_nr) {
 			CDEBUG(D_INFO, "ooi_pos %d nr %d\n",
 			       (int)it->ooi_pos_ent, (int)idxpage->lip_nr);
 			RETURN(0);
 		}
+finish_cur_idxpage:
 		it->ooi_cur_idxpage = NULL;
 		it->ooi_pos_lu_page++;
 
-again1:
+process_page:
 		if (it->ooi_pos_lu_page < LU_PAGE_COUNT) {
 			it->ooi_cur_idxpage = (void *)it->ooi_cur_page +
 					 LU_PAGE_SIZE * it->ooi_pos_lu_page;
@@ -2013,19 +2014,19 @@ again1:
 				RETURN(-EINVAL);
 			}
 			it->ooi_pos_ent = -1;
-			goto again2;
+			goto process_idxpage;
 		}
 
 		kunmap(it->ooi_cur_page);
 		it->ooi_cur_page = NULL;
 		it->ooi_pos_page++;
 
-again0:
+start:
 		pages = it->ooi_pages;
 		if (it->ooi_pos_page < it->ooi_valid_npages) {
 			it->ooi_cur_page = kmap(pages[it->ooi_pos_page]);
 			it->ooi_pos_lu_page = 0;
-			goto again1;
+			goto process_page;
 		}
 
 		for (i = 0; i < it->ooi_total_npages; i++) {
@@ -2049,7 +2050,7 @@ again0:
 
 	rc = osp_it_fetch(env, it);
 	if (rc == 0)
-		goto again0;
+		goto start;
 
 	RETURN(rc);
 }

lustre/ptlrpc/nodemap_storage.c
@@ -1608,6 +1608,14 @@ int nodemap_index_read(struct lu_env *env,
 	if (rc >= 0)
 		ii->ii_version = version;
 
+	/*
+	 * For partial lu_idxpage filling of the end system page,
+	 * init the header of the remain lu_idxpages.
+	 */
+	if (rc > 0)
+		dt_index_page_adjust(rdpg->rp_pages, rdpg->rp_npages,
+				     ii->ii_count);
+
 	dt_read_unlock(env, nodemap_idx);
 	return rc;
 }
@@ -1670,6 +1678,7 @@ int nodemap_get_config_req(struct obd_device *mgs_obd,
 	nodemap_ii.ii_flags = II_FL_NOHASH;
 	nodemap_ii.ii_version = rqexp_ted->ted_nodemap_version;
 	nodemap_ii.ii_attrs = body->mcb_nm_cur_pass;
+	nodemap_ii.ii_count = 0;
 
 	bytes = nodemap_index_read(req->rq_svc_thread->t_env,
 				   obd2obt(mgs_obd)->obt_nodemap_config_file,