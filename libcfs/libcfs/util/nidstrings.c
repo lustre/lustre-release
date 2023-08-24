lustre/lustre-release

commit b6c702df5d4de8e5a806169404ba1c069ea8fff2

LU-10391 libcfs: add large-nid string conversion functions.
The user-space libcfs now has functions to convert between strings and
large nids.

Signed-off-by: Mr NeilBrown <neilb@suse.de>
Change-Id: I554e2da0c3d56397ebe60fc84fad28fec6704a18
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50092
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/util/nidstrings.c
@@ -41,6 +41,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 #include <libcfs/util/string.h>
 #include <linux/lnet/lnet-types.h>
@@ -97,6 +98,22 @@ libcfs_ip_addr2str(__u32 addr, char *str, size_t size)
 		 (addr >> 8) & 0xff, addr & 0xff);
 }
 
+static void
+libcfs_ip_addr2str_size(const __be32 *addr, size_t asize,
+			char *str, size_t size)
+{
+	switch (asize) {
+	case 4:
+		inet_ntop(AF_INET, addr, str, size);
+		break;
+	case 16:
+		inet_ntop(AF_INET6, addr, str, size);
+		break;
+	default:
+		return;
+	}
+}
+
 /* CAVEAT EMPTOR XscanfX
  * I use "%n" at the end of a sscanf format to detect trailing junk.  However
  * sscanf may return immediately if it sees the terminating '0' in a string, so
@@ -148,6 +165,68 @@ libcfs_ip_str2addr(const char *str, int nob, __u32 *addr)
 	}
 #endif
 	return 0;
+}
+
+static int
+libcfs_ip_str2addr_size(const char *str, int nob,
+			__be32 *addr, size_t *alen)
+{
+	char *tmp = malloc(nob+1);
+	int ret = 1;
+
+	if (!tmp)
+		return 0;
+	memcpy(tmp, str, nob);
+	tmp[nob] = 0;
+
+	if (inet_pton(AF_INET, tmp, (struct in_addr *)addr) == 0) {
+		*alen = 4;
+		goto out;
+	}
+	if (inet_pton(AF_INET6, tmp, (struct in6_addr *)addr) == 0) {
+		*alen = 16;
+		goto out;
+	}
+#ifdef HAVE_GETADDRINFO
+	/* known hostname? */
+	if (('a' <= str[0] && str[0] <= 'z') ||
+	    ('A' <= str[0] && str[0] <= 'Z')) {
+		struct addrinfo *ai = NULL;
+
+		if (getaddrinfo(tmp, NULL, NULL, &ai) == 0) {
+			struct addrinfo *a;
+			/* First look for an AF_INET address */
+			for (a = ai; a; a = a->ai_next) {
+				if (a->ai_family == AF_INET && a->ai_addr) {
+					struct sockaddr_in *sin =
+						(void *)ai->ai_addr;
+
+					memcpy(addr, &sin->sin_addr, 4);
+					*alen = 4;
+					freeaddrinfo(ai);
+					goto out;
+				}
+			}
+			/* Now consider AF_INET6 */
+			for (a = ai; a; a = a->ai_next) {
+				if (a->ai_family == AF_INET6 && a->ai_addr) {
+					struct sockaddr_in6 *sin6 =
+						(void *)ai->ai_addr;
+
+					memcpy(addr, &sin6->sin6_addr, 16);
+					*alen = 16;
+					freeaddrinfo(ai);
+					goto out;
+				}
+			}
+		}
+		freeaddrinfo(ai);
+	}
+#endif
+	ret = 0;
+out:
+	free(tmp);
+	return ret;
 }
 
 int
@@ -508,7 +587,9 @@ static struct netstrfns libcfs_netstrfns[] = {
 		.nf_name		= "tcp",
 		.nf_modname		= "ksocklnd",
 		.nf_addr2str		= libcfs_ip_addr2str,
+		.nf_addr2str_size	= libcfs_ip_addr2str_size,
 		.nf_str2addr		= libcfs_ip_str2addr,
+		.nf_str2addr_size	= libcfs_ip_str2addr_size,
 		.nf_parse_addrlist	= cfs_ip_addr_parse,
 		.nf_print_addrlist	= libcfs_ip_addr_range_print,
 		.nf_match_addr		= cfs_ip_addr_match,
@@ -709,6 +790,54 @@ libcfs_nid2str_r(lnet_nid_t nid, char *buf, size_t buf_size)
 	return buf;
 }
 
+char *
+libcfs_nidstr_r(const struct lnet_nid *nid, char *buf, size_t buf_size)
+{
+	__u32 nnum;
+	__u32 lnd;
+	struct netstrfns *nf;
+
+	if (LNET_NID_IS_ANY(nid)) {
+		strncpy(buf, "<?>", buf_size);
+		buf[buf_size - 1] = '\0';
+		return buf;
+	}
+
+	nnum = __be16_to_cpu(nid->nid_num);
+	lnd = nid->nid_type;
+	nf = libcfs_lnd2netstrfns(lnd);
+	if (nf) {
+		size_t addr_len;
+		/* Avoid take address in packed array */
+		__u32 addr[4] = { nid->nid_addr[0], nid->nid_addr[1],
+				 nid->nid_addr[2], nid->nid_addr[3]};
+
+		if (nf->nf_addr2str_size)
+			nf->nf_addr2str_size(addr, NID_ADDR_BYTES(nid),
+					     buf, buf_size);
+		else
+			nf->nf_addr2str(ntohl(nid->nid_addr[0]), buf, buf_size);
+		addr_len = strlen(buf);
+		if (nnum == 0)
+			snprintf(buf + addr_len, buf_size - addr_len, "@%s",
+				 nf->nf_name);
+		else
+			snprintf(buf + addr_len, buf_size - addr_len, "@%s%u",
+				 nf->nf_name, nnum);
+	} else {
+		int l = 0;
+		int words = (NID_ADDR_BYTES(nid) + 3) / 4;
+		int i;
+
+		for (i = 0; i < words && i < 4; i++)
+			l = snprintf(buf+l, buf_size-l, "%s%x",
+				     i ? ":" : "", ntohl(nid->nid_addr[i]));
+		snprintf(buf+l, buf_size-l, "@<%u:%u>", lnd, nnum);
+	}
+
+	return buf;
+}
+
 static struct netstrfns *
 libcfs_str2net_internal(const char *str, __u32 *net)
 {
@@ -779,6 +908,52 @@ libcfs_str2nid(const char *str)
 		return LNET_NID_ANY;
 
 	return LNET_MKNID(net, addr);
+}
+
+int
+libcfs_strnid(struct lnet_nid *nid, const char *str)
+{
+	const char *sep = strchr(str, '@');
+	struct netstrfns *nf;
+	__u32 net;
+
+	if (sep != NULL) {
+		nf = libcfs_str2net_internal(sep + 1, &net);
+		if (nf == NULL)
+			return -EINVAL;
+	} else {
+		sep = str + strlen(str);
+		net = LNET_MKNET(SOCKLND, 0);
+		nf = libcfs_lnd2netstrfns(SOCKLND);
+		assert(nf != NULL);
+	}
+
+	memset(nid, 0, sizeof(*nid));
+	nid->nid_type = LNET_NETTYP(net);
+	nid->nid_num = htons(LNET_NETNUM(net));
+	if (nf->nf_str2addr_size) {
+		size_t asize = 0;
+		__u32 addr[4];
+
+		if (!nf->nf_str2addr_size(str, (int)(sep - str),
+					  addr, &asize))
+			return -EINVAL;
+
+		/* Avoid take address in packed array */
+		nid->nid_addr[0] = addr[0];
+		nid->nid_addr[1] = addr[1];
+		nid->nid_addr[2] = addr[2];
+		nid->nid_addr[3] = addr[3];
+		nid->nid_size = asize - 4;
+	} else {
+		__u32 addr;
+
+		if (!nf->nf_str2addr(str, (int)(sep - str), &addr))
+			return -EINVAL;
+		nid->nid_addr[0] = htonl(addr);
+		nid->nid_size = 0;
+	}
+	return 0;
 }
 
 char *

libcfs/autoconf/lustre-libcfs.m4
@@ -2616,6 +2616,7 @@ AC_MSG_NOTICE([LibCFS core checks
 # libcfs/libcfs/util/nidstrings.c
 AC_CHECK_HEADERS([netdb.h asm/types.h endian.h])
 AC_CHECK_FUNCS([gethostbyname])
+AC_CHECK_FUNCS([getaddrinfo])
 
 # --------  Check for required packages  --------------
 

libcfs/include/libcfs/util/string.h
@@ -80,7 +80,11 @@ struct netstrfns {
 	char	*nf_name;
 	char	*nf_modname;
 	void	(*nf_addr2str)(__u32 addr, char *str, size_t size);
+	void	(*nf_addr2str_size)(const __be32 *addr, size_t asize, char *str,
+				    size_t size);
 	int	(*nf_str2addr)(const char *str, int nob, __u32 *addr);
+	int	(*nf_str2addr_size)(const char *str, int nob,
+				    __be32 *addr, size_t *asize);
 	int	(*nf_parse_addrlist)(char *str, int len,
 				     struct list_head *list);
 	int	(*nf_print_addrlist)(char *buffer, int count,

lnet/include/lnet/lib-types.h
@@ -53,17 +53,6 @@
 #include <uapi/linux/lnet/lnetctl.h>
 #include <uapi/linux/lnet/nidstr.h>
 
-char *libcfs_nidstr_r(const struct lnet_nid *nid,
-		      char *buf, size_t buf_size);
-
-static inline char *libcfs_nidstr(const struct lnet_nid *nid)
-{
-	return libcfs_nidstr_r(nid, libcfs_next_nidstring(),
-			       LNET_NIDSTR_SIZE);
-}
-
-int libcfs_strnid(struct lnet_nid *nid, const char *str);
-char *libcfs_idstr(struct lnet_processid *id);
 int libcfs_strid(struct lnet_processid *id, const char *str);
 
 int cfs_match_nid_net(struct lnet_nid *nid, u32 net,

lnet/include/uapi/linux/lnet/nidstr.h
@@ -86,6 +86,17 @@ static inline char *libcfs_nid2str(lnet_nid_t nid)
 				LNET_NIDSTR_SIZE);
 }
 
+char *libcfs_nidstr_r(const struct lnet_nid *nid,
+		      char *buf, __kernel_size_t buf_size);
+
+static inline char *libcfs_nidstr(const struct lnet_nid *nid)
+{
+	return libcfs_nidstr_r(nid, libcfs_next_nidstring(),
+			       LNET_NIDSTR_SIZE);
+}
+
+int libcfs_strnid(struct lnet_nid *nid, const char *str);
+char *libcfs_idstr(struct lnet_processid *id);
 __u32 libcfs_str2net(const char *str);
 lnet_nid_t libcfs_str2nid(const char *str);
 int libcfs_str2anynid(lnet_nid_t *nid, const char *str);

commit 101f6e84889a9b48238ca320557101058d935fb0

LU-10391 obdclass: handle large NIDs for mount strings
Mount strings support using ':' as a delimiter but this is also
a part of the some NID strings like IPv6, so rework class_parse_value()
to only look at ':' when it occurs after '@'.

The mount utilities use the function convert_hostnames() to ensure
the mount string containing an NID is valid. This only works for
small size nids so migrate the function to handle large NIDs. This
should allow mounting with IPv6 or other large NID addresses.

In testing the userland  libcfs_ip_str2addr_size() had bugs that
rendered incorrect NID strings. Fix those issues.

Fixes: b6c702df5d4 ("LU-10391 libcfs: add large-nid string conversion functions.")
Change-Id: Ic9b2a368456ba75ceb5911ac7f75ae00d6123870
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50362
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Nathaniel Clark <nclark@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/util/nidstrings.c
@@ -172,19 +172,33 @@ libcfs_ip_str2addr_size(const char *str, int nob,
 			__be32 *addr, size_t *alen)
 {
 	char *tmp = malloc(nob+1);
-	int ret = 1;
 
 	if (!tmp)
 		return 0;
 	memcpy(tmp, str, nob);
-	tmp[nob] = 0;
+	tmp[nob] = '\0';
 
-	if (inet_pton(AF_INET, tmp, (struct in_addr *)addr) == 0) {
-		*alen = 4;
+	*alen = 0;
+	if (inet_pton(AF_INET, tmp, (struct in_addr *)addr) == 1) {
+		struct in_addr *ipv4 = (struct in_addr *)addr;
+
+		/* Don't allow using loopback */
+		if (ipv4->s_addr != htonl(INADDR_LOOPBACK))
+			*alen = 4;
 		goto out;
 	}
-	if (inet_pton(AF_INET6, tmp, (struct in6_addr *)addr) == 0) {
-		*alen = 16;
+	if (inet_pton(AF_INET6, tmp, (struct in6_addr *)addr) == 1) {
+		struct in6_addr *ipv6 = (struct in6_addr *)addr;
+
+		/* Since link local doesn't allow forwarding packets
+		 * for router don't allow those addresses as well.
+		 * Site local is allowed since it similar to 10.0.0.0/8.
+		 * Be aware site local is deprecated by unique local
+		 * addresses.
+		 */
+		if (!IN6_IS_ADDR_LOOPBACK(ipv6->s6_addr) &&
+		    !IN6_IS_ADDR_LINKLOCAL(ipv6->s6_addr))
+			*alen = 16;
 		goto out;
 	}
 #ifdef HAVE_GETADDRINFO
@@ -223,10 +237,9 @@ libcfs_ip_str2addr_size(const char *str, int nob,
 		freeaddrinfo(ai);
 	}
 #endif
-	ret = 0;
 out:
 	free(tmp);
-	return ret;
+	return *alen != 0;
 }
 
 int

lustre/obdclass/obd_config.c
@@ -449,13 +449,20 @@ static int class_parse_value(char *buf, int opc, void *value, char **endh,
 
 	if (!buf)
 		return 1;
+
 	while (*buf == ',' || *buf == ':')
 		buf++;
 	if (*buf == ' ' || *buf == '/' || *buf == '\0')
 		return 1;
 
-	/* NID separators or end of NIDs */
-	endp = strpbrk(buf, ",: /");
+	/* NID separators or end of NIDs. Large NIDs can contain ':' so
+	 * skip ahead to @ and then look for one of the delimiters.
+	 */
+	endp = strchr(buf, '@');
+	if (!endp)
+		return 1;
+
+	endp = strpbrk(endp, ",: /");
 	if (!endp)
 		endp = buf + strlen(buf);
 

lustre/utils/mkfs_lustre.c
@@ -63,6 +63,7 @@
 #include <linux/lnet/lnetctl.h>
 #include <linux/lustre/lustre_user.h>
 #include <linux/lustre/lustre_ver.h>
+#include <libcfs/util/string.h>
 
 #include "mount_utils.h"
 
@@ -322,52 +323,6 @@ static int erase_param(const char *const buf, const char *const param,
 }
 #endif
 
-/* from mount_lustre */
-/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
-#define MAXNIDSTR 1024
-static char *convert_hostnames(char *s1)
-{
-	char *converted, *s2 = 0, *c, *end, sep;
-	int left = MAXNIDSTR;
-	lnet_nid_t nid;
-
-	converted = malloc(left);
-	if (!converted)
-		return NULL;
-
-	end = s1 + strlen(s1);
-	c = converted;
-	while ((left > 0) && (s1 < end)) {
-		s2 = strpbrk(s1, ",:");
-		if (!s2)
-			s2 = end;
-		sep = *s2;
-		*s2 = '\0';
-		nid = libcfs_str2nid(s1);
-		*s2 = sep;
-
-		if (nid == LNET_NID_ANY) {
-			fprintf(stderr, "%s: Cannot resolve hostname '%s'.\n",
-				progname, s1);
-			free(converted);
-			return NULL;
-		}
-		if (strncmp(libcfs_nid2str(nid), "127.0.0.1",
-			    strlen("127.0.0.1")) == 0) {
-			fprintf(stderr,
-				"%s: The NID '%s' resolves to the loopback address '%s'.  Lustre requires a non-loopback address.\n",
-				progname, s1, libcfs_nid2str(nid));
-			free(converted);
-			return NULL;
-		}
-
-		c += snprintf(c, left, "%s%c", libcfs_nid2str(nid), sep);
-		left = converted + MAXNIDSTR - c;
-		s1 = s2 + 1;
-	}
-	return converted;
-}
-
 int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 	       char **mountopts, char *old_fsname)
 {
@@ -470,7 +425,7 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 				return 1;
 			}
 
-			nids = convert_hostnames(optarg);
+			nids = convert_hostnames(optarg, false);
 			if (!nids)
 				return 1;
 
@@ -554,7 +509,7 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 			break;
 		}
 		case 'm': {
-			char *nids = convert_hostnames(optarg);
+			char *nids = convert_hostnames(optarg, false);
 
 			if (!nids)
 				return 1;

lustre/utils/mount_lustre.c
@@ -124,44 +124,6 @@ void usage(FILE *out)
 	exit((out != stdout) ? EINVAL : 0);
 }
 
-/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
-#define MAXNIDSTR 1024
-static char *convert_hostnames(char *s1)
-{
-	char *converted, *s2 = 0, *c;
-	char sep;
-	int left = MAXNIDSTR;
-	lnet_nid_t nid;
-
-	converted = malloc(left);
-	if (!converted) {
-		fprintf(stderr, "out of memory: needed %d bytes\n",
-			MAXNIDSTR);
-		return NULL;
-	}
-	c = converted;
-	while ((left > 0) && (*s1 != '/')) {
-		s2 = strpbrk(s1, ",:");
-		if (!s2)
-			goto out_free;
-		sep = *s2;
-		*s2 = '\0';
-		nid = libcfs_str2nid(s1);
-		*s2 = sep;                      /* back to original string */
-		if (nid == LNET_NID_ANY)
-			goto out_free;
-		c += scnprintf(c, left, "%s%c", libcfs_nid2str(nid), sep);
-		left = converted + MAXNIDSTR - c;
-		s1 = s2 + 1;
-	}
-	snprintf(c, left, "%s", s1);
-	return converted;
-out_free:
-	fprintf(stderr, "%s: Can't parse NID '%s'\n", progname, s1);
-	free(converted);
-	return NULL;
-}
-
 /*****************************************************************************
  *
  * This part was cribbed from util-linux/mount/mount.c.  There was no clear
@@ -786,7 +748,7 @@ static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
 
 	ptr = strstr(mop->mo_usource, ":/");
 	if (ptr) {
-		mop->mo_source = convert_hostnames(mop->mo_usource);
+		mop->mo_source = convert_hostnames(mop->mo_usource, true);
 		if (!mop->mo_source)
 			usage(stderr);
 	} else {

lustre/utils/mount_utils.c
@@ -41,6 +41,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <config.h>
+#include <linux/lnet/nidstr.h>
 #include <linux/lustre/lustre_ver.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
@@ -54,6 +55,7 @@
 #include <sys/types.h>
 #include <sys/xattr.h>
 #include <libmount/libmount.h>
+#include <libcfs/util/string.h>
 
 #ifdef HAVE_GSS
 #ifdef HAVE_LIBKEYUTILS
@@ -941,6 +943,93 @@ int file_create(char *path, __u64 size)
 	}
 
 	return 0;
+}
+
+/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
+#define MAXNIDSTR 1024
+
+char *convert_hostnames(char *s1, bool mount)
+{
+	char *converted, *s2 = 0, *c, *end, sep;
+	int left = MAXNIDSTR;
+	struct lnet_nid nid;
+
+	converted = malloc(left);
+	if (!converted) {
+		fprintf(stderr, "%s: cannot allocate %u bytes for NID: %s\n",
+			progname, left, strerror(ENOMEM));
+		return NULL;
+	}
+
+	/* end is different between mount and mkfs case */
+	if (mount) {
+		end = strchr(s1, '/');
+		if (!end) {
+			fprintf(stderr, "%s: Invalid mount string: %s\n",
+				progname, s1);
+			return NULL;
+		}
+		end--;
+	} else {
+		end = s1 + strlen(s1);
+	}
+
+	c = converted;
+	while ((left > 0) && (s1 < end)) {
+		int rc;
+
+		/* Needed to skip : in IPv6 / GUID strings. Lustre uses
+		 * ':' as a seperator as well which makes this complicated.
+		 */
+		s2 = strchr(s1, '@');
+		if (!s2 || s2 > end) {
+			fprintf(stderr, "%s: Invalid NID string '%s'\n",
+				progname, s1);
+			goto out_free;
+		}
+
+		s2 = strpbrk(s2, ",:");
+		if (!s2) {
+			s2 = end;
+		} else if (s2 > end) {
+			fprintf(stderr, "%s: Invalid NID string '%s'\n",
+				progname, s1);
+			goto out_free;
+		}
+		sep = *s2;
+		*s2 = '\0';
+
+		rc = libcfs_strnid(&nid, s1);
+		if (rc < 0) {
+			fprintf(stderr, "%s: Unsupported NID '%s': rc = %s.\n",
+				progname, s1, strerror(rc));
+			goto out_free;
+		}
+		*s2 = sep;	/* back to original string */
+
+		if (LNET_NID_IS_ANY(&nid)) {
+			fprintf(stderr, "%s: Cannot resolve hostname '%s'.\n",
+				progname, s1);
+			goto out_free;
+		}
+
+		if (sep != '\0')
+			c += scnprintf(c, left, "%s%c", libcfs_nidstr(&nid),
+				       sep);
+		else
+			c += scnprintf(c, left, "%s", libcfs_nidstr(&nid));
+		left = converted + MAXNIDSTR - c;
+		s1 = s2 + 1;
+	}
+
+	if (mount)
+		snprintf(c, left, "%s", s1);
+
+	return converted;
+out_free:
+	fprintf(stderr, "%s: Can't parse NID '%s'\n", progname, s1);
+	free(converted);
+	return NULL;
 }
 
 #ifdef HAVE_SERVER_SUPPORT

lustre/utils/mount_utils.h
@@ -178,6 +178,7 @@ int update_mtab_entry(char *spec, char *mtpt, char *type, char *opts,
 int update_utab_entry(struct mount_opts *mop);
 int check_mountfsoptions(char *mountopts, char *wanted_mountopts);
 void trim_mountfsoptions(char *s);
+char *convert_hostnames(char *s1, bool mount);
 #ifdef HAVE_SERVER_SUPPORT
 __u64 get_device_size(char* device);
 int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,