lustre/lustre-release

commit e26d7cc3992252e5fce5a51aee716f933b04c13a

LU-14399 hsm: process hsm_actions in coordinator
Wait for mdd setup in a separate thread to don't block mount.
The patch adds conf-sanity_131 to verify the fix.

Change-Id: Ifd0e8969d7ed4f8944ab61ab0e0ebe2655bad003
Fixes: a558006b ("LU-13920 hsm: process hsm_actions only after mdd setup")
HPE-bug-id: LUS-9750
Signed-off-by: Sergey Cheremencev <sergey.cheremencev@hpe.com>
Reviewed-on: https://review.whamcloud.com/41445
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_coordinator.c
@@ -549,7 +549,31 @@ static int set_cdt_state(struct coordinator *cdt, enum cdt_states new_state)
 	return rc;
 }
 
+static int mdt_hsm_pending_restore(struct mdt_thread_info *mti);
 
+static void cdt_start_pending_restore(struct mdt_device *mdt,
+				      struct coordinator *cdt)
+{
+	struct mdt_thread_info *cdt_mti;
+	unsigned int i = 0;
+	int rc;
+
+	/* wait until MDD initialize hsm actions llog */
+	while (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state) && i < obd_timeout) {
+		schedule_timeout_interruptible(cfs_time_seconds(1));
+		i++;
+	}
+	if (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state))
+		CWARN("%s: trying to init HSM before MDD\n", mdt_obd_name(mdt));
+
+	/* set up list of started restore requests */
+	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
+	rc = mdt_hsm_pending_restore(cdt_mti);
+	if (rc)
+		CERROR("%s: cannot take the layout locks needed for registered restore: %d\n",
+		       mdt_obd_name(mdt), rc);
+
+}
 
 /**
  * coordinator thread
@@ -579,6 +603,7 @@ static int mdt_coordinator(void *data)
 
 	/* Inform mdt_hsm_cdt_start(). */
 	wake_up(&cdt->cdt_waitq);
+	cdt_start_pending_restore(mdt, cdt);
 
 	while (1) {
 		int i;
@@ -1103,7 +1128,6 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 {
 	struct coordinator *cdt = &mdt->mdt_coordinator;
 	struct mdt_thread_info *cdt_mti;
-	unsigned int i = 0;
 	int rc;
 	void *ptr;
 	struct task_struct *task;
@@ -1135,28 +1159,13 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 	cdt->cdt_group_request_mask = (1UL << HSMA_RESTORE);
 	cdt->cdt_other_request_mask = (1UL << HSMA_RESTORE);
 
-	/* wait until MDD initialize hsm actions llog */
-	while (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state) && i < obd_timeout) {
-		schedule_timeout_interruptible(cfs_time_seconds(1));
-		i++;
-	}
-	if (!test_bit(MDT_FL_CFGLOG, &mdt->mdt_state))
-		CWARN("%s: trying to init HSM before MDD\n", mdt_obd_name(mdt));
-
 	/* to avoid deadlock when start is made through sysfs
 	 * sysfs entries are created by the coordinator thread
 	 */
-	/* set up list of started restore requests */
-	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
-	rc = mdt_hsm_pending_restore(cdt_mti);
-	if (rc)
-		CERROR("%s: cannot take the layout locks needed"
-		       " for registered restore: %d\n",
-		       mdt_obd_name(mdt), rc);
-
 	if (mdt->mdt_bottom->dd_rdonly)
 		RETURN(0);
 
+	cdt_mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
 	task = kthread_run(mdt_coordinator, cdt_mti, "hsm_cdtr");
 	if (IS_ERR(task)) {
 		rc = PTR_ERR(task);

lustre/tests/conf-sanity.sh
@@ -9397,6 +9397,28 @@ test_131() {
 }
 run_test 131 "MDT backup restore with project ID"
 
+test_132() {
+	local err_cnt
+	local err_cnt2
+
+	reformat
+	combined_mgs_mds || start_mgs || error "unable to start MGS"
+	start_mdt 1 || error "unable to start mdt1"
+
+	err_cnt=$(do_facet mds1 dmesg | grep -c "cannot take the layout locks")
+	stop_mdt 1 || error "stop mdt1 failed"
+
+	[ "$mds1_FSTYPE" == zfs ] && import_zpool mds1
+	do_facet mds1 $TUNEFS --param mdt.hsm_control=enabled $(mdsdevname 1) ||
+		error "tunefs failed"
+	start_mdt 1 || error "cannot start mdt1"
+
+	err_cnt2=$(do_facet mds1 dmesg | grep -c "cannot take the layout locks")
+	[ $err_cnt -eq $err_cnt2 ] || error "Can not take the layout lock"
+	stop_mdt 1 || error "stop mdt1 failed"
+}
+run_test 132 "hsm_actions processed after failover"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi

commit fe5706e0c19f96e4f821790004f05ab265002e9d

LU-16235 hsm: check CDT state before adding actions llog
Don't allow HSM requests to be added to the actions llog when
cdt_state is in CDT_STOPPED/CDT_STOPPING as the CDT is unavailable, or
in CDT_INIT as any HSM requests in the llog may not have been fully
processed and so cdt_last_cookie may not have been set appropriately,
otherwise a colliding cookie value can be reused in
mdt_agent_record_add() and the assertions in
cdt_agent_record_hash_add() can be triggered:

"ASSERTION( carl0->carl_cat_idx == carl1->carl_cat_idx ) failed"
"ASSERTION( carl0->carl_rec_idx == carl1->carl_rec_idx ) failed"

Requests needed to implement the Remove Archive on Last Unlink (RAoLU)
policy are allowed when the CDT is shutdown, as those are safe
operations. They are also allowed during CDT initialization, even
though this can lead to the assertions being triggered, as doing so
maintains administrator expectations regarding file archives always
being removed when the RAoLU policy is enabled. This could possibly be
improved by e.g. failing when mdt_handle_last_unlink() is not able to
add an HSM remove request, or saving the requests in an llog so they
can be sent if the CDT is available later.

For the same reason, the llog needs to be processed before setting
cdt_state to CDT_RUNNING in the coordinator thread.

Change-Id: I4b5f5ee22f74827b31d8ed5917a8fc16e35d1f16
Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
HPE-bug-id: LUS-8231, LUS-11064
Fixes: e26d7cc3 ("LU-14399 hsm: process hsm_actions in coordinator")
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48842
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -2523,6 +2523,11 @@ static inline char *hai_dump_data_field(const struct hsm_action_item *hai,
 	return buffer;
 }
 
+enum hal_flags {
+	/* Register even when the CDT is shutdown or being initialized */
+	HAL_CDT_FORCE = 1 << 1,
+};
+
 /* Copytool action list */
 #define HAL_VERSION 1
 #define HAL_MAXSIZE LNET_MTU /* bytes, used in userspace only */

lustre/mdt/mdt_coordinator.c
@@ -603,11 +603,8 @@ static int mdt_coordinator(void *data)
 	obd_uuid2fsname(hsd.hsd_fsname, mdt_obd_name(mdt),
 			sizeof(hsd.hsd_fsname));
 
-	set_cdt_state(cdt, CDT_RUNNING);
-
-	/* Inform mdt_hsm_cdt_start(). */
-	wake_up(&cdt->cdt_waitq);
 	cdt_start_pending_restore(mdt, cdt);
+	set_cdt_state(cdt, CDT_RUNNING);
 
 	while (1) {
 		int i;
@@ -1214,10 +1211,6 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 		       mdt_obd_name(mdt), rc);
 	} else {
 		cdt->cdt_task = task;
-		wait_event(cdt->cdt_waitq,
-			   cdt->cdt_state != CDT_INIT);
-		CDEBUG(D_HSM, "%s: coordinator thread started\n",
-		       mdt_obd_name(mdt));
 		rc = 0;
 	}
 

lustre/mdt/mdt_hsm.c
@@ -572,7 +572,8 @@ int mdt_hsm_request(struct tgt_session_info *tsi)
 
 	hal->hal_version = HAL_VERSION;
 	hal->hal_archive_id = hr->hr_archive_id;
-	hal->hal_flags = hr->hr_flags;
+	hal->hal_flags = hr->hr_flags & ~HAL_CDT_FORCE;
+
 	obd_uuid2fsname(hal->hal_fsname, mdt_obd_name(info->mti_mdt),
 			MTI_NAME_MAXLEN);
 

lustre/mdt/mdt_hsm_cdt_actions.c
@@ -284,7 +284,19 @@ int mdt_agent_record_add(const struct lu_env *env, struct mdt_device *mdt,
 	if (lctxt == NULL || lctxt->loc_handle == NULL)
 		GOTO(free, rc = -ENOENT);
 
+	/* Preserve lock order wrt hsm_cancel_all_actions() */
+	mutex_lock(&cdt->cdt_state_lock);
 	down_write(&cdt->cdt_llog_lock);
+
+	/* Need cdt_last_cookie to be set during CDT startup; allow RAoLU
+	 * requests, even though this can trigger the assertions in
+	 * cdt_agent_record_hash_add(). This could be improved e.g. by failing
+	 * the unlink during CDT_INIT, or adding RAoLU requests in an llog and
+	 * issuing them if the CDT is available later
+	 */
+	if ((cdt->cdt_state == CDT_STOPPED || cdt->cdt_state == CDT_STOPPING ||
+	    cdt->cdt_state == CDT_INIT) && !(flags & HAL_CDT_FORCE))
+		GOTO(unavail, rc = -EAGAIN);
 
 	/* in case of cancel request, the cookie is already set to the
 	 * value of the request cookie to be cancelled
@@ -297,8 +309,9 @@ int mdt_agent_record_add(const struct lu_env *env, struct mdt_device *mdt,
 	rc = llog_cat_add(env, lctxt->loc_handle, &larr->arr_hdr, NULL);
 	if (rc > 0)
 		rc = 0;
-
+unavail:
 	up_write(&cdt->cdt_llog_lock);
+	mutex_unlock(&cdt->cdt_state_lock);
 	llog_ctxt_put(lctxt);
 
 	EXIT;

lustre/mdt/mdt_lib.c
@@ -984,8 +984,8 @@ int mdt_handle_last_unlink(struct mdt_thread_info *info, struct mdt_object *mo,
 
 	hai.hai_fid = *mdt_object_fid(mo);
 
-	rc = mdt_agent_record_add(info->mti_env, info->mti_mdt, archive_id, 0,
-				  &hai);
+	rc = mdt_agent_record_add(info->mti_env, info->mti_mdt, archive_id,
+				  HAL_CDT_FORCE, &hai);
 	if (rc)
 		CERROR("%s: unable to add HSM remove request for "DFID
 		       ": rc=%d\n", mdt_obd_name(info->mti_mdt),