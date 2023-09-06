lustre/lustre-release

commit abf04e7ea356e8b1523e4183d5c46c0462f1f605

LU-14337 lov: return valid stripe_count/size for PFL files
Dump struct lov_comp_md_v1 in function ll_lov_getstripe_ea_info()
correctly to avoid stripe_count=0 or stripe_size=0 returned by
old interface llapi_file_get_stripe(), which will cause
divide-by-zero for older userspace that calls this ioctl,
e.g. lustre ADIO driver.
The rule is:
- if stripe_count=0, return stripe_count=1;
- if stripe_size=0,
  -- for DoM files, return the stripe size of the second component,
     since the first component of DoM file data is placed on the
     MDT for faster access;
  -- else, return the stripe size of the last component.

Also, lov_getstripe_old.c and santy-pfl.sh test_25 is added to
verify this patch.

Test-parameters: testlist=sanity-pfl env=ONLY=25

Signed-off-by: Emoly Liu <emoly@whamcloud.com>
Change-Id: I4023ca4baff1b1ad2a439aa497baaabc56e891d2
Reviewed-on: https://review.whamcloud.com/41803
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -2149,44 +2149,45 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
                              struct lov_mds_md **lmmp, int *lmm_size,
                              struct ptlrpc_request **request)
 {
-        struct ll_sb_info *sbi = ll_i2sbi(inode);
-        struct mdt_body  *body;
-        struct lov_mds_md *lmm = NULL;
-        struct ptlrpc_request *req = NULL;
-        struct md_op_data *op_data;
-        int rc, lmmsize;
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	struct mdt_body *body;
+	struct lov_mds_md *lmm = NULL;
+	struct ptlrpc_request *req = NULL;
+	struct md_op_data *op_data;
+	int rc, lmmsize;
+
+	ENTRY;
 
 	rc = ll_get_default_mdsize(sbi, &lmmsize);
 	if (rc)
 		RETURN(rc);
 
-        op_data = ll_prep_md_op_data(NULL, inode, NULL, filename,
-                                     strlen(filename), lmmsize,
-                                     LUSTRE_OPC_ANY, NULL);
-        if (IS_ERR(op_data))
-                RETURN(PTR_ERR(op_data));
+	op_data = ll_prep_md_op_data(NULL, inode, NULL, filename,
+				     strlen(filename), lmmsize,
+				     LUSTRE_OPC_ANY, NULL);
+	if (IS_ERR(op_data))
+		RETURN(PTR_ERR(op_data));
 
-        op_data->op_valid = OBD_MD_FLEASIZE | OBD_MD_FLDIREA;
-        rc = md_getattr_name(sbi->ll_md_exp, op_data, &req);
-        ll_finish_md_op_data(op_data);
-        if (rc < 0) {
-                CDEBUG(D_INFO, "md_getattr_name failed "
-                       "on %s: rc %d\n", filename, rc);
-                GOTO(out, rc);
-        }
+	op_data->op_valid = OBD_MD_FLEASIZE | OBD_MD_FLDIREA;
+	rc = md_getattr_name(sbi->ll_md_exp, op_data, &req);
+	ll_finish_md_op_data(op_data);
+	if (rc < 0) {
+		CDEBUG(D_INFO, "md_getattr_name failed "
+		       "on %s: rc %d\n", filename, rc);
+		GOTO(out, rc);
+	}
 
-        body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
-        LASSERT(body != NULL); /* checked by mdc_getattr_name */
+	body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
+	LASSERT(body != NULL); /* checked by mdc_getattr_name */
 
 	lmmsize = body->mbo_eadatasize;
 
 	if (!(body->mbo_valid & (OBD_MD_FLEASIZE | OBD_MD_FLDIREA)) ||
-                        lmmsize == 0) {
-                GOTO(out, rc = -ENODATA);
-        }
+	    lmmsize == 0)
+		GOTO(out, rc = -ENODATA);
 
-        lmm = req_capsule_server_sized_get(&req->rq_pill, &RMF_MDT_MD, lmmsize);
-        LASSERT(lmm != NULL);
+	lmm = req_capsule_server_sized_get(&req->rq_pill, &RMF_MDT_MD, lmmsize);
+	LASSERT(lmm != NULL);
 
 	if (lmm->lmm_magic != cpu_to_le32(LOV_MAGIC_V1) &&
 	    lmm->lmm_magic != cpu_to_le32(LOV_MAGIC_V3) &&
@@ -2196,11 +2197,10 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 
 	/*
 	 * This is coming from the MDS, so is probably in
-	 * little endian.  We convert it to host endian before
+	 * little endian. We convert it to host endian before
 	 * passing it to userspace.
 	 */
-	if ((lmm->lmm_magic & __swab32(LOV_MAGIC_MAGIC)) ==
-	    __swab32(LOV_MAGIC_MAGIC)) {
+	if (cpu_to_le32(LOV_MAGIC) != LOV_MAGIC) {
 		int stripe_count = 0;
 
 		if (lmm->lmm_magic == cpu_to_le32(LOV_MAGIC_V1) ||
@@ -2209,28 +2209,74 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 			if (le32_to_cpu(lmm->lmm_pattern) &
 			    LOV_PATTERN_F_RELEASED)
 				stripe_count = 0;
-		}
+			lustre_swab_lov_user_md((struct lov_user_md *)lmm, 0);
 
-		lustre_swab_lov_user_md((struct lov_user_md *)lmm, 0);
-
-		/* if function called for directory - we should
-		 * avoid swab not existent lsm objects */
-		if (lmm->lmm_magic == LOV_MAGIC_V1 && S_ISREG(body->mbo_mode))
-			lustre_swab_lov_user_md_objects(
+			/* if function called for directory - we should
+			 * avoid swab not existent lsm objects
+			 */
+			if (lmm->lmm_magic == LOV_MAGIC_V1 &&
+			    S_ISREG(body->mbo_mode))
+				lustre_swab_lov_user_md_objects(
 				((struct lov_user_md_v1 *)lmm)->lmm_objects,
 				stripe_count);
-		else if (lmm->lmm_magic == LOV_MAGIC_V3 &&
-			 S_ISREG(body->mbo_mode))
-			lustre_swab_lov_user_md_objects(
+			else if (lmm->lmm_magic == LOV_MAGIC_V3 &&
+				 S_ISREG(body->mbo_mode))
+				lustre_swab_lov_user_md_objects(
 				((struct lov_user_md_v3 *)lmm)->lmm_objects,
 				stripe_count);
+		} else if (lmm->lmm_magic == cpu_to_le32(LOV_MAGIC_COMP_V1)) {
+			lustre_swab_lov_comp_md_v1(
+				(struct lov_comp_md_v1 *)lmm);
+		}
 	}
 
+	if (lmm->lmm_magic == LOV_MAGIC_COMP_V1) {
+		struct lov_comp_md_v1 *comp_v1 = NULL;
+		struct lov_comp_md_entry_v1 *ent;
+		struct lov_user_md_v1 *v1;
+		__u32 off;
+		int i = 0;
+
+		comp_v1 = (struct lov_comp_md_v1 *)lmm;
+		/* Dump the striping information */
+		for (; i < comp_v1->lcm_entry_count; i++) {
+			ent = &comp_v1->lcm_entries[i];
+			off = ent->lcme_offset;
+			v1 = (struct lov_user_md_v1 *)((char *)lmm + off);
+			CDEBUG(D_INFO,
+			       "comp[%d]: stripe_count=%u, stripe_size=%u\n",
+			       i, v1->lmm_stripe_count, v1->lmm_stripe_size);
+		}
+
+		/**
+		 * Return valid stripe_count and stripe_size instead of 0 for
+		 * DoM files to avoid divide-by-zero for older userspace that
+		 * calls this ioctl, e.g. lustre ADIO driver.
+		 */
+		if (lmm->lmm_stripe_count == 0)
+			lmm->lmm_stripe_count = 1;
+		if (lmm->lmm_stripe_size == 0) {
+			/* Since the first component of the file data is placed
+			 * on the MDT for faster access, the stripe_size of the
+			 * second one is always that applications which are
+			 * doing large IOs.
+			 */
+			if (lmm->lmm_pattern == LOV_PATTERN_MDT)
+				i = comp_v1->lcm_entry_count > 1 ? 1 : 0;
+			else
+				i = comp_v1->lcm_entry_count > 1 ?
+				    comp_v1->lcm_entry_count - 1 : 0;
+			ent = &comp_v1->lcm_entries[i];
+			off = ent->lcme_offset;
+			v1 = (struct lov_user_md_v1 *)((char *)lmm + off);
+			lmm->lmm_stripe_size = v1->lmm_stripe_size;
+		}
+	}
 out:
 	*lmmp = lmm;
 	*lmm_size = lmmsize;
 	*request = req;
-	return rc;
+	RETURN(rc);
 }
 
 static int ll_lov_setea(struct inode *inode, struct file *file,

lustre/lov/lov_pack.c
@@ -468,13 +468,6 @@ int lov_getstripe(const struct lu_env *env, struct lov_object *obj,
 	}
 
 	/**
-	 * Return stripe_count=1 instead of 0 for DoM files to avoid
-	 * divide-by-zero for older userspace that calls this ioctl,
-	 * e.g. lustre ADIO driver.
-	 */
-	if ((lum.lmm_stripe_count == 0) && (lum.lmm_pattern & LOV_PATTERN_MDT))
-		lum.lmm_stripe_count = 1;
-	/**
 	 * User specified limited buffer size, usually the buffer is
 	 * from ll_lov_setstripe(), and the buffer can only hold basic
 	 * layout template info.

lustre/tests/Makefile.am
@@ -78,7 +78,7 @@ THETESTS += swap_lock_test lockahead_test mirror_io mmap_mknod_test
 THETESTS += create_foreign_file parse_foreign_file
 THETESTS += create_foreign_dir parse_foreign_dir
 THETESTS += check_fallocate splice-test lseek_test expand_truncate_test
-THETESTS += foreign_symlink_striping
+THETESTS += foreign_symlink_striping lov_getstripe_old
 
 if LIBAIO
 THETESTS += aiocp
@@ -122,6 +122,7 @@ rwv_LDADD = $(LIBLUSTREAPI)
 lockahead_test_LDADD = $(LIBLUSTREAPI)
 mirror_io_LDADD = $(LIBLUSTREAPI)
 ll_dirstripe_verify_LDADD = $(LIBLUSTREAPI)
+lov_getstripe_old_LDADD = $(LIBLUSTREAPI)
 flocks_test_LDADD = $(LIBLUSTREAPI) $(PTHREAD_LIBS)
 create_foreign_dir_LDADD = $(LIBLUSTREAPI)
 check_fallocate_LDADD = $(LIBLUSTREAPI)

lustre/tests/lov_getstripe_old.c
@@ -1,0 +1,95 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * Copyright (c) 2021, DDN Storage Corporation.
+ */
+/*
+ * lustre/tests/lov_getstripe_old.c
+ *
+ * ll_getstripe_old <file>:
+ * - to verify if the striping information of composite layout files returned
+ *   by llapi_file_get_stripe() is valid.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/vfs.h>
+#include <lustre/lustreapi.h>
+#define LOV_MAGIC_MAGIC 0x0BD0
+#define LOV_MAGIC_MASK  0xFFFF
+#define LOV_MAGIC_V1            (0x0BD10000 | LOV_MAGIC_MAGIC)
+#define LOV_MAGIC_JOIN_V1       (0x0BD20000 | LOV_MAGIC_MAGIC)
+#define LOV_MAGIC_V3            (0x0BD30000 | LOV_MAGIC_MAGIC)
+static inline int maxint(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+static void *alloc_lum()
+{
+	int v1, v3;
+
+	v1 = sizeof(struct lov_user_md_v1) +
+	     LOV_MAX_STRIPE_COUNT * sizeof(struct lov_user_ost_data_v1);
+	v3 = sizeof(struct lov_user_md_v3) +
+	     LOV_MAX_STRIPE_COUNT * sizeof(struct lov_user_ost_data_v1);
+
+	return malloc(maxint(v1, v3));
+}
+
+int main(int argc, char **argv)
+{
+	struct lov_user_md *lum_file = NULL;
+	int rc;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return 1;
+	}
+	lum_file = alloc_lum();
+	if (lum_file == NULL) {
+		rc = ENOMEM;
+		goto cleanup;
+	}
+
+	rc = llapi_file_get_stripe(argv[1], lum_file);
+	if (rc) {
+		rc = errno;
+		goto cleanup;
+	}
+	/* stripe_size stripe_count */
+	if (lum_file->lmm_magic == LOV_MAGIC_V1)
+		printf("lmm_magic: v1\n");
+	else if (lum_file->lmm_magic == LOV_MAGIC_V3)
+		printf("lmm_magic: v3\n");
+	else if (lum_file->lmm_magic == (0x0BD60000 | LOV_MAGIC_MAGIC))
+		printf("lmm_magic: LOV_MAGIC component\n");
+
+	printf("stripe_count: %d\nstripe_size: %d\n",
+		lum_file->lmm_stripe_count, lum_file->lmm_stripe_size);
+
+cleanup:
+	if (lum_file != NULL)
+		free(lum_file);
+	return rc;
+}

lustre/tests/sanity-pfl.sh
@@ -2398,6 +2398,53 @@ test_24a() {
 }
 run_test 24a "FIEMAP upon PFL file"
 
+test_25() {
+	local pfl_f=$DIR/$tdir/"$tfile"_pfl
+	local dom_f=$DIR/$tdir/"$tfile"_dom
+	local common_f=$DIR/$tdir/"$tfile"_common
+	local stripe_count
+	local stripe_size
+
+	mkdir -p $DIR/$tdir || error "mkdir $DIR/$tdir failed"
+	$LFS setstripe -E 10M -S 64k -c -1 -E 20M -S 1M -E -1 -S 2M -c 1 \
+		$pfl_f || error "setstripe $pfl_f failed"
+	$LFS setstripe -E 256k -L mdt -E -1 -S 1M $dom_f ||
+		error "setstripe $dom_f failed"
+	$LFS setstripe -S 512K -c -1 $common_f ||
+		error "setstripe $common_f failed"
+
+	#verify lov_getstripe_old with PFL file
+	stripe_count=$(lov_getstripe_old $pfl_f |
+			awk '/stripe_count/ { print $2 }')
+	stripe_size=$(lov_getstripe_old $pfl_f |
+			awk '/stripe_size/ { print $2 }')
+	[ $stripe_count -eq 1 ] ||
+		error "stripe_count $stripe_count !=1 for $pfl_f"
+	[ $stripe_size -eq 2097152 ] ||
+		error "stripe_size $stripe_size != 2097152 for $pfl_f"
+
+	#verify lov_getstripe_old with DoM file
+	stripe_count=$(lov_getstripe_old $dom_f |
+			awk '/stripe_count/ { print $2 }')
+	stripe_size=$(lov_getstripe_old $dom_f |
+			awk '/stripe_size/ { print $2 }')
+	[ $stripe_count -eq 1 ] ||
+		error "stripe_count $stripe_count !=1 for $dom_f"
+	[ $stripe_size -eq 1048576 ] ||
+		error "stripe_size $stripe_size != 1048576 for $dom_f"
+
+	#verify lov_getstripe_old with common file
+	stripe_count=$(lov_getstripe_old $common_f |
+			awk '/stripe_count/ { print $2 }')
+	stripe_size=$(lov_getstripe_old $common_f |
+			awk '/stripe_size/ { print $2 }')
+	[ $stripe_count -eq $OSTCOUNT ] ||
+		error "stripe_count $stripe_count !=$OSTCOUNT for $common_f"
+	[ $stripe_size -eq 524288 ] ||
+		error "stripe_size $stripe_size != 524288 for $common_f"
+}
+run_test 25 "Verify old lov stripe API with PFL files"
+
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status

commit bb0cc84fbed51e006bfac230dada426bfac4f500

LU-10026 csdc: DoM pattern could be a combined value
DoM pattern is LOV_PATTERN_MDT for now, and in the future it could
be combined with LOV_PATTERN_COMPRESS to represent a compressed
DoM component.

Fix a minor glitch for lov_getstripe_old code path (in
ll_lov_getstripe_ea_info), which intends to return the last component
stripe info but the commit abf04e7ea3 omits to correctly set the
last component stripe info before using it.

Fixes: abf04e7ea3 ("LU-14337 lov: return valid stripe_count/size for PFL files")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Id0779c30c004b6979f88bf96b7b7b74a8b8c26e4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51978
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -731,10 +731,11 @@ struct fsxattr {
 
 static inline bool lov_pattern_supported(__u32 pattern)
 {
-	return (pattern & ~LOV_PATTERN_F_RELEASED) == LOV_PATTERN_RAID0 ||
-	       (pattern & ~LOV_PATTERN_F_RELEASED) ==
-			(LOV_PATTERN_RAID0 | LOV_PATTERN_OVERSTRIPING) ||
-	       (pattern & ~LOV_PATTERN_F_RELEASED) == LOV_PATTERN_MDT;
+	__u32 pattern_base = pattern & ~LOV_PATTERN_F_RELEASED;
+
+	return pattern_base == LOV_PATTERN_RAID0 ||
+	       pattern_base == (LOV_PATTERN_RAID0 | LOV_PATTERN_OVERSTRIPING) ||
+	       pattern_base == LOV_PATTERN_MDT;
 }
 
 /* RELEASED and MDT patterns are not valid in many places, so rather than

lustre/lfsck/lfsck_layout.c
@@ -357,10 +357,10 @@ static int lfsck_layout_verify_header_v1v3(struct dt_object *obj,
 	}
 
 	pattern = le32_to_cpu(lmm->lmm_pattern);
-	*dom = !!(lov_pattern(pattern) == LOV_PATTERN_MDT);
+	*dom = !!(lov_pattern(pattern) & LOV_PATTERN_MDT);
 
 	/* XXX: DoM file verification will be supportted via LU-11081. */
-	if (lov_pattern(pattern) == LOV_PATTERN_MDT) {
+	if (lov_pattern(pattern) & LOV_PATTERN_MDT) {
 #if 0
 		if (start != 0) {
 			CDEBUG(D_LFSCK, "The DoM entry for "DFID" is not "

lustre/llite/file.c
@@ -2587,7 +2587,7 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 	if (lmm->lmm_magic == LOV_MAGIC_COMP_V1) {
 		struct lov_comp_md_v1 *comp_v1 = NULL;
 		struct lov_comp_md_entry_v1 *ent;
-		struct lov_user_md_v1 *v1;
+		struct lov_user_md_v1 *v1 = NULL;
 		__u32 off;
 		int i = 0;
 
@@ -2602,6 +2602,11 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 			       i, v1->lmm_stripe_count, v1->lmm_stripe_size);
 		}
 
+		if (v1 == NULL)
+			GOTO(out, rc = -EINVAL);
+
+		lmm->lmm_stripe_count = v1->lmm_stripe_count;
+		lmm->lmm_stripe_size = v1->lmm_stripe_size;
 		/**
 		 * Return valid stripe_count and stripe_size instead of 0 for
 		 * DoM files to avoid divide-by-zero for older userspace that
@@ -2615,7 +2620,7 @@ int ll_lov_getstripe_ea_info(struct inode *inode, const char *filename,
 			 * second one is always that applications which are
 			 * doing large IOs.
 			 */
-			if (lmm->lmm_pattern == LOV_PATTERN_MDT)
+			if (lmm->lmm_pattern & LOV_PATTERN_MDT)
 				i = comp_v1->lcm_entry_count > 1 ? 1 : 0;
 			else
 				i = comp_v1->lcm_entry_count > 1 ?

lustre/lod/lod_lov.c
@@ -1656,7 +1656,7 @@ static int lod_verify_v1v3(struct lod_device *d, const struct lu_buf *buf,
 
 	stripe_offset = le16_to_cpu(lum->lmm_stripe_offset);
 	if (!is_from_disk && stripe_offset != LOV_OFFSET_DEFAULT &&
-	    lov_pattern(le32_to_cpu(lum->lmm_pattern)) != LOV_PATTERN_MDT) {
+	    !(lov_pattern(le32_to_cpu(lum->lmm_pattern)) & LOV_PATTERN_MDT)) {
 		/* if offset is not within valid range [0, osts_size) */
 		if (stripe_offset >= d->lod_ost_descs.ltd_tgts_size) {
 			CDEBUG(D_LAYOUT, "stripe offset %u >= bitmap size %u\n",
@@ -2012,7 +2012,7 @@ int lod_verify_striping(const struct lu_env *env, struct lod_device *d,
 	case LOV_USER_MAGIC_V1:
 	case LOV_USER_MAGIC_V3:
 	case LOV_USER_MAGIC_SPECIFIC:
-		if (lov_pattern(le32_to_cpu(lum->lmm_pattern)) ==
+		if (lov_pattern(le32_to_cpu(lum->lmm_pattern)) &
 		    LOV_PATTERN_MDT) {
 			/* DoM must use composite layout */
 			CDEBUG(D_LAYOUT, "DoM without composite layout\n");
@@ -2111,7 +2111,7 @@ recheck:
 
 		/* Check DoM entry is always the first one */
 		lum = tmp.lb_buf;
-		if (lov_pattern(le32_to_cpu(lum->lmm_pattern)) ==
+		if (lov_pattern(le32_to_cpu(lum->lmm_pattern)) &
 		    LOV_PATTERN_MDT) {
 			/* DoM component must be the first in a mirror */
 			if (le64_to_cpu(ext->e_start) > 0) {

lustre/lod/lod_object.c
@@ -1159,7 +1159,7 @@ static inline void lod_adjust_stripe_info(struct lod_layout_component *comp,
 					  struct lov_desc *desc,
 					  int append_stripes)
 {
-	if (comp->llc_pattern != LOV_PATTERN_MDT) {
+	if (!(comp->llc_pattern & LOV_PATTERN_MDT)) {
 		if (append_stripes) {
 			comp->llc_stripe_count = append_stripes;
 		} else if (!comp->llc_stripe_count) {
@@ -3411,7 +3411,7 @@ static int lod_declare_layout_merge(const struct lu_env *env,
 	/* check if first entry in new layout is DOM */
 	lmm = (struct lov_mds_md_v1 *)((char *)merge_lcm +
 					merge_lcm->lcm_entries[0].lcme_offset);
-	merge_has_dom = lov_pattern(le32_to_cpu(lmm->lmm_pattern)) ==
+	merge_has_dom = lov_pattern(le32_to_cpu(lmm->lmm_pattern)) &
 			LOV_PATTERN_MDT;
 
 	for (i = 0; i < merge_entry_count; i++) {
@@ -4013,7 +4013,7 @@ static void embed_pool_to_comp_v1(const struct lov_comp_md_v1 *src,
 		lum = (struct lov_user_md_v1 *)((char *)src + offset);
 		lum3 = (struct lov_user_md_v3 *)((char *)tgt + offset + shift);
 		*(struct lov_user_md_v1 *)lum3 = *lum;
-		if (lum->lmm_pattern == cpu_to_le32(LOV_PATTERN_MDT)) {
+		if (lum->lmm_pattern & cpu_to_le32(LOV_PATTERN_MDT)) {
 			lum3->lmm_magic = cpu_to_le32(LOV_USER_MAGIC_V1);
 		} else {
 			lum3->lmm_magic = cpu_to_le32(LOV_USER_MAGIC_V3);
@@ -4122,7 +4122,7 @@ static int lod_xattr_set_default_lov_on_dir(const struct lu_env *env,
 			if (le32_to_cpu(lum->lmm_magic) != LOV_USER_MAGIC_V1)
 				/* the i-th component includes pool info */
 				break;
-			if (lum->lmm_pattern == cpu_to_le32(LOV_PATTERN_MDT))
+			if (lum->lmm_pattern & cpu_to_le32(LOV_PATTERN_MDT))
 				size += sizeof(struct lov_user_md_v1);
 			else
 				size += sizeof(struct lov_user_md_v3);
@@ -6366,7 +6366,7 @@ int lod_striped_create(const struct lu_env *env, struct dt_object *dt,
 		if (lod_comp->llc_pattern & LOV_PATTERN_F_RELEASED)
 			lod_comp_set_init(lod_comp);
 
-		if (lov_pattern(lod_comp->llc_pattern) == LOV_PATTERN_MDT)
+		if (lov_pattern(lod_comp->llc_pattern) & LOV_PATTERN_MDT)
 			lod_comp_set_init(lod_comp);
 
 		if (lod_comp->llc_stripe == NULL)
@@ -6417,7 +6417,7 @@ static inline bool lod_obj_is_dom(struct dt_object *dt)
 	if (!lo->ldo_comp_cnt)
 		return false;
 
-	return (lov_pattern(lo->ldo_comp_entries[0].llc_pattern) ==
+	return (lov_pattern(lo->ldo_comp_entries[0].llc_pattern) &
 		LOV_PATTERN_MDT);
 }
 
@@ -7438,7 +7438,7 @@ out:
 /* If striping is already instantiated or INIT'ed DOM? */
 static bool lod_is_instantiation_needed(struct lod_layout_component *comp)
 {
-	return !(((lov_pattern(comp->llc_pattern) == LOV_PATTERN_MDT) &&
+	return !(((lov_pattern(comp->llc_pattern) & LOV_PATTERN_MDT) &&
 		  lod_comp_inited(comp)) || comp->llc_stripe);
 }
 

lustre/lod/lod_qos.c
@@ -2478,10 +2478,7 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 
 		if (v1->lmm_pattern == 0)
 			v1->lmm_pattern = LOV_PATTERN_RAID0;
-		if (lov_pattern(v1->lmm_pattern) != LOV_PATTERN_RAID0 &&
-		    lov_pattern(v1->lmm_pattern) != LOV_PATTERN_MDT &&
-		    lov_pattern(v1->lmm_pattern) !=
-			(LOV_PATTERN_RAID0 | LOV_PATTERN_OVERSTRIPING)) {
+		if (!lov_pattern_supported(lov_pattern(v1->lmm_pattern))) {
 			CDEBUG(D_LAYOUT, "%s: invalid pattern: %x\n",
 			       lod2obd(d)->obd_name, v1->lmm_pattern);
 			GOTO(free_comp, rc = -EINVAL);
@@ -2493,10 +2490,10 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 
 		lod_comp->llc_stripe_count = desc->ld_default_stripe_count;
 		if (v1->lmm_stripe_count ||
-		    lov_pattern(v1->lmm_pattern) == LOV_PATTERN_MDT)
+		    (lov_pattern(v1->lmm_pattern) & LOV_PATTERN_MDT))
 			lod_comp->llc_stripe_count = v1->lmm_stripe_count;
 
-		if (lov_pattern(lod_comp->llc_pattern) == LOV_PATTERN_MDT &&
+		if ((lov_pattern(lod_comp->llc_pattern) & LOV_PATTERN_MDT) &&
 		    lod_comp->llc_stripe_count != 0) {
 			CDEBUG(D_LAYOUT, "%s: invalid stripe count: %u\n",
 			       lod2obd(d)->obd_name,
@@ -2718,7 +2715,7 @@ int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 		RETURN(0);
 
 	/* A Data-on-MDT component is being created */
-	if (lov_pattern(lod_comp->llc_pattern) == LOV_PATTERN_MDT)
+	if (lov_pattern(lod_comp->llc_pattern) & LOV_PATTERN_MDT)
 		RETURN(0);
 
 	if (lod_comp->llc_pool)

lustre/lov/lov_cl_internal.h
@@ -145,10 +145,10 @@ static inline char *llt2str(enum lov_layout_type llt)
 static inline __u32 lov_entry_type(struct lov_stripe_md_entry *lsme)
 {
 	if ((lov_pattern(lsme->lsme_pattern) & LOV_PATTERN_RAID0) ||
-	    (lov_pattern(lsme->lsme_pattern) == LOV_PATTERN_MDT) ||
+	    (lov_pattern(lsme->lsme_pattern) & LOV_PATTERN_MDT) ||
 	    (lov_pattern(lsme->lsme_pattern) == LOV_PATTERN_FOREIGN))
 		return lov_pattern(lsme->lsme_pattern &
-				   ~LOV_PATTERN_OVERSTRIPING);
+			   ~(LOV_PATTERN_OVERSTRIPING | LOV_PATTERN_COMPRESS));
 	return 0;
 }
 

lustre/lov/lov_ea.c
@@ -457,7 +457,7 @@ lsme_unpack_comp(struct lov_obd *lov, struct lov_mds_md *lmm,
 
 	if (magic != LOV_MAGIC_FOREIGN &&
 	    le16_to_cpu(lmm->lmm_stripe_count) == 0 &&
-	    lov_pattern(le32_to_cpu(lmm->lmm_pattern)) != LOV_PATTERN_MDT)
+	    !(lov_pattern(le32_to_cpu(lmm->lmm_pattern)) & LOV_PATTERN_MDT))
 		RETURN(ERR_PTR(-EINVAL));
 
 	if (magic == LOV_MAGIC_V1) {

lustre/lov/lov_internal.h
@@ -56,7 +56,7 @@ struct lov_stripe_md_entry {
 
 static inline bool lsme_is_dom(struct lov_stripe_md_entry *lsme)
 {
-	return (lov_pattern(lsme->lsme_pattern) == LOV_PATTERN_MDT);
+	return (lov_pattern(lsme->lsme_pattern) & LOV_PATTERN_MDT);
 }
 
 static inline void copy_lsm_entry(struct lov_stripe_md_entry *dst,

lustre/mdd/mdd_object.c
@@ -1226,7 +1226,7 @@ static inline __u64 mdd_lmm_dom_size(void *buf)
 	v1 = (struct lov_mds_md *)((char *)comp_v1 + off);
 
 	/* DoM entry is the first entry always */
-	if (lov_pattern(le32_to_cpu(v1->lmm_pattern)) == LOV_PATTERN_MDT)
+	if (lov_pattern(le32_to_cpu(v1->lmm_pattern)) & LOV_PATTERN_MDT)
 		return le64_to_cpu(comp_v1->lcm_entries[0].lcme_extent.e_end);
 
 	return 0;

lustre/mdt/mdt_handler.c
@@ -620,7 +620,7 @@ __u32 mdt_lmm_dom_entry_check(struct lov_mds_md *lmm, int *is_dom_only)
 
 	/* Fast check for DoM entry with no mirroring, should be the first */
 	if (le16_to_cpu(comp_v1->lcm_mirror_count) == 0 &&
-	    lov_pattern(le32_to_cpu(v1->lmm_pattern)) != LOV_PATTERN_MDT)
+	    !(lov_pattern(le32_to_cpu(v1->lmm_pattern)) & LOV_PATTERN_MDT))
 		RETURN(0);
 
 	/* check all entries otherwise */
@@ -634,7 +634,7 @@ __u32 mdt_lmm_dom_entry_check(struct lov_mds_md *lmm, int *is_dom_only)
 		off = le32_to_cpu(lcme->lcme_offset);
 		v1 = (struct lov_mds_md *)((char *)comp_v1 + off);
 
-		if (lov_pattern(le32_to_cpu(v1->lmm_pattern)) ==
+		if (lov_pattern(le32_to_cpu(v1->lmm_pattern)) &
 		    LOV_PATTERN_MDT)
 			dom_stripesize = le32_to_cpu(v1->lmm_stripe_size);
 		else

lustre/utils/liblustreapi.c
@@ -381,7 +381,7 @@ static int llapi_stripe_param_verify(const struct llapi_stripe_param *param,
 	}
 
 	count = param->lsp_stripe_count;
-	if (param->lsp_stripe_pattern == LOV_PATTERN_MDT) {
+	if (param->lsp_stripe_pattern & LOV_PATTERN_MDT) {
 		rc = -EINVAL;
 		llapi_error(LLAPI_MSG_ERROR, rc,
 			    "Invalid pattern: '-L mdt', must be specified "
@@ -2702,7 +2702,7 @@ static void lov_dump_user_lmm_header(struct lov_user_md *lum, char *path,
 				     space, prefix);
 		if (is_dir) {
 			if (!is_raw && lum->lmm_stripe_count == 0 &&
-			    lov_pattern(lum->lmm_pattern) != LOV_PATTERN_MDT) {
+			    !(lov_pattern(lum->lmm_pattern) & LOV_PATTERN_MDT)){
 				unsigned int scount;
 
 				rc = sattr_cache_get_defaults(NULL, path,
@@ -2797,7 +2797,7 @@ static void lov_dump_user_lmm_header(struct lov_user_md *lum, char *path,
 			else
 				llapi_printf(LLAPI_MSG_NORMAL, fmt_idx,
 					     lum->lmm_stripe_offset);
-		else if (lov_pattern(lum->lmm_pattern) == LOV_PATTERN_MDT)
+		else if (lov_pattern(lum->lmm_pattern) & LOV_PATTERN_MDT)
 			llapi_printf(LLAPI_MSG_NORMAL, "0");
 		else
 			llapi_printf(LLAPI_MSG_NORMAL, fmt_idx,
@@ -2856,7 +2856,7 @@ void lov_dump_user_lmm_v1v3(struct lov_user_md *lum, char *pool_name,
 
 	if (!skip_objs && (verbose & VERBOSE_OBJID) &&
 	    ((!is_dir && !(lum->lmm_pattern & LOV_PATTERN_F_RELEASED ||
-			   lov_pattern(lum->lmm_pattern) == LOV_PATTERN_MDT)) ||
+			   lov_pattern(lum->lmm_pattern) & LOV_PATTERN_MDT)) ||
 	     (is_dir && (lum->lmm_magic == LOV_USER_MAGIC_SPECIFIC)))) {
 		char *space = "      - ";
 

lustre/utils/liblustreapi_layout.c
@@ -556,7 +556,7 @@ struct llapi_layout *llapi_layout_get_by_xattr(void *lov_xattr,
 		else if (v1->lmm_pattern == (LOV_PATTERN_RAID0 |
 					 LOV_PATTERN_OVERSTRIPING))
 			comp->llc_pattern = LLAPI_LAYOUT_OVERSTRIPING;
-		else if (v1->lmm_pattern == LOV_PATTERN_MDT)
+		else if (v1->lmm_pattern & LOV_PATTERN_MDT)
 			comp->llc_pattern = LLAPI_LAYOUT_MDT;
 		else
 			/* Lustre only supports RAID0, overstripping
@@ -3424,8 +3424,8 @@ static int llapi_layout_sanity_cb(struct llapi_layout *layout,
 		goto out_err;
 
 	/* DoM sanity checks */
-	if (comp->llc_pattern == LLAPI_LAYOUT_MDT ||
-	    comp->llc_pattern == LOV_PATTERN_MDT) {
+	if (!(comp->llc_pattern & LLAPI_LAYOUT_INVALID) &&
+	    (comp->llc_pattern & (LLAPI_LAYOUT_MDT | LOV_PATTERN_MDT))) {
 		/* DoM components can't be extension components */
 		if (comp->llc_flags & LCME_FL_EXTENSION) {
 			args->lsa_rc = LSE_DOM_EXTENSION;
@@ -3615,7 +3615,8 @@ int llapi_layout_dom_size(struct llapi_layout *layout, uint64_t *size)
 	if (rc)
 		return -errno;
 
-	if (pattern != LOV_PATTERN_MDT && pattern != LLAPI_LAYOUT_MDT) {
+	if ((pattern & LLAPI_LAYOUT_INVALID) ||
+	    !(pattern & (LOV_PATTERN_MDT | LLAPI_LAYOUT_MDT))) {
 		*size = 0;
 		return 0;
 	}