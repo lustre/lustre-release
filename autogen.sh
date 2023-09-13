dbe4f860977455a9abe50165645a025bb6c46350
Jian Yu <yujian@whamcloud.com>
2023-09-13 11:00:53 -0700

    LU-17095 build: avoid modules.order nonexistence failure
    The modules.order is a temporary output file generated by
    kbuild while running "make" command. Sometimes, there is
    a race condition that causes the file not created and makes
    make command fail as follows:
    
    cat: ...//modules.order: No such file or directory
    
    This patch creates an empty modules.order file to avoid
    the error.
    
    Test-Parameters: trivial
    
    Change-Id: If779a727731f18e9409c35c0cd0deddd79559d3a
    Signed-off-by: Jian Yu <yujian@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52323
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- autogen.sh
+++ autogen.sh
@@ -16,6 +16,9 @@ for dir in libcfs lnet lustre snmp ; do
 	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $pw/$dir/autoconf"
 done
 
+# avoid the "modules.order: No such file or directory" failure
+touch modules.order
+
 libtoolize -q
 aclocal -I $pw/config $ACLOCAL_FLAGS
 autoheader
