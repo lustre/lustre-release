lustre/lustre-release

commit 70f092a0587866662735e1a6eaf27701a576370d

LU-1757 brw: add short io osc/ost transfer.
There's no need to do target bulk io for small amount of
data, and it requires extra network operations.

For this case we add short i/o.  When the i/o size is less
than or equal to some number of pages (default 3), we
encapsulate the data in the ptlrpc request.

With this patch, 4k direct i/o read latency on a Cray Aries
network (data is on flash on another node on the Aries)
drops from ~280 microseconds to ~200 microseconds.  Write
latency drops from ~370 microseconds to ~350 microseconds
(much more of write latency is waiting for write commit).

This translates to about a 25-30% performance improvement
on 4k direct i/o reads and 4k random reads.  (Write
performance improvement was small to non-existent.)

Improvement was similar with 8k i/o.

Buffered sequential i/o sees no improvement, because it
does not perform small i/os.

Performance data:
        access             = file-per-process
        pattern            = segmented (1 segment)
        ordering in a file = random offsets
        ordering inter file= no tasks offsets
        xfersize           = 4096 bytes
        blocksize          = 100 MiB

nprocs  xfsize  shortio dio     random  Read (MB/s)
1       4k      no      yes     no      15.0
8       4k      no      yes     no      73.4
16      4k      no      yes     no      81.1
1       4k      yes     yes     no      16.5
8       4k      yes     yes     no      95.2
16      4k      yes     yes     no      107.3
1       4k      no      no      yes     15.5
8       4k      no      no      yes     73.4
16      4k      no      no      yes     81.2
1       4k      yes     no      yes     16.8
8       4k      yes     no      yes     95.0
16      4k      yes     no      yes     106.5

Note even when individual i/o performance is not improved,
this change reduces the # of network operations required
for small i/o, which can help on large systems.

Signed-off-by: Patrick Farrell <paf@cray.com>

lustre/osc/osc_page.c
@@ -874,17 +874,27 @@ void osc_lru_unreserve(struct client_obd *cli, unsigned long npages)
  * are likely from the same page zone.
  */
 static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
+					    struct osc_brw_async_args *aa,
 					    int factor)
 {
-	int page_count = desc->bd_iov_count;
+	int page_count;
 	void *zone = NULL;
 	int count = 0;
 	int i;
 
-	LASSERT(ptlrpc_is_bulk_desc_kiov(desc->bd_type));
+	if (desc != NULL) {
+		LASSERT(ptlrpc_is_bulk_desc_kiov(desc->bd_type));
+		page_count = desc->bd_iov_count;
+	} else {
+		page_count = aa->aa_page_count;
+	}
 
 	for (i = 0; i < page_count; i++) {
-		void *pz = page_zone(BD_GET_KIOV(desc, i).kiov_page);
+		void *pz;
+		if (desc)
+			pz = page_zone(BD_GET_KIOV(desc, i).kiov_page);
+		else
+			pz = page_zone(aa->aa_ppga[i]->pg);
 
 		if (likely(pz == zone)) {
 			++count;
@@ -903,14 +913,16 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 		mod_zone_page_state(zone, NR_UNSTABLE_NFS, factor * count);
 }
 
-static inline void add_unstable_page_accounting(struct ptlrpc_bulk_desc *desc)
+static inline void add_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
+						struct osc_brw_async_args *aa)
 {
-	unstable_page_accounting(desc, 1);
+	unstable_page_accounting(desc, aa, 1);
 }
 
-static inline void dec_unstable_page_accounting(struct ptlrpc_bulk_desc *desc)
+static inline void dec_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
+						struct osc_brw_async_args *aa)
 {
-	unstable_page_accounting(desc, -1);
+	unstable_page_accounting(desc, aa, -1);
 }
 
 /**
@@ -927,12 +939,19 @@ static inline void dec_unstable_page_accounting(struct ptlrpc_bulk_desc *desc)
 void osc_dec_unstable_pages(struct ptlrpc_request *req)
 {
 	struct ptlrpc_bulk_desc *desc       = req->rq_bulk;
+	struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
 	struct client_obd       *cli        = &req->rq_import->imp_obd->u.cli;
-	int			 page_count = desc->bd_iov_count;
+	int			 page_count;
 	long			 unstable_count;
 
+	if (desc)
+		page_count = desc->bd_iov_count;
+	else
+		page_count = aa->aa_page_count;
+
 	LASSERT(page_count >= 0);
-	dec_unstable_page_accounting(desc);
+
+	dec_unstable_page_accounting(desc, aa);
 
 	unstable_count = atomic_long_sub_return(page_count,
 						&cli->cl_unstable_count);
@@ -954,14 +973,20 @@ void osc_dec_unstable_pages(struct ptlrpc_request *req)
 void osc_inc_unstable_pages(struct ptlrpc_request *req)
 {
 	struct ptlrpc_bulk_desc *desc = req->rq_bulk;
+	struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
 	struct client_obd       *cli  = &req->rq_import->imp_obd->u.cli;
-	long			 page_count = desc->bd_iov_count;
+	long			 page_count;
 
 	/* No unstable page tracking */
 	if (cli->cl_cache == NULL || !cli->cl_cache->ccc_unstable_check)
 		return;
 
-	add_unstable_page_accounting(desc);
+	if (desc)
+		page_count = desc->bd_iov_count;
+	else
+		page_count = aa->aa_page_count;
+
+	add_unstable_page_accounting(desc, aa);
 	atomic_long_add(page_count, &cli->cl_unstable_count);
 	atomic_long_add(page_count, &cli->cl_cache->ccc_unstable_nr);
 

lustre/include/lprocfs_status.h
@@ -776,6 +776,10 @@ int lprocfs_obd_max_pages_per_rpc_seq_show(struct seq_file *m, void *data);
 ssize_t lprocfs_obd_max_pages_per_rpc_seq_write(struct file *file,
 						const char __user *buffer,
 						size_t count, loff_t *off);
+int lprocfs_obd_short_io_bytes_seq_show(struct seq_file *m, void *data);
+ssize_t lprocfs_obd_short_io_bytes_seq_write(struct file *file,
+					     const char __user *buffer,
+					     size_t count, loff_t *off);
 
 struct root_squash_info;
 int lprocfs_wr_root_squash(const char __user *buffer, unsigned long count,

lustre/include/lustre_export.h
@@ -409,6 +409,13 @@ static inline bool imp_connect_disp_stripe(struct obd_import *imp)
 	return ocd->ocd_connect_flags & OBD_CONNECT_DISP_STRIPE;
 }
 
+static inline bool imp_connect_shortio(struct obd_import *imp)
+{
+	struct obd_connect_data *ocd = &imp->imp_connect_data;
+
+	return ocd->ocd_connect_flags & OBD_CONNECT_SHORTIO;
+}
+
 static inline __u64 exp_connect_ibits(struct obd_export *exp)
 {
 	struct obd_connect_data *ocd;

lustre/include/lustre_net.h
@@ -472,19 +472,30 @@
  * - single object with 16 pages is 512 bytes
  * - OST_IO_MAXREQSIZE must be at least 1 page of cookies plus some spillover
  * - Must be a multiple of 1024
- * - actual size is about 18K
  */
-#define _OST_MAXREQSIZE_SUM (sizeof(struct lustre_msg) + \
-			     sizeof(struct ptlrpc_body) + \
-			     sizeof(struct obdo) + \
-			     sizeof(struct obd_ioobj) + \
-			     sizeof(struct niobuf_remote) * DT_MAX_BRW_PAGES)
+#define _OST_MAXREQSIZE_BASE (sizeof(struct lustre_msg) + \
+			      sizeof(struct ptlrpc_body) + \
+			      sizeof(struct obdo) + \
+			      sizeof(struct obd_ioobj) + \
+			      sizeof(struct niobuf_remote))
+#define _OST_MAXREQSIZE_SUM (_OST_MAXREQSIZE_BASE + \
+			     sizeof(struct niobuf_remote) * \
+			     (DT_MAX_BRW_PAGES - 1))
 /**
  * FIEMAP request can be 4K+ for now
  */
 #define OST_MAXREQSIZE		(16 * 1024)
 #define OST_IO_MAXREQSIZE	max_t(int, OST_MAXREQSIZE, \
 				(((_OST_MAXREQSIZE_SUM - 1) | (1024 - 1)) + 1))
+/* Safe estimate of free space in standard RPC, provides upper limit for # of
+ * bytes of i/o to pack in RPC (skipping bulk transfer). */
+#define OST_SHORT_IO_SPACE	(OST_IO_MAXREQSIZE - _OST_MAXREQSIZE_BASE)
+
+/* Actual size used for short i/o buffer.  Calculation means this:
+ * At least one page (for large PAGE_SIZE), or 16 KiB, but not more
+ * than the available space aligned to a page boundary. */
+#define OBD_MAX_SHORT_IO_BYTES	(min(max(PAGE_SIZE, 16UL * 1024UL), \
+				    OST_SHORT_IO_SPACE & PAGE_MASK))
 
 #define OST_MAXREPSIZE		(9 * 1024)
 #define OST_IO_MAXREPSIZE	OST_MAXREPSIZE
@@ -497,6 +508,7 @@
  * rate of request buffer, please check comment of MDS_LOV_BUFSIZE for details.
  */
 #define OST_IO_BUFSIZE		max_t(int, OST_IO_MAXREQSIZE + 1024, 64 * 1024)
+
 
 /* Macro to hide a typecast. */
 #define ptlrpc_req_async_args(req) ((void *)&req->rq_async_args)

lustre/include/lustre_osc.h
@@ -445,6 +445,18 @@ struct osc_page {
 	cfs_time_t		ops_submit_time;
 };
 
+struct osc_brw_async_args {
+	struct obdo		*aa_oa;
+	int			 aa_requested_nob;
+	int			 aa_nio_count;
+	u32			 aa_page_count;
+	int			 aa_resends;
+	struct brw_page		**aa_ppga;
+	struct client_obd	*aa_cli;
+	struct list_head	 aa_oaps;
+	struct list_head	 aa_exts;
+};
+
 extern struct kmem_cache *osc_lock_kmem;
 extern struct kmem_cache *osc_object_kmem;
 extern struct kmem_cache *osc_thread_kmem;

lustre/include/lustre_req_layout.h
@@ -317,6 +317,7 @@ extern struct req_msg_field RMF_RCS;
 extern struct req_msg_field RMF_FIEMAP_KEY;
 extern struct req_msg_field RMF_FIEMAP_VAL;
 extern struct req_msg_field RMF_OST_ID;
+extern struct req_msg_field RMF_SHORT_IO;
 
 /* MGS config read message format */
 extern struct req_msg_field RMF_MGS_CONFIG_BODY;

lustre/include/obd.h
@@ -246,6 +246,7 @@ struct client_obd {
 	atomic_t		cl_pending_r_pages;
 	__u32			cl_max_pages_per_rpc;
 	__u32			cl_max_rpcs_in_flight;
+	__u32			cl_short_io_bytes;
 	struct obd_histogram	cl_read_rpc_hist;
 	struct obd_histogram	cl_write_rpc_hist;
 	struct obd_histogram	cl_read_page_hist;

lustre/ldlm/ldlm_lib.c
@@ -409,6 +409,8 @@ int client_obd_setup(struct obd_device *obddev, struct lustre_cfg *lcfg)
 	 * from OFD after connecting. */
 	cli->cl_max_pages_per_rpc = PTLRPC_MAX_BRW_PAGES;
 
+	cli->cl_short_io_bytes = OBD_MAX_SHORT_IO_BYTES;
+
 	/* set cl_chunkbits default value to PAGE_SHIFT,
 	 * it will be updated at OSC connection time. */
 	cli->cl_chunkbits = PAGE_SHIFT;

commit 4ba4976f525e957ef4c3ca7981bea01f72109ed6

LU-16671 osc: fix unstable pages for short IO
Unstable pages was written with theoretical support for
short IO (ie, no bulk, data-in-rpc, LU-1757), but since the
short IO code wasn't merged until years later, they were
probably never tested together.  And when you do, it
crashes.

In truth, short IO has no separate pages to be tracked,
which is why this is crashing.  This means that small write
RPCs won't be tracked in unstable pages, but that's a very
minor limitation and unlikely to cause trouble.  (and since
RPC allocations are not 'pages', they're just malloc'ed,
there's no good way to track them anyway)

Fixes: 70f092a ("LU-1757 brw: add short io osc/ost transfer.")
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I34b09f8324424c3ff0b0c09c86f01c938b643e37
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50451
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osc/osc_page.c
@@ -892,7 +892,6 @@ void osc_lru_unreserve(struct client_obd *cli, unsigned long npages)
 #endif
 
 static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
-					    struct osc_brw_async_args *aa,
 					    int factor)
 {
 	int page_count;
@@ -900,18 +899,15 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 	int count = 0;
 	int i;
 
-	if (desc != NULL) {
-		page_count = desc->bd_iov_count;
-	} else {
-		page_count = aa->aa_page_count;
-	}
+	ENTRY;
+
+	page_count = desc->bd_iov_count;
+
+	CDEBUG(D_PAGE, "%s %d unstable pages\n",
+	       factor == 1 ? "adding" : "removing", page_count);
 
 	for (i = 0; i < page_count; i++) {
-		void *pz;
-		if (desc)
-			pz = page_zone(desc->bd_vec[i].bv_page);
-		else
-			pz = page_zone(aa->aa_ppga[i]->bp_page);
+		void *pz = page_zone(desc->bd_vec[i].bv_page);
 
 		if (likely(pz == zone)) {
 			++count;
@@ -930,18 +926,18 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 	if (count > 0)
 		mod_zone_page_state(zone, (enum zone_stat_item)NR_WRITEBACK,
 				    factor * count);
+
+	EXIT;
 }
 
-static inline void add_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
-						struct osc_brw_async_args *aa)
+static inline void add_unstable_pages(struct ptlrpc_bulk_desc *desc)
 {
-	unstable_page_accounting(desc, aa, 1);
+	unstable_page_accounting(desc, 1);
 }
 
-static inline void dec_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
-						struct osc_brw_async_args *aa)
+static inline void dec_unstable_pages(struct ptlrpc_bulk_desc *desc)
 {
-	unstable_page_accounting(desc, aa, -1);
+	unstable_page_accounting(desc, -1);
 }
 
 /**
@@ -958,19 +954,21 @@ static inline void dec_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 void osc_dec_unstable_pages(struct ptlrpc_request *req)
 {
 	struct ptlrpc_bulk_desc *desc       = req->rq_bulk;
-	struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
 	struct client_obd       *cli        = &req->rq_import->imp_obd->u.cli;
 	int			 page_count;
 	long			 unstable_count;
 
-	if (desc)
-		page_count = desc->bd_iov_count;
-	else
-		page_count = aa->aa_page_count;
+	/* no desc means short io, which doesn't have separate unstable pages,
+	 * it's just using space inside the RPC itself
+	 */
+	if (!desc)
+		return;
+
+	page_count = desc->bd_iov_count;
 
 	LASSERT(page_count >= 0);
 
-	dec_unstable_page_accounting(desc, aa);
+	dec_unstable_pages(desc);
 
 	unstable_count = atomic_long_sub_return(page_count,
 						&cli->cl_unstable_count);
@@ -992,7 +990,6 @@ void osc_dec_unstable_pages(struct ptlrpc_request *req)
 void osc_inc_unstable_pages(struct ptlrpc_request *req)
 {
 	struct ptlrpc_bulk_desc *desc = req->rq_bulk;
-	struct osc_brw_async_args *aa = (void *)&req->rq_async_args;
 	struct client_obd       *cli  = &req->rq_import->imp_obd->u.cli;
 	long			 page_count;
 
@@ -1000,12 +997,15 @@ void osc_inc_unstable_pages(struct ptlrpc_request *req)
 	if (cli->cl_cache == NULL || !cli->cl_cache->ccc_unstable_check)
 		return;
 
-	if (desc)
-		page_count = desc->bd_iov_count;
-	else
-		page_count = aa->aa_page_count;
+	/* no desc means short io, which doesn't have separate unstable pages,
+	 * it's just using space inside the RPC itself
+	 */
+	if (!desc)
+		return;
 
-	add_unstable_page_accounting(desc, aa);
+	page_count = desc->bd_iov_count;
+
+	add_unstable_pages(desc);
 	atomic_long_add(page_count, &cli->cl_unstable_count);
 	atomic_long_add(page_count, &cli->cl_cache->ccc_unstable_nr);
 