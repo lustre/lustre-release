lustre/lustre-release

commit d15bfca07858c175c67ac6715fca2a876834b1ce

LU-10391 lnet: migrate full LNet NI information collection
Fill in the rest of the LNet NI state to report to user land. This
mostly covers the stats information of the NI and LND specific
tunables. To handle the LND specific tunables we need to reorder
the code to send an updated key table. With the additional
information I found status wasn't properly set and the nesting
for was properly set for multiple NIs per NET. This is now fixed.

Test-Parameters: trivial testlist=sanity-lnet
Fixes: 8f8f6e2f36e ("LU-10003 lnet: use Netlink to support old and new NI APIs")
Change-Id: I32b06b1ce8cb049a33f45f2310d31897ffa7dc90
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50255
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/gnilnd/gnilnd.c
@@ -24,6 +24,23 @@
  */
 #include "gnilnd.h"
 
+static int
+kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
+{
+	struct lnet_ioctl_config_gnilnd_tunables *tuns;
+	struct lnet_ni *ni = data;
+
+	if (!ni || !msg)
+		return -EINVAL;
+
+	if (cmd != LNET_CMD_NETS || type != LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES)
+		return -EOPNOTSUPP;
+
+	tuns = &ni->ni_lnd_tunables.lnd_tun_u.lnd_gni;
+	nla_put_u32(msg, LNET_NET_GNILND_TUNABLES_ATTR_LND_TIMEOUT,
+		    kgnilnd_timeout());
+}
+
 /* Primary entry points from LNET.  There are no guarantees against reentrance. */
 const struct lnet_lnd the_kgnilnd = {
 	.lnd_type       = GNILND,
@@ -33,6 +50,7 @@ const struct lnet_lnd the_kgnilnd = {
 	.lnd_send       = kgnilnd_send,
 	.lnd_recv       = kgnilnd_recv,
 	.lnd_eager_recv = kgnilnd_eager_recv,
+	.lnd_nl_get	= kgnilnd_nl_get,
 };
 
 kgn_data_t      kgnilnd_data;

lnet/include/lnet/lib-types.h
@@ -326,6 +326,7 @@ struct lnet_lnd {
 					 unsigned int dev_idx);
 
 	/* Handle LND specific Netlink handling */
+	int (*lnd_nl_get)(int cmd, struct sk_buff *msg, int type, void *data);
 	int (*lnd_nl_set)(int cmd, struct nlattr *attr, int type, void *data);
 
 	const struct ln_key_list *lnd_keys;
@@ -463,7 +464,7 @@ struct lnet_net {
  *
  * @LNET_NET_ATTR_UNSPEC:		unspecified attribute to catch errors
  *
- * @LNET_NET_ATTR_HDR:			grouping for LNet net data (NLA_NESTED)
+ * @LNET_NET_ATTR_HDR:			grouping for LNet net data (NLA_NUL_STRING)
  * @LNET_NET_ATTR_TYPE:			LNet net this NI belongs to (NLA_STRING)
  * @LNET_NET_ATTR_LOCAL:		Local NI information (NLA_NESTED)
  */
@@ -479,15 +480,32 @@ enum lnet_net_attrs {
 
 #define LNET_NET_ATTR_MAX (__LNET_NET_ATTR_MAX_PLUS_ONE - 1)
 
-/** enum lnet_net_local_ni_attrs      - LNet local NI netlink properties
- *					attributes that describe local NI
+/** enum lnet_net_local_ni_attrs	      - LNet local NI netlink properties
+ *						attributes that describe local
+ *						NI
  *
- * @LNET_NET_LOCAL_NI_ATTR_UNSPEC:	unspecified attribute to catch errors
+ * @LNET_NET_LOCAL_NI_ATTR_UNSPEC:		unspecified attribute to catch
+ *						errors
  *
- * @LNET_NET_LOCAL_NI_ATTR_NID:		NID that represents this NI (NLA_STRING)
- * @LNET_NET_LOCAL_NI_ATTR_STATUS:	State of this NI (NLA_STRING)
- * @LNET_NET_LOCAL_NI_ATTR_INTERFACE:	Defines physical devices (NLA_NESTED)
- *					Used to be many devices but no longer.
+ * @LNET_NET_LOCAL_NI_ATTR_NID:			NID that represents this NI
+ *						(NLA_STRING)
+ * @LNET_NET_LOCAL_NI_ATTR_STATUS:		State of this NI (NLA_STRING)
+ * @LNET_NET_LOCAL_NI_ATTR_INTERFACE:		Defines physical devices. used
+ *						to be many devices but no longer
+ *						(NLA_NESTED)
+ *
+ * @LNET_NET_LOCAL_NI_ATTR_STATS:		NI general msg stats (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_UDSP_INFO:		NI UDSP state (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_SEND_STATS:		NI send stats (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_RECV_STATS:		NI recieved stats (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_DROPPED_STATS:	NI dropped stats (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_HEALTH_STATS:	NI health stats (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_TUNABLES:		NI tunables (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES:	NI LND tunables (NLA_NESTED)
+ * @LNET_NET_LOCAL_NI_ATTR_DEV_CPT:		NI CPT interface bound to
+ *						(NLA_S32)
+ * @LNET_NET_LOCAL_NI_ATTR_CPTS:		CPT core used by this NI
+ *						(NLA_STRING)
  */
 enum lnet_net_local_ni_attrs {
 	LNET_NET_LOCAL_NI_ATTR_UNSPEC = 0,
@@ -495,6 +513,17 @@ enum lnet_net_local_ni_attrs {
 	LNET_NET_LOCAL_NI_ATTR_NID,
 	LNET_NET_LOCAL_NI_ATTR_STATUS,
 	LNET_NET_LOCAL_NI_ATTR_INTERFACE,
+
+	LNET_NET_LOCAL_NI_ATTR_STATS,
+	LNET_NET_LOCAL_NI_ATTR_UDSP_INFO,
+	LNET_NET_LOCAL_NI_ATTR_SEND_STATS,
+	LNET_NET_LOCAL_NI_ATTR_RECV_STATS,
+	LNET_NET_LOCAL_NI_ATTR_DROPPED_STATS,
+	LNET_NET_LOCAL_NI_ATTR_HEALTH_STATS,
+	LNET_NET_LOCAL_NI_ATTR_TUNABLES,
+	LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES,
+	LNET_NET_LOCAL_NI_DEV_CPT,
+	LNET_NET_LOCAL_NI_CPTS,
 
 	__LNET_NET_LOCAL_NI_ATTR_MAX_PLUS_ONE,
 };
@@ -518,6 +547,110 @@ enum lnet_net_local_ni_intf_attrs {
 };
 
 #define LNET_NET_LOCAL_NI_INTF_ATTR_MAX (__LNET_NET_LOCAL_NI_INTF_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_net_local_ni_stats_attrs	      - LNet NI netlink properties
+ *						attributes that reports the
+ *						network traffic stats
+ *
+ * @LNET_NET_LOCAL_NI_STATS_ATTR_UNSPEC:	unspecified attribute to catch
+ *						errors
+ *
+ * @LNET_NET_LOCAL_NI_STATS_ATTR_SEND_COUNT:	Number of sent messages
+ *						(NLA_U32)
+ * @LNET_NET_LOCAL_NI_STATS_ATTR_RECV_COUNT:	Number of recieved messages
+ *						(NLA_U32)
+ * @LNET_NET_LOCAL_NI_STATS_ATTR_DROP_COUNT:	Number of dropped messages
+ *						(NLA_U32)
+ */
+enum lnet_net_local_ni_stats_attrs {
+	LNET_NET_LOCAL_NI_STATS_ATTR_UNSPEC = 0,
+
+	LNET_NET_LOCAL_NI_STATS_ATTR_SEND_COUNT,
+	LNET_NET_LOCAL_NI_STATS_ATTR_RECV_COUNT,
+	LNET_NET_LOCAL_NI_STATS_ATTR_DROP_COUNT,
+	__LNET_NET_LOCAL_NI_STATS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_NET_LOCAL_NI_STATS_ATTR_MAX (__LNET_NET_LOCAL_NI_STATS_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_net_local_ni_msg_stats_attrs	      - LNet NI netlink
+ *							properties attributes
+ *							that reports the message
+ *							type traffic stats
+ *
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_UNSPEC:		unspecified attribute
+ *							to catch errors
+ *
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_PUT_COUNT:		Number of PUT messages
+ *							(NLA_U32)
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_GET_COUNT:		Number of GET messages
+ *							(NLA_U32)
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_REPLY_COUNT:	Number of REPLY messages
+ *							(NLA_U32)
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_ACK_COUNT:		Number of ACK messages
+ *							(NLA_U32)
+ * @LNET_NET_LOCAL_NI_MSG_STATS_ATTR_HELLO_COUNT:	Number of HELLO messages
+ *							(NLA_U32)
+ */
+enum lnet_net_local_ni_msg_stats_attrs {
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_UNSPEC = 0,
+
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_PUT_COUNT,
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_GET_COUNT,
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_REPLY_COUNT,
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_ACK_COUNT,
+	LNET_NET_LOCAL_NI_MSG_STATS_ATTR_HELLO_COUNT,
+	__LNET_NET_LOCAL_NI_MSG_STATS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_NET_LOCAL_NI_MSG_STATS_ATTR_MAX (__LNET_NET_LOCAL_NI_MSG_STATS_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_net_local_ni_health_stats_attrs	      - LNet NI netlink
+ *							properties attributes
+ *							that reports how
+ *							healthly it is.
+ *
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_UNSPEC:		unspecified attribute
+ *							to catch errors
+ *
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_FATAL_ERRORS:	How many fatal errors
+ *							(NLA_S32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_LEVEL:		How healthly is NI
+ *							(NLA_S32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_INTERRUPTS:	How many interrupts
+ *							happened (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_DROPPED:	How much traffic has
+ *							been dropped (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_ABORTED:	How many aborts
+ *							happened (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_NO_ROUTE:	How often routing broke
+ *							(NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_TIMEOUTS:	How often timeouts
+ *							occurred (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_ERROR:		The number of errors
+ *							reported (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_PING_COUNT:	Number of successful
+ *							ping (NLA_U32)
+ * @LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_NEXT_PING:	Number of next pings
+ *							(NLA_U64)
+ */
+enum lnet_net_local_ni_health_stats_attrs {
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_UNSPEC = 0,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_PAD = LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_UNSPEC,
+
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_FATAL_ERRORS,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_LEVEL,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_INTERRUPTS,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_DROPPED,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_ABORTED,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_NO_ROUTE,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_TIMEOUTS,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_ERROR,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_PING_COUNT,
+	LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_NEXT_PING,
+	__LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_MAX_PLUS_ONE,
+};
+#define LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_MAX (__LNET_NET_LOCAL_NI_HEALTH_STATS_ATTR_MAX_PLUS_ONE - 1)
 
 /** enum lnet_net_local_ni_tunables_attrs	      - LNet NI tunables
  *							netlink properties.
@@ -632,6 +765,51 @@ enum lnet_ping_peer_ni_attr {
 };
 
 #define LNET_PING_PEER_NI_ATTR_MAX (__LNET_PING_PEER_NI_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_udsp_info_attr			      - LNet UDSP information reported for
+ *							some subsystem that tracks it.
+ *
+ * @LNET_UDSP_INFO_ATTR_UNSPEC:				unspecified attribute to catch errors
+ *
+ * @LNET_UDSP_INFO_ATTR_NET_PRIORITY,			LNet net priority in selection.
+ *							(NLA_S32)
+ * @LNET_UDSP_INFO_ATTR_NID_PRIORITY,			NID's priority in selection.
+ *							(NLA_S32)
+ * @LNET_UDSP_INFO_ATTR_PREF_RTR_NIDS_LIST:		Which gateway's are preferred.
+ *							(NLA_NESTED)
+ * @LNET_UDSP_INFO_ATTR_PREF_NIDS_LIST:			Which NIDs are preferred.
+ *							(NLA_NESTED)
+ */
+enum lnet_udsp_info_attr {
+	LNET_UDSP_INFO_ATTR_UNSPEC = 0,
+
+	LNET_UDSP_INFO_ATTR_NET_PRIORITY,
+	LNET_UDSP_INFO_ATTR_NID_PRIORITY,
+	LNET_UDSP_INFO_ATTR_PREF_RTR_NIDS_LIST,
+	LNET_UDSP_INFO_ATTR_PREF_NIDS_LIST,
+	__LNET_UDSP_INFO_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_UDSP_INFO_ATTR_MAX (__LNET_UDSP_INFO_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_udsp_info_pref_nids_attr		      - LNet UDSP information reported for
+ *							some subsystem that tracks it.
+ *
+ * @LNET_UDSP_INFO_PREF_NIDS_ATTR_UNSPEC:		unspecified attribute to catch errors
+ *
+ * @LNET_UDSP_INFO_PREF_NIDS_ATTR_INDEX,		UDSP prority NIDs label
+ *							(NLA_NUL_STRING)
+ * @LNET_UDSP_INFO_PREF_NIDS_ATTR_NID,			UDSP prority NID (NLA_STRING)
+ */
+enum lnet_udsp_info_pref_nids_attr {
+	LNET_UDSP_INFO_PREF_NIDS_ATTR_UNSPEC = 0,
+
+	LNET_UDSP_INFO_PREF_NIDS_ATTR_INDEX,
+	LNET_UDSP_INFO_PREF_NIDS_ATTR_NID,
+	__LNET_UDSP_INFO_PREF_NIDS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_UDSP_INFO_PREF_NIDS_ATTR_MAX (__LNET_UDSP_INFO_PREF_NIDS_ATTR_MAX_PLUS_ONE - 1)
 
 struct lnet_ni {
 	/* chain on the lnet_net structure */

lnet/include/lnet/udsp.h
@@ -138,6 +138,7 @@ int lnet_udsp_demarshal_add(void *bulk, __u32 bulk_size);
  *	get information of how the UDSP policies impacted the given
  *	construct.
  */
-void lnet_udsp_get_construct_info(struct lnet_ioctl_construct_udsp_info *info);
+void lnet_udsp_get_construct_info(struct lnet_ioctl_construct_udsp_info *info,
+				  struct lnet_nid *nid);
 
 #endif /* UDSP_H */

lnet/klnds/gnilnd/gnilnd.h
@@ -1776,6 +1776,15 @@ kgnilnd_find_net(lnet_nid_t nid, kgn_net_t **netp)
 #define KGNILND_POISON(ptr, c, s) memset(ptr, c, s)
 #endif
 
+enum kgnilnd_ni_lnd_tunables_attr {
+	LNET_NET_GNILND_TUNABLES_ATTR_UNSPEC = 0,
+
+	LNET_NET_GNILND_TUNABLES_ATTR_LND_TIMEOUT,
+	__LNET_NET_GNILND_TUNABLES_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_NET_GNILND_TUNABLES_ATTR_MAX (__LNET_NET_GNILND_TUNABLES_ATTR_MAX_PLUS_ONE - 1)
+
 int kgnilnd_dev_init(kgn_device_t *dev);
 void kgnilnd_dev_fini(kgn_device_t *dev);
 int kgnilnd_startup(struct lnet_ni *ni);

lnet/klnds/kfilnd/kfilnd.c
@@ -366,6 +366,31 @@ static const struct ln_key_list kfilnd_tunables_keys = {
 };
 
 static int
+kfilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
+{
+	struct lnet_lnd_tunables *tunables;
+	struct lnet_ni *ni = data;
+
+	if (!ni || !msg)
+		return -EINVAL;
+
+	if (cmd != LNET_CMD_NETS || type != LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES)
+		return -EOPNOTSUPP;
+
+	tunables = &ni->ni_lnd_tunables;
+	nla_put_s32(msg, LNET_NET_KFILND_TUNABLES_ATTR_PROV_MAJOR,
+		    tunables->lnd_tun_u.lnd_kfi.lnd_prov_major_version);
+	nla_put_s32(msg, LNET_NET_KFILND_TUNABLES_ATTR_PROV_MINOR,
+		    tunables->lnd_tun_u.lnd_kfi.lnd_prov_minor_version);
+	nla_put_s32(msg, LNET_NET_KFILND_TUNABLES_ATTR_AUTH_KEY,
+		    tunables->lnd_tun_u.lnd_kfi.lnd_auth_key);
+	nla_put_string(msg, LNET_NET_KFILND_TUNABLES_ATTR_TRAFFIC_CLASS,
+		       tunables->lnd_tun_u.lnd_kfi.lnd_traffic_class_str);
+
+	return 0;
+}
+
+static int
 kfilnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 {
 	struct lnet_lnd_tunables *tunables = data;
@@ -407,6 +432,7 @@ static const struct lnet_lnd the_kfilnd = {
 	.lnd_shutdown	= kfilnd_shutdown,
 	.lnd_send	= kfilnd_send,
 	.lnd_recv	= kfilnd_recv,
+	.lnd_nl_get	= kfilnd_nl_get,
 	.lnd_nl_set	= kfilnd_nl_set,
 	.lnd_keys	= &kfilnd_tunables_keys,
 };

lnet/klnds/o2iblnd/o2iblnd.c
@@ -1251,8 +1251,48 @@ static const struct ln_key_list kiblnd_tunables_keys = {
 			.lkp_value	= "conns_per_peer",
 			.lkp_data_type	= NLA_U16
 		},
+		[LNET_NET_O2IBLND_TUNABLES_ATTR_LND_TIMEOUT] = {
+			.lkp_value	= "timeout",
+			.lkp_data_type	= NLA_U32,
+		},
 	},
 };
+
+static int
+kiblnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
+{
+	struct lnet_ioctl_config_o2iblnd_tunables *tuns;
+	struct lnet_ni *ni = data;
+
+	if (!ni || !msg)
+		return -EINVAL;
+
+	if (cmd != LNET_CMD_NETS || type != LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES)
+		return -EOPNOTSUPP;
+
+	tuns = &ni->ni_lnd_tunables.lnd_tun_u.lnd_o2ib;
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_HIW_PEER_CREDITS,
+		    tuns->lnd_peercredits_hiw);
+	if (tuns->lnd_map_on_demand) {
+		nla_put_flag(msg,
+			     LNET_NET_O2IBLND_TUNABLES_ATTR_MAP_ON_DEMAND);
+	}
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_CONCURRENT_SENDS,
+		    tuns->lnd_concurrent_sends);
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_FMR_POOL_SIZE,
+		    tuns->lnd_fmr_pool_size);
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_FMR_FLUSH_TRIGGER,
+		    tuns->lnd_fmr_flush_trigger);
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_FMR_CACHE,
+		    tuns->lnd_fmr_cache);
+	nla_put_u16(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_NTX, tuns->lnd_ntx);
+	nla_put_u16(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_CONNS_PER_PEER,
+		    tuns->lnd_conns_per_peer);
+	nla_put_u32(msg, LNET_NET_O2IBLND_TUNABLES_ATTR_LND_TIMEOUT,
+		    kiblnd_timeout());
+
+	return 0;
+}
 
 static int
 kiblnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
@@ -3713,6 +3753,7 @@ static const struct lnet_lnd the_o2iblnd = {
 	.lnd_send	= kiblnd_send,
 	.lnd_recv	= kiblnd_recv,
 	.lnd_get_dev_prio = kiblnd_get_dev_prio,
+	.lnd_nl_get	= kiblnd_nl_get,
 	.lnd_nl_set	= kiblnd_nl_set,
 	.lnd_keys	= &kiblnd_tunables_keys,
 };

lnet/klnds/o2iblnd/o2iblnd.h
@@ -111,6 +111,7 @@ enum kiblnd_ni_lnd_tunables_attr {
 	LNET_NET_O2IBLND_TUNABLES_ATTR_FMR_CACHE,
 	LNET_NET_O2IBLND_TUNABLES_ATTR_NTX,
 	LNET_NET_O2IBLND_TUNABLES_ATTR_CONNS_PER_PEER,
+	LNET_NET_O2IBLND_TUNABLES_ATTR_LND_TIMEOUT,
 	__LNET_NET_O2IBLND_TUNABLES_ATTR_MAX_PLUS_ONE,
 };
 

lnet/klnds/socklnd/socklnd.c
@@ -806,10 +806,35 @@ static const struct ln_key_list ksocknal_tunables_keys = {
 	.lkl_list			= {
 		[LNET_NET_SOCKLND_TUNABLES_ATTR_CONNS_PER_PEER]  = {
 			.lkp_value	= "conns_per_peer",
-			.lkp_data_type	= NLA_S32
+			.lkp_data_type	= NLA_U16
+		},
+		[LNET_NET_SOCKLND_TUNABLES_ATTR_LND_TIMEOUT]	= {
+			.lkp_value	= "timeout",
+			.lkp_data_type	= NLA_U32
 		},
 	},
 };
+
+static int
+ksocknal_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
+{
+	struct lnet_lnd_tunables *tun;
+	struct lnet_ni *ni = data;
+
+	if (!ni || !msg)
+		return -EINVAL;
+
+	 if (cmd != LNET_CMD_NETS || type != LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES)
+		return -EOPNOTSUPP;
+
+	tun = &ni->ni_lnd_tunables;
+	nla_put_u16(msg, LNET_NET_SOCKLND_TUNABLES_ATTR_CONNS_PER_PEER,
+		    tun->lnd_tun_u.lnd_sock.lnd_conns_per_peer);
+	nla_put_u32(msg, LNET_NET_SOCKLND_TUNABLES_ATTR_LND_TIMEOUT,
+		    ksocknal_timeout());
+
+	return 0;
+}
 
 static int
 ksocknal_nl_set(int cmd, struct nlattr *attr, int type, void *data)
@@ -2590,6 +2615,7 @@ static const struct lnet_lnd the_ksocklnd = {
 	.lnd_recv		= ksocknal_recv,
 	.lnd_notify_peer_down	= ksocknal_notify_gw_down,
 	.lnd_accept		= ksocknal_accept,
+	.lnd_nl_get		= ksocknal_nl_get,
 	.lnd_nl_set		= ksocknal_nl_set,
 	.lnd_keys		= &ksocknal_tunables_keys,
 };

commit 87ca3cffe697d11518a65bec5ab0504a9ceaea35

LU-17259 lnet: kgnilnd_nl_get should return 0 on success
Fix build failure
error: control reaches end of non-void function [-Werror=return-type]

Test-Parameters: trivial
Fixes: d15bfca078 ("LU-10391 lnet: migrate full LNet NI information collection")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I09dd76c46620107d6c3f89cf59b9d9190578ef60
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52972
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/gnilnd/gnilnd.c
@@ -39,6 +39,7 @@ kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
 	tuns = &ni->ni_lnd_tunables.lnd_tun_u.lnd_gni;
 	nla_put_u32(msg, LNET_NET_GNILND_TUNABLES_ATTR_LND_TIMEOUT,
 		    kgnilnd_timeout());
+	return 0;
 }
 
 /* Primary entry points from LNET.  There are no guarantees against reentrance. */