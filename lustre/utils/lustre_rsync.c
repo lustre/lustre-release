lustre/lustre-release

commit 4fc998320b51b1730326df5db470b2ca9c1da7c9

LU-17000 coverity: Fix Resource Leak(1)
This patch fixes Resource leak error reported
by coverity run.

CoverityID: 397493 ("Resource Leak"): liblustreapi_mirror.c
CoverityID: 397494 ("Resource Leak"): lustre_rsync.c

Test-Parameters: trivial fstype=zfs testlist=sanity,sanityn,conf-sanity
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I10e9fd0945cf13824c25faa62b4310796b09bade
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52384
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lustre_rsync.c
@@ -1728,10 +1728,12 @@ int lr_replicate(void)
 	rc = 0;
 
 out:
-	if (info)
-		free(info);
+	if (changelog_priv)
+		free(changelog_priv);
 	if (ext)
 		free(ext);
+	if (info)
+		free(info);
 
 	return rc;
 }

lustre/utils/liblustreapi_mirror.c
@@ -310,8 +310,10 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 				continue;
 			}
 		}
-		if (!nr)
+		if (!nr) {
+			free(buf);
 			return result;
+		}
 	}
 
 	while (!eof) {

commit 6aede1254803dfbde728a6b34c627ccc924a8f79

LU-16518 rsync: fix new clang error in lustre_rsync.c
If we bail out of the function early, changelog_priv
may not be initialized when we check it during the
cleanup code. Initialize changelog_priv to NULL to
avoid this.

Fixes: 4fc99832 ("LU-17000 coverity: Fix Resource Leak(1)")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Ide0430284b18f085e421cdea677ea14e0a4bac14
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52565
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/utils/lustre_rsync.c
@@ -1544,7 +1544,7 @@ void lr_print_failure(struct lr_info *info, int rc)
 /* Replicate filesystem operations from src_path to target_path */
 int lr_replicate(void)
 {
-	void *changelog_priv;
+	void *changelog_priv = NULL;
 	struct lr_info *info;
 	struct lr_info *ext = NULL;
 	time_t start;