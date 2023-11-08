lustre/lustre-release

commit 306a9b666e5ea2882f704d93483355e7e147544f

LU-16272 libcfs: cfs_hash_for_each_empty optimization
Restarts from bucket 0 in cfs_hash_for_each_empty()
cause excessive cpu consumption while checking first empty
buckets.

HPE-bug-id: LUS-11311
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: Ic03875ea25101052468213043128912ac46daf32
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48972
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/hash.c
@@ -1597,7 +1597,7 @@ EXPORT_SYMBOL(cfs_hash_size_get);
  */
 static int
 cfs_hash_for_each_relax(struct cfs_hash *hs, cfs_hash_for_each_cb_t func,
-			void *data, int start)
+			void *data, int *pstart)
 {
 	struct hlist_node	*hnode;
 	struct hlist_node	*next = NULL;
@@ -1620,7 +1620,7 @@ again:
 	cfs_hash_for_each_bucket(hs, &bd, i) {
 		struct hlist_head *hhead;
 
-		if (i < start)
+		if (pstart && i < *pstart)
 			continue;
 		else if (end > 0 && i >= end)
 			break;
@@ -1679,13 +1679,15 @@ again:
 			break;
         }
 
-	if (start > 0 && rc == 0) {
-		end = start;
-		start = 0;
+	if (pstart && *pstart > 0 && rc == 0) {
+		end = *pstart;
+		*pstart = 0;
 		goto again;
 	}
 
 	cfs_hash_unlock(hs, 0);
+	if (pstart)
+		*pstart = i;
 	return count;
 }
 
@@ -1706,7 +1708,7 @@ cfs_hash_for_each_nolock(struct cfs_hash *hs,
 		RETURN(-EOPNOTSUPP);
 
 	cfs_hash_for_each_enter(hs);
-	cfs_hash_for_each_relax(hs, func, data, start);
+	cfs_hash_for_each_relax(hs, func, data, &start);
 	cfs_hash_for_each_exit(hs);
 
 	RETURN(0);
@@ -1729,6 +1731,7 @@ cfs_hash_for_each_empty(struct cfs_hash *hs,
                         cfs_hash_for_each_cb_t func, void *data)
 {
         unsigned  i = 0;
+	int start = 0;
         ENTRY;
 
         if (cfs_hash_with_no_lock(hs))
@@ -1740,11 +1743,12 @@ cfs_hash_for_each_empty(struct cfs_hash *hs,
 		return -EOPNOTSUPP;
 
 	cfs_hash_for_each_enter(hs);
-	while (cfs_hash_for_each_relax(hs, func, data, 0)) {
+	while (cfs_hash_for_each_relax(hs, func, data, &start)) {
 		CDEBUG(D_INFO, "Try to empty hash: %s, loop: %u\n",
 		       hs->hs_name, i++);
 	}
 	cfs_hash_for_each_exit(hs);
+	LASSERT(atomic_read(&hs->hs_count) == 0);
 	RETURN(0);
 }
 EXPORT_SYMBOL(cfs_hash_for_each_empty);

commit f2f8b6deaf54f1a264b31b44f6cf875fa1629ab2

LU-17203 libcfs: ignore remaining items
remove the assertion checking libcfs hashtable for emptiness
in cfs_hash_for_each_empty(). the only user of this hashtable
is per-export ldlm locks set. in this case it's legal that
some locks can't be removed from the hashtable being in the
process of enqueuing. the hashtable is destroyed from the
export destroy function which in turn is called only when all
RPCs on this export are done (exp_rpc_count==0).

Fixes: 306a9b666e ("LU-16272 libcfs: cfs_hash_for_each_empty optimization")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I2b853b017bb7247a0c60cc8f464c2e08d649f0eb
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52726
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

libcfs/libcfs/hash.c
@@ -1748,7 +1748,6 @@ cfs_hash_for_each_empty(struct cfs_hash *hs,
 		       hs->hs_name, i++);
 	}
 	cfs_hash_for_each_exit(hs);
-	LASSERT(atomic_read(&hs->hs_count) == 0);
 	RETURN(0);
 }
 EXPORT_SYMBOL(cfs_hash_for_each_empty);