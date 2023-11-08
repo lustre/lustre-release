lustre/lustre-release

commit 7ac399c5aec01186ad4c9a7153aea400777c897f

LU-16949 lnet: get monitor thread to update ping buffer
Make sure that ping buffer updates requested by o2iblnd and
socklnd are performed by the LNet monitor thread.
Having the LNDs do these updates via an LNet API directly caused a
lock-up due to spinlock acquisition while in an interrupt context
in Centos 7.9 environment.
To avoid LNet trying to update the ping buffer for an LNI which is
still initializing, check that o2iblnd net is fully initialized
(IBLND_INIT_ALL) before requesting the ping buffer update.

Fixes: da230373bd ("LU-16563 lnet: use discovered ni status")
Signed-off-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Change-Id: I87ff8791937f5a0ead6096ff33e8c0a8087f8ddd
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51635
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1315,4 +1315,5 @@ static inline int notifier_from_ioctl_errno(int err)
 	return notifier_from_errno(err) | NOTIFY_STOP_MASK;
 }
 
+void lnet_mark_ping_buffer_for_update(void);
 #endif

lnet/include/lnet/lib-types.h
@@ -1519,6 +1519,9 @@ struct lnet {
 	atomic_t			ln_late_msg_count;
 	/* Total amount of time past their deadline for all late ^ messages */
 	atomic64_t			ln_late_msg_nsecs;
+
+	/* for LNDs to signal that ping buffer needs updating */
+	atomic_t                        ln_update_ping_buf;
 };
 
 struct genl_filter_list {

lnet/lnet/api-ni.c
@@ -3920,12 +3920,22 @@ out:
 	return rc;
 }
 
+void lnet_mark_ping_buffer_for_update(void)
+{
+	if (the_lnet.ln_routing)
+		return;
+
+	atomic_set(&the_lnet.ln_update_ping_buf, 1);
+	complete(&the_lnet.ln_mt_wait_complete);
+}
+EXPORT_SYMBOL(lnet_mark_ping_buffer_for_update);
+
 void lnet_update_ping_buffer(void)
 {
 	struct lnet_ping_buffer *pbuf;
 	struct lnet_handle_md ping_mdh;
 
-	if (the_lnet.ln_routing)
+	if (atomic_dec_if_positive(&the_lnet.ln_update_ping_buf) < 0)
 		return;
 
 	mutex_lock(&the_lnet.ln_api_mutex);
@@ -3938,7 +3948,6 @@ void lnet_update_ping_buffer(void)
 
 	mutex_unlock(&the_lnet.ln_api_mutex);
 }
-EXPORT_SYMBOL(lnet_update_ping_buffer);
 
 void lnet_incr_dlc_seq(void)
 {

lnet/lnet/lib-move.c
@@ -4039,10 +4039,12 @@ lnet_monitor_thread(void *arg)
 	 *  1. Checks the aliveness of routers
 	 *  2. Checks if there are messages on the resend queue to resend
 	 *     them.
-	 *  3. Check if there are any NIs on the local recovery queue and
+	 *  3. Checks if there are any NIs on the local recovery queue and
 	 *     pings them
 	 *  4. Checks if there are any NIs on the remote recovery queue
 	 *     and pings them.
+	 *  5. Updates the ping buffer if requested by LNDs upon interface
+	 *     state change
 	 */
 	while (the_lnet.ln_mt_state == LNET_MT_STATE_RUNNING) {
 		now = ktime_get_real_seconds();
@@ -4061,6 +4063,7 @@ lnet_monitor_thread(void *arg)
 		nlpnis = lnet_recover_peer_nis(peer_nids, LNET_MAX_NNIDS);
 		lnet_health_update_console(local_nids, nnis, peer_nids, nlpnis,
 					   now);
+		lnet_update_ping_buffer();
 
 		/*
 		 * TODO do we need to check if we should sleep without

lnet/klnds/o2iblnd/o2iblnd.c
@@ -2649,12 +2649,13 @@ kiblnd_set_ni_fatal_on(struct kib_hca_dev *hdev, int val)
 
 		if (!update_ping_buf &&
 		    (ni->ni_state == LNET_NI_STATE_ACTIVE) &&
-		    (val != ni_state_before))
+		    (val != ni_state_before) &&
+		    (net->ibn_init == IBLND_INIT_ALL))
 			update_ping_buf = true;
 	}
 
 	if (update_ping_buf)
-		lnet_update_ping_buffer();
+		lnet_mark_ping_buffer_for_update();
 }
 
 static void
@@ -3154,12 +3155,13 @@ kiblnd_handle_link_state_change(struct net_device *dev,
 ni_done:
 		if (!update_ping_buf &&
 		    (ni->ni_state == LNET_NI_STATE_ACTIVE) &&
-		    (atomic_read(&ni->ni_fatal_error_on) != ni_state_before))
+		    (atomic_read(&ni->ni_fatal_error_on) != ni_state_before) &&
+		    (net->ibn_init == IBLND_INIT_ALL))
 			update_ping_buf = true;
 	}
 
 	if (update_ping_buf)
-		lnet_update_ping_buffer();
+		lnet_mark_ping_buffer_for_update();
 out:
 	return 0;
 }
@@ -3191,12 +3193,13 @@ kiblnd_handle_inetaddr_change(struct in_ifaddr *ifa, unsigned long event)
 		ni_state_before = lnet_set_link_fatal_state(ni, link_down);
 		if (!update_ping_buf &&
 		    (ni->ni_state == LNET_NI_STATE_ACTIVE) &&
-		    ((event == NETDEV_DOWN) != ni_state_before))
+		    ((event == NETDEV_DOWN) != ni_state_before) &&
+		    (net->ibn_init == IBLND_INIT_ALL))
 			update_ping_buf = true;
 	}
 
 	if (update_ping_buf)
-		lnet_update_ping_buffer();
+		lnet_mark_ping_buffer_for_update();
 out:
 	return 0;
 }

lnet/klnds/socklnd/socklnd.c
@@ -1999,7 +1999,7 @@ ni_done:
 	}
 
 	if (update_ping_buf)
-		lnet_update_ping_buffer();
+		lnet_mark_ping_buffer_for_update();
 out:
 	return 0;
 }
@@ -2048,7 +2048,7 @@ ksocknal_handle_inetaddr_change(struct in_ifaddr *ifa, unsigned long event)
 	}
 
 	if (update_ping_buf)
-		lnet_update_ping_buffer();
+		lnet_mark_ping_buffer_for_update();
 out:
 	return 0;
 }

commit a8e66b899afc0bc09285d426e3acef9d3d1f7694

LU-17103 lnet: use workqueue for lnd ping buffer updates
Introduce workqueue for handling lnd-initiated ping buffer
update requests.

This is done to avoid the possibility of monitor thread
lock up waiting for the "old" ping buffer refcount to get
decremented during the update, while the message which
triggers the decrement is on the monitor thread's own queue
waiting to be processed.

Test-Parameters: trivial
Test-Parameters: testlist=sanity-lnet env=ONLY="207 500",ONLY_REPEAT=50
Fixes: 7ac399c5 ("LU-16949 lnet: get monitor thread to update ping buffer")
Signed-off-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Change-Id: I5176581703e52f4adbfff417040bebcc2489b79e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52522
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1304,7 +1304,6 @@ lnet_set_route_aliveness(struct lnet_route *route, bool alive)
 		       old ? "up" : "down",
 		       alive ? "up" : "down");
 }
-void lnet_update_ping_buffer(void);
 
 extern struct blocking_notifier_head lnet_ioctl_list;
 static inline int notifier_from_ioctl_errno(int err)
@@ -1315,4 +1314,5 @@ static inline int notifier_from_ioctl_errno(int err)
 }
 
 void lnet_mark_ping_buffer_for_update(void);
+void lnet_queue_ping_buffer_update(void);
 #endif

lnet/include/lnet/lib-types.h
@@ -1910,6 +1910,12 @@ struct lnet {
 
 	/* for LNDs to signal that ping buffer needs updating */
 	atomic_t                        ln_update_ping_buf;
+
+	/* workqueue for serving lnd ping buffer update requests */
+	struct workqueue_struct		*ln_pb_update_wq;
+	struct work_struct		ln_pb_update_work;
+
+	atomic_t                        ln_pb_update_ready;
 };
 
 struct genl_filter_list {

lnet/lnet/api-ni.c
@@ -3937,23 +3937,39 @@ void lnet_mark_ping_buffer_for_update(void)
 }
 EXPORT_SYMBOL(lnet_mark_ping_buffer_for_update);
 
-void lnet_update_ping_buffer(void)
+void lnet_update_ping_buffer(struct work_struct *work)
 {
 	struct lnet_ping_buffer *pbuf;
 	struct lnet_handle_md ping_mdh;
 
-	if (atomic_dec_if_positive(&the_lnet.ln_update_ping_buf) < 0)
-		return;
-
 	mutex_lock(&the_lnet.ln_api_mutex);
 
-	if (!lnet_ping_target_setup(&pbuf, &ping_mdh,
+	atomic_set(&the_lnet.ln_pb_update_ready, 1);
+
+	if ((the_lnet.ln_state == LNET_STATE_RUNNING) &&
+	    !lnet_ping_target_setup(&pbuf, &ping_mdh,
 				    LNET_PING_INFO_HDR_SIZE +
 				    lnet_get_ni_bytes(),
 				    false))
 		lnet_ping_target_update(pbuf, ping_mdh);
 
+
 	mutex_unlock(&the_lnet.ln_api_mutex);
+}
+
+
+void lnet_queue_ping_buffer_update(void)
+{
+	/* don't queue pb update if it is not needed */
+	if (atomic_dec_if_positive(&the_lnet.ln_update_ping_buf) < 0)
+		return;
+
+	/* don't queue pb update if already queued and not processed */
+	if (atomic_dec_if_positive(&the_lnet.ln_pb_update_ready) < 0)
+		return;
+
+	INIT_WORK(&the_lnet.ln_pb_update_work, lnet_update_ping_buffer);
+	queue_work(the_lnet.ln_pb_update_wq, &the_lnet.ln_pb_update_work);
 }
 
 void lnet_incr_dlc_seq(void)

lnet/lnet/lib-move.c
@@ -4065,7 +4065,8 @@ lnet_monitor_thread(void *arg)
 		nlpnis = lnet_recover_peer_nis(peer_nids, LNET_MAX_NNIDS);
 		lnet_health_update_console(local_nids, nnis, peer_nids, nlpnis,
 					   now);
-		lnet_update_ping_buffer();
+
+		lnet_queue_ping_buffer_update();
 
 		/*
 		 * TODO do we need to check if we should sleep without
@@ -4302,6 +4303,16 @@ int lnet_monitor_thr_start(void)
 	if (rc)
 		goto clean_queues;
 
+	the_lnet.ln_pb_update_wq = alloc_workqueue("lnetpb_wq",
+						   WQ_UNBOUND,
+						   1);
+	if (!the_lnet.ln_pb_update_wq) {
+		rc = -ENOMEM;
+		CERROR("Failed to allocate LNet ping buffer workqueue\n");
+		goto clean_queues;
+	}
+	atomic_set(&the_lnet.ln_pb_update_ready, 1);
+
 	sema_init(&the_lnet.ln_mt_signal, 0);
 
 	lnet_net_lock(LNET_LOCK_EX);
@@ -4346,6 +4357,18 @@ void lnet_monitor_thr_stop(void)
 		return;
 
 	LASSERT(the_lnet.ln_mt_state == LNET_MT_STATE_RUNNING);
+
+	/* clean up the ping buffer update workqueue before telling
+	 * the monitor thread to shut down to avoid getting stuck
+	 * on pending messages
+	 */
+	mutex_unlock(&the_lnet.ln_api_mutex);
+	flush_workqueue(the_lnet.ln_pb_update_wq);
+	destroy_workqueue(the_lnet.ln_pb_update_wq);
+	atomic_set(&the_lnet.ln_pb_update_ready, 0);
+	the_lnet.ln_pb_update_wq = NULL;
+	mutex_lock(&the_lnet.ln_api_mutex);
+
 	lnet_net_lock(LNET_LOCK_EX);
 	the_lnet.ln_mt_state = LNET_MT_STATE_STOPPING;
 	lnet_net_unlock(LNET_LOCK_EX);

lnet/lnet/peer.c
@@ -935,6 +935,9 @@ lnet_push_update_to_peers(int force)
 	int lncpt;
 	int cpt;
 
+	if (the_lnet.ln_dc_state != LNET_DC_STATE_RUNNING)
+		return;
+
 	lnet_net_lock(LNET_LOCK_EX);
 	if (lnet_peer_discovery_disabled)
 		force = 0;
@@ -3574,6 +3577,7 @@ __must_hold(&lp->lp_lock)
 	 */
 	mutex_lock(&the_lnet.ln_api_mutex);
 	if (the_lnet.ln_state != LNET_STATE_RUNNING) {
+		lnet_ping_buffer_decref(pbuf);
 		rc = -ESHUTDOWN;
 		goto out;
 	}

lustre/tests/sanity-lnet.sh
@@ -3742,6 +3742,35 @@ test_402() {
 }
 run_test 402 "Destination net rule should not panic"
 
+test_500() {
+	reinit_dlc || return $?
+
+	cleanup_netns || error "Failed to cleanup netns before test execution"
+	setup_fakeif || error "Failed to add fake IF"
+	have_interface "$FAKE_IF" ||
+		error "Expect $FAKE_IF configured but not found"
+
+	add_net "tcp" "${INTERFACES[0]}"
+	add_net "tcp" "${FAKE_IF}"
+
+	do_lnetctl discover $($LCTL list_nids | head -n 1) ||
+		error "Failed to discover self"
+
+	$LCTL net_delay_add -s *@tcp -d *@tcp -r 1 -l 1 -m PUT ||
+		error "Failed to add delay rule"
+
+	$LCTL net_drop_add -s *@tcp -d $($LCTL list_nids | head -n 1) -m PUT -e local_timeout -r 1
+	$LCTL net_drop_add -s *@tcp -d $($LCTL list_nids | tail -n 1) -m PUT -e local_timeout -r 1
+
+	ip link set $FAKE_IF down ||
+		error "Failed to set link down"
+	ip link set $FAKE_IF up ||
+		error "Failed to set link up"
+
+	unload_modules
+}
+run_test 500 "Check deadlock on ping target update"
+
 complete_test $SECONDS
 cleanup_testsuite
 exit_status