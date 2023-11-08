lustre/lustre-release

commit da893c6c9707ca3b2e7532d05f754fccf1cffc74

LU-16191 socklnd: limit retries on conns_per_peer mismatch
If connection initiator has a higher conns-per-peer setting than
its peer, don't try to create extra connections forever as the
peer will keep rejecting them. A few retries should suffice to
resolve a valid race.

Test-Parameters: trivial
Fixes: 71b2476e ("LU-12815 socklnd: add conns_per_peer parameter")
Signed-off-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Change-Id: I7d04d4ac41e98a738b6c85c3d323608038f5c51e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48664
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/socklnd/socklnd.c
@@ -144,6 +144,7 @@ ksocknal_create_conn_cb(struct sockaddr *addr)
 	conn_cb->ksnr_blki_conn_count = 0;
 	conn_cb->ksnr_blko_conn_count = 0;
 	conn_cb->ksnr_max_conns = 0;
+	conn_cb->ksnr_busy_retry_count = 0;
 
 	return conn_cb;
 }

lnet/klnds/socklnd/socklnd.h
@@ -365,6 +365,7 @@ struct ksock_conn {
 };
 
 #define SOCKNAL_CONN_COUNT_MAX_BITS	8	/* max conn count bits */
+#define SOCKNAL_MAX_BUSY_RETRIES	3
 
 struct ksock_conn_cb {
 	struct list_head	ksnr_connd_list;/* chain on ksnr_connd_routes */
@@ -387,6 +388,9 @@ struct ksock_conn_cb {
 	unsigned int		ksnr_max_conns; /* conns_per_peer at peer
 						 * creation
 						 */
+	unsigned int		ksnr_busy_retry_count;/* counts retry attempts
+						       * due to EALREADY rc
+						       */
 };
 
 #define SOCKNAL_KEEPALIVE_PING          1       /* cookie for keepalive ping */

lnet/klnds/socklnd/socklnd_cb.c
@@ -1907,7 +1907,7 @@ ksocknal_connect(struct ksock_conn_cb *conn_cb)
 {
 	LIST_HEAD(zombies);
 	struct ksock_peer_ni *peer_ni = conn_cb->ksnr_peer;
-	int type;
+	int type = SOCKLND_CONN_NONE;
 	int wanted;
 	struct socket *sock;
 	time64_t deadline;
@@ -1987,19 +1987,34 @@ ksocknal_connect(struct ksock_conn_cb *conn_cb)
 			goto failed;
 		}
 
+		if (rc == EALREADY && conn_cb->ksnr_conn_count > 0)
+			conn_cb->ksnr_busy_retry_count += 1;
+		else
+			conn_cb->ksnr_busy_retry_count = 0;
+
 		/* A +ve RC means I have to retry because I lost the connection
 		 * race or I have to renegotiate protocol version
 		 */
 		retry_later = (rc != 0);
+
 		if (retry_later)
-			CDEBUG(D_NET, "peer_ni %s: conn race, retry later.\n",
-			       libcfs_nidstr(&peer_ni->ksnp_id.nid));
+			CDEBUG(D_NET, "peer_ni %s: conn race, retry later. rc %d\n",
+			       libcfs_nidstr(&peer_ni->ksnp_id.nid), rc);
 
 		write_lock_bh(&ksocknal_data.ksnd_global_lock);
 	}
 
 	conn_cb->ksnr_scheduled = 0;
 	conn_cb->ksnr_connecting = 0;
+
+	if (conn_cb->ksnr_busy_retry_count >= SOCKNAL_MAX_BUSY_RETRIES &&
+	    type > SOCKLND_CONN_NONE) {
+		/* After so many retries due to EALREADY assume that
+		 * the peer doesn't support as many connections as we want
+		 */
+		conn_cb->ksnr_connected |= BIT(type);
+		retry_later = false;
+	}
 
 	if (retry_later) {
 		/* re-queue for attention; this frees me up to handle

commit 5afe3b0538c533c3cca370bc9c0901abccca299a

LU-17258 socklnd: ensure connection type established upon race
When a connection race is hit between two peers, only increment the
retry count if a connection of the specific type has already been
established; otherwise, this can lead to an unexpected value set in
ksnr_connected and some of the assertions being triggered in
ksocknal_connect():

"ASSERTION( (wanted & ((((1UL))) << (3))) != 0 ) failed"

Fixes: da893c6c97 ("LU-16191 socklnd: limit retries on conns_per_peer mismatch")
HPE-bug-id: LUS-11922
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Change-Id: I6e8abb39ad3c0bcd7fbc8f8c5478c903029df908
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52957
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/klnds/socklnd/socklnd.c
@@ -380,33 +380,6 @@ ksocknal_get_peer_info(struct lnet_ni *ni, int index,
 }
 
 static unsigned int
-ksocknal_get_conn_count_by_type(struct ksock_conn_cb *conn_cb,
-				int type)
-{
-	unsigned int count = 0;
-
-	switch (type) {
-	case SOCKLND_CONN_CONTROL:
-		count = conn_cb->ksnr_ctrl_conn_count;
-		break;
-	case SOCKLND_CONN_BULK_IN:
-		count = conn_cb->ksnr_blki_conn_count;
-		break;
-	case SOCKLND_CONN_BULK_OUT:
-		count = conn_cb->ksnr_blko_conn_count;
-		break;
-	case SOCKLND_CONN_ANY:
-		count = conn_cb->ksnr_conn_count;
-		break;
-	default:
-		LBUG();
-		break;
-	}
-
-	return count;
-}
-
-static unsigned int
 ksocknal_get_conns_per_peer(struct ksock_peer_ni *peer_ni)
 {
 	struct lnet_ni *ni = peer_ni->ksnp_ni;
@@ -575,6 +548,33 @@ ksocknal_del_conn_cb_locked(struct ksock_conn_cb *conn_cb)
 		 */
 		ksocknal_unlink_peer_locked(peer_ni);
 	}
+}
+
+unsigned int
+ksocknal_get_conn_count_by_type(struct ksock_conn_cb *conn_cb,
+				int type)
+{
+	unsigned int count = 0;
+
+	switch (type) {
+	case SOCKLND_CONN_CONTROL:
+		count = conn_cb->ksnr_ctrl_conn_count;
+		break;
+	case SOCKLND_CONN_BULK_IN:
+		count = conn_cb->ksnr_blki_conn_count;
+		break;
+	case SOCKLND_CONN_BULK_OUT:
+		count = conn_cb->ksnr_blko_conn_count;
+		break;
+	case SOCKLND_CONN_ANY:
+		count = conn_cb->ksnr_conn_count;
+		break;
+	default:
+		LBUG();
+		break;
+	}
+
+	return count;
 }
 
 int

lnet/klnds/socklnd/socklnd.h
@@ -607,6 +607,8 @@ int ksocknal_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
                   unsigned int offset, unsigned int mlen, unsigned int rlen);
 int ksocknal_accept(struct lnet_ni *ni, struct socket *sock);
 
+unsigned int ksocknal_get_conn_count_by_type(struct ksock_conn_cb *conn_cb,
+					     int type);
 int ksocknal_add_peer(struct lnet_ni *ni, struct lnet_processid *id,
 		      struct sockaddr *addr);
 struct ksock_peer_ni *ksocknal_find_peer_locked(struct lnet_ni *ni,

lnet/klnds/socklnd/socklnd_cb.c
@@ -2001,7 +2001,8 @@ ksocknal_connect(struct ksock_conn_cb *conn_cb)
 			goto failed;
 		}
 
-		if (rc == EALREADY && conn_cb->ksnr_conn_count > 0)
+		if (rc == EALREADY &&
+		    ksocknal_get_conn_count_by_type(conn_cb, type) > 0)
 			conn_cb->ksnr_busy_retry_count += 1;
 		else
 			conn_cb->ksnr_busy_retry_count = 0;