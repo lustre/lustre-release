lustre/lustre-release

commit ac8c28f959d87c1ae852d9daccce3c2a3f7a6f39

LU-8130 osc: convert osc_quota hash to xarray
The cl_quota_hash originally had 3 hashes, one for each type of quota
(USR, GRP, PRJ) that just stored on the client whether a particular
quota ID was over its limit. This was overkill since cl_quota_hash
only needs one bit to check if a particular ID has exceeded quota
with IO from this client, and there will usually be only a few IDs
that are actually exceeding their limit where a client is involved.
Instead, use the quota ID as the index into an Xarray, and store
a value with the quota TYPE(s) that are over the limit for that ID.
We only need to test the presence/absence of an ID and a quota type
without the need to store any additional values (the clients do not
track the actual quota usage or limits).
To test if a quota is exceeded for particular ID is a two-step
process. First check if there is any entry for the particular ID,
and if it exists then check which quota type (USR, GRP, PRJ) is
over the limit for that ID value.  The same is done when setting
a particular quota ID/TYPE is over its limit - first lookup the
ID and then add the TYPE flag to the value if not already set.
The Xarray implementation does offer using "marks" (up to 3 bits
per index) but in this case there is no other value that needs to
be stored into the Xarray other than one bit for any exceeded type,
so they are not used here.

Change-Id: I9355ed2a7158f0d5cc0d600ad51ea1a1434f3e98
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/32038
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osc/osc_quota.c
@@ -28,39 +28,26 @@
  * Code originally extracted from quota directory
  */
 
-#include <obd.h>
+#include <obd_class.h>
 #include <lustre_osc.h>
 
 #include "osc_internal.h"
 
-static inline struct osc_quota_info *osc_oqi_alloc(u32 id)
-{
-	struct osc_quota_info *oqi;
-
-	OBD_SLAB_ALLOC_PTR(oqi, osc_quota_kmem);
-	if (oqi != NULL)
-		oqi->oqi_id = id;
-
-	return oqi;
-}
-
 int osc_quota_chkdq(struct client_obd *cli, const unsigned int qid[])
 {
 	int type;
+
 	ENTRY;
-
 	for (type = 0; type < LL_MAXQUOTAS; type++) {
-		struct osc_quota_info *oqi;
+		u8 *qtype;
 
-		oqi = cfs_hash_lookup(cli->cl_quota_hash[type], &qid[type]);
-		if (oqi) {
-			/* do not try to access oqi here, it could have been
-			 * freed by osc_quota_setdq() */
-
+		qtype = xa_load(&cli->cl_quota_exceeded_ids, qid[type]);
+		if (qtype && (xa_to_value(qtype) & BIT(type))) {
 			/* the slot is busy, the user is about to run out of
-			 * quota space on this OST */
+			 * quota space on this OST
+			 */
 			CDEBUG(D_QUOTA, "chkdq found noquota for %s %d\n",
-			       type == USRQUOTA ? "user" : "grout", qid[type]);
+			       qtype_name(type), qid[type]);
 			RETURN(-EDQUOT);
 		}
 	}
@@ -96,14 +83,13 @@ static inline u32 fl_quota_flag(int qtype)
 	}
 }
 
-int osc_quota_setdq(struct client_obd *cli, __u64 xid, const unsigned int qid[],
+int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 		    u64 valid, u32 flags)
 {
 	int type;
 	int rc = 0;
 
         ENTRY;
-
 	if ((valid & (OBD_MD_FLALLQUOTA)) == 0)
 		RETURN(0);
 
@@ -121,48 +107,57 @@ int osc_quota_setdq(struct client_obd *cli, __u64 xid, const unsigned int qid[],
 		cli->cl_quota_last_xid = xid;
 
 	for (type = 0; type < LL_MAXQUOTAS; type++) {
-		struct osc_quota_info *oqi;
+		unsigned long bits = 0;
+		u8 *qtypes;
 
 		if ((valid & md_quota_flag(type)) == 0)
 			continue;
 
-		/* lookup the ID in the per-type hash table */
-		oqi = cfs_hash_lookup(cli->cl_quota_hash[type], &qid[type]);
+		/* lookup the quota IDs in the ID xarray */
+		qtypes = xa_load(&cli->cl_quota_exceeded_ids, qid[type]);
 		if ((flags & fl_quota_flag(type)) != 0) {
 			/* This ID is getting close to its quota limit, let's
-			 * switch to sync I/O */
-			if (oqi != NULL)
-				continue;
+			 * switch to sync I/O
+			 */
+			if (qtypes) {
+				bits = xa_to_value(qtypes);
+				/* test if already set */
+				if (bits & BIT(type))
+					continue;
+			}
 
-			oqi = osc_oqi_alloc(qid[type]);
-			if (oqi == NULL) {
-				rc = -ENOMEM;
+			bits |= BIT(type);
+			rc = xa_insert(&cli->cl_quota_exceeded_ids, qid[type],
+				       xa_mk_value(bits), GFP_KERNEL);
+			if (rc)
 				break;
-			}
 
-			rc = cfs_hash_add_unique(cli->cl_quota_hash[type],
-						 &qid[type], &oqi->oqi_hash);
-			/* race with others? */
-			if (rc == -EALREADY) {
-				rc = 0;
-				OBD_SLAB_FREE_PTR(oqi, osc_quota_kmem);
-			}
-
-			CDEBUG(D_QUOTA, "%s: setdq to insert for %s %d (%d)\n",
+			CDEBUG(D_QUOTA, "%s: setdq to insert for %s %d: rc = %d\n",
 			       cli_name(cli), qtype_name(type), qid[type], rc);
 		} else {
 			/* This ID is now off the hook, let's remove it from
-			 * the hash table */
-			if (oqi == NULL)
+			 * the xarray
+			 */
+			if (!qtypes)
 				continue;
 
-			oqi = cfs_hash_del_key(cli->cl_quota_hash[type],
-					       &qid[type]);
-			if (oqi)
-				OBD_SLAB_FREE_PTR(oqi, osc_quota_kmem);
+			bits = xa_to_value(qtypes);
+			if (!(bits & BIT(type)))
+				continue;
 
-			CDEBUG(D_QUOTA, "%s: setdq to remove for %s %d (%p)\n",
-			       cli_name(cli), qtype_name(type), qid[type], oqi);
+			bits &= ~BIT(type);
+			if (bits) {
+				if (xa_cmpxchg(&cli->cl_quota_exceeded_ids,
+					       qid[type], qtypes,
+					       xa_mk_value(bits),
+					       GFP_KERNEL) != qtypes)
+					GOTO(out_unlock, rc = -ENOENT);
+			} else {
+				xa_erase(&cli->cl_quota_exceeded_ids, qid[type]);
+			}
+
+			CDEBUG(D_QUOTA, "%s: setdq to remove for %s %d\n",
+			       cli_name(cli), qtype_name(type), qid[type]);
 		}
 	}
 
@@ -171,117 +166,22 @@ out_unlock:
 	RETURN(rc);
 }
 
-/*
- * Hash operations for uid/gid <-> osc_quota_info
- */
-static unsigned
-oqi_hashfn(struct cfs_hash *hs, const void *key, unsigned mask)
-{
-	return cfs_hash_32(*((__u32 *)key), 0) & mask;
-}
-
-static int
-oqi_keycmp(const void *key, struct hlist_node *hnode)
-{
-	struct osc_quota_info *oqi;
-	u32 uid;
-
-	LASSERT(key != NULL);
-	uid = *((u32 *)key);
-	oqi = hlist_entry(hnode, struct osc_quota_info, oqi_hash);
-
-	return uid == oqi->oqi_id;
-}
-
-static void *
-oqi_key(struct hlist_node *hnode)
-{
-	struct osc_quota_info *oqi;
-	oqi = hlist_entry(hnode, struct osc_quota_info, oqi_hash);
-	return &oqi->oqi_id;
-}
-
-static void *
-oqi_object(struct hlist_node *hnode)
-{
-	return hlist_entry(hnode, struct osc_quota_info, oqi_hash);
-}
-
-static void
-oqi_get(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-}
-
-static void
-oqi_put_locked(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-}
-
-static void
-oqi_exit(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct osc_quota_info *oqi;
-
-	oqi = hlist_entry(hnode, struct osc_quota_info, oqi_hash);
-
-        OBD_SLAB_FREE_PTR(oqi, osc_quota_kmem);
-}
-
-#define HASH_QUOTA_BKT_BITS 5
-#define HASH_QUOTA_CUR_BITS 5
-#define HASH_QUOTA_MAX_BITS 15
-
-static struct cfs_hash_ops quota_hash_ops = {
-	.hs_hash	= oqi_hashfn,
-	.hs_keycmp	= oqi_keycmp,
-	.hs_key		= oqi_key,
-	.hs_object	= oqi_object,
-	.hs_get		= oqi_get,
-	.hs_put_locked	= oqi_put_locked,
-	.hs_exit	= oqi_exit,
-};
-
 int osc_quota_setup(struct obd_device *obd)
 {
 	struct client_obd *cli = &obd->u.cli;
-	int i, type;
-	ENTRY;
 
 	mutex_init(&cli->cl_quota_mutex);
 
-	for (type = 0; type < LL_MAXQUOTAS; type++) {
-		cli->cl_quota_hash[type] = cfs_hash_create("QUOTA_HASH",
-							   HASH_QUOTA_CUR_BITS,
-							   HASH_QUOTA_MAX_BITS,
-							   HASH_QUOTA_BKT_BITS,
-							   0,
-							   CFS_HASH_MIN_THETA,
-							   CFS_HASH_MAX_THETA,
-							   &quota_hash_ops,
-							   CFS_HASH_DEFAULT);
-		if (cli->cl_quota_hash[type] == NULL)
-			break;
-	}
+	xa_init(&cli->cl_quota_exceeded_ids);
 
-	if (type == LL_MAXQUOTAS)
-		RETURN(0);
-
-	for (i = 0; i < type; i++)
-		cfs_hash_putref(cli->cl_quota_hash[i]);
-
-	RETURN(-ENOMEM);
+	return 0;
 }
 
-int osc_quota_cleanup(struct obd_device *obd)
+void osc_quota_cleanup(struct obd_device *obd)
 {
-	struct client_obd     *cli = &obd->u.cli;
-	int type;
-	ENTRY;
+	struct client_obd *cli = &obd->u.cli;
 
-	for (type = 0; type < LL_MAXQUOTAS; type++)
-		cfs_hash_putref(cli->cl_quota_hash[type]);
-
-	RETURN(0);
+	xa_destroy(&cli->cl_quota_exceeded_ids);
 }
 
 int osc_quotactl(struct obd_device *unused, struct obd_export *exp,

libcfs/include/libcfs/linux/xarray.h
@@ -1761,6 +1761,8 @@ static inline void *xas_next(struct xa_state *xas)
 	xas->xa_offset++;
 	return xa_entry(xas->xa, node, xas->xa_offset);
 }
+#else
+#include <linux/xarray.h>
 #endif /* !HAVE_XARRAY_SUPPORT */
 
 #endif /* _LINUX_XARRAY_LUSTRE_H */

lustre/include/lustre_osc.h
@@ -54,12 +54,6 @@
  *  @{
  */
 
-struct osc_quota_info {
-	/** linkage for quota hash table */
-	struct hlist_node oqi_hash;
-	__u32             oqi_id;
-};
-
 enum oap_async_flags {
 	ASYNC_READY	   = 0x1, /* ap_make_ready will not be called before
 				   * this page is added to an rpc */
@@ -556,7 +550,6 @@ extern struct kmem_cache *osc_object_kmem;
 extern struct kmem_cache *osc_thread_kmem;
 extern struct kmem_cache *osc_session_kmem;
 extern struct kmem_cache *osc_extent_kmem;
-extern struct kmem_cache *osc_quota_kmem;
 extern struct kmem_cache *osc_obdo_kmem;
 
 extern struct lu_context_key osc_key;

lustre/include/obd.h
@@ -37,7 +37,7 @@
 #include <linux/kobject.h>
 #include <linux/spinlock.h>
 #include <linux/sysfs.h>
-#include <linux/xarray.h>
+#include <libcfs/linux/xarray.h>
 
 #include <uapi/linux/lustre/lustre_idl.h>
 #include <lustre_lib.h>
@@ -363,8 +363,8 @@ struct client_obd {
 	void			*cl_writeback_work;
 	void			*cl_lru_work;
 	struct mutex		  cl_quota_mutex;
-	/* hash tables for osc_quota_info */
-	struct cfs_hash		*cl_quota_hash[LL_MAXQUOTAS];
+	/* quota IDs/types that have exceeded quota */
+	struct xarray		 cl_quota_exceeded_ids;
 	/* the xid of the request updating the hash tables */
 	__u64			 cl_quota_last_xid;
 	/* Links to the global list of registered changelog devices */

lustre/osc/osc_dev.c
@@ -54,7 +54,6 @@ EXPORT_SYMBOL(osc_object_kmem);
 struct kmem_cache *osc_thread_kmem;
 struct kmem_cache *osc_session_kmem;
 struct kmem_cache *osc_extent_kmem;
-struct kmem_cache *osc_quota_kmem;
 struct kmem_cache *osc_obdo_kmem;
 
 struct lu_kmem_descr osc_caches[] = {
@@ -82,11 +81,6 @@ struct lu_kmem_descr osc_caches[] = {
 		.ckd_cache = &osc_extent_kmem,
 		.ckd_name  = "osc_extent_kmem",
 		.ckd_size  = sizeof (struct osc_extent)
-	},
-	{
-		.ckd_cache = &osc_quota_kmem,
-		.ckd_name  = "osc_quota_kmem",
-		.ckd_size  = sizeof(struct osc_quota_info)
 	},
 	{
 		.ckd_cache = &osc_obdo_kmem,

lustre/osc/osc_internal.h
@@ -151,7 +151,7 @@ struct osc_async_args {
 };
 
 int osc_quota_setup(struct obd_device *obd);
-int osc_quota_cleanup(struct obd_device *obd);
+void osc_quota_cleanup(struct obd_device *obd);
 int osc_quota_setdq(struct client_obd *cli, __u64 xid, const unsigned int qid[],
 		    u64 valid, u32 flags);
 int osc_quota_chkdq(struct client_obd *cli, const unsigned int qid[]);

commit 67e0d9e40acc6adcebf89e2a4ac3860f0c4273d2

LU-17191 osc: only call xa_insert for new entries
The handling of updating the Xarray entry is incorrect. If the
Xarray already exist and we call xa_insert() it will return -EBUSY
and not continue examining the entry. The correct approach is
if xa_load() returns a value never call xa_insert(). With the
return value of xa_load() we test if type bit was set and if not
set it.

We add ll_xa_insert to spelling.txt since the return value changed
with kernel versions. This will avoid mistakes in the future for
cases we look at the return value of xa_insert().

Test-Parameters: testlist=sanity-quota
Test-parameters: clientdistro=ubuntu2204 testlist=sanity-quota
Fixes: ac8c28f959 ("LU-8130 osc: convert osc_quota hash to xarray")
Change-Id: Icd88f5e47fb24f669bf362c1daec500b743b447b
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52713
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osc/osc_quota.c
@@ -119,17 +119,29 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 			/* This ID is getting close to its quota limit, let's
 			 * switch to sync I/O
 			 */
-			if (qtypes) {
+			if (qtypes) { /* ID already cached */
 				bits = xa_to_value(qtypes);
-				/* test if already set */
-				if (bits & BIT(type))
-					continue;
+				/* test if ID type already set */
+				if (!(bits & BIT(type))) {
+					bits |= BIT(type);
+					rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
+							     qid[type],
+							     xa_mk_value(bits),
+							     GFP_KERNEL));
+					if (rc < 0)
+						GOTO(out_unlock, rc);
+				}
+				continue;
 			}
 
+			/* Only insert if we see the this ID for the
+			 * very first time.
+			 */
 			bits |= BIT(type);
-			rc = xa_insert(&cli->cl_quota_exceeded_ids, qid[type],
-				       xa_mk_value(bits), GFP_KERNEL);
-			if (rc)
+			rc = ll_xa_insert(&cli->cl_quota_exceeded_ids,
+					  qid[type], xa_mk_value(bits),
+					  GFP_KERNEL);
+			if (rc == -ENOMEM)
 				break;
 
 			CDEBUG(D_QUOTA, "%s: setdq to insert for %s %d: rc = %d\n",
@@ -147,11 +159,12 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 
 			bits &= ~BIT(type);
 			if (bits) {
-				if (xa_cmpxchg(&cli->cl_quota_exceeded_ids,
-					       qid[type], qtypes,
-					       xa_mk_value(bits),
-					       GFP_KERNEL) != qtypes)
-					GOTO(out_unlock, rc = -ENOENT);
+				rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
+						     qid[type],
+						     xa_mk_value(bits),
+						     GFP_KERNEL));
+				if (rc < 0)
+					GOTO(out_unlock, rc);
 			} else {
 				xa_erase(&cli->cl_quota_exceeded_ids, qid[type]);
 			}