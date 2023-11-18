lustre/lustre-release

commit f0be00678cc8005ec122fa1982d3aa432f6e3cbd

LU-9680 lnet: collect data about peer_ni by using Netlink
Migrate the LNet peer NI API to use the Netlink API for
the case of collecting data on peer. This change also
allows large NID support for IPv6. Since this doesn't
cover creation and deletion of peers we can't setup
large NID peers just yet.

Test-Parameters: trivial testlist=sanity-lnet
Change-Id: Iefa3f566255e768047b0f9ff21d64bc74634f284
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49516
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/utils/lnetctl.c
@@ -2562,6 +2562,266 @@ static int jt_set_ni_value(int argc, char **argv)
 	return rc;
 }
 
+static int yaml_lnet_peer_display(yaml_parser_t *reply, bool list_only)
+{
+	yaml_emitter_t debug;
+	int rc;
+
+	rc = yaml_emitter_initialize(&debug);
+	if (rc == 0)
+		goto out_err;
+
+	yaml_emitter_set_indent(&debug, 6);
+	yaml_emitter_set_output_file(&debug, stdout);
+
+	if (list_only) {
+		bool done = false;
+
+		while (!done) {
+			yaml_event_t event;
+			char *value;
+
+			rc = yaml_parser_parse(reply, &event);
+			if (rc == 0)
+				goto report_reply_error;
+
+			if (event.type != YAML_SCALAR_EVENT)
+				goto merge_event;
+
+			value = (char *)event.data.scalar.value;
+			if (strcmp(value, "peer") == 0) {
+				yaml_event_delete(&event);
+
+				yaml_scalar_event_initialize(&event, NULL,
+							     (yaml_char_t *)YAML_STR_TAG,
+							     (yaml_char_t *)"peer list",
+							     strlen("peer list"),
+							     1, 0,
+							     YAML_PLAIN_SCALAR_STYLE);
+			} else if (strcmp(value, "primary nid") == 0) {
+				yaml_event_delete(&event);
+
+				yaml_scalar_event_initialize(&event, NULL,
+							     (yaml_char_t *)YAML_STR_TAG,
+							     (yaml_char_t *)"nid",
+							     strlen("nid"),
+							     1, 0,
+							     YAML_PLAIN_SCALAR_STYLE);
+				rc = yaml_emitter_emit(&debug, &event);
+				if (rc == 0)
+					break;
+
+				/* Now print NID address */
+				rc = yaml_parser_parse(reply, &event);
+				if (rc == 0)
+					goto report_reply_error;
+
+				rc = yaml_emitter_emit(&debug, &event);
+				if (rc == 0)
+					break;
+
+				/* skip reset */
+				while (event.type != YAML_MAPPING_END_EVENT) {
+					rc = yaml_parser_parse(reply, &event);
+					if (rc == 0)
+						goto report_reply_error;
+				}
+
+				/* we can have map end, seq end, map end or
+				 * just map end event. If we see seq end event
+				 * then skip to next mapping end event
+				 */
+				rc = yaml_parser_parse(reply, &event);
+				if (rc == 0)
+					goto report_reply_error;
+
+				if (event.type == YAML_SEQUENCE_END_EVENT) {
+					yaml_event_delete(&event);
+
+					rc = yaml_parser_parse(reply, &event);
+					if (rc == 0)
+						goto report_reply_error;
+				}
+			}
+merge_event:
+			rc = yaml_emitter_emit(&debug, &event);
+			if (rc == 0)
+				break;
+
+			done = (event.type == YAML_DOCUMENT_END_EVENT);
+		}
+	} else {
+		yaml_document_t errmsg;
+
+		rc = yaml_parser_load(reply, &errmsg);
+		if (rc == 1)
+			rc = yaml_emitter_dump(&debug, &errmsg);
+		yaml_document_delete(&errmsg);
+	}
+out_err:
+	if (rc == 0)
+		yaml_emitter_log_error(&debug, stderr);
+report_reply_error:
+	yaml_emitter_delete(&debug);
+
+	return rc;
+}
+
+static int yaml_lnet_peer(char *prim_nid, char *nidstr, bool disable_mr,
+			  int health_value, int state, bool list_only,
+			  int version, int flags)
+{
+	struct nl_sock *sk = NULL;
+	const char *msg = NULL;
+	yaml_emitter_t output;
+	yaml_parser_t reply;
+	yaml_event_t event;
+	int rc;
+
+	/* Create Netlink emitter to send request to kernel */
+	sk = nl_socket_alloc();
+	if (!sk)
+		return -EOPNOTSUPP;
+
+	/* Setup parser to receive Netlink packets */
+	rc = yaml_parser_initialize(&reply);
+	if (rc == 0) {
+		nl_socket_free(sk);
+		return -EOPNOTSUPP;
+	}
+
+	rc = yaml_parser_set_input_netlink(&reply, sk, false);
+	if (rc == 0) {
+		msg = yaml_parser_get_reader_error(&reply);
+		goto free_reply;
+	}
+
+	/* Create Netlink emitter to send request to kernel */
+	rc = yaml_emitter_initialize(&output);
+	if (rc == 0) {
+		msg = "failed to initialize emitter";
+		goto free_reply;
+	}
+
+	rc = yaml_emitter_set_output_netlink(&output, sk, LNET_GENL_NAME,
+					     version, LNET_CMD_PEERS, flags);
+	if (rc == 0)
+		goto emitter_error;
+
+	yaml_emitter_open(&output);
+	yaml_document_start_event_initialize(&event, NULL, NULL, NULL, 0);
+	rc = yaml_emitter_emit(&output, &event);
+	if (rc == 0)
+		goto emitter_error;
+
+	yaml_mapping_start_event_initialize(&event, NULL,
+					    (yaml_char_t *)YAML_MAP_TAG,
+					    1, YAML_ANY_MAPPING_STYLE);
+	rc = yaml_emitter_emit(&output, &event);
+	if (rc == 0)
+		goto emitter_error;
+
+	yaml_scalar_event_initialize(&event, NULL,
+				     (yaml_char_t *)YAML_STR_TAG,
+				     (yaml_char_t *)"peer",
+				     strlen("peer"), 1, 0,
+				     YAML_PLAIN_SCALAR_STYLE);
+	rc = yaml_emitter_emit(&output, &event);
+	if (rc == 0)
+		goto emitter_error;
+
+	if (prim_nid) {
+		yaml_sequence_start_event_initialize(&event, NULL,
+						     (yaml_char_t *)YAML_SEQ_TAG,
+						     1,
+						     YAML_BLOCK_SEQUENCE_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+
+		yaml_mapping_start_event_initialize(&event, NULL,
+						    (yaml_char_t *)YAML_MAP_TAG,
+						    1,
+						    YAML_BLOCK_MAPPING_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)"primary nid",
+					     strlen("primary nid"), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)prim_nid,
+					     strlen(prim_nid), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+
+		yaml_mapping_end_event_initialize(&event);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+
+		yaml_sequence_end_event_initialize(&event);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+	} else {
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)"",
+					     strlen(""), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+	}
+
+	yaml_mapping_end_event_initialize(&event);
+	rc = yaml_emitter_emit(&output, &event);
+	if (rc == 0)
+		goto emitter_error;
+
+	yaml_document_end_event_initialize(&event, 0);
+	rc = yaml_emitter_emit(&output, &event);
+	if (rc == 0)
+		goto emitter_error;
+
+	rc = yaml_emitter_close(&output);
+emitter_error:
+	if (rc == 0) {
+		yaml_emitter_log_error(&output, stderr);
+		rc = -EINVAL;
+	} else {
+		rc = yaml_lnet_peer_display(&reply, list_only);
+		if (rc == 0) {
+			msg = yaml_parser_get_reader_error(&reply);
+			/* If we didn't find any peers just be silent */
+			if (msg && strcmp(msg, "No peers found") == 0)
+				rc = 1;
+		}
+
+	}
+	yaml_emitter_delete(&output);
+free_reply:
+	if (rc == 0) {
+		yaml_lnet_print_error(flags, "peer", msg);
+		rc = -EINVAL;
+	}
+	yaml_parser_delete(&reply);
+	nl_socket_free(sk);
+
+	return rc == 1 ? 0 : rc;
+}
+
 static int jt_set_peer_ni_value(int argc, char **argv)
 {
 	return set_value_helper(argc, argv, lustre_lnet_config_peer_ni_healthv);
@@ -3356,13 +3616,13 @@ static int jt_show_peer(int argc, char **argv)
 	int rc, opt;
 	struct cYAML *err_rc = NULL, *show_rc = NULL;
 	long int detail = 0;
-
 	const char *const short_opts = "hn:v::";
 	const struct option long_opts[] = {
-	{ .name = "help",	.has_arg = no_argument,		.val = 'h' },
-	{ .name = "nid",	.has_arg = required_argument,	.val = 'n' },
-	{ .name = "verbose",	.has_arg = optional_argument,	.val = 'v' },
-	{ .name = NULL } };
+		{ .name = "help",	.has_arg = no_argument,		.val = 'h' },
+		{ .name = "nid",	.has_arg = required_argument,	.val = 'n' },
+		{ .name = "verbose",	.has_arg = optional_argument,	.val = 'v' },
+		{ .name = NULL }
+	};
 
 	rc = check_cmd(peer_cmds, "peer", "show", 1, argc, argv);
 	if (rc)
@@ -3390,6 +3650,14 @@ static int jt_show_peer(int argc, char **argv)
 		}
 	}
 
+	rc = yaml_lnet_peer(nid, NULL, false, -1, -1, false, detail,
+			    NLM_F_DUMP);
+	if (rc <= 0) {
+		if (rc == -EOPNOTSUPP)
+			goto old_api;
+		return rc;
+	}
+old_api:
 	rc = lustre_lnet_show_peer(nid, (int) detail, -1, &show_rc, &err_rc,
 				   false);
 
@@ -3413,8 +3681,13 @@ static int jt_list_peer(int argc, char **argv)
 	if (rc)
 		return rc;
 
-	rc = lustre_lnet_list_peer(-1, &list_rc, &err_rc);
-
+	rc = yaml_lnet_peer(NULL, NULL, false, -1, -1, true, 0, NLM_F_DUMP);
+	if (rc <= 0) {
+		if (rc == -EOPNOTSUPP)
+			goto old_api;
+		return rc;
+	}
+old_api:
 	if (rc != LUSTRE_CFG_RC_NO_ERR)
 		cYAML_print_tree2file(stderr, err_rc);
 	else if (list_rc)

lnet/include/lnet/lib-types.h
@@ -721,9 +721,219 @@ enum lnet_route_attrs {
 
 #define LNET_ROUTE_ATTR_MAX (__LNET_ROUTE_ATTR_MAX_PLUS_ONE - 1)
 
+/** enum lnet_peer_ni_attrs	      - LNet peer NI netlink properties
+ *					attributes that describe LNet peer 'NI'.
+ *					These values are used to piece together
+ *					messages for sending and receiving.
+ *
+ * @LNET_PEER_NI_ATTR_UNSPEC:		unspecified attribute to catch errors
+ *
+ * @LNET_PEER_NI_ATTR_HDR:		grouping for LNet peer data
+ *					(NLA_NUL_STRING)
+ * @LNET_PEER_NI_ATTR_PRIMARY_NID:	primary NID of this peer (NLA_STRING)
+ * @LNET_PEER_NI_ATTR_MULTIRAIL:	Do we support MultiRail ? (NLA_FLAG)
+ * @LNET_PEER_NI_ATTR_STATE:		Bitfields of the peer state (NLA_U32)
+ * @LNET_PEER_NI_ATTR_PEER_NI_LIST:	List of remote peers we can reach
+ *					(NLA_NESTED)
+ */
+enum lnet_peer_ni_attrs {
+	LNET_PEER_NI_ATTR_UNSPEC = 0,
+
+	LNET_PEER_NI_ATTR_HDR,
+	LNET_PEER_NI_ATTR_PRIMARY_NID,
+	LNET_PEER_NI_ATTR_MULTIRAIL,
+	LNET_PEER_NI_ATTR_STATE,
+	LNET_PEER_NI_ATTR_PEER_NI_LIST,
+	__LNET_PEER_NI_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_PEER_NI_ATTR_MAX (__LNET_PEER_NI_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_peer_ni_list_attrs	      - LNet remote peer netlink
+ *						properties attributes that
+ *						describe remote LNet peer 'NI'.
+ *						These values are used to piece
+ *						together messages for sending
+ *						and receiving.
+ *
+ * @LNET_PEER_NI_LIST_ATTR_UNSPEC:		unspecified attribute to catch
+ *						errors
+ *
+ * @LNET_PEER_NI_LIST_ATTR_NID:			remote peer's NID (NLA_STRING)
+ * @LNET_PEER_NI_LIST_ATTR_UDSP_INFO:		remote peer's UDSP info
+ *						(NLA_NESTED)
+ * @LNET_PEER_NI_LIST_ATTR_STATE:		state of remote peer
+ *						(NLA_STRING)
+ *
+ * @LNET_PEER_NI_LIST_ATTR_MAX_TX_CREDITS:	Maximum TX credits for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_CUR_TX_CREDITS:	Current TX credits for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_MIN_TX_CREDITS:	Minimum TX credits for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_QUEUE_BUF_COUNT:	Size of TX queue buffer
+ *						(NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_CUR_RTR_CREDITS:	Current router credits for
+ *						remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_MIN_RTR_CREDITS:	Minimum router credits for
+ *						remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_REFCOUNT:		Remote peer reference count
+ *						(NLA_U32)
+ * @LNET_PEER_NI_LIST_ATTR_STATS_COUNT:		Remote peer general stats,
+ *						reports sent, recieved, and
+ *						dropped packets. (NLA_NESTED)
+ *
+ * @LNET_PEER_NI_LIST_ATTR_SENT_STATS:		Remote peer sent stats,
+ *						reports gets, puts, acks, and
+ *						hello packets. (NLA_NESTED)
+ * @LNET_PEER_NI_LIST_ATTR_RECV_STATS:		Remote peer recieved stats,
+ *						reports gets, puts, acks, and
+ *						hello packets. (NLA_NESTED)
+ * @LNET_PEER_NI_LIST_ATTR_DROP_STATS:		Remote peer dropped stats,
+ *						reports gets, puts, acks, and
+ *						hello packets. (NLA_NESTED)
+ * @LNET_PEER_NI_LIST_ATTR_HEALTH_STATS:	Report the stats about the
+ *						health of the remote peer.
+ *						(NLA_NESTED)
+ */
+enum lnet_peer_ni_list_attr {
+	LNET_PEER_NI_LIST_ATTR_UNSPEC = 0,
+
+	LNET_PEER_NI_LIST_ATTR_NID,
+	LNET_PEER_NI_LIST_ATTR_UDSP_INFO,
+	LNET_PEER_NI_LIST_ATTR_STATE,
+
+	LNET_PEER_NI_LIST_ATTR_MAX_TX_CREDITS,
+	LNET_PEER_NI_LIST_ATTR_CUR_TX_CREDITS,
+	LNET_PEER_NI_LIST_ATTR_MIN_TX_CREDITS,
+	LNET_PEER_NI_LIST_ATTR_QUEUE_BUF_COUNT,
+	LNET_PEER_NI_LIST_ATTR_CUR_RTR_CREDITS,
+	LNET_PEER_NI_LIST_ATTR_MIN_RTR_CREDITS,
+	LNET_PEER_NI_LIST_ATTR_REFCOUNT,
+	LNET_PEER_NI_LIST_ATTR_STATS_COUNT,
+
+	LNET_PEER_NI_LIST_ATTR_SENT_STATS,
+	LNET_PEER_NI_LIST_ATTR_RECV_STATS,
+	LNET_PEER_NI_LIST_ATTR_DROP_STATS,
+	LNET_PEER_NI_LIST_ATTR_HEALTH_STATS,
+
+	__LNET_PEER_NI_LIST_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_PEER_NI_LIST_ATTR_MAX (__LNET_PEER_NI_LIST_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_peer_ni_list_stats_count		      - LNet remote peer traffic
+ *							stats netlink properties
+ *							attributes that provide
+ *							traffic stats on the
+ *							remote LNet peer 'NI'.
+ *							These values are used to
+ *							piece together messages
+ *							for sending and receiving.
+ *
+ * @LNET_PEER_NI_LIST_STATS_COUNT_ATTR_UNSPEC:		unspecified attribute to
+ *							catch errors
+ *
+ * @LNET_PEER_NI_LIST_STATS_COUNT_ATTR_SEND_COUNT:	Number of sent packets for
+ *							remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_COUNT_ATTR_RECV_COUNT:	Numebr of recieved packets
+ *							for remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_COUNT_ATTR_DROP_COUNT:	Number of dropped packets
+ *							for remote peer (NLA_U32)
+ */
+enum lnet_peer_ni_list_stats_count {
+	LNET_PEER_NI_LIST_STATS_COUNT_ATTR_UNSPEC = 0,
+
+	LNET_PEER_NI_LIST_STATS_COUNT_ATTR_SEND_COUNT,
+	LNET_PEER_NI_LIST_STATS_COUNT_ATTR_RECV_COUNT,
+	LNET_PEER_NI_LIST_STATS_COUNT_ATTR_DROP_COUNT,
+	__LNET_PEER_NI_LIST_STATS_COUNT_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_PEER_NI_LIST_STATS_COUNT_ATTR_MAX (__LNET_PEER_NI_LIST_STATS_COUNT_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_peer_ni_list_stats	      - LNet remote peer stats netlink
+ *						properties attributes that
+ *						provide stats on the remote
+ *						LNet peer 'NI'. These values are
+ *						used to piece together messages
+ *						for sending and receiving.
+ *
+ * @LNET_PEER_NI_LIST_STATS_ATTR_UNSPEC:	unspecified attribute to catch
+ *						errors
+ *
+ * @LNET_PEER_NI_LIST_STATS_ATTR_PUT:		PUT message count for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_ATTR_GET:		GET message count for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_ATTR_REPLY:		REPLY message count for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_ATTR_ACK:		ACK message count for remote
+ *						peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_STATS_ATTR_HEALTH:	HELLO message count for remote
+ *						peer (NLA_U32)
+ */
+enum lnet_peer_ni_list_stats {
+	LNET_PEER_NI_LIST_STATS_ATTR_UNSPEC = 0,
+
+	LNET_PEER_NI_LIST_STATS_ATTR_PUT,
+	LNET_PEER_NI_LIST_STATS_ATTR_GET,
+	LNET_PEER_NI_LIST_STATS_ATTR_REPLY,
+	LNET_PEER_NI_LIST_STATS_ATTR_ACK,
+	LNET_PEER_NI_LIST_STATS_ATTR_HELLO,
+	__LNET_PEER_NI_LIST_STATS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_PEER_NI_LIST_STATS_ATTR_MAX (__LNET_PEER_NI_LIST_STATS_ATTR_MAX_PLUS_ONE - 1)
+
+/** enum lnet_peer_ni_list_health_stats		      - LNet remote peer health
+ *							stats netlink properties
+ *							attributes that provide
+ *							stats on the health of a
+ *							remote LNet peer 'NI'.
+ *							These values are used to
+ *							piece together messages
+ *							for sending and receiving.
+ *
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_UNSPEC:		unspecified attribute to
+ *							catch errors
+ *
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_VALUE:		Health level of remote
+ *							peer (NLA_S32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_DROP:		drop message state for
+ *							remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_TIMEOUT:	timeout set for remote
+ *							peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_ERROR:		total errors for remote
+ *							peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_NETWORK_TIMEOUT: network timeout for
+ *							 remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_PING_COUNT:	number of pings for
+ *							remote peer (NLA_U32)
+ * @LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_NEXT_PING:	timestamp for next ping
+ *							sent by remote peer
+ *							(NLA_S64)
+ */
+enum lnet_peer_ni_list_health_stats {
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_UNSPEC = 0,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_PAD = LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_UNSPEC,
+
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_VALUE,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_DROPPED,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_TIMEOUT,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_ERROR,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_NETWORK_TIMEOUT,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_PING_COUNT,
+	LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_NEXT_PING,
+
+	__LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_MAX (__LNET_PEER_NI_LIST_HEALTH_STATS_ATTR_MAX_PLUS_ONE - 1)
+
 /** LNet netlink ping API */
 
-/** enum lnet_ping_atts				      - LNet ping netlink properties
+/** enum lnet_ping_attr				      - LNet ping netlink properties
  *							attributes to describe ping format
  *							These values are used to piece together
  *							messages for sending and receiving.
@@ -749,7 +959,7 @@ enum lnet_ping_attr {
 
 #define LNET_PING_ATTR_MAX (__LNET_PING_ATTR_MAX_PLUS_ONE - 1)
 
-/** enium lnet_ping_peer_ni_attr		      - LNet peer ni information reported by
+/** enum lnet_ping_peer_ni_attr		      - LNet peer ni information reported by
  *							ping command. A list of these are
  *							returned with a ping request.
  *

commit 4d95238de1b9021afaaf337ca60e3335e99881a6

LU-17000 coverity: Fix Logically dead code under lnetctl.c
This patch fixes Logically dead code reported by
coverity run. This uncovers the missing call
to lustre_lnet_list_peer() to list peer under
old API.

CoverityID: 404746 ("Logically dead code")

Test-Parameters: trivial testlist=sanity-lnet
Fixes: f0be00678c ("LU-9680 lnet: collect data about peer_ni by using Netlink")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I0659ce403110118697fb8c88ade70f1695509382
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52921
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/utils/lnetctl.c
@@ -4102,8 +4102,8 @@ old_api:
 
 static int jt_list_peer(int argc, char **argv)
 {
-	int rc;
 	struct cYAML *err_rc = NULL, *list_rc = NULL;
+	int rc;
 
 	rc = check_cmd(peer_cmds, "peer", "list", 0, argc, argv);
 	if (rc)
@@ -4115,7 +4115,9 @@ static int jt_list_peer(int argc, char **argv)
 			goto old_api;
 		return rc;
 	}
+
 old_api:
+	rc = lustre_lnet_list_peer(-1, &list_rc, &err_rc);
 	if (rc != LUSTRE_CFG_RC_NO_ERR)
 		cYAML_print_tree2file(stderr, err_rc);
 	else if (list_rc)