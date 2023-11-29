lustre/lustre-release

commit 239e826876e5e20405e14a180a8fd4377d6553b2

LU-16518 misc: use fixed hash code
There is a configure check to avoid using
broken hashing code. All calls to 'hash_*'
are replace by the 'cfs_hash_*' equivalents,
to make use of this check.

Two functions which hash then apply a mask
are removed. The calls are replaced with
'cfs_hash_*' and manually applying the mask.

Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Ia4b27cb6fb1329b9df45c00f748a8d22178b0654
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49916
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

libcfs/include/libcfs/libcfs_hash.h
@@ -37,24 +37,9 @@
 #ifndef __LIBCFS_HASH_H__
 #define __LIBCFS_HASH_H__
 
-#include <linux/hash.h>
 #include <linux/spinlock.h>
 #include <linux/workqueue.h>
-
-/*
- * Knuth recommends primes in approximately golden ratio to the maximum
- * integer representable by a machine word for multiplicative hashing.
- * Chuck Lever verified the effectiveness of this technique:
- * http://www.citi.umich.edu/techreports/reports/citi-tr-00-1.pdf
- *
- * These primes are chosen to be bit-sparse, that is operations on
- * them can use shifts and additions instead of multiplications for
- * machines where multiplications are slow.
- */
-/* 2^31 + 2^29 - 2^25 + 2^22 - 2^19 - 2^16 + 1 */
-#define CFS_GOLDEN_RATIO_PRIME_32 0x9e370001UL
-/*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
-#define CFS_GOLDEN_RATIO_PRIME_64 0x9e37fffffffc0001ULL
+#include <libcfs/linux/linux-hash.h>
 
 /** disable debug */
 #define CFS_HASH_DEBUG_NONE	0
@@ -828,24 +813,6 @@ cfs_hash_djb2_hash(const void *key, size_t size, unsigned mask)
                 hash = hash * 33 + ((char *)key)[i];
 
         return (hash & mask);
-}
-
-/*
- * Generic u32 hash algorithm.
- */
-static inline unsigned
-cfs_hash_u32_hash(const __u32 key, unsigned mask)
-{
-        return ((key * CFS_GOLDEN_RATIO_PRIME_32) & mask);
-}
-
-/*
- * Generic u64 hash algorithm.
- */
-static inline unsigned
-cfs_hash_u64_hash(const __u64 key, unsigned mask)
-{
-        return ((unsigned)(key * CFS_GOLDEN_RATIO_PRIME_64) & mask);
 }
 
 /** iterate over all buckets in @bds (array of struct cfs_hash_bd) */

lustre/ldlm/ldlm_flock.c
@@ -853,7 +853,7 @@ void ldlm_flock_policy_local_to_wire(const union ldlm_policy_data *lpolicy,
 static unsigned
 ldlm_export_flock_hash(struct cfs_hash *hs, const void *key, unsigned mask)
 {
-	return cfs_hash_u64_hash(*(__u64 *)key, mask);
+	return cfs_hash_64(*(__u64 *)key, 0) & mask;
 }
 
 static void *

lustre/ldlm/ldlm_lockd.c
@@ -3101,7 +3101,7 @@ void ldlm_put_ref(void)
 static unsigned
 ldlm_export_lock_hash(struct cfs_hash *hs, const void *key, unsigned int mask)
 {
-	return cfs_hash_u64_hash(((struct lustre_handle *)key)->cookie, mask);
+	return cfs_hash_64(((struct lustre_handle *)key)->cookie, 0) & mask;
 }
 
 static void *

lustre/quota/lquota_entry.c
@@ -41,7 +41,7 @@ MODULE_PARM_DESC(hash_lqs_cur_bits, "the current bits of lqe hash");
 static unsigned
 lqe64_hash_hash(struct cfs_hash *hs, const void *key, unsigned mask)
 {
-	return cfs_hash_u64_hash(*((__u64 *)key), mask);
+	return cfs_hash_64(*((__u64 *)key), 0) & mask;
 }
 
 static void *lqe64_hash_key(struct hlist_node *hnode)

libcfs/include/libcfs/linux/linux-hash.h
@@ -59,10 +59,20 @@ static __always_inline u32 cfs_hash_64(u64 val, unsigned int bits)
 	return cfs_hash_32(((u32)val ^ ((val >> 32) * GOLDEN_RATIO_32)), bits);
 #endif
 }
+
+#if BITS_PER_LONG == 32
+#define cfs_hash_long(val, bits) cfs_hash_32(val, bits)
+#elif BITS_PER_LONG == 64
+#define cfs_hash_long(val, bits) cfs_hash_64(val, bits)
+#else
+#error Wordsize not 32 or 64
+#endif
+
 #else
 
-#define cfs_hash_32	hash_32
-#define cfs_hash_64	hash_64
+#define cfs_hash_32 hash_32
+#define cfs_hash_64 hash_64
+#define cfs_hash_long hash_long
 
 #endif /* HAVE_BROKEN_HASH_64 */
 

libcfs/libcfs/linux/linux-wait.c
@@ -1,7 +1,7 @@
 /*
  * The implementation of the wait_bit*() and related waiting APIs:
  */
-#include <linux/hash.h>
+#include <libcfs/linux/linux-hash.h>
 #include <linux/sched.h>
 #ifdef HAVE_SCHED_HEADERS
 #include <linux/sched/signal.h>

lnet/include/lnet/lib-lnet.h
@@ -513,8 +513,8 @@ lnet_nid2peerhash(struct lnet_nid *nid)
 	int i;
 
 	for (i = 0; i < 4; i++)
-		h = hash_32(nid->nid_addr[i]^h, 32);
-	return hash_32(LNET_NID_NET(nid) ^ h, LNET_PEER_HASH_BITS);
+		h = cfs_hash_32(nid->nid_addr[i]^h, 32);
+	return cfs_hash_32(LNET_NID_NET(nid) ^ h, LNET_PEER_HASH_BITS);
 }
 
 static inline struct list_head *

lnet/lnet/api-ni.c
@@ -1635,8 +1635,8 @@ lnet_nid_cpt_hash(struct lnet_nid *nid, unsigned int number)
 		return lnet_nid4_cpt_hash(lnet_nid_to_nid4(nid), number);
 
 	for (i = 0; i < 4; i++)
-		h = hash_32(nid->nid_addr[i]^h, 32);
-	val = hash_32(LNET_NID_NET(nid) ^ h, LNET_CPT_BITS);
+		h = cfs_hash_32(nid->nid_addr[i]^h, 32);
+	val = cfs_hash_32(LNET_NID_NET(nid) ^ h, LNET_CPT_BITS);
 	if (val < number)
 		return val;
 	return (unsigned int)(h + val + (val >> 1)) % number;

commit ce404bd07c37f45d8c8fe0cf3e3ecf6e18740f43

LU-17174 misc: fix hash functions
1) LU-16518 landing caused a bug which visible with debug kernel

UBSAN: Undefined behaviour in include/linux/hash.h:81:31
shift exponent 64 is too large for 64-bit type
	'long long unsigned int'
Call Trace:
dump_stack+0x8e/0xd0
ubsan_epilogue+0x5/0x21
ldlm_export_lock_hash+0x49/0x4d [ptlrpc]
cfs_hash_bd_from_key+0x88/0x2e0 [libcfs]

2) use a high bits unstead of low as it more accurate.

HPe-bug-id: LUS-11925
Fixes: 239e8268 (LU-16518 misc: use fixed hash code)
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: Ie1c531ad220f44e55fbf80674a49472fb6024252
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52611
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

libcfs/include/libcfs/libcfs_hash.h
@@ -280,7 +280,8 @@ struct cfs_hash_hlist_ops {
 
 struct cfs_hash_ops {
 	/** return hashed value from @key */
-	unsigned (*hs_hash)(struct cfs_hash *hs, const void *key, unsigned mask);
+	unsigned int (*hs_hash)(struct cfs_hash *hs, const void *key,
+				const unsigned int bits);
 	/** return key address of @hnode */
 	void *   (*hs_key)(struct hlist_node *hnode);
 	/** copy key from @hnode to @key */
@@ -443,10 +444,10 @@ cfs_hash_bkt_size(struct cfs_hash *hs)
                hs->hs_extra_bytes;
 }
 
-static inline unsigned
-cfs_hash_id(struct cfs_hash *hs, const void *key, unsigned mask)
+static inline unsigned int
+cfs_hash_id(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return hs->hs_ops->hs_hash(hs, key, mask);
+	return hs->hs_ops->hs_hash(hs, key, bits);
 }
 
 static inline void *
@@ -803,16 +804,16 @@ void cfs_hash_debug_str(struct cfs_hash *hs, struct seq_file *m);
  * Generic djb2 hash algorithm for character arrays.
  */
 static inline unsigned
-cfs_hash_djb2_hash(const void *key, size_t size, unsigned mask)
+cfs_hash_djb2_hash(const void *key, size_t size, const unsigned int bits)
 {
-        unsigned i, hash = 5381;
+	unsigned int i, hash = 5381;
 
-        LASSERT(key != NULL);
+	LASSERT(key != NULL);
 
-        for (i = 0; i < size; i++)
-                hash = hash * 33 + ((char *)key)[i];
+	for (i = 0; i < size; i++)
+		hash = hash * 33 + ((char *)key)[i];
 
-        return (hash & mask);
+	return (hash & ((1U << bits) - 1));
 }
 
 /** iterate over all buckets in @bds (array of struct cfs_hash_bd) */

libcfs/libcfs/hash.c
@@ -519,12 +519,12 @@ static void
 cfs_hash_bd_from_key(struct cfs_hash *hs, struct cfs_hash_bucket **bkts,
 		     unsigned int bits, const void *key, struct cfs_hash_bd *bd)
 {
-        unsigned int index = cfs_hash_id(hs, key, (1U << bits) - 1);
+	unsigned int index = cfs_hash_id(hs, key, bits);
 
-        LASSERT(bits == hs->hs_cur_bits || bits == hs->hs_rehash_bits);
+	LASSERT(bits == hs->hs_cur_bits || bits == hs->hs_rehash_bits);
 
-        bd->bd_bucket = bkts[index & ((1U << (bits - hs->hs_bkt_bits)) - 1)];
-        bd->bd_offset = index >> (bits - hs->hs_bkt_bits);
+	bd->bd_bucket = bkts[index & ((1U << (bits - hs->hs_bkt_bits)) - 1)];
+	bd->bd_offset = index >> (bits - hs->hs_bkt_bits);
 }
 
 void

lustre/ldlm/ldlm_flock.c
@@ -853,10 +853,11 @@ void ldlm_flock_policy_local_to_wire(const union ldlm_policy_data *lpolicy,
 /*
  * Export handle<->flock hash operations.
  */
-static unsigned
-ldlm_export_flock_hash(struct cfs_hash *hs, const void *key, unsigned mask)
+static unsigned int
+ldlm_export_flock_hash(struct cfs_hash *hs, const void *key,
+		       const unsigned int bits)
 {
-	return cfs_hash_64(*(__u64 *)key, 0) & mask;
+	return cfs_hash_64(*(__u64 *)key, bits);
 }
 
 static void *

lustre/ldlm/ldlm_lockd.c
@@ -3120,9 +3120,10 @@ void ldlm_put_ref(void)
  * Export handle<->lock hash operations.
  */
 static unsigned
-ldlm_export_lock_hash(struct cfs_hash *hs, const void *key, unsigned int mask)
+ldlm_export_lock_hash(struct cfs_hash *hs, const void *key,
+		      const unsigned int bits)
 {
-	return cfs_hash_64(((struct lustre_handle *)key)->cookie, 0) & mask;
+	return cfs_hash_64(((struct lustre_handle *)key)->cookie, bits);
 }
 
 static void *

lustre/mdt/mdt_hsm_cdt_actions.c
@@ -73,9 +73,10 @@ static inline void cdt_agent_record_loc_put(struct cdt_agent_record_loc *carl)
 }
 
 static unsigned int
-cdt_agent_record_hash(struct cfs_hash *hs, const void *key, unsigned int mask)
+cdt_agent_record_hash(struct cfs_hash *hs, const void *key,
+		      const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(u64), mask);
+	return cfs_hash_djb2_hash(key, sizeof(u64), bits);
 }
 
 static void *cdt_agent_record_object(struct hlist_node *hnode)

lustre/mdt/mdt_hsm_cdt_requests.c
@@ -44,9 +44,10 @@
 #include "mdt_internal.h"
 
 static unsigned int
-cdt_request_cookie_hash(struct cfs_hash *hs, const void *key, unsigned int mask)
+cdt_request_cookie_hash(struct cfs_hash *hs, const void *key,
+			const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(u64), mask);
+	return cfs_hash_djb2_hash(key, sizeof(u64), bits);
 }
 
 static void *cdt_request_cookie_object(struct hlist_node *hnode)

lustre/obdclass/jobid.c
@@ -807,10 +807,10 @@ EXPORT_SYMBOL(jobid_cache_fini);
 /*
  * Hash operations for pid<->jobid
  */
-static unsigned jobid_hashfn(struct cfs_hash *hs, const void *key,
-			     unsigned mask)
+static unsigned int
+jobid_hashfn(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(pid_t), mask);
+	return cfs_hash_djb2_hash(key, sizeof(pid_t), bits);
 }
 
 static void *jobid_key(struct hlist_node *hnode)

lustre/obdclass/lprocfs_jobstats.c
@@ -73,10 +73,10 @@ struct job_stat {
 	struct rcu_head		js_rcu;		/* RCU head for job_reclaim_rcu*/
 };
 
-static unsigned
-job_stat_hash(struct cfs_hash *hs, const void *key, unsigned mask)
+static unsigned int
+job_stat_hash(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, strlen(key), mask);
+	return cfs_hash_djb2_hash(key, strlen(key), bits);
 }
 
 static void *job_stat_key(struct hlist_node *hnode)

lustre/obdclass/obd_config.c
@@ -2373,10 +2373,10 @@ EXPORT_SYMBOL(class_manual_cleanup);
 /*
  * nid<->nidstats hash operations
  */
-static unsigned
-nidstats_hash(struct cfs_hash *hs, const void *key, unsigned int mask)
+static unsigned int
+nidstats_hash(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(struct lnet_nid), mask);
+	return cfs_hash_djb2_hash(key, sizeof(struct lnet_nid), bits);
 }
 
 static void *
@@ -2433,10 +2433,11 @@ static struct cfs_hash_ops nid_stat_hash_ops = {
  * client_generation<->export hash operations
  */
 
-static unsigned
-gen_hash(struct cfs_hash *hs, const void *key, unsigned mask)
+static unsigned int
+gen_hash(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(__u32), mask);
+	/* XXX did hash needs ? */
+	return cfs_hash_djb2_hash(key, sizeof(__u32), bits);
 }
 
 static void *

lustre/ptlrpc/nodemap_handler.c
@@ -118,10 +118,11 @@ void nodemap_putref(struct lu_nodemap *nodemap)
 }
 EXPORT_SYMBOL(nodemap_putref);
 
-static __u32 nodemap_hashfn(struct cfs_hash *hash_body,
-			    const void *key, unsigned mask)
+static unsigned int
+nodemap_hashfn(struct cfs_hash *hash_body,
+	       const void *key, const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, strlen(key), mask);
+	return cfs_hash_djb2_hash(key, strlen(key), bits);
 }
 
 static void *nodemap_hs_key(struct hlist_node *hnode)

lustre/ptlrpc/nrs_tbf.c
@@ -544,10 +544,11 @@ static struct binheap_ops nrs_tbf_heap_ops = {
 	.hop_compare	= tbf_cli_compare,
 };
 
-static unsigned nrs_tbf_jobid_hop_hash(struct cfs_hash *hs, const void *key,
-				  unsigned mask)
+static unsigned int
+nrs_tbf_jobid_hop_hash(struct cfs_hash *hs, const void *key,
+		       const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, strlen(key), mask);
+	return cfs_hash_djb2_hash(key, strlen(key), bits);
 }
 
 static int nrs_tbf_jobid_hop_keycmp(const void *key, struct hlist_node *hnode)
@@ -1043,10 +1044,11 @@ static struct nrs_tbf_ops nrs_tbf_jobid_ops = {
 #define NRS_TBF_NID_BKT_BITS	8
 #define NRS_TBF_NID_BITS	16
 
-static unsigned nrs_tbf_nid_hop_hash(struct cfs_hash *hs, const void *key,
-				  unsigned mask)
+static unsigned int
+nrs_tbf_nid_hop_hash(struct cfs_hash *hs, const void *key,
+		     const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(lnet_nid_t), mask);
+	return cfs_hash_djb2_hash(key, sizeof(lnet_nid_t), bits);
 }
 
 static int nrs_tbf_nid_hop_keycmp(const void *key, struct hlist_node *hnode)
@@ -1273,10 +1275,11 @@ static struct nrs_tbf_ops nrs_tbf_nid_ops = {
 	.o_rule_fini = nrs_tbf_nid_rule_fini,
 };
 
-static unsigned nrs_tbf_hop_hash(struct cfs_hash *hs, const void *key,
-				 unsigned mask)
+static unsigned int
+nrs_tbf_hop_hash(struct cfs_hash *hs, const void *key,
+		 const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, strlen(key), mask);
+	return cfs_hash_djb2_hash(key, strlen(key), bits);
 }
 
 static int nrs_tbf_hop_keycmp(const void *key, struct hlist_node *hnode)
@@ -2079,10 +2082,12 @@ static void nrs_tbf_opcode_rule_fini(struct nrs_tbf_rule *rule)
 	OBD_FREE(rule->tr_opcodes_str, strlen(rule->tr_opcodes_str) + 1);
 }
 
-static unsigned nrs_tbf_opcode_hop_hash(struct cfs_hash *hs, const void *key,
-					unsigned mask)
+static unsigned int
+nrs_tbf_opcode_hop_hash(struct cfs_hash *hs, const void *key,
+			const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(__u32), mask);
+	/* XXX did hash needs ? */
+	return cfs_hash_djb2_hash(key, sizeof(__u32), bits);
 }
 
 static int nrs_tbf_opcode_hop_keycmp(const void *key, struct hlist_node *hnode)
@@ -2345,10 +2350,11 @@ struct nrs_tbf_ops nrs_tbf_opcode_ops = {
 	.o_rule_fini = nrs_tbf_opcode_rule_fini,
 };
 
-static unsigned nrs_tbf_id_hop_hash(struct cfs_hash *hs, const void *key,
-				    unsigned mask)
+static unsigned int
+nrs_tbf_id_hop_hash(struct cfs_hash *hs, const void *key,
+		    const unsigned int bits)
 {
-	return cfs_hash_djb2_hash(key, sizeof(struct tbf_id), mask);
+	return cfs_hash_djb2_hash(key, sizeof(struct tbf_id), bits);
 }
 
 static int nrs_tbf_id_hop_keycmp(const void *key, struct hlist_node *hnode)

lustre/quota/lquota_entry.c
@@ -39,9 +39,9 @@ module_param(hash_lqs_cur_bits, int, 0444);
 MODULE_PARM_DESC(hash_lqs_cur_bits, "the current bits of lqe hash");
 
 static unsigned
-lqe64_hash_hash(struct cfs_hash *hs, const void *key, unsigned mask)
+lqe64_hash_hash(struct cfs_hash *hs, const void *key, const unsigned int bits)
 {
-	return cfs_hash_64(*((__u64 *)key), 0) & mask;
+	return cfs_hash_64(*((__u64 *)key), bits);
 }
 
 static void *lqe64_hash_key(struct hlist_node *hnode)