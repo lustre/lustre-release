lustre/lustre-release

commit 8d828762d18ffaa2945bde56039127d0e75aceb3

LU-17015 gss: support large kerberos token for rpc sec init
If the current Kerberos setup is using large token, like when PAC
feature is enabled for Kerberos, authentication can fail due to server
side unable to exchange token between kernel and userspace.
This limitation is inherent to the sunrpc cache mechanism, that can
only handle tokens up to PAGE_SIZE.

For RPC sec init phase, use Lustre's upcall cache mechanism
instead of deprecated kernel's sunrpc cache. The upcall calls a new
userspace command 'l_getauth', that fowards the sec init request to
the lsvcgssd daemon via Unix domain sockets.

Test-Parameters: kerberos=true testlist=sanity-krb5
Change-Id: I709cd79894a5a13fc4cdfab2109c86f2230db3b8
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52224
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -255,8 +255,6 @@ enum nodemap_idx_type {
 	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
 };
 
-#define LUSTRE_NODEMAP_NAME_LENGTH	16
-
 /* lu_nodemap flags */
 enum nm_flag_bits {
 	NM_FL_ALLOW_ROOT_ACCESS = 0x1,

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3725,6 +3725,8 @@ struct llog_update_record {
 				     SELINUX_POLICY_VER_LEN + \
 				     SELINUX_POLICY_HASH_LEN + 3)
 
+#define LUSTRE_NODEMAP_NAME_LENGTH	16
+
 /** enums containing the types of ids contained in a nodemap
  * kept so other modules (mgs, mdt, etc) can define the type
  * of search easily

lustre/include/upcall_cache.h
@@ -34,6 +34,8 @@
 
 #include <libcfs/libcfs.h>
 #include <uapi/linux/lnet/lnet-types.h>
+#include <obd.h>
+#include <lustre_sec.h>
 
 /** \defgroup ucache ucache
  *
@@ -78,6 +80,19 @@ struct md_identity {
 	struct md_perm            *mi_perms;
 };
 
+struct gss_rsi {
+	struct upcall_cache_entry *si_uc_entry;
+	lnet_nid_t		   si_nid4; /* FIXME Support larger NID */
+	char			   si_nm_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
+	__u32			   si_lustre_svc;
+	rawobj_t		   si_in_handle;
+	rawobj_t		   si_in_token;
+	rawobj_t		   si_out_handle;
+	rawobj_t		   si_out_token;
+	int			   si_major_status;
+	int			   si_minor_status;
+};
+
 struct upcall_cache_entry {
 	struct list_head	ue_hash;
 	uint64_t		ue_key;
@@ -88,6 +103,7 @@ struct upcall_cache_entry {
 	time64_t		ue_expire;
 	union {
 		struct md_identity	identity;
+		struct gss_rsi		rsi;
 	} u;
 };
 

libcfs/libcfs/crypto/fname.c
@@ -207,9 +207,6 @@ static inline int llcrypt_base64_decode(const char *src, int len, u8 *dst)
  * This base64 encoding is RFC 4648 compliant base64-url encoding.
  */
 
-static const char base64url_table[] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
 #define LLCRYPT_BASE64URL_CHARS(nbytes)	DIV_ROUND_UP((nbytes) * 4, 3)
 
 /**

libcfs/libcfs/crypto/llcrypt_private.h
@@ -18,6 +18,7 @@
 #include <libcfs/crypto/llcrypt.h>
 #include <crypto/hash.h>
 #include <lustre_disk.h>
+#include <uapi/linux/lustre/lgss.h>
 
 #ifndef CRYPTO_TFM_REQ_FORBID_WEAK_KEYS
 #define CRYPTO_TFM_REQ_FORBID_WEAK_KEYS CRYPTO_TFM_REQ_WEAK_KEY

lustre/include/lustre_sec.h
@@ -1154,12 +1154,49 @@ void sptlrpc_target_update_exp_flavor(struct obd_device *obd,
                                       struct sptlrpc_rule_set *rset);
 
 /*
- * reverse context
+ * context and reverse context
  */
+#define GSS_SEQ_WIN			(2048)
+#define GSS_SEQ_WIN_MAIN		GSS_SEQ_WIN
+#define GSS_SEQ_WIN_BACK		(128)
+#define GSS_SEQ_REPACK_THRESHOLD	(GSS_SEQ_WIN_MAIN / 2 + \
+					 GSS_SEQ_WIN_MAIN / 4)
+
+struct gss_svc_seq_data {
+	spinlock_t		ssd_lock;
+	/*
+	 * highest sequence number seen so far, for main and back window
+	 */
+	__u32			ssd_max_main;
+	__u32			ssd_max_back;
+	/*
+	 * main and back window
+	 * for i such that ssd_max - GSS_SEQ_WIN < i <= ssd_max, the i-th bit
+	 * of ssd_win is nonzero iff sequence number i has been seen already.
+	 */
+	unsigned long		ssd_win_main[GSS_SEQ_WIN_MAIN/BITS_PER_LONG];
+	unsigned long		ssd_win_back[GSS_SEQ_WIN_BACK/BITS_PER_LONG];
+};
+
+struct gss_svc_ctx {
+	struct gss_ctx	       *gsc_mechctx;
+	struct gss_svc_seq_data gsc_seqdata;
+	rawobj_t		gsc_rvs_hdl;
+	__u32			gsc_rvs_seq;
+	uid_t			gsc_uid;
+	gid_t			gsc_gid;
+	uid_t			gsc_mapped_uid;
+	unsigned int		gsc_usr_root:1,
+				gsc_usr_mds:1,
+				gsc_usr_oss:1,
+				gsc_remote:1,
+				gsc_reverse:1;
+};
+
 int sptlrpc_svc_install_rvs_ctx(struct obd_import *imp,
-                                struct ptlrpc_svc_ctx *ctx);
+				struct ptlrpc_svc_ctx *ctx);
 int sptlrpc_cli_install_rvs_ctx(struct obd_import *imp,
-                                struct ptlrpc_cli_ctx *ctx);
+				struct ptlrpc_cli_ctx *ctx);
 
 /* bulk security api */
 int sptlrpc_enc_pool_add_user(void);

lustre/include/uapi/linux/lustre/lgss.h
@@ -29,7 +29,23 @@
 #ifndef _LGSS_H
 #define _LGSS_H
 
+#ifndef __KERNEL__
+# define __USE_ISOC99	1
+# include <stdio.h> /* snprintf() */
+# include <stdlib.h> /* abs() */
+# include <inttypes.h> /* PRIu64 */
+# include <ctype.h> /* isascii() */
+# define __USE_GNU      1
+# define __USE_XOPEN2K8  1
+#else
+#include <linux/ctype.h>
+#define PRIu64 "llu"
+#define PRIx64 "llx"
+#endif /* !__KERNEL__ */
+
 #include <linux/types.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
 
 /*
  * sparse kernel source annotations
@@ -55,4 +71,434 @@ struct lgssd_ioctl_param {
 	__u64 reply_length;
 };
 
+#define GSS_SOCKET_PATH	"/tmp/svcgssd.socket"
+#define RSI_DOWNCALL_MAGIC	0x6d6dd62a
+#define RSI_DOWNCALL_PATH	"sptlrpc/gss/rsi_info"
+#define RSI_CACHE_NAME		"rsicache"
+
+struct rsi_downcall_data {
+	__u32	  sid_magic;
+	__u32	  sid_err;
+	__u32	  sid_hash;
+	__u32	  sid_maj_stat;
+	__u32	  sid_min_stat;
+	__u32	  sid_len;
+	__s64	  sid_offset;
+	/* sid_val contains in_handle, in_token,
+	 * out_handle, out_token
+	 */
+	char	  sid_val[0];
+};
+
+/*
+ * gss_string_write() - write some string
+ *
+ * If string is empty, write single digit 0.
+ * Pad with a trailing space.
+ */
+static inline void gss_string_write(char **dst, int *dstlen, const char *src)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	if (!strlen(src))
+		ret = snprintf(cp, *dstlen, "0");
+	else
+		ret = snprintf(cp, *dstlen, "%s", src);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write() - write some u64
+ */
+static inline void gss_u64_write_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "%"PRIu64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write_hex() - write some u64 in hex
+ */
+static inline void gss_u64_write_hex_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "0x%"PRIx64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_buffer_write() - write some buffer
+ */
+static inline void gss_buffer_write(char **dst, int *dstlen,
+				    const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	/* write size of data */
+	p = (__u32 *)cp;
+	*p = srclen;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+	if (!srclen)
+		goto out;
+
+	/* write data itself */
+	while (srclen && len) {
+		*cp++ = *src++;
+		len--;
+		srclen--;
+	}
+	if (!len && srclen)
+		len = -1;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_u32_write() - write some u32
+ */
+static inline void gss_u32_write(char **dst, int *dstlen, __u32 val)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	p = (__u32 *)cp;
+	*p = val;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+static const char base64url_table[] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+#define BASE64URL_CHARS(nbytes) ((((nbytes) * 4) + 3 - 1) / 3)
+
+/*
+ * gss_base64url_encode() - base64url-encode some binary data
+ *
+ * Encode data using base64url encoding, i.e. the "Base 64 Encoding with URL
+ * and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't used,
+ * as it's unneeded and not required by the RFC.
+ * Pad with a trailing space.
+ */
+static inline void gss_base64url_encode(char **dst, int *dstlen,
+					const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 ac = 0;
+	int bits = 0;
+	int i;
+
+	if (len < 0)
+		return;
+
+	if (!srclen)
+		return gss_string_write(dst, dstlen, "");
+
+	for (i = 0; i < srclen; i++) {
+		if (!len)
+			break;
+		ac = (ac << 8) | src[i];
+		bits += 8;
+		do {
+			bits -= 6;
+			*cp++ = base64url_table[(ac >> bits) & 0x3f];
+			len--;
+		} while (bits >= 6 && len > 0);
+	}
+	if (i < srclen) {
+		len = -1;
+		goto out;
+	}
+
+	if (bits) {
+		*cp++ = base64url_table[(ac << (6 - bits)) & 0x3f];
+		len--;
+	}
+
+	if (!len) {
+		len = -1;
+		goto out;
+	}
+	*cp++ = ' ';
+	len--;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_base64url_decode() - base64url-decode a string
+ *
+ * Decode a string using base64url encoding, i.e. the "Base 64 Encoding with
+ * URL and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't
+ * accepted, nor are non-encoding characters such as whitespace.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_base64url_decode(char **src, char *dst, int destsize)
+{
+	int bits = 0, len = 0;
+	char *cp = *src, *p;
+	char *bp = dst;
+	__u32 ac = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (*cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n' || *(cp + 1) == '\0')) {
+		cp++;
+		goto fini;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n' || *cp == '\0')
+			break;
+
+		p = strchr(base64url_table, *cp);
+		if (len > destsize || p == NULL || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		cp++;
+		ac = (ac << 6) | (p - base64url_table);
+		bits += 6;
+		if (bits >= 8) {
+			bits -= 8;
+			*bp++ = (__u8)(ac >> bits);
+			len++;
+		}
+	}
+
+	if (!isascii(*cp) || (ac & ((1 << bits) - 1))) {
+		len = -1;
+		goto out;
+	}
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_string_read() - read some string
+ *
+ * An empty string is represented with the single digit 0.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_string_read(char **src, char *dst, int destsize,
+				  int allowzero)
+{
+	char *cp = *src;
+	char *bp = dst;
+	int len = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (!allowzero && *cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n')) {
+		cp++;
+		goto out;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n')
+			break;
+
+		if (len >= destsize || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		*(bp++) = *(cp++);
+		len++;
+	}
+
+	if (!isascii(*cp)) {
+		len = -1;
+		goto out;
+	}
+
+	*src = cp;
+
+out:
+	return len;
+}
+
+#ifndef __KERNEL__
+/*
+ * gss_u64_read() - read some u64
+ */
+static inline int gss_u64_read_string(char **src, __u64 *n)
+{
+	char buf[24];
+	char *ep;
+	int ret;
+
+	ret = gss_string_read(src, buf, sizeof(buf), 1);
+	if (ret < 0)
+		return ret;
+
+	buf[ret] = '\0';
+	*n = strtoull(buf, &ep, 0);
+	if (*ep)
+		return -1;
+
+	return 0;
+}
 #endif
+
+/*
+ * gss_buffer_read() - read some buffer
+ */
+static inline int gss_buffer_read(char **src, char *dst, int destsize)
+{
+	char *cp = *src;
+	char *bp = dst;
+	__u32 *p;
+	int len, size;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	len = *p;
+	cp += sizeof(__u32);
+
+	if (len > destsize) {
+		len = -1;
+		goto out;
+	}
+
+	if (!len)
+		goto fini;
+
+	/* read data itself */
+	size = len;
+	while (size && destsize) {
+		*(bp++) = *(cp++);
+		destsize--;
+		size--;
+	}
+	if (!destsize && size)
+		len = -1;
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_buffer_get() - get reference to gss buffer
+ */
+static inline int gss_buffer_get(char **src, __u32 *len, __u8 **data)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	*len = *p;
+	cp += sizeof(__u32);
+
+	/* point to data buf */
+	if (!*len)
+		*data = NULL;
+	else
+		*data = (__u8 *)cp;
+
+	/* move forward */
+	cp += *len;
+
+	*src = cp;
+	return *len;
+}
+
+/*
+ * gss_u32_read() - read some u32
+ */
+static inline int gss_u32_read(char **src, __u32 *val)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	p = (__u32 *)cp;
+	*val = *p;
+	cp += sizeof(__u32);
+
+	*src = cp;
+	return 0;
+}
+
+#endif /* _LGSS_H */

lustre/obdclass/Makefile.in
@@ -19,14 +19,13 @@ obdclass-all-objs += lustre_handles.o lustre_peer.o local_storage.o
 obdclass-all-objs += statfs_pack.o obdo.o obd_config.o obd_mount.o obd_sysfs.o
 obdclass-all-objs += lu_object.o dt_object.o
 obdclass-all-objs += cl_object.o cl_page.o cl_lock.o cl_io.o lu_ref.o
-obdclass-all-objs += linkea.o
+obdclass-all-objs += linkea.o upcall_cache.o
 obdclass-all-objs += kernelcomm.o jobid.o
 obdclass-all-objs += integrity.o obd_cksum.o
 obdclass-all-objs += lu_tgt_descs.o lu_tgt_pool.o
 obdclass-all-objs += range_lock.o interval_tree.o
 
 @SERVER_TRUE@obdclass-all-objs += idmap.o
-@SERVER_TRUE@obdclass-all-objs += upcall_cache.o
 @SERVER_TRUE@obdclass-all-objs += lprocfs_jobstats.o
 @SERVER_TRUE@obdclass-all-objs += lprocfs_status_server.o
 @SERVER_TRUE@obdclass-all-objs += lu_ucred.o
@@ -43,7 +42,6 @@ EXTRA_DIST += cl_internal.h local_storage.h
 EXTRA_DIST += range_lock.c interval_tree.c
 
 @SERVER_FALSE@EXTRA_DIST += idmap.c
-@SERVER_FALSE@EXTRA_DIST += upcall_cache.c
 @SERVER_FALSE@EXTRA_DIST += lprocfs_jobstats.c
 @SERVER_FALSE@EXTRA_DIST += lprocfs_status_server.c
 @SERVER_FALSE@EXTRA_DIST += lu_ucred.c

commit fc67e5d18a70ca8b5f01abc89eabdbfc78381f7c

LU-16739 uapi: restore LUSTRE_NODEMAP_NAME_LENGTH in lustre_disk.h
sanity test 400b fails with:

lustre_disk.h:266:18: error: 'LUSTRE_NODEMAP_NAME_LENGTH' undeclared here (not in a function)
 char   ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];

This is due to the move of LUSTRE_NODEMAP_NAME_LENGTH to lustre_idl.h.
Move it back and this time add a message to avoid this in the
future.

Test-Parameters: trivial
Fixes: 8d828762d1 ("LU-17015 gss: support large kerberos token for rpc sec init")
Change-Id: I5479bbf13f26bfd3b4f6e5f7c0c1688d810fca53
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53208
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -255,6 +255,11 @@ enum nodemap_idx_type {
 	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
 };
 
+/* This is needed for struct nodemap_clustre_rec. Please don't move
+ * to lustre_idl.h which will break user land builds.
+ */
+#define LUSTRE_NODEMAP_NAME_LENGTH     16
+
 /* lu_nodemap flags */
 enum nm_flag_bits {
 	NM_FL_ALLOW_ROOT_ACCESS = 0x1,

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3727,8 +3727,6 @@ struct llog_update_record {
 				     SELINUX_POLICY_VER_LEN + \
 				     SELINUX_POLICY_HASH_LEN + 3)
 
-#define LUSTRE_NODEMAP_NAME_LENGTH	16
-
 /** enums containing the types of ids contained in a nodemap
  * kept so other modules (mgs, mdt, etc) can define the type
  * of search easily

lustre/include/upcall_cache.h
@@ -34,6 +34,7 @@
 
 #include <libcfs/libcfs.h>
 #include <uapi/linux/lnet/lnet-types.h>
+#include <uapi/linux/lustre/lustre_disk.h>
 #include <obd.h>
 #include <lustre_sec.h>
 