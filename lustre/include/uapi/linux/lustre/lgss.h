lustre/lustre-release

commit 8d828762d18ffaa2945bde56039127d0e75aceb3

LU-17015 gss: support large kerberos token for rpc sec init
If the current Kerberos setup is using large token, like when PAC
feature is enabled for Kerberos, authentication can fail due to server
side unable to exchange token between kernel and userspace.
This limitation is inherent to the sunrpc cache mechanism, that can
only handle tokens up to PAGE_SIZE.

For RPC sec init phase, use Lustre's upcall cache mechanism
instead of deprecated kernel's sunrpc cache. The upcall calls a new
userspace command 'l_getauth', that fowards the sec init request to
the lsvcgssd daemon via Unix domain sockets.

Test-Parameters: kerberos=true testlist=sanity-krb5
Change-Id: I709cd79894a5a13fc4cdfab2109c86f2230db3b8
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52224
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>

lustre/include/uapi/linux/lustre/lgss.h
@@ -29,7 +29,23 @@
 #ifndef _LGSS_H
 #define _LGSS_H
 
+#ifndef __KERNEL__
+# define __USE_ISOC99	1
+# include <stdio.h> /* snprintf() */
+# include <stdlib.h> /* abs() */
+# include <inttypes.h> /* PRIu64 */
+# include <ctype.h> /* isascii() */
+# define __USE_GNU      1
+# define __USE_XOPEN2K8  1
+#else
+#include <linux/ctype.h>
+#define PRIu64 "llu"
+#define PRIx64 "llx"
+#endif /* !__KERNEL__ */
+
 #include <linux/types.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
 
 /*
  * sparse kernel source annotations
@@ -55,4 +71,434 @@ struct lgssd_ioctl_param {
 	__u64 reply_length;
 };
 
+#define GSS_SOCKET_PATH	"/tmp/svcgssd.socket"
+#define RSI_DOWNCALL_MAGIC	0x6d6dd62a
+#define RSI_DOWNCALL_PATH	"sptlrpc/gss/rsi_info"
+#define RSI_CACHE_NAME		"rsicache"
+
+struct rsi_downcall_data {
+	__u32	  sid_magic;
+	__u32	  sid_err;
+	__u32	  sid_hash;
+	__u32	  sid_maj_stat;
+	__u32	  sid_min_stat;
+	__u32	  sid_len;
+	__s64	  sid_offset;
+	/* sid_val contains in_handle, in_token,
+	 * out_handle, out_token
+	 */
+	char	  sid_val[0];
+};
+
+/*
+ * gss_string_write() - write some string
+ *
+ * If string is empty, write single digit 0.
+ * Pad with a trailing space.
+ */
+static inline void gss_string_write(char **dst, int *dstlen, const char *src)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	if (!strlen(src))
+		ret = snprintf(cp, *dstlen, "0");
+	else
+		ret = snprintf(cp, *dstlen, "%s", src);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write() - write some u64
+ */
+static inline void gss_u64_write_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "%"PRIu64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write_hex() - write some u64 in hex
+ */
+static inline void gss_u64_write_hex_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "0x%"PRIx64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_buffer_write() - write some buffer
+ */
+static inline void gss_buffer_write(char **dst, int *dstlen,
+				    const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	/* write size of data */
+	p = (__u32 *)cp;
+	*p = srclen;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+	if (!srclen)
+		goto out;
+
+	/* write data itself */
+	while (srclen && len) {
+		*cp++ = *src++;
+		len--;
+		srclen--;
+	}
+	if (!len && srclen)
+		len = -1;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_u32_write() - write some u32
+ */
+static inline void gss_u32_write(char **dst, int *dstlen, __u32 val)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	p = (__u32 *)cp;
+	*p = val;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+static const char base64url_table[] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+#define BASE64URL_CHARS(nbytes) ((((nbytes) * 4) + 3 - 1) / 3)
+
+/*
+ * gss_base64url_encode() - base64url-encode some binary data
+ *
+ * Encode data using base64url encoding, i.e. the "Base 64 Encoding with URL
+ * and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't used,
+ * as it's unneeded and not required by the RFC.
+ * Pad with a trailing space.
+ */
+static inline void gss_base64url_encode(char **dst, int *dstlen,
+					const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 ac = 0;
+	int bits = 0;
+	int i;
+
+	if (len < 0)
+		return;
+
+	if (!srclen)
+		return gss_string_write(dst, dstlen, "");
+
+	for (i = 0; i < srclen; i++) {
+		if (!len)
+			break;
+		ac = (ac << 8) | src[i];
+		bits += 8;
+		do {
+			bits -= 6;
+			*cp++ = base64url_table[(ac >> bits) & 0x3f];
+			len--;
+		} while (bits >= 6 && len > 0);
+	}
+	if (i < srclen) {
+		len = -1;
+		goto out;
+	}
+
+	if (bits) {
+		*cp++ = base64url_table[(ac << (6 - bits)) & 0x3f];
+		len--;
+	}
+
+	if (!len) {
+		len = -1;
+		goto out;
+	}
+	*cp++ = ' ';
+	len--;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_base64url_decode() - base64url-decode a string
+ *
+ * Decode a string using base64url encoding, i.e. the "Base 64 Encoding with
+ * URL and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't
+ * accepted, nor are non-encoding characters such as whitespace.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_base64url_decode(char **src, char *dst, int destsize)
+{
+	int bits = 0, len = 0;
+	char *cp = *src, *p;
+	char *bp = dst;
+	__u32 ac = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (*cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n' || *(cp + 1) == '\0')) {
+		cp++;
+		goto fini;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n' || *cp == '\0')
+			break;
+
+		p = strchr(base64url_table, *cp);
+		if (len > destsize || p == NULL || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		cp++;
+		ac = (ac << 6) | (p - base64url_table);
+		bits += 6;
+		if (bits >= 8) {
+			bits -= 8;
+			*bp++ = (__u8)(ac >> bits);
+			len++;
+		}
+	}
+
+	if (!isascii(*cp) || (ac & ((1 << bits) - 1))) {
+		len = -1;
+		goto out;
+	}
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_string_read() - read some string
+ *
+ * An empty string is represented with the single digit 0.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_string_read(char **src, char *dst, int destsize,
+				  int allowzero)
+{
+	char *cp = *src;
+	char *bp = dst;
+	int len = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (!allowzero && *cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n')) {
+		cp++;
+		goto out;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n')
+			break;
+
+		if (len >= destsize || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		*(bp++) = *(cp++);
+		len++;
+	}
+
+	if (!isascii(*cp)) {
+		len = -1;
+		goto out;
+	}
+
+	*src = cp;
+
+out:
+	return len;
+}
+
+#ifndef __KERNEL__
+/*
+ * gss_u64_read() - read some u64
+ */
+static inline int gss_u64_read_string(char **src, __u64 *n)
+{
+	char buf[24];
+	char *ep;
+	int ret;
+
+	ret = gss_string_read(src, buf, sizeof(buf), 1);
+	if (ret < 0)
+		return ret;
+
+	buf[ret] = '\0';
+	*n = strtoull(buf, &ep, 0);
+	if (*ep)
+		return -1;
+
+	return 0;
+}
 #endif
+
+/*
+ * gss_buffer_read() - read some buffer
+ */
+static inline int gss_buffer_read(char **src, char *dst, int destsize)
+{
+	char *cp = *src;
+	char *bp = dst;
+	__u32 *p;
+	int len, size;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	len = *p;
+	cp += sizeof(__u32);
+
+	if (len > destsize) {
+		len = -1;
+		goto out;
+	}
+
+	if (!len)
+		goto fini;
+
+	/* read data itself */
+	size = len;
+	while (size && destsize) {
+		*(bp++) = *(cp++);
+		destsize--;
+		size--;
+	}
+	if (!destsize && size)
+		len = -1;
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_buffer_get() - get reference to gss buffer
+ */
+static inline int gss_buffer_get(char **src, __u32 *len, __u8 **data)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	*len = *p;
+	cp += sizeof(__u32);
+
+	/* point to data buf */
+	if (!*len)
+		*data = NULL;
+	else
+		*data = (__u8 *)cp;
+
+	/* move forward */
+	cp += *len;
+
+	*src = cp;
+	return *len;
+}
+
+/*
+ * gss_u32_read() - read some u32
+ */
+static inline int gss_u32_read(char **src, __u32 *val)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	p = (__u32 *)cp;
+	*val = *p;
+	cp += sizeof(__u32);
+
+	*src = cp;
+	return 0;
+}
+
+#endif /* _LGSS_H */

lustre/ptlrpc/wiretest.c
@@ -43,6 +43,7 @@
 #include <uapi/linux/lustre/lustre_access_log.h>
 #include <uapi/linux/lustre/lustre_lfsck_user.h>
 #include <uapi/linux/lustre/lustre_cfg.h>
+#include <uapi/linux/lustre/lgss.h>
 
 #include "ptlrpc_internal.h"
 
@@ -4389,6 +4390,42 @@ void lustre_assert_wire_constants(void)
 	LASSERTF((int)sizeof(((struct llog_changelog_user_rec *)0)->cur_tail) == 8, "found %lld\n",
 		 (long long)(int)sizeof(((struct llog_changelog_user_rec *)0)->cur_tail));
 #endif /* HAVE_SERVER_SUPPORT */
+
+	/* Checks for struct rsi_downcall_data */
+	LASSERTF((int)sizeof(struct rsi_downcall_data) == 32, "found %lld\n",
+		 (long long)(int)sizeof(struct rsi_downcall_data));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_magic) == 0, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_magic));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_magic) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_magic));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_err) == 4, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_err));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_err) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_err));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_hash) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_hash));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_hash) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_hash));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_maj_stat) == 12, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_maj_stat));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_maj_stat) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_maj_stat));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_min_stat) == 16, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_min_stat));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_min_stat) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_min_stat));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_len) == 20, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_len));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_len) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_len));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_offset) == 24, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_offset));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_offset) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_offset));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_val) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_val));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_val) == 0, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_val));
 
 	/* Checks for struct llog_gen */
 	LASSERTF((int)sizeof(struct llog_gen) == 16, "found %lld\n",

commit 7d764f1f11be144ad26e33aa8cecedc5bb708793

LU-17000 gss: Fix Out-of-bounds access under svcgssd_proc.c
Problem reported by coverity was passing 32bit type and
then dereferencing to larger 64bit under function
handle_channel_request(). This patch address this issue.

Since this is an uapi and to catch corner cases like
kernel modules being updated separately from user tools
RSI_DOWNCALL_MAGIC is also changed from 0x6d6dd62a to
0x6d6dd63a.

This patch also changes 32bit member (sid_hash) of
'struct rsi_downcall_data' to 64bit. Which also requires
changing of wiretest.c and wirecheck.c

CoverityID: 404758 ("Out-of-bounds access")
Fixes: 8d828762d1 ("LU-17015 gss: support large kerberos token for rpc sec init")
Test-Parameters: kerberos=true testlist=sanity-krb5
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I8041cd4063f1b1cefdebf5681df426be61820f99
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52920
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lgss.h
@@ -72,18 +72,27 @@ struct lgssd_ioctl_param {
 };
 
 #define GSS_SOCKET_PATH	"/tmp/svcgssd.socket"
-#define RSI_DOWNCALL_MAGIC	0x6d6dd62a
+/*
+ * Old RSI_DOWNCALL_MAGIC was:
+ * #define RSI_DOWNCALL_MAGIC	0x6d6dd62a
+ *
+ * This is an uapi and to catch cases like kernel modules
+ * being updated separately from user tools new
+ * RSI_DOWNCALL_MAGIC(0x6d6dd63a) was introduced
+ */
+#define RSI_DOWNCALL_MAGIC	0x6d6dd63a
 #define RSI_DOWNCALL_PATH	"sptlrpc/gss/rsi_info"
 #define RSI_CACHE_NAME		"rsicache"
 
 struct rsi_downcall_data {
 	__u32	  sid_magic;
 	__u32	  sid_err;
-	__u32	  sid_hash;
+	__u32	  sid_unused;
 	__u32	  sid_maj_stat;
 	__u32	  sid_min_stat;
 	__u32	  sid_len;
 	__s64	  sid_offset;
+	__u64	  sid_hash;
 	/* sid_val contains in_handle, in_token,
 	 * out_handle, out_token
 	 */

lustre/ptlrpc/wiretest.c
@@ -4392,7 +4392,7 @@ void lustre_assert_wire_constants(void)
 #endif /* HAVE_SERVER_SUPPORT */
 
 	/* Checks for struct rsi_downcall_data */
-	LASSERTF((int)sizeof(struct rsi_downcall_data) == 32, "found %lld\n",
+	LASSERTF((int)sizeof(struct rsi_downcall_data) == 40, "found %lld\n",
 		 (long long)(int)sizeof(struct rsi_downcall_data));
 	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_magic) == 0, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_magic));
@@ -4402,10 +4402,10 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_err));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_err) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_err));
-	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_hash) == 8, "found %lld\n",
-		 (long long)(int)offsetof(struct rsi_downcall_data, sid_hash));
-	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_hash) == 4, "found %lld\n",
-		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_hash));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_unused) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_unused));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_unused) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_unused));
 	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_maj_stat) == 12, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_maj_stat));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_maj_stat) == 4, "found %lld\n",
@@ -4422,7 +4422,11 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_offset));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_offset) == 8, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_offset));
-	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_val) == 32, "found %lld\n",
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_hash) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_hash));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_hash) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_hash));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_val) == 40, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_val));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_val) == 0, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_val));

lustre/utils/gss/svcgssd_proc.c
@@ -177,7 +177,7 @@ out:
 
 #define RPCSEC_GSS_SEQ_WIN	5
 
-static int send_response(int auth_res, uint64_t hash,
+static int send_response(int auth_res, __u64 hash,
 			gss_buffer_desc *in_handle, gss_buffer_desc *in_token,
 			u_int32_t maj_stat, u_int32_t min_stat,
 			gss_buffer_desc *out_handle, gss_buffer_desc *out_token)
@@ -912,7 +912,8 @@ int handle_channel_request(int fd)
 		.maj_stat		= GSS_S_FAILURE,
 		.ctx			= GSS_C_NO_CONTEXT,
 	};
-	uint64_t hash = 0;
+	__u64 hash = 0;
+	__u64 tmp_lustre_svc = 0;
 
 	printerr(LL_INFO, "handling request\n");
 	if (readline(fd, &lbuf, &lbuflen) != 1) {
@@ -923,16 +924,17 @@ int handle_channel_request(int fd)
 	cp = lbuf;
 
 	/* see rsi_do_upcall() for the format of data being input here */
-	rc = gss_u64_read_string(&cp, (__u64 *)&hash);
+	rc = gss_u64_read_string(&cp, &hash);
 	if (rc < 0) {
 		printerr(LL_ERR, "ERROR: failed parsing request: hash\n");
 		goto out_err;
 	}
-	rc = gss_u64_read_string(&cp, (__u64 *)&snd.lustre_svc);
+	rc = gss_u64_read_string(&cp, &tmp_lustre_svc);
 	if (rc < 0) {
 		printerr(LL_ERR, "ERROR: failed parsing request: lustre svc\n");
 		goto out_err;
 	}
+	snd.lustre_svc = tmp_lustre_svc;
 	/* lustre_svc is the svc and gss subflavor */
 	lustre_mech = (snd.lustre_svc & LUSTRE_GSS_MECH_MASK) >>
 		LUSTRE_GSS_MECH_SHIFT;

lustre/utils/wirecheck.c
@@ -2052,11 +2052,12 @@ check_rsi_downcall_data(void)
 	CHECK_STRUCT(rsi_downcall_data);
 	CHECK_MEMBER(rsi_downcall_data, sid_magic);
 	CHECK_MEMBER(rsi_downcall_data, sid_err);
-	CHECK_MEMBER(rsi_downcall_data, sid_hash);
+	CHECK_MEMBER(rsi_downcall_data, sid_unused);
 	CHECK_MEMBER(rsi_downcall_data, sid_maj_stat);
 	CHECK_MEMBER(rsi_downcall_data, sid_min_stat);
 	CHECK_MEMBER(rsi_downcall_data, sid_len);
 	CHECK_MEMBER(rsi_downcall_data, sid_offset);
+	CHECK_MEMBER(rsi_downcall_data, sid_hash);
 	CHECK_MEMBER(rsi_downcall_data, sid_val);
 }
 

lustre/utils/wiretest.c
@@ -4453,7 +4453,7 @@ void lustre_assert_wire_constants(void)
 #endif /* HAVE_SERVER_SUPPORT */
 
 	/* Checks for struct rsi_downcall_data */
-	LASSERTF((int)sizeof(struct rsi_downcall_data) == 32, "found %lld\n",
+	LASSERTF((int)sizeof(struct rsi_downcall_data) == 40, "found %lld\n",
 		 (long long)(int)sizeof(struct rsi_downcall_data));
 	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_magic) == 0, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_magic));
@@ -4463,10 +4463,10 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_err));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_err) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_err));
-	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_hash) == 8, "found %lld\n",
-		 (long long)(int)offsetof(struct rsi_downcall_data, sid_hash));
-	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_hash) == 4, "found %lld\n",
-		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_hash));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_unused) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_unused));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_unused) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_unused));
 	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_maj_stat) == 12, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_maj_stat));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_maj_stat) == 4, "found %lld\n",
@@ -4483,7 +4483,11 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_offset));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_offset) == 8, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_offset));
-	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_val) == 32, "found %lld\n",
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_hash) == 32, "found %lld\n",
+		 (long long)(int)offsetof(struct rsi_downcall_data, sid_hash));
+	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_hash) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_hash));
+	LASSERTF((int)offsetof(struct rsi_downcall_data, sid_val) == 40, "found %lld\n",
 		 (long long)(int)offsetof(struct rsi_downcall_data, sid_val));
 	LASSERTF((int)sizeof(((struct rsi_downcall_data *)0)->sid_val) == 0, "found %lld\n",
 		 (long long)(int)sizeof(((struct rsi_downcall_data *)0)->sid_val));