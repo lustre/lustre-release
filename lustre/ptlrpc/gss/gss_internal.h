lustre/lustre-release

commit 8d828762d18ffaa2945bde56039127d0e75aceb3

LU-17015 gss: support large kerberos token for rpc sec init
If the current Kerberos setup is using large token, like when PAC
feature is enabled for Kerberos, authentication can fail due to server
side unable to exchange token between kernel and userspace.
This limitation is inherent to the sunrpc cache mechanism, that can
only handle tokens up to PAGE_SIZE.

For RPC sec init phase, use Lustre's upcall cache mechanism
instead of deprecated kernel's sunrpc cache. The upcall calls a new
userspace command 'l_getauth', that fowards the sec init request to
the lsvcgssd daemon via Unix domain sockets.

Test-Parameters: kerberos=true testlist=sanity-krb5
Change-Id: I709cd79894a5a13fc4cdfab2109c86f2230db3b8
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52224
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>

lustre/ptlrpc/gss/gss_internal.h
@@ -14,6 +14,7 @@
 #include <crypto/hash.h>
 #include <libcfs/libcfs_crypto.h>
 #include <lustre_sec.h>
+#include <upcall_cache.h>
 
 /*
  * rawobj stuff
@@ -138,43 +139,6 @@ static inline __u64 gss_handle_to_u64(rawobj_t *handle)
                 return -1;
         return *((__u64 *) handle->data);
 }
-
-#define GSS_SEQ_WIN                     (2048)
-#define GSS_SEQ_WIN_MAIN                GSS_SEQ_WIN
-#define GSS_SEQ_WIN_BACK                (128)
-#define GSS_SEQ_REPACK_THRESHOLD        (GSS_SEQ_WIN_MAIN / 2 + \
-                                         GSS_SEQ_WIN_MAIN / 4)
-
-struct gss_svc_seq_data {
-	spinlock_t		ssd_lock;
-        /*
-         * highest sequence number seen so far, for main and back window
-         */
-        __u32                   ssd_max_main;
-        __u32                   ssd_max_back;
-        /*
-         * main and back window
-         * for i such that ssd_max - GSS_SEQ_WIN < i <= ssd_max, the i-th bit
-         * of ssd_win is nonzero iff sequence number i has been seen already.
-         */
-        unsigned long           ssd_win_main[GSS_SEQ_WIN_MAIN/BITS_PER_LONG];
-        unsigned long           ssd_win_back[GSS_SEQ_WIN_BACK/BITS_PER_LONG];
-};
-
-struct gss_svc_ctx {
-        struct gss_ctx         *gsc_mechctx;
-        struct gss_svc_seq_data gsc_seqdata;
-        rawobj_t                gsc_rvs_hdl;
-        __u32                   gsc_rvs_seq;
-        uid_t                   gsc_uid;
-        gid_t                   gsc_gid;
-        uid_t                   gsc_mapped_uid;
-        unsigned int            gsc_usr_root:1,
-                                gsc_usr_mds:1,
-                                gsc_usr_oss:1,
-                                gsc_remote:1,
-                                gsc_reverse:1;
-};
 
 struct gss_svc_reqctx {
         struct ptlrpc_svc_ctx           src_base;
@@ -478,5 +442,14 @@ static inline unsigned int ll_read_key_usage(struct key *key)
 	return atomic_read(&key->usage);
 #endif
 }
+
+#define RSI_UPCALL_PATH "/usr/sbin/l_getauth"
+#define UC_RSICACHE_HASH_SIZE 64
+
+extern struct upcall_cache_ops rsi_upcall_cache_ops;
+extern struct upcall_cache *rsicache;
+struct gss_rsi *rsi_entry_get(struct upcall_cache *cache, struct gss_rsi *rsi);
+void rsi_entry_put(struct upcall_cache *cache, struct gss_rsi *rsi);
+void rsi_flush(struct upcall_cache *cache, int hash);
 
 #endif /* __PTLRPC_GSS_GSS_INTERNAL_H_ */

lustre/ptlrpc/gss/lproc_gss.c
@@ -181,6 +181,161 @@ static ssize_t sptlrpc_gss_check_upcall_ns_seq_write(struct file *file,
 LPROC_SEQ_FOPS(sptlrpc_gss_check_upcall_ns);
 #endif /* HAVE_GSS_KEYRING */
 
+static int rsi_upcall_seq_show(struct seq_file *m,
+			       void *data)
+{
+	down_read(&rsicache->uc_upcall_rwsem);
+	seq_printf(m, "%s\n", rsicache->uc_upcall);
+	up_read(&rsicache->uc_upcall_rwsem);
+
+	return 0;
+}
+
+static ssize_t rsi_upcall_seq_write(struct file *file,
+				    const char __user *buffer,
+				    size_t count, loff_t *off)
+{
+	int rc;
+
+	if (count >= UC_CACHE_UPCALL_MAXPATH) {
+		CERROR("%s: rsi upcall too long\n", rsicache->uc_name);
+		return -EINVAL;
+	}
+
+	/* Remove any extraneous bits from the upcall (e.g. linefeeds) */
+	down_write(&rsicache->uc_upcall_rwsem);
+	rc = sscanf(buffer, "%s", rsicache->uc_upcall);
+	up_write(&rsicache->uc_upcall_rwsem);
+
+	if (rc != 1) {
+		CERROR("%s: invalid rsi upcall provided\n", rsicache->uc_name);
+		return -EINVAL;
+	}
+
+	CDEBUG(D_CONFIG, "%s: rsi upcall set to %s\n", rsicache->uc_name,
+	       rsicache->uc_upcall);
+
+	return count;
+}
+LPROC_SEQ_FOPS(rsi_upcall);
+
+static ssize_t lprocfs_rsi_flush_seq_write(struct file *file,
+					   const char __user *buffer,
+					   size_t count, void *data)
+{
+	int hash, rc;
+
+	rc = kstrtoint_from_user(buffer, count, 0, &hash);
+	if (rc)
+		return rc;
+
+	rsi_flush(rsicache, hash);
+	return count;
+}
+LPROC_SEQ_FOPS_WR_ONLY(gss, rsi_flush);
+
+static ssize_t lprocfs_rsi_info_seq_write(struct file *file,
+					  const char __user *buffer,
+					  size_t count, void *data)
+{
+	struct rsi_downcall_data *param;
+	int size = sizeof(*param), rc, checked = 0;
+
+again:
+	if (count < size) {
+		CERROR("%s: invalid data count = %lu, size = %d\n",
+		       rsicache->uc_name, (unsigned long)count, size);
+		return -EINVAL;
+	}
+
+	OBD_ALLOC_LARGE(param, size);
+	if (param == NULL)
+		return -ENOMEM;
+
+	if (copy_from_user(param, buffer, size)) {
+		CERROR("%s: bad rsi data\n", rsicache->uc_name);
+		GOTO(out, rc = -EFAULT);
+	}
+
+	if (checked == 0) {
+		checked = 1;
+		if (param->sid_magic != RSI_DOWNCALL_MAGIC) {
+			CERROR("%s: rsi downcall bad params\n",
+			       rsicache->uc_name);
+			GOTO(out, rc = -EINVAL);
+		}
+
+		rc = param->sid_len; /* save sid_len */
+		OBD_FREE_LARGE(param, size);
+		size = offsetof(struct rsi_downcall_data, sid_val[rc]);
+		goto again;
+	}
+
+	rc = upcall_cache_downcall(rsicache, param->sid_err,
+				   param->sid_hash, param);
+
+out:
+	if (param != NULL)
+		OBD_FREE_LARGE(param, size);
+
+	return rc ? rc : count;
+}
+LPROC_SEQ_FOPS_WR_ONLY(gss, rsi_info);
+
+static int rsi_entry_expire_seq_show(struct seq_file *m,
+				     void *data)
+{
+	seq_printf(m, "%lld\n", rsicache->uc_entry_expire);
+	return 0;
+}
+
+static ssize_t rsi_entry_expire_seq_write(struct file *file,
+					  const char __user *buffer,
+					  size_t count, loff_t *off)
+{
+	time64_t val;
+	int rc;
+
+	rc = kstrtoll_from_user(buffer, count, 10, &val);
+	if (rc)
+		return rc;
+
+	if (val < 0)
+		return -ERANGE;
+
+	rsicache->uc_entry_expire = val;
+
+	return count;
+}
+LPROC_SEQ_FOPS(rsi_entry_expire);
+
+static int rsi_acquire_expire_seq_show(struct seq_file *m,
+				       void *data)
+{
+	seq_printf(m, "%lld\n", rsicache->uc_acquire_expire);
+	return 0;
+}
+
+static ssize_t rsi_acquire_expire_seq_write(struct file *file,
+					    const char __user *buffer,
+					    size_t count, loff_t *off)
+{
+	time64_t val;
+	int rc;
+
+	rc = kstrtoll_from_user(buffer, count, 10, &val);
+	if (rc)
+		return rc;
+
+	if (val < 0 || val > INT_MAX)
+		return -ERANGE;
+
+	rsicache->uc_acquire_expire = val;
+
+	return count;
+}
+LPROC_SEQ_FOPS(rsi_acquire_expire);
+
 static struct ldebugfs_vars gss_debugfs_vars[] = {
 	{ .name	=	"replays",
 	  .fops	=	&gss_proc_oos_fops	},
@@ -197,6 +352,16 @@ static struct lprocfs_vars gss_lprocfs_vars[] = {
 	{ .name	=	"gss_check_upcall_ns",
 	  .fops	=	&sptlrpc_gss_check_upcall_ns_fops },
 #endif
+	{ .name	=	"rsi_upcall",
+	  .fops	=	&rsi_upcall_fops },
+	{ .name =	"rsi_flush",
+	  .fops =	&gss_rsi_flush_fops },
+	{ .name =	"rsi_info",
+	  .fops =	&gss_rsi_info_fops },
+	{ .name	=	"rsi_entry_expire",
+	  .fops	=	&rsi_entry_expire_fops },
+	{ .name	=	"rsi_acquire_expire",
+	  .fops	=	&rsi_acquire_expire_fops },
 	{ NULL }
 };
 

libcfs/libcfs/crypto/fname.c
@@ -207,9 +207,6 @@ static inline int llcrypt_base64_decode(const char *src, int len, u8 *dst)
  * This base64 encoding is RFC 4648 compliant base64-url encoding.
  */
 
-static const char base64url_table[] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
 #define LLCRYPT_BASE64URL_CHARS(nbytes)	DIV_ROUND_UP((nbytes) * 4, 3)
 
 /**

libcfs/libcfs/crypto/llcrypt_private.h
@@ -18,6 +18,7 @@
 #include <libcfs/crypto/llcrypt.h>
 #include <crypto/hash.h>
 #include <lustre_disk.h>
+#include <uapi/linux/lustre/lgss.h>
 
 #ifndef CRYPTO_TFM_REQ_FORBID_WEAK_KEYS
 #define CRYPTO_TFM_REQ_FORBID_WEAK_KEYS CRYPTO_TFM_REQ_WEAK_KEY

lustre/include/lustre_sec.h
@@ -1154,12 +1154,49 @@ void sptlrpc_target_update_exp_flavor(struct obd_device *obd,
                                       struct sptlrpc_rule_set *rset);
 
 /*
- * reverse context
+ * context and reverse context
  */
+#define GSS_SEQ_WIN			(2048)
+#define GSS_SEQ_WIN_MAIN		GSS_SEQ_WIN
+#define GSS_SEQ_WIN_BACK		(128)
+#define GSS_SEQ_REPACK_THRESHOLD	(GSS_SEQ_WIN_MAIN / 2 + \
+					 GSS_SEQ_WIN_MAIN / 4)
+
+struct gss_svc_seq_data {
+	spinlock_t		ssd_lock;
+	/*
+	 * highest sequence number seen so far, for main and back window
+	 */
+	__u32			ssd_max_main;
+	__u32			ssd_max_back;
+	/*
+	 * main and back window
+	 * for i such that ssd_max - GSS_SEQ_WIN < i <= ssd_max, the i-th bit
+	 * of ssd_win is nonzero iff sequence number i has been seen already.
+	 */
+	unsigned long		ssd_win_main[GSS_SEQ_WIN_MAIN/BITS_PER_LONG];
+	unsigned long		ssd_win_back[GSS_SEQ_WIN_BACK/BITS_PER_LONG];
+};
+
+struct gss_svc_ctx {
+	struct gss_ctx	       *gsc_mechctx;
+	struct gss_svc_seq_data gsc_seqdata;
+	rawobj_t		gsc_rvs_hdl;
+	__u32			gsc_rvs_seq;
+	uid_t			gsc_uid;
+	gid_t			gsc_gid;
+	uid_t			gsc_mapped_uid;
+	unsigned int		gsc_usr_root:1,
+				gsc_usr_mds:1,
+				gsc_usr_oss:1,
+				gsc_remote:1,
+				gsc_reverse:1;
+};
+
 int sptlrpc_svc_install_rvs_ctx(struct obd_import *imp,
-                                struct ptlrpc_svc_ctx *ctx);
+				struct ptlrpc_svc_ctx *ctx);
 int sptlrpc_cli_install_rvs_ctx(struct obd_import *imp,
-                                struct ptlrpc_cli_ctx *ctx);
+				struct ptlrpc_cli_ctx *ctx);
 
 /* bulk security api */
 int sptlrpc_enc_pool_add_user(void);

lustre/include/uapi/linux/lustre/lgss.h
@@ -29,7 +29,23 @@
 #ifndef _LGSS_H
 #define _LGSS_H
 
+#ifndef __KERNEL__
+# define __USE_ISOC99	1
+# include <stdio.h> /* snprintf() */
+# include <stdlib.h> /* abs() */
+# include <inttypes.h> /* PRIu64 */
+# include <ctype.h> /* isascii() */
+# define __USE_GNU      1
+# define __USE_XOPEN2K8  1
+#else
+#include <linux/ctype.h>
+#define PRIu64 "llu"
+#define PRIx64 "llx"
+#endif /* !__KERNEL__ */
+
 #include <linux/types.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
 
 /*
  * sparse kernel source annotations
@@ -55,4 +71,434 @@ struct lgssd_ioctl_param {
 	__u64 reply_length;
 };
 
+#define GSS_SOCKET_PATH	"/tmp/svcgssd.socket"
+#define RSI_DOWNCALL_MAGIC	0x6d6dd62a
+#define RSI_DOWNCALL_PATH	"sptlrpc/gss/rsi_info"
+#define RSI_CACHE_NAME		"rsicache"
+
+struct rsi_downcall_data {
+	__u32	  sid_magic;
+	__u32	  sid_err;
+	__u32	  sid_hash;
+	__u32	  sid_maj_stat;
+	__u32	  sid_min_stat;
+	__u32	  sid_len;
+	__s64	  sid_offset;
+	/* sid_val contains in_handle, in_token,
+	 * out_handle, out_token
+	 */
+	char	  sid_val[0];
+};
+
+/*
+ * gss_string_write() - write some string
+ *
+ * If string is empty, write single digit 0.
+ * Pad with a trailing space.
+ */
+static inline void gss_string_write(char **dst, int *dstlen, const char *src)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	if (!strlen(src))
+		ret = snprintf(cp, *dstlen, "0");
+	else
+		ret = snprintf(cp, *dstlen, "%s", src);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write() - write some u64
+ */
+static inline void gss_u64_write_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "%"PRIu64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_u64_write_hex() - write some u64 in hex
+ */
+static inline void gss_u64_write_hex_string(char **dst, int *dstlen, uint64_t n)
+{
+	char *cp = *dst;
+	int ret;
+
+	if (*dstlen < 0)
+		return;
+
+	ret = snprintf(cp, *dstlen, "0x%"PRIx64, n);
+	if (ret >= *dstlen) {
+		cp += *dstlen;
+		*dstlen = -1;
+	} else {
+		cp[ret] = ' ';
+		cp += ret + 1;
+		*dstlen -= ret + 1;
+	}
+	*dst = cp;
+}
+
+/*
+ * gss_buffer_write() - write some buffer
+ */
+static inline void gss_buffer_write(char **dst, int *dstlen,
+				    const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	/* write size of data */
+	p = (__u32 *)cp;
+	*p = srclen;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+	if (!srclen)
+		goto out;
+
+	/* write data itself */
+	while (srclen && len) {
+		*cp++ = *src++;
+		len--;
+		srclen--;
+	}
+	if (!len && srclen)
+		len = -1;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_u32_write() - write some u32
+ */
+static inline void gss_u32_write(char **dst, int *dstlen, __u32 val)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 *p;
+
+	if (len < 0)
+		return;
+
+	if (len < sizeof(__u32)) {
+		len = -1;
+		goto out;
+	}
+
+	p = (__u32 *)cp;
+	*p = val;
+	cp += sizeof(__u32);
+	len -= sizeof(__u32);
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+static const char base64url_table[] =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+#define BASE64URL_CHARS(nbytes) ((((nbytes) * 4) + 3 - 1) / 3)
+
+/*
+ * gss_base64url_encode() - base64url-encode some binary data
+ *
+ * Encode data using base64url encoding, i.e. the "Base 64 Encoding with URL
+ * and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't used,
+ * as it's unneeded and not required by the RFC.
+ * Pad with a trailing space.
+ */
+static inline void gss_base64url_encode(char **dst, int *dstlen,
+					const __u8 *src, int srclen)
+{
+	char *cp = *dst;
+	int len = *dstlen;
+	__u32 ac = 0;
+	int bits = 0;
+	int i;
+
+	if (len < 0)
+		return;
+
+	if (!srclen)
+		return gss_string_write(dst, dstlen, "");
+
+	for (i = 0; i < srclen; i++) {
+		if (!len)
+			break;
+		ac = (ac << 8) | src[i];
+		bits += 8;
+		do {
+			bits -= 6;
+			*cp++ = base64url_table[(ac >> bits) & 0x3f];
+			len--;
+		} while (bits >= 6 && len > 0);
+	}
+	if (i < srclen) {
+		len = -1;
+		goto out;
+	}
+
+	if (bits) {
+		*cp++ = base64url_table[(ac << (6 - bits)) & 0x3f];
+		len--;
+	}
+
+	if (!len) {
+		len = -1;
+		goto out;
+	}
+	*cp++ = ' ';
+	len--;
+
+out:
+	*dst = cp;
+	*dstlen = len;
+}
+
+/*
+ * gss_base64url_decode() - base64url-decode a string
+ *
+ * Decode a string using base64url encoding, i.e. the "Base 64 Encoding with
+ * URL and Filename Safe Alphabet" specified by RFC 4648.  '='-padding isn't
+ * accepted, nor are non-encoding characters such as whitespace.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_base64url_decode(char **src, char *dst, int destsize)
+{
+	int bits = 0, len = 0;
+	char *cp = *src, *p;
+	char *bp = dst;
+	__u32 ac = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (*cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n' || *(cp + 1) == '\0')) {
+		cp++;
+		goto fini;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n' || *cp == '\0')
+			break;
+
+		p = strchr(base64url_table, *cp);
+		if (len > destsize || p == NULL || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		cp++;
+		ac = (ac << 6) | (p - base64url_table);
+		bits += 6;
+		if (bits >= 8) {
+			bits -= 8;
+			*bp++ = (__u8)(ac >> bits);
+			len++;
+		}
+	}
+
+	if (!isascii(*cp) || (ac & ((1 << bits) - 1))) {
+		len = -1;
+		goto out;
+	}
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_string_read() - read some string
+ *
+ * An empty string is represented with the single digit 0.
+ * String end is marked with a trailing space or '\n' or '\0'.
+ */
+static inline int gss_string_read(char **src, char *dst, int destsize,
+				  int allowzero)
+{
+	char *cp = *src;
+	char *bp = dst;
+	int len = 0;
+
+	while (*cp == ' ')
+		cp++;
+
+	/* the single digit 0 is inserted if field is empty */
+	if (!allowzero && *cp == '0' &&
+	    (*(cp + 1) == ' ' || *(cp + 1) == '\n')) {
+		cp++;
+		goto out;
+	}
+
+	while (isascii(*cp)) {
+		if (*cp == ' ' || *cp == '\n')
+			break;
+
+		if (len >= destsize || *cp == '\0') {
+			len = -1;
+			goto out;
+		}
+
+		*(bp++) = *(cp++);
+		len++;
+	}
+
+	if (!isascii(*cp)) {
+		len = -1;
+		goto out;
+	}
+
+	*src = cp;
+
+out:
+	return len;
+}
+
+#ifndef __KERNEL__
+/*
+ * gss_u64_read() - read some u64
+ */
+static inline int gss_u64_read_string(char **src, __u64 *n)
+{
+	char buf[24];
+	char *ep;
+	int ret;
+
+	ret = gss_string_read(src, buf, sizeof(buf), 1);
+	if (ret < 0)
+		return ret;
+
+	buf[ret] = '\0';
+	*n = strtoull(buf, &ep, 0);
+	if (*ep)
+		return -1;
+
+	return 0;
+}
 #endif
+
+/*
+ * gss_buffer_read() - read some buffer
+ */
+static inline int gss_buffer_read(char **src, char *dst, int destsize)
+{
+	char *cp = *src;
+	char *bp = dst;
+	__u32 *p;
+	int len, size;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	len = *p;
+	cp += sizeof(__u32);
+
+	if (len > destsize) {
+		len = -1;
+		goto out;
+	}
+
+	if (!len)
+		goto fini;
+
+	/* read data itself */
+	size = len;
+	while (size && destsize) {
+		*(bp++) = *(cp++);
+		destsize--;
+		size--;
+	}
+	if (!destsize && size)
+		len = -1;
+
+fini:
+	*src = cp;
+out:
+	return len;
+}
+
+/*
+ * gss_buffer_get() - get reference to gss buffer
+ */
+static inline int gss_buffer_get(char **src, __u32 *len, __u8 **data)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	/* read data size */
+	p = (__u32 *)cp;
+	*len = *p;
+	cp += sizeof(__u32);
+
+	/* point to data buf */
+	if (!*len)
+		*data = NULL;
+	else
+		*data = (__u8 *)cp;
+
+	/* move forward */
+	cp += *len;
+
+	*src = cp;
+	return *len;
+}
+
+/*
+ * gss_u32_read() - read some u32
+ */
+static inline int gss_u32_read(char **src, __u32 *val)
+{
+	char *cp = *src;
+	__u32 *p;
+
+	p = (__u32 *)cp;
+	*val = *p;
+	cp += sizeof(__u32);
+
+	*src = cp;
+	return 0;
+}
+
+#endif /* _LGSS_H */

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -255,8 +255,6 @@ enum nodemap_idx_type {
 	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
 };
 
-#define LUSTRE_NODEMAP_NAME_LENGTH	16
-
 /* lu_nodemap flags */
 enum nm_flag_bits {
 	NM_FL_ALLOW_ROOT_ACCESS = 0x1,

commit 3615fa4a86be793652d53c94818c5aeb81e2257e

LU-17317 gss: no cache flush for rsi and rsc
RPCSEC init and RPCSEC context caches hold gss-related information
of security contexts established between network peers. These cache
entries are tightly coupled with contexts handled in the sptlrpc layer
so they must not be purged directly. They are inserted into the cache
when sptlrpc security contexts are established, and removed when the
corresponding security contexts are destroyed.

Test-Parameters: trivial
Test-Parameters: kerberos=true testlist=sanity-krb5
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Fixes: 8d828762d1 ("LU-17015 gss: support large kerberos token for rpc sec init")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I903f75a4b5229286fcaed3e9d96b5eee7f653f15
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53377
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/gss/gss_internal.h
@@ -440,14 +440,12 @@ extern struct upcall_cache_ops rsi_upcall_cache_ops;
 extern struct upcall_cache *rsicache;
 struct gss_rsi *rsi_entry_get(struct upcall_cache *cache, struct gss_rsi *rsi);
 void rsi_entry_put(struct upcall_cache *cache, struct gss_rsi *rsi);
-void rsi_flush(struct upcall_cache *cache, int hash);
 #define RSC_UPCALL_PATH "NONE"
 #define UC_RSCCACHE_HASH_SIZE 1024
 extern struct upcall_cache_ops rsc_upcall_cache_ops;
 extern struct upcall_cache *rsccache;
 struct gss_rsc *rsc_entry_get(struct upcall_cache *cache, struct gss_rsc *rsc);
 void rsc_entry_put(struct upcall_cache *cache, struct gss_rsc *rsc);
-void rsc_flush(struct upcall_cache *cache, int hash);
 void __rsc_free(struct gss_rsc *rsc);
 
 #endif /* __PTLRPC_GSS_GSS_INTERNAL_H_ */

lustre/ptlrpc/gss/gss_svc_upcall.c
@@ -431,14 +431,6 @@ void rsi_entry_put(struct upcall_cache *cache, struct gss_rsi *rsi)
 	upcall_cache_put_entry(cache, rsi->si_uc_entry);
 }
 
-void rsi_flush(struct upcall_cache *cache, int hash)
-{
-	if (hash < 0)
-		upcall_cache_flush_idle(cache);
-	else
-		upcall_cache_flush_one(cache, (__u64)hash, NULL);
-}
-
 struct upcall_cache_ops rsi_upcall_cache_ops = {
 	.init_entry	  = rsi_entry_init,
 	.free_entry	  = rsi_entry_free,
@@ -635,14 +627,6 @@ void rsc_entry_put(struct upcall_cache *cache, struct gss_rsc *rsc)
 		return;
 
 	upcall_cache_put_entry(cache, rsc->sc_uc_entry);
-}
-
-void rsc_flush(struct upcall_cache *cache, int hash)
-{
-	if (hash < 0)
-		upcall_cache_flush_idle(cache);
-	else
-		upcall_cache_flush_one(cache, (__u64)hash, NULL);
 }
 
 struct upcall_cache_ops rsc_upcall_cache_ops = {

lustre/ptlrpc/gss/lproc_gss.c
@@ -231,21 +231,6 @@ out:
 }
 LPROC_SEQ_FOPS(rsi_upcall);
 
-static ssize_t lprocfs_rsi_flush_seq_write(struct file *file,
-					   const char __user *buffer,
-					   size_t count, void *data)
-{
-	int hash, rc;
-
-	rc = kstrtoint_from_user(buffer, count, 0, &hash);
-	if (rc)
-		return rc;
-
-	rsi_flush(rsicache, hash);
-	return count;
-}
-LPROC_SEQ_FOPS_WR_ONLY(gss, rsi_flush);
-
 static ssize_t lprocfs_rsi_info_seq_write(struct file *file,
 					  const char __user *buffer,
 					  size_t count, void *data)
@@ -347,21 +332,6 @@ static ssize_t rsi_acquire_expire_seq_write(struct file *file,
 	return count;
 }
 LPROC_SEQ_FOPS(rsi_acquire_expire);
-
-static ssize_t lprocfs_rsc_flush_seq_write(struct file *file,
-					   const char __user *buffer,
-					   size_t count, void *data)
-{
-	int hash, rc;
-
-	rc = kstrtoint_from_user(buffer, count, 0, &hash);
-	if (rc)
-		return rc;
-
-	rsc_flush(rsccache, hash);
-	return count;
-}
-LPROC_SEQ_FOPS_WR_ONLY(gss, rsc_flush);
 
 static ssize_t lprocfs_rsc_info_seq_write(struct file *file,
 					  const char __user *buffer,
@@ -476,16 +446,12 @@ static struct lprocfs_vars gss_lprocfs_vars[] = {
 #endif
 	{ .name	=	"rsi_upcall",
 	  .fops	=	&rsi_upcall_fops },
-	{ .name =	"rsi_flush",
-	  .fops =	&gss_rsi_flush_fops },
 	{ .name =	"rsi_info",
 	  .fops =	&gss_rsi_info_fops },
 	{ .name	=	"rsi_entry_expire",
 	  .fops	=	&rsi_entry_expire_fops },
 	{ .name	=	"rsi_acquire_expire",
 	  .fops	=	&rsi_acquire_expire_fops },
-	{ .name =	"rsc_flush",
-	  .fops =	&gss_rsc_flush_fops },
 	{ .name =	"rsc_info",
 	  .fops =	&gss_rsc_info_fops },
 	{ NULL }