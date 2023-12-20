lustre/lustre-release

commit cb5f92c0e38eaca4b76f17356ba6d7817c220f56

LU-10391 ksocklnd: use ksocknal_protocol v4 for IPv6
During testing of IPv6 I encountered the following error:

LNetError: 11c-c: Protocol error connecting to ...

This was due to the ksocklnd protocol not being set to the correct
version when using IPv6 addresses. Test if we have a IPv6 peer and
set the proper protocol.

Test-Parameters: trivial testlist=sanity-lnet
Change-Id: I19183e904acee4cb8b9d3b7e77284c81f6cdc2b4
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52911
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd_cb.c
@@ -1807,21 +1807,29 @@ ksocknal_recv_hello(struct lnet_ni *ni, struct ksock_conn *conn,
 	proto = ksocknal_parse_proto_version(hello);
 	if (proto == NULL) {
 		if (!active) {
+			struct sockaddr_in *psa = (void *)&conn->ksnc_peeraddr;
+
 			/* unknown protocol from peer_ni,
 			 * tell peer_ni my protocol.
 			 */
-			conn->ksnc_proto = &ksocknal_protocol_v3x;
+			if (psa->sin_family == AF_INET6)
+				conn->ksnc_proto = &ksocknal_protocol_v4x;
+			else if (psa->sin_family == AF_INET)
+				conn->ksnc_proto = &ksocknal_protocol_v3x;
 #if SOCKNAL_VERSION_DEBUG
 			if (*ksocknal_tunables.ksnd_protocol == 2)
 				conn->ksnc_proto = &ksocknal_protocol_v2x;
 			else if (*ksocknal_tunables.ksnd_protocol == 1)
 				conn->ksnc_proto = &ksocknal_protocol_v1x;
 #endif
+			if (!conn->ksnc_proto)
+				goto unknown;
+
 			hello->kshm_nips = 0;
 			ksocknal_send_hello(ni, conn, &ni->ni_nid,
 					    hello);
 		}
-
+unknown:
 		CERROR("Unknown protocol version (%d.x expected) from %pISc\n",
 		       conn->ksnc_proto->pro_version, &conn->ksnc_peeraddr);
 

lnet/klnds/socklnd/socklnd.c
@@ -934,8 +934,9 @@ ksocknal_create_conn(struct lnet_ni *ni, struct ksock_conn_cb *conn_cb,
 	 * Passive connections use the listener timeout since the peer_ni sends
 	 * eagerly
 	 */
-
 	if (active) {
+		struct sockaddr_in *psa = (void *)&conn->ksnc_peeraddr;
+
 		peer_ni = conn_cb->ksnr_peer;
 		LASSERT(ni == peer_ni->ksnp_ni);
 
@@ -948,13 +949,20 @@ ksocknal_create_conn(struct lnet_ni *ni, struct ksock_conn_cb *conn_cb,
 		write_unlock_bh(global_lock);
 
 		if (conn->ksnc_proto == NULL) {
-			conn->ksnc_proto = &ksocknal_protocol_v3x;
+			if (psa->sin_family == AF_INET6)
+				conn->ksnc_proto = &ksocknal_protocol_v4x;
+			else if (psa->sin_family == AF_INET)
+				conn->ksnc_proto = &ksocknal_protocol_v3x;
 #if SOCKNAL_VERSION_DEBUG
 			if (*ksocknal_tunables.ksnd_protocol == 2)
 				conn->ksnc_proto = &ksocknal_protocol_v2x;
 			else if (*ksocknal_tunables.ksnd_protocol == 1)
 				conn->ksnc_proto = &ksocknal_protocol_v1x;
 #endif
+		}
+		if (!conn->ksnc_proto) {
+			rc = -EPROTO;
+			goto failed_1;
 		}
 
 		rc = ksocknal_send_hello(ni, conn, &peerid.nid, hello);

commit c5685cc9a61e8cffdbb7e3e2a17a9b0bcf4a8a9b

LU-17000 lnet: Fix dereference after NULL under ksocknal_recv_hello
This patch fixes 'conn->ksnc_proto' which was
dereferenced under function ksocknal_recv_hello()
even though it could be NULL.

This patch also removes 'returns' in between
the function and replaces it with 'goto'.
Allowing exit from a single place.

CoverityID: 410244 ("Dereference after null check")
Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Fixes: cb5f92c0e (LU-10391 ksocklnd: use ksocknal_protocol v4 for IPv6)
Change-Id: I95196d481b537281ab8643f1ee6162db450bef20
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53305
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd_cb.c
@@ -1794,7 +1794,7 @@ ksocknal_recv_hello(struct lnet_ni *ni, struct ksock_conn *conn,
 		CERROR("Error %d reading HELLO from %pISc\n",
 		       rc, &conn->ksnc_peeraddr);
 		LASSERT(rc < 0);
-		return rc;
+		goto out_fatal;
 	}
 
 	if (hello->kshm_magic != LNET_PROTO_MAGIC &&
@@ -1804,7 +1804,7 @@ ksocknal_recv_hello(struct lnet_ni *ni, struct ksock_conn *conn,
 		CERROR("Bad magic(1) %#08x (%#08x expected) from %pISc\n",
 		       __cpu_to_le32 (hello->kshm_magic),
 		       LNET_PROTO_TCP_MAGIC, &conn->ksnc_peeraddr);
-		return -EPROTO;
+		goto out_unknown;
 	}
 
 	rc = lnet_sock_read(sock, &hello->kshm_version,
@@ -1813,7 +1813,7 @@ ksocknal_recv_hello(struct lnet_ni *ni, struct ksock_conn *conn,
 		CERROR("Error %d reading HELLO from %pISc\n",
 		       rc, &conn->ksnc_peeraddr);
 		LASSERT(rc < 0);
-		return rc;
+		goto out_fatal;
 	}
 
 	proto = ksocknal_parse_proto_version(hello);
@@ -1834,18 +1834,22 @@ ksocknal_recv_hello(struct lnet_ni *ni, struct ksock_conn *conn,
 			else if (*ksocknal_tunables.ksnd_protocol == 1)
 				conn->ksnc_proto = &ksocknal_protocol_v1x;
 #endif
-			if (!conn->ksnc_proto)
-				goto unknown;
+			if (!conn->ksnc_proto) {
+				CERROR("Unknown protocol.Error reading HELLO from %pISc\n",
+				       &conn->ksnc_peeraddr);
+				goto out_unknown;
+			}
 
 			hello->kshm_nips = 0;
 			ksocknal_send_hello(ni, conn, &ni->ni_nid,
 					    hello);
 		}
-unknown:
 		CERROR("Unknown protocol version (%d.x expected) from %pISc\n",
 		       conn->ksnc_proto->pro_version, &conn->ksnc_peeraddr);
-
-		return -EPROTO;
+out_unknown:
+		rc = -EPROTO;
+out_fatal:
+		return rc;
 	}
 
 	proto_match = (conn->ksnc_proto == proto);