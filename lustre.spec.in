lustre/lustre-release

commit 67cd54d05d0ced81cd7c2c40e58b1707e89bc4fa

LU-16967 build: Separate lnet LND rpm packaging
Enable separate packaging of lnet lnd kernel modules into
separate packages.

Use --with multiple_lnds to enable separate packages for
lnet lnds:
  [always builds]: kmod-lustre-lnet-socklnd for socklnd.ko
  --with o2ib: for kmod-lustre-lnet-in-kernel-o2iblnd
         ko2iblnd.ko -> in-kernel-ko2iblnd.ko
  --with mofed: kmod-lustre-lnet-o2iblnd for ko2iblnd.ko
  --with kfi: kmod-lustre-lnet-gnilnd for kgnilnd.ko
  --with gni: kmod-lustre-lnet-kfilnd for kkfilnd.ko

Test-Parameters: trivial
HPE-bug-id: LUS-11711
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: If2dace7ced96be2a2194f66362e9419b017c625f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51692
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

rpm/kmp-lnet-o2iblnd.preamble
@@ -1,0 +1,8 @@
+Summary:        Lustre LNet o2iblnd for 3rd party OFED
+License:        GPL-2.0-only
+%if 0%{?suse_version} > 1
+Requires:       kernel-%1
+%endif
+Provides:       %{name}-lnet-o2iblnd = %{version}
+Obsoletes:      %{name}-lnet-o2iblnd < %{version}
+Conflicts:      %{name}-lnet-in-kernel-o2iblnd

lustre.spec.in
@@ -21,10 +21,18 @@
 %bcond_without manpages
 %bcond_without shared
 %bcond_without static
-%bcond_with    systemd
-%bcond_with mofed
+%bcond_with systemd
 %bcond_without mpi
 %bcond_with kabi
+%bcond_with multiple_lnds
+%bcond_with mofed
+%bcond_without o2ib
+%bcond_with kfi
+%bcond_with gni
+
+%if %{with multiple_lnds}
+%global enable_multi_lnds 1
+%endif
 
 # LUTF Turn off brp-python-precompile script as we don't want the python files
 # to be compiled on installation
@@ -209,6 +217,16 @@ Source7: kmp-lustre-tests.preamble
 Source8: kmp-lustre-tests.files
 Source9: find-provides
 Source10: find-provides.ksyms
+Source11: kmp-lnet-socklnd.preamble
+Source12: kmp-lnet-socklnd.files
+Source13: kmp-lnet-o2iblnd.preamble
+Source14: kmp-lnet-o2iblnd.files
+Source15: kmp-lnet-gnilnd.preamble
+Source16: kmp-lnet-gnilnd.files
+Source17: kmp-lnet-kfilnd.preamble
+Source18: kmp-lnet-kfilnd.files
+Source19: kmp-lnet-in-kernel-o2iblnd.preamble
+Source20: kmp-lnet-in-kernel-o2iblnd.files
 URL: https://wiki.whamcloud.com/
 BuildRoot: %{_tmppath}/lustre-%{version}-root
 %if %{with lustre_modules}
@@ -440,6 +458,35 @@ to be used by the Lustre testing framework.
 %endif
 %endif
 
+%if %{with multiple_lnds}
+# begin multiple_lnds
+%kernel_module_package -n %{name}-lnet-socklnd -p %SOURCE11 -f %SOURCE12 %{_flavor}
+%if %{with o2ib}
+## re-write post/preun generated by kmodtool [ls -s and rm -f]
+%define inkernmod %{modules_fs_path}/%{lustre_name}/net/in-kernel-ko2iblnd.ko
+%define inkernsym %{modules_fs_path}/%{lustre_name}/net/ko2iblnd.ko
+%define ofed_module_package %{expand:%(
+TMPFILE=`mktemp`
+cat <<EOF > $TMPFILE
+%kernel_module_package -n %{name}-lnet-in-kernel-o2iblnd -p %SOURCE19 -f %SOURCE20 %{_flavor}
+EOF
+sed -i -e '/^%%post.*/a ln -sf %{inkernmod} %{inkernsym}' -e '/^%%preun.*/a rm -f %{inkernsym}' $TMPFILE
+echo $TMPFILE
+)}
+%{expand:%(cat '%{ofed_module_package}')}
+%endif
+%if %{with mofed}
+%kernel_module_package -n %{name}-lnet-o2iblnd -p %SOURCE13 -f %SOURCE14 %{_flavor}
+%endif
+%if %{with gni}
+%kernel_module_package -n %{name}-lnet-gnilnd -p %SOURCE15 -f %SOURCE16 %{_flavor}
+%endif
+%if %{with kfi}
+%kernel_module_package -n %{name}-lnet-kfilnd -p %SOURCE17 -f %SOURCE18 %{_flavor}
+%endif
+# end multiple_lnds
+%endif
+
 %if %{with lustre_iokit}
 %package -n lustre-iokit
 Summary: Collection of benchmark tools for a cluster with the Lustre file system
@@ -560,6 +607,7 @@ fi
 # mpi
 %endif
 
+WITH_O2IB=""
 %if %{with mofed}
 	o2ib_path=$(find /usr/src/ofa_kernel/ -type d -name ofed_scripts | xargs dirname)
 	if [ -z "$o2ib_path" ]; then
@@ -570,6 +618,11 @@ fi
 		echo "ERROR: multiple OFED versions installed"
 		exit 1
 	fi
+	WITH_O2IB="--with-o2ib=${o2ib_path}"
+%else
+%if %{without o2ib}
+	WITH_O2IB="--with-o2ib=no"
+%endif
 %endif
 
 %eval_configure $CONFIGURE_ARGS \
@@ -587,7 +640,10 @@ fi
 	%{!?with_manpages:--disable-manpages} \
 	%{!?with_systemd:--with-systemdsystemunitdir=no} \
 	%{?with_systemd:--with-systemdsystemunitdir=%{_unitdir}} \
-	%{?with_mofed:--with-o2ib=$o2ib_path} \
+	%{?with_multiple_lnds:--enable-multiple-lnds} \
+	%{?with_kfi:--with-kfi=/usr/src/cray-kfabric/default} \
+	%{?with_gni:--enable-gni} \
+	${WITH_O2IB} \
 	--with-linux=%{kdir} \
 	--with-linux-obj=%{kobjdir} \
 	--with-kmp-moddir=%{kmoddir}/%{name}

config/lustre-build.m4
@@ -530,6 +530,31 @@ elif test x$enable_crypto = xno ; then
 	RPMBINARGS="$RPMBINARGS --without crypto"
 	AC_SUBST(ENABLE_CRYPTO, no)
 fi
+if test x$enable_multiple_lnds = xyes ; then
+	RPMBINARGS="$RPMBINARGS --with multiple_lnds"
+else
+	RPMBINARGS="$RPMBINARGS --without multiple_lnds"
+fi
+if test x$EXTERNAL_KO2IBLND = xyes ; then
+	RPMBINARGS="$RPMBINARGS --with mofed"
+else
+	RPMBINARGS="$RPMBINARGS --without mofed"
+fi
+if test x$BUILT_IN_KO2IBLND = xyes ; then
+	RPMBINARGS="$RPMBINARGS --with o2ib"
+else
+	RPMBINARGS="$RPMBINARGS --without o2ib"
+fi
+if test x$KFILND = xkfilnd ; then
+	RPMBINARGS="$RPMBINARGS --with kfi"
+else
+	RPMBINARGS="$RPMBINARGS --without kfi"
+fi
+if test x$ENABLE_GNI = xyes ; then
+	RPMBINARGS="$RPMBINARGS --with gni"
+else
+	RPMBINARGS="$RPMBINARGS --without gni"
+fi
 if test x$enable_iokit != xyes ; then
 	RPMBINARGS="$RPMBINARGS --without lustre_iokit"
 fi

rpm/kmp-lnet-gnilnd.files
@@ -1,0 +1,3 @@
+%defattr(-,root,root)
+%dir %{modules_fs_path}/%{lustre_name}/net
+%{modules_fs_path}/%{lustre_name}/net/kgnilnd.ko

rpm/kmp-lnet-gnilnd.preamble
@@ -1,0 +1,7 @@
+Summary:        Lustre LNet gnilnd
+License:        GPL-2.0-only
+%if 0%{?suse_version} > 1
+Requires:       kernel-%1
+%endif
+Provides:       %{name}-lnet-gnilnd = %{version}
+Obsoletes:      %{name}-lnet-gnilnd < %{version}

rpm/kmp-lnet-in-kernel-o2iblnd.files
@@ -1,0 +1,3 @@
+%defattr(-,root,root)
+%dir %{modules_fs_path}/%{lustre_name}/net
+%{modules_fs_path}/%{lustre_name}/net/in-kernel-ko2iblnd.ko

rpm/kmp-lnet-in-kernel-o2iblnd.preamble
@@ -1,0 +1,10 @@
+Summary:        Lustre LNet o2iblnd for in-kernel OFED
+License:        GPL-2.0-only
+%if 0%{?suse_version} > 1
+Requires:       kernel-%1
+%endif
+Provides:       %{name}-lnet-in-kernel-o2iblnd = %{version}
+Obsoletes:      %{name}-lnet-in-kernel-o2iblnd < %{version}
+# Common package name
+Provides:       %{name}-lnet-o2iblnd = %{version}
+Obsoletes:      %{name}-lnet-o2iblnd < %{version}

rpm/kmp-lnet-kfilnd.files
@@ -1,0 +1,3 @@
+%defattr(-,root,root)
+%dir %{modules_fs_path}/%{lustre_name}/net
+%{modules_fs_path}/%{lustre_name}/net/kkfilnd.ko

commit 3c66185c84c94c47f5d8031b92165c68ef666e0e

LU-17398 build: detect mlnx-ofa_kernel-devel contents
Parse the configure_args for --with-o2ib and allow the
user specified path to override the mofed defaults.

Further when mlnx-ofa_kernel-devel contents are available silence
the BuildRequires: to allow for an mlnx source installation to
satisfy the lustre build requirements.

In addition move the mlnx specfic requirements to the mofed lnd
when '--with multiple_lnds' is enabled.

Fixes: 67cd54d05d ("LU-16967 build: Separate lnet LND rpm packaging")
Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I30c6b3a196021634c621f6f6c556bf32f28faeed
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53613
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

rpm/kmp-lnet-o2iblnd.preamble
@@ -3,6 +3,14 @@ License:        GPL-2.0-only
 %if 0%{?suse_version} > 1
 Requires:       kernel-%1
 %endif
+%if 0%{?mofed_source} > 0
+BuildRequires:  mlnx-ofa_kernel-devel
+%endif
+%if "%{_vendor}" == "redhat" || "%{_vendor}" == "openEuler"
+Requires:       kmod-mlnx-ofa_kernel
+%else
+Requires:       mlnx-ofa_kernel-kmp
+%endif
 Provides:       %{name}-lnet-o2iblnd = %{version}
 Obsoletes:      %{name}-lnet-o2iblnd < %{version}
 Conflicts:      %{name}-lnet-in-kernel-o2iblnd

lustre.spec.in
@@ -195,6 +195,16 @@
 %define with_systemd 1
 %endif
 
+# Detect when mlnx-ofa_kernel-devel contents exist and mofed_source to 0
+%define mofed_src_path	$(echo %{?configure_args} | tr ' ' '\\n' | grep -- '--with-o2ib=' | cut -c 13-)
+%define mofed_source	%(test -f %mofed_src_path/Module.symvers; echo $?)
+# Update the search path for mofed_scripts
+%if 0%{?mofed_source} > 0
+%define mofed_default_path	/usr/src/ofa_kernel/
+%else
+%define mofed_default_path	%{mofed_src_path}
+%endif
+
 %{!?mpi_name: %global mpi_name openmpi}
 
 Summary: Lustre File System
@@ -281,11 +291,15 @@ BuildRequires: pkgconfig(libselinux)
 %endif
 %if %{with lustre_modules}
 %if %{with mofed}
+%if 0%{?mofed_source} > 0
 BuildRequires: mlnx-ofa_kernel-devel
+%endif
+%if %{with multiple_lnds}
 %if "%{_vendor}" == "redhat" || "%{_vendor}" == "openEuler"
 Requires: kmod-mlnx-ofa_kernel
 %else
 Requires: mlnx-ofa_kernel-kmp
+%endif
 %endif
 %endif
 %if 0%{?rhel} >= 8 || "%{_vendor}" == "openEuler"
@@ -609,7 +623,7 @@ fi
 
 WITH_O2IB=""
 %if %{with mofed}
-	o2ib_path=$(find /usr/src/ofa_kernel/ -type d -name ofed_scripts | xargs dirname)
+	o2ib_path=$(find %{mofed_default_path} -type d -name ofed_scripts | xargs dirname)
 	if [ -z "$o2ib_path" ]; then
 		echo "ERROR: could not find OFED devel headers"
 		exit 1