lustre/lustre-release

commit 7154244354e3275f6a68f7581f7cfe9ef0a6507c

LU-16786 utils: Replace open call to WANT_FD
Replace open call to WANT_FD with newly added API
llapi_root_path_open() which was added under LU-16427

Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I97d55321cf32e40eaf7d6284c47f313199a6c406
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50764
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/utils/liblustreapi_fid.c
@@ -233,18 +233,13 @@ int llapi_fid2path(const char *path_or_device, const char *fidstr, char *path,
 		goto out;
 	}
 
-	if (path_or_device[0] == '/')
-		rc = get_root_path(WANT_FD, NULL, &mnt_fd,
-				   (char *)path_or_device, -1, NULL, NULL);
-	else
-		rc = get_root_path(WANT_FD, (char *)path_or_device,
-				   &mnt_fd, NULL, -1, NULL, NULL);
-
+	rc = llapi_root_path_open(path_or_device, &mnt_fd);
 	if (rc < 0)
 		goto out;
 
 	/* mnt_fd is cached internally, no need to close it */
 	rc = llapi_fid2path_at(mnt_fd, &fid, path, pathlen, recno, linkno);
+	close(mnt_fd);
 
 out:
 	return rc;
@@ -479,8 +474,8 @@ int llapi_open_by_fid(const char *lustre_dir, const struct lu_fid *fid,
 
 	/* this will return a cached FD if available, so only one open needed.
 	 * WANT_FD doesn't modify lustre_dir so casting away "const" is OK */
-	rc = get_root_path(WANT_FD, NULL, &mnt_fd, (char *)lustre_dir, 0, NULL,
-			   NULL);
+
+	rc = llapi_root_path_open(lustre_dir, &mnt_fd);
 	if (rc)
 		goto out;
 

lustre/utils/liblustreapi_hsm.c
@@ -1647,7 +1647,7 @@ int llapi_hsm_request(const char *path, const struct hsm_user_request *request)
 	int rc;
 	int fd;
 
-	rc = get_root_path(WANT_FD, NULL, &fd, (char *)path, -1, NULL, NULL);
+	rc = llapi_root_path_open(path, &fd);
 	if (rc)
 		return rc;
 
@@ -1655,7 +1655,7 @@ int llapi_hsm_request(const char *path, const struct hsm_user_request *request)
 	/* If error, save errno value */
 	rc = rc ? -errno : 0;
 
-	/* fd is cached internally, no need to close it */
+	close(fd);
 	return rc;
 }
 

lustre/utils/liblustreapi_pcc.c
@@ -215,7 +215,7 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
 	int fd;
 	struct lu_pcc_detach_fid detach;
 
-	rc = get_root_path(WANT_FD, NULL, &fd, (char *)mntpath, -1, NULL, NULL);
+	rc = llapi_root_path_open(mntpath, &fd);
 	if (rc) {
 		llapi_error(LLAPI_MSG_ERROR, rc, "cannot get root path: %s",
 			    mntpath);
@@ -232,10 +232,10 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
 	detach.pccd_fid = *fid;
 	detach.pccd_opt = option;
 
-	/* fd is cached internally, no need to close */
 	rc = ioctl(fd, LL_IOC_PCC_DETACH_BY_FID, &detach);
 	rc = rc ? -errno : 0;
 
+	close(fd);
 	return rc;
 }
 

commit d30e1dc85834cb3f7985da0c50f5fda5c7c5f0d9

LU-17444 utils: fix fd leak after conversion to llapi_root_path_open
Conversions to llapi_root_path_open missed a few close() calls, leading
to fd leaks.

These should be obvious enough to regroup in a single commit.

Fixes: 7154244354e3 ("LU-16786 utils: Replace open call to WANT_FD")
Change-Id: I3af25ef2981367bfaea7f5280972f84bee09a5c2
Signed-off-by: Dominique Martinet <asmadeus@codewreck.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53736
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Stephane Thiell <sthiell@stanford.edu>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/lfs.c
@@ -10102,10 +10102,11 @@ static int lfs_rmfid(int argc, char **argv)
 
 	fa = malloc(offsetof(struct fid_array, fa_fids[nr + 1]));
 	if (!fa) {
+		rc = -errno ?: -ENOMEM;
 		fprintf(stderr, "%s rmfid: error allocating %zd bytes: %s\n",
 			progname, offsetof(struct fid_array, fa_fids[nr + 1]),
-			strerror(errno));
-		return -ENOMEM;
+			strerror(-rc));
+		goto out_close;
 	}
 
 	fa->fa_nr = 0;
@@ -10149,6 +10150,7 @@ static int lfs_rmfid(int argc, char **argv)
 		fa = NULL;
 	}
 
+out_close:
 	close(rootfd);
 	return rc;
 }

lustre/utils/liblustreapi_fid.c
@@ -472,15 +472,12 @@ int llapi_open_by_fid(const char *lustre_dir, const struct lu_fid *fid,
 {
 	int mnt_fd, rc;
 
-	/* this will return a cached FD if available, so only one open needed.
-	 * WANT_FD doesn't modify lustre_dir so casting away "const" is OK */
-
 	rc = llapi_root_path_open(lustre_dir, &mnt_fd);
 	if (rc)
 		goto out;
 
-	/* "mnt_fd" is cached internally for reuse, no need to close it */
 	rc = llapi_open_by_fid_at(mnt_fd, fid, flags);
+	close(mnt_fd);
 out:
 	return rc;
 }

lustre/utils/liblustreapi_util.c
@@ -342,6 +342,7 @@ int llapi_rmfid(const char *path, struct fid_array *fa)
 	}
 
 	rc = llapi_rmfid_at(rootfd, fa);
+	close(rootfd);
 	if (rc < 0) {
 		fprintf(stderr, "lfs rmfid: cannot remove FIDs: %s\n",
 			strerror(-rc));