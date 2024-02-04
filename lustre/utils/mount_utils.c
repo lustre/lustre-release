lustre/lustre-release

commit d0c6e97fa53ae26dec458087e96dcbb0ed0d469a

LU-8900 snapshot: rename filesysetem fsname
Introuduce new tunefs options "--rename" for renaming the existing
Lustre filesystem fsname with configuration/parameters preserved.
Usage: tunefs.lustre --fsname $NEW_FSNAME --rename $OLD_FSNAME ...

NEW_FSNAME is the new fsname, must be specified.
OLD_FSNAME is the old fsname, it is optional if the MGS and the MDT
are combined together.

It shares the functionality of fork/erase Lustre configuration logs
in kernel.

Signed-off-by: Fan Yong <fan.yong@intel.com>
Change-Id: Ic9aaf6ed6209c7580bfabd4b660a6a7667576484
Reviewed-on: https://review.whamcloud.com/24268
Reviewed-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-by: Lai Siyao <lai.siyao@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
Tested-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/utils/mount_utils.c
@@ -51,6 +51,11 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <dlfcn.h>
+#include <lustre/lustre_idl.h>
+#include <lustre_cfg.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
 
 #ifdef HAVE_GSS
 #include <keyutils.h>
@@ -586,6 +591,7 @@ struct module_backfs_ops *load_backfs_module(enum ldd_mount_type mount_type)
 	DLSYM(name, ops, prepare_lustre);
 	DLSYM(name, ops, tune_lustre);
 	DLSYM(name, ops, label_lustre);
+	DLSYM(name, ops, rename_fsname);
 	DLSYM(name, ops, enable_quota);
 
 	error = dlerror();
@@ -748,6 +754,21 @@ int osd_label_lustre(struct mount_opts *mop)
 	return ret;
 }
 
+/* Rename filesystem fsname */
+int osd_rename_fsname(struct mkfs_opts *mop, const char *oldname)
+{
+	struct lustre_disk_data *ldd = &mop->mo_ldd;
+	int ret;
+
+	if (backfs_mount_type_okay(ldd->ldd_mount_type))
+		ret = backfs_ops[ldd->ldd_mount_type]->rename_fsname(mop,
+								     oldname);
+	else
+		ret = EINVAL;
+
+	return ret;
+}
+
 /* Enable quota accounting */
 int osd_enable_quota(struct mkfs_opts *mop)
 {
@@ -873,6 +894,196 @@ int file_create(char *path, __u64 size)
 	}
 
 	return 0;
+}
+
+
+struct lustre_cfg_entry {
+	struct list_head lce_list;
+	char		 lce_name[0];
+};
+
+static struct lustre_cfg_entry *lustre_cfg_entry_init(const char *name)
+{
+	struct lustre_cfg_entry *lce;
+	int len = strlen(name) + 1;
+
+	lce = malloc(sizeof(*lce) + len);
+	if (lce) {
+		INIT_LIST_HEAD(&lce->lce_list);
+		memcpy(lce->lce_name, name, len);
+	}
+
+	return lce;
+}
+
+static void lustre_cfg_entry_fini(struct lustre_cfg_entry *lce)
+{
+	free(lce);
+}
+
+int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
+			 const char *oldname)
+{
+	struct lustre_disk_data *ldd = &mop->mo_ldd;
+	struct lr_server_data lsd;
+	char filepnm[128];
+	char cfg_dir[128];
+	DIR *dir = NULL;
+	struct dirent64 *dirent;
+	struct lustre_cfg_entry *lce;
+	struct list_head cfg_list;
+	int old_namelen = strlen(oldname);
+	int new_namelen = strlen(ldd->ldd_fsname);
+	int ret;
+	int fd;
+
+	INIT_LIST_HEAD(&cfg_list);
+
+	snprintf(filepnm, sizeof(filepnm), "%s/%s", mntpt, LAST_RCVD);
+	fd = open(filepnm, O_RDWR);
+	if (fd < 0) {
+		if (errno == ENOENT)
+			goto config;
+
+		if (errno != 0)
+			ret = errno;
+		else
+			ret = fd;
+		fprintf(stderr, "Unable to open %s: %s\n",
+			filepnm, strerror(ret));
+		return ret;
+	}
+
+	ret = read(fd, &lsd, sizeof(lsd));
+	if (ret != sizeof(lsd)) {
+		if (errno != 0)
+			ret = errno;
+		fprintf(stderr, "Unable to read %s: %s\n",
+			filepnm, strerror(ret));
+		close(fd);
+		return ret;
+	}
+
+	ret = lseek(fd, 0, SEEK_SET);
+	if (ret) {
+		if (errno != 0)
+			ret = errno;
+		fprintf(stderr, "Unable to lseek %s: %s\n",
+			filepnm, strerror(ret));
+		close(fd);
+		return ret;
+	}
+
+	/* replace fsname in lr_server_data::lsd_uuid. */
+	if (old_namelen > new_namelen)
+		memmove(lsd.lsd_uuid + new_namelen,
+			lsd.lsd_uuid + old_namelen,
+			sizeof(lsd.lsd_uuid) - old_namelen);
+	else if (old_namelen < new_namelen)
+		memmove(lsd.lsd_uuid + new_namelen,
+			lsd.lsd_uuid + old_namelen,
+			sizeof(lsd.lsd_uuid) - new_namelen);
+	memcpy(lsd.lsd_uuid, ldd->ldd_fsname, new_namelen);
+	ret = write(fd, &lsd, sizeof(lsd));
+	if (ret != sizeof(lsd)) {
+		if (errno != 0)
+			ret = errno;
+		fprintf(stderr, "Unable to write %s: %s\n",
+			filepnm, strerror(ret));
+		close(fd);
+		return ret;
+	}
+
+	close(fd);
+
+config:
+	snprintf(cfg_dir, sizeof(cfg_dir), "%s/%s", mntpt, MOUNT_CONFIGS_DIR);
+	dir = opendir(cfg_dir);
+	if (!dir) {
+		if (errno != 0)
+			ret = errno;
+		else
+			ret = EINVAL;
+		fprintf(stderr, "Unable to opendir %s: %s\n",
+			cfg_dir, strerror(ret));
+		return ret;
+	}
+
+	while ((dirent = readdir64(dir)) != NULL) {
+		char *ptr;
+
+		if (strlen(dirent->d_name) <= old_namelen)
+			continue;
+
+		ptr = strrchr(dirent->d_name, '-');
+		if (!ptr || (ptr - dirent->d_name) != old_namelen)
+			continue;
+
+		if (strncmp(dirent->d_name, oldname, old_namelen) != 0)
+			continue;
+
+		lce = lustre_cfg_entry_init(dirent->d_name);
+		if (!lce) {
+			if (errno != 0)
+				ret = errno;
+			else
+				ret = EINVAL;
+
+			fprintf(stderr, "Fail to init item for %s: %s\n",
+				dirent->d_name, strerror(ret));
+			goto out;
+		}
+
+		list_add(&lce->lce_list, &cfg_list);
+	}
+
+	closedir(dir);
+	dir = NULL;
+	ret = 0;
+
+	while (!list_empty(&cfg_list) && ret == 0) {
+		lce = list_entry(cfg_list.next, struct lustre_cfg_entry,
+				 lce_list);
+		list_del(&lce->lce_list);
+		snprintf(filepnm, sizeof(filepnm), "%s/%s", cfg_dir,
+			 lce->lce_name);
+		if (IS_MGS(ldd))
+			/* Store the new fsname in the XATTR_TARGET_RENAME EA.
+			 * When the MGS start, it will scan config logs, and
+			 * for the ones which have the XATTR_TARGET_RENAME EA,
+			 * it will replace old fsname with the new fsname in
+			 * the config log by some shared kernel level config
+			 * logs {fork,erase} functionalities automatically. */
+			ret = setxattr(filepnm, XATTR_TARGET_RENAME,
+				       ldd->ldd_fsname,
+				       strlen(ldd->ldd_fsname), 0);
+		else
+			ret = unlink(filepnm);
+
+		if (ret) {
+			if (errno != 0)
+				ret = errno;
+
+			fprintf(stderr, "Fail to %s %s: %s\n",
+				IS_MGS(ldd) ? "setxattr" : "unlink",
+				filepnm, strerror(ret));
+		}
+
+		lustre_cfg_entry_fini(lce);
+	}
+
+out:
+	if (dir)
+		closedir(dir);
+
+	while (!list_empty(&cfg_list)) {
+		lce = list_entry(cfg_list.next, struct lustre_cfg_entry,
+				 lce_list);
+		list_del(&lce->lce_list);
+		lustre_cfg_entry_fini(lce);
+	}
+
+	return ret;
 }
 
 #ifdef HAVE_GSS

lustre/include/dt_object.h
@@ -1975,6 +1975,10 @@ int local_object_create(const struct lu_env *env,
 			struct dt_object *o,
 			struct lu_attr *attr, struct dt_object_format *dof,
 			struct thandle *th);
+struct dt_object *local_file_find(const struct lu_env *env,
+				  struct local_oid_storage *los,
+				  struct dt_object *parent,
+				  const char *name);
 struct dt_object *local_file_find_or_create(const struct lu_env *env,
 					    struct local_oid_storage *los,
 					    struct dt_object *parent,

lustre/include/lustre_disk.h
@@ -119,6 +119,8 @@
 
 #define LDD_F_MASK          0xFFFF
 
+#define XATTR_TARGET_RENAME "trusted.rename_tgt"
+
 enum ldd_mount_type {
 	LDD_MT_EXT3 = 0,
 	LDD_MT_LDISKFS,
@@ -184,12 +186,13 @@ struct lustre_disk_data {
 };
 
 
-#define IS_MDT(data)    ((data)->lsi_flags & LDD_F_SV_TYPE_MDT)
-#define IS_OST(data)    ((data)->lsi_flags & LDD_F_SV_TYPE_OST)
-#define IS_MGS(data)    ((data)->lsi_flags & LDD_F_SV_TYPE_MGS)
-#define IS_SERVER(data) ((data)->lsi_flags & (LDD_F_SV_TYPE_MGS | \
-			 LDD_F_SV_TYPE_MDT | LDD_F_SV_TYPE_OST))
-#define MT_STR(data)    mt_str((data)->ldd_mount_type)
+#define IS_MDT(data)		((data)->lsi_flags & LDD_F_SV_TYPE_MDT)
+#define IS_OST(data)		((data)->lsi_flags & LDD_F_SV_TYPE_OST)
+#define IS_MGS(data)		((data)->lsi_flags & LDD_F_SV_TYPE_MGS)
+#define IS_SEPARATED_MGS(data)	((data)->ldd_flags == LDD_F_SV_TYPE_MGS)
+#define IS_SERVER(data)		((data)->lsi_flags & (LDD_F_SV_TYPE_MGS | \
+				 LDD_F_SV_TYPE_MDT | LDD_F_SV_TYPE_OST))
+#define MT_STR(data)		mt_str((data)->ldd_mount_type)
 
 /* Make the mdt/ost server obd name based on the filesystem name */
 static inline bool server_make_name(__u32 flags, __u16 index, char *fs,

lustre/mgs/mgs_handler.c
@@ -1294,6 +1294,10 @@ static int mgs_init0(const struct lu_env *env, struct mgs_device *mgs,
 	mutex_init(&mgs->mgs_health_mutex);
 	init_rwsem(&mgs->mgs_barrier_rwsem);
 
+	rc = mgs_lcfg_rename(env, mgs);
+	if (rc)
+		GOTO(err_llog, rc);
+
 	rc = lproc_mgs_setup(mgs, lustre_cfg_string(lcfg, 3));
 	if (rc != 0) {
 		CERROR("%s: cannot initialize proc entry: rc = %d\n",

lustre/mgs/mgs_internal.h
@@ -426,5 +426,6 @@ int mgs_lcfg_fork(const struct lu_env *env, struct mgs_device *mgs,
 		  const char *oldname, const char *newname);
 int mgs_lcfg_erase(const struct lu_env *env, struct mgs_device *mgs,
 		   const char *fsname);
+int mgs_lcfg_rename(const struct lu_env *env, struct mgs_device *mgs);
 
 #endif /* _MGS_INTERNAL_H */

lustre/mgs/mgs_llog.c
@@ -4517,6 +4517,141 @@ int mgs_lcfg_erase(const struct lu_env *env, struct mgs_device *mgs,
 	RETURN(rc);
 }
 
+static int mgs_xattr_del(const struct lu_env *env, struct dt_object *obj)
+{
+	struct dt_device *dev;
+	struct thandle *th = NULL;
+	int rc = 0;
+
+	ENTRY;
+
+	dev = container_of0(obj->do_lu.lo_dev, struct dt_device, dd_lu_dev);
+	th = dt_trans_create(env, dev);
+	if (IS_ERR(th))
+		RETURN(PTR_ERR(th));
+
+	rc = dt_declare_xattr_del(env, obj, XATTR_TARGET_RENAME, th);
+	if (rc)
+		GOTO(stop, rc);
+
+	rc = dt_trans_start_local(env, dev, th);
+	if (rc)
+		GOTO(stop, rc);
+
+	dt_write_lock(env, obj, 0);
+	rc = dt_xattr_del(env, obj, XATTR_TARGET_RENAME, th);
+
+	GOTO(unlock, rc);
+
+unlock:
+	dt_write_unlock(env, obj);
+
+stop:
+	dt_trans_stop(env, dev, th);
+
+	return rc;
+}
+
+int mgs_lcfg_rename(const struct lu_env *env, struct mgs_device *mgs)
+{
+	struct list_head log_list;
+	struct mgs_direntry *dirent, *n;
+	char fsname[16];
+	struct lu_buf buf = {
+		.lb_buf = fsname,
+		.lb_len = sizeof(fsname)
+	};
+	int rc = 0;
+
+	ENTRY;
+
+	rc = class_dentry_readdir(env, mgs, &log_list);
+	if (rc)
+		RETURN(rc);
+
+	if (list_empty(&log_list))
+		RETURN(0);
+
+	list_for_each_entry_safe(dirent, n, &log_list, mde_list) {
+		struct dt_object *o = NULL;
+		char oldname[16];
+		char *ptr;
+		int len;
+
+		list_del_init(&dirent->mde_list);
+		ptr = strrchr(dirent->mde_name, '-');
+		if (!ptr)
+			goto next;
+
+		len = ptr - dirent->mde_name;
+		if (unlikely(len >= sizeof(oldname))) {
+			CDEBUG(D_MGS, "Skip invalid configuration file %s\n",
+			       dirent->mde_name);
+			goto next;
+		}
+
+		o = local_file_find(env, mgs->mgs_los, mgs->mgs_configs_dir,
+				    dirent->mde_name);
+		if (IS_ERR(o)) {
+			rc = PTR_ERR(o);
+			CDEBUG(D_MGS, "Fail to locate file %s: rc = %d\n",
+			       dirent->mde_name, rc);
+			goto next;
+		}
+
+		rc = dt_xattr_get(env, o, &buf, XATTR_TARGET_RENAME);
+		if (rc < 0) {
+			if (rc == -ENODATA)
+				rc = 0;
+			else
+				CDEBUG(D_MGS,
+				       "Fail to get EA for %s: rc = %d\n",
+				       dirent->mde_name, rc);
+			goto next;
+		}
+
+		if (unlikely(rc == len &&
+			     memcmp(fsname, dirent->mde_name, len) == 0)) {
+			/* The new fsname is the same as the old one. */
+			rc = mgs_xattr_del(env, o);
+			goto next;
+		}
+
+		memcpy(oldname, dirent->mde_name, len);
+		oldname[len] = '\0';
+		fsname[rc] = '\0';
+		rc = mgs_lcfg_fork_one(env, mgs, dirent, oldname, fsname);
+		if (rc && rc != -EEXIST) {
+			CDEBUG(D_MGS, "Fail to fork %s: rc = %d\n",
+			       dirent->mde_name, rc);
+			goto next;
+		}
+
+		rc = mgs_erase_log(env, mgs, dirent->mde_name);
+		if (rc) {
+			CDEBUG(D_MGS, "Fail to erase old %s: rc = %d\n",
+			       dirent->mde_name, rc);
+			/* keep it there if failed to remove it. */
+			rc = 0;
+		}
+
+next:
+		if (o && !IS_ERR(o))
+			lu_object_put(env, &o->do_lu);
+
+		mgs_direntry_free(dirent);
+		if (rc)
+			break;
+	}
+
+	list_for_each_entry_safe(dirent, n, &log_list, mde_list) {
+		list_del_init(&dirent->mde_list);
+		mgs_direntry_free(dirent);
+	}
+
+	RETURN(rc);
+}
+
 /* from llog_swab */
 static void print_lustre_cfg(struct lustre_cfg *lcfg)
 {

lustre/obdclass/local_storage.c
@@ -425,6 +425,28 @@ out:
 	RETURN(dto);
 }
 
+struct dt_object *local_file_find(const struct lu_env *env,
+				  struct local_oid_storage *los,
+				  struct dt_object *parent,
+				  const char *name)
+{
+	struct dt_thread_info	*dti = dt_info(env);
+	struct dt_object	*dto;
+	int			 rc;
+
+	LASSERT(parent);
+
+	rc = dt_lookup_dir(env, parent, name, &dti->dti_fid);
+	if (!rc)
+		dto = ls_locate(env, dt2ls_dev(los->los_dev),
+				&dti->dti_fid, NULL);
+	else
+		dto = ERR_PTR(rc);
+
+	return dto;
+}
+EXPORT_SYMBOL(local_file_find);
+
 /*
  * Look up and create (if it does not exist) a local named file or directory in
  * parent directory.
@@ -438,30 +460,21 @@ struct dt_object *local_file_find_or_create(const struct lu_env *env,
 	struct dt_object	*dto;
 	int			 rc;
 
-	LASSERT(parent);
+	dto = local_file_find(env, los, parent, name);
+	if (!IS_ERR(dto) || PTR_ERR(dto) != -ENOENT)
+		return dto;
 
-	rc = dt_lookup_dir(env, parent, name, &dti->dti_fid);
-	if (rc == 0)
-		/* name is found, get the object */
-		dto = ls_locate(env, dt2ls_dev(los->los_dev),
-				&dti->dti_fid, NULL);
-	else if (rc != -ENOENT)
-		dto = ERR_PTR(rc);
-	else {
-		rc = local_object_fid_generate(env, los, &dti->dti_fid);
-		if (rc < 0) {
-			dto = ERR_PTR(rc);
-		} else {
-			/* create the object */
-			dti->dti_attr.la_valid	= LA_MODE;
-			dti->dti_attr.la_mode	= mode;
-			dti->dti_dof.dof_type	= dt_mode_to_dft(mode & S_IFMT);
-			dto = __local_file_create(env, &dti->dti_fid, los,
-						  dt2ls_dev(los->los_dev),
-						  parent, name, &dti->dti_attr,
-						  &dti->dti_dof);
-		}
-	}
+	rc = local_object_fid_generate(env, los, &dti->dti_fid);
+	if (rc)
+		return ERR_PTR(rc);
+
+	/* create the object */
+	dti->dti_attr.la_valid = LA_MODE;
+	dti->dti_attr.la_mode = mode;
+	dti->dti_dof.dof_type = dt_mode_to_dft(mode & S_IFMT);
+	dto = __local_file_create(env, &dti->dti_fid, los,
+				  dt2ls_dev(los->los_dev), parent, name,
+				  &dti->dti_attr, &dti->dti_dof);
 	return dto;
 }
 EXPORT_SYMBOL(local_file_find_or_create);

lustre/tests/conf-sanity.sh
@@ -7076,6 +7076,131 @@ test_102() {
 }
 run_test 102 "obdclass module cleanup upon error"
 
+test_renamefs() {
+	local newname=$1
+
+	echo "rename $FSNAME to $newname"
+
+	if [ ! combined_mgs_mds ]; then
+		local facet=$(mgsdevname)
+
+		do_facet mgs \
+			"$TUNEFS --fsname=$newname --rename=$FSNAME -v $facet"||
+			error "(7) Fail to rename MGS"
+		if [ "$(facet_fstype $facet)" = "zfs" ]; then
+			reimport_zpool mgs $newname-mgs
+		fi
+	fi
+
+	for num in $(seq $MDSCOUNT); do
+		local facet=$(mdsdevname $num)
+
+		do_facet mds${num} \
+			"$TUNEFS --fsname=$newname --rename=$FSNAME -v $facet"||
+			error "(8) Fail to rename MDT $num"
+		if [ "$(facet_fstype $facet)" = "zfs" ]; then
+			reimport_zpool mds${num} $newname-mdt${num}
+		fi
+	done
+
+	for num in $(seq $OSTCOUNT); do
+		local facet=$(ostdevname $num)
+
+		do_facet ost${num} \
+			"$TUNEFS --fsname=$newname --rename=$FSNAME -v $facet"||
+			error "(9) Fail to rename OST $num"
+		if [ "$(facet_fstype $facet)" = "zfs" ]; then
+			reimport_zpool ost${num} $newname-ost${num}
+		fi
+	done
+}
+
+test_103_set_pool() {
+	local pname=$1
+	local ost_x=$2
+
+	do_facet mgs $LCTL pool_add $FSNAME.$pname ${FSNAME}-$ost_x ||
+		error "Fail to add $ost_x to $FSNAME.$pname"
+	wait_update $HOSTNAME \
+		"lctl get_param -n lov.$FSNAME-clilov-*.pools.$pname |
+		 grep $ost_x" "$FSNAME-${ost_x}_UUID" ||
+		error "$ost_x is NOT in pool $FSNAME.$pname"
+}
+
+test_103_check_pool() {
+	local save_fsname=$1
+	local errno=$2
+
+	stat $DIR/$tdir/test-framework.sh ||
+		error "($errno) Fail to stat"
+	do_facet mgs $LCTL pool_list $FSNAME.pool1 ||
+		error "($errno) Fail to list $FSNAME.pool1"
+	do_facet mgs $LCTL pool_list $FSNAME.$save_fsname ||
+		error "($errno) Fail to list $FSNAME.$save_fsname"
+	do_facet mgs $LCTL pool_list $FSNAME.$save_fsname |
+		grep ${FSNAME}-OST0000 ||
+		error "($errno) List $FSNAME.$save_fsname is invalid"
+
+	local pname=$($LFS getstripe --pool $DIR/$tdir/d0)
+	[ "$pname" = "$save_fsname" ] ||
+		error "($errno) Unexpected pool name $pname"
+}
+
+test_103() {
+	check_mount_and_prep
+	rm -rf $DIR/$tdir
+	mkdir $DIR/$tdir || error "(1) Fail to mkdir $DIR/$tdir"
+	cp $LUSTRE/tests/test-framework.sh $DIR/$tdir ||
+		error "(2) Fail to copy test-framework.sh"
+
+	do_facet mgs $LCTL pool_new $FSNAME.pool1 ||
+		error "(3) Fail to create $FSNAME.pool1"
+	# name the pool name as the fsname
+	do_facet mgs $LCTL pool_new $FSNAME.$FSNAME ||
+		error "(4) Fail to create $FSNAME.$FSNAME"
+
+	test_103_set_pool $FSNAME OST0000
+
+	$SETSTRIPE -p $FSNAME $DIR/$tdir/d0 ||
+		error "(6) Fail to setstripe on $DIR/$tdir/d0"
+
+	KEEP_ZPOOL=true
+	stopall
+
+	test_renamefs mylustre
+
+	local save_fsname=$FSNAME
+	FSNAME="mylustre"
+	setupall
+
+	test_103_check_pool $save_fsname 7
+
+	if [ $OSTCOUNT -ge 2 ]; then
+		test_103_set_pool $save_fsname OST0001
+	fi
+
+	$SETSTRIPE -p $save_fsname $DIR/$tdir/f0 ||
+		error "(16) Fail to setstripe on $DIR/$tdir/f0"
+
+	stopall
+
+	test_renamefs tfs
+
+	FSNAME="tfs"
+	setupall
+
+	test_103_check_pool $save_fsname 17
+
+	stopall
+
+	test_renamefs $save_fsname
+
+	FSNAME=$save_fsname
+	setupall
+	KEEP_ZPOOL=false
+}
+run_test 103 "rename filesystem name"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi

commit 04d95ff28d1ddca79c204ceff7cd5d6ddfe83c34

LU-17000 utils: Remove check for errno != 0
In lustre_rename_fsname() after calls to system calls
like open/read/write/lseek there is a check if global
errno is not equal to zero. This is noop and not
required, as these system calls do reset errno to 0
on success.

The side effect of this check was that it appeared
that the errno could be 0 which would leave 'ret' as
negative. This would never happen, but was causing
Coverity to complain.

This patch fixes this coverity issue by removing
the comparison of errno != 0 which is not required.

Test-Parameters: trivial testlist=conf-sanity,sanityn
Fixes: d0c6e97fa53 ("LU-8900 snapshot: rename filesysetem fsname")
CoverityID: 397153 ("Argument cannot be negative")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I7257434e6546f56f30841f49a3bde35e80360bb8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53742
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/mount_utils.c
@@ -1082,10 +1082,7 @@ int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
 		if (errno == ENOENT)
 			goto config;
 
-		if (errno != 0)
-			ret = errno;
-		else
-			ret = fd;
+		ret = errno;
 		fprintf(stderr, "Unable to open %s: %s\n",
 			filepnm, strerror(ret));
 		return ret;
@@ -1093,8 +1090,11 @@ int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
 
 	ret = read(fd, &lsd, sizeof(lsd));
 	if (ret != sizeof(lsd)) {
-		if (errno != 0)
+		if (ret < 0)
 			ret = errno;
+		else
+			/* short read */
+			ret = EINTR;
 		fprintf(stderr, "Unable to read %s: %s\n",
 			filepnm, strerror(ret));
 		close(fd);
@@ -1102,9 +1102,8 @@ int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
 	}
 
 	ret = lseek(fd, 0, SEEK_SET);
-	if (ret) {
-		if (errno != 0)
-			ret = errno;
+	if (ret < 0) {
+		ret = errno;
 		fprintf(stderr, "Unable to lseek %s: %s\n",
 			filepnm, strerror(ret));
 		close(fd);
@@ -1123,8 +1122,11 @@ int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,
 	memcpy(lsd.lsd_uuid, ldd->ldd_fsname, new_namelen);
 	ret = write(fd, &lsd, sizeof(lsd));
 	if (ret != sizeof(lsd)) {
-		if (errno != 0)
+		if (ret < 0)
 			ret = errno;
+		else
+			 /* short writes */
+			ret = EINTR;
 		fprintf(stderr, "Unable to write %s: %s\n",
 			filepnm, strerror(ret));
 		close(fd);
@@ -1137,10 +1139,7 @@ config:
 	snprintf(cfg_dir, sizeof(cfg_dir), "%s/%s", mntpt, MOUNT_CONFIGS_DIR);
 	dir = opendir(cfg_dir);
 	if (!dir) {
-		if (errno != 0)
-			ret = errno;
-		else
-			ret = EINVAL;
+		ret = errno;
 		fprintf(stderr, "Unable to opendir %s: %s\n",
 			cfg_dir, strerror(ret));
 		return ret;
@@ -1199,9 +1198,8 @@ config:
 		else
 			ret = unlink(filepnm);
 
-		if (ret) {
-			if (errno != 0)
-				ret = errno;
+		if (ret < 0) {
+			ret = errno;
 
 			fprintf(stderr, "Fail to %s %s: %s\n",
 				IS_MGS(ldd) ? "setxattr" : "unlink",