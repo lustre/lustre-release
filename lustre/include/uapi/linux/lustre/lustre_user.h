lustre/lustre-release

commit 6331eadbd60a8c58c1c456548d067aaf772dfbaf

LU-15420 uapi: avoid gcc-11 -Werror=stringop-overread
GCC 11 warns about string and memory operations on fixed address:

In function ‘strlen’,
    inlined from ‘changelog_rec_sname’ at include/uapi/linux/lustre/lustre_user.h:1981:19,
    inlined from ‘mdd_changelog_rec_ext_rename’ at lustre/mdd/mdd_dir.c:932:2,
    inlined from ‘mdd_changelog_ns_store’ at lustre/mdd/mdd_dir.c:1061:3:
include/linux/fortify-string.h:25:33: error: ‘__builtin_strlen’
reading 1 or more bytes from a region of size 0 [-Werror=stringop-overread]
   25 | #define __underlying_strlen     __builtin_strlen

The reason is that we are looking for an address right after the end
of the chanelog record header which gcc thinks is an overrun. Rework
the code to allow us to index the memory right after the changelog
record header.

Also fix a long hidden bug in the lustre snmp code.

Change-Id: I13479b9074a392330d39f01656b26f9e9a91a8ec
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/46319
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -40,6 +40,16 @@
  *
  * @{
  */
+#ifndef __KERNEL__
+# define __USE_ISOC99	1
+# include <stdbool.h>
+# include <stdio.h> /* snprintf() */
+# include <sys/stat.h>
+
+# define __USE_GNU      1
+# define __USE_XOPEN2K8  1
+# define FILEID_LUSTRE 0x97 /* for name_to_handle_at() (and llapi_fd2fid()) */
+#endif /* !__KERNEL__ */
 
 #include <linux/fs.h>
 #include <linux/limits.h>
@@ -50,14 +60,6 @@
 #include <linux/unistd.h>
 #include <linux/lustre/lustre_fiemap.h>
 #include <linux/lustre/lustre_ver.h>
-
-#ifndef __KERNEL__
-# define __USE_ISOC99	1
-# include <stdbool.h>
-# include <stdio.h> /* snprintf() */
-# include <sys/stat.h>
-# define FILEID_LUSTRE 0x97 /* for name_to_handle_at() (and llapi_fd2fid()) */
-#endif /* !__KERNEL__ */
 
 #if defined(__cplusplus)
 extern "C" {
@@ -1985,16 +1987,14 @@ static inline char *changelog_rec_name(const struct changelog_rec *rec)
 		(enum changelog_rec_extra_flags)(cref & CLFE_SUPPORTED));
 }
 
-static inline __kernel_size_t changelog_rec_snamelen(const struct changelog_rec *rec)
-{
-	return rec->cr_namelen - strlen(changelog_rec_name(rec)) - 1;
-}
-
 static inline char *changelog_rec_sname(const struct changelog_rec *rec)
 {
-	char *cr_name = changelog_rec_name(rec);
+	return strchrnul(changelog_rec_name(rec), '\0') + 1;
+}
 
-	return cr_name + strlen(cr_name) + 1;
+static inline __kernel_size_t changelog_rec_snamelen(const struct changelog_rec *rec)
+{
+	return strlen(changelog_rec_sname(rec));
 }
 
 /**

lustre/tests/mpi/Makefile.am
@@ -1,5 +1,5 @@
 # Lustre MPI test Makefile
-AM_CFLAGS := -fPIC -D_LARGEFILE64_SOURCE=1 -D_FILE_OFFSET_BITS=64
+AM_CFLAGS := -fPIC -D_GNU_SOURCE -D_LARGEFILE64_SOURCE=1 -D_FILE_OFFSET_BITS=64
 
 CC = @MPICC_WRAPPER@
 

snmp/lustre-snmp.c
@@ -712,7 +712,7 @@ void deinit_lustresnmp(void) {
 	/* deregister ourselves with the agent */
 	unregister_mib(clusterFileSystems_variables_oid,
 		       sizeof(clusterFileSystems_variables_oid)/
-		       sizeof(clusterFileSystems_variables_oid));
+		       sizeof(clusterFileSystems_variables_oid[0]));
 
 	terminate_trap_handler();
 

commit b8f4a96b5ebd5cd35747b0a061277d02d46e7a4c

LU-17400 uapi: Fix incorrect snamelen return value
The sname char array is limited by the struct
changelog_rec.cr_namelen value and has no '\0' character allocated
to it, so strlen() will overrun the char array till it finds the next
'\0' char.

This issue can be seen on the client side when "lfs changelog"
is run and 08RENME record types are present.

Pointer arithmetic was used between sname and name to avoid the
GCC 11 warnings mentioned in 6331eadbd6.

Added Andreas's safety/range check code to changelog_rec_sname.

Fixes: 6331eadbd6 ("LU-15420 uapi: avoid gcc-11 -Werror=stringop-overread")
Signed-off-by: Josh Samuelson <josh@1up.unl.edu>
Change-Id: Ie0817dfdd1d02e06b9399e66f1affaadb9e156c4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53624
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -2091,15 +2091,17 @@ static inline char *changelog_rec_name(const struct changelog_rec *rec)
 static inline char *changelog_rec_sname(const struct changelog_rec *rec)
 {
 	char *str = changelog_rec_name(rec);
+	char *end = str + NAME_MAX; /* NB: NAME_MAX use in CR_MAXSIZE */
 
-	while (*str != '\0')
+	while (*str != '\0' && str <= end)
 		str++;
 	return str + 1;
 }
 
 static inline __kernel_size_t changelog_rec_snamelen(const struct changelog_rec *rec)
 {
-	return strlen(changelog_rec_sname(rec));
+	return rec->cr_namelen -
+	       (changelog_rec_sname(rec) - changelog_rec_name(rec));
 }
 
 /**

lustre/tests/sanity.sh
@@ -18738,6 +18738,42 @@ test_160t() {
 }
 run_test 160t "changelog garbage collect on lack of space"
 
+test_160u() { # LU-17400
+	[ $PARALLEL == "yes" ] && skip "skip parallel run"
+	remote_mds_nodsh && skip "remote MDS with nodsh"
+	[ $MDS1_VERSION -ge $(version_code 2.2.0) ] ||
+		skip "Need MDS version at least 2.2.0"
+
+	cd $DIR || error "cd $DIR failed"
+
+	# ensure changelog has a clean view if tests are run multiple times
+	[ -d rename ] && rm -rf rename
+
+	changelog_register || error "changelog_register failed"
+	local cl_user="${CL_USERS[$SINGLEMDS]%% *}"
+
+	changelog_users $SINGLEMDS | grep -q $cl_user ||
+		error "User '$cl_user' not found in changelog_users"
+
+	local longname1=$(str_repeat a 255)
+
+	echo "creating simple directory tree"
+	mkdir -p rename/a || error "create of simple directory tree failed"
+	echo "creating rename/hw file"
+	echo "hello world" > rename/hw || error "create of rename/hw failed"
+	echo "creating very long named file"
+	touch rename/$longname1 || error "create of 'rename/$longname1' failed"
+	echo "move rename/hw to rename/a/a.hw"
+	mv rename/hw rename/a/a.hw || error "mv failed"
+
+	RENME=($(changelog_dump | grep "RENME"))
+	#declare -p RENME # for debugging captured value with indexes
+
+	[[ "${RENME[11]}" == "a.hw" && "${RENME[14]}" == "hw" ]] ||
+		error "changelog rename record type name/sname error"
+}
+run_test 160u "changelog rename record type name and sname strings are correct"
+
 test_161a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 