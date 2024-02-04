lustre/lustre-release

commit 0561c144cc1bb623e05d08b5055009e8d86047f4

LU-13397 lfs: mirror extend/copy keeps sparseness
- make ll_lseek() to work under group lock and on designated
  mirror
- enhance lfs mirror copy functions migrate_copy_data() and
  llapi_mirror_copy_many() with lseek() to find holes and copy
  only data chunks.

Both 'migrate' and 'copy' lfs functionality rewrite designated
mirror fully, so holes are not punched in destination file, but
truncate is called first to make sure old data is erased.

Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ic4a8768b816c921acd7f0adb3311138caac05a7c
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Xi <lixi@ddn.com>
Reviewed-on: https://review.whamcloud.com/40772
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -925,14 +925,13 @@ out:
 static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int))
 {
 	struct llapi_layout *layout;
-	size_t	 buf_size = 4 * 1024 * 1024;
-	void	*buf = NULL;
-	ssize_t	 rsize = -1;
-	ssize_t	 wsize = 0;
-	size_t	 rpos = 0;
-	size_t	 wpos = 0;
-	off_t	 bufoff = 0;
-	int	 rc;
+	size_t buf_size = 4 * 1024 * 1024;
+	void *buf = NULL;
+	off_t pos = 0;
+	off_t data_end = 0;
+	size_t page_size = sysconf(_SC_PAGESIZE);
+	bool sparse;
+	int rc;
 
 	layout = llapi_layout_get_by_fd(fd_src, 0);
 	if (layout) {
@@ -946,43 +945,76 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int))
 	}
 
 	/* Use a page-aligned buffer for direct I/O */
-	rc = posix_memalign(&buf, getpagesize(), buf_size);
+	rc = posix_memalign(&buf, page_size, buf_size);
 	if (rc != 0)
 		return -rc;
 
+	sparse = llapi_file_is_sparse(fd_src);
+	if (sparse) {
+		rc = ftruncate(fd_dst, pos);
+		if (rc < 0) {
+			rc = -errno;
+			return rc;
+		}
+	}
+
 	while (1) {
-		/*
-		 * read new data only if we have written all
-		 * previously read data
-		 */
-		if (wpos == rpos) {
-			if (check_file) {
-				rc = check_file(fd_src);
+		off_t data_off;
+		size_t to_read, to_write;
+		ssize_t rsize;
+
+		if (sparse && pos >= data_end) {
+			size_t data_size;
+
+			data_off = llapi_data_seek(fd_src, pos, &data_size);
+			if (data_off < 0) {
+				/* Non-fatal, switch to full copy */
+				sparse = false;
+				continue;
+			}
+			/* hole at the end of file, truncate up to it */
+			if (!data_size) {
+				rc = ftruncate(fd_dst, data_off);
 				if (rc < 0)
 					goto out;
 			}
-
-			rsize = read(fd_src, buf, buf_size);
-			if (rsize < 0) {
-				rc = -errno;
-				goto out;
-			}
-
-			rpos += rsize;
-			bufoff = 0;
+			pos = data_off & ~(page_size - 1);
+			data_end = data_off + data_size;
+			to_read = ((data_end - pos - 1) | (page_size - 1)) + 1;
+			to_read = MIN(to_read, buf_size);
+		} else {
+			to_read = buf_size;
 		}
 
-		/* eof ? */
+		if (check_file) {
+			rc = check_file(fd_src);
+			if (rc < 0)
+				goto out;
+		}
+
+		rsize = pread(fd_src, buf, to_read, pos);
+		if (rsize < 0) {
+			rc = -errno;
+			goto out;
+		}
+		/* EOF */
 		if (rsize == 0)
 			break;
 
-		wsize = write(fd_dst, buf + bufoff, rpos - wpos);
-		if (wsize < 0) {
-			rc = -errno;
-			break;
+		to_write = rsize;
+		while (to_write > 0) {
+			ssize_t written;
+
+			written = pwrite(fd_dst, buf, to_write, pos);
+			if (written < 0) {
+				rc = -errno;
+				goto out;
+			}
+			pos += written;
+			to_write -= written;
 		}
-		wpos += wsize;
-		bufoff += wsize;
+		if (rc || rsize < to_read)
+			break;
 	}
 
 	rc = fsync(fd_dst);

lustre/utils/liblustreapi_mirror.c
@@ -203,6 +203,48 @@ int llapi_mirror_truncate(int fd, unsigned int id, off_t length)
 	return rc;
 }
 
+bool llapi_mirror_is_sparse(int fd, unsigned int id)
+{
+	bool sparse;
+	int rc;
+
+	rc = llapi_mirror_set(fd, id);
+	if (rc < 0)
+		return false;
+
+	sparse = llapi_file_is_sparse(fd);
+	(void) llapi_mirror_clear(fd);
+
+	return sparse;
+}
+
+/**
+ * Seek data in a specified mirror with @id. This function looks for the
+ * first data segment from given offset and returns its offset and length
+ *
+ * \param fd	file descriptor, should be opened with O_DIRECT
+ * \param id	mirror id to be read from
+ * \param pos	position for start data seek from
+ * \param size	size of data segment found
+ *
+ * \result >= 0	Number of bytes has been read
+ * \result < 0	The last seen error
+ */
+off_t llapi_mirror_data_seek(int fd, unsigned int id, off_t pos, size_t *size)
+{
+	off_t data_off;
+	int rc;
+
+	rc = llapi_mirror_set(fd, id);
+	if (rc < 0)
+		return rc;
+
+	data_off = llapi_data_seek(fd, pos, size);
+	(void) llapi_mirror_clear(fd);
+
+	return data_off;
+}
+
 /**
  * Copy data contents from source mirror @src to multiple destinations
  * pointed by @dst. The destination array @dst will be altered to store
@@ -220,10 +262,12 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 {
 	const size_t buflen = 4 * 1024 * 1024; /* 4M */
 	void *buf;
-	loff_t pos = 0;
+	off_t pos = 0;
+	off_t data_end = 0;
 	size_t page_size = sysconf(_SC_PAGESIZE);
 	ssize_t result = 0;
 	bool eof = false;
+	bool sparse;
 	int nr;
 	int i;
 	int rc;
@@ -235,12 +279,61 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 	if (rc) /* error code is returned directly */
 		return -rc;
 
-	nr = count;
-	while (!eof) {
-		ssize_t bytes_read;
-		size_t to_write;
+	sparse = llapi_mirror_is_sparse(fd, src);
 
-		bytes_read = llapi_mirror_read(fd, src, buf, buflen, pos);
+	nr = count;
+	if (sparse) {
+		/* for sparse src we have to be sure that dst has no
+		 * data in src holes, so truncate it first
+		 */
+		for (i = 0; i < nr; i++) {
+			rc = llapi_mirror_truncate(fd, dst[i], pos);
+			if (rc < 0) {
+				result = rc;
+				/* exclude the failed one */
+				dst[i] = dst[--nr];
+				i--;
+				continue;
+			}
+		}
+		if (!nr)
+			return result;
+	}
+
+	while (!eof) {
+		off_t data_off;
+		ssize_t bytes_read;
+		size_t to_write, to_read;
+
+		if (sparse && pos >= data_end) {
+			size_t data_size;
+
+			data_off = llapi_mirror_data_seek(fd, src, pos,
+							  &data_size);
+			if (data_off < 0) {
+				/* Non-fatal, switch to full copy */
+				sparse = false;
+				continue;
+			}
+			if (!data_size) {
+				/* hole at the end of file, set pos to the
+				 * data_off, so truncate block at the end
+				 * will set final dst size.
+				 */
+				pos = data_off;
+				break;
+			}
+
+			data_end = data_off + data_size;
+			/* align by page */
+			pos = data_off & ~(page_size - 1);
+			data_end = ((data_end - 1) | (page_size - 1)) + 1;
+			to_read = MIN(data_end - pos, buflen);
+		} else {
+			to_read = buflen;
+		}
+
+		bytes_read = llapi_mirror_read(fd, src, buf, to_read, pos);
 		if (!bytes_read) { /* end of file */
 			break;
 		} else if (bytes_read < 0) {
@@ -267,12 +360,10 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 				i--;
 				continue;
 			}
-
 			assert(written == to_write);
 		}
-
 		pos += bytes_read;
-		eof = bytes_read < buflen;
+		eof = bytes_read < to_read;
 	}
 
 	free(buf);

lustre/include/lustre/lustreapi.h
@@ -608,6 +608,9 @@ int llapi_lease_put(int fd); /* obsoleted */
 int llapi_group_lock(int fd, int gid);
 int llapi_group_unlock(int fd, int gid);
 
+bool llapi_file_is_sparse(int fd);
+off_t llapi_data_seek(int src_fd, off_t offset, size_t *length);
+
 /* Ladvise */
 int llapi_ladvise(int fd, unsigned long long flags, int num_advise,
 		  struct llapi_lu_ladvise *ladvise);
@@ -1152,10 +1155,12 @@ ssize_t llapi_mirror_read(int fd, unsigned int id,
 			   void *buf, size_t count, off_t pos);
 ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count);
 int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst,
-		       off_t pos, size_t count);
+		      off_t pos, size_t count);
+off_t llapi_mirror_data_seek(int fd, unsigned int id, off_t pos, size_t *size);
 
 int llapi_heat_get(int fd, struct lu_heat *heat);
 int llapi_heat_set(int fd, __u64 flags);
+
 int llapi_layout_sanity(struct llapi_layout *layout, bool incomplete, bool flr);
 void llapi_layout_sanity_perror(int error);
 int llapi_layout_dom_size(struct llapi_layout *layout, uint64_t *size);

lustre/llite/file.c
@@ -4088,8 +4088,9 @@ out_state:
 	}
 }
 
-loff_t ll_lseek(struct inode *inode, loff_t offset, int whence)
+loff_t ll_lseek(struct file *file, loff_t offset, int whence)
 {
+	struct inode *inode = file_inode(file);
 	struct lu_env *env;
 	struct cl_io *io;
 	struct cl_lseek_io *lsio;
@@ -4105,6 +4106,7 @@ loff_t ll_lseek(struct inode *inode, loff_t offset, int whence)
 
 	io = vvp_env_thread_io(env);
 	io->ci_obj = ll_i2info(inode)->lli_clob;
+	ll_io_set_mirror(io, file);
 
 	lsio = &io->u.ci_lseek;
 	lsio->ls_start = offset;
@@ -4113,10 +4115,14 @@ loff_t ll_lseek(struct inode *inode, loff_t offset, int whence)
 
 	do {
 		rc = cl_io_init(env, io, CIT_LSEEK, io->ci_obj);
-		if (!rc)
+		if (!rc) {
+			struct vvp_io *vio = vvp_env_io(env);
+
+			vio->vui_fd = file->private_data;
 			rc = cl_io_loop(env, io);
-		else
+		} else {
 			rc = io->ci_result;
+		}
 		retval = rc ? : lsio->ls_result;
 		cl_io_fini(env, io);
 	} while (unlikely(io->ci_need_restart));
@@ -4153,7 +4159,7 @@ static loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 		cl_sync_file_range(inode, offset, OBD_OBJECT_EOF,
 				   CL_FSYNC_LOCAL, 0);
 
-		retval = ll_lseek(inode, offset, origin);
+		retval = ll_lseek(file, offset, origin);
 		if (retval < 0)
 			return retval;
 		retval = vfs_setpos(file, retval, ll_file_maxbytes(inode));

lustre/tests/sanity-flr.sh
@@ -2339,7 +2339,7 @@ test_49a() {
 }
 run_test 49a "FIEMAP upon FLR file"
 
-test_50() {	# EX-2179
+test_50A() {	# EX-2179
 	mkdir -p $DIR/$tdir
 
 	local file=$DIR/$tdir/$tfile
@@ -2365,7 +2365,105 @@ test_50() {	# EX-2179
 
 	$LFS getstripe -v $file || error "getstripe $file failed"
 }
-run_test 50 "mirror split update layout generation"
+run_test 50A "mirror split update layout generation"
+
+test_50a() {
+	$LCTL get_param osc.*.import | grep -q 'connect_flags:.*seek' ||
+		skip "OST does not support SEEK_HOLE"
+
+	local file=$DIR/$tdir/$tfile
+	local offset
+	local sum1
+	local sum2
+	local blocks
+
+	mkdir -p $DIR/$tdir
+
+	echo " ** create striped file $file"
+	$LFS setstripe -E 1M -c1 -S 1M -E eof -c2 -S1M $file ||
+		error "cannot create file with PFL layout"
+	echo " ** write 1st data chunk at 1M boundary"
+	dd if=/dev/urandom of=$file bs=1k count=20 seek=1021 ||
+		error "cannot write data at 1M boundary"
+	echo " ** write 2nd data chunk at 2M boundary"
+	dd if=/dev/urandom of=$file bs=1k count=20 seek=2041 ||
+		error "cannot write data at 2M boundary"
+	echo " ** create hole at the file end"
+	$TRUNCATE $file 3700000 || error "truncate fails"
+
+	echo " ** verify sparseness"
+	offset=$(lseek_test -d 1000 $file)
+	echo "    first data offset: $offset"
+	[[ $offset == 1000 ]] &&
+		error "src: data is not expected at offset $offset"
+	offset=$(lseek_test -l 3500000 $file)
+	echo "    hole at the end: $offset"
+	[[ $offset == 3500000 ]] ||
+		error "src: hole is expected at offset $offset"
+
+	echo " ** extend the file with new mirror"
+	# migrate_copy_data() is used
+	$LFS mirror extend -N -E 2M -S 1M -E 1G -S 2M -E eof $file ||
+		error "cannot create mirror"
+	$LFS getstripe $file | grep lcme_flags | grep stale > /dev/null &&
+		error "$file still has stale component"
+
+	# check migrate_data_copy() was correct
+	sum_1=$($LFS mirror read -N 1 $file | md5sum)
+	sum_2=$($LFS mirror read -N 2 $file | md5sum)
+	[[ $sum_1 == $sum_2 ]] ||
+		error "data mismatch: \'$sum_1\' vs. \'$sum_2\'"
+
+	# stale first mirror
+	$LFS setstripe --comp-set -I0x10001 --comp-flags=stale $file
+	$LFS setstripe --comp-set -I0x10002 --comp-flags=stale $file
+
+	echo " ** verify mirror #2 sparseness"
+	offset=$(lseek_test -d 1000 $file)
+	echo "    first data offset: $offset"
+	[[ $offset == 1000 ]] &&
+		error "dst: data is not expected at offset $offset"
+	offset=$(lseek_test -l 3500000 $file)
+	echo "    hole at the end: $offset"
+	[[ $offset == 3500000 ]] ||
+		error "dst: hole is expected at offset $offset"
+
+	echo " ** copy mirror #2 to mirror #1"
+	$LFS mirror copy -i 2 -o 1 $file || error "mirror copy fails"
+	$LFS getstripe $file | grep lcme_flags | grep stale > /dev/null &&
+		error "$file still has stale component"
+
+	# check llapi_mirror_copy_many correctness
+	sum_1=$($LFS mirror read -N 1 $file | md5sum)
+	sum_2=$($LFS mirror read -N 2 $file | md5sum)
+	[[ $sum_1 == $sum_2 ]] ||
+		error "data mismatch: \'$sum_1\' vs. \'$sum_2\'"
+
+	# stale 1st component of mirror #2 before lseek call
+	$LFS setstripe --comp-set -I0x20001 --comp-flags=stale $file
+
+	echo " ** verify mirror #1 sparseness again"
+	offset=$(lseek_test -d 1000 $file)
+	echo "    first data offset: $offset"
+	[[ $offset == 1000 ]] &&
+		error "dst: data is not expected at offset $offset"
+	offset=$(lseek_test -l 3500000 $file)
+	echo "    hole at the end: $offset"
+	[[ $offset == 3500000 ]] ||
+		error "dst: hole is expected at offset $offset"
+
+	cancel_lru_locks osc
+
+	blocks=$(stat -c%b $file)
+	echo " ** final consumed blocks: $blocks"
+	# for 3.5Mb file consumes ~6000 blocks, use 1000 to check
+	# that file is still sparse
+	(( blocks < 1000 )) ||
+		error "Mirrored file consumes $blocks blocks"
+
+	rm $file
+}
+run_test 50a "mirror extend/copy preserves sparseness"
 
 ctrl_file=$(mktemp /tmp/CTRL.XXXXXX)
 lock_file=$(mktemp /var/lock/FLR.XXXXXX)

lustre/utils/Makefile.am
@@ -103,7 +103,8 @@ liblustreapi_la_SOURCES = liblustreapi.c liblustreapi_hsm.c \
 			  liblustreapi_kernelconn.c liblustreapi_param.c \
 			  liblustreapi_mirror.c liblustreapi_fid.c \
 			  liblustreapi_ladvise.c liblustreapi_chlg.c \
-			  liblustreapi_heat.c liblustreapi_pcc.c
+			  liblustreapi_heat.c liblustreapi_pcc.c \
+			  liblustreapi_lseek.c
 liblustreapi_la_LDFLAGS = $(LIBREADLINE) -version-info 1:0:0 \
 			  -Wl,--version-script=liblustreapi.map
 liblustreapi_la_LIBADD = $(top_builddir)/libcfs/libcfs/libcfs.la

lustre/utils/liblustreapi_lseek.c
@@ -1,0 +1,111 @@
+/*
+ * LGPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * (C) Copyright (c) 2020, DataDirect Networks Inc, all rights reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * LGPL version 2.1 or (at your discretion) any later version.
+ * LGPL version 2.1 accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * LGPL HEADER END
+ */
+/*
+ * lustre/utils/liblustreapi_lseek.c
+ *
+ * lustreapi library for lseek-related functionality
+ *
+ * Author: Mikhail Pershin <mpershin@whamcloud.com>
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+#include <lustre/lustreapi.h>
+#include "lustreapi_internal.h"
+
+/**
+ * Check if file has a hole
+ *
+ * \param fd	file descriptor
+ *
+ * \retval boolean, true if file has a hole, false otherwise
+ */
+bool llapi_file_is_sparse(int fd)
+{
+	off_t file_end, hole_off;
+
+	file_end = lseek(fd, 0, SEEK_END);
+	hole_off = lseek(fd, 0, SEEK_HOLE);
+
+	/* Errors are ignored and file is just reported as non-sparse */
+	return file_end > 0 && hole_off >= 0 && hole_off < file_end;
+}
+
+/**
+ * Get the first data segment in given extent.
+ *
+ * \param src_fd  source file descriptor
+ * \param offset  offset to start from
+ * \param length  length of data segment found
+ *
+ * \retval next data offset and length on \p length on success.
+ * \retval -errno on failure.
+ */
+off_t llapi_data_seek(int src_fd, off_t offset, size_t *length)
+{
+	off_t data_off, hole_off;
+	int rc;
+
+	if (offset < 0) {
+		rc = -EINVAL;
+		llapi_error(LLAPI_MSG_ERROR, rc, "wrong offset: %jd",
+			    offset);
+		return rc;
+	}
+
+	data_off = lseek(src_fd, offset, SEEK_DATA);
+	if (data_off < 0) {
+		if (errno != ENXIO) {
+			rc = -errno;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "failed SEEK_DATA from %jd",
+				    offset);
+			return rc;
+		}
+		hole_off = lseek(src_fd, 0, SEEK_END);
+		if (data_off > hole_off) /* out of file range */
+			return -ENXIO;
+		/* no more data in src file, return end of file and zero size
+		 * so caller will know there must be hole up to that offset
+		 */
+		*length = 0;
+		return hole_off;
+	}
+
+	hole_off = lseek(src_fd, data_off, SEEK_HOLE);
+	if (hole_off < 0) {
+		rc = -errno;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "failed SEEK_HOLE from %jd", data_off);
+		return rc;
+	}
+	*length = hole_off - data_off;
+	return data_off;
+}

commit b02a9bc1bf1af07cf7784f7f19ce10dc55b7bedb

LU-17000 utils: Add check after calling sysconf(_SC_PAGESIZE)
Calling sysconf(_SC_PAGESIZE) could return -1
on error. This patch adds check after calling
sysconf() instead of directly using the value

Test-Parameters: trivial testlist=sanity-flr
CoverityID: 397578 ("Argument cannot be negative")
CoverityID: 397246 ("Argument cannot be negative")
CoverityID: 397320 ("Argument cannot be negative")
CoverityID: 397671 ("Argument cannot be negative")
CoverityID: 397826 ("Argument cannot be negative")
CoverityID: 397898 ("Argument cannot be negative")
CoverityID: 397917 ("Argument cannot be negative")
CoverityID: 399702 ("Argument cannot be negative")
Fixes: 0561c144 (LU-13397 lfs: mirror extend/copy keeps sparseness)
Fixes: a5905b2a (LU-11245 flr: lfs mirror dump command)
Fixes: f1daa8fc (LU-10287 flr: lfs mirror verify command)
Fixes: 0e5c12ac (LU-10916 lfs: improve lfs mirror resync)
Fixes: 5d7c4fa6 (LU-9771 flr: mirror read and write)
Fixes: 5999c0b8 (LU-9771 flr: resync support and test tool)
Fixes: 9b44cf70 (LU-13224 utils: expose llapi_param* functions)
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I19d13528f63d4586a17aaa9d15313872f8c40c94
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53693
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -807,7 +807,7 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 	void *buf = NULL;
 	off_t pos = 0;
 	off_t data_end = 0;
-	size_t page_size = sysconf(_SC_PAGESIZE);
+	size_t page_size;
 	bool sparse;
 	int rc;
 	size_t write_bytes = 0;
@@ -835,6 +835,12 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 	if (bandwidth_bytes_sec && bandwidth_bytes_sec < buf_size)
 		buf_size = (bandwidth_bytes_sec + stripe_size - 1) &
 			~(stripe_size - 1);
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		return rc;
+	}
 
 	/* Use a page-aligned buffer for direct I/O */
 	rc = posix_memalign(&buf, page_size, buf_size);
@@ -11715,6 +11721,7 @@ static inline int lfs_mirror_read(int argc, char **argv)
 	int c;
 	void *buf;
 	const size_t buflen = 4 << 20;
+	size_t page_size;
 	off_t pos;
 	struct option long_opts[] = {
 	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
@@ -11800,8 +11807,14 @@ static inline int lfs_mirror_read(int argc, char **argv)
 		outfd = STDOUT_FILENO;
 	}
 
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		goto close_fd;
+	}
+
 	/* allocate buffer */
-	rc = posix_memalign(&buf, sysconf(_SC_PAGESIZE), buflen);
+	rc = posix_memalign(&buf, page_size, buflen);
 	if (rc) {
 		fprintf(stderr, "%s %s: posix_memalign returns %d\n",
 				progname, argv[0], rc);
@@ -12610,7 +12623,7 @@ int lfs_mirror_verify_chunk(int fd, size_t file_size,
 {
 	const size_t buflen = 4 * 1024 * 1024; /* 4M */
 	void *buf;
-	size_t page_size = sysconf(_SC_PAGESIZE);
+	size_t page_size;
 	ssize_t bytes_read;
 	ssize_t bytes_done;
 	size_t count;
@@ -12622,6 +12635,12 @@ int lfs_mirror_verify_chunk(int fd, size_t file_size,
 
 	if (file_size == 0)
 		return 0;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		return rc;
+	}
 
 	rc = posix_memalign(&buf, page_size, buflen);
 	if (rc) /* error code is returned directly */

lustre/utils/liblustreapi_layout.c
@@ -3008,8 +3008,8 @@ int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 				    unsigned long stats_interval_sec,
 				    unsigned long bandwidth_bytes_sec)
 {
-	size_t page_size = sysconf(_SC_PAGESIZE);
 	size_t buflen = 64 << 20; /* 64M */
+	size_t page_size;
 	void *buf;
 	uint64_t pos = start;
 	uint64_t data_off = pos, data_end = pos;
@@ -3037,6 +3037,13 @@ int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 	/* limit transfer size to what can be sent in one second */
 	if (bandwidth_bytes_sec && bandwidth_bytes_sec < buflen)
 		buflen = (bandwidth_bytes_sec + ONE_MB - 1) & ~(ONE_MB - 1);
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		return rc;
+	}
+
 	rc = posix_memalign(&buf, page_size, buflen);
 	if (rc)
 		return -rc;

lustre/utils/liblustreapi_mirror.c
@@ -113,9 +113,15 @@ int llapi_mirror_clear(int fd)
 ssize_t llapi_mirror_read(int fd, unsigned int id, void *buf, size_t count,
 			  off_t pos)
 {
-	size_t page_size = sysconf(_SC_PAGESIZE);
 	ssize_t result = 0;
+	size_t page_size;
 	int rc;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		return rc;
+	}
 
 	rc = llapi_mirror_set(fd, id);
 	if (rc < 0)
@@ -150,9 +156,13 @@ ssize_t llapi_mirror_read(int fd, unsigned int id, void *buf, size_t count,
 ssize_t llapi_mirror_write(int fd, unsigned int id, const void *buf,
 			   size_t count, off_t pos)
 {
-	size_t page_size = sysconf(_SC_PAGESIZE);
 	ssize_t result = 0;
+	size_t page_size;
 	int rc;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0)
+		return -EINVAL;
 
 	if (((unsigned long)buf & (page_size - 1)) || pos & (page_size - 1))
 		return -EINVAL;
@@ -278,7 +288,7 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 	void *buf;
 	off_t pos = 0;
 	off_t data_end = 0;
-	size_t page_size = sysconf(_SC_PAGESIZE);
+	size_t page_size;
 	ssize_t result = 0;
 	bool eof = false;
 	bool sparse;
@@ -288,6 +298,12 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 
 	if (!count)
 		return 0;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		return rc;
+	}
 
 	rc = posix_memalign(&buf, page_size, buflen);
 	if (rc) /* error code is returned directly */
@@ -417,13 +433,17 @@ int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst, off_t pos,
 		      size_t count)
 {
 	const size_t buflen = 4 * 1024 * 1024; /* 4M */
-	void *buf;
-	size_t page_size = sysconf(_SC_PAGESIZE);
 	ssize_t result = 0;
+	size_t page_size;
+	void *buf;
 	int rc;
 
 	if (!count)
 		return 0;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0)
+		return -EINVAL;
 
 	if (pos & (page_size - 1) || !dst)
 		return -EINVAL;

lustre/utils/liblustreapi_param.c
@@ -202,10 +202,14 @@ int llapi_param_get_paths(const char *pattern, glob_t *paths)
  */
 static int bytes_remaining(int fd, size_t *file_size)
 {
-	int rc = 0;
 	size_t bytes_read = 0;
-	long page_size = sysconf(_SC_PAGESIZE);
+	long page_size;
 	char *temp_buf;
+	int rc = 0;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0)
+		return -EINVAL;
 
 	temp_buf = malloc(page_size);
 	if (temp_buf == NULL)
@@ -252,11 +256,16 @@ static int required_size(const char *path, size_t *file_size)
 static
 int copy_file_expandable(const char *path, char **buf, size_t *file_size)
 {
-	long page_size = sysconf(_SC_PAGESIZE);
-	int rc = 0;
+	long page_size;
 	char *temp_buf;
-	int fd;
+	int rc = 0, fd;
 	FILE *fp;
+
+	page_size = sysconf(_SC_PAGESIZE);
+	if (page_size < 0) {
+		rc = -errno;
+		goto out;
+	}
 
 	fp = open_memstream(buf, file_size);
 	if (fp == NULL) {