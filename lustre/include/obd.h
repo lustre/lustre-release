lustre/lustre-release

commit 0f2bc318d7a31d6a8960ab1e198d2bc686132b8f

LU-15246 ptlrpc: per-device adaptive timeout parameters
When a client is mounting multiple filesystems with different
MGSes setting global parameters at_min, at_max, etc., then the
settings from one filesystem's MGS config will also apply to RPCs
sent for the OSC, MDC, and MGC devices on the other filesystem(s).
Typically the settings of the last filesystem to mount on the client
override the earlier values, and there is no way to separate them.

Moving the parameters to be per-device values allows them to be
set independently for each set of client devices, so that the
client can interact properly with each set of servers.  This allows
e.g. different timeouts for local and remote mounts, or for flash
and HDD filesystems that have different load and performance.

Add per-device adaptive timeout parameters that can optionally
replace global parameters of the same name:

    at_min     -> *.<fsname>*.at_min
    at_max     -> *.<fsname>*.at_max
    at_history -> *.<fsname>*.at_history
    ldlm_enqeue_min -> *.<fsname>*.ldlm_enqueue_min

These parameters should always be set with fsname in the device
name, rather than pure wildcard '*' device names, or it will be
be same as the global parameters in the end (settings from one
MGS will apply to devices on other filesystems).  That is a bug
in how "lctl set_param -P" works, but will be fixed separately.

Signed-off-by: Lei Feng <flei@whamcloud.com>
Change-Id: I5b04c9aa53a446fb5a78bfaff372b4f236c9eb8a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/45598
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd.h
@@ -750,7 +750,46 @@ struct obd_device {
 	struct kset		        obd_kset; /* sysfs object collection */
 	struct kobj_type		obd_ktype;
 	struct completion		obd_kobj_unregister;
+
+	/* adaptive timeout parameters */
+	unsigned int			obd_at_min;
+	unsigned int			obd_at_max;
+	unsigned int			obd_at_history;
+	unsigned int			obd_ldlm_enqueue_min;
 };
+
+#define obd_get_at_min(obd) ({ \
+	struct obd_device *_obd = obd; \
+	if (_obd == NULL) \
+		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
+	_obd && _obd->obd_at_min ? _obd->obd_at_min : at_min; \
+})
+#define obd_get_at_max(obd) ({\
+	struct obd_device *_obd = obd; \
+	if (_obd == NULL) \
+		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
+	_obd && _obd->obd_at_max ? _obd->obd_at_max : at_max; \
+})
+#define obd_get_at_history(obd) ({ \
+	struct obd_device *_obd = obd; \
+	if (_obd == NULL) \
+		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
+	_obd && _obd->obd_at_history ? _obd->obd_at_history : at_history; \
+})
+extern unsigned int ldlm_enqueue_min;
+#define obd_get_ldlm_enqueue_min(obd) ({ \
+	struct obd_device *_obd = obd; \
+	if (_obd == NULL) \
+		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
+	_obd && _obd->obd_ldlm_enqueue_min ? _obd->obd_ldlm_enqueue_min : \
+					     ldlm_enqueue_min; \
+})
+#define obd_at_off(obd) (obd_get_at_max(obd) == 0)
+
+#define obd_at_get(obd, at) ({ \
+	timeout_t t1 = obd_get_at_min(obd); \
+	max_t(timeout_t, (at)->at_current_timeout, t1); \
+})
 
 int obd_uuid_add(struct obd_device *obd, struct obd_export *export);
 void obd_uuid_del(struct obd_device *obd, struct obd_export *export);

lustre/include/lprocfs_status.h
@@ -877,6 +877,31 @@ ssize_t lustre_attr_store(struct kobject *kobj, struct attribute *attr,
 
 extern const struct sysfs_ops lustre_sysfs_ops;
 
+#define LUSTRE_OBD_UINT_PARAM_ATTR(name)\
+static ssize_t name##_show(struct kobject *kobj, struct attribute *attr,\
+			   char *buf)					\
+{									\
+	int rc;								\
+	struct obd_device *obd =					\
+		container_of(kobj, struct obd_device, obd_kset.kobj);	\
+	rc = snprintf(buf, PAGE_SIZE, "%u\n", obd->obd_##name); \
+	return rc;							\
+}									\
+static ssize_t name##_store(struct kobject *kobj, struct attribute *attr,\
+			    const char *buffer, size_t count)		\
+{									\
+	int rc;								\
+	unsigned int val;						\
+	struct obd_device *obd =					\
+		container_of(kobj, struct obd_device, obd_kset.kobj);	\
+	rc = kstrtouint(buffer, 10, &val);				\
+	if (rc)								\
+		return rc;						\
+	obd->obd_##name = val;						\
+	return count;							\
+}									\
+LUSTRE_RW_ATTR(name)
+
 /* lproc_ptlrpc.c */
 struct ptlrpc_request;
 extern void target_print_req(void *seq_file, struct ptlrpc_request *req);

lustre/include/lustre_dlm.h
@@ -1808,7 +1808,6 @@ int ldlm_cli_inodebits_convert(struct ldlm_lock *lock,
 
 /** @} ldlm_cli_api */
 
-extern unsigned int ldlm_enqueue_min;
 
 /* mds/handler.c */
 /* This has to be here because recursive inclusion sucks. */

lustre/include/lustre_import.h
@@ -90,9 +90,9 @@ struct ptlrpc_at_array {
 
 #define IMP_AT_MAX_PORTALS 8
 struct imp_at {
-        int                     iat_portal[IMP_AT_MAX_PORTALS];
-        struct adaptive_timeout iat_net_latency;
-        struct adaptive_timeout iat_service_estimate[IMP_AT_MAX_PORTALS];
+	int			iat_portal[IMP_AT_MAX_PORTALS];
+	struct adaptive_timeout	iat_net_latency;
+	struct adaptive_timeout	iat_service_estimate[IMP_AT_MAX_PORTALS];
 };
 
 
@@ -252,7 +252,7 @@ struct obd_import {
 	int			  imp_initiated_at;
         /** Incremented every time we send reconnection request */
         __u32                     imp_conn_cnt;
-       /** 
+       /**
         * \see ptlrpc_free_committed remembers imp_generation value here
         * after a check to save on unnecessary replay list iterations
         */
@@ -409,17 +409,9 @@ static inline void at_reinit(struct adaptive_timeout *at, timeout_t timeout,
 	spin_unlock(&at->at_lock);
 }
 
-extern unsigned int at_min;
-extern unsigned int at_max;
-#define AT_OFF (at_max == 0)
+timeout_t obd_at_measure(struct obd_device *obd, struct adaptive_timeout *at,
+			    timeout_t timeout);
 
-static inline timeout_t at_get(struct adaptive_timeout *at)
-{
-	return (at->at_current_timeout > at_min) ?
-		at->at_current_timeout : at_min;
-}
-
-timeout_t at_measured(struct adaptive_timeout *at, timeout_t timeout);
 int import_at_get_index(struct obd_import *imp, int portal);
 
 /* genops.c */

lustre/include/lustre_net.h
@@ -2585,14 +2585,7 @@ static inline int ptlrpc_no_resend(struct ptlrpc_request *req)
 	return req->rq_no_resend;
 }
 
-static inline int
-ptlrpc_server_get_timeout(struct ptlrpc_service_part *svcpt)
-{
-	int at = AT_OFF ? 0 : at_get(&svcpt->scp_at_estimate);
-
-	return svcpt->scp_service->srv_watchdog_factor *
-	       max_t(int, at, obd_timeout);
-}
+int ptlrpc_server_get_timeout(struct ptlrpc_service_part *svcpt);
 
 static inline struct ptlrpc_service *
 ptlrpc_req2svc(struct ptlrpc_request *req)

lustre/ldlm/ldlm_lib.c
@@ -2029,8 +2029,8 @@ check_and_start_recovery_timer(struct obd_device *obd,
 		 * Teach server about old server's estimates, as first guess
 		 * at how long new requests will take.
 		 */
-		at_measured(&req->rq_rqbd->rqbd_svcpt->scp_at_estimate,
-			    service_timeout);
+		obd_at_measure(obd, &req->rq_rqbd->rqbd_svcpt->scp_at_estimate,
+			       service_timeout);
 
 	target_start_recovery_timer(obd);
 
@@ -2424,12 +2424,13 @@ static void handle_recovery_req(struct ptlrpc_thread *thread,
 	/* don't reset timer for final stage */
 	if (!exp_finished(req->rq_export)) {
 		timeout_t timeout = obd_timeout;
+		struct obd_device *obd = req->rq_export->exp_obd;
 
 		/**
 		 * Add request @timeout to the recovery time so next request from
 		 * this client may come in recovery time
 		 */
-		if (!AT_OFF) {
+		if (!obd_at_off(obd)) {
 			struct ptlrpc_service_part *svcpt;
 			timeout_t est_timeout;
 
@@ -2441,7 +2442,7 @@ static void handle_recovery_req(struct ptlrpc_thread *thread,
 			 * use the maxium timeout here for waiting the client
 			 * sending the next req
 			 */
-			est_timeout = at_get(&svcpt->scp_at_estimate);
+			est_timeout = obd_at_get(obd, &svcpt->scp_at_estimate);
 			timeout = max_t(timeout_t, at_est2timeout(est_timeout),
 					lustre_msg_get_timeout(req->rq_reqmsg));
 			/*

lustre/ldlm/ldlm_lockd.c
@@ -438,7 +438,8 @@ static void ldlm_add_blocked_lock(struct ldlm_lock *lock)
 
 static int ldlm_add_waiting_lock(struct ldlm_lock *lock, timeout_t timeout)
 {
-	int ret;
+	struct obd_device *obd = NULL;
+	int at_off, ret;
 
 	/* NB: must be called with hold of lock_res_and_lock() */
 	LASSERT(ldlm_is_res_locked(lock));
@@ -448,9 +449,12 @@ static int ldlm_add_waiting_lock(struct ldlm_lock *lock, timeout_t timeout)
 	 * Do not put cross-MDT lock in the waiting list, since we
 	 * will not evict it due to timeout for now
 	 */
-	if (lock->l_export != NULL &&
-	    (exp_connect_flags(lock->l_export) & OBD_CONNECT_MDS_MDS))
-		return 0;
+	if (lock->l_export != NULL) {
+		obd = lock->l_export->exp_obd;
+
+		if (exp_connect_flags(lock->l_export) & OBD_CONNECT_MDS_MDS)
+			return 0;
+	}
 
 	spin_lock_bh(&waiting_locks_spinlock);
 	if (ldlm_is_cancel(lock)) {
@@ -484,9 +488,10 @@ static int ldlm_add_waiting_lock(struct ldlm_lock *lock, timeout_t timeout)
 	if (ret)
 		ldlm_add_blocked_lock(lock);
 
+	at_off = obd_at_off(obd);
 	LDLM_DEBUG(lock, "%sadding to wait list(timeout: %d, AT: %s)",
 		   ret == 0 ? "not re-" : "", timeout,
-		   AT_OFF ? "off" : "on");
+		   at_off ? "off" : "on");
 	return ret;
 }
 
@@ -631,8 +636,9 @@ int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, timeout_t timeout)
 timeout_t ldlm_bl_timeout(struct ldlm_lock *lock)
 {
 	timeout_t timeout;
+	struct obd_device *obd = lock->l_export->exp_obd;
 
-	if (AT_OFF)
+	if (obd_at_off(obd))
 		return obd_timeout / 2;
 
 	/*
@@ -641,9 +647,9 @@ timeout_t ldlm_bl_timeout(struct ldlm_lock *lock)
 	 * It would be nice to have some kind of "early reply" mechanism for
 	 * lock callbacks too...
 	 */
-	timeout = at_get(&lock->l_export->exp_bl_lock_at);
+	timeout = obd_at_get(obd, &lock->l_export->exp_bl_lock_at);
 	return max_t(timeout_t, timeout + (timeout >> 1),
-		     (timeout_t)ldlm_enqueue_min);
+		     (timeout_t)obd_get_ldlm_enqueue_min(obd));
 }
 EXPORT_SYMBOL(ldlm_bl_timeout);
 
@@ -667,8 +673,9 @@ timeout_t ldlm_bl_timeout_by_rpc(struct ptlrpc_request *req)
 {
 	struct ptlrpc_service_part *svcpt = req->rq_rqbd->rqbd_svcpt;
 	timeout_t timeout, req_timeout, at_timeout, netl;
+	struct obd_device *obd = req->rq_export->exp_obd;
 
-	if (AT_OFF)
+	if (obd_at_off(obd))
 		return obd_timeout / 2;
 
 	/* A blocked lock means somebody in the cluster is waiting, and we
@@ -681,9 +688,11 @@ timeout_t ldlm_bl_timeout_by_rpc(struct ptlrpc_request *req)
 	 * Either this on the next RPC times out, take the max.
 	 * Considering the current RPC, take just the left time.
 	 */
-	netl = at_get(&req->rq_export->exp_imp_reverse->imp_at.iat_net_latency);
+	netl = obd_at_get(obd,
+			  &req->rq_export->exp_imp_reverse->imp_at.iat_net_latency);
 	req_timeout = req->rq_deadline - ktime_get_real_seconds() + netl;
-	at_timeout = at_est2timeout(at_get(&svcpt->scp_at_estimate)) + netl;
+	at_timeout = at_est2timeout(obd_at_get(obd, &svcpt->scp_at_estimate))
+				    + netl;
 	req_timeout = max(req_timeout, at_timeout);
 
 	/* Take 1 re-connect failure and 1 re-connect success into account. */
@@ -691,7 +700,8 @@ timeout_t ldlm_bl_timeout_by_rpc(struct ptlrpc_request *req)
 
 	/* Client's timeout is calculated as at_est2timeout(), let's be a bit
 	 * more conservative than client */
-	return max(timeout + (timeout >> 4), (timeout_t)ldlm_enqueue_min);
+	return max(timeout + (timeout >> 4),
+		   (timeout_t)obd_get_ldlm_enqueue_min(obd));
 }
 EXPORT_SYMBOL(ldlm_bl_timeout_by_rpc);
 
@@ -936,6 +946,7 @@ int ldlm_server_blocking_ast(struct ldlm_lock *lock,
 	struct ptlrpc_request  *req;
 	int instant_cancel = 0;
 	int rc = 0;
+	struct obd_device *obd;
 
 	ENTRY;
 
@@ -950,7 +961,9 @@ int ldlm_server_blocking_ast(struct ldlm_lock *lock,
 
 	LASSERT(lock);
 	LASSERT(data != NULL);
-	if (lock->l_export->exp_obd->obd_recovering != 0)
+
+	obd = lock->l_export->exp_obd;
+	if (obd->obd_recovering != 0)
 		LDLM_ERROR(lock, "BUG 6063: lock collide during recovery");
 
 	ldlm_lock_reorder_req(lock);
@@ -1019,7 +1032,7 @@ int ldlm_server_blocking_ast(struct ldlm_lock *lock,
 
 	req->rq_send_state = LUSTRE_IMP_FULL;
 	/* ptlrpc_request_alloc_pack already set timeout */
-	if (AT_OFF)
+	if (obd_at_off(obd))
 		req->rq_timeout = ldlm_get_rq_timeout();
 
 	if (lock->l_export && lock->l_export->exp_nid_stats &&
@@ -1048,6 +1061,7 @@ int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data)
 	int instant_cancel = 0;
 	int rc = 0;
 	int lvb_len;
+	struct obd_device *obd;
 
 	ENTRY;
 
@@ -1059,6 +1073,7 @@ int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data)
 		RETURN(0);
 	}
 
+	obd = lock->l_export->exp_obd;
 	req = ptlrpc_request_alloc(lock->l_export->exp_imp_reverse,
 				   &RQF_LDLM_CP_CALLBACK);
 	if (req == NULL)
@@ -1120,7 +1135,7 @@ int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data)
 
 	req->rq_send_state = LUSTRE_IMP_FULL;
 	/* ptlrpc_request_pack already set timeout */
-	if (AT_OFF)
+	if (obd_at_off(obd))
 		req->rq_timeout = ldlm_get_rq_timeout();
 
 	/* We only send real blocking ASTs after the lock is granted */
@@ -1182,6 +1197,7 @@ int ldlm_server_glimpse_ast(struct ldlm_lock *lock, void *data)
 	struct ldlm_cb_async_args *ca;
 	int rc;
 	struct req_format *req_fmt;
+	struct obd_device *obd = lock->l_export->exp_obd;
 
 	ENTRY;
 
@@ -1223,7 +1239,7 @@ int ldlm_server_glimpse_ast(struct ldlm_lock *lock, void *data)
 
 	req->rq_send_state = LUSTRE_IMP_FULL;
 	/* ptlrpc_request_alloc_pack already set timeout */
-	if (AT_OFF)
+	if (obd_at_off(obd))
 		req->rq_timeout = ldlm_get_rq_timeout();
 
 	req->rq_interpret_reply = ldlm_cb_interpret;
@@ -1815,7 +1831,9 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 			LDLM_DEBUG(lock,
 				   "server cancels blocked lock after %ds",
 				   delay);
-			at_measured(&lock->l_export->exp_bl_lock_at, delay);
+			obd_at_measure(lock->l_export->exp_obd,
+				       &lock->l_export->exp_bl_lock_at,
+				       delay);
 		}
 		ldlm_lock_cancel(lock);
 		LDLM_LOCK_PUT(lock);

lustre/ldlm/ldlm_request.c
@@ -174,8 +174,11 @@ EXPORT_SYMBOL(is_granted_or_cancelled_nolock);
 static timeout_t ldlm_cp_timeout(struct ldlm_lock *lock)
 {
 	timeout_t timeout;
+	struct obd_device *obd;
 
-	if (AT_OFF)
+	obd = class_exp2obd(lock->l_conn_export);
+
+	if (obd_at_off(obd))
 		return obd_timeout;
 
 	/*
@@ -183,8 +186,8 @@ static timeout_t ldlm_cp_timeout(struct ldlm_lock *lock)
 	 * lock from another client.  Server will evict the other client if it
 	 * doesn't respond reasonably, and then give us the lock.
 	 */
-	timeout = at_get(ldlm_lock_to_ns_at(lock));
-	return max(3 * timeout, (timeout_t)ldlm_enqueue_min);
+	timeout = obd_at_get(obd, ldlm_lock_to_ns_at(lock));
+	return max(3 * timeout, (timeout_t)obd_get_ldlm_enqueue_min(obd));
 }
 
 /**
@@ -201,6 +204,7 @@ static int ldlm_completion_tail(struct ldlm_lock *lock, void *data)
 	} else if (data == NULL) {
 		LDLM_DEBUG(lock, "client-side enqueue: granted");
 	} else {
+		struct obd_device *obd = class_exp2obd(lock->l_conn_export);
 		/* Take into AT only CP RPC, not immediately granted locks */
 		timeout_t delay = 0;
 
@@ -213,7 +217,7 @@ static int ldlm_completion_tail(struct ldlm_lock *lock, void *data)
 		LDLM_DEBUG(lock, "client-side enqueue: granted after %ds",
 			   delay);
 		/* Update our time estimate */
-		at_measured(ldlm_lock_to_ns_at(lock), delay);
+		obd_at_measure(obd, ldlm_lock_to_ns_at(lock), delay);
 	}
 	return result;
 }

commit 5b872613b1adbd4add57bf82fac6cebfbd09d093

LU-17285 obdclass: remove debug from obd_get_at_*
Removing debugging CDEBUG() from obd_get_at_*() helpers.
Messages like the following:
00010000:00100000:0.0:1699657756.854481:0:27978:0:
	(ldlm_request.c:181:ldlm_cp_timeout()) NULL obd

Fixes: 0f2bc318d7 ("LU-15246 ptlrpc: per-device adaptive timeout parameters")
Test-Parameters: trivial
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Iaaa964d4e3ca62de5fb273f865d7cd4c3a4f9a29
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53106
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd.h
@@ -758,27 +758,19 @@ struct obd_device {
 
 #define obd_get_at_min(obd) ({ \
 	struct obd_device *_obd = obd; \
-	if (_obd == NULL) \
-		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
 	_obd && _obd->obd_at_min ? _obd->obd_at_min : at_min; \
 })
 #define obd_get_at_max(obd) ({\
 	struct obd_device *_obd = obd; \
-	if (_obd == NULL) \
-		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
 	_obd && _obd->obd_at_max ? _obd->obd_at_max : at_max; \
 })
 #define obd_get_at_history(obd) ({ \
 	struct obd_device *_obd = obd; \
-	if (_obd == NULL) \
-		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
 	_obd && _obd->obd_at_history ? _obd->obd_at_history : at_history; \
 })
 extern unsigned int ldlm_enqueue_min;
 #define obd_get_ldlm_enqueue_min(obd) ({ \
 	struct obd_device *_obd = obd; \
-	if (_obd == NULL) \
-		CDEBUG(D_RPCTRACE, "NULL obd\n"); \
 	_obd && _obd->obd_ldlm_enqueue_min ? _obd->obd_ldlm_enqueue_min : \
 					     ldlm_enqueue_min; \
 })