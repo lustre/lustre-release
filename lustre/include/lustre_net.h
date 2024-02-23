lustre/lustre-release

commit babf0232273467b7199ec9a7c36047b1968913df

LU-13368 lnet: discard the callback
Lustre need a completion callback for event that request
has been sent. And then need other callback when reply
arrived. Sometime the request completion callback maybe
lost by some reason even reply has been received.
system will wait forever even timeout. We needn't to wait
request completion in such case. So provide a way to
discard the callback.

Signed-off-by: Yang Sheng <ys@whamcloud.com>
Change-Id: If9cd8420ee76947ee5053180e0f5219f76bb94c2
Reviewed-on: https://review.whamcloud.com/38845
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_net.h
@@ -2501,8 +2501,10 @@ ptlrpc_client_recv(struct ptlrpc_request *req)
 	return req->rq_receiving_reply;
 }
 
+#define ptlrpc_cli_wait_unlink(req) __ptlrpc_cli_wait_unlink(req, NULL)
+
 static inline int
-ptlrpc_client_recv_or_unlink(struct ptlrpc_request *req)
+__ptlrpc_cli_wait_unlink(struct ptlrpc_request *req, bool *discard)
 {
 	int rc;
 
@@ -2514,6 +2516,15 @@ ptlrpc_client_recv_or_unlink(struct ptlrpc_request *req)
 	if (req->rq_req_deadline > ktime_get_real_seconds()) {
 		spin_unlock(&req->rq_lock);
 		return 1;
+	}
+
+	if (discard) {
+		*discard = false;
+		if (req->rq_reply_unlinked && req->rq_req_unlinked == 0) {
+			*discard = true;
+			spin_unlock(&req->rq_lock);
+			return 1; /* Should call again after LNetMDUnlink */
+		}
 	}
 
 	rc = !req->rq_req_unlinked || !req->rq_reply_unlinked ||

lustre/ptlrpc/client.c
@@ -1880,7 +1880,7 @@ int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
 			 * not corrupt any data.
 			 */
 			if (req->rq_phase == RQ_PHASE_UNREG_RPC &&
-			    ptlrpc_client_recv_or_unlink(req))
+			    ptlrpc_cli_wait_unlink(req))
 				continue;
 			if (req->rq_phase == RQ_PHASE_UNREG_BULK &&
 			    ptlrpc_client_bulk_active(req))
@@ -1918,7 +1918,7 @@ int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
 			/*
 			 * Check if we still need to wait for unlink.
 			 */
-			if (ptlrpc_client_recv_or_unlink(req) ||
+			if (ptlrpc_cli_wait_unlink(req) ||
 			    ptlrpc_client_bulk_active(req))
 				continue;
 			/* If there is no need to resend, fail it now. */
@@ -2758,6 +2758,7 @@ EXPORT_SYMBOL(ptlrpc_req_xid);
  */
 static int ptlrpc_unregister_reply(struct ptlrpc_request *request, int async)
 {
+	bool discard = false;
 	/*
 	 * Might sleep.
 	 */
@@ -2772,15 +2773,18 @@ static int ptlrpc_unregister_reply(struct ptlrpc_request *request, int async)
 	/*
 	 * Nothing left to do.
 	 */
-	if (!ptlrpc_client_recv_or_unlink(request))
+	if (!__ptlrpc_cli_wait_unlink(request, &discard))
 		RETURN(1);
 
 	LNetMDUnlink(request->rq_reply_md_h);
 
+	if (discard) /* Discard the request-out callback */
+		__LNetMDUnlink(request->rq_req_md_h, discard);
+
 	/*
 	 * Let's check it once again.
 	 */
-	if (!ptlrpc_client_recv_or_unlink(request))
+	if (!ptlrpc_cli_wait_unlink(request))
 		RETURN(1);
 
 	/* Move to "Unregistering" phase as reply was not unlinked yet. */
@@ -2809,7 +2813,7 @@ static int ptlrpc_unregister_reply(struct ptlrpc_request *request, int async)
 		while (seconds > 0 &&
 		       wait_event_idle_timeout(
 			       *wq,
-			       !ptlrpc_client_recv_or_unlink(request),
+			       !ptlrpc_cli_wait_unlink(request),
 			       cfs_time_seconds(1)) == 0)
 			seconds -= 1;
 		if (seconds > 0) {

ldiskfs/kernel_patches/patches/base/ext4-htree-lock.patch
@@ -38,7 +38,7 @@ The idea is simple, like how pdirop is implement at LDLM level.
     while holding CW lock, we drop CW lock and take EX lock and retry.
   - disable pdirops by always EX lock on change and PR lock on lookup/readdir.
 
-Lustre-bug-id: LU-50" target="_blank">https://jira.whamcloud.com/browse/LU-50
+Lustre-bug-id: https://jira.whamcloud.com/browse/LU-50
 Lustre-change: http://review.whamcloud.com/375
 Signed-off-by: Liang Zhen <liang@whamcloud.com>
 

lnet/include/lnet/api.h
@@ -125,7 +125,8 @@ int LNetMDBind(const struct lnet_md *md_in,
 	       enum lnet_unlink unlink_in,
 	       struct lnet_handle_md *md_handle_out);
 
-int LNetMDUnlink(struct lnet_handle_md md_in);
+int __LNetMDUnlink(struct lnet_handle_md md_in, bool discard);
+#define LNetMDUnlink(handle) __LNetMDUnlink(handle, false)
 
 void lnet_assert_handler_unused(lnet_handler_t handler);
 /** @} lnet_md */

lnet/include/lnet/lib-lnet.h
@@ -669,6 +669,7 @@ void lnet_set_reply_msg_len(struct lnet_ni *ni, struct lnet_msg *msg,
 void lnet_detach_rsp_tracker(struct lnet_libmd *md, int cpt);
 void lnet_clean_zombie_rstqs(void);
 
+bool lnet_md_discarded(struct lnet_libmd *md);
 void lnet_finalize(struct lnet_msg *msg, int rc);
 bool lnet_send_error_simulation(struct lnet_msg *msg,
 				enum lnet_msg_hstatus *hstatus);

lnet/include/lnet/lib-types.h
@@ -226,6 +226,7 @@ struct lnet_libmd {
  * call.
  */
 #define LNET_MD_FLAG_HANDLING	 BIT(3)
+#define LNET_MD_FLAG_DISCARD	 BIT(4)
 
 struct lnet_test_peer {
 	/* info about peers we are trying to fail */

lnet/klnds/o2iblnd/o2iblnd.c
@@ -3052,6 +3052,7 @@ kiblnd_base_startup(struct net *ns)
 
 	spin_lock_init(&kiblnd_data.kib_connd_lock);
 	INIT_LIST_HEAD(&kiblnd_data.kib_connd_conns);
+	INIT_LIST_HEAD(&kiblnd_data.kib_connd_waits);
 	INIT_LIST_HEAD(&kiblnd_data.kib_connd_zombies);
 	INIT_LIST_HEAD(&kiblnd_data.kib_reconn_list);
 	INIT_LIST_HEAD(&kiblnd_data.kib_reconn_wait);

lnet/klnds/o2iblnd/o2iblnd.h
@@ -437,6 +437,8 @@ struct kib_data {
 	struct list_head	kib_reconn_list;
 	/* peers wait for reconnection */
 	struct list_head	kib_reconn_wait;
+	/* connections wait for completion */
+	struct list_head	kib_connd_waits;
 	/*
 	 * The second that peers are pulled out from \a kib_reconn_wait
 	 * for reconnection.
@@ -690,6 +692,8 @@ struct kib_conn {
 	__u16			ibc_queue_depth;
 	/* connections max frags */
 	__u16			ibc_max_frags;
+	/* count of timeout txs waiting on cq */
+	__u16			ibc_waits;
 	/* receive buffers owned */
 	unsigned int		ibc_nrx:16;
 	/* scheduled for attention */

commit a6886dba0ed8a622c9831cd33d310d933492c72d

LU-17379 ptlrpc: fix check for callback discard
In ptlrpc_unregister_reply() decision about need to
discard request-out callback is done too early, before
LNetMDUnlink() invokes reply callback. Therefore at the
monent of discard check rq_reply_unlinked is not set yet
and discard is skipped always.

Patch removes discard check from __ptlrpc_cli_wait_unlink()
and does that after LNetMDUnlink() call right inside
ptlrpc_unregister_reply().
That makes __ptlrpc_cli_wait_unlink() unused, so it was
removed and only ptlrpc_cli_wait_unlink() remains

Fixes: babf0232273 ("LU-13368 lnet: discard the callback")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I6448cafa8a0b81d7ba0172ad1709e75e592d4924
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53937
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_net.h
@@ -2476,10 +2476,8 @@ ptlrpc_client_recv(struct ptlrpc_request *req)
 	return req->rq_receiving_reply;
 }
 
-#define ptlrpc_cli_wait_unlink(req) __ptlrpc_cli_wait_unlink(req, NULL)
-
 static inline int
-__ptlrpc_cli_wait_unlink(struct ptlrpc_request *req, bool *discard)
+ptlrpc_cli_wait_unlink(struct ptlrpc_request *req)
 {
 	int rc;
 
@@ -2491,15 +2489,6 @@ __ptlrpc_cli_wait_unlink(struct ptlrpc_request *req, bool *discard)
 	if (req->rq_req_deadline > ktime_get_real_seconds()) {
 		spin_unlock(&req->rq_lock);
 		return 1;
-	}
-
-	if (discard) {
-		*discard = false;
-		if (req->rq_reply_unlinked && req->rq_req_unlinked == 0) {
-			*discard = true;
-			spin_unlock(&req->rq_lock);
-			return 1; /* Should call again after LNetMDUnlink */
-		}
 	}
 
 	rc = !req->rq_req_unlinked || !req->rq_reply_unlinked ||

lustre/ptlrpc/client.c
@@ -2800,7 +2800,7 @@ EXPORT_SYMBOL(ptlrpc_req_xid);
  */
 static int ptlrpc_unregister_reply(struct ptlrpc_request *request, int async)
 {
-	bool discard = false;
+	bool discard;
 	/*
 	 * Might sleep.
 	 */
@@ -2815,10 +2815,14 @@ static int ptlrpc_unregister_reply(struct ptlrpc_request *request, int async)
 	/*
 	 * Nothing left to do.
 	 */
-	if (!__ptlrpc_cli_wait_unlink(request, &discard))
+	if (!ptlrpc_cli_wait_unlink(request))
 		RETURN(1);
 
 	LNetMDUnlink(request->rq_reply_md_h);
+
+	spin_lock(&request->rq_lock);
+	discard = request->rq_reply_unlinked && !request->rq_req_unlinked;
+	spin_unlock(&request->rq_lock);
 
 	if (discard) /* Discard the request-out callback */
 		__LNetMDUnlink(request->rq_req_md_h, discard);