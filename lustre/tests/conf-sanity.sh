lustre/lustre-release

commit f3494a6e9ffeb82bf1b34e557b4cfda1eaf8ef9d

LU-9859 libcfs: refactor libcfs initialization.
Many lustre modules depend on libcfs having initialized
properly, but do not explicit check that it did.
When lustre is built as discrete modules, this does not
cause a problem because if the libcfs module fails
initialization, the other modules don't even get loaded.

When lustre is compiled into the kernel, all module_init()
routines get run, so they need to check the required initialization
succeeded.

This patch splits out the initialization of libcfs into a new
libcfs_setup(), and has all modules call that.

The misc_register() call is kept separate as it does not allocate any
resources and if it fails, it fails hard - no point in retrying.
Other set-up allocates resources and so is best delayed until they
are needed, and can be worth retrying.

Ideally, the initialization would happen at mount time (or similar)
rather than at load time.  Doing this requires each module to
check dependencies when they are activated rather than when
they are loaded.  Achieving that is a much larger job that would
have to progress in stages.

For now, this change ensures that if some initialization in libcfs
fails, other modules will fail-safe.

Linux-commit: 64bf0b1a079d61e9e059b9dc7a58e064c7d994ae

Change-Id: I6b5ecdba0defc6e033f78d8fc2b9be9e26c7f720
Signed-off-by: Mr. NeilBrown <neilb@suse.de>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52700
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/module.c
@@ -660,17 +660,23 @@ void lnet_remove_debugfs(struct ctl_table *table)
 }
 EXPORT_SYMBOL_GPL(lnet_remove_debugfs);
 
-static void *debugfs_state;
-static int __init libcfs_init(void)
-{
-	int rc;
+static DEFINE_MUTEX(libcfs_startup);
+static int libcfs_active;
 
-	cfs_arch_init();
+static void *debugfs_state;
+
+int libcfs_setup(void)
+{
+	int rc = -EINVAL;
+
+	mutex_lock(&libcfs_startup);
+	if (libcfs_active)
+		goto out;
 
 	rc = libcfs_debug_init(5 * 1024 * 1024);
 	if (rc < 0) {
 		pr_err("LustreError: libcfs_debug_init: rc = %d\n", rc);
-		return (rc);
+		goto cleanup_lock;
 	}
 
 	rc = cfs_cpu_init();
@@ -687,30 +693,16 @@ static int __init libcfs_init(void)
 
 	rc = cfs_crypto_register();
 	if (rc) {
-		CERROR("cfs_crypto_regster: error %d\n", rc);
+		CERROR("cfs_crypto_register: error %d\n", rc);
 		goto cleanup_wq;
 	}
 
-	lnet_insert_debugfs(lnet_table, THIS_MODULE, &debugfs_state);
-	if (!IS_ERR_OR_NULL(lnet_debugfs_root))
-		lnet_insert_debugfs_links(lnet_debugfs_symlinks);
-
-	rc = llcrypt_init();
-	if (rc) {
-		CERROR("llcrypt_init: error %d\n", rc);
-		goto cleanup_lnet;
-	}
-
-	CDEBUG(D_OTHER, "portals setup OK\n");
+	CDEBUG(D_OTHER, "libcfs setup OK\n");
+out:
+	libcfs_active = 1;
+	mutex_unlock(&libcfs_startup);
 	return 0;
 
-cleanup_lnet:
-	if (!IS_ERR_OR_NULL(lnet_debugfs_root)) {
-		debugfs_remove_recursive(lnet_debugfs_root);
-		lnet_debugfs_root = NULL;
-		lnet_debugfs_fini(&debugfs_state);
-	}
-	cfs_crypto_unregister();
 cleanup_wq:
 	destroy_workqueue(cfs_rehash_wq);
 	cfs_rehash_wq = NULL;
@@ -718,6 +710,26 @@ cleanup_cpu:
 	cfs_cpu_fini();
 cleanup_debug:
 	libcfs_debug_cleanup();
+cleanup_lock:
+	mutex_unlock(&libcfs_startup);
+	return rc;
+}
+EXPORT_SYMBOL(libcfs_setup);
+
+static int __init libcfs_init(void)
+{
+	int rc;
+
+	rc = cfs_arch_init();
+	if (rc < 0) {
+		CERROR("cfs_arch_init: error %d\n", rc);
+		return rc;
+	}
+
+	lnet_insert_debugfs(lnet_table, THIS_MODULE, &debugfs_state);
+	if (!IS_ERR_OR_NULL(lnet_debugfs_root))
+		lnet_insert_debugfs_links(lnet_debugfs_symlinks);
+
 	return rc;
 }
 
@@ -734,12 +746,8 @@ static void __exit libcfs_exit(void)
 	CDEBUG(D_MALLOC, "before Portals cleanup: kmem %lld\n",
 	       libcfs_kmem_read());
 
-	llcrypt_exit();
-
-	if (cfs_rehash_wq) {
+	if (cfs_rehash_wq)
 		destroy_workqueue(cfs_rehash_wq);
-		cfs_rehash_wq = NULL;
-	}
 
 	cfs_crypto_unregister();
 
@@ -753,6 +761,8 @@ static void __exit libcfs_exit(void)
 	rc = libcfs_debug_cleanup();
 	if (rc)
 		pr_err("LustreError: libcfs_debug_cleanup: rc = %d\n", rc);
+
+	cfs_arch_exit();
 }
 
 MODULE_AUTHOR("OpenSFS, Inc. <http://www.lustre.org/>");

lnet/lnet/module.c
@@ -429,7 +429,11 @@ static struct miscdevice lnet_dev = {
 static int __init lnet_init(void)
 {
 	int rc;
+
 	ENTRY;
+	rc = libcfs_setup();
+	if (rc)
+		return rc;
 
 	rc = lnet_lib_init();
 	if (rc != 0) {

lnet/selftest/module.c
@@ -92,6 +92,10 @@ lnet_selftest_init(void)
 	int rc = -ENOMEM;
 	int i;
 
+	rc = libcfs_setup();
+	if (rc)
+		return rc;
+
 	lst_serial_wq = alloc_ordered_workqueue("lst_s", 0);
 	if (!lst_serial_wq) {
 		CERROR("Failed to create serial WI scheduler for LST\n");

libcfs/include/libcfs/libcfs.h
@@ -67,6 +67,7 @@
 
 typedef s32 timeout_t;
 
+int libcfs_setup(void);
 int libcfs_ioctl(unsigned int cmd, struct libcfs_ioctl_data *data);
 
 extern struct workqueue_struct *cfs_rehash_wq;

libcfs/include/libcfs/linux/linux-misc.h
@@ -119,7 +119,7 @@ static inline int kref_read(const struct kref *kref)
 }
 #endif /* HAVE_KREF_READ */
 
-void cfs_arch_init(void);
+int cfs_arch_init(void);
 void cfs_arch_exit(void);
 
 #ifndef container_of_safe

libcfs/libcfs/linux/linux-prim.c
@@ -47,6 +47,7 @@
 #endif
 
 #include <lustre_compat.h>
+#include <libcfs/crypto/llcrypt.h>
 #include <libcfs/linux/linux-time.h>
 #include <libcfs/linux/linux-wait.h>
 #include <libcfs/linux/linux-misc.h>
@@ -202,7 +203,7 @@ void __init init_libcfs_vfree_atomic(void)
 	}
 }
 
-void __init cfs_arch_init(void)
+int __init cfs_arch_init(void)
 {
 	init_libcfs_vfree_atomic();
 
@@ -221,12 +222,15 @@ void __init cfs_arch_init(void)
 					  SLAB_PANIC | SLAB_RECLAIM_ACCOUNT,
 					  xarray_node_ctor);
 #endif
+	return llcrypt_init();
 }
 
 void __exit cfs_arch_exit(void)
 {
 	/* exit_libcfs_vfree_atomic */
 	flush_scheduled_work();
+
+	llcrypt_exit();
 }
 
 int cfs_kernel_write(struct file *filp, const void *buf, size_t count,

lnet/klnds/gnilnd/gnilnd.c
@@ -2840,6 +2840,10 @@ static int __init kgnilnd_init(void)
 	kgnilnd_insert_sysctl();
 	kgnilnd_proc_init();
 
+	rc = libcfs_setup();
+	if (rc)
+		return rc;
+
 	lnet_register_lnd(&the_kgnilnd);
 
 	return 0;

lnet/klnds/kfilnd/kfilnd.c
@@ -511,6 +511,10 @@ static int __init kfilnd_init(void)
 	if (rc)
 		goto err;
 
+	rc = libcfs_setup();
+	if (rc)
+		return rc;
+
 	/* Do any initialization of the transaction system */
 	rc = kfilnd_tn_init();
 	if (rc) {

commit d12b18195cedd629e94b5bafecd7c9509484c89f

LU-17418 libcfs: support debug setup for libcfs modules
Work was landed to make Lustre ensure key libcfs components
were initialized for both a module build and a build directly
into the kernel. This change resulted in an defect that allows
you to crash a node when you only load libcfs.ko and run a
user land tool to set a debugfs setting of libcfs. The debug
handling is critical to load before anything. Update Lustre
to handle both a module and builtin setup for Lustre. When
lustre is built into the kernel we can't control if libcfs_init()
is called first so have libcfs_setup() handle setting up the
debug handling. When built as a module have libcfs_init()
setup the debug handling instead. For both cases
libcfs_debug_init() is always called so make sure we only
initialize it only once. Add a test to validate this fix.

Fixes: f3494a6e9 ("LU-9859 libcfs: refactor libcfs initialization.")
Test-Parameters: trivial testlist=conf-sanity env=ONLY="5j"
Change-Id: If4a229e43b9e06a723546c03eb2b787ba0b16f5a
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53825
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/debug.c
@@ -642,11 +642,17 @@ static void libcfs_unregister_panic_notifier(void)
 					 &libcfs_panic_notifier);
 }
 
+static bool debug_started;
+
 int libcfs_debug_init(unsigned long bufsize)
 {
 	unsigned int max = libcfs_debug_mb;
 	int rc = 0;
 
+	if (debug_started)
+		return 0;
+
+	debug_started = true;
 	if (libcfs_console_max_delay <= 0 || /* not set by user or */
 	    libcfs_console_min_delay <= 0 || /* set to invalid values */
 	    libcfs_console_min_delay >= libcfs_console_max_delay) {
@@ -679,6 +685,7 @@ int libcfs_debug_cleanup(void)
 	kernel_param_lock(THIS_MODULE);
 	cfs_tracefile_exit();
 	kernel_param_unlock(THIS_MODULE);
+	debug_started = false;
 	return 0;
 }
 

libcfs/libcfs/module.c
@@ -667,7 +667,7 @@ static void *debugfs_state;
 
 int libcfs_setup(void)
 {
-	int rc = -EINVAL;
+	int rc = 0;
 
 	mutex_lock(&libcfs_startup);
 	if (libcfs_active)
@@ -720,9 +720,18 @@ static int __init libcfs_init(void)
 {
 	int rc;
 
+	mutex_lock(&libcfs_startup);
+	rc = libcfs_debug_init(5 * 1024 * 1024);
+	mutex_unlock(&libcfs_startup);
+	if (rc < 0) {
+		pr_err("LustreError: libcfs_debug_init: rc = %d\n", rc);
+		return rc;
+	}
+
 	rc = cfs_arch_init();
 	if (rc < 0) {
 		CERROR("cfs_arch_init: error %d\n", rc);
+		libcfs_debug_cleanup();
 		return rc;
 	}
 

lustre/tests/conf-sanity.sh
@@ -548,6 +548,15 @@ test_5i() {
 }
 run_test 5i "start mdt failure at mdt_quota_init()"
 
+test_5j() {
+	unload_modules
+	load_module ../libcfs/libcfs/libcfs ||
+		error "Failed to load modules libcfs rc = $?"
+	$LCTL set_param debug=all
+	rmmod -v libcfs
+}
+run_test 5j "verify libcfs doesn't crash when setting debug flags"
+
 test_6() {
 	setup
 	manual_umount_client