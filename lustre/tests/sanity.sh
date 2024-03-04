lustre/lustre-release

commit 4468f6c9d92448cb72c5a616ec74653e83ee8e10

LU-16025 llite: adjust read count as file got truncated
File read will not notice the file size truncate by another node,
and continue to read 0 filled pages beyond the new file size.

This patch add a confinement in the read to prevent the issue and
add a test case verifying the fix.

Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Ie51ba09201a1ca1464c3a3892d367590e978ee34
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47896
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/llite/file.c
@@ -1939,6 +1939,59 @@ ll_do_fast_read(struct kiocb *iocb, struct iov_iter *iter)
 	return result;
 }
 
+/**
+ * Confine read iter lest read beyond the EOF
+ *
+ * \param iocb [in]	kernel iocb
+ * \param to [in]	reader iov_iter
+ *
+ * \retval <0	failure
+ * \retval 0	success
+ * \retval >0	@iocb->ki_pos has passed the EOF
+ */
+static int file_read_confine_iter(struct lu_env *env, struct kiocb *iocb,
+				  struct iov_iter *to)
+{
+	struct cl_attr *attr = vvp_env_thread_attr(env);
+	struct file *file = iocb->ki_filp;
+	struct inode *inode = file_inode(file);
+	struct ll_inode_info *lli = ll_i2info(inode);
+	loff_t read_end = iocb->ki_pos + iov_iter_count(to);
+	loff_t kms;
+	loff_t size;
+	int rc;
+
+	cl_object_attr_lock(lli->lli_clob);
+	rc = cl_object_attr_get(env, lli->lli_clob, attr);
+	cl_object_attr_unlock(lli->lli_clob);
+	if (rc != 0)
+		return rc;
+
+	kms = attr->cat_kms;
+	/* if read beyond end-of-file, adjust read count */
+	if (kms > 0 && (iocb->ki_pos >= kms || read_end > kms)) {
+		rc = ll_glimpse_size(inode);
+		if (rc != 0)
+			return rc;
+
+		size = i_size_read(inode);
+		if (iocb->ki_pos >= size || read_end > size) {
+			CDEBUG(D_VFSTRACE,
+			       "%s: read [%llu, %llu] over eof, kms %llu, file_size %llu.\n",
+			       file_dentry(file)->d_name.name,
+			       iocb->ki_pos, read_end, kms, size);
+
+			if (iocb->ki_pos >= size)
+				return 1;
+
+			if (read_end > size)
+				iov_iter_truncate(to, size - iocb->ki_pos);
+		}
+	}
+
+	return rc;
+}
+
 /*
  * Read from a file (through the page cache).
  */
@@ -1952,6 +2005,7 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	__u16 refcheck;
 	ktime_t kstart = ktime_get();
 	bool cached;
+	bool stale_data = false;
 
 	ENTRY;
 
@@ -1962,6 +2016,16 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 
 	if (!iov_iter_count(to))
 		RETURN(0);
+
+	env = cl_env_get(&refcheck);
+	if (IS_ERR(env))
+		RETURN(PTR_ERR(env));
+
+	result = file_read_confine_iter(env, iocb, to);
+	if (result < 0)
+		GOTO(out, result);
+	else if (result > 0)
+		stale_data = true;
 
 	/**
 	 * Currently when PCC read failed, we do not fall back to the
@@ -1984,10 +2048,6 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	if (result < 0 || iov_iter_count(to) == 0)
 		GOTO(out, result);
 
-	env = cl_env_get(&refcheck);
-	if (IS_ERR(env))
-		RETURN(PTR_ERR(env));
-
 	args = ll_env_args(env);
 	args->u.normal.via_iter = to;
 	args->u.normal.via_iocb = iocb;
@@ -1999,8 +2059,18 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	else if (result == 0)
 		result = rc2;
 
-	cl_env_put(env, &refcheck);
 out:
+	cl_env_put(env, &refcheck);
+
+	if (stale_data && result > 0) {
+		/**
+		 * we've reached EOF before the read, the data read are cached
+		 * stale data.
+		 */
+		iov_iter_truncate(to, 0);
+		result = 0;
+	}
+
 	if (result > 0) {
 		ll_rw_stats_tally(ll_i2sbi(file_inode(file)), current->pid,
 				  file->private_data, iocb->ki_pos, result,

lustre/llite/glimpse.c
@@ -210,7 +210,12 @@ int cl_glimpse_size0(struct inode *inode, int agl)
 		} else if (result == 0) {
 			result = cl_glimpse_lock(env, io, inode, io->ci_obj,
 						 agl);
-			if (!agl && result == -EAGAIN)
+			/**
+			 * need to limit retries for FLR mirrors if fast read
+			 * is short because of concurrent truncate.
+			 */
+			if (!agl && result == -EAGAIN &&
+			    !io->ci_tried_all_mirrors)
 				io->ci_need_restart = 1;
 		}
 

lustre/lov/lov_cl_internal.h
@@ -375,8 +375,10 @@ static inline struct lov_layout_entry *lov_entry(struct lov_object *lov, int i)
 }
 
 #define lov_for_layout_entry(lov, entry, start, end)			\
-	for (entry = lov_entry(lov, start);				\
-	     entry <= lov_entry(lov, end); entry++)
+	if (lov->u.composite.lo_entries &&				\
+	    lov->u.composite.lo_entry_count > 0)			\
+		for (entry = lov_entry(lov, start);			\
+		     entry <= lov_entry(lov, end); entry++)
 
 #define lov_foreach_layout_entry(lov, entry)				\
 	lov_for_layout_entry(lov, entry, 0,				\

lustre/lov/lov_object.c
@@ -848,19 +848,17 @@ static int lov_delete_composite(const struct lu_env *env,
 				union lov_layout_state *state)
 {
 	struct lov_layout_entry *entry;
-	struct lov_layout_composite *comp = &state->composite;
 
 	ENTRY;
 
 	dump_lsm(D_INODE, lov->lo_lsm);
 
 	lov_layout_wait(env, lov);
-	if (comp->lo_entries)
-		lov_foreach_layout_entry(lov, entry) {
-			if (entry->lle_lsme && lsme_is_foreign(entry->lle_lsme))
-				continue;
+	lov_foreach_layout_entry(lov, entry) {
+		if (entry->lle_lsme && lsme_is_foreign(entry->lle_lsme))
+			continue;
 
-			lov_delete_raid0(env, lov, entry);
+		lov_delete_raid0(env, lov, entry);
 	}
 
 	RETURN(0);
@@ -1009,6 +1007,8 @@ static int lov_attr_get_composite(const struct lu_env *env,
 
 	attr->cat_size = 0;
 	attr->cat_blocks = 0;
+	attr->cat_kms = 0;
+
 	lov_foreach_layout_entry(lov, entry) {
 		struct cl_attr *lov_attr = NULL;
 		int index = lov_layout_entry_index(lov, entry);

lustre/tests/sanityn.sh
@@ -631,6 +631,51 @@ test_16h() {
 }
 run_test 16h "mmap read after truncate file"
 
+test_16i() {
+	local tf=$DIR/$tdir/$tfile
+	local tf2=$DIR2/$tdir/$tfile
+
+	test_mkdir $DIR/$tdir
+
+	# create file and populate data
+	cp /etc/passwd $tf || error "cp failed"
+
+	local size=$(stat -c %s $tf)
+
+	c1=$(dd if=$tf bs=1 2>/dev/null | od -x | tail -q -n4)
+	c2=$(dd if=$tf2 bs=1 2>/dev/null | od -x | tail -q -n4)
+
+	if [[ "$c1" != "$c2" ]]; then
+		echo "  ------- mount 1 read --------"
+		echo $c1
+		echo "  ------- mount 2 read --------"
+		echo $c2
+		error "content mismatch"
+	fi
+
+	echo "  ------- before truncate --------"
+	echo $c1
+
+	# truncate file
+	$TRUNCATE $tf $((size / 2)) || error "truncate file"
+
+	echo "  ------- after truncate --------"
+
+	# repeat the comparison
+	c1=$(dd if=$tf bs=1 2>/dev/null | od -x | tail -q -n4)
+	c2=$(dd if=$tf2 bs=1 2>/dev/null | od -x | tail -q -n4)
+
+	if [[ "$c1" != "$c2" ]]; then
+		echo "  ------- mount 1 read --------"
+		echo $c1
+		echo "  ------- mount 2 read --------"
+		echo $c2
+		error "content mismatch after truncate"
+	fi
+	echo $c2
+}
+run_test 16i "read after truncate file"
+
 test_17() { # bug 3513, 3667
 	remote_ost_nodsh && skip "remote OST with nodsh" && return
 

commit 35fb413683affe9d41f3521259c260de6caf81d9

LU-17482 llite: short read could mess up next read offset
When read reaches EOF, it could read data from stale pagecache, but
we need to restore the iocb->ki_pos so that next read could continue
from the correct offset.

Fixes: 4468f6c9d9 ("LU-16025 llite: adjust read count as file got truncated")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Ib8b62c41bf65f8efec82dda53fcfbdb68ad08b38
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53827
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Patrick Farrell <patrick.farrell@oracle.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/obd_support.h
@@ -637,6 +637,7 @@ extern bool obd_enable_health_write;
 #define OBD_FAIL_LOV_COMP_PATTERN		    0x1427
 #define OBD_FAIL_LOV_INVALID_OSTIDX		    0x1428
 #define OBD_FAIL_LLITE_DELAY_TRUNCATE		    0x1430
+#define OBD_FAIL_LLITE_READ_PAUSE		    0x1431
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

lustre/llite/file.c
@@ -2176,6 +2176,7 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	struct lu_env *env;
 	struct vvp_io_args *args;
 	struct file *file = iocb->ki_filp;
+	loff_t orig_ki_pos = iocb->ki_pos;
 	ssize_t result;
 	ssize_t rc2;
 	__u16 refcheck;
@@ -2204,6 +2205,8 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	else if (result > 0)
 		stale_data = true;
 
+	CFS_FAIL_TIMEOUT_ORSET(OBD_FAIL_LLITE_READ_PAUSE, CFS_FAIL_ONCE,
+			       cfs_fail_val);
 	/**
 	 * Currently when PCC read failed, we do not fall back to the
 	 * normal read path, just return the error.
@@ -2253,6 +2256,7 @@ out:
 		 * we've reached EOF before the read, the data read are cached
 		 * stale data.
 		 */
+		iocb->ki_pos = orig_ki_pos;
 		iov_iter_truncate(to, 0);
 		result = 0;
 	}

lustre/tests/multiop.c
@@ -638,6 +638,9 @@ int main(int argc, char **argv)
 			}
 
 			while (len > 0) {
+				off_t start, off;
+
+				start = lseek(fd, 0, SEEK_CUR);
 				rc = read(fd, buf_align, len);
 				if (rc == -1) {
 					save_errno = errno;
@@ -645,8 +648,9 @@ int main(int argc, char **argv)
 					exit(save_errno);
 				}
 				if (rc < len) {
-					fprintf(stderr, "short read: %lld/%u\n",
-						rc, len);
+					off = lseek(fd, 0, SEEK_CUR);
+					fprintf(stderr, "short read: %ld ->+ %u -> %ld %lld\n",
+						start, len, off, rc);
 					if (rc == 0)
 						break;
 				}

lustre/tests/sanity.sh
@@ -23867,7 +23867,7 @@ test_250() {
 }
 run_test 250 "Write above 16T limit"
 
-test_251() {
+test_251a() {
 	$LFS setstripe -c -1 -S 1048576 $DIR/$tfile
 
 	#define OBD_FAIL_LLITE_LOST_LAYOUT 0x1407
@@ -23882,7 +23882,29 @@ test_251() {
 
 	rm -f $DIR/$tfile
 }
-run_test 251 "Handling short read and write correctly"
+run_test 251a "Handling short read and write correctly"
+
+test_251b() {
+	dd if=/dev/zero of=$DIR/$tfile bs=1k count=4 ||
+		error "write $tfile failed"
+
+	sleep 2 && echo 12345 >> $DIR/$tfile &
+
+	#define OBD_FAIL_LLITE_READ_PAUSE 0x1431
+	$LCTL set_param fail_loc=0x1431 fail_val=5
+	# seek to 4096, 2 seconds later, file size expand to 4102, and after
+	# 5 seconds, read 10 bytes, the short read should
+	# report:
+	#                start ->+ read_len -> offset_after_read read_count
+	#     short read: 4096 ->+ 10 -> 4096 0
+	# not:
+	#     short read: 4096 ->+ 10 -> 4102 0
+	local off=$($MULTIOP $DIR/$tfile oO_RDONLY:z4096r10c 2>&1 | \
+			awk '/short read/ { print $7 }')
+	(( off == 4096 )) ||
+		error "short read should set offset at 4096, not $off"
+}
+run_test 251b "short read restore offset correctly"
 
 test_252() {
 	remote_mds_nodsh && skip "remote MDS with nodsh"