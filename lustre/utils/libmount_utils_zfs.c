lustre/lustre-release

commit c7e9bdf8d4bb5e1127eb87472fbf0414823d5461

LU-8191 utils: remove unused, fix non-static functions
Remove several functions which are never called.

Static analysis shows that a number of functions
could be made static. This patch declares several
functions in various Lustre utils static.

Some missing headers caused some functions being
incorrectly marked as possible candidates for
being made static. These missing headers have
been added.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Id51f922be57c33c011ee2f9e509ca164cc480edf
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51439
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/libmount_utils_zfs.c
@@ -320,7 +320,7 @@ static int osd_check_zfs_setup(void)
 }
 
 /* Write the server config as properties associated with the dataset */
-int zfs_write_ldd(struct mkfs_opts *mop)
+static int zfs_write_ldd(struct mkfs_opts *mop)
 {
 	struct lustre_disk_data *ldd = &mop->mo_ldd;
 	char *ds = mop->mo_device;
@@ -367,7 +367,7 @@ out:
 }
 
 /* Mark a property to be removed by the form of "key=" */
-int zfs_erase_ldd(struct mkfs_opts *mop, char *param)
+static int zfs_erase_ldd(struct mkfs_opts *mop, char *param)
 {
 	char key[ZFS_MAXPROPLEN] = "";
 
@@ -470,7 +470,7 @@ static int zfs_get_prop_params(zfs_handle_t *zhp, char *param)
  * Read the server config as properties associated with the dataset.
  * Missing entries as not treated error and are simply skipped.
  */
-int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
+static int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
 {
 	zfs_handle_t *zhp;
 	struct zfs_ldd_prop_bridge *bridge;
@@ -519,7 +519,7 @@ out:
 }
 
 /* Print ldd params */
-void zfs_print_ldd_params(struct mkfs_opts *mop)
+static void zfs_print_ldd_params(struct mkfs_opts *mop)
 {
 	char *from = mop->mo_ldd.ldd_params;
 	char *to;
@@ -554,7 +554,7 @@ void zfs_print_ldd_params(struct mkfs_opts *mop)
 	}
 }
 
-int zfs_is_lustre(char *ds, unsigned *mount_type)
+static int zfs_is_lustre(char *ds, unsigned int *mount_type)
 {
 	struct lustre_disk_data tmp_ldd;
 	int ret;
@@ -637,7 +637,7 @@ static int zfs_create_vdev(struct mkfs_opts *mop, char *vdev)
 	return ret;
 }
 
-int zfs_make_lustre(struct mkfs_opts *mop)
+static int zfs_make_lustre(struct mkfs_opts *mop)
 {
 	zfs_handle_t *zhp;
 	zpool_handle_t *php;
@@ -818,14 +818,14 @@ out:
 	return ret;
 }
 
-int zfs_enable_quota(struct mkfs_opts *mop)
+static int zfs_enable_quota(struct mkfs_opts *mop)
 {
 	fprintf(stderr, "this option is not only valid for zfs\n");
 	return ENOSYS;
 }
 
-int zfs_prepare_lustre(struct mkfs_opts *mop,
-		       char *wanted_mountopts, size_t len)
+static int zfs_prepare_lustre(struct mkfs_opts *mop,
+			      char *wanted_mountopts, size_t len)
 {
 	if (osd_check_zfs_setup() == 0)
 		return EINVAL;
@@ -846,7 +846,7 @@ int zfs_prepare_lustre(struct mkfs_opts *mop,
 	return 0;
 }
 
-int zfs_tune_lustre(char *dev, struct mount_opts *mop)
+static int zfs_tune_lustre(char *dev, struct mount_opts *mop)
 {
 	if (osd_check_zfs_setup() == 0)
 		return EINVAL;
@@ -854,7 +854,7 @@ int zfs_tune_lustre(char *dev, struct mount_opts *mop)
 	return 0;
 }
 
-int zfs_label_lustre(struct mount_opts *mop)
+static int zfs_label_lustre(struct mount_opts *mop)
 {
 	zfs_handle_t *zhp;
 	int ret;
@@ -872,7 +872,7 @@ int zfs_label_lustre(struct mount_opts *mop)
 	return ret;
 }
 
-int zfs_rename_fsname(struct mkfs_opts *mop, const char *oldname)
+static int zfs_rename_fsname(struct mkfs_opts *mop, const char *oldname)
 {
 	struct mount_opts opts;
 	char mntpt[] = "/tmp/mntXXXXXX";
@@ -936,7 +936,7 @@ out_rmdir:
 	return ret;
 }
 
-int zfs_init(void)
+static int zfs_init(void)
 {
 	int ret = 0;
 

lustre/utils/check_iam.c
@@ -56,7 +56,7 @@ struct node_info {
 	int node_type;
 };
 
-void usage(char *str)
+static void usage(char *str)
 {
 	printf("Usage: %s [-hrv] iam_file\n", str);
 }
@@ -79,7 +79,7 @@ struct iam_params {
 	int rc;
 };
 
-int check_idle_blocks(char *buf, struct iam_params *params)
+static int check_idle_blocks(char *buf, struct iam_params *params)
 {
 	struct iam_idle_head *idle;
 	int i;

lustre/utils/debug.c
@@ -59,6 +59,8 @@
 #include <linux/lnet/lnetctl.h>
 #include <libcfs/util/string.h>
 
+#include "obdctl.h"
+
 static char rawbuf[8192];
 static char *buf = rawbuf;
 static int max = 8192;

lustre/utils/gss/gss_util.c
@@ -80,6 +80,7 @@
 # include <com_err.h>
 #endif
 #include "lsupport.h"
+#include "svcgssd.h"
 
 #define GSSD_SERVICE_MGS			"lustre_mgs"
 #define GSSD_SERVICE_MDS			"lustre_mds"

lustre/utils/gss/lgss_keyring.c
@@ -194,10 +194,10 @@ out_params:
 	return rc;
 }
 
-int do_nego_rpc(struct lgss_nego_data *lnd,
-		gss_buffer_desc *gss_token,
-		struct lgss_init_res *gr,
-		int req_fd[2], int reply_fd[2])
+static int do_nego_rpc(struct lgss_nego_data *lnd,
+		       gss_buffer_desc *gss_token,
+		       struct lgss_init_res *gr,
+		       int req_fd[2], int reply_fd[2])
 {
 	struct lgssd_ioctl_param param;
 	struct passwd *pw;
@@ -535,7 +535,7 @@ static int lgssc_init_nego_data(struct lgss_nego_data *lnd,
         return 0;
 }
 
-void lgssc_fini_nego_data(struct lgss_nego_data *lnd)
+static void lgssc_fini_nego_data(struct lgss_nego_data *lnd)
 {
         OM_uint32       maj_stat, min_stat;
 

lustre/utils/gss/sk_utils.c
@@ -1297,9 +1297,9 @@ void sk_free_cred(struct sk_cred *skc)
  *
  * If the size is smaller it will take copy the first N bytes necessary to
  * fill the derived key. */
-int sk_kdf(gss_buffer_desc *derived_key , gss_buffer_desc *origin_key,
-	   gss_buffer_desc *key_binding_bufs, int numbufs,
-	   enum cfs_crypto_hash_alg hmac_alg)
+static int sk_kdf(gss_buffer_desc *derived_key, gss_buffer_desc *origin_key,
+		  gss_buffer_desc *key_binding_bufs, int numbufs,
+		  enum cfs_crypto_hash_alg hmac_alg)
 {
 	size_t remain;
 	size_t bytes;
@@ -1458,9 +1458,9 @@ int sk_compute_keys(struct sk_cred *skc)
 	return 0;
 }
 
-uint32_t __sk_compute_dh_key(struct sk_cred *skc,
-			     const gss_buffer_desc *pub_key,
-			     size_t *expected_len)
+static uint32_t __sk_compute_dh_key(struct sk_cred *skc,
+				    const gss_buffer_desc *pub_key,
+				    size_t *expected_len)
 {
 	gss_buffer_desc *dh_shared = &skc->sc_dh_shared_key;
 	uint32_t rc = GSS_S_FAILURE;

lustre/utils/gss/svcgssd.c
@@ -65,7 +65,7 @@ int null_enabled;
 int krb_enabled;
 int sk_enabled;
 
-void
+static void
 closeall(int min)
 {
 	DIR *dir = opendir("/proc/self/fd");
@@ -168,7 +168,7 @@ release_parent()
 	}
 }
 
-void
+static void
 sig_die(int signal)
 {
 	/* destroy krb5 machine creds */
@@ -181,7 +181,7 @@ sig_die(int signal)
 	exit(1);
 }
 
-void
+static void
 sig_hup(int signal)
 {
 	/* don't exit on SIGHUP */

lustre/utils/gss/svcgssd.h
@@ -31,6 +31,7 @@
 #ifndef _RPC_SVCGSSD_H_
 #define _RPC_SVCGSSD_H_
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <gssapi/gssapi.h>

commit f45a0288b00597bc797963f7aa01cae5167b024e

LU-17611 utils: fix wrong static declarations
Revert wrong changes made to zfs mount utils

Fixes: c7e9bdf8d4 ("LU-8191 utils: remove unused, fix non-static functions")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I162d349ebadbf93a89abf49bd41465979d561423
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54293
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/libmount_utils_zfs.c
@@ -320,7 +320,7 @@ static int osd_check_zfs_setup(void)
 }
 
 /* Write the server config as properties associated with the dataset */
-static int zfs_write_ldd(struct mkfs_opts *mop)
+int zfs_write_ldd(struct mkfs_opts *mop)
 {
 	struct lustre_disk_data *ldd = &mop->mo_ldd;
 	char *ds = mop->mo_device;
@@ -367,7 +367,7 @@ out:
 }
 
 /* Mark a property to be removed by the form of "key=" */
-static int zfs_erase_ldd(struct mkfs_opts *mop, char *param)
+int zfs_erase_ldd(struct mkfs_opts *mop, char *param)
 {
 	char key[ZFS_MAXPROPLEN] = "";
 
@@ -470,7 +470,7 @@ static int zfs_get_prop_params(zfs_handle_t *zhp, char *param)
  * Read the server config as properties associated with the dataset.
  * Missing entries as not treated error and are simply skipped.
  */
-static int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
+int zfs_read_ldd(char *ds,  struct lustre_disk_data *ldd)
 {
 	zfs_handle_t *zhp;
 	struct zfs_ldd_prop_bridge *bridge;
@@ -519,7 +519,7 @@ out:
 }
 
 /* Print ldd params */
-static void zfs_print_ldd_params(struct mkfs_opts *mop)
+void zfs_print_ldd_params(struct mkfs_opts *mop)
 {
 	char *from = mop->mo_ldd.ldd_params;
 	char *to;
@@ -554,7 +554,7 @@ static void zfs_print_ldd_params(struct mkfs_opts *mop)
 	}
 }
 
-static int zfs_is_lustre(char *ds, unsigned int *mount_type)
+int zfs_is_lustre(char *ds, unsigned int *mount_type)
 {
 	struct lustre_disk_data tmp_ldd;
 	int ret;
@@ -637,7 +637,7 @@ static int zfs_create_vdev(struct mkfs_opts *mop, char *vdev)
 	return ret;
 }
 
-static int zfs_make_lustre(struct mkfs_opts *mop)
+int zfs_make_lustre(struct mkfs_opts *mop)
 {
 	zfs_handle_t *zhp;
 	zpool_handle_t *php;
@@ -818,13 +818,13 @@ out:
 	return ret;
 }
 
-static int zfs_enable_quota(struct mkfs_opts *mop)
+int zfs_enable_quota(struct mkfs_opts *mop)
 {
 	fprintf(stderr, "this option is not only valid for zfs\n");
 	return ENOSYS;
 }
 
-static int zfs_prepare_lustre(struct mkfs_opts *mop,
+int zfs_prepare_lustre(struct mkfs_opts *mop,
 			      char *wanted_mountopts, size_t len)
 {
 	if (osd_check_zfs_setup() == 0)
@@ -846,7 +846,7 @@ static int zfs_prepare_lustre(struct mkfs_opts *mop,
 	return 0;
 }
 
-static int zfs_tune_lustre(char *dev, struct mount_opts *mop)
+int zfs_tune_lustre(char *dev, struct mount_opts *mop)
 {
 	if (osd_check_zfs_setup() == 0)
 		return EINVAL;
@@ -854,7 +854,7 @@ static int zfs_tune_lustre(char *dev, struct mount_opts *mop)
 	return 0;
 }
 
-static int zfs_label_lustre(struct mount_opts *mop)
+int zfs_label_lustre(struct mount_opts *mop)
 {
 	zfs_handle_t *zhp;
 	int ret;
@@ -872,7 +872,7 @@ static int zfs_label_lustre(struct mount_opts *mop)
 	return ret;
 }
 
-static int zfs_rename_fsname(struct mkfs_opts *mop, const char *oldname)
+int zfs_rename_fsname(struct mkfs_opts *mop, const char *oldname)
 {
 	struct mount_opts opts;
 	char mntpt[] = "/tmp/mntXXXXXX";
@@ -936,7 +936,7 @@ out_rmdir:
 	return ret;
 }
 
-static int zfs_init(void)
+int zfs_init(void)
 {
 	int ret = 0;
 