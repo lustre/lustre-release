lustre/lustre-release

commit 101f6e84889a9b48238ca320557101058d935fb0

LU-10391 obdclass: handle large NIDs for mount strings
Mount strings support using ':' as a delimiter but this is also
a part of the some NID strings like IPv6, so rework class_parse_value()
to only look at ':' when it occurs after '@'.

The mount utilities use the function convert_hostnames() to ensure
the mount string containing an NID is valid. This only works for
small size nids so migrate the function to handle large NIDs. This
should allow mounting with IPv6 or other large NID addresses.

In testing the userland  libcfs_ip_str2addr_size() had bugs that
rendered incorrect NID strings. Fix those issues.

Fixes: b6c702df5d4 ("LU-10391 libcfs: add large-nid string conversion functions.")
Change-Id: Ic9b2a368456ba75ceb5911ac7f75ae00d6123870
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50362
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Nathaniel Clark <nclark@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/obd_config.c
@@ -449,13 +449,20 @@ static int class_parse_value(char *buf, int opc, void *value, char **endh,
 
 	if (!buf)
 		return 1;
+
 	while (*buf == ',' || *buf == ':')
 		buf++;
 	if (*buf == ' ' || *buf == '/' || *buf == '\0')
 		return 1;
 
-	/* NID separators or end of NIDs */
-	endp = strpbrk(buf, ",: /");
+	/* NID separators or end of NIDs. Large NIDs can contain ':' so
+	 * skip ahead to @ and then look for one of the delimiters.
+	 */
+	endp = strchr(buf, '@');
+	if (!endp)
+		return 1;
+
+	endp = strpbrk(endp, ",: /");
 	if (!endp)
 		endp = buf + strlen(buf);
 

libcfs/libcfs/util/nidstrings.c
@@ -172,19 +172,33 @@ libcfs_ip_str2addr_size(const char *str, int nob,
 			__be32 *addr, size_t *alen)
 {
 	char *tmp = malloc(nob+1);
-	int ret = 1;
 
 	if (!tmp)
 		return 0;
 	memcpy(tmp, str, nob);
-	tmp[nob] = 0;
+	tmp[nob] = '\0';
 
-	if (inet_pton(AF_INET, tmp, (struct in_addr *)addr) == 0) {
-		*alen = 4;
+	*alen = 0;
+	if (inet_pton(AF_INET, tmp, (struct in_addr *)addr) == 1) {
+		struct in_addr *ipv4 = (struct in_addr *)addr;
+
+		/* Don't allow using loopback */
+		if (ipv4->s_addr != htonl(INADDR_LOOPBACK))
+			*alen = 4;
 		goto out;
 	}
-	if (inet_pton(AF_INET6, tmp, (struct in6_addr *)addr) == 0) {
-		*alen = 16;
+	if (inet_pton(AF_INET6, tmp, (struct in6_addr *)addr) == 1) {
+		struct in6_addr *ipv6 = (struct in6_addr *)addr;
+
+		/* Since link local doesn't allow forwarding packets
+		 * for router don't allow those addresses as well.
+		 * Site local is allowed since it similar to 10.0.0.0/8.
+		 * Be aware site local is deprecated by unique local
+		 * addresses.
+		 */
+		if (!IN6_IS_ADDR_LOOPBACK(ipv6->s6_addr) &&
+		    !IN6_IS_ADDR_LINKLOCAL(ipv6->s6_addr))
+			*alen = 16;
 		goto out;
 	}
 #ifdef HAVE_GETADDRINFO
@@ -223,10 +237,9 @@ libcfs_ip_str2addr_size(const char *str, int nob,
 		freeaddrinfo(ai);
 	}
 #endif
-	ret = 0;
 out:
 	free(tmp);
-	return ret;
+	return *alen != 0;
 }
 
 int

lustre/utils/mkfs_lustre.c
@@ -63,6 +63,7 @@
 #include <linux/lnet/lnetctl.h>
 #include <linux/lustre/lustre_user.h>
 #include <linux/lustre/lustre_ver.h>
+#include <libcfs/util/string.h>
 
 #include "mount_utils.h"
 
@@ -322,52 +323,6 @@ static int erase_param(const char *const buf, const char *const param,
 }
 #endif
 
-/* from mount_lustre */
-/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
-#define MAXNIDSTR 1024
-static char *convert_hostnames(char *s1)
-{
-	char *converted, *s2 = 0, *c, *end, sep;
-	int left = MAXNIDSTR;
-	lnet_nid_t nid;
-
-	converted = malloc(left);
-	if (!converted)
-		return NULL;
-
-	end = s1 + strlen(s1);
-	c = converted;
-	while ((left > 0) && (s1 < end)) {
-		s2 = strpbrk(s1, ",:");
-		if (!s2)
-			s2 = end;
-		sep = *s2;
-		*s2 = '\0';
-		nid = libcfs_str2nid(s1);
-		*s2 = sep;
-
-		if (nid == LNET_NID_ANY) {
-			fprintf(stderr, "%s: Cannot resolve hostname '%s'.\n",
-				progname, s1);
-			free(converted);
-			return NULL;
-		}
-		if (strncmp(libcfs_nid2str(nid), "127.0.0.1",
-			    strlen("127.0.0.1")) == 0) {
-			fprintf(stderr,
-				"%s: The NID '%s' resolves to the loopback address '%s'.  Lustre requires a non-loopback address.\n",
-				progname, s1, libcfs_nid2str(nid));
-			free(converted);
-			return NULL;
-		}
-
-		c += snprintf(c, left, "%s%c", libcfs_nid2str(nid), sep);
-		left = converted + MAXNIDSTR - c;
-		s1 = s2 + 1;
-	}
-	return converted;
-}
-
 int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 	       char **mountopts, char *old_fsname)
 {
@@ -470,7 +425,7 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 				return 1;
 			}
 
-			nids = convert_hostnames(optarg);
+			nids = convert_hostnames(optarg, false);
 			if (!nids)
 				return 1;
 
@@ -554,7 +509,7 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 			break;
 		}
 		case 'm': {
-			char *nids = convert_hostnames(optarg);
+			char *nids = convert_hostnames(optarg, false);
 
 			if (!nids)
 				return 1;

lustre/utils/mount_lustre.c
@@ -124,44 +124,6 @@ void usage(FILE *out)
 	exit((out != stdout) ? EINVAL : 0);
 }
 
-/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
-#define MAXNIDSTR 1024
-static char *convert_hostnames(char *s1)
-{
-	char *converted, *s2 = 0, *c;
-	char sep;
-	int left = MAXNIDSTR;
-	lnet_nid_t nid;
-
-	converted = malloc(left);
-	if (!converted) {
-		fprintf(stderr, "out of memory: needed %d bytes\n",
-			MAXNIDSTR);
-		return NULL;
-	}
-	c = converted;
-	while ((left > 0) && (*s1 != '/')) {
-		s2 = strpbrk(s1, ",:");
-		if (!s2)
-			goto out_free;
-		sep = *s2;
-		*s2 = '\0';
-		nid = libcfs_str2nid(s1);
-		*s2 = sep;                      /* back to original string */
-		if (nid == LNET_NID_ANY)
-			goto out_free;
-		c += scnprintf(c, left, "%s%c", libcfs_nid2str(nid), sep);
-		left = converted + MAXNIDSTR - c;
-		s1 = s2 + 1;
-	}
-	snprintf(c, left, "%s", s1);
-	return converted;
-out_free:
-	fprintf(stderr, "%s: Can't parse NID '%s'\n", progname, s1);
-	free(converted);
-	return NULL;
-}
-
 /*****************************************************************************
  *
  * This part was cribbed from util-linux/mount/mount.c.  There was no clear
@@ -786,7 +748,7 @@ static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
 
 	ptr = strstr(mop->mo_usource, ":/");
 	if (ptr) {
-		mop->mo_source = convert_hostnames(mop->mo_usource);
+		mop->mo_source = convert_hostnames(mop->mo_usource, true);
 		if (!mop->mo_source)
 			usage(stderr);
 	} else {

lustre/utils/mount_utils.c
@@ -41,6 +41,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <config.h>
+#include <linux/lnet/nidstr.h>
 #include <linux/lustre/lustre_ver.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
@@ -54,6 +55,7 @@
 #include <sys/types.h>
 #include <sys/xattr.h>
 #include <libmount/libmount.h>
+#include <libcfs/util/string.h>
 
 #ifdef HAVE_GSS
 #ifdef HAVE_LIBKEYUTILS
@@ -941,6 +943,93 @@ int file_create(char *path, __u64 size)
 	}
 
 	return 0;
+}
+
+/* Get rid of symbolic hostnames for tcp, since kernel can't do lookups */
+#define MAXNIDSTR 1024
+
+char *convert_hostnames(char *s1, bool mount)
+{
+	char *converted, *s2 = 0, *c, *end, sep;
+	int left = MAXNIDSTR;
+	struct lnet_nid nid;
+
+	converted = malloc(left);
+	if (!converted) {
+		fprintf(stderr, "%s: cannot allocate %u bytes for NID: %s\n",
+			progname, left, strerror(ENOMEM));
+		return NULL;
+	}
+
+	/* end is different between mount and mkfs case */
+	if (mount) {
+		end = strchr(s1, '/');
+		if (!end) {
+			fprintf(stderr, "%s: Invalid mount string: %s\n",
+				progname, s1);
+			return NULL;
+		}
+		end--;
+	} else {
+		end = s1 + strlen(s1);
+	}
+
+	c = converted;
+	while ((left > 0) && (s1 < end)) {
+		int rc;
+
+		/* Needed to skip : in IPv6 / GUID strings. Lustre uses
+		 * ':' as a seperator as well which makes this complicated.
+		 */
+		s2 = strchr(s1, '@');
+		if (!s2 || s2 > end) {
+			fprintf(stderr, "%s: Invalid NID string '%s'\n",
+				progname, s1);
+			goto out_free;
+		}
+
+		s2 = strpbrk(s2, ",:");
+		if (!s2) {
+			s2 = end;
+		} else if (s2 > end) {
+			fprintf(stderr, "%s: Invalid NID string '%s'\n",
+				progname, s1);
+			goto out_free;
+		}
+		sep = *s2;
+		*s2 = '\0';
+
+		rc = libcfs_strnid(&nid, s1);
+		if (rc < 0) {
+			fprintf(stderr, "%s: Unsupported NID '%s': rc = %s.\n",
+				progname, s1, strerror(rc));
+			goto out_free;
+		}
+		*s2 = sep;	/* back to original string */
+
+		if (LNET_NID_IS_ANY(&nid)) {
+			fprintf(stderr, "%s: Cannot resolve hostname '%s'.\n",
+				progname, s1);
+			goto out_free;
+		}
+
+		if (sep != '\0')
+			c += scnprintf(c, left, "%s%c", libcfs_nidstr(&nid),
+				       sep);
+		else
+			c += scnprintf(c, left, "%s", libcfs_nidstr(&nid));
+		left = converted + MAXNIDSTR - c;
+		s1 = s2 + 1;
+	}
+
+	if (mount)
+		snprintf(c, left, "%s", s1);
+
+	return converted;
+out_free:
+	fprintf(stderr, "%s: Can't parse NID '%s'\n", progname, s1);
+	free(converted);
+	return NULL;
 }
 
 #ifdef HAVE_SERVER_SUPPORT

lustre/utils/mount_utils.h
@@ -178,6 +178,7 @@ int update_mtab_entry(char *spec, char *mtpt, char *type, char *opts,
 int update_utab_entry(struct mount_opts *mop);
 int check_mountfsoptions(char *mountopts, char *wanted_mountopts);
 void trim_mountfsoptions(char *s);
+char *convert_hostnames(char *s1, bool mount);
 #ifdef HAVE_SERVER_SUPPORT
 __u64 get_device_size(char* device);
 int lustre_rename_fsname(struct mkfs_opts *mop, const char *mntpt,

commit 11d851c51c6ce3893aaea705f000680ef04737da

LU-10391 obd: Update lmd_parse to handle IPv6 NIDs
lmd_find_delimiter()/lmd_parse_nidlist() are updated to handle IPv6
NIDs.

class_parse_value() is updated to handle IPv6 NIDs that begin with
'::'. Also, when parsing a Lustre network name, the buffer needn't
contain an '@', nor do we want to search for '@' when locating the
next delimiter.

Fixes: 101f6e8 ("LU-10391 obdclass: handle large NIDs for mount strings")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ib9e5a0d161babfea368989dd9521d923ec592185
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53882
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/obd_config.c
@@ -443,25 +443,41 @@ static int class_parse_value(char *buf, int opc, void *value, char **endh,
 			     int quiet)
 {
 	char *endp;
-	char  tmp;
-	int   rc = 0;
+	char tmp;
+	int rc = 0;
+	int ncolons = 0;
 
 	if (!buf)
 		return 1;
 
-	while (*buf == ',' || *buf == ':')
+	while (*buf == ',' || *buf == ':') {
+		if (*buf == ':')
+			ncolons++;
+		else
+			ncolons = 0;
 		buf++;
+	}
+
+	/* IPv6 addresses can start with '::' */
+	if (opc == CLASS_PARSE_NID && ncolons >= 2)
+		buf = buf - 2;
+
 	if (*buf == ' ' || *buf == '/' || *buf == '\0')
 		return 1;
 
 	/* NID separators or end of NIDs. Large NIDs can contain ':' so
 	 * skip ahead to @ and then look for one of the delimiters.
 	 */
-	endp = strchr(buf, '@');
-	if (!endp)
-		return 1;
+	if (opc == CLASS_PARSE_NID) {
+		endp = strchr(buf, '@');
+		if (!endp)
+			return 1;
 
-	endp = strpbrk(endp, ",: /");
+		endp = strpbrk(endp, ",: /");
+	} else {
+		endp = strpbrk(buf, ",: /");
+	}
+
 	if (!endp)
 		endp = buf + strlen(buf);
 

lustre/obdclass/obd_mount.c
@@ -40,6 +40,7 @@
 #define D_MOUNT (D_SUPER|D_CONFIG/*|D_WARNING */)
 #define PRINT_CMD CDEBUG
 
+#include <linux/inet.h>
 #include <linux/random.h>
 #include <linux/uuid.h>
 #include <linux/version.h>
@@ -1226,8 +1227,9 @@ static int lmd_parse_mgs(struct lustre_mount_data *lmd, char **ptr)
 
 /**
  * Find the first delimiter (comma or colon) from the specified \a buf and
- * make \a *endh point to the string starting with the delimiter. The commas
- * in expression list [...] will be skipped.
+ * make \a *endh point to the string starting with the delimiter. The commas and
+ * colons in expression list [...] will be skipped. Colons that are part of a
+ * NID address are not considered delimiters.
  *
  * @buf		a delimiter-separated string
  * @endh	a pointer to a pointer that will point to the string
@@ -1237,44 +1239,103 @@ static int lmd_parse_mgs(struct lustre_mount_data *lmd, char **ptr)
  */
 static bool lmd_find_delimiter(char *buf, char **endh)
 {
-	char *c = buf;
-	size_t pos;
-	bool found;
+	char *c;
+	char *at = NULL;
+	char *colon = NULL;
+	char *delim = NULL;
+	int brackets = 0;
+	int ncolons = 0;
 
-	if (!buf)
-		return false;
-try_again:
-	if (*c == ',' || *c == ':')
-		return true;
-
-	pos = strcspn(c, "[:,]");
-	if (!pos)
+	if (!buf || *buf == '\0')
 		return false;
 
-	/* Not a valid mount string */
-	if (*c == ']') {
-		CWARN("invalid mount string format\n");
-		return false;
-	}
-
-	c += pos;
-	if (*c == '[') {
-		c = strchr(c, ']');
-
-		/* invalid mount string */
-		if (!c) {
-			CWARN("invalid mount string format\n");
-			return false;
+	c = buf;
+	do {
+		if (*c == '[') {
+			brackets++;
+			continue;
 		}
-		c++;
-		goto try_again;
+
+		if (*c == ']') {
+			brackets--;
+			if (brackets < 0) {
+				CWARN("imbalanced brackets detected in \"%s\"",
+				      buf);
+				return false;
+			}
+			continue;
+		}
+
+		/* Ignore all characters inside brackets */
+		if (brackets > 0)
+			continue;
+
+		if (*c == ':') {
+			if (at) {
+				/* We previously saw an '@', so this ':' is a
+				 * delimiter separating NIDs
+				 */
+				delim = c;
+				break;
+			}
+
+			/* Record the first ':' that we find, as this may be a
+			 * delimiter
+			 */
+			if (!colon)
+				colon = c;
+
+			ncolons++;
+			if (ncolons > 2) {
+				/* Something like :::<foo>. The first ':' is a
+				 * delimiter
+				 */
+				delim = colon;
+				break;
+			}
+			continue;
+		}
+
+		ncolons = 0;
+		if (*c == '@') {
+			at = c;
+		} else if (*c == ',') {
+			/* When we find a ',', we can determine whether a ':'
+			 * seen earlier was a delimiter or part of a NID
+			 */
+			if (colon && !at) {
+				/* We previously saw a ':', but never
+				 * encountered an '@'. Thus, the ':' was a
+				 * delimiter
+				 */
+				delim = colon;
+			} else {
+				/* We either never saw a ':', or we saw both a
+				 * ':' and an '@' (so the ':' was part of a
+				 * NID). In both cases this ',' is our delimiter
+				 */
+				delim = c;
+			}
+			break;
+		}
+	} while (c++ && *c != '\0');
+
+
+	if (brackets != 0) {
+		CWARN("imbalanced brackets detected in \"%s\"", buf);
+		return false;
 	}
 
-	found = *c != '\0';
-	if (found && endh)
-		*endh = c;
+	if (!delim && colon && !at)
+		/* We saw a ':' but reached end of string without finding an '@'
+		 * or ','. Thus, the ':' is considered a delimiter
+		 */
+		delim = colon;
 
-	return found;
+	if (delim && endh)
+		*endh = delim;
+
+	return delim != NULL;
 }
 
 /**
@@ -1295,11 +1356,23 @@ static int lmd_parse_nidlist(char *buf, char **endh)
 	char *endp = buf;
 	char tmp;
 	int rc = 0;
+	int ncolons = 0;
 
-	if (buf == NULL)
+	if (!buf)
 		return 1;
-	while (*buf == ',' || *buf == ':')
+
+	while (*buf == ',' || *buf == ':') {
+		if (*buf == ':')
+			ncolons++;
+		else
+			ncolons = 0;
 		buf++;
+	}
+
+	/* IPv6 addresses can start with '::' */
+	if (ncolons >= 2)
+		buf = buf - 2;
+
 	if (*buf == ' ' || *buf == '/' || *buf == '\0')
 		return 1;
 
@@ -1309,15 +1382,38 @@ static int lmd_parse_nidlist(char *buf, char **endh)
 	tmp = *endp;
 	*endp = '\0';
 
-	if (cfs_parse_nidlist(buf, &nidlist) < 0)
+	/* FIXME: cfs_parse_nidlist does not support IPv6 addresses, so if
+	 * buf contains a ':' and an '@' then we assume this is supposed to be
+	 * an IPv6 address.
+	 */
+	if (strchr(buf, ':')) {
+		struct in6_addr addr;
+		char *at;
+
 		rc = 1;
+		at = strchr(buf, '@');
+		if (at) {
+			*at = '\0';
+			rc = in6_pton(buf, -1, (u8 *)&addr.s6_addr, -1, NULL);
+			if (rc == 1)
+				rc = 0;
+			*at = '@';
+		}
+	} else if (cfs_parse_nidlist(buf, &nidlist) < 0) {
+		rc = 1;
+	}
+
 	cfs_free_nidlist(&nidlist);
 
+	/* Restore the delimiter */
 	*endp = tmp;
-	if (rc != 0)
+
+	if (rc)
 		return rc;
-	if (endh != NULL)
+
+	if (endh)
 		*endh = endp;
+
 	return 0;
 }
 

lustre/osd-ldiskfs/osd_handler.c
@@ -8397,6 +8397,7 @@ static int osd_mount(const struct lu_env *env,
 			"resetoi",
 			NULL
 		};
+
 		strncat(options, opts, PAGE_SIZE);
 		for (rc = 0, str = options; sout[rc]; ) {
 			char *op = strstr(str, sout[rc]);