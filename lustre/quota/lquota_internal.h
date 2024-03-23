lustre/lustre-release

commit 1dbcbd70f87b7390998cd3b1fa546ece9d2a493e

LU-15021 quota: protect lqe_glbl_data in lqe
The lqe_glbl_data in "struct lquota_entry" is allocated in
qmt_lvbo_init and freed in qmt_lvbo_free, it could be freed
during qmt_seed_glbe called by qmt_set_id_notify, and cause
panic because of using freed memory.

Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I274f07ee8609c83852572be51625cc929a9130ec
Reviewed-on: https://review.whamcloud.com/45098
Reviewed-by: Sergey Cheremencev <sergey.cheremencev@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/lquota_internal.h
@@ -189,6 +189,8 @@ struct lquota_entry {
 			lqe_is_global:1,  /* lqe belongs to global pool "0x0"*/
 			lqe_is_deleted:1; /* lqe will be deleted soon */
 
+	/* the lock to protect lqe_glbl_data */
+	struct mutex		 lqe_glbl_data_lock;
 	struct lqe_glbl_data	*lqe_glbl_data;
 };
 

lustre/quota/qmt_entry.c
@@ -44,6 +44,7 @@ static void qmt_lqe_init(struct lquota_entry *lqe, void *arg)
 
 	lqe->lqe_revoke_time = 0;
 	init_rwsem(&lqe->lqe_sem);
+	mutex_init(&lqe->lqe_glbl_data_lock);
 }
 
 /* Apply the default quota setting to the specified quota entry
@@ -841,8 +842,13 @@ bool qmt_adjust_edquot_qunit_notify(const struct lu_env *env,
 		return need_reseed;
 	}
 
-	if (lqe_gl->lqe_glbl_data && need_reseed) {
-		qmt_seed_glbe_all(env, lqe_gl->lqe_glbl_data, qunit, edquot);
+	if (need_reseed) {
+		mutex_lock(&lqe_gl->lqe_glbl_data_lock);
+		if (lqe_gl->lqe_glbl_data)
+			qmt_seed_glbe_all(env, lqe_gl->lqe_glbl_data, qunit,
+					  edquot);
+		mutex_unlock(&lqe_gl->lqe_glbl_data_lock);
+
 		qmt_id_lock_notify(qmt, lqe_gl);
 	}
 	return need_reseed;
@@ -879,17 +885,23 @@ void qmt_revalidate_lqes(const struct lu_env *env,
 	for (i = 0; i < qti_lqes_cnt(env); i++)
 		need_notify |= qmt_revalidate(env, qti_lqes(env)[i]);
 
+	if (!need_notify)
+		return;
+
 	/* There could be no ID lock to the moment of reconciliation.
 	 * As a result lqe global data is not initialised yet. It is ok
 	 * for release and report requests. */
 	if (!lqe_gl->lqe_glbl_data &&
-	    (req_is_rel(qb_flags) || req_has_rep(qb_flags)))
+	    (req_is_rel(qb_flags) || req_has_rep(qb_flags))) {
 		return;
-
-	if (need_notify) {
-		qmt_seed_glbe(env, lqe_gl->lqe_glbl_data);
-		qmt_id_lock_notify(qmt, lqe_gl);
 	}
+
+	mutex_lock(&lqe_gl->lqe_glbl_data_lock);
+	if (lqe_gl->lqe_glbl_data)
+		qmt_seed_glbe(env, lqe_gl->lqe_glbl_data);
+	mutex_unlock(&lqe_gl->lqe_glbl_data_lock);
+
+	qmt_id_lock_notify(qmt, lqe_gl);
 }
 
 void qti_lqes_init(const struct lu_env *env)

lustre/quota/qmt_lock.c
@@ -520,10 +520,13 @@ int qmt_lvbo_free(struct lu_device *ld, struct ldlm_resource *res)
 		struct lquota_entry *lqe = res->lr_lvb_data;
 		struct lqe_glbl_data *lgd = lqe->lqe_glbl_data;
 
-		/* release lqe reference */
+		mutex_lock(&lqe->lqe_glbl_data_lock);
 		lqe->lqe_glbl_data = NULL;
-		lqe_putref(lqe);
 		qmt_free_lqe_gd(lgd);
+		mutex_unlock(&lqe->lqe_glbl_data_lock);
+
+		/* release lqe reference */
+		lqe_putref(lqe);
 	} else {
 		struct dt_object *obj = res->lr_lvb_data;
 		/* release object reference */

lustre/quota/qmt_handler.c
@@ -122,8 +122,11 @@ static void qmt_set_id_notify(const struct lu_env *env, struct qmt_device *qmt,
 	if (!lqe_gl)
 		GOTO(lqes_fini, rc);
 
+	mutex_lock(&lqe_gl->lqe_glbl_data_lock);
 	if (lqe_gl->lqe_glbl_data)
 		qmt_seed_glbe(env, lqe_gl->lqe_glbl_data);
+	mutex_unlock(&lqe_gl->lqe_glbl_data_lock);
+
 	/* Even if slaves haven't enqueued quota lock yet,
 	 * it is needed to set lqe_revoke_time in qmt_id_lock_glimpse
 	 * in case of reaching qpi_least_qunit */

lustre/quota/qsd_entry.c
@@ -50,6 +50,7 @@ static void qsd_lqe_init(struct lquota_entry *lqe, void *arg)
 	init_waitqueue_head(&lqe->lqe_waiters);
 	lqe->lqe_usage    = 0;
 	lqe->lqe_nopreacq = false;
+	mutex_init(&lqe->lqe_glbl_data_lock);
 }
 
 /*

commit 2cc18ece1e50c760786a13a9dcb5857d7768cb0f

LU-14535 quota: free lvbo in a wq
Mutex lqe_glbl_data_lock holded in
qmt_lvbo_free might be the reason of
sleeping while atommic if
cfs_hash_for_each_relax is getting a
spinlock on an upper layer:

 BUG: sleeping function called from invalid
	context at kernel/mutex.c:104
 ...
 Call Trace:
 dump_stack+0x19/0x1b
 __might_sleep+0xd9/0x100
 mutex_lock+0x20/0x40
 qmt_lvbo_free+0xc7/0x380 [lquota]
 mdt_lvbo_free+0x12d/0x140 [mdt]
 ldlm_resource_putref+0x189/0x250 [ptlrpc]
 ldlm_lock_put+0x1c8/0x760 [ptlrpc]
 ldlm_export_lock_put+0x12/0x20 [ptlrpc]
 cfs_hash_for_each_relax+0x3ff/0x450 [libcfs]
 cfs_hash_for_each_empty+0x9a/0x210 [libcfs]
 ldlm_export_cancel_locks+0xc2/0x1a0 [ptlrpc]
 ldlm_bl_thread_main+0x7c8/0xb00 [ptlrpc]
 kthread+0xe4/0xf0
 ret_from_fork_nospec_begin+0x7/0x21

Move freeing of lvbo to a workqueue. This
patch could be probably reverted as soon
as https://review.whamcloud.com/45882 will
be landed.

Fixes: 1dbcbd70f8 ("LU-15021 quota: protect lqe_glbl_data in lqe")
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I56aee72a7adbc6514b40689bae30669e607b5ecd
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54107
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/quota/lquota_internal.h
@@ -195,6 +195,7 @@ struct lquota_entry {
 	/* the lock to protect lqe_glbl_data */
 	struct mutex		 lqe_glbl_data_lock;
 	struct lqe_glbl_data	*lqe_glbl_data;
+	struct work_struct	 lqe_work; /* workitem to free lvbo */
 };
 
 #define lqe_qtype(lqe)		(lqe->lqe_site->lqs_qtype)

lustre/quota/qmt_dev.c
@@ -97,6 +97,11 @@ static struct lu_device *qmt_device_fini(const struct lu_env *env,
 	CDEBUG(D_QUOTA, "%s: initiating QMT shutdown\n", qmt->qmt_svname);
 	qmt->qmt_stopping = true;
 
+	if (qmt_lvbo_free_wq) {
+		destroy_workqueue(qmt_lvbo_free_wq);
+		qmt_lvbo_free_wq = NULL;
+	}
+
 	/* kill pool instances, if any */
 	qmt_pool_fini(env, qmt);
 
@@ -279,6 +284,14 @@ static int qmt_device_init0(const struct lu_env *env, struct qmt_device *qmt,
 	rc = qmt_pool_init(env, qmt);
 	if (rc)
 		GOTO(out, rc);
+
+	qmt_lvbo_free_wq = alloc_workqueue("qmt_lvbo_free", WQ_UNBOUND, 0);
+	if (!qmt_lvbo_free_wq) {
+		rc = -ENOMEM;
+		CERROR("%s: failed to start qmt_lvbo_free workqueue: rc = %d\n",
+		       qmt->qmt_svname, rc);
+		goto out;
+	}
 
 	EXIT;
 out:

lustre/quota/qmt_entry.c
@@ -32,6 +32,31 @@
 
 #include "qmt_internal.h"
 
+static void qmt_work_lvbo_free(struct work_struct *work)
+{
+	struct lqe_glbl_data *lgd;
+	struct lquota_entry *lqe;
+
+	lqe = container_of(work, struct lquota_entry, lqe_work);
+	mutex_lock(&lqe->lqe_glbl_data_lock);
+	lgd = lqe->lqe_glbl_data;
+	lqe->lqe_glbl_data = NULL;
+	mutex_unlock(&lqe->lqe_glbl_data_lock);
+	qmt_free_lqe_gd(lgd);
+
+	if (unlikely(lgd == NULL)) {
+		struct qmt_pool_info *pool;
+
+		pool = (struct qmt_pool_info *)lqe->lqe_site->lqs_parent;
+		CWARN("%s: lvbo for (id=%llx) not fully inited\n",
+		      pool->qpi_qmt->qmt_svname,
+		      lqe->lqe_id.qid_uid);
+	}
+
+	/* release lqe reference */
+	lqe_putref(lqe);
+}
+
 /*
  * Initialize qmt-specific fields of quota entry.
  *
@@ -45,6 +70,7 @@ static void qmt_lqe_init(struct lquota_entry *lqe, void *arg)
 	lqe->lqe_revoke_time = 0;
 	init_rwsem(&lqe->lqe_sem);
 	mutex_init(&lqe->lqe_glbl_data_lock);
+	INIT_WORK(&lqe->lqe_work, qmt_work_lvbo_free);
 }
 
 /* Apply the default quota setting to the specified quota entry

lustre/quota/qmt_internal.h
@@ -30,6 +30,8 @@
 
 #include "lquota_internal.h"
 
+extern struct workqueue_struct *qmt_lvbo_free_wq;
+
 /*
  * The Quota Master Target Device.
  * The qmt is responsible for:

lustre/quota/qmt_lock.c
@@ -31,12 +31,15 @@
 #define DEBUG_SUBSYSTEM S_LQUOTA
 
 #include <linux/kthread.h>
+#include <linux/workqueue.h>
 
 #include <lustre_dlm.h>
 #include <lustre_swab.h>
 #include <obd_class.h>
 
 #include "qmt_internal.h"
+
+struct workqueue_struct *qmt_lvbo_free_wq;
 
 /* intent policy function called from mdt_intent_opc() when the intent is of
  * quota type */
@@ -550,16 +553,8 @@ int qmt_lvbo_free(struct lu_device *ld, struct ldlm_resource *res)
 
 	if (res->lr_name.name[LUSTRE_RES_ID_QUOTA_SEQ_OFF] != 0) {
 		struct lquota_entry *lqe = res->lr_lvb_data;
-		struct lqe_glbl_data *lgd;
 
-		mutex_lock(&lqe->lqe_glbl_data_lock);
-		lgd = lqe->lqe_glbl_data;
-		lqe->lqe_glbl_data = NULL;
-		mutex_unlock(&lqe->lqe_glbl_data_lock);
-		qmt_free_lqe_gd(lgd);
-
-		/* release lqe reference */
-		lqe_putref(lqe);
+		queue_work(qmt_lvbo_free_wq, &lqe->lqe_work);
 	} else {
 		struct dt_object *obj = res->lr_lvb_data;
 		/* release object reference */