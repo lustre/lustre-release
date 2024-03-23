lustre/lustre-release

commit b5fde4d6c02324a8511afe30d02eb2cf46ea799d

LU-17197 obdclass: preserve fairness when waiting for rpc slot
When obd_get_mod_rpc_slot() waits for an available slot it places the
waiting thread at the HEAD of the queue, so it will be woken before
anything else that is already queued.  This is clearly unfair and can
hurt performance.

So change to always add to the tail to ensure a FIFO ordering (except
that CLOSE might sometimes be woken a bit early).

This regression was introduced in a rewrite that was supposed to make
waiting more fair - by avoiding a broadcast wakeup for "close"
requests.

Also fix some stale comments and expose __add_wait_queue_entry_tail

Running mdtest with the patch applied shows about a 3% improvement:

                             master            patched
  mdtest-easy-write      350.585906 kIOPS   353.783545 kIOPS
   mdtest-easy-stat     1320.329353 kIOPS  1408.320419 kIOPS
 mdtest-easy-delete      285.084103 kIOPS   289.625900 kIOPS
            [SCORE]      509.115803 kiops   524.516113 kiops

Fixes: 5243630b09d2 ("LU-15947 obdclass: improve precision of wakeups for mod_rpcs")
Signed-off-by: Mr NeilBrown <neilb@suse.de>
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: If767c4299bcbab71589b0f3c01e85bf461686ca5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52738
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/obdclass/genops.c
@@ -2217,13 +2217,12 @@ __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc)
 	wait.wqe.func = claim_mod_rpc_function;
 
 	spin_lock_irq(&cli->cl_mod_rpcs_waitq.lock);
-	__add_wait_queue(&cli->cl_mod_rpcs_waitq, &wait.wqe);
+	__add_wait_queue_entry_tail(&cli->cl_mod_rpcs_waitq, &wait.wqe);
 	/* This wakeup will only succeed if the maximums haven't
-	 * been reached.  If that happens, WQ_FLAG_WOKEN will be cleared
+	 * been reached.  If that happens, wait.woken will be set
 	 * and there will be no need to wait.
 	 */
 	wake_up_locked(&cli->cl_mod_rpcs_waitq);
-	/* XXX: handle spurious wakeups (from unknown yet source */
 	while (wait.woken == false) {
 		spin_unlock_irq(&cli->cl_mod_rpcs_waitq.lock);
 		wait_woken(&wait.wqe, TASK_UNINTERRUPTIBLE,

libcfs/include/libcfs/linux/linux-wait.h
@@ -16,6 +16,10 @@
 #define wait_queue_entry_t wait_queue_t
 #endif
 
+#ifndef HAVE_PREPARE_TO_WAIT_EVENT
+#define __add_wait_queue_entry_tail __add_wait_queue_tail
+#endif
+
 #ifndef HAVE_WAIT_BIT_HEADER_H
 struct wait_bit_queue_entry {
 	struct wait_bit_key	key;

libcfs/libcfs/linux/linux-wait.c
@@ -10,8 +10,6 @@
 
 #ifndef HAVE_PREPARE_TO_WAIT_EVENT
 
-#define __add_wait_queue_entry_tail __add_wait_queue_tail
-
 long prepare_to_wait_event(wait_queue_head_t *wq_head,
 			   wait_queue_entry_t *wq_entry, int state)
 {

commit 7a2296a397381a5f6f9473b297f0062e8ff15948

LU-17510 obdclass: fix wake up when queuing close request.
The waitqueue for requests that need to be sent but that haven't been
allocated a slot is kept ordered by request arrival for fairness.  So
new requests are added to the end.

For requests other than 'close' there is a limit to the number of
active requests (slots) and requests are assigned to slot on a
first-come-first-served basis, so they are simply removed from the
head of the list.

For 'close' requests it is important that these not block indefinitely
behind other other requests so there is one slot that can only be used
by a close request - and only if no other slots are used by a close
request.  These requests do not follow a strict FIFO order.

When a non-"close" request completes we wake the first request on the
list.  There is no point searching all the way down the list for a
close request that could also be woken.  We only do that when a
"close" request completes.  This optimises the common case.

However: when a request is first queued we add it to the end of the
queue and then wake up the first deserving request if there is one.
When there are free slots, this is expected to wake the request just
queued.  When there are no free slots, nothing is woken.

When a "close" request is queued and added to the end of the queue
after other non-close requests, we need to potentially search to the
end of the queue for a close request to wake, just as we do when a
close request completes.  Unfortunately we don't.  This can result in
a close request blocking indefinitely.

So: change the wakeup in obd_get_mod_rpc_slot() to match the wakeup in
obd_put_mod_rpc_slot().  This ensure consistent handling and in
particular will handle a close request immediately if there are no
other close requests in flight.

Clarify comment in claim_mod_rpc_function() to make and perform minor
code cleanup there.

Fixes: b5fde4d6c023 ("LU-17197 obdclass: preserve fairness when waiting for rpc slot")

lustre/obdclass/genops.c
@@ -2210,16 +2210,17 @@ static int claim_mod_rpc_function(wait_queue_entry_t *wq_entry,
 		(close_req && cli->cl_close_rpcs_in_flight == 0);
 	if (avail) {
 		cli->cl_mod_rpcs_in_flight++;
-		if (w->close_req)
+		if (close_req)
 			cli->cl_close_rpcs_in_flight++;
 		ret = woken_wake_function(wq_entry, mode, flags, key);
 		w->woken = true;
 	} else if (cli->cl_close_rpcs_in_flight)
 		/* No other waiter could be woken */
 		ret = -1;
-	else if (key == NULL)
-		/* This was not a wakeup from a close completion, so there is no
-		 * point seeing if there are close waiters to be woken
+	else if (!key)
+		/* This was not a wakeup from a close completion or a new close
+		 * being queued, so there is no point seeing if there are close
+		 * waiters to be woken.
 		 */
 		ret = -1;
 	else
@@ -2253,8 +2254,12 @@ __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc)
 	/* This wakeup will only succeed if the maximums haven't
 	 * been reached.  If that happens, wait.woken will be set
 	 * and there will be no need to wait.
+	 * If a close_req was enqueue, ensure we search all the way to the
+	 * end of the waitqueue for a close request.
 	 */
-	wake_up_locked(&cli->cl_mod_rpcs_waitq);
+	__wake_up_locked_key(&cli->cl_mod_rpcs_waitq, TASK_NORMAL,
+			     (void*)wait.close_req);
+
 	while (wait.woken == false) {
 		spin_unlock_irq(&cli->cl_mod_rpcs_waitq.lock);
 		wait_woken(&wait.wqe, TASK_UNINTERRUPTIBLE,