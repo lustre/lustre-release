lustre/lustre-release

commit e7ce67de92dea68707c96ef44918ebdaacd6533b

LU-15451 sec: read-only nodemap flag
Add a new 'readonly_mount' property to nodemaps. When set, we return
-EROFS from server side if the client is not mounting read-only.
So the client will have to specify the read-only mount option to be
allowed to mount.

Fixes: 928714dddabb ("LU-5092 nodemap: save id maps to targets in new index file")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I9931844ae46dfd5d724f592f8dfacc4a8011c7e3
Reviewed-on: https://review.whamcloud.com/46149
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanity-sec.sh
@@ -2779,11 +2779,27 @@ cleanup_for_enc_tests() {
 }
 
 cleanup_nodemap_after_enc_tests() {
+	umount_client $MOUNT || true
+
 	do_facet mgs $LCTL nodemap_modify --name default \
 		--property forbid_encryption --value 0
-	wait_nm_sync default forbid_encryption
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property readonly_mount --value 0 || true
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property trusted --value 0
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property admin --value 0
 	do_facet mgs $LCTL nodemap_activate 0
+
+	wait_nm_sync default forbid_encryption '' inactive
+	[ -z "$(do_facet mgs \
+			lctl get_param -n nodemap.default.readonly_mount)" ] ||
+		wait_nm_sync default readonly_mount '' inactive
+	wait_nm_sync default trusted_nodemap '' inactive
+	wait_nm_sync default admin_nodemap '' inactive
 	wait_nm_sync active
+
+	mount_client $MOUNT ${MOUNT_OPTS} || error "re-mount failed"
 }
 
 test_36() {
@@ -4972,6 +4988,55 @@ test_60() {
 		error "cat $DIR/$tdir/subdir/subfile1 failed"
 }
 run_test 60 "Subdirmount of encrypted dir"
+
+test_61() {
+	local testfile=$DIR/$tdir/$tfile
+	local readonly
+
+	readonly=$(do_facet mgs \
+			lctl get_param -n nodemap.default.readonly_mount)
+	[ -n "$readonly" ] ||
+		skip "Server does not have readonly_mount nodemap flag"
+
+	stack_trap cleanup_nodemap_after_enc_tests EXIT
+	umount_client $MOUNT || error "umount $MOUNT failed (1)"
+
+	# Activate nodemap, and mount rw.
+	# Should succeed as rw mount is not forbidden on default nodemap
+	# by default.
+	do_facet mgs $LCTL nodemap_activate 1
+	wait_nm_sync active
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property admin --value 1
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property trusted --value 1
+	wait_nm_sync default admin_nodemap
+	wait_nm_sync default trusted_nodemap
+	readonly=$(do_facet mgs \
+			lctl get_param -n nodemap.default.readonly_mount)
+	[ $readonly -eq 0 ] || error "wrong default value for readonly_mount"
+
+	mount_client $MOUNT ${MOUNT_OPTS},rw ||
+		error "mount '-o rw' failed with default"
+	findmnt $MOUNT --output=options -n -f | grep -q "rw," ||
+		error "should be rw mount"
+	mkdir -p $DIR/$tdir || error "mkdir $DIR/$tdir failed"
+	echo a > $testfile || error "write $testfile failed"
+	umount_client $MOUNT || error "umount $MOUNT failed (2)"
+
+	# Now enforce read-only, and retry.
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property readonly_mount --value 1
+	wait_nm_sync default readonly_mount
+	mount_client $MOUNT ${MOUNT_OPTS},rw &&
+		error "mount '-o rw' should have failed"
+	mount_client $MOUNT ${MOUNT_OPTS},ro ||
+		error "mount '-o ro' failed"
+	cat $testfile || error "read $testfile failed"
+	echo b > $testfile && error "write $testfile should fail"
+	umount_client $MOUNT || error "umount $MOUNT failed (3)"
+}
+run_test 61 "Nodemap enforces read-only mount"
 
 log "cleanup: ======================================================"
 

lustre/doc/lctl-nodemap-modify.8
@@ -74,6 +74,12 @@ Defaults to all, which means the nodemap maps UIDs, GIDs, and PROJIDs.
 Other possible values (multiple can be specified, comma separated) are uid to
 map UIDs, gid to map GIDs, both to map UIDs and GIDs, and projid to map PROJIDs.
 .RE
+.PP
+readonly_mount
+.RS 4
+Defaults to off, which lets clients mount in read-write mode. If set to 1,
+clients are only allowed to mount in read-only mount, and get -EROFS otherwise.
+.RE
 
 .RE
 .I value

lustre/include/lustre_nodemap.h
@@ -82,7 +82,8 @@ struct lu_nodemap {
 				 nmf_deny_unknown:1,
 				 nmf_allow_root_access:1,
 				 nmf_enable_audit:1,
-				 nmf_forbid_encryption:1;
+				 nmf_forbid_encryption:1,
+				 nmf_readonly_mount:1;
 	/* bitmap for mapping type */
 	enum nodemap_mapping_modes
 				nmf_map_mode;
@@ -156,6 +157,7 @@ int nodemap_set_squash_gid(const char *name, gid_t gid);
 int nodemap_set_squash_projid(const char *name, projid_t projid);
 int nodemap_set_audit_mode(const char *name, bool enable_audit);
 int nodemap_set_forbid_encryption(const char *name, bool forbid_encryption);
+int nodemap_set_readonly_mount(const char *name, bool readonly_mount);
 bool nodemap_can_setquota(struct lu_nodemap *nodemap, __u32 qc_type, __u32 id);
 int nodemap_add_idmap(const char *name, enum nodemap_id_type id_type,
 		      const __u32 map[2]);

lustre/include/uapi/linux/lustre/lustre_cfg.h
@@ -140,6 +140,7 @@ enum lcfg_command_type {
 	LCFG_NODEMAP_SET_SEPOL	  = 0x00ce05b, /**< set SELinux policy */
 	LCFG_NODEMAP_FORBID_ENCRYPT	= 0x00ce05c, /**< forbid encryption */
 	LCFG_NODEMAP_SQUASH_PROJID	= 0x00ce05d, /**< default map projid */
+	LCFG_NODEMAP_READONLY_MOUNT	= 0x00ce05e, /**< read-only mount */
 };
 
 struct lustre_cfg_bufs {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3588,15 +3588,31 @@ struct llog_update_record {
 				     SELINUX_POLICY_VER_LEN + \
 				     SELINUX_POLICY_HASH_LEN + 3)
 
+/* lu_nodemap flags */
+enum nm_flag_bits {
+	NM_FL_ALLOW_ROOT_ACCESS = 0x1,
+	NM_FL_TRUST_CLIENT_IDS = 0x2,
+	NM_FL_DENY_UNKNOWN = 0x4,
+	NM_FL_MAP_UID = 0x8,
+	NM_FL_MAP_GID = 0x10,
+	NM_FL_ENABLE_AUDIT = 0x20,
+	NM_FL_FORBID_ENCRYPT = 0x40,
+	NM_FL_MAP_PROJID = 0x80,
+};
+enum nm_flag2_bits {
+	NM_FL2_READONLY_MOUNT = 0x1,
+};
+
 /* nodemap records, uses 32 byte record length */
 #define LUSTRE_NODEMAP_NAME_LENGTH 16
 struct nodemap_cluster_rec {
-	char	ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
-	__u8	ncr_flags;
-	__u16	ncr_padding1;
-	__u32	ncr_squash_projid;
-	__u32	ncr_squash_uid;
-	__u32	ncr_squash_gid;
+	char			ncr_name[LUSTRE_NODEMAP_NAME_LENGTH + 1];
+	enum nm_flag_bits	ncr_flags:8;
+	enum nm_flag2_bits	ncr_flags2:8;
+	__u8			ncr_padding1;
+	__u32			ncr_squash_projid;
+	__u32			ncr_squash_uid;
+	__u32			ncr_squash_gid;
 };
 
 /* lnet_nid_t is 8 bytes */

lustre/mgs/mgs_handler.c
@@ -862,6 +862,7 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 	case LCFG_NODEMAP_MAP_MODE:
 	case LCFG_NODEMAP_AUDIT_MODE:
 	case LCFG_NODEMAP_FORBID_ENCRYPT:
+	case LCFG_NODEMAP_READONLY_MOUNT:
 		if (lcfg->lcfg_bufcount != 4)
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);

lustre/mgs/mgs_llog.c
@@ -5521,6 +5521,12 @@ int mgs_nodemap_cmd(const struct lu_env *env, struct mgs_device *mgs,
 			rc = nodemap_set_forbid_encryption(nodemap_name,
 							   bool_switch);
 		break;
+	case LCFG_NODEMAP_READONLY_MOUNT:
+		rc = kstrtobool(param, &bool_switch);
+		if (rc == 0)
+			rc = nodemap_set_readonly_mount(nodemap_name,
+							bool_switch);
+		break;
 	case LCFG_NODEMAP_MAP_MODE:
 	{
 		char *p;

lustre/ptlrpc/import.c
@@ -1338,10 +1338,11 @@ out:
 		time64_t next_connect;
 
 		import_set_state_nolock(imp, LUSTRE_IMP_DISCON);
-		if (rc == -EACCES) {
+		if (rc == -EACCES || rc == -EROFS) {
 			/*
 			 * Give up trying to reconnect
 			 * EACCES means client has no permission for connection
+			 * EROFS means client must mount read-only
 			 */
 			imp->imp_obd->obd_no_recov = 1;
 			ptlrpc_deactivate_import_nolock(imp);

commit 27cf3e0ac8576841106b3fcbd58fd5d7d419197d

LU-17609 sec: nodemap readonly_mount for remount
The readonly_mount property on nodemaps forces read-only mount from
clients. Clients trying rw remount (via mount -o remount,rw) should
also be forced to read-only.

Also improve sanity-sec test_61 to exercise client remount.

Fixes: e7ce67de92 ("LU-15451 sec: read-only nodemap flag")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I61f8141001d2ff9e832e5c93d8f5997479af98a6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54282
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -2852,6 +2852,22 @@ static int mdt_set_info(struct tgt_session_info *tsi)
 
 	/* Swab any part of val you need to here */
 	if (KEY_IS(KEY_READ_ONLY)) {
+		/* If client wants rw, make sure nodemap does not enforce ro. */
+		if (!*(__u32 *)val) {
+			struct lu_nodemap *nm = NULL;
+			bool readonly = false;
+
+			if (req->rq_export)
+				nm = nodemap_get_from_exp(req->rq_export);
+
+			if (!IS_ERR_OR_NULL(nm)) {
+				readonly = nm->nmf_readonly_mount;
+				nodemap_putref(nm);
+			}
+
+			if (unlikely(readonly))
+				RETURN(-EROFS);
+		}
 		spin_lock(&req->rq_export->exp_lock);
 		if (*(__u32 *)val)
 			*exp_connect_flags_ptr(req->rq_export) |=

lustre/tests/sanity-sec.sh
@@ -5467,13 +5467,17 @@ test_61() {
 	do_facet mgs $LCTL nodemap_modify --name c0 \
 		--property readonly_mount --value 1
 	wait_nm_sync c0 readonly_mount
+
+	# mount without option should turn into ro
 	zconf_mount_clients $HOSTNAME $MOUNT ${MOUNT_OPTS} ||
-		error "mount failed"
+		error "mount failed (1)"
 	findmnt $MOUNT --output=options -n -f | grep -q "ro," ||
 		error "mount should have been turned into ro"
 	cat $testfile || error "read $testfile failed (1)"
 	echo b > $testfile && error "write $testfile should fail (1)"
 	umount_client $MOUNT || error "umount $MOUNT failed (3)"
+
+	# mount rw should turn into ro
 	zconf_mount_clients $HOSTNAME $MOUNT ${MOUNT_OPTS},rw ||
 		error "mount '-o rw' failed"
 	findmnt $MOUNT --output=options -n -f | grep -q "ro," ||
@@ -5481,12 +5485,24 @@ test_61() {
 	cat $testfile || error "read $testfile failed (2)"
 	echo b > $testfile && error "write $testfile should fail (2)"
 	umount_client $MOUNT || error "umount $MOUNT failed (4)"
+
+	# mount ro should work as expected
 	zconf_mount_clients $HOSTNAME $MOUNT ${MOUNT_OPTS},ro ||
 		error "mount '-o ro' failed"
 	wait_ssk
 	cat $testfile || error "read $testfile failed (3)"
 	echo b > $testfile && error "write $testfile should fail (3)"
 	umount_client $MOUNT || error "umount $MOUNT failed (5)"
+
+	# remount rw should not work
+	zconf_mount_clients $HOSTNAME $MOUNT ${MOUNT_OPTS} ||
+		error "mount failed (2)"
+	mount_client $MOUNT remount,rw || error "remount failed"
+	findmnt $MOUNT --output=options -n -f | grep -q "ro," ||
+		error "remount rw should have been turned into ro"
+	cat $testfile || error "read $testfile failed (4)"
+	echo b > $testfile && error "write $testfile should fail (4)"
+	umount_client $MOUNT || error "umount $MOUNT failed (6)"
 }
 run_test 61 "Nodemap enforces read-only mount"
 