lustre/lustre-release

commit 94a4663db95656ade6b6e695b849cd7763f0bd49

LU-17334 lmv: handle object created on newly added MDT
When a new MDT is added to a filesystem without no_create, then a new
object is created on the MDT relatively quickly after it is added to
the filesystem, in particular because the new MDT would be preferred
by QOS space balancing due to lots of free space. However, it might
take a few seconds for the addition of the new MDT to be propagated
across all of the clients, so there is a risk that one client creates
a directory on an MDT that a client is not yet aware of, which returns
an error to the application immediately.

This patch fixes the issue by adding lmv_tgt_retry() that will retry
to use the MDT and wait for some number of seconds for the filesystem
layout to be updated if the MDT index an existing file/directory is
not found.

Commands that depend on user input, like 'lfs mkdir -i' and 'lfs df'
and round-robin MDT allocation will continue to use lmv_tgt() which
doesn't retry in case user specifies wrong MDT index, otherwise it can
hang the command for an extended period of time.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Idb0cf65e95f665628d6799298732b7a06cde4a86
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53363
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -130,6 +130,49 @@ static int lmv_set_mdc_active(struct lmv_obd *lmv,
 	return rc;
 }
 
+struct lu_tgt_desc *lmv_tgt_retry(struct lmv_obd *lmv, __u32 index)
+{
+	struct obd_device *obd = lmv2obd_dev(lmv);
+	struct lu_tgt_desc *tgt;
+	static time64_t next_print;
+	time64_t retry_limit = 0;
+	time64_t now;
+	unsigned int level;
+	int rc;
+
+	might_sleep();
+retry:
+	tgt = lmv_tgt(lmv, index);
+	if (likely(tgt && tgt->ltd_exp))
+		return tgt;
+
+	now = ktime_get_seconds();
+	if (retry_limit == 0) {
+		level = now > next_print ? D_WARNING : D_INFO;
+		retry_limit = now + RECONNECT_DELAY_MAX;
+	} else if (now > retry_limit) {
+		level = D_ERROR;
+	} else {
+		level = D_INFO;
+	}
+	CDEBUG_LIMIT(level, index < lmv->lmv_mdt_count ?
+		     "%s: MDT index %u/%u not configured\n" :
+		     "%s: MDT index %u more than MDT count %u\n",
+		     obd->obd_name, index, lmv->lmv_mdt_count);
+	if (now > next_print) {
+		LCONSOLE_INFO("%s: wait %ds while client connects to new MDT\n",
+			      obd->obd_name, (int)(retry_limit - now));
+		next_print = retry_limit + 600;
+	}
+	if (now < retry_limit) {
+		rc = schedule_timeout_interruptible(cfs_time_seconds(1));
+		if (rc == 0)
+			goto retry;
+	}
+
+	return NULL;
+}
+
 static struct obd_uuid *lmv_get_uuid(struct obd_export *exp)
 {
 	struct lmv_obd *lmv = &exp->exp_obd->u.lmv;
@@ -1747,7 +1790,7 @@ lmv_locate_tgt_by_name(struct lmv_obd *lmv, struct lmv_stripe_object *lso,
 
 	*fid = oinfo->lmo_fid;
 	*mds = oinfo->lmo_mds;
-	tgt = lmv_tgt(lmv, oinfo->lmo_mds);
+	tgt = lmv_tgt_retry(lmv, oinfo->lmo_mds);
 
 	CDEBUG(D_INODE, "locate MDT %u parent "DFID"\n", *mds, PFID(fid));
 
@@ -2367,7 +2410,7 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 
 		/* save source stripe FID in fid4 temporarily for ELC */
 		op_data->op_fid4 = oinfo->lmo_fid;
-		sp_tgt = lmv_tgt(lmv, oinfo->lmo_mds);
+		sp_tgt = lmv_tgt_retry(lmv, oinfo->lmo_mds);
 		if (!sp_tgt)
 			RETURN(-ENODEV);
 
@@ -2382,7 +2425,7 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 				RETURN(PTR_ERR(oinfo));
 
 			op_data->op_fid2 = oinfo->lmo_fid;
-			tp_tgt = lmv_tgt(lmv, oinfo->lmo_mds);
+			tp_tgt = lmv_tgt_retry(lmv, oinfo->lmo_mds);
 			if (!tp_tgt)
 				RETURN(-ENODEV);
 
@@ -2819,7 +2862,7 @@ static struct lu_dirent *stripe_dirent_load(struct lmv_dir_ctxt *ctxt,
 			break;
 		}
 
-		tgt = lmv_tgt(ctxt->ldc_lmv, oinfo->lmo_mds);
+		tgt = lmv_tgt_retry(ctxt->ldc_lmv, oinfo->lmo_mds);
 		if (!tgt) {
 			rc = -ENODEV;
 			break;

lustre/lmv/lmv_intent.c
@@ -208,7 +208,7 @@ int lmv_revalidate_slaves(struct obd_export *exp,
 		op_data->op_bias = MDS_CROSS_REF;
 		op_data->op_cli_flags = CLI_NO_SLOT;
 
-		tgt = lmv_tgt(lmv, lsm->lsm_md_oinfo[i].lmo_mds);
+		tgt = lmv_tgt_retry(lmv, lsm->lsm_md_oinfo[i].lmo_mds);
 		if (!tgt)
 			GOTO(cleanup, rc = -ENODEV);
 

lustre/lmv/lmv_internal.h
@@ -80,15 +80,14 @@ static inline struct obd_device *lmv2obd_dev(struct lmv_obd *lmv)
 	return container_of_safe(lmv, struct obd_device, u.lmv);
 }
 
-static inline struct lu_tgt_desc *
-lmv_tgt(struct lmv_obd *lmv, __u32 index)
+static inline struct lu_tgt_desc *lmv_tgt(struct lmv_obd *lmv, __u32 index)
 {
 	return index < lmv->lmv_mdt_descs.ltd_tgts_size ?
 		LTD_TGT(&lmv->lmv_mdt_descs, index) : NULL;
 }
+struct lu_tgt_desc *lmv_tgt_retry(struct lmv_obd *lmv, __u32 index);
 
-static inline bool
-lmv_mdt0_inited(struct lmv_obd *lmv)
+static inline bool lmv_mdt0_inited(struct lmv_obd *lmv)
 {
 	return lmv->lmv_mdt_descs.ltd_tgts_size > 0 &&
 	       test_bit(0, lmv->lmv_mdt_descs.ltd_tgt_bitmap);
@@ -148,11 +147,14 @@ lmv_fid2tgt(struct lmv_obd *lmv, const struct lu_fid *fid)
 	struct lu_tgt_desc *tgt;
 	int index;
 
+	if (!fid_is_sane(fid))
+		return ERR_PTR(-EINVAL);
+
 	index = lmv_fid2tgt_index(lmv, fid);
 	if (index < 0)
 		return ERR_PTR(index);
 
-	tgt = lmv_tgt(lmv, index);
+	tgt = lmv_tgt_retry(lmv, index);
 
 	return tgt ? tgt : ERR_PTR(-ENODEV);
 }

commit 4ae823762db40d790ddd00c29e969b5c8e376430

LU-17261 lov: unlink can handle bogus striping
Allow removing a file which has uninitialized OST objects in the
layout, possibly because LFSCK reconnected an orphan object back
into a mirrored file after the mirror had been deleted.

Don't wait and retry to access the bogus OST or MDT index in this
case, because the target will never appear, so waiting is futile.

Test-Parameters: testlist=sanity-flr,sanity-flr,sanity-flr,sanity-flr
Fixes: 94a4663db9 ("LU-17334 lmv: handle object created on newly added MDT")
Fixes: f35f897ec8 ("LU-17334 lov: handle object created on newly added OST")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I90b97c0e2d560d71b2a4c32a47fcfd7ae4e5535d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54544
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Zhenyu Xu <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -159,6 +159,10 @@ retry:
 		     "%s: MDT index %u/%u not configured\n" :
 		     "%s: MDT index %u more than MDT count %u\n",
 		     obd->obd_name, index, lmv->lmv_mdt_count);
+
+	if (index >= LOV_V1_INSANE_STRIPE_COUNT)
+		return NULL;
+
 	if (now > next_print) {
 		LCONSOLE_INFO("%s: wait %ds while client connects to new MDT\n",
 			      obd->obd_name, (int)(retry_limit - now));

lustre/lov/lov_ea.c
@@ -276,7 +276,7 @@ lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
 			continue;
 
 retry_new_ost:
-		if (unlikely(loi->loi_ost_idx >= lov->desc.ld_tgt_count ||
+		if (unlikely((u32)loi->loi_ost_idx >= lov->desc.ld_tgt_count ||
 			     !(ltd = lov->lov_tgts[loi->loi_ost_idx]))) {
 			time64_t now = ktime_get_seconds();
 
@@ -292,11 +292,15 @@ retry_new_ost:
 
 			/* log debug every loop, just to see it is trying */
 			CDEBUG_LIMIT(level,
-				loi->loi_ost_idx < lov->desc.ld_tgt_count ?
+				(u32)loi->loi_ost_idx < lov->desc.ld_tgt_count ?
 				"%s: FID "DOSTID" OST index %d/%u missing\n" :
 				"%s: FID "DOSTID" OST index %d more than OST count %u\n",
 				lov->desc.ld_uuid.uuid, POSTID(&loi->loi_oi),
 				loi->loi_ost_idx, lov->desc.ld_tgt_count);
+
+			if ((u32)loi->loi_ost_idx >= LOV_V1_INSANE_STRIPE_COUNT)
+				GOTO(out_lsme, rc = -EINVAL);
+
 			if (now > next_print) {
 				LCONSOLE_INFO("%s: wait %ds while client connects to new OST\n",
 					      lov->desc.ld_uuid.uuid,

lustre/tests/sanity-flr.sh
@@ -4454,6 +4454,42 @@ test_210a() {
 }
 run_test 210a "handle broken mirrored lovea"
 
+test_210b() {
+	local tf=$DIR/$tfile
+	local after
+	local mirrorred
+	local before
+
+	[ "$FSTYPE" != "zfs" ] || skip "ZFS file number is not accurate"
+
+	stack_trap "rm -f $tf"
+
+	# XXX: how to avoid precreate
+	wait_delete_completed
+	$LFS df -i | grep OST
+	before=$($LFS df -i|grep OST|awk '{sum=sum+$3}END{print sum}')
+	echo "before file creation: $before"
+	$LFS setstripe -c 2 $tf || error "can't create file"
+	dd if=/dev/zero of=$tf bs=1M count=1 || error "can't dd"
+
+#define OBD_FAIL_LOV_INVALID_OSTIDX		    0x1428
+	do_facet mds1 "$LCTL set_param fail_loc=0x1428"
+	$LFS mirror extend -N $tf || error "can't mirror"
+	$LFS df -i | grep OST
+	mirrored=$($LFS df -i|grep OST|awk '{sum=sum+$3}END{print sum}')
+	echo "after mirror: $mirrored"
+
+	rm $tf || error "can't remove"
+	[[ -f $tf ]] && error "rm failed"
+	wait_delete_completed
+
+	$LFS df -i | grep OST
+	after=$($LFS df -i|grep OST|awk '{sum=sum+$3}END{print sum}')
+	echo "after rm: $after"
+	(( after < before )) || error "something went wrong with unlink"
+}
+run_test 210b "handle broken mirrored lovea (unlink)"
+
 complete_test $SECONDS
 check_and_cleanup_lustre
 exit_status