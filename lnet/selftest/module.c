lustre/lustre-release

commit 43cbc93f1edc493e47fe5c4059bf0bae6a20c207

LU-8191 lnet: remove unused, fix non-static functions
lnet_selftest_structure_assertion() and
lnet_net_is_pref_rtr_locked() are never called.
This patch removes both functions.

Static analysis shows that a number of functions
could be made static. This patch also declares
several functions in lnet static.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Ie1b49c5652553715cd9f96b56090d33a95e3b438
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51436
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/selftest/module.c
@@ -85,19 +85,6 @@ lnet_selftest_exit(void)
 	}
 }
 
-void
-lnet_selftest_structure_assertion(void)
-{
-	BUILD_BUG_ON(sizeof(struct srpc_msg) != 160);
-	BUILD_BUG_ON(sizeof(struct srpc_test_reqst) != 70);
-	BUILD_BUG_ON(offsetof(struct srpc_msg, msg_body.tes_reqst.tsr_concur) !=
-		     72);
-	BUILD_BUG_ON(offsetof(struct srpc_msg, msg_body.tes_reqst.tsr_ndest) !=
-			      78);
-	BUILD_BUG_ON(sizeof(struct srpc_stat_reply) != 136);
-	BUILD_BUG_ON(sizeof(struct srpc_stat_reqst) != 28);
-}
-
 static int __init
 lnet_selftest_init(void)
 {

lnet/lnet/module.c
@@ -40,7 +40,7 @@ MODULE_PARM_DESC(config_on_load, "configure network at module load");
 
 static DEFINE_MUTEX(lnet_config_mutex);
 
-int lnet_configure(void *arg)
+static int lnet_configure(void *arg)
 {
 	/* 'arg' only there so I can be passed to cfs_create_thread() */
 	int    rc = 0;
@@ -67,7 +67,7 @@ out:
 	return rc;
 }
 
-int lnet_unconfigure(void)
+static int lnet_unconfigure(void)
 {
 	int refcount;
 

lnet/klnds/o2iblnd/o2iblnd.c
@@ -2657,7 +2657,7 @@ kiblnd_set_ni_fatal_on(struct kib_hca_dev *hdev, int val)
 		lnet_update_ping_buffer();
 }
 
-void
+static void
 kiblnd_event_handler(struct ib_event_handler *handler, struct ib_event *event)
 {
 	rwlock_t *g_lock = &kiblnd_data.kib_global_lock;

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -53,7 +53,7 @@ static void kiblnd_queue_tx(struct kib_tx *tx, struct kib_conn *conn);
 static void kiblnd_unmap_tx(struct kib_tx *tx);
 static void kiblnd_check_sends_locked(struct kib_conn *conn);
 
-void
+static void
 kiblnd_tx_done(struct kib_tx *tx)
 {
 	struct lnet_msg *lntmsg[2];

lnet/klnds/socklnd/socklnd.c
@@ -1640,7 +1640,7 @@ ksocknal_close_matching_conns(struct lnet_processid *id, __u32 ipaddr)
 	return (count == 0 ? -ENOENT : 0);
 }
 
-void
+static void
 ksocknal_notify_gw_down(struct lnet_nid *gw_nid)
 {
 	/* The router is telling me she's been notified of a change in

lnet/lnet/api-ni.c
@@ -1583,29 +1583,6 @@ __must_hold(&the_lnet.ln_api_mutex)
 	return 0;
 }
 
-bool
-lnet_net_is_pref_rtr_locked(struct lnet_net *net, struct lnet_nid *rtr_nid)
-{
-	struct lnet_nid_list *ne;
-
-	CDEBUG(D_NET, "%s: rtr pref empty: %d\n",
-	       libcfs_net2str(net->net_id),
-	       list_empty(&net->net_rtr_pref_nids));
-
-	if (list_empty(&net->net_rtr_pref_nids))
-		return false;
-
-	list_for_each_entry(ne, &net->net_rtr_pref_nids, nl_list) {
-		CDEBUG(D_NET, "Comparing pref %s with gw %s\n",
-		       libcfs_nidstr(&ne->nl_nid),
-		       libcfs_nidstr(rtr_nid));
-		if (nid_same(rtr_nid, &ne->nl_nid))
-			return true;
-	}
-
-	return false;
-}
-
 static unsigned int
 lnet_nid4_cpt_hash(lnet_nid_t nid, unsigned int number)
 {
@@ -3445,7 +3422,7 @@ lnet_get_next_ni_locked(struct lnet_net *mynet, struct lnet_ni *prev)
 	return ni;
 }
 
-int
+static int
 lnet_get_net_config(struct lnet_ioctl_config_data *config)
 {
 	struct lnet_ni *ni;
@@ -3468,7 +3445,7 @@ lnet_get_net_config(struct lnet_ioctl_config_data *config)
 	return rc;
 }
 
-int
+static int
 lnet_get_ni_config(struct lnet_ioctl_config_ni *cfg_ni,
 		   struct lnet_ioctl_config_lnd_tunables *tun,
 		   struct lnet_ioctl_element_stats *stats,
@@ -3496,7 +3473,7 @@ lnet_get_ni_config(struct lnet_ioctl_config_ni *cfg_ni,
 	return rc;
 }
 
-int lnet_get_ni_stats(struct lnet_ioctl_element_msg_stats *msg_stats)
+static int lnet_get_ni_stats(struct lnet_ioctl_element_msg_stats *msg_stats)
 {
 	struct lnet_ni *ni;
 	int cpt;
@@ -5461,7 +5438,7 @@ static int lnet_route_show_done(struct netlink_callback *cb)
 	return 0;
 }
 
-int lnet_scan_route(struct lnet_genl_route_list *rlist,
+static int lnet_scan_route(struct lnet_genl_route_list *rlist,
 		    struct lnet_route_properties *settings)
 {
 	struct lnet_remotenet *rnet;

lnet/lnet/lib-move.c
@@ -2086,7 +2086,7 @@ lnet_handle_spec_local_mr_dst(struct lnet_send_data *sd)
 	return -EHOSTUNREACH;
 }
 
-struct lnet_ni *
+static struct lnet_ni *
 lnet_find_best_ni_on_spec_net(struct lnet_ni *cur_best_ni,
 			      struct lnet_peer *peer,
 			      struct lnet_peer_net *peer_net,
@@ -2452,7 +2452,7 @@ lnet_handle_spec_router_dst(struct lnet_send_data *sd)
 	return lnet_handle_send(sd);
 }
 
-struct lnet_ni *
+static struct lnet_ni *
 lnet_find_best_ni_on_local_net(struct lnet_peer *peer, int md_cpt,
 			       struct lnet_msg *msg, bool discovery)
 {

lnet/lnet/lib-msg.c
@@ -624,7 +624,7 @@ lnet_resend_msg_locked(struct lnet_msg *msg)
 	complete(&the_lnet.ln_mt_wait_complete);
 }
 
-int
+static int
 lnet_check_finalize_recursion_locked(struct lnet_msg *msg,
 				     struct list_head *containerq,
 				     int nworkers, void **workers)
@@ -650,7 +650,7 @@ lnet_check_finalize_recursion_locked(struct lnet_msg *msg,
 	return my_slot;
 }
 
-int
+static int
 lnet_attempt_msg_resend(struct lnet_msg *msg)
 {
 	struct lnet_msg_container *container;

commit 9f7b60739e11f5e6e762020d3fb1c82727ea2ae6

LU-8191 selftest: restore BUILD_BUGs
It was wrong to remove lnet_selftest_structure_assertion()
since it contained BUILD_BUGs used to ensure different LNet
Selftest versions can interoperate.

Restore the BUILD_BUGs and add a dummy user in LNet Selftest
init. This should prevent analyzers from picking this up as
an unused function.

Fixes: 43cbc93 ("LU-8191 lnet: remove unused, fix non-static functions")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I5e498dc411048f0ae3ca69cc5d24a728a711d6f7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54635
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/selftest/module.c
@@ -67,12 +67,31 @@ lnet_selftest_exit(void)
 	}
 }
 
+void
+lnet_selftest_structure_assertion(void)
+{
+	BUILD_BUG_ON(sizeof(struct srpc_msg) != 160);
+	BUILD_BUG_ON(sizeof(struct srpc_test_reqst) != 70);
+	BUILD_BUG_ON(offsetof(struct srpc_msg, msg_body.tes_reqst.tsr_concur) !=
+		     72);
+	BUILD_BUG_ON(offsetof(struct srpc_msg, msg_body.tes_reqst.tsr_ndest) !=
+			      78);
+	BUILD_BUG_ON(sizeof(struct srpc_stat_reply) != 136);
+	BUILD_BUG_ON(sizeof(struct srpc_stat_reqst) != 28);
+}
+
 static int __init
 lnet_selftest_init(void)
 {
 	int nscheds;
 	int rc = -ENOMEM;
 	int i;
+
+	/* This assertion checks that struct sizes do not drift
+	 * inadvertently and induce crashes when different nodes
+	 * running LNet Selftest have mismatched structures.
+	 */
+	lnet_selftest_structure_assertion();
 
 	rc = libcfs_setup();
 	if (rc)