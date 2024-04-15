lustre/lustre-release

commit a9411a9856a0a1539ea2c8dc9c7eb1bd8fa2c409

LU-17076 nrs: wait for RCU completion
before we destroy the slab holding the objects scheduled
for a release via RCU.

    BUG: unable to handle kernel paging request at 000000046474e5c6
    Oops: 0000 [#1] SMP
    CPU: 0 PID: 9 Comm: ksoftirqd/0  4.18.0 #3
    RIP: 0010:kmem_cache_free+0xe3/0x170
    Call Trace:
     rcu_core+0x27a/0x770
     __do_softirq+0xc2/0x44d
     run_ksoftirqd+0x35/0x50
     smpboot_thread_fn+0xb8/0x160
     kthread+0x14a/0x170

Fixes: 42bf5f78ba ("LU-8130 nrs: convert NRS ORR/TRR to rhashtable")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Ia4b1cf6f190f17c3b85548fcb6876be72822cdd2
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52515
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/ptlrpc/nrs_orr.c
@@ -623,6 +623,7 @@ static void nrs_orr_stop(struct ptlrpc_nrs_policy *policy)
 		rhashtable_free_and_destroy(&orrd->od_obj_hash,
 					    nrs_orr_hash_exit, NULL);
 	}
+	synchronize_rcu();
 	kmem_cache_destroy(orrd->od_cache);
 
 	OBD_FREE_PTR(orrd);

commit f2868d1edfd57f81211d411728d076a10c77bcdc

LU-17705 ptlrpc: replace synchronize_rcu() with rcu_barrier()
synchronize_rcu() does not wait for in-flight rcu callback completion,
thus kmem_cache_free() can still race with kmem_cache_destroy().

Fixes: a9411a9856a ("LU-17076 nrs: wait for RCU completion")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I2da668c06b532a41c8ce2fe681ea17cf6f3013ef
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54669
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/ldlm/ldlm_lockd.c
@@ -3578,7 +3578,7 @@ void ldlm_exit(void)
 {
 	if (ldlm_refcount)
 		CERROR("ldlm_refcount is %d in %s\n", ldlm_refcount, __func__);
-	synchronize_rcu();
+	rcu_barrier();
 	kmem_cache_destroy(ldlm_resource_slab);
 	/*
 	 * ldlm_lock_put() use RCU to call ldlm_lock_free, so need call

lustre/ptlrpc/nrs_orr.c
@@ -623,7 +623,7 @@ static void nrs_orr_stop(struct ptlrpc_nrs_policy *policy)
 		rhashtable_free_and_destroy(&orrd->od_obj_hash,
 					    nrs_orr_hash_exit, NULL);
 	}
-	synchronize_rcu();
+	rcu_barrier();
 	kmem_cache_destroy(orrd->od_cache);
 
 	OBD_FREE_PTR(orrd);