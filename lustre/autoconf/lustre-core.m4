lustre/lustre-release

commit 0e6648940171c60cbaa295f54af1093232e66430

LU-16619 build: Ubuntu jammy 5.19 client support
Ubuntu 5.19 kernel removed lsmcontext_init() and changed
security_dentry_init_security to require struct context *

Linux kernel linux-hwe-5.19
LSM: Removed scaffolding function lsmcontext_init

Linux kernel linux-hwe-5.19
LSM: security_dentry_init_security with struct lsmcontext

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ib6479a2cd20df5e565ae6203e05df2afa3f3de31
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50210
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -2349,7 +2349,7 @@ AC_DEFUN([LC_CURRENT_TIME], [
 		AC_DEFINE(HAVE_CURRENT_TIME, 1,
 			[current_time() has replaced CURRENT_TIME])
 	])
-]) # LIBCFS_CURRENT_TIME
+]) # LC_CURRENT_TIME
 
 #
 # LC_HAVE_GET_INODE_USAGE
@@ -3416,6 +3416,58 @@ AC_DEFUN([LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO], [
 ]) # LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
 
 #
+# LC_HAVE_LSMCONTEXT_INIT
+#
+# repo: git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy
+# kernel linux-hwe-5.19 commit fef1deb99dad87dd700afae76b35c5b5750e33a8
+# LSM: Removed scaffolding function lsmcontext_init
+#
+AC_DEFUN([LC_SRC_HAVE_LSMCONTEXT_INIT], [
+	LB2_LINUX_TEST_SRC([lsmcontext_init], [
+		#include <linux/security.h>
+	],[
+		struct lsmcontext ctx = {};
+
+		lsmcontext_init(&ctx, "", 0, 0);
+	],[])
+])
+AC_DEFUN([LC_HAVE_LSMCONTEXT_INIT], [
+	AC_MSG_CHECKING([if lsmcontext_init is available])
+	LB2_LINUX_TEST_RESULT([lsmcontext_init], [
+		AC_DEFINE(HAVE_LSMCONTEXT_INIT, 1,
+			[lsmcontext_init is available])
+	])
+]) # LC_HAVE_LSMCONTEXT_INIT
+
+#
+# LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+#
+# repo: git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy
+# kernel linux-hwe-5.19 commit 57d0004bc811254916be30f94c86d9607867deb0
+# LSM: Use lsmcontext in security_dentry_init_security
+#
+AC_DEFUN([LC_SRC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX], [
+	LB2_LINUX_TEST_SRC([security_dentry_init_security_with_ctx], [
+		#include <linux/security.h>
+	],[
+		struct dentry *dentry = NULL;
+		const struct qstr *name = NULL;
+		struct lsmcontext *ctx = NULL;
+		const char *xattr_name = "";
+
+		(void)security_dentry_init_security(dentry, 0, name,
+						    &xattr_name, ctx);
+	],[-Werror])
+])
+AC_DEFUN([LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX], [
+	AC_MSG_CHECKING([if security_dentry_init_security needs lsmcontext])
+	LB2_LINUX_TEST_RESULT([security_dentry_init_security_with_ctx], [
+		AC_DEFINE(HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX, 1,
+			[security_dentry_init_security needs lsmcontext])
+	])
+]) # LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+
+#
 # LC_HAVE_NO_LLSEEK
 #
 # Linux commit v5.19-rc2-6-g868941b14441
@@ -3888,6 +3940,8 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_SRC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
+	LC_SRC_HAVE_LSMCONTEXT_INIT
+	LC_SRC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
 
 	# 6.0
 	LC_SRC_HAVE_NO_LLSEEK
@@ -4143,6 +4197,8 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
+	LC_HAVE_LSMCONTEXT_INIT
+	LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
 
 	# 6.0
 	LC_HAVE_NO_LLSEEK

lustre/include/lustre_compat.h
@@ -597,12 +597,26 @@ static inline bool is_root_inode(struct inode *inode)
 #define ll_access_ok(ptr, len) access_ok(ptr, len)
 #endif
 
-static inline void ll_security_release_secctx(char *secdata, u32 seclen)
+#ifdef HAVE_SEC_RELEASE_SECCTX_1ARG
+#ifndef HAVE_LSMCONTEXT_INIT
+/* Ubuntu 5.19 */
+static inline void lsmcontext_init(struct lsmcontext *cp, char *context,
+				   u32 size, int slot)
+{
+	cp->slot = slot;
+	cp->context = context;
+	cp->len = size;
+}
+#endif
+#endif
+
+static inline void ll_security_release_secctx(char *secdata, u32 seclen,
+					      int slot)
 {
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG
 	struct lsmcontext context = { };
 
-	lsmcontext_init(&context, secdata, seclen, 0);
+	lsmcontext_init(&context, secdata, seclen, slot);
 	return security_release_secctx(&context);
 #else
 	return security_release_secctx(secdata, seclen);

lustre/llite/xattr_security.c
@@ -47,16 +47,23 @@
 # define XATTR_NAME_SELINUX XATTR_SECURITY_PREFIX XATTR_SELINUX_SUFFIX
 #endif
 
+#ifdef HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+#define HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG 1
+#endif
+
 /*
  * Check for LL_SBI_FILE_SECCTX before calling.
  */
 int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
 			    const char **secctx_name, __u32 *secctx_name_size,
-			    void **secctx, __u32 *secctx_size)
+			    void **secctx, __u32 *secctx_size, int *secctx_slot)
 {
 	struct ll_sb_info *sbi = ll_s2sbi(dentry->d_sb);
 #ifdef HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 	const char *secctx_name_lsm = NULL;
+#endif
+#ifdef HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+	struct lsmcontext ctx = {};
 #endif
 	int rc;
 
@@ -81,12 +88,22 @@ int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
 #ifdef HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 					   &secctx_name_lsm,
 #endif
+#ifdef HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+					   &ctx);
+#else
 					   secctx, secctx_size);
+#endif
 	/* ignore error if the hook is not supported by the LSM module */
 	if (rc == -EOPNOTSUPP)
 		return 0;
 	if (rc < 0)
 		return rc;
+
+#ifdef HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
+	*secctx = ctx.context;
+	*secctx_size = ctx.len;
+	*secctx_slot = ctx.slot;
+#endif
 
 #ifdef HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 	if (strncmp(*secctx_name, secctx_name_lsm, *secctx_name_size) != 0) {

lustre/include/obd.h
@@ -931,6 +931,7 @@ struct md_op_data {
 	__u32			op_file_secctx_name_size;
 	void		       *op_file_secctx;
 	__u32			op_file_secctx_size;
+	int			op_file_secctx_slot;
 
 	/* File encryption context, for creates/metadata ops */
 	void		       *op_file_encctx;

lustre/llite/dir.c
@@ -546,7 +546,8 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 					      &op_data->op_file_secctx_name,
 					      &op_data->op_file_secctx_name_size,
 					      &op_data->op_file_secctx,
-					      &op_data->op_file_secctx_size);
+					      &op_data->op_file_secctx_size,
+					      &op_data->op_file_secctx_slot);
 		if (err < 0)
 			GOTO(out_op_data, err);
 	}

lustre/llite/llite_internal.h
@@ -493,7 +493,9 @@ static inline bool ll_xattr_suffix_is_seclabel(const char *suffix)
 
 int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
 			    const char **secctx_name, __u32 *secctx_name_size,
-			    void **secctx, __u32 *secctx_size);
+			    void **secctx, __u32 *secctx_size,
+			    int *secctx_slot);
+
 int ll_inode_init_security(struct dentry *dentry, struct inode *inode,
 			   struct inode *dir);
 

lustre/llite/llite_lib.c
@@ -3679,7 +3679,8 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
 {
 	ll_unlock_md_op_lsm(op_data);
 	ll_security_release_secctx(op_data->op_file_secctx,
-				   op_data->op_file_secctx_size);
+				   op_data->op_file_secctx_size,
+				   op_data->op_file_secctx_slot);
 	if (op_data->op_flags & MF_OPNAME_KMALLOCED)
 		/* allocated via ll_setup_filename called
 		 * from ll_prep_md_op_data

lustre/llite/namei.c
@@ -810,6 +810,7 @@ out:
 static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 				   struct lookup_intent *it,
 				   void **secctx, __u32 *secctxlen,
+				   int *secctxslot,
 				   struct pcc_create_attach *pca,
 				   bool encrypt,
 				   void **encctx, __u32 *encctxlen)
@@ -882,18 +883,23 @@ static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 					     &op_data->op_file_secctx_name,
 					     &op_data->op_file_secctx_name_size,
 					     &op_data->op_file_secctx,
-					     &op_data->op_file_secctx_size);
+					     &op_data->op_file_secctx_size,
+					     &op_data->op_file_secctx_slot);
 		if (rc < 0)
 			GOTO(out, retval = ERR_PTR(rc));
 		if (secctx != NULL)
 			*secctx = op_data->op_file_secctx;
 		if (secctxlen != NULL)
 			*secctxlen = op_data->op_file_secctx_size;
+		if (secctxslot != NULL)
+			*secctxslot = op_data->op_file_secctx_slot;
 	} else {
 		if (secctx != NULL)
 			*secctx = NULL;
 		if (secctxlen != NULL)
 			*secctxlen = 0;
+		if (secctxslot != NULL)
+			*secctxslot = 0;
 	}
 	if (it->it_op & IT_CREAT && encrypt) {
 		if (unlikely(filename_is_volatile(dentry->d_name.name,
@@ -1107,7 +1113,7 @@ static struct dentry *ll_lookup_nd(struct inode *parent, struct dentry *dentry,
 		itp = NULL;
 	else
 		itp = &it;
-	de = ll_lookup_it(parent, dentry, itp, NULL, NULL, NULL, false,
+	de = ll_lookup_it(parent, dentry, itp, NULL, NULL, NULL, NULL, false,
 			  NULL, NULL);
 
 	if (itp != NULL)
@@ -1149,6 +1155,7 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 	long long lookup_flags = LOOKUP_OPEN;
 	void *secctx = NULL;
 	__u32 secctxlen = 0;
+	int secctxslot = 0;
 	void *encctx = NULL;
 	__u32 encctxlen = 0;
 	struct ll_sb_info *sbi = NULL;
@@ -1247,8 +1254,8 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 		it->it_flags |= MDS_OPEN_LOCK;
 
 	/* Dentry added to dcache tree in ll_lookup_it */
-	de = ll_lookup_it(dir, dentry, it, &secctx, &secctxlen, &pca, encrypt,
-			  &encctx, &encctxlen);
+	de = ll_lookup_it(dir, dentry, it, &secctx, &secctxlen, &secctxslot,
+			  &pca, encrypt, &encctx, &encctxlen);
 	if (IS_ERR(de))
 		rc = PTR_ERR(de);
 	else if (de != NULL)
@@ -1262,7 +1269,8 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 			rc = ll_create_it(dir, dentry, it, secctx, secctxlen,
 					  encrypt, encctx, encctxlen,
 					  open_flags);
-			ll_security_release_secctx(secctx, secctxlen);
+			ll_security_release_secctx(secctx, secctxlen,
+						   secctxslot);
 			llcrypt_free_ctx(encctx, encctxlen);
 			if (rc) {
 				/* We dget in ll_splice_alias. */
@@ -1559,7 +1567,8 @@ again:
 					      &op_data->op_file_secctx_name,
 					      &op_data->op_file_secctx_name_size,
 					      &op_data->op_file_secctx,
-					      &op_data->op_file_secctx_size);
+					      &op_data->op_file_secctx_size,
+					      &op_data->op_file_secctx_slot);
 		if (err < 0)
 			GOTO(err_exit, err);
 	}

commit d74d86840056e83cd0138a261ab7b6040a43bfb4

LU-17767 build: struct lsmcontext has slot or id member
With Ubuntu 24.04 kernel 6.8.0-31-generic, the struct lsmcontext uses
a field named 'id' to identify the LSM module, instead of 'slot' in
previous kernel versions.

Fixes: 0e66489401 ("LU-16619 build: Ubuntu jammy 5.19 client support")
Test-Parameters: trivial
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I5080e60614b42ed63103f93cae1f481851742d0b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54881
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/autoconf/lustre-core.m4
@@ -3675,6 +3675,28 @@ AC_DEFUN([LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX], [
 ]) # LC_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
 
 #
+# LC_LSMCONTEXT_HAS_ID
+#
+# repo: git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/noble
+# commit dad7cffb719e45fd43121ade4ada90ff376c9cad
+# LSM stacking v39: LSM: Ensure the correct LSM context releaser
+#
+AC_DEFUN([LC_SRC_LSMCONTEXT_HAS_ID], [
+	LB2_LINUX_TEST_SRC([lsmcontext_has_id], [
+		#include <linux/security.h>
+	],[
+		((struct lsmcontext *)1)->id = 0;
+	],[-Werror])
+])
+AC_DEFUN([LC_LSMCONTEXT_HAS_ID], [
+	LB2_MSG_LINUX_TEST_RESULT([if lsmcontext has id],
+	[lsmcontext_has_id], [
+		AC_DEFINE(HAVE_LSMCONTEXT_HAS_ID, 1,
+			[lsmcontext has id])
+	])
+]) # LC_LSMCONTEXT_HAS_ID
+
+#
 # LC_HAVE_NO_LLSEEK
 #
 # Linux commit v5.19-rc2-6-g868941b14441
@@ -4803,6 +4825,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	# 6.8
 	LC_SRC_HAVE_DENTRY_D_CHILDREN
 	LC_SRC_HAVE_GENERIC_ERROR_REMOVE_FOLIO
+	LC_SRC_LSMCONTEXT_HAS_ID
 
 	# kernel patch to extend integrity interface
 	LC_SRC_BIO_INTEGRITY_PREP_FN
@@ -5113,6 +5136,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	# 6.8
 	LC_HAVE_DENTRY_D_CHILDREN
 	LC_HAVE_GENERIC_ERROR_REMOVE_FOLIO
+	LC_LSMCONTEXT_HAS_ID
 
 	# kernel patch to extend integrity interface
 	LC_BIO_INTEGRITY_PREP_FN

lustre/include/lustre_compat.h
@@ -774,7 +774,11 @@ ll_shrinker_create(struct ll_shrinker_ops *ops, unsigned int flags,
 static inline void lsmcontext_init(struct lsmcontext *cp, char *context,
 				   u32 size, int slot)
 {
+#ifdef HAVE_LSMCONTEXT_HAS_ID
+	cp->id = slot;
+#else
 	cp->slot = slot;
+#endif
 	cp->context = context;
 	cp->len = size;
 }

lustre/llite/xattr_security.c
@@ -102,8 +102,12 @@ int ll_dentry_init_security(struct dentry *dentry, int mode, struct qstr *name,
 #ifdef HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX
 	*secctx = ctx.context;
 	*secctx_size = ctx.len;
+#ifdef HAVE_LSMCONTEXT_HAS_ID
+	*secctx_slot = ctx.id;
+#else
 	*secctx_slot = ctx.slot;
-#endif
+#endif /* HAVE_LSMCONTEXT_HAS_ID */
+#endif /* HAVE_SECURITY_DENTRY_INIT_SECURTY_WITH_CTX */
 
 #ifdef HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 	if (strncmp(*secctx_name, secctx_name_lsm, *secctx_name_size) != 0) {