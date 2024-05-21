lustre/lustre-release

commit 7a0517fa25162a6af2e10827851d7d8c7a4ed1ac

LU-17592 build: kernel 6.8 removed strlcpy()
Linux commit v6.7-11707-gd26270061ae6
  string: Remove strlcpy()

strlcpy() is removed, use strscpy() and provide a strscpy()
for kernels that do not have one.

HPE-bug-id: LUS-12181
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ieab872f20e08d17a4842bc944fa38f9867de81f9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54227
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_lov.c
@@ -778,11 +778,11 @@ static int lod_gen_component_ea(const struct lu_env *env,
 		objs = &lmm->lmm_objects[0];
 	} else {
 		struct lov_mds_md_v3 *v3 = (struct lov_mds_md_v3 *)lmm;
-		size_t cplen = strlcpy(v3->lmm_pool_name,
+		size_t cplen = strscpy(v3->lmm_pool_name,
 				       lod_comp->llc_pool,
 				       sizeof(v3->lmm_pool_name));
-		if (cplen >= sizeof(v3->lmm_pool_name))
-			RETURN(-E2BIG);
+		if (cplen < 0)
+			RETURN(cplen);
 		objs = &v3->lmm_objects[0];
 	}
 	lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);

lustre/lov/lov_ea.c
@@ -241,10 +241,10 @@ lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
 	if (pool_name) {
 		size_t pool_name_len;
 
-		pool_name_len = strlcpy(lsme->lsme_pool_name, pool_name,
+		pool_name_len = strscpy(lsme->lsme_pool_name, pool_name,
 					sizeof(lsme->lsme_pool_name));
-		if (pool_name_len >= sizeof(lsme->lsme_pool_name))
-			GOTO(out_lsme, rc = -E2BIG);
+		if (pool_name_len < 0)
+			GOTO(out_lsme, rc = pool_name_len);
 	}
 
 	/* with Data-on-MDT set maxbytes to stripe size */

libcfs/autoconf/lustre-libcfs.m4
@@ -625,7 +625,9 @@ AC_DEFUN([LIBCFS_SRC_STRSCPY_EXISTS], [
 	LB2_LINUX_TEST_SRC([strscpy_exists], [
 		#include <linux/string.h>
 	],[
-		strscpy((char *)NULL, (const char *)NULL, 0);
+		char buf[129];
+
+		strscpy(buf, "something", sizeof(buf));
 	],[-Werror])
 ])
 AC_DEFUN([LIBCFS_STRSCPY_EXISTS], [

libcfs/include/libcfs/linux/linux-misc.h
@@ -204,7 +204,12 @@ static inline void *cfs_kallsyms_lookup_name(const char *name)
 #endif
 
 #ifndef HAVE_STRSCPY
-#define strscpy(s1, s2, sz)    strlcpy((s1), (s2), (sz))
+static inline ssize_t strscpy(char *s1, const char *s2, size_t sz)
+{
+	ssize_t len = strlcpy(s1, s2, sz);
+
+	return (len >= sz) ? -E2BIG : len;
+}
 #endif
 
 #ifndef HAVE_BITMAP_TO_ARR32

libcfs/libcfs/hash.c
@@ -1068,7 +1068,7 @@ cfs_hash_create(char *name, unsigned cur_bits, unsigned max_bits,
         if (hs == NULL)
                 RETURN(NULL);
 
-	strlcpy(hs->hs_name, name, len);
+	strscpy(hs->hs_name, name, len);
 	hs->hs_flags = flags;
 
 	kref_init(&hs->hs_refcount);

libcfs/libcfs/tracefile.c
@@ -660,12 +660,12 @@ void libcfs_debug_msg(struct libcfs_debug_msg_data *msgdata,
 		debug_buf += sizeof(header);
 	}
 
-	strlcpy(debug_buf, file, PAGE_SIZE - tage->used);
+	snprintf(debug_buf, PAGE_SIZE - tage->used, "%s", file);
 	tage->used += strlen(file) + 1;
 	debug_buf += strlen(file) + 1;
 
 	if (fn) {
-		strlcpy(debug_buf, fn, PAGE_SIZE - tage->used);
+		snprintf(debug_buf, PAGE_SIZE - tage->used, "%s", fn);
 		tage->used += strlen(fn) + 1;
 		debug_buf += strlen(fn) + 1;
 	}

lnet/klnds/o2iblnd/o2iblnd.c
@@ -2276,15 +2276,15 @@ kiblnd_init_poolset(struct kib_poolset *ps, int cpt,
 	memset(ps, 0, sizeof(struct kib_poolset));
 
 	ps->ps_cpt	    = cpt;
-        ps->ps_net          = net;
-        ps->ps_pool_create  = po_create;
-        ps->ps_pool_destroy = po_destroy;
-        ps->ps_node_init    = nd_init;
-        ps->ps_node_fini    = nd_fini;
-        ps->ps_pool_size    = size;
-	if (strlcpy(ps->ps_name, name, sizeof(ps->ps_name))
-	    >= sizeof(ps->ps_name))
-		return -E2BIG;
+	ps->ps_net          = net;
+	ps->ps_pool_create  = po_create;
+	ps->ps_pool_destroy = po_destroy;
+	ps->ps_node_init    = nd_init;
+	ps->ps_node_fini    = nd_fini;
+	ps->ps_pool_size    = size;
+	rc = strscpy(ps->ps_name, name, sizeof(ps->ps_name));
+	if (rc < 0)
+		return rc;
 	spin_lock_init(&ps->ps_lock);
 	INIT_LIST_HEAD(&ps->ps_pool_list);
 	INIT_LIST_HEAD(&ps->ps_failed_pool_list);
@@ -3736,7 +3736,7 @@ kiblnd_startup(struct lnet_ni *ni)
 		}
 
 		ibdev->ibd_ifip = ntohl(ifaces[i].li_ipaddr);
-		strlcpy(ibdev->ibd_ifname, ifaces[i].li_name,
+		strscpy(ibdev->ibd_ifname, ifaces[i].li_name,
 			sizeof(ibdev->ibd_ifname));
 		ibdev->ibd_can_failover = ifaces[i].li_iff_master;
 

lnet/klnds/socklnd/socklnd.c
@@ -2633,7 +2633,7 @@ ksocknal_startup(struct lnet_ni *ni)
 		ni->ni_nid.nid_size = 0;
 		ni->ni_nid.nid_addr[0] = sa->sin_addr.s_addr;
 	}
-	strlcpy(ksi->ksni_name, ifaces[if_idx].li_name, sizeof(ksi->ksni_name));
+	strscpy(ksi->ksni_name, ifaces[if_idx].li_name, sizeof(ksi->ksni_name));
 
 	/* call it before add it to ksocknal_data.ksnd_nets */
 	rc = ksocknal_net_start_threads(net, ni->ni_cpts, ni->ni_ncpts);

commit b9591d968a723aab1b121c6008d97cba6b010013

LU-17000 misc: fix strscpy() Coverity warnings
Fix warning reported for use of uninitialized vairable

CoverityID: 425254 ("Uninitialized scalar variable")

Fix warning reported when changing call from strlcpy()
to strscpy()

CoverityID: 425253 ("Unsigned compared against 0")
CoverityID: 425262 ("Unsigned compared against 0")
Fixes: 7a0517fa2 ("LU-17592 build: kernel 6.8 removed strlcpy()")

Test-Parameters: trivial
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Id3804c77a105e4776a0242db787dc1ca2528d9ca
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54865
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_lov.c
@@ -781,9 +781,10 @@ static int lod_gen_component_ea(const struct lu_env *env,
 		objs = &lmm->lmm_objects[0];
 	} else {
 		struct lov_mds_md_v3 *v3 = (struct lov_mds_md_v3 *)lmm;
-		size_t cplen = strscpy(v3->lmm_pool_name,
+		ssize_t cplen = strscpy(v3->lmm_pool_name,
 				       lod_comp->llc_pool ? : "\0",
 				       sizeof(v3->lmm_pool_name));
+
 		if (cplen < 0)
 			RETURN(cplen);
 		objs = &v3->lmm_objects[0];

lustre/lov/lov_ea.c
@@ -239,7 +239,7 @@ lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
 	lsme->lsme_layout_gen = le16_to_cpu(lmm->lmm_layout_gen);
 
 	if (pool_name) {
-		size_t pool_name_len;
+		ssize_t pool_name_len;
 
 		pool_name_len = strscpy(lsme->lsme_pool_name, pool_name,
 					sizeof(lsme->lsme_pool_name));

lustre/kunit/llog_test.c
@@ -754,7 +754,7 @@ static int llog_cancel_rec_cb(const struct lu_env *env,
 static int llog_test_5(const struct lu_env *env, struct obd_device *obd)
 {
 	struct llog_handle *llh = NULL;
-	char name[10];
+	char name[10] = "";
 	int rc, rc2;
 	struct llog_mini_rec lmr;
 	struct llog_ctxt *ctxt;