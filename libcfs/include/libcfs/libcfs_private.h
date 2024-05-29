lustre/lustre-release

commit 718e3f3e680f422d865a15890ac60e66dcd9e240

LU-17504 build: fix gcc-13 [-Werror=stringop-overread] error
This patch fixes the following [-Werror=stringop-overread] and
[-Werror=attribute-warning] errors detected by gcc 13:

lustre/mgc/mgc_request.c:190:21: error: 'strcmp' reading 1 or
more bytes from a region of size 0 [-Werror=stringop-overread]
  190 | if (strcmp(logname, cld->cld_logname) == 0) {
      |     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

In function 'fortify_memcpy_chk',
    inlined from 'class_handle_ioctl' at
/root/lustre-release/lustre/obdclass/class_obd.c:381:3:
include/linux/fortify-string.h:528:25: error:
call to '__write_overflow_field' declared with attribute warning:
detected write beyond size of field (1st parameter);
maybe use struct_group()? [-Werror=attribute-warning]
  528 |  __write_overflow_field(p_size_field, size);
      |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Signed-off-by: Jian Yu <yujian@whamcloud.com>
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I59f5a88b4cd64c9f4e67e568546baada371543b1
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54834
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

libcfs/libcfs/hash.c
@@ -1064,7 +1064,7 @@ cfs_hash_create(char *name, unsigned cur_bits, unsigned max_bits,
 
         len = (flags & CFS_HASH_BIGNAME) == 0 ?
               CFS_HASH_NAME_LEN : CFS_HASH_BIGNAME_LEN;
-	LIBCFS_ALLOC(hs, offsetof(struct cfs_hash, hs_name[len]));
+	LIBCFS_ALLOC(hs, sizeof(struct cfs_hash) + len);
         if (hs == NULL)
                 RETURN(NULL);
 

libcfs/include/libcfs/libcfs_hash.h
@@ -251,8 +251,8 @@ struct cfs_hash {
         /** workitem to output max depth */
 	struct work_struct		hs_dep_work;
 #endif
-        /** name of htable */
-        char                        hs_name[0];
+	/** name of htable */
+	char                        hs_name[];
 };
 
 struct cfs_hash_lock_ops {

lnet/lnet/config.c
@@ -20,7 +20,7 @@
 struct lnet_text_buf {
 	struct list_head	ltb_list;	/* stash on lists */
 	int			ltb_size;	/* allocated size */
-	char			ltb_text[0];	/* text buffer */
+	char			ltb_text[];	/* text buffer */
 };
 
 static int lnet_tbnob = 0;			/* track text buf allocation */

lnet/selftest/module.c
@@ -67,7 +67,7 @@ lnet_selftest_exit(void)
 	}
 }
 
-void
+static void
 lnet_selftest_structure_assertion(void)
 {
 	BUILD_BUG_ON(sizeof(struct srpc_msg) != 160);

lustre/include/lustre_quota.h
@@ -78,7 +78,7 @@ struct lquota_iter {
 	struct list_head li_link;
 	__u32		 li_md_size;
 	__u32		 li_dt_size;
-	char		 li_buffer[0];
+	char		 li_buffer[];
 };
 
 struct if_quotactl_iter {

lustre/include/lustre_sec.h
@@ -835,7 +835,7 @@ struct sptlrpc_sepol {
 	 * <mode>:<policy name>:<policy version>:<policy hash>
 	 */
 	__u32		ssp_sepol_size;
-	char		ssp_sepol[0];
+	char		ssp_sepol[];
 };
 
 /**

lustre/include/obd_class.h
@@ -308,7 +308,7 @@ struct config_llog_data {
 						     * watching */
 				    cld_lostlock:1, /* lock not requeued */
 				    cld_processed:1;  /* successfully fetched */
-	char			    cld_logname[0];
+	char			    cld_logname[];
 };
 
 struct lustre_profile {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -452,7 +452,7 @@ struct lu_dirent {
 	 *  their natural order. After the last attribute, padding bytes are
 	 *  added to make ->lde_reclen a multiple of 8.
 	 */
-	char          lde_name[0];
+	char          lde_name[];
 };
 
 /*
@@ -3363,7 +3363,7 @@ struct lu_idxpage {
 	 *
 	 * For the time being, we only support fixed-size key & record.
 	 */
-	char	lip_entries[0];
+	char	lip_entries[];
 };
 
 #define LIP_HDR_SIZE (offsetof(struct lu_idxpage, lip_entries))
@@ -3529,7 +3529,7 @@ struct object_update_param {
 	__u16	oup_len;	/* length of this parameter */
 	__u16	oup_padding;
 	__u32	oup_padding2;
-	char	oup_buf[0];
+	char	oup_buf[];
 } __attribute__((packed));
 
 /* object update */
@@ -3541,7 +3541,7 @@ struct object_update {
 	__u32		ou_padding1;		/* padding 1 */
 	__u64		ou_batchid;		/* op transno on master */
 	struct lu_fid	ou_fid;			/* object to be updated */
-	struct object_update_param ou_params[0]; /* update params */
+	struct object_update_param ou_params[]; /* update params */
 };
 
 #define	UPDATE_REQUEST_MAGIC_V1	0xBDDE0001
@@ -3552,7 +3552,7 @@ struct object_update_request {
 	__u32			ourq_magic;
 	__u16			ourq_count;	/* number of ourq_updates[] */
 	__u16			ourq_padding;
-	struct object_update	ourq_updates[0];
+	struct object_update	ourq_updates[];
 };
 
 #define OUT_UPDATE_HEADER_MAGIC		0xBDDF0001
@@ -3563,7 +3563,7 @@ struct out_update_header {
 	__u32		ouh_count;
 	__u32		ouh_inline_length;
 	__u32		ouh_reply_size;
-	__u32		ouh_inline_data[0];
+	__u32		ouh_inline_data[];
 };
 
 struct out_update_buffer {
@@ -3576,7 +3576,7 @@ struct object_update_result {
 	__u32   our_rc;
 	__u16   our_datalen;
 	__u16   our_padding;
-	__u32   our_data[0];
+	__u32   our_data[];
 };
 
 #define UPDATE_REPLY_MAGIC_V1	0x00BD0001
@@ -3587,7 +3587,7 @@ struct object_update_reply {
 	__u32	ourp_magic;
 	__u16	ourp_count;
 	__u16	ourp_padding;
-	__u16	ourp_lens[0];
+	__u16	ourp_lens[];
 };
 
 /* read update result */
@@ -3595,7 +3595,7 @@ struct out_read_reply {
 	__u32	orr_size;
 	__u32	orr_padding;
 	__u64	orr_offset;
-	char	orr_data[0];
+	char	orr_data[];
 };
 
 #define BUT_REQUEST_MAGIC	0xBADE0001

commit 0d3a9607655adc8f9dd4ae1c341bde0b57fe88bf

LU-17504 libcfs: safer LIBCFS_ALLOC
Make the LIBCFS_ALLOC() family of macros safer by adding
parenthesis around arguments such as (size) to avoid uninteded
expansion.

CoverityID: 415056 ("Integer handling issues")

Fixes: 718e3f3e68 ("LU-17504 build: fix gcc-13 [-Werror=stringop-overread] error")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I9701f87025bc5ce038a6bf34413b64a3f019d998
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55015
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/include/libcfs/libcfs_private.h
@@ -128,12 +128,12 @@ extern atomic64_t libcfs_kmem;
 
 # define libcfs_kmem_inc(ptr, size)		\
 do {						\
-	atomic64_add(size, &libcfs_kmem);	\
+	atomic64_add((size), &libcfs_kmem);	\
 } while (0)
 
 # define libcfs_kmem_dec(ptr, size)		\
 do {						\
-	atomic64_sub(size, &libcfs_kmem);	\
+	atomic64_sub((size), &libcfs_kmem);	\
 } while (0)
 
 # define libcfs_kmem_read()			\
@@ -169,13 +169,13 @@ do {									      \
 		       libcfs_kmem_read());				      \
 	} else {							      \
 		libcfs_kmem_inc((ptr), (size));				      \
-		LIBCFS_MEM_MSG(ptr, size, name);			      \
+		LIBCFS_MEM_MSG(ptr, (size), name);			      \
 	}                                                                     \
 } while (0)
 
 #define LIBCFS_FREE_PRE(ptr, size, name)				\
 	libcfs_kmem_dec((ptr), (size));					\
-	LIBCFS_MEM_MSG(ptr, size, name)
+	LIBCFS_MEM_MSG(ptr, (size), name)
 
 /**
  * allocate memory with GFP flags @mask
@@ -193,13 +193,13 @@ do {									    \
  * default allocator
  */
 #define LIBCFS_ALLOC(ptr, size) \
-	LIBCFS_ALLOC_GFP(ptr, size, GFP_NOFS)
+	LIBCFS_ALLOC_GFP(ptr, (size), GFP_NOFS)
 
 /**
  * non-sleeping allocator
  */
 #define LIBCFS_ALLOC_ATOMIC(ptr, size) \
-	LIBCFS_ALLOC_GFP(ptr, size, GFP_ATOMIC)
+	LIBCFS_ALLOC_GFP(ptr, (size), GFP_ATOMIC)
 
 /**
  * allocate memory for specified CPU partition
@@ -218,7 +218,7 @@ do {									    \
 
 /** default numa allocator */
 #define LIBCFS_CPT_ALLOC(ptr, cptab, cpt, size)				    \
-	LIBCFS_CPT_ALLOC_GFP(ptr, cptab, cpt, size, GFP_NOFS)
+	LIBCFS_CPT_ALLOC_GFP(ptr, (cptab), (cpt), (size), GFP_NOFS)
 
 #define LIBCFS_FREE(ptr, size)						\
 do {									\
@@ -228,7 +228,7 @@ do {									\
 		       "%s:%d\n", s, __FILE__, __LINE__);		\
 		break;							\
 	}								\
-	LIBCFS_FREE_PRE(ptr, size, "kfreed");				\
+	LIBCFS_FREE_PRE(ptr, (size), "kfreed");				\
 	if (unlikely(s > LIBCFS_VMALLOC_SIZE))				\
 		libcfs_vfree_atomic(ptr);				\
 	else								\

libcfs/libcfs/hash.c
@@ -1096,7 +1096,7 @@ cfs_hash_create(char *name, unsigned cur_bits, unsigned max_bits,
         if (hs->hs_buckets != NULL)
                 return hs;
 
-	LIBCFS_FREE(hs, offsetof(struct cfs_hash, hs_name[len]));
+	LIBCFS_FREE(hs, sizeof(struct cfs_hash) + len);
         RETURN(NULL);
 }
 EXPORT_SYMBOL(cfs_hash_create);
@@ -1162,7 +1162,7 @@ cfs_hash_destroy(struct kref *kref)
 			      0, CFS_HASH_NBKT(hs));
 	i = cfs_hash_with_bigname(hs) ?
 	    CFS_HASH_BIGNAME_LEN : CFS_HASH_NAME_LEN;
-	LIBCFS_FREE(hs, offsetof(struct cfs_hash, hs_name[i]));
+	LIBCFS_FREE(hs, sizeof(struct cfs_hash) + i);
 
 	EXIT;
 }