lustre/lustre-release

commit 4435d0121f72aac3ad01c98a33b265a496359890

LU-14139 statahead: batched statahead processing
Batched metadata processing can get a big performance boost.
In this patch, it implements a batched statahead mechanism which
can also increase the performance for a directory traverse or
listing such as the command 'ls'.

For the batched statahead, one batch getattr() RPC equals to
'N' normal lookup/getattr RPCs. It can pack a number of dentry
name getting from the readdir() call and prepared lock handles
one client side lock namespace into one large batched RPC
transfering via bulk I/O to obtain ibits DLM locks and
associated attributes for a lot of files in one blow.
When MDS receives a batched getattr() RPC, it executes the sub
requests in it one by one serially.

A tunable parameter named "statahead_batch_max" is defined, it
means the maximal items can be batched and processed within one
aggregate RPC. Once the number of sub requests exceeds this
predefined limit, it will pack and trigger the batched RPC.
The batched RPC will also be triggered explictly when the
readdir() call comes to the end position of the directory or
the statahead thread exits abnormally.

Batched metadata processing can get a big performance boost.
The mdtest performance results without/with this patch series are
as follow:
mdtest-easy-stat      720.562369 kIOPS : time 118.695 seconds
mdtest-easy-stat     1218.290192 kIOPS : time 70.656 seconds

In this patch, we set statahead_batch_max=0 and disabled batched
statahead by default. It will enable accordingly once some
subsequent fixes about batched RPC have been merged.

Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I5a80c2c377093dc8b8e21341f440e3038f017ca8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/40720
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/llite/statahead.c
@@ -142,6 +142,21 @@ static inline int sa_sent_full(struct ll_statahead_info *sai)
 	return atomic_read(&sai->sai_cache_count) >= sai->sai_max;
 }
 
+/* Batch metadata handle */
+static inline bool sa_has_batch_handle(struct ll_statahead_info *sai)
+{
+	return sai->sai_bh != NULL;
+}
+
+static inline void ll_statahead_flush_nowait(struct ll_statahead_info *sai)
+{
+	if (sa_has_batch_handle(sai)) {
+		sai->sai_index_end = sai->sai_index - 1;
+		(void) md_batch_flush(ll_i2mdexp(sai->sai_dentry->d_inode),
+				      sai->sai_bh, false);
+	}
+}
+
 static inline int agl_list_empty(struct ll_statahead_info *sai)
 {
 	return list_empty(&sai->sai_agls);
@@ -269,19 +284,35 @@ sa_kill(struct ll_statahead_info *sai, struct sa_entry *entry)
 
 /* called by scanner after use, sa_entry will be killed */
 static void
-sa_put(struct ll_statahead_info *sai, struct sa_entry *entry)
+sa_put(struct inode *dir, struct ll_statahead_info *sai, struct sa_entry *entry)
 {
+	struct ll_inode_info *lli = ll_i2info(dir);
 	struct sa_entry *tmp, *next;
+	bool wakeup = false;
 
 	if (entry && entry->se_state == SA_ENTRY_SUCC) {
 		struct ll_sb_info *sbi = ll_i2sbi(sai->sai_dentry->d_inode);
 
 		sai->sai_hit++;
 		sai->sai_consecutive_miss = 0;
-		sai->sai_max = min(2 * sai->sai_max, sbi->ll_sa_max);
+		if (sai->sai_max < sbi->ll_sa_max) {
+			sai->sai_max = min(2 * sai->sai_max, sbi->ll_sa_max);
+			wakeup = true;
+		} else if (sai->sai_max_batch_count > 0) {
+			if (sai->sai_max >= sai->sai_max_batch_count &&
+			   (sai->sai_index_end - entry->se_index) %
+			   sai->sai_max_batch_count == 0) {
+				wakeup = true;
+			} else if (entry->se_index == sai->sai_index_end) {
+				wakeup = true;
+			}
+		} else {
+			wakeup = true;
+		}
 	} else {
 		sai->sai_miss++;
 		sai->sai_consecutive_miss++;
+		wakeup = true;
 	}
 
 	if (entry)
@@ -296,6 +327,11 @@ sa_put(struct ll_statahead_info *sai, struct sa_entry *entry)
 			break;
 		sa_kill(sai, tmp);
 	}
+
+	spin_lock(&lli->lli_sa_lock);
+	if (wakeup && sai->sai_task)
+		wake_up_process(sai->sai_task);
+	spin_unlock(&lli->lli_sa_lock);
 }
 
 /*
@@ -339,6 +375,8 @@ static void sa_fini_data(struct md_op_item *item)
 		kfree(op_data->op_name);
 	ll_unlock_md_op_lsm(&item->mop_data);
 	iput(item->mop_dir);
+	if (item->mop_subpill_allocated)
+		OBD_FREE_PTR(item->mop_pill);
 	OBD_FREE_PTR(item);
 }
 
@@ -369,6 +407,7 @@ sa_prep_data(struct inode *dir, struct inode *child, struct sa_entry *entry)
 	if (!child)
 		op_data->op_fid2 = entry->se_fid;
 
+	item->mop_opc = MD_OP_GETATTR;
 	item->mop_it.it_op = IT_GETATTR;
 	item->mop_dir = igrab(dir);
 	item->mop_cb = ll_statahead_interpret;
@@ -672,8 +711,12 @@ static void ll_statahead_interpret_work(struct work_struct *work)
 		GOTO(out, rc = -EAGAIN);
 
 	rc = ll_prep_inode(&child, pill, dir->i_sb, it);
-	if (rc)
+	if (rc) {
+		CERROR("%s: getattr callback for %.*s "DFID": rc = %d\n",
+		       ll_i2sbi(dir)->ll_fsname, entry->se_qstr.len,
+		       entry->se_qstr.name, PFID(&entry->se_fid), rc);
 		GOTO(out, rc);
+	}
 
 	/* If encryption context was returned by MDT, put it in
 	 * inode now to save an extra getxattr.
@@ -796,6 +839,19 @@ out:
 	RETURN(rc);
 }
 
+static inline int sa_getattr(struct inode *dir, struct md_op_item *item)
+{
+	struct ll_statahead_info *sai = ll_i2info(dir)->lli_sai;
+	int rc;
+
+	if (sa_has_batch_handle(sai))
+		rc = md_batch_add(ll_i2mdexp(dir), sai->sai_bh, item);
+	else
+		rc = md_intent_getattr_async(ll_i2mdexp(dir), item);
+
+	return rc;
+}
+
 /* async stat for file not found in dcache */
 static int sa_lookup(struct inode *dir, struct sa_entry *entry)
 {
@@ -808,7 +864,7 @@ static int sa_lookup(struct inode *dir, struct sa_entry *entry)
 	if (IS_ERR(item))
 		RETURN(PTR_ERR(item));
 
-	rc = md_intent_getattr_async(ll_i2mdexp(dir), item);
+	rc = sa_getattr(dir, item);
 	if (rc < 0)
 		sa_fini_data(item);
 
@@ -853,7 +909,7 @@ static int sa_revalidate(struct inode *dir, struct sa_entry *entry,
 		RETURN(1);
 	}
 
-	rc = md_intent_getattr_async(ll_i2mdexp(dir), item);
+	rc = sa_getattr(dir, item);
 	if (rc < 0) {
 		entry->se_inode = NULL;
 		iput(inode);
@@ -898,6 +954,9 @@ static void sa_statahead(struct dentry *parent, const char *name, int len,
 		sai->sai_sent++;
 
 	sai->sai_index++;
+
+	if (sa_sent_full(sai))
+		ll_statahead_flush_nowait(sai);
 
 	EXIT;
 }
@@ -1015,6 +1074,7 @@ static int ll_statahead_thread(void *arg)
 	int first = 0;
 	struct md_op_data *op_data;
 	struct page *page = NULL;
+	struct lu_batch *bh = NULL;
 	__u64 pos = 0;
 	int rc = 0;
 
@@ -1023,6 +1083,15 @@ static int ll_statahead_thread(void *arg)
 	CDEBUG(D_READA, "statahead thread starting: sai %p, parent %pd\n",
 	       sai, parent);
 
+	sai->sai_max_batch_count = sbi->ll_sa_batch_max;
+	if (sai->sai_max_batch_count) {
+		bh = md_batch_create(ll_i2mdexp(dir), BATCH_FL_RDONLY,
+				     sai->sai_max_batch_count);
+		if (IS_ERR(bh))
+			GOTO(out_stop_agl, rc = PTR_ERR(bh));
+	}
+
+	sai->sai_bh = bh;
 	OBD_ALLOC_PTR(op_data);
 	if (!op_data)
 		GOTO(out, rc = -ENOMEM);
@@ -1183,6 +1252,8 @@ static int ll_statahead_thread(void *arg)
 		spin_unlock(&lli->lli_sa_lock);
 	}
 
+	ll_statahead_flush_nowait(sai);
+
 	/*
 	 * statahead is finished, but statahead entries need to be cached, wait
 	 * for file release closedir() call to stop me.
@@ -1196,6 +1267,12 @@ static int ll_statahead_thread(void *arg)
 
 	EXIT;
 out:
+	if (bh) {
+		rc = md_batch_stop(ll_i2mdexp(dir), sai->sai_bh);
+		sai->sai_bh = NULL;
+	}
+
+out_stop_agl:
 	ll_stop_agl(sai);
 
 	/*
@@ -1567,11 +1644,7 @@ out:
 	 */
 	if (lld_is_init(*dentryp))
 		ll_d2d(*dentryp)->lld_sa_generation = lli->lli_sa_generation;
-	sa_put(sai, entry);
-	spin_lock(&lli->lli_sa_lock);
-	if (sai->sai_task)
-		wake_up_process(sai->sai_task);
-	spin_unlock(&lli->lli_sa_lock);
+	sa_put(dir, sai, entry);
 
 	RETURN(rc);
 }

lustre/include/lustre_dlm.h
@@ -1428,6 +1428,8 @@ struct ldlm_callback_suite {
  */
 int ldlm_server_blocking_ast(struct ldlm_lock *, struct ldlm_lock_desc *,
 			     void *data, int flag);
+int tgt_blocking_ast(struct ldlm_lock *lock, struct ldlm_lock_desc *desc,
+		     void *data, int flag);
 int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data);
 int ldlm_server_glimpse_ast(struct ldlm_lock *lock, void *data);
 int ldlm_glimpse_locks(struct ldlm_resource *res,
@@ -1439,9 +1441,9 @@ int ldlm_glimpse_locks(struct ldlm_resource *res,
  * MDT or OST to pass through LDLM requests to LDLM for handling
  * @{
  */
-int ldlm_handle_enqueue0(struct ldlm_namespace *ns, struct ptlrpc_request *req,
-			 const struct ldlm_request *dlm_req,
-			 const struct ldlm_callback_suite *cbs);
+int ldlm_handle_enqueue(struct ldlm_namespace *ns, struct req_capsule *pill,
+			const struct ldlm_request *dlm_req,
+			const struct ldlm_callback_suite *cbs);
 int ldlm_handle_convert0(struct ptlrpc_request *req,
 			 const struct ldlm_request *dlm_req);
 int ldlm_handle_cancel(struct ptlrpc_request *req);
@@ -1732,10 +1734,10 @@ int ldlm_prep_elc_req(struct obd_export *exp, struct ptlrpc_request *req,
 		      struct list_head *cancels, int count);
 
 struct ptlrpc_request *ldlm_enqueue_pack(struct obd_export *exp, int lvb_len);
-int ldlm_handle_enqueue0(struct ldlm_namespace *ns, struct ptlrpc_request *req,
-			 const struct ldlm_request *dlm_req,
-			 const struct ldlm_callback_suite *cbs);
-int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
+int ldlm_handle_enqueue(struct ldlm_namespace *ns, struct req_capsule *pill,
+			const struct ldlm_request *dlm_req,
+			const struct ldlm_callback_suite *cbs);
+int ldlm_cli_enqueue_fini(struct obd_export *exp, struct req_capsule *pill,
 			  struct ldlm_enqueue_info *einfo, __u8 with_policy,
 			  __u64 *flags, void *lvb, __u32 lvb_len,
 			  const struct lustre_handle *lockh, int rc,
@@ -1751,6 +1753,14 @@ int ldlm_cli_enqueue_local(const struct lu_env *env,
 			   void *data, __u32 lvb_len, enum lvb_type lvb_type,
 			   const __u64 *client_cookie,
 			   struct lustre_handle *lockh);
+int ldlm_cli_lock_create_pack(struct obd_export *exp,
+			      struct ldlm_request *dlmreq,
+			      struct ldlm_enqueue_info *einfo,
+			      const struct ldlm_res_id *res_id,
+			      union ldlm_policy_data const *policy,
+			      __u64 *flags, void *lvb, __u32 lvb_len,
+			      enum lvb_type lvb_type,
+			      struct lustre_handle *lockh);
 int ldlm_cli_convert_req(struct ldlm_lock *lock, __u32 *flags, __u64 new_bits);
 int ldlm_cli_convert(struct ldlm_lock *lock,
 		     enum ldlm_cancel_flags cancel_flags);

lustre/include/lustre_req_layout.h
@@ -312,6 +312,7 @@ extern struct req_format RQF_LFSCK_NOTIFY;
 extern struct req_format RQF_LFSCK_QUERY;
 
 /* Batch UpdaTe req_format */
+extern struct req_format RQF_BUT_GETATTR;
 extern struct req_format RQF_MDS_BATCH;
 
 extern struct req_msg_field RMF_GENERIC_DATA;

lustre/include/obd.h
@@ -988,6 +988,8 @@ struct md_op_item {
 	struct inode			*mop_dir;
 	struct req_capsule		*mop_pill;
 	struct work_struct		 mop_work;
+	__u64				 mop_lock_flags;
+	unsigned int			 mop_subpill_allocated:1;
 };
 
 enum lu_batch_flags {

lustre/ldlm/ldlm_lockd.c
@@ -1166,6 +1166,7 @@ int ldlm_server_completion_ast(struct ldlm_lock *lock, __u64 flags, void *data)
 
 	RETURN(lvb_len < 0 ? lvb_len : rc);
 }
+EXPORT_SYMBOL(ldlm_server_completion_ast);
 
 /**
  * Server side ->l_glimpse_ast handler for client locks.
@@ -1277,10 +1278,10 @@ EXPORT_SYMBOL(ldlm_request_lock);
  * Main server-side entry point into LDLM for enqueue. This is called by ptlrpc
  * service threads to carry out client lock enqueueing requests.
  */
-int ldlm_handle_enqueue0(struct ldlm_namespace *ns,
-			 struct ptlrpc_request *req,
-			 const struct ldlm_request *dlm_req,
-			 const struct ldlm_callback_suite *cbs)
+int ldlm_handle_enqueue(struct ldlm_namespace *ns,
+			struct req_capsule *pill,
+			const struct ldlm_request *dlm_req,
+			const struct ldlm_callback_suite *cbs)
 {
 	struct ldlm_reply *dlm_rep;
 	__u64 flags;
@@ -1289,23 +1290,27 @@ int ldlm_handle_enqueue0(struct ldlm_namespace *ns,
 	void *cookie = NULL;
 	int rc = 0;
 	struct ldlm_resource *res = NULL;
+	struct ptlrpc_request *req = pill->rc_req;
 	const struct lu_env *env = req->rq_svc_thread->t_env;
 
 	ENTRY;
 
 	LDLM_DEBUG_NOLOCK("server-side enqueue handler START");
 
-	ldlm_request_cancel(req, dlm_req, LDLM_ENQUEUE_CANCEL_OFF, LATF_SKIP);
-	flags = ldlm_flags_from_wire(dlm_req->lock_flags);
+	LASSERT(req && req->rq_export);
 
-	LASSERT(req->rq_export);
+	if (req_capsule_ptlreq(pill))
+		ldlm_request_cancel(req, dlm_req, LDLM_ENQUEUE_CANCEL_OFF,
+				    LATF_SKIP);
+
+	flags = ldlm_flags_from_wire(dlm_req->lock_flags);
 
 	/* for intent enqueue the stat will be updated inside intent policy */
 	if (ptlrpc_req2svc(req)->srv_stats != NULL &&
 	    !(dlm_req->lock_flags & LDLM_FL_HAS_INTENT))
 		ldlm_svc_get_eopc(dlm_req, ptlrpc_req2svc(req)->srv_stats);
 
-	if (req->rq_export && req->rq_export->exp_nid_stats &&
+	if (req->rq_export->exp_nid_stats &&
 	    req->rq_export->exp_nid_stats->nid_ldlm_stats)
 		lprocfs_counter_incr(req->rq_export->exp_nid_stats->nid_ldlm_stats,
 				     LDLM_ENQUEUE - LDLM_FIRST_OPC);
@@ -1426,13 +1431,13 @@ existing_lock:
 		/* based on the assumption that lvb size never changes during
 		 * resource life time otherwise it need resource->lr_lock's
 		 * protection */
-		req_capsule_set_size(&req->rq_pill, &RMF_DLM_LVB,
+		req_capsule_set_size(pill, &RMF_DLM_LVB,
 				     RCL_SERVER, ldlm_lvbo_size(lock));
 
 		if (OBD_FAIL_CHECK(OBD_FAIL_LDLM_ENQUEUE_EXTENT_ERR))
 			GOTO(out, rc = -ENOMEM);
 
-		rc = req_capsule_server_pack(&req->rq_pill);
+		rc = req_capsule_server_pack(pill);
 		if (rc)
 			GOTO(out, rc);
 	}
@@ -1444,12 +1449,12 @@ existing_lock:
 		GOTO(out, err);
 	}
 
-	dlm_rep = req_capsule_server_get(&req->rq_pill, &RMF_DLM_REP);
+	dlm_rep = req_capsule_server_get(pill, &RMF_DLM_REP);
 
 	ldlm_lock2desc(lock, &dlm_rep->lock_desc);
 	ldlm_lock2handle(lock, &dlm_rep->lock_handle);
 
-	if (lock && lock->l_resource->lr_type == LDLM_EXTENT)
+	if (lock->l_resource->lr_type == LDLM_EXTENT)
 		OBD_FAIL_TIMEOUT(OBD_FAIL_LDLM_BL_EVICT, 6);
 
 	/*
@@ -1515,11 +1520,13 @@ existing_lock:
 
 	EXIT;
 out:
-	req->rq_status = rc ?: err; /* return either error - b=11190 */
-	if (!req->rq_packed_final) {
-		int rc1 = lustre_pack_reply(req, 1, NULL, NULL);
-		if (rc == 0)
-			rc = rc1;
+	if (req_capsule_ptlreq(pill)) {
+		req->rq_status = rc ?: err; /* return either error - b=11190 */
+		if (!req->rq_packed_final) {
+			int rc1 = lustre_pack_reply(req, 1, NULL, NULL);
+			if (rc == 0)
+				rc = rc1;
+		}
 	}
 
 	/*
@@ -1532,18 +1539,17 @@ out:
 			   err, rc);
 
 		if (rc == 0 &&
-		    req_capsule_has_field(&req->rq_pill, &RMF_DLM_LVB,
+		    req_capsule_has_field(pill, &RMF_DLM_LVB,
 					  RCL_SERVER) &&
 		    ldlm_lvbo_size(lock) > 0) {
 			void *buf;
 			int buflen;
 
 retry:
-			buf = req_capsule_server_get(&req->rq_pill,
-						     &RMF_DLM_LVB);
+			buf = req_capsule_server_get(pill, &RMF_DLM_LVB);
 			LASSERTF(buf != NULL, "req %p, lock %p\n", req, lock);
-			buflen = req_capsule_get_size(&req->rq_pill,
-					&RMF_DLM_LVB, RCL_SERVER);
+			buflen = req_capsule_get_size(pill, &RMF_DLM_LVB,
+						      RCL_SERVER);
 			/*
 			 * non-replayed lock, delayed lvb init may
 			 * need to be occur now
@@ -1553,13 +1559,12 @@ retry:
 
 				rc2 = ldlm_lvbo_fill(lock, buf, &buflen);
 				if (rc2 >= 0) {
-					req_capsule_shrink(&req->rq_pill,
-							   &RMF_DLM_LVB,
+					req_capsule_shrink(pill, &RMF_DLM_LVB,
 							   rc2, RCL_SERVER);
 				} else if (rc2 == -ERANGE) {
 					rc2 = req_capsule_server_grow(
-							&req->rq_pill,
-							&RMF_DLM_LVB, buflen);
+							pill, &RMF_DLM_LVB,
+							buflen);
 					if (!rc2) {
 						goto retry;
 					} else {
@@ -1569,8 +1574,7 @@ retry:
 						 * to client.
 						 */
 						req_capsule_shrink(
-							&req->rq_pill,
-							&RMF_DLM_LVB, 0,
+							pill, &RMF_DLM_LVB, 0,
 							RCL_SERVER);
 					}
 				} else {
@@ -1579,8 +1583,7 @@ retry:
 			} else if (flags & LDLM_FL_REPLAY) {
 				/* no LVB resend upon replay */
 				if (buflen > 0)
-					req_capsule_shrink(&req->rq_pill,
-							   &RMF_DLM_LVB,
+					req_capsule_shrink(pill, &RMF_DLM_LVB,
 							   0, RCL_SERVER);
 				else
 					rc = buflen;
@@ -1614,6 +1617,7 @@ retry:
 
 	return rc;
 }
+EXPORT_SYMBOL(ldlm_handle_enqueue);
 
 /*
  * Clear the blocking lock, the race is possible between ldlm_handle_convert0()

lustre/ldlm/ldlm_request.c
@@ -605,7 +605,7 @@ static bool ldlm_request_slot_needed(struct ldlm_enqueue_info *einfo)
  *
  * Called after receiving reply from server.
  */
-int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
+int ldlm_cli_enqueue_fini(struct obd_export *exp, struct req_capsule *pill,
 			  struct ldlm_enqueue_info *einfo,
 			  __u8 with_policy, __u64 *ldlm_flags, void *lvb,
 			  __u32 lvb_len, const struct lustre_handle *lockh,
@@ -620,13 +620,17 @@ int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
 
 	ENTRY;
 
-	if (request_slot)
-		obd_put_request_slot(&req->rq_import->imp_obd->u.cli);
+	if (req_capsule_ptlreq(pill)) {
+		struct ptlrpc_request *req = pill->rc_req;
 
-	ptlrpc_put_mod_rpc_slot(req);
+		if (request_slot)
+			obd_put_request_slot(&req->rq_import->imp_obd->u.cli);
 
-	if (req && req->rq_svc_thread)
-		env = req->rq_svc_thread->t_env;
+		ptlrpc_put_mod_rpc_slot(req);
+
+		if (req && req->rq_svc_thread)
+			env = req->rq_svc_thread->t_env;
+	}
 
 	lock = ldlm_handle2lock(lockh);
 	/* ldlm_cli_enqueue is holding a reference on this lock. */
@@ -648,15 +652,14 @@ int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
 	}
 
 	/* Before we return, swab the reply */
-	reply = req_capsule_server_get(&req->rq_pill, &RMF_DLM_REP);
+	reply = req_capsule_server_get(pill, &RMF_DLM_REP);
 	if (reply == NULL)
 		GOTO(cleanup, rc = -EPROTO);
 
 	if (lvb_len > 0) {
 		int size = 0;
 
-		size = req_capsule_get_size(&req->rq_pill, &RMF_DLM_LVB,
-					    RCL_SERVER);
+		size = req_capsule_get_size(pill, &RMF_DLM_LVB, RCL_SERVER);
 		if (size < 0) {
 			LDLM_ERROR(lock, "Fail to get lvb_len, rc = %d", size);
 			GOTO(cleanup, rc = size);
@@ -671,7 +674,7 @@ int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
 
 	if (rc == ELDLM_LOCK_ABORTED) {
 		if (lvb_len > 0 && lvb != NULL)
-			rc = ldlm_fill_lvb(lock, &req->rq_pill, RCL_SERVER,
+			rc = ldlm_fill_lvb(lock, pill, RCL_SERVER,
 					   lvb, lvb_len);
 		GOTO(cleanup, rc = rc ? : ELDLM_LOCK_ABORTED);
 	}
@@ -766,7 +769,7 @@ int ldlm_cli_enqueue_fini(struct obd_export *exp, struct ptlrpc_request *req,
 		 */
 		lock_res_and_lock(lock);
 		if (!ldlm_is_granted(lock))
-			rc = ldlm_fill_lvb(lock, &req->rq_pill, RCL_SERVER,
+			rc = ldlm_fill_lvb(lock, pill, RCL_SERVER,
 					   lock->l_lvb_data, lvb_len);
 		unlock_res_and_lock(lock);
 		if (rc < 0) {
@@ -1117,8 +1120,9 @@ int ldlm_cli_enqueue(struct obd_export *exp, struct ptlrpc_request **reqp,
 
 	rc = ptlrpc_queue_wait(req);
 
-	err = ldlm_cli_enqueue_fini(exp, req, einfo, policy ? 1 : 0, flags,
-				    lvb, lvb_len, lockh, rc, need_req_slot);
+	err = ldlm_cli_enqueue_fini(exp, &req->rq_pill, einfo, policy ? 1 : 0,
+				    flags, lvb, lvb_len, lockh, rc,
+				    need_req_slot);
 
 	/*
 	 * If ldlm_cli_enqueue_fini did not find the lock, we need to free
@@ -1139,6 +1143,62 @@ out:
 	RETURN(rc);
 }
 EXPORT_SYMBOL(ldlm_cli_enqueue);
+
+/**
+ * Client-side IBITS lock create and pack for WBC EX lock request.
+ */
+int ldlm_cli_lock_create_pack(struct obd_export *exp,
+			      struct ldlm_request *dlmreq,
+			      struct ldlm_enqueue_info *einfo,
+			      const struct ldlm_res_id *res_id,
+			      union ldlm_policy_data const *policy,
+			      __u64 *flags, void *lvb, __u32 lvb_len,
+			      enum lvb_type lvb_type,
+			      struct lustre_handle *lockh)
+{
+	const struct ldlm_callback_suite cbs = {
+		.lcs_completion	= einfo->ei_cb_cp,
+		.lcs_blocking	= einfo->ei_cb_bl,
+		.lcs_glimpse	= einfo->ei_cb_gl
+	};
+	struct ldlm_namespace *ns;
+	struct ldlm_lock *lock;
+
+	ENTRY;
+
+	LASSERT(exp != NULL);
+	LASSERT(!(*flags & LDLM_FL_REPLAY));
+
+	ns = exp->exp_obd->obd_namespace;
+	lock = ldlm_lock_create(ns, res_id, einfo->ei_type, einfo->ei_mode,
+				&cbs, einfo->ei_cbdata, lvb_len, lvb_type);
+	if (IS_ERR(lock))
+		RETURN(PTR_ERR(lock));
+
+	if (einfo->ei_cb_created)
+		einfo->ei_cb_created(lock);
+
+	/* For the local lock, add the reference */
+	ldlm_lock_addref_internal(lock, einfo->ei_mode);
+	ldlm_lock2handle(lock, lockh);
+	if (policy != NULL)
+		lock->l_policy_data = *policy;
+
+	LDLM_DEBUG(lock, "client-side enqueue START, flags %#llx", *flags);
+	lock->l_conn_export = exp;
+	lock->l_export = NULL;
+	lock->l_blocking_ast = einfo->ei_cb_bl;
+	lock->l_flags |= (*flags & (LDLM_FL_NO_LRU | LDLM_FL_EXCL |
+				    LDLM_FL_ATOMIC_CB));
+	lock->l_activity = ktime_get_real_seconds();
+
+	ldlm_lock2desc(lock, &dlmreq->lock_desc);
+	dlmreq->lock_flags = ldlm_flags_to_wire(*flags);
+	dlmreq->lock_handle[0] = *lockh;
+
+	RETURN(0);
+}
+EXPORT_SYMBOL(ldlm_cli_lock_create_pack);
 
 /**
  * Client-side IBITS lock convert.

lustre/llite/llite_internal.h
@@ -799,6 +799,8 @@ struct ll_sb_info {
 	/* metadata stat-ahead */
 	unsigned int		  ll_sa_running_max;/* max concurrent
 						     * statahead instances */
+	unsigned int		  ll_sa_batch_max;/* max SUB request count in
+						   * a batch PTLRPC request */
 	unsigned int		  ll_sa_max;     /* max statahead RPCs */
 	atomic_t		  ll_sa_total;   /* statahead thread started
 						  * count */
@@ -1541,9 +1543,9 @@ void ll_ra_stats_inc(struct inode *inode, enum ra_stat which);
 
 /* statahead.c */
 
-#define LL_SA_RPC_MIN           2
+#define LL_SA_RPC_MIN           8
 #define LL_SA_RPC_DEF           32
-#define LL_SA_RPC_MAX           512
+#define LL_SA_RPC_MAX           2048
 
 /* XXX: If want to support more concurrent statahead instances,
  *	please consider to decentralize the RPC lists attached
@@ -1551,6 +1553,9 @@ void ll_ra_stats_inc(struct inode *inode, enum ra_stat which);
  *	LU-11079 */
 #define LL_SA_RUNNING_MAX	256
 #define LL_SA_RUNNING_DEF	16
+
+#define LL_SA_BATCH_MAX		1024
+#define LL_SA_BATCH_DEF		0
 
 #define LL_SA_CACHE_BIT         5
 #define LL_SA_CACHE_SIZE        (1 << LL_SA_CACHE_BIT)
@@ -1592,6 +1597,9 @@ struct ll_statahead_info {
 	struct list_head	sai_cache[LL_SA_CACHE_SIZE];
 	spinlock_t		sai_cache_lock[LL_SA_CACHE_SIZE];
 	atomic_t		sai_cache_count; /* entry count in cache */
+	struct lu_batch		*sai_bh;
+	__u32			sai_max_batch_count;
+	__u64			sai_index_end;
 };
 
 int ll_revalidate_statahead(struct inode *dir, struct dentry **dentry,

lustre/llite/llite_lib.c
@@ -177,6 +177,7 @@ static struct ll_sb_info *ll_init_sbi(struct lustre_sb_info *lsi)
 
 	/* metadata statahead is enabled by default */
 	sbi->ll_sa_running_max = LL_SA_RUNNING_DEF;
+	sbi->ll_sa_batch_max = LL_SA_BATCH_DEF;
 	sbi->ll_sa_max = LL_SA_RPC_DEF;
 	atomic_set(&sbi->ll_sa_total, 0);
 	atomic_set(&sbi->ll_sa_wrong, 0);
@@ -347,7 +348,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 				   OBD_CONNECT2_GETATTR_PFID |
 				   OBD_CONNECT2_DOM_LVB |
 				   OBD_CONNECT2_REP_MBITS |
-				   OBD_CONNECT2_ATOMIC_OPEN_LOCK;
+				   OBD_CONNECT2_ATOMIC_OPEN_LOCK |
+				   OBD_CONNECT2_BATCH_RPC;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
 	if (test_bit(LL_SBI_LRU_RESIZE, sbi->ll_flags))

commit 628d1e25ea891470487ef32054069ba997602411

LU-17783 statahead: disable batch statahead for old server
Disable the batch statahead for the old server that does not
support MDS_BATCH batch RPC.

Fixes: 4435d0121f ("LU-14139 statahead: batched statahead processing")
Test-Parameters: testlist=sanity serverjob=lustre-b_es6_0 serverbuildno=638 clientdistro=el9.3 serverdistro=el8.8 env=ONLY=123
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I79fba4204e0ed44e2bc9a4c4f2758d087f0e406b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55017
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>

lustre/include/lustre_export.h
@@ -525,6 +525,11 @@ static inline bool exp_connect_unaligned_dio(struct obd_export *exp)
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_UNALIGNED_DIO);
 }
 
+static inline bool exp_connect_batch_rpc(struct obd_export *exp)
+{
+	return (exp_connect_flags2(exp) & OBD_CONNECT2_BATCH_RPC);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/llite/statahead.c
@@ -1533,7 +1533,11 @@ static int ll_statahead_thread(void *arg)
 	CDEBUG(D_READA, "statahead thread starting: sai %p, parent %pd\n",
 	       sai, parent);
 
-	sai->sai_max_batch_count = sbi->ll_sa_batch_max;
+	if (exp_connect_batch_rpc(sbi->ll_md_exp))
+		sai->sai_max_batch_count = sbi->ll_sa_batch_max;
+	else
+		sai->sai_max_batch_count = 0;
+
 	if (sai->sai_max_batch_count) {
 		bh = md_batch_create(ll_i2mdexp(dir), BATCH_FL_RDONLY,
 				     sai->sai_max_batch_count);