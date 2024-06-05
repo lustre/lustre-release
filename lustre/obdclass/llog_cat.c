lustre/lustre-release

commit 1ebc9ed460922e6fcf2432f70f2f6412974e963a

LU-15902 obdclass: dt_try_as_dir() check dir exists
If an object is not directory, but dt_lookup() is called on it, it
may crash because .do_lookup is NULL for non-directory file.

Add argument to check object existence and type in dt_try_as_dir(),
and for object to create, skip this check.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I51df0cbb5a4e7abca370ee27dac678f995b76159
Reviewed-on: https://review.whamcloud.com/47483
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/llog_osd.c
@@ -1247,7 +1247,7 @@ static struct dt_object *llog_osd_dir_get(const struct lu_env *env,
 			return ERR_PTR(rc);
 		dir = dt_locate(env, dt, &dti->dti_fid);
 
-		if (!IS_ERR(dir) && !dt_try_as_dir(env, dir)) {
+		if (!IS_ERR(dir) && !dt_try_as_dir(env, dir, true)) {
 			dt_object_put(env, dir);
 			return ERR_PTR(-ENOTDIR);
 		}
@@ -1454,7 +1454,7 @@ struct dt_object *llog_osd_get_regular_fid_dir(const struct lu_env *env,
 	if (IS_ERR(dir))
 		RETURN(dir);
 
-	if (!dt_try_as_dir(env, dir)) {
+	if (!dt_try_as_dir(env, dir, true)) {
 		dt_object_put(env, dir);
 		RETURN(ERR_PTR(-ENOTDIR));
 	}

lustre/include/dt_object.h
@@ -2055,7 +2055,7 @@ int dt_txn_hook_start(const struct lu_env *env,
                       struct dt_device *dev, struct thandle *txn);
 int dt_txn_hook_stop(const struct lu_env *env, struct thandle *txn);
 
-int dt_try_as_dir(const struct lu_env *env, struct dt_object *obj);
+int dt_try_as_dir(const struct lu_env *env, struct dt_object *obj, bool check);
 
 /**
  * Callback function used for parsing path.

lustre/lfsck/lfsck_bookmark.c
@@ -166,7 +166,7 @@ int lfsck_bookmark_setup(const struct lu_env *env,
 	if (IS_ERR(root))
 		RETURN(PTR_ERR(root));
 
-	if (unlikely(!dt_try_as_dir(env, root))) {
+	if (unlikely(!dt_try_as_dir(env, root, true))) {
 		lfsck_object_put(env, root);
 
 		RETURN(-ENOTDIR);

lustre/lfsck/lfsck_engine.c
@@ -350,7 +350,7 @@ int lfsck_open_dir(const struct lu_env *env,
 	LASSERT(obj != NULL);
 	LASSERT(di == NULL);
 
-	if (unlikely(!dt_try_as_dir(env, obj)))
+	if (unlikely(!dt_try_as_dir(env, obj, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	list_for_each_entry(com, &lfsck->li_list_dir, lc_link_dir) {

lustre/lfsck/lfsck_layout.c
@@ -6978,7 +6978,7 @@ int lfsck_layout_setup(const struct lu_env *env, struct lfsck_instance *lfsck)
 	if (IS_ERR(root))
 		GOTO(out, rc = PTR_ERR(root));
 
-	if (unlikely(!dt_try_as_dir(env, root)))
+	if (unlikely(!dt_try_as_dir(env, root, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	obj = local_file_find_or_create(env, lfsck->li_los, root,

lustre/lfsck/lfsck_lib.c
@@ -697,7 +697,7 @@ static int lfsck_create_lpf_local(const struct lu_env *env,
 	if (rc != 0)
 		GOTO(stop, rc);
 
-	if (!dt_try_as_dir(env, child))
+	if (!dt_try_as_dir(env, child, false))
 		GOTO(stop, rc = -ENOTDIR);
 
 	/* 2a. increase child nlink */
@@ -875,7 +875,7 @@ static int lfsck_create_lpf_remote(const struct lu_env *env,
 	if (rc != 0)
 		GOTO(stop, rc);
 
-	if (!dt_try_as_dir(env, child))
+	if (!dt_try_as_dir(env, child, false))
 		GOTO(stop, rc = -ENOTDIR);
 
 	/* 2a. increase child nlink */
@@ -1077,8 +1077,8 @@ static int lfsck_create_lpf(const struct lu_env *env,
 	if (IS_ERR(child))
 		GOTO(unlock, rc = PTR_ERR(child));
 
-	if (dt_object_exists(child) != 0) {
-		if (unlikely(!dt_try_as_dir(env, child)))
+	if (dt_object_exists(child)) {
+		if (unlikely(!dt_try_as_dir(env, child, true)))
 			rc = -ENOTDIR;
 		else
 			lfsck->li_lpf_obj = child;
@@ -1293,13 +1293,7 @@ static int lfsck_verify_lpf_pairs(const struct lu_env *env,
 	if (IS_ERR(parent2))
 		GOTO(linkea, parent2);
 
-	if (!dt_object_exists(parent2)) {
-		lfsck_object_put(env, parent2);
-
-		GOTO(linkea, parent2 = ERR_PTR(-ENOENT));
-	}
-
-	if (!dt_try_as_dir(env, parent2)) {
+	if (!dt_try_as_dir(env, parent2, true)) {
 		lfsck_object_put(env, parent2);
 
 		GOTO(linkea, parent2 = ERR_PTR(-ENOTDIR));
@@ -1450,7 +1444,7 @@ int lfsck_verify_lpf(const struct lu_env *env, struct lfsck_instance *lfsck)
 
 	LASSERT(dt_object_exists(parent));
 
-	if (unlikely(!dt_try_as_dir(env, parent))) {
+	if (unlikely(!dt_try_as_dir(env, parent, true))) {
 		lfsck_object_put(env, parent);
 
 		GOTO(put, rc = -ENOTDIR);
@@ -1499,7 +1493,7 @@ int lfsck_verify_lpf(const struct lu_env *env, struct lfsck_instance *lfsck)
 		goto find_child1;
 	}
 
-	if (unlikely(!dt_try_as_dir(env, child2)))
+	if (unlikely(!dt_try_as_dir(env, child2, true)))
 		GOTO(put, rc = -ENOTDIR);
 
 find_child1:
@@ -1562,7 +1556,7 @@ find_child1:
 		goto check_child2;
 	}
 
-	if (unlikely(!dt_try_as_dir(env, child1))) {
+	if (unlikely(!dt_try_as_dir(env, child1, true))) {
 		lfsck_object_put(env, child1);
 		child1 = NULL;
 		rc = -ENOTDIR;
@@ -1583,7 +1577,7 @@ check_child2:
 
 put:
 	if (lfsck->li_lpf_obj != NULL) {
-		if (unlikely(!dt_try_as_dir(env, lfsck->li_lpf_obj))) {
+		if (unlikely(!dt_try_as_dir(env, lfsck->li_lpf_obj, true))) {
 			lfsck_object_put(env, lfsck->li_lpf_obj);
 			lfsck->li_lpf_obj = NULL;
 			rc = -ENOTDIR;
@@ -2021,7 +2015,7 @@ lfsck_assistant_object_load(const struct lu_env *env,
 		return ERR_PTR(-ENOENT);
 	}
 
-	if (lso->lso_is_dir && unlikely(!dt_try_as_dir(env, obj))) {
+	if (lso->lso_is_dir && unlikely(!dt_try_as_dir(env, obj, true))) {
 		lfsck_object_put(env, obj);
 
 		return ERR_PTR(-ENOTDIR);
@@ -3737,9 +3731,6 @@ int lfsck_register(const struct lu_env *env, struct dt_device *key,
 						 &lfsck->li_global_root_fid);
 			if (rc != 0)
 				GOTO(out, rc);
-
-			if (unlikely(!dt_try_as_dir(env, obj)))
-				GOTO(out, rc = -ENOTDIR);
 
 			*pfid = *fid;
 			rc = dt_lookup_dir(env, obj, lostfound, fid);

lustre/lfsck/lfsck_namespace.c
@@ -1317,7 +1317,7 @@ static int lfsck_namespace_insert_normal(const struct lu_env *env,
 		}
 	}
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(unlock, rc = -ENOTDIR);
 
 	th = lfsck_trans_create(env, dev, lfsck);
@@ -1480,7 +1480,7 @@ static int lfsck_namespace_create_orphan_dir(const struct lu_env *env,
 		if (IS_ERR(parent))
 			GOTO(log, rc = PTR_ERR(parent));
 
-		if (unlikely(!dt_try_as_dir(env, parent)))
+		if (unlikely(!dt_try_as_dir(env, parent, true)))
 			GOTO(log, rc = -ENOTDIR);
 	} else {
 		if (unlikely(lfsck->li_lpf_obj == NULL))
@@ -1552,7 +1552,7 @@ again:
 	if (rc != 0)
 		GOTO(stop, rc);
 
-	if (unlikely(!dt_try_as_dir(env, orphan)))
+	if (unlikely(!dt_try_as_dir(env, orphan, false)))
 		GOTO(stop, rc = -ENOTDIR);
 
 	rc = dt_declare_ref_add(env, orphan, th);
@@ -1954,7 +1954,7 @@ static int lfsck_namespace_replace_cond(const struct lu_env *env,
 	if (IS_ERR(parent))
 		GOTO(log, rc = PTR_ERR(parent));
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(log, rc = -ENOTDIR);
 
 	rc = lfsck_lock(env, lfsck, parent, name, pllh,
@@ -2226,7 +2226,7 @@ int lfsck_namespace_repair_dirent(const struct lu_env *env,
 	if (IS_ERR(parent))
 		GOTO(log, rc = PTR_ERR(parent));
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(log, rc = -ENOTDIR);
 
 	if (!update || strcmp(name, name2) == 0)
@@ -2657,7 +2657,7 @@ lost_parent:
 	} /* !dt_object_exists(parent) */
 
 	/* The unique linkEA entry with bad parent will be handled as orphan. */
-	if (unlikely(!dt_try_as_dir(env, parent))) {
+	if (unlikely(!dt_try_as_dir(env, parent, true))) {
 		if (!lustre_handle_is_used(lh) && retry != NULL)
 			*retry = true;
 		else
@@ -2913,7 +2913,7 @@ again:
 		}
 
 		/* The linkEA entry with bad parent will be removed. */
-		if (unlikely(!dt_try_as_dir(env, parent))) {
+		if (unlikely(!dt_try_as_dir(env, parent, true))) {
 			lfsck_object_put(env, parent);
 			lfsck_linkea_del_buf(ldata, cname);
 			continue;
@@ -3256,7 +3256,7 @@ static int lfsck_namespace_double_scan_dir(const struct lu_env *env,
 		RETURN(0);
 	}
 
-	if (unlikely(!dt_try_as_dir(env, child)))
+	if (unlikely(!dt_try_as_dir(env, child, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	/* We only take ldlm lock on the @child when required. When the
@@ -3861,7 +3861,7 @@ lost_parent:
 		} /* !dt_object_exists(parent) */
 
 		/* The linkEA entry with bad parent will be removed. */
-		if (unlikely(!dt_try_as_dir(env, parent))) {
+		if (unlikely(!dt_try_as_dir(env, parent, true))) {
 			lfsck_object_put(env, parent);
 			rc = lfsck_namespace_shrink_linkea(env, com, child,
 						&ldata, cname, pfid, true);
@@ -4237,7 +4237,7 @@ static int lfsck_namespace_reset(const struct lu_env *env,
 	if (IS_ERR(root))
 		GOTO(log, rc = PTR_ERR(root));
 
-	if (unlikely(!dt_try_as_dir(env, root)))
+	if (unlikely(!dt_try_as_dir(env, root, true)))
 		GOTO(put, rc = -ENOTDIR);
 
 	down_write(&com->lc_sem);
@@ -5323,7 +5323,7 @@ int lfsck_namespace_repair_dangling(const struct lu_env *env,
 	if (IS_ERR(parent))
 		GOTO(log, rc = PTR_ERR(parent));
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(log, rc = -ENOTDIR);
 
 	child = lfsck_object_locate(dev, child);
@@ -5395,7 +5395,7 @@ int lfsck_namespace_repair_dangling(const struct lu_env *env,
 		GOTO(stop, rc);
 
 	if (S_ISDIR(type)) {
-		if (unlikely(!dt_try_as_dir(env, child)))
+		if (unlikely(!dt_try_as_dir(env, child, false)))
 			GOTO(stop, rc = -ENOTDIR);
 
 		/* 2a. increase child nlink */
@@ -6287,7 +6287,7 @@ static void lfsck_namespace_scan_local_lpf(const struct lu_env *env,
 	if (!dt_object_exists(parent))
 		GOTO(out, rc = 0);
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	CDEBUG(D_LFSCK, "%s: start to scan backend /lost+found\n",
@@ -7122,7 +7122,7 @@ int lfsck_namespace_setup(const struct lu_env *env,
 	if (IS_ERR(root))
 		GOTO(out, rc = PTR_ERR(root));
 
-	if (unlikely(!dt_try_as_dir(env, root)))
+	if (unlikely(!dt_try_as_dir(env, root, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	obj = local_index_find_or_create(env, lfsck->li_los, root,

lustre/lfsck/lfsck_striped_dir.c
@@ -1171,7 +1171,7 @@ static int lfsck_allow_regenerate_master_lmv(const struct lu_env *env,
 	__u16				 type;
 	ENTRY;
 
-	if (unlikely(!dt_try_as_dir(env, obj)))
+	if (unlikely(!dt_try_as_dir(env, obj, true)))
 		RETURN(-ENOTDIR);
 
 	/* Check whether the shard and the master MDT-object matches or not. */
@@ -1637,7 +1637,7 @@ int lfsck_namespace_scan_shard(const struct lu_env *env,
 	if (lmv->lmv_magic != LMV_MAGIC_STRIPE)
 		RETURN(1);
 
-	if (unlikely(!dt_try_as_dir(env, child)))
+	if (unlikely(!dt_try_as_dir(env, child, true)))
 		RETURN(-ENOTDIR);
 
 	OBD_ALLOC_PTR(llmv);
@@ -1782,7 +1782,7 @@ int lfsck_namespace_verify_stripe_slave(const struct lu_env *env,
 	if (unlikely(!dt_object_exists(parent)))
 		GOTO(out, rc = 1);
 
-	if (unlikely(!dt_try_as_dir(env, parent)))
+	if (unlikely(!dt_try_as_dir(env, parent, true)))
 		GOTO(out, rc = -ENOTDIR);
 
 	rc = lfsck_read_stripe_lmv(env, lfsck, parent, plmv);

commit bd9839f7dbdf59751e7cdc234602eb338c518104

LU-15644 llog: don't replace llog error with -ENOTDIR
The dt_try_as_dir() contains check for object existence
which is reported as -ENOTDIR after all. In case of llog
that goes to upper level and cause error reporting to
console. It is not relevant neither by error code nor by
debug level

Patch skips check for object existence in case of llog,
it is excessive anyway.
Debug level is reduced as well to don't spawn console
messages in case of -ENOENT, -ESTALE or -EIO errors

Fixes: 1ebc9ed460 ("LU-15902 obdclass: dt_try_as_dir() check dir exists")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Id404204566898a6ac2e258b7824491effc5fc92e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55151
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/llog_cat.c
@@ -725,17 +725,16 @@ int llog_cat_cancel_arr_rec(const struct lu_env *env,
 		cat_index = loghandle->u.phd.phd_cookie.lgc_index;
 		rc = llog_cat_cleanup(env, cathandle, loghandle, cat_index);
 		if (rc)
-			CERROR("%s: fail to cancel catalog record: rc = %d\n",
+			CDEBUG(D_HA,
+			       "%s: fail to cancel catalog record: rc = %d\n",
 			       loghandle2name(cathandle), rc);
 		rc = 0;
 
 	}
 	llog_handle_put(env, loghandle);
-
-	if (rc)
-		CERROR("%s: fail to cancel %d llog-records: rc = %d\n",
-		       loghandle2name(cathandle), count, rc);
-
+	if (rc != -ENOENT && rc != -ESTALE && rc != -EIO)
+		CWARN("%s: fail to cancel %d records in "DFID": rc = %d\n",
+		      loghandle2name(cathandle), count, PLOGID(lgl), rc);
 	RETURN(rc);
 }
 EXPORT_SYMBOL(llog_cat_cancel_arr_rec);
@@ -753,7 +752,7 @@ int llog_cat_cancel_records(const struct lu_env *env,
 			    struct llog_handle *cathandle, int count,
 			    struct llog_cookie *cookies)
 {
-	int i, rc = 0, failed = 0;
+	int i, rc = 0;
 
 	ENTRY;
 
@@ -761,16 +760,11 @@ int llog_cat_cancel_records(const struct lu_env *env,
 		int lrc;
 
 		lrc = llog_cat_cancel_arr_rec(env, cathandle, &cookies->lgc_lgl,
-					     1, &cookies->lgc_index);
-		if (lrc) {
-			failed++;
-			if (!rc)
-				rc = lrc;
-		}
+					      1, &cookies->lgc_index);
+		if (lrc && !rc)
+			rc = lrc;
 	}
-	if (failed)
-		CERROR("%s: fail to cancel %d of %d llog-records: rc = %d\n",
-		       loghandle2name(cathandle), failed, count, rc);
+
 	RETURN(rc);
 }
 EXPORT_SYMBOL(llog_cat_cancel_records);

lustre/obdclass/llog_osd.c
@@ -1235,7 +1235,7 @@ static struct dt_object *llog_osd_dir_get(const struct lu_env *env,
 			return ERR_PTR(rc);
 		dir = dt_locate(env, dt, &dti->dti_fid);
 
-		if (!IS_ERR(dir) && !dt_try_as_dir(env, dir, true)) {
+		if (!IS_ERR(dir) && !dt_try_as_dir(env, dir, false)) {
 			dt_object_put(env, dir);
 			return ERR_PTR(-ENOTDIR);
 		}
@@ -1442,7 +1442,7 @@ static struct dt_object *llog_osd_get_regular_fid_dir(const struct lu_env *env,
 	if (IS_ERR(dir))
 		RETURN(dir);
 
-	if (!dt_try_as_dir(env, dir, true)) {
+	if (!dt_try_as_dir(env, dir, false)) {
 		dt_object_put(env, dir);
 		RETURN(ERR_PTR(-ENOTDIR));
 	}

lustre/osp/osp_sync.c
@@ -1077,13 +1077,9 @@ static void osp_sync_process_committed(const struct lu_env *env,
 		    ((i * sizeof(int)) == arr_size ||
 		     (list_empty(&list) && i > 0))) {
 			rc = llog_cat_cancel_arr_rec(env, llh, &lgid, i, arr);
-
-			if (rc)
-				CERROR("%s: can't cancel %d records: rc = %d\n",
-				       obd->obd_name, i, rc);
-			else
-				CDEBUG(D_OTHER, "%s: massive records cancel id "DFID" num %d\n",
-				       obd->obd_name, PLOGID(&lgid), i);
+			CDEBUG(D_OTHER,
+			       "%s: cancel %d records in "DFID": rc = %d\n",
+			       obd->obd_name, i, PLOGID(&lgid), rc);
 			i = 0;
 		}
 