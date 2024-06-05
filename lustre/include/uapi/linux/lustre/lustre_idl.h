lustre/lustre-release

commit 67f90e42889ff22d574e82cc647f6076e48c65a5

LU-17034 quota: lqeg_arr memmory corruption
Fix memory corruption caused by accessing memory
out of array lqeg_arr. It could happen when at least
one of OSTs has index larger than the whole number
of OSTs. For example, if the system has 4 OSTs with
indexes 0001, 0002, 00c9, 00ca. This issue more often
corrupted bucket_table in obd_uuid_hash or obd_nid_hash
causing to crash rhashtable code. However, it could
be the reason of other panics depending on the type
of corrupted neighbour memory region.

This patch adds an lge_idx field to each lqe global entry
to store index of the OST. It is needed to map OST index
to the array index to avoid out-of-bound array access.

This patch also add locking to protect lqe_glbl_data in
qmt_set_revoke and qmt_clear_lgeg_arr_nu. This was
forgotten in 50ff4d1da6.

This patch begins to store all connected MDTs in the quota
global pool. Thus handling MDTs beginning from this patch
is the same with OSTs stored in the global pool. It is the
1st step to introduce MDT pools.

Add conf-sanity_33c that reproduces mentioned memory
corruption without the fix.

Fixes: 50ff4d1da6 ("LU-16772 quota: protect lqe_glbl_data in qmt_site_recalc_cb")
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: Id6e4bcde09d9f32726d69f711eedb82729a2266e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52094
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/lquota_internal.h
@@ -202,7 +202,9 @@ struct lquota_entry {
 
 struct lqe_glbl_entry {
 	__u64			 lge_qunit;
-	unsigned long		 lge_edquot:1,
+	unsigned long		 lge_idx:16,
+				 /* index of target */
+				 lge_edquot:1,
 				 /* true when minimum qunit is set */
 				 lge_qunit_set:1,
 				 /* qunit or edquot is changed - need

lustre/quota/qmt_internal.h
@@ -431,8 +431,15 @@ int qmt_pool_new_conn(const struct lu_env *, struct qmt_device *,
 		qmt_pool_lookup(env, qmt, type, NULL, -1, false)
 #define qmt_pool_lookup_name(env, qmt, type, name) \
 		qmt_pool_lookup(env, qmt, type, name, -1, false)
-#define qmt_pool_lookup_arr(env, qmt, type, idx) \
-		qmt_pool_lookup(env, qmt, type, NULL, idx, true)
+
+/*
+ * Until MDT pools are not emplemented, all MDTs belong to
+ * global pool, thus lookup lqes only in global pool for the
+ * DOM case.
+ */
+#define qmt_pool_lookup_arr(env, qmt, type, idx, stype) \
+		qmt_pool_lookup(env, qmt, type, NULL, \
+		qmt_dom(type, stype) ? -1 : idx, true)
 struct qmt_pool_info *qmt_pool_lookup(const struct lu_env *env,
 					     struct qmt_device *qmt,
 					     int rtype,
@@ -452,7 +459,10 @@ int qmt_pool_add(struct obd_device *obd, char *poolname, char *ostname);
 int qmt_pool_rem(struct obd_device *obd, char *poolname, char *ostname);
 int qmt_pool_del(struct obd_device *obd, char *poolname);
 
-struct rw_semaphore *qmt_sarr_rwsem(struct qmt_pool_info *qpi);
+#define qmt_sarr_read_down(qpi) down_read(&qpi->qpi_sarr.osts.op_rw_sem)
+#define qmt_sarr_read_up(qpi) up_read(&qpi->qpi_sarr.osts.op_rw_sem)
+#define qmt_sarr_write_down(qpi) down_write(&qpi->qpi_sarr.osts.op_rw_sem)
+#define qmt_sarr_write_up(qpi) up_write(&qpi->qpi_sarr.osts.op_rw_sem)
 int qmt_sarr_get_idx(struct qmt_pool_info *qpi, int arr_idx);
 unsigned int qmt_sarr_count(struct qmt_pool_info *qpi);
 
@@ -500,6 +510,7 @@ void qti_lqes_restore_fini(const struct lu_env *env);
 void qti_lqes_write_lock(const struct lu_env *env);
 void qti_lqes_write_unlock(const struct lu_env *env);
 
+int qmt_map_lge_idx(struct lqe_glbl_data *lgd, int ostidx);
 struct lqe_glbl_data *qmt_alloc_lqe_gd(struct qmt_pool_info *, int);
 void qmt_free_lqe_gd(struct lqe_glbl_data *);
 void qmt_setup_lqe_gd(const struct lu_env *,  struct qmt_device *,

lustre/quota/qmt_lock.c
@@ -271,48 +271,71 @@ out:
  */
 static bool qmt_clear_lgeg_arr_nu(struct lquota_entry *lqe, int stype, int idx)
 {
-	unsigned long least_qunit = lqe2qpi(lqe)->qpi_least_qunit;
-	struct lqe_glbl_data *lgd = lqe->lqe_glbl_data;
+	unsigned long least = lqe2qpi(lqe)->qpi_least_qunit;
+	bool revoke = false;
 
 	/* There is no array to store lge for the case of DOM.
-	 * Ignore it until MDT pools will be ready. */
+	 * Ignore it until MDT pools will be ready.
+	 */
 	if (!qmt_dom(lqe_rtype(lqe), stype)) {
-		lqe->lqe_glbl_data->lqeg_arr[idx].lge_qunit_nu = 0;
-		lqe->lqe_glbl_data->lqeg_arr[idx].lge_edquot_nu = 0;
+		struct lqe_glbl_data *lgd;
 
-		/* We shouldn't call revoke for DOM case, it will be updated
-		 * at qmt_id_lock_glimpse. */
-		return (lgd->lqeg_arr[idx].lge_qunit == least_qunit);
+		mutex_lock(&lqe->lqe_glbl_data_lock);
+		lgd = lqe->lqe_glbl_data;
+		if (lgd) {
+			int lge_idx = qmt_map_lge_idx(lgd, idx);
+
+			lgd->lqeg_arr[lge_idx].lge_qunit_nu = 0;
+			lgd->lqeg_arr[lge_idx].lge_edquot_nu = 0;
+			/* We shouldn't call revoke for DOM case, it will be
+			 * updated at qmt_id_lock_glimpse.
+			 */
+			revoke = lgd->lqeg_arr[lge_idx].lge_qunit == least;
+		}
+		mutex_unlock(&lqe->lqe_glbl_data_lock);
 	}
 
-	return false;
+	return revoke;
 }
 
-static bool qmt_set_revoke(struct lu_env *env, struct lquota_entry *lqe,
+static bool qmt_set_revoke(struct lu_env *env, struct lquota_entry *lqe_gl,
 			  int stype, int idx)
 {
-	unsigned long least_qunit = lqe2qpi(lqe)->qpi_least_qunit;
-	struct lqe_glbl_data *lgd = lqe->lqe_glbl_data;
+	unsigned long least_qunit = lqe2qpi(lqe_gl)->qpi_least_qunit;
 	bool notify = false;
 
-	if (lgd->lqeg_arr[idx].lge_qunit == least_qunit) {
-		int i;
+	if (qmt_dom(lqe_rtype(lqe_gl), stype))
+		return false;
 
-		qti_lqes_write_lock(env);
-		for (i = 0; i < qti_lqes_cnt(env); i++) {
-			LQUOTA_DEBUG(qti_lqes(env)[i],
-				     "idx %d lge_qunit %llu least_qunit %lu\n",
-				     idx, lgd->lqeg_arr[idx].lge_qunit,
-				     least_qunit);
-			if (qti_lqes(env)[i]->lqe_qunit == least_qunit) {
-				qti_lqes(env)[i]->lqe_revoke_time =
+	qti_lqes_write_lock(env);
+	mutex_lock(&lqe_gl->lqe_glbl_data_lock);
+	if (lqe_gl->lqe_glbl_data) {
+		struct lqe_glbl_data *lgd = lqe_gl->lqe_glbl_data;
+		int lge_idx;
+
+		lge_idx = qmt_map_lge_idx(lgd, idx);
+		if (lgd->lqeg_arr[lge_idx].lge_qunit == least_qunit) {
+			struct lquota_entry *lqe;
+			int i;
+
+			for (i = 0; i < qti_lqes_cnt(env); i++) {
+				lqe = qti_lqes(env)[i];
+				LQUOTA_DEBUG(lqe,
+					     "lge_qunit %llu least_qunit %lu idx %d\n",
+					     lgd->lqeg_arr[lge_idx].lge_qunit,
+					     least_qunit, idx);
+				if (lqe->lqe_qunit == least_qunit) {
+					lqe->lqe_revoke_time =
 							ktime_get_seconds();
-				notify |= qmt_adjust_edquot(qti_lqes(env)[i],
+					notify |= qmt_adjust_edquot(lqe,
 						  ktime_get_real_seconds());
+				}
 			}
 		}
-		qti_lqes_write_unlock(env);
 	}
+	mutex_unlock(&lqe_gl->lqe_glbl_data_lock);
+	qti_lqes_write_unlock(env);
+
 	return notify;
 }
 
@@ -403,12 +426,16 @@ int qmt_lvbo_update(struct lu_device *ld, struct ldlm_resource *res,
 		GOTO(out_exp, rc);
 
 	if (need_revoke && qmt_set_revoke(env, lqe, stype, idx)) {
+		int notify = false;
+
 		mutex_lock(&lqe->lqe_glbl_data_lock);
 		if (lqe->lqe_glbl_data) {
 			qmt_seed_glbe_edquot(env, lqe->lqe_glbl_data);
-			qmt_id_lock_notify(qmt, lqe);
+			notify = true;
 		}
 		mutex_unlock(&lqe->lqe_glbl_data_lock);
+		if (notify)
+			qmt_id_lock_notify(qmt, lqe);
 	}
 
 	if (lvb->lvb_id_rel) {
@@ -652,12 +679,14 @@ static void qmt_setup_id_desc(struct ldlm_lock *lock, union ldlm_gl_desc *desc,
 		qunit = lqe->lqe_qunit;
 	} else {
 		struct lqe_glbl_data *lgd;
+		int lge_idx;
 
 		mutex_lock(&lqe->lqe_glbl_data_lock);
 		lgd = lqe->lqe_glbl_data;
 		if (lgd) {
-			edquot = lgd->lqeg_arr[idx].lge_edquot;
-			qunit = lgd->lqeg_arr[idx].lge_qunit;
+			lge_idx = qmt_map_lge_idx(lgd, idx);
+			edquot = lgd->lqeg_arr[lge_idx].lge_edquot;
+			qunit = lgd->lqeg_arr[lge_idx].lge_qunit;
 		} else {
 			edquot = lqe->lqe_edquot;
 			qunit = lqe->lqe_qunit;
@@ -858,20 +887,32 @@ void qmt_glb_lock_notify(const struct lu_env *env, struct lquota_entry *lqe,
  * broadcasting the new qunit value */
 static int qmt_id_lock_cb(struct ldlm_lock *lock, struct lquota_entry *lqe)
 {
-	struct obd_uuid	*uuid = &(lock)->l_export->exp_client_uuid;
+	struct obd_uuid *uuid = &(lock)->l_export->exp_client_uuid;
 	struct lqe_glbl_data *lgd = lqe->lqe_glbl_data;
 	int idx;
 	int stype = qmt_uuid2idx(uuid, &idx);
 
 	LASSERT(stype == QMT_STYPE_OST || stype == QMT_STYPE_MDT);
 
+	CDEBUG(D_QUOTA, "stype %d rtype %d idx %d uuid %s\n",
+	       stype, lqe_rtype(lqe), idx, uuid->uuid);
 	/* Quota pools support only OSTs, despite MDTs also could be registered
 	 * as LQUOTA_RES_DT devices(DOM). */
 	if (qmt_dom(lqe_rtype(lqe), stype))
 		return 1;
-	else
-		return lgd ? lgd->lqeg_arr[idx].lge_edquot_nu ||
-		       lgd->lqeg_arr[idx].lge_qunit_nu : 0;
+
+	if (lgd) {
+		int lge_idx = qmt_map_lge_idx(lgd, idx);
+
+		CDEBUG(D_QUOTA,
+		       "tgt idx:%d lge_idx:%d edquot_nu:%d qunit_nu:%d\n",
+		       idx, lge_idx, lgd->lqeg_arr[lge_idx].lge_edquot_nu,
+		       lgd->lqeg_arr[lge_idx].lge_qunit_nu);
+		return lgd->lqeg_arr[lge_idx].lge_edquot_nu ||
+		       lgd->lqeg_arr[lge_idx].lge_qunit_nu;
+	}
+
+	return 0;
 }
 
 

commit b64bc2c6f35f9fee35da6eec1e476eab76ad7646

LU-17356 quota: fix qmt_pool_new_conn
Wrong argument passed into qmt_dom from
qmt_pool_new_conn caused a panic:

  qmt_sarr_get_idx()) ASSERTION( arr_idx <
    qpi->qpi_sarr.osts.op_count && arr_idx >= 0 )
    failed: idx invalid 0 op_count 0

Add conf-sanity_33d that reproduces above
assertion without the fix.

Fixes: 67f90e4288 ("LU-17034 quota: lqeg_arr memmory corruption")
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I48801f1fb7e69097cbfbe083f1d31a4639d4bf4d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55019
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1626,7 +1626,7 @@ struct quota_body {
 #define QUOTA_DQACQ_FL_REPORT	0x8  /* report usage */
 
 /* Quota types currently supported */
-enum {
+enum lquota_type {
 	LQUOTA_TYPE_USR	= 0x00, /* maps to USRQUOTA */
 	LQUOTA_TYPE_GRP	= 0x01, /* maps to GRPQUOTA */
 	LQUOTA_TYPE_PRJ	= 0x02, /* maps to PRJQUOTA */
@@ -1637,7 +1637,7 @@ enum {
  * - inodes on the MDTs
  * - blocks on the OSTs
  */
-enum {
+enum lquota_res_type {
 	LQUOTA_RES_MD		= 0x01, /* skip 0 to avoid null oid in FID */
 	LQUOTA_RES_DT		= 0x02,
 	LQUOTA_LAST_RES,

lustre/quota/lproc_quota.c
@@ -212,7 +212,8 @@ static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 		if (fid_is_acct(fid)) {
 			seq_printf(p, "%s:\n", oid2name(fid_oid(fid)));
 		} else if (fid_seq(fid) == FID_SEQ_QUOTA_GLB) {
-			int	rtype, qtype;
+			enum lquota_res_type rtype;
+			enum lquota_type qtype;
 
 			rc = lquota_extract_fid(fid, &rtype, &qtype);
 			if (rc)

lustre/quota/lquota_disk.c
@@ -404,7 +404,8 @@ struct dt_object *lquota_disk_slv_find_create(const struct lu_env *env,
 	qti->qti_fid.f_seq = FID_SEQ_QUOTA;
 	qti->qti_fid.f_ver = 0;
 	if (local) {
-		int pool_type, qtype;
+		enum lquota_res_type pool_type;
+		enum lquota_type qtype;
 
 		rc = lquota_extract_fid(glb_fid, &pool_type, &qtype);
 		if (rc)

lustre/quota/lquota_internal.h
@@ -447,7 +447,8 @@ void lquota_lqe_debug0(struct lquota_entry *lqe,
 struct dt_object *acct_obj_lookup(const struct lu_env *, struct dt_device *,
 				  int);
 void lquota_generate_fid(struct lu_fid *, int, int);
-int lquota_extract_fid(const struct lu_fid *, int *, int *);
+int lquota_extract_fid(const struct lu_fid *, enum lquota_res_type *,
+		       enum lquota_type *);
 const struct dt_index_features *glb_idx_feature(struct lu_fid *);
 int lquota_obj_iter(const struct lu_env *env, struct dt_device *dev,
 		    struct dt_object *obj, struct obd_quotactl *oqctl,

lustre/quota/lquota_lib.c
@@ -491,8 +491,9 @@ void lquota_generate_fid(struct lu_fid *fid, int pool_type, int quota_type)
  * Helper routine used to extract pool type and quota type from a
  * given FID.
  */
-int lquota_extract_fid(const struct lu_fid *fid, int *pool_type,
-		       int *quota_type)
+int lquota_extract_fid(const struct lu_fid *fid,
+		       enum lquota_res_type *pool_type,
+		       enum lquota_type *quota_type)
 {
 	unsigned int lqtype;
 	ENTRY;

lustre/quota/qmt_handler.c
@@ -1182,7 +1182,7 @@ out:
  * \retval		slave type(QMT_STYPE_MDT or QMT_STYPE_OST)
  * \retval -EINVAL	wrong uuid
  */
-int qmt_uuid2idx(struct obd_uuid *uuid, int *idx)
+enum qmt_stype qmt_uuid2idx(struct obd_uuid *uuid, int *idx)
 {
 	char *uuid_str, *name, *dash;
 	int rc = -EINVAL;
@@ -1236,8 +1236,10 @@ static int qmt_dqacq(const struct lu_env *env, struct lu_device *ld,
 	struct quota_body *qbody, *repbody;
 	struct obd_uuid	*uuid;
 	struct ldlm_lock *lock;
-	int rtype, qtype;
-	int rc, idx, stype;
+	enum lquota_res_type rtype;
+	enum lquota_type qtype;
+	enum qmt_stype stype;
+	int rc, idx;
 	struct obd_device *obd = NULL;
 
 	ENTRY;

lustre/quota/qmt_internal.h
@@ -93,7 +93,7 @@ union qmt_sarray {
 
 /* Since DOM support, data resources can exist
  * on both MDT and OST targets. */
-enum {
+enum qmt_stype {
 	QMT_STYPE_MDT,
 	QMT_STYPE_OST,
 	QMT_STYPE_CNT
@@ -532,7 +532,7 @@ int qmt_set_with_lqe(const struct lu_env *env, struct qmt_device *qmt,
 		     __u64 time, __u32 valid, bool is_default, bool is_updated);
 int qmt_dqacq0(const struct lu_env *, struct qmt_device *, struct obd_uuid *,
 	       __u32, __u64, __u64, struct quota_body *, int);
-int qmt_uuid2idx(struct obd_uuid *, int *);
+enum qmt_stype qmt_uuid2idx(struct obd_uuid *, int *);
 
 /* qmt_lock.c */
 int qmt_intent_policy(const struct lu_env *, struct lu_device *,

lustre/quota/qmt_lock.c
@@ -90,9 +90,10 @@ int qmt_intent_policy(const struct lu_env *env, struct lu_device *ld,
 	switch (it->opc) {
 
 	case IT_QUOTA_DQACQ: {
-		struct lquota_entry	*lqe;
-		struct ldlm_lock	*lock;
-		int idx, stype;
+		struct lquota_entry *lqe;
+		struct ldlm_lock *lock;
+		enum qmt_stype stype;
+		int idx;
 
 		if (res->lr_name.name[LUSTRE_RES_ID_QUOTA_SEQ_OFF] == 0)
 			/* acquire on global lock? something is wrong ... */
@@ -178,11 +179,12 @@ out:
  */
 int qmt_lvbo_init(struct lu_device *ld, struct ldlm_resource *res)
 {
-	struct lu_env		*env;
-	struct qmt_thread_info	*qti;
-	struct qmt_device	*qmt = lu2qmt_dev(ld);
-	int			 pool_type, qtype;
-	int			 rc;
+	struct lu_env *env;
+	struct qmt_thread_info *qti;
+	struct qmt_device *qmt = lu2qmt_dev(ld);
+	enum lquota_res_type pool_type;
+	enum lquota_type qtype;
+	int rc;
 	ENTRY;
 
 	LASSERT(res != NULL);
@@ -350,15 +352,16 @@ static bool qmt_set_revoke(struct lu_env *env, struct lquota_entry *lqe_gl,
 int qmt_lvbo_update(struct lu_device *ld, struct ldlm_resource *res,
 		    struct ptlrpc_request *req, int increase_only)
 {
-	struct lu_env		*env;
-	struct qmt_thread_info	*qti;
-	struct qmt_device	*qmt = lu2qmt_dev(ld);
-	struct lquota_entry	*lqe;
-	struct lquota_lvb	*lvb;
-	struct ldlm_lock	*lock;
-	struct obd_export	*exp;
-	bool			 need_revoke;
-	int			 rc = 0, idx, stype;
+	struct lu_env *env;
+	struct qmt_thread_info *qti;
+	struct qmt_device *qmt = lu2qmt_dev(ld);
+	struct lquota_entry *lqe;
+	struct lquota_lvb *lvb;
+	struct ldlm_lock *lock;
+	struct obd_export *exp;
+	bool need_revoke;
+	enum qmt_stype stype;
+	int rc = 0, idx;
 	ENTRY;
 
 	LASSERT(res != NULL);
@@ -505,16 +508,17 @@ int qmt_lvbo_fill(struct lu_device *ld, struct ldlm_lock *lock, void *lvb,
 		struct lquota_entry *lqe = res->lr_lvb_data;
 		struct qmt_device *qmt;
 		struct obd_uuid *uuid;
+		enum qmt_stype stype;
 		int idx;
 
 		uuid = &(lock)->l_export->exp_client_uuid;
-		rc = qmt_uuid2idx(uuid, &idx);
-		if (rc < 0)
-			RETURN(rc);
+		stype = qmt_uuid2idx(uuid, &idx);
+		if (stype < 0)
+			RETURN(stype);
 		qmt = lu2qmt_dev(ld);
 		/* return current qunit value & edquot flags in lvb */
 		lqe_getref(lqe);
-		rc = qmt_pool_lqes_lookup(env, qmt, lqe_rtype(lqe), rc,
+		rc = qmt_pool_lqes_lookup(env, qmt, lqe_rtype(lqe), stype,
 					  lqe_qtype(lqe), &lqe->lqe_id,
 					  NULL, idx);
 		if (!rc) {
@@ -661,7 +665,8 @@ static void qmt_setup_id_desc(struct ldlm_lock *lock, union ldlm_gl_desc *desc,
 			      struct lquota_entry *lqe)
 {
 	struct obd_uuid *uuid = &(lock)->l_export->exp_client_uuid;
-	int idx, stype;
+	enum qmt_stype stype;
+	int idx;
 	__u64 qunit;
 	bool edquot;
 
@@ -884,9 +889,10 @@ static int qmt_id_lock_cb(struct ldlm_lock *lock, struct lquota_entry *lqe)
 {
 	struct obd_uuid *uuid = &(lock)->l_export->exp_client_uuid;
 	struct lqe_glbl_data *lgd = lqe->lqe_glbl_data;
+	enum qmt_stype stype;
 	int idx;
-	int stype = qmt_uuid2idx(uuid, &idx);
 
+	stype = qmt_uuid2idx(uuid, &idx);
 	LASSERT(stype == QMT_STYPE_OST || stype == QMT_STYPE_MDT);
 
 	CDEBUG(D_QUOTA, "stype %d rtype %d idx %d uuid %s\n",

lustre/quota/qmt_pool.c
@@ -60,7 +60,7 @@ static inline int qmt_sarr_pool_init(struct qmt_pool_info *qpi);
 #define qmt_sarr_pool_add_locked(qpi, idx, stype) \
 		_qmt_sarr_pool_add(qpi, idx, stype, true)
 static inline int _qmt_sarr_pool_add(struct qmt_pool_info *qpi,
-				    int idx, int min, bool locked);
+				    int idx, enum qmt_stype min, bool locked);
 static inline int qmt_sarr_pool_rem(struct qmt_pool_info *qpi, int idx);
 static inline void qmt_sarr_pool_free(struct qmt_pool_info *qpi);
 static inline int qmt_sarr_check_idx(struct qmt_pool_info *qpi, int idx);
@@ -519,8 +519,9 @@ static int qmt_slv_add(const struct lu_env *env, struct lu_fid *glb_fid,
 {
 	struct obd_uuid uuid;
 	struct qmt_pool_info *qpi = arg;
-	int stype, qtype, idx;
-	int rc;
+	enum lquota_type qtype;
+	enum qmt_stype stype;
+	int idx, rc;
 
 	rc = lquota_extract_fid(glb_fid, NULL, &qtype);
 	LASSERT(!rc);
@@ -786,11 +787,13 @@ int qmt_pool_new_conn(const struct lu_env *env, struct qmt_device *qmt,
 		      struct lu_fid *glb_fid, struct lu_fid *slv_fid,
 		      __u64 *slv_ver, struct obd_uuid *uuid)
 {
-	struct qmt_pool_info	*pool;
-	struct dt_object	*slv_obj;
-	int			 pool_type, qtype, stype;
-	bool			 created = false;
-	int			 idx, i, rc = 0;
+	struct qmt_pool_info *pool;
+	struct dt_object *slv_obj;
+	enum lquota_res_type pool_type;
+	enum lquota_type qtype;
+	enum qmt_stype stype;
+	bool created = false;
+	int idx, i, rc = 0;
 
 	stype = qmt_uuid2idx(uuid, &idx);
 	if (stype < 0)
@@ -833,7 +836,7 @@ int qmt_pool_new_conn(const struct lu_env *env, struct qmt_device *qmt,
 		CDEBUG(D_QUOTA, "add tgt idx:%d pool_type:%d qtype:%d stype:%d\n",
 		       idx, pool_type, qtype, stype);
 
-		if (!qmt_dom(qtype, stype)) {
+		if (!qmt_dom(pool_type, stype)) {
 			qmt_sarr_write_down(pool);
 			rc = qmt_sarr_pool_add_locked(pool, idx, stype);
 			if (!rc) {
@@ -1636,7 +1639,8 @@ static inline int qmt_sarr_pool_init(struct qmt_pool_info *qpi)
 }
 
 static inline int
-_qmt_sarr_pool_add(struct qmt_pool_info *qpi, int idx, int stype, bool locked)
+_qmt_sarr_pool_add(struct qmt_pool_info *qpi, int idx, enum qmt_stype stype,
+		   bool locked)
 {
 	/* We don't have an array for DOM */
 	if (qmt_dom(qpi->qpi_rtype, stype))

lustre/tests/conf-sanity.sh
@@ -3175,6 +3175,19 @@ test_33c() {
 }
 run_test 33c "Mount ost with a large index number"
 
+test_33d() {
+	setup
+	stack_trap cleanup
+
+	mkdir_on_mdt0 $DIR/$tdir || error "cannot create $DIR/$tdir"
+	touch $DIR/$tfile
+	$LFS setquota -p 1 -I1K $MOUNT
+
+	do_facet mgs $LCTL set_param osd*.*.quota_slave.enabled=p
+	$LFS project -p 1 $DIR/
+}
+run_test 33d "Don't panic when enable project quota"
+
 test_34a() {
 	setup
 	do_facet client "bash runmultiop_bg_pause $DIR/file O_c"