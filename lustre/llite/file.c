lustre/lustre-release

commit 7194eb6431d2ef7245ef3b13394b60e220145187

LU-13805 clio: bounce buffer for unaligned DIO
Direct I/O must normally be page aligned both in terms of
I/O size and memory alignment.  This is because the I/O
must be page aligned before being written to disk.  This
is also true for buffered I/O, but the I/O is aligned
using the page cache.

In recent versions of Lustre, direct I/O is significantly
faster than buffered I/O, due to lower overhead for page
management.  Thus it is desirable to be able to do more
I/O as direct I/O.

This patch allows unaligned direct I/O by creating a buffer
inside the kernel and aligning the I/O by copying in to
this aligned buffer.  Because the main cost of buffered I/O
is locking in the page cache rather than memcopy(), this is
still significantly faster than buffered I/O (though slower
than normal direct I/O).

This will eventually allow us to convert buffered I/O to
direct I/O when doing so would increase performance.

Here are some comparative benchmarks using IOR, all single
process.

UDIO is unaligned DIO.

io size  	1M           4M          16M          64M
----------------------------------------------------------
BIO Write  | 1502 MiB/s | 1382 MiB/s | 1683 MiB/s | 1677 MiB/s
BIO Read   | 2169 MiB/s | 1902 MiB/s | 2131 MiB.s | 1955 MiB/s
DIO Write  | 1010 MiB/s | 2778 MiB/s | 5905 MiB/s | 7917 MiB/s
DIO Read   | 893 MiB/s  | 2657 MiB/s | 4724 MiB/s | 7579 MiB/s
UDIO Write | 848 MiB/s  | 1666 MiB/s | 2117 MiB/s | 2243 MiB/s
UDIO Read  | 933 MIB/s  | 2412 MiB/s | 3690 MiB/s | 5370 MiB/s

Unaligned DIO offers benefits vs buffered write and
buffered read, but is of course slower than DIO.

Notice on this node the best case DIO performance is
~8 GiB/s.  On a node with 12 GiB/s best case DIO, best case
UDIO read is 8 GiB/s and best case UDIO write is 2.5 GiB/s.

This is because UDIO read is fully parallel, UDIO write is
not.

Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I7eeebf9a608f006c8095b95f0677adb99f19d640

lustre/llite/rw26.c
@@ -492,26 +492,39 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	ssize_t tot_bytes = 0, result = 0;
 	loff_t file_offset = iocb->ki_pos;
 	bool sync_submit = false;
+	bool unaligned = false;
 	struct vvp_io *vio;
 	ssize_t rc2;
 
+	ENTRY;
+
 	/* Check EOF by ourselves */
 	if (rw == READ && file_offset >= i_size_read(inode))
-		return 0;
+		RETURN(0);
 
-	/* FIXME: io smaller than PAGE_SIZE is broken on ia64 ??? */
 	if (file_offset & ~PAGE_MASK)
-		RETURN(-EINVAL);
+		unaligned = true;
 
-	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p), size=%zd (max %lu), "
-	       "offset=%lld=%llx, pages %zd (max %lu)\n",
-	       PFID(ll_inode2fid(inode)), inode, count, MAX_DIO_SIZE,
-	       file_offset, file_offset, count >> PAGE_SHIFT,
-	       MAX_DIO_SIZE >> PAGE_SHIFT);
+	if (count & ~PAGE_MASK)
+		unaligned = true;
 
 	/* Check that all user buffers are aligned as well */
 	if (ll_iov_iter_alignment(iter) & ~PAGE_MASK)
-		RETURN(-EINVAL);
+		unaligned = true;
+
+	CDEBUG(D_VFSTRACE,
+	       "VFS Op:inode="DFID"(%p), size=%zd (max %lu), offset=%lld=%llx, pages %zd (max %lu)%s\n",
+	       PFID(ll_inode2fid(inode)), inode, count, MAX_DIO_SIZE,
+	       file_offset, file_offset,
+	       (count >> PAGE_SHIFT) + !!(count & ~PAGE_MASK),
+	       MAX_DIO_SIZE >> PAGE_SHIFT, unaligned ? ", unaligned" : "");
+
+	/* the requirement to not return EIOCBQUEUED for pipes (see bottom of
+	 * this function) plays havoc with the unaligned I/O lifecycle, so
+	 * don't allow unaligned I/O on pipes
+	 */
+	if (unaligned && iov_iter_is_pipe(iter))
+		RETURN(0);
 
 	lcc = ll_cl_find(inode);
 	if (lcc == NULL)
@@ -522,6 +535,15 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	vio = vvp_env_io(env);
 	io = lcc->lcc_io;
 	LASSERT(io != NULL);
+
+	/* if one part of an I/O is unaligned, just handle all of it that way -
+	 * otherwise we create significant complexities with managing the iovec
+	 * in different ways, etc, all for very marginal benefits
+	 */
+	if (unaligned)
+		io->ci_unaligned_dio = true;
+	if (io->ci_unaligned_dio)
+		unaligned = true;
 
 	ll_dio_aio = io->ci_dio_aio;
 	LASSERT(ll_dio_aio);
@@ -557,14 +579,28 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 		 * (either in this function or from a ptlrpcd daemon)
 		 */
 		sdio = cl_sub_dio_alloc(ll_dio_aio, iter, rw == WRITE,
-					sync_submit);
+					unaligned, sync_submit);
 		if (!sdio)
 			GOTO(out, result = -ENOMEM);
 
 		pvec = &sdio->csd_dio_pages;
 		pvec->ldp_file_offset = file_offset;
 
-		result = ll_get_user_pages(rw, iter, pvec, count);
+		if (!unaligned) {
+			result = ll_get_user_pages(rw, iter, pvec, count);
+			/* ll_get_user_pages returns bytes in the IO or error*/
+			count = result;
+		} else {
+			/* same calculation used in ll_get_user_pages */
+			count = min_t(size_t, count, iter->iov->iov_len);
+			result = ll_allocate_dio_buffer(pvec, count);
+			/* allocate_dio_buffer returns number of pages or
+			 * error, so do not set count = result
+			 */
+		}
+
+		/* now we have the actual count, so store it in the sdio */
+		sdio->csd_bytes = count;
 
 		if (unlikely(result <= 0)) {
 			cl_sync_io_note(env, &sdio->csd_sync, result);
@@ -574,10 +610,33 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 			}
 			GOTO(out, result);
 		}
-		count = result;
 
-		result = ll_direct_rw_pages(env, io, count,
-					    rw, inode, sdio);
+		if (unaligned && rw == WRITE) {
+			result = ll_dio_user_copy(sdio, iter);
+			if (unlikely(result <= 0)) {
+				cl_sync_io_note(env, &sdio->csd_sync, result);
+				if (sync_submit) {
+					cl_sub_dio_free(sdio);
+					LASSERT(sdio->csd_creator_free);
+				}
+				GOTO(out, result);
+			}
+		}
+
+		result = ll_direct_rw_pages(env, io, count, rw, inode, sdio);
+		/* if the i/o was unsuccessful, we zero the number of bytes to
+		 * copy back.  Note that partial I/O completion isn't possible
+		 * here - I/O either completes or fails.  So there's no need to
+		 * handle short I/O here by changing 'count' with the result
+		 * from ll_direct_rw_pages.
+		 *
+		 * This must be done before we release the reference
+		 * immediately below, because releasing the reference allows
+		 * i/o completion (and copyback to userspace, if unaligned) to
+		 * start.
+		 */
+		if (result != 0)
+			sdio->csd_bytes = 0;
 		/* We've submitted pages and can now remove the extra
 		 * reference for that
 		 */
@@ -595,12 +654,15 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 			GOTO(out, result);
 
 		iov_iter_advance(iter, count);
+
 		tot_bytes += count;
 		file_offset += count;
+		CDEBUG(D_VFSTRACE,
+		       "result %zd tot_bytes %zd count %zd file_offset %lld\n",
+		       result, tot_bytes, count, file_offset);
 	}
 
 out:
-
 	if (rw == WRITE)
 		vio->u.readwrite.vui_written += tot_bytes;
 	else
@@ -612,10 +674,10 @@ out:
 	if (result == 0 && !iov_iter_is_pipe(iter))
 		result = -EIOCBQUEUED;
 
-	return result;
+	RETURN(result);
 }
 
-#if defined(HAVE_DIO_ITER)
+#ifdef HAVE_DIO_ITER
 static ssize_t ll_direct_IO(
 #ifndef HAVE_IOV_ITER_RW
 	     int rw,

lustre/autoconf/lustre-core.m4
@@ -3182,6 +3182,26 @@ fileattr_set, [
 EXTRA_KCFLAGS="$tmp_flags"
 ]) # LC_HAVE_FILEATTR_GET
 
+# LC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC
+#
+# Kernel 5.13 commit f0b65f39ac505e8f1dcdaa165aa7b8c0bd6fd454
+# iov_iter: replace iov_iter_copy_from_user_atomic() with iterator-advancing variant
+#
+AC_DEFUN([LC_SRC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC], [
+	LB2_LINUX_TEST_SRC([copy_page_from_iter_atomic], [
+		#include <linux/uio.h>
+	],[
+		copy_page_from_iter_atomic(NULL, 0, 0, NULL);
+	])
+])
+AC_DEFUN([LC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC], [
+	AC_MSG_CHECKING([if have copy_page_from_iter_atomic])
+	LB2_LINUX_TEST_RESULT([copy_page_from_iter_atomic], [
+		AC_DEFINE(HAVE_COPY_PAGE_FROM_ITER_ATOMIC, 1,
+			['copy_page_from_iter_atomic' exists])
+	])
+]) # LC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC
+
 #
 # LC_HAVE_GET_ACL_RCU_ARG
 #
@@ -3204,6 +3224,26 @@ AC_DEFUN([LC_HAVE_GET_ACL_RCU_ARG], [
 			['get_acl' has a rcu argument])
 	])
 ]) # LC_HAVE_GET_ACL_RCU_ARG
+
+# LC_HAVE_FAULT_IN_IOV_ITER_READABLE
+#
+# Kernel 5.15 commit a6294593e8a1290091d0b078d5d33da5e0cd3dfe
+# iov_iter: Turn iov_iter_fault_in_readable into fault_in_iov_iter_readable
+#
+AC_DEFUN([LC_SRC_HAVE_FAULT_IN_IOV_ITER_READABLE], [
+	LB2_LINUX_TEST_SRC([fault_in_iov_iter_readable], [
+		#include <linux/uio.h>
+	],[
+		fault_in_iov_iter_readable(NULL, 0);
+	])
+])
+AC_DEFUN([LC_HAVE_FAULT_IN_IOV_ITER_READABLE], [
+	AC_MSG_CHECKING([if have fault_in_iov_iter_readable])
+	LB2_LINUX_TEST_RESULT([fault_in_iov_iter_readable], [
+		AC_DEFINE(HAVE_FAULT_IN_IOV_ITER_READABLE, 1,
+			['fault_in_iov_iter_readable' exists])
+	])
+]) # LC_HAVE_FAULT_IN_IOV_ITER_READABLE
 
 #
 # LC_HAVE_INVALIDATE_LOCK
@@ -4129,8 +4169,12 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	# 5.12
 	LC_SRC_HAVE_USER_NAMESPACE_ARG
 
+	# 5.13
+	LC_SRC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC
+
 	# 5.15
 	LC_SRC_HAVE_GET_ACL_RCU_ARG
+	LC_SRC_HAVE_FAULT_IN_IOV_ITER_READABLE
 
 	# 5.16
 	LC_SRC_HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
@@ -4398,10 +4442,15 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 
 	# 5.13
 	LC_HAVE_FILEATTR_GET
+	LC_HAVE_COPY_PAGE_FROM_ITER_ATOMIC
+
+        # 5.15
+        LC_HAVE_GET_ACL_RCU_ARG
 
 	# 5.15
 	LC_HAVE_GET_ACL_RCU_ARG
 	LC_HAVE_INVALIDATE_LOCK
+	LC_HAVE_FAULT_IN_IOV_ITER_READABLE
 
 	# 5.16
 	LC_HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG

lustre/include/cl_object.h
@@ -1942,7 +1942,12 @@ struct cl_io {
 	 * to userspace, only the RPCs are submitted async, then waited for at
 	 * the llite layer before returning.
 	 */
-			     ci_parallel_dio:1;
+			     ci_parallel_dio:1,
+	/**
+	 * this DIO is at least partly unaligned, and so the unaligned DIO
+	 * path is being used for this entire IO
+	 */
+			     ci_unaligned_dio:1;
 	/**
 	 * Bypass quota check
 	 */
@@ -2521,7 +2526,7 @@ struct cl_dio_aio *cl_dio_aio_alloc(struct kiocb *iocb, struct cl_object *obj,
 				    bool is_aio);
 struct cl_sub_dio *cl_sub_dio_alloc(struct cl_dio_aio *ll_aio,
 				    struct iov_iter *iter, bool write,
-				    bool sync);
+				    bool unaligned, bool sync);
 void cl_dio_aio_free(const struct lu_env *env, struct cl_dio_aio *aio);
 void cl_sub_dio_free(struct cl_sub_dio *sdio);
 static inline void cl_sync_io_init(struct cl_sync_io *anchor, int nr)
@@ -2553,14 +2558,14 @@ struct cl_sync_io {
 /** direct IO pages */
 struct ll_dio_pages {
 	/*
-	 * page array to be written. we don't support
-	 * partial pages except the last one.
+	 * page array for RDMA - for aligned i/o, this is the user provided
+	 * pages, but for unaligned i/o, this is the internal buffer
 	 */
-	struct page             **ldp_pages;
+	struct page		**ldp_pages;
 	/** # of pages in the array. */
-	size_t                  ldp_count;
+	size_t			ldp_count;
 	/* the file offset of the first page. */
-	loff_t                  ldp_file_offset;
+	loff_t			ldp_file_offset;
 };
 
 /* Top level struct used for AIO and DIO */
@@ -2585,7 +2590,8 @@ struct cl_sub_dio {
 	struct ll_dio_pages	csd_dio_pages;
 	struct iov_iter		csd_iter;
 	unsigned		csd_creator_free:1,
-				csd_write:1;
+				csd_write:1,
+				csd_unaligned:1;
 };
 #if defined(HAVE_DIRECTIO_ITER) || defined(HAVE_IOV_ITER_RW) || \
 	defined(HAVE_DIRECTIO_2ARGS)
@@ -2593,6 +2599,9 @@ struct cl_sub_dio {
 #endif
 
 void ll_release_user_pages(struct page **pages, int npages);
+int ll_allocate_dio_buffer(struct ll_dio_pages *pvec, size_t io_size);
+void ll_free_dio_buffer(struct ll_dio_pages *pvec);
+ssize_t ll_dio_user_copy(struct cl_sub_dio *sdio, struct iov_iter *write_iov);
 
 #ifndef HAVE_KTHREAD_USE_MM
 #define kthread_use_mm(mm) use_mm(mm)

commit a3a0cced416ce9721f5baa7c2da458ff07b33838

LU-16025 llite: allow unaligned DIO reaching EOF
Direct IO requires file offset and iov_iter count be page aligned, if
server does not support unaligned DIO.

For old servers, they do not have OBD_CONNECT2_UNALIGNED_DIO support,
and be deemed as not supporting unaligned DIO.

Since mirror resync would use direct IO to read data from a mirror,
and if the file size is not page aligned, the last read iov_iter
would be truncated by commit 4468f6c9d9 and would contain unaligned
iov_iter count, so it would fail with old servers.

This patch fixes this interop issue by allowing unaligned DIO
reaching the end of the file.

Test-Parameters: testlist=sanity-sec serverversion=EXA6
Fixes: 7194eb6431 ("LU-13805 clio: bounce buffer for unaligned DIO")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I229e193c3f0df0c21284991809573e312d18a556
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54718
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Patrick Farrell <patrick.farrell@oracle.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -2182,7 +2182,7 @@ fini_io:
 	if (kms > 0 && (iocb->ki_pos >= kms || read_end > kms)) {
 		rc = ll_glimpse_size(inode);
 		if (rc != 0)
-			return rc;
+			RETURN(rc);
 
 		size = i_size_read(inode);
 		if (iocb->ki_pos >= size || read_end > size) {

lustre/llite/rw26.c
@@ -494,7 +494,7 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	if (file_offset & ~PAGE_MASK)
 		unaligned = true;
 
-	if (count & ~PAGE_MASK)
+	if ((file_offset + count < i_size_read(inode)) && (count & ~PAGE_MASK))
 		unaligned = true;
 
 	/* Check that all user buffers are aligned as well */
@@ -512,7 +512,7 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	LASSERT(io != NULL);
 
 	CDEBUG(D_VFSTRACE,
-	       "VFS Op:inode="DFID"(%p), size=%zd (max %lu), offset=%lld=%llx, pages %zd (max %lu)%s%s%s%s\n",
+	       "VFS Op:inode="DFID"(%p), size=%zd (max %lu), offset=%lld=%#llx, pages %zd (max %lu)%s%s%s%s\n",
 	       PFID(ll_inode2fid(inode)), inode, count, MAX_DIO_SIZE,
 	       file_offset, file_offset,
 	       (count >> PAGE_SHIFT) + !!(count & ~PAGE_MASK),

lustre/utils/lfs.c
@@ -843,6 +843,8 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 		rc = ftruncate(fd_dst, pos);
 		if (rc < 0) {
 			rc = -errno;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "fail to ftruncate dst file to %ld", pos);
 			free(buf);
 			return rc;
 		}
@@ -868,8 +870,12 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 			/* hole at the end of file, truncate up to it */
 			if (!data_size) {
 				rc = ftruncate(fd_dst, data_off);
-				if (rc < 0)
+				if (rc < 0) {
+					llapi_error(LLAPI_MSG_ERROR, rc,
+						    "fail to ftruncate dst file to %ld",
+						    data_off);
 					goto out;
+				}
 			}
 			pos = data_off & ~(page_size - 1);
 			data_end = data_off + data_size;
@@ -881,14 +887,20 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 
 		if (check_file) {
 			rc = check_file(fd_src);
-			if (rc < 0)
+			if (rc < 0) {
+				llapi_error(LLAPI_MSG_ERROR, rc,
+					    "error checking src file");
 				goto out;
+			}
 		}
 
 		rsize = pread(fd_src, buf, to_read, pos);
 		read_bytes += rsize;
 		if (rsize < 0) {
 			rc = -errno;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "error reading src bytes %ld-%ld",
+				    pos, to_read);
 			goto out;
 		}
 		/* EOF */
@@ -904,6 +916,9 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 			written = pwrite(fd_dst, buf, to_write, pos);
 			if (written < 0) {
 				rc = -errno;
+				llapi_error(LLAPI_MSG_ERROR, rc,
+					    "error writing dst bytes %ld-%ld",
+					    pos, to_write);
 				goto out;
 			}
 			pos += written;
@@ -939,10 +954,8 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 
 				if (rc < 0) {
 					if (stats_interval_sec)
-						fprintf(stderr,
-							"error %s: delay for bandwidth control failed: %s\n",
-							progname,
-							strerror(-rc));
+						llapi_error(LLAPI_MSG_WARN, rc,
+							    "delay for bandwidth control failed");
 					rc = 0;
 				}
 			}
@@ -970,8 +983,11 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 	}
 
 	rc = fsync(fd_dst);
-	if (rc < 0)
+	if (rc < 0) {
 		rc = -errno;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "failed to fsync dst file");
+	}
 out:
 	/* Try to avoid page cache pollution after migration. */
 	(void)posix_fadvise(fd_src, 0, 0, POSIX_FADV_DONTNEED);
@@ -11925,8 +11941,8 @@ int lfs_mirror_resync_file(const char *fname, struct ll_ioc_lease *ioc,
 					     start, end, stats_interval_sec,
 					     bandwidth_bytes_sec);
 	if (rc < 0)
-		fprintf(stderr, "%s: '%s' llapi_mirror_resync_many: %s.\n",
-			progname, fname, strerror(-rc));
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "fail to mirror resync '%s'\n", fname);
 
 	rc2 = migrate_set_timestamps(fd, &stbuf);
 	if (rc2 < 0) {
@@ -12478,8 +12494,12 @@ static inline int lfs_mirror_write(int argc, char **argv)
 
 	if (pos & (page_size - 1)) {
 		rc = llapi_mirror_truncate(fd, mirror_id, pos);
-		if (rc < 0)
+		if (rc < 0) {
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "fail to trucate mirror %u of file '%s' to %ld",
+				    mirror_id, fname, pos);
 			goto free_buf;
+		}
 	}
 
 	ioc.lil_mode = LL_LEASE_UNLCK;

lustre/utils/liblustreapi_layout.c
@@ -3293,6 +3293,8 @@ int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 							&mirror_end);
 				if (rc < 0) {
 					free(buf);
+					llapi_error(LLAPI_MSG_ERROR, rc,
+						    "cannot find source mirror");
 					return rc;
 				}
 				src = rc;
@@ -3387,6 +3389,9 @@ do_read:
 		}
 		if (bytes_read < 0) {
 			rc = bytes_read;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "error reading bytes %ld-%ld of mirror %u",
+				    pos, to_read, src);
 			break;
 		}
 		total_bytes_read += bytes_read;

lustre/utils/liblustreapi_mirror.c
@@ -136,6 +136,9 @@ ssize_t llapi_mirror_read(int fd, unsigned int id, void *buf, size_t count,
 
 		if (bytes_read < 0) {
 			result = -errno;
+			llapi_error(LLAPI_MSG_WARN, result,
+				    "fail to pread %ld-%ld of mirror %u",
+				    pos, count, id);
 			break;
 		}
 
@@ -182,6 +185,9 @@ ssize_t llapi_mirror_write(int fd, unsigned int id, const void *buf,
 		bytes_written = pwrite(fd, buf, count, pos);
 		if (bytes_written < 0) {
 			result = -errno;
+			llapi_error(LLAPI_MSG_WARN, result,
+				    "fail to pwrite %ld-%ld of mirror %u",
+				    pos, count, id);
 			break;
 		}
 
@@ -205,8 +211,11 @@ int llapi_mirror_truncate(int fd, unsigned int id, off_t length)
 		return rc;
 
 	rc = ftruncate(fd, length);
-	if (rc < 0)
+	if (rc < 0) {
 		rc = -errno;
+		llapi_error(LLAPI_MSG_WARN, rc,
+			    "fail to ftruncate mirror %u to %ld", id, length);
+	}
 
 	(void) llapi_mirror_clear(fd);
 
@@ -371,6 +380,9 @@ ssize_t llapi_mirror_copy_many(int fd, __u16 src, __u16 *dst, size_t count)
 		} else if (bytes_read < 0) {
 			result = bytes_read;
 			nr = 0;
+			llapi_error(LLAPI_MSG_ERROR, result,
+				    "error reading bytes %ld-%ld of mirror %u",
+				    pos, to_read, src);
 			break;
 		}
 
@@ -469,6 +481,9 @@ int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst, off_t pos,
 			break;
 		} else if (bytes_read < 0) {
 			result = bytes_read;
+			llapi_error(LLAPI_MSG_ERROR, result,
+				    "error reading bytes %ld-%ld of mirror %u",
+				    pos, to_read, src);
 			break;
 		}
 
@@ -480,6 +495,9 @@ int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst, off_t pos,
 						    pos);
 		if (bytes_written < 0) {
 			result = bytes_written;
+			llapi_error(LLAPI_MSG_ERROR, result,
+				    "error writing bytes %ld-%ld of mirror %u",
+				    pos, to_write, dst);
 			break;
 		}
 
@@ -496,8 +514,12 @@ int llapi_mirror_copy(int fd, unsigned int src, unsigned int dst, off_t pos,
 
 	if (result > 0 && pos & (page_size - 1)) {
 		rc = llapi_mirror_truncate(fd, dst, pos);
-		if (rc < 0)
+		if (rc < 0) {
+			llapi_error(LLAPI_MSG_ERROR, result,
+				    "error truncating mirror %u to %ld",
+				    dst, pos);
 			result = rc;
+		}
 	}
 
 	return result;