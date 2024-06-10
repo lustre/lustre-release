lustre/lustre-release

commit c6878334a1a73cef379e26bc985acf3302e35146

LU-17741 gss: fix lsvcgss service for systemd
Add a systemd unit file for lsvcgss service, so that the lsvcgssd
daemon can be handled correctly via systemctl.

Test-Parameters: trivial
Test-Parameters: kerberos=true testlist=sanity-krb5 clientdistro=el9.3 serverdistro=el9.3
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7581996e1e28567415da0827681841ac228ad6c5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54915
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/scripts/lsvcgss_sysd
@@ -1,0 +1,22 @@
+#!/bin/bash
+#
+# lsvcgss      Lustre GSS daemon
+#
+
+# Must be identical to what lsvcgss.service uses
+PIDFILE=/var/run/lsvcgss.pid
+
+# If service is not configured, launch with all mechs
+# -k -- Enable kerberos support
+# -s -- Enable shared key support
+# -z -- Enable gssnull support
+LSVCGSSDARGS="-k -s -z"
+
+# Check for and source configuration file
+[ -f /etc/sysconfig/lsvcgss ] && . /etc/sysconfig/lsvcgss
+
+/usr/sbin/lsvcgssd ${LSVCGSSDARGS}
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo $(pidof lsvcgssd) > $PIDFILE
+
+exit $RETVAL

lustre/utils/gss/svcgssd.c
@@ -178,7 +178,10 @@ sig_die(int signal)
 	/* remove socket */
 	unlink(GSS_SOCKET_PATH);
 	printerr(LL_WARN, "exiting on signal %d\n", signal);
-	exit(1);
+	if (signal == SIGTERM)
+		exit(EXIT_SUCCESS);
+	else
+		exit(EXIT_FAILURE);
 }
 
 static void

lustre.spec.in
@@ -774,6 +774,7 @@ echo '%{_sysconfdir}/ha.d/resource.d/Lustre' >>lustre.files
 # systemd is on redhat, fedora, and suse
 %if %{with systemd}
 echo '%{_unitdir}/lnet.service' >>lustre.files
+echo '%{_unitdir}/lsvcgss.service' >>lustre.files
 %endif
 
 %if "%{_vendor}" == "redhat" || 0%{?openEuler}
@@ -892,6 +893,7 @@ echo '%{_libdir}/lustre/tests/lutf/*' >>lustre-tests.files
 %{_bindir}/lustre_req_history
 %{_bindir}/remove_changelog
 %{_bindir}/remove_updatelog
+%{_bindir}/lsvcgss_sysd
 %endif
 
 %{_bindir}/llobdstat
@@ -1000,16 +1002,19 @@ echo '%{_libdir}/lustre/tests/lutf/*' >>lustre-tests.files
 %post
 %if %{with systemd}
 %systemd_post lnet.service
+%systemd_post lsvcgss.service
 %endif
 
 %preun
 %if %{with systemd}
 %systemd_preun lnet.service
+%systemd_preun lsvcgss.service
 %endif
 
 %postun
 %if %{with systemd}
 %systemd_postun_with_restart lnet.service
+%systemd_postun_with_restart lsvcgss.service
 %endif
 
 %clean

lustre/scripts/Makefile.am
@@ -60,7 +60,8 @@ bin_SCRIPTS   = lfs_migrate
 
 if SERVER
 sbin_SCRIPTS += $(genscripts) lc_mon lhbadm lc_servip
-bin_SCRIPTS  += lustre_req_history remove_changelog remove_updatelog
+bin_SCRIPTS  += lustre_req_history remove_changelog remove_updatelog \
+		lsvcgss_sysd
 
 hadir = $(sysconfdir)/ha.d/resource.d
 ha_SCRIPTS = Lustre.ha_v2
@@ -95,7 +96,7 @@ EXTRA_DIST = lustre_rmmod ldev lc_mon lhbadm \
 	     zfsobj2fid ko2iblnd-probe ksocklnd-config statechange-lustre.sh \
 	     vdev_attach-lustre.sh vdev_remove-lustre.sh vdev_clear-lustre.sh \
 	     bash-completion/lustre bash-completion/lctl bash-completion/lfs \
-	     remove_changelog remove_updatelog
+	     remove_changelog remove_updatelog lsvcgss_sysd
 
 CLEANFILES = $(genscripts)
 

lustre/scripts/systemd/Makefile.am
@@ -3,9 +3,10 @@ genunitfiles = lnet.service
 
 if HAVE_SYSTEMD
 systemdsystemunit_DATA = lnet.service
+systemdsystemunit_SCRIPTS = lsvcgss.service
 endif
 
-EXTRA_DIST = $(addsuffix .in,$(genunitfiles))
+EXTRA_DIST = $(addsuffix .in,$(genunitfiles)) lsvcgss.service
 
 CLEANFILES = $(genunitfiles)
 

lustre/scripts/systemd/lsvcgss.service
@@ -1,0 +1,8 @@
+[Unit]
+Description=Lustre GSS daemon
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/lsvcgss_sysd
+ExecReload=/bin/kill -HUP $MAINPID
+PIDFile=/var/run/lsvcgss.pid

commit f28a7a33a8254fc25c8cb348f87a0c133286393f

LU-17899 gss: lsvcgss service fix
The lsvcgss service can fail to start if the daemon is invoked with
the '-k' option whereas no proper Kerberos configuration is in place
on the server. The daemon should ignore the '-k' option is such case
and try to start the other provided modes if any (SSK, Null).
And in case the daemon is started with the '-s' option (SSK), it
spawns a temporary additional thread to compute the number of rounds
used for Miller-Rabin prime testing. So the lsvcgss_sysd script should
support that.

Fixes: c6878334a1 ("LU-17741 gss: fix lsvcgss service for systemd")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Iba632bd0ea9696ccea52bff5982a4d4e490597a7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55293
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/scripts/lsvcgss_sysd
@@ -17,6 +17,6 @@ LSVCGSSDARGS="-k -s -z"
 
 /usr/sbin/lsvcgssd ${LSVCGSSDARGS}
 RETVAL=$?
-[ $RETVAL -eq 0 ] && echo $(pidof lsvcgssd) > $PIDFILE
+[ $RETVAL -eq 0 ] && echo $(pidof -s lsvcgssd) > $PIDFILE
 
 exit $RETVAL

lustre/utils/gss/svcgssd.c
@@ -304,13 +304,14 @@ main(int argc, char *argv[])
 		if (gssd_check_mechs()) {
 			printerr(LL_ERR,
 				 "ERROR: problem with gssapi library\n");
-			exit(1);
+			ret = -1;
+			goto err_krb;
 		}
 		ret = gss_get_realm(realm);
 		if (ret) {
 			printerr(LL_ERR, "ERROR: no Kerberos realm: %s\n",
 				 error_message(ret));
-			exit(1);
+			goto err_krb;
 		}
 		printerr(LL_WARN, "Kerberos realm: %s\n", krb5_this_realm);
 		if (get_creds &&
@@ -320,7 +321,16 @@ main(int argc, char *argv[])
 				 "unable to obtain root (machine) credentials\n");
 			printerr(LL_ERR,
 				 "do you have a keytab entry for <lustre_xxs>/<your.host>@<YOUR.REALM> in /etc/krb5.keytab?\n");
-			exit(1);
+			ret = -1;
+			goto err_krb;
+		}
+
+err_krb:
+		if (ret) {
+			krb_enabled = 0;
+			printerr(LL_ERR, "ERROR: disabling Kerberos support\n");
+			if (!sk_enabled && !krb_enabled && !null_enabled)
+				exit(EXIT_FAILURE);
 		}
 	}
 