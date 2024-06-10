lustre/lustre-release

commit 668dfb53de8c3ccb9f17ba93a449eb454cf17899

LU-13577 wbc: reimplement mkdir() by using intent lock
This patch reworks mkdir() by using intent lock.
Instead of reint mkdir implementation without any lock returned,
a ibits lock (current PR LOOKUP|PERM) is granted to the client and
cached on the client-side lock namespaces by the mkdir() intent
lock request.

This is also a basic requirement for the coming WBC feature, i.e,
create a new directory and an EX WBC lock is returned from MDT in
intent lock request, then this root WBC directory can be safely
cached on the client under the protection of the root WBC EX lock.

This patch also adds a tuning parameter "llite.*.intent_mkdir" to
enable or disable mkdir() by using intent lock. It is set with 0
by default to disable intent mkdir().

Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I94e4c2f8262d7ffb27d85b5569070049a47354d7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/38647
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/namei.c
@@ -1552,37 +1552,25 @@ unlock:
 	up_read(&rlli->lli_lsm_sem);
 }
 
-static int ll_new_node(struct inode *dir, struct dentry *dchild,
-		       const char *tgt, umode_t mode, __u64 rdev, __u32 opc)
+static int ll_new_node_prepare(struct inode *dir, struct dentry *dchild,
+			       umode_t mode, __u32 opc, bool *encrypt,
+			       const char *tgt, struct md_op_data **op_datap,
+			       struct lmv_user_md **lump, void **datap,
+			       size_t *datalen, struct llcrypt_str *disk_link)
 {
-	struct qstr *name = &dchild->d_name;
-	struct ptlrpc_request *request = NULL;
-	struct md_op_data *op_data = NULL;
-	struct inode *inode = NULL;
 	struct ll_sb_info *sbi = ll_i2sbi(dir);
-	struct llcrypt_str *disk_link = NULL;
-	bool encrypt = false;
-	struct lmv_user_md *lum = NULL;
-	const void *data = NULL;
-	size_t datalen = 0;
+	struct lmv_user_md *lum = *lump;
+	struct md_op_data *op_data = NULL;
 	int err;
 
 	ENTRY;
-	if (unlikely(tgt != NULL)) {
-		disk_link = (struct llcrypt_str *)rdev;
-		rdev = 0;
-		if (!disk_link)
-			RETURN(-EINVAL);
-		data = disk_link->name;
-		datalen = disk_link->len;
-	}
 
-again:
-	op_data = ll_prep_md_op_data(NULL, dir, NULL, name->name,
-				     name->len, 0, opc, NULL);
+	op_data = ll_prep_md_op_data(NULL, dir, NULL, dchild->d_name.name,
+				     dchild->d_name.len, mode, opc, NULL);
 	if (IS_ERR(op_data))
-		GOTO(err_exit, err = PTR_ERR(op_data));
+		RETURN(PTR_ERR(op_data));
 
+	*op_datap = op_data;
 	if (S_ISDIR(mode)) {
 		ll_qos_mkdir_prep(op_data, dir);
 		if ((exp_connect_flags2(ll_i2mdexp(dir)) &
@@ -1609,8 +1597,9 @@ again:
 			lum->lum_max_inherit_rr = lsm->lsm_md_max_inherit_rr;
 			lum->lum_pool_name[0] = 0;
 			op_data->op_bias |= MDS_CREATE_DEFAULT_LMV;
-			data = lum;
-			datalen = sizeof(*lum);
+			*lump = lum;
+			*datap = lum;
+			*datalen = sizeof(*lum);
 		}
 	}
 
@@ -1635,10 +1624,10 @@ again:
 			GOTO(err_exit, err);
 		if (!llcrypt_has_encryption_key(dir))
 			GOTO(err_exit, err = -ENOKEY);
-		encrypt = true;
+		*encrypt = true;
 	}
 
-	if (encrypt) {
+	if (*encrypt) {
 		err = llcrypt_inherit_context(dir, NULL, op_data, false);
 		if (err)
 			GOTO(err_exit, err);
@@ -1674,10 +1663,110 @@ again:
 			if (err)
 				GOTO(err_exit, err);
 
-			data = disk_link->name;
-			datalen = disk_link->len;
+			*datap = disk_link->name;
+			*datalen = disk_link->len;
 		}
 	}
+
+	RETURN(0);
+err_exit:
+	if (!IS_ERR_OR_NULL(op_data)) {
+		ll_finish_md_op_data(op_data);
+		*op_datap = NULL;
+	}
+	if (lum) {
+		OBD_FREE_PTR(lum);
+		*lump = NULL;
+	}
+	RETURN(err);
+}
+
+static int ll_new_node_finish(struct inode *dir, struct dentry *dchild,
+			      bool encrypt, umode_t mode, const char *tgt,
+			      struct inode **inode, struct md_op_data *op_data,
+			      struct ptlrpc_request *request)
+{
+	int err;
+
+	ENTRY;
+
+	ll_update_times(request, dir);
+
+	CFS_FAIL_TIMEOUT(OBD_FAIL_LLITE_NEWNODE_PAUSE, cfs_fail_val);
+
+	err = ll_prep_inode(inode, &request->rq_pill, dchild->d_sb, NULL);
+	if (err)
+		RETURN(err);
+
+	/* must be done before d_instantiate, because it calls
+	 * security_d_instantiate, which means a getxattr if security
+	 * context is not set yet
+	 */
+	err = ll_inode_notifysecctx(*inode,
+				    op_data->op_file_secctx,
+				    op_data->op_file_secctx_size);
+	if (err)
+		RETURN(err);
+
+	d_instantiate(dchild, *inode);
+
+	if (encrypt) {
+		err = ll_set_encflags(*inode, op_data->op_file_encctx,
+				      op_data->op_file_encctx_size, true);
+		if (err)
+			RETURN(err);
+
+		if (S_ISLNK(mode)) {
+			struct ll_inode_info *lli = ll_i2info(*inode);
+
+			/* Cache the plaintext symlink target
+			 * for later use by get_link()
+			 */
+			OBD_ALLOC(lli->lli_symlink_name, strlen(tgt) + 1);
+			/* do not return an error if we cannot
+			 * cache the symlink locally
+			 */
+			if (lli->lli_symlink_name)
+				memcpy(lli->lli_symlink_name,
+				       tgt, strlen(tgt) + 1);
+		}
+	}
+
+	if (!test_bit(LL_SBI_FILE_SECCTX, ll_i2sbi(dir)->ll_flags))
+		err = ll_inode_init_security(dchild, *inode, dir);
+
+	RETURN(err);
+}
+
+static int ll_new_node(struct inode *dir, struct dentry *dchild,
+		       const char *tgt, umode_t mode, __u64 rdev, __u32 opc)
+{
+	struct ptlrpc_request *request = NULL;
+	struct md_op_data *op_data = NULL;
+	struct inode *inode = NULL;
+	struct ll_sb_info *sbi = ll_i2sbi(dir);
+	struct llcrypt_str *disk_link = NULL;
+	bool encrypt = false;
+	struct lmv_user_md *lum = NULL;
+	void *data = NULL;
+	size_t datalen = 0;
+	int err;
+
+	ENTRY;
+	if (unlikely(tgt != NULL)) {
+		disk_link = (struct llcrypt_str *)rdev;
+		rdev = 0;
+		if (!disk_link)
+			RETURN(-EINVAL);
+		data = disk_link->name;
+		datalen = disk_link->len;
+	}
+
+again:
+	err = ll_new_node_prepare(dir, dchild, mode, opc, &encrypt, tgt,
+				  &op_data, &lum, &data, &datalen, disk_link);
+	if (err)
+		GOTO(err_exit, err);
 
 	err = md_create(sbi->ll_md_exp, op_data, data, datalen, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
@@ -1753,53 +1842,10 @@ again:
 	if (err < 0)
 		GOTO(err_exit, err);
 
-	ll_update_times(request, dir);
-
-	CFS_FAIL_TIMEOUT(OBD_FAIL_LLITE_NEWNODE_PAUSE, cfs_fail_val);
-
-	err = ll_prep_inode(&inode, &request->rq_pill, dchild->d_sb, NULL);
+	err = ll_new_node_finish(dir, dchild, encrypt, mode, tgt,
+				 &inode, op_data, request);
 	if (err)
 		GOTO(err_exit, err);
-
-	/* must be done before d_instantiate, because it calls
-	 * security_d_instantiate, which means a getxattr if security
-	 * context is not set yet
-	 */
-	err = ll_inode_notifysecctx(inode,
-				    op_data->op_file_secctx,
-				    op_data->op_file_secctx_size);
-	if (err)
-		GOTO(err_exit, err);
-
-	d_instantiate(dchild, inode);
-
-	if (encrypt) {
-		err = ll_set_encflags(inode, op_data->op_file_encctx,
-				      op_data->op_file_encctx_size, true);
-		if (err)
-			GOTO(err_exit, err);
-
-		if (S_ISLNK(mode)) {
-			struct ll_inode_info *lli = ll_i2info(inode);
-
-			/* Cache the plaintext symlink target
-			 * for later use by get_link()
-			 */
-			OBD_ALLOC(lli->lli_symlink_name, strlen(tgt) + 1);
-			/* do not return an error if we cannot
-			 * cache the symlink locally
-			 */
-			if (lli->lli_symlink_name)
-				memcpy(lli->lli_symlink_name,
-				       tgt, strlen(tgt) + 1);
-		}
-	}
-
-	if (!test_bit(LL_SBI_FILE_SECCTX, sbi->ll_flags)) {
-		err = ll_inode_init_security(dchild, inode, dir);
-		if (err)
-			GOTO(err_exit, err);
-	}
 
 	EXIT;
 err_exit:
@@ -1977,8 +2023,18 @@ clear:
 static int ll_mkdir(struct mnt_idmap *map, struct inode *dir,
 		    struct dentry *dchild, umode_t mode)
 {
+	struct lookup_intent mkdir_it = { .it_op = IT_CREAT };
+	struct ll_sb_info *sbi = ll_i2sbi(dir);
+	struct ptlrpc_request *request = NULL;
+	struct md_op_data *op_data;
+	struct inode *inode = NULL;
+	struct lmv_user_md *lum = NULL;
+	bool encrypt = false;
+	void *data = NULL;
+	size_t datalen = 0;
 	ktime_t kstart = ktime_get();
-	int err;
+	int rc;
+
 	ENTRY;
 
 	/* VFS has locked the inode before calling this */
@@ -1990,16 +2046,60 @@ static int ll_mkdir(struct mnt_idmap *map, struct inode *dir,
 	if (!IS_POSIXACL(dir) || !exp_connect_umask(ll_i2mdexp(dir)))
 		mode &= ~current_umask();
 
-	mode = (mode & (S_IRWXUGO|S_ISVTX)) | S_IFDIR;
+	mode = (mode & (S_IRWXUGO | S_ISVTX)) | S_IFDIR;
+	if (!sbi->ll_intent_mkdir_enabled) {
+		rc = ll_new_node(dir, dchild, NULL, mode, 0, LUSTRE_OPC_MKDIR);
+		GOTO(out_tally, rc);
+	}
 
-	err = ll_new_node(dir, dchild, NULL, mode, 0, LUSTRE_OPC_MKDIR);
-	if (err == 0)
-		ll_stats_ops_tally(ll_i2sbi(dir), LPROC_LL_MKDIR,
+	mkdir_it.it_create_mode = mode;
+	rc = ll_new_node_prepare(dir, dchild, mode, LUSTRE_OPC_MKDIR, &encrypt,
+				 NULL, &op_data, &lum, &data, &datalen, NULL);
+	if (rc)
+		GOTO(out_tally, rc);
+
+	op_data->op_data = data;
+	op_data->op_data_size = datalen;
+	rc = md_intent_lock(sbi->ll_md_exp, op_data, &mkdir_it,
+			    &request, &ll_md_blocking_ast, 0);
+	if (rc)
+		GOTO(out_fini, rc);
+
+	/* dir layout may change */
+	ll_unlock_md_op_lsm(op_data);
+
+	rc = ll_new_node_finish(dir, dchild, encrypt, mode, NULL,
+				&inode, op_data, request);
+	if (rc)
+		GOTO(out_fini, rc);
+
+	if (mkdir_it.it_lock_mode) {
+		__u64 bits = 0;
+
+		LASSERT(it_disposition(&mkdir_it, DISP_LOOKUP_NEG));
+		ll_set_lock_data(sbi->ll_md_exp, inode, &mkdir_it, &bits);
+		if (bits & MDS_INODELOCK_LOOKUP) {
+			if (!ll_d_setup(dchild, false))
+				GOTO(out_fini, rc = -ENOMEM);
+			d_lustre_revalidate(dchild);
+		}
+	}
+
+out_fini:
+	ll_finish_md_op_data(op_data);
+	ll_intent_release(&mkdir_it);
+	ptlrpc_req_finished(request);
+	if (lum)
+		OBD_FREE_PTR(lum);
+
+out_tally:
+	if (rc == 0)
+		ll_stats_ops_tally(sbi, LPROC_LL_MKDIR,
 				   ktime_us_delta(ktime_get(), kstart));
 
 	ll_clear_inode_lock_owner(dir);
 
-	RETURN(err);
+	RETURN(rc);
 }
 
 static int ll_rmdir(struct inode *dir, struct dentry *dchild)

lustre/llite/llite_internal.h
@@ -884,7 +884,8 @@ struct ll_sb_info {
 				 ll_client_common_fill_super_succeeded:1,
 				 ll_checksum_set:1,
 				 ll_inode_cache_enabled:1,
-				 ll_enable_statahead_fname:1;
+				 ll_enable_statahead_fname:1,
+				 ll_intent_mkdir_enabled:1;
 
 	struct lustre_client_ocd ll_lco;
 

lustre/llite/lproc_llite.c
@@ -1239,6 +1239,33 @@ static ssize_t xattr_cache_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(xattr_cache);
 
+static ssize_t intent_mkdir_show(struct kobject *kobj,
+				 struct attribute *attr, char *buf)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_intent_mkdir_enabled);
+}
+
+static ssize_t intent_mkdir_store(struct kobject *kobj, struct attribute *attr,
+				  const char *buffer, size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	bool val;
+	int rc;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	sbi->ll_intent_mkdir_enabled = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(intent_mkdir);
+
 static ssize_t tiny_write_show(struct kobject *kobj,
 			       struct attribute *attr,
 			       char *buf)
@@ -2097,6 +2124,7 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_max_easize.attr,
 	&lustre_attr_default_easize.attr,
 	&lustre_attr_xattr_cache.attr,
+	&lustre_attr_intent_mkdir.attr,
 	&lustre_attr_fast_read.attr,
 	&lustre_attr_tiny_write.attr,
 	&lustre_attr_parallel_dio.attr,

lustre/lmv/lmv_intent.c
@@ -353,7 +353,7 @@ retry:
 		LASSERT(fid_is_zero(&op_data->op_fid2));
 		LASSERT(op_data->op_name != NULL);
 
-		tgt = lmv_locate_tgt(lmv, op_data);
+		tgt = lmv_locate_tgt_create(obd, lmv, op_data);
 		if (IS_ERR(tgt))
 			RETURN(PTR_ERR(tgt));
 	}
@@ -564,7 +564,7 @@ int lmv_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
 	if (it->it_op & (IT_LOOKUP | IT_GETATTR | IT_LAYOUT | IT_GETXATTR))
 		rc = lmv_intent_lookup(exp, op_data, it, reqp, cb_blocking,
 				       extra_lock_flags);
-	else if (it->it_op & IT_OPEN)
+	else if (it->it_op & (IT_OPEN | IT_CREAT))
 		rc = lmv_intent_open(exp, op_data, it, reqp, cb_blocking,
 				     extra_lock_flags);
 	else

lustre/lmv/lmv_internal.h
@@ -209,6 +209,9 @@ static inline bool lmv_dir_retry_check_update(struct md_op_data *op_data)
 	return false;
 }
 
+struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,
+					   struct lmv_obd *lmv,
+					   struct md_op_data *op_data);
 struct lmv_tgt_desc *lmv_locate_tgt(struct lmv_obd *lmv,
 				    struct md_op_data *op_data);
 int lmv_old_layout_lookup(struct lmv_obd *lmv, struct md_op_data *op_data);

lustre/lmv/lmv_obd.c
@@ -2162,6 +2162,61 @@ static bool lmv_qos_exclude(struct lmv_obd *lmv, struct md_op_data *op_data)
 	return prefix != NULL;
 }
 
+struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,
+					   struct lmv_obd *lmv,
+					   struct md_op_data *op_data)
+{
+	struct lmv_tgt_desc *tgt;
+
+	ENTRY;
+
+	tgt = lmv_locate_tgt(lmv, op_data);
+	if (IS_ERR(tgt))
+		RETURN(tgt);
+
+	/* the order to apply policy in mkdir:
+	 * 1. is "lfs mkdir -i N"? mkdir on MDT N.
+	 * 2. is "lfs mkdir -i -1"? mkdir by space usage.
+	 * 3. is starting MDT specified in default LMV? mkdir on MDT N.
+	 * 4. is default LMV space balanced? mkdir by space usage.
+	 *
+	 * If the existing parent or specific MDT selected is deactivated
+	 * with OS_STATFS_NOCREATE then select a different MDT by QOS.
+	 */
+	if (lmv_op_user_specific_mkdir(op_data)) {
+		struct lmv_user_md *lum = op_data->op_data;
+
+		op_data->op_mds = le32_to_cpu(lum->lum_stripe_offset);
+		tgt = lmv_tgt(lmv, op_data->op_mds);
+		if (!tgt)
+			RETURN(ERR_PTR(-ENODEV));
+		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+			GOTO(new_tgt, -EAGAIN);
+	} else if (lmv_op_user_qos_mkdir(op_data)) {
+		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
+		if (IS_ERR(tgt))
+			RETURN(tgt);
+	} else if (lmv_op_default_specific_mkdir(op_data)) {
+		struct lmv_stripe_md *lsm = &op_data->op_default_lso1->lso_lsm;
+
+		op_data->op_mds = lsm->lsm_md_master_mdt_index;
+		tgt = lmv_tgt(lmv, op_data->op_mds);
+		if (!tgt)
+			RETURN(ERR_PTR(-ENODEV));
+		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+			GOTO(new_tgt, -EAGAIN);
+	} else if ((lmv_op_default_qos_mkdir(op_data) &&
+		    !lmv_qos_exclude(lmv, op_data)) ||
+		   tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE) {
+new_tgt:
+		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
+		if (IS_ERR(tgt))
+			RETURN(tgt);
+	}
+
+	RETURN(tgt);
+}
+
 static int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 		      const void *data, size_t datalen, umode_t mode, uid_t uid,
 		      gid_t gid, kernel_cap_t cap_effective, __u64 rdev,
@@ -2193,49 +2248,9 @@ static int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 		op_data->op_new_layout = true;
 	}
 
-	tgt = lmv_locate_tgt(lmv, op_data);
+	tgt = lmv_locate_tgt_create(obd, lmv, op_data);
 	if (IS_ERR(tgt))
 		RETURN(PTR_ERR(tgt));
-
-	/* the order to apply policy in mkdir:
-	 * 1. is "lfs mkdir -i N"? mkdir on MDT N.
-	 * 2. is "lfs mkdir -i -1"? mkdir by space usage.
-	 * 3. is starting MDT specified in default LMV? mkdir on MDT N.
-	 * 4. is default LMV space balanced? mkdir by space usage.
-	 *
-	 * If the existing parent or specific MDT selected is deactivated
-	 * with OS_STATFS_NOCREATE then select a different MDT by QOS.
-	 */
-	if (lmv_op_user_specific_mkdir(op_data)) {
-		struct lmv_user_md *lum = op_data->op_data;
-
-		op_data->op_mds = le32_to_cpu(lum->lum_stripe_offset);
-		tgt = lmv_tgt(lmv, op_data->op_mds);
-		if (!tgt)
-			RETURN(-ENODEV);
-		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
-			GOTO(new_tgt, -EAGAIN);
-	} else if (lmv_op_user_qos_mkdir(op_data)) {
-		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
-		if (IS_ERR(tgt))
-			RETURN(PTR_ERR(tgt));
-	} else if (lmv_op_default_specific_mkdir(op_data)) {
-		struct lmv_stripe_md *lsm = &op_data->op_default_lso1->lso_lsm;
-
-		op_data->op_mds = lsm->lsm_md_master_mdt_index;
-		tgt = lmv_tgt(lmv, op_data->op_mds);
-		if (!tgt)
-			RETURN(-ENODEV);
-		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
-			GOTO(new_tgt, -EAGAIN);
-	} else if ((lmv_op_default_qos_mkdir(op_data) &&
-		    !lmv_qos_exclude(lmv, op_data)) ||
-		   tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE) {
-new_tgt:
-		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
-		if (IS_ERR(tgt))
-			RETURN(PTR_ERR(tgt));
-	}
 
 retry:
 	rc = lmv_fid_alloc(NULL, exp, &op_data->op_fid2, op_data);

lustre/mdc/mdc_locks.c
@@ -439,6 +439,90 @@ err_free_rq:
 	return ERR_PTR(rc);
 }
 
+static struct ptlrpc_request *
+mdc_intent_create_pack(struct obd_export *exp, struct lookup_intent *it,
+		       struct md_op_data *op_data, __u32 acl_bufsize,
+		       __u64 extra_lock_flags)
+{
+	LIST_HEAD(cancels);
+	struct ptlrpc_request *req;
+	struct obd_device *obd = class_exp2obd(exp);
+	struct sptlrpc_sepol *sepol;
+	struct ldlm_intent *lit;
+	int count = 0;
+	int rc;
+
+	ENTRY;
+
+	if (fid_is_sane(&op_data->op_fid1))
+		/* cancel parent's UPDATE lock. */
+		count = mdc_resource_get_unused(exp, &op_data->op_fid1,
+						&cancels, LCK_EX,
+						MDS_INODELOCK_UPDATE);
+
+	req = ptlrpc_request_alloc(class_exp2cliimp(exp),
+				   &RQF_LDLM_INTENT_CREATE);
+	if (req == NULL) {
+		ldlm_lock_list_put(&cancels, l_bl_ast, count);
+		RETURN(ERR_PTR(-ENOMEM));
+	}
+
+	req_capsule_set_size(&req->rq_pill, &RMF_NAME, RCL_CLIENT,
+			     op_data->op_namelen + 1);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_SECCTX_NAME,
+			     RCL_CLIENT, op_data->op_file_secctx_name != NULL ?
+			     strlen(op_data->op_file_secctx_name) + 1 : 0);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_SECCTX, RCL_CLIENT,
+			     op_data->op_file_secctx_size);
+	req_capsule_set_size(&req->rq_pill, &RMF_EADATA, RCL_CLIENT,
+			     op_data->op_data_size);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX, RCL_CLIENT,
+			     op_data->op_file_encctx_size);
+
+	/* get SELinux policy info if any */
+	sepol = sptlrpc_sepol_get(req);
+	if (IS_ERR(sepol)) {
+		ldlm_lock_list_put(&cancels, l_bl_ast, count);
+		GOTO(err_free_rq, rc = PTR_ERR(sepol));
+	}
+	req_capsule_set_size(&req->rq_pill, &RMF_SELINUX_POL, RCL_CLIENT,
+			     sptlrpc_sepol_size(sepol));
+
+	rc = ldlm_prep_enqueue_req(exp, req, &cancels, count);
+	if (rc < 0)
+		GOTO(err_put_sepol, rc);
+
+	/* Pack the intent */
+	lit = req_capsule_client_get(&req->rq_pill, &RMF_LDLM_INTENT);
+	lit->opc = (__u64)it->it_op;
+
+	/* Pack the intent request. */
+	mdc_create_pack(&req->rq_pill, op_data, op_data->op_data,
+			op_data->op_data_size, it->it_create_mode,
+			op_data->op_fsuid, op_data->op_fsgid,
+			op_data->op_cap, 0, sepol);
+
+	sptlrpc_sepol_put(sepol);
+
+	req_capsule_set_size(&req->rq_pill, &RMF_MDT_MD, RCL_SERVER,
+			     obd->u.cli.cl_default_mds_easize);
+	req_capsule_set_size(&req->rq_pill, &RMF_ACL, RCL_SERVER, acl_bufsize);
+	req_capsule_set_size(&req->rq_pill, &RMF_DEFAULT_MDT_MD, RCL_SERVER,
+			     sizeof(struct lmv_user_md));
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_SECCTX,
+			     RCL_SERVER, 0);
+	req_capsule_set_size(&req->rq_pill, &RMF_FILE_ENCCTX, RCL_SERVER, 0);
+
+	ptlrpc_request_set_replen(req);
+	RETURN(req);
+
+err_put_sepol:
+	sptlrpc_sepol_put(sepol);
+err_free_rq:
+	ptlrpc_request_free(req);
+	return ERR_PTR(rc);
+}
+
 #define GA_DEFAULT_EA_NAME_LEN	 20
 #define GA_DEFAULT_EA_VAL_LEN	250
 #define GA_DEFAULT_EA_NUM	 10
@@ -949,7 +1033,7 @@ static int mdc_enqueue_base(struct obd_export *exp,
 		LASSERT(policy == NULL);
 
 		saved_flags |= LDLM_FL_HAS_INTENT;
-		if (it->it_op & (IT_GETATTR | IT_READDIR))
+		if (it->it_op & (IT_GETATTR | IT_READDIR | IT_CREAT))
 			policy = &update_policy;
 		else if (it->it_op & IT_LAYOUT)
 			policy = &layout_policy;
@@ -987,6 +1071,9 @@ resend:
 		lvb_type = LVB_T_LAYOUT;
 	} else if (it->it_op & IT_GETXATTR) {
 		req = mdc_intent_getxattr_pack(exp, it, op_data);
+	} else if (it->it_op == IT_CREAT) {
+		req = mdc_intent_create_pack(exp, it, op_data, acl_bufsize,
+					     extra_lock_flags);
 	} else {
 		LBUG();
 		RETURN(-EINVAL);

lustre/mdt/mdt_handler.c
@@ -4493,6 +4493,7 @@ void mdt_thread_info_reset(struct mdt_thread_info *info)
 	info->mti_big_lmm_used = 0;
 	info->mti_big_acl_used = 0;
 	info->mti_som_strict = 0;
+	info->mti_intent_lock = 0;
 
 	info->mti_spec.no_create = 0;
 	info->mti_spec.sp_rm_entry = 0;
@@ -5105,6 +5106,7 @@ static int mdt_intent_opc(enum ldlm_intent_flags it_opc,
 
 	switch (it_opc) {
 	case IT_OPEN:
+	case IT_CREAT:
 	case IT_OPEN|IT_CREAT:
 		/*
 		 * OCREAT is not a IS_MUTABLE request since the file may
@@ -5252,6 +5254,13 @@ static int mdt_intent_policy(const struct lu_env *env,
 		it = req_capsule_client_get(pill, &RMF_LDLM_INTENT);
 		if (it != NULL) {
 			mdt_ptlrpc_stats_update(req, it->opc);
+			info->mti_intent_lock = 1;
+			/*
+			 * For intent lock request with policy, the ELC locks
+			 * have been cancelled in ldlm_handle_enqueue0().
+			 * Thus set @mti_dlm_req with null here.
+			 */
+			info->mti_dlm_req = NULL;
 			rc = mdt_intent_opc(it->opc, info, lockp, flags);
 			if (rc == 0)
 				rc = ELDLM_OK;

commit 5ed290a235d3479fda3279bf3386f016aade9ba5

LU-13577 wbc: theoretical link with missing destination
While unlikely it would be possible so return -EINVAL here

CoverityID: 425353 ("Null pointer dereferences (FORWARD NULL)")

Fixes: 668dfb53de ("LU-13577 wbc: reimplement mkdir() by using intent lock")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I2a41d1a37820a3bc7b06dff42a4cc09386a88820
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55016
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/namei.c
@@ -1660,6 +1660,8 @@ static int ll_new_node_prepare(struct inode *dir, struct dentry *dchild,
 
 			if (!fakeinode)
 				GOTO(err_exit, err = -ENOMEM);
+			if (!disk_link)
+				GOTO(err_exit, err = -EINVAL);
 			fakeinode->i_sb = dchild->d_sb;
 			fakeinode->i_mode |= S_IFLNK;
 #ifdef IOP_XATTR