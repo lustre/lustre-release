lustre/lustre-release

commit 8b1d2a72f10e8486ff83e4c5a22420232246b582

LU-16967 build: Add in-kernel-ko2iblnd driver
Add in-kernel-ko2iblnd.ko for users of in-kernel OFED
and only build ko2iblnd.ko if an external OFED is available

This allows for building and packaging both an external
(MOFED or OPA) o2ib driver and an in-kernel o2ib driver.

Packaging rules will be written so that only enable one
of the o2iblnd drivers can be installed.

In the case of the in-kernel-ko2iblnd.ko driver a symlink
named ko2iblnd.ko will be created to point to the in-kernel
based o2ib driver which allows for a reasonable migration path
for the majority of users.

It is useful for dist build and test to be able to build
both in-kernel IB and external OFED in the same build.

This also means there would be some install/configure
adjustments that ought to have some discussion.

Test-Parameters: trivial
HPE-bug-id: LUS-11711
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I8105fad0b20c36705d7e14e3ae976bf3d81e9f1b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51915
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

autoMakefile.am
@@ -101,17 +101,30 @@ ldiskfs-sources:
 if LDISKFS_ENABLED
 	$(MAKE) sources -C @LDISKFS_SUBDIR@ || exit $$?
 endif
+if BUILT_IN_KO2IBLND
+	$(MAKE) sources -C lnet/klnds/in-kernel-o2iblnd || exit $$?
+endif
 
 if LINUX
 all-am: modules
 
 modules: undef.h ldiskfs-sources
 	$(MAKE) DEQUOTE_CC_VERSION_TEXT=$(DEQUOTE) \
+	KBUILD_EXTRA_SYMBOLS="@INT_O2IB_SYMBOLS@ @EXTRA_SYMBOLS@" \
 	LDFLAGS= CC="$(CC)" -C $(LINUX_OBJ) \
 	-f $(PWD)/build/Makefile LUSTRE_LINUX_CONFIG=$(LINUX_CONFIG) \
 	LINUXINCLUDE='-I$$(srctree)/arch/$$(SRCARCH)/include -Iarch/$$(SRCARCH)/include/generated -Iinclude $$(if $$(KBUILD_SRC)$$(building_out_of_srctree),-Iinclude2 -I$$(srctree)/include) -I$$(srctree)/arch/$$(SRCARCH)/include/uapi -Iarch/$$(SRCARCH)/include/generated/uapi -I$$(srctree)/include/uapi -Iinclude/generated/uapi -include $(CONFIG_INCLUDE)' \
 	$(MODULE_TARGET)=$(PWD) -o tmp_include_depends -o scripts -o \
 	include/config/MARKER $@
+if EXTERNAL_KO2IBLND
+	$(MAKE) DEQUOTE_CC_VERSION_TEXT=$(DEQUOTE) BUILD_EXT_O2IB=yes \
+	KBUILD_EXTRA_SYMBOLS="$(PWD)/Module.symvers @EXT_O2IB_SYMBOLS@ @EXTRA_SYMBOLS@" \
+	LDFLAGS= CC="$(CC)" -C $(LINUX_OBJ) \
+	-f $(PWD)/build/Makefile LUSTRE_LINUX_CONFIG=$(LINUX_CONFIG) \
+	LINUXINCLUDE='-I$$(srctree)/arch/$$(SRCARCH)/include -Iarch/$$(SRCARCH)/include/generated -Iinclude $$(if $$(KBUILD_SRC)$$(building_out_of_srctree),-Iinclude2 -I$$(srctree)/include) -I$$(srctree)/arch/$$(SRCARCH)/include/uapi -Iarch/$$(SRCARCH)/include/generated/uapi -I$$(srctree)/include/uapi -Iinclude/generated/uapi -include $(CONFIG_INCLUDE)' \
+	$(MODULE_TARGET)=$(PWD)/lnet/klnds/o2iblnd -o tmp_include_depends -o scripts -o \
+	include/config/MARKER $@
+endif # EXTERNAL_KO2IBLND
 endif # LINUX
 
 endif # MODULES

lnet/klnds/in-kernel-o2iblnd/autoMakefile.am
@@ -21,7 +21,7 @@
 #
 
 #
-# Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2004, 2010, Oracle and/or its affiliates. All rights reserved.
 # Use is subject to license terms.
 #
 
@@ -29,4 +29,27 @@
 # This file is part of Lustre, http://www.lustre.org/
 #
 
-SUBDIRS = socklnd gnilnd o2iblnd kfilnd in-kernel-o2iblnd
+if MODULES
+if BUILT_IN_KO2IBLND
+modulenet_DATA = in-kernel-ko2iblnd$(KMODEXT)
+in-kernel-ko2iblnd$(KMODEXT): sources
+if EXTERNAL_KO2IBLND
+else
+install-exec-hook:
+	(cd "$(DESTDIR)$(modulenetdir)"; \
+	 $(LN_S) in-kernel-ko2iblnd$(KMODEXT) ko2iblnd$(KMODEXT))
+uninstall-hook:
+	(cd "$(DESTDIR)$(modulenetdir)"; rm -f ko2iblnd$(KMODEXT))
+endif
+endif
+endif
+
+MOSTLYCLEANFILES = @MOSTLYCLEANFILES@
+EXTRA_DIST = $(in-kernel-ko2iblnd-objs:%.o=%.c) o2iblnd-idl.h o2iblnd.h
+
+all-local: $(in-kernel-ko2iblnd_headers) $(in-kernel-ko2iblnd_sources)
+
+CLEANFILES = $(in-kernel-ko2iblnd_headers) $(in-kernel-ko2iblnd_sources)
+
+clean-local:
+	rm -rf $(in-kernel-ko2iblnd_headers) $(in-kernel-ko2iblnd_sources)

lnet/klnds/o2iblnd/autoMakefile.am
@@ -30,7 +30,7 @@
 #
 
 if MODULES
-if BUILD_O2IBLND
+if EXTERNAL_KO2IBLND
 modulenet_DATA = ko2iblnd$(KMODEXT)
 endif
 endif

.gitignore
@@ -110,3 +110,10 @@ cscope.*
 # doxygen files
 doxygen.*
 *.mod
+
+#
+# LNet generated files
+#
+/lnet/klnds/in-kernel-o2iblnd/o2iblnd*.c
+/lnet/klnds/in-kernel-o2iblnd/o2iblnd*.h
+/lnet/klnds/o2iblnd/Module.symvers

config/lustre-build-linux.m4
@@ -754,12 +754,26 @@ AC_DEFUN([LB_CHECK_LINUX_HEADER], [
 # without serious confusion or complication.
 # ------------------------------------------------------------------------------
 
+#
+## LB2_LINUX_CONFTEST_C
+# $1 - *unique* test case name
+# $2 - mlnx: if MLNXPATH should be included and mlnx rewrites
+#      ofed: ofed rewrites are needed
+# $3 - Program source
+#
 AC_DEFUN([LB2_LINUX_CONFTEST_C], [
 TEST_DIR=${TEST_DIR:-${ac_pwd}/_lpb}
 test -d ${TEST_DIR}/$1_pc || mkdir -p ${TEST_DIR}/$1_pc
 cat confdefs.h - <<_EOF >${TEST_DIR}/$1_pc/$1_pc.c
-$2
+$3
 _EOF
+# for in-kernel test case #defines:
+if test x$2 = "xin_kernel" ; then
+	sed -i --regexp-extended \
+	    -e 's:#define HAVE_OFED_COMPAT_RDMA :// External build only: #define HAVE_OFED_COMPAT_RDMA :g' \
+	    -e 's/([^a-zA-Z0-9_])HAVE_OFED_/\1IN_KERNEL_HAVE_OFED_/g' \
+		${TEST_DIR}/$1_pc/$1_pc.c
+fi
 ])
 
 #
@@ -769,6 +783,7 @@ _EOF
 # $2 - additional build flags (ccflags)
 # $3 - external kernel includes for lnet o2ib|gni
 # $4 - extra symbol in Psuedo.symvers (optional)
+# $5 - optional: external|in_kernel
 #
 AC_DEFUN([LB2_LINUX_CONFTEST_MAKEFILE], [
 	TEST_DIR=${TEST_DIR:-${ac_pwd}/_lpb}
@@ -784,11 +799,18 @@ AC_DEFUN([LB2_LINUX_CONFTEST_MAKEFILE], [
 		echo -e "0x12345678\t${EXT_SYMBOL}\tvmlinux\tEXPORT_SYMBOL\t" > ${PSYM_FILE}
 	fi
 	XTRA_SYM=
-	if test "x$O2IBPATH" != "x"; then
-		if test "x$O2IBPATH" != "x$LINUX_OBJ"; then
-			XTRA_SYM="$O2IBPATH/Module.symvers"
+	NEED_MODULE_TESTED="yes"
+	if test "x$5" = "xexternal"; then
+		XTRA_SYM="$EXT_O2IB_SYMBOLS"
+		if test "x$EXTERNAL_KO2IBLND" = "xno" ; then
+			NEED_MODULE_TESTED="no"
 		fi
 	fi
+	if test "x$5" = "xin_kernel"; then
+		XTRA_SYM="$INT_O2IB_SYMBOLS"
+		if test "x$BUILT_IN_KO2IBLND" = "xno" ; then
+			NEED_MODULE_TESTED="no"
+	fi	fi
 
 	cat - <<_EOF >$file
 # Example command line to manually build source
@@ -847,9 +869,15 @@ _EOF
 
 	# Include the test case in the build, only if the result is not cached
 	AS_VAR_PUSHDEF([lb2_cache_name], [lb_cv_test_$1])
-	AS_IF(AS_VAR_TEST_SET(lb2_cache_name), [], [
-		echo "obj-m += $1_pc/" >>${TEST_DIR}/Makefile
-		LB2_MODULES_COUNT=$((LB2_MODULES_COUNT + 1))
+	AS_IF(AS_VAR_TEST_SET(lb2_cache_name), [
+		# cached - result is known
+	], [
+		if test "x$NEED_MODULE_TESTED" != "xno" ; then
+			echo "obj-m += $1_pc/" >>${TEST_DIR}/Makefile
+			LB2_MODULES_COUNT=$((LB2_MODULES_COUNT + 1))
+		else
+			AS_VAR_SET([lb2_cache_name], [unused])
+		fi
 	])
 	AS_VAR_POPDEF([lb2_cache_name])
 ])
@@ -918,6 +946,8 @@ AC_DEFUN([LB2_LINUX_TEST_COMPILE_ALL], [
 # $4 - extra cflags
 # $5 - external include paths
 # $6 - fake symvers entry
+# $7 - external: for extra ofed path/symvers
+#      in_kernel: for in-kernel-ofed defines
 #
 # NOTICE as all of the test cases are compiled in parallel tests may not
 # depend on the results other tests.
@@ -928,8 +958,8 @@ AC_DEFUN([LB2_LINUX_TEST_SRC], [
 	TEST_DIR=${TEST_DIR:-${ac_pwd}/_lpb}
 	AS_VAR_PUSHDEF([lb_test], [lb_cv_test_$1])
 	# Skip test write and build steps if the result is already known.
-	LB2_LINUX_CONFTEST_C([$1], [LB_LANG_PROGRAM([[$2]], [[$3]])])
-	LB2_LINUX_CONFTEST_MAKEFILE([$1], [$4], [$5], [$6])
+	LB2_LINUX_CONFTEST_C([$1], [$7], [LB_LANG_PROGRAM([[$2]], [[$3]])])
+	LB2_LINUX_CONFTEST_MAKEFILE([$1], [$4], [$5], [$6], [$7])
 	AS_VAR_POPDEF([lb_test])
 ])
 
@@ -940,7 +970,7 @@ AC_DEFUN([LB2_LINUX_TEST_SRC], [
 # $2 - *unique* name matching the LB2_LINUX_TEST_SRC macro
 # $3 - run on success (valid .ko generated)
 # $4 - run on failure (unable to compile)
-# $5 - compile only
+# $5 - optional: module to enforce linking of .ko
 #
 AC_DEFUN([LB2_MSG_LINUX_TEST_RESULT],[
 	TEST_DIR=${TEST_DIR:-${ac_pwd}/_lpb}
@@ -1096,7 +1126,7 @@ AC_DEFUN([LB2_CHECK_LINUX_HEADER_SRC], [
 	UNIQUE_ID=$(echo $1 | tr /. __)
 	AS_VAR_PUSHDEF([lb_test], [lb_cv_test_${UNIQUE_ID}])
 	# Skip test write and build steps if the result is already known.
-	LB2_LINUX_CONFTEST_C([${UNIQUE_ID}], [LB_LANG_PROGRAM([@%:@include <$1>])])
+	LB2_LINUX_CONFTEST_C([${UNIQUE_ID}], [], [LB_LANG_PROGRAM([@%:@include <$1>])])
 	LB2_LINUX_CONFTEST_MAKEFILE([${UNIQUE_ID}], [$2])
 	AS_VAR_POPDEF([lb_test])
 ])
@@ -1112,4 +1142,47 @@ AC_DEFUN([LB2_CHECK_LINUX_HEADER_RESULT], [
 	UNIQUE_ID=$(echo $1 | tr /. __)
 	LB2_MSG_LINUX_TEST_RESULT([for linux header $1], [${UNIQUE_ID}],
 				  [$2], [$3])
+])
+
+#
+# LB2_OFED_TEST_SRC() Used for o2ib in-kernel|external configure tests
+#
+# $1 - *unique* name
+# $2 - global
+# $3 - source
+# $4 - extra cflags
+# $5 - external include paths
+# $6 - fake symvers entry
+# $7 - external: for extra ofed path/symvers
+#      in_kernel: for in-kernel-ofed defines
+#
+AC_DEFUN([LB2_OFED_TEST_SRC], [
+	LB2_LINUX_TEST_SRC([external_$1], [$2], [$3], [$4], [$5], [$6], [external])
+	LB2_LINUX_TEST_SRC([in_kernel_$1], [$2], [$3], [$4], [], [$6], [in_kernel])
+])
+
+#
+# LB2_OFED_TEST_RESULTS() Used for o2ib in-kernel|external configure tests
+#
+# $1 - Message
+# $2 - *unique* name matching the LB2_LINUX_TEST_SRC macro
+# $3 - run on success (compile success, optional .ko generated)
+# $4 - optional: 'module' to enforce .ko successfully built.
+#
+# checking if 'rdma_create_id' wants four args mlnx... no
+#
+AC_DEFUN([LB2_OFED_TEST_RESULTS], [
+	LB2_MSG_LINUX_TEST_RESULT(
+		[(external) if $1],
+		[external_$2],
+		[AC_DEFINE_UNQUOTED([$3], 1, [(external) $1])],
+		[], # else statement not available
+		[$4])
+
+	# in-kernel tests ... NOTE: IN_KERNEL_ here matches sed in Makefile.in
+	LB2_MSG_LINUX_TEST_RESULT(
+		[(in-kernel) if $1], [in_kernel_$2],
+		[AC_DEFINE_UNQUOTED([IN_KERNEL_$3], 1, [(in kernel) $1])],
+		[], # else statement not available
+		[$4])
 ])

lnet/klnds/Makefile.in
@@ -1,5 +1,8 @@
 @BUILD_GNILND_TRUE@obj-m += gnilnd/
-@BUILD_O2IBLND_TRUE@obj-m += o2iblnd/
+@BUILT_IN_KO2IBLND_TRUE@obj-m += in-kernel-o2iblnd/
+ifneq ($(BUILD_EXT_O2IB),)
+@EXTERNAL_KO2IBLND_TRUE@obj-m += o2iblnd/
+endif
 @BUILD_KFILND_TRUE@obj-m += kfilnd/
 obj-m += socklnd/
 

lnet/klnds/in-kernel-o2iblnd/Makefile.in
@@ -1,0 +1,28 @@
+MODULES := in-kernel-ko2iblnd
+in-kernel-ko2iblnd-objs := o2iblnd.o o2iblnd_cb.o o2iblnd_modparams.o
+
+in-kernel-ko2iblnd_headers := o2iblnd.h o2iblnd-idl.h
+in-kernel-ko2iblnd_sources := o2iblnd.c o2iblnd_cb.c o2iblnd_modparams.c
+
+sources: $(in-kernel-ko2iblnd_headers) $(in-kernel-ko2iblnd_sources)
+
+o2ib_sed_flags = --regexp-extended \
+	-e 's/([^a-zA-Z0-9_])HAVE_OFED_/\1IN_KERNEL_HAVE_OFED_/g'
+
+o2iblnd-idl.h: ../o2iblnd/o2iblnd-idl.h
+	sed $(strip $(o2ib_sed_flags)) $< > $@
+
+o2iblnd.c: ../o2iblnd/o2iblnd.c
+	sed $(strip $(o2ib_sed_flags)) $< > $@
+
+o2iblnd.h: ../o2iblnd/o2iblnd.h
+	sed $(strip $(o2ib_sed_flags)) $< > $@
+
+o2iblnd_cb.c: ../o2iblnd/o2iblnd_cb.c
+	sed $(strip $(o2ib_sed_flags)) $< > $@
+
+o2iblnd_modparams.c: ../o2iblnd/o2iblnd_modparams.c
+	sed $(strip $(o2ib_sed_flags)) $< > $@
+
+@INCLUDE_RULES@
+

commit e27b5f1c31c2eccb6fc87c9eab8c95497e3814c8

LU-17832 gni: build should not collapse extra symbols
cray-obs spec files (ari,gem,dmp) define:
   KBUILD_EXTRA_SYMBOLS and GNICPPFLAGS

When building kgnilnd the environment variable needs to be passed
through to make.

HPE-bug-id: LUS-12269
Test-Parameters: trivial
Fixes: 8b1d2a72f1 ("LU-16967 build: Add in-kernel-ko2iblnd driver")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Icc7ac33138300bf3836082a014daf580a1632436
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55052
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Patrick Farrell <patrick.farrell@oracle.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

autoMakefile.am
@@ -141,7 +141,7 @@ all-am: modules
 
 modules: undef.h ldiskfs-sources
 	$(MAKE) DEQUOTE_CC_VERSION_TEXT=$(DEQUOTE) \
-	KBUILD_EXTRA_SYMBOLS="@INT_O2IB_SYMBOLS@ @EXTRA_SYMBOLS@" \
+	KBUILD_EXTRA_SYMBOLS="@INT_O2IB_SYMBOLS@ @EXTRA_SYMBOLS@ $(KBUILD_EXTRA_SYMBOLS)" \
 	LDFLAGS= CC="$(CC)" -C $(LINUX_OBJ) \
 	-f $(PWD)/build/Makefile LUSTRE_LINUX_CONFIG=$(LINUX_CONFIG) \
 	LINUXINCLUDE='-I$$(srctree)/arch/$$(SRCARCH)/include -Iarch/$$(SRCARCH)/include/generated -Iinclude $$(if $$(KBUILD_SRC)$$(building_out_of_srctree),-Iinclude2 -I$$(srctree)/include) -I$$(srctree)/arch/$$(SRCARCH)/include/uapi -Iarch/$$(SRCARCH)/include/generated/uapi -I$$(srctree)/include/uapi -Iinclude/generated/uapi -include $(CONFIG_INCLUDE)' \

lnet/autoconf/lustre-lnet.m4
@@ -911,7 +911,8 @@ AC_ARG_ENABLE([gni],
 AC_MSG_RESULT([$enable_gni])
 
 AS_IF([test "x$enable_gni" = xyes], [
-	# GNICPPFLAGS was set in spec file
+	# GNICPPFLAGS and KBUILD_EXTRA_SYMBOLS were set in spec file
+	# to include the additional module dependancies of gni kernel driver
 	EXTRA_KCFLAGS_save="$EXTRA_KCFLAGS"
 	EXTRA_KCFLAGS="$EXTRA_KCFLAGS $GNICPPFLAGS"
 	LB_CHECK_COMPILE([if GNI kernel headers are present],