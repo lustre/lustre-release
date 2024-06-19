lustre/lustre-release

commit 1dbcd0bab881fac38d8a5e4ef1559f12618f8f0e

LU-12998 mds: add no_create parameter to stop creates
Add an target tunable parameter and mount option "no_create" to
disable new *directory* creation on an MDT.  This sends the
flag OS_STATFS_NOCREATE to the clients, and the DNE MDT space
balance will avoid selecting that MDT when creating a new
subdirectory, without disabling access to existing files/dirs.

This allows "soft disabling" an MDT in advance of storage
upgrades to minimize new directories and files created on that
MDT, reduce future migration, and/or backup/restore workload.

As yet it does not totally disable *file* creation on the MDT,
but it may be extended to do so in the future.

This is analogous to the "no_precreate" option that was added
on the OSTs, and "no_create" has been added to the OSTs for
consistency ("no_precreate" is kept for compatibility for now).

Test-Parameters: testlist=conf-sanity env=ONLY=112b,ONLY_REPEAT=50
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I53cfb48ade2f844b18bfc630e7fcea6de9ce7057
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47124
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -563,6 +563,16 @@ static int lmv_disconnect(struct obd_export *exp)
 	RETURN(rc);
 }
 
+static void lmv_statfs_update(struct lmv_obd *lmv, struct lmv_tgt_desc *tgt,
+			      struct obd_statfs *osfs)
+{
+	spin_lock(&lmv->lmv_lock);
+	tgt->ltd_statfs = *osfs;
+	tgt->ltd_statfs_age = ktime_get_seconds();
+	spin_unlock(&lmv->lmv_lock);
+	set_bit(LQ_DIRTY, &lmv->lmv_qos.lq_flags);
+}
+
 static int lmv_fid2path(struct obd_export *exp, int len, void *karg,
 			void __user *uarg)
 {
@@ -909,9 +919,9 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 				0);
 		if (rc)
 			RETURN(rc);
+		lmv_statfs_update(lmv, tgt, &stat_buf);
 		if (copy_to_user(data->ioc_pbuf1, &stat_buf,
-				 min((int) data->ioc_plen1,
-				     (int) sizeof(stat_buf))))
+				 min_t(int, data->ioc_plen1, sizeof(stat_buf))))
 			RETURN(-EFAULT);
 		break;
 	}
@@ -1363,7 +1373,7 @@ out_free_temp:
 	RETURN(rc);
 }
 
-static int lmv_statfs_update(void *cookie, int rc)
+static int lmv_statfs_cb(void *cookie, int rc)
 {
 	struct obd_info *oinfo = cookie;
 	struct obd_device *obd = oinfo->oi_obd;
@@ -1375,13 +1385,8 @@ static int lmv_statfs_update(void *cookie, int rc)
 	 * NB: don't deactivate TGT upon error, because we may not trigger async
 	 * statfs any longer, then there is no chance to activate TGT.
 	 */
-	if (!rc) {
-		spin_lock(&lmv->lmv_lock);
-		tgt->ltd_statfs = *osfs;
-		tgt->ltd_statfs_age = ktime_get_seconds();
-		spin_unlock(&lmv->lmv_lock);
-		set_bit(LQ_DIRTY, &lmv->lmv_qos.lq_flags);
-	}
+	if (!rc)
+		lmv_statfs_update(lmv, tgt, osfs);
 
 	return rc;
 }
@@ -1392,7 +1397,7 @@ int lmv_statfs_check_update(struct obd_device *obd, struct lmv_tgt_desc *tgt)
 	struct obd_info oinfo = {
 		.oi_obd	= obd,
 		.oi_tgt = tgt,
-		.oi_cb_up = lmv_statfs_update,
+		.oi_cb_up = lmv_statfs_cb,
 	};
 	int rc;
 
@@ -1555,7 +1560,8 @@ static struct lu_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv,
 		GOTO(unlock, tgt = ERR_PTR(rc));
 
 	lmv_foreach_tgt(lmv, tgt) {
-		if (!tgt->ltd_exp || !tgt->ltd_active) {
+		if (!tgt->ltd_exp || !tgt->ltd_active ||
+		    (tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)) {
 			tgt->ltd_qos.ltq_usable = 0;
 			continue;
 		}
@@ -1572,7 +1578,7 @@ static struct lu_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv,
 		total_usable++;
 	}
 
-	/* If current MDT has above-average space and dir is not aleady using
+	/* If current MDT has above-average space and dir is not already using
 	 * round-robin to spread across more MDTs, stay on the parent MDT
 	 * to avoid creating needless remote MDT directories.  Remote dirs
 	 * close to the root balance space more effectively than bottom dirs,
@@ -1627,7 +1633,8 @@ static struct lu_tgt_desc *lmv_locate_tgt_rr(struct lmv_obd *lmv)
 		index = (i + lmv->lmv_qos_rr_index) %
 			lmv->lmv_mdt_descs.ltd_tgts_size;
 		tgt = lmv_tgt(lmv, index);
-		if (!tgt || !tgt->ltd_exp || !tgt->ltd_active)
+		if (!tgt || !tgt->ltd_exp || !tgt->ltd_active ||
+		    (tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
 			continue;
 
 		lmv->lmv_qos_rr_index = (tgt->ltd_index + 1) %
@@ -1660,7 +1667,8 @@ static struct lu_tgt_desc *lmv_locate_tgt_lf(struct lmv_obd *lmv)
 		GOTO(unlock, tgt = ERR_PTR(-EAGAIN));
 
 	lmv_foreach_tgt(lmv, tgt) {
-		if (!tgt->ltd_exp || !tgt->ltd_active) {
+		if (!tgt->ltd_exp || !tgt->ltd_active ||
+		    (tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)) {
 			tgt->ltd_qos.ltq_usable = 0;
 			continue;
 		}
@@ -1751,7 +1759,7 @@ lmv_locate_tgt_by_name(struct lmv_obd *lmv, struct lmv_stripe_object *lso,
  * which is set outside, and if dir is migrating, 'op_data->op_new_layout'
  * indicates whether old or new layout is used to locate.
  *
- * For plain direcotry, it just locate the MDT of op_data->op_fid1.
+ * For plain directory, it just locate the MDT of op_data->op_fid1.
  *
  * \param[in] lmv		LMV device
  * \param[in/out] op_data	client MD stack parameters, name, namelen etc,
@@ -1967,7 +1975,8 @@ static struct lu_tgt_desc *lmv_locate_tgt_by_space(struct lmv_obd *lmv,
 	if (tgt == ERR_PTR(-EAGAIN)) {
 		if (ltd_qos_is_balanced(&lmv->lmv_mdt_descs) &&
 		    !lmv_op_default_rr_mkdir(op_data) &&
-		    !lmv_op_user_qos_mkdir(op_data))
+		    !lmv_op_user_qos_mkdir(op_data) &&
+		    !(tmp->ltd_statfs.os_state & OS_STATFS_NOCREATE))
 			/* if not necessary, don't create remote directory. */
 			tgt = tmp;
 		else
@@ -1978,6 +1987,12 @@ static struct lu_tgt_desc *lmv_locate_tgt_by_space(struct lmv_obd *lmv,
 
 	if (!IS_ERR(tgt))
 		op_data->op_mds = tgt->ltd_index;
+
+	/* If space balance was called because the original target was marked
+	 * NOCREATE, periodically check whether the state has changed.
+	 */
+	if (tmp != tgt && tmp->ltd_statfs.os_state & OS_STATFS_NOCREATE)
+		lmv_statfs_check_update(lmv2obd_dev(lmv), tmp);
 
 	return tgt;
 }
@@ -2022,6 +2037,9 @@ int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 	 * 2. is "lfs mkdir -i -1"? mkdir by space usage.
 	 * 3. is starting MDT specified in default LMV? mkdir on MDT N.
 	 * 4. is default LMV space balanced? mkdir by space usage.
+	 *
+	 * If the existing parent or specific MDT selected is deactivated
+	 * with OS_STATFS_NOCREATE then select a different MDT by QOS.
 	 */
 	if (lmv_op_user_specific_mkdir(op_data)) {
 		struct lmv_user_md *lum = op_data->op_data;
@@ -2030,6 +2048,8 @@ int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 		tgt = lmv_tgt(lmv, op_data->op_mds);
 		if (!tgt)
 			RETURN(-ENODEV);
+		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+			GOTO(new_tgt, -EAGAIN);
 	} else if (lmv_op_user_qos_mkdir(op_data)) {
 		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
 		if (IS_ERR(tgt))
@@ -2041,7 +2061,11 @@ int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 		tgt = lmv_tgt(lmv, op_data->op_mds);
 		if (!tgt)
 			RETURN(-ENODEV);
-	} else if (lmv_op_default_qos_mkdir(op_data)) {
+		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+			GOTO(new_tgt, -EAGAIN);
+	} else if (lmv_op_default_qos_mkdir(op_data) ||
+		   tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE) {
+new_tgt:
 		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
 		if (IS_ERR(tgt))
 			RETURN(PTR_ERR(tgt));

lustre/lod/lod_object.c
@@ -2091,6 +2091,9 @@ static int lod_mdt_alloc_specific(const struct lu_env *env,
 				/* this OSP doesn't feel well */
 				continue;
 
+			if (tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)
+				continue;
+
 			rc = dt_fid_alloc(env, tgt_dt, &fid, NULL, NULL);
 			if (rc < 0)
 				continue;
@@ -7041,9 +7044,11 @@ static bool lod_sel_osts_allowed(const struct lu_env *env,
 
 		if (sfs->os_state & OS_STATFS_ENOSPC ||
 		    sfs->os_state & OS_STATFS_READONLY ||
+		    sfs->os_state & OS_STATFS_NOCREATE ||
 		    sfs->os_state & OS_STATFS_DEGRADED) {
-			CDEBUG(D_LAYOUT, "ost %d is not availble for SEL "
-			       "extension, state %u\n", index, sfs->os_state);
+			CDEBUG(D_LAYOUT,
+			       "OST%04x unusable for SEL extension, state %x\n",
+			       index, sfs->os_state);
 			ret = false;
 			break;
 		}

lustre/include/lu_target.h
@@ -174,7 +174,8 @@ struct lu_target {
 				 lut_no_reconstruct:1,
 				 /* enforce recovery for local clients */
 				 lut_local_recovery:1,
-				 lut_cksum_t10pi_enforce:1;
+				 lut_cksum_t10pi_enforce:1,
+				 lut_no_create:1;
 	/* checksum types supported on this node */
 	enum cksum_types	 lut_cksum_types_supported;
 	/** last_rcvd file */

lustre/include/lustre_disk.h
@@ -83,7 +83,6 @@
 #define LMD_PARAMS_MAXLEN	4096
 
 enum lmd_flags {
-	LMD_FLG_SERVER		= 0,	/* Mounting a server */
 	LMD_FLG_CLIENT,			/* Mounting a client */
 	LMD_FLG_SKIP_LFSCK,		/* NOT auto resume LFSCK when mount */
 	LMD_FLG_ABORT_RECOV,		/* Abort recovery */
@@ -95,15 +94,14 @@ enum lmd_flags {
 					 */
 	LMD_FLG_WRITECONF,		/* Rewrite config log */
 	LMD_FLG_NOIR,			/* NO imperative recovery */
-	LMD_FLG_NOSCRUB,			/* Do not trigger scrub automatically */
+	LMD_FLG_NOSCRUB,		/* Do not trigger scrub automatically */
 	LMD_FLG_MGS,			/* Also start MGS along with server */
-	LMD_FLG_IAM,			/* IAM dir */
 	LMD_FLG_NO_PRIMNODE,		/* all nodes are service nodes */
 	LMD_FLG_VIRGIN,			/* the service registers first time */
 	LMD_FLG_UPDATE,			/* update parameters */
 	LMD_FLG_HSM,			/* Start coordinator */
 	LMD_FLG_DEV_RDONLY,		/* discard modification quitely */
-	LMD_FLG_NO_PRECREATE,		/* do not allow OST object creation */
+	LMD_FLG_NO_CREATE,		/* prevent MDT/OST object creation */
 	LMD_FLG_LOCAL_RECOV,		/* force recovery for local clients */
 	LMD_FLG_ABORT_RECOV_MDT,	/* Abort recovery between MDTs */
 	LMD_FLG_NO_LOCAL_LOGS,		/* Use config logs from MGS */

lustre/include/obd.h
@@ -439,7 +439,7 @@ struct lmv_obd {
 	struct kobject		*lmv_tgts_kobj;
 	void			*lmv_cache;
 
-	__u32			lmv_qos_rr_index;
+	__u32			lmv_qos_rr_index; /* next round-robin MDT idx */
 };
 
 #define lmv_mdt_count	lmv_mdt_descs.ltd_lmv_desc.ld_tgt_count

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -264,7 +264,7 @@ typedef struct statx lstatx_t;
 enum obd_statfs_state {
 	OS_STATFS_DEGRADED	= 0x00000001, /**< RAID degraded/rebuilding */
 	OS_STATFS_READONLY	= 0x00000002, /**< filesystem is read-only */
-	OS_STATFS_NOPRECREATE	= 0x00000004, /**< no object precreation */
+	OS_STATFS_NOCREATE	= 0x00000004, /**< no object creation */
 	OS_STATFS_UNUSED1	= 0x00000008, /**< obsolete 1.6, was EROFS=30 */
 	OS_STATFS_UNUSED2	= 0x00000010, /**< obsolete 1.6, was EROFS=30 */
 	OS_STATFS_ENOSPC	= 0x00000020, /**< not enough free space */
@@ -272,6 +272,9 @@ enum obd_statfs_state {
 	OS_STATFS_SUM		= 0x00000100, /**< aggregated for all tagrets */
 	OS_STATFS_NONROT	= 0x00000200, /**< non-rotational device */
 };
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 20, 53, 0)
+#define OS_STATFS_NOPRECREATE OS_STATFS_NOCREATE
+#endif
 
 /** filesystem statistics/attributes for target device */
 struct obd_statfs {

lustre/lod/lod_qos.c
@@ -62,8 +62,8 @@ static inline int lod_statfs_check(struct lu_tgt_descs *ltd,
 	if (sfs->os_state & OS_STATFS_READONLY)
 		return -EROFS;
 
-	/* object precreation is skipped on targets with max_create_count=0 */
-	if (sfs->os_state & OS_STATFS_NOPRECREATE)
+	/* object creation is skipped on the OST with max_create_count=0 */
+	if (!ltd->ltd_is_mdt && sfs->os_state & OS_STATFS_NOCREATE)
 		return -ENOBUFS;
 
 	return 0;
@@ -999,6 +999,9 @@ repeat_find:
 		if (lod_statfs_check(ltd, mdt))
 			continue;
 
+		if (mdt->ltd_statfs.os_state & OS_STATFS_NOCREATE)
+			continue;
+
 		/* try to use another OSP if this one is degraded */
 		if (mdt->ltd_statfs.os_state & OS_STATFS_DEGRADED &&
 		    !use_degraded) {
@@ -1858,7 +1861,8 @@ int lod_mdt_alloc_qos(const struct lu_env *env, struct lod_object *lo,
 		if (mdt->ltd_discon || lod_statfs_check(ltd, mdt))
 			continue;
 
-		if (mdt->ltd_statfs.os_state & OS_STATFS_DEGRADED)
+		if (mdt->ltd_statfs.os_state &
+		    (OS_STATFS_DEGRADED | OS_STATFS_NOCREATE))
 			continue;
 
 		mdt->ltd_qos.ltq_usable = 1;

lustre/mdt/mdt_handler.c
@@ -573,6 +573,8 @@ static int mdt_statfs(struct tgt_session_info *tsi)
 				   osfs->os_bsize - 1) >> current_blockbits));
 
 	tgt_grant_sanity_check(mdt->mdt_lu_dev.ld_obd, __func__);
+	if (mdt->mdt_lut.lut_no_create)
+		osfs->os_state |= OS_STATFS_NOCREATE;
 	CDEBUG(D_CACHE, "%llu blocks: %llu free, %llu avail; "
 	       "%llu objects: %llu free; state %x\n",
 	       osfs->os_blocks, osfs->os_bfree, osfs->os_bavail,
@@ -6179,6 +6181,8 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 		obd_obt_init(obd);
 		if (test_bit(LMD_FLG_SKIP_LFSCK, lsi->lsi_lmd->lmd_flags))
 			m->mdt_skip_lfsck = 1;
+		if (test_bit(LMD_FLG_NO_CREATE, lsi->lsi_lmd->lmd_flags))
+			m->mdt_lut.lut_no_create = 1;
 	}
 
 	/* Just try to get a DoM lock by default. Otherwise, having a group

commit 6dbb4c6c826133452c8c2da3937711c2030f99eb

LU-12998 lod: statfs upon nocreate check
lod_declare_create() checks whether directory create target MDT is
current MDT, this may happen if nocreate is set on some MDT. Upon
such mismatch, call dt_statfs() to fetch latest statfs to know
whether nocreate is set.

lmv_create() will choose another MDT if target MDT is set with
nocreate, but in case the flag is cleared, call obd_statfs() to fetch
cached statfs and check again.

Fixes: 1dbcd0bab88 (LU-12998 mds: add no_create parameter to stop creates)
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I2575d15416968554c66d40dcf18ecca2a06c7a37
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53437
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -2131,6 +2131,17 @@ static struct lu_tgt_desc *lmv_locate_tgt_by_space(struct lmv_obd *lmv,
 	return tgt;
 }
 
+static bool lmv_tgt_nocreate(struct lmv_obd *lmv, struct lmv_tgt_desc *tgt)
+{
+	if (likely(!(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)))
+		return false;
+
+	obd_statfs(NULL, tgt->ltd_exp, &tgt->ltd_statfs,
+		   ktime_get_seconds() -
+			lmv->lmv_mdt_descs.ltd_lmv_desc.ld_qos_maxage, 0);
+	return tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE;
+}
+
 static bool lmv_qos_exclude(struct lmv_obd *lmv, struct md_op_data *op_data)
 {
 	const char *name = op_data->op_name;
@@ -2190,7 +2201,7 @@ struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,
 		tgt = lmv_tgt(lmv, op_data->op_mds);
 		if (!tgt)
 			RETURN(ERR_PTR(-ENODEV));
-		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+		if (unlikely(lmv_tgt_nocreate(lmv, tgt)))
 			GOTO(new_tgt, -EAGAIN);
 	} else if (lmv_op_user_qos_mkdir(op_data)) {
 		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
@@ -2203,11 +2214,11 @@ struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,
 		tgt = lmv_tgt(lmv, op_data->op_mds);
 		if (!tgt)
 			RETURN(ERR_PTR(-ENODEV));
-		if (unlikely(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE))
+		if (unlikely(lmv_tgt_nocreate(lmv, tgt)))
 			GOTO(new_tgt, -EAGAIN);
 	} else if ((lmv_op_default_qos_mkdir(op_data) &&
 		    !lmv_qos_exclude(lmv, op_data)) ||
-		   tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE) {
+		   unlikely(lmv_tgt_nocreate(lmv, tgt))) {
 new_tgt:
 		tgt = lmv_locate_tgt_by_space(lmv, op_data, tgt);
 		if (IS_ERR(tgt))

lustre/lod/lod_object.c
@@ -6326,47 +6326,42 @@ static int lod_declare_create(const struct lu_env *env, struct dt_object *dt,
 		struct lu_buf buf = { NULL };
 
 		ss = lu_site2seq(dt->do_lu.lo_dev->ld_site);
-
-		/* If the parent has default stripeEA, and client
-		 * did not find it before sending create request,
-		 * then MDT will return -EREMOTE, and client will
-		 * retrieve the default stripeEA and re-create the
-		 * sub directory.
-		 *
-		 * Note: if dah_eadata != NULL, it means creating the
-		 * striped directory with specified stripeEA, then it
-		 * should ignore the default stripeEA */
-		if (hint != NULL && hint->dah_eadata == NULL) {
+		if (hint && (!hint->dah_eadata || hint->dah_eadata_is_dmv)) {
 			if (CFS_FAIL_CHECK(OBD_FAIL_MDS_STALE_DIR_LAYOUT))
 				GOTO(out, rc = -EREMOTE);
-
-			if (lo->ldo_dir_stripe_offset != LMV_OFFSET_DEFAULT &&
-			    lo->ldo_dir_stripe_offset != ss->ss_node_id) {
-				struct lod_device *lod;
-				struct lu_tgt_desc *mdt = NULL;
-				bool found_mdt = false;
-
-				lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
-				lod_foreach_mdt(lod, mdt) {
-					if (mdt->ltd_index ==
-						lo->ldo_dir_stripe_offset) {
-						found_mdt = true;
-						break;
-					}
-				}
-
-				/* If the MDT indicated by stripe_offset can be
-				 * found, then tell client to resend the create
-				 * request to the correct MDT, otherwise return
-				 * error to client */
-				if (found_mdt)
-					GOTO(out, rc = -EREMOTE);
-				else
-					GOTO(out, rc = -EINVAL);
-			}
-		} else if (hint && hint->dah_eadata) {
+		} else if (hint) {
+			LASSERT(hint->dah_eadata && !hint->dah_eadata_is_dmv);
 			buf.lb_buf = (void *)hint->dah_eadata;
 			buf.lb_len = hint->dah_eadata_len;
+		}
+
+
+		/* if dir target MDT is not current MDT, it's possible that
+		 * directory creation is disabled on the target MDT.
+		 */
+		if (lo->ldo_dir_stripe_offset != LMV_OFFSET_DEFAULT &&
+		    lo->ldo_dir_stripe_offset != ss->ss_node_id) {
+			struct lod_device *lod;
+			struct lu_tgt_desc *mdt = NULL;
+			bool no_create = false;
+
+			lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
+			rc = -EINVAL;
+			lod_foreach_mdt(lod, mdt) {
+				if (mdt->ltd_index ==
+				    lo->ldo_dir_stripe_offset) {
+					rc = -EPROTO;
+					/* refresh statfs */
+					dt_statfs(env, mdt->ltd_tgt,
+						  &mdt->ltd_statfs);
+					no_create = (mdt->ltd_statfs.os_state &
+						     OS_STATFS_NOCREATE);
+					break;
+				}
+			}
+
+			if (!no_create)
+				GOTO(out, rc);
 		}
 
 		rc = lod_declare_dir_striping_create(env, dt, attr, &buf, dof,