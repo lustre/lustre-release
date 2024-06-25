lustre/lustre-release

commit 28c366cee6dd1ec8293e0a5aed61c9b15d8b9a96

LU-17218 ofd: improve filter_fid upgrade compatibility
filter_fid could be expanded in later Lustre version, and with
upgrade then downgrade process, the filter_fid EA on disk
could has been expanded during upgrade, and won't work after
the downgrade.

This patch improves this process by allocating bigger buffer to
hold the expanded filter_fid EA then trims the unrecognizable
fileds off.

Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I4c99f1d9f3962d46ebf9e9b799988ff3dba4f919
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52798
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>

lustre/ofd/ofd_objects.c
@@ -145,6 +145,20 @@ int ofd_object_ff_load(const struct lu_env *env, struct ofd_object *fo)
 	buf->lb_buf = ff;
 	buf->lb_len = sizeof(*ff);
 	rc = dt_xattr_get(env, ofd_object_child(fo), buf, XATTR_NAME_FID);
+	if (rc == -ERANGE) {
+		struct filter_fid *ff_new;
+
+		OBD_ALLOC(ff_new, sizeof(*ff) + FILTER_FID_EXTRA_SIZE);
+		if (!ff_new)
+			return -ENOMEM;
+		buf->lb_buf = ff_new;
+		buf->lb_len = sizeof(*ff) + FILTER_FID_EXTRA_SIZE;
+		rc = dt_xattr_get(env, ofd_object_child(fo), buf,
+				  XATTR_NAME_FID);
+		if (rc > 0)
+			memcpy(ff, ff_new, sizeof(*ff));
+		OBD_FREE(ff_new, sizeof(*ff) + FILTER_FID_EXTRA_SIZE);
+	}
 	if (rc < 0)
 		return rc;
 

lustre/ofd/ofd_internal.h
@@ -179,6 +179,13 @@ static inline char *ofd_name(struct ofd_device *ofd)
 	return ofd->ofd_dt_dev.dd_lu_dev.ld_obd->obd_name;
 }
 
+/**
+ * for compatibility, filter_fid could occupy more space in newer version and
+ * downgraded Lustre would fail reading it with -ERANGE, so it can read it
+ * again with more space to hold it.
+ */
+#define FILTER_FID_EXTRA_SIZE  32
+
 struct ofd_object {
 	struct lu_object_header	ofo_header;
 	struct dt_object	ofo_obj;

commit 23bebc2da59ff5204bf89f2bf1f7924afdde0f37

LU-17941 ofd: do not copy over filter_fid structure
When a bigger filter_fid has been writen on disk by newer server,
downgraded Lustre would read more data but we need store less to
fit smaller filter_fid structure.

Test-Parameters: serverdistro=el8.10
Fixes: 28c366cee6d ("LU-17218 ofd: improve filter_fid upgrade compatibility")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: Idb5c8fffe4af22f35b64aa93e7efce7f9dd206d6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55408
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ofd/ofd_objects.c
@@ -167,7 +167,7 @@ int ofd_object_ff_load(const struct lu_env *env, struct ofd_object *fo)
 		return -EINVAL;
 	}
 
-	filter_fid_le_to_cpu(ff, ff, rc);
+	filter_fid_le_to_cpu(ff, ff, sizeof(*ff));
 
 	return 0;
 }