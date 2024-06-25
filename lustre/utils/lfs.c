lustre/lustre-release

commit 58d744e3eaab358ef346e51ff4aa17e9f08efbb3

LU-10092 pcc: Non-blocking PCC caching
Current PCC uses refcount of PCC inode to determine whether a
previous PCC-attached file can be detached. If a file is open
(refcount > 1), the detaching will return -EBUSY.

When another client accesses the PCC-cached file, it will trigger
the restore process as the file is HSM released. During restore,
the Agent needs to detach the PCC-cached file.
Thus, if a PCC-attached file is keeping opened but not closed
for a long time, the restore request will always return failure.

In this patch, we implement a non-blocking PCC caching mechanism
for Lustre. After attaching the file into PCC, the client acquires
the layout lock for the file, and the layout generation is
maintained in the PCC inode. Under the layout lock protection, the
PCC caching state is valid and all I/O will direct into PCC. When
the layout lock is revoked, in the blocking AST it will invalidate
the PCC caching state and detach the file automatically.

This patch is also helpful to handle the ENOSPC error for PCC
write by fallback to normal I/O path which will restore the file
data into OSTs (The file is in HSM released state) and redo the
write again.

Change-Id: I9130c04dc0e6eae879ea2ff3fdda65726e74d177
Test-Parameters: clientcount=3 testlist=sanity-pcc,sanity-pcc,sanity-pcc
Signed-off-by: Qian Yingjin <qian@ddn.com>
Reviewed-on: https://review.whamcloud.com/32966
Tested-by: Jenkins
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -128,6 +128,7 @@ static inline int lfs_mirror_read(int argc, char **argv);
 static inline int lfs_mirror_write(int argc, char **argv);
 static inline int lfs_mirror_copy(int argc, char **argv);
 static int lfs_pcc_attach(int argc, char **argv);
+static int lfs_pcc_attach_fid(int argc, char **argv);
 static int lfs_pcc_detach(int argc, char **argv);
 static int lfs_pcc_detach_fid(int argc, char **argv);
 static int lfs_pcc_state(int argc, char **argv);
@@ -339,6 +340,12 @@ command_t pcc_cmdlist[] = {
 	  .pc_help = "Attach given files to the Persistent Client Cache.\n"
 		"usage: lfs pcc attach <--id|-i NUM> <file> ...\n"
 		"\t-i: archive id for RW-PCC\n" },
+	{ .pc_name = "attach_fid", .pc_func = lfs_pcc_attach_fid,
+	  .pc_help = "Attach given files into PCC by FID(s).\n"
+		"usage: lfs pcc attach_id <--id|-i NUM> <--mnt|-m mnt> "
+		"<fid> ...\n"
+		"\t-i: archive id for RW-PCC\n"
+		"\t-m: Lustre mount point\n" },
 	{ .pc_name = "state", .pc_func = lfs_pcc_state,
 	  .pc_help = "Display the PCC state for given files.\n"
 		"usage: lfs pcc state <file> ...\n" },
@@ -664,6 +671,7 @@ command_t cmdlist[] = {
 	{"pcc", lfs_pcc, pcc_cmdlist,
 	 "lfs commands used to interact with PCC features:\n"
 	 "lfs pcc attach - attach given files to Persistent Client Cache\n"
+	 "lfs pcc attach_fid - attach given files into PCC by FID(s)\n"
 	 "lfs pcc state  - display the PCC state for given files\n"
 	 "lfs pcc detach - detach given files from Persistent Client Cache\n"
 	 "lfs pcc detach_fid - detach given files from PCC by FID(s)\n"},
@@ -10433,6 +10441,79 @@ static int lfs_pcc_attach(int argc, char **argv)
 	return rc;
 }
 
+static int lfs_pcc_attach_fid(int argc, char **argv)
+{
+	struct option long_opts[] = {
+	{ .val = 'i',	.name = "id",	.has_arg = required_argument },
+	{ .val = 'm',	.name = "mnt",	.has_arg = required_argument },
+	{ .name = NULL } };
+	char			 short_opts[] = "i:m:";
+	int			 c;
+	int			 rc = 0;
+	__u32			 archive_id = 0;
+	char			*end;
+	const char		*mntpath = NULL;
+	const char		*fidstr;
+	enum lu_pcc_type	 type = LU_PCC_READWRITE;
+
+	optind = 0;
+	while ((c = getopt_long(argc, argv, short_opts,
+				long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'i':
+			archive_id = strtoul(optarg, &end, 0);
+			if (*end != '\0') {
+				fprintf(stderr, "error: %s: bad archive ID "
+					"'%s'\n", argv[0], optarg);
+				return CMD_HELP;
+			}
+			break;
+		case 'm':
+			mntpath = optarg;
+			break;
+		case '?':
+			return CMD_HELP;
+		default:
+			fprintf(stderr, "%s: option '%s' unrecognized\n",
+				argv[0], argv[optind - 1]);
+			return CMD_HELP;
+		}
+	}
+
+	if (archive_id == 0) {
+		fprintf(stderr, "%s: must specify an archive ID\n", argv[0]);
+		return CMD_HELP;
+	}
+
+	if (mntpath == NULL) {
+		fprintf(stderr, "%s: must specify Lustre mount point\n",
+			argv[0]);
+		return CMD_HELP;
+	}
+
+	if (argc <= optind) {
+		fprintf(stderr, "%s: must specify one or more fids\n", argv[0]);
+		return CMD_HELP;
+	}
+
+	while (optind < argc) {
+		int rc2;
+
+		fidstr = argv[optind++];
+
+		rc2 = llapi_pcc_attach_fid_str(mntpath, fidstr,
+					       archive_id, type);
+		if (rc2 < 0) {
+			fprintf(stderr, "%s: cannot attach '%s' on '%s' to PCC "
+				"with archive ID '%u': %s\n", argv[0],
+				fidstr, mntpath, archive_id, strerror(rc2));
+		}
+		if (rc == 0 && rc2 < 0)
+			rc = rc2;
+	}
+	return rc;
+}
+
 static int lfs_pcc_detach(int argc, char **argv)
 {
 	int			 rc = 0;
@@ -10461,8 +10542,9 @@ static int lfs_pcc_detach(int argc, char **argv)
 
 		rc2 = llapi_pcc_detach_file(fullpath);
 		if (rc2 < 0) {
+			rc2 = -errno;
 			fprintf(stderr, "%s: cannot detach '%s' from PCC: "
-				"%s\n", argv[0], path, strerror(-rc2));
+				"%s\n", argv[0], path, strerror(errno));
 			if (rc == 0)
 				rc = rc2;
 		}
@@ -10549,9 +10631,7 @@ static int lfs_pcc_state(int argc, char **argv)
 
 		printf(", PCC file: %s", state.pccs_path);
 		printf(", user number: %u", state.pccs_open_count);
-		printf(", attr cached: %s",
-		       state.pccs_flags &  PCC_STATE_FLAG_ATTR_VALID ?
-		       "true" : "false");
+		printf(", flags: %x", state.pccs_flags);
 		printf("\n");
 	}
 	return rc;

lustre/utils/liblustreapi_pcc.c
@@ -45,25 +45,15 @@
  * Fetch and attach a file to readwrite PCC.
  *
  */
-static int llapi_readwrite_pcc_attach(const char *path, __u32 archive_id)
+static int llapi_readwrite_pcc_attach_fd(int fd, __u32 archive_id)
 {
-	int fd;
 	int rc;
 	struct ll_ioc_lease *data;
 
-	fd = open(path, O_RDWR | O_NONBLOCK);
-	if (fd < 0) {
-		rc = -errno;
-		llapi_error(LLAPI_MSG_ERROR, rc, "cannot open '%s'",
-			    path);
-		return rc;
-	}
-
 	rc = llapi_lease_acquire(fd, LL_LEASE_WRLCK);
 	if (rc < 0) {
-		llapi_error(LLAPI_MSG_ERROR, rc,
-			    "cannot get lease for '%s'", path);
-		goto out_close;
+		llapi_error(LLAPI_MSG_ERROR, rc, "cannot get lease");
+		return rc;
 	}
 
 	data = malloc(offsetof(typeof(*data), lil_ids[1]));
@@ -71,7 +61,7 @@ static int llapi_readwrite_pcc_attach(const char *path, __u32 archive_id)
 		rc = -ENOMEM;
 		llapi_err_noerrno(LLAPI_MSG_ERROR,
 				  "failed to allocate memory");
-		goto out_close;
+		return rc;
 	}
 
 	data->lil_mode = LL_LEASE_UNLCK;
@@ -83,14 +73,30 @@ static int llapi_readwrite_pcc_attach(const char *path, __u32 archive_id)
 		if (rc == 0) /* lost lease lock */
 			rc = -EBUSY;
 		llapi_error(LLAPI_MSG_ERROR, rc,
-			    "cannot attach '%s' with ID: %u",
-			     path, archive_id);
+			    "cannot attach with ID: %u", archive_id);
 	} else {
 		rc = 0;
 	}
 
 	free(data);
-out_close:
+	return rc;
+}
+
+static int llapi_readwrite_pcc_attach(const char *path, __u32 archive_id)
+{
+	int fd;
+	int rc;
+
+	fd = open(path, O_RDWR | O_NONBLOCK);
+	if (fd < 0) {
+		rc = -errno;
+		llapi_error(LLAPI_MSG_ERROR, rc, "cannot open '%s'",
+			    path);
+		return rc;
+	}
+
+	rc = llapi_readwrite_pcc_attach_fd(fd, archive_id);
+
 	close(fd);
 	return rc;
 }
@@ -110,41 +116,85 @@ int llapi_pcc_attach(const char *path, __u32 id, enum lu_pcc_type type)
 	return rc;
 }
 
-
-/**
- * detach PCC cache of a file by an ioctl on the dir fd (usually a mount
- * point fd that the copytool already has open).
- *
- * If the file is being used, the detaching will return -EBUSY immediately.
- * Thus, if a PCC-attached file is kept open for a long time, the restore
- * request will always return failure.
- *
- * \param fd		Directory file descriptor.
- * \param fid		FID of the file.
- *
- * \return 0 on success, an error code otherwise.
- */
-int llapi_pcc_detach_fid_fd(int fd, const struct lu_fid *fid)
+static int llapi_readwrite_pcc_attach_fid(const char *mntpath,
+					  const struct lu_fid *fid,
+					  __u32 id)
 {
 	int rc;
-	struct lu_pcc_detach detach;
+	int fd;
 
-	detach.pccd_fid = *fid;
-	rc = ioctl(fd, LL_IOC_PCC_DETACH, &detach);
-	if (rc == -EAGAIN)
+	fd = llapi_open_by_fid(mntpath, fid, O_RDWR | O_NONBLOCK);
+	if (fd < 0) {
+		rc = -errno;
 		llapi_error(LLAPI_MSG_ERROR, rc,
-			    "FID "DFID" may be in the attaching state, "
-			    "or you may need to re-run the pcc_attach "
-			    "to finish the attach process.", PFID(fid));
-	else if (rc)
-		llapi_error(LLAPI_MSG_ERROR, rc,
-			    "cannot detach FID "DFID" from PCC", PFID(fid));
+			    "llapi_open_by_fid for " DFID "failed",
+			    PFID(fid));
+		return rc;
+	}
+
+	rc = llapi_readwrite_pcc_attach_fd(fd, id);
+
+	close(fd);
+	return rc;
+}
+
+int llapi_pcc_attach_fid(const char *mntpath, const struct lu_fid *fid,
+			 __u32 id, enum lu_pcc_type type)
+{
+	int rc;
+
+	switch (type) {
+	case LU_PCC_READWRITE:
+		rc = llapi_readwrite_pcc_attach_fid(mntpath, fid, id);
+		break;
+	default:
+		rc = -EINVAL;
+		break;
+	}
+	return rc;
+}
+
+
+int llapi_pcc_attach_fid_str(const char *mntpath, const char *fidstr,
+			     __u32 id, enum lu_pcc_type type)
+{
+	int rc;
+	struct lu_fid fid;
+	const char *fidstr_orig = fidstr;
+
+	while (*fidstr == '[')
+		fidstr++;
+	rc = sscanf(fidstr, SFID, RFID(&fid));
+	if (rc != 3) {
+		llapi_err_noerrno(LLAPI_MSG_ERROR,
+				  "bad FID format '%s', should be [seq:oid:ver]"
+				  " (e.g. "DFID")\n", fidstr_orig,
+				  (unsigned long long)FID_SEQ_NORMAL, 2, 0);
+		return -EINVAL;
+	}
+
+	rc = llapi_pcc_attach_fid(mntpath, &fid, id, type);
 
 	return rc;
 }
 
 /**
- * detach PCC cache of a file.
+ * detach PCC cache of a file by using fd.
+ *
+ * \param fd		File handle.
+ *
+ * \return 0 on success, an error code otherwise.
+ */
+int llapi_pcc_detach_fd(int fd)
+{
+	int rc;
+
+	rc = ioctl(fd, LL_IOC_PCC_DETACH);
+	return rc;
+}
+
+/**
+ * detach PCC cache of a file via FID.
  *
  * \param mntpath	Fullpath to the client mount point.
  * \param fid		FID of the file.
@@ -155,6 +205,7 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid)
 {
 	int rc;
 	int fd;
+	struct lu_pcc_detach detach;
 
 	rc = get_root_path(WANT_FD, NULL, &fd, (char *)mntpath, -1);
 	if (rc) {
@@ -163,14 +214,21 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid)
 		return rc;
 	}
 
-	rc = llapi_pcc_detach_fid_fd(fd, fid);
-
+	/*
+	 * PCC prefetching algorithm scans Lustre OPEN/CLOSE changelogs
+	 * to determine the candidate files needing to prefetch into
+	 * PCC. To avoid generattion of unnecessary open/close changelogs,
+	 * we implement a new dir ioctl LL_IOC_PCC_DETACH_BY_FID to detach
+	 * files.
+	 */
+	detach.pccd_fid = *fid;
+	rc = ioctl(fd, LL_IOC_PCC_DETACH_BY_FID, &detach);
 	close(fd);
 	return rc;
 }
 
 /**
- * detach PCC cache of a file.
+ * detach PCC cache of a file via FID.
  *
  * \param mntpath	Fullpath to the client mount point.
  * \param fid		FID string of the file.
@@ -209,16 +267,18 @@ int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr)
 int llapi_pcc_detach_file(const char *path)
 {
 	int rc;
-	lustre_fid fid;
+	int fd;
 
-	rc = llapi_path2fid(path, &fid);
-	if (rc) {
-		llapi_error(LLAPI_MSG_ERROR, rc, "cannot get FID of '%s'",
+	fd = open(path, O_RDWR | O_NONBLOCK);
+	if (fd < 0) {
+		rc = -errno;
+		llapi_error(LLAPI_MSG_ERROR, rc, "cannot open '%s'",
 			    path);
 		return rc;
 	}
 
-	rc = llapi_pcc_detach_fid(path, &fid);
+	rc = llapi_pcc_detach_fd(fd);
+	close(fd);
 	return rc;
 }
 

lustre/doc/lfs-pcc.1
@@ -2,7 +2,9 @@
 .SH NAME
 lfs pcc commands used to interact with PCC features.
 .SH SYNOPSIS
-.B lfs pcc attach <\fB--id\fR|\fB-i\fR NUM>  <\fIfile \fR...>
+.B lfs pcc attach <\fB--id\fR|\fB-i\fR \fINUM\fR>  <\fIfile \fR...>
+.br
+.B lfs pcc attach <\fB--id\fR|\fB-i\fR \fINUM\fR>  <\fB--mnt\fR|\fB-m\fR \fImntpath\fR> <\fIfid \fR...>
 .br
 .B lfs pcc detach <\fIfile \fR...>
 .br
@@ -11,8 +13,11 @@ lfs pcc commands used to interact with PCC features.
 .B lfs pcc state <\fIfile \fR...>
 .SH DESCRIPTION
 .TP
-.B lfs pcc attach <\fB--id\fR|\fB-i\fR NUM>  <\fIfile \fR...>
+.B lfs pcc attach <\fB--id\fR|\fB-i\fR \fINUM\fR>  <\fIfile \fR...>
 Attach given files on the persistent client cache.
+.TP
+.B lfs pcc attach <\fB--id\fR|\fB-i\fR \fINUM\fR>  <\fB--mnt\fR|\fB-m\fR \fImntpath\fR> <\fIfid \fR...>
+Attach given fils into the persistent client cache by FID(s).
 .TP
 .B lfs pcc detach <\fIfile \fR...>
 Detach given files from the persistent client cache.
@@ -28,9 +33,12 @@ Display the PCC state for given files.
 .B --id | -i
 For RW-PCC, it is HSM ARCHIVE ID to choose which backend for cache files.
 .TP
+.B --mnt | -m
+Specifies Lustre mount point.
+.TP
 Before using RW-PCC, you need to configure HSM root and Archive ID mapping properly:
 .TP
-.B lfs pcc add $MNTPATH $PCCPATH "$ARCHIVE_ID $PROJID"
+.B lfs pcc add $MNTPATH $PCCPATH \ "$ARCHIVE_ID $PROJID"
 Add one PCC backend to the Lustre client, you need to specify hsm root,
 archive ID, and project ID. On this client any subsequently created
 files with this project ID will be persistently cached automatically.
@@ -49,15 +57,21 @@ Enable HSM on the appropriate MDT.
 .B # lhsmtool_posix --daemon --hsm-root /mnt/pcc/ --archive=1 /mnt/lustre
 Launch one copytool on client node to connect cache storage.
 .TP
-.B # lfs pcc add /mnt/lustre /mnt/pcc \ "1\ 1"
+.B # lfs pcc add /mnt/lustre /mnt/pcc \ "1\ 100"
 Add HSM root and Archive ID mapping for RW-PCC.
 .TP
 .B $ lfs pcc attach -i 1 /mnt/lustre/file
 Attach an existing file into PCC and migrate data from lustre to Cache Device,
 any I/O to the Lustre file will direct to the RW-PCC copy.
 .TP
+.B $ lfs pcc attach_fid -i 1 -m /mnt/lustre 0x200000401:0x1:0x0
+Attach an existing file referenced by FID "0x200000401:0x1:0x0" into PCC.
+.TP
 .B $ lfs pcc detach /mnt/lustre/file
-Detach the file from RW-PCC, IO to the file will come to Lustre after this command.
+Detach the file from RW-PCC, IO to the file will come to Lustre after this
+command.
+.B $ lfs pcc detach_fid /mnt/lustre 0x200000401:0x1:0x0
+Detach the file referenced by FID "0x200000401:0x1:0x0" from PCC.
 .TP
 .B $ lfs pcc state /mnt/lustre/file
 .br

lustre/doc/llapi_pcc_attach.3
@@ -1,19 +1,31 @@
 .TH llapi_pcc_attach 3 "2019 April 20" "Lustre User API"
 .SH NAME
-llapi_pcc_attach \- attach a file into PCC
+llapi_pcc_attach, llapi_pcc_attach_fid, llapi_pcc_attach_fid_str \- attach a file into PCC
 .SH SYNOPSIS
 .nf
 .B #include <lustre/lustreapi.h>
 .PP
 .BI "int llapi_pcc_attach(const char *" path ", __u32 " id ,
 .BI "                     enum lu_pcc_type " type ");"
+.PP
+.BI "int llapi_pcc_attach_fid(const char *" mntpath ", const struct lu_fid *" fid ,
+.BI "                         __u32 " id ", enum lu_pcc_type " type ");"
+.PP
+.BI "int llapi_pcc_attach_fid_str(const char *" mntpath ", const char *" fidstr ,
+.BI "                             __u32 " id ", enum lu_pcc_type " type ");"
 .fi
 .SH DESCRIPTION
 .PP
 The function
-.B llapi_pcc_attach()
+.BR llapi_pcc_attach() ,
+.BR llapi_pcc_attach_fid() ,
+and
+.BR llapi_pcc_attach_fid_str()
 tries to attach the file referenced by
-.BR path
+.IR path ,
+.IR fid ,
+or
+.IR fidstr
 into PCC backend. PCC provides a group of local caches and works in two modes:
 RW-PCC enables a read-write cache on the local SSDs of a single client; RO-PCC
 provides a read-only cache on the local SSDs of multiple clients. For RW-PCC,

lustre/doc/llapi_pcc_attach_fid.3
@@ -1,0 +1,1 @@
+.so man3/llapi_pcc_attach.3

lustre/doc/llapi_pcc_attach_fid_str.3
@@ -1,0 +1,1 @@
+.so man3/llapi_pcc_attach.3

lustre/include/lustre/lustreapi.h
@@ -521,7 +521,11 @@ int llapi_ladvise(int fd, unsigned long long flags, int num_advise,
 
 /* PCC */
 int llapi_pcc_attach(const char *path, __u32 id, enum lu_pcc_type type);
-int llapi_pcc_detach_fid_fd(int fd, const struct lu_fid *fid);
+int llapi_pcc_attach_fid(const char *mntpath, const struct lu_fid *fid,
+			 __u32 id, enum lu_pcc_type type);
+int llapi_pcc_attach_fid_str(const char *mntpath, const char *fidstr,
+			     __u32 id, enum lu_pcc_type type);
+int llapi_pcc_detach_fd(int fd);
 int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid);
 int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr);
 int llapi_pcc_detach_file(const char *path);

lustre/include/obd_support.h
@@ -568,6 +568,10 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_IMUTEX_SEC		    0x140e
 #define OBD_FAIL_LLITE_IMUTEX_NOSEC		    0x140f
 #define OBD_FAIL_LLITE_OPEN_BY_NAME		    0x1410
+#define OBD_FAIL_LLITE_PCC_FAKE_ERROR		    0x1411
+#define OBD_FAIL_LLITE_PCC_DETACH_MKWRITE	    0x1412
+#define OBD_FAIL_LLITE_PCC_MKWRITE_PAUSE	    0x1413
+#define OBD_FAIL_LLITE_PCC_ATTACH_PAUSE		    0x1414
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

commit 55831a3128d6b395fac19f88c81dc27d27fb690f

LU-17000 utils: Fix negative argument passed
This patch fixes bunch of "Argument cannot be negative"
reported by Coverity.

CoverityID: 397713 ("Argument cannot be negative")
CoverityID: 397899 ("Argument cannot be negative")
Pass -rc to strerror() since rc is guaranteed to be negative.

CoverityID: 397769 ("Argument cannot be negative")
read() could return -1 on error. Check before using the value

CoverityID: 397780 ("Argument cannot be negative")
Use static allocation removing the need to call sysconf()

CoverityID: 403104 ("Argument cannot be negative")
On failure to open do not try to call close()

Fixes: 58d744e3 (LU-10092 pcc: Non-blocking PCC caching)
Fixes: f172b116 (LU-10092 llite: Add persistent cache on client)
Fixes: aed82919 (LU-16029 utils: add options to lr_reader to parse raw files)
Fixes: 86ba46c2 (LU-9680 obdclass: user netlink to collect devices information)
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Id51c6c184d30dce596e7ab948a6fd0768eed1503
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53796
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

lustre/utils/lfs.c
@@ -13751,7 +13751,7 @@ static int lfs_pcc_attach_fid(int argc, char **argv)
 			fprintf(stderr,
 				"%s: cannot attach '%s' on '%s' to PCC with attach ID '%u': %s\n",
 				argv[0], fidstr, mntpath, attach_id,
-				strerror(rc2));
+				strerror(-rc2));
 		}
 		if (rc == 0 && rc2 < 0)
 			rc = rc2;

lustre/utils/liblustreapi_pcc.c
@@ -463,10 +463,9 @@ out:
  */
 int llapi_pccdev_get(const char *mntpath)
 {
-	long page_size = sysconf(_SC_PAGESIZE);
 	char pathbuf[sizeof(struct obd_uuid)];
+	char buf[65536]; /* large engough to hold PPC dev list */
 	glob_t path;
-	char *buf;
 	int fd;
 	int rc;
 
@@ -491,17 +490,8 @@ int llapi_pccdev_get(const char *mntpath)
 		goto out_free_param;
 	}
 
-	buf = calloc(1, page_size);
-	if (buf == NULL) {
-		rc = -ENOMEM;
-		llapi_error(LLAPI_MSG_ERROR, rc,
-			    "error: pccdev_get: allocating '%s' buffer",
-			    path.gl_pathv[0]);
-		goto out_close;
-	}
-
 	while (1) {
-		ssize_t count = read(fd, buf, page_size);
+		ssize_t count = read(fd, buf, sizeof(buf));
 
 		if (count == 0)
 			break;
@@ -521,9 +511,10 @@ int llapi_pccdev_get(const char *mntpath)
 			break;
 		}
 	}
-out_close:
+
 	close(fd);
-	free(buf);
+	cfs_free_param_data(&path);
+	return rc;
 out_free_param:
 	cfs_free_param_data(&path);
 	return rc;

lustre/utils/liblustreapi_util.c
@@ -388,8 +388,8 @@ int llapi_direntry_remove(char *dname)
 		llapi_error(LLAPI_MSG_ERROR, errno,
 			    "error on ioctl %#lx for '%s' (%d)",
 			    (long)LL_IOC_LMV_SETSTRIPE, filename, fd);
-out:
 	close(fd);
+out:
 	free(namepath);
 out_dirpath:
 	free(dirpath);

lustre/utils/lr_reader.c
@@ -79,6 +79,12 @@ static void dump_log(int fd)
 
 	do {
 		n = read(fd, buf, sizeof(buf));
+		if (n < 0) {
+			fprintf(stderr,
+				"%s: dump_log() Failed to read: %s\n",
+				progname, strerror(errno));
+			exit(errno);
+		}
 		n = write(2, buf, n);
 	} while (n == sizeof(buf));
 

lustre/utils/lustre_cfg.c
@@ -1640,10 +1640,9 @@ static int do_name2dev(char *func, char *name, int dev_id)
 int parse_devname(char *func, char *name, int dev_id)
 {
 	int rc = 0;
-	int ret = -1;
 
 	if (!name)
-		return ret;
+		return -EINVAL;
 
 	/* Test if its a pure number string */
 	if (strspn(name, "0123456789") != strlen(name)) {
@@ -1651,19 +1650,17 @@ int parse_devname(char *func, char *name, int dev_id)
 			name++;
 
 		rc = do_name2dev(func, name, dev_id);
-		if (rc >= 0)
-			ret = rc;
 	} else {
 		errno = 0;
-		ret = strtoul(name, NULL, 10);
+		rc = strtoul(name, NULL, 10);
 		if (errno)
-			rc = errno;
+			rc = -errno;
 	}
 
 	if (rc < 0)
 		fprintf(stderr, "No device found for name %s: %s\n",
-			name, strerror(rc));
-	return ret;
+			name, strerror(-rc));
+	return rc;
 }
 
 #ifdef HAVE_SERVER_SUPPORT