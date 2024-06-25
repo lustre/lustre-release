lustre/lustre-release

commit 40d91eafe257fb407d27c54cd2f7ae9961672f60

LU-12275 sec: atomicity of encryption context getting/setting
Encryption layer needs to set an encryption context on files and dirs
that are encrypted. This context is stored as an extended attribute,
that then needs to be fetched upon metadata ops like lookup, getattr,
open, truncate, and layout.

With this patch we send encryption context to the MDT along with
create RPCs. This closes the insecure window between creation and
setting of the encryption context, and saves a setxattr request.

This patch also introduces a way to have the MDT return encryption
context upon granted lock reply, making the encryption context
retrieval atomic, and sparing the client an additional getxattr
request.

Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=ldiskfs mdscount=2 mdtcount=4
Test-Parameters: testlist=sanity-sec envdefinitions=ONLY="36 37 38 39 40 41 42 43 44 45 46 47 48 49" clientdistro=el8.1 fstype=zfs mdscount=2 mdtcount=4
Test-Parameters: clientversion=2.12 env=SANITY_EXCEPT="27M 56ra 151 156 802"
Test-Parameters: serverversion=2.12 env=SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Test-Parameters: serverversion=2.12 clientdistro=el8.1 env=SANITYN_EXCEPT=106,SANITY_EXCEPT="56oc 56od 165a 165b 165d 205b"
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I45599cdff13d5587103aff6edd699abcda6cb8f4
Reviewed-on: https://review.whamcloud.com/38430
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/crypto.c
@@ -51,48 +51,77 @@ static int ll_get_context(struct inode *inode, void *ctx, size_t len)
 	return rc;
 }
 
+int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
+		    bool preload)
+{
+	unsigned int ext_flags;
+	int rc = 0;
+
+	/* used as encryption unit size */
+	if (S_ISREG(inode->i_mode))
+		inode->i_blkbits = LUSTRE_ENCRYPTION_BLOCKBITS;
+	ext_flags = ll_inode_to_ext_flags(inode->i_flags) | LUSTRE_ENCRYPT_FL;
+	ll_update_inode_flags(inode, ext_flags);
+
+	if (encctx && encctxlen)
+		rc = ll_xattr_cache_insert(inode,
+					   LL_XATTR_NAME_ENCRYPTION_CONTEXT,
+					   encctx, encctxlen);
+	if (rc)
+		return rc;
+
+	return preload ? llcrypt_get_encryption_info(inode) : 0;
+}
+
+/* ll_set_context has 2 distinct behaviors, depending on the value of inode
+ * parameter:
+ * - inode is NULL:
+ *   passed fs_data is a struct md_op_data *. We need to store enc ctx in
+ *   op_data, so that it will be sent along to the server with the request that
+ *   the caller is preparing, thus saving a setxattr request.
+ * - inode is not NULL:
+ *   normal case in which passed fs_data is a struct dentry *, letting proceed
+ *   with setxattr operation.
+ *   This use case should only be used when explicitly setting a new encryption
+ *   policy on an existing, empty directory.
+ */
 static int ll_set_context(struct inode *inode, const void *ctx, size_t len,
 			  void *fs_data)
 {
-	unsigned int ext_flags;
 	struct dentry *dentry;
-	struct md_op_data *op_data;
-	struct ptlrpc_request *req = NULL;
 	int rc;
 
-	if (inode == NULL)
-		return 0;
+	if (inode == NULL) {
+		struct md_op_data *op_data = (struct md_op_data *)fs_data;
 
-	ext_flags = ll_inode_to_ext_flags(inode->i_flags) | LUSTRE_ENCRYPT_FL;
-	dentry = (struct dentry *)fs_data;
+		if (!op_data)
+			return -EINVAL;
+
+		OBD_ALLOC(op_data->op_file_encctx, len);
+		if (op_data->op_file_encctx == NULL)
+			return -ENOMEM;
+		op_data->op_file_encctx_size = len;
+		memcpy(op_data->op_file_encctx, ctx, len);
+		return 0;
+	}
 
 	/* Encrypting the root directory is not allowed */
 	if (inode->i_ino == inode->i_sb->s_root->d_inode->i_ino)
 		return -EPERM;
 
-	op_data = ll_prep_md_op_data(NULL, inode, NULL, NULL, 0, 0,
-				     LUSTRE_OPC_ANY, NULL);
-	if (IS_ERR(op_data))
-		return PTR_ERR(op_data);
-
-	op_data->op_attr_flags = LUSTRE_ENCRYPT_FL;
-	op_data->op_xvalid |= OP_XVALID_FLAGS;
-	rc = md_setattr(ll_i2sbi(inode)->ll_md_exp, op_data, NULL, 0, &req);
-	ll_finish_md_op_data(op_data);
-	ptlrpc_req_finished(req);
-	if (rc)
-		return rc;
-
+	dentry = (struct dentry *)fs_data;
 	rc = ll_vfs_setxattr(dentry, inode, LL_XATTR_NAME_ENCRYPTION_CONTEXT,
 			     ctx, len, XATTR_CREATE);
 	if (rc)
 		return rc;
 
-	/* used as encryption unit size */
-	if (S_ISREG(inode->i_mode))
-		inode->i_blkbits = LUSTRE_ENCRYPTION_BLOCKBITS;
-	ll_update_inode_flags(inode, ext_flags);
-	return 0;
+	return ll_set_encflags(inode, (void *)ctx, len, false);
+}
+
+inline void llcrypt_free_ctx(void *encctx, __u32 size)
+{
+	if (encctx)
+		OBD_FREE(encctx, size);
 }
 
 inline bool ll_sbi_has_test_dummy_encryption(struct ll_sb_info *sbi)
@@ -142,6 +171,16 @@ const struct llcrypt_operations lustre_cryptops = {
 	.max_namelen		= NAME_MAX,
 };
 #else /* !HAVE_LUSTRE_CRYPTO */
+int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
+		    bool preload)
+{
+	return 0;
+}
+
+inline void llcrypt_free_ctx(void *encctx, __u32 size)
+{
+}
+
 inline bool ll_sbi_has_test_dummy_encryption(struct ll_sb_info *sbi)
 {
 	return false;

lustre/llite/llite_internal.h
@@ -442,6 +442,11 @@ int ll_xattr_cache_get(struct inode *inode,
 			size_t size,
 			__u64 valid);
 
+int ll_xattr_cache_insert(struct inode *inode,
+			  const char *name,
+			  char *buffer,
+			  size_t size);
+
 static inline bool obd_connect_has_secctx(struct obd_connect_data *data)
 {
 #ifdef CONFIG_SECURITY

lustre/llite/llite_lib.c
@@ -2966,7 +2966,8 @@ void ll_finish_md_op_data(struct md_op_data *op_data)
 	ll_unlock_md_op_lsm(op_data);
 	security_release_secctx(op_data->op_file_secctx,
 				op_data->op_file_secctx_size);
-        OBD_FREE_PTR(op_data);
+	llcrypt_free_ctx(op_data->op_file_encctx, op_data->op_file_encctx_size);
+	OBD_FREE_PTR(op_data);
 }
 
 int ll_show_options(struct seq_file *seq, struct dentry *dentry)

lustre/mdd/mdd_internal.h
@@ -600,8 +600,18 @@ static inline int mdo_declare_xattr_set(const struct lu_env *env,
                                         const char *name,
                                         int fl, struct thandle *handle)
 {
-        struct dt_object *next = mdd_object_child(obj);
-        return dt_declare_xattr_set(env, next, buf, name, fl, handle);
+	struct dt_object *next = mdd_object_child(obj);
+	int rc;
+
+	rc = dt_declare_xattr_set(env, next, buf, name, fl, handle);
+	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+		struct lu_attr la = { 0 };
+
+		la.la_valid = LA_FLAGS;
+		la.la_flags = LUSTRE_ENCRYPT_FL;
+		rc = dt_declare_attr_set(env, next, &la, handle);
+	}
+	return rc;
 }
 
 static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
@@ -609,11 +619,20 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 				int fl, struct thandle *handle)
 {
 	struct dt_object *next = mdd_object_child(obj);
+	int rc;
 
 	if (!mdd_object_exists(obj))
 		return -ENOENT;
 
-	return dt_xattr_set(env, next, buf, name, fl, handle);
+	rc = dt_xattr_set(env, next, buf, name, fl, handle);
+	if (rc >= 0 && strcmp(name, LL_XATTR_NAME_ENCRYPTION_CONTEXT) == 0) {
+		struct lu_attr la = { 0 };
+
+		la.la_valid = LA_FLAGS;
+		la.la_flags = LUSTRE_ENCRYPT_FL;
+		rc = dt_attr_set(env, next, &la, handle);
+	}
+	return rc;
 }
 
 static inline int mdo_declare_xattr_del(const struct lu_env *env,

lustre/tests/sanity-sec.sh
@@ -3533,6 +3533,61 @@ test_48b() {
 }
 run_test 48b "encrypted file: concurrent truncate"
 
+trace_cmd() {
+	local cmd="$@"
+	local xattr_name="security.c"
+
+	sync ; sync ; echo 3 > /proc/sys/vm/drop_caches
+	$LCTL set_param debug=+info
+	$LCTL clear
+
+	echo $cmd
+	eval $cmd
+
+	$LCTL dk | grep -E "get xattr '${xattr_name}'|get xattrs"
+	[ $? -ne 0 ] || error "get xattr event was triggered"
+}
+
+test_49() {
+	$LCTL get_param mdc.*.import | grep -q client_encryption ||
+		skip "client encryption not supported"
+
+	mount.lustre --help |& grep -q "test_dummy_encryption:" ||
+		skip "need dummy encryption support"
+
+	stack_trap cleanup_for_enc_tests EXIT
+	setup_for_enc_tests
+
+	local dirname=$DIR/$tdir/subdir
+
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+	trace_cmd $LFS setdirstripe -i 1 $dirname/d2
+	trace_cmd $LFS migrate -m 0 $dirname/d2
+
+	$LFS setdirstripe -i 1 -c 1 $dirname/d3
+	dirname=$dirname/d3/subdir
+	mkdir $dirname
+
+	trace_cmd stat $dirname
+	trace_cmd touch $dirname/f1
+	trace_cmd stat $dirname/f1
+	trace_cmd cat $dirname/f1
+	dd if=/dev/zero of=$dirname/f1 bs=1M count=10 conv=fsync
+	trace_cmd $TRUNCATE $dirname/f1 10240
+	trace_cmd $LFS setstripe -E -1 -S 4M $dirname/f2
+	trace_cmd $LFS migrate -E -1 -S 256K $dirname/f2
+}
+run_test 49 "Avoid getxattr for encryption context"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {

lustre/include/lustre_crypto.h
@@ -30,6 +30,9 @@
 #define _LUSTRE_CRYPTO_H_
 
 struct ll_sb_info;
+int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
+		    bool preload);
+void llcrypt_free_ctx(void *encctx, __u32 size);
 bool ll_sbi_has_test_dummy_encryption(struct ll_sb_info *sbi);
 bool ll_sbi_has_encrypt(struct ll_sb_info *sbi);
 void ll_sbi_set_encrypt(struct ll_sb_info *sbi, bool set);

lustre/include/lustre_export.h
@@ -475,6 +475,11 @@ static inline int exp_connect_sepol(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_SELINUX_POLICY);
 }
 
+static inline int exp_connect_encrypt(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_req_layout.h
@@ -59,7 +59,7 @@ enum req_location {
 };
 
 /* Maximal number of fields (buffers) in a request message. */
-#define REQ_MAX_FIELD_NR 11
+#define REQ_MAX_FIELD_NR 12
 
 struct req_capsule {
         struct ptlrpc_request   *rc_req;
@@ -260,6 +260,7 @@ extern struct req_msg_field RMF_CLOSE_DATA;
 extern struct req_msg_field RMF_FILE_SECCTX_NAME;
 extern struct req_msg_field RMF_FILE_SECCTX;
 extern struct req_msg_field RMF_FID_ARRAY;
+extern struct req_msg_field RMF_FILE_ENCCTX;
 
 /*
  * connection handle received in MDS_CONNECT request.

commit cfde96fb2e77c33a8ab9afede79cdf093ee8ff9e

LU-17907 enc: enc flag should not remove other flags
When updating inode flags, the lli_flags must be taken into account
so that they do not get lost. So provide helper functions for callers
of ll_update_inode_flags(), as an overlay to ll_inode_to_ext_flags().
And on server side, the mdd layer must fetch the existing flags when
setting LUSTRE_ENCRYPT_FL attr flag.

Fixes: 40d91eafe2 ("LU-12275 sec: atomicity of encryption context getting/setting")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I150f2d87cef112beab81d1d2030133671d4b7361
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55317
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/crypto.c
@@ -55,7 +55,7 @@ int ll_set_encflags(struct inode *inode, void *encctx, __u32 encctxlen,
 	/* used as encryption unit size */
 	if (S_ISREG(inode->i_mode))
 		inode->i_blkbits = LUSTRE_ENCRYPTION_BLOCKBITS;
-	ext_flags = ll_inode_to_ext_flags(inode->i_flags) | LUSTRE_ENCRYPT_FL;
+	ext_flags = ll_inode2ext_flags(inode) | LUSTRE_ENCRYPT_FL;
 	ll_update_inode_flags(inode, ext_flags);
 
 	if (encctx && encctxlen)

lustre/llite/file.c
@@ -129,9 +129,7 @@ static void ll_prepare_close(struct inode *inode, struct md_op_data *op_data,
 				      ATTR_CTIME);
 	op_data->op_xvalid |= OP_XVALID_CTIME_SET;
 	op_data->op_attr_blocks = inode->i_blocks;
-	op_data->op_attr_flags = ll_inode_to_ext_flags(inode->i_flags);
-	if (test_bit(LLIF_PROJECT_INHERIT, &ll_i2info(inode)->lli_flags))
-		op_data->op_attr_flags |= LUSTRE_PROJINHERIT_FL;
+	op_data->op_attr_flags = ll_inode2ext_flags(inode);
 	op_data->op_open_handle = och->och_open_handle;
 
 	if (och->och_flags & FMODE_WRITE &&
@@ -4099,7 +4097,6 @@ int ll_set_project(struct inode *inode, __u32 xflags, __u32 projid)
 	struct ptlrpc_request *req = NULL;
 	struct md_op_data *op_data;
 	struct cl_object *obj;
-	unsigned int inode_flags;
 	int rc = 0;
 
 	CDEBUG(D_QUOTA, DFID" xflags=%x projid=%u\n",
@@ -4113,10 +4110,7 @@ int ll_set_project(struct inode *inode, __u32 xflags, __u32 projid)
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
-	inode_flags = ll_xflags_to_inode_flags(xflags);
-	op_data->op_attr_flags = ll_inode_to_ext_flags(inode_flags);
-	if (xflags & FS_XFLAG_PROJINHERIT)
-		op_data->op_attr_flags |= LUSTRE_PROJINHERIT_FL;
+	op_data->op_attr_flags = ll_xflags_to_ext_flags(xflags);
 
 	/* pass projid to md_op_data */
 	op_data->op_projid = projid;

lustre/llite/llite_internal.h
@@ -518,6 +518,9 @@ enum ll_inode_flags {
 	/* Xattr cache is filled */
 	LLIF_XATTR_CACHE_FILLED	= 7,
 
+/* New flags added to this enum potentially need to be handled in
+ * ll_inode2ext_flags/ll_set_inode_flags
+ */
 };
 
 int ll_xattr_cache_destroy(struct inode *inode);
@@ -1476,6 +1479,8 @@ int ll_statfs(struct dentry *de, struct kstatfs *sfs);
 int ll_statfs_internal(struct ll_sb_info *sbi, struct obd_statfs *osfs,
 		       u32 flags);
 int ll_update_inode(struct inode *inode, struct lustre_md *md);
+u32 ll_inode2ext_flags(struct inode *inode);
+u32 ll_xflags_to_ext_flags(u32 xflags);
 void ll_update_inode_flags(struct inode *inode, unsigned int ext_flags);
 void ll_update_dir_depth_dmv(struct inode *dir, struct dentry *de);
 int ll_read_inode2(struct inode *inode, void *opaque);

lustre/llite/llite_lib.c
@@ -2759,11 +2759,40 @@ int ll_inode_size_trylock(struct inode *inode)
 	return mutex_trylock(&lli->lli_size_mutex);
 }
 
+u32 ll_inode2ext_flags(struct inode *inode)
+{
+	u32 ext_flags;
+
+	ext_flags = ll_inode_to_ext_flags(inode->i_flags);
+
+	/* add here future new lli_flags */
+	if (test_bit(LLIF_PROJECT_INHERIT, &ll_i2info(inode)->lli_flags))
+		ext_flags |= LUSTRE_PROJINHERIT_FL;
+
+	return ext_flags;
+}
+
+u32 ll_xflags_to_ext_flags(u32 xflags)
+{
+	u32 ext_flags;
+	u32 inode_flags;
+
+	inode_flags = ll_xflags_to_inode_flags(xflags);
+	ext_flags = ll_inode_to_ext_flags(inode_flags);
+	if (xflags & FS_XFLAG_PROJINHERIT)
+		ext_flags |= LUSTRE_PROJINHERIT_FL;
+
+	return ext_flags;
+}
+
 void ll_update_inode_flags(struct inode *inode, unsigned int ext_flags)
 {
 	/* do not clear encryption flag */
 	ext_flags |= ll_inode_to_ext_flags(inode->i_flags) & LUSTRE_ENCRYPT_FL;
+
 	inode->i_flags = ll_ext_to_inode_flags(ext_flags);
+
+	/* add here future new lli_flags */
 	if (ext_flags & LUSTRE_PROJINHERIT_FL)
 		set_bit(LLIF_PROJECT_INHERIT, &ll_i2info(inode)->lli_flags);
 	else

lustre/mdd/mdd_internal.h
@@ -684,8 +684,13 @@ static inline int mdo_xattr_set(const struct lu_env *env,struct mdd_object *obj,
 	     (rc = mdd_dir_is_empty(env, obj)) == 0)) {
 		struct lu_attr la = { 0 };
 
+		/* try to fetch existing attrs, to not lose them */
+		(void)dt_attr_get(env, next, &la);
+		if (la.la_valid & LA_FLAGS)
+			la.la_flags |= LUSTRE_ENCRYPT_FL;
+		else
+			la.la_flags = LUSTRE_ENCRYPT_FL;
 		la.la_valid = LA_FLAGS;
-		la.la_flags = LUSTRE_ENCRYPT_FL;
 		/* if this is an old client using the old enc xattr name,
 		 * switch to the new name for consistency
 		 */

lustre/tests/sanity-sec.sh
@@ -6360,6 +6360,68 @@ test_70() {
 }
 run_test 70 "targets have local copy of sptlrpc llog"
 
+test_71() {
+	local vaultdir=$DIR/$tdir/vault
+	local projid=101
+	local res
+
+	(( $MDS1_VERSION >= $(version_code 2.15.63) )) ||
+		skip "Need MDS version at least 2.15.63"
+
+	[[ $($LCTL get_param mdc.*.import) =~ client_encryption ]] ||
+		skip "need encryption support"
+	which fscrypt || skip_env "Need fscrypt"
+
+	mkdir -p $DIR/$tdir || error "mkdir $DIR/$tdir failed"
+
+	yes | fscrypt setup --force --verbose ||
+		echo "fscrypt global setup already done"
+	sed -i 's/\(.*\)policy_version\(.*\):\(.*\)\"[0-9]*\"\(.*\)/\1policy_version\2:\3"2"\4/' \
+		/etc/fscrypt.conf
+	yes | fscrypt setup --verbose $MOUNT ||
+		echo "fscrypt setup $MOUNT already done"
+	stack_trap "rm -rf $MOUNT/.fscrypt"
+
+	mkdir -p $vaultdir
+	stack_trap "rm -rf $vaultdir"
+	$LFS project -p $projid -s $vaultdir
+	$LFS project -d $vaultdir
+	res=$($LFS project -d $vaultdir | awk '{print $1}')
+	[[ "$res" == "$projid" ]] ||
+		error "project id set to $res instead of $projid"
+	res=$($LFS project -d $vaultdir | awk '{print $2}')
+	[[ "$res" == "P" ]] ||
+		error "project id should have inherit flag (1)"
+
+	echo -e 'mypass\nmypass' | fscrypt encrypt --verbose \
+	     --source=custom_passphrase --name=protector_71 $vaultdir ||
+		error "fscrypt encrypt $vaultdir failed"
+
+	$LFS project -d $vaultdir
+	res=$($LFS project -d $vaultdir | awk '{print $1}')
+	[[ "$res" == "$projid" ]] ||
+		error "project id changed to $res after enc"
+	res=$($LFS project -d $vaultdir | awk '{print $2}')
+	[[ "$res" == "P" ]] ||
+		error "project id should have inherit flag (2)"
+
+	touch $vaultdir/fileA || error "touch $vaultdir/fileA failed"
+	$LFS project $vaultdir/fileA
+	res=$($LFS project $vaultdir/fileA | awk '{print $1}')
+	[[ "$res" == "$projid" ]] ||
+		error "project id on fileA is $res after enc"
+
+	mkdir $vaultdir/dirA || error "touch $vaultdir/dirA failed"
+	$LFS project -d $vaultdir/dirA
+	res=$($LFS project -d $vaultdir/dirA | awk '{print $1}')
+	[[ "$res" == "$projid" ]] ||
+		error "project id on dirA is $res after enc"
+	res=$($LFS project -d $vaultdir/dirA | awk '{print $2}')
+	[[ "$res" == "P" ]] ||
+		error "project id should have inherit flag (3)"
+}
+run_test 71 "encryption does not remove project flag"
+
 log "cleanup: ======================================================"
 
 sec_unsetup() {