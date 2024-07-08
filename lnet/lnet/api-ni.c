lustre/lustre-release

commit f1c66236c6869a83f57d07206d845bba42d7cd9f

LU-10391 lnet: Fault injection add/del ioctls to netlink
Convert the fault injection add/del ioctls to a netlink
implementaiton.

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I20f38d4e7c0215a1b19772c6253c617174c0b00c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53732
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -2925,6 +2925,33 @@ nla_extract_val(struct nlattr **attr, int *rem,
 	RETURN(rc);
 }
 
+int
+nla_strnid(struct nlattr **attr, struct lnet_nid *nid, int *rem,
+	   struct netlink_ext_ack *extack)
+{
+	char nidstr[LNET_NIDSTR_SIZE];
+	int rc;
+
+	ENTRY;
+	rc = nla_extract_val(attr, rem, LN_SCALAR_ATTR_VALUE,
+			     nidstr, sizeof(nidstr), extack);
+	if (rc < 0) {
+		NL_SET_ERR_MSG(extack, "failed to copy nidstring attribute");
+		RETURN(rc);
+	}
+
+	rc = libcfs_strnid(nid, strim(nidstr));
+	if (rc < 0) {
+		CDEBUG(D_NET, "Invalid nidstr \"%s\"\n", nidstr);
+		NL_SET_ERR_MSG(extack, "failed to convert nidstring to NID");
+		RETURN(rc);
+	}
+
+	CDEBUG(D_NET, "%s -> %s\n", nidstr, libcfs_nidstr(nid));
+
+	RETURN(0);
+}
+
 static struct genl_family lnet_family;
 
 /**
@@ -9555,6 +9582,169 @@ int lnet_old_fault_show_dump(struct sk_buff *msg, struct netlink_callback *cb)
 }
 #endif
 
+static int lnet_fault_cmd(struct sk_buff *skb, struct genl_info *info)
+{
+	struct nlmsghdr *nlh = nlmsg_hdr(skb);
+	struct genlmsghdr *gnlh = nlmsg_data(nlh);
+	struct nlattr *params = genlmsg_data(gnlh);
+	struct netlink_ext_ack *extack = NULL;
+	struct lnet_fault_large_attr fattr;
+	int msg_len, rem, rc = 0;
+	struct nlattr *entry;
+	s64 opc = 0;
+
+	ENTRY;
+#ifdef HAVE_NL_PARSE_WITH_EXT_ACK
+	extack = info->extack;
+#endif
+	msg_len = genlmsg_len(gnlh);
+	if (!msg_len) {
+		GENL_SET_ERR_MSG(info, "no configuration");
+		RETURN(-ENOMSG);
+	}
+
+	if (!(nla_type(params) & LN_SCALAR_ATTR_LIST)) {
+		GENL_SET_ERR_MSG(info, "invalid configuration");
+		RETURN(-EINVAL);
+	}
+
+	fattr.fa_src = LNET_ANY_NID;
+	fattr.fa_dst = LNET_ANY_NID;
+
+	nla_for_each_attr(entry, params, msg_len, rem) {
+		u64 tmp;
+
+		CDEBUG(D_NET, "attr type: %d\n", nla_type(entry));
+		if (nla_type(entry) != LN_SCALAR_ATTR_VALUE)
+			continue;
+
+		if (nla_strcmp(entry, "rule_type") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &opc, sizeof(opc), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+		} else if (nla_strcmp(entry, "fa_src") == 0) {
+			rc = nla_strnid(&entry, &fattr.fa_src, &rem, extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+		} else if (nla_strcmp(entry, "fa_dst") == 0) {
+			rc = nla_strnid(&entry, &fattr.fa_dst, &rem, extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+		} else if (nla_strcmp(entry, "fa_local_nid") == 0) {
+			rc = nla_strnid(&entry, &fattr.fa_local_nid, &rem,
+					extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+		} else if (nla_strcmp(entry, "fa_ptl_mask") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.fa_ptl_mask = tmp;
+		} else if (nla_strcmp(entry, "fa_msg_mask") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.fa_msg_mask = tmp;
+		} else if (nla_strcmp(entry, "da_rate") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.drop.da_rate = tmp;
+		} else if (nla_strcmp(entry, "da_interval") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.drop.da_interval = tmp;
+		} else if (nla_strcmp(entry, "da_health_error_mask") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.drop.da_health_error_mask = tmp;
+		} else if (nla_strcmp(entry, "da_random") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.drop.da_random = !!tmp;
+		} else if (nla_strcmp(entry, "da_drop_all") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.drop.da_drop_all = !!tmp;
+		} else if (nla_strcmp(entry, "la_rate") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.delay.la_rate = tmp;
+		} else if (nla_strcmp(entry, "la_interval") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.delay.la_interval = tmp;
+		} else if (nla_strcmp(entry, "la_latency") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+
+			fattr.u.delay.la_latency = tmp;
+		}
+	}
+
+	if (info->nlhdr->nlmsg_flags & NLM_F_CREATE) {
+		if (opc == LNET_CTL_DROP_ADD)
+			rc = lnet_drop_rule_add(&fattr);
+		else
+			rc = lnet_delay_rule_add(&fattr);
+	} else if (info->nlhdr->nlmsg_flags & NLM_F_REPLACE) {
+		if (opc == LNET_CTL_DROP_RESET)
+			lnet_drop_rule_reset();
+		else
+			lnet_delay_rule_reset();
+	} else if (!(info->nlhdr->nlmsg_flags & (NLM_F_CREATE | NLM_F_REPLACE))) {
+		if (opc == LNET_CTL_DROP_DEL)
+			rc = lnet_drop_rule_del(&fattr.fa_src, &fattr.fa_dst);
+		else
+			rc = lnet_delay_rule_del(&fattr.fa_src, &fattr.fa_dst,
+						 false);
+		if (rc == 0)
+			rc = -ENOENT;
+		else
+			rc = 0;
+	}
+report_error:
+	RETURN(rc);
+}
+
 static const struct genl_multicast_group lnet_mcast_grps[] = {
 	{ .name	=	"ip2net",	},
 	{ .name =	"net",		},
@@ -9667,6 +9857,7 @@ static const struct genl_ops lnet_genl_ops[] = {
 		.dumpit		= lnet_old_fault_show_dump,
 #endif
 		.done		= lnet_fault_show_done,
+		.doit		= lnet_fault_cmd,
 	},
 };
 

libcfs/libcfs/util/nidstrings.c
@@ -1029,6 +1029,20 @@ libcfs_str2anynid(lnet_nid_t *nidp, const char *str)
 	return *nidp != LNET_NID_ANY;
 }
 
+int
+libcfs_stranynid(struct lnet_nid *nid, const char *str)
+{
+	if (!strcmp(str, "*")) {
+		*nid = LNET_ANY_NID;
+		return 1;
+	}
+
+	if (libcfs_strnid(nid, str) < 0)
+		*nid = LNET_ANY_NID;
+
+	return !LNET_NID_IS_ANY(nid);
+}
+
 /**
  * Nid range list syntax.
  * \verbatim

lnet/include/lnet/lib-lnet.h
@@ -830,12 +830,15 @@ struct lnet_genl_fault_rule_list {
 };
 
 int lnet_fault_ctl(int cmd, struct libcfs_ioctl_data *data);
+int lnet_drop_rule_add(struct lnet_fault_large_attr *attr);
+int lnet_drop_rule_del(struct lnet_nid *fa_src, struct lnet_nid *fa_dst);
 int lnet_fault_init(void);
 void lnet_fault_fini(void);
 
 bool lnet_drop_rule_match(struct lnet_hdr *hdr, struct lnet_nid *local_nid,
 			  enum lnet_msg_hstatus *hstatus);
 int lnet_drop_rule_collect(struct lnet_genl_fault_rule_list *rlist);
+void lnet_drop_rule_reset(void);
 int lnet_delay_rule_add(struct lnet_fault_large_attr *attr);
 int lnet_delay_rule_del(struct lnet_nid *src, struct lnet_nid *dst,
 			bool shutdown);

lnet/include/uapi/linux/lnet/nidstr.h
@@ -99,6 +99,7 @@ char *libcfs_idstr(struct lnet_processid *id);
 __u32 libcfs_str2net(const char *str);
 lnet_nid_t libcfs_str2nid(const char *str);
 int libcfs_str2anynid(lnet_nid_t *nid, const char *str);
+int libcfs_stranynid(struct lnet_nid *nid, const char *str);
 int libcfs_num_parse(char *str, int len, struct list_head *list);
 char *libcfs_id2str(struct lnet_process_id id);
 void cfs_free_nidlist(struct list_head *list);

lnet/lnet/net_fault.c
@@ -173,8 +173,7 @@ lnet_fault_stat_inc(struct lnet_fault_stat *stat, unsigned int type)
  * There is no check for duplicated drop rule, all rules will be checked for
  * incoming message.
  */
-static int
-lnet_drop_rule_add(struct lnet_fault_large_attr *attr)
+int lnet_drop_rule_add(struct lnet_fault_large_attr *attr)
 {
 	struct lnet_drop_rule *rule;
 	ENTRY;
@@ -222,8 +221,7 @@ lnet_drop_rule_add(struct lnet_fault_large_attr *attr)
  * If \a dst is zero, then all rules have \a src as source will be removed
  * If both of them are zero, all rules will be removed
  */
-static int
-lnet_drop_rule_del(struct lnet_nid *src, struct lnet_nid *dst)
+int lnet_drop_rule_del(struct lnet_nid *src, struct lnet_nid *dst)
 {
 	struct lnet_drop_rule *rule;
 	struct lnet_drop_rule *tmp;
@@ -231,6 +229,8 @@ lnet_drop_rule_del(struct lnet_nid *src, struct lnet_nid *dst)
 	int n = 0;
 	ENTRY;
 
+	CDEBUG(D_NET, "src %s dst %s\n", libcfs_nidstr(src),
+	       libcfs_nidstr(dst));
 	lnet_net_lock(LNET_LOCK_EX);
 	list_for_each_entry_safe(rule, tmp, &the_lnet.ln_drop_rules, dr_link) {
 		if (!(LNET_NID_IS_ANY(src) || nid_same(&rule->dr_attr.fa_src, src)))
@@ -318,8 +318,7 @@ int lnet_drop_rule_collect(struct lnet_genl_fault_rule_list *rlist)
 /**
  * reset counters for all drop rules
  */
-static void
-lnet_drop_rule_reset(void)
+void lnet_drop_rule_reset(void)
 {
 	struct lnet_drop_rule *rule;
 	int		       cpt;
@@ -958,13 +957,14 @@ lnet_delay_rule_del(struct lnet_nid *src, struct lnet_nid *dst, bool shutdown)
 	bool cleanup;
 	ENTRY;
 
-	if (shutdown)
-		src = dst = 0;
-
 	mutex_lock(&delay_dd.dd_mutex);
 	lnet_net_lock(LNET_LOCK_EX);
 
 	list_for_each_entry_safe(rule, tmp, &the_lnet.ln_delay_rules, dl_link) {
+		CDEBUG(D_NET, "src %s dst %s fa_src %s fa_dst %s\n",
+		       libcfs_nidstr(src), libcfs_nidstr(dst),
+		       libcfs_nidstr(&rule->dl_attr.fa_src),
+		       libcfs_nidstr(&rule->dl_attr.fa_dst));
 		if (!(LNET_NID_IS_ANY(src) || nid_same(&rule->dl_attr.fa_src, src)))
 			continue;
 

lnet/lnet/nidstrings.c
@@ -1513,3 +1513,18 @@ libcfs_str2anynid(lnet_nid_t *nidp, const char *str)
 	return *nidp != LNET_NID_ANY;
 }
 EXPORT_SYMBOL(libcfs_str2anynid);
+
+int
+libcfs_stranynid(struct lnet_nid *nid, const char *str)
+{
+	if (!strcmp(str, "*")) {
+		*nid = LNET_ANY_NID;
+		return 1;
+	}
+
+	if (libcfs_strnid(nid, str))
+		*nid = LNET_ANY_NID;
+
+	return !LNET_NID_IS_ANY(nid);
+}
+EXPORT_SYMBOL(libcfs_stranynid);

lnet/utils/lnetconfig/liblnetconfig.c
@@ -528,14 +528,58 @@ free_reply:
 	return rc == 1 ? 0 : rc;
 }
 
+static int
+fault_attr_parse_nid(char *str, struct lnet_nid *nid)
+{
+	int rc = 0;
+	__u32 net;
+
+	if (!str) {
+		*nid = LNET_ANY_NID;
+		return 0;
+	}
+
+	if (strlen(str) > 2 && str[0] == '*' && str[1] == '@') {
+		net = libcfs_str2net(str + 2);
+		if (net == LNET_NET_ANY) {
+			rc = -EINVAL;
+			goto failed;
+		}
+
+		lnet_nid4_to_nid(LNET_MKNID(net, LNET_NIDADDR(LNET_NID_ANY)),
+				 nid);
+	} else {
+		rc = libcfs_stranynid(nid, str);
+		if (rc != 1)
+			rc = -EINVAL;
+	}
+failed:
+	if (rc < 0)
+		fprintf(stderr, "Invalid NID: %s\n", str);
+
+	return rc;
+}
+
 static char *
 fault_opc_to_str(__u32 opc)
 {
 	switch (opc) {
+	case LNET_CTL_DROP_ADD:
+		return "add drop rule";
+	case LNET_CTL_DELAY_ADD:
+		return "add delay rule";
 	case LNET_CTL_DROP_LIST:
 		return "list drop rule";
 	case LNET_CTL_DELAY_LIST:
 		return "list delay rules";
+	case LNET_CTL_DROP_DEL:
+		return "delete drop rule";
+	case LNET_CTL_DELAY_DEL:
+		return "delete delay rule";
+	case LNET_CTL_DROP_RESET:
+		return "drop_reset rule";
+	case LNET_CTL_DELAY_RESET:
+		return "delay reset_rule";
 	default:
 		return "unrecognized command";
 	}
@@ -546,14 +590,56 @@ int yaml_lnet_fault_rule(yaml_document_t *results, __u32 opc, char *src,
 			 struct lnet_fault_attr *attr)
 {
 	struct nl_sock *sk = NULL;
-	char num[INT_STRING_LEN];
+	struct lnet_nid fa_local_nid;
+	struct lnet_nid fa_src;
+	struct lnet_nid fa_dst;
 	const char *msg = NULL;
-	int flags = NLM_F_DUMP;
 	yaml_emitter_t output;
 	yaml_parser_t reply;
 	yaml_event_t event;
-	int rc;
+	int flags, rc;
 
+	switch (opc) {
+	case LNET_CTL_DROP_ADD:
+	case LNET_CTL_DELAY_ADD:
+		flags = NLM_F_CREATE;
+		break;
+	case LNET_CTL_DROP_RESET:
+	case LNET_CTL_DELAY_RESET:
+		flags = NLM_F_REPLACE;
+		break;
+	case LNET_CTL_DROP_LIST:
+	case LNET_CTL_DELAY_LIST:
+		flags = NLM_F_DUMP;
+		break;
+	case LNET_CTL_DROP_DEL:
+	case LNET_CTL_DELAY_DEL:
+	default:
+		flags = 0;
+		break;
+	}
+
+	if (opc == LNET_CTL_DROP_LIST || opc == LNET_CTL_DELAY_LIST ||
+	    opc == LNET_CTL_DROP_RESET || opc == LNET_CTL_DELAY_RESET)
+		goto skip_options;
+
+	rc = fault_attr_parse_nid(src, &fa_src);
+	if (rc < 0)
+		return rc;
+
+	rc = fault_attr_parse_nid(dst, &fa_dst);
+	if (rc < 0)
+		return rc;
+
+	if (local_nid) {
+		rc = fault_attr_parse_nid(local_nid, &fa_local_nid);
+		if (rc < 0)
+			return rc;
+	} else {
+		fa_local_nid = LNET_ANY_NID;
+	}
+
+skip_options:
 	/* Create Netlink emitter to send request to kernel */
 	sk = nl_socket_alloc();
 	if (!sk)
@@ -592,25 +678,97 @@ int yaml_lnet_fault_rule(yaml_document_t *results, __u32 opc, char *src,
 	if (rc == 0)
 		goto emitter_error;
 
-	yaml_scalar_event_initialize(&event, NULL,
-				     (yaml_char_t *)YAML_STR_TAG,
-				     (yaml_char_t *)"rule_type",
-				     strlen("rule_type"), 1, 0,
-				     YAML_PLAIN_SCALAR_STYLE);
-	rc = yaml_emitter_emit(&output, &event);
+	rc = lnet_yaml_uint_mapping(&event, &output, "rule_type", &opc,
+				    sizeof(__u32));
 	if (rc == 0)
 		goto emitter_error;
 
-	snprintf(num, sizeof(num), "%d", opc);
-	yaml_scalar_event_initialize(&event, NULL,
-				     (yaml_char_t *)YAML_INT_TAG,
-				     (yaml_char_t *)num,
-				     strlen(num), 1, 0,
-				     YAML_PLAIN_SCALAR_STYLE);
-	rc = yaml_emitter_emit(&output, &event);
+	if (opc == LNET_CTL_DROP_LIST || opc == LNET_CTL_DELAY_LIST ||
+	    opc == LNET_CTL_DROP_RESET || opc == LNET_CTL_DELAY_RESET)
+		goto yaml_mapping_end_event;
+
+	rc = lnet_yaml_str_mapping(&event, &output, "fa_src",
+				   libcfs_nidstr(&fa_src));
 	if (rc == 0)
 		goto emitter_error;
 
+	rc = lnet_yaml_str_mapping(&event, &output, "fa_dst",
+				   libcfs_nidstr(&fa_dst));
+	if (rc == 0)
+		goto emitter_error;
+
+	if (opc == LNET_CTL_DROP_DEL || opc == LNET_CTL_DELAY_DEL)
+		goto yaml_mapping_end_event;
+
+	rc = lnet_yaml_str_mapping(&event, &output, "fa_local_nid",
+				   libcfs_nidstr(&fa_local_nid));
+	if (rc == 0)
+		goto emitter_error;
+
+	rc = lnet_yaml_uint_mapping(&event, &output, "fa_ptl_mask",
+				    &attr->fa_ptl_mask,
+				    sizeof(attr->fa_ptl_mask));
+	if (rc == 0)
+		goto emitter_error;
+
+	rc = lnet_yaml_uint_mapping(&event, &output, "fa_msg_mask",
+				    &attr->fa_msg_mask,
+				    sizeof(attr->fa_msg_mask));
+	if (rc == 0)
+		goto emitter_error;
+
+	if (opc == LNET_CTL_DROP_ADD) {
+		rc = lnet_yaml_uint_mapping(&event, &output, "da_rate",
+					    &attr->u.drop.da_rate,
+					    sizeof(attr->u.drop.da_rate));
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_uint_mapping(&event, &output, "da_interval",
+					    &attr->u.drop.da_interval,
+					    sizeof(attr->u.drop.da_interval));
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_uint_mapping(&event, &output,
+					    "da_health_error_mask",
+					    &attr->u.drop.da_health_error_mask,
+					    sizeof(attr->u.drop.da_health_error_mask));
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_bool_mapping(&event, &output, "da_random",
+					    attr->u.drop.da_random ? "True" :
+								     "False");
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_bool_mapping(&event, &output, "da_drop_all",
+					    attr->u.drop.da_drop_all ? "True" :
+								       "False");
+		if (rc == 0)
+			goto emitter_error;
+	} else if (opc == LNET_CTL_DELAY_ADD) {
+		rc = lnet_yaml_uint_mapping(&event, &output, "la_rate",
+					    &attr->u.delay.la_rate,
+					    sizeof(attr->u.delay.la_rate));
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_uint_mapping(&event, &output, "la_interval",
+					    &attr->u.delay.la_interval,
+					    sizeof(attr->u.delay.la_interval));
+		if (rc == 0)
+			goto emitter_error;
+
+		rc = lnet_yaml_uint_mapping(&event, &output, "la_latency",
+					    &attr->u.delay.la_latency,
+					    sizeof(attr->u.delay.la_latency));
+		if (rc == 0)
+			goto emitter_error;
+	}
+
+yaml_mapping_end_event:
 	yaml_mapping_end_event_initialize(&event);
 	rc = yaml_emitter_emit(&output, &event);
 	if (rc == 0)
@@ -6114,4 +6272,131 @@ int lustre_yaml_exec(char *f, int len, struct cYAML **show_rc,
 {
 	return lustre_yaml_cb_helper(f, len, lookup_exec_tbl,
 				     show_rc, err_rc);
+}
+
+static int
+val_to_str(char *buf, int buf_size, const void *val, int size,
+	   bool is_unsigned)
+{
+	int rc = 0;
+
+	if (is_unsigned) {
+		switch (size) {
+		case sizeof(__u8):
+			rc = snprintf(buf, buf_size, "%u", *((__u8 *)val));
+			break;
+		case sizeof(__u16):
+			rc = snprintf(buf, buf_size, "%u", *((__u16 *)val));
+			break;
+		case sizeof(__u32):
+			rc = snprintf(buf, buf_size, "%u", *((__u32 *)val));
+			break;
+		case sizeof(__u64):
+			rc = snprintf(buf, buf_size, "%llu", *((__u64 *)val));
+			break;
+		default:
+			return -EINVAL;
+		}
+	} else {
+		switch (size) {
+		case sizeof(__s8):
+			rc = snprintf(buf, buf_size, "%d", *((__s8 *)val));
+			break;
+		case sizeof(__s16):
+			rc = snprintf(buf, buf_size, "%d", *((__s16 *)val));
+			break;
+		case sizeof(__s32):
+			rc = snprintf(buf, buf_size, "%d", *((__s32 *)val));
+			break;
+		case sizeof(__s64):
+			rc = snprintf(buf, buf_size, "%lld", *((__s64 *)val));
+			break;
+		default:
+			return -EINVAL;
+		}
+	}
+
+	return (rc >= 0 && rc < buf_size) ? 0 : -EOVERFLOW;
+}
+
+static int
+lnet_yaml_event_helper(yaml_event_t *event, yaml_emitter_t *emitter,
+		       const char *key, yaml_char_t *tag, const void *val,
+		       int size, bool is_unsigned)
+{
+	int rc;
+
+	yaml_scalar_event_initialize(event, NULL,
+				     (yaml_char_t *)YAML_STR_TAG,
+				     (yaml_char_t *)key,
+				     strlen(key), 1, 0,
+				     YAML_PLAIN_SCALAR_STYLE);
+	rc = yaml_emitter_emit(emitter, event);
+	if (rc == 0)
+		return rc;
+
+	if (strcmp((char *)tag, YAML_INT_TAG) == 0) {
+		char num[INT_STRING_LEN];
+
+		if (val_to_str(num, INT_STRING_LEN, val, size, is_unsigned))
+			return 0;
+
+		yaml_scalar_event_initialize(event, NULL,
+					     (yaml_char_t *)YAML_INT_TAG,
+					     (yaml_char_t *)num,
+					     strlen(num), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+	} else if (strcmp((char *)tag, YAML_STR_TAG) == 0) {
+		yaml_scalar_event_initialize(event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)val,
+					     strlen((char *)val), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+	} else if (strcmp((char *)tag, YAML_BOOL_TAG) == 0) {
+		yaml_scalar_event_initialize(event, NULL,
+					     (yaml_char_t *)YAML_BOOL_TAG,
+					     (yaml_char_t *)val,
+					     strlen((char *)val), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+	} else {
+		return 0;
+	}
+
+	return yaml_emitter_emit(emitter, event);
+}
+
+int
+lnet_yaml_int_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		      const char *key, const void *val, int size)
+{
+	return lnet_yaml_event_helper(event, emitter, key,
+				      (yaml_char_t *)YAML_INT_TAG, val, size,
+				      false);
+}
+
+int
+lnet_yaml_uint_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		       const char *key, const void *val, int size)
+{
+	return lnet_yaml_event_helper(event, emitter, key,
+				      (yaml_char_t *)YAML_INT_TAG, val, size,
+				      true);
+}
+
+int
+lnet_yaml_bool_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		       const char *key, const char *val)
+{
+	return lnet_yaml_event_helper(event, emitter, key,
+				      (yaml_char_t *)YAML_BOOL_TAG, val, 0,
+				      false);
+}
+
+int
+lnet_yaml_str_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		      const char *key, const char *val)
+{
+	return lnet_yaml_event_helper(event, emitter, key,
+				      (yaml_char_t *)YAML_STR_TAG, val, 0,
+				      false);
 }

lnet/utils/lnetconfig/liblnetconfig.h
@@ -1014,4 +1014,17 @@ int lustre_lnet_show_peer_debug_info(char *peer_nid, int seq_no,
 int lustre_lnet_set_peer_state(int state, char *lpni_nid, int seq_no,
 			       struct cYAML **err_rc);
 
+int
+lnet_yaml_int_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		      const char *key, const void *val, int size);
+int
+lnet_yaml_uint_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		       const char *key, const void *val, int size);
+int
+lnet_yaml_bool_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		       const char *key, const char *val);
+int
+lnet_yaml_str_mapping(yaml_event_t *event, yaml_emitter_t *emitter,
+		      const char *key, const char *val);
+
 #endif /* LIB_LNET_CONFIG_API_H */

commit 3fe8d551cb945c5ad10ebf6fd6e28915e28a8423

LU-17976 build: fix nla_strnid() -Werror=missing-prototypes
This patch explicitly defines nla_strnid() as a static function
to fix the following build failure:

  lnet/lnet/api-ni.c:2929:1: error:
  no previous prototype for 'nla_strnid' [-Werror=missing-prototypes]
   2929 | nla_strnid(struct nlattr **attr, struct lnet_nid *nid, int *rem,
        | ^~~~~~~~~~

It also fixes:
- lnet_fault_show_done()
- lnet_fault_show_start()
- lnet_fault_show_dump()
- __ll_dio_user_copy()
- ll_dio_user_copy_helper()

Change-Id: I0225794b3fac2f36aafadff783ca921fbc757edd
Fixes: f1c6623 ("LU-10391 lnet: Fault injection add/del ioctls to netlink")
Fixes: 1fa633c ("LU-17478 clio: parallelize unaligned DIO write copy")
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55537
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/lnet/api-ni.c
@@ -2925,7 +2925,7 @@ nla_extract_val(struct nlattr **attr, int *rem,
 	RETURN(rc);
 }
 
-int
+static int
 nla_strnid(struct nlattr **attr, struct lnet_nid *nid, int *rem,
 	   struct netlink_ext_ack *extack)
 {
@@ -9294,7 +9294,7 @@ lnet_fault_dump_ctx(struct netlink_callback *cb)
 	return (struct lnet_genl_fault_rule_list *)cb->args[0];
 }
 
-int lnet_fault_show_done(struct netlink_callback *cb)
+static int lnet_fault_show_done(struct netlink_callback *cb)
 {
 	struct lnet_genl_fault_rule_list *rlist = lnet_fault_dump_ctx(cb);
 
@@ -9308,7 +9308,7 @@ int lnet_fault_show_done(struct netlink_callback *cb)
 	RETURN(0);
 }
 
-int lnet_fault_show_start(struct netlink_callback *cb)
+static int lnet_fault_show_start(struct netlink_callback *cb)
 {
 	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = NULL;
@@ -9456,7 +9456,8 @@ static const struct ln_key_list fault_attr_list = {
 	},
 };
 
-int lnet_fault_show_dump(struct sk_buff *msg, struct netlink_callback *cb)
+static int lnet_fault_show_dump(struct sk_buff *msg,
+				struct netlink_callback *cb)
 {
 	struct lnet_genl_fault_rule_list *rlist = lnet_fault_dump_ctx(cb);
 #ifdef HAVE_NL_PARSE_WITH_EXT_ACK

lustre/obdclass/cl_io.c
@@ -1465,7 +1465,7 @@ EXPORT_SYMBOL(ll_release_user_pages);
 #endif
 
 /* copy IO data to/from internal buffer and userspace iovec */
-ssize_t __ll_dio_user_copy(struct cl_sub_dio *sdio)
+static ssize_t __ll_dio_user_copy(struct cl_sub_dio *sdio)
 {
 	struct iov_iter *iter = &sdio->csd_iter;
 	struct ll_dio_pages *pvec = &sdio->csd_dio_pages;
@@ -1615,7 +1615,7 @@ struct dio_user_copy_data {
 	ssize_t ducd_result;
 };
 
-int ll_dio_user_copy_helper(void *data)
+static int ll_dio_user_copy_helper(void *data)
 {
 	struct dio_user_copy_data *ducd = data;
 	struct cl_sub_dio *sdio = ducd->ducd_sdio;