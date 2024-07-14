lustre/lustre-release

commit 14ca3157b21d8bd22be29c9578819b72fd39a1e5

LU-10948 llite: Revalidate dentries in ll_intent_file_open
We might get a lookup lock in response to our open request and we
definitely want to ensure that our dentry is valid, so it could
actually be matched by dcache code in future operations.

Benchmark results:

This patch can significantly improve open-create + stat on the same
client.

This patch in combination with two others:

https://review.whamcloud.com/#/c/33584
https://review.whamcloud.com/#/c/33585

Improves the 'stat' side of open-create + stat by >10x.

Without patches (master branch commit 26a7abe):

mpirun -np 24 --allow-run-as-root /work/tools/bin/mdtest -n 50000 -d /cache1/out/ -F -C -T -v -w 32k

   Operation                      Max            Min           Mean        Std Dev
   ---------                      ---            ---           ----        -------
   File creation     :       3838.205       3838.204       3838.204          0.000
   File stat         :      33459.289      33459.249      33459.271          0.011
   File read         :          0.000          0.000          0.000          0.000
   File removal      :          0.000          0.000          0.000          0.000
   Tree creation     :       3146.841       3146.841       3146.841          0.000
   Tree removal      :          0.000          0.000          0.000          0.000
   
With the three patches:

mpirun -np 24 --allow-run-as-root /work/tools/bin/mdtest -n 50000 -d /cache1/out/ -F -C -T -v -w 32k
SUMMARY rate: (of 1 iterations)
   Operation                      Max            Min           Mean        Std Dev
   ---------                      ---            ---           ----        -------
   File creation     :       3822.440       3822.439       3822.440          0.000
   File stat         :     350620.140     350615.980     350617.193          1.051
   File read         :          0.000          0.000          0.000          0.000
   File removal      :          0.000          0.000          0.000          0.000

lustre/llite/file.c
@@ -413,25 +413,12 @@ void ll_dom_finish_open(struct inode *inode, struct ptlrpc_request *req,
 	struct page *vmpage;
 	struct niobuf_remote *rnb;
 	char *data;
-	struct lustre_handle lockh;
-	struct ldlm_lock *lock;
 	unsigned long index, start;
 	struct niobuf_local lnb;
-	bool dom_lock = false;
 
 	ENTRY;
 
 	if (obj == NULL)
-		RETURN_EXIT;
-
-	if (it->it_lock_mode != 0) {
-		lockh.cookie = it->it_lock_handle;
-		lock = ldlm_handle2lock(&lockh);
-		if (lock != NULL)
-			dom_lock = ldlm_has_dom(lock);
-		LDLM_LOCK_PUT(lock);
-	}
-	if (!dom_lock)
 		RETURN_EXIT;
 
 	if (!req_capsule_has_field(&req->rq_pill, &RMF_NIOBUF_INLINE,
@@ -570,8 +557,27 @@ retry:
 	rc = ll_prep_inode(&de->d_inode, req, NULL, itp);
 
 	if (!rc && itp->it_lock_mode) {
-		ll_dom_finish_open(de->d_inode, req, itp);
+		struct lustre_handle handle = {.cookie = itp->it_lock_handle};
+		struct ldlm_lock *lock;
+		bool has_dom_bit = false;
+
+		/* If we got a lock back and it has a LOOKUP bit set,
+		 * make sure the dentry is marked as valid so we can find it.
+		 * We don't need to care about actual hashing since other bits
+		 * of kernel will deal with that later.
+		 */
+		lock = ldlm_handle2lock(&handle);
+		if (lock) {
+			has_dom_bit = ldlm_has_dom(lock);
+			if (lock->l_policy_data.l_inodebits.bits &
+			    MDS_INODELOCK_LOOKUP)
+				d_lustre_revalidate(de);
+
+			LDLM_LOCK_PUT(lock);
+		}
 		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, NULL);
+		if (has_dom_bit)
+			ll_dom_finish_open(de->d_inode, req, itp);
 	}
 
 out:

commit 32582842cae452984f74e76a4eb69379cc48ce5f

LU-17583 llite: getattr/open should not revalidate dentry
ll_getattr() and ll_intent_file_open() do not perform a lookup, it
get the attr and ldlm locks by FID (inode). So this should not
revalidate the dentry, otherwise it may produce dir cache
inconsistencies (e.g: with cwd fd).

Add a regression test: sanityn 31s, 31t

Fixes: 14ca315 ("LU-10948 llite: Revalidate dentries in ll_intent_file_open")
Fixes: 92fadf9 ("LU-15200 llite: revalidate dentry if LOOKUP lock fetched")
Test-Parameters: testlist=sanityn env=ONLY=31s,ONLY_REPEAT=20
Test-Parameters: testlist=sanityn env=ONLY=31t,ONLY_REPEAT=20
Signed-off-by: Etienne AUJAMES <eaujames@ddn.com>
Change-Id: Ic9823cddf37373dc95f4de3219c88c0fa0600fa7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54354
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>

lustre/llite/dcache.c
@@ -273,16 +273,12 @@ int ll_revalidate_it_finish(struct ptlrpc_request *request,
 
 	ll_set_lock_data(ll_i2sbi(inode)->ll_md_exp, inode, it,
 			 &bits);
-	if (bits & MDS_INODELOCK_LOOKUP) {
-		if (!ll_d_setup(de, true))
-			RETURN(-ENOMEM);
-		d_lustre_revalidate(de);
-		if (S_ISDIR(inode->i_mode)) {
-			struct dentry *parent = dget_parent(de);
+	if ((bits & MDS_INODELOCK_LOOKUP) &&
+	    !d_lustre_invalid(de) && S_ISDIR(inode->i_mode)) {
+		struct dentry *parent = dget_parent(de);
 
-			ll_update_dir_depth_dmv(d_inode(parent), de);
-			dput(parent);
-		}
+		ll_update_dir_depth_dmv(d_inode(parent), de);
+		dput(parent);
 	}
 
 	RETURN(rc);

lustre/llite/file.c
@@ -729,25 +729,17 @@ retry:
 	if (!rc && itp->it_lock_mode) {
 		__u64 bits = 0;
 
-		/* If we got a lock back and it has a LOOKUP bit set,
-		 * make sure the dentry is marked as valid so we can find it.
-		 * We don't need to care about actual hashing since other bits
-		 * of kernel will deal with that later.
-		 */
-		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, &bits);
-		if (bits & MDS_INODELOCK_LOOKUP)
-			d_lustre_revalidate(de);
-
 		/* if DoM bit returned along with LAYOUT bit then there
 		 * can be read-on-open data returned.
 		 */
+		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, &bits);
 		if (bits & MDS_INODELOCK_DOM && bits & MDS_INODELOCK_LAYOUT)
 			ll_dom_finish_open(de->d_inode, req);
 	}
 	/* open may not fetch LOOKUP lock, update dir depth and default LMV
 	 * anyway.
 	 */
-	if (!rc && S_ISDIR(de->d_inode->i_mode))
+	if (!rc && !d_lustre_invalid(de) && S_ISDIR(de->d_inode->i_mode))
 		ll_update_dir_depth_dmv(parent->d_inode, de);
 
 out:
@@ -5700,13 +5692,17 @@ static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 	struct md_op_data *op_data;
 	const char *name = NULL;
 	size_t namelen = 0;
+	int flags = 0;
 	int rc = 0;
 
 	ENTRY;
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p),name=%s\n",
 	       PFID(ll_inode2fid(inode)), inode, dentry->d_name.name);
 
-	if (exp_connect_flags2(exp) & OBD_CONNECT2_GETATTR_PFID) {
+	/* Call getattr by fid */
+	if ((exp_connect_flags2(exp) & OBD_CONNECT2_GETATTR_PFID) &&
+		!d_lustre_invalid(dentry)) {
+		flags = MF_GETATTR_BY_FID;
 		parent = dget_parent(dentry);
 		dir = d_inode(parent);
 		name = dentry->d_name.name;
@@ -5722,9 +5718,7 @@ static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
-	/* Call getattr by fid */
-	if (exp_connect_flags2(exp) & OBD_CONNECT2_GETATTR_PFID)
-		op_data->op_flags = MF_GETATTR_BY_FID;
+	op_data->op_flags |= flags;
 	rc = md_intent_lock(exp, op_data, &oit, &req, &ll_md_blocking_ast, 0);
 	ll_finish_md_op_data(op_data);
 	if (rc < 0) {

lustre/tests/sanityn.sh
@@ -1208,6 +1208,50 @@ test_31r() {
 }
 run_test 31r "open-rename(replace) race"
 
+test_31s() {
+	local pid ls_str
+
+	mkdir_on_mdt0 $DIR/$tdir
+	touch $DIR/$tdir/$tfile || error "touch $tdir/$tfile failed"
+	( cd $DIR/$tdir; tail -f $tfile || $MULTIOP . Dc) & pid=$!
+
+	stack_trap "pkill -P $pid 2> /dev/null" ERR
+
+	mv $DIR2/$tdir $DIR2/$tdir.old || error "mv $tdir $tdir.old failed"
+	mkdir_on_mdt0 $DIR2/$tdir || error "mkdir $DIR2/$tdir failed"
+	mv $DIR2/$tdir.old $DIR2/$tdir/ || error "mv $tdir.old $tdir/ failed"
+
+	pkill -P $pid || error "fail to stop $pid"
+	wait $pid
+
+	ls_str="$(ls  $DIR/$tdir)" || error "ls $DIR/$tdir failed"
+	[[ "$ls_str" == "$tdir.old" ]] ||
+		error "invalid dentry $DIR/$tdir: $ls_str != $tdir.old"
+}
+run_test 31s "open should not revalidate invalid dentry"
+
+test_31t() {
+	local pid i i_new
+
+	touch $DIR/$tfile || error "touch $tfile failed"
+
+	$MULTIOP $DIR/$tfile o_Sc & pid=$!
+	stack_trap "kill $pid 2> /dev/null" ERR
+	sleep 1
+
+	mv $DIR2/$tfile $DIR2/$tfile.old || error "mv $tfile $tfile.old failed"
+	touch $DIR2/$tfile || error "touch $tfile failed"
+	i_new=$(stat -c "%i" $DIR2/$tfile) || error "stat $tfile failed"
+
+	kill -SIGUSR1 $pid || error "fail to stop $pid"
+	wait $pid
+
+	i=$(stat -c "%i" $DIR/$tfile) || error "stat $tfile failed"
+	(( i == i_new )) ||
+		error "invalid dentry $DIR/$tfile, not updated (inode: $i != $i_new)"
+}
+run_test 31t "getattr should not revalidate invalid dentry"
+
 test_32b() { # bug 11270
 	remote_ost_nodsh && skip "remote OST with nodsh" && return
 