b368c872265454acff03a14d76bcaa1611b00f76
Timothy Day <timday@amazon.com>
2024-07-29 23:15:24 -0400

    LU-6142 fid: SPDX for FID/FLD code
    Convert from verbose license text to SDPX.
    
    Test-Parameters: trivial
    Signed-off-by: Timothy Day <timday@amazon.com>
    Change-Id: Iaadd10e60e08fb3f551812289e2c30de12824fd4
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55886
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Reviewed-by: jsimmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/fld/fld_handler.c
+++ lustre/fld/fld_handler.c
@@ -1,34 +1,14 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
  * Copyright (c) 2011, 2015, Intel Corporation.
  */
+
 /*
  * This file is part of Lustre, http://www.lustre.org/
- *
- * lustre/fld/fld_handler.c
  *
  * FLD (Fids Location Database)
  *
