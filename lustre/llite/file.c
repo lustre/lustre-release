lustre/lustre-release

commit 07a1272233c56ac0191a4e9e20d77234b3fb3d90

LU-17525 llite: unaligned DIO interop page alignment
Correctly size brw/ptlrpc bulk ops I/O between archs with differing
page sizes (ex: 64k client and 4k server).

Since the number of MDs need for a bulk are not stable until all the
pages are added we have two parts to interop calculation.

In the fist step if this is an unaligned dio bulk with a 64k offset
greater than 4k in size calculate the initial MD utilization
as if first partial page is 64k in length.

In the second step after bulk is sized and split across 1 or more
MDs, if the number of MDs:
 - if the number of MDs is 1, clear the interop flag
 - if the number of MDs is 3 or more, keep the interop flag
 - if the number of MDs is 2 and the size with the 64k offset does
   not exceed the LNET_MUT then collapse the extra MD.
This is done by assuming the first page is 64k length.

Additionally fixup OBD_CONNECT2_UNALIGNED_DIO and add a ZFS osd
heuristic check. No unaligned DIO should be performed with older
zfs-osd, however unaligned dio with unpatched ldiskfs servers is
allowed for most cases. Allow I/O that will trigger the page
size interop issue will get fail with -EINVAL, were previously
these i/o would fail sending with an error:

LNetError: 7386:0:(lib-ptl.c:189:lnet_try_match_md()) Matching packet
   from 12345-10.240.22.81@tcp, match 1789613636069888 length 1044481
   too big: 983041 left, 983041 allowed

triggering the MD to be resent, however the size calcuations reamin
unchanged resulting in a hang.

Test-Parameters: testlist=sanity clientarch=aarch64 clientdistro=el9.3
Test-Parameters: testlist=sanity clientarch=ppc64le clientdistro=el8.8 env=SANITY_EXCEPT="398c 411b"
Test-Parameters: testlist=sanity serverversion=2.15.4 serverdistro=el8.9 env=SANITY_EXCEPT="17n 24g 27R 56oc 56wc 162c 230b 230c 230x 230t 273c 300i"

lustre/osc/osc_io.c
@@ -1340,16 +1340,18 @@ static const struct cl_io_operations osc_io_ops = {
 int osc_io_init(const struct lu_env *env,
                 struct cl_object *obj, struct cl_io *io)
 {
-	struct obd_export *exp = osc_export(cl2osc(obj));
-        struct osc_io *oio = osc_env_io(env);
+	struct osc_io *oio = osc_env_io(env);
+	struct osc_object *osc = cl2osc(obj);
+	struct obd_export *exp = osc_export(osc);
 
-        CL_IO_SLICE_CLEAN(oio, oi_cl);
-        cl_io_slice_add(io, &oio->oi_cl, obj, &osc_io_ops);
+	CL_IO_SLICE_CLEAN(oio, oi_cl);
+	cl_io_slice_add(io, &oio->oi_cl, obj, &osc_io_ops);
 
+	cl_io_top(io)->ci_target_is_zfs = exp_connect_target_is_zfs(exp);
 	if (!exp_connect_unaligned_dio(exp))
 		cl_io_top(io)->ci_allow_unaligned_dio = false;
 
-        return 0;
+	return 0;
 }
 
 /** @} osc */

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -432,8 +432,9 @@ struct lnet_md {
 /* Max Transfer Unit (minimum supported everywhere).
  * CAVEAT EMPTOR, with multinet (i.e. routers forwarding between networks)
  * these limits are system wide and not interface-local. */
-#define LNET_MTU_BITS	20
-#define LNET_MTU	(1 << LNET_MTU_BITS)
+#define LNET_MTU_BITS		20
+#define LNET_MTU		(1u << LNET_MTU_BITS)
+#define LNET_MTU_IOV_LIMIT	(1u << (LNET_MTU_BITS - PAGE_SHIFT))
 
 /**
  * Options for the MD structure. See struct lnet_md::options.

lustre/include/cl_object.h
@@ -1933,6 +1933,12 @@ struct cl_io {
 	 * to annotate that in the IO (since we learn if there is a problematic
 	 * OST/MDT target as we build the IO)
 	 */
+			     ci_target_is_zfs:1,
+	/**
+	 * there is an interop issue with unpatched clients/servers that
+	 * exceed 4k read/write offsets with I/O exceeding LNET_MTU.
+	 * This flag cleared if a target is not patched.
+	 */
 			     ci_allow_unaligned_dio:1,
 	/**
 	 * Bypass quota check
@@ -2544,6 +2550,11 @@ struct cl_sub_dio {
 				csd_write:1,
 				csd_unaligned:1;
 };
+
+static inline u64 cl_io_nob_aligned(u64 off, u32 nob, u32 pgsz)
+{
+	return (((nob / pgsz) - 1) * pgsz) + (pgsz - (off & (pgsz - 1)));
+}
 
 void ll_release_user_pages(struct page **pages, int npages);
 int ll_allocate_dio_buffer(struct ll_dio_pages *pvec, size_t io_size);

lustre/include/lustre_export.h
@@ -520,6 +520,13 @@ static inline bool imp_connect_replay_create(struct obd_import *imp)
 #define imp_connect_replay_create(exp) true
 #endif
 
+static inline bool imp_connect_unaligned_dio(struct obd_import *imp)
+{
+	struct obd_connect_data *ocd = &imp->imp_connect_data;
+
+	return (ocd->ocd_connect_flags2 & OBD_CONNECT2_UNALIGNED_DIO);
+}
+
 static inline bool exp_connect_unaligned_dio(struct obd_export *exp)
 {
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_UNALIGNED_DIO);
@@ -528,6 +535,18 @@ static inline bool exp_connect_unaligned_dio(struct obd_export *exp)
 static inline bool exp_connect_batch_rpc(struct obd_export *exp)
 {
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_BATCH_RPC);
+}
+
+static inline bool exp_connect_target_is_zfs(struct obd_export *exp)
+{
+	struct obd_connect_data *ocd = &exp->exp_connect_data;
+	bool is_zfs = false;
+
+	/* > 2ULL << 59 implies ZFS, so this is ldiskfs */
+	if (exp_connect_flags(exp) & OBD_CONNECT_MAXBYTES)
+		is_zfs = !(ocd->ocd_maxbytes < (2ULL << 59));
+
+	return is_zfs;
 }
 
 enum {

lustre/include/lustre_net.h
@@ -81,7 +81,12 @@
 #if PTLRPC_BULK_OPS_BITS > 16
 #error "More than 65536 BRW RPCs not allowed by IOOBJ_MAX_BRW_BITS."
 #endif
-#define PTLRPC_BULK_OPS_COUNT	(1U << PTLRPC_BULK_OPS_BITS)
+/**
+ * PTLRPC_BULK_OPS_COUNT is a protocol maximum and must be a power of 2
+ * However PTLRPC_BULK_OPS_LIMIT (ops_count/2 +1) is enforced as the
+ * 64G with alignment interop limit.
+ */
+#define PTLRPC_BULK_OPS_COUNT	(1U << (PTLRPC_BULK_OPS_BITS + 1))
 /**
  * PTLRPC_BULK_OPS_MASK is for the convenience of the client only, and
  * should not be used on the server at all.  Otherwise, it imposes a
@@ -91,7 +96,14 @@
  * RPC count.
  */
 #define PTLRPC_BULK_OPS_MASK	(~((__u64)PTLRPC_BULK_OPS_COUNT - 1))
-
+/*
+ * Unaligned DIO adjust MD size for alignment to the interop page size
+ * Enable page alignmen interop range:
+ *  MD_MAX_INTEROP_PAGE_SIZE(64k) <-> MD_MIN_INTEROP_PAGE_SIZE(4k)
+ */
+#define MD_MIN_INTEROP_PAGE_SHIFT	12
+#define MD_MIN_INTEROP_PAGE_SIZE	(1u << MD_MIN_INTEROP_PAGE_SHIFT)
+#define MD_MAX_INTEROP_PAGE_SIZE	(1u << 16)
 /**
  * Define maxima for bulk I/O.
  *
@@ -110,6 +122,8 @@
 #define DT_DEF_BRW_SIZE		(4 * ONE_MB_BRW_SIZE)
 #define DT_MAX_BRW_PAGES	(DT_MAX_BRW_SIZE >> PAGE_SHIFT)
 #define OFD_MAX_BRW_SIZE	(1U << LNET_MTU_BITS)
+/* unaligned dio needs an extra md vector 65 instead of 64 */
+#define PTLRPC_BULK_OPS_LIMIT	((1U << PTLRPC_BULK_OPS_BITS) + 1)
 
 /* When PAGE_SIZE is a constant, we can check our arithmetic here with cpp! */
 #if ((PTLRPC_MAX_BRW_PAGES & (PTLRPC_MAX_BRW_PAGES - 1)) != 0)
@@ -123,6 +137,9 @@
 #endif
 #if (PTLRPC_MAX_BRW_PAGES > LNET_MAX_IOV * PTLRPC_BULK_OPS_COUNT)
 # error "PTLRPC_MAX_BRW_PAGES too big"
+#endif
+#if (PTLRPC_BULK_OPS_LIMIT > PTLRPC_BULK_OPS_COUNT)
+# error "PTLRPC_BULK_OPS_LIMIT too big"
 #endif
 
 #define PTLRPC_NTHRS_INIT	2
@@ -1400,9 +1417,10 @@ struct ptlrpc_bulk_desc {
 	/** completed with failure */
 	unsigned long bd_failure:1;
 	/** client side */
-	unsigned long bd_registered:1,
+	unsigned short bd_md_offset; /* offset in 4k pages ranged [0, 15] */
+	unsigned int bd_registered:1,
 	/* bulk request is RDMA transfer, use page->host as real address */
-			bd_is_rdma:1;
+		     bd_is_rdma:1;
 	/** For serialization with callback */
 	spinlock_t bd_lock;
 	/** {put,get}{source,sink}{kvec,kiov} */
@@ -1421,7 +1439,7 @@ struct ptlrpc_bulk_desc {
 	int			bd_max_iov;	/* allocated size of bd_iov */
 	int			bd_nob;		/* # bytes covered */
 	int			bd_nob_transferred; /* # bytes GOT/PUT */
-	unsigned int		bd_nob_last;	/* # bytes in last MD */
+	unsigned int		bd_iop_len;	/* md iop bytes */
 
 	__u64			bd_last_mbits;
 
@@ -1431,9 +1449,9 @@ struct ptlrpc_bulk_desc {
 	int			bd_md_max_brw;	/* max entries in bd_mds */
 
 	/** array of offsets for each MD */
-	unsigned int		bd_mds_off[PTLRPC_BULK_OPS_COUNT];
+	unsigned int		bd_mds_off[PTLRPC_BULK_OPS_LIMIT];
 	/** array of associated MDs */
-	struct lnet_handle_md	bd_mds[PTLRPC_BULK_OPS_COUNT];
+	struct lnet_handle_md	bd_mds[PTLRPC_BULK_OPS_LIMIT];
 
 	/* encrypted iov, size is either 0 or bd_iov_count. */
 	struct bio_vec *bd_enc_vec;

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1476,12 +1476,15 @@ struct hsm_state_set {
 #define OST_MAX_PRECREATE 20000
 
 struct obd_ioobj {
-	struct ost_id	ioo_oid;	/* object ID, if multi-obj BRW */
-	__u32		ioo_max_brw;	/* low 16 bits were o_mode before 2.4,
-					 * now (PTLRPC_BULK_OPS_COUNT - 1) in
-					 * high 16 bits in 2.4 and later
-					 */
-	__u32		ioo_bufcnt;	/* number of niobufs for this object */
+	/* object ID, if multi-obj BRW */
+	struct ost_id	ioo_oid;
+	/* low 16 bits were o_mode before 2.4, now (PTLRPC_BULK_OPS_LIMIT - 1)
+	 * in high 16 bits in 2.4 and later.
+	 * With OBD_CONNECT2_UNALIGNED_DIO udio uses low 4 bits for offset
+	 */
+	__u32		ioo_max_brw;
+	/* number of niobufs for this object */
+	__u32		ioo_bufcnt;
 };
 
 /* NOTE: IOOBJ_MAX_BRW_BITS defines the _offset_ of the max_brw field in
@@ -1489,9 +1492,16 @@ struct obd_ioobj {
  * That said, ioo_max_brw is a 32-bit field so the limit is also 16 bits.
  */
 #define IOOBJ_MAX_BRW_BITS	16
+#define IOOBJ_OFFSET_BITS	4
+#define IOOBJ_OFFSET_MASK	((1u << IOOBJ_OFFSET_BITS) - 1)
+
+#define ioobj_page_interop_offset(ioo) ((ioo)->ioo_max_brw & IOOBJ_OFFSET_MASK)
 #define ioobj_max_brw_get(ioo)	(((ioo)->ioo_max_brw >> IOOBJ_MAX_BRW_BITS) + 1)
-#define ioobj_max_brw_set(ioo, num)					\
-do { (ioo)->ioo_max_brw = ((num) - 1) << IOOBJ_MAX_BRW_BITS; } while (0)
+#define ioobj_max_brw_set(ioo, num, offset)				\
+do {									\
+	(ioo)->ioo_max_brw = ((num) - 1) << IOOBJ_MAX_BRW_BITS;		\
+	(ioo)->ioo_max_brw |= (offset) & IOOBJ_OFFSET_MASK;		\
+} while (0)
 
 /* multiple of 8 bytes => can array */
 struct niobuf_remote {

lustre/ldlm/ldlm_lib.c
@@ -670,14 +670,6 @@ int client_connect_import(const struct lu_env *env,
 		data->ocd_connect_flags = ocd->ocd_connect_flags;
 		data->ocd_connect_flags2 = ocd->ocd_connect_flags2;
 	}
-	/* ldiskfs servers do not actually need patching to support unaligned
-	 * DIO, so we always set the flag in that case
-	 */
-	if (data->ocd_connect_flags & OBD_CONNECT_MAXBYTES) {
-		/* > 2ULL << 59 implies ZFS, so this is ldiskfs */
-		if (data->ocd_maxbytes < (2ULL << 59))
-			data->ocd_connect_flags2 |= OBD_CONNECT2_UNALIGNED_DIO;
-	}
 
 	ptlrpc_pinger_add_import(imp);
 

lustre/llite/rw26.c
@@ -537,11 +537,28 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	if (unaligned && iov_iter_is_pipe(iter))
 		RETURN(0);
 
-	/* this means we encountered an old server which can't safely support
-	 * unaligned DIO, so we have to disable it
+	/* Unpatched older servers which cannot safely support unaligned DIO
+	 * (osd-zfs) or i/o with page size interop issues should abort here
 	 */
-	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio)
-		RETURN(-EINVAL);
+	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio) {
+		unsigned int md0_offset;
+
+		if (cl_io_top(io)->ci_target_is_zfs)
+			RETURN(-EINVAL);
+
+		/* unpatched ldiskfs is fine, unless MD0 does not align/fit */
+		md0_offset = file_offset & (MD_MAX_INTEROP_PAGE_SIZE - 1);
+		if ((count + md0_offset) >= LNET_MTU) {
+			u64 iomax, iomin;
+
+			iomax = cl_io_nob_aligned(file_offset, count,
+						  MD_MAX_INTEROP_PAGE_SIZE);
+			iomin = cl_io_nob_aligned(file_offset, count,
+						  MD_MIN_INTEROP_PAGE_SIZE);
+			if (iomax != iomin)
+				RETURN(-EINVAL);
+		}
+	}
 
 	/* if one part of an I/O is unaligned, just handle all of it that way -
 	 * otherwise we create significant complexities with managing the iovec

commit b07b31d44ffee44c9044cdebedf1d42a35c82929

LU-17525 llite: unaligned DIO zfs detection
Update to the ZFS detection logic to be stable. If any
component of the I/O is ZFS treat the entire I/O as ZFS

HPE-bug-id: LUS-12175
Fixes: 07a1272233 ("LU-17525 llite: unaligned DIO interop page alignment")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ie9d5c8af373cec96855f81f5330e174ed71f370a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55895
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/file.c
@@ -1699,6 +1699,7 @@ void ll_io_init(struct cl_io *io, struct file *file, enum cl_io_type iot,
 	 * avaliable mirror for write.
 	 */
 	io->ci_ndelay = !(iot == CIT_WRITE);
+	io->ci_target_is_zfs = 0;
 	/* unaligned DIO has compat issues with some older servers, but we find
 	 * out if there are such servers while setting up the IO, so it starts
 	 * out allowed

lustre/osc/osc_io.c
@@ -1351,7 +1351,8 @@ int osc_io_init(const struct lu_env *env,
 	CL_IO_SLICE_CLEAN(oio, oi_cl);
 	cl_io_slice_add(io, &oio->oi_cl, obj, &osc_io_ops);
 
-	cl_io_top(io)->ci_target_is_zfs = exp_connect_target_is_zfs(exp);
+	if (exp_connect_target_is_zfs(exp))
+		cl_io_top(io)->ci_target_is_zfs = 1;
 	if (!exp_connect_unaligned_dio(exp))
 		cl_io_top(io)->ci_allow_unaligned_dio = false;
 