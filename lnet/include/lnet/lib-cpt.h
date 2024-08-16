lustre/lustre-release

commit 0f39311369b97d7ce4e7e4d4f47b9068da805e51

LU-6142 lnet: SPDX for lnet/utils/
Convert from verbose license text to SDPX.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I0568f692c6799834794ed9c565bdac7ec9aef1d3
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54173
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/utils/lnetctl.c
@@ -1,28 +1,15 @@
+// SPDX-License-Identifier: LGPL-2.0
+
 /*
- * LGPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of the
- * License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- *
- * LGPL HEADER END
- *
  * Copyright (c) 2014, 2017, Intel Corporation.
- *
- * Author:
- *   Amir Shehata <amir.shehata@intel.com>
  */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Author: Amir Shehata <amir.shehata@intel.com>
+ */
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>

lnet/utils/Makefile.am
@@ -1,24 +1,4 @@
-#
-# GPL HEADER START
-#
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 only,
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License version 2 for more details (a copy is included
-# in the LICENSE file that accompanied this code).
-#
-# You should have received a copy of the GNU General Public License
-# version 2 along with this program; If not, see
-# http://www.gnu.org/licenses/gpl-2.0.html
-#
-# GPL HEADER END
-#
+# SPDX-License-Identifier: GPL-2.0
 
 #
 # Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.

lnet/utils/lbstats
@@ -4,8 +4,6 @@
 #
 # This file is part of Lustre, http://www.lustre.org/
 #
-# lnet/utils/lbstats
-#
 # Cleanly print out info about lnet router buffers,
 # network interfaces, and peers
 #

lnet/utils/lnetunload
@@ -4,8 +4,6 @@
 #
 # This file is part of Lustre, http://www.lustre.org/
 #
-# lnet/utils/lnetunload
-#
 # Unload lnet related kernel modules
 #
 

lnet/utils/lst.c
@@ -1,37 +1,18 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
  * Copyright (c) 2012, 2017, Intel Corporation.
  */
+
 /*
  * This file is part of Lustre, http://www.lustre.org/
  *
- * lnet/selftest/conctl.c
- *
  * Author: Liang Zhen <liangzhen@clusterfs.com>
  */
+
 #include <errno.h>
 #include <getopt.h>
 #include <inttypes.h>

lnet/utils/routerstat.c
@@ -1,30 +1,12 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
  * Copyright (c) 2011, Intel Corporation.
  */
+
 /*
  * This file is part of Lustre, http://www.lustre.org/
  */

lnet/utils/wirecheck.c
@@ -1,30 +1,12 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
  * Copyright (c) 2014, Intel Corporation.
  */
+
 /*
  * This file is part of Lustre, http://www.lustre.org/
  */

commit 8b422d1c80f9ecae49a5a9a8389de1a9017989a3

LU-6142 lnet: update files with correct SPDX
Some files were given LGPL-2.0 when they should have been
LGPL-2.1 based on the previous license text. Add the Linux
syscall note to UAPI headers.

Add SPDX to a few missed files in LNET.

Fixes: 0f39311369 ("LU-6142 lnet: SPDX for lnet/utils/")
Fixes: c9a7728476 ("LU-6142 lnet: SPDX for lnet/include/ and misc files")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I6ceadfa182909ed4e3e52f88b4d37249cb30b6ef
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55930
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-cpt.h
@@ -1,24 +1,10 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * GPL HEADER END
- */
+/* SPDX-License-Identifier: GPL-2.0 */
+
 /* Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
  *
  * Copyright (c) 2012, 2017, Intel Corporation.
  */
+
 /* This file is part of Lustre, http://www.lustre.org/
  *
  * CPU partition

lnet/include/lnet/lnet_crypto.h
@@ -1,25 +1,4 @@
-/* GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see http://www.gnu.org/licenses
- *
- * Please  visit http://www.xyratex.com/contact if you need additional
- * information or have any questions.
- *
- * GPL HEADER END
- */
+/* SPDX-License-Identifier: GPL-2.0 */
 
 /* Copyright 2012 Xyratex Technology Limited
  *

lnet/include/uapi/linux/lnet/libcfs_debug.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/libcfs_ioctl.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/lnet-crypto.h
@@ -1,25 +1,4 @@
-/* GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see http://www.gnu.org/licenses
- *
- * Please  visit http://www.xyratex.com/contact if you need additional
- * information or have any questions.
- *
- * GPL HEADER END
- */
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright 2012 Xyratex Technology Limited
  *

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+/* SPDX-License-Identifier: LGPL-2.1 WITH Linux-syscall-note */
 
 /* Copyright (c) 2014, 2017, Intel Corporation. */
 

lnet/include/uapi/linux/lnet/lnet-idl.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/lnet-nl.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+/* SPDX-License-Identifier: LGPL-2.1 WITH Linux-syscall-note */
 
 /* Copyright (c) 2021,  UT-Battelle, LLC
  *

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/lnetctl.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2014, 2017, Intel Corporation. */
 

lnet/include/uapi/linux/lnet/lnetst.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/nidstr.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/include/uapi/linux/lnet/socklnd.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
 /* Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.

lnet/klnds/in-kernel-o2iblnd/Makefile.in
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
 MODULES := in-kernel-ko2iblnd
 in-kernel-ko2iblnd-objs := o2iblnd.o o2iblnd_cb.o o2iblnd_modparams.o
 

lnet/klnds/in-kernel-o2iblnd/autoMakefile.am
@@ -1,24 +1,4 @@
-#
-# GPL HEADER START
-#
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 only,
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License version 2 for more details (a copy is included
-# in the LICENSE file that accompanied this code).
-#
-# You should have received a copy of the GNU General Public License
-# version 2 along with this program; If not, see
-# http://www.gnu.org/licenses/gpl-2.0.html
-#
-# GPL HEADER END
-#
+# SPDX-License-Identifier: GPL-2.0
 
 #
 # Copyright (c) 2004, 2010, Oracle and/or its affiliates. All rights reserved.

lnet/lnet/adler.c
@@ -1,25 +1,4 @@
-/* GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see http://www.gnu.org/licenses
- *
- * Please  visit http://www.xyratex.com/contact if you need additional
- * information or have any questions.
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
 
 /* Copyright 2012 Xyratex Technology Limited
  *

lnet/lnet/adler.h
@@ -1,26 +1,4 @@
- /*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see http://www.gnu.org/licenses
- *
- * Please  visit http://www.xyratex.com/contact if you need additional
- * information or have any questions.
- *
- * GPL HEADER END
- */
+/* SPDX-License-Identifier: GPL-2.0 */
 
 /* Copyright 2012 Xyratex Technology Limited
  *

lnet/lnet/lnet-crypto.c
@@ -1,25 +1,4 @@
-/* GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see http://www.gnu.org/licenses
- *
- * Please  visit http://www.xyratex.com/contact if you need additional
- * information or have any questions.
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
 
 /* Copyright 2012 Xyratex Technology Limited
  *

lnet/utils/lnetconfig/Makefile.am
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: LGPL-2.0
+# SPDX-License-Identifier: LGPL-2.1
 
 #
 # Copyright (c) 2014, 2017, Intel Corporation.

lnet/utils/lnetconfig/cyaml.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+// SPDX-License-Identifier: LGPL-2.1
 
 /*
  * Copyright (c) 2014, 2017, Intel Corporation.

lnet/utils/lnetconfig/cyaml.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+/* SPDX-License-Identifier: LGPL-2.1 */
 
 /*
  * Copyright (c) 2014, 2017, Intel Corporation.

lnet/utils/lnetconfig/liblnd.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+/* SPDX-License-Identifier: LGPL-2.1 */
 
 /*
  * Copyright (c) 2015, James Simmons <jsimmons@infradead.org>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+// SPDX-License-Identifier: LGPL-2.1
 
 /*
  * Copyright (c) 2014, 2017, Intel Corporation.

lnet/utils/lnetconfig/liblnetconfig.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+/* SPDX-License-Identifier: LGPL-2.1 */
 
 /*
  * Copyright (c) 2014, 2017, Intel Corporation.

lnet/utils/lnetconfig/liblnetconfig_lnd.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+// SPDX-License-Identifier: LGPL-2.1
 
 /*
  * Copyright (c) 2015, James Simmons

lnet/utils/lnetconfig/liblnetconfig_netlink.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+// SPDX-License-Identifier: LGPL-2.1
 
 /*
  * Copyright (c) 2021  UT-Battelle, LLC

lnet/utils/lnetctl.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.0
+// SPDX-License-Identifier: LGPL-2.1
 
 /*
  * Copyright (c) 2014, 2017, Intel Corporation.