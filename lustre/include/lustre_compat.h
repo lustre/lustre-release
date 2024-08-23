lustre/lustre-release

commit b9ef5d1e7f7dd1055a6ea6d3dc9f176fa910a372

LU-17546 osd: use __vfs_removexattr
as otherwise vfs_removexattr() taking inode's lock confict with
osd_execute_truncate() while we don't really need inode's lock
because another per-object lock has been already taken.

Fixes: dcd5607ce0 ("LU-13430 vfs: add ll_vfs_getxattr/ll_vfs_setxattr compat macro")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I43c1c60d2a9f911b6395e1b7546507074a90b1cf
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54072
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/lustre_compat.h
@@ -559,7 +559,7 @@ static inline int ll_vfs_setxattr(struct dentry *dentry, struct inode *inode,
 				  const void *value, size_t size, int flags)
 {
 #if defined(HAVE_MNT_IDMAP_ARG) || defined(HAVE_USER_NAMESPACE_ARG)
-	return vfs_setxattr(&nop_mnt_idmap, dentry, name,
+	return __vfs_setxattr(&nop_mnt_idmap, dentry, inode, name,
 			    VFS_SETXATTR_VALUE(value), size, flags);
 #elif defined(HAVE_VFS_SETXATTR)
 	return __vfs_setxattr(dentry, inode, name, value, size, flags);
@@ -575,7 +575,7 @@ static inline int ll_vfs_removexattr(struct dentry *dentry, struct inode *inode,
 				     const char *name)
 {
 #if defined(HAVE_MNT_IDMAP_ARG) || defined(HAVE_USER_NAMESPACE_ARG)
-	return vfs_removexattr(&nop_mnt_idmap, dentry, name);
+	return __vfs_removexattr(&nop_mnt_idmap, dentry, name);
 #elif defined(HAVE_VFS_SETXATTR)
 	return __vfs_removexattr(dentry, name);
 #else

commit 215f1905cbaa0fadd6dee682c1f9c3782288c08c

LU-18088 osd: use __vfs_getxattr
vfs_getxattr() is not suitable for osd. To get security xattr it goes
via:

vfs_getxattr
  xattr_getsecurity
    security_inode_getsecurity
      cap_inode_getsecurity
        dentry = d_find_any_alias(inode)
          if (!dentry)
            return -EINVAL;
and return -EINVAL because osd object inodes have empty alias list.

Use __vfs_getxattr() instead.

Fixes: b9ef5d1e7f7 ("LU-17546 osd: use __vfs_removexattr")
HPE-bug-id: LUS-12018
Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
Change-Id: I71ed0141ebf5124d4ff0862c24fd719f29d883ae
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55917
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>

lustre/include/lustre_compat.h
@@ -561,9 +561,8 @@ static inline int ll_vfs_getxattr(struct dentry *dentry, struct inode *inode,
 				  const char *name,
 				  void *value, size_t size)
 {
-#if defined(HAVE_MNT_IDMAP_ARG) || defined(HAVE_USER_NAMESPACE_ARG)
-	return vfs_getxattr(&nop_mnt_idmap, dentry, name, value, size);
-#elif defined(HAVE_VFS_SETXATTR)
+#if defined(HAVE_MNT_IDMAP_ARG) || defined(HAVE_USER_NAMESPACE_ARG) || \
+	defined(HAVE_VFS_SETXATTR)
 	return __vfs_getxattr(dentry, inode, name, value, size);
 #else
 	if (unlikely(!inode->i_op->getxattr))

lustre/tests/sanity-sec.sh
@@ -15,10 +15,6 @@ init_test_env $@
 init_logging
 
 ALWAYS_EXCEPT="$SANITY_SEC_EXCEPT "
-if [[ "$MDS1_OS_ID_LIKE" =~ "rhel" ]] &&
-   (( $MDS1_OS_VERSION_CODE >= $(version_code 9.3) )); then
-	always_except LU-16915 51
-fi
 
 [[ "$SLOW" == "no" ]] && EXCEPT_SLOW="26"
 