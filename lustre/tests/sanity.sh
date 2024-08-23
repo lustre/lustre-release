lustre/lustre-release

commit e6b8e3a86b1a31fec97555de7c557470ed26492a

LU-5044 mdd: do not return .lustre from readdir
In mdd_dir_page_build() skip dirents with the FID of .lustre
preventing that directory from being return by readdir on Lustre
clients. Add a sanity tets to this effect.

Signed-off-by: John L. Hammond <john.hammond@intel.com>
Change-Id: Ieecb77a286748c4bf4df4dee68d7f33670a0c286
Reviewed-on: http://review.whamcloud.com/11186
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: wangdi <di.wang@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>

lustre/mdd/mdd_object.c
@@ -1849,6 +1849,7 @@ static int mdd_dir_page_build(const struct lu_env *env, union lu_page *lp,
 	__u64			 hash = 0;
 	struct lu_dirent	*ent;
 	struct lu_dirent	*last = NULL;
+	struct lu_fid		 fid;
 	int			 first = 1;
 
         memset(area, 0, sizeof (*dp));
@@ -1885,6 +1886,12 @@ static int mdd_dir_page_build(const struct lu_env *env, union lu_page *lp,
                         /* osd might not able to pack all attributes,
                          * so recheck rec length */
                         recsize = le16_to_cpu(ent->lde_reclen);
+
+			if (le32_to_cpu(ent->lde_attrs) & LUDA_FID) {
+				fid_le_to_cpu(&fid, &ent->lde_fid);
+				if (fid_is_dot_lustre(&fid))
+					goto next;
+			}
                 } else {
                         result = (last != NULL) ? 0 :-EINVAL;
                         goto out;

lustre/tests/sanity.sh
@@ -9670,6 +9670,17 @@ test_154d() {
 }
 run_test 154d "Verify open file fid"
 
+test_154e()
+{
+	[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.6.50) ]] &&
+		skip "Need MDS version at least 2.6.50" && return
+
+	if ls -a $MOUNT | grep -q '^\.lustre$'; then
+		error ".lustre returned by readdir"
+	fi
+}
+run_test 154e ".lustre is not returned by readdir"
+
 test_155_small_load() {
     local temp=$TMP/$tfile
     local file=$DIR/$tfile

commit 609fc54142281d3ac5e36b257f8890a2fb0c96a0

LU-18138 mdd: handle corner case for .lustre on readdir
If '.lustre' entry is dispaced into last directory block and is alone
in it - the entry to returned by mdd_readdir.

Test to illustrate the issus is added.

Fixes: e6b8e3a86b ("LU-5044 mdd: do not return .lustre from readdir")
Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
Change-Id: Ib4dfe92b66932ede1b1336e55f1af33c1fc6c878
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56021
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/mdd/mdd_object.c
@@ -4092,14 +4092,20 @@ out:
 		last->lde_reclen = 0; /* end mark */
 	}
 out_err:
-	if (result > 0)
+	if (result > 0) {
 		/* end of directory */
 		dp->ldp_hash_end = cpu_to_le64(MDS_DIR_END_OFF);
-	else if (result < 0)
+		if (last == NULL && fid_is_dot_lustre(&fid))
+			/*
+			 * .lustre is last in directory and alone in
+			 * last directory block
+			 */
+			dp->ldp_flags = cpu_to_le32(LDF_EMPTY);
+	} else if (result < 0) {
 		CWARN("%s: build page failed for "DFID": rc = %d\n",
 		      lu_dev_name(obj->do_lu.lo_dev),
 		      PFID(lu_object_fid(&obj->do_lu)), result);
-
+	}
 	return result;
 }
 

lustre/tests/sanity.sh
@@ -18291,6 +18291,40 @@ test_154e()
 }
 run_test 154e ".lustre is not returned by readdir"
 
+test_154ea()
+{
+	stack_trap "rm -rf $MOUNT/[abcdeg]*" EXIT
+
+	# long specially selected names are to displace .lustre out of
+	# first directory block
+	touch $MOUNT/$(printf "a%0100x" 6)
+	for i in 11 15
+	do
+	    touch $MOUNT/$(printf "a%0254x" $i)
+	done
+	touch $MOUNT/$(printf "b%0254x" $i)
+	for i in 16 6 7
+	do
+	    touch $MOUNT/$(printf "c%0254x" $i)
+	done
+	for i in 4 10 8
+	do
+	    touch $MOUNT/$(printf "d%0254x" $i)
+	done
+	for i in 1 14
+	do
+	    touch /mnt/lustre/$(printf "e%0254x" $i)
+	done
+	for i in 13 14
+	do
+	    touch /mnt/lustre/$(printf "g%0254x" $i)
+	done
+	if ls -a $MOUNT | grep -q '^\.lustre$'; then
+	    error ".lustre returned by readdir"
+	fi
+}
+run_test 154ea ".lustre is not returned by readdir (2)"
+
 test_154f() {
 	[ -n "$FILESET" ] && skip "SKIP due to FILESET set"
 