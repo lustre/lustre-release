ccaff8d3c0d7213c59ad7440d06de53de4c0a925
Andreas Dilger <adilger@whamcloud.com>
2024-09-04 20:35:30 -0600

    LU-17567 tests: skip sanity/27T for older OSTs
    Skip sanity test_27T for OSTs < 2.14.57.71 since the test depends on
    changes made to the OST code on the server.
    
    Test-Parameters: trivial testlist=sanity env=ONLY=27T serverversion=2.14
    Fixes: 73d5ee7033 ("LU-15459 llite: clear async errors on write commit sync")
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: I15dad430c52a351e21cf85511e2c55de07740087
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56261
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -3528,6 +3528,8 @@ test_27R() {
 run_test 27R "test max_stripecount limitation when stripe count is set to -1"
 
 test_27T() {
+	(( $OST1_VERSION >= $(version_code 2.14.57.71) )) ||
+		skip "need ost1 >= v2_14_57-71-g73d5ee7033 for ENOSPC fix"
 	[ $(facet_host client) == $(facet_host ost1) ] &&
 		skip "need ost1 and client on different nodes"
 
