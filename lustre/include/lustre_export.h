lustre/lustre-release

commit 14171e787dd00902af7c9edf10a6b54aab45f7b2

LU-10258 lfs: lfs mirror write command
Rename "lfs mirror dump" command to "lfs mirror read".

Add "lfs mirror write" command to write a mirror's content of a
mirrored file.

Usage:

lfs mirror write {--mirror-id|-N <mirror_id>}
	[--inputfile|-i <input_file>] <mirrored_file>

Options:

--mirror-id|-N <mirror_id>
  This  option  indicates  the  content of which mirror specified by
  mirror_id needs to be written. The mirror_id is the numerical unique
  identifier for a mirror.

--inputfile|-i <input_file>
  The path name of the input file, if not specified, the standard
  input stream will be used. The input stream or input_file cannot
  be the same mirrored file as the mirrored_file.

This command will issue a RESYNC lease write lock to notify MDS to
prepare destination mirror for the write (instantiate components of
the mirror), then client copy data from a file or STDIN to the
specified mirror of the mirrored file. After the data copy, a
RESYNC_DONE lease unlock is issued to MDS to update the layout
of the mirrored file.

Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I02022349d4ce871319903a8714ffc4534186c0e4
Reviewed-on: https://review.whamcloud.com/33219
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2101,7 +2101,8 @@ struct mdt_rec_resync {
 	__u32           rs_padding6;	/* rr_flags */
 	__u32           rs_padding7;	/* rr_flags_h */
 	__u32           rs_padding8;	/* rr_umask */
-	__u32           rs_padding9;	/* rr_padding_4 */
+	__u16           rs_mirror_id;
+	__u16           rs_padding9;	/* rr_padding_4 */
 };
 
 /*
@@ -2135,7 +2136,8 @@ struct mdt_rec_reint {
 	__u32           rr_flags;
 	__u32           rr_flags_h;
 	__u32           rr_umask;
-	__u32           rr_padding_4; /* also fix lustre_swab_mdt_rec_reint */
+	__u16		rr_mirror_id;
+	__u16           rr_padding_4; /* also fix lustre_swab_mdt_rec_reint */
 };
 
 /* lmv structures */

lustre/mdc/mdc_reint.c
@@ -462,6 +462,7 @@ int mdc_file_resync(struct obd_export *exp, struct md_op_data *op_data)
 	rec->rs_cap	= op_data->op_cap;
 	rec->rs_fid	= op_data->op_fid1;
 	rec->rs_bias	= op_data->op_bias;
+	rec->rs_mirror_id = op_data->op_mirror_id;
 
 	lock = ldlm_handle2lock(&op_data->op_lease_handle);
 	if (lock != NULL) {

lustre/mdt/mdt_lib.c
@@ -1670,6 +1670,7 @@ static int mdt_resync_unpack(struct mdt_thread_info *info)
 	uc->uc_cap   = rec->rs_cap;
 
 	rr->rr_fid1   = &rec->rs_fid;
+	rr->rr_mirror_id = rec->rs_mirror_id;
 
 	/* cookie doesn't need to be swapped but it has been swapped
 	 * in lustre_swab_mdt_rec_reint() as rr_mtime, so here it needs

lustre/ptlrpc/pack_generic.c
@@ -2068,6 +2068,7 @@ void lustre_swab_mdt_rec_reint (struct mdt_rec_reint *rr)
 	__swab32s(&rr->rr_flags);
 	__swab32s(&rr->rr_flags_h);
 	__swab32s(&rr->rr_umask);
+	__swab16s(&rr->rr_mirror_id);
 
 	CLASSERT(offsetof(typeof(*rr), rr_padding_4) != 0);
 };

lustre/ptlrpc/wiretest.c
@@ -3193,9 +3193,13 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding8));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 132, "found %lld\n",
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding9));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 4, "found %lld\n",
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 2, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9));
 
 	/* Checks for struct mdt_rec_reint */
@@ -3289,9 +3293,13 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_umask));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_umask) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_umask));
-	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 132, "found %lld\n",
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id));
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_padding_4));
-	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 4, "found %lld\n",
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 2, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4));
 
 	/* Checks for struct lmv_desc */

lustre/utils/wirecheck.c
@@ -1351,6 +1351,7 @@ check_mdt_rec_resync(void)
 	CHECK_MEMBER(mdt_rec_resync, rs_padding6);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding7);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding8);
+	CHECK_MEMBER(mdt_rec_resync, rs_mirror_id);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding9);
 }
 
@@ -1381,6 +1382,7 @@ check_mdt_rec_reint(void)
 	CHECK_MEMBER(mdt_rec_reint, rr_flags);
 	CHECK_MEMBER(mdt_rec_reint, rr_flags_h);
 	CHECK_MEMBER(mdt_rec_reint, rr_umask);
+	CHECK_MEMBER(mdt_rec_reint, rr_mirror_id);
 	CHECK_MEMBER(mdt_rec_reint, rr_padding_4);
 }
 

lustre/utils/wiretest.c
@@ -3215,9 +3215,13 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding8));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 132, "found %lld\n",
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding9));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 4, "found %lld\n",
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 2, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9));
 
 	/* Checks for struct mdt_rec_reint */
@@ -3311,9 +3315,13 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_umask));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_umask) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_umask));
-	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 132, "found %lld\n",
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id));
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_padding_4));
-	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 4, "found %lld\n",
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 2, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4));
 
 	/* Checks for struct lmv_desc */

lustre/doc/lfs-mirror-read.1
@@ -1,13 +1,13 @@
-.TH LFS-MIRROR-DUMP 1 2018-08-16 "Lustre" "Lustre Utilities"
+.TH LFS-MIRROR-READ 1 2018-08-16 "Lustre" "Lustre Utilities"
 .SH NAME
-lfs mirror dump \- dump a mirror's content of a mirrored file
+lfs mirror read \- read a mirror's content of a mirrored file
 .SH SYNOPSIS
-.B lfs mirror dump
-<\fB\-\-mirror-id|\-N\fR <\fImirror_id\fR>>
+.B lfs mirror read
+{\fB\-\-mirror-id|\-N\fR <\fImirror_id\fR>}
 [\fB\-\-outfile|\-o\fR <\fIoutput_file\fR>]
 <\fImirrored_file\fR>
 .SH DESCRIPTION
-This command dumps the content of a file's mirror, the file is specified by the
+This command reads the content of a file's mirror, the file is specified by the
 path name \fImirrored_file\fR, the mirror is specified by its mirror ID
 \fImirror_id\fR.
 .br
@@ -17,7 +17,7 @@ otherwise the standard output stream will be used.
 .TP
 .BR \-\-mirror-id|\-N\fR\ <\fImirror_id\fR>
 This option indicates the content of which mirror specified by \fImirror_id\fR
-needs to be dumped. The \fImirror_id\fR is the numerical unique identifier for
+needs to be read. The \fImirror_id\fR is the numerical unique identifier for
 a mirror.
 .TP
 .BR \-\-outfile|\-o\fR\ <\fIoutput_file\fR>
@@ -25,14 +25,14 @@ The path name of the output file, if not specified, the standard output stream
 will be used.
 .SH EXAMPLES
 .TP
-.B lfs mirror dump --mirror-id 3 /mnt/lustre/file1
+.B lfs mirror read --mirror-id 3 /mnt/lustre/file1
 Dump the content of mirror with mirror ID 3 for /mnt/lustre/file1 to STDOUT.
 .TP
-.B lfs mirror dump -N 2 -o /tmp/m2 /mnt/lustre/file1
+.B lfs mirror read -N 2 -o /tmp/m2 /mnt/lustre/file1
 Dump the content of mirror with mirror ID 2 for /mnt/lustre/file1 to file
 /tmp/m2.
 .SH AUTHOR
-The \fBlfs mirror dump\fR command is part of the Lustre filesystem.
+The \fBlfs mirror read\fR command is part of the Lustre filesystem.
 .SH SEE ALSO
 .BR lfs (1),
 .BR lfs-setstripe (1),
@@ -41,3 +41,4 @@ The \fBlfs mirror dump\fR command is part of the Lustre filesystem.
 .BR lfs-mirror-extend (1),
 .BR lfs-mirror-split (1),
 .BR lfs-mirror-verify (1)
+.BR lfs-mirror-write (1)

commit e56829645671de4d7fc628e075cdade840e89f80

LU-18108 idl: deprecate rr_mirror_id usage
The rr_mirror_id_old field used the last reserved field in
struct mdt_rec_reint but is unused for any other reint type.
There are lots of unused fields in this struct that could be
used instead, so rr_umask was chosen for rs_mirror_id_new
since it is unlikely that a file resync will ever need it.
The other rs_padding fields could potentially be needed at
some point (timestamps or size/blocks) so they were not used.

This field is only used by "lfs mirror resync" operations, so
its usage is limited during normal operations, and only in a
few small functions, so the impact of this change is minimal.
It still needs interoperability, but the number of sites using
FLR mirroring is still not very widespread even today, so will
help shorten the time that interop needs to be maintained, but
I didn't add a LUSTRE_VERSION_CODE check for this yet.

After the rr_mirror_id_old field is no longer being used, it
could revert to a single __u32 and be used for something else.

Test-Parameters: testlist=sanity-flr serverversion=2.15
Test-Parameters: testlist=sanity-flr clientversion=2.15
Fixes: 14171e787d ("LU-10258 lfs: lfs mirror write command")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I5b760d3bb359ff28ca5c2b299880f00e953ebbe5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55988
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Feng Lei <flei@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/lustre_export.h
@@ -352,56 +352,58 @@ static inline int exp_max_brw_size(struct obd_export *exp)
 	return ONE_MB_BRW_SIZE;
 }
 
-static inline int exp_connect_multibulk(struct obd_export *exp)
+static inline bool exp_connect_multibulk(struct obd_export *exp)
 {
 	return exp_max_brw_size(exp) > ONE_MB_BRW_SIZE;
 }
 
-static inline int exp_connect_cancelset(struct obd_export *exp)
+static inline bool exp_connect_cancelset(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);
-	return !!(exp_connect_flags(exp) & OBD_CONNECT_CANCELSET);
+	return exp_connect_flags(exp) & OBD_CONNECT_CANCELSET;
 }
 
-static inline int exp_connect_lru_resize(struct obd_export *exp)
+static inline bool exp_connect_lru_resize(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);
-	return !!(exp_connect_flags(exp) & OBD_CONNECT_LRU_RESIZE);
+	return exp_connect_flags(exp) & OBD_CONNECT_LRU_RESIZE;
 }
 
-static inline int exp_connect_vbr(struct obd_export *exp)
+static inline bool exp_connect_vbr(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);
 	LASSERT(exp->exp_connection);
-	return !!(exp_connect_flags(exp) & OBD_CONNECT_VBR);
+	return exp_connect_flags(exp) & OBD_CONNECT_VBR;
 }
 
-static inline int exp_connect_umask(struct obd_export *exp)
+static inline bool exp_connect_umask(struct obd_export *exp)
 {
-	return !!(exp_connect_flags(exp) & OBD_CONNECT_UMASK);
+	return exp_connect_flags(exp) & OBD_CONNECT_UMASK;
 }
 
-static inline int imp_connect_lru_resize(struct obd_import *imp)
+static inline bool imp_connect_lru_resize(struct obd_import *imp)
 {
 	struct obd_connect_data *ocd;
 
 	LASSERT(imp != NULL);
 	ocd = &imp->imp_connect_data;
-	return !!(ocd->ocd_connect_flags & OBD_CONNECT_LRU_RESIZE);
+	return ocd->ocd_connect_flags & OBD_CONNECT_LRU_RESIZE;
 }
 
-static inline int exp_connect_layout(struct obd_export *exp)
+static inline bool exp_connect_layout(struct obd_export *exp)
 {
-	return !!(exp_connect_flags(exp) & OBD_CONNECT_LAYOUTLOCK);
+	return exp_connect_flags(exp) & OBD_CONNECT_LAYOUTLOCK;
 }
 
 static inline bool exp_connect_lvb_type(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);
-	if (exp_connect_flags(exp) & OBD_CONNECT_LVB_TYPE)
-		return true;
-	else
-		return false;
+	return exp_connect_flags(exp) & OBD_CONNECT_LVB_TYPE;
+}
+
+static inline bool exp_connect_mirror_id_fix(struct obd_export *exp)
+{
+	return exp_connect_flags2(exp) & OBD_CONNECT2_MIRROR_ID_FIX;
 }
 
 static inline bool imp_connect_lvb_type(struct obd_import *imp)
@@ -410,10 +412,7 @@ static inline bool imp_connect_lvb_type(struct obd_import *imp)
 
 	LASSERT(imp != NULL);
 	ocd = &imp->imp_connect_data;
-	if (ocd->ocd_connect_flags & OBD_CONNECT_LVB_TYPE)
-		return true;
-	else
-		return false;
+	return ocd->ocd_connect_flags & OBD_CONNECT_LVB_TYPE;
 }
 
 static inline bool imp_connect_disp_stripe(struct obd_import *imp)

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -866,6 +866,7 @@ struct ptlrpc_body_v2 {
  */
 #define OBD_CONNECT2_UNALIGNED_DIO	0x400000000ULL /* unaligned DIO */
 #define OBD_CONNECT2_CONN_POLICY	0x800000000ULL /* server-side connection policy */
+#define OBD_CONNECT2_MIRROR_ID_FIX     0x2000000000ULL /* rr_mirror_id move */
 /* XXX README XXX README XXX README XXX README XXX README XXX README XXX
  * Please DO NOT add OBD_CONNECT flags before first ensuring that this value
  * is not in use by some other branch/patch.  Email adilger@whamcloud.com
@@ -936,7 +937,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_ENCRYPT_FID2PATH | \
 				OBD_CONNECT2_DMV_IMP_INHERIT |\
 				OBD_CONNECT2_UNALIGNED_DIO | \
-				OBD_CONNECT2_PCCRO)
+				OBD_CONNECT2_PCCRO | \
+				OBD_CONNECT2_MIRROR_ID_FIX)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \
@@ -2236,8 +2238,16 @@ struct mdt_rec_resync {
 	__u32           rs_padding5;	/* rr_mode */
 	__u32           rs_padding6;	/* rr_flags */
 	__u32           rs_padding7;	/* rr_flags_h */
-	__u32           rs_padding8;	/* rr_umask */
-	__u16           rs_mirror_id;
+	/* The rr_mirror_id_old field used the last reserved field
+	 * in mdt_rec_reint but is not used for any other reint type.
+	 * There are lots of unused fields in this strict that could be
+	 * used instead, so rr_umask was chosen for rs_mirror_id_new
+	 * since it is unlikely that a file resync operation will ever
+	 * need it.  The other unused fields could potentially be needed
+	 * eventually (timestamps or size/blocks) so they were not used.
+	 */
+	__u32           rs_mirror_id_new; /* rr_umask */
+	__u16           rs_mirror_id_old; /* deprecated 2.16.0 */
 	__u16           rs_padding9;	/* rr_padding_4 */
 };
 
@@ -2272,9 +2282,10 @@ struct mdt_rec_reint {
 	__u32           rr_flags;
 	__u32           rr_flags_h;
 	__u32           rr_umask;
-	__u16		rr_mirror_id;
+	__u16		rr_mirror_id_old; /* deprecated 2.16.0 */
 	__u16           rr_padding_4; /* also fix lustre_swab_mdt_rec_reint */
 };
+#define rr_mirror_id_new rr_umask
 
 #define LMV_DESC_QOS_MAXAGE_DEFAULT 60  /* Seconds */
 

lustre/llite/llite_lib.c
@@ -381,7 +381,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 				   OBD_CONNECT2_BATCH_RPC |
 				   OBD_CONNECT2_DMV_IMP_INHERIT |
 				   OBD_CONNECT2_UNALIGNED_DIO |
-				   OBD_CONNECT2_PCCRO;
+				   OBD_CONNECT2_PCCRO |
+				   OBD_CONNECT2_MIRROR_ID_FIX;
 
 #ifdef HAVE_LRU_RESIZE_SUPPORT
 	if (test_bit(LL_SBI_LRU_RESIZE, sbi->ll_flags))

lustre/mdc/mdc_reint.c
@@ -567,7 +567,10 @@ int mdc_file_resync(struct obd_export *exp, struct md_op_data *op_data)
 	rec->rs_cap	= ll_capability_u32(op_data->op_cap);
 	rec->rs_fid	= op_data->op_fid1;
 	rec->rs_bias	= op_data->op_bias;
-	rec->rs_mirror_id = op_data->op_mirror_id;
+	if (exp_connect_mirror_id_fix(exp))
+		rec->rs_mirror_id_new = op_data->op_mirror_id;
+	else
+		rec->rs_mirror_id_old = op_data->op_mirror_id;
 
 	lock = ldlm_handle2lock(&op_data->op_lease_handle);
 	if (lock != NULL) {

lustre/mdt/mdt_lib.c
@@ -1290,7 +1290,8 @@ static int mdt_close_handle_unpack(struct mdt_thread_info *info)
 	RETURN(0);
 }
 
-static inline int mdt_dlmreq_unpack(struct mdt_thread_info *info) {
+static inline int mdt_dlmreq_unpack(struct mdt_thread_info *info)
+{
 	struct req_capsule *pill = info->mti_pill;
 
 	if (!info->mti_intent_lock &&
@@ -1898,6 +1899,7 @@ static int mdt_setxattr_unpack(struct mdt_thread_info *info)
 
 static int mdt_resync_unpack(struct mdt_thread_info *info)
 {
+	struct obd_export *exp = mdt_info_req(info)->rq_export;
 	struct req_capsule *pill = info->mti_pill;
 	struct mdt_reint_record *rr = &info->mti_rr;
 	struct lu_ucred *uc = mdt_ucred(info);
@@ -1915,8 +1917,11 @@ static int mdt_resync_unpack(struct mdt_thread_info *info)
 	uc->uc_cap = CAP_EMPTY_SET;
 	ll_set_capability_u32(&uc->uc_cap, rec->rs_cap);
 
-	rr->rr_fid1   = &rec->rs_fid;
-	rr->rr_mirror_id = rec->rs_mirror_id;
+	rr->rr_fid1 = &rec->rs_fid;
+	if (exp_connect_mirror_id_fix(exp))
+		rr->rr_mirror_id = (__u16)rec->rs_mirror_id_new;
+	else
+		rr->rr_mirror_id = rec->rs_mirror_id_old;
 
 	/* cookie doesn't need to be swapped but it has been swapped
 	 * in lustre_swab_mdt_rec_reint() as rr_mtime, so here it needs

lustre/obdclass/lprocfs_status.c
@@ -654,6 +654,8 @@ static const char *const obd_connect_names[] = {
 	"compressed_file",		/* 0x200000000 */
 	"unaligned_dio",		/* 0x400000000 */
 	"conn_policy",			/* 0x800000000 */
+	"sparse_read",		       /* 0x1000000000 */
+	"mirror_id_fix",	       /* 0x2000000000 */
 	NULL
 };
 

lustre/ptlrpc/pack_generic.c
@@ -2244,7 +2244,7 @@ void lustre_swab_mdt_rec_reint (struct mdt_rec_reint *rr)
 	__swab32s(&rr->rr_flags);
 	__swab32s(&rr->rr_flags_h);
 	__swab32s(&rr->rr_umask);
-	__swab16s(&rr->rr_mirror_id);
+	__swab16s(&rr->rr_mirror_id_old);
 
 	BUILD_BUG_ON(offsetof(typeof(*rr), rr_padding_4) == 0);
 };

lustre/ptlrpc/wiretest.c
@@ -1467,6 +1467,8 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT2_UNALIGNED_DIO);
 	LASSERTF(OBD_CONNECT2_CONN_POLICY == 0x800000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_CONN_POLICY);
+	LASSERTF(OBD_CONNECT2_MIRROR_ID_FIX == 0x2000000000ULL, "found 0x%.16llxULL\n",
+		 OBD_CONNECT2_MIRROR_ID_FIX);
 
 	LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
 		(unsigned)OBD_CKSUM_CRC32);
@@ -3505,14 +3507,14 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding7));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding7) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding7));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding8) == 128, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding8));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8) == 4, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id) == 132, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id) == 2, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id_new) == 128, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id_new));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_new) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_new));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id_old) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id_old));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_old) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_old));
 	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding9));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 2, "found %lld\n",
@@ -3609,10 +3611,10 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_umask));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_umask) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_umask));
-	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id) == 132, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id));
-	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id) == 2, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id_old) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id_old));
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id_old) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id_old));
 	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_padding_4));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 2, "found %lld\n",

lustre/utils/wirecheck.c
@@ -703,6 +703,7 @@ check_obd_connect_data(void)
 	CHECK_DEFINE_64X(OBD_CONNECT2_COMPRESS);
 	CHECK_DEFINE_64X(OBD_CONNECT2_UNALIGNED_DIO);
 	CHECK_DEFINE_64X(OBD_CONNECT2_CONN_POLICY);
+	CHECK_DEFINE_64X(OBD_CONNECT2_MIRROR_ID_FIX);
 
 	BLANK_LINE();
 	CHECK_VALUE_X(OBD_CKSUM_CRC32);
@@ -1564,8 +1565,8 @@ check_mdt_rec_resync(void)
 	CHECK_MEMBER(mdt_rec_resync, rs_padding5);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding6);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding7);
-	CHECK_MEMBER(mdt_rec_resync, rs_padding8);
-	CHECK_MEMBER(mdt_rec_resync, rs_mirror_id);
+	CHECK_MEMBER(mdt_rec_resync, rs_mirror_id_new);
+	CHECK_MEMBER(mdt_rec_resync, rs_mirror_id_old);
 	CHECK_MEMBER(mdt_rec_resync, rs_padding9);
 }
 
@@ -1596,7 +1597,7 @@ check_mdt_rec_reint(void)
 	CHECK_MEMBER(mdt_rec_reint, rr_flags);
 	CHECK_MEMBER(mdt_rec_reint, rr_flags_h);
 	CHECK_MEMBER(mdt_rec_reint, rr_umask);
-	CHECK_MEMBER(mdt_rec_reint, rr_mirror_id);
+	CHECK_MEMBER(mdt_rec_reint, rr_mirror_id_old);
 	CHECK_MEMBER(mdt_rec_reint, rr_padding_4);
 }
 

lustre/utils/wiretest.c
@@ -1492,6 +1492,8 @@ void lustre_assert_wire_constants(void)
 		 OBD_CONNECT2_UNALIGNED_DIO);
 	LASSERTF(OBD_CONNECT2_CONN_POLICY == 0x800000000ULL, "found 0x%.16llxULL\n",
 		 OBD_CONNECT2_CONN_POLICY);
+	LASSERTF(OBD_CONNECT2_MIRROR_ID_FIX == 0x2000000000ULL, "found 0x%.16llxULL\n",
+		 OBD_CONNECT2_MIRROR_ID_FIX);
 
 	LASSERTF(OBD_CKSUM_CRC32 == 0x00000001UL, "found 0x%.8xUL\n",
 		(unsigned)OBD_CKSUM_CRC32);
@@ -3530,14 +3532,14 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding7));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding7) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding7));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding8) == 128, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding8));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8) == 4, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_padding8));
-	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id) == 132, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id));
-	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id) == 2, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id_new) == 128, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id_new));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_new) == 4, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_new));
+	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_mirror_id_old) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_resync, rs_mirror_id_old));
+	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_old) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_resync *)0)->rs_mirror_id_old));
 	LASSERTF((int)offsetof(struct mdt_rec_resync, rs_padding9) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_resync, rs_padding9));
 	LASSERTF((int)sizeof(((struct mdt_rec_resync *)0)->rs_padding9) == 2, "found %lld\n",
@@ -3634,10 +3636,10 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_umask));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_umask) == 4, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_umask));
-	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id) == 132, "found %lld\n",
-		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id));
-	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id) == 2, "found %lld\n",
-		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id));
+	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_mirror_id_old) == 132, "found %lld\n",
+		 (long long)(int)offsetof(struct mdt_rec_reint, rr_mirror_id_old));
+	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id_old) == 2, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdt_rec_reint *)0)->rr_mirror_id_old));
 	LASSERTF((int)offsetof(struct mdt_rec_reint, rr_padding_4) == 134, "found %lld\n",
 		 (long long)(int)offsetof(struct mdt_rec_reint, rr_padding_4));
 	LASSERTF((int)sizeof(((struct mdt_rec_reint *)0)->rr_padding_4) == 2, "found %lld\n",