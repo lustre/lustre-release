lustre/lustre-release

commit ff018bb77a371415a3973a58a70dfcc431862535

LU-18284 llite: disallow udio exceptions
Require the unaligned_dio feature flag to be present and
disable limited udio support for 2.14 and 2.15 as these
exceptions cause failures with 2.12 servers.

Test-Parameters: trivial
Test-Parameters: testlist=sanity clientarch=x86_64 clientdistro=el9.4  serverversion=2.12.9 serverdistro=el7.9 env=ONLY="119e 119f 119g 119h 119i 119j",ONLY_REPEAT=10
Test-Parameters: testlist=sanity clientarch=x86_64 clientdistro=el9.2  serverversion=2.12.9 serverdistro=el7.9 env=ONLY="119e 119f 119g 119h 119i 119j",ONLY_REPEAT=10
Test-Parameters: testlist=sanity clientarch=x86_64 clientdistro=el8.10 serverversion=2.12.9 serverdistro=el7.9 env=ONLY="119e 119f 119g 119h 119i 119j",ONLY_REPEAT=10
Test-Parameters: testlist=sanity clientarch=x86_64 clientdistro=el8.10 serverversion=2.15.4 serverdistro=el8.9 env=ONLY="119e 119f 119g 119h 119i 119j",ONLY_REPEAT=10
Fixes: 07a1272233 ("LU-17525 llite: unaligned DIO interop page alignment")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: If3d3d41f3f1f4f96279e9c3d7978f78dd7333259
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56571
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw26.c
@@ -541,27 +541,10 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 		RETURN(0);
 
 	/* Unpatched older servers which cannot safely support unaligned DIO
-	 * (osd-zfs) or i/o with page size interop issues should abort here
+	 * should abort here
 	 */
-	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio) {
-		unsigned int md0_offset;
-
-		if (cl_io_top(io)->ci_target_is_zfs)
-			RETURN(-EINVAL);
-
-		/* unpatched ldiskfs is fine, unless MD0 does not align/fit */
-		md0_offset = file_offset & (MD_MAX_INTEROP_PAGE_SIZE - 1);
-		if ((count + md0_offset) >= LNET_MTU) {
-			u64 iomax, iomin;
-
-			iomax = cl_io_nob_aligned(file_offset, count,
-						  MD_MAX_INTEROP_PAGE_SIZE);
-			iomin = cl_io_nob_aligned(file_offset, count,
-						  MD_MIN_INTEROP_PAGE_SIZE);
-			if (iomax != iomin)
-				RETURN(-EINVAL);
-		}
-	}
+	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio)
+		RETURN(-EINVAL);
 
 	/* if one part of an I/O is unaligned, just handle all of it that way -
 	 * otherwise we create significant complexities with managing the iovec

lustre/tests/sanity.sh
@@ -14405,27 +14405,20 @@ unaligned_dio() {
 }
 
 # With unaligned_dio enabled there are no restrictions on dio.
-unaligned_dio_or_ldiskfs_with_same_page_size()
+unaligned_dio_or_skip()
 {
-	if [[ "${ost1_FSTYPE}" == "zfs" ]]; then
-		$LCTL get_param osc.*.import | grep connect_flags: |
-			grep -q "unaligned_dio" ||
-		skip "Need ldiskfs server or 'unaligned_dio' support"
-	fi
-	if [[ $(get_page_size ost1) != $PAGE_SIZE ]]; then
-		$LCTL get_param osc.*.import | grep connect_flags: |
-			grep -q "unaligned_dio" ||
-		skip "Need page interop support"
+	if ! unaligned_dio; then
+		skip "Need 'unaligned_dio' support"
 	fi
 }
 
 dio_readv_writev_support()
 {
 	# Kernels after 3.16 work:
-	(( $(version_code $(uname -r)) >= $(version_code 3.16) ))
+	(( $(version_code $(uname -r)) >= $(version_code 3.16) )) &&
 		return 0
 	# Lustre with LU-17524 works:
-	(( $OST1_VERSION > $(version_code 2.15.61.196) ))
+	(( $OST1_VERSION > $(version_code 2.15.61.196) )) &&
 		return 0
 
 	skip "need readv/writev with O_DIRECT support"
@@ -14474,7 +14467,7 @@ run_test 119c "Testing for direct read hitting hole"
 
 test_119e()
 {
-	unaligned_dio_or_ldiskfs_with_same_page_size
+	unaligned_dio_or_skip
 	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
 
 	local stripe_size=$((1024 * 1024)) #1 MiB
@@ -14550,7 +14543,7 @@ run_test 119e "Basic tests of dio read and write at various sizes"
 
 test_119f()
 {
-	unaligned_dio_or_ldiskfs_with_same_page_size
+	unaligned_dio_or_skip
 	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
 
 	local stripe_size=$((1024 * 1024)) #1 MiB
@@ -14625,7 +14618,7 @@ run_test 119f "dio vs dio race"
 
 test_119g()
 {
-	unaligned_dio_or_ldiskfs_with_same_page_size
+	unaligned_dio_or_skip
 	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
 
 	local stripe_size=$((1024 * 1024)) #1 MiB
@@ -14695,7 +14688,7 @@ run_test 119g "dio vs buffered I/O race"
 
 test_119h()
 {
-	unaligned_dio_or_ldiskfs_with_same_page_size
+	unaligned_dio_or_skip
 	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
 
 	local stripe_size=$((1024 * 1024)) #1 MiB
@@ -14771,7 +14764,7 @@ run_test 119h "basic tests of memory unaligned dio"
 # aiocp with the '-a' option makes testing memory unaligned aio trivial
 test_119i()
 {
-	unaligned_dio_or_ldiskfs_with_same_page_size
+	unaligned_dio_or_skip
 	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
 	which aiocp || skip_env "no aiocp installed"
 	skip "LU-18032: Unaligned AIO is disabled and may not be re-enabled"
@@ -14835,6 +14828,7 @@ run_test 119i "test unaligned aio at varying sizes"
 
 test_119j()
 {
+	unaligned_dio_or_skip
 	(( $LINUX_VERSION_CODE > $(version_code 4.5.0) )) ||
 		skip "needs kernel > 4.5.0 for ki_flags support"
 

commit a5546155a87a245f40062b68d26e53bed7dca44f

LU-17525 llite: soft fail unaligned dio
Skip unaligned DIO and pass io as buffered I/O rather than
hard fail unaligned DIO.

Test-Parameters: testlist=sanity serverversion=2.14 env=SANITY_EXCEPT="65n 211 413"
Fixes: ff018bb77a ("LU-18284 llite: disallow udio exceptions")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I638f62ec96abc3032da5fbcf895cd835022fd759
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56775
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Patrick Farrell <patrick.farrell@oracle.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw26.c
@@ -529,23 +529,6 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	if (rw == READ && file_offset >= i_size_read(inode))
 		RETURN(0);
 
-	/* unaligned DIO support can be turned off, so is it on? */
-	if (unaligned && !ll_sbi_has_unaligned_dio(ll_i2sbi(inode)))
-		RETURN(-EINVAL);
-
-	/* the requirement to not return EIOCBQUEUED for pipes (see bottom of
-	 * this function) plays havoc with the unaligned I/O lifecycle, so
-	 * don't allow unaligned I/O on pipes
-	 */
-	if (unaligned && iov_iter_is_pipe(iter))
-		RETURN(0);
-
-	/* Unpatched older servers which cannot safely support unaligned DIO
-	 * should abort here
-	 */
-	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio)
-		RETURN(-EINVAL);
-
 	/* if one part of an I/O is unaligned, just handle all of it that way -
 	 * otherwise we create significant complexities with managing the iovec
 	 * in different ways, etc, all for very marginal benefits
@@ -559,9 +542,30 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 	LASSERT(ll_dio_aio);
 	LASSERT(ll_dio_aio->cda_iocb == iocb);
 
+	/* unaligned DIO support can be turned off, so is it on? */
+	if (unaligned && !ll_sbi_has_unaligned_dio(ll_i2sbi(inode)))
+		RETURN(-EINVAL);
+
 	/* unaligned AIO is not supported - see LU-18032 */
 	if (unaligned && ll_dio_aio->cda_is_aio)
 		RETURN(-EINVAL);
+
+	/* the requirement to not return EIOCBQUEUED for pipes (see bottom of
+	 * this function) plays havoc with the unaligned I/O lifecycle, so
+	 * don't allow unaligned I/O on pipes
+	 */
+	if (unaligned && iov_iter_is_pipe(iter))
+		RETURN(0);
+
+	/* returning 0 here forces the remaining I/O through buffered I/O
+	 * while returning -EINVAL stops the I/O from continuing
+	 */
+
+	/* Unpatched older servers which cannot safely support unaligned DIO
+	 * should abort here
+	 */
+	if (unaligned && !cl_io_top(io)->ci_allow_unaligned_dio)
+		RETURN(0);
 
 	/* We cannot do parallel submission of sub-I/Os - for AIO or regular
 	 * DIO - unless lockless because it causes us to release the lock