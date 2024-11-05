061f1b576b9d10cc962c5ded2d404e41e0a4d84c
Jian Yu <yujian@whamcloud.com>
2024-11-04 20:35:27 -0800

    LU-17152 tests: unmount NFS clients with zconf_umount_clients
    This patch fixes cleanup_nfs() to unmount NFS clients by running
    zconf_umount_clients(), which can find and kill active processes
    that are accessing the NFS mount point so as to avoid the
    "device is busy" failure.
    
    The patch also adds racer_on_nfs test into always_except list for
    parallel-scale-nfsv4 due to LU-17154.
    
    Lustre-change: https://review.whamcloud.com/52533
    Lustre-commit: 563deecae0ac2690b6d8d5571bf7af09408943cd
    
    Test-Parameters: trivial testlist=parallel-scale-nfsv4
    
    Change-Id: I37a38502362399540c28e78d1343e768b490ce8b
    Signed-off-by: Jian Yu <yujian@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56886
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Alex Deiter <adeiter@ddn.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/parallel-scale-nfsv4.sh
+++ lustre/tests/parallel-scale-nfsv4.sh
@@ -4,4 +4,7 @@ LUSTRE=${LUSTRE:-$(dirname $0)/..}
 . $LUSTRE/tests/test-framework.sh
 init_test_env $@
 
+export ALWAYS_EXCEPT="$PARALLEL_SCALE_NFSV4_EXCEPT "
+always_except LU-17154 racer_on_nfs
+
 sh $LUSTRE/tests/parallel-scale-nfs.sh 4
