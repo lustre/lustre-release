8655a2db930599d631f89d31b9b37eb1bec22aa0
Timothy Day <timday@amazon.com>
2024-12-09 23:39:40 -0500

    LU-6142 obdclass: SPDX for OBD Class
    Convert from verbose license text to SPDX.
    
    Test-Parameters: trivial
    Signed-off-by: Timothy Day <timday@amazon.com>
    Change-Id: Ieaa62c9f4177abdeae18e55e5c8ce6dc638a8918
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57351
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/obdclass/range_lock.c
+++ lustre/obdclass/range_lock.c
@@ -1,25 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
+
 /*
- * GPL HEADER START
+ * This file is part of Lustre, http://www.lustre.org/
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
-/*
  * Range lock is used to allow multiple threads writing a single shared
  * file given each thread is writing to a non-overlapping portion of the
  * file.
@@ -28,11 +11,11 @@
  * Jan Kara <jack@suse.cz>: https://lkml.org/lkml/2013/1/31/480
  *
  * This file could later replaced by the upstream kernel version.
- */
-/*
+ *
  * Author: Prakash Surya <surya1@llnl.gov>
  * Author: Bobi Jam <bobijam.xu@intel.com>
  */
+
 #ifdef HAVE_SCHED_HEADERS
 #include <linux/sched/signal.h>
 #endif
