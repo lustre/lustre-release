lustre/lustre-release

commit 209607fd79571187722116ede0aed3392bc43618

LU-18096 enc: ll_get_symlink overlay function
ll_get_symlink() overlay function is necessary to properly encode for
presentation the symlink target when the encryption key is not
available, in a way that is compatible with the overlay function
ll_setup_filename(), so that further readlink without the encryption
key works properly.

Test-Parameters: testlist=sanity-sec env=ONLY=47,ONLY_REPEAT=10 mdscount=2 mdtcount=4 osscount=1 ostcount=8 clientcount=2
Test-Parameters: testlist=sanity-sec env=ONLY=47,ONLY_REPEAT=10 mdscount=2 mdtcount=4 osscount=1 ostcount=8 clientcount=2 clientdistro=ubuntu2404 serverdistro=el8.8
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I141a89d43f392dd4782e148590bc324ea252c2af
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56568
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/crypto.c
@@ -243,7 +243,7 @@ static int ll_digest_long_name(struct inode *dir, struct llcrypt_name *fname,
 		}
 		digest = (struct ll_digest_filename *)fname->disk_name.name;
 		*fid = digest->ldf_fid;
-		if (!fid_is_sane(fid)) {
+		if (!fid_is_sane(fid) && !fid_is_zero(fid)) {
 			rc = -EINVAL;
 			goto out_free;
 		}
@@ -458,6 +458,56 @@ int ll_setup_filename(struct inode *dir, const struct qstr *iname,
 		return rc;
 
 	return ll_digest_long_name(dir, fname, fid, digested);
+}
+
+/**
+ * ll_get_symlink() - overlay to llcrypt_get_symlink()
+ * @inode: the symlink inode
+ * @caddr: the on-disk contents of the symlink
+ * @max_size: size of @caddr buffer
+ * @done: if successful, will be set up to free the returned target if needed
+ *
+ * This overlay function is necessary to properly encode for presentation the
+ * symlink target when the encryption key is not available, in a way that is
+ * compatible with the overlay function ll_setup_filename(), so that further
+ * readlink without the encryption key works properly.
+ */
+const char *ll_get_symlink(struct inode *inode, const void *caddr,
+			   unsigned int max_size,
+			   struct delayed_call *done)
+{
+	struct llcrypt_str lltr = LLTR_INIT(NULL, 0);
+	struct llcrypt_str de_name;
+	struct lu_fid fid;
+	int rc;
+
+	rc = llcrypt_get_encryption_info(inode);
+	if (rc)
+		return ERR_PTR(rc);
+
+	/* If enc key is available, just call llcrypt function. */
+	if (llcrypt_has_encryption_key(inode))
+		return llcrypt_get_symlink(inode, caddr, max_size, done);
+
+	/* When enc key is not available, we need to build an encoded name to
+	 * userspace that can later be decoded by ll_setup_filename().
+	 */
+	rc = llcrypt_fname_alloc_buffer(inode, NAME_MAX + 1, &lltr);
+	if (rc < 0)
+		return ERR_PTR(rc);
+
+	fid_zero(&fid);
+	de_name.name = (char *)caddr;
+	de_name.len = max_size;
+	rc = ll_fname_disk_to_usr(inode, 0, 0, &de_name, &lltr, &fid);
+	if (rc) {
+		llcrypt_fname_free_buffer(&lltr);
+		return ERR_PTR(rc);
+	}
+	lltr.name[lltr.len] = '\0';
+
+	set_delayed_call(done, kfree_link, lltr.name);
+	return lltr.name;
 }
 
 /**

lustre/llite/llite_internal.h
@@ -2128,6 +2128,13 @@ int ll_prepare_lookup(struct inode *dir, struct dentry *de,
 int ll_setup_filename(struct inode *dir, const struct qstr *iname,
 		      int lookup, struct llcrypt_name *fname,
 		      struct lu_fid *fid);
+#ifdef CONFIG_LL_ENCRYPTION
+const char *ll_get_symlink(struct inode *inode, const void *caddr,
+			   unsigned int max_size,
+			   struct delayed_call *done);
+#else
+#define ll_get_symlink(inode, caddr, max_size, done)   ERR_PTR(-EOPNOTSUPP)
+#endif
 int ll_fname_disk_to_usr(struct inode *inode,
 			 u32 hash, u32 minor_hash,
 			 struct llcrypt_str *iname, struct llcrypt_str *oname,

lustre/llite/symlink.c
@@ -120,8 +120,8 @@ static int ll_readlink_internal(struct inode *inode,
 
 #ifdef HAVE_IOP_GET_LINK
 	if (IS_ENCRYPTED(inode)) {
-		const char *target = llcrypt_get_symlink(inode, *symname,
-							 symlen, done);
+		const char *target = ll_get_symlink(inode, *symname,
+						    symlen, done);
 		if (IS_ERR(target))
 			RETURN(PTR_ERR(target));
 		symlen = strlen(target) + 1;

commit ec4bc9c6007df4207cf3a84128caf3b030fe2f8d

LU-15420 sec: fix native fscrypt builds
The recent changes in libcfs prevents native fscrypt from
building. Replace llcrypt.h with lustre_crypto.h in linux-prim.c
which will build under all conditions. We really only need
llcrypt_[init|exit] for this code. The new symlink fix for
fscrypt also breaks when building native fscrypt. Now 5.9 or
less kernels will build natively and it pass all testing.

Test-Parameters: trivial
Fixes: 209607fd79 ("LU-18096 enc: ll_get_symlink overlay function")
Change-Id: I8230364604992150da5bbfb27a3407e99e212ba6
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57344
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/linux/linux-prim.c
@@ -47,7 +47,6 @@
 #endif
 
 #include <lustre_compat.h>
-#include <libcfs/crypto/llcrypt.h>
 #include <libcfs/linux/linux-time.h>
 #include <libcfs/linux/linux-wait.h>
 #include <libcfs/linux/linux-misc.h>
@@ -55,6 +54,7 @@
 #ifndef HAVE_XARRAY_SUPPORT
 #include <libcfs/linux/xarray.h>
 #endif
+#include <lustre_crypto.h>
 
 #ifndef HAVE_KTIME_GET_TS64
 void ktime_get_ts64(struct timespec64 *ts)

libcfs/libcfs/module.c
@@ -51,7 +51,7 @@
 
 #include <libcfs/libcfs.h>
 #include <lnet/lib-lnet.h>
-#include <lustre_crypto.h>
+#include <uapi/linux/lustre/lustre_ver.h>
 #include "tracefile.h"
 
 int cpu_npartitions;

lustre/llite/crypto.c
@@ -481,7 +481,7 @@ const char *ll_get_symlink(struct inode *inode, const void *caddr,
 	struct lu_fid fid;
 	int rc;
 
-	rc = llcrypt_get_encryption_info(inode);
+	rc = llcrypt_prepare_readdir(inode);
 	if (rc)
 		return ERR_PTR(rc);
 

lustre/llite/llite_internal.h
@@ -2151,7 +2151,7 @@ int ll_prepare_lookup(struct inode *dir, struct dentry *de,
 int ll_setup_filename(struct inode *dir, const struct qstr *iname,
 		      int lookup, struct llcrypt_name *fname,
 		      struct lu_fid *fid);
-#ifdef CONFIG_LL_ENCRYPTION
+#ifdef HAVE_LUSTRE_CRYPTO
 const char *ll_get_symlink(struct inode *inode, const void *caddr,
 			   unsigned int max_size,
 			   struct delayed_call *done);