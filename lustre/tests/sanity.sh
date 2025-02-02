lustre/lustre-release

commit 83f5544d8518ad12ea49e27829fff8f2739b86e2

LU-11303 quota: enforce block quota for chgrp
In patch https://review.whamcloud.com/30146 "LU-5152 quota: enforce
block quota for chgrp", problems were introduced due to synchronous
requests from the MDS to the OSS to change the quota assignment of
files during chgrp operations. However, in some cases, the OSTs are
themselves out of grant and may send a quota request to the MDS,
which may result in a deadlock. Another issue is the slow performance
caused by the synchronous operation between MDT and OSTs.

This patch drops the synchronous RPC requirement of the original
patch #30146 to avoid this problem.

Previously, problems in quota tracking related to chgrp were introduced
due to synchronous RPCs from the MDS to the OSS when changing the group
ownership of objects for quota tracking since
Fixes: 8a71fd5061b ("LU-5152 quota: enforce block quota for chgrp")

Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I40556b9e8a0628eb18aa806d2f6b3dfb9b53e874
Reviewed-on: https://review.whamcloud.com/33996
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Wang Shilong <wangshilong1991@gmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_object.c
@@ -1217,7 +1217,9 @@ int mdd_attr_set(const struct lu_env *env, struct md_object *obj,
 	struct lu_attr *attr = MDD_ENV_VAR(env, cattr);
 	const struct lu_attr *la = &ma->ma_attr;
 	struct lu_ucred  *uc;
+	bool quota_reserved = false;
 	bool chrgrp_by_unprivileged_user = false;
+	__s64 quota_size = 0;
 	int rc;
 	ENTRY;
 
@@ -1250,11 +1252,37 @@ int mdd_attr_set(const struct lu_env *env, struct md_object *obj,
 	uc = lu_ucred_check(env);
 	if (S_ISREG(attr->la_mode) && la->la_valid & LA_GID &&
 	    la->la_gid != attr->la_gid && uc != NULL && uc->uc_fsuid != 0) {
-		/* LU-10048: disable synchronous chgrp operation for it will
-		 * cause deadlock between MDT and OST.
-		la_copy->la_valid |= LA_FLAGS;
-		la_copy->la_flags |= LUSTRE_SET_SYNC_FL;
-		 */
+		CDEBUG(D_QUOTA, "%s: reserve quota for changing group: gid=%u size=%llu\n",
+		       mdd2obd_dev(mdd)->obd_name, la->la_gid, la->la_size);
+
+		if (la->la_valid & LA_BLOCKS)
+			quota_size = la->la_blocks << 9;
+		else if (la->la_valid & LA_SIZE)
+			quota_size = la->la_size;
+		/* use local attr gotten above */
+		else if (attr->la_valid & LA_BLOCKS)
+			quota_size = attr->la_blocks << 9;
+		else if (attr->la_valid & LA_SIZE)
+			quota_size = attr->la_size;
+
+		if (quota_size > 0) {
+			rc = dt_reserve_or_free_quota(env, mdd->mdd_bottom,
+						      GRPQUOTA, attr->la_uid,
+						      la->la_gid, quota_size,
+						      false);
+
+			if (rc) {
+				CDEBUG(D_QUOTA, "%s: failed to reserve quota for gid %d size %llu\n",
+				       mdd2obd_dev(mdd)->obd_name,
+				       la->la_gid, quota_size);
+
+				GOTO(out, rc);
+			}
+
+			quota_reserved = true;
+			la_copy->la_valid |= LA_FLAGS;
+		}
+
 		chrgrp_by_unprivileged_user = true;
 
 		/* Flush the possible existing client setattr requests to OSTs
@@ -1329,6 +1357,21 @@ out:
 	if (rc == 0)
 		rc = mdd_attr_set_changelog(env, obj, handle, &ma->ma_pfid,
 					    la_copy->la_valid);
+
+	if (rc == 0 && quota_reserved) {
+		struct thandle *sub_th;
+
+		sub_th = thandle_get_sub_by_dt(env, handle, mdd->mdd_bottom);
+		if (unlikely(IS_ERR(sub_th))) {
+			dt_reserve_or_free_quota(env, mdd->mdd_bottom, GRPQUOTA,
+						 attr->la_uid, la->la_gid,
+						 -quota_size, false);
+		} else {
+			sub_th->th_reserved_quota.qrr_type = GRPQUOTA;
+			sub_th->th_reserved_quota.qrr_id.qid_gid = la->la_gid;
+			sub_th->th_reserved_quota.qrr_count = quota_size;
+		}
+	}
 
 	if (handle != NULL)
 		rc = mdd_trans_stop(env, mdd, rc, handle);

lustre/include/dt_object.h
@@ -312,8 +312,33 @@ struct dt_device_operations {
 	 * \retval 0		on success
 	 * \retval negative	negated errno on error
 	 */
-         int   (*dt_commit_async)(const struct lu_env *env,
-                                  struct dt_device *dev);
+	int   (*dt_commit_async)(const struct lu_env *env,
+				 struct dt_device *dev);
+
+	/**
+	 * The unit of \a count is byte for block or inodes for metadata.
+	 *
+	 * If \a count > 0, reserve quota in advance of an operation that
+	 * changes the quota assignment, such as chgrp() or rename() into
+	 * a directory with a different group ID.
+	 *
+	 * If \a count < 0, free the reserved quota previously.
+	 *
+	 * \param[in] env       execution environment for this thread
+	 * \param[in] dev       the bottom OSD device to reserve quota
+	 * \param[in] type      quota type (LQUOTA_RES_DT or LQUOTA_RES_MD)
+	 * \param[in] uid       quota uid
+	 * \param[in] gid       quota gid
+	 * \param[in] count     space (bytes or inodes) to reserve or free
+	 * \param[in] md        true for inode, false for block
+	 *
+	 * \retval 0            on success
+	 * \retval negative     negated errno on error
+	 */
+	int   (*dt_reserve_or_free_quota)(const struct lu_env *env,
+					  struct dt_device *dev,
+					  enum quota_type type, __u64 uid,
+					  __u64 gid, __s64 count, bool md);
 };
 
 struct dt_index_features {
@@ -1961,6 +1986,12 @@ static inline struct dt_object *dt_object_child(struct dt_object *o)
 			    struct dt_object, do_lu);
 }
 
+struct dt_quota_reserve_rec {
+	enum quota_type	 qrr_type;
+	union lquota_id	 qrr_id;
+	__u64		 qrr_count;
+};
+
 /**
  * This is the general purpose transaction handle.
  * 1. Transaction Life Cycle
@@ -1986,6 +2017,9 @@ struct thandle {
 	 * top thandle here for now, will fix it when we have better
 	 * callback mechanism */
 	struct thandle	*th_top;
+
+	/* reserved quota for this handle */
+	struct dt_quota_reserve_rec th_reserved_quota;
 
 	/** the last operation result in this transaction.
 	 * this value is used in recovery */
@@ -2900,6 +2934,18 @@ static inline int dt_commit_async(const struct lu_env *env,
         LASSERT(dev->dd_ops);
         LASSERT(dev->dd_ops->dt_commit_async);
         return dev->dd_ops->dt_commit_async(env, dev);
+}
+
+static inline int dt_reserve_or_free_quota(const struct lu_env *env,
+					   struct dt_device *dev,
+					   enum quota_type type, __u64 uid,
+					   __u64 gid, int count, bool is_md)
+{
+	LASSERT(dev);
+	LASSERT(dev->dd_ops);
+	LASSERT(dev->dd_ops->dt_reserve_or_free_quota);
+	return dev->dd_ops->dt_reserve_or_free_quota(env, dev, type, uid, gid,
+						     count, is_md);
 }
 
 static inline int dt_lookup(const struct lu_env *env,

lustre/include/lustre_quota.h
@@ -196,6 +196,9 @@ void qsd_op_end(const struct lu_env *, struct qsd_instance *,
 		struct lquota_trans *);
 void qsd_op_adjust(const struct lu_env *, struct qsd_instance *,
 		   union lquota_id *, int);
+int qsd_reserve_or_free_quota(const struct lu_env *env,
+			      struct qsd_instance *qsd,
+			      struct lquota_id_info *qi);
 
 /*
  * Quota information attached to a transaction
@@ -244,5 +247,29 @@ struct lquota_trans {
  * on slave */
 int lquotactl_slv(const struct lu_env *, struct dt_device *,
 		  struct obd_quotactl *);
+
+static inline int quota_reserve_or_free(const struct lu_env *env,
+					struct qsd_instance *qsd,
+					struct lquota_id_info *qi,
+					enum quota_type type, __u64 uid,
+					__u64 gid, __s64 count, bool is_md)
+{
+	qi->lqi_type = type;
+	if (count > 0)
+		qi->lqi_space = toqb(count);
+	else
+		qi->lqi_space = -toqb(-count);
+
+	if (is_md)
+		qi->lqi_is_blk = false;
+	else
+		qi->lqi_is_blk = true;
+
+	qi->lqi_id.qid_uid = uid;
+	qi->lqi_id.qid_gid = gid;
+
+	return qsd_reserve_or_free_quota(env, qsd, qi);
+}
+
 /** @} quota */
 #endif /* _LUSTRE_QUOTA_H */

lustre/llite/llite_lib.c
@@ -1794,17 +1794,29 @@ void ll_clear_inode(struct inode *inode)
 
 static int ll_md_setattr(struct dentry *dentry, struct md_op_data *op_data)
 {
-        struct lustre_md md;
-        struct inode *inode = dentry->d_inode;
-        struct ll_sb_info *sbi = ll_i2sbi(inode);
-        struct ptlrpc_request *request = NULL;
-        int rc, ia_valid;
-        ENTRY;
+	struct lustre_md md;
+	struct inode *inode = dentry->d_inode;
+	struct ll_sb_info *sbi = ll_i2sbi(inode);
+	struct ptlrpc_request *request = NULL;
+	int rc, ia_valid;
 
-        op_data = ll_prep_md_op_data(op_data, inode, NULL, NULL, 0, 0,
-                                     LUSTRE_OPC_ANY, NULL);
-        if (IS_ERR(op_data))
-                RETURN(PTR_ERR(op_data));
+	ENTRY;
+
+	op_data = ll_prep_md_op_data(op_data, inode, NULL, NULL, 0, 0,
+				     LUSTRE_OPC_ANY, NULL);
+	if (IS_ERR(op_data))
+		RETURN(PTR_ERR(op_data));
+
+	/* If this is a chgrp of a regular file, we want to reserve enough
+	 * quota to cover the entire file size.
+	 */
+	if (S_ISREG(inode->i_mode) && op_data->op_attr.ia_valid & ATTR_GID &&
+	    from_kgid(&init_user_ns, op_data->op_attr.ia_gid) !=
+	    from_kgid(&init_user_ns, inode->i_gid)) {
+		op_data->op_xvalid |= OP_XVALID_BLOCKS;
+		op_data->op_attr_blocks = inode->i_blocks;
+	}
+
 
 	rc = md_setattr(sbi->ll_md_exp, op_data, NULL, 0, &request);
 	if (rc) {

lustre/osd-ldiskfs/osd_handler.c
@@ -2572,18 +2572,42 @@ const int osd_dto_credits_noquota[DTO_NR] = {
 	[DTO_ATTR_SET_CHOWN] = 0
 };
 
+/* reserve or free quota for some operation */
+static int osd_reserve_or_free_quota(const struct lu_env *env,
+				     struct dt_device *dev,
+				     enum quota_type type, __u64 uid,
+				     __u64 gid, __s64 count, bool is_md)
+{
+	int rc;
+	struct osd_device	*osd = osd_dt_dev(dev);
+	struct osd_thread_info	*info = osd_oti_get(env);
+	struct lquota_id_info	*qi = &info->oti_qi;
+	struct qsd_instance	*qsd = NULL;
+
+	ENTRY;
+
+	if (is_md)
+		qsd = osd->od_quota_slave_md;
+	else
+		qsd = osd->od_quota_slave_dt;
+
+	rc = quota_reserve_or_free(env, qsd, qi, type, uid, gid, count, is_md);
+	RETURN(rc);
+}
+
 static const struct dt_device_operations osd_dt_ops = {
-	.dt_root_get       = osd_root_get,
-	.dt_statfs         = osd_statfs,
-	.dt_trans_create   = osd_trans_create,
-	.dt_trans_start    = osd_trans_start,
-	.dt_trans_stop     = osd_trans_stop,
-	.dt_trans_cb_add   = osd_trans_cb_add,
-	.dt_conf_get       = osd_conf_get,
-	.dt_mnt_sb_get	   = osd_mnt_sb_get,
-	.dt_sync           = osd_sync,
-	.dt_ro             = osd_ro,
-	.dt_commit_async   = osd_commit_async,
+	.dt_root_get		  = osd_root_get,
+	.dt_statfs		  = osd_statfs,
+	.dt_trans_create	  = osd_trans_create,
+	.dt_trans_start		  = osd_trans_start,
+	.dt_trans_stop		  = osd_trans_stop,
+	.dt_trans_cb_add	  = osd_trans_cb_add,
+	.dt_conf_get		  = osd_conf_get,
+	.dt_mnt_sb_get		  = osd_mnt_sb_get,
+	.dt_sync		  = osd_sync,
+	.dt_ro			  = osd_ro,
+	.dt_commit_async	  = osd_commit_async,
+	.dt_reserve_or_free_quota = osd_reserve_or_free_quota,
 };
 
 static void osd_read_lock(const struct lu_env *env, struct dt_object *dt,
@@ -2781,7 +2805,7 @@ static int osd_declare_attr_qid(const struct lu_env *env,
 				struct osd_object *obj,
 				struct osd_thandle *oh, long long bspace,
 				qid_t old_id, qid_t new_id, bool enforce,
-				unsigned int type, bool ignore_edquot)
+				unsigned int type)
 {
 	int rc;
 	struct osd_thread_info *info = osd_oti_get(env);
@@ -2796,7 +2820,7 @@ static int osd_declare_attr_qid(const struct lu_env *env,
 	qi->lqi_space      = 1;
 	/* Reserve credits for the new id */
 	rc = osd_declare_qid(env, oh, qi, NULL, enforce, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 	if (rc)
 		RETURN(rc);
@@ -2805,7 +2829,7 @@ static int osd_declare_attr_qid(const struct lu_env *env,
 	qi->lqi_id.qid_uid = old_id;
 	qi->lqi_space = -1;
 	rc = osd_declare_qid(env, oh, qi, obj, enforce, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 	if (rc)
 		RETURN(rc);
@@ -2821,7 +2845,7 @@ static int osd_declare_attr_qid(const struct lu_env *env,
 	 * to save credit reservation.
 	 */
 	rc = osd_declare_qid(env, oh, qi, obj, enforce, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 	if (rc)
 		RETURN(rc);
@@ -2830,7 +2854,7 @@ static int osd_declare_attr_qid(const struct lu_env *env,
 	qi->lqi_id.qid_uid = old_id;
 	qi->lqi_space      = -bspace;
 	rc = osd_declare_qid(env, oh, qi, obj, enforce, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 
 	RETURN(rc);
@@ -2881,20 +2905,11 @@ static int osd_declare_attr_set(const struct lu_env *env,
 	 * space adjustment once the operation is completed.
 	 */
 	if (attr->la_valid & LA_UID || attr->la_valid & LA_GID) {
-		bool ignore_edquot = !(attr->la_flags & LUSTRE_SET_SYNC_FL);
-
-		if (!ignore_edquot)
-			CDEBUG(D_QUOTA,
-			       "%s: enforce quota on UID %u, GID %u (quota space is %lld)\n",
-			       osd_ino2name(obj->oo_inode), attr->la_uid,
-			       attr->la_gid, bspace);
-
 		/* USERQUOTA */
 		uid = i_uid_read(obj->oo_inode);
 		enforce = (attr->la_valid & LA_UID) && (attr->la_uid != uid);
 		rc = osd_declare_attr_qid(env, obj, oh, bspace, uid,
-					  attr->la_uid, enforce, USRQUOTA,
-					  true);
+					  attr->la_uid, enforce, USRQUOTA);
 		if (rc)
 			RETURN(rc);
 
@@ -2903,8 +2918,7 @@ static int osd_declare_attr_set(const struct lu_env *env,
 		       attr->la_uid, gid, attr->la_gid);
 		enforce = (attr->la_valid & LA_GID) && (attr->la_gid != gid);
 		rc = osd_declare_attr_qid(env, obj, oh, bspace, gid,
-					  attr->la_gid, enforce, GRPQUOTA,
-					  ignore_edquot);
+					  attr->la_gid, enforce, GRPQUOTA);
 		if (rc)
 			RETURN(rc);
 
@@ -2917,7 +2931,7 @@ static int osd_declare_attr_set(const struct lu_env *env,
 					(attr->la_projid != projid);
 		rc = osd_declare_attr_qid(env, obj, oh, bspace,
 					  (qid_t)projid, (qid_t)attr->la_projid,
-					  enforce, PRJQUOTA, true);
+					  enforce, PRJQUOTA);
 		if (rc)
 			RETURN(rc);
 	}
@@ -6213,7 +6227,7 @@ static int osd_index_declare_ea_insert(const struct lu_env *env,
 		    i_projid_read(inode) != 0)
 			rc = osd_declare_attr_qid(env, osd_dt_obj(dt), oh,
 						  0, i_projid_read(inode),
-						  0, false, PRJQUOTA, true);
+						  0, false, PRJQUOTA);
 #endif
 	}
 

lustre/osd-ldiskfs/osd_quota.c
@@ -28,6 +28,7 @@
  * Author: Niu    Yawei    <niu@whamcloud.com>
  */
 
+#include <dt_object.h>
 #include <lustre_quota.h>
 #include "osd_internal.h"
 

lustre/osd-zfs/osd_handler.c
@@ -698,17 +698,39 @@ static int osd_ro(const struct lu_env *env, struct dt_device *d)
 	RETURN(0);
 }
 
+/* reserve or free quota for some operation */
+static int osd_reserve_or_free_quota(const struct lu_env *env,
+				     struct dt_device *dev,
+				     enum quota_type type, __u64 uid,
+				     __u64 gid, __s64 count, bool is_md)
+{
+	int rc;
+	struct osd_device       *osd = osd_dt_dev(dev);
+	struct osd_thread_info  *info = osd_oti_get(env);
+	struct lquota_id_info   *qi = &info->oti_qi;
+	struct qsd_instance     *qsd = NULL;
+
+	if (is_md)
+		qsd = osd->od_quota_slave_md;
+	else
+		qsd = osd->od_quota_slave_dt;
+
+	rc = quota_reserve_or_free(env, qsd, qi, type, uid, gid, count, is_md);
+	RETURN(rc);
+}
+
 static const struct dt_device_operations osd_dt_ops = {
-	.dt_root_get		= osd_root_get,
-	.dt_statfs		= osd_statfs,
-	.dt_trans_create	= osd_trans_create,
-	.dt_trans_start		= osd_trans_start,
-	.dt_trans_stop		= osd_trans_stop,
-	.dt_trans_cb_add	= osd_trans_cb_add,
-	.dt_conf_get		= osd_conf_get,
-	.dt_sync		= osd_sync,
-	.dt_commit_async	= osd_commit_async,
-	.dt_ro			= osd_ro,
+	.dt_root_get		  = osd_root_get,
+	.dt_statfs		  = osd_statfs,
+	.dt_trans_create	  = osd_trans_create,
+	.dt_trans_start		  = osd_trans_start,
+	.dt_trans_stop		  = osd_trans_stop,
+	.dt_trans_cb_add	  = osd_trans_cb_add,
+	.dt_conf_get		  = osd_conf_get,
+	.dt_sync		  = osd_sync,
+	.dt_commit_async	  = osd_commit_async,
+	.dt_ro			  = osd_ro,
+	.dt_reserve_or_free_quota = osd_reserve_or_free_quota,
 };
 
 /*

lustre/osd-zfs/osd_object.c
@@ -1055,7 +1055,7 @@ static inline int qsd_transfer(const struct lu_env *env,
 			       struct qsd_instance *qsd,
 			       struct lquota_trans *trans, int qtype,
 			       __u64 orig_id, __u64 new_id, __u64 bspace,
-			       struct lquota_id_info *qi, bool ignore_edquot)
+			       struct lquota_id_info *qi)
 {
 	int	rc;
 
@@ -1072,7 +1072,7 @@ static inline int qsd_transfer(const struct lu_env *env,
 	qi->lqi_id.qid_uid = new_id;
 	qi->lqi_space      = 1;
 	rc = qsd_op_begin(env, qsd, trans, qi, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 	if (rc)
 		return rc;
@@ -1094,7 +1094,7 @@ static inline int qsd_transfer(const struct lu_env *env,
 	qi->lqi_id.qid_uid = new_id;
 	qi->lqi_space      = bspace;
 	rc = qsd_op_begin(env, qsd, trans, qi, NULL);
-	if (ignore_edquot && (rc == -EDQUOT || rc == -EINPROGRESS))
+	if (rc == -EDQUOT || rc == -EINPROGRESS)
 		rc = 0;
 	if (rc)
 		return rc;
@@ -1181,7 +1181,7 @@ static int osd_declare_attr_set(const struct lu_env *env,
 			rc = qsd_transfer(env, osd_def_qsd(osd),
 					  &oh->ot_quota_trans, USRQUOTA,
 					  obj->oo_attr.la_uid, attr->la_uid,
-					  bspace, &info->oti_qi, true);
+					  bspace, &info->oti_qi);
 			if (rc)
 				GOTO(out, rc);
 		}
@@ -1192,9 +1192,7 @@ static int osd_declare_attr_set(const struct lu_env *env,
 			rc = qsd_transfer(env, osd_def_qsd(osd),
 					  &oh->ot_quota_trans, GRPQUOTA,
 					  obj->oo_attr.la_gid, attr->la_gid,
-					  bspace, &info->oti_qi,
-					  !(attr->la_flags &
-							LUSTRE_SET_SYNC_FL));
+					  bspace, &info->oti_qi);
 			if (rc)
 				GOTO(out, rc);
 		}
@@ -1230,7 +1228,7 @@ static int osd_declare_attr_set(const struct lu_env *env,
 					  &oh->ot_quota_trans, PRJQUOTA,
 					  obj->oo_attr.la_projid,
 					  attr->la_projid, bspace,
-					  &info->oti_qi, true);
+					  &info->oti_qi);
 			if (rc)
 				GOTO(out, rc);
 		}

commit ee47ed2491e08ab293dfa2675d7d015bdb9b1031

LU-18626 mdd: allow chgrp with read-only permissions
In patch LU-11303, chgrp was modified to be asynchronous
and updates the quotas. However, this resulted in chgrp
calls failing for file owners with read-only permissions.

This commit relaxes the permission requirements for quota updates
for chgrp calls to allow read-only permissions.

Fixes: 83f5544d8518 ("LU-11303 quota: enforce block quota for chgrp")
Test-Parameters: testlist=pjdfstest
Signed-off-by: Max Wang <wamax@amazon.com>
Change-Id: I6c4b73d1d48cb2ffc4866ab2e03a86a6c723e960
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57717
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_object.c
@@ -889,8 +889,16 @@ static int mdd_fix_attr(const struct lu_env *env, struct mdd_object *obj,
 		if (!((flags & MDS_OWNEROVERRIDE) &&
 		      (uc->uc_fsuid == oattr->la_uid)) &&
 		    !(flags & MDS_PERM_BYPASS)) {
+			int mask = MAY_WRITE;
+
+			/* for chgrp, allow the update with
+			 * read-only permissions
+			 */
+			if (S_ISREG(oattr->la_mode) && la->la_valid & LA_GID &&
+			    la->la_gid != oattr->la_gid)
+				mask = MAY_READ;
 			rc = mdd_permission_internal(env, obj, oattr,
-						     MAY_WRITE);
+						     mask);
 			if (rc != 0)
 				RETURN(rc);
 		}

lustre/tests/sanity.sh
@@ -376,6 +376,21 @@ test_6h() { # bug 7331
 }
 run_test 6h "$RUNAS chown RUNAS_ID.0 .../$tfile (should return error)"
 
+test_6i() {
+        (( MDS1_VERSION >= $(version_code 2.16.51) )) ||
+		skip "Need MDS version at least 2.16.51"
+	(( $RUNAS_ID != $UID )) || skip_env "RUNAS_ID = UID = $UID"
+
+        touch $DIR/$tfile
+        chmod 444 $DIR/$tfile
+        chown $RUNAS_ID $DIR/$tfile || error "initial chown failed"
+        $RUNAS -u $RUNAS_ID -g $RUNAS_ID chgrp $RUNAS_ID $DIR/$tfile ||
+		error "chgrp $RUNAS_ID $file failed"
+        $CHECKSTAT -t file -u \#$RUNAS_ID -g \#$RUNAS_ID $DIR/$tfile ||
+                error "$tfile should be owned by GID $RUNAS_ID"
+}
+run_test 6i "touch+chmod+chgrp $tfile; chgrp read-only file should succeed"
+
 test_7a() {
 	test_mkdir $DIR/$tdir
 	$MCREATE $DIR/$tdir/$tfile