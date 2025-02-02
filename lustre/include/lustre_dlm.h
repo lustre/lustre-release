lustre/lustre-release

commit 5517eab06eb99e4ecb66be251a10e70c37547610

LU-1876 hsm: layout lock implementation on server side
* enabled OBD_CONNECT_LAYOUTLOCK both on client and mdt
* mdt_object_lock_try() is implemented to try layout lock
* dlm flag LDLM_FL_BLOCK_NOWAIT is added to implement nonblock mode
  dlm lock enqueue
* try layout lock for IT_GETATTR and IT_OPEN
* make layout lock work
* add layout intent support.

Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
Change-Id: I84a6dfca83ded2344240fa5430c9922b7cfc7bcf
Reviewed-on: http://review.whamcloud.com/4417
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: wangdi <di.wang@intel.com>
Reviewed-by: jacques-Charles Lafoucriere <jacques-charles.lafoucriere@cea.fr>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_dlm.h
@@ -90,6 +90,7 @@ typedef enum {
         ELDLM_LOCK_ABORTED = 301,
         ELDLM_LOCK_REPLACED = 302,
         ELDLM_NO_LOCK_DATA = 303,
+        ELDLM_LOCK_WOULDBLOCK = 304,
 
         ELDLM_NAMESPACE_EXISTS = 400,
         ELDLM_BAD_NAMESPACE    = 401
@@ -1152,6 +1153,12 @@ struct ldlm_resource {
 
 	struct inode		*lr_lvb_inode;
 };
+
+static inline bool ldlm_has_layout(struct ldlm_lock *lock)
+{
+	return lock->l_resource->lr_type == LDLM_IBITS &&
+		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_LAYOUT;
+}
 
 static inline char *
 ldlm_ns_name(struct ldlm_namespace *ns)

lustre/include/cl_object.h
@@ -277,10 +277,25 @@ struct cl_object_conf {
          */
         struct inode             *coc_inode;
 	/**
-	 * Invalidate the current stripe configuration due to losing
-	 * layout lock.
+	 * Layout lock handle.
 	 */
-	bool			  coc_invalidate;
+	struct ldlm_lock	 *coc_lock;
+	/**
+	 * Operation to handle layout, OBJECT_CONF_XYZ.
+	 */
+	int			  coc_opc;
+};
+
+enum {
+	/** configure layout, set up a new stripe, must be called while
+	 * holding layout lock. */
+	OBJECT_CONF_SET = 0,
+	/** invalidate the current stripe configuration due to losing
+	 * layout lock. */
+	OBJECT_CONF_INVALIDATE = 1,
+	/** wait for old layout to go away so that new layout can be
+	 * set up. */
+	OBJECT_CONF_WAIT = 2
 };
 
 /**

lustre/include/lustre/lustre_idl.h
@@ -1236,7 +1236,7 @@ extern void lustre_swab_ptlrpc_body(struct ptlrpc_body *pb);
 				OBD_CONNECT_64BITHASH | OBD_CONNECT_JOBSTATS | \
 				OBD_CONNECT_EINPROGRESS | \
 				OBD_CONNECT_LIGHTWEIGHT | OBD_CONNECT_UMASK | \
-				OBD_CONNECT_LVB_TYPE)
+				OBD_CONNECT_LVB_TYPE | OBD_CONNECT_LAYOUTLOCK)
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
                                 OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \
                                 OBD_CONNECT_TRUNCLOCK | OBD_CONNECT_INDEX | \
@@ -1251,7 +1251,8 @@ extern void lustre_swab_ptlrpc_body(struct ptlrpc_body *pb);
                                 OBD_CONNECT_MAX_EASIZE | \
 				OBD_CONNECT_EINPROGRESS | \
 				OBD_CONNECT_JOBSTATS | \
-				OBD_CONNECT_LIGHTWEIGHT | OBD_CONNECT_LVB_TYPE)
+				OBD_CONNECT_LIGHTWEIGHT | OBD_CONNECT_LVB_TYPE|\
+				OBD_CONNECT_LAYOUTLOCK)
 #define ECHO_CONNECT_SUPPORTED (0)
 #define MGS_CONNECT_SUPPORTED  (OBD_CONNECT_VERSION | OBD_CONNECT_AT | \
 				OBD_CONNECT_FULL20 | OBD_CONNECT_IMP_RECOV | \
@@ -1894,10 +1895,7 @@ extern void lustre_swab_generic_32s (__u32 *val);
 #define MDS_INODELOCK_OPEN   0x000004       /* For opened files */
 #define MDS_INODELOCK_LAYOUT 0x000008       /* for layout */
 
-/* Do not forget to increase MDS_INODELOCK_MAXSHIFT when adding new bits
- * XXX: MDS_INODELOCK_MAXSHIFT should be increased to 3 once the layout lock is
- * supported */
-#define MDS_INODELOCK_MAXSHIFT 2
+#define MDS_INODELOCK_MAXSHIFT 3
 /* This FULL lock is useful to take on unlink sort of operations */
 #define MDS_INODELOCK_FULL ((1<<(MDS_INODELOCK_MAXSHIFT+1))-1)
 
@@ -3275,6 +3273,25 @@ struct getinfo_fid2path {
 
 void lustre_swab_fid2path (struct getinfo_fid2path *gf);
 
+enum {
+        LAYOUT_INTENT_ACCESS    = 0,
+        LAYOUT_INTENT_READ      = 1,
+        LAYOUT_INTENT_WRITE     = 2,
+        LAYOUT_INTENT_GLIMPSE   = 3,
+        LAYOUT_INTENT_TRUNC     = 4,
+        LAYOUT_INTENT_RELEASE   = 5,
+        LAYOUT_INTENT_RESTORE   = 6
+};
+
+/* enqueue layout lock with intent */
+struct layout_intent {
+	__u32 li_opc; /* intent operation for enqueue, read, write etc */
+	__u32 li_flags;
+	__u64 li_start;
+	__u64 li_end;
+};
+
+void lustre_swab_layout_intent(struct layout_intent *li);
 
 #endif
 /** @} lustreidl */

lustre/include/lustre_export.h
@@ -330,6 +330,11 @@ static inline int imp_connect_lru_resize(struct obd_import *imp)
         return !!(ocd->ocd_connect_flags & OBD_CONNECT_LRU_RESIZE);
 }
 
+static inline int exp_connect_layout(struct obd_export *exp)
+{
+	return !!(exp->exp_connect_flags & OBD_CONNECT_LAYOUTLOCK);
+}
+
 static inline bool exp_connect_lvb_type(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);

lustre/include/lustre_fid.h
@@ -498,6 +498,16 @@ static inline int fid_res_name_eq(const struct lu_fid *f,
                name->name[LUSTRE_RES_ID_VER_OID_OFF] == fid_ver_oid(f);
 }
 
+/* reverse function of fid_build_reg_res_name() */
+static inline void fid_build_from_res_name(struct lu_fid *f,
+					   const struct ldlm_res_id *name)
+{
+	fid_zero(f);
+	f->f_seq = name->name[LUSTRE_RES_ID_SEQ_OFF];
+	f->f_oid = name->name[LUSTRE_RES_ID_VER_OID_OFF] & 0xffffffff;
+	f->f_ver = name->name[LUSTRE_RES_ID_VER_OID_OFF] >> 32;
+	LASSERT(fid_res_name_eq(f, name));
+}
 
 static inline struct ldlm_res_id *
 fid_build_pdo_res_name(const struct lu_fid *f,

lustre/include/lustre_req_layout.h
@@ -211,6 +211,7 @@ extern struct req_format RQF_LDLM_ENQUEUE_LVB;
 extern struct req_format RQF_LDLM_CONVERT;
 extern struct req_format RQF_LDLM_INTENT;
 extern struct req_format RQF_LDLM_INTENT_BASIC;
+extern struct req_format RQF_LDLM_INTENT_LAYOUT;
 extern struct req_format RQF_LDLM_INTENT_GETATTR;
 extern struct req_format RQF_LDLM_INTENT_OPEN;
 extern struct req_format RQF_LDLM_INTENT_CREATE;
@@ -257,6 +258,7 @@ extern struct req_msg_field RMF_DLM_REP;
 extern struct req_msg_field RMF_DLM_LVB;
 extern struct req_msg_field RMF_DLM_GL_DESC;
 extern struct req_msg_field RMF_LDLM_INTENT;
+extern struct req_msg_field RMF_LAYOUT_INTENT;
 extern struct req_msg_field RMF_MDT_MD;
 extern struct req_msg_field RMF_REC_REINT;
 extern struct req_msg_field RMF_EADATA;

lustre/include/obd_support.h
@@ -234,6 +234,11 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MDS_OSC_CREATE_FAIL     0x147
 #define OBD_FAIL_MDS_NEGATIVE_POSITIVE	 0x148
 
+/* layout lock */
+#define OBD_FAIL_MDS_NO_LL_GETATTR	 0x170
+#define OBD_FAIL_MDS_NO_LL_OPEN		 0x171
+#define OBD_FAIL_MDS_LL_BLOCK		 0x172
+
 /* CMD */
 #define OBD_FAIL_MDS_IS_SUBDIR_NET       0x180
 #define OBD_FAIL_MDS_IS_SUBDIR_PACK      0x181
@@ -446,6 +451,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_LLITE_FAULT_TRUNC_RACE             0x1401
 #define OBD_FAIL_LOCK_STATE_WAIT_INTR               0x1402
 #define OBD_FAIL_LOV_INIT			    0x1403
+#define OBD_FAIL_GLIMPSE_DELAY			    0x1404
 
 /* Assign references to moved code to reduce code changes */
 #define OBD_FAIL_PRECHECK(id)                   CFS_FAIL_PRECHECK(id)

lustre/lclient/glimpse.c
@@ -237,6 +237,8 @@ int cl_glimpse_size0(struct inode *inode, int agl)
                 else if (result == 0)
                         result = cl_glimpse_lock(env, io, inode, io->ci_obj,
                                                  agl);
+
+		OBD_FAIL_TIMEOUT(OBD_FAIL_GLIMPSE_DELAY, 2);
                 cl_io_fini(env, io);
 		if (unlikely(io->ci_need_restart))
 			goto again;

commit 3cf163f78cf8ef3b1a6fb9c06fb75a48fbabb2e3

LU-18609 ldlm: use of logical '&&' with constant operand
CoverityID: 454269 (CONSTANT_EXPRESSION_RESULT)
Transform bitwise operator result into a boolean value for boolean
operation.

Fixes: 5517eab06e ("LU-1876 hsm: layout lock implementation on server side")
Fixes: 70b9dc57a9 ("LU-17812 ldlm: stack trace log for LDLM error")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I6a6c22a0d1178da99a5253cb03020b5cc2372a31
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57613
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

lustre/include/lustre_dlm.h
@@ -1176,13 +1176,13 @@ static inline int ldlm_is_granted(struct ldlm_lock *lock)
 static inline bool ldlm_has_layout(struct ldlm_lock *lock)
 {
 	return lock->l_resource->lr_type == LDLM_IBITS &&
-		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_LAYOUT;
+		!!(lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_LAYOUT);
 }
 
 static inline bool ldlm_has_dom(struct ldlm_lock *lock)
 {
 	return lock->l_resource->lr_type == LDLM_IBITS &&
-		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_DOM;
+		!!(lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_DOM);
 }
 
 static inline char *
@@ -1351,7 +1351,7 @@ extern const char *ldlm_it2str(enum ldlm_intent_flags it);
 		_ldlm_lock_debug(lock, msgdata, fmt, ##a);              \
 		if (unlikely(ldlm_lock_to_ns(lock)->                    \
 				ns_dump_stack_on_error) &&              \
-						(mask) & D_ERROR)       \
+						!!((mask) & D_ERROR))   \
 			dump_stack();				        \
 	}								\
 } while (0)