lustre/lustre-release

commit 6791fbc530ad64d5764882f4c866ee2a42a3f1fc

LU-18497 gss: carry out creds prepare as user
Instead of forking a child process that switches to user identity to
get the credentials cache name, just carry out the whole credentials
preparation as the user, and switch back to original uid/gid in order
to proceed to ioctls, eliminating the need to map memory between
processes.

Test-Parameters: trivial
Test-Parameters: testgroup=review-dne-selinux-ssk-part-1
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Test-Parameters: kerberos=true testlist=sanity-krb5
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7807263d71cd3fc8a9934cc4c4da8b497b845e6f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57192
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/gss/lgss_keyring.c
@@ -993,8 +993,7 @@ static int prepare_and_instantiate(struct lgss_cred *cred, key_serial_t keyid,
 {
 	key_serial_t inst_keyring;
 	bool prepared = true;
-	pid_t child = 1;
-	int rc = 0;
+	int rc;
 
 	if (lgss_prepare_cred(cred)) {
 		logmsg(LL_ERR, "key %08x: failed to prepare credentials "
@@ -1018,19 +1017,13 @@ static int prepare_and_instantiate(struct lgss_cred *cred, key_serial_t keyid,
 	if (cred->lc_root_flags) {
 		inst_keyring = 0;
 	} else {
-		inst_keyring = KEY_SPEC_USER_KEYRING;
-		/* fork to not change identity in main process */
-		rc = fork_and_switch_id(uid, &child);
-		if (rc || child)
-			goto out;
-	}
-
-	/* if dealing with a user key, grant user write permission,
-	 * it will be required for key update
-	 */
-	if (child == 0) {
 		key_perm_t perm;
 
+		inst_keyring = KEY_SPEC_USER_KEYRING;
+
+		/* when dealing with a user key, grant user write permission,
+		 * it will be required for key update
+		 */
 		perm = KEY_POS_VIEW | KEY_POS_WRITE | KEY_POS_SEARCH |
 			KEY_POS_LINK | KEY_POS_SETATTR |
 			KEY_USR_VIEW | KEY_USR_WRITE;
@@ -1050,10 +1043,6 @@ static int prepare_and_instantiate(struct lgss_cred *cred, key_serial_t keyid,
 		       keyid, inst_keyring);
 	}
 
-out:
-	if (child == 0)
-		/* job done for child */
-		exit(rc);
 	return prepared ? rc : 1;
 }
 
@@ -1376,11 +1365,29 @@ out_pipe:
 		else
 			logmsg(LL_TRACE, "stick with current namespace\n");
 
+		if (!cred->lc_root_flags) {
+			/* switch to user id for creds prepare */
+			rc = switch_identity(uparam.kup_uid);
+			if (rc)
+				goto out_reg;
+		}
+
 		/* In case of prepare error, a key will be instantiated
 		 * all the same. But then we will have to error this key
 		 * instead of doing normal gss negotiation.
 		 */
 		rc = prepare_and_instantiate(cred, keyid, uparam.kup_uid);
+
+		if (!cred->lc_root_flags) {
+			int rc2;
+
+			/* switch back to root in order to proceed to ioctls */
+			rc2 = switch_identity(0);
+			if (rc2) {
+				rc = rc2;
+				goto out_reg;
+			}
+		}
 
 		/*
 		 * fork a child to do the real gss negotiation

lustre/utils/gss/lgss_krb5_utils.c
@@ -487,83 +487,6 @@ end_find:
 }
 
 /**
- * Compose the TGT cc name for user, needs to fork process and switch identity.
- * For that reason, ccname buffer passed in must be mmapped with MAP_SHARED.
- */
-static int get_user_tgt_ccname(struct lgss_cred *cred, krb5_context ctx,
-			       char *ccname, int size)
-{
-	pid_t child;
-	int status, rc = 0;
-
-	/* fork to not change identity in main process, it needs to stay root
-	 * in order to proceed to ioctls
-	 */
-	child = fork();
-	if (child == -1) {
-		logmsg(LL_ERR, "cannot fork child for user %u: %s\n",
-		       cred->lc_uid, strerror(errno));
-		rc = -errno;
-	} else if (child == 0) {
-		/* switch identity */
-		rc = switch_identity(cred->lc_uid);
-		if (rc)
-			exit(1);
-
-		/* getting default ccname requires impersonating user */
-		rc = lgss_krb5_get_default_ccache_name(ctx, ccname, size);
-		if (rc) {
-			logmsg(LL_ERR,
-			       "cannot get default ccname for user %u\n",
-			       cred->lc_uid);
-			exit(1);
-		}
-
-		/* job done for child */
-		exit(0);
-	} else {
-		logmsg(LL_TRACE, "forked child %d\n", child);
-		if (wait(&status) < 0) {
-			logmsg(LL_ERR, "wait child %d failed: %s\n",
-			       child, strerror(errno));
-			return -errno;
-		}
-		if (!WIFEXITED(status)) {
-			logmsg(LL_ERR, "child %d terminated with %d\n",
-			       child, status);
-			return status;
-		}
-	}
-
-	/* try ccname as fetched by child */
-	rc = acquire_user_cred_and_check(ccname);
-	if (!rc)
-		/* user's creds found in default ccache */
-		goto end_ccache;
-
-	/* fallback: look at every file matching
-	 * - /tmp/ *krb5cc*
-	 * - /run/user/<uid>/ *krb5cc*
-	 */
-	rc = find_existing_krb5_ccache(cred->lc_uid, LGSS_DEFAULT_CRED_DIR,
-				       ccname, size);
-	if (!rc)
-		/* user's creds found in LGSS_DEFAULT_CRED_DIR */
-		goto end_ccache;
-
-	rc = find_existing_krb5_ccache(cred->lc_uid, LGSS_USER_CRED_DIR,
-				       ccname, size);
-	if (!rc)
-		/* user's creds found in LGSS_USER_CRED_DIR */
-		goto end_ccache;
-
-	rc = -ENODATA;
-
-end_ccache:
-	return rc;
-}
-
-/**
  * find out whether current TGT cache is valid or not
  */
 static int lkrb5_check_root_tgt_cc(krb5_context ctx, unsigned int flag,
@@ -886,7 +809,7 @@ static int lkrb5_prepare_user_cred(struct lgss_cred *cred)
 {
 	krb5_context ctx;
 	krb5_error_code code;
-	int size = PATH_MAX;
+	int size = PATH_MAX + 1;
 	void *ccname;
 	int rc;
 
@@ -899,30 +822,49 @@ static int lkrb5_prepare_user_cred(struct lgss_cred *cred)
 		return -1;
 	}
 
-	/* buffer passed to get_user_tgt_ccname() must be mmapped with
-	 * MAP_SHARED because it is accessed read/write from a child process
-	 */
-	ccname = mmap(NULL, size, PROT_READ | PROT_WRITE,
-		      MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	if (ccname == MAP_FAILED) {
-		logmsg(LL_ERR, "cannot mmap memory for user %u: %s\n",
-		       cred->lc_uid, strerror(errno));
+	ccname = calloc(1, size);
+	if (!ccname) {
 		rc = -errno;
 		goto free;
 	}
 
-	rc = get_user_tgt_ccname(cred, ctx, ccname, size);
+	/* getting default ccname requires impersonating user */
+	rc = lgss_krb5_get_default_ccache_name(ctx, ccname, size);
+	if (rc)
+		goto end_ccache;
+
+	/* try ccname as returned by gss */
+	rc = acquire_user_cred_and_check(ccname);
+	if (!rc)
+		/* user's creds found in default ccache */
+		goto end_ccache;
+
+	/* fallback: look at every file matching
+	 * - /tmp/ *krb5cc*
+	 * - /run/user/<uid>/ *krb5cc*
+	 */
+	rc = find_existing_krb5_ccache(cred->lc_uid, LGSS_DEFAULT_CRED_DIR,
+				       ccname, size);
+	if (!rc)
+		/* user's creds found in LGSS_DEFAULT_CRED_DIR */
+		goto end_ccache;
+
+	rc = find_existing_krb5_ccache(cred->lc_uid, LGSS_USER_CRED_DIR,
+				       ccname, size);
+	if (!rc)
+		/* user's creds found in LGSS_USER_CRED_DIR */
+		goto end_ccache;
+
+	rc = -ENODATA;
+
+end_ccache:
 	if (rc)
 		logmsg(LL_ERR, "cannot get user %u ccname: %s\n",
 		       cred->lc_uid, strerror(-rc));
 	else
 		logmsg(LL_INFO, "using krb5 cache name: %s\n", (char *)ccname);
 
-	if (munmap(ccname, size) == -1) {
-		logmsg(LL_ERR, "cannot munmap memory for user %u: %s\n",
-		       cred->lc_uid, strerror(errno));
-		rc = rc ? rc : -errno;
-	}
+	free(ccname);
 
 free:
 	krb5_free_context(ctx);

lustre/utils/gss/lgss_utils.c
@@ -543,13 +543,15 @@ int switch_identity(uid_t uid)
 	struct passwd *pw;
 	int rc;
 
-	/* drop list of supp groups */
-	rc = setgroups(0, NULL);
-	if (rc == -1) {
-		logmsg(LL_ERR, "cannot drop list of supp groups: %s\n",
-		       strerror(errno));
-		rc = -errno;
-		goto end_switch;
+	/* drop list of supp groups for regular user */
+	if (uid) {
+		rc = setgroups(0, NULL);
+		if (rc == -1) {
+			logmsg(LL_ERR, "cannot drop list of supp groups: %s\n",
+			       strerror(errno));
+			rc = -errno;
+			goto end_switch;
+		}
 	}
 
 	pw = getpwuid(uid);
@@ -560,7 +562,7 @@ int switch_identity(uid_t uid)
 		goto end_switch;
 	}
 
-	rc = setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid);
+	rc = setresgid(pw->pw_gid, pw->pw_gid, 0);
 	if (rc == -1) {
 		logmsg(LL_ERR, "cannot set real gid to %u: %s\n",
 		       pw->pw_gid, strerror(errno));
@@ -568,7 +570,7 @@ int switch_identity(uid_t uid)
 		goto end_switch;
 	}
 
-	rc = setresuid(uid, uid, uid);
+	rc = setresuid(uid, uid, 0);
 	if (rc == -1) {
 		logmsg(LL_ERR, "cannot set real uid to %u: %s\n",
 		       uid, strerror(errno));

commit 7ff7bd1805ae2b5adfdb9dd2af01c09ab515d413

LU-18633 gss: carry out creds negotiation as user
Instead of switching back to root id right after we did credentials
preparation, just do the whole credentials negotiation as the user.
We just need root privileges to proceed to ioctls that exchange data
with kernel space.

sanity-krb5 test_11 is added to exercise this.

Fixes: 6791fbc530 ("LU-18497 gss: carry out creds prepare as user")
Test-Parameters: trivial
Test-Parameters: testgroup=review-dne-selinux-ssk-part-1
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Test-Parameters: kerberos=true testlist=sanity-krb5
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I93dd367abadca077e61c1910638337a2d80996c8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57752
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/gss/lgss_keyring.c
@@ -167,6 +167,12 @@ static int gss_do_ioctl(struct lgssd_ioctl_param *param)
 	glob_t path;
 	int rc;
 
+	/* switch to root in order to proceed to ioctls */
+	if (param->uid && switch_identity(0)) {
+		rc = -EACCES;
+		goto out_params;
+	}
+
 	rc = cfs_get_param_paths(&path, "sptlrpc/gss/init_channel");
 	if (rc != 0)
 		return rc;
@@ -191,6 +197,11 @@ static int gss_do_ioctl(struct lgssd_ioctl_param *param)
 
 out_params:
 	cfs_free_param_data(&path);
+
+	/* switch back to user */
+	if (param->uid && switch_identity(param->uid))
+		rc = -EACCES;
+
 	return rc;
 }
 
@@ -554,35 +565,6 @@ static void lgssc_fini_nego_data(struct lgss_nego_data *lnd)
         }
 }
 
-static int fork_and_switch_id(int uid, pid_t *child)
-{
-	int status, rc = 0;
-
-	*child = fork();
-	if (*child == -1) {
-		logmsg(LL_ERR, "cannot fork child for user %u: %s\n",
-		       uid, strerror(errno));
-		rc = errno;
-	} else if (*child == 0) {
-		/* switch identity */
-		rc = switch_identity(uid);
-		if (rc)
-			rc = errno;
-	} else {
-		if (wait(&status) < 0) {
-			rc = errno;
-			logmsg(LL_ERR, "child %d failed: %s\n",
-			       *child, strerror(rc));
-		} else {
-			rc = WEXITSTATUS(status);
-			if (rc)
-				logmsg(LL_ERR, "child %d terminated with %d\n",
-				       *child, rc);
-		}
-	}
-	return rc;
-}
-
 static int do_keyctl_update(char *reason, key_serial_t keyid,
 			    const void *payload, size_t plen)
 {
@@ -605,8 +587,7 @@ static int do_keyctl_update(char *reason, key_serial_t keyid,
 static int error_kernel_key(key_serial_t keyid, int rpc_error, int gss_error,
 			    uid_t uid)
 {
-	key_serial_t inst_keyring = KEY_SPEC_SESSION_KEYRING;
-	pid_t child = 1;
+	key_serial_t inst_keyring;
 	int seqwin = 0;
 	char *p, *end;
 	char buf[32];
@@ -614,16 +595,10 @@ static int error_kernel_key(key_serial_t keyid, int rpc_error, int gss_error,
 
 	logmsg(LL_TRACE, "revoking kernel key %08x\n", keyid);
 
-	/* Only possessor and uid can update the key. So for a user key that is
-	 * linked to the user keyring, switch uid/gid in a subprocess to not
-	 * change identity in main process.
-	 */
-	if (uid) {
-		rc = fork_and_switch_id(uid, &child);
-		if (rc || child)
-			goto out;
+	if (uid)
 		inst_keyring = KEY_SPEC_USER_KEYRING;
-	}
+	else
+		inst_keyring = KEY_SPEC_SESSION_KEYRING;
 
 	p = buf;
 	end = buf + sizeof(buf);
@@ -645,10 +620,6 @@ static int error_kernel_key(key_serial_t keyid, int rpc_error, int gss_error,
 		       keyid, inst_keyring);
 	}
 
-out:
-	if (child == 0)
-		/* job done for child */
-		exit(rc);
 	return rc;
 }
 
@@ -658,21 +629,9 @@ static int update_kernel_key(key_serial_t keyid,
 {
 	char *buf = NULL, *p = NULL, *end = NULL;
 	unsigned int buf_size = 0;
-	pid_t child = 1;
-	int uid, rc = 0;
+	int rc = 0;
 
 	logmsg(LL_TRACE, "updating kernel key %08x\n", keyid);
-
-	/* Only possessor and uid can update the key. So for a user key that is
-	 * linked to the user keyring, switch uid/gid in a subprocess to not
-	 * change identity in main process.
-	 */
-	uid = lnd->lnd_uid;
-	if (uid) {
-		rc = fork_and_switch_id(uid, &child);
-		if (rc || child)
-			goto out;
-	}
 
 	buf_size = sizeof(lnd->lnd_seq_win) +
 		sizeof(lnd->lnd_rmt_ctx.length) + lnd->lnd_rmt_ctx.length +
@@ -700,9 +659,6 @@ static int update_kernel_key(key_serial_t keyid,
 
 out:
 	free(buf);
-	if (child == 0)
-		/* job done for child */
-		exit(rc);
 	return rc;
 }
 
@@ -716,8 +672,8 @@ static int lgssc_kr_negotiate_krb(key_serial_t keyid, struct lgss_cred *cred,
 	bool redo = true;
 
 	if (lgss_get_service_str(&g_service, kup->kup_svc, kup->kup_nid)) {
-		logmsg(LL_ERR, "key %08x: failed to construct service "
-		       "string\n", keyid);
+		logmsg(LL_ERR, "key %08x: failed to construct service string\n",
+		       keyid);
 		error_kernel_key(keyid, -EACCES, 0, cred->lc_uid);
 		goto out_cred;
 	}
@@ -731,8 +687,9 @@ static int lgssc_kr_negotiate_krb(key_serial_t keyid, struct lgss_cred *cred,
 retry_nego:
 	memset(&lnd, 0, sizeof(lnd));
 	if (lgssc_init_nego_data(&lnd, kup, cred->lc_mech->lmt_mech_n)) {
-		logmsg(LL_ERR, "key %08x: failed to initialize "
-		       "negotiation data\n", keyid);
+		logmsg(LL_ERR,
+		       "key %08x: failed to initialize negotiation data\n",
+		       keyid);
 		error_kernel_key(keyid, lnd.lnd_rpc_err, lnd.lnd_gss_err,
 				 cred->lc_uid);
 		goto out_cred;
@@ -787,8 +744,8 @@ static int lgssc_kr_negotiate_manual(key_serial_t keyid, struct lgss_cred *cred,
 
 	rc = lgss_get_service_str(&g_service, kup->kup_svc, kup->kup_nid);
 	if (rc) {
-		logmsg(LL_ERR, "key %08x: failed to construct service "
-		       "string\n", keyid);
+		logmsg(LL_ERR, "key %08x: failed to construct service string\n",
+		       keyid);
 		error_kernel_key(keyid, -EACCES, 0, 0);
 		goto out_cred;
 	}
@@ -804,8 +761,9 @@ retry:
 	memset(&lnd, 0, sizeof(lnd));
 	rc = lgssc_init_nego_data(&lnd, kup, cred->lc_mech->lmt_mech_n);
 	if (rc) {
-		logmsg(LL_ERR, "key %08x: failed to initialize "
-		       "negotiation data\n", keyid);
+		logmsg(LL_ERR,
+		       "key %08x: failed to initialize negotiation data\n",
+		       keyid);
 		error_kernel_key(keyid, lnd.lnd_rpc_err, lnd.lnd_gss_err, 0);
 		goto out_cred;
 	}
@@ -1208,6 +1166,13 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	if (!cred->lc_root_flags) {
+		/* switch to user id for creds handling */
+		rc = switch_identity(uparam.kup_uid);
+		if (rc)
+			return rc;
+	}
+
 	/*
 	 * if caller's namespace is different, fork a child and associate it
 	 * with caller's namespace to do credentials preparation
@@ -1365,29 +1330,11 @@ out_pipe:
 		else
 			logmsg(LL_TRACE, "stick with current namespace\n");
 
-		if (!cred->lc_root_flags) {
-			/* switch to user id for creds prepare */
-			rc = switch_identity(uparam.kup_uid);
-			if (rc)
-				goto out_reg;
-		}
-
 		/* In case of prepare error, a key will be instantiated
 		 * all the same. But then we will have to error this key
 		 * instead of doing normal gss negotiation.
 		 */
 		rc = prepare_and_instantiate(cred, keyid, uparam.kup_uid);
-
-		if (!cred->lc_root_flags) {
-			int rc2;
-
-			/* switch back to root in order to proceed to ioctls */
-			rc2 = switch_identity(0);
-			if (rc2) {
-				rc = rc2;
-				goto out_reg;
-			}
-		}
 
 		/*
 		 * fork a child to do the real gss negotiation

lustre/tests/sanity-krb5.sh
@@ -529,6 +529,61 @@ test_10() {
 }
 run_test 10 "Support revoked session keyring"
 
+exit_11() {
+	zconf_umount $HOSTNAME $MOUNT
+
+	zconf_mount $HOSTNAME $MOUNT
+	if [ "$MOUNT_2" ]; then
+		zconf_mount $HOSTNAME $MOUNT2
+	fi
+
+	restore_krb5_cred
+}
+
+test_11() {
+	local count
+
+	$LFS mkdir -i 0 -c $MDSCOUNT $DIR/$tdir ||
+		error "mkdir $DIR/$tdir failed"
+	chmod 0777 $DIR/$tdir || error "chmod $DIR/$tdir failed"
+	$RUNAS ls -ld $DIR/$tdir || error "ls -ld $DIR/$tdir failed"
+	$RUNAS grep lgssc /proc/keys
+	$RUNAS klist
+
+	# get rid of gss context and credentials for user
+	$RUNAS $LFS flushctx -k -r $MOUNT || error "can't flush context (1)"
+	$RUNAS grep lgssc /proc/keys
+	$RUNAS klist
+
+	stack_trap exit_11 EXIT
+	zconf_umount $HOSTNAME $MOUNT || error "umount $MOUNT failed"
+	if [ "$MOUNT_2" ]; then
+		zconf_umount $HOSTNAME $MOUNT2 ||
+			error "umount $MOUNT2 failed"
+	fi
+	kdestroy
+	klist
+
+	# we want KCM ccache
+	cp /etc/krb5.conf /etc/krb5.conf.bkp
+	stack_trap "/bin/mv /etc/krb5.conf.bkp /etc/krb5.conf" EXIT
+	sed -i '1i default_ccache_name = KCM:' /etc/krb5.conf
+	sed -i '1i [libdefaults]' /etc/krb5.conf
+	zconf_mount $HOSTNAME $MOUNT || error "remount $MOUNT failed"
+	klist
+
+	$RUNAS touch $DIR/$tdir/$tfile && error "write $tfile should fail"
+	restore_krb5_cred
+	$RUNAS klist
+	$RUNAS touch $DIR/$tdir/$tfile || error "write $tfile failed"
+	$RUNAS klist
+	$RUNAS klist | grep -q lustre_mds || error "mds ticket not present"
+
+	$RUNAS $LFS flushctx -k -r $MOUNT || error "can't flush context (2)"
+	kdestroy
+}
+run_test 11 "KCM ccache"
+
 #
 # following tests will manipulate flavors and may end with any flavor set,
 # so each test should not assume any start flavor.