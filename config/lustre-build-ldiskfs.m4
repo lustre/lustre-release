lustre/lustre-release

commit 40b69da398ad9c99771528b9a909a9153c7dadfc

LU-18423 kernel: update RHEL 8.10 [4.18.0-553.27.1.el8_10]
Update RHEL 8.10 kernel to 4.18.0-553.27.1.el8_10.

Test-Parameters: trivial fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.9 testlist=sanity

Test-Parameters: trivial fstype=zfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.9 testlist=sanity

Test-Parameters: trivial fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.9 serverdistro=el8.10 testlist=sanity

Test-Parameters: trivial fstype=zfs mdtcount=4 mdscount=2 \
  clientdistro=el8.9 serverdistro=el8.10 testlist=sanity

Test-Parameters: optional clientdistro=el8.10 serverdistro=el8.10 \
  testgroup=full-part-1

Test-Parameters: optional clientdistro=el8.10 serverdistro=el8.10 \
  testgroup=full-part-2

Test-Parameters: optional clientdistro=el8.10 serverdistro=el8.10 \
  testgroup=full-part-3

Change-Id: I3737c1f1b2941d2095225f1ab80fd76768c4782c
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56888
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Alex Deiter <adeiter@ddn.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>

lustre/kernel_patches/targets/4.18-rhel8.10.target.in
@@ -1,5 +1,5 @@
 lnxmaj="4.18.0"
-lnxrel="553.16.1.el8_10"
+lnxrel="553.27.1.el8_10"
 
 KERNEL_SRPM=kernel-${lnxmaj}-${lnxrel}.src.rpm
 SERIES=4.18-rhel8.10.series

lustre/kernel_patches/which_patch
@@ -20,7 +20,7 @@ PATCH SERIES FOR SERVER KERNELS:
 4.18-rhel8.7.series     4.18.0-425.10.1.el8  (RHEL 8.7)
 4.18-rhel8.8.series     4.18.0-477.27.1.el8  (RHEL 8.8)
 4.18-rhel8.9.series     4.18.0-513.24.1.el8  (RHEL 8.9)
-4.18-rhel8.10.series    4.18.0-553.16.1.el8  (RHEL 8.10)
+4.18-rhel8.10.series    4.18.0-553.27.1.el8  (RHEL 8.10)
 5.14-rhel9.1.series     5.14.0-162.23.1.el9  (RHEL 9.1)
 5.14-rhel9.2.series     5.14.0-284.30.1.el9  (RHEL 9.2)
 5.14-rhel9.3.series     5.14.0-362.24.1.el9  (RHEL 9.3)

ldiskfs/kernel_patches/patches/rhel8.10/ext4-delayed-iput.patch
@@ -1,0 +1,175 @@
+When changing a large xattr value to a different large xattr value,
+the old xattr inode is freed. Truncate during the final iput causes
+current transaction restart. Eventually, parent inode bh is marked
+dirty and kernel panic happens when jbd2 figures out that this bh
+belongs to the committed transaction.
+
+A possible fix is to call this final iput in a separate thread.
+This way, setxattr transactions will never be split into two.
+Since the setxattr code adds xattr inodes with nlink=0 into the
+orphan list, old xattr inodes will be properly cleaned up in
+any case.
+
+Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
+HPE-bug-id: LUS-10534
+
+Changes since v1:
+- fixed a bug added during the porting
+- fixed a workqueue related deadlock reported by Tetsuo Handa
+---
+ fs/ext4/ext4.h    |  7 +++++--
+ fs/ext4/page-io.c |  2 +-
+ fs/ext4/super.c   | 15 ++++++++-------
+ fs/ext4/xattr.c   | 39 +++++++++++++++++++++++++++++++++++++--
+ 4 files changed, 51 insertions(+), 12 deletions(-)
+
+diff --git a/fs/ext4/ext4.h b/fs/ext4/ext4.h
+index 3f87cca49f0c..52db5d6bae7f 100644
+--- a/fs/ext4/ext4.h
++++ b/fs/ext4/ext4.h
+@@ -1650,8 +1650,11 @@ struct ext4_sb_info {
+ 	struct flex_groups * __rcu *s_flex_groups;
+ 	ext4_group_t s_flex_groups_allocated;
+
+-	/* workqueue for reserved extent conversions (buffered io) */
+-	struct workqueue_struct *rsv_conversion_wq;
++	/*
++	 * workqueue for reserved extent conversions (buffered io)
++	 * and large ea inodes reclaim
++	 */
++	struct workqueue_struct *s_misc_wq;
+
+ 	/* timer for periodic error stats printing */
+ 	struct timer_list s_err_report;
+diff --git a/fs/ext4/page-io.c b/fs/ext4/page-io.c
+index 495ce59fb4ad..0142b88471ff 100644
+--- a/fs/ext4/page-io.c
++++ b/fs/ext4/page-io.c
+@@ -228,7 +228,7 @@ static void ext4_add_complete_io(ext4_io_end_t *io_end)
+ 	WARN_ON(!(io_end->flag & EXT4_IO_END_UNWRITTEN));
+ 	WARN_ON(!io_end->handle && sbi->s_journal);
+ 	spin_lock_irqsave(&ei->i_completed_io_lock, flags);
+-	wq = sbi->rsv_conversion_wq;
++	wq = sbi->s_misc_wq;
+ 	if (list_empty(&ei->i_rsv_conversion_list))
+ 		queue_work(wq, &ei->i_rsv_conversion_work);
+ 	list_add_tail(&io_end->list, &ei->i_rsv_conversion_list);
+diff --git a/fs/ext4/super.c b/fs/ext4/super.c
+index 81749eaddf4c..ee03f593b264 100644
+--- a/fs/ext4/super.c
++++ b/fs/ext4/super.c
+@@ -1200,8 +1200,9 @@ static void ext4_put_super(struct super_block *sb)
+ 	int i, err;
+
+ 	ext4_unregister_li_request(sb);
++	flush_workqueue(sbi->s_misc_wq);
+ 	ext4_quota_off_umount(sb);
+
+-	destroy_workqueue(sbi->rsv_conversion_wq);
++	destroy_workqueue(sbi->s_misc_wq);
+
+ 	/*
+@@ -5294,9 +5295,9 @@ static int __ext4_fill_super(struct fs_context *fc, struct super_block *sb)
+ 	 * The maximum number of concurrent works can be high and
+ 	 * concurrency isn't really necessary.  Limit it to 1.
+ 	 */
+-	EXT4_SB(sb)->rsv_conversion_wq =
+-		alloc_workqueue("ext4-rsv-conversion", WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
+-	if (!EXT4_SB(sb)->rsv_conversion_wq) {
++	EXT4_SB(sb)->s_misc_wq =
++		alloc_workqueue("ext4-misc", WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
++	if (!EXT4_SB(sb)->s_misc_wq) {
+ 		printk(KERN_ERR "EXT4-fs: failed to create workqueue\n");
+ 		ret = -ENOMEM;
+ 		goto failed_mount4;
+@@ -5514,8 +5515,8 @@ static int __ext4_fill_super(struct fs_context *fc, struct super_block *sb)
+ 	sb->s_root = NULL;
+ failed_mount4:
+ 	ext4_msg(sb, KERN_ERR, "mount failed");
+-	if (EXT4_SB(sb)->rsv_conversion_wq)
+-		destroy_workqueue(EXT4_SB(sb)->rsv_conversion_wq);
++	if (EXT4_SB(sb)->s_misc_wq)
++		destroy_workqueue(EXT4_SB(sb)->s_misc_wq);
+ failed_mount_wq:
+ 	ext4_xattr_destroy_cache(sbi->s_ea_inode_cache);
+ 	sbi->s_ea_inode_cache = NULL;
+@@ -6129,7 +6130,7 @@ static int ext4_sync_fs(struct super_block *sb, int wait)
+ 		return 0;
+
+ 	trace_ext4_sync_fs(sb, wait);
+-	flush_workqueue(sbi->rsv_conversion_wq);
++	flush_workqueue(sbi->s_misc_wq);
+ 	/*
+ 	 * Writeback quota in non-journalled quota case - journalled quota has
+ 	 * no dirty dquots
+diff --git a/fs/ext4/xattr.c b/fs/ext4/xattr.c
+index 042325349098..ee13675fbead 100644
+--- a/fs/ext4/xattr.c
++++ b/fs/ext4/xattr.c
+@@ -1603,6 +1603,36 @@ out_err:
+ 	return ERR_PTR(err);
+ }
+
++struct delayed_iput_work {
++	struct work_struct work;
++	struct inode *inode;
++};
++
++static void delayed_iput_fn(struct work_struct *work)
++{
++	struct delayed_iput_work *diwork;
++
++	diwork = container_of(work, struct delayed_iput_work, work);
++	iput(diwork->inode);
++	kfree(diwork);
++}
++
++static void delayed_iput(struct inode *inode, struct delayed_iput_work *work)
++{
++	if (!inode) {
++		kfree(work);
++		return;
++	}
++
++	if (!work) {
++		iput(inode);
++	} else {
++		INIT_WORK(&work->work, delayed_iput_fn);
++		work->inode = inode;
++		queue_work(EXT4_SB(inode->i_sb)->s_misc_wq, &work->work);
++	}
++}
++
+ /*
+  * Reserve min(block_size/8, 1024) bytes for xattr entries/names if ea_inode
+  * feature is enabled.
+@@ -1620,6 +1650,7 @@ static int ext4_xattr_set_entry(struct e
+ 	size_t min_offs = s->end - s->base, name_len = strlen(i->name);
+ 	int in_inode = i->in_inode;
+ 	struct inode *old_ea_inode = NULL;
++	struct delayed_iput_work *diwork = NULL;
+ 	size_t old_size, new_size;
+ 	int ret;
+
+@@ -1696,7 +1727,11 @@ static int ext4_xattr_set_entry(struct e
+ 	 * Finish that work before doing any modifications to the xattr data.
+ 	 */
+ 	if (!s->not_found && here->e_value_inum) {
+-		ret = ext4_xattr_inode_iget(inode,
++		diwork = kmalloc(sizeof(*diwork), GFP_NOFS);
++		if (!diwork)
++			ret = -ENOMEM;
++		else
++			ret = ext4_xattr_inode_iget(inode,
+ 					    le32_to_cpu(here->e_value_inum),
+ 					    le32_to_cpu(here->e_hash),
+ 					    &old_ea_inode);
+@@ -1817,7 +1852,7 @@ update_hash:
+
+ 	ret = 0;
+ out:
+-	iput(old_ea_inode);
++	delayed_iput(old_ea_inode, diwork);
+ 	return ret;
+ }
+

ldiskfs/kernel_patches/series/ldiskfs-4.18-rhel8.10.series
@@ -33,7 +33,7 @@ rhel8/ext4-ialloc-uid-gid-and-pass-owner-down.patch
 base/ext4-projid-xattrs.patch
 rhel8.5/ext4-enc-flag.patch
 rhel8/ext4-ext-merge.patch
-base/ext4-delayed-iput.patch
+rhel8.10/ext4-delayed-iput.patch
 rhel8/ext4-add-periodic-superblock-update.patch
 rhel8.7/ext4-filename-encode.patch
 rhel8/ext4-old_ea_inodes_handling_fix.patch

commit 44af0501c4afa0cc125d3cbec025632b9e594ed9

LU-18702 kernel: update RHEL 8.10 [4.18.0-553.37.1.el8_10]
Update RHEL 8.10 kernel to 4.18.0-553.37.1.el8_10.

The patch also provides a fallback series which uses
the former base/ext4-delayed-iput.patch for kernel
before 4.18.0-553.22.1.el8_10.

Test-Parameters: trivial fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testlist=sanity

Test-Parameters: optional fstype=zfs mdtcount=4 mdscount=2 \
  env=SANITY_EXCEPT="66 413" \
  clientdistro=el8.10 serverdistro=el8.10 testlist=sanity

Test-Parameters: optional fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-part-1

Test-Parameters: optional fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-part-2

Test-Parameters: optional fstype=ldiskfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-part-3

Test-Parameters: optional fstype=zfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-zfs-part-1

Test-Parameters: optional fstype=zfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-zfs-part-2

Test-Parameters: optional fstype=zfs mdtcount=4 mdscount=2 \
  clientdistro=el8.10 serverdistro=el8.10 testgroup=full-dne-zfs-part-3

Fixes: 40b69da398a ("LU-18423 kernel: update RHEL 8.10 [4.18.0-553.27.1.el8_10]")
Change-Id: I5cc375a4a3586e1485fa0d9934ea1693f6899a19
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58006
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Alex Deiter <adeiter@ddn.com>

config/lustre-build-ldiskfs.m4
@@ -32,7 +32,10 @@ AS_IF([test x$RHEL_KERNEL = xyes], [
 	92)     LDISKFS_SERIES="5.14-rhel9.2.series"    ;;
 	91)     LDISKFS_SERIES="5.14-rhel9.1.series"    ;;
 	90)     LDISKFS_SERIES="5.14-rhel9.series"      ;;
-	810)    LDISKFS_SERIES="4.18-rhel8.10.series"   ;;
+	810)    LDISKFS_SERIES="4.18-rhel8.10.series"
+		AS_VERSION_COMPARE([$RHEL_RELEASE_STR],[553.22.1],
+			[LDISKFS_SERIES="4.18-rhel8.10-553.series"],[],[])
+		;;
 	89)     LDISKFS_SERIES="4.18-rhel8.9.series"    ;;
 	88)     LDISKFS_SERIES="4.18-rhel8.8.series"    ;;
 	87)     LDISKFS_SERIES="4.18-rhel8.7.series"    ;;

ldiskfs/kernel_patches/series/ldiskfs-4.18-rhel8.10-553.series
@@ -1,0 +1,44 @@
+rhel8.1/ext4-inode-version.patch
+suse15/ext4-lookup-dotdot.patch
+suse15/ext4-print-inum-in-htree-warning.patch
+rhel8/ext4-prealloc.patch
+ubuntu18/ext4-osd-iop-common.patch
+rhel8.7/ext4-misc.patch
+rhel8.7/ext4-mballoc-extra-checks.patch
+rhel8.7/ext4-hash-indexed-dir-dotdot-update.patch
+rhel8.1/ext4-kill-dx-root.patch
+rhel8.7/ext4-mballoc-pa-free-mismatch.patch
+rhel8.4/ext4-data-in-dirent.patch
+rhel8/ext4-nocmtime.patch
+base/ext4-htree-lock.patch
+rhel8.7/ext4-pdirop.patch
+rhel8/ext4-deep-tree.patch
+rhel8/ext4-max-dir-size.patch
+rhel8.7/ext4-corrupted-inode-block-bitmaps-handling-patches.patch
+ubuntu18/ext4-give-warning-with-dir-htree-growing.patch
+ubuntu18/ext4-jcb-optimization.patch
+rhel8.2/ext4-attach-jinode-in-writepages.patch
+rhel8/ext4-dont-check-before-replay.patch
+rhel7.6/ext4-use-GFP_NOFS-in-ext4_inode_attach_jinode.patch
+rhel7.6/ext4-export-orphan-add.patch
+rhel8/ext4-export-mb-stream-allocator-variables.patch
+rhel8/ext4-simple-blockalloc.patch
+rhel8.7/ext4-mballoc-skip-uninit-groups-cr0.patch
+rhel8.7/ext4-mballoc-prefetch.patch
+rhel8.3/ext4-xattr-disable-credits-check.patch
+base/ext4-no-max-dir-size-limit-for-iam-objects.patch
+rhel7.6/ext4-dquot-commit-speedup.patch
+rhel8.7/ext4-introduce-EXT4_BG_TRIMMED-to-optimize-fstrim.patch
+rhel8/ext4-ialloc-uid-gid-and-pass-owner-down.patch
+base/ext4-projid-xattrs.patch
+rhel8.5/ext4-enc-flag.patch
+rhel8/ext4-ext-merge.patch
+base/ext4-delayed-iput.patch
+rhel8/ext4-add-periodic-superblock-update.patch
+rhel8.7/ext4-filename-encode.patch
+rhel8/ext4-old_ea_inodes_handling_fix.patch
+rhel8.4/ext4-optimize-find_delayed_extent.patch
+rhel8/ext4-encdata.patch
+rhel8/ext4-race-in-ext4-destroy-inode.patch
+rhel8/ext4-mballoc-dense.patch
+rhel8/ext4-limit-per-inode-preallocation-list.patch

lustre/kernel_patches/targets/4.18-rhel8.10.target.in
@@ -1,5 +1,5 @@
 lnxmaj="4.18.0"
-lnxrel="553.27.1.el8_10"
+lnxrel="553.37.1.el8_10"
 
 KERNEL_SRPM=kernel-${lnxmaj}-${lnxrel}.src.rpm
 SERIES=4.18-rhel8.10.series

lustre/kernel_patches/which_patch
@@ -20,7 +20,7 @@ PATCH SERIES FOR SERVER KERNELS:
 4.18-rhel8.7.series     4.18.0-425.10.1.el8  (RHEL 8.7)
 4.18-rhel8.8.series     4.18.0-477.27.1.el8  (RHEL 8.8)
 4.18-rhel8.9.series     4.18.0-513.24.1.el8  (RHEL 8.9)
-4.18-rhel8.10.series    4.18.0-553.27.1.el8  (RHEL 8.10)
+4.18-rhel8.10.series    4.18.0-553.37.1.el8  (RHEL 8.10)
 5.14-rhel9.1.series     5.14.0-162.23.1.el9  (RHEL 9.1)
 5.14-rhel9.2.series     5.14.0-284.30.1.el9  (RHEL 9.2)
 5.14-rhel9.3.series     5.14.0-362.24.1.el9  (RHEL 9.3)