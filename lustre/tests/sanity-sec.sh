lustre/lustre-release

commit e3051ad0f1c8c3399c33eaf7cb0cc906177f7891

LU-18109 utils: adding nodemap offset capability
nodemap offset: the ability to add an offset range to nodemaps.
When an offset is defined, idmaps can only be set inside of their
given range. This is defined as:

lctl nodemap_add_offset --name nodemap --offset 100000 --limit 99999

And removed as:

lctl nodemap_del_offset --name nodemap

Test sanity-sec 27ab is added to exercise this new feature.

Signed-off-by: Maximilian Dilger <mdilger@whamcloud.com>
Change-Id: Iba2116d21bc7de1ba03111b0313427301e4b0b50
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55943
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -26,6 +26,7 @@
  *
  * Author: Joshua Walgenbach <jjw@iu.edu>
  */
+
 #include <linux/module.h>
 #include <linux/sort.h>
 #include <uapi/linux/lnet/nidstr.h>
@@ -782,8 +783,11 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 		     enum nodemap_id_type id_type,
 		     enum nodemap_tree_type tree_type, __u32 id)
 {
-	struct lu_idmap		*idmap = NULL;
-	__u32			 found_id;
+	struct lu_idmap *idmap = NULL;
+	__u32 offset_start;
+	__u32 offset_limit;
+	__u32 found_id = id;
+	bool attempted_squash = false;
 
 	ENTRY;
 
@@ -801,18 +805,18 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 
 	if (id_type == NODEMAP_UID &&
 	    !(nodemap->nmf_map_mode & NODEMAP_MAP_UID))
-		goto out;
+		goto offset;
 
 	if (id_type == NODEMAP_GID &&
 	    !(nodemap->nmf_map_mode & NODEMAP_MAP_GID))
-		goto out;
+		goto offset;
 
 	if (id_type == NODEMAP_PROJID &&
 	    !(nodemap->nmf_map_mode & NODEMAP_MAP_PROJID))
-		goto out;
+		goto offset;
 
 	if (nodemap->nmf_trust_client_ids)
-		goto out;
+		goto offset;
 
 map:
 	if (is_default_nodemap(nodemap))
@@ -830,17 +834,51 @@ map:
 	else
 		found_id = idmap->id_fs;
 	up_read(&nodemap->nm_idmap_lock);
-	RETURN(found_id);
+	GOTO(offset, found_id);
 
 squash:
 	if (id_type == NODEMAP_UID)
-		RETURN(nodemap->nm_squash_uid);
-	if (id_type == NODEMAP_GID)
-		RETURN(nodemap->nm_squash_gid);
-	if (id_type == NODEMAP_PROJID)
-		RETURN(nodemap->nm_squash_projid);
+		found_id = nodemap->nm_squash_uid;
+	else if (id_type == NODEMAP_GID)
+		found_id = nodemap->nm_squash_gid;
+	else if (id_type == NODEMAP_PROJID)
+		found_id = nodemap->nm_squash_projid;
+	attempted_squash = true;
+offset:
+	if (id_type == NODEMAP_UID) {
+		offset_start = nodemap->nm_offset_start_uid;
+		offset_limit = nodemap->nm_offset_limit_uid;
+	} else if (id_type == NODEMAP_GID) {
+		offset_start = nodemap->nm_offset_start_uid;
+		offset_limit = nodemap->nm_offset_limit_gid;
+	} else if (id_type == NODEMAP_PROJID) {
+		offset_start = nodemap->nm_offset_start_projid;
+		offset_limit = nodemap->nm_offset_limit_projid;
+	} else {
+		CERROR("%s: nodemap invalid id_type provided\n",
+		       nodemap->nm_name);
+		GOTO(out, id);
+	}
+
+	if (offset_start == 0 && offset_limit == 0)
+		GOTO(out, found_id);
+
+	if (tree_type == NODEMAP_FS_TO_CLIENT) {
+		found_id -= offset_start;
+	} else {
+		if (found_id >= offset_limit && !attempted_squash)
+			GOTO(squash, found_id);
+
+		if (attempted_squash) {
+			CERROR("%s: squash_id for type %u is outside nodemap limit %u, use unmapped value %u\n",
+			       nodemap->nm_name, id_type, offset_limit,
+			       found_id);
+			GOTO(out, found_id);
+		}
+		found_id += offset_start;
+	}
 out:
-	RETURN(id);
+	RETURN(found_id);
 }
 EXPORT_SYMBOL(nodemap_map_id);
 
@@ -1333,6 +1371,12 @@ struct lu_nodemap *nodemap_create(const char *name,
 		nodemap->nm_squash_projid = NODEMAP_NOBODY_PROJID;
 		nodemap->nm_fileset[0] = '\0';
 		nodemap->nm_sepol[0] = '\0';
+		nodemap->nm_offset_start_uid = 0;
+		nodemap->nm_offset_limit_uid = 0;
+		nodemap->nm_offset_start_gid = 0;
+		nodemap->nm_offset_limit_gid = 0;
+		nodemap->nm_offset_start_projid = 0;
+		nodemap->nm_offset_limit_projid = 0;
 		if (!is_default)
 			CWARN("adding nodemap '%s' to config without"
 			      " default nodemap\n", nodemap->nm_name);
@@ -1355,6 +1399,12 @@ struct lu_nodemap *nodemap_create(const char *name,
 		nodemap->nm_squash_projid = default_nodemap->nm_squash_projid;
 		nodemap->nm_fileset[0] = '\0';
 		nodemap->nm_sepol[0] = '\0';
+		nodemap->nm_offset_start_uid = 0;
+		nodemap->nm_offset_limit_uid = 0;
+		nodemap->nm_offset_start_gid = 0;
+		nodemap->nm_offset_limit_gid = 0;
+		nodemap->nm_offset_start_projid = 0;
+		nodemap->nm_offset_limit_projid = 0;
 	}
 
 	RETURN(nodemap);
@@ -1877,6 +1927,201 @@ out:
 }
 EXPORT_SYMBOL(nodemap_del);
 
+/* Do not call this method directly unless the ranges and nodemap have been
+ * previously verified.
+ * Store separate offset+limit in case this needs to be changed
+ * in the future, but for now there is no good reason to expose
+ * this complexity to userspace.
+ * TODO allow individual setting of values
+ */
+int nodemap_add_offset_helper(struct lu_nodemap *nodemap, __u32 offset_start,
+			      __u32 offset_limit)
+{
+	if (IS_ERR_OR_NULL(nodemap))
+		return -ENOENT;
+
+	nodemap->nm_offset_start_uid = offset_start;
+	nodemap->nm_offset_limit_uid = offset_limit;
+	nodemap->nm_offset_start_gid = offset_start;
+	nodemap->nm_offset_limit_gid = offset_limit;
+	nodemap->nm_offset_start_projid = offset_start;
+	nodemap->nm_offset_limit_projid = offset_limit;
+	return 0;
+}
+
+/**
+ * The nodemap offset shifts client UID/GID/PROJIDs from the range [0,limit)
+ * to a new range [offset,offset+limit).  This is useful for clusters that share
+ * a single filesystem among several tenants that administer their IDs
+ * independently. The offsets provide non-overlapping spaces with "limit"
+ * IDs each without having to configure individual idmaps for each ID.
+ *
+ * \param	name		name of nodemmap
+ * \param	offset		offset+limit
+ * \retval	0		success
+ * \retval	-EINVAL		invalid input
+ * \retval	-ENOENT		no existing nodemap
+ */
+int nodemap_add_offset(const char *nodemap_name, char *offset)
+{
+	struct lu_nodemap *nodemap;
+	struct lu_nodemap *nm_iterating;
+	struct lu_nodemap *nm_tmp;
+	unsigned long offset_start, offset_limit;
+	unsigned long min, max;
+	bool overlap = false;
+	LIST_HEAD(nodemap_list_head);
+	char *offset_max;
+	int rc = 0;
+
+	offset_max = strchr(offset, '+');
+	if (offset_max == NULL)
+		GOTO(out, rc = -EINVAL);
+	*offset_max = '\0';
+	offset_max++;
+
+	rc = kstrtoul(offset, 10, &offset_start);
+	if (rc) {
+		CERROR("%s: nodemap offset_start '%lu' not valid: rc = %d\n",
+		       nodemap_name, offset_start, rc);
+		GOTO(out, rc);
+	}
+	rc = kstrtoul(offset_max, 10, &offset_limit);
+	if (rc) {
+		CERROR("%s: nodemap offset_limit '%lu' not valid: rc = %d\n",
+		       nodemap_name, offset_limit, rc);
+		GOTO(out, rc);
+	}
+	if (offset_start == 0 || offset_start >= UINT_MAX) {
+		rc = -EINVAL;
+		CERROR("%s: nodemap offset_start '%lu' is invalid: rc = %d\n",
+		       nodemap_name, offset_start, rc);
+		GOTO(out, rc);
+	}
+	if (offset_limit == 0 || offset_limit >= UINT_MAX) {
+		rc = -EINVAL;
+		CERROR("%s: nodemap offset_limit '%lu' is invalid: rc = %d\n",
+		       nodemap_name, offset_limit, rc);
+		GOTO(out, rc);
+	}
+	if (offset_start + offset_limit >= UINT_MAX) {
+		rc = -EINVAL;
+		CERROR("%s: nodemap offset_start+offset_limit '%s+%s' would overflow: rc = %d\n",
+		       nodemap_name, offset, offset_max, rc);
+		GOTO(out, rc);
+	}
+
+	mutex_lock(&active_config_lock);
+	nodemap = nodemap_lookup(nodemap_name);
+	if (IS_ERR(nodemap)) {
+		mutex_unlock(&active_config_lock);
+		GOTO(out, rc = -ENOENT);
+	}
+
+	if (is_default_nodemap(nodemap))
+		GOTO(out_putref, rc = -EINVAL);
+
+	if (nodemap->nm_offset_start_uid) {
+		/* nodemap has already offset  */
+		nm_iterating = nodemap;
+		GOTO(overlap, rc = -ERANGE);
+	}
+
+	cfs_hash_for_each_safe(active_config->nmc_nodemap_hash,
+			       nm_hash_list_cb, &nodemap_list_head);
+
+	list_for_each_entry_safe(nm_iterating, nm_tmp, &nodemap_list_head,
+				 nm_list) {
+		if (nodemap_name == nm_iterating->nm_name)
+			continue;
+		min = nm_iterating->nm_offset_start_uid;
+		max = nm_iterating->nm_offset_start_uid +
+			nm_iterating->nm_offset_limit_uid;
+		if (min == 0 && max == 0) /* nodemaps with no set offset */
+			continue;
+		/* seeing if new offset / offset_max overlaps with other
+		 * existing nodemap offsets
+		 */
+		if (offset_start <= max - 1 &&
+		    offset_start + offset_limit - 1 >= min) {
+			overlap = true;
+			break;
+		}
+	}
+
+	if (overlap) {
+overlap:
+		rc = -ERANGE;
+		CERROR("%s: new offset %lu+%lu overlaps with existing nodemap %s offset %u+%u: rc = %d\n",
+		       nodemap_name, offset_start, offset_limit,
+		       nm_iterating->nm_name, nm_iterating->nm_offset_start_uid,
+		       nm_iterating->nm_offset_limit_uid, rc);
+		GOTO(out_putref, rc);
+	}
+
+	rc = nodemap_add_offset_helper(nodemap, offset_start, offset_limit);
+	if (rc == 0)
+		rc = nodemap_idx_offset_add(nodemap);
+	if (rc == 0)
+		nm_member_revoke_locks(nodemap);
+
+out_putref:
+	mutex_unlock(&active_config_lock);
+	nodemap_putref(nodemap);
+out:
+	return rc;
+}
+
+int nodemap_del_offset_helper(struct lu_nodemap *nodemap)
+{
+	if (IS_ERR_OR_NULL(nodemap))
+		return -ENOENT;
+
+	nodemap->nm_offset_start_uid = 0;
+	nodemap->nm_offset_limit_uid = 0;
+	nodemap->nm_offset_start_gid = 0;
+	nodemap->nm_offset_limit_gid = 0;
+	nodemap->nm_offset_start_projid = 0;
+	nodemap->nm_offset_limit_projid = 0;
+	return 0;
+}
+
+/**
+ * Delete mapping offset.
+ *
+ * \param	name		name of nodemmap
+ * \retval	0		success
+ * \retval	-EINVAL		invalid input
+ * \retval	-ENOENT		no existing nodemap
+ */
+int nodemap_del_offset(const char *nodemap_name)
+{
+	struct lu_nodemap *nodemap;
+	int rc = 0;
+
+	mutex_lock(&active_config_lock);
+	nodemap = nodemap_lookup(nodemap_name);
+	if (IS_ERR(nodemap)) {
+		mutex_unlock(&active_config_lock);
+		GOTO(out, rc = -ENOENT);
+	}
+
+	if (is_default_nodemap(nodemap))
+		GOTO(out_putref, rc = -EINVAL);
+
+	rc = nodemap_del_offset_helper(nodemap);
+	if (rc == 0)
+		rc = nodemap_idx_offset_del(nodemap);
+	if (rc == 0)
+		nm_member_revoke_locks(nodemap);
+
+out_putref:
+	mutex_unlock(&active_config_lock);
+	nodemap_putref(nodemap);
+out:
+	return rc;
+}
+
 /**
  * activate nodemap functions
  *
@@ -2345,6 +2590,12 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
 			break;
 		rc = nodemap_set_squash_projid(nodemap_name, int_id);
 		break;
+	case LCFG_NODEMAP_ADD_OFFSET:
+		rc = nodemap_add_offset(nodemap_name, param);
+		break;
+	case LCFG_NODEMAP_DEL_OFFSET:
+		rc = nodemap_del_offset(nodemap_name);
+		break;
 	case LCFG_NODEMAP_ADD_UIDMAP:
 	case LCFG_NODEMAP_ADD_GIDMAP:
 	case LCFG_NODEMAP_ADD_PROJIDMAP:
@@ -2439,6 +2690,7 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 		break;
 	case LCFG_NODEMAP_ADD:
 	case LCFG_NODEMAP_DEL:
+	case LCFG_NODEMAP_DEL_OFFSET:
 		if (lcfg->lcfg_bufcount != 2)
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);
@@ -2495,6 +2747,7 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 				 sizeof(fs_idstr)) != 0)
 			GOTO(out_lcfg, rc = -EINVAL);
 		break;
+	case LCFG_NODEMAP_ADD_OFFSET:
 	case LCFG_NODEMAP_ADD_RANGE:
 	case LCFG_NODEMAP_DEL_RANGE:
 	case LCFG_NODEMAP_ADD_UIDMAP:

commit b162043239646ca7a26da2146b56578b5ed0d3af

LU-18109 nodemap: fix idmap offset for root
root must be mapped and offset just like other ids, so that all ids on
the client in the range [0, offset_limit-1] are mapped to the
filesystem id range [offset, offset+offset_limit-1].
This means that as soon as an offset is defined on a nodemap, root is
necessarily mapped:
- if admin=1, root id is offset
- if admin=0, root is squashed then offset.
Similarly:
- if trusted=1, ids are mapped (with any explicit mapping) and offset
- if trusted=0, ids are squashed then offset.

Moreover, add_offset and del_offset operations are only allowed on the
MGS or for a dynamic nodemap.

Finally, enhance sanity-sec test_27ab to test id mappings with offset.

Fixes: e3051ad0f1 ("LU-18109 utils: adding nodemap offset capability")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7a20c59496174d9d1fc040c5b3b43d8dad3447f9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57856
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>

lustre/mdt/mdt_lib.c
@@ -258,7 +258,10 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 		GOTO(out_nodemap, rc = -EACCES);
 	}
 
-	if (nodemap && ucred->uc_o_uid == nodemap->nm_squash_uid &&
+	if (nodemap &&
+	    ucred->uc_o_uid == nodemap_map_id(nodemap, NODEMAP_UID,
+					      NODEMAP_CLIENT_TO_FS,
+					      nodemap->nm_squash_uid) &&
 	    nodemap->nmf_deny_unknown)
 		/* deny access before we get identity ref */
 		GOTO(out, rc = -EACCES);
@@ -528,8 +531,11 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 	struct mdt_device *mdt = info->mti_mdt;
 	struct md_identity *identity = NULL;
 
-	if (nodemap && uc->uc_o_uid == nodemap->nm_squash_uid
-	    && nodemap->nmf_deny_unknown)
+	if (nodemap &&
+	    uc->uc_o_uid == nodemap_map_id(nodemap, NODEMAP_UID,
+					   NODEMAP_CLIENT_TO_FS,
+					   nodemap->nm_squash_uid) &&
+	    nodemap->nmf_deny_unknown)
 		/* deny access before we get identity ref */
 		RETURN(-EACCES);
 

lustre/ptlrpc/nodemap_handler.c
@@ -780,9 +780,12 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 	if (unlikely(nodemap == NULL))
 		goto out;
 
-	if (id == 0) {
+	if (id_type != NODEMAP_PROJID && id == 0) {
+		/* root id is mapped and offset just as the other ids. This
+		 * means root cannot remain root as soon as offset is defined.
+		 */
 		if (nodemap->nmf_allow_root_access)
-			goto out;
+			goto offset;
 		goto map;
 	}
 
@@ -847,13 +850,30 @@ offset:
 		GOTO(out, found_id);
 
 	if (tree_type == NODEMAP_FS_TO_CLIENT) {
+		if (found_id < offset_start) {
+			/* If we are outside boundaries, try to squash before
+			 * offsetting, and return unmapped otherwise.
+			 */
+			if (!attempted_squash)
+				GOTO(squash, found_id);
+
+			CDEBUG(D_SEC,
+			       "%s: squash_id for type %u is below nodemap start %u, use unmapped value %u\n",
+			       nodemap->nm_name, id_type, offset_start,
+			       found_id);
+			GOTO(out, found_id);
+		}
 		found_id -= offset_start;
 	} else {
-		if (found_id >= offset_limit && !attempted_squash)
-			GOTO(squash, found_id);
+		if (found_id >= offset_limit) {
+			/* If we are outside boundaries, try to squash before
+			 * offsetting, and return unmapped otherwise.
+			 */
+			if (!attempted_squash)
+				GOTO(squash, found_id);
 
-		if (attempted_squash) {
-			CERROR("%s: squash_id for type %u is outside nodemap limit %u, use unmapped value %u\n",
+			CDEBUG(D_SEC,
+			       "%s: squash_id for type %u is outside nodemap limit %u, use unmapped value %u\n",
 			       nodemap->nm_name, id_type, offset_limit,
 			       found_id);
 			GOTO(out, found_id);
@@ -1681,7 +1701,8 @@ EXPORT_SYMBOL(nodemap_set_squash_projid);
  * Check if nodemap allows setting quota.
  *
  * If nodemap is not active, always allow.
- * For user and group quota, allow if the nodemap allows root access.
+ * For user and group quota, allow if the nodemap allows root access, unless
+ * root is mapped.
  * For project quota, allow if project id is not squashed or deny_unknown
  * is not set.
  *
@@ -1697,6 +1718,9 @@ bool nodemap_can_setquota(struct lu_nodemap *nodemap, __u32 qc_type, __u32 id)
 
 	if (!nodemap || !nodemap->nmf_allow_root_access ||
 	    !(nodemap->nmf_rbac & NODEMAP_RBAC_QUOTA_OPS))
+		return false;
+
+	if (nodemap_map_id(nodemap, NODEMAP_UID, NODEMAP_CLIENT_TO_FS, 0) != 0)
 		return false;
 
 	if (qc_type == PRJQUOTA) {
@@ -2011,6 +2035,8 @@ int nodemap_add_offset(const char *nodemap_name, char *offset)
 
 	if (is_default_nodemap(nodemap))
 		GOTO(out_putref, rc = -EINVAL);
+	if (!allow_op_on_nm(nodemap))
+		GOTO(out_putref, rc = -EPERM);
 
 	if (nodemap->nm_offset_start_uid) {
 		/* nodemap has already offset  */
@@ -2099,6 +2125,8 @@ int nodemap_del_offset(const char *nodemap_name)
 
 	if (is_default_nodemap(nodemap))
 		GOTO(out_putref, rc = -EINVAL);
+	if (!allow_op_on_nm(nodemap))
+		GOTO(out_putref, rc = -EPERM);
 
 	rc = nodemap_del_offset_helper(nodemap);
 	if (rc == 0)

lustre/tests/sanity-sec.sh
@@ -2360,35 +2360,44 @@ test_27aa() { #LU-17922
 run_test 27aa "test nodemap idmap range"
 
 test_27ab() { #LU-18109
-	local idmap
+	local offset_start=100000
+	local offset_limit=200000
+	local nid=1.1.1.1@tcp777
+	local activedefault
+	local nm1=Test18109
+	local nm2=OffsetTest
+	local squash=65534
+	local id_start=500
+	local expected
 	local id=500
+	local idmap
 	local offset
 
 	(( MDS1_VERSION > $(version_code 2.16.50.170) )) ||
 		skip "need MDS > 2.16.50.170 for nodemap range offset"
 
-	do_facet mgs $LCTL nodemap_add Test18109 ||
-		error "unable to add Test18109 as nodemap"
-	stack_trap "do_facet mgs $LCTL nodemap_del Test18109 || true"
+	do_facet mgs $LCTL nodemap_add $nm1 ||
+		error "unable to add $nm1 as nodemap"
+	stack_trap "do_facet mgs $LCTL nodemap_del $nm1 || true"
 
-	do_facet mgs $LCTL nodemap_add OffsetTest ||
-		error "unable to add OffsetTest as nodemap"
-	stack_trap "do_facet mgs $LCTL nodemap_del OffsetTest || true"
+	do_facet mgs $LCTL nodemap_add $nm2 ||
+		error "unable to add $nm2 as nodemap"
+	stack_trap "do_facet mgs $LCTL nodemap_del $nm2 || true"
 
-	do_facet mgs $LCTL nodemap_add_offset --name Test18109 \
-		--offset 100000 --limit 200000 ||
-			error "cannot set offset 100000-299999 for Test18109"
+	do_facet mgs $LCTL nodemap_add_offset --name $nm1 \
+		--offset $offset_start --limit $offset_limit ||
+			error "cannot set offset $offset_start-$((offset_start+offset_limit-1)) for $nm1"
 
 	#expected error, invalid offset range supplied
-	do_facet mgs $LCTL nodemap_add_offset --name OffsetTest \
-		--offset 150000 --limit 100000 &&
-			error "cannot set offset 150000-249999 for OffsetTest"
+	do_facet mgs $LCTL nodemap_add_offset --name $nm2 \
+		--offset $((offset_start+50000)) --limit 100000 &&
+			error "setting offset $((offset_start+50000))-249999 on $nm2 should fail"
 
-	do_facet mgs $LCTL nodemap_add_idmap --name Test18109 \
+	do_facet mgs $LCTL nodemap_add_idmap --name $nm1 \
 		 --idtype uid --idmap 500-509:0-9 ||
 		 error "unable to add idmap range 500-509:0-9"
 
-	idmap=$(do_facet mgs $LCTL get_param nodemap.Test18109.idmap |
+	idmap=$(do_facet mgs $LCTL get_param nodemap.$nm1.idmap |
 		grep idtype)
 	while IFS= read -r idmap; do
 		if (( $id <= 509 )); then
@@ -2398,51 +2407,194 @@ test_27ab() { #LU-18109
 		((id++))
 	done < <(echo "$idmap")
 
-	do_facet mgs $LCTL nodemap_del_idmap --name Test18109 \
+	do_facet mgs $LCTL nodemap_add_range --name $nm1 --range $nid ||
+		error "Add range $nid to $nm1 failed"
+	do_facet mgs $LCTL nodemap_modify --name $nm1 \
+		--property admin --value 1 ||
+		error "Setting admin=1 on $nm1 failed"
+	do_facet mgs $LCTL nodemap_modify --name $nm1 \
+		--property trusted --value 1 ||
+		error "Setting trusted=1 on $nm1 failed"
+	do_facet mgs $LCTL nodemap_modify --name $nm1 \
+		--property squash_uid --value $squash ||
+		error "Setting squash_uid=$squash on $nm1 failed"
+	do_facet mgs $LCTL nodemap_modify --name $nm1 \
+		--property squash_gid --value $squash ||
+		error "Setting squash_gid=$squash on $nm1 failed"
+
+	activedefault=$(do_facet mgs $LCTL get_param -n nodemap.active)
+	if ((activedefault != 1)); then
+		do_facet mgs $LCTL nodemap_activate 1
+		wait_nm_sync active
+		stack_trap cleanup_active EXIT
+	fi
+
+	if (( MDS1_VERSION >= $(version_code 2.16.51.45) )); then
+		# with admin=1, we expect root to be offset
+		id=0
+		expected=$offset_start
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+		# with trusted=1, we expect ids to be offset
+		id=$((id_start+1))
+		expected=$((offset_start+id_start+1))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property trusted --value 0 ||
+			error "Setting trusted=0 on $nm1 failed"
+
+		# with trusted=0, we expect uid to be mapped+offset,
+		# gid to be squashed+offset
+		expected=$((offset_start+1))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		expected=$((offset_start+squash))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property admin --value 0 ||
+			error "Setting admin=0 on $nm1 failed"
+
+		# with admin=0, we expect root to be squashed+offset
+		id=0
+		expected=$((offset_start+squash))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property admin --value 1 ||
+			error "Setting admin=1 on $nm1 failed"
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property trusted --value 1 ||
+			error "Setting trusted=1 on $nm1 failed"
+	fi
+
+	do_facet mgs $LCTL nodemap_del_idmap --name $nm1 \
 		 --idtype uid --idmap 500-509:0 ||
 			error "cannot delete idmap range 500-509:0"
 
 	#expected error, invalid secondary range supplied
-	do_facet mgs $LCTL nodemap_add --name Test18109 \
+	do_facet mgs $LCTL nodemap_add --name $nm1 \
 		 --idtype uid --idmap 500-509:200000-200010 &&
 		 error "Invalid range 200000-200010 was supplied"
 
-	(( $(do_facet mgs $LCTL get_param nodemap.Test18109.idmap |
+	(( $(do_facet mgs $LCTL get_param nodemap.$nm1.idmap |
 		grep -c idtype) == 0 )) ||
 		error "invalid range 200000-200010 supplied and passed"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep start_uid)
-	[[ "$offset" == *"start_uid: 100000"* ]] ||
-		error "expected start_uid of 100000 not found before remounting"
+	[[ "$offset" == *"start_uid: $offset_start"* ]] ||
+		error "expected start_uid of $offset_start not found before remounting"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep limit_uid)
-	[[ "$offset" == *"limit_uid: 200000"* ]] ||
-		error "expected limit_uid of 200000 not found before remounting"
+	[[ "$offset" == *"limit_uid: $offset_limit"* ]] ||
+		error "expected limit_uid of $offset_limit not found before remounting"
+
+	if (( MDS1_VERSION >= $(version_code 2.16.51.45) )); then
+		# with admin=1, we expect root to be offset
+		id=0
+		expected=$offset_start
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+		# with trusted=1, we expect ids to be offset
+		id=$((id_start+1))
+		expected=$((offset_start+id_start+1))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property trusted --value 0 ||
+			error "Setting trusted=0 on $nm1 failed"
+
+		# with trusted=0, we expect uid to be squashed+offset
+		expected=$((offset_start+squash))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+
+		do_facet mgs $LCTL nodemap_modify --name $nm1 \
+			--property admin --value 0 ||
+			error "Setting admin=0 on $nm1 failed"
+
+		# with admin=0, we expect root to be squashed+offset
+		id=0
+		expected=$((offset_start+squash))
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype uid --id $id)
+		((idmap == expected)) ||
+			error "uid $id should be mapped to $expected"
+		idmap=$(do_facet mgs $LCTL nodemap_test_id --nid $nid \
+			--idtype gid --id $id)
+		((idmap == expected)) ||
+			error "gid $id should be mapped to $expected"
+	fi
 
 	stopall || error "failed to unmount servers"
 	setupall || error "failed to remount servers"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep start_uid)
-	[[ "$offset" == *"start_uid: 100000"* ]] ||
-		error "expected start_uid of 100000 not found after remounting"
+	[[ "$offset" == *"start_uid: $offset_start"* ]] ||
+		error "expected start_uid of $offset_start not found after remounting"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep limit_uid)
-	[[ "$offset" == *"limit_uid: 200000"* ]] ||
-		error "expected limit_uid of 200000 not found after remounting"
+	[[ "$offset" == *"limit_uid: $offset_limit"* ]] ||
+		error "expected limit_uid of $offset_limit not found after remounting"
 
-	do_facet mgs $LCTL nodemap_del_offset --name Test18109 ||
-		error "cannot del offset from Test18109"
+	do_facet mgs $LCTL nodemap_del_offset --name $nm1 ||
+		error "cannot del offset from $nm1"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep start_uid)
 	[[ "$offset" == *"start_uid: 0"* ]] ||
 		error "expected start_uid 0, found $offset"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep limit_uid)
 	[[ "$offset" == *"limit_uid: 0"* ]] ||
 		error "expected limit_uid 0, found $offset"
@@ -2450,21 +2602,21 @@ test_27ab() { #LU-18109
 	stopall || error "failed to unmount servers"
 	setupall || error "failed to remount servers"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep start_uid)
 	[[ "$offset" == *"start_uid: 0"* ]] ||
 		error "expected start_uid 0, found $offset after remounting"
 
-	offset=$(do_facet mgs $LCTL get_param nodemap.Test18109.offset |
+	offset=$(do_facet mgs $LCTL get_param nodemap.$nm1.offset |
 		 grep limit_uid)
 	[[ "$offset" == *"limit_uid: 0"* ]] ||
 		error "expected limit_uid 0, found $offset after remounting"
 
-	do_facet mgs $LCTL nodemap_del Test18109 ||
-		error "failed to remove nodemap Test18109"
+	do_facet mgs $LCTL nodemap_del $nm1 ||
+		error "failed to remove nodemap $nm1"
 
-	do_facet mgs $LCTL nodemap_del OffsetTest ||
-		error "failed to remove nodemap OffsetTest"
+	do_facet mgs $LCTL nodemap_del $nm2 ||
+		error "failed to remove nodemap $nm2"
 }
 run_test 27ab "test nodemap idmap offset"
 