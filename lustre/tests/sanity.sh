lustre/lustre-release

commit 2e2b16c28bcf4048ba4f34129b7fb91c36b55a71

LU-11025 dne: support directory restripe
This patch adds directory restripe support:
* 'lfs setdirstripe -m -1 -c <stripe_count>' on an existed directory
  will change this directory layout, if 'stripe_count' is larger than
  current count, new stripes are allocated after current stripes,
  otherwise merge stripes of this directory, NB, if stripe count is
  unchanged, but hash type changed, it's treated as merging, but
  rehashing actually.
* mdt_restripe() ia added to restripe directory.
* mdd_dir_declare_layout_split() is added to split directory, which
  handles both plain and striped directory split.
* lod_dir_declare_layout_split() will handle the internal of directory
  split.
* directory merge is simple compared to split, which just records
  target stripe count in LMV, and update it.

NB. this patch only restripe directory, but doesn't add the code to
migrate sub files, which will be implemented in the following patch.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I526f7423b909eb83cf8723e65981d713b3e42499
Reviewed-on: https://review.whamcloud.com/36898
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_object.c
@@ -1560,13 +1560,13 @@ static int lod_xattr_get(const struct lu_env *env, struct dt_object *dt,
 			/* The on-disk LMV EA only contains header, but the
 			 * returned LMV EA size should contain the space for
 			 * the FIDs of all shards of the striped directory. */
-			if (lmv_is_sane(lmv1))
+			if (le32_to_cpu(lmv1->lmv_magic) == LMV_MAGIC_V1)
 				rc = lmv_mds_md_size(
-					le32_to_cpu(lmv1->lmv_stripe_count),
-					le32_to_cpu(lmv1->lmv_magic));
+						le32_to_cpu(lmv1->lmv_stripe_count),
+						le32_to_cpu(lmv1->lmv_magic));
 		} else {
-			lfm = buf->lb_buf;
-			if (le32_to_cpu(lfm->lfm_magic) == LMV_MAGIC_FOREIGN)
+			lmv1 = buf->lb_buf;
+			if (le32_to_cpu(lmv1->lmv_magic) != LMV_MAGIC_V1)
 				RETURN(rc);
 
 			if (rc != sizeof(*lmv1))
@@ -1712,7 +1712,8 @@ static int lod_prep_lmv_md(const struct lu_env *env, struct dt_object *dt,
 	lmm1->lmv_magic = cpu_to_le32(LMV_MAGIC);
 	lmm1->lmv_stripe_count = cpu_to_le32(stripe_count);
 	lmm1->lmv_hash_type = cpu_to_le32(lo->ldo_dir_hash_type);
-	if (lo->ldo_dir_hash_type & LMV_HASH_FLAG_MIGRATION) {
+	lmm1->lmv_layout_version = cpu_to_le32(lo->ldo_dir_layout_version);
+	if (lod_is_layout_changing(lo)) {
 		lmm1->lmv_migrate_hash = cpu_to_le32(lo->ldo_dir_migrate_hash);
 		lmm1->lmv_migrate_offset =
 			cpu_to_le32(lo->ldo_dir_migrate_offset);
@@ -1818,6 +1819,8 @@ out:
 	lo->ldo_dir_stripe_count = le32_to_cpu(lmv1->lmv_stripe_count);
 	lo->ldo_dir_stripes_allocated = le32_to_cpu(lmv1->lmv_stripe_count);
 	lo->ldo_dir_layout_version = le32_to_cpu(lmv1->lmv_layout_version);
+	lo->ldo_dir_migrate_offset = le32_to_cpu(lmv1->lmv_migrate_offset);
+	lo->ldo_dir_migrate_hash = le32_to_cpu(lmv1->lmv_migrate_hash);
 	lo->ldo_dir_hash_type = le32_to_cpu(lmv1->lmv_hash_type);
 	if (rc != 0)
 		lod_striping_free_nolock(env, lo);
@@ -1889,31 +1892,74 @@ static int lod_dir_declare_create_stripes(const struct lu_env *env,
 		if (!dto)
 			continue;
 
-		rc = lod_sub_declare_create(env, dto, attr, NULL, dof, th);
-		if (rc != 0)
-			GOTO(out, rc);
+		/* directory split skip create for existing stripes */
+		if (!(lod_is_splitting(lo) && i < lo->ldo_dir_split_offset)) {
+			rc = lod_sub_declare_create(env, dto, attr, NULL, dof,
+						    th);
+			if (rc != 0)
+				GOTO(out, rc);
 
-		if (!dt_try_as_dir(env, dto))
-			GOTO(out, rc = -EINVAL);
+			if (!dt_try_as_dir(env, dto))
+				GOTO(out, rc = -EINVAL);
 
-		rc = lod_sub_declare_ref_add(env, dto, th);
-		if (rc != 0)
-			GOTO(out, rc);
+			rc = lod_sub_declare_ref_add(env, dto, th);
+			if (rc != 0)
+				GOTO(out, rc);
 
-		rec->rec_fid = lu_object_fid(&dto->do_lu);
-		rc = lod_sub_declare_insert(env, dto,
-					    (const struct dt_rec *)rec,
-					    (const struct dt_key *)dot, th);
-		if (rc != 0)
-			GOTO(out, rc);
+			rec->rec_fid = lu_object_fid(&dto->do_lu);
+			rc = lod_sub_declare_insert(env, dto,
+						    (const struct dt_rec *)rec,
+						    (const struct dt_key *)dot,
+						    th);
+			if (rc != 0)
+				GOTO(out, rc);
 
-		/* master stripe FID will be put to .. */
-		rec->rec_fid = lu_object_fid(&dt->do_lu);
-		rc = lod_sub_declare_insert(env, dto,
-					    (const struct dt_rec *)rec,
-					    (const struct dt_key *)dotdot, th);
-		if (rc != 0)
-			GOTO(out, rc);
+			/* master stripe FID will be put to .. */
+			rec->rec_fid = lu_object_fid(&dt->do_lu);
+			rc = lod_sub_declare_insert(env, dto,
+						  (const struct dt_rec *)rec,
+						  (const struct dt_key *)dotdot,
+						  th);
+			if (rc != 0)
+				GOTO(out, rc);
+
+			if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_BAD_SLAVE_NAME) &&
+			    cfs_fail_val == i)
+				snprintf(stripe_name, sizeof(info->lti_key),
+					 DFID":%u",
+					 PFID(lu_object_fid(&dto->do_lu)),
+					 i + 1);
+			else
+				snprintf(stripe_name, sizeof(info->lti_key),
+					 DFID":%u",
+					 PFID(lu_object_fid(&dto->do_lu)), i);
+
+			sname = lod_name_get(env, stripe_name,
+					     strlen(stripe_name));
+			rc = linkea_links_new(&ldata, &info->lti_linkea_buf,
+					      sname, lu_object_fid(&dt->do_lu));
+			if (rc != 0)
+				GOTO(out, rc);
+
+			linkea_buf.lb_buf = ldata.ld_buf->lb_buf;
+			linkea_buf.lb_len = ldata.ld_leh->leh_len;
+			rc = lod_sub_declare_xattr_set(env, dto, &linkea_buf,
+						       XATTR_NAME_LINK, 0, th);
+			if (rc != 0)
+				GOTO(out, rc);
+
+			rec->rec_fid = lu_object_fid(&dto->do_lu);
+			rc = lod_sub_declare_insert(env, dt_object_child(dt),
+					(const struct dt_rec *)rec,
+					(const struct dt_key *)stripe_name, th);
+			if (rc != 0)
+				GOTO(out, rc);
+
+			rc = lod_sub_declare_ref_add(env, dt_object_child(dt),
+						     th);
+			if (rc != 0)
+				GOTO(out, rc);
+		}
 
 		if (!OBD_FAIL_CHECK(OBD_FAIL_LFSCK_LOST_SLAVE_LMV) ||
 		    cfs_fail_val != i) {
@@ -1929,39 +1975,6 @@ static int lod_dir_declare_create_stripes(const struct lu_env *env,
 			if (rc != 0)
 				GOTO(out, rc);
 		}
-
-		if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_BAD_SLAVE_NAME) &&
-		    cfs_fail_val == i)
-			snprintf(stripe_name, sizeof(info->lti_key), DFID":%u",
-				PFID(lu_object_fid(&dto->do_lu)), i + 1);
-		else
-			snprintf(stripe_name, sizeof(info->lti_key), DFID":%u",
-				PFID(lu_object_fid(&dto->do_lu)), i);
-
-		sname = lod_name_get(env, stripe_name, strlen(stripe_name));
-		rc = linkea_links_new(&ldata, &info->lti_linkea_buf,
-				      sname, lu_object_fid(&dt->do_lu));
-		if (rc != 0)
-			GOTO(out, rc);
-
-		linkea_buf.lb_buf = ldata.ld_buf->lb_buf;
-		linkea_buf.lb_len = ldata.ld_leh->leh_len;
-		rc = lod_sub_declare_xattr_set(env, dto, &linkea_buf,
-					       XATTR_NAME_LINK, 0, th);
-		if (rc != 0)
-			GOTO(out, rc);
-
-		rec->rec_fid = lu_object_fid(&dto->do_lu);
-		rc = lod_sub_declare_insert(env, dt_object_child(dt),
-					    (const struct dt_rec *)rec,
-					    (const struct dt_key *)stripe_name,
-					    th);
-		if (rc != 0)
-			GOTO(out, rc);
-
-		rc = lod_sub_declare_ref_add(env, dt_object_child(dt), th);
-		if (rc != 0)
-			GOTO(out, rc);
 	}
 
 	rc = lod_sub_declare_xattr_set(env, dt_object_child(dt),
@@ -2335,6 +2348,7 @@ static int lod_dir_layout_set(const struct lu_env *env,
 {
 	struct dt_object *next = dt_object_child(dt);
 	struct lod_object *lo = lod_dt_obj(dt);
+	struct lod_device *lod = lu2lod_dev(lod2lu_obj(lo)->lo_dev);
 	struct lmv_mds_md_v1 *lmv = buf->lb_buf;
 	struct lmv_mds_md_v1 *slave_lmv;
 	struct lu_buf slave_buf;
@@ -2343,9 +2357,28 @@ static int lod_dir_layout_set(const struct lu_env *env,
 
 	ENTRY;
 
+	if (!lmv_is_sane2(lmv))
+		RETURN(-EINVAL);
+
+	/* adjust hash for dir merge, which may not be set in user command */
+	if (lmv_is_merging(lmv) && !lmv->lmv_migrate_hash)
+		lmv->lmv_merge_hash =
+			lod->lod_mdt_descs.ltd_lmv_desc.ld_pattern;
+
+	LMV_DEBUG(D_INFO, lmv, "set");
+
 	rc = lod_sub_xattr_set(env, next, buf, XATTR_NAME_LMV, fl, th);
 	if (rc)
 		RETURN(rc);
+
+	/* directory restripe may update stripe LMV directly */
+	if (!lo->ldo_dir_stripe_count)
+		RETURN(0);
+
+	lo->ldo_dir_hash_type = le32_to_cpu(lmv->lmv_hash_type);
+	lo->ldo_dir_migrate_offset = le32_to_cpu(lmv->lmv_migrate_offset);
+	lo->ldo_dir_migrate_hash = le32_to_cpu(lmv->lmv_migrate_hash);
+	lo->ldo_dir_layout_version = le32_to_cpu(lmv->lmv_layout_version);
 
 	OBD_ALLOC_PTR(slave_lmv);
 	if (!slave_lmv)
@@ -2368,7 +2401,6 @@ static int lod_dir_layout_set(const struct lu_env *env,
 			break;
 	}
 
-	lod_striping_free(env, lod_dt_obj(dt));
 	OBD_FREE_PTR(slave_lmv);
 
 	RETURN(rc);
@@ -3743,9 +3775,15 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 		if (i && OBD_FAIL_CHECK(OBD_FAIL_MDS_STRIPE_CREATE))
 			continue;
 
-		/* if it's source stripe of migrating directory, don't create */
-		if (!((lo->ldo_dir_hash_type & LMV_HASH_FLAG_MIGRATION) &&
-		      i >= lo->ldo_dir_migrate_offset)) {
+		/* don't create stripe if:
+		 * 1. it's source stripe of migrating directory
+		 * 2. it's existed stripe of splitting directory
+		 */
+		if ((lod_is_migrating(lo) && i >= lo->ldo_dir_migrate_offset) ||
+		    (lod_is_splitting(lo) && i < lo->ldo_dir_split_offset)) {
+			if (!dt_object_exists(dto))
+				GOTO(out, rc = -EINVAL);
+		} else {
 			dt_write_lock(env, dto, DT_TGT_CHILD);
 			rc = lod_sub_create(env, dto, attr, NULL, dof, th);
 			if (rc != 0) {
@@ -3766,12 +3804,6 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 				GOTO(out, rc);
 		}
 
-		rec->rec_fid = lu_object_fid(&dt->do_lu);
-		rc = lod_sub_insert(env, dto, (struct dt_rec *)rec,
-				    (const struct dt_key *)dotdot, th);
-		if (rc != 0)
-			GOTO(out, rc);
-
 		if (!OBD_FAIL_CHECK(OBD_FAIL_LFSCK_LOST_SLAVE_LMV) ||
 		    cfs_fail_val != i) {
 			if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_BAD_SLAVE_LMV) &&
@@ -3787,6 +3819,21 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 			if (rc != 0)
 				GOTO(out, rc);
 		}
+
+		/* don't insert stripe if it's existed stripe of splitting
+		 * directory (this directory is striped).
+		 * NB, plain directory will insert itself as the first
+		 * stripe in target.
+		 */
+		if (lod_is_splitting(lo) && lo->ldo_dir_split_offset > 1 &&
+		    lo->ldo_dir_split_offset > i)
+			continue;
+
+		rec->rec_fid = lu_object_fid(&dt->do_lu);
+		rc = lod_sub_insert(env, dto, (struct dt_rec *)rec,
+				    (const struct dt_key *)dotdot, th);
+		if (rc != 0)
+			GOTO(out, rc);
 
 		if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_BAD_SLAVE_NAME) &&
 		    cfs_fail_val == i)
@@ -7727,7 +7774,12 @@ static int lod_dir_declare_layout_attach(const struct lu_env *env,
 	lo->ldo_dir_migrate_hash = le32_to_cpu(lmv->lmv_hash_type);
 	lo->ldo_dir_stripe_count += stripe_count;
 	lo->ldo_dir_stripes_allocated += stripe_count;
-	lo->ldo_dir_hash_type |= LMV_HASH_FLAG_MIGRATION;
+
+	/* plain directory split creates target as a plain directory, while
+	 * after source attached as the first stripe, it becomes a striped
+	 * directory, set correct do_index_ops, otherwise it can't be unlinked.
+	 */
+	dt->do_index_ops = &lod_striped_index_ops;
 
 	RETURN(0);
 out:
@@ -7904,6 +7956,86 @@ static int lod_dir_declare_layout_shrink(const struct lu_env *env,
 	rc = lod_sub_declare_xattr_set(env, next, lmv_buf, XATTR_NAME_LMV,
 				       LU_XATTR_REPLACE, th);
 	return rc;
+}
+
+/**
+ * Allocate stripes for split directory.
+ *
+ * \param[in] env	execution environment
+ * \param[in] dt	target object
+ * \param[in] mlc	layout change data
+ * \param[in] th	transaction handle
+ *
+ * \retval		0 on success
+ * \retval		negative if failed
+ */
+static int lod_dir_declare_layout_split(const struct lu_env *env,
+					struct dt_object *dt,
+					const struct md_layout_change *mlc,
+					struct thandle *th)
+{
+	struct lod_thread_info *info = lod_env_info(env);
+	struct lod_device *lod = lu2lod_dev(dt->do_lu.lo_dev);
+	struct lod_object *lo = lod_dt_obj(dt);
+	struct dt_object_format *dof = &info->lti_format;
+	struct lmv_user_md_v1 *lum = mlc->mlc_spec->u.sp_ea.eadata;
+	struct dt_object **stripes;
+	u32 stripe_count;
+	u32 saved_count;
+	int i;
+	int rc;
+
+	ENTRY;
+
+	LASSERT(le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC);
+	LASSERT(le32_to_cpu(lum->lum_stripe_offset) == LMV_OFFSET_DEFAULT);
+
+	saved_count = lo->ldo_dir_stripes_allocated;
+	stripe_count = le32_to_cpu(lum->lum_stripe_count);
+	if (stripe_count <= saved_count)
+		RETURN(-EINVAL);
+
+	dof->dof_type = DFT_DIR;
+
+	OBD_ALLOC(stripes, sizeof(*stripes) * stripe_count);
+	if (!stripes)
+		RETURN(-ENOMEM);
+
+	for (i = 0; i < lo->ldo_dir_stripes_allocated; i++)
+		stripes[i] = lo->ldo_stripe[i];
+
+	lod_qos_statfs_update(env, lod, &lod->lod_mdt_descs);
+	rc = lod_mdt_alloc_qos(env, lo, stripes, saved_count, stripe_count);
+	if (rc == -EAGAIN)
+		rc = lod_mdt_alloc_rr(env, lo, stripes, saved_count,
+				      stripe_count);
+	if (rc < 0) {
+		OBD_FREE(stripes, sizeof(*stripes) * stripe_count);
+		RETURN(rc);
+	}
+
+	LASSERT(rc > saved_count);
+	OBD_FREE(lo->ldo_stripe,
+		 sizeof(*stripes) * lo->ldo_dir_stripes_allocated);
+	lo->ldo_stripe = stripes;
+	lo->ldo_dir_striped = 1;
+	lo->ldo_dir_stripe_count = rc;
+	lo->ldo_dir_stripes_allocated = stripe_count;
+	lo->ldo_dir_split_hash = lo->ldo_dir_hash_type;
+	lo->ldo_dir_hash_type = le32_to_cpu(lum->lum_hash_type);
+	if (!lmv_is_known_hash_type(lo->ldo_dir_hash_type))
+		lo->ldo_dir_hash_type =
+			lod->lod_mdt_descs.ltd_lmv_desc.ld_pattern;
+	lo->ldo_dir_hash_type |= LMV_HASH_FLAG_SPLIT | LMV_HASH_FLAG_MIGRATION;
+	lo->ldo_dir_split_offset = saved_count;
+	lo->ldo_dir_layout_version++;
+	lo->ldo_dir_stripe_loaded = 1;
+
+	rc = lod_dir_declare_create_stripes(env, dt, mlc->mlc_attr, dof, th);
+	if (rc)
+		lod_striping_free(env, lo);
+
+	RETURN(rc);
 }
 
 /*
@@ -8093,6 +8225,7 @@ static mlc_handler dir_mlc_declare_ops[MD_LAYOUT_MAX] = {
 	[MD_LAYOUT_ATTACH] = lod_dir_declare_layout_attach,
 	[MD_LAYOUT_DETACH] = lod_dir_declare_layout_detach,
 	[MD_LAYOUT_SHRINK] = lod_dir_declare_layout_shrink,
+	[MD_LAYOUT_SPLIT]  = lod_dir_declare_layout_split,
 };
 
 static mlc_handler dir_mlc_ops[MD_LAYOUT_MAX] = {

lustre/include/lustre_lmv.h
@@ -67,7 +67,7 @@ static inline bool lmv_dir_foreign(const struct lmv_stripe_md *lsm)
 static inline bool lmv_dir_layout_changing(const struct lmv_stripe_md *lsm)
 {
 	return lmv_dir_striped(lsm) &&
-	       (lsm->lsm_md_hash_type & LMV_HASH_FLAG_LAYOUT_CHANGE);
+	       lmv_hash_is_layout_changing(lsm->lsm_md_hash_type);
 }
 
 static inline bool lmv_dir_bad_hash(const struct lmv_stripe_md *lsm)
@@ -275,6 +275,15 @@ lmv_hash_crush(unsigned int count, const char *name, int namelen)
 	return idx;
 }
 
+/* directory layout may change in three ways:
+ * 1. directory migration, in its LMV source stripes are appended after
+ *    target stripes, \a migrate_hash is source hash type, \a migrate_offset is
+ *    target stripe count,
+ * 2. directory split, \a migrate_hash is hash type before split,
+ *    \a migrate_offset is stripe count before split.
+ * 3. directory merge, \a migrate_hash is hash type after merge,
+ *    \a migrate_offset is stripe count after merge.
+ */
 static inline int
 __lmv_name_to_stripe_index(__u32 hash_type, __u32 stripe_count,
 			   __u32 migrate_hash, __u32 migrate_offset,
@@ -287,7 +296,17 @@ __lmv_name_to_stripe_index(__u32 hash_type, __u32 stripe_count,
 	LASSERT(namelen > 0);
 	LASSERT(stripe_count > 0);
 
-	if (hash_type & LMV_HASH_FLAG_MIGRATION) {
+	if (lmv_hash_is_splitting(hash_type)) {
+		if (!new_layout) {
+			hash_type = migrate_hash;
+			stripe_count = migrate_offset;
+		}
+	} else if (lmv_hash_is_merging(hash_type)) {
+		if (new_layout) {
+			hash_type = migrate_hash;
+			stripe_count = migrate_offset;
+		}
+	} else if (lmv_hash_is_migrating(hash_type)) {
 		if (new_layout) {
 			stripe_count = migrate_offset;
 		} else {
@@ -317,12 +336,12 @@ __lmv_name_to_stripe_index(__u32 hash_type, __u32 stripe_count,
 
 	LASSERT(stripe_index < stripe_count);
 
-	if ((saved_hash & LMV_HASH_FLAG_MIGRATION) && !new_layout)
+	if (!new_layout && lmv_hash_is_migrating(saved_hash))
 		stripe_index += migrate_offset;
 
 	LASSERT(stripe_index < saved_count);
 
-	CDEBUG(D_INFO, "name %.*s hash %#x/%#x idx %d/%u/%u under %s layout\n",
+	CDEBUG(D_INFO, "name %.*s hash=%#x/%#x idx=%d/%u/%u under %s layout\n",
 	       namelen, name, saved_hash, migrate_hash, stripe_index,
 	       saved_count, migrate_offset, new_layout ? "new" : "old");
 
@@ -380,21 +399,80 @@ static inline bool lmv_user_magic_supported(__u32 lum_magic)
 	       lum_magic == LMV_MAGIC_FOREIGN;
 }
 
+/* master LMV is sane */
 static inline bool lmv_is_sane(const struct lmv_mds_md_v1 *lmv)
 {
+	if (!lmv)
+		return false;
+
 	if (le32_to_cpu(lmv->lmv_magic) != LMV_MAGIC_V1)
 		goto insane;
 
 	if (le32_to_cpu(lmv->lmv_stripe_count) == 0)
 		goto insane;
 
-	if (!lmv_is_known_hash_type(lmv->lmv_hash_type))
+	if (!lmv_is_known_hash_type(le32_to_cpu(lmv->lmv_hash_type)))
 		goto insane;
 
 	return true;
 insane:
 	LMV_DEBUG(D_ERROR, lmv, "insane");
 	return false;
+}
+
+/* LMV can be either master or stripe LMV */
+static inline bool lmv_is_sane2(const struct lmv_mds_md_v1 *lmv)
+{
+	if (!lmv)
+		return false;
+
+	if (le32_to_cpu(lmv->lmv_magic) != LMV_MAGIC_V1 &&
+	    le32_to_cpu(lmv->lmv_magic) != LMV_MAGIC_STRIPE)
+		goto insane;
+
+	if (le32_to_cpu(lmv->lmv_stripe_count) == 0)
+		goto insane;
+
+	if (!lmv_is_known_hash_type(le32_to_cpu(lmv->lmv_hash_type)))
+		goto insane;
+
+	return true;
+insane:
+	LMV_DEBUG(D_ERROR, lmv, "insane");
+	return false;
+}
+
+static inline bool lmv_is_splitting(const struct lmv_mds_md_v1 *lmv)
+{
+	LASSERT(lmv_is_sane2(lmv));
+	return lmv_hash_is_splitting(cpu_to_le32(lmv->lmv_hash_type));
+}
+
+static inline bool lmv_is_merging(const struct lmv_mds_md_v1 *lmv)
+{
+	LASSERT(lmv_is_sane2(lmv));
+	return lmv_hash_is_merging(cpu_to_le32(lmv->lmv_hash_type));
+}
+
+static inline bool lmv_is_migrating(const struct lmv_mds_md_v1 *lmv)
+{
+	LASSERT(lmv_is_sane(lmv));
+	return lmv_hash_is_migrating(cpu_to_le32(lmv->lmv_hash_type));
+}
+
+static inline bool lmv_is_restriping(const struct lmv_mds_md_v1 *lmv)
+{
+	LASSERT(lmv_is_sane2(lmv));
+	return lmv_hash_is_splitting(cpu_to_le32(lmv->lmv_hash_type)) ||
+	       lmv_hash_is_merging(cpu_to_le32(lmv->lmv_hash_type));
+}
+
+static inline bool lmv_is_layout_changing(const struct lmv_mds_md_v1 *lmv)
+{
+	LASSERT(lmv_is_sane2(lmv));
+	return lmv_hash_is_splitting(cpu_to_le32(lmv->lmv_hash_type)) ||
+	       lmv_hash_is_merging(cpu_to_le32(lmv->lmv_hash_type)) ||
+	       lmv_hash_is_migrating(cpu_to_le32(lmv->lmv_hash_type));
 }
 
 #endif

lustre/include/md_object.h
@@ -168,7 +168,8 @@ struct md_op_spec {
 		     sp_cr_lookup:1, /* do lookup sanity check or not. */
 		     sp_rm_entry:1,  /* only remove name entry */
 		     sp_permitted:1, /* do not check permission */
-		     sp_migrate_close:1; /* close the file during migrate */
+		     sp_migrate_close:1, /* close the file during migrate */
+		     sp_migrate_nsonly:1; /* migrate dirent only */
 	/** Current lock mode for parent dir where create is performing. */
 	mdl_mode_t sp_cr_mode;
 
@@ -181,9 +182,10 @@ enum md_layout_opc {
 	MD_LAYOUT_WRITE,	/* FLR: write the file */
 	MD_LAYOUT_RESYNC,	/* FLR: resync starts */
 	MD_LAYOUT_RESYNC_DONE,	/* FLR: resync done */
-	MD_LAYOUT_ATTACH,	/* attach stripes to target dir */
-	MD_LAYOUT_DETACH,	/* detach stripes from dir */
-	MD_LAYOUT_SHRINK,	/* shrink stripes (check empty and destroy) */
+	MD_LAYOUT_ATTACH,	/* attach stripes */
+	MD_LAYOUT_DETACH,	/* detach stripes */
+	MD_LAYOUT_SHRINK,	/* shrink striped directory (destroy stripes) */
+	MD_LAYOUT_SPLIT,	/* split directory (allocate new stripes) */
 	MD_LAYOUT_MAX,
 };
 
@@ -191,13 +193,24 @@ enum md_layout_opc {
  * Parameters for layout change API.
  */
 struct md_layout_change {
-	enum md_layout_opc	 mlc_opc;
-	__u16			 mlc_mirror_id;
-	struct layout_intent	*mlc_intent;
-	struct lu_buf		 mlc_buf;
-	struct lustre_som_attrs	 mlc_som;
-	size_t			 mlc_resync_count;
-	__u32			*mlc_resync_ids;
+	enum md_layout_opc			 mlc_opc;
+	struct lu_buf				 mlc_buf;
+	union {
+		struct {
+			__u16			 mlc_mirror_id;
+			struct layout_intent	*mlc_intent;
+			struct lustre_som_attrs	 mlc_som;
+			size_t			 mlc_resync_count;
+			__u32			*mlc_resync_ids;
+		}; /* file */
+		struct {
+			struct md_object	*mlc_parent;	/* parent obj in plain dir split */
+			struct md_object	*mlc_target;	/* target obj in plain dir split */
+			struct lu_attr		*mlc_attr;	/* target attr in plain dir split */
+			const struct lu_name	*mlc_name;	/* target name in plain dir split */
+			struct md_op_spec	*mlc_spec;	/* dir split spec */
+		}; /* dir */
+	};
 };
 
 union ldlm_policy_data;

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2171,11 +2171,18 @@ struct lmv_mds_md_v1 {
 };
 
 #define LMV_DEBUG(mask, lmv, msg)					\
-	CDEBUG(mask, "%s LMV: magic %#x count %u index %u hash %#x version %u migrate offset %u migrate hash %u.\n",	\
+	CDEBUG(mask, "%s LMV: magic=%#x count=%u index=%u hash=%#x version=%u migrate offset=%u migrate hash=%u.\n",	\
 	       msg, (lmv)->lmv_magic, (lmv)->lmv_stripe_count,		\
 	       (lmv)->lmv_master_mdt_index, (lmv)->lmv_hash_type,	\
 	       (lmv)->lmv_layout_version, (lmv)->lmv_migrate_offset,	\
 	       (lmv)->lmv_migrate_hash)
+
+/* stripe count before directory split */
+#define lmv_split_offset	lmv_migrate_offset
+/* stripe count after directory merge */
+#define lmv_merge_offset	lmv_migrate_offset
+/* directory hash type after merge */
+#define lmv_merge_hash		lmv_migrate_hash
 
 /* foreign LMV EA */
 struct lmv_foreign_md {

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1014,6 +1014,9 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 	       (type & LMV_HASH_TYPE_MASK) == LMV_HASH_TYPE_CRUSH;
 }
 
+#define LMV_HASH_FLAG_MERGE		0x04000000
+#define LMV_HASH_FLAG_SPLIT		0x08000000
+
 /* The striped directory has ever lost its master LMV EA, then LFSCK
  * re-generated it. This flag is used to indicate such case. It is an
  * on-disk flag. */
@@ -1022,7 +1025,39 @@ static inline bool lmv_is_known_hash_type(__u32 type)
 #define LMV_HASH_FLAG_BAD_TYPE		0x20000000
 #define LMV_HASH_FLAG_MIGRATION		0x80000000
 
-#define LMV_HASH_FLAG_LAYOUT_CHANGE	LMV_HASH_FLAG_MIGRATION
+#define LMV_HASH_FLAG_LAYOUT_CHANGE	\
+	(LMV_HASH_FLAG_MIGRATION | LMV_HASH_FLAG_SPLIT | LMV_HASH_FLAG_MERGE)
+
+/* both SPLIT and MIGRATION are set for directory split */
+static inline bool lmv_hash_is_splitting(__u32 hash)
+{
+	return (hash & LMV_HASH_FLAG_LAYOUT_CHANGE) ==
+	       (LMV_HASH_FLAG_SPLIT | LMV_HASH_FLAG_MIGRATION);
+}
+
+/* both MERGE and MIGRATION are set for directory merge */
+static inline bool lmv_hash_is_merging(__u32 hash)
+{
+	return (hash & LMV_HASH_FLAG_LAYOUT_CHANGE) ==
+	       (LMV_HASH_FLAG_MERGE | LMV_HASH_FLAG_MIGRATION);
+}
+
+/* only MIGRATION is set for directory migration */
+static inline bool lmv_hash_is_migrating(__u32 hash)
+{
+	return (hash & LMV_HASH_FLAG_LAYOUT_CHANGE) == LMV_HASH_FLAG_MIGRATION;
+}
+
+static inline bool lmv_hash_is_restriping(__u32 hash)
+{
+	return lmv_hash_is_splitting(hash) || lmv_hash_is_merging(hash);
+}
+
+static inline bool lmv_hash_is_layout_changing(__u32 hash)
+{
+	return lmv_hash_is_splitting(hash) || lmv_hash_is_merging(hash) ||
+	       lmv_hash_is_migrating(hash);
+}
 
 extern char *mdt_hash_name[LMV_HASH_TYPE_MAX];
 

lustre/lfsck/lfsck_striped_dir.c
@@ -979,7 +979,7 @@ static inline bool lfsck_name_hash_match(struct lmv_mds_md_v1 *lmv,
 	if (idx == lmv->lmv_master_mdt_index)
 		return true;
 
-	if (!(lmv->lmv_hash_type & LMV_HASH_FLAG_LAYOUT_CHANGE))
+	if (!lmv_hash_is_layout_changing(lmv->lmv_hash_type))
 		return false;
 
 	idx = lmv_name_to_stripe_index(lmv, name, namelen);

commit 103c1f560c8c8dde601aac1fafed137b19fa3429

LU-18639 dne: a correct check for dir split
Use the actual dir stripe count while performing
a dir split sanity check in lod_dir_declare_dir_split().

Fix lod_object_lock() to work with a striped dir with
only one stripe correctly.

Improve sanity test_230p by adding a dir split right
after the dir merges.

Also fix a typo in lustre/doc/lfs-migrate.1 .

Fixes: 2e2b16c28b ("LU-11025 dne: support directory restripe")
Fixes: 392f558f40 ("LU-17810 dne: dir restripe without fixed hash flag")
HPE-bug-id: LUS-12701
Test-Parameters: envdefinitions=ONLY=230p fstype=ldiskfs mdtcount=2 mdscount=2 testlist=sanity
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I8d8501fd09f89d03ccb1ea92a8562326110ecc24
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57784
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/doc/lfs-migrate.1
@@ -155,7 +155,7 @@ and
 .RB ' "lfs getstripe -d" '.
 .TP
 .BR --clear-fixed
-Do not set LMV_HASH_FIXED_FLAG upon migrating the directory. It allows a striped dir
+Do not set LMV_HASH_FLAG_FIXED upon migrating the directory. It allows a striped dir
 with only one stripe to be converted to a plain directory.
 .TP
 .BR -H ", " --mdt-hash=\fIHASH_TYPE\fR

lustre/lod/lod_object.c
@@ -4297,8 +4297,8 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 	struct dt_insert_rec *rec = &info->lti_dt_rec;
 	int i;
 	int rc;
-
 	ENTRY;
+
 	/* lum is used to know whether it's replay */
 	LASSERT(lum);
 	if (!S_ISDIR(dt->do_lu.lo_header->loh_attr))
@@ -4451,6 +4451,12 @@ static int lod_xattr_set_lmv(const struct lu_env *env, struct dt_object *dt,
 		rc = lod_sub_insert(env, dt_object_child(dt),
 				    (const struct dt_rec *)rec,
 				    (const struct dt_key *)stripe_name, th);
+		if (rc == -EEXIST) {
+			CDEBUG(D_INFO, DFID": can't insert stripe %i "DFID"\n",
+			     PFID(lod_object_fid(lo)), i,
+			     PFID(lu_object_fid(&dt->do_lu)));
+			continue;
+		}
 		if (rc != 0)
 			GOTO(out, rc);
 
@@ -6866,7 +6872,7 @@ static int lod_object_lock(const struct lu_env *env,
 		RETURN(rc);
 
 	/* No stripes */
-	if (lo->ldo_dir_stripe_count <= 1)
+	if (lo->ldo_dir_stripe_count == 0)
 		RETURN(0);
 
 	slave_locks_size = offsetof(typeof(*slave_locks),
@@ -8969,7 +8975,7 @@ static int lod_dir_declare_layout_split(const struct lu_env *env,
 	LASSERT(le32_to_cpu(lum->lum_magic) == LMV_USER_MAGIC);
 	LASSERT(le32_to_cpu(lum->lum_stripe_offset) == LMV_OFFSET_DEFAULT);
 
-	saved_count = lo->ldo_dir_stripes_allocated;
+	saved_count = lo->ldo_dir_stripe_count;
 	stripe_count = le32_to_cpu(lum->lum_stripe_count);
 
 	/* if the split target is overstriped, we need to put that flag in the
@@ -8999,7 +9005,7 @@ static int lod_dir_declare_layout_split(const struct lu_env *env,
 	if (!stripes)
 		RETURN(-ENOMEM);
 
-	for (i = 0; i < lo->ldo_dir_stripes_allocated; i++)
+	for (i = 0; i < saved_count; i++)
 		stripes[i] = lo->ldo_stripe[i];
 
 	lod_qos_statfs_update(env, lod, &lod->lod_mdt_descs);

lustre/tests/sanity.sh
@@ -24153,7 +24153,7 @@ test_230p() {
 	createmany -d $DIR/$tdir/d 100 ||
 		error "create dirs under remote dir failed"
 
-	for c in $(seq $((MDSCOUNT - 1)) -1 1); do
+	for ((c = $MDSCOUNT - 1; c >= 1; c--)); do
 		local mdt_hash="crush"
 
 		do_nodes $mdts "$LCTL set_param mdt.*.md_stats=clear >/dev/null"
@@ -24174,6 +24174,16 @@ test_230p() {
 		(( delta < 200 / c + 4 )) ||
 			error "$delta files migrated >= $((200 / c + 4))"
 	done
+
+	# and restripe to -c 2
+	echo Splitting the dir back to 2 stripes.
+        $LFS setdirstripe -c 2 $DIR/$tdir || {
+		$LFS getdirstripe $DIR/$tdir
+                error "split $tdir to 2 stripes failed"
+	}
+        wait_update $HOSTNAME \
+                "$LFS getdirstripe -H $DIR/$tdir" "crush" $timeout ||
+                error "dir split not finished"
 }
 run_test 230p "dir merge"
 