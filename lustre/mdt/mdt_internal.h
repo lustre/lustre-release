lustre/lustre-release

commit 26b8238659974959780cd49de92595b4b0bdf89f

LU-3105 osd: remove capa related stuff from servers
capability feature is broken. it's not going to be used
anytime soon. the related tests are removed as well.

Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Change-Id: I865a92b57abaae679d7ff8319e0e3fda603beff9
Reviewed-on: http://review.whamcloud.com/5572
Tested-by: Jenkins
Reviewed-by: Mike Pershin <mike.pershin@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/mdt/mdt_internal.h
@@ -200,15 +200,6 @@ struct mdt_device {
 
         struct upcall_cache        *mdt_identity_cache;
 
-	/* capability keys */
-	unsigned long              mdt_capa_timeout;
-	__u32                      mdt_capa_alg;
-	struct dt_object          *mdt_ck_obj;
-	unsigned long              mdt_ck_timeout;
-	unsigned long              mdt_ck_expiry;
-	struct timer_list          mdt_ck_timer;
-	struct ptlrpc_thread       mdt_ck_thread;
-	struct lustre_capa_key     mdt_capa_keys[2];
 	unsigned int               mdt_capa_conf:1,
 				   /* Enable remote dir on non-MDT0 */
 				   mdt_enable_remote_dir:1;
@@ -429,7 +420,6 @@ struct mdt_thread_info {
 	loff_t                     mti_off;
 	struct lu_buf              mti_buf;
 	struct lu_buf              mti_big_buf;
-	struct lustre_capa_key     mti_capa_key;
 
         /* Ops object filename */
         struct lu_name             mti_name;
@@ -1023,15 +1013,6 @@ void mdt_rename_counter_tally(struct mdt_thread_info *info,
 			      struct mdt_device *mdt,
 			      struct ptlrpc_request *req,
 			      struct mdt_object *src, struct mdt_object *tgt);
-
-/* Capability */
-int mdt_ck_thread_start(struct mdt_device *mdt);
-void mdt_ck_thread_stop(struct mdt_device *mdt);
-void mdt_ck_timer_callback(unsigned long castmeharder);
-int mdt_capa_keys_init(const struct lu_env *env, struct mdt_device *mdt);
-void mdt_set_capainfo(struct mdt_thread_info *info, int offset,
-		      const struct lu_fid *fid, struct lustre_capa *capa);
-void mdt_dump_capainfo(struct mdt_thread_info *info);
 
 static inline struct obd_device *mdt2obd_dev(const struct mdt_device *mdt)
 {

lustre/mdt/mdt_recovery.c
@@ -123,9 +123,6 @@ void mdt_fs_cleanup(const struct lu_env *env, struct mdt_device *mdt)
 
         /* Remove transaction callback */
         dt_txn_callback_del(mdt->mdt_bottom, &mdt->mdt_txn_cb);
-        if (mdt->mdt_ck_obj)
-                lu_object_put(env, &mdt->mdt_ck_obj->do_lu);
-        mdt->mdt_ck_obj = NULL;
         EXIT;
 }
 

commit efbcbf618ea4ed75a82ed48cd0be586701f2391a

LU-18753 mdt: remove mdt_buf code
Remove unused MDT buffer code, including mdt_buf()
and mdt_buf_const().

Fixes: 26b823865997 ("LU-3105 osd: remove capa related stuff from servers")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I7c0ffc820e94886902a1dbf09e01d70761f7d8fd
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58228
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: Max Wang <wamax@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_internal.h
@@ -944,10 +944,6 @@ int find_name_matching_hash(struct mdt_thread_info *info, struct lu_name *lname,
 			   struct mdt_object *parent, struct mdt_object *child);
 int mdt_device_sync(const struct lu_env *env, struct mdt_device *mdt);
 
-struct lu_buf *mdt_buf(const struct lu_env *env, void *area, ssize_t len);
-const struct lu_buf *mdt_buf_const(const struct lu_env *env,
-				   const void *area, ssize_t len);
-
 void mdt_dump_lmm(int level, const struct lov_mds_md *lmm, __u64 valid);
 void mdt_dump_lmv(unsigned int level, const union lmv_mds_md *lmv);
 

lustre/mdt/mdt_recovery.c
@@ -20,32 +20,6 @@
 
 #include "mdt_internal.h"
 
-struct lu_buf *mdt_buf(const struct lu_env *env, void *area, ssize_t len)
-{
-	struct lu_buf *buf;
-	struct mdt_thread_info *mti;
-
-	mti = lu_context_key_get(&env->le_ctx, &mdt_thread_key);
-	buf = &mti->mti_buf;
-	buf->lb_buf = area;
-	buf->lb_len = len;
-	return buf;
-}
-
-const struct lu_buf *mdt_buf_const(const struct lu_env *env,
-				   const void *area, ssize_t len)
-{
-	struct lu_buf *buf;
-	struct mdt_thread_info *mti;
-
-	mti = lu_context_key_get(&env->le_ctx, &mdt_thread_key);
-	buf = &mti->mti_buf;
-
-	buf->lb_buf = (void *)area;
-	buf->lb_len = len;
-	return buf;
-}
-
 /* reconstruction code */
 static void mdt_steal_ack_locks(struct ptlrpc_request *req)
 {