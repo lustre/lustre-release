lustre/lustre-release

commit 0ea23e019454e03810d532758afa52aa054995d8

LU-13307 nodemap: have nodemap_add_member support large NIDs
Currently when mounting lustre using IPv6 address fails with

Lustre: 27361:0:(nodemap_handler.c:395:nodemap_add_member())
  lustre-MDT0000: error adding to nodemap, no valid NIDs found
LustreError: 11-0: lustre-MDT0000-osp-MDT0003:
  operation mds_connect to node 0@lo failed: rc = -22

This was due to no nodemap being set so the ptlrpc layer was not
seeing any new peers. Adding minimal support to nodemap allows
mounting.

Change-Id: If9cfe88ec92afc3f14788f3f3ded8387a1b5d8c7
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53135
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nodemap_range.c
@@ -40,8 +40,8 @@
  * controlled to prevent read access during update operations.
  */
 
-#define START(node)	((node)->rn_start)
-#define LAST(node)	((node)->rn_end)
+#define START(node)	(lnet_nid_to_nid4(&((node)->rn_start)))
+#define LAST(node)	(lnet_nid_to_nid4(&((node)->rn_end)))
 
 INTERVAL_TREE_DEFINE(struct lu_nid_range, rn_rb, lnet_nid_t, rn_subtree_last,
 		     START, LAST, static, nm_range)
@@ -54,15 +54,30 @@ INTERVAL_TREE_DEFINE(struct lu_nid_range, rn_rb, lnet_nid_t, rn_subtree_last,
  * \param	nodemap		nodemap that contains this range
  * \retval	lu_nid_range on success, NULL on failure
  */
-struct lu_nid_range *range_create(struct nodemap_range_tree *nm_range_tree,
-				  lnet_nid_t start_nid, lnet_nid_t end_nid,
-				  struct lu_nodemap *nodemap, unsigned range_id)
+struct lu_nid_range *range_create(struct nodemap_config *config,
+				  const struct lnet_nid *start_nid,
+				  const struct lnet_nid *end_nid,
+				  u8 netmask, struct lu_nodemap *nodemap,
+				  unsigned int range_id)
 {
+	struct nodemap_range_tree *nm_range_tree;
 	struct lu_nid_range *range;
 
-	if (LNET_NIDNET(start_nid) != LNET_NIDNET(end_nid) ||
-	    LNET_NIDADDR(start_nid) > LNET_NIDADDR(end_nid))
+	if (LNET_NID_NET(start_nid) != LNET_NID_NET(end_nid))
 		return NULL;
+
+	if (!netmask) {
+		lnet_nid_t nid4[2] = {
+			lnet_nid_to_nid4(start_nid),
+			lnet_nid_to_nid4(end_nid)
+		};
+
+		if (LNET_NIDADDR(nid4[0]) > LNET_NIDADDR(nid4[1]))
+			return NULL;
+	} else if (!nid_same(start_nid, end_nid)) {
+		/* FIXME Currently we only support one large NID per nodemap */
+		return NULL;
+	}
 
 	OBD_ALLOC_PTR(range);
 	if (range == NULL) {
@@ -72,6 +87,7 @@ struct lu_nid_range *range_create(struct nodemap_range_tree *nm_range_tree,
 	}
 
 	/* if we are loading from save, use on disk id num */
+	nm_range_tree = &config->nmc_range_tree;
 	if (range_id != 0) {
 		if (nm_range_tree->nmrt_range_highest_id < range_id)
 			nm_range_tree->nmrt_range_highest_id = range_id;
@@ -82,8 +98,9 @@ struct lu_nid_range *range_create(struct nodemap_range_tree *nm_range_tree,
 	}
 	range->rn_nodemap = nodemap;
 
-	range->rn_start = start_nid;
-	range->rn_end = end_nid;
+	range->rn_netmask = netmask;
+	range->rn_start = *start_nid;
+	range->rn_end = *end_nid;
 
 	INIT_LIST_HEAD(&range->rn_list);
 
@@ -97,17 +114,44 @@ struct lu_nid_range *range_create(struct nodemap_range_tree *nm_range_tree,
  * \param	end_nid			ending nid
  * \retval	matching range or NULL
  */
-struct lu_nid_range *range_find(struct nodemap_range_tree *nm_range_tree,
-				lnet_nid_t start_nid, lnet_nid_t end_nid)
+struct lu_nid_range *range_find(struct nodemap_config *config,
+				const struct lnet_nid *start_nid,
+				const struct lnet_nid *end_nid,
+				u8 netmask)
 {
-	struct lu_nid_range *range;
+	struct lu_nid_range *range = NULL;
 
-	range = nm_range_iter_first(
-		&nm_range_tree->nmrt_range_interval_root, start_nid, end_nid);
-	while (range &&
-	       (range->rn_start != start_nid || range->rn_end != end_nid))
-		range = nm_range_iter_next(range, start_nid, end_nid);
+	if (!netmask) {
+		struct nodemap_range_tree *nm_range_tree;
+		lnet_nid_t nid4[2];
 
+		if (!nid_is_nid4(start_nid) || !nid_is_nid4(end_nid))
+			return NULL;
+
+		nid4[0] = lnet_nid_to_nid4(start_nid);
+		nid4[1] = lnet_nid_to_nid4(end_nid);
+		nm_range_tree = &config->nmc_range_tree;
+		range = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+					    nid4[0], nid4[1]);
+		while (range &&
+		       (!nid_same(&range->rn_start, start_nid) ||
+			!nid_same(&range->rn_end, end_nid)))
+			range = nm_range_iter_next(range, nid4[0], nid4[1]);
+	} else if (!list_empty(&config->nmc_netmask_setup)) {
+		struct lu_nid_range *range_temp;
+
+		/* FIXME. We scan the config for large NIDs. Each range
+		 * only contains one large NID for now.
+		 */
+		list_for_each_entry_safe(range, range_temp,
+					 &config->nmc_netmask_setup,
+					 rn_collect) {
+			if (nid_same(&range->rn_start, start_nid) &&
+			    range->rn_netmask == netmask)
+				break;
+			range = NULL;
+		}
+	}
 	return range;
 }
 
@@ -131,16 +175,22 @@ void range_destroy(struct lu_nid_range *range)
  * does not overlap so that each nid can belong
  * to exactly one range
  */
-int range_insert(struct nodemap_range_tree *nm_range_tree,
-		 struct lu_nid_range *range)
+int range_insert(struct nodemap_config *config, struct lu_nid_range *range)
 {
-	if (nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
-				range->rn_start,
-				range->rn_end))
-		return -EEXIST;
+	if (!range->rn_netmask) {
+		struct nodemap_range_tree *nm_range_tree;
 
-	nm_range_insert(range, &nm_range_tree->nmrt_range_interval_root);
+		nm_range_tree = &config->nmc_range_tree;
+		if (nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+					lnet_nid_to_nid4(&range->rn_start),
+					lnet_nid_to_nid4(&range->rn_end)))
+			return -EEXIST;
 
+		nm_range_insert(range,
+				&nm_range_tree->nmrt_range_interval_root);
+	} else {
+		list_add(&range->rn_collect, &config->nmc_netmask_setup);
+	}
 	return 0;
 }
 
@@ -150,11 +200,18 @@ int range_insert(struct nodemap_range_tree *nm_range_tree,
  *
  * \param	range		range to remove
  */
-void range_delete(struct nodemap_range_tree *nm_range_tree,
-		  struct lu_nid_range *range)
+void range_delete(struct nodemap_config *config, struct lu_nid_range *range)
 {
 	list_del(&range->rn_list);
-	nm_range_remove(range, &nm_range_tree->nmrt_range_interval_root);
+	if (!range->rn_netmask) {
+		struct nodemap_range_tree *nm_range_tree;
+
+		nm_range_tree = &config->nmc_range_tree;
+		nm_range_remove(range,
+				&nm_range_tree->nmrt_range_interval_root);
+	} else {
+		list_del(&range->rn_collect);
+	}
 	range_destroy(range);
 }
 
@@ -163,9 +220,32 @@ void range_delete(struct nodemap_range_tree *nm_range_tree,
  *
  * \param	nid		nid to search for
  */
-struct lu_nid_range *range_search(struct nodemap_range_tree *nm_range_tree,
-				  lnet_nid_t nid)
+struct lu_nid_range *range_search(struct nodemap_config *config,
+				  struct lnet_nid *nid)
 {
-	return nm_range_iter_first(
-		&nm_range_tree->nmrt_range_interval_root, nid, nid);
+	struct lu_nid_range *range = NULL;
+
+	if (nid_is_nid4(nid)) {
+		struct nodemap_range_tree *nm_range_tree;
+
+		nm_range_tree = &config->nmc_range_tree;
+		range = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+					    lnet_nid_to_nid4(nid),
+					    lnet_nid_to_nid4(nid));
+	} else if (!list_empty(&config->nmc_netmask_setup)) {
+		struct lu_nid_range *range_temp;
+
+		/* FIXME. We scan the config for the large NIDs. Each range
+		 * only contains one large NID for now.
+		 */
+		list_for_each_entry_safe(range, range_temp,
+					 &config->nmc_netmask_setup,
+					 rn_collect) {
+			if (nid_same(&range->rn_start, nid))
+				break;
+			range = NULL;
+		}
+	}
+
+	return range;
 }

lustre/include/lustre_nodemap.h
@@ -127,12 +127,15 @@ struct nm_config_file {
 void nodemap_activate(const bool value);
 int nodemap_add(const char *nodemap_name);
 int nodemap_del(const char *nodemap_name);
-int nodemap_add_member(lnet_nid_t nid, struct obd_export *exp);
+int nodemap_add_member(struct lnet_nid *nid, struct obd_export *exp);
 void nodemap_del_member(struct obd_export *exp);
-int nodemap_parse_range(const char *range_string, lnet_nid_t range[2]);
+int nodemap_parse_range(const char *range_string, struct lnet_nid range[2],
+			u8 *netmask);
 int nodemap_parse_idmap(char *idmap_string, __u32 idmap[2]);
-int nodemap_add_range(const char *name, const lnet_nid_t nid[2]);
-int nodemap_del_range(const char *name, const lnet_nid_t nid[2]);
+int nodemap_add_range(const char *name, const struct lnet_nid nid[2],
+		      u8 netmask);
+int nodemap_del_range(const char *name, const struct lnet_nid nid[2],
+		      u8 netmask);
 int nodemap_set_allow_root(const char *name, bool allow_root);
 int nodemap_set_trust_client_ids(const char *name, bool trust_client_ids);
 int nodemap_set_deny_unknown(const char *name, bool deny_unknown);
@@ -160,12 +163,12 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 ssize_t nodemap_map_acl(struct lu_nodemap *nodemap, void *buf, size_t size,
 			enum nodemap_tree_type tree_type);
 #ifdef HAVE_SERVER_SUPPORT
-void nodemap_test_nid(lnet_nid_t nid, char *name_buf, size_t name_len);
+void nodemap_test_nid(struct lnet_nid *nid, char *name_buf, size_t name_len);
 #else
 #define nodemap_test_nid(nid, name_buf, name_len) do {} while(0)
 #endif
-int nodemap_test_id(lnet_nid_t nid, enum nodemap_id_type idtype,
-		    __u32 client_id, __u32 *fs_id);
+int nodemap_test_id(struct lnet_nid *nid, enum nodemap_id_type idtype,
+		    u32 client_id, u32 *fs_id);
 
 struct nm_config_file *nm_config_file_register_mgs(const struct lu_env *env,
 						   struct dt_object *obj,
@@ -197,6 +200,9 @@ struct nodemap_config {
 
 	/* Pointer to default nodemap as it is needed more often */
 	struct lu_nodemap *nmc_default_nodemap;
+
+	/* list of netmask + address prefix */
+	struct list_head nmc_netmask_setup;
 
 	/**
 	 * Lock required to access the range tree.

lustre/include/uapi/linux/lustre/lustre_disk.h
@@ -252,6 +252,7 @@ enum nodemap_idx_type {
 	NODEMAP_UIDMAP_IDX = 3,		/* uid map assigned to a nm cluster */
 	NODEMAP_GIDMAP_IDX = 4,		/* gid map assigned to a nm cluster */
 	NODEMAP_PROJIDMAP_IDX = 5,	/* projid map assigned to nm cluster */
+	NODEMAP_NID_MASK_IDX = 6,	/* large NID setup for a nm cluster */
 	NODEMAP_GLOBAL_IDX = 15,	/* stores nodemap activation status */
 };
 
@@ -302,6 +303,15 @@ struct nodemap_range_rec {
 	__u64		nrr_padding2;
 };
 
+struct nodemap_range2_rec {
+	struct lnet_nid	nrr_nid_prefix;
+	__u32		nrr_padding1; /* these fields may be used if */
+	__u32		nrr_padding2; /* nrr_nid_prefix.nid_size > 12 */
+	__u16		nrr_padding3;
+	__u8		nrr_padding4;
+	__u8		nrr_netmask;
+};
+
 struct nodemap_id_rec {
 	__u32	nir_id_fs;
 	__u32	nir_padding1;
@@ -330,6 +340,7 @@ struct nodemap_cluster_roles_rec {
 union nodemap_rec {
 	struct nodemap_cluster_rec ncr;
 	struct nodemap_range_rec nrr;
+	struct nodemap_range2_rec nrr2;
 	struct nodemap_id_rec nir;
 	struct nodemap_global_rec ngr;
 	struct nodemap_cluster_roles_rec ncrr;

lustre/mdt/mdt_handler.c
@@ -7087,11 +7087,9 @@ static int mdt_obd_connect(const struct lu_env *env,
 	lexp = class_conn2export(&conn);
 	LASSERT(lexp != NULL);
 
-	if (nid_is_nid4(client_nid)) {
-		rc = nodemap_add_member(lnet_nid_to_nid4(client_nid), lexp);
-		if (rc != 0 && rc != -EEXIST)
-			GOTO(out, rc);
-	}
+	rc = nodemap_add_member(client_nid, lexp);
+	if (rc != 0 && rc != -EEXIST)
+		GOTO(out, rc);
 
 	rc = mdt_connect_internal(env, lexp, mdt, data, false);
 	if (rc == 0) {
@@ -7135,11 +7133,9 @@ static int mdt_obd_reconnect(const struct lu_env *env,
 	if (exp == NULL || obd == NULL || cluuid == NULL)
 		RETURN(-EINVAL);
 
-	if (nid_is_nid4(client_nid)) {
-		rc = nodemap_add_member(lnet_nid_to_nid4(client_nid), exp);
-		if (rc != 0 && rc != -EEXIST)
-			RETURN(rc);
-	}
+	rc = nodemap_add_member(client_nid, exp);
+	if (rc != 0 && rc != -EEXIST)
+		RETURN(rc);
 
 	rc = mdt_connect_internal(env, exp, mdt_dev(obd->obd_lu_dev), data,
 				  true);

lustre/mgs/mgs_handler.c
@@ -772,7 +772,7 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 {
 	struct lustre_cfg	*lcfg = NULL;
 	struct fs_db		*fsdb;
-	lnet_nid_t		nid;
+	struct lnet_nid	nid;
 	const char		*nodemap_name = NULL;
 	const char		*nidstr = NULL;
 	const char		*client_idstr = NULL;
@@ -827,8 +827,11 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 		if (lcfg->lcfg_bufcount != 2)
 			GOTO(out_lcfg, rc = -EINVAL);
 		nidstr = lustre_cfg_string(lcfg, 1);
-		nid = libcfs_str2nid(nidstr);
-		nodemap_test_nid(nid, name_buf, sizeof(name_buf));
+		rc = libcfs_strnid(&nid, nidstr);
+		if (rc < 0)
+			GOTO(out_lcfg, rc);
+
+		nodemap_test_nid(&nid, name_buf, sizeof(name_buf));
 		rc = copy_to_user(data->ioc_pbuf1, name_buf,
 				  min_t(size_t, data->ioc_plen1,
 					sizeof(name_buf)));
@@ -842,7 +845,10 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 		idtype_str = lustre_cfg_string(lcfg, 2);
 		client_idstr = lustre_cfg_string(lcfg, 3);
 
-		nid = libcfs_str2nid(nidstr);
+		rc = libcfs_strnid(&nid, nidstr);
+		if (rc < 0)
+			GOTO(out_lcfg, rc);
+
 		if (strcmp(idtype_str, "uid") == 0)
 			idtype = NODEMAP_UID;
 		else if (strcmp(idtype_str, "gid") == 0)
@@ -856,7 +862,7 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 		if (rc != 0)
 			GOTO(out_lcfg, rc = -EINVAL);
 
-		rc = nodemap_test_id(nid, idtype, client_id, &fs_id);
+		rc = nodemap_test_id(&nid, idtype, client_id, &fs_id);
 		if (rc < 0)
 			GOTO(out_lcfg, rc = -EINVAL);
 

lustre/mgs/mgs_llog.c
@@ -5605,9 +5605,10 @@ int mgs_nodemap_cmd(const struct lu_env *env, struct mgs_device *mgs,
 		    enum lcfg_command_type cmd, const char *nodemap_name,
 		    char *param)
 {
-	lnet_nid_t nid[2];
+	struct lnet_nid nid[2];
 	u32 idmap[2];
 	bool bool_switch;
+	u8 netmask = 0;
 	u32 int_id;
 	int rc = 0;
 
@@ -5620,16 +5621,16 @@ int mgs_nodemap_cmd(const struct lu_env *env, struct mgs_device *mgs,
 		rc = nodemap_del(nodemap_name);
 		break;
 	case LCFG_NODEMAP_ADD_RANGE:
-		rc = nodemap_parse_range(param, nid);
+		rc = nodemap_parse_range(param, nid, &netmask);
 		if (rc != 0)
 			break;
-		rc = nodemap_add_range(nodemap_name, nid);
+		rc = nodemap_add_range(nodemap_name, nid, netmask);
 		break;
 	case LCFG_NODEMAP_DEL_RANGE:
-		rc = nodemap_parse_range(param, nid);
+		rc = nodemap_parse_range(param, nid, &netmask);
 		if (rc != 0)
 			break;
-		rc = nodemap_del_range(nodemap_name, nid);
+		rc = nodemap_del_range(nodemap_name, nid, netmask);
 		break;
 	case LCFG_NODEMAP_ADMIN:
 		rc = kstrtobool(param, &bool_switch);

lustre/ofd/ofd_obd.c
@@ -309,11 +309,9 @@ static int ofd_obd_reconnect(const struct lu_env *env, struct obd_export *exp,
 	if (!exp || !obd || !cluuid)
 		RETURN(-EINVAL);
 
-	if (nid_is_nid4(client_nid)) {
-		rc = nodemap_add_member(lnet_nid_to_nid4(client_nid), exp);
-		if (rc != 0 && rc != -EEXIST)
-			RETURN(rc);
-	}
+	rc = nodemap_add_member(client_nid, exp);
+	if (rc != 0 && rc != -EEXIST)
+		RETURN(rc);
 
 	ofd = ofd_dev(obd->obd_lu_dev);
 
@@ -367,8 +365,8 @@ static int ofd_obd_connect(const struct lu_env *env, struct obd_export **_exp,
 	exp = class_conn2export(&conn);
 	LASSERT(exp != NULL);
 
-	if (client_nid && nid_is_nid4(client_nid)) {
-		rc = nodemap_add_member(lnet_nid_to_nid4(client_nid), exp);
+	if (client_nid) {
+		rc = nodemap_add_member(client_nid, exp);
 		if (rc != 0 && rc != -EEXIST)
 			GOTO(out, rc);
 	} else {

lustre/ptlrpc/gss/gss_svc_upcall.c
@@ -802,7 +802,7 @@ int gss_svc_upcall_handle_init(struct ptlrpc_request *req,
 	 */
 	LNetPrimaryNID(&req->rq_source.nid);
 	rsi.si_nid4 = lnet_nid_to_nid4(&req->rq_source.nid);
-	nodemap_test_nid(lnet_nid_to_nid4(&req->rq_peer.nid), rsi.si_nm_name,
+	nodemap_test_nid(&req->rq_peer.nid, rsi.si_nm_name,
 			 sizeof(rsi.si_nm_name));
 
 	/* Note that context handle is always 0 for for INIT. */

commit 2d1efde7f4dec9ca28ce4d82cabc9690d465dc75

LU-18785 build: ofd, ptlrpc missing prototypes
nodemap_range.c:271:6: error: no previous prototype
  for '__range_delete' [-Werror=missing-prototypes]
  271 | void __range_delete(struct nodemap_range_tree *nm_range_tree,
      |      ^~~~~~~~~~~~~~

ofd_oss.c:430:5: error: no previous prototype for 'oss_mod_init'
  [-Werror=missing-prototypes]
  430 | int oss_mod_init(void)
      |     ^~~~~~~~~~~~

Test-Parameters: trivial
Fixes: 0ea23e01945 ("LU-13307 nodemap: have nodemap_add_member support large NIDs")
Fixes: b84f014d733 ("LU-14291 ofd: merge ost module into ofd")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ie846dfae7ceb511318ab4ccd9494a633129c2c4d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58342
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/ofd/ofd_oss.c
@@ -21,6 +21,7 @@
 #include <lprocfs_status.h>
 #include <lustre_nodemap.h>
 #include <obd_class.h>
+#include "ofd_internal.h"
 
 #define OSS_SERVICE_WATCHDOG_FACTOR 2
 

lustre/ptlrpc/nodemap_range.c
@@ -268,8 +268,8 @@ int range_insert(struct nodemap_config *config, struct lu_nid_range *range)
  *
  * \param	range		range to remove
  */
-void __range_delete(struct nodemap_range_tree *nm_range_tree,
-		    struct lu_nid_range *range)
+static void __range_delete(struct nodemap_range_tree *nm_range_tree,
+			   struct lu_nid_range *range)
 {
 	nm_range_remove(range,
 			&nm_range_tree->nmrt_range_interval_root);