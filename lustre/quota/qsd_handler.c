lustre/lustre-release

commit 2686838fefabba038c5a8192b84bfb375391ab14

LU-18240 sec: enforce per-nodemap project quota for root
Add a new 'ignore_root_prjquota' rbac role on nodemaps, enabled by
default. When not present, project quota for root is enforced for
clients part of this nodemap.

The tricky part is to find a way to pass this nodemap-related
information to the quota enforcement code. On the data path, this is
put in the transaction handle in a field 'th_ignore_root_proj_quota'
when ofd_trans_create() is called. Then the OSD layer is able to put
it in a field 'lqi_ignore_root_proj_quota' of struct lquota_id_info,
so that it can be checked by the QSD handler.
On the metadata path, it needs one more intermediate step as the MDD
layer does not have access to the client export to retrieve the
nodemap. So a new field 'uc_rbac_ignore_root_prjquota' is added to
struct lu_ucred. It is set in ucred_set_rbac_roles(), and used in
mdd_trans_create() to put the value in the transaction handle.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I71292fb76cbc6476b8ead83bf7a686fd1b03611e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56436
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/qsd_handler.c
@@ -889,7 +889,8 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 	    (qsd->qsd_type_array[qi->lqi_type])->qqi_acct_failed)
 		RETURN(0);
 
-	if (local_flags && qi->lqi_id.qid_projid && qsd->qsd_root_prj_enable)
+	if (local_flags && qi->lqi_id.qid_projid &&
+	    (qsd->qsd_root_prj_enable || !qi->lqi_ignore_root_proj_quota))
 		*local_flags |= QUOTA_FL_ROOT_PRJQUOTA;
 
 	LASSERTF(trans->lqt_id_cnt <= QUOTA_MAX_TRANSIDS, "id_cnt=%d\n",

lustre/doc/lctl-nodemap-modify.8
@@ -99,6 +99,8 @@ Defaults to all, which means all roles are allowed. Other possible values
 it is still possible to lock or unlock encrypted directories,
 as these operations only need read access to fscrypt metadata.
 .br
+- ignore_root_prjquota, so that project quota is not enforced for the root user.
+.br
 - quota_ops, to allow quota modifications.
 .br
 - server_upcall, to define which identity upcall to use. If set, identity upcall

lustre/include/dt_object.h
@@ -2034,7 +2034,9 @@ struct thandle {
 	/* whether ignore quota */
 				th_ignore_quota:1,
 	/* whether restart transaction */
-				th_restart_tran:1;
+				th_restart_tran:1,
+	/* enforce project quota for root */
+				th_ignore_root_proj_quota:1;
 };
 
 /*

lustre/include/lustre_nodemap.h
@@ -33,6 +33,7 @@ static const struct nodemap_rbac_name {
 	{ NODEMAP_RBAC_CHLG_OPS,	"chlg_ops"	},
 	{ NODEMAP_RBAC_FSCRYPT_ADMIN,   "fscrypt_admin"	},
 	{ NODEMAP_RBAC_SERVER_UPCALL,	"server_upcall"	},
+	{ NODEMAP_RBAC_IGN_ROOT_PRJQUOTA,	"ignore_root_prjquota"	},
 };
 
 struct nodemap_pde {

lustre/include/lustre_quota.h
@@ -238,6 +238,8 @@ struct lquota_id_info {
 
 	/* whether we are reporting blocks or inodes */
 	bool			 lqi_is_blk;
+	/* enforce project quota for root */
+	bool			 lqi_ignore_root_proj_quota;
 };
 
 /* With the DoM, both inode quota in meta pool and block quota in data pool

lustre/include/md_object.h
@@ -701,6 +701,7 @@ struct lu_ucred {
 	int			 uc_rbac_chlg_ops:1;
 	int			 uc_rbac_fscrypt_admin:1;
 	int			 uc_rbac_server_upcall:1;
+	int			 uc_rbac_ignore_root_prjquota:1;
 };
 
 struct lu_ucred *lu_ucred(const struct lu_env *env);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3825,13 +3825,15 @@ enum nodemap_rbac_roles {
 	NODEMAP_RBAC_CHLG_OPS		= 0x00000010,
 	NODEMAP_RBAC_FSCRYPT_ADMIN	= 0x00000020,
 	NODEMAP_RBAC_SERVER_UPCALL	= 0x00000040,
+	NODEMAP_RBAC_IGN_ROOT_PRJQUOTA	= 0x00000080,
 	NODEMAP_RBAC_NONE	= (__u32)~(NODEMAP_RBAC_FILE_PERMS	|
 					   NODEMAP_RBAC_DNE_OPS	|
 					   NODEMAP_RBAC_QUOTA_OPS	|
 					   NODEMAP_RBAC_BYFID_OPS	|
 					   NODEMAP_RBAC_CHLG_OPS	|
 					   NODEMAP_RBAC_FSCRYPT_ADMIN	|
-					   NODEMAP_RBAC_SERVER_UPCALL),
+					   NODEMAP_RBAC_SERVER_UPCALL	|
+					   NODEMAP_RBAC_IGN_ROOT_PRJQUOTA),
 	NODEMAP_RBAC_ALL	= 0xFFFFFFFF, /* future caps ON by default */
 };
 

lustre/lod/lod_sub_object.c
@@ -57,6 +57,8 @@ struct thandle *lod_sub_get_thandle(const struct lu_env *env,
 
 	tth = container_of(th, struct top_thandle, tt_super);
 	tth->tt_master_sub_thandle->th_ignore_quota = th->th_ignore_quota;
+	tth->tt_master_sub_thandle->th_ignore_root_proj_quota =
+		th->th_ignore_root_proj_quota;
 
 	/* local object must be mdt object, Note: during ost object
 	 * creation, FID is not assigned until osp_create(),
@@ -87,6 +89,7 @@ struct thandle *lod_sub_get_thandle(const struct lu_env *env,
 	if (IS_ERR(sub_th))
 		RETURN(sub_th);
 	sub_th->th_ignore_quota = th->th_ignore_quota;
+	sub_th->th_ignore_root_proj_quota = th->th_ignore_root_proj_quota;
 
 	if (tth->tt_multiple_thandle != NULL && record_update != NULL &&
 	    th->th_result == 0)

commit 67d4234e5180c2a12371f2072d032f1323936ce1

LU-18720 quota: check for project quota
lqi_ignore_root_proj_quota should be checked only with project quota,
otherwise it's state is undefined and can lead to undefined results.

Fixes: 2686838fef ("LU-18240 sec: enforce per-nodemap project quota for root")
Test-Parameters: testlist=sanity-quota,sanity-quota,sanity-quota
Test-Parameters: fstype=zfs testlist=sanity-quota,sanity-quota,sanity-quota
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I02896639ed375ddd314fa44dc9cddb008b1dbb0b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58066
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>

lustre/quota/qsd_handler.c
@@ -896,7 +896,7 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 	    (qsd->qsd_type_array[qi->lqi_type])->qqi_acct_failed)
 		RETURN(0);
 
-	if (local_flags && qi->lqi_id.qid_projid &&
+	if (qi->lqi_type == PRJQUOTA && local_flags && qi->lqi_id.qid_projid &&
 	    (qsd->qsd_root_prj_enable || !qi->lqi_ignore_root_proj_quota))
 		*local_flags |= QUOTA_FL_ROOT_PRJQUOTA;
 