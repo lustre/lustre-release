lustre/lustre-release

commit 0ba690a526be74c4cdffe7a7dd3031b4bd2b37d8

LU-7251 osp: do not assign commit callback to every thandle
with OSP there is a risk of getting a lot of commit callbacks.
say, 10K unlinks/sec on 4-striped files could result in 4*10K*5
= 200K commit callbacks. this patch implements another schema:
every OSP registers own callback every second. this should result
in 4*5 commit callbacks in the same situation. in case of forced
sync the commit callback is registered unconditionally.

the patch removes th_tags and th_ctx from struct thandle as they
are not used anymore. this elimintates 3 allocations from every
transaction:
(lu_object.c:1714:keys_init()) kmalloced 'ctx->lc_value': 320
(update_records.c:1217:update_key_init()) kmalloced 'value': 408
(osp_dev.c:1807:osp_txn_key_init()) kmalloced 'value': 4

Change-Id: I460d5eccb585b166423d84d5c142af2e27751d8b
Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Reviewed-on: https://review.whamcloud.com/17270
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Lai Siyao <lai.siyao@intel.com>

lustre/osp/osp_dev.c
@@ -797,19 +797,24 @@ static int osp_sync(const struct lu_env *env, struct dt_device *dev)
 	struct osp_device *d = dt2osp_dev(dev);
 	cfs_time_t	   expire;
 	struct l_wait_info lwi = { 0 };
-	unsigned long	   id, old;
-	int		   rc = 0;
-	unsigned long	   start = cfs_time_current();
+	int recs, rc = 0;
+	unsigned long start = cfs_time_current();
+	__u64 old;
+
 	ENTRY;
 
 	/* No Sync between MDTs yet. */
 	if (d->opd_connect_mdt)
 		RETURN(0);
 
+	recs = atomic_read(&d->opd_sync_changes);
+	old = atomic64_read(&d->opd_sync_processed_recs);
+
+	osp_sync_force(env, dt2osp_dev(dev));
+
 	if (unlikely(d->opd_imp_active == 0))
 		RETURN(-ENOTCONN);
 
-	id = d->opd_sync_last_used_id;
 	down_write(&d->opd_async_updates_rwsem);
 
 	CDEBUG(D_OTHER, "%s: async updates %d\n", d->opd_obd->obd_name,
@@ -825,26 +830,23 @@ static int osp_sync(const struct lu_env *env, struct dt_device *dev)
 	if (rc != 0)
 		GOTO(out, rc);
 
-	CDEBUG(D_CACHE, "%s: id: used %lu, processed %llu\n",
-	       d->opd_obd->obd_name, id, d->opd_sync_last_processed_id);
+	CDEBUG(D_CACHE, "%s: processed %lu\n", d->opd_obd->obd_name,
+	       atomic64_read(&d->opd_sync_processed_recs));
 
-	/* wait till all-in-line are processed */
-	while (d->opd_sync_last_processed_id < id) {
-
-		old = d->opd_sync_last_processed_id;
-
+	while (atomic64_read(&d->opd_sync_processed_recs) < old + recs) {
+		__u64 last = atomic64_read(&d->opd_sync_processed_recs);
 		/* make sure the connection is fine */
 		expire = cfs_time_shift(obd_timeout);
 		lwi = LWI_TIMEOUT(expire - cfs_time_current(),
 				  osp_sync_timeout, d);
 		l_wait_event(d->opd_sync_barrier_waitq,
-			     d->opd_sync_last_processed_id >= id,
-			     &lwi);
+			     atomic64_read(&d->opd_sync_processed_recs)
+			     >= old + recs, &lwi);
 
-		if (d->opd_sync_last_processed_id >= id)
+		if (atomic64_read(&d->opd_sync_processed_recs) >= old + recs)
 			break;
 
-		if (d->opd_sync_last_processed_id != old) {
+		if (atomic64_read(&d->opd_sync_processed_recs) != last) {
 			/* some progress have been made,
 			 * keep trying... */
 			continue;
@@ -1344,8 +1346,12 @@ static struct lu_device *osp_device_fini(const struct lu_env *env,
 		osp->opd_async_requests = NULL;
 	}
 
-	if (osp->opd_storage_exp)
+	if (osp->opd_storage_exp) {
+		/* wait for the commit callbacks to complete */
+		wait_event(osp->opd_sync_waitq,
+			  atomic_read(&osp->opd_commits_registered) == 0);
 		obd_disconnect(osp->opd_storage_exp);
+	}
 
 	if (osp->opd_symlink)
 		lprocfs_remove(&osp->opd_symlink);
@@ -1869,7 +1875,7 @@ struct lu_context_key osp_thread_key = {
 LU_KEY_INIT_FINI(osp_txn, struct osp_txn_info);
 
 struct lu_context_key osp_txn_key = {
-	.lct_tags = LCT_OSP_THREAD | LCT_TX_HANDLE,
+	.lct_tags = LCT_OSP_THREAD,
 	.lct_init = osp_txn_key_init,
 	.lct_fini = osp_txn_key_fini
 };

lustre/osp/osp_internal.h
@@ -224,19 +224,20 @@ struct osp_device {
 	/* osd api's commit cb control structure */
 	struct dt_txn_callback		 opd_sync_txn_cb;
 	/* last used change number -- semantically similar to transno */
-	__u64				 opd_sync_last_used_id;
+	unsigned long			 opd_sync_last_used_id;
 	/* last committed change number -- semantically similar to
 	 * last_committed */
 	__u64				 opd_sync_last_committed_id;
-	/* last processed (taken from llog) id */
-	volatile __u64			 opd_sync_last_processed_id;
 	/* last processed catalog index */
-	int				 opd_sync_last_catalog_idx;
-	struct osp_id_tracker		*opd_sync_tracker;
-	struct list_head		 opd_sync_ontrack;
+	int                              opd_sync_last_catalog_idx;
+	/* number of processed records */
+	atomic64_t			 opd_sync_processed_recs;
 	/* stop processing new requests until barrier=0 */
 	atomic_t			 opd_sync_barrier;
 	wait_queue_head_t		 opd_sync_barrier_waitq;
+	/* last generated id */
+	cfs_time_t			 opd_sync_next_commit_cb;
+	atomic_t			 opd_commits_registered;
 
 	/*
 	 * statfs related fields: OSP maintains it on its own
@@ -814,6 +815,11 @@ int osp_sync_add(const struct lu_env *env, struct osp_object *o,
 int osp_sync_init(const struct lu_env *env, struct osp_device *d);
 int osp_sync_fini(struct osp_device *d);
 void osp_sync_check_for_work(struct osp_device *osp);
+void osp_sync_force(const struct lu_env *env, struct osp_device *d);
+int osp_sync_add_commit_cb(const struct lu_env *env, struct osp_device *d,
+			   struct thandle *th);
+int osp_sync_add_commit_cb_1s(const struct lu_env *env, struct osp_device *d,
+			      struct thandle *th);
 
 /* lwp_dev.c */
 extern struct obd_ops lwp_obd_device_ops;

lustre/include/dt_object.h
@@ -1885,11 +1885,6 @@ struct thandle {
 	 * top thandle here for now, will fix it when we have better
 	 * callback mechanism */
 	struct thandle	*th_top;
-	/** context for this transaction, tag is LCT_TX_HANDLE */
-	struct lu_context th_ctx;
-
-	/** additional tags (layers can add in declare) */
-	__u32             th_tags;
 
 	/** the last operation result in this transaction.
 	 * this value is used in recovery */

lustre/include/lu_object.h
@@ -998,10 +998,6 @@ enum lu_context_tag {
          */
         LCT_DT_THREAD = 1 << 1,
         /**
-         * Context for transaction handle
-         */
-        LCT_TX_HANDLE = 1 << 2,
-        /**
          * Thread on client
          */
         LCT_CL_THREAD = 1 << 3,

lustre/osd-ldiskfs/osd_handler.c
@@ -1590,8 +1590,6 @@ static void osd_trans_commit_cb(struct super_block *sb,
         lu_device_put(lud);
         th->th_dev = NULL;
 
-        lu_context_exit(&th->th_ctx);
-        lu_context_fini(&th->th_ctx);
 	OBD_FREE_PTR(oh);
 }
 
@@ -1629,7 +1627,6 @@ static struct thandle *osd_trans_create(const struct lu_env *env,
 		th = &oh->ot_super;
 		th->th_dev = d;
 		th->th_result = 0;
-		th->th_tags = LCT_TX_HANDLE;
 		oh->ot_credits = 0;
 		INIT_LIST_HEAD(&oh->ot_commit_dcb_list);
 		INIT_LIST_HEAD(&oh->ot_stop_dcb_list);
@@ -1763,8 +1760,6 @@ static int osd_trans_start(const struct lu_env *env, struct dt_device *d,
         if (!IS_ERR(jh)) {
                 oh->ot_handle = jh;
                 LASSERT(oti->oti_txns == 0);
-                lu_context_init(&th->th_ctx, th->th_tags);
-                lu_context_enter(&th->th_ctx);
 
                 lu_device_get(&d->dd_lu_dev);
 		lu_ref_add_at(&d->dd_lu_dev.ld_reference, &oh->ot_dev_link,

lustre/osd-zfs/osd_handler.c
@@ -166,8 +166,6 @@ static void osd_trans_commit_cb(void *cb_data, int error)
 
 	lu_device_put(lud);
 	th->th_dev = NULL;
-	lu_context_exit(&th->th_ctx);
-	lu_context_fini(&th->th_ctx);
 	OBD_FREE_PTR(oh);
 
 	EXIT;
@@ -225,8 +223,6 @@ static int osd_trans_start(const struct lu_env *env, struct dt_device *d,
 		/* add commit callback */
 		dmu_tx_callback_register(oh->ot_tx, osd_trans_commit_cb, oh);
 		oh->ot_assigned = 1;
-		lu_context_init(&th->th_ctx, th->th_tags);
-		lu_context_enter(&th->th_ctx);
 		lu_device_get(&d->dd_lu_dev);
 	}
 
@@ -360,7 +356,6 @@ static struct thandle *osd_trans_create(const struct lu_env *env,
 	th = &oh->ot_super;
 	th->th_dev = dt;
 	th->th_result = 0;
-	th->th_tags = LCT_TX_HANDLE;
 	RETURN(th);
 }
 

lustre/osp/lproc_osp.c
@@ -874,6 +874,26 @@ osp_reserved_mb_low_seq_write(struct file *file, const char __user *buffer,
 }
 LPROC_SEQ_FOPS(osp_reserved_mb_low);
 
+static ssize_t
+lprocfs_force_sync_seq_write(struct file *file, const char __user *buffer,
+			     size_t count, loff_t *off)
+{
+	struct seq_file	  *m = file->private_data;
+	struct obd_device *dev = m->private;
+	struct dt_device  *dt = lu2dt_dev(dev->obd_lu_dev);
+	struct lu_env	   env;
+	int		   rc;
+
+	rc = lu_env_init(&env, LCT_LOCAL);
+	if (rc)
+		return rc;
+	rc = dt_sync(&env, dt);
+	lu_env_fini(&env);
+
+	return rc == 0 ? count : rc;
+}
+LPROC_SEQ_FOPS_WR_ONLY(osp, force_sync);
+
 static struct lprocfs_vars lprocfs_osp_obd_vars[] = {
 	{ .name =	"ping",
 	  .fops =	&osp_ping_fops,
@@ -926,6 +946,8 @@ static struct lprocfs_vars lprocfs_osp_obd_vars[] = {
 	  .fops =	&osp_reserved_mb_high_fops	},
 	{ .name =	"reserved_mb_low",
 	  .fops =	&osp_reserved_mb_low_fops	},
+	{ .name	=	"force_sync",
+	  .fops	=	&osp_force_sync_fops	},
 
 	/* for compatibility reasons */
 	{ .name =	"destroys_in_flight",

lustre/osp/osp_precreate.c
@@ -29,7 +29,7 @@
  * This file is part of Lustre, http://www.lustre.org/
  * Lustre is a trademark of Sun Microsystems, Inc.
  *
- * lustre/osp/osp_sync.c
+ * lustre/osp/osp_precreate.c
  *
  * Lustre OST Proxy Device
  *
@@ -168,7 +168,7 @@ out:
  *
  * \param[in] d		OSP device
  */
-static int osp_statfs_update(struct osp_device *d)
+static int osp_statfs_update(const struct lu_env *env, struct osp_device *d)
 {
 	struct ptlrpc_request	*req;
 	struct obd_import	*imp;
@@ -208,6 +208,29 @@ static int osp_statfs_update(struct osp_device *d)
 
 	ptlrpcd_add_req(req);
 
+	/* we still want to sync changes if no new changes are coming */
+	if (cfs_time_before(cfs_time_current(), d->opd_sync_next_commit_cb))
+		GOTO(out, rc);
+
+	if (atomic_read(&d->opd_sync_changes)) {
+		struct thandle *th;
+
+		th = dt_trans_create(env, d->opd_storage);
+		if (IS_ERR(th)) {
+			CERROR("%s: can't sync\n", d->opd_obd->obd_name);
+			GOTO(out, rc);
+		}
+		rc = dt_trans_start_local(env, d->opd_storage, th);
+		if (rc == 0) {
+			CDEBUG(D_OTHER, "%s: sync forced, %d changes\n",
+			       d->opd_obd->obd_name,
+			       atomic_read(&d->opd_sync_changes));
+			osp_sync_add_commit_cb_1s(env, d, th);
+			dt_trans_stop(env, d->opd_storage, th);
+		}
+	}
+
+out:
 	RETURN(0);
 }
 
@@ -1193,7 +1216,7 @@ static int osp_precreate_thread(void *_arg)
 			continue;
 		}
 
-		if (osp_statfs_update(d)) {
+		if (osp_statfs_update(&env, d)) {
 			l_wait_event(d->opd_pre_waitq,
 				     !osp_precreate_running(d), &lwi2);
 			continue;
@@ -1226,7 +1249,7 @@ static int osp_precreate_thread(void *_arg)
 				break;
 
 			if (osp_statfs_need_update(d))
-				if (osp_statfs_update(d))
+				if (osp_statfs_update(&env, d))
 					break;
 
 			/* To avoid handling different seq in precreate/orphan
@@ -1359,7 +1382,7 @@ int osp_precreate_reserve(const struct lu_env *env, struct osp_device *d)
 {
 	struct l_wait_info	 lwi;
 	cfs_time_t		 expire = cfs_time_shift(obd_timeout);
-	int			 precreated, rc;
+	int			 precreated, rc, synced = 0;
 
 	ENTRY;
 
@@ -1418,9 +1441,11 @@ int osp_precreate_reserve(const struct lu_env *env, struct osp_device *d)
 		 * wait till that is done - some space might be released
 		 */
 		if (unlikely(rc == -ENOSPC)) {
-			if (atomic_read(&d->opd_sync_changes)) {
+			if (atomic_read(&d->opd_sync_changes) && synced == 0) {
 				/* force local commit to release space */
 				dt_commit_async(env, d->opd_storage);
+				osp_sync_force(env, d);
+				synced = 1;
 			}
 			if (atomic_read(&d->opd_sync_rpcs_in_progress)) {
 				/* just wait till destroys are done */

commit d13151733f4fd460fa84f57185a2f931dce8604c

LU-16518 osp: remove unused osp_txn_info() and friends
The 'struct osp_txn_info' is no longer used anywhere. So
we no longer need to store this on OSP threads.

Fixes: 0ba690a526be ("LU-7251 osp: do not assign commit callback to every thandle")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I8dd5c20c8a9e667aa1ff98cc53430f44f8376ef9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58412
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osp/osp_dev.c
@@ -1807,6 +1807,7 @@ static int osp_obd_set_info_async(const struct lu_env *env,
 
 /* context key constructor/destructor: mdt_key_init, mdt_key_fini */
 LU_KEY_INIT_FINI(osp, struct osp_thread_info);
+
 static void osp_key_exit(const struct lu_context *ctx,
 			 struct lu_context_key *key, void *data)
 {
@@ -1822,15 +1823,7 @@ struct lu_context_key osp_thread_key = {
 	.lct_exit = osp_key_exit
 };
 
-/* context key constructor/destructor: mdt_txn_key_init, mdt_txn_key_fini */
-LU_KEY_INIT_FINI(osp_txn, struct osp_txn_info);
-
-struct lu_context_key osp_txn_key = {
-	.lct_tags = LCT_OSP_THREAD,
-	.lct_init = osp_txn_key_init,
-	.lct_fini = osp_txn_key_fini
-};
-LU_TYPE_INIT_FINI(osp, &osp_thread_key, &osp_txn_key);
+LU_TYPE_INIT_FINI(osp, &osp_thread_key);
 
 static const struct lu_device_type_operations osp_device_type_ops = {
 	.ldto_init           = osp_type_init,

lustre/osp/osp_internal.h
@@ -437,20 +437,6 @@ static inline struct osp_thread_info *osp_env_info(const struct lu_env *env)
 	return lu_env_info(env, &osp_thread_key);
 }
 
-struct osp_txn_info {
-	__u64   oti_current_id;
-};
-
-extern struct lu_context_key osp_txn_key;
-
-static inline struct osp_txn_info *osp_txn_info(struct lu_context *ctx)
-{
-	struct osp_txn_info *info;
-
-	info = lu_context_key_get(ctx, &osp_txn_key);
-	return info;
-}
-
 extern const struct lu_device_operations osp_lu_ops;
 
 static inline int lu_device_is_osp(struct lu_device *d)