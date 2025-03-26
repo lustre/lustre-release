lustre/lustre-release

commit 89df5e712ffd40064f1d4ce2f00f9156f68a2262

LU-11415 socklnd: improve scheduling algorithm
Modified the scheduling algorithm to use all scheduler threads
available. Previously a connection is assigned a single thread
and can only use that one. With this patch any scheduler thread
available on the assigned CPT can pick up and work on requests
queued on the connection.

Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Change-Id: I7e8e5220e1914155fd793994485e3a443fe1ba44
Reviewed-on: https://review.whamcloud.com/33740
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@gmail.com>
Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
Reviewed-by: Patrick Farrell <paf@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd_lib.c
@@ -71,7 +71,8 @@ ksocknal_lib_zc_capable(struct ksock_conn *conn)
 }
 
 int
-ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx)
+ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx,
+		      struct kvec *scratchiov)
 {
 	struct socket  *sock = conn->ksnc_sock;
 	int		nob;
@@ -92,7 +93,6 @@ ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx)
 		struct kvec *scratchiov = &scratch;
 		unsigned int niov = 1;
 #else
-		struct kvec *scratchiov = conn->ksnc_scheduler->kss_scratch_iov;
 		unsigned int niov = tx->tx_niov;
 #endif
 		struct msghdr msg = { .msg_flags = MSG_DONTWAIT };
@@ -113,41 +113,42 @@ ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx)
 }
 
 int
-ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx)
+ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx,
+		       struct kvec *scratchiov)
 {
-        struct socket *sock = conn->ksnc_sock;
-        lnet_kiov_t   *kiov = tx->tx_kiov;
-        int            rc;
-        int            nob;
+	struct socket *sock = conn->ksnc_sock;
+	lnet_kiov_t   *kiov = tx->tx_kiov;
+	int            rc;
+	int            nob;
 
-        /* Not NOOP message */
-        LASSERT (tx->tx_lnetmsg != NULL);
+	/* Not NOOP message */
+	LASSERT(tx->tx_lnetmsg != NULL);
 
-        /* NB we can't trust socket ops to either consume our iovs
-         * or leave them alone. */
-        if (tx->tx_msg.ksm_zc_cookies[0] != 0) {
-                /* Zero copy is enabled */
-                struct sock   *sk = sock->sk;
-                struct page   *page = kiov->kiov_page;
-                int            offset = kiov->kiov_offset;
-                int            fragsize = kiov->kiov_len;
-                int            msgflg = MSG_DONTWAIT;
+	/* NB we can't trust socket ops to either consume our iovs
+	 * or leave them alone. */
+	if (tx->tx_msg.ksm_zc_cookies[0] != 0) {
+		/* Zero copy is enabled */
+		struct sock   *sk = sock->sk;
+		struct page   *page = kiov->kiov_page;
+		int            offset = kiov->kiov_offset;
+		int            fragsize = kiov->kiov_len;
+		int            msgflg = MSG_DONTWAIT;
 
-                CDEBUG(D_NET, "page %p + offset %x for %d\n",
-                               page, offset, kiov->kiov_len);
+		CDEBUG(D_NET, "page %p + offset %x for %d\n",
+			       page, offset, kiov->kiov_len);
 
 		if (!list_empty(&conn->ksnc_tx_queue) ||
-                    fragsize < tx->tx_resid)
-                        msgflg |= MSG_MORE;
+		    fragsize < tx->tx_resid)
+			msgflg |= MSG_MORE;
 
-                if (sk->sk_prot->sendpage != NULL) {
-                        rc = sk->sk_prot->sendpage(sk, page,
-                                                   offset, fragsize, msgflg);
-                } else {
-                        rc = cfs_tcp_sendpage(sk, page, offset, fragsize,
-                                              msgflg);
-                }
-        } else {
+		if (sk->sk_prot->sendpage != NULL) {
+			rc = sk->sk_prot->sendpage(sk, page,
+						   offset, fragsize, msgflg);
+		} else {
+			rc = cfs_tcp_sendpage(sk, page, offset, fragsize,
+					      msgflg);
+		}
+	} else {
 #if SOCKNAL_SINGLE_FRAG_TX || !SOCKNAL_RISK_KMAP_DEADLOCK
 		struct kvec	scratch;
 		struct kvec   *scratchiov = &scratch;
@@ -156,7 +157,6 @@ ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx)
 #ifdef CONFIG_HIGHMEM
 #warning "XXX risk of kmap deadlock on multiple frags..."
 #endif
-		struct kvec *scratchiov = conn->ksnc_scheduler->kss_scratch_iov;
 		unsigned int  niov = tx->tx_nkiov;
 #endif
 		struct msghdr msg = { .msg_flags = MSG_DONTWAIT };
@@ -196,14 +196,13 @@ ksocknal_lib_eager_ack(struct ksock_conn *conn)
 }
 
 int
-ksocknal_lib_recv_iov(struct ksock_conn *conn)
+ksocknal_lib_recv_iov(struct ksock_conn *conn, struct kvec *scratchiov)
 {
 #if SOCKNAL_SINGLE_FRAG_RX
 	struct kvec  scratch;
 	struct kvec *scratchiov = &scratch;
 	unsigned int  niov = 1;
 #else
-	struct kvec *scratchiov = conn->ksnc_scheduler->kss_scratch_iov;
 	unsigned int  niov = conn->ksnc_rx_niov;
 #endif
 	struct kvec *iov = conn->ksnc_rx_iov;
@@ -301,7 +300,8 @@ ksocknal_lib_kiov_vmap(lnet_kiov_t *kiov, int niov,
 }
 
 int
-ksocknal_lib_recv_kiov(struct ksock_conn *conn)
+ksocknal_lib_recv_kiov(struct ksock_conn *conn, struct page **pages,
+		       struct kvec *scratchiov)
 {
 #if SOCKNAL_SINGLE_FRAG_RX || !SOCKNAL_RISK_KMAP_DEADLOCK
 	struct kvec   scratch;
@@ -312,8 +312,6 @@ ksocknal_lib_recv_kiov(struct ksock_conn *conn)
 #ifdef CONFIG_HIGHMEM
 #warning "XXX risk of kmap deadlock on multiple frags..."
 #endif
-	struct kvec  *scratchiov = conn->ksnc_scheduler->kss_scratch_iov;
-	struct page  **pages      = conn->ksnc_scheduler->kss_rx_scratch_pgs;
 	unsigned int   niov       = conn->ksnc_rx_nkiov;
 #endif
 	lnet_kiov_t   *kiov = conn->ksnc_rx_kiov;

lnet/klnds/socklnd/socklnd.c
@@ -665,31 +665,18 @@ ksocknal_get_conn_by_idx(struct lnet_ni *ni, int index)
 static struct ksock_sched *
 ksocknal_choose_scheduler_locked(unsigned int cpt)
 {
-	struct ksock_sched_info	*info = ksocknal_data.ksnd_sched_info[cpt];
-	struct ksock_sched *sched;
+	struct ksock_sched *sched = ksocknal_data.ksnd_schedulers[cpt];
 	int i;
 
-	if (info->ksi_nthreads == 0) {
-		cfs_percpt_for_each(info, i, ksocknal_data.ksnd_sched_info) {
-			if (info->ksi_nthreads > 0) {
+	if (sched->kss_nthreads == 0) {
+		cfs_percpt_for_each(sched, i, ksocknal_data.ksnd_schedulers) {
+			if (sched->kss_nthreads > 0) {
 				CDEBUG(D_NET, "scheduler[%d] has no threads. selected scheduler[%d]\n",
-				       cpt, info->ksi_cpt);
-				goto select_sched;
+				       cpt, sched->kss_cpt);
+				return sched;
 			}
 		}
 		return NULL;
-	}
-
-select_sched:
-	sched = &info->ksi_scheds[0];
-	/*
-	 * NB: it's safe so far, but info->ksi_nthreads could be changed
-	 * at runtime when we have dynamic LNet configuration, then we
-	 * need to take care of this.
-	 */
-	for (i = 1; i < info->ksi_nthreads; i++) {
-		if (sched->kss_nconns > info->ksi_scheds[i].kss_nconns)
-			sched = &info->ksi_scheds[i];
 	}
 
 	return sched;
@@ -1280,7 +1267,7 @@ ksocknal_create_conn(struct lnet_ni *ni, struct ksock_route *route,
 	 * The cpt might have changed if we ended up selecting a non cpt
 	 * native scheduler. So use the scheduler's cpt instead.
 	 */
-	cpt = sched->kss_info->ksi_cpt;
+	cpt = sched->kss_cpt;
         sched->kss_nconns++;
         conn->ksnc_scheduler = sched;
 
@@ -1319,11 +1306,10 @@ ksocknal_create_conn(struct lnet_ni *ni, struct ksock_route *route,
          */
 
 	CDEBUG(D_NET, "New conn %s p %d.x %pI4h -> %pI4h/%d"
-	       " incarnation:%lld sched[%d:%d]\n",
+	       " incarnation:%lld sched[%d]\n",
 	       libcfs_id2str(peerid), conn->ksnc_proto->pro_version,
 	       &conn->ksnc_myipaddr, &conn->ksnc_ipaddr,
-	       conn->ksnc_port, incarnation, cpt,
-	       (int)(sched - &sched->kss_info->ksi_scheds[0]));
+	       conn->ksnc_port, incarnation, cpt);
 
         if (active) {
                 /* additional routes after interface exchange? */
@@ -2208,7 +2194,7 @@ ksocknal_ctl(struct lnet_ni *ni, unsigned int cmd, void *arg)
                 data->ioc_u32[1] = conn->ksnc_port;
                 data->ioc_u32[2] = conn->ksnc_myipaddr;
                 data->ioc_u32[3] = conn->ksnc_type;
-		data->ioc_u32[4] = conn->ksnc_scheduler->kss_info->ksi_cpt;
+		data->ioc_u32[4] = conn->ksnc_scheduler->kss_cpt;
                 data->ioc_u32[5] = rxmem;
                 data->ioc_u32[6] = conn->ksnc_peer->ksnp_id.pid;
                 ksocknal_conn_decref(conn);
@@ -2247,19 +2233,8 @@ ksocknal_free_buffers (void)
 {
 	LASSERT (atomic_read(&ksocknal_data.ksnd_nactive_txs) == 0);
 
-	if (ksocknal_data.ksnd_sched_info != NULL) {
-		struct ksock_sched_info	*info;
-		int			i;
-
-		cfs_percpt_for_each(info, i, ksocknal_data.ksnd_sched_info) {
-			if (info->ksi_scheds != NULL) {
-				LIBCFS_FREE(info->ksi_scheds,
-					    info->ksi_nthreads_max *
-					    sizeof(info->ksi_scheds[0]));
-			}
-		}
-		cfs_percpt_free(ksocknal_data.ksnd_sched_info);
-	}
+	if (ksocknal_data.ksnd_schedulers != NULL)
+		cfs_percpt_free(ksocknal_data.ksnd_schedulers);
 
         LIBCFS_FREE (ksocknal_data.ksnd_peers,
 		     sizeof(struct list_head) *
@@ -2288,10 +2263,8 @@ ksocknal_free_buffers (void)
 static void
 ksocknal_base_shutdown(void)
 {
-	struct ksock_sched_info *info;
 	struct ksock_sched *sched;
 	int i;
-	int j;
 
 	CDEBUG(D_MALLOC, "before NAL cleanup: kmem %d\n",
 	       atomic_read (&libcfs_kmemory));
@@ -2314,23 +2287,14 @@ ksocknal_base_shutdown(void)
 		LASSERT(list_empty(&ksocknal_data.ksnd_connd_connreqs));
 		LASSERT(list_empty(&ksocknal_data.ksnd_connd_routes));
 
-		if (ksocknal_data.ksnd_sched_info != NULL) {
-			cfs_percpt_for_each(info, i,
-					    ksocknal_data.ksnd_sched_info) {
-				if (info->ksi_scheds == NULL)
-					continue;
+		if (ksocknal_data.ksnd_schedulers != NULL) {
+			cfs_percpt_for_each(sched, i,
+					    ksocknal_data.ksnd_schedulers) {
 
-				for (j = 0; j < info->ksi_nthreads_max; j++) {
-
-					sched = &info->ksi_scheds[j];
-					LASSERT(list_empty(&sched->\
-							       kss_tx_conns));
-					LASSERT(list_empty(&sched->\
-							       kss_rx_conns));
-					LASSERT(list_empty(&sched-> \
-						  kss_zombie_noop_txs));
-					LASSERT(sched->kss_nconns == 0);
-				}
+				LASSERT(list_empty(&sched->kss_tx_conns));
+				LASSERT(list_empty(&sched->kss_rx_conns));
+				LASSERT(list_empty(&sched->kss_zombie_noop_txs));
+				LASSERT(sched->kss_nconns == 0);
 			}
 		}
 
@@ -2339,17 +2303,10 @@ ksocknal_base_shutdown(void)
 		wake_up_all(&ksocknal_data.ksnd_connd_waitq);
 		wake_up_all(&ksocknal_data.ksnd_reaper_waitq);
 
-		if (ksocknal_data.ksnd_sched_info != NULL) {
-			cfs_percpt_for_each(info, i,
-					    ksocknal_data.ksnd_sched_info) {
-				if (info->ksi_scheds == NULL)
-					continue;
-
-				for (j = 0; j < info->ksi_nthreads_max; j++) {
-					sched = &info->ksi_scheds[j];
+		if (ksocknal_data.ksnd_schedulers != NULL) {
+			cfs_percpt_for_each(sched, i,
+					    ksocknal_data.ksnd_schedulers)
 					wake_up_all(&sched->kss_waitq);
-				}
-			}
 		}
 
 		i = 4;
@@ -2382,9 +2339,9 @@ ksocknal_base_shutdown(void)
 static int
 ksocknal_base_startup(void)
 {
-	struct ksock_sched_info	*info;
-	int			rc;
-	int			i;
+	struct ksock_sched *sched;
+	int rc;
+	int i;
 
         LASSERT (ksocknal_data.ksnd_init == SOCKNAL_INIT_NOTHING);
         LASSERT (ksocknal_data.ksnd_nnets == 0);
@@ -2424,45 +2381,38 @@ ksocknal_base_startup(void)
 	ksocknal_data.ksnd_init = SOCKNAL_INIT_DATA;
 	try_module_get(THIS_MODULE);
 
-	ksocknal_data.ksnd_sched_info = cfs_percpt_alloc(lnet_cpt_table(),
-							 sizeof(*info));
-	if (ksocknal_data.ksnd_sched_info == NULL)
+	/* Create a scheduler block per available CPT */
+	ksocknal_data.ksnd_schedulers = cfs_percpt_alloc(lnet_cpt_table(),
+							 sizeof(*sched));
+	if (ksocknal_data.ksnd_schedulers == NULL)
 		goto failed;
 
-	cfs_percpt_for_each(info, i, ksocknal_data.ksnd_sched_info) {
-		struct ksock_sched *sched;
+	cfs_percpt_for_each(sched, i, ksocknal_data.ksnd_schedulers) {
 		int nthrs;
 
+		/*
+		 * make sure not to allocate more threads than there are
+		 * cores/CPUs in teh CPT
+		 */
 		nthrs = cfs_cpt_weight(lnet_cpt_table(), i);
 		if (*ksocknal_tunables.ksnd_nscheds > 0) {
 			nthrs = min(nthrs, *ksocknal_tunables.ksnd_nscheds);
 		} else {
-			/* max to half of CPUs, assume another half should be
-			 * reserved for upper layer modules */
+			/*
+			 * max to half of CPUs, assume another half should be
+			 * reserved for upper layer modules
+			 */
 			nthrs = min(max(SOCKNAL_NSCHEDS, nthrs >> 1), nthrs);
 		}
 
-		info->ksi_nthreads_max = nthrs;
-		info->ksi_cpt = i;
+		sched->kss_nthreads_max = nthrs;
+		sched->kss_cpt = i;
 
-		if (nthrs != 0) {
-			LIBCFS_CPT_ALLOC(info->ksi_scheds, lnet_cpt_table(), i,
-					 info->ksi_nthreads_max *
-						sizeof(*sched));
-			if (info->ksi_scheds == NULL)
-				goto failed;
-
-			for (; nthrs > 0; nthrs--) {
-				sched = &info->ksi_scheds[nthrs - 1];
-
-				sched->kss_info = info;
-				spin_lock_init(&sched->kss_lock);
-				INIT_LIST_HEAD(&sched->kss_rx_conns);
-				INIT_LIST_HEAD(&sched->kss_tx_conns);
-				INIT_LIST_HEAD(&sched->kss_zombie_noop_txs);
-				init_waitqueue_head(&sched->kss_waitq);
-			}
-		}
+		spin_lock_init(&sched->kss_lock);
+		INIT_LIST_HEAD(&sched->kss_rx_conns);
+		INIT_LIST_HEAD(&sched->kss_tx_conns);
+		INIT_LIST_HEAD(&sched->kss_zombie_noop_txs);
+		init_waitqueue_head(&sched->kss_waitq);
         }
 
         ksocknal_data.ksnd_connd_starting         = 0;
@@ -2720,37 +2670,35 @@ ksocknal_search_new_ipif(struct ksock_net *net)
 }
 
 static int
-ksocknal_start_schedulers(struct ksock_sched_info *info)
+ksocknal_start_schedulers(struct ksock_sched *sched)
 {
 	int	nthrs;
 	int	rc = 0;
 	int	i;
 
-	if (info->ksi_nthreads == 0) {
+	if (sched->kss_nthreads == 0) {
 		if (*ksocknal_tunables.ksnd_nscheds > 0) {
-			nthrs = info->ksi_nthreads_max;
+			nthrs = sched->kss_nthreads_max;
 		} else {
 			nthrs = cfs_cpt_weight(lnet_cpt_table(),
-					       info->ksi_cpt);
+					       sched->kss_cpt);
 			nthrs = min(max(SOCKNAL_NSCHEDS, nthrs >> 1), nthrs);
 			nthrs = min(SOCKNAL_NSCHEDS_HIGH, nthrs);
 		}
-		nthrs = min(nthrs, info->ksi_nthreads_max);
+		nthrs = min(nthrs, sched->kss_nthreads_max);
 	} else {
-		LASSERT(info->ksi_nthreads <= info->ksi_nthreads_max);
+		LASSERT(sched->kss_nthreads <= sched->kss_nthreads_max);
 		/* increase two threads if there is new interface */
-		nthrs = min(2, info->ksi_nthreads_max - info->ksi_nthreads);
+		nthrs = min(2, sched->kss_nthreads_max - sched->kss_nthreads);
 	}
 
 	for (i = 0; i < nthrs; i++) {
 		long id;
 		char name[20];
-		struct ksock_sched *sched;
 
-		id = KSOCK_THREAD_ID(info->ksi_cpt, info->ksi_nthreads + i);
-		sched = &info->ksi_scheds[KSOCK_THREAD_SID(id)];
+		id = KSOCK_THREAD_ID(sched->kss_cpt, sched->kss_nthreads + i);
 		snprintf(name, sizeof(name), "socknal_sd%02d_%02d",
-			 info->ksi_cpt, (int)(sched - &info->ksi_scheds[0]));
+			 sched->kss_cpt, (int)KSOCK_THREAD_SID(id));
 
 		rc = ksocknal_thread_start(ksocknal_scheduler,
 					   (void *)id, name);
@@ -2758,11 +2706,11 @@ ksocknal_start_schedulers(struct ksock_sched_info *info)
 			continue;
 
 		CERROR("Can't spawn thread %d for scheduler[%d]: %d\n",
-		       info->ksi_cpt, info->ksi_nthreads + i, rc);
+		       sched->kss_cpt, (int) KSOCK_THREAD_SID(id), rc);
 		break;
 	}
 
-	info->ksi_nthreads += i;
+	sched->kss_nthreads += i;
 	return rc;
 }
 
@@ -2777,16 +2725,16 @@ ksocknal_net_start_threads(struct ksock_net *net, __u32 *cpts, int ncpts)
 		return -EINVAL;
 
 	for (i = 0; i < ncpts; i++) {
-		struct ksock_sched_info	*info;
+		struct ksock_sched *sched;
 		int cpt = (cpts == NULL) ? i : cpts[i];
 
 		LASSERT(cpt < cfs_cpt_number(lnet_cpt_table()));
-		info = ksocknal_data.ksnd_sched_info[cpt];
+		sched = ksocknal_data.ksnd_schedulers[cpt];
 
-		if (!newif && info->ksi_nthreads > 0)
+		if (!newif && sched->kss_nthreads > 0)
 			continue;
 
-		rc = ksocknal_start_schedulers(info);
+		rc = ksocknal_start_schedulers(sched);
 		if (rc != 0)
 			return rc;
 	}

lnet/klnds/socklnd/socklnd.h
@@ -88,32 +88,25 @@
 # define SOCKNAL_RISK_KMAP_DEADLOCK  1
 #endif
 
-struct ksock_sched_info;
-
-struct ksock_sched {				/* per scheduler state */
-	spinlock_t		kss_lock;	/* serialise */
-	struct list_head	kss_rx_conns;	/* conn waiting to be read */
+/* per scheduler state */
+struct ksock_sched {
+	/* serialise */
+	spinlock_t kss_lock;
 	/* conn waiting to be written */
-	struct list_head	kss_tx_conns;
+	struct list_head kss_rx_conns;
+	struct list_head kss_tx_conns;
 	/* zombie noop tx list */
-	struct list_head	kss_zombie_noop_txs;
-	wait_queue_head_t	kss_waitq;	/* where scheduler sleeps */
+	struct list_head kss_zombie_noop_txs;
+	/* where scheduler sleeps */
+	wait_queue_head_t kss_waitq;
 	/* # connections assigned to this scheduler */
-	int			kss_nconns;
-	struct ksock_sched_info	*kss_info;	/* owner of it */
-#if !SOCKNAL_SINGLE_FRAG_RX
-	struct page		*kss_rx_scratch_pgs[LNET_MAX_IOV];
-#endif
-#if !SOCKNAL_SINGLE_FRAG_TX || !SOCKNAL_SINGLE_FRAG_RX
-	struct kvec		kss_scratch_iov[LNET_MAX_IOV];
-#endif
-};
-
-struct ksock_sched_info {
-	int			ksi_nthreads_max; /* max allowed threads */
-	int			ksi_nthreads;	/* number of threads */
-	int			ksi_cpt;	/* CPT id */
-	struct ksock_sched	*ksi_scheds;	/* array of schedulers */
+	int kss_nconns;
+	/* max allowed threads */
+	int kss_nthreads_max;
+	/* number of threads */
+	int kss_nthreads;
+	/* CPT id */
+	int kss_cpt;
 };
 
 #define KSOCK_CPT_SHIFT			16
@@ -199,7 +192,7 @@ struct ksock_nal_data {
 	int			ksnd_nthreads;	/* # live threads */
 	int			ksnd_shuttingdown; /* tell threads to exit */
 	/* schedulers information */
-	struct ksock_sched_info	**ksnd_sched_info;
+	struct ksock_sched	**ksnd_schedulers;
 
 	atomic_t      ksnd_nactive_txs;    /* #active txs */
 
@@ -662,11 +655,15 @@ extern void ksocknal_lib_reset_callback(struct socket *sock,
 extern void ksocknal_lib_push_conn(struct ksock_conn *conn);
 extern int ksocknal_lib_get_conn_addrs(struct ksock_conn *conn);
 extern int ksocknal_lib_setup_sock(struct socket *so);
-extern int ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx);
-extern int ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx);
+extern int ksocknal_lib_send_iov(struct ksock_conn *conn, struct ksock_tx *tx,
+				 struct kvec *scratch_iov);
+extern int ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx,
+				  struct kvec *scratch_iov);
 extern void ksocknal_lib_eager_ack(struct ksock_conn *conn);
-extern int ksocknal_lib_recv_iov(struct ksock_conn *conn);
-extern int ksocknal_lib_recv_kiov(struct ksock_conn *conn);
+extern int ksocknal_lib_recv_iov(struct ksock_conn *conn,
+				 struct kvec *scratchiov);
+extern int ksocknal_lib_recv_kiov(struct ksock_conn *conn, struct page **pages,
+		       struct kvec *scratchiov);
 extern int ksocknal_lib_get_conn_tunables(struct ksock_conn *conn, int *txmem,
 					  int *rxmem, int *nagle);
 

commit 36dced9600a0019260e2becee1e39e797c65d417

LU-18822 build: fix symbol redeclaration error in socklnd_lib.c
This patch fixes the following symbol redeclaration errors
in socklnd_lib.c:

  lnet/klnds/socklnd/socklnd_lib.c: In function 'ksocknal_lib_recv_kiov':
  lnet/klnds/socklnd/socklnd_lib.c:314:23: error: 'scratchiov' redeclared as different kind of symbol
    314 |         struct kvec  *scratchiov = &scratch;
        |                       ^~~~~~~~~~
  lnet/klnds/socklnd/socklnd_lib.c:310:37: note: previous definition of 'scratchiov' with type 'struct kvec *'
    310 |                        struct kvec *scratchiov)
        |                        ~~~~~~~~~~~~~^~~~~~~~~~
  lnet/klnds/socklnd/socklnd_lib.c:315:24: error: 'pages' redeclared as different kind of symbol
    315 |         struct page  **pages      = NULL;
        |                        ^~~~~
  lnet/klnds/socklnd/socklnd_lib.c:309:63: note: previous definition of 'pages' with type 'struct page **'
    309 | ksocknal_lib_recv_kiov(struct ksock_conn *conn, struct page **pages,
        |                                                 ~~~~~~~~~~~~~~^~~~~

Fixes: 89df5e712f ("LU-11415 socklnd: improve scheduling algorithm")
Change-Id: I047746e93ef1b4421f0bf7ff0c39848996698331
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58441
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd_lib.c
@@ -68,7 +68,7 @@ ksocknal_lib_send_hdr(struct ksock_conn *conn, struct ksock_tx *tx,
 	{
 #if SOCKNAL_SINGLE_FRAG_TX
 		struct kvec scratch;
-		struct kvec *scratchiov = &scratch;
+		scratchiov = &scratch;
 		unsigned int niov = 1;
 #else
 		unsigned int niov = tx->tx_niov;
@@ -144,7 +144,7 @@ ksocknal_lib_send_kiov(struct ksock_conn *conn, struct ksock_tx *tx,
 	} else {
 #if SOCKNAL_SINGLE_FRAG_TX || !SOCKNAL_RISK_KMAP_DEADLOCK
 		struct kvec scratch;
-		struct kvec *scratchiov = &scratch;
+		scratchiov = &scratch;
 		unsigned int niov = 1;
 #else
 #ifdef CONFIG_HIGHMEM
@@ -190,7 +190,7 @@ ksocknal_lib_recv_iov(struct ksock_conn *conn, struct kvec *scratchiov)
 {
 #if SOCKNAL_SINGLE_FRAG_RX
 	struct kvec  scratch;
-	struct kvec *scratchiov = &scratch;
+	scratchiov = &scratch;
 	unsigned int niov = 1;
 #else
 	unsigned int niov = conn->ksnc_rx_niov;
@@ -297,8 +297,8 @@ ksocknal_lib_recv_kiov(struct ksock_conn *conn, struct page **pages,
 {
 #if SOCKNAL_SINGLE_FRAG_RX || !SOCKNAL_RISK_KMAP_DEADLOCK
 	struct kvec   scratch;
-	struct kvec  *scratchiov = &scratch;
-	struct page  **pages      = NULL;
+	scratchiov = &scratch;
+	pages      = NULL;
 	unsigned int   niov       = 1;
 #else
 #ifdef CONFIG_HIGHMEM