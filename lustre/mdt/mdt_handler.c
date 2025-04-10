lustre/lustre-release

commit 2686838fefabba038c5a8192b84bfb375391ab14

LU-18240 sec: enforce per-nodemap project quota for root
Add a new 'ignore_root_prjquota' rbac role on nodemaps, enabled by
default. When not present, project quota for root is enforced for
clients part of this nodemap.

The tricky part is to find a way to pass this nodemap-related
information to the quota enforcement code. On the data path, this is
put in the transaction handle in a field 'th_ignore_root_proj_quota'
when ofd_trans_create() is called. Then the OSD layer is able to put
it in a field 'lqi_ignore_root_proj_quota' of struct lquota_id_info,
so that it can be checked by the QSD handler.
On the metadata path, it needs one more intermediate step as the MDD
layer does not have access to the client export to retrieve the
nodemap. So a new field 'uc_rbac_ignore_root_prjquota' is added to
struct lu_ucred. It is set in ucred_set_rbac_roles(), and used in
mdd_trans_create() to put the value in the transaction handle.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I71292fb76cbc6476b8ead83bf7a686fd1b03611e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56436
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -7147,6 +7147,7 @@ static int mdt_ctxt_add_dirty_flag(struct lu_env *env,
 	mdt_ucred(info)->uc_rbac_chlg_ops = 1;
 	mdt_ucred(info)->uc_rbac_fscrypt_admin = 1;
 	mdt_ucred(info)->uc_rbac_server_upcall = 1;
+	mdt_ucred(info)->uc_rbac_ignore_root_prjquota = 1;
 	rc = mdt_add_dirty_flag(info, mfd->mfd_object, &info->mti_attr);
 
 	lu_context_exit(&ses);

lustre/tests/sanity-quota.sh
@@ -1226,6 +1226,9 @@ test_1j() {
 	local testf="$DIR/$tdir/$tfile-0"
 	local testf1="$DIR/$tdir/$tfile-1"
 	local testf2="$DIR/$tdir/$tfile-2"
+	local client_ip=$(host_nids_address $HOSTNAME $NETTYPE)
+	local client_nid=$(h2nettype $client_ip)
+	local nm=test_1j
 
 	(( $OST1_VERSION >= $(version_code 2.14.0.74) )) ||
 		skip "need OST at least 2.14.0.74"
@@ -1291,6 +1294,39 @@ test_1j() {
 		error "disable root quotas for project failed"
 	do_facet ost2 $LCTL set_param $procf=0 ||
 		error "disable root quotas for project failed"
+
+	if (( $OST1_VERSION >= $(version_code 2.16.50) )); then
+		do_facet mgs $LCTL nodemap_modify --name default \
+			--property admin --value 1
+		do_facet mgs $LCTL nodemap_modify --name default \
+			--property trusted --value 1
+		do_facet mgs $LCTL nodemap_add $nm
+		stack_trap "do_facet mgs $LCTL nodemap_del $nm || true"
+		do_facet mgs $LCTL nodemap_add_range 	\
+			--name $nm --range $client_nid
+		do_facet mgs $LCTL nodemap_modify --name $nm \
+			--property admin --value 1
+		do_facet mgs $LCTL nodemap_modify --name $nm \
+			--property trusted --value 1
+		# do not set ignore_root_prjquota rbac role
+		do_facet mgs $LCTL nodemap_modify --name $nm --property rbac \
+			--value file_perms,dne_ops,quota_ops,byfid_ops,chlg_ops
+		do_facet mgs $LCTL nodemap_activate 1
+		stack_trap "do_facet mgs $LCTL nodemap_activate 0"
+		wait_nm_sync active
+		wait_nm_sync default admin_nodemap
+		wait_nm_sync default trusted_nodemap
+		wait_nm_sync $nm admin_nodemap
+		wait_nm_sync $nm trusted_nodemap
+		wait_nm_sync $nm rbac
+
+		runas -u 0 -g 0 $DD of=$testf count=$((limit/2)) \
+			seek=$limit oflag=direct &&
+			quota_error "project" $TSTPRJID "root write should fail"
+
+		do_facet mgs $LCTL nodemap_activate 0
+		wait_nm_sync active
+	fi
 
 	runas -u 0 -g 0 $DD of=$testf count=$limit seek=$limit oflag=direct ||
 		quota_error "project" $TSTPRJID "root write to project failed"

lustre/doc/lctl-nodemap-modify.8
@@ -99,6 +99,8 @@ Defaults to all, which means all roles are allowed. Other possible values
 it is still possible to lock or unlock encrypted directories,
 as these operations only need read access to fscrypt metadata.
 .br
+- ignore_root_prjquota, so that project quota is not enforced for the root user.
+.br
 - quota_ops, to allow quota modifications.
 .br
 - server_upcall, to define which identity upcall to use. If set, identity upcall

lustre/include/dt_object.h
@@ -2034,7 +2034,9 @@ struct thandle {
 	/* whether ignore quota */
 				th_ignore_quota:1,
 	/* whether restart transaction */
-				th_restart_tran:1;
+				th_restart_tran:1,
+	/* enforce project quota for root */
+				th_ignore_root_proj_quota:1;
 };
 
 /*

lustre/include/lustre_nodemap.h
@@ -33,6 +33,7 @@ static const struct nodemap_rbac_name {
 	{ NODEMAP_RBAC_CHLG_OPS,	"chlg_ops"	},
 	{ NODEMAP_RBAC_FSCRYPT_ADMIN,   "fscrypt_admin"	},
 	{ NODEMAP_RBAC_SERVER_UPCALL,	"server_upcall"	},
+	{ NODEMAP_RBAC_IGN_ROOT_PRJQUOTA,	"ignore_root_prjquota"	},
 };
 
 struct nodemap_pde {

lustre/include/lustre_quota.h
@@ -238,6 +238,8 @@ struct lquota_id_info {
 
 	/* whether we are reporting blocks or inodes */
 	bool			 lqi_is_blk;
+	/* enforce project quota for root */
+	bool			 lqi_ignore_root_proj_quota;
 };
 
 /* With the DoM, both inode quota in meta pool and block quota in data pool

lustre/include/md_object.h
@@ -701,6 +701,7 @@ struct lu_ucred {
 	int			 uc_rbac_chlg_ops:1;
 	int			 uc_rbac_fscrypt_admin:1;
 	int			 uc_rbac_server_upcall:1;
+	int			 uc_rbac_ignore_root_prjquota:1;
 };
 
 struct lu_ucred *lu_ucred(const struct lu_env *env);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3825,13 +3825,15 @@ enum nodemap_rbac_roles {
 	NODEMAP_RBAC_CHLG_OPS		= 0x00000010,
 	NODEMAP_RBAC_FSCRYPT_ADMIN	= 0x00000020,
 	NODEMAP_RBAC_SERVER_UPCALL	= 0x00000040,
+	NODEMAP_RBAC_IGN_ROOT_PRJQUOTA	= 0x00000080,
 	NODEMAP_RBAC_NONE	= (__u32)~(NODEMAP_RBAC_FILE_PERMS	|
 					   NODEMAP_RBAC_DNE_OPS	|
 					   NODEMAP_RBAC_QUOTA_OPS	|
 					   NODEMAP_RBAC_BYFID_OPS	|
 					   NODEMAP_RBAC_CHLG_OPS	|
 					   NODEMAP_RBAC_FSCRYPT_ADMIN	|
-					   NODEMAP_RBAC_SERVER_UPCALL),
+					   NODEMAP_RBAC_SERVER_UPCALL	|
+					   NODEMAP_RBAC_IGN_ROOT_PRJQUOTA),
 	NODEMAP_RBAC_ALL	= 0xFFFFFFFF, /* future caps ON by default */
 };
 

commit 7e5a5855ca19d3fe773b17a19c78a022b27bdfcb

LU-18859 quota: show default quota and root squash
When show default quotas don't swap ID=0 with squashed ID.
It is not required as default quotas are stored in a ROOT
lqe. Patch is against the following panic:

 (qmt_handler.c:58:qmt_get())
	ASSERTION( !is_default || id->qid_uid == 0 ) failed:
 (qmt_handler.c:58:qmt_get()) LBUG
  ...
  dump_stack+0x41/0x60
  lbug_with_loc.cold.8+0x5/0x43 [libcfs]
  qmt_get+0x185/0x3e0 [lquota]
  qmt_quotactl+0x309/0xa10 [lquota]
  mdt_quotactl+0x21f/0x880 [mdt]
  tgt_request_handle+0xc9c/0x1990 [ptlrpc]
  ptlrpc_server_handle_request+0x323/0xbd0 [ptlrpc]
  ptlrpc_main+0xb45/0x13a0 [ptlrpc]
  kthread+0x134/0x150

Cleanup lqes created in sanity-quota_87 "lfs quota -a should
print default quota setting". Without that new default quota
limits are applied to 50 default lqes created in test case 87.
It produces too many useless lustre debug logs.

Add into stack_trap restoring initial nodemap values changed
in tests 1j and 75.

Fixes: 2686838fef ("LU-18240 sec: enforce per-nodemap project quota for root")
Fixes: a4fbe7341b ("LU-14739 quota: nodemap squashed root cannot bypass quota")
Fixes: 115562106b ("LU-18453 quota: show default quota in lfs quota -a")
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: Ieef5f8b99c92d8fef3addcb7d55771c96c82129f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58563
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -3485,6 +3485,17 @@ put:
 	return rc;
 }
 
+/* To get default quotas ID needs to be 0, so
+ * no reasons to swap this according to nodemap.
+ */
+static inline bool qmt_need_swap(__u32 cmd)
+{
+	if (cmd == LUSTRE_Q_GETDEFAULT || cmd == LUSTRE_Q_GETDEFAULT_POOL)
+		return false;
+
+	return true;
+}
+
 /*
  * Handle quota control requests to consult current usage/limit, but also
  * to configure quota enforcement
@@ -3588,7 +3599,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 	if (oqctl->qc_cmd == Q_SETINFO || oqctl->qc_cmd == Q_SETQUOTA)
 		barrier_exit(tsi->tsi_tgt->lut_bottom);
 
-	if (oqctl->qc_id != id)
+	if (oqctl->qc_id != id && qmt_need_swap(oqctl->qc_cmd))
 		swap(oqctl->qc_id, id);
 
 	if (oqctl->qc_cmd == Q_SETINFO || oqctl->qc_cmd == Q_SETQUOTA) {
@@ -3633,7 +3644,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 		GOTO(out_nodemap, rc = -EFAULT);
 	}
 
-	if (oqctl->qc_id != id)
+	if (oqctl->qc_id != id && qmt_need_swap(oqctl->qc_cmd))
 		swap(oqctl->qc_id, id);
 
 	QCTL_COPY_NO_PNAME(repoqc, oqctl);

lustre/tests/sanity-quota.sh
@@ -1296,10 +1296,19 @@ test_1j() {
 		error "disable root quotas for project failed"
 
 	if (( $OST1_VERSION >= $(version_code 2.16.50) )); then
+		local cmd="do_facet mgs $LCTL get_param -n "
+		local adm=$($cmd nodemap.default.admin_nodemap)
+		local trs=$($cmd nodemap.default.trusted_nodemap)
+		local act=$($cmd nodemap.active)
+
 		do_facet mgs $LCTL nodemap_modify --name default \
 			--property admin --value 1
+		stack_trap "do_facet mgs $LCTL nodemap_modify --name default \
+			--property admin --value $adm"
 		do_facet mgs $LCTL nodemap_modify --name default \
 			--property trusted --value 1
+		stack_trap "do_facet mgs $LCTL nodemap_modify --name default \
+			--property trusted --value $trs"
 		do_facet mgs $LCTL nodemap_add $nm
 		stack_trap "do_facet mgs $LCTL nodemap_del $nm || true"
 		do_facet mgs $LCTL nodemap_add_range 	\
@@ -1312,7 +1321,7 @@ test_1j() {
 		do_facet mgs $LCTL nodemap_modify --name $nm --property rbac \
 			--value file_perms,dne_ops,quota_ops,byfid_ops,chlg_ops
 		do_facet mgs $LCTL nodemap_activate 1
-		stack_trap "do_facet mgs $LCTL nodemap_activate 0"
+		stack_trap "do_facet mgs $LCTL nodemap_activate $act"
 		wait_nm_sync active
 		wait_nm_sync default admin_nodemap
 		wait_nm_sync default trusted_nodemap
@@ -5726,21 +5735,35 @@ run_test 74 "check quota pools per user"
 function cleanup_quota_test_75()
 {
 	do_facet mgs $LCTL nodemap_modify --name default \
-		--property admin --value 1
+		--property admin --value $1
 	do_facet mgs $LCTL nodemap_modify --name default \
-		--property trusted --value 1
+		--property trusted --value $2
 	do_facet mgs $LCTL nodemap_modify --name default \
-		--property squash_uid --value 99
+		--property deny_unknown --value $3
 	do_facet mgs $LCTL nodemap_modify --name default \
-		--property squash_gid --value 99
+		--property squash_uid --value $4
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property squash_gid --value $5
 
 	wait_nm_sync default admin_nodemap
 	wait_nm_sync default trusted_nodemap
 
-	do_facet mgs $LCTL nodemap_activate 0
-	wait_nm_sync active
+	do_facet mgs $LCTL nodemap_activate $6
+	wait_nm_sync active $6
+	return 0
+}
 
-	resetquota -u $TSTUSR
+stack_trap_nodemap_cleanup_75()
+{
+	local cmd="do_facet mgs $LCTL get_param -n "
+	local adm=$($cmd nodemap.default.admin_nodemap)
+	local trs=$($cmd nodemap.default.trusted_nodemap)
+	local deny=$($cmd nodemap.default.deny_unknown)
+	local uid=$($cmd nodemap.default.squash_uid)
+	local gid=$($cmd nodemap.default.squash_gid)
+	local act=$($cmd nodemap.active)
+
+	stack_trap "cleanup_quota_test_75 $adm $trs $deny $uid $gid $act"
 }
 
 test_dom_75() {
@@ -5818,7 +5841,6 @@ test_75()
 	fi
 
 	setup_quota_test || error "setup quota failed with $?"
-	stack_trap cleanup_quota_test_75 EXIT
 
 	# enable ost quota
 	set_ost_qtype $QTYPE || error "enable ost quota failed"
@@ -5837,6 +5859,7 @@ test_75()
 	$LFS setstripe -E 1M -L mdt $DIR/$tdir_dom ||
 		error "setstripe $tdir_dom failed"
 
+	stack_trap_nodemap_cleanup_75
 	do_facet mgs $LCTL nodemap_activate 1
 	wait_nm_sync active
 	do_facet mgs $LCTL nodemap_modify --name default \
@@ -6460,6 +6483,24 @@ test_86()
 }
 run_test 86 "Pre-acquired quota should be released if quota is over limit"
 
+cleanup_lqes()
+{
+	for ((i = $1; i < $2; i++)); do
+		$LFS setquota -B0 -b0 -I0 -i0 -u $i $MOUNT ||
+			error "Can't cleanup user $i"
+		$LFS setquota -B0 -b0 -I0 -i0 -g $i $MOUNT ||
+			error "Can't cleanup group $i"
+		is_project_quota_supported &&
+			$LFS setquota -B0 -b0 -I0 -i0 -p $i $MOUNT ||
+				error "Can't cleanup project $i"
+	done
+	# The only way to remove lqes from the hash table
+	stop mds1 -f || error "MDS umount failed"
+	start mds1 $(mdsdevname 1) $MDS_MOUNT_OPTS
+	quota_init
+	clients_up || true
+}
+
 test_87()
 {
 	(( $MDS1_VERSION >= $(version_code 2.16.50) )) ||
@@ -6497,6 +6538,7 @@ test_87()
 
 	#define OBD_FAIL_QUOTA_NOSYNC		0xA09
 	do_facet mds1 $LCTL set_param fail_loc=0xa09
+	stack_trap "cleanup_lqes 100 200"
 
 	for ((i = 100; i < 150; i++)); do
 		$LFS setquota -u $i -b $blimit -B $blimit $MOUNT ||
@@ -6614,6 +6656,38 @@ test_88()
 	return 0
 }
 run_test 88 "Writing over quota should not hang"
+
+test_89()
+{
+	local cmd="do_facet mgs $LCTL get_param -n "
+
+	(( $MDS1_VERSION >= $(version_code 2.16.53) )) ||
+		skip "need MDS >= 2.16.53 to show default quota with squash_uid"
+
+	local act=$($cmd nodemap.active)
+	do_facet mgs $LCTL nodemap_activate 1
+	wait_nm_sync active
+	stack_trap "do_facet mgs $LCTL nodemap_activate $act; \
+		    wait_nm_sync active"
+
+	local suid=$($cmd nodemap.default.squash_uid)
+	do_facet mgs "$LCTL nodemap_modify --name default \
+		--property squash_uid --value $TSTID"
+	wait_nm_sync default squash_uid
+	stack_trap "do_facet mgs $LCTL nodemap_modify --name default \
+		--property squash_uid --value $suid"
+
+	$LFS setquota -P -B100M $MOUNT &&
+		error "Set default quotas with squashed uid"
+	is_project_quota_supported &&
+		$LFS quota -P $MOUNT ||
+			error "Can't get default prj quota for squashed uid"
+	$LFS quota -U $MOUNT ||
+		error "Can't get default usr quota for squashed uid"
+	$LFS quota -G $MOUNT ||
+		error "Can't get default grp quota for squashed uid"
+}
+run_test 89 "Show default quota with squash_uid"
 
 check_quota_no_mount()
 {