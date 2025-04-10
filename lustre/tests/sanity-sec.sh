lustre/lustre-release

commit db818bd0401038cbfac65bfd2f6f7a006d7fa150

LU-17431 nodemap: allow modifying properties of a dynamic nm
Modify properties of a dynamic nodemap, by sending the appropriate
ioctl to the MDS or OSS device.
On kernel side, we need to allow handling a nodemap even if the
nodemap config file is not accessible (i.e. we are not on the
MGS). This dynamic nodemap is not stored on disk.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I7253e51ecb8869c2d2ac268926c1c907005ec4a7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54513
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanity-sec.sh
@@ -6794,6 +6794,9 @@ test_72() {
 	local endnid=1.1.1.100@tcp
 	local clid=500
 	local fsid=1000
+	local properties="audit_mode deny_unknown forbid_encryption \
+			  readonly_mount"
+	local sepol="1:mls:31:40afb76d077c441b69af58cccaaa2ca63641ed6e21b0a887dc21a684f508b78f"
 	local val
 
 	(( OST1_VERSION >= $(version_code 2.15.64) )) ||
@@ -6801,6 +6804,13 @@ test_72() {
 
 	[[ "$(facet_active_host mgs)" != "$(facet_active_host ost1)" ]] ||
 		skip "Need servers on different hosts"
+
+	activedefault=$(do_facet mgs $LCTL get_param -n nodemap.active)
+	if [[ "$activedefault" != "1" ]]; then
+		do_facet mgs $LCTL nodemap_activate 1
+		wait_nm_sync active
+		stack_trap cleanup_active EXIT
+	fi
 
 	do_facet mgs $LCTL nodemap_add $mgsnm ||
 		error "adding $mgsnm on MGS failed"
@@ -6810,7 +6820,7 @@ test_72() {
 	do_facet mgs $LCTL nodemap_add_idmap --name $mgsnm --idtype uid \
 		--idmap $mgsclid:$mgsfsid ||
 		error "add_idmap for $mgsnm on MGS failed"
-	wait_nm_sync $mgsnm idmap '' inactive
+	wait_nm_sync $mgsnm idmap
 
 	stack_trap "do_facet ost1 $LCTL nodemap_del $nm || true" EXIT
 	do_facet ost1 $LCTL nodemap_add $nm &&
@@ -6849,6 +6859,74 @@ test_72() {
 		error "dynamic nodemap wrong fs id $val"
 	fi
 
+	for prop in $properties; do
+		do_facet ost1 $LCTL nodemap_modify --name $nm \
+			--property $prop --value 1 ||
+				error "dynamic modify of $prop failed"
+		val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+		[[ "x$val" == "x1" ]] || error "incorrect $prop $val"
+	done
+	prop=admin
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value 1 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.admin_nodemap)
+	[[ "x$val" == "x1" ]] || error "incorrect $prop $val"
+	prop=trusted
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value 0 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.trusted_nodemap)
+	[[ "x$val" == "x0" ]] || error "incorrect $prop $val"
+	prop=map_mode
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value uid ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "xuid" ]] || error "incorrect $prop $val"
+	prop=rbac
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value file_perms ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "xfile_perms" ]] || error "incorrect $prop $val"
+	prop=squash_uid
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value 77 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	prop=squash_gid
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value 77 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	prop=squash_projid
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value 77 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	prop=fileset
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset "/tmp" ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x/tmp" ]] || error "incorrect $prop $val"
+	prop=sepol
+	do_facet ost1 $LCTL nodemap_set_sepol --name $nm \
+		--sepol $sepol ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x$sepol" ]] || error "incorrect $prop $val"
+
+	val=$(do_facet ost1 $LCTL nodemap_test_id --nid $startnid \
+		--idtype uid --id $clid)
+	if [[ "x$val" != "x$fsid" ]]; then
+		error "dynamic test_id on server failed"
+	fi
+
 	do_facet ost1 $LCTL nodemap_del_idmap --name $nm --idtype uid \
 		--idmap $clid:$fsid ||
 			error "dynamic del_idmap on server failed"
@@ -6856,6 +6934,11 @@ test_72() {
 		awk 'BEGIN{RS=", "} $1=="client_id:"{print $2 ; exit}')
 	if [[ "x$val" != "x" ]]; then
 		error "idmap should be empty, got $val"
+	fi
+
+	val=$(do_facet ost1 $LCTL nodemap_test_nid $startnid)
+	if [[ "x$val" != "x$nm" ]]; then
+		error "dynamic test_nid on server failed"
 	fi
 
 	do_facet ost1 $LCTL nodemap_del_range --name $nm --range $nids ||
@@ -6883,6 +6966,9 @@ test_72() {
 	do_facet ost1 $LCTL nodemap_del_idmap --name $mgsnm --idtype uid \
 		--idmap $mgsclid:$mgsfsid &&
 			error "del_idmap $mgsnm on server should fail"
+	do_facet ost1 $LCTL nodemap_modify --name $mgsnm \
+		--property squash_projid --value 77 &&
+			error "modify $mgsnm on server should fail"
 	do_facet ost1 $LCTL nodemap_del $mgsnm &&
 		error "nodemap del $mgsnm on server should fail"
 	do_facet ost1 $LCTL get_param -R 'nodemap.*'

lustre/utils/lctl.c
@@ -515,8 +515,8 @@ command_t cmdlist[] = {
 	 "delete a range from a nodemap\n"
 	 "usage: nodemap_del_range --name NODEMAP_NAME --range NID_RANGE"},
 	{"nodemap_modify", jt_nodemap_modify, 0,
-	 "modify a nodemap parameters\n"
-	 "usage: nodemap_modify nodemap_name param value"},
+	 "modify a nodemap parameter\n"
+	 "usage: nodemap_modify --name NODEMAP_NAME --property PROPERTY_NAME --value VALUE"},
 	{"nodemap_add_offset", jt_nodemap_add_offset, 0,
 	 "add an offset for UID/GID/PROJID mappings\n"
 	 "usage: nodemap_add_offset --name NODEMAP_NAME --offset OFFSET --limit LIMIT\n"},
@@ -531,14 +531,14 @@ command_t cmdlist[] = {
 	 "usage: nodemap_del_idmap --name NODEMAP_NAME --idtype ID_TYPE --idmap CLIENTID:FSID"},
 	{"nodemap_set_fileset", jt_nodemap_set_fileset, 0,
 	 "set a fileset on a nodemap\n"
-	 "usage: nodemap_set_fileset <fileset>"},
+	 "usage: nodemap_set_fileset --name NODEMAP_NAME --fileset FILESET"},
 	{"nodemap_set_sepol", jt_nodemap_set_sepol, 0,
 	 "set SELinux policy info on a nodemap\n"
-	 "usage: nodemap_set_sepol <SELinux policy info>"},
+	 "usage: nodemap_set_sepol --name NODEMAP_NAME --sepol SEPOL"},
 	{"nodemap_test_nid", jt_nodemap_test_nid, 0,
-	 "usage: nodemap_test_nid <nid>"},
+	 "usage: nodemap_test_nid NID"},
 	{"nodemap_test_id", jt_nodemap_test_id, 0,
-	 "Usage: nodemap_test_id --nid <nid> --idtype [uid|gid] --id <id>"},
+	 "Usage: nodemap_test_id --nid NID --idtype ID_TYPE --id ID"},
 	{"nodemap_info", jt_nodemap_info, 0,
 	 "Usage: nodemap_info [list|nodemap_name|all]"},
 

lustre/utils/obd.c
@@ -4138,11 +4138,36 @@ del_usage:
  */
 int jt_nodemap_test_nid(int argc, char **argv)
 {
-	char	rawbuf[MAX_IOC_BUFLEN];
-	int	rc;
+	char rawbuf[MAX_IOC_BUFLEN];
+	char *nid = NULL;
+	int c, rc;
 
-	rc = nodemap_cmd(LCFG_NODEMAP_TEST_NID, false, &rawbuf, sizeof(rawbuf),
-			 argv[0], argv[1], NULL);
+	static struct option long_opts[] = {
+		{ .val = 'h', .name = "help",	 .has_arg = no_argument },
+		{ .name = NULL } };
+
+	while ((c = getopt_long(argc, argv, "dh",
+				long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+		default:
+			goto test_nid_usage;
+		}
+	}
+
+	if (optind < argc)
+		nid = argv[optind];
+
+	if (!nid) {
+		fprintf(stderr, "nodemap_test_nid: missing NID\n");
+test_nid_usage:
+		fprintf(stderr,
+			"usage: nodemap_test_nid NID\n");
+		return -EINVAL;
+	}
+
+	rc = nodemap_cmd(LCFG_NODEMAP_TEST_NID, false, &rawbuf,
+			 sizeof(rawbuf), argv[0], nid, NULL);
 	if (rc == 0)
 		printf("%s\n", (char *)rawbuf);
 
@@ -4163,39 +4188,53 @@ int jt_nodemap_test_nid(int argc, char **argv)
  */
 int jt_nodemap_test_id(int argc, char **argv)
 {
-	char	rawbuf[MAX_IOC_BUFLEN];
-	char	*nidstr = NULL;
-	char	*idstr = NULL;
-	char	*typestr = NULL;
-	int	rc = 0;
-	int	c;
+	char rawbuf[MAX_IOC_BUFLEN];
+	char *nidstr = NULL;
+	char *idstr = NULL;
+	char *typestr = NULL;
+	int c, rc = 0;
 
 	static struct option long_opts[] = {
+		{ .val = 'h',	.name = "help", .has_arg = no_argument },
 		{ .val = 'i',	.name = "id",	.has_arg = required_argument },
 		{ .val = 'n',	.name = "nid",	.has_arg = required_argument },
 		{ .val = 't',	.name = "idtype",
 						.has_arg = required_argument },
 		{ .name = NULL } };
 
-	while ((c = getopt_long(argc, argv, "n:t:i:",
+	while ((c = getopt_long(argc, argv, "hi:n:t:",
 				long_opts, NULL)) != -1) {
 		switch (c) {
+		case 'i':
+			idstr = optarg;
+			break;
 		case 'n':
 			nidstr = optarg;
 			break;
 		case 't':
 			typestr = optarg;
 			break;
-		case 'i':
-			idstr = optarg;
-			break;
+		case 'h':
+		default:
+			goto test_id_usage;
 		}
 	}
 
-	if (!nidstr || !typestr || !idstr) {
+	if (!nidstr) {
+		fprintf(stderr, "nodemap_test_id: missing NID\n");
+test_id_usage:
 		fprintf(stderr,
-			"usage: nodemap_test_id --nid <nid> --idtype [uid|gid] --id <id>\n");
-		return -1;
+			"usage: nodemap_test_id --nid NID --idtype IDTYPE --id ID\n");
+		return -EINVAL;
+	}
+	if (!typestr) {
+		fprintf(stderr,
+			"nodemap_test_id: missing idtype, must be one of uid, gid, projid\n");
+		goto test_id_usage;
+	}
+	if (!idstr) {
+		fprintf(stderr, "nodemap_test_id: missing id\n");
+		goto test_id_usage;
 	}
 
 	rc = nodemap_cmd(LCFG_NODEMAP_TEST_ID, false, &rawbuf, sizeof(rawbuf),
@@ -4404,30 +4443,39 @@ int jt_nodemap_set_fileset(int argc, char **argv)
 {
 	char *nodemap_name = NULL;
 	char *fileset_name = NULL;
-	int   rc = 0;
-	int   c;
+	int c, rc = 0;
 
 	static struct option long_opts[] = {
 	{ .val = 'f',	.name = "fileset",	.has_arg = required_argument },
+	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
 	{ .val = 'n',	.name = "name",		.has_arg = required_argument },
 	{ .name = NULL } };
 
-	while ((c = getopt_long(argc, argv, "n:f:",
+	while ((c = getopt_long(argc, argv, "f:hn:",
 				long_opts, NULL)) != -1) {
 		switch (c) {
-		case 'n':
-			nodemap_name = optarg;
-			break;
 		case 'f':
 			fileset_name = optarg;
 			break;
+		case 'n':
+			nodemap_name = optarg;
+			break;
+		case 'h':
+		default:
+			goto set_fileset_usage;
 		}
 	}
 
-	if (!nodemap_name || !fileset_name) {
+	if (!nodemap_name) {
+		fprintf(stderr, "nodemap_set_fileset: missing nodemap name\n");
+set_fileset_usage:
 		fprintf(stderr,
-			"usage: nodemap_set_fileset --name <name> --fileset <fileset>\n");
-		return -1;
+			"usage: nodemap_set_fileset --name NODEMAP_NAME --fileset FILESET\n");
+		return -EINVAL;
+	}
+	if (!fileset_name) {
+		fprintf(stderr, "nodemap_set_fileset: missing NID range\n");
+		goto set_fileset_usage;
 	}
 
 	rc = nodemap_cmd(LCFG_NODEMAP_SET_FILESET, false, NULL, 0, argv[0],
@@ -4457,10 +4505,14 @@ int jt_nodemap_set_sepol(int argc, char **argv)
 {
 	char *nodemap_name = NULL;
 	char *sepol = NULL;
-	int   rc = 0;
-	int   c;
+	int c, rc = 0;
 
 	static struct option long_options[] = {
+		{
+			.name		= "help",
+			.has_arg	= no_argument,
+			.val		= 'h',
+		},
 		{
 			.name		= "name",
 			.has_arg	= required_argument,
@@ -4476,7 +4528,7 @@ int jt_nodemap_set_sepol(int argc, char **argv)
 		}
 	};
 
-	while ((c = getopt_long(argc, argv, "n:s:",
+	while ((c = getopt_long(argc, argv, "hn:s:",
 				long_options, NULL)) != -1) {
 		switch (c) {
 		case 'n':
@@ -4485,13 +4537,22 @@ int jt_nodemap_set_sepol(int argc, char **argv)
 		case 's':
 			sepol = optarg;
 			break;
+		case 'h':
+		default:
+			goto set_sepol_usage;
 		}
 	}
 
-	if (!nodemap_name || !sepol) {
+	if (!nodemap_name) {
+		fprintf(stderr, "nodemap_set_sepol: missing nodemap name\n");
+set_sepol_usage:
 		fprintf(stderr,
-			"usage: nodemap_set_sepol --name <name> --sepol <sepol>\n");
-		return -1;
+			"usage: nodemap_set_sepol --name NODEMAP_NAME --sepol SEPOL\n");
+		return -EINVAL;
+	}
+	if (!sepol) {
+		fprintf(stderr, "nodemap_set_sepol: missing sepol\n");
+		goto set_sepol_usage;
 	}
 
 	rc = nodemap_cmd(LCFG_NODEMAP_SET_SEPOL, false, NULL, 0, argv[0],
@@ -4521,20 +4582,20 @@ int jt_nodemap_set_sepol(int argc, char **argv)
  */
 int jt_nodemap_modify(int argc, char **argv)
 {
-	int			c;
-	int			rc = 0;
 	enum lcfg_command_type	cmd = 0;
-	char			*nodemap_name = NULL;
-	char			*param = NULL;
-	char			*value = NULL;
+	char *nodemap_name = NULL;
+	char *param = NULL;
+	char *value = NULL;
+	int c, rc = 0;
 
 	static struct option long_opts[] = {
+	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
 	{ .val = 'n',	.name = "name",		.has_arg = required_argument },
 	{ .val = 'p',	.name = "property",	.has_arg = required_argument },
 	{ .val = 'v',	.name = "value",	.has_arg = required_argument },
 	{ .name = NULL } };
 
-	while ((c = getopt_long(argc, argv, "n:p:v:",
+	while ((c = getopt_long(argc, argv, "hn:p:v:",
 				long_opts, NULL)) != -1) {
 		switch (c) {
 		case 'n':
@@ -4546,15 +4607,29 @@ int jt_nodemap_modify(int argc, char **argv)
 		case 'v':
 			value = optarg;
 			break;
+		case 'h':
+		default:
+			goto modify_usage;
 		}
 	}
 
-	if (!nodemap_name || !param || !value) {
+	if (!nodemap_name) {
+		fprintf(stderr, "nodemap_modify: missing nodemap name\n");
+modify_usage:
 		fprintf(stderr,
-			"usage: nodemap_modify --name <nodemap_name> --property <property_name> --value <value>\n");
+			"usage: %s --name NODEMAP_NAME --property PROPERTY_NAME --value VALUE\n",
+			argv[0]);
 		fprintf(stderr,
 			"valid properties: admin trusted map_mode squash_uid squash_gid squash_projid deny_unknown audit_mode forbid_encryption readonly_mount rbac\n");
-		return -1;
+		return -EINVAL;
+	}
+	if (!param) {
+		fprintf(stderr, "nodemap_modify: missing property name\n");
+		goto modify_usage;
+	}
+	if (!value) {
+		fprintf(stderr, "nodemap_modify: missing value for property\n");
+		goto modify_usage;
 	}
 
 	if (strcmp("admin", param) == 0) {
@@ -4581,9 +4656,9 @@ int jt_nodemap_modify(int argc, char **argv)
 		cmd = LCFG_NODEMAP_RBAC;
 	} else {
 		fprintf(stderr,
-			"error: %s: nodemap_modify invalid subcommand: %s\n",
+			"error: %s: nodemap_modify invalid property: %s\n",
 			jt_cmdname(argv[0]), param);
-		return -1;
+		goto modify_usage;
 	}
 
 	rc = nodemap_cmd(cmd, false, NULL, 0, argv[0], nodemap_name, param,

commit 976a14cd8c5c805b10294a921eec7a943d80bf47

LU-17431 nodemap: modify all properties of a dynamic nm
All properties of a dynamic nodemap must be taken into account. So
move the check for dynamic in nodemap_idx_* functions, except for
nodemap_activate which is independent of any nodemap.
Update sanity-sec test_72 to correctly exercise this.

Fixes: db818bd040 ("LU-17431 nodemap: allow modifying properties of a dynamic nm")
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I5fb7877669fdf7689b1be1f065006644f0a3f15c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58088
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_nodemap.h
@@ -135,7 +135,7 @@ struct nm_config_file {
 	struct list_head		 ncf_list;
 };
 
-void nodemap_activate(const bool value);
+int nodemap_activate(const bool value);
 int nodemap_add(const char *nodemap_name, bool dynamic);
 int nodemap_del(const char *nodemap_name);
 int nodemap_add_member(struct lnet_nid *nid, struct obd_export *exp);

lustre/ptlrpc/nodemap_handler.c
@@ -550,8 +550,7 @@ int nodemap_add_idmap_helper(struct lu_nodemap *nodemap,
 
 		del_map[0] = temp->id_client;
 		idmap_delete(id_type, temp, nodemap);
-		if (!nodemap->nm_dyn)
-			rc = nodemap_idx_idmap_del(nodemap, id_type, del_map);
+		rc = nodemap_idx_idmap_del(nodemap, id_type, del_map);
 		/* In case there is any corrupted idmap */
 		if (!rc || unlikely(rc == -ENOENT)) {
 			temp = idmap_insert(id_type, idmap, nodemap);
@@ -610,7 +609,7 @@ int nodemap_add_idmap(const char *nodemap_name, enum nodemap_id_type id_type,
 		GOTO(out_unlock, rc = -EPERM);
 
 	rc = nodemap_add_idmap_helper(nodemap, id_type, map);
-	if (!rc && !nodemap->nm_dyn)
+	if (!rc)
 		rc = nodemap_idx_idmap_add(nodemap, id_type, map);
 
 out_unlock:
@@ -661,8 +660,7 @@ int nodemap_del_idmap(const char *nodemap_name, enum nodemap_id_type id_type,
 		rc = -EINVAL;
 	} else {
 		idmap_delete(id_type, idmap, nodemap);
-		if (!nodemap->nm_dyn)
-			rc = nodemap_idx_idmap_del(nodemap, id_type, map);
+		rc = nodemap_idx_idmap_del(nodemap, id_type, map);
 	}
 	up_write(&nodemap->nm_idmap_lock);
 
@@ -1057,8 +1055,8 @@ int nodemap_add_range_helper(struct nodemap_config *config,
 	up_write(&config->nmc_range_tree_lock);
 
 	/* if range_id is non-zero, we are loading from disk */
-	if (range_id == 0 && !nodemap->nm_dyn)
-		rc = nodemap_idx_range_add(range);
+	if (range_id == 0)
+		rc = nodemap_idx_range_add(nodemap, range);
 
 	if (config == active_config) {
 		nm_member_revoke_locks(config->nmc_default_nodemap);
@@ -1111,9 +1109,9 @@ EXPORT_SYMBOL(nodemap_add_range);
 int nodemap_del_range(const char *name, const struct lnet_nid nid[2],
 		      u8 netmask)
 {
-	struct lu_nodemap	*nodemap;
-	struct lu_nid_range	*range;
-	int			rc = 0;
+	struct lu_nodemap *nodemap;
+	struct lu_nid_range *range;
+	int rc = 0;
 
 	mutex_lock(&active_config_lock);
 	nodemap = nodemap_lookup(name);
@@ -1138,8 +1136,7 @@ int nodemap_del_range(const char *name, const struct lnet_nid nid[2],
 		up_write(&active_config->nmc_range_tree_lock);
 		GOTO(out_putref, rc = -EINVAL);
 	}
-	if (!nodemap->nm_dyn)
-		rc = nodemap_idx_range_del(range);
+	rc = nodemap_idx_range_del(nodemap, range);
 	range_delete(active_config, range);
 	nm_member_reclassify_nodemap(nodemap);
 	up_write(&active_config->nmc_range_tree_lock);
@@ -2070,8 +2067,7 @@ int nodemap_del(const char *nodemap_name)
 	down_write(&active_config->nmc_range_tree_lock);
 	list_for_each_entry_safe(range, range_temp, &nodemap->nm_ranges,
 				 rn_list) {
-		if (!nodemap->nm_dyn)
-			rc2 = nodemap_idx_range_del(range);
+		rc2 = nodemap_idx_range_del(nodemap, range);
 		if (rc2 < 0)
 			rc = rc2;
 
@@ -2087,11 +2083,9 @@ int nodemap_del(const char *nodemap_name)
 		nodemap_fileset_reset(nodemap);
 	}
 
-	if (!nodemap->nm_dyn) {
-		rc2 = nodemap_idx_nodemap_del(nodemap);
-		if (rc2 < 0)
-			rc = rc2;
-	}
+	rc2 = nodemap_idx_nodemap_del(nodemap);
+	if (rc2 < 0)
+		rc = rc2;
 
 	/*
 	 * remove procfs here in case nodemap_create called with same name
@@ -2319,16 +2313,25 @@ out:
  *
  * \param	value		1 for on, 0 for off
  */
-void nodemap_activate(const bool value)
+int nodemap_activate(const bool value)
 {
+	int rc = 0;
+
+	if (!nodemap_mgs()) {
+		CERROR("cannot activate for non-existing MGS.\n");
+		return -EINVAL;
+	}
+
 	mutex_lock(&active_config_lock);
 	active_config->nmc_nodemap_is_active = value;
 
 	/* copy active value to global to avoid locking in map functions */
 	nodemap_active = value;
-	nodemap_idx_nodemap_activate(value);
+	rc = nodemap_idx_nodemap_activate(value);
 	mutex_unlock(&active_config_lock);
 	nm_member_revoke_all();
+
+	return rc;
 }
 EXPORT_SYMBOL(nodemap_activate);
 
@@ -2887,14 +2890,14 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 		    strcasecmp(param, "y") == 0 ||
 		    strcasecmp(param, "true") == 0 ||
 		    strcasecmp(param, "t") == 0)
-			nodemap_activate(1);
+			rc = nodemap_activate(1);
 		else if (strcmp(param, "0") == 0 ||
 			 strcasecmp(param, "off") == 0 ||
 			 strcasecmp(param, "no") == 0 ||
 			 strcasecmp(param, "n") == 0 ||
 			 strcasecmp(param, "false") == 0 ||
 			 strcasecmp(param, "f") == 0)
-			nodemap_activate(0);
+			rc = nodemap_activate(0);
 		else
 			rc = -EINVAL;
 		break;

lustre/ptlrpc/nodemap_internal.h
@@ -198,8 +198,10 @@ int nodemap_idx_idmap_add(const struct lu_nodemap *nodemap,
 int nodemap_idx_idmap_del(const struct lu_nodemap *nodemap,
 			  enum nodemap_id_type id_type,
 			  const __u32 map[2]);
-int nodemap_idx_range_add(const struct lu_nid_range *range);
-int nodemap_idx_range_del(const struct lu_nid_range *range);
+int nodemap_idx_range_add(struct lu_nodemap *nodemap,
+			  const struct lu_nid_range *range);
+int nodemap_idx_range_del(struct lu_nodemap *nodemap,
+			  const struct lu_nid_range *range);
 int nodemap_idx_nodemap_activate(bool value);
 int nodemap_index_read(struct lu_env *env, struct nm_config_file *ncf,
 		       struct idx_info *ii, const struct lu_rdpg *rdpg);

lustre/ptlrpc/nodemap_lproc.c
@@ -435,9 +435,9 @@ static ssize_t
 nodemap_active_seq_write(struct file *file, const char __user *buffer,
 			 size_t count, loff_t *off)
 {
-	char			active_string[NODEMAP_LDEBUGFS_FLAG_LEN + 1];
-	long unsigned int	active;
-	int			rc;
+	char active_string[NODEMAP_LDEBUGFS_FLAG_LEN + 1];
+	unsigned long active;
+	int rc;
 
 	if (count == 0)
 		return 0;
@@ -453,9 +453,9 @@ nodemap_active_seq_write(struct file *file, const char __user *buffer,
 	if (rc != 0)
 		return -EINVAL;
 
-	nodemap_activate(active);
+	rc = nodemap_activate(active);
 
-	return count;
+	return rc ? rc : count;
 }
 LDEBUGFS_SEQ_FOPS(nodemap_active);
 

lustre/ptlrpc/nodemap_storage.c
@@ -596,17 +596,20 @@ int nodemap_idx_nodemap_update(const struct lu_nodemap *nodemap)
 
 int nodemap_idx_nodemap_del(const struct lu_nodemap *nodemap)
 {
-	struct rb_root		 root;
-	struct lu_idmap		*idmap;
-	struct lu_idmap		*temp;
-	struct lu_nid_range	*range;
-	struct lu_nid_range	*range_temp;
-	struct nodemap_key	 nk;
-	struct lu_env		 env;
-	int			 rc = 0;
-	int			 rc2 = 0;
+	struct rb_root root;
+	struct lu_idmap *idmap;
+	struct lu_idmap *temp;
+	struct lu_nid_range *range;
+	struct lu_nid_range *range_temp;
+	struct nodemap_key nk;
+	struct lu_env env;
+	int rc = 0;
+	int rc2 = 0;
 
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
 
 	if (!nodemap_mgs()) {
 		CERROR("cannot del nodemap config from non-existing MGS.\n");
@@ -703,6 +706,9 @@ int nodemap_idx_cluster_roles_del(const struct lu_nodemap *nodemap)
 
 	ENTRY;
 
+	if (nodemap->nm_dyn)
+		return 0;
+
 	if (!nodemap_mgs()) {
 		CERROR("cannot add nodemap config to non-existing MGS.\n");
 		return -EINVAL;
@@ -732,6 +738,9 @@ int nodemap_idx_offset_del(const struct lu_nodemap *nodemap)
 	int rc = 0;
 
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
 
 	if (!nodemap_mgs()) {
 		CERROR("cannot add nodemap config to non-existing MGS.\n");
@@ -1167,7 +1176,8 @@ int nodemap_idx_fileset_clear(const struct lu_nodemap *nodemap)
 	return rc;
 }
 
-int nodemap_idx_range_add(const struct lu_nid_range *range)
+int nodemap_idx_range_add(struct lu_nodemap *nodemap,
+			  const struct lu_nid_range *range)
 {
 	struct nodemap_key nk;
 	union nodemap_rec nr;
@@ -1175,6 +1185,10 @@ int nodemap_idx_range_add(const struct lu_nid_range *range)
 	int rc = 0;
 
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
+
 	if (!nodemap_mgs()) {
 		CERROR("cannot add nodemap config to non-existing MGS.\n");
 		return -EINVAL;
@@ -1198,12 +1212,17 @@ free_env:
 	RETURN(rc);
 }
 
-int nodemap_idx_range_del(const struct lu_nid_range *range)
+int nodemap_idx_range_del(struct lu_nodemap *nodemap,
+			  const struct lu_nid_range *range)
 {
-	struct nodemap_key	 nk;
-	struct lu_env		 env;
-	int			 rc = 0;
+	struct nodemap_key nk;
+	struct lu_env env;
+	int rc = 0;
+
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
 
 	if (!nodemap_mgs()) {
 		CERROR("cannot del nodemap config from non-existing MGS.\n");
@@ -1227,11 +1246,15 @@ int nodemap_idx_idmap_add(const struct lu_nodemap *nodemap,
 			  enum nodemap_id_type id_type,
 			  const u32 map[2])
 {
-	struct nodemap_key	 nk;
-	union nodemap_rec	 nr;
-	struct lu_env		 env;
-	int			 rc = 0;
+	struct nodemap_key nk;
+	union nodemap_rec nr;
+	struct lu_env env;
+	int rc = 0;
+
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
 
 	if (!nodemap_mgs()) {
 		CERROR("cannot add idmap to non-existing MGS.\n");
@@ -1255,10 +1278,14 @@ int nodemap_idx_idmap_del(const struct lu_nodemap *nodemap,
 			  enum nodemap_id_type id_type,
 			  const u32 map[2])
 {
-	struct nodemap_key	 nk;
-	struct lu_env		 env;
-	int			 rc = 0;
+	struct nodemap_key nk;
+	struct lu_env env;
+	int rc = 0;
+
 	ENTRY;
+
+	if (nodemap->nm_dyn)
+		return 0;
 
 	if (!nodemap_mgs()) {
 		CERROR("cannot del idmap from non-existing MGS.\n");
@@ -1279,14 +1306,15 @@ int nodemap_idx_idmap_del(const struct lu_nodemap *nodemap,
 
 static int nodemap_idx_global_add_update(bool value, enum nm_add_update update)
 {
-	struct nodemap_key	 nk;
-	union nodemap_rec	 nr;
-	struct lu_env		 env;
-	int			 rc = 0;
+	struct nodemap_key nk;
+	union nodemap_rec nr;
+	struct lu_env env;
+	int rc = 0;
+
 	ENTRY;
 
 	if (!nodemap_mgs()) {
-		CERROR("cannot add nodemap config to non-existing MGS.\n");
+		CERROR("cannot do global for non-existing MGS.\n");
 		return -EINVAL;
 	}
 

lustre/tests/sanity-sec.sh
@@ -7114,6 +7114,7 @@ test_72() {
 	local properties="audit_mode deny_unknown forbid_encryption \
 			  readonly_mount"
 	local sepol="1:mls:31:40afb76d077c441b69af58cccaaa2ca63641ed6e21b0a887dc21a684f508b78f"
+	local rbac_val
 	local val
 
 	(( OST1_VERSION >= $(version_code 2.15.64) )) ||
@@ -7138,6 +7139,8 @@ test_72() {
 		--idmap $mgsclid:$mgsfsid ||
 		error "add_idmap for $mgsnm on MGS failed"
 	wait_nm_sync $mgsnm idmap
+
+	rbac_val=$(do_facet mgs $LCTL get_param -n nodemap.$mgsnm.rbac)
 
 	stack_trap "do_facet ost1 $LCTL nodemap_del $nm || true" EXIT
 	do_facet ost1 $LCTL nodemap_add $nm &&
@@ -7207,6 +7210,11 @@ test_72() {
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
 	[[ "x$val" == "xfile_perms" ]] || error "incorrect $prop $val"
+	do_facet ost1 $LCTL nodemap_modify --name $nm \
+		--property $prop --value all ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
+	[[ "x$val" == "x$rbac_val" ]] || error "incorrect $prop $val"
 	prop=squash_uid
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 77 ||
@@ -7237,6 +7245,24 @@ test_72() {
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
 	[[ "x$val" == "x$sepol" ]] || error "incorrect $prop $val"
+	prop=offset
+	do_facet ost1 $LCTL nodemap_add_offset --name $nm \
+		--offset 100000 --limit 200000 ||
+			error "dynamic modify of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
+		awk '$1 == "start_uid:" {print $2}' | sed s+,++)
+	[[ "x$val" == "x100000" ]] || error "incorrect $prop start_uid $val"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
+		awk '$1 == "limit_uid:" {print $2}' | sed s+,++)
+	[[ "x$val" == "x200000" ]] || error "incorrect $prop limit_uid $val"
+	do_facet ost1 $LCTL nodemap_del_offset --name $nm ||
+			error "dynamic del of $prop failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
+		awk '$1 == "start_uid:" {print $2}' | sed s+,++)
+	[[ "x$val" == "x0" ]] || error "incorrect $prop start_uid $val"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
+		awk '$1 == "limit_uid:" {print $2}' | sed s+,++)
+	[[ "x$val" == "x0" ]] || error "incorrect $prop limit_uid $val"
 
 	val=$(do_facet ost1 $LCTL nodemap_test_id --nid $startnid \
 		--idtype uid --id $clid)