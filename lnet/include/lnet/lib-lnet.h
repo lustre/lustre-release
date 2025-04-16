lustre/lustre-release

commit 4e48761a57193279ce3f3d5170c3e38cf287b59a

LU-9120 lnet: refactor lnet_select_pathway()
lnet_select_pathway() is a complex monolithic function which handles
many send cases. Broke down lnet_select_pathway() to multiple
functions. Each function handles a different send case. This will
make it easier to add the handling of the different health cases in
future patches.

Test-Parameters: forbuildonly
Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Change-Id: I6e554c71eaa61f3e1bdfdc60bd9cd38f70df57b5
Reviewed-on: https://review.whamcloud.com/32760
Reviewed-by: Sonia Sharma <sharmaso@whamcloud.com>
Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
Tested-by: Jenkins
Reviewed-by: Chris Horn <hornc@cray.com>

lnet/include/lnet/lib-lnet.h
@@ -937,6 +937,19 @@ lnet_is_peer_healthy_locked(struct lnet_peer *peer)
 	return false;
 }
 
+static inline struct lnet_peer_net *
+lnet_find_peer_net_locked(struct lnet_peer *peer, __u32 net_id)
+{
+	struct lnet_peer_net *peer_net;
+
+	list_for_each_entry(peer_net, &peer->lp_peer_nets, lpn_peer_nets) {
+		if (peer_net->lpn_net_id == net_id)
+			return peer_net;
+	}
+
+	return NULL;
+}
+
 static inline void
 lnet_peer_set_alive(struct lnet_peer_ni *lp)
 {

commit df6a7264dcd08d18d5e4ea11a5ad9d99c2101fdb

LU-16518 lnet: remove unused lnet_find_peer_net_locked()
This function is no longer used, so let's drop it.

Fixes: 4e48761a5719 ("LU-9120 lnet: refactor lnet_select_pathway()")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I71ab22793e63577f7e0fb88dfc14cddcbc32e17e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58552
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1064,20 +1064,6 @@ lnet_peer_net_set_sel_priority_locked(struct lnet_peer_net *lpn, __u32 priority)
 	lpn->lpn_sel_priority = priority;
 }
 
-
-static inline struct lnet_peer_net *
-lnet_find_peer_net_locked(struct lnet_peer *peer, __u32 net_id)
-{
-	struct lnet_peer_net *peer_net;
-
-	list_for_each_entry(peer_net, &peer->lp_peer_nets, lpn_peer_nets) {
-		if (peer_net->lpn_net_id == net_id)
-			return peer_net;
-	}
-
-	return NULL;
-}
-
 static inline bool
 lnet_peer_is_multi_rail(struct lnet_peer *lp)
 {