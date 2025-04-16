lustre/lustre-release

commit 6e76d3569bf602d71745b8151417cff7ddfd7df4

LU-12511 lnet: rework lnd module loading
The kernel IS_MODULE() only works in the positive, ie no ifndef.
So rework the LND module loading code to handle if modules are
enabled.

Test-Parameters: trivial testlist=sanity-lnet
Change-Id: Ie97b00b6b5c21d6e20acf72f796707630369251e
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57946
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -2606,21 +2606,22 @@ static const struct lnet_lnd *lnet_load_lnd(u32 lnd_type)
 	mutex_lock(&the_lnet.ln_lnd_mutex);
 	lnd = lnet_find_lnd_by_type(lnd_type);
 	if (!lnd) {
+#ifdef HAVE_MODULE_LOADING_SUPPORT
 		mutex_unlock(&the_lnet.ln_lnd_mutex);
 		rc = request_module("%s", libcfs_lnd2modname(lnd_type));
 		mutex_lock(&the_lnet.ln_lnd_mutex);
 
 		lnd = lnet_find_lnd_by_type(lnd_type);
 		if (!lnd) {
-			mutex_unlock(&the_lnet.ln_lnd_mutex);
 			CERROR("Can't load LND %s, module %s, rc=%d\n",
 			libcfs_lnd2str(lnd_type),
 			libcfs_lnd2modname(lnd_type), rc);
-#ifndef HAVE_MODULE_LOADING_SUPPORT
-			LCONSOLE_ERROR("Your kernel must be compiled with kernel module loading support.");
-#endif
-			return ERR_PTR(-EINVAL);
+			lnd = ERR_PTR(rc);
 		}
+#else
+		LCONSOLE_ERROR("Your kernel must be compiled with kernel module loading support.");
+		lnd = ERR_PTR(-EINVAL);
+#endif
 	}
 	mutex_unlock(&the_lnet.ln_lnd_mutex);
 

commit 969a680544e630183c5cbbb5b38effe505459bc0

LU-18893 lnet: Use negative errno for ERR_PTR
request_module() can return a positive value from modprobe. If this
happens then we need to provide a negative errno for ERR_PTR().

Test-Parameters: trivial testlist=sanity-lnet
Fixes: 6e76d3569b ("LU-12511 lnet: rework lnd module loading ")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I400aa5b350fe274d18c2edb38479bb428907efb6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58669
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/lnet/api-ni.c
@@ -2617,6 +2617,8 @@ static const struct lnet_lnd *lnet_load_lnd(u32 lnd_type)
 			CERROR("Can't load LND %s, module %s, rc=%d\n",
 			libcfs_lnd2str(lnd_type),
 			libcfs_lnd2modname(lnd_type), rc);
+			if (rc >= 0)
+				rc = -EINVAL;
 			lnd = ERR_PTR(rc);
 		}
 #else

lustre/tests/sanity-lnet.sh
@@ -4211,6 +4211,34 @@ test_260() {
 }
 run_test 260 "test that linux sysctl parameter are set correctly"
 
+test_280() {
+	local rc=0
+
+	modinfo ksocklnd 2>/dev/null || rc=$?
+
+	((rc == 1)) || skip "Need request_module to fail"
+
+	local lnd=""
+
+	case $NETTYPE in
+		o2ib*) lnd=ko2iblnd;;
+		tcp*) lnd=ksocklnd;;
+		kfi*) lnd=kkfilnd;;
+		gni*) lnd=kgnilnd;;
+	esac
+
+	[[ -n $lnd ]] || skip "Unsupported NETTYPE $NETTYPE"
+
+	load_lnet || error "Failed to load lnet"
+
+	$LUSTRE_RMMOD $lnd || error "Failed to unload $lnd"
+
+	$LNETCTL lnet configure -a
+
+	$LUSTRE_RMMOD
+}
+run_test 280 "Don't panic when request_module fails"
+
 test_300() {
 	# LU-13274
 	local header