lustre/lustre-release

commit 30b356a28b5094015963e83457a196584c799558

LU-14296 lnet: use an unbound cred in kiblnd_resolve_addr()
In kiblnd_resolve_addr() call prepare_kernel_cred(NULL) rather than
prepare_creds() to get a cred with unbound capabilities.

Fixes: 1e4bd16acfa ("LU-14006 o2ib: raise bind cap before resolving address")
Signed-off-by: John L. Hammond <jhammond@whamcloud.com>
Change-Id: Ib2500d82aa4eab5590fa95ae73fd75b2ab542bcd
Reviewed-on: https://review.whamcloud.com/41137
Reviewed-by: Amir Shehata <ashehata@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1317,8 +1317,6 @@ kiblnd_resolve_addr_cap(struct rdma_cm_id *cmid,
         unsigned short port;
         int rc;
 
-	LASSERT(capable(CAP_NET_BIND_SERVICE));
-
         /* allow the port to be reused */
         rc = rdma_set_reuseaddr(cmid, 1);
         if (rc != 0) {
@@ -1344,8 +1342,9 @@ kiblnd_resolve_addr_cap(struct rdma_cm_id *cmid,
                 }
         }
 
-        CERROR("Failed to bind to a free privileged port\n");
-        return rc;
+	CERROR("cannot bind to a free privileged port: rc = %d\n", rc);
+
+	return rc;
 }
 
 static int
@@ -1359,7 +1358,7 @@ kiblnd_resolve_addr(struct rdma_cm_id *cmid,
 	int rc;
 
 	if (!capable(CAP_NET_BIND_SERVICE)) {
-		new_creds = prepare_creds();
+		new_creds = prepare_kernel_cred(NULL);
 		if (!new_creds)
 			return -ENOMEM;
 

commit 498c1c10d4ae05f9d46b19331c41d339c0e7dab0

LU-18888 o2ib: prepare_kernel_cred with &init_task
Linux v6.1-rc1-12-g5a17f040fa33
  cred: Do not default to init_cred in prepare_kernel_cred()

This breaks kiblnd_resolve_addr() by interpreting the NULL as
an -ENOMEM instead of an invalid task being used.

The new pattern is to pass &init_task to get init_cred instead
of NULL.

This needs no special handling for older kernels.

HPE-bug-id: LUS-12806
Fixes: 30b356a28b ("LU-14296 lnet: use an unbound cred in kiblnd_resolve_addr()")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I31caa7f5adb23265985fba6db7b28d48975e7e9d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58654
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1425,7 +1425,7 @@ kiblnd_resolve_addr(struct rdma_cm_id *cmid,
 	int rc;
 
 	if (!capable(CAP_NET_BIND_SERVICE)) {
-		new_creds = prepare_kernel_cred(NULL);
+		new_creds = prepare_kernel_cred(&init_task);
 		if (!new_creds)
 			return -ENOMEM;
 