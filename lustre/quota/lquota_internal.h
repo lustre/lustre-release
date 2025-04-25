lustre/lustre-release

commit 78be823f33396819724330d7154f054c52e11944

LU-15218 quota: delete unused quota ID
Add lfs option '--delete' to delete unused quota ID.

Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I0d8e6b61dc23c7b22b6054bcced087b8dc94a277
Reviewed-on: https://review.whamcloud.com/45548
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/lquota_internal.h
@@ -186,7 +186,8 @@ struct lquota_entry {
 			lqe_gl:1,	  /* glimpse is in progress */
 			lqe_nopreacq:1,	  /* pre-acquire disabled */
 			lqe_is_default:1, /* the default quota is used */
-			lqe_is_global:1;  /* lqe belongs to global pool "0x0"*/
+			lqe_is_global:1,  /* lqe belongs to global pool "0x0"*/
+			lqe_is_deleted:1; /* lqe will be deleted soon */
 
 	struct lqe_glbl_data	*lqe_glbl_data;
 };
@@ -456,6 +457,18 @@ struct lquota_entry *lqe_locate_find(const struct lu_env *,
 				     struct lquota_site *,
 				     union lquota_id *, bool);
 
+static inline void lqe_set_deleted(struct lquota_entry *lqe)
+{
+	lqe->lqe_enforced = 0;
+	lqe->lqe_edquot = 0;
+	lqe->lqe_is_default = 0;
+	lqe->lqe_hardlimit = 0;
+	lqe->lqe_softlimit = 0;
+	lqe->lqe_gracetime = 0;
+
+	lqe->lqe_is_deleted = 1;
+}
+
 /* lquota_disk.c */
 struct dt_object *lquota_disk_dir_find_create(const struct lu_env *,
 					      struct dt_device *,
@@ -484,6 +497,8 @@ int lquota_disk_declare_write(const struct lu_env *, struct thandle *,
 int lquota_disk_write(const struct lu_env *, struct thandle *,
 		      struct dt_object *, union lquota_id *, struct dt_rec *,
 		      __u32, __u64 *);
+int lquota_disk_delete(const struct lu_env *env, struct thandle *th,
+		       struct dt_object *obj, __u64 qid, __u64 *ver);
 int lquota_disk_update_ver(const struct lu_env *, struct dt_device *,
 			   struct dt_object *, __u64);
 int lquota_disk_write_glb(const struct lu_env *, struct dt_object *, __u64,

lustre/quota/qmt_handler.c
@@ -335,9 +335,74 @@ static int qmt_set(const struct lu_env *env, struct qmt_device *qmt,
 	if (IS_ERR(lqe))
 			RETURN(PTR_ERR(lqe));
 
+	lqe->lqe_is_deleted = 0;
 	rc = qmt_set_with_lqe(env, qmt, lqe, hard, soft, time, valid,
 			      is_default, is_updated);
+	if (rc == 0)
+		lqe->lqe_is_deleted = 0;
+
 	lqe_putref(lqe);
+	RETURN(rc);
+}
+
+/*
+ * Delete the quota setting of the specified quota ID
+ *
+ * \param env        - is the environment passed by the caller
+ * \param qmt        - is the quota master target
+ * \param restype    - is the pool type, either block (i.e. LQUOTA_RES_DT) or
+ *                     inode (i.e. LQUOTA_RES_MD)
+ * \param qtype      - is the quota type
+ * \param qid        - is the quota indentifier for which we want to delete its
+ *                     quota settings.
+ */
+static int qmt_delete_qid(const struct lu_env *env, struct qmt_device *qmt,
+			  __u8 restype, __u8 qtype, __u64 qid)
+{
+	struct qmt_thread_info *qti = qmt_info(env);
+	union lquota_id	*quota_id = &qti->qti_id;
+	struct thandle *th = NULL;
+	struct qmt_pool_info *qpi = NULL;
+	struct lquota_entry *lqe = NULL;
+	__u64 ver = 0;
+	int rc;
+
+	ENTRY;
+
+	quota_id->qid_uid = qid;
+	lqe = qmt_pool_lqe_lookup(env, qmt, restype, qtype, quota_id, NULL);
+	if (IS_ERR(lqe))
+		RETURN(PTR_ERR(lqe));
+
+	lqe_write_lock(lqe);
+
+	qpi = qmt_pool_lookup_glb(env, qmt, restype);
+	if (IS_ERR(qpi))
+		GOTO(out, rc = -ENOMEM);
+
+	th = qmt_trans_start(env, lqe);
+	if (IS_ERR(th))
+		GOTO(out, rc = PTR_ERR(th));
+
+	rc = lquota_disk_delete(env, th,
+				qpi->qpi_glb_obj[qtype], qid, &ver);
+
+	dt_trans_stop(env, qmt->qmt_child, th);
+
+	if (rc == 0) {
+		lqe_set_deleted(lqe);
+		qmt_glb_lock_notify(env, lqe, ver);
+	} else if (rc == -ENOENT) {
+		rc = 0;
+	}
+
+out:
+	if (!IS_ERR_OR_NULL(qpi))
+		qpi_putref(env, qpi);
+
+	lqe_write_unlock(lqe);
+	lqe_putref(lqe);
+
 	RETURN(rc);
 }
 
@@ -480,6 +545,16 @@ static int qmt_quotactl(const struct lu_env *env, struct lu_device *ld,
 				     dqb->dqb_bsoftlimit, dqb->dqb_btime,
 				     dqb->dqb_valid & QIF_BFLAGS, is_default,
 				     false, poolname);
+		break;
+
+	case LUSTRE_Q_DELETEQID:
+		rc = qmt_delete_qid(env, qmt, LQUOTA_RES_MD, oqctl->qc_type,
+				    oqctl->qc_id);
+		if (rc)
+			break;
+
+		rc = qmt_delete_qid(env, qmt, LQUOTA_RES_DT, oqctl->qc_type,
+				    oqctl->qc_id);
 		break;
 
 	default:

lustre/quota/qmt_lock.c
@@ -793,6 +793,11 @@ void qmt_glb_lock_notify(const struct lu_env *env, struct lquota_entry *lqe,
 		qti->qti_gl_desc.lquota_desc.gl_time = LQUOTA_GRACE_FLAG(0,
 							LQUOTA_FLAG_DEFAULT);
 
+	} else if (lqe->lqe_is_deleted) {
+		qti->qti_gl_desc.lquota_desc.gl_hardlimit = 0;
+		qti->qti_gl_desc.lquota_desc.gl_softlimit = 0;
+		qti->qti_gl_desc.lquota_desc.gl_time = LQUOTA_GRACE_FLAG(0,
+							LQUOTA_FLAG_DELETED);
 	} else {
 		qti->qti_gl_desc.lquota_desc.gl_hardlimit = lqe->lqe_hardlimit;
 		qti->qti_gl_desc.lquota_desc.gl_softlimit = lqe->lqe_softlimit;

lustre/quota/qsd_writeback.c
@@ -292,11 +292,47 @@ static int qsd_process_upd(const struct lu_env *env, struct qsd_upd_rec *upd)
 			return 1;
 	}
 
+	if (upd->qur_global &&
+	    (LQUOTA_FLAG(upd->qur_rec.lqr_glb_rec.qbr_time) &
+							LQUOTA_FLAG_DELETED)) {
+		struct thandle		*th = NULL;
+		struct dt_object	*obj;
+
+		obj = qqi->qqi_glb_obj;
+
+		th = dt_trans_create(env, qqi->qqi_qsd->qsd_dev);
+		if (IS_ERR(th))
+			RETURN(PTR_ERR(th));
+
+		rc = lquota_disk_declare_write(env, th, obj, &upd->qur_qid);
+		if (rc)
+			GOTO(out_del, rc);
+
+		rc = dt_trans_start_local(env, qqi->qqi_qsd->qsd_dev, th);
+		if (rc)
+			GOTO(out_del, rc);
+
+		rc = lquota_disk_delete(env, th, obj, upd->qur_qid.qid_uid,
+					NULL);
+		if (rc == -ENOENT)
+			rc = 0;
+
+out_del:
+		dt_trans_stop(env, qqi->qqi_qsd->qsd_dev, th);
+		if (lqe != NULL)
+			lqe_set_deleted(lqe);
+
+		qsd_bump_version(qqi, upd->qur_ver, true);
+		RETURN(rc);
+	}
+
 	if (lqe == NULL) {
 		lqe = lqe_locate(env, qqi->qqi_site, &upd->qur_qid);
 		if (IS_ERR(lqe))
 			GOTO(out, rc = PTR_ERR(lqe));
 	}
+
+	lqe->lqe_is_deleted = 0;
 
 	/* The in-memory lqe update for slave index copy isn't deferred,
 	 * we shouldn't touch it here. */

lustre/doc/lfs-setquota.1
@@ -15,13 +15,16 @@ lfs setquota \- set quota limits or grace time for users, groups or projects.
        [\fB--inode-grace\fR|\fB-i\fR \fIINODE_GRACE_TIME\fR] <\fIfilesystem\fR>
 .TP
 .BR "lfs setquota " { -u | --user | -g | --group | -p | --projid "} " \fIUID\fR|\fIGID\fR|\fIPROJID\fR
-       [\fB--default|-d\fR] <\fIfilesystem\fR>
+       [\fB--default|-D\fR] <\fIfilesystem\fR>
 .TP
 .BR "lfs setquota " { -U | --default-usr | -G | --default-grp | -P | --default-prj }
        [\fB--block-softlimit\fR|\fB-b\fR \fIBLOCK_SOFTLIMIT\fR[\fBkMGTPE\fR]]
        [\fB--block-hardlimit\fR|\fB-B\fR \fIBLOCK_HARDLIMIT\fR[\fBkMGTPE\fR]]
        [\fB--inode-softlimit\fR|\fB-i\fR \fIINODE_SOFTLIMIT\fR[\fBkMGTPE\fR]]
        [\fB--inode-hardlimit\fR|\fB-I\fR \fIINODE_HARDLIMIT\fR[\fBkMGTPE\fR]] <\fIfilesystem\fR>
+.TP
+.BR "lfs setquota " { -u | --user | -g | --group | -p | --projid "} " \fIUID\fR|\fIGID\fR|\fIPROJID\fR
+       [\fB--delete\fR] <\fIfilesystem\fR>
 .TP
 .SH DESCRIPTION
 .TP
@@ -41,7 +44,10 @@ Specify block softlimit, zero means unlimited.
 Specify block hardlimit, zero means unlimited. The block hardlimit should be
 greater than block softlimit when it's being specified.
 .TP
-.BR -d|--default
+.BR --delete
+Delete the unused UID|GID|PROJID.
+.TP
+.BR -D|--default
 Set user/group/project to use the default quota limits.
 .TP
 .BR -g | --group \fIGROUPNAME\fR|\fIGID
@@ -127,7 +133,7 @@ Set default project quota limit.
 .B $ lfs setquota -u bob --block-softlimit 2G --block-hardlimit 1G /mnt/lustre
 Set quotas of user `bob': 1GB block hardlimit and 2 GB block softlimit
 .TP
-.B $ lfs setquota -u bob -d /mnt/lustre
+.B $ lfs setquota -u bob -D /mnt/lustre
 Set quotas of user `bob' to use default quota setting
 .TP
 .B $ lfs setquota -U --block-softlimit 1G --block-hardlimit 2G /mnt/lustre
@@ -142,6 +148,15 @@ Set hard block limit 1G for user 'ivan' per pool 'flash_pool'
 .TP
 .B $ lfs setquota -t -u --block-grace 1000 --pool flash_pool /mnt/lustre
 Set grace time 1000 seconds for block quotas per pool 'flash_pool'
+.TP
+.BR "lfs setquota " { -u | -g | -p "} " \fIUID\fR|\fIGID\fR|\fIPROJID\fR " " [\fB--delete\fR] " " <\fIfilesystem\fR>
+Command deletes the unused UID|GID|PROJID from Quota settings.
+.TP
+.PP
+.SH EXAMPLES
+.TP
+.B $ lfs setquota -u bob --delete /mnt/lustre
+Delete unused user 'bob'.
 .SH SEE ALSO
 .BR lfs (1),
 .BR lfs-quota(1)

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1291,6 +1291,7 @@ static inline __u64 toqb(__kernel_size_t space)
 #define LUSTRE_Q_SETINFOPOOL	0x800012  /* set pool quota info */
 #define LUSTRE_Q_GETDEFAULT_POOL	0x800013  /* get default pool quota*/
 #define LUSTRE_Q_SETDEFAULT_POOL	0x800014  /* set default pool quota */
+#define LUSTRE_Q_DELETEQID	0x800015  /* delete quota ID */
 /* In the current Lustre implementation, the grace time is either the time
  * or the timestamp to be used after some quota ID exceeds the soft limt,
  * 48 bits should be enough, its high 16 bits can be used as quota flags.
@@ -1315,6 +1316,7 @@ static inline __u64 toqb(__kernel_size_t space)
  * and high 16 bits will contain this flag (see above comment).
  * */
 #define LQUOTA_FLAG_DEFAULT	0x0001
+#define LQUOTA_FLAG_DELETED	0x0002
 
 #define LUSTRE_Q_CMD_IS_POOL(cmd)		\
 	(cmd == LUSTRE_Q_GETQUOTAPOOL ||	\

lustre/llite/dir.c
@@ -1188,6 +1188,7 @@ int quotactl_ioctl(struct super_block *sb, struct if_quotactl *qctl)
 	case LUSTRE_Q_SETQUOTAPOOL:
 	case LUSTRE_Q_SETINFOPOOL:
 	case LUSTRE_Q_SETDEFAULT_POOL:
+	case LUSTRE_Q_DELETEQID:
 		if (!capable(CAP_SYS_ADMIN))
 			RETURN(-EPERM);
 

lustre/mdt/mdt_handler.c
@@ -3219,6 +3219,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 	case LUSTRE_Q_SETQUOTAPOOL:
 	case LUSTRE_Q_SETINFOPOOL:
 	case LUSTRE_Q_SETDEFAULT_POOL:
+	case LUSTRE_Q_DELETEQID:
 		if (!nodemap_can_setquota(nodemap, oqctl->qc_type,
 					  oqctl->qc_id))
 			GOTO(out_nodemap, rc = -EPERM);
@@ -3289,6 +3290,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 	case LUSTRE_Q_GETINFOPOOL:
 	case LUSTRE_Q_SETDEFAULT_POOL:
 	case LUSTRE_Q_GETDEFAULT_POOL:
+	case LUSTRE_Q_DELETEQID:
 		/* forward quotactl request to QMT */
 		rc = qmt_hdls.qmth_quotactl(tsi->tsi_env, qmt, oqctl);
 		break;

commit 3eca1e2d8d09652b968428e780766d61d0435926

LU-18875 quota: change lqe only under lock
All fields of an lqe, especially flags, must be changed only under a
lock protection. The flags like lqe_gl, lqe_is_reset, lqe_edquot are
the part of an unsigned long(8 bytes) memory region in lquota_entry.
It is impossible to change only 1 bit in a memory, so the general way
is to "mov" 1 byte to some register(AL), change it(AND, OR) in this
register and "mov" back to the memory. It is a window for a race and
might cause a corruption of the neighbour flags. So this patch adds a
locking for remain places where the flags or any lqe members are
changed without locking. One of known issues this patch is aimed to
sort out is the following assertion:

 (qmt_lock.c:973:qmt_id_lock_glimpse()) ASSERTION( lqe->lqe_gl )

Restore one old assertion in qmt_id_lock_glimpse that was removed by
mistake in 16ee9ecdb0 - qmt_reba_thread() is the only thread that can
change lqe_gl flag. Also check that lqe_gl is  not set when we are
freeing an lqe.

Fixes: 16ee9ecdb0 ("LU-16913 quota: fix ASSERTION(lqe->lqe_gl)")
Fixes: 78be823f33 ("LU-15218 quota: delete unused quota ID")
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I954ed8aa23be01846e479a64c376c4b37af05268
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58612
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/lquota_internal.h
@@ -453,6 +453,7 @@ struct lquota_entry *lqe_locate_find(const struct lu_env *,
 
 static inline void lqe_set_deleted(struct lquota_entry *lqe)
 {
+	lqe_write_lock(lqe);
 	lqe->lqe_enforced = 0;
 	lqe->lqe_edquot = 0;
 	lqe->lqe_is_default = 0;
@@ -461,6 +462,7 @@ static inline void lqe_set_deleted(struct lquota_entry *lqe)
 	lqe->lqe_gracetime = 0;
 
 	lqe->lqe_is_deleted = 1;
+	lqe_write_unlock(lqe);
 }
 
 /* lquota_disk.c */

lustre/quota/lquota_lib.c
@@ -41,6 +41,7 @@ void lqe_ref_free(struct kref *kref)
 	struct lquota_entry *lqe = container_of(kref, struct lquota_entry,
 						lqe_ref);
 
+	LASSERT(!lqe->lqe_gl);
 	OBD_SLAB_FREE_PTR(lqe, lqe_kmem);
 }
 

lustre/quota/qmt_handler.c
@@ -326,13 +326,12 @@ static int qmt_set(const struct lu_env *env, struct qmt_device *qmt,
 	if (IS_ERR(lqe))
 			RETURN(PTR_ERR(lqe));
 
+	lqe_write_lock(lqe);
 	lqe->lqe_is_deleted = 0;
 	lqe->lqe_is_reset = 0;
+	lqe_write_unlock(lqe);
 	rc = qmt_set_with_lqe(env, qmt, lqe, hard, soft, time, valid,
 			      is_default, is_updated);
-	if (rc == 0)
-		lqe->lqe_is_deleted = 0;
-
 	lqe_putref(lqe);
 	RETURN(rc);
 }
@@ -377,11 +376,8 @@ static int qmt_delete_qid(const struct lu_env *env, struct qmt_device *qmt,
 	if (CFS_FAIL_CHECK(OBD_FAIL_QUOTA_NOSYNC))
 		th->th_sync = 0;
 
-	lqe_write_lock(lqe);
 	rc = lquota_disk_delete(env, th,
 				qpi->qpi_glb_obj[qtype], qid, &ver);
-
-	lqe_write_unlock(lqe);
 	dt_trans_stop(env, qmt->qmt_child, th);
 
 	if (rc == 0) {

lustre/quota/qmt_lock.c
@@ -401,9 +401,12 @@ int qmt_lvbo_update(struct lu_device *ld, struct ldlm_resource *res,
 	need_revoke = qmt_clear_lgeg_arr_nu(lqe, stype, idx);
 	if (lvb->lvb_id_rel == 0) {
 		/* nothing to release */
-		if (lvb->lvb_id_may_rel != 0)
+		if (lvb->lvb_id_may_rel != 0) {
 			/* but might still release later ... */
+			lqe_write_lock(lqe);
 			lqe->lqe_may_rel += lvb->lvb_id_may_rel;
+			lqe_write_unlock(lqe);
+		}
 	}
 
 	if (!need_revoke && lvb->lvb_id_rel == 0)
@@ -942,12 +945,6 @@ static void qmt_id_lock_glimpse(const struct lu_env *env,
 	}
 
 	lqe_write_lock(lqe);
-	/*
-	 * It is possible to add an lqe in a 2nd time while the same lqe
-	 * from the 1st time is still sending glimpse
-	 */
-	if (lqe->lqe_gl)
-		GOTO(out, 0);
 	/* The purpose of glimpse callback on per-ID lock is twofold:
 	 * - notify slaves of new qunit value and hope they will release some
 	 *   spare quota space in return
@@ -966,6 +963,8 @@ static void qmt_id_lock_glimpse(const struct lu_env *env,
 		 * replies if needed */
 		lqe->lqe_may_rel = 0;
 
+	/* The rebalance thread is the only thread which can issue glimpses */
+	LASSERT(!lqe->lqe_gl);
 	lqe->lqe_gl = true;
 	lqe_write_unlock(lqe);
 
@@ -982,7 +981,6 @@ static void qmt_id_lock_glimpse(const struct lu_env *env,
 	}
 	LASSERT(lqe->lqe_gl);
 	lqe->lqe_gl = false;
-out:
 	lqe_write_unlock(lqe);
 	ldlm_resource_putref(res);
 	EXIT;

lustre/quota/qsd_entry.c
@@ -223,10 +223,12 @@ int qsd_refresh_usage(const struct lu_env *env, struct lquota_entry *lqe)
 		rc = 0;
 		break;
 	case 0:
+		lqe_write_lock(lqe);
 		if (qqi->qqi_qsd->qsd_is_md)
 			lqe->lqe_usage = rec->ispace;
 		else
 			lqe->lqe_usage = toqb(rec->bspace);
+		lqe_write_unlock(lqe);
 		break;
 	default:
 		LQUOTA_ERROR(lqe, "failed to read disk usage, rc:%d", rc);

lustre/quota/qsd_writeback.c
@@ -323,8 +323,10 @@ out_del:
 			GOTO(out, rc = PTR_ERR(lqe));
 	}
 
+	lqe_write_lock(lqe);
 	lqe->lqe_is_reset = false;
 	lqe->lqe_is_deleted = 0;
+	lqe_write_unlock(lqe);
 
 	/* The in-memory lqe update for slave index copy isn't deferred,
 	 * we shouldn't touch it here. */
@@ -340,12 +342,17 @@ out_del:
 		spin_unlock(&qsd->qsd_adjust_lock);
 
 		if (LQUOTA_FLAG(upd->qur_rec.lqr_glb_rec.qbr_time) &
-							LQUOTA_FLAG_REVOKE)
+							LQUOTA_FLAG_REVOKE) {
+			lqe_write_lock(lqe);
 			lqe->lqe_revoke = 1;
+			lqe_write_unlock(lqe);
+		}
 
 		/* Report usage asynchronously */
 		rc = qsd_adjust(env, lqe);
+		lqe_write_lock(lqe);
 		lqe->lqe_revoke = 0;
+		lqe_write_unlock(lqe);
 		if (rc)
 			LQUOTA_ERROR(lqe, "failed to report usage, rc:%d", rc);
 	}
@@ -358,12 +365,14 @@ out:
 	    upd->qur_rec.lqr_glb_rec.qbr_hardlimit == 0 &&
 	    (LQUOTA_FLAG(upd->qur_rec.lqr_glb_rec.qbr_time) &
 							LQUOTA_FLAG_DEFAULT)) {
+		lqe_write_lock(lqe);
 		lqe->lqe_is_default = true;
 		if (qqi->qqi_default_softlimit == 0 &&
 		    qqi->qqi_default_hardlimit == 0)
 			lqe->lqe_enforced = false;
 		else
 			lqe->lqe_enforced = true;
+		lqe_write_unlock(lqe);
 
 		LQUOTA_DEBUG(lqe, "update to use default quota");
 	}
@@ -372,11 +381,13 @@ out:
 							LQUOTA_FLAG_RESET)) {
 		struct lquota_slv_rec srec;
 
+		lqe_write_lock(lqe);
 		lqe->lqe_granted = 0;
 		lqe->lqe_softlimit = 0;
 		lqe->lqe_hardlimit = 0;
 		lqe->lqe_is_default = false;
 		lqe->lqe_is_reset = true;
+		lqe_write_unlock(lqe);
 
 		memset(&srec, 0, sizeof(srec));
 		rc = qsd_update_index(env, qqi, &upd->qur_qid, false, 0, &srec);