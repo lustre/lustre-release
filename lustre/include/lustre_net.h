lustre/lustre-release

commit 911b6167a37c2dba835baab4e100dcf206c03113

LU-3810 obd: Cleanup client import if client_obd_setup fails
client_obd_setup() allocates an obd_import which should be cleaned up
if there is any failure afterwards in callers of client_obd_setup().
This patch fixes the bug in osc_setup(), mgc_setup(), mdc_setup() and
lwp_setup(). The fix is to call obd_cleanup_client_import() before
calling client_obd_cleanup() in case of an error.

Signed-off-by: Swapnil Pimpale <spimpale@ddn.com>
Change-Id: Ib8c2194d80fd14538d4ca42fb8084c93dd575199
Reviewed-on: http://review.whamcloud.com/7561
Reviewed-by: John L. Hammond <john.hammond@intel.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Lai Siyao <lai.siyao@intel.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>

lustre/ldlm/ldlm_lib.c
@@ -473,15 +473,16 @@ EXPORT_SYMBOL(client_obd_setup);
 
 int client_obd_cleanup(struct obd_device *obddev)
 {
-        ENTRY;
+	ENTRY;
 
-        ldlm_namespace_free_post(obddev->obd_namespace);
-        obddev->obd_namespace = NULL;
+	ldlm_namespace_free_post(obddev->obd_namespace);
+	obddev->obd_namespace = NULL;
 
-        LASSERT(obddev->u.cli.cl_import == NULL);
+	obd_cleanup_client_import(obddev);
+	LASSERT(obddev->u.cli.cl_import == NULL);
 
-        ldlm_put_ref();
-        RETURN(0);
+	ldlm_put_ref();
+	RETURN(0);
 }
 EXPORT_SYMBOL(client_obd_cleanup);
 

lustre/mgc/mgc_request.c
@@ -893,27 +893,27 @@ static int mgc_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 {
 	struct lprocfs_static_vars	lvars;
 	int				rc;
-        ENTRY;
+	ENTRY;
 
 	rc = ptlrpcd_addref();
 	if (rc < 0)
 		RETURN(rc);
 
-        rc = client_obd_setup(obd, lcfg);
-        if (rc)
-                GOTO(err_decref, rc);
+	rc = client_obd_setup(obd, lcfg);
+	if (rc)
+		GOTO(err_decref, rc);
 
 	rc = mgc_llog_init(NULL, obd);
-        if (rc) {
-                CERROR("failed to setup llogging subsystems\n");
-                GOTO(err_cleanup, rc);
-        }
+	if (rc) {
+		CERROR("failed to setup llogging subsystems\n");
+		GOTO(err_cleanup, rc);
+	}
 
-        lprocfs_mgc_init_vars(&lvars);
-        lprocfs_obd_setup(obd, lvars.obd_vars);
-        sptlrpc_lprocfs_cliobd_attach(obd);
+	lprocfs_mgc_init_vars(&lvars);
+	lprocfs_obd_setup(obd, lvars.obd_vars);
+	sptlrpc_lprocfs_cliobd_attach(obd);
 
-        if (cfs_atomic_inc_return(&mgc_count) == 1) {
+	if (cfs_atomic_inc_return(&mgc_count) == 1) {
 		rq_state = 0;
 		init_waitqueue_head(&rq_waitq);
 
@@ -928,15 +928,15 @@ static int mgc_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 		}
 		/* rc is the task_struct pointer of mgc_requeue_thread. */
 		rc = 0;
-        }
+	}
 
-        RETURN(rc);
+	RETURN(rc);
 
 err_cleanup:
-        client_obd_cleanup(obd);
+	client_obd_cleanup(obd);
 err_decref:
-        ptlrpcd_decref();
-        RETURN(rc);
+	ptlrpcd_decref();
+	RETURN(rc);
 }
 
 /* based on ll_mdc_blocking_ast */

lustre/osp/lwp_dev.c
@@ -145,6 +145,7 @@ out:
 		lustre_cfg_free(lcfg);
 	if (rc)
 		client_obd_cleanup(lwp->lpd_obd);
+
 	RETURN(rc);
 }
 
@@ -320,8 +321,6 @@ static struct lu_device *lwp_device_fini(const struct lu_env *env,
 		ptlrpc_free_rq_pool(imp->imp_rq_pool);
 		imp->imp_rq_pool = NULL;
 	}
-
-	obd_cleanup_client_import(m->lpd_obd);
 
 	LASSERT(m->lpd_obd);
 	ptlrpc_lprocfs_unregister_obd(m->lpd_obd);

lustre/osp/osp_dev.c
@@ -745,7 +745,6 @@ out_last_used:
 out_proc:
 	ptlrpc_lprocfs_unregister_obd(obd);
 	lprocfs_obd_cleanup(obd);
-	obd_cleanup_client_import(obd);
 	if (m->opd_symlink)
 		lprocfs_remove(&m->opd_symlink);
 	client_obd_cleanup(obd);
@@ -824,8 +823,6 @@ static struct lu_device *osp_device_fini(const struct lu_env *env,
 		ptlrpc_free_rq_pool(imp->imp_rq_pool);
 		imp->imp_rq_pool = NULL;
 	}
-
-	obd_cleanup_client_import(m->opd_obd);
 
 	if (m->opd_symlink)
 		lprocfs_remove(&m->opd_symlink);

lustre/mgc/libmgc.c
@@ -53,33 +53,33 @@
 
 static int mgc_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 {
-        int rc;
-        ENTRY;
+	int rc;
+	ENTRY;
 
 	rc = ptlrpcd_addref();
 	if (rc < 0)
 		RETURN(rc);
 
-        rc = client_obd_setup(obd, lcfg);
-        if (rc)
-                GOTO(err_decref, rc);
+	rc = client_obd_setup(obd, lcfg);
+	if (rc)
+		GOTO(err_decref, rc);
 
-        /* liblustre only support null flavor to MGS */
-        obd->u.cli.cl_flvr_mgc.sf_rpc = SPTLRPC_FLVR_NULL;
+	/* liblustre only support null flavor to MGS */
+	obd->u.cli.cl_flvr_mgc.sf_rpc = SPTLRPC_FLVR_NULL;
 
-        rc = obd_llog_init(obd, &obd->obd_olg, obd, NULL);
-        if (rc) {
-                CERROR("failed to setup llogging subsystems\n");
-                GOTO(err_cleanup, rc);
-        }
+	rc = obd_llog_init(obd, &obd->obd_olg, obd, NULL);
+	if (rc) {
+		CERROR("failed to setup llogging subsystems\n");
+		GOTO(err_cleanup, rc);
+	}
 
-        RETURN(rc);
+	RETURN(rc);
 
 err_cleanup:
-        client_obd_cleanup(obd);
+	client_obd_cleanup(obd);
 err_decref:
-        ptlrpcd_decref();
-        RETURN(rc);
+	ptlrpcd_decref();
+	RETURN(rc);
 }
 
 static int mgc_precleanup(struct obd_device *obd, enum obd_cleanup_stage stage)

commit 9f0ce80e7c83eb584306709a34410a15fcea3bde

LU-18921 ldlm: make client_obd_cleanup return void
client_obd_cleanup() can never fail and it always returns
0. Make the function return void instead and fix all of the
callers.

Test-Parameters: trivial
Fixes: 911b6167a37c ("LU-3810 obd: Cleanup client import if client_obd_setup fail")
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I5a5987b4fafb712c35943620235620c6370a9f76
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58761
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Vandana Rungta <vrungta@amazon.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_net.h
@@ -2589,7 +2589,7 @@ ptlrpc_req2svc(struct ptlrpc_request *req)
  * @{
  */
 int client_obd_setup(struct obd_device *obd, struct lustre_cfg *lcfg);
-int client_obd_cleanup(struct obd_device *obd);
+void client_obd_cleanup(struct obd_device *obd);
 int client_connect_import(const struct lu_env *env,
 			  struct obd_export **exp, struct obd_device *obd,
 			  struct obd_uuid *cluuid, struct obd_connect_data *ocd,

lustre/ldlm/ldlm_lib.c
@@ -567,7 +567,7 @@ err:
 }
 EXPORT_SYMBOL(client_obd_setup);
 
-int client_obd_cleanup(struct obd_device *obd)
+void client_obd_cleanup(struct obd_device *obd)
 {
 	struct client_obd *cli = &obd->u.cli;
 
@@ -585,7 +585,7 @@ int client_obd_cleanup(struct obd_device *obd)
 		 BITS_TO_LONGS(OBD_MAX_RIF_MAX) * sizeof(long));
 	cli->cl_mod_tag_bitmap = NULL;
 
-	RETURN(0);
+	EXIT;
 }
 EXPORT_SYMBOL(client_obd_cleanup);
 

lustre/mgc/mgc_request.c
@@ -818,8 +818,6 @@ static int mgc_precleanup(struct obd_device *obd)
 
 static int mgc_cleanup(struct obd_device *obd)
 {
-	int rc;
-
 	ENTRY;
 
 	/* COMPAT_146 - old config logs may have added profiles secretly */
@@ -830,8 +828,8 @@ static int mgc_cleanup(struct obd_device *obd)
 	lprocfs_obd_cleanup(obd);
 	ptlrpcd_decref();
 
-	rc = client_obd_cleanup(obd);
-	RETURN(rc);
+	client_obd_cleanup(obd);
+	RETURN(0);
 }
 
 static int mgc_setup(struct obd_device *obd, struct lustre_cfg *lcfg)

lustre/osc/osc_request.c
@@ -4036,7 +4036,6 @@ static int osc_precleanup(struct obd_device *obd)
 int osc_cleanup_common(struct obd_device *obd)
 {
 	struct client_obd *cli = &obd->u.cli;
-	int rc;
 
 	ENTRY;
 
@@ -4057,11 +4056,9 @@ int osc_cleanup_common(struct obd_device *obd)
 
 	/* free memory of osc quota cache */
 	osc_quota_cleanup(obd);
-
-	rc = client_obd_cleanup(obd);
-
+	client_obd_cleanup(obd);
 	ptlrpcd_decref();
-	RETURN(rc);
+	RETURN(0);
 }
 EXPORT_SYMBOL(osc_cleanup_common);
 

lustre/osp/lwp_dev.c
@@ -369,9 +369,9 @@ static struct lu_device *lwp_device_alloc(const struct lu_env *env,
 static struct lu_device *lwp_device_fini(const struct lu_env *env,
 					 struct lu_device *ludev)
 {
-	struct lwp_device	*m = lu2lwp_dev(ludev);
-	struct task_struct	*task = NULL;
-	int			 rc;
+	struct lwp_device *m = lu2lwp_dev(ludev);
+	struct task_struct *task = NULL;
+
 	ENTRY;
 
 	task = xchg(&m->lpd_notify_task, NULL);
@@ -384,13 +384,9 @@ static struct lu_device *lwp_device_fini(const struct lu_env *env,
 		class_disconnect(m->lpd_exp);
 
 	LASSERT(m->lpd_obd);
-	rc = client_obd_cleanup(m->lpd_obd);
-	LASSERTF(rc == 0, "error %d\n", rc);
-
+	client_obd_cleanup(m->lpd_obd);
 	ptlrpc_lprocfs_unregister_obd(m->lpd_obd);
-
 	ptlrpcd_decref();
-
 	RETURN(NULL);
 }
 

lustre/osp/osp_dev.c
@@ -1370,7 +1370,6 @@ static struct lu_device *osp_device_fini(const struct lu_env *env,
 					 struct lu_device *ld)
 {
 	struct osp_device *osp = lu2osp_dev(ld);
-	int                rc;
 
 	ENTRY;
 
@@ -1392,10 +1391,9 @@ static struct lu_device *osp_device_fini(const struct lu_env *env,
 	 * the obd devices are already gone.
 	 */
 	osp_tunables_fini(osp);
-	rc = client_obd_cleanup(osp->opd_obd);
+	client_obd_cleanup(osp->opd_obd);
 	ptlrpcd_decref();
-
-	RETURN(rc != 0 ? ERR_PTR(rc) : NULL);
+	RETURN(NULL);
 }
 
 /**