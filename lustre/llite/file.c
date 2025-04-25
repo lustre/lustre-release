lustre/lustre-release

commit f7b1fea6bff50c190e9e644f86d709921aaa1974

LU-16518 lustre: fix implicit-fallthrough warnings
Explicitly lable switch-case intentional fallthroughs
and reorder cases as needed to silence implicit-fallthrough
warngins as reported by Clang.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I7322b3e4f37bcd1716ca48a31ce698798b58e6cf
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58348
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nrs_orr.c
@@ -1668,14 +1668,14 @@ LDEBUGFS_SEQ_FOPS(ptlrpc_lprocfs_nrs_orr_offset_type);
 static const char *nrs_orr_supp2str(enum nrs_orr_supp supp)
 {
 	switch(supp) {
-	default:
-		LBUG();
 	case NOS_OST_READ:
 		return LPROCFS_NRS_SUPP_NAME_READS;
 	case NOS_OST_WRITE:
 		return LPROCFS_NRS_SUPP_NAME_WRITES;
 	case NOS_OST_RW:
 		return LPROCFS_NRS_SUPP_NAME_READWRITES;
+	default:
+		LBUG();
 	}
 }
 

lustre/llite/vvp_io.c
@@ -64,8 +64,10 @@ static bool can_populate_pages(const struct lu_env *env, struct cl_io *io,
 			io->ci_continue = 0;
 			rc = false;
 		}
+		fallthrough;
 	case CIT_FAULT:
 		/* fault is okay because we've already had a page. */
+		fallthrough;
 	default:
 		break;
 	}

lustre/lod/lod_qos.c
@@ -2468,7 +2468,7 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 	case __swab32(LOV_USER_MAGIC_COMP_V1):
 		lustre_swab_lov_comp_md_v1(comp_v1);
 		magic = comp_v1->lcm_magic;
-		/* fall trhough */
+		fallthrough;
 	case LOV_USER_MAGIC_COMP_V1:
 		break;
 	case __swab32(LOV_USER_MAGIC_FOREIGN):

lustre/lov/lov_io.c
@@ -1890,8 +1890,6 @@ int lov_io_init_empty(const struct lu_env *env, struct cl_object *obj,
 
 	lio->lis_object = lov;
 	switch (io->ci_type) {
-	default:
-		LBUG();
 	case CIT_MISC:
 	case CIT_GLIMPSE:
 	case CIT_READ:
@@ -1912,6 +1910,8 @@ int lov_io_init_empty(const struct lu_env *env, struct cl_object *obj,
 		CERROR("Page fault on a file without stripes: "DFID"\n",
 		       PFID(lu_object_fid(&obj->co_lu)));
 		break;
+	default:
+		LBUG();
 	}
 	if (result == 0) {
 		cl_io_slice_add(io, &lio->lis_cl, obj, &lov_empty_io_ops);

lustre/mdd/mdd_object.c
@@ -3528,6 +3528,7 @@ mdd_layout_change(const struct lu_env *env, struct md_object *o,
 		if (intent->lai_opc == LAYOUT_INTENT_PCCRO_SET ||
 		    intent->lai_opc == LAYOUT_INTENT_PCCRO_CLEAR)
 			RETURN(mdd_layout_update_pccro(env, o, mlc));
+		break;
 	}
 	case MD_LAYOUT_RESYNC:
 	case MD_LAYOUT_RESYNC_DONE:

lustre/mgs/mgs_nids.c
@@ -578,6 +578,7 @@ int mgs_ir_update(const struct lu_env *env, struct mgs_device *mgs,
 		break;
 	case IR_DISABLED:
 		notify = false;
+		fallthrough;
 	case IR_STARTUP:
 	case IR_PARTIAL:
 		break;

lustre/obdclass/cl_io.c
@@ -100,6 +100,7 @@ void cl_io_fini(const struct lu_env *env, struct cl_io *io)
 		/* Check ignore layout change conf */
 		LASSERT(ergo(io->ci_ignore_layout || !io->ci_verify_layout,
 				!io->ci_need_restart));
+		break;
 	case CIT_GLIMPSE:
 		break;
 	case CIT_LADVISE:

lustre/obdclass/obd_config.c
@@ -467,14 +467,14 @@ static int class_parse_value(char *buf, int opc, void *value, char **endh,
 	tmp = *endp;
 	*endp = '\0';
 	switch (opc) {
-	default:
-		LBUG();
 	case CLASS_PARSE_NID:
 		rc = parse_nid(buf, value, quiet);
 		break;
 	case CLASS_PARSE_NET:
 		rc = parse_net(buf, value);
 		break;
+	default:
+		LBUG();
 	}
 	*endp = tmp;
 	if (rc != 0)

commit 51e5c0e80baf98043d181a376af1b4c1fb368c32

LU-18906 build: page_cache_alloc removed, missing return
Fix gcc -Wreturn-type
   nrs_orr.c:1680:1: error: control reaches end of non-void \
   function [-Werror=return-type]

Fix gcc -Wimplicit-function-declaration
   file.c:678:24: error: implicit declaration of function \
   page_cache_alloc [-Werror=implicit-function-declaration]

nodemap_idx_cluster_roles_modify() needs to be static to avoid
 error: no previous prototype for 'nodemap_idx_cluster_roles_modify' \
 [-Werror=missing-prototypes]

Test-Parameters: trivial
Fixes: f7b1fea6bff ("LU-16518 lustre: fix implicit-fallthrough warnings")
Fixes: 4a14a51a8e0 ("LU-18448 llite: read dir on open")
Fixes: 904a7f1e839 ("LU-17431 nodemap: introduce child_raise_privileges property")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I6d5131748efc7d7fcc51c4dc3b06070057cb0f0e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58724
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Robert Read <rread@ddn.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -675,7 +675,7 @@ void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
 	is_hash64 = test_bit(LL_SBI_64BIT_HASH, ll_i2sbi(inode)->ll_flags);
 
 	for (i = 0; i < npages; i++) {
-		page = page_cache_alloc(inode->i_mapping);
+		page = __page_cache_alloc(mapping_gfp_mask(inode->i_mapping));
 		if (!page)
 			continue;
 

lustre/ptlrpc/nodemap_handler.c
@@ -1887,6 +1887,7 @@ out:
 }
 EXPORT_SYMBOL(nodemap_set_mapping_mode);
 
+static
 int nodemap_idx_cluster_roles_modify(struct lu_nodemap *nodemap,
 				     enum nodemap_rbac_roles old_rbac,
 				     enum nodemap_raise_privs old_privs,

lustre/ptlrpc/nrs_orr.c
@@ -1676,6 +1676,7 @@ static const char *nrs_orr_supp2str(enum nrs_orr_supp supp)
 		return LPROCFS_NRS_SUPP_NAME_READWRITES;
 	default:
 		LBUG();
+		return NULL;
 	}
 }
 