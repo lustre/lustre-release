lustre/lustre-release

commit 74faf91f448bca86367ca08bfaa6e922f8e1b16b

LU-18687 compat: move xarray to lustre_compat
Migrate the backported xarray code to lustre_compat.
Along the way, create the needed build infrastructure
for lustre_compat. Currently, lustre_compat is built
into libcfs.ko.

Eventually, all of the Lustre/LNet compatability code
will live in lustre_compat - maintaining a clear
separation from the functional code in Lustre and LNet.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I74249d0b5714bee3549bf42a8fede3f279bc37ee
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58114
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

autoMakefile.am
@@ -13,12 +13,14 @@
 SUBDIRS := @LDISKFS_SUBDIR@ \
 	. \
 	@LUSTREIOKIT_SUBDIR@ \
+	lustre_compat \
 	libcfs \
 	lnet \
 	lustre
 
 DIST_SUBDIRS := ldiskfs \
 	lustre-iokit \
+	lustre_compat \
 	libcfs \
 	lnet \
 	lustre \

config/lustre-build.m4
@@ -433,6 +433,23 @@ AC_DEFUN([LB_CONFIG_FILES], [
 ])
 
 #
+# LB_COMPAT_CONFIG_FILES
+#
+# build-specific config files (lustre_compat)
+#
+AC_DEFUN([LB_COMPAT_CONFIG_FILES], [
+	AC_CONFIG_FILES(
+		lustre_compat/Makefile
+		lustre_compat/lib/Makefile
+		lustre_compat/autoMakefile
+		lustre_compat/lib/autoMakefile
+		lustre_compat/include/Makefile
+		lustre_compat/include/lustre_compat/Makefile
+		lustre_compat/include/lustre_compat/linux/Makefile
+	)
+])
+
+#
 # LB_CONFIG_SERVERS
 #
 AC_DEFUN([LB_CONFIG_SERVERS], [
@@ -692,6 +709,7 @@ LB_CONFIG_HEADERS
 LPLUG_CONFIGURE
 LIBCFS_CONFIG_FILES
 LB_CONFIG_FILES
+LB_COMPAT_CONFIG_FILES
 LN_CONFIG_FILES
 LC_CONFIG_FILES
 LPLUG_CONFIG_FILES

config/lustre-toolchain.m4
@@ -97,7 +97,7 @@ CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
 
 # everyone builds against lnet and lustre kernel headers
-EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include"
+EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include -I$PWD/lustre_compat/include"
 AC_SUBST(EXTRA_KCFLAGS)
 ]) # LTC_PROG_CC
 

libcfs/libcfs/Makefile.in
@@ -6,12 +6,18 @@
 
 MODULES = libcfs
 
+COMPAT := @top_srcdir@/lustre_compat/lib/
+
+libcfs_dir := $(dir $(lastword $(MAKEFILE_LIST)))
+-include $(libcfs_dir)/../../lustre_compat/lib/Makefile
+
 libcfs-linux-objs := linux-prim.o
 libcfs-linux-objs += linux-hash.o
 libcfs-linux-objs += linux-wait.o
 libcfs-linux-objs += generic-radix-tree.o
 libcfs-linux-objs += glob.o
-libcfs-linux-objs += xarray.o
+
+libcfs-compat-objs += $(patsubst %,$(COMPAT)%,$(compat_objs))
 
 libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
 libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o
@@ -23,7 +29,7 @@ libcfs-crypto-objs := $(addprefix crypto/,$(libcfs-crypto-objs))
 
 libcfs-all-objs := debug.o fail.o module.o tracefile.o
 
-libcfs-objs := $(libcfs-linux-objs) $(libcfs-all-objs)
+libcfs-objs := $(libcfs-linux-objs) $(libcfs-all-objs) $(libcfs-compat-objs)
 @LLCRYPT_TRUE@libcfs-objs += $(libcfs-crypto-objs)
 
 EXTRA_PRE_CFLAGS := -I@LUSTRE@/../libcfs/libcfs

lustre_compat/Makefile.in
@@ -1,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
+obj-m += lib/
+
+@INCLUDE_RULES@

lustre_compat/autoMakefile.am
@@ -1,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
+SUBDIRS = lib include

lustre_compat/include/.gitignore
@@ -1,0 +1,1 @@
+/Makefile.in

lustre_compat/include/Makefile.am
@@ -1,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
+SUBDIRS = lustre_compat
+DIST_SUBDIRS = lustre_compat

commit b492fba4f3dc730e6a5a245b4c15d55369e19229

LU-18687 compat: move include and simplify lustre_compat
Move the headers to the top-level. This will simplify the
comiler command-line, isolate autotools dependencies to the
headers, and allow for lustre_compat to use plain Makefiles
(similar to Linux).

Test-Parameters: trivial
Fixes: 74faf91f44 ("LU-18687 compat: move xarray to lustre_compat")
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I851fa183c14a2661bb1eca91d94b7d757f9e575a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58694
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

autoMakefile.am
@@ -14,7 +14,6 @@ SUBDIRS := @LDISKFS_SUBDIR@ \
 	. \
 	@LUSTREIOKIT_SUBDIR@ \
 	Documentation \
-	lustre_compat \
 	libcfs \
 	lnet \
 	lustre
@@ -22,7 +21,6 @@ SUBDIRS := @LDISKFS_SUBDIR@ \
 DIST_SUBDIRS := ldiskfs \
 	lustre-iokit \
 	Documentation \
-	lustre_compat \
 	libcfs \
 	lnet \
 	lustre \
@@ -177,6 +175,10 @@ undef.h: config.h.in
 	grep -v config.h.in config.h.in > $@
 
 dist-hook: undef.h
+	find include/ -type f \( -name "*.h" \) \
+		-exec cp --parents {} $(distdir) \;
+	find lustre_compat/ -type f \( -name "*.c" -o -iname "Makefile" \) \
+		-exec cp --parents {} $(distdir) \;
 	find $(distdir) -name .deps -o \
 			-name CVS -o \
 			-name .svn -o \

config/lustre-build.m4
@@ -439,23 +439,6 @@ AC_DEFUN([LB_CONFIG_FILES], [
 ])
 
 #
-# LB_COMPAT_CONFIG_FILES
-#
-# build-specific config files (lustre_compat)
-#
-AC_DEFUN([LB_COMPAT_CONFIG_FILES], [
-	AC_CONFIG_FILES(
-		lustre_compat/Makefile
-		lustre_compat/lib/Makefile
-		lustre_compat/autoMakefile
-		lustre_compat/lib/autoMakefile
-		lustre_compat/include/Makefile
-		lustre_compat/include/lustre_compat/Makefile
-		lustre_compat/include/lustre_compat/linux/Makefile
-	)
-])
-
-#
 # LB_CONFIG_SERVERS
 #
 AC_DEFUN([LB_CONFIG_SERVERS], [
@@ -715,7 +698,6 @@ LB_CONFIG_HEADERS
 LPLUG_CONFIGURE
 LIBCFS_CONFIG_FILES
 LB_CONFIG_FILES
-LB_COMPAT_CONFIG_FILES
 LN_CONFIG_FILES
 LC_CONFIG_FILES
 LPLUG_CONFIG_FILES

config/lustre-toolchain.m4
@@ -97,7 +97,7 @@ CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
 
 # everyone builds against lnet and lustre kernel headers
-EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include -I$PWD/lustre_compat/include"
+EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include -I$PWD/include"
 AC_SUBST(EXTRA_KCFLAGS)
 ]) # LTC_PROG_CC
 

libcfs/libcfs/Makefile.in
@@ -9,13 +9,15 @@ MODULES = libcfs
 COMPAT := @top_srcdir@/lustre_compat/lib/
 
 libcfs_dir := $(dir $(lastword $(MAKEFILE_LIST)))
--include $(libcfs_dir)/../../lustre_compat/lib/Makefile
+include $(libcfs_dir)/../../lustre_compat/lib/Makefile
 
 libcfs-linux-objs := linux-prim.o
 libcfs-linux-objs += linux-hash.o
 libcfs-linux-objs += linux-wait.o
 
 libcfs-compat-objs += $(patsubst %,$(COMPAT)%,$(compat_objs))
+
+EXTRA_DIST = $(libcfs-compat-objs:.o=.c)
 
 libcfs-crypto-objs := crypto.o fname.o hkdf.o hooks.o keyring.o
 libcfs-crypto-objs += keysetup.o keysetup_v1.o policy.o

lustre_compat/Makefile.in
@@ -1,9 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-obj-m += lib/
-
-@INCLUDE_RULES@

lustre_compat/autoMakefile.am
@@ -1,7 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-SUBDIRS = lib include

lustre_compat/include/.gitignore
@@ -1,1 +1,0 @@
-/Makefile.in

lustre_compat/include/Makefile.am
@@ -1,8 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-SUBDIRS = lustre_compat
-DIST_SUBDIRS = lustre_compat

lustre_compat/include/lustre_compat/.gitignore
@@ -1,1 +1,0 @@
-/Makefile.in

lustre_compat/include/lustre_compat/Makefile.am
@@ -1,8 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-SUBDIRS = linux
-DIST_SUBDIRS = linux

lustre_compat/include/lustre_compat/linux/.gitignore
@@ -1,1 +1,0 @@
-/Makefile.in

lustre_compat/include/lustre_compat/linux/Makefile.am
@@ -1,7 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-EXTRA_DIST = xarray.h glob.h generic-radix-tree.h

lustre_compat/lib/Makefile
@@ -4,4 +4,4 @@
 # This file is part of Lustre, http://www.lustre.org/
 #
 
-MOSTLYCLEANFILES := @MOSTLYCLEANFILES@
+compat_objs := xarray.o glob.o generic-radix-tree.o

lustre_compat/lib/Makefile.in
@@ -1,13 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-
-compat_objs := xarray.o glob.o generic-radix-tree.o
-
-EXTRA_DIST = $(compat_objs:.o=.c)
-
-ifneq "$(MODULES)" "libcfs"
-@INCLUDE_RULES@
-endif