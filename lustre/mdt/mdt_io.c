lustre/lustre-release

commit 13372d6c243ccfc1a0886469f3c0dc048783ed47

LU-10181 mdt: read on open for DoM files
Read file data upon open and return it in reply. That works
only for file with Data-on-MDT layout and no OST components
initialized. There are three possible cases may occur:
1) file data fits in already allocated reply buffer (~9K)
   and is returned in that buffer in OPEN reply.
2) File fits in the maximum reply buffer (128K) and reply is
   returned with larger size to the client causing resend
   with re-allocated buffer.
3) File doesn't fit in reply buffer but its tail fills page
   partially then that tail is returned. This can be useful
   for an append case

Test-Parameters: mdssizegb=20 testlist=sanity-dom,dom-performance,racer
Change-Id: I5574ce5f74017fc654715e212b71fc3b905bdcae
Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
Reviewed-on: https://review.whamcloud.com/23011
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_io.c
@@ -1332,3 +1332,186 @@ out:
 	repbody->mbo_flags = OBD_FL_FLUSH;
 	RETURN(rc);
 }
+
+/* read file data to the buffer */
+int mdt_dom_read_on_open(struct mdt_thread_info *mti, struct mdt_device *mdt,
+			 struct lustre_handle *lh)
+{
+	const struct lu_env *env = mti->mti_env;
+	struct tgt_session_info *tsi = tgt_ses_info(env);
+	struct req_capsule *pill = tsi->tsi_pill;
+	const struct lu_fid *fid;
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
+	struct mdt_body *mbo;
+	struct dt_device *dt = mdt->mdt_bottom;
+	struct dt_object *mo;
+	void *buf;
+	struct niobuf_remote *rnb = NULL;
+	struct niobuf_local *lnb;
+	int rc;
+	int max_reply_len;
+	loff_t offset;
+	unsigned int len, copied = 0;
+	int lnbs, nr_local, i;
+	bool dom_lock = false;
+
+	ENTRY;
+
+	if (!req_capsule_field_present(pill, &RMF_NIOBUF_INLINE, RCL_SERVER)) {
+		/* There is no reply buffers for this field, this means that
+		 * client has no support for data in reply.
+		 */
+		RETURN(0);
+	}
+
+	mbo = req_capsule_server_get(pill, &RMF_MDT_BODY);
+
+	if (lustre_handle_is_used(lh)) {
+		struct ldlm_lock *lock;
+
+		lock = ldlm_handle2lock(lh);
+		if (lock) {
+			dom_lock = ldlm_has_dom(lock) && ldlm_has_layout(lock);
+			LDLM_LOCK_PUT(lock);
+		}
+	}
+
+	/* return data along with open only along with DoM lock */
+	if (!dom_lock || !mdt->mdt_opts.mo_dom_read_open)
+		RETURN(0);
+
+	if (!(mbo->mbo_valid & OBD_MD_DOM_SIZE))
+		RETURN(0);
+
+	if (mbo->mbo_dom_size == 0)
+		RETURN(0);
+
+	/* check the maximum size available in reply */
+	max_reply_len =
+		req->rq_rqbd->rqbd_svcpt->scp_service->srv_max_reply_size;
+
+	CDEBUG(D_INFO, "File size %llu, reply sizes %d/%d/%d\n",
+	       mbo->mbo_dom_size, max_reply_len, req->rq_reqmsg->lm_repsize,
+	       req->rq_replen);
+	len = req->rq_reqmsg->lm_repsize - req->rq_replen;
+	max_reply_len -= req->rq_replen;
+
+	/* NB: at this moment we have the following sizes:
+	 * - req->rq_replen: used data in reply
+	 * - req->rq_reqmsg->lm_repsize: total allocated reply buffer at client
+	 * - max_reply_len: maximum reply size allowed by protocol
+	 *
+	 * Ideal case when file size fits in allocated reply buffer,
+	 * that mean we can return whole data in reply. We can also fit more
+	 * data up to max_reply_size in total reply size, but this will cause
+	 * re-allocation on client and resend with larger buffer. This is still
+	 * faster than separate READ IO.
+	 * Third case if file is too big to fit even in maximum size, in that
+	 * case we return just tail to optimize possible append.
+	 *
+	 * At the moment the following strategy is used:
+	 * 1) try to fit into the buffer we have
+	 * 2) respond with bigger buffer so client will re-allocate it and
+	 *    resend (up to srv_max_reply_size value).
+	 * 3) return just file tail otherwise.
+	 */
+	if (mbo->mbo_dom_size <= len) {
+		/* can fit whole data */
+		len = mbo->mbo_dom_size;
+		offset = 0;
+	} else if (mbo->mbo_dom_size <= max_reply_len) {
+		/* It is worth to make this tunable ON/OFF because this will
+		 * cause buffer re-allocation and resend
+		 */
+		len = mbo->mbo_dom_size;
+		offset = 0;
+	} else {
+		int tail = mbo->mbo_dom_size % PAGE_SIZE;
+
+		/* no tail or tail can't fit in reply */
+		if (tail == 0 || len < tail)
+			RETURN(0);
+
+		len = tail;
+		offset = mbo->mbo_dom_size - len;
+	}
+	LASSERT((offset % PAGE_SIZE) == 0);
+	rc = req_capsule_server_grow(pill, &RMF_NIOBUF_INLINE,
+				     sizeof(*rnb) + len);
+	if (rc != 0) {
+		/* failed to grow data buffer, just exit */
+		GOTO(out, rc = -E2BIG);
+	}
+
+	/* re-take MDT_BODY buffer after the buffer growing above */
+	mbo = req_capsule_server_get(pill, &RMF_MDT_BODY);
+	fid = &mbo->mbo_fid1;
+	if (!fid_is_sane(fid))
+		RETURN(0);
+
+	rnb = req_capsule_server_get(tsi->tsi_pill, &RMF_NIOBUF_INLINE);
+	if (rnb == NULL)
+		GOTO(out, rc = -EPROTO);
+	buf = (char *)rnb + sizeof(*rnb);
+	rnb->rnb_len = len;
+	rnb->rnb_offset = offset;
+
+	mo = dt_locate(env, dt, fid);
+	if (IS_ERR(mo))
+		GOTO(out, rc = PTR_ERR(mo));
+	LASSERT(mo != NULL);
+
+	dt_read_lock(env, mo, 0);
+	if (!dt_object_exists(mo))
+		GOTO(unlock, rc = -ENOENT);
+
+	/* parse remote buffers to local buffers and prepare the latter */
+	lnbs = (len >> PAGE_SHIFT) + 1;
+	OBD_ALLOC(lnb, sizeof(*lnb) * lnbs);
+	if (lnb == NULL)
+		GOTO(unlock, rc = -ENOMEM);
+
+	rc = dt_bufs_get(env, mo, rnb, lnb, 0);
+	if (unlikely(rc < 0))
+		GOTO(free, rc);
+	LASSERT(rc <= lnbs);
+	nr_local = rc;
+	rc = dt_read_prep(env, mo, lnb, nr_local);
+	if (unlikely(rc))
+		GOTO(buf_put, rc);
+	/* copy data to the buffer finally */
+	for (i = 0; i < nr_local; i++) {
+		char *p = kmap(lnb[i].lnb_page);
+		long off;
+
+		LASSERT(lnb[i].lnb_page_offset == 0);
+		off = lnb[i].lnb_len & ~PAGE_MASK;
+		if (off > 0)
+			memset(p + off, 0, PAGE_SIZE - off);
+
+		memcpy(buf + (i << PAGE_SHIFT), p, lnb[i].lnb_len);
+		kunmap(lnb[i].lnb_page);
+		copied += lnb[i].lnb_len;
+		LASSERT(rc <= len);
+	}
+	CDEBUG(D_INFO, "Read %i (wanted %u) bytes from %llu\n", copied,
+	       len, offset);
+	if (copied < len)
+		CWARN("%s: read %i bytes for "DFID
+		      " but wanted %u, is size wrong?\n",
+		      tsi->tsi_exp->exp_obd->obd_name, copied,
+		      PFID(&tsi->tsi_fid), len);
+	EXIT;
+buf_put:
+	dt_bufs_put(env, mo, lnb, nr_local);
+free:
+	OBD_FREE(lnb, sizeof(*lnb) * lnbs);
+unlock:
+	dt_read_unlock(env, mo);
+	lu_object_put(env, &mo->do_lu);
+out:
+	if (rnb != NULL)
+		rnb->rnb_len = copied;
+	RETURN(0);
+}
+

lustre/include/lustre_req_layout.h
@@ -311,6 +311,7 @@ extern struct req_msg_field RMF_OBD_IOOBJ;
 extern struct req_msg_field RMF_OBD_ID;
 extern struct req_msg_field RMF_FID;
 extern struct req_msg_field RMF_NIOBUF_REMOTE;
+extern struct req_msg_field RMF_NIOBUF_INLINE;
 extern struct req_msg_field RMF_RCS;
 extern struct req_msg_field RMF_FIEMAP_KEY;
 extern struct req_msg_field RMF_FIEMAP_VAL;

lustre/include/obd.h
@@ -187,6 +187,17 @@ struct client_obd {
 	 * run-time if a larger observed size is advertised by the MDT. */
 	__u32			 cl_max_mds_easize;
 
+	/* Data-on-MDT specific value to set larger reply buffer for possible
+	 * data read along with open/stat requests. By default it tries to use
+	 * unused space in reply buffer.
+	 * This value is used to ensure that reply buffer has at least as
+	 * much free space as value indicates. That free space is gained from
+	 * LOV EA buffer which is small for DoM files and on big systems can
+	 * provide up to 32KB of extra space in reply buffer.
+	 * Default value is 8K now.
+	 */
+	__u32			 cl_dom_min_inline_repsize;
+
 	enum lustre_sec_part	 cl_sp_me;
 	enum lustre_sec_part	 cl_sp_to;
 	struct sptlrpc_flavor	 cl_flvr_mgc; /* fixed flavor of mgc->mgs */

lustre/llite/file.c
@@ -381,6 +381,130 @@ int ll_file_release(struct inode *inode, struct file *file)
 	RETURN(rc);
 }
 
+static inline int ll_dom_readpage(void *data, struct page *page)
+{
+	struct niobuf_local *lnb = data;
+	void *kaddr;
+
+	kaddr = ll_kmap_atomic(page, KM_USER0);
+	memcpy(kaddr, lnb->lnb_data, lnb->lnb_len);
+	if (lnb->lnb_len < PAGE_SIZE)
+		memset(kaddr + lnb->lnb_len, 0,
+		       PAGE_SIZE - lnb->lnb_len);
+	flush_dcache_page(page);
+	SetPageUptodate(page);
+	ll_kunmap_atomic(kaddr, KM_USER0);
+	unlock_page(page);
+
+	return 0;
+}
+
+void ll_dom_finish_open(struct inode *inode, struct ptlrpc_request *req,
+			struct lookup_intent *it)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct cl_object *obj = lli->lli_clob;
+	struct address_space *mapping = inode->i_mapping;
+	struct page *vmpage;
+	struct niobuf_remote *rnb;
+	char *data;
+	struct lu_env *env;
+	struct cl_io *io;
+	__u16 refcheck;
+	struct lustre_handle lockh;
+	struct ldlm_lock *lock;
+	unsigned long index, start;
+	struct niobuf_local lnb;
+	int rc;
+	bool dom_lock = false;
+
+	ENTRY;
+
+	if (obj == NULL)
+		RETURN_EXIT;
+
+	if (it->it_lock_mode != 0) {
+		lockh.cookie = it->it_lock_handle;
+		lock = ldlm_handle2lock(&lockh);
+		if (lock != NULL)
+			dom_lock = ldlm_has_dom(lock);
+		LDLM_LOCK_PUT(lock);
+	}
+
+	if (!dom_lock)
+		RETURN_EXIT;
+
+	env = cl_env_get(&refcheck);
+	if (IS_ERR(env))
+		RETURN_EXIT;
+
+	if (!req_capsule_has_field(&req->rq_pill, &RMF_NIOBUF_INLINE,
+				   RCL_SERVER))
+		GOTO(out_env, rc = -ENODATA);
+
+	rnb = req_capsule_server_get(&req->rq_pill, &RMF_NIOBUF_INLINE);
+	data = (char *)rnb + sizeof(*rnb);
+
+	if (rnb == NULL || rnb->rnb_len == 0)
+		GOTO(out_env, rc = 0);
+
+	CDEBUG(D_INFO, "Get data buffer along with open, len %i, i_size %llu\n",
+	       rnb->rnb_len, i_size_read(inode));
+
+	io = vvp_env_thread_io(env);
+	io->ci_obj = obj;
+	io->ci_ignore_layout = 1;
+	rc = cl_io_init(env, io, CIT_MISC, obj);
+	if (rc)
+		GOTO(out_io, rc);
+
+	lnb.lnb_file_offset = rnb->rnb_offset;
+	start = lnb.lnb_file_offset / PAGE_SIZE;
+	index = 0;
+	LASSERT(lnb.lnb_file_offset % PAGE_SIZE == 0);
+	lnb.lnb_page_offset = 0;
+	do {
+		struct cl_page *clp;
+
+		lnb.lnb_data = data + (index << PAGE_SHIFT);
+		lnb.lnb_len = rnb->rnb_len - (index << PAGE_SHIFT);
+		if (lnb.lnb_len > PAGE_SIZE)
+			lnb.lnb_len = PAGE_SIZE;
+
+		vmpage = read_cache_page(mapping, index + start,
+					 ll_dom_readpage, &lnb);
+		if (IS_ERR(vmpage)) {
+			CWARN("%s: cannot fill page %lu for "DFID
+			      " with data: rc = %li\n",
+			      ll_get_fsname(inode->i_sb, NULL, 0),
+			      index + start, PFID(lu_object_fid(&obj->co_lu)),
+			      PTR_ERR(vmpage));
+			break;
+		}
+		lock_page(vmpage);
+		clp = cl_page_find(env, obj, vmpage->index, vmpage,
+				   CPT_CACHEABLE);
+		if (IS_ERR(clp)) {
+			unlock_page(vmpage);
+			put_page(vmpage);
+			GOTO(out_io, rc = PTR_ERR(clp));
+		}
+
+		/* export page */
+		cl_page_export(env, clp, 1);
+		cl_page_put(env, clp);
+		unlock_page(vmpage);
+		put_page(vmpage);
+		index++;
+	} while (rnb->rnb_len > (index << PAGE_SHIFT));
+	rc = 0;
+	EXIT;
+out_io:
+	cl_io_fini(env, io);
+out_env:
+	cl_env_put(env, &refcheck);
+}
+
 static int ll_intent_file_open(struct dentry *de, void *lmm, int lmmsize,
 				struct lookup_intent *itp)
 {
@@ -435,8 +559,11 @@ static int ll_intent_file_open(struct dentry *de, void *lmm, int lmmsize,
 	}
 
 	rc = ll_prep_inode(&de->d_inode, req, NULL, itp);
-	if (!rc && itp->it_lock_mode)
+
+	if (!rc && itp->it_lock_mode) {
+		ll_dom_finish_open(de->d_inode, req, itp);
 		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, NULL);
+	}
 
 out:
 	ptlrpc_req_finished(req);

lustre/llite/llite_internal.h
@@ -967,6 +967,9 @@ ssize_t ll_copy_user_md(const struct lov_user_md __user *md,
 			struct lov_user_md **kbuf);
 void ll_open_cleanup(struct super_block *sb, struct ptlrpc_request *open_req);
 
+void ll_dom_finish_open(struct inode *inode, struct ptlrpc_request *req,
+			struct lookup_intent *it);
+
 /* Compute expected user md size when passing in a md from user space */
 static inline ssize_t ll_lov_user_md_size(const struct lov_user_md *lum)
 {

lustre/llite/namei.c
@@ -606,20 +606,24 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 	CDEBUG(D_DENTRY, "it %p it_disposition %x\n", it,
 	       it->it_disposition);
 	if (!it_disposition(it, DISP_LOOKUP_NEG)) {
-                rc = ll_prep_inode(&inode, request, (*de)->d_sb, it);
-                if (rc)
-                        RETURN(rc);
+		rc = ll_prep_inode(&inode, request, (*de)->d_sb, it);
+		if (rc)
+			RETURN(rc);
 
-                ll_set_lock_data(ll_i2sbi(parent)->ll_md_exp, inode, it, &bits);
+		if (it->it_op & IT_OPEN)
+			ll_dom_finish_open(inode, request, it);
 
-                /* We used to query real size from OSTs here, but actually
-                   this is not needed. For stat() calls size would be updated
-                   from subsequent do_revalidate()->ll_inode_revalidate_it() in
-                   2.4 and
-                   vfs_getattr_it->ll_getattr()->ll_inode_revalidate_it() in 2.6
-                   Everybody else who needs correct file size would call
-                   ll_glimpse_size or some equivalent themselves anyway.
-                   Also see bug 7198. */
+		ll_set_lock_data(ll_i2sbi(parent)->ll_md_exp, inode, it, &bits);
+
+		/* We used to query real size from OSTs here, but actually
+		 * this is not needed. For stat() calls size would be updated
+		 * from subsequent do_revalidate()->ll_inode_revalidate_it() in
+		 * 2.4 and
+		 * vfs_getattr_it->ll_getattr()->ll_inode_revalidate_it() in 2.6
+		 * Everybody else who needs correct file size would call
+		 * ll_glimpse_size or some equivalent themselves anyway.
+		 * Also see bug 7198.
+		 */
 	}
 
 	/* Only hash *de if it is unhashed (new dentry).

lustre/mdc/lproc_mdc.c
@@ -453,6 +453,36 @@ static ssize_t mdc_stats_seq_write(struct file *file,
 }
 LPROC_SEQ_FOPS(mdc_stats);
 
+static int mdc_dom_min_repsize_seq_show(struct seq_file *m, void *v)
+{
+	struct obd_device *dev = m->private;
+
+	seq_printf(m, "%u\n", dev->u.cli.cl_dom_min_inline_repsize);
+
+	return 0;
+}
+
+static ssize_t mdc_dom_min_repsize_seq_write(struct file *file,
+					     const char __user *buffer,
+					     size_t count, loff_t *off)
+{
+	struct obd_device *dev;
+	unsigned int val;
+	int rc;
+
+	dev =  ((struct seq_file *)file->private_data)->private;
+	rc = kstrtouint_from_user(buffer, count, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val > MDC_DOM_MAX_INLINE_REPSIZE)
+		return -ERANGE;
+
+	dev->u.cli.cl_dom_min_inline_repsize = val;
+	return count;
+}
+LPROC_SEQ_FOPS(mdc_dom_min_repsize);
+
 LPROC_SEQ_FOPS_RO_TYPE(mdc, connect_flags);
 LPROC_SEQ_FOPS_RO_TYPE(mdc, server_uuid);
 LPROC_SEQ_FOPS_RO_TYPE(mdc, timeouts);
@@ -486,6 +516,8 @@ struct lprocfs_vars lprocfs_mdc_obd_vars[] = {
 	  .fops	=	&mdc_unstable_stats_fops	},
 	{ .name	=	"mdc_stats",
 	  .fops	=	&mdc_stats_fops			},
+	{ .name	=	"mdc_dom_min_repsize",
+	  .fops	=	&mdc_dom_min_repsize_fops	},
 	{ NULL }
 };
 

lustre/mdc/mdc_internal.h
@@ -170,4 +170,8 @@ int mdc_ldlm_blocking_ast(struct ldlm_lock *dlmlock,
 			  struct ldlm_lock_desc *new, void *data, int flag);
 int mdc_ldlm_glimpse_ast(struct ldlm_lock *dlmlock, void *data);
 int mdc_fill_lvb(struct ptlrpc_request *req, struct ost_lvb *lvb);
+
+#define MDC_DOM_DEF_INLINE_REPSIZE 8192
+#define MDC_DOM_MAX_INLINE_REPSIZE XATTR_SIZE_MAX
+
 #endif

commit 130792644e0d8047c60896df71f942f445dc3c37

LU-18987 mdt: Remove stray assertion for dom read
There's a strange assertion in mdt_dom_read_on_open()
that does not make sense and checks parameters in
a loop that don't even change.

Let's remove it.

Change-Id: I0662b621a39fbde79415676a8486ec4f9ab3a2ab
Test-Parameters: trivial
Fixes: 13372d6c243c ("LU-10181 mdt: read on open for DoM files")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59173
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>

lustre/mdt/mdt_io.c
@@ -1942,7 +1942,6 @@ int mdt_dom_read_on_open(struct mdt_thread_info *mti, struct mdt_device *mdt,
 		memcpy(buf + (i << PAGE_SHIFT), p, lnb[i].lnb_len);
 		kunmap(lnb[i].lnb_page);
 		copied += lnb[i].lnb_len;
-		LASSERT(rc <= len);
 	}
 	CDEBUG(D_INFO, "Read %i (wanted %u) bytes from %llu\n", copied,
 	       len, offset);