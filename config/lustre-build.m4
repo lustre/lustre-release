lustre/lustre-release

commit a13fc434c57fd72e5f8908a8a197fca1a0d373e5

LU-17662 osd-zfs: Support for ZFS 2.2.3
ZFS commit zfs-2.2.99-269-g9b1677fb5
   dmu: Allow buffer fills to fail
Adds a boolean_t to dmu_buf_will_fill() and dmu_buf_fill_done()

Lustre always uses B_FALSE for this argument.

Also re-arrange and split some configure macros so we can all
the zfs and ldiskfs tests can be run in the same parallel pass.

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I71a4723bfa8ce62ae6f270e26ab149bf98278d3f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54530
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Brian Atkinson <batkinson@lanl.gov>
Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build.m4
@@ -151,22 +151,36 @@ AC_ARG_ENABLE([modules],
 		LC_TARGET_SUPPORTED([enable_modules="yes"],
 				    [enable_modules="no"])
 	])
-AC_MSG_RESULT([$enable_modules ($target_os)])
+	AC_MSG_RESULT([$enable_modules ($target_os)])
+	AS_IF([test "x$enable_modules" = xyes], [
+		AS_IF([test "x$FLEX" = "x"], [AC_MSG_ERROR(
+			[flex package is required to build kernel modules])])
+		AS_IF([test "x$BISON" = "x"], [AC_MSG_ERROR(
+			[bison package is required to build kernel modules])])
+	AS_CASE([$target_os], [linux*], [
+			# Ensure SUBARCH is defined
+			SUBARCH=$(echo $target_cpu | sed -e 's/powerpc.*/powerpc/' -e 's/ppc.*/powerpc/' -e 's/x86_64/x86/' -e 's/i.86/x86/' -e 's/k1om/x86/' -e 's/aarch64.*/arm64/' -e 's/armv7.*/arm/')
+			LB_PROG_LINUX
+			AS_IF([test "x$enable_server" != xno],
+				[LB_EXT4_SOURCE_PATH])
+		], [*], [
+			# This is strange - Lustre supports a target we don't
+			AC_MSG_ERROR([Modules are not supported on $target_os])
+	])
+])
 
-AS_IF([test "x$enable_modules" = xyes], [
-	AS_IF([test "x$FLEX" = "x"], [AC_MSG_ERROR([flex package is required to build kernel modules])])
-	AS_IF([test "x$BISON" = "x"], [AC_MSG_ERROR([bison package is required to build kernel modules])])
+AC_DEFUN([LB_KABI_CHECKS], [
 	AS_CASE([$target_os],
 		[linux*], [
 			# Ensure SUBARCH is defined
 			SUBARCH=$(echo $target_cpu | sed -e 's/powerpc.*/powerpc/' -e 's/ppc.*/powerpc/' -e 's/x86_64/x86/' -e 's/i.86/x86/' -e 's/k1om/x86/' -e 's/aarch64.*/arm64/' -e 's/armv7.*/arm/')
 
 			# Run serial tests
-			LB_PROG_LINUX
 			LIBCFS_PROG_LINUX
 			LN_PROG_LINUX
-			AS_IF([test "x$enable_server" != xno], [LB_EXT4_SRC_DIR])
 			LC_PROG_LINUX
+			AS_IF([test "x$enable_zfs" = xyes],
+				[LZ_ZFS_KABI_SERIAL])
 
 			# Run 'early' checks. The results of these are used in
 			# other configure tests:
@@ -187,8 +201,11 @@ AS_IF([test "x$enable_modules" = xyes], [
 			LB_PROG_LINUX_SRC
 			LIBCFS_PROG_LINUX_SRC
 			LN_PROG_LINUX_SRC
-			AS_IF([test "x$enable_server" != xno], [LB_EXT4_SRC_DIR_SRC])
+			AS_IF([test "x$enable_server" != xno],
+				[LB_EXT4_LDISKFS_TESTS])
 			LC_PROG_LINUX_SRC
+			AS_IF([test "x$enable_zfs" = xyes],
+				[LZ_KABI_ZFS_TESTS])
 
 			LB2_LINUX_TEST_COMPILE_ALL([lustre],
 				[for available lustre kapi interfaces])
@@ -197,8 +214,11 @@ AS_IF([test "x$enable_modules" = xyes], [
 			LB_PROG_LINUX_RESULTS
 			LIBCFS_PROG_LINUX_RESULTS
 			LN_PROG_LINUX_RESULTS
-			AS_IF([test "x$enable_server" != xno], [LB_EXT4_SRC_DIR_RESULTS])
+			AS_IF([test "x$enable_server" != xno],
+				[LB_EXT4_LDISKFS_CHECKS])
 			LC_PROG_LINUX_RESULTS
+			AS_IF([test "x$enable_zfs" = xyes],
+				[LZ_KABI_ZFS_CHECKS])
 
 		], [*], [
 			# This is strange - Lustre supports a target we don't
@@ -439,6 +459,11 @@ LB_CONFIG_MODULES
 AS_IF([test x$enable_modules = xno], [enable_server=no])
 LB_CONFIG_LDISKFS
 LB_CONFIG_ZFS
+AS_IF([test "x$enable_dist" = xno], [
+	LB_KABI_LDISKFS
+	LZ_KABI_ZFS
+	LB_KABI_CHECKS
+])
 
 # If no backends were configured, and the user did not explicitly
 # require servers to be enabled, we just disable servers.

config/lustre-build-ldiskfs.m4
@@ -622,8 +622,8 @@ AC_MSG_RESULT([$enable_ldiskfs])
 AM_CONDITIONAL([LDISKFS_ENABLED], [test x$enable_ldiskfs = xyes])
 ]) # LB_CONFIG_LDISKFS
 
-AS_IF([test x$enable_ldiskfs != xno],[
-	AC_DEFUN([LB_EXT4_SRC_DIR_SRC],[
+AC_DEFUN([LB_KABI_LDISKFS], [AS_IF([test x$enable_ldiskfs != xno],[
+	AC_DEFUN([LB_EXT4_LDISKFS_TESTS],[
 		LB_SRC_EXT4_BREAD_4ARGS
 		LB_SRC_EXT4_HAVE_INFO_DQUOT
 		LB_SRC_EXT4_HAVE_I_CRYPT_INFO
@@ -635,7 +635,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 		LB_SRC_JBD2_JOURNAL_GET_MAX_TXN_BUFS
 		LB2_SRC_CHECK_CONFIG_IM([FS_ENCRYPTION])
 	])
-	AC_DEFUN([LB_EXT4_SRC_DIR_RESULTS], [
+	AC_DEFUN([LB_EXT4_LDISKFS_CHECKS], [
 		LB_EXT4_BREAD_4ARGS
 		LB_EXT4_HAVE_INFO_DQUOT
 		LB_EXT4_HAVE_I_CRYPT_INFO
@@ -650,7 +650,7 @@ AS_IF([test x$enable_ldiskfs != xno],[
 			EXT4_CRYPTO='%/crypto.c'])
 	])
 	AC_SUBST(EXT4_CRYPTO)
-])
+])])
 
 #
 # LB_VALIDATE_EXT4_SRC_DIR
@@ -696,12 +696,12 @@ and kernel-debuginfo-common-<arch> packages are installed.
 ]) # LB_VALIDATE_EXT4_SRC_DIR
 
 #
-# LB_EXT4_SRC_DIR
+# LB_EXT4_SOURCE_PATH
 #
 # Determine the location of the ext4 source code.  It it required
 # for several configure tests and to build ldiskfs.
 #
-AC_DEFUN([LB_EXT4_SRC_DIR], [
+AC_DEFUN([LB_EXT4_SOURCE_PATH], [
 AC_MSG_CHECKING([ext4 source directory])
 # Kernel ext source located with devel headers
 linux_src=$LINUX
@@ -729,7 +729,7 @@ AC_MSG_RESULT([$EXT4_SRC_DIR])
 AC_SUBST(EXT4_SRC_DIR)
 
 LB_VALIDATE_EXT4_SRC_DIR
-]) # LB_EXT4_SRC_DIR
+]) # LB_EXT4_SOURCE_PATH
 
 #
 # LB_DEFINE_E2FSPROGS_NAMES

lustre/osd-zfs/osd_io.c
@@ -301,7 +301,7 @@ static ssize_t osd_write_llog_header(struct osd_object *obj,
 		bufoff = offset - db->db_offset;
 		tocpy = MIN(db->db_size - bufoff, len);
 		if (tocpy == db->db_size)
-			dmu_buf_will_fill(db, oh->ot_tx);
+			dmu_buf_will_fill(db, oh->ot_tx LL_BFILL);
 		else
 			dmu_buf_will_dirty(db, oh->ot_tx);
 		LASSERT(offset >= db->db_offset);
@@ -309,7 +309,7 @@ static ssize_t osd_write_llog_header(struct osd_object *obj,
 		(void) memcpy((char *)db->db_data + bufoff, data, tocpy);
 
 		if (tocpy == db->db_size)
-			dmu_buf_fill_done(db, oh->ot_tx);
+			dmu_buf_fill_done(db, oh->ot_tx LL_BFILL);
 
 		offset += tocpy;
 		data += tocpy;

commit 38c013c04e42ff654750c2fa951a35090616cd30

LU-18684 config: don't run kernel checks with modules disabled
Running:

configure --enable-client --disable-server --disable-modules

runs kernel checks. They should only ever be run with modules enabled.

Fixes: a13fc434c57f ("LU-17662 osd-zfs: Support for ZFS 2.2.3")
Signed-off-by: Ake Sandgren <ake.sandgren@hpc2n.umu.se>
Change-Id: I97277474e1db554c23bf2fa3f80ac3a5b745bded
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57921
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-build.m4
@@ -466,7 +466,7 @@ LB_CONFIG_MODULES
 AS_IF([test x$enable_modules = xno], [enable_server=no])
 LB_CONFIG_LDISKFS
 LB_CONFIG_ZFS
-AS_IF([test "x$enable_dist" = xno], [
+AS_IF([test "x$enable_modules" = xyes], [
 	LB_KABI_LDISKFS
 	LZ_KABI_ZFS
 	LB_KABI_CHECKS