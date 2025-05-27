lustre/lustre-release

commit b711af7d243f3773cec3a37f64c0e0aa8bbc363f

LU-16101 tests: skip sanity/27J for more kernels
This is a bug in the kernel that is not present in older kernels
before commit v5.11-10234-gcbd59c48ae2b (5.12), and is fixed with
commit v6.2-rc4-61-g5956592ce337 (6.2).

Move this from ALWAYS_EXCEPT (bug that needs to be fixed) to skip
(test that is known to fail in some configs but has been fixed).

Fixes: af6f49698a18 ("LU-16101 tests: add sanity/27J to always_except")
Test-Parameters: trivial testlist=sanity clientdistro=el9.2 env=ONLY=27J
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I8ec0a6d25a90e05672b039cd6c2b2fbf8a3ebbe5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51567
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Alex Deiter <alex.deiter@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanity.sh
@@ -62,12 +62,6 @@ if (( $LINUX_VERSION_CODE >= $(version_code 4.18.0) &&
 	always_except LU-13063 411
 fi
 
-# skip basic ops on file with foreign LOV tests on 5.16.0+ kernels
-# until the filemap_read() issue is fixed
-if (( $LINUX_VERSION_CODE >= $(version_code 5.16.0) )); then
-	always_except LU-16101 27J
-fi
-
 #                                  5              12     8   12  15   (min)"
 [[ "$SLOW" = "no" ]] && EXCEPT_SLOW="27m 60i 64b 68 71 135 136 230d 300o"
 
@@ -105,9 +99,6 @@ if [ -r /etc/SuSE-release ] || [ -r /etc/SUSE-brand ]; then
 
 	[ $sles_version -lt $(version_code 12.0.0) ] &&
 		always_except LU-3703 234
-
-	[ $sles_version -ge $(version_code 15.4.0) ] &&
-		always_except LU-16101 27J
 elif [ -r /etc/os-release ]; then
 	if grep -qi ubuntu /etc/os-release; then
 		ubuntu_version=$(version_code $(sed -n -e 's/"//g' \
@@ -2791,8 +2782,14 @@ test_27I() {
 run_test 27I "check that root dir striping does not break parent dir one"
 
 test_27J() {
-	[[ $MDS1_VERSION -le $(version_code 2.12.51) ]] &&
+	(( $MDS1_VERSION > $(version_code 2.12.51) )) ||
 		skip "Need MDS version newer than 2.12.51"
+
+	# skip basic ops on file with foreign LOV tests on 5.12-6.2 kernels
+	# until the filemap_read() issue is fixed by v6.2-rc4-61-g5956592ce337
+	(( $LINUX_VERSION_CODE < $(version_code 5.12.0) ||
+	   $LINUX_VERSION_CODE >= $(version_code 6.2.0) )) ||
+		skip "Need kernel < 5.12.0 or >= 6.2.0 for filemap_read() fix"
 
 	test_mkdir $DIR/$tdir
 	local uuid1=$(cat /proc/sys/kernel/random/uuid)

commit 83f7a198c4a5886c958663957d914239731c4b6d

LU-19040 kernel: update SLES15 SP6 [6.4.0-150600.23.50.1]
Update SLES15 SP6 kernel to 6.4.0-150600.23.50.1 for Lustre client.

Lustre-change: https://review.whamcloud.com/59368
Lustre-commit: TBD (from a470380feef87413575d189e548a1a7c82ef0da4)

LU-17905 kernel: new kernel [SLES15 SP6 6.4.0-150600.23.14.2]

This patch makes changes to support new SLES15 SP6 release
with kernel 6.4.0-150600.23.14.2 for Lustre client.

Lustre-change: https://review.whamcloud.com/55563
Lustre-commit: 020e361cfdbad529e31525052e660268d7bd976d

Was-Change-Id: Ib9159d200122595d0a56e3581cfc66d75ddb59f6

LU-18102 tests: skip read/write in sanity/27J for some kernels

Kernel commit v5.11-10234-gcbd59c48ae2b (5.12) skips filemap_read()
with 0 file size, commit v6.2-rc4-61-g5956592ce337 (6.2) just correct
the last page read in filemap_read(), it still skips the real file
read with 0 sized file.

Lustre-change: https://review.whamcloud.com/55977
Lustre-commit: adb3d20f3c8d3a720b293d3d5977a1849a9e0cb7

Was-Change-Id: I1c562cce1374df7659c8d178fb3601e4dfb01744

Fixes: b711af7d24 ("LU-16101 tests: skip sanity/27J for more kernels")
Signed-off-by: Bobi Jam <bobijam@whamcloud.com>

Test-Parameters: trivial mdtcount=4 mdscount=2 \
  clientdistro=sles15sp6 testlist=sanity

Test-Parameters: optional mdtcount=4 mdscount=2 \
  clientdistro=sles15sp6 testgroup=full-dne-part-1

Test-Parameters: optional mdtcount=4 mdscount=2 \
  clientdistro=sles15sp6 testgroup=full-dne-part-2

Test-Parameters: optional mdtcount=4 mdscount=2 \
  clientdistro=sles15sp6 testgroup=full-dne-part-3

Change-Id: Ie2d530f0edb28326bbcbd1326f40e3e7db845c21
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59369
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Alex Deiter <adeiter@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

contrib/lbuild/funcs.sh
@@ -211,6 +211,7 @@ autodetect_target() {
         sles15.3) target="$(uname -r | cut -d . -f 1,2)-sles15sp3";;
 	sles15.4) target="$(uname -r | cut -d . -f 1,2)-sles15sp4";;
 	sles15.5) target="$(uname -r | cut -d . -f 1,2)-sles15sp5";;
+	sles15.6) target="$(uname -r | cut -d . -f 1,2)-sles15sp6";;
           fc18)   target="3.x-fc18";;
              *)   fatal 1 "I don't know what distro $distro is.\nEither update autodetect_target() or use the --target argument.";;
     esac

contrib/lbuild/lbuild-sles15
@@ -45,6 +45,10 @@ get_distro_kdir() {
 			;;
 		esac
 		;;
+	6.4)
+		distro=sles15.6
+		kdir=updates-sles15.6-src
+		;;
 	esac
 
 	case $1 in

lustre/autoconf/lustre-core.m4
@@ -5119,6 +5119,7 @@ lustre/kernel_patches/targets/5.3-sles15sp2.target
 lustre/kernel_patches/targets/5.3-sles15sp3.target
 lustre/kernel_patches/targets/5.14-sles15sp4.target
 lustre/kernel_patches/targets/5.14-sles15sp5.target
+lustre/kernel_patches/targets/6.4-sles15sp6.target
 lustre/kernel_patches/targets/3.x-fc18.target
 lustre/ldlm/Makefile
 lustre/fid/Makefile

lustre/kernel_patches/targets/6.4-sles15sp6.target.in
@@ -1,0 +1,37 @@
+lnxmaj="6.4"
+lnxmin=".0"
+lnxrel="150600.23.50"
+# use this when there is an "RPM fix" which means that the name of the
+# (source) RPM has been updated but the version of the kernel inside the
+# RPM is not also updated
+rpmfix=".1"
+
+# this is the delimeter that goes before the "smp" at the end of the version
+# defaults to empty
+FLAVOR_DELIMITER="-"
+
+KERNEL_SRPM=kernel-source-${lnxmaj}${lnxmin}-${lnxrel}${rpmfix}.src.rpm
+SERIES=""
+VERSION=$lnxmaj
+EXTRA_VERSION="${lnxmin#.}-${lnxrel}_lustre.@VERSION@"
+LUSTRE_VERSION=@VERSION@
+
+OFED_VERSION=inkernel
+
+BASE_ARCHS="i686 ppc x86_64 ia64 ppc64"
+BIGMEM_ARCHS=""
+BOOT_ARCHS=""
+JENSEN_ARCHS=""
+DEFAULT_ARCHS="i686 x86_64 ia64 ppc64"
+BIGSMP_ARCHS=""
+PSERIES64_ARCHS="ppc"
+UP_ARCHS=""
+SRC_ARCHS=""
+#RPMSMPTYPE="smp"
+
+for cc in gcc ; do
+    if which $cc >/dev/null 2>/dev/null ; then
+        export CC=$cc
+        break
+    fi
+done

lustre.spec.in
@@ -389,7 +389,12 @@ BuildRequires: mpich-devel
 %if "%{_vendor}" == "redhat" || 0%{?suse_version} < 1500
 BuildRequires: openmpi-devel
 %else
+%if 0%{?sle_version} && 0%{?sle_version} >= 150600
+BuildRequires: openmpi4-devel
+%else
 BuildRequires: openmpi2-devel
+%endif
+# openmpi-devel
 %endif
 # openmpi
 %endif

lustre/tests/sanity.sh
@@ -2789,11 +2789,6 @@ test_27J() {
 	(( $MDS1_VERSION > $(version_code 2.12.51) )) ||
 		skip "Need MDS version newer than 2.12.51"
 
-	# skip basic ops on file with foreign LOV tests on 5.12-6.2 kernels
-	# until the filemap_read() issue is fixed by v6.2-rc4-61-g5956592ce337
-	(( $LINUX_VERSION_CODE < $(version_code 5.12.0) ||
-	   $LINUX_VERSION_CODE >= $(version_code 6.2.0) )) ||
-		skip "Need kernel < 5.12.0 or >= 6.2.0 for filemap_read() fix"
 
 	test_mkdir $DIR/$tdir
 	local uuid1=$(cat /proc/sys/kernel/random/uuid)
@@ -2862,14 +2857,19 @@ test_27J() {
 	$LFS setstripe -c 2 $DIR/$tdir/${tfile}2 &&
 		error "$DIR/$tdir/${tfile}2: setstripe should fail"
 
-	# R/W should fail
-	cat $DIR/$tdir/$tfile && error "$DIR/$tdir/$tfile: read should fail"
-	cat $DIR/$tdir/${tfile}2 &&
-		error "$DIR/$tdir/${tfile}2: read should fail"
-	cat /etc/passwd > $DIR/$tdir/$tfile &&
-		error "$DIR/$tdir/$tfile: write should fail"
-	cat /etc/passwd > $DIR/$tdir/${tfile}2 &&
-		error "$DIR/$tdir/${tfile}2: write should fail"
+	# skip basic ops on file with foreign LOV tests on >5.12 kernels
+	(( $LINUX_VERSION_CODE < $(version_code 5.12.0) )) && {
+		# kernel >= 5.12.0 would skip filemap_read() with 0 sized file
+		# R/W should fail
+		cat $DIR/$tdir/$tfile &&
+			error "$DIR/$tdir/$tfile: read should fail"
+		cat $DIR/$tdir/${tfile}2 &&
+			error "$DIR/$tdir/${tfile}2: read should fail"
+		cat /etc/passwd > $DIR/$tdir/$tfile &&
+			error "$DIR/$tdir/$tfile: write should fail"
+		cat /etc/passwd > $DIR/$tdir/${tfile}2 &&
+			error "$DIR/$tdir/${tfile}2: write should fail"
+	}
 
 	# chmod should work
 	chmod 222 $DIR/$tdir/$tfile ||