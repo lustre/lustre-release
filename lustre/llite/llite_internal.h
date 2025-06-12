lustre/lustre-release

commit 1288681bb8aa76f43af5c53ec3ef68ecc8d412d7

LU-14361 statahead: add statahead advise IOCTL
This patch reuse ioctl(LL_IOC_LADVISE2) for statahead advise.
This allows userspace programs to advise the kernel statahead
of the order that they will be traversing a directory, so that
the client can prefetch inode attributes from the MDT, similar
to what posix_fadvise(POSIX_FADV_SEQUENTIAL) does for file data.

After patched mdtest via adding this statahead IOCTL hint, it
can support mdtest benchmark with regularized file naming format:
	mdtest.$rank.$i
The usage of this statahead advise IOCTL could be as follows:
	open(dir);
	ioctl(dir_fd, IOC_LADVISE2, ...);
	stat mdtest.0.0;
	stat mdtest.0.1;
	stat mdtest.0.2;
	stat mdtest.0.3;
	...
	clsoedir(dir);

Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: Iac38e33bfc6d7a0b755c2646ba8053a263e3afc9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48625
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -118,6 +118,13 @@ struct ll_trunc_sem {
 	atomic_t	ll_trunc_waiters;
 };
 
+enum ll_sa_pattern {
+	LSA_PATTERN_NONE	= 0x0000,
+	LSA_PATTERN_LIST	= 0x0001,
+	LSA_PATTERN_FNAME	= 0X0002,
+	LSA_PATTERN_MAX,
+};
+
 struct ll_inode_info {
 	__u32				lli_inode_magic;
 	rwlock_t			lli_lock;
@@ -171,6 +178,7 @@ struct ll_inode_info {
 			 * cleanup the dir readahead. */
 			void			       *lli_opendir_key;
 			struct ll_statahead_info       *lli_sai;
+			struct ll_statahead_context    *lli_sax;
 			/* protect statahead stuff. */
 			spinlock_t			lli_sa_lock;
 			/* "opendir_pid" is the token when lookup/revalid
@@ -195,6 +203,8 @@ struct ll_inode_info {
 							lli_def_lsm_obj_set:1;
 			/* generation for statahead */
 			unsigned int			lli_sa_generation;
+			/* access pattern for statahead */
+			enum ll_sa_pattern		lli_sa_pattern;
 			/* rw lock protects lli_lsm_md */
 			struct rw_semaphore		lli_lsm_sem;
 			/* directory stripe information */
@@ -1050,6 +1060,8 @@ struct ll_file_data {
 	 * -errno is saved here, and will return to user in close().
 	 */
 	int fd_partial_readdir_rc;
+	/* mdtest unique/shared dir stat mode: per process statahead struct. */
+	struct ll_statahead_info *fd_sai;
 };
 
 void llite_tunables_unregister(void);
@@ -1295,6 +1307,8 @@ int ll_ioctl_fssetxattr(struct inode *inode, unsigned int cmd,
 			void __user *uarg);
 #endif
 int ll_ioctl_project(struct file *file, unsigned int cmd, void __user *uarg);
+int ll_ioctl_ahead(struct file *file, struct llapi_lu_ladvise2 *ladvise);
+
 int ll_lov_setstripe_ea_info(struct inode *inode, struct dentry *dentry,
 			     __u64 flags, struct lov_user_md *lum,
 			     int lum_size);
@@ -1617,11 +1631,11 @@ void ll_ra_stats_inc(struct inode *inode, enum ra_stat which);
 #define LL_SA_BATCH_MAX		1024
 #define LL_SA_BATCH_DEF		64
 
-#define LL_SA_CACHE_BIT         5
+#define LL_SA_CACHE_BIT         6
 #define LL_SA_CACHE_SIZE        (1 << LL_SA_CACHE_BIT)
 #define LL_SA_CACHE_MASK        (LL_SA_CACHE_SIZE - 1)
 
-/* per inode struct, for dir only */
+/* statahead controller, per process struct, for dir only */
 struct ll_statahead_info {
 	struct dentry	       *sai_dentry;
 	atomic_t		sai_refcount;   /* when access this struct, hold
@@ -1654,12 +1668,23 @@ struct ll_statahead_info {
 	struct task_struct	*sai_agl_task;	/* AGL thread */
 	struct list_head	sai_entries;    /* completed entries */
 	struct list_head	sai_agls;	/* AGLs to be sent */
-	struct list_head	sai_cache[LL_SA_CACHE_SIZE];
-	spinlock_t		sai_cache_lock[LL_SA_CACHE_SIZE];
 	atomic_t		sai_cache_count; /* entry count in cache */
 	struct lu_batch		*sai_bh;
 	__u32			sai_max_batch_count;
 	__u64			sai_index_end;
+
+	__u64			sai_fstart;
+	__u64			sai_fend;
+	char			sai_fname[NAME_MAX];
+};
+
+/* Per inode statahead information */
+struct ll_statahead_context {
+	struct inode		*sax_inode;
+	atomic_t		 sax_refcount;
+	/* Local dcache */
+	struct list_head	 sax_cache[LL_SA_CACHE_SIZE];
+	spinlock_t		 sax_cache_lock[LL_SA_CACHE_SIZE];
 };
 
 int ll_revalidate_statahead(struct inode *dir, struct dentry **dentry,
@@ -1724,10 +1749,6 @@ dentry_may_statahead(struct inode *dir, struct dentry *dentry)
 	if (!lli->lli_sa_enabled)
 		return false;
 
-	/* not the same process, don't statahead */
-	if (lli->lli_opendir_pid != current->pid)
-		return false;
-
 	/*
 	 * When stating a dentry, kernel may trigger 'revalidate' or 'lookup'
 	 * multiple times, eg. for 'getattr', 'getxattr' and etc.
@@ -1745,6 +1766,13 @@ dentry_may_statahead(struct inode *dir, struct dentry *dentry)
 	ldd = ll_d2d(dentry);
 	if (ldd != NULL && lli->lli_sa_generation &&
 	    ldd->lld_sa_generation == lli->lli_sa_generation)
+		return false;
+
+	if (lli->lli_sa_pattern == LSA_PATTERN_FNAME)
+		return true;
+
+	/* not the same process, don't statahead */
+	if (lli->lli_opendir_pid != current->pid)
 		return false;
 
 	return true;

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -631,6 +631,7 @@ struct ll_ioc_lease_id {
 #define LL_IOC_FID2MDTIDX		_IOWR('f', 248, struct lu_fid)
 #define LL_IOC_GETPARENT		_IOWR('f', 249, struct getparent)
 #define LL_IOC_LADVISE			_IOR('f', 250, struct llapi_lu_ladvise)
+#define LL_IOC_LADVISE2			_IOW('f', 250, struct llapi_lu_ladvise2)
 #define LL_IOC_HEAT_GET			_IOWR('f', 251, struct lu_heat)
 #define LL_IOC_HEAT_SET			_IOW('f', 251, __u64)
 #define LL_IOC_PCC_ATTACH		_IOW('f', 252, struct lu_pcc_attach)
@@ -2610,6 +2611,8 @@ enum lu_ladvise_type {
 	LU_LADVISE_DONTNEED	= 2,
 	LU_LADVISE_LOCKNOEXPAND = 3,
 	LU_LADVISE_LOCKAHEAD	= 4,
+	/* Ahead operations for open|create|stat|read|write. */
+	LU_LADVISE_AHEAD	= 5,
 	LU_LADVISE_MAX
 };
 
@@ -2618,6 +2621,7 @@ enum lu_ladvise_type {
 	[LU_LADVISE_DONTNEED]		= "dontneed",			\
 	[LU_LADVISE_LOCKNOEXPAND]	= "locknoexpand",		\
 	[LU_LADVISE_LOCKAHEAD]		= "lockahead",			\
+	[LU_LADVISE_AHEAD]		= "ahead",			\
 }
 
 /* This is the userspace argument for ladvise.  It is currently the same as
@@ -2632,6 +2636,58 @@ struct llapi_lu_ladvise {
 	__u32 lla_value3;
 	__u32 lla_value4;
 };
+
+struct llapi_lu_ladvise2 {
+	__u16	lla_advice;	/* advice type */
+	__u16	lla_value1;	/* values for different advice types */
+	__u32	lla_value2;
+	__u64	lla_start;
+	__u64	lla_end;
+	__u32	lla_value3;
+	__u32	lla_value4;
+	union {
+		struct {
+			__u32	lla_value5;
+			__u32	lla_value6;
+		};
+		char lla_buf[NAME_MAX + 1];
+	};
+};
+
+/* I/O call sequences in a batch access. */
+enum lu_access_flags {
+	ACCESS_FL_NONE	= 0x0,
+	ACCESS_FL_STAT	= 0x01,
+	ACCESS_FL_OPEN	= 0x02,
+	ACCESS_FL_CREAT	= 0x04,
+	ACCESS_FL_READ	= 0x08,
+	ACCESS_FL_WRITE	= 0x10,
+	ACCESS_FL_OC	= ACCESS_FL_OPEN | ACCESS_FL_CREAT,
+	ACCESS_FL_SOR	= ACCESS_FL_STAT | ACCESS_FL_OPEN | ACCESS_FL_READ,
+	ACCESS_FL_OCW	= ACCESS_FL_OPEN | ACCESS_FL_CREAT | ACCESS_FL_WRITE,
+};
+
+enum lu_ahead_mode {
+	LU_AH_MODE_NONE		= 0,
+	/*
+	 * The batch access pattern obeys certain naming rules, such as mdtest
+	 * with the file naming format mdtest.$rank.$i.
+	 */
+	LU_AH_NAME_INDEX	= 1,
+	/*
+	 * Provide a file name list as input to do batch accesses with
+	 * irregular file name format.
+	 */
+	LU_AH_NAME_ARRAY	= 2,
+	/* Prefetching in readdir() order under a directory. */
+	LU_AH_NAME_READDIR	= 3,
+	LU_AH_MODE_MAX,
+};
+
+#define lla_ahead_mode		lla_value1
+#define lla_access_flags	lla_value2
+#define lla_batch_max		lla_value3
+#define lla_fname		lla_buf
 
 enum ladvise_flag {
 	LF_ASYNC	= 0x00000001,

lustre/llite/dir.c
@@ -1534,6 +1534,8 @@ static char *ll_getname(const char __user *filename)
 	return tmp;
 }
 
+static const char *const ladvise_names[] = LU_LADVISE_NAMES;
+
 #define ll_putname(filename) OBD_FREE(filename, NAME_MAX + 1);
 
 static long ll_dir_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
@@ -2290,6 +2292,58 @@ out_hur:
 migrate_free:
 		OBD_FREE_LARGE(data, len);
 
+		RETURN(rc);
+	}
+	case LL_IOC_LADVISE2: {
+		struct llapi_lu_ladvise2 *ladvise;
+
+		OBD_ALLOC_PTR(ladvise);
+		if (ladvise == NULL)
+			RETURN(-ENOMEM);
+
+		if (copy_from_user(ladvise, uarg, sizeof(*ladvise)))
+			GOTO(out_ladvise, rc = -EFAULT);
+
+		switch (ladvise->lla_advice) {
+		case LU_LADVISE_AHEAD:
+			if (ladvise->lla_start >= ladvise->lla_end) {
+				CDEBUG(D_VFSTRACE,
+				       "%s: Invalid range (%llu %llu) for %s\n",
+				       sbi->ll_fsname, ladvise->lla_start,
+				       ladvise->lla_end,
+				       ladvise_names[ladvise->lla_advice]);
+				GOTO(out_ladvise, rc = -EINVAL);
+			}
+
+			/*
+			 * Currently we only support name indexing format
+			 * ahead operations.
+			 */
+			if (ladvise->lla_ahead_mode != LU_AH_NAME_INDEX) {
+				CDEBUG(D_VFSTRACE,
+				       "%s: Invalid access mode (%d) for %s\n",
+				       sbi->ll_fsname, ladvise->lla_ahead_mode,
+				       ladvise_names[ladvise->lla_advice]);
+				GOTO(out_ladvise, rc = -EINVAL);
+			}
+
+			/* Currently we only support stat-ahead operations. */
+			if (!(ladvise->lla_access_flags & ACCESS_FL_STAT)) {
+				CDEBUG(D_VFSTRACE,
+				       "%s: Invalid access flags (%x) for %s\n",
+				       sbi->ll_fsname,
+				       ladvise->lla_access_flags,
+				       ladvise_names[ladvise->lla_advice]);
+				GOTO(out_ladvise, rc = -EINVAL);
+			}
+
+			rc = ll_ioctl_ahead(file, ladvise);
+			break;
+		default:
+			rc = -EINVAL;
+		}
+out_ladvise:
+		OBD_FREE_PTR(ladvise);
 		RETURN(rc);
 	}
 	case LL_IOC_PCC_DETACH_BY_FID: {

lustre/llite/file.c
@@ -414,7 +414,8 @@ int ll_file_release(struct inode *inode, struct file *file)
 
 	/* The last ref on @file, maybe not the the owner pid of statahead,
 	 * because parent and child process can share the same file handle. */
-	if (S_ISDIR(inode->i_mode) && lli->lli_opendir_key == fd)
+	if (S_ISDIR(inode->i_mode) &&
+	    (lli->lli_opendir_key == fd || fd->fd_sai))
 		ll_deauthorize_statahead(inode, fd);
 
 	if (is_root_inode(inode)) {

lustre/lmv/lmv_obd.c
@@ -3826,23 +3826,31 @@ static int lmv_intent_getattr_async(struct obd_export *exp,
 
 	ENTRY;
 
-	if (!fid_is_sane(&op_data->op_fid2))
+	if (!(fid_is_sane(&op_data->op_fid2) ||
+	      fid_is_zero(&op_data->op_fid2)))
 		RETURN(-EINVAL);
 
 	ptgt = lmv_locate_tgt(lmv, op_data);
 	if (IS_ERR(ptgt))
 		RETURN(PTR_ERR(ptgt));
 
-	ctgt = lmv_fid2tgt(lmv, &op_data->op_fid2);
-	if (IS_ERR(ctgt))
-		RETURN(PTR_ERR(ctgt));
-
 	/*
-	 * remote object needs two RPCs to lookup and getattr, considering the
-	 * complexity don't support statahead for now.
+	 * Zeroed FID @op_fid2 means that the intent getattr() comes from
+	 * statahead by regularized file names. Currently only do statahead
+	 * for the children files located same as the parent directory.
 	 */
-	if (ctgt != ptgt)
-		RETURN(-EREMOTE);
+	if (!fid_is_zero(&op_data->op_fid2)) {
+		ctgt = lmv_fid2tgt(lmv, &op_data->op_fid2);
+		if (IS_ERR(ctgt))
+			RETURN(PTR_ERR(ctgt));
+
+		/*
+		 * remote object needs two RPCs to lookup and getattr,
+		 * considering the complexity don't support statahead for now.
+		 */
+		if (ctgt != ptgt)
+			RETURN(-EREMOTE);
+	}
 
 	rc = md_intent_getattr_async(ptgt->ltd_exp, item);
 
@@ -4087,12 +4095,26 @@ lmv_batch_locate_tgt(struct lmv_obd *lmv, struct md_op_item *item)
 	case MD_OP_GETATTR: {
 		struct lmv_tgt_desc *ptgt;
 
-		if (!fid_is_sane(&op_data->op_fid2))
+		if (!(fid_is_sane(&op_data->op_fid2) ||
+		      fid_is_zero(&op_data->op_fid2)))
 			RETURN(ERR_PTR(-EINVAL));
 
 		ptgt = lmv_locate_tgt(lmv, op_data);
 		if (IS_ERR(ptgt))
 			RETURN(ptgt);
+
+		/*
+		 * Zeroed @op_fid2 means that it is a statahead populating call
+		 * in the file name pattern which is using file name format to
+		 * prefetch the attributes. Thus it has no idea about the FID of
+		 * the children file. The children file is considered to be
+		 * located on the same storage target with the parent directory
+		 * or the stripped directory.
+		 */
+		if (fid_is_zero(&op_data->op_fid2)) {
+			tgt = ptgt;
+			break;
+		}
 
 		tgt = lmv_fid2tgt(lmv, &op_data->op_fid2);
 		if (IS_ERR(tgt))

lustre/mdt/mdt_handler.c
@@ -2364,6 +2364,7 @@ static int mdt_getattr_name_lock(struct mdt_thread_info *info,
 			 */
 			if (mdt_object_remote(child))
 				GOTO(out_child, rc = -EREMOTE);
+
 			try_bits |= child_bits;
 			child_bits = 0;
 		}

commit 24a2a4ed6b2120aa21cbaad9496281ff69d1d7c2

LU-17000 llite: Handle not NUL terminated buffer in ll_statahead_info
Match ll_statahead_info:sai_fname(target) array
length with llapi_lu_ladvise2:lla_buf(source).

Test-Parameters: trivial
CoverityID: 400216 ("Buffer not null terminated")
Fixes: 1288681b (LU-14361 statahead: add statahead advise IOCTL)
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Id898ab4b49d54bd734831c09e3de725533e7c249
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59456
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/llite_internal.h
@@ -1887,7 +1887,8 @@ struct ll_statahead_info {
 			__u8	sai_fname_zeroed_len;
 		};
 	};
-	char			sai_fname[NAME_MAX];
+	/* match array size of llapi_lu_ladvise2:lla_buf */
+	char			sai_fname[NAME_MAX + 1];
 };
 
 /* Per inode statahead information */