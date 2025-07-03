lustre/lustre-release

commit be131d125ad128187da970963c543fb9144c947a

LU-16974 utils: lfs mirror resync to show progress
lfs mirror resync should be able to:
 - show progress like lfs mirror extend --stats does
 - throttle like lfs mirror extend -W does

use 64MB buffer for mirror resync by default.

Change-Id: Ibe60748542ff4a3731aa6a4a9907be82427a0ae9
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51750
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre/lustreapi.h
@@ -726,6 +726,12 @@ int llapi_layout_mirror_inherit(struct llapi_layout *f_layout,
 int llapi_mirror_find_stale(struct llapi_layout *layout,
 		struct llapi_resync_comp *comp, size_t comp_size,
 		__u16 *mirror_ids, int ids_nr);
+int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
+				    struct llapi_resync_comp *comp_array,
+				    int comp_size,  uint64_t start,
+				    uint64_t end,
+				    unsigned long stats_interval_sec,
+				    unsigned long bandwidth_bytes_sec);
 int llapi_mirror_resync_many(int fd, struct llapi_layout *layout,
 			     struct llapi_resync_comp *comp_array,
 			     int comp_size,  uint64_t start, uint64_t end);

commit 60784ed61524d8b273e7915ce470520ca588a9fb

LU-16974 utils: fix 'lfs mirror resync --stats' printing
If "lfs mirror resync --stats" is run without --bandwidth-limit,
then no stats are printed until the file resync is 100% finished.

Fix llapi_mirror_resync_many_params() to print the stats even if
no bandwidth throttle is being applied.

Fix write estimate calculation for a file with multiple components.
Print stats with requested granularity, not rounded to next second.

Update the code in lfs.c::migrate_copy_data() to use shared stats
printing and bandwidth throttling with llapi_mirror_resync_many().

Replace direct calls to fprintf() with llapi_err*().
Fix header ordering, remove duplicate headers found.

Test-Parameters: trivial testlist=sanity-flr
Fixes: be131d125a ("LU-16974 utils: lfs mirror resync to show progress")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I68df25501f3cac2647318cff2eb86062f9300c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59263
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Alexandre Ioffe <aioffe@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/lustre/lustreapi.h
@@ -833,17 +833,16 @@ int llapi_layout_get_last_init_comp(struct llapi_layout *layout);
 int llapi_layout_mirror_inherit(struct llapi_layout *f_layout,
 				struct llapi_layout *m_layout);
 int llapi_mirror_find_stale(struct llapi_layout *layout,
-		struct llapi_resync_comp *comp, size_t comp_size,
-		__u16 *mirror_ids, int ids_nr);
+			    struct llapi_resync_comp *comp, size_t comp_size,
+			    __u16 *mirror_ids, int ids_nr);
 int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 				    struct llapi_resync_comp *comp_array,
-				    int comp_size,  uint64_t start,
-				    uint64_t end,
+				    int comp_size, uint64_t start, uint64_t end,
 				    unsigned long stats_interval_sec,
-				    unsigned long bandwidth_bytes_sec);
+				    uint64_t bandwidth_bytes_sec);
 int llapi_mirror_resync_many(int fd, struct llapi_layout *layout,
 			     struct llapi_resync_comp *comp_array,
-			     int comp_size,  uint64_t start, uint64_t end);
+			     int comp_size, uint64_t start, uint64_t end);
 /*
  * Flags to control how layouts are retrieved.
  */

lustre/utils/lfs.c
@@ -40,51 +40,46 @@
 #define _GNU_SOURCE
 #endif
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <inttypes.h>
-#include <getopt.h>
-#include <string.h>
-#include <mntent.h>
-#include <unistd.h>
-#include <errno.h>
+#include <asm/byteorder.h>
+#include <ctype.h>
+#include <dirent.h>
 #include <err.h>
-#include <pwd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <getopt.h>
 #include <grp.h>
+#include <inttypes.h>
+#include <libgen.h>
+#include <mntent.h>
+#include <pwd.h>
 #include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
+#include <sys/param.h>
 #include <sys/quota.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/param.h>
 #include <sys/xattr.h>
-#include <fcntl.h>
-#include <dirent.h>
 #include <time.h>
-#include <ctype.h>
+#include <unistd.h>
 #include <zlib.h>
-#include <libgen.h>
-#include <asm/byteorder.h>
-#include "lfs_project.h"
 
-#include <libcfs/util/string.h>
 #include <libcfs/util/ioctl.h>
 #include <libcfs/util/parser.h>
 #include <libcfs/util/string.h>
-#include <lustre/lustreapi.h>
-#include <linux/lustre/lustre_ver.h>
-#include <linux/lustre/lustre_param.h>
 #include <linux/lnet/nidstr.h>
-#include <lnetconfig/cyaml.h>
-#include "lstddef.h"
 #include <linux/lustre/lustre_idl.h>
+#include <linux/lustre/lustre_param.h>
+#include <linux/lustre/lustre_ver.h>
+#include <lnetconfig/cyaml.h>
+#include <lustre/lustreapi.h>
 #include "callvpe.h"
-
-#ifndef NSEC_PER_SEC
-# define NSEC_PER_SEC 1000000000UL
-#endif
-#define ONE_MB 0x100000
+#include "lfs_project.h"
+#include "lstddef.h"
+#include "lustreapi_internal.h"
 
 /* all functions */
 static int lfs_find(int argc, char **argv);
@@ -884,44 +879,6 @@ out:
 	return rc;
 }
 
-static struct timespec timespec_sub(struct timespec *before,
-				    struct timespec *after)
-{
-	struct timespec ret;
-
-	ret.tv_sec = after->tv_sec - before->tv_sec;
-	if (after->tv_nsec < before->tv_nsec) {
-		ret.tv_sec--;
-		ret.tv_nsec = NSEC_PER_SEC + after->tv_nsec - before->tv_nsec;
-	} else {
-		ret.tv_nsec = after->tv_nsec - before->tv_nsec;
-	}
-
-	return ret;
-}
-
-static void stats_log(struct timespec *now, struct timespec *start_time,
-		      ssize_t read_bytes, size_t write_bytes,
-		      off_t file_size_bytes)
-{
-	struct timespec diff = timespec_sub(start_time, now);
-
-	if (file_size_bytes == 0)
-		return;
-
-	if (diff.tv_sec == 0 && diff.tv_nsec == 0)
-		return;
-
-	printf("- { seconds: %li, rmbps: %5.2g, wmbps: %5.2g, copied: %lu, size: %lu, pct: %lu%% }\n",
-		diff.tv_sec,
-		(double) read_bytes/((ONE_MB * diff.tv_sec) +
-			((ONE_MB * diff.tv_nsec)/NSEC_PER_SEC)),
-		(double) write_bytes/((ONE_MB * diff.tv_sec) +
-			((ONE_MB * diff.tv_nsec)/NSEC_PER_SEC)),
-		write_bytes/ONE_MB, file_size_bytes/ONE_MB,
-		((write_bytes*100)/file_size_bytes));
-}
-
 static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 			     unsigned long long bandwidth_bytes_sec,
 			     long stats_interval_sec, off_t file_size_bytes)
@@ -1043,9 +1000,7 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 
 		to_write = rsize;
 		while (to_write > 0) {
-			unsigned long long write_target;
 			ssize_t written;
-			struct timespec diff;
 
 			written = pwrite(fd_dst, buf, to_write, pos);
 			if (written < 0) {
@@ -1063,46 +1018,16 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 				continue;
 
 			clock_gettime(CLOCK_MONOTONIC, &now);
-			diff = timespec_sub(&start_time, &now);
-			write_target = ((bandwidth_bytes_sec * diff.tv_sec) +
-				((bandwidth_bytes_sec *
-				diff.tv_nsec)/NSEC_PER_SEC));
-
-			if (write_target < write_bytes) {
-				unsigned long long excess;
-				struct timespec delay = { 0, 0 };
-
-				excess = write_bytes - write_target;
-
-				if (excess == 0)
-					continue;
-
-				delay.tv_sec = excess / bandwidth_bytes_sec;
-				delay.tv_nsec = (excess % bandwidth_bytes_sec) *
-					NSEC_PER_SEC / bandwidth_bytes_sec;
-
-				do {
-					rc = clock_nanosleep(CLOCK_MONOTONIC, 0,
-							     &delay, &delay);
-				} while (rc < 0 && errno == EINTR);
-
-				if (rc < 0) {
-					if (stats_interval_sec)
-						llapi_error(LLAPI_MSG_WARN, rc,
-							    "delay for bandwidth control failed");
-					rc = 0;
-				}
-			}
+			llapi_bandwidth_throttle(&now, &start_time,
+						 bandwidth_bytes_sec,
+						 write_bytes);
 		}
 
-		clock_gettime(CLOCK_MONOTONIC, &now);
-		if (stats_interval_sec && (write_bytes != file_size_bytes) &&
-			(now.tv_sec >= last_bw_print.tv_sec +
-			stats_interval_sec)) {
-			stats_log(&now, &start_time,
-				  read_bytes, write_bytes,
-				  file_size_bytes);
-			last_bw_print = now;
+		if (stats_interval_sec && write_bytes != file_size_bytes) {
+			clock_gettime(CLOCK_MONOTONIC, &now);
+			llapi_stats_log(&now, &start_time, &last_bw_print,
+					stats_interval_sec, read_bytes,
+					write_bytes, pos, file_size_bytes);
 		}
 
 		if (rc || rsize < to_read)
@@ -1112,8 +1037,9 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int),
 	/* Output at least one log, regardless of stats_interval */
 	if (stats_interval_sec) {
 		clock_gettime(CLOCK_MONOTONIC, &now);
-		stats_log(&now, &start_time, read_bytes, write_bytes,
-			  file_size_bytes);
+		llapi_stats_log(&now, &start_time, &last_bw_print,
+				stats_interval_sec, read_bytes, write_bytes,
+				file_size_bytes, file_size_bytes);
 	}
 
 	rc = fsync(fd_dst);

lustre/utils/liblustreapi_fid.c
@@ -22,17 +22,18 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sched.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/xattr.h>
 #include <unistd.h>
-#include <sched.h>
 
-#include <libcfs/util/ioctl.h>
 #include <libcfs/util/hash.h>
-#include <lustre/lustreapi.h>
+#include <libcfs/util/ioctl.h>
 #include <linux/lustre/lustre_fid.h>
+#include <linux/lustre/lustre_ioctl.h>
+#include <lustre/lustreapi.h>
 #include "lustreapi_internal.h"
 
 /* strip instances of // (DNE striped directory) when copying to reply buffer */

lustre/utils/liblustreapi_hsm.c
@@ -16,32 +16,32 @@
  * Author: Henri Doreau <henri.doreau@cea.fr>
  */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stddef.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-#include <malloc.h>
-#include <errno.h>
 #include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <fnmatch.h>
+#include <malloc.h>
+#include <signal.h>
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <time.h>
-#include <utime.h>
-#include <sys/syscall.h>
-#include <fnmatch.h>
-#include <signal.h>
 #ifdef HAVE_LINUX_UNISTD_H
 #include <linux/unistd.h>
 #else
 #include <unistd.h>
 #endif
+#include <utime.h>
 
 #include <linux/lnet/lnetctl.h>
+#include <linux/lustre/lustre_idl.h>
 #include <lustre/lustreapi.h>
 #include "lustreapi_internal.h"
 

lustre/utils/liblustreapi_layout.c
@@ -25,8 +25,10 @@
 #include <time.h>
 
 #include <libcfs/util/list.h>
+#include <linux/lustre/lustre_idl.h>
 #include <lustre/lustreapi.h>
 #include "lustreapi_internal.h"
+#include "lstddef.h"
 
 /**
  * Layout component, which contains all attributes of a plain
@@ -3258,57 +3260,13 @@ int llapi_mirror_find(struct llapi_layout *layout, uint64_t file_start,
 	return mirror_id;
 }
 
-#ifndef NSEC_PER_SEC
-# define NSEC_PER_SEC 1000000000UL
-#endif
-#define ONE_MB 0x100000
-static struct timespec timespec_sub(struct timespec *before,
-				    struct timespec *after)
-{
-	struct timespec ret;
-
-	ret.tv_sec = after->tv_sec - before->tv_sec;
-	if (after->tv_nsec < before->tv_nsec) {
-		ret.tv_sec--;
-		ret.tv_nsec = NSEC_PER_SEC + after->tv_nsec - before->tv_nsec;
-	} else {
-		ret.tv_nsec = after->tv_nsec - before->tv_nsec;
-	}
-
-	return ret;
-}
-
-static void stats_log(struct timespec *now, struct timespec *start_time,
-		      ssize_t read_bytes, size_t write_bytes,
-		      off_t file_size_bytes)
-{
-	struct timespec diff = timespec_sub(start_time, now);
-
-	if (file_size_bytes == 0)
-		return;
-
-	if (diff.tv_sec == 0 && diff.tv_nsec == 0)
-		return;
-
-	llapi_printf(LLAPI_MSG_NORMAL,
-		     "- { seconds: %li, rmbps: %5.2g, wmbps: %5.2g, copied: %lu, size: %lu, pct: %lu%% }\n",
-		     diff.tv_sec,
-		     (double) read_bytes/((ONE_MB * diff.tv_sec) +
-			     ((ONE_MB * diff.tv_nsec)/NSEC_PER_SEC)),
-		     (double) write_bytes/((ONE_MB * diff.tv_sec) +
-			     ((ONE_MB * diff.tv_nsec)/NSEC_PER_SEC)),
-		     write_bytes/ONE_MB,
-		     file_size_bytes/ONE_MB,
-		     ((write_bytes*100)/file_size_bytes));
-}
-
 int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 				    struct llapi_resync_comp *comp_array,
-				    int comp_size,  uint64_t start,
-				    uint64_t end,
+				    int comp_size, uint64_t start, uint64_t end,
 				    unsigned long stats_interval_sec,
-				    unsigned long bandwidth_bytes_sec)
+				    uint64_t bandwidth_bytes_sec)
 {
+	struct stat stbuf;
 	size_t buflen = 64 << 20; /* 64M */
 	ssize_t page_size;
 	void *buf;
@@ -3322,16 +3280,22 @@ int llapi_mirror_resync_many_params(int fd, struct llapi_layout *layout,
 	struct timespec start_time;
 	struct timespec now;
 	struct timespec last_bw_print;
-	size_t total_bytes_read = 0;
-	size_t total_bytes_written = 0;
-	off_t write_estimation_bytes = 0;
-	struct stat st;
+	uint64_t total_bytes_read = 0;
+	uint64_t total_bytes_written = 0;
+	uint64_t write_estimation_bytes = 0;
 
-	rc = fstat(fd, &st);
+	rc = fstat(fd, &stbuf);
 	if (rc < 0)
 		return -errno;
-	if (bandwidth_bytes_sec > 0 || stats_interval_sec)
-		write_estimation_bytes = st.st_size * comp_size;
+
+	/* estimate is too big for sparse file, but good enough for % done */
+	if (bandwidth_bytes_sec > 0 || stats_interval_sec) {
+		for (i = 0; i < comp_size; i++) {
+			write_estimation_bytes +=
+				min_t(uint64_t, comp_array[i].lrc_end,
+				      stbuf.st_size) - comp_array[i].lrc_start;
+		}
+	}
 
 	/* limit transfer size to what can be sent in one second */
 	if (bandwidth_bytes_sec && bandwidth_bytes_sec < buflen)
@@ -3471,8 +3435,6 @@ do_read:
 		to_write = ((bytes_read - 1) | (page_size - 1)) + 1;
 
 		for (i = 0; i < comp_size; i++) {
-			unsigned long long write_target;
-			struct timespec diff;
 			ssize_t written;
 			off_t pos2 = pos;
 			size_t to_write2 = to_write;
@@ -3507,53 +3469,21 @@ do_read:
 			assert(written == to_write2);
 			total_bytes_written += written;
 
-			if (bandwidth_bytes_sec == 0)
+			if (!bandwidth_bytes_sec && !stats_interval_sec)
 				continue;
 
 			clock_gettime(CLOCK_MONOTONIC, &now);
-			diff = timespec_sub(&start_time, &now);
-			write_target = ((bandwidth_bytes_sec * diff.tv_sec) +
-				((bandwidth_bytes_sec *
-				diff.tv_nsec)/NSEC_PER_SEC));
+			llapi_bandwidth_throttle(&now, &start_time,
+						 bandwidth_bytes_sec,
+						 total_bytes_written);
 
-			if (write_target < total_bytes_written) {
-				unsigned long long excess;
-				struct timespec delay = { 0, 0 };
-
-				excess = total_bytes_written - write_target;
-
-				if (excess == 0)
-					continue;
-
-				delay.tv_sec = excess / bandwidth_bytes_sec;
-				delay.tv_nsec = (excess % bandwidth_bytes_sec) *
-					NSEC_PER_SEC / bandwidth_bytes_sec;
-
-				do {
-					rc = clock_nanosleep(CLOCK_MONOTONIC, 0,
-							     &delay, &delay);
-				} while (rc < 0 && errno == EINTR);
-
-				if (rc < 0) {
-					llapi_error(LLAPI_MSG_ERROR, rc,
-						"errors: delay for bandwidth control failed: %s\n",
-						strerror(-rc));
-					rc = 0;
-				}
-			}
-
-			if (stats_interval_sec) {
-				clock_gettime(CLOCK_MONOTONIC, &now);
-				if ((total_bytes_written != end - start) &&
-				     (now.tv_sec >= last_bw_print.tv_sec +
-						 stats_interval_sec)) {
-					stats_log(&now, &start_time,
-						  total_bytes_read,
-						  total_bytes_written,
-						  write_estimation_bytes);
-					last_bw_print = now;
-				}
-			}
+			if (stats_interval_sec &&
+			    total_bytes_written != write_estimation_bytes)
+				llapi_stats_log(&now, &start_time,
+					  &last_bw_print, stats_interval_sec,
+					  total_bytes_read, total_bytes_written,
+					  total_bytes_written,
+					  write_estimation_bytes);
 		}
 		pos += bytes_read;
 	}
@@ -3570,9 +3500,10 @@ do_read:
 	/* Output at least one log, regardless of stats_interval */
 	if (stats_interval_sec) {
 		clock_gettime(CLOCK_MONOTONIC, &now);
-		stats_log(&now, &start_time, total_bytes_read,
-			  total_bytes_written,
-			  write_estimation_bytes);
+		llapi_stats_log(&now, &start_time,
+				&last_bw_print, stats_interval_sec,
+				total_bytes_read, total_bytes_written,
+				write_estimation_bytes, write_estimation_bytes);
 	}
 
 	/**
@@ -3587,7 +3518,7 @@ do_read:
 		if (pos < comp->lrc_start || pos >= comp->lrc_end)
 			continue;
 
-		if (pos < st.st_size) {
+		if (pos < stbuf.st_size) {
 			rc = llapi_mirror_punch(fd, comp->lrc_mirror_id, pos,
 						comp->lrc_end - pos);
 		} else {
@@ -3901,13 +3832,13 @@ out_err:
 /* Print explanation of layout error */
 void llapi_layout_sanity_perror(int error)
 {
-	if (error >= LSE_LAST || error < 0) {
-		fprintf(stdout, "Invalid layout, unrecognized error: %d\n",
-			error);
-	} else {
-		fprintf(stdout, "Invalid layout: %s\n",
-			llapi_layout_strerror[error]);
-	}
+	if (error >= LSE_LAST || error < 0)
+		llapi_err_noerrno(LLAPI_MSG_ERROR,
+				  "Invalid layout, unrecognized error: %d\n",
+				  error);
+	else
+		llapi_err_noerrno(LLAPI_MSG_ERROR, "Invalid layout: %s\n",
+				  llapi_layout_strerror[error]);
 }
 
 /* Walk a layout and enforce sanity checks that apply to > 1 component

lustre/utils/liblustreapi_util.c
@@ -506,3 +506,67 @@ int llapi_get_fsname(const char *path, char *fsname, size_t fsname_len)
 {
 	return llapi_get_fsname_instance(path, fsname, fsname_len, NULL, 0);
 }
+
+void llapi_bandwidth_throttle(struct timespec *now, struct timespec *start_time,
+			      uint64_t bandwidth_bytes_sec,
+			      uint64_t total_bytes_written)
+{
+	struct timespec diff;
+	struct timespec delay = { 0, 0 };
+	size_t write_target;
+	long long excess;
+	int rc;
+
+	if (bandwidth_bytes_sec == 0)
+		return;
+
+	diff = timespec_sub(start_time, now);
+	write_target = (bandwidth_bytes_sec * diff.tv_sec) +
+		       (bandwidth_bytes_sec * diff.tv_nsec / NSEC_PER_SEC);
+
+	excess = (long long)total_bytes_written - (long long)write_target;
+	if (excess <= 0)
+		return;
+
+	delay.tv_sec = excess / bandwidth_bytes_sec;
+	delay.tv_nsec = (excess % bandwidth_bytes_sec) * NSEC_PER_SEC /
+			bandwidth_bytes_sec;
+
+	do {
+		rc = clock_nanosleep(CLOCK_MONOTONIC, 0, &delay, &delay);
+	} while (rc < 0 && errno == EINTR);
+
+	if (rc < 0)
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "errors: delay for bandwidth control failed\n");
+}
+
+void llapi_stats_log(struct timespec *now, struct timespec *start_time,
+		     struct timespec *last_print, int stats_interval_sec,
+		     uint64_t read_bytes, uint64_t write_bytes,
+		     uint64_t offset, uint64_t file_size_bytes)
+{
+	struct timespec diff_print;
+	struct timespec diff;
+
+	if (file_size_bytes == 0)
+		return;
+
+	diff_print = timespec_sub(last_print, now);
+	if (diff_print.tv_sec < stats_interval_sec &&
+	    offset != file_size_bytes)
+		return;
+
+	diff = timespec_sub(start_time, now);
+
+	llapi_printf(LLAPI_MSG_NORMAL,
+		     "- { seconds: %li, rmbps: %5.2g, wmbps: %5.2g, copied: %lu, total: %lu, pct: %lu%% }\n",
+		     diff.tv_sec,
+		     (double)read_bytes / (ONE_MB * diff.tv_sec +
+					  ONE_MB * diff.tv_nsec / NSEC_PER_SEC),
+		     (double)write_bytes / (ONE_MB * diff.tv_sec +
+					  ONE_MB * diff.tv_nsec / NSEC_PER_SEC),
+		     offset / ONE_MB, file_size_bytes / ONE_MB,
+		     offset * 100 / file_size_bytes);
+	*last_print = *now;
+}

lustre/utils/lustreapi_internal.h
@@ -34,14 +34,14 @@
 #ifndef _LUSTREAPI_INTERNAL_H_
 #define _LUSTREAPI_INTERNAL_H_
 
+#include <dirent.h>
 #include <limits.h>
 #include <stdint.h>
-#include <dirent.h>
+#include <time.h>
 
 #include <libcfs/util/ioctl.h>
 #include <libcfs/util/param.h>
 
-#include <linux/lustre/lustre_ioctl.h>
 #include <linux/lustre/lustre_kernelcomm.h>
 
 #include <lustre/lustreapi.h>
@@ -71,6 +71,7 @@
 
 int get_root_path(int want, char *fsname, int *outfd, char *path, int index,
 		  dev_t *dev, char **out_nid);
+struct obd_ioctl_data;
 int llapi_ioctl_pack(struct obd_ioctl_data *data, char **pbuf, int max_len);
 int llapi_ioctl_dev(int dev_id, unsigned int cmd, void *buf);
 int llapi_ioctl_unpack(struct obd_ioctl_data *data, char *pbuf, int max_len);
@@ -160,10 +161,6 @@ static inline bool llapi_pool_name_is_valid(const char **pool_name)
 	return true;
 }
 
-/* Compatibility macro for legacy llapi functions that use "offset"
- * terminology instead of the preferred "index". */
-#define llapi_stripe_offset_is_valid(os) llapi_stripe_index_is_valid(os)
-
 static inline bool llapi_dir_stripe_count_is_valid(int64_t count)
 {
 	return count >= LMV_OVERSTRIPE_COUNT_MAX &&
@@ -233,4 +230,36 @@ int llapi_semantic_traverse(char *path, int size, int parent,
 			    semantic_func_t sem_init,
 			    semantic_func_t sem_fini, void *data,
 			    struct dirent64 *de);
+
+#ifndef NSEC_PER_SEC
+#define NSEC_PER_SEC 1000000000UL
+#endif
+#ifndef ONE_MB
+#define ONE_MB (1024 * 1024)
+#endif
+
+static inline struct timespec timespec_sub(struct timespec *before,
+					   struct timespec *after)
+{
+	struct timespec ret;
+
+	ret.tv_sec = after->tv_sec - before->tv_sec;
+	if (after->tv_nsec < before->tv_nsec) {
+		ret.tv_sec--;
+		ret.tv_nsec = NSEC_PER_SEC + after->tv_nsec - before->tv_nsec;
+	} else {
+		ret.tv_nsec = after->tv_nsec - before->tv_nsec;
+	}
+
+	return ret;
+}
+
+/* not ready to expose as official APIs yet, but want to share code */
+void llapi_bandwidth_throttle(struct timespec *now, struct timespec *start_time,
+			      uint64_t bandwidth_bytes_sec,
+			      uint64_t total_bytes_written);
+void llapi_stats_log(struct timespec *now, struct timespec *start_time,
+		     struct timespec *last_print, int stats_interval_sec,
+		     uint64_t read_bytes, uint64_t write_bytes,
+		     uint64_t offset, uint64_t file_size_bytes);
 #endif /* _LUSTREAPI_INTERNAL_H_ */