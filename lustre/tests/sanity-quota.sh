lustre/lustre-release

commit 2354f2f040d6c581bf6e523de9dd26f5467ae7c4

LU-16771 llite: cache statfs data for projects
use per-fs rhashtable to cache project-related statfs

Test-Parameters: fstype=ldiskfs testlist=sanity
Test-Parameters: fstype=ldiskfs testlist=sanity-quota
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I0bcac75afb9ab74535e2c553c575a812116b14d4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58781
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/llite/llite_lib.c
@@ -60,6 +60,20 @@ struct kmem_cache *ll_file_data_slab;
 #define log2(n) ffz(~(n))
 #endif
 
+struct proj_sfs_cache {
+	unsigned int		psc_id;
+	struct rhash_head	psc_linkage;
+	struct kstatfs		psc_sfs;
+	time64_t		psc_age;
+	struct mutex		psc_mutex;
+};
+
+static const struct rhashtable_params proj_sfs_cache_params = {
+	.key_len	= sizeof(unsigned int),
+	.key_offset	= offsetof(struct proj_sfs_cache, psc_id),
+	.head_offset	= offsetof(struct proj_sfs_cache, psc_linkage),
+};
+
 /*
  * If there is only one number of core visible to Lustre,
  * async readahead will be disabled, to avoid massive over
@@ -218,6 +232,10 @@ static struct ll_sb_info *ll_init_sbi(struct lustre_sb_info *lsi)
 	sbi->ll_enable_setstripe_gid = -1;
 
 	INIT_LIST_HEAD(&sbi->ll_all_quota_list);
+
+	rc = rhashtable_init(&sbi->ll_proj_sfs_htable, &proj_sfs_cache_params);
+	LASSERT(rc == 0);
+
 	RETURN(sbi);
 out_destroy_ra:
 	OBD_FREE(sbi->ll_foreign_symlink_prefix, sizeof("/mnt/"));
@@ -233,6 +251,13 @@ out_sbi:
 	RETURN(ERR_PTR(rc));
 }
 
+static void proj_sfs_free(void *psa, void *arg)
+{
+	struct proj_sfs_cache *ps = psa;
+
+	OBD_FREE_PTR(ps);
+}
+
 static void ll_free_sbi(struct super_block *sb)
 {
 	struct ll_sb_info *sbi = ll_s2sbi(sb);
@@ -240,6 +265,8 @@ static void ll_free_sbi(struct super_block *sb)
 	ENTRY;
 
 	if (sbi != NULL) {
+		rhashtable_free_and_destroy(&sbi->ll_proj_sfs_htable,
+					    proj_sfs_free, NULL);
 		if (!list_empty(&sbi->ll_squash.rsi_nosquash_nids))
 			cfs_free_nidlist(&sbi->ll_squash.rsi_nosquash_nids);
 		if (sbi->ll_ra_info.ll_readahead_wq)
@@ -2621,13 +2648,49 @@ out:
 
 static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 {
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct ll_sb_info *sbi = ll_s2sbi(inode->i_sb);
 	struct if_quotactl qctl = {
 		.qc_cmd = LUSTRE_Q_GETQUOTA,
 		.qc_type = PRJQUOTA,
 		.qc_valid = QC_GENERAL,
 	};
+	struct proj_sfs_cache *ps, *orig;
 	u64 limit, curblock;
 	int ret;
+
+	LASSERT(S_ISDIR(inode->i_mode));
+
+	ps = rhashtable_lookup_fast(&sbi->ll_proj_sfs_htable,
+				    &lli->lli_projid,
+				    proj_sfs_cache_params);
+	if (!ps) {
+		OBD_ALLOC_PTR(ps);
+		if (!ps)
+			return -ENOMEM;
+		ps->psc_id = lli->lli_projid;
+		mutex_init(&ps->psc_mutex);
+		orig = rhashtable_lookup_get_insert_fast(&sbi->ll_proj_sfs_htable,
+							&ps->psc_linkage,
+							proj_sfs_cache_params);
+		if (orig) {
+			OBD_FREE_PTR(ps);
+			if (IS_ERR(orig))
+				return PTR_ERR(orig);
+			ps = orig;
+		}
+	}
+
+	if (ktime_get_seconds() - ps->psc_age < sbi->ll_statfs_max_age) {
+		*sfs = ps->psc_sfs;
+		return 0;
+	}
+
+	mutex_lock(&ps->psc_mutex);
+	if (ktime_get_seconds() - ps->psc_age < sbi->ll_statfs_max_age) {
+		*sfs = ps->psc_sfs;
+		GOTO(out, ret = 0);
+	}
 
 	qctl.qc_id = ll_i2info(inode)->lli_projid;
 	ret = quotactl_ioctl(inode->i_sb, &qctl);
@@ -2637,7 +2700,7 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 		 */
 		if (ret == -ESRCH || ret == -EOPNOTSUPP)
 			ret = 0;
-		return ret;
+		GOTO(out, ret);
 	}
 
 	limit = ((qctl.qc_dqblk.dqb_bsoftlimit ?
@@ -2662,7 +2725,13 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 			(sfs->f_files - qctl.qc_dqblk.dqb_curinodes) : 0;
 	}
 
-	return 0;
+	ps->psc_sfs = *sfs;
+	ps->psc_age = ktime_get_seconds();
+
+out:
+	mutex_unlock(&ps->psc_mutex);
+
+	return ret;
 }
 
 int ll_statfs(struct dentry *de, struct kstatfs *sfs)

lustre/llite/llite_internal.h
@@ -1031,6 +1031,8 @@ struct ll_sb_info {
 
 	/* LU-14535: the list of "lfs quota -a" */
 	struct list_head	 ll_all_quota_list;
+
+	struct rhashtable	 ll_proj_sfs_htable;
 };
 
 #define SBI_DEFAULT_HEAT_DECAY_WEIGHT	((80 * 256 + 50) / 100)

commit 7efafec4bd8944b8d5123b73583989383446fbcd

LU-16771 llite: statfs on regular files is OK
It shouldn't be considered a bug to statfs() a regular file.
There doesn't appear to be anything directory-specific in
ll_statfs_project() that would warrant this LASSERT(), and
it can be trivially triggered from userspace.

Fixes: 2354f2f040 ("LU-16771 llite: cache statfs data for projects")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I3cf6b123e7ff40d233b4bb247935835a582b4a8f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59898
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/llite_lib.c
@@ -2659,7 +2659,7 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 	u64 limit, curblock;
 	int ret;
 
-	LASSERT(S_ISDIR(inode->i_mode));
+	ENTRY;
 
 	ps = rhashtable_lookup_fast(&sbi->ll_proj_sfs_htable,
 				    &lli->lli_projid,
@@ -2667,7 +2667,7 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 	if (!ps) {
 		OBD_ALLOC_PTR(ps);
 		if (!ps)
-			return -ENOMEM;
+			RETURN(-ENOMEM);
 		ps->psc_id = lli->lli_projid;
 		mutex_init(&ps->psc_mutex);
 		orig = rhashtable_lookup_get_insert_fast(&sbi->ll_proj_sfs_htable,
@@ -2676,14 +2676,14 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 		if (orig) {
 			OBD_FREE_PTR(ps);
 			if (IS_ERR(orig))
-				return PTR_ERR(orig);
+				RETURN(PTR_ERR(orig));
 			ps = orig;
 		}
 	}
 
 	if (ktime_get_seconds() - ps->psc_age < sbi->ll_statfs_max_age) {
 		*sfs = ps->psc_sfs;
-		return 0;
+		RETURN(0);
 	}
 
 	mutex_lock(&ps->psc_mutex);
@@ -2731,7 +2731,7 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 out:
 	mutex_unlock(&ps->psc_mutex);
 
-	return ret;
+	RETURN(ret);
 }
 
 int ll_statfs(struct dentry *de, struct kstatfs *sfs)
@@ -2743,7 +2743,8 @@ int ll_statfs(struct dentry *de, struct kstatfs *sfs)
 	ktime_t kstart = ktime_get();
 	int rc;
 
-	CDEBUG(D_VFSTRACE, "VFS Op:sb=%s (%p)\n", sb->s_id, sb);
+	CDEBUG(D_VFSTRACE, "VFS Op:sb=%s (%p) "DNAME" proj=%u\n", sb->s_id, sb,
+	       encode_fn_dentry(de), ll_i2info(de->d_inode)->lli_projid);
 
 	/* Some amount of caching on the client is allowed */
 	rc = ll_statfs_internal(sbi, &osfs, OBD_STATFS_SUM);

lustre/tests/sanity-quota.sh
@@ -3921,6 +3921,7 @@ test_41() {
 		skip "Project quota is not supported"
 	setup_quota_test || error "setup quota failed with $?"
 	local dir="$DIR/$tdir/dir"
+	local file="$dir/$tfile"
 	local blimit=102400
 	local ilimit=4096
 	local projid=$((testnum * 1000))
@@ -3939,6 +3940,10 @@ test_41() {
 	test_mkdir -p $dir && change_project -sp $projid $dir
 	$LFS setquota -p $projid -b 0 -B ${blimit}K -i 0 -I $ilimit $dir ||
 		error "set project quota failed"
+	touch $file || error "touch $file failed"
+	$LFS project $file
+	local file_proj=($($LFS project $file))
+	[[ ${file_proj[0]} == $projid ]] || error "project not set on $file"
 
 	sync; sync_all_data
 	sleep_maxage
@@ -3953,9 +3958,8 @@ test_41() {
 	local iused=$(getquota -p $projid global curinodes)
 	local expected="$ilimit$iused"
 
-	wait_update $HOSTNAME \
-		"df -iP $dir | awk \\\"/$FSNAME/\\\"'{print \\\$2 \\\$3}'" \
-		"$expected" ||
+	wait_update $HOSTNAME "df -iP $dir |
+		awk \\\"/$FSNAME/\\\"'{print \\\$2 \\\$3}'" "$expected" ||
 		error "failed to get correct statfs for project quota"
 
 	expected=$(df -kP $dir | awk "/$FSNAME/"' {print $2}')
@@ -3967,16 +3971,26 @@ test_41() {
 	expected=$(df -kP $dir | awk "/$FSNAME/"' {print $3}')
 	(( expected - bused < 4)) || error "bused mismatch: $expected != $bused"
 
+	# check if df output on regular file works as expected
+	echo "== project statfs on file (prjid=$projid): $file =="
+	df -kP $file; df -iP $file; $LFS quota -p $projid $dir
+	local bused=$(getquota -p $projid global curspace)
+	local iused=$(getquota -p $projid global curinodes)
+	local expected="$ilimit$iused"
+
+	wait_update $HOSTNAME "df -iP $dir |
+		awk \\\"/$FSNAME/\\\"'{print \\\$2 \\\$3}'" "$expected" ||
+		error "failed to get correct statfs for project quota"
+
 	# disable statfs_project and check again
 	$LCTL set_param llite.*.statfs_project=0
 
 	expected=$({ df -kP $MOUNT; df -iP $MOUNT; } | \
 		awk '/'$FSNAME'/ { printf "%d %d ", $2,$3 }')
 
-	wait_update $HOSTNAME \
-		"{ df -kP $dir; df -iP $dir; } |
-		 awk '/$FSNAME/ { printf \\\"%d %d \\\", \\\$2,\\\$3 }'" \
-		"$expected" ||
+	wait_update $HOSTNAME "{ df -kP $dir; df -iP $dir; } |
+		awk '/$FSNAME/ { printf \\\"%d %d \\\", \\\$2,\\\$3 }'" \
+			"$expected" ||
 		error "failed to get correct statfs when statfs_project=0"
 }
 run_test 41 "df should return projid-specific values"