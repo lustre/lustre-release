lustre/lustre-release

commit 72726a311814bc0c0eefb22a769c9ebf7912839e

LU-14810 lnet: Do not issue multiple PUSHes
PUSH ACK may be delayed in network. Meanwhile, some event could cause
peer to go through discovery again (e.g. config change or NI state
change). The discovery state machine doesn't consider whether there
is an outstanding PUSH so it may issue another one for the same peer.
When delayed ACK arrives it will then clear PUSH_SENT, so now
discovery doesn't know that there is an outstanding PUSH. If discovery
is stopped then it doesn't unlink the push MD and this can cause an
assert in lnet_assert_handler_unused() because the push event handler
is still in use.

Modify the discovery state machine to check for PUSH_SENT when
determining whether a peer needs a PUSH.

sanity-lnet test_304 can reproduce this issue under ipv6
configuration if modules are unloaded at the end of the test.

Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ic3f7a8b44f85a18afb939fdbfa1f9bc5dc64d93d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55559
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1134,6 +1134,8 @@ lnet_peer_needs_push(struct lnet_peer *lp)
 		return false;
 	if (lp->lp_state & LNET_PEER_MARK_DELETED)
 		return false;
+	if (lp->lp_state & LNET_PEER_PUSH_SENT)
+		return false;
 	if (lp->lp_state & LNET_PEER_FORCE_PUSH)
 		return true;
 	if (lp->lp_state & LNET_PEER_NO_DISCOVERY)

lustre/tests/sanity-lnet.sh
@@ -4159,7 +4159,8 @@ EOF
 	#   LNET_PEER_DISCOVERED(4) | LNET_PEER_NIDS_UPTODATE(8)
 	(( $locked_peer_state != "1048849")) &&
 		error "Wrong peer state \"$locked_peer_state\" expected 1048849"
-	return 0
+
+	cleanup_lnet
 }
 run_test 304 "Check locked primary peer nid consolidation"
 

commit 99153d212d8c3c1d58cb088b23224d17eefb0fd7

LU-14810 lnet: Avoid multiple PUSH to same peer
It is possible to send multiple PUSHes to the same peer when the
LNET_PEER_FORCE_PUSH bit is set in the peer state. A partial solution
was added in https://review.whamcloud.com/55559/ where we modified
lnet_peer_needs_push() to check for the PUSH_SENT flag. However, we
missed that the main loop in lnet_peer_discovery() will check for the
LNET_PEER_FORCE_PUSH bit prior to calling lnet_peer_needs_push().
Update lnet_peer_discovery() to remove the problematic check for
LNET_PEER_FORCE_PUSH.

Also refactor the checks for sending a ping into a new function,
lnet_peer_needs_ping().

Test-Parameters: trivial
Test-Parameters: testlist=sanity-lnet env=ONLY=212,ONLY_REPEAT=100
Fixes: 72726a3118 ("LU-14810 lnet: Do not issue multiple PUSHes")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ie25089a07ac1d0fcc0e6c56ec69337d22371cc32
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59815
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1127,6 +1127,17 @@ lnet_peer_needs_push(struct lnet_peer *lp)
 	return false;
 }
 
+static inline bool
+lnet_peer_needs_ping(struct lnet_peer *lp)
+{
+	if (lp->lp_state & LNET_PEER_FORCE_PING)
+		return true;
+	else if (!(lp->lp_state & LNET_PEER_NIDS_UPTODATE))
+		return true;
+
+	return false;
+}
+
 static inline unsigned int
 lnet_get_next_recovery_ping(unsigned int ping_count, time64_t now)
 {

lnet/lnet/peer.c
@@ -4124,11 +4124,7 @@ static int lnet_peer_discovery(void *arg)
 				rc = lnet_peer_ping_failed(lp);
 			else if (lp->lp_state & LNET_PEER_PUSH_FAILED)
 				rc = lnet_peer_push_failed(lp);
-			else if (lp->lp_state & LNET_PEER_FORCE_PING)
-				rc = lnet_peer_send_ping(lp);
-			else if (lp->lp_state & LNET_PEER_FORCE_PUSH)
-				rc = lnet_peer_send_push(lp);
-			else if (!(lp->lp_state & LNET_PEER_NIDS_UPTODATE))
+			else if (lnet_peer_needs_ping(lp))
 				rc = lnet_peer_send_ping(lp);
 			else if (lnet_peer_needs_push(lp))
 				rc = lnet_peer_send_push(lp);