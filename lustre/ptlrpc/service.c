lustre/lustre-release

commit 4cb897ca4a970a35ef239de6148f487a9683ba2f

LU-18878 ptlrpc: improve ping evictor and recovery timeout
Let's re-use the logic used for extending the ldlm prolong timeout for
the ping evictor timeout and for recovery timer timeout - use the AT
instead of hard-coded values.

As the AT-based ping timeout is extended on different value each time,
moving an updated export to the end of the timed list is able to make
it not well sorted. Change the list of timed exports, to the rbtree.

Partially revert LUS-11054, tunable ping_evict_timeout_multiplier is
not needed anymore

A minor cleanup of exp_timed flag on connect/reconnect.

Fixes: 8e66f061c ("LU-16002 ptlrpc: adds configurable ping interval”)
HPE-bug-id: LUS-11723
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I97b9795746ccd4242f9798c8192abd4d7a57bbac
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162935
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58620
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/service.c
@@ -1095,34 +1095,158 @@ static void ptlrpc_server_finish_active_request(
 }
 
 /**
+ * Calculate an export eviction timeout.
+ * Used for both cases, lock prolong timeout and ping evictor timeout.
+ *
+ * Whereas a problem client may be still alive trying hard to reconnect and to
+ * resend its RPCs, we should not consider the worst ever case, consisting of
+ * a chain of failures on each step. Let this timeout survive a recovery of
+ * just 1 failure, but let this be the worst possible one - a dead server NID:
+ *
+ * - an RPC timeout;
+ * - the first re-connect is sent to the same NID and times out;
+ * - the second re-connect to the failover pair returns an error;
+ * - the third re-connect to the original node to a different NID succeeds;
+ * - the RPC resend succeeds;
+ *
+ * For lock prolong timeout, we are in the middle of the process -
+ * BL AST is sent, CANCEL is ahead - it is still 1 reply for the current RPC
+ * and at least 1 another RPC (which will trigger another refresh if it will be
+ * not CANCEL) - but more accurate than ldlm_bl_timeout as the timeout is taken
+ * from the RPC (i.e. the view of the client on the current AT) is taken into
+ * account.
+ *
+ * \param[in] at	      AT of RPC service time to calculate timeout for
+ * \param[in] net_at	      network AT
+ * \param[in] rpc_left_time   left service time for the current RPC
+ *                            0 if not applicable
+ * \param[in] pinger	      if the caller is ping evictor or ldlm
+ *
+ * \retval             timeout in seconds to wait for the next client's RPC
+ */
+static timeout_t ptlrpc_export_timeout(struct obd_device *obd,
+				       struct adaptive_timeout *at,
+				       timeout_t netl,
+				       timeout_t rpc_left_time,
+				       bool pinger)
+{
+	timeout_t timeout, at_timeout, req_timeout;
+
+	if (obd_at_off(obd))
+		return obd_timeout / 2;
+
+	if (pinger) {
+		/* There might be a delay till the next RPC. In fact it is two
+		 * PING_INTERVALs due to ptlrpc_pinger_main logic. */
+		timeout = 2 * PING_INTERVAL;
+	} else {
+		/* For the lock prolong, we have an RPC in hand, which may still
+		 * get its reply lost. Therefore, it may be either this one or
+		 * the next client's RPC times out, take the max.
+		 * Considering the current RPC, take just the left time. */
+		LASSERT(at != NULL);
+		at_timeout = at_est2timeout(obd_at_get(obd, at)) + netl;
+		req_timeout = max(rpc_left_time + netl, at_timeout);
+		/* Adding the RPC resend time - not needed in the ping evictor
+		 * case, export is updated on re-connect  */
+		timeout = req_timeout + at_timeout;
+	}
+
+	/* Adding the re-connect time: 1st re-connect timeout,
+	 * 2nd reconnect error, 3rd reconnect success. */
+	timeout += 3 * (INITIAL_CONNECT_TIMEOUT + netl);
+
+	/* Let's be a bit more conservative than client */
+	return max(timeout + (timeout >> 4),
+		   (timeout_t)obd_get_ldlm_enqueue_min(obd));
+}
+
+/**
+ * Used for lock prolog timeout, calculates a timeout for CANCEL to come.
+ * Also used for recovery, calculates a timeout for a next recovery RPC to come.
+ * In this case, there is an RPC, in hand. Thus, a particular svcpt AT is used.
+ *
+ * The reverse import network AT is used as an estimate for the client side one.
+ */
+timeout_t ptlrpc_export_prolong_timeout(struct ptlrpc_request *req,
+					bool recovery)
+{
+	timeout_t netl;
+
+	if (recovery)
+		netl = lustre_msg_get_service_timeout(req->rq_reqmsg);
+	else
+		netl = obd_at_get(req->rq_export->exp_obd,
+				  &req->rq_export->exp_imp_reverse->
+				  imp_at.iat_net_latency);
+
+	return ptlrpc_export_timeout(req->rq_export->exp_obd,
+				     &req->rq_rqbd->rqbd_svcpt->scp_at_estimate,
+				     netl, req->rq_deadline -
+				     ktime_get_real_seconds(), false);
+}
+
+/**
+ * Used for ping evictor, calculates a timeout for any next RPC to come.
+ * As there are different portals and the AT stats is separated for them,
+ * just the last RPC AT is used here.
+ *
+ * The reverse import network AT is used as an estimate for the client side one.
+ */
+static timeout_t ptlrpc_export_pinger_timeout(struct ptlrpc_request *req)
+{
+	struct obd_import *revimp = req->rq_export->exp_imp_reverse;
+	timeout_t netl = obd_at_get(req->rq_export->exp_obd,
+				    &revimp->imp_at.iat_net_latency);
+
+	return ptlrpc_export_timeout(req->rq_export->exp_obd,
+				     &req->rq_rqbd->rqbd_svcpt->scp_at_estimate,
+				     netl, 0, true);
+}
+
+/**
+ * In case the net was down and just came back, when the 1st timeout has been
+ * already expired, clients just keep sending re-connects. Applying the same
+ * formula as in ptlrpc_export_timeout() to this case we get:
+ * - a previous reconnect to not yet recovered network, times out;
+ * - the second reconnect to the failover pair, ENODEV;
+ * - the third reconnect succeeds;
+ */
+static timeout_t ptlrpc_export_extra_timeout(struct obd_export *exp)
+{
+	timeout_t netl;
+
+	/* As this is not the 1st re-connection failure, the client might
+	 * have net latency get extended to the max - CONNECTION_SWITCH_MAX */
+	netl = obd_at_get(exp->exp_obd,
+			  &exp->exp_imp_reverse->imp_at.iat_net_latency);
+	return 3 * INITIAL_CONNECT_TIMEOUT + CONNECTION_SWITCH_MAX + 2 * netl;
+}
+
+/**
  * This function makes sure dead exports are evicted in a timely manner.
  * This function is only called when some export receives a message (i.e.,
  * the network is up.)
  */
-void ptlrpc_update_export_timer(struct obd_export *exp, time64_t extra_delay)
+void ptlrpc_update_export_timer(struct ptlrpc_request *req)
 {
-	struct obd_export *oldest_exp, *newest_exp;
-	time64_t oldest_time, current_time;
-	bool	evict = false;
+	struct obd_export *oldest_exp, *newest_exp, *exp;
+	time64_t current_time, timeout;
+	bool evict = false;
+	void *data;
+	int rc;
 	ENTRY;
 
-	LASSERT(exp);
+	LASSERT(req != NULL);
+	LASSERT(req->rq_export != NULL);
 
-	/*
-	 * Compensate for slow machines, etc, by faking our request time
-	 * into the future.  Although this can break the strict time-ordering
-	 * of the list, we can be really lazy here - we don't have to evict
-	 * at the exact right moment.  Eventually, all silent exports
-	 * will make it to the top of the list.
-	 */
-
-	/* Do not pay attention on 1sec or smaller renewals. */
+	exp = req->rq_export;
 	current_time = ktime_get_real_seconds();
-	/* 1 seconds */
-	if (exp->exp_last_request_time + 1 >= current_time + extra_delay)
-		RETURN_EXIT;
 
-	exp->exp_last_request_time = current_time + extra_delay;
+	rc = obd_export_timed_init(exp, &data);
+	if (rc)
+		/* will be updated next time */
+		RETURN_EXIT;
 
 	/*
 	 * exports may get disconnected from the chain even though the
@@ -1130,54 +1254,60 @@ void ptlrpc_update_export_timer(struct obd_export *exp, time64_t extra_delay)
 	 * manipulating the lists
 	 */
 	spin_lock(&exp->exp_obd->obd_dev_lock);
-
-	if (list_empty(&exp->exp_obd_chain_timed)) {
+	if (list_empty(&exp->exp_timed_chain)) {
 		/* this one is not timed */
 		spin_unlock(&exp->exp_obd->obd_dev_lock);
-		RETURN_EXIT;
+		GOTO(err, 0);
 	}
 
-	newest_exp = list_last_entry(&exp->exp_obd->obd_exports_timed,
-				     struct obd_export, exp_obd_chain_timed);
+	exp->exp_last_request_time = current_time;
 
-	list_move_tail(&exp->exp_obd_chain_timed,
-		       &exp->exp_obd->obd_exports_timed);
+	timeout = ptlrpc_export_pinger_timeout(req);
+
+	/* Do not pay attention on 1sec or smaller renewals. */
+	if (exp->exp_deadline + 1 >= current_time + timeout) {
+		spin_unlock(&exp->exp_obd->obd_dev_lock);
+		GOTO(err, 0);
+	}
+
+	newest_exp = obd_export_timed_get(exp->exp_obd, true);
+	obd_export_timed_del(exp);
+	exp->exp_deadline = current_time + timeout;
+	obd_export_timed_add(exp, &data);
 
 	if (test_bit(OBDF_RECOVERING, exp->exp_obd->obd_flags)) {
 		/* be nice to everyone during recovery */
 		spin_unlock(&exp->exp_obd->obd_dev_lock);
-		RETURN_EXIT;
+		GOTO(err, 0);
 	}
-
-	oldest_exp = list_entry(exp->exp_obd->obd_exports_timed.next,
-				struct obd_export, exp_obd_chain_timed);
-
-	oldest_time = oldest_exp->exp_last_request_time;
+	oldest_exp = obd_export_timed_get(exp->exp_obd, false);
 
 	/* Check if the oldest entry is expired. */
-	if (exp->exp_obd->obd_eviction_timer == 0 &&
-	    current_time > oldest_time + PING_EVICT_TIMEOUT + extra_delay) {
-
-		if (current_time < newest_exp->exp_last_request_time +
-			     PING_EVICT_TIMEOUT / 2) {
-			/* If import is active - evict stale clients */
-			evict = true;
-		} else {
-			/*
-			 * We need a second timer, in case the net was down and
-			 * it just came back. Since the pinger may skip every
-			 * other PING_INTERVAL (see note in ptlrpc_pinger_main),
-			 * we better wait for 3.
-			 */
-			exp->exp_obd->obd_eviction_timer =
-				ktime_get_real_seconds() + 3 * PING_INTERVAL;
-			CDEBUG(D_HA, "%s: Think about evicting %s from %lld\n",
-			       exp->exp_obd->obd_name,
-			       obd_export_nid2str(oldest_exp), oldest_time);
-
+	if (exp->exp_obd->obd_eviction_timer == 0) {
+		if (current_time > oldest_exp->exp_deadline) {
+			timeout = newest_exp->exp_last_request_time +
+				((newest_exp->exp_deadline -
+				  newest_exp->exp_last_request_time) >> 1);
+			if (current_time < timeout) {
+				/* If import is active - evict stale clients */
+				evict = true;
+			} else {
+				/*
+				 * We need a second timer, in case the net was
+				 * down and it just came back.
+				 */
+				exp->exp_obd->obd_eviction_timer =
+					ktime_get_real_seconds() +
+					ptlrpc_export_extra_timeout(oldest_exp);
+				CDEBUG(D_HA, "%s: Think about evicting %s "
+				       "from %lld deadline at %lld\n",
+				       exp->exp_obd->obd_name,
+				       obd_export_nid2str(oldest_exp),
+				       oldest_exp->exp_deadline,
+				       exp->exp_obd->obd_eviction_timer);
+			}
 		}
 	}
-
 	spin_unlock(&exp->exp_obd->obd_dev_lock);
 
 	if (evict) {
@@ -1185,7 +1315,7 @@ void ptlrpc_update_export_timer(struct obd_export *exp, time64_t extra_delay)
 		ping_evictor_wake(exp);
 	} else {
 		if (ktime_get_real_seconds() >
-		    (exp->exp_obd->obd_eviction_timer + extra_delay)) {
+		    exp->exp_obd->obd_eviction_timer) {
 			/*
 			 * The evictor won't evict anyone who we've heard from
 			 * recently, so we don't have to check before we start
@@ -1197,6 +1327,8 @@ void ptlrpc_update_export_timer(struct obd_export *exp, time64_t extra_delay)
 	}
 
 	EXIT;
+err:
+	obd_export_timed_fini(exp, &data);
 }
 
 /**
@@ -2171,7 +2303,8 @@ static int ptlrpc_server_handle_req_in(struct ptlrpc_service_part *svcpt,
 
 		if (rc)
 			goto err_req;
-		ptlrpc_update_export_timer(req->rq_export, 0);
+
+		ptlrpc_update_export_timer(req);
 	}
 
 	/* req_in handling should/must be fast */
@@ -2306,9 +2439,8 @@ static int ptlrpc_server_handle_request(struct ptlrpc_service_part *svcpt,
 	if (likely(request->rq_export)) {
 		if (unlikely(ptlrpc_check_req(request)))
 			goto put_conn;
-		ptlrpc_update_export_timer(request->rq_export,
-					   div_u64(timediff_usecs,
-						   USEC_PER_SEC / 2));
+
+		ptlrpc_update_export_timer(request);
 	}
 
 	/*

lustre/include/lustre_dlm.h
@@ -1515,7 +1515,6 @@ int ldlm_request_cancel(struct ptlrpc_request *req,
 
 void ldlm_revoke_export_locks(struct obd_export *exp);
 timeout_t ldlm_bl_timeout(struct ldlm_lock *lock);
-timeout_t ldlm_bl_timeout_by_rpc(struct ptlrpc_request *req);
 #endif
 int ldlm_del_waiting_lock(struct ldlm_lock *lock);
 int ldlm_refresh_waiting_lock(struct ldlm_lock *lock, timeout_t timeout);

lustre/include/lustre_export.h
@@ -196,7 +196,7 @@ struct obd_export {
 	 * order
 	 * protected by obd_dev_lock
 	 */
-	struct list_head	exp_obd_chain_timed;
+	struct list_head	exp_timed_chain;
 	/** Obd device of this export */
 	struct obd_device      *exp_obd;
 	/**
@@ -223,6 +223,7 @@ struct obd_export {
 	__u64			exp_last_committed;
 	/** When was last request received */
 	time64_t		exp_last_request_time;
+	time64_t		exp_deadline;
 	/** On replay all requests waiting for replay are linked here */
 	struct list_head	exp_req_replay_queue;
 	/**
@@ -260,7 +261,7 @@ struct obd_export {
 				 */
 				exp_old_falloc:1,
 				exp_hashed:1,
-				exp_not_timed:1;
+				exp_timed:1;
 	/* also protected by exp_lock */
 	enum lustre_sec_part	exp_sp_peer;
 	struct sptlrpc_flavor	exp_flvr;		/* current */

lustre/include/lustre_net.h
@@ -2261,8 +2261,9 @@ int ptlrpc_service_health_check(struct ptlrpc_service *service);
 void ptlrpc_server_drop_request(struct ptlrpc_request *req);
 void ptlrpc_request_change_export(struct ptlrpc_request *req,
 				  struct obd_export *export);
-void ptlrpc_update_export_timer(struct obd_export *exp,
-				time64_t extra_delay);
+void ptlrpc_update_export_timer(struct ptlrpc_request *req);
+timeout_t ptlrpc_export_prolong_timeout(struct ptlrpc_request *req,
+					bool recovery);
 
 int ptlrpc_hr_init(void);
 void ptlrpc_hr_fini(void);

lustre/include/obd.h
@@ -683,7 +683,7 @@ struct obd_device {
 	struct obd_export       *obd_self_export;
 	struct obd_export	*obd_lwp_export;
 	/* list of exports in LRU order, for ping evictor, with obd_dev_lock */
-	struct list_head	obd_exports_timed;
+	struct rb_root		obd_exports_timed;
 	time64_t		obd_eviction_timer;	/* for ping evictor */
 
 	atomic_t                obd_max_recoverable_clients;

lustre/include/obd_class.h
@@ -406,6 +406,12 @@ void class_import_put(struct obd_import *);
 struct obd_import *class_new_import(struct obd_device *obd);
 void class_destroy_import(struct obd_import *exp);
 
+int obd_export_timed_init(struct obd_export *exp, void **data);
+void obd_export_timed_fini(struct obd_export *exp, void **data);
+void obd_export_timed_add(struct obd_export *exp, void **data);
+void obd_export_timed_del(struct obd_export *exp);
+struct obd_export *obd_export_timed_get(struct obd_device *obd, bool last);
+
 #ifdef HAVE_SERVER_SUPPORT
 struct obd_type *class_search_type(const char *name);
 struct obd_type *class_get_type(const char *name);

lustre/include/obd_support.h
@@ -41,7 +41,6 @@ extern unsigned int obd_lbug_on_eviction;
 extern unsigned int obd_timeout;          /* seconds */
 extern unsigned int ldlm_timeout;         /* seconds */
 extern unsigned int ping_interval;        /* seconds */
-extern unsigned int ping_evict_timeout_multiplier;
 extern unsigned int obd_timeout_set;
 extern unsigned int ldlm_timeout_set;
 extern unsigned int bulk_timeout;
@@ -90,7 +89,7 @@ extern bool obd_enable_fname_encoding;
  * and there's no urgent need to evict a client just because it's idle, we
  * should be very conservative here.
  */
-#define PING_EVICT_TIMEOUT (PING_INTERVAL * ping_evict_timeout_multiplier)
+#define PING_EVICT_TIMEOUT (PING_INTERVAL * 6)
 #define DISK_TIMEOUT 50          /* Beyond this we warn about disk speed */
 #define CONNECTION_SWITCH_MIN 5U /* Connection switching rate limiter */
  /* Max connect interval for nonresponsive servers; ~50s to avoid building up

lustre/ldlm/ldlm_extent.c
@@ -770,7 +770,7 @@ void ldlm_lock_prolong_one(struct ldlm_lock *lock,
 	/* OK. this is a possible lock the user holds doing I/O
 	 * let's refresh eviction timer for it.
 	 */
-	timeout = ldlm_bl_timeout_by_rpc(arg->lpa_req);
+	timeout = ptlrpc_export_prolong_timeout(arg->lpa_req, false);
 	LDLM_DEBUG(lock, "refreshed to %ds. ", timeout);
 	ldlm_refresh_waiting_lock(lock, timeout);
 }

commit 0498ab0fd65e4eff81d2692e765f9f43666e212e

LU-18878 ptlrpc: evictor/prolong timeout formula fix
Let's stay with the main rule - cluster is supposed to survive just
one (the worst ever) failure, not a set of failures. The current
formula covers:

- an RPC timeout;
- the 1st reconnect time out;
- the 2nd reconnect to the failover pair returns an error;
- the 3rd reconnect to the original node succeeds;

i.e. 2 failures. At the same time, The 2nd (reconnect timeout) failure
was the result of the LNET problems, which were fixed separately.

Besides that, a fix for the ping evictor timeout, a normal RPC case,
where the timeout of an ping itself was not accounted in the formula.

Besides that, a fix for the extra timeout used for obd_eviction_timer,
where net latencies for the accounted RPCs were taken not correctly.

HPE-bug-id: LUS-12823
Fixes: 4cb897ca4a ("LU-18878 ptlrpc: improve ping evictor and recovery timeout")
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: Ic7ccc83c5f10a8dd3382b9a337b70bc7c41a70ff
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163269
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60085
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/service.c
@@ -1124,13 +1124,9 @@ static void ptlrpc_server_finish_active_request(
  * Whereas a problem client may be still alive trying hard to reconnect and to
  * resend its RPCs, we should not consider the worst ever case, consisting of
  * a chain of failures on each step. Let this timeout survive a recovery of
- * just 1 failure, but let this be the worst possible one - a dead server NID:
- *
+ * just 1 failure:
  * - an RPC timeout;
- * - the first re-connect is sent to the same NID and times out;
- * - the second re-connect to the failover pair returns an error;
- * - the third re-connect to the original node to a different NID succeeds;
- * - the RPC resend succeeds;
+ * - a re-connect success;
  *
  * For lock prolong timeout, we are in the middle of the process -
  * BL AST is sent, CANCEL is ahead - it is still 1 reply for the current RPC
@@ -1152,26 +1148,27 @@ static timeout_t ptlrpc_export_timeout(struct obd_device *obd,
 	if (obd_at_off(obd))
 		return obd_timeout / 2;
 
+	LASSERT(at != NULL);
+	at_timeout = at_est2timeout(obd_at_get(obd, at)) + netl;
+
 	if (pinger) {
 		/* There might be a delay till the next RPC. In fact it is two
-		 * PING_INTERVALs due to ptlrpc_pinger_main logic. */
-		timeout = 2 * PING_INTERVAL;
+		 * PING_INTERVALs due to ptlrpc_pinger_main logic.
+		 * In addition, the ping itself may time out, but no resend will
+		 * be needed, export is updated on re-connect */
+		timeout = 2 * PING_INTERVAL + at_timeout;
 	} else {
 		/* For the lock prolong, we have an RPC in hand, which may still
-		 * get its reply lost. Therefore, it may be either this one or
-		 * the next client's RPC times out, take the max.
+		 * get its reply lost. Thus, it may be either this one or the
+		 * next client's RPC times out, take the max.
 		 * Considering the current RPC, take just the left time. */
-		LASSERT(at != NULL);
-		at_timeout = at_est2timeout(obd_at_get(obd, at)) + netl;
-		req_timeout = max(rpc_left_time + netl, at_timeout);
-		/* Adding the RPC resend time - not needed in the ping evictor
-		 * case, export is updated on re-connect  */
+		req_timeout = max(rpc_left_time + (netl >> 1), at_timeout);
+		/* Adding the RPC resend time */
 		timeout = req_timeout + at_timeout;
 	}
 
-	/* Adding the re-connect time: 1st re-connect timeout,
-	 * 2nd reconnect error, 3rd reconnect success. */
-	timeout += 3 * (INITIAL_CONNECT_TIMEOUT + netl);
+	/* Adding the re-connect time */
+	timeout += INITIAL_CONNECT_TIMEOUT + netl;
 
 	/* Let's be a bit more conservative than client */
 	return max(timeout + (timeout >> 4),
@@ -1228,22 +1225,26 @@ static timeout_t ptlrpc_export_pinger_timeout(struct ptlrpc_request *req)
 }
 
 /*
- * In case the net was down and just came back, when the 1st timeout has been
- * already expired, clients just keep sending re-connects. Applying the same
- * formula as in ptlrpc_export_timeout() to this case we get:
+ * In this case the net was down and just came back, when the 1st timeout has
+ * been already expired, clients just keep sending re-connects, switching
+ * between different connections, thus what is to be covered:
  * - a previous reconnect to not yet recovered network, times out;
  * - the second reconnect to the failover pair, ENODEV;
  * - the third reconnect succeeds;
  */
 static timeout_t ptlrpc_export_extra_timeout(struct obd_export *exp)
 {
-	timeout_t netl;
+	timeout_t netl, tout;
 
 	/* As this is not the 1st re-connection failure, the client might
 	 * have net latency get extended to the max - CONNECTION_SWITCH_MAX */
 	netl = obd_at_get(exp->exp_obd,
 			  &exp->exp_imp_reverse->imp_at.iat_net_latency);
-	return 3 * INITIAL_CONNECT_TIMEOUT + CONNECTION_SWITCH_MAX + 2 * netl;
+	tout = 3 * (INITIAL_CONNECT_TIMEOUT +
+		    max((timeout_t)CONNECTION_SWITCH_MAX, netl));
+
+	/* Let's be a bit more conservative than client */
+	return tout + (tout >> 4);
 }
 
 /*