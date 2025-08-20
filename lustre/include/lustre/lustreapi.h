lustre/lustre-release

commit 98e2d3230ef02ea41f7999cfe395bb68e69976f1

LU-10026 utils: reserve fields for compress find
Reserve fields for "lfs find" compressed file handling.

Fill holes in struct find_param with fp_unused members so that they
can be found more easily in the future.

Test-Parameters: trivial
Whamcloud-bug-id: EX-6856
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ic87b26a9120deb958247797512e648164b2540e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59877
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Artem Blagodarenko <ablagodarenko@ddn.com>
Reviewed-by: Frederick Dilger <fdilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre/lustreapi.h
@@ -400,17 +400,20 @@ struct find_param {
 	unsigned long long	 fp_comp_end_units;
 	unsigned long long	 fp_mdt_count;
 	unsigned int		 fp_projid;
+	unsigned int		 fp_unused_int;
 	unsigned long long	 fp_blocks;
 	unsigned long long	 fp_blocks_units;
 
 	unsigned long		 fp_got_uuids:1,
 				 fp_obds_printed:1,
 				 fp_no_follow:1,
-				 fp_hex_idx:1;
+				 fp_hex_idx:1,
+				 fp_unused_bits:28;
 	unsigned int		 fp_depth;
 	unsigned int		 fp_hash_type;
 	unsigned int		 fp_time_margin; /* time margin in seconds */
 	__u32			 fp_foreign_type;
+	__u32			 fp_unused_u32;
 	unsigned long long	 fp_ext_size;
 	unsigned long long	 fp_ext_size_units;
 
@@ -425,15 +428,40 @@ struct find_param {
 	unsigned int		 fp_hash_inflags;
 	unsigned int		 fp_hash_exflags;
 	__u8			 fp_thread_count;
+	__u8			 fp_unused_byte1;
+	__u8			 fp_unused_byte2;
+	__u8			 fp_unused_byte3;
 	/* Print all information (lfs find only) */
 	char			 *fp_format_printf_str;
 	nlink_t			 fp_nlink;
 	__u64			 fp_attrs;
 	__u64			 fp_neg_attrs;
-	struct xattr_match_info	*fp_xattr_match_info;
-	unsigned long int	 fp_skip_percent;
+
+	unsigned int		 fp_compr_type;
+	unsigned int		 fp_compr_lvl;
+	unsigned int		 fp_compr_chunk;
+
+	unsigned int		 fp_check_compr_type:1,
+				 fp_exclude_compr_type:1,
+				 fp_check_compr_lvl:1,
+				 fp_exclude_compr_lvl:1,
+				 fp_check_compr_chunk:1,
+				 fp_exclude_compr_chunk:1,
+				 fp_unused_bit1:1,  /* Fields available to use*/
+				 fp_unused_bit2:1,  /* once used, we must add */
+				 fp_unused_bit3:1,  /* a separate flag field  */
+				 fp_unused_bit4:1,  /* at                     */
+				 fp_unused_bits2:22; /* the end of this struct */
+
+	long long		 fp_compr_lvl_sign:2,
+				 fp_compr_chunk_sign:2,
+				 fp_unused1_sign:2,  /* add sperarate field */
+				 fp_unused_signs:50; /* once used up. */
+
+	unsigned char		 fp_skip_percent;
 	unsigned long long	 fp_skip_total;
 	unsigned long long	 fp_skip_count;
+	struct xattr_match_info	*fp_xattr_match_info;
 	struct find_work_queue	*fp_queue;
 };
 

lustre/utils/lfs.c
@@ -6183,10 +6183,10 @@ static int lfs_find(int argc, char **argv)
 				param.fp_check_hash_flag = 1;
 			param.fp_exclude_hash_type = !!neg_opt;
 			break;
-		case 'k':
-			param.fp_skip_percent = strtoul(optarg, &endptr, 10);
-			if (param.fp_skip_percent < 0 ||
-			    param.fp_skip_percent >= 100 ||
+		case 'k': {
+			unsigned long pct = strtoul(optarg, &endptr, 10);
+
+			if (pct < 0 || pct >= 100 ||
 			    (*endptr != '\0' && *endptr != '%')) {
 				ret = -1;
 				fprintf(stderr,
@@ -6194,7 +6194,9 @@ static int lfs_find(int argc, char **argv)
 					optarg);
 				goto err;
 			}
+			param.fp_skip_percent = pct;
 			break;
+		}
 		case 'l':
 			param.fp_lazy = 1;
 			break;

lustre/utils/liblustreapi.c
@@ -4801,7 +4801,7 @@ matched:
 static bool find_skip_file(struct find_param *param)
 {
 	if (param->fp_skip_count * 100 <
-	    param->fp_skip_percent * param->fp_skip_total++) {
+	    param->fp_skip_total++ * param->fp_skip_percent) {
 		param->fp_skip_count++;
 		return true;
 	}

commit 71f8fd5241fc59cbf9a10fd6baf92825ce5a0300

LU-10026 utils: reserve more values for compression
A few more values reserved for compression missed in previous patch.

Remove old compatibility values from 2.11.

Test-Parameters: trivial
Fixes: 98e2d3230e ("LU-10026 utils: reserve fields for compress find")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ica2d97d2da0799dbf89f468eea0a663d812540e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60177
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Artem Blagodarenko <ablagodarenko@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/lustre/lustreapi.h
@@ -202,6 +202,9 @@ enum llapi_layout_verbose  {
 	VERBOSE_EXT_SIZE	=  0x40000,
 	VERBOSE_INHERIT		=  0x80000,
 	VERBOSE_INHERIT_RR	= 0x100000,
+	VERBOSE_COMPRESS_TYPE	= 0x200000,
+	VERBOSE_COMPRESS_LEVEL	= 0x400000,
+	VERBOSE_COMPRESS_CHUNK	= 0x800000,
 	VERBOSE_DEFAULT		= VERBOSE_STRIPE_COUNT | VERBOSE_STRIPE_SIZE |
 				  VERBOSE_STRIPE_OFFSET | VERBOSE_POOL |
 				  VERBOSE_OBJID | VERBOSE_GENERATION |
@@ -212,11 +215,6 @@ enum llapi_layout_verbose  {
 				  VERBOSE_MIRROR_ID | VERBOSE_EXT_SIZE |
 				  VERBOSE_INHERIT | VERBOSE_INHERIT_RR
 };
-/* Compatibility with original names */
-#define VERBOSE_SIZE	VERBOSE_STRIPE_SIZE
-#define VERBOSE_COUNT	VERBOSE_STRIPE_COUNT
-#define VERBOSE_OFFSET	VERBOSE_STRIPE_OFFSET
-#define VERBOSE_LAYOUT	VERBOSE_PATTERN
 
 enum {
 	NEWERXY_ATIME = 0,	/* neweraY */
@@ -993,10 +991,11 @@ int llapi_layout_merge(struct llapi_layout **dst_layout,
  * stored using RAID0.  That is, data will be split evenly and without
  * redundancy across all OSTs in the layout.
  */
-#define LLAPI_LAYOUT_RAID0		0ULL
-#define LLAPI_LAYOUT_MDT		2ULL
-#define LLAPI_LAYOUT_OVERSTRIPING	4ULL
-#define LLAPI_LAYOUT_FOREIGN		8ULL
+#define LLAPI_LAYOUT_RAID0		0x00000000ULL
+#define LLAPI_LAYOUT_MDT		0x00000002ULL
+#define LLAPI_LAYOUT_OVERSTRIPING	0x00000004ULL
+#define LLAPI_LAYOUT_FOREIGN		0x00000008ULL
+#define LLAPI_LAYOUT_COMPRESS		0x00000010ULL
 
 /**
  * The layout includes a specific set of OSTs on which to allocate.

lustre/utils/lfs.c
@@ -6858,7 +6858,7 @@ static int lfs_getstripe_internal(int argc, char **argv,
 			break;
 		case 'c':
 			if (!(param->fp_verbose & VERBOSE_DETAIL)) {
-				param->fp_verbose |= VERBOSE_COUNT;
+				param->fp_verbose |= VERBOSE_STRIPE_COUNT;
 				param->fp_max_depth = 0;
 			}
 			break;
@@ -7246,7 +7246,7 @@ static int lfs_getdirstripe(int argc, char **argv)
 		switch (c) {
 		case 'c':
 		case 'T':
-			param.fp_verbose |= VERBOSE_COUNT;
+			param.fp_verbose |= VERBOSE_STRIPE_COUNT;
 			break;
 		case 'D':
 			param.fp_get_default_lmv = 1;