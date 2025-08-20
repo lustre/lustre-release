lustre/lustre-release

commit 07f6a59ad71324ed253c2eba519cf095f346bde9

LU-18556 hsm: optimize llog record modification
This commit introduces a new llog modification mechanism for HSM
operations to address inefficiencies caused by prior reliance on
catalog processing. The new approach directly modifies llog record,
eliminating the need for catalog-based processing and reducing
latency.

Key changes include:
* Replacing the hsm_action_item (HAI) with a full in-memory llog
 record representation, increasing memory usage by ~80 bytes per
 record but removing the need for a dedicated llog cookie hash
 table.
* Unifying the coordinator's read/store logic for HAI data into a
 single in-memory item shared by mdt_hsm_agent_send() and
 mdt_hsm_add_hsr(). This reduces memory allocation steps: only one
 cdt_agent_req allocation is now required during llog read
 operations, eliminating subsequent allocations/copies.

Performance results on VMs 2 MDTs/2 OSTs/2 Clients no-op copytool:
Test 1 (1M archive requests): 572s -> 187s (~3 times faster)
Test 2 (1M archive + 1M queued): 558s -> 392s (~1.4 times faster)

HPE-bug-id: LUS-12583
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: I4b6e697bc3b1f0cf2c76f5433b49affbc933c653
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57428
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_hsm_cdt_agent.c
@@ -21,6 +21,7 @@
 #include <lustre_export.h>
 #include <lprocfs_status.h>
 #include <lustre_kernelcomm.h>
+#include <lustre_log.h>
 #include "mdt_internal.h"
 
 /*
@@ -339,35 +340,109 @@ static int mdt_hsm_send_action_to_each_archive(struct mdt_thread_info *mti,
 	RETURN(rc);
 }
 
+int mdt_hsm_agent_modify_record(const struct lu_env *env,
+				struct mdt_device *mdt,
+				struct hsm_mem_req_rec *hmm)
+{
+	struct obd_device *obd = mdt2obd_dev(mdt);
+	struct llog_ctxt *lctxt;
+	struct llog_cookie cookie;
+	int rc;
+
+	lctxt = llog_get_context(obd, LLOG_AGENT_ORIG_CTXT);
+	if (lctxt == NULL || lctxt->loc_handle == NULL)
+		RETURN(-ENOENT);
+
+	cookie.lgc_offset = hmm->mr_offset;
+	cookie.lgc_index = hmm->mr_rec.arr_hdr.lrh_index;
+	hmm->mr_rec.arr_req_change = ktime_get_real_seconds();
+	rc = llog_cat_modify_rec(env, lctxt->loc_handle, &hmm->mr_lid,
+				 (struct llog_rec_hdr *)&hmm->mr_rec,
+				 &cookie);
+
+	llog_ctxt_put(lctxt);
+	return rc;
+}
+
+static size_t hsr_hal_size(struct hsm_scan_request *rq)
+{
+	struct cdt_agent_req *car;
+	struct hsm_action_item *hai;
+	size_t sz;
+
+	sz = sizeof(struct hsm_action_list) +
+	     __ALIGN_KERNEL(strlen(rq->hsr_fsname) + 1, 8);
+	list_for_each_entry(car, &rq->hsr_cars, car_scan_list) {
+		hai = &car->car_hai;
+		sz += __ALIGN_KERNEL(hai->hai_len, 8);
+	}
+	return sz;
+}
+
+static int hsr_hal_copy(struct hsm_scan_request *rq, void *buf, size_t buf_size)
+{
+	struct hsm_action_list *hal = buf;
+	struct cdt_agent_req *car;
+	struct hsm_action_item *hai;
+	struct hsm_action_item *shai;
+
+	hal->hal_version = rq->hsr_version;
+	strscpy(hal->hal_fsname, rq->hsr_fsname, MTI_NAME_MAXLEN + 1);
+	hal->hal_archive_id = hsr_get_archive_id(rq);
+	hal->hal_count = 0;
+
+	hai = hai_first(hal);
+	/* Copy only valid hai base on a record status */
+	list_for_each_entry(car, &rq->hsr_cars, car_scan_list) {
+		shai = &car->car_hai;
+		hal->hal_flags = car->car_flags;
+		if (car->car_hmm->mr_rec.arr_status == ARS_FAILED)
+			continue;
+		if ((buf_size - ((char *)hai - (char *)buf)) < shai->hai_len) {
+			CDEBUG(D_HA, "buffer overflow for hsm_action_item\n");
+			return -EOVERFLOW;
+		}
+		memcpy(hai, shai, shai->hai_len);
+		hal->hal_count++;
+		hai = hai_next(hai);
+	}
+
+	return 0;
+}
+
 /**
- * send a HAL to the agent
+ * Checks agent records, creates a hal and sends it to the agent. Updates llog
+ * records at the end.
  * \param mti [IN] context
- * \param hal [IN] request (can be a kuc payload)
- * \param purge [IN] purge mode (no record)
+ * \param rq [IN] request
+ * \param purge [IN] purge mode (not register a record)
  * \retval 0 success
  * \retval -ve failure
  * This function supposes:
  *  - all actions are for the same archive number
  *  - in case of cancel, all cancel are for the same agent
  * This implies that request split has to be done
- *  before when building the hal
+ *  before when building the rq
  */
-int mdt_hsm_agent_send(struct mdt_thread_info *mti,
-		       struct hsm_action_list *hal, bool purge)
+int mdt_hsm_agent_send(struct mdt_thread_info *mti, struct hsm_scan_request *rq,
+		       bool purge)
 {
-	struct obd_export	*exp;
-	struct mdt_device	*mdt = mti->mti_mdt;
-	struct coordinator	*cdt = &mti->mti_mdt->mdt_coordinator;
-	struct hsm_action_list	*buf = NULL;
-	struct hsm_action_item	*hai;
-	struct obd_uuid		 uuid;
-	int			 len, i, rc = 0;
-	bool			 fail_request;
-	bool			 is_registered = false;
+	struct obd_export *exp;
+	struct mdt_device *mdt = mti->mti_mdt;
+	struct coordinator *cdt = &mti->mti_mdt->mdt_coordinator;
+	struct hsm_action_list *buf = NULL;
+	struct hsm_action_item *hai;
+	struct cdt_agent_req *car;
+	struct obd_uuid uuid;
+	int len, rc = 0;
+	int fail_request = 0;
+	bool is_registered = false;
+	u32 archive_id = hsr_get_archive_id(rq);
+
 	ENTRY;
 
-	rc = mdt_hsm_find_best_agent(cdt, hal->hal_archive_id, &uuid);
-	if (rc && hal->hal_archive_id == 0) {
+	rc = mdt_hsm_find_best_agent(cdt, archive_id, &uuid);
+	if (rc && archive_id == 0) {
 		uint notrmcount = 0;
 		int rc2 = 0;
 
@@ -380,11 +455,8 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 		 *     _ create a new LLOG record for each archive_id
 		 *       presently being served by any CT
 		 */
-		hai = hai_first(hal);
-		for (i = 0; i < hal->hal_count; i++,
-		     hai = hai_next(hai)) {
-			struct hsm_record_update update;
-
+		list_for_each_entry(car, &rq->hsr_cars, car_scan_list) {
+			hai = &car->car_hai;
 			/* only removes are concerned */
 			if (hai->hai_action != HSMA_REMOVE) {
 				/* count if other actions than HSMA_REMOVE,
@@ -405,102 +477,51 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 			 * unless a method to record already successfully
 			 * reached archive_ids is implemented */
 
-			update.cookie = hai->hai_cookie;
-			update.status = ARS_SUCCEED;
-			rc2 = mdt_agent_record_update(mti, &update, 1);
-			if (rc2) {
-				CERROR("%s: mdt_agent_record_update() "
-				      "failed, cannot update "
-				      "status to %s for cookie "
-				      "%#llx: rc = %d\n",
-				      mdt_obd_name(mdt),
-				      agent_req_status2name(ARS_SUCCEED),
-				      hai->hai_cookie, rc2);
-				break;
-			}
+			car->car_hmm->mr_rec.arr_status = ARS_SUCCEED;
 		}
 		/* only remove requests with archive_id=0 */
 		if (notrmcount == 0)
-			RETURN(rc2);
+			GOTO(update_records, rc = rc2);
 
 	}
 
 	if (rc) {
 		CERROR("%s: Cannot find agent for archive %d: rc = %d\n",
-		       mdt_obd_name(mdt), hal->hal_archive_id, rc);
-		RETURN(rc);
+		       mdt_obd_name(mdt), archive_id, rc);
+		GOTO(update_records, rc);
 	}
 
-	CDEBUG(D_HSM, "Agent %s selected for archive %d\n", obd_uuid2str(&uuid),
-	       hal->hal_archive_id);
-
-	len = hal_size(hal);
-	buf = kuc_alloc(len, KUC_TRANSPORT_HSM, HMT_ACTION_LIST);
-	if (IS_ERR(buf))
-		RETURN(PTR_ERR(buf));
-	memcpy(buf, hal, len);
+	CDEBUG(D_HSM, "Agent %s selected for archive %d request %px items %d\n",
+	       obd_uuid2str(&uuid), archive_id, rq, rq->hsr_count);
 
 	/* Check if request is still valid (cf file hsm flags) */
-	fail_request = false;
-	hai = hai_first(hal);
-	for (i = 0; i < hal->hal_count; i++, hai = hai_next(hai)) {
+	list_for_each_entry(car, &rq->hsr_cars, car_scan_list) {
 		struct mdt_object *obj;
 		struct md_hsm hsm;
 
+		hai = &car->car_hai;
 		if (hai->hai_action == HSMA_CANCEL)
 			continue;
 
 		obj = mdt_hsm_get_md_hsm(mti, &hai->hai_fid, &hsm);
 		if (!IS_ERR(obj)) {
 			mdt_object_put(mti->mti_env, obj);
-		} else if (PTR_ERR(obj) == -ENOENT) {
-			struct hsm_record_update update = {
-				.cookie = hai->hai_cookie,
-				.status = ARS_FAILED,
-			};
-
-			if (hai->hai_action == HSMA_REMOVE)
+		} else {
+			if (PTR_ERR(obj) == -ENOENT &&
+			    hai->hai_action == HSMA_REMOVE)
 				continue;
 
-			fail_request = true;
-			rc = mdt_agent_record_update(mti, &update, 1);
-			if (rc < 0) {
-				CERROR("%s: mdt_agent_record_update() failed, "
-				       "cannot update status to %s for cookie "
-				       "%#llx: rc = %d\n",
-				       mdt_obd_name(mdt),
-				       agent_req_status2name(ARS_FAILED),
-				       hai->hai_cookie, rc);
-				GOTO(out_buf, rc);
-			}
-
+			fail_request++;
+			car->car_hmm->mr_rec.arr_status = ARS_FAILED;
 			continue;
-		} else {
-			GOTO(out_buf, rc = PTR_ERR(obj));
 		}
 
-		if (!mdt_hsm_is_action_compat(hai, hal->hal_archive_id,
-					      hal->hal_flags, &hsm)) {
-			struct hsm_record_update update = {
-				.cookie = hai->hai_cookie,
-				.status = ARS_FAILED,
-			};
+		if (!mdt_hsm_is_action_compat(hai, archive_id, car->car_flags,
+					      &hsm)) {
 
 			/* incompatible request, we abort the request */
-			/* next time coordinator will wake up, it will
-			 * make the same HAL with valid only
-			 * records */
-			fail_request = true;
-			rc = mdt_agent_record_update(mti, &update, 1);
-			if (rc) {
-				CERROR("%s: mdt_agent_record_update() failed, "
-				       "cannot update status to %s for cookie "
-				       "%#llx: rc = %d\n",
-				       mdt_obd_name(mdt),
-				       agent_req_status2name(ARS_FAILED),
-				       hai->hai_cookie, rc);
-				GOTO(out_buf, rc);
-			}
+			fail_request++;
+			car->car_hmm->mr_rec.arr_status = ARS_FAILED;
 
 			/* if restore and record status updated, give
 			 * back granted layout lock */
@@ -509,13 +530,24 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 		}
 	}
 
-	/* we found incompatible requests, so the HAL cannot be sent
-	 * as is. Bad records have been invalidated in llog.
-	 * Valid one will be reschedule next time coordinator will wake up
-	 * So no need the rebuild a full valid HAL now
+	/* we found incompatible requests, so the HAL will be built only
+	 * with a vaild one. Bad records have been invalidated in llog.
 	 */
 	if (fail_request)
-		GOTO(out_buf, rc = -EAGAIN);
+		CDEBUG(D_HSM, "Some HSM actions are invalid, skipping it\n");
+
+	/* nothing to send to agent */
+	if (fail_request == rq->hsr_count)
+		GOTO(update_records, rc = 0);
+
+	len = hsr_hal_size(rq);
+	buf = kuc_alloc(len, KUC_TRANSPORT_HSM, HMT_ACTION_LIST);
+	if (IS_ERR(buf))
+		GOTO(update_records, rc = PTR_ERR(buf));
+
+	rc = hsr_hal_copy(rq, buf, len);
+	if (rc)
+		GOTO(update_records, rc);
 
 	/* Cancel memory registration is useless for purge
 	 * non registration avoid a deadlock :
@@ -524,12 +556,10 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 	 * by purge
 	 */
 	if (!purge) {
-		/* set is_registered even if failure because we may have
-		 * partial work done */
 		is_registered = true;
-		rc = mdt_hsm_add_hal(mti, hal, &uuid);
+		rc = mdt_hsm_add_hsr(mti, rq, &uuid);
 		if (rc)
-			GOTO(out_buf, rc);
+			GOTO(update_records, rc);
 	}
 
 	/* Uses the ldlm reverse import; this rpc will be seen by
@@ -546,7 +576,7 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 		       " rc = %d\n",
 		       mdt_obd_name(mdt), obd_uuid2str(&uuid), rc);
 		mdt_hsm_agent_unregister(mti, &uuid);
-		GOTO(out, rc);
+		GOTO(update_records, rc);
 	}
 
 	/* send request to agent */
@@ -568,19 +598,38 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti,
 		mdt_hsm_agent_unregister(mti, &uuid);
 	}
 
-out:
-	if (rc != 0 && is_registered) {
-		/* in case of error, we have to unregister requests */
-		hai = hai_first(hal);
-		for (i = 0; i < hal->hal_count; i++, hai = hai_next(hai)) {
-			if (hai->hai_action == HSMA_CANCEL)
-				continue;
-			mdt_cdt_remove_request(cdt, hai->hai_cookie);
-		}
-	}
+update_records:
+	/* for purge record updates do hsm_cancel_all_actions() */
+	if (purge)
+		GOTO(out_free, rc);
 
-out_buf:
-	kuc_free(buf, len);
+	/* in case of error, we have to unregister requests
+	 * also update request status here
+	 */
+	list_for_each_entry(car, &rq->hsr_cars, car_scan_list) {
+		int rc2;
+
+		hai = &car->car_hai;
+		if (rc != 0 && hai->hai_action != HSMA_CANCEL)
+			mdt_cdt_remove_request(cdt, hai->hai_cookie);
+
+		if (car->car_hmm->mr_rec.arr_status == ARS_WAITING && !rc)
+			car->car_hmm->mr_rec.arr_status = ARS_STARTED;
+
+		/* update llog record with ARS_ status */
+		rc2 = mdt_hsm_agent_modify_record(mti->mti_env, mdt,
+						  car->car_hmm);
+		if (!rc2)
+			continue;
+
+		CERROR("%s: modify record failed, cannot update status to %s for cookie %#llx: rc = %d\n",
+		       mdt_obd_name(mdt),
+		       agent_req_status2name(car->car_hmm->mr_rec.arr_status),
+		       hai->hai_cookie, rc2);
+	}
+out_free:
+	if (!IS_ERR_OR_NULL(buf))
+		kuc_free(buf, len);
 
 	RETURN(rc);
 }

lustre/mdt/mdt_hsm_cdt_requests.c
@@ -38,7 +38,7 @@ static void *cdt_request_cookie_key(struct hlist_node *hnode)
 {
 	struct cdt_agent_req *car = cdt_request_cookie_object(hnode);
 
-	return &car->car_hai->hai_cookie;
+	return &car->car_hai.hai_cookie;
 }
 
 static int cdt_request_cookie_keycmp(const void *key, struct hlist_node *hnode)
@@ -88,16 +88,16 @@ void dump_requests(char *prefix, struct coordinator *cdt)
 		       " action=%s archive#=%d flags=%#llx"
 		       " extent=%#llx-%#llx"
 		       " gid=%#llx refcount=%d canceled=%d\n",
-		       prefix, PFID(&car->car_hai->hai_fid),
-		       PFID(&car->car_hai->hai_dfid),
-		       car->car_hai->hai_cookie,
-		       hsm_copytool_action2name(car->car_hai->hai_action),
+		       prefix, PFID(&car->car_hai.hai_fid),
+		       PFID(&car->car_hai.hai_dfid),
+		       car->car_hai.hai_cookie,
+		       hsm_copytool_action2name(car->car_hai.hai_action),
 		       car->car_archive_id, car->car_flags,
-		       car->car_hai->hai_extent.offset,
-		       car->car_hai->hai_extent.length,
-		       car->car_hai->hai_gid,
+		       car->car_hai.hai_extent.offset,
+		       car->car_hai.hai_extent.length,
+		       car->car_hai.hai_gid,
 		       kref_read(&car->car_refcount),
-		       car->car_canceled);
+		       car->car_cancel ? 1 : 0);
 	}
 	up_read(&cdt->cdt_request_lock);
 }
@@ -199,18 +199,21 @@ static void mdt_cdt_init_request_tree(struct cdt_req_progress *crp)
 		progress_iter_next(NULL, 0, 0);
 }
 
+static inline int hmmr_size(int rec_size)
+{
+	return __ALIGN_KERNEL(offsetof(struct hsm_mem_req_rec, mr_rec) +
+			      rec_size, 8);
+}
+
 /** Allocate/init an agent request and its sub-structures.
  *
- * \param archive_id [IN]
- * \param flags [IN]
  * \param uuid [IN]
- * \param hai [IN]
+ * \param rec [IN]
  * \retval car [OUT] success valid structure
- * \retval car [OUT]
+ * \retval -ve failure
  */
-struct cdt_agent_req *mdt_cdt_alloc_request(__u32 archive_id, __u64 flags,
-					    struct obd_uuid *uuid,
-					    struct hsm_action_item *hai)
+struct cdt_agent_req *mdt_cdt_alloc_request(struct obd_uuid *uuid,
+					    struct llog_agent_req_rec *rec)
 {
 	struct cdt_agent_req *car;
 	ENTRY;
@@ -220,20 +223,15 @@ struct cdt_agent_req *mdt_cdt_alloc_request(__u32 archive_id, __u64 flags,
 		RETURN(ERR_PTR(-ENOMEM));
 
 	kref_init(&car->car_refcount);
-	car->car_archive_id = archive_id;
-	car->car_flags = flags;
-	car->car_canceled = 0;
-	car->car_req_start = ktime_get_real_seconds();
-	car->car_req_update = car->car_req_start;
 	car->car_uuid = *uuid;
-	OBD_ALLOC(car->car_hai, hai->hai_len);
-	if (car->car_hai == NULL) {
+	OBD_ALLOC(car->car_hmm, hmmr_size(rec->arr_hdr.lrh_len));
+	if (car->car_hmm == NULL) {
 		OBD_SLAB_FREE_PTR(car, mdt_hsm_car_kmem);
 		RETURN(ERR_PTR(-ENOMEM));
 	}
-	memcpy(car->car_hai, hai, hai->hai_len);
+	memcpy(&car->car_hmm->mr_rec, rec, rec->arr_hdr.lrh_len);
 	mdt_cdt_init_request_tree(&car->car_progress);
-
+	car->car_cancel = NULL;
 	RETURN(car);
 }
 
@@ -245,7 +243,7 @@ struct cdt_agent_req *mdt_cdt_alloc_request(__u32 archive_id, __u64 flags,
 void mdt_cdt_free_request(struct cdt_agent_req *car)
 {
 	mdt_cdt_free_request_tree(&car->car_progress);
-	OBD_FREE(car->car_hai, car->car_hai->hai_len);
+	OBD_FREE(car->car_hmm, hmmr_size(car->car_hmm->mr_rec.arr_hdr.lrh_len));
 	OBD_SLAB_FREE_PTR(car, mdt_hsm_car_kmem);
 }
 
@@ -289,12 +287,12 @@ int mdt_cdt_add_request(struct coordinator *cdt, struct cdt_agent_req *car)
 	ENTRY;
 
 	/* cancel requests are not kept in memory */
-	LASSERT(car->car_hai->hai_action != HSMA_CANCEL);
+	LASSERT(car->car_hai.hai_action != HSMA_CANCEL);
 
 	down_write(&cdt->cdt_request_lock);
 
 	rc = cfs_hash_add_unique(cdt->cdt_request_cookie_hash,
-				 &car->car_hai->hai_cookie,
+				 &car->car_hai.hai_cookie,
 				 &car->car_cookie_hash);
 	if (rc < 0) {
 		up_write(&cdt->cdt_request_lock);
@@ -303,11 +301,13 @@ int mdt_cdt_add_request(struct coordinator *cdt, struct cdt_agent_req *car)
 
 	list_add_tail(&car->car_request_list, &cdt->cdt_request_list);
 
+	mdt_cdt_get_request(car);
+
 	up_write(&cdt->cdt_request_lock);
 
 	mdt_hsm_agent_update_statistics(cdt, 0, 0, 1, &car->car_uuid);
 
-	switch (car->car_hai->hai_action) {
+	switch (car->car_hai.hai_action) {
 	case HSMA_ARCHIVE:
 		atomic_inc(&cdt->cdt_archive_count);
 		break;
@@ -363,7 +363,7 @@ int mdt_cdt_remove_request(struct coordinator *cdt, __u64 cookie)
 	list_del(&car->car_request_list);
 	up_write(&cdt->cdt_request_lock);
 
-	switch (car->car_hai->hai_action) {
+	switch (car->car_hai.hai_action) {
 	case HSMA_ARCHIVE:
 		atomic_dec(&cdt->cdt_archive_count);
 		break;
@@ -373,6 +373,13 @@ int mdt_cdt_remove_request(struct coordinator *cdt, __u64 cookie)
 	case HSMA_REMOVE:
 		atomic_dec(&cdt->cdt_remove_count);
 		break;
+	}
+
+	if (car->car_cancel) {
+		mdt_cdt_put_request(car->car_cancel);
+		/* ref from mdt_hsm_add_hsr()->mdt_cdt_find_request() */
+		mdt_cdt_put_request(car);
+		car->car_cancel = NULL;
 	}
 
 	/* Drop reference from cdt_request_list. */
@@ -410,7 +417,7 @@ struct cdt_agent_req *mdt_cdt_update_request(struct coordinator *cdt,
 	car->car_req_update = ktime_get_real_seconds();
 
 	/* update data move progress done by copy tool */
-	if (car->car_hai->hai_action != HSMA_REMOVE && pgs->hpk_errval == 0 &&
+	if (car->car_hai.hai_action != HSMA_REMOVE && pgs->hpk_errval == 0 &&
 	    pgs->hpk_extent.length != 0) {
 		rc = hsm_update_work(&car->car_progress, &pgs->hpk_extent);
 		if (rc) {
@@ -502,16 +509,16 @@ static int mdt_hsm_active_requests_proc_show(struct seq_file *s, void *v)
 		   " action=%s archive#=%d flags=%#llx"
 		   " extent=%#llx-%#llx gid=%#llx"
 		   " data=[%s] canceled=%d uuid=%s done=%llu\n",
-		   PFID(&car->car_hai->hai_fid),
-		   PFID(&car->car_hai->hai_dfid),
-		   0ULL /* compound_id */, car->car_hai->hai_cookie,
-		   hsm_copytool_action2name(car->car_hai->hai_action),
+		   PFID(&car->car_hai.hai_fid),
+		   PFID(&car->car_hai.hai_dfid),
+		   0ULL /* compound_id */, car->car_hai.hai_cookie,
+		   hsm_copytool_action2name(car->car_hai.hai_action),
 		   car->car_archive_id, car->car_flags,
-		   car->car_hai->hai_extent.offset,
-		   car->car_hai->hai_extent.length,
-		   car->car_hai->hai_gid,
-		   hai_dump_data_field(car->car_hai, buf, sizeof(buf)),
-		   car->car_canceled, obd_uuid2str(&car->car_uuid),
+		   car->car_hai.hai_extent.offset,
+		   car->car_hai.hai_extent.length,
+		   car->car_hai.hai_gid,
+		   hai_dump_data_field(&car->car_hai, buf, sizeof(buf)),
+		   car->car_cancel ? 1 : 0, obd_uuid2str(&car->car_uuid),
 		   car->car_progress.crp_total);
 	RETURN(0);
 }

lustre/mdt/mdt_internal.h
@@ -151,11 +151,6 @@ struct coordinator {
 						       * restore requests
 						       */
 
-	/* Hash of cookies to locations of record locations in agent
-	 * request log.
-	 */
-	struct cfs_hash		*cdt_agent_record_hash;
-
 	/* Bitmasks indexed by the HSMA_XXX constants. */
 	__u64			 cdt_user_request_mask;
 	__u64			 cdt_group_request_mask;
@@ -604,17 +599,19 @@ struct cdt_req_progress {
 struct cdt_agent_req {
 	struct hlist_node	 car_cookie_hash;  /**< find req by cookie */
 	struct list_head	 car_request_list; /**< to chain all the req. */
+	struct list_head	 car_scan_list;    /**< list for scan process */
 	struct kref		 car_refcount;     /**< reference counter */
-	__u64			 car_flags;        /**< request original flags */
 	struct obd_uuid		 car_uuid;         /**< agent doing the req. */
-	__u32			 car_archive_id;   /**< archive id */
-	int			 car_canceled;     /**< request was canceled */
-	time64_t		 car_req_start;    /**< start time */
-	time64_t		 car_req_update;   /**< last update time */
-	struct hsm_action_item	*car_hai;          /**< req. to the agent */
+	struct hsm_mem_req_rec	*car_hmm;	   /**< llog rec with cookies */
 	struct cdt_req_progress	 car_progress;     /**< track data mvt
 						    *   progress */
+	struct cdt_agent_req *car_cancel;	   /**< corresponding cancel */
 };
+#define car_flags	car_hmm->mr_rec.arr_flags /**< request original flags */
+#define car_archive_id	car_hmm->mr_rec.arr_archive_id /**< archive id */
+#define car_req_update	car_hmm->mr_rec.arr_req_change /**< last update time */
+#define car_hai		car_hmm->mr_rec.arr_hai /**< req. to the agent */
+
 extern struct kmem_cache *mdt_hsm_car_kmem;
 
 struct hsm_agent {
@@ -642,6 +639,37 @@ struct hsm_record_update {
 	__u64 cookie;
 	enum agent_req_status status;
 };
+
+struct hsm_mem_req_rec {
+	struct llog_logid mr_lid;
+	u64 mr_offset;
+	struct llog_agent_req_rec mr_rec;
+};
+
+/**
+ * data passed to llog_cat_process() callback
+ * to scan requests and take actions
+ */
+struct hsm_scan_request {
+	struct list_head hsr_cars;
+	char *hsr_fsname;
+	int hsr_used_sz;
+	u32 hsr_version;
+	u32 hsr_count;
+};
+
+static inline u32 hsr_get_archive_id(struct hsm_scan_request *rq)
+{
+	struct cdt_agent_req *car;
+
+	if (rq->hsr_count > 0) {
+		car = list_first_entry(&rq->hsr_cars, struct cdt_agent_req,
+				       car_scan_list);
+
+		return car->car_archive_id;
+	}
+	return 0;
+}
 
 static inline
 const struct md_device_operations *mdt_child_ops(struct mdt_device *m)
@@ -1071,15 +1099,9 @@ int cdt_llog_process(const struct lu_env *env, struct mdt_device *mdt,
 int mdt_agent_record_add(const struct lu_env *env, struct mdt_device *mdt,
 			 __u32 archive_id, __u64 flags,
 			 struct hsm_action_item *hai);
-int mdt_agent_record_update(struct mdt_thread_info *mti,
-			    struct hsm_record_update *updates,
-			    unsigned int updates_count);
-void cdt_agent_record_hash_add(struct coordinator *cdt, u64 cookie, u32 cat_idt,
-			       u32 rec_idx);
-void cdt_agent_record_hash_lookup(struct coordinator *cdt, u64 cookie,
-				  u32 *cat_idt, u32 *rec_idx);
-void cdt_agent_record_hash_del(struct coordinator *cdt, u64 cookie);
-
+int mdt_hsm_agent_modify_record(const struct lu_env *env,
+				struct mdt_device *mdt,
+				struct hsm_mem_req_rec *hmm);
 /* mdt/mdt_hsm_cdt_agent.c */
 extern const struct file_operations mdt_hsm_agent_fops;
 int mdt_hsm_agent_register(struct mdt_thread_info *info,
@@ -1095,7 +1117,7 @@ int mdt_hsm_agent_update_statistics(struct coordinator *cdt,
 				    const struct obd_uuid *uuid);
 int mdt_hsm_find_best_agent(struct coordinator *cdt, __u32 archive,
 			    struct obd_uuid *uuid);
-int mdt_hsm_agent_send(struct mdt_thread_info *mti, struct hsm_action_list *hal,
+int mdt_hsm_agent_send(struct mdt_thread_info *mti, struct hsm_scan_request *rq,
 		       bool purge);
 /* mdt/mdt_hsm_cdt_client.c */
 int mdt_hsm_add_actions(struct mdt_thread_info *info,
@@ -1109,12 +1131,10 @@ bool mdt_hsm_restore_is_running(struct mdt_thread_info *mti,
 				const struct lu_fid *fid);
 /* mdt/mdt_hsm_cdt_requests.c */
 extern struct cfs_hash_ops cdt_request_cookie_hash_ops;
-extern struct cfs_hash_ops cdt_agent_record_hash_ops;
 extern const struct file_operations mdt_hsm_active_requests_fops;
 void dump_requests(char *prefix, struct coordinator *cdt);
-struct cdt_agent_req *mdt_cdt_alloc_request(__u32 archive_id, __u64 flags,
-					    struct obd_uuid *uuid,
-					    struct hsm_action_item *hai);
+struct cdt_agent_req *mdt_cdt_alloc_request(struct obd_uuid *uuid,
+					    struct llog_agent_req_rec *rec);
 void mdt_cdt_free_request(struct cdt_agent_req *car);
 int mdt_cdt_add_request(struct coordinator *cdt, struct cdt_agent_req *new_car);
 struct cdt_agent_req *mdt_cdt_find_request(struct coordinator *cdt, u64 cookie);
@@ -1161,8 +1181,8 @@ struct mdt_object *mdt_hsm_get_md_hsm(struct mdt_thread_info *mti,
 				      const struct lu_fid *fid,
 				      struct md_hsm *hsm);
 /* actions/request helpers */
-int mdt_hsm_add_hal(struct mdt_thread_info *mti,
-		    struct hsm_action_list *hal, struct obd_uuid *uuid);
+int mdt_hsm_add_hsr(struct mdt_thread_info *mti, struct hsm_scan_request *hsr,
+		    struct obd_uuid *uuid);
 bool mdt_hsm_is_action_compat(const struct hsm_action_item *hai,
 			      u32 archive_id, u64 rq_flags,
 			      const struct md_hsm *hsm);

lustre/include/lustre_log.h
@@ -164,6 +164,9 @@ int llog_cat_reverse_process(const struct lu_env *env,
 			     void *data);
 int llog_cat_retain_cb(const struct lu_env *env, struct llog_handle *cat,
 		       struct llog_rec_hdr *rec, void *data);
+int llog_cat_modify_rec(const struct lu_env *env, struct llog_handle *cathandle,
+			struct llog_logid *lid, struct llog_rec_hdr *hdr,
+			struct llog_cookie *cookie);
 /* llog_obd.c */
 int llog_setup(const struct lu_env *env, struct obd_device *obd,
 	       struct obd_llog_group *olg, int index,
@@ -600,7 +603,7 @@ int llog_open_create(const struct lu_env *env, struct llog_ctxt *ctxt,
 		     char *name);
 int llog_erase(const struct lu_env *env, struct llog_ctxt *ctxt,
 	       struct llog_logid *logid, char *name);
-int llog_get_cookie(const struct lu_env *env, struct llog_cookie *out);
+void llog_get_cookie(const struct lu_env *env, struct llog_cookie *out);
 int llog_write_cookie(const struct lu_env *env, struct llog_handle *loghandle,
 		      struct llog_rec_hdr *rec, struct llog_cookie *cookie,
 		      int idx);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2920,6 +2920,12 @@ struct llog_rec_tail {
 	__u32	lrt_index;
 } __attribute__((packed));
 
+static inline struct llog_rec_tail *llog_get_rec_tail(struct llog_rec_hdr *rec)
+{
+	return (struct llog_rec_tail *)((char *)rec + rec->lrh_len -
+					sizeof(struct llog_rec_tail));
+}
+
 /* Where data follow just after header */
 #define REC_DATA(ptr)						\
 	((void *)((char *)ptr + sizeof(struct llog_rec_hdr)))

commit d3003b2a5a71c0244dc08006debc4cfa3f3f4a0e

LU-16518 mdt: fix unused variable compiler warnings
is_registered in mdt_hsm_agent_send() is now unused.
Remove it.

dump_requests() should be marked with __maybe_unused
rather than being given a fake user.

This fixes two compilation errors complaining about
unused variables (Clang 18).

Fixes: 07f6a59ad713 ("LU-18556 hsm: optimize llog record modification")
Fixes: 1294081248bb ("LU-3343 mdt: HSM coordinator main thread")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I5521624ef857703b046afca026cdcbfa7b722905
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60497
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_coordinator.c
@@ -1235,14 +1235,8 @@ static int mdt_hsm_cdt_start(struct mdt_device *mdt)
 	struct coordinator *cdt = &mdt->mdt_coordinator;
 	struct mdt_thread_info *cdt_mti;
 	int rc;
-	void *ptr;
 	struct task_struct *task;
 	ENTRY;
-
-	/* functions defined but not yet used
-	 * this avoid compilation warning
-	 */
-	ptr = dump_requests;
 
 	rc = set_cdt_state(cdt, CDT_INIT);
 	if (rc) {

lustre/mdt/mdt_hsm_cdt_agent.c
@@ -436,7 +436,6 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti, struct hsm_scan_request *rq,
 	struct obd_uuid uuid;
 	int len, rc = 0;
 	int fail_request = 0;
-	bool is_registered = false;
 	u32 archive_id = hsr_get_archive_id(rq);
 
 	ENTRY;
@@ -556,7 +555,6 @@ int mdt_hsm_agent_send(struct mdt_thread_info *mti, struct hsm_scan_request *rq,
 	 * by purge
 	 */
 	if (!purge) {
-		is_registered = true;
 		rc = mdt_hsm_add_hsr(mti, rq, &uuid);
 		if (rc)
 			GOTO(update_records, rc);

lustre/mdt/mdt_hsm_cdt_requests.c
@@ -77,7 +77,7 @@ struct cfs_hash_ops cdt_request_cookie_hash_ops = {
  * dump requests list
  * \param cdt [IN] coordinator
  */
-void dump_requests(char *prefix, struct coordinator *cdt)
+void __maybe_unused dump_requests(char *prefix, struct coordinator *cdt)
 {
 	struct cdt_agent_req	*car;
 

lustre/mdt/mdt_internal.h
@@ -1134,7 +1134,7 @@ bool mdt_hsm_restore_is_running(struct mdt_thread_info *mti,
 /* mdt/mdt_hsm_cdt_requests.c */
 extern struct cfs_hash_ops cdt_request_cookie_hash_ops;
 extern const struct file_operations mdt_hsm_active_requests_fops;
-void dump_requests(char *prefix, struct coordinator *cdt);
+void __maybe_unused dump_requests(char *prefix, struct coordinator *cdt);
 struct cdt_agent_req *mdt_cdt_alloc_request(struct obd_uuid *uuid,
 					    struct llog_agent_req_rec *rec);
 void mdt_cdt_free_request(struct cdt_agent_req *car);