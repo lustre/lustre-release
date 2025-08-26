lustre/lustre-release

commit ea0bdfc7cf0c31ab95a74bbb92c63e469f792350

LU-18925 libcfs: prefer kmap_local_page
Prefer kmap_local* in favor of kmap and kmap_atomic in preparation
of conversion to struct folio and kmap_local_folio

Provide kmap_local_page alias to maintain mapping to
kmap_atomic use in older kernels.

Currently md_read_page [mdc_read_page] returns the acquired page
and kmap()'d. The page is to be kunmap()'d in ll_release_page.

Remove the kmap() in mdc_read_page and kunmap() in ll_release_page
and convert the intervening page_address() usage into kmap_local_page

HPE-bug-id: LUS-12822
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I89ec7de9b6317a72df739500c0c3a942568e5eeb
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58776
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/include/obd_support.h
@@ -930,8 +930,11 @@ do {									      \
 #endif
 
 #ifdef POISON_BULK
-#define POISON_PAGE(page, val) do { memset(kmap(page), val, PAGE_SIZE); \
-                                    kunmap(page); } while (0)
+#define POISON_PAGE(page, val) do {			\
+	void *kaddr = kmap_local_page(page);		\
+	memset(kaddr, val, PAGE_SIZE);			\
+	kunmap_local(kaddr);				\
+} while (0)
 #else
 #define POISON_PAGE(page, val) do { } while (0)
 #endif

lustre/osd-ldiskfs/osd_io.c
@@ -421,8 +421,10 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 			nblocks = 1;
 
 			if (blocks[block_idx + i] == 0) {  /* hole */
+				void *addr;
 				struct niobuf_local *lnb =
 					iobuf->dr_lnbs[page_idx];
+
 				CDEBUG(D_INODE,
 				       "hole at page_idx %d, block_idx %d, at offset %llu\n",
 				       page_idx, block_idx,
@@ -434,8 +436,9 @@ static int osd_do_bio(struct osd_device *osd, struct inode *inode,
 					 page_idx, block_idx, i,
 					 (unsigned long long)start_blocks,
 					 (unsigned long long)count, npages);
-				memset(kmap(page) + page_offset, 0, blocksize);
-				kunmap(page);
+				addr = kmap_local_page(page);
+				memset(addr + page_offset, 0, blocksize);
+				kunmap_local(addr);
 				continue;
 			}
 
@@ -1161,7 +1164,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 			osd_iobuf_add_page(iobuf, &lnb[i]);
 		} else {
 			long off;
-			char *p = kmap(lnb[i].lnb_page);
+			char *p = kmap_local_page(lnb[i].lnb_page);
 
 			off = lnb[i].lnb_page_offset;
 			if (off)
@@ -1170,7 +1173,7 @@ static int osd_write_prep(const struct lu_env *env, struct dt_object *dt,
 			      ~PAGE_MASK;
 			if (off)
 				memset(p + off, 0, PAGE_SIZE - off);
-			kunmap(lnb[i].lnb_page);
+			kunmap_local(p);
 		}
 	}
 	end = ktime_get();

lustre/osd-zfs/osd_io.c
@@ -1040,10 +1040,12 @@ static int osd_write_commit(const struct lu_env *env, struct dt_object *dt,
 			continue;
 
 		if (lnb[i].lnb_page->mapping == (void *)obj) {
+			void *addr = kmap(lnb[i].lnb_page);
+
 			osd_dmu_write(osd, obj->oo_dn, lnb[i].lnb_file_offset,
-				      lnb[i].lnb_len, kmap(lnb[i].lnb_page) +
+				      lnb[i].lnb_len, addr +
 				      lnb[i].lnb_page_offset, oh->ot_tx);
-			kunmap(lnb[i].lnb_page);
+			kunmap(kmap_to_page(addr));
 			iosize += lnb[i].lnb_len;
 			abufsz = lnb[i].lnb_len; /* to drop cache below */
 		} else if (lnb[i].lnb_data) {

config/lustre-libcfs.m4
@@ -2130,6 +2130,36 @@ AC_DEFUN([LIBCFS_SEC_RELEASE_SECCTX], [
 ]) # LIBCFS_SEC_RELEASE_SECCTX
 
 #
+# LIBCFS_HAVE_KMAP_LOCAL
+#
+# Linux commit v5.10-rc2-80-gf3ba3c710ac5
+#   mm/highmem: Provide kmap_local*
+#
+AC_DEFUN([LIBCFS_SRC_HAVE_KMAP_LOCAL], [
+	LB2_LINUX_TEST_SRC([kmap_local_page], [
+		#include <linux/highmem.h>
+	],[
+		struct page *pg = NULL;
+		void *kaddr = kmap_local_page(pg);
+
+		kunmap_local(kaddr);
+	],[-Werror])
+])
+AC_DEFUN([LIBCFS_HAVE_KMAP_LOCAL], [
+	LB2_MSG_LINUX_TEST_RESULT([if 'kmap_local*' are available],
+	[kmap_local_page], [
+		AC_DEFINE(HAVE_KMAP_LOCAL, 1,
+			[kmap_local_* functions are available])
+	],[
+		## Map kmap_local_page to kmap_atomic for older kernels
+		AC_DEFINE([kmap_local_page(p)], [kmap_atomic(p)],
+			  [need kmap_local_page map to atomic])
+		AC_DEFINE([kunmap_local(kaddr)], [kunmap_atomic((kaddr))],
+			  [need kunmap_local map to atomic])
+	])
+]) # LIBCFS_HAVE_KMAP_LOCAL
+
+#
 # LIBCFS_HAVE_KFREE_SENSITIVE
 #
 # kernel v5.10-rc1~3
@@ -2650,6 +2680,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_KEY_NEED_UNLINK
 	LIBCFS_SRC_SEC_RELEASE_SECCTX
 	# 5.10
+	LIBCFS_SRC_HAVE_KMAP_LOCAL
 	LIBCFS_SRC_HAVE_KFREE_SENSITIVE
 	LIBCFS_SRC_HAVE_CRYPTO_SHA2_HEADER
 	LIBCFS_SRC_HAVE_LIST_CMP_FUNC_T
@@ -2807,6 +2838,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	LIBCFS_KEY_NEED_UNLINK
 	LIBCFS_SEC_RELEASE_SECCTX
 	# 5.10
+	LIBCFS_HAVE_KMAP_LOCAL
 	LIBCFS_HAVE_KFREE_SENSITIVE
 	LIBCFS_HAVE_CRYPTO_SHA2_HEADER
 	LIBCFS_HAVE_LIST_CMP_FUNC_T

lnet/klnds/gnilnd/gnilnd_cb.c
@@ -382,6 +382,8 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 		vunmap(addr);
 	} else {
 		do {
+			void *kaddr;
+
 			fraglen = min(kiov->bv_len - offset, nob);
 
 			/* make dang sure we don't send a bogus checksum if somehow we get
@@ -392,9 +394,8 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 				 "odd fraglen %u on nkiov %d, nob %u bv_len %u offset %u kiov 0x%p\n",
 				 fraglen, nkiov, nob, kiov->bv_len,
 				 offset, kiov);
-
-			addr = (void *)kmap(kiov->bv_page) + kiov->bv_offset +
-				offset;
+			kaddr = kmap_local_page(kiov->bv_page)
+			addr = kaddr + kiov->bv_offset + offset;
 			tmpck = _kgnilnd_cksum(cksum, addr, fraglen);
 
 			CDEBUG(D_BUFFS,
@@ -407,7 +408,7 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 			if (dump_blob)
 				kgnilnd_dump_blob(D_BUFFS, "kiov cksum", addr, fraglen);
 
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
 
 			kiov++;
 			nkiov--;

lnet/klnds/socklnd/socklnd_lib.c
@@ -389,12 +389,11 @@ ksocknal_lib_csum_tx(struct ksock_tx *tx)
 			     tx->tx_hdr.iov_len);
 
 	for (i = 0; i < tx->tx_nkiov; i++) {
-		base = kmap(tx->tx_kiov[i].bv_page) +
-			tx->tx_kiov[i].bv_offset;
+		void *kaddr = kmap_local_page(tx->tx_kiov[i].bv_page);
 
+		base = kaddr + tx->tx_kiov[i].bv_offset;
 		csum = ksocknal_csum(csum, base, tx->tx_kiov[i].bv_len);
-
-		kunmap(tx->tx_kiov[i].bv_page);
+		kunmap_local(kaddr);
 	}
 
 	if (*ksocknal_tunables.ksnd_inject_csum_error) {

lnet/lnet/lib-move.c
@@ -362,9 +362,11 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 		    unsigned int nob)
 {
 	/* NB diov, siov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *daddr = NULL;
-	char	       *saddr = NULL;
+	unsigned int this_nob;
+	char *daddr = NULL;
+	char *saddr = NULL;
+	void *dkaddr;
+	void *skaddr;
 
 	if (nob == 0)
 		return;
@@ -394,12 +396,14 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 				siov->bv_len - soffset,
 				nob);
 
-		if (daddr == NULL)
-			daddr = ((char *)kmap(diov->bv_page)) +
-				diov->bv_offset + doffset;
-		if (saddr == NULL)
-			saddr = ((char *)kmap(siov->bv_page)) +
-				siov->bv_offset + soffset;
+		if (!daddr) {
+			dkaddr = kmap_local_page(diov->bv_page);
+			daddr = dkaddr + diov->bv_offset + doffset;
+		}
+		if (!saddr) {
+			skaddr = kmap_local_page(siov->bv_page);
+			saddr = skaddr + siov->bv_offset + soffset;
+		}
 
 		/* Vanishing risk of kmap deadlock when mapping 2 pages.
 		 * However in practice at least one of the kiovs will be mapped
@@ -416,7 +420,8 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 			daddr += this_nob;
 			doffset += this_nob;
 		} else {
-			kunmap(diov->bv_page);
+			kunmap_local(dkaddr);
+			dkaddr = NULL;
 			daddr = NULL;
 			diov++;
 			ndiov--;
@@ -427,7 +432,8 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 			saddr += this_nob;
 			soffset += this_nob;
 		} else {
-			kunmap(siov->bv_page);
+			kunmap_local(skaddr);
+			skaddr = NULL;
 			saddr = NULL;
 			siov++;
 			nsiov--;
@@ -435,22 +441,23 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 		}
 	} while (nob > 0);
 
-	if (daddr != NULL)
-		kunmap(diov->bv_page);
-	if (saddr != NULL)
-		kunmap(siov->bv_page);
+	if (daddr)
+		kunmap_local(dkaddr);
+	if (saddr)
+		kunmap_local(skaddr);
 }
 EXPORT_SYMBOL(lnet_copy_kiov2kiov);
 
 void
-lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
-		    unsigned int nkiov, struct bio_vec *kiov,
-		    unsigned int kiovoffset,
-		    unsigned int nob)
+lnet_copy_kiov2iov(unsigned int niov, struct kvec *iov, unsigned int iovoffset,
+		   unsigned int nkiov, struct bio_vec *kiov,
+		   unsigned int kiovoffset,
+		   unsigned int nob)
 {
 	/* NB iov, kiov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *addr = NULL;
+	unsigned int this_nob;
+	void *kaddr = NULL;
+	char *addr = NULL;
 
 	if (nob == 0)
 		return;
@@ -480,9 +487,10 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 				(unsigned int)kiov->bv_len - kiovoffset,
 				nob);
 
-		if (addr == NULL)
-			addr = ((char *)kmap(kiov->bv_page)) +
-				kiov->bv_offset + kiovoffset;
+		if (addr == NULL) {
+			kaddr = kmap_local_page(kiov->bv_page);
+			addr = kaddr + kiov->bv_offset + kiovoffset;
+		}
 
 		memcpy((char *)iov->iov_base + iovoffset, addr, this_nob);
 		nob -= this_nob;
@@ -499,7 +507,8 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 			addr += this_nob;
 			kiovoffset += this_nob;
 		} else {
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
+			kaddr = NULL;
 			addr = NULL;
 			kiov++;
 			nkiov--;
@@ -509,7 +518,7 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 	} while (nob > 0);
 
 	if (addr != NULL)
-		kunmap(kiov->bv_page);
+		kunmap_local(kaddr);
 }
 EXPORT_SYMBOL(lnet_copy_kiov2iov);
 
@@ -520,8 +529,9 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 		   unsigned int nob)
 {
 	/* NB kiov, iov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *addr = NULL;
+	unsigned int this_nob;
+	void *kaddr = NULL;
+	char *addr = NULL;
 
 	if (nob == 0)
 		return;
@@ -551,18 +561,20 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 				(unsigned int)iov->iov_len - iovoffset,
 				nob);
 
-		if (addr == NULL)
-			addr = ((char *)kmap(kiov->bv_page)) +
-				kiov->bv_offset + kiovoffset;
+		if (!addr) {
+			kaddr = kmap_local_page(kiov->bv_page);
+			addr = kaddr + kiov->bv_offset + kiovoffset;
+		}
 
-		memcpy (addr, (char *)iov->iov_base + iovoffset, this_nob);
+		memcpy(addr, (char *)iov->iov_base + iovoffset, this_nob);
 		nob -= this_nob;
 
 		if (kiov->bv_len > kiovoffset + this_nob) {
 			addr += this_nob;
 			kiovoffset += this_nob;
 		} else {
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
+			kaddr = NULL;
 			addr = NULL;
 			kiov++;
 			nkiov--;
@@ -578,8 +590,8 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 		}
 	} while (nob > 0);
 
-	if (addr != NULL)
-		kunmap(kiov->bv_page);
+	if (addr)
+		kunmap_local(kaddr);
 }
 EXPORT_SYMBOL(lnet_copy_iov2kiov);
 

lnet/lnet/lnet-crypto.c
@@ -326,9 +326,9 @@ static void cfs_crypto_performance_test(enum cfs_crypto_hash_alg hash_alg)
 		goto out_err;
 	}
 
-	buf = kmap(page);
+	buf = kmap_local_page(page);
 	memset(buf, 0xAD, PAGE_SIZE);
-	kunmap(page);
+	kunmap_local(buf);
 
 	for (start = jiffies, end = start + cfs_time_seconds(1) / 4,
 	     bcount = 0; time_before(jiffies, end) && err == 0; bcount++) {

commit f472b887e3dcc0e3fdfd735a47284c5669ce2ff4

LU-19068 quota: wait to release quota
In LDiskFS, if some large file is deleted, the blocks occupied
by this file could have not been deleted when the transaction is
stopped for the transaction has not been committed, then the quota
usage won't be refreshed in time in qsd_op_end0 and the quota is
not released, then the write on other OST could fail with -EDQUOT.

Test-Parameters: ostcount=2 ostsizegb=5 mdtfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs testlist=sanity-quota env=ONLY=96,ONLY_REPEAT=10
Fixes: ea0bdfc7cf ("LU-18925 libcfs: prefer kmap_local_page")
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: Ibde821e2b5083210c5994ec028c0ed08a7ab043f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59468
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/include/lustre_quota.h
@@ -235,6 +235,9 @@ struct lquota_id_info {
 	 */
 	long long		 lqi_space;
 
+	/* the space released by truncating big files */
+	long long		 lqi_truncated_space;
+
 	/* quota slave entry structure associated with this ID */
 	struct lquota_entry	*lqi_qentry;
 

lustre/include/obd_support.h
@@ -554,6 +554,7 @@ extern bool obd_enable_fname_encoding;
 #define OBD_FAIL_QUOTA_RECALC            0xA07
 #define OBD_FAIL_QUOTA_GRANT             0xA08
 #define OBD_FAIL_QUOTA_NOSYNC            0xA09
+#define OBD_FAIL_QUOTA_USAGE_NOWAIT      0xA10
 
 #define OBD_FAIL_LPROC_REMOVE            0xB00
 

lustre/osd-ldiskfs/osd_handler.c
@@ -3898,12 +3898,15 @@ static int osd_declare_destroy(const struct lu_env *env, struct dt_object *dt,
 	struct osd_object *obj = osd_dt_obj(dt);
 	struct inode *inode = obj->oo_inode;
 	struct osd_thandle *oh;
+	long long	    space;
 	int rc;
 
 	ENTRY;
 
 	if (inode == NULL)
 		RETURN(-ENOENT);
+
+	space = -toqb(LDISKFS_I(inode)->i_disksize);
 
 	oh = container_of(th, struct osd_thandle, ot_super);
 	LASSERT(oh->ot_handle == NULL);
@@ -3930,7 +3933,7 @@ static int osd_declare_destroy(const struct lu_env *env, struct dt_object *dt,
 		RETURN(rc);
 	/* data to be truncated */
 	rc = osd_declare_inode_qid(env, i_uid_read(inode), i_gid_read(inode),
-				   i_projid_read(inode), 0, oh, obj, NULL,
+				   i_projid_read(inode), space, oh, obj, NULL,
 				   OSD_QID_BLK);
 	if (rc)
 		RETURN(rc);

lustre/osd-ldiskfs/osd_io.c
@@ -2358,6 +2358,8 @@ static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
 	struct osd_thandle *oh;
 	struct osd_object  *obj = osd_dt_obj(dt);
 	struct inode	   *inode;
+	long long	    space = 0;
+	loff_t		    size;
 	int		    rc;
 	ENTRY;
 
@@ -2378,8 +2380,12 @@ static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
 	inode = obj->oo_inode;
 	LASSERT(inode);
 
+	size = i_size_read(inode);
+	if (size > start)
+		space = -toqb((size > end ? end : size - start));
+
 	rc = osd_declare_inode_qid(env, i_uid_read(inode), i_gid_read(inode),
-				   i_projid_read(inode), 0, oh, obj,
+				   i_projid_read(inode), space, oh, obj,
 				   NULL, OSD_QID_BLK);
 
 	/* if object holds encrypted content, we need to make sure we truncate

lustre/quota/lquota_internal.h
@@ -177,6 +177,9 @@ struct lquota_entry {
 	struct mutex		 lqe_glbl_data_lock;
 	struct lqe_glbl_data	*lqe_glbl_data;
 	struct work_struct	 lqe_work; /* workitem to free lvbo */
+
+	/* the time when the blocks belonging to this QID should be truncated */
+	time64_t		 lqe_truncated_time;
 };
 
 #define lqe_qtype(lqe)		(lqe->lqe_site->lqs_qtype)

lustre/quota/qsd_handler.c
@@ -924,6 +924,13 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 		trans->lqt_id_cnt++;
 	}
 
+	if (qi->lqi_space < 0) {
+		if (found)
+			trans->lqt_ids[i].lqi_truncated_space += -qi->lqi_space;
+		else
+			trans->lqt_ids[i].lqi_truncated_space = -qi->lqi_space;
+	}
+
 	/* manage quota enforcement for this ID */
 	rc = qsd_op_begin0(env, qsd->qsd_type_array[qi->lqi_type],
 			   &trans->lqt_ids[i], qi->lqi_space, local_flags);
@@ -1104,6 +1111,27 @@ static void qsd_op_end0(const struct lu_env *env, struct qsd_qtype_info *qqi,
 			/* no suitable environment, handle adjustment in
 			 * separate thread context */
 			qsd_adjust_schedule(lqe, false, false);
+	}
+
+	/* Currently, ZFS will call qsd_op_end->qsd_op_end0 with env = NULL,
+	 * but ZFS will update the quota after commit, then it doesn't need
+	 * to wait to adjust the quota usage (LU-19068).
+	 */
+	if (!CFS_FAIL_CHECK(OBD_FAIL_QUOTA_USAGE_NOWAIT) &&
+	    env && qid->lqi_truncated_space > 1048576) {
+		__u32 seconds;
+
+		/* one second per gigabyte */
+		seconds = qid->lqi_truncated_space >> 20;
+		if (seconds > 120)
+			seconds = 120;
+
+		lqe_write_lock(lqe);
+		if (lqe->lqe_truncated_time < seconds + ktime_get_seconds())
+			lqe->lqe_truncated_time = seconds + ktime_get_seconds();
+		lqe_write_unlock(lqe);
+
+		qsd_adjust_schedule(lqe, true, false);
 	}
 	lqe_putref(lqe);
 	EXIT;

lustre/quota/qsd_writeback.c
@@ -402,8 +402,9 @@ out:
 
 void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 {
-	struct qsd_instance	*qsd = lqe2qqi(lqe)->qqi_qsd;
-	bool			 added = false;
+	struct qsd_instance *qsd = lqe2qqi(lqe)->qqi_qsd;
+	struct lquota_entry *tmp;
+	bool added = false;
 
 	read_lock(&qsd->qsd_lock);
 	if (qsd->qsd_stopping) {
@@ -426,18 +427,28 @@ void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 	if (list_empty(&lqe->lqe_link)) {
 		if (!cancel) {
 			lqe->lqe_adjust_time = ktime_get_seconds();
-			if (defer)
-				lqe->lqe_adjust_time += QSD_WB_INTERVAL;
+			if (defer) {
+				if (lqe->lqe_truncated_time)
+					lqe->lqe_adjust_time += 5;
+				else
+					lqe->lqe_adjust_time += QSD_WB_INTERVAL;
+			}
 		} else {
 			lqe->lqe_adjust_time = 0;
 		}
 
 		/* lqe reference transferred to list */
-		if (defer)
-			list_add_tail(&lqe->lqe_link,
-					  &qsd->qsd_adjust_list);
-		else
-			list_add(&lqe->lqe_link, &qsd->qsd_adjust_list);
+		list_for_each_entry(tmp, &qsd->qsd_adjust_list, lqe_link) {
+			if (tmp->lqe_adjust_time >= lqe->lqe_adjust_time) {
+				list_add(&lqe->lqe_link, &tmp->lqe_link);
+				added = true;
+				break;
+			}
+		}
+
+		if (!added)
+			list_add_tail(&lqe->lqe_link, &qsd->qsd_adjust_list);
+
 		added = true;
 	}
 	spin_unlock(&qsd->qsd_adjust_lock);
@@ -455,21 +466,26 @@ void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 /* return true if there is pending writeback records or the pending
  * adjust requests */
 static bool qsd_job_pending(struct qsd_instance *qsd, struct list_head *upd,
-			    bool *uptodate)
+			    bool *uptodate, int *wait)
 {
+	time64_t cur;
 	bool	job_pending = false;
 	int	qtype;
 
 	LASSERT(list_empty(upd));
 	*uptodate = true;
+	*wait = QSD_WB_INTERVAL;
 
 	spin_lock(&qsd->qsd_adjust_lock);
+	cur = ktime_get_seconds();
 	if (!list_empty(&qsd->qsd_adjust_list)) {
 		struct lquota_entry *lqe;
 		lqe = list_first_entry(&qsd->qsd_adjust_list,
 				       struct lquota_entry, lqe_link);
-		if (ktime_get_seconds() >= lqe->lqe_adjust_time)
+		if (cur >= lqe->lqe_adjust_time)
 			job_pending = true;
+		else
+			*wait = lqe->lqe_adjust_time - cur;
 	}
 	spin_unlock(&qsd->qsd_adjust_lock);
 
@@ -525,15 +541,16 @@ struct qsd_upd_args {
 
 static int qsd_upd_thread(void *_args)
 {
-	struct qsd_upd_args	*args = _args;
-	struct qsd_instance	*qsd = args->qua_inst;
+	struct qsd_upd_args *args = _args;
+	struct qsd_instance *qsd = args->qua_inst;
 	LIST_HEAD(queue);
-	struct qsd_upd_rec	*upd, *n;
-	struct lu_env		*env = &args->qua_env;
-	int			 qtype, rc = 0;
-	bool			 uptodate;
-	struct lquota_entry	*lqe;
+	struct qsd_upd_rec *upd, *n;
+	struct lu_env *env = &args->qua_env;
+	int qtype, rc = 0;
+	bool uptodate;
+	struct lquota_entry *lqe;
 	time64_t cur_time;
+	int wait_time;
 	ENTRY;
 
 	complete(args->qua_started);
@@ -541,8 +558,8 @@ static int qsd_upd_thread(void *_args)
 		 !kthread_should_stop(); })) {
 		int count = 0;
 
-		if (!qsd_job_pending(qsd, &queue, &uptodate))
-			schedule_timeout(cfs_time_seconds(QSD_WB_INTERVAL));
+		if (!qsd_job_pending(qsd, &queue, &uptodate, &wait_time))
+			schedule_timeout(cfs_time_seconds(wait_time));
 		__set_current_state(TASK_RUNNING);
 
 		while (1) {
@@ -593,8 +610,22 @@ static int qsd_upd_thread(void *_args)
 					qsd_adjust(env, lqe);
 			}
 
-			lqe_putref(lqe);
 			spin_lock(&qsd->qsd_adjust_lock);
+			if (!kthread_should_stop() && uptodate &&
+			    list_empty(&lqe->lqe_link) &&
+			    lqe->lqe_truncated_time > cur_time) {
+				if (lqe->lqe_truncated_time < cur_time + 5)
+					lqe->lqe_adjust_time =
+							lqe->lqe_truncated_time;
+				else
+					lqe->lqe_adjust_time = cur_time + 5;
+
+				list_add_tail(&lqe->lqe_link,
+					      &qsd->qsd_adjust_list);
+			} else {
+				lqe->lqe_truncated_time = 0;
+				lqe_putref(lqe);
+			}
 		}
 		spin_unlock(&qsd->qsd_adjust_lock);
 

lustre/tests/sanity-quota.sh
@@ -7227,6 +7227,87 @@ test_95b()
 }
 run_test 95b "df respects squashed project id"
 
+test_delete_big_file() {
+	local equot_expected=$1
+	local testdir=$DIR/$tdir
+	local tfile1=$DIR/$tdir/dd.out.0
+	local tfile2=$DIR/$tdir/dd.out.1
+
+	chown $TSTID:$TSTID $testdir || error "failed to chown $testdir"
+	$LFS project -sp $TSTPRJID $testdir ||
+		error "fail to set project on $testdir"
+
+	$LFS setquota -p $TSTPRJID -B 3G $DIR ||
+		error "failed to set quota limits for $TSTPRJID"
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	$LFS setstripe $tfile1 -i 0 -c 1 || error "setstripe $tfile1 failed"
+	chown $TSTID:$TSTID $tfile1 || error "fail to chown $tfile1"
+
+	$RUNAS fallocate -l2GiB $tfile1 || error "fail to write 2GiB"
+	$RUNAS $DD of=$tfile1 seek=2048 count=1500 oflag=sync &&
+		error "DD should fail on first write"
+
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	rm -f $tfile1
+	sleep 15
+
+	$LFS quota -p $TSTPRJID -h $DIR
+	$LFS setstripe $tfile2 -i 1 -c 1 || error "setstripe $tfile2 failed"
+	chown $TSTID:$TSTID $tfile2 || error "fail to chown $tfile2"
+
+	$RUNAS fallocate -l2GiB $tfile2 || error "fail to write 2GiB"
+
+	(( equot_expected == 1 )) && {
+		$RUNAS $DD of=$tfile2 seek=2048 count=512 oflag=sync &&
+			error "write should fail after deleting big files"
+	}
+	(( equot_expected == 0 )) && {
+		$RUNAS $DD of=$tfile2 seek=2048 count=512 oflag=sync ||
+			error "write should succeed after deleting big files"
+	}
+
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	rm -f $tfile2
+	sleep 10
+	$LFS quota -p $TSTPRJID -h $DIR
+}
+
+test_96() {
+	local smallost
+
+	(( $MDS1_VERSION >= $(version_code 2_16_57) )) ||
+		skip "need OST >= 2.16.57 to run this test"
+
+	[ "$ost1_FSTYPE" == zfs ] &&
+		skip "the test in only needed to run on LDiskFS"
+
+	(( OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
+
+	is_project_quota_supported || skip "project is not supported"
+
+	smallost=($($LFS df -o $DIR | awk "/OST00/ { print \$4 }" | sort -n))
+
+	if (( smallost < 3657728 )); then
+		skip "OST is too small, skip the test"
+	fi
+
+	setup_quota_test || error "setup quota failed with $?"
+	set_ost_qtype $QTYPE || error "enable ost quota failed"
+
+	#define OBD_FAIL_QUOTA_USAGE_NOWAIT 0xA10
+	do_facet ost1 $LCTL set_param fail_loc=0xa10
+	do_facet ost2 $LCTL set_param fail_loc=0xa10
+	test_delete_big_file 1
+
+	do_facet ost1 $LCTL set_param fail_loc=0
+	do_facet ost2 $LCTL set_param fail_loc=0
+	test_delete_big_file 0
+}
+run_test 96 "quota grant should be released when big files are deleted"
+
 quota_fini()
 {
 	do_nodes $(comma_list $(nodes_list)) \