lustre/lustre-release

commit 21080400f90e80d5864936465aa85830bee34131

LU-16723 libcfs: refactor parser to be simpler
The parser code has a lot of unused complexity that can be
streamlined. Refactor the parser makes the interface simpler
from a development perspective and can provide a consistent
user experience for all of the lustre utilities.

All functions which are not used outside of the parser have
been made static. Functions which don't appear to be used at
all have been removed.

The file headers have been standardized and the SDX text
added. The function names have been changed to be more standard.

Test-Parameters: testlist=sanity env=ONLY=60a,ONLY_REPEAT=10
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I3354f213de424f51aef94c840083a4cb781d7aae
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50583
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

libcfs/libcfs/util/parser.c
@@ -1,22 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (C) 2001 Cluster File Systems, Inc.
  *
  * Copyright (c) 2014, 2017, Intel Corporation.
  *
- *   This file is part of Lustre, http://www.sf.net/projects/lustre/
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
  *
- *   Lustre is free software; you can redistribute it and/or
- *   modify it under the terms of version 2 of the GNU General Public
- *   License as published by the Free Software Foundation.
+ * libcfs/libcfs/parser.c
  *
- *   Lustre is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Lustre; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * A command line parser.
  *
  */
 
@@ -38,10 +34,8 @@
 #include <libcfs/util/parser.h>
 #include <linux/lustre/lustre_ver.h>
 
-/* Top level of commands, initialized by InitParser */
+/* Top level of commands */
 static command_t *top_level;
-/* Parser prompt, set by InitParser */
-static char *parser_prompt;
 /* Set to 1 if user types exit or quit */
 static int done;
 /*
@@ -50,12 +44,43 @@ static int done;
  */
 static int ignore_errors;
 
-/* static functions */
 static char *skipwhitespace(char *s);
 static char *skiptowhitespace(char *s);
 static command_t *find_cmd(char *name, command_t cmds[], char **next);
 static int process(char *s, char **next, command_t *lookup, command_t **result,
 		   char **prev);
+static int line2args(char *line, char **argv, int maxargs);
+static int cfs_parser_commands(command_t *cmds);
+static int cfs_parser_execarg(int argc, char **argv, command_t cmds[]);
+static int cfs_parser_list_commands(const command_t *cmdlist, int line_len,
+				int col_num);
+static int cfs_parser_list(int argc, char **argv);
+static int cfs_parser_help(int argc, char **argv);
+static int cfs_parser_quit(int argc, char **argv);
+static int cfs_parser_version(int argc, char **argv);
+static int cfs_parser_ignore_errors(int argc, char **argv);
+
+command_t override_cmdlist[] = {
+	{ .pc_name = "quit", .pc_func = cfs_parser_quit, .pc_help = "quit" },
+	{ .pc_name = "exit", .pc_func = cfs_parser_quit, .pc_help = "exit" },
+	{ .pc_name = "help", .pc_func = cfs_parser_help, .pc_help = "help" },
+	{ .pc_name = "--help", .pc_func = cfs_parser_help, .pc_help = "help" },
+	{ .pc_name = "version", .pc_func = cfs_parser_version,
+	  .pc_help = "version" },
+	{ .pc_name = "--version", .pc_func = cfs_parser_version,
+	  .pc_help = "version" },
+	{ .pc_name = "list-commands", .pc_func = cfs_parser_list,
+	  .pc_help = "list" },
+	{ .pc_name = "--list-commands", .pc_func = cfs_parser_list,
+	  .pc_help = "list" },
+	{ .pc_name = "--ignore_errors", .pc_func = cfs_parser_ignore_errors,
+	  .pc_help = "ignore errors that occur during script processing\n"
+		"--ignore_errors"},
+	{ .pc_name = "ignore_errors", .pc_func = cfs_parser_ignore_errors,
+	  .pc_help = "ignore errors that occur during script processing\n"
+		"ignore_errors"},
+	{ .pc_name = 0, .pc_func = NULL, .pc_help = 0 }
+};
 
 static char *skipwhitespace(char *s)
 {
@@ -93,7 +118,7 @@ static int line2args(char *line, char **argv, int maxargs)
 }
 
 /* find a command -- return it if unique otherwise print alternatives */
-static command_t *Parser_findargcmd(char *name, command_t cmds[])
+static command_t *cfs_parser_findargcmd(char *name, command_t cmds[])
 {
 	command_t *cmd;
 
@@ -104,16 +129,40 @@ static command_t *Parser_findargcmd(char *name, command_t cmds[])
 	return NULL;
 }
 
-void Parser_ignore_errors(int ignore)
+static int cfs_parser_ignore_errors(int argc, char **argv)
 {
-	ignore_errors = ignore;
+	(void) argc;
+	(void) argv;
+
+	ignore_errors = 1;
+
+	return 0;
 }
 
-int Parser_execarg(int argc, char **argv, command_t cmds[])
+int cfs_parser(int argc, char **argv, command_t cmds[])
+{
+	int rc = 0;
+
+	done = 0;
+	top_level = cmds;
+
+	if (argc > 1)
+		rc = cfs_parser_execarg(argc - 1, argv + 1, cmds);
+	else
+		rc = cfs_parser_commands(cmds);
+
+	return rc;
+}
+
+static int cfs_parser_execarg(int argc, char **argv, command_t cmds[])
 {
 	command_t *cmd;
 
-	cmd = Parser_findargcmd(argv[0], cmds);
+	cmd = cfs_parser_findargcmd(argv[0], override_cmdlist);
+
+	if (!cmd)
+		cmd = cfs_parser_findargcmd(argv[0], cmds);
+
 	if (cmd && cmd->pc_func) {
 		int rc = cmd->pc_func(argc, argv);
 
@@ -123,17 +172,7 @@ int Parser_execarg(int argc, char **argv, command_t cmds[])
 		}
 		return rc;
 	}
-	/*
-	 * handle --help here as a synonym of --list-commands so that it can be
-	 * applied to all commands without needing to individually add a --help
-	 * option to each command.
-	 * This does not apply to lctl which has its own --help option because
-	 * `lctl --list-commands` uses a unique format.
-	 */
-	if (strcmp(argv[0], "--help") == 0) {
-		Parser_list_commands(cmds, 80, 4);
-		return 0;
-	}
+
 	fprintf(stderr,
 		"%s: '%s' is not a valid command. See '%s --list-commands'.\n",
 		program_invocation_short_name, argv[0],
@@ -165,6 +204,12 @@ static command_t *find_cmd(char *name, command_t cmds[], char **next)
 	if (len == 0)
 		return NULL;
 
+	for (i = 0; override_cmdlist[i].pc_name; i++) {
+		if (strncasecmp(name, override_cmdlist[i].pc_name, len) == 0) {
+			*next = skipwhitespace(*next);
+			return &override_cmdlist[i];
+		}
+	}
 	for (i = 0; cmds[i].pc_name; i++) {
 		if (strncasecmp(name, cmds[i].pc_name, len) == 0) {
 			*next = skipwhitespace(*next);
@@ -402,7 +447,7 @@ outfree:
 #endif
 
 /* this is the command execution machine */
-int Parser_commands(void)
+static int cfs_parser_commands(command_t *cmds)
 {
 	char *line, *s;
 	int rc = 0, save_error = 0;
@@ -411,7 +456,7 @@ int Parser_commands(void)
 	interactive = init_input();
 
 	while (!done) {
-		line = readline(interactive ? parser_prompt : NULL);
+		line = readline(interactive ? "> " : NULL);
 
 		if (!line)
 			break;
@@ -438,52 +483,7 @@ int Parser_commands(void)
 	return rc;
 }
 
-/* sets the parser prompt */
-void Parser_init(char *prompt, command_t *cmds)
-{
-	done = 0;
-	top_level = cmds;
-	if (parser_prompt)
-		free(parser_prompt);
-	parser_prompt = strdup(prompt);
-}
-
-/* frees the parser prompt */
-void Parser_exit(int argc, char *argv[])
-{
-	done = 1;
-	free(parser_prompt);
-	parser_prompt = NULL;
-}
-
-/* convert a string to an integer */
-int Parser_int(char *s, int *val)
-{
-	int ret;
-
-	if (*s != '0') {
-		ret = sscanf(s, "%d", val);
-	} else if (*(s + 1) != 'x') {
-		ret = sscanf(s, "%o", val);
-	} else {
-		s++;
-		ret = sscanf(++s, "%x", val);
-	}
-
-	return ret;
-}
-
-void Parser_qhelp(int argc, char *argv[])
-{
-	printf("usage: %s [COMMAND] [OPTIONS]... [ARGS]\n",
-	       program_invocation_short_name);
-	printf("Without any parameters, interactive mode is invoked\n");
-
-	printf("Try '%s help <COMMAND>', or '%s --list-commands' for a list of commands.\n",
-		program_invocation_short_name, program_invocation_short_name);
-}
-
-int Parser_help(int argc, char **argv)
+static int cfs_parser_help(int argc, char **argv)
 {
 	char line[1024];
 	char *next, *prev, *tmp;
@@ -491,7 +491,12 @@ int Parser_help(int argc, char **argv)
 	int i;
 
 	if (argc == 1) {
-		Parser_qhelp(argc, argv);
+		printf("usage: %s [COMMAND] [OPTIONS]... [ARGS]\n",
+			program_invocation_short_name);
+		printf("Without any parameters, interactive mode is invoked\n");
+		printf("Try '%s help <COMMAND>', or '%s --list-commands' for a list of commands.\n",
+			program_invocation_short_name,
+			program_invocation_short_name);
 		return 0;
 	}
 
@@ -542,17 +547,8 @@ int Parser_help(int argc, char **argv)
 	return 0;
 }
 
-void Parser_printhelp(char *cmd)
-{
-	char *argv[] = { "help", cmd };
-
-	Parser_help(2, argv);
-}
-
-/* COMMANDS */
-
 /**
- * Parser_list_commands() - Output a list of the supported commands.
+ * cfs_parser_list_commands() - Output a list of the supported commands.
  * @cmdlist:	  Array of structures describing the commands.
  * @line_len:	  Length of output line.
  * @col_num:	  The number of commands printed in a single row.
@@ -562,8 +558,8 @@ void Parser_printhelp(char *cmd)
  *
  * Return: The number of items that were printed.
  */
-int Parser_list_commands(const command_t *cmdlist, int line_len,
-			 int col_num)
+static int cfs_parser_list_commands(const command_t *cmdlist, int line_len,
+				int col_num)
 {
 	int char_max;
 	int count = 0;
@@ -600,243 +596,47 @@ int Parser_list_commands(const command_t *cmdlist, int line_len,
 	return count;
 }
 
-char *Parser_getstr(const char *prompt, const char *deft, char *res,
-		    size_t len)
-{
-	char *line = NULL;
-	int size = strlen(prompt) + strlen(deft) + 8;
-	char *theprompt;
-
-	theprompt = malloc(size);
-	assert(theprompt);
-
-	snprintf(theprompt, size, "%s [%s]: ", prompt, deft);
-
-	line  = readline(theprompt);
-	free(theprompt);
-
-	/*
-	 * The function strlcpy() cannot be used here because of
-	 * this function is used in LNet utils that is not linked
-	 * with libcfs.a.
-	 */
-	if (!line || *line == '\0')
-		strncpy(res, deft, len);
-	else
-		strncpy(res, line, len);
-	res[len - 1] = '\0';
-
-	if (line) {
-		free(line);
-		return res;
-	}
-	return NULL;
-}
-
-/* get integer from prompt, loop forever to get it */
-int Parser_getint(const char *prompt, long min, long max, long deft, int base)
-{
-	int rc;
-	long result;
-	char *line;
-	int size = strlen(prompt) + 40;
-	char *theprompt = malloc(size);
-
-	assert(theprompt);
-	snprintf(theprompt, size, "%s [%ld, (0x%lx)]: ", prompt, deft, deft);
-	fflush(stdout);
-
-	do {
-		line = NULL;
-		line = readline(theprompt);
-		if (!line) {
-			fprintf(stdout, "Please enter an integer.\n");
-			fflush(stdout);
-			continue;
-		}
-		if (*line == '\0') {
-			free(line);
-			result =  deft;
-			break;
-		}
-		rc = Parser_arg2int(line, &result, base);
-		free(line);
-		if (rc != 0) {
-			fprintf(stdout, "Invalid string.\n");
-			fflush(stdout);
-		} else if (result > max || result < min) {
-			fprintf(stdout,
-				"Error: response must lie between %ld and %ld.\n",
-				min, max);
-			fflush(stdout);
-		} else {
-			break;
-		}
-	} while (1);
-
-	if (theprompt)
-		free(theprompt);
-	return result;
-}
-
-/* get boolean (starting with YyNn; loop forever */
-int Parser_getbool(const char *prompt, int deft)
-{
-	int result = 0;
-	char *line;
-	int size = strlen(prompt) + 8;
-	char *theprompt = malloc(size);
-
-	assert(theprompt);
-	fflush(stdout);
-
-	if (deft != 0 && deft != 1) {
-		fprintf(stderr, "Error: Parser_getbool given bad default %d\n",
-			deft);
-		assert(0);
-	}
-	snprintf(theprompt, size, "%s [%s]: ", prompt, (deft == 0) ? "N" : "Y");
-
-	do {
-		line = NULL;
-		line = readline(theprompt);
-		if (!line) {
-			result = deft;
-			break;
-		}
-		if (*line == '\0') {
-			result = deft;
-			break;
-		}
-		if (*line == 'y' || *line == 'Y') {
-			result = 1;
-			break;
-		}
-		if (*line == 'n' || *line == 'N') {
-			result = 0;
-			break;
-		}
-		if (line)
-			free(line);
-		fprintf(stdout, "Invalid string. Must start with yY or nN\n");
-		fflush(stdout);
-	} while (1);
-
-	if (line)
-		free(line);
-	if (theprompt)
-		free(theprompt);
-
-	return result;
-}
-
-/* parse int out of a string or prompt for it */
-long Parser_intarg(const char *inp, const char *prompt, int deft,
-		   int min, int max, int base)
-{
-	long result;
-	int rc;
-
-	rc = Parser_arg2int(inp, &result, base);
-	if (rc == 0)
-		return result;
-	else
-		return Parser_getint(prompt, deft, min, max, base);
-}
-
-/* parse int out of a string or prompt for it */
-char *Parser_strarg(char *inp, const char *prompt, const char *deft,
-		    char *answer, int len)
-{
-	if (!inp || *inp == '\0')
-		return Parser_getstr(prompt, deft, answer, len);
-	else
-		return inp;
-}
-
-/*
- * change a string into a number: return 0 on success. No invalid characters
- * allowed. The processing of base and validity follows strtol(3)
- */
-int Parser_arg2int(const char *inp, long *result, int base)
-{
-	char *endptr;
-
-	if ((base != 0) && (base < 2 || base > 36))
-		return 1;
-
-	*result = strtol(inp, &endptr, base);
-
-	if (*inp != '\0' && *endptr == '\0')
-		return 0;
-	else
-		return 1;
-}
-
-/* Convert human readable size string to and int; "1k" -> 1000 */
-int Parser_size(unsigned long *sizep, char *str)
-{
-	unsigned long size;
-	char mod[32];
-
-	switch (sscanf(str, "%lu%1[gGmMkK]", &size, mod)) {
-	default:
-		return -1;
-	case 1:
-		*sizep = size;
-		return 0;
-	case 2:
-		switch (*mod) {
-		case 'g':
-		case 'G':
-			*sizep = size << 30;
-			return 0;
-		case 'm':
-		case 'M':
-			*sizep = size << 20;
-			return 0;
-		case 'k':
-		case 'K':
-			*sizep = size << 10;
-			return 0;
-		default:
-			*sizep = size;
-			return 0;
-		}
-	}
-}
-
-/* Convert a string boolean to an int; "enable" -> 1 */
-int Parser_bool(int *b, char *str)
-{
-	if (!strcasecmp(str, "no") || !strcasecmp(str, "n") ||
-	    !strcasecmp(str, "off") || !strcasecmp(str, "down") ||
-	    !strcasecmp(str, "disable")) {
-		*b = 0;
-		return 0;
-	}
-
-	if (!strcasecmp(str, "yes") || !strcasecmp(str, "y") ||
-	    !strcasecmp(str, "on") || !strcasecmp(str, "up") ||
-	    !strcasecmp(str, "enable")) {
-		*b = 1;
-		return 0;
-	}
-
-	return -1;
-}
-
-int Parser_quit(int argc, char **argv)
+static int cfs_parser_quit(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
+
 	done = 1;
+
 	return 0;
 }
 
-int Parser_version(int argc, char **argv)
+static int cfs_parser_version(int argc, char **argv)
 {
+	(void) argc;
+	(void) argv;
+
 	fprintf(stdout, "%s %s\n", program_invocation_short_name,
 		LUSTRE_VERSION_STRING);
+
+	return 0;
+}
+
+static int cfs_parser_list(int argc, char **argv)
+{
+	command_t *cmd;
+	int num_cmds_listed;
+
+	(void) argc;
+	(void) argv;
+
+	cmd = top_level;
+	while (cmd->pc_name != NULL) {
+		if (!cmd->pc_func) {
+			/*
+			 * print the command category
+			 */
+			printf("\n%s\n", cmd->pc_name);
+			cmd++;
+		}
+		num_cmds_listed = cfs_parser_list_commands(cmd, 80, 4);
+		cmd += num_cmds_listed;
+	}
+
 	return 0;
 }

lnet/utils/lst.c
@@ -55,7 +55,6 @@
 
 struct lst_sid LST_INVALID_SID = { .ses_nid = LNET_NID_ANY, .ses_stamp = -1 };
 static unsigned int session_key;
-static int lst_list_commands(int argc, char **argv);
 
 /* All nodes running 2.6.50 or later understand feature LST_FEAT_BULK_LEN */
 static unsigned int session_features = LST_FEATS_MASK;
@@ -361,7 +360,9 @@ lst_test_name2type(char *name)
 void
 lst_print_usage(char *cmd)
 {
-        Parser_printhelp(cmd);
+	char *argv[] = { "help", cmd };
+
+	cfs_parser(2, argv, NULL);
 }
 
 void
@@ -3952,8 +3953,6 @@ static command_t lst_cmdlist[] = {
         {"add_test",            jt_lst_add_test,        NULL,
          "Usage: lst add_test [--batch BATCH] [--loop #] [--concurrency #] "
          " [--distribute #:#] [--from GROUP] [--to GROUP] TEST..."                      },
-        {"help",                Parser_help,            0,     "help"                   },
-	{"--list-commands",     lst_list_commands,      0,     "list commands"          },
         {0,                     0,                      0,      NULL                    }
 };
 
@@ -3987,37 +3986,22 @@ lst_initialize(void)
         return 0;
 }
 
-static int lst_list_commands(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	Parser_list_commands(lst_cmdlist, 80, 4);
+	int rc = 0;
 
-	return 0;
-}
+	setlinebuf(stdout);
 
-int
-main(int argc, char **argv)
-{
-        int rc = 0;
-
-        setlinebuf(stdout);
-
-        rc = lst_initialize();
-        if (rc < 0)
-                goto errorout;
+	rc = lst_initialize();
+	if (rc < 0)
+		goto errorout;
 
 	rc = lustre_lnet_config_lib_init();
 	if (rc < 0)
 		goto errorout;
 
-        Parser_init("lst > ", lst_cmdlist);
-
-        if (argc != 1)  {
-                rc = Parser_execarg(argc - 1, argv + 1, lst_cmdlist);
-                goto errorout;
-        }
-
-        Parser_commands();
+	rc = cfs_parser(argc, argv, lst_cmdlist);
 
 errorout:
-        return rc;
+	return rc;
 }

libcfs/include/libcfs/util/parser.h
@@ -1,30 +1,13 @@
-/*
- * GPL HEADER START
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 only,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License version 2 for more details (a copy is included
- * in the LICENSE file that accompanied this code).
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.gnu.org/licenses/gpl-2.0.html
- *
- * GPL HEADER END
- */
+// SPDX-License-Identifier: GPL-2.0
+
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  * Use is subject to license terms.
  *
  * Copyright (c) 2014, 2017, Intel Corporation.
+ *
  */
+
 /*
  * This file is part of Lustre, http://www.lustre.org/
  *
@@ -37,7 +20,7 @@
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
-#define HISTORY	100		/* Don't let history grow unbounded    */
+#define HISTORY	100
 #define MAXARGS 512
 
 #define CMD_COMPLETE	0
@@ -65,48 +48,6 @@ typedef struct network {
 	int	port;
 } network_t;
 
-int Parser_quit(int argc, char **argv);
-int Parser_version(int argc, char **argv);
-void Parser_init(char *, command_t *);	/* Set prompt and load command list */
-int Parser_commands(void);			/* Start the command parser */
-void Parser_qhelp(int, char **);	/* Quick help routine */
-int Parser_help(int, char **);		/* Detailed help routine */
-void Parser_ignore_errors(int ignore);	/* Set the ignore errors flag */
-void Parser_printhelp(char *);		/* Detailed help routine */
-void Parser_exit(int, char **);		/* Shuts down command parser */
-int Parser_execarg(int argc, char **argv, command_t cmds[]);
-int Parser_list_commands(const command_t *cmdlist, int line_len,
-			 int col_num);
-
-/* Converts a string to an integer */
-int Parser_int(char *, int *);
-
-/* Prompts for a string, with default values and a maximum length */
-char *Parser_getstr(const char *prompt, const char *deft, char *res, 
-		    size_t len);
-
-/* Prompts for an integer, with minimum, maximum and default values and base */
-int Parser_getint(const char *prompt, long min, long max, long deft,
-		  int base);
-
-/* Prompts for a yes/no, with default */
-int Parser_getbool(const char *prompt, int deft);
-
-/* Extracts an integer from a string, or prompts if it cannot get one */
-long Parser_intarg(const char *inp, const char *prompt, int deft,
-		   int min, int max, int base);
-
-/* Extracts a word from the input, or propmts if it cannot get one */
-char *Parser_strarg(char *inp, const char *prompt, const char *deft,
-		    char *answer, int len);
-
-/* Extracts an integer from a string  with a base */
-int Parser_arg2int(const char *inp, long *result, int base);
-
-/* Convert human readable size string to and int; "1k" -> 1000 */
-int Parser_size(unsigned long *sizep, char *str);
-
-/* Convert a string boolean to an int; "enable" -> 1 */
-int Parser_bool(int *b, char *str);
+int cfs_parser(int argc, char **argv, command_t cmds[]);
 
 #endif

libcfs/include/libcfs/util/string.h
@@ -126,6 +126,7 @@ int cfs_expr_list_values(struct cfs_expr_list *expr_list, int max, __u32 **valpp
 int cfs_gettok(struct cfs_lstr *next, char delim, struct cfs_lstr *res);
 int cfs_str2num_check(char *str, int nob, unsigned *num,
 		      unsigned min, unsigned max);
+int cfs_human_readable2long(unsigned long *sizep, char *str);
 int cfs_expr2str(struct list_head *list, char *str, size_t size);
 int cfs_expr_list_match(__u32 value, struct cfs_expr_list *expr_list);
 int cfs_expr_list_print(char *buffer, int count,

commit 48a1a3d2a84547bbdd3af94218701def8a7859bf

LU-19295 utils: handle insufficient arguments to lst
When passing insufficient arguments to lst command it
was expecting to throw a usage message. Instead it was
segfaulting. The reason was that cfs_parser() did not
have an exit condition and was recursively calling
the same function and finally reaching the stack limit
and segfaulting. This behavior was seen across all the
lst commands.

Add an exit condition and pass the return value to the
calling function so it can bail out with a usage message.

------------
Before patch
------------
$ lst add_group
Segmentation fault (core dumped)

$ lst del_group
Segmentation fault (core dumped)

------------
After patch
------------
$ lst add_group
Usage: lst group NAME IDs [IDs]...

$ lst del_group
Usage: lst del_group NAME

Test-Parameters: trivial
Fixes: 21080400f ("LU-16723 libcfs: refactor parser to be simpler")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Iaedd068629f1f1699b5efad0104279eae9ef9907
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60886
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

libcfs/libcfs/util/parser.c
@@ -156,6 +156,9 @@ int cfs_parser(int argc, char **argv, command_t cmds[])
 	for (cmd = cmds; cmd->pc_name && i < MAXCMDS; cmd++)
 		top_level[i++] = *cmd;
 
+	if (argc == 2 && (strcmp(argv[0], "help") == 0))
+		return CMD_HELP;
+
 	if (argc > 1)
 		rc = cfs_parser_execarg(argc - 1, argv + 1, cmds);
 	else

lnet/utils/lst.c
@@ -34,7 +34,7 @@
 #include <linux/lnet/nidstr.h>
 #include "lnetconfig/liblnetconfig.h"
 
-static void lst_print_usage(char *cmd);
+static int lst_print_usage(char *cmd);
 
 static int lst_info_batch_ioctl(char *batch, int test, int server,
 		       struct lstcon_test_batch_ent *entp, int *idxp,
@@ -781,8 +781,7 @@ jt_lst_new_session(int argc, char **argv)
 			timeout_s = optarg;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
@@ -818,8 +817,7 @@ jt_lst_new_session(int argc, char **argv)
 		snprintf(buf, sizeof(buf), "%s@%s", user, host);
 		name = buf;
 	} else {
-		lst_print_usage(argv[0]);
-		return -1;
+		return lst_print_usage(argv[0]);
 	}
 
 	rc = lst_yaml_session(name, timeout_s, nlflags, "new session");
@@ -1467,15 +1465,12 @@ jt_lst_ping(int argc,  char **argv)
 			break;
 
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
-	if (type == 0 || timeout <= 0 || optind != argc) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (type == 0 || timeout <= 0 || optind != argc)
+		return lst_print_usage(argv[0]);
 
 	if (type == LST_OPC_BATCHCLI && server)
 		type = LST_OPC_BATCHSRV;
@@ -1626,10 +1621,8 @@ jt_lst_add_group(int argc, char **argv)
 		return -1;
 	}
 
-	if (argc < 3) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (argc < 3)
+		return lst_print_usage(argv[0]);
 
 	name = argv[1];
 	if (strlen(name) >= LST_NAME_SIZE) {
@@ -1746,10 +1739,8 @@ jt_lst_del_group(int argc, char **argv)
 		return -1;
 	}
 
-	if (argc != 2) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (argc != 2)
+		return lst_print_usage(argv[0]);
 
 	rc = lst_del_group(argv[1]);
 
@@ -1810,42 +1801,36 @@ jt_lst_update_group(int argc, char **argv)
 
 		switch (c) {
 		case 'f':
-			if (opc != 0) {
-				lst_print_usage(argv[0]);
-				return -1;
-			}
+			if (opc != 0)
+				return lst_print_usage(argv[0]);
+
 			opc = LST_GROUP_REFRESH;
 			break;
 
 		case 'r':
-			if (opc != 0) {
-				lst_print_usage(argv[0]);
-				return -1;
-			}
+			if (opc != 0)
+				return lst_print_usage(argv[0]);
+
 			opc = LST_GROUP_RMND;
 			str = optarg;
 			break;
 
 		case 'c':
 			clean = lst_node_str2state(optarg);
-			if (opc != 0 || clean <= 0) {
-				lst_print_usage(argv[0]);
-				return -1;
-			}
+			if (opc != 0 || clean <= 0)
+				return lst_print_usage(argv[0]);
+
 			opc = LST_GROUP_CLEAN;
 			break;
 
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
 	/* no OPC or group is specified */
-	if (opc == 0 || optind != argc - 1) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (opc == 0 || optind != argc - 1)
+		return lst_print_usage(argv[0]);
 
 	grp = argv[optind];
 
@@ -2018,8 +2003,7 @@ jt_lst_list_group(int argc, char **argv)
 			verbose = all = 1;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
@@ -2584,14 +2568,12 @@ jt_lst_stat(int argc, char **argv)
 			break;
 
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
 	if (optind == argc) {
-		lst_print_usage(argv[0]);
-		return -1;
+		return lst_print_usage(argv[0]);
 	}
 
 	if (timeout <= 0 || delay <= 0) {
@@ -2696,15 +2678,12 @@ jt_lst_show_error(int argc, char **argv)
 			break;
 
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
-	if (optind == argc) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (optind == argc)
+		return lst_print_usage(argv[0]);
 
 	INIT_LIST_HEAD(&head);
 
@@ -2815,10 +2794,8 @@ jt_lst_add_batch(int argc, char **argv)
 		return -1;
 	}
 
-	if (argc != 2) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (argc != 2)
+		return lst_print_usage(argv[0]);
 
 	name = argv[1];
 	if (strlen(name) >= LST_NAME_SIZE) {
@@ -2885,8 +2862,7 @@ jt_lst_start_batch(int argc, char **argv)
 			timeout = atoi(optarg);
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
@@ -2897,8 +2873,7 @@ jt_lst_start_batch(int argc, char **argv)
 		batch = argv[optind];
 
 	} else {
-		lst_print_usage(argv[0]);
-		return -1;
+		return lst_print_usage(argv[0]);
 	}
 
 	rc = lst_get_node_count(LST_OPC_BATCHCLI, batch, &count, NULL);
@@ -2983,8 +2958,7 @@ jt_lst_stop_batch(int argc, char **argv)
 			force = 1;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
@@ -2995,8 +2969,7 @@ jt_lst_stop_batch(int argc, char **argv)
 		batch = argv[optind];
 
 	} else {
-		lst_print_usage(argv[0]);
-		return -1;
+		return lst_print_usage(argv[0]);
 	}
 
 	rc = lst_get_node_count(LST_OPC_BATCHCLI, batch, &count, NULL);
@@ -3212,8 +3185,7 @@ jt_lst_list_batch(int argc, char **argv)
 			ntest = 1;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
@@ -3228,10 +3200,8 @@ jt_lst_list_batch(int argc, char **argv)
 		return -1;
 	}
 
-	if (optind != argc - 1) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (optind != argc - 1)
+		return lst_print_usage(argv[0]);
 
 	batch = argv[optind];
 
@@ -3410,23 +3380,19 @@ jt_lst_query_batch(int argc, char **argv)
 			verbose = 1;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
-	if (test < 0 || timeout <= 0 || delay <= 0 || loop <= 0) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (test < 0 || timeout <= 0 || delay <= 0 || loop <= 0)
+		return lst_print_usage(argv[0]);
 
 	if (optind == argc) {
 		batch = LST_DEFAULT_BATCH;
 	} else if (optind == argc - 1) {
 		batch = argv[optind];
 	} else {
-		lst_print_usage(argv[0]);
-		return -1;
+		return lst_print_usage(argv[0]);
 	}
 
 
@@ -3766,15 +3732,12 @@ jt_lst_add_test(int argc, char **argv)
 			to = optarg;
 			break;
 		default:
-			lst_print_usage(argv[0]);
-			return -1;
+			return lst_print_usage(argv[0]);
 		}
 	}
 
-	if (optind == argc || from == NULL || to == NULL) {
-		lst_print_usage(argv[0]);
-		return -1;
-	}
+	if (optind == argc || from == NULL || to == NULL)
+		return lst_print_usage(argv[0]);
 
 	if (concur <= 0 || concur > LST_MAX_CONCUR) {
 		fprintf(stderr, "Invalid concurrency of test: %d\n", concur);
@@ -3924,12 +3887,12 @@ lst_initialize(void)
 	return 0;
 }
 
-static void
+static int
 lst_print_usage(char *cmd)
 {
 	char *argv[] = { "help", cmd };
 
-	cfs_parser(2, argv, lst_cmdlist);
+	return cfs_parser(2, argv, lst_cmdlist);
 }
 
 int main(int argc, char **argv)