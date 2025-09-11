8f3af593fa91f2718e222c7662454d8f373a59b1
Shaun Tancheff <shaun.tancheff@hpe.com>
2025-09-11 11:16:59 +0700

    LU-16807 lnet: use flexible array members when possible
    sanity-lnet/270 fails with UBSAN enabled.
    
    Test-Parameters: trivial
    Fixes: 124bc53a970 ("LU-18808 lnet: allow LNDs to discover metadata via pings")
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I9e539d2fdf8966b79039ed18a9c2d640258d9a34
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61252
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Timothy Day <timday@amazon.com>
    Reviewed-by: Chris Horn <chris.horn@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lnet/include/uapi/linux/lnet/lnet-idl.h
+++ lnet/include/uapi/linux/lnet/lnet-idl.h
@@ -258,7 +258,7 @@ struct lnet_nid_md_entry {
 
 struct lnet_nid_metadata {
 	__u32				num_nid_mappings;
-	struct lnet_nid_md_entry	nid_mappings[0];
+	struct lnet_nid_md_entry	nid_mappings[];
 } __attribute__((packed));
 
 /* NB: value of these features equal to LNET_PROTO_PING_VERSION_x
