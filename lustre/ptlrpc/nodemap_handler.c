lustre/lustre-release

commit 26361c3f3206473182fa15bda7675f57ca5412ba

LU-19157 nodemap: allow ban list on default nodemap
Allow the default nodemap to have a ban list. As the default nodemap
does not have any NID ranges, the constraint on its ban list is that
it cannot conflict with any NID range defined on any other nodemap.

Enhance sanity-sec test_81a to test ban list on the default nodemap.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I4c3d6f86353aa5f4ec00750891d9b47511b4e384
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60074
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -1270,8 +1270,36 @@ int nodemap_add_range_helper(struct nodemap_config *config,
 {
 	struct lu_nid_range *prange = NULL;
 	struct lu_nid_range *range;
-	int rc;
+	int rc = 0;
 
+	/* If range_id is non-zero, we are loading from disk. So when the NID
+	 * range was added initially, it was checked that it does not conflict
+	 * with any existing ban list on the default nodemap.
+	 * Skip the test in this case.
+	 */
+	if (range_id)
+		GOTO(new_range, rc);
+
+	/* As the default nodemap can have a banlist, we need to check this
+	 * before adding a regular NID range to a nodemap.
+	 */
+	down_write(&active_config->nmc_ban_range_tree_lock);
+	range = ban_range_search(config, (struct lnet_nid *)&nid[0]);
+	if (!range)
+		range = ban_range_search(config, (struct lnet_nid *)&nid[1]);
+	up_write(&active_config->nmc_ban_range_tree_lock);
+	if (range) {
+		rc = -EEXIST;
+		CDEBUG(D_SEC,
+		       "Cannot add range [ %s - %s ] to nodemap %s, conflicts with banlist [ %s - %s ] from nodemap %s: rc = %d\n",
+		       libcfs_nidstr(&nid[0]), libcfs_nidstr(&nid[1]),
+		       nodemap->nm_name, libcfs_nidstr(&range->rn_start),
+		       libcfs_nidstr(&range->rn_end),
+		       range->rn_nodemap->nm_name, rc);
+		GOTO(out, rc);
+	}
+
+new_range:
 	down_write(&config->nmc_range_tree_lock);
 	range = range_create(config, &nid[0], &nid[1], netmask, nodemap,
 			     range_id);
@@ -1490,11 +1518,21 @@ int nodemap_add_ban_range_helper(struct nodemap_config *config,
 
 	/* Find out if range to be added to ban list is included in
 	 * regular NID ranges for this nodemap.
+	 * If nodemap is default, the ban range must not be included totally or
+	 * partially in any regular NID ranges from any other nodemap.
 	 */
 	down_write(&active_config->nmc_range_tree_lock);
 	range = range_find(config, &nid[0], &nid[1], netmask, false);
-	if (!range || range->rn_nodemap != nodemap)
-		rc = -EINVAL;
+	if (is_default_nodemap(nodemap)) {
+		if (range)
+			rc = -EINVAL;
+		if (range_search(config, (struct lnet_nid *)&nid[0]) ||
+		    range_search(config, (struct lnet_nid *)&nid[1]))
+			rc = -EINVAL;
+	} else {
+		if (!range || range->rn_nodemap != nodemap)
+			rc = -EINVAL;
+	}
 	up_write(&active_config->nmc_range_tree_lock);
 	if (rc)
 		GOTO(out, rc);
@@ -1526,7 +1564,8 @@ new_range:
 	/* nodemaps have no members if they aren't on the active config */
 	if (config == active_config) {
 		nm_member_reclassify_nodemap(config->nmc_default_nodemap);
-		nm_member_reclassify_nodemap(nodemap);
+		if (nodemap != config->nmc_default_nodemap)
+			nm_member_reclassify_nodemap(nodemap);
 	}
 	up_read(&active_config->nmc_range_tree_lock);
 
@@ -1536,7 +1575,8 @@ new_range:
 
 	if (config == active_config) {
 		nm_member_revoke_locks(config->nmc_default_nodemap);
-		nm_member_revoke_locks(nodemap);
+		if (nodemap != config->nmc_default_nodemap)
+			nm_member_revoke_locks(nodemap);
 	}
 
 out:
@@ -1555,9 +1595,6 @@ int nodemap_add_banlist(const char *name, const struct lnet_nid nid[2],
 		mutex_unlock(&active_config_lock);
 		GOTO(out, rc = PTR_ERR(nodemap));
 	}
-
-	if (is_default_nodemap(nodemap))
-		GOTO(out_unlock, rc = -EINVAL);
 
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_unlock, rc = -ENXIO);
@@ -1598,9 +1635,6 @@ int nodemap_del_banlist(const char *name, const struct lnet_nid nid[2],
 		mutex_unlock(&active_config_lock);
 		GOTO(out, rc = PTR_ERR(nodemap));
 	}
-
-	if (is_default_nodemap(nodemap))
-		GOTO(out_putref, rc = -EINVAL);
 
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);

Documentation/man8/lctl-nodemap-banlist-add.8
@@ -10,9 +10,13 @@ or
 .YS
 .SH DESCRIPTION
 .B nodemap_banlist_add
-adds a range of banned NIDs to an existing nodemap.
-The banned NID range must be included into the normal NID range associated with
-the nodemap.
+adds a range of banned NIDs to an existing nodemap, either a normal or the
+default nodemap.
+For normal nodemaps, the banned NID range must be included into a regular NID
+range associated with the nodemap.
+For the default nodemap, the added range of banned NIDs must not conflict with
+any NID range defined on any other nodemap.
+.br
 Clients with NIDs that fall into the banned range will be blocked by servers,
 and will get 'Operation not permitted' for all requests except unmount. The
 banning happens as soon as the nodemap definition is propagated to the servers.
@@ -21,6 +25,7 @@ As long as a client is banned, it cannot mount Lustre.
 .TP
 .BI --name " NODEMAP_NAME"
 The name of the nodemap that this banned NID range should be added to.
+It may be 'default' and will accept any NID not in another nodemap.
 .TP
 .BI --range " NID_RANGE"
 The banned NID range that should be added to the nodemap.

lustre/ptlrpc/nodemap_lproc.c
@@ -1355,6 +1355,10 @@ static struct ldebugfs_vars lprocfs_default_nodemap_vars[] = {
 		.fops		= &nodemap_raise_privs_fops,
 	},
 	{
+		.name		= "banlist",
+		.fops		= &nodemap_ban_ranges_fops,
+	},
+	{
 		.name		= "readonly_mount",
 		.fops		= &nodemap_readonly_mount_fops,
 	},

lustre/tests/sanity-sec.sh
@@ -9938,6 +9938,48 @@ test_81a() {
 	$LFS mkdir -i 0 -c 1 $DIR/$tdir || error "mkdir $DIR/$tdir failed"
 	echo hi > $DIR/$tdir/$tfile || error "create $DIR/$tdir/$tfile failed"
 
+	do_facet mgs $LCTL nodemap_add $nm
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property admin=1
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property trusted=1
+	do_facet mgs $LCTL nodemap_activate 1
+	wait_nm_sync active
+
+	# check nodemap exports
+	do_facet mds1 "$LCTL get_param nodemap.*.exports"
+	exp_cnt_orig=$(do_facet mds1 "$LCTL get_param nodemap.default.exports |
+			grep MDT | grep -c $client_nid")
+	(( exp_cnt_orig != 0 )) ||
+		error "no exports for $client_nid found on default"
+
+	# check client access: should succeed
+	cancel_lru_locks
+	ls $DIR/$tdir || error "ls $DIR/$tdir failed (0)"
+	cat $DIR/$tdir/$tfile || error "cat $DIR/$tdir/$tfile failed (0)"
+
+	# add ban list to default nodemap
+	stack_trap "do_facet mgs $LCTL nodemap_banlist_del --name default \
+		--range $client_nid || true" EXIT
+	do_facet mgs $LCTL nodemap_banlist_add --name default \
+		--range $client_nid ||
+		      error "Setting banlist=$client_nid on default failed"
+	wait_nm_sync default banlist
+
+	# check client access: should fail
+	cancel_lru_locks
+	ls $DIR/$tdir && error "ls $DIR/$tdir should fail (0)"
+	cat $DIR/$tdir/$tfile && error "cat $DIR/$tdir/$tfile should fail (0)"
+
+	# no nodemap can have a NID range that conflicts with default's banlist
+	do_facet mgs $LCTL nodemap_add_range \
+		--name $nm --range $client_nid &&
+		error "Add range $client_nid to $nm should fail"
+	do_facet mgs $LCTL nodemap_del $nm
+	do_facet mgs $LCTL nodemap_banlist_del --name default \
+		--range $client_nid ||
+		error "Deleting banlist=$client_nid on default failed"
+
 	# setup nodemap
 	setup_local_client_nodemap $nm 1 1
 
@@ -9954,6 +9996,9 @@ test_81a() {
 	cat $DIR/$tdir/$tfile || error "cat $DIR/$tdir/$tfile failed (1)"
 
 	# add ban list
+	do_facet mgs $LCTL nodemap_banlist_add --name default \
+		--range $client_nid &&
+		      error "Setting banlist=$client_nid on default should fail"
 	do_facet mgs $LCTL nodemap_banlist_add --name $nm --range $fake_nid &&
 		error "Setting banlist=$fake_nid on $nm should fail"
 	do_facet mgs $LCTL nodemap_banlist_add --name $nm --range $client_nid ||

commit 5a3722af0849e6503777f46b163a5372fde3e6d9

LU-19157 nodemap: read semaphore for ban_range_search
Acquire the nmc_ban_range_tree_lock semaphore for read when calling
ban_range_search.

Fixes: 26361c3f32 ("LU-19157 nodemap: allow ban list on default nodemap")
Test-Parameters: trivial
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I6511e7d45213876bb0af843b5b49e2a34da36a68
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60984
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -1281,11 +1281,11 @@ int nodemap_add_range_helper(struct nodemap_config *config,
 	/* As the default nodemap can have a banlist, we need to check this
 	 * before adding a regular NID range to a nodemap.
 	 */
-	down_write(&active_config->nmc_ban_range_tree_lock);
+	down_read(&active_config->nmc_ban_range_tree_lock);
 	range = ban_range_search(config, (struct lnet_nid *)&nid[0]);
 	if (!range)
 		range = ban_range_search(config, (struct lnet_nid *)&nid[1]);
-	up_write(&active_config->nmc_ban_range_tree_lock);
+	up_read(&active_config->nmc_ban_range_tree_lock);
 	if (range) {
 		rc = -EEXIST;
 		CDEBUG(D_SEC,