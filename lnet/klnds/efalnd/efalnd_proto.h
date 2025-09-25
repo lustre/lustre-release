lustre/lustre-release

commit 0c773016d837a4751678a0eeff00dee30e1d2586

LU-18977 efalnd: Introduce AWS EFA Lustre network driver
This commit introduces a new Lustre network driver (LND) for AWS Elastic
Fabric Adapter (EFA), enabling high-performance, low-latency networking
for Lustre clusters on AWS infrastructure.

EFA is a network device that provides reliable communication and kernel
bypass capabilities, targeting more consistent latency and higher
throughput than traditional TCP-based communication.

Key components added:
- EFA LND core implementation with support for RDMA operations
- Dual NID support:
  * Small NIDs: Uses TCP NI for out-of-band communication to discover
    remote GID and CM QP data, this utilizing LNET ping operations.
  * Large NIDs: Leverages 16 bytes of available address space to store
    connection data directly, eliminating the need for out-of-band
    discovery.
- DebugFS interface for monitoring and troubleshooting
- Module parameter configuration for tuning performance
- Integration with existing LNET infrastructure

The LND already has active customers and has been thoroughly tested
using selftest, IOR, FIO, GDS, and multiple sanity test variations.

Test-Parameters: trivial testlist=sanity-lnet serverversion=2.16
Signed-off-by: Timothy Day <timday@amazon.com>
Signed-off-by: Michael Margolin <mrgolin@amazon.com>
Signed-off-by: Yehuda Yitschak <yehuday@amazon.com>
Signed-off-by: Yonatan Nachum <ynachum@amazon.com>
Change-Id: I2217c0bbffefebb52152ea36feaaa2e5e78ad10c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59470
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Max Wang <wamax@amazon.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/efalnd/efalnd_proto.h
@@ -1,0 +1,201 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2023-2025, Amazon and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Author: Yehuda Yitschak <yehuday@amazon.com>
+ * Author: Yonatan Nachum <ynachum@amazon.com>
+ */
+
+#ifndef _EFALND_PROTO_H_
+#define _EFALND_PROTO_H_
+
+enum kefa_comp_status {
+	/* Successful completion */
+	KEFA_COMP_STATUS_OK				= 0,
+	/* Unsupported operation */
+	KEFA_COMP_STATUS_UNSUPPORTED_OP			= 1,
+	/* No memory or pool empty on remote side */
+	KEFA_COMP_STATUS_NO_MEMORY			= 2,
+	/* Remote general error */
+	KEFA_COMP_STATUS_GENERAL_ERROR			= 3,
+
+	/* Failed to send message */
+	KEFA_COMP_STATUS_COMM_FAILURE			= 20,
+	/* No LNET message on remote side */
+	KEFA_COMP_STATUS_NO_LNET_MSG			= 21,
+	/* Failed translating BIO vector to SG list */
+	KEFA_COMP_STATUS_BAD_ADDRESS			= 22,
+	/* Failed mapping SG list (to device or MR) */
+	KEFA_COMP_STATUS_DMA_FAILURE			= 23,
+	/* Protocol not supported on remote side */
+	KEFA_COMP_STATUS_UNSUPPORTED_PROTO		= 24,
+};
+
+struct kefa_nid_md_entry {
+	lnet_nid_t nid;
+	u8 gid[16];
+	u16 qp_num;
+	u32 qkey;
+	u8 buffer[10];
+} __packed;
+
+struct kefa_qp_proto {
+	u16 qp_num;
+	u32 qkey;
+} __packed;
+
+struct kefa_rdma_desc {
+	u32 key;			/* local/remote key */
+	u64 addr;
+	u32 nob;			/* # of bytes */
+} __packed;
+
+struct kefa_immediate_msg_v2 {
+	struct lnet_hdr_nid16 hdr;	/* portals header */
+	char payload[0];		/* piggy-backed payload */
+} __packed;
+
+struct kefa_putr_req_msg_v2 {
+	struct lnet_hdr_nid16 hdr;	/* portals header */
+	u64 cookie;			/* opaque completion cookie */
+	struct kefa_rdma_desc rdma_desc;/* src rdma desc */
+} __packed;
+
+struct kefa_getr_req_msg_v2 {
+	struct lnet_hdr_nid16 hdr;	/* portals header */
+	u64 sink_cookie;		/* opaque completion cookie */
+} __packed;
+
+struct kefa_getr_ack_msg {
+	u64 sink_cookie;		/* opaque completion cookie */
+	u64 src_cookie;			/* opaque completion cookie */
+	struct kefa_rdma_desc rdma_desc;/* src rdma desc */
+} __packed;
+
+struct kefa_completion_msg {
+	u64 cookie;			/* opaque completion cookie */
+	s16 status;			/* enum kefa_comp_status */
+} __packed;
+
+struct kefa_conn_probe_msg {
+	u16 lnd_ver;			/* LND version */
+	u8 src_gid[16];			/* GID in raw format */
+	u64 src_epoch;			/* Sender's epoch */
+	struct kefa_qp_proto cm_qp;
+	u64 caps;			/* Capabilities bit array */
+} __packed;
+
+struct kefa_conn_probe_resp_msg {
+	u16 lnd_ver;			/* LND version */
+	s16 status;			/* enum kefa_comp_status */
+	u64 src_epoch;			/* Responder's epoch */
+	u64 caps;			/* Capabilities bit array */
+	u8 min_proto_ver;		/* Min supported protocol version */
+	u8 max_proto_ver;		/* Max supported protocol version */
+} __packed;
+
+struct kefa_conn_req_msg {
+	/* Optional for skipping conn probe */
+	u16 lnd_ver;			/* LND version */
+	u8 src_gid[16];			/* GID in raw format */
+	u64 src_epoch;			/* Sender's epoch */
+	struct kefa_qp_proto cm_qp;
+	u64 caps;			/* Capabilities bit array */
+	u64 reserved;
+	u64 requests;			/* Requests bit array */
+	u32 src_conn_id;		/* Sender's connection ID */
+	u32 nqps;			/* Number of data QPs on the array */
+	struct kefa_qp_proto data_qps[0]; /* Data QPs array */
+} __packed;
+
+struct kefa_conn_req_ack {
+	u16 lnd_ver;			/* LND version */
+	u64 src_epoch;			/* Responder's epoch */
+	u64 caps;			/* Capabilities bit array */
+	u64 reserved;
+	s16 status;			/* enum kefa_comp_status */
+	u32 src_conn_id;		/* Responder's connection ID */
+	u32 nqps;			/* Number of data QPs on the array */
+	struct kefa_qp_proto data_qps[0]; /* Data QPs array */
+} __packed;
+
+struct kefa_msg_v1 {
+	lnet_nid_t srcnid;		/* sender's NID */
+	lnet_nid_t dstnid;		/* destination's NID */
+	u64 dst_epoch;			/* destination's epoch */
+	u32 dst_conn_id;		/* ID for fast connection retrieval */
+	u8 credits;			/* returned credits */
+
+	union {
+		struct kefa_conn_probe_msg conn_probe;
+		struct kefa_conn_probe_resp_msg conn_probe_resp;
+	} __packed u;
+	/* No additional fields can be added after the union. */
+} __packed;
+
+struct kefa_msg_v2 {
+	struct lnet_nid srcnid;		/* sender's NID */
+	struct lnet_nid dstnid;		/* destination's NID */
+	u64 dst_epoch;			/* destination's epoch */
+	u32 dst_conn_id;		/* ID for fast connection retrieval */
+	u8 credits;			/* returned credits */
+
+	union {
+		struct kefa_immediate_msg_v2 immediate;
+		struct kefa_putr_req_msg_v2 putr_req;
+		struct kefa_getr_req_msg_v2 getr_req;
+		struct kefa_getr_ack_msg getr_ack;
+		struct kefa_completion_msg completion;
+		struct kefa_conn_probe_msg conn_probe;
+		struct kefa_conn_probe_resp_msg conn_probe_resp;
+		struct kefa_conn_req_msg conn_request;
+		struct kefa_conn_req_ack conn_request_ack;
+	} __packed u;
+	/* No additional fields can be added after the union. */
+} __packed;
+
+struct kefa_hdr {
+	/* First 3 fields fixed FOR ALL TIME */
+	u32 magic;			/* I'm an efanal message */
+	u8 proto_ver;			/* this is my protocol version number */
+	u8 type;			/* efa msg type */
+	u16 nob;			/* # bytes in whole message */
+} __packed;
+
+struct kefa_msg {
+	struct kefa_hdr hdr;
+	union {
+		struct kefa_msg_v1 msg_v1;
+		struct kefa_msg_v2 msg_v2;
+	} __packed;
+	/* No additional fields can be added after the union. */
+} __packed;
+
+#define EFALND_MSG_MAGIC LNET_PROTO_EFA_MAGIC	/* unique magic */
+
+#define EFALND_PROTO_VER_1	0x81
+#define EFALND_PROTO_VER_2	0x82
+#define EFALND_MIN_PROTO_VER	EFALND_PROTO_VER_1
+#define EFALND_MAX_PROTO_VER	EFALND_PROTO_VER_2
+
+#define EFALND_MSG_RESERVED		0x00
+#define EFALND_MSG_CONN_PROBE		0x01	/* connection probe */
+#define EFALND_MSG_CONN_PROBE_RESP	0x02	/* connection probe response */
+#define EFALND_MSG_CONN_REQ		0x03	/* connection request */
+#define EFALND_MSG_CONN_REQ_ACK		0x04	/* connection request ack */
+#define EFALND_MSG_IMMEDIATE		0x05	/* immediate */
+#define EFALND_MSG_NACK			0x06	/* put/get request - nak */
+#define EFALND_MSG_PUTR_REQ		0x07	/* put request - READ based */
+#define EFALND_MSG_PUTR_DONE		0x08	/* put done - READ based */
+#define EFALND_MSG_GETR_REQ		0x09	/* get request - READ based */
+#define EFALND_MSG_GETR_ACK		0x0a	/* get request - READ based ack */
+#define EFALND_MSG_GETR_DONE		0x0b	/* get request - completed */
+#define EFALND_MSG_MAX			0x0c
+
+#endif

MAINTAINERS
@@ -392,6 +392,12 @@ R:	Chris Horn <chris.horn@hpe.com>
 S:	Supported
 F:	lnet/klnds/socklnd/
 
+Lustre Networking KEFALND
+R:	Yonatan Nachum <ynachum@amazon.com>
+R:	Michael Margolin <mrgolin@amazon.com>
+S:	Supported
+F:	lnet/klnds/efalnd/
+
 Lustre Nodemap
 R:	Sebastien Buisson <sbuisson@whamcloud.com>
 S:	Supported

autoMakefile.am
@@ -380,6 +380,9 @@ debs: undef.h debs_common
 	if test "x@DPKG_INT_O2IB@" != "x"; then \
 		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} int_o2ib"; \
 	fi; \
+	if test "x@EFALND@" != "x"; then \
+		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} efalnd"; \
+	fi; \
 	if test "x@CONFIG_CACHE_FILE@" != "x"; then \
 		export CONFIG_CACHE_FILE="@CONFIG_CACHE_FILE@"; \
 	fi; \

config/lustre-lnet.m4
@@ -866,6 +866,108 @@ AS_IF([test $ENABLEO2IB != "no"], [
 ]) # LN_CONFIG_O2IB
 
 #
+# LN_CONFIG_EFALND
+#
+# check whether to use the EFA Network Interface lnd
+#
+AC_DEFUN([LN_CONFIG_EFALND], [
+AC_MSG_CHECKING([whether to enable EFA LND])
+AC_ARG_ENABLE([efa],
+	AC_HELP_STRING([--enable-efa=[yes|no|<path>]],
+		       [enable EFA LND]),
+	[], [enable_efa="no"])
+
+AS_CASE([$enable_efa],
+	[yes],
+	[
+		AS_IF([test x$uses_dpkg = xyes], [
+			LS_PKG="dpkg --listfiles"
+		], [
+			LS_PKG="rpm -ql"
+		])
+
+		EFA_PKG="efa"
+		EFA_HEADER="efa_verbs.h"
+		EFA_HEADER_PATH=$(eval "$LS_PKG $EFA_PKG" | egrep "$EFA_HEADER" | head -n1)
+		AS_IF([test -n "$EFA_HEADER_PATH"], [
+			EFA_INCLUDE_PATH=$(dirname $EFA_HEADER_PATH)
+		])
+
+		KERNEL_RDMA_INCLUDE="$LINUX/include/rdma"
+		AS_IF([test -z "$EFA_INCLUDE_PATH" && test -f "$KERNEL_RDMA_INCLUDE/$EFA_HEADER"], [
+			EFA_INCLUDE_PATH=$KERNEL_RDMA_INCLUDE
+		])
+
+		AS_IF([test -z "$EFA_INCLUDE_PATH"], [
+			AC_MSG_ERROR([Could not find a supported EFA driver. For custom built driver, the path to header files can be supplied using --enable-efa=<path>.])
+		])
+
+		ENABLE_EFA="yes"
+	],
+	[no],
+	[
+		ENABLE_EFA="no"
+	],
+	[
+		EFA_INCLUDE_PATH=$enable_efa
+		ENABLE_EFA="yes"
+	])
+
+AS_IF([test $ENABLE_EFA = "no"], [
+	AC_MSG_RESULT([no])
+], [
+	AC_MSG_RESULT([yes])
+
+	# Check mandatory kernel definitions
+
+	LB_CHECK_COMPILE([if 'ib_device_get_by_name' exists],
+	ib_device_get_by_name, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ib_device_get_by_name(NULL, 0);
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if 'RDMA_DRIVER_EFA' exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		int x = RDMA_DRIVER_EFA;
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if ib_device 'kverbs_provider' flag exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		struct ib_device dev = {
+			.kverbs_provider = 0,
+		};
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	# Check optional kernel definitions
+
+	LB_CHECK_COMPILE([if 'ibdev_to_node' exists],
+	ibdev_to_node, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ibdev_to_node(NULL);
+	],[
+		AC_DEFINE(HAVE_IBDEV_TO_NODE, 1,
+			[ibdev_to_node() is defined])
+	])
+
+	EFALND="efalnd"
+	AC_SUBST(EFALND)
+	AC_SUBST(EFA_INCLUDE_PATH)
+])
+]) # LN_CONFIG_EFALND
+
+#
 # LN_CONFIG_GNILND
 #
 # check whether to use the Gemini Network Interface lnd
@@ -1276,6 +1378,7 @@ AC_MSG_NOTICE([LNet kernel checks
 
 LN_CONFIG_BACKOFF
 LN_CONFIG_O2IB
+LN_CONFIG_EFALND
 LN_CONFIG_GNILND
 LN_CONFIG_KFILND
 ]) # LN_PROG_LINUX
@@ -1431,6 +1534,7 @@ LN_USR_NLMSGERR
 AC_DEFUN([LN_CONDITIONALS], [
 AM_CONDITIONAL(EXTERNAL_KO2IBLND,  test x$EXTERNAL_KO2IBLND = "xyes")
 AM_CONDITIONAL(BUILT_IN_KO2IBLND,  test x$BUILT_IN_KO2IBLND = "xyes")
+AM_CONDITIONAL(BUILD_EFALND,	   test x$EFALND = "xefalnd")
 AM_CONDITIONAL(BUILD_GNILND,       test x$GNILND  = "xgnilnd")
 AM_CONDITIONAL(BUILD_KFILND,       test x$KFILND  = "xkfilnd")
 ]) # LN_CONDITIONALS
@@ -1453,6 +1557,8 @@ lnet/klnds/o2iblnd/Makefile
 lnet/klnds/o2iblnd/autoMakefile
 lnet/klnds/in-kernel-o2iblnd/Makefile
 lnet/klnds/in-kernel-o2iblnd/autoMakefile
+lnet/klnds/efalnd/Makefile
+lnet/klnds/efalnd/autoMakefile
 lnet/klnds/gnilnd/Makefile
 lnet/klnds/gnilnd/autoMakefile
 lnet/klnds/socklnd/Makefile

debian/rules
@@ -257,6 +257,9 @@ configure-stamp: autogen-stamp debian/control.main debian/control.modules.in
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
 	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
+	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \
 	options="$${options} libcfs-cdebug libcfs-trace libcfs-assert"; \
@@ -588,6 +591,9 @@ kdist_config: prep-deb-files patch-stamp
 	fi; \
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
+	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
 	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \

lnet/include/lnet/lib-lnet.h
@@ -619,6 +619,7 @@ extern int live_router_check_interval;
 extern int dead_router_check_interval;
 extern int portal_rotor;
 extern int lock_prim_nid;
+extern int lnet_interfaces_max;
 
 void lnet_mt_event_handler(struct lnet_event *event);
 

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -114,6 +114,11 @@ struct lnet_ioctl_config_kfilnd_tunables {
 	__u32 lnd_timeout;
 };
 
+struct lnet_ioctl_config_efalnd_tunables {
+	__u32 lnd_version;
+	__u32 lnd_nqps;
+};
+
 struct lnet_ioctl_config_socklnd_tunables {
 	__u32 lnd_version;
 	__u16 lnd_conns_per_peer;
@@ -130,6 +135,7 @@ struct lnet_ioctl_config_gnilnd_tunables {
 struct lnet_lnd_tunables {
 	union {
 		struct lnet_ioctl_config_o2iblnd_tunables lnd_o2ib;
+		struct lnet_ioctl_config_efalnd_tunables lnd_efa;
 		struct lnet_ioctl_config_socklnd_tunables lnd_sock;
 		struct lnet_ioctl_config_kfilnd_tunables lnd_kfi;
 		struct lnet_ioctl_config_gnilnd_tunables lnd_gni;

lnet/include/uapi/linux/lnet/lnet-idl.h
@@ -185,6 +185,7 @@ struct lnet_magicversion {
 #define LNET_PROTO_KFI_MAGIC		0xdeadbeef
 #define LNET_PROTO_ACCEPTOR_MAGIC	0xacce7100
 #define LNET_PROTO_PING_MAGIC		0x70696E67 /* 'ping' */
+#define LNET_PROTO_EFA_MAGIC		0x2be092be /* 2B or 9 2B */
 
 /* Placeholder for a future "unified" protocol across all LNDs */
 /* Current LNDs that receive a request with this magic will respond

commit 7399e4560708ae6de7b381e18c2ac9965d0ffc98

LU-16807 efalnd: use flexible array members when possible
Quash UBSAN warning

Test-Parameters: trivial
Fixes: 0c773016d83 ("LU-18977 efalnd: Introduce AWS EFA Lustre network driver")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ief1682b6686d1230eeef9841930a9973a32ac454
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61253
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Reviewed-by: Michael Margolin <mrgolin@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/klnds/efalnd/efalnd_proto.h
@@ -58,7 +58,7 @@ struct kefa_rdma_desc {
 
 struct kefa_immediate_msg_v2 {
 	struct lnet_hdr_nid16 hdr;	/* portals header */
-	char payload[0];		/* piggy-backed payload */
+	char payload[];			/* piggy-backed payload */
 } __packed;
 
 struct kefa_putr_req_msg_v2 {
@@ -111,7 +111,7 @@ struct kefa_conn_req_msg {
 	u64 requests;			/* Requests bit array */
 	u32 src_conn_id;		/* Sender's connection ID */
 	u32 nqps;			/* Number of data QPs on the array */
-	struct kefa_qp_proto data_qps[0]; /* Data QPs array */
+	struct kefa_qp_proto data_qps[]; /* Data QPs array */
 } __packed;
 
 struct kefa_conn_req_ack {
@@ -122,7 +122,7 @@ struct kefa_conn_req_ack {
 	s16 status;			/* enum kefa_comp_status */
 	u32 src_conn_id;		/* Responder's connection ID */
 	u32 nqps;			/* Number of data QPs on the array */
-	struct kefa_qp_proto data_qps[0]; /* Data QPs array */
+	struct kefa_qp_proto data_qps[]; /* Data QPs array */
 } __packed;
 
 struct kefa_msg_v1 {