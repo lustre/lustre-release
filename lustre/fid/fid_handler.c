lustre/lustre-release

commit 6f86519b3483b4cc754b42bddc98617de14cef2b

LU-10392 fid: improve seq allocation error messages
When MDTs are waiting for MDT0000 to start the master sequence
server and be granted a meta sequence for the first time, in case
of "-EINPROGRESS", the limited console messages would be clearer
and better than many error messages.

Change-Id: I64e80508cf9fb837a328b4f8228f29bfb764845d
Signed-off-by: Emoly Liu <emoly.liu@intel.com>
Reviewed-on: https://review.whamcloud.com/30623
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: Yang Sheng <yang.sheng@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Giuseppe Di Natale <dinatale2@llnl.gov>

lustre/fid/fid_handler.c
@@ -325,8 +325,17 @@ static int __seq_server_alloc_meta(struct lu_server_seq *seq,
 
 	rc = seq_server_check_and_alloc_super(env, seq);
 	if (rc < 0) {
-		CERROR("%s: Allocated super-sequence failed: rc = %d\n",
-			seq->lss_name, rc);
+		if (rc == -EINPROGRESS) {
+			static int printed;
+
+			if (printed++ % 8 == 0)
+				LCONSOLE_INFO("%s: Waiting to contact MDT0000 "
+					      "to allocate super-sequence\n",
+					      seq->lss_name);
+		} else {
+			CERROR("%s: Allocated super-sequence failed: rc = %d\n",
+			       seq->lss_name, rc);
+		}
 		RETURN(rc);
 	}
 

lustre/fid/fid_request.c
@@ -223,8 +223,9 @@ static int seq_client_alloc_seq(const struct lu_env *env,
 	if (lu_seq_range_is_exhausted(&seq->lcs_space)) {
                 rc = seq_client_alloc_meta(env, seq);
                 if (rc) {
-                        CERROR("%s: Can't allocate new meta-sequence,"
-                               "rc %d\n", seq->lcs_name, rc);
+			if (rc != -EINPROGRESS)
+				CERROR("%s: Can't allocate new meta-sequence,"
+				       "rc = %d\n", seq->lcs_name, rc);
                         RETURN(rc);
                 } else {
                         CDEBUG(D_INFO, "%s: New range - "DRANGE"\n",
@@ -379,8 +380,9 @@ int seq_client_alloc_fid(const struct lu_env *env,
 		/* Re-take seq::lcs_mutex via seq_fid_alloc_fini(). */
 		seq_fid_alloc_fini(seq, rc ? 0 : seqnr, false);
 		if (rc) {
-			CERROR("%s: Can't allocate new sequence: rc = %d\n",
-			       seq->lcs_name, rc);
+			if (rc != -EINPROGRESS)
+				CERROR("%s: Can't allocate new sequence: "
+				       "rc = %d\n", seq->lcs_name, rc);
 			mutex_unlock(&seq->lcs_mutex);
 
 			RETURN(rc);

commit ecdab1bc4e78d2202d0893f0ba420b07ccce9a01

LU-19352 fid: limit seq allocation error message
sequence allocation rpc could return -EINPROGRESS
and -EAGAIN when MDT0 is still starting up, we
should limit error message for both cases.

Test-Parameters: trivial
Fixes: 6f86519b34 ("LU-10392 fid: improve seq allocation error messages")
Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Change-Id: I473520843dc89823be20e57a8e777df80224421e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61279
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/fid/fid_handler.c
@@ -313,7 +313,7 @@ static int __seq_server_alloc_meta(struct lu_server_seq *seq,
 restart:
 	rc = seq_server_check_and_alloc_super(env, seq);
 	if (rc < 0) {
-		if (rc == -EINPROGRESS) {
+		if (rc == -EINPROGRESS || rc == -EAGAIN) {
 			static int printed;
 
 			if (printed++ % 8 == 0)

lustre/fid/fid_request.c
@@ -208,7 +208,7 @@ static int seq_client_alloc_seq(const struct lu_env *env,
 	if (lu_seq_range_is_exhausted(&seq->lcs_space)) {
 		rc = seq_client_alloc_meta(env, seq);
 		if (rc) {
-			if (rc != -EINPROGRESS)
+			if (rc != -EINPROGRESS && rc != -EAGAIN)
 				CERROR("%s: Cannot allocate new meta-sequence: rc = %d\n",
 				       seq->lcs_name, rc);
 			RETURN(rc);
@@ -311,7 +311,7 @@ int seq_client_alloc_fid(const struct lu_env *env,
 
 		rc = seq_client_alloc_seq(env, seq, &seqnr);
 		if (rc) {
-			if (rc != -EINPROGRESS)
+			if (rc != -EINPROGRESS && rc != -EAGAIN)
 				CERROR("%s: Can't allocate new sequence: rc = %d\n",
 				       seq->lcs_name, rc);
 		} else {