lustre/lustre-release

commit c5fb44f5ecf8494cd5a497947981bd6905ddc084

LU-15220 tests: avoid gcc-11 -Werror=stringop-overread warning
GCC 11 warns about string and memory operations on fixed address:

In function 'memcpy', inlined from 'obd_uuid2str' at
lustre/include/uapi/linux/lustre/lustre_user.h:1222:3,
include/linux/fortify-string.h:20:33: error: '__builtin_memcpy'
reading 39 bytes from a region of size 0 [-Werror=stringop-overread]
   20 | #define __underlying_memcpy     __builtin_memcpy
      |                                 ^
include/linux/fortify-string.h:191:16: note:
in expansion of macro '__underlying_memcpy'
  191 |         return __underlying_memcpy(p, q, size);
      |                ^~~~~~~~~~~~~~~~~~~

The patch avoids the above warning by not using a fixed address.

badarea_io.c:47:14: error: 'write' reading 5 bytes from a region
of size 0 [-Werror=stringop-overread]
   47 |         rc = write(fd, (void *)0x4096000, 5);
      |              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

The patch avoids the above warning by making the pointer volatile
as suggested in:
https://gcc.gnu.org/bugzilla/show_bug.cgi?id=99578#c16

Change-Id: I90b936835c6236a0f47e744013e3e480442f682c
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/45777
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1218,9 +1218,11 @@ static inline char *obd_uuid2str(const struct obd_uuid *uuid)
 	if (uuid->uuid[sizeof(*uuid) - 1] != '\0') {
 		/* Obviously not safe, but for printfs, no real harm done...
 		   we're always null-terminated, even in a race. */
-		static char temp[sizeof(*uuid)];
-		memcpy(temp, uuid->uuid, sizeof(*uuid) - 1);
-		temp[sizeof(*uuid) - 1] = '\0';
+		static char temp[sizeof(*uuid->uuid)];
+
+		memcpy(temp, uuid->uuid, sizeof(*uuid->uuid) - 1);
+		temp[sizeof(*uuid->uuid) - 1] = '\0';
+
 		return temp;
 	}
 	return (char *)(uuid->uuid);

lustre/tests/badarea_io.c
@@ -35,8 +35,11 @@
 int main(int argc, char **argv)
 {
 	int rc;
-	int fd = open(argv[1], O_WRONLY);
+	int fd;
+	void *volatile buf = (void *)0x4096000;
+	void *volatile fd_ptr;
 
+	fd = open(argv[1], O_WRONLY);
 	if (fd == -1) {
 		perror(argv[1]);
 		goto read;
@@ -44,27 +47,28 @@ int main(int argc, char **argv)
 
 	/* We need rc because Sles11 compiler warns against unchecked
 	 * return value of read and write */
-	rc = write(fd, (void *)0x4096000, 5);
+	rc = write(fd, buf, 5);
 	if (rc != 5)
 		perror("write badarea (Should have failed)");
 
-	rc = write(fd, &fd, 0);
+	fd_ptr = (void *)&fd;
+	rc = write(fd, fd_ptr, 0);
 	if (rc != 0)
 		perror("write zero bytes");
 
-	rc = write(fd, &fd, 1);
+	rc = write(fd, fd_ptr, 1);
 	if (rc != 1)
 		perror("write one byte");
 
-	rc = write(fd, &fd, 2UL*1024*1024);
+	rc = write(fd, fd_ptr, 2UL*1024*1024);
 	if (rc != 2UL*1024*1024)
 		perror("write 2M");
 
-	rc = write(fd, &fd, 2UL*1024*1024*1024);
+	rc = write(fd, fd_ptr, 2UL*1024*1024*1024);
 	if (rc != 2UL*1024*1024*1024)
 		perror("write 2G");
 
-	rc = write(fd, &fd, -2);
+	rc = write(fd, fd_ptr, -2);
 	if (rc != -2)
 		perror("write -2");
 
@@ -74,7 +78,7 @@ read:
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return 0;
-	rc = read(fd, (void *)0x4096000, 5);
+	rc = read(fd, buf, 5);
 	perror("read");
 
 	close(fd);

commit b8f79d83cb7a7475cd016e3bb0ea10335d543692

LU-19327 obdclass: fix obd_uuid2str()
- fix obd_uuid2str() check for UUID NULL-termination by
  using strnlen() instead of last byte check
- fix wrong sizeof(*uuid->uuid) which returns 1 to
  sizeof(uuid->uuid) which is real UUID buffer size
- obd_uuid_equals() to use strncmp() limited by UUID size
- obd_uuid2str() returns const char* so update some of it
  callers accordingly

Fixes: c5fb44f5ecf ("LU-15220 tests: avoid gcc-11 -Werror=stringop-overread warning")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I37b158cd59925a46914cbf567bb88a23f8b15b89
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61129
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>

lustre/include/cfs_hash.h
@@ -656,7 +656,7 @@ cfs_hash_dual_bd_finddel_locked(struct cfs_hash *hs, struct cfs_hash_bd *bds,
 
 /* Hash init/cleanup functions */
 struct cfs_hash *
-cfs_hash_create(char *name, unsigned int cur_bits, unsigned int max_bits,
+cfs_hash_create(const char *name, unsigned int cur_bits, unsigned int max_bits,
 		unsigned int bkt_bits, unsigned int extra_bytes,
 		unsigned int min_theta, unsigned int max_theta,
 		struct cfs_hash_ops *ops, unsigned int flags);

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1393,7 +1393,7 @@ struct obd_uuid {
 static inline bool obd_uuid_equals(const struct obd_uuid *u1,
 				   const struct obd_uuid *u2)
 {
-	return strcmp((char *)u1->uuid, (char *)u2->uuid) == 0;
+	return strncmp(u1->uuid, u2->uuid, sizeof(u1->uuid)) == 0;
 }
 
 static inline int obd_uuid_empty(struct obd_uuid *uuid)
@@ -1403,28 +1403,27 @@ static inline int obd_uuid_empty(struct obd_uuid *uuid)
 
 static inline void obd_str2uuid(struct obd_uuid *uuid, const char *tmp)
 {
-	strncpy((char *)uuid->uuid, tmp, sizeof(*uuid));
-	uuid->uuid[sizeof(*uuid) - 1] = '\0';
+	strncpy(uuid->uuid, tmp, sizeof(uuid->uuid));
+	uuid->uuid[sizeof(uuid->uuid) - 1] = '\0';
 }
 
 /* For printf's only, make sure uuid is terminated */
-static inline char *obd_uuid2str(const struct obd_uuid *uuid)
+static inline const char *obd_uuid2str(const struct obd_uuid *uuid)
 {
 	if (uuid == NULL)
 		return NULL;
 
-	if (uuid->uuid[sizeof(*uuid) - 1] != '\0') {
+	if (strnlen(uuid->uuid, sizeof(uuid->uuid)) >= sizeof(uuid->uuid)) {
 		/* Obviously not safe, but for printfs, no real harm done...
 		 * we're always null-terminated, even in a ce.
 		 */
-		static char temp[sizeof(*uuid->uuid)];
+		static char temp[sizeof(uuid->uuid)];
 
-		memcpy(temp, uuid->uuid, sizeof(*uuid->uuid) - 1);
-		temp[sizeof(*uuid->uuid) - 1] = '\0';
-
+		strncpy(temp, uuid->uuid, sizeof(temp));
+		temp[sizeof(temp) - 1] = '\0';
 		return temp;
 	}
-	return (char *)(uuid->uuid);
+	return uuid->uuid;
 }
 
 #define LUSTRE_MAXFSNAME 8

lustre/obdclass/hash.c
@@ -1027,7 +1027,7 @@ static inline void cfs_hash_depth_wi_cancel(struct cfs_hash *hs) {}
 #endif /* CFS_HASH_DEBUG_LEVEL >= CFS_HASH_DEBUG_1 */
 
 struct cfs_hash *
-cfs_hash_create(char *name, unsigned cur_bits, unsigned max_bits,
+cfs_hash_create(const char *name, unsigned cur_bits, unsigned max_bits,
                 unsigned bkt_bits, unsigned extra_bytes,
                 unsigned min_theta, unsigned max_theta,
 		struct cfs_hash_ops *ops, unsigned flags)

lustre/utils/lfs.c
@@ -7437,7 +7437,7 @@ static inline int obd_statfs_ratio(const struct obd_statfs *st, bool inodes)
 }
 
 static int showdf(char *mntdir, struct obd_statfs *stat,
-		  char *uuid, enum mntdf_flags flags,
+		  const char *uuid, enum mntdf_flags flags,
 		  char *type, int index, int rc)
 {
 	long long avail, used, total;
@@ -9415,7 +9415,7 @@ static void diff2str(time_t seconds, char *buf, time_t now)
 	__sec2str(seconds - now, buf);
 }
 
-static void print_quota_val(char *val, int cols, bool print_over,
+static void print_quota_val(const char *val, int cols, bool print_over,
 			    struct quota_param *param)
 {
 	if ((param->qp_detail & (param->qp_detail - 1)) == 0 &&
@@ -9510,7 +9510,7 @@ static void kbytes2str(__u64 num, char *buf, int buflen, bool h)
 #endif
 
 #define STRBUF_LEN	24
-static void print_quota(char *mnt, struct if_quotactl *qctl, int type,
+static void print_quota(const char *mnt, struct if_quotactl *qctl, int type,
 			int rc, struct quota_param *param)
 {
 	time_t now;