lustre/lustre-release

commit 588850bbeb258bfaf4b0e9b002efa1d2ebf05790

LU-19123 utils: allow 'lfs project' on bad symlinks
Running 'lfs project' on a bad symlink reports an error:

 # ln -s foo /myth/tmp/projid-900/symlink
 # lfs project -p 1000 /myth/tmp/projid-900/symlink
 lfs: failed to stat '/myth/tmp/projid-900/symlink':
        No such file or directory
 # ln -s symlink3 /myth/tmp/projid-900/symlink3
 # lfs project -p 1000 /myth/tmp/projid-900/symlink3
 lfs: failed to stat '/myth/tmp/projid-900/symlink3':
        Too many levels of symbolic links

Opening the symlink with open(O_NOFOLLOW) avoids following the
symlink and allows the project ID to be set on the symlink itself.

Fixes: 501e5b2c8a ("LU-18027 lfs: lfs find handling special files")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Ibe0504c63270306a68329d8b165e7b83276695c8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59905
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs_project.c
@@ -103,7 +103,7 @@ new_api:
 	strncpy(bname_path, pathname, PATH_MAX);
 	dname = dirname(dname_path);
 	bname = basename(bname_path);
-	fd = open(dname, O_RDONLY | O_NOCTTY | O_NDELAY);
+	fd = open(dname, O_RDONLY | O_NOCTTY | O_NDELAY | O_NOFOLLOW);
 	if (fd < 0) {
 		ret = -errno;
 		goto out;

lustre/tests/sanity-quota.sh
@@ -5035,18 +5035,20 @@ test_64() {
 
 	touch $dir1/file
 	ln -s $dir1/file $dir1/file_link
+	ln -s bad $dir1/bad_link
+	ln -s self $dir1/self_link
 	mkfifo $dir1/fifo
 
 	$LFS project -srp $TSTPRJID $dir1 >&/dev/null ||
 		error "set project should succeed"
 
 	used=$(getquota -p $TSTPRJID global curinodes)
-	[ $used -eq 4 ] || error "expected 4 got $used"
+	(( $used == 6 )) || error "expected 4 got $used"
 	$LFS project -rC $dir1 >&/dev/null ||
 		error "clear project should succeed"
 
 	used=$(getquota -p $TSTPRJID global curinodes)
-	[ $used -eq 0 ] || error "expected 0 got $used"
+	(( $used == 0 )) || error "expected 0 got $used"
 }
 run_test 64 "lfs project on non dir/files should succeed"
 

commit 8917af46032d26d04061650ff4833b3e8c5b6559

LU-19123 utils: fix 'lfs project' on bad symlink v2
Fix lfs_project_iterate() to also use lstat() instead of stat() to
handle broken symlinks.  Change the error messages for these cases to
make it more clear if someone is running the old version of this code
and is reporting a problem.

Improve sanity-quota test_64() to check this case.

Fixes: 588850bbeb ("LU-19123 utils: allow 'lfs project' on bad symlinks")
Fixes: 501e5b2c8a ("LU-18027 lfs: lfs find handling special files")
Test-Parameters: trivial testlist=sanity-quota
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: Iddd9553d7e3090dbff825e17bbda8541ff9c4ae5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61309
Reviewed-by: Rahul Bansal <rahulmay94@gmail.com>
Reviewed-by: Li Dongyang <dongyangli@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs_project.c
@@ -73,7 +73,7 @@ static int project_get_fsxattr(const char *pathname, struct fsxattr *fsx,
 
 	ret = lstat(pathname, st);
 	if (ret) {
-		fprintf(stderr, "%s: failed to stat '%s': %s\n",
+		fprintf(stderr, "%s: failed to lstat '%s': %s\n",
 			progname, pathname, strerror(errno));
 		ret = -errno;
 		goto out;
@@ -333,9 +333,9 @@ static int lfs_project_iterate(const char *pathname,
 	int ret = 0;
 	int rc = 0;
 
-	ret = stat(pathname, &st);
+	ret = lstat(pathname, &st);
 	if (ret) {
-		fprintf(stderr, "%s: failed to stat '%s': %s\n",
+		fprintf(stderr, "%s: failed to lstat '%s': %s\n",
 			progname, pathname, strerror(errno));
 		return ret;
 	}

lustre/tests/sanity-quota.sh
@@ -5128,21 +5128,26 @@ test_64() {
 	touch $dir1/file
 	ln -s $dir1/file $dir1/file_link
 	ln -s bad $dir1/bad_link
-	ln -s self $dir1/self_link
+	ln -s self_link $dir1/self_link
 	mkfifo $dir1/fifo
 
-	$LFS project -srp $TSTPRJID $dir1 >&/dev/null ||
+	$LFS project -p $((TSTPRJID-1)) $dir1/bad_link ||
+		error "set project on bad link should succeed"
+	$LFS project -p $((TSTPRJID-1)) $dir1/self_link ||
+		error "set project on recursive link should succeed"
+
+	$LFS project -srp $TSTPRJID $dir1 ||
 		error "set project should succeed"
 
 	used=$(getquota -p $TSTPRJID global curinodes)
-	(( $used == 6 )) || error "expected 4 got $used"
-	$LFS project -rC $dir1 >&/dev/null ||
+	(( $used == 6 )) || error "expected 6 got $used"
+	$LFS project -rC $dir1 ||
 		error "clear project should succeed"
 
 	used=$(getquota -p $TSTPRJID global curinodes)
 	(( $used == 0 )) || error "expected 0 got $used"
 }
-run_test 64 "lfs project on non dir/files should succeed"
+run_test 64 "lfs project on non-dir/files should succeed"
 
 test_65() {
 	local SIZE=10 # MB