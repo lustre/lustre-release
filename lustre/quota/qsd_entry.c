lustre/lustre-release

commit f472b887e3dcc0e3fdfd735a47284c5669ce2ff4

LU-19068 quota: wait to release quota
In LDiskFS, if some large file is deleted, the blocks occupied
by this file could have not been deleted when the transaction is
stopped for the transaction has not been committed, then the quota
usage won't be refreshed in time in qsd_op_end0 and the quota is
not released, then the write on other OST could fail with -EDQUOT.

Test-Parameters: ostcount=2 ostsizegb=5 mdtfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs testlist=sanity-quota env=ONLY=96,ONLY_REPEAT=10
Fixes: ea0bdfc7cf ("LU-18925 libcfs: prefer kmap_local_page")
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: Ibde821e2b5083210c5994ec028c0ed08a7ab043f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59468
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/quota/qsd_handler.c
@@ -924,6 +924,13 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 		trans->lqt_id_cnt++;
 	}
 
+	if (qi->lqi_space < 0) {
+		if (found)
+			trans->lqt_ids[i].lqi_truncated_space += -qi->lqi_space;
+		else
+			trans->lqt_ids[i].lqi_truncated_space = -qi->lqi_space;
+	}
+
 	/* manage quota enforcement for this ID */
 	rc = qsd_op_begin0(env, qsd->qsd_type_array[qi->lqi_type],
 			   &trans->lqt_ids[i], qi->lqi_space, local_flags);
@@ -1104,6 +1111,27 @@ static void qsd_op_end0(const struct lu_env *env, struct qsd_qtype_info *qqi,
 			/* no suitable environment, handle adjustment in
 			 * separate thread context */
 			qsd_adjust_schedule(lqe, false, false);
+	}
+
+	/* Currently, ZFS will call qsd_op_end->qsd_op_end0 with env = NULL,
+	 * but ZFS will update the quota after commit, then it doesn't need
+	 * to wait to adjust the quota usage (LU-19068).
+	 */
+	if (!CFS_FAIL_CHECK(OBD_FAIL_QUOTA_USAGE_NOWAIT) &&
+	    env && qid->lqi_truncated_space > 1048576) {
+		__u32 seconds;
+
+		/* one second per gigabyte */
+		seconds = qid->lqi_truncated_space >> 20;
+		if (seconds > 120)
+			seconds = 120;
+
+		lqe_write_lock(lqe);
+		if (lqe->lqe_truncated_time < seconds + ktime_get_seconds())
+			lqe->lqe_truncated_time = seconds + ktime_get_seconds();
+		lqe_write_unlock(lqe);
+
+		qsd_adjust_schedule(lqe, true, false);
 	}
 	lqe_putref(lqe);
 	EXIT;

lustre/quota/qsd_writeback.c
@@ -402,8 +402,9 @@ out:
 
 void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 {
-	struct qsd_instance	*qsd = lqe2qqi(lqe)->qqi_qsd;
-	bool			 added = false;
+	struct qsd_instance *qsd = lqe2qqi(lqe)->qqi_qsd;
+	struct lquota_entry *tmp;
+	bool added = false;
 
 	read_lock(&qsd->qsd_lock);
 	if (qsd->qsd_stopping) {
@@ -426,18 +427,28 @@ void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 	if (list_empty(&lqe->lqe_link)) {
 		if (!cancel) {
 			lqe->lqe_adjust_time = ktime_get_seconds();
-			if (defer)
-				lqe->lqe_adjust_time += QSD_WB_INTERVAL;
+			if (defer) {
+				if (lqe->lqe_truncated_time)
+					lqe->lqe_adjust_time += 5;
+				else
+					lqe->lqe_adjust_time += QSD_WB_INTERVAL;
+			}
 		} else {
 			lqe->lqe_adjust_time = 0;
 		}
 
 		/* lqe reference transferred to list */
-		if (defer)
-			list_add_tail(&lqe->lqe_link,
-					  &qsd->qsd_adjust_list);
-		else
-			list_add(&lqe->lqe_link, &qsd->qsd_adjust_list);
+		list_for_each_entry(tmp, &qsd->qsd_adjust_list, lqe_link) {
+			if (tmp->lqe_adjust_time >= lqe->lqe_adjust_time) {
+				list_add(&lqe->lqe_link, &tmp->lqe_link);
+				added = true;
+				break;
+			}
+		}
+
+		if (!added)
+			list_add_tail(&lqe->lqe_link, &qsd->qsd_adjust_list);
+
 		added = true;
 	}
 	spin_unlock(&qsd->qsd_adjust_lock);
@@ -455,21 +466,26 @@ void qsd_adjust_schedule(struct lquota_entry *lqe, bool defer, bool cancel)
 /* return true if there is pending writeback records or the pending
  * adjust requests */
 static bool qsd_job_pending(struct qsd_instance *qsd, struct list_head *upd,
-			    bool *uptodate)
+			    bool *uptodate, int *wait)
 {
+	time64_t cur;
 	bool	job_pending = false;
 	int	qtype;
 
 	LASSERT(list_empty(upd));
 	*uptodate = true;
+	*wait = QSD_WB_INTERVAL;
 
 	spin_lock(&qsd->qsd_adjust_lock);
+	cur = ktime_get_seconds();
 	if (!list_empty(&qsd->qsd_adjust_list)) {
 		struct lquota_entry *lqe;
 		lqe = list_first_entry(&qsd->qsd_adjust_list,
 				       struct lquota_entry, lqe_link);
-		if (ktime_get_seconds() >= lqe->lqe_adjust_time)
+		if (cur >= lqe->lqe_adjust_time)
 			job_pending = true;
+		else
+			*wait = lqe->lqe_adjust_time - cur;
 	}
 	spin_unlock(&qsd->qsd_adjust_lock);
 
@@ -525,15 +541,16 @@ struct qsd_upd_args {
 
 static int qsd_upd_thread(void *_args)
 {
-	struct qsd_upd_args	*args = _args;
-	struct qsd_instance	*qsd = args->qua_inst;
+	struct qsd_upd_args *args = _args;
+	struct qsd_instance *qsd = args->qua_inst;
 	LIST_HEAD(queue);
-	struct qsd_upd_rec	*upd, *n;
-	struct lu_env		*env = &args->qua_env;
-	int			 qtype, rc = 0;
-	bool			 uptodate;
-	struct lquota_entry	*lqe;
+	struct qsd_upd_rec *upd, *n;
+	struct lu_env *env = &args->qua_env;
+	int qtype, rc = 0;
+	bool uptodate;
+	struct lquota_entry *lqe;
 	time64_t cur_time;
+	int wait_time;
 	ENTRY;
 
 	complete(args->qua_started);
@@ -541,8 +558,8 @@ static int qsd_upd_thread(void *_args)
 		 !kthread_should_stop(); })) {
 		int count = 0;
 
-		if (!qsd_job_pending(qsd, &queue, &uptodate))
-			schedule_timeout(cfs_time_seconds(QSD_WB_INTERVAL));
+		if (!qsd_job_pending(qsd, &queue, &uptodate, &wait_time))
+			schedule_timeout(cfs_time_seconds(wait_time));
 		__set_current_state(TASK_RUNNING);
 
 		while (1) {
@@ -593,8 +610,22 @@ static int qsd_upd_thread(void *_args)
 					qsd_adjust(env, lqe);
 			}
 
-			lqe_putref(lqe);
 			spin_lock(&qsd->qsd_adjust_lock);
+			if (!kthread_should_stop() && uptodate &&
+			    list_empty(&lqe->lqe_link) &&
+			    lqe->lqe_truncated_time > cur_time) {
+				if (lqe->lqe_truncated_time < cur_time + 5)
+					lqe->lqe_adjust_time =
+							lqe->lqe_truncated_time;
+				else
+					lqe->lqe_adjust_time = cur_time + 5;
+
+				list_add_tail(&lqe->lqe_link,
+					      &qsd->qsd_adjust_list);
+			} else {
+				lqe->lqe_truncated_time = 0;
+				lqe_putref(lqe);
+			}
 		}
 		spin_unlock(&qsd->qsd_adjust_lock);
 

lustre/tests/sanity-quota.sh
@@ -7227,6 +7227,87 @@ test_95b()
 }
 run_test 95b "df respects squashed project id"
 
+test_delete_big_file() {
+	local equot_expected=$1
+	local testdir=$DIR/$tdir
+	local tfile1=$DIR/$tdir/dd.out.0
+	local tfile2=$DIR/$tdir/dd.out.1
+
+	chown $TSTID:$TSTID $testdir || error "failed to chown $testdir"
+	$LFS project -sp $TSTPRJID $testdir ||
+		error "fail to set project on $testdir"
+
+	$LFS setquota -p $TSTPRJID -B 3G $DIR ||
+		error "failed to set quota limits for $TSTPRJID"
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	$LFS setstripe $tfile1 -i 0 -c 1 || error "setstripe $tfile1 failed"
+	chown $TSTID:$TSTID $tfile1 || error "fail to chown $tfile1"
+
+	$RUNAS fallocate -l2GiB $tfile1 || error "fail to write 2GiB"
+	$RUNAS $DD of=$tfile1 seek=2048 count=1500 oflag=sync &&
+		error "DD should fail on first write"
+
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	rm -f $tfile1
+	sleep 15
+
+	$LFS quota -p $TSTPRJID -h $DIR
+	$LFS setstripe $tfile2 -i 1 -c 1 || error "setstripe $tfile2 failed"
+	chown $TSTID:$TSTID $tfile2 || error "fail to chown $tfile2"
+
+	$RUNAS fallocate -l2GiB $tfile2 || error "fail to write 2GiB"
+
+	(( equot_expected == 1 )) && {
+		$RUNAS $DD of=$tfile2 seek=2048 count=512 oflag=sync &&
+			error "write should fail after deleting big files"
+	}
+	(( equot_expected == 0 )) && {
+		$RUNAS $DD of=$tfile2 seek=2048 count=512 oflag=sync ||
+			error "write should succeed after deleting big files"
+	}
+
+	$LFS quota -p $TSTPRJID -h $DIR
+
+	rm -f $tfile2
+	sleep 10
+	$LFS quota -p $TSTPRJID -h $DIR
+}
+
+test_96() {
+	local smallost
+
+	(( $MDS1_VERSION >= $(version_code 2_16_57) )) ||
+		skip "need OST >= 2.16.57 to run this test"
+
+	[ "$ost1_FSTYPE" == zfs ] &&
+		skip "the test in only needed to run on LDiskFS"
+
+	(( OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
+
+	is_project_quota_supported || skip "project is not supported"
+
+	smallost=($($LFS df -o $DIR | awk "/OST00/ { print \$4 }" | sort -n))
+
+	if (( smallost < 3657728 )); then
+		skip "OST is too small, skip the test"
+	fi
+
+	setup_quota_test || error "setup quota failed with $?"
+	set_ost_qtype $QTYPE || error "enable ost quota failed"
+
+	#define OBD_FAIL_QUOTA_USAGE_NOWAIT 0xA10
+	do_facet ost1 $LCTL set_param fail_loc=0xa10
+	do_facet ost2 $LCTL set_param fail_loc=0xa10
+	test_delete_big_file 1
+
+	do_facet ost1 $LCTL set_param fail_loc=0
+	do_facet ost2 $LCTL set_param fail_loc=0
+	test_delete_big_file 0
+}
+run_test 96 "quota grant should be released when big files are deleted"
+
 quota_fini()
 {
 	do_nodes $(comma_list $(nodes_list)) \

lustre/include/lustre_quota.h
@@ -235,6 +235,9 @@ struct lquota_id_info {
 	 */
 	long long		 lqi_space;
 
+	/* the space released by truncating big files */
+	long long		 lqi_truncated_space;
+
 	/* quota slave entry structure associated with this ID */
 	struct lquota_entry	*lqi_qentry;
 

lustre/include/obd_support.h
@@ -554,6 +554,7 @@ extern bool obd_enable_fname_encoding;
 #define OBD_FAIL_QUOTA_RECALC            0xA07
 #define OBD_FAIL_QUOTA_GRANT             0xA08
 #define OBD_FAIL_QUOTA_NOSYNC            0xA09
+#define OBD_FAIL_QUOTA_USAGE_NOWAIT      0xA10
 
 #define OBD_FAIL_LPROC_REMOVE            0xB00
 

lustre/osd-ldiskfs/osd_handler.c
@@ -3898,12 +3898,15 @@ static int osd_declare_destroy(const struct lu_env *env, struct dt_object *dt,
 	struct osd_object *obj = osd_dt_obj(dt);
 	struct inode *inode = obj->oo_inode;
 	struct osd_thandle *oh;
+	long long	    space;
 	int rc;
 
 	ENTRY;
 
 	if (inode == NULL)
 		RETURN(-ENOENT);
+
+	space = -toqb(LDISKFS_I(inode)->i_disksize);
 
 	oh = container_of(th, struct osd_thandle, ot_super);
 	LASSERT(oh->ot_handle == NULL);
@@ -3930,7 +3933,7 @@ static int osd_declare_destroy(const struct lu_env *env, struct dt_object *dt,
 		RETURN(rc);
 	/* data to be truncated */
 	rc = osd_declare_inode_qid(env, i_uid_read(inode), i_gid_read(inode),
-				   i_projid_read(inode), 0, oh, obj, NULL,
+				   i_projid_read(inode), space, oh, obj, NULL,
 				   OSD_QID_BLK);
 	if (rc)
 		RETURN(rc);

lustre/osd-ldiskfs/osd_io.c
@@ -2358,6 +2358,8 @@ static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
 	struct osd_thandle *oh;
 	struct osd_object  *obj = osd_dt_obj(dt);
 	struct inode	   *inode;
+	long long	    space = 0;
+	loff_t		    size;
 	int		    rc;
 	ENTRY;
 
@@ -2378,8 +2380,12 @@ static int osd_declare_punch(const struct lu_env *env, struct dt_object *dt,
 	inode = obj->oo_inode;
 	LASSERT(inode);
 
+	size = i_size_read(inode);
+	if (size > start)
+		space = -toqb((size > end ? end : size - start));
+
 	rc = osd_declare_inode_qid(env, i_uid_read(inode), i_gid_read(inode),
-				   i_projid_read(inode), 0, oh, obj,
+				   i_projid_read(inode), space, oh, obj,
 				   NULL, OSD_QID_BLK);
 
 	/* if object holds encrypted content, we need to make sure we truncate

lustre/quota/lquota_internal.h
@@ -177,6 +177,9 @@ struct lquota_entry {
 	struct mutex		 lqe_glbl_data_lock;
 	struct lqe_glbl_data	*lqe_glbl_data;
 	struct work_struct	 lqe_work; /* workitem to free lvbo */
+
+	/* the time when the blocks belonging to this QID should be truncated */
+	time64_t		 lqe_truncated_time;
 };
 
 #define lqe_qtype(lqe)		(lqe->lqe_site->lqs_qtype)

commit 4acd6efe670289cb5a82fb6f3580806d780bfa7f

LU-19068 quota: truncated time should be kept during race
During waiting to refresh the quota usage in "qsd_upd_thread"
after some big files are deleted, the final time to wait
(lqe_truncated_time) should be kept when the lqe is added to
the queue for defer adjust.

The patch adds a fix in the test_96 to wait the deletion to be
completed to ensure the quota usage to be refreshed in time.

The patch also adds some debug output about the delayed refresh.

Test-Parameters: ostcount=2 ostsizegb=5 mdtfilesystemtype=ldiskfs ostfilesystemtype=ldiskfs testlist=sanity-quota env=ONLY=96,ONLY_REPEAT=30
Fixes: f472b887e3 ("LU-19068 quota: wait to release quota")
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: Id810327d18b910844ea71e46224d1e6563dec03e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61162
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/quota/qsd_entry.c
@@ -159,7 +159,7 @@ static void qsd_lqe_debug(struct lquota_entry *lqe, void *arg,
 	struct qsd_qtype_info	*qqi = (struct qsd_qtype_info *)arg;
 
 	libcfs_debug_msg(msgdata,
-			 "%pV qsd:%s qtype:%s id:%llu enforced:%d granted: %llu pending:%llu waiting:%llu req:%d usage: %llu qunit:%llu qtune:%llu edquot:%d default:%s revoke:%d\n",
+			 "%pV qsd:%s qtype:%s id:%llu enforced:%d granted: %llu pending:%llu waiting:%llu req:%d usage: %llu qunit:%llu qtune:%llu edquot:%d default:%s revoke:%d truncated:%lld\n",
 			 vaf,
 			 qqi->qqi_qsd->qsd_svname, qtype_name(qqi->qqi_qtype),
 			 lqe->lqe_id.qid_uid, lqe->lqe_enforced,
@@ -167,7 +167,7 @@ static void qsd_lqe_debug(struct lquota_entry *lqe, void *arg,
 			 lqe->lqe_waiting_write, lqe->lqe_pending_req,
 			 lqe->lqe_usage, lqe->lqe_qunit, lqe->lqe_qtune,
 			 lqe->lqe_edquot, lqe->lqe_is_default ? "yes" : "no",
-			 lqe->lqe_revoke);
+			 lqe->lqe_revoke, lqe->lqe_truncated_time);
 }
 
 /*

lustre/quota/qsd_handler.c
@@ -731,6 +731,8 @@ static int qsd_op_begin0(const struct lu_env *env, struct qsd_qtype_info *qqi,
 		/* lqe will be released in qsd_op_end() */
 	}
 
+	LQUOTA_DEBUG(lqe, "op_begin space: %lld", space);
+
 	if (space <= 0) {
 		/* when space is negative or null, we don't need to consume
 		 * quota space. That said, we still want to perform space
@@ -742,8 +744,6 @@ static int qsd_op_begin0(const struct lu_env *env, struct qsd_qtype_info *qqi,
 		}
 		RETURN(0);
 	}
-
-	LQUOTA_DEBUG(lqe, "op_begin space:%lld", space);
 
 	lqe_write_lock(lqe);
 	lqe->lqe_waiting_write += space;

lustre/quota/qsd_writeback.c
@@ -603,6 +603,11 @@ static int qsd_upd_thread(void *_args)
 			spin_unlock(&qsd->qsd_adjust_lock);
 
 			if (!kthread_should_stop() && uptodate) {
+				if (lqe->lqe_truncated_time != 0)
+					LQUOTA_DEBUG(lqe, "update for big file deletion, uptodate %d, cur_time %llu, until %lld\n",
+						     uptodate, cur_time,
+						     lqe->lqe_truncated_time);
+
 				qsd_refresh_usage(env, lqe);
 				if (lqe->lqe_adjust_time == 0)
 					qsd_id_lock_cancel(env, lqe);
@@ -623,7 +628,9 @@ static int qsd_upd_thread(void *_args)
 				list_add_tail(&lqe->lqe_link,
 					      &qsd->qsd_adjust_list);
 			} else {
-				lqe->lqe_truncated_time = 0;
+				if (lqe->lqe_truncated_time <= cur_time)
+					lqe->lqe_truncated_time = 0;
+
 				lqe_putref(lqe);
 			}
 		}

lustre/tests/sanity-quota.sh
@@ -7350,6 +7350,8 @@ test_delete_big_file() {
 	$LFS quota -p $TSTPRJID -h $DIR
 
 	rm -f $tfile1
+	wait_delete_completed
+
 	sleep 15
 
 	$LFS quota -p $TSTPRJID -h $DIR