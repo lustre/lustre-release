lustre/lustre-release

commit 0c773016d837a4751678a0eeff00dee30e1d2586

LU-18977 efalnd: Introduce AWS EFA Lustre network driver
This commit introduces a new Lustre network driver (LND) for AWS Elastic
Fabric Adapter (EFA), enabling high-performance, low-latency networking
for Lustre clusters on AWS infrastructure.

EFA is a network device that provides reliable communication and kernel
bypass capabilities, targeting more consistent latency and higher
throughput than traditional TCP-based communication.

Key components added:
- EFA LND core implementation with support for RDMA operations
- Dual NID support:
  * Small NIDs: Uses TCP NI for out-of-band communication to discover
    remote GID and CM QP data, this utilizing LNET ping operations.
  * Large NIDs: Leverages 16 bytes of available address space to store
    connection data directly, eliminating the need for out-of-band
    discovery.
- DebugFS interface for monitoring and troubleshooting
- Module parameter configuration for tuning performance
- Integration with existing LNET infrastructure

The LND already has active customers and has been thoroughly tested
using selftest, IOR, FIO, GDS, and multiple sanity test variations.

Test-Parameters: trivial testlist=sanity-lnet serverversion=2.16
Signed-off-by: Timothy Day <timday@amazon.com>
Signed-off-by: Michael Margolin <mrgolin@amazon.com>
Signed-off-by: Yehuda Yitschak <yehuday@amazon.com>
Signed-off-by: Yonatan Nachum <ynachum@amazon.com>
Change-Id: I2217c0bbffefebb52152ea36feaaa2e5e78ad10c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59470
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Max Wang <wamax@amazon.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-lnet.m4
@@ -866,6 +866,108 @@ AS_IF([test $ENABLEO2IB != "no"], [
 ]) # LN_CONFIG_O2IB
 
 #
+# LN_CONFIG_EFALND
+#
+# check whether to use the EFA Network Interface lnd
+#
+AC_DEFUN([LN_CONFIG_EFALND], [
+AC_MSG_CHECKING([whether to enable EFA LND])
+AC_ARG_ENABLE([efa],
+	AC_HELP_STRING([--enable-efa=[yes|no|<path>]],
+		       [enable EFA LND]),
+	[], [enable_efa="no"])
+
+AS_CASE([$enable_efa],
+	[yes],
+	[
+		AS_IF([test x$uses_dpkg = xyes], [
+			LS_PKG="dpkg --listfiles"
+		], [
+			LS_PKG="rpm -ql"
+		])
+
+		EFA_PKG="efa"
+		EFA_HEADER="efa_verbs.h"
+		EFA_HEADER_PATH=$(eval "$LS_PKG $EFA_PKG" | egrep "$EFA_HEADER" | head -n1)
+		AS_IF([test -n "$EFA_HEADER_PATH"], [
+			EFA_INCLUDE_PATH=$(dirname $EFA_HEADER_PATH)
+		])
+
+		KERNEL_RDMA_INCLUDE="$LINUX/include/rdma"
+		AS_IF([test -z "$EFA_INCLUDE_PATH" && test -f "$KERNEL_RDMA_INCLUDE/$EFA_HEADER"], [
+			EFA_INCLUDE_PATH=$KERNEL_RDMA_INCLUDE
+		])
+
+		AS_IF([test -z "$EFA_INCLUDE_PATH"], [
+			AC_MSG_ERROR([Could not find a supported EFA driver. For custom built driver, the path to header files can be supplied using --enable-efa=<path>.])
+		])
+
+		ENABLE_EFA="yes"
+	],
+	[no],
+	[
+		ENABLE_EFA="no"
+	],
+	[
+		EFA_INCLUDE_PATH=$enable_efa
+		ENABLE_EFA="yes"
+	])
+
+AS_IF([test $ENABLE_EFA = "no"], [
+	AC_MSG_RESULT([no])
+], [
+	AC_MSG_RESULT([yes])
+
+	# Check mandatory kernel definitions
+
+	LB_CHECK_COMPILE([if 'ib_device_get_by_name' exists],
+	ib_device_get_by_name, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ib_device_get_by_name(NULL, 0);
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if 'RDMA_DRIVER_EFA' exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		int x = RDMA_DRIVER_EFA;
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if ib_device 'kverbs_provider' flag exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		struct ib_device dev = {
+			.kverbs_provider = 0,
+		};
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	# Check optional kernel definitions
+
+	LB_CHECK_COMPILE([if 'ibdev_to_node' exists],
+	ibdev_to_node, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ibdev_to_node(NULL);
+	],[
+		AC_DEFINE(HAVE_IBDEV_TO_NODE, 1,
+			[ibdev_to_node() is defined])
+	])
+
+	EFALND="efalnd"
+	AC_SUBST(EFALND)
+	AC_SUBST(EFA_INCLUDE_PATH)
+])
+]) # LN_CONFIG_EFALND
+
+#
 # LN_CONFIG_GNILND
 #
 # check whether to use the Gemini Network Interface lnd
@@ -1276,6 +1378,7 @@ AC_MSG_NOTICE([LNet kernel checks
 
 LN_CONFIG_BACKOFF
 LN_CONFIG_O2IB
+LN_CONFIG_EFALND
 LN_CONFIG_GNILND
 LN_CONFIG_KFILND
 ]) # LN_PROG_LINUX
@@ -1431,6 +1534,7 @@ LN_USR_NLMSGERR
 AC_DEFUN([LN_CONDITIONALS], [
 AM_CONDITIONAL(EXTERNAL_KO2IBLND,  test x$EXTERNAL_KO2IBLND = "xyes")
 AM_CONDITIONAL(BUILT_IN_KO2IBLND,  test x$BUILT_IN_KO2IBLND = "xyes")
+AM_CONDITIONAL(BUILD_EFALND,	   test x$EFALND = "xefalnd")
 AM_CONDITIONAL(BUILD_GNILND,       test x$GNILND  = "xgnilnd")
 AM_CONDITIONAL(BUILD_KFILND,       test x$KFILND  = "xkfilnd")
 ]) # LN_CONDITIONALS
@@ -1453,6 +1557,8 @@ lnet/klnds/o2iblnd/Makefile
 lnet/klnds/o2iblnd/autoMakefile
 lnet/klnds/in-kernel-o2iblnd/Makefile
 lnet/klnds/in-kernel-o2iblnd/autoMakefile
+lnet/klnds/efalnd/Makefile
+lnet/klnds/efalnd/autoMakefile
 lnet/klnds/gnilnd/Makefile
 lnet/klnds/gnilnd/autoMakefile
 lnet/klnds/socklnd/Makefile

MAINTAINERS
@@ -392,6 +392,12 @@ R:	Chris Horn <chris.horn@hpe.com>
 S:	Supported
 F:	lnet/klnds/socklnd/
 
+Lustre Networking KEFALND
+R:	Yonatan Nachum <ynachum@amazon.com>
+R:	Michael Margolin <mrgolin@amazon.com>
+S:	Supported
+F:	lnet/klnds/efalnd/
+
 Lustre Nodemap
 R:	Sebastien Buisson <sbuisson@whamcloud.com>
 S:	Supported

autoMakefile.am
@@ -380,6 +380,9 @@ debs: undef.h debs_common
 	if test "x@DPKG_INT_O2IB@" != "x"; then \
 		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} int_o2ib"; \
 	fi; \
+	if test "x@EFALND@" != "x"; then \
+		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} efalnd"; \
+	fi; \
 	if test "x@CONFIG_CACHE_FILE@" != "x"; then \
 		export CONFIG_CACHE_FILE="@CONFIG_CACHE_FILE@"; \
 	fi; \

debian/rules
@@ -257,6 +257,9 @@ configure-stamp: autogen-stamp debian/control.main debian/control.modules.in
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
 	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
+	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \
 	options="$${options} libcfs-cdebug libcfs-trace libcfs-assert"; \
@@ -588,6 +591,9 @@ kdist_config: prep-deb-files patch-stamp
 	fi; \
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
+	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
 	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \

lnet/include/lnet/lib-lnet.h
@@ -619,6 +619,7 @@ extern int live_router_check_interval;
 extern int dead_router_check_interval;
 extern int portal_rotor;
 extern int lock_prim_nid;
+extern int lnet_interfaces_max;
 
 void lnet_mt_event_handler(struct lnet_event *event);
 

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -114,6 +114,11 @@ struct lnet_ioctl_config_kfilnd_tunables {
 	__u32 lnd_timeout;
 };
 
+struct lnet_ioctl_config_efalnd_tunables {
+	__u32 lnd_version;
+	__u32 lnd_nqps;
+};
+
 struct lnet_ioctl_config_socklnd_tunables {
 	__u32 lnd_version;
 	__u16 lnd_conns_per_peer;
@@ -130,6 +135,7 @@ struct lnet_ioctl_config_gnilnd_tunables {
 struct lnet_lnd_tunables {
 	union {
 		struct lnet_ioctl_config_o2iblnd_tunables lnd_o2ib;
+		struct lnet_ioctl_config_efalnd_tunables lnd_efa;
 		struct lnet_ioctl_config_socklnd_tunables lnd_sock;
 		struct lnet_ioctl_config_kfilnd_tunables lnd_kfi;
 		struct lnet_ioctl_config_gnilnd_tunables lnd_gni;

lnet/include/uapi/linux/lnet/lnet-idl.h
@@ -185,6 +185,7 @@ struct lnet_magicversion {
 #define LNET_PROTO_KFI_MAGIC		0xdeadbeef
 #define LNET_PROTO_ACCEPTOR_MAGIC	0xacce7100
 #define LNET_PROTO_PING_MAGIC		0x70696E67 /* 'ping' */
+#define LNET_PROTO_EFA_MAGIC		0x2be092be /* 2B or 9 2B */
 
 /* Placeholder for a future "unified" protocol across all LNDs */
 /* Current LNDs that receive a request with this magic will respond

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -89,6 +89,8 @@ static inline lnet_nid_t LNET_MKNID(__u32 net, __u32 addr)
 
 #define LNET_NET_ANY LNET_NIDNET(LNET_NID_ANY)
 
+#define LNET_ADDR_ANY LNET_NIDADDR(LNET_NID_ANY)
+
 static inline bool nid_is_nid4(const struct lnet_nid *nid)
 {
 	return NID_ADDR_BYTES(nid) == 4;

commit 2d612fad488579313cb4d606ff9eb29c5b496498

LU-16240 efalnd: Use new AS_HELP_STRING
Starting with autoconf 2.70 AC_HELP_STRING has been replaced with
AS_HELP_STRING. Move to this new macro.

Test-Parameters: trivial
Fixes: 0c773016d83 ("LU-18977 efalnd: Introduce AWS EFA Lustre network driver")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I19b52c880f71c948c29fdeb6091e46bdfacee89f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61540
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

config/lustre-lnet.m4
@@ -873,7 +873,7 @@ AS_IF([test $ENABLEO2IB != "no"], [
 AC_DEFUN([LN_CONFIG_EFALND], [
 AC_MSG_CHECKING([whether to enable EFA LND])
 AC_ARG_ENABLE([efa],
-	AC_HELP_STRING([--enable-efa=[yes|no|<path>]],
+	AS_HELP_STRING([--enable-efa=[yes|no|<path>]],
 		       [enable EFA LND]),
 	[], [enable_efa="no"])
 