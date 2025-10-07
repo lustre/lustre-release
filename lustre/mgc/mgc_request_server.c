lustre/lustre-release

commit b625dac2346c8a2828ea26fb206825502ab3babb

LU-19095 mgc: notify MGS about LNet changes
Patch adds functionality to MGC to send notifications
to MGS about network changes on target, i.e. network/NIDs
mangement dynamically. So adding new network on online
target will cause changes in MGS nidtable for that target
and IR update for clients.

If network is deleted then network identifier is sent
as single NID in list identified by special token

Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ia572dfe643307f654fea3146107b3bf7b6ef744e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59868
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/mgc/mgc_request_server.c
@@ -302,6 +302,89 @@ static int mgc_target_register(struct obd_export *exp,
 	RETURN(rc);
 }
 
+static int mgc_nid_notify_interpret(const struct lu_env *env,
+				    struct ptlrpc_request *req,
+				    void *args, int rc)
+{
+	struct mgs_target_info *mti;
+
+	mti = req_capsule_server_get(&req->rq_pill, &RMF_MGS_TARGET_INFO);
+	if (!mti)
+		return -EPROTO;
+
+	server_mti_print("mgc_nid_notify: rep", mti);
+
+	if (rc)
+		CDEBUG(D_MGC, "%s: NID notify failed, rc = %d\n",
+		       mti->mti_svname, rc);
+	return rc;
+}
+
+static int mgc_nid_notify(struct obd_export *exp,
+			  struct mgs_target_info *mti)
+{
+	struct ptlrpc_request *req;
+	struct mgs_target_info *request_mti;
+	struct mgs_target_nidlist *mtn;
+	size_t bufsize, nidlist_size;
+	unsigned int avail;
+	int rc;
+
+	server_mti_print("mgc_nid_notify: req", mti);
+
+	if (!(exp_connect_flags(exp) & OBD_CONNECT_MGS_NIDLIST))
+		RETURN(-ENOPROTOOPT);
+
+	req = ptlrpc_request_alloc(class_exp2cliimp(exp),
+				   &RQF_MGS_TARGET_REG_NIDLIST);
+	if (!req)
+		RETURN(-ENOMEM);
+
+	bufsize = MGS_MAXREQSIZE - sizeof(struct ptlrpc_body) -
+		  sizeof(*mti) - sizeof(*mtn);
+	avail = bufsize / MTN_NIDSTR_SIZE;
+
+	if (mti->mti_nid_count > avail) {
+		/* inline buffer should fit NIDs on single network, but still */
+		CWARN("%s: too many NIDs for buffer: %u > %d\n",
+		      mti->mti_svname, mti->mti_nid_count, avail);
+		mti->mti_nid_count = avail;
+	}
+	nidlist_size = NIDLIST_SIZE(mti->mti_nid_count);
+	req_capsule_set_size(&req->rq_pill, &RMF_MGS_TARGET_NIDLIST,
+			     RCL_CLIENT, sizeof(*mtn) + nidlist_size);
+
+	rc = ptlrpc_request_pack(req, LUSTRE_MGS_VERSION, MGS_TARGET_REG);
+	if (rc < 0) {
+		ptlrpc_request_free(req);
+		RETURN(rc);
+	}
+
+	request_mti = req_capsule_client_get(&req->rq_pill,
+					     &RMF_MGS_TARGET_INFO);
+	if (!request_mti) {
+		ptlrpc_req_put(req);
+		RETURN(-ENOMEM);
+	}
+	*request_mti = *mti;
+
+	mtn = req_capsule_client_get(&req->rq_pill, &RMF_MGS_TARGET_NIDLIST);
+	if (!mtn) {
+		ptlrpc_req_put(req);
+		RETURN(-ENOMEM);
+	}
+	mtn->mtn_nids = mti->mti_nid_count;
+	mtn->mtn_flags = NIDLIST_APPEND;
+	memcpy(mtn->mtn_inline_list, mti->mti_nidlist, nidlist_size);
+
+	ptlrpc_request_set_replen(req);
+	req->rq_interpret_reply = mgc_nid_notify_interpret;
+
+	ptlrpcd_add_req(req);
+
+	return 0;
+}
+
 int mgc_set_info_async_server(const struct lu_env *env,
 			      struct obd_export *exp,
 			      u32 keylen, void *key,
@@ -325,6 +408,19 @@ int mgc_set_info_async_server(const struct lu_env *env,
 		CDEBUG(D_MGC, "register_target %s %#x\n",
 		       mti->mti_svname, mti->mti_flags);
 		rc =  mgc_target_register(exp, mti);
+		RETURN(rc);
+	}
+	if (KEY_IS(KEY_NID_NOTIFY)) {
+		size_t mti_len = offsetof(struct mgs_target_info, mti_nidlist);
+		struct mgs_target_info *mti = val;
+
+		mti_len += NIDLIST_SIZE(mti->mti_nid_count);
+		if (vallen != mti_len)
+			RETURN(-EINVAL);
+
+		CDEBUG(D_MGC, "NID notify for %s about %d new NIDs\n",
+		       mti->mti_svname, mti->mti_nid_count);
+		rc =  mgc_nid_notify(exp, mti);
 		RETURN(rc);
 	}
 	if (KEY_IS(KEY_SET_FS)) {

lnet/lnet/api-ni.c
@@ -4127,6 +4127,8 @@ lnet_dyn_del_net(u32 net_id)
 
 	lnet_net_unlock(0);
 
+	lnet_notify_net_update(net, true);
+
 	/* create and link a new ping info, before removing the old one */
 	rc = lnet_ping_target_setup(&pbuf, &ping_mdh,
 				    LNET_PING_INFO_HDR_SIZE +

lustre/include/obd.h
@@ -846,6 +846,7 @@ static inline bool obd_mdt_recovery_abort(struct obd_device *obd)
 #define KEY_MGSSEC              "mgssec"
 #define KEY_READ_ONLY           "read-only"
 #define KEY_REGISTER_TARGET     "register_target"
+#define KEY_NID_NOTIFY		"nid_notify"
 #define KEY_SET_FS              "set_fs"
 #define KEY_TGT_COUNT           "tgt_count"
 /*      KEY_SET_INFO in lustre_idl.h */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2758,6 +2758,7 @@ enum mgs_nidlist_flags {
 
 #define MTN_NIDSTR_SIZE LNET_NIDSTR_SIZE
 #define NIDLIST_SIZE(count) (MTN_NIDSTR_SIZE * count)
+#define NETDEL_TOKEN '#'
 
 struct mgs_target_nidlist {
 	__u32	mtn_flags; /* flags from mgs_nids_info_flags */

lustre/mgs/mgs_nids.c
@@ -512,7 +512,26 @@ static int mgs_build_nidlists(struct mgs_nidtbl_target *tgt,
 	unsigned long net;
 	int i, rc = 0;
 
-	/* Usually it is build on target on network basis, so assume that
+	/* Delete NET */
+	if (mti->mti_nidlist[0][0] == NETDEL_TOKEN) {
+		struct tnt_nidlist *tnl;
+
+		net = libcfs_str2net(mti->mti_nidlist[0] + 1);
+		if (net == LNET_NET_ANY) {
+			CDEBUG(D_MGS, "nidtbl: can't parse NET %s\n",
+			       mti->mti_nidlist[0] + 1);
+			return 0;
+		}
+
+		CDEBUG(D_MGS, "nidtbl: remove NET #%lu\n", net);
+		tnl = xa_erase(&tgt->mnt_xa_nids, LNET_NETNUM(net));
+		if (tnl)
+			OBD_FREE(tnl, tnl->tnl_size);
+
+		return 0;
+	}
+
+	/* Usually it is built on target on network basis, so assume that
 	 * and search forward to find a sequence of nids at the same net
 	 */
 	i = 0;

lustre/target/tgt_mount.c
@@ -1451,15 +1451,48 @@ struct tgt_notifier_work {
 static int tgt_nids_notify(struct lustre_sb_info *lsi,
 			   struct tgt_notifier_work *tnw)
 {
-	struct obd_device *mgc = lsi->lsi_mgc;
-	int rc = 0;
+	struct mgs_target_info *mti = &tnw->tnw_mti;
+	struct obd_device *mgc = lsi->lsi_mgc, *obd;
+	int mti_len = sizeof(*mti) + NIDLIST_SIZE(mti->mti_nid_count);
+	const char *tgt;
+	int rc;
 
-	ENTRY;
 	LASSERT(mgc);
 
-	/* async RPC to be implement in next patch */
+	rc = strscpy(mti->mti_svname, lsi->lsi_svname, sizeof(mti->mti_svname));
+	if (rc < 0)
+		return rc;
 
-	RETURN(rc);
+	rc = server_name2fsname(lsi->lsi_svname, mti->mti_fsname, &tgt);
+	if (rc < 0) {
+		CDEBUG(D_CONFIG, "%s: can't get fsname, rc = %d\n",
+		       lsi->lsi_svname, rc);
+		return rc;
+	}
+
+	rc = target_name2index(++tgt, &mti->mti_stripe_index, NULL);
+	if (rc < 0) {
+		CDEBUG(D_CONFIG, "%s: can't get target index, rc = %d\n",
+		       lsi->lsi_svname, rc);
+		return rc;
+	}
+
+	mti->mti_flags = lsi->lsi_flags & LDD_F_SV_TYPE_MASK;
+	mti->mti_flags |= LDD_F_OPC_READY | LDD_F_LARGE_NID;
+
+	obd = class_name2obd(lsi->lsi_svname);
+	if (!obd) {
+		CDEBUG(D_CONFIG, "%s: can't find OBD by server name\n",
+		       lsi->lsi_svname);
+		return -ENXIO;
+	}
+	mti->mti_instance = obd2obt(obd)->obt_instance;
+
+	rc = obd_set_info_async(NULL, mgc->u.cli.cl_mgc_mgsexp,
+				sizeof(KEY_NID_NOTIFY), KEY_NID_NOTIFY,
+				mti_len, mti, NULL);
+
+	return rc;
 }
 
 static void tgt_nid_notifier(struct work_struct *ws)
@@ -1485,23 +1518,31 @@ static int tgt_nid_update_cb(void *data, struct nid_update_info *nui)
 {
 	struct tgt_notifier_work *tnw;
 	struct lnet_nid *tmp;
-	int i;
+	unsigned int i = nui->nui_count ? : 1;
 
-	OBD_ALLOC(tnw, sizeof(*tnw) + NIDLIST_SIZE(nui->nui_count));
+	OBD_ALLOC(tnw, sizeof(*tnw) + NIDLIST_SIZE(i));
 	if (!tnw)
 		RETURN(-ENOMEM);
 
 	INIT_DELAYED_WORK(&tnw->tnw_work, tgt_nid_notifier);
 
-	tnw->tnw_mti.mti_nid_count = nui->nui_count;
+	if (nui->nui_count) {
+		tnw->tnw_mti.mti_nid_count = nui->nui_count;
 
-	CDEBUG(D_CONFIG,"new NID update from LNet\n");
-	for (i = 0; i < nui->nui_count; i++) {
-		tmp = genradix_ptr(&nui->nui_rdx, i);
-		libcfs_nidstr_r(tmp, tnw->tnw_mti.mti_nidlist[i],
-				sizeof(tnw->tnw_mti.mti_nidlist[i]));
-		CDEBUG(D_CONFIG,
-		       "NID #%d: %s\n", i, tnw->tnw_mti.mti_nidlist[i]);
+		CDEBUG(D_CONFIG, "new NID update from LNet\n");
+		for (i = 0; i < nui->nui_count; i++) {
+			tmp = genradix_ptr(&nui->nui_rdx, i);
+			libcfs_nidstr_r(tmp, tnw->tnw_mti.mti_nidlist[i],
+					sizeof(tnw->tnw_mti.mti_nidlist[i]));
+			CDEBUG(D_CONFIG, "NID #%d: %s\n", i,
+			       tnw->tnw_mti.mti_nidlist[i]);
+		}
+	} else {
+		/* to delete network send string identifier in form #<net> */
+		tnw->tnw_mti.mti_nid_count = 1;
+		tnw->tnw_mti.mti_nidlist[0][0] = NETDEL_TOKEN;
+		libcfs_net2str_r(nui->nui_net, tnw->tnw_mti.mti_nidlist[0] + 1,
+				 MTN_NIDSTR_SIZE - 1);
 	}
 
 	queue_delayed_work(tgt_nu_wq, &tnw->tnw_work, 0);

commit d8dc4bdc0746831e974beee136a7d3f8f89f4ee8

LU-19389 mgc: check reply in mgc_nid_notify_interpret()
The mgc_nid_notify_interpret() access repmgs without check
it exists or valid

Patch adds check for repmsg validity before accessing it.

Fixes: b625dac234 ("LU-19095 mgc: notify MGS about LNet changes")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I00c1bd1c0b4f299668142212f460718cd9ddba02
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61374
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mgc/mgc_request_server.c
@@ -308,6 +308,12 @@ static int mgc_nid_notify_interpret(const struct lu_env *env,
 {
 	struct mgs_target_info *mti;
 
+	if (!ptlrpc_client_replied(req) ||
+	    lustre_msg_get_type(req->rq_repmsg) == PTL_RPC_MSG_ERR) {
+		CDEBUG(D_MGC, "fail to send NID notify, rc = %d\n", rc);
+		return rc;
+	}
+
 	mti = req_capsule_server_get(&req->rq_pill, &RMF_MGS_TARGET_INFO);
 	if (!mti)
 		return -EPROTO;