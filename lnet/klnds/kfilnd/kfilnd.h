lustre/lustre-release

commit dbf4d41d9879f6dd1c141eeda27cd9e9b971b7d9

LU-18687 libcfs: move include to top-level
Move all headers to the new top-level include/linux/ directory.
This aligns the libcfs/, lnet/, and lustre/ directory structure
with the upstream kernel.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I178e0563894071ddc898fad88f3e63759e89c5c5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60989
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/kfilnd/kfilnd.h
@@ -51,7 +51,7 @@
 #define DEBUG_SUBSYSTEM S_LND
 
 #include <libcfs/libcfs.h>
-#include <libcfs/linux/linux-net.h>
+#include <lustre_compat/linux/linux-net.h>
 #include <lnet/lib-lnet.h>
 #include "kfi_endpoint.h"
 #include "kfi_errno.h"

commit a394bc723c95966a1249777544ea0e714d21a034

LU-18687 kfilnd: Fix libcfs include paths
libcfs headers were moved by https://review.whamcloud.com/60989 but
the new paths in kfilnd.h were wrong. Correct the paths.

Test-Parameters: trivial
Fixes: dbf4d41d98 ("LU-18687 libcfs: move include to top-level")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I28e6022ec3d35cc7ffc9c9cad394e8979d00c86e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61593
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

lnet/klnds/kfilnd/kfilnd.h
@@ -50,8 +50,8 @@
 
 #define DEBUG_SUBSYSTEM S_LND
 
-#include <libcfs/libcfs.h>
-#include <lustre_compat/linux/linux-net.h>
+#include <linux/libcfs/libcfs.h>
+#include <lustre_compat/net/linux-net.h>
 #include <lnet/lib-lnet.h>
 #include "kfi_endpoint.h"
 #include "kfi_errno.h"