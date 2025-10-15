lustre/lustre-release

commit 67e0d9e40acc6adcebf89e2a4ac3860f0c4273d2

LU-17191 osc: only call xa_insert for new entries
The handling of updating the Xarray entry is incorrect. If the
Xarray already exist and we call xa_insert() it will return -EBUSY
and not continue examining the entry. The correct approach is
if xa_load() returns a value never call xa_insert(). With the
return value of xa_load() we test if type bit was set and if not
set it.

We add ll_xa_insert to spelling.txt since the return value changed
with kernel versions. This will avoid mistakes in the future for
cases we look at the return value of xa_insert().

Test-Parameters: testlist=sanity-quota
Test-parameters: clientdistro=ubuntu2204 testlist=sanity-quota
Fixes: ac8c28f959 ("LU-8130 osc: convert osc_quota hash to xarray")
Change-Id: Icd88f5e47fb24f669bf362c1daec500b743b447b
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52713
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osc/osc_quota.c
@@ -119,17 +119,29 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 			/* This ID is getting close to its quota limit, let's
 			 * switch to sync I/O
 			 */
-			if (qtypes) {
+			if (qtypes) { /* ID already cached */
 				bits = xa_to_value(qtypes);
-				/* test if already set */
-				if (bits & BIT(type))
-					continue;
+				/* test if ID type already set */
+				if (!(bits & BIT(type))) {
+					bits |= BIT(type);
+					rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
+							     qid[type],
+							     xa_mk_value(bits),
+							     GFP_KERNEL));
+					if (rc < 0)
+						GOTO(out_unlock, rc);
+				}
+				continue;
 			}
 
+			/* Only insert if we see the this ID for the
+			 * very first time.
+			 */
 			bits |= BIT(type);
-			rc = xa_insert(&cli->cl_quota_exceeded_ids, qid[type],
-				       xa_mk_value(bits), GFP_KERNEL);
-			if (rc)
+			rc = ll_xa_insert(&cli->cl_quota_exceeded_ids,
+					  qid[type], xa_mk_value(bits),
+					  GFP_KERNEL);
+			if (rc == -ENOMEM)
 				break;
 
 			CDEBUG(D_QUOTA, "%s: setdq to insert for %s %d: rc = %d\n",
@@ -147,11 +159,12 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 
 			bits &= ~BIT(type);
 			if (bits) {
-				if (xa_cmpxchg(&cli->cl_quota_exceeded_ids,
-					       qid[type], qtypes,
-					       xa_mk_value(bits),
-					       GFP_KERNEL) != qtypes)
-					GOTO(out_unlock, rc = -ENOENT);
+				rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
+						     qid[type],
+						     xa_mk_value(bits),
+						     GFP_KERNEL));
+				if (rc < 0)
+					GOTO(out_unlock, rc);
 			} else {
 				xa_erase(&cli->cl_quota_exceeded_ids, qid[type]);
 			}

commit ae8506b4ea219c64aa4df9111b4d1d0b7df07503

LU-19206 osc: xarray handing cleanup
xarray don't needs xa_insert and xa_store both.
xa_store may update an entry or insert new, so lets simplify a code.

HPe-bug-id: LUS-12974
Test-Parameters: testlist=sanity-quota
Test-parameters: clientdistro=ubuntu2204 testlist=sanity-quota
Fixes: 67e0d9e40ac (LU-17191 osc: only call xa_insert for new entries)
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: I154f9684a0ce9f5b28584a52f485604c42146dc9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60374
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>

lustre/osc/osc_quota.c
@@ -78,6 +78,9 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 	if ((valid & (OBD_MD_FLALLQUOTA)) == 0)
 		RETURN(0);
 
+	if (cli->cl_quota_last_xid > xid && !(flags & OBD_FL_NO_QUOTA_ALL))
+		RETURN(0);
+
 	mutex_lock(&cli->cl_quota_mutex);
 	cli->cl_root_squash = !!(flags & OBD_FL_ROOT_SQUASH);
 	cli->cl_root_prjquota = !!(flags & OBD_FL_ROOT_PRJQUOTA);
@@ -92,7 +95,7 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 		cli->cl_quota_last_xid = xid;
 
 	for (type = 0; type < LL_MAXQUOTAS; type++) {
-		unsigned long bits = 0;
+		unsigned long bits, old;
 		u8 *qtypes;
 
 		if ((valid & md_quota_flag(type)) == 0)
@@ -100,62 +103,34 @@ int osc_quota_setdq(struct client_obd *cli, u64 xid, const unsigned int qid[],
 
 		/* lookup the quota IDs in the ID xarray */
 		qtypes = xa_load(&cli->cl_quota_exceeded_ids, qid[type]);
+		if (qtypes) /* ID already cached */
+			old = bits = xa_to_value(qtypes);
+		else
+			old = bits = 0;
+
 		if ((flags & fl_quota_flag(type)) != 0) {
 			/* This ID is getting close to its quota limit, let's
 			 * switch to sync I/O
 			 */
-			if (qtypes) { /* ID already cached */
-				bits = xa_to_value(qtypes);
-				/* test if ID type already set */
-				if (!(bits & BIT(type))) {
-					bits |= BIT(type);
-					rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
-							     qid[type],
-							     xa_mk_value(bits),
-							     GFP_KERNEL));
-					if (rc < 0)
-						GOTO(out_unlock, rc);
-				}
-				continue;
-			}
-
-			/* Only insert if we see the this ID for the
-			 * very first time.
-			 */
 			bits |= BIT(type);
-			rc = ll_xa_insert(&cli->cl_quota_exceeded_ids,
-					  qid[type], xa_mk_value(bits),
-					  GFP_KERNEL);
-			if (rc == -ENOMEM)
-				break;
-
-			CDEBUG(D_QUOTA, "%s: setdq to insert for %s %d: rc = %d\n",
-			       cli_name(cli), qtype_name(type), qid[type], rc);
+			CDEBUG(D_QUOTA, "%s: setdq to for %s %d\n",
+			       cli_name(cli), qtype_name(type), qid[type]);
 		} else {
-			/* This ID is now off the hook, let's remove it from
-			 * the xarray
-			 */
-			if (!qtypes)
-				continue;
-
-			bits = xa_to_value(qtypes);
-			if (!(bits & BIT(type)))
-				continue;
-
 			bits &= ~BIT(type);
+			CDEBUG(D_QUOTA, "%s: setdq to remove for %s %d\n",
+			       cli_name(cli), qtype_name(type), qid[type]);
+		}
+		if (old != bits) {
 			if (bits) {
 				rc = xa_err(xa_store(&cli->cl_quota_exceeded_ids,
-						     qid[type],
-						     xa_mk_value(bits),
-						     GFP_KERNEL));
-				if (rc < 0)
-					GOTO(out_unlock, rc);
+					     qid[type],
+					     xa_mk_value(bits),
+					     GFP_KERNEL));
 			} else {
 				xa_erase(&cli->cl_quota_exceeded_ids, qid[type]);
 			}
-
-			CDEBUG(D_QUOTA, "%s: setdq to remove for %s %d\n",
-			       cli_name(cli), qtype_name(type), qid[type]);
+			if (rc < 0)
+				break;
 		}
 	}
 