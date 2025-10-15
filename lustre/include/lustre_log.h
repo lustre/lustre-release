lustre/lustre-release

commit 08bba544189b17c1631fc1814818585ba76ad206

LU-19102 llog: enospc catalog fix
A race condition may occur between llog_cat_cleanup() and
llog_cat_new_log(), resulting in error -28:
llog_cat_new_log()) kjcf05-MDT0000-osd: there are no more free slots
 in catalog [0x20044e180:0x1:0x0]

Thread 1:
llog_cat_cleanup
    llog_cat_set_first_idx
->
    llog_cancel_rec
Thread 2:
llog_cat_add_rec
->  llog_cat_new_log

The catalog wrap logic relies on three variables: llh_cat_idx,
lgh_last_idx, and llh_count. In llog_cat_cleanup(), the current
implementation first updates llh_cat_idx and then decrements
llh_count. This sequence creates a potential error scenario when
llh_count is greater than 1 when the values llh_cat_idx and
lgh_last_idx are equal. Reversing the order of these operations
(decrementing llh_count before updating llh_cat_idx) resolves the
issue by ensuring consistency during llog_cat_new_log() access.

HPE-bug-id: LUS-12875
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: I4f6b94993a44925ee25d149c28ac0a071a995d63
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59730
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/obdclass/llog_cat.c
@@ -1238,10 +1238,15 @@ int llog_cat_cleanup(const struct lu_env *env, struct llog_handle *cathandle,
 	if (cathandle->lgh_obj == NULL)
 		return 0;
 
+	/* cancel record and decrease count, then move llh_cat_idx */
 	/* remove plain llog entry from catalog by index */
-	llog_cat_set_first_idx(cathandle, index);
 	rc = llog_cancel_rec(env, cathandle, index);
-	if (!rc && loghandle)
+	if (rc < 0)
+		return rc;
+
+	llog_cat_set_first_idx(cathandle, index);
+
+	if (loghandle)
 		CDEBUG(D_HA,
 		       "cancel plain log "DFID" at index %u of catalog "DFID"\n",
 		       PLOGID(&loghandle->lgh_id), index,

commit 18d1c323456a61ce1026e7231423774c2a895aca

LU-19102 llog: update llh_cat_idx on disk with llh_count
lh_cat_idx may be not written to disk, because order of operations
is changed by "LU-19102 llog: enospc catalog fix", and might be lost
after server restart.
Let's call llog_cat_set_first_idx() inside llog_cancel_rec(),
rigth after llh_count modification. And make an attempt to fix
llh_cat_idx during llog handle initialization.

Fixes: 08bba544189b ("LU-19102 llog: enospc catalog fix")
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: Ib636829edbad141dccad65327e0590456701eb73
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61586
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>

lustre/include/lustre_log.h
@@ -167,6 +167,7 @@ int llog_cat_retain_cb(const struct lu_env *env, struct llog_handle *cat,
 int llog_cat_modify_rec(const struct lu_env *env, struct llog_handle *cathandle,
 			struct llog_logid *lid, struct llog_rec_hdr *hdr,
 			struct llog_cookie *cookie);
+int llog_cat_set_first_idx(struct llog_handle *cathandle, int newidx);
 /* llog_obd.c */
 int llog_setup(const struct lu_env *env, struct obd_device *obd,
 	       struct obd_llog_group *olg, int index,

lustre/obdclass/llog.c
@@ -200,6 +200,7 @@ int llog_cancel_arr_rec(const struct lu_env *env, struct llog_handle *loghandle,
 	struct llog_log_hdr	*llh;
 	struct thandle		*th;
 	__u32			 tmp_lgc_index;
+	int			 old_cat = -1;
 	int			 rc, i = 0;
 	int rc1;
 	bool subtract_count = false;
@@ -261,6 +262,10 @@ int llog_cancel_arr_rec(const struct lu_env *env, struct llog_handle *loghandle,
 	subtract_count = true;
 	spin_unlock(&loghandle->lgh_hdr_lock);
 
+	if (llh->llh_flags & LLOG_F_IS_CAT) {
+		old_cat = llh->llh_cat_idx;
+		llog_cat_set_first_idx(loghandle, index[0]);
+	}
 	/* Since llog_process_thread use lgi_cookie, it`s better to save them
 	 * and restore after using
 	 */
@@ -307,6 +312,10 @@ int llog_cancel_arr_rec(const struct lu_env *env, struct llog_handle *loghandle,
 
 out_unlock:
 	if (rc < 0) {
+		if (old_cat != -1) {
+			llh->llh_cat_idx = old_cat;
+			old_cat = -1;
+		}
 		/* restore bitmap while holding a mutex */
 		spin_lock(&loghandle->lgh_hdr_lock);
 		if (subtract_count) {
@@ -323,6 +332,8 @@ out_trans:
 	if (rc == 0)
 		rc = rc1;
 	if (rc1 < 0) {
+		if (old_cat != -1)
+			llh->llh_cat_idx = old_cat;
 		spin_lock(&loghandle->lgh_hdr_lock);
 		if (subtract_count)
 			loghandle->lgh_hdr->llh_count += num;
@@ -444,6 +455,9 @@ int llog_init_handle(const struct lu_env *env, struct llog_handle *handle,
 		INIT_LIST_HEAD(&handle->u.chd.chd_head);
 		llh->llh_size = sizeof(struct llog_logid_rec);
 		llh->llh_flags |= LLOG_F_IS_FIXSIZE;
+		/* Fixing llh_cat_idx if it has problem */
+		if (rc == 0)
+			llog_cat_set_first_idx(handle, llh->llh_cat_idx);
 	} else if (!(flags & LLOG_F_IS_PLAIN)) {
 		CERROR("%s: unknown flags: %#x (expected %#x or %#x)\n",
 		       loghandle2name(handle), flags, LLOG_F_IS_CAT,

lustre/obdclass/llog_cat.c
@@ -1173,7 +1173,7 @@ int llog_cat_reverse_process(const struct lu_env *env,
 }
 EXPORT_SYMBOL(llog_cat_reverse_process);
 
-static int llog_cat_set_first_idx(struct llog_handle *cathandle, int newidx)
+int llog_cat_set_first_idx(struct llog_handle *cathandle, int newidx)
 {
 	struct llog_log_hdr *llh = cathandle->lgh_hdr;
 	int idx = llh->llh_cat_idx;
@@ -1212,6 +1212,7 @@ static int llog_cat_set_first_idx(struct llog_handle *cathandle, int newidx)
 
 	RETURN(0);
 }
+EXPORT_SYMBOL(llog_cat_set_first_idx);
 
 /* Cleanup deleted plain llog traces from catalog */
 int llog_cat_cleanup(const struct lu_env *env, struct llog_handle *cathandle,
@@ -1238,13 +1239,13 @@ int llog_cat_cleanup(const struct lu_env *env, struct llog_handle *cathandle,
 	if (cathandle->lgh_obj == NULL)
 		return 0;
 
-	/* cancel record and decrease count, then move llh_cat_idx */
+	/* cancel record and decrease count, then move llh_cat_idx
+	 * llog_cat_set_first_idx() is called inside llog_cancel_arr_rec()
+	 */
 	/* remove plain llog entry from catalog by index */
 	rc = llog_cancel_rec(env, cathandle, index);
 	if (rc < 0)
 		return rc;
-
-	llog_cat_set_first_idx(cathandle, index);
 
 	if (loghandle)
 		CDEBUG(D_HA,