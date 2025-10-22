lustre/lustre-release

commit b601eb35e97ae74d3de448e07a28ce41afb4adef

LU-11213 lmv: reuse object alloc QoS code from LOD
Reuse the same object alloc QoS code as LOD, but the QoS code is
not moved to lower layer module, instead it's copied to LMV, because
it involves almost all LMV code, which is too big a change and should
be done separately in the future.

And for LMV round-robin object allocation, because we only need to
allocate one object, use the MDT index saved and update it to next
MDT.

Add sanity 413b.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I53c3d863dafda534eebb6b95da205b395071cd25
Reviewed-on: https://review.whamcloud.com/34657
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -60,9 +60,8 @@
 
 static int lmv_check_connect(struct obd_device *obd);
 
-static void lmv_activate_target(struct lmv_obd *lmv,
-                                struct lmv_tgt_desc *tgt,
-                                int activate)
+void lmv_activate_target(struct lmv_obd *lmv, struct lmv_tgt_desc *tgt,
+			 int activate)
 {
         if (tgt->ltd_active == activate)
                 return;
@@ -294,21 +293,21 @@ int lmv_connect_mdc(struct obd_device *obd, struct lmv_tgt_desc *tgt)
         int                      rc;
         ENTRY;
 
-        mdc_obd = class_find_client_obd(&tgt->ltd_uuid, LUSTRE_MDC_NAME,
-                                        &obd->obd_uuid);
-        if (!mdc_obd) {
-                CERROR("target %s not attached\n", tgt->ltd_uuid.uuid);
-                RETURN(-EINVAL);
-        }
+	mdc_obd = class_find_client_obd(&tgt->ltd_uuid, LUSTRE_MDC_NAME,
+					&obd->obd_uuid);
+	if (!mdc_obd) {
+		CERROR("target %s not attached\n", tgt->ltd_uuid.uuid);
+		RETURN(-EINVAL);
+	}
 
 	CDEBUG(D_CONFIG, "connect to %s(%s) - %s, %s\n",
 	       mdc_obd->obd_name, mdc_obd->obd_uuid.uuid,
 	       tgt->ltd_uuid.uuid, obd->obd_uuid.uuid);
 
-        if (!mdc_obd->obd_set_up) {
-                CERROR("target %s is not set up\n", tgt->ltd_uuid.uuid);
-                RETURN(-EINVAL);
-        }
+	if (!mdc_obd->obd_set_up) {
+		CERROR("target %s is not set up\n", tgt->ltd_uuid.uuid);
+		RETURN(-EINVAL);
+	}
 
 	rc = obd_connect(NULL, &mdc_exp, mdc_obd, &obd->obd_uuid,
 			 &lmv->conn_data, lmv->lmv_cache);
@@ -324,19 +323,19 @@ int lmv_connect_mdc(struct obd_device *obd, struct lmv_tgt_desc *tgt)
 	if (rc)
 		RETURN(rc);
 
-        target.ft_srv = NULL;
-        target.ft_exp = mdc_exp;
-        target.ft_idx = tgt->ltd_idx;
+	target.ft_srv = NULL;
+	target.ft_exp = mdc_exp;
+	target.ft_idx = tgt->ltd_index;
 
-        fld_client_add_target(&lmv->lmv_fld, &target);
+	fld_client_add_target(&lmv->lmv_fld, &target);
 
-        rc = obd_register_observer(mdc_obd, obd);
-        if (rc) {
-                obd_disconnect(mdc_exp);
-                CERROR("target %s register_observer error %d\n",
-                       tgt->ltd_uuid.uuid, rc);
-                RETURN(rc);
-        }
+	rc = obd_register_observer(mdc_obd, obd);
+	if (rc) {
+		obd_disconnect(mdc_exp);
+		CERROR("target %s register_observer error %d\n",
+		       tgt->ltd_uuid.uuid, rc);
+		RETURN(rc);
+	}
 
 	if (obd->obd_observer) {
 		/*
@@ -355,6 +354,12 @@ int lmv_connect_mdc(struct obd_device *obd, struct lmv_tgt_desc *tgt)
 	lmv->desc.ld_active_tgt_count++;
 
 	md_init_ea_size(tgt->ltd_exp, lmv->max_easize, lmv->max_def_easize);
+
+	rc = lqos_add_tgt(&lmv->lmv_qos, tgt);
+	if (rc) {
+		obd_disconnect(mdc_exp);
+		RETURN(rc);
+	}
 
 	CDEBUG(D_CONFIG, "Connected to %s(%s) successfully (%d)\n",
 		mdc_obd->obd_name, mdc_obd->obd_uuid.uuid,
@@ -375,6 +380,8 @@ static void lmv_del_target(struct lmv_obd *lmv, int index)
 	if (lmv->tgts[index] == NULL)
 		return;
 
+	lqos_del_tgt(&lmv->lmv_qos, lmv->tgts[index]);
+
 	OBD_FREE_PTR(lmv->tgts[index]);
 	lmv->tgts[index] = NULL;
 	return;
@@ -384,11 +391,12 @@ static int lmv_add_target(struct obd_device *obd, struct obd_uuid *uuidp,
 			   __u32 index, int gen)
 {
 	struct obd_device *mdc_obd;
-        struct lmv_obd      *lmv = &obd->u.lmv;
-        struct lmv_tgt_desc *tgt;
-	int		     orig_tgt_count = 0;
-        int                  rc = 0;
-        ENTRY;
+	struct lmv_obd *lmv = &obd->u.lmv;
+	struct lmv_tgt_desc *tgt;
+	int orig_tgt_count = 0;
+	int rc = 0;
+
+	ENTRY;
 
 	CDEBUG(D_CONFIG, "Target uuid: %s. index %d\n", uuidp->uuid, index);
 	mdc_obd = class_find_client_obd(uuidp, LUSTRE_MDC_NAME,
@@ -447,7 +455,7 @@ static int lmv_add_target(struct obd_device *obd, struct obd_uuid *uuidp,
 	}
 
 	mutex_init(&tgt->ltd_fid_mutex);
-	tgt->ltd_idx = index;
+	tgt->ltd_index = index;
 	tgt->ltd_uuid = *uuidp;
 	tgt->ltd_active = 0;
 	lmv->tgts[index] = tgt;
@@ -1111,7 +1119,7 @@ hsm_req_err:
 			RETURN(-EINVAL);
 
 		/* only files on same MDT can have their layouts swapped */
-		if (tgt1->ltd_idx != tgt2->ltd_idx)
+		if (tgt1->ltd_index != tgt2->ltd_index)
 			RETURN(-EPERM);
 
 		rc = obd_iocontrol(cmd, tgt1->ltd_exp, len, karg, uarg);
@@ -1264,9 +1272,12 @@ int lmv_fid_alloc(const struct lu_env *env, struct obd_export *exp,
 
 static int lmv_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 {
-	struct lmv_obd	*lmv = &obd->u.lmv;
+	struct lmv_obd *lmv = &obd->u.lmv;
 	struct lmv_desc	*desc;
-	int		rc;
+	struct lnet_process_id lnet_id;
+	int i = 0;
+	int rc;
+
 	ENTRY;
 
         if (LUSTRE_CFG_BUFLEN(lcfg, 1) < 1) {
@@ -1289,12 +1300,34 @@ static int lmv_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 	obd_str2uuid(&lmv->desc.ld_uuid, desc->ld_uuid.uuid);
 	lmv->desc.ld_tgt_count = 0;
 	lmv->desc.ld_active_tgt_count = 0;
-	lmv->desc.ld_qos_maxage = 60;
+	lmv->desc.ld_qos_maxage = LMV_DESC_QOS_MAXAGE_DEFAULT;
 	lmv->max_def_easize = 0;
 	lmv->max_easize = 0;
 
 	spin_lock_init(&lmv->lmv_lock);
 	mutex_init(&lmv->lmv_init_mutex);
+
+	/* Set up allocation policy (QoS and RR) */
+	INIT_LIST_HEAD(&lmv->lmv_qos.lq_svr_list);
+	init_rwsem(&lmv->lmv_qos.lq_rw_sem);
+	lmv->lmv_qos.lq_dirty = 1;
+	lmv->lmv_qos.lq_rr.lqr_dirty = 1;
+	lmv->lmv_qos.lq_reset = 1;
+	/* Default priority is toward free space balance */
+	lmv->lmv_qos.lq_prio_free = 232;
+	/* Default threshold for rr (roughly 17%) */
+	lmv->lmv_qos.lq_threshold_rr = 43;
+
+	/*
+	 * initialize rr_index to lower 32bit of netid, so that client
+	 * can distribute subdirs evenly from the beginning.
+	 */
+	while (LNetGetId(i++, &lnet_id) != -ENOENT) {
+		if (LNET_NETTYP(LNET_NIDNET(lnet_id.nid)) != LOLND) {
+			lmv->lmv_qos_rr_index = (u32)lnet_id.nid;
+			break;
+		}
+	}
 
 	rc = lmv_tunables_init(obd);
 	if (rc)
@@ -1472,6 +1505,7 @@ static int lmv_statfs_update(void *cookie, int rc)
 		tgt->ltd_statfs = *osfs;
 		tgt->ltd_statfs_age = ktime_get_seconds();
 		spin_unlock(&lmv->lmv_lock);
+		lmv->lmv_qos.lq_dirty = 1;
 	}
 
 	return rc;
@@ -1563,7 +1597,7 @@ static int lmv_getattr(struct obd_export *exp, struct md_op_data *op_data,
                 RETURN(PTR_ERR(tgt));
 
 	if (op_data->op_flags & MF_GET_MDT_IDX) {
-		op_data->op_mds = tgt->ltd_idx;
+		op_data->op_mds = tgt->ltd_index;
 		RETURN(0);
 	}
 
@@ -1613,17 +1647,6 @@ static int lmv_close(struct obd_export *exp, struct md_op_data *op_data,
         RETURN(rc);
 }
 
-static struct lmv_tgt_desc *lmv_locate_tgt_qos(struct lmv_obd *lmv, __u32 *mdt)
-{
-	static unsigned int rr_index;
-
-	/* locate MDT round-robin is the first step */
-	*mdt = rr_index % lmv->tgts_size;
-	rr_index++;
-
-	return lmv->tgts[*mdt];
-}
-
 static struct lmv_tgt_desc *
 lmv_locate_tgt_by_name(struct lmv_obd *lmv, struct lmv_stripe_md *lsm,
 		       const char *name, int namelen, struct lu_fid *fid,
@@ -1637,7 +1660,7 @@ lmv_locate_tgt_by_name(struct lmv_obd *lmv, struct lmv_stripe_md *lsm,
 		if (IS_ERR(tgt))
 			return tgt;
 
-		*mds = tgt->ltd_idx;
+		*mds = tgt->ltd_index;
 		return tgt;
 	}
 
@@ -1724,12 +1747,18 @@ lmv_locate_tgt(struct lmv_obd *lmv, struct md_op_data *op_data)
 		   lmv_dir_space_hashed(op_data->op_default_mea1) &&
 		   !lmv_dir_striped(lsm)) {
 		tgt = lmv_locate_tgt_qos(lmv, &op_data->op_mds);
+		if (tgt == ERR_PTR(-EAGAIN))
+			tgt = lmv_locate_tgt_rr(lmv, &op_data->op_mds);
 		/*
 		 * only update statfs when mkdir under dir with "space" hash,
 		 * this means the cached statfs may be stale, and current mkdir
 		 * may not follow QoS accurately, but it's not serious, and it
 		 * avoids periodic statfs when client doesn't mkdir under
 		 * "space" hashed directories.
+		 *
+		 * TODO: after MDT support QoS object allocation, also update
+		 * statfs for 'lfs mkdir -i -1 ...", currently it's done in user
+		 * space.
 		 */
 		if (!IS_ERR(tgt)) {
 			struct obd_device *obd;
@@ -1849,7 +1878,7 @@ int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 		if (IS_ERR(tgt))
 			RETURN(PTR_ERR(tgt));
 
-		op_data->op_mds = tgt->ltd_idx;
+		op_data->op_mds = tgt->ltd_index;
 	}
 
 	CDEBUG(D_INODE, "CREATE obj "DFID" -> mds #%x\n",
@@ -1884,7 +1913,7 @@ lmv_enqueue(struct obd_export *exp, struct ldlm_enqueue_info *einfo,
 		RETURN(PTR_ERR(tgt));
 
 	CDEBUG(D_INODE, "ENQUEUE on "DFID" -> mds #%u\n",
-	       PFID(&op_data->op_fid1), tgt->ltd_idx);
+	       PFID(&op_data->op_fid1), tgt->ltd_index);
 
 	rc = md_enqueue(tgt->ltd_exp, einfo, policy, op_data, lockh,
 			extra_lock_flags);
@@ -1911,7 +1940,7 @@ retry:
 
 	CDEBUG(D_INODE, "GETATTR_NAME for %*s on "DFID" -> mds #%d\n",
 		(int)op_data->op_namelen, op_data->op_name,
-		PFID(&op_data->op_fid1), tgt->ltd_idx);
+		PFID(&op_data->op_fid1), tgt->ltd_index);
 
 	rc = md_getattr_name(tgt->ltd_exp, op_data, preq);
 	if (rc == -ENOENT && lmv_dir_retry_check_update(op_data)) {
@@ -1967,7 +1996,7 @@ static int lmv_early_cancel(struct obd_export *exp, struct lmv_tgt_desc *tgt,
 			RETURN(PTR_ERR(tgt));
 	}
 
-	if (tgt->ltd_idx != op_tgt) {
+	if (tgt->ltd_index != op_tgt) {
 		CDEBUG(D_INODE, "EARLY_CANCEL on "DFID"\n", PFID(fid));
 		policy.l_inodebits.bits = bits;
 		rc = md_cancel_unused(tgt->ltd_exp, fid, &policy,
@@ -2014,7 +2043,7 @@ static int lmv_link(struct obd_export *exp, struct md_op_data *op_data,
 	 * Cancel UPDATE lock on child (fid1).
 	 */
 	op_data->op_flags |= MF_MDC_CANCEL_FID2;
-	rc = lmv_early_cancel(exp, NULL, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, NULL, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_UPDATE, MF_MDC_CANCEL_FID1);
 	if (rc != 0)
 		RETURN(rc);
@@ -2112,7 +2141,7 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 		RETURN(PTR_ERR(child_tgt));
 
 	if (!S_ISDIR(op_data->op_mode) && tp_tgt)
-		rc = __lmv_fid_alloc(lmv, &target_fid, tp_tgt->ltd_idx);
+		rc = __lmv_fid_alloc(lmv, &target_fid, tp_tgt->ltd_index);
 	else
 		rc = lmv_fid_alloc(NULL, exp, &target_fid, op_data);
 	if (rc)
@@ -2138,7 +2167,7 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 	}
 
 	/* cancel UPDATE lock of parent master object */
-	rc = lmv_early_cancel(exp, parent_tgt, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, parent_tgt, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_UPDATE, MF_MDC_CANCEL_FID1);
 	if (rc)
 		RETURN(rc);
@@ -2163,14 +2192,14 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 	op_data->op_fid4 = target_fid;
 
 	/* cancel UPDATE locks of target parent */
-	rc = lmv_early_cancel(exp, tp_tgt, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, tp_tgt, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_UPDATE, MF_MDC_CANCEL_FID2);
 	if (rc)
 		RETURN(rc);
 
 	/* cancel LOOKUP lock of source if source is remote object */
 	if (child_tgt != sp_tgt) {
-		rc = lmv_early_cancel(exp, sp_tgt, op_data, tgt->ltd_idx,
+		rc = lmv_early_cancel(exp, sp_tgt, op_data, tgt->ltd_index,
 				      LCK_EX, MDS_INODELOCK_LOOKUP,
 				      MF_MDC_CANCEL_FID3);
 		if (rc)
@@ -2178,7 +2207,7 @@ static int lmv_migrate(struct obd_export *exp, struct md_op_data *op_data,
 	}
 
 	/* cancel ELC locks of source */
-	rc = lmv_early_cancel(exp, child_tgt, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, child_tgt, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_ELC, MF_MDC_CANCEL_FID3);
 	if (rc)
 		RETURN(rc);
@@ -2238,7 +2267,7 @@ static int lmv_rename(struct obd_export *exp, struct md_op_data *op_data,
 	op_data->op_flags |= MF_MDC_CANCEL_FID4;
 
 	/* cancel UPDATE locks of target parent */
-	rc = lmv_early_cancel(exp, tp_tgt, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, tp_tgt, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_UPDATE, MF_MDC_CANCEL_FID2);
 	if (rc != 0)
 		RETURN(rc);
@@ -2247,7 +2276,7 @@ static int lmv_rename(struct obd_export *exp, struct md_op_data *op_data,
 		/* cancel LOOKUP lock of target on target parent */
 		if (tgt != tp_tgt) {
 			rc = lmv_early_cancel(exp, tp_tgt, op_data,
-					      tgt->ltd_idx, LCK_EX,
+					      tgt->ltd_index, LCK_EX,
 					      MDS_INODELOCK_LOOKUP,
 					      MF_MDC_CANCEL_FID4);
 			if (rc != 0)
@@ -2261,7 +2290,7 @@ static int lmv_rename(struct obd_export *exp, struct md_op_data *op_data,
 			RETURN(PTR_ERR(src_tgt));
 
 		/* cancel ELC locks of source */
-		rc = lmv_early_cancel(exp, src_tgt, op_data, tgt->ltd_idx,
+		rc = lmv_early_cancel(exp, src_tgt, op_data, tgt->ltd_index,
 				      LCK_EX, MDS_INODELOCK_ELC,
 				      MF_MDC_CANCEL_FID3);
 		if (rc != 0)
@@ -2276,7 +2305,7 @@ retry:
 		RETURN(PTR_ERR(sp_tgt));
 
 	/* cancel UPDATE locks of source parent */
-	rc = lmv_early_cancel(exp, sp_tgt, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, sp_tgt, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_UPDATE, MF_MDC_CANCEL_FID1);
 	if (rc != 0)
 		RETURN(rc);
@@ -2285,7 +2314,7 @@ retry:
 		/* cancel LOOKUP lock of source on source parent */
 		if (src_tgt != sp_tgt) {
 			rc = lmv_early_cancel(exp, sp_tgt, op_data,
-					      tgt->ltd_idx, LCK_EX,
+					      tgt->ltd_index, LCK_EX,
 					      MDS_INODELOCK_LOOKUP,
 					      MF_MDC_CANCEL_FID3);
 			if (rc != 0)
@@ -2330,7 +2359,7 @@ rename:
 		/* cancel LOOKUP lock of target on target parent */
 		if (tgt != tp_tgt) {
 			rc = lmv_early_cancel(exp, tp_tgt, op_data,
-					      tgt->ltd_idx, LCK_EX,
+					      tgt->ltd_index, LCK_EX,
 					      MDS_INODELOCK_LOOKUP,
 					      MF_MDC_CANCEL_FID4);
 			if (rc != 0)
@@ -2828,17 +2857,18 @@ retry:
 	op_data->op_flags |= MF_MDC_CANCEL_FID1 | MF_MDC_CANCEL_FID3;
 
 	if (parent_tgt != tgt)
-		rc = lmv_early_cancel(exp, parent_tgt, op_data, tgt->ltd_idx,
+		rc = lmv_early_cancel(exp, parent_tgt, op_data, tgt->ltd_index,
 				      LCK_EX, MDS_INODELOCK_LOOKUP,
 				      MF_MDC_CANCEL_FID3);
 
-	rc = lmv_early_cancel(exp, NULL, op_data, tgt->ltd_idx, LCK_EX,
+	rc = lmv_early_cancel(exp, NULL, op_data, tgt->ltd_index, LCK_EX,
 			      MDS_INODELOCK_ELC, MF_MDC_CANCEL_FID3);
 	if (rc)
 		RETURN(rc);
 
 	CDEBUG(D_INODE, "unlink with fid="DFID"/"DFID" -> mds #%u\n",
-	       PFID(&op_data->op_fid1), PFID(&op_data->op_fid2), tgt->ltd_idx);
+	       PFID(&op_data->op_fid1), PFID(&op_data->op_fid2),
+	       tgt->ltd_index);
 
 	rc = md_unlink(tgt->ltd_exp, op_data, request);
 	if (rc == -ENOENT && lmv_dir_retry_check_update(op_data)) {

lustre/lmv/lproc_lmv.c
@@ -75,6 +75,109 @@ static ssize_t desc_uuid_show(struct kobject *kobj, struct attribute *attr,
 }
 LUSTRE_RO_ATTR(desc_uuid);
 
+static ssize_t qos_maxage_show(struct kobject *kobj,
+			       struct attribute *attr,
+			       char *buf)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+
+	return sprintf(buf, "%u\n", dev->u.lmv.desc.ld_qos_maxage);
+}
+
+static ssize_t qos_maxage_store(struct kobject *kobj,
+				struct attribute *attr,
+				const char *buffer,
+				size_t count)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	dev->u.lmv.desc.ld_qos_maxage = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(qos_maxage);
+
+static ssize_t qos_prio_free_show(struct kobject *kobj,
+				  struct attribute *attr,
+				  char *buf)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+
+	return sprintf(buf, "%u%%\n",
+		       (dev->u.lmv.lmv_qos.lq_prio_free * 100 + 255) >> 8);
+}
+
+static ssize_t qos_prio_free_store(struct kobject *kobj,
+				   struct attribute *attr,
+				   const char *buffer,
+				   size_t count)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct lmv_obd *lmv = &dev->u.lmv;
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val > 100)
+		return -EINVAL;
+
+	lmv->lmv_qos.lq_prio_free = (val << 8) / 100;
+	lmv->lmv_qos.lq_dirty = 1;
+	lmv->lmv_qos.lq_reset = 1;
+
+	return count;
+}
+LUSTRE_RW_ATTR(qos_prio_free);
+
+static ssize_t qos_threshold_rr_show(struct kobject *kobj,
+				     struct attribute *attr,
+				     char *buf)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+
+	return sprintf(buf, "%u%%\n",
+		       (dev->u.lmv.lmv_qos.lq_threshold_rr * 100 + 255) >> 8);
+}
+
+static ssize_t qos_threshold_rr_store(struct kobject *kobj,
+				      struct attribute *attr,
+				      const char *buffer,
+				      size_t count)
+{
+	struct obd_device *dev = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct lmv_obd *lmv = &dev->u.lmv;
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val > 100)
+		return -EINVAL;
+
+	lmv->lmv_qos.lq_threshold_rr = (val << 8) / 100;
+	lmv->lmv_qos.lq_dirty = 1;
+
+	return count;
+}
+LUSTRE_RW_ATTR(qos_threshold_rr);
+
 #ifdef CONFIG_PROC_FS
 static void *lmv_tgt_seq_start(struct seq_file *p, loff_t *pos)
 {
@@ -117,7 +220,7 @@ static int lmv_tgt_seq_show(struct seq_file *p, void *v)
 		return 0;
 
 	seq_printf(p, "%u: %s %sACTIVE\n",
-		   tgt->ltd_idx, tgt->ltd_uuid.uuid,
+		   tgt->ltd_index, tgt->ltd_uuid.uuid,
 		   tgt->ltd_active ? "" : "IN");
 	return 0;
 }
@@ -156,6 +259,9 @@ static struct attribute *lmv_attrs[] = {
 	&lustre_attr_activeobd.attr,
 	&lustre_attr_desc_uuid.attr,
 	&lustre_attr_numobd.attr,
+	&lustre_attr_qos_maxage.attr,
+	&lustre_attr_qos_prio_free.attr,
+	&lustre_attr_qos_threshold_rr.attr,
 	NULL,
 };
 

lustre/tests/sanity.sh
@@ -19952,7 +19952,7 @@ test_412() {
 }
 run_test 412 "mkdir on specific MDTs"
 
-test_413() {
+test_413a() {
 	[ $MDSCOUNT -lt 2 ] &&
 		skip "We need at least 2 MDTs for this test"
 
@@ -19986,7 +19986,129 @@ test_413() {
 			error "don't expect $max"
 	done
 }
-run_test 413 "mkdir on less full MDTs"
+run_test 413a "mkdir on less full MDTs"
+
+test_413b() {
+	[ $MDSCOUNT -lt 2 ] &&
+		skip "We need at least 2 MDTs for this test"
+
+	[ $MDS1_VERSION -lt $(version_code 2.12.52) ] &&
+		skip "Need server version at least 2.12.52"
+
+	mkdir $DIR/$tdir || error "mkdir failed"
+	$LFS setdirstripe -D -i -1 -H space $DIR/$tdir ||
+		error "setdirstripe failed"
+
+	local qos_prio_free
+	local qos_threshold_rr
+	local count
+
+	qos_prio_free=$($LCTL get_param -n lmv.*.qos_prio_free | head -n1)
+	qos_prio_free=${qos_prio_free%%%}
+	qos_threshold_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr | head -n1)
+	qos_threshold_rr=${qos_threshold_rr%%%}
+
+	stack_trap "$LCTL set_param lmv.*.qos_prio_free=$qos_prio_free" EXIT
+	stack_trap "$LCTL set_param lmv.*.qos_threshold_rr=$qos_threshold_rr" \
+		EXIT
+
+	echo "mkdir with roundrobin"
+
+	$LCTL set_param lmv.*.qos_threshold_rr=100
+	for i in $(seq $((100 * MDSCOUNT))); do
+		mkdir $DIR/$tdir/subdir$i || error "mkdir subdir$i failed"
+	done
+	for i in $(seq $MDSCOUNT); do
+		count=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$((i - 1))$ |
+			wc -w)
+		echo "$count directories created on MDT$((i - 1))"
+		[ $count -eq 100 ] || error "subdirs are not evenly distributed"
+	done
+
+	rm -rf $DIR/$tdir/*
+
+	$LCTL set_param lmv.*.qos_threshold_rr=$qos_threshold_rr
+
+	local ffree
+	local max
+	local min
+	local max_index
+	local min_index
+
+	ffree=($(lctl get_param -n mdc.*[mM][dD][cC]-[^M]*.filesfree | uniq))
+	echo "MDT filesfree available: ${ffree[@]}"
+	max=${ffree[0]}
+	min=${ffree[0]}
+	max_index=0
+	min_index=0
+	for ((i = 0; i < ${#ffree[@]}; i++)); do
+		if [[ ${ffree[i]} -gt $max ]]; then
+			max=${ffree[i]}
+			max_index=$i
+		fi
+		if [[ ${ffree[i]} -lt $min ]]; then
+			min=${ffree[i]}
+			min_index=$i
+		fi
+	done
+	echo "Min free files: MDT$min_index: $min"
+	echo "Max free files: MDT$max_index: $max"
+
+	[ $min -eq 0 ] && skip "no free files in MDT$min_index"
+	[ $min -gt 10000000 ] && skip "too much free files in MDT$min_index"
+
+	# Check if we need to generate uneven MDTs
+	test_mkdir -i $min_index -c 1 -p $DIR/$tdir-MDT$min_index
+	local threshold=10
+	local diff=$((max - min))
+	local diff2=$((diff * 100 / min))
+
+	echo -n "Check for uneven MDTs: "
+	echo -n "diff=$diff files ($diff2%) must be > $threshold% ..."
+
+	if [ $diff2 -gt $threshold ]; then
+		echo "ok"
+		echo "Don't need to fill MDT$min_index"
+	else
+		# generate uneven MDTs, create till 25% diff
+		echo "no"
+		diff2=$((threshold - diff2))
+		diff=$((min * diff2 / 100))
+		# 50 sec per 10000 files in vm
+		[ $diff -gt 40000 ] && [ "$SLOW" = "no" ] &&
+			skip "$diff files to create"
+		echo "Fill $diff2% diff in MDT$min_index with $diff files"
+		local i
+		local value="$(generate_string 1024)"
+		for i in $(seq $diff); do
+			$OPENFILE -f O_CREAT:O_LOV_DELAY_CREATE \
+				$DIR/$tdir-MDT$min_index/f$i > /dev/null ||
+				error "create f$i failed"
+			setfattr -n user.413b -v $value \
+				$DIR/$tdir-MDT$min_index/f$i ||
+				error "setfattr f$i failed"
+		done
+	fi
+
+	min=$((100 *MDSCOUNT))
+	max=0
+
+	echo "mkdir with balanced space usage"
+	$LCTL set_param lmv.*.qos_prio_free=100
+	for i in $(seq $((100 * MDSCOUNT))); do
+		mkdir $DIR/$tdir/subdir$i || error "mkdir subdir$i failed"
+	done
+	for i in $(seq $MDSCOUNT); do
+		count=$($LFS getdirstripe -i $DIR/$tdir/* | grep ^$((i - 1))$ |
+			wc -w)
+		echo "$count directories created on MDT$((i - 1))"
+		[ $min -gt $count ] && min=$count
+		[ $max -lt $count ] && max=$count
+	done
+	[ $((max - min)) -gt $MDSCOUNT ] ||
+		error "subdirs shouldn't be evenly distributed"
+}
+run_test 413b "mkdir with balanced space usage"
 
 test_414() {
 #define OBD_FAIL_PTLRPC_BULK_ATTACH      0x521

lustre/include/lu_object.h
@@ -1387,5 +1387,89 @@ static inline bool lu_object_is_cl(const struct lu_object *o)
 	return lu_device_is_cl(o->lo_dev);
 }
 
+/* Generic subset of OSTs */
+struct ost_pool {
+	__u32		   *op_array;	/* array of index of
+					 * lov_obd->lov_tgts */
+	unsigned int	    op_count;	/* number of OSTs in the array */
+	unsigned int	    op_size;	/* allocated size of lp_array */
+	struct rw_semaphore op_rw_sem;	/* to protect ost_pool use */
+};
+
+/* round-robin QoS data for LOD/LMV */
+struct lu_qos_rr {
+	spinlock_t		 lqr_alloc;	/* protect allocation index */
+	__u32			 lqr_start_idx;	/* start index of new inode */
+	__u32			 lqr_offset_idx;/* aliasing for start_idx */
+	int			 lqr_start_count;/* reseed counter */
+	struct ost_pool		 lqr_pool;	/* round-robin optimized list */
+	unsigned long		 lqr_dirty:1;	/* recalc round-robin list */
+};
+
+/* QoS data per MDS/OSS */
+struct lu_svr_qos {
+	struct obd_uuid		 lsq_uuid;	/* ptlrpc's c_remote_uuid */
+	struct list_head	 lsq_svr_list;	/* link to lq_svr_list */
+	__u64			 lsq_bavail;	/* total bytes avail on svr */
+	__u64			 lsq_iavail;	/* tital inode avail on svr */
+	__u64			 lsq_penalty;	/* current penalty */
+	__u64			 lsq_penalty_per_obj; /* penalty decrease
+						       * every obj*/
+	time64_t		 lsq_used;	/* last used time, seconds */
+	__u32			 lsq_tgt_count;	/* number of tgts on this svr */
+	__u32			 lsq_id;	/* unique svr id */
+};
+
+/* QoS data per MDT/OST */
+struct lu_tgt_qos {
+	struct lu_svr_qos	*ltq_svr;	/* svr info */
+	__u64			 ltq_penalty;	/* current penalty */
+	__u64			 ltq_penalty_per_obj; /* penalty decrease
+						       * every obj*/
+	__u64			 ltq_weight;	/* net weighting */
+	time64_t		 ltq_used;	/* last used time, seconds */
+	bool			 ltq_usable:1;	/* usable for striping */
+};
+
+/* target descriptor */
+struct lu_tgt_desc {
+	union {
+		struct dt_device	*ltd_tgt;
+		struct obd_device	*ltd_obd;
+	};
+	struct obd_export *ltd_exp;
+	struct obd_uuid    ltd_uuid;
+	__u32              ltd_index;
+	__u32		   ltd_gen;
+	struct list_head   ltd_kill;
+	struct ptlrpc_thread	*ltd_recovery_thread;
+	struct mutex	   ltd_fid_mutex;
+	struct lu_tgt_qos  ltd_qos; /* qos info per target */
+	struct obd_statfs  ltd_statfs;
+	time64_t	   ltd_statfs_age;
+	unsigned long      ltd_active:1,/* is this target up for requests */
+			   ltd_activate:1,/* should target be activated */
+			   ltd_reap:1,  /* should this target be deleted */
+			   ltd_got_update_log:1, /* Already got update log */
+			   ltd_connecting:1; /* target is connecting */
+};
+
+/* QoS data for LOD/LMV */
+struct lu_qos {
+	struct list_head	 lq_svr_list;	/* lu_svr_qos list */
+	struct rw_semaphore	 lq_rw_sem;
+	__u32			 lq_active_svr_count;
+	unsigned int		 lq_prio_free;   /* priority for free space */
+	unsigned int		 lq_threshold_rr;/* priority for rr */
+	struct lu_qos_rr	 lq_rr;          /* round robin qos data */
+	unsigned long		 lq_dirty:1,     /* recalc qos data */
+				 lq_same_space:1,/* the servers all have approx.
+						  * the same space avail */
+				 lq_reset:1;     /* zero current penalties */
+};
+
+int lqos_add_tgt(struct lu_qos *qos, struct lu_tgt_desc *ltd);
+int lqos_del_tgt(struct lu_qos *qos, struct lu_tgt_desc *ltd);
+
 /** @} lu */
 #endif /* __LUSTRE_LU_OBJECT_H */

commit 99b251c971e95c56d52165e9bbaea16d91310d08

LU-19424 lmv: expose qos_rr_index parameter
Add a new lmv.*.qos_rr_index parameter that prints or sets
the current qos_rr_index value modulo the number of MDTs.

This can be used to determine if the round-robin index is
being initialized as expected, or can be set to ensure a
uniform distribution of clients across MDTs for testing.

Fixes: b601eb35e9 ("LU-11213 lmv: reuse object alloc QoS code from LOD")
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I5e3176c75bbaf2b146f9bb79872a6272382540e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61543
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Frederick Dilger <fdilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -1252,7 +1252,8 @@ static struct lu_device *lmv_device_alloc(const struct lu_env *env,
 
 	/*
 	 * initialize rr_index to lower 32bit of netid, so that client
-	 * can distribute subdirs evenly from the beginning.
+	 * can distribute new subdir creation in round-robin directories
+	 * relatively evenly across MDTs from the beginning.
 	 */
 	while (LNetGetId(i++, &lnet_id, true) != -ENOENT) {
 		if (!nid_is_lo0(&lnet_id.nid)) {

lustre/lmv/lproc_lmv.c
@@ -131,6 +131,41 @@ static ssize_t qos_prio_free_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(qos_prio_free);
 
+static ssize_t qos_rr_index_show(struct kobject *kobj, struct attribute *attr,
+				 char *buf)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct lmv_obd *lmv = &obd->u.lmv;
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", lmv->lmv_qos_rr_index %
+					lmv->lmv_mdt_descs.ltd_tgts_size);
+}
+
+static ssize_t qos_rr_index_store(struct kobject *kobj, struct attribute *attr,
+				  const char *buffer, size_t count)
+{
+	struct obd_device *obd = container_of(kobj, struct obd_device,
+					      obd_kset.kobj);
+	struct lmv_obd *lmv = &obd->u.lmv;
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	/* It doesn't really matter what value is stored here, since it will
+	 * always be used modulo of the current MDT count.  It is actually
+	 * better if this value is not constrained to 0..MDTCOUNT-1, because
+	 * it will continue to work even if the number of MDTs changes.
+	 */
+	lmv->lmv_qos_rr_index = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(qos_rr_index);
+
 static ssize_t qos_threshold_rr_show(struct kobject *kobj,
 				     struct attribute *attr,
 				     char *buf)
@@ -455,6 +490,7 @@ static struct attribute *lmv_attrs[] = {
 	&lustre_attr_numobd.attr,
 	&lustre_attr_qos_maxage.attr,
 	&lustre_attr_qos_prio_free.attr,
+	&lustre_attr_qos_rr_index.attr,
 	&lustre_attr_qos_threshold_rr.attr,
 	NULL,
 };

lustre/tests/sanity.sh
@@ -31931,6 +31931,28 @@ most_full_mdt() {
 	echo -n $min_index
 }
 
+test_413A() {
+	local index=0
+
+	echo "before"
+	do_nodes $CLIENTS $LCTL get_param lmv.*.qos_rr_index
+	for client in ${CLIENTS//,/ }; do
+		do_node $client $LCTL set_param -n lmv.*.qos_rr_index=$index
+		((index++))
+	done
+	echo "after"
+	do_nodes $CLIENTS $LCTL get_param lmv.*.qos_rr_index
+	index=0
+	for client in ${CLIENTS//,/ }; do
+		got=$(do_node $client $LCTL get_param -n lmv.*.qos_rr_index)
+		((got == index)) ||
+			error "lmv.*.qos_rr_index was $got != $index"
+		((index++))
+	done
+	echo "all OSCs set correctly"
+}
+run_test 413A "get and set qos_rr_index on all clients"
+
 test_413a() {
 	[ $MDSCOUNT -lt 2 ] &&
 		skip "We need at least 2 MDTs for this test"