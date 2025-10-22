lustre/lustre-release

commit ca6fdf73528a01ef05ebd292add3cd4f64e43e5c

LU-19234 lst: Fix leak under emitter/parser delete (1)
This is first series of patch with fixes memory
leak which is leaked when yaml_emitter_delete()
and yaml_parser_delete() is called. All calls to
yaml_emitter_delete() will be replaced with
yaml_emitter_cleanup() and yaml_parser_delete()
will be replaced with yaml_parser_cleanup() which
is introduced in this patch.

This patch gets the reference back then release
memory after call to yaml_emitter_delete(). To
do this both calls to delete emiiter and memory
held is placed under a single call to
yaml_emitter_cleanup().

Similarly, it gets the reference back then release
memory after call to yaml_parser_delete(). To
do this both calls to delete emiiter and memory
held is placed under a single call to
yaml_emitter_cleanup().

$ valgrind --leak-check=full lst list_group
------------
Before Patch
------------
==41478== LEAK SUMMARY:
==41478==    definitely lost: 376 bytes in 3 blocks
==41478==    indirectly lost: 65,536 bytes in 1 blocks
==41478==      possibly lost: 0 bytes in 0 blocks
==41478==    still reachable: 0 bytes in 0 blocks
==41478==         suppressed: 0 bytes in 0 blocks

-----------
After patch
-----------
==84336== LEAK SUMMARY:
==84336==    definitely lost: 224 bytes in 1 blocks
==84336==    indirectly lost: 0 bytes in 0 blocks
==84336==      possibly lost: 0 bytes in 0 blocks
==84336==    still reachable: 0 bytes in 0 blocks
==84336==         suppressed: 0 bytes in 0 blocks

Note 224 unfreed bytes comes from libnl and not lustre code
Therefore which require fix from within libnl and not lustre

==84336== 224 bytes in 1 blocks are definitely lost in loss record 1 of 1
==84336==    at 0x484BF70: calloc (vg_replace_malloc.c:1595)
==84336==    by 0x499946D: nl_cb_alloc (in /usr/lib64/libnl-3.so.200.26.0)
==84336==    by 0x499DC85: nl_socket_alloc (in /usr/lib64/libnl-3.so.200.26.0)
==84336==    by 0x4033C3: lst_yaml_groups.constprop.0 (lst.c:1150)
==84336==    by 0x404D0D: jt_lst_list_group (lst.c:2053)

lnet/utils/lnetconfig/liblnetconfig_netlink.c
@@ -1168,6 +1168,7 @@ static int yaml_netlink_read_handler(void *arg, unsigned char *buffer,
 		data->read += size;
 	} else if (data->complete) {
 		free(data->start);
+		data->start = NULL;
 	}
 	*size_read = size;
 	return 1;
@@ -1902,6 +1903,50 @@ void yaml_emitter_log_error(yaml_emitter_t *emitter, FILE *log)
 		fprintf(log, "Emitter error: %s\n", emitter->problem);
 	default:
 		break;
+	}
+}
+
+/*
+ * yaml_emitter_cleanup - Cleanup request & all memory held by reqeust
+ */
+void yaml_emitter_cleanup(yaml_emitter_t *request)
+{
+	struct yaml_netlink_output *out = NULL;
+
+	if (!request || !request->write_handler_data)
+		return;
+
+	out = request->write_handler_data;
+
+	/* first destroy emitter */
+	yaml_emitter_delete(request);
+
+	if (out)
+		free(out);
+}
+
+/*
+ * yaml_parser_cleanup - Cleanup parser & all memory held by parser
+ */
+void yaml_parser_cleanup(yaml_parser_t *reply)
+{
+	struct yaml_netlink_input *input = NULL;
+
+	if (!reply || !reply->read_handler_data)
+		return;
+
+	input = reply->read_handler_data;
+
+	/* delete parser first */
+	yaml_parser_delete(reply);
+
+	if (input) {
+		if (input->start) {
+			free(input->start);
+			input->start = NULL;
+		}
+
+		free(input);
 	}
 }
 

lnet/utils/lst.c
@@ -1318,7 +1318,7 @@ emitter_error:
 		rc = -EINVAL;
 		goto parser_error;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	/* display output */
 	if (nlflags == NLM_F_DUMP)
@@ -1326,7 +1326,7 @@ emitter_error:
 parser_error:
 	if (rc == 0)
 		yaml_parser_log_error(&reply, stderr, NULL);
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	if (print)

lnet/utils/lnetconfig/liblnetconfig.h
@@ -912,6 +912,16 @@ void yaml_parser_log_error(yaml_parser_t *parser, FILE *log,
  */
 void yaml_emitter_log_error(yaml_emitter_t *emitter, FILE *log);
 
+/*
+ * yaml_emitter_cleanup - Cleanup request & all memory held by reqeust
+ */
+void yaml_emitter_cleanup(yaml_emitter_t *request);
+
+/*
+ * yaml_parser_cleanup - Cleanup parser & all memory held by parser
+ */
+void yaml_parser_cleanup(yaml_parser_t *reply);
+
 /**
  * yaml_netlink_setup_emitter
  *

commit 36b493ef08d7601e36f6c24be621bb0336c28185

LU-19285 utils: Move yaml_parser_delete to yaml_parser_cleanup
This is a followup patch after commit ca6fdf73528a0 (LU-19234)
This patch moves direct calls from yaml_parser_delete() to
yaml_parser_cleanup() and direct calls from yaml_emitter_delete()
to yaml_emitter_cleanup() wherever it is required.

This patch also removes warnings added under spelling.txt
which was added under patch commit ca6fdf73528a0. The reason
being, not all calls must call *_cleanup(). So, direct call
to yaml_emitter_delete or yaml_parser_delect is still valid.

Fixes: ca6fdf73528a ("U-19234 lst: Fix leak under emitter/parser delete (1)")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Iec2826fde029650541936176d90abdf655061050
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60860
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/utils/lnetconfig/cyaml.c
@@ -29,6 +29,7 @@
 #include <float.h>
 #include <limits.h>
 #include <ctype.h>
+#include <lnetconfig/liblnetconfig.h>
 #include "libcfs/util/list.h"
 #include <cyaml.h>
 

lnet/utils/lnetconfig/liblnetconfig.c
@@ -442,7 +442,7 @@ int yaml_netlink_complete_emitter(yaml_emitter_t *output)
 	if (rc == 0)
 		goto emitter_error;
 
-	yaml_emitter_delete(output);
+	yaml_emitter_cleanup(output);
 emitter_error:
 	return rc;
 }
@@ -549,14 +549,14 @@ int yaml_lnet_configure(int flags, const char **msg)
 
 		rc = yaml_parser_load(&reply, &errmsg);
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 free_reply:
 	if (rc == 0) {
 		*msg = yaml_parser_get_reader_error(&reply);
 		rc = errno;
 	}
 
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	yaml_document_delete(&results);
 	nl_socket_free(sk);
 
@@ -824,7 +824,7 @@ emitter_error:
 				rc = 1;
 		}
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (rc == 0) {
 		if (!msg)
@@ -839,7 +839,7 @@ free_reply:
 		}
 		rc = errno;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;

lnet/utils/lnetconfig/liblnetconfig_netlink.c
@@ -1879,6 +1879,22 @@ yaml_emitter_set_streaming_output_netlink(yaml_emitter_t *sender,
 	return true;
 }
 
+/**
+ * yaml_emitter_set_output_netlink() - Set output to Netlink socket and not any
+ * YAML document. This is wrapper to yaml_emitter_set_streaming_output_netlink()
+ * @sender: emitter object
+ * @nl: netlink socket
+ * @family: name of socket (lnet)
+ * @version: version number
+ * @cmd: command identifier
+ * @flags: netlink flags (NLM_F_* under liblnetconfig.h)
+ *
+ * Note: Emitter object setup with this function should always call
+ * yaml_emitter_cleanup() and not yaml_emitter_delete(). Otherwise it will
+ * result in memory leak
+ *
+ * Return TRUE on success and FALSE on failure
+ */
 YAML_DECLARE(int)
 yaml_emitter_set_output_netlink(yaml_emitter_t *sender, struct nl_sock *nl,
 				char *family, int version, int cmd, int flags)
@@ -1907,7 +1923,7 @@ void yaml_emitter_log_error(yaml_emitter_t *emitter, FILE *log)
 }
 
 /*
- * yaml_emitter_cleanup - Cleanup request & all memory held by reqeust
+ * yaml_emitter_cleanup - Cleanup request & all memory held by request
  */
 void yaml_emitter_cleanup(yaml_emitter_t *request)
 {

lnet/utils/lnetctl.c
@@ -1274,7 +1274,7 @@ emitter_error:
 	} else {
 		rc = yaml_lnet_cpt_of_nid_display(&reply);
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 free_reply:
 	if (rc == 0) {
 		yaml_lnet_print_error(NLM_F_DUMP, "cpt-of-nid",
@@ -1282,7 +1282,7 @@ free_reply:
 		rc = -EINVAL;
 	}
 
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -1588,7 +1588,7 @@ static int yaml_lnet_route(char *nw, char *gw, int hops, int prio,
 			rc = lustre_lnet_parse_nid_range(&head, gw, &msg);
 			if (rc < 0) {
 				lustre_lnet_free_list(&head);
-				yaml_emitter_delete(&output);
+				yaml_emitter_cleanup(&output);
 				errno = rc;
 				rc = 0;
 				goto free_reply;
@@ -1678,7 +1678,7 @@ free_reply:
 		yaml_lnet_print_error(flags, "route", msg);
 		rc = -EINVAL;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -2401,13 +2401,13 @@ emitter_error:
 		}
 		yaml_document_delete(&errmsg);
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (rc == 0) {
 		yaml_lnet_print_error(flags, "net", msg);
 		rc = -EINVAL;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -3237,7 +3237,7 @@ static int yaml_lnet_peer(char *prim_nid, char *nidstr, bool disable_mr,
 			if (rc < 0) {
 				fprintf(stdout, "can't parse nidrange: \"%s\"\n", nidstr);
 				lustre_lnet_free_list(&head);
-				yaml_emitter_delete(&output);
+				yaml_emitter_cleanup(&output);
 				errno = rc;
 				rc = 0;
 				goto free_reply;
@@ -3245,7 +3245,7 @@ static int yaml_lnet_peer(char *prim_nid, char *nidstr, bool disable_mr,
 
 			if (nl_list_empty(&head.children)) {
 				lustre_lnet_free_list(&head);
-				yaml_emitter_delete(&output);
+				yaml_emitter_cleanup(&output);
 				msg = "Unable to parse nidlist: did not expand to any nids";
 				errno = -ENOENT;
 				rc = 0;
@@ -3258,7 +3258,7 @@ static int yaml_lnet_peer(char *prim_nid, char *nidstr, bool disable_mr,
 
 				if (count++ > LNET_MAX_NIDS_PER_PEER) {
 					lustre_lnet_free_list(&head);
-					yaml_emitter_delete(&output);
+					yaml_emitter_cleanup(&output);
 					msg = "Unable to parse nidlist: specifies more NIDs than allowed";
 					errno = -E2BIG;
 					rc = 0;
@@ -3407,13 +3407,13 @@ emitter_error:
 				rc = 1;
 		}
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (rc == 0) {
 		yaml_lnet_print_error(flags, "peer", msg);
 		rc = -EINVAL;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -3661,7 +3661,7 @@ static int yaml_debug_recovery(enum lnet_health_type type)
 		}
 		yaml_document_delete(&errmsg);
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (rc == 0) {
 		if (!msg)
@@ -3669,7 +3669,7 @@ free_reply:
 
 		fprintf(stdout, "Operation failed: %s\n", msg);
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -5387,7 +5387,7 @@ skip_test:
 emitter_error:
 	if (rc == 0) {
 		yaml_emitter_log_error(&output, stderr);
-		yaml_emitter_delete(&output);
+		yaml_emitter_cleanup(&output);
 		rc = -EINVAL;
 	} else if (!unspec) {
 		yaml_document_t errmsg;
@@ -5416,7 +5416,7 @@ free_reply:
 		yaml_lnet_print_error(flags, "import", msg);
 		rc = -EINVAL;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	yaml_parser_delete(&setup);
 	nl_socket_free(sk);
 	if (input)
@@ -6141,7 +6141,7 @@ static int yaml_lnet_ping(char *group, int timeout, struct lnet_nid *src_nid,
 		rc = lustre_lnet_parse_nid_range(&head, nids[i], &msg);
 		if (rc < 0) {
 			lustre_lnet_free_list(&head);
-			yaml_emitter_delete(&output);
+			yaml_emitter_cleanup(&output);
 			errno = rc;
 			rc = 0;
 			goto free_reply;
@@ -6192,14 +6192,14 @@ emitter_error:
 		if (rc == 0)
 			msg = yaml_parser_get_reader_error(&reply);
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (rc == 0) {
 		yaml_lnet_print_error(-1, group, msg);
 		rc = -EINVAL;
 	}
 
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -6866,7 +6866,7 @@ static int jt_fault_show_common(__u32 opc, int argc, char **argv)
 	yaml_emitter_set_output_file(&debug, stdout);
 	rc = yaml_emitter_dump(&debug, &results);
 
-	yaml_emitter_delete(&debug);
+	yaml_emitter_cleanup(&debug);
 	yaml_document_delete(&results);
 
 	return rc == 0 ? -EINVAL : 0;

lnet/utils/lst.c
@@ -631,11 +631,11 @@ skip_params:
 	if (rc == 0) {
 emitter_error:
 		yaml_emitter_log_error(&request, stderr);
-		yaml_emitter_delete(&request);
+		yaml_emitter_cleanup(&request);
 		errmsg = NULL;
 		goto parser_error;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	while (!done) {
 		rc = yaml_parser_parse(&reply, &event);
@@ -719,7 +719,7 @@ emitter_error:
 parser_error:
 	if (rc == 0 && errmsg)
 		yaml_parser_log_error(&reply, stderr, errmsg);
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	if (((nlflags & NLM_F_DUMP) == NLM_F_DUMP) && rc != 0) {
@@ -1312,7 +1312,7 @@ static int lst_yaml_groups(int nlflags, char *name, int states, bool print)
 	if (rc == 0) {
 emitter_error:
 		yaml_emitter_log_error(&request, stderr);
-		yaml_emitter_delete(&request);
+		yaml_emitter_cleanup(&request);
 		rc = -EINVAL;
 		goto parser_error;
 	}
@@ -1324,7 +1324,7 @@ emitter_error:
 parser_error:
 	if (rc == 0)
 		yaml_parser_log_error(&reply, stderr, NULL);
-	yaml_parser_cleanup(&reply);
+	yaml_parser_delete(&reply);
 	nl_socket_free(sk);
 
 	if (print)

lustre/utils/liblustreapi_param.c
@@ -854,7 +854,7 @@ error:
 		yaml_emitter_log_error(&request, stderr);
 		rc = -EINVAL;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	return rc == 1 ? 0 : -EINVAL;
 }
@@ -945,7 +945,7 @@ int llapi_param_display_value(char *path, int version,
 		}
 	}
 free_reply:
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 	return rc == 1 ? 0 : rc;
 }
@@ -985,7 +985,7 @@ int llapi_param_set_value(char *path, char *value, int version,
 
 	yaml_document_delete(&results);
 free_reply:
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 	return rc == 1 ? 0 : rc;
 }

lustre/utils/lustre_cfg.c
@@ -733,11 +733,11 @@ static int yaml_get_device_index(char *source)
 error:
 	if (rc == 0) {
 		yaml_emitter_log_error(&request, stderr);
-		yaml_emitter_delete(&request);
+		yaml_emitter_cleanup(&request);
 		rc = -EINVAL;
 		goto free_reply;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	while (!done) {
 		rc = yaml_parser_parse(&reply, &event);
@@ -769,7 +769,7 @@ error:
 		yaml_event_delete(&event);
 	}
 free_reply:
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc;

lustre/utils/portals.c
@@ -420,7 +420,7 @@ jt_ptl_list_nids(int argc, char **argv)
 	rc = yaml_parser_set_input_netlink(&reply, sk, false);
 	if (rc == 0) {
 		yaml_parser_log_error(&reply, stderr, NULL);
-		yaml_parser_delete(&reply);
+		yaml_parser_cleanup(&reply);
 		goto old_api;
 	}
 
@@ -428,7 +428,7 @@ jt_ptl_list_nids(int argc, char **argv)
 	rc = yaml_emitter_initialize(&request);
 	if (rc == 0) {
 		yaml_parser_log_error(&reply, stderr, NULL);
-		yaml_parser_delete(&reply);
+		yaml_parser_cleanup(&reply);
 		goto old_api;
 	}
 
@@ -436,8 +436,8 @@ jt_ptl_list_nids(int argc, char **argv)
 					     LNET_CMD_NETS, NLM_F_DUMP);
 	if (rc == 0) {
 		yaml_emitter_log_error(&request, stderr);
-		yaml_emitter_delete(&request);
-		yaml_parser_delete(&reply);
+		yaml_emitter_cleanup(&request);
+		yaml_parser_cleanup(&reply);
 		goto old_api;
 	}
 
@@ -534,7 +534,7 @@ emitter_error:
 		yaml_emitter_log_error(&request, stderr);
 		rc = -EINVAL;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	while (!done) {
 		rc = yaml_parser_parse(&reply, &event);
@@ -567,7 +567,7 @@ emitter_error:
 
 	if (rc == 0)
 		yaml_parser_log_error(&reply, stderr, NULL);
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 old_api: {
 #ifdef IOC_LIBCFS_GET_NI
 	int count;
@@ -729,7 +729,7 @@ emitter_error:
 		yaml_emitter_log_error(&request, stderr);
 		rc = -EINVAL;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	while (!done) {
 		rc = yaml_parser_parse(&reply, &event);
@@ -795,7 +795,7 @@ free_reply:
 		rc = 0;
 	}
 
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 	goto finished;
 
@@ -1480,7 +1480,7 @@ emitter_error:
 		rc = -EINVAL;
 		goto old_api;
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 
 	/* Now parse the reply results */
 	while (!done) {
@@ -1538,7 +1538,7 @@ free_reply:
 		/* yaml_* functions return 1 for success */
 		rc = 0;
 	}
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 	return rc;
 old_api:
@@ -1763,11 +1763,11 @@ emitter_error:
 		}
 		yaml_document_delete(&errmsg);
 	}
-	yaml_emitter_delete(&request);
+	yaml_emitter_cleanup(&request);
 free_reply:
 	if (rc == 0)
 		yaml_parser_log_error(&reply, stderr, NULL);
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;
@@ -2144,11 +2144,11 @@ emitter_error:
 		if (rc == 0)
 			msg = yaml_parser_get_reader_error(&reply);
 	}
-	yaml_emitter_delete(&output);
+	yaml_emitter_cleanup(&output);
 free_reply:
 	if (msg)
 		fprintf(stdout, "%s\n", msg);
-	yaml_parser_delete(&reply);
+	yaml_parser_cleanup(&reply);
 	nl_socket_free(sk);
 
 	return rc == 1 ? 0 : rc;