lustre/lustre-release

commit 144da866d6598ef552b6d306aa2a5632b399200f

LU-19159 lnet: Handle ip2nets in jt_import
jt_import() is updated to understand ip2nets yaml sequence.

Note that not all tunables and lnd tunables are supported because
yaml_lnet_config_ni does not understand all of them. This will be
fixed in a separate patch.

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I2bf41e5f97714c66e43172e758a36f91311ec88c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60035
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -49,25 +49,6 @@
 const char *gmsg_stat_names[] = {"sent_stats", "received_stats",
 				 "dropped_stats"};
 
-/*
- * lustre_lnet_ip_range_descr
- *	Describes an IP range.
- *	Each octect is an expression
- */
-struct lustre_lnet_ip_range_descr {
-	struct list_head ipr_entry;
-	struct list_head ipr_expr;
-};
-
-/*
- * lustre_lnet_ip2nets
- *	Describes an ip2nets rule. This can be on a list of rules.
- */
-struct lustre_lnet_ip2nets {
-	struct lnet_dlc_network_descr ip2nets_net;
-	struct list_head ip2nets_ip_ranges;
-};
-
 static int open_sysfs_file(const char *path, const char *attr, const int mode)
 {
 	int fd;
@@ -128,7 +109,7 @@ close_fd:
  * free_intf_descr
  *	frees the memory allocated for an intf descriptor.
  */
-static void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
+void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
 {
 	if (!intf_descr)
 		return;
@@ -150,7 +131,7 @@ static void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
  *		As a result of lnetctl command
  *		When building a NID or P2P selection rules
  */
-static int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
+int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
 {
 	struct lustre_lnet_ip_range_descr *ip_range;
 	int rc;
@@ -172,7 +153,7 @@ static int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
 	return LUSTRE_CFG_RC_NO_ERR;
 }
 
-static int lustre_lnet_add_intf_descr(struct list_head *list, char *intf,
+int lustre_lnet_add_intf_descr(struct list_head *list, char *intf,
 				      int len)
 {
 	char *open_sq_bracket = NULL, *close_sq_bracket = NULL,
@@ -1929,7 +1910,7 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 	return LUSTRE_CFG_RC_MATCH;
 }
 
-static int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
+int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 					    lnet_nid_t **nids, __u32 *nnids,
 					    char *err_str, size_t str_len)
 {
@@ -2342,7 +2323,7 @@ lustre_lnet_ioctl_config_ni(struct list_head *intf_list,
 	return LUSTRE_CFG_RC_NO_ERR;
 }
 
-static int
+int
 lustre_lnet_config_ip2nets(struct lustre_lnet_ip2nets *ip2nets,
 			   struct lnet_ioctl_config_lnd_tunables *tunables,
 			   struct cfs_expr_list *global_cpts,
@@ -5345,25 +5326,20 @@ yaml_extract_cpt(struct cYAML *tree,
 
 /*
  * net:
- *    - net type: <net>[<NUM>]
-  *      local NI(s):
- *        - nid: <ip>@<net>[<NUM>]
- *          status: up
- *          interfaces:
- *               0: <intf_name>['['<expr>']']
- *               1: <intf_name>['['<expr>']']
- *        tunables:
- *               peer_timeout: <NUM>
- *               peer_credits: <NUM>
- *               peer_buffer_credits: <NUM>
- *               credits: <NUM>
-*         lnd tunables:
- *               peercredits_hiw: <NUM>
- *               map_on_demand: <NUM>
- *               concurrent_sends: <NUM>
- *               fmr_pool_size: <NUM>
- *               fmr_flush_trigger: <NUM>
- *               fmr_cache: <NUM>
+ * -     net type: <net>[<NUM>]
+ *       local NI(s):
+ *       -     interfaces:
+ *                   0: <intf_name>['['<expr>']']
+ *                   1: <intf_name>['['<expr>']']
+ *             tunables:
+ *                   peer_timeout: <NUM>
+ *                   peer_credits: <NUM>
+ *                   peer_buffer_credits: <NUM>
+ *                   credits: <NUM>
+ *             lnd tunables:
+ *                   <lnd_param1>: <val>
+ *                   <lnd_param2>: <val>
+ *                   <...>
  *
  * At least one interface is required. If no interfaces are provided the
  * network interface can not be configured.
@@ -5442,13 +5418,22 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 
 /*
  * ip2nets:
- *  - net-spec: <tcp|o2ib|gni>[NUM]
+ *  - net-spec: <net>[NUM]
  *    interfaces:
  *        0: <intf name>['['<expr>']']
  *        1: <intf name>['['<expr>']']
  *    ip-range:
  *        0: <expr.expr.expr.expr>
  *        1: <expr.expr.expr.expr>
+ *    tunables:
+ *          peer_timeout: <NUM>
+ *          peer_credits: <NUM>
+ *          peer_buffer_credits: <NUM>
+ *          credits: <NUM>
+ *    lnd tunables:
+ *          <lnd_param1>: <val>
+ *          <lnd_param2>: <val>
+ *          <...>
  */
 static int handle_yaml_config_ip2nets(struct cYAML *tree,
 				      struct cYAML **show_rc,

commit f883eaa592686a8826a5a8076ccbcc29f4ee3b4d

LU-19436 lnet: Correct ip2nets yaml parsing
The ip2nets parsing in handle_ip2nets_sequence() had several issues
that prevented it from adhering to documented behavior:

- Rules that didn't match local interfaces were incorrectly treated
  as errors instead of being silently skipped as documented
- Duplicate networks could be configured multiple times in a sequence
- Error messages were generic and didn't help diagnose configuration
  issues

This patch fixes the parsing logic to:
- Properly handle LUSTRE_CFG_RC_NO_MATCH as a valid state for
  ip2nets rules that don't apply to the current node
  - To accomodate this change, lustre_lnet_kfi_intf2nid() was changed
    to return RC_BAD_PARAM rather than RC_NO_MATCH on error.
- Track configured networks to prevent duplicates within sequences
- Provide specific error messages for different failure scenarios
  (interface not found, interface down, IP pattern mismatch, etc.)

The changes ensure that ip2nets behaves as documented: each node
identifies locally available networks based on IP address pattern
matching, with non-matching rules being silently ignored rather
than causing configuration failures.

Test-Parameters: trivial
Fixes: 144da866d6 ("LU-19159 lnet: Handle ip2nets in jt_import")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I6e1dac32423fc4f2056d30f0b10fa2dd847cd490
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61591
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: James Simmons <jsimmons@infradead.org>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -1606,7 +1606,7 @@ static int lustre_lnet_kfi_intf2nid(struct lnet_dlc_intf_descr *intf,
 
 	rc = sscanf(intf->intf_name, "cxi%u", &nic_index);
 	if (rc != 1)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	size = snprintf(NULL, 0, cxi_nic_addr_path, nic_index) + 1;
 	nic_addr_path = malloc(size);
@@ -1617,11 +1617,11 @@ static int lustre_lnet_kfi_intf2nid(struct lnet_dlc_intf_descr *intf,
 	rc = read_sysfs_file(nic_addr_path, "nic_addr", val, 1, sizeof(val));
 	free(nic_addr_path);
 	if (rc)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	addr = strtol(val, NULL, 16);
 	if (addr == LONG_MIN || addr == LONG_MAX)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	*nid_addr = addr;
 
@@ -1677,6 +1677,7 @@ static int lustre_lnet_intf2nids(struct lnet_dlc_network_descr *nw,
 		if (rc) {
 			snprintf(err_str, str_len,
 				 "\"cannot read gni nid\"");
+			rc = LUSTRE_CFG_RC_BAD_PARAM;
 			goto failed;
 		}
 		gni_num = atoi(val);
@@ -1735,7 +1736,7 @@ static int lustre_lnet_intf2nids(struct lnet_dlc_network_descr *nw,
 out:
 	*nnids = count;
 
-	return 0;
+	return LUSTRE_CFG_RC_NO_ERR;
 
 failed:
 	free(*nids);
@@ -1769,7 +1770,8 @@ failed:
  */
 static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 					struct list_head *intf_list,
-					struct list_head *ip_ranges)
+					struct list_head *ip_ranges,
+					char *err_str, size_t str_len)
 {
 	int rc;
 	__u32 ip;
@@ -1803,6 +1805,8 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 				return LUSTRE_CFG_RC_MATCH;
 			}
 		}
+
+		snprintf(err_str, str_len, "No UP interfaces were found");
 		return LUSTRE_CFG_RC_NO_MATCH;
 	}
 
@@ -1847,6 +1851,9 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		if (!list_empty(intf_list))
 			return LUSTRE_CFG_RC_MATCH;
 
+		snprintf(err_str, str_len,
+			 "IP pattern(s) do not match available interfaces");
+
 		return LUSTRE_CFG_RC_NO_MATCH;
 	}
 
@@ -1871,15 +1878,20 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		}
 
 		if (ifaddr == NULL) {
+			snprintf(err_str, str_len, "No interface matching '%s'",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
-			continue;
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 
 		if ((ifaddr->ifa_flags & IFF_UP) == 0) {
+			snprintf(err_str, str_len,
+				 "Matched interface '%s' is not UP",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
-			continue;
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 
 		ip = ((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr.s_addr;
@@ -1892,9 +1904,16 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		}
 
 		if (!rc) {
-			/* no match for this interface */
+			/* This interface was specified by user but does not
+			 * match any IP-range that was specified. Therefore,
+			 * this ip2nets rule doesn't apply to this node
+			 */
+			snprintf(err_str, str_len,
+				 "Interface '%s' doesn't match an IP pattern",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 	}
 
@@ -1917,10 +1936,9 @@ int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 
 	rc = lustre_lnet_match_ip_to_intf(ifa,
 					  &ip2nets->ip2nets_net.nw_intflist,
-					  &ip2nets->ip2nets_ip_ranges);
+					  &ip2nets->ip2nets_ip_ranges, err_str,
+					  str_len);
 	if (rc != LUSTRE_CFG_RC_MATCH) {
-		snprintf(err_str, str_len,
-			 "\"couldn't match ip to existing interfaces\"");
 		freeifaddrs(ifa);
 		return rc;
 	}

lnet/utils/lnetctl.c
@@ -4992,6 +4992,9 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 	char errmsg[LNET_MAX_STR_LEN];
 	bool tunables_set = false;
 	bool lnd_tunables_set = false;
+	/* Track configured networks to ensure uniqueness for ip2nets */
+	__u32 *configured_nets = NULL;
+	size_t nets_cnt = 0;
 
 	init_ip2nets_tunables(&ip2nets, &tunables);
 
@@ -5005,6 +5008,16 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 		/* Reached the end of the ip2nets sequence */
 		if (event.type == YAML_SEQUENCE_END_EVENT) {
 			yaml_event_delete(&event);
+			if (nets_cnt == 0) {
+				/* If none of the rules applied then return an
+				 * error
+				 */
+				rc = -EINVAL;
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "No ip2nets rules applied");
+				goto print_error;
+			}
+
 			goto out;
 		}
 
@@ -5014,25 +5027,41 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			char *net;
 			lnet_nid_t *nids = NULL;
 			__u32 nnids = 0;
-			char err_str[LNET_MAX_STR_LEN] = "\"success\"";
+			bool duplicate_net = false;
+			int i;
+			__u32 *tmp;
 
 			rc = lustre_lnet_resolve_ip2nets_rule(&ip2nets, &nids,
-							      &nnids, err_str,
-							      sizeof(err_str));
+							      &nnids, errmsg,
+							      sizeof(errmsg));
 			if (nids)
 				free(nids);
 
+			/* NO_MATCH is okay for ip2nets, but anything else is
+			 * an error
+			 */
 			if (rc != LUSTRE_CFG_RC_NO_ERR &&
-			    rc != LUSTRE_CFG_RC_MATCH)
+			    rc != LUSTRE_CFG_RC_NO_MATCH)
 				goto print_error;
 
-			if (list_empty(&ip2nets.ip2nets_net.nw_intflist)) {
-				snprintf(err_str, sizeof(err_str),
-					 "No interfaces match ip2nets rules");
-				rc = -ENOENT;
-				goto print_error;
+			/* Skip configuration if resolution failed */
+			if (rc != LUSTRE_CFG_RC_NO_ERR)
+				goto cleanup_block;
+
+			/* Check for duplicate networks in ip2nets sequences */
+			for (i = 0; i < nets_cnt; i++) {
+				if (configured_nets[i] ==
+				    ip2nets.ip2nets_net.nw_id) {
+					duplicate_net = true;
+					break;
+				}
 			}
 
+			/* Skip configuration if duplicate */
+			if (duplicate_net)
+				goto cleanup_block;
+
+			/* Configure the network interface */
 			net = libcfs_net2str(ip2nets.ip2nets_net.nw_id);
 			if (tunables_set || lnd_tunables_set)
 				tun = &tunables;
@@ -5040,14 +5069,29 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			rc = yaml_lnet_config_ni(net, NULL,
 						 &ip2nets.ip2nets_net, tun, -1,
 						 global_cpts, LNET_GENL_VERSION,
-						 NLM_F_CREATE, stdout);
+						 flags, stdout);
 			if (rc < 0) {
-				snprintf(err_str, sizeof(err_str),
+				snprintf(errmsg, sizeof(errmsg),
 					 "Failed to configure NI on net '%s' rc = %d",
 					 net, rc);
 				goto print_error;
 			}
 
+			/* Record successfully configured network for ip2nets */
+			tmp = realloc(configured_nets, (nets_cnt + 1) *
+				      sizeof(*configured_nets));
+
+			if (!tmp) {
+				rc = -ENOMEM;
+				snprintf(errmsg, sizeof(errmsg),
+					 "Out of memory tracking configured nets");
+				goto print_error;
+			}
+			configured_nets = tmp;
+			configured_nets[nets_cnt] = ip2nets.ip2nets_net.nw_id;
+			nets_cnt++;
+
+cleanup_block:
 			free_ip2nets_lists(&ip2nets);
 			init_ip2nets_tunables(&ip2nets, &tunables);
 			global_cpts = NULL;
@@ -5173,6 +5217,9 @@ yaml_parser_error:
 	}
 out:
 	free_ip2nets_lists(&ip2nets);
+	if (configured_nets)
+		free(configured_nets);
+
 	return (rc < 0) ? rc : 1;
 }
 

lustre/tests/sanity-lnet.sh
@@ -1949,16 +1949,25 @@ test_112() {
 }
 run_test 112 "multiple net configurations"
 
+do_import_test() {
+	reinit_dlc || return $?
+
+	cat ${1:-"$TMP/sanity-lnet-$testnum-expected.yaml"}
+
+	do_lnetctl import ${1:-"$TMP/sanity-lnet-$testnum-expected.yaml"} ||
+		error "Import failed with rc = $?"
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-actual.yaml
+
+	compare_yaml_files || return $?
+}
+
 test_150() {
 	reinit_dlc || return $?
 
 	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
 
-	local nid=$($LCTL list_nids)
-
 	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
-
-	reinit_dlc || return $?
 
 	cat <<EOF > $TMP/sanity-lnet.yaml
 ip2nets:
@@ -1969,14 +1978,294 @@ ip2nets:
         0: $(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' | sed 's/\/.*//')
 EOF
 
-	do_lnetctl import $TMP/sanity-lnet.yaml ||
-		error "import failed with rc = $?"
-
-	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-actual.yaml
-
-	compare_yaml_files || return $?
+	do_import_test "$TMP/sanity-lnet.yaml" || return $?
 }
 run_test 150 "Check import of ip2nets yaml sequence"
+
+test_151() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+
+	setup_fakeif || error "Failed to add fake IF"
+
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+	add_net "${NETTYPE}" "${FAKE_IF}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+
+	echo "Check import of ip2nets with two matching interfaces"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+        1: ${FAKE_IF}
+    ip-range:
+        0: ${if0_ip}
+        1: ${FAKE_IP}
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+
+	echo "Additional rules for same net should not change config"
+
+	cat <<EOF >> $TMP/sanity-lnet-$testnum.yaml
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${FAKE_IF}
+    ip-range:
+        0: ${FAKE_IP}
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+
+	add_net "${NETTYPE}2" "${INTERFACES[0]}"
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	echo "Additional rule for second net should apply"
+
+	cat <<EOF >> $TMP/sanity-lnet-$testnum.yaml
+  - net-spec: ${NETTYPE}2
+    interfaces:
+        0: ${INTERFACES[0]}
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+
+	cleanup_fakeif
+}
+run_test 151 "Check correct application of multiple ip2nets rules"
+
+test_152() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+	local i range
+
+	echo "Check import of ip2nets with wildcard IP patterns"
+
+	for i in {3..1}; do
+		range=$(echo "$if0_ip" | cut -d. -f1-$i)
+		for ((j = 3; j >= $i; j--)); do
+			range="${range:+$range.}*"
+		done
+
+		cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: ${range}
+EOF
+
+		do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+	done
+
+	setup_fakeif || error "Failed to add fake IF"
+
+	echo "Check match-all expression mask later rules"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: "*.*.*.*"
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+        1: ${FAKE_IF}
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+        1: ${FAKE_IF}
+    ip-range:
+        0: $if0_ip
+        1: $FAKE_IP
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+
+	cleanup_fakeif
+}
+run_test 152 "Check import of ip2nets with wildcard IP patterns"
+
+test_153() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+	local if0_net=$(echo "$if0_ip" | cut -d. -f1-3)
+	local if0_host=$(echo "$if0_ip" | cut -d. -f4)
+	local range_start=$((if0_host - 2 < 1 ? 1 : if0_host - 2))
+	local range_end=$((if0_host + 2 > 254 ? 254 : if0_host + 2))
+
+	echo "Check import of ip2nets with IP address ranges"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: ${if0_net}.[${range_start}-${range_end}]
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+}
+run_test 153 "Check import of ip2nets with IP address ranges"
+
+test_154() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+
+	setup_fakeif || error "Failed to add fake IF"
+
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+	local if0_net=$(echo "$if0_ip" | cut -d. -f1-3)
+	local if0_host=$(echo "$if0_ip" | cut -d. -f4)
+	local range_start=$((if0_host - 4 < 1 ? 1 : if0_host - 4))
+	local range_end=$((if0_host + 4 > 254 ? 254 : if0_host + 4))
+
+	(((if0_host - range_start) % 2 == 0)) || range_start=${if0_host}
+
+	echo "Check import of ip2nets with stepped IP address ranges"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: ${if0_net}.[${range_start}-${range_end}/2]
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml" || return $?
+
+	cleanup_fakeif
+}
+run_test 154 "Check import of ip2nets with stepped IP address ranges"
+
+test_155() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	reinit_dlc || return $?
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+
+	echo "Check import with no matching rules (should fail)"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: 10.20.30.40
+EOF
+
+	! do_lnetctl import "$TMP/sanity-lnet-$testnum.yaml" ||
+		error "Import should have failed"
+}
+run_test 155 "Check ip2nets import failure with no matching rules"
+
+test_156() {
+	[[ $NETTYPE == tcp* ]] || skip "Need tcp nettype"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local if0_ip=$(ip -o -4 a s ${INTERFACES[0]} | awk '{print $4}' |
+		       sed 's/\/.*//')
+	local if0_net3=$(echo "$if0_ip" | cut -d. -f1-3)
+	local if0_host=$(echo "$if0_ip" | cut -d. -f4)
+	local host2=$if0_host
+	local i
+
+	for i in {1..4}; do
+		((host2 + 2 < 255)) &&
+			host2=$((host2 + 2)) ||
+			host2=1
+		range=${range:+$range,}$host2
+		((i == 2)) && range=${range},$if0_host
+	done
+
+	echo "Check import of ip2nets with comma-separated IP list"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - net-spec: ${NETTYPE}
+    interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: ${if0_net3}.[${range}]
+EOF
+
+	do_import_test "$TMP/sanity-lnet-$testnum.yaml"
+}
+run_test 156 "Check import of ip2nets with comma-separated IP ranges"
+
+test_157() {
+	reinit_dlc || return $?
+
+	echo "Check import failure with missing net-spec"
+
+	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+ip2nets:
+  - interfaces:
+        0: ${INTERFACES[0]}
+    ip-range:
+        0: 192.168.1.1
+EOF
+
+	! do_lnetctl import "$TMP/sanity-lnet-$testnum.yaml" ||
+		error "Import should have failed with missing net-spec"
+}
+run_test 157 "Check import failure with malformed ip2nets YAML"
 
 test_199() {
 	[[ ${NETTYPE} == tcp* || ${NETTYPE} == o2ib* ]] ||