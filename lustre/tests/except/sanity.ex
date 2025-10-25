d56c025a13fcf47326ac55947dd5a91a5e4f4d1d
Alex Zhuravlev <bzzz@whamcloud.com>
2025-10-25 08:48:17 +0300

    LU-19521 tests: disable sanity/270a on old clients
    The number of blocks used by llog files has changed since the
    batched llog block allocation landed, which breaks the old test.
    
    Fixes: 1f3e4df2fb ("LU-18562 osd: llog write batch block allocation")
    
    Test-Parameters: trivial
    Test-Parameters: testlist=sanity clientversion=2.15 env=ONLY=270,HONOR_EXCEPT=true
    Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
    Change-Id: Ie6645ed197ff4c4ba96142e303ed5fe60c421449
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62041
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Reviewed-by: Jian Yu <yujian@whamcloud.com>

--- lustre/tests/except/sanity.ex
+++ lustre/tests/except/sanity.ex
@@ -1,3 +1,4 @@
 # facet op need_version             jira     space_separated_subtests
 mds1    <  v2_14_55-100-g8a84c7f9c7 LU-14927 0f
+mds1    <= CLIENT_VERSION           LU-18562 270a
 client  >  v2_15_63-134-gdacc4b6d38 LU-15963 312
