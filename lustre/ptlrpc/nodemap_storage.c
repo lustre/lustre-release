lustre/lustre-release

commit 1e94980e4eb14ba2d0f210d4f42186f38855b43a

LU-18357 ptlrpc: Add in-place modification of filesets
To modify a nodemap's fileset, i.e., its subdirectory, type, or
access permissions, the fileset needs to be removed and re-added.

This patch adds the "lctl nodemap_fileset_modify" command to modify a
fileset in-place. For example, the command

lctl nodemap_fileset_modify --name nm1 --fileset "/remote" --rename
"/othersite" --ro --alt

renames the existing read-write primary fileset "/remote" to
"/othersite", making it alternate and read-only.

A new sanity-sec test 27f is added test this command.

Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I4406906ae9fc022272b2148dd48c6401a6c02879
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58192
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_storage.c
@@ -934,7 +934,7 @@ static int nodemap_idx_fileset_fragments_del(
  * @subid: subid of the fileset fragment
  *
  * Return:
- * * true	if subid is a fileset header
+ * * true if subid is a fileset header
  */
 static bool nodemap_fileset_is_header(int subid)
 {
@@ -996,8 +996,8 @@ static int nodemap_fileset_get_subid_header(unsigned int fileset_id)
 }
 
 /**
- * nodemap_fileset_info_init() - Initializes the fileset info structure based
- * on nodemap and fileset info.
+ * nodemap_idx_fileset_info_init() - Initializes the fileset info structure
+ * based on nodemap and fileset info
  * @fset_info: fileset info structure to be initialized
  * @nm_id: nodemap id the fileset belongs to
  * @fileset: fileset name
@@ -1154,7 +1154,77 @@ out:
 }
 
 /**
- * nodemap_idx_fileset_del() - Deletes a fileset from the nodemap IAM.
+ * nodemap_idx_fileset_update_header() - updates only the header of an
+ * existing fileset on the nodemap IAM
+ * @nodemap: the nodemap to update the fileset
+ * @fset_info_old: fileset info of the fileset to be updated from
+ * @fset_info_new: fileset info of the fileset to be updated to
+ *
+ * If an error occurs during the IAM operation, an undo operation is performed.
+ * In case the undo operation fails the fileset subid range is cleared and -EIO
+ * is returned.
+ *
+ * Return:
+ * * %0 on success
+ * * %-EINVAL if invalid input parameters
+ * * %-EIO if undo operation failed, fileset is deleted
+ */
+int nodemap_idx_fileset_update_header(
+	const struct lu_nodemap *nodemap,
+	struct lu_nodemap_fileset_info *fset_info_old,
+	struct lu_nodemap_fileset_info *fset_info_new)
+{
+	struct lu_env env;
+	struct dt_object *idx;
+	int rc = 0, rc2 = 0;
+
+	ENTRY;
+
+	if (!nodemap || !fset_info_old || !fset_info_new)
+		RETURN(-EINVAL);
+
+	if (!nodemap_mgs()) {
+		if (nodemap->nm_dyn)
+			return 0;
+
+		rc = -EINVAL;
+		CERROR("%s: cannot add nodemap config to non-existing MGS: rc = %d\n",
+		       nodemap->nm_name, rc);
+		RETURN(rc);
+	}
+
+	rc = lu_env_init(&env, LCT_LOCAL);
+	if (rc != 0)
+		RETURN(rc);
+
+	idx = nodemap_mgs_ncf->ncf_obj;
+
+	rc = nodemap_idx_fileset_header_del(fset_info_old, &env, idx);
+	if (rc)
+		GOTO(out, rc);
+
+	rc = nodemap_idx_fileset_header_add(fset_info_new, &env, idx);
+	/* attempt undo */
+	if (rc) {
+		rc2 = nodemap_idx_fileset_header_add(fset_info_old, &env, idx);
+		if (rc2) {
+			CERROR("%s: Undo updating fileset header failed. Corrupt fileset is deleted. rc = %d : rc2 = %d\n",
+			       fset_info_new->nfi_fileset, rc, rc2);
+			/* undo failed. wipe the fileset and set error code */
+			rc2 = nodemap_idx_fileset_fragments_clear(
+				nodemap, &env, idx,
+				fset_info_old->nfi_subid_header);
+			rc = -EIO;
+		}
+	}
+
+out:
+	lu_env_fini(&env);
+	return rc;
+}
+
+/**
+ * nodemap_idx_fileset_del() - Deletes a fileset from the nodemap IAM
  * @nodemap: the nodemap to delete from
  * @fset_info: fileset info of the fileset to be deleted
  *
@@ -1683,15 +1753,14 @@ out:
 /**
  * nodemap_cluster_rec_fileset_alt() - Processes an alternate fileset header
  * or fragment and applies it to the nodemap
- *
  * @nodemap: nodemap to update with this fileset fragment
  * @rec: fileset fragment record
  * @fset_id: fileset id
  * @is_header: whether the record is a header
  *
  * Return:
- * * %0	on success
- * * %-ENOMEM	memory allocation failure
+ * * %0 on success
+ * * %-ENOMEM memory allocation failure
  */
 static int nodemap_cluster_rec_fileset_alt(struct lu_nodemap *nodemap,
 					   const union nodemap_rec *rec,
@@ -1744,13 +1813,12 @@ static int nodemap_capabilities_helper(struct lu_nodemap *nodemap,
 }
 
 /**
- * nodemap_fileset_subid_to_id() - Get the fileset id from the subid
- *
+ * nodemap_fileset_get_id() - Get the fileset id from the subid
  * @subid: subid of the fileset fragment
  *
  * Return:
- * * >=0	on success, representing fileset id
- * * %-EINVAL	invalid subid: subid < NODEMAP_FILESET
+ * * >=0 on success, representing fileset id
+ * * %-EINVAL invalid subid: subid < NODEMAP_FILESET
  */
 static int nodemap_fileset_get_id(int subid)
 {
@@ -1761,16 +1829,15 @@ static int nodemap_fileset_get_id(int subid)
 }
 
 /**
- * nodemap_cluster_rec_fileset() - Process a fileset fragment and apply
+ * nodemap_cluster_fileset_helper() - Process a fileset fragment and apply
  * it to the current nodemap
- *
  * @nodemap: nodemap to update with this fileset fragment
  * @rec: fileset fragment record
  * @subid: cluster idx subid of the fileset fragment
  *
  * Return:
- * * %0	on success
- * * %-EINVAL	invalid subid
+ * * %0 on success
+ * * %-EINVAL invalid subid
  */
 static int nodemap_cluster_fileset_helper(struct lu_nodemap *nodemap,
 					  const union nodemap_rec *rec,

Documentation/man8/Makefile.am
@@ -12,7 +12,9 @@ SERVER_MANFILES =				\
 	lctl-nodemap-fileset-add.8		\
 	lctl-nodemap_fileset_add.8		\
 	lctl-nodemap-fileset-del.8		\
-	lctl-nodemap_fileset_del.8
+	lctl-nodemap_fileset_del.8		\
+	lctl-nodemap-fileset-modify.8		\
+	lctl-nodemap_fileset_modify.8
 
 if MANPAGES
 man_MANS =

Documentation/man8/lctl-nodemap-fileset-add.8
@@ -97,3 +97,4 @@ filesystem package since release 2.17.0.
 .BR lustre (7),
 .BR lctl-nodemap-add (8),
 .BR lctl-nodemap-fileset-del (8),
+.BR lctl-nodemap-fileset-modify (8),

Documentation/man8/lctl-nodemap-fileset-del.8
@@ -56,3 +56,4 @@ filesystem package since release 2.17.0.
 .BR lustre (7),
 .BR lctl-nodemap-add (8),
 .BR lctl-nodemap-fileset-add (8),
+.BR lctl-nodemap-fileset-modify (8),

Documentation/man8/lctl-nodemap-fileset-modify.8
@@ -1,0 +1,107 @@
+.TH LCTL-NODEMAP_FILESET_MODIFY 8 2025-06-18 Lustre "Lustre Configuration Utilities"
+.SH NAME
+lctl-nodemap_fileset_modify \- modifies an existing fileset of a nodemap
+.SH SYNOPSIS
+.SY "lctl nodemap_fileset_modify"
+.BI --name " NODEMAP_NAME"
+.BI --fileset " SUBDIRECTORY"
+.RB [ --rename | -r
+.IR "NEW_SUBDIRECTORY" ]
+.RB [ --primary | -p ]
+.RB [ --alt | -a ]
+.RB [ --rw ]
+.RB [ --ro ]
+.YS
+.SH DESCRIPTION
+.B nodemap_fileset_modify
+modifies the properties of an existing fileset within a nodemap. The fileset
+is identified by its
+.IR SUBDIRECTORY
+path within the nodemap specified by its
+.IR NODEMAP_NAME .
+Modifying a fileset can target the fileset's path, its type (primary or
+alternate), and its access permissions (read-write or read-only). This command
+cannot move filesets between nodemaps. The
+.B --name
+and
+.B --fileset
+options are
+mandatory.
+.SH OPTIONS
+.TP
+.BR -n ", " --name " NODEMAP_NAME"
+Specifies the nodemap containing the fileset to modify.
+.TP
+.BR -f ", " --fileset " SUBDIRECTORY"
+Specifies the path of the fileset to modify.
+.TP
+.BR -r ", " --rename " NEW_SUBDIRECTORY"
+Changes the fileset's path to the specified new subdirectory. The new path
+must begin with a slash
+.RB (' / ')
+and cannot yet exist as a fileset of that type on the same nodemap.
+.TP
+.BR -p ", " --primary
+Converts the fileset to a primary fileset. The flag has no effect if the fileset
+is already primary. Note that
+.B --primary
+can only convert an alternate fileset to primary if there is currently no
+primary fileset set on the nodemap. This flag is mutually exclusive with
+.B --alt
+option.
+.TP
+.BR -a ", " --alt
+Converts the fileset to an alternate fileset. The flag has no effect if the
+fileset is already alternate. This flag is mutually exclusive with
+.B --primary
+option.
+.TP
+.B --rw
+Sets the fileset to read-write access mode. The flag has no effect if the
+fileset is already set to read-write. This flag is mutually exclusive with
+.B --ro
+option.
+.TP
+.B --ro
+Sets the fileset to read-only access mode which is enforced when the fileset
+applies for a given
+.IR SUBDIRECTORY
+at mount time. The flag has no effect if the fileset is already set to
+read-only. This flag is mutually exclusive with
+.B --rw
+option.
+.SH EXAMPLES
+Rename a fileset. A fileset is unique, therefore the name suffices:
+.EX
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --rename '/dir2'
+.EE
+.PP
+Convert a primary fileset into an alternate fileset and vice versa:
+.EX
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --alt
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --primary
+.EE
+.PP
+Change the access mode of a fileset to read-only and back to read-write:
+.EX
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --ro
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --rw
+.EE
+.PP
+Combine all options to change a fileset's path, type, and access mode. Rename
+a primary fileset '/dir1' to '/dir2', convert it to an alternate fileset, and
+set it to read-only:
+.EX
+.B # lctl nodemap_fileset_modify --name tenant1 --fileset '/dir1' --rename '/dir2' --alt --ro
+.EE
+.SH AVAILABILITY
+.B lctl nodemap_fileset_modify
+is part of the
+.BR lustre (7)
+filesystem package since release 2.17.0.
+.\" Added in commit XXX
+.SH SEE ALSO
+.BR lustre (7),
+.BR lctl-nodemap-add (8),
+.BR lctl-nodemap-fileset-add (8),
+.BR lctl-nodemap-fileset-del (8)

Documentation/man8/lctl-nodemap_fileset_modify.8
@@ -1,0 +1,1 @@
+.so man8/lctl-nodemap-fileset-modify.8

lustre/include/lustre_nodemap.h
@@ -67,6 +67,25 @@ static const struct nodemap_priv_name {
 	{ NODEMAP_RAISE_PRIV_CAPS,		"caps"	},
 };
 
+enum fileset_modify_type {
+	FSM_TYPE_NONE		= 0,
+	FSM_TYPE_PRIMARY	= 1,
+	FSM_TYPE_ALTERNATE	= 2,
+};
+
+enum fileset_modify_access {
+	FSM_ACCESS_NONE		= 0,
+	FSM_ACCESS_RO		= 1,
+	FSM_ACCESS_RW		= 2,
+};
+
+struct lu_nodemap_fileset_modify {
+	/* the renamed fileset path */
+	char				*nfm_fileset;
+	enum fileset_modify_type	nfm_type;
+	enum fileset_modify_access	nfm_access;
+};
+
 /** The nodemap id 0 will be the default nodemap. It will have a configuration
  * set by the MGS, but no ranges will be allowed as all NIDs that do not map
  * will be added to the default nodemap

lustre/include/uapi/linux/lustre/lustre_cfg.h
@@ -140,6 +140,7 @@ enum lcfg_command_type {
 	LCFG_NODEMAP_FILESET_DEL  = 0x00ce064, /**< del fileset */
 	LCFG_NODEMAP_GSS_IDENTIFY	= 0x00ce065, /**< gss identification */
 	LCFG_NODEMAP_LOOKUP_SHA		= 0x00ce066, /**< lookup nm sha */
+	LCFG_NODEMAP_FILESET_MODIFY	= 0x00ce067, /**< modify fileset */
 };
 
 struct lustre_cfg_bufs {

commit 164909a705537193f094f1435e6cc0e0104bf98b

LU-19428 nodemap: fix fileset flag modification on dyn nodemap
When the MGS and MDS are on the same node, it is not possible to set a
fileset on the dynamic nodemap to read-only/read-write because it
looked for a non-existing fileset header on the nodemap IAM.

This patch fixes this issue by recognizing dynamic nodemaps correctly
on nodemap_storage.

Fixes: 1e94980e4e ("LU-18357 ptlrpc: Add in-place modification of filesets")
Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I38104792b774c2987de08c2fa04d5b13af0cecf0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61571
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_storage.c
@@ -1200,10 +1200,10 @@ int nodemap_idx_fileset_update_header(
 	if (!nodemap || !fset_info_old || !fset_info_new)
 		RETURN(-EINVAL);
 
-	if (!nodemap_mgs()) {
-		if (nodemap->nm_dyn)
-			return 0;
+	if (nodemap->nm_dyn)
+		return 0;
 
+	if (!nodemap_mgs()) {
 		rc = -EINVAL;
 		CERROR("%s: cannot add nodemap config to non-existing MGS: rc = %d\n",
 		       nodemap->nm_name, rc);