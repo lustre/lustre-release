lustre/lustre-release

commit 928f0409dfa1273e1542418d7156853ce0c03e04

LU-18212 ioctl: LL_IOC_FID2MDTIDX on server mount point
add LL_IOC_FID2MDTIDX ioctl support on server's mount point.

Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: If3c8c96e75573b812688686a331a38250826cd05
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56339
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -8889,6 +8889,42 @@ static int osd_health_check(const struct lu_env *env, struct obd_device *obd)
 	return (osd->od_mnt == NULL || sb->s_flags & SB_RDONLY);
 }
 
+static int osd_get_info(const struct lu_env *env, struct obd_export *exp,
+			__u32 keylen, void *key, __u32 *vallen, void *val)
+{
+	struct osd_device *osd;
+	int rc = -EINVAL;
+
+	ENTRY;
+
+	if (!exp->exp_obd) {
+		CDEBUG(D_IOCTL, "invalid client export %p\n", exp);
+		RETURN(-EINVAL);
+	}
+	osd = osd_dev(exp->exp_obd->obd_lu_dev);
+
+	if (KEY_IS(KEY_FID2IDX)) {
+		struct lu_seq_range range;
+		struct lu_fid fid;
+
+		if (osd_seq_site(osd)->ss_server_fld == NULL)
+			RETURN(-EINPROGRESS);
+
+		LASSERT(*vallen = sizeof(struct lu_fid));
+		memcpy(&fid, val, sizeof(struct lu_fid));
+
+		fld_range_set_any(&range);
+
+		rc = osd_fld_lookup(env, osd, fid_seq(&fid), &range);
+		if (rc == 0) {
+			memcpy(val, &range, sizeof(range));
+			*vallen = sizeof(range);
+		}
+	}
+
+	RETURN(rc);
+}
+
 /*
  * lprocfs legacy support.
  */
@@ -8897,6 +8933,7 @@ static const struct obd_ops osd_obd_device_ops = {
 	.o_connect	= osd_obd_connect,
 	.o_disconnect	= osd_obd_disconnect,
 	.o_health_check = osd_health_check,
+	.o_get_info	= osd_get_info,
 };
 
 static ssize_t delayed_unlink_mb_show(struct kobject *kobj,

lustre/include/obd.h
@@ -853,6 +853,7 @@ static inline bool obd_mdt_recovery_abort(struct obd_device *obd)
 
 #define KEY_CACHE_LRU_SHRINK	"cache_lru_shrink"
 #define KEY_OSP_CONNECTED	"osp_connected"
+#define KEY_FID2IDX		"fid2idx"
 
 #define KEY_UNEVICT_CACHE_SHRINK	"unevict_cache_shrink"
 

lustre/target/tgt_mount.c
@@ -49,6 +49,7 @@
 #ifdef HAVE_FSMAP_H
 #include <linux/fsmap.h>
 #endif
+#include <linux/uaccess.h>
 
 #include <llog_swab.h>
 #include <lustre_disk.h>
@@ -2008,6 +2009,40 @@ static long server_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	struct file *root_filp;
 	struct inode *root_inode;
 	int err = -ENOTTY;
+
+	if (cmd == LL_IOC_FID2MDTIDX) {
+		union {
+			struct lu_seq_range range;
+			struct lu_fid fid;
+		} u;
+		struct lu_env *env;
+		int len;
+
+		if (copy_from_user(&u.fid, (struct lu_fid __user *)arg,
+				   sizeof(u.fid)))
+			RETURN(-EFAULT);
+
+		OBD_ALLOC_PTR(env);
+		if (env == NULL)
+			return -ENOMEM;
+		err = lu_env_init(env, LCT_DT_THREAD);
+		if (err)
+			GOTO(out, err = -ENOMEM);
+
+		/* XXX: check for size */
+		len = sizeof(struct lu_fid);
+		err = obd_get_info(env, lsi->lsi_osd_exp, sizeof(KEY_FID2IDX),
+				   KEY_FID2IDX, &len, &u.fid);
+		if (err == 0) {
+			err = -EINVAL;
+			if (u.range.lsr_flags & LU_SEQ_RANGE_MDT)
+				err = u.range.lsr_index;
+		}
+		lu_env_fini(env);
+out:
+		OBD_FREE_PTR(env);
+		return err;
+	}
 
 	if (!is_cmd_supported(cmd))
 		return err;

commit cdaab552abd1f95843a9c8af2186e6e902b1a0ef

LU-18212 ioctl: fix incorrect LASSERT() in osd_get_info()
The = assignment in the LASSERT() should be == instead.

However, it is better not to LASSERT() on data that may come
from userspace, so just return an error if value is too small.

CoverityID: 445224 ("Incorrect expression")
CoverityID: 445223 ("Control flow issues")

Test-Parameters: trivial
Fixes: 928f040 ("LU-18212 ioctl: LL_IOC_FID2MDTIDX on server mount point")
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I4e2f915e9cf382c349bb1a81030913a2d3dbc8d5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56585
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Rahul Bansal <rahulmay94@gmail.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -9095,7 +9095,8 @@ static int osd_get_info(const struct lu_env *env, struct obd_export *exp,
 		if (osd_seq_site(osd)->ss_server_fld == NULL)
 			RETURN(-EINPROGRESS);
 
-		LASSERT(*vallen = sizeof(struct lu_fid));
+		if (*vallen < sizeof(struct lu_fid))
+			RETURN(-EINVAL);
 		memcpy(&fid, val, sizeof(struct lu_fid));
 
 		fld_range_set_any(&range);