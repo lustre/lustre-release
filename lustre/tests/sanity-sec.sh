lustre/lustre-release

commit e8c244e81e906d77fe08bdbd761ff63343c364c4

LU-17431 nodemap: fileset inheritance for dynamic nodemap
Filesets for a dynamic nodemap are inherited from the parent nodemap.
It is possible to define a primary or alternate fileset specific to
the dynamic nodemap, but only if it is a subdirectory of the parent
nodemap's filesets, or if the parent has no filesets defined.
As we do not want to leave dynamic nodemaps without any primary
fileset defined, the only way currently available is to use
'lctl nodemap_set_fileset'. 'lctl nodemap_fileset_add/del' cannot be
used for the primary fileset of a dynamic nodemap.

Add sanity-sec test_72d to exercise this feature.

Test-Parameters: trivial
Test-Parameters: testgroup=review-dne-part-2
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I61a5fe82d92233bc66d10b8c1b939c04777bcf94
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55614
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -62,7 +62,7 @@ static void nodemap_destroy(struct lu_nodemap *nodemap)
 	up_write(&nodemap->nm_idmap_lock);
 
 	down_write(&nodemap->nm_fileset_alt_lock);
-	fileset_alt_destroy_tree(&nodemap->nm_fileset_alt);
+	fileset_alt_destroy_tree(nodemap);
 	up_write(&nodemap->nm_fileset_alt_lock);
 
 	mutex_unlock(&active_config_lock);
@@ -1396,6 +1396,114 @@ out:
 }
 EXPORT_SYMBOL(nodemap_del_range);
 
+/**
+ * check_fileset_add_vs_parent() - verify constraints on fileset add
+ * @nodemap: nodemap to check
+ * @fileset_path: fileset to apply
+ *
+ * In case the child wants to set a primary fileset:
+ * - if the parent has not defined any fileset of any type, then the child can
+ *   set any (it has access to the whole namespace after all).
+ * - if the parent has any fileset of any type defined, then the child's fileset
+ *   must be identical or a subdir of an existing fileset on the parent,
+ *   regardless of its type (to keep the namespace restriction of the parent).
+ * In case the child wants to set an alternate fileset, the same rules apply
+ * as above.
+ *
+ * Return:
+ * * %0		fileset is acceptable
+ * * %-EINVAL	dynamic nodemap without parent
+ * * %-EPERM	fileset is not acceptable
+ */
+static int check_fileset_add_vs_parent(struct lu_nodemap *nodemap,
+				       const char *fileset_path)
+{
+	int p_prim_len;
+	char *p_prim;
+
+	ENTRY;
+
+	/* Not a dynamic nodemap: no constraints on fileset */
+	if (!nodemap->nm_dyn)
+		RETURN(0);
+
+	/* A dynamic nodemap without parent: should not happen */
+	if (!nodemap->nm_parent_nm)
+		RETURN(-EINVAL);
+
+	p_prim = nodemap->nm_parent_nm->nm_fileset_prim;
+	p_prim_len = p_prim ? strlen(p_prim) : 0;
+
+	/* If parent has no fileset of any type, child can set any */
+	if (!p_prim_len && !nodemap->nm_parent_nm->nm_fileset_alt_sz)
+		RETURN(0);
+
+	/* fileset starts like parent's primary fileset, and is followed
+	 * by '/' (subdirectory) or '\0' (identical to parent):
+	 * => accepted
+	 */
+	if (p_prim && strstr(fileset_path, p_prim) == fileset_path &&
+	    (fileset_path[p_prim_len] == '/' ||
+	     fileset_path[p_prim_len] == '\0'))
+		RETURN(0);
+
+	/* fileset is identical to a parent's alt fileset, or is a subdir of it:
+	 * => accepted
+	 */
+	if (fileset_alt_search_path(&nodemap->nm_parent_nm->nm_fileset_alt,
+				    fileset_path, true))
+		RETURN(0);
+
+	/* any other condition: refused */
+	RETURN(-EPERM);
+}
+
+/**
+ * check_fileset_del_vs_parent() - verify constraints on fileset del
+ * @nodemap: nodemap to check
+ *
+ * If the parent has no filesets, then the child can deleting any fileset.
+ * If the parent has any fileset, then the child can delete all filesets except
+ * the very last one, regardless of type, as is would allow unrestricted
+ * namespace access.
+ *
+ * Return:
+ * * %0		fileset removal is accepted
+ * * %-EINVAL	dynamic nodemap without parent
+ * * %-EPERM	fileset removal is not accepted
+ */
+static int check_fileset_del_vs_parent(struct lu_nodemap *nodemap)
+{
+	int prim_len;
+	char *prim;
+
+	ENTRY;
+
+	/* Not a dynamic nodemap: no constraints on fileset */
+	if (!nodemap->nm_dyn)
+		RETURN(0);
+
+	/* A dynamic nodemap without parent: should not happen */
+	if (!nodemap->nm_parent_nm)
+		RETURN(-EINVAL);
+
+	/* If parent has no fileset of any type, child can delete any */
+	prim = nodemap->nm_parent_nm->nm_fileset_prim;
+	prim_len = prim ? strlen(prim) : 0;
+	if (!prim_len && !nodemap->nm_parent_nm->nm_fileset_alt_sz)
+		RETURN(0);
+
+	/* Do not let the last child fileset be removed. */
+	prim = nodemap->nm_fileset_prim;
+	prim_len = prim ? strlen(prim) : 0;
+	if (nodemap->nm_fileset_alt_sz >= 2 ||
+	    (nodemap->nm_fileset_alt_sz == 1 && prim_len))
+		RETURN(0);
+
+	/* any other condition: refused */
+	RETURN(-EPERM);
+}
+
 static void nodemap_fileset_init(struct lu_nodemap *nodemap)
 {
 	nodemap->nm_fileset_prim = NULL;
@@ -1403,6 +1511,7 @@ static void nodemap_fileset_init(struct lu_nodemap *nodemap)
 	nodemap->nm_fileset_prim_ro = false;
 	init_rwsem(&nodemap->nm_fileset_alt_lock);
 	nodemap->nm_fileset_alt = RB_ROOT;
+	nodemap->nm_fileset_alt_sz = 0;
 }
 
 static void nodemap_fileset_prim_reset(struct lu_nodemap *nodemap)
@@ -1439,12 +1548,27 @@ static int nodemap_update_fileset_iam_flag(struct lu_nodemap *nodemap,
 	return rc;
 }
 
-static int nodemap_fileset_del_primary(struct lu_nodemap *nodemap)
+/**
+ * nodemap_fileset_del_primary() - remove primary fileset
+ * @nodemap: nodemap to remove primary fileset from
+ * @force: true to force fileset removal
+ *
+ * Return:
+ * * %0 on success
+ * * %-EINVAL	invalid nodemap or invalid input parameters
+ * * %-EPERM	fileset removal is not permitted
+ */
+static int nodemap_fileset_del_primary(struct lu_nodemap *nodemap, bool force)
 {
-	int rc;
+	int rc = 0;
 
 	if (!nodemap)
 		RETURN(-EINVAL);
+
+	if (!force)
+		rc = check_fileset_del_vs_parent(nodemap);
+	if (rc)
+		RETURN(rc);
 
 	rc = nodemap_idx_fileset_clear(nodemap, NODEMAP_FILESET_PRIM_ID);
 	if (!rc)
@@ -1466,10 +1590,12 @@ static int nodemap_fileset_del_alternate(struct lu_nodemap *nodemap,
 
 	fset = fileset_alt_search_path(&nodemap->nm_fileset_alt, fileset_path,
 				   false);
-	if (!fset) {
-		rc = -ENOENT;
+	if (!fset)
+		GOTO(out, rc = -ENOENT);
+
+	rc = check_fileset_del_vs_parent(nodemap);
+	if (rc)
 		GOTO(out, rc);
-	}
 
 	/* delete fileset from IAM nodemap records */
 	rc = nodemap_idx_fileset_clear(nodemap, fset->nfa_id);
@@ -1477,7 +1603,7 @@ static int nodemap_fileset_del_alternate(struct lu_nodemap *nodemap,
 		GOTO(out, rc);
 
 	/* delete fileset from rb tree and free memory */
-	rc = fileset_alt_delete(&nodemap->nm_fileset_alt, fset);
+	rc = fileset_alt_delete(nodemap, fset);
 	if (rc > 0)
 		rc = 0;
 
@@ -1508,7 +1634,7 @@ static int nodemap_fileset_del(struct lu_nodemap *nodemap,
 	/* attempt to delete from primary fileset first */
 	if (nodemap->nm_fileset_prim &&
 	    strcmp(nodemap->nm_fileset_prim, fileset_path) == 0)
-		return nodemap_fileset_del_primary(nodemap);
+		return nodemap_fileset_del_primary(nodemap, false);
 
 	return nodemap_fileset_del_alternate(nodemap, fileset_path);
 }
@@ -1517,12 +1643,13 @@ static int nodemap_fileset_del(struct lu_nodemap *nodemap,
  * nodemap_fileset_clear() - Deletes all types of filesets from the nodemap
  *
  * @nodemap: nodemap to clear filesets from
+ * @force: true to force fileset clear
  *
  * Return:
  * * %0 on success
- * * %-EINVAL	nodemap is NULL
+ * * %-EINVAL	nodemap is NULL, or dyn nm is not allowed to wipe all filesets
  */
-static int nodemap_fileset_clear(struct lu_nodemap *nodemap)
+static int nodemap_fileset_clear(struct lu_nodemap *nodemap, bool force)
 {
 	struct lu_fileset_alt *fileset;
 	struct rb_node *node;
@@ -1531,7 +1658,17 @@ static int nodemap_fileset_clear(struct lu_nodemap *nodemap)
 	if (!nodemap)
 		RETURN(-EINVAL);
 
-	rc = nodemap_fileset_del_primary(nodemap);
+	if (!force && nodemap->nm_dyn) {
+		/* If parent has any fileset (any type), child cannot wipe
+		 * all filesets.
+		 */
+		if ((nodemap->nm_parent_nm->nm_fileset_prim &&
+		     strlen(nodemap->nm_parent_nm->nm_fileset_prim)) ||
+		    nodemap->nm_parent_nm->nm_fileset_alt_sz)
+			RETURN(-EINVAL);
+	}
+
+	rc = nodemap_fileset_del_primary(nodemap, force);
 	if (rc) {
 		CERROR("%s: failed to clear prim fileset: rc = %d\n",
 		       nodemap->nm_name, rc);
@@ -1549,7 +1686,7 @@ static int nodemap_fileset_clear(struct lu_nodemap *nodemap)
 			       nodemap->nm_name, fileset->nfa_path, rc);
 		}
 	}
-	fileset_alt_destroy_tree(&nodemap->nm_fileset_alt);
+	fileset_alt_destroy_tree(nodemap);
 	up_write(&nodemap->nm_fileset_alt_lock);
 
 	return 0;
@@ -1584,6 +1721,10 @@ static int nodemap_fileset_add_primary(struct lu_nodemap *nodemap,
 	up_read(&nodemap->nm_fileset_alt_lock);
 	if (fset_alt_exists)
 		RETURN(-EEXIST);
+
+	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
+	if (rc)
+		RETURN(rc);
 
 	fileset_size = strlen(fileset_path) + 1;
 
@@ -1636,12 +1777,16 @@ static int nodemap_fileset_add_alternate(struct lu_nodemap *nodemap,
 		GOTO(out, rc = -EEXIST);
 	}
 
+	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
+	if (rc)
+		GOTO(out, rc);
+
 	fset = fileset_alt_create(fileset_path, read_only);
 	if (!fset)
 		GOTO(out, rc = -ENOMEM);
 
 	/* add fileset to in-memory rb tree */
-	rc = fileset_alt_add(&nodemap->nm_fileset_alt, fset);
+	rc = fileset_alt_add(nodemap, fset);
 	if (rc) {
 		fileset_alt_destroy(fset);
 		GOTO(out, rc);
@@ -1654,7 +1799,7 @@ static int nodemap_fileset_add_alternate(struct lu_nodemap *nodemap,
 	rc = nodemap_idx_fileset_add(nodemap, &fset_info);
 	if (rc) {
 		/* remove added fileset from rb tree on IAM error */
-		rc2 = fileset_alt_delete(&nodemap->nm_fileset_alt, fset);
+		rc2 = fileset_alt_delete(nodemap, fset);
 		if (rc2 < 0)
 			CERROR("%s: Undo adding fileset '%s' failed. rc = %d : rc2 = %d\n",
 			       nodemap->nm_name, fset->nfa_path, rc, rc2);
@@ -1738,7 +1883,7 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 		RETURN(-EINVAL);
 
 	if (fileset_path[0] == '\0' || strcmp(fileset_path, "clear") == 0) {
-		rc = nodemap_fileset_del_primary(nodemap);
+		rc = nodemap_fileset_del_primary(nodemap, false);
 		if (!rc && !nodemap->nm_dyn && out_clean_llog_fileset)
 			*out_clean_llog_fileset = true;
 		GOTO(out, rc);
@@ -1752,6 +1897,10 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 		rc = nodemap_fileset_add_primary(nodemap, fileset_path, false);
 		GOTO(out, rc);
 	}
+
+	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
+	if (rc)
+		RETURN(rc);
 
 	fileset_size = strlen(fileset_path) + 1;
 
@@ -1824,7 +1973,7 @@ static int nodemap_set_fileset_prim_llog(struct lu_nodemap *nodemap,
 {
 	size_t fileset_size_new;
 	char *fileset_new;
-	int rc;
+	int rc = 0;
 
 	/* Abort if the IAM is already in use and a fileset is set */
 	if (nodemap->nm_fileset_prim && nodemap->nmf_fileset_use_iam)
@@ -1840,6 +1989,10 @@ static int nodemap_set_fileset_prim_llog(struct lu_nodemap *nodemap,
 	 * 'fileset=""' is still kept for compatibility reason.
 	 */
 	if (fileset_path[0] == '\0' || strcmp(fileset_path, "clear") == 0) {
+		rc = check_fileset_del_vs_parent(nodemap);
+		if (rc)
+			RETURN(rc);
+
 		nodemap_fileset_prim_reset(nodemap);
 		/* back to default value for use_iam flag */
 		rc = nodemap_update_fileset_iam_flag(nodemap, true);
@@ -1848,6 +2001,10 @@ static int nodemap_set_fileset_prim_llog(struct lu_nodemap *nodemap,
 
 	if (fileset_path[0] != '/')
 		RETURN(-EINVAL);
+
+	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
+	if (rc)
+		RETURN(rc);
 
 	fileset_size_new = strlen(fileset_path) + 1;
 
@@ -1917,7 +2074,7 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
 out_unlock:
 	up_read(&src->nm_fileset_alt_lock);
 	if (rc)
-		nodemap_fileset_clear(dst);
+		nodemap_fileset_clear(dst, true);
 
 out:
 	return rc;
@@ -1964,12 +2121,6 @@ int nodemap_set_fileset_prim_lproc(const char *nodemap_name,
 		RETURN(PTR_ERR(nodemap));
 	}
 
-	/* FIXME: for now the fileset on a dynamic nodemap can just be
-	 * inherited from the parent, not set explicitly
-	 */
-	if (nodemap->nm_dyn)
-		GOTO(out_unlock, rc = -EPERM);
-
 	if (checkperm && !allow_op_on_nm(nodemap))
 		GOTO(out_unlock, rc = -ENXIO);
 
@@ -1999,13 +2150,10 @@ EXPORT_SYMBOL(nodemap_set_fileset_prim_lproc);
  * @nodemap: nodemap to get fileset from
  *
  * Return:
- * * fileset name, or NULL if not defined or not activated
+ * * fileset name, or NULL if not defined
  */
 char *nodemap_get_fileset_prim(const struct lu_nodemap *nodemap)
 {
-	if (!nodemap_active)
-		return NULL;
-
 	return (char *)nodemap->nm_fileset_prim;
 }
 EXPORT_SYMBOL(nodemap_get_fileset_prim);
@@ -2111,9 +2259,13 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 
 	/* 3. check if any fileset exists that matches fileset_src */
 	if (nodemap->nm_fileset_prim && nodemap->nm_fileset_prim[0] != '\0') {
-		rc = strncmp(nodemap->nm_fileset_prim, fileset_src,
-			     strlen(nodemap->nm_fileset_prim));
-		if (!rc) {
+		/* fileset_src starts like primary fileset, and is followed
+		 * by '/' (subdirectory) or '\0' (identical)
+		 */
+		if (strstr(fileset_src, nodemap->nm_fileset_prim)
+		      == fileset_src &&
+		    (fileset_src[strlen(nodemap->nm_fileset_prim)] == '/' ||
+		     fileset_src[strlen(nodemap->nm_fileset_prim)] == '\0')) {
 			rc = strscpy(fset, fileset_src, fset_size);
 			if (rc < 0)
 				GOTO(out, rc = -ENAMETOOLONG);
@@ -2271,7 +2423,7 @@ EXPORT_SYMBOL(nodemap_set_sepol);
  * nodemap_get_sepol() - get SELinux policy info defined on nodemap
  * @nodemap: nodemap to get SELinux policy info from
  *
- * Returns SELinux policy info, or NULL if not defined or not activated
+ * Returns SELinux policy info, or NULL if not defined
  */
 const char *nodemap_get_sepol(const struct lu_nodemap *nodemap)
 {
@@ -2478,6 +2630,7 @@ struct lu_nodemap *nodemap_create(const char *name,
 	nodemap->nm_client_to_fs_projidmap = RB_ROOT;
 
 	nodemap->nm_dyn = dynamic;
+	nodemap->nm_parent_nm = parent_nodemap;
 	if (!parent_nodemap)
 		rc = nodemap_inherit_properties(nodemap,
 					   is_default ? NULL : default_nodemap);
@@ -2495,7 +2648,6 @@ struct lu_nodemap *nodemap_create(const char *name,
 	INIT_LIST_HEAD(&nodemap->nm_member_list);
 	INIT_LIST_HEAD(&nodemap->nm_subnodemaps);
 	INIT_LIST_HEAD(&nodemap->nm_parent_entry);
-	nodemap->nm_parent_nm = parent_nodemap;
 	if (parent_nodemap)
 		list_add(&nodemap->nm_parent_entry,
 			 &parent_nodemap->nm_subnodemaps);
@@ -3173,7 +3325,7 @@ int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset)
 	if (nodemap->nm_fileset_prim)
 		fileset_prim_exists = true;
 
-	rc2 = nodemap_fileset_clear(nodemap);
+	rc2 = nodemap_fileset_clear(nodemap, true);
 	if (rc2)
 		rc = rc2;
 
@@ -3811,7 +3963,7 @@ static int cfg_nodemap_fileset_cmd(struct lustre_cfg *lcfg,
 		break;
 	case LCFG_NODEMAP_FILESET_DEL:
 		if (fset[0] == '*')
-			rc = nodemap_fileset_clear(nodemap);
+			rc = nodemap_fileset_clear(nodemap, false);
 		else
 			rc = nodemap_fileset_del(nodemap, fset);
 		break;

lustre/tests/sanity-sec.sh
@@ -7921,7 +7921,6 @@ run_test 71 "encryption does not remove project flag"
 
 dyn_nm_helper() {
 	local facet=$1
-	local have_multi_filesets=$2
 	local mgsnm=mgsnm
 	local mgsnids=1.1.0.[1-100]@tcp
 	local mgsnids2=1.0.0.[1-100]@tcp
@@ -8069,32 +8068,6 @@ dyn_nm_helper() {
 			error "dynamic modify of $prop failed"
 	val=$(do_facet $facet $LCTL get_param -n nodemap.$nm.$prop)
 	(( val == 77 )) || error "incorrect $prop $val"
-	prop=fileset
-	do_facet $facet $LCTL nodemap_set_fileset --name $nm \
-		--fileset "/tmp" ||
-			error "dynamic modify of primary $prop failed"
-	if $have_multi_filesets; then
-		val=$(do_facet_check_fileset $facet $nm "/tmp" "primary")
-		[[ "$val" == "/tmp" ]] || error "incorrect primary $prop $val"
-		do_facet $facet $LCTL nodemap_set_fileset --name $nm \
-			--fileset "clear" ||
-			error "dynamic clear primary $prop failed"
-		val=$(do_facet_check_fileset $facet $nm "" "primary")
-		[[ -z $val ]] || error "primary $prop should be empty"
-		do_facet $facet $LCTL nodemap_fileset_add --name $nm \
-			--fileset "/alttmp" --alt ||
-			error "dynamic modify of alt $prop failed"
-		val=$(do_facet_check_fileset $facet $nm "/alttmp" "alternate")
-		[[ "$val" == "/alttmp" ]] || error "incorrect alt $prop $val"
-		do_facet $facet $LCTL nodemap_fileset_del --name $nm \
-			--fileset "/alttmp" ||
-			error "dynamic clear alt $prop failed"
-		val=$(do_facet_check_fileset $facet $nm "" "alternate")
-		[[ -z $val ]] || error "alternate $prop should be empty"
-	else
-		val=$(do_facet $facet $LCTL get_param -n nodemap.$nm.$prop)
-		[[ "$val" == "/deffset" ]] || error "incorrect $prop $val"
-	fi
 
 	prop=sepol
 	do_facet $facet $LCTL nodemap_set_sepol --name $nm \
@@ -8212,31 +8185,21 @@ dyn_nm_helper() {
 }
 
 test_72a() {
-	local have_multi_filesets=false
-
-	(( OST1_VERSION >= $(version_code 2.15.64) )) ||
-		skip "Need MDS >= 2.15.64 dynamic nodemaps"
-
-	(( OST1_VERSION >= $(version_code 2.16.56) )) &&
-		have_multi_filesets=true
+	(( OST1_VERSION >= $(version_code 2.16.54) )) ||
+		skip "Need OSS >= 2.16.54 dynamic nodemaps"
 
 	[[ "$(facet_active_host mgs)" != "$(facet_active_host ost1)" ]] ||
 		skip "Need servers on different hosts"
 
-	dyn_nm_helper ost1 $have_multi_filesets
+	dyn_nm_helper ost1
 }
 run_test 72a "dynamic nodemap properties on OSS"
 
 test_72b() {
-	local have_multi_filesets=false
+	(( MDS1_VERSION >= $(version_code 2.16.54) )) ||
+		skip "Need MDS >= 2.16.54 dynamic nodemaps"
 
-	(( MDS1_VERSION >= $(version_code 2.15.64) )) ||
-		skip "Need MDS >= 2.15.64 dynamic nodemaps"
-
-	(( MDS1_VERSION >= $(version_code 2.16.56) )) &&
-		have_multi_filesets=true
-
-	dyn_nm_helper mds1 $have_multi_filesets
+	dyn_nm_helper mds1
 }
 run_test 72b "dynamic nodemap properties on MDS"
 
@@ -8245,8 +8208,8 @@ test_72c() {
 	local nm=nm_test72c
 	local val
 
-	(( MDS1_VERSION >= $(version_code 2.16.52) )) ||
-		skip "Need MDS >= 2.16.52 dynamic nodemaps"
+	(( MDS1_VERSION >= $(version_code 2.16.54) )) ||
+		skip "Need MDS >= 2.16.54 dynamic nodemaps"
 
 	do_facet mgs $LCTL nodemap_add $mgsnm ||
 		error "adding $mgsnm on MGS failed"
@@ -8354,6 +8317,162 @@ test_72c() {
 	do_facet mds1 $LCTL get_param -R nodemap.*
 }
 run_test 72c "child_raise_privileges nodemap property"
+
+cleanup_72d() {
+	local nm_name=${1:-"c0"}
+	local dynnm1=${2:-"dyn1"}
+	local dynnm2=${3:-"dyn2"}
+
+	do_facet ost1 $LCTL nodemap_del $dynnm1 || true
+	do_facet ost1 $LCTL nodemap_del $dynnm2 || true
+	do_facet mgs $LCTL nodemap_del $nm_name
+
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property admin --value 0
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property trusted --value 0
+	wait_nm_sync default trusted_nodemap
+}
+
+test_72d() {
+	local mgsnm=mgsnm
+	local nm=nm_test72d
+	local nm2=subnm_test72d
+	local val
+
+	(( OST1_VERSION >= $(version_code 2.16.56) )) ||
+		skip "Need OSS >= 2.16.56 multiple fileset"
+
+	[[ "$(facet_active_host mgs)" != "$(facet_active_host ost1)" ]] ||
+		skip "Need servers on different hosts"
+
+	stack_trap "cleanup_72d $mgsnm $nm $nm2" EXIT
+
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property admin --value 1
+	do_facet mgs $LCTL nodemap_modify --name default \
+		--property trusted --value 1
+	wait_nm_sync default trusted_nodemap
+
+	do_facet mgs $LCTL nodemap_add $mgsnm ||
+		error "adding $mgsnm on MGS failed"
+	wait_nm_sync $mgsnm id '' inactive
+
+	do_facet ost1 $LCTL nodemap_add -d -p $mgsnm $nm ||
+		error "dynamic nodemap on server failed (1)"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.id)
+	if [[ -z "$val" || "$val" == "0" ]]; then
+		error "dynamic nodemap wrong id $val (1)"
+	fi
+	val=$(do_facet_check_fileset ost1 $nm "" "primary")
+	[[ "$val" == "" ]] ||
+		error "$nm should have empty fileset (1)"
+
+	local filesetp="/subdir"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $filesetp ||
+		error "dynamic modify fileset to $filesetp failed"
+	val=$(do_facet_check_fileset ost1 $nm "$filesetp" "primary")
+	[[ "$val" == "$filesetp" ]] ||
+		error "$nm should have fileset $filesetp (1)"
+
+	do_facet ost1 $LCTL nodemap_fileset_del --name $nm \
+		--fileset $filesetp ||
+		error "dynamic del fileset $filesetp failed"
+	val=$(do_facet_check_fileset ost1 $nm "" "primary")
+	[[ "$val" == "" ]] ||
+		error "$nm should have empty fileset (2)"
+
+	do_facet mgs $LCTL nodemap_set_fileset --name $mgsnm \
+		--fileset $filesetp ||
+		error "modify fileset for $mgsnm on MGS failed"
+	wait_nm_sync $mgsnm fileset '' inactive
+
+	do_facet ost1 $LCTL nodemap_add -d -p $mgsnm $nm ||
+		error "dynamic nodemap on server failed (2)"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.id)
+	if [[ -z "$val" || "$val" == "0" ]]; then
+		error "dynamic nodemap wrong id $val (1)"
+	fi
+	val=$(do_facet_check_fileset ost1 $nm "$filesetp" "primary")
+	[[ "$val" == "$filesetp" ]] ||
+		error "$nm should have fileset $filesetp (2)"
+
+	local fileset1="/sub"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset1 &&
+		error "dynamic modify fileset to $fileset1 should fail"
+	val=$(do_facet_check_fileset ost1 $nm "$filesetp" "primary")
+	[[ "$val" == "$filesetp" ]] ||
+		error "$nm should have fileset $filesetp (3)"
+
+	local fileset2="/subdirother"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset2 &&
+		error "dynamic modify fileset to $fileset2 should fail"
+	val=$(do_facet_check_fileset ost1 $nm "$filesetp" "primary")
+	[[ "$val" == "$filesetp" ]] ||
+		error "$nm should have fileset $filesetp (4)"
+
+	local fileset3="/subdir/mydir"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset3 ||
+		error "dynamic modify fileset to $fileset3 failed (1)"
+	val=$(do_facet_check_fileset ost1 $nm "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm should have fileset $fileset3 (1)"
+
+	do_facet ost1 $LCTL nodemap_fileset_del --name $nm \
+		--fileset $fileset3 &&
+		error "dynamic del fileset $fileset3 should fail"
+	val=$(do_facet_check_fileset ost1 $nm "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm should have fileset $fileset3 (2)"
+
+	do_facet ost1 $LCTL nodemap_add -d -p $nm $nm2 ||
+		error "dynamic nodemap on server failed (3)"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm2.id)
+	if [[ -z "$val" || "$val" == "0" ]]; then
+		error "dynamic nodemap wrong id $val (2)"
+	fi
+	val=$(do_facet_check_fileset ost1 $nm2 "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm2 should have fileset $fileset3 (1)"
+
+	local fileset4="/subdir/myd"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm2 \
+		--fileset $fileset4 &&
+		error "dynamic modify fileset to $fileset4 should fail"
+	val=$(do_facet_check_fileset ost1 $nm2 "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm2 should have fileset $fileset3 (2)"
+
+	local fileset5="/subdir/mydirother"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm2 \
+		--fileset $fileset5 &&
+		error "dynamic modify fileset to $fileset5 should fail"
+	val=$(do_facet_check_fileset ost1 $nm2 "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm2 should have fileset $fileset3 (3)"
+
+	local fileset6="/subdir/mydir/dir2"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm2 \
+		--fileset $fileset6 ||
+		error "dynamic modify fileset to $fileset6 failed"
+	val=$(do_facet_check_fileset ost1 $nm2 "$fileset6" "primary")
+	[[ "$val" == "$fileset6" ]] ||
+		error "$nm2 should have fileset $fileset6"
+
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm2 \
+		--fileset $fileset3 ||
+		error "dynamic modify fileset to $fileset3 failed (2)"
+	val=$(do_facet_check_fileset ost1 $nm2 "$fileset3" "primary")
+	[[ "$val" == "$fileset3" ]] ||
+		error "$nm2 should have fileset $fileset3"
+
+	do_facet ost1 $LCTL get_param -R nodemap.*
+}
+run_test 72d "fileset inheritance for dynamic nodemap"
 
 test_73() {
 	local vaultdir1=$DIR/$tdir/vault1

lustre/include/lustre_nodemap.h
@@ -134,6 +134,8 @@ struct lu_nodemap {
 	struct rw_semaphore	 nm_fileset_alt_lock;
 	/* alternate fileset map */
 	struct rb_root		 nm_fileset_alt;
+	/* alternate fileset map size (# elements) */
+	unsigned int		 nm_fileset_alt_sz;
 	/* information about the expected SELinux policy on the nodes */
 	char			 nm_sepol[LUSTRE_NODEMAP_SEPOL_LENGTH + 1];
 	/* used when loading/unloading nodemaps */

commit b32f37a0f64d22cfbb797c49a30474d873f8ab2e

LU-19426 nodemap: check parent when setting fileset
When a parent nodemap only has alternate filesets set, then child
dynamic nodemaps can set any primary fileset even if it is outside the
parent's namespace restrictions by using "lctl nodemap_set_fileset".

This patch fixes this by repositioning "check_fileset_add_vs_parent()"
when setting a fileset to stay within namespace defined by the
parent's filesets.

Fixes: e8c244e81e ("LU-17431 nodemap: fileset inheritance for dynamic nodemap")
Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I392ca834f30d38ac1b32e8fa7c713f56d295b1e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61554
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -2603,15 +2603,15 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 	if (fileset_path[0] != '/')
 		RETURN(-EINVAL);
 
+	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
+	if (rc)
+		RETURN(rc);
+
 	/* if fileset is not set, add it instead */
 	if (!nodemap->nm_fileset_prim) {
 		rc = nodemap_fileset_add_primary(nodemap, fileset_path, false);
 		GOTO(out, rc);
 	}
-
-	rc = check_fileset_add_vs_parent(nodemap, fileset_path);
-	if (rc)
-		RETURN(rc);
 
 	fileset_size = strlen(fileset_path) + 1;
 

lustre/tests/sanity-sec.sh
@@ -2322,7 +2322,7 @@ wait_nm_sync_fileset() {
 			print \$4 }'"
 	fi
 
-	wait_nm_sync $nm fileset "$fset_expected" "" "$awk_expr"
+	wait_nm_sync $nm fileset "$fset_expected" "inactive" "$awk_expr"
 }
 
 wait_nm_sync_fileset_cleared() {
@@ -8742,16 +8742,57 @@ test_72d() {
 		error "dynamic modify fileset to $fileset6 failed"
 	val=$(do_facet_check_fileset ost1 $nm2 "$fileset6" "primary")
 	[[ "$val" == "$fileset6" ]] ||
-		error "$nm2 should have fileset $fileset6"
+		error "$nm2 should have fileset $fileset6 (4)"
 
 	do_facet ost1 $LCTL nodemap_set_fileset --name $nm2 \
 		--fileset $fileset3 ||
 		error "dynamic modify fileset to $fileset3 failed (2)"
 	val=$(do_facet_check_fileset ost1 $nm2 "$fileset3" "primary")
 	[[ "$val" == "$fileset3" ]] ||
-		error "$nm2 should have fileset $fileset3"
+		error "$nm2 should have fileset $fileset3 (5)"
 
 	do_facet ost1 $LCTL get_param -R nodemap.*
+
+	# tests for LU-19426 below. Fix not included in 2.16.57
+	(( $MDS1_VERSION >= $(version_code 2.16.58) )) ||
+		return 0
+
+	do_facet ost1 $LCTL nodemap_del $nm ||
+		error "removing dynamic nodemap on server failed"
+	do_facet mgs $LCTL nodemap_fileset_del --name $mgsnm --all ||
+		error "deleting fileset for $mgsnm on MGS failed"
+	do_facet mgs $LCTL nodemap_fileset_add --name $mgsnm \
+		--fileset $filesetp --alt ||
+		error "adding alt fileset for $mgsnm on MGS failed"
+	wait_nm_sync_fileset $mgsnm "$filesetp" "$filesetp" "alternate"
+
+	do_facet ost1 $LCTL nodemap_add -d -p $mgsnm $nm ||
+		error "dynamic nodemap on server failed (4)"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.id)
+	if [[ -z "$val" || "$val" == "0" ]]; then
+		error "dynamic nodemap wrong id $val (3)"
+	fi
+	val=$(do_facet_check_fileset ost1 $nm "$filesetp" "alternate")
+	[[ "$val" == "$filesetp" ]] ||
+		error "$nm should have fileset $filesetp (6)"
+	local fileset7="/outsidedir"
+	# attempt to set fileset outside parent's namespace restrictions.
+	# previously succeeded and fixed by LU-19426
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset7 &&
+		error "dyn set fileset to $fileset7 should fail (out-of-bounds)"
+	do_facet ost1 $LCTL nodemap_fileset_add --name $nm \
+		--fileset $fileset7 &&
+		error "dyn add fileset to $fileset7 should fail (out-of-bounds)"
+	local fileset8="/subdir/mydir"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset8 ||
+		error "dyn set prim fileset to $fileset8 failed"
+	do_facet ost1 $LCTL nodemap_set_fileset --name $nm --fileset 'clear' ||
+		error "remove prim fileset failed"
+	do_facet ost1 $LCTL nodemap_fileset_add --name $nm \
+		--fileset $fileset8 ||
+		error "dyn add prim fileset to $fileset8 failed"
 }
 run_test 72d "fileset inheritance for dynamic nodemap"
 