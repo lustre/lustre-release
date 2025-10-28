lustre/lustre-release

commit 74480d02bedde802af510b7c724fab47ea2edb5b

LU-19200 lnet: Support numa in jt_import
Add support for the numa sequence to jt_import and associated netlink
code.

Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I9c4150cdb3576e865f278854b217cb43b1a77cdc
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60311
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/lnet/api-ni.c
@@ -10032,6 +10032,56 @@ report_error:
 	RETURN(rc);
 }
 
+static int lnet_numa_cmd(struct sk_buff *skb, struct genl_info *info)
+{
+	struct nlmsghdr *nlh = nlmsg_hdr(skb);
+	struct genlmsghdr *gnlh = nlmsg_data(nlh);
+	struct nlattr *params = genlmsg_data(gnlh);
+	struct netlink_ext_ack *extack = NULL;
+	int msg_len, rem, rc = 0;
+	struct nlattr *entry;
+	int range;
+
+	ENTRY;
+#ifdef HAVE_NL_PARSE_WITH_EXT_ACK
+	extack = info->extack;
+#endif
+	msg_len = genlmsg_len(gnlh);
+	if (!msg_len) {
+		GENL_SET_ERR_MSG(info, "no configuration");
+		RETURN(-ENOMSG);
+	}
+
+	if (!(nla_type(params) & LN_SCALAR_ATTR_LIST)) {
+		GENL_SET_ERR_MSG(info, "invalid configuration");
+		RETURN(-EINVAL);
+	}
+
+	nla_for_each_attr(entry, params, msg_len, rem) {
+		u64 tmp;
+
+		CDEBUG(D_NET, "attr type: %d\n", nla_type(entry));
+		if (nla_type(entry) != LN_SCALAR_ATTR_VALUE)
+			continue;
+
+		if (nla_strcmp(entry, "range") == 0) {
+			rc = nla_extract_val(&entry, &rem,
+					     LN_SCALAR_ATTR_INT_VALUE,
+					     &tmp, sizeof(tmp), extack);
+			if (rc < 0)
+				GOTO(report_error, rc);
+			range = tmp;
+		}
+	}
+
+	lnet_net_lock(LNET_LOCK_EX);
+	lnet_numa_range = range;
+	lnet_net_unlock(LNET_LOCK_EX);
+
+report_error:
+	RETURN(rc);
+}
+
 static const struct genl_multicast_group lnet_mcast_grps[] = {
 	{ .name	=	"ip2net",	},
 	{ .name =	"net",		},
@@ -10044,6 +10094,7 @@ static const struct genl_multicast_group lnet_mcast_grps[] = {
 	{ .name =	"fault",	},
 	{ .name =	"routing",	},
 	{ .name =	"buffers",	},
+	{ .name =	"numa",	},
 };
 
 static const struct genl_ops lnet_genl_ops[] = {
@@ -10157,6 +10208,11 @@ static const struct genl_ops lnet_genl_ops[] = {
 		.cmd		= LNET_CMD_BUFFERS,
 		.flags		= GENL_ADMIN_PERM,
 		.doit		= lnet_buffers_cmd,
+	},
+	{
+		.cmd		= LNET_CMD_NUMA,
+		.flags		= GENL_ADMIN_PERM,
+		.doit		= lnet_numa_cmd,
 	},
 };
 

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -63,6 +63,7 @@ enum lnet_commands {
 	LNET_CMD_FAULT		= 12,
 	LNET_CMD_ROUTING	= 13,
 	LNET_CMD_BUFFERS	= 14,
+	LNET_CMD_NUMA		= 15,
 
 	__LNET_CMD_MAX_PLUS_ONE
 };

lnet/utils/lnetctl.c
@@ -4365,6 +4365,7 @@ static const struct command_mapping cmd_mappings[] = {
 	{"ping", LNET_CMD_PING, NLM_F_DUMP, true},
 	{"routing", LNET_CMD_ROUTING, 0, false},
 	{"buffers", LNET_CMD_BUFFERS, 0, false},
+	{"numa", LNET_CMD_NUMA, 0, false},
 	{NULL, 0, 0, false}
 };
 

commit 5cb13c7b36d796b1a56d834ca1bbb1de9d6aff6f

LU-17000 lnet: Initialize 'range' to zero before use
Initilize 'range' to 0. The code will safely return
-EINVAL in case of wrong/bad range. This initialization
to default is necessary to suppress Coverity warning.

CoverityID: 490484 ("Uninitialized scalar variable ")
Fixes: 74480d02b ("LU-19200 lnet: Support numa in jt_import")
Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Idabfb11b15e650366876ea7662d7b12bc57a404f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61794
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/lnet/api-ni.c
@@ -10100,7 +10100,7 @@ static int lnet_numa_cmd(struct sk_buff *skb, struct genl_info *info)
 	struct netlink_ext_ack *extack = NULL;
 	int msg_len, rem, rc = 0;
 	struct nlattr *entry;
-	int range;
+	int range = 0; /* default range */
 
 	ENTRY;
 #ifdef HAVE_NL_PARSE_WITH_EXT_ACK