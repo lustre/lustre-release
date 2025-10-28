lustre/lustre-release

commit bba59b1287c9cd8c30a85fafb4fd5788452bd05c

LU-16651 llite: hold invalidate_lock when invalidate cache pages
The newer kernel (such as Ubuntu 2204) introduces a new member:
invalidate_lock in the structure @address_space.
The filesystem must exclusively acquire invalidate_lock before
invalidating page cache in truncate / hole punch (and thus calling
into ->invalidatepage) to block races between page cache
invalidation and page cache filling functions (fault, read, ...)

However, current Lustre client does not hold this lock when remove
pages from page cache caused by the revocation of the extent DLM
lock protecting them.
If a client has two overlapped PR DLM extent locks, i.e:
- L1 = <PR, [1M, 4M - 1]
- L2 = <PR, [3M, 5M - 1]
A reader process holds L1 and reads data in range [3M, 4M - 1].
L2 is being revoken due to the conflict access.
Then the page read-in by the reader may be invalidated and deleted
from page cache by the revocation of L2 (in lock blocking AST).

The older kernel will check each page after read whether it was
invalidated and deleted from page cache. If so, it will retry the
page read.

In the newer kernel, it removes this check and retry.
Instead, it introduces a new rw_semaphore in the address_space -
invalidate_lock - that holding the shared lock to protect adding
of pages to page cache for page faults / reads / readahead, and
the exclusive lock to protect invalidating pages, removing them
from page cache for truncate / hole punch.

Thus, in this patch it holds exclusive invalidate_lock in newer
kernels when remove pages from page cache caused by the revocation
of a extent DLM lock protecting them. Otherwsie, it will result in
-EIO error or partial reads in the new added test case sanity/833.

Test-parameters: clientdistro=ubuntu2204 testlist=sanity env=ONLY=833,ONLY_REPEAT=10
Change-Id: If3a27002b89636b9fd4d7b5ea50afa9aeac5d121
Signed-off-by: Qian Yingjin <qian@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50371
Tested-by: jenkins <devops@whamcloud.com>

lustre/include/cl_object.h
@@ -1925,7 +1925,16 @@ struct cl_io {
 	/**
 	 * io_uring direct IO with flags IOCB_NOWAIT.
 	 */
-			     ci_iocb_nowait:1;
+			     ci_iocb_nowait:1,
+	/**
+	 * The filesystem must exclusively acquire invalidate_lock before
+	 * invalidating page cache in truncate / hole punch / DLM extent
+	 * lock blocking AST path (and thus calling into ->invalidatepage)
+	 * to block races between page cache invalidation and page cache
+	 * filling functions (fault, read, ...)
+	 */
+			     ci_invalidate_page_cache:1;
+
 	/**
 	 * How many times the read has retried before this one.
 	 * Set by the top level and consumed by the LOV.

lustre/llite/vvp_io.c
@@ -303,11 +303,18 @@ static void vvp_io_fini(const struct lu_env *env, const struct cl_io_slice *ios)
 	CLOBINVRNT(env, obj, vvp_object_invariant(obj));
 
 	CDEBUG(D_VFSTRACE, DFID" ignore/verify layout %d/%d, layout version %d "
-			   "need write layout %d, restore needed %d\n",
+	       "need write layout %d, restore needed %d, invalidate_lock %d\n",
 	       PFID(lu_object_fid(&obj->co_lu)),
 	       io->ci_ignore_layout, io->ci_verify_layout,
 	       vio->vui_layout_gen, io->ci_need_write_intent,
-	       io->ci_restore_needed);
+	       io->ci_restore_needed, io->ci_invalidate_page_cache);
+
+#ifdef HAVE_INVALIDATE_LOCK
+	if (io->ci_invalidate_page_cache) {
+		filemap_invalidate_unlock(inode->i_mapping);
+		io->ci_invalidate_page_cache = 0;
+	}
+#endif /* HAVE_INVALIDATE_LOCK */
 
 	if (io->ci_restore_needed) {
 		/* file was detected release, we need to restore it
@@ -1871,6 +1878,11 @@ int vvp_io_init(const struct lu_env *env, struct cl_object *obj,
 			       ll_i2sbi(inode)->ll_fsname,
 			       PFID(lu_object_fid(&obj->co_lu)), result);
 	}
+
+#ifdef HAVE_INVALIDATE_LOCK
+	if (io->ci_invalidate_page_cache)
+		filemap_invalidate_lock(inode->i_mapping);
+#endif /* HAVE_INVALIDATE_LOCK */
 
 	io->ci_result = result < 0 ? result : 0;
 	RETURN(result);

lustre/osc/osc_cache.c
@@ -3265,6 +3265,7 @@ int osc_lock_discard_pages(const struct lu_env *env, struct osc_object *osc,
 
 	io->ci_obj = cl_object_top(osc2cl(osc));
 	io->ci_ignore_layout = 1;
+	io->ci_invalidate_page_cache = 1;
 	io->u.ci_misc.lm_next_rpc_time = ktime_get_seconds() +
 					 5 * obd_timeout / 16;
 	result = cl_io_init(env, io, CIT_MISC, io->ci_obj);

lustre/autoconf/lustre-core.m4
@@ -3193,6 +3193,29 @@ AC_DEFUN([LC_HAVE_GET_ACL_RCU_ARG], [
 ]) # LC_HAVE_GET_ACL_RCU_ARG
 
 #
+# LC_HAVE_INVALIDATE_LOCK
+#
+# Kernel version v5.15-rc1 commit 730633f0b7f951726e87f912a6323641f674ae34
+# mm: Protect operations adding pages to page cache with invalidate_lock
+#
+AC_DEFUN([LC_HAVE_INVALIDATE_LOCK], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if have address_space have invalidate_lock member],
+address_space_invalidate_lock, [
+		#include <linux/fs.h>
+	],[
+		struct address_space *mapping = NULL;
+
+		filemap_invalidate_lock(mapping);
+	],[
+		AC_DEFINE(HAVE_INVALIDATE_LOCK, 1,
+			[address_space invalidate_lock member exists])
+	])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LC_HAVE_INVALIDATE_LOCK
+
+#
 # LC_HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 #
 # Linux v5.15-rc1-20-g15bf32398ad4
@@ -4258,6 +4281,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 
 	# 5.15
 	LC_HAVE_GET_ACL_RCU_ARG
+	LC_HAVE_INVALIDATE_LOCK
 
 	# 5.16
 	LC_HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG

lustre/tests/sanity.sh
@@ -30011,6 +30011,54 @@ test_832() {
 }
 run_test 832 "lfs rm_entry"
 
+test_833() {
+	local file=$DIR/$tfile
+
+	stack_trap "rm -f $file" EXIT
+	dd if=/dev/zero of=$file bs=1M count=50 || error "Write $file failed"
+
+	local wpid
+	local rpid
+	local rpid2
+
+	# Buffered I/O write
+	(
+		while [ ! -e $DIR/sanity.833.lck ]; do
+			dd if=/dev/zero of=$file bs=1M count=50 conv=notrunc ||
+				error "failed to write $file"
+			sleep 0.$((RANDOM % 4 + 1))
+		done
+	)&
+	wpid=$!
+
+	# Buffered I/O read
+	(
+		while [ ! -e $DIR/sanity.833.lck ]; do
+			dd if=$file of=/dev/null bs=1M count=50 ||
+				error "failed to read $file"
+			sleep 0.$((RANDOM % 4 + 1))
+		done
+	)&
+	rpid=$!
+
+	# Direct I/O read
+	(
+		while [ ! -e $DIR/sanity.833.lck ]; do
+			dd if=$file of=/dev/null bs=1M count=50 iflag=direct ||
+				error "failed to read $file in direct I/O mode"
+			sleep 0.$((RANDOM % 4 + 1))
+		done
+	)&
+	rpid2=$!
+
+	sleep 30
+	touch $DIR/sanity.833.lck
+	wait $wpid || error "$?: buffered write failed"
+	wait $rpid || error "$?: buffered read failed"
+	wait $rpid2 || error "$?: direct read failed"
+}
+run_test 833 "Mixed buffered/direct read and write should not return -EIO"
+
 #
 # tests that do cleanup/setup should be run at the end
 #

commit 56e59cbb11d857fd8679aaf219ed744080fd75a0

LU-19427 llite: fix invalidate_lock deadlock for mixed BIO/DIO
On the newer kernel such as rock9.5 or SLES15sp5, the test
case sanity-pcc/test_99b deadlock as follows:
A client is granted a PW extent lock L1;

TD1 - TD8: read direct I/O
On client side (OSC): use out all available RPC slots of 8.
On server side (OST):
- Waiting for the server side locking for direct read I/O;
- Lock blocking callback to revoke the conflict granted L1;
<== Waiting for the completion of T2.

T2: Client side lock blocking AST for L1
osc_dlm_blocking_ast0()
 ->osc_lock_flush()
  ->osc_lock_discard_pages()
  - Acquire execlusive invalidate_lock
<== Waiting for T3 which is holding shared invalidate_lock

T3: generic buffered read:
->ll_file_io_generic()
  - Acquire shared invalidate_lock
  ->ll_readpage()
    ->lov_io_submit()->osc_io_submit()
    - Waiting for I/O finished
    - I/O extent is waiting for RPC slots
<== Waiting for TD1-TD8

The deadlock is: TD1-TD8=>T2=>T3=>TD1-TD8

This patch solves the deadlock using trylock for invalidate_lock
in lock blocking AST.
If trylock failed, it will check the pending read extents in the
OSC object and move them into high priority list to make them fire
into wire as soon as possible. Thus the read holding the shared
invalidate_lock can release as quickly as possible.
And lock BAST can acquire the invalidate_lock finally.

Fixes: bba59b1287 ("LU-16651 llite: hold invalidate_lock when invalidate cache pages")
Change-Id: I6800c998cf0ce92de11d846d4715cec81cb9a513
Test-Parameters: clientdistro=sles15sp5 testlist=sanity-pcc env=ONLY=99b,ONLY_REPEAT=50
Test-Parameters: clientdistro=rocky9.5 testlist=sanity-pcc env=ONLY=99b,ONLY_REPEAT=50
Signed-off-by: Yingjin Qian <qian@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61608
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>

lustre/include/cl_object.h
@@ -292,6 +292,8 @@ enum coo_inode_opc {
 
 struct cl_dio_pages;
 
+typedef void (*buffered_read_check_cbt)(const struct lu_env *env, void *cbdata);
+
 /**
  * Operations implemented for each cl object layer.
  *
@@ -383,13 +385,27 @@ struct cl_object_operations {
 	void (*coo_dirty_for_sync)(const struct lu_env *env,
 				   struct cl_object *obj);
 	/**
-	 * Update object configuration. Called top-to-bottom to modify object
-	 * configuration.
-	 *
-	 * XXX error conditions and handling.
+	 * Take execlusive invalidate_lock for inode mapping.
+	 * The file system must execlusively acquire invalidate_lock before
+	 * invalidating the page cache in truncate / hole punch / DLM extent
+	 * lock blocking AST path (and thus calling into ->invalidatepage)
+	 * to block races between page cache invalidation and page cache
+	 * filling functions (fault, read, readahead, ...)
 	 */
-	int (*coo_conf_set)(const struct lu_env *env, struct cl_object *obj,
-			    const struct cl_object_conf *conf);
+	void (*coo_invalidate_lock)(const struct lu_env *env,
+				    struct cl_object *obj,
+				    buffered_read_check_cbt cb, void *cbdata);
+	/* Release invalidate_lock. */
+	void (*coo_invalidate_unlock)(const struct lu_env *env,
+				      struct cl_object *obj);
+        /**
+         * Update object configuration. Called top-to-bottom to modify object
+         * configuration.
+         *
+         * XXX error conditions and handling.
+         */
+        int (*coo_conf_set)(const struct lu_env *env, struct cl_object *obj,
+                            const struct cl_object_conf *conf);
 	/**
 	 * Glimpse ast. Executed when glimpse ast arrives for a lock on this
 	 * object. Layers are supposed to fill parts of \a lvb that will be
@@ -1989,14 +2005,6 @@ struct cl_io {
 	 * io_uring direct IO with flags IOCB_NOWAIT.
 	 */
 			     ci_iocb_nowait:1,
-	/**
-	 * The filesystem must exclusively acquire invalidate_lock before
-	 * invalidating page cache in truncate / hole punch / DLM extent
-	 * lock blocking AST path (and thus calling into ->invalidatepage)
-	 * to block races between page cache invalidation and page cache
-	 * filling functions (fault, read, ...)
-	 */
-			     ci_invalidate_page_cache:1,
 	/* was this IO switched from BIO to DIO for hybrid IO? */
 			     ci_hybrid_switched:1;
 
@@ -2181,6 +2189,10 @@ int  cl_object_attr_update(const struct lu_env *env, struct cl_object *obj,
 			   const struct cl_attr *attr,
 			   enum cl_attr_valid valid);
 void cl_object_dirty_for_sync(const struct lu_env *env, struct cl_object *obj);
+void cl_object_invalidate_lock(const struct lu_env *env, struct cl_object *obj,
+			       buffered_read_check_cbt cb, void *cbdata);
+void cl_object_invalidate_unlock(const struct lu_env *env,
+				 struct cl_object *obj);
 int  cl_object_glimpse(const struct lu_env *env, struct cl_object *obj,
 		       struct ost_lvb *lvb);
 int  cl_conf_set(const struct lu_env *env, struct cl_object *obj,

lustre/llite/vvp_io.c
@@ -282,18 +282,11 @@ static void vvp_io_fini(const struct lu_env *env, const struct cl_io_slice *ios)
 
 	CLOBINVRNT(env, obj, vvp_object_invariant(obj));
 
-	CDEBUG(D_VFSTRACE, DFID" ignore/verify layout %d/%d, layout version %d need write layout %d, restore needed %d, invalidate_lock %d\n",
+	CDEBUG(D_VFSTRACE, DFID" ignore/verify layout %d/%d, layout version %d need write layout %d, restore needed %d\n",
 	       PFID(lu_object_fid(&obj->co_lu)),
 	       io->ci_ignore_layout, io->ci_verify_layout,
 	       vio->vui_layout_gen, io->ci_need_write_intent,
-	       io->ci_restore_needed, io->ci_invalidate_page_cache);
-
-#ifdef HAVE_INVALIDATE_LOCK
-	if (io->ci_invalidate_page_cache) {
-		filemap_invalidate_unlock(inode->i_mapping);
-		io->ci_invalidate_page_cache = 0;
-	}
-#endif /* HAVE_INVALIDATE_LOCK */
+	       io->ci_restore_needed);
 
 	if (io->ci_restore_needed) {
 		/* file was detected release, we need to restore it
@@ -1894,11 +1887,6 @@ int vvp_io_init(const struct lu_env *env, struct cl_object *obj,
 			       ll_i2sbi(inode)->ll_fsname,
 			       PFID(lu_object_fid(&obj->co_lu)), result);
 	}
-
-#ifdef HAVE_INVALIDATE_LOCK
-	if (io->ci_invalidate_page_cache)
-		filemap_invalidate_lock(inode->i_mapping);
-#endif /* HAVE_INVALIDATE_LOCK */
 
 	io->ci_result = result < 0 ? result : 0;
 	RETURN(result);

lustre/llite/vvp_object.c
@@ -104,6 +104,31 @@ static void vvp_dirty_for_sync(const struct lu_env *env, struct cl_object *obj)
 	__mark_inode_dirty(inode, I_DIRTY_DATASYNC);
 }
 
+static void vvp_invalidate_lock(const struct lu_env *env, struct cl_object *obj,
+				buffered_read_check_cbt cb, void *cbdata)
+{
+#ifdef HAVE_INVALIDATE_LOCK
+	struct address_space *mapping = vvp_object_inode(obj)->i_mapping;
+
+	while (!down_write_trylock(&mapping->invalidate_lock)) {
+		cb(env, cbdata);
+		/* FIXME: It may cause waiting write lock starvation. */
+		if (need_resched())
+			cond_resched();
+	}
+#endif /* HAVE_INVALIDATE_LOCK */
+}
+
+static void vvp_invalidate_unlock(const struct lu_env *env,
+				  struct cl_object *obj)
+{
+#ifdef HAVE_INVALIDATE_LOCK
+	struct address_space *mapping = vvp_object_inode(obj)->i_mapping;
+
+	filemap_invalidate_unlock(mapping);
+#endif /* HAVE_INVALIDATE_LOCK */
+}
+
 static int vvp_conf_set(const struct lu_env *env, struct cl_object *obj,
 			const struct cl_object_conf *conf)
 {
@@ -215,6 +240,8 @@ static const struct cl_object_operations vvp_ops = {
 	.coo_attr_get     = vvp_attr_get,
 	.coo_attr_update  = vvp_attr_update,
 	.coo_dirty_for_sync = vvp_dirty_for_sync,
+	.coo_invalidate_lock	= vvp_invalidate_lock,
+	.coo_invalidate_unlock	= vvp_invalidate_unlock,
 	.coo_conf_set     = vvp_conf_set,
 	.coo_prune        = vvp_prune,
 	.coo_glimpse      = vvp_object_glimpse,

lustre/obdclass/cl_object.c
@@ -292,6 +292,38 @@ void cl_object_dirty_for_sync(const struct lu_env *env, struct cl_object *top)
 }
 EXPORT_SYMBOL(cl_object_dirty_for_sync);
 
+/* Acquire invalidate_lock of the inode mapping. */
+void cl_object_invalidate_lock(const struct lu_env *env, struct cl_object *top,
+			       buffered_read_check_cbt cb, void *cbdata)
+{
+	struct cl_object *obj;
+
+	ENTRY;
+
+	cl_object_for_each(obj, top) {
+		if (obj->co_ops->coo_invalidate_lock != NULL)
+			obj->co_ops->coo_invalidate_lock(env, obj, cb, cbdata);
+	}
+	EXIT;
+}
+EXPORT_SYMBOL(cl_object_invalidate_lock);
+
+/* Release invalidate_lock of the inode mapping. */
+void cl_object_invalidate_unlock(const struct lu_env *env,
+				 struct cl_object *top)
+{
+	struct cl_object *obj;
+
+	ENTRY;
+
+	cl_object_for_each(obj, top) {
+		if (obj->co_ops->coo_invalidate_unlock != NULL)
+			obj->co_ops->coo_invalidate_unlock(env, obj);
+	}
+	EXIT;
+}
+EXPORT_SYMBOL(cl_object_invalidate_unlock);
+
 /**
  * cl_object_glimpse() - Notifies layers (bottom-to-top) that glimpse AST was
  * received.

lustre/osc/osc_cache.c
@@ -3504,6 +3504,43 @@ bool osc_discard_cb(const struct lu_env *env, struct cl_io *io,
 }
 EXPORT_SYMBOL(osc_discard_cb);
 
+static void osc_buffered_read_check_cb(const struct lu_env *env, void *cbdata)
+{
+	struct osc_object *osc = (struct osc_object *)cbdata;
+	struct osc_extent *ext;
+	struct osc_extent *next;
+	bool unplug = false;
+
+	ENTRY;
+
+	osc_object_lock(osc);
+	/*
+	 * Check buffered read extents.
+	 * As we failed to acquire @invalidate_lock, there should be a read or
+	 * fault process holding the shared @invalidate_lock.
+	 * Move the reading extent waiting for RPC slots to high priority list,
+	 * thus they can be fired as soon as possible to avoid the possible
+	 * deadlock.
+	 */
+	list_for_each_entry_safe(ext, next, &osc->oo_reading_exts, oe_link) {
+		EASSERT(ext->oe_state == OES_LOCK_DONE, ext);
+		/* Skip write extent and direct I/O */
+		if (!ext->oe_rw || ext->oe_dio || ext->oe_srvlock)
+			continue;
+
+		list_move_tail(&ext->oe_link, &osc->oo_hp_read_exts);
+		OSC_EXTENT_DUMP(D_CACHE, ext,
+				"HP read this extent for invalidate_lock\n");
+		unplug = true;
+	}
+	osc_object_unlock(osc);
+
+	if (unplug)
+		osc_io_unplug(env, osc_cli(osc), osc);
+
+	EXIT;
+}
+
 /**
  * Discard pages protected by the given lock. This function traverses radix
  * tree to find all covering pages and discard them. If a page is being covered
@@ -3515,6 +3552,7 @@ EXPORT_SYMBOL(osc_discard_cb);
 int osc_lock_discard_pages(const struct lu_env *env, struct osc_object *osc,
 			   pgoff_t start, pgoff_t end, bool discard)
 {
+	struct cl_object *clob = cl_object_top(osc2cl(osc));
 	struct osc_thread_info *info = osc_env_info(env);
 	struct cl_io *io = osc_env_new_io(env);
 	osc_page_gang_cbt cb;
@@ -3522,9 +3560,10 @@ int osc_lock_discard_pages(const struct lu_env *env, struct osc_object *osc,
 
 	ENTRY;
 
-	io->ci_obj = cl_object_top(osc2cl(osc));
+	cl_object_invalidate_lock(env, clob, osc_buffered_read_check_cb, osc);
+
+	io->ci_obj = clob;
 	io->ci_ignore_layout = 1;
-	io->ci_invalidate_page_cache = 1;
 	io->u.ci_misc.lm_next_rpc_time = ktime_get_seconds() +
 					 5 * obd_timeout / 16;
 	result = cl_io_init(env, io, CIT_MISC, io->ci_obj);
@@ -3539,6 +3578,8 @@ int osc_lock_discard_pages(const struct lu_env *env, struct osc_object *osc,
 			     info->oti_next_index, end, cb, osc);
 out:
 	cl_io_fini(env, io);
+	cl_object_invalidate_unlock(env, clob);
+
 	RETURN(result);
 }
 