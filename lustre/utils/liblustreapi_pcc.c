lustre/lustre-release

commit c74878caa79305f77ba93eaa876bcc433dacc556

LU-12373 pcc: uncache the pcc copies when remove a PCC backend
Currently when remove a PCC backend from a client, it does not
make any special handling for previously cached files at all.
Users can still use PCC caching service for these files. This
may not what users want. The reason is as follows:

1) For RW-PCC cached files, it does not restore the data back
into Lustre OSTs of the main filesystem. Although the PCC
backend falls back as a tranditional HSM storage solution
since the lhsmtool_posix copytool is still running at this
client. But this is dangerous, and likly to cause user data
to be lost if the PCC device may be permanently unavailable.

2) The space used by these PCC cached files may not released.

In this patch, when remove a PCC backend from a client, the
default action is to scan the PCC backend fs, uncache
(detach and remove) the PCC copy from PCC by FID.

We also add an option "--keep|-k" for PCC backend removal.
It behaves as before, just remove the PCC backend, but
retain the data on the cache.

This patch also introduces a common library to scan the HSM
backend.

EX-2579 pcc: support a flatter HSM archive format

Add versioning (v1 and V2) to the HSM (PCC) archive format (directory
layout):
	v1: (oid & 0xffff)/-/-/-/-/-/FID
	v2: ((oid ^ seq) & 0xffff)/FID

v1 is the original layout and the default. v2 is the new layout which
should be selected for new installs.
Was-Change-Id: If660f3cf4c02469bb23e65a44f86f0346367adf6

LU-12373 pcc: delete stale PCC copy when remove PCC backend

By default, when removing a PCC backend from a client, the action
is to scan the PCC backend FS, uncache (detach and remove) all
scanned PCC copies from PCC by FIDs.

However, during the tests, we found that some old stale PCC copies
are not removed when an adminstrator runs "lctl pcc del|clear".
The reason is that these PCC copies are already detached from PCC
when running the commands.

This patch fixes this bug: when removing a PCC backend from a

lustre/utils/liblustreapi_pcc.c
@@ -39,7 +39,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <lnetconfig/cyaml.h>
 #include "lustreapi_internal.h"
+#include "libhsm_scanner.h"
 
 /**
  * Fetch and attach a file to readwrite PCC.
@@ -254,16 +256,16 @@ int llapi_pcc_attach_fid_str(const char *mntpath, const char *fidstr,
  * detach PCC cache of a file by using fd.
  *
  * \param fd		File handle.
- * \param option	Detach option
+ * \param flags		Detach flags.
  *
  * \return 0 on success, an error code otherwise.
  */
-int llapi_pcc_detach_fd(int fd, __u32 option)
+int llapi_pcc_detach_fd(int fd, __u32 flags)
 {
 	struct lu_pcc_detach detach;
 	int rc;
 
-	detach.pccd_opt = option;
+	detach.pccd_flags = flags;
 	rc = ioctl(fd, LL_IOC_PCC_DETACH, &detach);
 	/* If error, save errno value */
 	rc = rc ? -errno : 0;
@@ -276,12 +278,12 @@ int llapi_pcc_detach_fd(int fd, __u32 option)
  *
  * \param mntpath	Fullpath to the client mount point.
  * \param fid		FID of the file.
- * \param option	Detach option.
+ * \param flags		Detach flags.
  *
  * \return 0 on success, an error code otherwise.
  */
 int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
-			 __u32 option)
+			 __u32 flags)
 {
 	int rc;
 	int fd;
@@ -302,8 +304,7 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
 	 * files.
 	 */
 	detach.pccd_fid = *fid;
-	detach.pccd_opt = option;
-
+	detach.pccd_flags = flags;
 	rc = ioctl(fd, LL_IOC_PCC_DETACH_BY_FID, &detach);
 	rc = rc ? -errno : 0;
 
@@ -316,12 +317,12 @@ int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
  *
  * \param mntpath	Fullpath to the client mount point.
  * \param fidstr	FID string of the file.
- * \param option	Detach option.
+ * \param flags		Detach flags.
  *
  * \return 0 on success, an error code otherwise.
  */
 int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr,
-			     __u32 option)
+			     __u32 flags)
 {
 	int rc;
 	struct lu_fid fid;
@@ -338,7 +339,7 @@ int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr,
 		return -EINVAL;
 	}
 
-	rc = llapi_pcc_detach_fid(mntpath, &fid, option);
+	rc = llapi_pcc_detach_fid(mntpath, &fid, flags);
 
 	return rc;
 }
@@ -347,11 +348,11 @@ int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr,
  * detach PCC cache of a file.
  *
  * \param path		Fullpath to the file to operate on.
- * \param option	Detach option.
+ * \param flags		Detach flags.
  *
  * \return 0 on success, an error code otherwise.
  */
-int llapi_pcc_detach_file(const char *path, __u32 option)
+int llapi_pcc_detach_file(const char *path, __u32 flags)
 {
 	int rc;
 	int fd;
@@ -364,7 +365,7 @@ int llapi_pcc_detach_file(const char *path, __u32 option)
 		return rc;
 	}
 
-	rc = llapi_pcc_detach_fd(fd, option);
+	rc = llapi_pcc_detach_fd(fd, flags);
 	close(fd);
 	return rc;
 }
@@ -526,4 +527,276 @@ out_close:
 out_free_param:
 	cfs_free_param_data(&path);
 	return rc;
+}
+
+static int llapi_pcc_scan_detach(const char *pname, const char *fname,
+				 struct hsm_scan_control *hsc)
+{
+	struct lu_pcc_detach_fid detach;
+	char fullname[PATH_MAX];
+	char fidstr[FID_LEN];
+	const char *fidname;
+	bool lov_file;
+	int fd;
+	int rc;
+
+	/* It is the saved lov file when archive on HSM backend. */
+	detach.pccd_flags = PCC_DETACH_FL_UNCACHE;
+	lov_file = endswith(fname, ".lov");
+	if (lov_file) {
+		size_t len;
+
+		len = strlen(fname) - strlen(".lov");
+		if (len > sizeof(fidstr)) {
+			rc = -ENAMETOOLONG;
+			errno = ENAMETOOLONG;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "Too long PCC-RO fname %s/%s",
+				    pname, fname);
+			return rc;
+		}
+		strncpy(fidstr, fname, FID_LEN);
+		fidstr[len] = '\0';
+		detach.pccd_flags |= PCC_DETACH_FL_KNOWN_READWRITE;
+		fidname = fidstr;
+	} else {
+		fidname = fname;
+	}
+
+	rc = sscanf(fidname, SFID, RFID(&detach.pccd_fid));
+	if (rc != 3 || !fid_is_sane(&detach.pccd_fid)) {
+		llapi_err_noerrno(LLAPI_MSG_ERROR,
+				  "bad FID format '%s', should be [seq:oid:ver] (e.g. "DFID")\n",
+				  fidname, (unsigned long long)FID_SEQ_NORMAL,
+				  2, 0);
+		return -EINVAL;
+	}
+
+	llapi_printf(LLAPI_MSG_DEBUG, "Handle the file: %s\n", fidname);
+
+	rc = llapi_root_path_open(hsc->hsc_mntpath, &fd);
+	if (rc) {
+		llapi_error(LLAPI_MSG_ERROR, rc, "cannot get root path: %s",
+			    hsc->hsc_mntpath);
+		return rc;
+	}
+
+	rc = ioctl(fd, LL_IOC_PCC_DETACH_BY_FID, &detach);
+	close(fd);
+
+	if (rc) {
+		rc = -errno;
+		llapi_printf(LLAPI_MSG_DEBUG,
+			     "failed to detach file '%s': rc = %d\n",
+			     fidname, rc);
+		return rc;
+	}
+
+	if (detach.pccd_flags & PCC_DETACH_FL_CACHE_REMOVED) {
+		llapi_printf(LLAPI_MSG_DEBUG,
+			     "Detach and remove the PCC cached file: %s\n",
+			     fidname);
+	} else {
+		snprintf(fullname, sizeof(fullname), "%s/%s", pname, fidname);
+		llapi_printf(LLAPI_MSG_DEBUG,
+			     "Remove non-cached file: %s flags: %X\n",
+			     fullname, detach.pccd_flags);
+		if (unlink(fullname) && errno != ENOENT) {
+			rc = -errno;
+			llapi_error(LLAPI_MSG_ERROR, rc,
+				    "Unlink %s failed", fullname);
+		}
+
+		if (detach.pccd_flags & PCC_DETACH_FL_KNOWN_READWRITE) {
+			snprintf(fullname, sizeof(fullname), "%s/%s",
+				 pname, fname);
+			/* Remove *.lov file */
+			unlink(fullname);
+		}
+	}
+
+	return rc;
+}
+
+static int llapi_pcc_del_internal(const char *mntpath, const char *pccpath,
+				  enum hsmtool_type type,
+				  enum lu_pcc_cleanup_flags flags)
+{
+	struct hsm_scan_control hsc;
+	char cmd[PATH_MAX];
+	int rc;
+
+	snprintf(cmd, sizeof(cmd), "del %s", pccpath);
+	rc = llapi_pccdev_set(mntpath, cmd);
+	if (rc < 0) {
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "failed to run '%s' on %s", cmd, mntpath);
+		return rc;
+	}
+
+	if (flags & PCC_CLEANUP_FL_KEEP_DATA)
+		return 0;
+
+	hsc.hsc_type = type;
+	hsc.hsc_mntpath = mntpath;
+	hsc.hsc_hsmpath = pccpath;
+	hsc.hsc_func = llapi_pcc_scan_detach;
+	hsc.hsc_errnum = 0;
+	rc = hsm_scan_process(&hsc);
+
+	return rc;
+}
+
+struct pcc_cmd_handler;
+
+typedef int (*pcc_handler_t)(struct cYAML *node, struct pcc_cmd_handler *pch);
+
+enum pcc_cmd_t {
+	PCC_CMD_DEL,
+	PCC_CMD_CLEAR,
+};
+
+struct pcc_cmd_handler {
+	enum pcc_cmd_t			 pch_cmd;
+	enum lu_pcc_type		 pch_type;
+	bool				 pch_iter_cont;
+	enum lu_pcc_cleanup_flags	 pch_flags;
+	const char			*pch_mntpath;
+	const char			*pch_pccpath;
+	pcc_handler_t			 pch_cb;
+	__u32				 pch_id;
+};
+
+static int llapi_pcc_yaml_cb_helper(struct pcc_cmd_handler *pch)
+{
+	struct cYAML *tree = NULL, *err_rc = NULL, *pcc_node = NULL,
+		     *node = NULL;
+	char pathbuf[sizeof(struct obd_uuid)];
+	glob_t path;
+	int rc;
+
+	rc = llapi_getname(pch->pch_mntpath, pathbuf, sizeof(pathbuf));
+	if (rc < 0) {
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "cannot get name for '%s'\n", pch->pch_mntpath);
+		return rc;
+	}
+
+	rc = cfs_get_param_paths(&path, "llite/%s/pcc", pathbuf);
+	if (rc != 0)
+		return -errno;
+
+	tree = cYAML_build_tree(path.gl_pathv[0], NULL, 0, &err_rc, false);
+	if (!tree) {
+		rc = -EINVAL;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "cannot parse YAML file %s\n", path.gl_pathv[0]);
+		cYAML_build_error(rc, -1, "yaml", "from PCC yaml",
+				  "can't parse", &err_rc);
+		cYAML_print_tree2file(stderr, err_rc);
+		cYAML_free_tree(err_rc);
+		goto out_free;
+	}
+
+	pcc_node = cYAML_get_object_item(tree, "pcc");
+	if (!pcc_node)
+		goto out_free;
+
+	if (!cYAML_is_sequence(pcc_node)) {
+		rc = -EINVAL;
+		llapi_error(LLAPI_MSG_ERROR, rc,
+			    "bad PCC backend Array!");
+		goto out_free;
+	}
+
+	while (cYAML_get_next_seq_item(pcc_node, &node) != NULL &&
+	       pch->pch_iter_cont) {
+		int ret;
+
+		ret = pch->pch_cb(node, pch);
+		if (ret && !rc)
+			rc = ret;
+	}
+
+	/* Not found the given PCC backend on the client. */
+	if (pch->pch_iter_cont && pch->pch_cmd == PCC_CMD_DEL)
+		rc = -ENOENT;
+
+out_free:
+	if (tree)
+		cYAML_free_tree(tree);
+	cfs_free_param_data(&path);
+	return rc;
+
+}
+
+static int llapi_handle_yaml_pcc_del(struct cYAML *node,
+				     struct pcc_cmd_handler *pch)
+{
+	struct cYAML *pccpath, *hsmtool;
+	enum hsmtool_type type;
+
+	pccpath = cYAML_get_object_item(node, PCC_YAML_PCCPATH);
+	hsmtool = cYAML_get_object_item(node, PCC_YAML_HSMTOOL);
+
+	if (!pccpath || !pccpath->cy_valuestring ||
+	    !hsmtool || !hsmtool->cy_valuestring)
+		return 0;
+
+	if (strcmp(pccpath->cy_valuestring, pch->pch_pccpath))
+		return 0;
+
+	pch->pch_iter_cont = false;
+	type = hsmtool_string2type(hsmtool->cy_valuestring);
+	return llapi_pcc_del_internal(pch->pch_mntpath, pch->pch_pccpath,
+				      type, pch->pch_flags);
+}
+
+int llapi_pcc_del(const char *mntpath, const char *pccpath,
+		  enum lu_pcc_cleanup_flags flags)
+{
+	struct pcc_cmd_handler pch;
+
+	pch.pch_cmd = PCC_CMD_DEL;
+	pch.pch_iter_cont = true;
+	pch.pch_mntpath = mntpath;
+	pch.pch_pccpath = pccpath;
+	pch.pch_flags = flags;
+	pch.pch_cb = llapi_handle_yaml_pcc_del;
+
+	return llapi_pcc_yaml_cb_helper(&pch);
+}
+
+
+static int llapi_handle_yaml_pcc_clear(struct cYAML *node,
+				       struct pcc_cmd_handler *pch)
+{
+	struct cYAML *pccpath, *hsmtool;
+	enum hsmtool_type type;
+
+	pccpath = cYAML_get_object_item(node, PCC_YAML_PCCPATH);
+	hsmtool = cYAML_get_object_item(node, PCC_YAML_HSMTOOL);
+
+	if (!pccpath || !pccpath->cy_valuestring ||
+	    !hsmtool || !hsmtool->cy_valuestring)
+		return 0;
+
+	type = hsmtool_string2type(hsmtool->cy_valuestring);
+	return llapi_pcc_del_internal(pch->pch_mntpath,
+				      pccpath->cy_valuestring,
+				      type, pch->pch_flags);
+}
+
+int llapi_pcc_clear(const char *mntpath, enum lu_pcc_cleanup_flags flags)
+{
+	struct pcc_cmd_handler pch;
+
+	pch.pch_cmd = PCC_CMD_CLEAR;
+	pch.pch_iter_cont = true;
+	pch.pch_mntpath = mntpath;
+	pch.pch_pccpath = NULL;
+	pch.pch_flags = flags;
+	pch.pch_cb = llapi_handle_yaml_pcc_clear;
+
+	return llapi_pcc_yaml_cb_helper(&pch);
 }

lustre/doc/Makefile.am
@@ -196,6 +196,8 @@ LIBMAN = 					\
 	llapi_pccdev_set.3			\
 	llapi_pcc_state_get.3			\
 	llapi_pcc_state_get_fd.3		\
+	llapi_pcc_clear.3			\
+	llapi_pcc_del.3				\
 	llapi_quotactl.3			\
 	llapi_rmfid.3				\
 	llapi_rmfid_at.3			\

lustre/doc/lctl-pcc.8
@@ -4,9 +4,9 @@ lctl-pcc \- commands used to interact with PCC features
 .SH SYNOPSIS
 .B lctl pcc add \fR<\fImntpath\fR> <\fIpccpath\fR> [\fB--param\fR|\fB-p\fR <\fIparam\fR>]
 .br
-.B lctl pcc del <\fImntpath\fR> <\fIpccpath\fR>
+.B lctl pcc del [\fB--keep\fR|\fB-k\fR] <\fImntpath\fR> <\fIpccpath\fR>
 .br
-.B lctl pcc clear <\fImntpath\fR>
+.B lctl pcc clear [\fB--keep\fR|\fB-k\fR] <\fImntpath\fR>
 .br
 .B lctl pcc list <\fImntpath\fR>
 .SH DESCRIPTION
@@ -28,13 +28,13 @@ and the suffix of the file name is "h5". "rwid" represents the read-write
 attach id (2) which value is same as the archive ID of the copytool agent
 running on this PCC node.
 .TP
-.B lctl pcc del <\fImntpath\fR> <\fIpccpath\fR>
+.B lctl pcc del [\fB--keep\fR|\fB-k\fR] <\fImntpath\fR> <\fIpccpath\fR>
 Delete a PCC backend specified by path
 .IR pccpath
 on a Lustre client referenced by the mount point of
 .IR mntpath .
 .TP
-.B lctl pcc clear <\fImntpath\fR>
+.B lctl pcc clear [\fB--keep\fR|\fB-k\fR] <\fImntpath\fR>
 Remove all PCC backend on a Lustre client referenced by the mount point of
 .IR mntpath .
 .TP
@@ -45,6 +45,14 @@ List all PCC backends on a Lustre client referenced by the mount point of
 .TP
 .B --param | -p
 Specifies the configuration parameters for a PCC backend.
+.TP
+.B --keep | -k
+By default, when remove a PCC backend from a client, the action is to scan the
+PCC backend fs, and then uncache (detach and remove) all scanned PCC copies
+from PCC by FIDs. With the option "--keep|-k", the "lctl pcc del|clear" command
+just removes the PCC backend from the Lustre client, and retains the data on the
+cache. In this way, the PCC-RW backend falls back as a tranditional HSM storage
+solution since the copytool is still running at this client.
 .TP
 .SH SEE ALSO
 .BR lfs (1),

lustre/doc/llapi_pcc_clear.3
@@ -1,0 +1,40 @@
+.TH llapi_pcc_clear 3 "2019 June 20" "Lustre User API"
+.SH NAME
+llapi_pcc_clear \- Remove all PCC backends from a client
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_pcc_clear(const char *" mntpath ,
+.BI "                    enum lu_pcc_cleanup_flags " flags );
+.fi
+.SH DESCRIPTION
+.PP
+The function
+.BR llapi_pcc_clear()
+deletes all PCC backends on the client with the mount point referenced by
+.IR mntpath .
+Refer
+.BR llapi_pcc_del()
+for the usage of the input parameter
+.IR flags .
+.SH RETURN VALUES
+.PP
+.B llapi_pcc_clear()
+returns 0 on success or a negative errno on failure.
+.SH ERRORS
+.TP 15
+.SM -ENOMEM
+Insufficient memory to complete operation.
+.TP
+.SM -EFAULT
+Memory region is not properly mapped.
+.TP
+.SM -EINVAL
+One or more invalid arguments are given.
+.TP
+.SM -EOPNOTSUPP
+PCC backend operation is not supported.
+.SH "SEE ALSO"
+.BR llapi_pcc_del (3),
+.BR lustreapi (7)

lustre/doc/llapi_pcc_del.3
@@ -1,0 +1,47 @@
+.TH llapi_pcc_del 3 "2019 June 20" "Lustre User API"
+.SH NAME
+llapi_pcc_del \- Delete a PCC backend from a client
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.PP
+.BI "int llapi_pcc_del(const char *" mntpath ", const char *" pccpath ,
+.BI "                  enum lu_pcc_cleanup_flags " flags );
+.fi
+.SH DESCRIPTION
+.PP
+The function
+.BR llapi_pcc_del()
+deletes a PCC backend referenced by
+.IR pccpath
+on the client with the mount point of
+.IR mntpath .
+By default, when remove a PCC backend from a client, the action is to scan the
+PCC backend fs, uncache (detach and remove) all scanned PCC copies from PCC by
+FIDs. The input parameter
+.IR flags
+currently only has one value
+.B PCC_CLEANUP_FL_KEEP_DATA
+that means it removes the PCC backend from the Lustre client, but retains
+the data on the cache. In this case, the PCC-RW backend falls back as a
+traditional HSM storage solution and the file data is still accessible as long
+as the copytool is still running at this client.
+.SH RETURN VALUES
+.PP
+.B llapi_pcc_del()
+returns 0 on success or a negative errno value on failure.
+.SH ERRORS
+.TP 15
+.SM -ENOMEM
+Insufficient memory to complete operation.
+.TP
+.SM -EFAULT
+Memory region is not properly mapped.
+.TP
+.SM -EINVAL
+One or more invalid arguments are given.
+.TP
+.SM -EOPNOTSUPP
+PCC backend operation is not supported.
+.SH "SEE ALSO"
+.BR lustreapi (7)

lustre/doc/llapi_pcc_detach_fid_fd.3
@@ -6,13 +6,16 @@ llapi_pcc_detach_file \- detach the given file from PCC
 .nf
 .B #include <lustre/lustreapi.h>
 .PP
-.BI "int llapi_pcc_detach_fid_fd(int " dirfd ", const struct lu_fid *" fid ");"
+.BI "int llapi_pcc_detach_fid_fd(int " dirfd ", const struct lu_fid *" fid ",
+.BI "                            __u32 " flags );
 .PP
-.BI "int llapi_pcc_detach_fid(const char *" mntpath ", const struct lu_fid *" fid ");"
+.BI "int llapi_pcc_detach_fid(const char *" mntpath ",
+.BI "                         const struct lu_fid *" fid ", __u32 " flags );
 .PP
-.BI "int llapi_pcc_detach_fid_str(const char *" mntpath ", const char *" fidstr ");"
+.BI "int llapi_pcc_detach_fid_str(const char *" mntpath ",
+.BI "                             const char *" fidstr ", __u32 " flags );
 .PP
-.BI "int llapi_pcc_detach_file(const char *" path ");"
+.BI "int llapi_pcc_detach_file(const char *" path ", __u32 " flags );
 .fi
 .SH DESCRIPTION
 .PP
@@ -21,7 +24,8 @@ llapi_pcc_detach_file \- detach the given file from PCC
 .BR llapi_pcc_detach_fid_str() ,
 and
 .BR llapi_pcc_detach_file()
-detaches a cached file from PCC by an ioctl on the dir. The file is referenced
+detach a cached file from PCC by an ioctl on the dir or the file itself. The
+file is referenced
 by
 .IR fid ,
 .IR fidstr ,
@@ -32,6 +36,35 @@ is referenced by
 .IR dirfd ,
 .IR mntpath ,
 .IR path .
+The detach flags is specified by
+.IR flags
+argument, which is a
+.B enum lu_pcc_detach_flags
+data structure, which contains the following values:
+.nf
+.LP
+	PCC_DETACH_FL_NONE			= 0x0,
+	PCC_DETACH_FL_UNCACHE		= 0x01,
+	PCC_DETACH_FL_KNOWN_READWRITE	= 0x02,
+	PCC_DETACH_FL_KNOWN_READONLY	= 0x04,
+	PCC_DETACH_FL_CACHE_REMOVED	= 0x08,
+.fi
+.TP
+PCC_DETACH_FL_NONE
+means that detach the file from PCC yet retain the data copy on PCC backend.
+.TP
+PCC_DETACH_FL_UNCACHE
+means that remove the PCC copy after detach.
+.TP
+PCC_DETACH_KNOWN_READWRITE
+means that it is known that the file was once cached as PCC-RW.
+.TP
+PCC_DETACH_KNOWN_READONLY
+means that it is known that the file was once cached as PCC-RO.
+.TP
+PCC_DETACH_FL_CACHE_REMOVED
+indicates that PCC cached copy is removed. It is used to tell the user space
+caller that the file is detached and the corresponding PCC copy is removed.
 .SH RETURN VALUES
 .LP
 .BR llapi_pcc_detach_fid_fd() ,

lustre/include/lustre/lustreapi.h
@@ -708,16 +708,18 @@ int llapi_pcc_attach_fid(const char *mntpath, const struct lu_fid *fid,
 			 __u32 id, enum lu_pcc_type type);
 int llapi_pcc_attach_fid_str(const char *mntpath, const char *fidstr,
 			     __u32 id, enum lu_pcc_type type);
-int llapi_pcc_detach_fd(int fd, __u32 option);
+int llapi_pcc_detach_fd(int fd, __u32 flags);
 int llapi_pcc_detach_fid(const char *mntpath, const struct lu_fid *fid,
-			 __u32 option);
+			 __u32 flags);
 int llapi_pcc_detach_fid_str(const char *mntpath, const char *fidstr,
-			     __u32 option);
-int llapi_pcc_detach_file(const char *path, __u32 option);
+			     __u32 flags);
+int llapi_pcc_detach_file(const char *path, __u32 flags);
 int llapi_pcc_state_get_fd(int fd, struct lu_pcc_state *state);
 int llapi_pcc_state_get(const char *path, struct lu_pcc_state *state);
 int llapi_pccdev_set(const char *mntpath, const char *cmd);
 int llapi_pccdev_get(const char *mntpath);
+int llapi_pcc_del(const char *mntpath, const char *pccpath, __u32 flags);
+int llapi_pcc_clear(const char *mntpath, __u32 flags);
 /** @} llapi */
 
 /* llapi_layout user interface */

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -650,8 +650,9 @@ struct ll_ioc_lease_id {
 #define LL_IOC_HEAT_GET			_IOWR('f', 251, struct lu_heat)
 #define LL_IOC_HEAT_SET			_IOW('f', 251, __u64)
 #define LL_IOC_PCC_ATTACH		_IOW('f', 252, struct lu_pcc_attach)
-#define LL_IOC_PCC_DETACH		_IOW('f', 252, struct lu_pcc_detach)
-#define LL_IOC_PCC_DETACH_BY_FID	_IOW('f', 252, struct lu_pcc_detach_fid)
+#define LL_IOC_PCC_DETACH		_IOWR('f', 252, struct lu_pcc_detach)
+#define LL_IOC_PCC_DETACH_BY_FID	_IOWR('f', 252, \
+						struct lu_pcc_detach_fid)
 #define LL_IOC_PCC_STATE		_IOR('f', 252, struct lu_pcc_state)
 #define LL_IOC_PROJECT			_IOW('f', 253, struct lu_project)
 
@@ -2944,24 +2945,75 @@ static inline const char *pcc_type2string(enum lu_pcc_type type)
 	}
 }
 
+#define PCC_YAML_PCCPATH	"pccpath"
+#define PCC_YAML_HSMTOOL	"hsmtool"
+#define PCC_YAML_RWID		"rwid"
+#define PCC_YAML_ROID		"roid"
+#define PCC_YAML_FLAGS		"flags"
+#define PCC_YAML_AUTOCACHE	"autocache"
+
+enum hsmtool_type {
+	HSMTOOL_UNKNOWN		= 0,
+	/*
+	 * v1 (original) using 6 directories (oid & 0xffff)/-/-/-/-/-/FID.
+	 * Places only one FID per directory. See ct_path_archive() below.
+	 */
+	HSMTOOL_POSIX_V1	= 1,
+	/* v2 using (OID & 0xffff)^(SEQ & 0xffff)/FID. */
+	HSMTOOL_POSIX_V2	= 2,
+	HSMTOOL_DEFAULT		= HSMTOOL_POSIX_V2,
+};
+
+static inline const char *hsmtool_type2string(enum hsmtool_type type)
+{
+	switch (type) {
+	case HSMTOOL_POSIX_V1:
+		return "posix_v1";
+	case HSMTOOL_POSIX_V2:
+		return "posix_v2";
+	default:
+		return "unknown";
+	}
+}
+
+static inline enum hsmtool_type hsmtool_string2type(const char *str)
+{
+	if (strcmp(str, "posix") == 0)
+		return HSMTOOL_DEFAULT;
+	if (strcmp(str, "posix_v1") == 0)
+		return HSMTOOL_POSIX_V1;
+	if (strcmp(str, "posix_v2") == 0)
+		return HSMTOOL_POSIX_V2;
+
+	return HSMTOOL_UNKNOWN;
+}
+
 struct lu_pcc_attach {
 	__u32 pcca_type; /* PCC type */
 	__u32 pcca_id; /* Attach ID */
 };
 
-enum lu_pcc_detach_opts {
-	PCC_DETACH_OPT_NONE = 0, /* Detach only, keep the PCC copy */
-	PCC_DETACH_OPT_UNCACHE, /* Remove the cached file after detach */
+enum lu_pcc_detach_flags {
+	/* Detach only, keep the PCC copy */
+	PCC_DETACH_FL_NONE		= 0x0,
+	/* Remove the cached file after detach */
+	PCC_DETACH_FL_UNCACHE		= 0x01,
+	/* Known the file was once used as PCC-RW */
+	PCC_DETACH_FL_KNOWN_READWRITE	= 0x02,
+	/* Known the file was once used as PCC-RO */
+	PCC_DETACH_FL_KNOWN_READONLY	= 0x04,
+	/* Indicate PCC cached copy is removed */
+	PCC_DETACH_FL_CACHE_REMOVED	= 0x08,
 };
 
 struct lu_pcc_detach_fid {
 	/* fid of the file to detach */
 	struct lu_fid	pccd_fid;
-	__u32		pccd_opt;
+	__u32		pccd_flags;
 };
 
 struct lu_pcc_detach {
-	__u32		pccd_opt;
+	__u32		pccd_flags;
 };
 
 enum lu_pcc_state_flags {
@@ -2980,6 +3032,12 @@ struct lu_pcc_state {
 	__u32	pccs_flags; /* enum lu_pcc_state_flags */
 	__u32	pccs_padding;
 	char	pccs_path[PATH_MAX];
+};
+
+enum lu_pcc_cleanup_flags {
+	PCC_CLEANUP_FL_NONE		= 0x0,
+	/* Remove the PCC backend but retain the data on the cache */
+	PCC_CLEANUP_FL_KEEP_DATA	= 0x1,
 };
 
 enum lu_project_type {

commit 044fb879b33fb5617782419989ad76c27a569141

LU-10499 pcc: memory corruption in llapi_pcc_scan_detach()
This patch fixes a potential memory corruption detected by
Coverity in the function llapi_pcc_scan_detach().

Test-Parameters: trivial
CoverityID: 426260: Memory - corruptions (OVERRUN)
Fixes: c74878caa79 ("LU-12373 pcc: uncache the pcc copies when remove a PCC backend")
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: Ib40db69ea8bfba3cf9b495396b2bfa50ff138d15
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55386
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/utils/liblustreapi_pcc.c
@@ -544,7 +544,7 @@ static int llapi_pcc_scan_detach(const char *pname, const char *fname,
 {
 	struct lu_pcc_detach_fid detach;
 	char fullname[PATH_MAX];
-	char fidstr[FID_LEN];
+	char fidstr[FID_LEN + 1];
 	const char *fidname;
 	bool lov_file;
 	int rc;
@@ -556,7 +556,7 @@ static int llapi_pcc_scan_detach(const char *pname, const char *fname,
 		size_t len;
 
 		len = strlen(fname) - strlen(".lov");
-		if (len > sizeof(fidstr)) {
+		if (len >= sizeof(fidstr)) {
 			rc = -ENAMETOOLONG;
 			errno = ENAMETOOLONG;
 			llapi_error(LLAPI_MSG_ERROR, rc,