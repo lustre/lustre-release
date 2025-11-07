lustre/lustre-release

commit 277b07e1750ec101e0fe238cada651668a57e39e

LU-18540 lnet: Fix lnetctl ping timeout parsing
The src_nid argument to yaml_lnet_ping() is never null, so we need to
check it with nid_addr_is_set() to determine whether the source key
should be added to the ping command yaml.

sanity-lnet/256 is updated to verify that all ping commands complete
successfully. This verifies that lnetctl ping --timeout works
correctly.

Test-Parameters: trivial testlist=sanity-lnet
Fixes: 5ddc054540 ("LU-17629 utils: support hostname with lustre_lnet_parse_nid_range()")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Id6617b38edc68e49800ae37982390e17bc95e141
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57405
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/utils/lnetctl.c
@@ -5190,7 +5190,7 @@ static int yaml_lnet_ping(char *group, int timeout, struct lnet_nid *src_nid,
 		goto emitter_error;
 
 	if (timeout != 1000 || (src_nid && nid_addr_is_set(src_nid))) {
-		if (src_nid) {
+		if (src_nid && nid_addr_is_set(src_nid)) {
 			char *src_nidstr = libcfs_nidstr(src_nid);
 
 			yaml_scalar_event_initialize(&event, NULL,

lustre/tests/sanity-lnet.sh
@@ -3982,25 +3982,36 @@ test_256() {
 		done
 	done
 
-	local i
+	local idx
+	declare -a pids
 
-	for i in $(seq 1 ${rtr_pc}); do
+	for idx in $(seq 1 ${rtr_pc}); do
+		echo "$LNETCTL ping --timeout $((delay+2)) $rpnid"
 		$LNETCTL ping --timeout $((delay+2)) $rpnid 1>/dev/null &
+		pids[$idx]=$!
 	done
+
+	((idx++))
 
 	echo "Issued ${rtr_pc} pings to $rpnid"
 
-	local pid
-
 	# This ping should be queued on the router's peer NI tx credit queue
 	$LNETCTL ping --timeout $((delay+2)) $rpnid &
+	pids[$idx]=$!
 
 	echo "Issued last ping - sleep $delay"
 	sleep ${delay}
 
 	do_node $router $LCTL net_delay_del -a
 
-	wait
+	local rc=0 rcsum=0
+	for idx in $(seq 1 $((rtr_pc + 1))); do
+		wait ${pids[$idx]} || rc=$?
+		((rc != 0)) && echo "ping pid ${pids[$idx]} returned rc=$rc"
+		((rcsum += rc))
+	done
+
+	((rcsum == 0)) || error "Detected ping failures"
 
 	do_node $router $LNETCTL set transaction_timeout ${old_tto}
 	do_node $router $LNETCTL set retry_count ${old_retry}

commit 18ce133df03a4db11f133eec4ab5a8ddbcaf190d

LU-19473 lnet: nid_addr_is_set false negatives
Numeric based NIDs can have address "0". This causes problems for
nid_addr_is_set() which can only look for a non-zero address value.
This causes two bugs:
 - If a node has multiple NIDs on one net, one of the NIDs has a 0
   address, then an attempt to delete the 0 address NID will cause
   all NIDs on the network to be removed.
 - If the source NID specified for lnetctl ping has a 0 address then
   this is not correctly passed down to the kernel (so the ping is
   processed as though no souce NID were specified).

To solve the first issue we pass a new flag to lnet_dyn_del_ni() to
indicate whether to allow deletion of the entire network in that
function. This is only needed for backward compatability with the
IOCTL interface. The netlink callers of this function always pass a
NID whose address has been "set".

To solve the second issue we modify jt_ping() to pass a NULL argument
to yaml_lnet_ping() when a source nid has not been specified by the
user.

A comment is added to nid_addr_is_set() to document the limitation of
this function.

Fixes: 277b07e175 ("LU-18540 lnet: Fix lnetctl ping timeout parsing")
Fixes: 8f8f6e2f36 ("LU-10003 lnet: use Netlink to support old and new NI APIs.")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Id9c9f8ff93f7cdb040c9c166af4cdfcb64b2b6e8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61822
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -676,7 +676,7 @@ int lnet_dyn_del_net(__u32 net);
 int lnet_dyn_add_ni(struct lnet_ioctl_config_ni *conf, u32 net,
 		    struct lnet_nid *nid,
 		    struct lnet_ioctl_config_lnd_tunables *tun);
-int lnet_dyn_del_ni(struct lnet_nid *nid);
+int lnet_dyn_del_ni(struct lnet_nid *nid, bool allow_net_del);
 int lnet_clear_lazy_portal(struct lnet_ni *ni, int portal, char *reason);
 struct lnet_net *lnet_get_net_locked(__u32 net_id);
 void lnet_net_clr_pref_rtrs(struct lnet_net *net);

lnet/include/uapi/linux/lnet/lnet-types.h
@@ -96,7 +96,32 @@ static inline bool nid_is_nid4(const struct lnet_nid *nid)
 	return NID_ADDR_BYTES(nid) == 4;
 }
 
-/* check for address set */
+/**
+ * nid_addr_is_set - check if address portion of NID is set
+ * @nid: the NID to check
+ *
+ * This function attempts to distinguish between NIDs where:
+ * 1. Only the network is specified (nid_type and nid_num set, address unset)
+ * 2. A full NID is specified (network and address both set)
+ *
+ * LIMITATIONS AND KNOWN ISSUES:
+ * This function returns false for any NID whose address bytes are all zero.
+ * However, this creates ambiguity because NIDs like "0@kfi" or "0@gni" are
+ * valid, fully-specified NIDs where the address portion is legitimately zero.
+ *
+ * The function cannot distinguish between:
+ * - A network-only NID (e.g., "kfi0" with no specific address)
+ * - A fully-qualified NID with address=0 (e.g., "0@kfi")
+ *
+ * This is a fundamental limitation because struct lnet_nid has no explicit
+ * field to mark "address not specified" vs "address is zero". Both cases
+ * result in nid_addr[] being all zeros.
+ *
+ * Callers should be aware that if a user explicitly specifies an address of 0,
+ * this function will incorrectly return false.
+ *
+ * Return: true if any byte in the address portion is non-zero, false otherwise
+ */
 static inline bool nid_addr_is_set(const struct lnet_nid *nid)
 {
 	__u8 *addr = (__u8 *)(&nid->nid_addr[0]);

lnet/lnet/api-ni.c
@@ -4028,7 +4028,7 @@ int lnet_dyn_add_ni(struct lnet_ioctl_config_ni *conf, u32 net_id,
 	return rc;
 }
 
-int lnet_dyn_del_ni(struct lnet_nid *nid)
+int lnet_dyn_del_ni(struct lnet_nid *nid, bool allow_net_del)
 {
 	struct lnet_net *net;
 	struct lnet_ni *ni;
@@ -4058,8 +4058,8 @@ int lnet_dyn_del_ni(struct lnet_nid *nid)
 		goto unlock_net;
 	}
 
-	if (!nid_addr_is_set(nid)) {
-		/* remove the entire net */
+	if (!nid_addr_is_set(nid) && allow_net_del) {
+		CDEBUG(D_NET, "remove the entire net\n");
 		net_bytes = lnet_get_net_ni_bytes_locked(net);
 
 		lnet_net_unlock(0);
@@ -6561,7 +6561,7 @@ lnet_genl_parse_local_ni(struct nlattr *entry, struct genl_info *info,
 
 				found = true;
 				lnet_net_unlock(LNET_LOCK_EX);
-				rc = lnet_dyn_del_ni(&ni->ni_nid);
+				rc = lnet_dyn_del_ni(&ni->ni_nid, false);
 				break;
 			}
 
@@ -6571,7 +6571,7 @@ lnet_genl_parse_local_ni(struct nlattr *entry, struct genl_info *info,
 				lnet_net_unlock(LNET_LOCK_EX);
 			}
 		} else {
-			rc = lnet_dyn_del_ni(&nid);
+			rc = lnet_dyn_del_ni(&nid, false);
 		}
 
 		if (rc < 0) {

lnet/lnet/module.c
@@ -162,7 +162,7 @@ lnet_dyn_unconfigure_ni(struct libcfs_ioctl_hdr *hdr)
 	lnet_nid4_to_nid(conf->lic_nid, &nid);
 	mutex_lock(&lnet_config_mutex);
 	if (the_lnet.ln_niinit_self)
-		rc = lnet_dyn_del_ni(&nid);
+		rc = lnet_dyn_del_ni(&nid, true);
 	else
 		rc = -EINVAL;
 	mutex_unlock(&lnet_config_mutex);

lnet/utils/lnetctl.c
@@ -6198,51 +6198,49 @@ static int yaml_lnet_ping(char *group, int timeout, struct lnet_nid *src_nid,
 	if (rc == 0)
 		goto emitter_error;
 
-	if (timeout != 1000 || (src_nid && nid_addr_is_set(src_nid))) {
-		if (src_nid && nid_addr_is_set(src_nid)) {
-			char *src_nidstr = libcfs_nidstr(src_nid);
+	if (src_nid) {
+		char *src_nidstr = libcfs_nidstr(src_nid);
 
-			yaml_scalar_event_initialize(&event, NULL,
-						     (yaml_char_t *)YAML_STR_TAG,
-						     (yaml_char_t *)"source",
-						     strlen("source"), 1, 0,
-						     YAML_PLAIN_SCALAR_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)"source",
+					     strlen("source"), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
 
-			yaml_scalar_event_initialize(&event, NULL,
-						     (yaml_char_t *)YAML_STR_TAG,
-						     (yaml_char_t *)src_nidstr,
-						     strlen(src_nidstr), 1, 0,
-						     YAML_PLAIN_SCALAR_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
-		}
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)src_nidstr,
+					     strlen(src_nidstr), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
+	}
 
-		if (timeout != 1000) {
-			char time[23];
+	if (timeout != 1000) {
+		char time[23];
 
-			yaml_scalar_event_initialize(&event, NULL,
-						     (yaml_char_t *)YAML_STR_TAG,
-						     (yaml_char_t *)"timeout",
-						     strlen("timeout"), 1, 0,
-						     YAML_PLAIN_SCALAR_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)"timeout",
+					     strlen("timeout"), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
 
-			snprintf(time, sizeof(time), "%u", timeout);
-			yaml_scalar_event_initialize(&event, NULL,
-						     (yaml_char_t *)YAML_INT_TAG,
-						     (yaml_char_t *)time,
-						     strlen(time), 1, 0,
-						     YAML_PLAIN_SCALAR_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
-		}
+		snprintf(time, sizeof(time), "%u", timeout);
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_INT_TAG,
+					     (yaml_char_t *)time,
+					     strlen(time), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		rc = yaml_emitter_emit(&output, &event);
+		if (rc == 0)
+			goto emitter_error;
 	}
 
 	yaml_scalar_event_initialize(&event, NULL,
@@ -6370,8 +6368,8 @@ static int jt_ping(int argc, char **argv)
 	if (rc < 0)
 		return rc;
 
-	rc = yaml_lnet_ping("ping", timeout, &src, optind, argc, argv,
-			    NLM_F_DUMP);
+	rc = yaml_lnet_ping("ping", timeout, src_nidstr ? &src : NULL, optind,
+			    argc, argv, NLM_F_DUMP);
 	if (rc <= 0) {
 		if (rc != -EOPNOTSUPP)
 			return rc;