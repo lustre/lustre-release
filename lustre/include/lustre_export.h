lustre/lustre-release

commit 262957eaef3daeeb4a1bf4f587d4d864fa8aaab1

LU-18691 quota: quota interop check for 64k page clients
When hitting the end of available quota a race condition can be hit
which allows an 64k unaligned I/O to be submitted and causes the
node to hang indefinitely.

This happens when a partial write hits quota limits and a subsequent
write is not aligned on 64k page boundary triggering a hang due to
64k vs 4k page aligned transfers.

HPE-bug-id: LUS-12724
Test-Parameters: testlist=sanity-quota clientarch=ppc64le clientdistro=el8.9 serverdistro=el9.4 env=ONLY=88,ONLY_REPEAT=10
Test-Parameters: testlist=sanity-quota clientarch=ppc64le clientdistro=el8.9 serverdistro=el8.9 serverversion=2.15.4 env=ONLY=88,ONLY_REPEAT=10
Test-Parameters: testlist=sanity-quota clientarch=aarch64 clientdistro=el9.3 serverdistro=el8.10 env=ONLY=88,ONLY_REPEAT=10
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0f8638062f8b0e57207695c45e1fccbd7492c32d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57961
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: xinliang <xinliang.liu@linaro.org>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osc/osc_request.c
@@ -1553,19 +1553,15 @@ osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
 	bool enable_checksum = true;
 	struct cl_page *clpage;
 	u64 foffset = 0;
+	u32 iop_pages = 0;
 
 	ENTRY;
 	if (pga[0]->bp_page) {
 		clpage = oap2cl_page(brw_page2oap(pga[0]));
 		inode = clpage->cp_inode;
-		if (clpage->cp_type == CPT_TRANSIENT) {
+		foffset = pga[0]->bp_off;
+		if (clpage->cp_type == CPT_TRANSIENT)
 			directio = true;
-			/* When page size interop logic is not supported by the
-			 * remote server use the old logic.
-			 */
-			if (imp_connect_unaligned_dio(cli->cl_import))
-				foffset = pga[0]->bp_off;
-		}
 	}
 	if (CFS_FAIL_CHECK(OBD_FAIL_OSC_BRW_PREP_REQ))
 		RETURN(-ENOMEM); /* Recoverable */
@@ -1789,6 +1785,12 @@ retry_encrypt:
 		goto no_bulk;
 	}
 
+	if (foffset)
+		iop_pages = interop_pages(foffset, page_count, pga);
+	/* need interop but server does not support, return failure */
+	if (iop_pages && !imp_connect_unaligned_dio(cli->cl_import))
+			GOTO(out, rc = -EINVAL); /* -EDQUOT? */
+
 	desc = ptlrpc_prep_bulk_imp(req, page_count,
 		cli->cl_import->imp_connect_data.ocd_brw_size >> LNET_MTU_BITS,
 		(opc == OST_WRITE ? PTLRPC_BULK_GET_SOURCE :
@@ -1800,8 +1802,8 @@ retry_encrypt:
 		GOTO(out, rc = -ENOMEM);
 	/* NB request now owns desc and will free it when it gets freed */
 	desc->bd_is_rdma = gpu;
-	if (directio && foffset)
-		desc->bd_md_offset = interop_pages(foffset, page_count, pga);
+	if (iop_pages)
+		desc->bd_md_offset = iop_pages;
 
 no_bulk:
 	body = req_capsule_client_get(pill, &RMF_OST_BODY);

lustre/tests/sanity-quota.sh
@@ -6592,6 +6592,38 @@ test_87()
 }
 run_test 87 "lfs quota -a should print default quota setting"
 
+# interop quota
+test_88()
+{
+	(($PAGE_SIZE > 4096)) || skip "require client with >4k pages"
+	setup_quota_test || error "setup quota failed with $?"
+
+	set_ost_qtype $QTYPE || error "enable ost quota failed"
+
+	$LFS setquota -u $TSTUSR -B 100M -i 0 $MOUNT ||
+		error "enable quota -B 100M failed."
+
+	local tfile
+	local result
+
+	local repeat=$(seq 10)
+	local arr=(1075761 1075770 1075800 1076000 1080000 1093000 2010000 \
+		   2080000 2095000 4096000)
+	[[ "$SLOW" = "no" ]] && repeat=1
+
+	for r in $repeat; do
+		for bs in ${arr[@]}; do
+			tfile=$DIR/$tdir/dd_largefile.${bs}
+			${RUNAS} dd if=/dev/urandom of=${tfile} bs=${bs} \
+				count=100 status=progress
+			rm -f ${tfile}
+		done
+	done
+
+	return 0
+}
+run_test 88 "Writing over quota should not hang"
+
 check_quota_no_mount()
 {
 	local opts="$1"

commit e74ab6aed6ecc51b42b9297683b3254749b592e1

LU-19180 ptlrpc: virtual 4k bulk alignment
A 64k side cannot transfer a 64k page if the first page has
an offset, so the total size exceeds LNet_MTU. However, a 4k
side can transfer some pages in MD for the same transfer.
This causes a situation where MD has a different amount of
data for transfer on both sides, and the transfer is hung.
Use 4k virtual alignment for the transfer to synchronize
both sides.

Test-Parameters: clientdistro=rocky9.5 clientarch=aarch64 serverarch=x86_64 env=SANITY_EXCEPT=130i
Test-Parameters: serverversion=2.15 env=ONLY=16 testlist=sanityn
Fixes: 262957eaef ("LU-18691 quota: quota interop check for 64k page clients")
Fixes: 07a1272233 ("LU-17525 llite: unaligned DIO interop page alignment")
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I8939a1dd0ddf01518025fc0587d1ec37aee99117
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60398
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_export.h
@@ -527,13 +527,6 @@ static inline bool imp_connect_replay_create(struct obd_import *imp)
 #define imp_connect_replay_create(exp) true
 #endif
 
-static inline bool imp_connect_unaligned_dio(struct obd_import *imp)
-{
-	struct obd_connect_data *ocd = &imp->imp_connect_data;
-
-	return (ocd->ocd_connect_flags2 & OBD_CONNECT2_UNALIGNED_DIO);
-}
-
 static inline bool exp_connect_unaligned_dio(struct obd_export *exp)
 {
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_UNALIGNED_DIO);

lustre/include/lustre_net.h
@@ -73,14 +73,7 @@
  * RPC count.
  */
 #define PTLRPC_BULK_OPS_MASK	(~((__u64)PTLRPC_BULK_OPS_COUNT - 1))
-/*
- * Unaligned DIO adjust MD size for alignment to the interop page size
- * Enable page alignmen interop range:
- *  MD_MAX_INTEROP_PAGE_SIZE(64k) <-> MD_MIN_INTEROP_PAGE_SIZE(4k)
- */
-#define MD_MIN_INTEROP_PAGE_SHIFT	12
-#define MD_MIN_INTEROP_PAGE_SIZE	(1u << MD_MIN_INTEROP_PAGE_SHIFT)
-#define MD_MAX_INTEROP_PAGE_SIZE	(1u << 16)
+#define PTLRPC_BULK_INTEROP_PAGE_SIZE	4096
 /**
  * Define maxima for bulk I/O.
  *
@@ -1422,12 +1415,12 @@ extern const struct ptlrpc_bulk_frag_ops ptlrpc_bulk_kiov_nopin_ops;
 struct ptlrpc_bulk_desc {
 	unsigned int	bd_refs; /* number MD's assigned including zero-sends */
 	/** completed with failure */
-	unsigned long bd_failure:1;
+	unsigned long bd_failure:1,
 	/** client side */
-	unsigned short bd_md_offset; /* offset in 4k pages ranged [0, 15] */
-	unsigned int bd_registered:1,
-	/* bulk request is RDMA transfer, use page->host as real address */
-		     bd_is_rdma:1;
+		    bd_registered:1,
+	/* bulk request is GPU RDMA transfer, use page->host as real address */
+		     bd_is_rdma:1,
+		     bd_is_srv:1; /* export or import should be used */
 	/** For serialization with callback */
 	spinlock_t bd_lock;
 	/** {put,get}{source,sink}{kvec,kiov} */

lustre/osc/osc_cache.c
@@ -1805,7 +1805,35 @@ struct extent_rpc_data {
 	unsigned int		erd_max_pages;
 	unsigned int		erd_max_chunks;
 	unsigned int		erd_max_extents;
+	unsigned int		erd_max_io_slots; /* max iov count */
+	unsigned int		erd_io_slots;	  /* iov used */
 };
+
+#define IO_SLOTS(brw)	(((size_t)LNET_MAX_IOV * (brw)) >> LNET_MTU_BITS)
+
+#define write_rpc_def(rpclist, cli)			\
+{							\
+	.erd_rpc_list	= (rpclist),			\
+	.erd_page_count	= 0,				\
+	.erd_max_pages	= (cli)->cl_max_pages_per_rpc,	\
+	.erd_max_chunks	= osc_max_write_chunks(cli),	\
+	.erd_max_extents = 256,				\
+	.erd_io_slots	= 0,				\
+	.erd_max_io_slots = IO_SLOTS(\
+		(cli)->cl_import->imp_connect_data.ocd_brw_size), \
+}
+
+#define read_rpc_def(rpclist, cli)			\
+{							\
+	.erd_rpc_list	= (rpclist),			\
+	.erd_page_count	= 0,				\
+	.erd_max_pages	= (cli)->cl_max_pages_per_rpc,	\
+	.erd_max_chunks	= UINT_MAX,			\
+	.erd_max_extents = UINT_MAX,			\
+	.erd_io_slots	= 0,				\
+	.erd_max_io_slots = IO_SLOTS(\
+		(cli)->cl_import->imp_connect_data.ocd_brw_size), \
+}
 
 static inline unsigned osc_extent_chunks(const struct osc_extent *ext)
 {
@@ -1854,6 +1882,9 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 {
 	struct osc_extent *tmp;
 	unsigned int chunk_count;
+#if PAGE_SIZE != PTLRPC_BULK_INTEROP_PAGE_SIZE
+	unsigned int unalign_tax = 0;
+#endif
 	ENTRY;
 
 	EASSERT((ext->oe_state == OES_CACHE || ext->oe_state == OES_LOCK_DONE),
@@ -1874,12 +1905,23 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 
 	data->erd_max_pages = max(ext->oe_mppr, data->erd_max_pages);
 	EASSERTF(data->erd_page_count != 0 ||
-		ext->oe_nr_pages <= data->erd_max_pages, ext,
-		"The first extent to be fit in a RPC contains %u pages, "
-		"which is over the limit %u.\n", ext->oe_nr_pages,
-		data->erd_max_pages);
+		 ext->oe_nr_pages <= data->erd_max_pages, ext,
+		 "The first extent to be fit in a RPC contains %u pages, "
+		 "which is over the limit %u.\n", ext->oe_nr_pages,
+		 data->erd_max_pages);
 	if (data->erd_page_count + ext->oe_nr_pages > data->erd_max_pages)
 		RETURN(0);
+
+#if PAGE_SIZE != PTLRPC_BULK_INTEROP_PAGE_SIZE
+	/** unaligned extent might needs one more page due split */
+	if (ext->oe_start != 0)
+		unalign_tax = 1;
+
+	/* check for full bulk size when free bulk 64k IOV remain */
+	if (data->erd_io_slots + ext->oe_nr_pages + unalign_tax >
+	    data->erd_max_io_slots)
+		RETURN(0);
+#endif
 
 	list_for_each_entry(tmp, data->erd_rpc_list, oe_link) {
 		EASSERT(tmp->oe_owner == current, tmp);
@@ -1891,6 +1933,9 @@ static int try_to_add_extent_for_io(struct client_obd *cli,
 	data->erd_max_extents--;
 	data->erd_max_chunks -= chunk_count;
 	data->erd_page_count += ext->oe_nr_pages;
+#if PAGE_SIZE != PTLRPC_BULK_INTEROP_PAGE_SIZE
+	data->erd_io_slots += ext->oe_nr_pages + unalign_tax;
+#endif
 	list_move_tail(&ext->oe_link, data->erd_rpc_list);
 	ext->oe_owner = current;
 	RETURN(1);
@@ -1914,13 +1959,7 @@ static unsigned int get_write_extents(struct osc_object *obj,
 {
 	struct client_obd *cli = osc_cli(obj);
 	struct osc_extent *ext;
-	struct extent_rpc_data data = {
-		.erd_rpc_list	= rpclist,
-		.erd_page_count	= 0,
-		.erd_max_pages	= cli->cl_max_pages_per_rpc,
-		.erd_max_chunks	= osc_max_write_chunks(cli),
-		.erd_max_extents = 256,
-	};
+	struct extent_rpc_data data = write_rpc_def(rpclist, cli);
 
 	assert_osc_object_is_locked(obj);
 	while ((ext = list_first_entry_or_null(&obj->oo_hp_exts,
@@ -2041,13 +2080,7 @@ static unsigned int get_read_extents(struct osc_object *obj,
 	struct client_obd *cli = osc_cli(obj);
 	struct osc_extent *ext;
 	struct osc_extent *next;
-	struct extent_rpc_data data = {
-		.erd_rpc_list	= rpclist,
-		.erd_page_count	= 0,
-		.erd_max_pages	= cli->cl_max_pages_per_rpc,
-		.erd_max_chunks	= UINT_MAX,
-		.erd_max_extents = UINT_MAX,
-	};
+	struct extent_rpc_data data = read_rpc_def(rpclist, cli);
 
 	assert_osc_object_is_locked(obj);
 	while ((ext = list_first_entry_or_null(&obj->oo_hp_read_exts,

lustre/osc/osc_request.c
@@ -1493,33 +1493,6 @@ static inline void osc_release_bounce_pages(struct brw_page **pga,
 #endif
 }
 
-static inline bool is_interop_required(u64 foffset, u32 off0, u32 npgs,
-				       struct brw_page **pga)
-{
-	struct brw_page *pg0 = pga[0];
-	struct brw_page *pgN = pga[npgs - 1];
-	const u32 nob = ((npgs - 2) << PAGE_SHIFT) + pg0->bp_count +
-			pgN->bp_count;
-
-	return ((nob + off0) >= LNET_MTU &&
-	    cl_io_nob_aligned(foffset, nob, MD_MAX_INTEROP_PAGE_SIZE) !=
-	    cl_io_nob_aligned(foffset, nob, MD_MIN_INTEROP_PAGE_SIZE));
-}
-
-static inline u32 interop_pages(u64 foffset, u32 npgs, struct brw_page **pga)
-{
-	u32 off0;
-
-	if (foffset == 0 || npgs < 15)
-		return 0;
-
-	off0 = (foffset & (MD_MAX_INTEROP_PAGE_SIZE - 1));
-	if (is_interop_required(foffset, off0, npgs, pga))
-		return off0 >> MD_MIN_INTEROP_PAGE_SHIFT;
-
-	return 0;
-}
-
 static int
 osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
 		     u32 page_count, struct brw_page **pga,
@@ -1540,15 +1513,13 @@ osc_brw_prep_request(int cmd, struct client_obd *cli, struct obdo *oa,
 	bool directio = false;
 	bool gpu = 0;
 	bool enable_checksum = true;
+	unsigned int unaligned = 0;
 	struct cl_page *clpage;
-	u64 foffset = 0;
-	u32 iop_pages = 0;
 
 	ENTRY;
 	if (pga[0]->bp_page) {
 		clpage = oap2cl_page(brw_page2oap(pga[0]));
 		inode = clpage->cp_inode;
-		foffset = pga[0]->bp_off;
 		if (clpage->cp_type == CPT_TRANSIENT)
 			directio = true;
 	}
@@ -1725,6 +1696,11 @@ retry_encrypt:
 
 	for (i = 0; i < page_count; i++) {
 		short_io_size += pga[i]->bp_count;
+		/* each unaligned page might needs one page for split */
+#if PAGE_SIZE != PTLRPC_BULK_INTEROP_PAGE_SIZE
+		if (pga[i]->bp_off != 0)
+			unaligned++;
+#endif
 		if (!inode || !IS_ENCRYPTED(inode) ||
 		    !ll_has_encryption_key(inode)) {
 			pga[i]->bp_count_diff = 0;
@@ -1774,13 +1750,7 @@ retry_encrypt:
 		goto no_bulk;
 	}
 
-	if (foffset)
-		iop_pages = interop_pages(foffset, page_count, pga);
-	/* need interop but server does not support, return failure */
-	if (iop_pages && !imp_connect_unaligned_dio(cli->cl_import))
-			GOTO(out, rc = -EINVAL); /* -EDQUOT? */
-
-	desc = ptlrpc_prep_bulk_imp(req, page_count,
+	desc = ptlrpc_prep_bulk_imp(req, page_count + unaligned,
 		cli->cl_import->imp_connect_data.ocd_brw_size >> LNET_MTU_BITS,
 		(opc == OST_WRITE ? PTLRPC_BULK_GET_SOURCE :
 			PTLRPC_BULK_PUT_SINK),
@@ -1791,9 +1761,6 @@ retry_encrypt:
 		GOTO(out, rc = -ENOMEM);
 	/* NB request now owns desc and will free it when it gets freed */
 	desc->bd_is_rdma = gpu;
-	if (iop_pages)
-		desc->bd_md_offset = iop_pages;
-
 no_bulk:
 	body = osc_pack_req_body(req, oa);
 	ioobj = req_capsule_client_get(pill, &RMF_OBD_IOOBJ);
@@ -1899,8 +1866,7 @@ no_bulk:
 	 *  - OBD_IOOBJ_INTEROP_PAGE_ALIGNMENT
 	 */
 	if (desc)
-		ioobj_max_brw_set(ioobj, desc->bd_md_max_brw,
-				  desc->bd_md_offset);
+		ioobj_max_brw_set(ioobj, desc->bd_md_max_brw, 0);
 	else
 		ioobj_max_brw_set(ioobj, 0, 0); /* short io */
 

lustre/ptlrpc/client.c
@@ -186,7 +186,6 @@ struct ptlrpc_bulk_desc *ptlrpc_new_bulk(unsigned int nfrags,
 	desc->bd_frag_ops = ops;
 	LASSERT(max_brw > 0);
 	desc->bd_md_max_brw = min(max_brw, PTLRPC_BULK_OPS_COUNT);
-	desc->bd_md_offset = 0;
 	/*
 	 * PTLRPC_BULK_OPS_COUNT is the compile-time transfer limit for this
 	 * node. Negotiated ocd_brw_size will always be <= this number.
@@ -245,46 +244,19 @@ struct ptlrpc_bulk_desc *ptlrpc_prep_bulk_imp(struct ptlrpc_request *req,
 }
 EXPORT_SYMBOL(ptlrpc_prep_bulk_imp);
 
-#define MD0_PAGE_SHIFT	(PAGE_SHIFT - MD_MIN_INTEROP_PAGE_SHIFT)
+#define IOP_MASK	(~(PTLRPC_BULK_INTEROP_PAGE_SIZE - 1))
+#define IOP_LEN(len)	((len + PTLRPC_BULK_INTEROP_PAGE_SIZE - 1) & IOP_MASK)
 
-void __ptlrpc_prep_bulk_page(struct ptlrpc_bulk_desc *desc,
+static void __ptlrpc_add_bulk_chunk(struct ptlrpc_bulk_desc *desc,
 			     struct page *page, int pageoffset, int len,
 			     int pin)
 {
 	struct bio_vec *kiov;
-	int ilen = len;
-	int start = 0;
-	int nvecs = desc->bd_iov_count;
-
-	LASSERT(desc->bd_iov_count < desc->bd_max_iov);
-	LASSERT(page != NULL);
-	LASSERT(pageoffset >= 0);
-	LASSERT(len > 0);
-	LASSERT(pageoffset + len <= PAGE_SIZE);
 
 	kiov = &desc->bd_vec[desc->bd_iov_count];
 
-	/* unaligned i/o: accelerate MD0 consumption based offset 4k pages */
-	if (desc->bd_md_offset && desc->bd_md_count == 1)
-		nvecs += desc->bd_md_offset >> MD0_PAGE_SHIFT;
-
-	/* unaligned i/o: first vector may be less than LNET_MAX_IOV */
-	if (desc->bd_md_count > 0)
-		start = desc->bd_mds_off[desc->bd_md_count - 1];
-	nvecs -= start; /* kiov enties in this MD */
-	/* Initial page or adding this page will exceed iov or mtu limit */
-	if (desc->bd_iov_count == 0 || nvecs == LNET_MTU_IOV_LIMIT ||
-	    (desc->bd_iop_len + ilen) > LNET_MTU) {
-		desc->bd_mds_off[desc->bd_md_count++] = desc->bd_iov_count;
-		LASSERT(desc->bd_md_count <= PTLRPC_BULK_OPS_LIMIT);
-		desc->bd_iop_len = 0;
-		/* extend max_brw to the next power of 2 */
-		if (desc->bd_md_count > desc->bd_md_max_brw &&
-		    (desc->bd_md_max_brw << 1) <= PTLRPC_BULK_OPS_COUNT)
-			desc->bd_md_max_brw = (desc->bd_md_max_brw << 1);
-	}
-	desc->bd_iop_len += ilen; /* this vector, if 64k page aligned */
-	desc->bd_nob += len; /* total number of bytes for this bulk */
+	desc->bd_iop_len += IOP_LEN(len);
+	desc->bd_nob += len;
 
 	if (pin)
 		get_page(page);
@@ -294,6 +266,44 @@ void __ptlrpc_prep_bulk_page(struct ptlrpc_bulk_desc *desc,
 	kiov->bv_len = len;
 
 	desc->bd_iov_count++;
+	LASSERT(desc->bd_iov_count <= desc->bd_max_iov);
+}
+
+/* add one page to desc->bd_vec bio_vec array and advance */
+void __ptlrpc_prep_bulk_page(struct ptlrpc_bulk_desc *desc,
+			     struct page *page, int pageoffset, int len,
+			     int pin)
+{
+	LASSERT(page != NULL);
+	LASSERT(pageoffset >= 0);
+	LASSERT(len > 0);
+	LASSERT(pageoffset + len <= PAGE_SIZE);
+
+restart:
+	if (((desc->bd_iov_count % LNET_MAX_IOV) == 0) ||
+	    ((desc->bd_iop_len + PTLRPC_BULK_INTEROP_PAGE_SIZE) > LNET_MTU)) {
+		/* no free for align chunk */
+		desc->bd_mds_off[desc->bd_md_count] = desc->bd_iov_count;
+		desc->bd_md_count++;
+		desc->bd_iop_len = 0;
+		LASSERT(desc->bd_md_count <= PTLRPC_BULK_OPS_LIMIT);
+		if (desc->bd_md_count > desc->bd_md_max_brw &&
+		   (desc->bd_md_max_brw << 1) <= PTLRPC_BULK_OPS_COUNT)
+			desc->bd_md_max_brw = (desc->bd_md_max_brw << 1);
+	} else if ((desc->bd_iop_len + len) > LNET_MTU) {
+		/* can't fit full chunk but should have some aligned chunks */
+		/* lets find how much align chunks can fit in current md */
+		unsigned int ch = LNET_MTU - desc->bd_iop_len;
+		unsigned int sz = ch & ~(PTLRPC_BULK_INTEROP_PAGE_SIZE - 1);
+
+		__ptlrpc_add_bulk_chunk(desc, page, pageoffset, sz, pin);
+
+		/* create a new, different kiov chunk mapped to the same page */
+		pageoffset += sz;
+		len -= sz;
+		goto restart;
+	}
+	__ptlrpc_add_bulk_chunk(desc, page, pageoffset, len, pin);
 }
 EXPORT_SYMBOL(__ptlrpc_prep_bulk_page);
 
@@ -311,7 +321,7 @@ void ptlrpc_free_bulk(struct ptlrpc_bulk_desc *desc)
 
 	obd_pool_put_desc_pages(desc);
 
-	if (desc->bd_export)
+	if (desc->bd_is_srv)
 		class_export_put(desc->bd_export);
 	else
 		class_import_put(desc->bd_import);

lustre/ptlrpc/niobuf.c
@@ -151,6 +151,7 @@ struct ptlrpc_bulk_desc *ptlrpc_prep_bulk_exp(struct ptlrpc_request *req,
 
 	desc->bd_export = class_export_get(exp);
 	desc->bd_req = req;
+	desc->bd_is_srv = 1;
 
 	desc->bd_cbid.cbid_fn  = server_bulk_callback;
 	desc->bd_cbid.cbid_arg = desc;

lustre/target/tgt_handler.c
@@ -2439,8 +2439,6 @@ int tgt_brw_read(struct tgt_session_info *tsi)
 					    &ptlrpc_bulk_kiov_nopin_ops);
 		if (desc == NULL)
 			GOTO(out_commitrw, rc = -ENOMEM);
-		/* client may have MD handling requirements  */
-		desc->bd_md_offset = ioobj_page_interop_offset(ioo);
 	}
 
 	npages_read = npages;
@@ -2849,9 +2847,6 @@ int tgt_brw_write(struct tgt_session_info *tsi)
 					    &ptlrpc_bulk_kiov_nopin_ops);
 		if (desc == NULL)
 			GOTO(skip_transfer, rc = -ENOMEM);
-		/* client may have MD handling requirements  */
-		desc->bd_md_offset = ioobj_page_interop_offset(ioo);
-
 		/* NB Having prepped, we must commit... */
 		for (i = 0; i < npages; i++)
 			desc->bd_frag_ops->add_kiov_frag(desc,