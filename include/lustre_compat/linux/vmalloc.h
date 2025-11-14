lustre/lustre-release

commit 87da90954d3524db891f6b5ab2ef0bfa09bf4307

LU-18687 compat: use unexported vfree_atomic in kernel
With the removal of RHEL7 support we can drop the special
vfree_atomic() implementation we have. Instead just use
cfs_kallsym_lookup to use vfree_atomic() that is available
in modern kernels.

Test-Parameters: trivial
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I1cff923eda6a7b6df8db81477b9cd507416fb825
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61414
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

include/lustre_compat/linux/vmalloc.h
@@ -1,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __LIBCFS_LINUX_VMALLOC_H
+#define __LIBCFS_LINUX_VMALLOC_H
+
+#include <linux/vmalloc.h>
+
+#ifdef HAVE_VMALLOC_2ARGS
+#define __compat_vmalloc(size, flags) __vmalloc(size, flags)
+#else
+#define __compat_vmalloc(size, flags) __vmalloc(size, flags, PAGE_KERNEL)
+#endif
+
+extern void compat_vfree_atomic(const void *addr);
+
+#endif /* __LICBFS_LINUX_VMALLOC_H */

libcfs/libcfs/linux/linux-prim.c
@@ -37,61 +37,9 @@
 #include <lustre_compat/linux/wait.h>
 #include <lustre_crypto.h>
 
-/*
- * This is opencoding of vfree_atomic from Linux kernel added in 4.10 with
- * minimum changes needed to work on older kernels too.
- */
-
-#ifndef llist_for_each_safe
-#define llist_for_each_safe(pos, n, node)                       \
-	for ((pos) = (node); (pos) && ((n) = (pos)->next, true); (pos) = (n))
-#endif
-
-struct vfree_deferred {
-	struct llist_head list;
-	struct work_struct wq;
-};
-static DEFINE_PER_CPU(struct vfree_deferred, vfree_deferred);
-
-static void free_work(struct work_struct *w)
-{
-	struct vfree_deferred *p = container_of(w, struct vfree_deferred, wq);
-	struct llist_node *t, *llnode;
-
-	llist_for_each_safe(llnode, t, llist_del_all(&p->list))
-		vfree((void *)llnode);
-}
-
-void libcfs_vfree_atomic(const void *addr)
-{
-	struct vfree_deferred *p = raw_cpu_ptr(&vfree_deferred);
-
-	if (!addr)
-		return;
-
-	if (llist_add((struct llist_node *)addr, &p->list))
-		schedule_work(&p->wq);
-}
-EXPORT_SYMBOL(libcfs_vfree_atomic);
-
-void __init init_libcfs_vfree_atomic(void)
-{
-	int i;
-
-	for_each_possible_cpu(i) {
-		struct vfree_deferred *p;
-
-		p = &per_cpu(vfree_deferred, i);
-		init_llist_head(&p->list);
-		INIT_WORK(&p->wq, free_work);
-	}
-}
-
 int __init cfs_arch_init(void)
 {
 	int rc = 0;
-
-	init_libcfs_vfree_atomic();
 
 #ifndef HAVE_WAIT_VAR_EVENT
 	wait_bit_init();
@@ -123,9 +71,6 @@ failed:
 
 void __exit cfs_arch_exit(void)
 {
-	/* exit_libcfs_vfree_atomic */
-	__flush_workqueue(system_wq);
-
 	shrinker_debugfs_fini();
 #ifdef CONFIG_LL_ENCRYPTION
 	llcrypt_exit();

lustre/include/obd_support.h
@@ -882,7 +882,7 @@ do {									      \
 #define __OBD_VMALLOC_VERBOSE(ptr, cptab, cpt, size)			      \
 do {									      \
 	(ptr) = cptab == NULL ?						      \
-		__ll_vmalloc(size, GFP_NOFS | __GFP_HIGHMEM | __GFP_ZERO) :   \
+		__compat_vmalloc(size, GFP_NOFS | __GFP_HIGHMEM | __GFP_ZERO) :\
 		cfs_cpt_vzalloc(cptab, cpt, size);			      \
 	if (unlikely((ptr) == NULL)) {                                        \
 		CERROR("vmalloc of '" #ptr "' (%d bytes) failed\n",           \
@@ -967,7 +967,7 @@ do {									      \
 	if (is_vmalloc_addr(ptr)) {					      \
 		OBD_FREE_PRE(ptr, size, "vfreed");			      \
 		POISON(ptr, 0x5a, size);				      \
-		libcfs_vfree_atomic(ptr);				      \
+		compat_vfree_atomic(ptr);				      \
 		POISON_PTR(ptr);					      \
 	} else {							      \
 		OBD_FREE(ptr, size);					      \

lustre_compat/symbols.c
@@ -69,6 +69,14 @@ int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
 }
 EXPORT_SYMBOL_GPL(compat_apply_workqueue_attrs);
 
+static void (*__vfree_atomic)(const void *addr);
+
+void compat_vfree_atomic(const void *addr)
+{
+	__vfree_atomic(addr);
+}
+EXPORT_SYMBOL_GPL(compat_vfree_atomic);
+
 int lustre_symbols_init(void)
 {
 	int rc;
@@ -86,6 +94,10 @@ int lustre_symbols_init(void)
 
 	__apply_workqueue_attrs = cfs_kallsyms_lookup_name("apply_workqueue_attrs");
 	if (!__apply_workqueue_attrs)
+		return -EINVAL;
+
+	__vfree_atomic = cfs_kallsyms_lookup_name("vfree_atomic");
+	if (!__vfree_atomic)
 		return -EINVAL;
 
 	return 0;

include/linux/libcfs/libcfs_private.h
@@ -21,8 +21,7 @@
 #endif
 
 #include <linux/slab.h>
-#include <linux/vmalloc.h>
-#include <lustre_compat/linux/linux-mem.h>
+#include <lustre_compat/linux/vmalloc.h>
 
 #ifdef LIBCFS_DEBUG
 
@@ -211,7 +210,7 @@ do {									\
 	if (likely(ptr)) {						\
 		LIBCFS_FREE_PRE(ptr, (size), "kfreed");			\
 		if (unlikely(s > LIBCFS_VMALLOC_SIZE))			\
-			libcfs_vfree_atomic(ptr);			\
+			compat_vfree_atomic(ptr);			\
 		else							\
 			kfree(ptr);					\
 	}								\

include/lustre_compat/linux/linux-mem.h
@@ -48,13 +48,6 @@ static inline void bitmap_free(const unsigned long *bitmap)
 }
 #endif /* !HAVE_BITMAP_ALLOC */
 
-/*
- * Shrinker
- */
-#ifndef SHRINK_STOP
-# define SHRINK_STOP (~0UL)
-#endif
-
 #ifndef HAVE_MMAP_LOCK
 static inline void mmap_write_lock(struct mm_struct *mm)
 {
@@ -94,18 +87,6 @@ static inline bool mmap_write_trylock(struct mm_struct *mm)
 }
  #endif /* HAVE_MMAP_WRITE_TRYLOCK */
 #endif
-
-#ifdef HAVE_VMALLOC_2ARGS
-#define __ll_vmalloc(size, flags) __vmalloc(size, flags)
-#else
-#define __ll_vmalloc(size, flags) __vmalloc(size, flags, PAGE_KERNEL)
-#endif
-
-void init_libcfs_vfree_atomic(void);
-void exit_libcfs_vfree_atomic(void);
-
-/* atomic-context safe vfree */
-void libcfs_vfree_atomic(const void *addr);
 
 #ifndef HAVE_KFREE_SENSITIVE
 #define kfree_sensitive(x)      kzfree(x)

lustre/osd-wbcfs/wbcfs.c
@@ -19,6 +19,7 @@
 #include <linux/statfs.h>
 #include <linux/fs_context.h>
 
+#include <lustre_compat/linux/linux-mem.h>
 #include <lustre_compat.h>
 
 #include "wbcfs.h"

commit 94b21a46bfa186d1c455b4699d28ba96761cc71c

LU-18687 obd: don't always use vfree_atomic
There is a large chance upstream will not want to export
vfree_atomic(). Also there are issues with our approach to
export vfree_atomic() so reintroduce our manual version of
handling vfree_atomic(). Sadly this shows that the day will
come that some key component in the kernel needed by Lustre
will not be available and permentally break out of tree
Lustre. We do limit the use of this functionality to only
the ptlrpc layer. Instead of working around poor coding we
should fix any potential __might_sleep issues. The only
reason we need this functionality is to handle the poor
import lock handling in the ptlrpc layer. What is really
needed is to rework the locking in ptlrpc and this code can
go away.

Fixes: 87da90954 ("LU-18687 compat: use unexported vfree_atomic in kernel")
Change-Id: I588f171e242eb0b61003ece4e664add4c90bc865
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61855
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

include/lustre_compat/linux/vmalloc.h
@@ -13,4 +13,7 @@
 
 extern void compat_vfree_atomic(const void *addr);
 
+void init_compat_vfree_atomic(void);
+void exit_compat_vfree_atomic(void);
+
 #endif /* __LICBFS_LINUX_VMALLOC_H */

include/lustre_compat/linux/workqueue.h
@@ -24,4 +24,8 @@ struct workqueue_attrs *compat_alloc_workqueue_attrs(void);
 int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
 				  const struct workqueue_attrs *attrs);
 
+#ifndef HAVE_FLUSH___WORKQUEUE
+#define __flush_workqueue(wq)  flush_scheduled_work()
+#endif
+
 #endif /* __LIBCFS_WORKQUEUE_H__ */

libcfs/libcfs/linux/linux-prim.c
@@ -36,6 +36,7 @@
 #include <lustre_compat/linux/wait_bit.h>
 #include <lustre_compat/linux/wait.h>
 #include <lustre_compat/linux/shrinker.h>
+#include <lustre_compat/linux/vmalloc.h>
 #include <lustre_crypto.h>
 
 int __init cfs_arch_init(void)
@@ -45,6 +46,8 @@ int __init cfs_arch_init(void)
 #ifndef HAVE_WAIT_VAR_EVENT
 	wait_bit_init();
 #endif
+	init_compat_vfree_atomic();
+
 	rc = lustre_symbols_init();
 	if (rc < 0) {
 		pr_info("lustre_symbols_init: error %d\n", rc);
@@ -72,6 +75,7 @@ failed:
 
 void __exit cfs_arch_exit(void)
 {
+	exit_compat_vfree_atomic();
 	shrinker_debugfs_fini();
 #ifdef CONFIG_LL_ENCRYPTION
 	llcrypt_exit();

lustre/include/lustre_compat.h
@@ -731,10 +731,6 @@ static inline pgoff_t folio_index_page(struct page *page)
 
 #endif /* HAVE_FOLIO_BATCH && HAVE_FILEMAP_GET_FOLIOS */
 
-#ifndef HAVE_FLUSH___WORKQUEUE
-#define __flush_workqueue(wq)	flush_scheduled_work()
-#endif
-
 #ifdef HAVE_NSPROXY_COUNT_AS_REFCOUNT
 #define nsproxy_dec(ns)		refcount_dec(&(ns)->count)
 #else

lustre/include/obd_support.h
@@ -943,7 +943,7 @@ do {									\
 	}								\
 } while (0)
 
-#define OBD_FREE_LARGE(ptr, size)					      \
+#define OBD_FREE_LARGE_ATOMIC(ptr, size)			\
 do {									      \
 	if (is_vmalloc_addr(ptr)) {					      \
 		OBD_FREE_PRE(ptr, size, "vfreed");			      \
@@ -952,6 +952,15 @@ do {									      \
 	} else {							      \
 		OBD_FREE(ptr, size);					      \
 	}                                                                     \
+} while (0)
+
+#define OBD_FREE_LARGE(ptr, size)				\
+do {								\
+	if (likely(ptr)) {					\
+		OBD_FREE_PRE(ptr, size, "kvfree");		\
+		kvfree(ptr);					\
+		POISON_PTR(ptr);				\
+	}							\
 } while (0)
 
 #define OBD_FREE_PTR_ARRAY_LARGE(ptr, n)			\

lustre/ptlrpc/sec_null.c
@@ -200,7 +200,7 @@ void null_free_repbuf(struct ptlrpc_sec *sec,
 {
 	LASSERT(req->rq_repbuf);
 
-	OBD_FREE_LARGE(req->rq_repbuf, req->rq_repbuf_len);
+	OBD_FREE_LARGE_ATOMIC(req->rq_repbuf, req->rq_repbuf_len);
 	req->rq_repbuf = NULL;
 	req->rq_repbuf_len = 0;
 }

lustre/ptlrpc/sec_plain.c
@@ -574,7 +574,7 @@ void plain_free_reqbuf(struct ptlrpc_sec *sec,
 {
 	ENTRY;
 	if (!req->rq_pool) {
-		OBD_FREE_LARGE(req->rq_reqbuf, req->rq_reqbuf_len);
+		OBD_FREE_LARGE_ATOMIC(req->rq_reqbuf, req->rq_reqbuf_len);
 		req->rq_reqbuf = NULL;
 		req->rq_reqbuf_len = 0;
 	}

lustre_compat/mm/Makefile
@@ -4,4 +4,4 @@
 # This file is part of Lustre, http://www.lustre.org/
 #
 
-mm_objs := shrinker_debug.o
+mm_objs := shrinker_debug.o vmalloc.o

lustre_compat/mm/vmalloc.c
@@ -1,0 +1,65 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#include <lustre_compat/linux/vmalloc.h>
+#include <lustre_compat/linux/workqueue.h>
+
+/*
+ * This is opencoding of vfree_atomic from Linux kernel added in 4.10 with
+ * minimum changes needed to work on older kernels too.
+ *
+ * We do this because using kallaym_lookup_name() can fail with exporting
+ * vfree_atomic() which shows Lustre's days out of tree are numbered. In
+ * reality the only reason we do this is because of the poor handling of
+ * locking in the ptlrpc layer. Hoepfully a rework of ptlrpc layer will
+ * remove the need of this code.
+ */
+#ifndef llist_for_each_safe
+#define llist_for_each_safe(pos, n, node)                       \
+	for ((pos) = (node); (pos) && ((n) = (pos)->next, true); (pos) = (n))
+#endif
+
+struct vfree_deferred {
+	struct llist_head list;
+	struct work_struct wq;
+};
+static DEFINE_PER_CPU(struct vfree_deferred, vfree_deferred);
+
+static void free_work(struct work_struct *w)
+{
+	struct vfree_deferred *p = container_of(w, struct vfree_deferred, wq);
+	struct llist_node *t, *llnode;
+
+	llist_for_each_safe(llnode, t, llist_del_all(&p->list))
+		vfree((void *)llnode);
+}
+
+void init_compat_vfree_atomic(void)
+{
+	int i;
+
+	for_each_possible_cpu(i) {
+		struct vfree_deferred *p;
+
+		p = &per_cpu(vfree_deferred, i);
+		init_llist_head(&p->list);
+		INIT_WORK(&p->wq, free_work);
+	}
+}
+
+void exit_compat_vfree_atomic(void)
+{
+	/* exit_libcfs_vfree_atomic */
+	__flush_workqueue(system_wq);
+}
+
+void compat_vfree_atomic(const void *addr)
+{
+	struct vfree_deferred *p = raw_cpu_ptr(&vfree_deferred);
+
+	if (!addr)
+		 return;
+
+	if (llist_add((struct llist_node *)addr, &p->list))
+		schedule_work(&p->wq);
+}
+EXPORT_SYMBOL(compat_vfree_atomic);

lustre_compat/symbols.c
@@ -69,14 +69,6 @@ int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
 }
 EXPORT_SYMBOL_GPL(compat_apply_workqueue_attrs);
 
-static void (*__vfree_atomic)(const void *addr);
-
-void compat_vfree_atomic(const void *addr)
-{
-	__vfree_atomic(addr);
-}
-EXPORT_SYMBOL_GPL(compat_vfree_atomic);
-
 int lustre_symbols_init(void)
 {
 	int rc;
@@ -94,10 +86,6 @@ int lustre_symbols_init(void)
 
 	__apply_workqueue_attrs = cfs_kallsyms_lookup_name("apply_workqueue_attrs");
 	if (!__apply_workqueue_attrs)
-		return -EINVAL;
-
-	__vfree_atomic = cfs_kallsyms_lookup_name("vfree_atomic");
-	if (!__vfree_atomic)
 		return -EINVAL;
 
 	return 0;