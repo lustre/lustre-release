lustre/lustre-release

commit 3e04d1197db35b53dd4c227013f34cb240c02fca

LU-19245 dne: update lmm_oi in dir migrate
A dir migrate doesn't update lov lmm->lmm_oi during
migrate so LFSCK starts to complain about "bad lmm_oi".
Also adding a sane error code to lfsck_layout_master_exec_oit()
so it reports -EINVAL in dryrun mode instead of buffer size
from the previous function call.

Fix lfsck_layout_master_exec_oit() to be aware of
LOV_MAGIC_FOREIGN layout while checking and updating lmm_oi.

HPE-bug-id: LUS-12978
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I458afaeb2b2a92e083631622b7a7b554b62fa2ea
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60578
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -3888,11 +3888,13 @@ static inline void mdd_xattrs_fini(struct mdd_xattrs *xattrs)
 static int mdd_xattrs_migrate_prep(const struct lu_env *env,
 				   struct mdd_xattrs *xattrs,
 				   struct mdd_object *sobj,
+				   struct mdd_object *tobj,
 				   bool skip_linkea,
 				   bool skip_dmv)
 {
 	struct lu_attr *attr = MDD_ENV_VAR(env, cattr);
 	struct mdd_xattr_entry *entry;
+	struct ost_id oi = {0, };
 	bool needencxattr = false;
 	bool encxattrfound = false;
 	char *xname;
@@ -3903,6 +3905,13 @@ static int mdd_xattrs_migrate_prep(const struct lu_env *env,
 	int rc;
 
 	ENTRY;
+
+	if (S_ISREG(attr->la_mode)) {
+		LASSERT(tobj != NULL);
+
+		fid_to_lmm_oi(mdd_object_fid(tobj), &oi);
+		lmm_oi_cpu_to_le(&oi, &oi);
+	}
 
 	list_xsize = mdo_xattr_list(env, sobj, &LU_BUF_NULL);
 	if (list_xsize == -ENODATA)
@@ -3970,6 +3979,13 @@ reloop:
 			if (rc == -ENODATA)
 				continue;
 			GOTO(fini, rc);
+		}
+
+		if (S_ISREG(attr->la_mode) &&
+		    strcmp(XATTR_NAME_LOV, xname) == 0) {
+			struct lov_mds_md *lmm = entry->mxe_buf.lb_buf;
+
+			mdd_set_lmm_oi(lmm, &oi);
 		}
 
 		entry->mxe_name = xname;
@@ -4871,7 +4887,8 @@ retry:
 	 * RPCs inside transaction.
 	 */
 	if (!spec->sp_migrate_nsonly) {
-		rc = mdd_xattrs_migrate_prep(env, &xattrs, sobj, true, true);
+		rc = mdd_xattrs_migrate_prep(env, &xattrs, sobj, tobj,
+					     true, true);
 		if (rc)
 			GOTO(out, rc);
 	}
@@ -5195,8 +5212,8 @@ int mdd_dir_layout_shrink(const struct lu_env *env,
 		}
 
 		if (!lmv_is_fixed(lmv))
-			rc = mdd_xattrs_migrate_prep(env, &xattrs, obj, false,
-						     false);
+			rc = mdd_xattrs_migrate_prep(env, &xattrs, obj, NULL,
+						     false, false);
 	}
 
 	handle = mdd_trans_create(env, mdd);
@@ -5465,7 +5482,8 @@ int mdd_dir_layout_split(const struct lu_env *env, struct md_object *o,
 
 	mdd_xattrs_init(&xattrs);
 	if (is_plain)
-		rc = mdd_xattrs_migrate_prep(env, &xattrs, obj, true, true);
+		rc = mdd_xattrs_migrate_prep(env, &xattrs, obj, tobj,
+					     true, true);
 
 	handle = mdd_trans_create(env, mdd);
 	if (IS_ERR(handle))

lustre/lfsck/lfsck_layout.c
@@ -5848,12 +5848,15 @@ again:
 		for (i = 0; i < count; i++) {
 			lcme = &lcm->lcm_entries[i];
 			v1 = buf->lb_buf + le32_to_cpu(lcme->lcme_offset);
+			if (le32_to_cpu(v1->lmm_magic) == LOV_MAGIC_FOREIGN)
+				continue;
 			if (memcmp(oi, &v1->lmm_oi, sizeof(*oi)) != 0)
 				goto fix;
 		}
 
 		GOTO(out, stripe = true);
-	} else if (memcmp(oi, &lmm->lmm_oi, sizeof(*oi)) == 0) {
+	} else if ((magic != LOV_MAGIC_FOREIGN) &&
+		   memcmp(oi, &lmm->lmm_oi, sizeof(*oi)) == 0) {
 		GOTO(out, stripe = true);
 	}
 
@@ -5904,9 +5907,11 @@ fix:
 		for (i = 0; i < count; i++) {
 			lcme = &lcm->lcm_entries[i];
 			v1 = buf->lb_buf + le32_to_cpu(lcme->lcme_offset);
+			if (le32_to_cpu(v1->lmm_magic) == LOV_MAGIC_FOREIGN)
+				continue;
 			v1->lmm_oi = *oi;
 		}
-	} else {
+	} else if (magic != LOV_MAGIC_FOREIGN) {
 		lmm->lmm_oi = *oi;
 	}
 

lustre/mdd/mdd_internal.h
@@ -379,6 +379,17 @@ int mdd_changelog_data_store_xattr(const struct lu_env *env,
 				   struct thandle *handle);
 int mdd_dom_fixup(const struct lu_env *env, struct mdd_device *mdd,
 		  struct mdd_object *mo, struct mdd_object *vo);
+int mdd_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi, bool set);
+
+static inline int mdd_get_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi)
+{
+	return mdd_lmm_oi(lmm, oi, false);
+}
+
+static inline int mdd_set_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi)
+{
+	return mdd_lmm_oi(lmm, oi, true);
+}
 
 /* mdd_trans.c */
 void mdd_object_make_hint(const struct lu_env *env, struct mdd_object *parent,

lustre/mdd/mdd_object.c
@@ -2393,7 +2393,7 @@ static int mdd_layout_swap_allowed(const struct lu_env *env,
 /* XXX To set the proper lmm_oi & lmm_layout_gen when swap layouts, we have to
  *     look into the layout in MDD layer.
  */
-static int mdd_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi, bool get)
+int mdd_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi, bool set)
 {
 	struct lov_comp_md_v1	*comp_v1;
 	struct lov_mds_md	*v1;
@@ -2407,7 +2407,17 @@ static int mdd_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi, bool get)
 		if (ent_count == 0)
 			return -EINVAL;
 
-		if (get) {
+		if (set) {
+			for (i = 0; i < le32_to_cpu(ent_count); i++) {
+				off = le32_to_cpu(comp_v1->lcm_entries[i].
+						lcme_offset);
+				v1 = (struct lov_mds_md *)((char *)comp_v1 +
+						off);
+				if (le32_to_cpu(v1->lmm_magic) !=
+							LOV_MAGIC_FOREIGN)
+					v1->lmm_oi = *oi;
+			}
+		} else {
 			int i = 0;
 
 			off = le32_to_cpu(comp_v1->lcm_entries[i].lcme_offset);
@@ -2429,37 +2439,17 @@ static int mdd_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi, bool get)
 
 				*oi = v1->lmm_oi;
 			}
-		} else {
-			for (i = 0; i < le32_to_cpu(ent_count); i++) {
-				off = le32_to_cpu(comp_v1->lcm_entries[i].
-						lcme_offset);
-				v1 = (struct lov_mds_md *)((char *)comp_v1 +
-						off);
-				if (le32_to_cpu(v1->lmm_magic) !=
-							LOV_MAGIC_FOREIGN)
-					v1->lmm_oi = *oi;
-			}
 		}
 	} else if (le32_to_cpu(lmm->lmm_magic) == LOV_MAGIC_V1 ||
 		   le32_to_cpu(lmm->lmm_magic) == LOV_MAGIC_V3) {
-		if (get)
-			*oi = lmm->lmm_oi;
-		else
+		if (set)
 			lmm->lmm_oi = *oi;
+		else
+			*oi = lmm->lmm_oi;
 	} else {
 		return -EINVAL;
 	}
 	return 0;
-}
-
-static inline int mdd_get_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi)
-{
-	return mdd_lmm_oi(lmm, oi, true);
-}
-
-static inline int mdd_set_lmm_oi(struct lov_mds_md *lmm, struct ost_id *oi)
-{
-	return mdd_lmm_oi(lmm, oi, false);
 }
 
 static int mdd_lmm_gen(struct lov_mds_md *lmm, __u32 *gen, bool get)

lustre/tests/sanity.sh
@@ -25329,6 +25329,33 @@ test_230z() {
 }
 run_test 230z "resume dir migration with bad hash type"
 
+test_230A_check_lmm_oi() {
+	local FID LMM_FID
+
+	FID=$($LFS path2fid "$1")
+	while read LMM_FID; do
+	[[ "$FID" == "$LMM_FID" ]] || {
+		echo "FID=$FID LMM_FID=$LMM_FID"
+		return 1
+	}
+	done < <( $LFS getstripe -v "$1" | awk '/lmm_fid:/ {print $2 }' )
+	return 0
+}
+
+test_230A()
+{
+        (( MDSCOUNT > 1 )) || skip "needs >= 2 MDTs"
+        (( MDS1_VERSION >= $(version_code 2.16.57) )) ||
+                skip "need MDS >= 2.16.57 for lmm_oi migrate update"
+
+	$LFS mkdir -i 0 -c 1 $DIR/$tdir || error "mkdir $tdir failed"
+	$LFS setstripe -E 10M -c 1 -E -1 -c 1 $DIR/$tdir/file || error "file creation failed"
+	test_230A_check_lmm_oi $DIR/$tdir/file || error "Wrong lmm oi before migrate"
+	$LFS migrate -m 1 $DIR/$tdir || error "migrate failed"
+	test_230A_check_lmm_oi $DIR/$tdir/file || error "Wrong lmm oi after migrate"
+}
+run_test 230A "dir migrate should update lmm_oi"
+
 test_231a()
 {
 	# For simplicity this test assumes that max_pages_per_rpc

commit b7f5dcef2a0a5173f211bb0f7e1519eac68b75f4

LU-19308 mdd: mdd_xattr_migrate_prep check
Check the source object type to determine
whether lmm_oi needs to be updated.

Fixes: 3e04d1197d ("LU-19245 dne: update lmm_oi in dir migrate")
HPE-bug-id: LUS-13117
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: I86e3d49e95ceaa05e788169965a7fe26b88e31d5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62347
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>

lustre/mdd/mdd_dir.c
@@ -3906,7 +3906,7 @@ static int mdd_xattrs_migrate_prep(const struct lu_env *env,
 
 	ENTRY;
 
-	if (S_ISREG(attr->la_mode)) {
+	if (S_ISREG(mdd_object_type(sobj))) {
 		LASSERT(tobj != NULL);
 
 		fid_to_lmm_oi(mdd_object_fid(tobj), &oi);