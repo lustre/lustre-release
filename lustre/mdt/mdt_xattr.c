lustre/lustre-release

commit 719f6763fe19d208833040a36d47ad360da465f2

LU-18756 sec: add resource id check to oss and mds
This patch includes the resource id check into the relevant code paths
on the oss and mds side. It is therefore included for the following
operations.

On the MDT-side:
- open
- create (file and directory)
- unlink (file and directory)
- setattr
- setxattr
- getxattr
- rename
- link

On the OST-side and on the MDT-side for Data on MDT (DoM) files:
- write
- read
- truncate
- fallocate

Some caveats:
The resource id check is not included for MDS_GETATTR RPCs due to
functional and usability concerns. Specifically for the latter, the
"struct stat" would no longer be filled resulting in "?" when running
"ls -l", which can be misunderstood.

Also, if the check is only enabled on the OST-side, writes are only
denied for "sync"/"fsync"-type operations on a file as the check is at
the server-side. If the check is enabled on the MDT-side, write-access
is denied before the OST_WRITE RPC is sent, i.e., immediately
returning the access denied error code. If a file is still in the page
cache before the check is enabled, a client can still read the local
copy of the file, which is expected.

Sanity-sec test 75a was added to exercise the ID check for the above
cases in several disciplines further testing that access to
neighboring nodemap offset ranges work as expected.

Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I040ddb1b934707baa84b492337139f45b856692e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59208
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/mdt/mdt_xattr.c
@@ -246,6 +246,10 @@ int mdt_getxattr(struct mdt_thread_info *info)
 	if (rc)
 		RETURN(err_serious(rc));
 
+	rc = mdt_check_resource_ids(info, info->mti_object);
+	if (unlikely(rc))
+		RETURN(err_serious(rc));
+
 	next = mdt_object_child(info->mti_object);
 	easize = mdt_getxattr_pack_reply(info);
 	if (easize == -ENODATA)
@@ -616,6 +620,10 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
 	obj = mdt_object_find_lock(info, rr->rr_fid1, lh, lockpart, LCK_EX);
 	if (IS_ERR(obj))
 		GOTO(out, rc = PTR_ERR(obj));
+
+	rc = mdt_check_resource_ids(info, obj);
+	if (unlikely(rc))
+		GOTO(out_unlock, rc);
 
 	tgt_vbr_obj_set(env, mdt_obj2dt(obj));
 	rc = mdt_version_get_check_save(info, obj, 0);

lustre/mdt/mdt_io.c
@@ -322,6 +322,7 @@ static int mdt_preprw_read(const struct lu_env *env, struct obd_export *exp,
 			   struct niobuf_remote *rnb, int *nr_local,
 			   struct niobuf_local *lnb)
 {
+	struct mdt_thread_info *info = mdt_th_info(env);
 	struct dt_object *dob;
 	int i, j, rc;
 	int maxlnb = *nr_local;
@@ -348,6 +349,11 @@ static int mdt_preprw_read(const struct lu_env *env, struct obd_export *exp,
 		 */
 		RETURN(0);
 	}
+
+	rc = mdt_check_resource_ids(info, mo);
+	if (unlikely(rc))
+		GOTO(out_sem, rc);
+
 	if (lu_object_is_dying(&mo->mot_header)) {
 		CDEBUG_LIMIT(level,
 			     "%s: READ IO to stale obj "DFID": rc = %d\n",
@@ -382,6 +388,7 @@ static int mdt_preprw_read(const struct lu_env *env, struct obd_export *exp,
 	RETURN(0);
 buf_put:
 	dt_bufs_put(env, dob, lnb, *nr_local);
+out_sem:
 	up_read(&mo->mot_dom_sem);
 	return rc;
 }
@@ -393,6 +400,7 @@ static int mdt_preprw_write(const struct lu_env *env, struct obd_export *exp,
 			    struct niobuf_remote *rnb, int *nr_local,
 			    struct niobuf_local *lnb)
 {
+	struct mdt_thread_info *info = mdt_th_info(env);
 	struct dt_object *dob;
 	int i, j, k, rc = 0;
 	int maxlnb = *nr_local;
@@ -417,6 +425,11 @@ static int mdt_preprw_write(const struct lu_env *env, struct obd_export *exp,
 		/* exit with no data written, note nr_local = 0 above */
 		GOTO(unlock, rc);
 	}
+
+	rc = mdt_check_resource_ids(info, mo);
+	if (unlikely(rc))
+		GOTO(unlock, rc);
+
 	if (lu_object_is_dying(&mo->mot_header)) {
 		/* This is possible race between object destroy followed by
 		 * discard BL AST and client cache flushing. Object is
@@ -1101,6 +1114,10 @@ int mdt_fallocate_hdl(struct tgt_session_info *tsi)
 		GOTO(out_put, rc);
 	}
 
+	rc = mdt_check_resource_ids(info, mo);
+	if (unlikely(rc))
+		GOTO(out_put, rc);
+
 	la_from_obdo(la, oa, OBD_MD_FLMTIME | OBD_MD_FLATIME | OBD_MD_FLCTIME);
 
 	down_write(&mo->mot_dom_sem);
@@ -1373,6 +1390,10 @@ int mdt_punch_hdl(struct tgt_session_info *tsi)
 		       exp->exp_obd->obd_name, PFID(&tsi->tsi_fid), rc);
 		GOTO(out_put, rc);
 	}
+
+	rc = mdt_check_resource_ids(info, mo);
+	if (unlikely(rc))
+		GOTO(out_put, rc);
 
 	down_write(&mo->mot_dom_sem);
 	dob = mdt_obj2dt(mo);

lustre/mdt/mdt_open.c
@@ -744,6 +744,10 @@ static int mdt_open_by_fid(struct mdt_thread_info *info, struct ldlm_reply *rep,
 	if (IS_ERR(o))
 		RETURN(rc = PTR_ERR(o));
 
+	rc = mdt_check_resource_ids(info, o);
+	if (unlikely(rc))
+		GOTO(out, rc);
+
 	rc = mdt_check_enc(info, o);
 	if (rc)
 		GOTO(out, rc);
@@ -1158,8 +1162,12 @@ static int mdt_open_by_fid_lock(struct mdt_thread_info *info,
 		GOTO(out, rc = -ENOENT);
 	}
 
-	/* do not check enc for directory: always allow open */
+	/* do not check enc or id for directory: always allow open */
 	if (!S_ISDIR(lu_object_attr(&o->mot_obj))) {
+		rc = mdt_check_resource_ids(info, o);
+		if (unlikely(rc))
+			GOTO(out, rc);
+
 		rc = mdt_check_enc(info, o);
 		if (rc)
 			GOTO(out, rc);
@@ -1249,9 +1257,14 @@ static int mdt_cross_open(struct mdt_thread_info *info,
 	int rc;
 
 	ENTRY;
+
 	o = mdt_object_find(info->mti_env, info->mti_mdt, fid);
 	if (IS_ERR(o))
 		RETURN(rc = PTR_ERR(o));
+
+	rc = mdt_check_resource_ids(info, o);
+	if (unlikely(rc))
+		GOTO(out, rc);
 
 	rc = mdt_check_enc(info, o);
 	if (rc)
@@ -1529,6 +1542,10 @@ int mdt_reint_open(struct mdt_thread_info *info, struct mdt_lock_handle *lhc)
 	    parent->mot_obj.lo_header->loh_attr & LOHA_FSCRYPT_MD &&
 	    open_flags & MDS_OPEN_CREAT)
 		GOTO(out_parent, result = -EPERM);
+
+	result = mdt_check_resource_ids(info, parent);
+	if (unlikely(result))
+		GOTO(out_parent, result);
 
 	result = mdt_check_enc(info, parent);
 	if (result)

lustre/mdt/mdt_reint.c
@@ -558,6 +558,10 @@ static int mdt_create(struct mdt_thread_info *info, struct mdt_lock_handle *lhc)
 	if (!mdt_object_exists(parent))
 		GOTO(put_parent, rc = -ENOENT);
 
+	rc = mdt_check_resource_ids(info, parent);
+	if (unlikely(rc))
+		GOTO(put_parent, rc);
+
 	rc = mdt_check_enc(info, parent);
 	if (rc)
 		GOTO(put_parent, rc);
@@ -926,6 +930,10 @@ static int mdt_reint_setattr(struct mdt_thread_info *info,
 	if (!mdt_object_exists(mo))
 		GOTO(out_put, rc = -ENOENT);
 
+	rc = mdt_check_resource_ids(info, mo);
+	if (unlikely(rc))
+		GOTO(out_put, rc);
+
 	if (mdt_object_remote(mo))
 		GOTO(out_put, rc = -EREMOTE);
 
@@ -1288,6 +1296,10 @@ static int mdt_reint_unlink(struct mdt_thread_info *info,
 			GOTO(put_child, rc = -ENOENT);
 	}
 
+	rc = mdt_check_resource_ids(info, mc);
+	if (unlikely(rc))
+		GOTO(put_child, rc);
+
 	child_lh = &info->mti_lh[MDT_LH_CHILD];
 	if (mdt_object_remote(mc)) {
 		struct mdt_body	 *repbody;
@@ -1464,6 +1476,10 @@ static int mdt_reint_link(struct mdt_thread_info *info,
 		GOTO(put_source, rc = -ENOENT);
 	}
 
+	rc = mdt_check_resource_ids(info, ms);
+	if (unlikely(rc))
+		GOTO(put_source, rc);
+
 	CFS_RACE(OBD_FAIL_MDS_LINK_RENAME_RACE);
 
 	lhp = &info->mti_lh[MDT_LH_PARENT];
@@ -1473,6 +1489,10 @@ static int mdt_reint_link(struct mdt_thread_info *info,
 
 	rc = mdt_version_get_check_save(info, mp, 0);
 	if (rc)
+		GOTO(unlock_parent, rc);
+
+	rc = mdt_check_resource_ids(info, mp);
+	if (unlikely(rc))
 		GOTO(unlock_parent, rc);
 
 	rc = mdt_check_enc(info, mp);
@@ -2288,6 +2308,10 @@ int mdt_reint_migrate(struct mdt_thread_info *info,
 	if (!S_ISDIR(lu_object_attr(&pobj->mot_obj)))
 		GOTO(put_parent, rc = -ENOTDIR);
 
+	rc = mdt_check_resource_ids(info, pobj);
+	if (unlikely(rc))
+		GOTO(put_parent, rc);
+
 	rc = mdt_check_enc(info, pobj);
 	if (rc)
 		GOTO(put_parent, rc);
@@ -2739,6 +2763,10 @@ lock_bfl:
 	if (IS_ERR(msrcdir))
 		RETURN(PTR_ERR(msrcdir));
 
+	rc = mdt_check_resource_ids(info, msrcdir);
+	if (unlikely(rc))
+		GOTO(out_put_srcdir, rc);
+
 	rc = mdt_check_enc(info, msrcdir);
 	if (rc)
 		GOTO(out_put_srcdir, rc);
@@ -2754,6 +2782,11 @@ lock_bfl:
 		if (IS_ERR(mtgtdir))
 			GOTO(out_put_srcdir, rc = PTR_ERR(mtgtdir));
 	}
+
+	/* if this succeeds we do not need to check "mnew" later again */
+	rc = mdt_check_resource_ids(info, mtgtdir);
+	if (unlikely(rc))
+		GOTO(out_put_tgtdir, rc);
 
 	rc = mdt_check_enc(info, mtgtdir);
 	if (rc)
@@ -2877,6 +2910,10 @@ lock_bfl:
 				"object does not exist");
 		GOTO(out_put_old, rc = -ENOENT);
 	}
+
+	rc = mdt_check_resource_ids(info, mold);
+	if (unlikely(rc))
+		GOTO(out_put_old, rc);
 
 	if (mdt_object_remote(mold) && !mdt->mdt_enable_remote_rename)
 		GOTO(out_put_old, rc = -EXDEV);

lustre/ofd/ofd_io.c
@@ -605,6 +605,10 @@ static int ofd_preprw_read(const struct lu_env *env, struct obd_export *exp,
 	if (!ofd_object_exists(fo))
 		GOTO(obj_put, rc = -ENOENT);
 
+	rc = ofd_check_resource_ids(env, exp);
+	if (unlikely(rc))
+		GOTO(obj_put, rc);
+
 	if (ptlrpc_connection_is_local(exp->exp_connection))
 		dbt |= DT_BUFS_TYPE_LOCAL;
 
@@ -1250,6 +1254,10 @@ ofd_commitrw_write(const struct lu_env *env, struct obd_export *exp,
 		GOTO(out, rc = old_rc);
 	if (!ofd_object_exists(fo))
 		GOTO(out, rc = -ENOENT);
+
+	rc = ofd_check_resource_ids(env, exp);
+	if (unlikely(rc))
+		GOTO(out, rc);
 
 	/*
 	 * The first write to each object must set some attributes.  It is

lustre/ofd/ofd_objects.c
@@ -684,6 +684,11 @@ int ofd_attr_set(const struct lu_env *env, struct ofd_object *fo,
 	if (!ofd_object_exists(fo))
 		GOTO(out, rc = -ENOENT);
 
+	ofd_info(env)->fti_obj = fo;
+
+	rc = ofd_check_resource_ids(env, ofd_info(env)->fti_exp);
+	if (unlikely(rc))
+		GOTO(out, rc);
 
 	if (la->la_valid & LA_PROJID &&
 	    CFS_FAIL_CHECK(OBD_FAIL_OUT_DROP_PROJID_SET))
@@ -798,6 +803,12 @@ int ofd_object_fallocate(const struct lu_env *env, struct ofd_object *fo,
 
 	if (!ofd_object_exists(fo))
 		RETURN(-ENOENT);
+
+	ofd_info(env)->fti_obj = fo;
+
+	rc = ofd_check_resource_ids(env, ofd_info(env)->fti_exp);
+	if (unlikely(rc))
+		RETURN(rc);
 
 	/* VBR: version recovery check */
 	rc = ofd_version_get_check(info, fo);
@@ -931,6 +942,12 @@ int ofd_object_punch(const struct lu_env *env, struct ofd_object *fo,
 		if (rc != 0)
 			GOTO(out, rc);
 	}
+
+	ofd_info(env)->fti_obj = fo;
+
+	rc = ofd_check_resource_ids(env, ofd_info(env)->fti_exp);
+	if (unlikely(rc))
+		GOTO(out, rc);
 
 	/* VBR: version recovery check */
 	rc = ofd_version_get_check(info, fo);

commit 21bd3cd84759e35c77a72bc939ddf2ded4878efa

LU-18756 mdt: check resource IDs before init_ucred in getxattr
When getting xattr on the MDT, "mdt_check_resource_ids()" should be
called before "mdt_init_ucred()" to avoid acquiring identity cache
entries for requests that will be rejected, and it ensures the
identity cache entries are correctly cleaned-up.

Fixes: 719f6763fe ("LU-18756 sec: add resource id check to oss and mds")
Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I1c3168cc41caf4c00852223d7c4263d54850a3c9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62113
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@dilger.ca>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/mdt/mdt_xattr.c
@@ -242,12 +242,12 @@ int mdt_getxattr(struct mdt_thread_info *info)
 	if (reqbody == NULL)
 		RETURN(err_serious(-EFAULT));
 
-	rc = mdt_init_ucred(info, reqbody);
-	if (rc)
-		RETURN(err_serious(rc));
-
 	rc = mdt_check_resource_ids(info, info->mti_object);
 	if (unlikely(rc))
+		RETURN(err_serious(rc));
+
+	rc = mdt_init_ucred(info, reqbody);
+	if (rc)
 		RETURN(err_serious(rc));
 
 	next = mdt_object_child(info->mti_object);