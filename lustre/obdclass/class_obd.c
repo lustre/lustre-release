lustre/lustre-release

commit 5eae8514f5f1730fe93d55d348400f5ecf681078

LU-16634 misc: standardize iocontrol param handling
Validate uarg and karg early in iocontrol processing where needed.
This needs kernel interop for 4.20+ kernels for access_ok(), but
this can be checked by #ifdef and does not need an autoconf test.

Fix incorrect definition of OBD_IOC_BARRIER to match reality.

Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Reported-by: Tao Lyu <tao.lyu@epfl.ch>
Change-Id: I1a0d2f839949debf346aa15c65b0f407e9ce7057
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50314
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Vitaliy Kuznetsov <vkuznetsov@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/class_obd.c
@@ -296,16 +296,11 @@ int obd_ioctl_getdata(struct obd_ioctl_data **datap, int *len, void __user *arg)
 	}
 	*len = hdr.ioc_len;
 
-	if (copy_from_user(data, arg, hdr.ioc_len)) {
-		OBD_FREE_LARGE(data, hdr.ioc_len);
-		RETURN(-EFAULT);
-	}
+	if (copy_from_user(data, arg, hdr.ioc_len))
+		GOTO(out_free, rc = -EFAULT);
 
-	if (obd_ioctl_is_invalid(data)) {
-		CERROR("ioctl not correctly formatted\n");
-		OBD_FREE_LARGE(data, hdr.ioc_len);
-		RETURN(-EINVAL);
-	}
+	if (obd_ioctl_is_invalid(data))
+		GOTO(out_free, rc = -EINVAL);
 
 	if (data->ioc_inllen1) {
 		data->ioc_inlbuf1 = &data->ioc_bulk[0];
@@ -328,6 +323,10 @@ int obd_ioctl_getdata(struct obd_ioctl_data **datap, int *len, void __user *arg)
 	*datap = data;
 
 	RETURN(0);
+
+out_free:
+	OBD_FREE_LARGE(data, *len);
+	RETURN(rc);
 }
 EXPORT_SYMBOL(obd_ioctl_getdata);
 
@@ -339,6 +338,10 @@ int class_handle_ioctl(unsigned int cmd, void __user *uarg)
 
 	ENTRY;
 	CDEBUG(D_IOCTL, "obdclass: cmd=%x len=%u uarg=%pK\n", cmd, len, uarg);
+	if (unlikely(_IOC_TYPE(cmd) != 'f' && cmd != IOC_OSC_SET_ACTIVE &&
+		     cmd != OBD_IOC_BARRIER))
+		RETURN(OBD_IOC_ERROR(obd->obd_name, cmd, "unknown", -ENOTTY));
+
 	rc = obd_ioctl_getdata(&data, &len, uarg);
 	if (rc) {
 		CERROR("%s: ioctl data error: rc = %d\n", current->comm, rc);

libcfs/libcfs/crypto/policy.c
@@ -438,13 +438,13 @@ int llcrypt_ioctl_get_policy_ex(struct file *filp, void __user *uarg)
 		     offsetof(typeof(arg), policy));
 	BUILD_BUG_ON(sizeof(arg.policy) != sizeof(*policy));
 
+	if (copy_from_user(&arg, uarg, sizeof(arg.policy_size)))
+		return -EFAULT;
+
 	err = llcrypt_get_policy(file_inode(filp), policy);
 	if (err)
 		return err;
 	policy_size = llcrypt_policy_size(policy);
-
-	if (copy_from_user(&arg, uarg, sizeof(arg.policy_size)))
-		return -EFAULT;
 
 	if (policy_size > arg.policy_size)
 		return -EOVERFLOW;

lustre/include/lustre_compat.h
@@ -586,6 +586,12 @@ static inline bool is_root_inode(struct inode *inode)
 # endif
 #endif
 
+#ifdef VERIFY_WRITE /* removed in kernel commit v4.20-10979-g96d4f267e40f */
+#define ll_access_ok(ptr, len) access_ok(VERIFY_WRITE, ptr, len)
+#else
+#define ll_access_ok(ptr, len) access_ok(ptr, len)
+#endif
+
 static inline void ll_security_release_secctx(char *secdata, u32 seclen)
 {
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG

lustre/include/uapi/linux/lustre/lustre_ioctl.h
@@ -224,7 +224,8 @@ enum obd_abort_recovery_flags {
 /*	lustre/lustre_user.h	240-249 */
 /* was	LIBCFS_IOC_DEBUG_MASK	_IOWR('f', 250, long) until 2.11 */
 
-#define OBD_IOC_BARRIER		_IOWR('f', 261, OBD_IOC_DATA_TYPE)
+/* OBD_IOC_BARRIER wrongly defined as _IOWR('f', 261, OBD_IOC_DATA_TYPE) */
+#define OBD_IOC_BARRIER		_IOWR('g', 5, OBD_IOC_DATA_TYPE)
 
 #define IOC_OSC_SET_ACTIVE	_IOWR('h', 21, void *)
 

lustre/ldlm/ldlm_lib.c
@@ -2835,7 +2835,7 @@ static int target_recovery_thread(void *arg)
 	/* cancel update llogs upon recovery abort */
 	if (obd->obd_abort_recovery || obd->obd_abort_mdt_recovery)
 		OBP(obd, iocontrol)(OBD_IOC_LLOG_CANCEL, obd->obd_self_export,
-				    0, NULL, NULL);
+				    0, trd, NULL);
 
 	list_for_each_entry(req, &obd->obd_final_req_queue, rq_list) {
 		/*

lustre/llite/file.c
@@ -2639,10 +2639,14 @@ out_lump:
 
 static int ll_file_getstripe(struct inode *inode, void __user *lum, size_t size)
 {
-	struct lu_env	*env;
-	__u16		refcheck;
-	int		rc;
+	struct lu_env *env;
+	__u16 refcheck;
+	int rc;
 	ENTRY;
+
+	/* exit before doing any work if pointer is bad */
+	if (unlikely(!ll_access_ok(lum, sizeof(struct lov_user_md))))
+		RETURN(-EFAULT);
 
 	env = cl_env_get(&refcheck);
 	if (IS_ERR(env))
@@ -3948,7 +3952,7 @@ static long ll_file_unlock_lease(struct file *file, struct ll_ioc_lease *ioc,
 	bool lease_broken = false;
 	fmode_t fmode = 0;
 	enum mds_op_bias bias = 0;
-	int fdv;
+	__u32 fdv;
 	struct file *layout_file = NULL;
 	void *data = NULL;
 	size_t data_size = 0;
@@ -3984,12 +3988,12 @@ static long ll_file_unlock_lease(struct file *file, struct ll_ioc_lease *ioc,
 
 		bias = MDS_CLOSE_RESYNC_DONE;
 		break;
-	case LL_LEASE_LAYOUT_MERGE: {
+	case LL_LEASE_LAYOUT_MERGE:
 		if (ioc->lil_count != 1)
 			GOTO(out_lease_close, rc = -EINVAL);
 
 		uarg += sizeof(*ioc);
-		if (copy_from_user(&fdv, uarg, sizeof(__u32)))
+		if (copy_from_user(&fdv, uarg, sizeof(fdv)))
 			GOTO(out_lease_close, rc = -EFAULT);
 
 		layout_file = fget(fdv);
@@ -4003,20 +4007,21 @@ static long ll_file_unlock_lease(struct file *file, struct ll_ioc_lease *ioc,
 		data = file_inode(layout_file);
 		bias = MDS_CLOSE_LAYOUT_MERGE;
 		break;
-	}
 	case LL_LEASE_LAYOUT_SPLIT: {
-		int mirror_id;
+		__u32 mirror_id;
 
 		if (ioc->lil_count != 2)
 			GOTO(out_lease_close, rc = -EINVAL);
 
 		uarg += sizeof(*ioc);
-		if (copy_from_user(&fdv, uarg, sizeof(__u32)))
+		if (copy_from_user(&fdv, uarg, sizeof(fdv)))
 			GOTO(out_lease_close, rc = -EFAULT);
 
-		uarg += sizeof(__u32);
-		if (copy_from_user(&mirror_id, uarg, sizeof(__u32)))
+		uarg += sizeof(fdv);
+		if (copy_from_user(&mirror_id, uarg, sizeof(mirror_id)))
 			GOTO(out_lease_close, rc = -EFAULT);
+		if (mirror_id >= MIRROR_ID_NEG)
+			GOTO(out_lease_close, rc = -EINVAL);
 
 		layout_file = fget(fdv);
 		if (!layout_file)
@@ -4214,6 +4219,9 @@ ll_file_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	if (_IOC_TYPE(cmd) == 'T' || _IOC_TYPE(cmd) == 't') /* tty ioctls */
 		RETURN(-ENOTTY);
 
+	/* can't do a generic karg == NULL check here, since it is too noisy and
+	 * we need to return -ENOTTY for unsupported ioctls instead of -EINVAL.
+	 */
 	switch (cmd) {
 	case LL_IOC_GETFLAGS:
 		/* Get the current value of the file flags */
@@ -4315,6 +4323,9 @@ out:
 		struct hsm_user_state *hus;
 		int rc;
 
+		if (!ll_access_ok(uarg, sizeof(*hus)))
+			RETURN(-EFAULT);
+
 		OBD_ALLOC_PTR(hus);
 		if (hus == NULL)
 			RETURN(-ENOMEM);
@@ -4322,17 +4333,16 @@ out:
 		op_data = ll_prep_md_op_data(NULL, inode, NULL, NULL, 0, 0,
 					     LUSTRE_OPC_ANY, hus);
 		if (IS_ERR(op_data)) {
-			OBD_FREE_PTR(hus);
-			RETURN(PTR_ERR(op_data));
+			rc = PTR_ERR(op_data);
+		} else {
+			rc = obd_iocontrol(cmd, ll_i2mdexp(inode),
+					   sizeof(*op_data), op_data, NULL);
+
+			if (copy_to_user(uarg, hus, sizeof(*hus)))
+				rc = -EFAULT;
+
+			ll_finish_md_op_data(op_data);
 		}
-
-		rc = obd_iocontrol(cmd, ll_i2mdexp(inode), sizeof(*op_data),
-				   op_data, NULL);
-
-		if (copy_to_user(uarg, hus, sizeof(*hus)))
-			rc = -EFAULT;
-
-		ll_finish_md_op_data(op_data);
 		OBD_FREE_PTR(hus);
 		RETURN(rc);
 	}
@@ -4344,12 +4354,10 @@ out:
 		if (hss == NULL)
 			RETURN(-ENOMEM);
 
-		if (copy_from_user(hss, uarg, sizeof(*hss))) {
-			OBD_FREE_PTR(hss);
-			RETURN(-EFAULT);
-		}
-
-		rc = ll_hsm_state_set(inode, hss);
+		if (copy_from_user(hss, uarg, sizeof(*hss)))
+			rc = -EFAULT;
+		else
+			rc = ll_hsm_state_set(inode, hss);
 
 		OBD_FREE_PTR(hss);
 		RETURN(rc);
@@ -4359,6 +4367,9 @@ out:
 		struct hsm_current_action *hca;
 		const char *action;
 		int rc;
+
+		if (!ll_access_ok(uarg, sizeof(*hca)))
+			RETURN(-EFAULT);
 
 		OBD_ALLOC_PTR(hca);
 		if (hca == NULL)
@@ -4440,12 +4451,10 @@ skip_copy:
 		if (hui == NULL)
 			RETURN(-ENOMEM);
 
-		if (copy_from_user(hui, uarg, sizeof(*hui))) {
-			OBD_FREE_PTR(hui);
-			RETURN(-EFAULT);
-		}
-
-		rc = ll_hsm_import(inode, file, hui);
+		if (copy_from_user(hui, uarg, sizeof(*hui)))
+			rc = -EFAULT;
+		else
+			rc = ll_hsm_import(inode, file, hui);
 
 		OBD_FREE_PTR(hui);
 		RETURN(rc);

lustre/llite/llite_lib.c
@@ -36,19 +36,20 @@
 #define DEBUG_SUBSYSTEM S_LLITE
 
 #include <linux/cpu.h>
+#include <linux/delay.h>
+#include <linux/file.h>
+#include <linux/fs_struct.h>
+#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/random.h>
 #include <linux/statfs.h>
 #include <linux/time.h>
-#include <linux/file.h>
 #include <linux/types.h>
+#include <linux/uaccess.h>
+#include <linux/uidgid.h>
+#include <linux/user_namespace.h>
 #include <linux/uuid.h>
 #include <linux/version.h>
-#include <linux/mm.h>
-#include <linux/user_namespace.h>
-#include <linux/delay.h>
-#include <linux/uidgid.h>
-#include <linux/fs_struct.h>
 
 #ifndef HAVE_CPUS_READ_LOCK
 #include <libcfs/linux/linux-cpu.h>
@@ -3008,6 +3009,9 @@ int ll_iocontrol(struct inode *inode, struct file *file,
 		struct mdt_body *body;
 		struct md_op_data *op_data;
 
+		if (!ll_access_ok(uarg, sizeof(int)))
+			RETURN(-EFAULT);
+
 		op_data = ll_prep_md_op_data(NULL, inode, NULL, NULL, 0, 0,
 					     LUSTRE_OPC_ANY, NULL);
 		if (IS_ERR(op_data))
@@ -3091,6 +3095,9 @@ int ll_iocontrol(struct inode *inode, struct file *file,
 	case IOC_OBD_STATFS:
 		RETURN(ll_obd_statfs(inode, uarg));
 	case LL_IOC_GET_MDTIDX: {
+		if (!ll_access_ok(uarg, sizeof(rc)))
+			RETURN(-EFAULT);
+
 		rc = ll_get_mdt_idx(inode);
 		if (rc < 0)
 			RETURN(rc);

lustre/lmv/lmv_obd.c
@@ -853,9 +853,25 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 	ENTRY;
 	CDEBUG(D_IOCTL, "%s: cmd=%x len=%u karg=%pK uarg=%pK\n",
 	       exp->exp_obd->obd_name, cmd, len, karg, uarg);
-
 	if (count == 0)
 		RETURN(-ENOTTY);
+
+	/* exit early for unknown ioctl types */
+	if (unlikely(_IOC_TYPE(cmd) != 'f' && cmd != IOC_OSC_SET_ACTIVE))
+		RETURN(OBD_IOC_ERROR(obd->obd_name, cmd, "unknown", -ENOTTY));
+
+	/* handle commands that don't use @karg first */
+	switch (cmd) {
+	case LL_IOC_GET_CONNECT_FLAGS:
+		tgt = lmv_tgt(lmv, 0);
+		rc = -ENODATA;
+		if (tgt && tgt->ltd_exp)
+			rc = obd_iocontrol(cmd, tgt->ltd_exp, len, NULL, uarg);
+		RETURN(rc);
+	}
+
+	if (unlikely(karg == NULL))
+		RETURN(OBD_IOC_ERROR(obd->obd_name, cmd, "karg=NULL", -EINVAL));
 
 	switch (cmd) {
 	case IOC_OBD_STATFS: {
@@ -944,13 +960,6 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 			qctl->obd_uuid = tgt->ltd_uuid;
 		}
 		OBD_FREE_PTR(oqctl);
-		break;
-	}
-	case LL_IOC_GET_CONNECT_FLAGS: {
-		tgt = lmv_tgt(lmv, 0);
-		rc = -ENODATA;
-		if (tgt && tgt->ltd_exp)
-			rc = obd_iocontrol(cmd, tgt->ltd_exp, len, karg, uarg);
 		break;
 	}
 	case LL_IOC_FID2MDTIDX: {
@@ -1089,12 +1098,16 @@ hsm_req_err:
 						      tgt->ltd_uuid.uuid, err);
 					if (!rc)
 						rc = err;
+					if (unlikely(err == -ENOTTY))
+						break;
 				}
-			} else
+			} else {
 				set = 1;
+			}
 		}
 		if (!set && !rc)
 			rc = -EIO;
+		break;
 	}
 	RETURN(rc);
 }

commit 4e122133436f2e31b050cd00bc7d14e721fc6beb

LU-19558 obdclass: Use static string as obd is NULL
Use static string "obdclass" instead of de-referencing
device name as obd is stil NULL

Found using:
$ cppcheck --enable=warning --error-exitcode=0 <file path>

Test-Parameters: trivial
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Fixes: 5eae8514f5 ("LU-16634 misc: standardize iocontrol param handling")
Change-Id: Ia5960ffc22c8b0685f88190139972ae93f8074aa
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62168
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@dilger.ca>

lustre/obdclass/class_obd.c
@@ -317,7 +317,7 @@ int class_handle_ioctl(unsigned int cmd, void __user *uarg)
 	CDEBUG(D_IOCTL, "obdclass: cmd=%x len=%u uarg=%pK\n", cmd, len, uarg);
 	if (unlikely(_IOC_TYPE(cmd) != 'f' && !OBD_IOC_BARRIER_ALLOW(cmd) &&
 		     !IOC_OSC_SET_ACTIVE_ALLOW(cmd)))
-		RETURN(OBD_IOC_ERROR(obd->obd_name, cmd, "unknown", -ENOTTY));
+		RETURN(OBD_IOC_ERROR("obdclass", cmd, "unknown", -ENOTTY));
 
 	rc = obd_ioctl_getdata(&data, &len, uarg);
 	if (rc) {