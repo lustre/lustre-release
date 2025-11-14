lustre/lustre-release

commit b18bfc7c7935f40e4641b81b14eba5653313acbf

LU-18687 compat: improve cfs_kallsyms_lookup_name()
We can (ab)use kprobes to lookup kallsyms_lookup_name()
to enable us more kernel symbols. This can greatly simplify
Lustre compatibiliy code. lustre_compat/symbols.c will
serve as the central location for all of Lustre's symbol
re-exports.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I72b23c15c49f6a974187c96b09c0a701a96d5a92
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59813
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre_compat/symbols.c
@@ -1,0 +1,92 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
+ * Copyright (c) 2025, Amazon and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+/*
+ * Author: Timothy Day <timday@amazon.com>
+ */
+
+#include <linux/kprobes.h>
+#include <lustre_compat/linux/workqueue.h>
+
+#include <linux/libcfs/libcfs.h>
+
+static void *(*__cfs_kallsyms_lookup_name)(const char *name);
+
+void *cfs_kallsyms_lookup_name(const char *name)
+{
+	return __cfs_kallsyms_lookup_name(name);
+}
+EXPORT_SYMBOL_GPL(cfs_kallsyms_lookup_name);
+
+#ifdef HAVE_KALLSYMS_LOOKUP_NAME
+static int find_kallsyms_lookup_name(void)
+{
+	__cfs_kallsyms_lookup_name = (void *(*)(const char *))kallsyms_lookup_name;
+
+	return 0;
+}
+#else
+static int find_kallsyms_lookup_name(void)
+{
+	struct kprobe kp = {
+		.symbol_name = "kallsyms_lookup_name",
+	};
+	int rc;
+
+	rc = register_kprobe(&kp);
+	if (rc < 0)
+		return rc;
+
+	__cfs_kallsyms_lookup_name = (void *)kp.addr;
+	if (!__cfs_kallsyms_lookup_name)
+		return -EINVAL;
+
+	unregister_kprobe(&kp);
+
+	return 0;
+}
+#endif
+
+static struct workqueue_attrs *(*__alloc_workqueue_attrs)(void);
+
+struct workqueue_attrs *compat_alloc_workqueue_attrs(void)
+{
+	return __alloc_workqueue_attrs();
+}
+EXPORT_SYMBOL_GPL(compat_alloc_workqueue_attrs);
+
+static int (*__apply_workqueue_attrs)(struct workqueue_struct *wq,
+				      const struct workqueue_attrs *attrs);
+
+int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
+				  const struct workqueue_attrs *attrs)
+{
+	return __apply_workqueue_attrs(wq, attrs);
+}
+EXPORT_SYMBOL_GPL(compat_apply_workqueue_attrs);
+
+int lustre_symbols_init(void)
+{
+	int rc;
+
+	rc = find_kallsyms_lookup_name();
+	if (rc < 0)
+		return rc;
+
+	if (!cfs_kallsyms_lookup_name("kallsyms_lookup_name"))
+		return -EINVAL;
+
+	__alloc_workqueue_attrs = cfs_kallsyms_lookup_name("alloc_workqueue_attrs");
+	if (!__alloc_workqueue_attrs)
+		return -EINVAL;
+
+	__apply_workqueue_attrs = cfs_kallsyms_lookup_name("apply_workqueue_attrs");
+	if (!__apply_workqueue_attrs)
+		return -EINVAL;
+
+	return 0;
+}

include/lustre_compat/linux/linux-misc.h
@@ -29,6 +29,10 @@ static inline unsigned long cfs_time_seconds(time64_t seconds)
 	return nsecs_to_jiffies64(seconds * NSEC_PER_SEC);
 }
 
+/* TODO: This will soon be private... */
+void *cfs_kallsyms_lookup_name(const char *name);
+int lustre_symbols_init(void);
+
 /*
  * Since 4.20 commit 00e23707442a75b404392cef1405ab4fd498de6b
  * iov_iter: Use accessor functions to access an iterator's type and direction.
@@ -135,9 +139,6 @@ static inline void kernel_param_lock(struct module *mod)
 	__kernel_param_lock();
 }
 #endif /* ! HAVE_KERNEL_PARAM_LOCK */
-
-int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
-			      const struct workqueue_attrs *attrs);
 
 #ifndef HAVE_MATCH_WILDCARD
 bool match_wildcard(const char *pattern, const char *str);
@@ -304,18 +305,6 @@ int cfs_stack_trace_save_tsk(struct task_struct *task, unsigned long *store,
 	       sizeof(*(obj)) - offsetof(typeof(*(obj)), member));	\
 })
 #endif /* memset_startat() */
-
-#ifdef HAVE_KALLSYMS_LOOKUP_NAME
-static inline void *cfs_kallsyms_lookup_name(const char *name)
-{
-	return (void *)kallsyms_lookup_name(name);
-}
-#else
-static inline void *cfs_kallsyms_lookup_name(const char *name)
-{
-	return NULL;
-}
-#endif
 
 #ifndef HAVE_STRSCPY
 static inline ssize_t strscpy(char *s1, const char *s2, size_t sz)

include/lustre_compat/linux/workqueue.h
@@ -1,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Copyright (c) 2012, 2017, Intel Corporation.
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Basic library routines.
+ *
+ * Author: liang@whamcloud.com
+ */
+
+#ifndef __LIBCFS_WORKQUEUE_H__
+#define __LIBCFS_WORKQUEUE_H__
+
+#include <linux/workqueue.h>
+
+struct workqueue_attrs *compat_alloc_workqueue_attrs(void);
+
+int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
+				  const struct workqueue_attrs *attrs);
+
+#endif /* __LIBCFS_WORKQUEUE_H__ */

libcfs/libcfs/Makefile.in
@@ -23,6 +23,9 @@ COMPAT_LIB := @top_srcdir@/lustre_compat/lib/
 include $(libcfs_dir)/../../lustre_compat/lib/Makefile
 libcfs-compat-objs += $(patsubst %,$(COMPAT_LIB)%,$(lib_objs))
 
+COMPAT_MODULE := @top_srcdir@/lustre_compat/
+include $(libcfs_dir)/../../lustre_compat/Makefile
+libcfs-compat-objs += $(patsubst %,$(COMPAT_MODULE)%,$(module_objs))
 
 libcfs-linux-objs := linux-prim.o
 

libcfs/libcfs/linux/linux-prim.c
@@ -37,18 +37,6 @@
 #include <lustre_compat/linux/wait.h>
 #include <lustre_crypto.h>
 
-static int (*cfs_apply_workqueue_attrs_t)(struct workqueue_struct *wq,
-					  const struct workqueue_attrs *attrs);
-
-int cfs_apply_workqueue_attrs(struct workqueue_struct *wq,
-			      const struct workqueue_attrs *attrs)
-{
-	if (cfs_apply_workqueue_attrs_t)
-		return cfs_apply_workqueue_attrs_t(wq, attrs);
-	return 0;
-}
-EXPORT_SYMBOL_GPL(cfs_apply_workqueue_attrs);
-
 /* Linux v5.1-rc5 214d8ca6ee ("stacktrace: Provide common infrastructure")
  * CONFIG_ARCH_STACKWALK indicates that save_stack_trace_tsk symbol is not
  * exported. Use symbol_get() to find if save_stack_trace_tsk is available.
@@ -130,13 +118,15 @@ int __init cfs_arch_init(void)
 #ifndef HAVE_WAIT_VAR_EVENT
 	wait_bit_init();
 #endif
+	rc = lustre_symbols_init();
+	if (rc < 0) {
+		pr_info("lustre_symbols_init: error %d\n", rc);
+		return rc;
+	}
 #ifdef CONFIG_ARCH_STACKWALK
 	task_dump_stack_t =
 		(void *)cfs_kallsyms_lookup_name("stack_trace_save_tsk");
 #endif
-	cfs_apply_workqueue_attrs_t =
-		(void *)cfs_kallsyms_lookup_name("apply_workqueue_attrs");
-
 	rc = shrinker_debugfs_init();
 	if (rc < 0)
 		goto failed;

lnet/include/lnet/lib-cpt.h
@@ -71,8 +71,7 @@
 #include <linux/topology.h>
 #include <linux/version.h>
 #include <linux/vmalloc.h>
-
-#include <lustre_compat/linux/linux-misc.h>
+#include <lustre_compat/linux/workqueue.h>
 
 /* any CPU partition */
 #define CFS_CPT_ANY		(-1)
@@ -325,19 +324,27 @@ struct workqueue_struct *cfs_cpt_bind_workqueue(const char *wq_name,
 						int flags, int cpt, int nthrs)
 {
 	cpumask_var_t *mask = cfs_cpt_cpumask(tbl, cpt);
-	struct workqueue_attrs attrs = { };
+	struct workqueue_attrs *attrs;
 	struct workqueue_struct *wq;
 
-	wq = alloc_workqueue("%s", WQ_UNBOUND | flags, nthrs, wq_name);
-	if (!wq)
+	attrs = compat_alloc_workqueue_attrs();
+	if (!attrs)
 		return ERR_PTR(-ENOMEM);
 
-	if (mask && alloc_cpumask_var(&attrs.cpumask, GFP_KERNEL)) {
-		cpumask_copy(attrs.cpumask, *mask);
+	wq = alloc_workqueue("%s", WQ_UNBOUND | flags, nthrs, wq_name);
+	if (!wq) {
+		kfree(attrs);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	if (mask) {
+		cpumask_copy(attrs->cpumask, *mask);
 		cpus_read_lock();
-		cfs_apply_workqueue_attrs(wq, &attrs);
+		compat_apply_workqueue_attrs(wq, attrs);
 		cpus_read_unlock();
-		free_cpumask_var(attrs.cpumask);
+		/* Same as free_workqueue_attrs() which is not exported */
+		free_cpumask_var(attrs->cpumask);
+		kfree(attrs);
 	}
 
 	return wq;

lustre/include/lustre_compat.h
@@ -33,6 +33,8 @@
 #include <lustre_compat/linux/xarray.h>
 #include <obd_support.h>
 
+#include <lustre_compat/linux/linux-misc.h>
+
 #ifdef HAVE_4ARGS_VFS_SYMLINK
 #define ll_vfs_symlink(dir, dentry, mnt, path, mode) \
 		       vfs_symlink(dir, dentry, path, mode)

lustre_compat/Makefile
@@ -1,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
+module_objs := symbols.o

commit c8b171bd436a192451545bae8dacbb8518219f5f

LU-19514 compat: alloc_workqueue_attrs_noprof
Linux commit v6.16-rc1-2-gfda6add92438
  workqueue: Basic memory allocation profiling support

Adjust the name of alloc_workqueue_attrs_noprof and fallback to
alloc_workqueue_attrs for older kernels

Test-Parameters: trivial
Fixes: b18bfc7c793 ("LU-18687 compat: improve cfs_kallsyms_lookup_name()")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0cdab19c000e394d393ad81cdf79070f8d63cb94
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62138
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre_compat/symbols.c
@@ -69,6 +69,12 @@ int compat_apply_workqueue_attrs(struct workqueue_struct *wq,
 }
 EXPORT_SYMBOL_GPL(compat_apply_workqueue_attrs);
 
+#ifdef alloc_workqueue_attrs
+# define ALLOC_WQ_ATTRS_FUNC	"alloc_workqueue_attrs_noprof"
+#else
+# define ALLOC_WQ_ATTRS_FUNC	"alloc_workqueue_attrs"
+#endif
+
 int lustre_symbols_init(void)
 {
 	int rc;
@@ -80,7 +86,7 @@ int lustre_symbols_init(void)
 	if (!cfs_kallsyms_lookup_name("kallsyms_lookup_name"))
 		return -EINVAL;
 
-	__alloc_workqueue_attrs = cfs_kallsyms_lookup_name("alloc_workqueue_attrs");
+	__alloc_workqueue_attrs = cfs_kallsyms_lookup_name(ALLOC_WQ_ATTRS_FUNC);
 	if (!__alloc_workqueue_attrs)
 		return -EINVAL;
 