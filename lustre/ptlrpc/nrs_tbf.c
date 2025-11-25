lustre/lustre-release

commit 323a6672b7c06834ffdc8cbc7051f645233d34e1

LU-19137 build: Compatability updates for kernel v6.15
Linux commit v3.11-8733-g55f841ce9395
  super: fix calculation of shrinkable objects for small number
Added vfs_pressure_ratio()
Linux commit v6.14-rc1-13-g52e66823e0be
  fs: dcache: move the sysctl to fs/dcache.c
Removed sysctl_vfs_cache_pressure. Use vfs_pressure_ratio()
and provide a vfs_pressure_ratio() for older kernels.
Also keep a minimum of 99 ldlm objects.

Linux commit v6.14-rc1-13-g52e66823e0be
  fs: dcache: move the sysctl to fs/dcache.c
Use vfs_pressure_ratio() instead of open coding with
sysctl_vfs_cache_pressure which is no longer available

Linux commit v6.14-rc1-45-ge33ce6bd4ea2
  mm: Remove grab_cache_page_write_begin()
Open code grab_cache_page_write_begin() from folio-compat until
direct folio support is landed.

Linux commit v6.14-rc1-61-gd96e2802a802
  mm: Remove wait_on_page_locked()
Open code wait_on_page_locked() from folio-compat until
direct folio support is landed.

Linux v6.12-rc1-119-g908a1d775422
  hrtimers: Introduce hrtimer_setup() to replace hrtimer_init()
Provide a compat hrtimer_setup() for older kernels

Linux commit v6.14-rc4-9-g88d5baf69082
  Change inode_operations.mkdir to return struct dentry *
Provide a ll_vfs_mkdir and mkdir wrappers for older kernels

zfs 2.3.3 build fails with:

lustre/include/obd_support.h:977:49: error: pointer/integer type \
mismatch in conditional expression [-Werror]
  977 |                 kmem_cache_zalloc(slab, (type)) :
      |                                                 ^

Workaround with an explicit inline wrapper for kmem_cache_zalloc()

Test-Parameters: trivial testlist=sanity-pcc
Test-Parameters: trivial testlist=recovery-small
Test-Parameters: trivial testlist=sanity-flr
HPE-bug-id: LUS-12933
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: If0c33faa9139e94c8c7a9aba1eb74f08405de0de
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59970
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nrs_tbf.c
@@ -2882,8 +2882,8 @@ static int nrs_tbf_start(struct ptlrpc_nrs_policy *policy, char *arg)
 	atomic_set(&head->th_rule_sequence, 0);
 	spin_lock_init(&head->th_rule_lock);
 	INIT_LIST_HEAD(&head->th_list);
-	hrtimer_init(&head->th_timer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
-	head->th_timer.function = nrs_tbf_timer_cb;
+	hrtimer_setup(&head->th_timer, nrs_tbf_timer_cb, CLOCK_MONOTONIC,
+		      HRTIMER_MODE_REL);
 	rc = head->th_ops->o_startup(policy, head);
 	if (rc)
 		GOTO(out_free_heap, rc);

config/Rules.in
@@ -27,6 +27,8 @@ EXTRA_CFLAGS := $(EXTRA_PRE_CFLAGS)
 EXTRA_CFLAGS += @EXTRA_KCFLAGS@ @CFLAGS@
 EXTRA_CFLAGS += $(EXTRA_POST_CFLAGS)
 
+override LINUXINCLUDE += $(EXTRA_CFLAGS)
+
 KBUILD_EXTRA_SYMBOLS += @EXTRA_SYMBOLS@
 
 obj-m += $(patsubst %,%.o,$(MODULES))

config/lustre-build.m4
@@ -194,6 +194,7 @@ AC_DEFUN([LB_KABI_CHECKS], [
 			LC_SRC_HAVE_INVALIDATE_LOCK
 			LC_SRC_HAVE_WB_STAT_MOD
 			LC_SRC_STRUCT_LSM_CONTEXT_EARLY
+			LC_SRC_GRAB_CACHE_PAGE_WRITE_BEGIN_WITH_FLAGS
 
 			LB2_LINUX_TEST_COMPILE_ALL([early],
 				[for available lustre kapi interfaces])
@@ -208,6 +209,7 @@ AC_DEFUN([LB_KABI_CHECKS], [
 			LC_HAVE_INVALIDATE_LOCK
 			LC_HAVE_WB_STAT_MOD
 			LC_STRUCT_LSM_CONTEXT_EARLY
+			LC_GRAB_CACHE_PAGE_WRITE_BEGIN_WITH_FLAGS
 
 			# Run any parallel compile tests
 			LB_PROG_LINUX_SRC

config/lustre-core.m4
@@ -617,6 +617,31 @@ AC_DEFUN([LC_PID_NS_FOR_CHILDREN], [
 ]) # LC_PID_NS_FOR_CHILDREN
 
 #
+# LB2_MSG_LINUX_TEST_RESULT
+#
+# Linux commit v3.11-8733-g55f841ce9395
+#  super: fix calculation of shrinkable objects for small numbers
+#
+AC_DEFUN([LC_SRC_VFS_PRESSURE_RATIO], [
+	LB2_LINUX_TEST_SRC([vfs_pressure_ratio], [
+		#include <linux/dcache.h>
+	],[
+		(void)vfs_pressure_ratio(10);
+	])
+])
+AC_DEFUN([LC_VFS_PRESSURE_RATIO], [
+	LB2_MSG_LINUX_TEST_RESULT([if vfs_pressure_ratio() is available],
+	[vfs_pressure_ratio], [
+		AC_DEFINE(HAVE_VFS_PRESSURE_RATIO, 1,
+			  [vfs_pressure_ratio() is available])
+	], [
+		AC_DEFINE([vfs_pressure_ratio(val)],
+			  [mult_frac((unsigned long)(val), sysctl_vfs_cache_pressure, 100)],
+			  [vfs_pressure_ratio() is not available])
+	])
+]) # LC_VFS_PRESSURE_RATIO
+
+#
 # LC_OLDSIZE_TRUNCATE_PAGECACHE
 #
 # 3.12 truncate_pagecache without oldsize parameter
@@ -4912,12 +4937,126 @@ AC_DEFUN([LC_SRC_HAVE_D_REVALIDATE_WITH_INODE_NAME], [
 	],[-Werror])
 ])
 AC_DEFUN([LC_HAVE_D_REVALIDATE_WITH_INODE_NAME], [
-	LB2_MSG_LINUX_TEST_RESULT([if PagePrivate2() is available],
+	LB2_MSG_LINUX_TEST_RESULT([if d_revalidate() takes inode, name],
 	[dentry_ops_d_revalidate_inode_name], [
 		AC_DEFINE(HAVE_D_REVALIDATE_WITH_INODE_NAME, 1,
 			[dentry operations d_revalidate() takes inode, name])
 	])
 ]) # LC_HAVE_D_REVALIDATE_WITH_INODE_NAME
+
+#
+# LC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN
+#
+# Linux v6.14-rc1-45-ge33ce6bd4ea2
+#   mm: Remove grab_cache_page_write_begin()
+#
+AC_DEFUN([LC_SRC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN], [
+	LB2_LINUX_TEST_SRC([grab_cache_page_write_begin], [
+		#include <linux/pagemap.h>
+	],[
+		struct address_space *mapping = NULL;
+		pgoff_t index = 0;
+
+		(void)grab_cache_page_write_begin(mapping, index
+#ifdef HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN_WITH_FLAGS
+			, 0
+#endif
+			);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN], [
+	LB2_MSG_LINUX_TEST_RESULT([if grab_cache_page_write_begin() is available],
+	[grab_cache_page_write_begin], [
+		AC_DEFINE(HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN, 1,
+			[grab_cache_page_write_begin() is available])
+	], [
+		AC_DEFINE([grab_cache_page_write_begin(m, i)],
+			  [pagecache_get_page((m), (i), FGP_WRITEBEGIN, mapping_gfp_mask((m)))],
+			  [grab_cache_page_write_begin() is unavailable])
+	])
+]) # LC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN
+
+#
+# LC_HAVE_WAIT_ON_PAGE_LOCKED
+#
+# Linux v6.14-rc1-61-gd96e2802a802
+#   mm: Remove wait_on_page_locked()
+#
+AC_DEFUN([LC_SRC_HAVE_WAIT_ON_PAGE_LOCKED], [
+	LB2_LINUX_TEST_SRC([wait_on_page_locked], [
+		#include <linux/pagemap.h>
+	],[
+		wait_on_page_locked((struct page *)NULL);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_WAIT_ON_PAGE_LOCKED], [
+	LB2_MSG_LINUX_TEST_RESULT([if wait_on_page_locked() is available],
+	[wait_on_page_locked], [
+		AC_DEFINE(HAVE_WAIT_ON_PAGE_LOCKED, 1,
+			[wait_on_page_locked() is available])
+	], [
+		AC_DEFINE([wait_on_page_locked(page)],
+			[folio_wait_locked(page_folio((page)))],
+			[wait_on_page_locked() is unavailable])
+	])
+]) # LC_HAVE_WAIT_ON_PAGE_LOCKED
+
+#
+# LC_HAVE_HRTIMER_SETUP
+#
+# Linux v6.12-rc1-119-g908a1d775422
+#   hrtimers: Introduce hrtimer_setup() to replace hrtimer_init()
+#
+AC_DEFUN([LC_SRC_HAVE_HRTIMER_SETUP], [
+	LB2_LINUX_TEST_SRC([hrtimer_setup], [
+		#include <linux/hrtimer.h>
+
+		static enum hrtimer_restart fn(struct hrtimer *timer)
+		{ return HRTIMER_NORESTART; }
+	],[
+		struct hrtimer *timer = NULL;
+
+		hrtimer_setup(timer, fn, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_HRTIMER_SETUP], [
+	LB2_MSG_LINUX_TEST_RESULT([if hrtimer_setup() is available],
+	[hrtimer_setup], [
+		AC_DEFINE(HAVE_HRTIMER_SETUP, 1,
+			[hrtimer_setup() is available])
+	], [
+		AC_DEFINE([hrtimer_setup(t, f, c, m)],
+			  [(hrtimer_init((t), (c), (m)), (t)->function = (f))],
+			  [hrtimer_setup() is unavailable])
+	])
+]) # LC_HAVE_HRTIMER_SETUP
+
+#
+# LC_HAVE_IOPS_MKDIR_RETURNS_DENTRY
+#
+# Linux v6.14-rc4-9-g88d5baf69082
+#   Change inode_operations.mkdir to return struct dentry *
+#
+AC_DEFUN([LC_SRC_HAVE_IOPS_MKDIR_RETURNS_DENTRY], [
+	LB2_LINUX_TEST_SRC([iops_mkdir_returns_dentry], [
+		#include <linux/fs.h>
+	],[
+		struct inode_operations *iop = NULL;
+		struct dentry *din = NULL;
+		struct inode *parent = NULL;
+		umode_t mode = 0700;
+		struct dentry *dentry;
+
+		dentry = iop->mkdir(&nop_mnt_idmap, parent, din, mode);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_IOPS_MKDIR_RETURNS_DENTRY], [
+	LB2_MSG_LINUX_TEST_RESULT([if inode_operations.mkdir() returns dentry],
+	[iops_mkdir_returns_dentry], [
+		AC_DEFINE(HAVE_IOPS_MKDIR_RETURNS_DENTRY, 1,
+			[inode_operations.mkdir() returns dentry])
+	])
+]) # LC_HAVE_IOPS_MKDIR_RETURNS_DENTRY
 
 #
 # LC_PROG_LINUX
@@ -4947,6 +5086,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_FOP_READDIR
 
 	# 3.12
+	LC_SRC_VFS_PRESSURE_RATIO
 	LC_SRC_OLDSIZE_TRUNCATE_PAGECACHE
 	LC_SRC_PTR_ERR_OR_ZERO_MISSING
 	LC_SRC_KIOCB_KI_LEFT
@@ -5147,7 +5287,6 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_ALLOC_INODE_SB
 
 	# 5.19
-	LC_SRC_GRAB_CACHE_PAGE_WRITE_BEGIN_WITH_FLAGS
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_SRC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_SRC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
@@ -5228,6 +5367,12 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	# 6.14
 	LC_SRC_HAVE_D_REVALIDATE_WITH_INODE_NAME
 
+	# 6.15
+	LC_SRC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN
+	LC_SRC_HAVE_WAIT_ON_PAGE_LOCKED
+	LC_SRC_HAVE_HRTIMER_SETUP
+	LC_SRC_HAVE_IOPS_MKDIR_RETURNS_DENTRY
+
 	# kernel patch to extend integrity interface
 	LC_SRC_BIO_INTEGRITY_PREP_FN
 ])
@@ -5264,6 +5409,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_FOP_READDIR
 
 	# 3.12
+	LC_VFS_PRESSURE_RATIO
 	LC_OLDSIZE_TRUNCATE_PAGECACHE
 	LC_PTR_ERR_OR_ZERO_MISSING
 	LC_KIOCB_KI_LEFT
@@ -5472,7 +5618,6 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_ALLOC_INODE_SB
 
 	# 5.19
-	LC_GRAB_CACHE_PAGE_WRITE_BEGIN_WITH_FLAGS
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_READ_FOLIO
 	LC_HAVE_READ_CACHE_PAGE_FILLER_WITH_FILE
 	LC_HAVE_ADDRESS_SPACE_OPERATIONS_RELEASE_FOLIO
@@ -5553,6 +5698,12 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 
 	# 6.14
 	LC_HAVE_D_REVALIDATE_WITH_INODE_NAME
+
+	# 6.15
+	LC_HAVE_GRAB_CACHE_PAGE_WRITE_BEGIN
+	LC_HAVE_WAIT_ON_PAGE_LOCKED
+	LC_HAVE_HRTIMER_SETUP
+	LC_HAVE_IOPS_MKDIR_RETURNS_DENTRY
 
 	# kernel patch to extend integrity interface
 	LC_BIO_INTEGRITY_PREP_FN

lustre/include/lustre_compat.h
@@ -624,6 +624,20 @@ static inline void ll_security_release_secctx(char *secdata, u32 seclen,
 #define ll_set_acl(ns, inode, acl, type)	ll_set_acl(inode, acl, type)
 #endif
 
+#ifdef HAVE_IOPS_MKDIR_RETURNS_DENTRY
+#define ll_vfs_mkdir(id, inode, dentry, mode)	\
+	vfs_mkdir((id), (inode), (dentry), (mode))
+#else
+#define ll_vfs_mkdir(i, inode, dentry, mode) ({				\
+	int rc = vfs_mkdir((i), (inode), (dentry), (mode));		\
+	if (rc) {							\
+		dput((dentry));						\
+		dentry = ERR_PTR(rc);					\
+	}								\
+	(dentry);							\
+})
+#endif
+
 #ifndef HAVE_GENERIC_ERROR_REMOVE_FOLIO
 #ifdef HAVE_FOLIO_BATCH
 #define generic_folio			folio

lustre/include/obd_support.h
@@ -976,11 +976,16 @@ do {									      \
 #define OBD_FREE_PTR_ARRAY_LARGE(ptr, n)			\
 	OBD_FREE_LARGE(ptr, (n) * sizeof(*(ptr)))
 
+static inline void *__kmem_cache_zalloc(struct kmem_cache *cachep, gfp_t flags)
+{
+	return kmem_cache_zalloc(cachep, flags);
+}
+
 #define __OBD_SLAB_ALLOC_VERBOSE(ptr, slab, cptab, cpt, size, type)	      \
 do {									      \
 	LASSERT(ergo((type) != GFP_ATOMIC, !in_interrupt()));		      \
 	(ptr) = (cptab) == NULL ?					      \
-		kmem_cache_zalloc(slab, (type)) :			      \
+		__kmem_cache_zalloc(slab, (type)) :			      \
 		cfs_mem_cache_cpt_alloc(slab, cptab, cpt, (type) | __GFP_ZERO); \
 	if (likely((ptr)))                                                    \
 		OBD_ALLOC_POST(ptr, size, "slab-alloced");                    \

lustre/ldlm/ldlm_lib.c
@@ -2968,10 +2968,8 @@ void target_recovery_init(struct lu_target *lut, svc_handler_t handler)
 	obd->obd_next_recovery_transno = obd->obd_last_committed + 1;
 	obd->obd_recovery_start = 0;
 	obd->obd_recovery_end = 0;
-
-	hrtimer_init(&obd->obd_recovery_timer, CLOCK_MONOTONIC,
-		     HRTIMER_MODE_ABS);
-	obd->obd_recovery_timer.function = &target_recovery_expired;
+	hrtimer_setup(&obd->obd_recovery_timer, target_recovery_expired,
+		      CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
 	target_start_recovery_thread(lut, handler);
 }
 EXPORT_SYMBOL(target_recovery_init);

lustre/ldlm/ldlm_pool.c
@@ -529,7 +529,7 @@ static int ldlm_cli_pool_shrink(struct ldlm_pool *pl,
 	spin_unlock(&ns->ns_lock);
 
 	if (nr == 0)
-		return (unused / 100) * sysctl_vfs_cache_pressure;
+		return vfs_pressure_ratio(unused < 100 ? 0 : unused);
 	else
 		return ldlm_cancel_lru(ns, nr, LCF_ASYNC, 0);
 }

commit 8eae1533c99995431339501256767edcbf9b5f83

LU-19137 ptlrpc: restore timer mode to absolute
Clock mode was incorrectly changed to relative when it is set to
expire at absolute time.

HPE-bug-id: LUS-12933
Fixes: 323a6672b7c0 ("LU-19137 build: Compatability updates for kernel v6.15")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ife8b30668f643d1c7c6524dfbd079fead5e35b39
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62456
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/ptlrpc/nrs_tbf.c
@@ -2977,7 +2977,7 @@ static int nrs_tbf_start(struct ptlrpc_nrs_policy *policy, char *arg)
 	spin_lock_init(&head->th_rule_lock);
 	INIT_LIST_HEAD(&head->th_list);
 	hrtimer_setup(&head->th_timer, nrs_tbf_timer_cb, CLOCK_MONOTONIC,
-		      HRTIMER_MODE_REL);
+		      HRTIMER_MODE_ABS);
 	rc = head->th_ops->o_startup(policy, head);
 	if (rc)
 		GOTO(out_free_heap, rc);