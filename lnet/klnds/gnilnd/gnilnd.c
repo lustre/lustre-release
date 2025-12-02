lustre/lustre-release

commit ea0bdfc7cf0c31ab95a74bbb92c63e469f792350

LU-18925 libcfs: prefer kmap_local_page
Prefer kmap_local* in favor of kmap and kmap_atomic in preparation
of conversion to struct folio and kmap_local_folio

Provide kmap_local_page alias to maintain mapping to
kmap_atomic use in older kernels.

Currently md_read_page [mdc_read_page] returns the acquired page
and kmap()'d. The page is to be kunmap()'d in ll_release_page.

Remove the kmap() in mdc_read_page and kunmap() in ll_release_page
and convert the intervening page_address() usage into kmap_local_page

HPE-bug-id: LUS-12822
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I89ec7de9b6317a72df739500c0c3a942568e5eeb
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58776
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

lnet/klnds/gnilnd/gnilnd_cb.c
@@ -382,6 +382,8 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 		vunmap(addr);
 	} else {
 		do {
+			void *kaddr;
+
 			fraglen = min(kiov->bv_len - offset, nob);
 
 			/* make dang sure we don't send a bogus checksum if somehow we get
@@ -392,9 +394,8 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 				 "odd fraglen %u on nkiov %d, nob %u bv_len %u offset %u kiov 0x%p\n",
 				 fraglen, nkiov, nob, kiov->bv_len,
 				 offset, kiov);
-
-			addr = (void *)kmap(kiov->bv_page) + kiov->bv_offset +
-				offset;
+			kaddr = kmap_local_page(kiov->bv_page)
+			addr = kaddr + kiov->bv_offset + offset;
 			tmpck = _kgnilnd_cksum(cksum, addr, fraglen);
 
 			CDEBUG(D_BUFFS,
@@ -407,7 +408,7 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 			if (dump_blob)
 				kgnilnd_dump_blob(D_BUFFS, "kiov cksum", addr, fraglen);
 
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
 
 			kiov++;
 			nkiov--;

config/lustre-libcfs.m4
@@ -2130,6 +2130,36 @@ AC_DEFUN([LIBCFS_SEC_RELEASE_SECCTX], [
 ]) # LIBCFS_SEC_RELEASE_SECCTX
 
 #
+# LIBCFS_HAVE_KMAP_LOCAL
+#
+# Linux commit v5.10-rc2-80-gf3ba3c710ac5
+#   mm/highmem: Provide kmap_local*
+#
+AC_DEFUN([LIBCFS_SRC_HAVE_KMAP_LOCAL], [
+	LB2_LINUX_TEST_SRC([kmap_local_page], [
+		#include <linux/highmem.h>
+	],[
+		struct page *pg = NULL;
+		void *kaddr = kmap_local_page(pg);
+
+		kunmap_local(kaddr);
+	],[-Werror])
+])
+AC_DEFUN([LIBCFS_HAVE_KMAP_LOCAL], [
+	LB2_MSG_LINUX_TEST_RESULT([if 'kmap_local*' are available],
+	[kmap_local_page], [
+		AC_DEFINE(HAVE_KMAP_LOCAL, 1,
+			[kmap_local_* functions are available])
+	],[
+		## Map kmap_local_page to kmap_atomic for older kernels
+		AC_DEFINE([kmap_local_page(p)], [kmap_atomic(p)],
+			  [need kmap_local_page map to atomic])
+		AC_DEFINE([kunmap_local(kaddr)], [kunmap_atomic((kaddr))],
+			  [need kunmap_local map to atomic])
+	])
+]) # LIBCFS_HAVE_KMAP_LOCAL
+
+#
 # LIBCFS_HAVE_KFREE_SENSITIVE
 #
 # kernel v5.10-rc1~3
@@ -2650,6 +2680,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_KEY_NEED_UNLINK
 	LIBCFS_SRC_SEC_RELEASE_SECCTX
 	# 5.10
+	LIBCFS_SRC_HAVE_KMAP_LOCAL
 	LIBCFS_SRC_HAVE_KFREE_SENSITIVE
 	LIBCFS_SRC_HAVE_CRYPTO_SHA2_HEADER
 	LIBCFS_SRC_HAVE_LIST_CMP_FUNC_T
@@ -2807,6 +2838,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	LIBCFS_KEY_NEED_UNLINK
 	LIBCFS_SEC_RELEASE_SECCTX
 	# 5.10
+	LIBCFS_HAVE_KMAP_LOCAL
 	LIBCFS_HAVE_KFREE_SENSITIVE
 	LIBCFS_HAVE_CRYPTO_SHA2_HEADER
 	LIBCFS_HAVE_LIST_CMP_FUNC_T

lnet/klnds/socklnd/socklnd_lib.c
@@ -389,12 +389,11 @@ ksocknal_lib_csum_tx(struct ksock_tx *tx)
 			     tx->tx_hdr.iov_len);
 
 	for (i = 0; i < tx->tx_nkiov; i++) {
-		base = kmap(tx->tx_kiov[i].bv_page) +
-			tx->tx_kiov[i].bv_offset;
+		void *kaddr = kmap_local_page(tx->tx_kiov[i].bv_page);
 
+		base = kaddr + tx->tx_kiov[i].bv_offset;
 		csum = ksocknal_csum(csum, base, tx->tx_kiov[i].bv_len);
-
-		kunmap(tx->tx_kiov[i].bv_page);
+		kunmap_local(kaddr);
 	}
 
 	if (*ksocknal_tunables.ksnd_inject_csum_error) {

lnet/lnet/lib-move.c
@@ -362,9 +362,11 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 		    unsigned int nob)
 {
 	/* NB diov, siov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *daddr = NULL;
-	char	       *saddr = NULL;
+	unsigned int this_nob;
+	char *daddr = NULL;
+	char *saddr = NULL;
+	void *dkaddr;
+	void *skaddr;
 
 	if (nob == 0)
 		return;
@@ -394,12 +396,14 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 				siov->bv_len - soffset,
 				nob);
 
-		if (daddr == NULL)
-			daddr = ((char *)kmap(diov->bv_page)) +
-				diov->bv_offset + doffset;
-		if (saddr == NULL)
-			saddr = ((char *)kmap(siov->bv_page)) +
-				siov->bv_offset + soffset;
+		if (!daddr) {
+			dkaddr = kmap_local_page(diov->bv_page);
+			daddr = dkaddr + diov->bv_offset + doffset;
+		}
+		if (!saddr) {
+			skaddr = kmap_local_page(siov->bv_page);
+			saddr = skaddr + siov->bv_offset + soffset;
+		}
 
 		/* Vanishing risk of kmap deadlock when mapping 2 pages.
 		 * However in practice at least one of the kiovs will be mapped
@@ -416,7 +420,8 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 			daddr += this_nob;
 			doffset += this_nob;
 		} else {
-			kunmap(diov->bv_page);
+			kunmap_local(dkaddr);
+			dkaddr = NULL;
 			daddr = NULL;
 			diov++;
 			ndiov--;
@@ -427,7 +432,8 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 			saddr += this_nob;
 			soffset += this_nob;
 		} else {
-			kunmap(siov->bv_page);
+			kunmap_local(skaddr);
+			skaddr = NULL;
 			saddr = NULL;
 			siov++;
 			nsiov--;
@@ -435,22 +441,23 @@ lnet_copy_kiov2kiov(unsigned int ndiov, struct bio_vec *diov,
 		}
 	} while (nob > 0);
 
-	if (daddr != NULL)
-		kunmap(diov->bv_page);
-	if (saddr != NULL)
-		kunmap(siov->bv_page);
+	if (daddr)
+		kunmap_local(dkaddr);
+	if (saddr)
+		kunmap_local(skaddr);
 }
 EXPORT_SYMBOL(lnet_copy_kiov2kiov);
 
 void
-lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
-		    unsigned int nkiov, struct bio_vec *kiov,
-		    unsigned int kiovoffset,
-		    unsigned int nob)
+lnet_copy_kiov2iov(unsigned int niov, struct kvec *iov, unsigned int iovoffset,
+		   unsigned int nkiov, struct bio_vec *kiov,
+		   unsigned int kiovoffset,
+		   unsigned int nob)
 {
 	/* NB iov, kiov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *addr = NULL;
+	unsigned int this_nob;
+	void *kaddr = NULL;
+	char *addr = NULL;
 
 	if (nob == 0)
 		return;
@@ -480,9 +487,10 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 				(unsigned int)kiov->bv_len - kiovoffset,
 				nob);
 
-		if (addr == NULL)
-			addr = ((char *)kmap(kiov->bv_page)) +
-				kiov->bv_offset + kiovoffset;
+		if (addr == NULL) {
+			kaddr = kmap_local_page(kiov->bv_page);
+			addr = kaddr + kiov->bv_offset + kiovoffset;
+		}
 
 		memcpy((char *)iov->iov_base + iovoffset, addr, this_nob);
 		nob -= this_nob;
@@ -499,7 +507,8 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 			addr += this_nob;
 			kiovoffset += this_nob;
 		} else {
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
+			kaddr = NULL;
 			addr = NULL;
 			kiov++;
 			nkiov--;
@@ -509,7 +518,7 @@ lnet_copy_kiov2iov (unsigned int niov, struct kvec *iov, unsigned int iovoffset,
 	} while (nob > 0);
 
 	if (addr != NULL)
-		kunmap(kiov->bv_page);
+		kunmap_local(kaddr);
 }
 EXPORT_SYMBOL(lnet_copy_kiov2iov);
 
@@ -520,8 +529,9 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 		   unsigned int nob)
 {
 	/* NB kiov, iov are READ-ONLY */
-	unsigned int	this_nob;
-	char	       *addr = NULL;
+	unsigned int this_nob;
+	void *kaddr = NULL;
+	char *addr = NULL;
 
 	if (nob == 0)
 		return;
@@ -551,18 +561,20 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 				(unsigned int)iov->iov_len - iovoffset,
 				nob);
 
-		if (addr == NULL)
-			addr = ((char *)kmap(kiov->bv_page)) +
-				kiov->bv_offset + kiovoffset;
+		if (!addr) {
+			kaddr = kmap_local_page(kiov->bv_page);
+			addr = kaddr + kiov->bv_offset + kiovoffset;
+		}
 
-		memcpy (addr, (char *)iov->iov_base + iovoffset, this_nob);
+		memcpy(addr, (char *)iov->iov_base + iovoffset, this_nob);
 		nob -= this_nob;
 
 		if (kiov->bv_len > kiovoffset + this_nob) {
 			addr += this_nob;
 			kiovoffset += this_nob;
 		} else {
-			kunmap(kiov->bv_page);
+			kunmap_local(kaddr);
+			kaddr = NULL;
 			addr = NULL;
 			kiov++;
 			nkiov--;
@@ -578,8 +590,8 @@ lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
 		}
 	} while (nob > 0);
 
-	if (addr != NULL)
-		kunmap(kiov->bv_page);
+	if (addr)
+		kunmap_local(kaddr);
 }
 EXPORT_SYMBOL(lnet_copy_iov2kiov);
 

lnet/lnet/lnet-crypto.c
@@ -326,9 +326,9 @@ static void cfs_crypto_performance_test(enum cfs_crypto_hash_alg hash_alg)
 		goto out_err;
 	}
 
-	buf = kmap(page);
+	buf = kmap_local_page(page);
 	memset(buf, 0xAD, PAGE_SIZE);
-	kunmap(page);
+	kunmap_local(buf);
 
 	for (start = jiffies, end = start + cfs_time_seconds(1) / 4,
 	     bcount = 0; time_before(jiffies, end) && err == 0; bcount++) {

lustre/include/lu_object.h
@@ -879,7 +879,7 @@ struct lu_rdpg {
 
 /* for dt_index_walk / mdd_readpage */
 void *rdpg_page_get(const struct lu_rdpg *rdpg, unsigned int index);
-void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index);
+void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index, void *kaddr);
 
 enum lu_xattr_flags {
 	LU_XATTR_REPLACE = BIT(0),

lustre/include/obd_support.h
@@ -930,8 +930,11 @@ do {									      \
 #endif
 
 #ifdef POISON_BULK
-#define POISON_PAGE(page, val) do { memset(kmap(page), val, PAGE_SIZE); \
-                                    kunmap(page); } while (0)
+#define POISON_PAGE(page, val) do {			\
+	void *kaddr = kmap_local_page(page);		\
+	memset(kaddr, val, PAGE_SIZE);			\
+	kunmap_local(kaddr);				\
+} while (0)
 #else
 #define POISON_PAGE(page, val) do { } while (0)
 #endif

lustre/llite/dir.c
@@ -163,8 +163,6 @@ struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
 void ll_release_page(struct inode *inode, struct page *page,
 		     bool remove)
 {
-	kunmap(page);
-
 	/* Always remove the page for striped dir, because the page is
 	 * built from temporarily in LMV layer
 	 */
@@ -212,6 +210,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 				partial_readdir_rc);
 
 	while (rc == 0 && !done) {
+		void *kaddr = NULL;
 		struct lu_dirpage *dp;
 		struct lu_dirent  *ent;
 		__u64 hash;
@@ -223,7 +222,8 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 		}
 
 		hash = MDS_DIR_END_OFF;
-		dp = page_address(page);
+		kaddr = kmap(page);
+		dp = kaddr;
 		for (ent = lu_dirent_start(dp); ent != NULL && !done;
 		     ent = lu_dirent_next(ent)) {
 			__u16          type;
@@ -285,6 +285,10 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 
 		if (done) {
 			pos = hash;
+			if (kaddr) {
+				kunmap(kmap_to_page(kaddr));
+				kaddr = NULL;
+			}
 			ll_release_page(inode, page, false);
 			break;
 		}
@@ -294,12 +298,20 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 		if (pos == MDS_DIR_END_OFF) {
 			/* End of directory reached. */
 			done = 1;
+			if (kaddr) {
+				kunmap(kmap_to_page(kaddr));
+				kaddr = NULL;
+			}
 			ll_release_page(inode, page, false);
 		} else {
+			u32 flags = le32_to_cpu(dp->ldp_flags);
+
 			/* Normal case: continue to the next page.*/
-			ll_release_page(inode, page,
-					le32_to_cpu(dp->ldp_flags) &
-					LDF_COLLIDE);
+			if (kaddr) {
+				kunmap(kmap_to_page(kaddr));
+				kaddr = NULL;
+			}
+			ll_release_page(inode, page, flags & LDF_COLLIDE);
 			next = pos;
 			page = ll_get_dir_page(inode, op_data, pos,
 					       is_hash64, partial_readdir_rc);

commit ef3c02010cd86a04ae316377205984b60428f26d

LU-19570 build: unused 'ni' missing semi-colon
Fix missing semi-colon in gnilnd

lnet/klnds/gnilnd/gnilnd.c:37:18: error: \
   unused variable 'ni' [-Werror=unused-variable]

HPE-bug-id: LUS-13064, LUS-13095
Test-Parameters: trivial
Fixes: ea0bdfc7cf ("LU-18925 libcfs: prefer kmap_local_page")
Fixes: aa2fa25325 ("LU-10391 lnet: support setting LND timeouts")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I281ebafac054936547cdb25b90fe4790d8374a36
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62331
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/klnds/gnilnd/gnilnd.c
@@ -72,8 +72,6 @@ kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data,
 static int
 kgnilnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 {
-	struct lnet_ni *ni = data;
-
 	if (cmd != LNET_CMD_NETS)
 		return -EOPNOTSUPP;
 

lnet/klnds/gnilnd/gnilnd_cb.c
@@ -394,7 +394,7 @@ kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 				 "odd fraglen %u on nkiov %d, nob %u bv_len %u offset %u kiov 0x%p\n",
 				 fraglen, nkiov, nob, kiov->bv_len,
 				 offset, kiov);
-			kaddr = kmap_local_page(kiov->bv_page)
+			kaddr = kmap_local_page(kiov->bv_page);
 			addr = kaddr + kiov->bv_offset + offset;
 			tmpck = _kgnilnd_cksum(cksum, addr, fraglen);
 