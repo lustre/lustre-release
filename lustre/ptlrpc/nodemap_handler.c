lustre/lustre-release

commit e899baa2b2e460bc3d13a33f73978dd45f10a20d

LU-18357 ptlrpc: Add read-only flag for nodemap filesets
When adding a fileset to a nodemap, each fileset was previously set to
read-write except if the whole nodemap is set to read-only.

This patch allows setting individual filesets as read-only. Depending
on which fileset applies during a client mount, the fileset's
read-only flag is used to force the mount as read-only. This is done
by setting an -EROFS error code server-side without setting the root
FID if the MDT connection does not match the fileset's ro-flag.

Note, the global nodemap "readonly_mount" property overwrites the
individual fileset read-only flag.

This patch adds the optional "--ro" flag to "lctl
nodemap_fileset_add". "lctl get_param nodemap.NM_NAME.fileset" was
modified to visualize the flag per fileset:

nodemap.c0.fileset=
[
 { primary:     /remote, mode: ro },
 { alternate:   /home },
 { alternate:   /data, mode: ro }
]

Sanity-sec "27e" was added to test this flag. Sanity-sec "27d" was
extended to explicitly test for a read-write mount.

Important implementation details:
The root fid retrieval is now done earlier in
"client_common_fill_super()". Previously, "md_get_root()" was called
after the client already connected to an MDT and OST. Since setting
the mount-point to read-only requires reconnecting to the MDT with the
corresponding read-only flag, "md_get_root()" is now called after
connecting to an MDT (before connecting to an OST) and after the
nodemap may have been set to read-only due to the "readonly_mount"
property.

Further, checking whether subdirectory mounts are server-side
available with the "OBD_CONNECT_SUBTREE" connect flag is now done
after the "MDS_GET_ROOT" RPC is sent (instead of before as
previously).  This guarantees that the connect flags are set and
"OBD_CONNECT_SUBTREE" can be checked.

Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I25e97e4c8da6318ec6d96a7797392257a4947e6b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57829

Documentation/man8/lctl-nodemap-fileset-add.8
@@ -5,7 +5,8 @@ lctl-nodemap_fileset_add \- add a fileset to a nodemap
 .SY "lctl nodemap_fileset_add"
 or
 .SY "lctl nodemap fileset_add"
-.B --alt
+.RB [ --alt ]
+.RB [ --ro ]
 .BI --name " NODEMAP_NAME"
 .BI --fileset " SUBDIRECTORY"
 .YS
@@ -28,7 +29,15 @@ file system by explicitly restricting clients of a nodemap to a specific
 when mounting the client. The restriction depends on whether the fileset is
 designated as primary or alternate (using the
 .B --alt
-option). On mounting the Lustre client the following rules apply:
+option). Further, the fileset can be restricted to be mounted as read-only
+using the
+.B --ro
+option.
+Note that the nodemap
+.RB ' readonly_mount '
+property takes precedence if a fileset is set to read-write which is the
+fileset default.
+On mounting the Lustre client the following rules apply:
 .IP 1.
 If the nodemap is inactive or no filesets are defined, no subdirectory
 restrictions are applied.
@@ -48,22 +57,27 @@ the primary fileset.
 If no primary fileset is set, only 3 applies.
 .SH OPTIONS
 .TP
-.BI --name " NODEMAP_NAME"
+.BR -n ", " --name " NODEMAP_NAME"
 The name of the nodemap that this fileset should be associated with.
 .TP
-.BI --fileset " SUBDIRECTORY"
+.BR -f ", " --fileset " SUBDIRECTORY"
 The fileset to restrict the clients to. The fileset must begin with '/'.
 .TP
-.B --alt
+.BR -a ", " --alt
 Indicates that this fileset should be added as an alternate fileset. Without
 this option, the fileset is added as the primary fileset. A nodemap is
 restricted to a single primary fileset while up to 255 alternate filesets
 can be added per nodemap. Duplicate fileset definitions across fileset types
 are not allowed.
+.TP
+.BR -r ", " --ro
+Indicates that the fileset should be mounted as read-only (defaults to
+read-write if not set).
 .SH EXAMPLES
 Add a primary fileset to nodemap 'tenant1':
 .EX
-.B # lctl nodemap_fileset_add --name tenant1 --fileset '/dir1'
+.B # lctl nodemap_fileset_add --name tenant1 --fileset '/dir0'
+.B # lctl nodemap_fileset_add --name tenant1 --fileset '/dir1' --ro
 .EE
 .PP
 Add alternate filesets to different nodemaps:
@@ -71,6 +85,7 @@ Add alternate filesets to different nodemaps:
 .B # lctl nodemap_fileset_add --name tenant1 --fileset '/dir2' --alt
 .B # lctl nodemap_fileset_add --name tenant1 --fileset '/dir3' --alt
 .B # lctl nodemap_fileset_add --name tenant2 --fileset '/dir10' --alt
+.B # lctl nodemap_fileset_add --name tenant2 --fileset '/dir11' --alt --ro
 .EE
 .SH AVAILABILITY
 .B lctl nodemap_fileset_add

lustre/ptlrpc/nodemap_handler.c
@@ -1400,6 +1400,7 @@ static void nodemap_fileset_init(struct lu_nodemap *nodemap)
 {
 	nodemap->nm_fileset_prim = NULL;
 	nodemap->nm_fileset_prim_size = 0;
+	nodemap->nm_fileset_prim_ro = false;
 	init_rwsem(&nodemap->nm_fileset_alt_lock);
 	nodemap->nm_fileset_alt = RB_ROOT;
 }
@@ -1409,6 +1410,7 @@ static void nodemap_fileset_prim_reset(struct lu_nodemap *nodemap)
 	OBD_FREE(nodemap->nm_fileset_prim, nodemap->nm_fileset_prim_size);
 	nodemap->nm_fileset_prim = NULL;
 	nodemap->nm_fileset_prim_size = 0;
+	nodemap->nm_fileset_prim_ro = false;
 }
 
 /**
@@ -1554,8 +1556,9 @@ static int nodemap_fileset_clear(struct lu_nodemap *nodemap)
 }
 
 static int nodemap_fileset_add_primary(struct lu_nodemap *nodemap,
-				       const char *fileset_path)
+				       const char *fileset_path, bool read_only)
 {
+	struct lu_nodemap_fileset_info fset_info_new;
 	bool fset_alt_exists;
 	size_t fileset_size;
 	char *fileset;
@@ -1567,7 +1570,8 @@ static int nodemap_fileset_add_primary(struct lu_nodemap *nodemap,
 	/* Check if a primary fileset already exists */
 	if (nodemap->nm_fileset_prim) {
 		/* Silently ignore duplicate primary fileset */
-		if (strcmp(nodemap->nm_fileset_prim, fileset_path) == 0)
+		if (strcmp(nodemap->nm_fileset_prim, fileset_path) == 0 &&
+		    read_only == nodemap->nm_fileset_prim_ro)
 			RETURN(0);
 		else
 			RETURN(-EEXIST);
@@ -1589,11 +1593,15 @@ static int nodemap_fileset_add_primary(struct lu_nodemap *nodemap,
 
 	memcpy(fileset, fileset_path, fileset_size);
 
-	rc = nodemap_idx_fileset_add(nodemap, fileset, NODEMAP_FILESET_PRIM_ID);
+	nodemap_idx_fileset_info_init(&fset_info_new, nodemap->nm_id, fileset,
+				      read_only, NODEMAP_FILESET_PRIM_ID);
+
+	rc = nodemap_idx_fileset_add(nodemap, &fset_info_new);
 
 	if (!rc) {
 		nodemap->nm_fileset_prim = fileset;
 		nodemap->nm_fileset_prim_size = fileset_size;
+		nodemap->nm_fileset_prim_ro = read_only;
 	} else {
 		OBD_FREE(fileset, fileset_size);
 	}
@@ -1602,8 +1610,10 @@ static int nodemap_fileset_add_primary(struct lu_nodemap *nodemap,
 }
 
 static int nodemap_fileset_add_alternate(struct lu_nodemap *nodemap,
-					 const char *fileset_path)
+					 const char *fileset_path,
+					 bool read_only)
 {
+	struct lu_nodemap_fileset_info fset_info;
 	struct lu_fileset_alt *fset;
 	int rc, rc2;
 
@@ -1617,10 +1627,16 @@ static int nodemap_fileset_add_alternate(struct lu_nodemap *nodemap,
 
 	down_write(&nodemap->nm_fileset_alt_lock);
 	/* Silently ignore duplicate alternate fileset as its already set */
-	if (fileset_alt_path_exists(&nodemap->nm_fileset_alt, fileset_path))
-		GOTO(out, rc = 0);
+	fset = fileset_alt_search_path(&nodemap->nm_fileset_alt, fileset_path,
+				   false);
+	if (fset) {
+		if (fset->nfa_ro == read_only)
+			GOTO(out, rc = 0);
 
-	fset = fileset_alt_create(fileset_path);
+		GOTO(out, rc = -EEXIST);
+	}
+
+	fset = fileset_alt_create(fileset_path, read_only);
 	if (!fset)
 		GOTO(out, rc = -ENOMEM);
 
@@ -1632,7 +1648,10 @@ static int nodemap_fileset_add_alternate(struct lu_nodemap *nodemap,
 	}
 
 	/* add fileset to IAM nodemap records */
-	rc = nodemap_idx_fileset_add(nodemap, fset->nfa_path, fset->nfa_id);
+	nodemap_idx_fileset_info_init(&fset_info, nodemap->nm_id,
+				      fset->nfa_path, fset->nfa_ro,
+				      fset->nfa_id);
+	rc = nodemap_idx_fileset_add(nodemap, &fset_info);
 	if (rc) {
 		/* remove added fileset from rb tree on IAM error */
 		rc2 = fileset_alt_delete(&nodemap->nm_fileset_alt, fset);
@@ -1653,6 +1672,7 @@ out:
  * @nodemap: the nodemap to the fileset to
  * @fileset_path: the fileset to be added
  * @alt: true if operation refers to an alt fileset
+ * @read_only: true if the fileset is read-only
  *
  * Return:
  * * %0 on success
@@ -1664,7 +1684,8 @@ out:
  * * %-ENOMEM	could not allocate memory for fileset
  */
 static int nodemap_fileset_add(struct lu_nodemap *nodemap,
-			       const char *fileset_path, bool alt)
+			       const char *fileset_path, bool alt,
+			       bool read_only)
 {
 	int rc;
 
@@ -1672,9 +1693,11 @@ static int nodemap_fileset_add(struct lu_nodemap *nodemap,
 		RETURN(-EINVAL);
 
 	if (alt)
-		rc = nodemap_fileset_add_alternate(nodemap, fileset_path);
+		rc = nodemap_fileset_add_alternate(nodemap, fileset_path,
+						   read_only);
 	else
-		rc = nodemap_fileset_add_primary(nodemap, fileset_path);
+		rc = nodemap_fileset_add_primary(nodemap, fileset_path,
+						 read_only);
 
 	return rc;
 }
@@ -1705,6 +1728,8 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 					const char *fileset_path,
 					bool *out_clean_llog_fileset)
 {
+	struct lu_nodemap_fileset_info fset_info_old;
+	struct lu_nodemap_fileset_info fset_info_new;
 	size_t fileset_size;
 	char *fileset;
 	int rc = 0;
@@ -1724,7 +1749,7 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 
 	/* if fileset is not set, add it instead */
 	if (!nodemap->nm_fileset_prim) {
-		rc = nodemap_fileset_add_primary(nodemap, fileset_path);
+		rc = nodemap_fileset_add_primary(nodemap, fileset_path, false);
 		GOTO(out, rc);
 	}
 
@@ -1736,19 +1761,22 @@ static int nodemap_set_fileset_prim_iam(struct lu_nodemap *nodemap,
 
 	memcpy(fileset, fileset_path, fileset_size);
 
+	nodemap_idx_fileset_info_init(&fset_info_new, nodemap->nm_id, fileset,
+				      false, NODEMAP_FILESET_PRIM_ID);
 	/*
 	 * If a fileset was set by the params llog, it is not set in the IAM
 	 * records yet. In this case, the fileset must be cleaned from the llog.
 	 * Otherwise, we can update the existing IAM fileset.
 	 */
 	if (nodemap->nmf_fileset_use_iam) {
-		rc = nodemap_idx_fileset_update(nodemap,
-						nodemap->nm_fileset_prim,
-						fileset,
-						NODEMAP_FILESET_PRIM_ID);
+		nodemap_idx_fileset_info_init(&fset_info_old, nodemap->nm_id,
+					      nodemap->nm_fileset_prim,
+					      nodemap->nm_fileset_prim_ro,
+					      NODEMAP_FILESET_PRIM_ID);
+		rc = nodemap_idx_fileset_update(nodemap, &fset_info_old,
+						&fset_info_new);
 	} else {
-		rc = nodemap_idx_fileset_add(nodemap, fileset,
-					     NODEMAP_FILESET_PRIM_ID);
+		rc = nodemap_idx_fileset_add(nodemap, &fset_info_new);
 		if (!rc && !nodemap->nm_dyn && out_clean_llog_fileset)
 			*out_clean_llog_fileset = true;
 	}
@@ -1866,7 +1894,8 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
 
 	fileset = nodemap_get_fileset_prim(src);
 	if (fileset) {
-		rc = nodemap_fileset_add(dst, fileset, false);
+		rc = nodemap_fileset_add(dst, fileset, false,
+					 src->nm_fileset_prim_ro);
 		if (rc)
 			GOTO(out, rc);
 	}
@@ -1879,7 +1908,8 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
 
 		src_fset = rb_entry(node, struct lu_fileset_alt, nfa_rb);
 
-		rc = nodemap_fileset_add(dst, src_fset->nfa_path, true);
+		rc = nodemap_fileset_add(dst, src_fset->nfa_path, true,
+					 src_fset->nfa_ro);
 		if (rc)
 			GOTO(out_unlock, rc);
 	}
@@ -2013,7 +2043,8 @@ static bool nodemap_has_any_fileset(const struct lu_nodemap *nodemap)
  * @fileset_out: the output fileset based on the nodemap's fileset information.
  *		 It is the caller's responsibility to OBD_FREE() the output
  *		 buffer. It is only allocated on success (retval == 0).
- * @fileset_out_size: a pointer to the output buffer size
+ * @fileset_size_out: a pointer to the output buffer size
+ * @fileset_ro_out: a pointer to the output read-only flag
  *
  * This function does not verify whether the fileset returned as fileset_dest
  * exists in the Lustre namespace nor does it check any permissions.
@@ -2027,13 +2058,15 @@ static bool nodemap_has_any_fileset(const struct lu_nodemap *nodemap)
  */
 int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 			     const char *fileset_src, char **fileset_out,
-			     int *fileset_out_size)
+			     int *fileset_size_out, bool *fileset_ro_out)
 {
+	struct lu_fileset_alt *fset_alt;
 	size_t combined_path_len;
 	char *fset = NULL;
+	bool fset_ro = false;
 	int fset_size, rc;
 
-	if (!nodemap || !fileset_out || !fileset_out_size)
+	if (!nodemap || !fileset_out || !fileset_size_out)
 		RETURN(-EINVAL);
 
 	fset_size = PATH_MAX + 1;
@@ -2072,6 +2105,7 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 		if (rc < 0)
 			GOTO(out, rc = -ENAMETOOLONG);
 
+		fset_ro = nodemap->nm_fileset_prim_ro;
 		GOTO(out, rc = 0);
 	}
 
@@ -2083,12 +2117,15 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 			rc = strscpy(fset, fileset_src, fset_size);
 			if (rc < 0)
 				GOTO(out, rc = -ENAMETOOLONG);
+
+			fset_ro = nodemap->nm_fileset_prim_ro;
 			GOTO(out, rc = 0);
 		}
 	}
 
-	if (fileset_alt_search_path(&nodemap->nm_fileset_alt, fileset_src,
-				    true)) {
+	fset_alt = fileset_alt_search_path(&nodemap->nm_fileset_alt,
+					   fileset_src, true);
+	if (fset_alt) {
 		/* if fileset_src matches any fileset either exactly or as
 		 * a prefix, set fileset_out to fileset_src
 		 */
@@ -2096,6 +2133,7 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 		if (rc < 0)
 			GOTO(out, rc = -ENAMETOOLONG);
 
+		fset_ro = fset_alt->nfa_ro;
 		GOTO(out, rc = 0);
 	}
 
@@ -2113,6 +2151,7 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 		if (rc < 0 || rc >= fset_size)
 			GOTO(out, rc = -ENAMETOOLONG);
 
+		fset_ro = nodemap->nm_fileset_prim_ro;
 		GOTO(out, rc = 0);
 	}
 
@@ -2124,7 +2163,8 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 out:
 	if (rc == 0) {
 		*fileset_out = fset;
-		*fileset_out_size = fset_size;
+		*fileset_size_out = fset_size;
+		*fileset_ro_out = fset_ro;
 	} else {
 		OBD_FREE(fset, fset_size);
 	}
@@ -3701,11 +3741,7 @@ EXPORT_SYMBOL(nodemap_test_id);
  * cfg_nodemap_fileset_cmd() - Fileset command handler and entry point for
  * all "lctl nodemap_fileset*" ops
  *
- * @cmd: command type
- * @nodemap_name: name of the nodemap to set fileset on
- * @fileset: string containing fileset
- * @alt: true if operation refers to an alt fileset
- * @checkperm: true if permission check is required
+ * @lcfg: lustre cfg for fileset operation
  *
  * Return:
  * * %0 on success
@@ -3713,20 +3749,27 @@ EXPORT_SYMBOL(nodemap_test_id);
  * * %-ENAMETOOLONG	fileset is too long
  * * %-EIO		undo operation failed during IAM update
  */
-static int cfg_nodemap_fileset_cmd(enum lcfg_command_type cmd,
-				   const char *nodemap_name,
-				   const char *fileset, bool alt,
-				   bool checkperm, bool *out_clean_llog_fileset)
+static int cfg_nodemap_fileset_cmd(struct lustre_cfg *lcfg,
+				   bool *out_clean_llog_fileset)
 {
+	char *param, *nodemap_name, *fset;
+	bool fset_ro = false, fset_alt = false;
 	struct lu_nodemap *nodemap = NULL;
 	int rc;
 
 	ENTRY;
 
-	if (!nodemap_name || nodemap_name[0] == '\0' || !fileset)
+	if (lcfg->lcfg_bufcount < 3 || lcfg->lcfg_bufcount > 5)
 		RETURN(-EINVAL);
 
-	if (strlen(fileset) > PATH_MAX)
+	nodemap_name = lustre_cfg_string(lcfg, 1);
+	fset = lustre_cfg_string(lcfg, 2);
+
+	/* fileset can be \0 in some operations like nodemap_set_fileset */
+	if (!nodemap_name || nodemap_name[0] == '\0' || !fset)
+		RETURN(-EINVAL);
+
+	if (strlen(fset) > PATH_MAX)
 		RETURN(-ENAMETOOLONG);
 
 	mutex_lock(&active_config_lock);
@@ -3736,18 +3779,33 @@ static int cfg_nodemap_fileset_cmd(enum lcfg_command_type cmd,
 		RETURN(PTR_ERR(nodemap));
 	}
 
-	if (checkperm && !allow_op_on_nm(nodemap))
+	if (!allow_op_on_nm(nodemap))
 		GOTO(out_unlock, rc = -ENXIO);
 
-	switch (cmd) {
+	switch (lcfg->lcfg_command) {
 	case LCFG_NODEMAP_SET_FILESET:
-		rc = nodemap_set_fileset_prim_iam(nodemap, fileset, out_clean_llog_fileset);
+		rc = nodemap_set_fileset_prim_iam(nodemap, fset,
+						  out_clean_llog_fileset);
 		break;
 	case LCFG_NODEMAP_FILESET_ADD:
-		rc = nodemap_fileset_add(nodemap, fileset, alt);
+		if (lcfg->lcfg_bufcount != 5)
+			GOTO(out_unlock, rc = -EINVAL);
+		/* check if alternate fileset */
+		param = lustre_cfg_string(lcfg, 3);
+		rc = kstrtobool(param, &fset_alt);
+		if (rc)
+			GOTO(out_unlock, rc);
+
+		/* get read-only flag */
+		param = lustre_cfg_string(lcfg, 4);
+		rc = kstrtobool(param, &fset_ro);
+		if (rc)
+			GOTO(out_unlock, rc);
+
+		rc = nodemap_fileset_add(nodemap, fset, fset_alt, fset_ro);
 		break;
 	case LCFG_NODEMAP_FILESET_DEL:
-		rc = nodemap_fileset_del(nodemap, fileset);
+		rc = nodemap_fileset_del(nodemap, fset);
 		break;
 	default:
 		rc = -EINVAL;
@@ -4060,7 +4118,6 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 	const char *nidstr = NULL;
 	unsigned long client_id;
 	struct lnet_nid	nid;
-	bool fset_alt = false;
 	char *param = NULL;
 	char fs_idstr[16];
 	__u32 fs_id, cmd;
@@ -4206,20 +4263,8 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 	case LCFG_NODEMAP_SET_FILESET:
 	case LCFG_NODEMAP_FILESET_ADD:
 	case LCFG_NODEMAP_FILESET_DEL:
-		if (lcfg->lcfg_bufcount < 3 || lcfg->lcfg_bufcount > 4)
-			GOTO(out_lcfg, rc = -EINVAL);
-		if (lcfg->lcfg_bufcount == 4) {
-			param = lustre_cfg_string(lcfg, 3);
-			rc = kstrtobool(param, &fset_alt);
-			if (rc)
-				break;
-		}
-		nodemap_name = lustre_cfg_string(lcfg, 1);
-		param = lustre_cfg_string(lcfg, 2);
-		rc = cfg_nodemap_fileset_cmd(cmd, nodemap_name, param, fset_alt,
-					     true, out_clean_llog_fileset);
+		rc = cfg_nodemap_fileset_cmd(lcfg, out_clean_llog_fileset);
 		break;
-
 	default:
 		rc = -ENOTTY;
 	}

commit 2eb71b22af1f74a3ccc1aabd75d26b3be88990dc

LU-19506 nodemap: appended filesets must consider alt filesets
When mounting a subdirectory, several mounting rules are in place that
handle matching a subdirectory to the correct fileset. Here, the man
page for "lctl nodemap_fileset_add" states case 4: "If fileset matches
in 3, the presented mounting subdirectory is appended to the primary
fileset."

However, this results in the read-only flag not being considered if
the appended fileset now matches a read-only alternate fileset. For
instance, for the fileset configuration
1. /dir
2. /dir/subdir (ro)
it is possible to mount "...:/FSNAME/subdir", which appends "subdir"
to the primary fileset, resulting in the subdirectory "/dir/subdir".
Currently, no other fileset is considered, but, in such a case, the
alt fileset imposes read-only restrictions. Therefore, alt filesets
should be considered in case 4 of the mounting rules for filesets.

This patch addresses this issue and modifies sanity-sec test_27e to
test this case.

Fixes: e899baa2b2 ("LU-18357 ptlrpc: Add read-only flag for nodemap filesets")
Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I0e6531a09b78595fc2c9e1f55c9a9bcf6617a73a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62425
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

Documentation/man8/lctl-nodemap-fileset-add.8
@@ -52,7 +52,9 @@ mounted subdirectory exactly or as a prefix, the subdirectory mount is used
 as the file system root.
 .IP 4.
 If fileset matches in 3, the presented mounting subdirectory is appended to
-the primary fileset.
+the primary fileset. Note, if the resulting subdirectory is matched by any other
+(alternate) fileset that has the read-only flag set, read-only is enforced for
+this mounting subdirectory.
 .PP
 If no primary fileset is set, only 3 applies.
 .SH OPTIONS

lustre/ptlrpc/nodemap_handler.c
@@ -3002,6 +3002,16 @@ int nodemap_fileset_get_root(struct lu_nodemap *nodemap,
 			GOTO(out, rc = -ENAMETOOLONG);
 
 		fset_ro = nodemap->nm_fileset_prim_ro;
+
+		/* check whether the appended fileset is represented by any
+		 * alternate fileset with a different read-only flag. In this
+		 * case the alternate fileset's read-only flag takes precedence.
+		 */
+		fset_alt = fileset_alt_search_path(&nodemap->nm_fileset_alt,
+						   fset, true);
+		if (fset_alt)
+			fset_ro = fset_alt->nfa_ro;
+
 		GOTO(out, rc = 0);
 	}
 

lustre/tests/sanity-sec.sh
@@ -3213,11 +3213,6 @@ multi_fileset_test_cleanup() {
 		error "unable to mount client ${clients_arr[0]}"
 	wait_ssk
 
-	do_node ${clients_arr[0]} rm "${DIR}/$tfile" ||
-		error "unable to remove test file ${DIR}/$tfile"
-	do_node ${clients_arr[0]} rm -rf "${DIR}/$tdir" ||
-		error "unable to remove dir ${DIR}/$tdir"
-
 	if [[ -n "$FILESET" && -z "$SKIP_FILESET" ]]; then
 		cleanup_mount $MOUNT
 		zconf_mount_clients $CLIENTS $MOUNT
@@ -3471,6 +3466,12 @@ multi_fileset_test_run() {
 		multi_fileset_test_success "/${mult_fset_root}/${subd_p}" \
 			"${MOUNT}/${prefix}${subd_p}" false
 		multi_fileset_test_success "/${mult_fset_root}/$prim_subdir" \
+			"${MOUNT}/${prefix}${subsubd_p0}" true
+		# exercise multi fileset mount case 4 appending mounting subdir
+		# to primary fileset. In this case, the appended fileset
+		# represents an alt fileset which means its read-only flag needs
+		# to be taken into account.
+		multi_fileset_test_success "/$subsubd_p0" \
 			"${MOUNT}/${prefix}${subsubd_p0}" true
 
 		# 2: prim fileset (alt subdir) is ro and alt fileset is rw