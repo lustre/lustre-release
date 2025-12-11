lustre/lustre-release

commit 45c9ef1dfff207086665c764c7d500c00aa03c7f

LU-4020 hsm: allow copytool event monitoring with JSON
Adds hooks into various llapi_hsm_* functions to emit JSON-formatted
event messages for consumption by a monitoring agent. The copytool
needs to be supplied with an optional --event-fifo argument to
enable this feature.

Incorporates the following work done by Bruno Faccini:
* Put all JSON routines in a separate file/lib to allow for
  LGPL licensing.
* Major code cleanup to follow best practices and address review
  concerns.

Signed-off-by: Michael MacDonald <michael.macdonald@intel.com>
Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: I1fecfb8e60ef091c6d37d16dc1faf40be45c5ee2
Reviewed-on: http://review.whamcloud.com/7790
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/utils/liblustreapi_json.c
@@ -1,0 +1,325 @@
+/*
+ * LGPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * (C) Copyright 2014 Intel Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 or (at your discretion) any later version.
+ * (LGPL) version 2.1 accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * LGPL HEADER END
+ */
+/*
+ * lustre/utils/liblustreapi_json.c
+ *
+ * lustreapi library for json calls
+ *
+ * Author: Michael MacDonald <michael.macdonald@intel.com>
+ * Author: Bruno Faccini <bruno.faccini@intel.com>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <malloc.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#ifdef HAVE_LINUX_UNISTD_H
+#include <linux/unistd.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <liblustre.h>
+#include <lustre/lustreapi.h>
+
+/** Quick-n'-dirty JSON string escape routine.
+ * \param[out]	out_string	JSON-escaped string, allocated here
+ * \param[in]	in_string	Unescaped string
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ *
+ * http://json.org/
+ * http://www.ietf.org/rfc/rfc4627.txt (section 2.5)
+ */
+int llapi_json_escape_string(char **out_string, char *in_string)
+{
+	int	i;
+	char	escape_chars[] = {'\b', '\f', '\n', '\r', '\t', '"', '\\',
+				  '\0'};
+	char	*escaped_chars[] = {"\\\\b", "\\\\f", "\\\\n", "\\\\r",
+				    "\\\\t", "\\\\\"", "\\\\\\\\"};
+	char	*src = in_string;
+	char	*idx, *dst, *tmp;
+	char	*escaped_string;
+	size_t	tmp_len, escaped_length = strlen(in_string);
+
+	/* add up the extra space needed for the escapes */
+	while (*src) {
+		idx = strchr(escape_chars, *src);
+		if (idx != NULL) {
+			tmp = escaped_chars[idx - escape_chars];
+			escaped_length += strlen(tmp);
+		}
+		src++;
+	}
+
+	escaped_string = calloc(1, escaped_length + 1);
+	if (escaped_string == NULL)
+		return -ENOMEM;
+
+	src = in_string;
+	dst = escaped_string;
+	for (i = 0; *src && i <= escaped_length; i++) {
+		idx = strchr(escape_chars, *src);
+		if (idx != NULL) {
+			tmp = escaped_chars[idx - escape_chars];
+			tmp_len = strlen(tmp);
+			memcpy(dst, tmp, tmp_len);
+			dst += tmp_len;
+			src++;
+		} else {
+			*dst = *src;
+			src++;
+			dst++;
+		}
+	}
+
+	*dst = '\0';
+
+	*out_string = escaped_string;
+
+	return 0;
+}
+
+/** Write a list of JSON items to a filehandle.
+ * \param	json_items	list of JSON items to be written
+ * \param	fp		open filehandle to use for write
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ */
+int llapi_json_write_list(struct llapi_json_item_list **json_items, FILE *fp)
+{
+	int				i;
+	char				*escaped_string = NULL;
+	struct llapi_json_item_list	*list;
+	struct llapi_json_item		*item;
+
+	if (json_items == NULL || *json_items == NULL)
+		return -EINVAL;
+
+	list = *json_items;
+	item = list->ljil_items;
+
+	if (fprintf(fp, "{") < 0)
+		return -errno;
+	for (i = 0; i < list->ljil_item_count; i++) {
+		if (item == NULL) {
+			llapi_err_noerrno(LLAPI_MSG_ERROR,
+					  "%d json items but %d is NULL!",
+					  list->ljil_item_count, i);
+			/* Don't bomb out here so that we still emit
+			 * valid JSON. */
+			break;
+		}
+
+		if (fprintf(fp, "\"%s\": ", item->lji_key) < 0)
+			return -errno;
+		switch (item->lji_type) {
+		case LLAPI_JSON_INTEGER:
+			if (fprintf(fp, "%d", item->lji_integer) < 0)
+				return -errno;
+			break;
+		case LLAPI_JSON_BIGNUM:
+			if (fprintf(fp, LPU64, item->lji_u64) < 0)
+				return -errno;
+			break;
+		case LLAPI_JSON_REAL:
+			if (fprintf(fp, "%f", item->lji_real) < 0)
+				return -errno;
+			break;
+		case LLAPI_JSON_STRING:
+			if (llapi_json_escape_string(&escaped_string,
+							item->lji_string) < 0) {
+				if (escaped_string != NULL)
+					free(escaped_string);
+				return -errno;
+			}
+
+			if (fprintf(fp, "\"%s\"", escaped_string) < 0) {
+				if (escaped_string != NULL)
+					free(escaped_string);
+				return -errno;
+			}
+
+			if (escaped_string != NULL)
+				free(escaped_string);
+			break;
+		default:
+			llapi_err_noerrno(LLAPI_MSG_ERROR,
+				    "Invalid item type: %d", item->lji_type);
+			/* Ensure valid JSON */
+			if (fprintf(fp, "\"\"") < 0)
+				return -errno;
+			break;
+		}
+
+		if (i < list->ljil_item_count - 1)
+			if (fprintf(fp, ", ") < 0)
+				return -errno;
+
+		item = item->lji_next;
+	}
+	if (fprintf(fp, "}\n") < 0)
+		return -errno;
+
+	return 0;
+}
+
+/** Create a list to hold JSON items.
+ * \param[out]	json_items	Item list handle, allocated here
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ */
+int llapi_json_init_list(struct llapi_json_item_list **json_items)
+{
+	struct llapi_json_item_list	*new_list;
+
+	new_list = calloc(1, sizeof(*new_list));
+	if (new_list == NULL)
+		return -ENOMEM;
+
+	new_list->ljil_item_count = 0;
+
+	*json_items = new_list;
+
+	return 0;
+}
+
+/** Deallocate a list of JSON items.
+ * \param	json_items	Item list handle, deallocated here
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ */
+int llapi_json_destroy_list(struct llapi_json_item_list **json_items)
+{
+	int				i;
+	struct llapi_json_item_list	*list;
+	struct llapi_json_item		*cur_item;
+	struct llapi_json_item		*last_item;
+
+	if (json_items == NULL || *json_items == NULL)
+		return -EINVAL;
+
+	list = *json_items;
+	cur_item = list->ljil_items;
+
+	for (i = 0; i < list->ljil_item_count; i++) {
+		if (cur_item == NULL) {
+			llapi_err_noerrno(LLAPI_MSG_ERROR,
+					  "%d json items but %d is NULL!",
+					  list->ljil_item_count, i);
+			return -EINVAL;
+		}
+
+		if (cur_item->lji_key != NULL)
+			free(cur_item->lji_key);
+
+		if (cur_item->lji_type == LLAPI_JSON_STRING
+		    && cur_item->lji_string != NULL)
+			free(cur_item->lji_string);
+
+		last_item = cur_item;
+		cur_item = last_item->lji_next;
+		free(last_item);
+	}
+
+	free(list);
+	*json_items = NULL;
+
+	return 0;
+}
+
+/** Add an item to a list of JSON items.
+ * \param	json_items	Item list handle
+ * \param	key		Item key name
+ * \param	type		Item key type
+ * \param	val		Item key value
+ *
+ * \retval	0 on success.
+ * \retval	-errno on error.
+ */
+int llapi_json_add_item(struct llapi_json_item_list **json_items,
+			char *key, __u32 type, void *val)
+{
+	struct llapi_json_item_list	*list;
+	struct llapi_json_item		*new_item;
+
+	if (json_items == NULL || *json_items == NULL)
+		return -EINVAL;
+
+	if (val == NULL)
+		return -EINVAL;
+
+	list = *json_items;
+
+	new_item = calloc(1, sizeof(*new_item));
+	if (new_item == NULL)
+		return -ENOMEM;
+
+	new_item->lji_key = calloc(1, strlen(key) + 1);
+	if (new_item->lji_key == NULL)
+		return -ENOMEM;
+
+	strncpy(new_item->lji_key, key, strlen(key));
+	new_item->lji_type = type;
+	new_item->lji_next = NULL;
+
+	switch (new_item->lji_type) {
+	case LLAPI_JSON_INTEGER:
+		new_item->lji_integer = *(int *)val;
+		break;
+	case LLAPI_JSON_BIGNUM:
+		new_item->lji_u64 = *(__u64 *)val;
+		break;
+	case LLAPI_JSON_REAL:
+		new_item->lji_real = *(double *)val;
+		break;
+	case LLAPI_JSON_STRING:
+		new_item->lji_string = calloc(1, strlen((char *)val) + 1);
+		if (new_item->lji_string == NULL)
+			return -ENOMEM;
+		strncpy(new_item->lji_string,
+			(char *)val, strlen((char *)val));
+		break;
+	default:
+		llapi_err_noerrno(LLAPI_MSG_ERROR, "Unknown JSON type: %d",
+				  new_item->lji_type);
+		return -EINVAL;
+	}
+
+	if (list->ljil_item_count == 0) {
+		list->ljil_items = new_item;
+	} else {
+		new_item->lji_next = list->ljil_items;
+		list->ljil_items = new_item;
+	}
+	list->ljil_item_count++;
+
+	return 0;
+}

lustre/include/lustre/lustre_user.h
@@ -1202,6 +1202,31 @@ struct hsm_copy {
 	struct hsm_action_item	hc_hai;
 };
 
+/* JSON objects */
+enum llapi_json_types {
+	LLAPI_JSON_INTEGER = 1,
+	LLAPI_JSON_BIGNUM,
+	LLAPI_JSON_REAL,
+	LLAPI_JSON_STRING
+};
+
+struct llapi_json_item {
+	char			*lji_key;
+	__u32			lji_type;
+	union {
+		int	lji_integer;
+		__u64	lji_u64;
+		double	lji_real;
+		char	*lji_string;
+	};
+	struct llapi_json_item	*lji_next;
+};
+
+struct llapi_json_item_list {
+	int			ljil_item_count;
+	struct llapi_json_item	*ljil_items;
+};
+
 /** @} lustreuser */
 
 #endif /* _LUSTRE_USER_H */

lustre/include/lustre/lustreapi.h
@@ -68,6 +68,17 @@ typedef void (*llapi_log_callback_t)(enum llapi_message_level level, int err,
 #define LLAPI_MSG_MASK          0x00000007
 #define LLAPI_MSG_NO_ERRNO      0x00000010
 
+static inline const char *llapi_msg_level2str(enum llapi_message_level level)
+{
+	static const char *levels[LLAPI_MSG_MAX] = {"OFF", "FATAL", "ERROR",
+						    "WARNING", "NORMAL",
+						    "INFO", "DEBUG"};
+
+	if (level >= LLAPI_MSG_MAX)
+		return NULL;
+
+	return levels[level];
+}
 extern void llapi_msg_set_level(int level);
 extern llapi_log_callback_t llapi_error_callback_set(llapi_log_callback_t cb);
 extern llapi_log_callback_t llapi_info_callback_set(llapi_log_callback_t cb);
@@ -270,6 +281,7 @@ extern int llapi_fid2path(const char *device, const char *fidstr, char *path,
 			  int pathlen, long long *recno, int *linkno);
 extern int llapi_path2fid(const char *path, lustre_fid *fid);
 extern int llapi_fd2fid(const int fd, lustre_fid *fid);
+extern int llapi_chomp_string(char *buf);
 
 extern int llapi_get_version(char *buffer, int buffer_size, char **version);
 extern int llapi_get_data_version(int fd, __u64 *data_version, __u64 flags);
@@ -279,7 +291,12 @@ extern int llapi_hsm_state_set_fd(int fd, __u64 setmask, __u64 clearmask,
 				  __u32 archive_id);
 extern int llapi_hsm_state_set(const char *path, __u64 setmask, __u64 clearmask,
 			       __u32 archive_id);
+extern int llapi_hsm_register_event_fifo(char *path);
+extern int llapi_hsm_unregister_event_fifo(char *path);
+extern void llapi_hsm_log_error(enum llapi_message_level level, int _rc,
+				const char *fmt, va_list args);
 
+extern int llapi_get_agent_uuid(char *path, char *buf, size_t bufsize);
 extern int llapi_create_volatile_idx(char *directory, int idx, int mode);
 static inline int llapi_create_volatile(char *directory, int mode)
 {
@@ -335,7 +352,8 @@ extern int llapi_hsm_action_end(struct hsm_copyaction_private **phcp,
 				const struct hsm_extent *he,
 				int hp_flags, int errval);
 extern int llapi_hsm_action_progress(struct hsm_copyaction_private *hcp,
-				     const struct hsm_extent *he, int hp_flags);
+				     const struct hsm_extent *he, __u64 total,
+				     int hp_flags);
 extern int llapi_hsm_action_get_dfid(const struct hsm_copyaction_private *hcp,
 				     lustre_fid *fid);
 extern int llapi_hsm_action_get_fd(const struct hsm_copyaction_private *hcp);
@@ -351,6 +369,14 @@ extern int llapi_hsm_request(const char *path,
 			     const struct hsm_user_request *request);
 extern int llapi_hsm_current_action(const char *path,
 				    struct hsm_current_action *hca);
+
+/* JSON handling */
+extern int llapi_json_init_list(struct llapi_json_item_list **item_list);
+extern int llapi_json_destroy_list(struct llapi_json_item_list **item_list);
+extern int llapi_json_add_item(struct llapi_json_item_list **item_list,
+			       char *key, __u32 type, void *val);
+extern int llapi_json_write_list(struct llapi_json_item_list **item_list,
+				 FILE *fp);
 /** @} llapi */
 
 #endif

lustre/tests/sanity-hsm.sh
@@ -91,6 +91,8 @@ init_agt_vars() {
 	export HSMTOOL=${HSMTOOL:-"lhsmtool_posix"}
 	export HSMTOOL_VERBOSE=${HSMTOOL_VERBOSE:-""}
 	export HSMTOOL_UPDATE_INTERVAL=${HSMTOOL_UPDATE_INTERVAL:=""}
+	export HSMTOOL_EVENT_FIFO=${HSMTOOL_EVENT_FIFO:=""}
+	export HSMTOOL_TESTDIR
 	export HSMTOOL_BASE=$(basename "$HSMTOOL" | cut -f1 -d" ")
 	HSM_ARCHIVE=$(copytool_device $SINGLEAGT)
 	HSM_ARCHIVE_NUMBER=2
@@ -113,6 +115,7 @@ copytool_device() {
 
 # Stop copytool and unregister an existing changelog user.
 cleanup() {
+	copytool_monitor_cleanup
 	copytool_cleanup
 	changelog_cleanup
 	cdt_set_sanity_policy
@@ -141,6 +144,57 @@ search_and_kill_copytool() {
 	do_nodesv $agents "killall -q $HSMTOOL_BASE" || true
 }
 
+copytool_monitor_setup() {
+	local facet=${1:-$SINGLEAGT}
+	local agent=$(facet_active_host $facet)
+
+	local cmd="mktemp --tmpdir=/tmp -d ${TESTSUITE}.${TESTNAME}.XXXX"
+	local test_dir=$(do_node $agent "$cmd") ||
+		error "Failed to create tempdir on $agent"
+	export HSMTOOL_MONITOR_DIR=$test_dir
+
+	# Create the fifo and a monitor (cat dies when copytool dies)
+	do_node $agent "mkfifo -m 0644 $test_dir/fifo" ||
+		error "failed to create copytool fifo on $agent"
+	cmd="cat $test_dir/fifo > $test_dir/events &"
+	cmd+=" echo \\\$! > $test_dir/monitor_pid"
+
+	# This is required for pdsh -Rmrsh and its handling of remote shells.
+	# Regular ssh and pdsh -Rssh work fine without this backgrounded
+	# subshell nonsense.
+	(do_node $agent "$cmd") &
+	export HSMTOOL_MONITOR_PDSH=$!
+
+	# Slightly racy, but just making a best-effort to catch obvious
+	# problems. If we get rid of the ridiculous backgrounded subshell,
+	# this check will need to be updated to just look at the returncode
+	# of do_node.
+	sleep 1
+	ps -p $HSMTOOL_MONITOR_PDSH >&- ||
+		error "Failed to start copytool monitor on $agent"
+}
+
+copytool_monitor_cleanup() {
+	local facet=${1:-$SINGLEAGT}
+	local agent=$(facet_active_host $facet)
+
+	if [ -n "$HSMTOOL_MONITOR_DIR" ]; then
+		# Should die when the copytool dies, but just in case.
+		local cmd="kill \\\$(cat $HSMTOOL_MONITOR_DIR/monitor_pid)"
+		cmd+=" 2>/dev/null || true"
+		do_node $agent "$cmd"
+		do_node $agent "rm -fr $HSMTOOL_MONITOR_DIR"
+		export HSMTOOL_MONITOR_DIR=
+	fi
+
+	# The pdsh should die on its own when the monitor dies. Just
+	# in case, though, try to clean up to avoid any cruft.
+	if [ -n "$HSMTOOL_MONITOR_PDSH" ]; then
+		kill $HSMTOOL_MONITOR_PDSH 2>/dev/null
+		export HSMTOOL_MONITOR_PDSH=
+	fi
+}
+
 copytool_setup() {
 	local facet=${1:-$SINGLEAGT}
 	local lustre_mntpnt=${2:-$MOUNT}
@@ -167,6 +221,8 @@ copytool_setup() {
 	[[ -z "$arc_id" ]] || cmd+=" --archive $arc_id"
 	[[ -z "$HSMTOOL_UPDATE_INTERVAL" ]] ||
 		cmd+=" --update-interval $HSMTOOL_UPDATE_INTERVAL"
+	[[ -z "$HSMTOOL_EVENT_FIFO" ]] ||
+		cmd+=" --event-fifo $HSMTOOL_EVENT_FIFO"
 	cmd+=" --bandwidth 1 $lustre_mntpnt"
 
 	# Redirect the standard output and error to a log file which
@@ -178,6 +234,17 @@ copytool_setup() {
 	do_facet $facet "$cmd < /dev/null > $copytool_log 2>&1" ||
 		error "start copytool $facet on $agent failed"
 	trap cleanup EXIT
+}
+
+get_copytool_event_log() {
+	local facet=${1:-$SINGLEAGT}
+	local agent=$(facet_active_host $facet)
+
+	[ -z "$HSMTOOL_MONITOR_DIR" ] &&
+		error "Can't get event log: No monitor directory!"
+
+	do_node $agent "cat $HSMTOOL_MONITOR_DIR/events" ||
+		error "Could not collect event log from $agent"
 }
 
 copytool_cleanup() {
@@ -594,6 +661,18 @@ wait_all_done() {
 wait_for_grace_delay() {
 	local val=$(get_hsm_param grace_delay)
 	sleep $val
+}
+
+parse_json_event() {
+	local raw_event=$1
+
+	# python2.6 in EL6 includes an internal json module
+	local json_parser='import json; import fileinput;'
+	json_parser+=' print "\n".join(["local %s=\"%s\"" % tuple for tuple in '
+	json_parser+='json.loads([line for line in '
+	json_parser+='fileinput.input()][0]).items()])'
+
+	echo $raw_event | python -c "$json_parser"
 }
 
 # populate MDT device array
@@ -2543,7 +2622,7 @@ test_60() {
 	# option changes the progress reporting interval from the default
 	# (30 seconds) to the user-specified interval.
 	local interval=5
-	local progress_timeout=$((interval * 2))
+	local progress_timeout=$((interval * 3))
 
 	# test needs a new running copytool
 	copytool_cleanup
@@ -2596,6 +2675,227 @@ test_60() {
 	copytool_cleanup
 }
 run_test 60 "Changing progress update interval from default"
+
+test_70() {
+	# test needs a new running copytool
+	copytool_cleanup
+	copytool_monitor_setup
+	HSMTOOL_EVENT_FIFO=$HSMTOOL_MONITOR_DIR/fifo copytool_setup
+
+	# Just start and stop the copytool to generate events.
+	cdt_clear_no_retry
+	copytool_cleanup
+
+	local REGISTER_EVENT
+	local UNREGISTER_EVENT
+	while read event; do
+		local parsed=$(parse_json_event "$event")
+		if [ -z "$parsed" ]; then
+			error "Copytool sent malformed event: $event"
+		fi
+		eval $parsed
+
+		if [ $event_type == "REGISTER" ]; then
+			REGISTER_EVENT=$event
+		elif [ $event_type == "UNREGISTER" ]; then
+			UNREGISTER_EVENT=$event
+		fi
+	done < <(echo $"$(get_copytool_event_log)")
+
+	if [ -z "$REGISTER_EVENT" ]; then
+		error "Copytool failed to send register event to FIFO"
+	fi
+
+	if [ -z "$UNREGISTER_EVENT" ]; then
+		error "Copytool failed to send unregister event to FIFO"
+	fi
+
+	copytool_monitor_cleanup
+	echo "Register/Unregister events look OK."
+}
+run_test 70 "Copytool logs JSON register/unregister events to FIFO"
+
+test_71() {
+	# Bump progress interval for livelier events.
+	local interval=5
+
+	# test needs a new running copytool
+	copytool_cleanup
+	copytool_monitor_setup
+	HSMTOOL_UPDATE_INTERVAL=$interval \
+	HSMTOOL_EVENT_FIFO=$HSMTOOL_MONITOR_DIR/fifo copytool_setup
+
+	mkdir -p $DIR/$tdir
+	local f=$DIR/$tdir/$tfile
+	local fid=$(make_large_for_progress $f)
+
+	$LFS hsm_archive --archive $HSM_ARCHIVE_NUMBER $f ||
+		error "could not archive file"
+	wait_request_state $fid ARCHIVE SUCCEED
+
+	local expected_fields="event_time data_fid source_fid"
+	expected_fields+=" total_bytes current_bytes"
+
+	local START_EVENT
+	local FINISH_EVENT
+	while read event; do
+		# Make sure we're not getting anything from previous events.
+		for field in $expected_fields; do
+			unset $field
+		done
+
+		local parsed=$(parse_json_event "$event")
+		if [ -z "$parsed" ]; then
+			error "Copytool sent malformed event: $event"
+		fi
+		eval $parsed
+
+		if [ $event_type == "ARCHIVE_START" ]; then
+			START_EVENT=$event
+			continue
+		elif [ $event_type == "ARCHIVE_FINISH" ]; then
+			FINISH_EVENT=$event
+			continue
+		elif [ $event_type != "ARCHIVE_RUNNING" ]; then
+			continue
+		fi
+
+		# Do some simple checking of the progress update events.
+		for expected_field in $expected_fields; do
+			if [ -z ${!expected_field+x} ]; then
+				error "Missing $expected_field field in event"
+			fi
+		done
+
+		if [ $total_bytes -eq 0 ]; then
+			error "Expected total_bytes to be > 0"
+		fi
+
+		# These should be identical throughout an archive
+		# operation.
+		if [ $source_fid != $data_fid ]; then
+			error "Expected source_fid to equal data_fid"
+		fi
+	done < <(echo $"$(get_copytool_event_log)")
+
+	if [ -z "$START_EVENT" ]; then
+		error "Copytool failed to send archive start event to FIFO"
+	fi
+
+	if [ -z "$FINISH_EVENT" ]; then
+		error "Copytool failed to send archive finish event to FIFO"
+	fi
+
+	echo "Archive events look OK."
+
+	cdt_clear_no_retry
+	copytool_cleanup
+	copytool_monitor_cleanup
+}
+run_test 71 "Copytool logs JSON archive events to FIFO"
+
+test_72() {
+	# Bump progress interval for livelier events.
+	local interval=5
+
+	# test needs a new running copytool
+	copytool_cleanup
+	copytool_monitor_setup
+	HSMTOOL_UPDATE_INTERVAL=$interval \
+	HSMTOOL_EVENT_FIFO=$HSMTOOL_MONITOR_DIR/fifo copytool_setup
+	local test_file=$HSMTOOL_MONITOR_DIR/file
+
+	local cmd="dd if=/dev/urandom of=$test_file count=16 bs=1000000 "
+	cmd+="conv=fsync"
+	do_facet $SINGLEAGT "$cmd" ||
+		error "cannot create $test_file on $SINGLEAGT"
+	copy2archive $test_file $tdir/$tfile
+
+	mkdir -p $DIR/$tdir
+	local f=$DIR/$tdir/$tfile
+	import_file $tdir/$tfile $f
+	f=$DIR2/$tdir/$tfile
+	echo "Verifying released state: "
+	check_hsm_flags $f "0x0000000d"
+
+	local fid=$(path2fid $f)
+	$LFS hsm_restore $f
+	wait_request_state $fid RESTORE SUCCEED
+
+	local expected_fields="event_time data_fid source_fid"
+	expected_fields+=" total_bytes current_bytes"
+
+	local START_EVENT
+	local FINISH_EVENT
+	while read event; do
+		# Make sure we're not getting anything from previous events.
+		for field in $expected_fields; do
+			unset $field
+		done
+
+		local parsed=$(parse_json_event "$event")
+		if [ -z "$parsed" ]; then
+			error "Copytool sent malformed event: $event"
+		fi
+		eval $parsed
+
+		if [ $event_type == "RESTORE_START" ]; then
+			START_EVENT=$event
+			if [ $source_fid != $data_fid ]; then
+				error "source_fid should == data_fid at start"
+			fi
+			continue
+		elif [ $event_type == "RESTORE_FINISH" ]; then
+			FINISH_EVENT=$event
+			if [ $source_fid != $data_fid ]; then
+				error "source_fid should == data_fid at finish"
+			fi
+			continue
+		elif [ $event_type != "RESTORE_RUNNING" ]; then
+			continue
+		fi
+
+		# Do some simple checking of the progress update events.
+		for expected_field in $expected_fields; do
+			if [ -z ${!expected_field+x} ]; then
+				error "Missing $expected_field field in event"
+			fi
+		done
+
+		if [ $total_bytes -eq 0 ]; then
+			error "Expected total_bytes to be > 0"
+		fi
+
+		# When a restore starts out, the data fid is the same as the
+		# source fid. After the restore has gotten going, we learn
+		# the new data fid. Once the restore has finished, the source
+		# fid is set to the new data fid.
+		#
+		# We test this because some monitoring software may depend on
+		# this behavior. If it changes, then the consumers of these
+		# events may need to be modified.
+		if [ $source_fid == $data_fid ]; then
+			error "source_fid should != data_fid during restore"
+		fi
+	done < <(echo $"$(get_copytool_event_log)")
+
+	if [ -z "$START_EVENT" ]; then
+		error "Copytool failed to send restore start event to FIFO"
+	fi
+
+	if [ -z "$FINISH_EVENT" ]; then
+		error "Copytool failed to send restore finish event to FIFO"
+	fi
+
+	echo "Restore events look OK."
+
+	cdt_clear_no_retry
+	copytool_cleanup
+	copytool_monitor_cleanup
+
+	rm -rf $test_dir
+}
+run_test 72 "Copytool logs JSON restore events to FIFO"
 
 test_90() {
 	file_count=51 # Max number of files constrained by LNET message size

lustre/utils/Makefile.am
@@ -84,7 +84,7 @@ L_IOCTL := $(top_builddir)/libcfs/libcfs/util/l_ioctl.c
 L_KERNELCOMM := $(top_builddir)/libcfs/libcfs/kernel_user_comm.c
 liblustreapitmp_a_SOURCES = liblustreapi.c liblustreapi_hsm.c \
 			    liblustreapi_nodemap.c lustreapi_internal.h \
-			    $(L_IOCTL) $(L_KERNELCOMM)
+			    liblustreapi_json.c $(L_IOCTL) $(L_KERNELCOMM)
 
 # build static and shared lib lustreapi
 liblustreapi.a : liblustreapitmp.a

commit 0f23219ec0f517b5681c856b63196311b91cb85f

LU-19650 utils: Fix Mem leak in function llapi_json_add_item
Fix Memory leak in function llapi_json_add_item which
was reported by cppcheck

Test-Parameters: trivial testlist=sanity-hsm
Fixes: 45c9ef1dfff (LU-4020 hsm: allow copytool event monitoring with JSON)
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I6bbeb586d01da80398a9f1d78c7c71abcd02b551
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62830
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/liblustreapi_json.c
@@ -227,21 +227,24 @@ int llapi_json_destroy_list(struct llapi_json_item_list **json_items)
 	return 0;
 }
 
-/** Add an item to a list of JSON items.
- * \param	json_items	Item list handle
- * \param	key		Item key name
- * \param	type		Item key type
- * \param	val		Item key value
+/**
+ * llapi_json_add_item() - Add an item to a list of JSON items.
+ * @json_items: Item list handle
+ * @key: Item key name
+ * @type: Item key type
+ * @val: Item key value
  *
- * \retval	0 on success.
- * \retval	-errno on error.
+ * Return:
+ * * %0 on success.
+ * * %negative on error.
  */
 int llapi_json_add_item(struct llapi_json_item_list **json_items,
 			char *key, __u32 type, void *val)
 {
-	struct llapi_json_item_list	*list;
-	struct llapi_json_item		*new_item;
+	struct llapi_json_item_list *list;
+	struct llapi_json_item *new_item;
 	size_t len;
+	int rc = 0;
 
 	if (json_items == NULL || *json_items == NULL)
 		return -EINVAL;
@@ -257,8 +260,10 @@ int llapi_json_add_item(struct llapi_json_item_list **json_items,
 
 	len = strlen(key) + 1;
 	new_item->lji_key = calloc(len, sizeof(char));
-	if (new_item->lji_key == NULL)
-		return -ENOMEM;
+	if (new_item->lji_key == NULL) {
+		rc = -ENOMEM;
+		goto out_item;
+	}
 
 	snprintf(new_item->lji_key, len, "%s", key);
 	new_item->lji_type = type;
@@ -277,14 +282,17 @@ int llapi_json_add_item(struct llapi_json_item_list **json_items,
 	case LLAPI_JSON_STRING:
 		len = strlen((char *)val) + 1;
 		new_item->lji_string = calloc(len, sizeof(char));
-		if (new_item->lji_string == NULL)
-			return -ENOMEM;
+		if (new_item->lji_string == NULL) {
+			rc = -ENOMEM;
+			goto out_key;
+		}
 		snprintf(new_item->lji_string, len, "%s", (char *)val);
 		break;
 	default:
 		llapi_err_noerrno(LLAPI_MSG_ERROR, "Unknown JSON type: %d",
 				  new_item->lji_type);
-		return -EINVAL;
+		rc = -EINVAL;
+		goto out_key;
 	}
 
 	if (list->ljil_item_count == 0) {
@@ -295,5 +303,11 @@ int llapi_json_add_item(struct llapi_json_item_list **json_items,
 	}
 	list->ljil_item_count++;
 
-	return 0;
+	return rc;
+out_key:
+	free(new_item->lji_key);
+out_item:
+	free(new_item);
+
+	return rc;
 }