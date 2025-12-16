lustre/lustre-release

commit 0f21e02f17a998fe4ebd61b7bf8261d7308d98d8

LU-12738 pfl: do not instantiate full PFL layout on append
Appending to a PFL file should avoid all component instantiation.
Let's lock only the region of already instantiated components and
check the size again once locked. If the position has changed and
the append size requires the next component instantiation, restart.
Thus, in the worst case we restart only the amount of component
times, not endlessly.

If a parallel IO writes to already instantiated components, it
does not differ from old code - extent lock conflict appears.

If a parallel IO writes to next components, i.e. there is no lock
conflicts, the resulted file will be like a sequential append and
a later write.

HPE-bug-id: LUS-13005 LUS-13100
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: Ic4beb44cafcde6cd27f0d3f9907a99218a880b8d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61688
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/mdd/mdd_device.c
@@ -170,7 +170,7 @@ static int mdd_init0(const struct lu_env *env, struct mdd_device *mdd,
 	/* with a very few number of free catalog entries */
 	mdd->mdd_changelog_min_free_cat_entries = CHLOG_MIN_FREE_CAT_ENTRIES;
 	/* special default striping for files created with O_APPEND */
-	mdd->mdd_append_stripe_count = 1;
+	mdd->mdd_append_stripe_count = 0;
 	mdd->mdd_append_pool[0] = '\0';
 
 	dt_conf_get(env, mdd->mdd_child, &mdd->mdd_dt_conf);

lustre/tests/sanity-flr.sh
@@ -3424,6 +3424,56 @@ test_71() {
 }
 run_test 71 "check mirror extend parent fid"
 
+test_71() {
+	local comp_file=$DIR/$tfile
+	local id
+
+	$LFS mirror create -N -E1M -S1M -c1 -E 2M -E 3M -E eof \
+		-N -E2M -S1M -c1 -E4M -E eof $comp_file ||
+		error "Create $comp_file failed"
+
+	$LFS getstripe $comp_file
+	id=$($LFS getstripe --mirror-id=1 -I0x10001 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "1: 1st component uninstantiated"
+	id=$($LFS getstripe --mirror-id=1 -I0x10002 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "1: 2nd component instantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20005 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "2: 1st component uninstantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20006 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "2: 2nd component instantiated"
+
+	dd if=/dev/urandom bs=1M count=2 >> $comp_file
+	$LFS mirror resync $comp_file || error "cannot resync mirror $comp_file"
+
+	id=$($LFS getstripe --mirror-id=1 -I0x10002 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "3: 2nd component uninstantiated"
+	id=$($LFS getstripe --mirror-id=1 -I0x10003 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "3: 3rd component instantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20005 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "4: 1st component uninstantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20006 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "4: 2nd component instantiated"
+
+	$LFS getstripe $comp_file
+
+	dd if=/dev/urandom bs=1M count=1 >> $comp_file
+	$LFS getstripe $comp_file
+	$LFS mirror resync $comp_file || error "cannot resync mirror $comp_file"
+
+	$LFS getstripe $comp_file
+	id=$($LFS getstripe --mirror-id=1 -I0x10003 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "5: 3rd component uninstantiated"
+	id=$($LFS getstripe --mirror-id=1 -I0x10004 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "5: 4th component instantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20006 $comp_file | grep "l_fid")
+	[[ -z $id ]] && error "6: 2nd component uninstantiated"
+	id=$($LFS getstripe --mirror-id=2 -I0x20007 $comp_file | grep "l_fid")
+	[[ -n $id ]] && error "6: 3rd component instantiated"
+
+	return 0
+}
+run_test 71 "check append on FLR file"
+
 write_file_200() {
 	local tf=$1
 

lustre/tests/sanity-pfl.sh
@@ -719,7 +719,7 @@ test_11a() {
 	rm -f $comp_file
 
 	# only 1st component instantiated
-	$LFS setstripe -E 1M -S 1M -E 2M -E 3M -E -1 $comp_file ||
+	$LFS setstripe -E 1M -S 1M -E 2M -E 3M -E 4M -E -1 $comp_file ||
 		error "Create $comp_file failed"
 
 	local f1=$($LFS getstripe -I1 $comp_file | grep "l_fid")
@@ -753,14 +753,18 @@ test_11a() {
 	f4=$($LFS getstripe -I4 $comp_file | grep "l_fid")
 	[[ -n $f4 ]] && error "3: 4th component instantiated"
 
-	# all 4 components instantiated, using append write
+	# 4 of 5 components instantiated, using append write
 	dd if=/dev/zero of=$comp_file bs=1k count=1 seek=2k
+
 	ls -l $comp_file
 	rwv -f $comp_file -w -a -n 2 $((1024*1023)) 1
 	ls -l $comp_file
 
 	f4=$($LFS getstripe -I4 $comp_file | grep "l_fid")
 	[[ -z $f4 ]] && error "4: 4th component uninstantiated"
+
+	local f5=$($LFS getstripe -I5 $comp_file | grep "l_fid")
+	[[ -n $f5 ]] && error "3: 5th component instantiated"
 
 	return 0
 }
@@ -2299,16 +2303,16 @@ test_23a() {
 	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file ||
 		error "dd append failed"
 
-	local flg_opts="--comp-start 0 -E EOF --comp-flags init"
+	local flg_opts="--comp-start 0 -E 64M --comp-flags init"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: first component (0-EOF) not found"
+	[ $found -eq 1 ] || error "Append: first component (0-64M) not found"
 
 	local ost_idx=$($LFS getstripe -I2 -i $comp_file)
-	[ "$ost_idx" != "" ] && error "Append: second component still exists"
+	[ "$ost_idx" == "" ] && error "Append: second component doesn't exist"
 
-	sel_layout_sanity $comp_file 1
+	sel_layout_sanity $comp_file 2
 }
-run_test 23a "Append: remove EXT comp"
+run_test 23a "Append: EXT comp in place"
 
 test_23b() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
@@ -2321,19 +2325,19 @@ test_23b() {
 	$LFS setstripe -E 64m -E -1 -z 64M $comp_file ||
 		error "Create $comp_file failed"
 
-	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file ||
+	dd if=/dev/zero bs=1M oflag=append count=2 seek=63 of=$comp_file ||
 		error "dd append failed"
 
-	local flg_opts="--comp-start 64M -E EOF --comp-flags init"
+	local flg_opts="--comp-start 64M -E 128M --comp-flags init"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: component (64M-EOF) not found"
+	[ $found -eq 1 ] || error "Append: component (64M-128M) not found"
 
 	local ost_idx=$($LFS getstripe -I3 -i $comp_file)
-	[ "$ost_idx" != "" ] && error "Append: third component still exists"
+	[ "$ost_idx" == "" ] && error "Append: third component doesn't exist"
 
-	sel_layout_sanity $comp_file 2
+	sel_layout_sanity $comp_file 3
 }
-run_test 23b "Append with 0-length comp: remove EXT comp"
+run_test 23b "Append with 0-length comp: shorten EXT comp"
 
 test_23c() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
@@ -2352,20 +2356,20 @@ test_23c() {
 		error "Create $comp_file failed"
 
 	local wms=$(ost_watermarks_set_low_space 0 | grep "watermarks")
-	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file
+	dd if=/dev/zero bs=1M oflag=append count=1 seek=64 of=$comp_file
 	RC=$?
 
 	ost_watermarks_clear_enospc $tfile 0 $wms
 	[ $RC -eq 0 ] || error "dd append failed: $RC"
 
-	local flg_opts="--comp-start 64M -E EOF --comp-flags init"
+	local flg_opts="--comp-start 64M -E 128M --comp-flags init"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: component (64M-EOF) not found"
+	[ $found -eq 1 ] || error "Append: component (64M-128M) not found"
 
 	local ost_idx=$($LFS getstripe -I3 -i $comp_file)
-	[ "$ost_idx" != "" ] && error "Append: EXT component still exists"
+	[ "$ost_idx" == "" ] && error "Append: EXT component doesn't exist"
 
-	sel_layout_sanity $comp_file 2
+	sel_layout_sanity $comp_file 3
 }
 run_test 23c "Append with low on space + 0-length comp: force extension"
 
@@ -2383,16 +2387,16 @@ test_23d() {
 	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file ||
 		error "dd append failed"
 
-	flg_opts="--comp-start 64M -E 640M --comp-flags init"
+	flg_opts="--comp-start 64M -E 640M --comp-flags extension"
 	found=$($LFS find $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Append: component (64M-640M) not found"
 
 	ost_idx=$($LFS getstripe -I3 -i $comp_file)
-	[ "$ost_idx" != "" ] && error "Append: third component still exists"
+	[ "$ost_idx" == "" ] && error "Append: third component doesn't exist"
 
-	sel_layout_sanity $comp_file 3
+	sel_layout_sanity $comp_file 4
 }
-run_test 23d "Append with 0-length comp + next real comp: remove EXT comp"
+run_test 23d "Append with 0-length comp + next real comp: EXT comp in place"
 
 test_23e() {
 	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
@@ -2413,7 +2417,7 @@ test_23e() {
 
 	local wms=$(ost_watermarks_set_low_space 0 | grep "watermarks")
 
-	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file
+	dd if=/dev/zero bs=1M oflag=append count=1 seek=64 of=$comp_file
 	RC=$?
 
 	ost_watermarks_clear_enospc $tfile 0 $wms
@@ -2445,23 +2449,37 @@ test_23f() {
 
 	local ost_idx=$($LFS getstripe -I1 -i $comp_file)
 	local wms=$(ost_watermarks_set_low_space $ost_idx | grep "watermarks")
+	stack_trap "ost_watermarks_clear_enospc $tfile $ost_idx $wms" EXIT
 
 	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file
 	RC=$?
 
-	ost_watermarks_clear_enospc $tfile $ost_idx $wms
 	[ $RC -eq 0 ] || error "dd append failed"
 
-	local flg_opts="--comp-start 64M -E EOF --comp-flags init"
+	local flg_opts="--comp-start 64M -E EOF --comp-flags extension"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: component (64M-EOF) not found"
+	[ $found -eq 1 ] || error "Append: EXT component (64M-EOF) not found"
 
 	ost_idx=$($LFS getstripe -I2 -i $comp_file)
-	[ "$ost_idx" != "" ] && error "Append: extension component still exists"
+	[ "$ost_idx" == "" ] && error "Append: EXT component doesn't exist"
 
 	sel_layout_sanity $comp_file 2
+
+	dd if=/dev/zero bs=1M oflag=append count=1 seek=64 of=$comp_file
+	RC=$?
+
+	[ $RC -eq 0 ] || error "dd append failed"
+
+	local flg_opts="--comp-start 64M -E 128M --comp-flags init"
+	local found=$($LFS find $flg_opts $comp_file | wc -l)
+	[ $found -eq 1 ] || error "Append: EXT component (64M-128M) not found"
+
+	ost_idx=$($LFS getstripe -I2 -i $comp_file)
+	[ "$ost_idx" == "" ] && error "Append: EXT component doesn't exist"
+
+	sel_layout_sanity $comp_file 3
 }
-run_test 23f "Append with low on space: repeat and remove EXT comp"
+run_test 23f "Append with low on space: repeat comp"
 
 OLDIFS="$IFS"
 cleanup_24() {
@@ -2599,6 +2617,24 @@ test_26c() {
 	[ $? != 0 ] || error "append must return an error"
 }
 run_test 26c "Append to not-existend component, crossing the component border"
+
+test_26d() {
+	$LFS setstripe -E 1m -S 1M -c 1 -E 10M $DIR/$tfile
+	dd if=/dev/urandom bs=1M count=1 >> $DIR/$tfile
+	[ $? == 0 ] || error "append failed on 1st dd"
+
+	local id=$($LFS getstripe -I2 $DIR/$tfile | grep "l_fid")
+	[[ -n $id ]] && error "2nd component instantiated"
+
+	dd if=/dev/urandom bs=1M count=5 >> $DIR/$tfile
+	[ $? == 0 ] || error "append failed on 2nd dd"
+
+	id=$($LFS getstripe -I2 $DIR/$tfile | grep "l_fid")
+	[[ -z $id ]] && error "2nd component uninstantiated"
+
+	return 0
+}
+run_test 26d "Append to existend component, not fully specified layout"
 
 test_27() {
 	[[ $($LCTL get_param mdc.*.import) =~ connect_flags.*overstriping ]] ||

lustre/include/cl_object.h
@@ -1821,6 +1821,7 @@ struct cl_io {
 		} ci_rd;
 		struct cl_wr_io {
 			struct cl_io_rw_common wr;
+			loff_t                 wr_append_lockpos;
 			int                    wr_append;
 			int                    wr_sync;
 		} ci_wr;

lustre/include/obd_support.h
@@ -643,6 +643,7 @@ extern bool obd_enable_fname_encoding;
 #define OBD_FAIL_LLITE_STATAHEAD_PAUSE		    0x1433
 #define OBD_FAIL_LLITE_STAT_RACE1		    0x1434
 #define OBD_FAIL_LLITE_STAT_RACE2		    0x1435
+#define OBD_FAIL_LLITE_TRUNC_PAUSE		    0x1436
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

lustre/llite/file.c
@@ -1948,6 +1948,8 @@ out:
 #endif
 }
 
+#define RETRY_ATTEMPTS 1000
+
 static ssize_t
 ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 		   struct file *file, enum cl_io_type iot,
@@ -1969,7 +1971,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 	bool is_aio = false;
 	size_t max_io_bytes;
 	ssize_t result = 0;
-	int retries = 1000;
+	int retries = RETRY_ATTEMPTS;
 	size_t per_bytes;
 	bool partial_io;
 	int rc2 = 0;
@@ -2027,6 +2029,17 @@ restart:
 	io->ci_dio_lock = dio_lock;
 	io->ci_ndelay_tried = retried;
 	io->ci_parallel_dio = is_parallel_dio;
+
+	if (io->u.ci_wr.wr_append) {
+		/* If restarted, attrs are already merged */
+		if (retries == RETRY_ATTEMPTS) {
+			rc = ll_merge_attr(env, inode);
+			if (rc != 0)
+				RETURN(rc);
+		}
+
+		*ppos = i_size_read(inode);
+	}
 
 	if (cl_io_rw_init(env, io, iot, *ppos, per_bytes) == 0) {
 		if (iocb_ki_flags_check(flags, APPEND))

lustre/llite/lcommon_cl.c
@@ -64,7 +64,6 @@ int cl_setattr_ost(struct inode *inode, const struct iattr *attr,
 
 	io = vvp_env_new_io(env);
 	io->ci_obj = obj;
-	io->ci_verify_layout = 1;
 
 	io->u.ci_setattr.sa_attr.lvb_atime = attr->ia_atime.tv_sec;
 	io->u.ci_setattr.sa_attr.lvb_mtime = attr->ia_mtime.tv_sec;
@@ -97,6 +96,7 @@ again:
 			vio->vui_fd = attr->ia_file->private_data;
 
 		result = cl_io_loop(env, io);
+		CFS_FAIL_TIMEOUT(OBD_FAIL_LLITE_TRUNC_PAUSE, 2);
 	} else {
 		result = io->ci_result;
 	}

lustre/llite/vvp_io.c
@@ -596,8 +596,11 @@ static int vvp_io_write_lock(const struct lu_env *env,
 	loff_t end;
 
 	if (io->u.ci_wr.wr_append) {
+		LASSERT(io->u.ci_wr.wr_append_lockpos >=
+			io->u.ci_wr.wr.crw_pos + io->u.ci_wr.wr.crw_bytes);
+
 		start = 0;
-		end   = OBD_OBJECT_EOF;
+		end   = io->u.ci_wr.wr_append_lockpos;
 	} else {
 		start = io->u.ci_wr.wr.crw_pos;
 		end   = start + io->u.ci_wr.wr.crw_bytes - 1;
@@ -1278,8 +1281,18 @@ static int vvp_io_write_start(const struct lu_env *env,
 		 * out-of-order writes.
 		 */
 		ll_merge_attr(env, inode);
-		pos = io->u.ci_wr.wr.crw_pos = i_size_read(inode);
-		vio->vui_iocb->ki_pos = pos;
+		io->u.ci_wr.wr.crw_pos = i_size_read(inode);
+
+		if (io->u.ci_wr.wr_append_lockpos <
+		    io->u.ci_wr.wr.crw_pos + io->u.ci_wr.wr.crw_bytes) {
+			CDEBUG(D_VFSTRACE, "size changed during append old size %llu, new size %llu, bytes %lu\n",
+			       pos, io->u.ci_wr.wr.crw_pos, io->ci_bytes);
+			if (io->ci_bytes == 0)
+				io->ci_need_restart = 1;
+			RETURN(0);
+		}
+
+		pos = vio->vui_iocb->ki_pos = io->u.ci_wr.wr.crw_pos;
 	} else {
 		LASSERTF(vio->vui_iocb->ki_pos == pos,
 			 "ki_pos %lld [%lld, %lld)\n",
@@ -1287,8 +1300,9 @@ static int vvp_io_write_start(const struct lu_env *env,
 			 pos, pos + crw_bytes);
 	}
 
-	CDEBUG(D_VFSTRACE, DNAME": write [%llu, %llu)\n",
-	       encode_fn_file(file), pos, pos + crw_bytes);
+	CDEBUG(D_VFSTRACE, DNAME": write [%llu, %llu) append %d append lockpos "
+	       "%llu\n", encode_fn_file(file), pos, pos + crw_bytes,
+	       cl_io_is_append(io), io->u.ci_wr.wr_append_lockpos);
 
 	/* The maximum Lustre file size is variable, based on the OST maximum
 	 * object size and number of stripes.  This needs another check in

commit f0fa8173c136b990de9f8df18b700c7901a99f4e

LU-12738 mdd: enable append_stripe_count=1 by default
Re-enable mdd.*.append_stripe_count=1 by default until the client and
MDS can negotiate whether an appending write will instantiate all of
the components of the file or not.

This is needed temporarily to ensure that old clients do not cause
problems for MDS servers that do not enable append layouts by default.

Fixes: 0f21e02f17 ("LU-12738 pfl: do not instantiate full PFL layout on append")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: Ibefbb30b646671f1e91a822dc8d66b9258169811
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63067
Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/mdd/mdd_device.c
@@ -170,7 +170,7 @@ static int mdd_init0(const struct lu_env *env, struct mdd_device *mdd,
 	/* with a very few number of free catalog entries */
 	mdd->mdd_changelog_min_free_cat_entries = CHLOG_MIN_FREE_CAT_ENTRIES;
 	/* special default striping for files created with O_APPEND */
-	mdd->mdd_append_stripe_count = 0;
+	mdd->mdd_append_stripe_count = 1;
 	mdd->mdd_append_pool[0] = '\0';
 	mutex_init(&mdd->mdd_changelog_mutex);
 

lustre/tests/sanity-flr.sh
@@ -3424,7 +3424,7 @@ test_71() {
 }
 run_test 71 "check mirror extend parent fid"
 
-test_71() {
+test_72() {
 	local comp_file=$DIR/$tfile
 	local id
 
@@ -3472,7 +3472,7 @@ test_71() {
 
 	return 0
 }
-run_test 71 "check append on FLR file"
+run_test 72 "check append on FLR file"
 
 write_file_200() {
 	local tf=$1

lustre/tests/sanity-pfl.sh
@@ -2437,8 +2437,8 @@ test_23e() {
 run_test 23e "Append with next real comp: spillover and backward extension"
 
 test_23f() {
-	[ $OSTCOUNT -lt 2 ] && skip "needs >= 2 OSTs"
-	[ "$MDS1_VERSION" -lt $(version_code $SEL_VER) ] &&
+	(( $OSTCOUNT >= 2 )) || skip "needs >= 2 OSTs"
+	(( "$MDS1_VERSION" >= $(version_code $SEL_VER) )) ||
 		skip "skipped for lustre < $SEL_VER"
 
 	local comp_file=$DIR/$tdir/$tfile
@@ -2454,11 +2454,11 @@ test_23f() {
 	dd if=/dev/zero bs=1M oflag=append count=1 of=$comp_file
 	RC=$?
 
-	[ $RC -eq 0 ] || error "dd append failed"
+	(( $RC == 0 )) || error "dd append failed"
 
 	local flg_opts="--comp-start 64M -E EOF --comp-flags extension"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: EXT component (64M-EOF) not found"
+	(( $found == 1 )) || error "Append: EXT component (64M-EOF) not found"
 
 	ost_idx=$($LFS getstripe -I2 -i $comp_file)
 	[ "$ost_idx" == "" ] && error "Append: EXT component doesn't exist"
@@ -2468,11 +2468,11 @@ test_23f() {
 	dd if=/dev/zero bs=1M oflag=append count=1 seek=64 of=$comp_file
 	RC=$?
 
-	[ $RC -eq 0 ] || error "dd append failed"
+	(( $RC == 0 )) || error "dd append failed"
 
 	local flg_opts="--comp-start 64M -E 128M --comp-flags init"
 	local found=$($LFS find $flg_opts $comp_file | wc -l)
-	[ $found -eq 1 ] || error "Append: EXT component (64M-128M) not found"
+	(( $found == 1 )) || error "Append: EXT component (64M-128M) not found"
 
 	ost_idx=$($LFS getstripe -I2 -i $comp_file)
 	[ "$ost_idx" == "" ] && error "Append: EXT component doesn't exist"