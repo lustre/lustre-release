894398b260276825d7d37ee95cf2d3a7efac105f
Jian Yu <yujian@whamcloud.com>
2025-12-17 15:55:35 -0800

    LU-19430 tests: except sanity-pcc test 6, 7b, 35, 102 on el9.7+
    These tests are failing 100% on RHEL 9.7, 10.0 and 10.1 clients.
    Temporarily skip them until the issue is fixed.
    
    Test-Parameters: trivial clientdistro=el8.10 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=el9.6 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=el9.7 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=el10.0 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=el10.1 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=sles15sp7 testlist=sanity-pcc
    Test-Parameters: trivial clientdistro=ubuntu2404 testlist=sanity-pcc
    
    Change-Id: I3a2f2b918e32802d8788c068e656f0e1d4664847
    Signed-off-by: Jian Yu <yujian@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63097
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Qian Yingjin <qian@ddn.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/tests/sanity-pcc.sh
+++ lustre/tests/sanity-pcc.sh
@@ -56,7 +56,9 @@ fi
 
 if [[ -r /etc/redhat-release ]]; then
 	rhel_version=$(sed -e 's/[^0-9.]*//g' /etc/redhat-release)
-	if (( $(version_code $rhel_version) >= $(version_code 9.3.0) )); then
+	if (( $(version_code $rhel_version) >= $(version_code 9.7.0) )); then
+		always_except LU-19430 6 7b 35 102
+	elif (( $(version_code $rhel_version) >= $(version_code 9.3.0) )); then
 		always_except LU-17289 102          # fio io_uring
 		always_except LU-17781 33	    # inconsistent LSOM
 	elif (( $(version_code $rhel_version) >= $(version_code 8.9.0) )); then
