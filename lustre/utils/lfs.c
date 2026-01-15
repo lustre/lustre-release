lustre/lustre-release

commit 80a961261a23c5d0a7a16114e454b08de237a843

LU-17863 hsm: add ioctl to set data version in HSM xattr
When migrating files to a Lustre filesystem (e.g. from another Lustre
filesystem), certain HSM solutions might prefer to bypass the Lustre
HSM subsystem, i.e. not use use llapi_hsm_import(), the Lustre HSM
coordinator and a copytool and instead just copy files to the
filesystem, as this enables a simpler workflow.

Since there already exists another copy of some of the files, users
might want to set the HS_EXISTS and HS_ARCHIVED HSM flags and perform
an HSM release operation to free disk space; however, the latter
operation fails in mdt_hsm_release(), as there is a data version
mismatch between the data version that is sent from the client, which
has been calculated from the OSTs and the data version in the HSM
xattr, as the latter hasn't been set appropriately via
hsm_cdt_request_completed(), as the Lustre HSM subsystem was bypassed
and the relevant HSM flags were force-set via llapi_hsm_state_set().

Add an ioctl to set the data version field in the HSM xattr to a
specified value and enable "lfs data_version" to synchronize it, to
the data version that is calculated from the OST objects.

HPE-bug-id: LUS-12152
Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Change-Id: Ic8538aa884c675e8561cbc4e2b1d83d64e23a67a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55599
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -498,8 +498,9 @@ command_t cmdlist[] = {
 	 "usage: path2fid [--parents] <path> ..."},
 	{"rmfid", lfs_rmfid, 0, "Remove file(s) by FID(s)\n"
 	 "usage: rmfid <fsname|rootpath> <fid> ..."},
-	{"data_version", lfs_data_version, 0, "Display file data version for "
-	 "a given path.\n" "usage: data_version [-n|-r|-w] <path>"},
+	{"data_version", lfs_data_version, 0, "Display file data version or "
+	 "set the data version in the HSM xattr for a given path.\n"
+	"usage: data_version [-n|-r|-w|-s] <path>"},
 	{"hsm_state", lfs_hsm_state, 0, "Display the HSM information (states, "
 	 "undergoing actions) for given files.\n usage: hsm_state <file> ..."},
 	{"hsm_set", lfs_hsm_set, 0, "Set HSM user flag on specified files.\n"
@@ -10861,6 +10862,7 @@ static int lfs_data_version(int argc, char **argv)
 	int data_version_flags = LL_DV_RD_FLUSH; /* Read by default */
 	__u64 data_version;
 	char *path;
+	bool hsm_sync = false;
 	int fd;
 	int rc;
 	int c;
@@ -10871,7 +10873,7 @@ static int lfs_data_version(int argc, char **argv)
 		return CMD_HELP;
 	}
 
-	while ((c = getopt(argc, argv, "hnrw")) != -1) {
+	while ((c = getopt(argc, argv, "hnrws")) != -1) {
 		switch (c) {
 		case 'n':
 			data_version_flags = 0;
@@ -10881,6 +10883,9 @@ static int lfs_data_version(int argc, char **argv)
 			break;
 		case 'w':
 			data_version_flags |= LL_DV_WR_FLUSH;
+			break;
+		case 's':
+			hsm_sync = true;
 			break;
 		default:
 			fprintf(stderr,
@@ -10907,12 +10912,23 @@ static int lfs_data_version(int argc, char **argv)
 	}
 
 	rc = llapi_get_data_version(fd, &data_version, data_version_flags);
-	if (rc < 0)
+	if (rc < 0) {
 		fprintf(stderr,
 			"%s data_version: cannot get version for '%s': %s\n",
 			progname, path, strerror(-rc));
-	else
+	} else {
 		printf("%ju" "\n", (uintmax_t)data_version);
+
+		if (hsm_sync) {
+			rc = llapi_hsm_data_version_set(fd, data_version);
+			if (rc < 0)
+				fprintf(stderr,
+					"%s data_version: cannot set version %llu for"
+					" '%s': %s\n", progname,
+					(unsigned long long)data_version, path,
+					strerror(-rc));
+		}
+	}
 
 	close(fd);
 	return rc;

lustre/utils/liblustreapi_swap.c
@@ -91,6 +91,24 @@ int llapi_get_ost_layout_version(int fd, __u32 *layout_version)
 	return rc;
 }
 
+/**
+ * Set the data version in the HSM xattr on the MDT inode of a file to a
+ * specific value.
+ *
+ * \retval 0 on success.
+ * \retval -errno on error.
+ */
+int llapi_hsm_data_version_set(int fd, __u64 data_version)
+{
+	int rc;
+
+	rc = ioctl(fd, LL_IOC_HSM_DATA_VERSION, &data_version);
+	if (rc)
+		rc = -errno;
+
+	return rc;
+}
+
 /*
  * Create a file without any name and open it for read/write
  *

lustre/doc/lfs.1
@@ -9,7 +9,7 @@ lfs \- client utility for Lustre-specific file layout and other attributes
 .br
 .B lfs check \fR<\fBmgts\fR|\fBmdts\fR|\fBosts\fR|\fBall\fR> [\fIpath\fR]
 .br
-.B lfs data_version \fR[\fB-nrw\fR] \fB<\fIfilename\fR>
+.B lfs data_version \fR[\fB-nrws\fR] \fB<\fIfilename\fR>
 .br
 .B lfs df \fR[\fB-ihlv\fR] [\fB--pool\fR|\fB-p \fR<\fIfsname\fR>[.<\fIpool\fR>]] [\fIpath\fR]
 .br
@@ -155,14 +155,15 @@ Display the status of the MGTs, MDTs or OSTs (as specified in the command) or
 all the servers (MGTs, MDTs and OSTs). If \fBpath\fR is provided, display
 the status of the lustre file system mounted at specified \fBpath\fR only.
 .TP
-.B data_version [-nrw] <filename>
-Display the current version of file data. If -n is specified, the data version
-is read without taking a lock. As a consequence, the data version could be
-outdated if there are dirty caches on filesystem clients, but this option will
-not force data flushes and has less of an impact on the filesystem. If -r is
-specified, the data version is read after dirty pages on clients are flushed. If
--w is specified, the data version is read after all caching pages on clients are
-flushed.
+.B data_version [-nrws] <filename>
+Display the current version of file data and optionally set the data version
+stored in the HSM xattr. If -n is specified, the data version is read without
+taking a lock. As a consequence, the data version could be outdated if there are
+dirty caches on filesystem clients, but this option will not force data flushes
+and has less of an impact on the filesystem. If -r is specified, the data
+version is read after dirty pages on clients are flushed. If -w is specified,
+the data version is read after all caching pages on clients are flushed. If -s
+is specified, the data version that was read, is also written to the HSM xattr.
 
 Even with -r or -w, race conditions are possible and the data version should be
 checked before and after an operation to be confident the data did not change

lustre/include/lustre/lustreapi.h
@@ -564,6 +564,7 @@ int llapi_get_version(char *buffer, int buffer_size, char **version)
 int llapi_get_data_version(int fd, __u64 *data_version, __u64 flags);
 int llapi_file_flush(int fd);
 extern int llapi_get_ost_layout_version(int fd, __u32 *layout_version);
+int llapi_hsm_data_version_set(int fd, __u64 data_version);
 int llapi_hsm_state_get_fd(int fd, struct hsm_user_state *hus);
 int llapi_hsm_state_get(const char *path, struct hsm_user_state *hus);
 int llapi_hsm_state_set_fd(int fd, __u64 setmask, __u64 clearmask,

lustre/include/lustre_req_layout.h
@@ -258,6 +258,7 @@ extern struct req_format RQF_MDS_HSM_PROGRESS;
 extern struct req_format RQF_MDS_HSM_CT_REGISTER;
 extern struct req_format RQF_MDS_HSM_CT_UNREGISTER;
 extern struct req_format RQF_MDS_HSM_REQUEST;
+extern struct req_format RQF_MDS_HSM_DATA_VERSION;
 /* OST req_format */
 extern struct req_format RQF_OST_CONNECT;
 extern struct req_format RQF_OST_DISCONNECT;

lustre/include/obd_support.h
@@ -276,6 +276,7 @@ extern bool obd_enable_health_write;
 #define OBD_FAIL_MDS_HSM_RESTORE_RACE	 0x18b
 #define OBD_FAIL_MDS_CHANGELOG_ENOSPC	 0x18c
 #define OBD_FAIL_MDS_BATCH_NET		 0x18d
+#define OBD_FAIL_MDS_HSM_DATA_VERSION_NET 0x18e
 
 /* OI scrub */
 #define OBD_FAIL_OSD_SCRUB_DELAY			0x190

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1790,6 +1790,7 @@ enum mds_cmd {
 	MDS_SWAP_LAYOUTS	= 61,
 	MDS_RMFID		= 62,
 	MDS_BATCH		= 63,
+	MDS_HSM_DATA_VERSION	= 64,
 	MDS_LAST_OPC
 };
 

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -655,6 +655,7 @@ struct ll_ioc_lease_id {
 						struct lu_pcc_detach_fid)
 #define LL_IOC_PCC_STATE		_IOR('f', 252, struct lu_pcc_state)
 #define LL_IOC_PROJECT			_IOW('f', 253, struct lu_project)
+#define LL_IOC_HSM_DATA_VERSION		_IOW('f', 254, struct ioc_data_version)
 
 #ifndef	FS_IOC_FSGETXATTR
 /*

commit 1267210223cf328c06f1bb1ee9c788ac0c80e7fe

LU-12847 docs: add man pages for data_version handling
Split man page for the lfs-data_version(1) command from lfs(1).
Add a man page to describe the llapi_get_data_version(3) function.
Add long options to the "lfs data_version" command while updating
its man page, since the short option meaningss were not very clear.

Also add a man page for llapi_hsm_data_version_set(3), since it is
related and was missing its own man page.

Add a proper example for llapi_fsync(3), instead of referencing lfs.c,
since it is implemented via llapi_get_data_version() internally.

Test-Parameters: trivial
Fixes: 80a961261a ("LU-17863 hsm: add ioctl to set data version in HSM xattr")
Fixes: 61e3e2d698 ("LU-930 doc: update data_version command in lfs man page")
Fixes: de7fe0875b ("LU-827 lov: Implement a per file data_version.")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I1a34085396b5ab2e229c2244494d722f93aa481d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63150
Reviewed-by: Frederick Dilger <fdilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

Documentation/man1/lfs-data_version.1
@@ -1,0 +1,74 @@
+.TH LFS-DATA_VERSION 1 2025-12-22 Lustre "Lustre user utilities"
+.SH NAME
+lfs-data_version \- display or update the stored file data version
+.SH SYNOPSIS
+.SY "lfs data_version"
+.RB { --nosync | -n | --read | -r | --write | -w }
+.RB [ --set-hsm | -s ]
+.I FILENAME
+.YS
+.SH DESCRIPTION
+Display the current version of file data for
+.IR FILENAME ,
+and optionally set the data version stored in the HSM xattr.
+.PP
+The data version is the sum of the last committed transaction numbers
+of all data objects of a file for
+.BR write()
+(or similar data modification),
+.BR truncate ()
+(or similar size modification)
+RPC in which an object's file data was modified.  It does not track
+metadata operations on the file such as file permissions, ownership, etc.
+There is no guarantee that one data version is ordered compared to another,
+and should only be used for equality or inequality comparison for a single
+.I FILENAME
+to check if its data objects were changed since the last data_version read.
+.PP
+The data version is typically used by data management engines such as HSM,
+OST object migration, and mirroring,
+primarily to verify that file data has not been changed concurrently by
+another process during a data movement or release operation.
+.PP
+Even when a cache flush is triggered on clients accessing this file using
+.B -r
+or
+.BR -w ,
+race conditions are possible and the data version should be checked before
+and after data management operations to ensure the data did not change
+concurrently.
+.SH OPTIONS
+.TP
+.B -n ", " --nosync
+Read the data version without taking any data lock to sync file data.
+The data version could be stale if cached pages exist on filesystem clients,
+but this option avoids data flush and has less of an impact on the filesystem.
+.TP
+.B -r ", " --read-lock
+Read the data version after getting a read lock on
+.I FILENAME
+to flush its dirty client pages on all clients.
+.TP
+.B -s ", " --set-hsm
+Read the data version from
+.I FILENAME
+(by default using a read lock) and write it to the
+.B trusted.hsm
+xattr.
+.TP
+.B -w ", " --write-lock
+Read the data version after getting a write lock on
+.I FILENAME
+to flush all of its cached data pages from all clients.
+.SH AVAILABILITY
+The
+.B lfs data_version
+command is part of the
+.BR lustre (7)
+filesystem package since release 2.3.0.
+.\" added in commit v2_2_50_0-10-gde7fe0875b
+.SH SEE ALSO
+.BR lfs (1),
+.BR llapi_get_data_version (3),
+.BR llapi_hsm_data_version_set (3),
+.BR lustre (7)

Documentation/man1/lfs.1
@@ -35,28 +35,13 @@ the server status. NOTE:
 .B lfs osts
 may be preferable for some use cases.
 .TP
-.BR data_version " [" -nrw "] " \fIFILENAME
-Display the current version of file data and optionally set the data version
-stored in the HSM xattr. If -n is specified, the data version is read without
-taking a lock. As a consequence, the data version could be outdated if there are
-dirty caches on filesystem clients, but this option will not force data flushes
-and has less of an impact on the filesystem. If -r is specified, the data
-version is read after dirty pages on clients are flushed. If -w is specified,
-the data version is read after all caching pages on clients are flushed. If -s
-is specified, the data version that was read, is also written to the HSM xattr.
-
-Even with -r or -w, race conditions are possible and the data version should be
-checked before and after an operation to be confident the data did not change
-during it.
-.IP
-The data version is the sum of the last committed transaction numbers of all
-data objects of a file. It is used by HSM policy engines for verifying that file
-data has not been changed during an archive operation or before a release
-operation, and by OST migration, primarily for verifying that file data has not
-been changed during a data copy, when done in non-blocking mode.
+.BR data_version
+See
+.BR lfs-data_version (1).
 .TP
 .B flushctx
-See lfs-flushctx(1).
+See
+.BR lfs-flushctx (1).
 .TP
 .BR osts " [" PATH ]
 List all the OSTs for all mounted filesystems. If a
@@ -172,6 +157,7 @@ filesystem package since release 0.10.0
 .BR lctl (8),
 .BR lfs-changelog (1),
 .BR lfs-changelog_clear (1),
+.BR lfs-data_version (1),
 .BR lfs-df (1),
 .BR lfs-fid2path (1),
 .BR lfs-find (1),

Documentation/man3/llapi_fsync.3
@@ -1,4 +1,4 @@
-.TH LLAPI_FSYNC 3 2024-09-24 "Lustre User API" "Lustre Library Functions"
+.TH LLAPI_FSYNC 3 2025-12-22 "Lustre User API" "Lustre Library Functions"
 .SH NAME
 llapi_fsync \- flush dirty pages from all Lustre clients
 .SH SYNOPSIS
@@ -24,7 +24,18 @@ on success
 <0
 a negative errno value on failure
 .SH EXAMPLES
-An example can be found in lfs.c source file.
+Flush a file's data across all clients after a parallel IO operation:
+.EX
+        int fd;
+        int rc;
+\&
+        /* write file concurrently from many clients */
+        rc = llapi_fsync(fd);
+        if (rc == 0)
+                /* success, all client data persistent for this file */
+        else
+                /* error, some problem was seen during data flush */
+.EE
 .SH AVAILABILITY
 The
 .B llapi_fsync
@@ -33,5 +44,6 @@ API is part of the
 user application interface library since release 2.16.0.
 .\" Added in commit v2_15_91-1-g425b9f75225d
 .SH SEE ALSO
+.BR llapi_get_data_version (3),
 .BR lustre (7),
 .BR lustreapi (7)

Documentation/man3/llapi_get_data_version.3
@@ -1,0 +1,105 @@
+.TH LLAPI_GET_DATA_VERSION 3 2025-12-22 "Lustre User API" "Lustre Library Functions"
+.SH NAME
+llapi_get_data_version \- fetch OST data version for a file
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_get_data_version(int " fd ", __u64 *" data_version ", __u64 " flags );
+.YS
+.fi
+.SH DESCRIPTION
+Fetch the combined data version for all OST objects associated with the
+open file handle.  
+.PP
+The data version is the sum of the last committed transaction numbers
+of all data objects of a file for
+.BR write()
+(or similar data modification),
+.BR truncate ()
+(or similar size modification)
+RPC in which an object's file data was modified.  It does not track
+metadata operations on the file such as file permissions, ownership, etc.
+There is no guarantee that one data version is ordered compared to another,
+and should only be used for (in)equality comparison for a single file
+to check if its data objects were changed since the last data_version read.
+.PP
+The data version is typically used by data management engines such as HSM,
+OST object migration, and mirroring,
+primarily to verify that file data has not been changed concurrently by
+another process during a data movement or release operation.
+.PP
+Even when a cache flush is triggered on clients accessing this file,
+race conditions are possible and the data version should be checked before and
+after data management operations to ensure the data did not change concurrently.
+This allows data management operations to avoid blocking application file IO,
+while maintaining data consistency during data copy or migration.
+.TP
+.I fd
+open file descriptor
+.TP
+.I data_version
+pointer to 64-bit integer to store the file version.
+.TP
+.I flags
+flags that affect whether cached file data on clients is flushed before
+fetching the data version.  Flags may have the following values:
+.TP
+.B LL_DV_RD_FLUSH
+OSTs will take a read lock to flush dirty pages of
+.I fd
+from clients.
+.TP
+.B LL_DV_WR_FLUSH
+OSTs will take a write lock to flush all cache pages of
+.I fd
+from clients.
+.TP
+.B LL_DV_SZ_UPDATE
+Update the file size on the client
+.SH RETURN VALUES
+.BR llapi_get_data_version ()
+returns:
+.TP
+0
+on success and
+.I data_version
+contains the file's object data version,
+or zero if no objects are initialized for this file.
+.TP
+<0
+a negative errno value on failure
+.SH EXAMPLES
+.EX
+	int fd_src;
+	__u64 dv1, dv2;
+\&
+again:
+	fd_src = open(filename, O_RDONLY);
+	rc = llapi_get_data_version(fd_src, &dv1, 0);
+	/* perform some data copy operation */
+	rc = llapi_get_data_version(fd_src, &dv2, LL_DV_RD_FLUSH);
+        close(fd_src);
+
+	if (dv1 != dv2) {
+		rc = -EAGAIN;
+		fprintf(stderr, "%s: %s changed during copy, retrying\n",
+                        progname, filename);
+		goto again;
+	}
+.EE
+.SH AVAILABILITY
+The
+.BR llapi_get_data_version ()
+API is part of the
+.BR lustre (7)
+user application interface library since release 2.3.0.
+.\" Added in commit v2_2_50_0-10-gde7fe0875b
+.SH SEE ALSO
+.BR lfs-data_version (1),
+.BR lfs-migrate (1),
+.BR llapi_fswap_layouts (3),
+.BR llapi_fsync (3),
+.BR llapi_get_data_version (3),
+.BR lustre (7),
+.BR lustreapi (7)

Documentation/man3/llapi_hsm_data_version_set.3
@@ -1,0 +1,63 @@
+.TH LLAPI_HSM_DATA_VERSION_SET 3 2025-12-22 "Lustre User API" "Lustre Library Functions"
+.SH NAME
+llapi_hsm_data_version_set \- set OST data version to HSM xattr
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_hsm_data_version_set(int " fd ", __u64 " data_version );
+.YS
+.fi
+.SH DESCRIPTION
+Update the file's data version and store it in the
+.B trusted.hsm
+xattr on the file after doing some file data migration operation
+that does not affect the data contents.
+.PP
+The data version is typically fetched via
+.BR llapi_get_data_version (3),
+which should be referenced for details about data version.
+.TP
+.I fd
+open file descriptor
+.TP
+.I data_version
+64-bit integer that contains the file data version.
+.SH RETURN VALUES
+.BR llapi_hsm_data_version_set ()
+returns:
+.TP
+0
+on success
+.TP
+<0
+a negative errno value on failure
+.SH EXAMPLES
+.EX
+	int fd_src;
+	__u64 dv1, dv2;
+\&
+	fd_src = open(filename, O_RDONLY);
+	rc = llapi_get_data_version(fd_src, &dv1, 0);
+	/* manage the file data - migrate, mirror, etc. */
+	rc = llapi_get_data_version(fd_src, &dv2, LL_DV_RD_FLUSH);
+        close(fd_src);
+
+	if (dv1 == dv2)
+		llapi_hsm_data_version_set(fd_src, dv2);
+.EE
+.SH AVAILABILITY
+The
+.BR llapi_hsm_data_version_set ()
+API is part of the
+.BR lustre (7)
+user application interface library since release 2.16.0.
+.\" Added in commit v2_15_90-10-g80a961261a23
+.SH SEE ALSO
+.BR lfs-data_version (1),
+.BR lfs-migrate (1),
+.BR llapi_fswap_layouts (3),
+.BR llapi_fsync (3),
+.BR llapi_hsm_data_version_set (3),
+.BR lustre (7),
+.BR lustreapi (7)

Documentation/man7/lustreapi.7
@@ -33,6 +33,7 @@ filesystem package since release 0.10.0
 .BR llapi_file_create (3),
 .BR llapi_file_get_stripe (3),
 .BR llapi_file_open (3),
+.BR llapi_get_data_version (3),
 .BR llapi_group_lock (3),
 .BR llapi_group_unlock (3),
 .BR llapi_heat_get (3),
@@ -46,6 +47,7 @@ filesystem package since release 0.10.0
 .BR llapi_hsm_copytool_recv (3),
 .BR llapi_hsm_copytool_register (3),
 .BR llapi_hsm_copytool_unregister (3),
+.BR llapi_hsm_data_version_set (3),
 .BR llapi_hsm_state_get (3),
 .BR llapi_hsm_state_set (3),
 .BR llapi_ladvise (3),

lustre/utils/lfs.c
@@ -577,10 +577,9 @@ command_t cmdlist[] = {
 	 "usage: path2fid [--parents] <path> ..."},
 	{"rmfid", lfs_rmfid, 0, "Remove file(s) by FID(s)\n"
 	 "usage: rmfid <fsname|rootpath> <fid> ..."},
-	{"data_version", lfs_data_version, 0, "Display file data version or "
-	 "set the data version in the HSM xattr for a given path.\n"
-	"usage: data_version [-n|-r|-w|-s] <path>"},
-
+	{"data_version", lfs_data_version, 0, "Display file data version, "
+	 "and optionally set the data version in the HSM xattr for FILENAME.\n"
+	"usage: data_version {--nosync|--read|--write} [--set-hsm] FILENAME"},
 	{"hsm_state", lfs_hsm_state, 0,
 	 "Display the HSM information for given files.\n"
 	 "usage: hsm_state FILE"},
@@ -12157,6 +12156,13 @@ out_close:
 
 static int lfs_data_version(int argc, char **argv)
 {
+	struct option long_opts[] = {
+	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
+	{ .val = 'n',	.name = "nosync",	.has_arg = no_argument },
+	{ .val = 'r',	.name = "read-lock",	.has_arg = no_argument },
+	{ .val = 's',	.name = "set-hsm",	.has_arg = no_argument },
+	{ .val = 'w',	.name = "write-lock",	.has_arg = no_argument },
+	{ .name = NULL } };
 	int data_version_flags = LL_DV_RD_FLUSH; /* Read by default */
 	__u64 data_version;
 	char *path;
@@ -12165,13 +12171,7 @@ static int lfs_data_version(int argc, char **argv)
 	int rc;
 	int c;
 
-	if (argc < 2) {
-		fprintf(stderr, "%s: FILE must be specified\n",
-			progname);
-		return CMD_HELP;
-	}
-
-	while ((c = getopt(argc, argv, "hnrws")) != -1) {
+	while ((c = getopt_long(argc, argv, "hnrsw", long_opts, NULL)) != -1) {
 		switch (c) {
 		case 'n':
 			data_version_flags = 0;
@@ -12179,11 +12179,11 @@ static int lfs_data_version(int argc, char **argv)
 		case 'r':
 			data_version_flags |= LL_DV_RD_FLUSH;
 			break;
-		case 'w':
-			data_version_flags |= LL_DV_WR_FLUSH;
-			break;
 		case 's':
 			hsm_sync = true;
+			break;
+		case 'w':
+			data_version_flags |= LL_DV_WR_FLUSH;
 			break;
 		default:
 			fprintf(stderr,
@@ -12195,7 +12195,7 @@ static int lfs_data_version(int argc, char **argv)
 		}
 	}
 	if (optind == argc) {
-		fprintf(stderr, "%s data_version: FILE must be specified\n",
+		fprintf(stderr, "%s data_version: FILENAME must be specified\n",
 			progname);
 		return CMD_HELP;
 	}

lustre/utils/liblustreapi_swap.c
@@ -43,8 +43,11 @@
  */
 int llapi_get_data_version(int fd, __u64 *data_version, __u64 flags)
 {
-	int rc;
 	struct ioc_data_version idv;
+	int rc;
+
+	if (!data_version)
+		return -EFAULT;
 
 	idv.idv_flags = (__u32)flags;
 