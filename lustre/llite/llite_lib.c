lustre/lustre-release

commit e13f17ce3a6f816fa3498c3d89d629444b21824b

LU-18448 llite: read dir on open fixes
add an special test for read dir on open,
to indicate this feature works.
lets enable it by default and increase a buffer size.
Fix 64k page size interoperability.

HPE-bug-id: LUS-7695
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: Ib019b15cfa1fba9fc1edf4daac5297cb779ab34c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61641
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -547,6 +547,7 @@ retry_connect:
 	sb->s_magic = LL_SUPER_MAGIC;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sbi->ll_inode_cache_enabled = 1;
+	sbi->ll_dir_open_read = 1;
 	sbi->ll_namelen = min_t(u32, osfs->os_namelen, NAME_MAX);
 	sbi->ll_mnt.mnt = current->fs->root.mnt;
 	sbi->ll_mnt_ns = current->nsproxy->mnt_ns;

lustre/include/obd.h
@@ -1392,6 +1392,10 @@ struct md_ops {
 			     bool wait);
 	int (*m_batch_add)(struct obd_export *exp, struct lu_batch *bh,
 			   struct md_op_item *item);
+	int (*m_dirpage_add)(struct obd_export *exp, struct inode *inode,
+			     struct page **pool,
+			     unsigned int cfs_pgs, unsigned int lu_pgs,
+			     int is_hash64);
 };
 
 static inline struct md_open_data *obd_mod_alloc(void)

lustre/include/obd_class.h
@@ -1975,6 +1975,24 @@ static inline int md_batch_add(struct obd_export *exp, struct lu_batch *bh,
 	return exp->exp_obd->obd_type->typ_md_ops->m_batch_add(exp, bh, item);
 }
 
+static inline int md_dirpage_add(struct obd_export *exp,
+				 struct inode *inode,
+				 struct page **pool,
+				 unsigned int cfs_pgs,
+				 unsigned int lu_pgs, int is_hash64)
+{
+	int rc;
+	const struct md_ops	*md_ops;
+
+	rc = exp_check_ops(exp);
+	if (rc)
+		return rc;
+
+	md_ops = exp->exp_obd->obd_type->typ_md_ops;
+	return md_ops->m_dirpage_add(exp, inode, pool, cfs_pgs,
+				     lu_pgs, is_hash64);
+}
+
 /* OBD Metadata Support */
 
 extern int obd_init_caches(void);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -926,7 +926,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_DMV_IMP_INHERIT |\
 				OBD_CONNECT2_UNALIGNED_DIO | \
 				OBD_CONNECT2_PCCRO | \
-				OBD_CONNECT2_MIRROR_ID_FIX)
+				OBD_CONNECT2_MIRROR_ID_FIX |\
+				OBD_CONNECT2_READDIR_OPEN)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_VERSION | OBD_CONNECT_INDEX | \

lustre/llite/file.c
@@ -641,18 +641,24 @@ out_io:
 
 	EXIT;
 }
+
 void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
 {
 	struct obd_export *exp = ll_i2mdexp(inode);
-	void *data;
+	char *data;
+	struct page **page_pool;
 	struct page *page;
-	struct lu_dirpage *dp;
-	int is_hash64;
-	int rc;
-	unsigned long	offset;
-	__u64		hash;
 	unsigned int i;
+	unsigned int rep_size;
 	unsigned int npages;
+	unsigned int rd_pgs;
+	unsigned int lu_pgs;
+	int 		is_hash64;
+	struct lu_dirpage *dp;
+	int 		rc;
+	unsigned long   offset;
+	__u64		hash;
+	gfp_t gfp;
 
 	ENTRY;
 
@@ -667,38 +673,56 @@ void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
 	if (data == NULL)
 		RETURN_EXIT;
 
-	npages = req_capsule_get_size(&req->rq_pill, &RMF_NIOBUF_INLINE,
-				      RCL_SERVER);
-	if (npages < sizeof(*dp))
+	rep_size = req_capsule_get_size(&req->rq_pill, &RMF_NIOBUF_INLINE,
+					RCL_SERVER);
+	if (rep_size < sizeof(struct lu_dirpage))
 		RETURN_EXIT;
 
-	/* div rou*/
-	npages = DIV_ROUND_UP(npages, PAGE_SIZE);
+	npages = (rep_size + PAGE_SIZE - 1) >> PAGE_SHIFT;
+	lu_pgs = rep_size >> LU_PAGE_SHIFT;
+
 	is_hash64 = test_bit(LL_SBI_64BIT_HASH, ll_i2sbi(inode)->ll_flags);
+	gfp = mapping_gfp_mask(inode->i_mapping);
 
-	for (i = 0; i < npages; i++) {
-		page = __page_cache_alloc(mapping_gfp_mask(inode->i_mapping));
-		if (!page)
-			continue;
+	OBD_ALLOC_PTR_ARRAY(page_pool, npages);
+	if (page_pool == NULL)
+		RETURN_EXIT;
 
-		lock_page(page);
-		SetPageUptodate(page);
+	for (rd_pgs = 0; rd_pgs < npages; rd_pgs++) {
+		page = __page_cache_alloc(gfp);
+		if (page == NULL)
+			break;
+		page_pool[rd_pgs] = page;
 
 		dp = kmap_local_page(page);
+		CDEBUG(D_INFO, "page %p - %p - %p -> %llu %llu\n", page, dp, data, dp->ldp_hash_start, dp->ldp_hash_end);
 		memcpy(dp, data, PAGE_SIZE);
-		hash = le64_to_cpu(dp->ldp_hash_start);
 		kunmap_local(dp);
 
-		offset = hash_x_index(hash, is_hash64);
-
-		prefetchw(&page->flags);
-		rc = add_to_page_cache_lru(page, inode->i_mapping, offset,
-				   GFP_KERNEL);
-		if (rc == 0)
-			unlock_page(page);
-
-		put_page(page);
+		data += PAGE_SIZE;
 	}
+	if (rd_pgs == 0)
+		goto exit;
+
+	page = page_pool[0];
+	dp = kmap_local_page(page);
+	hash = le64_to_cpu(dp->ldp_hash_start);
+	kunmap_local(dp);
+
+	offset = hash_x_index(hash, is_hash64);
+
+	prefetchw(&page->flags);
+	rc = add_to_page_cache_lru(page, inode->i_mapping, offset, GFP_KERNEL);
+	if (rc == 0)
+		md_dirpage_add(exp, inode, page_pool, rd_pgs, lu_pgs, is_hash64);
+exit:
+	if (rc < 0) {
+		/* release extra pages */
+		for (i = 0; i < rd_pgs; i++)
+			put_page(page_pool[i]);
+	}
+	OBD_FREE_PTR_ARRAY(page_pool, npages);
+
 	EXIT;
 }
 

lustre/lmv/lmv_obd.c
@@ -4483,6 +4483,25 @@ static int lmv_batch_add(struct obd_export *exp, struct lu_batch *bh,
 	RETURN(rc);
 }
 
+static int lmv_dirpage_add(struct obd_export *exp,
+			   struct inode *inode,
+			   struct page **pool,
+			   unsigned int cfs_pgs,
+			   unsigned int lu_pgs, int is_hash64)
+{
+	struct lmv_obd *lmv = &exp->exp_obd->u.lmv;
+	struct lmv_tgt_desc *tgt = lmv_tgt(lmv, 0);
+	int rc;
+
+	ENTRY;
+
+	if (tgt == NULL || tgt->ltd_exp == NULL)
+		RETURN(-EINVAL);
+	rc =  md_dirpage_add(tgt->ltd_exp,
+			     inode, pool, cfs_pgs, lu_pgs, is_hash64);
+	RETURN(rc);
+}
+
 static const struct obd_ops lmv_obd_ops = {
 	.o_owner                = THIS_MODULE,
 	.o_connect              = lmv_connect,
@@ -4534,6 +4553,7 @@ static const struct md_ops lmv_md_ops = {
 	.m_batch_add		= lmv_batch_add,
 	.m_batch_stop		= lmv_batch_stop,
 	.m_batch_flush		= lmv_batch_flush,
+	.m_dirpage_add		= lmv_dirpage_add,
 };
 
 static const struct lu_device_operations lmv_lu_ops = {

lustre/mdc/mdc_request.c
@@ -1249,6 +1249,7 @@ static struct page *mdc_page_locate(struct address_space *mapping, __u64 *hash,
 static void mdc_adjust_dirpages(struct page **pages, int cfs_pgs, int lu_pgs)
 {
 	int i;
+	ENTRY;
 
 	for (i = 0; i < cfs_pgs; i++) {
 		void *addr = kmap_local_page(pages[i]);
@@ -1301,6 +1302,54 @@ static void mdc_adjust_dirpages(struct page **pages, int cfs_pgs, int lu_pgs)
 #define mdc_adjust_dirpages(pages, cfs_pgs, lu_pgs) do {} while (0)
 #endif	/* PAGE_SIZE > LU_PAGE_SIZE */
 
+static int mdc_dirpage_add(struct obd_export *exp,
+			   struct inode *inode,
+			   struct page **page_pool,
+			   unsigned int rd_pgs,
+			   unsigned int lu_pgs, int is_hash64)
+{
+	int i;
+	ENTRY;
+
+	mdc_adjust_dirpages(page_pool, rd_pgs, lu_pgs);
+
+	SetPageUptodate(page_pool[0]);
+	unlock_page(page_pool[0]);
+
+	CDEBUG(D_CACHE, "read %u/%u\n", rd_pgs, lu_pgs);
+
+	for (i = 1; i < rd_pgs; i++) {
+		unsigned long	offset;
+		__u64		hash;
+		int ret;
+		struct lu_dirpage *dp;
+		struct page *page;
+
+		page = page_pool[i];
+
+		SetPageUptodate(page);
+
+		dp = kmap_local_page(page);
+		hash = le64_to_cpu(dp->ldp_hash_start);
+		kunmap_local(dp);
+
+		offset = hash_x_index(hash, is_hash64);
+
+		prefetchw(&page->flags);
+		ret = add_to_page_cache_lru(page, inode->i_mapping, offset,
+					    GFP_KERNEL);
+		if (ret == 0)
+			unlock_page(page);
+		else
+			CDEBUG(D_VFSTRACE,
+			       "page %lu add to page cache failed: rc = %d\n",
+			       offset, ret);
+		put_page(page);
+	}
+
+	return 0;
+}
+
 /* parameters for readdir page */
 struct readpage_param {
 	struct md_op_data	*rp_mod;
@@ -1324,13 +1373,13 @@ static int ll_mdc_read_page_remote(void *data, struct page *page0)
 	struct readpage_param *rp = data;
 	struct page **page_pool;
 	struct page *page;
-	struct lu_dirpage *dp;
 	struct md_op_data *op_data = rp->rp_mod;
 	struct ptlrpc_request *req;
 	int max_pages;
 	struct inode *inode;
 	struct lu_fid *fid;
 	int rd_pgs = 0; /* number of pages actually read */
+	int lu_pgs = 0;
 	int npages;
 	int i;
 	int rc;
@@ -1343,7 +1392,7 @@ static int ll_mdc_read_page_remote(void *data, struct page *page0)
 	LASSERT(inode != NULL);
 	gfp = mapping_gfp_mask(inode->i_mapping);
 
-	OBD_ALLOC_PTR_ARRAY_LARGE(page_pool, max_pages);
+	OBD_ALLOC_PTR_ARRAY(page_pool, max_pages);
 	if (page_pool != NULL) {
 		page_pool[0] = page0;
 	} else {
@@ -1363,58 +1412,29 @@ static int ll_mdc_read_page_remote(void *data, struct page *page0)
 	if (rc < 0) {
 		/* page0 is special, which was added into page cache early */
 		cfs_delete_from_page_cache(page0);
-	} else {
-		int lu_pgs;
-
-		rd_pgs = (req->rq_bulk->bd_nob_transferred + PAGE_SIZE - 1) >>
-			PAGE_SHIFT;
-		lu_pgs = req->rq_bulk->bd_nob_transferred >> LU_PAGE_SHIFT;
-		LASSERT(!(req->rq_bulk->bd_nob_transferred & ~LU_PAGE_MASK));
-
-		CDEBUG(D_INODE, "read %d(%d) pages\n", rd_pgs, lu_pgs);
-
-		mdc_adjust_dirpages(page_pool, rd_pgs, lu_pgs);
-
-		SetPageUptodate(page0);
+		unlock_page(page0);
+		goto exit;
 	}
-	unlock_page(page0);
 
+	rd_pgs = (req->rq_bulk->bd_nob_transferred + PAGE_SIZE - 1) >> PAGE_SHIFT;
+	lu_pgs = req->rq_bulk->bd_nob_transferred >> LU_PAGE_SHIFT;
+	LASSERT(!(req->rq_bulk->bd_nob_transferred & ~LU_PAGE_MASK));
 	ptlrpc_req_put(req);
-	CDEBUG(D_CACHE, "read %d/%d pages\n", rd_pgs, npages);
-	for (i = 1; i < npages; i++) {
-		unsigned long	offset;
-		__u64		hash;
-		int ret;
 
+	mdc_dirpage_add(NULL, inode, page_pool, rd_pgs, lu_pgs, rp->rp_hash64);
+exit:
+	/* release extra pages */
+	for (i = 1; i < npages; i++) {
 		page = page_pool[i];
 
 		if (rc < 0 || i >= rd_pgs) {
 			put_page(page);
 			continue;
 		}
-
-		SetPageUptodate(page);
-
-		dp = kmap_local_page(page);
-		hash = le64_to_cpu(dp->ldp_hash_start);
-		kunmap_local(dp);
-
-		offset = hash_x_index(hash, rp->rp_hash64);
-
-		prefetchw(&page->flags);
-		ret = add_to_page_cache_lru(page, inode->i_mapping, offset,
-					    GFP_KERNEL);
-		if (ret == 0)
-			unlock_page(page);
-		else
-			CDEBUG(D_VFSTRACE,
-			       "page %lu add to page cache failed: rc = %d\n",
-			       offset, ret);
-		put_page(page);
 	}
 
 	if (page_pool != &page0)
-		OBD_FREE_PTR_ARRAY_LARGE(page_pool, max_pages);
+		OBD_FREE_PTR_ARRAY(page_pool, max_pages);
 
 	RETURN(rc);
 }
@@ -3127,6 +3147,7 @@ static const struct md_ops mdc_md_ops = {
 	.m_batch_stop		= cli_batch_stop,
 	.m_batch_flush		= cli_batch_flush,
 	.m_batch_add		= mdc_batch_add,
+	.m_dirpage_add		= mdc_dirpage_add,
 };
 
 dev_t mdc_changelog_dev;

lustre/mdt/mdt_handler.c
@@ -3082,8 +3082,6 @@ int mdt_object_striped(struct mdt_thread_info *mti, struct mdt_object *obj)
 	return (rc > 0) ? 1 : (rc == -ENODATA) ? 0 : rc;
 }
 
-#define DIR_READ_ON_OPEN_PAGES 1
-
 static int mdt_dir_read_on_open(struct mdt_thread_info	*info,
 				struct lustre_handle *lhc)
 {
@@ -3123,15 +3121,13 @@ static int mdt_dir_read_on_open(struct mdt_thread_info	*info,
 	rdpg->rp_attrs = LUDA_FID | LUDA_TYPE;
 	if (exp_connect_flags(info->mti_exp) & OBD_CONNECT_64BITHASH)
 		rdpg->rp_attrs |= LUDA_64BITHASH;
-	rdpg->rp_count  = min_t(unsigned int, req->rq_reqmsg->lm_repsize,
-			    DIR_READ_ON_OPEN_PAGES << PAGE_SHIFT);
+	rdpg->rp_count  = req->rq_reqmsg->lm_repsize;
 	rdpg->rp_npages = 0;
 
 	rc = req_capsule_server_grow(pill, &RMF_NIOBUF_INLINE, rdpg->rp_count);
-	if (rc != 0) {
+	if (rc != 0)
 		/* failed to grow data buffer, just exit */
 		GOTO(out_err, rc = -E2BIG);
-	}
 
 	/* re-take MDT_BODY and NIOBUF_INLINE buffers after the buffer grow */
 	mbo = req_capsule_server_get(pill, &RMF_MDT_BODY);
@@ -3160,6 +3156,8 @@ out_put:
 out_rnb:
 	if (rc < 0)
 		req_capsule_shrink(pill, &RMF_NIOBUF_INLINE, 0, RCL_SERVER);
+	else
+		req_capsule_shrink(pill, &RMF_NIOBUF_INLINE, rc, RCL_SERVER);
 out_err:
 	if (rc)
 		CDEBUG(D_INFO, "read dir on open failed with rc = %d\n", rc);

commit 811c1fd0e13429ec47d0082e45cdccf1619ba0e9

LU-18448 llite: disable dir read on open by default
Disable the new dir read on open feature by default, until test
failures related to this feature on aarch64 are fixed.

Test-Parameters: trivial testlist=sanity clientarch=aarch64
Test-Parameters: trivial testlist=sanity clientarch=aarch64
Test-Parameters: trivial testlist=sanity clientarch=aarch64
Test-Parameters: trivial testlist=sanity clientarch=aarch64
Test-Parameters: trivial testlist=sanity clientarch=aarch64
Test-Parameters: trivial testlist=sanity clientarch=aarch64
Fixes: e13f17ce3a ("LU-18448 llite: read dir on open fixes")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: Ia012f02695d7ae2694bda5c60019e76c9e040a4a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63451
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_lib.c
@@ -547,7 +547,7 @@ retry_connect:
 	sb->s_magic = LL_SUPER_MAGIC;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sbi->ll_inode_cache_enabled = 1;
-	sbi->ll_dir_open_read = 1;
+	sbi->ll_dir_open_read = 0;
 	sbi->ll_namelen = min_t(u32, osfs->os_namelen, NAME_MAX);
 	sbi->ll_mnt.mnt = current->fs->root.mnt;
 	sbi->ll_mnt_ns = current->nsproxy->mnt_ns;

lustre/llite/lproc_llite.c
@@ -2250,10 +2250,7 @@ static ssize_t dir_read_on_open_store(struct kobject *kobj,
 	if (rc)
 		return rc;
 
-	if (val)
-		sbi->ll_dir_open_read = 1;
-	else
-		sbi->ll_dir_open_read = 0;
+	sbi->ll_dir_open_read = val;
 
 	return count;
 }