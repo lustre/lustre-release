lustre/lustre-release

commit 0c45e49457a3f61ca661f4f7b0ad749cceaf7709

LU-12945 lnet: Disable zero copy when running on VM
When running on a hypervisor platform zero copy buffers
may still be referenced when write queue size is zero

So when running on a hypervisor push the zero copy size limit
above max payload size of 16M.

Use the hypervisor test added to linux v4.14-119-g79cc74155218
and provide a replacement for earlier kernels.

kernel-commit: 79cc74155218316b9a5d28577c7077b2adba8e58

Cray-bug-id: LUS-8072
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I5582a6aa8da6f48deafaf13d60cf67a09d7a7231
Reviewed-on: https://review.whamcloud.com/37300
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/socklnd/socklnd_modparams.c
@@ -21,6 +21,11 @@
 
 #include "socklnd.h"
 
+#include <linux/kvm_host.h>
+#if defined(__x86_64__) || defined(__i386__)
+#include <asm/hypervisor.h>
+#endif
+
 static int sock_timeout = 50;
 module_param(sock_timeout, int, 0644);
 MODULE_PARM_DESC(sock_timeout, "dead socket timeout (seconds)");
@@ -153,39 +158,49 @@ module_param(protocol, int, 0644);
 MODULE_PARM_DESC(protocol, "protocol version");
 #endif
 
+static inline bool is_native_host(void)
+{
+#ifdef HAVE_HYPERVISOR_IS_TYPE
+	return hypervisor_is_type(X86_HYPER_NATIVE);
+#elif defined(__x86_64__) || defined(__i386__)
+	return x86_hyper == NULL;
+#else
+	return true;
+#endif
+}
+
 struct ksock_tunables ksocknal_tunables;
 
 int ksocknal_tunables_init(void)
 {
-
-        /* initialize ksocknal_tunables structure */
-        ksocknal_tunables.ksnd_timeout            = &sock_timeout;
+	/* initialize ksocknal_tunables structure */
+	ksocknal_tunables.ksnd_timeout            = &sock_timeout;
 	ksocknal_tunables.ksnd_nscheds		  = &nscheds;
-        ksocknal_tunables.ksnd_nconnds            = &nconnds;
-        ksocknal_tunables.ksnd_nconnds_max        = &nconnds_max;
-        ksocknal_tunables.ksnd_min_reconnectms    = &min_reconnectms;
-        ksocknal_tunables.ksnd_max_reconnectms    = &max_reconnectms;
-        ksocknal_tunables.ksnd_eager_ack          = &eager_ack;
-        ksocknal_tunables.ksnd_typed_conns        = &typed_conns;
-        ksocknal_tunables.ksnd_min_bulk           = &min_bulk;
-        ksocknal_tunables.ksnd_tx_buffer_size     = &tx_buffer_size;
-        ksocknal_tunables.ksnd_rx_buffer_size     = &rx_buffer_size;
-        ksocknal_tunables.ksnd_nagle              = &nagle;
-        ksocknal_tunables.ksnd_round_robin        = &round_robin;
-        ksocknal_tunables.ksnd_keepalive          = &keepalive;
-        ksocknal_tunables.ksnd_keepalive_idle     = &keepalive_idle;
-        ksocknal_tunables.ksnd_keepalive_count    = &keepalive_count;
-        ksocknal_tunables.ksnd_keepalive_intvl    = &keepalive_intvl;
-        ksocknal_tunables.ksnd_credits            = &credits;
-        ksocknal_tunables.ksnd_peertxcredits      = &peer_credits;
-        ksocknal_tunables.ksnd_peerrtrcredits     = &peer_buffer_credits;
-        ksocknal_tunables.ksnd_peertimeout        = &peer_timeout;
-        ksocknal_tunables.ksnd_enable_csum        = &enable_csum;
-        ksocknal_tunables.ksnd_inject_csum_error  = &inject_csum_error;
-        ksocknal_tunables.ksnd_nonblk_zcack       = &nonblk_zcack;
-        ksocknal_tunables.ksnd_zc_min_payload     = &zc_min_payload;
-        ksocknal_tunables.ksnd_zc_recv            = &zc_recv;
-        ksocknal_tunables.ksnd_zc_recv_min_nfrags = &zc_recv_min_nfrags;
+	ksocknal_tunables.ksnd_nconnds            = &nconnds;
+	ksocknal_tunables.ksnd_nconnds_max        = &nconnds_max;
+	ksocknal_tunables.ksnd_min_reconnectms    = &min_reconnectms;
+	ksocknal_tunables.ksnd_max_reconnectms    = &max_reconnectms;
+	ksocknal_tunables.ksnd_eager_ack          = &eager_ack;
+	ksocknal_tunables.ksnd_typed_conns        = &typed_conns;
+	ksocknal_tunables.ksnd_min_bulk           = &min_bulk;
+	ksocknal_tunables.ksnd_tx_buffer_size     = &tx_buffer_size;
+	ksocknal_tunables.ksnd_rx_buffer_size     = &rx_buffer_size;
+	ksocknal_tunables.ksnd_nagle              = &nagle;
+	ksocknal_tunables.ksnd_round_robin        = &round_robin;
+	ksocknal_tunables.ksnd_keepalive          = &keepalive;
+	ksocknal_tunables.ksnd_keepalive_idle     = &keepalive_idle;
+	ksocknal_tunables.ksnd_keepalive_count    = &keepalive_count;
+	ksocknal_tunables.ksnd_keepalive_intvl    = &keepalive_intvl;
+	ksocknal_tunables.ksnd_credits            = &credits;
+	ksocknal_tunables.ksnd_peertxcredits      = &peer_credits;
+	ksocknal_tunables.ksnd_peerrtrcredits     = &peer_buffer_credits;
+	ksocknal_tunables.ksnd_peertimeout        = &peer_timeout;
+	ksocknal_tunables.ksnd_enable_csum        = &enable_csum;
+	ksocknal_tunables.ksnd_inject_csum_error  = &inject_csum_error;
+	ksocknal_tunables.ksnd_nonblk_zcack       = &nonblk_zcack;
+	ksocknal_tunables.ksnd_zc_min_payload     = &zc_min_payload;
+	ksocknal_tunables.ksnd_zc_recv            = &zc_recv;
+	ksocknal_tunables.ksnd_zc_recv_min_nfrags = &zc_recv_min_nfrags;
 
 	if (enable_irq_affinity) {
 		CWARN("irq_affinity is removed from socklnd because modern "
@@ -193,19 +208,25 @@ int ksocknal_tunables_init(void)
 		      "# NICs, although you still can set irq_affinity by "
 		      "another way, please check manual for details.\n");
 	}
-        ksocknal_tunables.ksnd_irq_affinity       = &enable_irq_affinity;
+	ksocknal_tunables.ksnd_irq_affinity       = &enable_irq_affinity;
 
 #ifdef SOCKNAL_BACKOFF
-        ksocknal_tunables.ksnd_backoff_init       = &backoff_init;
-        ksocknal_tunables.ksnd_backoff_max        = &backoff_max;
+	ksocknal_tunables.ksnd_backoff_init       = &backoff_init;
+	ksocknal_tunables.ksnd_backoff_max        = &backoff_max;
 #endif
 
 #if SOCKNAL_VERSION_DEBUG
-        ksocknal_tunables.ksnd_protocol           = &protocol;
+	ksocknal_tunables.ksnd_protocol           = &protocol;
 #endif
 
-        if (*ksocknal_tunables.ksnd_zc_min_payload < (2 << 10))
-                *ksocknal_tunables.ksnd_zc_min_payload = (2 << 10);
+	if (*ksocknal_tunables.ksnd_zc_min_payload < (2 << 10))
+		*ksocknal_tunables.ksnd_zc_min_payload = (2 << 10);
+
+	/* When on a hypervisor set the minimum zero copy size
+	 * above the maximum payload size
+	 */
+	if (!is_native_host())
+		*ksocknal_tunables.ksnd_zc_min_payload = (16 << 20) + 1;
 
 	return 0;
 };

lnet/autoconf/lustre-lnet.m4
@@ -638,6 +638,27 @@ LB_CHECK_EXPORT([kmap_to_page], [mm/highmem.c],
 ]) # LN_EXPORT_KMAP_TO_PAG
 
 #
+# LN_HAVE_HYPERVISOR_IS_TYPE
+#
+# 4.14 commit 79cc74155218316b9a5d28577c7077b2adba8e58
+# x86/paravirt: Provide a way to check for hypervisors
+#
+AC_DEFUN([LN_HAVE_HYPERVISOR_IS_TYPE], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if hypervisor_is_type function is available],
+hypervisor_is_type_exists, [
+	#include <asm/hypervisor.h>
+],[
+	(void)hypervisor_is_type(X86_HYPER_NATIVE);
+],[
+	AC_DEFINE(HAVE_HYPERVISOR_IS_TYPE, 1,
+		[hypervisor_is_type function exists])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LN_HAVE_HYPERVISOR_IS_TYPE
+
+#
 # LN_HAVE_ORACLE_OFED_EXTENSIONS
 #
 # Oracle UEK 5
@@ -746,6 +767,7 @@ LN_CONFIG_SK_DATA_READY
 # 4.x
 LN_CONFIG_SOCK_CREATE_KERN
 # 4.14
+LN_HAVE_HYPERVISOR_IS_TYPE
 LN_HAVE_ORACLE_OFED_EXTENSIONS
 # 4.17
 LN_CONFIG_SOCK_GETNAME

commit bf67df3212759c40b434c8182663f88ee23ad618

LU-19763 socklnd: Check sendpage_ok for ZC
Add a check for sendpage_ok() on pages to be sent via zero-copy.
If any page is not suitable for sendpage, disable zero-copy for
the transaction.

Also add configuration check for sendpage_ok() availability.
This was added in kernel commit v5.9-rc6-325-gc381b07941.

This reverts commit v2_13_52-101-g0c45e49457 because this code
is no longer needed.

Test-Parameters: trivial
Fixes: 0c45e49457 ("LU-12945 lnet: Disable zero copy when running on VM")
Signed-off-by: Jinshan Xiong <jinshanx@google.com>
Change-Id: Ic0e3a069c1919f8170dd2d037c2791fa6b07244e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63266
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Timothy Day <timday@amazon.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

config/lustre-lnet.m4
@@ -1206,27 +1206,6 @@ AC_DEFUN([LN_ETHTOOL_LINK_SETTINGS], [
 ]) # LN_ETHTOOL_LINK_SETTINGS
 
 #
-# LN_HAVE_HYPERVISOR_IS_TYPE
-#
-# 4.14 commit 79cc74155218316b9a5d28577c7077b2adba8e58
-# x86/paravirt: Provide a way to check for hypervisors
-#
-AC_DEFUN([LN_SRC_HAVE_HYPERVISOR_IS_TYPE], [
-	LB2_LINUX_TEST_SRC([hypervisor_is_type_exists], [
-		#include <asm/hypervisor.h>
-	],[
-		(void)hypervisor_is_type(X86_HYPER_NATIVE);
-	],[-Werror])
-])
-AC_DEFUN([LN_HAVE_HYPERVISOR_IS_TYPE], [
-	LB2_MSG_LINUX_TEST_RESULT([if hypervisor_is_type function is available],
-	[hypervisor_is_type_exists], [
-		AC_DEFINE(HAVE_HYPERVISOR_IS_TYPE, 1,
-			[hypervisor_is_type function exists])
-	])
-]) # LN_HAVE_HYPERVISOR_IS_TYPE
-
-#
 # LN_HAVE_ORACLE_OFED_EXTENSIONS
 #
 # Oracle UEK 5
@@ -1337,6 +1316,28 @@ AC_COMPILE_IFELSE([AC_LANG_SOURCE([
 ])
 ]) # LN_USR_RDMA
 
+#
+# LN_CONFIG_SENDPAGE_OK
+#
+# kernel commit v5.9-rc6-325-gc381b07941 added sendpage_ok() helper
+# check if sendpage_ok() is available
+#
+AC_DEFUN([LN_SRC_CONFIG_SENDPAGE_OK], [
+	LB2_LINUX_TEST_SRC([sendpage_ok], [
+		#include <linux/net.h>
+	],[
+		struct page *page = NULL;
+		(void)sendpage_ok(page);
+	],[-Werror])
+])
+AC_DEFUN([LN_CONFIG_SENDPAGE_OK], [
+	LB2_MSG_LINUX_TEST_RESULT([if sendpage_ok() is available],
+	[sendpage_ok], [
+		AC_DEFINE(HAVE_SENDPAGE_OK, 1,
+			[sendpage_ok() is available])
+	])
+]) # LN_CONFIG_SENDPAGE_OK
+
 AC_DEFUN([LN_PROG_LINUX_SRC], [
 	LN_CONFIG_O2IB_SRC
 	# 3.15
@@ -1348,7 +1349,6 @@ AC_DEFUN([LN_PROG_LINUX_SRC], [
 	# 4.6
 	LN_SRC_ETHTOOL_LINK_SETTINGS
 	# 4.14
-	LN_SRC_HAVE_HYPERVISOR_IS_TYPE
 	LN_SRC_HAVE_ORACLE_OFED_EXTENSIONS
 	# 4.16
 	LN_SRC_HAVE_NETDEV_CMD_TO_NAME
@@ -1356,6 +1356,8 @@ AC_DEFUN([LN_PROG_LINUX_SRC], [
 	LN_SRC_CONFIG_SOCK_GETNAME
 	# 5.3 and 4.18.0-193.el8
 	LN_SRC_HAVE_IN_DEV_FOR_EACH_IFA_RTNL
+	# 5.9
+	LN_SRC_CONFIG_SENDPAGE_OK
 ])
 
 AC_DEFUN([LN_PROG_LINUX_RESULTS], [
@@ -1369,7 +1371,6 @@ AC_DEFUN([LN_PROG_LINUX_RESULTS], [
 	# 4.6
 	LN_ETHTOOL_LINK_SETTINGS
 	# 4.14
-	LN_HAVE_HYPERVISOR_IS_TYPE
 	LN_HAVE_ORACLE_OFED_EXTENSIONS
 	# 4.16
 	LN_HAVE_NETDEV_CMD_TO_NAME
@@ -1377,6 +1378,8 @@ AC_DEFUN([LN_PROG_LINUX_RESULTS], [
 	LN_CONFIG_SOCK_GETNAME
 	# 5.3 and 4.18.0-193.el8
 	LN_HAVE_IN_DEV_FOR_EACH_IFA_RTNL
+	# 5.9
+	LN_CONFIG_SENDPAGE_OK
 ])
 
 #

include/lustre_compat/net/linux-net.h
@@ -198,4 +198,11 @@ static inline void ip_sock_set_tos(struct sock *sk, int val)
 #endif /* HAVE_IP_SET_TOS */
 #endif /* HAVE_KERNEL_SETSOCKOPT */
 
+#if !defined(HAVE_SENDPAGE_OK)
+static inline bool sendpage_ok(struct page *page)
+{
+	return true;
+}
+#endif
+
 #endif /* __LIBCFS_LINUX_NET_H__ */

lnet/klnds/socklnd/socklnd_cb.c
@@ -1029,8 +1029,17 @@ ksocknal_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 
 	LASSERT(tx->tx_nkiov <= LNET_MAX_IOV);
 
-	if (payload_nob >= *ksocknal_tunables.ksnd_zc_min_payload)
+	if (payload_nob >= *ksocknal_tunables.ksnd_zc_min_payload) {
+		int i;
+
 		tx->tx_zc_capable = 1;
+		for (i = 0; i < tx->tx_nkiov; i++) {
+			if (!sendpage_ok(tx->tx_kiov[i].bv_page)) {
+				tx->tx_zc_capable = 0;
+				break;
+			}
+		}
+	}
 
 	tx->tx_msg.ksm_csum = 0;
 	tx->tx_msg.ksm_type = KSOCK_MSG_LNET;

lnet/klnds/socklnd/socklnd_lib.c
@@ -95,15 +95,8 @@ ksocknal_lib_sendpage(struct socket *sock, struct bio_vec *kiov,
 {
 #ifdef MSG_SPLICE_PAGES
 	struct msghdr msg = {.msg_flags = msgflg | MSG_SPLICE_PAGES};
-	int i;
 
 	iov_iter_bvec(&msg.msg_iter, ITER_SOURCE, kiov, 1, kiov->bv_len);
-	for (i = 0; i < nkiov; i++) {
-		if (!sendpage_ok(kiov[i].bv_page)) {
-			msg.msg_flags &= ~MSG_SPLICE_PAGES;
-			break;
-		}
-	}
 
 	return sock_sendmsg(sock, &msg);
 #else

lnet/klnds/socklnd/socklnd_modparams.c
@@ -181,17 +181,6 @@ module_param_call(tos, param_set_tos, param_get_int, &tos, 0444);
 #endif
 MODULE_PARM_DESC(tos, "Set the type of service (=-1 to disable)");
 
-static inline bool is_native_host(void)
-{
-#ifdef HAVE_HYPERVISOR_IS_TYPE
-	return hypervisor_is_type(X86_HYPER_NATIVE);
-#elif defined(__x86_64__) || defined(__i386__)
-	return x86_hyper == NULL;
-#else
-	return true;
-#endif
-}
-
 struct ksock_tunables ksocknal_tunables;
 struct lnet_ioctl_config_socklnd_tunables ksock_default_tunables;
 
@@ -383,12 +372,6 @@ int ksocknal_tunables_init(void)
 
 	if (*ksocknal_tunables.ksnd_zc_min_payload < (2 << 10))
 		*ksocknal_tunables.ksnd_zc_min_payload = (2 << 10);
-
-	/* When on a hypervisor set the minimum zero copy size
-	 * above the maximum payload size
-	 */
-	if (!is_native_host())
-		*ksocknal_tunables.ksnd_zc_min_payload = (16 << 20) + 1;
 
 	return 0;
 }