lustre/lustre-release

commit 15c0a21ea9a6e98d642e6d16898e46ba9e9b2fa9

LU-12341 tests: Add kmemleak awareness to test-framework
If active kmemleak is detected, perform a clear operation to
ensure all non-Lustre related leaks are not getting in the way.

When it comes time to unload modules, first perform a scan
and then save the output if it's not empty, print to
syslog (for simplicity).
Also save /proc/modules content for the next step (we can save ogdb
from /tmp, but that seems to be getting stale and needs its own
fixing)

After modules unload perform another scan and if the result is non-empty,
output the saved /proc/modules output and the updated memleaks
into syslog as well

Test-Parameters: trivial
Change-Id: Ibba9047e4d8b98e7ab74aeb0906078549029ad43
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34959
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>

lustre/scripts/lustre_rmmod
@@ -35,8 +35,26 @@ if [ -z "$modules" ] || [ "$modules" = "ldiskfs" ]; then
 	modules="ptlrpc lnet_selftest ldiskfs libcfs"
 fi
 
+if [ -f /sys/kernel/debug/kmemleak ] ; then
+	cat /proc/modules >/tmp/kmemleak-modules-list.txt
+	echo scan > /sys/kernel/debug/kmemleak
+	cat /sys/kernel/debug/kmemleak > /tmp/kmemleak-before-unload.txt
+	test -s /tmp/kmemleak-before-unload.txt && logger -t leak-pre -f /tmp/kmemleak-before-unload.txt
+	rm /tmp/kmemleak-before-unload.txt
+	# Clear everything here so that only new leaks show up
+	# after module unload
+	echo clear > /sys/kernel/debug/kmemleak
+fi
+
 for mod in $modules; do
 	unload_dep_modules_inclusive $mod || exit 1
 done
+
+if [ -f /sys/kernel/debug/kmemleak ] ; then
+	echo scan > /sys/kernel/debug/kmemleak
+	cat /sys/kernel/debug/kmemleak > /tmp/kmemleak-after-unload.txt
+	test -s /tmp/kmemleak-after-unload.txt && logger -t leak-mods -f /tmp/kmemleak-modules-list.txt && logger -t leak-post -f /tmp/kmemleak-after-unload.txt
+	rm -f /tmp/kmemleak-after-unload.txt /tmp/kmemleak-modules-list.txt
+fi
 
 exit 0

lustre/tests/test-framework.sh
@@ -639,6 +639,15 @@ load_modules_local() {
 	udevadm control --reload-rules
 	udevadm trigger
 
+	# For kmemleak-enabled kernels we need clear all past state
+	# that obviously has nothing to do with this Lustre run
+	# Disable automatic memory scanning to avoid perf hit.
+	if [ -f /sys/kernel/debug/kmemleak ] ; then
+		echo scan=off > /sys/kernel/debug/kmemleak
+		echo scan > /sys/kernel/debug/kmemleak
+		echo clear > /sys/kernel/debug/kmemleak
+	fi
+
 	echo Loading modules from $LUSTRE
 
 	local ncpus

commit 1e45c2ab82652e0a06ec4d089930980c681f2255

LU-19739 tests: quiet spurious kmemleak errors
If kmemleak is not configured, it will print error messages like:

    lustre_rmmod: line 114: echo: write error: Device or resource busy

but this can be misunderstood to indicate there is something wrong.
Quiet these error messages if kmemleak is present, but inactive, and
do not try to check for leaks every time the modules are unloaded.

Clean up code to avoid repeated full pathnames and bash code style.

Fixes: 15c0a21ea9 ("LU-12341 tests: Add kmemleak awareness to test-framework")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: Ia108dce0474c5d09e0c46c3d90fd3d25a02540e5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63149
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Frederick Dilger <fdilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/scripts/lustre_rmmod
@@ -30,6 +30,7 @@ print_usage() {
 # Print kernel debug message for lustre modules
 print_debug() {
 	local debug_file
+
 	$LCTL mark "$SCRIPT_NAME : Stop debug"
 	if [[ $DEBUG_RMMOD == "-" ]]; then
 		debug_file="" # dump to stdout
@@ -109,15 +110,26 @@ else
 	unload_all=false
 fi
 
-if [ -f /sys/kernel/debug/kmemleak ] ; then
-	cat /proc/modules >/tmp/kmemleak-modules-list.txt
-	echo scan > /sys/kernel/debug/kmemleak
-	cat /sys/kernel/debug/kmemleak > /tmp/kmemleak-before-unload.txt
-	test -s /tmp/kmemleak-before-unload.txt && logger -t leak-pre -f /tmp/kmemleak-before-unload.txt
-	rm /tmp/kmemleak-before-unload.txt
-	# Clear everything here so that only new leaks show up
-	# after module unload
-	echo clear > /sys/kernel/debug/kmemleak
+export KMEMLEAK=${KMEMLEAK:-/sys/kernel/debug/kmemleak}
+KMEMLEAK_MODS=/tmp/kmemleak-modules-list.txt
+if [[ -w $KMEMLEAK ]]; then
+	kmemleak_out=$(echo scan > $KMEMLEAK 2>&1 || true)
+
+	if [[ "$kmemleak_out" =~ "Device or resource busy" ]]; then
+		echo "kmemleak disabled"
+		export KMEMLEAK=disabled
+	else
+		kmemleak_pre=/tmp/kmemleak-pre-unload.txt
+
+		echo $kmemleak_out
+		cat /proc/modules > $KMEMLEAK_MODS
+		cat $KMEMLEAK > $kmemleak_pre
+		[[ -s $kmemleak_pre ]] && logger -t leak-pre -f $kmemleak_pre
+		rm -f $kmemleak_pre
+		# Clear everything here so that only new leaks show up
+		# after module unload
+		echo clear > $KMEMLEAK
+	fi
 fi
 
 # Manage debug
@@ -152,11 +164,14 @@ if $DEBUG; then
 	print_debug
 fi
 
-if [ -f /sys/kernel/debug/kmemleak ] ; then
-	echo scan > /sys/kernel/debug/kmemleak
-	cat /sys/kernel/debug/kmemleak > /tmp/kmemleak-after-unload.txt
-	test -s /tmp/kmemleak-after-unload.txt && logger -t leak-mods -f /tmp/kmemleak-modules-list.txt && logger -t leak-post -f /tmp/kmemleak-after-unload.txt
-	rm -f /tmp/kmemleak-after-unload.txt /tmp/kmemleak-modules-list.txt
+if [[ -f $KMEMLEAK ]]; then
+	kmemleak_post=/tmp/kmemleak-post-unload.txt
+
+	echo scan > $KMEMLEAK 2>&1 | grep -v "Device or resource busy"
+	cat $KMEMLEAK > $kmemleak_post
+	[[ -s $kmemleak_post ]] && logger -t leak-mods -f $KMEMLEAK_MODS &&
+		logger -t leak-post -f $kmemleak_post
+	rm -f $kmemleak_post $KMEMLEAK_MODS
 fi
 
 exit 0

lustre/tests/test-framework.sh
@@ -1025,14 +1025,21 @@ do_lctl() {
 	$LCTL "$@"
 }
 
+KMEMLEAK=${KMEMLEAK:-/sys/kernel/debug/kmemleak}
 load_lnet() {
 	# For kmemleak-enabled kernels we need clear all past state
 	# that obviously has nothing to do with this Lustre run
 	# Disable automatic memory scanning to avoid perf hit.
-	if [ -f /sys/kernel/debug/kmemleak ] ; then
-		echo scan=off > /sys/kernel/debug/kmemleak || true
-		echo scan > /sys/kernel/debug/kmemleak || true
-		echo clear > /sys/kernel/debug/kmemleak || true
+	if [[ -w $KMEMLEAK ]] ; then
+		local kmemleak_out=$(echo scan=off > $KMEMLEAK 2>&1 || true)
+
+		if [[ "$kmemleak_out" =~ "Device or resource busy" ]]; then
+			echo "kmemleak disabled"
+			export KMEMLEAK=disabled
+		else
+			echo scan > $KMEMLEAK || true
+			echo clear > $KMEMLEAK || true
+		fi
 	fi
 
 	echo Loading modules from $LUSTRE