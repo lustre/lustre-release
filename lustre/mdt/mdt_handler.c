lustre/lustre-release

commit 4e8fcee92d75102314f6c39c72b9c2cbce109bd4

LU-13064 sec: check permissions for changelogs access
root permissions should be checked when reading or clearing changelogs
from clients. In particular, if root is squashed via a nodemap entry,
it should not be allowed to access changelogs.
To achieve this send mdt body along with RQF_LLOG_ORIGIN_HANDLE_CREATE
and RQF_MDT_SET_INFO requests. And on server side, retrieve user
credentials and make sure they have root permission.

Test-Parameters: clientversion=2.12 envdefinitions=SANITY_EXCEPT="27M 56ra 151 156 802"
Test-Parameters: serverversion=2.12
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I0c6cc99f8a7c5a13c2b31009d73f38976931ec37
Reviewed-on: https://review.whamcloud.com/36990
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Emoly Liu <emoly@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -2273,6 +2273,8 @@ static int mdt_set_info(struct tgt_session_info *tsi)
 			__swab32s(&cs->cs_id);
 		}
 
+		if (!mdt_is_rootadmin(tsi2mdt_info(tsi)))
+			RETURN(-EACCES);
 		rc = mdt_iocontrol(OBD_IOC_CHANGELOG_CLEAR, req->rq_export,
 				   vallen, val, NULL);
 	} else if (KEY_IS(KEY_EVICT_BY_NID)) {
@@ -5147,6 +5149,16 @@ static int mdt_tgt_getxattr(struct tgt_session_info *tsi)
 	return rc;
 }
 
+static int mdt_llog_open(struct tgt_session_info *tsi)
+{
+	ENTRY;
+
+	if (!mdt_is_rootadmin(tsi2mdt_info(tsi)))
+		RETURN(err_serious(-EACCES));
+
+	RETURN(tgt_llog_open(tsi));
+}
+
 #define OBD_FAIL_OST_READ_NET	OBD_FAIL_OST_BRW_NET
 #define OBD_FAIL_OST_WRITE_NET	OBD_FAIL_OST_BRW_NET
 #define OST_BRW_READ	OST_READ
@@ -5161,7 +5173,7 @@ TGT_RPC_HANDLER(MDS_FIRST_OPC,
 		&RQF_MDS_DISCONNECT, LUSTRE_OBD_VERSION),
 TGT_RPC_HANDLER(MDS_FIRST_OPC,
 		HAS_REPLY,		MDS_SET_INFO,	mdt_set_info,
-		&RQF_OBD_SET_INFO, LUSTRE_MDS_VERSION),
+		&RQF_MDT_SET_INFO, LUSTRE_MDS_VERSION),
 TGT_MDT_HDL(0,				MDS_GET_INFO,	mdt_get_info),
 TGT_MDT_HDL(HAS_REPLY,		MDS_GET_ROOT,	mdt_get_root),
 TGT_MDT_HDL(HAS_BODY,		MDS_GETATTR,	mdt_getattr),
@@ -5214,6 +5226,13 @@ static struct tgt_handler mdt_quota_ops[] = {
 TGT_QUOTA_HDL(HAS_REPLY,		QUOTA_DQACQ,	  mdt_quota_dqacq),
 };
 
+static struct tgt_handler mdt_llog_handlers[] = {
+	TGT_LLOG_HDL(0,	LLOG_ORIGIN_HANDLE_CREATE,	mdt_llog_open),
+	TGT_LLOG_HDL(0,	LLOG_ORIGIN_HANDLE_NEXT_BLOCK,	tgt_llog_next_block),
+	TGT_LLOG_HDL(0,	LLOG_ORIGIN_HANDLE_READ_HEADER,	tgt_llog_read_header),
+	TGT_LLOG_HDL(0,	LLOG_ORIGIN_HANDLE_PREV_BLOCK,	tgt_llog_prev_block),
+};
+
 static struct tgt_opc_slice mdt_common_slice[] = {
 	{
 		.tos_opc_start	= MDS_FIRST_OPC,
@@ -5258,7 +5277,7 @@ static struct tgt_opc_slice mdt_common_slice[] = {
 	{
 		.tos_opc_start	= LLOG_FIRST_OPC,
 		.tos_opc_end	= LLOG_LAST_OPC,
-		.tos_hs		= tgt_llog_handlers
+		.tos_hs		= mdt_llog_handlers
 	},
 	{
 		.tos_opc_start	= LFSCK_FIRST_OPC,

lustre/mdt/mdt_internal.h
@@ -1365,4 +1365,32 @@ long mdt_grant_connect(const struct lu_env *env, struct obd_export *exp,
 		       u64 want, bool conservative);
 extern struct kmem_cache *ldlm_glimpse_work_kmem;
 
+static inline bool mdt_is_rootadmin(struct mdt_thread_info *info)
+{
+	struct lu_ucred *uc = NULL;
+	bool is_admin;
+	int rc;
+
+	if (info == NULL || info->mti_body == NULL)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 3, 0)
+		/* return true in case old client did not send mdt body */
+		return true;
+#else
+		return false
+#endif
+
+	rc = mdt_init_ucred(info, (struct mdt_body *)info->mti_body);
+	if (rc < 0)
+		return false;
+
+	uc = mdt_ucred(info);
+	is_admin = (uc->uc_uid == 0 && uc->uc_gid == 0 &&
+		    md_capable(uc, CFS_CAP_SYS_ADMIN));
+
+	mdt_exit_ucred(info);
+
+	return is_admin;
+}
+
+
 #endif /* _MDT_INTERNAL_H */

lustre/include/lustre_req_layout.h
@@ -132,6 +132,7 @@ int req_check_sepol(struct req_capsule *pill);
 
 extern struct req_format RQF_OBD_PING;
 extern struct req_format RQF_OBD_SET_INFO;
+extern struct req_format RQF_MDT_SET_INFO;
 extern struct req_format RQF_SEC_CTX;
 extern struct req_format RQF_OBD_IDX_READ;
 /* MGS req_format */

lustre/ptlrpc/layout.c
@@ -365,6 +365,13 @@ static const struct req_msg_field *obd_set_info_client[] = {
         &RMF_SETINFO_VAL
 };
 
+static const struct req_msg_field *mdt_set_info_client[] = {
+	&RMF_PTLRPC_BODY,
+	&RMF_SETINFO_KEY,
+	&RMF_SETINFO_VAL,
+	&RMF_MDT_BODY
+};
+
 static const struct req_msg_field *ost_grant_shrink_client[] = {
         &RMF_PTLRPC_BODY,
         &RMF_SETINFO_KEY,
@@ -575,9 +582,10 @@ static const struct req_msg_field *mds_update_server[] = {
 };
 
 static const struct req_msg_field *llog_origin_handle_create_client[] = {
-        &RMF_PTLRPC_BODY,
-        &RMF_LLOGD_BODY,
-        &RMF_NAME
+	&RMF_PTLRPC_BODY,
+	&RMF_LLOGD_BODY,
+	&RMF_NAME,
+	&RMF_MDT_BODY
 };
 
 static const struct req_msg_field *llogd_body_only[] = {
@@ -747,6 +755,7 @@ static const struct req_msg_field *obd_lfsck_reply[] = {
 static struct req_format *req_formats[] = {
 	&RQF_OBD_PING,
 	&RQF_OBD_SET_INFO,
+	&RQF_MDT_SET_INFO,
 	&RQF_OBD_IDX_READ,
 	&RQF_SEC_CTX,
 	&RQF_MGS_TARGET_REG,
@@ -1327,6 +1336,10 @@ EXPORT_SYMBOL(RQF_OBD_PING);
 struct req_format RQF_OBD_SET_INFO =
         DEFINE_REQ_FMT0("OBD_SET_INFO", obd_set_info_client, empty);
 EXPORT_SYMBOL(RQF_OBD_SET_INFO);
+
+struct req_format RQF_MDT_SET_INFO =
+	DEFINE_REQ_FMT0("MDT_SET_INFO", mdt_set_info_client, empty);
+EXPORT_SYMBOL(RQF_MDT_SET_INFO);
 
 /* Read index file through the network */
 struct req_format RQF_OBD_IDX_READ =

lustre/ptlrpc/llog_client.c
@@ -45,6 +45,8 @@
 #include <lustre_log.h>
 #include <lustre_net.h>
 
+#include "ptlrpc_internal.h"
+
 #define LLOG_CLIENT_ENTRY(ctxt, imp) do {                             \
 	mutex_lock(&ctxt->loc_mutex);                                 \
 	if (ctxt->loc_imp) {                                          \
@@ -121,6 +123,8 @@ static int llog_client_open(const struct lu_env *env,
 						   strlen(name) + 1);
 		LASSERT(tmp);
 		strcpy(tmp, name);
+
+		do_pack_body(req);
 	}
 
 	rc = ptlrpc_queue_wait(req);

lustre/ptlrpc/pack_generic.c
@@ -1666,7 +1666,10 @@ int do_set_info_async(struct obd_import *imp,
 	int rc;
 
 	ENTRY;
-	req = ptlrpc_request_alloc(imp, &RQF_OBD_SET_INFO);
+
+	req = ptlrpc_request_alloc(imp, KEY_IS(KEY_CHANGELOG_CLEAR) ?
+						&RQF_MDT_SET_INFO :
+						&RQF_OBD_SET_INFO);
 	if (req == NULL)
 		RETURN(-ENOMEM);
 
@@ -1679,6 +1682,9 @@ int do_set_info_async(struct obd_import *imp,
 		ptlrpc_request_free(req);
 		RETURN(rc);
 	}
+
+	if (KEY_IS(KEY_CHANGELOG_CLEAR))
+		do_pack_body(req);
 
 	tmp = req_capsule_client_get(&req->rq_pill, &RMF_SETINFO_KEY);
 	memcpy(tmp, key, keylen);

lustre/ptlrpc/ptlrpc_internal.h
@@ -418,4 +418,23 @@ static inline bool ptlrpc_req_is_disconnect(struct ptlrpc_request *req)
 		return false;
 }
 
+static inline void do_pack_body(struct ptlrpc_request *req)
+{
+	struct mdt_body *b = req_capsule_client_get(&req->rq_pill,
+						    &RMF_MDT_BODY);
+
+	if (b == NULL)
+		return;
+
+	b->mbo_valid = 0;
+	b->mbo_eadatasize = 0;
+	b->mbo_flags = 0;
+	b->mbo_suppgid = -1;
+	b->mbo_uid = from_kuid(&init_user_ns, current_uid());
+	b->mbo_gid = from_kgid(&init_user_ns, current_gid());
+	b->mbo_fsuid = from_kuid(&init_user_ns, current_fsuid());
+	b->mbo_fsgid = from_kgid(&init_user_ns, current_fsgid());
+	b->mbo_capability = cfs_curproc_cap_pack();
+}
+
 #endif /* PTLRPC_INTERNAL_H */

lustre/target/tgt_handler.c
@@ -319,11 +319,15 @@ static int tgt_request_preprocess(struct tgt_session_info *tsi,
 		     h->th_fmt != NULL));
 	if (h->th_fmt != NULL) {
 		req_capsule_set(pill, h->th_fmt);
-		if (req_capsule_has_field(pill, &RMF_MDT_BODY, RCL_CLIENT)) {
+		if (req_capsule_has_field(pill, &RMF_MDT_BODY, RCL_CLIENT) &&
+		    req_capsule_field_present(pill, &RMF_MDT_BODY,
+					      RCL_CLIENT)) {
 			rc = tgt_mdt_body_unpack(tsi, flags);
 			if (rc < 0)
 				RETURN(rc);
 		} else if (req_capsule_has_field(pill, &RMF_OST_BODY,
+						 RCL_CLIENT) &&
+			   req_capsule_field_present(pill, &RMF_OST_BODY,
 						 RCL_CLIENT)) {
 			rc = tgt_ost_body_unpack(tsi, flags);
 			if (rc < 0)

commit 1c0d9f16e4736f3ae0d907431bfdb290a24ac1e4

LU-19750 mdt: update mdt_changelog_allow() version check
Update MDT llog version check to allow clients to access llog
until client-side fix to initialize mdt_body is widely available
(currently set to expire after build version 3.2.53).

Pass obd_export from caller to mdt_changelog_allow() instead of
doing more work to extract it out of mdt_thread_info each time.
Always check NODEMAP_RBAC_CHLG_OPS role even if mdt_body unset.

Test-Parameters: trivial
Fixes: 4e8fcee92d ("LU-13064 sec: check permissions for changelogs access")
Fixes: 815dfd3451 ("LU-19750 ptlrpc: llog client to set mdt_body")
Fixes: 971e025f5f ("LU-16524 sec: enforce rbac roles")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I2820e09fb89fdbc05716d851807a00fe753ebbe5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63272
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_handler.c
@@ -2863,10 +2863,11 @@ int mdt_io_set_info(struct tgt_session_info *tsi)
 
 static int mdt_set_info(struct tgt_session_info *tsi)
 {
-	struct ptlrpc_request	*req = tgt_ses_req(tsi);
-	char			*key;
-	void			*val;
-	int			 keylen, vallen, rc = 0;
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
+	struct obd_export *exp = req->rq_export;
+	char *key;
+	void *val;
+	int keylen, vallen, rc = 0;
 
 	ENTRY;
 
@@ -2895,8 +2896,8 @@ static int mdt_set_info(struct tgt_session_info *tsi)
 			struct lu_nodemap *nm = NULL;
 			bool readonly = false;
 
-			if (req->rq_export)
-				nm = nodemap_get_from_exp(req->rq_export);
+			if (exp)
+				nm = nodemap_get_from_exp(exp);
 
 			if (!IS_ERR_OR_NULL(nm)) {
 				readonly = nm->nmf_readonly_mount;
@@ -2906,14 +2907,12 @@ static int mdt_set_info(struct tgt_session_info *tsi)
 			if (unlikely(readonly))
 				RETURN(-EROFS);
 		}
-		spin_lock(&req->rq_export->exp_lock);
+		spin_lock(&exp->exp_lock);
 		if (*(__u32 *)val)
-			*exp_connect_flags_ptr(req->rq_export) |=
-				OBD_CONNECT_RDONLY;
+			*exp_connect_flags_ptr(exp) |= OBD_CONNECT_RDONLY;
 		else
-			*exp_connect_flags_ptr(req->rq_export) &=
-				~OBD_CONNECT_RDONLY;
-		spin_unlock(&req->rq_export->exp_lock);
+			*exp_connect_flags_ptr(exp) &= ~OBD_CONNECT_RDONLY;
+		spin_unlock(&exp->exp_lock);
 	} else if (KEY_IS(KEY_CHANGELOG_CLEAR)) {
 		struct changelog_setinfo *cs = val;
 
@@ -2927,13 +2926,13 @@ static int mdt_set_info(struct tgt_session_info *tsi)
 			__swab32s(&cs->cs_id);
 		}
 
-		if (!mdt_changelog_allow(tsi2mdt_info(tsi)))
+		if (!mdt_changelog_allow(tsi2mdt_info(tsi), exp))
 			RETURN(-EACCES);
-		rc = mdt_iocontrol(OBD_IOC_CHANGELOG_CLEAR, req->rq_export,
+		rc = mdt_iocontrol(OBD_IOC_CHANGELOG_CLEAR, exp,
 				   vallen, val, NULL);
 	} else if (KEY_IS(KEY_EVICT_BY_NID)) {
 		if (vallen > 0)
-			obd_export_evict_by_nid(req->rq_export->exp_obd, val);
+			obd_export_evict_by_nid(exp->exp_obd, val);
 	} else {
 		RETURN(-EINVAL);
 	}
@@ -6238,10 +6237,15 @@ static int mdt_tgt_getxattr(struct tgt_session_info *tsi)
 
 static int mdt_llog_open(struct tgt_session_info *tsi)
 {
-	ENTRY;
+	struct mdt_thread_info *info;
 
-	if (!mdt_changelog_allow(tsi2mdt_info(tsi)))
+	ENTRY;
+	info = tsi2mdt_info(tsi);
+	if (!info)
 		RETURN(err_serious(-EACCES));
+
+	if (!mdt_changelog_allow(info, tsi->tsi_exp))
+		RETURN(-EACCES);
 
 	RETURN(tgt_llog_open(tsi));
 }

lustre/mdt/mdt_internal.h
@@ -1444,16 +1444,28 @@ long mdt_grant_connect(const struct lu_env *env, struct obd_export *exp,
 		       u64 want, bool conservative);
 extern struct kmem_cache *ldlm_glimpse_work_kmem;
 
-static inline bool mdt_changelog_allow(struct mdt_thread_info *info)
+static inline bool mdt_changelog_allow(struct mdt_thread_info *info,
+				       struct obd_export *exp)
 {
 	struct lu_ucred *uc = NULL;
 	bool is_admin;
 	int rc;
 
-	if (info == NULL || info->mti_body == NULL)
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 3, 0)
-		/* return true in case old client did not send mdt body */
-		return true;
+	if (!info->mti_body)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 2, 53, 0)
+	{
+		/* if client does not send mti_body, check nodemap directly */
+		enum nodemap_rbac_roles rbac = NODEMAP_RBAC_ALL;
+		struct lu_nodemap *nodemap;
+
+		nodemap = nodemap_get_from_exp(exp);
+		if (!IS_ERR_OR_NULL(nodemap)) {
+			rbac = nodemap->nmf_rbac;
+			nodemap_putref(nodemap);
+
+		}
+		return rbac & NODEMAP_RBAC_CHLG_OPS;
+	}
 #else
 		return false;
 #endif