lustre/lustre-release

commit d67cf23fb18aee0e5859475f6a97a187a7b6068e

LU-18993 osc: Add IO RPC latency statistics by size
Add a new 'osc.*.io_latency_stats' stats that tracks RPC latency
histograms separately for each RPC size (in KiB). This provides
more detailed performance metrics for diagnosing Lustre client-server
communication issues by showing how RPC latency varies with size.

The implementation:
1. Adds new histogram arrays to client_obd for read and write RPCs
2. Updates brw_interpret() to record latency in the appropriate
   histogram based on RPC size (aa->aa_page_count)
3. Adds a new proc entry 'rpc_latency_stats' that displays the
   histograms in a human-readable format
4. Skips printing zero-valued entries for better readability
5. Adds test_127e to verify the functionality

Example output format:
  io_latency_by_size:
  snapshot_time: 1756402276.152694808
  start_time:    14.921641461
  elapsed_time:  1756402261.231053347
  rd_4K: { 256us: 5, 512us: 5, }
  rd_64K: { 512us: 10, }
  rd_1024K: { 1024us: 5, 2048us: 4, 4096us: 1, }
  rd_4096K: { 4096us: 9, 8192us: 1, }
  wr_4K: { 2048us: 8, 4096us: 1, 8192us: 1, }
  wr_64K: { 2048us: 8, 4096us: 2, }
  wr_1024K: { 4096us: 10, }
  wr_4096K: { 8192us: 9, 16384us: 1, }

Signed-off-by: Fredrick Dilger <fdilger@whamcloud.com>
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I61351dc51e1e74c4744167d281e39e5b9061973b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59175
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lprocfs_status.h
@@ -519,6 +519,14 @@ extern int ldebugfs_alloc_obd_stats(struct obd_device *obd,
 				    unsigned int num_stats);
 extern int lprocfs_alloc_md_stats(struct obd_device *obd,
 				  unsigned int num_private_stats);
+void obd_io_latency_stats_clear(struct obd_histogram *read_io_latency_by_size,
+				struct obd_histogram *write_io_latency_by_size,
+				int num_buckets, ktime_t *stats_init);
+extern int obd_io_latency_stats_seq_show(struct seq_file *seq,
+				 struct obd_histogram *read_io_latency_by_size,
+				 struct obd_histogram *write_io_latency_by_size,
+				 int num_buckets, ktime_t stats_init,
+				 spinlock_t *list_lock);
 extern void lprocfs_counter_init(struct lprocfs_stats *stats, int index,
 				 enum lprocfs_counter_config config,
 				 const char *name);

lustre/include/obd.h
@@ -270,6 +270,10 @@ struct client_obd {
 	struct obd_histogram	cl_write_offset_hist;
 	struct obd_histogram	cl_read_io_latency_hist;
 	struct obd_histogram	cl_write_io_latency_hist;
+	/* RPC latency histograms by size (in pages) */
+	ktime_t			cl_io_latency_stats_init;
+	struct obd_histogram	*cl_read_io_latency_by_size;
+	struct obd_histogram	*cl_write_io_latency_by_size;
 	ktime_t			cl_batch_stats_init;
 	struct obd_histogram	cl_batch_rpc_hist;
 

lustre/obdclass/lprocfs_status.c
@@ -629,6 +629,99 @@ void lprocfs_stats_collect(struct lprocfs_stats *stats, int idx,
 }
 EXPORT_SYMBOL(lprocfs_stats_collect);
 
+void obd_io_latency_stats_clear(struct obd_histogram *read_io_latency_by_size,
+				struct obd_histogram *write_io_latency_by_size,
+				int num_buckets, ktime_t *stats_init)
+{
+	int i;
+
+	*stats_init = ktime_get_real();
+	for (i = 0; i < num_buckets; i++)
+		lprocfs_oh_clear(&read_io_latency_by_size[i]);
+
+	for (i = 0; i < num_buckets; i++)
+		lprocfs_oh_clear(&write_io_latency_by_size[i]);
+}
+EXPORT_SYMBOL(obd_io_latency_stats_clear);
+
+int obd_io_latency_stats_seq_show(struct seq_file *seq,
+				 struct obd_histogram *read_io_latency_by_size,
+				 struct obd_histogram *write_io_latency_by_size,
+				 int num_buckets, ktime_t stats_init,
+				 spinlock_t *list_lock)
+{
+	unsigned long read_tot, write_tot, read_cum, write_cum;
+	int i, j, kb;
+
+	spin_lock(list_lock);
+
+	seq_puts(seq, "io_latency_by_size:\n");
+	lprocfs_stats_header(seq, ktime_get_real(), stats_init, 13,
+			     ":", false, "");
+
+	/* Print read latency histograms */
+	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		if (!read_io_latency_by_size)
+			continue;
+
+		read_tot = lprocfs_oh_sum(&read_io_latency_by_size[i]);
+
+		if (read_tot == 0)
+			continue;
+
+		seq_printf(seq, "rd_%uK: { ", kb);
+		read_cum = 0;
+		for (j = 0; j < OBD_HIST_MAX; j++) {
+			struct obd_histogram *h = &read_io_latency_by_size[i];
+			unsigned long r = h->oh_buckets[j];
+
+			if (r == 0)
+				continue;
+
+			read_cum += r;
+			seq_printf(seq, "%dus: %lu, ",
+				   (j == 0) ? 0 : 1 << (j - 1), r);
+
+			if (read_cum == read_tot)
+				break;
+		}
+		seq_puts(seq, "}\n");
+	}
+	/* Print write latency histograms */
+	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		if (!write_io_latency_by_size)
+			continue;
+
+		write_tot = lprocfs_oh_sum(&write_io_latency_by_size[i]);
+
+		if (write_tot == 0)
+			continue;
+
+		seq_printf(seq, "wr_%uK: { ", kb);
+		write_cum = 0;
+		for (j = 0; j < OBD_HIST_MAX; j++) {
+			struct obd_histogram *h = &write_io_latency_by_size[i];
+			unsigned long w = h->oh_buckets[j];
+
+			if (w == 0)
+				continue;
+
+			write_cum += w;
+			seq_printf(seq, "%dus: %lu, ",
+				   (j == 0) ? 0 : 1 << (j - 1), w);
+
+			if (write_cum == write_tot)
+				break;
+		}
+		seq_puts(seq, "}\n");
+	}
+
+	spin_unlock(list_lock);
+
+	return 0;
+}
+EXPORT_SYMBOL(obd_io_latency_stats_seq_show);
+
 static void obd_import_flags2str(struct obd_import *imp, struct seq_file *m)
 {
 	bool first = true;

lustre/osc/lproc_osc.c
@@ -662,23 +662,36 @@ LDEBUGFS_SEQ_FOPS_RO_TYPE(osc, timeouts);
 LDEBUGFS_SEQ_FOPS_RO_TYPE(osc, state);
 LDEBUGFS_SEQ_FOPS_RW_TYPE(osc, import);
 
-static struct ldebugfs_vars ldebugfs_osc_obd_vars[] = {
-	{ .name	=	"connect_flags",
-	  .fops	=	&osc_connect_flags_fops		},
-	{ .name	=	"ost_server_uuid",
-	  .fops	=	&osc_server_uuid_fops		},
-	{ .name	=	"osc_cached_mb",
-	  .fops	=	&osc_cached_mb_fops		},
-	{ .name	=	"timeouts",
-	  .fops	=	&osc_timeouts_fops		},
-	{ .name	=	"import",
-	  .fops	=	&osc_import_fops		},
-	{ .name	=	"state",
-	  .fops	=	&osc_state_fops			},
-	{ .name	=	"unstable_stats",
-	  .fops	=	&osc_unstable_stats_fops	},
-	{ NULL }
-};
+static int osc_io_latency_stats_seq_show(struct seq_file *seq, void *v)
+{
+	struct obd_device *obd = seq->private;
+	struct client_obd *cli = &obd->u.cli;
+	int num_buckets = PTLRPC_MAX_BRW_BITS - PAGE_SHIFT;
+
+	return obd_io_latency_stats_seq_show(seq,
+					     cli->cl_read_io_latency_by_size,
+					     cli->cl_write_io_latency_by_size,
+					     num_buckets,
+					     cli->cl_io_latency_stats_init,
+					     &cli->cl_loi_list_lock);
+}
+
+static ssize_t osc_io_latency_stats_seq_write(struct file *file,
+					       const char __user *buf,
+					       size_t len, loff_t *off)
+{
+	struct seq_file *seq = file->private_data;
+	struct obd_device *obd = seq->private;
+	struct client_obd *cli = &obd->u.cli;
+	int num_buckets = PTLRPC_MAX_BRW_BITS - PAGE_SHIFT;
+
+	obd_io_latency_stats_clear(cli->cl_read_io_latency_by_size,
+				   cli->cl_write_io_latency_by_size,
+				   num_buckets, &cli->cl_io_latency_stats_init);
+
+	return len;
+}
+LDEBUGFS_SEQ_FOPS(osc_io_latency_stats);
 
 static int osc_rpc_stats_seq_show(struct seq_file *seq, void *v)
 {
@@ -856,16 +869,31 @@ static ssize_t osc_stats_seq_write(struct file *file,
 
 	return len;
 }
-
 LDEBUGFS_SEQ_FOPS(osc_stats);
 
-static void ldebugfs_osc_attach_seqstat(struct obd_device *obd)
-{
-	debugfs_create_file("osc_stats", 0644, obd->obd_debugfs_entry, obd,
-			    &osc_stats_fops);
-	debugfs_create_file("rpc_stats", 0644, obd->obd_debugfs_entry, obd,
-			    &osc_rpc_stats_fops);
-}
+static struct ldebugfs_vars ldebugfs_osc_obd_vars[] = {
+	{ .name	=	"connect_flags",
+	  .fops	=	&osc_connect_flags_fops		},
+	{ .name	=	"import",
+	  .fops	=	&osc_import_fops		},
+	{ .name	=	"io_latency_stats",
+	  .fops	=	&osc_io_latency_stats_fops	},
+	{ .name	=	"osc_cached_mb",
+	  .fops	=	&osc_cached_mb_fops		},
+	{ .name	=	"osc_stats",
+	  .fops	=	&osc_stats_fops			},
+	{ .name	=	"ost_server_uuid",
+	  .fops	=	&osc_server_uuid_fops		},
+	{ .name	=	"rpc_stats",
+	  .fops	=	&osc_rpc_stats_fops		},
+	{ .name	=	"state",
+	  .fops	=	&osc_state_fops			},
+	{ .name	=	"timeouts",
+	  .fops	=	&osc_timeouts_fops		},
+	{ .name	=	"unstable_stats",
+	  .fops	=	&osc_unstable_stats_fops	},
+	{ NULL }
+};
 
 LUSTRE_OBD_UINT_PARAM_ATTR(at_min);
 LUSTRE_OBD_UINT_PARAM_ATTR(at_max);
@@ -915,8 +943,6 @@ int osc_tunables_init(struct obd_device *obd)
 	rc = lprocfs_obd_setup(obd, false);
 	if (rc)
 		return rc;
-
-	ldebugfs_osc_attach_seqstat(obd);
 
 	rc = sptlrpc_lprocfs_cliobd_attach(obd);
 	if (rc) {

lustre/osc/osc_request.c
@@ -2720,13 +2720,32 @@ static int brw_interpret(const struct lu_env *env,
 	if (ktime_to_ns(start_time)) {
 		/* binary convertion, must convert to decimal for display */
 		ktime_t latency_us = ktime_sub(completion_time, start_time) >> 10;
+		u32 page_count = aa->aa_page_count;
+		int idx;
 
-		if (lustre_msg_get_opc(req->rq_reqmsg) == OST_WRITE)
+		if (lustre_msg_get_opc(req->rq_reqmsg) == OST_WRITE) {
 			lprocfs_oh_tally_log2(&cli->cl_write_io_latency_hist,
 					      latency_us);
-		else
+
+			/* Update latency by size histogram */
+			if (cli->cl_write_io_latency_by_size && page_count) {
+				idx = fls(page_count) - 1;
+				lprocfs_oh_tally_log2(
+					&cli->cl_write_io_latency_by_size[idx],
+					latency_us);
+			}
+		} else {
 			lprocfs_oh_tally_log2(&cli->cl_read_io_latency_hist,
 					      latency_us);
+
+			/* Update latency by size histogram */
+			if (cli->cl_read_io_latency_by_size && page_count) {
+				idx = fls(page_count) - 1;
+				lprocfs_oh_tally_log2(
+					&cli->cl_read_io_latency_by_size[idx],
+					latency_us);
+			}
+		}
 	}
 	osc_wake_cache_waiters(cli);
 	spin_unlock(&cli->cl_loi_list_lock);

commit c688e33f11ffa5946d981390179cf1f072029eb9

LU-18934 osd: Add BRW IO latency statistics by size
Add new '{osd-ldiskfs,osd-zfs}.*.io_latency_stats' stats files, which
contain a percpu histogram of the server-side I/O latency by size.

These statistics can be used to identify performance issues related
to specific I/O sizes, such as small vs. large I/O operations.
Specifically, this can help identify whether small files are
causing performance issues due to high latency where they could be
masked by the creation of other large files.

These histograms are formatted the same as 'osc.*.io_latency_stats':
  io_latency_by_size:
  snapshot_time: 1756402276.152694808
  start_time:    14.921641461
  elapsed_time:  1756402261.231053347
  rd_4K: { 16us: 3, 32us: 3, 128us: 4, }
  rd_64K: { 32us: 5, 64us: 1, 128us: 1, 256us: 3, }
  rd_1024K: { 256us: 6, 1024us: 3, 2048us: 1, }
  rd_4096K: { 2048us: 9, 4096us: 1, }
  wr_4K: { 32us: 10, }
  wr_64K: { 64us: 8, 128us: 2, }
  wr_1024K: { 512us: 10, }
  wr_4096K: { 2048us: 10, }

The histograms are indexed by pages and store "binary usec" to avoid
64-bit divide for each stats increment.  The histograms are shown in
KiB and times are converted back to decimal usec for display.

Clean up the stats collection on the osc as well.

Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools
Test-Parameters: testlist=ost-pools

Fixes: d67cf23fb1 ("LU-18993 osc: Add IO RPC latency statistics by size")
Signed-off-by: Frederick Dilger <fdilger@whamcloud.com>
Change-Id: I5900c2c05687b8859555d3e0bad9ad39cc10d73c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60769
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/include/lprocfs_status.h
@@ -394,11 +394,21 @@ struct brw_stats_props {
 	bool		 bsp_scale;
 };
 
+/* hard to get PTLRPC_MAX_BRW_BITS here, verified in lprocfs_init_brw_stats() */
+#define IO_LATENCY_BUCKETS (26 - 12 + 1) /* (PTLRPC_MAX_BRW_BITS - PAGE_SHIFT) */
+
 struct brw_stats {
 	ktime_t			bs_init;
 	struct obd_hist_pcpu	bs_hist[BRW_RW_STATS_NUM];
 	struct brw_stats_props	bs_props[BRW_RW_STATS_NUM / 2];
 	bool			bs_inflight_io_log2;
+	/* latency_by_size index log2(pages), stores "binary usec" (ns >> 10) */
+	ktime_t			bs_io_latency_init;
+	struct obd_hist_pcpu	bs_read_io_latency_by_size[IO_LATENCY_BUCKETS];
+	struct obd_hist_pcpu	bs_write_io_latency_by_size[IO_LATENCY_BUCKETS];
+	u32			bs_max_pages_per_rpc;
+	spinlock_t		bs_loi_list_lock;
+	char			*bs_devname;
 };
 
 int lprocfs_init_brw_stats(struct brw_stats *brw_stats);
@@ -406,6 +416,8 @@ void lprocfs_fini_brw_stats(struct brw_stats *brw_stats);
 
 void ldebugfs_register_brw_stats(struct dentry *parent,
 				 struct brw_stats *brw_stats);
+void ldebugfs_register_io_latency_stats(struct dentry *parent,
+					struct brw_stats *brw_stats);
 #endif /* HAVE_SERVER_SUPPORT */
 
 #define EXTRA_FIRST_OPC LDLM_GLIMPSE_ENQUEUE
@@ -521,6 +533,7 @@ extern int ldebugfs_alloc_obd_stats(struct obd_device *obd,
 				    unsigned int num_stats);
 extern int lprocfs_alloc_md_stats(struct obd_device *obd,
 				  unsigned int num_private_stats);
+#define binary_usec_to_dec(b) (((b) / 1000) * 1024 + ((b) % 1000) * 1024 / 1000)
 void obd_io_latency_stats_clear(struct obd_histogram *read_io_latency_by_size,
 				struct obd_histogram *write_io_latency_by_size,
 				int num_buckets, ktime_t *stats_init);

lustre/include/obd.h
@@ -270,7 +270,7 @@ struct client_obd {
 	struct obd_histogram	cl_write_offset_hist;
 	struct obd_histogram	cl_read_io_latency_hist;
 	struct obd_histogram	cl_write_io_latency_hist;
-	/* RPC latency histograms by size (in pages) */
+	/* RPC latency histograms by size (in pages) stores "binary usec" */
 	ktime_t			cl_io_latency_stats_init;
 	struct obd_histogram	*cl_read_io_latency_by_size;
 	struct obd_histogram	*cl_write_io_latency_by_size;

lustre/obdclass/lprocfs_status.c
@@ -651,7 +651,7 @@ int obd_io_latency_stats_seq_show(struct seq_file *seq,
 				 int num_buckets, ktime_t stats_init,
 				 spinlock_t *list_lock)
 {
-	unsigned long read_tot, write_tot, read_cum, write_cum;
+	bool hdr_printed;
 	int i, j, kb;
 
 	spin_lock(list_lock);
@@ -662,59 +662,53 @@ int obd_io_latency_stats_seq_show(struct seq_file *seq,
 
 	/* Print read latency histograms */
 	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		struct obd_histogram *h = &read_io_latency_by_size[i];
+
 		if (!read_io_latency_by_size)
-			continue;
+			break;
 
-		read_tot = lprocfs_oh_sum(&read_io_latency_by_size[i]);
-
-		if (read_tot == 0)
-			continue;
-
-		seq_printf(seq, "rd_%uK: { ", kb);
-		read_cum = 0;
+		hdr_printed = false;
 		for (j = 0; j < OBD_HIST_MAX; j++) {
-			struct obd_histogram *h = &read_io_latency_by_size[i];
 			unsigned long r = h->oh_buckets[j];
 
 			if (r == 0)
 				continue;
 
-			read_cum += r;
+			if (!hdr_printed) {
+				seq_printf(seq, "rd_%uK: { ", kb);
+				hdr_printed = true;
+			}
 			seq_printf(seq, "%dus: %lu, ",
-				   (j == 0) ? 0 : 1 << (j - 1), r);
-
-			if (read_cum == read_tot)
-				break;
+				   (j == 0) ? 0 : 1 << (j - 1),
+				   binary_usec_to_dec(r));
 		}
-		seq_puts(seq, "}\n");
+		if (hdr_printed)
+			seq_puts(seq, "}\n");
 	}
 	/* Print write latency histograms */
 	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		struct obd_histogram *h = &write_io_latency_by_size[i];
+
 		if (!write_io_latency_by_size)
-			continue;
+			break;
 
-		write_tot = lprocfs_oh_sum(&write_io_latency_by_size[i]);
-
-		if (write_tot == 0)
-			continue;
-
-		seq_printf(seq, "wr_%uK: { ", kb);
-		write_cum = 0;
+		hdr_printed = false;
 		for (j = 0; j < OBD_HIST_MAX; j++) {
-			struct obd_histogram *h = &write_io_latency_by_size[i];
 			unsigned long w = h->oh_buckets[j];
 
 			if (w == 0)
 				continue;
 
-			write_cum += w;
+			if (!hdr_printed) {
+				seq_printf(seq, "wr_%uK: { ", kb);
+				hdr_printed = true;
+			}
 			seq_printf(seq, "%dus: %lu, ",
-				   (j == 0) ? 0 : 1 << (j - 1), w);
-
-			if (write_cum == write_tot)
-				break;
+				   (j == 0) ? 0 : 1 << (j - 1),
+				   binary_usec_to_dec(w));
 		}
-		seq_puts(seq, "}\n");
+		if (hdr_printed)
+			seq_puts(seq, "}\n");
 	}
 
 	spin_unlock(list_lock);

lustre/obdclass/lprocfs_status_server.c
@@ -826,29 +826,159 @@ static ssize_t brw_stats_seq_write(struct file *file,
 
 	return len;
 }
-
 LDEBUGFS_SEQ_FOPS(brw_stats);
 
-int lprocfs_init_brw_stats(struct brw_stats *brw_stats)
+static int io_latency_stats_seq_show(struct seq_file *seq, void *v)
 {
-	int i, result;
+	struct brw_stats *bs = seq->private;
+	int num_buckets = IO_LATENCY_BUCKETS;
+	struct obd_hist_pcpu *h;
+	bool hdr_printed;
+	int i, j, kb;
 
+	seq_puts(seq, "io_latency_by_size:\n");
+	spin_lock(&bs->bs_loi_list_lock);
+	lprocfs_stats_header(seq, ktime_get_real(),
+			     bs->bs_io_latency_init, 15, ":", false, "");
+
+	/* Print read latency histograms */
+	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		u64 r;
+
+		h = &bs->bs_read_io_latency_by_size[i];
+		if (unlikely(!h->oh_initialized)) {
+			seq_printf(seq, "rd_%uK: { uninit }\n", kb);
+			continue;
+		}
+
+		hdr_printed = false;
+		for (j = 0; j < OBD_HIST_MAX; j++) {
+			r = percpu_counter_sum(&h->oh_pc_buckets[j]);
+			if (r == 0)
+				continue;
+
+			if (!hdr_printed) {
+				seq_printf(seq, "rd_%uK: { ", kb);
+				hdr_printed = true;
+			}
+			seq_printf(seq, "%dus: %llu, ",
+				   (j == 0) ? 0 : 1 << (j - 1),
+				   binary_usec_to_dec(r));
+		}
+		if (hdr_printed)
+			seq_puts(seq, "}\n");
+	}
+
+	/* Print write latency histograms */
+	for (i = 0, kb = PAGE_SIZE / 1024; i < num_buckets; i++, kb <<= 1) {
+		u64 w;
+
+		h = &bs->bs_write_io_latency_by_size[i];
+		if (unlikely(!h->oh_initialized)) {
+			seq_printf(seq, "wr_%uK: { uninit }\n", kb);
+			continue;
+		}
+
+		hdr_printed = false;
+		for (j = 0; j < OBD_HIST_MAX; j++) {
+
+			w = percpu_counter_sum(&h->oh_pc_buckets[j]);
+			if (w == 0)
+				continue;
+
+			if (!hdr_printed) {
+				seq_printf(seq, "wr_%uK: { ", kb);
+				hdr_printed = true;
+			}
+			seq_printf(seq, "%dus: %llu, ",
+				   (j == 0) ? 0 : 1 << (j - 1),
+				   binary_usec_to_dec(w));
+		}
+		if (hdr_printed)
+			seq_puts(seq, "}\n");
+	}
+
+	spin_unlock(&bs->bs_loi_list_lock);
+
+	return 0;
+}
+
+static ssize_t io_latency_stats_seq_write(struct file *file,
+					  const char __user *buf,
+					  size_t len, loff_t *off)
+{
+	struct seq_file *seq = file->private_data;
+	struct brw_stats *bs = seq->private;
+	int i;
+
+	spin_lock(&bs->bs_loi_list_lock);
+	bs->bs_io_latency_init = ktime_get_real();
+	for (i = 0; i < IO_LATENCY_BUCKETS; i++) {
+		if (likely(bs->bs_read_io_latency_by_size[i].oh_initialized))
+			lprocfs_oh_clear_pcpu(&bs->bs_read_io_latency_by_size[i]);
+	}
+
+	for (i = 0; i < IO_LATENCY_BUCKETS; i++) {
+		if (likely(bs->bs_write_io_latency_by_size[i].oh_initialized))
+			lprocfs_oh_clear_pcpu(&bs->bs_write_io_latency_by_size[i]);
+	}
+	spin_unlock(&bs->bs_loi_list_lock);
+
+	return len;
+}
+LDEBUGFS_SEQ_FOPS(io_latency_stats);
+
+int lprocfs_init_brw_stats(struct brw_stats *bs)
+{
+	int i, rc;
+
+	bs->bs_init = ktime_get_real();
 	for (i = 0; i < BRW_RW_STATS_NUM; i++) {
-		result = lprocfs_oh_alloc_pcpu(&brw_stats->bs_hist[i]);
-		if (result)
+		rc = lprocfs_oh_alloc_pcpu(&bs->bs_hist[i]);
+		if (rc)
 			break;
 	}
 
-	return result;
+	spin_lock_init(&bs->bs_loi_list_lock);
+
+	/* verify IO_LATENCY_BUCKETS is large enough for all RPCs */
+	BUILD_BUG_ON(IO_LATENCY_BUCKETS < PTLRPC_MAX_BRW_BITS - PAGE_SHIFT);
+
+	/* initialize RPC latency by size histograms */
+	bs->bs_io_latency_init = ktime_get_real();
+	for (i = 0; i < IO_LATENCY_BUCKETS; i++) {
+		rc = lprocfs_oh_alloc_pcpu(&bs->bs_read_io_latency_by_size[i]);
+		if (rc) {
+			CWARN("%s: can't init read pcpu stats idx = %u\n",
+			      bs->bs_devname, i);
+			break;
+		}
+	}
+
+	for (i = 0; i < IO_LATENCY_BUCKETS; i++) {
+		rc = lprocfs_oh_alloc_pcpu(&bs->bs_write_io_latency_by_size[i]);
+		if (rc) {
+			CWARN("%s: can't init write pcpu stats idx = %u\n",
+			      bs->bs_devname, i);
+			break;
+		}
+	}
+
+	return rc;
 }
 EXPORT_SYMBOL(lprocfs_init_brw_stats);
 
-void lprocfs_fini_brw_stats(struct brw_stats *brw_stats)
+void lprocfs_fini_brw_stats(struct brw_stats *bs)
 {
 	int i;
 
 	for (i = 0; i < BRW_RW_STATS_NUM; i++)
-		lprocfs_oh_release_pcpu(&brw_stats->bs_hist[i]);
+		lprocfs_oh_release_pcpu(&bs->bs_hist[i]);
+
+	for (i = 0; i < IO_LATENCY_BUCKETS; i++) {
+		lprocfs_oh_release_pcpu(&bs->bs_read_io_latency_by_size[i]);
+		lprocfs_oh_release_pcpu(&bs->bs_write_io_latency_by_size[i]);
+	}
 }
 EXPORT_SYMBOL(lprocfs_fini_brw_stats);
 
@@ -858,7 +988,6 @@ void ldebugfs_register_brw_stats(struct dentry *parent,
 	int i;
 
 	LASSERT(brw_stats);
-	brw_stats->bs_init = ktime_get_real();
 	for (i = 0; i < BRW_RW_STATS_NUM; i++) {
 		struct brw_stats_props *props = brw_stats->bs_props;
 
@@ -876,6 +1005,17 @@ void ldebugfs_register_brw_stats(struct dentry *parent,
 			    &brw_stats_fops);
 }
 EXPORT_SYMBOL(ldebugfs_register_brw_stats);
+
+void ldebugfs_register_io_latency_stats(struct dentry *parent,
+					struct brw_stats *brw_stats)
+{
+	if (!parent)
+		return;
+
+	debugfs_create_file("io_latency_stats", 0644, parent, brw_stats,
+			    &io_latency_stats_fops);
+}
+EXPORT_SYMBOL(ldebugfs_register_io_latency_stats);
 
 int lprocfs_hash_seq_show(struct seq_file *m, void *data)
 {

lustre/osc/lproc_osc.c
@@ -798,10 +798,12 @@ static int osc_rpc_stats_seq_show(struct seq_file *seq, void *v)
 	write_lat_cum = 0;
 	for (i = 0; i < OBD_HIST_MAX; i++) {
 		unsigned long read_lat =
-			cli->cl_read_io_latency_hist.oh_buckets[i] * 1024 / 1000;
+			cli->cl_read_io_latency_hist.oh_buckets[i];
 		unsigned long write_lat =
-			cli->cl_write_io_latency_hist.oh_buckets[i] * 1024 / 1000;
+			cli->cl_write_io_latency_hist.oh_buckets[i];
 
+		read_lat = binary_usec_to_dec(read_lat);
+		write_lat = binary_usec_to_dec(write_lat);
 		read_lat_cum += read_lat;
 		write_lat_cum += write_lat;
 		seq_printf(seq, "%d:\t\t%10lu %3u %3u   | %10lu %3u %3u\n",

lustre/osc/osc_request.c
@@ -2687,7 +2687,8 @@ static int brw_interpret(const struct lu_env *env,
 	/* Calculate RPC latency in microseconds and update histogram */
 	if (ktime_to_ns(start_time)) {
 		/* binary convertion, must convert to decimal for display */
-		ktime_t latency_us = ktime_sub(completion_time, start_time) >> 10;
+		ktime_t latency = ktime_sub(completion_time, start_time);
+		unsigned int latency_us = ktime_to_ns(latency) >> 10 ?: 1;
 		u32 page_count = aa->aa_page_count;
 		int idx;
 

lustre/osd-ldiskfs/osd_handler.c
@@ -8734,6 +8734,7 @@ static int osd_device_init0(const struct lu_env *env,
 	rc = lprocfs_init_brw_stats(&o->od_brw_stats);
 	if (rc)
 		GOTO(out_brw_stats, rc);
+	o->od_brw_stats.bs_devname = o->od_svname;
 
 	/* setup scrub, including OI files initialization */
 	o->od_in_init = 1;

lustre/osd-ldiskfs/osd_io.c
@@ -178,21 +178,38 @@ static void osd_iobuf_add_page(struct osd_iobuf *iobuf,
 
 void osd_fini_iobuf(struct osd_device *d, struct osd_iobuf *iobuf)
 {
+	struct brw_stats *bs = &d->od_brw_stats;
+	struct obd_hist_pcpu *stats = NULL;
+	unsigned int latency_us;
+	int page_count = iobuf->dr_npages;
+	int idx = fls(page_count) - 1;
 	int rw = iobuf->dr_rw;
 
-	if (iobuf->dr_elapsed_valid) {
-		struct brw_stats *h = &d->od_brw_stats;
-
-		iobuf->dr_elapsed_valid = 0;
-		LASSERT(iobuf->dr_dev == d);
-		LASSERT(iobuf->dr_frags > 0);
-		lprocfs_oh_tally_pcpu(&h->bs_hist[BRW_R_DIO_FRAGS+rw],
-				      iobuf->dr_frags);
-		lprocfs_oh_tally_log2_pcpu(&h->bs_hist[BRW_R_IO_TIME+rw],
-					   ktime_to_ms(iobuf->dr_elapsed));
-	}
-
 	iobuf->dr_error = 0;
+
+	if (!iobuf->dr_elapsed_valid)
+		return;
+	if (unlikely(idx < 0)) {
+		CDEBUG(D_PAGE, "%s: histogram index %d < 0\n",
+		       d->od_svname, idx);
+		idx = 0;
+	}
+	iobuf->dr_elapsed_valid = 0;
+
+	lprocfs_oh_tally_pcpu(&bs->bs_hist[BRW_R_DIO_FRAGS + rw],
+			      iobuf->dr_frags);
+	lprocfs_oh_tally_log2_pcpu(&bs->bs_hist[BRW_R_IO_TIME + rw],
+				   ktime_to_ms(iobuf->dr_elapsed));
+	if (unlikely(idx >= IO_LATENCY_BUCKETS))
+		idx = IO_LATENCY_BUCKETS - 1;
+	latency_us = ktime_to_ns(iobuf->dr_elapsed) >> 10;
+
+	if (rw == READ)
+		stats = bs->bs_read_io_latency_by_size;
+	else if (rw == WRITE)
+		stats = bs->bs_write_io_latency_by_size;
+	if (likely(stats && stats[idx].oh_initialized))
+		lprocfs_oh_tally_log2_pcpu(stats + idx, latency_us);
 }
 
 void osd_bio_fini(struct bio *bio)

lustre/osd-ldiskfs/osd_lproc.c
@@ -127,6 +127,9 @@ static int osd_stats_init(struct osd_device *osd)
 
 	ldebugfs_register_brw_stats(osd->od_dt_dev.dd_debugfs_entry,
 				    &osd->od_brw_stats);
+	/* only for osd-ldiskfs until osd-zfs is fixed, then merge into above */
+	ldebugfs_register_io_latency_stats(osd->od_dt_dev.dd_debugfs_entry,
+					   &osd->od_brw_stats);
 
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 	osd_symlink_brw_stats(osd);

lustre/osd-zfs/osd_handler.c
@@ -1257,6 +1257,7 @@ static int osd_mount(const struct lu_env *env,
 	rc = lprocfs_init_brw_stats(&o->od_brw_stats);
 	if (rc)
 		GOTO(err, rc);
+	o->od_brw_stats.bs_devname = o->od_svname;
 
 	o->od_in_init = 1;
 	rc = osd_scrub_setup(env, o, interval, resetoi);

lustre/tests/sanity.sh
@@ -17262,6 +17262,16 @@ test_127_io_latency_test() {
 	local io_latency_param=$2
 	local dev=osc
 
+	[[ $facet == *ost* ]] && {
+		# disable cache to have read IO
+		local rcc="osd-*.$FSNAME-OST0000.read_cache_enable"
+		local wcc="osd-*.$FSNAME-OST0000.writethrough_cache_enable"
+		local rsaved=$(do_facet $facet $LCTL get_param -n $rcc)
+		local wsaved=$(do_facet $facet $LCTL get_param -n $wcc)
+		do_facet $facet $LCTL set_param $rcc=0 $wcc=0
+		stack_trap "do_facet $facet $LCTL set_param $rcc=$rsaved $wcc=$wsaved"
+	}
+
 	[[ $io_latency_param =~ "md[ct]" ]] && dev="mdc"
 
 	# avoid leftovers after preceding tests
@@ -17270,14 +17280,17 @@ test_127_io_latency_test() {
 
 	# Clear RPC stats
 	do_facet $facet $LCTL set_param $io_latency_param=clear
+	[[ $facet == *ost* ]] &&
+		do_facet $facet $LCTL set_param osd-*.*OST*.stats=clear
 
 	# Generate I/O with different sizes to populate multiple histograms
 	local file=$DIR/$tdir/$tfile
 	local count=10
 	local sizes=()
 	local mppr=($($LCTL get_param -n $dev.*.max_pages_per_rpc))
-	for size in 4 64 1024 4096; do
-		(( size >= PAGE_SIZE && $mppr * $PAGE_SIZE / 1024 >= size )) &&
+	for size in 4 64 1024; do
+		(( size >= $PAGE_SIZE / 1024 &&
+		   size <= $mppr * $PAGE_SIZE / 1024 )) &&
 			sizes+=(${size}K)
 	done
 
@@ -17288,12 +17301,13 @@ test_127_io_latency_test() {
 		echo "Generating I/O with size $size"
 
 		# Write with current size
-		[[ $dev == "mdc" ]] && $LFS setstripe -E 4M -L mdt ${file}_${size} ||
-				       $LFS setstripe -c 1 -i 0 ${file}_${size}
+		[[ $dev == "mdc" ]] &&
+			$LFS setstripe -E 4M -L mdt ${file}_${size} ||
+			$LFS setstripe -c 1 -i 0 ${file}_${size}
 		dd if=/dev/urandom of=${file}_${size} bs=$size count=$count \
 			oflag=direct ||
 			error "dd write failed for size $size"
-		sync
+		cancel_lru_locks osc
 
 		# Read with current size
 		dd if=${file}_${size} of=/dev/null bs=$size count=$count \
@@ -17301,6 +17315,7 @@ test_127_io_latency_test() {
 			error "dd read failed for size $size"
 	done
 
+	sleep 5
 	# Get client IO latency stats for debugging
 	local io_latency_stats=$(do_facet $facet $LCTL get_param $io_latency_param)
 	echo "$io_latency_param stats after I/O:"
@@ -17308,33 +17323,49 @@ test_127_io_latency_test() {
 
 	# Check that we have entries for different sizes
 	for size in ${sizes[@]}; do
-		echo "$io_latency_stats" | grep -q _$size ||
+		echo "$io_latency_stats" | grep -q _$size || {
+			$LCTL get_param osc.*.{io_latency,rpc}_stats
+			[[ $facet == *ost* ]] &&
+				do_facet ost1 $LCTL get_param osd-*.*.{brw,io_latency}_stats &&
+				do_facet ost1 $LCTL get_param osd-*.*OST*.stats
 			error "No stats found for size $size"
+		}
 	done
 
+	# sometimes stray writes are seen, filter to expected sizes
 	local check_counts=$(echo "$io_latency_stats" |
+		grep -E "$(sed -e 's/ /K|_/g' <<<${sizes[*]})" |
 		awk -v count="$count" '/K: / {sum = 0
 		while (match($0, /[0-9]+us: ([0-9]+),/, arr)) {
 			sum += arr[1]
 			$0 = substr($0, RSTART + RLENGTH)
 		}
-		{ if (sum != count) print "Expected "count" entries, found "sum}
+		{ if (sum < count) print "Expected "count" entries, found "sum}
 		}')
 	[[ -z "$check_counts" ]] || error "$check_counts"
 
 	local has_read=$(echo "$io_latency_stats" | grep -c "rd_")
 	local has_write=$(echo "$io_latency_stats" | grep -c "wr_")
 
-	(( has_read == ${#sizes[@]} )) ||
+	(( has_read >= ${#sizes[@]} )) ||
 		error "Expected ${#sizes[@]} read stats, found $has_read"
-	(( has_write == ${#sizes[@]} )) ||
+	(( has_write >= ${#sizes[@]} )) ||
 		error "Expected ${#sizes[@]} write stats, found $has_write"
 }
 
 test_127e() {
-	test_127_io_latency_test client osc.*.io_latency_stats
+	test_127_io_latency_test client osc.$FSNAME-OST0000-*.io_latency_stats
 }
 run_test 127e "client IO latency histograms by size"
+
+test_127f() {
+	(( $OST1_VERSION >= $(version_code 2.16.58.105) )) ||
+		skip "need OST > 2.16.58.105 for OST IO latency stats"
+	[[ $(facet_fstype ost1) == ldiskfs ]] || skip "ldiskfs only"
+
+	test_127_io_latency_test ost1 osd-*.$FSNAME-OST0000.io_latency_stats
+}
+run_test 127f "OST IO latency histograms by size"
 
 test_128() { # bug 15212
 	touch $DIR/$tfile