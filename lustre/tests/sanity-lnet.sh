lustre/lustre-release

commit 70c5f58403e4634cda9df357ca1e41076cc00442

LU-18444 lnet: Use only NI status for route aliveness
lnet_is_route_alive() considers a route to be alive if the associated
gateway has at least one peer NI on both the local and remote nets
that is alive. A peer NI is alive if the NI status is UP and its
health value is at LNET_MAX_HEALTH_VALUE.

Unfortunately, LNet health values do not accurately reflect whether a
route is actually up or down. For example, when a router is forwarding
a PUT w/ACK to a peer that is down, the ACK will not be sent back to
the message originator. This will result in the originator
decrementing the health value of the router peer NI. If this is the
only NI on the local net then this route will be considered dead until
the health value recovers.

It is typical for the number of clients to greatly outnumber the
number of routers. When many clients are doing IO, and those clients
crash or otherwise become unresponsive, this can result in servers
temporarily losing many routes as they hit response timeouts.

To address this issue we no longer consider health values when
determining whether a router peer NI, and thus a route, is considered
alive. Instead we rely solely on the NI status.

Health values of router peer NIs are now considered in
lnet_compare_gw_lpnis() so that we will prefer healthier routes.

lnet_find_route_locked() is lightly re-factored to remove some
code duplication.

The description of router_sensitivity_percentage is modified to note
that this parameter is now deprecated.

New test added to verify that routes stay up when the health value of
router NIs is decremented.

HPE-bug-id: LUS-12539
Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ic27b440bd296686262d4f3bad80cd0de95a32540
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57492
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/include/lnet/lib-lnet.h
@@ -1172,20 +1172,12 @@ lnet_ni_set_next_ping(struct lnet_ni *ni, time64_t now)
 	ni->ni_next_ping = lnet_get_next_recovery_ping(ni->ni_ping_count, now);
 }
 
-/*
- * A peer NI is alive if it satisfies the following two conditions:
- *  1. peer NI health >= LNET_MAX_HEALTH_VALUE * router_sensitivity_percentage
- *  2. the cached NI status received when we discover the peer is UP
+/* We consider a peer NI to be alive if its cached NI status is UP
  */
 static inline bool
 lnet_is_peer_ni_alive(struct lnet_peer_ni *lpni)
 {
-	bool halive = false;
-
-	halive = (atomic_read(&lpni->lpni_healthv) >=
-		 (LNET_MAX_HEALTH_VALUE * router_sensitivity_percentage / 100));
-
-	return halive && lpni->lpni_ns_status == LNET_NI_STATUS_UP;
+	return lpni->lpni_ns_status == LNET_NI_STATUS_UP;
 }
 
 static inline void

lnet/lnet/lib-move.c
@@ -1450,9 +1450,39 @@ lnet_find_best_lpni(struct lnet_ni *lni, struct lnet_nid *dst_nid,
 	return NULL;
 }
 
+/* Compare router peer NIs health, outstanding traffic, and available credits.
+ * \retval 1	If \lpni1 is the better peer NI
+ * \retval -1	If \lpni2 is the better peer NI
+ * \retval 0	If \lpni1 and lpni2 are equally good peer NIs
+ */
 static int
 lnet_compare_gw_lpnis(struct lnet_peer_ni *lpni1, struct lnet_peer_ni *lpni2)
 {
+	int lpni1_healthv;
+	int lpni2_healthv;
+
+	if (lpni1 && !lpni2)
+		return 1;
+
+	if (!lpni1 && lpni2)
+		return -1;
+
+	lpni1_healthv = atomic_read(&lpni1->lpni_healthv);
+	lpni2_healthv = atomic_read(&lpni2->lpni_healthv);
+
+	CDEBUG(D_NET, "n:[%s, %s] h:[%d, %d] q:[%ld, %ld] c:[%d, %d]\n",
+	       libcfs_nidstr(&lpni1->lpni_nid),
+	       libcfs_nidstr(&lpni2->lpni_nid),
+	       lpni1_healthv, lpni2_healthv,
+	       lpni1->lpni_txqnob, lpni2->lpni_txqnob,
+	       lpni1->lpni_txcredits, lpni2->lpni_txcredits);
+
+	if (lpni1_healthv > lpni2_healthv)
+		return 1;
+
+	if (lpni1_healthv < lpni2_healthv)
+		return -1;
+
 	if (lpni1->lpni_txqnob < lpni2->lpni_txqnob)
 		return 1;
 
@@ -1468,18 +1498,45 @@ lnet_compare_gw_lpnis(struct lnet_peer_ni *lpni1, struct lnet_peer_ni *lpni2)
 	return 0;
 }
 
-/* Compare route priorities and hop counts */
+/* Compare route UDSP, priorities, and hop counts.
+ * \retval 1	If \r1 is the better route
+ * \retval -1	If \r2 is the better route
+ * \retval 0	If \r1 and \r2 are equally good routes
+ */
 static int
-lnet_compare_routes(struct lnet_route *r1, struct lnet_route *r2)
+lnet_compare_routes(struct lnet_route *r1, struct lnet_route *r2,
+		    bool r1_preferred, bool r2_preferred)
 {
-	int r1_hops = (r1->lr_hops == LNET_UNDEFINED_HOPS) ? 1 : r1->lr_hops;
-	int r2_hops = (r2->lr_hops == LNET_UNDEFINED_HOPS) ? 1 : r2->lr_hops;
+	int r1_hops;
+	int r2_hops;
+
+	if (r1 && !r2)
+		return 1;
+
+	if (!r1 && r2)
+		return -1;
+
+	CDEBUG(D_NET, "n:[%s, %s] u:[%s, %s] p:[%u, %u] h:[%u, %u]\n",
+	       libcfs_nidstr(&r1->lr_gateway->lp_primary_nid),
+	       libcfs_nidstr(&r2->lr_gateway->lp_primary_nid),
+	       r1_preferred ? "Y" : "N", r2_preferred ? "Y" : "N",
+	       r1->lr_priority, r2->lr_priority,
+	       r1->lr_hops, r2->lr_hops);
+
+	if (r1_preferred && !r2_preferred)
+		return 1;
+
+	if (!r1_preferred && r2_preferred)
+		return -1;
 
 	if (r1->lr_priority < r2->lr_priority)
 		return 1;
 
 	if (r1->lr_priority > r2->lr_priority)
 		return -1;
+
+	r1_hops = (r1->lr_hops == LNET_UNDEFINED_HOPS) ? 1 : r1->lr_hops;
+	r2_hops = (r2->lr_hops == LNET_UNDEFINED_HOPS) ? 1 : r2->lr_hops;
 
 	if (r1_hops < r2_hops)
 		return 1;
@@ -1497,11 +1554,12 @@ lnet_find_route_locked(struct lnet_remotenet *rnet, __u32 src_net,
 		       struct lnet_peer_ni **gwni)
 {
 	struct lnet_peer_ni *lpni, *best_gw_ni = NULL;
-	struct lnet_route *best_route;
-	struct lnet_route *last_route;
+	struct lnet_route *best_route = NULL;
+	struct lnet_route *last_route = NULL;
 	struct lnet_route *route;
 	int rc;
 	bool best_rte_is_preferred = false;
+	bool rte_is_preferred = false;
 	struct lnet_nid *gw_pnid;
 
 	CDEBUG(D_NET, "Looking up a route to %s, from %s\n",
@@ -1511,70 +1569,31 @@ lnet_find_route_locked(struct lnet_remotenet *rnet, __u32 src_net,
 	list_for_each_entry(route, &rnet->lrn_routes, lr_list) {
 		if (!lnet_is_route_alive(route))
 			continue;
+
 		gw_pnid = &route->lr_gateway->lp_primary_nid;
 
-		/* no protection on below fields, but it's harmless */
-		if (last_route && (last_route->lr_seq - route->lr_seq < 0))
+		/* Track which route has the highest sequence number. i.e. the
+		 * last route that was used. This information is used by our
+		 * caller to increment the sequence number of the route we'll
+		 * select.
+		 * no protection on below fields, but it's harmless
+		 */
+		if (last_route && (last_route->lr_seq < route->lr_seq))
 			last_route = route;
 
-		/* if the best route found is in the preferred list then
-		 * tag it as preferred and use it later on. But if we
-		 * didn't find any routes which are on the preferred list
-		 * then just use the best route possible.
+		/* Check whether this router is on the destination peer NI's
+		 * preferred list
 		 */
-		rc = lnet_peer_is_pref_rtr_locked(remote_lpni, gw_pnid);
+		rte_is_preferred = lnet_peer_is_pref_rtr_locked(remote_lpni,
+								gw_pnid);
 
-		if (!best_route || (rc && !best_rte_is_preferred)) {
-			/* Restrict the selection of the router NI on the
-			 * src_net provided. If the src_net is LNET_NID_ANY,
-			 * then select the best interface available.
-			 */
-			lpni = lnet_find_best_lpni(NULL, NULL,
-						   route->lr_gateway,
-						   src_net);
-			if (!lpni) {
-				CDEBUG(D_NET,
-				       "Gateway %s does not have a peer NI on net %s\n",
-				       libcfs_nidstr(gw_pnid),
-				       libcfs_net2str(src_net));
-				continue;
-			}
-		}
-
-		if (rc && !best_rte_is_preferred) {
-			/* This is the first preferred route we found,
-			 * so it beats any route found previously
-			 */
-			best_route = route;
-			if (!last_route)
-				last_route = route;
-			best_gw_ni = lpni;
-			best_rte_is_preferred = true;
-			CDEBUG(D_NET, "preferred gw = %s\n",
-			       libcfs_nidstr(gw_pnid));
-			continue;
-		} else if ((!rc) && best_rte_is_preferred)
-			/* The best route we found so far is in the preferred
-			 * list, so it beats any non-preferred route
-			 */
-			continue;
-
-		if (!best_route) {
-			best_route = last_route = route;
-			best_gw_ni = lpni;
-			continue;
-		}
-
-		rc = lnet_compare_routes(route, best_route);
+		rc = lnet_compare_routes(route, best_route, rte_is_preferred,
+					 best_rte_is_preferred);
 		if (rc == -1)
 			continue;
 
-		/* Restrict the selection of the router NI on the
-		 * src_net provided. If the src_net is LNET_NID_ANY,
-		 * then select the best interface available.
-		 */
-		lpni = lnet_find_best_lpni(NULL, NULL, route->lr_gateway,
-					   src_net);
+		lpni = lnet_find_best_lpni(NULL, NULL,
+					   route->lr_gateway, src_net);
 		if (!lpni) {
 			CDEBUG(D_NET,
 			       "Gateway %s does not have a peer NI on net %s\n",
@@ -1583,25 +1602,31 @@ lnet_find_route_locked(struct lnet_remotenet *rnet, __u32 src_net,
 			continue;
 		}
 
-		if (rc == 1) {
-			best_route = route;
-			best_gw_ni = lpni;
-			continue;
+		/* Only need to compare the peer NIs if lnet_compare_routes()
+		 * returned zero.
+		 */
+		if (rc == 0) {
+			rc = lnet_compare_gw_lpnis(lpni, best_gw_ni);
+			if (rc == -1 ||
+			    (rc == 0 && route->lr_seq > best_route->lr_seq))
+				continue;
 		}
 
-		rc = lnet_compare_gw_lpnis(lpni, best_gw_ni);
-		if (rc == -1)
-			continue;
-
-		if (rc == 1 || route->lr_seq <= best_route->lr_seq) {
-			best_route = route;
-			best_gw_ni = lpni;
-			continue;
-		}
+		best_route = route;
+		best_gw_ni = lpni;
+		best_rte_is_preferred = rte_is_preferred;
+		if (!last_route)
+			last_route = route;
 	}
 
 	*prev_route = last_route;
 	*gwni = best_gw_ni;
+
+	if (best_gw_ni)
+		CDEBUG(D_NET,
+		       "Selected next-hop %s(%p) on src net %s\n",
+		       libcfs_nidstr(&best_gw_ni->lpni_nid), best_gw_ni,
+		       libcfs_net2str(src_net));
 
 	return best_route;
 }

lustre/tests/sanity-lnet.sh
@@ -3763,6 +3763,42 @@ test_227() {
 }
 run_test 227 "Check router peer health w/DD disabled"
 
+test_228() {
+	setup_router_test -r 2 || return $?
+
+	do_basic_rtr_test || return $?
+
+	do_rpc_nodes $HOSTNAME,${RPEERS[0]} load_module \
+		../lnet/selftest/lnet_selftest ||
+			error "Failed to load lnet-selftest module"
+
+	local lstpid rc
+
+	$LSTSH -H -t $HOSTNAME -f ${RPEERS[0]} -m rw &
+	lstpid=$!
+
+	do_lnetctl peer set --health 500 --all ||
+		error "Failed to set peer NI health rc = $?"
+
+	wait $lstpid
+	rc=$?
+
+	((rc == 0)) ||
+		error "LST returned non-zero rc = $rc"
+
+	do_lnetctl peer set --health 1000 --all ||
+		error "Failed to set peer NI health rc = $?"
+
+	local no_route=$(dmesg | tail | grep lnet_handle_find_routed_path |
+			 grep -c "no route")
+
+	((no_route == 0)) ||
+		error "Detected no route send failures"
+
+	cleanup_router_test || return $?
+}
+run_test 228 "Routes should stay up when health is decremented"
+
 test_230() {
 	[[ ${NETTYPE} == tcp* ]] || skip "Need tcp NETTYPE"
 

lnet/lnet/router.c
@@ -103,7 +103,7 @@ module_param_call(router_sensitivity_percentage, rtr_sensitivity_set, param_get_
 		  &router_sensitivity_percentage, S_IRUGO|S_IWUSR);
 #endif
 MODULE_PARM_DESC(router_sensitivity_percentage,
-		"How healthy a gateway should be to be used in percent");
+		"(Deprecated) How healthy a gateway should be to be used in percent");
 
 static void lnet_add_route_to_rnet(struct lnet_remotenet *rnet,
 				   struct lnet_route *route);

commit bb6237483d50cf67f9743f62bccd83cf377f0a81

LU-19827 lnet: Fix MR forwarding
The change https://review.whamcloud.com/57492 introduced a regression
in the multi-rail forwarding code. MR forwarding allows a router to
select a different target interface for a message when the one chosen
by the sender is down or unhealthy.

Prior to the above change, lnet_is_peer_ni_alive() would check both
the down and unhealthy criteria, but now it only checks the down
criteria. We need to add a check for the peer NI's health value.

There was also a second bug here where we were inadvertently checking
the MR capability of the destination peer rather than the source peer.
This is also fixed.

Add sanity-lnet tests to validate MR forwarding feature

Test-Parameters: trivial
Fixes: 70c5f58403 ("LU-18444 lnet: Use only NI status for route aliveness")
Fixes: b0e8ab1a5f ("LU-13606 lnet: Allow router to forward to healthier NID")
HPE-bug-id: LUS-13084
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Idaa40618d1df4c6793266c7cf9ae0d63a5a8bff7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63582
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

lnet/include/lnet/lib-lnet.h
@@ -19,6 +19,7 @@
 #define CFS_FAIL_MATCH_MD_NID		0xe001
 #define CFS_FAIL_DELAY_MSG_FORWARD	0xe002
 #define CFS_FAIL_TEST_PING_MD		0xe003
+#define CFS_FAIL_RTR_HEALTH_INC		0xe004
 
 #include <lustre_compat/linux/generic-radix-tree.h>
 #include <lustre_compat/linux/hash.h>

lnet/lnet/lib-move.c
@@ -2999,6 +2999,38 @@ lnet_handle_send_case_locked(struct lnet_send_data *sd)
 	}
 }
 
+/* Determine whether to allow MR forwarding for this message.
+ * NB: MR forwarding is allowed if the message originator and the
+ * destination are both MR capable, and the destination lpni that was
+ * originally chosen by the originator is unhealthy or down.
+ * The MR status of the destination lpni is checked later during path selection.
+ */
+static bool
+lnet_mr_forwarding_allowed(struct lnet_peer_ni *dst_lpni,
+			   struct lnet_nid *src_nid, int cpt)
+{
+	struct lnet_peer_ni *src_lpni;
+	bool mr_forwarding_allowed;
+
+	src_lpni = lnet_peerni_by_nid_locked(src_nid, NULL, cpt);
+	if (IS_ERR(src_lpni))
+		return false;
+
+	if (!lnet_peer_is_multi_rail(src_lpni->lpni_peer_net->lpn_peer))
+		mr_forwarding_allowed = false;
+	else if (!lnet_is_peer_ni_alive(dst_lpni))
+		mr_forwarding_allowed = true;
+	else if (atomic_read(&dst_lpni->lpni_healthv) < LNET_MAX_HEALTH_VALUE)
+		mr_forwarding_allowed = true;
+	else
+		mr_forwarding_allowed = false;
+
+	/* Drop ref taken by lnet_peerni_by_nid_locked() */
+	lnet_peer_ni_decref_locked(src_lpni);
+
+	return mr_forwarding_allowed;
+}
+
 static int
 lnet_select_pathway(struct lnet_nid *src_nid,
 		    struct lnet_nid *dst_nid,
@@ -3109,31 +3141,12 @@ again:
 	if (msg->msg_routing && (send_case & LOCAL_DST))
 		final_hop = true;
 
-	/* Determine whether to allow MR forwarding for this message.
-	 * NB: MR forwarding is allowed if the message originator and the
-	 * destination are both MR capable, and the destination lpni that was
-	 * originally chosen by the originator is unhealthy or down.
-	 * We check the MR capability of the destination further below
-	 */
 	mr_forwarding_allowed = false;
 	if (final_hop) {
-		struct lnet_peer *src_lp;
-		struct lnet_peer_ni *src_lpni;
+		if (lnet_mr_forwarding_allowed(lpni, &msg->msg_hdr.src_nid,
+					       cpt))
+			mr_forwarding_allowed = true;
 
-		src_lpni = lnet_peerni_by_nid_locked(&msg->msg_hdr.src_nid,
-						   NULL, cpt);
-		/* We don't fail the send if we hit any errors here. We'll just
-		 * try to send it via non-multi-rail criteria
-		 */
-		if (!IS_ERR(src_lpni)) {
-			/* Drop ref taken by lnet_peerni_by_nid_locked() */
-			lnet_peer_ni_decref_locked(src_lpni);
-			src_lp = lpni->lpni_peer_net->lpn_peer;
-			if (lnet_peer_is_multi_rail(src_lp) &&
-			    !lnet_is_peer_ni_alive(lpni))
-				mr_forwarding_allowed = true;
-
-		}
 		CDEBUG(D_NET, "msg %p MR forwarding %s\n", msg,
 		       mr_forwarding_allowed ? "allowed" : "not allowed");
 	}

lnet/lnet/lib-msg.c
@@ -13,6 +13,7 @@
 
 #define DEBUG_SUBSYSTEM S_LNET
 
+#include <linux/libcfs/libcfs_fail.h>
 #include <lnet/lib-lnet.h>
 
 void
@@ -898,7 +899,8 @@ lnet_health_check(struct lnet_msg *msg)
 			 * I'm a router, then set that lpni's health to
 			 * maximum so we can commence communication
 			 */
-			if (lnet_isrouter(lpni) || lnet_routing_enabled()) {
+			if ((lnet_isrouter(lpni) || lnet_routing_enabled()) &&
+			     likely(!CFS_FAIL_CHECK(CFS_FAIL_RTR_HEALTH_INC))) {
 				lnet_set_lpni_healthv_locked(lpni,
 					LNET_MAX_HEALTH_VALUE);
 			} else {

lustre/tests/sanity-lnet.sh
@@ -1448,19 +1448,23 @@ declare -A ROUTER_NIDS
 declare -A RPEER_NIDS
 LNIDS=()
 LOCAL_NET=${NETTYPE}
+LOCAL_NET2=${NETTYPE}2
 REMOTE_NET=${NETTYPE}1
+REMOTE_NET2=${NETTYPE}3
 setup_router_test() {
 	(( $MDS1_VERSION >= $(version_code 2.15.0) )) ||
 		skip "need at least 2.15.0 for load_lnet"
 
 	local routers_required=1
 	local rpeers_required=1
+	local mr_peers=false
 	local flag
 
-	while getopts "r:p:" flag; do
+	while getopts "mp:r:" flag; do
 		case $flag in
-			r) routers_required="$OPTARG";;
+			m) mr_peers=true;;
 			p) rpeers_required="$OPTARG";;
+			r) routers_required="$OPTARG";;
 			*) ;;
 		esac
 	done
@@ -1496,6 +1500,12 @@ setup_router_test() {
 			return $?
 		do_net_add $router $REMOTE_NET ${router_interfaces[0]} ||
 			return $?
+		if $mr_peers; then
+			do_net_add $router $LOCAL_NET2 ${router_interfaces[0]} ||
+				return $?
+			do_net_add $router $REMOTE_NET2 ${router_interfaces[0]} ||
+				return $?
+		fi
 	done
 
 	for rpeer in ${!RPEER_INTERFACES[@]}; do
@@ -1503,10 +1513,18 @@ setup_router_test() {
 
 		do_net_add $rpeer $REMOTE_NET ${rpeer_interfaces[0]} ||
 			return $?
+		if $mr_peers; then
+			do_net_add $rpeer $REMOTE_NET2 ${rpeer_interfaces[0]} ||
+				return $?
+		fi
 	done
 
 	add_net $LOCAL_NET ${INTERFACES[0]} ||
 		return $?
+	if $mr_peers; then
+		add_net $LOCAL_NET2 ${INTERFACES[0]} ||
+			return $?
+	fi
 
 	for router in ${!ROUTER_INTERFACES[@]}; do
 		ROUTER_NIDS[$router]=$(do_node $router $LCTL list_nids \
@@ -4694,6 +4712,99 @@ test_236() {
 	cleanup_router_test
 }
 run_test 236 "Local NI state propagates to routers"
+
+do_mr_forwarding_test() {
+	local expect_mr_forwarding="$1"
+
+	local rpeer=${RPEERS[0]}
+	local rpeer_nids=( ${RPEER_NIDS[$rpeer]} )
+
+	do_lnetctl discover ${rpeer_nids[0]} ||
+		error "Discovery failed with rc = $?"
+
+	local router=${ROUTERS[0]}
+	local i
+
+#define CFS_FAIL_RTR_HEALTH_INC                0xe004
+	do_node $router "$LCTL set_param fail_loc=0xe004" ||
+		error "Failed to set fail_loc rc = $?"
+	do_node $router "$LNETCTL set health_sensitivity 0" ||
+		error "Failed to set health_sensitivity 0 rc = $?"
+	do_node $router "$LNETCTL peer set --health 0 --nid ${rpeer_nids[0]}" ||
+		error "Failed to set health 0 for ${rpeer_nids[0]} rc = $?"
+
+	local pre_sends=( $(do_node $router \
+			    "$LNETCTL peer show -v --nid ${rpeer_nids[0]}" |
+			    awk '/send_count/{print $NF}' | xargs echo) )
+
+	do_node $router "$LNETCTL peer show -v 2 | grep -e nid -e health"
+
+	local my_nid=$($LCTL list_nids | head -n 1)
+
+	for i in {1..50}; do
+		do_lnetctl ping --source ${my_nid} ${rpeer_nids[0]} ||
+			error "Ping failed with rc=$?"
+	done
+
+	local post_sends=( $(do_node $router \
+			     "$LNETCTL peer show -v --nid ${rpeer_nids[0]}" |
+			     awk '/send_count/{print $NF}' | xargs echo ) )
+
+	do_node $router "$LNETCTL peer show -v 2 | grep -e nid -e health"
+
+	echo "pre_sends: ${pre_sends[@]}"
+	echo "post_sends: ${post_sends[@]}"
+
+	do_node $router "$LCTL set_param fail_loc=0" ||
+		error "Failed to set fail_loc rc = $?"
+
+	if [[ ${#pre_sends[@]} != 2 ]] || [[ ${#post_sends[@]} != 2 ]]; then
+		error "Unexpected send counts"
+	fi
+
+	if $expect_mr_forwarding ; then
+		((post_sends[1] - pre_sends[1] >= 50)) ||
+			error "Expected ${post_sends[1]}-${pre_sends[1]} >= 50"
+	else
+		((post_sends[1] - pre_sends[1] < 50)) ||
+			error "Expected ${post_sends[1]}-${pre_sends[1]} < 50"
+	fi
+
+	return 0
+}
+
+test_237() {
+	setup_router_test -m || return $?
+
+	do_basic_rtr_test || return $?
+
+	do_mr_forwarding_test true || return $?
+
+	cleanup_router_test
+}
+run_test 237 "Check MR forwarding feature"
+
+test_238() {
+	setup_router_test -m
+
+	do_lnetctl set discovery 0 ||
+		error "Failed to disable discovery rc = $?"
+
+	do_basic_rtr_test || return $?
+
+	# Router should show non-MR for me
+	local my_nid=$($LCTL list_nids | head -n 1)
+	local mr=$(do_node ${ROUTERS[0]} "$LNETCTL peer show --nid $my_nid" |
+		   awk '/Multi-Rail:/{print $NF}')
+
+	[[ $mr == false ]] ||
+		error "Expect 'Multi-Rail: false', found $mr"
+
+	do_mr_forwarding_test false || return $?
+
+	cleanup_router_test
+}
+run_test 238 "Check MR forwarding feature skipped for non-MR source"
 
 test_241() {
 	reinit_dlc || return $?