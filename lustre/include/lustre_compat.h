lustre/lustre-release

commit 2c2a4091ff56677fece7d70dbd00b3699c1a98ed

LU-19179 build: remove all remaining 3.X kernel support
All kernels before 4.0 are no longer supported. Remove
the remain 3.X kernel support.

Test-Parameters: trivial
Change-Id: I4ffa3c64144682758a0ab3ef05b1021e61bc54bf
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63819
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_compat.h
@@ -44,17 +44,6 @@
 #endif
 
 #ifdef HAVE_BVEC_ITER
-#define bio_idx(bio)			(bio->bi_iter.bi_idx)
-#define bio_set_sector(bio, sector)	(bio->bi_iter.bi_sector = sector)
-#define bvl_to_page(bvl)		(bvl->bv_page)
-#else
-#define bio_idx(bio)			(bio->bi_idx)
-#define bio_set_sector(bio, sector)	(bio->bi_sector = sector)
-#define bio_sectors(bio)		((bio)->bi_size >> 9)
-#define bvl_to_page(bvl)		(bvl->bv_page)
-#endif
-
-#ifdef HAVE_BVEC_ITER
 #define bio_start_sector(bio) (bio->bi_iter.bi_sector)
 #else
 #define bio_start_sector(bio) (bio->bi_sector)
@@ -137,20 +126,10 @@ static inline int d_in_lookup(struct dentry *dentry)
 #define iterate_shared iterate
 #endif
 
-#ifdef HAVE_VFS_RENAME_5ARGS
-#define ll_vfs_rename(a, b, c, d) vfs_rename(a, b, c, d, NULL)
-#elif defined HAVE_VFS_RENAME_6ARGS
-#define ll_vfs_rename(a, b, c, d) vfs_rename(a, b, c, d, NULL, 0)
-#else
-#define ll_vfs_rename(a, b, c, d) vfs_rename(a, b, c, d)
-#endif
-
 #ifdef HAVE_USER_NAMESPACE_ARG
 #define vfs_unlink(ns, dir, de) vfs_unlink(ns, dir, de, NULL)
-#elif defined HAVE_VFS_UNLINK_3ARGS
-#define vfs_unlink(ns, dir, de) vfs_unlink(dir, de, NULL)
 #else
-#define vfs_unlink(ns, dir, de) vfs_unlink(dir, de)
+#define vfs_unlink(ns, dir, de) vfs_unlink(dir, de, NULL)
 #endif
 
 #ifndef HAVE_MNT_IDMAP_ARG
@@ -180,18 +159,6 @@ static inline int ll_vfs_getattr(struct path *path, struct kstat *st,
 # define inode_lock(inode) mutex_lock(&(inode)->i_mutex)
 # define inode_unlock(inode) mutex_unlock(&(inode)->i_mutex)
 # define inode_trylock(inode) mutex_trylock(&(inode)->i_mutex)
-#endif
-
-#ifndef HAVE_PAGECACHE_GET_PAGE
-#define pagecache_get_page(mapping, index, fp, gfp) \
-	grab_cache_page_nowait(mapping, index)
-#endif
-
-#ifndef HAVE_TRUNCATE_INODE_PAGES_FINAL
-static inline void truncate_inode_pages_final(struct address_space *map)
-{
-	truncate_inode_pages(map, 0);
-}
 #endif
 
 #ifdef HAVE_U64_CAPABILITY
@@ -254,14 +221,6 @@ static inline int posix_acl_update_mode(struct inode *inode, umode_t *mode_p,
 }
 #endif /* HAVE_POSIX_ACL_UPDATE_MODE */
 #endif
-#endif
-
-#ifndef HAVE_IOV_ITER_TRUNCATE
-static inline void iov_iter_truncate(struct iov_iter *i, u64 count)
-{
-	if (i->count > count)
-		i->count = count;
-}
 #endif
 
 /*
@@ -423,11 +382,6 @@ static inline int ll_vfs_removexattr(struct dentry *dentry, struct inode *inode,
 #endif
 }
 
-/* until v3.19-rc5-3-gb4caecd48005 */
-#ifndef BDI_CAP_MAP_COPY
-#define BDI_CAP_MAP_COPY		0
-#endif
-
 /* from v4.1-rc2-56-g89e9b9e07a39, until v5.9-rc3-161-gf56753ac2a90 */
 #ifndef BDI_CAP_CGROUP_WRITEBACK
 #define BDI_CAP_CGROUP_WRITEBACK	0
@@ -443,7 +397,7 @@ static inline int ll_vfs_removexattr(struct dentry *dentry, struct inode *inode,
 #define BDI_CAP_WRITEBACK_ACCT		0
 #endif
 
-#define LL_BDI_CAP_FLAGS	(BDI_CAP_CGROUP_WRITEBACK | BDI_CAP_MAP_COPY | \
+#define LL_BDI_CAP_FLAGS	(BDI_CAP_CGROUP_WRITEBACK | \
 				 BDI_CAP_WRITEBACK | BDI_CAP_WRITEBACK_ACCT)
 
 #ifndef FALLOC_FL_COLLAPSE_RANGE
@@ -462,8 +416,8 @@ static inline int ll_vfs_removexattr(struct dentry *dentry, struct inode *inode,
 #define raw_cpu_ptr(p) __this_cpu_ptr(p)
 #endif
 
-#if defined(HAVE_DIRECTIO_ITER) || defined(HAVE_IOV_ITER_RW) || \
-	defined(HAVE_DIRECTIO_2ARGS) || defined(HAVE_IOV_ITER_GET_PAGES_ALLOC2)
+#if defined(HAVE_DIRECTIO_ITER) || defined(HAVE_DIRECTIO_2ARGS) || \
+    defined(HAVE_IOV_ITER_GET_PAGES_ALLOC2)
 #define HAVE_DIO_ITER 1
 #endif
 

lustre/llite/file.c
@@ -2409,14 +2409,6 @@ fini_io:
 	RETURN(rc);
 }
 
-#ifdef HAVE_IOV_ITER_INIT_DIRECTION
-# define ll_iov_iter_init(i, d, v, n, l) \
-	 iov_iter_init((i), (d), (v), (n), (l))
-# else
-# define ll_iov_iter_init(i, d, v, n, l) \
-	 iov_iter_init((i), (v), (n), (l), 0)
-# endif
-
 typedef ssize_t (*iter_fn_t)(struct kiocb *, struct iov_iter *);
 
 static ssize_t do_loop_readv_writev(struct kiocb *iocb, const struct iovec *iov,
@@ -2430,7 +2422,7 @@ static ssize_t do_loop_readv_writev(struct kiocb *iocb, const struct iovec *iov,
 		ssize_t nr;
 		size_t len = vector->iov_len;
 
-		ll_iov_iter_init(&i, rw, vector, 1, len);
+		iov_iter_init(&i, rw, vector, 1, len);
 		nr = fn(iocb, &i);
 		if (nr < 0) {
 			if (!ret)
@@ -5543,11 +5535,7 @@ static void ll_file_flock_async_cb(struct ldlm_flock_info *args)
 
 		wait_event_idle(args->fa_waitq, args->fa_ready);
 
-#ifdef HAVE_LM_GRANT_2ARGS
 		rc = args->fa_notify(&notify_lock, err);
-#else
-		rc = args->fa_notify(&notify_lock, NULL, err);
-#endif
 		if (rc) {
 			CDEBUG_LIMIT(D_ERROR,
 				     "notify failed file_lock=%p err=%d\n",

lustre/llite/namei.c
@@ -32,11 +32,7 @@
 #ifndef HAVE_USER_NAMESPACE_ARG
 #define ll_create_nd(ns, dir, de, mode, ex)	ll_create_nd(dir, de, mode, ex)
 #define ll_mknod(ns, dir, dch, mode, rd)	ll_mknod(dir, dch, mode, rd)
-#ifdef HAVE_IOPS_RENAME_WITH_FLAGS
 #define ll_rename(ns, src, sdc, tgt, tdc, fl)	ll_rename(src, sdc, tgt, tdc, fl)
-#else
-#define ll_rename(ns, src, sdc, tgt, tdc)	ll_rename(src, sdc, tgt, tdc)
-#endif /* HAVE_IOPS_RENAME_WITH_FLAGS */
 #define ll_symlink(nd, dir, dch, old)		ll_symlink(dir, dch, old)
 #endif
 
@@ -2463,11 +2459,8 @@ out:
 
 static int ll_rename(struct mnt_idmap *map,
 		     struct inode *src, struct dentry *src_dchild,
-		     struct inode *tgt, struct dentry *tgt_dchild
-#if defined(HAVE_USER_NAMESPACE_ARG) || defined(HAVE_IOPS_RENAME_WITH_FLAGS)
-		     , unsigned int flags
-#endif
-		     )
+		     struct inode *tgt, struct dentry *tgt_dchild,
+		     unsigned int flags)
 {
 	struct ptlrpc_request *request = NULL;
 	struct ll_sb_info *sbi = ll_i2sbi(src);
@@ -2479,10 +2472,8 @@ static int ll_rename(struct mnt_idmap *map,
 
 	ENTRY;
 
-#if defined(HAVE_USER_NAMESPACE_ARG) || defined(HAVE_IOPS_RENAME_WITH_FLAGS)
 	if (flags)
 		GOTO(out, err = -EINVAL);
-#endif
 
 	CDEBUG(D_VFSTRACE,
 	       "VFS Op:oldname="DNAME", src_dir="DFID"(%p), newname=%pd, tgt_dir="DFID"(%p)\n",
@@ -2492,11 +2483,7 @@ static int ll_rename(struct mnt_idmap *map,
 	if (unlikely(d_mountpoint(src_dchild) || d_mountpoint(tgt_dchild)))
 		GOTO(out, err = -EBUSY);
 
-#if defined(HAVE_USER_NAMESPACE_ARG) || defined(HAVE_IOPS_RENAME_WITH_FLAGS)
 	err = llcrypt_prepare_rename(src, src_dchild, tgt, tgt_dchild, flags);
-#else
-	err = llcrypt_prepare_rename(src, src_dchild, tgt, tgt_dchild, 0);
-#endif
 	if (err)
 		GOTO(out, err);
 	/* we prevent an encrypted file from being renamed

config/lustre-build.m4
@@ -189,7 +189,6 @@ AC_DEFUN([LB_KABI_CHECKS], [
 			LIBCFS_SRC_LINUX_BLK_INTEGRITY_HEADER
 			LIBCFS_SRC_LINUX_BIO_INTEGRITY_HEADER
 			LIBCFS_SRC_HAVE_MMAP_LOCK
-			LC_SRC_HAVE_LM_GRANT_2ARGS
 			LC_SRC_PAGEVEC_LOOKUP_THREE_PARAM
 			LC_SRC_HAVE_INVALIDATE_LOCK
 			LC_SRC_HAVE_WB_STAT_MOD
@@ -204,7 +203,6 @@ AC_DEFUN([LB_KABI_CHECKS], [
 			LIBCFS_LINUX_BLK_INTEGRITY_HEADER
 			LIBCFS_LINUX_BIO_INTEGRITY_HEADER
 			LIBCFS_HAVE_MMAP_LOCK
-			LC_HAVE_LM_GRANT_2ARGS
 			LC_PAGEVEC_LOOKUP_THREE_PARAM
 			LC_HAVE_INVALIDATE_LOCK
 			LC_HAVE_WB_STAT_MOD

config/lustre-core.m4
@@ -523,367 +523,6 @@ AC_DEFUN([LC_HAVE_LIBAIO], [
 ]) # LC_HAVE_LIBAIO
 
 #
-# LC_VFS_RENAME_5ARGS
-#
-# 3.13 has vfs_rename with 5 args
-#
-AC_DEFUN([LC_SRC_VFS_RENAME_5ARGS], [
-	LB2_LINUX_TEST_SRC([vfs_rename_5args], [
-		#include <linux/fs.h>
-	],[
-		vfs_rename(NULL, NULL, NULL, NULL, NULL);
-	])
-])
-AC_DEFUN([LC_VFS_RENAME_5ARGS], [
-	LB2_MSG_LINUX_TEST_RESULT([if Linux kernel has 'vfs_rename' with 5 args],
-	[vfs_rename_5args], [
-		AC_DEFINE(HAVE_VFS_RENAME_5ARGS, 1,
-			[kernel has vfs_rename with 5 args])
-	])
-]) # LC_VFS_RENAME_5ARGS
-
-#
-# LC_VFS_UNLINK_3ARGS
-#
-# 3.13 has vfs_unlink with 3 args
-#
-AC_DEFUN([LC_SRC_VFS_UNLINK_3ARGS], [
-	LB2_LINUX_TEST_SRC([vfs_unlink_3args], [
-		#include <linux/fs.h>
-	],[
-		vfs_unlink(NULL, NULL, NULL);
-	])
-])
-AC_DEFUN([LC_VFS_UNLINK_3ARGS], [
-	LB2_MSG_LINUX_TEST_RESULT([if Linux kernel has 'vfs_unlink' with 3 args],
-	[vfs_unlink_3args], [
-		AC_DEFINE(HAVE_VFS_UNLINK_3ARGS, 1,
-			[kernel has vfs_unlink with 3 args])
-	])
-]) # LC_VFS_UNLINK_3ARGS
-
-#
-# LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
-#
-# Linux commit v3.13-rc1-10-gd57a5f7c6605 replaced bip_sector with bip_iter
-# for struct bio_integrity_payload
-#
-# LB_CHECK_LINUX_HEADER has already run so we can rely on
-# HAVE_LINUX_BIO_INTEGRITY_HEADER being set correctly before
-# this test is run.
-#
-AC_DEFUN([LC_SRC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD], [
-	LB2_LINUX_TEST_SRC([bio_integrity_payload_bip_iter], [
-		#ifdef HAVE_LINUX_BIO_INTEGRITY_HEADER
-		# include <linux/bio-integrity.h>
-		#else
-		# include <linux/bio.h>
-		#endif
-	],[
-		((struct bio_integrity_payload *)0)->bip_iter.bi_size = 0;
-	])
-])
-AC_DEFUN([LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'bio_integrity_payload.bip_iter' exist],
-	[bio_integrity_payload_bip_iter], [
-		AC_DEFINE(HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD, 1,
-			[bio_integrity_payload.bip_iter exist])
-	])
-]) # LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
-
-#
-# LC_HAVE_BVEC_ITER
-#
-# 3.14 move some of its data in struct bio into the new
-# struct bvec_iter
-#
-AC_DEFUN([LC_SRC_HAVE_BVEC_ITER], [
-	LB2_LINUX_TEST_SRC([have_bvec_iter], [
-		#include <linux/bio.h>
-	],[
-		struct bvec_iter iter;
-
-		iter.bi_bvec_done = 0;
-	])
-])
-AC_DEFUN([LC_HAVE_BVEC_ITER], [
-	LB2_MSG_LINUX_TEST_RESULT([if Linux kernel has struct bvec_iter],
-	[have_bvec_iter], [
-		AC_DEFINE(HAVE_BVEC_ITER, 1,
-			[kernel has struct bvec_iter])
-	])
-]) # LC_HAVE_BVEC_ITER
-
-#
-# LC_IOP_SET_ACL
-#
-# 3.14 adds set_acl method to inode_operations
-# see kernel commit 893d46e443346370cd4ea81d9d35f72952c62a37
-#
-AC_DEFUN([LC_SRC_IOP_SET_ACL], [
-	LB2_LINUX_TEST_SRC([inode_ops_set_acl], [
-		#include <linux/fs.h>
-	],[
-		struct inode_operations iop;
-		iop.set_acl = NULL;
-	])
-])
-AC_DEFUN([LC_IOP_SET_ACL], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'inode_operations' has '.set_acl' member function],
-	[inode_ops_set_acl], [
-		AC_DEFINE(HAVE_IOP_SET_ACL, 1,
-			[inode_operations has .set_acl member function])
-	])
-]) # LC_IOP_SET_ACL
-
-#
-# LC_HAVE_TRUNCATE_IPAGE_FINAL
-#
-# 3.14 bring truncate_inode_pages_final for evict_inode
-#
-AC_DEFUN([LC_SRC_HAVE_TRUNCATE_IPAGES_FINAL], [
-	LB2_LINUX_TEST_SRC([truncate_ipages_final], [
-		#include <linux/mm.h>
-	],[
-		truncate_inode_pages_final(NULL);
-	])
-])
-AC_DEFUN([LC_HAVE_TRUNCATE_IPAGES_FINAL], [
-	LB2_MSG_LINUX_TEST_RESULT([if Linux kernel has truncate_inode_pages_final],
-	[truncate_ipages_final], [
-		AC_DEFINE(HAVE_TRUNCATE_INODE_PAGES_FINAL, 1,
-			[kernel has truncate_inode_pages_final])
-	])
-]) # LC_HAVE_TRUNCATE_IPAGES_FINAL
-
-#
-# LC_IOPS_RENAME_WITH_FLAGS
-#
-# 3.14 has inode_operations->rename with 5 args
-# commit 520c8b16505236fc82daa352e6c5e73cd9870cff
-#
-AC_DEFUN([LC_SRC_IOPS_RENAME_WITH_FLAGS], [
-	LB2_LINUX_TEST_SRC([iops_rename_with_flags], [
-		#include <linux/fs.h>
-	],[
-		struct inode_operations *iops = NULL;
-		struct inode *i1 = NULL, *i2 = NULL;
-		struct dentry *d1 = NULL, *d2 = NULL;
-
-		iops->rename(i1, d1, i2, d2, 0);
-	])
-]) # LC_IOPS_RENAME_WITH_FLAGS
-AC_DEFUN([LC_IOPS_RENAME_WITH_FLAGS], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'inode_operations->rename' taken flags as argument],
-	[iops_rename_with_flags], [
-		AC_DEFINE(HAVE_IOPS_RENAME_WITH_FLAGS, 1,
-			[inode_operations->rename need flags as argument])
-	])
-]) # LC_IOPS_RENAME_WITH_FLAGS
-
-#
-# LC_VFS_RENAME_6ARGS
-#
-# 3.15 has vfs_rename with 6 args
-#
-AC_DEFUN([LC_SRC_VFS_RENAME_6ARGS], [
-	LB2_LINUX_TEST_SRC([vfs_rename_6args], [
-		#include <linux/fs.h>
-	],[
-		vfs_rename(NULL, NULL, NULL, NULL, NULL, NULL);
-	])
-])
-AC_DEFUN([LC_VFS_RENAME_6ARGS], [
-	LB2_MSG_LINUX_TEST_RESULT([if Linux kernel has 'vfs_rename' with 6 args],
-	[vfs_rename_6args], [
-		AC_DEFINE(HAVE_VFS_RENAME_6ARGS, 1,
-			[kernel has vfs_rename with 6 args])
-	])
-]) # LC_VFS_RENAME_6ARGS
-
-#
-# LC_PMQOS_RESUME_LATENCY
-#
-# DEV_PM_QOS_LATENCY is used until v3.14 included
-# DEV_PM_QOS_RESUME_LATENCY is used since v3.15
-#
-AC_DEFUN([LC_SRC_PMQOS_RESUME_LATENCY], [
-        LB2_LINUX_TEST_SRC([pmqos_resume_latency], [
-		#include <linux/pm_qos.h>
-	], [
-			struct dev_pm_qos_request req;
-			struct device dev;
-
-			dev_pm_qos_add_request(&dev, &req, DEV_PM_QOS_LATENCY, 0);
-	])
-])
-
-AC_DEFUN([LC_PMQOS_RESUME_LATENCY], [
-saved_flags="$CFLAGS"
-CFLAGS="-Werror"
-LB2_MSG_LINUX_TEST_RESULT([if 'DEV_PM_QOS_LATENCY' vs 'DEV_PM_QOS_RESUME_LATENCY'],
-	[pmqos_resume_latency], [
-		AC_DEFINE(DEV_PM_QOS_RESUME_LATENCY, DEV_PM_QOS_LATENCY, [using 'DEV_PM_QOS_LATENCY'])
-	], [])
-CFLAGS="$saved_flags"
-])
-
-#
-# LC_DIRECTIO_USE_ITER
-#
-# 3.16 kernel changes direct IO to use iov_iter
-#
-AC_DEFUN([LC_SRC_DIRECTIO_USE_ITER], [
-	LB2_LINUX_TEST_SRC([direct_io_iter], [
-		#include <linux/fs.h>
-	],[
-		struct address_space_operations ops = { };
-		struct iov_iter *iter = NULL;
-		loff_t offset = 0;
-
-		ops.direct_IO(0, NULL, iter, offset);
-	])
-])
-AC_DEFUN([LC_DIRECTIO_USE_ITER], [
-	LB2_MSG_LINUX_TEST_RESULT([if direct IO uses iov_iter],
-	[direct_io_iter], [
-		AC_DEFINE(HAVE_DIRECTIO_ITER, 1, [direct IO uses iov_iter])
-	])
-]) # LC_DIRECTIO_USE_ITER
-
-#
-# LC_HAVE_IOV_ITER_INIT_DIRECTION
-#
-#
-# 3.16 linux commit 71d8e532b1549a478e6a6a8a44f309d050294d00
-#      changed iov_iter_init api to start accepting a tag
-#      that defines if its a read or write operation
-#
-AC_DEFUN([LC_SRC_HAVE_IOV_ITER_INIT_DIRECTION], [
-	LB2_LINUX_TEST_SRC([iter_init], [
-		#include <linux/uio.h>
-		#include <linux/fs.h>
-	],[
-		const struct iovec *iov = NULL;
-
-		iov_iter_init(NULL, READ, iov, 1, 0);
-	],[-Werror])
-])
-AC_DEFUN([LC_HAVE_IOV_ITER_INIT_DIRECTION], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'iov_iter_init' takes a tag],
-	[iter_init], [
-		AC_DEFINE(HAVE_IOV_ITER_INIT_DIRECTION, 1,
-			[iov_iter_init handles directional tag])
-	])
-]) # LC_HAVE_IOV_ITER_INIT_DIRECTION
-
-#
-# LC_HAVE_IOV_ITER_TRUNCATE
-#
-#
-# 3.16 introduces a new API iov_iter_truncate()
-#
-AC_DEFUN([LC_SRC_HAVE_IOV_ITER_TRUNCATE], [
-	LB2_LINUX_TEST_SRC([iter_truncate], [
-		#include <linux/uio.h>
-		#include <linux/fs.h>
-	],[
-		struct iov_iter *i = NULL;
-
-		iov_iter_truncate(i, 0);
-	],[-Werror])
-])
-AC_DEFUN([LC_HAVE_IOV_ITER_TRUNCATE], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'iov_iter_truncate' exists],
-	[iter_truncate], [
-		AC_DEFINE(HAVE_IOV_ITER_TRUNCATE, 1, [iov_iter_truncate exists])
-	])
-]) # LC_HAVE_IOV_ITER_TRUNCATE
-
-#
-# LC_PAGECACHE_GET_PAGE
-#
-# Kernel version 3.16 commit 2457aec63745e235bcafb7ef312b182d8682f0fc
-# @pagecache_get_page was introduced since Linux 3.16
-#
-AC_DEFUN([LC_SRC_PAGECACHE_GET_PAGE], [
-	LB2_LINUX_TEST_SRC([pagecache_get_page], [
-		#include <linux/pagemap.h>
-	],[
-		pagecache_get_page(NULL, 0, 0, 0);
-	])
-])
-AC_DEFUN([LC_PAGECACHE_GET_PAGE], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'pagecache_get_page' exists],
-	[pagecache_get_page], [
-		AC_DEFINE(HAVE_PAGECACHE_GET_PAGE, 1,
-			['pagecache_get_page' is available])
-	])
-]) # LC_PAGECACHE_GET_PAGE
-
-#
-# LC_HAVE_INTERVAL_BLK_INTEGRITY
-#
-# 3.17 replace sector_size with interval in struct blk_integrity
-#
-AC_DEFUN([LC_SRC_HAVE_INTERVAL_BLK_INTEGRITY], [
-	LB2_LINUX_TEST_SRC([interval_blk_integrity], [
-		#include <linux/blkdev.h>
-	],[
-		((struct blk_integrity *)0)->interval = 0;
-	])
-])
-AC_DEFUN([LC_HAVE_INTERVAL_BLK_INTEGRITY], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'blk_integrity.interval' exist],
-	[interval_blk_integrity], [
-		AC_DEFINE(HAVE_INTERVAL_BLK_INTEGRITY, 1,
-			[blk_integrity.interval exist])
-	])
-]) # LC_HAVE_INTERVAL_BLK_INTEGRITY
-
-#
-# LC_KEY_MATCH_DATA
-#
-# 3.17	replaces key_type::match with match_preparse
-#	and has new struct key_match_data
-#
-AC_DEFUN([LC_SRC_KEY_MATCH_DATA], [
-	LB2_LINUX_TEST_SRC([key_match], [
-		#include <linux/key-type.h>
-	],[
-		struct key_match_data data;
-
-		data.raw_data = NULL;
-	])
-])
-AC_DEFUN([LC_KEY_MATCH_DATA], [
-	LB2_MSG_LINUX_TEST_RESULT([if struct key_match field exist],
-	[key_match], [
-		AC_DEFINE(HAVE_KEY_MATCH_DATA, 1, [struct key_match_data exist])
-	])
-]) # LC_KEY_MATCH_DATA
-
-#
-# LC_HAVE_LM_GRANT_2ARGS
-#
-# 3.17 removed unused argument from lm_grant
-#
-AC_DEFUN([LC_SRC_HAVE_LM_GRANT_2ARGS], [
-	LB2_LINUX_TEST_SRC([lock_manager_operations_lm_grant], [
-		#include <linux/fs.h>
-	],[
-		((struct lock_manager_operations *)NULL)->lm_grant(NULL, 0);
-	])
-])
-AC_DEFUN([LC_HAVE_LM_GRANT_2ARGS], [
-	LB2_MSG_LINUX_TEST_RESULT([if 'lock_manager_operations.lm_grant' takes two args],
-	[lock_manager_operations_lm_grant], [
-		AC_DEFINE(HAVE_LM_GRANT_2ARGS, 1,
-			[lock_manager_operations.lm_grant takes two args])
-	])
-]) # LC_HAVE_LM_GRANT_2ARGS
-
-#
 # LC_HAVE_PROJECT_QUOTA
 #
 # Kernel version v4.0-rc1-197-g847aac644e92
@@ -4735,33 +4374,10 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	])
 	LC_SRC_CONFIG_FHANDLE
 	LC_SRC_POSIX_ACL_CONFIG
-	LC_SRC_HAVE_PROJECT_QUOTA
 	LC_SRC_CONFIG_XARRAY_MULTI
 
-	# 3.13
-	LC_SRC_VFS_RENAME_5ARGS
-	LC_SRC_VFS_UNLINK_3ARGS
-	LC_SRC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
-
-	# 3.14
-	LC_SRC_HAVE_BVEC_ITER
-	LC_SRC_HAVE_TRUNCATE_IPAGES_FINAL
-	LC_SRC_IOPS_RENAME_WITH_FLAGS
-	LC_SRC_IOP_SET_ACL
-
-	# 3.15
-	LC_SRC_VFS_RENAME_6ARGS
-	LC_SRC_PMQOS_RESUME_LATENCY
-
-	# 3.16
-	LC_SRC_DIRECTIO_USE_ITER
-	LC_SRC_HAVE_IOV_ITER_INIT_DIRECTION
-	LC_SRC_HAVE_IOV_ITER_TRUNCATE
-	LC_SRC_PAGECACHE_GET_PAGE
-
-	# 3.17
-	LC_SRC_HAVE_INTERVAL_BLK_INTEGRITY
-	LC_SRC_KEY_MATCH_DATA
+	# 4.0
+	LC_SRC_HAVE_PROJECT_QUOTA
 
 	# 4.1.0
 	LC_SRC_IOV_ITER_RW
@@ -5035,33 +4651,10 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	])
 	LC_CONFIG_FHANDLE
 	LC_POSIX_ACL_CONFIG
-	LC_HAVE_PROJECT_QUOTA
 	LC_CONFIG_XARRAY_MULTI
 
-	# 3.13
-	LC_VFS_RENAME_5ARGS
-	LC_VFS_UNLINK_3ARGS
-	LC_HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
-
-	# 3.14
-	LC_HAVE_BVEC_ITER
-	LC_HAVE_TRUNCATE_IPAGES_FINAL
-	LC_IOPS_RENAME_WITH_FLAGS
-	LC_IOP_SET_ACL
-
-	# 3.15
-	LC_VFS_RENAME_6ARGS
-	LC_PMQOS_RESUME_LATENCY
-
-	# 3.16
-	LC_DIRECTIO_USE_ITER
-	LC_HAVE_IOV_ITER_INIT_DIRECTION
-	LC_HAVE_IOV_ITER_TRUNCATE
-	LC_PAGECACHE_GET_PAGE
-
-	# 3.17
-	LC_HAVE_INTERVAL_BLK_INTEGRITY
-	LC_KEY_MATCH_DATA
+	# 4.0
+	LC_HAVE_PROJECT_QUOTA
 
 	# 4.1.0
 	LC_IOV_ITER_RW

lustre/include/lustre_dlm.h
@@ -1413,11 +1413,7 @@ struct ldlm_flock_info {
 	struct file_lock	fa_flc; /* lock copy */
 	enum ldlm_flock_flags	fa_flags;
 	enum ldlm_mode		fa_mode;
-#ifdef HAVE_LM_GRANT_2ARGS
 	int (*fa_notify)(struct file_lock *, int);
-#else
-	int (*fa_notify)(struct file_lock *, struct file_lock *, int);
-#endif
 	int			fa_err;
 	int			fa_ready;
 	wait_queue_head_t       fa_waitq;

lustre/llite/llite_nfs.c
@@ -281,11 +281,7 @@ do_nfs_get_name_filldir(struct ll_getname_data *lgd, const char *name,
 	return lgd->lgd_found;
 }
 
-#ifdef HAVE_FILLDIR_USE_CTX_RETURN_BOOL
-static bool
-#else
-static int
-#endif
+static FILLDIR_TYPE
 ll_nfs_get_name_filldir(struct dir_context *ctx, const char *name, int namelen,
 			loff_t hash, u64 ino, unsigned int type)
 {

lustre/osd-ldiskfs/osd_integrity.c
@@ -12,29 +12,11 @@
  * Author: Li Xi
  */
 
+#include <linux/t10-pi.h>
 #include <obd_cksum.h>
 #include <lustre_compat.h>
 
 #include "osd_internal.h"
-
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
-# include <linux/t10-pi.h>
-#else
-# define blk_status_t int
-# define BLK_STS_PROTECTION -EIO
-# define BLK_STS_OK 0
-/*
- * Data Integrity Field tuple.
- */
-struct t10_pi_tuple {
-       __be16 guard_tag;        /* Checksum */
-       __be16 app_tag;          /* Opaque storage */
-       __be32 ref_tag;          /* Target LBA or indirect LBA */
-};
-
-#define T10_PI_APP_ESCAPE cpu_to_be16(0xffff)
-#define T10_PI_REF_ESCAPE cpu_to_be32(0xffffffff)
-#endif
 
 struct osd_blk_integrity_iter {
 	void			*prot_buf;
@@ -314,7 +296,7 @@ static int osd_bio_integrity_compare(struct bio *bio, struct block_device *bdev,
 	void *bio_prot_buf = page_address(bip->bip_vec->bv_page) +
 		bip->bip_vec->bv_offset;
 	struct bio_vec *bv;
-	sector_t sector = bio_start_sector(bio);
+	sector_t sector = bio->bi_iter.bi_sector;
 	unsigned int i, sectors, total;
 	DECLARE_BVEC_ITER_ALL(iter_all);
 	__be16 *expected_guard;
@@ -356,11 +338,10 @@ static int osd_bio_integrity_compare(struct bio *bio, struct block_device *bdev,
 }
 #endif
 
-
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 static blk_status_t osd_bio_integrity_process(struct bio *bio,
-		struct bvec_iter *proc_iter, void *prot_buf,
-		osd_integrity_proc_fn *proc_fn)
+					      struct bvec_iter *proc_iter,
+					      void *prot_buf,
+					      osd_integrity_proc_fn *proc_fn)
 {
 	struct blk_integrity *bi = blk_get_integrity(bio_get_disk(bio));
 	struct osd_blk_integrity_iter iter;
@@ -406,40 +387,6 @@ static blk_status_t osd_bio_integrity_process(struct bio *bio,
 	}
 	return ret;
 }
-#else
-static blk_status_t osd_bio_integrity_process(struct bio *bio, void *prot_buf,
-					      osd_integrity_proc_fn *proc_fn)
-{
-	struct blk_integrity *bi = blk_get_integrity(bio_get_disk(bio));
-	struct osd_blk_integrity_iter iter;
-	struct bio_vec *bv;
-	unsigned int i;
-	blk_status_t ret = BLK_STS_OK;
-
-	iter.disk_name = bio_get_disk(bio)->disk_name;
-	iter.interval = bi->sector_size;
-	iter.tuple_size = bi->tuple_size;
-	iter.seed = bio->bi_integrity->bip_sector;
-	iter.prot_buf = prot_buf;
-	iter.bio = bio;
-
-	bio_for_each_segment(bv, bio, i) {
-		void *kaddr = kmap_atomic(bv->bv_page);
-
-		iter.data_buf = kaddr + bv->bv_offset;
-		iter.data_size = bv->bv_len;
-		iter.bi_idx = i;
-
-		ret = proc_fn(&iter);
-		kunmap_atomic(kaddr);
-
-		if (ret)
-			break;
-
-	}
-	return ret;
-}
-#endif
 
 void osd_bio_integrity_verify_fn(struct work_struct *work)
 {
@@ -455,18 +402,12 @@ void osd_bio_integrity_verify_fn(struct work_struct *work)
 				       &verify_fn);
 	if (rc)
 		goto out;
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
+
 	ret = osd_bio_integrity_process(bio, &bio_private->obp_integrity_iter,
-				bio_private->obp_integrity_buf, verify_fn);
+					bio_private->obp_integrity_buf,
+					verify_fn);
 	iobuf->dr_error = blk_status_to_errno(ret);
 	bio->bi_status = ret;
-#else
-	bio->bi_idx = bio->bi_integrity->saved_bi_idx;
-	ret = osd_bio_integrity_process(bio, bio_private->obp_integrity_buf,
-					verify_fn);
-	iobuf->dr_error = ret;
-#endif
-
 out:
 	osd_bio_fini(bio);
 }
@@ -525,19 +466,12 @@ int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
 	}
 
 	/* Allocate kernel buffer for protection data */
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 	len = bio_integrity_bytes(bi, bio_sectors(bio));
-#else
-	len = (bi->sector_size == 4096 ?
-	       bio_sectors(bio) >> 3 : bio_sectors(bio)) * bi->tuple_size;
-#endif
 	buf = kmalloc(len, gfp);
 	if (unlikely(buf == NULL)) {
 		CERROR("%s: could not allocate integrity buffer\n",
 		       osd_name(osd));
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 		bio->bi_status = BLK_STS_RESOURCE;
-#endif
 		RETURN(-ENOMEM);
 	}
 
@@ -551,20 +485,16 @@ int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
 		CERROR("%s: could not allocate data integrity bioset\n",
 		       osd_name(osd));
 		kfree(buf);
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 		bio->bi_status = BLK_STS_RESOURCE;
-#endif
 		RETURN(-ENOMEM);
 	}
 
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 #  ifdef HAVE_CSUM_TYPE_BLK_INTEGRITY
 	if (bi->csum_type == BLK_INTEGRITY_CSUM_IP)
 #  else
 	if (bi->flags & BLK_INTEGRITY_IP_CHECKSUM)
 #  endif
 		bip->bip_flags |= BIP_IP_CHECKSUM;
-#endif
 
 	/* Map it */
 	offset = offset_in_page(buf);
@@ -579,9 +509,7 @@ int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
 					   offset) < bytes) {
 			CERROR("%s: could not attach integrity payload\n",
 			       osd_name(osd));
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 			bio->bi_status = BLK_STS_RESOURCE;
-#endif
 			kfree(buf);
 			RETURN(-ENOMEM);
 		}
@@ -591,28 +519,16 @@ int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
 		offset = 0;
 	}
 
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 	/*
 	 * reset bip_iter.bi_size, usually bio_integrity_add_page does that
 	 * for us, but some older kernels doesn't have 80814b8e359f7
 	 */
 	bip->bip_iter.bi_size = bio_integrity_bytes(bi, bio_sectors(bio));
 	bip_set_seed(bip, bio->bi_iter.bi_sector);
-#else
-	bip->bip_buf = buf;
-	bip->bip_size = (bi->sector_size == 4096 ?
-			 bio_sectors(bio) >> 3 : bio_sectors(bio)) *
-			bi->tuple_size;
-	bip->bip_sector = bio->bi_sector;
-#endif
 	bio_private->obp_integrity_buf = buf;
 
 	if (iobuf->dr_rw == 1) {
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 		osd_bio_integrity_process(bio, &bio->bi_iter, buf, generate_fn);
-#else
-		osd_bio_integrity_process(bio, buf, generate_fn);
-#endif
 
 		/* Verify and inject fault only when writing */
 		if (unlikely(CFS_FAIL_CHECK(OBD_FAIL_OST_INTEGRITY_CMP))) {
@@ -628,11 +544,7 @@ int osd_bio_integrity_handle(struct osd_device *osd, struct bio *bio,
 		if (unlikely(CFS_FAIL_CHECK(OBD_FAIL_OST_INTEGRITY_FAULT)))
 			bio_integrity_fault_inject(bio);
 	} else {
-#ifdef HAVE_BIP_ITER_BIO_INTEGRITY_PAYLOAD
 		bio_private->obp_integrity_iter = bio->bi_iter;
-#else
-		bip->saved_bi_idx = bio->bi_idx;
-#endif
 	}
 
 	RETURN(0);

commit f1585bafaa0db04ff88ce99490fed3ac9c0b524a

LU-19179 build: remove references to HAVE_IOP_SET_ACL
... since this ought to always be true.

Fixes: 2c2a4091ff56 ("LU-19179 build: remove all remaining 3.X kernel support")
Test-Parameters: trivial testlist=sanityn env=ONLY=25a,ONLY_REPEAT=25
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Ic95ff3a7f76256fed42c1df082be33a86a81ef5c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64030
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/include/lustre_compat.h
@@ -197,7 +197,6 @@ static inline int ll_vfs_getattr(struct path *path, struct kstat *st,
 #define posix_acl_valid(a, b)		posix_acl_valid(b)
 #endif
 
-#ifdef HAVE_IOP_SET_ACL
 #ifdef CONFIG_LUSTRE_FS_POSIX_ACL
 #if !defined(HAVE_USER_NAMESPACE_ARG) && \
 	!defined(HAVE_POSIX_ACL_UPDATE_MODE) && \
@@ -220,7 +219,6 @@ static inline int posix_acl_update_mode(struct inode *inode, umode_t *mode_p,
 	return 0;
 }
 #endif /* HAVE_POSIX_ACL_UPDATE_MODE */
-#endif
 #endif
 
 /*

lustre/llite/acl.c
@@ -111,7 +111,6 @@ struct posix_acl *ll_get_acl(
 	return ll_get_acl_common(inode, type, rcu);
 }
 
-#ifdef HAVE_IOP_SET_ACL
 int ll_set_acl(struct mnt_idmap *map,
 #ifdef HAVE_ACL_WITH_DENTRY
 	       struct dentry *dentry,
@@ -178,4 +177,3 @@ out:
 		set_cached_acl(inode, type, acl);
 	RETURN(rc);
 }
-#endif /* HAVE_IOP_SET_ACL */

lustre/llite/file.c
@@ -6763,9 +6763,7 @@ const struct inode_operations ll_file_inode_operations = {
 	.get_inode_acl	= ll_get_inode_acl,
 #endif
 	.get_acl	= ll_get_acl,
-#ifdef HAVE_IOP_SET_ACL
 	.set_acl	= ll_set_acl,
-#endif
 #ifdef HAVE_FILEATTR_GET
 	.fileattr_get	= ll_fileattr_get,
 	.fileattr_set	= ll_fileattr_set,

lustre/llite/namei.c
@@ -2574,9 +2574,7 @@ const struct inode_operations ll_dir_inode_operations = {
 	.get_inode_acl	= ll_get_inode_acl,
 #endif
 	.get_acl	= ll_get_acl,
-#ifdef HAVE_IOP_SET_ACL
 	.set_acl	= ll_set_acl,
-#endif
 #ifdef HAVE_FILEATTR_GET
 	.fileattr_get	= ll_fileattr_get,
 	.fileattr_set	= ll_fileattr_set,
@@ -2597,7 +2595,5 @@ const struct inode_operations ll_special_inode_operations = {
 	.get_inode_acl	= ll_get_inode_acl,
 #endif
 	.get_acl	= ll_get_acl,
-#ifdef HAVE_IOP_SET_ACL
 	.set_acl	= ll_set_acl,
-#endif
 };