lustre/lustre-release

commit a050dfb80dd5bc3087592673904aca29b4174baf

LU-19229 mgc: client to handle IR with unknown nets
When client processes config log or IR information it
assumes that server LNet configuration is the same, e.g.
list of networks. If LNet configuration is not the same,
e.g. server has many nets but client just single one, it
fails to mount over that network in most cases.

Patch makes client more flexible and able to mount in
such environments.

- don't fail when initial connection is not established
  based on config file, enable dynamic_nids for target
  and let IR to fill the gap
- when using IR NID list, don't stick to first NID while
  setting/checking initial connection but try all until
  one on valid network is found
- LNetAddPeer() to don't add peers on missing networks
  when peer discovery is off
- add conf-sanity test 73e for dynamic NIDs feature

Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ifeecf3ce8bacab6e3de3249ad843f31eb49b40c5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60549
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/mgc/mgc_request.c
@@ -1196,6 +1196,61 @@ static int mgc_import_event(struct obd_device *obd,
 	RETURN(rc);
 }
 
+static int mgc_create_new_conn(struct obd_import *imp, struct lnet_nid *nidlist,
+			       int nid_count, int nid_size,
+			       struct obd_uuid *uuid)
+{
+	struct obd_uuid node_uuid;
+	char prim_nid[LNET_NIDSTR_SIZE] = { 0 };
+	int i = 0;
+	int rc;
+
+	/* client has no existing connection to that target yet, and
+	 * it has list of target NIDs, which may have NIDs on
+	 * networks not set up on client.
+	 * Find first NID in list on a client network and use it as
+	 * primary NID for new connection
+	 */
+	while (i < nid_count) {
+		libcfs_nidstr_r(&nidlist[i], prim_nid, sizeof(prim_nid));
+
+		if (strlen(prim_nid) >= UUID_MAX) {
+			rc = -E2BIG;
+			CDEBUG(D_MGC, "%s: skipping too big NID '%s'\n",
+			      imp->imp_obd->obd_name, prim_nid);
+			i++;
+			continue;
+		}
+
+		obd_str2uuid(&node_uuid, prim_nid);
+		rc = client_import_dyn_add_conn(imp, &node_uuid,
+						&nidlist[i], 1);
+		if (!rc)
+			break;
+		CDEBUG(D_MGC, "%s: can't add conn with NID '%s': rc = %d\n",
+		       imp->imp_obd->obd_name, prim_nid, rc);
+		i++;
+	}
+	if (rc < 0) {
+		CWARN("%s: no valid NIDs for new import connection: rc = %d\n",
+		      imp->imp_obd->obd_name, rc);
+		return rc;
+	}
+
+	CDEBUG(D_INFO, "Adding a connection for %s on %s\n",
+	       imp->imp_obd->obd_name, prim_nid);
+
+	/* Add remaining NIDs in list to that connection */
+	rc = client_import_add_nids_to_conn(imp, nidlist, nid_count,
+					    nid_size, uuid);
+	if (rc < 0)
+		CERROR("%s: failed to update NID list: rc = %d\n",
+		       imp->imp_obd->obd_name, rc);
+
+	return rc;
+}
+
+
 static int mgc_apply_recover_logs(struct obd_device *mgc,
 				  struct config_llog_data *cld,
 				  __u64 max_version,
@@ -1211,7 +1266,7 @@ static int mgc_apply_recover_logs(struct obd_device *mgc,
 	int pos = 0;
 	int rc  = 0;
 	int off = 0;
-	unsigned long dynamic_nids;
+	bool dynamic_nids;
 
 	ENTRY;
 	LASSERT(cfg->cfg_instance != 0);
@@ -1222,7 +1277,7 @@ static int mgc_apply_recover_logs(struct obd_device *mgc,
 
 	if (!IS_SERVER(s2lsi(cfg->cfg_sb))) {
 		pos = snprintf(inst, sizeof(inst), "%016lx", cfg->cfg_instance);
-		if (pos >= PAGE_SIZE)
+		if (pos >= sizeof(inst))
 			return -E2BIG;
 #ifdef HAVE_SERVER_SUPPORT
 	} else {
@@ -1378,48 +1433,17 @@ static int mgc_apply_recover_logs(struct obd_device *mgc,
 		uuid = (struct obd_uuid *)(buf + pos);
 
 		with_imp_locked(obd, imp, rc) {
-			struct obd_uuid server_uuid;
-			char *primary_nid;
-			int prim_nid_len;
-
-			/* iterate all nids to find one */
-			/* find uuid by nid */
-			/* create import entries if they don't exist */
+			/* refresh existing connection NID list with new one */
 			rc = client_import_add_nids_to_conn(imp, nidlist,
-							   entry->mne_nid_count,
-							   entry->mne_nid_size,
-							   uuid);
-			if (rc != -ENOENT || !dynamic_nids)
-				continue;
-
-			/* create a new connection for this import */
-			primary_nid = libcfs_nidstr(&nidlist[0]);
-			prim_nid_len = strlen(primary_nid) + 1;
-			if (prim_nid_len > UUID_MAX)
-				goto fail;
-
-			strncpy(server_uuid.uuid, primary_nid,
-				prim_nid_len);
-
-			CDEBUG(D_INFO, "Adding a connection for %s\n",
-			       primary_nid);
-
-			rc = client_import_dyn_add_conn(imp, &server_uuid,
-							&nidlist[0], 1);
-			if (rc < 0) {
-				CERROR("%s: Failed to add new connection with NID '%s' to import: rc = %d\n",
-				       obd->obd_name, primary_nid, rc);
-				goto fail;
-			}
-
-			rc = client_import_add_nids_to_conn(imp, nidlist,
-							   entry->mne_nid_count,
-							   entry->mne_nid_size,
-							   uuid);
-			if (rc < 0)
-				CERROR("%s: failed to lookup UUID: rc = %d\n",
-				       obd->obd_name, rc);
-fail:;
+							entry->mne_nid_count,
+							entry->mne_nid_size,
+							uuid);
+			if (rc == -ENOENT &&
+			    (dynamic_nids || obd->obd_dynamic_nids))
+				rc = mgc_create_new_conn(imp, nidlist,
+							 entry->mne_nid_count,
+							 entry->mne_nid_size,
+							 uuid);
 		}
 
 		if (rc == -ENODEV) {

lnet/lnet/peer.c
@@ -1342,7 +1342,7 @@ int
 LNetAddPeer(struct lnet_nid *nids, u32 num_nids)
 {
 	struct lnet_nid pnid = LNET_ANY_NID;
-	bool mr;
+	bool discovery;
 	int i, rc;
 	int flags = lock_prim_nid ? LNET_PEER_LOCK_PRIMARY : 0;
 
@@ -1355,7 +1355,7 @@ LNetAddPeer(struct lnet_nid *nids, u32 num_nids)
 
 	mutex_lock(&the_lnet.ln_api_mutex);
 
-	mr = lnet_peer_discovery_disabled == 0;
+	discovery = !lnet_peer_discovery_disabled;
 
 	rc = 0;
 
@@ -1363,9 +1363,14 @@ LNetAddPeer(struct lnet_nid *nids, u32 num_nids)
 		if (nid_is_lo0(&nids[i]))
 			continue;
 
+		/* only add NIDs on local networks if discovery is off */
+		if (!discovery && !lnet_islocalnet(LNET_NID_NET(&nids[i])))
+			continue;
+
 		if (LNET_NID_IS_ANY(&pnid)) {
 			pnid = nids[i];
-			rc = lnet_add_peer_ni(&pnid, &LNET_ANY_NID, mr, flags);
+			rc = lnet_add_peer_ni(&pnid, &LNET_ANY_NID, discovery,
+					      flags);
 			if (rc == -EALREADY) {
 				struct lnet_peer *lp;
 
@@ -1378,15 +1383,17 @@ LNetAddPeer(struct lnet_nid *nids, u32 num_nids)
 				pnid = lp->lp_primary_nid;
 				/* Drop refcount from lookup */
 				lnet_peer_decref_locked(lp);
-			} else if (mr && !rc) {
+			} else if (rc) {
+				/* reset pnid back to ANY if error */
+				pnid = LNET_ANY_NID;
+			} else if (discovery) {
 				lnet_discover_peer_nid(&pnid);
 			}
-		} else if (lnet_peer_discovery_disabled) {
-			rc = lnet_add_peer_ni(&nids[i], &LNET_ANY_NID, mr,
+		} else if (!discovery) {
+			rc = lnet_add_peer_ni(&nids[i], &LNET_ANY_NID, false,
 					      flags);
 		} else if (!nid_same(&pnid, &nids[i])) {
-			rc = lnet_add_peer_ni(&nids[i], &LNET_ANY_NID,
-					      mr, 0);
+			rc = lnet_add_peer_ni(&nids[i], &LNET_ANY_NID, true, 0);
 			if (!rc) {
 				if (lock_prim_nid) {
 					struct lnet_peer *lp;

lustre/ldlm/ldlm_lib.c
@@ -142,6 +142,8 @@ int client_import_dyn_add_conn(struct obd_import *imp, struct obd_uuid *uuid,
 			       imp->imp_obd->obd_name, str_uuid, rc);
 			return rc;
 		}
+	} else {
+		ptlrpc_connection_put(ptlrpc_conn);
 	}
 	return import_set_conn(imp, uuid, priority, 1);
 }
@@ -161,7 +163,11 @@ int client_import_add_nids_to_conn(struct obd_import *imp,
 
 	spin_lock(&imp->imp_lock);
 	list_for_each_entry(conn, &imp->imp_conn_list, oic_item) {
-		if (class_check_uuid(&conn->oic_uuid, &nidlist[0])) {
+		int i;
+
+		for (i = 0; i < nid_count; i++) {
+			if (!class_check_uuid(&conn->oic_uuid, &nidlist[i]))
+				continue;
 			*uuid = conn->oic_uuid;
 			spin_unlock(&imp->imp_lock);
 			rc = class_add_nids_to_uuid(&conn->oic_uuid, nidlist,
@@ -519,6 +525,17 @@ int client_obd_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 	}
 
 	rc = client_import_add_conn(imp, &server_uuid, 1);
+	if (rc == -ENOENT) {
+		CWARN("%s: config has no valid NIDs, force dynamic NIDs\n",
+		      obd->obd_name);
+		rc = 0;
+		/* set dynamic nids parameter in client OBD, so MGC will
+		 * detects that while process IR log for that OBD
+		 */
+		spin_lock(&obd->obd_dev_lock);
+		obd->obd_dynamic_nids = 1;
+		spin_unlock(&obd->obd_dev_lock);
+	}
 	if (rc) {
 		CERROR("can't add initial connection\n");
 		GOTO(err_import, rc);

lustre/tests/conf-sanity.sh
@@ -6638,6 +6638,49 @@ test_73d() { #LU-18896
 }
 run_test 73d "erase + new parameter doesn't corrupt mountdata"
 
+test_73e() {
+	(( $MDS1_VERSION >= $(version_code 2.16.57) )) ||
+		skip "Need server version at least 2.16.57 for dynamic config"
+	remote_servers || skip "need servers on remote nodes"
+
+	cleanup
+	LOAD_MODULES_REMOTE=true load_modules
+
+	INTERFACES=( $(lnet_if_list) )
+	local inf=${INTERFACES[0]}
+	local net=${NETTYPE}42
+
+	stack_trap "cleanup_73c"
+
+	$LNETCTL lnet configure
+	$LNETCTL set discovery 0
+
+	start_mgsmds
+	start_ost
+
+	for rnode in $(remote_nodes_list); do
+		rinf=$(do_rpc_nodes --quiet $rnode lnet_if_list)
+		echo "Node: $rnode, rinf=$rinf"
+		[[ -z $rinf ]] &&
+			error "Failed to determine interface for $rnode"
+		do_node $rnode "$LNETCTL lnet configure"
+		do_node $rnode "$LNETCTL net show"
+		do_rpc_nodes $rnode "$LNETCTL net add --net $net --if $rinf" ||
+			echo "can't add network $net on $node"
+	done
+
+	$LNETCTL net add --net $net --if $inf
+	$LNETCTL net del --net ${NETTYPE}
+	$LNETCTL net show
+
+	local mgs_nid=$(do_facet mgs $LCTL list_nids | grep $net | head -1)
+	echo "Try to mount to MGS NID $mgs_nid"
+	$MOUNT_CMD $mgs_nid:/$FSNAME $MOUNT ||
+		error "Mount fails on $mgs_nid"
+	umount $MOUNT
+}
+run_test 73e "Mount client with dynamic server NIDs"
+
 # LU-15246
 test_74() {
 	(( $MDS1_VERSION >= $(version_code 2.15.57.16) )) ||

commit cd049d0a0d3c5124f5fa927ecc757e9de7fc9b6b

LU-19748 gni: build fixes for gni
libcfs/linux/linux-mem.h -> lustre_compat/linux/linux-mem.h
libcfs/linux/linux-misc.h -> lustre_compat/linux/linux-misc.h

__ll_vmalloc() -> __compat_vmalloc()

missing semi-colon after:
  ENUM2STR(GNI_RC_SUCCESS)

GNI needs to include:
  lustre_compat/linux/timer.h
  linux/libcfs/libcfs_fail.h

Build fix: -Werror=maybe-uninitialized

  .../lustre/mgc/mgc_request.c: In function 'mgc_create_new_conn':
  .../include/linux/libcfs/libcfs_debug.h:122:3: error: 'rc' may be
  used uninitialized in this function [-Werror=maybe-uninitialized]

      libcfs_debug_msg(&msgdata, format, ## __VA_ARGS__);
      ^~~~~~~~~~~~~~~~
  .../lustre/mgc/mgc_request.c:1208:6: note: 'rc' was declared here
     int rc;
         ^~

HPE-bug-id: LUS-13154
Test-Parameters: trivial
Fixes: a050dfb80dd5 ("LU-19229 mgc: client to handle IR with unknown nets")
Fixes: dbf4d41d9879 ("LU-18687 libcfs: move include to top-level)"
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I16dd895b2ba5a6322a0c629b083760ed930b659f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63216
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/klnds/gnilnd/gnilnd.h
@@ -50,6 +50,8 @@
 #include <linux/in.h>
 #include <linux/nmi.h>
 
+#include <lustre_compat/linux/timer.h>
+#include <linux/libcfs/libcfs_fail.h>
 #include <lnet/lib-lnet.h>
 
 #include <gni_pub.h>
@@ -992,10 +994,11 @@ static inline void *kgnilnd_vzalloc(int size)
 {
 	void *ret;
 	if (*kgnilnd_tunables.kgn_vzalloc_noretry)
-		ret = __ll_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO | __GFP_ZERO |
-				   __GFP_NORETRY);
+		ret = __compat_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO |
+				       __GFP_ZERO | __GFP_NORETRY);
 	else
-		ret = __ll_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO | __GFP_ZERO);
+		ret = __compat_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO |
+				       __GFP_ZERO);
 
 	LIBCFS_ALLOC_POST(ret, size, "alloc");
 	return ret;

lnet/klnds/gnilnd/gnilnd_api_wrap.h
@@ -109,7 +109,7 @@ kgnilnd_api_rc2str(gni_return_t rrc)
 {
 
 	switch (rrc) {
-		ENUM2STR(GNI_RC_SUCCESS)
+		ENUM2STR(GNI_RC_SUCCESS);
 		ENUM2STR(GNI_RC_NOT_DONE);
 		ENUM2STR(GNI_RC_INVALID_PARAM);
 		ENUM2STR(GNI_RC_ERROR_RESOURCE);

lnet/klnds/gnilnd/gnilnd_cb.c
@@ -16,7 +16,7 @@
 #include <linux/nmi.h>
 #include <linux/pagemap.h>
 
-#include <libcfs/linux/linux-mem.h>
+#include <lustre_compat/linux/linux-mem.h>
 
 #include "gnilnd.h"
 

lnet/klnds/gnilnd/gnilnd_sysctl.c
@@ -15,7 +15,7 @@
 
 #define DEBUG_SUBSYSTEM S_LND
 
-#include <libcfs/linux/linux-misc.h>
+#include <lustre_compat/linux/linux-misc.h>
 
 #include "gnilnd.h"
 

lustre/mgc/mgc_request.c
@@ -1206,7 +1206,7 @@ static int mgc_create_new_conn(struct obd_import *imp, struct lnet_nid *nidlist,
 	struct obd_uuid node_uuid;
 	char prim_nid[LNET_NIDSTR_SIZE] = { 0 };
 	int i = 0;
-	int rc;
+	int rc = 0;
 
 	/* client has no existing connection to that target yet, and
 	 * it has list of target NIDs, which may have NIDs on

lustre_compat/fs/crypto/llcrypt_private.h
@@ -16,6 +16,7 @@
 #define _LLCRYPT_PRIVATE_H
 
 #include <lustre_compat/linux/llcrypt.h>
+#include <lustre_compat/linux/linux-mem.h>
 #include <linux/libcfs/libcfs_debug.h>
 #include <linux/libcfs/libcfs_private.h>
 #include <crypto/hash.h>