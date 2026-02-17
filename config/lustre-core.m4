lustre/lustre-release

commit 59080e87c74779ce2dec4145a02f9a885f8aa976

LU-18687 compat: move folio_memcg_lock/unlock() import to symbols.c
Move folio_memcg_lock/unlock() import to the standard location
for importing symbols, lustre_compat/symbols.c.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I2c9a24e27677a63a97d4da26075d364e62d6edda
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59835
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre_compat/symbols.c
@@ -10,6 +10,7 @@
  */
 
 #include <linux/kprobes.h>
+#include <linux/memcontrol.h>
 #include <lustre_compat/linux/security.h>
 #include <lustre_compat/linux/workqueue.h>
 
@@ -84,6 +85,24 @@ EXPORT_SYMBOL(compat_apply_workqueue_attrs);
 # define ALLOC_WQ_ATTRS_FUNC	"alloc_workqueue_attrs"
 #endif
 
+#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK)
+static void (*__folio_memcg_lock)(struct folio *folio);
+
+void folio_memcg_lock(struct folio *folio)
+{
+	__folio_memcg_lock(folio);
+}
+EXPORT_SYMBOL_GPL(folio_memcg_lock);
+
+static void (*__folio_memcg_unlock)(struct folio *folio);
+
+void folio_memcg_unlock(struct folio *folio)
+{
+	__folio_memcg_unlock(folio);
+}
+EXPORT_SYMBOL_GPL(folio_memcg_unlock);
+#endif
+
 #ifdef CONFIG_SECURITY
 static int (*__security_file_alloc)(struct file *file);
 static void (*__security_file_free)(struct file *file);
@@ -124,6 +143,15 @@ int lustre_symbols_init(void)
 	if (!__apply_workqueue_attrs)
 		return -EINVAL;
 
+#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK)
+	__folio_memcg_lock = cfs_kallsyms_lookup_name("folio_memcg_lock");
+	if (!__folio_memcg_lock)
+		return -EINVAL;
+
+	__folio_memcg_unlock = cfs_kallsyms_lookup_name("folio_memcg_unlock");
+	if (!__folio_memcg_unlock)
+		return -EINVAL;
+#endif
 #ifdef CONFIG_SECURITY
 	__security_file_alloc = cfs_kallsyms_lookup_name("security_file_alloc");
 	if (!__security_file_alloc)

libcfs/libcfs/debug.c
@@ -20,7 +20,6 @@
 #include <linux/kthread.h>
 #include <linux/stacktrace.h>
 #include <linux/utsname.h>
-#include <linux/kallsyms.h>
 #include <linux/delay.h>
 #ifdef HAVE_PANIC_NOTIFIER_H
 #include <linux/panic_notifier.h>

lustre/llite/vvp_dev.c
@@ -247,11 +247,6 @@ struct lu_device_type vvp_device_type = {
 
 unsigned int (*vvp_account_page_dirtied)(struct page *page,
 					 struct address_space *mapping);
-#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK) && \
-     defined(HAVE_KALLSYMS_LOOKUP_NAME)
-void (*vvp_folio_memcg_lock)(struct folio *folio);
-void (*vvp_folio_memcg_unlock)(struct folio *folio);
-#endif
 
 /**
  * vvp_global_init() - init global resources required by the VVP layer
@@ -283,17 +278,6 @@ int vvp_global_init(void)
 	vvp_account_page_dirtied = (void *)
 		cfs_kallsyms_lookup_name("account_page_dirtied");
 #endif
-#endif
-
-#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK) && \
-     defined(HAVE_KALLSYMS_LOOKUP_NAME)
-	vvp_folio_memcg_lock = (void *)
-		cfs_kallsyms_lookup_name("folio_memcg_lock");
-	LASSERT(vvp_folio_memcg_lock);
-
-	vvp_folio_memcg_unlock = (void *)
-		cfs_kallsyms_lookup_name("folio_memcg_unlock");
-	LASSERT(vvp_folio_memcg_unlock);
 #endif
 
 	return 0;

lustre/llite/vvp_internal.h
@@ -265,18 +265,8 @@ extern unsigned int (*vvp_account_page_dirtied)(struct page *page,
 #endif
 
 #ifdef HAVE_FOLIO_MEMCG_LOCK
-#ifdef FOLIO_MEMCG_LOCK_EXPORTED
 #define folio_memcg_lock_page(page)	folio_memcg_lock(page_folio((page)))
 #define folio_memcg_unlock_page(page)	folio_memcg_unlock(page_folio((page)))
-#elif defined(HAVE_KALLSYMS_LOOKUP_NAME)
-/* Use kallsyms_lookup_name to acquire folio_memcg_[un]lock */
-extern void (*vvp_folio_memcg_lock)(struct folio *folio);
-extern void (*vvp_folio_memcg_unlock)(struct folio *folio);
-#define folio_memcg_lock_page(page) \
-	vvp_folio_memcg_lock(page_folio((page)))
-#define folio_memcg_unlock_page(page) \
-	vvp_folio_memcg_unlock(page_folio((page)))
-#endif
 #elif defined HAVE_LOCK_PAGE_MEMCG
 #define folio_memcg_lock_page(page)	lock_page_memcg((page))
 #define folio_memcg_unlock_page(page)	unlock_page_memcg((page))

commit cea7a926245f8e5145e097881f12a3253e8b555c

LU-17916 compat: folio_memcg_lock is memcg-v1 only
Linux commit v6.10-rc6-285-ge93d4166b40a8
  mm: memcg: put cgroup v1-specific code under a config option

A new configure option CONFIG_MEMCG_V1 is added under CONFIG_MEMCG
and folio_memcg_lock() may be a static inline function.

Linux commit v6.12-rc6-127-ga29c0e4b2e867
 memcg-v1: remove memcg move locking code

CONFIG_MEMCG_V1 remains however folio_memcg_lock() is removed.

Add a configure test to ensure we do not create an exported
version of a static inline function.

Test-Parameters: trivial
Fixes: 59080e87c74 ("LU-18687 compat: move folio_memcg_lock/unlock() import to symbols.c")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ida26c6ebafef71bbcf79ab6e46d547ddc187a506
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63938
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-core.m4
@@ -3941,6 +3941,31 @@ AC_DEFUN([LC_HAVE_CSUM_TYPE_BLK_INTEGRITY], [
 ]) # LC_HAVE_CSUM_TYPE_BLK_INTEGRITY
 
 #
+# LC_HAVE_FOLIO_MEMCG_LOCK_STATIC
+#
+# Linux commit v6.10-rc6-285-ge93d4166b40a8
+#   mm: memcg: put cgroup v1-specific code under a config option
+# Linux commit v6.12-rc6-127-ga29c0e4b2e867
+#  memcg-v1: remove memcg move locking code
+#
+AC_DEFUN([LC_SRC_HAVE_FOLIO_MEMCG_LOCK_STATIC],[
+	LB2_LINUX_TEST_SRC([folio_memcg_lock_static_inline], [
+		#include <linux/memcontrol.h>
+
+		static inline void folio_memcg_lock(struct folio *folio);
+	],[
+		folio_memcg_lock(NULL);
+	],[])
+])
+AC_DEFUN([LC_HAVE_FOLIO_MEMCG_LOCK_STATIC],[
+	LB2_MSG_LINUX_TEST_RESULT([if folio_memcg_lock() is static inline],
+	[folio_memcg_lock_static_inline], [
+		AC_DEFINE(HAVE_FOLIO_MEMCG_LOCK_STATIC, 1,
+			[folio_memcg_lock() is static inline])
+	])
+]) # LC_HAVE_FOLIO_MEMCG_LOCK_STATIC
+
+#
 # LC_HAVE_LINUX_UNALIGNED_HEADER
 #
 # Linux v6.12-rc1-3-g5f60d5f6bbc1
@@ -4605,6 +4630,9 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	# 6.10
 	LC_SRC_HAVE_CSUM_TYPE_BLK_INTEGRITY
 
+	# 6.11
+	LC_SRC_HAVE_FOLIO_MEMCG_LOCK_STATIC
+
 	# 6.12
 	LC_SRC_HAVE_LINUX_UNALIGNED_HEADER
 	LC_SRC_HAVE_WRITE_BEGIN_FOLIO
@@ -4890,6 +4918,9 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 
 	# 6.10
 	LC_HAVE_CSUM_TYPE_BLK_INTEGRITY
+
+	# 6.11
+	LC_HAVE_FOLIO_MEMCG_LOCK_STATIC
 
 	# 6.12
 	LC_HAVE_LINUX_UNALIGNED_HEADER

lustre_compat/symbols.c
@@ -85,7 +85,8 @@ EXPORT_SYMBOL(compat_apply_workqueue_attrs);
 # define ALLOC_WQ_ATTRS_FUNC	"alloc_workqueue_attrs"
 #endif
 
-#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK)
+#if defined(CONFIG_MEMCG) && !defined(HAVE_FOLIO_MEMCG_LOCK_STATIC) \
+ && defined(HAVE_FOLIO_MEMCG_LOCK) && !defined(FOLIO_MEMCG_LOCK_EXPORTED)
 static void (*__folio_memcg_lock)(struct folio *folio);
 
 void folio_memcg_lock(struct folio *folio)
@@ -143,7 +144,8 @@ int lustre_symbols_init(void)
 	if (!__apply_workqueue_attrs)
 		return -EINVAL;
 
-#if !defined(FOLIO_MEMCG_LOCK_EXPORTED) && defined(HAVE_FOLIO_MEMCG_LOCK)
+#if defined(CONFIG_MEMCG) && !defined(HAVE_FOLIO_MEMCG_LOCK_STATIC) \
+ && defined(HAVE_FOLIO_MEMCG_LOCK) && !defined(FOLIO_MEMCG_LOCK_EXPORTED)
 	__folio_memcg_lock = cfs_kallsyms_lookup_name("folio_memcg_lock");
 	if (!__folio_memcg_lock)
 		return -EINVAL;