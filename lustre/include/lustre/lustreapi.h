lustre/lustre-release

commit 73fd18caa6f7951e88aedd17cd87689f74921a73

LU-17925 utils: fix 'lfs setstripe -c -1' interop
This issue was caused by the -C option was storing -1 as 0xffdf
and -33 was stored as 0xffff, which changed the on-disk file layout
compared to all older versions storing -1 as 0xffff. This resulted
in interop failure of 'lfs setstripe -c/-C' usage.

Instead, store stripe_count as -1 = 0xffff, and -32 = 0xffe0, like
normal unsigned short.  There is no need to distinguish "-c -1"
from "-C -1" in the layout since they both mean the same thing, to
allocate one object per OST.  However, don't allow "-c -2..-32"
since this has no meaning today.  Keep these in reserve in case we
assign some meaning to them in the future.

Restore LLAPI_LAYOUT_WIDE and LOV_ALL_STRIPES definitions, since
these are part of the API and may be used by userspace applications.

Rename LOV_ALL_STRIPES_MIN to LOV_ALL_STRIPES_WIDE since it was
confusing that the "MIN" value created files with the most stripes.

Rename LOV_V1_INSANE_STRIPE_COUNT to LOV_V1_INSANE_STRIPE_INDEX
since it is really the maximum OST index and not a stripe count.

Test-Parameters: testlist=sanity-flr serverversion=2.15 env=SANITY_FLR_EXCEPT="0k 44e 205"
Fixes: 1a6ef725c285 ("LU-16938 utils: setstripe overstripe multiple OST count")
Signed-off-by: Rajeev Mishra <rajeevm@hpe.com>
Change-Id: I3908bd5a70aa35305ef8f278fb0346319055e5b3
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/55548
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/include/lustre/lustreapi.h
@@ -875,10 +875,13 @@ int llapi_layout_merge(struct llapi_layout **dst_layout,
  * When specified or returned as the value for stripe count, all
  * available OSTs will be used.
  */
-#define LLAPI_MIN_STRIPE_COUNT    (-1)
-#define LLAPI_MAX_STRIPE_COUNT    (-32)
-#define LLAPI_LAYOUT_WIDE_MIN	(LLAPI_LAYOUT_INVALID + 2)
-#define LLAPI_LAYOUT_WIDE_MAX	(LLAPI_LAYOUT_WIDE_MIN + 32)
+#define LLAPI_OVERSTRIPE_COUNT_MIN    ((__s16)LOV_ALL_STRIPES)      /*  -1 */
+#define LLAPI_OVERSTRIPE_COUNT_MAX    ((__s16)LOV_ALL_STRIPES_WIDE) /* -32 */
+#define LLAPI_LAYOUT_WIDE_MIN (LLAPI_LAYOUT_DEFAULT - \
+				LLAPI_OVERSTRIPE_COUNT_MIN)
+#define LLAPI_LAYOUT_WIDE_MAX (LLAPI_LAYOUT_DEFAULT - \
+				LLAPI_OVERSTRIPE_COUNT_MAX)
+#define LLAPI_LAYOUT_WIDE     LLAPI_LAYOUT_WIDE_MIN /* backward compatibility */
 
 /**
  * When specified as the value for layout pattern, file objects will be

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -813,12 +813,11 @@ static inline bool lov_pool_is_reserved(const char *pool)
  */
 #define LOV_MAX_STRIPE_COUNT 2000  /* ~((12 * 4096 - 256) / 24) */
 
-/* Below given max and min values are used to check range of stripe count */
-/* LOV_ALL_STRIPES_MAX  LLAPI_MIN_STRIPE_COUNT */
-/* LOV_ALL_STRIPES_MIN  LLAPI_MIN_STRIPE_COUNT+ LLAPI_MAX_STRIPE_COUNT */
-#define LOV_ALL_STRIPES_MAX 0xffff
-#define LOV_ALL_STRIPES_MIN 0xffdf
-#define LOV_V1_INSANE_STRIPE_COUNT 65532 /* maximum stripe count bz13933 */
+/* max and min values are used to check range of overstripe count */
+#define LOV_ALL_STRIPES       0xffff /* only valid for directories */
+#define LOV_ALL_STRIPES_WIDE  0xffe0 /* LLAPI_OVERSTRIPE_COUNT_MAX */
+#define LOV_V1_INSANE_STRIPE_INDEX (LOV_ALL_STRIPES_WIDE - 1) /* max index */
+#define LOV_V1_INSANE_STRIPE_COUNT LOV_V1_INSANE_STRIPE_INDEX /* deprecated */
 
 #define XATTR_LUSTRE_PREFIX	"lustre."
 #define XATTR_LUSTRE_LOV	XATTR_LUSTRE_PREFIX"lov"
@@ -1059,7 +1058,7 @@ struct lov_comp_md_v1 {
 
 static inline __u32 lov_user_md_size(__u16 stripes, __u32 lmm_magic)
 {
-	if (stripes >= LOV_ALL_STRIPES_MIN && stripes <= LOV_ALL_STRIPES_MAX)
+	if (stripes <= LOV_ALL_STRIPES && stripes >= LOV_ALL_STRIPES_WIDE)
 		stripes = 0;
 
 	if (lmm_magic == LOV_USER_MAGIC_V1)

lustre/include/lu_object.h
@@ -1641,7 +1641,7 @@ static inline __u64 tgt_statfs_iavail(struct lu_tgt_desc *tgt)
 /* number of pointers at 2nd level */
 #define TGT_PTRS_PER_BLOCK	(PAGE_SIZE / sizeof(void *))
 /* number of pointers at 1st level - only need as many as max OST/MDT count */
-#define TGT_PTRS		((LOV_ALL_STRIPES_MAX + 1) / TGT_PTRS_PER_BLOCK)
+#define TGT_PTRS		((LOV_ALL_STRIPES + 1) / TGT_PTRS_PER_BLOCK)
 
 struct lu_tgt_desc_idx {
 	struct lu_tgt_desc *ldi_tgt[TGT_PTRS_PER_BLOCK];

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1308,7 +1308,7 @@ struct lov_mds_md_v3 {            /* LOV EA mds/wire data (little-endian) */
 
 static inline __u32 lov_mds_md_size(__u16 stripes, __u32 lmm_magic)
 {
-	if (stripes >= LOV_ALL_STRIPES_MIN && stripes <= LOV_ALL_STRIPES_MAX)
+	if (stripes <= LOV_ALL_STRIPES && stripes >= LOV_ALL_STRIPES_WIDE)
 		stripes = 0;
 
 	if (lmm_magic == LOV_MAGIC_V1)

lustre/lmv/lmv_obd.c
@@ -160,7 +160,7 @@ retry:
 		     "%s: MDT index %u more than MDT count %u\n",
 		     obd->obd_name, index, lmv->lmv_mdt_count);
 
-	if (index >= LOV_V1_INSANE_STRIPE_COUNT)
+	if (index >= LOV_V1_INSANE_STRIPE_INDEX)
 		return NULL;
 
 	if (now > next_print) {

lustre/lod/lod_object.c
@@ -2731,8 +2731,8 @@ __u16 lod_comp_entry_stripe_count(struct lod_object *lo, int comp_idx,
 	entry = &lo->ldo_comp_entries[comp_idx];
 	if (lod_comp_inited(entry))
 		return entry->llc_stripe_count;
-	if (entry->llc_stripe_count >= LOV_ALL_STRIPES_MIN &&
-	     entry->llc_stripe_count <= LOV_ALL_STRIPES_MAX)
+	if (entry->llc_stripe_count <= LOV_ALL_STRIPES &&
+	    entry->llc_stripe_count >= LOV_ALL_STRIPES_WIDE)
 		return lod_get_stripe_count_plain(lod, lo,
 						  entry->llc_stripe_count,
 						  entry->llc_pattern &
@@ -2886,8 +2886,8 @@ static int lod_declare_layout_add(const struct lu_env *env,
 		    (lod_comp_inited(lod_comp) ||
 		     lod_comp->llc_extent.e_start <
 		     lod_comp->llc_extent.e_end) &&
-		     !(lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_MIN &&
-		       lod_comp->llc_stripe_count <= LOV_ALL_STRIPES_MAX) &&
+		    !(lod_comp->llc_stripe_count <= LOV_ALL_STRIPES &&
+		      lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_WIDE) &&
 		    ext->e_end != OBD_OBJECT_EOF &&
 		    (__u64)(lod_comp->llc_stripe_count *
 			    lod_comp->llc_stripe_size) >
@@ -5509,15 +5509,11 @@ static int lod_get_default_lov_striping(const struct lu_env *env,
 			}
 		}
 
-		CDEBUG(D_LAYOUT, DFID" magic = %#08x, pattern = %#x, stripe_count = %hu, stripe_size = %u, stripe_offset = %hu, append_pool = '%s', append_stripe_count = %d\n",
-		       PFID(lu_object_fid(&lo->ldo_obj.do_lu)),
-		       v1->lmm_magic,
-		       v1->lmm_pattern,
-		       v1->lmm_stripe_count,
-		       v1->lmm_stripe_size,
-		       v1->lmm_stripe_offset,
-		       append_pool ?: "",
-		       append_stripe_count);
+		CDEBUG(D_LAYOUT, DFID" magic = %#08x, pattern = %#x, stripe_count = %hd, stripe_size = %u, stripe_offset = %hd, append_pool = '%s', append_stripe_count = %d\n",
+		       PFID(lu_object_fid(&lo->ldo_obj.do_lu)), v1->lmm_magic,
+		       v1->lmm_pattern, (__s16)v1->lmm_stripe_count,
+		       v1->lmm_stripe_size, (__s16)v1->lmm_stripe_offset,
+		       append_pool ?: "", append_stripe_count);
 
 		if (!lov_pattern_supported(v1->lmm_pattern) &&
 		    !(v1->lmm_pattern & LOV_PATTERN_F_RELEASED)) {

lustre/lod/lod_qos.c
@@ -2069,30 +2069,28 @@ __u16 lod_get_stripe_count_plain(struct lod_device *lod, struct lod_object *lo,
 {
 	struct lov_desc *lov_desc = &lod->lod_ost_descs.ltd_lov_desc;
 
-	/* Overstriping allows more stripes than targets */
-	if (stripe_count > lov_desc->ld_active_tgt_count) {
-		if (overstriping) {
-			if (stripe_count >= LOV_ALL_STRIPES_MIN &&
-				stripe_count <= LOV_ALL_STRIPES_MAX) {
-				stripe_count =
-				((stripe_count - LOV_ALL_STRIPES_MIN) + 1) *
-				lov_desc->ld_active_tgt_count;
-			}
-		} else {
-			*flags |= LOD_USES_DEFAULT_STRIPE;
-			if ((stripe_count >= LOV_ALL_STRIPES_MIN &&
-			     stripe_count <= LOV_ALL_STRIPES_MAX) &&
-			     lod->lod_max_stripecount)
-				stripe_count = lod->lod_max_stripecount;
-			else
-				stripe_count = lov_desc->ld_active_tgt_count;
-		}
-	}
-
 	if (!stripe_count)
 		stripe_count = lov_desc->ld_default_stripe_count;
 
-	if (overstriping && stripe_count > LOV_MAX_STRIPE_COUNT)
+	/* Overstriping allows more stripes than targets */
+	if (stripe_count > lov_desc->ld_active_tgt_count) {
+		if (!overstriping) {
+			*flags |= LOD_USES_DEFAULT_STRIPE;
+			if (stripe_count == LOV_ALL_STRIPES &&
+			    lod->lod_max_stripecount)
+				stripe_count = lod->lod_max_stripecount;
+			else
+				stripe_count = lov_desc->ld_active_tgt_count;
+		} else if (stripe_count <= LOV_ALL_STRIPES &&
+			   stripe_count >= LOV_ALL_STRIPES_WIDE) {
+			stripe_count = lov_desc->ld_active_tgt_count *
+				(LOV_ALL_STRIPES - stripe_count + 1);
+		}
+	}
+	if (!stripe_count)
+		stripe_count = 1;
+
+	if (stripe_count > LOV_MAX_STRIPE_COUNT)
 		stripe_count = LOV_MAX_STRIPE_COUNT;
 
 	return stripe_count;
@@ -2308,9 +2306,9 @@ int lod_use_defined_striping(const struct lu_env *env,
 		    !(lod_comp->llc_flags & LCME_FL_EXTENSION) &&
 		    (lod_comp_inited(lod_comp) ||
 		     lod_comp->llc_extent.e_start <
-		     lod_comp->llc_extent.e_end) &&
-		    !(lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_MIN &&
-		      lod_comp->llc_stripe_count <= LOV_ALL_STRIPES_MAX) &&
+						lod_comp->llc_extent.e_end) &&
+		    !(lod_comp->llc_stripe_count <= LOV_ALL_STRIPES &&
+		      lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_WIDE) &&
 		    lod_comp->llc_extent.e_end != OBD_OBJECT_EOF &&
 		    (__u64)lod_comp->llc_stripe_count *
 			   lod_comp->llc_stripe_size >
@@ -2605,11 +2603,11 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 		 */
 		if (lo->ldo_is_composite &&
 		    !(lod_comp->llc_flags & LCME_FL_EXTENSION) &&
-		    !(lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_MIN &&
-		      lod_comp->llc_stripe_count <= LOV_ALL_STRIPES_MAX) &&
+		    !(lod_comp->llc_stripe_count <= LOV_ALL_STRIPES &&
+		      lod_comp->llc_stripe_count >= LOV_ALL_STRIPES_WIDE) &&
 		    (lod_comp_inited(lod_comp) ||
 		     lod_comp->llc_extent.e_start <
-		     lod_comp->llc_extent.e_end) &&
+						lod_comp->llc_extent.e_end) &&
 		    lod_comp->llc_extent.e_end != OBD_OBJECT_EOF &&
 		    lod_comp->llc_stripe_count * lod_comp->llc_stripe_size >
 		    (lod_comp->llc_extent.e_end - lod_comp->llc_extent.e_start))

lustre/lov/lov_ea.c
@@ -298,7 +298,7 @@ retry_new_ost:
 				lov->desc.ld_uuid.uuid, POSTID(&loi->loi_oi),
 				loi->loi_ost_idx, lov->desc.ld_tgt_count);
 
-			if ((u32)loi->loi_ost_idx >= LOV_V1_INSANE_STRIPE_COUNT)
+			if ((u32)loi->loi_ost_idx >= LOV_V1_INSANE_STRIPE_INDEX)
 				GOTO(out_lsme, rc = -EINVAL);
 
 			if (now > next_print) {

commit f8fc0d2d5048c9b9d24e87c3c921a7f5c28b1695

LU-17925 llapi: quiet -Wsign-conversion warnings
Builds with some compilers warn when using LLAPI_LAYOUT_WIDE_MIN
about sign conversion when subtracting a "negative" unsigned value:

    uint64_t stripe_count;

    warning: negative integer implicitly converted to unsigned type
    stripe_count = LLAPI_LAYOUT_WIDE;
                   ^~~~~~~~~~~~~~~~~

Change subtraction of a negative value to addition to avoid this.
Fix other similar issues in UAPI headers for sign conversion warnings.

The 'hur_len' return type was silently changed from ssize_t (signed)
to __kernel_size_t (unsigned) in commit v2_13_53-78-gb93ef29660,
but it is also too old to change it back.  It could never return an
error on a 64-bit architecture (max 2^32 * (32 + 1) ~= 2^37 bytes),
so disable impossible/unused negative error in this case.
On 32-bit systems (are any left?) return ~0UL = -1 in the caller.

Github: fixes #15

Test-Parameters: trivial testlist=sanity-hsm
Fixes: 73fd18caa6 ("LU-17925 utils: fix 'lfs setstripe -c -1' interop")
Fixes: b93ef29660 ("LU-13274 uapi: make lustre UAPI headers pass CONFIG_UAPI_HEADER_TEST")
Reported-by: Wei-keng Liao <wkliao@northwestern.edu>
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I11fece9eb1de9c49013849172f35d9aca1500c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63797
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Timothy Day <timday@amazon.com>

lustre/include/lustre/lustreapi.h
@@ -998,10 +998,10 @@ int llapi_layout_merge(struct llapi_layout **dst_layout,
  */
 #define LLAPI_OVERSTRIPE_COUNT_MIN    ((__s16)LOV_ALL_STRIPES)      /*  -1 */
 #define LLAPI_OVERSTRIPE_COUNT_MAX    ((__s16)LOV_ALL_STRIPES_WIDE) /* -32 */
-#define LLAPI_LAYOUT_WIDE_MIN (LLAPI_LAYOUT_DEFAULT - \
-				LLAPI_OVERSTRIPE_COUNT_MIN)
-#define LLAPI_LAYOUT_WIDE_MAX (LLAPI_LAYOUT_DEFAULT - \
-				LLAPI_OVERSTRIPE_COUNT_MAX)
+#define LLAPI_LAYOUT_WIDE_MIN (LLAPI_LAYOUT_DEFAULT + \
+			       (-LLAPI_OVERSTRIPE_COUNT_MIN))
+#define LLAPI_LAYOUT_WIDE_MAX (LLAPI_LAYOUT_DEFAULT + \
+			       (-LLAPI_OVERSTRIPE_COUNT_MAX))
 #define LLAPI_LAYOUT_WIDE     LLAPI_LAYOUT_WIDE_MIN /* backward compatibility */
 
 /**

lustre/include/uapi/linux/lustre/lustre_fiemap.h
@@ -35,24 +35,24 @@
  */
 #define fe_device	fe_reserved[0]
 
-static inline int get_fe_device(struct fiemap_extent *fe)
+static inline unsigned int get_fe_device(struct fiemap_extent *fe)
 {
 	return fe->fe_device & 0x1ffff;
 }
-static inline void set_fe_device(struct fiemap_extent *fe, int devno)
+static inline void set_fe_device(struct fiemap_extent *fe, unsigned int devno)
 {
 	fe->fe_device = (fe->fe_device & 0xfffe0000) | (devno & 0x1ffff);
 }
-static inline int get_fe_stripenr(struct fiemap_extent *fe)
+static inline unsigned int get_fe_stripenr(struct fiemap_extent *fe)
 {
 	return fe->fe_device >> 17;
 }
-static inline void set_fe_stripenr(struct fiemap_extent *fe, int nr)
+static inline void set_fe_stripenr(struct fiemap_extent *fe, unsigned int nr)
 {
 	fe->fe_device = (fe->fe_device & 0x1ffff) | (nr << 17);
 }
-static inline void set_fe_device_stripenr(struct fiemap_extent *fe, int devno,
-					  int nr)
+static inline void set_fe_device_stripenr(struct fiemap_extent *fe,
+					  unsigned int devno, unsigned int nr)
 {
 	fe->fe_device = (nr << 17) | (devno & 0x1ffff);
 }

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1362,9 +1362,10 @@ enum {
 	LMV_INHERIT_RR_UNLIMITED	= 255,
 };
 
-static inline int lmv_user_md_size(int stripes, int lmm_magic)
+static inline unsigned int lmv_user_md_size(unsigned int stripes,
+					    unsigned int lmm_magic)
 {
-	int size = sizeof(struct lmv_user_md);
+	unsigned int size = sizeof(struct lmv_user_md);
 
 	if (lmm_magic == LMV_USER_MAGIC_SPECIFIC)
 		size += stripes * sizeof(struct lmv_user_mds_data);
@@ -1433,9 +1434,13 @@ static inline const char *obd_uuid2str(const struct obd_uuid *uuid)
  * e.g. (myfs-OST0007_UUID -> myfs)
  * see also deuuidify.
  */
-static inline void obd_uuid2fsname(char *buf, char *uuid, int buflen)
+static inline void obd_uuid2fsname(char *buf, char *uuid,
+				   unsigned int buflen)
 {
 	char *p;
+
+	if (buflen == 0)
+		return;
 
 	strncpy(buf, uuid, buflen - 1);
 	buf[buflen - 1] = '\0';
@@ -1928,7 +1933,7 @@ static inline void hsm_set_cl_event(enum changelog_rec_flags *clf_flags,
 				    enum hsm_event he)
 {
 	*clf_flags = (enum changelog_rec_flags)
-		(*clf_flags | (he << CLF_HSM_EVENT_L));
+		((__u32)*clf_flags | ((__u32)he << CLF_HSM_EVENT_L));
 }
 
 static inline __u16 hsm_get_cl_flags(enum changelog_rec_flags clf_flags)
@@ -1940,7 +1945,7 @@ static inline void hsm_set_cl_flags(enum changelog_rec_flags *clf_flags,
 				    unsigned int bits)
 {
 	*clf_flags = (enum changelog_rec_flags)
-		(*clf_flags | (bits << CLF_HSM_FLAG_L));
+		((__u32)*clf_flags | (__u32)(bits << CLF_HSM_FLAG_L));
 }
 
 static inline int hsm_get_cl_error(enum changelog_rec_flags clf_flags)
@@ -2252,8 +2257,12 @@ static inline char *changelog_rec_sname(const struct changelog_rec *rec)
 static
 inline __kernel_size_t changelog_rec_snamelen(const struct changelog_rec *rec)
 {
-	return rec->cr_namelen -
-	       (changelog_rec_sname(rec) - changelog_rec_name(rec));
+	size_t snamelen;
+
+	/* always positive but < cr_namelen,(see changelog_rec_sname() code */
+	snamelen = (size_t)(changelog_rec_sname(rec) - changelog_rec_name(rec));
+
+	return rec->cr_namelen - snamelen;
 }
 
 enum changelog_message_type {
@@ -2492,23 +2501,25 @@ static inline void *hur_data(struct hsm_user_request *hur)
 }
 
 /**
- * Compute the current length of the provided hsm_user_request.  This returns -1
- * instead of an errno because __kernel_ssize_t is defined to be only
- * [ -1, SSIZE_MAX ]
+ * Compute the current length of the provided hsm_user_request. This returns
+ * ~0UL (-1 for 32-bit arches) instead of an errno because __kernel_ssize_t
+ * is defined to be only [ -1, SSIZE_MAX ] there.  On 64-bit architectures
+ * the max return value is 2^32 * (sizeof(hur_user_item) + 1) ~= 2^37 bytes.
  *
- * return -1 on bounds check error.
+ * return -1 on bounds check error (32-bit only).
  */
 static inline __kernel_size_t hur_len(struct hsm_user_request *hur)
 {
-	__u64	size;
+	__u64 size;
 
 	/* can't overflow a __u64 since hr_itemcount is only __u32 */
 	size = offsetof(struct hsm_user_request, hur_user_item[0]) +
 		(__u64)hur->hur_request.hr_itemcount *
 		sizeof(hur->hur_user_item[0]) + hur->hur_request.hr_data_len;
 
-	if ((__kernel_ssize_t)size < 0)
-		return -1;
+	/* this "if (0 && ..)" is removed by the compiler on 64-bit */
+	if (sizeof(__kernel_size_t) == 4 && (__kernel_ssize_t)size < 0)
+		return ~0UL;
 
 	return size;
 }

lustre/lov/lov_object.c
@@ -1666,11 +1666,13 @@ static int fiemap_calc_last_stripe(struct lov_stripe_md *lsm, int index,
  */
 static void fiemap_prepare_and_copy_exts(struct fiemap *fiemap,
 					 struct fiemap_extent *lcl_fm_ext,
-					 int ost_index, unsigned int ext_count,
-					 int current_extent, int abs_stripeno)
+					 unsigned int ost_index,
+					 unsigned int ext_count,
+					 unsigned int current_extent,
+					 unsigned int abs_stripeno)
 {
-	char		*to;
-	unsigned int	ext;
+	unsigned int ext;
+	char *to;
 
 	for (ext = 0; ext < ext_count; ext++) {
 		set_fe_device_stripenr(&lcl_fm_ext[ext], ost_index,
@@ -1974,18 +1976,18 @@ static int lov_object_fiemap(const struct lu_env *env, struct cl_object *obj,
 	struct fiemap *fm_local = NULL;
 	loff_t whole_start;
 	loff_t whole_end;
-	int entry;
-	int start_entry = -1;
-	int end_entry;
-	int cur_stripe = 0;
-	int stripe_count;
+	unsigned int entry;
+	unsigned int start_entry = ~0U;
+	unsigned int end_entry;
+	unsigned int cur_stripe = 0;
+	unsigned int stripe_count;
 	unsigned int buffer_size = FIEMAP_BUFFER_SIZE;
 	int rc = 0;
 	struct fiemap_state fs = { 0 };
 	struct lu_extent range;
-	int cur_ext;
-	int stripe_last = 0;
-	int start_stripe = 0;
+	unsigned int cur_ext;
+	unsigned int stripe_last = 0;
+	unsigned int start_stripe = 0;
 	bool resume = false;
 	ENTRY;
 
@@ -2094,9 +2096,9 @@ static int lov_object_fiemap(const struct lu_env *env, struct cl_object *obj,
 		cur_stripe += stripes;
 	}
 
-	if (start_entry == -1) {
-		CERROR(DFID": FIEMAP does not init start entry, cur_stripe=%d, "
-		       "stripe_last=%d\n", PFID(lu_object_fid(&obj->co_lu)),
+	if (start_entry == ~0U) {
+		CERROR(DFID": FIEMAP does not init start entry, cur_stripe=%u, "
+		       "stripe_last=%u\n", PFID(lu_object_fid(&obj->co_lu)),
 		       cur_stripe, stripe_last);
 		GOTO(out_fm_local, rc = -EINVAL);
 	}