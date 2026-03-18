lustre/lustre-release

commit 45418aa71a53141e8110b8e554dde1162c3fcb14

LU-19859 scripts: Use actual bash binary
Replace #!/bin/bash with #!/usr/bin/bash in scripts to avoid conflicts with
/bin/sh on RHEL 9. Also replace #!/bin/sh with #!/usr/bin/bash in scripts
shebangs because /bin/sh is not always bash.

Change-Id: Ibad5c2e7226df3fffa24faa1777074c23067b18f
Signed-off-by: Nikos Papakonstantinou <npapakonstantinou@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64112
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

contrib/git-cmds/git-checkpatch
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # SPDX-License-Identifier: GPL-2.0-only
 
 #

contrib/git-cmds/git-tabcheck
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # SPDX-License-Identifier: GPL-2.0-only
 
 #

contrib/git-hooks/commit-msg
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 #
 # A hook script to check the commit log message to ensure that it has
 # a well-formed commit summary and body, a valid Signed-off-by: line,

contrib/git-hooks/prepare-commit-msg
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
 #

contrib/scripts/clearpatches.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 PROG=$(basename $0)
 [ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
 [ "$1" = "-d" ] && shift && DELETE="git rm" || DELETE="echo Unused"

contrib/scripts/confirmpatches.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 PROG=$(basename $0)
 [ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
 [ "$1" = "-v" ] && shift && VERBOSE="echo" || VERBOSE=":"

contrib/scripts/fscrypt_inclusion.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 #
 # Purpose of this script is to show the method used to integrate fscrypt

contrib/scripts/generate-manpage.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Generate a man page with section-specific content
 # Usage: ./generate_manpage.sh NAME SECTION [OUTPUT_FILE]

commit 6432aaffed27a27c40394765b3781e9ba3bfc770

LU-19859 scripts: fix bash path for developer scripts
Hard-coding /usr/bin/bash prevents editing files on MacOS with
the Git commit hooks enabled, since MacOS still uses /bin/bash.
Change them over to "/usr/bin/env bash" so they run everywhere.

Test-Parameters: trivial
Fixes: 45418aa71a ("LU-19859 scripts: Use actual bash binary")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I364af44d277a4746eae561df8ab837d1313ebbe5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64447
Reviewed-by: Nathaniel Clark <nclark@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Timothy Day <tday141@gmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

contrib/git-cmds/git-checkpatch
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-only
 
 #

contrib/git-cmds/git-tabcheck
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-only
 
 #

contrib/git-hooks/commit-msg
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 # A hook script to check the commit log message to ensure that it has
 # a well-formed commit summary and body, a valid Signed-off-by: line,

contrib/git-hooks/prepare-commit-msg
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # SPDX-License-Identifier: GPL-2.0
 
 #

contrib/scripts/clearpatches.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 PROG=$(basename $0)
 [ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
 [ "$1" = "-d" ] && shift && DELETE="git rm" || DELETE="echo Unused"

contrib/scripts/confirmpatches.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 PROG=$(basename $0)
 [ "$1" = "-h" -o "$1" = "--help" ] && echo "usage: $PROG [patch dir]" && exit 0
 [ "$1" = "-v" ] && shift && VERBOSE="echo" || VERBOSE=":"

contrib/scripts/fscrypt_inclusion.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #
 # Purpose of this script is to show the method used to integrate fscrypt

contrib/scripts/generate-manpage.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Generate a man page with section-specific content
 # Usage: ./generate_manpage.sh NAME SECTION [OUTPUT_FILE]

contrib/scripts/kernel_doc_helper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0
 
 #

contrib/scripts/license-status
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-2.0-only
 
 #

contrib/scripts/objdump-resolve-str.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 function usage() {
 	echo "Usage: $0 .."

contrib/scripts/updatecw.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # Adds Intel copyright notices to files modified by Intel.
 # Does not add copyright notices to files that are missing them.
 #