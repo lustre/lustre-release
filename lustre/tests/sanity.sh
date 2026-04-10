lustre/lustre-release

commit 11b26155714118d38348eadde91ce623ec570ec6

LU-2738 mdt: add proc entry to enable lfs mkdir for non-admin
Add enable_remote_dir_gid to enable lfs mkdir for non-admin user,
1. enable_remote_dir_gid = group, only users, whose gid == group
   can create remote dir.
2. enable_remote_dir_gid = -1, all users can create remote dir.

Signed-off-by: wang di <di.wang@intel.com>
Change-Id: I6605016aa89ef3e2763ca14f94a763685fc689b6
Reviewed-on: http://review.whamcloud.com/5442
Tested-by: Hudson
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: John Hammond <johnlockwoodhammond@gmail.com>
Tested-by: Maloo <whamcloud.maloo@gmail.com>

lustre/mdt/mdt_handler.c
@@ -4683,6 +4683,7 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	spin_lock_init(&m->mdt_osfs_lock);
 	m->mdt_osfs_age = cfs_time_shift_64(-1000);
 	m->mdt_enable_remote_dir = 0;
+	m->mdt_enable_remote_dir_gid = 0;
 
         m->mdt_md_dev.md_lu_dev.ld_ops = &mdt_lu_ops;
         m->mdt_md_dev.md_lu_dev.ld_obd = obd;

lustre/mdt/mdt_internal.h
@@ -161,8 +161,10 @@ struct mdt_device {
         struct lustre_capa_key     mdt_capa_keys[2];
 	unsigned int               mdt_capa_conf:1,
 				   mdt_som_conf:1,
+				   /* Enable remote dir on non-MDT0 */
 				   mdt_enable_remote_dir:1;
 
+	gid_t			   mdt_enable_remote_dir_gid;
 	/* statfs optimization: we cache a bit  */
 	struct obd_statfs	   mdt_osfs;
 	__u64			   mdt_osfs_age;

commit 8dbd7d3d2ec5d6bdb17b25a810bf41e3b5720a2b

LU-17049 mdt: 'lfs mkdir' check supplementary groups
Check all supplementary groups when checking enable_remote_dir_gid
and enable_pin_gid, instead of only checking the primary GID under
which the RPC was sent.

Add a helper function `mdt_enable_gid_deny()` that checks whether
a particular `mdt_enable_XXX_gid` group matches current credentials
or is overridden by a capability, since this check was getting
increasingly complex.

Add MDT_ENABLE_GID_LUSTRE_RW_ATTR() helper macro to generate the
respective enable_XXX_gid() parameter code to reduce boilerplate.
Use for enable_remote_dir_gid, enable_pin_gid, enable_chprojid_gid.

Use MDT_INVALID_GID instead of '-1' to avoid signed/unsigned issues.
When printing the parameters in sysfs, handle MDT_INVALID_GID as a
special case, so GIDs > 2B print as unsigned instead of negative.

Fix 'lfs mkdir' to report an error if multiple arguments are given,
but there is a failure creating a directory that isn't last.

Add sanity.sh test_300ne to check enable_remote_dir_gid=N for a
GID that is not the primary group for the request.

Add mdt.enable_remote_dir.4 and mdt.enable_remote_dir_gid.4 man pages.

Fixes: 11b2615571 ("LU-2738 mdt: add proc entry to enable lfs mkdir for non-admin")
Fixes: 84100d7f7d ("LU-10499 pcc: tunable GID to pin/unpin files")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I0f1ded1dd1a9bb8bd15d956542e582b943300c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/52063
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Artem Blagodarenko <ablagodarenko@thelustrecollective.com>
Reviewed-by: Sohei Koyama <skoyama@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

Documentation/man4/mdt.enable_remote_dir.4
@@ -1,0 +1,133 @@
+.TH ENABLE_REMOTE_DIR 4 "2026-03-10" "Lustre" "Lustre Kernel Parameters"
+.SH NAME
+enable_remote_dir, enable_remote_dir_gid \- control remote directory creation
+.SH SYNOPSIS
+.SY
+lctl set_param mdt.\fIFSNAME\fR*.enable_remote_dir=\fIENABLE
+.SY
+lctl set_param mdt.\fIFSNAME\fR*.enable_remote_dir_gid=\fIGID
+.SY
+lctl set_param mdt.\fIFSNAME\fR*.enable_striped_dir=\fIENABLE
+.YS
+.SS PROPERTIES
+.TP
+.B Access Permissions
+.BR 644 | -rw-r--r--
+.TP
+.B Scope
+.br
+Per-MDT device.
+.TP
+.B Config
+.br
+.BR enable_remote_dir ,
+.BR enable_remote_dir_gid ,
+and
+.B enable_striped_dir
+are always present for MDT devices.
+.TP
+.B Default
+.RB enable_remote_dir= 1
+.br
+.RB enable_remote_dir_gid= 0
+.br
+.RB enable_striped_dir= 1
+.SH DESCRIPTION
+The boolean
+.B enable_remote_dir
+parameter controls whether clients can create remote directories that
+reference other MDTs within the filesystem.
+Explicitly specifying the MDT index for a directory by default also
+constrains all subdirectories for that tree to remain on the given MDT,
+which can negatively impact MDT space balance if used incorrectly.
+If
+.I ENABLE
+is
+.B 1
+(or aliases
+.BR on " or " true ),
+then this functionality is globally enabled. If it is
+.B 0
+or
+.B off
+then this functionality is globally disabled.
+The boolean
+.B enable_striped_dir
+parameter controls whether clients can create striped directories that
+span multiple MDTs within the filesystem.
+DNE striped directories should only be used for large directories that
+will directly contain 100k or more files, and can otherwise have a negative
+performance impact.
+If
+.I ENABLE
+is
+.B 1
+(or aliases
+.BR on " or " true ),
+then this functionality is globally enabled. If it is
+.B 0
+or
+.B off
+then this functionality is globally disabled.
+The
+.B enable_remote_dir_gid
+parameter controls which user or group can create both remote and striped
+directories.
+.PP
+The default
+.I GID
+value of
+.B 0
+indicates that only the root user is allowed to create these directories,
+while
+.B \-1
+indicates that any user is allowed to create them
+If
+.I GID
+is another value, it means users with the numeric Group ID that can do so.
+For example, this could be used to allow an
+.I admin
+group for administrators, or a
+.I dne_dir
+group for power users, using the numeric values of those groups.
+.PP
+The creation of remote directories may optionally be restricted on
+specific client nodes by a per-nodemap
+.RB ' dne_ops '
+RBAC, see
+.BR lctl-nodemap-modify (8)
+for details.
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B mdt.*.enable_remote_dir
+.B mdt.*.enable_remote_dir_gid
+.B mdt.*.enable_striped_dir
+.EE
+.SH EXAMPLES
+.TP
+Enable remote directory creation for all users on the \fBtestfs\fR filesystem:
+.EX
+mgs# lctl set_param -P mdt.testfs-*.enable_remote_dir_gid=-1
+.EE
+.SH AVAILABILITY
+The
+.B enable_remote_dir
+and
+.B enable_remote_dir_gid
+parameters are part of the
+.BR lustre (7)
+filesystem package since release 2.4.0.
+.\" Added in commit v2_3_60-28-gbc962bde3b
+The
+.B enable_remote_dir
+parameter is part of the
+.BR lustre (7)
+filesystem package since release 2.12.0.
+.\" Added in commit v2_11_56_0-156-g2dae2b8ffb
+.SH SEE ALSO
+.BR lfs-setdirstripe (1),
+.BR mdt.enable_foreign_dir (4),
+.BR nodemap.rbac (4),
+.BR lctl-nodemap-modify (8),
+.BR lctl-set_param (8)

Documentation/man4/mdt.enable_remote_dir_gid.4
@@ -1,0 +1,1 @@
+.so man4/mdt.enable_remote_dir.4

Documentation/man4/mdt.enable_striped_dir.4
@@ -1,0 +1,1 @@
+.so man4/mdt.enable_remote_dir.4

lustre/include/lustre_idmap.h
@@ -41,6 +41,8 @@ void lustre_groups_sort(struct group_info *group_info);
 int lustre_groups_search(struct group_info *group_info, gid_t grp);
 int lustre_in_group_p(struct lu_ucred *mu, gid_t grp);
 int has_proper_groups(struct lu_ucred *ucred);
+int lustre_print_groups(struct group_info *group_info);
+int lustre_print_ucred(struct lu_ucred *ucred);
 
 /** @} idmap */
 

lustre/llite/file.c
@@ -4646,7 +4646,7 @@ ll_file_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	}
 	case LL_IOC_LOV_SETSTRIPE:
 	case LL_IOC_LOV_SETSTRIPE_NEW:
-		if (sbi->ll_enable_setstripe_gid != -1 &&
+		if (sbi->ll_enable_setstripe_gid != MDT_INVALID_GID &&
 		    !capable(CAP_SYS_RESOURCE) &&
 		    /* in_group_p always returns true for gid == 0, so we check
 		     * for this case directly

lustre/llite/llite_lib.c
@@ -225,7 +225,7 @@ static struct ll_sb_info *ll_init_sbi(struct lustre_sb_info *lsi)
 		SBI_DEFAULT_HYBRID_IO_READ_THRESHOLD;
 
 	/* setstripe is allowed for all groups by default */
-	sbi->ll_enable_setstripe_gid = -1;
+	sbi->ll_enable_setstripe_gid = MDT_INVALID_GID;
 
 	/* erasure coding is disabled by default */
 	sbi->ll_enable_erasure_coding = 0;

lustre/llite/lproc_llite.c
@@ -1765,7 +1765,10 @@ static ssize_t enable_setstripe_gid_show(struct kobject *kobj,
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
 
-	return scnprintf(buf, PAGE_SIZE, "%d\n", sbi->ll_enable_setstripe_gid);
+	if (sbi->ll_enable_setstripe_gid == MDT_INVALID_GID)
+		return scnprintf(buf, PAGE_SIZE, "-1\n");
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_enable_setstripe_gid);
 }
 
 static ssize_t enable_setstripe_gid_store(struct kobject *kobj,

lustre/mdd/mdd_object.c
@@ -697,7 +697,7 @@ static int mdd_fix_attr(const struct lu_env *env, struct mdd_object *obj,
 		if (!uc->uc_rbac_file_perms ||
 		    (!cap_raised(uc->uc_cap, CAP_SYS_RESOURCE) &&
 		     !lustre_in_group_p(uc, ma->ma_enable_chprojid_gid) &&
-		     !(ma->ma_enable_chprojid_gid == -1 &&
+		     !(ma->ma_enable_chprojid_gid == MDT_INVALID_GID &&
 		       mdd_permission_internal(env, obj, oattr, MAY_WRITE))))
 			RETURN(-EPERM);
 	}

lustre/mdt/mdt_handler.c
@@ -6583,9 +6583,9 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	m->mdt_enable_parallel_rename_dir = 1;
 	m->mdt_enable_parallel_rename_file = 1;
 	m->mdt_enable_parallel_rename_crossdir = 1;
+	m->mdt_enable_pin_gid = 0;
 	m->mdt_enable_remote_dir = 1;
 	m->mdt_enable_remote_dir_gid = 0;
-	m->mdt_enable_pin_gid = 0;
 	m->mdt_enable_remote_rename = 1;
 	m->mdt_enable_rename_trylock = 1;
 	m->mdt_enable_striped_dir = 1;

lustre/mdt/mdt_identity.c
@@ -155,6 +155,7 @@ static int mdt_identity_parse_downcall(struct upcall_cache *cache,
 
 		lustre_groups_from_list(ginfo, data->idd_groups);
 		lustre_groups_sort(ginfo);
+		lustre_print_groups(ginfo);
 	}
 
 	if (data->idd_nperms) {

lustre/mdt/mdt_internal.h
@@ -988,6 +988,7 @@ int mdt_check_ucred(struct mdt_thread_info *info);
 int mdt_init_ucred(struct mdt_thread_info *info, struct mdt_body *body);
 int mdt_init_ucred_reint(struct mdt_thread_info *info);
 void mdt_exit_ucred(struct mdt_thread_info *info);
+bool mdt_enable_gid_deny(struct lu_ucred *uc, unsigned int cap, gid_t need_gid);
 int mdt_check_resource_ids(struct mdt_thread_info *info,
 			   struct mdt_object *obj);
 int mdt_version_get_check(struct mdt_thread_info *info, struct mdt_object *mto,

lustre/mdt/mdt_lib.c
@@ -757,6 +757,40 @@ int mdt_init_ucred_reint(struct mdt_thread_info *info)
 }
 
 /**
+ * mdt_enable_gid_deny - check if operation allowed by enable_XXX_gid
+ *
+ * @uc:		process credentials
+ * @cap:	capability required to override permission checks
+ * @ena_gid:	enable_foo_gid value that specifies required GID
+ *
+ * Returns false if request allowed to perform action, true if request is denied
+ */
+bool mdt_enable_gid_deny(struct lu_ucred *uc, unsigned int cap, gid_t ena_gid)
+{
+	if (unlikely(!uc))			/* cannot check permissions */
+		RETURN(false);
+
+	CDEBUG(D_INFO,
+	       "cap=%x raised=%u ena_gid=%u gid=%u fsgid=%u ingrp=%u\n", cap,
+	       cap_raised(uc->uc_cap, cap), ena_gid, uc->uc_gid, uc->uc_fsgid,
+	       lustre_in_group_p(uc, ena_gid));
+
+	if (ena_gid == MDT_INVALID_GID)		/* any GID is allowed */
+		GOTO(out_allow, false);
+
+	if (cap_raised(uc->uc_cap, cap))	/* process has capability set */
+		GOTO(out_allow, false);
+
+	if (lustre_in_group_p(uc, ena_gid))	/* process has required GID */
+		GOTO(out_allow, false);
+
+	lustre_print_ucred(uc);
+	RETURN(true);				/* process missing needed GID */
+out_allow:
+	return false;
+}
+
+/**
  * mdt_check_resource_ids() - check client access to resource via nodemap
  *
  * @info: mdt thread environment

lustre/mdt/mdt_lproc.c
@@ -681,65 +681,44 @@ static ssize_t enable_cap_mask_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(enable_cap_mask);
 
-static ssize_t enable_remote_dir_gid_show(struct kobject *kobj,
-					  struct attribute *attr, char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
+#define MDT_ENABLE_GID_LUSTRE_RW_ATTR(name)				\
+static ssize_t name##_show(struct kobject *kobj, struct attribute *attr,\
+			   char *buf)					\
+{									\
+	struct obd_device *obd = container_of(kobj, struct obd_device,	\
+					      obd_kset.kobj);		\
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);		\
+									\
+	if (IS_ERR_OR_NULL(mdt))					\
+		return -ENOENT;						\
+	if (mdt->mdt_##name == MDT_INVALID_GID)				\
+		return scnprintf(buf, PAGE_SIZE, "-1\n");		\
+									\
+	return scnprintf(buf, PAGE_SIZE, "%u\n", mdt->mdt_##name);	\
+}									\
+static ssize_t name##_store(struct kobject *kobj, struct attribute *attr,\
+			    const char *buffer, size_t count)		\
+{									\
+	struct obd_device *obd = container_of(kobj, struct obd_device,	\
+					      obd_kset.kobj);		\
+	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);		\
+	int val;							\
+	int rc;								\
+									\
+	if (IS_ERR_OR_NULL(mdt))					\
+		return -ENOENT;						\
+	rc = kstrtoint(buffer, 0, &val);				\
+	if (rc)								\
+		return rc;						\
+									\
+	mdt->mdt_##name = val;						\
+	return count;							\
+}									\
+LUSTRE_RW_ATTR(name)
 
-	return scnprintf(buf, PAGE_SIZE, "%d\n",
-			 (int)mdt->mdt_enable_remote_dir_gid);
-}
-
-static ssize_t enable_remote_dir_gid_store(struct kobject *kobj,
-					   struct attribute *attr,
-					   const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
-	int val;
-	int rc;
-
-	rc = kstrtoint(buffer, 0, &val);
-	if (rc)
-		return rc;
-
-	mdt->mdt_enable_remote_dir_gid = val;
-	return count;
-}
-LUSTRE_RW_ATTR(enable_remote_dir_gid);
-
-static ssize_t enable_chprojid_gid_show(struct kobject *kobj,
-					struct attribute *attr, char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
-
-	return scnprintf(buf, PAGE_SIZE, "%d\n",
-			 (int)mdt->mdt_enable_chprojid_gid);
-}
-
-static ssize_t enable_chprojid_gid_store(struct kobject *kobj,
-					 struct attribute *attr,
-					 const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
-	int val;
-	int rc;
-
-	rc = kstrtoint(buffer, 0, &val);
-	if (rc)
-		return rc;
-
-	mdt->mdt_enable_chprojid_gid = val;
-	return count;
-}
-LUSTRE_RW_ATTR(enable_chprojid_gid);
+MDT_ENABLE_GID_LUSTRE_RW_ATTR(enable_chprojid_gid);
+MDT_ENABLE_GID_LUSTRE_RW_ATTR(enable_pin_gid);
+MDT_ENABLE_GID_LUSTRE_RW_ATTR(enable_remote_dir_gid);
 
 #define MDT_BOOL_RW_ATTR(name)						\
 static ssize_t name##_show(struct kobject *kobj, struct attribute *attr,\
@@ -850,37 +829,6 @@ static ssize_t enable_resource_id_check_store(struct kobject *kobj,
 	return count;
 }
 LUSTRE_RW_ATTR(enable_resource_id_check);
-
-static ssize_t enable_pin_gid_show(struct kobject *kobj,
-				   struct attribute *attr, char *buf)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
-
-	if (mdt->mdt_enable_pin_gid == ~0U)
-		return scnprintf(buf, PAGE_SIZE, "-1\n");
-	return scnprintf(buf, PAGE_SIZE, "%u\n", mdt->mdt_enable_pin_gid);
-}
-
-static ssize_t enable_pin_gid_store(struct kobject *kobj,
-				    struct attribute *attr,
-				    const char *buffer, size_t count)
-{
-	struct obd_device *obd = container_of(kobj, struct obd_device,
-					      obd_kset.kobj);
-	struct mdt_device *mdt = mdt_dev(obd->obd_lu_dev);
-	int val;
-	int rc;
-
-	rc = kstrtoint(buffer, 0, &val);
-	if (rc)
-		return rc;
-
-	mdt->mdt_enable_pin_gid = val;
-	return count;
-}
-LUSTRE_RW_ATTR(enable_pin_gid);
 
 /**
  * Show if the MDT is in no create mode.
@@ -1445,20 +1393,12 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_ir_factor.attr,
 	&lustre_attr_num_exports.attr,
 	&lustre_attr_grant_check_threshold.attr,
+	&lustre_attr_commit_on_sharing.attr,
 	&lustre_attr_evict_client.attr,
 	&lustre_attr_eviction_count.attr,
-	&lustre_attr_identity_expire.attr,
-	&lustre_attr_identity_acquire_expire.attr,
-	&lustre_attr_identity_upcall.attr,
-	&lustre_attr_identity_flush.attr,
-	&lustre_attr_identity_info.attr,
-	&lustre_attr_identity_int_expire.attr,
-	&lustre_attr_identity_int_acquire_expire.attr,
-	&lustre_attr_identity_int_flush.attr,
 	&lustre_attr_evict_tgt_nids.attr,
 	&lustre_attr_enable_cap_mask.attr,
 	&lustre_attr_enable_chprojid_gid.attr,
-	&lustre_attr_enable_pin_gid.attr,
 	&lustre_attr_enable_dir_migration.attr,
 	&lustre_attr_enable_dir_restripe.attr,
 	&lustre_attr_enable_dir_auto_split.attr,
@@ -1467,6 +1407,7 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_enable_parallel_rename_dir.attr,
 	&lustre_attr_enable_parallel_rename_file.attr,
 	&lustre_attr_enable_parallel_rename_crossdir.attr,
+	&lustre_attr_enable_pin_gid.attr,
 	&lustre_attr_enable_remote_dir.attr,
 	&lustre_attr_enable_remote_dir_gid.attr,
 	&lustre_attr_enable_remote_rename.attr,
@@ -1475,7 +1416,14 @@ static struct attribute *mdt_attrs[] = {
 	&lustre_attr_enable_resource_id_check.attr,
 	&lustre_attr_enable_strict_som.attr,
 	&lustre_attr_enable_striped_dir.attr,
-	&lustre_attr_commit_on_sharing.attr,
+	&lustre_attr_identity_expire.attr,
+	&lustre_attr_identity_acquire_expire.attr,
+	&lustre_attr_identity_upcall.attr,
+	&lustre_attr_identity_flush.attr,
+	&lustre_attr_identity_info.attr,
+	&lustre_attr_identity_int_expire.attr,
+	&lustre_attr_identity_int_acquire_expire.attr,
+	&lustre_attr_identity_int_flush.attr,
 	&lustre_attr_local_recovery.attr,
 	&lustre_attr_no_create.attr,
 	&lustre_attr_async_commit_count.attr,

lustre/mdt/mdt_reint.c
@@ -576,9 +576,8 @@ static int mdt_create(struct mdt_thread_info *info, struct mdt_lock_handle *lhc)
 		 * permission or capability checks
 		 */
 		if (!uc->uc_rbac_dne_ops ||
-		    (!cap_raised(uc->uc_cap, CAP_SYS_ADMIN) &&
-		     uc->uc_gid != mdt->mdt_enable_remote_dir_gid &&
-		     mdt->mdt_enable_remote_dir_gid != -1))
+		    mdt_enable_gid_deny(uc, CAP_SYS_ADMIN,
+					mdt->mdt_enable_remote_dir_gid))
 			RETURN(-EPERM);
 
 		/* restripe if later found dir exists, MDS_OPEN_CREAT means
@@ -1090,9 +1089,8 @@ static int mdt_reint_setattr(struct mdt_thread_info *info,
 			 * permission or capability checks
 			 */
 			if (!uc->uc_rbac_dne_ops ||
-			    (!cap_raised(uc->uc_cap, CAP_SYS_ADMIN) &&
-			     uc->uc_gid != mdt->mdt_enable_remote_dir_gid &&
-			     mdt->mdt_enable_remote_dir_gid != -1))
+			    mdt_enable_gid_deny(uc, CAP_SYS_ADMIN,
+						mdt->mdt_enable_remote_dir_gid))
 				GOTO(out_put, rc = -EPERM);
 		}
 
@@ -2326,9 +2324,8 @@ int mdt_reint_migrate(struct mdt_thread_info *info,
 	 * permission or capability checks
 	 */
 	if (uc && (!uc->uc_rbac_dne_ops ||
-		   (!cap_raised(uc->uc_cap, CAP_SYS_ADMIN) &&
-		    uc->uc_gid != mdt->mdt_enable_remote_dir_gid &&
-		    mdt->mdt_enable_remote_dir_gid != -1)))
+		   mdt_enable_gid_deny(uc, CAP_SYS_ADMIN,
+				       mdt->mdt_enable_remote_dir_gid)))
 		RETURN(-EPERM);
 
 	/*

lustre/mdt/mdt_xattr.c
@@ -341,9 +341,8 @@ int mdt_dir_layout_update(struct mdt_thread_info *info)
 	 * permission or capability checks
 	 */
 	if (!uc->uc_rbac_dne_ops ||
-	    (!cap_raised(uc->uc_cap, CAP_SYS_ADMIN) &&
-	     uc->uc_gid != mdt->mdt_enable_remote_dir_gid &&
-	     mdt->mdt_enable_remote_dir_gid != -1))
+	    mdt_enable_gid_deny(uc, CAP_SYS_ADMIN,
+				mdt->mdt_enable_remote_dir_gid))
 		RETURN(-EPERM);
 
 	obj = mdt_object_find(env, mdt, rr->rr_fid1);
@@ -597,9 +596,8 @@ int mdt_reint_setxattr(struct mdt_thread_info *info,
 		struct mdt_device *mdt = info->mti_mdt;
 		struct lu_ucred *uc = mdt_ucred(info);
 
-		if (!cap_raised(uc->uc_cap, CAP_SYS_RESOURCE) &&
-		    !lustre_in_group_p(uc, mdt->mdt_enable_pin_gid) &&
-		    !(mdt->mdt_enable_pin_gid == -1))
+		if (mdt_enable_gid_deny(uc, CAP_SYS_RESOURCE,
+					mdt->mdt_enable_pin_gid))
 			GOTO(out, rc = -EPERM);
 
 		xattr_name = XATTR_NAME_PIN;

lustre/obdclass/idmap.c
@@ -143,3 +143,40 @@ int has_proper_groups(struct lu_ucred *ucred)
 	return rc;
 }
 EXPORT_SYMBOL(has_proper_groups);
+
+int lustre_print_groups(struct group_info *group_info)
+{
+	int g;
+
+	if (!group_info)
+		RETURN(-ENOENT);
+
+	get_group_info(group_info);
+	CDEBUG(D_INFO, "ngroups=%u\n", group_info->ngroups);
+	for (g = 0; g < group_info->ngroups; g++)
+		CDEBUG(D_INFO, "group[%u] = %u\n", g,
+		       from_kgid(&init_user_ns, group_info->gid[g]));
+	put_group_info(group_info);
+
+	RETURN(0);
+}
+EXPORT_SYMBOL(lustre_print_groups);
+
+int lustre_print_ucred(struct lu_ucred *ucred)
+{
+	struct group_info *group_info;
+
+	if (unlikely(!ucred))
+		return -EINVAL;
+
+	group_info = ucred->uc_ginfo;
+	if (!group_info && ucred->uc_identity)
+		group_info = ucred->uc_identity->mi_ginfo;
+
+	CDEBUG(D_INFO, "uid=%u gid=%u fsuid=%u fsgid=%u supp=%u/%u ginfo=%px\n",
+	       ucred->uc_uid, ucred->uc_gid, ucred->uc_fsuid, ucred->uc_fsgid,
+	       ucred->uc_suppgids[0], ucred->uc_suppgids[1], group_info);
+
+	return lustre_print_groups(group_info);
+}
+EXPORT_SYMBOL(lustre_print_ucred);

lustre/utils/lfs.c
@@ -8776,20 +8776,26 @@ static int lfs_setdirstripe(int argc, char **argv)
 
 	dname = argv[optind];
 	do {
+		int rc;
+
 		if (default_stripe) {
-			result = llapi_dir_set_default_lmv(dname, param);
-			if (result)
+			rc = llapi_dir_set_default_lmv(dname, param);
+			if (rc)
 				fprintf(stderr,
 					"%s setdirstripe: cannot set default stripe on dir '%s': %s\n",
-					progname, dname, strerror(-result));
+					progname, dname, strerror(-rc));
+			if (!result)
+				result = rc; /* save first error for return */
 			continue;
 		}
 
-		result = llapi_dir_create(dname, mode, param);
-		if (result)
+		rc = llapi_dir_create(dname, mode, param);
+		if (rc)
 			fprintf(stderr,
 				"%s setdirstripe: cannot create dir '%s': %s\n",
-				progname, dname, strerror(-result));
+				progname, dname, strerror(-rc));
+		if (!result)
+			result = rc;	     /* save first error for return */
 	} while ((dname = argv[++optind]));
 
 	if (mode_opt)

lustre/tests/sanity.sh
@@ -29429,55 +29429,96 @@ test_300m() {
 }
 run_test 300m "setstriped directory on single MDT FS"
 
-cleanup_300n() {
-	local mdts=$(mdts_nodes)
-
-	trap 0
-	do_nodes $mdts $LCTL set_param -n mdt.*.enable_remote_dir_gid=0
-}
-
 test_300n() {
-	[ $PARALLEL == "yes" ] && skip "skip parallel run"
-	[ $MDSCOUNT -lt 2 ] && skip_env "needs >= 2 MDTs"
-	[ $MDS1_VERSION -lt $(version_code 2.7.55) ] &&
-		skip "Need MDS version at least 2.7.55"
+	[[ $PARALLEL != "yes" ]] || skip "skip parallel run"
+	(( $MDSCOUNT >= 2 )) || skip_env "needs >= 2 MDTs"
+	(( $MDS1_VERSION >= $(version_code 2.7.55) )) ||
+		skip "Need MDS >= 2.7.55 for enable_remote_dir_gid"
 	remote_mds_nodsh && skip "remote MDS with nodsh"
 
 	local stripe_index
 	local mdts=$(mdts_nodes)
+	local param_gid="mdt.*.enable_remote_dir_gid"
+	local old_gid=($(do_facet mds1 $LCTL get_param -n $param_gid))
 
-	trap cleanup_300n RETURN EXIT
-	mkdir -p $DIR/$tdir
-	chmod 777 $DIR/$tdir
+	mkdir --mode=0777 $DIR/$tdir
+
+	stack_trap "do_nodes $mdts '$LCTL set_param $param_gid=$old_gid'"
+	# mkdir _gid=0 with explicit setstripe, should not work for regular user
+	do_nodes $mdts $LCTL set_param $param_gid=0 ||
+		error "set $param_gid=0 failed"
 	$RUNAS $LFS setdirstripe -i0 -c$MDSCOUNT \
 				$DIR/$tdir/striped_dir > /dev/null 2>&1 &&
-		error "create striped dir succeeds with gid=0"
+		error "create striped dir succeeds with remote_gid=0"
+	$RUNAS $LFS setdirstripe -D -i 1 -c$MDSCOUNT $DIR/$tdir/striped_dir &&
+		error "set default dir layout succeeds with remote_gid=0"
+	[[ ! -e $DIR/$tdir/striped_dir ]] || error "striped_dir was created"
 
-	do_nodes $mdts $LCTL set_param -n mdt.*.enable_remote_dir_gid=-1
+	# mkdir _gid=-1 with explicit setstripe, should work for any user
+	do_nodes $mdts $LCTL set_param $param_gid=-1
 	$RUNAS $LFS setdirstripe -i0 -c$MDSCOUNT $DIR/$tdir/striped_dir ||
-		error "create striped dir fails with gid=-1"
+		error "create striped dir fails with remote_gid=-1"
+	$RUNAS $LFS setdirstripe -D -i 1 -c$MDSCOUNT $DIR/$tdir/striped_dir ||
+		error "set default dir layout fails with remote_gid=-1"
 
-	do_nodes $mdts $LCTL set_param -n mdt.*.enable_remote_dir_gid=0
-	$RUNAS $LFS setdirstripe -i 1 -c$MDSCOUNT -D \
-				$DIR/$tdir/striped_dir > /dev/null 2>&1 &&
-		error "set default striped dir succeeds with gid=0"
-
-
-	do_nodes $mdts $LCTL set_param -n mdt.*.enable_remote_dir_gid=-1
-	$RUNAS $LFS setdirstripe -i 1 -c$MDSCOUNT -D $DIR/$tdir/striped_dir ||
-		error "set default striped dir fails with gid=-1"
-
-
-	do_nodes $mdts $LCTL set_param -n mdt.*.enable_remote_dir_gid=0
-	$RUNAS mkdir $DIR/$tdir/striped_dir/test_dir ||
-					error "create test_dir fails"
-	$RUNAS mkdir $DIR/$tdir/striped_dir/test_dir1 ||
-					error "create test_dir1 fails"
-	$RUNAS mkdir $DIR/$tdir/striped_dir/test_dir2 ||
-					error "create test_dir2 fails"
-	cleanup_300n
+	# mkdir _gid=0 with default setstripe, should work for any user
+	do_nodes $mdts $LCTL set_param $param_gid=0
+	$RUNAS mkdir $DIR/$tdir/striped_dir/test_dir.{1,2,3} ||
+		error "mkdir in default test_dir with remote_gid=0 fails"
+	[[ -d $DIR/$tdir/striped_dir/test_dir.1 ]] ||
+		error "striped_dir/test_dir.1 not created"
 }
 run_test 300n "non-root user to create dir under striped dir with default EA"
+
+test_300ne()
+{
+	[[ $PARALLEL != "yes" ]] || skip "skip parallel run"
+	(( $MDSCOUNT >= 2 )) || skip_env "needs >= 2 MDTs"
+	(( $MDS1_VERSION >= $(version_code 2.17.51) )) ||
+		skip "Need MDS >= 2.17.51 for supp remote_dir_gid"
+	remote_mds_nodsh && skip "remote MDS with nodsh"
+
+	local param_gid="mdt.*.enable_remote_dir_gid"
+	local old_gid=($(do_facet mds1 $LCTL get_param -n $param_gid))
+	local mdts=$(mdts_nodes)
+
+	echo "using primary group $RUNAS_GID"
+	do_nodes $mdts "grep '$RUNAS_GID:' /etc/group"
+	do_nodes $mdts "id $RUNAS_ID"
+	do_nodes $mdts "$LCTL get_param mdt.*.identity_upcall"
+	do_nodes $mdts "$L_GETIDENTITY -d $RUNAS_ID"
+	start_full_debug_logging
+	stack_trap stop_full_debug_logging
+
+	mkdir_on_mdt0 -o 0777 $DIR/$tdir
+
+	stack_trap "do_nodes $mdts '$LCTL set_param $param_gid=$old_gid'"
+	# mkdir _gid=N with explicit setstripe, should work for primary group
+	do_nodes $mdts $LCTL set_param $param_gid=$RUNAS_GID
+	$RUNAS $LFS mkdir -i 1 $DIR/$tdir/group_dir ||
+		error "create remote dir fails with remote_gid=$RUNAS_ID"
+	[[ -d $DIR/$tdir/group_dir ]] || error "$tdir/group_dir not created"
+
+	echo "using $TSTUSR with secondary groups $new_gids"
+	local tst_gids=$(do_facet mds1 id -G $TSTUSR | tr ' ' ',')
+	local new_gids="$tst_gids,$RUNAS_ID"
+	local runas="$RUNAS -u $TSTUSR -g $TSTUSR -G$new_gids"
+
+	# add $RUNAS_ID as a secondary group to $TSTUSR
+	stack_trap "do_nodes $mdts usermod -G $tst_gids $TSTUSR"
+	do_nodes $mdts usermod -a -G $RUNAS_ID $TSTUSR ||
+		error "can't add $TSTUSR to group $RUNAS_ID"
+
+	do_nodes $mdts "grep ':$RUNAS_ID:' /etc/group"
+	do_nodes $mdts "id $TSTUSR"
+	do_nodes $mdts "$L_GETIDENTITY -d $TSTUSR"
+
+	# mkdir _gid=N with explicit setstripe, should work for secondary group
+	$runas $LFS mkdir -i 1 $DIR/$tdir/secondary_dir ||
+		error "create remote dir fails with secondary gids=$new_gids"
+	[[ -d $DIR/$tdir/secondary_dir ]] || error "secondary_dir not created"
+}
+run_test 300ne "create remote dir with various mdt.enable_remote_gid"
 
 test_300o() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"

lustre/tests/test-framework.sh
@@ -10398,6 +10398,7 @@ test_mkdir() {
 	local hash_name=("all_char" "fnv_1a_64" "crush")
 	local dirstripe_count=${DIRSTRIPE_COUNT:-"2"}
 	local dirstripe_index=${DIRSTRIPE_INDEX:-$((base % $MDSCOUNT))}
+	local mode
 	local OPTIND=1
 	local overstripe_count
 	local stripe_command="-c"
@@ -10405,12 +10406,13 @@ test_mkdir() {
 	(( $MDS1_VERSION > $(version_code v2_15_50-185-g1ac4b9598a) )) &&
 		hash_name+=("crush2")
 
-	while getopts "c:C:H:i:p" opt; do
+	while getopts "c:C:H:i:o:p" opt; do
 		case $opt in
 			c) dirstripe_count=$OPTARG;;
 			C) overstripe_count=$OPTARG;;
 			H) hash_type=$OPTARG;;
 			i) dirstripe_index=$OPTARG;;
+			o) mode="--mode $OPTARG";;
 			p) p_option="-p";;
 			\?) error "only support -c -H -i -p";;
 		esac
@@ -10457,9 +10459,10 @@ test_mkdir() {
 			dirstripe_count=1
 		fi
 
-		echo "striped dir -i$mdt_index $stripe_command$dirstripe_count -H $hash_type $path"
-		$LFS mkdir -i$mdt_index $stripe_command$dirstripe_count -H $hash_type $path ||
-			error "mkdir -i $mdt_index $stripe_command$dirstripe_count -H $hash_type $path failed"
+		local args="-i$mdt_index $stripe_command$dirstripe_count "
+		     args+="-H $hash_type $mode $path"
+		echo "striped dir $args"
+		$LFS mkdir $args || error "'mkdir $args' failed"
 	fi
 }
 
@@ -12452,17 +12455,19 @@ function restore_opencache()
 # explicitly, and set default LMV to create subdirs on the same MDT too.
 mkdir_on_mdt() {
 	local mdt
+	local mode
 	local OPTIND=1
 
-	while getopts "i:" opt $*; do
+	while getopts "i:o:" opt $*; do
 		case $opt in
-			i) mdt=$OPTARG;;
+			i) mdt="-i $OPTARG";;
+			o) mode="--mode $OPTARG";;
 		esac
 	done
 
 	shift $((OPTIND - 1))
 
-	$LFS mkdir -i $mdt -c 1 $*
+	$LFS mkdir $mdt $mode -c 1 $*
 }
 
 mkdir_on_mdt0() {