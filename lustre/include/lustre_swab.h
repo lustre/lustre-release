lustre/lustre-release

commit 63caacb042787b63586f1b64796764843f2b4c00

LU-5896 mdt: allow release after blocking migrations
We now pass the new and old dataversion of the file after a blocking
migration to the kernel on the client to the MDS via an extension to
the MDS_SWAP_LAYOUTS RPC. Two data versions are added to the
mdc_swap_layouts part of this RPC.

These new fields are checked on the MDS only if the new flag
SWAP_LAYOUTS_WITH_DV12 is set in mdc_swap_layouts::msl_flags
which was previously set to 0 by the client, so no change in behavior
for old clients. Old servers will ignore the newly-added DV members in
the request as the structure doesn't contain it.

We can then transfer the new dataversion from the client
to the MDS through this extension. The MDD layer can
then update the dataversion accordingly on the server.

Add regression test sanity-hsm 607e.

Test-Parameters: testlist=sanity-hsm env=ONLY=607,ONLY_REPEAT=15
Change-Id: Id5d808d3f3bf64982dce2f930d9c5b5e93dd0251
Signed-off-by: Etienne AUJAMES <eaujames@ddn.com>
Signed-off-by: Guillaume Courrier <guillaume.courrier@cea.fr>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49377
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Robert Read <rread@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Timothy Day <tday141@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3774,10 +3774,15 @@ enum batch_update_cmd {
 
 /** layout swap request structure
  * fid1 and fid2 are in mdt_body
+ *
+ * /!\ msl_dv1 and msl_dv2 are valid only if msl_flags contains
+ *     SWAP_LAYOUTS_WITH_DV12
  */
 struct mdc_swap_layouts {
 	__u64           msl_flags;
-} __attribute__((packed));
+	__u64		msl_dv1; /* data version of file 1 */
+	__u64		msl_dv2; /* data version of file 2 */
+};
 
 #define INLINE_RESYNC_ARRAY_SIZE	15
 struct close_data_resync_done {

lustre/mdc/mdc_request.c
@@ -2234,10 +2234,12 @@ out:
 static int mdc_ioc_swap_layouts(struct obd_export *exp,
 				struct md_op_data *op_data)
 {
+	struct mdc_swap_layouts *payload;
+	struct mdc_swap_layouts *msl;
+	struct ptlrpc_request *req;
 	LIST_HEAD(cancels);
-	struct ptlrpc_request	*req;
-	int			 rc, count;
-	struct mdc_swap_layouts *msl, *payload;
+	int count;
+	int rc;
 
 	ENTRY;
 	msl = op_data->op_data;

lustre/mdt/mdt_handler.c
@@ -1841,12 +1841,15 @@ static void mdt_swap_lov_flag(struct mdt_object *o1, struct mdt_object *o2)
 
 static int mdt_swap_layouts(struct tgt_session_info *tsi)
 {
-	struct mdt_thread_info	*info;
-	struct ptlrpc_request	*req = tgt_ses_req(tsi);
-	struct obd_export	*exp = req->rq_export;
-	struct mdt_object	*o1, *o2, *o;
-	struct mdt_lock_handle	*lh1, *lh2;
+	struct ptlrpc_request *req = tgt_ses_req(tsi);
+	struct obd_export *exp = req->rq_export;
+	struct mdt_lock_handle *lh1, *lh2;
+	struct mdt_object *o1, *o2, *o;
+	struct mdt_thread_info *info;
 	struct mdc_swap_layouts *msl;
+	bool swap_objects = false;
+	__u64 dv1 = 0;
+	__u64 dv2 = 0;
 	int rc;
 
 	ENTRY;
@@ -1882,8 +1885,10 @@ static int mdt_swap_layouts(struct tgt_session_info *tsi)
 	if (unlikely(rc == 0)) /* same file, you kidding me? no-op. */
 		GOTO(put, rc);
 
-	if (rc < 0)
+	if (rc < 0) {
+		swap_objects = true;
 		swap(o1, o2);
+	}
 
 	/* permission check. Make sure the calling process having permission
 	 * to write both files.
@@ -1902,6 +1907,14 @@ static int mdt_swap_layouts(struct tgt_session_info *tsi)
 	if (msl == NULL)
 		GOTO(put, rc = -EPROTO);
 
+	if (msl->msl_flags & SWAP_LAYOUTS_WITH_DV12) {
+		dv1 = msl->msl_dv1;
+		dv2 = msl->msl_dv2;
+
+		if (swap_objects)
+			swap(dv1, dv2);
+	}
+
 	lh1 = &info->mti_lh[MDT_LH_NEW];
 	lh2 = &info->mti_lh[MDT_LH_OLD];
 	rc = mdt_object_lock(info, o1, lh1, MDS_INODELOCK_LAYOUT |
@@ -1915,7 +1928,7 @@ static int mdt_swap_layouts(struct tgt_session_info *tsi)
 		GOTO(unlock1, rc);
 
 	rc = mo_swap_layouts(info->mti_env, mdt_object_child(o1),
-			     mdt_object_child(o2), 0, 0, msl->msl_flags);
+			     mdt_object_child(o2), dv1, dv2, msl->msl_flags);
 	if (rc < 0)
 		GOTO(unlock2, rc);
 

lustre/ptlrpc/pack_generic.c
@@ -3067,6 +3067,15 @@ EXPORT_SYMBOL(lustre_swab_but_update_buffer);
 void lustre_swab_swap_layouts(struct mdc_swap_layouts *msl)
 {
 	__swab64s(&msl->msl_flags);
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	/* Only swab dv1/dv2 fields if WITH_DV12 flag is sent by client, to
+	 * avoid access beyond the end of old struct mdc_swap_layouts.
+	 */
+	if (!(msl->msl_flags & SWAP_LAYOUTS_WITH_DV12))
+		return;
+ #endif
+	__swab64s(&msl->msl_dv1);
+	__swab64s(&msl->msl_dv2);
 }
 
 void lustre_swab_close_data(struct close_data *cd)

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1701,15 +1701,23 @@ struct lqa_id_range {
 #define LQA_RANGE_SIZE (sizeof(struct lqa_id_range)) /* LQA range size: %u%u */
 
 /* swap layout flags */
-#define SWAP_LAYOUTS_CHECK_DV1		(1 << 0)
-#define SWAP_LAYOUTS_CHECK_DV2		(1 << 1)
-#define SWAP_LAYOUTS_KEEP_MTIME		(1 << 2)
-#define SWAP_LAYOUTS_KEEP_ATIME		(1 << 3)
-#define SWAP_LAYOUTS_CLOSE		(1 << 4)
-#define SWAP_LAYOUTS_WITH_DV12		(1 << 5)
+enum lustre_swap_layouts_flags {
+	SWAP_LAYOUTS_CHECK_DV1		= 0x00000001,
+	SWAP_LAYOUTS_CHECK_DV2		= 0x00000002,
+	SWAP_LAYOUTS_KEEP_MTIME		= 0x00000004,
+	SWAP_LAYOUTS_KEEP_ATIME		= 0x00000008,
+	SWAP_LAYOUTS_CLOSE		= 0x00000010,
 
-/* Skip the UID/GID check before a swap layout for a release (server only) */
-#define SWAP_LAYOUTS_MDS_RELEASE	(1 << 31)
+	/* Sent to the MDT through mdc_swap_layouts::msl_flags to indicate that
+	 * mdc_swap_layouts contains valid msl_dv1 and msl_dv2.
+	 */
+	SWAP_LAYOUTS_WITH_DV12		= 0x00000020,
+
+	/* Skip the UID/GID check before a swap layout for a release
+	 * (server only)
+	 */
+	SWAP_LAYOUTS_MDS_RELEASE	= 0x80000000,
+};
 
 struct lustre_swap_layouts {
 	__u64	sl_flags;

lustre/llite/file.c
@@ -3618,10 +3618,10 @@ static int ll_swap_layouts(struct file *file1, struct file *file2,
 			   struct lustre_swap_layouts *lsl)
 {
 	struct mdc_swap_layouts msl;
+	struct ll_swap_stack *llss;
 	struct md_op_data *op_data;
 	__u32 gid;
 	__u64 dv;
-	struct ll_swap_stack *llss = NULL;
 	int rc;
 
 	OBD_ALLOC_PTR(llss);
@@ -3696,7 +3696,9 @@ static int ll_swap_layouts(struct file *file1, struct file *file2,
 	 * flags from user space have to be converted before they are send to
 	 * server, no flag is sent today, they are only used on the client
 	 */
-	msl.msl_flags = 0;
+	msl.msl_flags = SWAP_LAYOUTS_WITH_DV12;
+	msl.msl_dv1 = llss->dv1;
+	msl.msl_dv2 = llss->dv2;
 	rc = -ENOMEM;
 	op_data = ll_prep_md_op_data(NULL, llss->inode1, llss->inode2, NULL, 0,
 				     0, LUSTRE_OPC_ANY, &msl);

lustre/ptlrpc/wiretest.c
@@ -5404,12 +5404,20 @@ void lustre_assert_wire_constants(void)
 		 (long long)(int)sizeof(((struct lustre_swap_layouts *)0)->sl_dv2));
 
 	/* Checks for struct mdc_swap_layouts */
-	LASSERTF((int)sizeof(struct mdc_swap_layouts) == 8, "found %lld\n",
+	LASSERTF((int)sizeof(struct mdc_swap_layouts) == 24, "found %lld\n",
 		 (long long)(int)sizeof(struct mdc_swap_layouts));
 	LASSERTF((int)offsetof(struct mdc_swap_layouts, msl_flags) == 0, "found %lld\n",
 		 (long long)(int)offsetof(struct mdc_swap_layouts, msl_flags));
 	LASSERTF((int)sizeof(((struct mdc_swap_layouts *)0)->msl_flags) == 8, "found %lld\n",
 		 (long long)(int)sizeof(((struct mdc_swap_layouts *)0)->msl_flags));
+	LASSERTF((int)offsetof(struct mdc_swap_layouts, msl_dv1) == 8, "found %lld\n",
+		 (long long)(int)offsetof(struct mdc_swap_layouts, msl_dv1));
+	LASSERTF((int)sizeof(((struct mdc_swap_layouts *)0)->msl_dv1) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdc_swap_layouts *)0)->msl_dv1));
+	LASSERTF((int)offsetof(struct mdc_swap_layouts, msl_dv2) == 16, "found %lld\n",
+		 (long long)(int)offsetof(struct mdc_swap_layouts, msl_dv2));
+	LASSERTF((int)sizeof(((struct mdc_swap_layouts *)0)->msl_dv2) == 8, "found %lld\n",
+		 (long long)(int)sizeof(((struct mdc_swap_layouts *)0)->msl_dv2));
 
 	/* Checks for mdc_swap_layouts::msl_flags */
 	LASSERTF(SWAP_LAYOUTS_CHECK_DV1 == 1, "found %lld\n",
@@ -5422,6 +5430,10 @@ void lustre_assert_wire_constants(void)
 		 (long long)SWAP_LAYOUTS_KEEP_ATIME);
 	LASSERTF(SWAP_LAYOUTS_CLOSE == 16, "found %lld\n",
 		 (long long)SWAP_LAYOUTS_CLOSE);
+	LASSERTF(SWAP_LAYOUTS_MDS_RELEASE == 2147483648, "found %lld\n",
+		 (long long)SWAP_LAYOUTS_MDS_RELEASE);
+	LASSERTF(SWAP_LAYOUTS_WITH_DV12 == 32, "found %lld\n",
+		 (long long)SWAP_LAYOUTS_WITH_DV12);
 
 	/* Checks for struct hsm_state_set */
 	LASSERTF((int)sizeof(struct hsm_state_set) == 24, "found %lld\n",

lustre/tests/sanity-hsm.sh
@@ -6215,8 +6215,8 @@ test_607a()
 	local f="$d/$tfile"
 	local fid
 
-	(( MDS1_VERSION >= $(version_code 2.15.60) )) ||
-		skip "need MDS version at least 2.15.60"
+	(( MDS1_VERSION >= $(version_code v2_15_61-80-g94d02e5774) )) ||
+		skip "need MDS >= 2.15.61.80 for non-blocking migrate release"
 
 	(( OSTCOUNT >= 2 )) || skip_env "needs >= 2 OSTs"
 
@@ -6231,12 +6231,12 @@ test_607a()
 		error "could not migrate file to OST 1"
 
 	$LFS hsm_release "$f" ||
-		error "could not release file after non blocking migrate"
+		error "could not release file after non-blocking migrate"
 	$LFS hsm_restore "$f" ||
-		error "could not restore file after non blocking migrate"
+		error "could not restore file after non-blocking migrate"
 	wait_request_state $fid RESTORE SUCCEED
 }
-run_test 607a "release a file that was migrated after being archived"
+run_test 607a "release file migrated after archive (non-blocking migrate)"
 
 test_607b()
 {
@@ -6247,8 +6247,8 @@ test_607b()
 	local new_hsm
 	local fid
 
-	(( MDS1_VERSION >= $(version_code 2.15.60) )) ||
-		skip "need MDS version at least 2.15.60"
+	(( MDS1_VERSION >= $(version_code v2_15_61-80-g94d02e5774) )) ||
+		skip "need MDS >= 2.15.61.80 for non-blocking migrate release"
 
 	(( OSTCOUNT >= 2 )) || skip_env "needs >= 2 OSTs"
 
@@ -6267,16 +6267,13 @@ test_607b()
 	echo 10 >> "$f"
 
 	old_hsm=$(get_hsm_xattr_sha "$f")
-	$LFS migrate -n -i 1 "$f" ||
-		error "could not migrate file to OST 1"
+	$LFS migrate -n -i 1 "$f" || error "could not migrate file to OST 1"
 
 	$LFS hsm_state "$f" | grep dirty || error "dirty flag not found"
 
 	new_hsm=$(get_hsm_xattr_sha "$f")
-	[ "$old_hsm" != "$new_hsm" ] &&
+	[[ "$old_hsm" == "$new_hsm" ]] ||
 		 error "migrate should not modify data version of dirty files"
-
-	return 0
 }
 run_test 607b "Migrate should not change the HSM attribute of dirty files"
 
@@ -6287,8 +6284,8 @@ test_607c()
 	local fid1 fid2 fid3
 	local nbr_dirty
 
-	(( MDS1_VERSION >= $(version_code 2.15.60) )) ||
-		skip "need MDS version at least 2.15.60"
+	(( MDS1_VERSION >= $(version_code v2_15_61-80-g94d02e5774) )) ||
+		skip "need MDS >= 2.15.61.80 for non-blocking migrate release"
 
 	mkdir_on_mdt0 $d
 	fid1=$(create_small_file "$f-1")
@@ -6314,7 +6311,6 @@ test_607c()
 
 	nbr_dirty=$($LFS hsm_state "$f-1" "$f-3" | grep -c 'dirty')
 	((nbr_dirty == 2)) || error "dirty flag should be set on $f-1 and $f-3"
-
 }
 run_test 607c "'lfs swap_layouts' should set dirty flag on HSM file"
 
@@ -6347,6 +6343,32 @@ test_607d()
 	wait_request_state $fid RESTORE SUCCEED
 }
 run_test 607d "Migrate should not set HSM dirty flag for an empty file"
+
+test_607e()
+{
+	local d="$DIR/$tdir"
+	local f="$DIR/$tdir/$tfile"
+	local fid
+
+	(( MDS1_VERSION >= $(version_code 2.17.50.192) )) ||
+		skip "need MDS >= 2.17.50.192 for non-blocking migrate release"
+
+	(( OSTCOUNT >= 2 )) || skip_env "needs >= 2 OSTs"
+
+	fid=$(test_hsm_migrate_init "$d" "$f" | tail -1)
+
+	copytool setup
+
+	$LFS hsm_archive "$f" || error "could not archive file"
+	wait_request_state $fid ARCHIVE SUCCEED
+
+	$LFS migrate -b -i 1 "$f" || error "could not migrate file to OST 1"
+
+	$LFS hsm_release "$f" || error "could not release file after migrate"
+	$LFS hsm_restore "$f" || error "could not restore file after migrate"
+	wait_request_state $fid RESTORE SUCCEED
+}
+run_test 607e "release file migrated after archive (blocking migrate)"
 
 complete_test $SECONDS
 check_and_cleanup_lustre

commit 529ea457da301b80dd850d7910a20bca956322ea

LU-5896 mdt: allow clients with old mdc_swap_layouts
The size of struct mdc_swap_layouts was increased to improve HSM
layout swap handling, but this broke 'lfs migrate' interoperability
with old clients and new servers due to request buffer size checks:

    lustre_msg_buf_v2() buffer[2] size 8 too small required 24, opc=61
    __req_capsule_get() Wrong buffer for field 'swap_layouts' (2 of 6)
        in format 'MDS_SWAP_LAYOUTS', 8 vs. 24 (client)

Add a new RMF_F_MINIMUM_SIZE RPC buffer check, to ensure the buffer is
at least the requested size, but can be larger.

Add compatibility for smaller mdc_swap_layouts_217 until 3.4.53.

Fixes: 63caacb042 ("LU-5896 mdt: allow release after blocking migrations")
Test-Parameters: testlist=sanity clientversion=2.17 env=ONLY=56
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I26d0d321dd870b4bdd688410a3425dd620300c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64757
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

lustre/include/lustre_swab.h
@@ -114,7 +114,7 @@ void lustre_swab_batch_update_request(struct batch_update_request *bur);
 void lustre_swab_but_update_header(struct but_update_header *buh);
 void lustre_swab_but_update_buffer(struct but_update_buffer *bub);
 void lustre_swab_batch_update_reply(struct batch_update_reply *bur);
-void lustre_swab_swap_layouts(struct mdc_swap_layouts *msl);
+int lustre_swab_swap_layouts(struct mdc_swap_layouts *msl, __u32 size);
 void lustre_swab_close_data(struct close_data *data);
 void lustre_swab_close_data_resync_done(struct close_data_resync_done *resync);
 void lustre_swab_close_data_special(struct close_data *cd, enum mds_op_bias b);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3789,6 +3789,15 @@ struct mdc_swap_layouts {
 	__u64		msl_dv2; /* data version of file 2 */
 };
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+/* extra fields were added in commit v2_17_51-2-g63caacb042.
+ * struct mdc_swap_layouts_217 not needed after 3.4.53.
+ */
+struct mdc_swap_layouts_217 {
+	__u64           msl_flags;
+};
+#endif
+
 #define INLINE_RESYNC_ARRAY_SIZE	15
 struct close_data_resync_done {
 	__u32	resync_count;

lustre/mdc/mdc_request.c
@@ -2276,6 +2276,11 @@ static int mdc_ioc_swap_layouts(struct obd_export *exp,
 
 	payload = req_capsule_client_get(&req->rq_pill, &RMF_SWAP_LAYOUTS);
 	LASSERT(payload);
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	/* allow client to set msl_dv1/msl_dv2 fields until compat not needed */
+	req_capsule_set_size(&req->rq_pill, &RMF_SWAP_LAYOUTS, RCL_CLIENT,
+			     sizeof(*msl));
+#endif
 
 	*payload = *msl;
 

lustre/mdt/mdt_handler.c
@@ -6315,8 +6315,7 @@ TGT_MDT_HDL(HAS_BODY | HAS_REPLY, MDS_HSM_ACTION,	mdt_hsm_action),
 TGT_MDT_HDL(HAS_BODY | HAS_REPLY, MDS_HSM_REQUEST,
 							mdt_hsm_request),
 TGT_MDT_HDL(HAS_KEY | HAS_BODY | HAS_REPLY | IS_MUTABLE,
-	    MDS_SWAP_LAYOUTS,
-	    mdt_swap_layouts),
+				MDS_SWAP_LAYOUTS,	mdt_swap_layouts),
 TGT_MDT_HDL(HAS_BODY | HAS_REPLY | IS_MUTABLE, MDS_HSM_DATA_VERSION,
 	    mdt_hsm_data_version),
 TGT_MDT_HDL(IS_MUTABLE,		MDS_RMFID,	mdt_rmfid),

lustre/ptlrpc/layout.c
@@ -940,6 +940,11 @@ enum rmf_flags {
 	 * @rmf_size worth of bytes.
 	 */
 	RMF_F_STRUCT_ARRAY	= BIT(2),
+	/*
+	 * The field's buffer size must be at least @rmf_size bytes,
+	 * and @rmf_swabber/@rmf_swab_len must handle the variable size.
+	 */
+	RMF_F_MINIMUM_SIZE	= BIT(3),
 };
 
 struct req_capsule;
@@ -1326,8 +1331,14 @@ struct req_msg_field RMF_MDS_HSM_REQUEST =
 EXPORT_SYMBOL(RMF_MDS_HSM_REQUEST);
 
 struct req_msg_field RMF_SWAP_LAYOUTS =
-	DEFINE_MSGF("swap_layouts", 0, sizeof(struct  mdc_swap_layouts),
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	DEFINE_MSGFL("swap_layouts", RMF_F_MINIMUM_SIZE,
+		    sizeof(struct mdc_swap_layouts_217),
 		    lustre_swab_swap_layouts, NULL);
+#else
+	DEFINE_MSGFL("swap_layouts", 0, sizeof(struct mdc_swap_layouts),
+		    lustre_swab_swap_layouts, NULL);
+#endif
 EXPORT_SYMBOL(RMF_SWAP_LAYOUTS);
 
 struct req_msg_field RMF_LFSCK_REQUEST =
@@ -2349,15 +2360,19 @@ static void *__req_capsule_get(struct req_capsule *pill,
 	getter = (field->rmf_flags & RMF_F_STRING) ?
 		(typeof(getter))lustre_msg_string : lustre_msg_buf;
 
-	if (field->rmf_flags & (RMF_F_STRUCT_ARRAY|RMF_F_NO_SIZE_CHECK)) {
-		/*
-		 * We've already asserted that field->rmf_size > 0 in
-		 * req_layout_init().
-		 */
+	/* req_layout_init() already asserted that field->rmf_size > 0 */
+	if (field->rmf_flags &
+	    (RMF_F_STRUCT_ARRAY|RMF_F_NO_SIZE_CHECK|RMF_F_MINIMUM_SIZE)) {
 		len = lustre_msg_buflen(msg, offset);
-		if (!(field->rmf_flags & RMF_F_NO_SIZE_CHECK) &&
+		if (field->rmf_flags & RMF_F_STRUCT_ARRAY &&
 		    (len % field->rmf_size) != 0) {
 			CERROR("%s: array field size mismatch %d modulo %u != 0 (%d)\n",
+				field->rmf_name, len, field->rmf_size, loc);
+			return NULL;
+		}
+		if (field->rmf_flags & RMF_F_MINIMUM_SIZE &&
+		    len < field->rmf_size) {
+			CERROR("%s: array field size %d < minimum %u (%d)\n",
 				field->rmf_name, len, field->rmf_size, loc);
 			return NULL;
 		}
@@ -2544,24 +2559,24 @@ void req_capsule_set_size(struct req_capsule *pill,
 			  const struct req_msg_field *field,
 			  enum req_location loc, __u32 size)
 {
+	__u32 rmf_size = (__u32)field->rmf_size;
+
 	LASSERT(loc == RCL_SERVER || loc == RCL_CLIENT);
 
-	if ((size != (__u32)field->rmf_size) &&
-	    (field->rmf_size != -1) &&
-	    !(field->rmf_flags & RMF_F_NO_SIZE_CHECK) &&
-	    (size > 0)) {
-		__u32 rmf_size = (__u32)field->rmf_size;
+	if (size == rmf_size || size == 0 || field->rmf_size == -1)
+		goto out;
+	if (field->rmf_flags & RMF_F_NO_SIZE_CHECK)
+		goto out;
 
-		LASSERTF(!((field->rmf_flags & RMF_F_STRUCT_ARRAY) &&
-			   (size % rmf_size != 0)),
-			 "%s: array field size mismatch %u %% %u != 0 (%d)\n",
-			 field->rmf_name, size, rmf_size, loc);
-		LASSERTF(!(!(field->rmf_flags & RMF_F_STRUCT_ARRAY) &&
-			   size < rmf_size),
-			 "%s: field size mismatch %u != %u (%d)\n",
-			 field->rmf_name, size, rmf_size, loc);
-	}
+	LASSERTF(!(field->rmf_flags & RMF_F_STRUCT_ARRAY) ||
+		  (size % rmf_size) == 0,
+		 "%s: array field size mismatch %u %% %u != 0 (%d)\n",
+		 field->rmf_name, size, rmf_size, loc);
+	LASSERTF((field->rmf_flags & RMF_F_STRUCT_ARRAY) || size >= rmf_size,
+		 "%s: field size mismatch %u != %u (%d)\n",
+		 field->rmf_name, size, rmf_size, loc);
 
+out:
 	pill->rc_area[loc][__req_capsule_offset(pill, field, loc)] = size;
 }
 EXPORT_SYMBOL(req_capsule_set_size);
@@ -2691,8 +2706,10 @@ void req_capsule_extend(struct req_capsule *pill, const struct req_format *fmt)
 	for (i = 0; i < RCL_NR; ++i) {
 		LASSERT(fmt->rf_fields[i].nr >= old->rf_fields[i].nr);
 		for (j = 0; j < old->rf_fields[i].nr - 1; ++j) {
-			const struct req_msg_field *ofield = FMT_FIELD(old, i,
-								       j);
+			const struct req_msg_field *ofield =
+				FMT_FIELD(old, i, j);
+			const struct req_msg_field *nfield =
+				FMT_FIELD(fmt, i, j);
 
 			/* "opaque" fields can be transmogrified */
 			if (ofield->rmf_swabber == NULL &&
@@ -2700,11 +2717,9 @@ void req_capsule_extend(struct req_capsule *pill, const struct req_format *fmt)
 			    (ofield->rmf_size == -1 ||
 			     ofield->rmf_flags == RMF_F_NO_SIZE_CHECK))
 				continue;
-			LASSERT(FMT_FIELD(fmt, i, j) == FMT_FIELD(old, i, j));
+			LASSERT(ofield == nfield);
 		}
-		/*
-		 * Last field in old format can be shorter than in new.
-		 */
+		/* Last field in new format can be larger than in old. */
 		LASSERT(FMT_FIELD(fmt, i, j)->rmf_size >=
 			FMT_FIELD(old, i, j)->rmf_size);
 	}

lustre/ptlrpc/pack_generic.c
@@ -3073,18 +3073,21 @@ void lustre_swab_but_update_buffer(struct but_update_buffer *bub)
 }
 EXPORT_SYMBOL(lustre_swab_but_update_buffer);
 
-void lustre_swab_swap_layouts(struct mdc_swap_layouts *msl)
+int lustre_swab_swap_layouts(struct mdc_swap_layouts *msl, __u32 size)
 {
 	__swab64s(&msl->msl_flags);
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
 	/* Only swab dv1/dv2 fields if WITH_DV12 flag is sent by client, to
-	 * avoid access beyond the end of old struct mdc_swap_layouts.
+	 * avoid access beyond the end of struct mdc_swap_layouts_217.
 	 */
-	if (!(msl->msl_flags & SWAP_LAYOUTS_WITH_DV12))
-		return;
+	if (!(msl->msl_flags & SWAP_LAYOUTS_WITH_DV12) ||
+	    size < offsetof(struct mdc_swap_layouts, msl_dv2) + sizeof(msl->msl_dv2))
+		return 0;
  #endif
 	__swab64s(&msl->msl_dv1);
 	__swab64s(&msl->msl_dv2);
+
+	return 0;
 }
 
 void lustre_swab_close_data(struct close_data *cd)