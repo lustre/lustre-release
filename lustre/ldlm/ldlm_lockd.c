lustre/lustre-release

commit e150810faa5b22382b2213225478c3763195438b

LU-12931 timers: correctly offset mod_timer.
During a high level code review of the lustre time code it was
discovered that some of the mod_timer() calles was missing
adding the current jiffies value to the timeout that converted
to jiffies from seconds. Add this proper offset.

Fixes: b11be372c21d ("LU-9019 lnet: move ping an delay injection to time64_t")
Fixes: e920b3681451 ("LU-9019 ldlm: migrate the rest of the code to 64 bit time")
Change-Id: Ie4be14946032308610aff2fe72d15d4d70773da1
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/36688
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lockd.c
@@ -349,12 +349,15 @@ static void waiting_locks_callback(TIMER_DATA_TYPE unused)
 	 * left.
 	 */
 	if (!list_empty(&waiting_locks_list)) {
-		unsigned long timeout_jiffies;
+		time64_t now = ktime_get_seconds();
+		time_t delta = 0;
 
 		lock = list_entry(waiting_locks_list.next, struct ldlm_lock,
 				  l_pending_chain);
-		timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
-		mod_timer(&waiting_locks_timer, timeout_jiffies);
+		if (lock->l_callback_timeout - now > 0)
+			delta = lock->l_callback_timeout - now;
+		mod_timer(&waiting_locks_timer,
+			  jiffies + cfs_time_seconds(delta));
 	}
 	spin_unlock_bh(&waiting_locks_spinlock);
 }
@@ -373,8 +376,10 @@ static void waiting_locks_callback(TIMER_DATA_TYPE unused)
  */
 static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds)
 {
-	unsigned long timeout_jiffies;
-	time64_t timeout;
+	unsigned long timeout_jiffies = jiffies;
+	time64_t now = ktime_get_seconds();
+	time64_t deadline;
+	time_t timeout;
 
 	if (!list_empty(&lock->l_pending_chain))
 		return 0;
@@ -383,11 +388,13 @@ static int __ldlm_add_waiting_lock(struct ldlm_lock *lock, time64_t seconds)
 	    OBD_FAIL_CHECK(OBD_FAIL_PTLRPC_HPREQ_TIMEOUT))
 		seconds = 1;
 
-	timeout = ktime_get_seconds() + seconds;
-	if (likely(timeout > lock->l_callback_timeout))
-		lock->l_callback_timeout = timeout;
+	deadline = now + seconds;
+	if (likely(deadline > lock->l_callback_timeout))
+		lock->l_callback_timeout = deadline;
 
-	timeout_jiffies = cfs_time_seconds(lock->l_callback_timeout);
+	timeout = clamp_t(time_t, lock->l_callback_timeout - now,
+			  0, seconds);
+	timeout_jiffies += cfs_time_seconds(timeout);
 
 	if (time_before(timeout_jiffies, waiting_locks_timer.expires) ||
 	    !timer_pending(&waiting_locks_timer))
@@ -504,12 +511,17 @@ static int __ldlm_del_waiting_lock(struct ldlm_lock *lock)
 			/* No more, just cancel. */
 			del_timer(&waiting_locks_timer);
 		} else {
+			time64_t now = ktime_get_seconds();
 			struct ldlm_lock *next;
+			time_t delta = 0;
 
 			next = list_entry(list_next, struct ldlm_lock,
 					  l_pending_chain);
+			if (next->l_callback_timeout - now > 0)
+				delta = lock->l_callback_timeout - now;
+
 			mod_timer(&waiting_locks_timer,
-				  cfs_time_seconds(next->l_callback_timeout));
+				  jiffies + cfs_time_seconds(delta));
 		}
 	}
 	list_del_init(&lock->l_pending_chain);

lnet/lnet/net_fault.c
@@ -487,8 +487,8 @@ struct lnet_delay_rule {
 	time64_t		dl_delay_time;
 	/** baseline to caculate dl_delay_time */
 	time64_t		dl_time_base;
-	/** jiffies to send the next delayed message */
-	unsigned long		dl_msg_send;
+	/** seconds until we send the next delayed message */
+	time64_t		dl_msg_send;
 	/** delayed message list */
 	struct list_head	dl_msg_list;
 	/** statistic of delayed messages */
@@ -593,7 +593,8 @@ delay_rule_match(struct lnet_delay_rule *rule, lnet_nid_t src,
 	msg->msg_delay_send = ktime_get_seconds() + attr->u.delay.la_latency;
 	if (rule->dl_msg_send == -1) {
 		rule->dl_msg_send = msg->msg_delay_send;
-		mod_timer(&rule->dl_timer, rule->dl_msg_send);
+		mod_timer(&rule->dl_timer,
+			  jiffies + cfs_time_seconds(rule->dl_msg_send));
 	}
 
 	spin_unlock(&rule->dl_lock);
@@ -661,7 +662,8 @@ delayed_msg_check(struct lnet_delay_rule *rule, bool all,
 		msg = list_entry(rule->dl_msg_list.next,
 				 struct lnet_msg, msg_list);
 		rule->dl_msg_send = msg->msg_delay_send;
-		mod_timer(&rule->dl_timer, rule->dl_msg_send);
+		mod_timer(&rule->dl_timer,
+			  jiffies + cfs_time_seconds(rule->dl_msg_send));
 	}
 	spin_unlock(&rule->dl_lock);
 }

commit 614580008484b1169ee8da627bef11571c2cc3df

LU-14171 ldlm: fix wrong lock in __ldlm_del_waiting_lock
In __ldlm_del_waiting_lock(), when the head of the
waiting_locks_list is removed and the timer is recalculated
for the next lock, the delta computation incorrectly uses
the removed lock's timestamp instead of the next lock's.

The condition correctly checks next->l_callback_timestamp,
but the delta assignment uses lock->l_callback_timestamp
(the lock being removed). Since the removed lock's callback
has already arrived, its timestamp is at or before the
current time, making delta zero or negative. This causes
the timer to fire immediately, expiring the next lock
prematurely and leading to unnecessary client evictions.

Fix by using next->l_callback_timestamp in the delta
calculation, consistent with waiting_locks_callback().

Fixes: e150810faa5b ("LU-12931 timers: correctly offset mod_timer.")
Test-Parameters: trivial
Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: Idcecc1d882ccbdd458fc69f24b7684578bd7a776
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63845
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lockd.c
@@ -526,7 +526,7 @@ static int __ldlm_del_waiting_lock(struct ldlm_lock *lock)
 			next = list_entry(list_next, struct ldlm_lock,
 					  l_pending_chain);
 			if (next->l_callback_timestamp - now > 0)
-				delta = lock->l_callback_timestamp - now;
+				delta = next->l_callback_timestamp - now;
 
 			mod_timer(&waiting_locks_timer,
 				  jiffies + cfs_time_seconds(delta));