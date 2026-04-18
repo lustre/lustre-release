lustre/lustre-release

commit b054fcd7852f6a22f8ec469ce94ddf6f3331ab34

LU-16159 lod: cancel update llogs upon recovery abort
If recovery is aborted, cancel update catalog from catlist, and keep
them on disk for some time (for debug purpose), as can avoid
accumulating stale update records, and also avoid recovery problems
if update llogs are corrupt.

Update llogs are canceled after recovery completes and before regular
request processing. For these logs, their ctime will be set, and log
header will be marked with LLOG_F_MAX_AGE|LLOG_F_RM_ON_ERR, and when
30 days passed, they will be removed automatically.

Tidy up recovery abort code:
* if obd_abort_recovery is set, or OBD is stopping, stop both
  client recovery and MDT recovery.
* otherwise if obd_abort_mdt_recovery is set, stop MDT recovery only.

lctl llog_print support printing update log FIDs used by specified
MDT:
* "lctl --device <MDT> llog_print update_log" will list all update
  llog FIDs used by this MDT device.

Disabled replay-single.sh 100c stripe check because abort_recovery
will cancel update llogs, and won't replay them upon next recovery.

Added replay-single.sh 100d.

Formatall in the end of replay-single.sh because directory unlink may
fail.

Test-Parameters: mdscount=2 mdtcount=4 testlist=replay-single,replay-single,replay-single,replay-single,replay-single,replay-single,replay-single,replay-single,replay-single
Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: Ie2bda6c097d65f5c51cba66c2dbf6ae4a5d36dda
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48584
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_dev.c
@@ -93,9 +93,11 @@
 #include <md_object.h>
 #include <lustre_fid.h>
 #include <uapi/linux/lustre/lustre_param.h>
+#include <uapi/linux/lustre/lustre_ioctl.h>
 #include <lustre_update.h>
 #include <lustre_log.h>
 #include <lustre_lmv.h>
+#include <llog_swab.h>
 
 #include "lod_internal.h"
 
@@ -275,12 +277,6 @@ struct lod_recovery_data {
 	struct completion	*lrd_started;
 };
 
-static bool lod_recovery_abort(struct obd_device *top)
-{
-	return (top->obd_stopping || top->obd_abort_recovery ||
-		top->obd_abort_recov_mdt);
-}
-
 /**
  * process update recovery record
  *
@@ -336,12 +332,106 @@ static int lod_process_recovery_updates(const struct lu_env *env,
 	       PLOGID(&llh->lgh_id), rec->lrh_index);
 	lut = lod2lu_dev(lrd->lrd_lod)->ld_site->ls_tgt;
 
-	if (lod_recovery_abort(lut->lut_obd))
+	if (obd_mdt_recovery_abort(lut->lut_obd))
 		return -ESHUTDOWN;
 
 	return insert_update_records_to_replay_list(lut->lut_tdtd,
 					(struct llog_update_record *)rec,
 					cookie, index);
+}
+
+/* retain old catalog, create new catalog and update catlist */
+static int lod_sub_recreate_llog(const struct lu_env *env,
+				 struct lod_device *lod, struct dt_device *dt,
+				 int index)
+{
+	struct lod_thread_info *lti = lod_env_info(env);
+	struct llog_ctxt *ctxt;
+	struct llog_handle *lgh;
+	struct llog_catid *cid = &lti->lti_cid;
+	struct lu_fid *fid = &lti->lti_fid;
+	struct obd_device *obd;
+	int rc;
+
+	ENTRY;
+	lu_update_log_fid(fid, index);
+	rc = lodname2mdt_index(lod2obd(lod)->obd_name, (__u32 *)&index);
+	if (rc < 0)
+		RETURN(rc);
+
+	rc = llog_osd_get_cat_list(env, dt, index, 1, NULL, fid);
+	if (rc < 0) {
+		CERROR("%s: can't access update_log: rc = %d\n",
+		       lod2obd(lod)->obd_name, rc);
+		RETURN(rc);
+	}
+
+	obd = dt->dd_lu_dev.ld_obd;
+	ctxt = llog_get_context(obd, LLOG_UPDATELOG_ORIG_CTXT);
+	LASSERT(ctxt != NULL);
+	if (ctxt->loc_handle) {
+		/* retain old catalog */
+		llog_retain(env, ctxt->loc_handle);
+		llog_cat_close(env, ctxt->loc_handle);
+		LASSERT(!ctxt->loc_handle);
+	}
+
+	ctxt->loc_flags |= LLOG_CTXT_FLAG_NORMAL_FID;
+	ctxt->loc_chunk_size = LLOG_MIN_CHUNK_SIZE * 4;
+	rc = llog_open_create(env, ctxt, &lgh, NULL, NULL);
+	if (rc < 0)
+		GOTO(out_put, rc);
+
+	LASSERT(lgh != NULL);
+	rc = llog_init_handle(env, lgh, LLOG_F_IS_CAT, NULL);
+	if (rc != 0)
+		GOTO(out_close, rc);
+
+	cid->lci_logid = lgh->lgh_id;
+	rc = llog_osd_put_cat_list(env, dt, index, 1, cid, fid);
+	if (rc != 0)
+		GOTO(out_close, rc);
+
+	ctxt->loc_handle = lgh;
+
+	CDEBUG(D_INFO, "%s: recreate catalog "DFID"\n",
+	       obd->obd_name, PLOGID(&cid->lci_logid));
+out_close:
+	if (rc)
+		llog_cat_close(env, lgh);
+out_put:
+	llog_ctxt_put(ctxt);
+	RETURN(rc);
+}
+
+/* retain update catalog and llogs, and create a new catalog */
+static int lod_sub_cancel_llog(const struct lu_env *env,
+			       struct lod_device *lod, struct dt_device *dt,
+			       int index)
+{
+	struct llog_ctxt *ctxt;
+	int rc = 0;
+
+	ctxt = llog_get_context(dt->dd_lu_dev.ld_obd,
+				LLOG_UPDATELOG_ORIG_CTXT);
+	if (!ctxt)
+		return 0;
+
+	if (ctxt->loc_handle) {
+		LCONSOLE(D_INFO, "%s: cancel update llog "DFID"\n",
+			 dt->dd_lu_dev.ld_obd->obd_name,
+			 PLOGID(&ctxt->loc_handle->lgh_id));
+		/* set startcat to "lgh_last_idx + 1" to zap empty llogs */
+		llog_cat_process(env, ctxt->loc_handle, NULL, NULL,
+				 ctxt->loc_handle->lgh_last_idx + 1, 0);
+		/* set retention on logs to simplify reclamation */
+		llog_process_or_fork(env, ctxt->loc_handle, llog_cat_retain_cb,
+				     NULL, NULL, false);
+		/* retain old catalog and create a new one */
+		lod_sub_recreate_llog(env, lod, dt, index);
+	}
+	llog_ctxt_put(ctxt);
+	return rc;
 }
 
 /**
@@ -404,7 +494,7 @@ again:
 
 	top_device = lod->lod_dt_dev.dd_lu_dev.ld_site->ls_top_dev;
 	if (rc < 0 && dt != lod->lod_child &&
-	    !lod_recovery_abort(top_device->ld_obd)) {
+	    !obd_mdt_recovery_abort(top_device->ld_obd)) {
 		if (rc == -EBADR) {
 			/* remote update llog is shorter than expected from
 			 * local header. Cached copy could be de-synced during
@@ -433,19 +523,20 @@ again:
 
 	llog_ctxt_put(ctxt);
 	if (rc < 0) {
-		CERROR("%s get update log failed: rc = %d\n",
-		       dt->dd_lu_dev.ld_obd->obd_name, rc);
-		spin_lock(&top_device->ld_obd->obd_dev_lock);
-		if (!lod_recovery_abort(top_device->ld_obd))
-			/* abort just MDT-MDT recovery */
-			top_device->ld_obd->obd_abort_recov_mdt = 1;
-		spin_unlock(&top_device->ld_obd->obd_dev_lock);
-		GOTO(out, rc);
+		CERROR("%s: get update log duration %lld, retries %d, failed: rc = %d\n",
+		       dt->dd_lu_dev.ld_obd->obd_name,
+		       ktime_get_real_seconds() - start, retries, rc);
+		/* abort MDT recovery of this target, but not all targets,
+		 * because recovery still has chance to succeed.
+		 */
+		if (!obd_mdt_recovery_abort(top_device->ld_obd))
+			lod_sub_cancel_llog(env, lod, dt, lrd->lrd_idx);
+	} else {
+		CDEBUG(D_HA,
+		       "%s retrieved update log, duration %lld, retries %d\n",
+		       dt->dd_lu_dev.ld_obd->obd_name,
+		       ktime_get_real_seconds() - start, retries);
 	}
-
-	CDEBUG(D_HA, "%s retrieved update log, duration %lld, retries %d\n",
-	       dt->dd_lu_dev.ld_obd->obd_name, ktime_get_real_seconds() - start,
-	       retries);
 
 	spin_lock(&lod->lod_lock);
 	if (!lrd->lrd_ltd)
@@ -455,13 +546,13 @@ again:
 
 	if (!lod->lod_child_got_update_log) {
 		spin_unlock(&lod->lod_lock);
-		GOTO(out, rc = 0);
+		GOTO(out, rc);
 	}
 
 	lod_foreach_mdt(lod, mdt) {
 		if (!mdt->ltd_got_update_log) {
 			spin_unlock(&lod->lod_lock);
-			GOTO(out, rc = 0);
+			GOTO(out, rc);
 		}
 	}
 	lut->lut_tdtd->tdtd_replay_ready = 1;
@@ -482,7 +573,7 @@ out:
 		wait_var_event(lrd, kthread_should_stop());
 	lu_env_fini(env);
 	OBD_FREE_PTR(lrd);
-	return 0;
+	return rc;
 }
 
 /**
@@ -1165,6 +1256,167 @@ static int lod_sub_init_llogs(const struct lu_env *env, struct lod_device *lod)
 	RETURN(rc);
 }
 
+#define UPDATE_LOG_MAX_AGE	(30 * 24 * 60 * 60)	/* 30 days, in sec */
+
+static int lod_update_log_stale(const struct lu_env *env, struct dt_object *dto,
+				struct lu_buf *buf)
+{
+	struct lu_attr *attr = &lod_env_info(env)->lti_attr;
+	struct llog_log_hdr *hdr;
+	loff_t off = 0;
+	int rc;
+
+	ENTRY;
+	rc = dt_attr_get(env, dto, attr);
+	if (rc)
+		RETURN(rc);
+
+	if (!(attr->la_valid & (LA_CTIME | LA_SIZE)))
+		RETURN(-EFAULT);
+
+	/* by default update log ctime is not set */
+	if (attr->la_ctime == 0)
+		RETURN(0);
+
+	/* update log not expired yet */
+	if (attr->la_ctime + UPDATE_LOG_MAX_AGE > ktime_get_real_seconds())
+		RETURN(0);
+
+	if (attr->la_size == 0)
+		RETURN(-EFAULT);
+
+	rc = dt_read(env, dto, buf, &off);
+	if (rc < 0)
+		RETURN(rc);
+
+	hdr = (struct llog_log_hdr *)buf->lb_buf;
+	if (LLOG_REC_HDR_NEEDS_SWABBING(&hdr->llh_hdr))
+		lustre_swab_llog_hdr(hdr);
+	/* log header is sane and flag LLOG_F_MAX_AGE|LLOG_F_RM_ON_ERR is set */
+	if (rc >= sizeof(*hdr) &&
+	    hdr->llh_hdr.lrh_type == LLOG_HDR_MAGIC &&
+	    (hdr->llh_flags & (LLOG_F_MAX_AGE | LLOG_F_RM_ON_ERR)) ==
+	    (LLOG_F_MAX_AGE | LLOG_F_RM_ON_ERR))
+		RETURN(1);
+
+	RETURN(0);
+}
+
+/**
+ * Reclaim stale update log.
+ *
+ * When update log is canceld (upon recovery abort), it's not destroy, but
+ * canceled from catlist, and set ctime and LLOG_F_MAX_AGE|LLOG_F_RM_ON_ERR,
+ * which is kept for debug. If it expired (more than UPDATE_LOG_MAX_AGE seconds
+ * passed), destroy it to save space.
+ */
+static int lod_update_log_gc(const struct lu_env *env, struct lod_device *lod,
+			     struct dt_object *dir, struct dt_object *dto,
+			     const char *name)
+{
+	struct dt_device *dt = lod->lod_child;
+	struct thandle *th;
+	int rc;
+
+	ENTRY;
+	th = dt_trans_create(env, dt);
+	if (IS_ERR(th))
+		RETURN(PTR_ERR(th));
+
+	rc = dt_declare_delete(env, dir, (const struct dt_key *)name, th);
+	if (rc)
+		GOTO(out_trans, rc);
+
+	rc = dt_declare_destroy(env, dto, th);
+	if (rc)
+		GOTO(out_trans, rc);
+
+	rc = dt_trans_start_local(env, dt, th);
+	if (rc)
+		GOTO(out_trans, rc);
+
+	rc = dt_delete(env, dir, (const struct dt_key *)name, th);
+	if (rc)
+		GOTO(out_trans, rc);
+
+	rc = dt_destroy(env, dto, th);
+	GOTO(out_trans, rc);
+out_trans:
+	dt_trans_stop(env, dt, th);
+
+	return rc;
+}
+
+/* reclaim stale update llogs under "update_log_dir" */
+static int lod_update_log_dir_gc(const struct lu_env *env,
+				 struct lod_device *lod,
+				 struct dt_object *dir)
+{
+	struct lod_thread_info *info = lod_env_info(env);
+	struct lu_buf *buf = &info->lti_linkea_buf;
+	struct lu_dirent *ent = (struct lu_dirent *)info->lti_key;
+	struct lu_fid *fid = &info->lti_fid;
+	struct dt_it *it;
+	const struct dt_it_ops *iops;
+	struct dt_object *dto;
+	int rc;
+
+	ENTRY;
+
+	if (unlikely(!dt_try_as_dir(env, dir, true)))
+		RETURN(-ENOTDIR);
+
+	lu_buf_alloc(buf, sizeof(struct llog_log_hdr));
+	if (!buf->lb_buf)
+		RETURN(-ENOMEM);
+
+	iops = &dir->do_index_ops->dio_it;
+	it = iops->init(env, dir, LUDA_64BITHASH);
+	if (IS_ERR(it))
+		GOTO(out, rc = PTR_ERR(it));
+
+	rc = iops->load(env, it, 0);
+	if (rc == 0)
+		rc = iops->next(env, it);
+	else if (rc > 0)
+		rc = 0;
+
+	while (rc == 0) {
+		rc = iops->rec(env, it, (struct dt_rec *)ent, LUDA_64BITHASH);
+		if (rc != 0)
+			break;
+
+		ent->lde_namelen = le16_to_cpu(ent->lde_namelen);
+		if (ent->lde_name[0] == '.') {
+			if (ent->lde_namelen == 1)
+				goto next;
+
+			if (ent->lde_namelen == 2 && ent->lde_name[1] == '.')
+				goto next;
+		}
+
+		fid_le_to_cpu(fid, &ent->lde_fid);
+		dto = dt_locate(env, lod->lod_child, fid);
+		if (IS_ERR(dto))
+			goto next;
+
+		buf->lb_len = sizeof(struct llog_log_hdr);
+		if (lod_update_log_stale(env, dto, buf) == 1)
+			lod_update_log_gc(env, lod, dir, dto, ent->lde_name);
+		dt_object_put(env, dto);
+next:
+		rc = iops->next(env, it);
+	}
+
+	iops->put(env, it);
+	iops->fini(env, it);
+out:
+	buf->lb_len = sizeof(struct llog_log_hdr);
+	lu_buf_free(buf);
+
+	RETURN(rc > 0 ? 0 : rc);
+}
+
 /**
  * Implementation of lu_device_operations::ldo_prepare() for LOD
  *
@@ -1222,6 +1474,7 @@ static int lod_prepare(const struct lu_env *env, struct lu_device *pdev,
 	if (IS_ERR(dto))
 		GOTO(out_put, rc = PTR_ERR(dto));
 
+	lod_update_log_dir_gc(env, lod, dto);
 	dt_object_put(env, dto);
 
 	rc = lod_prepare_distribute_txn(env, lod);
@@ -2349,8 +2602,188 @@ static int lod_pool_del_q(struct obd_device *obd, char *poolname)
 	return err;
 }
 
+static inline int lod_sub_print_llog(const struct lu_env *env,
+				     struct dt_device *dt, void *data)
+{
+	struct llog_ctxt *ctxt;
+	int rc = 0;
+
+	ENTRY;
+	ctxt = llog_get_context(dt->dd_lu_dev.ld_obd,
+				LLOG_UPDATELOG_ORIG_CTXT);
+	if (!ctxt)
+		RETURN(0);
+
+	if (ctxt->loc_handle) {
+		struct llog_print_data *lprd = data;
+		struct obd_ioctl_data *ioc_data = lprd->lprd_data;
+		int l, remains;
+		long from;
+		char *out;
+
+		LASSERT(ioc_data);
+		if (ioc_data->ioc_inllen1 > 0) {
+			remains = ioc_data->ioc_inllen4 +
+				  round_up(ioc_data->ioc_inllen1, 8) +
+				  round_up(ioc_data->ioc_inllen2, 8) +
+				  round_up(ioc_data->ioc_inllen3, 8);
+
+			rc = kstrtol(ioc_data->ioc_inlbuf2, 0, &from);
+			if (rc)
+				GOTO(ctxt_put, rc);
+
+			/* second iteration from jt_llog_print_iter() */
+			if (from > 1)
+				GOTO(ctxt_put, rc = 0);
+
+			out = ioc_data->ioc_bulk;
+			ioc_data->ioc_inllen1 = 0;
+		} else {
+			out = ioc_data->ioc_bulk + ioc_data->ioc_offset;
+			remains = ioc_data->ioc_count;
+		}
+
+		l = snprintf(out, remains, "%s [catalog]: "DFID"\n",
+			     ctxt->loc_obd->obd_name,
+			     PLOGID(&ctxt->loc_handle->lgh_id));
+		out += l;
+		remains -= l;
+		if (remains <= 0) {
+			CERROR("%s: not enough space for print log records: rc = %d\n",
+			       ctxt->loc_obd->obd_name, -LLOG_EEMPTY);
+			GOTO(ctxt_put, rc = -LLOG_EEMPTY);
+		}
+
+		ioc_data->ioc_offset += l;
+		ioc_data->ioc_count = remains;
+
+		rc = llog_process_or_fork(env, ctxt->loc_handle, llog_print_cb,
+					  data, NULL, false);
+	}
+	GOTO(ctxt_put, rc);
+ctxt_put:
+	llog_ctxt_put(ctxt);
+
+	return rc;
+}
+
+/* print update catalog and update logs FID of all sub devices */
+static int lod_llog_print(const struct lu_env *env, struct lod_device *lod,
+			  void *data)
+{
+	struct lod_tgt_desc *mdt;
+	bool empty = true;
+	int rc = 0;
+
+	ENTRY;
+	rc = lod_sub_print_llog(env, lod->lod_child, data);
+	if (!rc) {
+		empty = false;
+	} else if (rc == -LLOG_EEMPTY) {
+		rc = 0;
+	} else {
+		CERROR("%s: llog_print failed: rc = %d\n",
+		       lod2obd(lod)->obd_name, rc);
+		RETURN(rc);
+	}
+
+	lod_getref(&lod->lod_mdt_descs);
+	lod_foreach_mdt(lod, mdt) {
+		rc = lod_sub_print_llog(env, mdt->ltd_tgt, data);
+		if (!rc) {
+			empty = false;
+		} else if (rc == -LLOG_EEMPTY) {
+			rc = 0;
+		} else {
+			CERROR("%s: llog_print of MDT %u failed: rc = %d\n",
+			       lod2obd(lod)->obd_name, mdt->ltd_index, rc);
+			break;
+		}
+	}
+	lod_putref(lod, &lod->lod_mdt_descs);
+
+	RETURN(rc ? rc : empty ? -LLOG_EEMPTY : 0);
+}
+
+/* cancel update catalog from update catlist */
+static int lod_llog_cancel(const struct lu_env *env, struct lod_device *lod)
+{
+	struct lod_tgt_desc *tgt;
+	int index;
+	int rc;
+	int rc2;
+
+	rc = lodname2mdt_index(lod2obd(lod)->obd_name, (__u32 *)&index);
+	if (rc < 0)
+		return rc;
+
+	rc = lod_sub_cancel_llog(env, lod, lod->lod_child, index);
+
+	lod_getref(&lod->lod_mdt_descs);
+	lod_foreach_mdt(lod, tgt) {
+		LASSERT(tgt && tgt->ltd_tgt);
+		rc2 = lod_sub_cancel_llog(env, lod, tgt->ltd_tgt,
+					  tgt->ltd_index);
+		if (rc2 && !rc)
+			rc = rc2;
+	}
+	lod_putref(lod, &lod->lod_mdt_descs);
+
+	return rc;
+}
+
+static int lod_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
+			 void *karg, void __user *uarg)
+{
+	struct obd_device *obd = exp->exp_obd;
+	struct lod_device *lod = lu2lod_dev(obd->obd_lu_dev);
+	struct obd_ioctl_data *data = karg;
+	struct lu_env env;
+	int rc;
+
+	ENTRY;
+	rc = lu_env_init(&env, LCT_LOCAL | LCT_MD_THREAD);
+	if (rc) {
+		CERROR("%s: can't initialize env: rc = %d\n",
+		       lod2obd(lod)->obd_name, rc);
+		RETURN(rc);
+	}
+
+	switch (cmd) {
+	case OBD_IOC_LLOG_PRINT: {
+		struct llog_print_data lprd = {
+			.lprd_data = data,
+			.lprd_raw = data->ioc_u32_1,
+		};
+		char *logname;
+
+		logname = data->ioc_inlbuf1;
+		if (strcmp(logname, lod_update_log_name) != 0) {
+			rc = -EINVAL;
+			CERROR("%s: llog iocontrol support %s only: rc = %d\n",
+			       lod2obd(lod)->obd_name, lod_update_log_name, rc);
+			RETURN(rc);
+		}
+
+		LASSERT(data->ioc_inllen1 > 0);
+		rc = lod_llog_print(&env, lod, &lprd);
+		break;
+	}
+	case OBD_IOC_LLOG_CANCEL:
+		rc = lod_llog_cancel(&env, lod);
+		break;
+	default:
+		CERROR("%s: unrecognized ioctl %#x by %s\n",
+		       obd->obd_name, cmd, current->comm);
+		rc = -ENOTTY;
+	}
+	lu_env_fini(&env);
+
+	RETURN(rc);
+}
+
 static const struct obd_ops lod_obd_device_ops = {
-	.o_owner        = THIS_MODULE,
+	.o_owner	= THIS_MODULE,
 	.o_connect      = lod_obd_connect,
 	.o_disconnect   = lod_obd_disconnect,
 	.o_get_info     = lod_obd_get_info,
@@ -2359,6 +2792,7 @@ static const struct obd_ops lod_obd_device_ops = {
 	.o_pool_rem     = lod_pool_remove_q,
 	.o_pool_add     = lod_pool_add_q,
 	.o_pool_del     = lod_pool_del_q,
+	.o_iocontrol	= lod_iocontrol,
 };
 
 static int __init lod_init(void)

lustre/include/lustre_log.h
@@ -109,6 +109,7 @@ int llog_backup(const struct lu_env *env, struct obd_device *obd,
 int llog_read_header(const struct lu_env *env, struct llog_handle *handle,
 		     const struct obd_uuid *uuid);
 __u64 llog_size(const struct lu_env *env, struct llog_handle *llh);
+int llog_retain(const struct lu_env *env, struct llog_handle *log);
 
 /* llog_process flags */
 #define LLOG_FLAG_NODEAMON 0x0001
@@ -180,6 +181,8 @@ __u32 llog_cat_free_space(struct llog_handle *cat_llh);
 int llog_cat_reverse_process(const struct lu_env *env,
 			     struct llog_handle *cat_llh, llog_cb_t cb,
 			     void *data);
+int llog_cat_retain_cb(const struct lu_env *env, struct llog_handle *cat,
+		       struct llog_rec_hdr *rec, void *data);
 /* llog_obd.c */
 int llog_setup(const struct lu_env *env, struct obd_device *obd,
 	       struct obd_llog_group *olg, int index,
@@ -190,11 +193,19 @@ int llog_sync(struct llog_ctxt *ctxt, struct obd_export *exp, int flags);
 
 /* llog_ioctl.c */
 struct obd_ioctl_data;
+int llog_print_cb(const struct lu_env *env, struct llog_handle *handle,
+		  struct llog_rec_hdr *rec, void *data);
 int llog_ioctl(const struct lu_env *env, struct llog_ctxt *ctxt, int cmd,
 	       struct obd_ioctl_data *data);
 int llog_catalog_list(const struct lu_env *env, struct dt_device *d,
 		      int count, struct obd_ioctl_data *data,
 		      const struct lu_fid *fid);
+
+struct llog_print_data {
+	struct obd_ioctl_data *lprd_data;
+	unsigned int	       lprd_cfg_flags;
+	bool		       lprd_raw;
+};
 
 /* llog_net.c */
 int llog_initiator_connect(struct llog_ctxt *ctxt);

lustre/include/obd.h
@@ -612,8 +612,8 @@ struct obd_device {
 		obd_attached:1,		/* finished attach */
 		obd_set_up:1,		/* finished setup */
 		obd_recovering:1,	/* there are recoverable clients */
-		obd_abort_recovery:1,	/* recovery expired */
-		obd_abort_recov_mdt:1,	/* only abort recovery between MDTs */
+		obd_abort_recovery:1,	/* abort client and MDT recovery */
+		obd_abort_mdt_recovery:1, /* abort recovery between MDTs */
 		obd_version_recov:1,	/* obd uses version checking */
 		obd_replayable:1,	/* recovery enabled; inform clients */
 		obd_no_recov:1,		/* fail instead of retry messages */
@@ -761,6 +761,19 @@ int obd_nid_export_for_each(struct obd_device *obd, struct lnet_nid *nid,
 			    void *data);
 int obd_nid_add(struct obd_device *obd, struct obd_export *exp);
 void obd_nid_del(struct obd_device *obd, struct obd_export *exp);
+
+/* both client and MDT recovery are aborted, or MDT is stopping  */
+static inline bool obd_recovery_abort(struct obd_device *obd)
+{
+	return obd->obd_stopping || obd->obd_abort_recovery;
+}
+
+/* MDT recovery is aborted, or MDT is stopping */
+static inline bool obd_mdt_recovery_abort(struct obd_device *obd)
+{
+	return obd->obd_stopping || obd->obd_abort_recovery ||
+	       obd->obd_abort_mdt_recovery;
+}
 #endif
 
 /* get/set_info keys */

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2954,6 +2954,7 @@ enum llog_flag {
 	LLOG_F_EXT_X_OMODE	= 0x100,
 	LLOG_F_EXT_X_XATTR	= 0x200,
 	LLOG_F_RM_ON_ERR	= 0x400,
+	LLOG_F_MAX_AGE		= 0x800,
 
 	/* Note: Flags covered by LLOG_F_EXT_MASK will be inherited from
 	 * catlog to plain log, so do not add LLOG_F_IS_FIXSIZE here,

lustre/ldlm/ldlm_lib.c
@@ -49,6 +49,7 @@
 #include <lustre_dlm.h>
 #include <lustre_net.h>
 #include <lustre_sec.h>
+#include <uapi/linux/lustre/lustre_ioctl.h>
 #include "ldlm_internal.h"
 
 /*
@@ -1875,8 +1876,9 @@ void target_cleanup_recovery(struct obd_device *obd)
 		EXIT;
 		return;
 	}
-	obd->obd_recovering = obd->obd_abort_recovery = 0;
-	obd->obd_abort_recov_mdt = 0;
+	obd->obd_recovering = 0;
+	obd->obd_abort_recovery = 0;
+	obd->obd_abort_mdt_recovery = 0;
 	spin_unlock(&obd->obd_dev_lock);
 
 	spin_lock(&obd->obd_recovery_task_lock);
@@ -2123,7 +2125,7 @@ static int check_for_next_transno(struct lu_target *lut)
 		req_transno = lustre_msg_get_transno(req->rq_reqmsg);
 	}
 
-	if (!obd->obd_abort_recov_mdt && tdtd)
+	if (!obd_mdt_recovery_abort(obd) && tdtd)
 		update_transno = distribute_txn_get_next_transno(tdtd);
 
 	connected = atomic_read(&obd->obd_connected_clients);
@@ -2137,13 +2139,13 @@ static int check_for_next_transno(struct lu_target *lut)
 	       connected, completed,
 	       queue_len, req_transno, next_transno);
 
-	if (obd->obd_abort_recovery) {
+	if (obd_recovery_abort(obd)) {
 		CDEBUG(D_HA, "waking for aborted recovery\n");
 		wake_up = 1;
 	} else if (obd->obd_recovery_expired) {
 		CDEBUG(D_HA, "waking for expired recovery\n");
 		wake_up = 1;
-	} else if (!obd->obd_abort_recov_mdt && tdtd && req &&
+	} else if (!obd_mdt_recovery_abort(obd) && tdtd && req &&
 		   is_req_replayed_by_update(req)) {
 		LASSERTF(req_transno < next_transno,
 			 "req_transno %llu next_transno%llu\n", req_transno,
@@ -2211,7 +2213,7 @@ static int check_update_llog(struct lu_target *lut)
 	struct obd_device *obd = lut->lut_obd;
 	struct target_distribute_txn_data *tdtd = lut->lut_tdtd;
 
-	if (obd->obd_abort_recovery) {
+	if (obd_mdt_recovery_abort(obd)) {
 		CDEBUG(D_HA, "waking for aborted recovery\n");
 		return 1;
 	}
@@ -2255,7 +2257,7 @@ repeat:
 		 * left in the queue
 		 */
 		spin_lock(&obd->obd_recovery_task_lock);
-		if (!obd->obd_abort_recov_mdt && lut->lut_tdtd) {
+		if (!obd_mdt_recovery_abort(obd) && lut->lut_tdtd) {
 			next_update_transno =
 				distribute_txn_get_next_transno(lut->lut_tdtd);
 
@@ -2279,7 +2281,7 @@ repeat:
 			}
 		}
 
-		if (next_update_transno != 0 && !obd->obd_abort_recovery) {
+		if (next_update_transno != 0 && !obd_recovery_abort(obd)) {
 			obd->obd_next_recovery_transno = next_update_transno;
 			spin_unlock(&obd->obd_recovery_task_lock);
 			/*
@@ -2309,7 +2311,7 @@ repeat:
 				  cfs_time_seconds(60)) == 0)
 		; /* wait indefinitely for event, but don't trigger watchdog */
 
-	if (obd->obd_abort_recovery) {
+	if (obd_recovery_abort(obd)) {
 		CWARN("recovery is aborted, evict exports in recovery\n");
 		if (lut->lut_tdtd != NULL) {
 			tdtd = lut->lut_tdtd;
@@ -2474,7 +2476,7 @@ static int check_for_recovery_ready(struct lu_target *lut)
 	       atomic_read(&obd->obd_max_recoverable_clients),
 	       obd->obd_abort_recovery, obd->obd_recovery_expired);
 
-	if (!obd->obd_abort_recovery && !obd->obd_recovery_expired) {
+	if (!obd_recovery_abort(obd) && !obd->obd_recovery_expired) {
 		LASSERT(clnts <=
 			atomic_read(&obd->obd_max_recoverable_clients));
 		if (clnts + obd->obd_stale_clients <
@@ -2482,20 +2484,16 @@ static int check_for_recovery_ready(struct lu_target *lut)
 			return 0;
 	}
 
-	if (!obd->obd_abort_recov_mdt && lut->lut_tdtd != NULL) {
-		if (!lut->lut_tdtd->tdtd_replay_ready &&
-		    !obd->obd_abort_recovery && !obd->obd_stopping) {
-			/*
-			 * Let's extend recovery timer, in case the recovery
-			 * timer expired, and some clients got evicted
-			 */
-			extend_recovery_timer(obd, obd->obd_recovery_timeout,
-					      true);
-			CDEBUG(D_HA,
-			       "%s update recovery is not ready, extend recovery %d\n",
-			       obd->obd_name, obd->obd_recovery_timeout);
-			return 0;
-		}
+	if (!obd_mdt_recovery_abort(obd) && lut->lut_tdtd &&
+	    !lut->lut_tdtd->tdtd_replay_ready) {
+		/* Let's extend recovery timer, in case the recovery timer
+		 * expired, and some clients got evicted
+		 */
+		extend_recovery_timer(obd, obd->obd_recovery_timeout, true);
+		CDEBUG(D_HA,
+		       "%s update recovery is not ready, extend recovery %d\n",
+		       obd->obd_name, obd->obd_recovery_timeout);
+		return 0;
 	}
 
 	return 1;
@@ -2534,7 +2532,7 @@ static __u64 get_next_transno(struct lu_target *lut, int *type)
 	if (type != NULL)
 		*type = REQUEST_RECOVERY;
 
-	if (!tdtd || obd->obd_abort_recov_mdt)
+	if (!tdtd || obd_mdt_recovery_abort(obd))
 		RETURN(transno);
 
 	update_transno = distribute_txn_get_next_transno(tdtd);
@@ -2821,13 +2819,20 @@ static int target_recovery_thread(void *arg)
 	CDEBUG(D_INFO, "3: final stage - process recovery completion pings\n");
 	/** Update server last boot epoch */
 	tgt_boot_epoch_update(lut);
+
+	/* cancel update llogs upon recovery abort */
+	if (obd->obd_abort_recovery || obd->obd_abort_mdt_recovery)
+		OBP(obd, iocontrol)(OBD_IOC_LLOG_CANCEL, obd->obd_self_export,
+				    0, NULL, NULL);
+
 	/*
 	 * We drop recoverying flag to forward all new requests
 	 * to regular mds_handle() since now
 	 */
 	spin_lock(&obd->obd_dev_lock);
-	obd->obd_recovering = obd->obd_abort_recovery = 0;
-	obd->obd_abort_recov_mdt = 0;
+	obd->obd_recovering = 0;
+	obd->obd_abort_recovery = 0;
+	obd->obd_abort_mdt_recovery = 0;
 	spin_unlock(&obd->obd_dev_lock);
 	spin_lock(&obd->obd_recovery_task_lock);
 	target_cancel_recovery_timer(obd);

lustre/mdd/mdd_device.c
@@ -2283,6 +2283,10 @@ static int mdd_iocontrol(const struct lu_env *env, struct md_device *m,
 				 (struct lfsck_query *)karg);
 		RETURN(rc);
 	}
+	case OBD_IOC_LLOG_PRINT:
+	case OBD_IOC_LLOG_CANCEL:
+		rc = obd_iocontrol(cmd, mdd->mdd_child_exp, len, karg, NULL);
+		RETURN(rc);
 	}
 
 	/* Below ioctls use obd_ioctl_data */

commit 8c84bd28fa030f0b6aa0e82bb8301022705f5bec

LU-20026 lod: delete reference on update logs when destroyed
Add a missing reference deletion to update logs, so that the inode
link count drops to 0 when they are destroyed. This issue was found by
running e2fsck on the MDTs, which caused the inodes to be considered
unattached, as the names in update_log_dir had already been removed:

 # e2fsck -E no_optimize_extents -f -v -t -t -n <device node>
...
Pass 4: Checking reference counts
Unattached inode 141
Connect to /lost+found? no

Unattached inode 70665135
Connect to /lost+found? no
...

HPE-bug-id: LUS-12225
Fixes: b054fcd785 ("LU-16159 lod: cancel update llogs upon recovery abort")
Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Change-Id: I301dde20b976663c98b91d33aac45d90daaaf6b7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64755
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>

lustre/lod/lod_dev.c
@@ -1305,6 +1305,10 @@ static int lod_update_log_gc(const struct lu_env *env, struct lod_device *lod,
 	if (rc)
 		GOTO(out_trans, rc);
 
+	rc = dt_declare_ref_del(env, dto, th);
+	if (rc)
+		GOTO(out_trans, rc);
+
 	rc = dt_declare_destroy(env, dto, th);
 	if (rc)
 		GOTO(out_trans, rc);
@@ -1317,7 +1321,11 @@ static int lod_update_log_gc(const struct lu_env *env, struct lod_device *lod,
 	if (rc)
 		GOTO(out_trans, rc);
 
-	rc = dt_destroy(env, dto, th);
+	dt_write_lock(env, dto, DT_TGT_CHILD);
+	rc = dt_ref_del(env, dto, th);
+	if (!rc)
+		rc = dt_destroy(env, dto, th);
+	dt_write_unlock(env, dto);
 	GOTO(out_trans, rc);
 out_trans:
 	dt_trans_stop(env, dt, th);