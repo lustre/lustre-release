lustre/lustre-release

commit 73ee4e7fe31f31fae6b823086f706869b50adec6

LU-16386 mkfs: Handle --version argument correctly
Running mkfs.lustre with --version or -V argument
fails instead of printing the version. This patch
fixes the error.

Without patch:
--------------
$ ./lustre/utils/mkfs.lustre --version
usage: mkfs.lustre <target type> [--backfstype=ldiskfs]
<snip>

With patch:
-----------
$ ./lustre/utils/mkfs.lustre --version
mkfs.lustre 2.15.52_175_ge7aa83d

Test-Parameters: trivial fstype=zfs testlist=sanity
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I4d4d1144d669fce8b02e9f8c3fb5f45f68b337b4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49379
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/mkfs_lustre.c
@@ -825,6 +825,41 @@ int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 	return 0;
 }
 
+/*
+ * This function checks args for sanity.
+ *
+ * Returns 0 if all is found good.
+ * Returns 1 if invalid args is detected
+ */
+int chk_args(int argc, char *const argv[])
+{
+	/* If no argument is given to mkfs.lustre, bail out */
+	if (argc < 2)
+		return 1;
+
+	/* If argc is exactly 2, make sure the user wants "--version"
+	 * or "-V" and handle correctly if that is what the user wants
+	 * without bailing out with error.
+	 */
+	if (argc == 2) {
+		if (strncmp(argv[1], "--version", strlen(argv[1])) == 0 &&
+		    (strlen(argv[1]) == 9))
+			return 0;
+
+		if (strncmp(argv[1], "-V", strlen(argv[1])) == 0 &&
+		    (strlen(argv[1]) == 2))
+			return 0;
+	}
+
+	/* This check validates if the last argument is really a device name
+	 * and does not start with char '-'.
+	 */
+	if ((argv[argc - 1][0] == '-'))
+		return 1;
+	else
+		return 0;
+}
+
 int main(int argc, char *const argv[])
 {
 	struct mkfs_opts mop;
@@ -842,7 +877,7 @@ int main(int argc, char *const argv[])
 	else
 		progname = argv[0];
 
-	if ((argc < 2) || (argv[argc - 1][0] == '-')) {
+	if (chk_args(argc, argv)) {
 		usage(stderr);
 		return EINVAL;
 	}

commit 1bc7f48e6c1999cac88b59703fcf821307b1067d

LU-19944 utils: Fix tunefs.lustre -V/--version
tunefs.lustre -V/--version printed an error instead of version.

Add conf-sanity test_11 to verify --version works for Lustre utils.

Test-Parameters: trivial testlist=conf-sanity
Fixes: 73ee4e7fe31f ("LU-16386 mkfs: Handle --version argument correctly")
Signed-off-by: Xiao Yang <xyang@ddn.com>
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I06bdc929364ca12422d9981ba93987bc7daadb02
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64348
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/mkfs_lustre.c
@@ -50,7 +50,7 @@
 char *progname;
 int replace = 0;
 int verbose = 1;
-int version;
+static int version;
 static int print_only;
 #ifdef TUNEFS
 static int erase_all;
@@ -344,6 +344,12 @@ static int parse_opts_early(int argc, char *const argv[], struct mkfs_opts *mop)
 {
 	int opt;
 
+	/* If no argument is given, bail out */
+	if (argc < 2) {
+		usage(stderr);
+		return EINVAL;
+	}
+
 	while ((opt = getopt_long(argc, argv, short_opts, long_opts,
 				  NULL)) != EOF) {
 		switch (opt) {
@@ -352,6 +358,11 @@ static int parse_opts_early(int argc, char *const argv[], struct mkfs_opts *mop)
 			erase_all++;
 			break;
 #endif
+		case 'V':
+			version++;
+			fprintf(stdout, "%s %s\n", progname,
+				LUSTRE_VERSION_STRING);
+			return 0;
 		default:
 			break;
 		}
@@ -561,10 +572,8 @@ static int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 			verbose++;
 			break;
 		case 'V':
-			++version;
-			fprintf(stdout, "%s %s\n", progname,
-				LUSTRE_VERSION_STRING);
-			return 0;
+			/* Already handled in parse_opts_early() */
+			break;
 #ifndef TUNEFS
 		case 'b': {
 			int i = 0;
@@ -760,41 +769,6 @@ static int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 	return 0;
 }
 
-/*
- * This function checks args for sanity.
- *
- * Returns 0 if all is found good.
- * Returns 1 if invalid args is detected
- */
-static int chk_args(int argc, char *const argv[])
-{
-	/* If no argument is given to mkfs.lustre, bail out */
-	if (argc < 2)
-		return 1;
-
-	/* If argc is exactly 2, make sure the user wants "--version"
-	 * or "-V" and handle correctly if that is what the user wants
-	 * without bailing out with error.
-	 */
-	if (argc == 2) {
-		if (strncmp(argv[1], "--version", strlen(argv[1])) == 0 &&
-		    (strlen(argv[1]) == 9))
-			return 0;
-
-		if (strncmp(argv[1], "-V", strlen(argv[1])) == 0 &&
-		    (strlen(argv[1]) == 2))
-			return 0;
-	}
-
-	/* This check validates if the last argument is really a device name
-	 * and does not start with char '-'.
-	 */
-	if (argv[argc - 1][0] == '-')
-		return 1;
-	else
-		return 0;
-}
-
 int main(int argc, char *const argv[])
 {
 	struct mkfs_opts mop;
@@ -820,17 +794,14 @@ int main(int argc, char *const argv[])
 		return ret;
 	}
 
-	if (chk_args(argc, argv)) {
-		usage(stderr);
-		return EINVAL;
-	}
-
 	memset(&mop, 0, sizeof(mop));
 	set_defaults(&mop);
 
 	ret = parse_opts_early(argc, argv, &mop);
-	if (ret != 0)
-		goto out;
+	if (ret != 0 || version) {
+		osd_fini();
+		return ret;
+	}
 
 #ifdef TUNEFS
 	/*
@@ -889,7 +860,7 @@ int main(int argc, char *const argv[])
 
 	ret = parse_opts(argc, argv, &mop, &mountopts, old_fsname,
 			 mountdata_arg);
-	if (ret != 0 || version)
+	if (ret != 0)
 		goto out;
 
 #ifdef TUNEFS

lustre/tests/conf-sanity.sh
@@ -417,6 +417,24 @@ test_10a() {
 }
 run_test 10a "find lctl param broken symlinks"
 
+test_11() {
+	# set $version variable with tool build version
+	local version=$($LCTL --version | awk '{ print $2 }')
+
+	[[ -n "$version" ]] || error "'$LCTL --version' did not print version"
+
+	for cmd in $LFS $LNETCTL $LST $MKFS $MOUNT_LUSTRE $TUNEFS; do
+		local tool_ver0=$(do_facet mgs $cmd --version 2>&1)
+		local tool_ver1=$(do_facet mgs $cmd --version some_args 2>&1)
+
+		[[ "$tool_ver0" =~ $version ]] ||
+			error "'$cmd --version' has ' $tool_ver0', not '$version'"
+		[[ "$tool_ver1" =~ $version ]] ||
+			error "'$cmd --version some_args ' has '$tool_ver1', not '$version'"
+	done
+}
+run_test 11 "Verify tool --version option works properly"
+
 #
 # Test 16 was to "verify that lustre will correct the mode of OBJECTS".
 # But with new MDS stack we don't care about the mode of local objects

lustre/tests/test-framework.sh
@@ -578,6 +578,8 @@ init_test_env() {
 		export LL_DECODE_LINKEA="ll_decode_linkea"
 	export MKFS=${MKFS:-"$LUSTRE/utils/mkfs.lustre"}
 	[ ! -f "$MKFS" ] && export MKFS="mkfs.lustre"
+	export MOUNT_LUSTRE=${MOUNT_LUSTRE:-"$LUSTRE/utils/mount.lustre"}
+	[[ ! -f "$MOUNT_LUSTRE" ]] && export MOUNT_LUSTRE=$(which mount.lustre)
 	export TUNEFS=${TUNEFS:-"$LUSTRE/utils/tunefs.lustre"}
 	[ ! -f "$TUNEFS" ] && export TUNEFS="tunefs.lustre"
 	export CHECKSTAT="${CHECKSTAT:-"checkstat -v"} "