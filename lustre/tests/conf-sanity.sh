lustre/lustre-release

commit 8f819877be20240028bc34eb5173e6dd90acdd29

LU-18986 mgs: server part of new registration protocol
Rework mgs_target_reg() to handle new protocol along with
old one for older targets

It handles old protocol with NIDs either in mti_nids or
in mti_nidlist[], and new protocol with NIDs in
mtn_inline_list[] or bulk

All NIDs are put in mti_nidlist[] as result of request
processing, so that eliminates need in extra changes in
further code path

Test-Parameters: testlist=runtests ossversion=EXA6.3.2
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I41dd487c37136e24328914e33c9ce056be013aae
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59206
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mgs/mgs_handler.c
@@ -330,13 +330,16 @@ static int mgs_target_reg(struct tgt_session_info *tsi)
 {
 	struct obd_device *obd = tsi->tsi_exp->exp_obd;
 	struct mgs_device *mgs = exp2mgs_dev(tsi->tsi_exp);
-	struct mgs_target_info *mti, *rep_mti;
+	struct mgs_target_info *mti, *reply_mti, *request_mti;
+	struct mgs_target_nidlist *mtn = NULL;
+	struct ptlrpc_bulk_desc *desc = NULL;
 	struct fs_db *b_fsdb = NULL; /* barrier fsdb */
 	struct fs_db *c_fsdb = NULL; /* config fsdb */
 	char barrier_name[20];
-	size_t mti_len = 0;
+	size_t mti_buflen, mti_alloc = 0;
 	int opc;
 	int rc = 0;
+	bool nidlist;
 
 	ENTRY;
 	rc = lu_env_refill((struct lu_env *)tsi->tsi_env);
@@ -345,16 +348,87 @@ static int mgs_target_reg(struct tgt_session_info *tsi)
 
 	tgt_counter_incr(tsi->tsi_exp, LPROC_MGS_TARGET_REG);
 
-	mti = req_capsule_client_get(tsi->tsi_pill, &RMF_MGS_TARGET_INFO);
-	if (mti == NULL) {
-		DEBUG_REQ(D_HA, tgt_ses_req(tsi), "no mgs_send_param");
-		RETURN(err_serious(-EFAULT));
+	nidlist = exp_connect_flags(tsi->tsi_exp) & OBD_CONNECT_MGS_NIDLIST;
+
+	request_mti = req_capsule_client_get(tsi->tsi_pill,
+					     &RMF_MGS_TARGET_INFO);
+	if (!request_mti) {
+		DEBUG_REQ(D_HA, tgt_ses_req(tsi), "no mgs_target_info");
+		RETURN(err_serious(-EPROTO));
+	}
+	mti_buflen = req_capsule_get_size(tsi->tsi_pill, &RMF_MGS_TARGET_INFO,
+					  RCL_CLIENT);
+
+	/* Compatibility code for older targets, process mti as is */
+	if (!nidlist) {
+		int limit;
+
+		mti = request_mti;
+		/* sanity check for mti_nid_count */
+		if (mti_buflen > sizeof(*mti))
+			limit = (mti_buflen - sizeof(*mti)) / MTN_NIDSTR_SIZE;
+		else
+			limit = MTI_NIDS_MAX;
+
+		if (mti->mti_nid_count > limit) {
+			CWARN("%s: bad NID count in mti: %d, req limit: %d\n",
+			      mti->mti_svname, mti->mti_nid_count, limit);
+			mti->mti_nid_count = limit;
+		}
+		goto process;
 	}
 
+	req_capsule_extend(tsi->tsi_pill, &RQF_MGS_TARGET_REG_NIDLIST);
+	if (!req_capsule_field_present(tsi->tsi_pill, &RMF_MGS_TARGET_NIDLIST,
+				       RCL_CLIENT)) {
+		DEBUG_REQ(D_HA, tgt_ses_req(tsi), "no mgs_target_nidlist");
+		RETURN(err_serious(-EPROTO));
+	}
+
+	/* new protocol with nidlist */
+	mtn = req_capsule_client_get(tsi->tsi_pill, &RMF_MGS_TARGET_NIDLIST);
+	mti_alloc = sizeof(*mti) + NIDLIST_SIZE(mtn->mtn_nids);
+	OBD_ALLOC_LARGE(mti, mti_alloc);
+	if (!mti)
+		RETURN(err_serious(-ENOMEM));
+
+	if (mtn->mtn_flags & NIDLIST_IN_BULK) {
+		int pages;
+		size_t nidlist_size = NIDLIST_SIZE(mtn->mtn_nids);
+
+		pages = DIV_ROUND_UP((sizeof(*mti) & ~PAGE_MASK) +
+				     nidlist_size, PAGE_SIZE);
+		desc = ptlrpc_prep_bulk_exp(tsi->tsi_pill->rc_req,
+					    pages, PTLRPC_BULK_OPS_COUNT,
+					    PTLRPC_BULK_GET_SINK,
+					    MGS_BULK_PORTAL,
+					    &ptlrpc_bulk_kiov_nopin_ops);
+		if (!desc)
+			GOTO(out_mti_free, rc = err_serious(-ENOMEM));
+
+		desc->bd_frag_ops->add_iov_frag(desc, mti->mti_nidlist,
+						nidlist_size);
+		tsi->tsi_pill->rc_req->rq_bulk_write = 1;
+		rc = sptlrpc_svc_prep_bulk(tsi->tsi_pill->rc_req, desc);
+		if (rc != 0)
+			GOTO(out_free, rc = err_serious(rc));
+
+		rc = target_bulk_io(tsi->tsi_pill->rc_req->rq_export, desc);
+		if (rc < 0)
+			GOTO(out_free, rc = err_serious(rc));
+	} else {
+		memcpy(mti->mti_nidlist, mtn->mtn_inline_list,
+		       NIDLIST_SIZE(mtn->mtn_nids));
+	}
+	*mti = *request_mti;
+	mti->mti_nid_count = mtn->mtn_nids;
+	mti->mti_flags |= LDD_F_LARGE_NID;
+
+process:
+	/* at this point all NIDs are in mti */
 	down_read(&mgs->mgs_barrier_rwsem);
 
-	if (OCD_HAS_FLAG(&tgt_ses_req(tsi)->rq_export->exp_connect_data,
-			 IMP_RECOV))
+	if (OCD_HAS_FLAG(&tsi->tsi_exp->exp_connect_data, IMP_RECOV))
 		opc = mti->mti_flags & LDD_F_OPC_MASK;
 	else
 		opc = LDD_F_OPC_REG;
@@ -527,29 +601,35 @@ out_norevoke:
 	if (rc)
 		mti->mti_flags |= LDD_F_ERROR;
 
-	/* send back the whole mti in the reply */
-	if (target_supports_large_nid(mti)) {
-		size_t len = offsetof(struct mgs_target_info, mti_nidlist);
+	/* Compatibility code:
+	 * if large mti was received, send back the same buffer size as that
+	 * MGC expects, so avoid buffer size mismatch errors on MGC side
+	 */
+	if (mti_buflen > sizeof(*mti)) {
 		int err;
 
-		mti_len = mti->mti_nid_count * LNET_NIDSTR_SIZE;
 		err = req_capsule_server_grow(tsi->tsi_pill,
-					      &RMF_MGS_TARGET_INFO,
-					      len + mti_len);
+					      &RMF_MGS_TARGET_INFO, mti_buflen);
 		if (err < 0)
-			RETURN(err);
+			GOTO(out_fsdb, rc = err_serious(err));
 	}
-	rep_mti = req_capsule_server_get(tsi->tsi_pill, &RMF_MGS_TARGET_INFO);
-	*rep_mti = *mti;
-	if (target_supports_large_nid(mti))
-		memcpy(rep_mti->mti_nidlist, mti->mti_nidlist, mti_len);
+	reply_mti = req_capsule_server_get(tsi->tsi_pill, &RMF_MGS_TARGET_INFO);
+	*reply_mti = *mti;
 
 	/* Flush logs to disk */
 	dt_sync(tsi->tsi_env, mgs->mgs_bottom);
+
+out_fsdb:
 	if (b_fsdb)
 		mgs_put_fsdb(mgs, b_fsdb);
 	if (c_fsdb)
 		mgs_put_fsdb(mgs, c_fsdb);
+out_free:
+	ptlrpc_free_bulk(desc);
+out_mti_free:
+	if (mti_alloc)
+		OBD_FREE_LARGE(mti, mti_alloc);
+
 	RETURN(rc);
 }
 

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -955,6 +955,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT_FULL20 | OBD_CONNECT_IMP_RECOV | \
 				OBD_CONNECT_PINGLESS |\
 				OBD_CONNECT_BULK_MBITS | OBD_CONNECT_BARRIER | \
+				OBD_CONNECT_MGS_NIDLIST | \
 				OBD_CONNECT_FLAGS2)
 
 #define MGS_CONNECT_SUPPORTED2	OBD_CONNECT2_REP_MBITS | \

lustre/ptlrpc/client.c
@@ -306,7 +306,9 @@ void ptlrpc_free_bulk(struct ptlrpc_bulk_desc *desc)
 {
 	ENTRY;
 
-	LASSERT(desc != NULL);
+	if (!desc)
+		return;
+
 	LASSERT(desc->bd_iov_count != LI_POISON); /* not freed already */
 	LASSERT(desc->bd_refs == 0);         /* network hands off */
 	LASSERT((desc->bd_export != NULL) ^ (desc->bd_import != NULL));

commit 2867efe066c8e188c66eca54f3e295fb6241111a

LU-20054 mgs: more checks for incoming buffers
Check for TARGET_NIDLIST buffer size to make sure
that NIDs were packed in it but not in mti buffer.
This can be in case of initial registration when MGC
is not yet connected, so export connection flags
are empty and buffers were packed in compatible manner

Test conf-sanity 73f is added to simulate that situation

Patch also handles IPv6 NID on target by assuming that MGS
has large NID support if there are IPv6 NIDs on target.
So if MGC is not yet connected but there are IPv6 NIDs
then target will pack them in registration request, and
they will not be packed only if MGS is connected and
reports no support for large NIDs explicitly

Test-Parameters: testgroup=review-dne-ipv6
Fixes: 8f819877be ("LU-18986 mgs: server part of new registration protocol")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ifaeab2c5e0d5f33cd55047c96a64b2581cb580f8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64953
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>

lustre/include/obd_support.h
@@ -543,6 +543,7 @@ extern bool obd_enable_fname_encoding;
 #define OBD_FAIL_MGS_CONFIG_READ_NET	 0x90c
 #define OBD_FAIL_MGS_LDLM_REPLY_NET	 0x90d
 #define OBD_FAIL_MGS_WRITE_TARGET_DELAY	 0x90e
+#define OBD_FAIL_MGC_REG_BEFORE_CONN	 0x90f
 
 #define OBD_FAIL_QUOTA_DQACQ_NET         0xA01
 #define OBD_FAIL_QUOTA_EDQUOT            0xA02

lustre/mgc/mgc_request_server.c
@@ -189,7 +189,10 @@ static int mgc_target_register(struct obd_export *exp,
 	server_mti_print("mgc_target_register: req", mti);
 
 	nidlist = exp_connect_flags(exp) & OBD_CONNECT_MGS_NIDLIST;
-	large_nids = exp_connect_flags2(exp) & OBD_CONNECT2_LARGE_NID;
+	large_nids = target_supports_large_nid(mti);
+
+	if (CFS_FAIL_CHECK(OBD_FAIL_MGC_REG_BEFORE_CONN))
+		nidlist = false;
 
 	/* it is OK to use new protocol with an old MGS, mti buffer is the
 	 * same in both cases
@@ -248,12 +251,13 @@ static int mgc_target_register(struct obd_export *exp,
 		ptlrpc_req_put(req);
 		RETURN(-ENOMEM);
 	}
-	mtn->mtn_nids = mti->mti_nid_count;
+	mtn->mtn_nids = 0;
 	mtn->mtn_flags = 0;
 
 	if (pages) {
 		LASSERT(nidlist);
 		mtn->mtn_flags |= NIDLIST_IN_BULK;
+		mtn->mtn_nids = mti->mti_nid_count;
 		req->rq_bulk_write = 1;
 		desc = ptlrpc_prep_bulk_imp(req, pages,
 					    MD_MAX_BRW_SIZE >> LNET_MTU_BITS,
@@ -267,6 +271,7 @@ static int mgc_target_register(struct obd_export *exp,
 		desc->bd_frag_ops->add_iov_frag(desc, mti->mti_nidlist,
 						nidlist_size);
 	} else if (nidlist) {
+		mtn->mtn_nids = mti->mti_nid_count;
 		memcpy(mtn->mtn_inline_list, mti->mti_nidlist, nidlist_size);
 	} else if (large_nids) {
 		memcpy(request_mti, mti, sizeof(*mti) + nidlist_size);

lustre/mgs/mgs_handler.c
@@ -358,6 +358,30 @@ static int mgs_target_reg(struct tgt_session_info *tsi)
 	}
 	mti_buflen = req_capsule_get_size(tsi->tsi_pill, &RMF_MGS_TARGET_INFO,
 					  RCL_CLIENT);
+	if (nidlist) {
+		req_capsule_extend(tsi->tsi_pill, &RQF_MGS_TARGET_REG_NIDLIST);
+		if (!req_capsule_field_present(tsi->tsi_pill,
+					       &RMF_MGS_TARGET_NIDLIST,
+					       RCL_CLIENT)) {
+			DEBUG_REQ(D_HA, tgt_ses_req(tsi),
+				  "no mgs_target_nidlist");
+			RETURN(err_serious(-EPROTO));
+		}
+
+		/* new protocol with nidlist */
+		mtn = req_capsule_client_get(tsi->tsi_pill,
+					     &RMF_MGS_TARGET_NIDLIST);
+
+		/* Initial request may be with NIDs in mti, in that case
+		 * NIDLIST buffer has just header without NIDs.
+		 * Check it to avoid mismatch with client and set nidlist
+		 * to false if so. Exception is a bulk request.
+		 */
+		if (req_capsule_get_size(tsi->tsi_pill,
+					 &RMF_MGS_TARGET_NIDLIST,
+					 RCL_CLIENT) <= sizeof(*mtn))
+			nidlist = !!(mtn->mtn_flags & NIDLIST_IN_BULK);
+	}
 
 	/* Compatibility code for older targets, process mti as is */
 	if (!nidlist || !target_supports_large_nid(request_mti)) {
@@ -378,15 +402,6 @@ static int mgs_target_reg(struct tgt_session_info *tsi)
 		goto process;
 	}
 
-	req_capsule_extend(tsi->tsi_pill, &RQF_MGS_TARGET_REG_NIDLIST);
-	if (!req_capsule_field_present(tsi->tsi_pill, &RMF_MGS_TARGET_NIDLIST,
-				       RCL_CLIENT)) {
-		DEBUG_REQ(D_HA, tgt_ses_req(tsi), "no mgs_target_nidlist");
-		RETURN(err_serious(-EPROTO));
-	}
-
-	/* new protocol with nidlist */
-	mtn = req_capsule_client_get(tsi->tsi_pill, &RMF_MGS_TARGET_NIDLIST);
 	mti_alloc = sizeof(*mti) + NIDLIST_SIZE(mtn->mtn_nids);
 	OBD_ALLOC_LARGE(mti, mti_alloc);
 	if (!mti)

lustre/target/tgt_mount.c
@@ -1196,6 +1196,8 @@ struct nid_fetch_data {
 	GENRADIX(struct lnet_nid) nfd_radix;
 	struct lustre_mount_data *nfd_lmd;
 	unsigned int nfd_pos;
+	bool nfd_skip_ipv6;
+	bool nfd_has_ipv6;
 };
 
 static int server_nid2radix(void *data, struct lnet_nid *nid)
@@ -1205,6 +1207,13 @@ static int server_nid2radix(void *data, struct lnet_nid *nid)
 
 	if (nid_is_lo0(nid))
 		return 0;
+
+	/* skip IPv6 NIDs for an old server */
+	if (!nid_is_nid4(nid)) {
+		if (nfd->nfd_skip_ipv6)
+			return 0;
+		nfd->nfd_has_ipv6 = true;
+	}
 
 	if (nfd->nfd_lmd &&
 	    test_bit(LMD_FLG_NO_PRIMNODE, nfd->nfd_lmd->lmd_flags) &&
@@ -1231,10 +1240,14 @@ static struct mgs_target_info *server_lsi2mti(struct lustre_sb_info *lsi,
 	__u32 refnet = LNET_NET_ANY;
 	char *buf;
 	int rc, i = 0, nid_count;
+	bool mgc_no_connect;
 
 	ENTRY;
 	if (!IS_SERVER(lsi))
 		RETURN(ERR_PTR(-EINVAL));
+
+	/* MGC has no flags if it was never connected to MGS yet */
+	mgc_no_connect = !exp_connect_flags(lsi->lsi_mgc->u.cli.cl_mgc_mgsexp);
 
 	if (exp_connect_flags2(lsi->lsi_mgc->u.cli.cl_mgc_mgsexp) &
 	    OBD_CONNECT2_LARGE_NID)
@@ -1267,6 +1280,9 @@ static struct mgs_target_info *server_lsi2mti(struct lustre_sb_info *lsi,
 	genradix_prealloc(&nfd.nfd_radix, MTI_NIDS_MAX, GFP_KERNEL);
 	nfd.nfd_lmd = registration ? lsi->lsi_lmd : NULL;
 	nfd.nfd_pos = 0;
+	/* skip IPv6 NIDs if MGS reports that explicitly by finished connect */
+	nfd.nfd_skip_ipv6 = !large_nid && !mgc_no_connect;
+	nfd.nfd_has_ipv6 = false;
 
 	LNetFetchNIDs(server_nid2radix, refnet, &nfd);
 	nid_count = nfd.nfd_pos;
@@ -1275,7 +1291,11 @@ static struct mgs_target_info *server_lsi2mti(struct lustre_sb_info *lsi,
 		       lsi->lsi_svname);
 		GOTO(free_radix, mti = ERR_PTR(-EINVAL));
 	}
-
+	/* at this point large_nid is either set already or MGC is not yet
+	 * connected, but LNet reports IPv6 NIDs, so assume MGS is not too
+	 * old and aware about large NIDs
+	 */
+	large_nid |= nfd.nfd_has_ipv6;
 	if (large_nid)
 		len += NIDLIST_SIZE(nid_count);
 	else if (nid_count > MTI_NIDS_MAX)
@@ -1354,8 +1374,7 @@ static int server_register_target(struct lustre_sb_info *lsi)
 	if (IS_ERR(mti))
 		GOTO(out, rc = PTR_ERR(mti));
 
-	if (exp_connect_flags2(lsi->lsi_mgc->u.cli.cl_mgc_mgsexp) &
-	    OBD_CONNECT2_LARGE_NID) {
+	if (target_supports_large_nid(mti)) {
 		nidstr = mti->mti_nidlist[0]; /* large_nid */
 	} else {
 		lnet_nid4_to_nid(mti->mti_nids[0], &nid);

lustre/tests/conf-sanity.sh
@@ -6750,6 +6750,19 @@ test_73e() {
 }
 run_test 73e "Mount client with dynamic server NIDs"
 
+test_73f() {
+	(( $MDS1_VERSION >= $(version_code 2.17.51) )) ||
+		skip "Need server version at least 2.16.51 for LU-20054"
+
+	stack_trap "cleanup"
+	reformat
+	start_mgsmds || error "Failed to start MDT"
+#define OBD_FAIL_MGC_REG_BEFORE_CONN 0x90f
+	do_facet ost1 "$LCTL set_param fail_loc=0x8000090f"
+	start_ost || error "Failed to start ost1"
+}
+run_test 73f "mgc register before connect"
+
 # LU-15246
 test_74() {
 	(( $MDS1_VERSION >= $(version_code 2.15.57.16) )) ||