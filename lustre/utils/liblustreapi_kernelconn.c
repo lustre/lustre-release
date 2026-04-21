4b23f993fab7bbbd23e49bbe8313acab73014b50
Aurelien Degremont <adegremont@nvidia.com>
2026-04-21 15:25:53 +0200

    LU-20183 hsm: handle short-read with KUC
    It is possible for some coordinator workload to write
    KUC messages larger than a page (ie: large 'data' buffer).
    libcfs_ukuc_msg_get() is calling read() and is expecting this read
    to get a full kuc message in 1 call. However, when message
    is larger than a page, and the system is under load, it is possible
    that the read() will return the 1st page first, and a second call
    is needed to get the full page. This is causing message drop.
    
    This patch is fixing this short-read ensuring partial reads
    and other typical read errors are handled.
    
    If a Changelog reader is registered with O_NONBLOCK, it will
    still receive -EAGAIN if the pipe is empty while trying a read
    a new message. However, if the pipe does not have enough bytes
    to read a full message, while some bytes have already been read,
    it will block until the full message is read. This keep
    the main use case for O_NONBLOCK, while fixing the bug.
    
    Fixes: fd44a6d8c7 ("LU-6245 utils: split kernel comm between user and kernel")
    Signed-off-by: Aurelien Degremont <adegremont@nvidia.com>
    Change-Id: I0d22aa7b4dc59c8d119267ad101bcfd032ec114c
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65462
    Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Chakshu Kansal <ckansal@ddn.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/utils/liblustreapi_kernelconn.c
+++ lustre/utils/liblustreapi_kernelconn.c
@@ -18,6 +18,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <poll.h>
 
 #include <lustre/lustreapi.h>
 
@@ -77,8 +78,46 @@ int libcfs_ukuc_get_rfd(struct lustre_kernelcomm *link)
 
 #define lhsz sizeof(*kuch)
 
+/* Read exactly @count bytes from the pipe into @buf, looping over short
+ * reads. Returns 0 on success, a negative errno on failure.
+ */
+static int ukuc_full_read(int fd, char *buf, size_t count)
+{
+	size_t total = 0;
+
+	while (total < count) {
+		ssize_t rc = read(fd, buf + total, count - total);
+
+		if (rc > 0) {
+			total += rc;
+		} else if (rc == 0) {
+			return -EPIPE;
+		} else if (errno == EINTR) {
+			continue;
+		} else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+			struct pollfd pfd = { .fd = fd, .events = POLLIN };
+
+			/* No bytes yet: propagate EAGAIN so that O_NONBLOCK
+			 * callers multiplexing with poll(2) keep working.
+			 */
+			if (total == 0)
+				return -EAGAIN;
+
+			/* Mid-message: we have already consumed part of a
+			 * framed message and must finish it to stay in sync.
+			 * Wait for the remainder.
+			 */
+			if (poll(&pfd, 1, -1) < 0 && errno != EINTR)
+				return -errno;
+		} else {
+			return -errno;
+		}
+	}
+	return 0;
+}
+
 /** Read a message from the link.
- * Allocates memory, returns handle
+ * Reads one complete message into @a buf (caller-allocated).
  *
  * @param link Private descriptor for pipe/socket.
  * @param buf Buffer to read into, must include size for kuc_hdr
@@ -92,15 +131,17 @@ int libcfs_ukuc_msg_get(struct lustre_kernelcomm *link, char *buf, int maxsize,
 	struct kuc_hdr *kuch;
 	int rc = 0;
 
+	if (buf == NULL || maxsize < 0 || maxsize < lhsz)
+		return -EINVAL;
+
 	memset(buf, 0, maxsize);
 
 	while (1) {
 		/* Read header first to get message size */
-		rc = read(link->lk_rfd, buf, lhsz);
-		if (rc <= 0) {
-			rc = -errno;
+		rc = ukuc_full_read(link->lk_rfd, buf, lhsz);
+		if (rc < 0)
 			break;
-		}
+
 		kuch = (struct kuc_hdr *)buf;
 
 		if (kuch->kuc_magic != KUC_MAGIC) {
@@ -111,24 +152,20 @@ int libcfs_ukuc_msg_get(struct lustre_kernelcomm *link, char *buf, int maxsize,
 			break;
 		}
 
+		if (kuch->kuc_msglen < lhsz) {
+			rc = -EPROTO;
+			break;
+		}
+
 		if (kuch->kuc_msglen > maxsize) {
 			rc = -EMSGSIZE;
 			break;
 		}
 
 		/* Read payload */
-		rc = read(link->lk_rfd, buf + lhsz, kuch->kuc_msglen - lhsz);
-		if (rc < 0) {
-			rc = -errno;
+		rc = ukuc_full_read(link->lk_rfd, buf + lhsz, kuch->kuc_msglen - lhsz);
+		if (rc < 0)
 			break;
-		}
-		if (rc < (kuch->kuc_msglen - lhsz)) {
-			llapi_err_noerrno(LLAPI_MSG_ERROR,
-					  "short read: got %d of %d bytes\n",
-					  rc, kuch->kuc_msglen);
-			rc = -EPROTO;
-			break;
-		}
 
 		if (kuch->kuc_transport == transport ||
 		    kuch->kuc_transport == KUC_TRANSPORT_GENERIC) {
