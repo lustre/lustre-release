lustre/lustre-release

commit 2d91a30ae324bd9773612890b25afc1b84e88d0c

LU-19478 nodemap: clear dyn nodemaps when config is changed
When persistent nodemaps are modified, synchronization to other server
targets replaces the nodemap config. As a result, all dynamic nodemaps
are removed as well, which is indended.

However, when the MGS is co-located with another server target like
MDS or OSS, the dynamic nodemaps are not cleared on the MGS node and
only on the remote server targets. This results in an inconsistent
dynamic nodemap configuration across server targets.

This patch fixes this issue by explicitly clearing all dynamic
nodemaps on the MGS node if the MGS is co-located with an MDS or OSS.

The FIXME in Sanity-sec tests 72d and 72f was replaced to cover this
use case.

Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: I6923ad469fb7031885bafd1f409e46359a9bb9ec
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62808
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_nodemap.h
@@ -203,6 +203,7 @@ struct nm_config_file {
 int nodemap_activate(const bool value);
 int nodemap_add(const char *nodemap_name, bool dynamic);
 int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset);
+void nodemap_clear_dynamic_nodemaps(void);
 int nodemap_add_member(struct lnet_nid *nid, struct obd_export *exp);
 void nodemap_del_member(struct obd_export *exp);
 int nodemap_parse_range(const char *range_string, struct lnet_nid range[2],

lustre/mgs/mgs_handler.c
@@ -933,6 +933,37 @@ out:
 	return rc;
 }
 
+/**
+ * mgs_has_local_targets() - check if MGS is co-located with an MDT or OST
+ *
+ * Check if there are any MDT or OST targets on the MGS node. This is done by
+ * checking whether the device types are registered.
+ *
+ * Return: true if MDT or OST type exists, false otherwise
+ */
+static bool mgs_has_local_targets(void)
+{
+	struct obd_type *mdt_type;
+	struct obd_type *ost_type;
+	bool has_targets = false;
+
+	mdt_type = class_search_type(LUSTRE_MDT_NAME);
+	if (mdt_type) {
+		kobject_put(&mdt_type->typ_kobj);
+		has_targets = true;
+	}
+
+	if (!has_targets) {
+		ost_type = class_search_type(LUSTRE_OST_NAME);
+		if (ost_type) {
+			kobject_put(&ost_type->typ_kobj);
+			has_targets = true;
+		}
+	}
+
+	return has_targets;
+}
+
 static int mgs_iocontrol_nodemap(const struct lu_env *env,
 				 struct mgs_device *mgs,
 				 struct obd_ioctl_data *data)
@@ -973,6 +1004,10 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 			CWARN("%s: failed to cleanup llog fileset: %d\n",
 			      mgs->mgs_obd->obd_name, rc2);
 	}
+
+	/* if MGS is co-located with an MDT or OST, clear dynamic nodemaps */
+	if (mgs_has_local_targets())
+		nodemap_clear_dynamic_nodemaps();
 
 	/* revoke nodemap lock */
 	rc = mgs_find_or_make_fsdb(env, mgs, LUSTRE_NODEMAP_NAME, &fsdb);

lustre/ptlrpc/nodemap_handler.c
@@ -4157,6 +4157,45 @@ out:
 }
 EXPORT_SYMBOL(nodemap_del);
 
+/**
+ * nodemap_clear_dynamic_nodemaps() - Remove all dynamic nodemaps
+ *
+ * This function iterates over all persistent nodemaps and deletes their
+ * sub-nodemaps.
+ */
+void nodemap_clear_dynamic_nodemaps(void)
+{
+	struct lu_nodemap *nodemap, *tmp;
+	struct lu_nodemap *dyn_nm, *dyn_nm_tmp;
+	LIST_HEAD(nodemap_list);
+
+	mutex_lock(&active_config_lock);
+	cfs_hash_for_each_safe(active_config->nmc_nodemap_hash, nm_hash_list_cb,
+			       &nodemap_list);
+
+	/* take refs on persistent nodemaps, remove dynamic ones from list */
+	list_for_each_entry_safe(nodemap, tmp, &nodemap_list, nm_list) {
+		if (nodemap->nm_dyn)
+			list_del(&nodemap->nm_list);
+		else
+			nodemap_getref(nodemap);
+	}
+	mutex_unlock(&active_config_lock);
+
+	/* for each persistent nodemap, delete its sub-nodemaps */
+	list_for_each_entry_safe(nodemap, tmp, &nodemap_list, nm_list) {
+		list_for_each_entry_safe(dyn_nm, dyn_nm_tmp,
+					 &nodemap->nm_subnodemaps,
+					 nm_parent_entry) {
+			/* nodemap_del() recursively deletes sub-dyn-nodemaps */
+			nodemap_del(dyn_nm->nm_name, NULL);
+		}
+
+		nodemap_putref(nodemap);
+	}
+}
+EXPORT_SYMBOL(nodemap_clear_dynamic_nodemaps);
+
 /* Do not call this method directly unless the ranges and nodemap have been
  * previously verified.
  * Store separate offset+limit in case this needs to be changed

lustre/tests/sanity-sec.sh
@@ -8703,18 +8703,17 @@ test_72d() {
 	local nm=nm_test72d
 	local nm2=subnm_test72d
 	local val
-	local same_server_node=false
+	local delete_dyn_nm=false
 
 	is_multi_fileset_supported "ost1" ||
 		skip "Need OSS >= 2.16.57 for multiple filesets"
 
 	# When MGS and OST are colocated, we need to explicitly remove the
-	# dynamic nodemap.
-	# FIXME we should be able to rely on the nodemap synchronization wiping
-	# all dynamic nodemaps even if the MGS and OST are colocated. In this
-	# case, "same_server_node" should be removed and not separately handled.
+	# dynamic nodemap on earlier versions (LU-19478) because nodemap
+	# synchronization does not explicitly wipe them.
 	[[ "$(facet_active_host mgs)" == "$(facet_active_host ost1)" ]] &&
-		same_server_node=true
+		(( OST1_VERSION < $(version_code 2.16.61) )) &&
+		delete_dyn_nm=true
 
 	stack_trap "cleanup_72d $mgsnm $nm $nm2" EXIT
 
@@ -8757,7 +8756,7 @@ test_72d() {
 		error "modify fileset for $mgsnm on MGS failed"
 	wait_nm_sync $mgsnm fileset '' inactive
 
-	if $same_server_node; then
+	if $delete_dyn_nm; then
 		do_facet ost1 $LCTL nodemap_del $nm
 	fi
 
@@ -8984,19 +8983,18 @@ test_72f() {
 	local fileset_prim="/primary"
 	local fileset_alt1="/alt1"
 	local fileset_alt2="/alt2"
-	local same_server_node=false
+	local delete_dyn_nm=false
 	local val
 
 	is_multi_fileset_supported "ost1" ||
 		skip "Need OSS >= 2.16.57 for multiple filesets"
 
 	# When MGS and OST are colocated, we need to explicitly remove the
-	# dynamic nodemap.
-	# FIXME we should be able to rely on the nodemap synchronization wiping
-	# all dynamic nodemaps even if the MGS and OST are colocated. In this
-	# case, "same_server_node" should be removed and not separately handled.
+	# dynamic nodemap on earlier versions (LU-19478) because nodemap
+	# synchronization does not explicitly wipe them.
 	[[ "$(facet_active_host mgs)" == "$(facet_active_host ost1)" ]] &&
-		same_server_node=true
+		(( OST1_VERSION < $(version_code 2.16.61) )) &&
+		delete_dyn_nm=true
 
 	stack_trap "cleanup_72f $mgsnm $nm" EXIT
 
@@ -9072,7 +9070,7 @@ test_72f() {
 		error "add fileset $fileset_alt2 to $mgsnm failed"
 	wait_nm_sync $mgsnm fileset '' inactive
 
-	if $same_server_node; then
+	if $delete_dyn_nm; then
 		do_facet ost1 $LCTL nodemap_del $nm
 	fi
 

commit c667633d42253150256213e80ba0c8756da2b68b

LU-20151 nodemap: skip dynamic nodemap clear when none exist
To ensure dynamic nodemaps are properly removed if the MGS is
co-located with an MDT/OST, dynamic nodemaps are cleared on every
ioctl that involves updating the nodemap config. This is important
because on non-co-located targets, changing the nodemap wipes the
config including dynamic nodemaps. Clearing dynamic nodemaps involves
iterating the full nodemap hash even when no dynamic nodemaps exist,
making bulk operations O(n^2). As a result, sanity-sec test 26
regressed from ~400s to ~2000s.

This patch tracks the number of dynamic nodemaps in the
nodemap_config, so the MGS ioctl path can skip clearning dyn. nodemaps
when none exist. As a result, test 26 completes in similar completion times as before.

Fixes: 2d91a30ae3 ("LU-19478 nodemap: clear dyn nodemaps when config is changed")
Test-Parameters: trivial testlist=sanity-sec
Test-Parameters: testlist=sanity-sec env=ONLY=26,ONLY_REPEAT=5
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: Iccf79819eceeb7a60921a4993c51ad298e7a4c63
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65438
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/include/lustre_nodemap.h
@@ -220,6 +220,7 @@ struct lu_nodemap *nodemap_lookup_unlocked(const char *name);
 int nodemap_add(const char *nodemap_name, bool dynamic);
 int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset);
 void nodemap_clear_dynamic_nodemaps(void);
+bool nodemap_has_dynamic_nodemaps(void);
 int nodemap_add_member(struct ptlrpc_svc_ctx *svc_ctx, struct lnet_nid *nid,
 		       struct obd_export *exp);
 void nodemap_del_member(struct obd_export *exp);
@@ -351,6 +352,9 @@ struct nodemap_config {
 	 * containing all nodemaps
 	 */
 	struct rhashtable nmc_nodemap_sha_hash;
+
+	/* Number of dynamic nodemaps in this config */
+	atomic_t nmc_dyn_count;
 };
 
 struct nodemap_config *nodemap_config_alloc(void);

lustre/mgs/mgs_handler.c
@@ -1044,7 +1044,7 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 	}
 
 	/* if MGS is co-located with an MDT or OST, clear dynamic nodemaps */
-	if (mgs_has_local_targets())
+	if (nodemap_has_dynamic_nodemaps() && mgs_has_local_targets())
 		nodemap_clear_dynamic_nodemaps();
 
 	/* revoke nodemap lock */

lustre/ptlrpc/nodemap_handler.c
@@ -3607,6 +3607,9 @@ struct lu_nodemap *nodemap_create(const char *name,
 		CWARN("adding nodemap '%s' to config without default nodemap\n",
 		      nodemap->nm_name);
 
+	if (dynamic)
+		atomic_inc(&config->nmc_dyn_count);
+
 	RETURN(nodemap);
 
 out_list_hash:
@@ -4325,6 +4328,8 @@ int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset)
 	(void)rhashtable_remove_fast(&active_config->nmc_nodemap_sha_hash,
 				     &nodemap->nm_sha_hash,
 				     nodemap_sha_hash_params);
+	if (nodemap->nm_dyn)
+		atomic_dec(&active_config->nmc_dyn_count);
 	nodemap_putref(nodemap);
 
 	/* erase nodemap from active ranges to prevent client assignment */
@@ -4391,6 +4396,17 @@ out:
 	return rc;
 }
 EXPORT_SYMBOL(nodemap_del);
+
+/**
+ * nodemap_has_dynamic_nodemaps() - Check if any dynamic nodemaps exist
+ *
+ * Return: true if any dynamic nodemaps are present in the active config
+ */
+bool nodemap_has_dynamic_nodemaps(void)
+{
+	return atomic_read(&active_config->nmc_dyn_count) > 0;
+}
+EXPORT_SYMBOL(nodemap_has_dynamic_nodemaps);
 
 /**
  * nodemap_clear_dynamic_nodemaps() - Remove all dynamic nodemaps
@@ -4717,6 +4733,7 @@ struct nodemap_config *nodemap_config_alloc(void)
 	config->nmc_range_tree.nmrt_range_interval_root = RB_ROOT_CACHED;
 	config->nmc_ban_range_tree.nmrt_range_interval_root =
 		RB_ROOT_CACHED;
+	atomic_set(&config->nmc_dyn_count, 0);
 
 	return config;
 }