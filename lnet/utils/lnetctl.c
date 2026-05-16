lustre/lustre-release

commit 18bc6c4a54786efec127a92ffa6559dcac3e9e88

LU-20000 lnetctl: bad CPTs/tunables during import
This patch fixes only old API. There will be another patch
for netlink.

When an LNet configuration is imported, parsing of CPTs and tunables
was done wrong. This patch fixes the following issues:
- Wrong CPTs if different between two NIs of same net
- lnd_tunables cannot be different between NIs
- lnd_tunables not read if no tunables
- Wrong tunables (lnd_tunables) read from NI if first is empty
- Tunables can be set directly in the net
- Documentation had malformed yaml example
- Show warning if we try to set different tunables (not lnd_tunables)
  for NIs of a same net

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Cyril Bordage <cbordage@whamcloud.com>
Change-Id: I234c4bf3ca5b0ed7caa47c30308dca0465daaa40
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65055
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Manish Regmi <mregmi@ddn.com>
Reviewed-by: Xinxia Qu <xqu@ddn.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/utils/lnetctl.c
@@ -5370,17 +5370,19 @@ static int jt_import(int argc, char **argv)
 	char *yaml_blk = NULL, *buf, cmd = 'a';
 	int flags = NLM_F_CREATE;
 	bool release = true;
+	bool oldapi = false;
 	char err_str[256];
 	struct stat st;
 	FILE *input;
 	size_t len;
-	const char *const short_options = "adseh";
+	const char *const short_options = "adseoh";
 	static const struct option long_options[] = {
-		{ .name = "add",  .has_arg = no_argument, .val = 'a' },
-		{ .name = "del",  .has_arg = no_argument, .val = 'd' },
-		{ .name = "show", .has_arg = no_argument, .val = 's' },
-		{ .name = "exec", .has_arg = no_argument, .val = 'e' },
-		{ .name = "help", .has_arg = no_argument, .val = 'h' },
+		{ .name = "add",     .has_arg = no_argument, .val = 'a' },
+		{ .name = "del",     .has_arg = no_argument, .val = 'd' },
+		{ .name = "show",    .has_arg = no_argument, .val = 's' },
+		{ .name = "exec",    .has_arg = no_argument, .val = 'e' },
+		{ .name = "old-api", .has_arg = no_argument, .val = 'o' },
+		{ .name = "help",    .has_arg = no_argument, .val = 'h' },
 		{ .name = NULL }
 	};
 	bool done = false, unspec = true;
@@ -5412,6 +5414,9 @@ static int jt_import(int argc, char **argv)
 			/* use NLM_F_CREATE for discover */
 			cmd = opt;
 			break;
+		case 'o':
+			oldapi = true;
+			break;
 		case 'h':
 			printf("import FILE\n"
 			       "import < FILE : import a file\n"
@@ -5419,6 +5424,7 @@ static int jt_import(int argc, char **argv)
 			       "\t--del: delete configuration\n"
 			       "\t--show: show configuration\n"
 			       "\t--exec: execute command\n"
+			       "\t--old-api: do not use netlink (for tests)\n"
 			       "\t--help: display this help\n"
 			       "If no command option is given then --add"
 			       " is assumed by default\n");
@@ -5451,6 +5457,9 @@ static int jt_import(int argc, char **argv)
 	} else {
 		input = stdin;
 	}
+
+	if (oldapi)
+		goto old_api;
 
 	/* Create Netlink emitter to send request to kernel */
 	sk = nl_socket_alloc();

lnet/lnet/api-ni.c
@@ -2860,10 +2860,12 @@ lnet_startup_lndnet(struct lnet_net *net, struct lnet_lnd_tunables *tun)
 		 * TODO - note. currently the tunables can not be updated
 		 * once added
 		 */
+		if (memcmp(&net_l->net_tunables, &net->net_tunables, sizeof(net->net_tunables)))
+			CWARN("All tunables of NIs of a net should be the same\n");
 		lnet_net_free(net);
 	} else {
 		/*
-		 * restore tunables after it has been overwitten by the
+		 * restore tunables after it has been overwritten by the
 		 * lnd
 		 */
 		if (peer_timeout != -1)

lnet/utils/lnetconfig/cyaml.c
@@ -734,6 +734,21 @@ struct cYAML *cYAML_get_object_item(struct cYAML *parent, const char *name)
 	return found;
 }
 
+struct cYAML *cYAML_get_object_child(struct cYAML *parent, const char *name)
+{
+	struct cYAML *node = parent;
+
+	if (!node || !name || node->cy_type != CYAML_TYPE_OBJECT)
+		return NULL;
+
+	for (node = node->cy_child; node != NULL; node = node->cy_next)
+		if (node->cy_string)
+			if (strcmp(node->cy_string, name) == 0)
+				return node;
+
+	return NULL;
+}
+
 struct cYAML *cYAML_get_next_seq_item(struct cYAML *seq, struct cYAML **itm)
 {
 	if (*itm != NULL && (*itm)->cy_next != NULL) {

lnet/utils/lnetconfig/cyaml.h
@@ -116,14 +116,23 @@ void cYAML_free_tree(struct cYAML *node);
 
 /*
  * cYAML_get_object_item
- *   Returns the cYAML object which key correspods to the name passed in
+ *   Returns the cYAML object which key corresponds to the name passed in.
  *   This function searches only through the current level.
  *
  *   parent - is the parent object on which you want to conduct the search
  *   name - key name of the object you want to find.
  */
-struct cYAML *cYAML_get_object_item(struct cYAML *parent,
-				    const char *name);
+struct cYAML *cYAML_get_object_item(struct cYAML *parent, const char *name);
+
+/*
+ * cYAML_get_object_child
+ *   Returns the cYAML object which key corresponds to the name passed in.
+ *   This function searches only among the children of the given parent.
+ *
+ *   parent - is the parent object on which you want to conduct the search
+ *   name - key name of the object you want to find.
+ */
+struct cYAML *cYAML_get_object_child(struct cYAML *parent, const char *name);
 
 /*
  * cYAML_get_next_seq_item
@@ -152,8 +161,8 @@ bool cYAML_is_sequence(struct cYAML *node);
 
 /*
  * cYAML_find_object
- *   Returns the cYAML object which key correspods to the name passed in
- *   this function searches the entire tree.
+ *   Returns the cYAML object which key corresponds to the name passed in.
+ *   This function searches the entire tree.
  *
  *   root - is the root of the tree on which you want to conduct the search
  *   name - key name of the object you want to find.

lnet/utils/lnetconfig/liblnd.h
@@ -26,7 +26,7 @@ lustre_ni_show_tunables(struct cYAML *lnd_tunables,
 			__u32 net_type,
 			struct lnet_lnd_tunables *lnd, bool backup);
 
-void
+bool
 lustre_yaml_extract_lnd_tunables(struct cYAML *tree,
 				 __u32 net_type,
 				 struct lnet_lnd_tunables *tun);

lnet/utils/lnetconfig/liblnetconfig.c
@@ -5416,7 +5416,7 @@ yaml_extract_cmn_tunables(struct cYAML *tree,
 {
 	struct cYAML *tun, *item;
 
-	tun = cYAML_get_object_item(tree, "tunables");
+	tun = cYAML_get_object_child(tree, "tunables");
 	if (tun != NULL) {
 		item = cYAML_get_object_item(tun, "peer_timeout");
 		if (item != NULL)
@@ -5439,22 +5439,23 @@ yaml_extract_cmn_tunables(struct cYAML *tree,
 	return false;
 }
 
-static bool
+#define FOUND_CMN_TUNABLES 1
+#define FOUND_LND_TUNABLES 2
+static int
 yaml_extract_tunables(struct cYAML *tree,
 		      struct lnet_ioctl_config_lnd_tunables *tunables,
 		      __u32 net_type)
 {
-	bool rc;
+	int tunables_found_mask = 0;
 
-	rc = yaml_extract_cmn_tunables(tree, &tunables->lt_cmn);
+	if (yaml_extract_cmn_tunables(tree, &tunables->lt_cmn))
+		tunables_found_mask |= FOUND_CMN_TUNABLES;
 
-	if (!rc)
-		return rc;
+	if (lustre_yaml_extract_lnd_tunables(tree, net_type,
+					     &tunables->lt_tun))
+		tunables_found_mask |= FOUND_LND_TUNABLES;
 
-	lustre_yaml_extract_lnd_tunables(tree, net_type,
-					 &tunables->lt_tun);
-
-	return rc;
+	return tunables_found_mask;
 }
 
 static void
@@ -5464,7 +5465,7 @@ yaml_extract_cpt(struct cYAML *tree,
 	int rc;
 	struct cYAML *smp;
 
-	smp = cYAML_get_object_item(tree, "CPT");
+	smp = cYAML_get_object_child(tree, "CPT");
 	if (smp != NULL) {
 		rc = cfs_expr_list_parse(smp->cy_valuestring,
 					 strlen(smp->cy_valuestring),
@@ -5498,29 +5499,22 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 				 struct cYAML **err_rc)
 {
 	struct cYAML *net, *intf, *seq_no, *ip2net = NULL, *local_nis = NULL,
-		     *item = NULL;
-	int num_entries = 0, rc;
+		     *local_ni = NULL;
+	int num_entries = 0, rc = 0;
 	struct lnet_dlc_network_descr nw_descr;
-	struct cfs_expr_list *global_cpts = NULL;
-	struct lnet_ioctl_config_lnd_tunables tunables;
-	bool found = false;
+	struct cfs_expr_list *intf_cpts = NULL;
+	struct lnet_ioctl_config_lnd_tunables tunables, net_tunables;
+	int tun_found_mask, net_tun_found_mask;
 
-	memset(&tunables, 0, sizeof(tunables));
+	memset(&net_tunables, 0, sizeof(net_tunables));
 	/* Use LND defaults */
-	tunables.lt_cmn.lct_peer_timeout = -1;
-	tunables.lt_cmn.lct_peer_tx_credits = -1;
-	tunables.lt_cmn.lct_peer_rtr_credits = -1;
-	tunables.lt_cmn.lct_max_tx_credits = -1;
-
-	INIT_LIST_HEAD(&nw_descr.network_on_rule);
-	INIT_LIST_HEAD(&nw_descr.nw_intflist);
+	net_tunables.lt_cmn.lct_peer_timeout = -1;
+	net_tunables.lt_cmn.lct_peer_tx_credits = -1;
+	net_tunables.lt_cmn.lct_peer_rtr_credits = -1;
+	net_tunables.lt_cmn.lct_max_tx_credits = -1;
 
 	ip2net = cYAML_get_object_item(tree, "ip2net");
 	net = cYAML_get_object_item(tree, "net type");
-	if (net)
-		nw_descr.nw_id = libcfs_str2net(net->cy_valuestring);
-	else
-		nw_descr.nw_id = LOLND;
 
 	/*
 	 * if neither net nor ip2nets are present, then we can not
@@ -5536,8 +5530,18 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 	if (!cYAML_is_sequence(local_nis))
 		return LUSTRE_CFG_RC_BAD_PARAM;
 
-	while (cYAML_get_next_seq_item(local_nis, &item) != NULL) {
-		intf = cYAML_get_object_item(item, "interfaces");
+	if (net)
+		nw_descr.nw_id = libcfs_str2net(net->cy_valuestring);
+	else
+		nw_descr.nw_id = LOLND;
+
+	net_tun_found_mask = yaml_extract_tunables(tree, &net_tunables,
+						   LNET_NETTYP(nw_descr.nw_id));
+	seq_no = cYAML_get_object_item(tree, "seq_no");
+	while (cYAML_get_next_seq_item(local_nis, &local_ni) != NULL) {
+		INIT_LIST_HEAD(&nw_descr.network_on_rule);
+		INIT_LIST_HEAD(&nw_descr.nw_intflist);
+		intf = cYAML_get_object_item(local_ni, "interfaces");
 		if (intf == NULL)
 			continue;
 		num_entries = yaml_copy_intf_info(intf, &nw_descr);
@@ -5547,22 +5551,41 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 					err_rc);
 			return LUSTRE_CFG_RC_BAD_PARAM;
 		}
+
+		memset(&tunables, 0, sizeof(tunables));
+		/* Use LND defaults */
+		tunables.lt_cmn.lct_peer_timeout = -1;
+		tunables.lt_cmn.lct_peer_tx_credits = -1;
+		tunables.lt_cmn.lct_peer_rtr_credits = -1;
+		tunables.lt_cmn.lct_max_tx_credits = -1;
+		tun_found_mask = yaml_extract_tunables(local_ni, &tunables,
+						      LNET_NETTYP(nw_descr.nw_id));
+		if (!(tun_found_mask & FOUND_CMN_TUNABLES) &&
+				(net_tun_found_mask & FOUND_CMN_TUNABLES)) {
+			tunables.lt_cmn = net_tunables.lt_cmn;
+			tun_found_mask |= FOUND_CMN_TUNABLES;
+		}
+		if (!(tun_found_mask & FOUND_LND_TUNABLES) &&
+				(net_tun_found_mask & FOUND_LND_TUNABLES)) {
+			tunables.lt_tun = net_tunables.lt_tun;
+			tun_found_mask |= FOUND_LND_TUNABLES;
+		}
+
+		yaml_extract_cpt(local_ni, &intf_cpts);
+
+		rc = lustre_lnet_config_ni(&nw_descr, intf_cpts,
+					   (ip2net) ? ip2net->cy_valuestring : NULL,
+					   (tun_found_mask) ? &tunables : NULL,
+					   (seq_no) ? seq_no->cy_valueint : -1,
+					   err_rc);
+
+		if (intf_cpts != NULL) {
+			cfs_expr_list_free(intf_cpts);
+			intf_cpts = NULL;
+		}
+		if (rc != 0)
+			return rc;
 	}
-
-	found = yaml_extract_tunables(tree, &tunables,
-				      LNET_NETTYP(nw_descr.nw_id));
-	yaml_extract_cpt(tree, &global_cpts);
-	seq_no = cYAML_get_object_item(tree, "seq_no");
-
-	rc = lustre_lnet_config_ni(&nw_descr, global_cpts,
-				   (ip2net) ? ip2net->cy_valuestring : NULL,
-				   (found) ? &tunables : NULL,
-				   (seq_no) ? seq_no->cy_valueint : -1,
-				   err_rc);
-
-	if (global_cpts != NULL)
-		cfs_expr_list_free(global_cpts);
-
 	return rc;
 }
 

lnet/utils/lnetconfig/liblnetconfig_lnd.c
@@ -207,7 +207,7 @@ lustre_ni_show_tunables(struct cYAML *lnd_tunables,
 	return rc;
 }
 
-static void
+static bool
 yaml_extract_o2ib_tun(struct cYAML *tree,
 		      struct lnet_ioctl_config_o2iblnd_tunables *lnd_cfg)
 {
@@ -217,9 +217,9 @@ yaml_extract_o2ib_tun(struct cYAML *tree,
 	struct cYAML *conns_per_peer = NULL, *ntx = NULL;
 	struct cYAML *tos = NULL;
 
-	lndparams = cYAML_get_object_item(tree, "lnd tunables");
+	lndparams = cYAML_get_object_child(tree, "lnd tunables");
 	if (!lndparams)
-		return;
+		return false;
 
 	map_on_demand = cYAML_get_object_item(lndparams, "map_on_demand");
 	lnd_cfg->lnd_map_on_demand =
@@ -252,10 +252,12 @@ yaml_extract_o2ib_tun(struct cYAML *tree,
 	tos = cYAML_get_object_item(lndparams, "tos");
 	lnd_cfg->lnd_tos =
 		(tos) ? tos->cy_valueint : -1;
+
+	return true;
 }
 
 #ifdef HAVE_KFILND
-static void
+static bool
 yaml_extract_kfi_tun(struct cYAML *tree,
 		      struct lnet_ioctl_config_kfilnd_tunables *lnd_cfg)
 {
@@ -265,9 +267,9 @@ yaml_extract_kfi_tun(struct cYAML *tree,
 	struct cYAML *traffic_class = NULL;
 	struct cYAML *lndparams = NULL;
 
-	lndparams = cYAML_get_object_item(tree, "lnd tunables");
+	lndparams = cYAML_get_object_child(tree, "lnd tunables");
 	if (!lndparams)
-		return;
+		return false;
 
 	prov_major_version =
 		cYAML_get_object_item(lndparams, "prov_major_version");
@@ -288,25 +290,29 @@ yaml_extract_kfi_tun(struct cYAML *tree,
 	    strlen(traffic_class->cy_valuestring) < LNET_MAX_STR_LEN)
 		strcpy(&lnd_cfg->lnd_traffic_class_str[0],
 		       traffic_class->cy_valuestring);
+
+	return true;
 }
 #endif
 
-static void
+static bool
 yaml_extract_efa_tun(struct cYAML *tree,
 		     struct lnet_ioctl_config_efalnd_tunables *lnd_cfg)
 {
 	struct cYAML *lndparams = NULL;
 	struct cYAML *nqps = NULL;
 
-	lndparams = cYAML_get_object_item(tree, "lnd tunables");
+	lndparams = cYAML_get_object_child(tree, "lnd tunables");
 	if (!lndparams)
-		return;
+		return false;
 
 	nqps = cYAML_get_object_item(lndparams, "nqps");
 	lnd_cfg->lnd_nqps = (nqps) ? nqps->cy_valueint : 0;
+
+	return true;
 }
 
-static void
+static bool
 yaml_extract_sock_tun(struct cYAML *tree,
 			 struct lnet_ioctl_config_socklnd_tunables *lnd_cfg)
 {
@@ -314,9 +320,9 @@ yaml_extract_sock_tun(struct cYAML *tree,
 	struct cYAML *tos = NULL;
 	struct cYAML *lndparams = NULL;
 
-	lndparams = cYAML_get_object_item(tree, "lnd tunables");
+	lndparams = cYAML_get_object_child(tree, "lnd tunables");
 	if (!lndparams)
-		return;
+		return false;
 
 	conns_per_peer = cYAML_get_object_item(lndparams, "conns_per_peer");
 	lnd_cfg->lnd_conns_per_peer =
@@ -325,26 +331,29 @@ yaml_extract_sock_tun(struct cYAML *tree,
 	tos = cYAML_get_object_item(lndparams, "tos");
 	lnd_cfg->lnd_tos =
 		(tos) ? tos->cy_valueint : -1;
+
+	return true;
 }
 
-void
+bool
 lustre_yaml_extract_lnd_tunables(struct cYAML *tree,
 				 __u32 net_type,
 				 struct lnet_lnd_tunables *tun)
 {
 	if (net_type == O2IBLND)
-		yaml_extract_o2ib_tun(tree,
+		return yaml_extract_o2ib_tun(tree,
 				      &tun->lnd_tun_u.lnd_o2ib);
 	else if (net_type == EFALND)
-		yaml_extract_efa_tun(tree,
-				      &tun->lnd_tun_u.lnd_efa);
+		return yaml_extract_efa_tun(tree,
+					    &tun->lnd_tun_u.lnd_efa);
 	else if (net_type == SOCKLND)
-		yaml_extract_sock_tun(tree,
-				      &tun->lnd_tun_u.lnd_sock);
+		return yaml_extract_sock_tun(tree,
+					     &tun->lnd_tun_u.lnd_sock);
 #ifdef HAVE_KFILND
 	else if (net_type == KFILND)
-		yaml_extract_kfi_tun(tree,
-				     &tun->lnd_tun_u.lnd_kfi);
+		return yaml_extract_kfi_tun(tree,
+					    &tun->lnd_tun_u.lnd_kfi);
 #endif
+	return false;
 }
 

lustre/tests/sanity-lnet.sh
@@ -2484,6 +2484,170 @@ EOF
 }
 run_test 162 "Check ip2nets import failure with non-matching IPv6"
 
+check_tunable_warning() {
+	dmesg | tac | sed '/LNet: Added LNI/q' | \
+		grep -q "All tunables of NIs of a net should be the same"
+}
+
+test_170() {
+	[[ ${NETTYPE} == tcp* ]] || skip "Test written for tcp"
+
+	cleanup_lnet || error "Failed to unload modules before test execution"
+	cleanup_netns || error "Failed to cleanup netns before test execution"
+	setup_fakeif || error "Failed to add fake IF"
+	reinit_dlc || return $?
+
+	# Disable console ratelimit
+	rlpath="/sys/module/libcfs/parameters/libcfs_console_ratelimit"
+	local rl=$(cat $rlpath)
+	echo 0 > $rlpath
+
+	local IMPORT="do_lnetctl import --old-api"
+	local expected="$TMP/sanity-lnet-$testnum-expected.yaml"
+	local actual="$TMP/sanity-lnet-$testnum-actual.yaml"
+
+	echo "Different CPTs"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+          CPT: "[0]"
+        - interfaces:
+                0: ${FAKE_IF}
+          CPT: "[1]"
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	cat <<EOF > $expected
+CPT:"[0]"
+CPT:"[1]"
+EOF
+	$LNETCTL export --backup | grep CPT: | tr -d ' ' > $actual
+	diff $expected $actual || error "Bad parsing"
+	reinit_dlc
+
+	echo "lnd_tunables not read if no tunables"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+          lnd tunables:
+                conns_per_peer: 5
+                tos: -1
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	$LNETCTL export --backup | grep -q "conns_per_peer: 5" || error "Bad parsing"
+	reinit_dlc
+
+	echo "lnd_tunables cannot be different"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+          lnd tunables:
+                conns_per_peer: 5
+                tos: -1
+        - interfaces:
+                0: ${FAKE_IF}
+          lnd tunables:
+                conns_per_peer: 3
+                tos: -1
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	$LNETCTL export --backup | grep -q "conns_per_peer: 5" || error "Bad parsing"
+	$LNETCTL export --backup | grep -q "conns_per_peer: 3" || error "Bad parsing"
+	reinit_dlc
+
+	echo "Wrong tunables (lnd_tunables) read from NI if first is empty"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+        - interfaces:
+                0: ${FAKE_IF}
+          tunables:
+                peer_timeout: 181
+                peer_credits: 8
+                peer_buffer_credits: 0
+                credits: 256
+          lnd tunables:
+                conns_per_peer: 3
+                tos: -1
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	# conns_per_peer has default value for first ni and 3 for second
+	$LNETCTL export --backup | grep "conns_per_peer:" | grep -qv 3 || error "Bad parsing"
+	$LNETCTL export --backup | grep -q "conns_per_peer: 3" || error "Bad parsing"
+	# Previous ni got set tunables to default value
+	(( $($LNETCTL export --backup | grep -c "peer_timeout: 181") == 0 )) || \
+		error "Bad parsing"
+	check_tunable_warning || error "Missing warning"
+	reinit_dlc
+
+	echo "Tunables can be set directly in the net"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+        - interfaces:
+                0: ${FAKE_IF}
+      tunables:
+            peer_timeout: 182
+            peer_credits: 8
+            peer_buffer_credits: 0
+            credits: 256
+      lnd tunables:
+            conns_per_peer: 3
+            tos: -1
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	(( $($LNETCTL export --backup | grep -c "peer_timeout: 182") == 2 )) ||
+		error "Bad parsing"
+	(( $($LNETCTL export --backup | grep -c "conns_per_peer: 3") == 2 )) ||
+		error "Bad parsing"
+	reinit_dlc
+
+	echo "Show warning if we try to set different tunables (not lnd_tunables) for NIs of a same net"
+	cat <<EOF > $expected
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+        - interfaces:
+                0: ${INTERFACES[0]}
+          tunables:
+                peer_timeout: 182
+                peer_credits: 8
+                peer_buffer_credits: 0
+                credits: 256
+        - interfaces:
+                0: ${FAKE_IF}
+          tunables:
+                peer_timeout: 183
+                peer_credits: 8
+                peer_buffer_credits: 0
+                credits: 256
+EOF
+	$IMPORT <  ${expected} || error "Import failed $?"
+	# Tunables for the two nis will be the same, a warning will be emitted
+	(( $($LNETCTL export --backup | grep -c "peer_timeout: 182") == 2 )) ||
+	check_tunable_warning || error "Missing warning"
+
+	# Restore console_ratelimit
+	echo $rl > $rlpath
+
+	cleanup_lnet
+}
+run_test 170 "Check CPTs and tunables parsing when importing"
+
 test_199() {
 	[[ ${NETTYPE} == tcp* || ${NETTYPE} == o2ib* ]] ||
 		skip "Need tcp or o2ib NETTYPE"

commit 20c2a77d69d3dfe6e2aec01fbf0bf3b6b3f1c423

LU-20245 lnetctl: Fix buffer overflow in import --old-api
The fgets() loop that slurps the import YAML into a heap buffer
advanced 'buf' before recomputing strlen(), so the subsequent
'len -= strlen(buf)' always subtracted zero. 'len' therefore stayed
equal to the original allocation while 'buf' walked forward,
causing fgets() to be called with a length larger than the
remaining writable space. Newer glibc fortify (using
__builtin_dynamic_object_size on the malloc'd region) detects the
mismatch and aborts with "*** buffer overflow detected ***",
core-dumping lnetctl on the first multi-line import. This is hit
immediately by sanity-lnet test_170.

Capture strlen(buf) into a local before advancing 'buf' so 'len'
shrinks correctly, and guard the loop against len reaching zero.

Test-Parameters: trivial clientdistro=el10.1 serverdistro=el10.1 testlist=sanity-lnet

Fixes: 18bc6c4a5 ("LU-20000 lnetctl: bad CPTs/tunables during import")
Signed-off-by: Chris Horn <chorn@ddn.com>
Change-Id: Ib1cbe85724435eefe05a9c1c3b81280c3d3f0227
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65826
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/utils/lnetctl.c
@@ -5636,7 +5636,7 @@ old_api:
 		goto err;
 	}
 
-	yaml_blk = buf = malloc(st.st_size);
+	yaml_blk = buf = malloc(st.st_size + 1);
 	if (!yaml_blk) {
 		rc = -ENOMEM;
 		snprintf(err_str, sizeof(err_str),
@@ -5647,10 +5647,11 @@ old_api:
 				  &err_rc);
 		goto err;
 	}
-	len = st.st_size;
+	len = st.st_size + 1;
 
-	while (fgets(buf, len, input) != NULL) {
+	while (len > 1 && fgets(buf, len, input) != NULL) {
 		char *seq = strstr(buf, "-     ");
+		size_t n;
 
 		if (seq) {
 			int skip;
@@ -5666,8 +5667,9 @@ old_api:
 			 */
 			release = false;
 		}
-		buf += strlen(buf);
-		len -= strlen(buf);
+		n = strlen(buf);
+		buf += n;
+		len -= n;
 	}
 
 	switch (cmd) {