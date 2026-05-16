lustre/lustre-release

commit 169738e30a7e0b57f27a517d78d2c928b3bb0f5c

LU-4684 migrate: migrate striped directory
Migrate striped directory in below steps:
1. create target object if needed: if source is directory, a
   target object is always created, otherwise if source is
   already located on the target MDT, or source still has
   link on source MDT, then skip creating.
	a) if source is directory, detach source stripes and
	   attach them to target.
	b) migrate source xattrs to target.
	c) if source is regular file, update PFID to target
	   fid.
	d) update fid to target for all links of source
2. update namespace
	a) migrate dirent from source parent to target parent.
	b) update linkea parent fid to target parent.
	c) destroy source object.

This implementation improves following fields:
1. all involved objects are locked to avoid race.
2. directory migration doesn't migrate its dir entries, instead
   it's done in each sub file migration, this avoids timeout in
   migrating dir entries for large directory, and also avoids
   touching dir entries without lock.
3. file/dir is migrated in one transaction, so migrate recovery
   is the same as others.
4. migrating directory can be accessed (modifiable) like normal
   directory.
5. if migration of sub files under a directory fails, user can
   redo migrate to finish migration of this directory.

Disable migrate in racer.sh.

Signed-off-by: Lai Siyao <lai.siyao@whamcloud.com>
Change-Id: I7906e50a0bf55375eafdf2cf5500979dd2d94d48
Reviewed-on: https://review.whamcloud.com/31427
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_lmv.h
@@ -46,6 +46,8 @@ struct lmv_stripe_md {
 	__u32	lsm_md_master_mdt_index;
 	__u32	lsm_md_hash_type;
 	__u32	lsm_md_layout_version;
+	__u32	lsm_md_migrate_offset;
+	__u32	lsm_md_migrate_hash;
 	__u32	lsm_md_default_count;
 	__u32	lsm_md_default_index;
 	char	lsm_md_pool_name[LOV_MAXPOOLNAME + 1];
@@ -64,6 +66,10 @@ lsm_md_eq(const struct lmv_stripe_md *lsm1, const struct lmv_stripe_md *lsm2)
 	    lsm1->lsm_md_hash_type != lsm2->lsm_md_hash_type ||
 	    lsm1->lsm_md_layout_version !=
 				lsm2->lsm_md_layout_version ||
+	    lsm1->lsm_md_migrate_offset !=
+				lsm2->lsm_md_migrate_offset ||
+	    lsm1->lsm_md_migrate_hash !=
+				lsm2->lsm_md_migrate_hash ||
 	    strcmp(lsm1->lsm_md_pool_name,
 		      lsm2->lsm_md_pool_name) != 0)
 		return false;
@@ -141,18 +147,14 @@ static inline int lmv_name_to_stripe_index(__u32 lmv_hash_type,
 					   unsigned int stripe_count,
 					   const char *name, int namelen)
 {
-	int	idx;
-	__u32	hash_type = lmv_hash_type & LMV_HASH_TYPE_MASK;
+	int idx;
 
 	LASSERT(namelen > 0);
+
 	if (stripe_count <= 1)
 		return 0;
 
-	/* for migrating object, always start from 0 stripe */
-	if (lmv_hash_type & LMV_HASH_FLAG_MIGRATION)
-		return 0;
-
-	switch (hash_type) {
+	switch (lmv_hash_type & LMV_HASH_TYPE_MASK) {
 	case LMV_HASH_TYPE_ALL_CHARS:
 		idx = lmv_hash_all_chars(stripe_count, name, namelen);
 		break;
@@ -164,8 +166,8 @@ static inline int lmv_name_to_stripe_index(__u32 lmv_hash_type,
 		break;
 	}
 
-	CDEBUG(D_INFO, "name %.*s hash_type %d idx %d\n", namelen, name,
-	       hash_type, idx);
+	CDEBUG(D_INFO, "name %.*s hash_type %#x idx %d/%u\n", namelen, name,
+	       lmv_hash_type, idx, stripe_count);
 
 	return idx;
 }

lustre/include/lu_object.h
@@ -1265,6 +1265,26 @@ struct lu_name {
         int            ln_namelen;
 };
 
+static inline bool name_is_dot_or_dotdot(const char *name, int namelen)
+{
+	return name[0] == '.' &&
+	       (namelen == 1 || (namelen == 2 && name[1] == '.'));
+}
+
+static inline bool lu_name_is_dot_or_dotdot(const struct lu_name *lname)
+{
+	return name_is_dot_or_dotdot(lname->ln_name, lname->ln_namelen);
+}
+
+static inline bool lu_name_is_valid_len(const char *name, size_t name_len)
+{
+	return name != NULL &&
+	       name_len > 0 &&
+	       name_len < INT_MAX &&
+	       strlen(name) == name_len &&
+	       memchr(name, '/', name_len) == NULL;
+}
+
 /**
  * Validate names (path components)
  *
@@ -1276,12 +1296,7 @@ struct lu_name {
  */
 static inline bool lu_name_is_valid_2(const char *name, size_t name_len)
 {
-	return name != NULL &&
-	       name_len > 0 &&
-	       name_len < INT_MAX &&
-	       name[name_len] == '\0' &&
-	       strlen(name) == name_len &&
-	       memchr(name, '/', name_len) == NULL;
+	return lu_name_is_valid_len(name, name_len) && name[name_len] == '\0';
 }
 
 static inline bool lu_name_is_valid(const struct lu_name *ln)

lustre/include/md_object.h
@@ -138,15 +138,15 @@ struct md_attr {
 
 /** Additional parameters for create */
 struct md_op_spec {
-        union {
-                /** symlink target */
-                const char               *sp_symname;
-                /** eadata for regular files */
-                struct md_spec_reg {
-                        const void *eadata;
-                        int  eadatalen;
-                } sp_ea;
-        } u;
+	union {
+		/** symlink target */
+		const char *sp_symname;
+		/** eadata for regular files */
+		struct md_spec_reg {
+			void *eadata;
+			int  eadatalen;
+		} sp_ea;
+	} u;
 
 	/** Create flag from client: such as MDS_OPEN_CREAT, and others. */
 	__u64      sp_cr_flags;
@@ -163,10 +163,10 @@ struct md_op_spec {
 		     sp_permitted:1, /* do not check permission */
 		     sp_migrate_close:1; /* close the file during migrate */
 	/** Current lock mode for parent dir where create is performing. */
-        mdl_mode_t sp_cr_mode;
+	mdl_mode_t sp_cr_mode;
 
-        /** to create directory */
-        const struct dt_index_features *sp_feat;
+	/** to create directory */
+	const struct dt_index_features *sp_feat;
 };
 
 enum md_layout_opc {
@@ -317,7 +317,8 @@ struct md_dir_operations {
 
 	int (*mdo_migrate)(const struct lu_env *env, struct md_object *pobj,
 			   struct md_object *sobj, const struct lu_name *lname,
-			   struct md_object *tobj, struct md_attr *ma);
+			   struct md_object *tobj, struct md_op_spec *spec,
+			   struct md_attr *ma);
 };
 
 struct md_device_operations {
@@ -614,10 +615,12 @@ static inline int mdo_migrate(const struct lu_env *env,
 			     struct md_object *sobj,
 			     const struct lu_name *lname,
 			     struct md_object *tobj,
+			     struct md_op_spec *spec,
 			     struct md_attr *ma)
 {
 	LASSERT(pobj->mo_dir_ops->mdo_migrate);
-	return pobj->mo_dir_ops->mdo_migrate(env, pobj, sobj, lname, tobj, ma);
+	return pobj->mo_dir_ops->mdo_migrate(env, pobj, sobj, lname, tobj, spec,
+					     ma);
 }
 
 static inline int mdo_is_subdir(const struct lu_env *env,

lustre/include/obd_support.h
@@ -617,10 +617,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_INVALIDATE_UPDATE	0x1705
 
 /* MIGRATE */
-#define OBD_FAIL_MIGRATE_NET_REP		0x1800
 #define OBD_FAIL_MIGRATE_ENTRIES		0x1801
-#define OBD_FAIL_MIGRATE_LINKEA			0x1802
-#define OBD_FAIL_MIGRATE_DELAY			0x1803
 
 /* LMV */
 #define OBD_FAIL_UNKNOWN_LMV_STRIPE		0x1901

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -880,7 +880,8 @@ struct ptlrpc_body_v2 {
 
 #define MDT_CONNECT_SUPPORTED2 (OBD_CONNECT2_FILE_SECCTX | OBD_CONNECT2_FLR | \
                                 OBD_CONNECT2_SUM_STATFS | \
-				OBD_CONNECT2_LOCK_CONVERT)
+				OBD_CONNECT2_LOCK_CONVERT | \
+				OBD_CONNECT2_DIR_MIGRATE)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \
@@ -2161,9 +2162,16 @@ struct lmv_mds_md_v1 {
 					 * used for now. Higher 16 bits will
 					 * be used to mark the object status,
 					 * for example migrating or dead. */
-	__u32 lmv_layout_version;	/* Used for directory restriping */
-	__u32 lmv_padding1;
-	__u64 lmv_padding2;
+	__u32 lmv_layout_version;	/* increased each time layout changed,
+					 * by directory migration, restripe
+					 * and LFSCK. */
+	__u32 lmv_migrate_offset;	/* once this is set, it means this
+					 * directory is been migrated, stripes
+					 * before this offset belong to target,
+					 * from this to source. */
+	__u32 lmv_migrate_hash;		/* hash type of source stripes of
+					 * migrating directory */
+	__u32 lmv_padding2;
 	__u64 lmv_padding3;
 	char lmv_pool_name[LOV_MAXPOOLNAME + 1];	/* pool name */
 	struct lu_fid lmv_stripe_fids[0];	/* FIDs for each stripe */

lustre/lfsck/lfsck_internal.h
@@ -1095,12 +1095,6 @@ extern const char *lfsck_flags_names[];
 extern const char *lfsck_param_names[];
 extern struct lu_context_key lfsck_thread_key;
 
-static inline bool name_is_dot_or_dotdot(const char *name, int namelen)
-{
-	return name[0] == '.' &&
-	       (namelen == 1 || (namelen == 2 && name[1] == '.'));
-}
-
 static inline struct dt_device *lfsck_obj2dev(struct dt_object *obj)
 {
 	return container_of0(obj->do_lu.lo_dev, struct dt_device, dd_lu_dev);
@@ -1504,6 +1498,8 @@ static inline void lfsck_lmv_header_le_to_cpu(struct lmv_mds_md_v1 *dst,
 	dst->lmv_master_mdt_index = le32_to_cpu(src->lmv_master_mdt_index);
 	dst->lmv_hash_type = le32_to_cpu(src->lmv_hash_type);
 	dst->lmv_layout_version = le32_to_cpu(src->lmv_layout_version);
+	dst->lmv_migrate_offset = le32_to_cpu(src->lmv_migrate_offset);
+	dst->lmv_migrate_hash = le32_to_cpu(src->lmv_migrate_hash);
 }
 
 static inline void lfsck_lmv_header_cpu_to_le(struct lmv_mds_md_v1 *dst,
@@ -1514,6 +1510,8 @@ static inline void lfsck_lmv_header_cpu_to_le(struct lmv_mds_md_v1 *dst,
 	dst->lmv_master_mdt_index = cpu_to_le32(src->lmv_master_mdt_index);
 	dst->lmv_hash_type = cpu_to_le32(src->lmv_hash_type);
 	dst->lmv_layout_version = cpu_to_le32(src->lmv_layout_version);
+	dst->lmv_migrate_offset = cpu_to_le32(src->lmv_migrate_offset);
+	dst->lmv_migrate_hash = cpu_to_le32(src->lmv_migrate_hash);
 }
 
 static inline struct lfsck_assistant_object *

lustre/lfsck/lfsck_striped_dir.c
@@ -1419,6 +1419,7 @@ static int lfsck_namespace_set_lmv_master(const struct lu_env *env,
 
 	lmv3->lmv_magic = LMV_MAGIC;
 	lmv3->lmv_master_mdt_index = pidx;
+	lmv3->lmv_layout_version++;
 
 	if (flags & LEF_SET_LMV_ALL) {
 		rc = lfsck_allow_regenerate_master_lmv(env, com, obj,

lustre/llite/file.c
@@ -4173,6 +4173,17 @@ int ll_migrate(struct inode *parent, struct file *file, struct lmv_user_md *lum,
 	if (!child_inode)
 		RETURN(-ENOENT);
 
+	if (!(exp_connect_flags2(ll_i2sbi(parent)->ll_md_exp) &
+	      OBD_CONNECT2_DIR_MIGRATE)) {
+		if (le32_to_cpu(lum->lum_stripe_count) > 1 ||
+		    ll_i2info(child_inode)->lli_lsm_md) {
+			CERROR("%s: MDT doesn't support stripe directory "
+			       "migration!\n",
+			       ll_get_fsname(parent->i_sb, NULL, 0));
+			GOTO(out_iput, rc = -EOPNOTSUPP);
+		}
+	}
+
 	/*
 	 * lfs migrate command needs to be blocked on the client
 	 * by checking the migrate FID against the FID of the

commit a878ee6f57b8e001510378a6aa48c935782fdbf1

LU-16518 lmv: remove unused lmv1_le_to_cpu function
This function is called anywhere, we can remove it.

Fixes: 169738e30a7e ("LU-4684 migrate: migrate striped directory")
Fixes: 5f3e926ac9ff ("LU-3529 lod: create striped directory")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: If58622dce4321ad536887511b43a8f30ba62e95b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65598
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: wangdi <ddiwang@google.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_lmv.h
@@ -198,36 +198,6 @@ void lmv_stripe_object_put(struct lmv_stripe_object **lsm_obj);
 struct lmv_stripe_object *
 	lmv_stripe_object_get(struct lmv_stripe_object *lsm_obj);
 
-static inline void lmv1_le_to_cpu(struct lmv_mds_md_v1 *lmv_dst,
-				  const struct lmv_mds_md_v1 *lmv_src)
-{
-	__u32 i;
-
-	lmv_dst->lmv_magic = le32_to_cpu(lmv_src->lmv_magic);
-	lmv_dst->lmv_stripe_count = le32_to_cpu(lmv_src->lmv_stripe_count);
-	lmv_dst->lmv_master_mdt_index =
-				le32_to_cpu(lmv_src->lmv_master_mdt_index);
-	lmv_dst->lmv_hash_type = le32_to_cpu(lmv_src->lmv_hash_type);
-	lmv_dst->lmv_layout_version = le32_to_cpu(lmv_src->lmv_layout_version);
-	if (lmv_src->lmv_stripe_count > LMV_MAX_STRIPE_COUNT)
-		return;
-	for (i = 0; i < lmv_src->lmv_stripe_count; i++)
-		fid_le_to_cpu(&lmv_dst->lmv_stripe_fids[i],
-			      &lmv_src->lmv_stripe_fids[i]);
-}
-
-static inline void lmv_le_to_cpu(union lmv_mds_md *lmv_dst,
-				 const union lmv_mds_md *lmv_src)
-{
-	switch (le32_to_cpu(lmv_src->lmv_magic)) {
-	case LMV_MAGIC_V1:
-		lmv1_le_to_cpu(&lmv_dst->lmv_md_v1, &lmv_src->lmv_md_v1);
-		break;
-	default:
-		break;
-	}
-}
-
 /* This hash is only for testing purpose */
 static inline unsigned int
 lmv_hash_all_chars(unsigned int count, const char *name, int namelen)