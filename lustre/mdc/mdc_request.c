lustre/lustre-release

commit 529ea457da301b80dd850d7910a20bca956322ea

LU-5896 mdt: allow clients with old mdc_swap_layouts
The size of struct mdc_swap_layouts was increased to improve HSM
layout swap handling, but this broke 'lfs migrate' interoperability
with old clients and new servers due to request buffer size checks:

    lustre_msg_buf_v2() buffer[2] size 8 too small required 24, opc=61
    __req_capsule_get() Wrong buffer for field 'swap_layouts' (2 of 6)
        in format 'MDS_SWAP_LAYOUTS', 8 vs. 24 (client)

Add a new RMF_F_MINIMUM_SIZE RPC buffer check, to ensure the buffer is
at least the requested size, but can be larger.

Add compatibility for smaller mdc_swap_layouts_217 until 3.4.53.

Fixes: 63caacb042 ("LU-5896 mdt: allow release after blocking migrations")
Test-Parameters: testlist=sanity clientversion=2.17 env=ONLY=56
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I26d0d321dd870b4bdd688410a3425dd620300c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64757
Reviewed-by: Etienne AUJAMES <eaujames@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

lustre/mdc/mdc_request.c
@@ -2276,6 +2276,11 @@ static int mdc_ioc_swap_layouts(struct obd_export *exp,
 
 	payload = req_capsule_client_get(&req->rq_pill, &RMF_SWAP_LAYOUTS);
 	LASSERT(payload);
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	/* allow client to set msl_dv1/msl_dv2 fields until compat not needed */
+	req_capsule_set_size(&req->rq_pill, &RMF_SWAP_LAYOUTS, RCL_CLIENT,
+			     sizeof(*msl));
+#endif
 
 	*payload = *msl;
 

lustre/include/lustre_swab.h
@@ -114,7 +114,7 @@ void lustre_swab_batch_update_request(struct batch_update_request *bur);
 void lustre_swab_but_update_header(struct but_update_header *buh);
 void lustre_swab_but_update_buffer(struct but_update_buffer *bub);
 void lustre_swab_batch_update_reply(struct batch_update_reply *bur);
-void lustre_swab_swap_layouts(struct mdc_swap_layouts *msl);
+int lustre_swab_swap_layouts(struct mdc_swap_layouts *msl, __u32 size);
 void lustre_swab_close_data(struct close_data *data);
 void lustre_swab_close_data_resync_done(struct close_data_resync_done *resync);
 void lustre_swab_close_data_special(struct close_data *cd, enum mds_op_bias b);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -3789,6 +3789,15 @@ struct mdc_swap_layouts {
 	__u64		msl_dv2; /* data version of file 2 */
 };
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+/* extra fields were added in commit v2_17_51-2-g63caacb042.
+ * struct mdc_swap_layouts_217 not needed after 3.4.53.
+ */
+struct mdc_swap_layouts_217 {
+	__u64           msl_flags;
+};
+#endif
+
 #define INLINE_RESYNC_ARRAY_SIZE	15
 struct close_data_resync_done {
 	__u32	resync_count;

lustre/mdt/mdt_handler.c
@@ -6315,8 +6315,7 @@ TGT_MDT_HDL(HAS_BODY | HAS_REPLY, MDS_HSM_ACTION,	mdt_hsm_action),
 TGT_MDT_HDL(HAS_BODY | HAS_REPLY, MDS_HSM_REQUEST,
 							mdt_hsm_request),
 TGT_MDT_HDL(HAS_KEY | HAS_BODY | HAS_REPLY | IS_MUTABLE,
-	    MDS_SWAP_LAYOUTS,
-	    mdt_swap_layouts),
+				MDS_SWAP_LAYOUTS,	mdt_swap_layouts),
 TGT_MDT_HDL(HAS_BODY | HAS_REPLY | IS_MUTABLE, MDS_HSM_DATA_VERSION,
 	    mdt_hsm_data_version),
 TGT_MDT_HDL(IS_MUTABLE,		MDS_RMFID,	mdt_rmfid),

lustre/ptlrpc/layout.c
@@ -940,6 +940,11 @@ enum rmf_flags {
 	 * @rmf_size worth of bytes.
 	 */
 	RMF_F_STRUCT_ARRAY	= BIT(2),
+	/*
+	 * The field's buffer size must be at least @rmf_size bytes,
+	 * and @rmf_swabber/@rmf_swab_len must handle the variable size.
+	 */
+	RMF_F_MINIMUM_SIZE	= BIT(3),
 };
 
 struct req_capsule;
@@ -1326,8 +1331,14 @@ struct req_msg_field RMF_MDS_HSM_REQUEST =
 EXPORT_SYMBOL(RMF_MDS_HSM_REQUEST);
 
 struct req_msg_field RMF_SWAP_LAYOUTS =
-	DEFINE_MSGF("swap_layouts", 0, sizeof(struct  mdc_swap_layouts),
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	DEFINE_MSGFL("swap_layouts", RMF_F_MINIMUM_SIZE,
+		    sizeof(struct mdc_swap_layouts_217),
 		    lustre_swab_swap_layouts, NULL);
+#else
+	DEFINE_MSGFL("swap_layouts", 0, sizeof(struct mdc_swap_layouts),
+		    lustre_swab_swap_layouts, NULL);
+#endif
 EXPORT_SYMBOL(RMF_SWAP_LAYOUTS);
 
 struct req_msg_field RMF_LFSCK_REQUEST =
@@ -2349,15 +2360,19 @@ static void *__req_capsule_get(struct req_capsule *pill,
 	getter = (field->rmf_flags & RMF_F_STRING) ?
 		(typeof(getter))lustre_msg_string : lustre_msg_buf;
 
-	if (field->rmf_flags & (RMF_F_STRUCT_ARRAY|RMF_F_NO_SIZE_CHECK)) {
-		/*
-		 * We've already asserted that field->rmf_size > 0 in
-		 * req_layout_init().
-		 */
+	/* req_layout_init() already asserted that field->rmf_size > 0 */
+	if (field->rmf_flags &
+	    (RMF_F_STRUCT_ARRAY|RMF_F_NO_SIZE_CHECK|RMF_F_MINIMUM_SIZE)) {
 		len = lustre_msg_buflen(msg, offset);
-		if (!(field->rmf_flags & RMF_F_NO_SIZE_CHECK) &&
+		if (field->rmf_flags & RMF_F_STRUCT_ARRAY &&
 		    (len % field->rmf_size) != 0) {
 			CERROR("%s: array field size mismatch %d modulo %u != 0 (%d)\n",
+				field->rmf_name, len, field->rmf_size, loc);
+			return NULL;
+		}
+		if (field->rmf_flags & RMF_F_MINIMUM_SIZE &&
+		    len < field->rmf_size) {
+			CERROR("%s: array field size %d < minimum %u (%d)\n",
 				field->rmf_name, len, field->rmf_size, loc);
 			return NULL;
 		}
@@ -2544,24 +2559,24 @@ void req_capsule_set_size(struct req_capsule *pill,
 			  const struct req_msg_field *field,
 			  enum req_location loc, __u32 size)
 {
+	__u32 rmf_size = (__u32)field->rmf_size;
+
 	LASSERT(loc == RCL_SERVER || loc == RCL_CLIENT);
 
-	if ((size != (__u32)field->rmf_size) &&
-	    (field->rmf_size != -1) &&
-	    !(field->rmf_flags & RMF_F_NO_SIZE_CHECK) &&
-	    (size > 0)) {
-		__u32 rmf_size = (__u32)field->rmf_size;
+	if (size == rmf_size || size == 0 || field->rmf_size == -1)
+		goto out;
+	if (field->rmf_flags & RMF_F_NO_SIZE_CHECK)
+		goto out;
 
-		LASSERTF(!((field->rmf_flags & RMF_F_STRUCT_ARRAY) &&
-			   (size % rmf_size != 0)),
-			 "%s: array field size mismatch %u %% %u != 0 (%d)\n",
-			 field->rmf_name, size, rmf_size, loc);
-		LASSERTF(!(!(field->rmf_flags & RMF_F_STRUCT_ARRAY) &&
-			   size < rmf_size),
-			 "%s: field size mismatch %u != %u (%d)\n",
-			 field->rmf_name, size, rmf_size, loc);
-	}
+	LASSERTF(!(field->rmf_flags & RMF_F_STRUCT_ARRAY) ||
+		  (size % rmf_size) == 0,
+		 "%s: array field size mismatch %u %% %u != 0 (%d)\n",
+		 field->rmf_name, size, rmf_size, loc);
+	LASSERTF((field->rmf_flags & RMF_F_STRUCT_ARRAY) || size >= rmf_size,
+		 "%s: field size mismatch %u != %u (%d)\n",
+		 field->rmf_name, size, rmf_size, loc);
 
+out:
 	pill->rc_area[loc][__req_capsule_offset(pill, field, loc)] = size;
 }
 EXPORT_SYMBOL(req_capsule_set_size);
@@ -2691,8 +2706,10 @@ void req_capsule_extend(struct req_capsule *pill, const struct req_format *fmt)
 	for (i = 0; i < RCL_NR; ++i) {
 		LASSERT(fmt->rf_fields[i].nr >= old->rf_fields[i].nr);
 		for (j = 0; j < old->rf_fields[i].nr - 1; ++j) {
-			const struct req_msg_field *ofield = FMT_FIELD(old, i,
-								       j);
+			const struct req_msg_field *ofield =
+				FMT_FIELD(old, i, j);
+			const struct req_msg_field *nfield =
+				FMT_FIELD(fmt, i, j);
 
 			/* "opaque" fields can be transmogrified */
 			if (ofield->rmf_swabber == NULL &&
@@ -2700,11 +2717,9 @@ void req_capsule_extend(struct req_capsule *pill, const struct req_format *fmt)
 			    (ofield->rmf_size == -1 ||
 			     ofield->rmf_flags == RMF_F_NO_SIZE_CHECK))
 				continue;
-			LASSERT(FMT_FIELD(fmt, i, j) == FMT_FIELD(old, i, j));
+			LASSERT(ofield == nfield);
 		}
-		/*
-		 * Last field in old format can be shorter than in new.
-		 */
+		/* Last field in new format can be larger than in old. */
 		LASSERT(FMT_FIELD(fmt, i, j)->rmf_size >=
 			FMT_FIELD(old, i, j)->rmf_size);
 	}

lustre/ptlrpc/pack_generic.c
@@ -3073,18 +3073,21 @@ void lustre_swab_but_update_buffer(struct but_update_buffer *bub)
 }
 EXPORT_SYMBOL(lustre_swab_but_update_buffer);
 
-void lustre_swab_swap_layouts(struct mdc_swap_layouts *msl)
+int lustre_swab_swap_layouts(struct mdc_swap_layouts *msl, __u32 size)
 {
 	__swab64s(&msl->msl_flags);
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
 	/* Only swab dv1/dv2 fields if WITH_DV12 flag is sent by client, to
-	 * avoid access beyond the end of old struct mdc_swap_layouts.
+	 * avoid access beyond the end of struct mdc_swap_layouts_217.
 	 */
-	if (!(msl->msl_flags & SWAP_LAYOUTS_WITH_DV12))
-		return;
+	if (!(msl->msl_flags & SWAP_LAYOUTS_WITH_DV12) ||
+	    size < offsetof(struct mdc_swap_layouts, msl_dv2) + sizeof(msl->msl_dv2))
+		return 0;
  #endif
 	__swab64s(&msl->msl_dv1);
 	__swab64s(&msl->msl_dv2);
+
+	return 0;
 }
 
 void lustre_swab_close_data(struct close_data *cd)

commit dac2ec9d6912233ae379bbf907948dc330f60aa8

LU-20117 mdc: fix RMF_SWAP_LAYOUTS packing
Calling req_capsule_set_size() after ptlrpc_request_pack() does not
update buffer offsets. So writing "struct mdc_swap_layouts" in
mdc_ioc_swap_layouts() can overwrite data set by mdc_prep_elc_req().

Test-Parameters: testlist=sanity
Test-Parameters: testlist=sanity env=ONLY=27
Test-Parameters: testlist=sanity env=ONLY=27
Test-Parameters: testlist=sanity env=ONLY=27X,ONLY_REPEAT=50
Test-Parameters: testlist=sanity clientversion=2.17 env=ONLY="27 56"
Test-Parameters: testlist=sanity serverversion=2.17 env=ONLY="27X 56"
Fixes: 529ea457da30 ("LU-5896 mdt: allow clients with old mdc_swap_layouts")
Signed-off-by: Etienne AUJAMES <eaujames@ddn.com>
Change-Id: Ic9e908c043f0c45900c75907d78acd4f4ec1c664
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65343
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: George Zhao <georgezhaojobs@gmail.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/mdc/mdc_request.c
@@ -2266,6 +2266,11 @@ static int mdc_ioc_swap_layouts(struct obd_export *exp,
 		RETURN(-ENOMEM);
 	}
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
+	/* allow client to set msl_dv1/msl_dv2 fields until compat not needed */
+	req_capsule_set_size(&req->rq_pill, &RMF_SWAP_LAYOUTS, RCL_CLIENT,
+			     sizeof(*msl));
+#endif
 	rc = mdc_prep_elc_req(exp, req, MDS_SWAP_LAYOUTS, &cancels, count);
 	if (rc) {
 		ptlrpc_request_free(req);
@@ -2276,12 +2281,6 @@ static int mdc_ioc_swap_layouts(struct obd_export *exp,
 
 	payload = req_capsule_client_get(&req->rq_pill, &RMF_SWAP_LAYOUTS);
 	LASSERT(payload);
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 4, 53, 0)
-	/* allow client to set msl_dv1/msl_dv2 fields until compat not needed */
-	req_capsule_set_size(&req->rq_pill, &RMF_SWAP_LAYOUTS, RCL_CLIENT,
-			     sizeof(*msl));
-#endif
-
 	*payload = *msl;
 
 	ptlrpc_request_set_replen(req);