lustre/lustre-release

commit fc986328690ed104ab719bbd6761cf71c8ae2b11

LU-20227 lod,llite: fix div-by-zero on uninitialized os_bsize
When a snapshot MDT is mounted in DNE configuration, lod_foreach_mdt
iterates sub-MDT OSPs. If a sub-MDT OSP returns rc=0 with uninitialized
opd_statfs cache (os_bsize=0), lod_statfs_sum() silently shifts
sfs->os_bsize down to zero, which eventually causes a divide-by-zero
kernel panic in ll_statfs_project() when project quota is active.

Server side (lod_dev.c):
Add the same os_bsize==0 guard in lod_foreach_mdt that already exists
in lod_foreach_ost to prevent this silent corruption.

Client side (llite_lib.c):
Add a defensive f_bsize==0 check in ll_statfs_project() before the
division, falling back to 4096, to prevent kernel
panic even if a corrupted os_bsize=0 reaches the client.

Change-Id: Ibbd60320b19225e5f4058ebe4377b412d7d60be8
Signed-off-by: Sunghwan Kim <shkim3220@gluesys.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65707
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/llite/llite_lib.c
@@ -2715,6 +2715,17 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 		GOTO(out, ret);
 	}
 
+	/*
+	 * f_bsize=0: server lod_foreach_mdt passed uninitialized
+	 * sub-MDT opd_statfs (os_bsize=0) into lod_statfs_sum(),
+	 * corrupting os_bsize. Fall back to 4096.
+	 */
+	if (unlikely(sfs->f_bsize == 0)) {
+		sfs->f_bsize = 4096;
+		CDEBUG(D_SUPER, "%s: f_bsize=0 sub-MDT opd_statfs uninitialized, corrected to %u\n",
+			ll_i2sbi(inode)->ll_fsname, sfs->f_bsize);
+	}
+
 	limit = ((qctl.qc_dqblk.dqb_bsoftlimit ?
 		 qctl.qc_dqblk.dqb_bsoftlimit :
 		 qctl.qc_dqblk.dqb_bhardlimit) * 1024) / sfs->f_bsize;

lustre/lod/lod_dev.c
@@ -1607,7 +1607,8 @@ static int lod_statfs(const struct lu_env *env, struct dt_device *dev,
 	lod_foreach_mdt(lod, tgt) {
 		rc = dt_statfs(env, tgt->ltd_tgt, &ost_sfs);
 		/* ignore errors */
-		if (rc)
+		/* skip uninitialized sub-MDT, prevent divide-by-zero */
+		if (rc || ost_sfs.os_bsize == 0)
 			continue;
 		sfs->os_files += ost_sfs.os_files;
 		sfs->os_ffree += ost_sfs.os_ffree;

commit bb13ea466cb8f1b055cf932710eb255e1b6d74f4

LU-20314 llite: f_bsize is a long, use %lu
Cleanup the CDEBUG to use the correct string format specifier

Test-Parameters: trivial
Fixes: fc98632869 ("LU-20227 lod,llite: fix div-by-zero on uninitialized os_bsize")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Idd089af566374d9dd600c014f17c2f6007743e8e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66259
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Sunghwan Kim <shkim3220@gluesys.com>

lustre/llite/llite_lib.c
@@ -2722,7 +2722,7 @@ static int ll_statfs_project(struct inode *inode, struct kstatfs *sfs)
 	 */
 	if (unlikely(sfs->f_bsize == 0)) {
 		sfs->f_bsize = 4096;
-		CDEBUG(D_SUPER, "%s: f_bsize=0 sub-MDT opd_statfs uninitialized, corrected to %u\n",
+		CDEBUG(D_SUPER, "%s: f_bsize=0 sub-MDT opd_statfs uninitialized, corrected to %lu\n",
 			ll_i2sbi(inode)->ll_fsname, sfs->f_bsize);
 	}
 