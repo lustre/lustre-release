lustre/lustre-release

commit 5b3747294a4bf4a12ad494d2c72b72512794ba90

LU-13502 lnet: Add param to control response tracking
Add lnet_response_tracking parameter which will be used to control
the behavior of LNet response tracking.

Test-Parameters: trivial
HPE-bug-id: LUS-8827
Signed-off-by: Chris Horn <hornc@cray.com>
Change-Id: I9c5be488673bbaa3c3cb983fe099d2203c1d9fa7
Reviewed-on: https://review.whamcloud.com/38449
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -222,6 +222,24 @@ module_param_call(lnet_retry_count, retry_count_set, param_get_int,
 MODULE_PARM_DESC(lnet_retry_count,
 		 "Maximum number of times to retry transmitting a message");
 
+unsigned int lnet_response_tracking = 3;
+static int response_tracking_set(const char *val, cfs_kernel_param_arg_t *kp);
+
+#ifdef HAVE_KERNEL_PARAM_OPS
+static struct kernel_param_ops param_ops_response_tracking = {
+	.set = response_tracking_set,
+	.get = param_get_int,
+};
+
+#define param_check_response_tracking(name, p)  \
+	__param_check(name, p, int)
+module_param(lnet_response_tracking, response_tracking, 0644);
+#else
+module_param_call(lnet_response_tracking, response_tracking_set, param_get_int,
+		  &lnet_response_tracking, 0644);
+#endif
+MODULE_PARM_DESC(lnet_response_tracking,
+		 "(0|1|2|3) LNet Internal Only|GET Reply only|PUT ACK only|Full Tracking (default)");
 
 #define LNET_LND_TIMEOUT_DEFAULT ((LNET_TRANSACTION_TIMEOUT_HEALTH_DEFAULT - 1) / \
 				  (LNET_RETRY_COUNT_HEALTH_DEFAULT + 1))
@@ -529,6 +547,29 @@ intf_max_set(const char *val, cfs_kernel_param_arg_t *kp)
 	}
 
 	*(int *)kp->arg = value;
+
+	return 0;
+}
+
+static int
+response_tracking_set(const char *val, cfs_kernel_param_arg_t *kp)
+{
+	int rc;
+	unsigned long new_value;
+
+	rc = kstrtoul(val, 0, &new_value);
+	if (rc) {
+		CERROR("Invalid value for 'lnet_response_tracking'\n");
+		return -EINVAL;
+	}
+
+	if (new_value < 0 || new_value > 3) {
+		CWARN("Invalid value (%lu) for 'lnet_response_tracking'\n",
+		      new_value);
+		return -EINVAL;
+	}
+
+	lnet_response_tracking = new_value;
 
 	return 0;
 }

lnet/include/lnet/lib-lnet.h
@@ -457,6 +457,7 @@ struct lnet_net *lnet_get_net_locked(__u32 net_id);
 int lnet_lib_init(void);
 void lnet_lib_exit(void);
 
+extern unsigned int lnet_response_tracking;
 extern unsigned lnet_transaction_timeout;
 extern unsigned lnet_retry_count;
 extern unsigned int lnet_lnd_timeout;

commit 7aef6586d88c5bc77b3bbfbc7e820abe3b357e98

LU-17000 lnet: Fix Unsigned compare in response_tracking_set
Fix unsigned compare against 0 in response_tracking_set.
This would have led to compression always being false
since less than zero is compares with unsigned value.

CoverityID: 397767 ("Unsigned compared against 0")
Fixes: 5b3747294 ("LU-13502 lnet: Add param to control response tracking")
Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I4b2c189914c4aa4b4fe97c545e214fe9228b9a0b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62728
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Chris Horn <chorn@ddn.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/lnet/api-ni.c
@@ -533,8 +533,8 @@ intf_max_set(const char *val, const struct kernel_param *kp)
 static int
 response_tracking_set(const char *val, const struct kernel_param *kp)
 {
-	int rc;
 	unsigned long new_value;
+	int rc;
 
 	rc = kstrtoul(val, 0, &new_value);
 	if (rc) {
@@ -542,7 +542,7 @@ response_tracking_set(const char *val, const struct kernel_param *kp)
 		return -EINVAL;
 	}
 
-	if (new_value < 0 || new_value > 3) {
+	if (new_value > 3) {
 		CWARN("Invalid value (%lu) for 'lnet_response_tracking'\n",
 		      new_value);
 		return -EINVAL;