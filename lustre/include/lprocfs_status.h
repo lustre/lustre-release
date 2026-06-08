lustre/lustre-release

commit 5803284ac3a5d477df9afffe48ff35f08d67da1a

LU-11850 obd: use netlink to get lustre stats
This adds the ability to collect performance metrics from lustre
in another way then from proc / debugfs files. The move to debugfs
has limited the scope of access to only root. Additionally there
is an expensive cost accessing many virtual file system files to
collect that data. Netlink will scale much better in this case as
well as offer a much more flexiable API.

The new ldebugfs_stats_alloc() replaces lprocfs_stats_alloc() and
registers the stats to be accessible BOTH throught debugfs AND
through Netlink. The new global "lstats_list" contains a list of
all registered sets of statistics, so it mirrors a subset of
debugfs. Netlink access can report on any statistics registered in
lstats_list.

Test-Parameters: trivial
Change-Id: If2d662baa62348fe6f0dd5c8d77344650c2a27d8
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/34256
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lprocfs_status.h
@@ -179,8 +179,14 @@ enum lprocfs_fields_flags {
 };
 
 struct lprocfs_stats {
+	/* source for the stats */
+	char				*ls_source;
+	/* index in Xarray */
+	unsigned int			ls_index;
 	/* # of counters */
 	unsigned short			ls_num;
+	/* track reference */
+	struct kref			ls_refcount;
 	/* 1 + the biggest cpu # whose ls_percpu slot has been allocated */
 	unsigned short			ls_biggest_alloc_num;
 	enum lprocfs_stats_flags	ls_flags;
@@ -398,9 +404,8 @@ struct brw_stats {
 int lprocfs_init_brw_stats(struct brw_stats *brw_stats);
 void lprocfs_fini_brw_stats(struct brw_stats *brw_stats);
 
-void ldebugfs_register_osd_stats(struct dentry *parent,
-				 struct brw_stats *brw_stats,
-				 struct lprocfs_stats *stats);
+void ldebugfs_register_brw_stats(struct dentry *parent,
+				 struct brw_stats *brw_stats);
 #endif /* HAVE_SERVER_SUPPORT */
 
 #define EXTRA_FIRST_OPC LDLM_GLIMPSE_ENQUEUE
@@ -495,8 +500,12 @@ lprocfs_stats_alloc(unsigned int num, enum lprocfs_stats_flags flags);
 extern void lprocfs_stats_clear(struct lprocfs_stats *stats);
 extern void lprocfs_stats_free(struct lprocfs_stats **stats);
 extern void lprocfs_init_ldlm_stats(struct lprocfs_stats *ldlm_stats);
-extern int lprocfs_alloc_obd_stats(struct obd_device *obd,
-				   unsigned int num_stats);
+struct lprocfs_stats *ldebugfs_stats_alloc(int num, char *name,
+					   struct dentry *entry,
+					   struct kobject *kobj,
+					   enum lprocfs_stats_flags flags);
+extern int ldebugfs_alloc_obd_stats(struct obd_device *obd,
+				    unsigned int num_stats);
 extern int lprocfs_alloc_md_stats(struct obd_device *obd,
 				  unsigned int num_private_stats);
 extern void lprocfs_counter_init(struct lprocfs_stats *stats, int index,
@@ -505,7 +514,7 @@ extern void lprocfs_counter_init(struct lprocfs_stats *stats, int index,
 extern void lprocfs_counter_init_units(struct lprocfs_stats *stats, int index,
 				       enum lprocfs_counter_config config,
 				       const char *name, const char *units);
-extern void lprocfs_free_obd_stats(struct obd_device *obd);
+extern void ldebugfs_free_obd_stats(struct obd_device *obd);
 extern void lprocfs_free_md_stats(struct obd_device *obd);
 struct obd_export;
 struct nid_stat;
@@ -979,20 +988,10 @@ static inline void lprocfs_init_ldlm_stats(struct lprocfs_stats *ldlm_stats)
 {
 }
 
-static inline int lprocfs_alloc_obd_stats(struct obd_device *obd,
-					  unsigned int num_stats)
-{
-	return 0;
-}
-
 static inline int lprocfs_alloc_md_stats(struct obd_device *obd,
 					 unsigned int num_private_stats)
 {
 	return 0;
-}
-
-static inline void lprocfs_free_obd_stats(struct obd_device *obd)
-{
 }
 
 static inline void lprocfs_free_md_stats(struct obd_device *obd)

lustre/include/lustre_kernelcomm.h
@@ -19,6 +19,9 @@
 #ifndef __LUSTRE_KERNELCOMM_H__
 #define __LUSTRE_KERNELCOMM_H__
 
+#include <linux/generic-radix-tree.h>
+#include <net/genetlink.h>
+#include <net/sock.h>
 /* For declarations shared with userspace */
 #include <uapi/linux/lustre/lustre_kernelcomm.h>
 
@@ -55,6 +58,106 @@ enum lustre_device_attrs {
 };
 
 #define LUSTRE_DEVICE_ATTR_MAX (__LUSTRE_DEVICE_ATTR_MAX_PLUS_ONE - 1)
+
+/**
+ * enum lustre_param_list_attrs	      - General header to list all sources
+ *					supporting an specific query.
+ *
+ * @LUSTRE_PARAM_ATTR_UNSPEC:		unspecified attribute to catch errors
+ *
+ * @LUSTRE_PARAM_ATTR_HDR:		groups params belong to (NLA_NUL_STRING)
+ * @LUSTRE_PARAM_ATTR_SOURCE:		source of the params (NLA_STRING)
+ */
+enum lustre_param_list_attrs {
+	LUSTRE_PARAM_ATTR_UNSPEC = 0,
+
+	LUSTRE_PARAM_ATTR_HDR,
+	LUSTRE_PARAM_ATTR_SOURCE,
+
+	__LUSTRE_PARAM_ATTR_MAX_PLUS_ONE
+};
+
+#define LUSTRE_PARAM_ATTR_MAX (__LUSTRE_PARAM_ATTR_MAX_PLUS_ONE - 1)
+
+/**
+ * enum lustre_stats_attrs	     - Lustre stats netlink attributes used
+ *				       to compose messages for sending or
+ *				       receiving.
+ *
+ * @LUSTRE_STATS_ATTR_UNSPEC:	       unspecified attribute to catch errors
+ * @LUSTRE_STATS_ATTR_PAD:	       padding for 64-bit attributes, ignore
+ *
+ * @LUSTRE_STATS_ATTR_HDR:	       groups stats belong to (NLA_NUL_STRING)
+ * @LUSTRE_STATS_ATTR_SOURCE:	       source of the stats (NLA_STRING)
+ * @LUSTRE_STATS_ATTR_TIMESTAMP:       time of collection in nanoseconds
+ *				       (NLA_S64)
+ * @LUSTRE_STATS_ATTR_START_TIME:      start time of collection (NLA_S64)
+ * @LUSTRE_STATS_ATTR_ELPASE_TIME:     elpase time of collection (NLA_S64)
+ * @LUSTRE_STATS_ATTR_DATASET:	       bookmarks for that stats data
+ *				       (NLA_NESTED)
+ */
+enum lustre_stats_attrs {
+	LUSTRE_STATS_ATTR_UNSPEC = 0,
+	LUSTRE_STATS_ATTR_PAD = LUSTRE_STATS_ATTR_UNSPEC,
+
+	LUSTRE_STATS_ATTR_HDR,
+	LUSTRE_STATS_ATTR_SOURCE,
+	LUSTRE_STATS_ATTR_TIMESTAMP,
+	LUSTRE_STATS_ATTR_START_TIME,
+	LUSTRE_STATS_ATTR_ELAPSE_TIME,
+	LUSTRE_STATS_ATTR_DATASET,
+
+	__LUSTRE_STATS_ATTR_MAX_PLUS_ONE,
+};
+
+#define LUSTRE_STATS_ATTR_MAX	(__LUSTRE_STATS_ATTR_MAX_PLUS_ONE - 1)
+
+/**
+ * enum lustre_stats_dataset_attrs    - Lustre stats counter's netlink
+ *					attributes used to compose messages
+ *					for sending or receiving.
+ *
+ * @LUSTRE_STATS_ATTR_DATASET_UNSPEC:	unspecified attribute to catch errors
+ * @LUSTRE_STATS_ATTR_DATASET_PAD:	padding for 64-bit attributes, ignore
+ *
+ * @LUSTRE_STATS_ATTR_DATASET_NAME:	name of counter (NLA_NUL_STRING)
+ * @LUSTRE_STATS_ATTR_DATASET_COUNT:	counter interation (NLA_U64)
+ * @LUSTRE_STATS_ATTR_DATASET_UNITS:	units of counter values (NLA_STRING)
+ * @LUSTRE_STATS_ATTR_DATASET_MINIMUM:	smallest counter value collected
+ *					(NLA_U64)
+ * @LUSTRE_STATS_ATTR_DATASET_MAXIMUM:	largest count value collected (NLA_U64)
+ * @LUSTRE_STATS_ATTR_DATASET_SUM:	total of all values of the counter
+ *					(NLA_U64)
+ * @LUSTRE_STATS_ATTR_DATASET_SUMSQUARE: Sum of the square of all values.
+ *					 Allows user land apps to calculate
+ *					 standard deviation. (NLA_U64)
+ */
+enum lustre_stats_dataset_attrs {
+	LUSTRE_STATS_ATTR_DATASET_UNSPEC = 0,
+	LUSTRE_STATS_ATTR_DATASET_PAD = LUSTRE_STATS_ATTR_DATASET_UNSPEC,
+
+	LUSTRE_STATS_ATTR_DATASET_NAME,
+	LUSTRE_STATS_ATTR_DATASET_COUNT,
+	LUSTRE_STATS_ATTR_DATASET_UNITS,
+	LUSTRE_STATS_ATTR_DATASET_MINIMUM,
+	LUSTRE_STATS_ATTR_DATASET_MAXIMUM,
+	LUSTRE_STATS_ATTR_DATASET_SUM,
+	LUSTRE_STATS_ATTR_DATASET_SUMSQUARE,
+
+	__LUSTRE_STATS_ATTR_DATASET_MAX_PLUS_ONE,
+};
+
+#define LUSTRE_STATS_ATTR_DATASET_MAX	(__LUSTRE_STATS_ATTR_DATASET_MAX_PLUS_ONE - 1)
+
+struct lustre_stats_list {
+	GENRADIX(struct lprocfs_stats *)	gfl_list;
+	unsigned int				gfl_count;
+	unsigned int				gfl_index;
+};
+
+unsigned int lustre_stats_scan(struct lustre_stats_list *slist, const char *filter);
+int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb);
+int lustre_stats_done(struct netlink_callback *cb);
 
 /* prototype for callback function on kuc groups */
 typedef int (*libcfs_kkuc_cb_t)(void *data, void *cb_arg);

commit 314dcc614be559c7a2d58774eb0c5f847224d685

LU-11850 obd: fixes for Netlink stats
Bugs have been found in the Netlink stats code that
need to handled:

- We were accessing prev stats structure after being freed.
- Ensure stats lc_name never exceeded 32 bytes. Fix debugfs
  to handle lc_name larger then 25.
- Correct the elapsed-time calculation (was negative)
- Some misspellings.

Note since we allocate the needed size using cb_min_dump_alloc
the return values for the wrapper of nla_put() are not needed.

Test-Parameters: trivial
Fixes: 5803284ac3a ("LU-11850 obd: use netlink to get lustre stats")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Signed-off-by: Minh <mdiep@thelustrecollective.com>
Change-Id: Ibafcef51b960926e0b2d718e67a39daa6432c86d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65510
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/include/lprocfs_status.h
@@ -139,6 +139,9 @@ enum lprocfs_counter_config {
 };
 #define LC_MIN_INIT ((~(__u64)0) >> 1)
 
+/* lc_name string displayed in debugfs output length is limited to 35 */
+#define LC_NAME_MAX_SIZE	32
+
 struct lprocfs_counter_header {
 	enum lprocfs_counter_config	lc_config;
 	const char			*lc_name;   /* must be static */

lustre/include/lustre_kernelcomm.h
@@ -92,7 +92,7 @@ enum lustre_param_list_attrs {
  * @LUSTRE_STATS_ATTR_TIMESTAMP:       time of collection in nanoseconds
  *				       (NLA_S64)
  * @LUSTRE_STATS_ATTR_START_TIME:      start time of collection (NLA_S64)
- * @LUSTRE_STATS_ATTR_ELPASE_TIME:     elpase time of collection (NLA_S64)
+ * @LUSTRE_STATS_ATTR_ELAPSE_TIME:     elapsed time of collection (NLA_S64)
  * @LUSTRE_STATS_ATTR_DATASET:	       bookmarks for that stats data
  *				       (NLA_NESTED)
  */
@@ -163,7 +163,7 @@ int lustre_stats_done(struct netlink_callback *cb);
  * enum lustre_target_attrs	      - Lustre general top-level netlink
  *					attributes that describe lustre
  *					'target_obd'. These values are used
- *					to piece togther messages for
+ *					to piece together messages for
  *					sending and receiving.
  *
  * @LUSTRE_TARGET_ATTR_UNSPEC:		unspecified attribute to catch errors

lustre/obdclass/kernelcomm.c
@@ -127,7 +127,8 @@ static int lustre_device_list_start(struct netlink_callback *cb)
 					continue;
 
 				prop = nla_next(prop, &rem2);
-				if (nla_type(prop) != LN_SCALAR_ATTR_VALUE)
+				if (!nla_ok(prop, rem2) ||
+				    nla_type(prop) != LN_SCALAR_ATTR_VALUE)
 					GOTO(report_err, rc = -EINVAL);
 
 				rc = nla_strscpy(name, prop, sizeof(name));
@@ -343,7 +344,8 @@ static int lustre_targets_start(struct netlink_callback *cb)
 					continue;
 
 				prop = nla_next(prop, &rem2);
-				if (nla_type(prop) != LN_SCALAR_ATTR_VALUE)
+				if (!nla_ok(prop, rem2) ||
+				    nla_type(prop) != LN_SCALAR_ATTR_VALUE)
 					GOTO(report_err, rc = -EINVAL);
 
 				len = nla_strscpy(name, prop, sizeof(name));
@@ -765,19 +767,8 @@ static int lustre_stats_start(struct netlink_callback *cb)
 			len += STATS_MSG_DATASET_SIZE * rc;
 		rc = 0;
 	}
+	cb->min_dump_alloc = len;
 
-	/* Older kernels only support 64K. Our stats can be huge. */
-	if (len >= (1UL << (sizeof(cb->min_dump_alloc) << 3))) {
-		struct lprocfs_stats **stats;
-		struct genradix_iter iter;
-
-		genradix_for_each(&slist->gfl_list, iter, stats)
-			lprocfs_stats_free(stats);
-		NL_SET_ERR_MSG(extack, "Netlink msg is too large");
-		rc = -EMSGSIZE;
-	} else {
-		cb->min_dump_alloc = len;
-	}
 report_err:
 	if (rc < 0)
 		lustre_stats_done(cb);
@@ -792,7 +783,7 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 	struct genlmsghdr *gnlh = nlmsg_data(cb->nlh);
 	struct netlink_ext_ack *extack = cb->extack;
 	int portid = NETLINK_CB(cb->skb).portid;
-	struct lprocfs_stats *prev = NULL;
+	char prev_name[LC_NAME_MAX_SIZE] = "";
 	int seq = cb->nlh->nlmsg_seq;
 	int idx = slist->gfl_index;
 	int count, i, rc = 0;
@@ -806,20 +797,26 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 
 		tmp = genradix_ptr(&slist->gfl_list, idx);
 		stats = tmp[0];
-		if (!stats)
+		if (!stats) {
+			idx++;
 			continue;
+		}
 
 		if (gnlh->version &&
-		   (!idx || (prev && strcmp(stats->ls_cnt_header[2].lc_name,
-					    prev->ls_cnt_header[2].lc_name) != 0))) {
+		   (!idx || (prev_name[0] &&
+			     strcmp(stats->ls_cnt_header[2].lc_name,
+				    prev_name) != 0))) {
 			size_t len = sizeof(struct ln_key_list);
 			int flags = NLM_F_CREATE | NLM_F_MULTI;
 			const struct ln_key_list **all;
 			struct ln_key_list *start;
 
-			/* LUSTRE_STATS_ATTR_MAX includes one stat entry
-			 * by default since we need to define what a stat
-			 * entry is.
+			/* enum lustre_stats_attrs defines one
+			 * LUSTRE_STATS_ATTR_DATASET. The stats have a
+			 * count of ls_num LUSTRE_STATS_ATTR_DATASET.
+			 * Since by default LUSTRE_STATS_ATTR_MAX covers
+			 * one LUSTRE_STATS_ATTR_DATASET we need to allocate
+			 * ls_num - 1 dataset attributes.
 			 */
 			count = LUSTRE_STATS_ATTR_MAX + stats->ls_num - 1;
 			len += sizeof(struct ln_key_props) * count;
@@ -877,7 +874,9 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 				GOTO(out_cancel, rc);
 			}
 		}
-		prev = stats;
+		memset(prev_name, 0, sizeof(prev_name));
+		strscpy(prev_name, stats->ls_cnt_header[2].lc_name,
+			sizeof(prev_name));
 
 		ghdr = genlmsg_put(msg, portid, seq, info->family, NLM_F_MULTI,
 				  gnlh->cmd);
@@ -912,8 +911,8 @@ int lustre_stats_dump(struct sk_buff *msg, struct netlink_callback *cb)
 				GOTO(out_cancel, rc);
 
 			rc = nla_put_s64(msg, LUSTRE_STATS_ATTR_ELAPSE_TIME,
-					 ktime_to_ns(ktime_sub(stats->ls_init,
-							       ktime_get())),
+					 ktime_to_ns(ktime_sub(ktime_get_real(),
+							       stats->ls_init)),
 					 LUSTRE_STATS_ATTR_PAD);
 			if (rc < 0)
 				GOTO(out_cancel, rc);

lustre/obdclass/lprocfs_status.c
@@ -1819,7 +1819,7 @@ static int lprocfs_stats_seq_show(struct seq_file *p, void *v)
 	int idx = *(loff_t *)v;
 
 	if (idx == 0)
-		lprocfs_stats_header(p, ktime_get_real(), stats->ls_init, 25,
+		lprocfs_stats_header(p, ktime_get_real(), stats->ls_init, 35,
 				     "", true, "");
 
 	hdr = &stats->ls_cnt_header[idx];
@@ -1828,7 +1828,7 @@ static int lprocfs_stats_seq_show(struct seq_file *p, void *v)
 	if (ctr.lc_count == 0)
 		return 0;
 
-	seq_printf(p, "%-25s %lld samples [%s]", hdr->lc_name,
+	seq_printf(p, "%-35s %lld samples [%s]", hdr->lc_name,
 		   ctr.lc_count, hdr->lc_units);
 
 	if ((hdr->lc_config & LPROCFS_CNTR_AVGMINMAX) && ctr.lc_count > 0) {
@@ -1937,7 +1937,6 @@ void lprocfs_counter_init_units(struct lprocfs_stats *stats, int index,
 	header = &stats->ls_cnt_header[index];
 	LASSERTF(header != NULL, "Failed to allocate stats header:[%d]%s/%s\n",
 		 index, name, units);
-
 	header->lc_config = config;
 	header->lc_name = name;
 	header->lc_units = units;

lustre/utils/liblustreapi_param.c
@@ -635,7 +635,7 @@ static int print_out_stats(yaml_parser_t *reply, int version, int flags)
 
 				value = (char *)event.data.scalar.value;
 				num = strtoll(value, NULL, 10);
-				fprintf(stdout, "%-25s %lu.%09lu secs.nsecs\n",
+				fprintf(stdout, "%-35s %lu.%09lu secs.nsecs\n",
 					"snapshot_time", num / 1000000000L,
 					num % 1000000000L);
 			} else if (strcmp(value, "start_time") == 0) {
@@ -646,7 +646,7 @@ static int print_out_stats(yaml_parser_t *reply, int version, int flags)
 
 				value = (char *)event.data.scalar.value;
 				num = strtoll(value, NULL, 10);
-				fprintf(stdout, "%-25s %lu.%09lu secs.nsecs\n",
+				fprintf(stdout, "%-35s %lu.%09lu secs.nsecs\n",
 					"start_time", num / 1000000000L,
 					num % 1000000000L);
 			} else if (strcmp(value, "elapsed_time") == 0) {
@@ -657,7 +657,7 @@ static int print_out_stats(yaml_parser_t *reply, int version, int flags)
 
 				value = (char *)event.data.scalar.value;
 				num = strtoll(value, NULL, 10);
-				fprintf(stdout, "%-25s %lu.%09lu secs.nsecs\n",
+				fprintf(stdout, "%-35s %lu.%09lu secs.nsecs\n",
 					"elapsed_time", num / 1000000000L,
 					num % 1000000000L);
 			} else if (strcmp(value, "source") == 0) {
@@ -699,7 +699,7 @@ static int print_out_stats(yaml_parser_t *reply, int version, int flags)
 
 				value = (char *)event.data.scalar.value;
 				if (tmp == buf)
-					len = sprintf(tmp, "%-26s", value);
+					len = sprintf(tmp, "%-36s", value);
 				else if (index == 1)
 					len = sprintf(tmp, " [%s]", value);
 				else