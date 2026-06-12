lustre/lustre-release

commit 1bb0d6b4b583bce7984bf19cc3bc697da5e1a9c3

LU-14633 lnet: have lnet layer use struct iov_iter
The native Linux client LNet layer was updated to use
struct iov_iter but due to RHEL7 support was never backported.
Now that RHEL7 support is dropped we can forward port Al Viro's
work. Due to delay to land that work much of the original
code has evolved over time. The LNet code is brought up to
sync with the current native Linux Lustre client

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: Id8d8e135b002facea9c0edd940a877c8cfbffc51
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63928
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Yonatan Nachum <ynachum@amazon.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -690,7 +690,7 @@ static int kiblnd_map_tx(struct lnet_ni *ni, struct kib_tx *tx,
 
 static int kiblnd_setup_rd_kiov(struct lnet_ni *ni, struct kib_tx *tx,
 				struct kib_rdma_desc *rd, int nkiov,
-				struct bio_vec *kiov, int offset, int nob)
+				const struct bio_vec *kiov, int offset, int nob)
 {
 	struct kib_net *net = ni->ni_data;
 	struct scatterlist *sg;
@@ -1647,15 +1647,15 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 	unsigned int payload_offset = lntmsg->msg_offset;
 	unsigned int payload_nob = lntmsg->msg_len;
 	struct lnet_libmd *msg_md = lntmsg->msg_md;
-	bool gpu;
+	struct iov_iter from;
 	struct kib_msg *ibmsg;
 	struct kib_rdma_desc *rd;
 	struct kib_tx *tx;
+	bool gpu;
 	int nob;
 	int rc;
 
 	/* NB 'private' is different depending on what we're sending.... */
-
 	CDEBUG(D_NET, "sending %d bytes in %d frags to %s\n",
 	       payload_nob, payload_niov, libcfs_idstr(target));
 
@@ -1663,6 +1663,12 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 
 	/* Thread context */
 	LASSERT(!in_interrupt());
+
+	iov_iter_bvec(&from, WRITE,
+		      payload_kiov, payload_niov,
+		      payload_nob + payload_offset);
+
+	iov_iter_advance(&from, payload_offset);
 
 	tx = kiblnd_get_idle_tx(ni, &target->nid);
 	if (tx == NULL) {
@@ -1677,7 +1683,7 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 	switch (type) {
 	default:
 		LBUG();
-		return (-EIO);
+		return -EIO;
 
 	case LNET_MSG_ACK:
 		LASSERT(payload_nob == 0);
@@ -1696,9 +1702,8 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 		rd = &ibmsg->ibm_u.get.ibgm_rd;
 		tx->tx_gpu = gpu;
 		rc = kiblnd_setup_rd_kiov(ni, tx, rd,
-					  msg_md->md_niov,
-					  msg_md->md_kiov,
-					  0, msg_md->md_length);
+					  payload_niov, payload_kiov,
+					  payload_offset, payload_nob);
 		if (rc != 0) {
 			CERROR("Can't setup GET sink %s: rc = %d\n",
 			       libcfs_nidstr(&target->nid), rc);
@@ -1793,11 +1798,12 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 			wrq->wr.num_sge++;
 		}
 	} else {
-		lnet_copy_kiov2flat(IBLND_MSG_SIZE, ibmsg,
-				    offsetof(struct kib_msg,
-					     ibm_u.immediate.ibim_payload),
-				    payload_niov, payload_kiov,
-				    payload_offset, payload_nob);
+		rc = copy_from_iter(&ibmsg->ibm_u.immediate.ibim_payload,
+				    payload_nob, &from);
+		if (rc != payload_nob) {
+			kiblnd_tx_done(tx);
+			return -EFAULT;
+		}
 
 		nob = offsetof(struct kib_immediate_msg,
 			       ibim_payload[payload_nob]);
@@ -1889,19 +1895,19 @@ kiblnd_get_dev_prio(struct lnet_ni *ni, unsigned int dev_idx)
 
 int
 kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
-	    int delayed, unsigned int niov, struct bio_vec *kiov,
-	    unsigned int offset, unsigned int mlen, unsigned int rlen)
+	    int delayed, struct iov_iter *to, unsigned int rlen)
 {
 	struct kib_rx *rx = private;
 	struct kib_msg *rxmsg = rx->rx_msg;
 	struct kib_conn *conn = rx->rx_conn;
 	struct kib_tx *tx;
-	__u64 ibprm_cookie;
+	u64 ibprm_cookie;
 	int nob;
 	int post_credit = IBLND_POSTRX_PEER_CREDIT;
+	int wanted = iov_iter_count(to);
 	int rc = 0;
 
-	LASSERT(mlen <= rlen);
+	LASSERT(wanted <= rlen);
 	LASSERT(!in_interrupt());
 
 	switch (rxmsg->ibm_type) {
@@ -1922,16 +1928,19 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			break;
 		}
 
-		lnet_copy_flat2kiov(niov, kiov, offset,
-				    IBLND_MSG_SIZE, rxmsg,
-				    offsetof(struct kib_msg,
-					     ibm_u.immediate.ibim_payload),
-				    mlen);
+		rc = copy_to_iter(&rxmsg->ibm_u.immediate.ibim_payload, wanted,
+				  to);
+		if (rc != wanted) {
+			rc = -EFAULT;
+			break;
+		}
+
+		rc = 0;
 		lnet_finalize(lntmsg, 0);
 		break;
 
 	case IBLND_MSG_PUT_REQ: {
-		struct kib_msg	*txmsg;
+		struct kib_msg *txmsg;
 		struct kib_rdma_desc *rd;
 		struct lnet_libmd *msg_md = NULL;
 
@@ -1939,7 +1948,8 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			msg_md = lntmsg->msg_md;
 
 		ibprm_cookie = rxmsg->ibm_u.putreq.ibprm_cookie;
-		if (mlen == 0) {
+
+		if (!wanted) {
 			lnet_finalize(lntmsg, 0);
 			kiblnd_send_completion(rx->rx_conn, IBLND_MSG_PUT_NAK,
 					       0, ibprm_cookie);
@@ -1960,7 +1970,9 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 		txmsg = tx->tx_msg;
 		rd = &txmsg->ibm_u.putack.ibpam_rd;
 		rc = kiblnd_setup_rd_kiov(ni, tx, rd,
-					  niov, kiov, offset, mlen);
+					  to->nr_segs, to->bvec,
+					  to->iov_offset,
+					  wanted);
 		if (rc != 0) {
 			CERROR("Can't setup PUT sink for %s: rc = %d\n",
 			       libcfs_nidstr(&conn->ibc_peer->ibp_nid), rc);

lnet/include/lnet/lib-lnet.h
@@ -843,46 +843,12 @@ lnet_net_set_sel_priority_locked(struct lnet_net *net, __u32 priority)
 unsigned int lnet_iov_nob(unsigned int niov, struct kvec *iov);
 unsigned int lnet_kiov_nob(unsigned int niov, struct bio_vec *iov);
 int lnet_extract_kiov(int dst_niov, struct bio_vec *dst,
-		      int src_niov, struct bio_vec *src,
+		      int src_niov, const struct bio_vec *src,
 		      unsigned int offset, unsigned int len);
 
-void lnet_copy_iov2iov(unsigned int ndiov, struct kvec *diov,
-		       unsigned int doffset,
-		       unsigned int nsiov, struct kvec *siov,
-		       unsigned int soffset, unsigned int nob);
-void lnet_copy_kiov2iov(unsigned int niov, struct kvec *iov,
-			unsigned int iovoffset,
-			unsigned int nkiov, struct bio_vec *kiov,
-			unsigned int kiovoffset, unsigned int nob);
-void lnet_copy_iov2kiov(unsigned int nkiov, struct bio_vec *kiov,
-			unsigned int kiovoffset,
-			unsigned int niov, struct kvec *iov,
-			unsigned int iovoffset, unsigned int nob);
-void lnet_copy_kiov2kiov(unsigned int ndkiov, struct bio_vec *dkiov,
-			 unsigned int doffset,
-			 unsigned int nskiov, struct bio_vec *skiov,
-			 unsigned int soffset, unsigned int nob);
-
-static inline void
-lnet_copy_kiov2flat(int dlen, void *dest, unsigned int doffset,
-		    unsigned int nsiov, struct bio_vec *skiov,
-		    unsigned int soffset, unsigned int nob)
-{
-	struct kvec diov = { .iov_base = dest, .iov_len = dlen };
-
-	lnet_copy_kiov2iov(1, &diov, doffset,
-			   nsiov, skiov, soffset, nob);
-}
-
-static inline void
-lnet_copy_flat2kiov(unsigned int ndiov, struct bio_vec *dkiov,
-		    unsigned int doffset, int slen, void *src,
-		    unsigned int soffset, unsigned int nob)
-{
-	struct kvec siov = { .iov_base = src, .iov_len = slen };
-	lnet_copy_iov2kiov(ndiov, dkiov, doffset,
-			   1, &siov, soffset, nob);
-}
+void lnet_copy_kiov2iter(struct iov_iter *to,
+			 unsigned int nkiov, const struct bio_vec *kiov,
+			 unsigned int kiovoffset, unsigned int nob);
 
 void lnet_me_unlink(struct lnet_me *me);
 

lnet/include/lnet/lib-types.h
@@ -306,15 +306,14 @@ struct lnet_lnd {
 	int (*lnd_send)(struct lnet_ni *ni, void *private,
 			struct lnet_msg *msg);
 
-	/* Start receiving 'mlen' bytes of payload data, skipping the following
-	 * 'rlen' - 'mlen' bytes. 'private' is the 'private' passed to
+	/* Start receiving @io bytes of payload data, skipping the following
+	 * 'rlen - @io' bytes. 'private' is the 'private' passed to
 	 * lnet_parse().  Return non-zero for immedaite failure, otherwise
 	 * complete later with lnet_finalize().  This also gives back a receive
-	 * credit if the LND does flow control. */
+	 * credit if the LND does flow control.
+	 */
 	int (*lnd_recv)(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
-			int delayed, unsigned int niov,
-			struct bio_vec *kiov,
-			unsigned int offset, unsigned int mlen, unsigned int rlen);
+			int delayed, struct iov_iter *to, unsigned int rlen);
 
 	/* lnet_parse() has had to delay processing of this message
 	 * (e.g. waiting for a forwarding buffer or send credits).  Give the

lnet/klnds/efalnd/efalnd.c
@@ -986,22 +986,29 @@ kefalnd_fill_putr_msg(struct kefa_conn *conn, struct kefa_tx *tx,
 	msg->msg_v2.u.putr_req.rdma_desc = tx->rdma_desc;
 }
 
-static inline void
+static inline int
 kefalnd_fill_imm_msg(struct kefa_conn *conn, struct lnet_msg *lntmsg,
 		     struct kefa_tx *tx, struct lnet_hdr *hdr)
 {
 	struct kefa_msg *msg = tx->msg;
-	int body_nob;
+	struct iov_iter from;
+	int body_nob, rc;
 
 	body_nob = offsetof(struct kefa_immediate_msg_v2,
 			    payload[lntmsg->msg_len]);
 	kefalnd_init_tx_protocol_msg(tx, conn, EFALND_MSG_IMMEDIATE, body_nob,
 				     conn->proto_ver);
 	lnet_hdr_to_nid16(hdr, &msg->msg_v2.u.immediate.hdr);
-	lnet_copy_kiov2flat(EFALND_MSG_SIZE, msg,
-			    offsetof(struct kefa_msg, msg_v2.u.immediate.payload),
-			    lntmsg->msg_niov, lntmsg->msg_kiov,
-			    lntmsg->msg_offset, lntmsg->msg_len);
+
+	iov_iter_bvec(&from, WRITE,
+		      lntmsg->msg_kiov, lntmsg->msg_niov,
+		      lntmsg->msg_len + lntmsg->msg_offset);
+	iov_iter_advance(&from, lntmsg->msg_offset);
+
+	rc = copy_from_iter(&msg->msg_v2.u.immediate.payload,
+			    lntmsg->msg_len, &from);
+
+	return rc != lntmsg->msg_len ? -EFAULT : 0;
 }
 
 static int
@@ -1124,7 +1131,11 @@ kefalnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 	}
 
 	/* SEND based (non-RDMA flow) */
-	kefalnd_fill_imm_msg(conn, lntmsg, tx, hdr);
+	rc = kefalnd_fill_imm_msg(conn, lntmsg, tx, hdr);
+	if (rc < 0) {
+		kefalnd_tx_done(tx);
+		return rc;
+	}
 
 	/* finalise lntmsg on completion */
 	tx->lntmsg[0] = lntmsg;
@@ -1390,12 +1401,11 @@ kefalnd_free_rx(struct kefa_rx *rx)
 
 static int
 kefalnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
-	    int delayed, unsigned int niov, struct bio_vec *kiov,
-	    unsigned int offset, unsigned int mlen, unsigned int rlen)
+	     int delayed, struct iov_iter *to, unsigned int rlen)
 {
 	struct kefa_ni *efa_ni = ni->ni_data;
+	int wanted = iov_iter_count(to);
 	struct kefa_rx *rx = private;
-	unsigned int imm_offset;
 	struct kefa_conn *conn;
 	struct kefa_msg *msg;
 	struct lnet_nid *nid;
@@ -1422,7 +1432,6 @@ kefalnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 		break;
 
 	case EFALND_MSG_IMMEDIATE:
-		imm_offset = offsetof(struct kefa_msg, msg_v2.u.immediate.payload);
 		imm_nob = offsetof(struct kefa_msg, msg_v2.u.immediate.payload[rlen]);
 		if (imm_nob > rx->rx_nob) {
 			EFA_DEV_ERR(efa_ni->efa_dev,
@@ -1433,14 +1442,20 @@ kefalnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			break;
 		}
 
-		lnet_copy_flat2kiov(niov, kiov, offset, EFALND_MSG_SIZE, msg,
-				    imm_offset, mlen);
+		rc = copy_to_iter(&msg->msg_v2.u.immediate.payload, wanted,
+				  to);
+		if (rc != wanted) {
+			rc = -EFAULT;
+			break;
+		}
+
 		lnet_finalize(lntmsg, 0);
 		break;
 
 	case EFALND_MSG_PUTR_REQ:
 		rc = kefalnd_handle_putr_req_v2(efa_ni, conn, lntmsg,
-						&msg->msg_v2.u.putr_req, mlen);
+						&msg->msg_v2.u.putr_req,
+						wanted);
 		break;
 
 	case EFALND_MSG_GETR_REQ:

lnet/klnds/gnilnd/gnilnd.h
@@ -1787,10 +1787,7 @@ int kgnilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg);
 int kgnilnd_eager_recv(struct lnet_ni *ni, void *private,
 			struct lnet_msg *lntmsg, void **new_private);
 int kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
-		int delayed, unsigned int niov,
-		struct bio_vec *kiov,
-		unsigned int offset, unsigned int mlen, unsigned int rlen);
-
+		 int delayed, struct iov_iter *to, unsigned int rlen);
 __u16 kgnilnd_cksum_kiov(unsigned int nkiov, struct bio_vec *kiov,
 			 unsigned int offset, unsigned int nob, int dump_blob);
 

lnet/klnds/gnilnd/gnilnd_cb.c
@@ -2326,25 +2326,23 @@ kgnilnd_eager_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 
 int
 kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
-	     int delayed, unsigned int niov,
-	     struct bio_vec *kiov,
-	     unsigned int offset, unsigned int mlen, unsigned int rlen)
+	     int delayed, struct iov_iter *to, unsigned int rlen)
 {
 	kgn_rx_t    *rx = private;
 	kgn_conn_t  *conn = rx->grx_conn;
 	kgn_msg_t   *rxmsg = rx->grx_msg;
+	int wanted = iov_iter_count(to);
 	kgn_tx_t    *tx;
 	int          rc = 0;
-	__u32        pload_cksum;
+	u32        pload_cksum;
+
 	ENTRY;
-
 	LASSERT(!in_interrupt());
-	LASSERTF(mlen <= rlen, "%d <= %d\n", mlen, rlen);
+	LASSERTF(wanted <= rlen, "%d <= %d\n", wanted, rlen);
 
-	GNIDBG_MSG(D_NET, rxmsg, "conn %p, rxmsg %p, lntmsg %p"
-		" niov=%d kiov=%p offset=%d mlen=%d rlen=%d",
-		conn, rxmsg, lntmsg,
-		niov, kiov, offset, mlen, rlen);
+	GNIDBG_MSG(D_NET, rxmsg,
+		   "conn %px, rxmsg %px, lntmsg %px iov=%px iov len=%d rlen=%d",
+		   conn, rxmsg, lntmsg, to, wanted, rlen);
 
 	/* we need to lock here as recv can be called from any context */
 	read_lock(&kgnilnd_data.kgn_peer_conn_lock);
@@ -2360,17 +2358,17 @@ kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 
 	switch (rxmsg->gnm_type) {
 	default:
-		GNIDBG_MSG(D_NETERROR, rxmsg, "conn %p, rx %p, rxmsg %p, lntmsg %p"
-		" niov=%d kiov=%p offset=%d mlen=%d rlen=%d",
-		conn, rx, rxmsg, lntmsg, niov, kiov, offset, mlen, rlen);
+		GNIDBG_MSG(D_NETERROR, rxmsg,
+			   "conn %px, rx %px, rxmsg %px, lntmsg %px iov %px iov len=%d rlen=%d",
+		conn, rx, rxmsg, lntmsg, to, wanted, rlen);
 		LBUG();
 
 	case GNILND_MSG_IMMEDIATE:
-		if (mlen > rxmsg->gnm_payload_len) {
+		if (wanted > rxmsg->gnm_payload_len) {
 			GNIDBG_MSG(D_ERROR, rxmsg,
-				"Immediate message from %s too big: %d > %d",
-				libcfs_nid2str(conn->gnc_peer->gnp_nid), mlen,
-				rxmsg->gnm_payload_len);
+				   "Immediate message from %s too big: %d > %d",
+				   libcfs_nid2str(conn->gnc_peer->gnp_nid),
+				   wanted, rxmsg->gnm_payload_len);
 			rc = -EINVAL;
 			kgnilnd_consume_rx(rx);
 			RETURN(rc);
@@ -2415,10 +2413,11 @@ kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			}
 		}
 
-		lnet_copy_flat2kiov(
-			niov, kiov, offset,
-			*kgnilnd_tunables.kgn_max_immediate,
-			&rxmsg[1], 0, mlen);
+		rc = copy_to_iter(&rxmsg[1], wanted, to);
+		if (rc != wanted) {
+			rc = -EFAULT;
+			break;
+		}
 
 		kgnilnd_consume_rx(rx);
 		lnet_finalize(lntmsg, 0);
@@ -2426,7 +2425,7 @@ kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 
 	case GNILND_MSG_PUT_REQ:
 		/* LNET wants to truncate or drop transaction, sending NAK */
-		if (mlen == 0) {
+		if (wanted == 0) {
 			kgnilnd_consume_rx(rx);
 			lnet_finalize(lntmsg, 0);
 
@@ -2451,8 +2450,10 @@ kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			GOTO(nak_put_req, rc);
 		}
 
-		rc = kgnilnd_setup_rdma_buffer(tx, niov,
-					       kiov, offset, mlen);
+		rc = kgnilnd_setup_rdma_buffer(tx, to->nr_segs,
+					       (struct bio_vec *)to->bvec,
+					       to->iov_offset,
+					       wanted);
 		if (rc != 0) {
 			GOTO(nak_put_req, rc);
 		}
@@ -2462,7 +2463,7 @@ kgnilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 		tx->tx_msg.gnm_u.putack.gnpam_dst_cookie = tx->tx_id.txe_cookie;
 		tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_addr =
 			(__u64)((unsigned long)tx->tx_buffer);
-		tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_nob = mlen;
+		tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_nob = wanted;
 
 		tx->tx_lntmsg[0] = lntmsg; /* finalize this on RDMA_DONE */
 		tx->tx_qtime = jiffies;
@@ -2489,7 +2490,7 @@ nak_put_req:
 		RETURN(-EIO);
 	case GNILND_MSG_GET_REQ_REV:
 		/* LNET wants to truncate or drop transaction, sending NAK */
-		if (mlen == 0) {
+		if (wanted == 0) {
 			kgnilnd_consume_rx(rx);
 			lnet_finalize(lntmsg, 0);
 
@@ -2515,8 +2516,10 @@ nak_put_req:
 			if (rc != 0)
 				GOTO(nak_get_req_rev, rc);
 
-			rc = kgnilnd_setup_rdma_buffer(tx, niov,
-						       kiov, offset, mlen);
+			rc = kgnilnd_setup_rdma_buffer(tx, to->nr_segs,
+						       (struct bio_vec *)to->bvec,
+						       to->iov_offset,
+						       wanted);
 			if (rc != 0)
 				GOTO(nak_get_req_rev, rc);
 
@@ -2525,8 +2528,7 @@ nak_put_req:
 			tx->tx_msg.gnm_u.putack.gnpam_dst_cookie = tx->tx_id.txe_cookie;
 			tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_addr =
 				(__u64)((unsigned long)tx->tx_buffer);
-			tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_nob = mlen;
-
+			tx->tx_msg.gnm_u.putack.gnpam_desc.gnrd_nob = wanted;
 			tx->tx_lntmsg[0] = lntmsg; /* finalize this on RDMA_DONE */
 
 			/* we only queue from kgnilnd_recv - we might get called from other contexts
@@ -2561,7 +2563,7 @@ nak_get_req_rev:
 
 	case GNILND_MSG_PUT_REQ_REV:
 		/* LNET wants to truncate or drop transaction, sending NAK */
-		if (mlen == 0) {
+		if (wanted == 0) {
 			kgnilnd_consume_rx(rx);
 			lnet_finalize(lntmsg, 0);
 
@@ -2576,7 +2578,7 @@ nak_get_req_rev:
 
 		if (lntmsg != NULL) {
 			/* Matched! */
-			kgnilnd_setup_rdma(ni, rx, lntmsg, mlen);
+			kgnilnd_setup_rdma(ni, rx, lntmsg, wanted);
 		} else {
 			/* No match */
 			kgnilnd_nak_rdma(conn, rxmsg->gnm_type,
@@ -2589,7 +2591,7 @@ nak_get_req_rev:
 	case GNILND_MSG_GET_REQ:
 		if (lntmsg != NULL) {
 			/* Matched! */
-			kgnilnd_setup_rdma(ni, rx, lntmsg, mlen);
+			kgnilnd_setup_rdma(ni, rx, lntmsg, wanted);
 		} else {
 			/* No match */
 			kgnilnd_nak_rdma(conn, rxmsg->gnm_type,
@@ -3020,24 +3022,29 @@ kgnilnd_reaper(void *arg)
 	return 0;
 }
 
-int
-kgnilnd_recv_bte_get(kgn_tx_t *tx) {
+static int kgnilnd_recv_bte_get(kgn_tx_t *tx)
+{
 	unsigned niov, offset, nob;
 	struct bio_vec *kiov;
 	struct lnet_msg *lntmsg = tx->tx_lntmsg[0];
-	kgnilnd_parse_lnet_rdma(lntmsg, &niov, &offset, &nob, &kiov, tx->tx_nob_rdma);
+	int rc = 0;
 
+	kgnilnd_parse_lnet_rdma(lntmsg, &niov, &offset, &nob, &kiov, tx->tx_nob_rdma);
 	if (kiov != NULL) {
-		lnet_copy_flat2kiov(
-			niov, kiov, offset,
-			nob,
-			tx->tx_buffer_copy + tx->tx_offset, 0, nob);
+		struct iov_iter to;
+
+		iov_iter_bvec(&to, READ, kiov, niov, offset + nob);
+		iov_iter_advance(&to, offset);
+
+		rc = copy_to_iter(tx->tx_buffer_copy + tx->tx_offset, nob, &to);
+		if (rc != nob)
+			rc = -EFAULT;
 	} else {
 		memcpy(tx->tx_buffer, tx->tx_buffer_copy + tx->tx_offset, nob);
 	}
-	return 0;
-}
 
+	return rc;
+}
 
 int
 kgnilnd_check_rdma_cq(kgn_device_t *dev)
@@ -3125,15 +3132,21 @@ kgnilnd_check_rdma_cq(kgn_device_t *dev)
 
 		rc = 0;
 		if (tx->tx_msg.gnm_type == GNILND_MSG_GET_DONE_REV && desc->status == GNI_RC_SUCCESS) {
+			u16 cksum = tx->tx_putinfo.gnpam_payload_cksum;
+
 			if (tx->tx_buffer_copy != NULL)
-				kgnilnd_recv_bte_get(tx);
-			rc = kgnilnd_verify_rdma_cksum(tx, tx->tx_putinfo.gnpam_payload_cksum, tx->tx_nob_rdma);
+				rc = kgnilnd_recv_bte_get(tx);
+			if (rc == 0)
+				rc = kgnilnd_verify_rdma_cksum(tx, cksum, tx->tx_nob_rdma);
 		}
 
 		if (tx->tx_msg.gnm_type == GNILND_MSG_PUT_DONE_REV && desc->status == GNI_RC_SUCCESS) {
+			u16 cksum = tx->tx_getinfo.gngm_payload_cksum;
+
 			if (tx->tx_buffer_copy != NULL)
-				kgnilnd_recv_bte_get(tx);
-			rc = kgnilnd_verify_rdma_cksum(tx, tx->tx_getinfo.gngm_payload_cksum, tx->tx_nob_rdma);
+				rc = kgnilnd_recv_bte_get(tx);
+			if (rc == 0)
+				rc = kgnilnd_verify_rdma_cksum(tx, cksum, tx->tx_nob_rdma);
 		}
 
 		/* remove from rdmaq */

lnet/klnds/gnilnd/gnilnd_proc.c
@@ -31,6 +31,7 @@ _kgnilnd_proc_run_cksum_test(int caseno, int nloops, int nob)
 {
 	struct bio_vec *src, *dest;
 	struct timespec begin, end, diff;
+	struct iov_iter to;
 	int niov;
 	int rc = 0;
 	int i = 0, j = 0, n;
@@ -64,6 +65,8 @@ _kgnilnd_proc_run_cksum_test(int caseno, int nloops, int nob)
 			GOTO(unwind, rc = -ENOMEM);
 		}
 	}
+	iov_iter_bvec(&to, WRITE, dest, GNILND_MAX_IOV,
+		      GNILND_MAX_IOV * PAGE_SIZE);
 
 	/* add extra 2 pages - one for offset of src, 2nd to allow dest offset */
 	niov = (nob / PAGE_SIZE) + 2;
@@ -96,12 +99,13 @@ _kgnilnd_proc_run_cksum_test(int caseno, int nloops, int nob)
 	src[0].bv_len = PAGE_SIZE - src[0].bv_offset;
 	dest[0].bv_len = PAGE_SIZE - dest[0].bv_offset;
 
+	iov_iter_advance(&to, dest[0].bv_offset);
 	for (i = 0; i < niov; i++) {
 		memset(page_address(src[i].bv_page) + src[i].bv_offset,
 		       0xf0 + i, src[i].bv_len);
 	}
 
-	lnet_copy_kiov2kiov(niov, dest, 0, niov, src, 0, nob);
+	lnet_copy_kiov2iter(&to, GNILND_MAX_IOV, src, 0, nob);
 
 	getnstimeofday(&begin);
 

lnet/klnds/kfilnd/kfilnd.c
@@ -90,6 +90,12 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 	bool tn_key = false;
 	lnet_nid_t tgt_nid4;
 	bool gpu = lnet_md_is_gpu(msg->msg_md);
+	struct iov_iter from;
+
+	iov_iter_bvec(&from, WRITE,
+		      msg->msg_kiov, msg->msg_niov,
+		      msg->msg_len + msg->msg_offset);
+	iov_iter_advance(&from, msg->msg_offset);
 
 	switch (type) {
 	default:
@@ -162,12 +168,10 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 		       tn, msg->msg_kiov, msg->msg_niov,
 		       msg->msg_offset, msg->msg_len);
 
-		lnet_copy_kiov2flat(KFILND_IMMEDIATE_MSG_SIZE,
-				    tn->tn_tx_msg.msg,
-				    offsetof(struct kfilnd_msg,
-					     proto.immed.payload),
-				    msg->msg_niov, msg->msg_kiov,
-				    msg->msg_offset, msg->msg_len);
+		rc = copy_from_iter(&tn->tn_tx_msg.msg->proto.immed.payload,
+				    msg->msg_len, &from);
+		if (rc != msg->msg_len)
+			return -EFAULT;
 
 		tn->tn_nob = msg->msg_len;
 		event = TN_EVENT_INIT_IMMEDIATE;
@@ -236,19 +240,17 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 }
 
 static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
-		       int delayed, unsigned int niov,
-		       struct bio_vec *kiov,
-		       unsigned int offset, unsigned int mlen,
-		       unsigned int rlen)
+		       int delayed, struct iov_iter *to, unsigned int rlen)
 {
 	struct kfilnd_transaction *tn = private;
 	struct kfilnd_msg *rxmsg = tn->tn_rx_msg.msg;
+	int wanted = iov_iter_count(to);
 	int nob;
 	int rc = 0;
 	int status = 0;
 	enum tn_events event;
 
-	if (mlen > rlen)
+	if (wanted > rlen)
 		return -EINVAL;
 
 	/* Transaction must be in receive state */
@@ -272,17 +274,18 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 		}
 		tn->tn_nob = nob;
 
-		lnet_copy_flat2kiov(niov, kiov, offset,
-				    KFILND_IMMEDIATE_MSG_SIZE, rxmsg,
-				    offsetof(struct kfilnd_msg,
-					     proto.immed.payload),
-				    mlen);
+		rc = copy_to_iter(&rxmsg->proto.immed.payload, wanted, to);
+		if (rc != wanted) {
+			rc = -EFAULT;
+			break;
+		}
 
+		rc = 0;
 		kfilnd_tn_event_handler(tn, TN_EVENT_RX_OK, 0);
 		return 0;
 
 	case KFILND_MSG_BULK_PUT_REQ:
-		if (mlen == 0) {
+		if (wanted == 0) {
 			event = TN_EVENT_SKIP_TAG_RMA;
 		} else {
 			struct lnet_libmd *msg_md = NULL;
@@ -294,8 +297,10 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 
 			/* Post the buffer given us as a sink  */
 			tn->sink_buffer = true;
-			rc = kfilnd_tn_set_buf(ni, tn, kiov, niov, offset,
-					       mlen);
+			rc = kfilnd_tn_set_buf(ni, tn,
+					       (struct bio_vec *)to->bvec,
+					       to->nr_segs, to->iov_offset,
+					       wanted);
 			if (rc) {
 				CERROR("Failed to setup PUT sink buffer rc %d\n", rc);
 				kfilnd_tn_free(tn);

commit 73c8cc8821579badc2cbbb6c057baa9d855d1df3

LU-20326 o2iblnd: fix GET sink buffer length for RDMA
The conversion of the LNet layer to struct iov_iter changed the
LNET_MSG_GET case of kiblnd_send() to describe the RDMA GET sink
from the outbound payload (payload_niov/payload_kiov/payload_nob)
instead of the attached memory descriptor (msg_md). For a GET the
initiator sends no payload: lnet_prep_send() is called as
lnet_prep_send(msg, LNET_MSG_GET, target, 0, 0), so msg_len, and
therefore payload_nob, is always 0. The sink length lives in
msg_md->md_length.

As a result kiblnd_setup_rd_kiov() is now invoked with nob == 0 and
trips its first assertion:

  LNetError: (o2iblnd_cb.c:704:kiblnd_setup_rd_kiov())
             ASSERTION( nob > 0 ) failed
  LBUG

The IMMEDIATE-vs-RDMA size check immediately above still correctly
uses msg_md->md_length, so small GET replies take the IMMEDIATE path
and escape. Only GET sinks large enough to require RDMA reach
kiblnd_setup_rd_kiov() and hit the assert. This covers every
server-side BULK_GET_SINK transfer over o2ib -- client writes and
target_bulk_io() bulk pulls (e.g. the MDS_BATCH buffer pulled by
mdt_batch() during batched statahead) -- so a large write or an
mdtest stat phase reliably LBUGs the target.

Restore the GET sink description to come from msg_md, matching the
adjacent size check and the pre-conversion behaviour.

Fixes: 1bb0d6b4b5 ("LU-14633 lnet: have lnet layer use struct iov_iter")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chorn@ddn.com>
Change-Id: I1f1402b3bdc39956eb93ef63c65e41bc49f3b958
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66432
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1702,8 +1702,8 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 		rd = &ibmsg->ibm_u.get.ibgm_rd;
 		tx->tx_gpu = gpu;
 		rc = kiblnd_setup_rd_kiov(ni, tx, rd,
-					  payload_niov, payload_kiov,
-					  payload_offset, payload_nob);
+					  msg_md->md_niov, msg_md->md_kiov,
+					  0, msg_md->md_length);
 		if (rc != 0) {
 			CERROR("Can't setup GET sink %s: rc = %d\n",
 			       libcfs_nidstr(&target->nid), rc);