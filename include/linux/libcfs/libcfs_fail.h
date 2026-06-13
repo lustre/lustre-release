lustre/lustre-release

commit 3780efa1c77985c600e8ffb35e0c326b4d87cefa

LU-18022 lmv: quota-aware MDT selection on -EDQUOT
When mkdir hits -EDQUOT on an MDT, instead of failing immediately,
retry the operation on another active MDT.

The client walks the MDT list to find the next usable target. if
suitable target is found, the mkdir is retried there, otherwise the
original -EDQUOT is returned.

This improves user experience by automatically selecting an MDT with
available quota space.

Change-Id: I2d1b1797c8b405cfd69406cc480db298a08aa3d4
Signed-off-by: Chakshu Kansal <ckansal@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64294
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -2135,6 +2135,55 @@ static struct lu_tgt_desc *lmv_locate_tgt_by_space(struct lmv_obd *lmv,
 	return tgt;
 }
 
+/**
+ * lmv_locate_tgt_next_avail() - Select next available MDT
+ * @lmv: LMV device
+ * @op_data: operation data containing user/group IDs
+ * @cur: current MDT
+ * @exclude_bitmap: bitmap of MDT indices to exclude from selection (already
+ * tried)
+ *
+ *
+ * Return:
+ * Pointer to lu_tgt_desc of selected MDT, or error pointer.
+ */
+static
+struct lu_tgt_desc *lmv_locate_tgt_next_avail(struct lmv_obd *lmv,
+					      struct md_op_data *op_data,
+					      struct lmv_tgt_desc *cur,
+					      unsigned long *exclude_bitmap)
+{
+	__u32 count = lmv->lmv_mdt_count;
+	__u32 start;
+	__u32 i;
+
+	if (count <= 1)
+		return ERR_PTR(-EDQUOT);
+
+	/* Make start random to spread creates across MDTs */
+	start = (cur->ltd_index + get_random_u32()) % count;
+
+	for (i = 0; i < count ; i++) {
+		__u32 idx = (start+i) % count;
+
+		struct lu_tgt_desc *tgt;
+
+		if (test_bit(idx, exclude_bitmap))
+			continue;
+
+		tgt = lmv_tgt_retry(lmv, idx);
+		if (!tgt || !tgt->ltd_active)
+			continue;
+		if (tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)
+			continue;
+
+		op_data->op_mds = tgt->ltd_index;
+		return tgt;
+	}
+
+	return ERR_PTR(-EDQUOT);
+}
+
 static bool lmv_tgt_nocreate(struct lmv_obd *lmv, struct lmv_tgt_desc *tgt)
 {
 	if (likely(!(tgt->ltd_statfs.os_state & OS_STATFS_NOCREATE)))
@@ -2227,9 +2276,13 @@ static int lmv_create(struct obd_export *exp, struct md_op_data *op_data,
 	struct lmv_obd *lmv = &obd->u.lmv;
 	struct lmv_tgt_desc *tgt;
 	struct mdt_body *repbody;
+	DECLARE_BITMAP(exclude_bitmap, LMV_MAX_TGT_COUNT);
+	int quota_retry = 0;
 	int rc;
 
 	ENTRY;
+
+	bitmap_zero(exclude_bitmap, LMV_MAX_TGT_COUNT);
 
 	if (!lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count)
 		RETURN(-EIO);
@@ -2277,6 +2330,58 @@ retry:
 		if (*request == NULL)
 			RETURN(rc);
 		CDEBUG(D_INODE, "Created - "DFID"\n", PFID(&op_data->op_fid2));
+	}
+
+	/* If we hit quota limit and this is a directory creation, try to
+	 * find an alternative MDT with available quota space. Retry up to
+	 * the number of active MDTs.
+	 */
+	if (rc == -EDQUOT && S_ISDIR(mode) &&
+	    quota_retry < lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count &&
+	    lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count > 1) {
+		struct lmv_tgt_desc *new_tgt;
+
+		/* below message is checked in sanity-quota test_98 */
+		CDEBUG(D_QUOTA,
+		       "mkdir hit EDQUOT on MDT%04x (retry %d/%u), searching for alternative MDT\n",
+		       tgt->ltd_index, quota_retry,
+		       lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count);
+
+		set_bit(tgt->ltd_index, exclude_bitmap);
+
+		new_tgt = lmv_locate_tgt_next_avail(lmv, op_data, tgt,
+						    exclude_bitmap);
+		if (!IS_ERR(new_tgt) && new_tgt != tgt) {
+			CDEBUG(D_INFO,
+			       "\nretrying mkdir on MDT%04x (was MDT%04x)\n",
+			       new_tgt->ltd_index, tgt->ltd_index);
+
+			if (*request != NULL) {
+				ptlrpc_req_put(*request);
+				*request = NULL;
+			}
+
+			tgt = new_tgt;
+			quota_retry++;
+			/* Reset FID2 for retry, and allocate a new FID */
+			fid_zero(&op_data->op_fid2);
+			rc = lmv_fid_alloc(NULL, exp, &op_data->op_fid2, op_data);
+			if (rc)
+				RETURN(rc);
+			goto retry;
+		}
+
+		CDEBUG(D_QUOTA,
+		       "Failed to find alternative MDT: new_tgt=%p, tgt=%p, is_err=%d\n",
+		       new_tgt, tgt, IS_ERR(new_tgt));
+	} else {
+		if (rc == -EDQUOT) {
+			CDEBUG(D_QUOTA,
+			       "EDQUOT but conditions not met: S_ISDIR=%d, retry_count=%d/%u, mdt_count=%u\n",
+			       S_ISDIR(mode), quota_retry,
+			       lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count,
+			       lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count);
+		}
 	}
 
 	/* dir restripe needs to send to MDT where dir is located */

lustre/lod/lod_object.c
@@ -6226,9 +6226,9 @@ static int lod_declare_create(const struct lu_env *env, struct dt_object *dt,
 			      struct dt_allocation_hint *hint,
 			      struct dt_object_format *dof, struct thandle *th)
 {
-	struct dt_object   *next = dt_object_child(dt);
-	struct lod_object  *lo = lod_dt_obj(dt);
-	int		    rc;
+	struct dt_object *next = dt_object_child(dt);
+	struct lod_object *lo = lod_dt_obj(dt);
+	int rc;
 	ENTRY;
 
 	LASSERT(dof);
@@ -6241,6 +6241,10 @@ static int lod_declare_create(const struct lu_env *env, struct dt_object *dt,
 	rc = lod_sub_declare_create(env, next, attr, hint, dof, th);
 	if (rc != 0)
 		GOTO(out, rc);
+
+	/* Inject EDQUOT for sanity-quota test_98 */
+	if (CFS_FAIL_CHECK(OBD_FAIL_QUOTA_EDQUOT) && S_ISDIR(attr->la_mode))
+		GOTO(out, rc = -EDQUOT);
 
 	/*
 	 * it's lod_ah_init() that has decided the object will be striped

lustre/tests/sanity-quota.sh
@@ -7908,6 +7908,53 @@ test_97d() {
 }
 run_test 97d "LQA disk persistence using standard commands"
 
+test_98() {
+	(( $MDSCOUNT >= 2 )) || skip "needs >= 2 MDTs"
+
+	local testdir="$DIR/$tdir"
+
+	# Use the current round-robin index and map it into the
+	# range of configured MDT indices [0..MDSCOUNT-1].
+	local rr_idx=$($LCTL get_param -n lmv.*.qos_rr_index | head -1)
+	local idx=$((rr_idx % MDSCOUNT))
+	local expected_mdt=$(((rr_idx+1) % MDSCOUNT))
+
+	wait_clients_import_state $HOSTNAME mds$((idx+1)) FULL
+	wait_clients_import_state $HOSTNAME mds$((expected_mdt+1)) FULL
+
+	local idx_hex
+	printf -v idx_hex "%04x" "$idx"
+
+	local old_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr | head -1)
+	stack_trap "$LCTL set_param lmv.*.qos_threshold_rr=$old_rr"
+
+	#define OBD_FAIL_QUOTA_EDQUOT            0xA02
+	stack_trap "do_facet mds$((idx+1)) $LCTL set_param fail_loc=0"
+	do_facet mds$((idx+1)) $LCTL set_param fail_loc=0x80000A02
+
+	$LCTL set_param lmv.*.qos_threshold_rr=100
+
+	local old_qos_maxage=$($LCTL get_param -n lmv.*.qos_maxage)
+	$LCTL set_param lmv.*.qos_maxage=10
+	stack_trap "$LCTL set_param -n lmv.*.qos_maxage=$old_qos_maxage"
+	# import can be used only after (qos_maxage >> 1)s after setup
+	sleep 5
+
+	echo "First MDT: $idx, expected retry MDT: $expected_mdt"
+
+	mkdir "$testdir" || error "mkdir failed - retry mechanism not working"
+
+	[[ -d "$testdir" ]] || error "Directory not created after retry"
+
+	local stripe_index=$($LFS getdirstripe -i $testdir)
+
+	(( stripe_index != idx )) ||
+		error "Failed to create directory on another MDT"
+
+	echo "Directory successfully created on MDT$stripe_index after retry"
+}
+run_test 98 "Verify MDT retry on -EDQUOT during mkdir"
+
 quota_fini()
 {
 	do_nodes $(comma_list $(nodes_list)) \

commit 0751175785ef43fb338ff283696a15a6948d49d0

LU-20249 lmv: improve quota-aware MDT retry testing
Add MDT-specific EDQUOT injection via fail_val for
independent quota testing. Improve random MDT selection
by adding 1 before get_random_u32_below(count - 1) to
avoid selecting the current MDT and reduce unnecessary
fallback iterations.

Add parameter verification to sanity-quota test_98.

Fixes: 3780efa1c7 ("LU-18022 lmv: quota-aware MDT selection on -EDQUOT")
Change-Id: I74de7c35d0125d416b735772a4d1111978334be7
Signed-off-by: Chakshu Kansal <ckansal@ddn.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65948
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

include/linux/libcfs/libcfs_fail.h
@@ -99,6 +99,24 @@ static inline int cfs_fail_check_set_loc(const char *file, const char *func,
 	return ret;
 }
 
+/* Helper function to check if fail_val targets a specific index.
+ * Supports three modes:
+ *   0            - target all indices
+ *   1..0xffff    - target single index (index == fail_val - 1)
+ *   0x10000+     - bitmask mode: bits 0-15 are a bitmask of target indices
+ *
+ * Returns true if the given index should fail, false otherwise.
+ */
+static inline bool cfs_fail_index(unsigned int fail_val, unsigned int index)
+{
+	if (fail_val == 0)
+		return true; /* fail all */
+	if (fail_val > 0xffff)
+		return (index < 16) && (fail_val & BIT(index)); /* bitmask mode */
+
+	return index == (fail_val - 1); /* single index mode */
+}
+
 /*
  * If id hit cfs_fail_loc, return 1, otherwise return 0
  */

lustre/lmv/lmv_obd.c
@@ -2178,7 +2178,7 @@ struct lu_tgt_desc *lmv_locate_tgt_next_avail(struct lmv_obd *lmv,
 		return ERR_PTR(-EDQUOT);
 
 	/* Make start random to spread creates across MDTs */
-	start = (cur->ltd_index + get_random_u32()) % count;
+	start = (cur->ltd_index + 1 + get_random_u32_below(count - 1)) % count;
 
 	for (i = 0; i < count ; i++) {
 		__u32 idx = (start+i) % count;
@@ -2358,11 +2358,10 @@ retry:
 	    lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count > 1) {
 		struct lmv_tgt_desc *new_tgt;
 
-		/* below message is checked in sanity-quota test_98 */
 		CDEBUG(D_QUOTA,
-		       "mkdir hit EDQUOT on MDT%04x (retry %d/%u), searching for alternative MDT\n",
-		       tgt->ltd_index, quota_retry,
-		       lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count);
+		       "%s: mkdir hit EDQUOT on MDT%04x (retry %d/%u), searching for alternative MDT: rc = %d\n",
+		       lmv2obd_dev(lmv)->obd_name, tgt->ltd_index, quota_retry,
+		       lmv->lmv_mdt_descs.ltd_lmv_desc.ld_active_tgt_count, rc);
 
 		set_bit(tgt->ltd_index, exclude_bitmap);
 

lustre/lod/lod_object.c
@@ -6256,8 +6256,18 @@ static int lod_declare_create(const struct lu_env *env, struct dt_object *dt,
 		GOTO(out, rc);
 
 	/* Inject EDQUOT for sanity-quota test_98 */
-	if (CFS_FAIL_CHECK(OBD_FAIL_QUOTA_EDQUOT) && S_ISDIR(attr->la_mode))
-		GOTO(out, rc = -EDQUOT);
+	if (CFS_FAIL_CHECK(OBD_FAIL_QUOTA_EDQUOT) && S_ISDIR(attr->la_mode)) {
+		struct seq_server_site *ss;
+
+		ss = lu_site2seq(dt->do_lu.lo_dev->ld_site);
+		if (cfs_fail_index(cfs_fail_val, ss->ss_node_id)) {
+			rc = -EDQUOT;
+			CERROR("%s: Injecting -EDQUOT for directory create on MDT%04x (fail_val=%x): rc = %d\n",
+			       dt->do_lu.lo_dev->ld_obd->obd_name,
+			       ss->ss_node_id, cfs_fail_val, rc);
+			GOTO(out, rc);
+		}
+	}
 
 	/*
 	 * it's lod_ah_init() that has decided the object will be striped

lustre/tests/sanity-quota.sh
@@ -7922,21 +7922,35 @@ test_98() {
 	wait_clients_import_state $HOSTNAME mds$((idx+1)) FULL
 	wait_clients_import_state $HOSTNAME mds$((expected_mdt+1)) FULL
 
-	local idx_hex
-	printf -v idx_hex "%04x" "$idx"
-
 	local old_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr | head -1)
 	stack_trap "$LCTL set_param lmv.*.qos_threshold_rr=$old_rr"
 
 	#define OBD_FAIL_QUOTA_EDQUOT            0xA02
-	stack_trap "do_facet mds$((idx+1)) $LCTL set_param fail_loc=0"
-	do_facet mds$((idx+1)) $LCTL set_param fail_loc=0x80000A02
+	# fail_val selects which MDTs to fail:
+	#   0            - fail all MDTs
+	#   1..0xffff    - fail single MDT at index (fail_val - 1)
+	#   0x10000+     - bitmask mode: bits 0-15 are MDT indices to fail
+	# We use mode 2 (single MDT) with fail_val = idx+1 to target specific MDT
+	stack_trap "do_facet mds$((idx+1)) $LCTL set_param fail_loc=0 fail_val=0"
+	do_facet mds$((idx+1)) $LCTL set_param \
+		fail_val=$((idx+1)) fail_loc=0xA02 ||
+		error "Failed to set fail_loc on mds$((idx+1))"
 
-	$LCTL set_param lmv.*.qos_threshold_rr=100
+	$LCTL set_param lmv.*.qos_threshold_rr=100 ||
+		error "Failed to set qos_threshold_rr"
+
+	local set_qos_rr=$($LCTL get_param -n lmv.*.qos_threshold_rr | head -1)
+	[[ "$set_qos_rr" == "100%" ]] ||
+		error "qos_threshold_rr expected 100%, got $set_qos_rr"
 
 	local old_qos_maxage=$($LCTL get_param -n lmv.*.qos_maxage)
-	$LCTL set_param lmv.*.qos_maxage=10
+	$LCTL set_param lmv.*.qos_maxage=10 ||
+		error "Failed to set qos_maxage"
 	stack_trap "$LCTL set_param -n lmv.*.qos_maxage=$old_qos_maxage"
+
+	local set_qos_maxage=$($LCTL get_param -n lmv.*.qos_maxage | head -1)
+	(( set_qos_maxage == 10 )) ||
+		error "qos_maxage not set correctly: expected 10, got $set_qos_maxage"
 	# import can be used only after (qos_maxage >> 1)s after setup
 	sleep 5
 
@@ -7949,7 +7963,7 @@ test_98() {
 	local stripe_index=$($LFS getdirstripe -i $testdir)
 
 	(( stripe_index != idx )) ||
-		error "Failed to create directory on another MDT"
+		error "Created on MDT$stripe_index, should not be MDT$idx"
 
 	echo "Directory successfully created on MDT$stripe_index after retry"
 }