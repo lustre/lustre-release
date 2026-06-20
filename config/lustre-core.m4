Cray/lustre

commit f85a7cf4c8d32d38479eeac03ec5045575c5f43a

LU-20133 build: Compatibility updates for kernel v7.0
Linux commit v6.19-rc1-31-g6cbfdf89470ef
  posix_acl: make posix_acl_to_xattr() alloc the buffer
Provide a compat wrapper of posix_acl_to_xattr for older kernels

Linux commit v6.19-rc3-135-g2719bd1ee1a1c
  block: introduce blk_queue_rot()
Provide a compat wrapper of blk_queue_rot for older kernels

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I52e8d3ae85203e4506bf7cee23571aa80a1e66a7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65294
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

config/lustre-core.m4
@@ -3609,6 +3609,36 @@ AC_DEFUN([LC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
 ]) # LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 
 #
+# LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
+#
+# Linux commit v6.19-rc1-31-g6cbfdf89470ef
+#  posix_acl: make posix_acl_to_xattr() alloc the buffer
+#
+AC_DEFUN([LC_SRC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER],[
+	LB2_LINUX_TEST_SRC([posix_acl_to_xattr], [
+		#include <linux/posix_acl_xattr.h>
+	],[
+		struct posix_acl *acl = NULL;
+		size_t acl_sz;
+		void *value = posix_acl_to_xattr(&init_user_ns, acl, &acl_sz,
+						 GFP_NOFS);
+
+		(void)value;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER],[
+	LB2_MSG_LINUX_TEST_RESULT([if posix_acl_to_xattr() returns allocated buffer],
+	[posix_acl_to_xattr], [
+		AC_DEFINE(HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER, 1,
+			  [posix_acl_to_xattr() returns allocated buffer])
+	], [
+		AC_DEFINE([old_posix_acl_to_xattr(ns, acl, buf, sz)],
+			  [posix_acl_to_xattr(ns, acl, buf, sz)],
+			  [real->posix_acl_to_xattr() ])
+	])
+]) # LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
+
+#
 # LC_PROG_LINUX
 #
 # Lustre linux kernel checks
@@ -3813,6 +3843,9 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_INODE_STATE_READ
 	LC_SRC_HAVE_VFS_CREATE_DELEGATE
 	LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+
+	# 7.0
+	LC_SRC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
 ])
 
 AC_DEFUN([LC_PROG_LINUX_RESULTS], [
@@ -4030,6 +4063,9 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_INODE_STATE_READ
 	LC_HAVE_VFS_CREATE_DELEGATE
 	LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+
+	# 7.0
+	LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
 ])
 
 #

include/lustre_compat/linux/blkdev.h
@@ -1,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ */
+
+#ifndef __LIBCFS_LINUX_BLKDEV_H__
+#define __LIBCFS_LINUX_BLKDEV_H__
+
+#ifndef blk_queue_rot
+#define blk_queue_rot(q)	(!blk_queue_nonrot((q)))
+#endif
+
+#endif /* __LIBCFS_LINUX_BLKDEV_H__ */

include/lustre_compat/linux/posix_acl_xattr.h
@@ -1,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ */
+
+#ifndef __LIBCFS_LINUX_POSIX_ACL_XATTR_H__
+#define __LIBCFS_LINUX_POSIX_ACL_XATTR_H__
+
+#include <linux/posix_acl_xattr.h>
+
+#ifndef HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
+static inline void *
+compat_posix_acl_to_xattr(struct user_namespace *user_ns,
+			  const struct posix_acl *acl,
+			  size_t *sizep, gfp_t gfp)
+{
+	size_t acl_sz = posix_acl_xattr_size(acl->a_count);
+	void *value = NULL;
+	int rc;
+
+	*sizep = acl_sz;
+	if (acl_sz > 0)
+		value = kmalloc(acl_sz, gfp);
+	if (!value)
+		return NULL;
+
+	rc = posix_acl_to_xattr(user_ns, acl, value, acl_sz);
+	if (rc < 0) {
+		kfree(value);
+		return NULL;
+	}
+
+	return value;
+}
+#define posix_acl_to_xattr(ns, acl, sz, gfp) \
+	compat_posix_acl_to_xattr((ns), (acl), (sz), (gfp))
+#endif
+
+#endif /* __LIBCFS_LINUX_POSIX_ACL_XATTR_H__ */

lustre/include/lustre_compat.h
@@ -31,6 +31,8 @@
 #include <lustre_compat/linux/shrinker.h>
 #include <lustre_compat/linux/xarray.h>
 #include <lustre_compat/linux/folio.h>
+#include <lustre_compat/linux/blkdev.h>
+#include <lustre_compat/linux/posix_acl_xattr.h>
 #include <obd_support.h>
 
 #ifdef HAVE_4ARGS_VFS_SYMLINK

lustre/llite/acl.c
@@ -149,14 +149,10 @@ int ll_set_acl(struct mnt_idmap *map,
 		return rc;
 
 	if (acl) {
-		value_size = posix_acl_xattr_size(acl->a_count);
-		value = kmalloc(value_size, GFP_NOFS);
-		if (value == NULL)
+		value = posix_acl_to_xattr(&init_user_ns, acl, &value_size,
+					   GFP_NOFS);
+		if (!value)
 			GOTO(out, rc = -ENOMEM);
-
-		rc = posix_acl_to_xattr(&init_user_ns, acl, value, value_size);
-		if (rc < 0)
-			GOTO(out_value, rc);
 	}
 
 	rc = md_setxattr(sbi->ll_md_exp, ll_inode2fid(inode),
@@ -168,9 +164,8 @@ int ll_set_acl(struct mnt_idmap *map,
 		ll_i2info(inode)->lli_synced_to_mds = false;
 
 	ptlrpc_req_put(req);
-out_value:
-	kfree(value);
 out:
+	kfree(value);
 	if (rc)
 		forget_cached_acl(inode, type);
 	else

lustre/llite/xattr.c
@@ -569,6 +569,8 @@ static int ll_xattr_get_common(const struct xattr_handler *handler,
 	if (handler->flags == XATTR_ACL_ACCESS_T) {
 		struct ll_inode_info *lli = ll_i2info(inode);
 		struct posix_acl *acl;
+		size_t acl_sz;
+		void *value = NULL;
 
 		read_lock(&lli->lli_lock);
 		acl = posix_acl_dup(lli->lli_posix_acl);
@@ -576,8 +578,20 @@ static int ll_xattr_get_common(const struct xattr_handler *handler,
 
 		if (!acl)
 			RETURN(-ENODATA);
-
-		rc = posix_acl_to_xattr(&init_user_ns, acl, buffer, size);
+		value = posix_acl_to_xattr(&init_user_ns, acl, &acl_sz,
+					   GFP_NOFS);
+		/* caller wants size */
+		if (!buffer || !size)
+			GOTO(out_acl, rc = acl_sz);
+		if (!value)
+			GOTO(out_acl, rc = -ENOMEM);
+		/* setfacl, getxattr() checks for -ERANGE */
+		if (acl_sz > size)
+			GOTO(out_acl, rc = -ERANGE);
+		rc = acl_sz;
+		memcpy(buffer, value, acl_sz);
+out_acl:
+		kfree(value);
 		posix_acl_release(acl);
 		RETURN(rc);
 	}

lustre/osd-ldiskfs/osd_handler.c
@@ -8570,7 +8570,7 @@ static int osd_mount(const struct lu_env *env,
 	if (test_bit(LMD_FLG_NOSCRUB, &lmd_flags))
 		o->od_scrub.os_scrub.os_auto_scrub_interval = AS_NEVER;
 
-	if (blk_queue_nonrot(bdev_get_queue(osd_sb(o)->s_bdev))) {
+	if (!blk_queue_rot(bdev_get_queue(osd_sb(o)->s_bdev))) {
 		/* do not use pagecache with flash-backed storage */
 		o->od_writethrough_cache = 0;
 		o->od_read_cache = 0;
@@ -8670,7 +8670,7 @@ static int osd_device_init0(const struct lu_env *env,
 
 	/* Can only check block device after mount */
 	o->od_nonrotational =
-		blk_queue_nonrot(bdev_get_queue(osd_sb(o)->s_bdev));
+		!blk_queue_rot(bdev_get_queue(osd_sb(o)->s_bdev));
 
 	osd_init_t10_type(o);
 	if (o->od_t10_type != OSD_T10_TYPE_UNKNOWN) {

lustre/osd-ldiskfs/osd_internal.h
@@ -806,6 +806,8 @@ static inline int __osd_acl_get(struct inode *inode, const char *name,
 {
 	struct posix_acl *acl;
 	int acl_type, error;
+	size_t acl_sz;
+	void *value = NULL;
 
 	acl_type = posix_acl_type(name);
 	if (acl_type < 0)
@@ -821,7 +823,18 @@ static inline int __osd_acl_get(struct inode *inode, const char *name,
 	if (!acl)
 		return -ENODATA;
 
-	error = posix_acl_to_xattr(&init_user_ns, acl, buf, len);
+	value = posix_acl_to_xattr(&init_user_ns, acl, &acl_sz, GFP_NOFS);
+	/* caller just wanted the required acl size */
+	if (!buf || !len)
+		GOTO(out, error = acl_sz);
+	if (IS_ERR_OR_NULL(value))
+		GOTO(out, error = -ENOMEM);
+	if (acl_sz > len)
+		GOTO(out, error = -ERANGE);
+	error = acl_sz;
+	memcpy(buf, value, acl_sz);
+out:
+	kfree(value);
 	posix_acl_release(acl);
 
 	return error;

lustre/lustre-release

commit 062253ccce7d9ef20ca3f282e4e33beb8d1f87a7

LU-20133 build: fix includes in posix_acl_to_xattr conftest
On Linux 7.0, header cleanups mean that <linux/posix_acl.h> no
longer gets a complete struct inode definition transitively and
struct mnt_idmap is not declared in scope. Including only
<linux/posix_acl_xattr.h> thus fails to compile and the configure
test returns a false negative.

Fix this by including <linux/fs.h> in the conftest
HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER.

Test-Parameters: trivial
Fixes: f85a7cf4c8d3 ("LU-20133 build: Compatibility updates for kernel v7.0")
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I4f6b170b40b2c0f46ccf857eda1a0e725c71ec78
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66559
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

config/lustre-core.m4
@@ -3687,6 +3687,7 @@ AC_DEFUN([LC_HAVE_D_MAKE_PERSISTENT],[
 #
 AC_DEFUN([LC_SRC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER],[
 	LB2_LINUX_TEST_SRC([posix_acl_to_xattr], [
+		#include <linux/fs.h>
 		#include <linux/posix_acl_xattr.h>
 	],[
 		struct posix_acl *acl = NULL;