lustre/lustre-release

commit c6e12f54dc950916abb6db254a7f32d20abd0929

LU-18357 ptlrpc: Fix fileset llog compatibility on nodemaps
There are currently two issues related to old nodemap llog fileset
handling that can cause inconsistent state across servers during
nodemap synchronization:

(1) - IAM filesets are incorrectly migrated: When a nodemap is deleted
and recreated with the same name within a single IAM synchronization
step, the new nodemap keeps the fileset from the old nodemap. This
occurs because the "nmf_fileset_use_iam" flag defaults to 0,
triggering unnecessary fileset migration (used for llog filesets) even
for IAM filesets. The result is an inconsistent state where MGS has no
fileset while MDS/OSS retain the old fileset.

(2) - Stale llog filesets persist after nodemap deletion in the params
llog: When using llog-based filesets, deleting a nodemap without
explicitly removing its fileset causes the fileset to reappear when a
new nodemap is created with the same name. This is long-standing bug
and occurrs any time a fileset is set on a nodemap that has no IAM
nodemap defined.

This patch addresses both issues:
- For (1), "nmf_fileset_use_iam" is set to 1 by default for all newly
  created nodemaps. Note, nodemaps of older Lustre version with llog
  filesets keep 0 for this flag to allow backward-compatibility. Now,
  llog filesets can be set even if some IAM fileset was set before
  (this was not possible before). However, while IAM filesets can
  "overwrite" llog filesets, the opposite remains not possible.
- For (2), llog filesets are now automatically cleaned up from the
  params llog whenever a nodemap is deleted or when an IAM fileset is
  set (or cleared) via "lctl nodemap_set_fileset". This is done by
  internally triggering the equivalent of
  "lctl set_param -P -d nodemap.NODEMAP_NAME.fileset"
  to remove stale llog entries across all server targets.
  This is only done when we detect that a fileset was indeed set
  via the llog and whenever a fileset is explicitly cleared.

lustre/ptlrpc/nodemap_handler.c
@@ -1149,7 +1149,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nmf_readonly_mount = 0;
 		dst->nmf_rbac = NODEMAP_RBAC_ALL;
 		dst->nmf_deny_mount = 0;
-		dst->nmf_fileset_use_iam = 0;
+		dst->nmf_fileset_use_iam = 1;
 		dst->nmf_raise_privs = NODEMAP_RAISE_PRIV_NONE;
 		dst->nmf_rbac_raise = NODEMAP_RBAC_NONE;
 
@@ -1177,7 +1177,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nmf_readonly_mount = src->nmf_readonly_mount;
 		dst->nmf_rbac = src->nmf_rbac;
 		dst->nmf_deny_mount = src->nmf_deny_mount;
-		dst->nmf_fileset_use_iam = 0;
+		dst->nmf_fileset_use_iam = 1;
 		dst->nmf_raise_privs = src->nmf_raise_privs;
 		dst->nmf_rbac_raise = src->nmf_rbac_raise;
 		dst->nm_squash_uid = src->nm_squash_uid;
@@ -1401,9 +1401,39 @@ static void nodemap_fileset_reset(struct lu_nodemap *nodemap)
 }
 
 /**
+ * nodemap_update_fileset_iam_flag() - Update the "nmf_fileset_use_iam" flag and
+ * persist it to the nodemap IAM record (if called on the MGS).
+ *
+ * @nodemap: the nodemap to update
+ * @use_iam: the new value for the flag
+ *
+ * Return:
+ * * %0 on success
+ * * %negative on failure
+ */
+static int nodemap_update_fileset_iam_flag(struct lu_nodemap *nodemap,
+					  bool use_iam)
+{
+	int rc = 0;
+
+	if (nodemap->nmf_fileset_use_iam == use_iam)
+		return rc;
+
+	nodemap->nmf_fileset_use_iam = use_iam;
+	if (nodemap_mgs())
+		rc = nodemap_idx_nodemap_update(nodemap);
+
+	return rc;
+}
+
+/**
  * nodemap_set_fileset_iam() - Set a fileset on a nodemap
  * @nodemap: the nodemap to set fileset on
  * @fileset: string containing fileset
+ * @out_clean_llog_fileset: set to true if the llog fileset entry needs to be
+ * cleaned up. This is only set to true if a fileset exists, but
+ * "nmf_fileset_use_iam" is 0 meaning that the fileset might have been set
+ * through the params llog.
  *
  * Set a fileset on a nodemap in memory and the nodemap IAM records.
  * If the nodemap is dynamic, the nodemap IAM update is transparently skipped in
@@ -1415,7 +1445,8 @@ static void nodemap_fileset_reset(struct lu_nodemap *nodemap)
  * * %-EIO		undo operation failed during IAM update
  */
 static int nodemap_set_fileset_iam(struct lu_nodemap *nodemap,
-				   const char *fileset)
+				   const char *fileset,
+				   bool *out_clean_llog_fileset)
 {
 	size_t fileset_size_new;
 	char *fileset_new;
@@ -1423,8 +1454,11 @@ static int nodemap_set_fileset_iam(struct lu_nodemap *nodemap,
 
 	if (fileset[0] == '\0' || strcmp(fileset, "clear") == 0) {
 		rc = nodemap_idx_fileset_clear(nodemap);
-		if (!rc)
+		if (!rc) {
 			nodemap_fileset_reset(nodemap);
+			if (!nodemap->nm_dyn && out_clean_llog_fileset)
+				*out_clean_llog_fileset = true;
+		}
 		GOTO(out, rc);
 	}
 
@@ -1448,7 +1482,12 @@ static int nodemap_set_fileset_iam(struct lu_nodemap *nodemap,
 			rc = nodemap_idx_fileset_update(
 				nodemap, nodemap->nm_prim_fileset, fileset_new,
 				0);
+		} else {
+			rc = nodemap_idx_fileset_add(nodemap, fileset_new, 0);
+			if (!rc && !nodemap->nm_dyn && out_clean_llog_fileset)
+				*out_clean_llog_fileset = true;
 		}
+
 		if (!rc)
 			nodemap_fileset_reset(nodemap);
 	} else {
@@ -1463,17 +1502,17 @@ static int nodemap_set_fileset_iam(struct lu_nodemap *nodemap,
 	}
 
 out:
-	if (!nodemap->nm_dyn && !nodemap->nmf_fileset_use_iam && !rc) {
-		nodemap->nmf_fileset_use_iam = 1;
-		rc = nodemap_idx_nodemap_update(nodemap);
-	}
+	if (!nodemap->nm_dyn && !nodemap->nmf_fileset_use_iam && !rc)
+		rc = nodemap_update_fileset_iam_flag(nodemap, true);
 
 	return rc;
 }
 
 /**
- * nodemap_set_fileset_local() - Set a fileset on a nodemap. This is a local
- * operation and not persistent.
+ * nodemap_set_fileset_llog() - Set a fileset on a nodemap. This is a local
+ * operation, not persistent, and only called when running
+ * "lctl set_param nodemap.NAME.fileset=...".
+ *
  * @nodemap: the nodemap to set fileset on
  * @fileset: string containing fileset
  *
@@ -1481,15 +1520,22 @@ out:
  * the params llog, which caused "lctl set_param" to be called on
  * each server locally. For backward compatibility this functionality is kept.
  *
+ * This function should not be used for any other purpose.
+ *
  * Return:
  * * %0 on success
  * * %-EINVAL		invalid fileset: Does not start with '/'
  */
-static int nodemap_set_fileset_local(struct lu_nodemap *nodemap,
+static int nodemap_set_fileset_llog(struct lu_nodemap *nodemap,
 				     const char *fileset)
 {
 	size_t fileset_size_new;
 	char *fileset_new;
+	int rc;
+
+	/* Abort if the IAM is already in use and a fileset is set */
+	if (nodemap->nm_prim_fileset && nodemap->nmf_fileset_use_iam)
+		RETURN(-EINVAL);
 
 	/* Allow 'fileset=clear' in addition to 'fileset=""' to clear fileset
 	 * because either command 'lctl set_param -P *.*.fileset=""' or
@@ -1502,7 +1548,9 @@ static int nodemap_set_fileset_local(struct lu_nodemap *nodemap,
 	 */
 	if (fileset[0] == '\0' || strcmp(fileset, "clear") == 0) {
 		nodemap_fileset_reset(nodemap);
-		RETURN(0);
+		/* back to default value for use_iam flag */
+		rc = nodemap_update_fileset_iam_flag(nodemap, true);
+		RETURN(rc);
 	}
 
 	if (fileset[0] != '/')
@@ -1523,7 +1571,10 @@ static int nodemap_set_fileset_local(struct lu_nodemap *nodemap,
 	nodemap->nm_prim_fileset = fileset_new;
 	nodemap->nm_prim_fileset_size = fileset_size_new;
 
-	return 0;
+	/* Set fileset as llog fileset and update nodemap record */
+	rc = nodemap_update_fileset_iam_flag(nodemap, false);
+
+	return rc;
 }
 
 /**
@@ -1550,7 +1601,7 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
 		/* nodemap_set_fileset_iam() knows how to
 		 * handle a dynamic nodemap
 		 */
-		rc = nodemap_set_fileset_iam(dst, fileset);
+		rc = nodemap_set_fileset_iam(dst, fileset, NULL);
 	}
 
 	return rc;
@@ -1562,6 +1613,8 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
  * @fileset: string containing fileset
  * @checkperm: true if permission check is required
  * @ioctl_op: true if called from ioctl nodemap functions
+ * @out_clean_llog_fileset: set to true if the llog fileset entry needs to be
+ * cleaned up on the MGS side
  *
  * Return:
  * * %0 on success
@@ -1569,7 +1622,7 @@ static int nodemap_copy_fileset(struct lu_nodemap *dst, struct lu_nodemap *src)
  * * %-EINVAL		name or fileset is empty or NULL
  */
 int nodemap_set_fileset(const char *name, const char *fileset, bool checkperm,
-			bool ioctl_op)
+			bool ioctl_op, bool *out_clean_llog_fileset)
 {
 	struct lu_nodemap *nodemap = NULL;
 	int rc = 0;
@@ -1608,11 +1661,10 @@ int nodemap_set_fileset(const char *name, const char *fileset, bool checkperm,
 	 * both backends. Local updates are disabled once IAM records are used.
 	 */
 	if (ioctl_op)
-		rc = nodemap_set_fileset_iam(nodemap, fileset);
-	else if (!ioctl_op && !nodemap->nmf_fileset_use_iam)
-		rc = nodemap_set_fileset_local(nodemap, fileset);
+		rc = nodemap_set_fileset_iam(nodemap, fileset,
+					     out_clean_llog_fileset);
 	else
-		rc = -EINVAL;
+		rc = nodemap_set_fileset_llog(nodemap, fileset);
 
 out_unlock:
 	mutex_unlock(&active_config_lock);
@@ -2565,13 +2617,15 @@ EXPORT_SYMBOL(nodemap_add);
 /**
  * nodemap_del() - Delete a nodemap
  * @nodemap_name: name of nodemmap
+ * @out_clean_llog_fileset: set to true if the llog fileset entry needs to be
+ * cleaned up on the MGS side.
  *
  * Return:
  * * %0		success
  * * %-EINVAL		invalid input
  * * %-ENOENT		no existing nodemap
  */
-int nodemap_del(const char *nodemap_name)
+int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset)
 {
 	struct lu_nodemap	*nodemap;
 	struct lu_nid_range	*range;
@@ -2601,7 +2655,7 @@ int nodemap_del(const char *nodemap_name)
 			/* do our best and report any error on sub-nodemaps
 			 * but do not forward rc
 			 */
-			rc2 = nodemap_del(nm->nm_name);
+			rc2 = nodemap_del(nm->nm_name, NULL);
 			CDEBUG_LIMIT(D_INFO,
 				     "cannot del sub-nodemap %s: rc = %d\n",
 				     nm->nm_name, rc2);
@@ -2636,6 +2690,9 @@ int nodemap_del(const char *nodemap_name)
 			rc = rc2;
 
 		nodemap_fileset_reset(nodemap);
+
+		if (!nodemap->nm_dyn && !nodemap->nmf_fileset_use_iam)
+			*out_clean_llog_fileset = true;
 	}
 
 	rc2 = nodemap_idx_nodemap_del(nodemap);
@@ -3195,7 +3252,8 @@ int nodemap_test_id(struct lnet_nid *nid, enum nodemap_id_type idtype,
 EXPORT_SYMBOL(nodemap_test_id);
 
 static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
-			   char *param, bool dynamic)
+			   char *param, bool dynamic,
+			   bool *out_clean_llog_fileset)
 {
 	struct lnet_nid nid[2];
 	bool bool_switch;
@@ -3211,7 +3269,7 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
 		rc = nodemap_add(nodemap_name, dynamic);
 		break;
 	case LCFG_NODEMAP_DEL:
-		rc = nodemap_del(nodemap_name);
+		rc = nodemap_del(nodemap_name, out_clean_llog_fileset);
 		break;
 	case LCFG_NODEMAP_ADD_RANGE:
 		rc = nodemap_parse_range(param, nid, &netmask);
@@ -3458,7 +3516,8 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
 			rc = -EINVAL;
 		break;
 	case LCFG_NODEMAP_SET_FILESET:
-		rc = nodemap_set_fileset(nodemap_name, param, true, true);
+		rc = nodemap_set_fileset(nodemap_name, param, true, true,
+					 out_clean_llog_fileset);
 		break;
 	case LCFG_NODEMAP_SET_SEPOL:
 		rc = nodemap_set_sepol(nodemap_name, param, true);
@@ -3484,7 +3543,8 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
  * * %< 0 on error
  */
 int server_iocontrol_nodemap(struct obd_device *obd,
-			     struct obd_ioctl_data *data, bool dynamic)
+			     struct obd_ioctl_data *data, bool dynamic,
+			     bool *out_clean_llog_fileset)
 {
 	char name_buf[LUSTRE_NODEMAP_NAME_LENGTH + 1];
 	struct lustre_cfg *lcfg = NULL;
@@ -3545,7 +3605,8 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 		if (lcfg->lcfg_bufcount != 2)
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);
-		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic);
+		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic,
+				     out_clean_llog_fileset);
 		break;
 	case LCFG_NODEMAP_TEST_NID:
 		if (lcfg->lcfg_bufcount != 2)
@@ -3614,7 +3675,8 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);
 		param = lustre_cfg_string(lcfg, 2);
-		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic);
+		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic,
+				     out_clean_llog_fileset);
 		break;
 	case LCFG_NODEMAP_ADMIN:
 	case LCFG_NODEMAP_TRUSTED:
@@ -3633,7 +3695,7 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);
 		param = lustre_cfg_string(lcfg, 3);
-		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic);
+		rc = cfg_nodemap_cmd(cmd, nodemap_name, param, dynamic, NULL);
 		break;
 	default:
 		rc = -ENOTTY;

lustre/include/lustre_nodemap.h
@@ -174,7 +174,7 @@ struct nm_config_file {
 
 int nodemap_activate(const bool value);
 int nodemap_add(const char *nodemap_name, bool dynamic);
-int nodemap_del(const char *nodemap_name);
+int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset);
 int nodemap_add_member(struct lnet_nid *nid, struct obd_export *exp);
 void nodemap_del_member(struct obd_export *exp);
 int nodemap_parse_range(const char *range_string, struct lnet_nid range[2],
@@ -208,7 +208,7 @@ int nodemap_add_idmap(const char *nodemap_name, enum nodemap_id_type id_type,
 int nodemap_del_idmap(const char *nodemap_name, enum nodemap_id_type id_type,
 		      const __u32 map[2]);
 int nodemap_set_fileset(const char *name, const char *fileset, bool checkperm,
-			bool ioctl_op);
+			bool ioctl_op, bool *out_clean_llog_fileset);
 char *nodemap_get_fileset(const struct lu_nodemap *nodemap);
 int nodemap_set_sepol(const char *name, const char *sepol, bool checkperm);
 const char *nodemap_get_sepol(const struct lu_nodemap *nodemap);
@@ -230,8 +230,8 @@ int nodemap_test_id(struct lnet_nid *nid, enum nodemap_id_type idtype,
 		    u32 client_id, u32 *fs_id);
 
 int server_iocontrol_nodemap(struct obd_device *obd,
-			     struct obd_ioctl_data *data, bool dynamic);
-
+			     struct obd_ioctl_data *data, bool dynamic,
+			     bool *out_clean_llog_fileset);
 
 struct nm_config_file *nm_config_file_register_mgs(const struct lu_env *env,
 						   struct dt_object *obj,

lustre/include/uapi/linux/lustre/lustre_cfg.h
@@ -31,6 +31,13 @@
 #define LUSTRE_CFG_VERSION 0x1cf60001
 #define LUSTRE_CFG_MAX_BUFCOUNT 8
 
+/*
+ * Target name "general" calls on all targets
+ * when set at (struct) lustre_cfg_bufs[0].
+ * See lustre_cfg_bufs_set_string() and lustre_cfg_bufs_reset()
+ */
+#define LUSTRE_CFG_ALL_TARGETS "general"
+
 #define LCFG_HDR_SIZE(count) \
 	__ALIGN_KERNEL(offsetof(struct lustre_cfg, lcfg_buflens[(count)]), 8)
 

lustre/mdt/mdt_mds.c
@@ -654,7 +654,7 @@ static int mds_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 	if (cmd != OBD_IOC_NODEMAP)
 		GOTO(out, rc = -EINVAL);
 
-	rc = server_iocontrol_nodemap(obd, data, true);
+	rc = server_iocontrol_nodemap(obd, data, true, NULL);
 	if (rc)
 		GOTO(out, rc);
 

lustre/mgs/mgs_handler.c
@@ -817,11 +817,125 @@ static int mgs_extract_fs_pool(char *arg, char *fsname, char *poolname)
 	RETURN(0);
 }
 
+/**
+ * __llog_fileset_cleanup_apply() - Forge the lustre_cfg to disable
+ * nodemap.NM_NAME.fileset on all server targets by invoking "mgs_set_param()"
+ *
+ * @env: thread context
+ * @mgs: mgs device
+ * @nodemap_name: name of the nodemap to cleanup
+ *
+ * Return:
+ * * %0 on success
+ * * %-negative error code on failure
+ */
+static int __llog_fileset_cleanup_apply(const struct lu_env *env,
+					struct mgs_device *mgs,
+					const char *nodemap_name)
+{
+	struct lustre_cfg_bufs *bufs = NULL;
+	struct lustre_cfg *lcfg = NULL;
+	char *lcfg_param = NULL;
+	const char *lcfg_format;
+	size_t lcfg_param_size;
+	int rc = 0;
+
+	lcfg_format = "nodemap.%s.fileset=";
+	lcfg_param_size = snprintf(NULL, 0, lcfg_format, nodemap_name) + 1;
+
+	OBD_ALLOC(lcfg_param, lcfg_param_size);
+	if (!lcfg_param)
+		RETURN(-ENOMEM);
+
+	snprintf(lcfg_param, lcfg_param_size, lcfg_format, nodemap_name);
+
+	OBD_ALLOC_PTR(bufs);
+	if (!bufs)
+		GOTO(out_cleanup, rc = -ENOMEM);
+
+	/* lcfg for all targets */
+	lustre_cfg_bufs_reset(bufs, LUSTRE_CFG_ALL_TARGETS);
+	lustre_cfg_bufs_set_string(bufs, 1, lcfg_param);
+
+	OBD_ALLOC(lcfg, lustre_cfg_len(bufs->lcfg_bufcount, bufs->lcfg_buflen));
+	if (!lcfg)
+		GOTO(out_cleanup, rc = -ENOMEM);
+
+	lustre_cfg_init(lcfg, LCFG_SET_PARAM, bufs);
+
+	rc = mgs_set_param(env, mgs, lcfg);
+	if (rc == -ENOENT)
+		rc = 0;
+
+out_cleanup:
+	if (lcfg)
+		OBD_FREE(lcfg, lustre_cfg_len(lcfg->lcfg_bufcount,
+					      lcfg->lcfg_buflens));
+	/* lustre_cfg maintains its own buffers */
+	OBD_FREE_PTR(bufs);
+	OBD_FREE(lcfg_param, lcfg_param_size);
+
+	return rc;
+}
+
+/**
+ * mgs_llog_fileset_cleanup() - Cleanup the fileset entry from the params
+ * llog on all server targets
+ *
+ * @env: thread context
+ * @mgs: mgs device
+ * @data: ioctl data containing the nodemap name
+ *
+ * This function is necessary to provide backward compatibility with old
+ * clients versions that still use the params llog to set the fileset. If
+ * the new ioctl based nodemap fileset functions are used, the llog entry
+ * would not be removed, and could re-appear for a new similar named nodemap.
+ *
+ * Return:
+ * * %0 on success
+ * * %-negative error code on failure
+ */
+static int mgs_llog_fileset_cleanup(const struct lu_env *env,
+				    struct mgs_device *mgs,
+				    struct obd_ioctl_data *data)
+{
+	struct lustre_cfg *lcfg_in;
+	char *nodemap_name;
+	int rc;
+
+	if (data->ioc_plen1 > PAGE_SIZE)
+		GOTO(out, rc = -E2BIG);
+
+	OBD_ALLOC(lcfg_in, data->ioc_plen1);
+	if (!lcfg_in)
+		GOTO(out, rc = -ENOMEM);
+
+	if (copy_from_user(lcfg_in, data->ioc_pbuf1, data->ioc_plen1))
+		GOTO(out_cleanup, rc = -EFAULT);
+	if (lustre_cfg_sanity_check(lcfg_in, data->ioc_plen1))
+		GOTO(out_cleanup, rc = -EINVAL);
+
+	if (lcfg_in->lcfg_bufcount < 2)
+		GOTO(out_cleanup, rc = -EINVAL);
+
+	nodemap_name = lustre_cfg_string(lcfg_in, 1);
+	rc = __llog_fileset_cleanup_apply(env, mgs, nodemap_name);
+	if (rc)
+		CWARN("%s: failed to cleanup llog fileset for nodemap %s: %d\n",
+		      mgs->mgs_obd->obd_name, nodemap_name, rc);
+
+out_cleanup:
+	OBD_FREE(lcfg_in, data->ioc_plen1);
+out:
+	return rc;
+}
+
 static int mgs_iocontrol_nodemap(const struct lu_env *env,
 				 struct mgs_device *mgs,
 				 struct obd_ioctl_data *data)
 {
 	struct fs_db *fsdb;
+	bool clean_llog_fileset = false;
 	int rc;
 
 	ENTRY;
@@ -832,9 +946,24 @@ static int mgs_iocontrol_nodemap(const struct lu_env *env,
 		GOTO(out, rc = -EINVAL);
 	}
 
-	rc = server_iocontrol_nodemap(mgs->mgs_obd, data, false);
+	rc = server_iocontrol_nodemap(mgs->mgs_obd, data, false,
+				      &clean_llog_fileset);
 	if (rc)
 		GOTO(out, rc);
+
+	/* A llog fileset entry might still exist and needs to be removed */
+	if (clean_llog_fileset) {
+		int rc2;
+
+		/* Attempt to clean up the llog fileset and provide a warning.
+		 * It is not serious enough to fail the original request which
+		 * was already applied on the MGS above.
+		 */
+		rc2 = mgs_llog_fileset_cleanup(env, mgs, data);
+		if (rc2)
+			CWARN("%s: failed to cleanup llog fileset: %d\n",
+			      mgs->mgs_obd->obd_name, rc2);
+	}
 
 	/* revoke nodemap lock */
 	rc = mgs_find_or_make_fsdb(env, mgs, LUSTRE_NODEMAP_NAME, &fsdb);

lustre/ofd/ofd_oss.c
@@ -416,7 +416,7 @@ static int oss_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
 	if (cmd != OBD_IOC_NODEMAP)
 		GOTO(out, rc = -EINVAL);
 
-	rc = server_iocontrol_nodemap(obd, data, true);
+	rc = server_iocontrol_nodemap(obd, data, true, NULL);
 	if (rc)
 		GOTO(out, rc);
 

lustre/ptlrpc/nodemap_lproc.c
@@ -338,7 +338,7 @@ nodemap_fileset_seq_write(struct file *file,
 	if (copy_from_user(nm_fileset, buffer, count))
 		GOTO(out, rc = -EFAULT);
 
-	rc = nodemap_set_fileset(m->private, nm_fileset, false, false);
+	rc = nodemap_set_fileset(m->private, nm_fileset, false, false, NULL);
 	if (rc != 0)
 		GOTO(out, rc = -EINVAL);
 

lustre/tests/sanity-sec.sh
@@ -2214,6 +2214,17 @@ nodemap_exercise_fileset_cleanup() {
 	done
 }
 
+stopall_restart_servers() {
+	stopall || error "unable to stop"
+	LOAD_MODULES_REMOTE=true unload_modules ||
+		error "unable to unload modules"
+	LOAD_MODULES_REMOTE=true load_modules ||
+		error "unable to load modules"
+	mountmgs || error "unable to start mgs"
+	mountmds || error "unable to start mds"
+	mountoss || error "unable to start oss"
+}
+
 nodemap_exercise_fileset() {
 	local have_persistent_fset_cmd
 	local check_proj=true
@@ -2294,15 +2305,7 @@ nodemap_exercise_fileset() {
 	fi
 
 	# re-start all components to verify persistence of fileset after restart
-	stopall || error "unable to stop"
-	# Unload modules to fully reload nodemap IAM
-	LOAD_MODULES_REMOTE=true unload_modules ||
-		error "unable to unload modules"
-	LOAD_MODULES_REMOTE=true load_modules ||
-		error "unable to load modules"
-	mountmgs || error "unable to start mgs"
-	mountmds || error "unable to start mds"
-	mountoss || error "unable to start oss"
+	stopall_restart_servers
 
 	stack_trap nodemap_exercise_fileset_cleanup EXIT
 
@@ -2739,6 +2742,104 @@ test_27ab() { #LU-18109
 }
 run_test 27ab "test nodemap idmap offset"
 
+cleanup_27ac() {
+	local nm=$1
+	do_facet mgs $LCTL nodemap_del $nm
+	wait_nm_sync $nm id ''
+	# restart clients
+	nodemap_exercise_fileset_cleanup
+	wait_ssk
+}
+
+test_27ac() {
+	local nm="test27ac_nm"
+	local fileset_llog="/llog_fileset"
+	local fileset_iam="/iam_fileset"
+
+	(( MDS1_VERSION >= $(version_code 2.16.56) )) ||
+		skip "Need OSS >= 2.16.56 llog fileset compatibility"
+
+	if [[ $(facet_active_host mgs) == $(facet_active_host mds) &&
+	      $(facet_active_host mgs) == $(facet_active_host ost1) ]]; then
+		skip "local mode not supported"
+	fi
+
+	do_facet mgs $LCTL nodemap_add $nm || error "unable to add $nm nodemap"
+
+	stack_trap "cleanup_27ac $nm" EXIT
+
+	# set iam fileset
+	do_facet mgs $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset_iam ||
+		error "unable to set fileset $fileset_iam on $nm"
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=$iam_fileset" ||
+		error "fileset $iam_fileset not synced"
+
+	# quickly delete and re-add nodemap in one synchronization step
+	do_facet mgs "$LCTL nodemap_del $nm ; $LCTL nodemap_add $nm" ||
+		error "unable to del and add $nm nodemap"
+	# 1. IAM fileset should be removed
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=" ||
+		error "fileset not cleared"
+
+	# set llog fileset
+	do_facet mgs $LCTL set_param nodemap.$nm.fileset=$fileset_llog ||
+		error "unable to set fileset $fileset_llog on $nm locally"
+	do_facet mgs $LCTL set_param -P nodemap.$nm.fileset=$fileset_llog ||
+		error "unable to set fileset $fileset_llog on $nm persistently"
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=$fileset_llog" ||
+		error "fileset $fileset_llog not synced"
+
+	# re-start all components to verify persistence of fileset after restart
+	stopall_restart_servers
+
+	# 2. Verify llog fileset is still set
+	wait_update_facet mds "$LCTL get_param nodemap.${nm}.fileset" \
+		"nodemap.${nm}.fileset=$fileset_llog" ||
+		error "fileset $fileset_llog shouldn't be removed after restart"
+
+	# overwrite llog fileset with iam fileset
+	do_facet mgs $LCTL nodemap_set_fileset --name $nm \
+		--fileset $fileset_iam ||
+		error "unable to set fileset $fileset_iam on $nm"
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=$fileset_iam" ||
+		error "fileset $fileset_iam not synced"
+
+	# 3. setting llog fileset should be denied. We omit set_param -P since
+	# it follows the same logic but can't be verified as the error is
+	# happens on the other server nodes which is not returned to the mgs
+	do_facet mgs $LCTL set_param nodemap.$nm.fileset=$fileset_llog &&
+		error "should not be able to set fileset $fileset_llog on $nm"
+
+	# reset fileset (allows setting an llog fileset)
+	do_facet mgs $LCTL nodemap_set_fileset --name $nm --fileset clear ||
+		error "unable to clear fileset on $nm"
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=" ||
+		error "fileset not cleared"
+
+	# 4. Restart and verify fileset is still cleared
+	stopall_restart_servers
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=" ||
+		error "fileset not cleared"
+
+	# 5. Verify fileset can be set
+	do_facet mgs $LCTL set_param nodemap.$nm.fileset=$fileset_llog ||
+		error "unable to set fileset $fileset_llog on $nm locally"
+	do_facet mgs $LCTL set_param -P nodemap.$nm.fileset=$fileset_llog ||
+		error "unable to set fileset $fileset_llog on $nm persistently"
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=$fileset_llog" ||
+		error "fileset $fileset_llog not synced"
+
+	# delete and re-add nodemap
+	do_facet mgs "$LCTL nodemap_del $nm ; $LCTL nodemap_add $nm" ||
+		error "unable to del and add $nm nodemap"
+	# 6. llog fileset should be removed
+	wait_nm_sync $nm fileset "nodemap.${nm}.fileset=" ||
+		error "fileset not cleared"
+
+}
+run_test 27ac "test nodemap llog and IAM fileset compatibility"
+
 test_27b() { #LU-10703
 	[ "$MDS1_VERSION" -lt $(version_code 2.11.50) ] &&
 		skip "Need MDS >= 2.11.50"
@@ -2761,14 +2862,17 @@ test_27b() { #LU-10703
 			error "add nodemap nm$i failed"
 		wait_nm_sync nm$i "" "" "-N"
 
-		if ! combined_mgs_mds; then
-			do_facet mgs \
-				$LCTL set_param nodemap.nm$i.fileset=/dir$i ||
-				error "set nm$i.fileset=/dir$i failed on MGS"
-		fi
+		do_facet mgs \
+			$LCTL set_param nodemap.nm$i.fileset=/dir$i ||
+			error "set nm$i.fileset=/dir$i failed on MGS"
 		do_facet mgs $LCTL set_param -P nodemap.nm$i.fileset=/dir$i ||
 			error "set nm$i.fileset=/dir$i failed on servers"
 		wait_nm_sync nm$i fileset "nodemap.nm$i.fileset=/dir$i"
+		# set a property to check that the nodemap have been synced
+		# as the sync disables the set_iam flag for llog filesets
+		do_facet mgs $LCTL nodemap_modify --name nm$i \
+			--property admin --value 1 || error "set admin failed"
+		wait_nm_sync nm$i admin_nodemap
 	done
 
 	# Check if all the filesets are correct

commit 0cf0f8d9e920e5f60d1cc0d02c78c446ebb5d41e

LU-20272 nodemap: fix NULL deref in nodemap_del()
nodemap_del() can be called with out_clean_llog_fileset=NULL, but
the pointer is dereferenced without a NULL guard. Add the guard,
consistent with other out_clean_llog_fileset uses in the file.

Fixes: c6e12f54dc95 ("LU-18357 ptlrpc: Fix fileset llog compatibility on nodemaps")
Fixes: 40cd140b8597 ("LU-18357 ptlrpc: Support add and del alternate filesets")
Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: I165f672b868852916bf7b3ecf1a5a64bdadd2ff7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65951
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/ptlrpc/nodemap_handler.c
@@ -4436,7 +4436,7 @@ int nodemap_del(const char *nodemap_name, bool *out_clean_llog_fileset)
 		rc = rc2;
 
 	if (fileset_prim_exists && !rc && !nodemap->nmf_fileset_use_iam &&
-	    !nodemap->nm_dyn)
+	    !nodemap->nm_dyn && out_clean_llog_fileset)
 		*out_clean_llog_fileset = true;
 
 	rc2 = nodemap_idx_nodemap_del(nodemap);