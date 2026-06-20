lustre/lustre-release

commit 635421641bfa8a412edebd4ffbbdf05fd0c47410

LU-5349 include: use __vmalloc() to avoid __GFP_FS default
When possible, try to Direct use of __vmalloc() which allows
for protection flag specification, and particularly to not
set __GFP_FS, which can cause some deadlock situations in our
code.

Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
Change-Id: I25fc4a55374c09c12156b2a2f8d8b2434b4cd689
Reviewed-on: http://review.whamcloud.com/11190
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: James Simmons <uja.ornl@gmail.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/include/obd_support.h
@@ -703,10 +703,15 @@ do {									      \
 #define OBD_CPT_ALLOC_PTR(ptr, cptab, cpt)				      \
 	OBD_CPT_ALLOC(ptr, cptab, cpt, sizeof *(ptr))
 
+/* Direct use of __vmalloc() allows for protection flag specification
+ * (and particularly to not set __GFP_FS, which is likely to cause some
+ * deadlock situations in our code).
+ */
 # define __OBD_VMALLOC_VERBOSE(ptr, cptab, cpt, size)			      \
 do {									      \
 	(ptr) = cptab == NULL ?						      \
-		vzalloc(size) :					      \
+		__vmalloc(size, GFP_NOFS | __GFP_HIGHMEM | __GFP_ZERO,	      \
+			  PAGE_KERNEL) :				      \
 		cfs_cpt_vzalloc(cptab, cpt, size);			      \
 	if (unlikely((ptr) == NULL)) {                                        \
 		CERROR("vmalloc of '" #ptr "' (%d bytes) failed\n",           \

libcfs/libcfs/linux/linux-mem.c
@@ -53,6 +53,14 @@ EXPORT_SYMBOL(cfs_cpt_malloc);
 void *
 cfs_cpt_vzalloc(struct cfs_cpt_table *cptab, int cpt, size_t nr_bytes)
 {
+	/* vzalloc_node() sets __GFP_FS by default but no current Kernel
+	 * exported entry-point allows for both a NUMA node specification
+	 * and a custom allocation flags mask. This may be an issue since
+	 * __GFP_FS usage can cause some deadlock situations in our code,
+	 * like when memory reclaim started, within the same context of a
+	 * thread doing FS operations, that can also attempt conflicting FS
+	 * operations, ...
+	 */
 	return vzalloc_node(nr_bytes, cfs_cpt_spread_node(cptab, cpt));
 }
 EXPORT_SYMBOL(cfs_cpt_vzalloc);

commit ed3196b02d275c1965db20c8f768ff19d4135f45

LU-20335 misc: remove __GFP_HIGHMEM from __vmalloc() callers
Linux commit 07003531e037 ("mm/vmalloc: warn on invalid vmalloc gfp
flags") enforces the set of gfp flags the vmalloc family accepts. Any
unsupported flag now triggers a warning and is stripped:

    +static gfp_t vmalloc_fix_flags(gfp_t flags)
    +{
    +	gfp_t invalid_mask = flags & ~GFP_VMALLOC_SUPPORTED;
    +
    +	flags &= GFP_VMALLOC_SUPPORTED;
    +	WARN_ONCE(1, "Unexpected gfp: %#x (%pGg). Fixing up to gfp: %#x (%pGg). Fix your code!\n",
    +		  invalid_mask, &invalid_mask, flags, &flags);
    +	return flags;
    +}

__GFP_HIGHMEM is not part of GFP_VMALLOC_SUPPORTED, so update various
__vmalloc() callers to stop passing this flag. __vmalloc will use it
automatically since 19809c2da28a ("mm, vmalloc: use __GFP_HIGHMEM
implicitly") which is present in all kernels (v4.12+) that Lustre
supports.

Test-Parameters: trivial
Fixes: 635421641bfa ("LU-5349 include: use __vmalloc() to avoid __GFP_FS default")
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I917d4c8a27c9754b5393fe1bd9c84a09ae2d74c9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66508
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/klnds/gnilnd/gnilnd.h
@@ -994,10 +994,10 @@ static inline void *kgnilnd_vzalloc(int size)
 {
 	void *ret;
 	if (*kgnilnd_tunables.kgn_vzalloc_noretry)
-		ret = __compat_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO |
+		ret = __compat_vmalloc(size, GFP_NOIO |
 				       __GFP_ZERO | __GFP_NORETRY);
 	else
-		ret = __compat_vmalloc(size, __GFP_HIGHMEM | GFP_NOIO |
+		ret = __compat_vmalloc(size, GFP_NOIO |
 				       __GFP_ZERO);
 
 	LIBCFS_ALLOC_POST(ret, size, "alloc");

lustre/include/obd_support.h
@@ -883,7 +883,7 @@ do {									      \
 #define __OBD_VMALLOC_VERBOSE(ptr, cptab, cpt, size)			      \
 do {									      \
 	(ptr) = cptab == NULL ?						      \
-		__compat_vmalloc(size, GFP_NOFS | __GFP_HIGHMEM | __GFP_ZERO) :\
+		__compat_vmalloc(size, GFP_NOFS | __GFP_ZERO) :		      \
 		cfs_cpt_vzalloc(cptab, cpt, size);			      \
 	if (unlikely((ptr) == NULL)) {                                        \
 		CERROR("vmalloc of '" #ptr "' (%d bytes) failed\n",           \