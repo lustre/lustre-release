Cray/lustre

commit 20527615330ea474044d81917d3f9a7fd2717880

LU-19837 build: Compatibility updates for kernel v6.19
Linux commit v6.18-rc1-6-ge12d203b8c880
  vfs: allow mkdir to wait for delegation break on parent
Provide a compatible vfs_mkdir for older kernels

Linux commit v6.18-rc1-7-gd8753f788ab49
  fs: provide accessors for ->i_state
Provide a compatible inode_state_read() for older kernels

Linux commit v6.18-rc1-10-gc826229c6a82f
  vfs: make vfs_create break delegations on parent directory
Provide a compatible vfs_create for older kernels

Linux commit v6.18-rc1-18-gb37491d72b43c
  interval_tree: Fix ITSTATIC usage for *_subtree_search()
Provide an alternate configure test for INTERVAL_TREE_DEFINE

Linux commit v6.18-rc1-20-ga27628f436343
  fs: rework I_NEW handling to operate without fences
Provide a compatible ilookup5_nowait for older kernels

Linux commit v6.18-rc2-1-g70e0a80a1f358
  treewide: Remove in_irq()
Use in_hardirq() directly

Linux commit v6.18-rc3-603-gbf33247a90d3e
  net: Add struct sockaddr_unsized for sockaddr of unknown length
Provide a mapping of sockaddr_unsized to sockaddr for older kernels

Linux commit v6.18-rc5-51-gfc45aee662232
  get rid of kill_litter_super()
Use kill_anon_super() when kill_litter_super() not available

Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I45e8df243829e03c271de8c159a3f2416848f43a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63685
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-core.m4
@@ -3500,6 +3500,115 @@ AC_DEFUN([LC_HAVE_FILE__F_PATH],[
 ]) # LC_HAVE_FILE__F_PATH
 
 #
+## LC_HAVE_VFS_MKDIR_DELEGATE
+#
+# Linux commit v6.18-rc1-6-ge12d203b8c880
+#   vfs: allow mkdir to wait for delegation break on parent
+#
+AC_DEFUN([LC_SRC_HAVE_VFS_MKDIR_DELEGATE],[
+	LB2_LINUX_TEST_SRC([vfs_mkdir_delegate], [
+		#include <linux/fs.h>
+	],[
+		struct mnt_idmap *idmap = NULL;
+		struct inode *dir = NULL;
+		struct dentry *dentry = NULL;
+		umode_t mode = 0;
+		struct delegated_inode *di = NULL;
+		struct dentry *result = vfs_mkdir(idmap, dir, dentry, mode, di);
+
+		(void)result;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_VFS_MKDIR_DELEGATE],[
+	LB2_MSG_LINUX_TEST_RESULT([if vfs_mkdir() takes delegate],
+	[vfs_mkdir_delegate], [
+		AC_DEFINE(HAVE_VFS_MKDIR_DELEGATE, 1,
+			  [vfs_mkdir() takes delegate])
+	])
+]) # LC_HAVE_VFS_MKDIR_DELEGATE
+
+#
+## LC_HAVE_INODE_STATE_READ
+#
+# Linux commit v6.18-rc1-7-gd8753f788ab49
+#   fs: provide accessors for ->i_state
+#
+AC_DEFUN([LC_SRC_HAVE_INODE_STATE_READ],[
+	LB2_LINUX_TEST_SRC([inode_state_read], [
+		#include <linux/fs.h>
+	],[
+		struct inode *inode = NULL;
+		enum inode_state_flags_enum state = inode_state_read(inode);
+
+		(void)state;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_INODE_STATE_READ],[
+	LB2_MSG_LINUX_TEST_RESULT([if inode_state_read() exists],
+	[inode_state_read], [
+		AC_DEFINE(HAVE_INODE_STATE_READ, 1,
+			  [inode_state_read() exists])
+	], [
+		AC_DEFINE([inode_state_read(inode)], [((inode)->i_state)],
+			  [inode_state_read() does not exist, provide one])
+	])
+]) # LC_HAVE_INODE_STATE_READ
+
+#
+## LC_HAVE_VFS_CREATE_DELEGATE
+#
+# Linux commit v6.18-rc1-10-gc826229c6a82f
+#   vfs: make vfs_create break delegations on parent directory
+#
+AC_DEFUN([LC_SRC_HAVE_VFS_CREATE_DELEGATE],[
+	LB2_LINUX_TEST_SRC([vfs_create_delegate], [
+		#include <linux/fs.h>
+	],[
+		struct mnt_idmap *idmap = NULL;
+		struct dentry *dentry = NULL;
+		umode_t mode = 0;
+		struct delegated_inode *di = NULL;
+		int rc = vfs_create(idmap, dentry, mode, di);
+		(void)rc;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_VFS_CREATE_DELEGATE],[
+	LB2_MSG_LINUX_TEST_RESULT([if vfs_create() takes delegate],
+	[vfs_create_delegate], [
+		AC_DEFINE(HAVE_VFS_CREATE_DELEGATE, 1,
+			  [vfs_create() takes delegate])
+	])
+]) # LC_HAVE_VFS_CREATE_DELEGATE
+
+#
+## LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+#
+# Linux commit v6.18-rc1-20-ga27628f436343
+#   fs: rework I_NEW handling to operate without fences
+#
+AC_DEFUN([LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
+	LB2_LINUX_TEST_SRC([ilookup5_nowait_isnew], [
+		#include <linux/fs.h>
+
+		static int test(struct inode *inode, void *data) { return 0; }
+	],[
+		struct super_block *sb = NULL;
+		bool isnew;
+		struct inode *node = ilookup5_nowait(sb, 0, test, NULL, &isnew);
+
+		(void)node;
+		(void)isnew;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
+	LB2_MSG_LINUX_TEST_RESULT([if ilookup5_nowait() takes isnew],
+	[ilookup5_nowait_isnew], [
+		AC_DEFINE(HAVE_ILOOKUP5_NOWAIT_ISNEW, 1,
+			  [ilookup5_nowait() takes isnew])
+	])
+]) # LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+
+#
 # LC_PROG_LINUX
 #
 # Lustre linux kernel checks
@@ -3698,6 +3807,12 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_MEMDESC_FLAGS_T
 	LC_SRC_HAVE_DENTRY__D_NAME
 	LC_SRC_HAVE_FILE__F_PATH
+
+	# 6.19
+	LC_SRC_HAVE_VFS_MKDIR_DELEGATE
+	LC_SRC_HAVE_INODE_STATE_READ
+	LC_SRC_HAVE_VFS_CREATE_DELEGATE
+	LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 ])
 
 AC_DEFUN([LC_PROG_LINUX_RESULTS], [
@@ -3909,6 +4024,12 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_MEMDESC_FLAGS_T
 	LC_HAVE_DENTRY__D_NAME
 	LC_HAVE_FILE__F_PATH
+
+	# 6.19
+	LC_HAVE_VFS_MKDIR_DELEGATE
+	LC_HAVE_INODE_STATE_READ
+	LC_HAVE_VFS_CREATE_DELEGATE
+	LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 ])
 
 #

include/lustre_compat/linux/fs.h
@@ -34,6 +34,49 @@
 #define nop_mnt_idmap   init_user_ns
 #endif
 
+#if !defined(HAVE_VFS_CREATE_DELEGATE)
+#if !defined(HAVE_USER_NAMESPACE_ARG) && !defined(HAVE_MNT_IDMAP_ARG)
+#define vfs_create(ns, de, mode, di)	\
+	vfs_create(d_inode((de)->d_parent), (de), (mode), !!(di))
+#else
+#define vfs_create(ns, de, mode, di) \
+	vfs_create((ns), d_inode((de)->d_parent), (de), (mode), !!(di))
+#endif
+#endif /* HAVE_VFS_CREATE_DELEGATE */
+
+#ifdef HAVE_VFS_MKDIR_DELEGATE
+#define VFS_MKDIR_DELEGATE(id, inode, dentry, mode) \
+	vfs_mkdir((id), (inode), (dentry), (mode), NULL)
+#else
+#define VFS_MKDIR_DELEGATE(id, inode, dentry, mode) \
+	vfs_mkdir((id), (inode), (dentry), (mode))
+#endif /* HAVE_VFS_MKDIR_DELEGATE */
+
+#ifdef HAVE_IOPS_MKDIR_RETURNS_DENTRY
+#define ll_vfs_mkdir(id, inode, dentry, mode)	\
+	VFS_MKDIR_DELEGATE((id), (inode), (dentry), (mode))
+#else
+#define ll_vfs_mkdir(i, inode, dentry, mode) ({				\
+	int rc = VFS_MKDIR_DELEGATE((i), (inode), (dentry), (mode));	\
+	if (rc) {							\
+		dput((dentry));						\
+		dentry = ERR_PTR(rc);					\
+	}								\
+	(dentry);							\
+})
+#endif /* HAVE_IOPS_MKDIR_RETURNS_DENTRY */
+
+static inline int ll_vfs_getattr(struct path *path, struct kstat *st,
+				 u32 request_mask, unsigned int flags)
+{
+#ifdef AT_GETATTR_NOSEC /* added in v6.7-rc1-1-g8a924db2d7b5 */
+	if (flags & AT_GETATTR_NOSEC)
+		return vfs_getattr_nosec(path, st, request_mask, flags);
+#endif /* AT_GETATTR_NOSEC */
+
+	return vfs_getattr(path, st, request_mask, flags);
+}
+
 #ifndef HAVE_INODE_JUST_DROP
 static inline int inode_just_drop(struct inode *inode)
 {
@@ -44,6 +87,11 @@ static inline int inode_generic_drop(struct inode *inode)
 {
 	return generic_drop_inode(inode);
 }
+#endif
+
+#ifndef HAVE_ILOOKUP5_NOWAIT_ISNEW
+#define ilookup5_nowait(sb, hash, fn, data, isnew) \
+	ilookup5_nowait((sb), (hash), (fn), (data))
 #endif
 
 #endif /* __LIBCFS_LINUX_CFS_FS_H__ */

lustre/osd-wbcfs/osd_object.c
@@ -579,7 +579,7 @@ static int __osd_create(const struct lu_env *env, struct osd_object *obj,
 	result = osd_create_type_f(dof->dof_type)(env, obj, attr, hint, dof,
 						  th);
 	if (likely(obj->oo_inode && result == 0)) {
-		LASSERT(obj->oo_inode->i_state & I_NEW);
+		LASSERT(inode_state_read(obj->oo_inode) & I_NEW);
 
 		/*
 		 * Unlock the inode before attr initialization to avoid

lustre/osd-wbcfs/wbcfs.c
@@ -1053,11 +1053,16 @@ static const struct address_space_operations memfs_aops = {
 	.write_end	= memfs_write_end,
 };
 
+static void memfs_kill_super(struct super_block *sb)
+{
+	kill_anon_super(sb);
+}
+
 static struct file_system_type memfs_fstype = {
 	.owner			= THIS_MODULE,
 	.name			= "wbcfs",
 	.init_fs_context	= memfs_init_fs_context,
-	.kill_sb		= kill_litter_super,
+	.kill_sb		= memfs_kill_super,
 	.fs_flags		= FS_USERNS_MOUNT,
 };
 

config/lustre-lnet.m4
@@ -1343,6 +1343,33 @@ AC_DEFUN([LN_CONFIG_SENDPAGE_OK], [
 	])
 ]) # LN_CONFIG_SENDPAGE_OK
 
+#
+## LN_HAVE_STRUCT_SOCKADDR_UNSIZED
+#
+# Linux commit v6.18-rc3-603-gbf33247a90d3e
+#   net: Add struct sockaddr_unsized for sockaddr of unknown length
+#
+AC_DEFUN([LN_SRC_HAVE_STRUCT_SOCKADDR_UNSIZED],[
+	LB2_LINUX_TEST_SRC([struct_sockaddr_unsized], [
+		#include <linux/socket.h>
+		#include <linux/net.h>
+	],[
+		struct sockaddr_unsized *addr = NULL;
+
+		(void)kernel_bind(NULL, addr, 0);
+	],[-Werror])
+])
+AC_DEFUN([LN_HAVE_STRUCT_SOCKADDR_UNSIZED],[
+	LB2_MSG_LINUX_TEST_RESULT([if struct sockaddr_unsized exists],
+	[struct_sockaddr_unsized], [
+		AC_DEFINE(HAVE_STRUCT_SOCKADDR_UNSIZED, 1,
+			  [struct sockaddr_unsized exists])
+	], [
+		AC_DEFINE([sockaddr_unsized], [sockaddr],
+			  [struct sockaddr_unsized does not exist])
+	])
+]) # LN_HAVE_STRUCT_SOCKADDR_UNSIZED
+
 AC_DEFUN([LN_PROG_LINUX_SRC], [
 	LN_CONFIG_O2IB_SRC
 	# 4.x
@@ -1363,6 +1390,8 @@ AC_DEFUN([LN_PROG_LINUX_SRC], [
 	LN_SRC_CONFIG_SENDPAGE_OK
 	# 6.15
 	LN_SRC_HAVE_NETDEV_LOCK_OPS
+	# 6.19
+	LN_SRC_HAVE_STRUCT_SOCKADDR_UNSIZED
 ])
 
 AC_DEFUN([LN_PROG_LINUX_RESULTS], [
@@ -1385,6 +1414,8 @@ AC_DEFUN([LN_PROG_LINUX_RESULTS], [
 	LN_CONFIG_SENDPAGE_OK
 	# 6.15
 	LN_HAVE_NETDEV_LOCK_OPS
+	# 6.19
+	LN_HAVE_STRUCT_SOCKADDR_UNSIZED
 ])
 
 #

include/linux/libcfs/libcfs.h
@@ -107,4 +107,11 @@ do {									\
 	"Struct contains a flexible member, the size of object is checked" \
 	"and can be safely copied in a single memcpy()"
 
+/* Linux commit v6.18-rc2-1-g70e0a80a1f358
+ *   treewide: Remove in_irq()
+ */
+#ifndef in_hardirq
+#define in_hardirq()	in_irq()
+#endif
+
 #endif /* _LIBCFS_LIBCFS_H_ */

include/lustre_compat/linux/dcache.h
@@ -1,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ */
+
+#ifndef __LIBCFS_LINUX_DCACHE_H__
+#define __LIBCFS_LINUX_DCACHE_H__
+
+#include <linux/dcache.h>
+
+#ifndef QSTR
+#define QSTR(name) QSTR_LEN((name), strlen((name)))
+#endif
+
+#ifndef QSTR_LEN
+#define QSTR_LEN(name, len) ((struct qstr)QSTR_INIT((name), (len)))
+#endif
+
+#endif /* __LIBCFS_LINUX_DCACHE_H__ */

libcfs/libcfs/tracefile.c
@@ -98,7 +98,7 @@ static void cfs_trace_unlock_tcd(struct cfs_trace_cpu_data *tcd, int walking)
 
 static enum cfs_trace_buf_type cfs_trace_buf_idx_get(void)
 {
-	if (in_irq())
+	if (in_hardirq())
 		return CFS_TCD_TYPE_IRQ;
 	if (in_softirq())
 		return CFS_TCD_TYPE_SOFTIRQ;

lustre/lustre-release

commit 586fdd80e65dd1f8d33b86786afd42028d995408

LU-20251 osd-wbcfs: fix dentry pinning
Swapping kill_litter_super() to kill_anon_super() removes the
implicit d_genocide() that cleans up stale dentry during umount.
This causes warnings and hangs during umount. Refactor osd-wbcfs
to handle these changes better.

Fixes: 20527615330e ("LU-19837 build: Compatibility updates for kernel v6.19")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Ic70a2959a0ad20c9b3ef1a2fbcbf6a66b87ec2be
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65838
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

config/lustre-core.m4
@@ -3632,6 +3632,54 @@ AC_DEFUN([LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA],[
 ]) # LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
 
 #
+## LC_HAVE_KILL_LITTER_SUPER
+#
+# Linux commit v6.18-rc5-51-gfc45aee662232
+#   get rid of kill_litter_super()
+#
+AC_DEFUN([LC_SRC_HAVE_KILL_LITTER_SUPER],[
+	LB2_LINUX_TEST_SRC([kill_litter_super], [
+		#include <linux/fs.h>
+	],[
+		struct super_block *sb = NULL;
+
+		kill_litter_super(sb);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_KILL_LITTER_SUPER],[
+	LB2_MSG_LINUX_TEST_RESULT([if kill_litter_super() exists],
+	[kill_litter_super], [
+		AC_DEFINE(HAVE_KILL_LITTER_SUPER, 1,
+			  [kill_litter_super() exists])
+	])
+]) # LC_HAVE_KILL_LITTER_SUPER
+
+#
+## LC_HAVE_D_MAKE_PERSISTENT
+#
+# Linux commit v6.18-rc5-9-gbacdf1d70bbe2
+#   primitives for maintaining persisitency
+#
+AC_DEFUN([LC_SRC_HAVE_D_MAKE_PERSISTENT],[
+	LB2_LINUX_TEST_SRC([d_make_persistent], [
+		#include <linux/dcache.h>
+	],[
+		struct dentry *dentry = NULL;
+		struct inode *inode = NULL;
+
+		dentry = d_make_persistent(dentry, inode);
+		d_make_discardable(dentry);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_D_MAKE_PERSISTENT],[
+	LB2_MSG_LINUX_TEST_RESULT([if d_make_persistent() exists],
+	[d_make_persistent], [
+		AC_DEFINE(HAVE_D_MAKE_PERSISTENT, 1,
+			  [d_make_persistent() exists])
+	])
+]) # LC_HAVE_D_MAKE_PERSISTENT
+
+#
 # LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
 #
 # Linux commit v6.19-rc1-31-g6cbfdf89470ef
@@ -3867,6 +3915,8 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_VFS_CREATE_DELEGATE
 	LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 	LC_SRC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
+	LC_SRC_HAVE_KILL_LITTER_SUPER
+	LC_SRC_HAVE_D_MAKE_PERSISTENT
 
 	# 7.0
 	LC_SRC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
@@ -4088,6 +4138,8 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_VFS_CREATE_DELEGATE
 	LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 	LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
+	LC_HAVE_KILL_LITTER_SUPER
+	LC_HAVE_D_MAKE_PERSISTENT
 
 	# 7.0
 	LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER

include/lustre_compat/linux/fs.h
@@ -10,6 +10,34 @@
 #define __LIBCFS_LINUX_CFS_FS_H__
 
 #include <linux/fs.h>
+#include <linux/dcache.h>
+
+#ifndef HAVE_D_MAKE_PERSISTENT
+/*
+ * Linux commit v6.18-rc5-9-gbacdf1d70bbe2 introduced d_make_persistent() and
+ * d_make_discardable() so that filesystems can mark dentries as pinned without
+ * leaking unbalanced dget()s. On older kernels we fall back to the equivalent
+ * open-coded sequence: d_instantiate() (or d_add() for unhashed dentries) plus
+ * an extra dget() to pin, and a matching dput() to unpin. Older kernels still
+ * have kill_litter_super() available, which uses d_genocide() to drop these
+ * extra references at unmount.
+ */
+static inline struct dentry *d_make_persistent(struct dentry *dentry,
+					       struct inode *inode)
+{
+	if (d_unhashed(dentry))
+		d_add(dentry, inode);
+	else
+		d_instantiate(dentry, inode);
+	dget(dentry);
+	return dentry;
+}
+
+static inline void d_make_discardable(struct dentry *dentry)
+{
+	dput(dentry);
+}
+#endif /* !HAVE_D_MAKE_PERSISTENT */
 
 #ifndef S_DT_SHIFT
 #define S_DT_SHIFT		12

lustre/osd-wbcfs/osd_dirent.c
@@ -163,12 +163,15 @@ static int osd_index_dir_insert(const struct lu_env *env, struct dt_object *dt,
 		/* Finally release the @dentry. */
 		dput(dentry);
 	} else {
-		/* Add dentry into dentry hashtable for VFS lookup. */
-		d_add(dentry, inode);
+		/*
+		 * Pin the dentry in dcache; the matching unpin happens in
+		 * osd_index_dir_delete() via d_make_discardable().
+		 */
+		d_make_persistent(dentry, inode);
 		ihold(inode);
+		/* Drop the d_alloc() reference; the persistence flag holds it. */
+		dput(dentry);
 	}
-	/* Extra count (already obtain in @d_alloc) - pin the dentry in core */
-	/* dget(dentry); */
 
 	CDEBUG(D_CACHE,
 	       "%s: Insert dirent "DFID"/%pd@%pK inode@%pK nlink=%d\n",
@@ -262,14 +265,11 @@ static int osd_index_dir_delete(const struct lu_env *env, struct dt_object *dt,
 		/* MDD layer drops @nlink later via @dt_ref_del(). */
 		/* drop_nlink(inode); */
 		/*
-		 * Undo the count from "create".
-		 * Unhash the dentry from the parent dentry hashtable which is
-		 * add by @d_add(), so that it would not be found through a VFS
-		 * lookup anymore.
-		 * Unpin/drop the dentry from dcache.
+		 * Undo the pin from osd_index_dir_insert(): clear persistence
+		 * and drop the held reference so the dentry can be evicted.
 		 */
 		if (!nedir_rename)
-			dput(dentry);
+			d_make_discardable(dentry);
 		break;
 	default:
 		LBUG();

lustre/osd-wbcfs/osd_object.c
@@ -86,13 +86,25 @@ static int osd_object_init(const struct lu_env *env, struct lu_object *l,
 			 (void *)fid);
 	obj->oo_dt.do_body_ops = &osd_wbcfs_body_ops;
 	if (inode) {
-		obj->oo_inode = inode;
-		__osd_object_init(obj);
-
 		/*
-		 * TODO: check LMA EA and convert LMAI flags to lustre
-		 * LMA flags and cache it in object.
+		 * The inode may still be in the icache after its last link
+		 * was dropped because a persistent dentry pins it. Treat such
+		 * a stale inode as non-existent: returning it as a live object
+		 * would later trip the nlink==0 && !oo_destroyed assertion in
+		 * osd_object_release().
 		 */
+		if (inode->i_nlink == 0) {
+			iput(inode);
+			inode = NULL;
+		} else {
+			obj->oo_inode = inode;
+			__osd_object_init(obj);
+
+			/*
+			 * TODO: check LMA EA and convert LMAI flags to lustre
+			 * LMA flags and cache it in object.
+			 */
+		}
 	}
 
 	CDEBUG(D_INODE, "%s: object init for fid="DFID" inode@%pK nlink=%d\n",

lustre/osd-wbcfs/wbcfs.c
@@ -307,8 +307,7 @@ static int memfs_mknod(struct mnt_idmap *map, struct inode *dir,
 
 	dir->i_size += BOGO_DIRENT_SIZE;
 	inode_set_mtime_to_ts(dir, inode_set_ctime_current(dir));
-	d_instantiate(dentry, inode);
-	dget(dentry); /* Extra count - pin the dentry in core */
+	d_make_persistent(dentry, inode);
 
 	RETURN(0);
 }
@@ -367,7 +366,7 @@ static int memfs_unlink(struct inode *dir, struct dentry *dentry)
 			      inode_set_ctime_current(inode)));
 	inode_inc_iversion(dir);
 	drop_nlink(inode);
-	dput(dentry);
+	d_make_discardable(dentry);
 	return 0;
 }
 
@@ -409,8 +408,7 @@ static int memfs_link(struct dentry *old_dentry, struct inode *dir,
 	inode_inc_iversion(dir);
 	inc_nlink(inode);
 	ihold(inode);	/* New dentry reference */
-	dget(dentry);	/* Extra pinning count for the created dentry */
-	d_instantiate(dentry, inode);
+	d_make_persistent(dentry, inode);
 	return 0;
 }
 
@@ -1055,7 +1053,18 @@ static const struct address_space_operations memfs_aops = {
 
 static void memfs_kill_super(struct super_block *sb)
 {
+	/*
+	 * On kernels that still have kill_litter_super(), it walks the dentry
+	 * tree via d_genocide() to drop the extra refs left by the
+	 * d_instantiate()+dget() compat fallback for d_make_persistent().
+	 * Newer kernels expose neither, but kill_anon_super() now strips
+	 * DCACHE_PERSISTENT during shrink_dcache_for_umount() instead.
+	 */
+#ifdef HAVE_KILL_LITTER_SUPER
+	kill_litter_super(sb);
+#else
 	kill_anon_super(sb);
+#endif
 }
 
 static struct file_system_type memfs_fstype = {