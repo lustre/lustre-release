lustre/lustre-release

commit 43669e3e6c565a51f5a6f08092f599423840da61

LU-18162 osc: convert Object Storage Client to LU device
Convert OSC to use LU device init/fini rather
than the legacy OBD API.

Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Iaf00c293d1c31eb11f8a5cdbff6aa313f5c96c28
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58818
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osc/osc_dev.c
@@ -158,15 +158,24 @@ static int osc_device_init(const struct lu_env *env, struct lu_device *d,
 static struct lu_device *osc_device_fini(const struct lu_env *env,
 					 struct lu_device *d)
 {
-	return NULL;
+	struct obd_device *obd = d->ld_obd;
+
+	ENTRY;
+
+	osc_precleanup_common(obd);
+	ptlrpc_lprocfs_unregister_obd(obd);
+
+	RETURN(NULL);
 }
 
 static struct lu_device *osc_device_free(const struct lu_env *env,
 					 struct lu_device *d)
 {
 	struct osc_device *oc = lu2osc_dev(d);
+	struct obd_device *obd = d->ld_obd;
 
 	cl_device_fini(lu2cl_dev(d));
+	osc_cleanup_common(obd);
 	OBD_FREE_PTR(oc);
 	return NULL;
 }

lustre/obdclass/lu_object.c
@@ -1337,6 +1337,8 @@ void lu_stack_fini(const struct lu_env *env, struct lu_device *top)
 	for (scan = top; scan != NULL; scan = next) {
 		if (strcmp(scan->ld_type->ldt_name, LUSTRE_MDC_NAME) == 0)
 			next = NULL;
+		else if (strcmp(scan->ld_type->ldt_name, LUSTRE_OSC_NAME) == 0)
+			next = NULL;
 		else
 			next = ldto_device_fini(env, scan);
 
@@ -1348,6 +1350,9 @@ void lu_stack_fini(const struct lu_env *env, struct lu_device *top)
 
 	for (scan = top; scan != NULL; scan = next) {
 		if (strcmp(scan->ld_type->ldt_name, LUSTRE_MDC_NAME) == 0)
+			break;
+
+		if (strcmp(scan->ld_type->ldt_name, LUSTRE_OSC_NAME) == 0)
 			break;
 
 		next = ldto_device_free(env, scan);

lustre/obdecho/echo_client.c
@@ -777,7 +777,8 @@ static struct lu_device *echo_device_fini(const struct lu_env *env,
 	struct echo_device *ed = cl2echo_dev(lu2cl_dev(d));
 	struct lu_device *next = ed->ed_next;
 
-	while (next && !ed->ed_next_ismd)
+	while (next && !ed->ed_next_ismd &&
+	       strcmp(next->ld_type->ldt_name, LUSTRE_OSC_NAME) != 0)
 		next = ldto_device_fini(env, next);
 	return NULL;
 }
@@ -833,7 +834,8 @@ static struct lu_device *echo_device_free(const struct lu_env *env,
 	echo_fid_fini(d->ld_obd);
 	echo_ed_los_fini(env, ed);
 #endif
-	while (next && !ed->ed_next_ismd)
+	while (next && !ed->ed_next_ismd &&
+	       strcmp(next->ld_type->ldt_name, LUSTRE_OSC_NAME) != 0)
 		next = ldto_device_free(env, next);
 
 	LASSERT(ed->ed_site == d->ld_site);

lustre/osc/osc_request.c
@@ -4118,16 +4118,6 @@ int osc_precleanup_common(struct obd_device *obd)
 }
 EXPORT_SYMBOL(osc_precleanup_common);
 
-static int osc_precleanup(struct obd_device *obd)
-{
-	ENTRY;
-
-	osc_precleanup_common(obd);
-
-	ptlrpc_lprocfs_unregister_obd(obd);
-	RETURN(0);
-}
-
 int osc_cleanup_common(struct obd_device *obd)
 {
 	struct client_obd *cli = &obd->u.cli;
@@ -4159,9 +4149,6 @@ EXPORT_SYMBOL(osc_cleanup_common);
 
 static const struct obd_ops osc_obd_ops = {
 	.o_owner                = THIS_MODULE,
-	.o_setup                = osc_setup,
-	.o_precleanup           = osc_precleanup,
-	.o_cleanup              = osc_cleanup_common,
 	.o_add_conn             = client_import_add_conn,
 	.o_del_conn             = client_import_del_conn,
 	.o_connect              = client_connect_import,

commit 5537bbc0b6cef29519d5aad6b00d539b40870846

LU-20160 mdc,osc: Do not crash on device registration failure
If device registration failed, avoid dereferencing NULL obd pointer

Change-Id: I43ae129eba19009d6b85d695c0b576c00f57bf2e
Fixes: 43669e3e6c5 ("LU-18162 osc: convert Object Storage Client to LU device")
Fixes: 40585fc40f8 ("LU-18162 mdc: convert Metadata Client to LU device")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65411
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: wangdi <ddiwang@google.com>

lustre/mdc/mdc_dev.c
@@ -1702,12 +1702,14 @@ static struct lu_device *mdc_device_free(const struct lu_env *env,
 					 struct lu_device *lu)
 {
 	struct obd_device *obd = lu->ld_obd;
-	struct client_obd *cli = &obd->u.cli;
 	struct osc_device *osc = lu2osc_dev(lu);
 
-	LASSERT(cli->cl_mod_rpcs_in_flight == 0);
 	cl_device_fini(lu2cl_dev(lu));
-	osc_cleanup_common(obd);
+	if (obd) {
+		struct client_obd *cli = &obd->u.cli;
+		LASSERT(cli->cl_mod_rpcs_in_flight == 0);
+		osc_cleanup_common(obd);
+	}
 	OBD_FREE_PTR(osc);
 
 	return NULL;

lustre/osc/osc_dev.c
@@ -175,7 +175,8 @@ static struct lu_device *osc_device_free(const struct lu_env *env,
 	struct obd_device *obd = d->ld_obd;
 
 	cl_device_fini(lu2cl_dev(d));
-	osc_cleanup_common(obd);
+	if (obd)
+		osc_cleanup_common(obd);
 	OBD_FREE_PTR(oc);
 	return NULL;
 }