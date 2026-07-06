lustre/lustre-release

commit 7fa1ec5446a2ca518559782ed178c396d7a103aa

LU-18687 debug: cleanly split lustre_compat and debug
... and de-automake libcfs/ in the process. The debug and compat
code will still be linked into libcfs.ko. But now we will have a
clean logical split between compatability code and debug code.

libcfs/libcfs/ will only be used for debugging code. lustre_compat/
will only be used for kernel backwards compatibility.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: I44f382d20b1c8ac6cafef6697e4f855d52eb8da4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64049
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

include/linux/libcfs/libcfs.h
@@ -39,6 +39,8 @@
 typedef s32 timeout_t;
 
 int libcfs_setup(void);
+int debug_module_init(void);
+void debug_module_exit(void);
 
 #ifdef HAVE_CONST_CTR_TABLE
 #define DEFINE_CTL_TABLE_INIT(__name, init)\

libcfs/libcfs/module.c
@@ -11,6 +11,7 @@
  * This file is part of Lustre, http://www.lustre.org/
  */
 
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
@@ -34,19 +35,9 @@
 #define DEBUG_SUBSYSTEM S_LNET
 
 #include <linux/libcfs/libcfs.h>
+#include <lustre_compat/linux/linux-misc.h>
 #include <lnet/lib-lnet.h>
-#include <uapi/linux/lustre/lustre_ver.h>
 #include "tracefile.h"
-
-int cpu_npartitions;
-EXPORT_SYMBOL(cpu_npartitions);
-module_param(cpu_npartitions, int, 0444);
-MODULE_PARM_DESC(cpu_npartitions, "# of CPU partitions");
-
-char *cpu_pattern = "N";
-EXPORT_SYMBOL(cpu_pattern);
-module_param(cpu_pattern, charp, 0444);
-MODULE_PARM_DESC(cpu_pattern, "CPU partitions pattern");
 
 struct lnet_debugfs_symlink_def {
 	const char *name;
@@ -619,7 +610,7 @@ cleanup_lock:
 }
 EXPORT_SYMBOL(libcfs_setup);
 
-static int __init libcfs_init(void)
+int debug_module_init(void)
 {
 	int rc;
 
@@ -631,21 +622,14 @@ static int __init libcfs_init(void)
 		return rc;
 	}
 
-	rc = cfs_arch_init();
-	if (rc < 0) {
-		CERROR("cfs_arch_init: error %d\n", rc);
-		libcfs_debug_cleanup();
-		return rc;
-	}
-
 	lnet_insert_debugfs(lnet_table, THIS_MODULE, &debugfs_state);
 	if (!IS_ERR_OR_NULL(lnet_debugfs_root))
 		lnet_insert_debugfs_links(lnet_debugfs_symlinks);
 
-	return rc;
+	return 0;
 }
 
-static void __exit libcfs_exit(void)
+void debug_module_exit(void)
 {
 	int rc;
 
@@ -668,7 +652,16 @@ static void __exit libcfs_exit(void)
 		pr_err("LustreError: libcfs_debug_cleanup: rc = %d\n", rc);
 
 	debug_format_buffer_free_buffers();
-	cfs_arch_exit();
+}
+
+static int __init libcfs_init(void)
+{
+	return debug_module_init();
+}
+
+static void __exit libcfs_exit(void)
+{
+	debug_module_exit();
 }
 
 MODULE_AUTHOR("OpenSFS, Inc. <http://www.lustre.org/>");
@@ -676,5 +669,5 @@ MODULE_DESCRIPTION("Lustre helper library");
 MODULE_VERSION(LIBCFS_VERSION);
 MODULE_LICENSE("GPL");
 
-late_initcall(libcfs_init);
-module_exit(libcfs_exit);
+compat_module_init(libcfs_init);
+compat_module_exit(libcfs_exit);

lustre_compat/module.c
@@ -1,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Copyright (c) 2012, 2017, Intel Corporation.
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+#define DEBUG_SUBSYSTEM S_LNET
+
+#include <linux/libcfs/libcfs.h>
+
+int cpu_npartitions;
+EXPORT_SYMBOL(cpu_npartitions);
+module_param(cpu_npartitions, int, 0444);
+MODULE_PARM_DESC(cpu_npartitions, "# of CPU partitions");
+
+char *cpu_pattern = "N";
+EXPORT_SYMBOL(cpu_pattern);
+module_param(cpu_pattern, charp, 0444);
+MODULE_PARM_DESC(cpu_pattern, "CPU partitions pattern");

autoMakefile.am
@@ -15,7 +15,6 @@ SUBDIRS := @LDISKFS_SUBDIR@ \
 	@LUSTREIOKIT_SUBDIR@ \
 	Documentation \
 	lib \
-	libcfs \
 	lnet/utils \
 	lustre/utils \
 	lustre/tests \
@@ -26,7 +25,6 @@ DIST_SUBDIRS := ldiskfs \
 	lustre-iokit \
 	Documentation \
 	lib \
-	libcfs \
 	lnet/utils \
 	lustre/utils \
 	lustre/tests \
@@ -69,6 +67,9 @@ if MODULES
 	done
 	$(MKDIR_P) $(DESTDIR)$(modulenetdir)
 	for ko in $$(find lnet/ -name '*.ko' 2>/dev/null); do \
+		$(INSTALL) -m 0644 $${ko} $(DESTDIR)$(modulenetdir)/; \
+	done
+	for ko in $$(find libcfs/ -name '*.ko' 2>/dev/null); do \
 		$(INSTALL) -m 0644 $${ko} $(DESTDIR)$(modulenetdir)/; \
 	done
 endif
@@ -132,7 +133,6 @@ checkpatch:
 	@git diff HEAD~1 | ./contrib/scripts/checkpatch.pl
 
 utils:
-	$(MAKE) -C libcfs/libcfs/util
 	$(MAKE) -C lnet/utils
 	$(MAKE) -C lustre/utils
 
@@ -242,6 +242,8 @@ dist-hook: undef.h
 	find include/ -type f \( -name "*.h" \) \
 		-exec cp --parents {} $(distdir) \;
 	find lustre/include/ -type f \( -name "*.h" \) \
+		-exec cp --parents {} $(distdir) \;
+	find libcfs/ -type f \( -name "*.c" -o -name "*.h" -o -iname "Makefile" \) \
 		-exec cp --parents {} $(distdir) \;
 	find lustre_compat/ -type f \( -name "*.c" -o -name "*.h" -o -iname "Makefile" \) \
 		-exec cp --parents {} $(distdir) \;

config/lustre-build.m4
@@ -707,7 +707,6 @@ LB_CONDITIONALS
 LB_CONFIG_HEADERS
 
 LPLUG_CONFIGURE
-LIBCFS_CONFIG_FILES
 LB_CONFIG_FILES
 EC_CONFIG_FILES
 LN_CONFIG_FILES

config/lustre-libcfs.m4
@@ -1096,16 +1096,3 @@ AS_IF([test "x$enable_libpthread" = xyes], [
 AC_SUBST(PTHREAD_LIBS)
 ]) # LIBCFS_CONFIGURE
 
-#
-# LIBCFS_CONFIG_FILES
-#
-# files that should be generated with AC_OUTPUT
-#
-AC_DEFUN([LIBCFS_CONFIG_FILES], [
-AC_CONFIG_FILES([
-libcfs/Makefile
-libcfs/autoMakefile
-libcfs/libcfs/Makefile
-libcfs/libcfs/autoMakefile
-])
-]) # LIBCFS_CONFIG_FILES

config/lustre-toolchain.m4
@@ -97,7 +97,7 @@ CCASFLAGS="-Wall -fPIC -D_GNU_SOURCE"
 AC_SUBST(CCASFLAGS)
 
 # everyone builds against lnet and lustre kernel headers
-EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/libcfs/include -I$PWD/libcfs/include/libcfs -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include -I$PWD/include"
+EXTRA_KCFLAGS="$EXTRA_KCFLAGS -g -I$PWD/lnet/include/uapi -I$PWD/lnet/include -I$PWD/lustre/include/uapi -I$PWD/lustre/include -I$PWD/include"
 AC_SUBST(EXTRA_KCFLAGS)
 ]) # LTC_PROG_CC
 

include/lustre_compat/linux/linux-misc.h
@@ -28,4 +28,44 @@ int lustre_symbols_init(void);
 int cfs_arch_init(void);
 void cfs_arch_exit(void);
 
+/*
+ * compat_module_init() / compat_module_exit() wrap a module's init/exit
+ * functions so that cfs_arch_init() / cfs_arch_exit() run around them.
+ * They are overridable: when lustre_compat is not linked into libcfs.ko,
+ * redefine these to bare late_initcall() / module_exit() before including
+ * this header.
+ */
+#ifndef compat_module_init
+#define compat_module_init(init_fn)					\
+static int __init __compat_module_init(void)				\
+{									\
+	int __rc;							\
+									\
+	__rc = cfs_arch_init();						\
+	if (__rc < 0) {							\
+		pr_err("LustreError: cfs_arch_init: rc = %d\n", __rc);	\
+		return __rc;						\
+	}								\
+									\
+	__rc = (init_fn)();						\
+	if (__rc) {							\
+		cfs_arch_exit();					\
+		return __rc;						\
+	}								\
+									\
+	return 0;							\
+}									\
+late_initcall(__compat_module_init)
+#endif
+
+#ifndef compat_module_exit
+#define compat_module_exit(exit_fn)					\
+static void __exit __compat_module_exit(void)				\
+{									\
+	(exit_fn)();							\
+	cfs_arch_exit();						\
+}									\
+module_exit(__compat_module_exit)
+#endif
+
 #endif /* __LIBCFS_LINUX_MISC_H__ */

commit b147603684e6251379ae8ba3eaaccc7ceec4a514

LU-20315 libcfs: fix up __init and __exit
... to functions that need these annotations. Also fix up
lustre_compat header inetdevice.h typo.h

Test-Parameters: trivial
Fixes: 7fa1ec5446a2 ("LU-18687 debug: cleanly split lustre_compat and debug")
Fixes: f49c332783c2 ("LU-18687 lnet: match compat headers to linux headers")
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I4b22cfe39f170a37c5d1a83e5418f02f7d8e86ee
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66270
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>

include/linux/libcfs/libcfs.h
@@ -39,8 +39,6 @@
 typedef s32 timeout_t;
 
 int libcfs_setup(void);
-int debug_module_init(void);
-void debug_module_exit(void);
 
 #ifdef HAVE_CONST_CTR_TABLE
 #define DEFINE_CTL_TABLE_INIT(__name, init)\

include/lustre_compat/linux/inetdevice.h
@@ -3,7 +3,7 @@
 /* This file is part of Lustre, http://www.lustre.org/ */
 
 #ifndef __LIBCFS_LINUX_INETDEVICE_H__
-#define __LIBCFS_LINUX_INETDEIVCE_H__
+#define __LIBCFS_LINUX_INETDEVICE_H__
 
 #include <linux/inetdevice.h>
 

include/lustre_compat/linux/llcrypt.h
@@ -142,7 +142,7 @@ static inline void folio_bounce_private(struct folio *dst, s32 dstpg,
 
 /* crypto.c */
 extern int __init llcrypt_init(void);
-extern void __exit llcrypt_exit(void);
+extern void llcrypt_exit(void);
 extern void llcrypt_enqueue_decrypt_work(struct work_struct *);
 extern struct llcrypt_ctx *llcrypt_get_ctx(gfp_t);
 extern void llcrypt_release_ctx(struct llcrypt_ctx *);

lnet/libcfs/debug.c
@@ -627,7 +627,7 @@ int libcfs_debug_init(unsigned long bufsize)
 	return rc;
 }
 
-int libcfs_debug_cleanup(void)
+int __exit libcfs_debug_cleanup(void)
 {
 	libcfs_unregister_panic_notifier();
 	kernel_param_lock(THIS_MODULE);

lnet/libcfs/module.c
@@ -562,7 +562,7 @@ void lnet_debugfs_fini(void **state)
 }
 EXPORT_SYMBOL_GPL(lnet_debugfs_fini);
 
-static void lnet_insert_debugfs_links(
+static void __init lnet_insert_debugfs_links(
 		const struct lnet_debugfs_symlink_def *symlinks)
 {
 	for (; symlinks && symlinks->name; symlinks++)
@@ -610,7 +610,7 @@ cleanup_lock:
 }
 EXPORT_SYMBOL(libcfs_setup);
 
-int debug_module_init(void)
+static int __init debug_module_init(void)
 {
 	int rc;
 
@@ -629,7 +629,7 @@ int debug_module_init(void)
 	return 0;
 }
 
-void debug_module_exit(void)
+static void __exit debug_module_exit(void)
 {
 	int rc;
 

lustre_compat/fs/crypto/crypto.c
@@ -617,7 +617,7 @@ fail:
 /**
  * llcrypt_exit() - Clean up for fs encryption.
  */
-void __exit llcrypt_exit(void)
+void llcrypt_exit(void)
 {
 	llcrypt_exit_keyring();
 

lustre_compat/fs/crypto/keyring.c
@@ -1002,7 +1002,7 @@ err_unregister_llcrypt:
 	return err;
 }
 
-void __exit llcrypt_exit_keyring(void)
+void llcrypt_exit_keyring(void)
 {
 	unregister_key_type(&key_type_llcrypt_user);
 	unregister_key_type(&key_type_llcrypt);

lustre_compat/fs/crypto/llcrypt_private.h
@@ -462,7 +462,7 @@ extern int llcrypt_verify_key_added(struct super_block *sb,
 
 extern int __init llcrypt_init_keyring(void);
 
-extern void __exit llcrypt_exit_keyring(void);
+extern void llcrypt_exit_keyring(void);
 
 /* keysetup.c */
 

lustre_compat/mm/vmalloc.c
@@ -33,7 +33,7 @@ static void free_work(struct work_struct *w)
 		vfree((void *)llnode);
 }
 
-void init_compat_vfree_atomic(void)
+void __init init_compat_vfree_atomic(void)
 {
 	int i;
 

lustre_compat/symbols.c
@@ -135,7 +135,7 @@ unsigned int compat_account_page_dirtied(struct page *page,
 EXPORT_SYMBOL(compat_account_page_dirtied);
 #endif
 
-int lustre_symbols_init(void)
+int __init lustre_symbols_init(void)
 {
 	int rc;
 