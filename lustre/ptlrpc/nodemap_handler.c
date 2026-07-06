lustre/lustre-release

commit d6e7ac7193d354c2cf41e344cc6d48339bb3eb52

LU-19079 nodemap: add flag to identify clients with gss
Add new nodemap property gssonly_identification.
If set to 1, clients for this nodemap are no longer identified based
on their NIDs, but instead by the GSS token they are using to
authenticate. Consequently, a nodemap with this property set cannot
have any NID ranges defined. Any client using the GSS token associated
with this nodemap to authenticate is considered part of this nodemap.

Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I3ee7ea8fbb0203a0e49ef71926da11701d7daa4c
Was-Change-Id: I007af3ebe8608a31f9cd9360f3aa880e100d237b
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61734
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -1223,6 +1223,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nmf_fileset_use_iam = 1;
 		dst->nmf_raise_privs = NODEMAP_RAISE_PRIV_NONE;
 		dst->nmf_rbac_raise = NODEMAP_RBAC_NONE;
+		dst->nmf_gss_identify = 0;
 
 		dst->nm_squash_uid = NODEMAP_NOBODY_UID;
 		dst->nm_squash_gid = NODEMAP_NOBODY_GID;
@@ -1250,6 +1251,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nmf_fileset_use_iam = 1;
 		dst->nmf_raise_privs = src->nmf_raise_privs;
 		dst->nmf_rbac_raise = src->nmf_rbac_raise;
+		dst->nmf_gss_identify = src->nmf_gss_identify;
 		dst->nm_squash_uid = src->nm_squash_uid;
 		dst->nm_squash_gid = src->nm_squash_gid;
 		dst->nm_squash_projid = src->nm_squash_projid;
@@ -1425,6 +1427,13 @@ int nodemap_add_range(const char *name, const struct lnet_nid nid[2],
 
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_unlock, rc = -ENXIO);
+
+	if (nodemap->nmf_gss_identify) {
+		CDEBUG(D_INFO,
+		       "cannot add any NID range on nodemap %s because 'gssonly_identification' property is set\n",
+		       nodemap->nm_name);
+		GOTO(out_unlock, rc = -EPERM);
+	}
 
 	rc = nodemap_add_range_helper(active_config, nodemap, nid,
 				      netmask, 0);
@@ -3995,6 +4004,48 @@ out_putref:
 EXPORT_SYMBOL(nodemap_set_deny_mount);
 
 /**
+ * nodemap_set_gss_identify() - Set the nmf_gss_identify flag to true or false.
+ * @name: nodemap name
+ * @gss_identify: if true, identify clients based on the GSS token
+ *
+ * Return:
+ * * %0 on success
+ */
+int nodemap_set_gss_identify(const char *name, bool gss_identify)
+{
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+
+	mutex_lock(&active_config_lock);
+	nodemap = nodemap_lookup(name);
+	mutex_unlock(&active_config_lock);
+	if (IS_ERR(nodemap))
+		RETURN(PTR_ERR(nodemap));
+
+	if (is_default_nodemap(nodemap))
+		GOTO(out_putref, rc = -EINVAL);
+	if (!allow_op_on_nm(nodemap))
+		GOTO(out_putref, rc = -EPERM);
+
+	if (!list_empty(&nodemap->nm_ranges)) {
+		CDEBUG(D_INFO,
+		       "nodemap %s must have empty NID range to set 'gssonly_identification' property\n",
+		       nodemap->nm_name);
+		GOTO(out_putref, rc = -EPERM);
+	}
+
+	nodemap->nmf_gss_identify = gss_identify;
+	rc = nodemap_idx_nodemap_update(nodemap);
+
+	nm_member_revoke_locks(nodemap);
+
+out_putref:
+	nodemap_putref(nodemap);
+	return rc;
+}
+EXPORT_SYMBOL(nodemap_set_gss_identify);
+
+/**
  * nodemap_add() - Add a nodemap
  * @nodemap_name: name of nodemap
  * @dynamic: if true nodemap will be dynamic (can be modified runtime)
@@ -4999,6 +5050,12 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
 		if (rc == 0)
 			rc = nodemap_set_deny_mount(nodemap_name, bool_switch);
 		break;
+	case LCFG_NODEMAP_GSS_IDENTIFY:
+		rc = kstrtobool(param, &bool_switch);
+		if (rc == 0)
+			rc = nodemap_set_gss_identify(nodemap_name,
+						      bool_switch);
+		break;
 	case LCFG_NODEMAP_MAP_MODE:
 	{
 		char *p;
@@ -5385,6 +5442,7 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 	case LCFG_NODEMAP_RAISE_PRIVS:
 	case LCFG_NODEMAP_READONLY_MOUNT:
 	case LCFG_NODEMAP_DENY_MOUNT:
+	case LCFG_NODEMAP_GSS_IDENTIFY:
 	case LCFG_NODEMAP_RBAC:
 		if (lcfg->lcfg_bufcount != 4)
 			GOTO(out_lcfg, rc = -EINVAL);

Documentation/man8/lctl-nodemap-modify.8
@@ -1,4 +1,4 @@
-.TH LCTL-NODEMAP_MODIFY 8 2025-02-28 Lustre "Lustre Configuration Utilities"
+.TH LCTL-NODEMAP_MODIFY 8 2025-07-09 Lustre "Lustre Configuration Utilities"
 .SH NAME
 lctl-nodemap_modify \- modify a nodemap property
 .SH SYNOPSIS
@@ -166,6 +166,15 @@ INTERNAL, so that servers trust supplementary groups as provided by clients.
 .br
 Apart from all, any role not explicitly specified is forbidden.
 And to forbid all roles, use 'none' value.
+.TP
+.B gssonly_identification
+If set to 1, clients for this nodemap are no longer identified based on their
+NIDs, but instead by the GSS token they are using to authenticate. Consequently,
+a nodemap with this property set cannot have any NID ranges defined. Any client
+using the GSS token associated with this nodemap to authenticate is considered
+part of this nodemap.
+.br
+Defaults to 0.
 .RE
 .TP
 .BI --value " VALUE"

lustre/include/lustre_nodemap.h
@@ -104,7 +104,8 @@ struct lu_nodemap {
 				 nmf_forbid_encryption:1,
 				 nmf_readonly_mount:1,
 				 nmf_deny_mount:1,
-				 nmf_fileset_use_iam:1;
+				 nmf_fileset_use_iam:1,
+				 nmf_gss_identify:1;
 	/* bitmap for mapping type */
 	enum nodemap_mapping_modes nmf_map_mode;
 	/* bitmap for rbac, enum nodemap_rbac_roles */
@@ -235,6 +236,7 @@ int nodemap_set_raise_privs(const char *name, enum nodemap_raise_privs privs,
 			    enum nodemap_rbac_roles rbac_raise);
 int nodemap_set_readonly_mount(const char *name, bool readonly_mount);
 int nodemap_set_deny_mount(const char *name, bool deny_mount);
+int nodemap_set_gss_identify(const char *name, bool gss_identify);
 bool nodemap_can_setquota(struct lu_nodemap *nodemap, __u32 qc_cmd,
 			  __u32 qc_type, __u32 id);
 int nodemap_add_idmap(const char *nodemap_name, enum nodemap_id_type id_type,

lustre/ptlrpc/nodemap_lproc.c
@@ -1158,6 +1158,34 @@ static int nodemap_parent_seq_show(struct seq_file *seq, void *data)
 	return 0;
 }
 
+/**
+ * Reads and prints the gssonly_identification flag for the given nodemap.
+ *
+ * \param	m		seq file in proc fs
+ * \param	data		unused
+ * \retval	0		success
+ */
+static int nodemap_gssonly_identify_seq_show(struct seq_file *m, void *data)
+{
+	struct lu_nodemap *nodemap;
+	int rc;
+
+	mutex_lock(&active_config_lock);
+	nodemap = nodemap_lookup(m->private);
+	mutex_unlock(&active_config_lock);
+	if (IS_ERR(nodemap)) {
+		rc = PTR_ERR(nodemap);
+		CERROR("cannot find nodemap '%s': rc = %d\n",
+		       (char *)m->private, rc);
+		return rc;
+	}
+
+	seq_printf(m, "%d\n", (int)nodemap->nmf_gss_identify);
+	nodemap_putref(nodemap);
+
+	return 0;
+}
+
 static struct ldebugfs_vars lprocfs_nm_module_vars[] = {
 	{
 		.name		= "active",
@@ -1185,6 +1213,7 @@ LDEBUGFS_SEQ_FOPS_RO(nodemap_raise_privs);
 LDEBUGFS_SEQ_FOPS_RO(nodemap_readonly_mount);
 LDEBUGFS_SEQ_FOPS_RO(nodemap_deny_mount);
 LDEBUGFS_SEQ_FOPS_RO(nodemap_parent);
+LDEBUGFS_SEQ_FOPS_RO(nodemap_gssonly_identify);
 
 static const struct file_operations nodemap_ranges_fops = {
 	.open		= nodemap_ranges_open,
@@ -1243,6 +1272,10 @@ static struct ldebugfs_vars lprocfs_nodemap_vars[] = {
 	{
 		.name		= "forbid_encryption",
 		.fops		= &nodemap_forbid_encryption_fops,
+	},
+	{
+		.name		= "gssonly_identification",
+		.fops		= &nodemap_gssonly_identify_fops,
 	},
 	{
 		.name		= "id",

lustre/ptlrpc/nodemap_storage.c
@@ -89,6 +89,7 @@ static void nodemap_cluster_rec_init(union nodemap_rec *nr,
 	nr->ncr.ncr_flags2 =
 		(nodemap->nmf_readonly_mount ? NM_FL2_READONLY_MOUNT : 0) |
 		(nodemap->nmf_deny_mount ? NM_FL2_DENY_MOUNT : 0) |
+		(nodemap->nmf_gss_identify ? NM_FL2_GSS_IDENTIFY : 0) |
 		(nodemap->nmf_fileset_use_iam ? NM_FL2_FILESET_USE_IAM : 0);
 	nr->ncr.ncr_padding1 = 0;
 	nr->ncr.ncr_squash_projid = cpu_to_le32(nodemap->nm_squash_projid);
@@ -1632,6 +1633,7 @@ static int nodemap_cluster_rec_helper(struct nodemap_config *config,
 	flags2 = rec->ncr.ncr_flags2;
 	nodemap->nmf_readonly_mount = flags2 & NM_FL2_READONLY_MOUNT;
 	nodemap->nmf_deny_mount = flags2 & NM_FL2_DENY_MOUNT;
+	nodemap->nmf_gss_identify = flags2 & NM_FL2_GSS_IDENTIFY;
 	nodemap->nmf_fileset_use_iam = flags2 & NM_FL2_FILESET_USE_IAM;
 
 	/* by default, and in the absence of cluster_roles, grant all rbac roles

lustre/utils/lctl.c
@@ -674,7 +674,7 @@ command_t cmdlist[] = {
 	{"nodemap_modify", jt_nodemap_modify, 0,
 	 "modify a nodemap property\n"
 	 "usage: nodemap_modify --name NODEMAP_NAME --property PROPERTY_NAME{=VALUE| --value VALUE}\n"
-	 "valid properties: admin trusted map_mode squash_uid squash_gid squash_projid deny_unknown audit_mode forbid_encryption readonly_mount rbac deny_mount child_raise_privileges"},
+	 "valid properties: admin trusted map_mode squash_uid squash_gid squash_projid deny_unknown audit_mode forbid_encryption readonly_mount rbac deny_mount child_raise_privileges gssonly_identification"},
 	{"nodemap_add_offset", jt_nodemap_add_offset, 0,
 	 "add an offset for UID/GID/PROJID mappings\n"
 	 "usage: nodemap_add_offset --name NODEMAP_NAME --offset OFFSET --limit LIMIT\n"},

lustre/utils/obd.c
@@ -5176,6 +5176,8 @@ int jt_nodemap_modify(int argc, char **argv)
 		cmd = LCFG_NODEMAP_RBAC;
 	} else if (strcmp("deny_mount", param) == 0) {
 		cmd = LCFG_NODEMAP_DENY_MOUNT;
+	} else if (strcmp("gssonly_identification", param) == 0) {
+		cmd = LCFG_NODEMAP_GSS_IDENTIFY;
 	} else {
 		fprintf(stderr,
 			"error: %s: nodemap_modify invalid property: %s\n",

commit e7e9a6d827a7cc2618af7472cd81a04af177ecc1

LU-20198 nodemap: do not inherit gss_identify from default
The gssonly_identification flag cannot be set on the default nodemap,
but nodemap_inherit_properties() copies it unconditionally from the
default to every newly created nodemap. If the on-disk default record
has this flag bit set (e.g. from stale data in a field that was
previously padded before being repurposed for NM_FL2_GSS_IDENTIFY),
every new nodemap inherits the unintended value, and there is no way
for an admin to clear it on the default.

This patch skips the inheritance of nmf_gss_identify when the source
is the default nodemap.

Test-Parameters: trivial testlist=sanity-sec
Fixes: d6e7ac7193d3 ("LU-19079 nodemap: add flag to identify clients with gss")
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: Iceeb8c37f87e7a1d71ffe228d81b607de0606d37
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65542
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/ptlrpc/nodemap_handler.c
@@ -1468,7 +1468,6 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nmf_fileset_use_iam = 1;
 		dst->nmf_raise_privs = src->nmf_raise_privs;
 		dst->nmf_rbac_raise = src->nmf_rbac_raise;
-		dst->nmf_gss_identify = src->nmf_gss_identify;
 		dst->nm_squash_uid = src->nm_squash_uid;
 		dst->nm_squash_gid = src->nm_squash_gid;
 		dst->nm_squash_projid = src->nm_squash_projid;
@@ -1480,6 +1479,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nm_offset_limit_projid = src->nm_offset_limit_projid;
 		if (src->nm_id == LUSTRE_NODEMAP_DEFAULT_ID) {
 			dst->nm_sepol[0] = '\0';
+			dst->nmf_gss_identify = 0;
 		} else {
 			/* because we are copying from an existing nodemap,
 			 * we already know this string is well formatted
@@ -1488,6 +1488,7 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 			rc = idmap_copy_tree(dst, src);
 			if (rc)
 				goto out;
+			dst->nmf_gss_identify = src->nmf_gss_identify;
 		}
 		/* only dynamic nodemap inherits fileset from parent */
 		if (dst->nm_dyn) {