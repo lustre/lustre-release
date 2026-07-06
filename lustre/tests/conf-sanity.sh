Cray/lustre

commit 34e1e7b85abcdf04e671993f34038a6c75182cc5

LU-19740 utils: Avoid redundant rewrite for tunefs.lustre
It's unnecessary to rewrite CONFIGS/mountdata when tunefs.lustre
changes nothing.

Signed-off-by: Xiao Yang <xyang@ddn.com>
Change-Id: I2164afd75c0470a99d4c087f2ba7008dbf3ec592
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64352
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/mkfs_lustre.c
@@ -805,6 +805,9 @@ static int parse_opts(int argc, char *const argv[], struct mkfs_opts *mop,
 int main(int argc, char *const argv[])
 {
 	struct mkfs_opts mop;
+#ifdef TUNEFS
+	struct mkfs_opts prev_mop;
+#endif
 	struct lustre_disk_data *ldd = &mop.mo_ldd;
 	char *mountopts = NULL;
 	char wanted_mountopts[512] = "";
@@ -879,6 +882,8 @@ int main(int argc, char *const argv[])
 	if (verbose > 0)
 		print_ldd("Read previous values", &mop);
 
+	memcpy(&prev_mop, &mop, sizeof(prev_mop));
+
 	/*
 	 * For the right semantics, if '-e'/'--erase-params' is specified,
 	 * it must be picked out and all old parameters should be erased
@@ -912,6 +917,14 @@ int main(int argc, char *const argv[])
 		fprintf(stderr,
 			"Failed to read previous Lustre data from %s (%s)\n",
 			mop.mo_device, strerror(ret2));
+		goto out;
+	}
+
+	/* Don't write anything when there is no change */
+	if (memcmp(&prev_mop, &mop, sizeof(prev_mop)) == 0) {
+		if (verbose > 0)
+			fprintf(stdout,
+				"exiting because there is no change.\n");
 		goto out;
 	}
 #endif

lustre/lustre-release

commit 23b31c5db51a5765ece00273206ba7179562b6a3

LU-20360 utils: fix tunefs.lustre dropping mountfsoptions
tunefs.lustre exits rc 0 without writing when --mountfsoptions (-o)
is the only option given, silently dropping the new mount options.

The -o value lands in @mountopts and is copied into ldd_mount_opts
only after the "no change" memcmp shortcut from LU-19740 runs, so
@mop still matches disk and the write is skipped. Another ldd-
modifying option (e.g. --param) hides the bug by changing @mop.

Skip the shortcut when mount options were given, so the later
ldd_mount_opts update is always written. Add conf-sanity test_88a
and rename the existing test_88 to test_88b (a bare test_88
alongside test_88a cannot be selected on its own via ONLY=).

Fixes: 34e1e7b85abc ("LU-19740 utils: Avoid redundant rewrite for tunefs.lustre")
Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: Ie2963fc0d48bb6f56272001ea8889031f2b48a36
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66624
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Xiao Yang <xyang@ddn.com>

lustre/utils/mkfs_lustre.c
@@ -937,8 +937,13 @@ int main(int argc, char *const argv[])
 		goto out;
 	}
 
-	/* Don't write anything when there is no change */
-	if (memcmp(&prev_mop, &mop, sizeof(prev_mop)) == 0) {
+	/*
+	 * Skip the no-change shortcut when -o/--mountfsoptions was given:
+	 * @mountopts is only copied into ldd_mount_opts further below, so
+	 * the change is not yet visible in @mop here.
+	 */
+	if (mountopts == NULL &&
+	    memcmp(&prev_mop, &mop, sizeof(prev_mop)) == 0) {
 		if (verbose > 0)
 			fprintf(stdout,
 				"exiting because there is no change.\n");

lustre/tests/conf-sanity.sh
@@ -7960,7 +7960,33 @@ test_87() { #LU-6544
 }
 run_test 87 "check if MDT inode can hold EAs with N stripes properly"
 
-test_88() {
+test_88a() { # LU-20360
+	[[ "$mds1_FSTYPE" == zfs ]] &&
+		skip "LU-6662: no implementation for ZFS"
+
+	local mdsdev=$(mdsdevname 1)
+	local saved newopts
+
+	stopall
+
+	saved=$(do_facet mds1 "$TUNEFS --dryrun $mdsdev" |
+		sed -n 's/.*Persistent mount opts: //p')
+	[[ -n "$saved" ]] || error "could not read current mount opts"
+	stack_trap "do_facet mds1 \"$TUNEFS --mountfsoptions=$saved $mdsdev\""
+
+	newopts="user_xattr,errors=panic"
+	[[ "$saved" == "$newopts" ]] &&
+		newopts="user_xattr,errors=remount-ro"
+
+	do_facet mds1 "$TUNEFS --mountfsoptions=$newopts $mdsdev" ||
+		error "tunefs --mountfsoptions=$newopts failed"
+	do_facet mds1 "$TUNEFS --dryrun $mdsdev |
+		grep 'Persistent mount opts: $newopts'" ||
+		error "lone --mountfsoptions not persisted"
+}
+run_test 88a "tunefs.lustre persists a --mountfsoptions-only change"
+
+test_88b() {
 	[ "$mds1_FSTYPE" == zfs ] &&
 		skip "LU-6662: no implementation for ZFS"
 
@@ -7981,7 +8007,7 @@ test_88() {
 		grep -e \".*opts:.*errors=panic.*\"" ||
 		error "user can't override default mount options"
 }
-run_test 88 "check the default mount options can be overridden"
+run_test 88b "check the default mount options can be overridden"
 
 test_89() { # LU-7131
 	[[ "$MDS1_VERSION" -ge $(version_code 2.9.54) ]] ||