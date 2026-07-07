e4b8916a9c63ecaecb4308d4b49ea39fb0deaf01
Shivaji Kant <shivajikant@google.com>
2026-07-07 07:10:24 +0000

    LU-20438 llite: Add ll_iov_iter_extract_pages wrapper
    Add ll_iov_iter_extract_pages() as a compatibility wrapper for older
    kernels where iov_iter_extract_pages is not available.
    
    This patch also introduces ll_release_page() for releasing pages
    extracted through this mechanism, and provides a fallback definition
    for ITER_ALLOW_P2PDMA (0) for kernels that lack native support.
    
    Test-Parameters: trivial
    Signed-off-by: Shivaji Kant <shivajikant@google.com>
    Change-Id: I97eac1e51b3291ba2f02817f0578de9471b12471
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66986
    Reviewed-by: Jinshan Xiong <jinshanx@google.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Pranjal Shrivastava <praan@google.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Timothy Day <timday@thelustrecollective.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>

--- include/lustre_compat/linux/uio.h
+++ include/lustre_compat/linux/uio.h
@@ -8,6 +8,7 @@
 #define __LIBCFS_LINUX_UIO_H__
 
 #include <linux/uio.h>
+#include <linux/mm.h>
 
 /*
  * Since 4.20 commit 00e23707442a75b404392cef1405ab4fd498de6b
@@ -43,5 +44,62 @@ static inline ssize_t iov_iter_get_pages_alloc2(struct iov_iter *i,
 	return result;
 }
 #endif
+
+/*
+ * ll_iov_iter_extract_pages - Wrapper for page extraction from iterator.
+ *
+ * iov_iter_extract_pages() (introduced in Linux 6.3) has different pinning
+ * semantics based on the iterator type:
+ * - For user-backed iterators (ITER_IOVEC, ITER_UBUF), it pins the pages.
+ *   These must be released using unpin_user_page().
+ * - For kernel-backed iterators (ITER_KVEC, ITER_BVEC, etc.), it returns the
+ *   pages without taking a reference. No release is required.
+ *
+ * For older kernels, we fall back to iov_iter_get_pages_alloc2(), which
+ * always pins the pages for user-backed iterators (needing put_page() to
+ * release). The @flags argument is intentionally discarded on older kernels,
+ * cleanly degrading any P2P DMA requests to standard get_user_pages()
+ * extraction.
+ *
+ * Lustre tracks whether pages are pinned using 'cdp_pinned' (which is set
+ * based on `iov_iter_extract_will_pin()`) and releases them using
+ * `ll_release_page()`, which wraps `unpin_user_page()` to unpin the pages.
+ */
+#ifndef HAVE_IOV_ITER_EXTRACT_PAGES
+#define ITER_ALLOW_P2PDMA 0
+#endif
+
+#ifdef HAVE_IOV_ITER_EXTRACT_PAGES
+static inline ssize_t ll_iov_iter_extract_pages(struct iov_iter *i,
+						struct page ***pages,
+						size_t maxsize,
+						unsigned int maxpages,
+						iov_iter_extraction_t flags,
+						size_t *start)
+{
+	return iov_iter_extract_pages(i, pages, maxsize, maxpages,
+				      flags, start);
+}
+#else
+static inline ssize_t ll_iov_iter_extract_pages(struct iov_iter *i,
+						struct page ***pages,
+						size_t maxsize,
+						unsigned int maxpages,
+						unsigned int flags,
+						size_t *start)
+{
+	return iov_iter_get_pages_alloc2(i, pages, maxsize, start);
+}
+#endif
+
+static inline void ll_release_page(struct page *page, bool pinned)
+{
+#ifdef HAVE_IOV_ITER_EXTRACT_PAGES
+	if (pinned)
+		unpin_user_page(page);
+#else
+	put_page(page);
+#endif
+}
 
 #endif /* __LIBCFS_LINUX_UIO_H__ */
