lustre/lustre-release

commit 04aa5e079b2650729704838647acfe649367ef65

LU-8229 ssk: support removing keys from keyring via lgss_sk
This patch improves usability and key handling with lgss_sk and the
kernel's keyring by allowing removing a key from the keyring via its
key file through lgss_sk. This is useful because it is not possible
to distinguish keys by using keyctl where only the "key name" format
is shown. The corresponding key file might have a different name where
users would need to consult "lgss_sk -r" to understand whether it is
the correct key to remove.

Using "lgss_sk -r" may also not always possible due to insufficient
permissions to compare the output of "lgss_sk -r" to identify the
keys.

This patch adds a new argument to lgss_sk: "-R/--remove". The remove
option takes a keyfile and compares its payload to find all matching
keys in the keyring. Each key is then revoked and unlinked from the
keyring.

Sanity-sec test_104 was added to test this feature.

Test-Parameters: trivial
Test-Parameters: testgroup=review-dne-selinux-ssk-part-1
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Signed-off-by: Marc Vef <mvef@whamcloud.com>
Change-Id: Ic043a611599160a8230ea9f48ba11b7f92feb5b0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62593
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/gss/sk_utils.c
@@ -377,31 +377,39 @@ out_close:
  * Build an array of all keys in the user keyring that start with
  * the given description.
  *
- * \param[in]	description    description to match keys against
+ * \param[in]	description   description to match keys against
+ * \param[out]	keys_array    found array of keys, last element is a zero key
  *
- * \retval	array of keys, last element is a zero key
- *		NULL in case of error
+ * \retval	0		success
+ * \retval	-errno		on failure
  */
-static key_serial_t *build_keys_array(char *description)
+static int build_keys_array(char *description, key_serial_t **keys_array)
 {
 	key_serial_t *tmp_keys, *curr_key;
-	key_serial_t *keys_array;
+	key_serial_t *keys_array_;
 	ssize_t user_kr_len;
 	int i;
+
+	if (!keys_array)
+		return -EINVAL;
 
 	user_kr_len = keyctl_read_alloc(KEY_SPEC_USER_KEYRING,
 					(void **)&tmp_keys);
 	if (user_kr_len < 0) {
 		printerr(0, "Cannot find any key in user keyring\n");
-		return NULL;
+		*keys_array = NULL;
+		return -ENOENT;
 	}
 
-	keys_array = malloc((user_kr_len / sizeof(key_serial_t) + 1) *
+	keys_array_ = malloc((user_kr_len / sizeof(key_serial_t) + 1) *
 			    sizeof(key_serial_t));
-	if (!keys_array)
-		return NULL;
+	if (!keys_array_) {
+		free(tmp_keys);
+		*keys_array = NULL;
+		return -ENOMEM;
+	}
 
-	curr_key = keys_array;
+	curr_key = keys_array_;
 	for (i = 0; i < user_kr_len / sizeof(key_serial_t); i++) {
 		key_serial_t k = tmp_keys[i];
 		char *desc = NULL, *sep;
@@ -432,7 +440,10 @@ next:
 	*curr_key = 0;
 
 	free(tmp_keys);
-	return keys_array;
+
+	*keys_array = keys_array_;
+
+	return 0;
 }
 
 /**
@@ -472,9 +483,17 @@ static key_serial_t sk_load_key(const struct sk_keyfile_config *skc,
 			size_t desclen = strlen(description);
 			struct timeval tv;
 			struct tm *tm;
+			int rc;
 
 			/* get list of former keys */
-			keys = build_keys_array(description);
+			rc = build_keys_array(description, &keys);
+			if (rc) {
+				printerr(0,
+					 "Failed to build keys array: rc=%d\n",
+					 rc);
+				free(keys);
+				return -1;
+			}
 
 			gettimeofday(&tv, NULL);
 			tm = localtime(&tv.tv_sec);
@@ -593,9 +612,9 @@ read_sk:
 	 * identify it */
 	if (config->skc_type & SK_TYPE_MGS) {
 		/* Any key can be an MGS key as long as we are told to use it */
-		rc = snprintf(description, SK_DESCRIPTION_SIZE, "lustre:MGS:%s",
+		rc = snprintf(description, sizeof(description), "lustre:MGS:%s",
 			      config->skc_nodemap);
-		if (rc >= SK_DESCRIPTION_SIZE) {
+		if (rc >= sizeof(description)) {
 			rc = -ENAMETOOLONG;
 			goto out;
 		}
@@ -629,9 +648,9 @@ read_sk:
 			rc = -ENOKEY;
 			goto out;
 		}
-		rc = snprintf(description, SK_DESCRIPTION_SIZE, "lustre:%s:%s",
+		rc = snprintf(description, sizeof(description), "lustre:%s:%s",
 			      config->skc_fsname, config->skc_nodemap);
-		if (rc >= SK_DESCRIPTION_SIZE) {
+		if (rc >= sizeof(description)) {
 			rc = -ENAMETOOLONG;
 			goto out;
 		}
@@ -685,11 +704,11 @@ read_sk:
 			/* add random chars at end of key if requested... */
 			if (randomize)
 				snprintf(rand, sizeof(rand), "%.4lX", random());
-			rc = snprintf(description, SK_DESCRIPTION_SIZE,
+			rc = snprintf(description, sizeof(description),
 				      "lustre:%s%s%s", config->skc_fsname,
 				      randomize || mntdir ? ":" : "",
 				      randomize ? rand : (mntdir ? uuid : ""));
-			if (rc >= SK_DESCRIPTION_SIZE) {
+			if (rc >= sizeof(description)) {
 				rc = -ENAMETOOLONG;
 				goto out;
 			}
@@ -701,9 +720,9 @@ read_sk:
 
 			/* ... and also load legacy key desc for interop */
 			if (randomize) {
-				rc = snprintf(description, SK_DESCRIPTION_SIZE,
+				rc = snprintf(description, sizeof(description),
 					      "lustre:%s", config->skc_fsname);
-				if (rc < SK_DESCRIPTION_SIZE)
+				if (rc < sizeof(description))
 					(void)sk_load_key(config, description,
 							  false, -1);
 			}
@@ -713,10 +732,10 @@ read_sk:
 		for (i = 0; i < MAX_MGSNIDS; i++) {
 			if (config->skc_mgsnids[i] == LNET_NID_ANY)
 				continue;
-			rc = snprintf(description, SK_DESCRIPTION_SIZE,
+			rc = snprintf(description, sizeof(description),
 				      "lustre:MGC%s",
 				      libcfs_nid2str(config->skc_mgsnids[i]));
-			if (rc >= SK_DESCRIPTION_SIZE) {
+			if (rc >= sizeof(description)) {
 				rc = -ENAMETOOLONG;
 				goto out;
 			}
@@ -732,6 +751,158 @@ out:
 	free(config);
 	if (keyid > 0)
 		return keyid;
+	return rc;
+}
+
+/**
+ * Checks if keys matching \a description are found in the keyring. Each key is
+ * verified against the key from \a skc, and if it matches, the key is revoked
+ * and removed from the keyring.
+ *
+ * \param[in]	skc		keyfile config to load
+ * \param[in]	description	Description used for key in keyring
+ *
+ * \retval	0		success (at least one key removed)
+ * \retval	< 0		failure (no key removed)
+ */
+static int sk_remove_key(const struct sk_keyfile_config *skc, char *description)
+{
+	key_serial_t *keys = NULL;
+	struct sk_keyfile_config file_payload;
+	struct sk_keyfile_config *keyring_payload;
+	size_t key_bytes;
+	int removed_cnt = 0;
+	int i, payload_len, rc;
+
+	memcpy(&file_payload, skc, sizeof(*skc));
+	/* store key length in bytes for later key comparison before swab */
+	key_bytes = file_payload.skc_shared_keylen / 8;
+
+	/* In the keyring use the disk layout so keyctl pipe can be used */
+	sk_config_cpu_to_disk(&file_payload);
+
+	rc = build_keys_array(description, &keys);
+	if (rc)
+		return rc;
+
+	/* for each key on the keyring, sanity check first before unlink:
+	 * 1. skc_shared_keylen matches for the file key and keyring key
+	 * 2. skc_shared_key matches for the file key and keyring key
+	 */
+	for (i = 0; keys[i] != 0; i++) {
+		payload_len = keyctl_read_alloc(keys[i], (void **)&keyring_payload);
+		if (payload_len < 0) {
+			printerr(0, "failed to read key %d - skipping\n", keys[i]);
+			continue;
+		}
+		if (payload_len != sizeof(struct sk_keyfile_config)) {
+			printerr(0, "key %d has unexpected size %d - skipping\n",
+				keys[i], payload_len);
+			goto next;
+		}
+
+		if (keyring_payload->skc_shared_keylen !=
+		    file_payload.skc_shared_keylen) {
+			printerr(1,
+				"key %d has different shared key length - skipping\n",
+				keys[i]);
+			goto next;
+		}
+
+		if (memcmp(keyring_payload->skc_shared_key,
+			   file_payload.skc_shared_key, key_bytes) != 0) {
+			printerr(1,
+				 "key %d has different shared key - skipping\n",
+				 keys[i]);
+			goto next;
+		}
+
+		rc = keyctl_revoke(keys[i]);
+		if (rc == -1) {
+			printerr(0, "failed to revoke key %d: %s\n", keys[i],
+				 strerror(errno));
+			goto next;
+		}
+		rc = keyctl_unlink(keys[i], KEY_SPEC_USER_KEYRING);
+		if (rc == -1) {
+			printerr(0, "failed to unlink key %d: %s\n", keys[i],
+				 strerror(errno));
+			goto next;
+		}
+		printerr(2, "Key %d revoked and removed from keyring\n",
+			 keys[i]);
+		removed_cnt++;
+
+next:
+		free(keyring_payload);
+	}
+
+	free(keys);
+
+	if (removed_cnt > 0)
+		printerr(1, "Removed %d key(s) from keyring\n", removed_cnt);
+	else
+		printerr(0, "No matching key found to remove\n");
+
+	return removed_cnt > 0 ? 0 : -ENOKEY;
+}
+
+/**
+ * Removes the key from \a path, verifies it and removes matching keys from the
+ * keyring.
+ *
+ * \param[in]	path		Path to key file
+ *
+ * \retval	0		success (at least one key removed)
+ * \retval	< 0		failure (no key removed)
+ */
+int sk_remove_keyfile(char *path)
+{
+	struct sk_keyfile_config *config;
+	char description[SK_DESCRIPTION_SIZE + 1] = { 0 };
+	struct stat buf;
+	int rc;
+
+	rc = stat(path, &buf);
+	if (rc == -1) {
+		printerr(0, "stat() failed for file %s: %s\n", path,
+			 strerror(errno));
+		return -errno;
+	}
+
+	config = sk_read_file(path);
+	if (!config)
+		return -ENOKEY;
+
+	rc = sk_validate_config(config);
+	if (rc != 0) {
+		rc = -EINVAL;
+		goto out;
+	}
+
+	if (config->skc_type & SK_TYPE_MGS) {
+		rc = snprintf(description, sizeof(description), "lustre:MGS:%s",
+			      config->skc_nodemap);
+	} else if (config->skc_type & SK_TYPE_SERVER) {
+		rc = snprintf(description, sizeof(description), "lustre:%s:%s",
+			      config->skc_fsname, config->skc_nodemap);
+	} else if (config->skc_type & SK_TYPE_CLIENT) {
+		rc = snprintf(description, sizeof(description), "lustre:%s",
+			      config->skc_fsname);
+	} else {
+		printerr(0, "Invalid key type\n");
+		rc = -EINVAL;
+		goto out;
+	}
+
+	if (rc >= sizeof(description)) {
+		rc = -ENAMETOOLONG;
+		goto out;
+	}
+
+	rc = sk_remove_key(config, description);
+out:
+	free(config);
 	return rc;
 }
 
@@ -1163,17 +1334,17 @@ struct sk_cred *sk_create_cred(const char *tgt, const char *nodemap,
 build_desc:
 	if (nodemap) {
 		if (mgsnid)
-			rc = snprintf(description, SK_DESCRIPTION_SIZE,
+			rc = snprintf(description, sizeof(description),
 				      "lustre:MGS:%s", nodemap);
 		else
-			rc = snprintf(description, SK_DESCRIPTION_SIZE,
+			rc = snprintf(description, sizeof(description),
 				      "lustre:%s:%s", fsname, nodemap);
 	} else {
-		rc = snprintf(description, SK_DESCRIPTION_SIZE, "lustre:%s%s%s",
+		rc = snprintf(description, sizeof(description), "lustre:%s%s%s",
 			      fsname, uuid ? ":" : "", uuid ?: "");
 	}
 
-	if (rc >= SK_DESCRIPTION_SIZE) {
+	if (rc >= sizeof(description)) {
 		printerr(0, "Invalid key description\n");
 		return NULL;
 	}
@@ -1184,9 +1355,12 @@ build_desc:
 			return NULL;
 		}
 
-		*user_keys_p = build_keys_array(description);
-		if (!*user_keys_p)
+		rc = build_keys_array(description,
+				      (key_serial_t **)user_keys_p);
+		if (rc) {
+			printerr(0, "Failed to build keys array: rc=%d\n", rc);
 			return NULL;
+		}
 		*key_p = *user_keys_p;
 	}
 

lustre/tests/sanity-sec.sh
@@ -12115,6 +12115,11 @@ test_93() {
 }
 run_test 93 "incremental RBAC role modifications"
 
+clear_client_keyring() {
+	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' |
+		xargs -IX keyctl unlink X 2>/dev/null || true
+}
+
 cleanup_100() {
 	local orig_sk_path="$1"
 	local test_key="$orig_sk_path/$FSNAME-test100.key"
@@ -12194,8 +12199,7 @@ test_100() {
 	fi
 
 	# Clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' | \
-    		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# Mount client using skpath pointing to the server key
 	export SK_PATH=$test_key
@@ -12394,8 +12398,7 @@ test_102() {
 		"keyctl show | grep lustre" || error "no lustre keys loaded"
 
 	# Clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' |
-		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# Mount should fail without .lgss key file and without skpath
 	echo "Mount client (1): $MOUNT_CMD -o user_xattr,flock $MGSNID:/$FSNAME $MOUNT"
@@ -12440,8 +12443,7 @@ cleanup_103() {
 		xargs -IX keyctl unlink X" || true
 
 	# clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' |
-		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# Remove test key file from all nodes
 	do_nodes $(comma_list $(all_nodes)) "rm -f $test_key" ||
@@ -12515,8 +12517,7 @@ test_103() {
 
 	umount_client $MOUNT || error "umount $MOUNT failed (2)"
 	# clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' | \
-		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# remount client: should work by using new key
 	$LGSS_SK -t client -m $test_key || error "failed to generate client key"
@@ -12529,8 +12530,7 @@ test_103() {
 
 	umount_client $MOUNT || error "umount $MOUNT failed (3)"
 	# clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' | \
-		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# remount with former key
 	$MOUNT_CMD -o $MOUNT_OPTS,skpath=$SK_PATH/$FSNAME.key \
@@ -12552,8 +12552,7 @@ test_103() {
 
 	umount_client $MOUNT || error "umount $MOUNT failed (4)"
 	# clear any existing keys from keyring on client
-	keyctl show | grep lustre | cut -c1-11 | sed -e 's/ //g;' | \
-		xargs -IX keyctl unlink X 2>/dev/null || true
+	clear_client_keyring
 
 	# remount client: should work by using new key
 	$LGSS_SK -t client -m $test_key || error "failed to generate client key"
@@ -12565,6 +12564,131 @@ test_103() {
 		error "file $DIR/$tdir/$tfile does not exist (4)"
 }
 run_test 103 "Multiple SSK keys on server side"
+
+cleanup_104() {
+	local test_key=$1
+	local nm=$2
+
+	keyctl show
+
+	umount_client $MOUNT || true
+	umount_client $MOUNT2 || true
+
+	# clear all keys on client
+	clear_client_keyring
+
+	# remove key files and nodemap
+	rm -f ${test_key}*
+	do_facet mgs $LCTL nodemap_del $nm || true
+
+	# load original key back and remount client
+	$LGSS_SK -l $SK_PATH/$FSNAME.key ||
+		error "failed to load original key on client"
+
+	zconf_mount $HOSTNAME $MOUNT || error "re-mount $MOUNT failed"
+	if [[ "$MOUNT_2" ]]; then
+		zconf_mount $HOSTNAME $MOUNT2 ||
+			error "remount $MOUNT2 failed"
+	fi
+	wait_ssk
+}
+
+test_104() {
+	local test_key="${SK_PATH}/${FSNAME}-test${testnum}"
+	local mgs_key="${test_key}-mgs.key"
+	local server_key="${test_key}-server.key"
+	local server_key2="${test_key}-server2.key"
+	local client_key="${test_key}-client.key"
+	local nm=test_${testnum}
+	local key_cnt
+
+	$SHARED_KEY || skip "Need shared key feature for this test"
+
+	local_mode && skip "in local mode."
+
+	# needs lgss_sk -s support for this test
+	(( MDS1_VERSION >= $(version_code 2.17.50) )) ||
+		skip "Need MDS >= 2.17.50 for multiple similar keys"
+
+	stack_trap "cleanup_104 $test_key $nm" EXIT
+
+	# clear all keys on client
+	clear_client_keyring
+
+	umount_client $MOUNT || error "umount client at $MOUNT failed"
+	if is_mounted $MOUNT2; then
+		umount_client $MOUNT2 || error "umount $MOUNT2 failed"
+	fi
+
+	do_facet mgs $LCTL nodemap_add $nm
+
+	# generate a key for each type for the same fs
+	$LGSS_SK -t mgs -f $FSNAME -w $mgs_key -n $nm -d /dev/urandom -p 512 ||
+		error "failed to create MGS key"
+	$LGSS_SK -t server -f $FSNAME -w $server_key -n $nm -d /dev/urandom \
+		-p 512 || error "failed to create server key"
+	$LGSS_SK -t server -f $FSNAME -w $server_key2 -n $nm -d /dev/urandom \
+		-p 512 || error "failed to create server key 2"
+	$LGSS_SK -t client -f $FSNAME -w $client_key -n $nm -d /dev/urandom \
+		-p 512 || error "failed to create client key"
+
+	# No need to distribute keys since this test only interacts with lgss_sk
+	# and the kernel keyring. Key loading, etc, is exercised in other tests.
+
+	# load all keys including with suffixes
+	$LGSS_SK -t mgs -l $mgs_key -vvv || error "failed to load MGS key (1)"
+	$LGSS_SK -t mgs -l $mgs_key -vvv -s ||
+		error "failed to load MGS key (2)"
+	$LGSS_SK -t server -l $server_key -vvv ||
+		error "failed to load server key (1)"
+	$LGSS_SK -t server -l $server_key -vvv -s ||
+		error "failed to load server key (2)"
+	# also use a different server key with same name
+	# remove must be able to distinguish keys later based on payload
+	$LGSS_SK -t server -l $server_key2 -vvv -s ||
+		error "failed to load server key (3)"
+	$LGSS_SK -t server -l $server_key2 -vvv -s ||
+		error "failed to load server key (4)"
+	$LGSS_SK -t client -l $client_key -vvv ||
+		error "failed to load client key"
+
+	# sanity check that all keys were loaded
+	key_cnt=$(keyctl show | grep lustre | wc -l)
+	(( key_cnt == 7 )) ||
+		error "wrong # keys in keyring after loading. Expected 7, got $key_cnt (1)"
+
+	# remove mgs keys
+	$LGSS_SK -R $mgs_key -vvv ||
+		error "failed to remove MGS key"
+
+	key_cnt=$(keyctl show | grep lustre | wc -l)
+	(( key_cnt == 5 )) ||
+		error "wrong # keys in keyring after removing MGS key. Expected 5, got $key_cnt (2)"
+
+	# remove client key
+	$LGSS_SK -R $client_key -vvv ||
+		error "failed to remove client keys"
+
+	key_cnt=$(keyctl show | grep lustre | wc -l)
+	(( key_cnt == 4 )) ||
+		error "wrong # keys in keyring after removing client key. Expected 4, got $key_cnt (3)"
+
+	# remove server keys
+	$LGSS_SK -R $server_key -vvv ||
+		error "failed to remove server keys"
+
+	key_cnt=$(keyctl show | grep lustre | wc -l)
+	(( key_cnt == 2 )) ||
+		error "wrong # keys in keyring after removing server key. Expected 2, got $key_cnt (4)"
+
+	$LGSS_SK -R $server_key2 -vvv ||
+		error "failed to remove server keys (2)"
+
+	key_cnt=$(keyctl show | grep lustre | wc -l)
+	(( key_cnt == 0 )) ||
+		error "wrong # keys in keyring after removing server key. Expected 0, got $key_cnt (5)"
+}
+run_test 104 "SSK key removal from keyring"
 
 test_300() {
 	local principal="mock_iam_test"

lustre/utils/gss/lgss_sk.c
@@ -46,6 +46,7 @@ static void usage(FILE *fp, char *program)
 		"session keyring\n");
 	fprintf(fp, "-m|--modify     <keyfile>	Modify keyfile's attributes\n");
 	fprintf(fp, "-r|--read       <keyfile>	Show keyfile's attributes\n");
+	fprintf(fp, "-R|--remove     <keyfile>	Remove key from user's session keyring\n");
 	fprintf(fp, "-w|--write      <keyfile>	Generate keyfile\n\n");
 	fprintf(fp, "Modify/Write Options:\n");
 	fprintf(fp, "-a|--ascii      <keyfile>	Output key in ASCII-encoded format\n");
@@ -261,13 +262,14 @@ int main(int argc, char **argv)
 {
 	struct sk_keyfile_config *config;
 	char *datafile = NULL;
+	char *fsname = NULL;
 	char *input = NULL;
 	char *load = NULL;
 	char *modify = NULL;
-	char *output = NULL;
 	char *mgsnids = NULL;
 	char *nodemap = NULL;
-	char *fsname = NULL;
+	char *output = NULL;
+	char *remove = NULL;
 	char *update_path = NULL;
 	char *tmp;
 	char *tmp2;
@@ -302,6 +304,7 @@ int main(int argc, char **argv)
 	{ .name = "nodemap",	.has_arg = required_argument, .val = 'n'},
 	{ .name = "prime-bits",	.has_arg = required_argument, .val = 'p'},
 	{ .name = "read",	.has_arg = required_argument, .val = 'r'},
+	{ .name = "remove",	.has_arg = required_argument, .val = 'R'},
 	{ .name = "suffix",	.has_arg = no_argument,	      .val = 's'},
 	{ .name = "type",	.has_arg = required_argument, .val = 't'},
 	{ .name = "update",	.has_arg = required_argument, .val = 'u'},
@@ -311,7 +314,7 @@ int main(int argc, char **argv)
 	{ .name = NULL, } };
 
 	while ((opt = getopt_long(argc, argv,
-				  "ac:d:e:f:g:hi:k:l:m:n:p:r:st:u:vw:x:",
+				  "ac:d:e:f:g:hi:k:l:m:n:p:r:R:st:u:vw:x:",
 				  long_opts, NULL)) != EOF) {
 		switch (opt) {
 		case 'a':
@@ -379,6 +382,9 @@ int main(int argc, char **argv)
 		case 'r':
 			input = optarg;
 			break;
+		case 'R':
+			remove = optarg;
+			break;
 		case 's':
 			suffix = true;
 			break;
@@ -433,7 +439,13 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if (!input && !output && !load && !modify) {
+	if (!input && !output && !load && !modify && !remove) {
+		usage(stderr, argv[0]);
+		return EXIT_FAILURE;
+	}
+	if (remove && (load || modify || input || output)) {
+		fprintf(stderr,
+			"error: remove option cannot be combined with other operations\n");
 		usage(stderr, argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -465,6 +477,17 @@ int main(int argc, char **argv)
 		if (rc < 0) {
 			fprintf(stderr,
 				"error: loading keyfile failed: rc=%d\n", rc);
+			return EXIT_FAILURE;
+		}
+		return EXIT_SUCCESS;
+	}
+
+	if (remove) {
+		int rc = sk_remove_keyfile(remove);
+
+		if (rc < 0) {
+			fprintf(stderr, "error: removing key failed: rc=%d\n",
+				rc);
 			return EXIT_FAILURE;
 		}
 		return EXIT_SUCCESS;

lustre/utils/gss/sk_utils.h
@@ -394,6 +394,7 @@ int write_config_file(char *output_file, struct sk_keyfile_config *config,
 struct sk_keyfile_config *sk_read_file(char *filename);
 int sk_load_keyfile(char *path, bool client, bool randomize, char *mntdir,
 		    bool suffix, int timeout);
+int sk_remove_keyfile(char *path);
 void sk_config_disk_to_cpu(struct sk_keyfile_config *config);
 void sk_config_cpu_to_disk(struct sk_keyfile_config *config);
 int sk_validate_config(const struct sk_keyfile_config *config);

commit 3fe9a55cb715d8d82df7e6c9624a6d6b335d5f7a

LU-20374 gss: link @u into @s in sk_load_key before setperm
sk_load_key() add_key()s an SSK key into @u then calls
keyctl_setperm() to broaden permissions from the kernel
default 0x3f010000 (POS_ALL | USR_VIEW) to 0x3f3f3f3f. The
setperm needs SETATTR on the key, which the default grants
only via possession (POS_SETATTR is set; USR_SETATTR is
not). Possession requires @u to be linked into the caller's
session keyring chain.

pam_keyinit links @u into @s at interactive PAM login, but
is not part of the default PAM stack for sudo, su, systemd
services, cron, or Pacemaker resource agents. In those
contexts setperm silently fails with EACCES (logged only at
printerr level 2) and the key keeps the kernel default. The
kernel's later request_key upcall to lgss_keyring cannot
keyctl_read() a USR_VIEW-only key, and mount.lustre returns
ENOKEY. lsvcgssd hits the same EACCES when validating
inbound GSS context init RPCs.

Fix: factor sk_link_user_keyring(), which keyctl_link()s @u
into @s so the caller possesses the keys held in @u, and
call it at the top of sk_load_key() before add_key() /
keyctl_setperm(). keyctl_link() is idempotent (success, or
EEXIST which is treated as success, when @u is already
linked). Call it from sk_remove_key() too: 'lgss_sk -R'
keyctl_read()s and keyctl_revoke()s keys in @u, which also
need possession to act on a key still at the restrictive
default, so without this -R cannot clean up a mis-permed
key. Also promote the keyctl_setperm() failure message from
printerr level 2 to level 0 so a future failure is visible
at the default debug level.

Add sanity-sec test_200 that loads a key inside a fresh
anonymous session keyring (via 'keyctl session -') and
asserts the resulting /proc/keys perm is 3f3f3f3f.

Finally, document the existing -R/--remove option in the
lgss_sk.8 man page.

Fixes: 04aa5e079b26 ("LU-8229 ssk: support removing keys from keyring via lgss_sk")
Fixes: f26503384099 ("LU-14479 ssk: explicitly set perm on key")

Documentation/man8/lgss_sk.8
@@ -1,20 +1,25 @@
-.TH LGSS_SK 8 2024-08-29 Lustre "Lustre Configuration Utilities"
+.TH LGSS_SK 8 2026-06-25 Lustre "Lustre Configuration Utilities"
 .SH NAME
 lgss_sk \- Lustre GSS Shared-Key tool
 .SH SYNOPSIS
 .SY lgss_sk
 .RI [ OPTIONS ]
-.RB { -r | -w | -m | -l }
+.RB { -r | -w | -m | -l | -R }
 .I KEYFILE
+.YS
 .SH DESCRIPTION
 .B lgss_sk
-can be used to read, write, modify, and load the contents of a shared-key keyfile.
+can be used to read, write, modify, and load the contents of a shared-key
+keyfile, and to remove a loaded key from the kernel keyring.
 .SH OPTIONS
 .B lgss_sk
 accepts the following options:
 .TP
 .BR -l ", " --load \ \fIKEYFILE
 Load key from file into user's session keyring.
+.TP
+.BR -R ", " --remove \ \fIKEYFILE
+Remove key matching file from user's session keyring.
 .TP
 .BR -m ", " --modify \ \fIKEYFILE
 Modify a file's key attributes.

lustre/utils/gss/sk_utils.c
@@ -446,6 +446,22 @@ next:
 	return 0;
 }
 
+/*
+ * Link @u into the caller's session keyring so we possess the keys we add to
+ * @u. keyctl_setperm() here (and keyctl_read()/keyctl_revoke() in
+ * sk_remove_key()) require a permission the kernel grants on a newly added key
+ * only via possession, not by UID match. @u is linked into @s by pam_keyinit
+ * at interactive login, but not under sudo, systemd, cron, or Pacemaker - so
+ * without this the setperm silently fails and the key keeps restrictive perms.
+ */
+static void sk_link_user_keyring(void)
+{
+	/* EEXIST just means @u was already linked (e.g. interactive login). */
+	if (keyctl_link(KEY_SPEC_USER_KEYRING,
+			KEY_SPEC_SESSION_KEYRING) < 0 && errno != EEXIST)
+		printerr(0, "Failed to link @u to @s: %s\n", strerror(errno));
+}
+
 /**
  * Checks if a key matching \a description is found in the keyring for
  * logging purposes and then attempts to load the payload from \a skc keyfile
@@ -472,6 +488,12 @@ static key_serial_t sk_load_key(const struct sk_keyfile_config *skc,
 
 	/* In the keyring use the disk layout so keyctl pipe can be used */
 	sk_config_cpu_to_disk(&payload);
+
+	/* Possess keys in @u so the keyctl_setperm() below - and the
+	 * keyctl_set_timeout() on former keys - succeed; see
+	 * sk_link_user_keyring().
+	 */
+	sk_link_user_keyring();
 
 	/* Check to see if a key is already loaded matching description */
 	key = keyctl_search(KEY_SPEC_USER_KEYRING, "user", description, 0);
@@ -527,8 +549,9 @@ static key_serial_t sk_load_key(const struct sk_keyfile_config *skc,
 			KEY_GRP_ALL | KEY_OTH_ALL;
 
 		if (keyctl_setperm(key, perm) < 0)
-			printerr(2, "Failed to set perm 0x%x on key %d\n",
-				 perm, key);
+			printerr(0,
+				 "Failed to set perm 0x%x on key %d: %s\n",
+				 perm, key, strerror(errno));
 		printerr(2, "Added key %d with description %s\n", key,
 			 description);
 
@@ -780,6 +803,13 @@ static int sk_remove_key(const struct sk_keyfile_config *skc, char *description)
 
 	/* In the keyring use the disk layout so keyctl pipe can be used */
 	sk_config_cpu_to_disk(&file_payload);
+
+	/* Possess keys in @u so the keyctl_read()/keyctl_revoke() below can act
+	 * on keys that were loaded with the restrictive default perms (e.g. by
+	 * an older mount.lustre, or before this fix); see
+	 * sk_link_user_keyring().
+	 */
+	sk_link_user_keyring();
 
 	rc = build_keys_array(description, &keys);
 	if (rc)

lustre/tests/sanity-sec.sh
@@ -12999,6 +12999,60 @@ test_106() {
 }
 run_test 106 "switch nodemap to gssonly"
 
+unlink_sktest_keys() {
+	# Unlink our test keys from @u directly. List @u with `keyctl list @u`
+	# (not `keyctl show`, which walks the session chain and only reaches @u
+	# when pam_keyinit linked it - the very thing this test avoids relying
+	# on) and unlink with an explicit @u argument.
+	keyctl list @u 2>/dev/null | awk -F: '/lustre:sktest:/ { print $1 }' |
+		xargs -r -I{} keyctl unlink {} @u 2>/dev/null || true
+}
+
+cleanup_200() {
+	unlink_sktest_keys
+	rm -f $1
+}
+
+test_200() {
+	local test_key=$TMP/sk_load_key_perms.key
+	local perm
+
+	$SHARED_KEY || skip "Need shared key feature for this test"
+
+	# Regression for sk_load_key()'s keyctl_setperm() silently failing when
+	# the caller does not possess @u (the sudo / systemd / cron case - no
+	# pam_keyinit). Load a key in a fresh session with no @u link and assert
+	# its perms end up broadened to 0x3f3f3f3f, not the default 0x3f010000.
+
+	stack_trap "cleanup_200 $test_key" EXIT
+
+	# Self-contained server key - metadata is baked into the file, no MGS
+	# needed. Loading it creates one keyring entry: lustre:sktest:default.
+	$LGSS_SK -t server -f sktest -g 127.0.0.1@tcp \
+		-w $test_key -d /dev/urandom -p 512 ||
+		error "failed to create test key"
+
+	# Drop any stale key so add_key() creates a fresh one - an
+	# update-in-place would keep old perms and mask a regression.
+	unlink_sktest_keys
+
+	# `keyctl session -` runs lgss_sk in a new anonymous session keyring
+	# with no @u link - the same starting point sudo / systemd give.
+	keyctl session - $LGSS_SK -l $test_key ||
+		error "lgss_sk -l failed in fresh session keyring"
+
+	# Read the perm mask (column 5) from /proc/keys; we parse it rather
+	# than `keyctl search` because a buggy USR_VIEW-only key has no SEARCH
+	# perm and a search would not find it.
+	perm=$(awk '$9 ~ /^lustre:sktest:default:/ {print $5}' /proc/keys)
+
+	[[ -n "$perm" ]] ||
+		error "key lustre:sktest:default not found in /proc/keys"
+	[[ "$perm" == "3f3f3f3f" ]] || error \
+		"key perm=$perm, expected 3f3f3f3f - sk_load_key did not broaden perms"
+}
+run_test 200 "sk_load_key broadens SSK key perms without @u linked"
+
 test_300() {
 	local principal="mock_iam_test"
 	local mount_opts=$(csa_add "$MOUNT_OPTS" "" "user_principal=$principal")