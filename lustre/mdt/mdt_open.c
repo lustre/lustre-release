lustre/lustre-release

commit db55a36770217315570b1fc3407528e4b41609e5

LU-18847 mdt: version layout checking
During migration, it's possible for a client to have an outdated
stripe and directory entry cached. While holding a CWD reference,
the client may create a directory using the stale layout, resulting
in duplicate entries with the same name - one on the new stripe and
one on the old (demonstrated by sanity test 230B).

This patch introduces layout version tracking between client and
server. The client packs the layout version during create operations,
and the server validates it. Since the OUT layer bypasses
MDT/MDD/LOD during XATTR modifications, stale layout version data
can occur. The osd_check_stale() part detects LMV updates to ensure
the version check uses current data.

HPE-bug-id: LUS-12792
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: Ic8279b7df3a9178a0b6eeaab0d093b74840a79c0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64468
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/mdt/mdt_open.c
@@ -1635,6 +1635,10 @@ again_pw:
 	tgt_open_obj_set(info->mti_env, mdt_obj2dt(child));
 
 	if (result == -ENOENT) {
+
+		if (!mdt_layout_version_check(info, parent, rr->rr_layout_ver))
+			GOTO(out_child, rc = -ESTALE);
+
 		/* Create under OBF and .lustre is not permitted */
 		if (!fid_is_md_operative(rr->rr_fid1) &&
 		    (open_flags & MDS_OPEN_VOLATILE) == 0)

lustre/include/dt_object.h
@@ -1054,6 +1054,19 @@ struct dt_object_operations {
 	bool (*do_check_stale)(struct dt_object *dt);
 
 	/**
+	 * do_change_stale() - Change object stale state.
+	 *
+	 * @dt: object
+	 *
+	 * OSP/OSD only.
+	 *
+	 * Return: previous value, true for stale object, false for not
+	 * stale object
+	 */
+	 bool (*do_change_stale)(struct dt_object *dt, bool val);
+
+
+	/**
 	 * do_declare_layout_change() - Declare intention to instantiate
 	 *                              extended layout component.
 	 *
@@ -2493,6 +2506,17 @@ static inline bool dt_object_stale(struct dt_object *dt)
 		return false;
 
 	return dt->do_ops->do_check_stale(dt);
+}
+
+static inline bool dt_change_stale(struct dt_object *dt, bool val)
+{
+	LASSERT(dt);
+	LASSERT(dt->do_ops);
+
+	if (!dt->do_ops->do_change_stale)
+		return false;
+
+	return dt->do_ops->do_change_stale(dt, val);
 }
 
 static inline int dt_declare_attr_get(const struct lu_env *env,

lustre/include/md_object.h
@@ -158,6 +158,7 @@ enum md_layout_opc {
 	MD_LAYOUT_DETACH,	/* detach stripes */
 	MD_LAYOUT_SHRINK,	/* shrink striped directory (destroy stripes) */
 	MD_LAYOUT_SPLIT,	/* split directory (allocate new stripes) */
+	MD_LAYOUT_VERSION,	/* check layout version */
 	MD_LAYOUT_MAX,
 };
 
@@ -166,7 +167,10 @@ enum md_layout_opc {
  */
 struct md_layout_change {
 	enum md_layout_opc			 mlc_opc;
-	struct lu_buf				 mlc_buf;
+	union {
+		struct lu_buf			 mlc_buf;
+		__u32				 mlc_layout_ver;
+	};
 	union {
 		struct {
 			__u16			 mlc_mirror_id;

lustre/include/obd.h
@@ -1055,6 +1055,8 @@ struct md_op_data {
 	__u32			op_stripe_index;
 	/* Archive ID for PCC attach */
 	__u32			op_archive_id;
+	/* layout version to pass from lmv to mdc level */
+	__u32			op_layout_version;
 };
 
 struct md_readdir_info {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2116,7 +2116,7 @@ struct mdt_rec_create {
 	__u32		cr_opcode;
 	__u32		cr_cap;
 	__u32		cr_fsuid;
-	__u32		cr_padding_1_h;
+	__u32		cr_layout_ver;	/* parent dir layout version */
 	__u32		cr_fsgid;
 	__u32		cr_padding_2_h;
 	__u32		cr_suppgid1;
@@ -2141,7 +2141,7 @@ struct mdt_rec_create {
 	__u32		cr_flags_l;	/* for use with open, low  32 bits  */
 	__u32		cr_flags_h;	/* for use with open, high 32 bits */
 	__u32		cr_umask;	/* umask for create */
-	__u32		cr_padding_4;   /* rr_padding_4 */
+	__u32		cr_padding_4;	/* rr_padding_4 */
 };
 
 /* instance of mdt_reint_rec */

lustre/llite/xattr.c
@@ -180,6 +180,7 @@ static int ll_xattr_set_common(const struct xattr_handler *handler,
 		}
 		GOTO(out, rc);
 	}
+
 	ll_i2info(inode)->lli_synced_to_mds = false;
 
 	ptlrpc_req_put(req);

lustre/lmv/lmv_intent.c
@@ -367,10 +367,18 @@ retry:
 			RETURN(rc);
 	}
 
+	if (lmv_dir_striped(op_data->op_lso1))
+		op_data->op_layout_version =
+			op_data->op_lso1->lso_lsm.lsm_md_layout_version;
+	else
+		op_data->op_layout_version = 1;
+
+
 	CDEBUG(D_INODE, "OPEN_INTENT with fid1="DFID", fid2="DFID","
-	       " name='"DNAME"' -> mds #%u\n", PFID(&op_data->op_fid1),
-	       PFID(&op_data->op_fid2), encode_fn_opdata(op_data),
-	       tgt->ltd_index);
+	       " name='"DNAME"' -> mds #%u layout version %u\n",
+	       PFID(&op_data->op_fid1), PFID(&op_data->op_fid2),
+	       encode_fn_opdata(op_data), tgt->ltd_index,
+	       op_data->op_layout_version);
 
 	rc = md_intent_lock(tgt->ltd_exp, op_data, it, reqp, cb_blocking,
 			    extra_lock_flags);

lustre/lmv/lmv_obd.c
@@ -2258,9 +2258,17 @@ retry:
 	if (rc)
 		RETURN(rc);
 
-	CDEBUG(D_INODE, "CREATE name '"DNAME"' "DFID" on "DFID" -> mds #%x\n",
-		encode_fn_opdata(op_data), PFID(&op_data->op_fid2),
-		PFID(&op_data->op_fid1), op_data->op_mds);
+	if (lmv_dir_striped(op_data->op_lso1))
+		op_data->op_layout_version =
+			op_data->op_lso1->lso_lsm.lsm_md_layout_version;
+	else
+		op_data->op_layout_version = 1;
+
+	CDEBUG(D_INODE, "CREATE name '"DNAME"' "DFID" on "DFID
+	       " -> mds #%x, layout version %u\n",
+	       encode_fn_opdata(op_data), PFID(&op_data->op_fid2),
+	       PFID(&op_data->op_fid1), op_data->op_mds,
+	       op_data->op_layout_version);
 
 	op_data->op_flags |= MF_MDC_CANCEL_FID1;
 	rc = md_create(tgt->ltd_exp, op_data, data, datalen, mode, uid, gid,

commit 39f184afe40dd86c64f99205869fefebceba8177

LU-18847 mdt: return -ESTALE for stale layout on open
mdt_reint_open() returns the variable "result", and the out_child
cleanup path acts on "result" too. When the layout version check
fails, the GOTO assigns -ESTALE to "rc" instead of "result". Since
this branch is only reached with result == -ENOENT, the intended
-ESTALE is dropped and the client gets -ENOENT, so it does not
refresh its stale layout and retry the create as designed.

Assign -ESTALE to "result" so the correct error is returned.

Fixes: db55a3677021 ("LU-18847 mdt: version layout checking")
Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: Ibb282309f87c91c1090976259552f2b6b3d9b259
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66833
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_open.c
@@ -1637,7 +1637,7 @@ again_pw:
 	if (result == -ENOENT) {
 
 		if (!mdt_layout_version_check(info, parent, rr->rr_layout_ver))
-			GOTO(out_child, rc = -ESTALE);
+			GOTO(out_child, result = -ESTALE);
 
 		/* Create under OBF and .lustre is not permitted */
 		if (!fid_is_md_operative(rr->rr_fid1) &&