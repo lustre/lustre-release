lustre/lustre-release

commit d963e66f609c3bf4766fe4f8d6eeb93a5471c569

LU-13903 uapi: fixup UAPI headers for native Linux client.
This covers all the UAPI problems outside of the user land
wiretest utility. One set of problems is build and the second is
that UAPI header definitions are either user land only or never
used to valid data going to or from user land.

1) Use UAPI header definitions to validate data send to or from
   kernel space. We check lum_hash_type using LMV_HASH_TYPE_MASK.
   This avoids a round trip to the server which will report back
   an error. The other case is we check the values returned for
   LL_IOC_HSM_ACTION. We keep the original behavior of passing
   unknown data to the user land application but add debug
   logging if the data looks corrupt to help track down bug
   issues.

2) We can use QIF_DQBLKSIZE* instead of Lustre specific values
   for our quota handling. QIF_DQBLKSIZE* is a Linux UAPI quota
   value.

3) The NOTIFY_GRACE_* macros are used only by user land. Move
   to lustreapi.h

4) A few of the UAPI definitions are used by utility code
   present on the client and the Lustre kernel server code; which
   are not sent over the wire. Handle these special cases. This
   covers the missing LCM_USER_MIRROR_FLAGS, LCME_TEMPLATE_FLAGS,
   and LQUOTA_* values. Once server code merges upstream we can
   clean this up.

5) lcfg_cmd2data() is server specific so in case of a client build
   we can have get_llog_event_name() just always return NULL.

6) Don't package OpenSFS UAPI headers when building for native
   Linux client.

Change-Id: I258ee917b005e438eb7c15fa6e0c4b72e9ea9d56
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/44664
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

config/lustre-build.m4
@@ -148,7 +148,7 @@ AC_MSG_CHECKING([whether to build Linux kernel modules])
 AC_ARG_ENABLE([modules],
 	AC_HELP_STRING([--disable-modules],
 		[disable building of Lustre kernel modules]),
-	[], [
+	[ AC_DEFINE(HAVE_NATIVE_LINUX_CLIENT, 1, [support native Linux client])], [
 		LC_TARGET_SUPPORTED([enable_modules="yes"],
 				    [enable_modules="no"])
 	])

lustre.spec.in
@@ -333,9 +333,6 @@ This package contains the header files needed for building additional
 applications against the Lustre / LNet utilities libraries.
 
 :> lustre-devel.files
-%if %{with lustre_modules}
-find . -type f -name '*.h' | grep uapi | sed 's/.*uapi\//usr\/include\//' >> lustre-devel.files
-%endif
 
 %if %{with lustre_tests}
 %package tests
@@ -630,8 +627,13 @@ echo '%{_libdir}/lustre/tests/lutf/*' >>lustre-tests.files
 %{_libdir}/liblustreapi.so
 %endif
 %{_includedir}/lustre
+%if %{with lustre_modules}
 %{_includedir}/linux/lnet
 %{_includedir}/linux/lustre
+%else
+%exclude %{_includedir}/linux/lnet
+%exclude %{_includedir}/linux/lustre
+%endif
 
 %files -f lustre.files
 %defattr(-,root,root)

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1472,18 +1472,6 @@ struct ost_lvb {
  *   lquota data structures
  */
 
-#ifndef QUOTABLOCK_BITS
-# define QUOTABLOCK_BITS LUSTRE_QUOTABLOCK_BITS
-#endif
-
-#ifndef QUOTABLOCK_SIZE
-# define QUOTABLOCK_SIZE LUSTRE_QUOTABLOCK_SIZE
-#endif
-
-#ifndef toqb
-# define toqb lustre_stoqb
-#endif
-
 /* The lquota_id structure is an union of all the possible identifier types that
  * can be used with quota, this includes:
  * - 64-bit user ID

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1253,12 +1253,10 @@ static inline void obd_uuid2fsname(char *buf, char *uuid, int buflen)
 
 /********* Quotas **********/
 
-#define LUSTRE_QUOTABLOCK_BITS 10
-#define LUSTRE_QUOTABLOCK_SIZE (1 << LUSTRE_QUOTABLOCK_BITS)
-
-static inline __u64 lustre_stoqb(__kernel_size_t space)
+/* From linux/fs/quota/quota.c */
+static inline __u64 toqb(__kernel_size_t space)
 {
-	return (space + LUSTRE_QUOTABLOCK_SIZE - 1) >> LUSTRE_QUOTABLOCK_BITS;
+	return (space + QIF_DQBLKSIZE - 1) >> QIF_DQBLKSIZE_BITS;
 }
 
 #define Q_QUOTACHECK	0x800100 /* deprecated as of 2.4 */

lustre/llite/dir.c
@@ -453,9 +453,10 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 		},
 	};
 	bool encrypt = false;
+	int hash_flags;
 	int err;
-	ENTRY;
 
+	ENTRY;
 	if (unlikely(!lmv_user_magic_supported(lump->lum_magic)))
 		RETURN(-EINVAL);
 
@@ -493,6 +494,10 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 			lump->lum_hash_type = (lump->lum_hash_type ^ type) |
 					      LMV_HASH_TYPE_FNV_1A_64;
 	}
+
+	hash_flags = lump->lum_hash_type & ~LMV_HASH_TYPE_MASK;
+	if (hash_flags & ~LMV_HASH_FLAG_KNOWN)
+		RETURN(-EINVAL);
 
 	if (unlikely(!lmv_user_magic_supported(cpu_to_le32(lump->lum_magic))))
 		lustre_swab_lmv_user_md(lump);

lustre/llite/file.c
@@ -4058,9 +4058,10 @@ out:
 		RETURN(rc);
 	}
 	case LL_IOC_HSM_ACTION: {
-		struct md_op_data		*op_data;
-		struct hsm_current_action	*hca;
-		int				 rc;
+		struct md_op_data *op_data;
+		struct hsm_current_action *hca;
+		const char *action;
+		int rc;
 
 		OBD_ALLOC_PTR(hca);
 		if (hca == NULL)
@@ -4075,10 +4076,26 @@ out:
 
 		rc = obd_iocontrol(cmd, ll_i2mdexp(inode), sizeof(*op_data),
 				   op_data, NULL);
+		if (rc < 0)
+			GOTO(skip_copy, rc);
+
+		/* The hsm_current_action retreived from the server could
+		 * contain corrupt information. If it is incorrect data collect
+		 * debug information. We still send the data even if incorrect
+		 * to user land to handle.
+		 */
+		action = hsm_user_action2name(hca->hca_action);
+		if (strcmp(action, "UNKNOWN") == 0 ||
+		    hca->hca_state > HPS_DONE) {
+			CDEBUG(D_HSM,
+			       "HSM current state %s action %s, offset = %llu, length %llu\n",
+			       hsm_progress_state2name(hca->hca_state), action,
+			       hca->hca_location.offset, hca->hca_location.length);
+		}
 
 		if (copy_to_user((char __user *)arg, hca, sizeof(*hca)))
 			rc = -EFAULT;
-
+skip_copy:
 		ll_finish_md_op_data(op_data);
 		OBD_FREE_PTR(hca);
 		RETURN(rc);

lustre/ptlrpc/wiretest.c
@@ -2049,10 +2049,10 @@ void lustre_assert_wire_constants(void)
 	LASSERTF((int)sizeof(union lquota_id) == 16, "found %lld\n",
 		 (long long)(int)sizeof(union lquota_id));
 
-	LASSERTF(QUOTABLOCK_BITS == 10, "found %lld\n",
-		 (long long)QUOTABLOCK_BITS);
-	LASSERTF(QUOTABLOCK_SIZE == 1024, "found %lld\n",
-		 (long long)QUOTABLOCK_SIZE);
+	LASSERTF(QIF_DQBLKSIZE_BITS == 10, "found %lld\n",
+		 (long long)QIF_DQBLKSIZE_BITS);
+	LASSERTF(QIF_DQBLKSIZE == 1024, "found %lld\n",
+		 (long long)QIF_DQBLKSIZE);
 
 	/* Checks for struct obd_quotactl */
 	LASSERTF((int)sizeof(struct obd_quotactl) == 112, "found %lld\n",

lustre/tests/mirror_io.c
@@ -45,7 +45,7 @@
 #include <sys/param.h>
 #include <err.h>
 
-#include <uapi/linux/lustre/lustre_idl.h>
+#include <linux/lustre/lustre_idl.h>
 #include <lustre/lustreapi.h>
 
 #define syserr(exp, str, args...)					\

commit aa1bc8f5174353e1256b54b60469b112039dde5f

LU-20419 build: Make --enable-modules work correctly
If explicit "--enable-modules" is given as input to
configure, it still leaves HAVE_NATIVE_LINUX_CLIENT
defined. The behavior of default action and
"--enable-modules" should be same. It should not define
HAVE_NATIVE_LINUX_CLIENT. Only --disable-modules should
define HAVE_NATIVE_LINUX_CLIENT

Also, improve configure --help message for --disable-modules

Before patch
=============
./configure --enable-modules
...
cat config.log | grep NATIVE
define HAVE_NATIVE_LINUX_CLIENT 1
...

After patch
=============
./configure --enable-modules
...
cat config.log | grep NATIVE | wc -l
0

Fixes: d963e66f6 ("LU-13903 uapi: fixup UAPI headers for native Linux client.")
Test-Parameters: trivial
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Ie4e52b9a03b7281e66cd68e595ca16cb1f2db268
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66802
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-build.m4
@@ -146,8 +146,15 @@ AC_DEFUN([LB_CONFIG_MODULES], [
 AC_MSG_CHECKING([whether to build Linux kernel modules])
 AC_ARG_ENABLE([modules],
 	AS_HELP_STRING([--disable-modules],
-		[disable building of Lustre kernel modules]),
-	[ AC_DEFINE(HAVE_NATIVE_LINUX_CLIENT, 1, [support native Linux client])], [
+		[disable building of Lustre kernel modules (defaults to --enable-modules)]),
+	[
+		enable_modules="$enableval"
+
+		AS_IF([test "x$enable_modules" = xno], [
+			AC_DEFINE(HAVE_NATIVE_LINUX_CLIENT, 1,
+				[support native Linux client])
+		])
+	], [
 		LC_TARGET_SUPPORTED([enable_modules="yes"],
 				    [enable_modules="no"])
 	])