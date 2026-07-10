Cray/lustre

commit 20527615330ea474044d81917d3f9a7fd2717880

LU-19837 build: Compatibility updates for kernel v6.19
Linux commit v6.18-rc1-6-ge12d203b8c880
  vfs: allow mkdir to wait for delegation break on parent
Provide a compatible vfs_mkdir for older kernels

Linux commit v6.18-rc1-7-gd8753f788ab49
  fs: provide accessors for ->i_state
Provide a compatible inode_state_read() for older kernels

Linux commit v6.18-rc1-10-gc826229c6a82f
  vfs: make vfs_create break delegations on parent directory
Provide a compatible vfs_create for older kernels

Linux commit v6.18-rc1-18-gb37491d72b43c
  interval_tree: Fix ITSTATIC usage for *_subtree_search()
Provide an alternate configure test for INTERVAL_TREE_DEFINE

Linux commit v6.18-rc1-20-ga27628f436343
  fs: rework I_NEW handling to operate without fences
Provide a compatible ilookup5_nowait for older kernels

Linux commit v6.18-rc2-1-g70e0a80a1f358
  treewide: Remove in_irq()
Use in_hardirq() directly

Linux commit v6.18-rc3-603-gbf33247a90d3e
  net: Add struct sockaddr_unsized for sockaddr of unknown length
Provide a mapping of sockaddr_unsized to sockaddr for older kernels

Linux commit v6.18-rc5-51-gfc45aee662232
  get rid of kill_litter_super()
Use kill_anon_super() when kill_litter_super() not available

Test-Parameters: trivial
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I45e8df243829e03c271de8c159a3f2416848f43a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63685
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-core.m4
@@ -3500,6 +3500,115 @@ AC_DEFUN([LC_HAVE_FILE__F_PATH],[
 ]) # LC_HAVE_FILE__F_PATH
 
 #
+## LC_HAVE_VFS_MKDIR_DELEGATE
+#
+# Linux commit v6.18-rc1-6-ge12d203b8c880
+#   vfs: allow mkdir to wait for delegation break on parent
+#
+AC_DEFUN([LC_SRC_HAVE_VFS_MKDIR_DELEGATE],[
+	LB2_LINUX_TEST_SRC([vfs_mkdir_delegate], [
+		#include <linux/fs.h>
+	],[
+		struct mnt_idmap *idmap = NULL;
+		struct inode *dir = NULL;
+		struct dentry *dentry = NULL;
+		umode_t mode = 0;
+		struct delegated_inode *di = NULL;
+		struct dentry *result = vfs_mkdir(idmap, dir, dentry, mode, di);
+
+		(void)result;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_VFS_MKDIR_DELEGATE],[
+	LB2_MSG_LINUX_TEST_RESULT([if vfs_mkdir() takes delegate],
+	[vfs_mkdir_delegate], [
+		AC_DEFINE(HAVE_VFS_MKDIR_DELEGATE, 1,
+			  [vfs_mkdir() takes delegate])
+	])
+]) # LC_HAVE_VFS_MKDIR_DELEGATE
+
+#
+## LC_HAVE_INODE_STATE_READ
+#
+# Linux commit v6.18-rc1-7-gd8753f788ab49
+#   fs: provide accessors for ->i_state
+#
+AC_DEFUN([LC_SRC_HAVE_INODE_STATE_READ],[
+	LB2_LINUX_TEST_SRC([inode_state_read], [
+		#include <linux/fs.h>
+	],[
+		struct inode *inode = NULL;
+		enum inode_state_flags_enum state = inode_state_read(inode);
+
+		(void)state;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_INODE_STATE_READ],[
+	LB2_MSG_LINUX_TEST_RESULT([if inode_state_read() exists],
+	[inode_state_read], [
+		AC_DEFINE(HAVE_INODE_STATE_READ, 1,
+			  [inode_state_read() exists])
+	], [
+		AC_DEFINE([inode_state_read(inode)], [((inode)->i_state)],
+			  [inode_state_read() does not exist, provide one])
+	])
+]) # LC_HAVE_INODE_STATE_READ
+
+#
+## LC_HAVE_VFS_CREATE_DELEGATE
+#
+# Linux commit v6.18-rc1-10-gc826229c6a82f
+#   vfs: make vfs_create break delegations on parent directory
+#
+AC_DEFUN([LC_SRC_HAVE_VFS_CREATE_DELEGATE],[
+	LB2_LINUX_TEST_SRC([vfs_create_delegate], [
+		#include <linux/fs.h>
+	],[
+		struct mnt_idmap *idmap = NULL;
+		struct dentry *dentry = NULL;
+		umode_t mode = 0;
+		struct delegated_inode *di = NULL;
+		int rc = vfs_create(idmap, dentry, mode, di);
+		(void)rc;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_VFS_CREATE_DELEGATE],[
+	LB2_MSG_LINUX_TEST_RESULT([if vfs_create() takes delegate],
+	[vfs_create_delegate], [
+		AC_DEFINE(HAVE_VFS_CREATE_DELEGATE, 1,
+			  [vfs_create() takes delegate])
+	])
+]) # LC_HAVE_VFS_CREATE_DELEGATE
+
+#
+## LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+#
+# Linux commit v6.18-rc1-20-ga27628f436343
+#   fs: rework I_NEW handling to operate without fences
+#
+AC_DEFUN([LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
+	LB2_LINUX_TEST_SRC([ilookup5_nowait_isnew], [
+		#include <linux/fs.h>
+
+		static int test(struct inode *inode, void *data) { return 0; }
+	],[
+		struct super_block *sb = NULL;
+		bool isnew;
+		struct inode *node = ilookup5_nowait(sb, 0, test, NULL, &isnew);
+
+		(void)node;
+		(void)isnew;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
+	LB2_MSG_LINUX_TEST_RESULT([if ilookup5_nowait() takes isnew],
+	[ilookup5_nowait_isnew], [
+		AC_DEFINE(HAVE_ILOOKUP5_NOWAIT_ISNEW, 1,
+			  [ilookup5_nowait() takes isnew])
+	])
+]) # LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+
+#
 # LC_PROG_LINUX
 #
 # Lustre linux kernel checks
@@ -3698,6 +3807,12 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_MEMDESC_FLAGS_T
 	LC_SRC_HAVE_DENTRY__D_NAME
 	LC_SRC_HAVE_FILE__F_PATH
+
+	# 6.19
+	LC_SRC_HAVE_VFS_MKDIR_DELEGATE
+	LC_SRC_HAVE_INODE_STATE_READ
+	LC_SRC_HAVE_VFS_CREATE_DELEGATE
+	LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 ])
 
 AC_DEFUN([LC_PROG_LINUX_RESULTS], [
@@ -3909,6 +4024,12 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_MEMDESC_FLAGS_T
 	LC_HAVE_DENTRY__D_NAME
 	LC_HAVE_FILE__F_PATH
+
+	# 6.19
+	LC_HAVE_VFS_MKDIR_DELEGATE
+	LC_HAVE_INODE_STATE_READ
+	LC_HAVE_VFS_CREATE_DELEGATE
+	LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 ])
 
 #

lustre/llite/lcommon_cl.c
@@ -155,7 +155,7 @@ int cl_file_inode_init(struct inode *inode, struct lustre_md *md)
 		 * unnecessary to perform lookup-alloc-lookup-insert, just
 		 * alloc and insert directly.
 		 */
-		if (!(inode->i_state & I_NEW)) {
+		if (!(inode_state_read(inode) & I_NEW)) {
 			result = -EIO;
 			CERROR("%s: unexpected not-NEW inode "DFID": rc = %d\n",
 			       ll_i2sbi(inode)->ll_fsname, PFID(fid), result);

lustre/llite/llite_lib.c
@@ -1626,8 +1626,8 @@ struct inode *ll_inode_from_resource_lock(struct ldlm_lock *lock)
 			}
 		} else {
 			inode = lock->l_resource->lr_lvb_inode;
-			LDLM_DEBUG_LIMIT(inode->i_state & I_FREEING ?  D_INFO :
-					 D_WARNING, lock,
+			LDLM_DEBUG_LIMIT(inode_state_read(inode) &
+					 I_FREEING ?  D_INFO : D_WARNING, lock,
 					 "lr_lvb_inode %p is bogus: magic %08x",
 					 lock->l_resource->lr_lvb_inode,
 					 lli->lli_inode_magic);
@@ -1671,7 +1671,7 @@ static struct inode *ll_iget_anon_dir(struct super_block *sb,
 	}
 
 	lli = ll_i2info(inode);
-	if (inode->i_state & I_NEW) {
+	if (inode_state_read(inode) & I_NEW) {
 		inode->i_mode = (inode->i_mode & ~S_IFMT) |
 				(body->mbo_mode & S_IFMT);
 		LASSERTF(S_ISDIR(inode->i_mode), "Not slave inode "DFID"\n",
@@ -3145,7 +3145,8 @@ void ll_truncate_inode_pages_final(struct inode *inode)
 
 		CWARN("%s: inode="DFID"(%p) nrpages=%lu state %#lx, lli_flags %#lx, see https://jira.whamcloud.com/browse/LU-118\n",
 		      ll_i2sbi(inode)->ll_fsname, PFID(ll_inode2fid(inode)),
-		      inode, nrpages, (unsigned long)inode->i_state,
+		      inode, nrpages,
+		      (unsigned long)inode_state_read(inode),
 		      ll_i2info(inode)->lli_flags);
 
 		rcu_read_lock();

lustre/llite/namei.c
@@ -117,7 +117,7 @@ struct inode *ll_iget(struct super_block *sb, ino_t hash,
 	if (inode == NULL)
 		RETURN(ERR_PTR(-ENOMEM));
 
-	if (inode->i_state & I_NEW) {
+	if (inode_state_read(inode) & I_NEW) {
 		rc = ll_read_inode2(inode, md);
 		if (rc == 0 && S_ISREG(inode->i_mode) &&
 		    ll_i2info(inode)->lli_clob == NULL)
@@ -141,7 +141,7 @@ struct inode *ll_iget(struct super_block *sb, ino_t hash,
 	} else if (is_bad_inode(inode)) {
 		iput(inode);
 		inode = ERR_PTR(-ESTALE);
-	} else if (!(inode->i_state & (I_FREEING | I_CLEAR))) {
+	} else if (!(inode_state_read(inode) & (I_FREEING | I_CLEAR))) {
 		rc = ll_update_inode(inode, md);
 		CDEBUG(D_VFSTRACE, "got inode: "DFID"(%p): rc = %d\n",
 		       PFID(&md->body->mbo_fid1), inode, rc);
@@ -234,6 +234,7 @@ static void ll_lock_cancel_bits(struct ldlm_lock *lock,
 {
 	struct inode *inode = ll_inode_from_resource_lock(lock);
 	struct ll_inode_info *lli;
+	bool is_new __maybe_unused;
 	int rc;
 
 	ENTRY;
@@ -246,7 +247,8 @@ static void ll_lock_cancel_bits(struct ldlm_lock *lock,
 		if (lock->l_resource->lr_lvb_inode)
 			LDLM_DEBUG(lock,
 				   "can't take inode for the lock (%sevicted)",
-				   lock->l_resource->lr_lvb_inode->i_state &
+				   inode_state_read(
+					lock->l_resource->lr_lvb_inode) &
 				   I_FREEING ? "" : "not ");
 		RETURN_EXIT;
 	}
@@ -380,7 +382,8 @@ static void ll_lock_cancel_bits(struct ldlm_lock *lock,
 			 */
 			master_inode = ilookup5_nowait(inode->i_sb, hash,
 							ll_test_inode_by_fid,
-							(void *)&lli->lli_pfid);
+							(void *)&lli->lli_pfid,
+							&is_new);
 			if (master_inode) {
 				ll_prune_negative_children(master_inode);
 				iput(master_inode);

lustre/llite/rw.c
@@ -1531,7 +1531,8 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 #endif
 	}
 
-	if (ll_i2info(inode)->lli_clob == NULL || (inode->i_state & I_FREEING))
+	if (ll_i2info(inode)->lli_clob == NULL ||
+	    (inode_state_read(inode) & I_FREEING))
 		RETURN(0);
 
 	/* for directio, it would call writepages() to evict cached pages

lustre/mdc/mdc_locks.c
@@ -106,10 +106,10 @@ int mdc_set_lock_data(struct obd_export *exp, const struct lustre_handle *lockh,
 	    lock->l_resource->lr_lvb_inode != data) {
 		struct inode *old_inode = lock->l_resource->lr_lvb_inode;
 
-		LASSERTF(old_inode->i_state & I_FREEING,
+		LASSERTF(inode_state_read(old_inode) & I_FREEING,
 			 "Found existing inode %px/%lu/%u state %lu in lock: setting data to %px/%lu/%u\n",
 			 old_inode, old_inode->i_ino, old_inode->i_generation,
-			 (unsigned long)old_inode->i_state,
+			 (unsigned long)inode_state_read(old_inode),
 			 new_inode, new_inode->i_ino, new_inode->i_generation);
 	}
 	lock->l_resource->lr_lvb_inode = new_inode;

lustre/osd-ldiskfs/osd_handler.c
@@ -3886,7 +3886,7 @@ static int __osd_create(struct osd_thread_info *info, struct osd_object *obj,
 	result = osd_create_type_f(dof->dof_type)(info, obj, attr, hint, dof,
 						  th);
 	if (likely(obj->oo_inode != NULL)) {
-		LASSERT(obj->oo_inode->i_state & I_NEW);
+		LASSERT(inode_state_read(obj->oo_inode) & I_NEW);
 
 		/*
 		 * Unlock the inode before attr initialization to avoid

lustre/osd-wbcfs/osd_object.c
@@ -579,7 +579,7 @@ static int __osd_create(const struct lu_env *env, struct osd_object *obj,
 	result = osd_create_type_f(dof->dof_type)(env, obj, attr, hint, dof,
 						  th);
 	if (likely(obj->oo_inode && result == 0)) {
-		LASSERT(obj->oo_inode->i_state & I_NEW);
+		LASSERT(inode_state_read(obj->oo_inode) & I_NEW);
 
 		/*
 		 * Unlock the inode before attr initialization to avoid

lustre/lustre-release

commit d29bc645e04a2cea1ad100d575f50b32172e31b5

LU-20422 llite: use lockless inode_state_read_once
On v6.18+, inode_state_read() asserts i_lock is held via
lockdep_assert_held(). Lustre's call sites read i_state locklessly
(e.g. the I_NEW check in ll_iget() after iget5_locked()), so the
assertion trips a WARN). Use the lockless inode_state_read_once()
variant instead, matching the original bare i_state reads.

Fixes: 20527615330e ("LU-19837 build: Compatibility updates for kernel v6.19")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Ie9a36f3e54a2c1f7fccda0a4cabcf20fa5d692da
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66823
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>

config/lustre-core.m4
@@ -3551,6 +3551,8 @@ AC_DEFUN([LC_HAVE_INODE_STATE_READ],[
 	], [
 		AC_DEFINE([inode_state_read(inode)], [((inode)->i_state)],
 			  [inode_state_read() does not exist, provide one])
+		AC_DEFINE([inode_state_read_once(inode)], [((inode)->i_state)],
+			  [inode_state_read_once() does not exist, provide one])
 	])
 ]) # LC_HAVE_INODE_STATE_READ
 

lustre/llite/lcommon_cl.c
@@ -155,7 +155,7 @@ int cl_file_inode_init(struct inode *inode, struct lustre_md *md)
 		 * unnecessary to perform lookup-alloc-lookup-insert, just
 		 * alloc and insert directly.
 		 */
-		if (!(inode_state_read(inode) & I_NEW)) {
+		if (!(inode_state_read_once(inode) & I_NEW)) {
 			result = -EIO;
 			CERROR("%s: unexpected not-NEW inode "DFID": rc = %d\n",
 			       ll_i2sbi(inode)->ll_fsname, PFID(fid), result);

lustre/llite/llite_lib.c
@@ -1655,7 +1655,7 @@ struct inode *ll_inode_from_resource_lock(struct ldlm_lock *lock)
 			}
 		} else {
 			inode = lock->l_resource->lr_lvb_inode;
-			LDLM_DEBUG_LIMIT(inode_state_read(inode) &
+			LDLM_DEBUG_LIMIT(inode_state_read_once(inode) &
 					 I_FREEING ?  D_INFO : D_WARNING, lock,
 					 "lr_lvb_inode %p is bogus: magic %08x",
 					 lock->l_resource->lr_lvb_inode,
@@ -1700,7 +1700,7 @@ static struct inode *ll_iget_anon_dir(struct super_block *sb,
 	}
 
 	lli = ll_i2info(inode);
-	if (inode_state_read(inode) & I_NEW) {
+	if (inode_state_read_once(inode) & I_NEW) {
 		inode->i_mode = (inode->i_mode & ~S_IFMT) |
 				(body->mbo_mode & S_IFMT);
 		LASSERTF(S_ISDIR(inode->i_mode), "Not slave inode "DFID"\n",
@@ -3190,7 +3190,7 @@ void ll_truncate_inode_pages_final(struct inode *inode)
 		CWARN("%s: inode="DFID"(%p) nrpages=%lu state %#lx, lli_flags %#lx, see https://jira.whamcloud.com/browse/LU-118\n",
 		      ll_i2sbi(inode)->ll_fsname, PFID(ll_inode2fid(inode)),
 		      inode, nrpages,
-		      (unsigned long)inode_state_read(inode),
+		      (unsigned long)inode_state_read_once(inode),
 		      ll_i2info(inode)->lli_flags);
 
 		rcu_read_lock();

lustre/llite/namei.c
@@ -117,7 +117,7 @@ struct inode *ll_iget(struct super_block *sb, ino_t hash,
 	if (inode == NULL)
 		RETURN(ERR_PTR(-ENOMEM));
 
-	if (inode_state_read(inode) & I_NEW) {
+	if (inode_state_read_once(inode) & I_NEW) {
 		rc = ll_read_inode2(inode, md);
 		if (rc == 0 && S_ISREG(inode->i_mode) &&
 		    ll_i2info(inode)->lli_clob == NULL)
@@ -141,7 +141,7 @@ struct inode *ll_iget(struct super_block *sb, ino_t hash,
 	} else if (is_bad_inode(inode)) {
 		iput(inode);
 		inode = ERR_PTR(-ESTALE);
-	} else if (!(inode_state_read(inode) & (I_FREEING | I_CLEAR))) {
+	} else if (!(inode_state_read_once(inode) & (I_FREEING | I_CLEAR))) {
 		rc = ll_update_inode(inode, md);
 		CDEBUG(D_VFSTRACE, "got inode: "DFID"(%p): rc = %d\n",
 		       PFID(&md->body->mbo_fid1), inode, rc);
@@ -247,7 +247,7 @@ static void ll_lock_cancel_bits(struct ldlm_lock *lock,
 		if (lock->l_resource->lr_lvb_inode)
 			LDLM_DEBUG(lock,
 				   "can't take inode for the lock (%sevicted)",
-				   inode_state_read(
+				   inode_state_read_once(
 					lock->l_resource->lr_lvb_inode) &
 				   I_FREEING ? "" : "not ");
 		RETURN_EXIT;

lustre/llite/rw.c
@@ -1532,7 +1532,7 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 	}
 
 	if (ll_i2info(inode)->lli_clob == NULL ||
-	    (inode_state_read(inode) & I_FREEING))
+	    (inode_state_read_once(inode) & I_FREEING))
 		RETURN(0);
 
 	/* for directio, it would call writepages() to evict cached pages

lustre/mdc/mdc_locks.c
@@ -106,10 +106,10 @@ int mdc_set_lock_data(struct obd_export *exp, const struct lustre_handle *lockh,
 	    lock->l_resource->lr_lvb_inode != data) {
 		struct inode *old_inode = lock->l_resource->lr_lvb_inode;
 
-		LASSERTF(inode_state_read(old_inode) & I_FREEING,
+		LASSERTF(inode_state_read_once(old_inode) & I_FREEING,
 			 "Found existing inode %px/%lu/%u state %lu in lock: setting data to %px/%lu/%u\n",
 			 old_inode, old_inode->i_ino, old_inode->i_generation,
-			 (unsigned long)inode_state_read(old_inode),
+			 (unsigned long)inode_state_read_once(old_inode),
 			 new_inode, new_inode->i_ino, new_inode->i_generation);
 	}
 	lock->l_resource->lr_lvb_inode = new_inode;

lustre/osd-ldiskfs/osd_handler.c
@@ -3886,7 +3886,7 @@ static int __osd_create(struct osd_thread_info *info, struct osd_object *obj,
 	result = osd_create_type_f(dof->dof_type)(info, obj, attr, hint, dof,
 						  th);
 	if (likely(obj->oo_inode != NULL)) {
-		LASSERT(inode_state_read(obj->oo_inode) & I_NEW);
+		LASSERT(inode_state_read_once(obj->oo_inode) & I_NEW);
 
 		/*
 		 * Unlock the inode before attr initialization to avoid

lustre/osd-wbcfs/osd_object.c
@@ -591,7 +591,7 @@ static int __osd_create(const struct lu_env *env, struct osd_object *obj,
 	result = osd_create_type_f(dof->dof_type)(env, obj, attr, hint, dof,
 						  th);
 	if (likely(obj->oo_inode && result == 0)) {
-		LASSERT(inode_state_read(obj->oo_inode) & I_NEW);
+		LASSERT(inode_state_read_once(obj->oo_inode) & I_NEW);
 
 		/*
 		 * Unlock the inode before attr initialization to avoid