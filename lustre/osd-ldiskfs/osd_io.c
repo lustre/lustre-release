lustre/lustre-release

commit 1f3e4df2fbc87b2853b29ab286178814ea225e04

LU-18562 osd: llog write batch block allocation
as we need to grab a semaphore to serialize block allocation,
it makes sense to allocate few blocks at once.

todo:
 - optimization: save last used bhs in osd object
 - concurrent append vs read test

Test-Parameters: fstype=ldiskfs testlist=sanity,sanityn,sanity-benchmark,sanity-flr,sanity-pfl
Test-Parameters: fstype=ldiskfs testlist=sanity,sanityn,sanity-benchmark,sanity-flr,sanity-pfl
Test-Parameters: fstype=ldiskfs testlist=sanity,sanityn,sanity-benchmark,sanity-flr,sanity-pfl
Test-Parameters: fstype=ldiskfs env=ONLY=60,ONLY_REPEAT=10 testlist=sanity
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: I16be9d637c99aedd288c331f2476e283aeb7cbc0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57456
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>

ldiskfs/kernel_patches/patches/linux-5.10/ext4-misc.patch
@@ -81,7 +81,7 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index ea9976b..e3630b0 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6206,3 +6206,20 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
+@@ -6206,3 +6206,21 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
  
  	return ret;
  }
@@ -93,6 +93,7 @@ index ea9976b..e3630b0 100644
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/linux-5.16/ext4-misc.patch
@@ -78,7 +78,7 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 98f0dc0e..a59c0c8a 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6313,3 +6313,20 @@ out_error:
+@@ -6313,3 +6313,21 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
@@ -90,6 +90,7 @@ index 98f0dc0e..a59c0c8a 100644
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/linux-5.8/ext4-misc.patch
@@ -90,7 +90,7 @@ diff -ur a/fs/ext4/ialloc.c b/fs/ext4/ialloc.c
 diff -ur a/fs/ext4/inode.c b/fs/ext4/inode.c
 --- a/fs/ext4/inode.c	2021-06-28 08:45:39.069955021 -0600
 +++ b/fs/ext4/inode.c	2021-06-28 08:46:06.921523449 -0600
-@@ -6088,3 +6088,19 @@
+@@ -6088,3 +6088,20 @@
  
  	return ret;
  }
@@ -102,6 +102,7 @@ diff -ur a/fs/ext4/inode.c b/fs/ext4/inode.c
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/linux-6.10/ext4-misc.patch
@@ -83,7 +83,7 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 339bdfac..cd8ab8d3 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6166,3 +6166,20 @@ out_error:
+@@ -6166,3 +6166,21 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
@@ -95,6 +95,7 @@ index 339bdfac..cd8ab8d3 100644
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/oe2203/ext4-misc.patch
@@ -73,7 +73,7 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index e868b33ed8f5..70f3bb4ef5eb 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6215,3 +6215,20 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
+@@ -6215,3 +6215,21 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
  
  	return ret;
  }
@@ -85,6 +85,7 @@ index e868b33ed8f5..70f3bb4ef5eb 100644
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/rhel8.3/ext4-misc.patch
@@ -82,7 +82,7 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 ===================================================================
 --- linux-4.18.0-240.1.1.el8.orig/fs/ext4/inode.c
 +++ linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
-@@ -6343,3 +6343,20 @@ vm_fault_t ext4_filemap_fault(struct vm_
+@@ -6343,3 +6343,21 @@ vm_fault_t ext4_filemap_fault(struct vm_
  
  	return ret;
  }
@@ -94,6 +94,7 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/rhel8.7/ext4-misc.patch
@@ -73,7 +73,7 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 ===================================================================
 --- linux-4.18.0-240.1.1.el8.orig/fs/ext4/inode.c
 +++ linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
-@@ -6343,3 +6343,20 @@ vm_fault_t ext4_filemap_fault(struct vm_
+@@ -6343,3 +6343,21 @@ vm_fault_t ext4_filemap_fault(struct vm_
 
  	return ret;
  }
@@ -85,6 +85,7 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

ldiskfs/kernel_patches/patches/sles15sp4/ext4-misc.patch
@@ -82,7 +82,7 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 06ee22e..bb109d9 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6204,3 +6204,20 @@ out_error:
+@@ -6204,3 +6204,21 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
@@ -94,6 +94,7 @@ index 06ee22e..bb109d9 100644
 +EXPORT_SYMBOL(ext4_force_commit);
 +EXPORT_SYMBOL(__ext4_mark_inode_dirty);
 +EXPORT_SYMBOL(ext4_get_group_desc);
++EXPORT_SYMBOL(__ext4_journal_get_create_access);
 +EXPORT_SYMBOL(__ext4_journal_get_write_access);
 +EXPORT_SYMBOL(__ext4_journal_start_sb);
 +EXPORT_SYMBOL(__ext4_journal_stop);

commit e0616b484c5a7c99610df5fafb601a28ac685213

LU-19862 osd: keep a_ops in sync with the inode journal mode
osd_ldiskfs_write()/osd_ldiskfs_write_fast() put llog inodes into
data=journal mode by setting LDISKFS_INODE_JOURNAL_DATA directly on
ei->i_flags.  ldiskfs picks inode->i_mapping->a_ops in ldiskfs_iget(),
via ldiskfs_set_aops(), from ldiskfs_inode_journal_mode() - i.e. before
that flag is set.  Since osd mounts with nodelalloc, the inode ends up
with ldiskfs_should_journal_data() == 1 but a_ops still == ldiskfs_aops
rather than ldiskfs_journalled_aops.

That inconsistency bites in ldiskfs_dirty_journalled_data(), which does
folio_mark_dirty() before handing the buffer to jbd2:

	if (S_ISREG(inode->i_mode))
		folio_mark_dirty(folio);
	return ldiskfs_handle_dirty_metadata(handle, NULL, bh);

With ldiskfs_journalled_aops, ->dirty_folio is filemap_dirty_folio(),
which leaves the buffers alone.  With ldiskfs_aops it is
block_dirty_folio(), which sets BH_Dirty on every buffer of the folio.
jbd2_journal_dirty_metadata() then files a plain-dirty buffer as
journalled metadata and complains:

	JBD2: Spotted dirty metadata buffer (dev = dm-0, blocknr = 57266).

A journalled metadata buffer carrying BH_Dirty later trips

	J_ASSERT_BH(bh, !buffer_dirty(bh))

in jbd2_journal_commit_transaction()'s BJ_Forget loop, panicking the MDS
on umount:

	kernel BUG at fs/jbd2/commit.c:1022!

The only caller reaching ldiskfs_dirty_journalled_data() for these inodes
is ldiskfs_block_truncate_page(), which zeroes the tail of the last
partial block.  osd_drop_preallocated_space() calls ldiskfs_truncate()
from osd_object_delete() to give back the blocks preallocated by the
batch allocator, so every eviction of a partially-filled llog object hits
it - and at umount the whole LU object cache is purged at once, which is
why the warnings arrive in a burst just before the panic.

Call ldiskfs_set_aops() when the flag is first set, so a_ops matches the
journal mode, exactly as ldiskfs does for an inode read in with the flag
already on disk.

Note the blocks freed by the truncate are not the problem: they are
forgotten correctly.  The offending block is the one the truncate keeps.

Add the jbd2 warning to check_dmesg_for_errors() so this is caught by
every suite rather than only when it escalates to the assertion.  The
message warns of on-disk corruption and should never appear.  Without the
osd fix sanity-scrub test_22 now fails in 9s ("Error in dmesg detected")
instead of relying on the much rarer panic.

Fixes: 1f3e4df2fb ("LU-18562 osd: llog write batch block allocation")

ldiskfs/kernel_patches/patches/linux-5.10/ext4-misc.patch
@@ -81,11 +81,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index ea9976b..e3630b0 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6206,3 +6206,21 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
+@@ -6206,3 +6206,22 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/linux-5.16/ext4-misc.patch
@@ -78,11 +78,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 98f0dc0e..a59c0c8a 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6313,3 +6313,21 @@ out_error:
+@@ -6313,3 +6313,22 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/linux-5.8/ext4-misc.patch
@@ -90,11 +90,12 @@ diff -ur a/fs/ext4/ialloc.c b/fs/ext4/ialloc.c
 diff -ur a/fs/ext4/inode.c b/fs/ext4/inode.c
 --- a/fs/ext4/inode.c	2021-06-28 08:45:39.069955021 -0600
 +++ b/fs/ext4/inode.c	2021-06-28 08:46:06.921523449 -0600
-@@ -6088,3 +6088,20 @@
+@@ -6088,3 +6088,21 @@
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/linux-6.10/ext4-misc.patch
@@ -83,11 +83,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 339bdfac..cd8ab8d3 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6166,3 +6166,21 @@ out_error:
+@@ -6166,3 +6166,22 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/oe2203/ext4-misc.patch
@@ -73,11 +73,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index e868b33ed8f5..70f3bb4ef5eb 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6215,3 +6215,21 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
+@@ -6215,3 +6215,22 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/rhel8.3/ext4-misc.patch
@@ -82,11 +82,12 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 ===================================================================
 --- linux-4.18.0-240.1.1.el8.orig/fs/ext4/inode.c
 +++ linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
-@@ -6343,3 +6343,21 @@ vm_fault_t ext4_filemap_fault(struct vm_
+@@ -6343,3 +6343,22 @@ vm_fault_t ext4_filemap_fault(struct vm_
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(__ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/rhel8.7/ext4-misc.patch
@@ -73,11 +73,12 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 ===================================================================
 --- linux-4.18.0-240.1.1.el8.orig/fs/ext4/inode.c
 +++ linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
-@@ -6343,3 +6343,21 @@ vm_fault_t ext4_filemap_fault(struct vm_
+@@ -6343,3 +6343,22 @@ vm_fault_t ext4_filemap_fault(struct vm_
 
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(__ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/sles15sp4/ext4-misc.patch
@@ -82,11 +82,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 06ee22e..bb109d9 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6204,3 +6204,21 @@ out_error:
+@@ -6204,3 +6204,22 @@ out_error:
  	ext4_journal_stop(handle);
  	goto out;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/ubuntu19/ext4-misc.patch
@@ -82,11 +82,12 @@ diff --git a/fs/ext4/inode.c b/fs/ext4/inode.c
 index 306d64b..890aa90 100644
 --- a/fs/ext4/inode.c
 +++ b/fs/ext4/inode.c
-@@ -6325,3 +6325,20 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
+@@ -6325,3 +6325,21 @@ vm_fault_t ext4_filemap_fault(struct vm_fault *vmf)
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/ubuntu20.04.3/ext4-misc.patch
@@ -70,11 +70,12 @@ diff -ur a/fs/ext4/ialloc.c b/fs/ext4/ialloc.c
 diff -ur a/fs/ext4/inode.c b/fs/ext4/inode.c
 --- a/fs/ext4/inode.c	2022-03-24 16:23:11.721060900 -0600
 +++ b/fs/ext4/inode.c	2022-03-24 16:24:35.412642805 -0600
-@@ -6222,3 +6222,21 @@
+@@ -6222,3 +6222,22 @@
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

ldiskfs/kernel_patches/patches/ubuntu2004/ext4-misc.patch
@@ -73,11 +73,12 @@ Index: linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
 ===================================================================
 --- linux-4.18.0-240.1.1.el8.orig/fs/ext4/inode.c
 +++ linux-4.18.0-240.1.1.el8/fs/ext4/inode.c
-@@ -6343,3 +6343,20 @@ vm_fault_t ext4_filemap_fault(struct vm_
+@@ -6343,3 +6343,21 @@ vm_fault_t ext4_filemap_fault(struct vm_
  
  	return ret;
  }
 +EXPORT_SYMBOL(ext4_map_blocks);
++EXPORT_SYMBOL(ext4_set_aops);
 +EXPORT_SYMBOL(ext4_truncate);
 +EXPORT_SYMBOL(ext4_iget);
 +EXPORT_SYMBOL(ext4_bread);

lustre/osd-ldiskfs/osd_io.c
@@ -2147,6 +2147,8 @@ static int osd_ldiskfs_write_fast(struct osd_object *o,  void *buf, int bufsize,
 	/* only the first flag-set matters */
 	dirty_inode = !test_and_set_bit(LDISKFS_INODE_JOURNAL_DATA,
 					&ei->i_flags);
+	if (dirty_inode)
+		ldiskfs_set_aops(inode);
 
 	rc = osd_attach_jinode(inode);
 	if (rc)
@@ -2242,6 +2244,8 @@ int osd_ldiskfs_write(struct osd_device *osd, struct inode *inode, void *buf,
 	/* only the first flag-set matters */
 	dirty_inode = !test_and_set_bit(LDISKFS_INODE_JOURNAL_DATA,
 				       &ei->i_flags);
+	if (dirty_inode)
+		ldiskfs_set_aops(inode);
 
 	/* sparse checking is racy, but sparse is very rare case, leave as is */
 	sparse = (new_size > 0 && (inode->i_blocks >> (inode->i_blkbits - 9)) <

lustre/tests/test-framework.sh
@@ -7824,6 +7824,7 @@ check_dmesg_for_errors() {
 	errors="VFS: Busy inodes after unmount of"
 	errors+="\|ldiskfs_check_descriptors: Checksum for group 0 failed"
 	errors+="\|group descriptors corrupted"
+	errors+="\|JBD2: Spotted dirty metadata buffer"
 	errors+="\|UBSAN\|KASAN"
 
 	res=$(do_nodes -q $(comma_list $(nodes_list)) "dmesg" |