lustre/lustre-release

commit a00b9416611f870dc694d9606d7025e6fbae0d74

LU-17814 utils: Enable pfind by default
Add error handling and enable pfind by default.

This requires switching find_skip_file from counting to
probability based, otherwise we'd need an expensive atomic
op to coordinate the exact skip count.  But the point of the
option is statistical rather than exact behavior.

Increase the number of files in the relevant test to get
the law of large numbers in our favor.  Some complex math
suggests the probability of failure is extremely small.

Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Change-Id: I3dcbe94483873bd84d16dc02a860d70d314b9964
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57306
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi_pfind.c
@@ -11,6 +11,7 @@
  */
 
 #include <pthread.h>
+#include <sys/xattr.h>
 #include "lustreapi_internal.h"
 #include "lstddef.h"
 
@@ -22,7 +23,9 @@ static void work_unit_free(struct find_work_unit *unit);
 static void *find_worker(void *arg)
 {
 	struct find_work_queue *queue = (struct find_work_queue *) arg;
-	struct find_work_unit *unit;
+	struct find_work_unit *unit = NULL;
+	char *fail_loc = getenv("LLAPI_FAIL_LOC");
+	int rc = 0;
 
 	while (!queue->fwq_shutdown) {
 		/* Get work unit from queue */
@@ -45,10 +48,21 @@ static void *find_worker(void *arg)
 			queue->fwq_tail = NULL;
 		pthread_mutex_unlock(&queue->fwq_lock);
 
-		// TODO: Error propagation, interesting...
-		llapi_semantic_traverse(unit->fwu_path, 2 * PATH_MAX, -1,
-					cb_find_init, cb_common_fini,
-					unit->fwu_param, NULL);
+		rc = llapi_semantic_traverse(unit->fwu_path, 2 * PATH_MAX, -1,
+					     cb_find_init, cb_common_fini,
+					     unit->fwu_param, NULL);
+		if (rc && queue->fwq_error == 0)
+			queue->fwq_error = rc;
+		if ((rc < 0 && rc != -EALREADY &&
+		     unit->fwu_param->fp_stop_on_error) ||
+		    (fail_loc && !strcmp(fail_loc, "LLAPI_FAIL_PFIND_SEM"))) {
+			if (fail_loc && !strcmp(fail_loc,
+						"LLAPI_FAIL_PFIND_SEM"))
+				rc = -EIO;
+			queue->fwq_shutdown = 1;
+			queue->fwq_error = rc;
+		}
+
 		work_unit_free(unit);
 		ll_atomic_fetch_sub(&queue->fwq_active_units, 1);
 	}
@@ -103,7 +117,32 @@ void free_find_param(struct find_param *fp)
 	free(fp->fp_mdt_indexes);
 	free(fp->fp_lmd);
 	free(fp->fp_lmv_md);
-	free(fp->fp_xattr_match_info);
+
+	/* Deep free xattr match info */
+	if (fp->fp_xattr_match_info) {
+		struct xattr_match_info *xmi = fp->fp_xattr_match_info;
+		int i;
+
+		free(xmi->xattr_regex_exclude);
+		free(xmi->xattr_regex_matched);
+
+		if (xmi->xattr_regex_name) {
+			for (i = 0; i < xmi->xattr_regex_count; i++)
+				free(xmi->xattr_regex_name[i]);
+			free(xmi->xattr_regex_name);
+		}
+
+		if (xmi->xattr_regex_value) {
+			for (i = 0; i < xmi->xattr_regex_count; i++)
+				free(xmi->xattr_regex_value[i]);
+			free(xmi->xattr_regex_value);
+		}
+
+		free(xmi->xattr_name_buf);
+		free(xmi->xattr_value_buf);
+		free(fp->fp_xattr_match_info);
+	}
+
 	free(fp->fp_format_printf_str);
 	free(fp);
 }
@@ -165,40 +204,143 @@ struct find_param *copy_find_param(const struct find_param *src)
 		       src->fp_num_alloc_mdts * sizeof(struct obd_uuid));
 	}
 
-	if (src->fp_mdt_indexes && src->fp_num_obds > 0) {
-		dst->fp_mdt_indexes = calloc(src->fp_num_obds,
+	if (src->fp_mdt_indexes && src->fp_num_mdts > 0) {
+		dst->fp_mdt_indexes = calloc(src->fp_num_mdts,
 					     sizeof(int));
 		if (!dst->fp_mdt_indexes)
 			goto error;
 		memcpy(dst->fp_mdt_indexes, src->fp_mdt_indexes,
-		       src->fp_num_obds * sizeof(int));
+		       src->fp_num_mdts * sizeof(int));
 	}
 
 	/* LMD and LMV data */
 	if (src->fp_lmd && src->fp_lum_size > 0) {
-		dst->fp_lmd = malloc(src->fp_lum_size);
+		size_t lmd_size = offsetof(typeof(*src->fp_lmd), lmd_lmm) +
+				  src->fp_lum_size;
+
+		dst->fp_lmd = malloc(lmd_size);
 		if (!dst->fp_lmd)
 			goto error;
-		memcpy(dst->fp_lmd, src->fp_lmd, src->fp_lum_size);
+		memcpy(dst->fp_lmd, src->fp_lmd, lmd_size);
 	}
 
 	if (src->fp_lmv_md) {
-		size_t lmv_size = sizeof(struct lmv_user_md) +
-			src->fp_lmv_stripe_count * sizeof(struct lmv_user_mds_data);
+		size_t lmv_size = lmv_user_md_size(src->fp_lmv_stripe_count,
+						   LMV_USER_MAGIC_SPECIFIC);
 		dst->fp_lmv_md = malloc(lmv_size);
 		if (!dst->fp_lmv_md)
 			goto error;
 		memcpy(dst->fp_lmv_md, src->fp_lmv_md, lmv_size);
 	}
 
-	/* xattr match info */
+	/* xattr match info - deep copy all pointer fields */
 	if (src->fp_xattr_match_info) {
+		struct xattr_match_info *src_xmi = src->fp_xattr_match_info;
+		struct xattr_match_info *dst_xmi;
+		int i;
+
 		dst->fp_xattr_match_info =
-			malloc(sizeof(struct xattr_match_info));
+			calloc(1, sizeof(struct xattr_match_info));
 		if (!dst->fp_xattr_match_info)
 			goto error;
-		memcpy(dst->fp_xattr_match_info, src->fp_xattr_match_info,
-			sizeof(struct xattr_match_info));
+
+		dst_xmi = dst->fp_xattr_match_info;
+		dst_xmi->xattr_regex_count = src_xmi->xattr_regex_count;
+
+		/* Deep copy all pointer fields */
+		if (src_xmi->xattr_regex_count > 0) {
+			/* Copy exclude array */
+			if (src_xmi->xattr_regex_exclude) {
+				dst_xmi->xattr_regex_exclude =
+					malloc(src_xmi->xattr_regex_count *
+					       sizeof(bool));
+				if (!dst_xmi->xattr_regex_exclude)
+					goto error;
+				memcpy(dst_xmi->xattr_regex_exclude,
+				       src_xmi->xattr_regex_exclude,
+				       src_xmi->xattr_regex_count *
+				       sizeof(bool));
+			}
+
+			/* Copy matched array */
+			if (src_xmi->xattr_regex_matched) {
+				dst_xmi->xattr_regex_matched =
+					malloc(src_xmi->xattr_regex_count *
+					       sizeof(bool));
+				if (!dst_xmi->xattr_regex_matched)
+					goto error;
+				memcpy(dst_xmi->xattr_regex_matched,
+				       src_xmi->xattr_regex_matched,
+				       src_xmi->xattr_regex_count *
+				       sizeof(bool));
+			}
+
+			/* Copy regex name array */
+			if (src_xmi->xattr_regex_name) {
+				dst_xmi->xattr_regex_name =
+					malloc(src_xmi->xattr_regex_count *
+					       sizeof(regex_t *));
+				if (!dst_xmi->xattr_regex_name)
+					goto error;
+				for (i = 0; i < src_xmi->xattr_regex_count;
+				     i++) {
+					if (!src_xmi->xattr_regex_name[i]) {
+						dst_xmi->xattr_regex_name[i] =
+							NULL;
+						continue;
+					}
+					dst_xmi->xattr_regex_name[i] =
+						malloc(sizeof(regex_t));
+					if (!dst_xmi->xattr_regex_name[i])
+						goto error;
+					memcpy(dst_xmi->xattr_regex_name[i],
+					       src_xmi->xattr_regex_name[i],
+					       sizeof(regex_t));
+				}
+			}
+
+			/* Copy regex value array */
+			if (src_xmi->xattr_regex_value) {
+				dst_xmi->xattr_regex_value =
+					malloc(src_xmi->xattr_regex_count *
+					       sizeof(regex_t *));
+				if (!dst_xmi->xattr_regex_value)
+					goto error;
+				for (i = 0; i < src_xmi->xattr_regex_count;
+				     i++) {
+					if (!src_xmi->xattr_regex_value[i]) {
+						dst_xmi->xattr_regex_value[i] =
+							NULL;
+						continue;
+					}
+					dst_xmi->xattr_regex_value[i] =
+						malloc(sizeof(regex_t));
+					if (!dst_xmi->xattr_regex_value[i])
+						goto error;
+					memcpy(dst_xmi->xattr_regex_value[i],
+					       src_xmi->xattr_regex_value[i],
+					       sizeof(regex_t));
+				}
+			}
+		}
+
+		/* Copy name buffer */
+		if (src_xmi->xattr_name_buf) {
+			dst_xmi->xattr_name_buf = malloc(XATTR_LIST_MAX);
+			if (!dst_xmi->xattr_name_buf)
+				goto error;
+			memcpy(dst_xmi->xattr_name_buf,
+			       src_xmi->xattr_name_buf, XATTR_LIST_MAX);
+		}
+
+		/* Copy value buffer */
+		if (src_xmi->xattr_value_buf) {
+			dst_xmi->xattr_value_buf = malloc(XATTR_SIZE_MAX);
+			if (!dst_xmi->xattr_value_buf)
+				goto error;
+			memcpy(dst_xmi->xattr_value_buf,
+			       src_xmi->xattr_value_buf, XATTR_SIZE_MAX);
+		}
 	}
 
 	/* Format string */
@@ -302,6 +444,22 @@ out:
 	return rc;
 }
 
+void cleanup_work_queue(struct find_work_queue *queue)
+{
+	struct find_work_unit *unit, *next;
+
+	pthread_mutex_lock(&queue->fwq_lock);
+	unit = queue->fwq_head;
+	while (unit) {
+		next = unit->fwu_next;
+		work_unit_free(unit);
+		ll_atomic_fetch_sub(&queue->fwq_active_units, 1);
+		unit = next;
+	}
+	queue->fwq_head = queue->fwq_tail = NULL;
+	pthread_mutex_unlock(&queue->fwq_lock);
+}
+
 static int pfind_param_callback(char *path, struct find_param *param,
 				struct find_work_queue *queue)
 {
@@ -330,8 +488,15 @@ static int pfind_param_callback(char *path, struct find_param *param,
 		goto out;
 
 	/* Wait for all work to complete */
-	while (ll_atomic_fetch_add(&queue->fwq_active_units, 0) > 0)
-		sched_yield();
+	while (ll_atomic_fetch_add(&queue->fwq_active_units, 0) > 0) {
+		/* if a worker hit an error, it forces shutdown... */
+		if (queue->fwq_shutdown)
+			cleanup_work_queue(queue);
+		else
+			sched_yield();
+	}
+	/* collect error if one occurred... */
+	ret = queue->fwq_error;
 
 out:
 	find_param_fini(param);
@@ -341,7 +506,7 @@ out:
 
 int parallel_find(char *path, struct find_param *param)
 {
-	struct find_work_queue queue;
+	struct find_work_queue queue = { 0 };
 	pthread_t *threads = NULL;
 	int numthreads = param->fp_thread_count;
 	int rc;
@@ -371,8 +536,10 @@ int parallel_find(char *path, struct find_param *param)
 
 
 	/* Signal shutdown and wait for threads before cleanup */
+	pthread_mutex_lock(&queue.fwq_lock);
 	queue.fwq_shutdown = true;
 	pthread_cond_broadcast(&queue.fwq_sleep_cond);
+	pthread_mutex_unlock(&queue.fwq_lock);
 	for (i = 0; i < numthreads; i++)
 		pthread_join(threads[i], NULL);
 

Documentation/man1/lfs-find.1
@@ -157,6 +157,8 @@ lfs-find \- Lustre client utility to list files with specific attributes
 .RB [[ ! ]
 .BR --type | -t
 .RB { bcdflps }]
+.RB [ --threads
+.IR N ]
 .RB [[ ! ]
 .BR --uid | -u | --user | -U
 .IR UNAME | UID ]
@@ -746,6 +748,16 @@ File has type:
 .RB sym l ink,
 or
 .BR s ocket.
+.TP
+.BI --threads " N"
+Use
+.I N
+threads for parallel directory traversal. If not specified, the default
+thread count is calculated as min(MDT_COUNT * 4, CPU_COUNT / 2),
+where CPU_COUNT is the total number of CPUs configured for Lustre.
+This allows faster scanning of large directory trees by processing
+multiple directories concurrently while respecting the available
+CPU resources.
 .TP
 .BR -u ", " --uid
 File has specified numeric user ID

lustre/doc/lctl-set_param.8
@@ -9,7 +9,7 @@ lctl-set_param \- Lustre filesystem set parameter utility
 .RB [ --file | -F ]
 .RB [ --no-name | -n ]
 .RB [ --permanent | -P ]
-.RB [ --thread | -t\c
+.RB [ --threads | -t\c
 .RI [ THREAD_COUNT ]]
 .IR PARAMETER \= VALUE " ..."
 .SY "lctl set_param -F"
@@ -92,7 +92,7 @@ such as the filesystem and/or target name. This option is only available
 in Lustre 2.5.0 and later clients, older clients cannot set persistent
 parameters, nor will they see them.
 .TP
-.BR -t ", " --thread " ["\fITHREAD_COUNT\fR]
+.BR -t ", " --threads " ["\fITHREAD_COUNT\fR]
 Spawn threads to set multiple parameters in parallel, optionally specifying
 the maximum number of threads to run (with no space between
 .B -t

lustre/include/lustre/lustreapi.h
@@ -502,7 +502,7 @@ struct find_work_queue {
 	int fwq_active_units;
 #endif
 	bool fwq_shutdown;		/* Flag to signal shutdown */
-	int fwq_error;
+	int fwq_error;			/* error code if failed */
 };
 
 int llapi_ostlist(char *path, struct find_param *param);

lustre/tests/sanity.sh
@@ -10178,7 +10178,9 @@ run_test 56eg "lfs find -xattr"
 test_56eh() {
 	local dir=$DIR/d$(basetest $testnum)g.$TESTSUITE
 
-	setup_56_special $dir $NUMFILES $NUMDIRS
+	# Enough files to give us a statistically reliable sampling
+	# setup_special is complicated, but this works out to about 2100 files
+	setup_56_special $dir 400 $NUMDIRS
 
 	local cmd="$LFS find $dir"
 	local total=$($cmd | wc -l)
@@ -10190,9 +10192,11 @@ test_56eh() {
 
 	while (( $n < 100 )); do
 		nums=$($LFS find --skip=$((100 - $n)) $dir | wc -l)
-		(( $nums <= $(($total * ($n + 5) / 100)) &&
-		   $nums >= $(($total * ($n - 5) / 100)) )) ||
-			error "--skip=$((100 - $n)): expected $(($total * $n / 100)) results with 5% error margin, got $nums"
+		(( $nums <= $(($total * ($n + 10) / 100)) &&
+		   $nums >= $(($total * ($n - 10) / 100)) )) ||
+			error "--skip=$((100 - $n)): expected" \
+			      "$(($total * $n / 100)) results with 10% error" \
+			      "margin, got $nums"
 		(( n++ ))
 	done
 }
@@ -10246,6 +10250,42 @@ test_56ej() {
 		error "migrate remote dir error $DIR/$tdir.src $f_mgrt"
 }
 run_test 56ej "lfs migration --non-block copy"
+
+test_56ek() {
+	local dir=$DIR/$tdir
+	local numfiles=10
+	local numdirs=5
+
+	# Setup test directory structure
+	echo "Creating test directory structure..."
+	mkdir -p $dir || error "Failed to create $dir"
+
+	# Create test directories and files
+	for ((i = 0; i < numdirs; i++)); do
+		mkdir -p $dir/subdir$i || error "Failed to create subdir$i"
+		createmany -o $dir/subdir$i/file $numfiles ||
+			error "Failed to create files in subdir$i"
+	done
+
+	# Normal find should work
+	echo "Verifying normal find works..."
+	local count=$($LFS find $dir | wc -l)
+	# dirs + files + root
+	local expected=$(($numdirs + ($numfiles * $numdirs) + 1))
+	[ $count -eq $expected ] ||
+		error "Wrong file count: found $count, expected $expected"
+
+	# Set failure injection
+	echo "Testing error handling..."
+	LLAPI_FAIL_LOC="LLAPI_FAIL_PFIND_SEM" \
+		$LFS find $dir 2>&1 | grep -q "Input/output error" ||
+		error "Expected I/O error not found with LLAPI_FAIL_LOC set"
+
+	# Cleanup
+	rm -rf $dir
+	return 0
+}
+run_test 56ek "Test lfs find error handling with LLAPI_FAIL_LOC"
 
 test_57a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"

lustre/utils/lfs.c
@@ -49,6 +49,7 @@
 #include <zlib.h>
 
 #include <libcfs/util/ioctl.h>
+#include <libcfs/util/param.h>
 #include <libcfs/util/parser.h>
 #include <libcfs/util/string.h>
 #include <linux/lnet/nidstr.h>
@@ -486,7 +487,8 @@ command_t cmdlist[] = {
 	 "     [[!] --projid <projid>] [[!] --size|-s [+-]N[bkMGTPE]]\n"
 	 "     [--skip|-k PERCENT] [[!] --stripe-count|-c [+-]<stripes>]\n"
 	 "     [[!] --stripe-index|-i <index,...>]\n"
-	 "     [[!] --stripe-size|-S [+-]N[kMGT]] [[!] --type|-t <filetype>]\n"
+	 "     [[!] --stripe-size|-S [+-]N[kMGT]] [--threads N]\n"
+	 "     [[!] --type|-t <filetype>]\n"
 	 "     [[!] --uid|-u|--user|-U <uid>|<uname>]\n"
 	 "\t !: used before an option indicates 'NOT' requested attribute\n"
 	 "\t -: used before a value indicates less than requested value\n"
@@ -5776,6 +5778,113 @@ static int str2mode_t(const char *input, mode_t *outmode)
 	return ret;
 }
 
+/*
+ * Get the number of CPUs configured for Lustre CPTs
+ * by reading cpu_partition_table parameter.
+ */
+static int get_lustre_cpu_count(void)
+{
+	glob_t paths;
+	FILE *fp;
+	char line[256];
+	int cpu_count = 0;
+	int rc;
+
+	/* Try to get cpu_partition_table parameter */
+	rc = cfs_get_param_paths(&paths, "cpu_partition_table");
+	if (rc != 0)
+		return 0;
+
+	if (paths.gl_pathc == 0) {
+		cfs_free_param_data(&paths);
+		return 0;
+	}
+
+	fp = fopen(paths.gl_pathv[0], "r");
+	if (fp == NULL) {
+		cfs_free_param_data(&paths);
+		return 0;
+	}
+
+	/* Count CPUs in cpu_partition_table
+	 * Format: "cpu_partition_table=0   : 0 1 2 3..."
+	 *         "1   : 16 17 18..."
+	 * Parse each line and count the CPU numbers listed
+	 */
+	while (fgets(line, sizeof(line), fp) != NULL) {
+		char *ptr = line;
+		char *colon_ptr;
+
+		/* Skip whitespace */
+		while (*ptr == ' ' || *ptr == '\t')
+			ptr++;
+
+		/* Look for lines with CPU partition info */
+		if (strncmp(ptr, "cpu_partition_table=", 20) == 0 ||
+		    isdigit(*ptr)) {
+			/* Find the colon separator */
+			colon_ptr = strchr(ptr, ':');
+			if (colon_ptr) {
+				colon_ptr++;
+				while ((colon_ptr = strchr(colon_ptr,
+							   ' ')) != NULL) {
+					cpu_count++;
+					colon_ptr++;
+				}
+			}
+		}
+	}
+
+	fclose(fp);
+	cfs_free_param_data(&paths);
+
+	return cpu_count;
+}
+
+/*
+ * Calculate default thread count for lfs find based on MDT count
+ * and CPU count. Uses the formula:
+ * threads = min(MDT_COUNT * 4, CPU_COUNT / 2)
+ * Falls back to 4 if unable to determine parameters.
+ */
+static int calculate_default_thread_count(const char *path)
+{
+	char mntdir[PATH_MAX] = "";
+	char fsname[PATH_MAX] = "";
+	int default_threads = 4;
+	int mdt_count = 0;
+	int cpu_count = 0;
+	int rc;
+
+	/* Get mount point and filesystem name */
+	rc = llapi_search_mounts(path, 0, mntdir, fsname);
+	if (rc < 0) {
+		/* Fallback to conservative default */
+		return default_threads;
+	}
+
+	/* Get MDT count */
+	rc = llapi_get_obd_count(mntdir, &mdt_count, 1);
+	if (rc < 0 || mdt_count <= 0)
+		mdt_count = 1; /* assume single MDT */
+
+	/* Get CPU count */
+	cpu_count = get_lustre_cpu_count();
+	if (cpu_count <= 0)
+		cpu_count = 1; /* fallback */
+
+	/* Calculate: min(MDT_COUNT * 4, CPU_COUNT / 2) */
+	default_threads = mdt_count * 4;
+	if (default_threads > cpu_count / 2)
+		default_threads = cpu_count / 2;
+
+	/* Ensure minimum of 4 */
+	if (default_threads < 4)
+		default_threads = 4;
+
+	return default_threads;
+}
+
 static int lfs_find(int argc, char **argv)
 {
 	int c, rc;
@@ -5906,7 +6015,7 @@ static int lfs_find(int argc, char **argv)
 			.name = "pool",		.has_arg = required_argument },
 	{ .val = '0',	.name = "print0",	.has_arg = no_argument },
 	{ .val = LFS_THREAD_OPT,
-		.name = "thread",		.has_arg = required_argument },
+			.name = "threads",	.has_arg = required_argument },
 	{ .val = 'P',	.name = "print",	.has_arg = no_argument },
 	{ .val = LFS_PRINTF_OPT,
 			.name = "printf",       .has_arg = required_argument },
@@ -6597,6 +6706,13 @@ static int lfs_find(int argc, char **argv)
 			break;
 		case LFS_THREAD_OPT:
 			param.fp_thread_count = strtol(optarg, &endptr, 0);
+			if (*endptr != '\0' || param.fp_thread_count < 1) {
+				fprintf(stderr,
+					"error: bad thread count '%s'\n",
+					optarg);
+				ret = -1;
+				goto err;
+			}
 			break;
 		case 'P': /* we always print, this option is a no-op */
 			break;
@@ -6783,6 +6899,12 @@ static int lfs_find(int argc, char **argv)
 			argv[0], param.fp_min_depth, param.fp_max_depth);
 		ret = CMD_HELP;
 		goto err;
+	}
+
+	/* Set default thread count if not specified */
+	if (param.fp_thread_count == 0) {
+		param.fp_thread_count =
+			calculate_default_thread_count(argv[pathstart]);
 	}
 
 	do {

lustre/utils/liblustreapi.c
@@ -2049,7 +2049,11 @@ int llapi_semantic_traverse(char *path, int size, int parent,
 	}
 
 	while ((dent = readdir64(dir)) != NULL) {
+		struct find_work_queue *queue = param->fp_queue;
 		int rc = 0;
+
+		if (param->fp_thread_count && queue->fwq_shutdown)
+			break;
 
 		if (!strcmp(dent->d_name, ".") || !strcmp(dent->d_name, ".."))
 			continue;

lustre/utils/lustre_param.c
@@ -1331,7 +1331,7 @@ static int setparam_cmdline(int argc, char **argv, struct param_opts *popt)
 	{ .val = 'n',	.name = "noname",	.has_arg = no_argument},
 	{ .val = 'P',	.name = "perm",		.has_arg = no_argument},
 	{ .val = 'P',	.name = "permanent",	.has_arg = no_argument},
-	{ .val = 't',	.name = "thread",	.has_arg = optional_argument},
+	{ .val = 't',	.name = "threads",	.has_arg = optional_argument},
 	{ .name = NULL },
 	};
 

commit ff045df9c5d3d5436c5c34beecbb45580d27c01e

LU-20040 llapi: use getpwuid_r/getgrgid_r in find -printf %u/%g
The printf_format_directive() %u and %g handlers cache the resolved
username/group per worker thread (LU-19557 made the cache __thread), but
still resolve names with getpwuid()/getgrgid(). Those are non-reentrant:
each returns a pointer into a shared static buffer that a concurrent
parallel-find (pfind) worker's lookup can overwrite between the lookup
and the strncpy into the thread-local cache. A file owned by e.g.
'sanityusr' then caches the name of whatever uid another thread resolved
concurrently (often 'root'), producing intermittent sanity test_56Eaa
failures: "expected 'sanityusr' but got 'root'".

This is the residual of LU-19557, which fixed the cache race (static ->
__thread) and localtime -> localtime_r for the same reason, but left the
getpwuid()/getgrgid() calls non-reentrant. Use the reentrant
getpwuid_r()/getgrgid_r() with caller-provided buffers.

This is a test of automated regression finding and fixing process.

Test-Parameters: testlist=sanity env=ONLY=56eaa,ONLY_REPEAT=100
Fixes: a00b941661 ("LU-17814 utils: Enable pfind by default")
Fixes: 08a3e19a04 ("LU-15837 utils: added lfs find --printf functions")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-4.8 llm_code_and_review_tools
Change-Id: If951fd6fff290e27ded94eca58b1a3b5a4af47d1
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67312
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/utils/liblustreapi_pfind.c
@@ -1955,9 +1955,12 @@ static int printf_format_directive(char *seq, char *buffer, size_t size,
 		static __thread gid_t save_gid = -1;
 
 		if (save_gid != param->fp_lmd->lmd_stx.stx_gid) {
-			struct group *gr;
+			struct group grp;
+			struct group *gr = NULL;
+			char gbuf[4096];
 
-			gr = getgrgid(param->fp_lmd->lmd_stx.stx_gid);
+			getgrgid_r(param->fp_lmd->lmd_stx.stx_gid, &grp,
+				   gbuf, sizeof(gbuf), &gr);
 			if (gr) {
 				save_gid = param->fp_lmd->lmd_stx.stx_gid;
 				strncpy(save_gr_name, gr->gr_name,
@@ -2010,9 +2013,12 @@ static int printf_format_directive(char *seq, char *buffer, size_t size,
 		static __thread uid_t save_uid = -1;
 
 		if (save_uid != param->fp_lmd->lmd_stx.stx_uid) {
-			struct passwd *pw;
+			struct passwd pwd;
+			struct passwd *pw = NULL;
+			char pbuf[4096];
 
-			pw = getpwuid(param->fp_lmd->lmd_stx.stx_uid);
+			getpwuid_r(param->fp_lmd->lmd_stx.stx_uid, &pwd,
+				   pbuf, sizeof(pbuf), &pw);
 			if (pw) {
 				save_uid = param->fp_lmd->lmd_stx.stx_uid;
 				strncpy(save_username, pw->pw_name,