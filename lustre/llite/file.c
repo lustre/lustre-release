lustre/lustre-release

commit e10bf68d7c3eb71a7e84986089c020aa4c9f98f7

LU-14361 statahead: regularized fname statahead pattern
Some applications do stat() calls under a directory within which
all children files have regularized file name:
- mdtest benchmark tool: mdtest.$rank.$i
- ML/AI with ingested data that have typically a format rule of
  the filename in the directory.

The most common format for regularized file name is that the
suffix part of the file name is number-indexing.
However, in the current statahead mechanism, the statahead is
populated by the order of the hash of the file name via readdir()
calls, not a kind of sorting order.

In this patch, we improve the statahead to prefetch attributes for
the files with regularized indexing file name via asynchronous
batching RPC.

This patch adds the support to do statahead for these kinds of
applications, which can be optimized, but without opendir()/
close() to start/stop statahead thread explicitly.

Instead, the statahead thread will stop and quit when found
that there was no acitivy for more than a certain time period
(i.e. 30 seconds).

Test-Parameters: mdtcount=4 mdscount=2 testlist=sanity env=ONLY=27p,ONLY_REPEAT=5
Test-Parameters: mdtcount=4 mdscount=2 testlist=sanity env=ONLY=27p,ONLY_REPEAT=5
Test-Parameters: mdtcount=4 mdscount=2 testlist=sanity env=ONLY=123f,ONLY_REPEAT=10
Test-Parameters: mdtcount=4 mdscount=2 testlist=sanity env=ONLY=123f,ONLY_REPEAT=10
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: Ide11ec5a651ae74884ddbe1cecede4f5c961e38d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/41308
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -5509,6 +5509,7 @@ int ll_getattr_dentry(struct dentry *de, struct kstat *stat, u32 request_mask,
 	      request_mask & STATX_MTIME))
 		need_glimpse = false;
 
+	ll_statahead_enter(dir, de);
 	if (dentry_may_statahead(dir, de))
 		ll_start_statahead(dir, de, need_glimpse &&
 				   !(flags & AT_STATX_DONT_SYNC));

lustre/llite/llite_internal.h
@@ -119,9 +119,28 @@ struct ll_trunc_sem {
 };
 
 enum ll_sa_pattern {
-	LSA_PATTERN_NONE	= 0x0000,
-	LSA_PATTERN_LIST	= 0x0001,
-	LSA_PATTERN_FNAME	= 0X0002,
+	LSA_PATTERN_NONE		= 0x0000,
+	/* once detected, and found no statahead pattern matched */
+	LSA_PATTERN_INVALID		= 0x0001,
+	/* do the directory listing. i.e ls $dir */
+	LSA_PATTERN_LIST		= 0x0002,
+	/* regularized file name scanning, i.e. mdtest.$i */
+	LSA_PATTERN_FNAME		= 0x0004,
+	/* statahead advise via statahead hint from users */
+	LSA_PATTERN_ADVISE		= 0x0008,
+	/* not first dirent, or is "." for listing */
+	LSA_PATTERN_LS_NOT_FIRST_DE	= 0x0100,
+	/* the file names of stat() calls has regularized predictable format */
+	LSA_PATTERN_FN_PREDICT		= 0x1000,
+	/* fname statahead workload similar to mdtest shared dir stat() */
+	LSA_PATTERN_FN_SHARED		= 0x2000,
+	/* fname statahead workload similar to mdtest unique dir stat() */
+	LSA_PATTERN_FN_UNIQUE		= 0x4000,
+	/* fname statahead workload with stride regularized naming format */
+	LSA_PATTERN_FN_STRIDE		= 0x8000,
+	LSA_PATTERN_MASK		= (LSA_PATTERN_LIST |
+					   LSA_PATTERN_FNAME |
+					   LSA_PATTERN_ADVISE),
 	LSA_PATTERN_MAX,
 };
 
@@ -205,6 +224,18 @@ struct ll_inode_info {
 			unsigned int			lli_sa_generation;
 			/* access pattern for statahead */
 			enum ll_sa_pattern		lli_sa_pattern;
+			/*
+			 * suffix index number of the latest stat dentry. It
+			 * is used for the detection of the file name statahead
+			 * pattern.
+			 */
+			__u32				lli_sa_fname_index;
+			/*
+			 * indicate the count that the suffix index number
+			 * matched continuously. This field is using for the
+			 * detection of the file name statahead pattern.
+			 */
+			unsigned int			lli_sa_match_count;
 			/* rw lock protects lli_lsm_md */
 			struct rw_semaphore		lli_lsm_sem;
 			/* directory stripe information */
@@ -872,6 +903,7 @@ struct ll_sb_info {
 	unsigned int		  ll_sa_batch_max;/* max SUB request count in
 						   * a batch PTLRPC request */
 	unsigned int		  ll_sa_max;     /* max statahead RPCs */
+	unsigned int		  ll_sa_min;	 /* min statahead req count */
 	atomic_t		  ll_sa_total;   /* statahead thread started
 						  * count */
 	atomic_t		  ll_sa_wrong;   /* statahead thread stopped for
@@ -881,6 +913,16 @@ struct ll_sb_info {
 	atomic_t		  ll_agl_total;  /* AGL thread started count */
 	atomic_t		  ll_sa_hit_total;  /* total hit count */
 	atomic_t		  ll_sa_miss_total; /* total miss count */
+	/* statahead thread count started for directory traversing pattern. */
+	atomic_t		  ll_sa_list_total;
+	/* statahead thread count started for regularized file name pattern. */
+	atomic_t		  ll_sa_fname_total;
+	/*
+	 * stop the statahead thread if it is not doing a stat() in such time
+	 * period as it probably does not care too much about performance or
+	 * the user is no longer using this directory.
+	 */
+	unsigned long		  ll_sa_timeout;
 
 	dev_t			  ll_sdev_orig; /* save s_dev before assign for
 						 * clustred nfs */
@@ -1653,9 +1695,12 @@ void ll_ra_stats_inc(struct inode *inode, enum ra_stat which);
 
 /* statahead.c */
 
-#define LL_SA_RPC_MIN           8
-#define LL_SA_RPC_DEF           128
-#define LL_SA_RPC_MAX           2048
+#define LL_SA_REQ_MIN           2
+#define LL_SA_REQ_MIN_DEF	8
+#define LL_SA_REQ_MAX		2048
+#define LL_SA_REQ_MAX_DEF	128
+
+#define LL_SA_TIMEOUT_DEF	30
 
 /* XXX: If want to support more concurrent statahead instances,
  *	please consider to decentralize the RPC lists attached
@@ -1670,6 +1715,9 @@ void ll_ra_stats_inc(struct inode *inode, enum ra_stat which);
 #define LL_SA_CACHE_BIT         6
 #define LL_SA_CACHE_SIZE        (1 << LL_SA_CACHE_BIT)
 #define LL_SA_CACHE_MASK        (LL_SA_CACHE_SIZE - 1)
+
+#define LSA_FN_PREDICT_HIT	2
+#define LSA_FN_MATCH_HIT	4
 
 /* statahead controller, per process struct, for dir only */
 struct ll_statahead_info {
@@ -1709,8 +1757,23 @@ struct ll_statahead_info {
 	__u32			sai_max_batch_count;
 	__u64			sai_index_end;
 
-	__u64			sai_fstart;
-	__u64			sai_fend;
+	union {
+		/* for ADVISE statahead pattern */
+		struct {
+			__u64	sai_fstart;
+			__u64	sai_fend;
+		};
+
+		/* for FNAME statahead pattern */
+		struct {
+			__u64	sai_fname_index;
+			/*
+			 * The length of file name statahead pattern where the
+			 * front part is padding with 0.
+			 */
+			__u8	sai_fname_zeroed_len;
+		};
+	};
 	char			sai_fname[NAME_MAX];
 };
 
@@ -1728,6 +1791,7 @@ int ll_revalidate_statahead(struct inode *dir, struct dentry **dentry,
 int ll_start_statahead(struct inode *dir, struct dentry *dentry, bool agl);
 void ll_authorize_statahead(struct inode *dir, void *key);
 void ll_deauthorize_statahead(struct inode *dir, void *key);
+void ll_statahead_enter(struct inode *dir, struct dentry *dentry);
 
 /* glimpse.c */
 blkcnt_t dirty_cnt(struct inode *inode);
@@ -1804,7 +1868,10 @@ dentry_may_statahead(struct inode *dir, struct dentry *dentry)
 	    ldd->lld_sa_generation == lli->lli_sa_generation)
 		return false;
 
-	if (lli->lli_sa_pattern == LSA_PATTERN_FNAME)
+	if (lli->lli_sa_pattern & LSA_PATTERN_ADVISE)
+		return true;
+
+	if (lli->lli_sa_pattern & (LSA_PATTERN_FNAME | LSA_PATTERN_FN_PREDICT))
 		return true;
 
 	/* not the same process, don't statahead */

lustre/llite/llite_lib.c
@@ -183,13 +183,17 @@ static struct ll_sb_info *ll_init_sbi(struct lustre_sb_info *lsi)
 	/* metadata statahead is enabled by default */
 	sbi->ll_sa_running_max = LL_SA_RUNNING_DEF;
 	sbi->ll_sa_batch_max = LL_SA_BATCH_DEF;
-	sbi->ll_sa_max = LL_SA_RPC_DEF;
+	sbi->ll_sa_max = LL_SA_REQ_MAX_DEF;
+	sbi->ll_sa_min = LL_SA_REQ_MIN_DEF;
+	sbi->ll_sa_timeout = LL_SA_TIMEOUT_DEF;
 	atomic_set(&sbi->ll_sa_total, 0);
 	atomic_set(&sbi->ll_sa_wrong, 0);
 	atomic_set(&sbi->ll_sa_running, 0);
 	atomic_set(&sbi->ll_agl_total, 0);
 	atomic_set(&sbi->ll_sa_hit_total, 0);
 	atomic_set(&sbi->ll_sa_miss_total, 0);
+	atomic_set(&sbi->ll_sa_list_total, 0);
+	atomic_set(&sbi->ll_sa_fname_total, 0);
 	set_bit(LL_SBI_AGL_ENABLED, sbi->ll_flags);
 	set_bit(LL_SBI_FAST_READ, sbi->ll_flags);
 	set_bit(LL_SBI_TINY_WRITE, sbi->ll_flags);

lustre/llite/lproc_llite.c
@@ -864,16 +864,82 @@ static ssize_t statahead_max_store(struct kobject *kobj,
 	if (rc)
 		return rc;
 
-	if (val > LL_SA_RPC_MAX) {
+	if (val > LL_SA_REQ_MAX) {
 		CWARN("%s: statahead_max value %lu limited to maximum %d\n",
-		      sbi->ll_fsname, val, LL_SA_RPC_MAX);
-		val = LL_SA_RPC_MAX;
+		      sbi->ll_fsname, val, LL_SA_REQ_MAX);
+		val = LL_SA_REQ_MAX;
 	}
 
 	sbi->ll_sa_max = val;
 	return count;
 }
 LUSTRE_RW_ATTR(statahead_max);
+
+static ssize_t statahead_min_show(struct kobject *kobj,
+				  struct attribute *attr,
+				  char *buf)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_sa_min);
+}
+
+static ssize_t statahead_min_store(struct kobject *kobj,
+				   struct attribute *attr,
+				   const char *buffer,
+				   size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	unsigned long val;
+	int rc;
+
+	rc = kstrtoul(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val < LL_SA_REQ_MIN)
+		CERROR("%s: bad statahead_min %lu < min %u\n",
+		       sbi->ll_fsname, val, LL_SA_REQ_MIN);
+	else if (val > sbi->ll_sa_max)
+		CERROR("%s: bad statahead_min %lu > max %u\n",
+		       sbi->ll_fsname, val, sbi->ll_sa_max);
+	else
+		sbi->ll_sa_min = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(statahead_min);
+
+static ssize_t statahead_timeout_show(struct kobject *kobj,
+				      struct attribute *attr,
+				      char *buf)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+
+	return scnprintf(buf, PAGE_SIZE, "%lu\n", sbi->ll_sa_timeout);
+}
+
+static ssize_t statahead_timeout_store(struct kobject *kobj,
+				       struct attribute *attr,
+				       const char *buffer,
+				       size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	unsigned long val;
+	int rc;
+
+	rc = kstrtoul(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	sbi->ll_sa_timeout = val;
+	return count;
+}
+LUSTRE_RW_ATTR(statahead_timeout);
 
 static ssize_t statahead_agl_show(struct kobject *kobj,
 				  struct attribute *attr,
@@ -917,11 +983,15 @@ static int ll_statahead_stats_seq_show(struct seq_file *m, void *v)
 	seq_printf(m, "statahead total: %u\n"
 		      "statahead wrong: %u\n"
 		      "agl total: %u\n"
+		      "list_total: %u\n"
+		      "fname_total: %u\n"
 		      "hit_total: %u\n"
 		      "miss_total: %u\n",
 		   atomic_read(&sbi->ll_sa_total),
 		   atomic_read(&sbi->ll_sa_wrong),
 		   atomic_read(&sbi->ll_agl_total),
+		   atomic_read(&sbi->ll_sa_list_total),
+		   atomic_read(&sbi->ll_sa_fname_total),
 		   atomic_read(&sbi->ll_sa_hit_total),
 		   atomic_read(&sbi->ll_sa_miss_total));
 	return 0;
@@ -938,6 +1008,8 @@ static ssize_t ll_statahead_stats_seq_write(struct file *file,
 	atomic_set(&sbi->ll_sa_total, 0);
 	atomic_set(&sbi->ll_sa_wrong, 0);
 	atomic_set(&sbi->ll_agl_total, 0);
+	atomic_set(&sbi->ll_sa_list_total, 0);
+	atomic_set(&sbi->ll_sa_fname_total, 0);
 	atomic_set(&sbi->ll_sa_hit_total, 0);
 	atomic_set(&sbi->ll_sa_miss_total, 0);
 
@@ -1950,6 +2022,8 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_statahead_running_max.attr,
 	&lustre_attr_statahead_batch_max.attr,
 	&lustre_attr_statahead_max.attr,
+	&lustre_attr_statahead_min.attr,
+	&lustre_attr_statahead_timeout.attr,
 	&lustre_attr_statahead_agl.attr,
 	&lustre_attr_lazystatfs.attr,
 	&lustre_attr_statfs_max_age.attr,

commit daa67440ed1853bf7a596d9b1946d0fa9643dae6

LU-20494 llite: don't run statahead on a non-directory parent
ll_getattr_dentry() derives the "parent" directory of the dentry
being stat'd with dget_parent() and feeds it to the statahead
machinery.  NFS re-export (knfsd) hands Lustre a disconnected,
self-parented dentry obtained from d_obtain_alias(): its d_parent
points at itself, so dget_parent() returns the file's own dentry and
@dir ends up being the regular file itself, not a directory.

ll_statahead_enter() and sa_pattern_fname_detect() then update the
directory-only members of struct ll_inode_info (lli_sa_match_count,
lli_sa_fname_index, ...).  These share a union with the regular-file
members, and lli_sa_match_count overlaps the upper half of
lli_size_mutex.wait_list.next.  Writing it zeroes the top 32 bits of
that list pointer.  The damage is silent until a later glimpse takes
lli_size_mutex on the slow (contended) path: __mutex_add_waiter()
walks the corrupted wait_list and hits

  kernel BUG at lib/list_debug.c:26!  (list_add corruption)
  __mutex_add_waiter -> __mutex_lock -> ll_merge_attr ->
  cl_glimpse_lock -> ll_getattr_dentry -> nfsd

This reproduces under parallel-scale-nfsv3 (connectathon, iorssf),
where many nfsd threads glimpse re-exported files concurrently.

Only run statahead when the parent really is a directory.  For a
normally-connected dentry the parent is always a directory, so this
is a no-op except for the disconnected re-export case.

Fixes: e10bf68d7c ("LU-14361 statahead: regularized fname statahead pattern")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Test-Parameters: trivial testlist=parallel-scale-nfsv3
Assisted-by: ClaudeCode:Opus-4.8 llm_code_and_review_tools
Change-Id: I3a479e6d864227c342b394ae300bf43c200da04d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67372
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/file.c
@@ -6289,10 +6289,19 @@ int ll_getattr_dentry(struct dentry *de, struct kstat *stat, u32 request_mask,
 
 	parent = dget_parent(de);
 	dir = d_inode(parent);
-	ll_statahead_enter(dir, de);
-	if (dentry_may_statahead(dir, de))
-		ll_start_statahead(dir, de, need_glimpse &&
-				   !(flags & AT_STATX_DONT_SYNC));
+	/*
+	 * NFS re-export hands us a disconnected (self-parented) dentry, so
+	 * dget_parent() returns the file itself and @dir is not a directory.
+	 * Statahead updates directory-only members of struct ll_inode_info
+	 * which share a union with the regular-file members (e.g.
+	 * lli_size_mutex), so it must not run unless @dir really is a directory.
+	 */
+	if (S_ISDIR(dir->i_mode)) {
+		ll_statahead_enter(dir, de);
+		if (dentry_may_statahead(dir, de))
+			ll_start_statahead(dir, de, need_glimpse &&
+					   !(flags & AT_STATX_DONT_SYNC));
+	}
 	dput(parent);
 
 	if (flags & AT_STATX_DONT_SYNC)