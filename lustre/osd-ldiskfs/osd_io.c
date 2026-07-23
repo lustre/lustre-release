lustre/lustre-release

commit cb037f305c64cd5121fa308afc1e6b7d3df3f61a

LU-14160 fallocate: Add punch mode to fallocate
This patch adds fallocate(2) punch operation
(FALLOCATE_FL_PUNCH_HOLE) mode support for ldiskfs backend
OSD and for OSC/OST

Test cases sanity/150{f,g} are added for verification.
FSX test was modified:
 - add 'punch' operation to an output
 - fix 'No space' problem when fallocate length become negative
 - fix wrong bytes number in output

Test-Parameters: testlist=sanity ostsizegb=12 env=ONLY="150f 150g"
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I0c180d413efdf995823e25d5c340013bec0c8611
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/40877
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_io.c
@@ -2170,10 +2170,10 @@ static int osd_declare_fallocate(const struct lu_env *env,
 	ENTRY;
 
 	/*
-	 * Only mode == 0 (which is standard prealloc) is supported now.
+	 * mode == 0 (which is standard prealloc) and PUNCH is supported
 	 * Rest of mode options is not supported yet.
 	 */
-	if (mode & ~FALLOC_FL_KEEP_SIZE)
+	if (mode & ~(FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE))
 		RETURN(-EOPNOTSUPP);
 
 	/* disable fallocate completely */
@@ -2182,6 +2182,16 @@ static int osd_declare_fallocate(const struct lu_env *env,
 
 	LASSERT(th);
 	LASSERT(inode);
+
+	if (mode & FALLOC_FL_PUNCH_HOLE) {
+		rc = osd_declare_inode_qid(env, i_uid_read(inode),
+					   i_gid_read(inode),
+					   i_projid_read(inode), 0, oh,
+					   osd_dt_obj(dt), NULL, OSD_QID_BLK);
+		if (rc == 0)
+			rc = osd_trunc_lock(osd_dt_obj(dt), oh, false);
+		RETURN(rc);
+	}
 
 	/* quota space for metadata blocks
 	 * approximate metadata estimate should be good enough.
@@ -2203,8 +2213,10 @@ static int osd_declare_fallocate(const struct lu_env *env,
 	RETURN(rc);
 }
 
-static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
-			 __u64 start, __u64 end, int mode, struct thandle *th)
+static int osd_fallocate_preallocate(const struct lu_env *env,
+				     struct dt_object *dt,
+				     __u64 start, __u64 end, int mode,
+				     struct thandle *th)
 {
 	struct osd_thandle *oh = container_of(th, struct osd_thandle, ot_super);
 	handle_t *handle = ldiskfs_journal_current_handle();
@@ -2326,6 +2338,61 @@ out:
 
 	inode_unlock(inode);
 
+	RETURN(rc);
+}
+
+static int osd_fallocate_punch(const struct lu_env *env, struct dt_object *dt,
+			       __u64 start, __u64 end, int mode,
+			       struct thandle *th)
+{
+	struct osd_object *obj = osd_dt_obj(dt);
+	struct inode *inode = obj->oo_inode;
+	struct osd_access_lock *al;
+	struct osd_thandle *oh;
+	int rc = 0, found = 0;
+
+	ENTRY;
+
+	LASSERT(dt_object_exists(dt));
+	LASSERT(osd_invariant(obj));
+	LASSERT(inode != NULL);
+
+	dquot_initialize(inode);
+
+	LASSERT(th);
+	oh = container_of(th, struct osd_thandle, ot_super);
+	LASSERT(oh->ot_handle->h_transaction != NULL);
+
+	list_for_each_entry(al, &oh->ot_trunc_locks, tl_list) {
+		if (obj != al->tl_obj)
+			continue;
+		LASSERT(al->tl_shared == 0);
+		found = 1;
+		/* do actual punch in osd_trans_stop() */
+		al->tl_start = start;
+		al->tl_end = end;
+		al->tl_mode = mode;
+		al->tl_punch = true;
+		break;
+	}
+
+	RETURN(rc);
+}
+
+static int osd_fallocate(const struct lu_env *env, struct dt_object *dt,
+			 __u64 start, __u64 end, int mode, struct thandle *th)
+{
+	int rc;
+
+	ENTRY;
+
+	if (mode & FALLOC_FL_PUNCH_HOLE) {
+		/* punch */
+		rc = osd_fallocate_punch(env, dt, start, end, mode, th);
+	} else {
+		/* standard preallocate */
+		rc = osd_fallocate_preallocate(env, dt, start, end, mode, th);
+	}
 	RETURN(rc);
 }
 
@@ -2651,6 +2718,27 @@ void osd_trunc_unlock_all(const struct lu_env *env, struct list_head *list)
 	}
 }
 
+/*
+ * For a partial-page truncate, flush the page to disk immediately to
+ * avoid data corruption during direct disk write.  b=17397
+ */
+static void osd_partial_page_flush(struct osd_device *d, struct inode *inode,
+				   loff_t offset)
+{
+	if (!(offset & ~PAGE_MASK))
+		return;
+
+	if (osd_use_page_cache(d)) {
+		filemap_fdatawrite_range(inode->i_mapping, offset, offset + 1);
+	} else {
+		/* Notice we use "wait" version to ensure I/O is complete */
+		filemap_write_and_wait_range(inode->i_mapping, offset,
+					     offset + 1);
+		invalidate_mapping_pages(inode->i_mapping, offset >> PAGE_SHIFT,
+					 offset >> PAGE_SHIFT);
+	}
+}
+
 void osd_execute_truncate(struct osd_object *obj)
 {
 	struct osd_device *d = osd_obj2dev(obj);
@@ -2686,24 +2774,22 @@ void osd_execute_truncate(struct osd_object *obj)
 		spin_unlock(&inode->i_lock);
 		osd_dirty_inode(inode, I_DIRTY_DATASYNC);
 	}
-
-	/*
-	 * For a partial-page truncate, flush the page to disk immediately to
-	 * avoid data corruption during direct disk write.  b=17397
-	 */
-	if ((size & ~PAGE_MASK) == 0)
-		return;
-	if (osd_use_page_cache(d)) {
-		filemap_fdatawrite_range(inode->i_mapping, size, size + 1);
-	} else {
-		/* Notice we use "wait" version to ensure I/O is complete */
-		filemap_write_and_wait_range(inode->i_mapping, size, size + 1);
-		invalidate_mapping_pages(inode->i_mapping, size >> PAGE_SHIFT,
-					 size >> PAGE_SHIFT);
-	}
+	osd_partial_page_flush(d, inode, size);
 }
 
-void osd_process_truncates(struct list_head *list)
+void osd_execute_punch(const struct lu_env *env, struct osd_object *obj,
+		       loff_t start, loff_t end, int mode)
+{
+	struct osd_device *d = osd_obj2dev(obj);
+	struct inode *inode = obj->oo_inode;
+	struct file *file = osd_quasi_file(env, inode);
+
+	file->f_op->fallocate(file, mode, start, end - start);
+	osd_partial_page_flush(d, inode, start);
+	osd_partial_page_flush(d, inode, end - 1);
+}
+
+void osd_process_truncates(const struct lu_env *env, struct list_head *list)
 {
 	struct osd_access_lock *al;
 
@@ -2712,8 +2798,10 @@ void osd_process_truncates(struct list_head *list)
 	list_for_each_entry(al, list, tl_list) {
 		if (al->tl_shared)
 			continue;
-		if (!al->tl_truncate)
-			continue;
-		osd_execute_truncate(al->tl_obj);
+		if (al->tl_truncate)
+			osd_execute_truncate(al->tl_obj);
+		else if (al->tl_punch)
+			osd_execute_punch(env, al->tl_obj, al->tl_start,
+					  al->tl_end, al->tl_mode);
 	}
 }

lustre/include/cl_object.h
@@ -1869,7 +1869,6 @@ struct cl_io {
 			/* The following are used for fallocate(2) */
 			int			 sa_falloc_mode;
 			loff_t			 sa_falloc_offset;
-			loff_t			 sa_falloc_len;
 			loff_t			 sa_falloc_end;
 		} ci_setattr;
 		struct cl_data_version_io {

lustre/llite/file.c
@@ -5121,7 +5121,7 @@ int cl_falloc(struct inode *inode, int mode, loff_t offset, loff_t len)
 	struct lu_env *env;
 	struct cl_io *io;
 	__u16 refcheck;
-	int rc; loff_t sa_falloc_end;
+	int rc;
 	loff_t size = i_size_read(inode);
 
 	ENTRY;
@@ -5136,34 +5136,32 @@ int cl_falloc(struct inode *inode, int mode, loff_t offset, loff_t len)
 	io->u.ci_setattr.sa_parent_fid = lu_object_fid(&io->ci_obj->co_lu);
 	io->u.ci_setattr.sa_falloc_mode = mode;
 	io->u.ci_setattr.sa_falloc_offset = offset;
-	io->u.ci_setattr.sa_falloc_len = len;
-	io->u.ci_setattr.sa_falloc_end = io->u.ci_setattr.sa_falloc_offset +
-		io->u.ci_setattr.sa_falloc_len;
+	io->u.ci_setattr.sa_falloc_end = offset + len;
 	io->u.ci_setattr.sa_subtype = CL_SETATTR_FALLOCATE;
-	sa_falloc_end = io->u.ci_setattr.sa_falloc_end;
-	if (sa_falloc_end > size) {
+	if (io->u.ci_setattr.sa_falloc_end > size) {
+		loff_t newsize = io->u.ci_setattr.sa_falloc_end;
+
 		/* Check new size against VFS/VM file size limit and rlimit */
-		rc = inode_newsize_ok(inode, sa_falloc_end);
+		rc = inode_newsize_ok(inode, newsize);
 		if (rc)
 			goto out;
-		if (sa_falloc_end > ll_file_maxbytes(inode)) {
+		if (newsize > ll_file_maxbytes(inode)) {
 			CDEBUG(D_INODE, "file size too large %llu > %llu\n",
-			       (unsigned long long)(sa_falloc_end),
+			       (unsigned long long)newsize,
 			       ll_file_maxbytes(inode));
 			rc = -EFBIG;
 			goto out;
 		}
 	}
 
-again:
-	if (cl_io_init(env, io, CIT_SETATTR, io->ci_obj) == 0)
-		rc = cl_io_loop(env, io);
-	else
-		rc = io->ci_result;
-
-	cl_io_fini(env, io);
-	if (unlikely(io->ci_need_restart))
-		goto again;
+	do {
+		rc = cl_io_init(env, io, CIT_SETATTR, io->ci_obj);
+		if (!rc)
+			rc = cl_io_loop(env, io);
+		else
+			rc = io->ci_result;
+		cl_io_fini(env, io);
+	} while (unlikely(io->ci_need_restart));
 
 out:
 	cl_env_put(env, &refcheck);
@@ -5175,6 +5173,8 @@ long ll_fallocate(struct file *filp, int mode, loff_t offset, loff_t len)
 	struct inode *inode = filp->f_path.dentry->d_inode;
 	int rc;
 
+	if (offset < 0 || len <= 0)
+		RETURN(-EINVAL);
 	/*
 	 * Encrypted inodes can't handle collapse range or zero range or insert
 	 * range since we would need to re-encrypt blocks with a different IV or
@@ -5187,10 +5187,10 @@ long ll_fallocate(struct file *filp, int mode, loff_t offset, loff_t len)
 		RETURN(-EOPNOTSUPP);
 
 	/*
-	 * Only mode == 0 (which is standard prealloc) is supported now.
-	 * Punch is not supported yet.
+	 * mode == 0 (which is standard prealloc) and PUNCH is supported
+	 * Rest of mode options are not supported yet.
 	 */
-	if (mode & ~FALLOC_FL_KEEP_SIZE)
+	if (mode & ~(FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE))
 		RETURN(-EOPNOTSUPP);
 
 	ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_FALLOCATE, 1);

lustre/mdc/mdc_dev.c
@@ -1067,6 +1067,9 @@ static int mdc_io_setattr_start(const struct lu_env *env,
 			return rc;
 	}
 
+	if (cl_io_is_fallocate(io))
+		return -EOPNOTSUPP;
+
 	if (oio->oi_lockless == 0) {
 		cl_object_attr_lock(obj);
 		rc = cl_object_attr_get(env, obj, attr);

lustre/ofd/ofd_dev.c
@@ -1969,15 +1969,29 @@ static int ofd_fallocate_hdl(struct tgt_session_info *tsi)
 	 * fallocate start and end are passed in o_size, o_blocks
 	 * on the wire.
 	 */
+	if ((oa->o_valid & (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS)) !=
+	    (OBD_MD_FLSIZE | OBD_MD_FLBLOCKS))
+		RETURN(err_serious(-EPROTO));
+
 	start = oa->o_size;
 	end = oa->o_blocks;
 	mode = oa->o_falloc_mode;
 	/*
-	 * Only mode == 0 (which is standard prealloc) is supported now.
-	 * Punch is not supported yet.
+	 * mode == 0 (which is standard prealloc) and PUNCH is supported
+	 * Rest of mode options are not supported yet.
 	 */
-	if (mode & ~FALLOC_FL_KEEP_SIZE)
+	if (mode & ~(FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE))
 		RETURN(-EOPNOTSUPP);
+
+	/* PUNCH_HOLE mode should always be accompanied with KEEP_SIZE flag
+	 * Check that and add the missing flag for such invalid call with
+	 * warning.
+	 */
+	if (mode & FALLOC_FL_PUNCH_HOLE && !(mode & FALLOC_FL_KEEP_SIZE)) {
+		CWARN("%s: PUNCH mode misses KEEP_SIZE flag, setting it\n",
+		      tsi->tsi_tgt->lut_obd->obd_name);
+		mode |= FALLOC_FL_KEEP_SIZE;
+	}
 
 	repbody->oa.o_oi = oa->o_oi;
 	repbody->oa.o_valid = OBD_MD_FLID;

lustre/ofd/ofd_objects.c
@@ -806,6 +806,10 @@ int ofd_object_fallocate(const struct lu_env *env, struct ofd_object *fo,
 	if (!ofd_object_exists(fo))
 		GOTO(unlock, rc = -ENOENT);
 
+	if (la->la_valid & (LA_ATIME | LA_MTIME | LA_CTIME))
+		tgt_fmd_update(info->fti_exp, &fo->ofo_header.loh_fid,
+			       info->fti_xid);
+
 	rc = dt_falloc(env, dob, start, end, mode, th);
 	if (rc)
 		GOTO(unlock, rc);

lustre/osc/osc_io.c
@@ -535,6 +535,30 @@ static void osc_trunc_check(const struct lu_env *env, struct cl_io *io,
 				trunc_check_cb, (void *)&size);
 }
 
+/**
+ * Flush affected pages prior punch.
+ * We shouldn't discard them locally first because that could be data loss
+ * if server doesn't support fallocate punch, we also need these data to be
+ * flushed first to prevent re-ordering with the punch
+ */
+static int osc_punch_start(const struct lu_env *env, struct cl_io *io,
+			   struct cl_object *obj)
+{
+	struct osc_object *osc = cl2osc(obj);
+	pgoff_t pg_start = cl_index(obj, io->u.ci_setattr.sa_falloc_offset);
+	pgoff_t pg_end = cl_index(obj, io->u.ci_setattr.sa_falloc_end - 1);
+	int rc;
+
+	ENTRY;
+	rc = osc_cache_writeback_range(env, osc, pg_start, pg_end, 1, 0);
+	if (rc < 0)
+		RETURN(rc);
+
+	osc_page_gang_lookup(env, io, osc, pg_start, pg_end, osc_discard_cb,
+			     osc);
+	RETURN(0);
+}
+
 static int osc_io_setattr_start(const struct lu_env *env,
                                 const struct cl_io_slice *slice)
 {
@@ -549,19 +573,17 @@ static int osc_io_setattr_start(const struct lu_env *env,
 	enum op_xvalid ia_xvalid = io->u.ci_setattr.sa_xvalid;
 	int result = 0;
 	__u64 size = io->u.ci_setattr.sa_attr.lvb_size;
-	__u64 end = OBD_OBJECT_EOF;
-	bool io_is_falloc = false;
+	bool io_is_falloc = cl_io_is_fallocate(io);
 
 	ENTRY;
 	/* truncate cache dirty pages first */
-	if (cl_io_is_trunc(io)) {
+	if (cl_io_is_trunc(io))
 		result = osc_cache_truncate_start(env, cl2osc(obj), size,
 						  &oio->oi_trunc);
-	} else if (cl_io_is_fallocate(io)) {
-		io_is_falloc = true;
-		size = io->u.ci_setattr.sa_falloc_offset;
-		end = io->u.ci_setattr.sa_falloc_end;
-	}
+	/* flush local pages prior punching them on server */
+	if (io_is_falloc &&
+	    io->u.ci_setattr.sa_falloc_mode & FALLOC_FL_PUNCH_HOLE)
+		result = osc_punch_start(env, io, obj);
 
 	if (result == 0 && oio->oi_lockless == 0) {
 		cl_object_attr_lock(obj);
@@ -571,14 +593,8 @@ static int osc_io_setattr_start(const struct lu_env *env,
 			unsigned int cl_valid = 0;
 
 			if (ia_avalid & ATTR_SIZE) {
-				if (io_is_falloc) {
-					attr->cat_size =
-					      io->u.ci_setattr.sa_attr.lvb_size;
-					attr->cat_kms = attr->cat_size;
-				} else {
-					attr->cat_size = size;
-					attr->cat_kms = size;
-				}
+				attr->cat_size = size;
+				attr->cat_kms = size;
 				cl_valid = (CAT_SIZE | CAT_KMS);
 			}
 			if (ia_avalid & ATTR_MTIME_SET) {
@@ -618,17 +634,8 @@ static int osc_io_setattr_start(const struct lu_env *env,
 			oa->o_valid |= OBD_MD_FLMTIME;
 			oa->o_mtime = attr->cat_mtime;
 		}
-		if (ia_avalid & ATTR_SIZE) {
-			if (io_is_falloc) {
-				oa->o_size = size;
-				oa->o_blocks = end;
-				oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
-			} else {
-				oa->o_size = size;
-				oa->o_blocks = OBD_OBJECT_EOF;
-				oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
-			}
 
+		if (ia_avalid & ATTR_SIZE || io_is_falloc) {
 			if (oio->oi_lockless) {
 				oa->o_flags = OBD_FL_SRVLOCK;
 				oa->o_valid |= OBD_MD_FLFLAGS;
@@ -653,10 +660,16 @@ static int osc_io_setattr_start(const struct lu_env *env,
 		if (io_is_falloc) {
 			int falloc_mode = io->u.ci_setattr.sa_falloc_mode;
 
+			oa->o_size = io->u.ci_setattr.sa_falloc_offset;
+			oa->o_blocks = io->u.ci_setattr.sa_falloc_end;
+			oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
 			result = osc_fallocate_base(osc_export(cl2osc(obj)),
 						    oa, osc_async_upcall,
 						    cbargs, falloc_mode);
 		} else if (ia_avalid & ATTR_SIZE) {
+			oa->o_size = size;
+			oa->o_blocks = OBD_OBJECT_EOF;
+			oa->o_valid |= OBD_MD_FLSIZE | OBD_MD_FLBLOCKS;
 			result = osc_punch_send(osc_export(cl2osc(obj)),
 						oa, osc_async_upcall, cbargs);
 		} else {
@@ -690,12 +703,11 @@ void osc_io_setattr_end(const struct lu_env *env,
 	if (result == 0) {
 		if (oio->oi_lockless) {
 			/* lockless truncate */
-			struct osc_device *osd = lu2osc_dev(obj->co_lu.lo_dev);
+			struct osc_device *osc = lu2osc_dev(obj->co_lu.lo_dev);
 
-			LASSERT(cl_io_is_trunc(io));
 			LASSERT(cl_io_is_trunc(io) || cl_io_is_fallocate(io));
 			/* XXX: Need a lock. */
-			osd->od_stats.os_lockless_truncates++;
+			osc->od_stats.os_lockless_truncates++;
 		}
 	}
 

lustre/osc/osc_request.c
@@ -449,14 +449,7 @@ int osc_fallocate_base(struct obd_export *exp, struct obdo *oa,
 	int rc;
 	ENTRY;
 
-	/*
-	 * Only mode == 0 (which is standard prealloc) is supported now.
-	 * Punch is not supported yet.
-	 */
-	if (mode & ~FALLOC_FL_KEEP_SIZE)
-		RETURN(-EOPNOTSUPP);
 	oa->o_falloc_mode = mode;
-
 	req = ptlrpc_request_alloc(class_exp2cliimp(exp),
 				   &RQF_OST_FALLOCATE);
 	if (req == NULL)

commit f2ffe89c0d38cfab952c7d9d0a25e13336c7ed8f

LU-19955 osd: flush the old EOF page after a growing fallocate
fsx based tests intermittently LBUG the OSS:

  LustreError: (osd_io.c:1525:osd_write_commit())
          ASSERTION( !PageDirty(lnb[i].lnb_page) ) failed

osd_write_commit() requires that no page it is handed is dirty.  That
holds only if nothing leaves a dirty page behind in the OST page
cache, because osd_get_page() reuses an existing page cache page for
an IO that bypasses the cache, rather than allocating a private one,
to avoid data aliasing.

fsx issues fallocate(FALLOC_FL_ZERO_RANGE) without FALLOC_FL_KEEP_SIZE,
so a zero range that reaches past EOF grows the file.  osd_fallocate()
defers such a request to osd_execute_fallocate(), which calls into
ldiskfs and then flushes the range it asked for:

	rc = file->f_op->fallocate(file, mode, start, end - start);
	if (rc == 0)
		osd_partial_page_flush_punch(d, inode, start, end - 1);

ldiskfs_zero_range() allocates the blocks via
ldiskfs_alloc_file_blocks(), which, as it extends i_size, does:

	if (epos > old_size) {
		pagecache_isize_extended(inode, old_size, epos);
		ldiskfs_zero_partial_blocks(handle, inode,
					    old_size, epos - old_size);
	}

That zeroes - and with mark_buffer_dirty() dirties - the partial block
at the *old* EOF.  The old EOF is unrelated to [start, end), so the
range flush above never covers it and the page stays dirty in the page
cache.  A write that lands on it before the next journal commit writes
it back picks it up out of the page cache and hits the assert.

Instrumenting osd_execute_fallocate() on a growing zero range shows the
page left behind, and shows it gone once the old EOF is flushed too:

  before: start=65536 end=69632 old_size=49999 new_size=69632 rc=0
          dirty_tag=1 nrpages=1
  after:  start=65536 end=69632 old_size=49999 new_size=69632 rc=0
          dirty_tag=0 nrpages=0

A crash dump agrees: the asserting page is a page cache page (mapping
set, private_2 clear) at the offset the write covers, PG_dirty and
PG_private set but PG_uptodate clear, while its sole buffer_head is
Uptodate|Dirty|Mapped - the state ldiskfs_block_zero_page_range()
leaves, since it reads the block in and dirties the buffer but never
marks the folio uptodate.  It was the only dirty page in the mapping,
no punch was in flight, and neither the fallocate nor writeback had
reported an error.

Flush the partial page at the old EOF when the fallocate grew the file.
osd_partial_page_flush() already skips the case where the old EOF was
block aligned and ldiskfs had nothing to zero.

The other edge ldiskfs zeroes, at epos - 1, is inside [start, end) and
is already covered.

No separate test is required as fsx in sanityn hits this pretty
reliably and a synthetic test seems complicated due to a relative
small window.

Fixes: cb037f305c ("LU-14160 fallocate: Add punch mode to fallocate")

lustre/osd-ldiskfs/osd_io.c
@@ -3133,17 +3133,27 @@ static int osd_execute_fallocate(const struct lu_env *env,
 	struct osd_device *d = osd_obj2dev(obj);
 	struct inode *inode = obj->oo_inode;
 	struct file *file;
+	loff_t old_size;
 	int rc;
 
 	file = osd_get_filp_for_inode(osd_oti_get(env), inode);
 	if (IS_ERR(file))
 		return PTR_ERR(file);
 
+	old_size = i_size_read(inode);
 	file->f_mode |= FMODE_64BITHASH;
 	rc = file->f_op->fallocate(file, mode, start, end - start);
 	compat_security_file_free(file);
-	if (rc == 0)
+	if (rc == 0) {
 		osd_partial_page_flush_punch(d, inode, start, end - 1);
+		/*
+		 * When the fallocate grows the file, ldiskfs also zeroes the
+		 * partial block at the old EOF, which lies outside the
+		 * [start, end) range flushed above.
+		 */
+		if (i_size_read(inode) > old_size)
+			osd_partial_page_flush(d, inode, old_size);
+	}
 	return rc;
 }
 