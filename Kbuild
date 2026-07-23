lustre/lustre-release

commit 4ff30bece12c937f8237189a2d722523aed9359e

LU-18687 build: refactor the top-level Makefile
Lustre uses automake far more than is strictly needed.
Limit the top-level autoMakefile to only creating dist
tarballs and handling install/deinstall. The remaining
of make targets are transfered to a series of plain
Makefiles. This completes our goal of building the
Lustre kernel modules without using automake.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Icf32edb539443cd4d629d84f697e2a14767c316b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64327
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

Kbuild
@@ -3,28 +3,22 @@
 #
 # This file is part of Lustre, http://www.lustre.org/
 #
-# Makefile.in
-#
-# Template to generate top-level Makefile
-#
 
 this-makefile := $(lastword $(MAKEFILE_LIST))
 lsrctree := $(realpath $(dir $(this-makefile)))
 
-ifneq (@LDISKFS_SUBDIR@,)
-obj-m += @LDISKFS_SUBDIR@/
+ifdef CONFIG_LUSTRE_FS_LDISKFS
+obj-m += ldiskfs/
 endif
 obj-m += libcfs/ lnet/
 obj-m += lustre/
 
-subdir-ccflags-y := -include $(lsrctree)/undef.h
-subdir-ccflags-y += -include $(lsrctree)/config.h
-subdir-ccflags-y += -I$(lsrctree)/lnet/include
-subdir-ccflags-y += -I$(lsrctree)/lustre/include
-subdir-ccflags-y += -I$(lsrctree)/include/uapi
-subdir-ccflags-y += -I$(lsrctree)/include
+NOSTDINC_FLAGS += -I$(lsrctree)/lnet/include
+NOSTDINC_FLAGS += -I$(lsrctree)/lustre/include
+NOSTDINC_FLAGS += -I$(lsrctree)/include/uapi
+NOSTDINC_FLAGS += -I$(lsrctree)/include
+
+subdir-ccflags-y := -include $(lsrctree)/config.h
 subdir-ccflags-y += $(call cc-option, -Wno-format-truncation)
 subdir-ccflags-y += $(call cc-option, -Wno-stringop-truncation)
 subdir-ccflags-y += $(call cc-option, -Wno-stringop-overflow)
-
-@INCLUDE_RULES@

config/Makefile.exports.in
@@ -1,0 +1,109 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+# config/Makefile.exports.in
+#
+# Template for config/Makefile.exports, generated by configure.
+# Provides configured values as Make variables for inclusion in
+# the top-level Makefile.
+#
+
+@MODULES_TRUE@LUSTRE_MODULES_MT = modules
+@BUILD_LUSTREIOKIT_TRUE@LUSTRE_IOKIT_MT = lustre-iokit
+@LDISKFS_ENABLED_TRUE@LUSTRE_LDISKFS_MT = ldiskfs
+@BUILT_IN_KO2IBLND_TRUE@LUSTRE_BUILT_IN_KO2IBLND_NT = lnet/klnds/in-kernel-o2iblnd
+@EXTERNAL_KO2IBLND_TRUE@LUSTRE_EXTERNAL_KO2IBLND_NT = lnet/klnds/o2iblnd
+
+CC = @CC@
+LINUX_OBJ = @LINUX_OBJ@
+LINUXRELEASE = @LINUXRELEASE@
+systemdsystemunitdir = @systemdsystemunitdir@
+
+PACKAGE = @PACKAGE@
+PACKAGE_TARNAME = @PACKAGE_TARNAME@
+VERSION = @VERSION@
+RPMBUILD = @RPMBUILD@
+RPMBUILD_BINARY_ARGS = @RPMBUILD_BINARY_ARGS@
+CONFIG_CACHE_FILE = @CONFIG_CACHE_FILE@
+
+ENABLE_SERVER = @ENABLE_SERVER@
+ENABLE_LDISKFS = @ENABLE_LDISKFS@
+ENABLE_ZFS = @ENABLE_ZFS@
+ENABLE_MULTIPLE_LNDS = @ENABLE_MULTIPLE_LNDS@
+ENABLE_GNI = @ENABLE_GNI@
+KFILND = @KFILND@
+DPKG_EXT_O2IB = @DPKG_EXT_O2IB@
+EXT_O2IBPATH = @EXT_O2IBPATH@
+DPKG_INT_O2IB = @DPKG_INT_O2IB@
+EFALND = @EFALND@
+ENABLE_GSS = @ENABLE_GSS@
+ENABLE_CRYPTO = @ENABLE_CRYPTO@
+ENABLE_PINGER = @ENABLE_PINGER@
+ENABLE_CHECKSUM = @ENABLE_CHECKSUM@
+ENABLE_FLOCK = @ENABLE_FLOCK@
+ENABLE_LRU_RESIZE = @ENABLE_LRU_RESIZE@
+ENABLE_MINDF = @ENABLE_MINDF@
+ENABLE_FAIL_ALLOC = @ENABLE_FAIL_ALLOC@
+ENABLE_INVARIANTS = @ENABLE_INVARIANTS@
+ENABLE_LU_REF = @ENABLE_LU_REF@
+ENABLE_PGSTAT_TRACK = @ENABLE_PGSTAT_TRACK@
+ENABLE_LIBCFS_CDEBUG = @ENABLE_LIBCFS_CDEBUG@
+ENABLE_LIBCFS_TRACE = @ENABLE_LIBCFS_TRACE@
+ENABLE_LIBCFS_ASSERT = @ENABLE_LIBCFS_ASSERT@
+ENABLE_PANIC_DUMPLOG = @ENABLE_PANIC_DUMPLOG@
+ENABLE_READLINE = @ENABLE_READLINE@
+ENABLE_LIBPTHREAD = @ENABLE_LIBPTHREAD@
+ENABLE_BACKOFF = @ENABLE_BACKOFF@
+ENABLE_EFENCE = @ENABLE_EFENCE@
+ENABLE_COVERAGE = @ENABLE_COVERAGE@
+ENABLEO2IB = @ENABLEO2IB@
+
+@MPITESTS_TRUE@ENABLE_MPITESTS = yes
+@MPITESTS_FALSE@ENABLE_MPITESTS = no
+@TESTS_TRUE@ENABLE_TESTS = yes
+@TESTS_FALSE@ENABLE_TESTS = no
+@UTILS_TRUE@ENABLE_UTILS = yes
+@UTILS_FALSE@ENABLE_UTILS = no
+@BUILD_LUSTREIOKIT_TRUE@ENABLE_IOKIT = yes
+@BUILD_LUSTREIOKIT_FALSE@ENABLE_IOKIT = no
+
+LUSTRE_CC = $(CC)
+LUSTRE_LINUX_OBJ = $(LINUX_OBJ)
+LUSTRE_INT_O2IB_SYMBOLS = @INT_O2IB_SYMBOLS@
+LUSTRE_EXT_O2IB_SYMBOLS = @EXT_O2IB_SYMBOLS@
+LUSTRE_EXTRA_SYMBOLS = @EXTRA_SYMBOLS@
+
+# Build configuration exports
+@SERVER_TRUE@export CONFIG_LUSTRE_FS_SERVER := m
+@CLIENT_TRUE@export CONFIG_LUSTRE_FS := m
+@TESTS_TRUE@export CONFIG_LUSTRE_FS_TESTS := m
+@LDISKFS_ENABLED_TRUE@export CONFIG_LUSTRE_FS_LDISKFS := m
+@ZFS_ENABLED_TRUE@export CONFIG_LUSTRE_FS_ZFS := m
+@GSS_TRUE@export CONFIG_LUSTRE_FS_GSS := m
+@GSS_SSK_TRUE@export CONFIG_LUSTRE_FS_GSS_SSK := m
+@GSS_KEYRING_TRUE@export CONFIG_LUSTRE_FS_GSS_KEYRING := m
+@SPL_ENABLED_TRUE@export CONFIG_LUSTRE_FS_SPL := m
+@BUILD_GNILND_TRUE@export CONFIG_LNET_GNILND := m
+@BUILT_IN_KO2IBLND_TRUE@export CONFIG_LNET_O2IBLND := m
+@BUILD_KFILND_TRUE@export CONFIG_LNET_KFILND := m
+@BUILD_EFALND_TRUE@export CONFIG_LNET_EFALND := m
+
+export CONFIG_GCOV_PROFILE_LUSTRE := @ENABLE_COVERAGE@
+export CONFIG_GCOV_PROFILE_LNET := @ENABLE_COVERAGE@
+export LUSTRE := @LUSTRE@
+export LINUX := @LINUX@
+export ABS_TOP_BUILDDIR := @abs_top_builddir@
+export GDS_PATH := @GDS_PATH@
+export CUDA_PATH := @CUDA_PATH@
+export EFA_INCLUDE_PATH := @EFA_INCLUDE_PATH@
+export KFICPPFLAGS := @KFICPPFLAGS@
+export GNICPPFLAGS := @GNICPPFLAGS@
+export EXTRA_OFED_CONFIG := @EXTRA_OFED_CONFIG@
+export EXTRA_OFED_INCLUDE := @EXTRA_OFED_INCLUDE@
+export EXT_O2IB_VER := @EXT_O2IB_VER@
+export ZFS := @ZFS@
+export ZFS_OBJ := @ZFS_OBJ@
+export SPL := @SPL@
+export SPL_OBJ := @SPL_OBJ@

config/Kbuild
@@ -1,52 +1,0 @@
-# SPDX-License-Identifier: GPL-2.0
-
-#
-# This file is part of Lustre, http://www.lustre.org/
-#
-# There are four ways this Makefile can be called:
-#
-# 1.  As a subdirectory from the toplevel, for automake
-# 2.  A wrapper around the kernel's makefile when building modules, to
-#     possibly override the .config file
-# 3.  At configure time, as the toplevel module dir for building
-#     kernel tests
-#
-
-ifeq ($(PATCHLEVEL),)
-
-ifeq ($(LUSTRE_LINUX_CONFIG),)
-
-# case #1
-include autoMakefile
-
-else
-
-# case #2
-# Note that this comes from make -C $LINUX -f $LUSTRE/build/Makefile
-# so "include Makefile" below includes $LINUX/Makefile, not this file
-include $(LUSTRE_LINUX_CONFIG)
-include Makefile
-ifeq ($(DEQUOTE_CC_VERSION_TEXT),yes)
-CONFIG_CC_VERSION_TEXT=$(shell echo ${CONFIG_CC_VERSION_TEXT:1:-1})
-endif
-
-endif # LUSTRE_LINUX_CONFIG
-
-else # PATCHLEVEL
-ifeq ($(DEQUOTE_CC_VERSION_TEXT),yes)
-CONFIG_CC_VERSION_TEXT=$(shell echo ${CONFIG_CC_VERSION_TEXT:1:-1})
-endif
-
-# case #3
-ifneq ($(LUSTRE_KERNEL_TEST),)
-# extra-y works prior to 5.10
-extra-y = $(LUSTRE_KERNEL_TEST)
-# always-y works since 5.6
-always-y = $(LUSTRE_KERNEL_TEST)
-# always works for SUSE SP3 LTSS kernel
-always = $(LUSTRE_KERNEL_TEST)
-endif
-
-obj-m := conftest.o
-
-endif # PATCHLEVEL

.gitignore
@@ -56,6 +56,7 @@ debs
 depcomp
 install-sh
 Makefile
+!/Makefile
 missing
 modules.order
 TAGS
@@ -77,7 +78,6 @@ TAGS
 /config.cache
 /config.h
 /config.h.in
-/undef.h
 /INSTALL
 /libtool
 /lustre.spec

Makefile
@@ -1,0 +1,161 @@
+# SPDX-License-Identifier: GPL-2.0
+
+#
+# This file is part of Lustre, http://www.lustre.org/
+#
+
+.DEFAULT_GOAL := __default
+
+#
+# The top-level autoMakefile is only used for 'make install' and 'make dist'.
+# For the remaining make targets (often run interactively), we want to avoid
+# warnings about redefining make targets. Hence, we conditionally include
+# the autoMakefile.
+#
+
+ALWAYS_TARGETS := help checkpatch utils modules \
+	TAGS tags etags ctags cscope mkid \
+	checkstack checkstack-update checkstack-clean
+
+
+ifeq ($(MAKECMDGOALS),)
+-include autoMakefile
+else ifneq ($(filter-out $(ALWAYS_TARGETS), $(MAKECMDGOALS)),)
+-include autoMakefile
+endif
+
+#
+# Include Makefile.exports to import the autoconf variables as plain make
+# variables. Include the remaining make files to import their specific
+# make targets.
+#
+
+-include config/Makefile.exports
+-include config/Makefile.codetags
+-include config/Makefile.pkg-rpm
+-include config/Makefile.dkms-rpm
+-include config/Makefile.pkg-deb
+-include config/Makefile.dkms-deb
+-include config/Makefile.checkstack
+
+#
+# Pass all lines in the make recipe to a single invocation of the shell
+# See: https://www.gnu.org/software/make/manual/html_node/One-Shell.html
+#
+
+.ONESHELL:
+
+#
+# Help text in the style of the Linux kernel. When in doubt, try to mimic Linux
+# style. And don't bother including intermediate build targets if they are not
+# likely to be run iteractively.
+#
+
+help:
+	@echo  'Cleaning targets:'
+	echo  '  clean	            - Remove generated object files but keep the files'
+	echo  '                      generated by autotools; use `git clean -xdf` to'
+	echo  '                      purge all files not tracked by git'
+	echo  ''
+	echo  'Generic targets:'
+	echo  '  all               - Build all modules and utilities enabled by'
+	echo  '                      autotools'
+	echo  '  utils             - Build only userspace utilities'
+	echo  '  modules           - Build only kernel modules'
+	echo  '  checkpatch        - Run checkpatch.pl on latest commit'
+	echo  '  checkstack        - Run checkstack.pl'
+	echo  '  checkstack-update - Update checkstack.pl'
+	echo  '  checkstack-clean  - Remove checkstack.pl artifacts'
+	echo  ''
+	echo  'Packaging targets:'
+	echo  '  rpms              - Create RPM packages'
+	echo  '  srpm              - Create source RPM packages'
+	echo  '  dkms-rpm          - Create DKMS RPM packages'
+	echo  '  dkms-srpm         - Create source DKMS RPM packages'
+	echo  '  debs              - Create Debian packages'
+	echo  '  dkms-debs         - Create DKMS Debian packages'
+
+#
+# Run Lustre's custom checkpatch.pl script against the latest commit.
+#
+
+checkpatch:
+	@git diff HEAD~1 | ./contrib/scripts/checkpatch.pl
+
+#
+# Several modules (like ldiskfs or in-kernel-o2iblnd) define sources targets.
+# This generic make target assigns 'foobar_sources' to run
+# 'make source -C foobar'.
+#
+
+SOURCE_SUBDIRS := $(LUSTRE_BUILT_IN_KO2IBLND_NT) \
+	$(LUSTRE_LDISKFS_MT)
+SOURCE_SUBDIRS_TGT = $(addsuffix _sources,$(SOURCE_SUBDIRS))
+
+
+.PHONY: $(SOURCE_SUBDIRS_TGT)
+$(SOURCE_SUBDIRS_TGT):
+	$(MAKE) sources -C $(patsubst %_sources,%,$@)
+
+#
+# Generic make target to assigns 'foobar' to 'make -C foobar'.
+#
+
+SUBDIRS := $(LUSTRE_IOKIT_MT) $(LUSTRE_LDISKFS_MT) \
+	Documentation lustre/tests lustre/scripts \
+	lustre/conf lib lnet/utils lustre/utils
+
+
+.PHONY: $(SUBDIRS)
+$(SUBDIRS):
+	$(MAKE) -C $@
+
+#
+# lnet/klnds/o2iblnd is used to build the Infiniband LNet driver against the
+# out-of-tree Mellanox driver. This is split to a separate build target so that
+# the generic module build target doesn't require external symbol versions.
+#
+
+.PHONY: lnet/klnds/o2iblnd
+lnet/klnds/o2iblnd: __modules
+	$(MAKE) BUILD_EXT_O2IB=yes \
+	KBUILD_EXTRA_SYMBOLS="$(PWD)/Module.symvers $(LUSTRE_EXT_O2IB_SYMBOLS) $(LUSTRE_EXTRA_SYMBOLS)" \
+	CC="$(LUSTRE_CC)" -C $(LUSTRE_LINUX_OBJ) \
+	M=$(PWD)/lnet/klnds/o2iblnd
+
+#
+# Build the in-kernel-o2iblnd module as ko2iblnd.ko and rename it to
+# in-kernel-ko2iblnd.ko so that the kernel recognizes the module as
+# ko2iblnd despite the file name being in-kernel-ko2iblnd.
+#
+
+.PHONY: lnet/klnds/in-kernel-o2iblnd
+lnet/klnds/in-kernel-o2iblnd: __modules
+	cp -vf lnet/klnds/in-kernel-o2iblnd/ko2iblnd.ko \
+		lnet/klnds/in-kernel-o2iblnd/in-kernel-ko2iblnd.ko
+
+#
+# This is the entry point to the Linux build system. The top-level Kbuild file
+# is the next file invoked by this target.
+#
+
+.PHONY: __modules
+__modules: $(SOURCE_SUBDIRS_TGT)
+	$(MAKE) KBUILD_EXTRA_SYMBOLS="$(LUSTRE_INT_O2IB_SYMBOLS) $(LUSTRE_EXTRA_SYMBOLS) $(KBUILD_EXTRA_SYMBOLS)" \
+	CC="$(LUSTRE_CC)" -C $(LUSTRE_LINUX_OBJ) \
+	M=$(PWD)
+
+#
+# Express dependencies for the already defined make targets. Make automatically
+# combines make targets with the same name if they are in the same file. By
+# listing the dependencies at the end, we can define most targets using generic
+# make targets.
+#
+
+lnet/utils: lib
+lustre/utils: lib lnet/utils
+utils: lib lnet/utils lustre/utils
+lustre/tests: utils
+ldiskfs: $(SOURCE_SUBDIRS_TGT)
+modules: __modules $(LUSTRE_BUILT_IN_KO2IBLND_NT) $(LUSTRE_EXTERNAL_KO2IBLND_NT)
+__default: utils $(LUSTRE_MODULES_MT) $(SUBDIRS)

config/.gitignore
@@ -1,3 +1,4 @@
+/Makefile.exports
 /Makefile.in
 /compile
 /config.guess

config/Makefile.am
@@ -12,3 +12,9 @@ EXTRA_DIST += lustre-toolchain.m4 lustre-compiler-plugins.m4
 EXTRA_DIST += lustre-core.m4 kerberos5.m4 ac_pkg_swig.m4
 EXTRA_DIST += ac_python_devel.m4 lustre-lnet.m4
 EXTRA_DIST += lustre-libcfs.m4 lustre-erasurecode.m4
+EXTRA_DIST += Makefile.exports.in
+EXTRA_DIST += Makefile.pkg-rpm
+EXTRA_DIST += Makefile.dkms-rpm
+EXTRA_DIST += Makefile.pkg-deb
+EXTRA_DIST += Makefile.dkms-deb
+EXTRA_DIST += Makefile.checkstack

commit 8c441281a734dee9f720cadd748365bcb871a03b

LU-18687 build: restore -Werror for kmod build
We don't want compiler warnings in the Lustre kernel modules.

Fixes: 4ff30bece12c ("LU-18687 build: refactor the top-level Makefile")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I84455aee5bf4e7215346db9f4d9a016f6598182f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67097
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

Kbuild
@@ -22,3 +22,4 @@ subdir-ccflags-y := -include $(lsrctree)/config.h
 subdir-ccflags-y += $(call cc-option, -Wno-format-truncation)
 subdir-ccflags-y += $(call cc-option, -Wno-stringop-truncation)
 subdir-ccflags-y += $(call cc-option, -Wno-stringop-overflow)
+subdir-ccflags-y += $(KMOD_WERROR_FLAG)

config/Makefile.exports.in
@@ -98,6 +98,7 @@ export ABS_TOP_BUILDDIR := @abs_top_builddir@
 export GDS_PATH := @GDS_PATH@
 export CUDA_PATH := @CUDA_PATH@
 export EFA_INCLUDE_PATH := @EFA_INCLUDE_PATH@
+export KMOD_WERROR_FLAG := @KMOD_WERROR_FLAG@
 export KFICPPFLAGS := @KFICPPFLAGS@
 export GNICPPFLAGS := @GNICPPFLAGS@
 export EXTRA_OFED_CONFIG := @EXTRA_OFED_CONFIG@

config/lustre-toolchain.m4
@@ -57,13 +57,16 @@ AC_DEFUN([LTC_CONFIG_ERROR], [
 AC_ARG_ENABLE([strict-errors],
     AS_HELP_STRING([--disable-strict-errors], [Disable strict error C flags]))
 
+KMOD_WERROR_FLAG=""
 AS_IF([test "x$enable_strict_errors" != "xno"], [
 AS_IF([test $target_cpu == "i686" -o $target_cpu == "x86_64"], [
 CFLAGS="$CFLAGS -Wall -Werror"
+KMOD_WERROR_FLAG="-Wall -Werror"
 ])
 ], [
 CFLAGS="$CFLAGS -Wall -Wno-error -Wno-error=incompatible-function-pointer-types -Wno-error=incompatible-pointer-types"
 ])
+AC_SUBST(KMOD_WERROR_FLAG)
 ]) # LTC_CONFIG_ERROR
 
 #