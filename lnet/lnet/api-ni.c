lustre/lustre-release

commit 6b46e3fd8a3b2b77210c3c081203ff3a51ddb0a4

LU-19485 lnet: Deprecate ip2nets and routes params
LNet currently supports network and routing configuration via kernel
module parameters (ip2nets= and routes=) supplied at module load time
(e.g. via modprobe.conf). This approach is being deprecated in favor
of Dynamic LNet Config (DLC) which uses user-space parsing of YAML +
netlink.

Modify the description of these parameters to note the deprecation,
and issue a one-time warning at startup if these parameters are being
used.

Surround relevant code in a version macro so the code will
automatically "disappear" in Lustre 2.18.

Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ifa60533c9a1362b9cb17c7451474e7a755a652c8
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61861
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -25,6 +25,7 @@
 #include <lustre_compat/net/linux-net.h>
 #include <lnet/udsp.h>
 #include <lnet/lib-lnet.h>
+#include <uapi/linux/lustre/lustre_ver.h>
 
 #define D_LNI D_CONSOLE
 
@@ -39,17 +40,19 @@ struct lnet the_lnet = {
 };		/* THE state of the network */
 EXPORT_SYMBOL(the_lnet);
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 static char *ip2nets = "";
 module_param(ip2nets, charp, 0444);
-MODULE_PARM_DESC(ip2nets, "LNET network <- IP table");
+MODULE_PARM_DESC(ip2nets, "LNET network <- IP table (Deprecated)");
+
+static char *routes = "";
+module_param(routes, charp, 0444);
+MODULE_PARM_DESC(routes, "routes to non-local networks (Deprecated)");
+#endif
 
 static char *networks = "";
 module_param(networks, charp, 0444);
 MODULE_PARM_DESC(networks, "local networks");
-
-static char *routes = "";
-module_param(routes, charp, 0444);
-MODULE_PARM_DESC(routes, "routes to non-local networks");
 
 static int rnet_htable_size = LNET_REMOTE_NETS_HASH_DEFAULT;
 module_param(rnet_htable_size, int, 0444);
@@ -599,15 +602,18 @@ response_tracking_set(const char *val, cfs_kernel_param_arg_t *kp)
 	return 0;
 }
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 static const char *
 lnet_get_routes(void)
 {
 	return routes;
 }
+#endif
 
 static const char *
 lnet_get_networks(void)
 {
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 	const char *nets;
 	int rc;
 
@@ -620,6 +626,7 @@ lnet_get_networks(void)
 		rc = lnet_parse_ip2nets(&nets, ip2nets);
 		return (rc == 0) ? nets : NULL;
 	}
+#endif
 
 	if (*networks != 0)
 		return networks;
@@ -3192,7 +3199,6 @@ void lnet_lib_exit(void)
 int
 LNetNIInit(lnet_pid_t requested_pid)
 {
-	int im_a_router = 0;
 	int rc;
 	int ni_bytes;
 	struct lnet_ping_buffer	*pbuf;
@@ -3253,7 +3259,10 @@ LNetNIInit(lnet_pid_t requested_pid)
 	if (rc < 0)
 		goto err_empty_list;
 
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 	if (!the_lnet.ln_nis_from_mod_params) {
+		int im_a_router = 0;
+
 		rc = lnet_parse_routes(lnet_get_routes(), &im_a_router);
 		if (rc != 0)
 			goto err_shutdown_lndnis;
@@ -3265,6 +3274,7 @@ LNetNIInit(lnet_pid_t requested_pid)
 		if (the_lnet.ln_routing == LNET_ROUTING_ENABLED)
 			LCONSOLE_INFO("Message forwarding enabled\n");
 	}
+#endif
 
 	rc = lnet_acceptor_start();
 	if (rc != 0)
@@ -3319,9 +3329,11 @@ err_acceptor_stop:
 	the_lnet.ln_refcount = 0;
 	lnet_acceptor_stop();
 err_destroy_routes:
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
 	if (!the_lnet.ln_nis_from_mod_params)
 		lnet_destroy_routes();
 err_shutdown_lndnis:
+#endif
 	lnet_shutdown_lndnets();
 err_empty_list:
 	lnet_unprepare();

lnet/lnet/config.c
@@ -1222,6 +1222,9 @@ lnet_parse_routes(const char *routes, int *im_a_router)
 
 	*im_a_router = 0;
 
+	if (strlen(routes))
+		CWARN("Kernel parsing of LNet routes is deprecated. Consider converting to LNet YAML configuration.\n");
+
 	if (lnet_str2tbs_sep(&tbs, routes) < 0) {
 		CERROR("Error parsing routes\n");
 		rc = -EINVAL;
@@ -1568,6 +1571,8 @@ lnet_parse_ip2nets(const char **networksp, const char *ip2nets)
 	int nip;
 	int rc;
 	int i;
+
+	CWARN("Kernel parsing of ip2nets is deprecated. Consider converting to LNet YAML configuration.\n");
 
 	if (current->nsproxy && current->nsproxy->net_ns)
 		nip = lnet_inet_enumerate(&ifaces, current->nsproxy->net_ns,

commit 227c1d8fe29cac1dd9591d5e0fd7e921a564ad31

LU-20441 lnet: bump ip2nets/routes deprecation to 2.20
The ip2nets/routes deprecation was only added in commit
2.16.61-44-g6b46e3fd8a, so it hasn't been in any LTS release We should
make ip2nets/routes deprecated, but available, in the next LTS.

Bump the deadline to 2.19.53 to give operators additional time to
migrate to DLC-based (YAML/netlink) configuration.

Fixes: 6b46e3fd8a ("LU-19485 lnet: Deprecate ip2nets and routes params")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chorn@ddn.com>
Change-Id: Iba538d19f911ae2802083e5b06dd782b713834d3
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66998
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/api-ni.c
@@ -36,7 +36,7 @@ struct lnet the_lnet = {
 };		/* THE state of the network */
 EXPORT_SYMBOL(the_lnet);
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
 static char *ip2nets = "";
 module_param(ip2nets, charp, 0444);
 MODULE_PARM_DESC(ip2nets, "LNET network <- IP table (Deprecated)");
@@ -551,7 +551,7 @@ response_tracking_set(const char *val, const struct kernel_param *kp)
 	return 0;
 }
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
 static const char *
 lnet_get_routes(void)
 {
@@ -562,7 +562,7 @@ lnet_get_routes(void)
 static const char *
 lnet_get_networks(void)
 {
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
 	const char *nets;
 	int rc;
 
@@ -3322,7 +3322,7 @@ LNetNIInit(lnet_pid_t requested_pid)
 	if (rc < 0)
 		goto err_empty_list;
 
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
 	if (!the_lnet.ln_nis_from_mod_params) {
 		int im_a_router = 0;
 
@@ -3392,7 +3392,7 @@ err_acceptor_stop:
 	the_lnet.ln_refcount = 0;
 	lnet_acceptor_stop();
 err_destroy_routes:
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 17, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
 	if (!the_lnet.ln_nis_from_mod_params)
 		lnet_destroy_routes();
 err_shutdown_lndnis: