lustre/lustre-release

commit fa9da556ad22b1485c53cf0337dc6872d89aedfa

LU-16205 sec: fid2path for encrypted files
Add support of fid2path for encrypted files. Server side returns raw
encrypted path name to client, which needs to process the returned
string. This is done from top to bottom, by iteratively decrypting
parent name and then doing a lookup on it, so that child can in turn
be decrypted.

For encrypted files that do not have their names encrypted, lookups
can be skipped. Indeed, name decryption is a no-op in this case, which
means it is not necessary to fetch the encryption key associated with
the parent inode.

Without the encryption key, lookups are skipped for the same reason.
But names have to be encoded and/or digested. So server needs to
insert FIDs of individual path components in the returned string.
These FIDs are interpreted by the client to build encoded/digested
names.

Add sanity-sec test_63 to exercise this new capability.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I165bf2e5657037ae2e25c9378e4713537ea94bec
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48930
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: jsimmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -538,6 +538,23 @@ static inline void obd_connect_set_name_enc(struct obd_connect_data *data)
 #endif
 }
 
+static inline bool obd_connect_has_enc_fid2path(struct obd_connect_data *data)
+{
+#ifdef HAVE_LUSTRE_CRYPTO
+	return data->ocd_connect_flags & OBD_CONNECT_FLAGS2 &&
+		data->ocd_connect_flags2 & OBD_CONNECT2_ENCRYPT_FID2PATH;
+#else
+	return false;
+#endif
+}
+
+static inline void obd_connect_set_enc_fid2path(struct obd_connect_data *data)
+{
+#ifdef HAVE_LUSTRE_CRYPTO
+	data->ocd_connect_flags2 |= OBD_CONNECT2_ENCRYPT_FID2PATH;
+#endif
+}
+
 /*
  * Locking to guarantee consistency of non-atomic updates to long long i_size,
  * consistency between file size and KMS.

lustre/include/lustre_export.h
@@ -484,6 +484,11 @@ static inline int exp_connect_encrypt(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT);
 }
 
+static inline int exp_connect_encrypt_fid2path(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT_FID2PATH);
+}
+
 static inline int exp_connect_lseek(struct obd_export *exp)
 {
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_LSEEK);

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -911,7 +911,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_REP_MBITS | \
 				OBD_CONNECT2_ATOMIC_OPEN_LOCK | \
 				OBD_CONNECT2_BATCH_RPC | \
-				OBD_CONNECT2_ENCRYPT_NAME)
+				OBD_CONNECT2_ENCRYPT_NAME | \
+				OBD_CONNECT2_ENCRYPT_FID2PATH)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \

lustre/llite/file.c
@@ -2865,14 +2865,150 @@ out:
 	RETURN(rc);
 }
 
+static int fid2path_for_enc_file(struct inode *parent, char *gfpath,
+				 __u32 gfpathlen)
+{
+	struct dentry *de = NULL, *de_parent = d_find_any_alias(parent);
+	struct llcrypt_str lltr = LLTR_INIT(NULL, 0);
+	struct llcrypt_str de_name;
+	char *p, *ptr = gfpath;
+	size_t len = 0, len_orig = 0;
+	int enckey = -1, nameenc = -1;
+	int rc = 0;
+
+	gfpath++;
+	while ((p = strsep(&gfpath, "/")) != NULL) {
+		struct lu_fid fid;
+
+		de = NULL;
+		if (!*p) {
+			dput(de_parent);
+			break;
+		}
+		len_orig = strlen(p);
+
+		rc = sscanf(p, "["SFID"]", RFID(&fid));
+		if (rc == 3)
+			p = strchr(p, ']') + 1;
+		else
+			fid_zero(&fid);
+		rc = 0;
+		len = strlen(p);
+
+		if (!IS_ENCRYPTED(parent)) {
+			if (gfpathlen < len + 1) {
+				dput(de_parent);
+				rc = -EOVERFLOW;
+				break;
+			}
+			memmove(ptr, p, len);
+			p = ptr;
+			ptr += len;
+			*(ptr++) = '/';
+			gfpathlen -= len + 1;
+			goto lookup;
+		}
+
+		/* From here, we know parent is encrypted */
+
+		if (enckey != 0) {
+			rc = llcrypt_get_encryption_info(parent);
+			if (rc && rc != -ENOKEY) {
+				dput(de_parent);
+				break;
+			}
+		}
+
+		if (enckey == -1) {
+			if (llcrypt_has_encryption_key(parent))
+				enckey = 1;
+			else
+				enckey = 0;
+			if (enckey == 1)
+				nameenc =
+					llcrypt_policy_has_filename_enc(parent);
+		}
+
+		/* Even if names are not encrypted, we still need to call
+		 * ll_fname_disk_to_usr in order to decode names as they are
+		 * coming from the wire.
+		 */
+		rc = llcrypt_fname_alloc_buffer(parent, NAME_MAX + 1, &lltr);
+		if (rc < 0) {
+			dput(de_parent);
+			break;
+		}
+
+		de_name.name = p;
+		de_name.len = len;
+		rc = ll_fname_disk_to_usr(parent, 0, 0, &de_name,
+					  &lltr, &fid);
+		if (rc) {
+			llcrypt_fname_free_buffer(&lltr);
+			dput(de_parent);
+			break;
+		}
+		lltr.name[lltr.len] = '\0';
+
+		if (lltr.len <= len_orig && gfpathlen >= lltr.len + 1) {
+			memcpy(ptr, lltr.name, lltr.len);
+			p = ptr;
+			len = lltr.len;
+			ptr += lltr.len;
+			*(ptr++) = '/';
+			gfpathlen -= lltr.len + 1;
+		} else {
+			rc = -EOVERFLOW;
+		}
+		llcrypt_fname_free_buffer(&lltr);
+
+		if (rc == -EOVERFLOW) {
+			dput(de_parent);
+			break;
+		}
+
+lookup:
+		if (!gfpath) {
+			/* We reached the end of the string, which means
+			 * we are dealing with the last component in the path.
+			 * So save a useless lookup and exit.
+			 */
+			dput(de_parent);
+			break;
+		}
+
+		if (enckey == 0 || nameenc == 0)
+			continue;
+
+		inode_lock(parent);
+		de = lookup_one_len(p, de_parent, len);
+		inode_unlock(parent);
+		if (IS_ERR_OR_NULL(de) || !de->d_inode) {
+			dput(de_parent);
+			rc = -ENODATA;
+			break;
+		}
+
+		parent = de->d_inode;
+		dput(de_parent);
+		de_parent = de;
+	}
+
+	if (len)
+		*(ptr - 1) = '\0';
+	if (!IS_ERR_OR_NULL(de))
+		dput(de);
+	return rc;
+}
+
 int ll_fid2path(struct inode *inode, void __user *arg)
 {
-	struct obd_export	*exp = ll_i2mdexp(inode);
+	struct obd_export *exp = ll_i2mdexp(inode);
 	const struct getinfo_fid2path __user *gfin = arg;
-	__u32			 pathlen;
-	struct getinfo_fid2path	*gfout;
-	size_t			 outsize;
-	int			 rc;
+	__u32 pathlen, pathlen_orig;
+	struct getinfo_fid2path *gfout;
+	size_t outsize;
+	int rc = 0;
 
 	ENTRY;
 
@@ -2886,7 +3022,9 @@ int ll_fid2path(struct inode *inode, void __user *arg)
 
 	if (pathlen > PATH_MAX)
 		RETURN(-EINVAL);
+	pathlen_orig = pathlen;
 
+gf_alloc:
 	outsize = sizeof(*gfout) + pathlen;
 	OBD_ALLOC(gfout, outsize);
 	if (gfout == NULL)
@@ -2898,17 +3036,35 @@ int ll_fid2path(struct inode *inode, void __user *arg)
 	 * can lookup the correct path, this is mainly for fileset.
 	 * old server without fileset mount support will ignore this. */
 	*gfout->gf_u.gf_root_fid = *ll_inode2fid(inode);
+	gfout->gf_pathlen = pathlen;
 
 	/* Call mdc_iocontrol */
 	rc = obd_iocontrol(OBD_IOC_FID2PATH, exp, outsize, gfout, NULL);
 	if (rc != 0)
 		GOTO(gf_free, rc);
 
-	if (copy_to_user(arg, gfout, outsize))
+	if (gfout->gf_pathlen && gfout->gf_u.gf_path[0] == '/') {
+		/* by convention, server side (mdt_path_current()) puts
+		 * a leading '/' to tell client that we are dealing with
+		 * an encrypted file
+		 */
+		rc = fid2path_for_enc_file(inode, gfout->gf_u.gf_path,
+					   gfout->gf_pathlen);
+		if (rc)
+			GOTO(gf_free, rc);
+		if (strlen(gfout->gf_u.gf_path) > gfin->gf_pathlen)
+			GOTO(gf_free, rc = -EOVERFLOW);
+	}
+
+	if (copy_to_user(arg, gfout, sizeof(*gfout) + pathlen_orig))
 		rc = -EFAULT;
 
 gf_free:
 	OBD_FREE(gfout, outsize);
+	if (rc == -ENAMETOOLONG) {
+		pathlen += PATH_MAX;
+		GOTO(gf_alloc, rc);
+	}
 	RETURN(rc);
 }
 

lustre/llite/llite_lib.c
@@ -381,6 +381,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt)
 
 	obd_connect_set_secctx(data);
 	if (ll_sbi_has_encrypt(sbi)) {
+		obd_connect_set_enc_fid2path(data);
 		obd_connect_set_name_enc(data);
 		obd_connect_set_enc(data);
 	}

lustre/lmv/lmv_obd.c
@@ -571,6 +571,8 @@ static int lmv_fid2path(struct obd_export *exp, int len, void *karg,
 	struct getinfo_fid2path *remote_gf = NULL;
 	struct lu_fid root_fid;
 	int remote_gf_size = 0;
+	int currentisenc = 0;
+	int globalisenc = 0;
 	int rc;
 
 	gf = karg;
@@ -586,10 +588,23 @@ repeat_fid2path:
 	if (rc != 0 && rc != -EREMOTE)
 		GOTO(out_fid2path, rc);
 
+	if (gf->gf_u.gf_path[0] == '/') {
+		/* by convention, server side (mdt_path_current()) puts
+		 * a leading '/' to tell client that we are dealing with
+		 * an encrypted file
+		 */
+		currentisenc = 1;
+		globalisenc = 1;
+	} else {
+		currentisenc = 0;
+	}
+
 	/* If remote_gf != NULL, it means just building the
-	 * path on the remote MDT, copy this path segement to gf */
+	 * path on the remote MDT, copy this path segment to gf.
+	 */
 	if (remote_gf != NULL) {
 		struct getinfo_fid2path *ori_gf;
+		int oldisenc = 0;
 		char *ptr;
 		int len;
 
@@ -599,13 +614,22 @@ repeat_fid2path:
 			GOTO(out_fid2path, rc = -EOVERFLOW);
 
 		ptr = ori_gf->gf_u.gf_path;
+		oldisenc = ptr[0] == '/';
 
 		len = strlen(gf->gf_u.gf_path);
-		/* move the current path to the right to release space
-		 * for closer-to-root part */
-		memmove(ptr + len + 1, ptr, strlen(ori_gf->gf_u.gf_path));
-		memcpy(ptr, gf->gf_u.gf_path, len);
-		ptr[len] = '/';
+		if (len) {
+			/* move the current path to the right to release space
+			 * for closer-to-root part
+			 */
+			memmove(ptr + len - currentisenc + 1 + globalisenc,
+				ptr + oldisenc,
+				strlen(ori_gf->gf_u.gf_path) - oldisenc + 1);
+			if (globalisenc)
+				*(ptr++) = '/';
+			memcpy(ptr, gf->gf_u.gf_path + currentisenc,
+			       len - currentisenc);
+			ptr[len - currentisenc] = '/';
+		}
 	}
 
 	CDEBUG(D_INFO, "%s: get path %s "DFID" rec: %llu ln: %u\n",
@@ -618,11 +642,11 @@ repeat_fid2path:
 
 	/* sigh, has to go to another MDT to do path building further */
 	if (remote_gf == NULL) {
-		remote_gf_size = sizeof(*remote_gf) + PATH_MAX;
+		remote_gf_size = sizeof(*remote_gf) + len - sizeof(*gf);
 		OBD_ALLOC(remote_gf, remote_gf_size);
 		if (remote_gf == NULL)
 			GOTO(out_fid2path, rc = -ENOMEM);
-		remote_gf->gf_pathlen = PATH_MAX;
+		remote_gf->gf_pathlen = len - sizeof(*gf);
 	}
 
 	if (!fid_is_sane(&gf->gf_fid)) {

lustre/mdc/mdc_request.c
@@ -1698,8 +1698,6 @@ static int mdc_ioc_fid2path(struct obd_export *exp, struct getinfo_fid2path *gf)
 	void *key;
 	int rc;
 
-	if (gf->gf_pathlen > PATH_MAX)
-		RETURN(-ENAMETOOLONG);
 	if (gf->gf_pathlen < 2)
 		RETURN(-EOVERFLOW);
 
@@ -1731,11 +1729,10 @@ static int mdc_ioc_fid2path(struct obd_export *exp, struct getinfo_fid2path *gf)
 	if (vallen > sizeof(*gf) + gf->gf_pathlen)
 		GOTO(out, rc = -EOVERFLOW);
 
-	CDEBUG(D_IOCTL, "path got "DFID" from %llu #%d: %s\n",
+	CDEBUG(D_IOCTL, "path got "DFID" from %llu #%d: %.*s\n",
 	       PFID(&gf->gf_fid), gf->gf_recno, gf->gf_linkno,
-	       gf->gf_pathlen < 512 ? gf->gf_u.gf_path :
-	       /* only log the last 512 characters of the path */
-	       gf->gf_u.gf_path + gf->gf_pathlen - 512);
+	       /* only log the first 512 characters of the path */
+	       512, gf->gf_u.gf_path);
 
 out:
 	OBD_FREE(key, keylen);

lustre/mdt/mdt_handler.c
@@ -7039,6 +7039,10 @@ static int mdt_path_current(struct mdt_thread_info *info,
 	struct mdt_object *mdt_obj;
 	struct link_ea_header *leh;
 	struct link_ea_entry *lee;
+	bool worthchecking = true;
+	bool needsfid = false;
+	bool supported = false;
+	int isenc = -1;
 	char *ptr;
 	int reclen;
 	int rc = 0;
@@ -7081,6 +7085,39 @@ static int mdt_path_current(struct mdt_thread_info *info,
 			GOTO(remote_out, rc = -EREMOTE);
 		}
 
+		if (worthchecking) {
+			/* we need to know if the FID being
+			 * looked up is encrypted
+			 */
+			struct lu_attr la = { 0 };
+			struct dt_object *dt = mdt_obj2dt(mdt_obj);
+
+			if (dt && dt->do_ops && dt->do_ops->do_attr_get)
+				dt_attr_get(info->mti_env, dt, &la);
+			if (la.la_valid & LA_FLAGS &&
+			    la.la_flags & LUSTRE_ENCRYPT_FL) {
+				if (!supported && mdt_info_req(info) &&
+				    !exp_connect_encrypt_fid2path(
+					    mdt_info_req(info)->rq_export)) {
+					/* client does not support fid2path
+					 * for encrypted files
+					 */
+					mdt_object_put(info->mti_env, mdt_obj);
+					GOTO(out, rc = -ENODATA);
+				} else {
+					supported = true;
+				}
+				needsfid = true;
+				if (isenc == -1)
+					isenc = 1;
+			} else {
+				worthchecking = false;
+				needsfid = false;
+				if (isenc == -1)
+					isenc = 0;
+			}
+		}
+
 		rc = mdt_links_read(info, mdt_obj, &ldata);
 		if (rc != 0) {
 			mdt_object_put(info->mti_env, mdt_obj);
@@ -7118,8 +7155,21 @@ static int mdt_path_current(struct mdt_thread_info *info,
 		/* Pack the name in the end of the buffer */
 		ptr -= tmpname->ln_namelen;
 		if (ptr - 1 <= fp->gf_u.gf_path)
-			GOTO(out, rc = -EOVERFLOW);
+			GOTO(out, rc = -ENAMETOOLONG);
 		strncpy(ptr, tmpname->ln_name, tmpname->ln_namelen);
+		if (needsfid) {
+			/* Pack FID before file name, so that client can build
+			 * encoded/digested form.
+			 */
+			char fidstr[FID_LEN + 1];
+
+			snprintf(fidstr, sizeof(fidstr), DFID,
+				 PFID(&fp->gf_fid));
+			ptr -= strlen(fidstr);
+			if (ptr - 1 <= fp->gf_u.gf_path)
+				GOTO(out, rc = -ENAMETOOLONG);
+			strncpy(ptr, fidstr, strlen(fidstr));
+		}
 		*(--ptr) = '/';
 
 		/* keep the last resolved fid to the client, so the
@@ -7134,7 +7184,8 @@ static int mdt_path_current(struct mdt_thread_info *info,
 	rc = 0;
 
 remote_out:
-	ptr++; /* skip leading / */
+	if (isenc != 1)
+		ptr++; /* skip leading / unless this is an encrypted file */
 	memmove(fp->gf_u.gf_path, ptr,
 		fp->gf_u.gf_path + fp->gf_pathlen - ptr);
 
@@ -7229,24 +7280,12 @@ static int mdt_fid2path(struct mdt_thread_info *info,
 		RETURN(rc);
 	}
 
-	if (mdt_object_remote(obj)) {
+	if (mdt_object_remote(obj))
 		rc = -EREMOTE;
-	} else if (!mdt_object_exists(obj)) {
+	else if (!mdt_object_exists(obj))
 		rc = -ENOENT;
-	} else {
-		struct lu_attr la = { 0 };
-		struct dt_object *dt = mdt_obj2dt(obj);
-
-		if (dt && dt->do_ops && dt->do_ops->do_attr_get)
-			dt_attr_get(info->mti_env, mdt_obj2dt(obj), &la);
-		if (la.la_valid & LA_FLAGS && la.la_flags & LUSTRE_ENCRYPT_FL)
-			/* path resolution cannot be carried out on server
-			 * side for encrypted files
-			 */
-			rc = -ENODATA;
-		else
-			rc = 0;
-	}
+	else
+		rc = 0;
 
 	if (rc < 0) {
 		mdt_object_put(info->mti_env, obj);

commit bb5a4e2a22c868a9de3edd0a75b33e23a111a272

LU-16518 llite: remove unused inline helpers
... since they are no longer used. obd_connect_has_enc_fid2path()
and ll_lov_delay_create_clear() have been unused ever since they
were introduced. sbi2mdc() and cl2vvp_dev() lost their last
callers a long time ago.

Fixes: fa9da556ad22 ("LU-16205 sec: fid2path for encrypted files")
Fixes: 19a2573e1633 ("LU-12885 mds: add enums for MDS_OPEN flags (4/4)")
Fixes: 006f258300e3 ("LU-3338 llite: Limit reply buffer size")
Fixes: 576f1994ae79 ("LU-6943 clio: get rid of cl_req")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Ife4f30b058153cc949244c1cb6668be11133c261
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67273
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/llite_internal.h
@@ -636,16 +636,6 @@ static inline void obd_connect_set_name_enc(struct obd_connect_data *data)
 #endif
 }
 
-static inline bool obd_connect_has_enc_fid2path(struct obd_connect_data *data)
-{
-#ifdef HAVE_LUSTRE_CRYPTO
-	return data->ocd_connect_flags & OBD_CONNECT_FLAGS2 &&
-		data->ocd_connect_flags2 & OBD_CONNECT2_ENCRYPT_FID2PATH;
-#else
-	return false;
-#endif
-}
-
 static inline void obd_connect_set_enc_fid2path(struct obd_connect_data *data)
 {
 #ifdef HAVE_LUSTRE_CRYPTO
@@ -1327,13 +1317,6 @@ static inline bool ll_lov_delay_create_is_set(unsigned int kernel_open_flags)
 	return (kernel_open_flags & O_LOV_DELAY_CREATE) == O_LOV_DELAY_CREATE;
 }
 
-/* Clear (file.f_flag) O_LOV_DELAY_CREATE(volatile) flag */
-static inline void ll_lov_delay_create_clear(unsigned int *kernel_open_flags)
-{
-	if (ll_lov_delay_create_is_set(*kernel_open_flags))
-		*kernel_open_flags &= ~O_LOV_DELAY_CREATE;
-}
-
 enum mds_open_flags ll_kernel_to_mds_open_flags(unsigned int kernel_open_flags);
 
 struct inode *ll_iget(struct super_block *sb, ino_t hash,
@@ -1670,15 +1653,6 @@ static inline struct obd_export *ll_s2dtexp(struct super_block *sb)
 static inline struct obd_export *ll_s2mdexp(struct super_block *sb)
 {
 	return ll_s2sbi(sb)->ll_md_exp;
-}
-
-static inline struct client_obd *sbi2mdc(struct ll_sb_info *sbi)
-{
-	struct obd_device *obd = sbi->ll_md_exp->exp_obd;
-
-	if (obd == NULL)
-		LBUG();
-	return &obd->u.cli;
 }
 
 // FIXME: replace the name of this with LL_SB to conform to kernel stuff

lustre/llite/vvp_internal.h
@@ -208,11 +208,6 @@ static inline struct vvp_device *lu2vvp_dev(const struct lu_device *d)
 	return container_of_safe(d, struct vvp_device, vdv_cl.cd_lu_dev);
 }
 
-static inline struct vvp_device *cl2vvp_dev(const struct cl_device *d)
-{
-	return container_of_safe(d, struct vvp_device, vdv_cl);
-}
-
 static inline struct vvp_object *cl2vvp(const struct cl_object *obj)
 {
 	return container_of_safe(obj, struct vvp_object, vob_cl);