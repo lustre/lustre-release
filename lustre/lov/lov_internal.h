lustre/lustre-release

commit 7107e54983b0a701c5c02a1e3c521302e8f79810

LU-9008 pfl: dynamic layout modification with write/truncate
* in lov_init_composite(), skip init sub object without LCME_FL_INIT
  layout component.
* issue layout intent RPC during write/trunc ops when try to write to
  an un-init-ed component (even if at the lock stage).
* After layout intent RPC issued, restart the IO.
* get rid of unused lov_layout_operations::llo_install() interface.
* add an empty mdt_layout_change() interface to handle intent layout
  write RPC.

Reviewed-on: https://review.whamcloud.com/25317

Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
Change-Id: I2f79482187d2af2660dd86e55da3f5dc0138e94a
Reviewed-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>

lustre/lov/lov_internal.h
@@ -45,6 +45,7 @@ struct lov_stripe_md_entry {
 	struct lu_extent	lsme_extent;
 	u32			lsme_id;
 	u32			lsme_magic;
+	u32			lsme_flags;
 	u32			lsme_pattern;
 	u32			lsme_stripe_size;
 	u16			lsme_stripe_count;
@@ -52,6 +53,16 @@ struct lov_stripe_md_entry {
 	char			lsme_pool_name[LOV_MAXPOOLNAME + 1];
 	struct lov_oinfo       *lsme_oinfo[];
 };
+
+static inline void copy_lsm_entry(struct lov_stripe_md_entry *dst,
+				  struct lov_stripe_md_entry *src)
+{
+	unsigned i;
+
+	for (i = 0; i < src->lsme_stripe_count; i++)
+		*dst->lsme_oinfo[i] = *src->lsme_oinfo[i];
+	memcpy(dst, src, offsetof(typeof(*src), lsme_oinfo));
+}
 
 struct lov_stripe_md {
 	atomic_t	lsm_refc;
@@ -327,5 +338,15 @@ static inline void lov_lsm2layout(struct lov_stripe_md *lsm,
 		ol->ol_comp_end = 0;
 		ol->ol_comp_id = 0;
 	}
+}
+
+static inline bool lsme_inited(const struct lov_stripe_md_entry *lsme)
+{
+	return lsme->lsme_flags & LCME_FL_INIT;
+}
+
+static inline bool lsm_entry_inited(const struct lov_stripe_md *lsm, int index)
+{
+	return lsme_inited(lsm->lsm_entries[index]);
 }
 #endif

lustre/include/cl_object.h
@@ -1829,6 +1829,11 @@ struct cl_io {
 	 */
 			     ci_ignore_layout:1,
 	/**
+	 * Need MDS intervention to complete a write. This usually means the
+	 * corresponding component is not initialized for the writing extent.
+	 */
+			     ci_need_write_intent:1,
+	/**
 	 * Check if layout changed after the IO finishes. Mainly for HSM
 	 * requirement. If IO occurs to openning files, it doesn't need to
 	 * verify layout because HSM won't release openning files.

lustre/include/lustre/lustre_idl.h
@@ -3119,22 +3119,22 @@ struct getparent {
 } __attribute__((packed));
 
 enum {
-        LAYOUT_INTENT_ACCESS    = 0,
-        LAYOUT_INTENT_READ      = 1,
-        LAYOUT_INTENT_WRITE     = 2,
-        LAYOUT_INTENT_GLIMPSE   = 3,
-        LAYOUT_INTENT_TRUNC     = 4,
-        LAYOUT_INTENT_RELEASE   = 5,
-        LAYOUT_INTENT_RESTORE   = 6
+	LAYOUT_INTENT_ACCESS	= 0,	/** generic access */
+	LAYOUT_INTENT_READ	= 1,	/** not used */
+	LAYOUT_INTENT_WRITE	= 2,	/** write file, for comp layout */
+	LAYOUT_INTENT_GLIMPSE	= 3,	/** not used */
+	LAYOUT_INTENT_TRUNC	= 4,	/** truncate file, for comp layout */
+	LAYOUT_INTENT_RELEASE	= 5,	/** reserved for HSM release */
+	LAYOUT_INTENT_RESTORE	= 6,	/** reserved for HSM restore */
 };
 
 /* enqueue layout lock with intent */
 struct layout_intent {
-	__u32 li_opc; /* intent operation for enqueue, read, write etc */
+	__u32 li_opc;	/* intent operation for enqueue, read, write etc */
 	__u32 li_flags;
 	__u64 li_start;
 	__u64 li_end;
-};
+} __attribute__((packed));
 
 /**
  * On the wire version of hsm_progress structure.

lustre/include/lustre_sec.h
@@ -63,6 +63,7 @@ struct ptlrpc_sec;
 struct ptlrpc_svc_ctx;
 struct ptlrpc_cli_ctx;
 struct ptlrpc_ctx_ops;
+struct req_msg_field;
 
 /**
  * \addtogroup flavor flavor
@@ -1084,7 +1085,8 @@ void sptlrpc_cli_free_reqbuf(struct ptlrpc_request *req);
 int sptlrpc_cli_alloc_repbuf(struct ptlrpc_request *req, int msgsize);
 void sptlrpc_cli_free_repbuf(struct ptlrpc_request *req);
 int sptlrpc_cli_enlarge_reqbuf(struct ptlrpc_request *req,
-                               int segment, int newsize);
+			       const struct req_msg_field *field,
+			       int newsize);
 int  sptlrpc_cli_unwrap_early_reply(struct ptlrpc_request *req,
                                     struct ptlrpc_request **req_ret);
 void sptlrpc_cli_finish_early_reply(struct ptlrpc_request *early_req);

lustre/llite/file.c
@@ -1191,7 +1191,6 @@ restart:
 		if (count > 0 && args->via_io_subtype == IO_NORMAL)
 			args->u.normal.via_iter = vio->vui_iter;
 	}
-	GOTO(out, rc);
 out:
 	cl_io_fini(env, io);
 
@@ -1226,7 +1225,7 @@ out:
 
 	CDEBUG(D_VFSTRACE, "iot: %d, result: %zd\n", iot, result);
 
-	return result > 0 ? result : rc;
+	RETURN(result > 0 ? result : rc);
 }
 
 /**
@@ -4121,9 +4120,9 @@ static int ll_layout_lock_set(struct lustre_handle *lockh, enum ldlm_mode mode,
 	lock_res_and_lock(lock);
 	lvb_ready = ldlm_is_lvb_ready(lock);
 	unlock_res_and_lock(lock);
+
 	/* checking lvb_ready is racy but this is okay. The worst case is
 	 * that multi processes may configure the file on the same time. */
-
 	if (lvb_ready)
 		GOTO(out, rc = 0);
 
@@ -4148,7 +4147,6 @@ static int ll_layout_lock_set(struct lustre_handle *lockh, enum ldlm_mode mode,
 	/* refresh layout failed, need to wait */
 	wait_layout = rc == -EBUSY;
 	EXIT;
-
 out:
 	LDLM_LOCK_PUT(lock);
 	ldlm_lock_decref(lockh, mode);
@@ -4173,39 +4171,37 @@ out:
 	RETURN(rc);
 }
 
-static int ll_layout_refresh_locked(struct inode *inode)
+/**
+ * Issue layout intent RPC to MDS.
+ * \param inode [in]	file inode
+ * \param intent [in]	layout intent
+ *
+ * \retval 0	on success
+ * \retval < 0	error code
+ */
+static int ll_layout_intent(struct inode *inode, struct layout_intent *intent)
 {
 	struct ll_inode_info  *lli = ll_i2info(inode);
 	struct ll_sb_info     *sbi = ll_i2sbi(inode);
 	struct md_op_data     *op_data;
-	struct lookup_intent	it;
-	struct lustre_handle	lockh;
-	enum ldlm_mode		mode;
+	struct lookup_intent it;
 	struct ptlrpc_request *req;
 	int rc;
 	ENTRY;
-
-again:
-	/* mostly layout lock is caching on the local side, so try to match
-	 * it before grabbing layout lock mutex. */
-	mode = ll_take_md_lock(inode, MDS_INODELOCK_LAYOUT, &lockh, 0,
-			       LCK_CR | LCK_CW | LCK_PR | LCK_PW);
-	if (mode != 0) { /* hit cached lock */
-		rc = ll_layout_lock_set(&lockh, mode, inode);
-		if (rc == -EAGAIN)
-			goto again;
-
-		RETURN(rc);
-	}
 
 	op_data = ll_prep_md_op_data(NULL, inode, inode, NULL,
 				     0, 0, LUSTRE_OPC_ANY, NULL);
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
-	/* have to enqueue one */
+	op_data->op_data = intent;
+	op_data->op_data_size = sizeof(*intent);
+
 	memset(&it, 0, sizeof(it));
 	it.it_op = IT_LAYOUT;
+	if (intent->li_opc == LAYOUT_INTENT_WRITE ||
+	    intent->li_opc == LAYOUT_INTENT_TRUNC)
+		it.it_flags = FMODE_WRITE;
 
 	LDLM_DEBUG_NOLOCK("%s: requeue layout lock for file "DFID"(%p)",
 			  ll_get_fsname(inode->i_sb, NULL, 0),
@@ -4219,18 +4215,11 @@ again:
 
 	ll_finish_md_op_data(op_data);
 
-	mode = it.it_lock_mode;
-	it.it_lock_mode = 0;
-	ll_intent_drop_lock(&it);
-
-	if (rc == 0) {
-		/* set lock data in case this is a new lock */
+	/* set lock data in case this is a new lock */
+	if (!rc)
 		ll_set_lock_data(sbi->ll_md_exp, inode, &it, NULL);
-		lockh.cookie = it.it_lock_handle;
-		rc = ll_layout_lock_set(&lockh, mode, inode);
-		if (rc == -EAGAIN)
-			goto again;
-	}
+
+	ll_intent_drop_lock(&it);
 
 	RETURN(rc);
 }
@@ -4252,6 +4241,11 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 {
 	struct ll_inode_info	*lli = ll_i2info(inode);
 	struct ll_sb_info	*sbi = ll_i2sbi(inode);
+	struct lustre_handle lockh;
+	struct layout_intent intent = {
+		.li_opc = LAYOUT_INTENT_ACCESS,
+	};
+	enum ldlm_mode mode;
 	int rc;
 	ENTRY;
 
@@ -4266,13 +4260,52 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 	/* take layout lock mutex to enqueue layout lock exclusively. */
 	mutex_lock(&lli->lli_layout_mutex);
 
-	rc = ll_layout_refresh_locked(inode);
-	if (rc < 0)
-		GOTO(out, rc);
+	while (1) {
+		/* mostly layout lock is caching on the local side, so try to
+		 * match it before grabbing layout lock mutex. */
+		mode = ll_take_md_lock(inode, MDS_INODELOCK_LAYOUT, &lockh, 0,
+				       LCK_CR | LCK_CW | LCK_PR | LCK_PW);
+		if (mode != 0) { /* hit cached lock */
+			rc = ll_layout_lock_set(&lockh, mode, inode);
+			if (rc == -EAGAIN)
+				continue;
+			break;
+		}
 
-	*gen = ll_layout_version_get(lli);
-out:
+		rc = ll_layout_intent(inode, &intent);
+		if (rc != 0)
+			break;
+	}
+
+	if (rc == 0)
+		*gen = ll_layout_version_get(lli);
 	mutex_unlock(&lli->lli_layout_mutex);
+
+	RETURN(rc);
+}
+
+/**
+ * Issue layout intent RPC indicating where in a file an IO is about to write.
+ *
+ * \param[in] inode	file inode.
+ * \param[in] start	start offset of fille in bytes where an IO is about to
+ *			write.
+ * \param[in] end	exclusive end offset in bytes of the write range.
+ *
+ * \retval 0	on success
+ * \retval < 0	error code
+ */
+int ll_layout_write_intent(struct inode *inode, __u64 start, __u64 end)
+{
+	struct layout_intent intent = {
+		.li_opc = LAYOUT_INTENT_WRITE,
+		.li_start = start,
+		.li_end = end,
+	};
+	int rc;
+	ENTRY;
+
+	rc = ll_layout_intent(inode, &intent);
 
 	RETURN(rc);
 }

lustre/llite/llite_internal.h
@@ -1419,6 +1419,7 @@ static inline void d_lustre_revalidate(struct dentry *dentry)
 int ll_layout_conf(struct inode *inode, const struct cl_object_conf *conf);
 int ll_layout_refresh(struct inode *inode, __u32 *gen);
 int ll_layout_restore(struct inode *inode, loff_t start, __u64 length);
+int ll_layout_write_intent(struct inode *inode, __u64 start, __u64 end);
 
 int ll_xattr_init(void);
 void ll_xattr_fini(void);

lustre/llite/vvp_io.c
@@ -298,18 +298,18 @@ static void vvp_io_fini(const struct lu_env *env, const struct cl_io_slice *ios)
 	struct cl_object *obj = io->ci_obj;
 	struct vvp_io    *vio = cl2vvp_io(env, ios);
 	struct inode     *inode = vvp_object_inode(obj);
+	int rc;
 
 	CLOBINVRNT(env, obj, vvp_object_invariant(obj));
 
 	CDEBUG(D_VFSTRACE, DFID" ignore/verify layout %d/%d, layout version %d "
-			   "restore needed %d\n",
+			   "need write layout %d, restore needed %d\n",
 	       PFID(lu_object_fid(&obj->co_lu)),
 	       io->ci_ignore_layout, io->ci_verify_layout,
-	       vio->vui_layout_gen, io->ci_restore_needed);
+	       vio->vui_layout_gen, io->ci_need_write_intent,
+	       io->ci_restore_needed);
 
 	if (io->ci_restore_needed) {
-		int	rc;
-
 		/* file was detected release, we need to restore it
 		 * before finishing the io
 		 */
@@ -332,6 +332,31 @@ static void vvp_io_fini(const struct lu_env *env, const struct cl_io_slice *ios)
 			io->ci_verify_layout = 0;
 			io->ci_result = rc;
 		}
+	}
+
+	/**
+	 * dynamic layout change needed, send layout intent
+	 * RPC.
+	 */
+	if (io->ci_need_write_intent) {
+		loff_t start = 0;
+		loff_t end = 0;
+
+		LASSERT(io->ci_type == CIT_WRITE || cl_io_is_trunc(io));
+
+		io->ci_need_write_intent = 0;
+
+		if (io->ci_type == CIT_WRITE) {
+			start = io->u.ci_rw.crw_pos;
+			end = io->u.ci_rw.crw_pos + io->u.ci_rw.crw_count;
+		} else {
+			end = io->u.ci_setattr.sa_attr.lvb_size;
+		}
+
+		rc = ll_layout_write_intent(inode, start, end);
+		io->ci_result = rc;
+		if (!rc)
+			io->ci_need_restart = 1;
 	}
 
 	if (!io->ci_ignore_layout && io->ci_verify_layout) {

lustre/lov/lov_ea.c
@@ -115,6 +115,9 @@ static void lsme_free(struct lov_stripe_md_entry *lsme)
 	unsigned int i;
 	size_t lsme_size;
 
+	if (!lsme_inited(lsme) ||
+	    lsme->lsme_pattern & LOV_PATTERN_F_RELEASED)
+		stripe_count = 0;
 	for (i = 0; i < stripe_count; i++)
 		OBD_SLAB_FREE_PTR(lsme->lsme_oinfo[i], lov_oinfo_slab);
 
@@ -142,7 +145,7 @@ void lsm_free(struct lov_stripe_md *lsm)
  */
 static struct lov_stripe_md_entry *
 lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
-	    const char *pool_name, struct lov_ost_data_v1 *objects,
+	    const char *pool_name, bool inited, struct lov_ost_data_v1 *objects,
 	    loff_t *maxbytes)
 {
 	struct lov_stripe_md_entry *lsme;
@@ -160,7 +163,7 @@ lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
 		RETURN(ERR_PTR(-EINVAL));
 
 	pattern = le32_to_cpu(lmm->lmm_pattern);
-	if (pattern & LOV_PATTERN_F_RELEASED)
+	if (pattern & LOV_PATTERN_F_RELEASED || !inited)
 		stripe_count = 0;
 	else
 		stripe_count = le16_to_cpu(lmm->lmm_stripe_count);
@@ -186,8 +189,10 @@ lsme_unpack(struct lov_obd *lov, struct lov_mds_md *lmm, size_t buf_size,
 
 	lsme->lsme_magic = magic;
 	lsme->lsme_pattern = pattern;
+	lsme->lsme_flags = 0;
 	lsme->lsme_stripe_size = le32_to_cpu(lmm->lmm_stripe_size);
-	lsme->lsme_stripe_count = stripe_count;
+	/* preserve the possible -1 stripe count for uninstantiated component */
+	lsme->lsme_stripe_count = le16_to_cpu(lmm->lmm_stripe_count);
 	lsme->lsme_layout_gen = le16_to_cpu(lmm->lmm_layout_gen);
 
 	if (pool_name != NULL) {
@@ -278,10 +283,12 @@ lsm_unpackmd_v1v3(struct lov_obd *lov,
 
 	pattern = le32_to_cpu(lmm->lmm_pattern);
 
-	lsme = lsme_unpack(lov, lmm, buf_size, pool_name, objects, &maxbytes);
+	lsme = lsme_unpack(lov, lmm, buf_size, pool_name, true, objects,
+			   &maxbytes);
 	if (IS_ERR(lsme))
 		RETURN(ERR_CAST(lsme));
 
+	lsme->lsme_flags = LCME_FL_INIT;
 	lsme->lsme_extent.e_start = 0;
 	lsme->lsme_extent.e_end = LUSTRE_EOF;
 
@@ -371,7 +378,7 @@ static int lsm_verify_comp_md_v1(struct lov_comp_md_v1 *lcm,
 
 static struct lov_stripe_md_entry *
 lsme_unpack_comp(struct lov_obd *lov, struct lov_mds_md *lmm,
-		 size_t lmm_buf_size, loff_t *maxbytes)
+		 size_t lmm_buf_size, bool inited, loff_t *maxbytes)
 {
 	unsigned int magic;
 	unsigned int stripe_count;
@@ -379,6 +386,9 @@ lsme_unpack_comp(struct lov_obd *lov, struct lov_mds_md *lmm,
 	stripe_count = le16_to_cpu(lmm->lmm_stripe_count);
 	if (stripe_count == 0)
 		RETURN(ERR_PTR(-EINVAL));
+	/* un-instantiated lmm contains no ost id info, i.e. lov_ost_data_v1 */
+	if (!inited)
+		stripe_count = 0;
 
 	magic = le32_to_cpu(lmm->lmm_magic);
 	if (magic != LOV_MAGIC_V1 && magic != LOV_MAGIC_V3)
@@ -389,12 +399,12 @@ lsme_unpack_comp(struct lov_obd *lov, struct lov_mds_md *lmm,
 
 	if (magic == LOV_MAGIC_V1) {
 		return lsme_unpack(lov, lmm, lmm_buf_size, NULL,
-				   lmm->lmm_objects, maxbytes);
+				   inited, lmm->lmm_objects, maxbytes);
 	} else {
 		struct lov_mds_md_v3 *lmm3 = (struct lov_mds_md_v3 *)lmm;
 
 		return lsme_unpack(lov, lmm, lmm_buf_size, lmm3->lmm_pool_name,
-				   lmm3->lmm_objects, maxbytes);
+				   inited, lmm3->lmm_objects, maxbytes);
 	}
 }
 
@@ -440,6 +450,8 @@ lsm_unpackmd_comp_md_v1(struct lov_obd *lov, void *buf, size_t buf_size)
 		blob = (char *)lcm + blob_offset;
 
 		lsme = lsme_unpack_comp(lov, blob, blob_size,
+					le32_to_cpu(lcme->lcme_flags) &
+					LCME_FL_INIT,
 					(i == entry_count - 1) ? &maxbytes :
 								 NULL);
 		if (IS_ERR(lsme))
@@ -450,6 +462,7 @@ lsm_unpackmd_comp_md_v1(struct lov_obd *lov, void *buf, size_t buf_size)
 
 		lsm->lsm_entries[i] = lsme;
 		lsme->lsme_id = le32_to_cpu(lcme->lcme_id);
+		lsme->lsme_flags = le32_to_cpu(lcme->lcme_flags);
 		lu_extent_le_to_cpu(&lsme->lsme_extent, &lcme->lcme_extent);
 
 		if (i == entry_count - 1) {
@@ -482,7 +495,7 @@ const struct lsm_operations lsm_comp_md_v1_ops = {
 
 void dump_lsm(unsigned int level, const struct lov_stripe_md *lsm)
 {
-	int i;
+	int i, j;
 
 	CDEBUG(level, "lsm %p, objid "DOSTID", maxbytes %#llx, magic 0x%08X, "
 	       "refc: %d, entry: %u, layout_gen %u\n",
@@ -493,12 +506,25 @@ void dump_lsm(unsigned int level, const struct lov_stripe_md *lsm)
 	for (i = 0; i < lsm->lsm_entry_count; i++) {
 		struct lov_stripe_md_entry *lse = lsm->lsm_entries[i];
 
-		CDEBUG(level,
-		       DEXT ": id: %u, magic 0x%08X, stripe count %u, "
-		       "size %u, layout_gen %u, pool: ["LOV_POOLNAMEF"]\n",
-		       PEXT(&lse->lsme_extent), lse->lsme_id, lse->lsme_magic,
+		CDEBUG(level, DEXT ": id: %u, flags: %x, "
+		       "magic 0x%08X, layout_gen %u, "
+		       "stripe count %u, sstripe size %u, "
+		       "pool: ["LOV_POOLNAMEF"]\n",
+		       PEXT(&lse->lsme_extent), lse->lsme_id, lse->lsme_flags,
+		       lse->lsme_magic, lse->lsme_layout_gen,
 		       lse->lsme_stripe_count, lse->lsme_stripe_size,
-		       lse->lsme_layout_gen, lse->lsme_pool_name);
+		       lse->lsme_pool_name);
+		if (!lsme_inited(lse) ||
+		    lse->lsme_pattern & LOV_PATTERN_F_RELEASED)
+			break;
+		for (j = 0; j < lse->lsme_stripe_count; j++) {
+			CDEBUG(level, "   oinfo:%p: ostid: "DOSTID
+			       " ost idx: %d gen: %d\n",
+			       lse->lsme_oinfo[j],
+			       POSTID(&lse->lsme_oinfo[j]->loi_oi),
+			       lse->lsme_oinfo[j]->loi_ost_idx,
+			       lse->lsme_oinfo[j]->loi_ost_gen);
+		}
 	}
 }
 

commit 6e5a0aa62da79d8b0d0eb13cd4bad15733e5ee8e

LU-16518 lov: remove unused lsm inline helpers
... since they are no longer used. copy_lsm_entry() has been
unused ever since it was first introduced. lsm_has_objects()
lost its last caller a long time ago.

Fixes: 7107e54983b0 ("LU-9008 pfl: dynamic layout modification with write/truncate")
Fixes: fcd45488711a ("LU-5683 clio: add CIT_DATA_VERSION")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Id77dc420757b9196968e7549ff32713ec0c7df0b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67274
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/lov/lov_internal.h
@@ -78,16 +78,6 @@ static inline bool lsme_is_dom(struct lov_stripe_md_entry *lsme)
 	return (lov_pattern(lsme->lsme_pattern) & LOV_PATTERN_MDT);
 }
 
-static inline void copy_lsm_entry(struct lov_stripe_md_entry *dst,
-				  struct lov_stripe_md_entry *src)
-{
-	unsigned int i;
-
-	for (i = 0; i < src->lsme_stripe_count; i++)
-		*dst->lsme_oinfo[i] = *src->lsme_oinfo[i];
-	memcpy(dst, src, offsetof(typeof(*src), lsme_oinfo));
-}
-
 struct lov_stripe_md {
 	struct kref	lsm_refc;
 	spinlock_t	lsm_lock;
@@ -191,11 +181,6 @@ static inline size_t lov_comp_md_size(const struct lov_stripe_md *lsm)
 	}
 
 	return size;
-}
-
-static inline bool lsm_has_objects(struct lov_stripe_md *lsm)
-{
-	return lsm != NULL && !lsm->lsm_is_released;
 }
 
 static inline unsigned int lov_comp_index(int entry, int stripe)