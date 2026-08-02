lustre/lustre-release

commit 14171e787dd00902af7c9edf10a6b54aab45f7b2

LU-10258 lfs: lfs mirror write command
Rename "lfs mirror dump" command to "lfs mirror read".

Add "lfs mirror write" command to write a mirror's content of a
mirrored file.

Usage:

lfs mirror write {--mirror-id|-N <mirror_id>}
	[--inputfile|-i <input_file>] <mirrored_file>

Options:

--mirror-id|-N <mirror_id>
  This  option  indicates  the  content of which mirror specified by
  mirror_id needs to be written. The mirror_id is the numerical unique
  identifier for a mirror.

--inputfile|-i <input_file>
  The path name of the input file, if not specified, the standard
  input stream will be used. The input stream or input_file cannot
  be the same mirrored file as the mirrored_file.

This command will issue a RESYNC lease write lock to notify MDS to
prepare destination mirror for the write (instantiate components of
the mirror), then client copy data from a file or STDIN to the
specified mirror of the mirrored file. After the data copy, a
RESYNC_DONE lease unlock is issued to MDS to update the layout
of the mirrored file.

Signed-off-by: Bobi Jam <bobijam@whamcloud.com>
Change-Id: I02022349d4ce871319903a8714ffc4534186c0e4
Reviewed-on: https://review.whamcloud.com/33219
Tested-by: Jenkins
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/file.c
@@ -1137,10 +1137,11 @@ static int ll_lease_close(struct obd_client_handle *och, struct inode *inode,
  * After lease is taken, send the RPC MDS_REINT_RESYNC to the MDT
  */
 static int ll_lease_file_resync(struct obd_client_handle *och,
-				struct inode *inode)
+				struct inode *inode, unsigned long arg)
 {
 	struct ll_sb_info *sbi = ll_i2sbi(inode);
 	struct md_op_data *op_data;
+	struct ll_ioc_lease_id ioc;
 	__u64 data_version_unused;
 	int rc;
 	ENTRY;
@@ -1150,6 +1151,10 @@ static int ll_lease_file_resync(struct obd_client_handle *och,
 	if (IS_ERR(op_data))
 		RETURN(PTR_ERR(op_data));
 
+	if (copy_from_user(&ioc, (struct ll_ioc_lease_id __user *)arg,
+			   sizeof(ioc)))
+		RETURN(-EFAULT);
+
 	/* before starting file resync, it's necessary to clean up page cache
 	 * in client memory, otherwise once the layout version is increased,
 	 * writing back cached data will be denied the OSTs. */
@@ -1158,6 +1163,7 @@ static int ll_lease_file_resync(struct obd_client_handle *och,
 		GOTO(out, rc);
 
 	op_data->op_lease_handle = och->och_lease_handle;
+	op_data->op_mirror_id = ioc.lil_mirror_id;
 	rc = md_file_resync(sbi->ll_md_exp, op_data);
 	if (rc)
 		GOTO(out, rc);
@@ -3274,7 +3280,7 @@ static long ll_file_set_lease(struct file *file, struct ll_ioc_lease *ioc,
 		RETURN(PTR_ERR(och));
 
 	if (ioc->lil_flags & LL_LEASE_RESYNC) {
-		rc = ll_lease_file_resync(och, inode);
+		rc = ll_lease_file_resync(och, inode, arg);
 		if (rc) {
 			ll_lease_close(och, inode, NULL);
 			RETURN(rc);

lustre/doc/lfs-mirror-read.1
@@ -1,13 +1,13 @@
-.TH LFS-MIRROR-DUMP 1 2018-08-16 "Lustre" "Lustre Utilities"
+.TH LFS-MIRROR-READ 1 2018-08-16 "Lustre" "Lustre Utilities"
 .SH NAME
-lfs mirror dump \- dump a mirror's content of a mirrored file
+lfs mirror read \- read a mirror's content of a mirrored file
 .SH SYNOPSIS
-.B lfs mirror dump
-<\fB\-\-mirror-id|\-N\fR <\fImirror_id\fR>>
+.B lfs mirror read
+{\fB\-\-mirror-id|\-N\fR <\fImirror_id\fR>}
 [\fB\-\-outfile|\-o\fR <\fIoutput_file\fR>]
 <\fImirrored_file\fR>
 .SH DESCRIPTION
-This command dumps the content of a file's mirror, the file is specified by the
+This command reads the content of a file's mirror, the file is specified by the
 path name \fImirrored_file\fR, the mirror is specified by its mirror ID
 \fImirror_id\fR.
 .br
@@ -17,7 +17,7 @@ otherwise the standard output stream will be used.
 .TP
 .BR \-\-mirror-id|\-N\fR\ <\fImirror_id\fR>
 This option indicates the content of which mirror specified by \fImirror_id\fR
-needs to be dumped. The \fImirror_id\fR is the numerical unique identifier for
+needs to be read. The \fImirror_id\fR is the numerical unique identifier for
 a mirror.
 .TP
 .BR \-\-outfile|\-o\fR\ <\fIoutput_file\fR>
@@ -25,14 +25,14 @@ The path name of the output file, if not specified, the standard output stream
 will be used.
 .SH EXAMPLES
 .TP
-.B lfs mirror dump --mirror-id 3 /mnt/lustre/file1
+.B lfs mirror read --mirror-id 3 /mnt/lustre/file1
 Dump the content of mirror with mirror ID 3 for /mnt/lustre/file1 to STDOUT.
 .TP
-.B lfs mirror dump -N 2 -o /tmp/m2 /mnt/lustre/file1
+.B lfs mirror read -N 2 -o /tmp/m2 /mnt/lustre/file1
 Dump the content of mirror with mirror ID 2 for /mnt/lustre/file1 to file
 /tmp/m2.
 .SH AUTHOR
-The \fBlfs mirror dump\fR command is part of the Lustre filesystem.
+The \fBlfs mirror read\fR command is part of the Lustre filesystem.
 .SH SEE ALSO
 .BR lfs (1),
 .BR lfs-setstripe (1),
@@ -41,3 +41,4 @@ The \fBlfs mirror dump\fR command is part of the Lustre filesystem.
 .BR lfs-mirror-extend (1),
 .BR lfs-mirror-split (1),
 .BR lfs-mirror-verify (1)
+.BR lfs-mirror-write (1)

lustre/doc/lfs-mirror-write.1
@@ -1,0 +1,43 @@
+.TH LFS-MIRROR-WRITE 1 2018-09-23 "Lustre" "Lustre Utilities"
+.SH NAME
+lfs mirror write \- write a mirror's content of a mirrored file
+.SH SYNOPSIS
+.B lfs mirror write
+{\fB\-\-mirror-id|\-N\fR <\fImirror_id\fR>}
+[\fB\-\-inputfile|\-i\fR <\fIinput_file\fR>]
+<\fImirrored_file\fR>
+.SH DESCRIPTION
+This command writes the content of a file's mirror, the file is specified by the
+path name \fImirrored_file\fR, the mirror is specified by its mirror ID
+\fImirror_id\fR.
+.SH OPTIONS
+.TP
+.BR \-\-mirror-id|\-N\fR\ <\fImirror_id\fR>
+This option indicates the content of which mirror specified by \fImirror_id\fR
+needs to be written. The \fImirror_id\fR is the numerical unique identifier for
+a mirror.
+.TP
+.BR \-\-inputfile|\-i\fR\ <\fIinput_file\fR>
+The path name of the input file, if not specified, the standard input stream
+will be used. The input stream or input_file cannot be the same mirrored file
+as the \fImirrored_file\fR.
+.SH EXAMPLES
+.TP
+.B lfs mirror write --mirror-id 3 /mnt/lustre/file1 < /tmp/m2
+Write the content of /mnt/m2 to the mirror with miro ID 3 for
+/mnt/lustre/file1.
+.TP
+.B lfs mirror write -N2 -i /tmp/m2 /mnt/lustre/file1
+Write the content of /mnt/m2 to the mirror with mirror ID 2 for
+/mnt/lustre/file1.
+.SH AUTHOR
+The \fBlfs mirror write\fR command is part of the Lustre filesystem.
+.SH SEE ALSO
+.BR lfs (1),
+.BR lfs-setstripe (1),
+.BR lfs-getstripe (1),
+.BR lfs-mirror-create (1),
+.BR lfs-mirror-extend (1),
+.BR lfs-mirror-split (1),
+.BR lfs-mirror-verify (1),
+.BR lfs-mirror-read (1)

lustre/include/md_object.h
@@ -181,6 +181,7 @@ enum md_layout_opc {
  */
 struct md_layout_change {
 	enum md_layout_opc	 mlc_opc;
+	__u16			 mlc_mirror_id;
 	struct layout_intent	*mlc_intent;
 	struct lu_buf		 mlc_buf;
 	struct lustre_som_attrs	 mlc_som;

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -2101,7 +2101,8 @@ struct mdt_rec_resync {
 	__u32           rs_padding6;	/* rr_flags */
 	__u32           rs_padding7;	/* rr_flags_h */
 	__u32           rs_padding8;	/* rr_umask */
-	__u32           rs_padding9;	/* rr_padding_4 */
+	__u16           rs_mirror_id;
+	__u16           rs_padding9;	/* rr_padding_4 */
 };
 
 /*
@@ -2135,7 +2136,8 @@ struct mdt_rec_reint {
 	__u32           rr_flags;
 	__u32           rr_flags_h;
 	__u32           rr_umask;
-	__u32           rr_padding_4; /* also fix lustre_swab_mdt_rec_reint */
+	__u16		rr_mirror_id;
+	__u16           rr_padding_4; /* also fix lustre_swab_mdt_rec_reint */
 };
 
 /* lmv structures */

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -361,6 +361,16 @@ struct ll_ioc_lease {
 	__u32		lil_ids[0];
 };
 
+struct ll_ioc_lease_id {
+	__u32		lil_mode;
+	__u32		lil_flags;
+	__u32		lil_count;
+	__u16		lil_mirror_id;
+	__u16		lil_padding1;
+	__u64		lil_padding2;
+	__u32		lil_ids[0];
+};
+
 /*
  * The ioctl naming rules:
  * LL_*     - works on the currently opened filehandle instead of parent dir

lustre/lod/lod_object.c
@@ -6136,6 +6136,30 @@ static int lod_primary_pick(const struct lu_env *env, struct lod_object *lo,
 	RETURN(picked);
 }
 
+static int lod_prepare_resync_mirror(const struct lu_env *env,
+				     struct lod_object *lo,
+				     __u16 mirror_id)
+{
+	struct lod_thread_info *info = lod_env_info(env);
+	struct lod_layout_component *lod_comp;
+	int i;
+
+	for (i = 0; i < lo->ldo_mirror_count; i++) {
+		if (lo->ldo_mirrors[i].lme_id != mirror_id)
+			continue;
+
+		lod_foreach_mirror_comp(lod_comp, lo, i) {
+			if (lod_comp_inited(lod_comp))
+				continue;
+
+			info->lti_comp_idx[info->lti_count++] =
+				lod_comp_index(lo, lod_comp);
+		}
+	}
+
+	return 0;
+}
+
 /**
  * figure out the components should be instantiated for resync.
  */
@@ -6251,23 +6275,33 @@ static int lod_declare_update_rdonly(const struct lu_env *env,
 		 * prep uninited all components assuming any non-stale mirror
 		 * could be picked as the primary mirror.
 		 */
-		for (i = 0; i < lo->ldo_mirror_count; i++) {
-			if (lo->ldo_mirrors[i].lme_stale)
-				continue;
+		if (mlc->mlc_mirror_id == 0) {
+			/* normal resync */
+			for (i = 0; i < lo->ldo_mirror_count; i++) {
+				if (lo->ldo_mirrors[i].lme_stale)
+					continue;
 
-			lod_foreach_mirror_comp(lod_comp, lo, i) {
-				if (!lod_comp_inited(lod_comp))
-					break;
+				lod_foreach_mirror_comp(lod_comp, lo, i) {
+					if (!lod_comp_inited(lod_comp))
+						break;
 
-				if (extent.e_end < lod_comp->llc_extent.e_end)
-					extent.e_end =
-						lod_comp->llc_extent.e_end;
+					if (extent.e_end <
+						lod_comp->llc_extent.e_end)
+						extent.e_end =
+						     lod_comp->llc_extent.e_end;
+				}
 			}
+			rc = lod_prepare_resync(env, lo, &extent);
+			if (rc)
+				GOTO(out, rc);
+		} else {
+			/* mirror write, try to init its all components */
+			rc = lod_prepare_resync_mirror(env, lo,
+						       mlc->mlc_mirror_id);
+			if (rc)
+				GOTO(out, rc);
 		}
 
-		rc = lod_prepare_resync(env, lo, &extent);
-		if (rc)
-			GOTO(out, rc);
 		/* change the file state to SYNC_PENDING */
 		lo->ldo_flr_state = LCM_FL_SYNC_PENDING;
 	}
@@ -6391,16 +6425,26 @@ static int lod_declare_update_write_pending(const struct lu_env *env,
 						lod_comp_index(lo, lod_comp);
 		}
 	} else { /* MD_LAYOUT_RESYNC */
-		lod_foreach_mirror_comp(lod_comp, lo, primary) {
-			if (!lod_comp_inited(lod_comp))
-				break;
+		if (mlc->mlc_mirror_id == 0) {
+			/* normal resync */
+			lod_foreach_mirror_comp(lod_comp, lo, primary) {
+				if (!lod_comp_inited(lod_comp))
+					break;
 
-			extent.e_end = lod_comp->llc_extent.e_end;
+				extent.e_end = lod_comp->llc_extent.e_end;
+			}
+
+			rc = lod_prepare_resync(env, lo, &extent);
+			if (rc)
+				GOTO(out, rc);
+		} else {
+			/* mirror write, try to init its all components */
+			rc = lod_prepare_resync_mirror(env, lo,
+						       mlc->mlc_mirror_id);
+			if (rc)
+				GOTO(out, rc);
 		}
 
-		rc = lod_prepare_resync(env, lo, &extent);
-		if (rc)
-			GOTO(out, rc);
 		/* change the file state to SYNC_PENDING */
 		lo->ldo_flr_state = LCM_FL_SYNC_PENDING;
 	}

lustre/mdc/mdc_reint.c
@@ -462,6 +462,7 @@ int mdc_file_resync(struct obd_export *exp, struct md_op_data *op_data)
 	rec->rs_cap	= op_data->op_cap;
 	rec->rs_fid	= op_data->op_fid1;
 	rec->rs_bias	= op_data->op_bias;
+	rec->rs_mirror_id = op_data->op_mirror_id;
 
 	lock = ldlm_handle2lock(&op_data->op_lease_handle);
 	if (lock != NULL) {

commit 1a3be2ccad82b8987f1702f8a2a96afa7824b3ed

LU-19524 llite: correctly free mem if copy_from_user fails
memory allocated from ll_prep_md_op_data call is left dangling
if copy_from_user() fails. This patch fixes this by jumping to
label out where it is correctly reclaimed vs just returning.

Test-Parameters: trivial
Fixes: 14171e787dd0 ("LU-10258 lfs: lfs mirror write command")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I7314c36a8678ed1cf74b5adf8b60433f8b8efb43
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62028
Reviewed-by: Qian Yingjin <qian@ddn.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/llite/file.c
@@ -1689,7 +1689,7 @@ static int ll_lease_file_resync(struct obd_client_handle *och,
 		RETURN(PTR_ERR(op_data));
 
 	if (copy_from_user(&ioc, uarg, sizeof(ioc)))
-		RETURN(-EFAULT);
+		GOTO(out, rc = -EFAULT);
 
 	/* before starting file resync, it's necessary to clean up page cache
 	 * in client memory, otherwise once the layout version is increased,