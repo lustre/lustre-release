lustre/lustre-release

commit 4a14a51a8e000e05e6a3c0dae7bf64b8d43f8254

LU-18448 llite: read dir on open
Let's read some pages at directory start,
a clients needs it probably.

walk over ~100k directories with 150 files on last leaf.

readdir on open enabled.

    real    0m39.977s
    user    0m0.121s
    sys     0m7.161s

readdir on open disabled

    real    1m18.106s
    user    0m0.151s
    sys     0m15.666s

HPE-bug-id: LUS-7695
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: Iaa674ce0d2e5723b380d7ca09407b27a90bc37f5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57069
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>

lustre/mdt/mdt_handler.c
@@ -3051,6 +3051,142 @@ static void mdt_preset_secctx_size(struct mdt_thread_info *info)
 	}
 }
 
+int mdt_object_striped(struct mdt_thread_info *mti, struct mdt_object *obj)
+{
+	struct lu_device *bottom_dev;
+	struct lu_object *bottom_obj;
+	int rc;
+
+	if (!S_ISDIR(obj->mot_header.loh_attr))
+		return 0;
+
+	/* getxattr from bottom obj to avoid reading in shard FIDs */
+	bottom_dev = dt2lu_dev(mti->mti_mdt->mdt_bottom);
+	bottom_obj = lu_object_find_slice(mti->mti_env, bottom_dev,
+					  mdt_object_fid(obj), NULL);
+	if (IS_ERR(bottom_obj))
+		return PTR_ERR(bottom_obj);
+
+	rc = dt_xattr_get(mti->mti_env, lu2dt(bottom_obj), &LU_BUF_NULL,
+			  XATTR_NAME_LMV);
+	lu_object_put(mti->mti_env, bottom_obj);
+
+	return (rc > 0) ? 1 : (rc == -ENODATA) ? 0 : rc;
+}
+
+#define DIR_READ_ON_OPEN_PAGES 1
+
+static int mdt_dir_read_on_open(struct mdt_thread_info	*info,
+				struct lustre_handle *lhc)
+{
+	const struct lu_env *env = info->mti_env;
+	struct lu_rdpg		*rdpg = &info->mti_u.rdpg.mti_rdpg;
+	struct req_capsule	*pill = info->mti_pill;
+	int			 rc;
+	struct mdt_body         *mbo;
+	struct mdt_device	*mdt = info->mti_mdt;
+	struct mdt_object	*o;
+	struct ptlrpc_request	*req = pill->rc_req;
+	bool have_lock = false;
+	struct lu_fid *fid; // dir fid
+
+	ENTRY;
+
+	if (CFS_FAIL_CHECK(OBD_FAIL_MDS_READPAGE_PACK))
+		GOTO(out_err, rc = -ENOMEM);
+
+	/* client don't want a reply */
+	if (!req->rq_reqmsg->lm_repsize)
+		RETURN(0);
+
+	if (lustre_handle_is_used(lhc)) {
+		struct ldlm_lock *lock;
+
+		lock = ldlm_handle2lock(lhc);
+		if (lock) {
+			have_lock = ldlm_has_update(lock);
+			ldlm_lock_put(lock);
+		}
+	}
+	if (!have_lock)
+		GOTO(out_err, rc = 0);
+
+	rdpg->rp_hash = 0;
+	rdpg->rp_attrs = LUDA_FID | LUDA_TYPE;
+	if (exp_connect_flags(info->mti_exp) & OBD_CONNECT_64BITHASH)
+		rdpg->rp_attrs |= LUDA_64BITHASH;
+	rdpg->rp_count  = min_t(unsigned int, req->rq_reqmsg->lm_repsize,
+			    DIR_READ_ON_OPEN_PAGES << PAGE_SHIFT);
+	rdpg->rp_npages = 0;
+
+	rc = req_capsule_server_grow(pill, &RMF_NIOBUF_INLINE, rdpg->rp_count);
+	if (rc != 0) {
+		/* failed to grow data buffer, just exit */
+		GOTO(out_err, rc = -E2BIG);
+	}
+
+	/* re-take MDT_BODY and NIOBUF_INLINE buffers after the buffer grow */
+	mbo = req_capsule_server_get(pill, &RMF_MDT_BODY);
+	fid = &mbo->mbo_fid1;
+	if (!fid_is_sane(fid))
+		GOTO(out_rnb, rc = -EINVAL);
+
+	rdpg->rp_data = req_capsule_server_get(pill, &RMF_NIOBUF_INLINE);
+	if (rdpg->rp_data == NULL)
+		GOTO(out_rnb, rc = -EPROTO);
+
+	o = mdt_object_find(info->mti_env, mdt, fid);
+	if (IS_ERR(o))
+		GOTO(out_rnb, rc = PTR_ERR(o));
+
+	if (!mdt_object_exists(o) ||
+	     mdt_object_remote(o) ||
+	     mdt_object_striped(info, o))
+		GOTO(out_put, rc = -ENOENT);
+
+	/* call lower layers to fill allocated pages with directory data */
+	rc = mo_readpage(env, mdt_object_child(o), rdpg);
+out_put:
+	mdt_object_put(env, o);
+
+out_rnb:
+	if (rc < 0)
+		req_capsule_shrink(pill, &RMF_NIOBUF_INLINE, 0, RCL_SERVER);
+out_err:
+	if (rc)
+		CDEBUG(D_INFO, "read dir on open failed with rc = %d\n", rc);
+	RETURN(0);
+}
+
+static int mdt_read_inline(struct mdt_thread_info *info,
+			   struct mdt_lock_handle *lhc)
+{
+	struct req_capsule	*pill = info->mti_pill;
+	struct md_attr		*ma  = &info->mti_attr;
+	struct lu_attr		*la  = &ma->ma_attr;
+	struct ptlrpc_request	*req = pill->rc_req;
+	int rc = 0;
+
+	ENTRY;
+	if (!req_capsule_field_present(pill, &RMF_NIOBUF_INLINE, RCL_SERVER)) {
+		/* There is no reply buffers for this field, this means that
+		 * client has no support for data in reply.
+		 */
+		RETURN(0);
+	}
+	/* client don't want a reply */
+	if (!req->rq_reqmsg->lm_repsize)
+		RETURN(0);
+
+	if (S_ISREG(la->la_mode))
+		rc = mdt_dom_read_on_open(info, info->mti_mdt,
+					  &lhc->mlh_reg_lh);
+	else if (S_ISDIR(la->la_mode))
+		rc = mdt_dir_read_on_open(info, &lhc->mlh_reg_lh);
+
+	return rc;
+}
+
 static int mdt_reint_internal(struct mdt_thread_info *info,
 			      struct mdt_lock_handle *lhc,
 			      __u32 op)
@@ -3146,8 +3282,7 @@ out_shrink:
 	 * in reply when possible.
 	 */
 	if (rc == 0 && op == REINT_OPEN && !req_is_replay(pill->rc_req))
-		rc = mdt_dom_read_on_open(info, info->mti_mdt,
-					  &lhc->mlh_reg_lh);
+		rc = mdt_read_inline(info, lhc);
 
 	return rc;
 }

lustre/include/lu_object.h
@@ -814,8 +814,15 @@ struct lu_rdpg {
 	/** requested attr */
 	__u32                   rp_attrs;
 	/** pointers to pages */
-	struct page           **rp_pages;
+	union {
+		struct page	**rp_pages;
+		void		*rp_data;
+	};
 };
+
+/* for dt_index_walk / mdd_readpage */
+void *rdpg_page_get(const struct lu_rdpg *rdpg, unsigned int index);
+void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index);
 
 enum lu_xattr_flags {
 	LU_XATTR_REPLACE = BIT(0),

lustre/include/lustre_dlm.h
@@ -1172,6 +1172,12 @@ static inline bool ldlm_has_dom(struct ldlm_lock *lock)
 		!!(lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_DOM);
 }
 
+static inline bool ldlm_has_update(struct ldlm_lock *lock)
+{
+	return lock->l_resource->lr_type == LDLM_IBITS &&
+		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_UPDATE;
+}
+
 static inline char *
 ldlm_ns_name(struct ldlm_namespace *ns)
 {

lustre/include/lustre_export.h
@@ -519,6 +519,11 @@ static inline bool exp_connect_batch_rpc(struct obd_export *exp)
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_BATCH_RPC);
 }
 
+static inline int exp_connect_open_readdir(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_READDIR_OPEN);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_mdc.h
@@ -86,6 +86,15 @@ static inline void cl_lov_delay_create_clear(unsigned int *flags)
 		*flags &= ~O_LOV_DELAY_CREATE;
 }
 
+static inline unsigned long hash_x_index(__u64 hash, int hash64)
+{
+	if (BITS_PER_LONG == 32 && hash64)
+		hash >>= 32;
+	/* save hash 0 with hash 1 */
+	return ~0UL - (hash + !hash);
+}
+
+
 /** @} mdc */
 
 #endif

lustre/include/lustre_osc.h
@@ -377,7 +377,7 @@ enum osc_lock_state {
  *
  * - When reply is received from the server (osc_enqueue_interpret())
  *      - ldlm_cli_enqueue_fini()
- *          - LDLM_LOCK_PUT(): releases caller reference acquired by
+ *          - ldlm_lock_put(): releases caller reference acquired by
  *            ldlm_lock_new().
  *          - if (rc != 0)
  *                ldlm_lock_decref(): error case: matches ldlm_cli_enqueue().
@@ -385,7 +385,7 @@ enum osc_lock_state {
  *
  * - When lock is being cancelled (ldlm_lock_cancel())
  *      - ldlm_lock_destroy()
- *          - LDLM_LOCK_PUT(): releases hash-table reference acquired by
+ *          - ldlm_lock_put(): releases hash-table reference acquired by
  *            ldlm_lock_new().
  *
  * osc_lock is detached from ldlm_lock by osc_lock_detach() that is called

lustre/include/obd.h
@@ -915,6 +915,8 @@ enum md_cli_flags {
 	CLI_MIGRATE	= BIT(4),
 	CLI_DIRTY_DATA	= BIT(5),
 	CLI_NO_SLOT     = BIT(6),
+	/**< read on open (used for directory for now) */
+	CLI_READ_ON_OPEN = BIT(7),
 };
 
 enum md_op_code {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -848,6 +848,8 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_SPARSE            0x1000000000ULL /* sparse LNet read */
 #define OBD_CONNECT2_MIRROR_ID_FIX     0x2000000000ULL /* rr_mirror_id move */
 #define OBD_CONNECT2_UPDATE_LAYOUT     0x4000000000ULL /* update compressibility */
+#define OBD_CONNECT2_READDIR_OPEN	0x8000000000ULL /* read first dir page on open */
+
 /* XXX README XXX README XXX README XXX README XXX README XXX README XXX
  * Please DO NOT add OBD_CONNECT flags before first ensuring that this value
  * is not in use by some other branch/patch.  Email adilger@whamcloud.com

commit e2dcd15f75b01469b549308d6427967ee36f6ff1

LU-20484 mdt: fix false error log in mdt_dir_read_on_open
mo_readpage returns bytes filled on success, so the condition
"if (rc)" triggered the "failed" CDEBUG message on every successful
read. Change to "if (rc < 0)" to log only real errors.

Fixes: 4a14a51a8e ("LU-18448 llite: read dir on open")
Signed-off-by: Artem Blagodarenko <ablagodarenko@thelustrecollective.com>
Change-Id: I552bea9c4e995111ed5cca5613c7fb52c6c70da5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67332
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

lustre/mdt/mdt_handler.c
@@ -3170,7 +3170,7 @@ out_rnb:
 	else
 		req_capsule_shrink(pill, &RMF_NIOBUF_INLINE, rc, RCL_SERVER);
 out_err:
-	if (rc)
+	if (rc < 0)
 		CDEBUG(D_INFO, "read dir on open failed with rc = %d\n", rc);
 	RETURN(0);
 }