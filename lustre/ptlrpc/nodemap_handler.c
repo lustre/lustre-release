lustre/lustre-release

commit 6ffae244d72c67f797f3f100c251f9cf5cd91b85

LU-19028 nodemap: do not squash trusted ids
When a root access is made with the admin property set, or when a
regular user access is made with the trusted property set, id should
not be squashed, even if it matches the defined squash_id on the
nodemap.

Add sanity-sec test_84 to verify trusted ids are not squashed.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: Id0dca0d23c29919781535df40a30f8f0c33aff8c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61967
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -888,6 +888,7 @@ EXPORT_SYMBOL(nodemap_get_from_exp);
  * @id_type: NODEMAP_UID or NODEMAP_GID or NODEMAP_PROJID
  * @tree_type: NODEMAP_CLIENT_TO_FS or NODEMAP_FS_TO_CLIENT
  * @id: id to map
+ * @id_is_squashed: out param, true if id is squashed
  *
  * if the nodemap_active is false, just return the passed id without mapping
  *
@@ -907,9 +908,10 @@ EXPORT_SYMBOL(nodemap_get_from_exp);
  * Return:
  * * %mapped id according to the rules below.
  */
-__u32 nodemap_map_id(struct lu_nodemap *nodemap,
-		     enum nodemap_id_type id_type,
-		     enum nodemap_tree_type tree_type, __u32 id)
+static __u32 __nodemap_map_id(struct lu_nodemap *nodemap,
+			      enum nodemap_id_type id_type,
+			      enum nodemap_tree_type tree_type, __u32 id,
+			      bool *id_is_squashed)
 {
 	struct lu_idmap *idmap = NULL;
 	__u32 offset_start;
@@ -918,6 +920,9 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 	bool attempted_squash = false;
 
 	ENTRY;
+
+	if (id_is_squashed)
+		*id_is_squashed = false;
 
 	if (!nodemap_active)
 		GOTO(out, found_id);
@@ -998,6 +1003,8 @@ map:
 	GOTO(offset, found_id);
 
 squash:
+	if (id_is_squashed)
+		*id_is_squashed = true;
 	if (id_type == NODEMAP_UID)
 		found_id = nodemap->nm_squash_uid;
 	else if (id_type == NODEMAP_GID)
@@ -1032,6 +1039,13 @@ offset:
 	}
 out:
 	RETURN(found_id);
+}
+
+__u32 nodemap_map_id(struct lu_nodemap *nodemap,
+		     enum nodemap_id_type id_type,
+		     enum nodemap_tree_type tree_type, __u32 id)
+{
+	return __nodemap_map_id(nodemap, id_type, tree_type, id, NULL);
 }
 EXPORT_SYMBOL(nodemap_map_id);
 
@@ -1122,6 +1136,32 @@ int nodemap_map_suppgid(struct lu_nodemap *nodemap, int suppgid)
 EXPORT_SYMBOL(nodemap_map_suppgid);
 
 /**
+ * nodemap_id_is_squashed() - check if ID is squashed by nodemap
+ * @nodemap: nodemap
+ * @id: id to check
+ * @type: id type, NODEMAP_UID or NODEMAP_GID or NODEMAP_PROJID
+ * @tree_type: tree type, NODEMAP_CLIENT_TO_FS or NODEMAP_FS_TO_CLIENT
+ *
+ * Checks whether an ID is squashed in the provided nodemap.
+ *
+ * Return:
+ * * %true if ID is squashed
+ */
+bool nodemap_id_is_squashed(struct lu_nodemap *nodemap, __u32 id,
+			    enum nodemap_id_type type,
+			    enum nodemap_tree_type tree_type)
+{
+	bool id_is_squashed = false;
+	__u32 tempid;
+
+	tempid = __nodemap_map_id(nodemap, type, tree_type, id,
+				  &id_is_squashed);
+
+	return id_is_squashed;
+}
+EXPORT_SYMBOL(nodemap_id_is_squashed);
+
+/**
  * nodemap_check_resource_ids() - check if export can access a resource
  * @exp: export to check
  * @fs_uid: uid of the resource
@@ -1140,8 +1180,6 @@ int nodemap_check_resource_ids(struct obd_export *exp, __u32 fs_uid,
 			       __u32 fs_gid)
 {
 	struct lu_nodemap *nodemap;
-	__u32 client_uid, client_gid;
-	__u32 client_squashed_uid, client_squashed_gid;
 	int rc = 0;
 
 	ENTRY;
@@ -1150,19 +1188,10 @@ int nodemap_check_resource_ids(struct obd_export *exp, __u32 fs_uid,
 	if (IS_ERR_OR_NULL(nodemap))
 		RETURN(0);
 
-	client_uid = nodemap_map_id(nodemap, NODEMAP_UID, NODEMAP_FS_TO_CLIENT,
-				    fs_uid);
-	client_gid = nodemap_map_id(nodemap, NODEMAP_GID, NODEMAP_FS_TO_CLIENT,
-				    fs_gid);
-	client_squashed_uid = nodemap_map_id(nodemap, NODEMAP_UID,
-					     NODEMAP_FS_TO_CLIENT,
-					     nodemap->nm_squash_uid);
-	client_squashed_gid = nodemap_map_id(nodemap, NODEMAP_GID,
-					     NODEMAP_FS_TO_CLIENT,
-					     nodemap->nm_squash_gid);
-
-	if (client_uid == client_squashed_uid &&
-	    client_gid == client_squashed_gid) {
+	if (nodemap_id_is_squashed(nodemap, fs_uid, NODEMAP_UID,
+				   NODEMAP_FS_TO_CLIENT) &&
+	    nodemap_id_is_squashed(nodemap, fs_gid, NODEMAP_GID,
+				   NODEMAP_FS_TO_CLIENT)) {
 		CDEBUG(D_SEC,
 		       "Nodemap %s: access denied for export %s (at %s) fs_uid=%u fs_gid=%u\n",
 		       nodemap->nm_name, obd_uuid2str(&exp->exp_client_uuid),

lustre/include/lustre_nodemap.h
@@ -255,6 +255,9 @@ __u32 nodemap_map_id(struct lu_nodemap *nodemap,
 ssize_t nodemap_map_acl(struct lu_nodemap *nodemap, void *buf, size_t size,
 			enum nodemap_tree_type tree_type);
 int nodemap_map_suppgid(struct lu_nodemap *nodemap, int suppgid);
+bool nodemap_id_is_squashed(struct lu_nodemap *nodemap, __u32 id,
+			    enum nodemap_id_type type,
+			    enum nodemap_tree_type tree_type);
 int nodemap_check_resource_ids(struct obd_export *exp, __u32 fs_uid,
 			       __u32 fs_gid);
 #ifdef HAVE_SERVER_SUPPORT

lustre/mdt/mdt_lib.c
@@ -214,6 +214,10 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 	LASSERT(ucred != NULL);
 
 	ucred->uc_valid = UCRED_INVALID;
+	ucred->uc_o_uid = pud->pud_uid;
+	ucred->uc_o_gid = pud->pud_gid;
+	ucred->uc_o_fsuid = pud->pud_fsuid;
+	ucred->uc_o_fsgid = pud->pud_fsgid;
 
 	nodemap = nodemap_get_from_exp(info->mti_exp);
 	if (IS_ERR(nodemap))
@@ -231,14 +235,8 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 		pud->pud_groups[i] = nodemap_map_suppgid(nodemap,
 							 pud->pud_groups[i]);
 
-	ucred->uc_o_uid = pud->pud_uid;
-	ucred->uc_o_gid = pud->pud_gid;
-	ucred->uc_o_fsuid = pud->pud_fsuid;
-	ucred->uc_o_fsgid = pud->pud_fsgid;
-
 	ucred->uc_uid = pud->pud_uid;
 	ucred->uc_gid = pud->pud_gid;
-
 	ucred->uc_fsuid = pud->pud_fsuid;
 	ucred->uc_fsgid = pud->pud_fsgid;
 
@@ -264,11 +262,9 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 		GOTO(out_nodemap, rc = -EACCES);
 	}
 
-	if (nodemap &&
-	    ucred->uc_o_uid == nodemap_map_id(nodemap, NODEMAP_UID,
-					      NODEMAP_CLIENT_TO_FS,
-					      nodemap->nm_squash_uid) &&
-	    nodemap->nmf_deny_unknown)
+	if (nodemap && nodemap->nmf_deny_unknown &&
+	    nodemap_id_is_squashed(nodemap, ucred->uc_o_uid, NODEMAP_UID,
+				   NODEMAP_CLIENT_TO_FS))
 		/* deny access before we get identity ref */
 		GOTO(out, rc = -EACCES);
 
@@ -343,10 +339,10 @@ static int new_init_ucred(struct mdt_thread_info *info, ucred_init_type_t type,
 	}
 
 	/* clear suppgids if uid or gid was squashed. */
-	if (nodemap &&
-	    (ucred->uc_o_uid == nodemap->nm_squash_uid ||
-	     ucred->uc_o_gid == nodemap->nm_squash_gid)) {
-
+	if (nodemap_id_is_squashed(nodemap, ucred->uc_o_uid, NODEMAP_UID,
+				   NODEMAP_CLIENT_TO_FS) ||
+	    nodemap_id_is_squashed(nodemap, ucred->uc_o_gid, NODEMAP_GID,
+				   NODEMAP_CLIENT_TO_FS)) {
 		ucred->uc_cap = CAP_EMPTY_SET;
 		ucred->uc_suppgids[0] = -1;
 		ucred->uc_suppgids[1] = -1;
@@ -547,11 +543,9 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 	struct mdt_device *mdt = info->mti_mdt;
 	struct md_identity *identity = NULL;
 
-	if (nodemap &&
-	    uc->uc_o_uid == nodemap_map_id(nodemap, NODEMAP_UID,
-					   NODEMAP_CLIENT_TO_FS,
-					   nodemap->nm_squash_uid) &&
-	    nodemap->nmf_deny_unknown)
+	if (nodemap && nodemap->nmf_deny_unknown &&
+	    nodemap_id_is_squashed(nodemap, uc->uc_o_uid, NODEMAP_UID,
+				   NODEMAP_CLIENT_TO_FS))
 		/* deny access before we get identity ref */
 		RETURN(-EACCES);
 
@@ -574,10 +568,10 @@ static int old_init_ucred_common(struct mdt_thread_info *info,
 	}
 	uc->uc_identity = identity;
 
-	if (nodemap &&
-	    (uc->uc_o_uid == nodemap->nm_squash_uid ||
-	     uc->uc_o_gid == nodemap->nm_squash_gid)) {
-
+	if (nodemap_id_is_squashed(nodemap, uc->uc_o_uid, NODEMAP_UID,
+				   NODEMAP_CLIENT_TO_FS) ||
+	    nodemap_id_is_squashed(nodemap, uc->uc_o_gid, NODEMAP_GID,
+				   NODEMAP_CLIENT_TO_FS)) {
 		uc->uc_cap = CAP_EMPTY_SET;
 		uc->uc_suppgids[0] = -1;
 		uc->uc_suppgids[1] = -1;
@@ -649,6 +643,12 @@ static int old_init_ucred(struct mdt_thread_info *info,
 	int rc;
 
 	ENTRY;
+
+	uc->uc_o_uid = body->mbo_uid;
+	uc->uc_o_gid = body->mbo_gid;
+	uc->uc_o_fsuid = body->mbo_fsuid;
+	uc->uc_o_fsgid = body->mbo_fsgid;
+
 	nodemap = nodemap_get_from_exp(info->mti_exp);
 	if (IS_ERR(nodemap))
 		RETURN(PTR_ERR(nodemap));
@@ -664,10 +664,10 @@ static int old_init_ucred(struct mdt_thread_info *info,
 
 	LASSERT(uc != NULL);
 	uc->uc_valid = UCRED_INVALID;
-	uc->uc_o_uid = uc->uc_uid = body->mbo_uid;
-	uc->uc_o_gid = uc->uc_gid = body->mbo_gid;
-	uc->uc_o_fsuid = uc->uc_fsuid = body->mbo_fsuid;
-	uc->uc_o_fsgid = uc->uc_fsgid = body->mbo_fsgid;
+	uc->uc_uid = body->mbo_uid;
+	uc->uc_gid = body->mbo_gid;
+	uc->uc_fsuid = body->mbo_fsuid;
+	uc->uc_fsgid = body->mbo_fsgid;
 	uc->uc_suppgids[0] = nodemap_map_suppgid(nodemap, body->mbo_suppgid);
 	uc->uc_suppgids[1] = -1;
 	uc->uc_ginfo = NULL;
@@ -687,6 +687,10 @@ static int old_init_ucred_reint(struct mdt_thread_info *info)
 	int rc;
 
 	ENTRY;
+
+	uc->uc_o_uid = uc->uc_o_fsuid = uc->uc_fsuid;
+	uc->uc_o_gid = uc->uc_o_fsgid = uc->uc_fsgid;
+
 	nodemap = nodemap_get_from_exp(info->mti_exp);
 	if (IS_ERR(nodemap))
 		RETURN(PTR_ERR(nodemap));
@@ -699,8 +703,8 @@ static int old_init_ucred_reint(struct mdt_thread_info *info)
 				      NODEMAP_CLIENT_TO_FS, uc->uc_fsgid);
 
 	uc->uc_valid = UCRED_INVALID;
-	uc->uc_o_uid = uc->uc_o_fsuid = uc->uc_uid = uc->uc_fsuid;
-	uc->uc_o_gid = uc->uc_o_fsgid = uc->uc_gid = uc->uc_fsgid;
+	uc->uc_uid = uc->uc_fsuid;
+	uc->uc_gid = uc->uc_fsgid;
 	uc->uc_ginfo = NULL;
 
 	rc = old_init_ucred_common(info, nodemap);

lustre/tests/sanity-sec.sh
@@ -10897,6 +10897,44 @@ test_83() {
 }
 run_test 83 "Suppgids for cross-MDT ops"
 
+test_84() {
+	local nm=c0
+
+	(( MDS1_VERSION > $(version_code 2.16.61) )) ||
+		skip "Need MDS version at least 2.16.61 for correct squashing"
+
+	mkdir -p $DIR/$tdir/$USER0
+	touch $DIR/$tdir/$USER0/fileA
+	chown -R $ID0:$ID0 $DIR/$tdir
+	chown $ID0:$ID1 $DIR/$tdir/$USER0/fileA
+
+	stack_trap cleanup_local_client_nodemap EXIT
+	setup_local_client_nodemap $nm 1 1
+	do_facet mgs $LCTL nodemap_modify --name $nm \
+		--property squash_uid=$ID0 ||
+			error "cannot set squash_uid=$ID0 on $nm"
+	do_facet mgs $LCTL nodemap_modify --name $nm \
+		--property squash_gid=$ID0 ||
+			error "cannot set squash_gid=$ID0 on $nm"
+	do_facet mgs $LCTL nodemap_modify --name $nm \
+		--property squash_projid=$ID0 ||
+			error "cannot set squash_projid=$ID0 on $nm"
+	do_facet mgs $LCTL nodemap_modify --name $nm \
+		--property deny_unknown=1 ||
+			error "cannot set deny_unknown=1 on $nm"
+	wait_nm_sync $nm deny_unknown
+
+	$RUNAS_CMD -u $ID0 touch $DIR/$tdir/file01 ||
+		error "touch $DIR/$tdir/file01 failed"
+	$RUNAS_CMD -u $ID0 mkdir $DIR/$tdir/dir01 ||
+		error "mkdir $DIR/$tdir/dir01 failed"
+	$RUNAS_CMD -u $ID0 ls -l $DIR/$tdir/$USER0 ||
+		error "ls -l $DIR/$tdir/$USER0 failed"
+	$RUNAS_CMD -u $ID0 chown $ID0:$ID0 $DIR/$tdir/$USER0/fileA ||
+		error "chown $ID0:$ID0 $DIR/$tdir/$USER0/fileA failed"
+}
+run_test 84 "do not squash trusted ids"
+
 cleanup_100() {
 	local orig_sk_path="$1"
 	local test_key="$orig_sk_path/$FSNAME-test100.key"

commit 706ab81149cb786e70a984d6e015be7d48d91d2c

LU-16518 nodemap: fix Clang compiler warnings
Drop the unused 'tempid' variable in nodemap_id_is_squashed(),
and fix a bogus '(void *)' cast that was meant to be a '(void)'
cast discarding the cfs_hash_del_key() return value.

Fixes: 6ffae244d72c ("LU-19028 nodemap: do not squash trusted ids")
Fixes: 14ae3eb69ce7 ("LU-19079 nodemap: compute sha256 of nodemap name")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: Ibd2649b58787f19057adf6d44f5082f8af47ff3e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67351
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/ptlrpc/nodemap_handler.c
@@ -1395,10 +1395,8 @@ bool nodemap_id_is_squashed(struct lu_nodemap *nodemap, __u32 id,
 			    enum nodemap_tree_type tree_type)
 {
 	bool id_is_squashed = false;
-	__u32 tempid;
 
-	tempid = __nodemap_map_id(nodemap, type, tree_type, id,
-				  &id_is_squashed);
+	__nodemap_map_id(nodemap, type, tree_type, id, &id_is_squashed);
 
 	return id_is_squashed;
 }
@@ -3820,7 +3818,7 @@ struct lu_nodemap *nodemap_create(const char *name,
 out_list_hash:
 	if (!list_empty(&nodemap->nm_parent_entry))
 		list_del(&nodemap->nm_parent_entry);
-	(void *)cfs_hash_del_key(hash, newname);
+	(void)cfs_hash_del_key(hash, newname);
 out:
 	OBD_FREE_PTR(nodemap);
 	if (!IS_ERR_OR_NULL(parent_nodemap))