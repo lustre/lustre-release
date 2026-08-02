lustre/lustre-release

commit 7fb531928daef1e7363e5102bd426a4001293eca

LU-15135 lnet: Add defines for state of routing
Add defines for state of routing. Update sanity/215 to handle the new
states.

HPE-bug-id: LUS-12556
Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I9c5260511a5a69bdcb858238d7801789b4a5fe9b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57489
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/lnet_debugfs.c
@@ -191,6 +191,19 @@ out_no_ctrs:
 	return rc;
 }
 
+static char *
+ln_routing2str(void)
+{
+	switch (the_lnet.ln_routing) {
+	case LNET_ROUTING_DISABLED:
+		return "Routing disabled\n";
+	case LNET_ROUTING_ENABLED:
+		return "Routing enabled\n";
+	default:
+		return "Routing unknown\n";
+	}
+}
+
 static int
 proc_lnet_routes(const struct ctl_table *table, int write,
 		 void __user *buffer, size_t *lenp, loff_t *ppos)
@@ -220,8 +233,8 @@ proc_lnet_routes(const struct ctl_table *table, int write,
 	s = tmpstr; /* points to current position in tmpstr[] */
 
 	if (*ppos == 0) {
-		s += scnprintf(s, tmpstr + tmpsiz - s, "Routing %s\n",
-			       the_lnet.ln_routing ? "enabled" : "disabled");
+		s += scnprintf(s, tmpstr + tmpsiz - s, ln_routing2str());
+
 		LASSERT(tmpstr + tmpsiz - s > 0);
 
 		s += scnprintf(s, tmpstr + tmpsiz - s, "%-8s %4s %8s %7s %s\n",
@@ -738,7 +751,7 @@ proc_lnet_nis(const struct ctl_table *table, int write,
 			int i;
 			int j;
 
-			if (the_lnet.ln_routing)
+			if (lnet_routing_enabled())
 				last_alive = now - ni->ni_net->net_last_alive;
 
 			stat = (lnet_ni_get_status(ni) ==

lnet/include/lnet/lib-lnet.h
@@ -240,7 +240,7 @@ __must_hold(&ni->ni_lock)
 		return LNET_NI_STATUS_UP;
 	else if (atomic_read(&ni->ni_fatal_error_on))
 		return LNET_NI_STATUS_DOWN;
-	else if (the_lnet.ln_routing && ni->ni_status)
+	else if (lnet_routing_enabled() && ni->ni_status)
 		return *ni->ni_status;
 	else
 		return LNET_NI_STATUS_UP;

lnet/include/lnet/lib-types.h
@@ -1687,9 +1687,10 @@ struct lnet_peer_table {
 /* peer aliveness is enabled only on routers for peers in a network where the
  * struct lnet_ni::ni_peertimeout has been set to a positive value
  */
-#define lnet_peer_aliveness_enabled(lp) (the_lnet.ln_routing != 0 && \
-					((lp)->lpni_net) && \
-					(lp)->lpni_net->net_tunables.lct_peer_timeout > 0)
+#define lnet_peer_aliveness_enabled(lp)				\
+	(lnet_routing_enabled() &&				\
+	((lp)->lpni_net) &&					\
+	(lp)->lpni_net->net_tunables.lct_peer_timeout > 0)
 
 struct lnet_route {
 	struct list_head	lr_list;	/* chain on net */
@@ -1920,6 +1921,13 @@ struct lnet_udsp {
 #define LNET_STATE_SHUTDOWN		0	/* not started */
 #define LNET_STATE_RUNNING		1	/* started up OK */
 #define LNET_STATE_STOPPING		2	/* telling thread to stop */
+
+/* LNet routing states */
+#define LNET_ROUTING_DISABLED		0	/* LNet routing disabled */
+#define LNET_ROUTING_ENABLED		1	/* LNet routing enabled */
+
+#define lnet_routing_disabled() (the_lnet.ln_routing == LNET_ROUTING_DISABLED)
+#define lnet_routing_enabled() (the_lnet.ln_routing != LNET_ROUTING_DISABLED)
 
 struct nid_update_info {
 	GENRADIX(struct lnet_nid) nui_rdx;

lnet/lnet/api-ni.c
@@ -1333,7 +1333,7 @@ lnet_prepare(lnet_pid_t requested_pid)
 
 	LASSERT(the_lnet.ln_refcount == 0);
 
-	the_lnet.ln_routing = 0;
+	the_lnet.ln_routing = LNET_ROUTING_DISABLED;
 
 	LASSERT((requested_pid & LNET_PID_USERFLAG) == 0);
 	the_lnet.ln_pid = requested_pid;
@@ -2090,7 +2090,7 @@ __must_hold(&the_lnet.ln_api_mutex)
 	/* switch the NIs to point to the new ping info created */
 	lnet_net_lock(LNET_LOCK_EX);
 
-	if (!the_lnet.ln_routing)
+	if (lnet_routing_disabled())
 		pbuf->pb_info.pi_features |= LNET_PING_FEAT_RTE_DISABLED;
 	if (!lnet_peer_discovery_disabled)
 		pbuf->pb_info.pi_features |= LNET_PING_FEAT_DISCOVERY;
@@ -4151,7 +4151,7 @@ out:
 
 void lnet_mark_ping_buffer_for_update(void)
 {
-	if (the_lnet.ln_routing)
+	if (lnet_routing_enabled())
 		return;
 
 	atomic_set(&the_lnet.ln_update_ping_buf, 1);

lnet/lnet/lib-move.c
@@ -1223,7 +1223,7 @@ lnet_return_rx_credits_locked(struct lnet_msg *msg)
 
 		/* If routing is now turned off, we just drop this buffer and
 		 * don't bother trying to return credits.  */
-		if (!the_lnet.ln_routing) {
+		if (lnet_routing_disabled()) {
 			lnet_destroy_rtrbuf(rb, rbp->rbp_npages);
 			goto routing_off;
 		}
@@ -1261,7 +1261,7 @@ routing_off:
 
 		/* drop all messages which are queued to be routed on that
 		 * peer. */
-		if (!the_lnet.ln_routing) {
+		if (lnet_routing_disabled()) {
 			LIST_HEAD(drop);
 			list_splice_init(&lp->lp_rtrq, &drop);
 			spin_unlock(&lp->lp_lock);
@@ -4678,7 +4678,7 @@ lnet_parse_ack(struct lnet_ni *ni, struct lnet_msg *msg)
 int
 lnet_parse_forward_locked(struct lnet_ni *ni, struct lnet_msg *msg)
 {
-	if (!the_lnet.ln_routing)
+	if (lnet_routing_disabled())
 		return -ECANCELED;
 
 	return lnet_post_routed_recv_locked(msg, 0);
@@ -4805,7 +4805,7 @@ lnet_parse(struct lnet_ni *ni, struct lnet_hdr *hdr,
 	 * This avoids situations where the router's own traffic results in NI
 	 * status changes
 	 */
-	if (the_lnet.ln_routing && type == LNET_MSG_GET &&
+	if (lnet_routing_enabled() && type == LNET_MSG_GET &&
 	    hdr->msg.get.ptl_index == LNET_RESERVED_PORTAL &&
 	    !lnet_islocalnid(&src_nid) &&
 	    ni->ni_net->net_last_alive != now) {
@@ -4855,7 +4855,7 @@ lnet_parse(struct lnet_ni *ni, struct lnet_hdr *hdr,
 			return -EPROTO;
 		}
 
-		if (!the_lnet.ln_routing) {
+		if (lnet_routing_disabled()) {
 			CERROR("%s, src %s: Dropping message for %s "
 			       "(routing not enabled)\n",
 				libcfs_nidstr(from_nid),

lnet/lnet/lib-msg.c
@@ -859,7 +859,7 @@ lnet_health_check(struct lnet_msg *msg)
 		    lpni->lpni_peer_net->lpn_peer &&
 		    lpni->lpni_peer_net->lpn_peer->lp_nnis <= 1) {
 			attempt_remote_resend = false;
-			if (!(lnet_isrouter(lpni) || the_lnet.ln_routing))
+			if (!(lnet_isrouter(lpni) || lnet_routing_enabled()))
 				handle_remote_health = false;
 		}
 		/* Do not put my interfaces into peer NI recovery. They should
@@ -898,7 +898,7 @@ lnet_health_check(struct lnet_msg *msg)
 			 * I'm a router, then set that lpni's health to
 			 * maximum so we can commence communication
 			 */
-			if (lnet_isrouter(lpni) || the_lnet.ln_routing) {
+			if (lnet_isrouter(lpni) || lnet_routing_enabled()) {
 				lnet_set_lpni_healthv_locked(lpni,
 					LNET_MAX_HEALTH_VALUE);
 			} else {

lnet/lnet/peer.c
@@ -254,7 +254,7 @@ lnet_peer_alloc(struct lnet_nid *nid)
 	 * all peers created on a router should have health on
 	 * if it's not already on.
 	 */
-	if (the_lnet.ln_routing && !lnet_health_sensitivity)
+	if (lnet_routing_enabled() && !lnet_health_sensitivity)
 		lp->lp_health_sensitivity = 1;
 
 	/*

lnet/lnet/router.c
@@ -1008,7 +1008,7 @@ lnet_update_ni_status_locked(void)
 	time64_t now;
 	time64_t timeout;
 
-	LASSERT(the_lnet.ln_routing);
+	LASSERT(lnet_routing_enabled());
 
 	timeout = router_ping_timeout + alive_router_check_interval;
 
@@ -1075,7 +1075,7 @@ bool lnet_router_checker_active(void)
 {
 	/* Router Checker thread needs to run when routing is enabled in
 	 * order to call lnet_update_ni_status_locked() */
-	if (the_lnet.ln_routing)
+	if (lnet_routing_enabled())
 		return true;
 
 	return !list_empty(&the_lnet.ln_routers) &&
@@ -1194,7 +1194,7 @@ rescan:
 		}
 	}
 
-	if (the_lnet.ln_routing)
+	if (lnet_routing_enabled())
 		push = lnet_update_ni_status_locked();
 
 	lnet_net_unlock(cpt);
@@ -1523,7 +1523,7 @@ lnet_rtrpools_alloc(int im_a_router)
 	}
 
 	lnet_net_lock(LNET_LOCK_EX);
-	the_lnet.ln_routing = 1;
+	the_lnet.ln_routing = LNET_ROUTING_ENABLED;
 	lnet_net_unlock(LNET_LOCK_EX);
 	complete(&the_lnet.ln_mt_wait_complete);
 	return 0;
@@ -1584,7 +1584,7 @@ lnet_rtrpools_adjust(int tiny, int small, int large)
 	 * failed.  It's up to the user space caller to revert the
 	 * changes. */
 
-	if (!the_lnet.ln_routing)
+	if (lnet_routing_disabled())
 		return 0;
 
 	return lnet_rtrpools_adjust_helper(tiny, small, large);
@@ -1595,7 +1595,7 @@ lnet_rtrpools_enable(void)
 {
 	int rc = 0;
 
-	if (the_lnet.ln_routing)
+	if (lnet_routing_enabled())
 		return 0;
 
 	if (the_lnet.ln_rtrpools == NULL)
@@ -1611,7 +1611,7 @@ lnet_rtrpools_enable(void)
 		return rc;
 
 	lnet_net_lock(LNET_LOCK_EX);
-	the_lnet.ln_routing = 1;
+	the_lnet.ln_routing = LNET_ROUTING_ENABLED;
 
 	the_lnet.ln_ping_target->pb_info.pi_features &=
 		~LNET_PING_FEAT_RTE_DISABLED;
@@ -1627,11 +1627,11 @@ lnet_rtrpools_enable(void)
 void
 lnet_rtrpools_disable(void)
 {
-	if (!the_lnet.ln_routing)
+	if (lnet_routing_disabled())
 		return;
 
 	lnet_net_lock(LNET_LOCK_EX);
-	the_lnet.ln_routing = 0;
+	the_lnet.ln_routing = LNET_ROUTING_DISABLED;
 	the_lnet.ln_ping_target->pb_info.pi_features |=
 		LNET_PING_FEAT_RTE_DISABLED;
 

commit 796bf817bc615645ca95ac70abbdf4ed25a3c76f

LU-16518 lnet: avoid non-literal format string
Pass the string returned by ln_routing2str() through a "%s"
format specifier rather than using it as the format string
itself. This fixes a -Wformat-security warning.

Fixes: 7fb531928dae ("LU-15135 lnet: Add defines for state of routing")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I07c5f1cc6f0e94c270fd6dad85eddd686ce14d38
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67349
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Chris Horn <chorn@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/lnet/lnet_debugfs.c
@@ -235,7 +235,7 @@ proc_lnet_routes(const struct ctl_table *table, int write,
 	s = tmpstr; /* points to current position in tmpstr[] */
 
 	if (*ppos == 0) {
-		s += scnprintf(s, tmpstr + tmpsiz - s, ln_routing2str());
+		s += scnprintf(s, tmpstr + tmpsiz - s, "%s", ln_routing2str());
 
 		LASSERT(tmpstr + tmpsiz - s > 0);
 