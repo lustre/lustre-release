lustre/lustre-release

commit c413d2ede5dfe71a89878aed305a8d61f03a30a3

LU-19014 memcg: fix client hang in balance_dirty_pages()
Two nodes (at least) append write a shared file in Lustre with
memcg enabled.
The client randomly hung in balance_dirty_pages() with the
following call trace:
[<0>] balance_dirty_pages+0x2ee/0xd10
[<0>] balance_dirty_pages_ratelimited_flags+0x27a/0x380
[<0>] generic_perform_write+0x150/0x210
[<0>] vvp_io_write_start+0x516/0xc00 [lustre]
[<0>] cl_io_start+0x5a/0x110 [obdclass]
[<0>] cl_io_loop+0x97/0x1f0 [obdclass]
[<0>] ll_file_io_generic+0x4d2/0xe50 [lustre]
[<0>] do_file_write_iter+0x3e9/0x5d0 [lustre]
[<0>] vfs_write+0x2cb/0x410
[<0>] ksys_write+0x5f/0xe0
[<0>] do_syscall_64+0x5c/0xf0

After analyze the core dump of the hung system, we found that the
bdi_writeback data structure (wb) corresponded to the memcg has
pending dirty pages (in state WB_registered | WB_has_diry_io), but
can not write-out the dirty pages and loop in balance_dirty_pages
function.

This is a bug in Lustre memcg code. In OSC/MDC layer, it will stop
to flush dirty pages once found that there are no any unstable
pages.
However, there may be some dirty pages queued in the cache. In
this case, the client should still write back the dirty pages.
Thus the wb stat accounting will be updated and the write process
can continue instead of looping endless.

Moreover, there are some problem in the current Lustre CLIO engine.
When the system or a certain memcg is under memory pressure, the
client just queues the dirty page in page cache or in the current
active extent (OES_ACTIVE osc_extent) when vvp_io_write_commit()/
cl_io_commit_async() is called in ->write_end(). The queued pages
can not be written back even the kernel is trying to flush dirty
pages in writeback via ->ll_writepages().
The client is looping in the following call sequences:

loop:
->write_begin()
->write_end()
->balance_dirty_pages()
  ->Launch file writeback in background but cannot flush any
    dirty pages.
  ->The current process is paused a certain time (i.e. 200ms) as

lustre/include/lustre_osc.h
@@ -563,7 +563,8 @@ int osc_cache_truncate_start(const struct lu_env *env, struct osc_object *obj,
 			     __u64 size, struct osc_extent **extp);
 void osc_cache_truncate_end(const struct lu_env *env, struct osc_extent *ext);
 int osc_cache_writeback_range(const struct lu_env *env, struct osc_object *obj,
-			      pgoff_t start, pgoff_t end, int hp, int discard);
+			      pgoff_t start, pgoff_t end, int hp, int discard,
+			      enum cl_io_priority prio);
 int osc_cache_wait_range(const struct lu_env *env, struct osc_object *obj,
 			 pgoff_t start, pgoff_t end);
 int osc_io_unplug0(const struct lu_env *env, struct client_obd *cli,
@@ -645,9 +646,10 @@ int osc_dio_submit(const struct lu_env *env, struct cl_io *io,
 int osc_io_commit_async(const struct lu_env *env,
 			const struct cl_io_slice *ios,
 			struct cl_page_list *qin, int from, int to,
-			cl_commit_cbt cb);
+			cl_commit_cbt cb, enum cl_io_priority prio);
 void osc_io_extent_release(const struct lu_env *env,
-			   const struct cl_io_slice *ios);
+			   const struct cl_io_slice *ios,
+			   enum cl_io_priority prio);
 int osc_io_iter_init(const struct lu_env *env, const struct cl_io_slice *ios);
 void osc_io_iter_fini(const struct lu_env *env,
 		      const struct cl_io_slice *ios);
@@ -731,6 +733,11 @@ static inline struct obd_export *osc_export(const struct osc_object *obj)
 static inline struct client_obd *osc_cli(const struct osc_object *obj)
 {
 	return &osc_export(obj)->exp_obd->u.cli;
+}
+
+static inline char *cli_name(struct client_obd *cli)
+{
+	return cli->cl_import->imp_obd->obd_name;
 }
 
 static inline struct osc_object *cl2osc(const struct cl_object *obj)

config/lustre-core.m4
@@ -4406,6 +4406,29 @@ AC_DEFUN([LC_HAVE_FOLIO_BATCH_REINIT], [
 ]) # LC_HAVE_FOLIO_BATCH_REINIT
 
 #
+# LC_HAVE_INODE_ATTACH_WB_FOLIO
+#
+# linux kernel v6.2-rc4 commit: 9cfb816b1c6c99f4b3c1d4a0fb096162cd17ec71
+# mm/fs: convert inode_attach_wb() to take a folio
+#
+AC_DEFUN([LC_SRC_HAVE_INODE_ATTACH_WB_FOLIO], [
+	LB2_LINUX_TEST_SRC([inode_attach_wb_folio_arg], [
+		#include <linux/writeback.h>
+	],[
+		struct folio *folio = NULL;
+
+		inode_attach_wb(NULL, folio);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_INODE_ATTACH_WB_FOLIO], [
+	LB2_MSG_LINUX_TEST_RESULT([if 'inode_attach_wb()' takes folio],
+	[inode_attach_wb_folio_arg], [
+		AC_DEFINE(HAVE_INODE_ATTACH_WB_FOLIO, 1,
+			['inode_attach_wb()' takes folio])
+	])
+]) # LC_HAVE_INODE_ATTACH_WB_FOLIO
+
+#
 # LC_HAVE_IOV_ITER_IOVEC
 #
 # linux kernel v6.3-rc4-32-g6eb203e1a868
@@ -5399,6 +5422,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK
 	LC_SRC_HAVE_U64_CAPABILITY
 	LC_SRC_HAVE_FOLIO_BATCH_REINIT
+	LC_SRC_HAVE_INODE_ATTACH_WB_FOLIO
 
 	# 6.4
 	LC_SRC_HAVE_IOV_ITER_IOVEC
@@ -5737,6 +5761,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_LOCKS_LOCK_FILE_WAIT_IN_FILELOCK
 	LC_HAVE_U64_CAPABILITY
 	LC_HAVE_FOLIO_BATCH_REINIT
+	LC_HAVE_INODE_ATTACH_WB_FOLIO
 
 	# 6.4
 	LC_HAVE_IOV_ITER_IOVEC

lustre/include/cl_object.h
@@ -1428,6 +1428,36 @@ static inline void cl_read_ahead_release(const struct lu_env *env,
 		ra->cra_release(env, ra);
 }
 
+enum cl_io_priority {
+	/* Normal I/O, usually just queue the pages in the client side cache. */
+	IO_PRIO_NORMAL	= 0,
+	/* I/O is urgent and should flush queued pages to OSTs ASAP. */
+	IO_PRIO_URGENT,
+	/* The memcg is under high memory pressure and the user write process
+	 * is dirty exceeded and under rate limiting in balance_dirty_pages().
+	 * It needs to flush dirty pages for the corresponding @wb ASAP.
+	 */
+	IO_PRIO_DIRTY_EXCEEDED,
+	/*
+	 * I/O is urgent and flushing pages are marked with OBD_BRW_SOFT_SYNC
+	 * flag and may trigger a soft sync on OSTs. Thus it can free unstable
+	 * pages much quickly.
+	 */
+	IO_PRIO_SOFT_SYNC,
+	/*
+	 * The system or a certain memcg is under high memory pressure. Need to
+	 * flush dirty pages to OSTs immediately and I/O RPC must wait the write
+	 * transcation commit on OSTs synchronously to release unstable pages.
+	 */
+	IO_PRIO_HARD_SYNC,
+	IO_PRIO_MAX,
+};
+
+static inline bool cl_io_high_prio(enum cl_io_priority prio)
+{
+	return prio >= IO_PRIO_URGENT;
+}
+
 /**
  * Per-layer io operations.
  * \see vvp_io_ops, lov_io_ops, lovsub_io_ops, osc_io_ops
@@ -1540,12 +1570,13 @@ struct cl_io_operations {
 	int  (*cio_commit_async)(const struct lu_env *env,
 				 const struct cl_io_slice *slice,
 				 struct cl_page_list *queue, int from, int to,
-				 cl_commit_cbt cb);
+				 cl_commit_cbt cb, enum cl_io_priority prio);
 	/**
 	 * Release active extent.
 	 */
 	void  (*cio_extent_release)(const struct lu_env *env,
-				    const struct cl_io_slice *slice);
+				    const struct cl_io_slice *slice,
+				    enum cl_io_priority prio);
 	/**
 	 * Decide maximum read ahead extent
 	 *
@@ -1833,13 +1864,14 @@ struct cl_io {
 			struct cl_page *ft_page;
 		} ci_fault;
 		struct cl_fsync_io {
-			loff_t             fi_start;
-			loff_t             fi_end;
+			loff_t			 fi_start;
+			loff_t			 fi_end;
 			/** file system level fid */
-			struct lu_fid     *fi_fid;
-			enum cl_fsync_mode fi_mode;
+			struct lu_fid	 	*fi_fid;
+			enum cl_fsync_mode	 fi_mode;
 			/* how many pages were written/discarded */
-			unsigned int       fi_nr_written;
+			unsigned int		 fi_nr_written;
+			enum cl_io_priority	 fi_prio;
 		} ci_fsync;
 		struct cl_ladvise_io {
 			__u64			 lio_start;
@@ -2389,8 +2421,9 @@ int   cl_io_submit_sync(const struct lu_env *env, struct cl_io *io,
 			long timeout);
 int   cl_io_commit_async(const struct lu_env *env, struct cl_io *io,
 			  struct cl_page_list *queue, int from, int to,
-			  cl_commit_cbt cb);
-void  cl_io_extent_release(const struct lu_env *env, struct cl_io *io);
+			  cl_commit_cbt cb, enum cl_io_priority prio);
+void  cl_io_extent_release(const struct lu_env *env, struct cl_io *io,
+			   enum cl_io_priority prio);
 int cl_io_lru_reserve(const struct lu_env *env, struct cl_io *io,
 		      loff_t pos, size_t bytes);
 int   cl_io_read_ahead(const struct lu_env *env, struct cl_io *io,

lustre/llite/file.c
@@ -5287,7 +5287,7 @@ static loff_t ll_file_seek(struct file *file, loff_t offset, int origin)
 
 		/* flush local cache first if any */
 		cl_sync_file_range(inode, offset, OBD_OBJECT_EOF,
-				   CL_FSYNC_LOCAL, 0);
+				   CL_FSYNC_LOCAL, 0, IO_PRIO_NORMAL);
 
 		retval = ll_lseek(file, offset, origin);
 		if (retval < 0)
@@ -5343,7 +5343,8 @@ static int ll_flush(struct file *file, fl_owner_t id)
  * Return how many pages have been written.
  */
 int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
-		       enum cl_fsync_mode mode, int ignore_layout)
+		       enum cl_fsync_mode mode, int ignore_layout,
+		       enum cl_io_priority prio)
 {
 	struct lu_env *env;
 	struct cl_io *io;
@@ -5373,6 +5374,7 @@ int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
 	fio->fi_fid = ll_inode2fid(inode);
 	fio->fi_mode = mode;
 	fio->fi_nr_written = 0;
+	fio->fi_prio = prio;
 
 	if (cl_io_init(env, io, CIT_FSYNC, io->ci_obj) == 0)
 		result = cl_io_loop(env, io);
@@ -5451,7 +5453,8 @@ int ll_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 		err = pcc_fsync(file, start, end, datasync, &cached);
 		if (!cached)
 			err = cl_sync_file_range(inode, start, end,
-						 CL_FSYNC_ALL, 0);
+						 CL_FSYNC_ALL, 0,
+						 IO_PRIO_NORMAL);
 		if (rc == 0 && err < 0)
 			rc = err;
 		if (rc < 0)

lustre/llite/llite_internal.h
@@ -1379,7 +1379,8 @@ int ll_read_folio(struct file *file, struct folio *folio);
 int ll_io_read_page(const struct lu_env *env, struct cl_io *io,
 			   struct cl_page *page, struct file *file);
 void ll_readahead_init(struct inode *inode, struct ll_readahead_state *ras);
-int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io);
+int vvp_io_write_commit(const struct lu_env *env, struct cl_io *io,
+			enum cl_io_priority prio);
 
 enum lcc_type;
 void ll_cl_add(struct inode *inode, const struct lu_env *env, struct cl_io *io,
@@ -2002,7 +2003,8 @@ dentry_may_statahead(struct inode *dir, struct dentry *dentry)
 }
 
 int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
-		       enum cl_fsync_mode mode, int ignore_layout);
+		       enum cl_fsync_mode mode, int ignore_layout,
+		       enum cl_io_priority prio);
 
 static inline int ll_file_nolock(const struct file *file)
 {

lustre/llite/llite_lib.c
@@ -3265,7 +3265,8 @@ void ll_delete_inode(struct inode *inode)
 		 * unlink, so that file is not opened somewhere else
 		 */
 		cl_sync_file_range(inode, 0, OBD_OBJECT_EOF, inode->i_nlink ?
-				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1);
+				   CL_FSYNC_LOCAL : CL_FSYNC_DISCARD, 1,
+				   IO_PRIO_NORMAL);
 	}
 
 	ll_truncate_inode_pages_final(inode);

lustre/llite/rw.c
@@ -1543,7 +1543,7 @@ int ll_writepage(struct page *vmpage, struct writeback_control *wbc)
 		 */
 		result = cl_sync_file_range(inode, offset,
 					    offset + PAGE_SIZE - 1,
-					    CL_FSYNC_LOCAL, 1);
+					    CL_FSYNC_LOCAL, 1, IO_PRIO_NORMAL);
 		if (result > 0) {
 			/* May have written more than one page. decreasing this
 			 * page because the caller will count it.
@@ -1570,6 +1570,7 @@ out:
 int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	struct inode *inode = mapping->host;
+	enum cl_io_priority prio = IO_PRIO_NORMAL;
 	loff_t start;
 	loff_t end;
 	enum cl_fsync_mode mode;
@@ -1611,8 +1612,11 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 		wb = inode_to_wb(inode);
 		if (wbc->for_background ||
 		    (wb->start_all_reason == WB_REASON_VMSCAN &&
-		     test_bit(WB_start_all, &wb->state)))
+		     test_bit(WB_start_all, &wb->state))) {
 			mode = CL_FSYNC_RECLAIM;
+			if (wb->dirty_exceeded)
+				prio = IO_PRIO_DIRTY_EXCEEDED;
+		}
 		spin_unlock(&inode->i_lock);
 #else
 		/*
@@ -1634,7 +1638,7 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 	 * inside the IO context of write, which will cause deadlock at
 	 * layout_conf since it waits for active IOs to complete.
 	 */
-	result = cl_sync_file_range(inode, start, end, mode, 1);
+	result = cl_sync_file_range(inode, start, end, mode, 1, prio);
 	if (result > 0) {
 		wbc->nr_to_write -= result;
 		result = 0;

lustre/llite/rw26.c
@@ -781,7 +781,7 @@ again:
 		}
 
 		/* commit pages and then wait for page lock */
-		result = vvp_io_write_commit(env, io);
+		result = vvp_io_write_commit(env, io, IO_PRIO_NORMAL);
 		if (result < 0)
 			GOTO(out, result);
 
@@ -903,6 +903,7 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 	struct cl_page *page;
 	struct page *vmpage = wbe_folio_page(vmfolio);
 	unsigned from = pos & (PAGE_SIZE - 1);
+	enum cl_io_priority prio = IO_PRIO_NORMAL;
 	bool unplug = false;
 	int result = 0;
 	ENTRY;
@@ -926,6 +927,29 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 	LASSERT(cl_page_is_owned(page, io));
 	if (copied > 0) {
 		struct cl_page_list *plist = &vio->u.readwrite.vui_queue;
+#ifdef SB_I_CGROUPWB
+		struct inode *inode = file_inode(file);
+		struct bdi_writeback *wb;
+
+		spin_lock(&inode->i_lock);
+#ifdef HAVE_INODE_ATTACH_WB_FOLIO
+		inode_attach_wb(inode, page_folio(vmpage));
+#else
+		inode_attach_wb(inode, vmpage);
+#endif
+		wb = inode_to_wb(inode);
+		LASSERTF(wb != NULL, "wb@%pK\n", wb);
+		if (wb->dirty_exceeded) {
+			unplug = true;
+			prio = IO_PRIO_URGENT;
+			CDEBUG(D_IOTRACE, "wb@%pK dirty_ratelimit=%lu balanced_dirty_ratelimit=%lu dirty_exceeded=%d state=%lX last_old_flush=%lu\n",
+			       wb, wb->dirty_ratelimit,
+			       wb->balanced_dirty_ratelimit,
+			       wb->dirty_exceeded, wb->state,
+			       wb->last_old_flush);
+		}
+		spin_unlock(&inode->i_lock);
+#endif
 
 		lcc->lcc_page = NULL; /* page will be queued */
 
@@ -962,7 +986,7 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 	    io->u.ci_rw.crw_pos + io->u.ci_rw.crw_bytes)
 		unplug = true;
 	if (unplug)
-		result = vvp_io_write_commit(env, io);
+		result = vvp_io_write_commit(env, io, prio);
 
 	if (result < 0)
 		io->ci_result = result;

commit 39a24e80284ed9efbf9e3915e0e618e8bbe4d03d

LU-20066 osc: fix NULL deref in osc_cache_shrink_scan on import teardown
Two bugs allow the OSC slab shrinker to crash when lctl cleanup/detach
tears down client OBDs concurrently:

1. cli_name() dereferences cli->cl_import->imp_obd->obd_name, but
   cl_import is set to NULL by obd_cleanup_client_import() during
   osc_precleanup_common().  Since cli_name() is only used for logging
   and imp_obd is always the same obd_device that owns the client_obd,
   reach obd_name directly via container_of() instead.

2. The shrink-list removal (list_del) happened in osc_cleanup_common(),
   after osc_precleanup_common() had already nulled cl_import.  During
   that window the shrinker could grab a cli from osc_shrink_list whose
   cl_import was already NULL.  Move list_del_init() to
   osc_precleanup_common(), before obd_cleanup_client_import(), so the
   OSC is off the shrink list before cl_import is torn down.

Crash reproduced by running sanity test_172 (lctl cleanup/detach of all
client OBDs) concurrently with tests that trigger drop_caches=2:

  pc : osc_cache_shrink_scan+0x178/0x3f0 [osc]
  drop_caches_sysctl_handler -> shrink_slab -> osc_cache_shrink_scan
    -> osc_lru_shrink -> cli_name -> cli->cl_import->imp_obd  (NULL)

AArch64 crash at virtual address 0xb8 (imp_obd offset in obd_import).

TLC-Bug: TLU-190
Fixes: c413d2ede5df ("LU-19014 memcg: fix client hang in balance_dirty_pages()")
Assisted-By: Claude Sonnet 4.6 <noreply@anthropic.com>
Signed-off-by: Minh Diep <mdiep@thelustrecollective.com>
Change-Id: I64c2acaaeb6206ee41fd078d04f07c0379ac6a90
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67344
Reviewed-by: Patrick Farrell <patrick@thelustrecollective.com>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_osc.h
@@ -735,7 +735,7 @@ static inline struct client_obd *osc_cli(const struct osc_object *obj)
 
 static inline char *cli_name(struct client_obd *cli)
 {
-	return cli->cl_import->imp_obd->obd_name;
+	return container_of(cli, struct obd_device, u.cli)->obd_name;
 }
 
 static inline struct osc_object *cl2osc(const struct cl_object *obj)

lustre/osc/osc_request.c
@@ -4155,6 +4155,14 @@ int osc_precleanup_common(struct obd_device *obd)
 	cancel_work_sync(&cli->cl_writeback_work);
 	cancel_work_sync(&cli->cl_lru_work);
 
+	/* Remove from the shrinker list before destroying the import so the
+	 * slab shrinker (osc_cache_shrink_scan) cannot grab a cli whose
+	 * cl_import, cl_cache or cl_lru_left are about to become NULL.
+	 */
+	spin_lock(&osc_shrink_lock);
+	list_del_init(&cli->cl_shrink_list);
+	spin_unlock(&osc_shrink_lock);
+
 	obd_cleanup_client_import(obd);
 	RETURN(0);
 }
@@ -4165,10 +4173,6 @@ int osc_cleanup_common(struct obd_device *obd)
 	struct client_obd *cli = &obd->u.cli;
 
 	ENTRY;
-
-	spin_lock(&osc_shrink_lock);
-	list_del(&cli->cl_shrink_list);
-	spin_unlock(&osc_shrink_lock);
 
 	/* lru cleanup */
 	if (cli->cl_cache != NULL) {