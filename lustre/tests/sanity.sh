lustre/lustre-release

commit 63ee4af3bb097256a5fdb3de8f27d7898ab1cb55

LU-19620 obdclass: llog to continue if broken
if a plain llog is corrupted for a reason, then llog_osd_next_block()
return -EINVAL. handle this error in llog_process_thread() so that
llog processing doesn't stop.

a catalog can be corrupted as well and we should be able to recover.

add a procfs entry dumping current plain llog used by OSP:
osp..sync_current_llog=[0x1:0x5:0x0] (1 plains), current [0x1:0x6:0x0]

add a test reproducing a broken llog problems.

Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: If72a169e11fda7ed88b7dfcbd7b65bd340efaa9c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62651
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/tests/sanity.sh
@@ -10509,7 +10509,7 @@ test_60b() { # bug 6411
 					print from_begin
 			  }")
 
-	[[ $LLOG_COUNT -gt 120 ]] &&
+	(( LLOG_COUNT <= 130 )) ||
 		error "CDEBUG_LIMIT not limiting messages ($LLOG_COUNT)" || true
 }
 run_test 60b "limit repeated messages from CERROR/CWARN"

lustre/kunit/llog_test.c
@@ -2282,6 +2282,205 @@ out_close:
 	RETURN(rc);
 }
 
+static int llog_obj_truncate(const struct lu_env *env,
+			     struct dt_object *obj,
+			     loff_t size)
+{
+	struct dt_device *dd;
+	struct lu_attr *attr;
+	struct thandle *th;
+	int		  rc;
+
+	ENTRY;
+
+	OBD_ALLOC_PTR(attr);
+	if (attr == NULL)
+		RETURN(-ENOMEM);
+
+	LASSERT(obj);
+	dd = lu2dt_dev(obj->do_lu.lo_dev);
+	if (IS_ERR_OR_NULL(dd))
+		GOTO(attr_free, rc = -EINVAL);
+
+	attr->la_size = size;
+	attr->la_valid = LA_SIZE;
+
+	th = dt_trans_create(env, dd);
+	if (IS_ERR(th))
+		GOTO(attr_free, rc = PTR_ERR(th));
+
+	rc = dt_declare_punch(env, obj, size, OBD_OBJECT_EOF, th);
+	if (rc)
+		GOTO(cleanup, rc);
+	rc = dt_declare_attr_set(env, obj, attr, th);
+	if (rc)
+		GOTO(cleanup, rc);
+	rc = dt_trans_start_local(env, dd, th);
+	if (rc)
+		GOTO(cleanup, rc);
+
+	rc = dt_punch(env, obj, size, OBD_OBJECT_EOF, th);
+	if (rc == 0)
+		rc = dt_attr_set(env, obj, attr, th);
+
+cleanup:
+	dt_trans_stop(env, dd, th);
+attr_free:
+	OBD_FREE_PTR(attr);
+
+	RETURN(rc);
+}
+
+/* truncate one-by-one bytes and check the catalog is still functional */
+static int llog_test_12(const struct lu_env *env, struct obd_device *obd)
+{
+	struct llog_handle *cath, *llh;
+	struct llog_mini_rec lmr;
+	struct llog_cookie cookie;
+	struct llog_logid catid;
+	int rc, rc2, i, numrecs;
+	struct llog_ctxt *ctxt;
+	struct dt_object *obj;
+	struct lu_attr *attr;
+	int bytes2trunc;
+	char name[10];
+
+	ENTRY;
+
+	OBD_ALLOC_PTR(attr);
+	if (attr == NULL)
+		RETURN(-ENOMEM);
+
+	ctxt = llog_get_context(obd, LLOG_TEST_ORIG_CTXT);
+	LASSERT(ctxt);
+
+	lmr.lmr_hdr.lrh_len = lmr.lmr_tail.lrt_len = LLOG_MIN_REC_SIZE;
+	lmr.lmr_hdr.lrh_type = LLOG_OP_MAGIC;
+
+	scnprintf(name, sizeof(name), "%x", llog_test_rand + 3);
+	CWARN("12: create a catalog log with name: %s\n", name);
+	rc = llog_open_create(env, ctxt, &cath, NULL, name);
+	if (rc) {
+		CERROR("12: llog_create failed: rc=%d\n", rc);
+		GOTO(ctxt_release, rc);
+	}
+	catid = cath->lgh_id;
+
+	rc = llog_init_handle(env, cath, LLOG_F_IS_CAT, &uuid);
+	if (rc) {
+		CERROR("12: can't init llog handle: %d\n", rc);
+		GOTO(out, rc);
+	}
+
+	numrecs = 0;
+
+	/* truncated plain llog cases */
+	for (bytes2trunc = sizeof(lmr); bytes2trunc >= 4; bytes2trunc--) {
+
+		for (i = 0; i < 10; i++) {
+			rc = llog_cat_add(env, cath, &lmr.lmr_hdr, &cookie);
+			if (rc != 1) {
+				CERROR("12: add to cat failed: rc=%d\n", rc);
+				GOTO(out, rc);
+			}
+			numrecs++;
+		}
+		plain_counter = 0;
+		rc = llog_cat_process(env, cath, plain_print_cb, "fooba", 0, 0);
+		if (rc) {
+			CERROR("12: cat process failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+		if (plain_counter != numrecs) {
+			CERROR("12: found %d recs, expect %d\n",
+			       plain_counter, numrecs);
+			GOTO(out, rc = -EINVAL);
+		}
+
+		llh = cath->u.chd.chd_current_log;
+		LASSERT(cath->u.chd.chd_current_log);
+
+		obj = llh->u.chd.chd_current_log->lgh_obj;
+		LASSERT(obj);
+		lu_object_get(&obj->do_lu);
+
+		rc = llog_cat_close(env, cath);
+		LASSERT(rc == 0);
+
+		rc = dt_attr_get(env, obj, attr);
+		if (rc) {
+			CERROR("12: attr_get failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+
+		LCONSOLE_INFO("12: truncate %d bytes\n", bytes2trunc);
+		rc = llog_obj_truncate(env, obj, attr->la_size - bytes2trunc);
+		if (rc) {
+			CERROR("12: truncate failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+
+		dt_object_put(env, obj);
+
+		LCONSOLE_INFO("12: re-open and process the catalog\n");
+		rc = llog_open(env, ctxt, &cath, &catid,
+			       NULL, LLOG_OPEN_EXISTS);
+		if (rc) {
+			CERROR("12: llog_create with logid failed: %d\n", rc);
+			GOTO(ctxt_release, rc);
+		}
+		rc = llog_init_handle(env, cath, LLOG_F_IS_CAT, &uuid);
+		if (rc) {
+			CERROR("12: can't init llog handle: %d\n", rc);
+			GOTO(out, rc);
+		}
+
+		plain_counter = 0;
+		rc = llog_cat_process(env, cath, plain_print_cb, "foobar", 0, 0);
+		if (rc) {
+			CERROR("12: cat process failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+		if (plain_counter != 0) {
+			CERROR("12: found %d recs, expect 0\n", plain_counter);
+			GOTO(out, rc = -EINVAL);
+		}
+		LCONSOLE_INFO("12: try to add to the catalog\n");
+		rc = llog_cat_add(env, cath, &lmr.lmr_hdr, &cookie);
+		if (rc != 1) {
+			CERROR("12: add to cat failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+		numrecs = 1;
+
+		plain_counter = 0;
+		rc = llog_cat_process(env, cath, plain_print_cb, "fooba", 0, 0);
+		if (rc) {
+			CERROR("12: cat process failed: rc=%d\n", rc);
+			GOTO(out, rc);
+		}
+		if (plain_counter != numrecs) {
+			CERROR("12: found %d recs, expect %d\n",
+			       plain_counter, numrecs);
+			GOTO(out, rc = -EINVAL);
+		}
+	}
+
+out:
+	CWARN("12: put newly-created catalog\n");
+	rc2 = llog_cat_close(env, cath);
+	if (rc2) {
+		CERROR("12: close log %s failed: %d\n", name, rc2);
+		if (rc == 0)
+			rc = rc2;
+	}
+ctxt_release:
+	llog_ctxt_put(ctxt);
+	OBD_FREE_PTR(attr);
+	RETURN(rc);
+}
+
+
 /*
  * -------------------------------------------------------------------------
  * Tests above, boring obd functions below
@@ -2341,6 +2540,10 @@ static int llog_run_tests(const struct lu_env *env, struct obd_device *obd)
 		GOTO(cleanup, rc);
 
 	rc = llog_test_11(env, obd);
+	if (rc)
+		GOTO(cleanup, rc);
+
+	rc = llog_test_12(env, obd);
 	if (rc)
 		GOTO(cleanup, rc);
 

lustre/obdclass/llog.c
@@ -856,7 +856,7 @@ out:
 	if (cd != NULL)
 		cd->lpcd_last_idx = last_called_index;
 
-	if (unlikely(rc == -EIO && loghandle->lgh_obj != NULL)) {
+	if ((rc == -EIO || rc == -EINVAL) && loghandle->lgh_obj != NULL) {
 		if (dt_object_remote(loghandle->lgh_obj)) {
 			/* If it is remote object, then -EIO might means
 			 * disconnection or eviction, let's return -EAGAIN,

lustre/osd-ldiskfs/osd_io.c
@@ -2775,6 +2775,20 @@ static int osd_punch(const struct lu_env *env, struct dt_object *dt,
 		GOTO(out, rc);
 	}
 
+	if (obj->oo_brm) {
+		/* reset block lookup/preallocation cache */
+		struct ldiskfs_inode_info *ei = LDISKFS_I(inode);
+		struct osd_block_ready_map *brm = obj->oo_brm;
+		int i;
+
+		down(&ei->i_append_sem);
+		for (i = 0; i < OSD_BRM_MAX; i++) {
+			brm[i].start = 1UL << 31;
+			brm[i].end = 1UL << 31;
+		}
+		up(&ei->i_append_sem);
+	}
+
 	inode_lock(inode);
 	/* add to orphan list to ensure truncate completion
 	 * if this transaction succeed. ldiskfs_truncate()
@@ -3028,6 +3042,29 @@ static void osd_partial_page_flush_punch(struct osd_device *d,
 	}
 }
 
+static void osd_invalidate_partial_page(struct inode *inode, loff_t offset)
+{
+	struct address_space *mapping = inode->i_mapping;
+	struct ldiskfs_inode_info *ei = LDISKFS_I(inode);
+	struct page *page;
+	int rc;
+
+	if (!test_bit(LDISKFS_INODE_JOURNAL_DATA, &ei->i_flags))
+		return;
+
+	page = find_or_create_page(mapping, i_size_read(inode) >> PAGE_SHIFT,
+				   mapping_gfp_constraint(mapping, ~__GFP_FS));
+	if (!page)
+		return;
+
+	rc = osd_jbd_invalidate_page(LDISKFS_SB(inode->i_sb)->s_journal,
+				     page, 0, PAGE_SIZE);
+	LASSERTF(rc == 0, "  last page %lu %s%s rc=%d\n", page->index,
+		 PageChecked(page) ? "C" : "", PageDirty(page) ? "D" : "", rc);
+	unlock_page(page);
+	put_page(page);
+}
+
 /*
  * For a partial-page truncate, flush the page to disk immediately to
  * avoid data corruption during direct disk write.  b=17397
@@ -3047,6 +3084,9 @@ static void osd_partial_page_flush(struct osd_device *d, struct inode *inode,
 		invalidate_mapping_pages(inode->i_mapping, offset >> PAGE_SHIFT,
 					 offset >> PAGE_SHIFT);
 	}
+
+	/* to prevent ldiskfs warning about forgottent page */
+	osd_invalidate_partial_page(inode, offset);
 }
 
 void osd_execute_truncate(struct osd_object *obj)

lustre/osp/lproc_osp.c
@@ -17,6 +17,7 @@
 
 #define DEBUG_SUBSYSTEM S_CLASS
 
+#include <lustre_log.h>
 #include "osp_internal.h"
 
 /**
@@ -219,6 +220,46 @@ static ssize_t max_sync_changes_store(struct kobject *kobj,
 }
 
 LUSTRE_RW_ATTR(max_sync_changes);
+
+static ssize_t sync_current_llog_show(struct kobject *kobj,
+				     struct attribute *attr,
+				     char *buf)
+{
+	struct dt_device *dt = container_of(kobj, struct dt_device,
+					    dd_kobj);
+	struct osp_device *osp = dt2osp_dev(dt);
+	struct llog_handle *cath, *lgh;
+	struct llog_ctxt *ctxt;
+	struct llog_logid *catid = NULL;
+	struct llog_logid *plid = NULL;
+	int rc = 0;
+
+	ctxt = llog_get_context(osp->opd_obd, LLOG_MDS_OST_ORIG_CTXT);
+	if (IS_ERR_OR_NULL(ctxt))
+		return 0;
+	cath = ctxt->loc_handle;
+	if (!cath || !cath->lgh_hdr)
+		goto out;
+	LASSERT(cath->lgh_hdr->llh_flags & LLOG_F_IS_CAT);
+	catid = &cath->lgh_id;
+	lgh = cath->u.chd.chd_current_log;
+	if (lgh) {
+		plid = &lgh->lgh_id;
+		rc = snprintf(buf, PAGE_SIZE,
+			     DFID" (%d plains), current "DFID"\n",
+			     PLOGID(catid),
+			     cath->lgh_hdr ? cath->lgh_hdr->llh_count : 0,
+			     PLOGID(plid));
+	} else {
+		rc = snprintf(buf, PAGE_SIZE,
+			     DFID" (%d plains)\n", PLOGID(catid),
+			     cath->lgh_hdr ? cath->lgh_hdr->llh_count : 0);
+	}
+out:
+	llog_ctxt_put(ctxt);
+	return rc;
+}
+LUSTRE_RO_ATTR(sync_current_llog);
 
 /**
  * max_rpcs_in_flight_show() - Show maximum number of RPCs in flight allowed
@@ -1274,6 +1315,7 @@ static struct attribute *osp_obd_attrs[] = {
 	&lustre_attr_max_sync_changes.attr,
 	&lustre_attr_force_sync.attr,
 	&lustre_attr_old_sync_processed.attr,
+	&lustre_attr_sync_current_llog.attr,
 	&lustre_attr_create_count.attr,
 	&lustre_attr_max_create_count.attr,
 	&lustre_attr_reserved_mb_high.attr,

commit d2030761bb370bbbacf87be5960863456b98ba8e

LU-20351 mdd: ignore too low clear chlog requests
ignore clear changelog requests with too low index, otherwise
generic llog processing interprets this as a sign of a corrupted
llog and then this and all changelog users registered later are lost.

Fixes: 63ee4af3bb09 ("LU-19620 obdclass: llog to continue if broken")
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Change-Id: Ia3d3a8a05458ce555a40cbe2c0f41bce8a940e83
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66587
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdd/mdd_device.c
@@ -2123,12 +2123,12 @@ static int mdd_changelog_clear_cb(const struct lu_env *env,
 	mdd_chlg_username(rec, mcuc->mcuc_name, sizeof(mcuc->mcuc_name));
 	/* cur_endrec is the oldest purgeable record, make sure we're newer */
 	if (rec->cur_endrec > mcuc->mcuc_endrec) {
-		rc = -EINVAL;
+		/* nothing bad if user uses too small value by mistake */
 		CDEBUG(D_IOCTL,
-		       "%s: request %llu < endrec %llu for user %s: rc = %d\n",
+		       "%s: request %llu < endrec %llu for user %s\n",
 		       mdd2obd_dev(mdd)->obd_name, mcuc->mcuc_endrec,
-		       rec->cur_endrec, mcuc->mcuc_name, rc);
-		RETURN(rc);
+		       rec->cur_endrec, mcuc->mcuc_name);
+		RETURN(0);
 	}
 
 	/*

lustre/tests/sanity.sh
@@ -22185,6 +22185,26 @@ test_160w() {
 }
 run_test 160w "lfs changelog --user --mask"
 
+test_160x() {
+	(( $MDS1_VERSION >= $(version_code 2.17.54) )) ||
+		skip "Need MGS version at least 2.17.54"
+
+	changelog_chmask "ALL"
+	changelog_register || error "changelog_register failed"
+	changelog_register || error "changelog_register failed"
+	changelog_register || error "changelog_register failed"
+	createmany -o $DIR/f- 1000 || error "can't create"
+	before=$(changelog_users $SINGLEMDS|grep ^cl|wc -l)
+	__changelog_clear $SINGLEMDS cl1 200
+	__changelog_clear $SINGLEMDS cl1 100
+	after=$(changelog_users $SINGLEMDS|grep ^cl|wc -l)
+	(( before == after )) || {
+		changelog_users $SINGLEMDS
+		error "$before != $after: lost changelog user"
+	}
+}
+run_test 160x "changelog users do not disappear"
+
 test_161a() {
 	[ $PARALLEL == "yes" ] && skip "skip parallel run"
 