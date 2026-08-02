lustre/lustre-release

commit 0c773016d837a4751678a0eeff00dee30e1d2586

LU-18977 efalnd: Introduce AWS EFA Lustre network driver
This commit introduces a new Lustre network driver (LND) for AWS Elastic
Fabric Adapter (EFA), enabling high-performance, low-latency networking
for Lustre clusters on AWS infrastructure.

EFA is a network device that provides reliable communication and kernel
bypass capabilities, targeting more consistent latency and higher
throughput than traditional TCP-based communication.

Key components added:
- EFA LND core implementation with support for RDMA operations
- Dual NID support:
  * Small NIDs: Uses TCP NI for out-of-band communication to discover
    remote GID and CM QP data, this utilizing LNET ping operations.
  * Large NIDs: Leverages 16 bytes of available address space to store
    connection data directly, eliminating the need for out-of-band
    discovery.
- DebugFS interface for monitoring and troubleshooting
- Module parameter configuration for tuning performance
- Integration with existing LNET infrastructure

The LND already has active customers and has been thoroughly tested
using selftest, IOR, FIO, GDS, and multiple sanity test variations.

Test-Parameters: trivial testlist=sanity-lnet serverversion=2.16
Signed-off-by: Timothy Day <timday@amazon.com>
Signed-off-by: Michael Margolin <mrgolin@amazon.com>
Signed-off-by: Yehuda Yitschak <yehuday@amazon.com>
Signed-off-by: Yonatan Nachum <ynachum@amazon.com>
Change-Id: I2217c0bbffefebb52152ea36feaaa2e5e78ad10c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59470
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Max Wang <wamax@amazon.com>
Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/efalnd/efalnd.h
@@ -1,0 +1,455 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2023-2025, Amazon and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Author: Yehuda Yitschak <yehuday@amazon.com>
+ * Author: Yonatan Nachum <ynachum@amazon.com>
+ */
+
+#ifndef _EFALND_EFALND_H_
+#define _EFALND_EFALND_H_
+
+#define DEBUG_SUBSYSTEM S_LND
+
+#include <linux/bvec.h>
+#include <linux/rhashtable.h>
+
+#include <lnet/lib-lnet.h>
+#include <lnet/lib-types.h>
+#include <lnet/lnet_rdma.h>
+
+#include <efa_verbs.h>
+
+#include "efalnd_proto.h"
+
+#define EFALND_MAJOR_VER        1
+#define EFALND_MINOR_VER        2
+#define EFALND_SUBMINOR_VER     0
+#define EFALND_MAJOR_SHIFT	8
+
+#define KEFA_IFNAME_SIZE		256
+#define EFALND_CREDITS_MIN		8	/* Min # of peer_ni credits */
+#define EFALND_CREDITS_MAX		255	/* Max # of peer_ni credits */
+
+#define KEFA_THREAD_SHIFT		16
+#define KEFA_THREAD_ID(cpt, tid)	((cpt) << KEFA_THREAD_SHIFT | (tid))
+#define KEFA_THREAD_CPT(id)		((id) >> KEFA_THREAD_SHIFT)
+#define KEFA_THREAD_TID(id)		((id) & ((1UL << KEFA_THREAD_SHIFT) - 1))
+
+#define EFALND_MAX_MTU			(8900)
+#define EFALND_MSG_SIZE			EFALND_MAX_MTU
+#define EFALND_MSG_SIZE_ALIGNED		PAGE_ALIGN(EFALND_MSG_SIZE)
+#define EFALND_MSG_PAGES		(EFALND_MSG_SIZE_ALIGNED / PAGE_SIZE)
+#define EFALND_RX_MSGS(q)		(2 * (q)->rq_depth)
+
+/* max # of fragments supported. + 1 for unaligned case */
+#define EFALND_MAX_TX_FRAGS		(LNET_MAX_IOV + 1)
+
+/* Max # of TXs each NI can allocate. */
+#define EFALND_MAX_NI_TX_POOL		2048
+
+/* default values in case no tunable was set */
+#define EFALND_MIN_SCHED_THRS		2
+#define EFALND_MAX_SCHED_THRS		4
+
+ /* Used only for small NIDs */
+#define EFALND_CM_STATIC_QKEY		(0x1111)
+
+#define EFALND_NO_RDMA_THRESH		EFALND_MSG_SIZE
+
+#define EFALND_CONN_HASH_BITS		7
+
+#define EFALND_INV_CONN			((u32)~0U)
+
+#define EFALND_MAX_PEER_QPS		256
+
+#define EFALND_MIN_INIT_CONN_TIMEOUT	200
+
+/*
+ * NI large NID is of the following format:
+ * |15            4|3       2|1         0|
+ * +---------------+---------+-----------+
+ * |      GID      | QP QKEY | QP Number |
+ * +---------------+---------+-----------+
+ * GID: EFA GID is 16 bytes with bytes 0-3 always constant.
+ * QP Number: CM QP number for the remote to be able to establish connection.
+ * QP QKEY: 2 byte QKEY to fit in the available area in EFA GID.
+ */
+#define EFALND_NID_CM_QP_NUM_OFFSET	0
+#define EFALND_NID_CM_QP_NUM_SIZE	2
+#define EFALND_NID_CM_QP_QKEY_OFFSET	2
+#define EFALND_NID_CM_QP_QKEY_SIZE	2
+#define EFALND_NID_GID_OFFSET		4
+#define EFALND_NID_GID_SIZE		12
+
+/* Define EFALND_CD so that we can easily add D_CONSOLE in test envs */
+#define EFALND_CD (D_NET)
+
+#define EFALND_FIELD_AVAIL(type, fld, sz) (offsetof(type, fld) < (sz))
+
+#define EFA_DEV_DEBUG(dev, format, ...) CDEBUG(EFALND_CD, "Device[%s] " format, (dev)->ifname, ## __VA_ARGS__)
+#define EFA_DEV_ERR(dev, format, ...) CERROR("Device[%s] " format, (dev)->ifname, ## __VA_ARGS__)
+#define EFA_DEV_WARN(dev, format, ...) CWARN("Device[%s] " format, (dev)->ifname, ## __VA_ARGS__)
+
+struct kefa_cq;
+struct kefa_qp;
+struct kefa_dev;
+struct kefa_ni;
+struct kefa_sched;
+struct kefa_conn;
+struct kefa_obj_pool;
+
+extern struct kefa_tunables kefalnd_tunables;
+extern struct kefa_data kefalnd;
+
+enum efalnd_init_state {
+	EFALND_INIT_NONE = 0,
+	EFALND_INIT_ALL
+};
+
+struct kefa_remote_qp {
+	u16 qp_num;
+	u32 qkey;
+};
+
+struct kefa_peer_ni {
+	struct kref refcount;             /* number of connections */
+	u32 remote_nid_addr;              /* address of EFA NID */
+	union ib_gid gid;
+	struct kefa_remote_qp cm_qp;
+	struct rhash_head linkage;
+	rwlock_t peer_ni_lock;            /* protects kefa_peer_ni data */
+	struct rcu_head rcu_read;         /* protects nid_gid_map lifetime */
+};
+
+static const struct rhashtable_params peer_ni_params = {
+	.key_len     = sizeof(u32),
+	.key_offset  = offsetof(struct kefa_peer_ni, remote_nid_addr),
+	.head_offset = offsetof(struct kefa_peer_ni, linkage),
+};
+
+struct kefa_tunables {
+	int *kefa_rnr_retry_count;
+	/* # threads on each CPT */
+	int *kefa_nscheds;
+	char **kefa_ipif_name;
+};
+
+/* global singelton EFA data */
+struct kefa_data {
+	enum efalnd_init_state init_state;	/* init state of global data */
+	struct list_head efa_ni_list;		/* list of EFA NIs */
+	struct kefa_sched **scheds;		/* global schedulers */
+	struct kefa_cm_deamon **cm_daemons;	/* Connection manager daemons */
+	struct rhashtable peer_ni;
+	atomic_t peer_ni_count;
+	atomic_t nthreads;			/* # live threads */
+	bool shutdown;				/* signal shutdown to threads */
+};
+
+struct kefa_obj_pool {
+	struct kefa_ni *efa_ni;
+	void *obj_arr;
+	struct list_head free_obj;
+	struct list_head free_pend_obj;	/* Objects pending to be freed */
+	atomic_t pending_work;		/* Pending list have objects on */
+	spinlock_t lock;		/* multithread lock */
+	u32 pool_size;
+	int cpt;
+};
+
+struct kefa_rx {
+	struct list_head list_node;
+	struct kefa_qp *qp;		/* owner QP */
+	struct kefa_msg *msg;		/* message buffer (host vaddr) */
+	struct ib_recv_wr wrq;		/* receive work item... */
+	struct ib_sge sge;		/* ...and its memory */
+	int rx_nob;			/* # bytes received (-1 while posted) */
+};
+
+struct kefa_qp {
+	struct kefa_dev *efa_dev;
+	struct kefa_cq *cq;
+	struct ib_qp *ib_qp;
+	struct kefa_rx *rx_msgs;	/* RX buffers posted to RQ */
+	struct list_head posted_rx;	/* list of posted RX */
+	struct list_head free_rx;	/* list of free RX */
+	spinlock_t rq_lock;
+	u32 rq_depth;
+	u32 rq_space;
+	u32 qkey;
+};
+
+struct kefa_cq {
+	struct ib_cq *ib_cq;
+	struct kefa_dev *efa_dev;	/* owner device */
+	struct list_head sched_node;	/* node on scheduler */
+	int cpt;
+};
+
+enum kefa_fmr_state {
+	KEFA_FMR_INACTIVE = 0,
+	KEFA_FMR_ACTIVATING,
+	KEFA_FMR_ACTIVE,
+	KEFA_FMR_DEACTIVATING,
+};
+
+struct kefa_fmr {
+	struct ib_mr *mr;
+	enum kefa_fmr_state state;
+	struct list_head list_node;
+	struct ib_reg_wr reg_wr;
+	struct ib_send_wr inv_wr;
+};
+
+/* EFA device information */
+struct kefa_dev {
+	struct ib_device *ib_dev;
+	char ifname[KEFA_IFNAME_SIZE];
+	struct kefa_ni *efa_ni;	/* The EFA NI associated with the device */
+	union ib_gid gid;
+	struct ib_pd *pd;		/* PD */
+
+	struct kefa_obj_pool fmr_pool;
+
+	struct kefa_qp *qps;		/* QP set */
+	atomic_t local_qpn;
+
+	struct kefa_cq *cqs;		/* CQ set */
+
+	struct kefa_qp *cm_qp;		/* Connection establishment QP */
+	struct kefa_cq *cm_cq;
+
+	__be32 ifip;			/* Eth interface IP */
+	u32 nqps;
+	u32 ncqs;
+	int cpt;			/* CPU partition of the device */
+};
+
+/* transmit message */
+struct kefa_tx {
+	struct list_head list_node;	/* node on pool/conn list */
+	struct kefa_obj_pool *tx_pool;	/* pool I'm from */
+	struct kefa_conn *conn;		/* connection for TX */
+	struct lnet_msg *lntmsg[2]; /* lnet msgs to finalize on completion */
+	struct kefa_msg *msg;		/* message buffer (host vaddr) */
+	dma_addr_t msgaddr;		/* message buffer (I/O addr) */
+	struct ib_srd_rdma_wr wrq;	/* send work item... */
+	struct ib_sge sge;		/* ...and its memory */
+	u32 lkey;			/* lkey of sge buffers */
+	enum lnet_msg_hstatus hstatus;	/* health status of tx */
+	int status;			/* overall status */
+	int nfrags;			/* # of mapped buffer fragments */
+	struct scatterlist *frags;	/* mapped buffer fragments */
+	struct kefa_rdma_desc rdma_desc;/* rdma descriptor to read/write */
+	enum dma_data_direction dmadir;
+	atomic_t ref_cnt;		/* track sends and completions */
+	atomic_t waiting_resp;
+	struct kefa_fmr *fmr;
+	atomic64_t send_time;		/* send time of send in seconds */
+
+	u8 type;
+	bool send_sync;		/* send ctrl message after RDMA completes */
+	u64 cookie;		/* opaque completion cookie for sync message */
+};
+
+/* Per Lnet network data */
+struct kefa_ni {
+	struct list_head lnd_node;	/* node in LND NI list */
+	struct list_head cm_node;	/* node in connection manager daemon */
+	struct kefa_dev *efa_dev;	/* underlying IB device */
+	struct lnet_ni *lnet_ni;	/* LNet interface */
+	u64 ni_epoch;			/* my epoch */
+	struct kefa_obj_pool tx_pool;
+	DECLARE_HASHTABLE(conns, EFALND_CONN_HASH_BITS);
+	rwlock_t conn_lock;
+	struct kefa_peer_ni *self_peer_ni;	/* Only valid for small NID NI*/
+};
+
+enum kefa_conn_state {
+	KEFA_CONN_INACTIVE,
+	KEFA_CONN_PROBE_TCP,
+	KEFA_CONN_PROBE_EFA,
+	KEFA_CONN_PROBE_EFA_PASSIVE,
+	KEFA_CONN_ESTABLISH,
+	KEFA_CONN_ACTIVE,
+	KEFA_CONN_DEACTIVATING,
+};
+
+enum kefa_conn_type {
+	KEFA_CONN_TYPE_LB,
+	KEFA_CONN_TYPE_INITIATOR,
+	KEFA_CONN_TYPE_RESPONDER,
+};
+
+struct kefa_conn {
+	spinlock_t lock;
+	enum kefa_conn_state state;
+	struct list_head active_tx;	/* LRU list of active kefa_tx */
+	struct list_head pend_tx;	/* list of pending kefa_tx */
+
+	/* Fields that can be changed not under connection lock */
+	struct ib_ah *ah;
+	u64 remote_epoch;		/*The epoch of the remote connection */
+	u8 proto_ver;
+	u32 nqps;
+	struct kefa_remote_qp *data_qps;
+	atomic_t last_qp_idx;
+	struct lnet_nid remote_nid;
+	time64_t last_use_time;	/* last time the conn was used in seconds */
+	struct hlist_node ni_node;	/* node on kefa_ni hashmap */
+	struct kefa_ni *efa_ni;
+
+	/* Low frequency fields */
+	struct list_head abort_tx;	/* Only CM iterates this list */
+	enum kefa_conn_type type;
+	struct lnet_nid local_nid;
+	struct kefa_peer_ni *peer_ni; /* my peer NI - only valid for small NID*/
+	u64 remote_caps;
+	u64 requests;
+};
+
+struct kefa_cm_deamon {
+	struct mutex ni_list_lock;	/* multithread lock */
+	struct list_head efa_ni_list;	/* list of EFA NIs */
+	wait_queue_head_t waitq;
+	bool active;
+	int iter;
+	int cpt;
+};
+
+struct kefa_sched {
+	spinlock_t lock;		/* multithread lock */
+	struct list_head pend_cqs;	/* CQs to poll */
+	wait_queue_head_t waitq;
+	int nthreads;			/* # of poll threads */
+	int nthreads_max;		/* max # of threads */
+	int cpt;			/* CPT id */
+};
+
+static inline u16
+kefalnd_get_lnd_version(void)
+{
+	return ((EFALND_MAJOR_VER << EFALND_MAJOR_SHIFT) | EFALND_MINOR_VER);
+}
+
+static inline void
+kefalnd_large_nid_create(struct lnet_nid *nid, const union ib_gid *gid,
+			 u16 qp_num, u16 qp_qkey)
+{
+	u16 be_qp_num = cpu_to_be16(qp_num);
+	u16 be_qp_qkey = cpu_to_be16(qp_qkey);
+
+	memcpy((u8 *)nid->nid_addr + EFALND_NID_GID_OFFSET,
+	       gid->raw + EFALND_NID_GID_OFFSET,
+	       EFALND_NID_GID_SIZE);
+	memcpy((u8 *)nid->nid_addr + EFALND_NID_CM_QP_NUM_OFFSET, &be_qp_num,
+	       EFALND_NID_CM_QP_NUM_SIZE);
+	memcpy((u8 *)nid->nid_addr + EFALND_NID_CM_QP_QKEY_OFFSET, &be_qp_qkey,
+	       EFALND_NID_CM_QP_QKEY_SIZE);
+
+	nid->nid_size = sizeof(union ib_gid) - 4;
+}
+
+static inline void
+kefalnd_large_nid_get_gid(struct lnet_nid *nid, union ib_gid *gid)
+{
+	memcpy(gid->raw, nid->nid_addr, sizeof(gid->raw));
+	gid->raw[0] = 0xfe;
+	gid->raw[1] = 0x80;
+	gid->raw[2] = 0;
+	gid->raw[3] = 0;
+}
+
+static inline u16
+kefalnd_large_nid_get_cm_qp_num(struct lnet_nid *nid)
+{
+	u16 qp_num;
+
+	memcpy(&qp_num, (u8 *)nid->nid_addr + EFALND_NID_CM_QP_NUM_OFFSET,
+	       sizeof(qp_num));
+
+	return be16_to_cpu(qp_num);
+}
+
+static inline u16
+kefalnd_large_nid_get_cm_qp_qkey(struct lnet_nid *nid)
+{
+	u16 qp_qkey;
+
+	memcpy(&qp_qkey, (u8 *)nid->nid_addr + EFALND_NID_CM_QP_QKEY_OFFSET,
+	       sizeof(qp_qkey));
+
+	return be16_to_cpu(qp_qkey);
+}
+
+static inline void
+kefalnd_thread_stop(void)
+{
+	atomic_dec(&kefalnd.nthreads);
+}
+
+static inline void
+kefalnd_msg_set_epoch(struct kefa_msg *msg, u64 remote_epoch)
+{
+	if (msg->hdr.proto_ver != EFALND_PROTO_VER_1)
+		msg->msg_v2.dst_epoch = remote_epoch;
+	else
+		msg->msg_v1.dst_epoch = remote_epoch;
+}
+
+int kefalnd_tunables_init(void);
+int kefalnd_tunables_setup(struct lnet_ni *ni);
+
+int kefalnd_msgtype2size(int type, u8 proto_ver);
+int kefalnd_efa_status_to_errno(s16 efa_status);
+s16 kefalnd_errno_to_efa_status(int status);
+void kefalnd_tx_done(struct kefa_tx *tx);
+void kefalnd_abort_tx(struct kefa_tx *tx, enum lnet_msg_hstatus hstatus,
+		      int status);
+/* Should be used only on TXs that we don't expect to get any completions for */
+void kefalnd_force_cancel_tx(struct kefa_tx *tx, enum lnet_msg_hstatus hstatus,
+			     int status);
+void kefalnd_init_tx_protocol_msg(struct kefa_tx *tx, struct kefa_conn *conn,
+				  int type, int body_nob, u8 proto_ver);
+struct kefa_tx *kefalnd_get_idle_tx(struct kefa_ni *efa_ni);
+void kefalnd_conn_post_tx_locked(struct kefa_conn *conn);
+void kefalnd_get_srcnid_from_msg(struct kefa_msg *msg, struct lnet_nid *srcnid);
+void kefalnd_get_dstnid_from_msg(struct kefa_msg *msg, struct lnet_nid *dstnid);
+
+struct kefa_peer_ni *kefalnd_find_remote_peer_ni(struct kefa_dev *efa_dev,
+						 struct lnet_nid *efa_nid);
+struct kefa_peer_ni *kefalnd_lookup_or_create_peer_ni(lnet_nid_t nid,
+						      union ib_gid *gid,
+						      u16 cm_qpn, u32 cm_qkey);
+void kefalnd_update_peer_ni(struct kefa_peer_ni *peer_ni, union ib_gid *gid,
+			    u16 cm_qpn, u32 cm_qkey);
+int kefalnd_get_nid_metadata(struct lnet_ni *ni,
+			     struct lnet_nid_md_entry *md_entry);
+void kefalnd_put_peer_ni(struct kefa_peer_ni *peer_ni);
+
+void kefalnd_debugfs_init(void);
+void kefalnd_debugfs_exit(void);
+
+struct kefa_conn *kefalnd_lookup_conn(struct kefa_ni *efa_ni,
+				      struct lnet_nid *nid,
+				      enum kefa_conn_type conn_type);
+struct kefa_conn *kefalnd_lookup_or_init_conn(struct kefa_ni *efa_ni,
+					      struct lnet_nid *nid,
+					      enum kefa_conn_type conn_type);
+void kefalnd_handle_conn_establishment(struct kefa_ni *efa_ni,
+				       struct kefa_msg *msg);
+void kefalnd_deactivate_conn(struct kefa_conn *conn);
+void kefalnd_destroy_conn(struct kefa_conn *conn, enum lnet_msg_hstatus hstatus,
+			  int status);
+int kefalnd_cm_daemon(void *arg);
+void kefalnd_add_ni_to_cm_daemon(struct kefa_ni *efa_ni);
+void kefalnd_del_ni_from_cm_daemon(struct kefa_ni *efa_ni);
+
+#endif

MAINTAINERS
@@ -392,6 +392,12 @@ R:	Chris Horn <chris.horn@hpe.com>
 S:	Supported
 F:	lnet/klnds/socklnd/
 
+Lustre Networking KEFALND
+R:	Yonatan Nachum <ynachum@amazon.com>
+R:	Michael Margolin <mrgolin@amazon.com>
+S:	Supported
+F:	lnet/klnds/efalnd/
+
 Lustre Nodemap
 R:	Sebastien Buisson <sbuisson@whamcloud.com>
 S:	Supported

autoMakefile.am
@@ -380,6 +380,9 @@ debs: undef.h debs_common
 	if test "x@DPKG_INT_O2IB@" != "x"; then \
 		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} int_o2ib"; \
 	fi; \
+	if test "x@EFALND@" != "x"; then \
+		export DEB_BUILD_PROFILES="$${DEB_BUILD_PROFILES} efalnd"; \
+	fi; \
 	if test "x@CONFIG_CACHE_FILE@" != "x"; then \
 		export CONFIG_CACHE_FILE="@CONFIG_CACHE_FILE@"; \
 	fi; \

config/lustre-lnet.m4
@@ -866,6 +866,108 @@ AS_IF([test $ENABLEO2IB != "no"], [
 ]) # LN_CONFIG_O2IB
 
 #
+# LN_CONFIG_EFALND
+#
+# check whether to use the EFA Network Interface lnd
+#
+AC_DEFUN([LN_CONFIG_EFALND], [
+AC_MSG_CHECKING([whether to enable EFA LND])
+AC_ARG_ENABLE([efa],
+	AC_HELP_STRING([--enable-efa=[yes|no|<path>]],
+		       [enable EFA LND]),
+	[], [enable_efa="no"])
+
+AS_CASE([$enable_efa],
+	[yes],
+	[
+		AS_IF([test x$uses_dpkg = xyes], [
+			LS_PKG="dpkg --listfiles"
+		], [
+			LS_PKG="rpm -ql"
+		])
+
+		EFA_PKG="efa"
+		EFA_HEADER="efa_verbs.h"
+		EFA_HEADER_PATH=$(eval "$LS_PKG $EFA_PKG" | egrep "$EFA_HEADER" | head -n1)
+		AS_IF([test -n "$EFA_HEADER_PATH"], [
+			EFA_INCLUDE_PATH=$(dirname $EFA_HEADER_PATH)
+		])
+
+		KERNEL_RDMA_INCLUDE="$LINUX/include/rdma"
+		AS_IF([test -z "$EFA_INCLUDE_PATH" && test -f "$KERNEL_RDMA_INCLUDE/$EFA_HEADER"], [
+			EFA_INCLUDE_PATH=$KERNEL_RDMA_INCLUDE
+		])
+
+		AS_IF([test -z "$EFA_INCLUDE_PATH"], [
+			AC_MSG_ERROR([Could not find a supported EFA driver. For custom built driver, the path to header files can be supplied using --enable-efa=<path>.])
+		])
+
+		ENABLE_EFA="yes"
+	],
+	[no],
+	[
+		ENABLE_EFA="no"
+	],
+	[
+		EFA_INCLUDE_PATH=$enable_efa
+		ENABLE_EFA="yes"
+	])
+
+AS_IF([test $ENABLE_EFA = "no"], [
+	AC_MSG_RESULT([no])
+], [
+	AC_MSG_RESULT([yes])
+
+	# Check mandatory kernel definitions
+
+	LB_CHECK_COMPILE([if 'ib_device_get_by_name' exists],
+	ib_device_get_by_name, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ib_device_get_by_name(NULL, 0);
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if 'RDMA_DRIVER_EFA' exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		int x = RDMA_DRIVER_EFA;
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	LB_CHECK_COMPILE([if ib_device 'kverbs_provider' flag exists],
+	rdma_driver_efa, [
+		#include <rdma/ib_verbs.h>
+	],[
+		struct ib_device dev = {
+			.kverbs_provider = 0,
+		};
+	],[],[
+		AC_MSG_ERROR([EFA LND is not supported with this Linux kerenel])
+	])
+
+	# Check optional kernel definitions
+
+	LB_CHECK_COMPILE([if 'ibdev_to_node' exists],
+	ibdev_to_node, [
+		#include <rdma/ib_verbs.h>
+	],[
+		ibdev_to_node(NULL);
+	],[
+		AC_DEFINE(HAVE_IBDEV_TO_NODE, 1,
+			[ibdev_to_node() is defined])
+	])
+
+	EFALND="efalnd"
+	AC_SUBST(EFALND)
+	AC_SUBST(EFA_INCLUDE_PATH)
+])
+]) # LN_CONFIG_EFALND
+
+#
 # LN_CONFIG_GNILND
 #
 # check whether to use the Gemini Network Interface lnd
@@ -1276,6 +1378,7 @@ AC_MSG_NOTICE([LNet kernel checks
 
 LN_CONFIG_BACKOFF
 LN_CONFIG_O2IB
+LN_CONFIG_EFALND
 LN_CONFIG_GNILND
 LN_CONFIG_KFILND
 ]) # LN_PROG_LINUX
@@ -1431,6 +1534,7 @@ LN_USR_NLMSGERR
 AC_DEFUN([LN_CONDITIONALS], [
 AM_CONDITIONAL(EXTERNAL_KO2IBLND,  test x$EXTERNAL_KO2IBLND = "xyes")
 AM_CONDITIONAL(BUILT_IN_KO2IBLND,  test x$BUILT_IN_KO2IBLND = "xyes")
+AM_CONDITIONAL(BUILD_EFALND,	   test x$EFALND = "xefalnd")
 AM_CONDITIONAL(BUILD_GNILND,       test x$GNILND  = "xgnilnd")
 AM_CONDITIONAL(BUILD_KFILND,       test x$KFILND  = "xkfilnd")
 ]) # LN_CONDITIONALS
@@ -1453,6 +1557,8 @@ lnet/klnds/o2iblnd/Makefile
 lnet/klnds/o2iblnd/autoMakefile
 lnet/klnds/in-kernel-o2iblnd/Makefile
 lnet/klnds/in-kernel-o2iblnd/autoMakefile
+lnet/klnds/efalnd/Makefile
+lnet/klnds/efalnd/autoMakefile
 lnet/klnds/gnilnd/Makefile
 lnet/klnds/gnilnd/autoMakefile
 lnet/klnds/socklnd/Makefile

debian/rules
@@ -257,6 +257,9 @@ configure-stamp: autogen-stamp debian/control.main debian/control.modules.in
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
 	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
+	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \
 	options="$${options} libcfs-cdebug libcfs-trace libcfs-assert"; \
@@ -588,6 +591,9 @@ kdist_config: prep-deb-files patch-stamp
 	fi; \
 	if echo "$${DEB_BUILD_PROFILES}" | grep -q "kfilnd"; then \
 		export EXTRAFLAGS="$${EXTRAFLAGS} --with-kfi=/usr/src/kfabric"; \
+	fi; \
+	if echo "$${DEB_BUILD_PROFILES}" | grep -q "efalnd"; then \
+		export EXTRAFLAGS="$${EXTRAFLAGS} --enable-efa"; \
 	fi; \
 	options="gss crypto pinger checksum flock lru-resize"; \
 	options="$${options} mindf fail-alloc invariants lu_ref pgstate-track"; \

lnet/include/lnet/lib-lnet.h
@@ -619,6 +619,7 @@ extern int live_router_check_interval;
 extern int dead_router_check_interval;
 extern int portal_rotor;
 extern int lock_prim_nid;
+extern int lnet_interfaces_max;
 
 void lnet_mt_event_handler(struct lnet_event *event);
 

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -114,6 +114,11 @@ struct lnet_ioctl_config_kfilnd_tunables {
 	__u32 lnd_timeout;
 };
 
+struct lnet_ioctl_config_efalnd_tunables {
+	__u32 lnd_version;
+	__u32 lnd_nqps;
+};
+
 struct lnet_ioctl_config_socklnd_tunables {
 	__u32 lnd_version;
 	__u16 lnd_conns_per_peer;
@@ -130,6 +135,7 @@ struct lnet_ioctl_config_gnilnd_tunables {
 struct lnet_lnd_tunables {
 	union {
 		struct lnet_ioctl_config_o2iblnd_tunables lnd_o2ib;
+		struct lnet_ioctl_config_efalnd_tunables lnd_efa;
 		struct lnet_ioctl_config_socklnd_tunables lnd_sock;
 		struct lnet_ioctl_config_kfilnd_tunables lnd_kfi;
 		struct lnet_ioctl_config_gnilnd_tunables lnd_gni;

commit a9a80b224d5b5434a81b13307b871cec80fb5b04

LU-20497 efalnd: Add protection against stale Rx completions
On Rx completion, we immediately fetch the Rx using the pointer stored
in the WR ID. If the Rx is stale due to NI destroy and creation, we
defer an Rx not relevant for the completion resulting in failures in
unpack flow or even crashes if the Rx is currently not posted so its
rx_nob is -1. To handle this situation, instead of storing the Rx
address in the WR ID, store its index in the first 4 bytes and part of
the NI epoch in the upper 4 bytes. This way we can validate the Rx
completion is for the current NI and if not drop it.

Test-Parameters: trivial
Fixes: 0c773016d837 ("LU-18977 efalnd: Introduce AWS EFA Lustre network driver")
Change-Id: I4c2c60f558fc5130cfc58d1bd7e4bb5dabec868e
Signed-off-by: Yonatan Nachum <ynachum@amazon.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67456
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Reviewed-by: Michael Margolin <mrgolin@amazon.com>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Tom Sela <tomsela@amazon.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/efalnd/efalnd.c
@@ -1824,15 +1824,34 @@ bad_pkt:
 	return -EPROTO;
 }
 
+static struct kefa_rx *
+kefalnd_get_rx_from_wc(struct kefa_ni *efa_ni, struct ib_wc *wc)
+{
+	u32 rx_idx = EFALND_RX_WRID_INDEX(wc->wr_id);
+	u32 epoch = EFALND_RX_WRID_EPOCH(wc->wr_id);
+	struct kefa_qp *qp = wc->qp->qp_context;
+
+	if (epoch != (u32)efa_ni->ni_epoch)
+		return NULL;
+
+	if (rx_idx >= EFALND_RX_MSGS(qp))
+		return NULL;
+
+	return qp->rx_msgs + rx_idx;
+}
+
 static void
 kefalnd_rx_complete(struct kefa_ni *efa_ni, struct ib_wc *wc)
 {
 	struct kefa_rx *rx;
 	int nob, rc;
 
-	rx = (void *)wc->wr_id;
+	rx = kefalnd_get_rx_from_wc(efa_ni, wc);
+	if (!rx)
+		return;
+
 	nob = wc->byte_len;
-	if (!rx || nob == 0) {
+	if (nob == 0) {
 		/* Reaching here means FW or LND did something bad */
 		CERROR("cpu[%u] received bad RX handle with status[%u] nob[%u]",
 		       smp_processor_id(), wc->status, nob);
@@ -2204,6 +2223,7 @@ static int
 kefalnd_init_rx_msgs(struct kefa_qp *qp)
 {
 	struct kefa_dev *efa_dev = qp->efa_dev;
+	struct kefa_ni *efa_ni = efa_dev->efa_ni;
 	struct kefa_rx *rx;
 	int i;
 
@@ -2235,7 +2255,7 @@ kefalnd_init_rx_msgs(struct kefa_qp *qp)
 
 		rx->wrq.sg_list = &rx->sge;
 		rx->wrq.num_sge = 1;
-		rx->wrq.wr_id = (u64)rx;
+		rx->wrq.wr_id = EFALND_RX_WRID(efa_ni->ni_epoch, i);
 		INIT_LIST_HEAD(&rx->list_node);
 		list_add_tail(&rx->list_node, &qp->free_rx);
 	}
@@ -2292,6 +2312,7 @@ kefalnd_create_qp(struct kefa_dev *efa_dev, struct kefa_qp *qp,
 	init_attr.send_cq = cq->ib_cq;
 	init_attr.recv_cq = cq->ib_cq;
 	init_attr.sq_sig_type = IB_SIGNAL_ALL_WR;
+	init_attr.qp_context = qp;
 
 	ib_qp = ib_create_qp(efa_dev->pd, &init_attr);
 	if (IS_ERR(ib_qp)) {

lnet/klnds/efalnd/efalnd.h
@@ -49,6 +49,11 @@
 #define EFALND_MSG_PAGES		(EFALND_MSG_SIZE_ALIGNED / PAGE_SIZE)
 #define EFALND_RX_MSGS(q)		(2 * (q)->rq_depth)
 
+/* RX work request ID encoding: [epoch(32) | rx_index(32)] */
+#define EFALND_RX_WRID(epoch, idx)	(((u64)(epoch) << 32) | (u32)(idx))
+#define EFALND_RX_WRID_EPOCH(wrid)	((u32)((wrid) >> 32))
+#define EFALND_RX_WRID_INDEX(wrid)	((u32)(wrid))
+
 /* max # of fragments supported. + 1 for unaligned case */
 #define EFALND_MAX_TX_FRAGS		(LNET_MAX_IOV + 1)
 