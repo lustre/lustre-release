lustre/lustre-release

commit b2cb6fd1095f9c483b7bc1ebbbfdaef719aea87c

LU-1842 quota: add core QSD code
Add acquire/release logic on the qsd side as well as reintegration
which is handled in 3 steps:
- enqueue global index lock
- fetch global index and update local enforce list
- fetch slave index and update local copy
- adjust/release quota space as appropriate

This patch also fixes the ofd code to call ->ldo_recovery_complete
across the stack.

Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>
Change-Id: Ib4a8910fe34de878159827c76b40a8f899aaad10
Signed-off-by: Niu Yawei <yawei.niu@intel.com>
Reviewed-on: http://review.whamcloud.com/4182
Tested-by: Hudson
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Jinshan Xiong <jinshan.xiong@whamcloud.com>
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Niu Yawei <niu@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -837,8 +837,24 @@ static void osd_object_delete(const struct lu_env *env, struct lu_object *l)
 
         osd_index_fini(obj);
         if (inode != NULL) {
+		struct qsd_instance	*qsd = osd_obj2dev(obj)->od_quota_slave;
+		qid_t			 uid = inode->i_uid;
+		qid_t			 gid = inode->i_gid;
+
                 iput(inode);
                 obj->oo_inode = NULL;
+
+		if (qsd != NULL) {
+			struct osd_thread_info	*info = osd_oti_get(env);
+			struct lquota_id_info	*qi = &info->oti_qi;
+
+			/* Release granted quota to master if necessary */
+			qi->lqi_id.qid_uid = uid;
+			qsd_adjust_quota(env, qsd, &qi->lqi_id, USRQUOTA);
+
+			qi->lqi_id.qid_uid = gid;
+			qsd_adjust_quota(env, qsd, &qi->lqi_id, GRPQUOTA);
+		}
         }
 }
 
@@ -4624,7 +4640,17 @@ static int osd_process_config(const struct lu_env *env,
 static int osd_recovery_complete(const struct lu_env *env,
                                  struct lu_device *d)
 {
-        RETURN(0);
+	struct osd_device	*osd = osd_dev(d);
+	int			 rc = 0;
+	ENTRY;
+
+	if (osd->od_quota_slave == NULL)
+		RETURN(0);
+
+	/* start qsd instance on recovery completion, this notifies the quota
+	 * slave code that we are about to process new requests now */
+	rc = qsd_start(env, osd->od_quota_slave);
+	RETURN(rc);
 }
 
 /*

lustre/osd-zfs/osd_handler.c
@@ -754,8 +754,17 @@ static int osd_process_config(const struct lu_env *env,
 
 static int osd_recovery_complete(const struct lu_env *env, struct lu_device *d)
 {
+	struct osd_device	*osd = osd_dev(d);
+	int			 rc = 0;
 	ENTRY;
-	RETURN(0);
+
+	if (osd->od_quota_slave == NULL)
+		RETURN(0);
+
+	/* start qsd instance on recovery completion, this notifies the quota
+	 * slave code that we are about to process new requests now */
+	rc = qsd_start(env, osd->od_quota_slave);
+	RETURN(rc);
 }
 
 /*

lustre/quota/qsd_internal.h
@@ -55,6 +55,12 @@ struct qsd_instance {
 	 * are exported */
 	cfs_proc_dir_entry_t	*qsd_proc;
 
+	/* export used for the connection to quota master */
+	struct obd_export     *qsd_exp;
+
+	/* ldlm namespace used for quota locks */
+	struct ldlm_namespace *qsd_ns;
+
 	/* on-disk directory where to store index files for this qsd instance */
 	struct dt_object	*qsd_root;
 
@@ -66,23 +72,41 @@ struct qsd_instance {
 	 * future. For the time being, we can just use an array. */
 	struct qsd_qtype_info	*qsd_type_array[MAXQUOTAS];
 
-	/* r/w spinlock protecting:
-	 * - the state flags
-	 * - the qsd update list
-	 * - the deferred list
-	 * - flags of the qsd_qtype_info
-	 *
-	 * probably way too much :(
-	 */
-	cfs_rwlock_t		 qsd_lock;
-
 	/* per-filesystem quota information */
 	struct qsd_fsinfo	*qsd_fsinfo;
 
 	/* link into qfs_qsd_list of qfs_fsinfo */
 	cfs_list_t		 qsd_link;
 
+	/* list of lqe entry which might need quota space adjustment */
+	cfs_list_t		 qsd_adjust_list;
+
+	/* lock protecting adjust list */
+	cfs_spinlock_t		 qsd_adjust_lock;
+
+	/* dedicated thread for updating slave index files. */
+	struct ptlrpc_thread	 qsd_upd_thread;
+
+	/* list of update tasks */
+	cfs_list_t		 qsd_upd_list;
+
+	/* r/w spinlock protecting:
+	 * - the state flags
+	 * - the qsd update list
+	 * - the deferred list
+	 * - flags of the qsd_qtype_info */
+	cfs_rwlock_t		 qsd_lock;
+
+	/* Default quota settings which apply to all identifiers */
+	/* when blk qunit reaches this value, later write reqs from client
+	 * should be sync. b=16642 */
+	unsigned long		 qsd_sync_threshold;
+
 	unsigned long		 qsd_is_md:1,    /* managing quota for mdt */
+				 qsd_started:1,  /* instance is now started */
+				 qsd_prepared:1, /* qsd_prepare() successfully
+						  * called */
+				 qsd_exp_valid:1,/* qsd_exp is now valid */
 				 qsd_stopping:1; /* qsd_instance is stopping */
 };
 
@@ -102,6 +126,9 @@ struct qsd_qtype_info {
 	/* Global index FID to use for this quota type */
 	struct lu_fid		 qqi_fid;
 
+	/* Slave index FID allocated by the master */
+	struct lu_fid		 qqi_slv_fid;
+
 	/* back pointer to qsd device
 	 * immutable after creation. */
 	struct qsd_instance	*qqi_qsd;
@@ -117,6 +144,21 @@ struct qsd_qtype_info {
 	/* Current object versions */
 	__u64			 qqi_slv_ver; /* slave index version */
 	__u64			 qqi_glb_ver; /* global index version */
+
+	/* per quota ID information. All lquota entry are kept in a hash table
+	 * and read from disk on cache miss. */
+	struct lquota_site	*qqi_site;
+
+	/* Reintegration thread */
+	struct ptlrpc_thread	 qqi_reint_thread;
+
+	/* statistics on operations performed by this slave */
+	struct lprocfs_stats	*qqi_stats;
+
+	/* deferred update for the global index copy */
+	cfs_list_t		 qqi_deferred_glb;
+	/* deferred update for the slave index copy */
+	cfs_list_t		 qqi_deferred_slv;
 
 	/* Various flags representing the current state of the slave for this
 	 * quota type. */
@@ -184,6 +226,17 @@ enum qsd_ops {
 };
 
 #define QSD_RES_TYPE(qsd) ((qsd)->qsd_is_md ? LQUOTA_RES_MD : LQUOTA_RES_DT)
+
+/* udpate record for slave & global index copy */
+struct qsd_upd_rec {
+	cfs_list_t		qur_link; /* link into qsd_upd_list */
+	union lquota_id		qur_qid;
+	union lquota_rec	qur_rec;
+	struct qsd_qtype_info  *qur_qqi;
+	struct lquota_entry    *qur_lqe;
+	__u64			qur_ver;
+	bool			qur_global;
+};
 
 /* Common data shared by qsd-level handlers. This is allocated per-thread to
  * reduce stack consumption.  */
@@ -265,6 +318,8 @@ static inline void qsd_set_qunit(struct lquota_entry *lqe, __u64 qunit)
 	lqe->lqe_nopreacq = false;
 }
 
+#define QSD_WB_INTERVAL	15 /* 15 seconds */
+
 /* qsd_entry.c */
 extern struct lquota_entry_operations qsd_lqe_ops;
 int qsd_refresh_usage(const struct lu_env *, struct lquota_entry *);
@@ -311,11 +366,14 @@ void qsd_put_fsinfo(struct qsd_fsinfo *);
 int qsd_process_config(struct lustre_cfg *);
 
 /* qsd_handler.c */
-/* XXX to be replaced with real function once qsd_handler landed */
-static inline int qsd_dqacq(const struct lu_env *env, struct lquota_entry *lqe,
-			    enum qsd_ops op)
-{
-	return 0;
-}
+int qsd_dqacq(const struct lu_env *, struct lquota_entry *, enum qsd_ops);
+__u64 qsd_calc_grants(struct lquota_entry *, __u64, __u32);
 
+/* qsd_writeback.c */
+void qsd_upd_schedule(struct qsd_qtype_info *, struct lquota_entry *,
+		      union lquota_id *, union lquota_rec *, __u64, bool);
+void qsd_bump_version(struct qsd_qtype_info *, __u64, bool);
+int qsd_start_upd_thread(struct qsd_instance *);
+void qsd_stop_upd_thread(struct qsd_instance *);
+void qsd_adjust_schedule(struct lquota_entry *, bool, bool);
 #endif /* _QSD_INTERNAL_H */

commit acbe52cc02ef4e7b41f829428ef5724808446339

LU-20080 quota: fix qsd_fini() vs qsd_op_end() race
Add transaction counter per qsd instance. Increment it in qsd_op_begin
for each lqe added to quota transaction array. Decrement it in
qsd_op_end and wake up appropriate qsd instance that might wait in
qsd_fini. Patch fixes a panic with backtrace:

  BUG: unable to handle kernel NULL pointer dereference at 0000000000000060
  ...
  RIP: 0010:qsd_op_end+0x99/0x340 [lquota]
  ...
  Call Trace:
  osd_trans_stop+0x363/0xaa0 [osd_ldiskfs]
  mgs_nidtbl_write+0x949/0xb20 [mgs]
  mgs_ir_update+0x79/0x2a0 [mgs]
  mgs_target_reg+0xcd3/0x1eb0 [mgs]

Cleanup qsd_op_end. Qids involved in a transaction may relate to data
as well as to metadata qsd instance. That said, it is not correct to
pass to qsd_op_end pointer to only one of QSDs. This pointer should be
better accessed through the lqe.

Fixes: b2cb6fd109 ("LU-1842 quota: add core QSD code")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I6df8881de76de2d6943e8b2c9e8f6aaff3d958f2
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66979
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Shaun Tancheff <shaun@tancheff.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/lustre_quota.h
@@ -204,8 +204,7 @@ void qsd_fini(const struct lu_env *env, struct qsd_instance *qsd);
 int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 		 struct lquota_trans *trans, struct lquota_id_info *lqi,
 		 enum osd_quota_local_flags *flags);
-void qsd_op_end(const struct lu_env *env, struct qsd_instance *qsd,
-		struct lquota_trans *trans);
+void qsd_op_end(const struct lu_env *env, struct lquota_trans *trans);
 void qsd_op_adjust(const struct lu_env *env, struct qsd_instance *qsd,
 		   union lquota_id *id, int i);
 int qsd_transfer(const struct lu_env *env, struct qsd_instance *qsd,

lustre/osd-ldiskfs/osd_handler.c
@@ -2295,7 +2295,6 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 	struct osd_thandle *oh;
 	struct osd_iobuf *iobuf = &oti->oti_iobuf;
 	struct osd_device *osd = osd_dt_dev(th->th_dev);
-	struct qsd_instance *qsd = osd_def_qsd(osd);
 	struct lquota_trans *qtrans;
 	LIST_HEAD(truncates);
 	int rc = 0, remove_agents = 0;
@@ -2361,7 +2360,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 	osd_trunc_unlock_all(env, &truncates);
 
 	/* inform the quota slave device that the transaction is stopping */
-	qsd_op_end(env, qsd, qtrans);
+	qsd_op_end(env, qtrans);
 
 	/*
 	 * as we want IO to journal and data IO be concurrent, we don't block

lustre/osd-zfs/osd_handler.c
@@ -144,9 +144,7 @@ static void osd_trans_commit_cb(void *cb_data, int error)
 	 * we can't provide a suitable environment. It will be performed
 	 * asynchronously by a lquota thread.
 	 */
-	qsd_op_end(NULL, osd->od_quota_slave_dt, &oh->ot_quota_trans);
-	if (osd->od_quota_slave_md != NULL)
-		qsd_op_end(NULL, osd->od_quota_slave_md, &oh->ot_quota_trans);
+	qsd_op_end(NULL, &oh->ot_quota_trans);
 
 	slot = oh->ot_txg & OSD_TXG_MAP_MASK;
 	LASSERT(atomic_read(&osd->od_commit_cb_in_txg[slot]) > 0);
@@ -303,10 +301,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 		/* there won't be any commit, release reserved quota space now,
 		 * if any
 		 */
-		qsd_op_end(env, osd->od_quota_slave_dt, &oh->ot_quota_trans);
-		if (osd->od_quota_slave_md != NULL)
-			qsd_op_end(env, osd->od_quota_slave_md,
-				   &oh->ot_quota_trans);
+		qsd_op_end(env, &oh->ot_quota_trans);
 		OBD_FREE_PTR(oh);
 		RETURN(0);
 	}

lustre/quota/qsd_handler.c
@@ -839,8 +839,8 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 		 struct lquota_trans *trans, struct lquota_id_info *qi,
 		 enum osd_quota_local_flags *local_flags)
 {
-	int	i, rc;
-	bool	found = false;
+	int i, rc;
+	bool had_qentry, found = false;
 
 	ENTRY;
 
@@ -900,12 +900,16 @@ int qsd_op_begin(const struct lu_env *env, struct qsd_instance *qsd,
 		trans->lqt_ids[i].lqi_id     = qi->lqi_id;
 		trans->lqt_ids[i].lqi_type   = qi->lqi_type;
 		trans->lqt_ids[i].lqi_is_blk = qi->lqi_is_blk;
+		trans->lqt_ids[i].lqi_qentry = NULL;
 		trans->lqt_id_cnt++;
 	}
 
+	had_qentry = trans->lqt_ids[i].lqi_qentry != NULL;
 	/* manage quota enforcement for this ID */
 	rc = qsd_op_begin0(env, qsd->qsd_type_array[qi->lqi_type],
 			   &trans->lqt_ids[i], qi->lqi_space, local_flags);
+	if (!had_qentry && trans->lqt_ids[i].lqi_qentry)
+		atomic_inc(&qsd->qsd_trans_count);
 	RETURN(rc);
 }
 EXPORT_SYMBOL(qsd_op_begin);
@@ -1030,21 +1034,12 @@ out:
 }
 
 /**
- * Post quota operation, pre-acquire/release quota from master.
- *
- * \param  env  - the environment passed by the caller
- * \param  qsd  - is the qsd instance attached to the OSD device which
- *                is handling the operation.
- * \param  qqi  - is the qsd_qtype_info structure associated with the quota ID
- *                subject to the operation
- * \param  qid  - stores information related to his ID for the operation
- *                which has just completed
- *
- * \retval 0    - success
- * \retval -ve  - failure
+ * qsd_op_end0() - Post quota operation, pre-acquire/release quota from master.
+ * @env: the environment passed by the caller
+ * @qid: stores information related to his ID for the operation which has just
+ * completed
  */
-static void qsd_op_end0(const struct lu_env *env, struct qsd_qtype_info *qqi,
-			struct lquota_id_info *qid)
+static void qsd_op_end0(const struct lu_env *env, struct lquota_id_info *qid)
 {
 	struct lquota_entry	*lqe;
 	bool			 adjust;
@@ -1091,53 +1086,35 @@ static void qsd_op_end0(const struct lu_env *env, struct qsd_qtype_info *qqi,
 }
 
 /**
- * Post quota operation. It's called after each operation transaction stopped.
+ * qsd_op_end() - Post quota operation after a transaction has stopped.
+ * @env: environment passed by the caller
+ * @trans: quota IDs involved in the transaction
  *
- * \param  env   - the environment passed by the caller
- * \param  qsd   - is the qsd instance associated with device which is handling
- *                 the operation.
- * \param  qids  - all qids information attached in the transaction handle
- * \param  count - is the number of qid entries in the qids array.
- *
- * \retval 0     - success
- * \retval -ve   - failure
+ * Called after each operation transaction has stopped.
  */
-void qsd_op_end(const struct lu_env *env, struct qsd_instance *qsd,
-		struct lquota_trans *trans)
+void qsd_op_end(const struct lu_env *env, struct lquota_trans *trans)
 {
-	int i;
+	int i, id_cnt;
 
 	ENTRY;
-
-	if (unlikely(qsd == NULL))
-		RETURN_EXIT;
-
-	if (qsd->qsd_dev->dd_rdonly)
-		RETURN_EXIT;
-
-	/* We don't enforce quota until the qsd_instance is started */
-	read_lock(&qsd->qsd_lock);
-	if (!qsd->qsd_started) {
-		read_unlock(&qsd->qsd_lock);
-		RETURN_EXIT;
-	}
-	read_unlock(&qsd->qsd_lock);
-
 	LASSERT(trans != NULL);
-
-	for (i = 0; i < trans->lqt_id_cnt; i++) {
-		struct qsd_qtype_info *qqi;
+	id_cnt = trans->lqt_id_cnt;
+	/* reset id_count to 0 so that a second accidental call to qsd_op_end()
+	 * does not result in failure
+	 */
+	trans->lqt_id_cnt = 0;
+	for (i = 0; i < id_cnt; i++) {
+		struct qsd_instance *qsd;
 
 		if (trans->lqt_ids[i].lqi_qentry == NULL)
 			continue;
 
-		qqi = qsd->qsd_type_array[trans->lqt_ids[i].lqi_type];
-		qsd_op_end0(env, qqi, &trans->lqt_ids[i]);
+		qsd = lqe2qqi(trans->lqt_ids[i].lqi_qentry)->qqi_qsd;
+		LASSERT(atomic_read(&qsd->qsd_trans_count) > 0);
+		qsd_op_end0(env, &trans->lqt_ids[i]);
+		if (atomic_dec_and_test(&qsd->qsd_trans_count))
+			wake_up_var(&qsd->qsd_trans_count);
 	}
-
-	/* reset id_count to 0 so that a second accidental call to qsd_op_end()
-	 * does not result in failure */
-	trans->lqt_id_cnt = 0;
 	EXIT;
 }
 EXPORT_SYMBOL(qsd_op_end);
@@ -1339,7 +1316,7 @@ int qsd_reserve_or_free_quota(const struct lu_env *env,
 		RETURN(0);
 
 	if (is_free) {
-		qsd_op_end0(env, qsd->qsd_type_array[qi->lqi_type], qi);
+		qsd_op_end0(env, qi);
 	} else {
 		long long qspace = qi->lqi_space;
 

lustre/quota/qsd_internal.h
@@ -91,6 +91,7 @@ struct qsd_instance {
 
 	/* count of qunit updates during glimpse */
 	int			 qsd_glimpse_refresh;
+	atomic_t		 qsd_trans_count;
 
 	unsigned long		qsd_is_md:1,    /* managing quota for mdt */
 				qsd_started:1,  /* instance is now started */

lustre/quota/qsd_lib.c
@@ -632,6 +632,18 @@ void qsd_fini(const struct lu_env *env, struct qsd_instance *qsd)
 	qsd->qsd_stopping = true;
 	write_unlock(&qsd->qsd_lock);
 
+	while (atomic_read(&qsd->qsd_trans_count) > 0) {
+		int count;
+
+		wait_var_event_timeout(&qsd->qsd_trans_count,
+				       !atomic_read(&qsd->qsd_trans_count),
+				       cfs_time_seconds(30));
+		count = atomic_read(&qsd->qsd_trans_count);
+		if (count)
+			CERROR("%s: QSD waiting on %d transactions\n",
+			       qsd->qsd_svname, count);
+	}
+
 	/* remove qsd proc entry */
 	if (qsd->qsd_proc != NULL) {
 		lprocfs_remove(&qsd->qsd_proc);
@@ -734,6 +746,7 @@ struct qsd_instance *qsd_init(const struct lu_env *env, char *svname,
 	qsd->qsd_updating = false;
 	qsd->qsd_exclusive = excl;
 	qsd->qsd_ver_reint_timeout = 3 * obd_timeout;
+	atomic_set(&qsd->qsd_trans_count, 0);
 
 	/* copy service name */
 	rc = strscpy(qsd->qsd_svname, svname, sizeof(qsd->qsd_svname));