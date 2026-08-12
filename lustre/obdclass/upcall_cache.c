lustre/lustre-release

commit d3157bc43390b56e2de2c7251802a67ccebe4952

LU-17605 obdclass: do not wait forever acquiring entry
The process of refreshing an entry via refresh_entry() goes through
an upcall/downcall. If the upcall succeeds, we enter a wait queue.
If after that the downcall is never called, we hit the expiry timeout,
and we get removed from the wait queue.
But if the entry is not new, the expiry time will be
MAX_SCHEDULE_TIMEOUT == LONG_MAX, which means an infinite wait.
So avoid waiting forever if an entry could not be refreshed, and call
wake_up_all() if the wait for the ACQUIRING state failed.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I50ee59654adc221027c79cb68fa182b9abed50fa
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54269
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Yang Sheng <ys@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/upcall_cache.c
@@ -283,6 +283,7 @@ find_with_lock:
 				       entry->ue_key, rc);
 				goto find_again;
 			}
+			wake_up_all(&entry->ue_waitq);
 			CERROR("%s: acquire for key %lld after %llu: rc = %d\n",
 			       cache->uc_name, entry->ue_key,
 			       cache->uc_acquire_expire, rc);

commit a6334ff4bfbe7bce730b4cc5e65ba6c2889357f7

LU-20530 obdclass: do not release ref before using
In upcall_cache_get_entry(), the currently processed
upcall_cache_entry may be freed due to too early
put_entry() leaving a window for use-after-free.

Test-Parameters: kerberos=true testlist=sanity-krb5 env=SANITY_GSS_EXCEPT="10 13 152 200 201"
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Fixes: d3157bc43390 ("LU-17605 obdclass: do not wait forever acquiring entry")
Fixes: 765e59d37dc2 ("LU-16911 sec: quiet messages from identity upcall retry mech")
Signed-off-by: Bruno Faccini <bfaccini@nvidia.com>
Change-Id: I807524fafba0c76bf625d5d10e3e6161655d24af
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67591
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Aurelien Degremont <adegremont@nvidia.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/obdclass/upcall_cache.c
@@ -328,20 +328,21 @@ find_with_lock:
 			 */
 			if (left <= 0 && !cache->uc_acquire_replay)
 				failedacquiring = true;
-			put_entry(cache, entry);
 			if (!failedacquiring) {
-				write_unlock(&cache->uc_lock);
 				failedacquiring = true;
 				new = NULL;
 				CDEBUG(D_OTHER,
 				       "retry acquire for key %llu (got %d)\n",
 				       entry->ue_key, rc);
+				put_entry(cache, entry);
+				write_unlock(&cache->uc_lock);
 				goto find_again;
 			}
 			wake_up_all(&entry->ue_waitq);
 			CERROR("%s: acquire for key %lld after %llu: rc = %d\n",
 			       cache->uc_name, entry->ue_key,
 			       cache->uc_acquire_expire, rc);
+			put_entry(cache, entry);
 			GOTO(out, entry = ERR_PTR(rc));
 		}
 	}