lustre/lustre-release

commit 3ccd941e2de36a2d791268111c381c72f0afa11e

LU-19080 compat: match kernel shrinker protocol
With the removal of RHEL7 we can now rework the shrinker code to
match the latest kernels. ll_shrinker_create() are broken into
two functions, ll_shrinker_alloc() and ll_shrinker_register().

Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: I885c7521e89c279b8df1385d0ed3b46f98880967
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60352
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Timothy Day <timday@amazon.com>

lustre_compat/mm/shrinker_debug.c
@@ -139,7 +139,7 @@ static const struct file_operations shrinker_debugfs_scan_fops = {
 	.write	= shrinker_debugfs_scan_write,
 };
 
-static int shrinker_add_debugfs(struct shrinker *shrinker)
+static int shrinker_add_debugfs(struct shrinker *shrinker, const char *name)
 {
 #ifndef HAVE_SHRINKER_ALLOC
 	struct ll_shrinker *s = container_of(shrinker, struct ll_shrinker,
@@ -161,7 +161,7 @@ static int shrinker_add_debugfs(struct shrinker *shrinker)
 
 	s->debugfs_id = id;
 
-	snprintf(buf, sizeof(buf), "%s-%d", s->name, id);
+	snprintf(buf, sizeof(buf), "%s-%d", name, id);
 
 	/* create debugfs entry */
 	entry = debugfs_create_dir(buf, shrinker_debugfs_root);
@@ -193,7 +193,6 @@ void ll_shrinker_free(struct shrinker *shrinker)
 		ida_free(&shrinker_debugfs_ida, s->debugfs_id);
 
 	debugfs_remove_recursive(s->debugfs_entry);
-	kfree(s->name);
 #endif /* !CONFIG_SHRINKER_DEBUG */
 
 #ifdef HAVE_SHRINKER_ALLOC
@@ -209,74 +208,85 @@ void ll_shrinker_free(struct shrinker *shrinker)
 }
 EXPORT_SYMBOL(ll_shrinker_free);
 
-/* allocate and register a shrinker, return should be checked with IS_ERR() */
-struct shrinker *ll_shrinker_create(unsigned int flags, const char *fmt, ...)
+struct shrinker *ll_shrinker_alloc(unsigned int flags, const char *fmt, ...)
 {
-	struct shrinker *shrinker;
-#ifndef CONFIG_SHRINKER_DEBUG
-	struct ll_shrinker *s;
-#endif
-#if defined(HAVE_REGISTER_SHRINKER_FORMAT_NAMED) || defined(HAVE_SHRINKER_ALLOC)
+	struct shrinker *shrinker = NULL;
+	struct ll_shrinker *s = NULL;
+#ifdef HAVE_SHRINKER_ALLOC
 	struct va_format vaf;
 #endif
 	va_list args;
 	int rc = 0;
 
+	/* Only time we don't need ll_shrinker is with latest kernels
+	 * that have the shrinker debugfs interface turned on.
+	 */
+#if defined(HAVE_REGISTER_SHRINKER_FORMAT_NAMED) || !defined(HAVE_SHRINKER_ALLOC) || !defined(CONFIG_SHRINKER_DEBUG)
+	LIBCFS_ALLOC(s, sizeof(*s));
+	if (s) {
+ #ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
+		s->vaf.fmt = fmt;
+		s->vaf.va = &args;
+ #endif
+	} else {
+		return ERR_PTR(-ENOMEM);
+	}
+#endif
 	va_start(args, fmt);
 #ifdef HAVE_SHRINKER_ALLOC
 	vaf.fmt = fmt;
 	vaf.va = &args;
 	shrinker = shrinker_alloc(flags, "%pV", &vaf);
-	va_end(args);
- #ifndef CONFIG_SHRINKER_DEBUG
-	LIBCFS_ALLOC(s, sizeof(*s));
-	if (!s) {
-		shrinker_free(shrinker);
-		shrinker = NULL;
-	} else {
-		s->name = kvasprintf_const(GFP_KERNEL, fmt, args);
+	if (shrinker)
 		shrinker->private_data = s;
-	}
- #endif
-#else /* !HAVE_SHRINKER_ALLOC */
-	LIBCFS_ALLOC(s, sizeof(*s));
- #if !defined(CONFIG_SHRINKER_DEBUG)
-	if (s)
-		s->name = kvasprintf_const(GFP_KERNEL, fmt, args);
- #endif
-	va_end(args);
+	else
+		rc = -ENOMEM;
+#else
 	shrinker = (struct shrinker *)s;
 #endif
-	if (!shrinker)
-		return ERR_PTR(-ENOMEM);
+#ifndef CONFIG_SHRINKER_DEBUG
+	if (rc == 0) {
+		const char *name = kvasprintf_const(GFP_KERNEL, fmt, args);
+
+		if (strncmp(name, "ldlm_pools", strlen("ldlm_pools")) != 0)
+			rc = shrinker_add_debugfs(shrinker, name);
+
+		kfree(name);
+	}
+#endif
+	va_end(args);
+
+	if (rc < 0) {
+		if (shrinker)
+			ll_shrinker_free(shrinker);
+		return ERR_PTR(rc);
+	}
 
 	shrinker->seeks = DEFAULT_SEEKS;
-#ifdef HAVE_SHRINKER_ALLOC
-	shrinker_register(shrinker);
-#else
- #ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
-	va_start(args, fmt);
-	vaf.fmt = fmt;
-	vaf.va = &args;
-	rc = register_shrinker(shrinker, "%pV", &vaf);
-	va_end(args);
- #else
-	if (rc == 0)
-		rc = register_shrinker(shrinker);
- #endif
-#endif
-  #ifndef CONFIG_SHRINKER_DEBUG
-	if (rc == 0 && s->name &&
-	    strncmp(s->name, "ldlm_pools", strlen("ldlm_pools")) != 0)
-		rc = shrinker_add_debugfs(shrinker);
-  #endif
-	if (rc) {
-		ll_shrinker_free(shrinker);
-		shrinker = ERR_PTR(rc);
-	}
+
 	return shrinker;
 }
-EXPORT_SYMBOL(ll_shrinker_create);
+EXPORT_SYMBOL(ll_shrinker_alloc);
+
+void ll_shrinker_register(struct shrinker *shrinker)
+{
+#ifndef HAVE_SHRINKER_ALLOC
+#ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
+	struct ll_shrinker *s = container_of(shrinker, struct ll_shrinker,
+					     ll_shrinker);
+#endif
+	int rc;
+
+ #ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
+	rc = register_shrinker(shrinker, "%pV", &s->vaf);
+ #else
+	rc = register_shrinker(shrinker);
+ #endif
+#else
+	shrinker_register(shrinker);
+#endif
+}
+EXPORT_SYMBOL(ll_shrinker_register);
 
 #ifndef CONFIG_SHRINKER_DEBUG
 void shrinker_debugfs_fini(void)

include/lustre_compat/linux/shrinker.h
@@ -8,22 +8,26 @@
 #include <linux/types.h>
 #include <libcfs/libcfs.h>
 
-#ifndef CONFIG_SHRINKER_DEBUG
+#if !defined(CONFIG_SHRINKER_DEBUG) || defined(HAVE_REGISTER_SHRINKER_FORMAT_NAMED)
 struct ll_shrinker {
 	struct shrinker ll_shrinker;
 
-	void *private_data;
-
+  #ifndef CONFIG_SHRINKER_DEBUG
 	int debugfs_id;
 	const char *name;
 	struct dentry *debugfs_entry;
+  #endif
+  #ifdef HAVE_REGISTER_SHRINKER_FORMAT_NAMED
+	struct va_format vaf;
+  #endif
 };
+#else
+#define ll_shrinker	shrinker
 #endif
 
+struct shrinker *ll_shrinker_alloc(unsigned int flags,
+				   const char *fmt, ...);
+void ll_shrinker_register(struct shrinker *shrinker);
 void ll_shrinker_free(struct shrinker *shrinker);
-
-/* allocate and register a shrinker, return should be checked with IS_ERR() */
-struct shrinker *ll_shrinker_create(unsigned int flags,
-				    const char *fmt, ...);
 
 #endif /* _LINUX_SHRINKER_LUSTRE_H */

lustre/ldlm/ldlm_pool.c
@@ -1361,19 +1361,23 @@ int ldlm_pools_init(void)
 #else
 	delay = LDLM_POOL_CLI_DEF_RECALC_PERIOD;
 #endif
-	ldlm_pools_srv_shrinker = ll_shrinker_create(0, "ldlm_pools_server");
+	ldlm_pools_srv_shrinker = ll_shrinker_alloc(0, "ldlm_pools_server");
 	if (IS_ERR(ldlm_pools_srv_shrinker))
 		GOTO(out, rc = PTR_ERR(ldlm_pools_srv_shrinker));
 
 	ldlm_pools_srv_shrinker->count_objects = ldlm_pools_srv_count;
 	ldlm_pools_srv_shrinker->scan_objects = ldlm_pools_srv_scan;
 
-	ldlm_pools_cli_shrinker = ll_shrinker_create(0, "ldlm_pools_client");
+	ll_shrinker_register(ldlm_pools_srv_shrinker);
+
+	ldlm_pools_cli_shrinker = ll_shrinker_alloc(0, "ldlm_pools_client");
 	if (IS_ERR(ldlm_pools_cli_shrinker))
 		GOTO(out_shrinker, rc = PTR_ERR(ldlm_pools_cli_shrinker));
 
 	ldlm_pools_cli_shrinker->count_objects = ldlm_pools_cli_count;
 	ldlm_pools_cli_shrinker->scan_objects = ldlm_pools_cli_scan;
+
+	ll_shrinker_register(ldlm_pools_cli_shrinker);
 
 	schedule_delayed_work(&ldlm_pools_recalc_work, delay);
 	ldlm_pools_init_done = true;

lustre/obdclass/lu_object.c
@@ -2115,7 +2115,7 @@ int lu_global_init(void)
 	 * inode, one for ea. Unfortunately setting this high value results in
 	 * lu_object/inode cache consuming all the memory.
 	 */
-	lu_site_shrinker = ll_shrinker_create(0, "lu_site");
+	lu_site_shrinker = ll_shrinker_alloc(0, "lu_site");
 	if (IS_ERR(lu_site_shrinker)) {
 		result = PTR_ERR(lu_site_shrinker);
 		goto out_env;
@@ -2123,6 +2123,8 @@ int lu_global_init(void)
 
 	lu_site_shrinker->count_objects = lu_cache_shrink_count;
 	lu_site_shrinker->scan_objects = lu_cache_shrink_scan;
+
+	ll_shrinker_register(lu_site_shrinker);
 
 	result = rhashtable_init(&lu_env_rhash, &lu_env_rhash_params);
 

lustre/obdclass/page_pools.c
@@ -1105,12 +1105,14 @@ int obd_pool_init(void)
 			GOTO(fail, rc = -ENOMEM);
 
 		/* Pass pool number as part of pool_shrinker_seeks value */
-		pool->pool_shrinker = ll_shrinker_create(0, "obd_pool");
+		pool->pool_shrinker = ll_shrinker_alloc(0, "obd_pool");
 		if (IS_ERR(pool->pool_shrinker))
 			GOTO(fail, rc = PTR_ERR(pool->pool_shrinker));
 
 		pool->pool_shrinker->count_objects = pool_shrink_count;
 		pool->pool_shrinker->scan_objects = pool_shrink_scan;
+
+		ll_shrinker_register(pool->pool_shrinker);
 
 		pool_shrinkers[pool_order] = pool->pool_shrinker;
 		mutex_init(&pool->add_pages_mutex);

lustre/osc/osc_request.c
@@ -4198,12 +4198,14 @@ static int __init osc_init(void)
 	if (rc)
 		RETURN(rc);
 
-	osc_cache_shrinker = ll_shrinker_create(0, "osc_cache");
+	osc_cache_shrinker = ll_shrinker_alloc(0, "osc_cache");
 	if (IS_ERR(osc_cache_shrinker))
 		GOTO(out_kmem, rc = PTR_ERR(osc_cache_shrinker));
 
 	osc_cache_shrinker->count_objects = osc_cache_shrink_count;
 	osc_cache_shrinker->scan_objects = osc_cache_shrink_scan;
+
+	ll_shrinker_register(osc_cache_shrinker);
 
 	/* This is obviously too much memory, only prevent overflow here */
 	if (osc_reqpool_mem_max >= 1 << 12 || osc_reqpool_mem_max == 0)

commit 2bb6a2a9f2e3459e48f57313522f2b6ad21938fe

LU-17000 compat: Fix resource leak in ll_shrinker_alloc
ll_shrinker is allocated early. If shrinker_alloc() fails
it leaves ll_strinker dangling on exit. This patch fixes
leak on a fail path while allocating 'struct shrinker'

Note that leak only exists after LU-19080 (branches having
LU-18175 but no LU-19080 does not have this leak)

Also, Not introduced by this patch, this patch also
handles case where kvasprintf_const() returns NULL
and then dereferences it. This patch also uses kfree_const()
instead of kfree()

CoverityID: 504221 ("Resource leak")
Fixes: 3ccd941e2de ("LU-19080 compat: match kernel shrinker protocol")
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Ica312c9a86558f70247a206d8733f06e3308f5eb
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67449
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre_compat/mm/shrinker_debug.c
@@ -251,17 +251,25 @@ struct shrinker *ll_shrinker_alloc(unsigned int flags, const char *fmt, ...)
 	if (rc == 0) {
 		const char *name = kvasprintf_const(GFP_KERNEL, fmt, args);
 
-		if (strncmp(name, "ldlm_pools", strlen("ldlm_pools")) != 0)
-			rc = shrinker_add_debugfs(shrinker, name);
-
-		kfree(name);
+		if (name) {
+			if (strncmp(name, "ldlm_pools",
+				    strlen("ldlm_pools")) != 0)
+				rc = shrinker_add_debugfs(shrinker, name);
+			kfree_const(name);
+		} else {
+			rc = -ENOMEM;
+		}
 	}
 #endif
 	va_end(args);
 
 	if (rc < 0) {
-		if (shrinker)
+		if (shrinker) {
 			ll_shrinker_free(shrinker);
+		} else if (s) {
+			LIBCFS_FREE_PRE(s, sizeof(*s), "kfreed");
+			kfree(s);
+		}
 		return ERR_PTR(rc);
 	}
 