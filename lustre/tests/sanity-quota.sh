lustre/lustre-release

commit bce5cb03fb3bf0c292650ec196b6531b663d6a29

LU-18222 quota: add lctl lqa commands
Deploy lctl lqa management commands: lqa new, lqa add, lqa remove,
lqa list and lqa destroy. These commands must be called only on the
MDT0000. Add a new IOCTL to process lqa commands at the QMT. There is
no actual support of LQA yet - QMT only parses lqa arguments. LQA
handlers will be added to the QMT in the next patches.

Signed-off-by: Sergey Cheremencev <scherementsev@ddn.com>
Change-Id: I285b659493c01cbe868720f468a7d7e497d00a01
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60247
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/utils/obd.c
@@ -48,10 +48,6 @@
 
 #include <linux/lnet/nidstr.h>
 #include <linux/lnet/lnetctl.h>
-#ifdef HAVE_SERVER_SUPPPORT
-#include <linux/lustre/lustre_barrier_user.h>
-#include <linux/lustre/lustre_disk.h>
-#endif
 #include <linux/lustre/lustre_cfg.h>
 #include <linux/lustre/lustre_ioctl.h>
 #include <linux/lustre/lustre_ostid.h>
@@ -827,6 +823,27 @@ static int get_mds_device(void)
 
 	do_disconnect(NULL, 1);
 	rc = do_device("mdsioc", mds);
+	if (rc) {
+		errno = ENODEV;
+		return -errno;
+	}
+	return cur_device;
+}
+
+static int get_qmt_device(const char *fsname)
+{
+	static const char postfix[] = "-QMT0000";
+	char qmt[LUSTRE_MAXFSNAME + sizeof(postfix)];
+	int fsname_len, rc;
+
+	fsname_len = strnlen(fsname, LUSTRE_MAXFSNAME);
+	if (fsname_len > LUSTRE_MAXFSNAME || !fsname_len)
+		return -EINVAL;
+
+	memcpy(qmt, fsname, fsname_len);
+	memcpy(qmt + fsname_len, postfix, sizeof(postfix));
+	do_disconnect(NULL, 1);
+	rc = do_device("qmtioc", qmt);
 	if (rc) {
 		errno = ENODEV;
 		return -errno;
@@ -6389,6 +6406,180 @@ out:
 }
 
 #ifdef HAVE_SERVER_SUPPORT
+int lqa_ioctl(enum lqa_cmd_type cmd, char *cmdname, char *fsname, char *lqaname,
+	      __u32 start, __u32 end)
+{
+	struct obd_ioctl_data data;
+	int rc, lqalen;
+	char rawbuf[MAX_IOC_BUFLEN], *buf = rawbuf;
+
+	if (!fsname)
+		return -EFAULT;
+
+	memset(&data, 0, sizeof(data));
+	rc = data.ioc_dev = get_qmt_device(fsname);
+	if (rc < 0) {
+		fprintf(stderr, "error: %s: can not get qmt device\n",
+			jt_cmdname(cmdname));
+		return rc;
+	}
+	memset(buf, 0, sizeof(rawbuf));
+
+	data.ioc_command = cmd;
+	if (lqaname) {
+		lqalen = strnlen(lqaname, LQA_NAME_MAX + 1);
+		if (lqalen > LQA_NAME_MAX) {
+			fprintf(stderr, "error: lqaname `%.*s` exceeds maximum length %u\n",
+				LQA_NAME_MAX, lqaname, LQA_NAME_MAX);
+			return -ENAMETOOLONG;
+		}
+		if (lqalen) {
+			data.ioc_inlbuf1 = lqaname;
+			data.ioc_inllen1 = lqalen + 1;
+
+			if (cmd == LQA_ADD || cmd == LQA_REM) {
+				data.ioc_u32_1 = start;
+				data.ioc_u32_2 = end;
+			}
+		}
+	}
+
+	memset(buf, 0, sizeof(rawbuf));
+	rc = llapi_ioctl_pack(&data, &buf, sizeof(rawbuf));
+	if (rc) {
+		fprintf(stderr, "error: %s: invalid ioctl: %s\n",
+			jt_cmdname(cmdname), strerror(-rc));
+		return rc;
+	}
+
+	rc = l_ioctl(OBD_DEV_ID, OBD_IOC_LQACTL, buf);
+	if (rc) {
+		fprintf(stderr, "error: %s: ioctl: %s\n",
+			jt_cmdname(cmdname), strerror(errno));
+		return -errno;
+	}
+
+	return 0;
+}
+
+static inline int lqa_get_range(const char *range, __u32 *start, __u32 *end)
+{
+	long tmp_start, tmp_end;
+	int rc;
+
+	if (range == NULL)
+		return -EINVAL;
+
+	rc = sscanf(range, "%ld-%ld", &tmp_start, &tmp_end);
+	switch (rc) {
+	case 1:
+		tmp_end = tmp_start;
+		break;
+	case 2:
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	if (tmp_end < tmp_start || tmp_start < 0 || tmp_end < 0)
+		return -EINVAL;
+
+	*start = (__u32)tmp_start;
+	*end = (__u32)tmp_end;
+
+	return 0;
+}
+
+static inline int lqa_cmd(int argc, char **argv, enum lqa_cmd_type cmd)
+{
+	char *lqaname = NULL;
+	char *fsname = NULL;
+	__u32 start = 0, end = 0;
+	struct option long_opts[] = {
+		{ .val = 'h', .name = "help", .has_arg = no_argument },
+		{ .val = 'f', .name = "fsname", .has_arg = required_argument },
+		{ .val = 'n', .name = "name", .has_arg = required_argument },
+		{ .val = 'r', .name = "range", .has_arg = required_argument },
+		{ .name = NULL },
+	};
+	bool range_defined = false;
+	int rc, c;
+
+	while ((c = getopt_long(argc, argv, "hf:l:r:",
+				long_opts, NULL)) != -1) {
+		switch (c) {
+		case 'f':
+			fsname = optarg;
+			if (strnlen(fsname, LUSTRE_MAXFSNAME + 1) >
+			    LUSTRE_MAXFSNAME) {
+				fprintf(stderr, "fsname is too long\n");
+				return -ENAMETOOLONG;
+			}
+			break;
+		case 'n':
+			lqaname = optarg;
+			if (strnlen(lqaname, LQA_NAME_MAX + 1) > LQA_NAME_MAX) {
+				fprintf(stderr, "lqaname is too long\n");
+				return -ENAMETOOLONG;
+			}
+			break;
+		case 'r':
+			if (!(cmd == LQA_ADD || cmd == LQA_REM))
+				return CMD_HELP;
+			if (lqa_get_range(optarg, &start, &end)) {
+				fprintf(stderr, "range is insane\n");
+				return -EINVAL;
+			}
+			range_defined = true;
+			break;
+		case 'h':
+			fallthrough;
+		default:
+			return CMD_HELP;
+		}
+	}
+
+	if (!fsname)
+		return CMD_HELP;
+
+	if (!lqaname && cmd != LQA_LIST)
+		return CMD_HELP;
+
+	if (!range_defined && (cmd == LQA_ADD || cmd == LQA_REM))
+		return CMD_HELP;
+
+	rc = lqa_ioctl(cmd, argv[0], fsname, lqaname, start, end);
+	if (rc < 0)
+		fprintf(stderr, "%s: %s\n", argv[0], strerror(-rc));
+
+	return rc;
+}
+
+int lctl_lqa_new(int argc, char **argv)
+{
+	return lqa_cmd(argc, argv, LQA_NEW);
+}
+
+int lctl_lqa_add(int argc, char **argv)
+{
+	return lqa_cmd(argc, argv, LQA_ADD);
+}
+
+int lctl_lqa_rem(int argc, char **argv)
+{
+	return lqa_cmd(argc, argv, LQA_REM);
+}
+
+int lctl_lqa_del(int argc, char **argv)
+{
+	return lqa_cmd(argc, argv, LQA_DEL);
+}
+
+int lctl_lqa_list(int argc, char **argv)
+{
+	return lqa_cmd(argc, argv, LQA_LIST);
+}
+
 static const char *barrier_status2name(enum barrier_status status)
 {
 	switch (status) {

lustre/tests/sanity-quota.sh
@@ -7463,6 +7463,53 @@ test_96() {
 }
 run_test 96 "quota grant should be released when big files are deleted"
 
+LQA_NEW="do_facet mds1 $LCTL lqa new --fsname $FSNAME"
+LQA_ADD="do_facet mds1 $LCTL lqa add --fsname $FSNAME"
+LQA_REMOVE="do_facet mds1 $LCTL lqa remove --fsname $FSNAME"
+LQA_DESTROY="do_facet mds1 $LCTL lqa destroy --fsname $FSNAME"
+LQA_LIST="do_facet mds1 $LCTL lqa list --fsname $FSNAME"
+
+test_97a ()
+{
+	local lqa="lqa1"
+	local longstr="0123456789123456789"
+
+	(( $MDS1_VERSION >= $(version_code 2.16.58) )) ||
+		skip "need MDS >= 2.16.58 to support lctl lqa commands"
+
+	$LQA_NEW && error "lqa_new succeeded with no lqa"
+#define LQA_NAME_MAX 15 /* Maximum lqa name length */
+	$LQA_NEW --name $longstr && error "lqa max name length is 16"
+	$LQA_NEW --name $lqa || error "cannot create $lqa"
+	stack_trap "$LQA_DESTROY --name $lqa || true"
+
+	$LQA_ADD && error "lqa add succeeded with no --name"
+	$LQA_ADD --name $lqa && error "lqa add succeeded wit no range"
+	$LQA_ADD --name $lqa --range -10 && error "lqa add wrong format 1"
+	$LQA_ADD --name $lqa --range 10-9 && error "lqa add wrong format 2"
+	$LQA_ADD --name $lqa --range 10--9 && error "lqa add wrong format 3"
+	$LQA_ADD --name $lqa --range 10 || error "lqa failed to add [10]"
+	$LQA_ADD --name $lqa --range 11-20 || error "lqa failed to add [10-20]"
+
+	$LQA_LIST || error "lqa list failed with no --fsname"
+	$LQA_LIST --name $lqa || error "lqa list failed to show $lqa ranges"
+
+	$LQA_REMOVE && error "lqa remove succeeded with no --name"
+	$LQA_REMOVE --name $lqa && error "lqa remove succeeded with no --range"
+	$LQA_REMOVE --name $lqa --range -10 && error "lqa remove wrong format 1"
+	$LQA_REMOVE --name $lqa --range 10-9 &&
+		error "lqa remove wrong format 2"
+	$LQA_REMOVE --name $lqa --range 10--9 &&
+		error "lqa remove wrong format 3"
+	$LQA_REMOVE --name $lqa --range 10 || error "lqa failed to remove 10"
+	$LQA_REMOVE --name $lqa --range 11-20 ||
+		error "lqa failed to remove 10-20"
+
+	$LQA_DESTROY && error "lqa destroy succeeded with no --name"
+	$LQA_DESTROY --name $lqa || error "cannot destroy $lqa"
+}
+run_test 97a "LQA control commands"
+
 quota_fini()
 {
 	do_nodes $(comma_list $(nodes_list)) \

lustre/include/uapi/linux/lustre/lustre_ioctl.h
@@ -159,6 +159,7 @@ enum obd_abort_recovery_flags {
 #define OBD_IOC_FID2PATH	_IOWR('f', 150, OBD_IOC_DATA_TYPE)
 /*	lustre/lustre_user.h	151-159 */
 #define OBD_IOC_QUOTACTL	_IOWR('f', 162, struct if_quotactl)
+#define OBD_IOC_LQACTL		_IOWR('f', 162, struct obd_ioctl_data)
 /*	lustre/lustre_user.h	163-176 */
 #define OBD_IOC_CHANGELOG_REG	_IOW('f', 177, struct obd_ioctl_data)
 #define OBD_IOC_CHANGELOG_DEREG	_IOW('f', 178, struct obd_ioctl_data)

lustre/include/uapi/linux/lustre/lustre_user.h
@@ -1678,6 +1678,16 @@ struct if_quotactl {
 #define qc_allquota_qid_end	qc_dqblk.dqb_btime
 #define qc_allquota_mark	qc_dqblk.dqb_itime
 
+enum lqa_cmd_type {
+	LQA_NEW = 1, /* create an LQA by name */
+	LQA_ADD = 2, /* add a range to LQA */
+	LQA_REM = 3, /* remove a range from an LQA */
+	LQA_DEL = 4, /* destroy an LQA */
+	LQA_LIST = 5 /* list ranges per LQA */
+};
+
+#define LQA_NAME_MAX LOV_MAXPOOLNAME /* Maximum lqa name length */
+
 /* swap layout flags */
 #define SWAP_LAYOUTS_CHECK_DV1		(1 << 0)
 #define SWAP_LAYOUTS_CHECK_DV2		(1 << 1)

lustre/quota/qmt_dev.c
@@ -423,6 +423,7 @@ static const struct obd_ops qmt_obd_ops = {
 	.o_pool_rem	= qmt_pool_rem,
 	.o_pool_add	= qmt_pool_add,
 	.o_pool_del	= qmt_pool_del,
+	.o_iocontrol	= qmt_iocontrol,
 };
 
 /*

lustre/quota/qmt_handler.c
@@ -1325,6 +1325,87 @@ static int qmt_dqacq(const struct lu_env *env, struct lu_device *ld,
 	RETURN(rc);
 }
 
+static int lqa_parse_args(struct obd_device *obd, struct obd_ioctl_data *data,
+			  char **lqa, __u32 *start, __u32 *end)
+{
+	int lqalen;
+
+	if (data->ioc_inlbuf1 && data->ioc_inllen1 &&
+	    data->ioc_inllen1 <= LQA_NAME_MAX + 1)
+		*lqa = data->ioc_inlbuf1;
+
+	if (!*lqa)
+		return 0;
+
+	lqalen = strnlen(*lqa, LQA_NAME_MAX + 1);
+	if (!lqalen || lqalen == LQA_NAME_MAX + 1) {
+		CERROR("%s: lqa name is larger than maximum %d: rc = %d\n",
+		       obd->obd_name, LQA_NAME_MAX, -EINVAL);
+		return -EINVAL;
+	}
+
+	if (data->ioc_command == LQA_ADD || data->ioc_command == LQA_REM) {
+		*start = (__u32)data->ioc_u32_1;
+		*end = (__u32)data->ioc_u32_2;
+		if (*end < *start) {
+			CERROR("%s: lqa:%s range has end %d < start %d: rc = %d\n",
+			       obd->obd_name, *lqa, *end, *start, -EINVAL);
+			return -EINVAL;
+		} else {
+			CDEBUG(D_QUOTA, "lqa:%s [%u-%u]\n", *lqa, *start, *end);
+		}
+	}
+
+	return 0;
+}
+
+int qmt_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
+		  void *karg, void __user *uarg)
+{
+	struct obd_device *obd = exp->exp_obd;
+	struct obd_ioctl_data *data;
+	char *lqa = NULL;
+	__u32 start, end;
+	int rc = 0;
+
+	ENTRY;
+	CDEBUG(D_IOCTL, "%s: cmd=%x len=%u karg=%pK uarg=%pK\n",
+	       obd->obd_name, cmd, len, karg, uarg);
+	data = karg;
+
+	/* qmt only supports LQA ioctls, for now */
+	if (cmd != OBD_IOC_LQACTL)
+		RETURN(-EINVAL);
+
+	start = end = 0;
+	rc = lqa_parse_args(obd, data, &lqa, &start, &end);
+	if (rc)
+		RETURN(rc);
+
+	switch (data->ioc_command) {
+	case LQA_NEW:
+		CDEBUG(D_QUOTA, "LQA_NEW lqa:%s\n", lqa);
+		break;
+	case LQA_ADD:
+		CDEBUG(D_QUOTA, "LQA_ADD lqa:%s, [%u-%u]\n", lqa, start, end);
+		break;
+	case LQA_REM:
+		CDEBUG(D_QUOTA, "LQA_REM lqa:%s [%u-%u]\n", lqa, start, end);
+		break;
+	case LQA_DEL:
+		CDEBUG(D_QUOTA, "LQA_DEL, lqa:%s\n", lqa);
+		break;
+	case LQA_LIST:
+		CDEBUG(D_QUOTA, "LQA_LIST, lqa:%s\n", lqa);
+		break;
+	default:
+		rc = OBD_IOC_ERROR(obd->obd_name, data->ioc_command,
+				   "unrecognized", -ENOTTY);
+	}
+
+	RETURN(rc);
+}
+
 /* Vector of quota request handlers. This vector is used by the MDT to forward
  * requests to the quota master. */
 struct qmt_handlers qmt_hdls = {

lustre/quota/qmt_internal.h
@@ -440,6 +440,8 @@ int qmt_pool_new(struct obd_device *obd, char *poolname);
 int qmt_pool_add(struct obd_device *obd, char *poolname, char *ostname);
 int qmt_pool_rem(struct obd_device *obd, char *poolname, char *ostname);
 int qmt_pool_del(struct obd_device *obd, char *poolname);
+int qmt_iocontrol(unsigned int cmd, struct obd_export *exp, int len,
+		  void *karg, void __user *uarg);
 
 #define qmt_sarr_read_down(qpi) down_read(&qpi->qpi_sarr.osts.op_rw_sem)
 #define qmt_sarr_read_up(qpi) up_read(&qpi->qpi_sarr.osts.op_rw_sem)

lustre/utils/lctl.c
@@ -336,6 +336,28 @@ command_t lfsck_cmdlist[] = {
 	{ .pc_help = NULL }
 };
 JT_SUBCMD(lfsck);
+
+/**
+ * command_t lqa_cmdlist - lctl lqa commands.
+ */
+command_t lqa_cmdlist[] = {
+	{.pc_name = "new", .pc_func = lctl_lqa_new,
+	 .pc_help = "create new Lustre Quota ID Aggregate (LQA)\n"
+	 "usage: lqa new --fsname FSNAME --name LQANAME"},
+	{.pc_name = "add", .pc_func = lctl_lqa_add,
+	 .pc_help = "add range of quota IDs to the named LQA\n"
+	 "usage: lqa add --fsname FSNAME --name LQANAME --range ID_RANGE"},
+	{.pc_name = "remove", .pc_func = lctl_lqa_rem,
+	 .pc_help = "remove range of quota IDs from the named LQA\n"
+	 "usage: lqa remove --fsname FSNAME --name LQANAME --range ID_RANGE"},
+	{.pc_name = "destroy", .pc_func = lctl_lqa_del,
+	 .pc_help = "destroy the named Lustre Quota ID Aggregate\n"
+	 "usage: lqa destroy --fsname FSNAME --name LQANAME"},
+	{.pc_name = "list", .pc_func = lctl_lqa_list,
+	 .pc_help = "list LQAs, either all for FSNAME or the details for LQANAME\n"
+	 "usage: lqa list --fsname FSNAME [--name LQANAME]"}
+};
+JT_SUBCMD(lqa);
 #endif
 
 command_t cmdlist[] = {
@@ -541,6 +563,24 @@ command_t cmdlist[] = {
 	 "usage: pool_list  <fsname>[.<poolname>] | <pathname>"},
 
 #ifdef HAVE_SERVER_SUPPORT
+	/* LQA commands */
+	{"===  LQA ==", NULL, 0, "lqa management"},
+	{"lqa_new", lctl_lqa_new, 0,
+	 "create new Lustre Quota ID Aggregate (LQA)\n"
+	 "usage: lqa new --fsname FSNAME --name LQANAME"},
+	{"lqa_add", lctl_lqa_add, 0,
+	 "add range of quota IDs to the named LQA\n"
+	 "usage: lqa add --fsname FSNAME --name LQANAME --range ID_RANGE"},
+	{"lqa_remove", lctl_lqa_rem, 0,
+	 "remove range of quota IDs from the named LQA\n"
+	 "usage: lqa remove --fsname FSNAME --name LQANAME --range ID_RANGE"},
+	{"lqa_destroy", lctl_lqa_del, 0,
+	 "destroy the named Lustre Quota ID Aggregate\n"
+	 "usage: lqa destroy --fsname FSNAME --name LQANAME"},
+	{"lqa_list", lctl_lqa_list, 0,
+	 "list LQAs, either all for FSNAME or the details for LQANAME\n"
+	 "usage: lqa list --fsname FSNAME [--name LQANAME]"},
+	{"lqa", jt_lqa, lqa_cmdlist, ""},
 	/* Barrier commands */
 	{"===  Barrier ==", NULL, 0, "barrier management"},
 	{"barrier_freeze", jt_barrier_freeze, 0,

commit 881a3641dc2157a18d431ceb3b290c2e7e54bbaf

LU-20456 utils: Reject invalid lqa ranges
Check lqa ranges for invalid characters and reject
them rather than silently accepting them.

Test-Parameters: trivial testlist=sanity-quota
Fixes: bce5cb03fb3b ("LU-18222 quota: add lctl lqa commands")
Signed-off-by: Xiao Yang <xyang@ddn.com>
Change-Id: Ib6fbc5f6e6b9efde9ba5aff687b32020a80af671
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67114
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>

lustre/utils/obd.c
@@ -6592,20 +6592,26 @@ int lqa_ioctl(enum lqa_cmd_type cmd, char *cmdname, char *fsname, char *lqaname,
 
 static inline int lqa_get_range(const char *range, __u32 *start, __u32 *end)
 {
-	long tmp_start, tmp_end;
-	int rc;
+	long long tmp_start, tmp_end;
+	char *endptr;
 
 	if (range == NULL)
 		return -EINVAL;
 
-	rc = sscanf(range, "%ld-%ld", &tmp_start, &tmp_end);
-	switch (rc) {
-	case 1:
+	errno = 0;
+	tmp_start = strtoll(range, &endptr, 10);
+	if (endptr == range || errno == ERANGE)
+		return -EINVAL;
+
+	if (*endptr == '\0') {
 		tmp_end = tmp_start;
-		break;
-	case 2:
-		break;
-	default:
+	} else if (*endptr == '-') {
+		range = endptr + 1;
+		errno = 0;
+		tmp_end = strtoll(range, &endptr, 10);
+		if (endptr == range || *endptr != '\0' || errno == ERANGE)
+			return -EINVAL;
+	} else {
 		return -EINVAL;
 	}
 

lustre/tests/sanity-quota.sh
@@ -7908,6 +7908,34 @@ test_97d() {
 }
 run_test 97d "LQA disk persistence using standard commands"
 
+test_97e()
+{
+	local lqa="lqa1"
+
+	(( $MDS1_VERSION >= $(version_code 2.17.56) )) ||
+		skip "need MDS >= 2.17.56 to reject invalid lqa ranges"
+
+	$LQA_NEW --name $lqa || error "cannot create $lqa"
+	stack_trap "$LQA_DESTROY --name $lqa || true"
+
+	$LQA_ADD --name $lqa --range 10a &&
+		error "lqa add succeeded with invalid range 10a"
+	$LQA_ADD --name $lqa --range 10-11b &&
+		error "lqa add succeeded with invalid range 10-11b"
+	$LQA_ADD --name $lqa --range 10- &&
+		error "lqa add succeeded with invalid range 10-"
+
+	$LQA_REMOVE --name $lqa --range 10a &&
+		error "lqa remove succeeded with invalid range 10a"
+	$LQA_REMOVE --name $lqa --range 10-11b &&
+		error "lqa remove succeeded with invalid range 10-11b"
+	$LQA_REMOVE --name $lqa --range 10- &&
+		error "lqa remove succeeded with invalid range 10-"
+
+	$LQA_DESTROY --name $lqa || error "cannot destroy $lqa"
+}
+run_test 97e "LQA add/remove should reject invalid ranges"
+
 test_98() {
 	(( $MDSCOUNT >= 2 )) || skip "needs >= 2 MDTs"
 