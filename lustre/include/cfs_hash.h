lustre/lustre-release

commit 61296c3256b905062af4ffb21eb16a6c9d2190f9

LU-8130 obdclass: convert nid_stats to rhashtable
Linux has a resizeable hashtable implementation in lib,
so we should use that instead of having one in libcfs.
The Linux version has better performance.

This patch converts the nid stats hash to use rhashtable.

Change-Id: I3bf5db793e67fb968281ffe767e79529e0639711
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/41092
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>

lustre/include/cfs_hash.h
@@ -778,11 +778,6 @@ __cfs_hash_set_theta(struct cfs_hash *hs, int min, int max)
 	hs->hs_max_theta = (__u16)max;
 }
 
-/* Generic debug formatting routines mainly for proc handler */
-struct seq_file;
-void cfs_hash_debug_header(struct seq_file *m);
-void cfs_hash_debug_str(struct cfs_hash *hs, struct seq_file *m);
-
 /* Generic djb2 hash algorithm for character arrays. */
 static inline unsigned
 cfs_hash_djb2_hash(const void *key, size_t size, const unsigned int bits)

lustre/include/lustre_export.h
@@ -124,7 +124,7 @@ struct mgs_export_data {
  */
 struct nid_stat {
 	struct lnet_nid		 nid;
-	struct hlist_node	 nid_hash;
+	struct rhlist_head	 nid_hash;
 	struct list_head	 nid_list;
 	struct obd_device       *nid_obd;
 	struct dentry		*nid_debugfs;

lustre/include/obd.h
@@ -664,7 +664,7 @@ struct obd_device {
 	/* nid-export hash body */
 	struct rhltable			obd_nid_hash;
 	/* nid stats body */
-	struct cfs_hash             *obd_nid_stats_hash;
+	struct rhltable			obd_nid_stats_hash;
 	/* client_generation-export hash body */
 	struct cfs_hash		    *obd_gen_hash;
 	struct list_head	obd_nid_stats;
@@ -817,6 +817,9 @@ int obd_nid_export_for_each(struct obd_device *obd, struct lnet_nid *nid,
 			    void *data);
 int obd_nid_add(struct obd_device *obd, struct obd_export *exp);
 void obd_nid_del(struct obd_device *obd, struct obd_export *exp);
+
+struct nid_stat *obd_nid_stats_get(struct obd_device *obd, struct nid_stat *ns);
+void obd_nid_stats_put(struct obd_device *obd, struct nid_stat *ns);
 
 /* both client and MDT recovery are aborted, or MDT is stopping  */
 static inline bool obd_recovery_abort(struct obd_device *obd)

lustre/include/obd_support.h
@@ -58,9 +58,6 @@ extern bool obd_enable_health_write;
 extern bool obd_enable_fname_encoding;
 
 /* Some hash init argument constants */
-#define HASH_NID_STATS_BKT_BITS 5
-#define HASH_NID_STATS_CUR_BITS 7
-#define HASH_NID_STATS_MAX_BITS 12
 #define HASH_GEN_BKT_BITS 5
 #define HASH_GEN_CUR_BITS 7
 #define HASH_GEN_MAX_BITS 12

lustre/obdclass/hash.c
@@ -2043,92 +2043,3 @@ void cfs_hash_rehash_key(struct cfs_hash *hs, const void *old_key,
         cfs_hash_unlock(hs, 0);
 }
 EXPORT_SYMBOL(cfs_hash_rehash_key);
-
-void cfs_hash_debug_header(struct seq_file *m)
-{
-	seq_printf(m, "%-*s   cur   min   max theta t-min t-max flags rehash   count  maxdep maxdepb distribution\n",
-		   CFS_HASH_BIGNAME_LEN, "name");
-}
-EXPORT_SYMBOL(cfs_hash_debug_header);
-
-static struct cfs_hash_bucket **
-cfs_hash_full_bkts(struct cfs_hash *hs)
-{
-        /* NB: caller should hold hs->hs_rwlock if REHASH is set */
-        if (hs->hs_rehash_buckets == NULL)
-                return hs->hs_buckets;
-
-        LASSERT(hs->hs_rehash_bits != 0);
-        return hs->hs_rehash_bits > hs->hs_cur_bits ?
-               hs->hs_rehash_buckets : hs->hs_buckets;
-}
-
-static unsigned int
-cfs_hash_full_nbkt(struct cfs_hash *hs)
-{
-        /* NB: caller should hold hs->hs_rwlock if REHASH is set */
-        if (hs->hs_rehash_buckets == NULL)
-                return CFS_HASH_NBKT(hs);
-
-        LASSERT(hs->hs_rehash_bits != 0);
-        return hs->hs_rehash_bits > hs->hs_cur_bits ?
-               CFS_HASH_RH_NBKT(hs) : CFS_HASH_NBKT(hs);
-}
-
-void cfs_hash_debug_str(struct cfs_hash *hs, struct seq_file *m)
-{
-	int dist[8] = { 0, };
-	int maxdep = -1;
-	int maxdepb = -1;
-	int total = 0;
-	int theta;
-	int i;
-
-	cfs_hash_lock(hs, 0);
-	theta = __cfs_hash_theta(hs);
-
-	seq_printf(m, "%-*s %5d %5d %5d %d.%03d %d.%03d %d.%03d  0x%02x %6d ",
-		   CFS_HASH_BIGNAME_LEN, hs->hs_name,
-		   1 << hs->hs_cur_bits, 1 << hs->hs_min_bits,
-		   1 << hs->hs_max_bits,
-		   __cfs_hash_theta_int(theta), __cfs_hash_theta_frac(theta),
-		   __cfs_hash_theta_int(hs->hs_min_theta),
-		   __cfs_hash_theta_frac(hs->hs_min_theta),
-		   __cfs_hash_theta_int(hs->hs_max_theta),
-		   __cfs_hash_theta_frac(hs->hs_max_theta),
-		   hs->hs_flags, hs->hs_rehash_count);
-
-	/*
-	 * The distribution is a summary of the chained hash depth in
-	 * each of the libcfs hash buckets.  Each buckets hsb_count is
-	 * divided by the hash theta value and used to generate a
-	 * histogram of the hash distribution.  A uniform hash will
-	 * result in all hash buckets being close to the average thus
-	 * only the first few entries in the histogram will be non-zero.
-	 * If you hash function results in a non-uniform hash the will
-	 * be observable by outlier bucks in the distribution histogram.
-	 *
-	 * Uniform hash distribution:		128/128/0/0/0/0/0/0
-	 * Non-Uniform hash distribution:	128/125/0/0/0/0/2/1
-	 */
-	for (i = 0; i < cfs_hash_full_nbkt(hs); i++) {
-		struct cfs_hash_bd bd;
-
-		bd.bd_bucket = cfs_hash_full_bkts(hs)[i];
-		cfs_hash_bd_lock(hs, &bd, 0);
-		if (maxdep < bd.bd_bucket->hsb_depmax) {
-			maxdep  = bd.bd_bucket->hsb_depmax;
-			maxdepb = ffz(~maxdep);
-		}
-		total += bd.bd_bucket->hsb_count;
-		dist[min(fls(bd.bd_bucket->hsb_count / max(theta, 1)), 7)]++;
-		cfs_hash_bd_unlock(hs, &bd, 0);
-	}
-
-	seq_printf(m, "%7d %7d %7d ", total, maxdep, maxdepb);
-	for (i = 0; i < 8; i++)
-		seq_printf(m, "%d%c",  dist[i], (i == 7) ? '\n' : '/');
-
-	cfs_hash_unlock(hs, 0);
-}
-EXPORT_SYMBOL(cfs_hash_debug_str);

lustre/obdclass/lprocfs_status_server.c
@@ -315,17 +315,16 @@ static void lprocfs_free_client_stats(struct nid_stat *client_stat)
 
 void lprocfs_free_per_client_stats(struct obd_device *obd)
 {
-	struct cfs_hash *hash = obd->obd_nid_stats_hash;
 	struct nid_stat *stat;
-	ENTRY;
 
+	ENTRY;
 	/* we need extra list - because hash_exit called to early */
 	/* not need locking because all clients is died */
 	while (!list_empty(&obd->obd_nid_stats)) {
 		stat = list_first_entry(&obd->obd_nid_stats,
 					struct nid_stat, nid_list);
 		list_del_init(&stat->nid_list);
-		cfs_hash_del(hash, &stat->nid, &stat->nid_hash);
+		obd_nid_stats_put(obd, stat);
 		lprocfs_free_client_stats(stat);
 	}
 	EXIT;
@@ -432,7 +431,7 @@ ldebugfs_exp_print_hash_seq(struct obd_export *exp, void *cb_data)
 	struct seq_file *m = cb_data;
 
 	if (exp->exp_lock_hash != NULL) {
-		seq_printf(m, "%-*s   cur   min        max theta t-min t-max flags rehash   count distribution\n",
+		seq_printf(m, "%-*s   cur   min        max theta t-min t-max flags  rehash   count  maxdep distribution\n",
 			   HASH_NAME_LEN, "name");
 		ldebugfs_rhash_seq_show("NID_HASH", &obd->obd_nid_hash.ht, m);
 	}
@@ -505,8 +504,8 @@ EXPORT_SYMBOL(lprocfs_nid_stats_clear_seq_show);
 static int ldebugfs_nid_stats_clear_write_cb(void *obj, void *data)
 {
 	struct nid_stat *stat = obj;
-	ENTRY;
 
+	ENTRY;
 	CDEBUG(D_INFO, "refcnt %d\n", atomic_read(&stat->nid_exp_ref_count));
 	if (atomic_read(&stat->nid_exp_ref_count) == 1) {
 		/* object has only hash references. */
@@ -515,7 +514,7 @@ static int ldebugfs_nid_stats_clear_write_cb(void *obj, void *data)
 		spin_unlock(&stat->nid_obd->obd_nid_lock);
 		RETURN(1);
 	}
-	/* we has reference to object - only clear data*/
+	/* we has reference to object - only clear data */
 	if (stat->nid_stats)
 		lprocfs_stats_clear(stat->nid_stats);
 
@@ -529,10 +528,23 @@ ldebugfs_nid_stats_clear_seq_write(struct file *file, const char __user *buffer,
 	struct seq_file *m = file->private_data;
 	struct obd_device *obd = m->private;
 	struct nid_stat *client_stat;
+	struct rhashtable_iter iter;
 	LIST_HEAD(free_list);
 
-	cfs_hash_cond_del(obd->obd_nid_stats_hash,
-			  ldebugfs_nid_stats_clear_write_cb, &free_list);
+	rhashtable_walk_enter(&obd->obd_nid_stats_hash.ht, &iter);
+	rhashtable_walk_start(&iter);
+	while ((client_stat = rhashtable_walk_next(&iter)) != NULL) {
+		if (IS_ERR(client_stat)) {
+			if (PTR_ERR(client_stat) == -EAGAIN)
+				continue;
+			break;
+		}
+
+		if (ldebugfs_nid_stats_clear_write_cb(client_stat, &free_list))
+			obd_nid_stats_put(obd, client_stat);
+	}
+	rhashtable_walk_stop(&iter);
+	rhashtable_walk_exit(&iter);
 
 	while (!list_empty(&free_list)) {
 		client_stat = list_first_entry(&free_list, struct nid_stat,
@@ -634,8 +646,7 @@ int lprocfs_exp_setup(struct obd_export *exp, struct lnet_nid *nid)
 	int rc = 0;
 
 	ENTRY;
-	if (!exp || !exp->exp_obd || !exp->exp_obd->obd_debugfs_exports ||
-	    !exp->exp_obd->obd_nid_stats_hash)
+	if (!exp || !exp->exp_obd || !exp->exp_obd->obd_debugfs_exports)
 		RETURN(-EINVAL);
 
 	/* not test against zero because eric say:
@@ -655,8 +666,6 @@ int lprocfs_exp_setup(struct obd_export *exp, struct lnet_nid *nid)
 
 	obd = exp->exp_obd;
 
-	CDEBUG(D_CONFIG, "using hash %p\n", obd->obd_nid_stats_hash);
-
 	OBD_ALLOC_PTR(new_stat);
 	if (new_stat == NULL)
 		RETURN(-ENOMEM);
@@ -666,9 +675,13 @@ int lprocfs_exp_setup(struct obd_export *exp, struct lnet_nid *nid)
 	/* we need set default refcount to 1 to balance obd_disconnect */
 	atomic_set(&new_stat->nid_exp_ref_count, 1);
 
-	old_stat = cfs_hash_findadd_unique(obd->obd_nid_stats_hash,
-					   &new_stat->nid,
-					   &new_stat->nid_hash);
+	old_stat = obd_nid_stats_get(obd, new_stat);
+	/* old_stat ERR pointer means we failed to add new_stat
+	 * to the hash
+	 */
+	if (IS_ERR(old_stat))
+		GOTO(destroy_new, rc = PTR_ERR(old_stat));
+
 	CDEBUG(D_INFO, "Found stats %p for nid %s - ref %d\n",
 	       old_stat, nidstr, atomic_read(&old_stat->nid_exp_ref_count));
 
@@ -923,9 +936,8 @@ int lprocfs_hash_seq_show(struct seq_file *m, void *data)
 		   HASH_NAME_LEN, "name");
 	ldebugfs_rhash_seq_show("UUID_HASH", &obd->obd_uuid_hash, m);
 	ldebugfs_rhash_seq_show("NID_HASH", &obd->obd_nid_hash.ht, m);
+	ldebugfs_rhash_seq_show("NID_STATS", &obd->obd_nid_stats_hash.ht, m);
 
-	cfs_hash_debug_header(m);
-	cfs_hash_debug_str(obd->obd_nid_stats_hash, m);
 	return 0;
 }
 EXPORT_SYMBOL(lprocfs_hash_seq_show);

lustre/obdclass/obd_config.c
@@ -15,6 +15,7 @@
 
 #define DEBUG_SUBSYSTEM S_CLASS
 
+#include <linux/delay.h>
 #include <linux/kobject.h>
 #include <linux/string.h>
 
@@ -32,7 +33,6 @@
 #include "llog_internal.h"
 
 #ifdef HAVE_SERVER_SUPPORT
-static struct cfs_hash_ops nid_stat_hash_ops;
 static struct cfs_hash_ops gen_hash_ops;
 #endif /* HAVE_SERVER_SUPPORT */
 
@@ -240,6 +240,93 @@ out_unlock:
 	return ret;
 }
 EXPORT_SYMBOL(obd_nid_export_for_each);
+
+/*
+ * nid<->nidstats hash operations
+ */
+static u32 nid_stats_keyhash(const void *data, u32 key_len, u32 seed)
+{
+	return jhash2(data, key_len / sizeof(u32), seed);
+}
+
+static int
+nid_stats_keycmp(struct rhashtable_compare_arg *arg, const void *obj)
+{
+	const struct lnet_nid *nid = arg->key;
+	const struct nid_stat *ns = obj;
+
+	return nid_same(&ns->nid, (struct lnet_nid *)nid) ? 0 : -ESRCH;
+}
+
+static void
+nid_stats_exit(void *vdata, void *data)
+{
+	struct nid_stat *ns = vdata;
+
+	nidstat_putref(ns);
+}
+
+static const struct rhashtable_params nid_stats_hash_params = {
+	.key_len		= sizeof(struct lnet_nid),
+	.key_offset		= offsetof(struct nid_stat, nid),
+	.head_offset		= offsetof(struct nid_stat, nid_hash),
+	.hashfn			= nid_stats_keyhash,
+	.obj_cmpfn		= nid_stats_keycmp,
+	.automatic_shrinking	= true,
+};
+
+struct nid_stat *obd_nid_stats_get(struct obd_device *obd, struct nid_stat *ns)
+{
+	struct rhlist_head *exports, *pos;
+	struct nid_stat *ns2 = NULL, *tmp;
+
+	rcu_read_lock();
+	exports = rhltable_lookup(&obd->obd_nid_stats_hash, &ns->nid,
+				  nid_stats_hash_params);
+	if (!exports)
+		goto insert_key;
+
+	rhl_for_each_entry_rcu(tmp, pos, exports, nid_hash) {
+		if (obd == tmp->nid_obd) {
+			nidstat_getref(tmp);
+			ns2 = tmp;
+			goto unlock;
+		}
+	}
+
+	if (!ns2) {
+		int rc;
+insert_key:
+		nidstat_getref(ns);
+		rc = rhltable_insert_key(&obd->obd_nid_stats_hash, &ns->nid,
+					 &ns->nid_hash, nid_stats_hash_params);
+		if (rc < 0) {
+			nidstat_putref(ns);
+			ns2 = ERR_PTR(rc);
+		} else {
+			ns2 = ns;
+		}
+	}
+unlock:
+	rcu_read_unlock();
+
+	return ns2;
+}
+EXPORT_SYMBOL(obd_nid_stats_get);
+
+void obd_nid_stats_put(struct obd_device *obd, struct nid_stat *ns)
+{
+	int rc;
+
+	rcu_read_lock();
+	rc = rhltable_remove(&obd->obd_nid_stats_hash, &ns->nid_hash,
+			     nid_stats_hash_params);
+	if (rc == 0)
+		nidstat_putref(ns);
+	rcu_read_unlock();
+}
+EXPORT_SYMBOL(obd_nid_stats_put);
+
 #endif /* HAVE_SERVER_SUPPORT */
 
 /*********** string parsing utils *********/
@@ -703,7 +790,6 @@ int class_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 	 * other fns check that status, and we're not actually set up yet.
 	 */
 	obd->obd_starting = 1;
-	obd->obd_nid_stats_hash = NULL;
 	obd->obd_gen_hash = NULL;
 	spin_unlock(&obd->obd_dev_lock);
 
@@ -719,15 +805,8 @@ int class_setup(struct obd_device *obd, struct lustre_cfg *lcfg)
 		GOTO(err_uuid_hash, err = -ENOMEM);
 
 	/* create a nid-stats lustre hash */
-	obd->obd_nid_stats_hash = cfs_hash_create("NID_STATS",
-						  HASH_NID_STATS_CUR_BITS,
-						  HASH_NID_STATS_MAX_BITS,
-						  HASH_NID_STATS_BKT_BITS, 0,
-						  CFS_HASH_MIN_THETA,
-						  CFS_HASH_MAX_THETA,
-						  &nid_stat_hash_ops,
-						  CFS_HASH_DEFAULT);
-	if (!obd->obd_nid_stats_hash)
+	err = rhltable_init(&obd->obd_nid_stats_hash, &nid_stats_hash_params);
+	if (err)
 		GOTO(err_nid_hash, err = -ENOMEM);
 
 	/* create a client_generation-export lustre hash */
@@ -769,10 +848,7 @@ err_gen_hash:
 		obd->obd_gen_hash = NULL;
 	}
 err_nid_stats_hash:
-	if (obd->obd_nid_stats_hash) {
-		cfs_hash_putref(obd->obd_nid_stats_hash);
-		obd->obd_nid_stats_hash = NULL;
-	}
+	rhltable_destroy(&obd->obd_nid_stats_hash);
 err_nid_hash:
 	rhltable_destroy(&obd->obd_nid_hash);
 #endif /* HAVE_SERVER_SUPPORT */
@@ -901,10 +977,8 @@ int class_cleanup(struct obd_device *obd, struct lustre_cfg *lcfg)
 	rhltable_free_and_destroy(&obd->obd_nid_hash, nid_export_exit, NULL);
 
 	/* destroy a nid-stats hash body */
-	if (obd->obd_nid_stats_hash) {
-		cfs_hash_putref(obd->obd_nid_stats_hash);
-		obd->obd_nid_stats_hash = NULL;
-	}
+	rhltable_free_and_destroy(&obd->obd_nid_stats_hash, nid_stats_exit,
+				  NULL);
 
 	/* destroy a client_generation-export hash body */
 	if (obd->obd_gen_hash) {
@@ -2385,65 +2459,6 @@ out:
 EXPORT_SYMBOL(class_manual_cleanup);
 
 #ifdef HAVE_SERVER_SUPPORT
-/*
- * nid<->nidstats hash operations
- */
-static unsigned int
-nidstats_hash(struct cfs_hash *hs, const void *key, const unsigned int bits)
-{
-	return cfs_hash_djb2_hash(key, sizeof(struct lnet_nid), bits);
-}
-
-static void *
-nidstats_key(struct hlist_node *hnode)
-{
-	struct nid_stat *ns;
-
-	ns = hlist_entry(hnode, struct nid_stat, nid_hash);
-
-	return &ns->nid;
-}
-
-static int
-nidstats_keycmp(const void *key, struct hlist_node *hnode)
-{
-	return nid_same((struct lnet_nid *)nidstats_key(hnode),
-			 (struct lnet_nid *)key);
-}
-
-static void *
-nidstats_object(struct hlist_node *hnode)
-{
-	return hlist_entry(hnode, struct nid_stat, nid_hash);
-}
-
-static void
-nidstats_get(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct nid_stat *ns;
-
-	ns = hlist_entry(hnode, struct nid_stat, nid_hash);
-	nidstat_getref(ns);
-}
-
-static void
-nidstats_put_locked(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct nid_stat *ns;
-
-	ns = hlist_entry(hnode, struct nid_stat, nid_hash);
-	nidstat_putref(ns);
-}
-
-static struct cfs_hash_ops nid_stat_hash_ops = {
-	.hs_hash	= nidstats_hash,
-	.hs_key		= nidstats_key,
-	.hs_keycmp	= nidstats_keycmp,
-	.hs_object	= nidstats_object,
-	.hs_get		= nidstats_get,
-	.hs_put_locked	= nidstats_put_locked,
-};
-
 /*
  * client_generation<->export hash operations
  */

commit 1e6831dae8070501f06e8157b5af9ef06ac856a9

LU-16518 obdclass: remove unused inline helpers
... since they are no longer used. The last callers of
__cfs_hash_theta_int/frac(), cl_attr2lvb(),
lprocfs_import_seq_write(), llog_idx_is_eof() and
llog_obd2ops() were removed a long time ago. cl_kmap_local(),
cl_page_from_folio(), cl_object_header_fini(),
cl_2queue_assume(), lu_object_invariant(),
l_fatal_signal_pending(), exp_connect_multibulk(),
exp_connect_parity(), exp_connect_sparse() and the mdo_statfs()
wrapper were never referenced since the day they were first
introduced. Remove them together with their now unnecessary
EXPORT_SYMBOL entries.

Fixes: 61296c3256b9 ("LU-8130 obdclass: convert nid_stats to rhashtable")
Fixes: df8d26f186c1 ("LU-1876 hsm: bugfix for deadlock in glimpse and layout change")
Fixes: 76626d6c52b1 ("LU-13344 all: Separate debugfs and procfs handling")
Fixes: 5ad117fc6f28 ("LU-18170 obdclass: fix llog_print_cb()")
Fixes: 6b4950863c59 ("LU-16518 misc: remove unused llog stuff")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I7cbc042304cc8c59de288114d31477d574f7b4c7
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67281
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/include/cfs_hash.h
@@ -732,19 +732,6 @@ cfs_hash_bucket_validate(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 #define CFS_HASH_MIN_THETA  (1U << (CFS_HASH_THETA_BITS - 1))
 #define CFS_HASH_MAX_THETA  (1U << (CFS_HASH_THETA_BITS + 1))
 
-/* Return integer component of theta */
-static inline int __cfs_hash_theta_int(int theta)
-{
-	return (theta >> CFS_HASH_THETA_BITS);
-}
-
-/* Return a fractional value between 0 and 999 */
-static inline int __cfs_hash_theta_frac(int theta)
-{
-	return ((theta * 1000) >> CFS_HASH_THETA_BITS) -
-		(__cfs_hash_theta_int(theta) * 1000);
-}
-
 static inline int __cfs_hash_theta(struct cfs_hash *hs)
 {
 	return (atomic_read(&hs->hs_count) <<

lustre/include/cl_object.h
@@ -967,11 +967,6 @@ static inline int cl_folio_pgno(const struct cl_page *cl_page)
 #endif
 }
 
-static inline void *cl_kmap_local(struct cl_page *pg)
-{
-	return kmap_local_page(pg->cp_vmpage);
-}
-
 static inline pgoff_t cl_page_index(const struct cl_page *cp)
 {
 	return folio_index_page(cl_page_vmpage(cp));
@@ -2170,7 +2165,6 @@ struct cl_object *cl_object_find(const struct lu_env *env, struct cl_device *cd,
 				 const struct cl_object_conf *c);
 
 int  cl_object_header_init(struct cl_object_header *h);
-void cl_object_header_fini(struct cl_object_header *h);
 void cl_object_put(const struct lu_env *env, struct cl_object *o);
 void cl_object_get(struct cl_object *o);
 void cl_object_attr_lock(struct cl_object *o);
@@ -2256,12 +2250,6 @@ void cl_page_print(const struct lu_env *env, void *cookie,
 void cl_page_header_print(const struct lu_env *env, void *cookie,
 			  lu_printer_t printer, const struct cl_page *pg);
 struct cl_page *cl_vmpage_page(struct page *vmpage, struct cl_object *obj);
-
-static inline struct cl_page *cl_page_from_folio(struct page *vmpage,
-						 pgoff_t index, bool get)
-{
-	return cl_vmpage_page(vmpage, NULL);
-}
 
 /**
  * \name ownership
@@ -2516,8 +2504,6 @@ void cl_page_list_fini(const struct lu_env *env, struct cl_page_list *plist);
 
 void cl_2queue_init(struct cl_2queue *queue);
 void cl_2queue_disown(const struct lu_env *env, struct cl_2queue *queue);
-void cl_2queue_assume(const struct lu_env *env, struct cl_io *io,
-		      struct cl_2queue *queue);
 void cl_2queue_discard(const struct lu_env *env, struct cl_io *io,
 		       struct cl_2queue *queue);
 void cl_2queue_fini(const struct lu_env *env, struct cl_2queue *queue);
@@ -2685,7 +2671,6 @@ void cl_env_percpu_put(struct lu_env *env);
 /*
  * Misc
  */
-void cl_attr2lvb(struct ost_lvb *lvb, const struct cl_attr *attr);
 void cl_lvb2attr(struct cl_attr *attr, const struct ost_lvb *lvb);
 
 struct cl_device *cl_type_setup(const struct lu_env *env, struct lu_site *site,

lustre/include/lprocfs_status.h
@@ -661,12 +661,6 @@ ssize_t ping_show(struct kobject *kobj, struct attribute *attr,
 extern ssize_t
 ldebugfs_import_seq_write(struct file *file, const char __user *buffer,
 			  size_t count, loff_t *off);
-static inline ssize_t
-lprocfs_import_seq_write(struct file *file, const char __user *buffer,
-			 size_t count, loff_t *off)
-{
-	return ldebugfs_import_seq_write(file, buffer, count, off);
-}
 ssize_t pinger_recov_show(struct kobject *kobj, struct attribute *attr,
 			  char *buf);
 ssize_t pinger_recov_store(struct kobject *kobj, struct attribute *attr,
@@ -1214,13 +1208,6 @@ lprocfs_ping_seq_write(struct file *file, const char __user *buffer,
 static inline ssize_t
 ldebugfs_import_seq_write(struct file *file, const char __user *buffer,
 			  size_t count, loff_t *off)
-{
-	return 0;
-}
-
-static inline ssize_t
-lprocfs_import_seq_write(struct file *file, const char __user *buffer,
-			 size_t count, loff_t *off)
 {
 	return 0;
 }

lustre/include/lu_object.h
@@ -837,10 +837,6 @@ void lu_object_header_print(const struct lu_env *env, void *cookie,
 			    lu_printer_t printer,
 			    const struct lu_object_header *hdr);
 
-/* Check object consistency. */
-int lu_object_invariant(const struct lu_object *o);
-
-
 /*
  * Check whether object exists, no matter on local or remote storage.
  * Note: LOHA_EXISTS will be set once some one created the object,

lustre/include/lustre_export.h
@@ -350,11 +350,6 @@ static inline int exp_max_brw_size(struct obd_export *exp)
 	return ONE_MB_BRW_SIZE;
 }
 
-static inline bool exp_connect_multibulk(struct obd_export *exp)
-{
-	return exp_max_brw_size(exp) > ONE_MB_BRW_SIZE;
-}
-
 static inline bool exp_connect_cancelset(struct obd_export *exp)
 {
 	LASSERT(exp != NULL);
@@ -462,11 +457,6 @@ static inline int exp_connect_flr(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_FLR);
 }
 
-static inline int exp_connect_parity(struct obd_export *exp)
-{
-	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_FLR_EC);
-}
-
 static inline int exp_connect_lock_convert(struct obd_export *exp)
 {
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_LOCK_CONVERT);
@@ -487,11 +477,6 @@ static inline int exp_connect_sepol(struct obd_export *exp)
 static inline int exp_connect_encrypt(struct obd_export *exp)
 {
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_ENCRYPT);
-}
-
-static inline int exp_connect_sparse(struct obd_export *exp)
-{
-	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_SPARSE);
 }
 
 static inline int exp_connect_encrypt_fid2path(struct obd_export *exp)

lustre/include/lustre_lib.h
@@ -78,10 +78,6 @@ void target_send_reply(struct ptlrpc_request *req, int rc, int fail_id);
 #define LUSTRE_FATAL_SIGS (sigmask(SIGKILL) | sigmask(SIGINT) |		\
 			   sigmask(SIGTERM) | sigmask(SIGQUIT) |	\
 			   sigmask(SIGALRM))
-static inline int l_fatal_signal_pending(struct task_struct *p)
-{
-	return signal_pending(p) && sigtestsetmask(&p->pending.signal, LUSTRE_FATAL_SIGS);
-}
 
 /** @} lib */
 

lustre/include/lustre_log.h
@@ -345,19 +345,6 @@ struct llog_ctxt {
 #define LLOG_DEL_PLAIN  0x0003
 #define LLOG_SKIP_PLAIN 0x0004
 
-static inline int llog_obd2ops(struct llog_ctxt *ctxt,
-			       const struct llog_operations **lop)
-{
-	if (ctxt == NULL)
-		return -ENOTCONN;
-
-	*lop = ctxt->loc_logops;
-	if (*lop == NULL)
-		return -EOPNOTSUPP;
-
-	return 0;
-}
-
 static inline int llog_handle2ops(struct llog_handle *loghandle,
 				  const struct llog_operations **lop)
 {

lustre/include/md_object.h
@@ -646,14 +646,6 @@ static inline int mdo_unlink(const struct lu_env *env,
 	return p->mo_dir_ops->mdo_unlink(env, p, c, lname, ma, no_name);
 }
 
-static inline int mdo_statfs(const struct lu_env *env,
-			     struct md_device *m,
-			     struct obd_statfs *sfs)
-{
-	LASSERT(m->md_ops->mdo_statfs);
-	return m->md_ops->mdo_statfs(env, m, sfs);
-}
-
 struct dt_device;
 
 void lustre_som_swab(struct lustre_som_attrs *attrs);

lustre/obdclass/cl_io.c
@@ -1113,16 +1113,6 @@ void cl_2queue_discard(const struct lu_env *env,
 EXPORT_SYMBOL(cl_2queue_discard);
 
 /*
- * Assume to own the pages in cl_2queue
- */
-void cl_2queue_assume(const struct lu_env *env,
-		      struct cl_io *io, struct cl_2queue *queue)
-{
-	cl_page_list_assume(env, io, &queue->c2_qin);
-	cl_page_list_assume(env, io, &queue->c2_qout);
-}
-
-/*
  * Finalize both page lists of a 2-queue.
  */
 void cl_2queue_fini(const struct lu_env *env, struct cl_2queue *queue)

lustre/obdclass/cl_object.c
@@ -69,14 +69,6 @@ int cl_object_header_init(struct cl_object_header *h)
 }
 EXPORT_SYMBOL(cl_object_header_init);
 
-/*
- * Finalize cl_object_header.
- */
-void cl_object_header_fini(struct cl_object_header *h)
-{
-        lu_object_header_fini(&h->coh_lu);
-}
-
 /**
  * cl_object_find() - Returns cl_object (client side object) based on given @fid
  * @env: current lustre environment
@@ -949,26 +941,10 @@ void cl_env_put(struct lu_env *env, __u16 *refcheck)
 }
 EXPORT_SYMBOL(cl_env_put);
 
-/*
- * Converts struct cl_attr to struct ost_lvb.
- *
- * see cl_lvb2attr
- */
-void cl_attr2lvb(struct ost_lvb *lvb, const struct cl_attr *attr)
-{
-        lvb->lvb_size   = attr->cat_size;
-        lvb->lvb_mtime  = attr->cat_mtime;
-        lvb->lvb_atime  = attr->cat_atime;
-        lvb->lvb_ctime  = attr->cat_ctime;
-        lvb->lvb_blocks = attr->cat_blocks;
-}
-
 /**
  * cl_lvb2attr() - Converts struct ost_lvb to struct cl_attr.
  * @attr: attribute which will be populated (converted to)
  * @lvb: lvb struct to be converted
- *
- * see cl_attr2lvb
  */
 void cl_lvb2attr(struct cl_attr *attr, const struct ost_lvb *lvb)
 {

lustre/obdclass/llog_ioctl.c
@@ -180,19 +180,6 @@ static int llog_check_cb(const struct lu_env *env, struct llog_handle *handle,
 	RETURN(rc);
 }
 
-static inline bool llog_idx_is_eof(struct llog_handle *llh, __u32 cur_idx)
-{
-	__u32 last_idx = llh->lgh_last_idx;
-
-	/* catalog is wrapped ? */
-	if (unlikely(llh->lgh_hdr->llh_flags & LLOG_F_IS_CAT &&
-		     llh->lgh_hdr->llh_cat_idx >= llh->lgh_last_idx &&
-		     llh->lgh_hdr->llh_count > 1))
-		last_idx = LLOG_HDR_BITMAP_SIZE(llh->lgh_hdr) - 1;
-
-	return cur_idx >= last_idx;
-}
-
 #define MARKER_DIFF	10
 int llog_print_cb(const struct lu_env *env, struct llog_handle *handle,
 		  struct llog_rec_hdr *rec, void *data)

lustre/obdclass/lu_object.c
@@ -650,20 +650,6 @@ void lu_object_print(const struct lu_env *env, void *cookie,
 }
 EXPORT_SYMBOL(lu_object_print);
 
-/* Check object consistency. */
-int lu_object_invariant(const struct lu_object *o)
-{
-	struct lu_object_header *top;
-
-	top = o->lo_header;
-	list_for_each_entry(o, &top->loh_layers, lo_linkage) {
-		if (o->lo_ops->loo_object_invariant != NULL &&
-		    !o->lo_ops->loo_object_invariant(o))
-			return 0;
-	}
-	return 1;
-}
-
 /*
  * Limit the lu_object cache to a maximum of lu_cache_nr objects.  Because the
  * calculation for the number of objects to reclaim is not covered by a lock the