lustre/lustre-release

commit 5a8f1885cd4cbaaba234fde324277aafbe3781df

LU-19828 kfilnd: Map intf to cxi device kfi config
Introduce lustre_lnet_kfi_intf2cxi() to perform a lookup of the cxi
device based on the provided interface name. This is called by
lustre_lnet_kfi_intf2nid() to resolve the interface names in the
YAML configuration path, as well as by jt_add_ni()/jt_del_ni() to
resolve names on the lnetctl net add/del path.

This allows administrators to specify regular Linux network device
names when configuring kfi networks.

Test-Parameters: trivial
HPE-bug-id: LUS-10728
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I4bfeca9b5fe3bb54ae3c79d46416d24456f33ae4
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63583
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/utils/lnetctl.c
@@ -2419,6 +2419,22 @@ free_reply:
 	return rc == 1 ? 0 : rc;
 }
 
+static int kfi_intflist2cxi(struct list_head *nw_intflist)
+{
+	struct lnet_dlc_intf_descr *intf;
+	int rc;
+
+	list_for_each_entry(intf, nw_intflist, intf_on_network) {
+		rc = lustre_lnet_kfi_intf2cxi(intf);
+		if (rc != LUSTRE_CFG_RC_NO_ERR) {
+			fprintf(stderr, "couldn't query kfi intf %s",
+				intf->intf_name);
+			return rc;
+		}
+	}
+	return LUSTRE_CFG_RC_NO_ERR;
+}
+
 static int jt_add_ni(int argc, char **argv)
 {
 	char *ip2net = NULL;
@@ -2594,6 +2610,13 @@ static int jt_add_ni(int argc, char **argv)
 		strcpy(&tunables.lt_tun.lnd_tun_u.lnd_kfi.lnd_traffic_class_str[0],
 		       traffic_class);
 		found = true;
+	}
+
+	/* kfi can convert linux network device names to cxi devices */
+	if (LNET_NETTYP(nw_descr.nw_id) == KFILND) {
+		rc = kfi_intflist2cxi(&nw_descr.nw_intflist);
+		if (rc)
+			return rc;
 	}
 #endif
 
@@ -2774,6 +2797,15 @@ static int jt_del_ni(int argc, char **argv)
 		}
 		net_id = libcfs_net2str(nw_descr.nw_id);
 	}
+
+#ifdef HAVE_KFILND
+	/* kfi can convert linux network device names to cxi devices */
+	if (LNET_NETTYP(nw_descr.nw_id) == KFILND) {
+		rc = kfi_intflist2cxi(&nw_descr.nw_intflist);
+		if (rc)
+			return rc;
+	}
+#endif
 
 	rc = yaml_lnet_config_ni(net_id, NULL, &nw_descr, NULL, -1, NULL,
 				 LNET_GENL_VERSION, 0, stdout);

lnet/utils/lnetconfig/liblnetconfig.c
@@ -40,12 +40,12 @@
 #include "liblnetconfig.h"
 #include <glob.h>
 #include <libcfs/util/param.h>
+#include <dirent.h>
 
 #ifndef HAVE_USRSPC_RDMA_PS_TCP
 #define RDMA_PS_TCP 0x0106
 #endif
 
-#define cxi_nic_addr_path "/sys/class/cxi/cxi%u/device/properties/"
 const char *gmsg_stat_names[] = {"sent_stats", "received_stats",
 				 "dropped_stats"};
 
@@ -1737,15 +1737,77 @@ static int lustre_lnet_queryip(struct lnet_dlc_intf_descr *intf, __u32 *ip)
 	return LUSTRE_CFG_RC_NO_ERR;
 }
 
+#define device_path "/sys/class/net/%s/device/cxi"
+int lustre_lnet_kfi_intf2cxi(struct lnet_dlc_intf_descr *intf)
+{
+	DIR *dir = NULL;
+	struct dirent *entry;
+	char *path;
+	int count = 0;
+	int rc = 0;
+	int size;
+
+	/* Already cxi device name, nothing to do */
+	if (!strncmp(intf->intf_name, "cxi", 3))
+		return LUSTRE_CFG_RC_NO_ERR;
+
+	/* Get cxi device name from the network interface. e.g. cxi0 below
+	 * # ls /sys/class/net/hsn0/device/cxi
+	 * cxi0
+	 * #
+	 */
+	size = snprintf(NULL, 0, device_path, intf->intf_name) + 1;
+	path = malloc(size);
+	if (!path)
+		return LUSTRE_CFG_RC_OUT_OF_MEM;
+	sprintf(path, device_path, intf->intf_name);
+
+	dir = opendir(path);
+	free(path);
+	if (!dir)
+		return LUSTRE_CFG_RC_BAD_PARAM;
+
+	while ((entry = readdir(dir)) != NULL) {
+		if (strncmp(entry->d_name, "cxi", 3))
+			continue;
+
+		/* There should only be a single cxi device, and
+		 * its name should fit in the intf_name buffer
+		 */
+		if (count ||
+		    strlen(entry->d_name) >= sizeof(intf->intf_name)) {
+			rc = LUSTRE_CFG_RC_BAD_PARAM;
+			break;
+		}
+
+		/* Overwrite the interface name with the cxi device */
+		strncpy(intf->intf_name, entry->d_name,
+			sizeof(intf->intf_name));
+		count++;
+	}
+
+	closedir(dir);
+
+	if (count != 1)
+		rc = LUSTRE_CFG_RC_BAD_PARAM;
+
+	return rc;
+}
+
+#define cxi_nic_addr_path "/sys/class/cxi/cxi%u/device/properties/"
 static int lustre_lnet_kfi_intf2nid(struct lnet_dlc_intf_descr *intf,
 				    __u32 *nid_addr)
 {
 	unsigned int nic_index;
-	int rc;
+	int rc = 0;
 	char *nic_addr_path;
 	char val[128];
 	int size;
 	long int addr;
+
+	rc = lustre_lnet_kfi_intf2cxi(intf);
+	if (rc != LUSTRE_CFG_RC_NO_ERR)
+		return rc;
 
 	rc = sscanf(intf->intf_name, "cxi%u", &nic_index);
 	if (rc != 1)

lnet/utils/lnetconfig/liblnetconfig.h
@@ -1114,4 +1114,5 @@ void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr);
 int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 				     lnet_nid_t **nids, __u32 *nnids,
 				     char *err_str, size_t str_len);
+int lustre_lnet_kfi_intf2cxi(struct lnet_dlc_intf_descr *intf);
 #endif /* LIB_LNET_CONFIG_API_H */

commit 159908c1810bdb8377461f78b8c3b3390081540d

LU-16918 build: Restore --disable-modules support
fix a defined but not used `kfi_intflist2cxi()` by wrapping
it in #ifdef HAVE_KFILND

enable building `--enable-gss --enable-gss-keyring` by skipping
the kernel checks when `--disable-modules` is used.

Test-Parameters: trivial
Fixes: 5a8f1885cd4c ("LU-19828 kfilnd: Map intf to cxi device kfi config")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I730c506d7cc8d29f4cbc8168fd27dd1e1bc5eb68
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51378
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

config/lustre-build.m4
@@ -675,6 +675,9 @@ AS_IF([test "x$enable_utils" = xyes], [
 	LC_OPENSSL_GETSEPOL
 	LC_CONFIG_GETSEPOL
 	LC_FID2PATH_ANON_UNION
+	AS_IF([test x$enable_modules = xno], [
+		LC_CONFIG_GSS
+	])
 ])
 AS_IF([test "x$enable_tests" = xyes], [
 	LC_HAVE_LIBAIO

config/lustre-core.m4
@@ -265,9 +265,11 @@ AC_ARG_ENABLE([gss_keyring],
 			enable_gss_keyring="$enable_gss"])])
 AC_MSG_RESULT([$enable_gss_keyring])
 AS_IF([test "x$enable_gss_keyring" != xno], [
-	LB_CHECK_CONFIG_IM([KEYS], [], [
-		gss_keyring_conf_test="fail"
-		AC_MSG_WARN([GSS keyring backend requires that CONFIG_KEYS be enabled in your kernel.])])
+	AS_IF([test x$enable_modules != xno], [
+		LB_CHECK_CONFIG_IM([KEYS], [], [
+			gss_keyring_conf_test="fail"
+			AC_MSG_WARN([GSS keyring backend requires that CONFIG_KEYS be enabled in your kernel.])])
+	])
 
 	AC_CHECK_LIB([keyutils], [keyctl_search], [], [
 		gss_keyring_conf_test="fail"
@@ -352,9 +354,11 @@ AC_SUBST(TEST_DIR)
 AS_IF([test "x$enable_gss" != xno], [
 	LC_CONFIG_GSS_KEYRING
 
-	sunrpc_required=$enable_gss
-	LC_CONFIG_SUNRPC
-	sunrpc_required="no"
+	AS_IF([test x$enable_modules != xno], [
+		sunrpc_required=$enable_gss
+		LC_CONFIG_SUNRPC
+		sunrpc_required="no"
+	])
 
 	require_krb5=$enable_gss
 	AC_KERBEROS_V5

lnet/utils/lnetctl.c
@@ -2418,6 +2418,7 @@ free_reply:
 	return rc == 1 ? 0 : rc;
 }
 
+#ifdef HAVE_KFILND
 static int kfi_intflist2cxi(struct list_head *nw_intflist)
 {
 	struct lnet_dlc_intf_descr *intf;
@@ -2433,6 +2434,7 @@ static int kfi_intflist2cxi(struct list_head *nw_intflist)
 	}
 	return LUSTRE_CFG_RC_NO_ERR;
 }
+#endif
 
 static int jt_add_ni(int argc, char **argv)
 {