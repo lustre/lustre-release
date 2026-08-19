lustre/lustre-release

commit e9278b8da16338db30ac34abff1f6bc3489352b0

LU-17431 nodemap: make dynamic nodemaps hierarchical
Introduce a new rn_subtree field in struct lu_nid_range.
This allows to have NID ranges included into other NID ranges,
hence creating a NID range hierarchy. NID ranges are created in
a subtree only if there is already a range in the parent tree
that includes it. Overlapping NID ranges remain forbidden.

Introduce new fields nm_subnodemaps, nm_parent_entry in struct
lu_nodemap to keep track of the sub-nodemaps attached to a given
nodemap. Also use a new nm_parent_nm field that points to the parent
nodemap. This allows to create a nodemap hierarchy, based on the
NID ranges.

Introduce a new -p|--parent option to lctl nodemap_add to specify
the parent nodemap of a dynamic nodemap at creation time. The NID
ranges of the child nodemap must be included in the ranges of the
parent nodemap. A dynamic nodemap can have only one parent.

Make a sub-nodemap inherit all the parent nodemap's properties,
and the id mapping as well. And expose parent name as part of nodemap
properties. This is valid for dynamic nodemaps only.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I04a14db92c068f3cb5b6b39e75feb802cebacd8a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54739
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nodemap_range.c
@@ -28,6 +28,19 @@
 INTERVAL_TREE_DEFINE(struct lu_nid_range, rn_rb, lnet_nid_t, rn_subtree_last,
 		     START, LAST, static, nm_range)
 
+static int __range_is_included(lnet_nid_t needle_start, lnet_nid_t needle_end,
+			       struct lu_nid_range *haystack)
+{
+	return LNET_NIDADDR(START(haystack)) <= LNET_NIDADDR(needle_start) &&
+	       LNET_NIDADDR(LAST(haystack)) >= LNET_NIDADDR(needle_end);
+}
+
+static int range_is_included(struct lu_nid_range *needle,
+			     struct lu_nid_range *haystack)
+{
+	return __range_is_included(START(needle), LAST(needle), haystack);
+}
+
 /*
  * range constructor
  *
@@ -145,6 +158,7 @@ struct lu_nid_range *range_create(struct nodemap_config *config,
 	INIT_LIST_HEAD(&range->rn_nidlist);
 	if (!list_empty(&tmp_nidlist))
 		list_splice(&tmp_nidlist, &range->rn_nidlist);
+	range->rn_subtree.nmrt_range_interval_root = INTERVAL_TREE_ROOT;
 
 	return range;
 }
@@ -172,10 +186,15 @@ struct lu_nid_range *__range_find(struct nodemap_range_tree *nm_range_tree,
 
 	range = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
 				    nid4[0], nid4[1]);
-	while (range &&
-	       (!nid_same(&range->rn_start, start_nid) ||
-		!nid_same(&range->rn_end, end_nid)))
+	while (range) {
+		if (nid_same(&range->rn_start, start_nid) &&
+		    nid_same(&range->rn_end, end_nid))
+			break;
+		if (__range_is_included(nid4[0], nid4[1], range))
+			return __range_find(&range->rn_subtree,
+					    start_nid, end_nid);
 		range = nm_range_iter_next(range, nid4[0], nid4[1]);
+	}
 
 	return range;
 }
@@ -233,24 +252,44 @@ void range_destroy(struct lu_nid_range *range)
  * to exactly one range
  */
 static int __range_insert(struct nodemap_range_tree *nm_range_tree,
-			  struct lu_nid_range *range)
+			  struct lu_nid_range *range,
+			  struct lu_nid_range **parent_range, bool dynamic)
 {
-	if (nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
-				lnet_nid_to_nid4(&range->rn_start),
-				lnet_nid_to_nid4(&range->rn_end)))
-		return -EEXIST;
+	struct lu_nid_range *found = NULL;
+	int rc = 0;
+
+	found = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+				    lnet_nid_to_nid4(&range->rn_start),
+				    lnet_nid_to_nid4(&range->rn_end));
+	if (found) {
+		if (dynamic && range_is_included(range, found)) {
+			rc = __range_insert(&found->rn_subtree,
+					    range, parent_range, dynamic);
+			if (!rc) {
+				if (parent_range && !*parent_range)
+					*parent_range = found;
+			}
+		} else {
+			rc = -EEXIST;
+		}
+		GOTO(out_insert, rc);
+	}
 
 	nm_range_insert(range,
 			&nm_range_tree->nmrt_range_interval_root);
-	return 0;
+
+out_insert:
+	return rc;
 }
 
-int range_insert(struct nodemap_config *config, struct lu_nid_range *range)
+int range_insert(struct nodemap_config *config, struct lu_nid_range *range,
+		 struct lu_nid_range **parent_range, bool dynamic)
 {
 	int rc = 0;
 
 	if (!range->rn_netmask) {
-		rc = __range_insert(&config->nmc_range_tree, range);
+		rc = __range_insert(&config->nmc_range_tree,
+				    range, parent_range, dynamic);
 	} else {
 		if (range_find(config, &range->rn_start, &range->rn_end,
 			       range->rn_netmask))
@@ -271,8 +310,28 @@ int range_insert(struct nodemap_config *config, struct lu_nid_range *range)
 static void __range_delete(struct nodemap_range_tree *nm_range_tree,
 			   struct lu_nid_range *range)
 {
-	nm_range_remove(range,
-			&nm_range_tree->nmrt_range_interval_root);
+	struct lu_nid_range *found;
+	lnet_nid_t nid4[2];
+
+	nid4[0] = lnet_nid_to_nid4(&range->rn_start);
+	nid4[1] = lnet_nid_to_nid4(&range->rn_end);
+
+	found = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+				    nid4[0], nid4[1]);
+	while (found) {
+		if (nid_same(&found->rn_start, &range->rn_start) &&
+		    nid_same(&found->rn_end, &range->rn_end))
+			break;
+		if (__range_is_included(nid4[0], nid4[1], found)) {
+			__range_delete(&found->rn_subtree, range);
+			return;
+		}
+		found = nm_range_iter_next(found, nid4[0], nid4[1]);
+	}
+
+	if (found)
+		nm_range_remove(found,
+				&nm_range_tree->nmrt_range_interval_root);
 }
 
 void range_delete(struct nodemap_config *config, struct lu_nid_range *range)
@@ -296,9 +355,18 @@ static
 struct lu_nid_range *__range_search(struct nodemap_range_tree *nm_range_tree,
 				    struct lnet_nid *nid)
 {
-	return nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
-				   lnet_nid_to_nid4(nid),
-				   lnet_nid_to_nid4(nid));
+	struct lu_nid_range *range, *subrange;
+
+	range = nm_range_iter_first(&nm_range_tree->nmrt_range_interval_root,
+				    lnet_nid_to_nid4(nid),
+				    lnet_nid_to_nid4(nid));
+	if (range) {
+		subrange = __range_search(&range->rn_subtree, nid);
+		if (subrange)
+			range = subrange;
+	}
+
+	return range;
 }
 
 struct lu_nid_range *range_search(struct nodemap_config *config,

lustre/tests/sanity-sec.sh
@@ -7109,6 +7109,12 @@ test_72() {
 	local nids=1.1.1.[1-100]@tcp
 	local startnid=1.1.1.1@tcp
 	local endnid=1.1.1.100@tcp
+	local subnids1=1.1.1.[2-50]@tcp
+	local subnids2=1.1.1.[51-100]@tcp
+	local subnids3=1.1.1.[2-25]@tcp
+	local subnids4=1.1.1.[51-52]@tcp
+	local subnids5=1.1.1.[26-60]@tcp
+	local subnids6=1.1.1.[1-60]@tcp
 	local clid=500
 	local fsid=1000
 	local properties="audit_mode deny_unknown forbid_encryption \
@@ -7130,6 +7136,9 @@ test_72() {
 		stack_trap cleanup_active EXIT
 	fi
 
+	do_facet mgs $LCTL nodemap_set_fileset --name default \
+		--fileset "/deffset" ||
+			error "setting fileset on default failed"
 	do_facet mgs $LCTL nodemap_add $mgsnm ||
 		error "adding $mgsnm on MGS failed"
 	stack_trap "do_facet mgs $LCTL nodemap_del $mgsnm" EXIT
@@ -7145,10 +7154,12 @@ test_72() {
 	stack_trap "do_facet ost1 $LCTL nodemap_del $nm || true" EXIT
 	do_facet ost1 $LCTL nodemap_add $nm &&
 		error "static nodemap on server should fail"
-	do_facet ost1 $LCTL nodemap_add -d $nm ||
+	do_facet ost1 $LCTL nodemap_add -d $nm &&
+		error "dynamic nodemap without parent should fail"
+	do_facet ost1 $LCTL nodemap_add -d -p default $nm ||
 		error "dynamic nodemap on server failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.id)
-	if [[ "x$val" == "x" ]] || [[ "x$val" == "x0" ]]; then
+	if [[ -z "$val" || "$val" == "0" ]]; then
 		error "dynamic nodemap wrong id $val"
 	fi
 
@@ -7156,148 +7167,174 @@ test_72() {
 		error "dynamic add_range on server failed"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.ranges |
 		awk 'BEGIN{RS=", "} $1=="start_nid:"{print $2 ; exit}')
-	if [[ "x$val" != "x$startnid" ]]; then
+	[[ "$val" == "$startnid" ]] ||
 		error "dynamic nodemap wrong start nid range $val"
-	fi
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.ranges |
 		awk 'BEGIN{RS=", "} $1=="end_nid:"{print $2 ; exit}')
-	if [[ "x$val" != "x$endnid" ]]; then
+	[[ "$val" == "$endnid" ]] ||
 		error "dynamic nodemap wrong end nid range $val"
-	fi
 
 	do_facet ost1 $LCTL nodemap_add_idmap --name $nm --idtype uid \
 		--idmap $clid:$fsid ||
 			error "dynamic add_idmap on server failed"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.idmap |
 		awk 'BEGIN{RS=", "} $1=="client_id:"{print $2 ; exit}')
-	if [[ "x$val" != "x$clid" ]]; then
-		error "dynamic nodemap wrong client id $val"
-	fi
+	(( val == clid )) || error "dynamic nodemap wrong client id $val"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.idmap |
 		awk 'BEGIN{RS=", "} $1=="fs_id:"{print $2 ; exit}')
-	if [[ "x$val" != "x$fsid" ]]; then
-		error "dynamic nodemap wrong fs id $val"
-	fi
+	(( val == fsid )) || error "dynamic nodemap wrong fs id $val"
 
 	for prop in $properties; do
 		do_facet ost1 $LCTL nodemap_modify --name $nm \
 			--property $prop --value 1 ||
 				error "dynamic modify of $prop failed"
 		val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-		[[ "x$val" == "x1" ]] || error "incorrect $prop $val"
+		(( val == 1 )) || error "incorrect $prop $val"
 	done
 	prop=admin
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 1 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.admin_nodemap)
-	[[ "x$val" == "x1" ]] || error "incorrect $prop $val"
+	(( val == 1 )) || error "incorrect $prop $val"
 	prop=trusted
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 0 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.trusted_nodemap)
-	[[ "x$val" == "x0" ]] || error "incorrect $prop $val"
+	(( val == 0 )) || error "incorrect $prop $val"
 	prop=map_mode
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value uid ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "xuid" ]] || error "incorrect $prop $val"
+	[[ "$val" == "uid" ]] || error "incorrect $prop $val"
 	prop=rbac
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value file_perms ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "xfile_perms" ]] || error "incorrect $prop $val"
+	[[ "$val" == "file_perms" ]] || error "incorrect $prop $val"
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value all ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x$rbac_val" ]] || error "incorrect $prop $val"
+	[[ "$val" == "$rbac_val" ]] || error "incorrect $prop $val"
 	prop=squash_uid
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 77 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	(( val == 77 )) || error "incorrect $prop $val"
 	prop=squash_gid
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 77 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	(( val == 77 )) || error "incorrect $prop $val"
 	prop=squash_projid
 	do_facet ost1 $LCTL nodemap_modify --name $nm \
 		--property $prop --value 77 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x77" ]] || error "incorrect $prop $val"
+	(( val == 77 )) || error "incorrect $prop $val"
 	prop=fileset
 	do_facet ost1 $LCTL nodemap_set_fileset --name $nm \
-		--fileset "/tmp" ||
-			error "dynamic modify of $prop failed"
+		--fileset "/tmp" &&
+			error "dynamic modify of $prop should fail"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x/tmp" ]] || error "incorrect $prop $val"
+	[[ "$val" == "/deffset" ]] || error "incorrect $prop $val"
 	prop=sepol
 	do_facet ost1 $LCTL nodemap_set_sepol --name $nm \
 		--sepol $sepol ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop)
-	[[ "x$val" == "x$sepol" ]] || error "incorrect $prop $val"
+	[[ "$val" == "$sepol" ]] || error "incorrect $prop $val"
+
 	prop=offset
 	do_facet ost1 $LCTL nodemap_add_offset --name $nm \
 		--offset 100000 --limit 200000 ||
 			error "dynamic modify of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
 		awk '$1 == "start_uid:" {print $2}' | sed s+,++)
-	[[ "x$val" == "x100000" ]] || error "incorrect $prop start_uid $val"
+	(( val == 100000 )) || error "incorrect $prop start_uid $val"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
 		awk '$1 == "limit_uid:" {print $2}' | sed s+,++)
-	[[ "x$val" == "x200000" ]] || error "incorrect $prop limit_uid $val"
+	(( val == 200000 )) || error "incorrect $prop limit_uid $val"
 	do_facet ost1 $LCTL nodemap_del_offset --name $nm ||
 			error "dynamic del of $prop failed"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
 		awk '$1 == "start_uid:" {print $2}' | sed s+,++)
-	[[ "x$val" == "x0" ]] || error "incorrect $prop start_uid $val"
+	(( val == 0 )) || error "incorrect $prop start_uid $val"
 	val=$(do_facet ost1 $LCTL get_param -n nodemap.$nm.$prop |
 		awk '$1 == "limit_uid:" {print $2}' | sed s+,++)
-	[[ "x$val" == "x0" ]] || error "incorrect $prop limit_uid $val"
+	(( val == 0 )) || error "incorrect $prop limit_uid $val"
 
 	val=$(do_facet ost1 $LCTL nodemap_test_id --nid $startnid \
 		--idtype uid --id $clid)
-	if [[ "x$val" != "x$fsid" ]]; then
-		error "dynamic test_id on server failed"
-	fi
+	(( val == fsid )) || error "dynamic test_id on server failed"
 
 	do_facet ost1 $LCTL nodemap_del_idmap --name $nm --idtype uid \
 		--idmap $clid:$fsid ||
 			error "dynamic del_idmap on server failed"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.idmap |
 		awk 'BEGIN{RS=", "} $1=="client_id:"{print $2 ; exit}')
-	if [[ "x$val" != "x" ]]; then
-		error "idmap should be empty, got $val"
-	fi
+	[[ -z "$val" ]] || error "idmap should be empty, got $val"
 
 	val=$(do_facet ost1 $LCTL nodemap_test_nid $startnid)
-	if [[ "x$val" != "x$nm" ]]; then
-		error "dynamic test_nid on server failed"
-	fi
+	[[ "$val" == "$nm" ]] || error "dynamic test_nid on server failed"
+
+	do_facet ost1 $LCTL nodemap_add -d -p $nm ${nm}_1 ||
+		error "nodemap add ${nm}_1 on server failed"
+	stack_trap "do_facet ost1 $LCTL nodemap_del ${nm}_1 || true" EXIT
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_1 \
+		--range $subnids1 ||
+			error "add_range for ${nm}_1 failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.${nm}_1.parent)
+	[[ "$val" == "$nm" ]] ||
+		error "parent of ${nm}_1 should be $nm, got $val"
+
+	do_facet ost1 $LCTL nodemap_add -d -p $nm ${nm}_2 ||
+		error "nodemap add ${nm}_2 on server failed"
+	stack_trap "do_facet ost1 $LCTL nodemap_del ${nm}_2 || true" EXIT
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_2 \
+		--range $subnids2 ||
+			error "add_range for ${nm}_2 failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.${nm}_2.parent)
+	[[ "$val" == "$nm" ]] ||
+		error "parent of ${nm}_2 should be $nm, got $val"
+
+	do_facet ost1 $LCTL nodemap_add -d -p ${nm}_1 ${nm}_3 ||
+		error "nodemap add ${nm}_3 on server failed"
+	stack_trap "do_facet ost1 $LCTL nodemap_del ${nm}_3 || true" EXIT
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_3 \
+		--range $subnids4 &&
+		       error "nodemap ${nm}_3 should not accept range $subnids4"
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_3 \
+		--range $subnids5 &&
+		       error "nodemap ${nm}_3 should not accept range $subnids5"
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_3 \
+		--range $subnids6 &&
+		       error "nodemap ${nm}_3 should not accept range $subnids6"
+	do_facet ost1 $LCTL nodemap_add_range --name ${nm}_3 \
+		--range $subnids3 ||
+			error "add_range $subnids3 for ${nm}_3 failed"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.${nm}_3.parent)
+	[[ "$val" == "${nm}_1" ]] ||
+		error "parent of ${nm}_3 should be ${nm}_1, got $val"
+	val=$(do_facet ost1 $LCTL get_param -n nodemap.${nm}_3.squash_projid)
+	(( val == 77 )) || error "squash_projid should be inherited, got $val"
 
 	do_facet ost1 $LCTL nodemap_del_range --name $nm --range $nids ||
 		error "dynamic del_range on server failed"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.ranges |
 		awk 'BEGIN{RS=", "} $1=="start_nid:"{print $2 ; exit}')
-	if [[ "x$val" != "x" ]]; then
-		error "nid range should be empty, got $val"
-	fi
+	[[ -z "$val" ]] || error "nid range should be empty, got $val"
 
 	do_facet ost1 $LCTL nodemap_del $nm ||
 		error "dynamic nodemap del on server failed"
 	val=$(do_facet ost1 $LCTL get_param nodemap.$nm.id)
-	if [[ "x$val" != "x" ]]; then
-		error "nodemap should be gone, got $val"
-	fi
+	[[ -z "$val" ]] || error "nodemap should be gone, got $val"
 
 	do_facet ost1 $LCTL nodemap_add_range --name $mgsnm --range $mgsnids2 &&
 			error "add_range $mgsnm on server should fail"

lustre/doc/lctl-nodemap-add.8
@@ -6,6 +6,9 @@ lctl-nodemap_add \- create a new nodemap to define client behavior
 or
 .SY "lctl nodemap add"
 .RB [ -d | --dynamic ]
+.RB [ -p |
+.BI --parent " PARENT_NAME"
+]
 .BI --name " NODEMAP_NAME"
 .YS
 .SH DESCRIPTION
@@ -18,6 +21,10 @@ and filesystem access permission of those NID(s).
 .BR -d ", " --dynamic
 Adds a temporary in-memory nodemap on the local node instead of a persistent
 one.
+.TP
+.BI -p ", " --parent " PARENT_NAME"
+Mandatory for dynamic nodemap. Specifies the parent nodemap to inherit
+properties from. This can be "default".
 .TP
 .BI --name " NODEMAP_NAME"
 The name to give the new nodemap. It can be any alphanumeric string of maximum

lustre/include/lustre_nodemap.h
@@ -123,6 +123,12 @@ struct lu_nodemap {
 	unsigned int		 nm_offset_start_projid;
 	/* number of values allocated to PROJID offset */
 	unsigned int		 nm_offset_limit_projid;
+	/* list of sub-nodemaps */
+	struct list_head	 nm_subnodemaps;
+	/* list entry for parent nodemap */
+	struct list_head	 nm_parent_entry;
+	/* link to parent nodemap */
+	struct lu_nodemap	*nm_parent_nm;
 };
 
 /* Store handles to local MGC storage to save config locally. In future

commit 7fae11ad4c80a3d4163585c4c0c500a874453d88

LU-20524 nodemap: do not orphan sub-ranges when deleting a range
The NID range of a dynamic sub-nodemap is stored in the interval tree
embedded in the range of its parent nodemap (lu_nid_range.rn_subtree),
and points back to that tree through rn_tree.

range_delete_generic() frees a range without looking at its subtree, so
deleting the parent nodemap's range - what 'lctl nodemap_del_range' on
the parent does in sanity-sec test_72a/72b - leaves every sub-range
linked into freed memory, with rn_tree dangling. The sub-ranges also
stop being found by range_search(), so their clients silently fall back
to the default nodemap. Deleting one of them afterwards, as the
recursive nodemap_del() of the sub-nodemaps does, then writes into the
freed parent range, which panics once that slab page has been returned
to the page allocator:

  BUG: unable to handle kernel paging request at ffff9ba0876d7460
  RIP: range_delete_generic+128 [ptlrpc]
   range_delete+0x25 [ptlrpc]
   nodemap_del+0x50b [ptlrpc]
   nodemap_del+0x1bb [ptlrpc]
   cfg_nodemap_cmd+0x2f3 [ptlrpc]

Move the sub-ranges up into the tree the deleted range itself belongs
to. They cannot conflict there: they were included in the deleted
range, which did not overlap any other range of that tree.

The subtree was introduced by e9278b8da1 ("LU-17431 nodemap: make
dynamic nodemaps hierarchical"), which already orphaned sub-ranges;
it became a use-after-free once range deletion started following
rn_tree.

Fixes: 5bde32348f ("LU-17431 nodemap: find innermost nid range")
Test-Parameters: trivial testlist=sanity-sec
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I676579d3e7b018d5c77f93d051881f153576bf97
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67534
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_range.c
@@ -459,6 +459,33 @@ int ban_range_insert(struct nodemap_config *config, struct lu_nid_range *range,
 }
 
 /**
+ * range_reparent_subtree() - Move sub-ranges one level up
+ * @range: range being removed from its tree
+ *
+ * The sub-ranges of @range live in the interval tree embedded in @range, and
+ * point back to it via their rn_tree. As @range is about to be freed, move
+ * them to the tree @range itself belongs to. They cannot conflict there: they
+ * were included in @range, which did not overlap any other range of that tree.
+ */
+static void range_reparent_subtree(struct lu_nid_range *range)
+{
+	struct rb_root_cached *subtree;
+	struct rb_node *node;
+
+	subtree = &range->rn_subtree.nmrt_range_interval_root;
+
+	while ((node = rb_first_cached(subtree)) != NULL) {
+		struct lu_nid_range *subrange;
+
+		subrange = rb_entry(node, struct lu_nid_range, rn_rb);
+		nm_range_remove(subrange, subtree);
+		nm_range_insert(subrange,
+				&range->rn_tree->nmrt_range_interval_root);
+		subrange->rn_tree = range->rn_tree;
+	}
+}
+
+/**
  * range_delete_generic() - Delete a range from the interval tree and any
  *			    associated nodemap references
  * @range: range to delete
@@ -468,9 +495,11 @@ static void range_delete_generic(struct lu_nid_range *range)
 	list_del(&range->rn_list);
 
 	if (!range->rn_netmask) {
-		if (range->rn_tree)
+		if (range->rn_tree) {
 			nm_range_remove(range,
 				    &range->rn_tree->nmrt_range_interval_root);
+			range_reparent_subtree(range);
+		}
 	} else {
 		list_del(&range->rn_collect);
 	}

lustre/tests/sanity-sec.sh
@@ -9535,6 +9535,18 @@ dyn_nm_helper() {
 		awk 'BEGIN{RS=", "} $1=="start_nid:"{print $2 ; exit}')
 	[[ -z "$val" ]] || error "nid range should be empty, got $val"
 
+	if (( $(lustre_version_code $facet) >= $(version_code 2.17.56) )); then
+		val=$(do_facet $facet $LCTL nodemap_test_nid 1.1.1.10@tcp)
+		[[ "$val" == "${nm}_3" ]] ||
+			error "1.1.1.10@tcp should be in ${nm}_3, got $val"
+		val=$(do_facet $facet $LCTL nodemap_test_nid 1.1.1.30@tcp)
+		[[ "$val" == "${nm}_1" ]] ||
+			error "1.1.1.30@tcp should be in ${nm}_1, got $val"
+		val=$(do_facet $facet $LCTL nodemap_test_nid 1.1.1.60@tcp)
+		[[ "$val" == "${nm}_2" ]] ||
+			error "1.1.1.60@tcp should be in ${nm}_2, got $val"
+	fi
+
 	do_facet $facet $LCTL nodemap_del $nm ||
 		error "dynamic nodemap del on server failed"
 	val=$(do_facet $facet $LCTL get_param nodemap.$nm.id)