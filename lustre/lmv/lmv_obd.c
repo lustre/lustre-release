lustre/lustre-release

commit 5fe3d1c7b87bead572c4f51ecc90c9c85553d7ee

LU-19268 lmv: Add qos_exclude_patterns
Introduce qos_exclude_patterns to complement
qos_exclude_prefixes. qos_exclude_patterns should
allow shell-style pattern matching (e.g., "*.[ch]"),
providing greater flexibility than prefix-only
matching.

Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: I7f307850aa6f0b2301ae24f96c27a2b80a60e96b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60794
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -24,6 +24,7 @@
 #include <linux/math64.h>
 #include <linux/seq_file.h>
 #include <linux/namei.h>
+#include <linux/glob.h>
 
 #include <lustre_compat/linux/stringhash.h>
 
@@ -1199,47 +1200,13 @@ int lmv_fid_alloc(const struct lu_env *env, struct obd_export *exp,
 	RETURN(rc);
 }
 
-static u32 qos_exclude_hashfh(const void *data, u32 len, u32 seed)
-{
-	const char *name = data;
-
-	return hashlen_hash(hashlen_string((void *)(unsigned long)seed,
-					   name));
-}
-
-static int qos_exclude_cmpfn(struct rhashtable_compare_arg *arg,
-			     const void *obj)
-{
-	const struct qos_exclude_prefix *prefix = obj;
-	const char *name = arg->key;
-
-	return strcmp(name, prefix->qep_name);
-}
-
-const struct rhashtable_params qos_exclude_hash_params = {
-	.key_len	= 1, /* actually variable */
-	.key_offset	= offsetof(struct qos_exclude_prefix, qep_name),
-	.head_offset	= offsetof(struct qos_exclude_prefix, qep_hash),
-	.hashfn		= qos_exclude_hashfh,
-	.obj_cmpfn	= qos_exclude_cmpfn,
-	.automatic_shrinking = true,
-};
-
-void qos_exclude_prefix_free(void *vprefix, void *data)
-{
-	struct qos_exclude_prefix *prefix = vprefix;
-
-	list_del(&prefix->qep_list);
-	kfree(prefix);
-}
-
 static const struct lu_device_operations lmv_lu_ops;
 
 static struct lu_device *lmv_device_alloc(const struct lu_env *env,
 					  struct lu_device_type *ldt,
 					  struct lustre_cfg *lcfg)
 {
-	struct qos_exclude_prefix *prefix;
+	struct qos_exclude_pattern *pat;
 	struct lnet_processid lnet_id;
 	struct obd_device *obd;
 	struct lmv_desc *desc;
@@ -1309,33 +1276,25 @@ static struct lu_device *lmv_device_alloc(const struct lu_env *env,
 		CWARN("%s: error initialize target table: rc = %d\n",
 		      obd->obd_name, rc);
 
-	rc = rhashtable_init(&lmv->lmv_qos_exclude_hash,
-			     &qos_exclude_hash_params);
-	if (rc) {
-		CERROR("%s: qos exclude hash initalize failed: %d\n",
-		       obd->obd_name, rc);
-		GOTO(out_free, rc);
-	}
-
-	prefix = kmalloc(sizeof(*prefix), __GFP_ZERO);
-	if (!prefix)
-		GOTO(out_destroy, rc = -ENOMEM);
+	OBD_ALLOC_PTR(pat);
+	if (!pat)
+		GOTO(out_free, rc = -ENOMEM);
 
 	/* Apache Spark creates a _temporary directory for staging files */
-	strcpy(prefix->qep_name, "_temporary");
-	rc = rhashtable_insert_fast(&lmv->lmv_qos_exclude_hash,
-				    &prefix->qep_hash, qos_exclude_hash_params);
-	if (rc) {
-		kfree(prefix);
-		GOTO(out_destroy, rc);
-	}
+	strcpy(pat->qep_name, "_temporary");
 
-	list_add_tail(&prefix->qep_list, &lmv->lmv_qos_exclude_list);
+	list_add_tail(&pat->qep_list, &lmv->lmv_qos_exclude_list);
+
+	OBD_ALLOC_PTR(pat);
+	if (!pat)
+		GOTO(out_free, rc = -ENOMEM);
+
+	strcpy(pat->qep_name, "_temporary.*");
+
+	list_add_tail(&pat->qep_list, &lmv->lmv_qos_exclude_list);
 
 	RETURN(lu);
 
-out_destroy:
-	rhashtable_destroy(&lmv->lmv_qos_exclude_hash);
 out_free:
 	OBD_FREE_PTR(lu);
 	RETURN(ERR_PTR(rc));
@@ -1348,11 +1307,18 @@ static struct lu_device *lmv_device_free(const struct lu_env *env,
 	struct lmv_obd *lmv = &obd->u.lmv;
 	struct lu_tgt_desc *tgt;
 	struct lu_tgt_desc *tmp;
+	struct qos_exclude_pattern *pat;
+	struct qos_exclude_pattern *ptmp;
 
 	ENTRY;
 
-	rhashtable_free_and_destroy(&lmv->lmv_qos_exclude_hash,
-				    qos_exclude_prefix_free, NULL);
+	spin_lock(&lmv->lmv_lock);
+	list_for_each_entry_safe(pat, ptmp,
+		&lmv->lmv_qos_exclude_list, qep_list) {
+		list_del(&pat->qep_list);
+		OBD_FREE_PTR(pat);
+	}
+	spin_unlock(&lmv->lmv_lock);
 	fld_client_fini(&lmv->lmv_fld);
 	fld_client_debugfs_fini(&lmv->lmv_fld);
 
@@ -2168,32 +2134,18 @@ static bool lmv_tgt_nocreate(struct lmv_obd *lmv, struct lmv_tgt_desc *tgt)
 static bool lmv_qos_exclude(struct lmv_obd *lmv, struct md_op_data *op_data)
 {
 	const char *name = op_data->op_name;
-	size_t namelen = op_data->op_namelen;
-	char buf[NAME_MAX + 1];
-	struct qos_exclude_prefix *prefix;
-	char *p;
+	struct qos_exclude_pattern *pat;
 
 	/* skip encrypted files */
 	if (op_data->op_file_encctx)
 		return false;
 
-	/* name length may not be validated yet */
-	if (namelen > NAME_MAX)
-		return false;
-
-	p = strrchr(name, '.');
-	if (p) {
-		namelen = p - name;
-		if (!namelen)
-			return false;
-		strncpy(buf, name, namelen);
-		buf[namelen] = '\0';
-		name = buf;
+	list_for_each_entry(pat, &lmv->lmv_qos_exclude_list, qep_list) {
+		if (glob_match(pat->qep_name, name))
+			return true;
 	}
 
-	prefix = rhashtable_lookup_fast(&lmv->lmv_qos_exclude_hash, name,
-					qos_exclude_hash_params);
-	return prefix != NULL;
+	return false;
 }
 
 struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,

lustre/include/obd.h
@@ -414,10 +414,9 @@ struct lov_obd {
 
 #define lmv_tgt_desc lu_tgt_desc
 
-struct qos_exclude_prefix {
+struct qos_exclude_pattern {
 	struct list_head	qep_list;
-	struct rhash_head	qep_hash;
-	char			qep_name[NAME_MAX + 1];
+	char			qep_name[NAME_MAX + 3]; /* +2 for ".*" */
 };
 
 struct lmv_obd {

lustre/lmv/lmv_internal.h
@@ -198,9 +198,6 @@ struct lmv_tgt_desc *lmv_locate_tgt(struct lmv_obd *lmv,
 				    struct md_op_data *op_data);
 int lmv_old_layout_lookup(struct lmv_obd *lmv, struct md_op_data *op_data);
 
-extern const struct rhashtable_params qos_exclude_hash_params;
-void qos_exclude_prefix_free(void *vprefix, void *data);
-
 /* lproc_lmv.c */
 int lmv_tunables_init(struct obd_device *obd);
 #endif

lustre/lmv/lproc_lmv.c
@@ -179,21 +179,23 @@ static ssize_t qos_threshold_rr_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(qos_threshold_rr);
 
-/* directories with exclude prefixes will be created on the same MDT as its
- * parent directory, the prefixes are set with the rule as shell environment
- * PATH: ':' is used as separator for prefixes. And for convenience, '+/-' is
- * used to add/remove prefixes.
- */
-static int qos_exclude_prefixes_seq_show(struct seq_file *m, void *v)
+static int qos_exclude_seq_show_internal(struct seq_file *m, void *v,
+						bool is_prefix)
 {
 	struct obd_device *obd = m->private;
 	struct lmv_obd *lmv = &obd->u.lmv;
-	struct qos_exclude_prefix *prefix;
+	struct qos_exclude_pattern *pat;
 
 restart:
 	spin_lock(&lmv->lmv_lock);
-	list_for_each_entry(prefix, &lmv->lmv_qos_exclude_list, qep_list) {
-		seq_printf(m, "%s\n", prefix->qep_name);
+	list_for_each_entry(pat, &lmv->lmv_qos_exclude_list, qep_list) {
+		if (is_prefix) {
+			size_t len = strnlen(pat->qep_name, NAME_MAX + 3);
+			if (len >= 2 && pat->qep_name[len - 2] == '.' &&
+			    pat->qep_name[len - 1] == '*')
+				continue;
+		}
+		seq_printf(m, "%s\n", pat->qep_name);
 		if (seq_has_overflowed(m)) {
 			spin_unlock(&lmv->lmv_lock);
 			kvfree(m->buf);
@@ -209,9 +211,25 @@ restart:
 	return 0;
 }
 
-static ssize_t qos_exclude_prefixes_seq_write(struct file *file,
+/* directories with exclude patterns will be created on the same MDT as its
+ * parent directory, the patterns are set with the rule as shell environment
+ * PATH: ':' is used as separator for patterns. And for convenience, '+/-' is
+ * used to add/remove patterns.
+ */
+static int qos_exclude_patterns_seq_show(struct seq_file *m, void *v)
+{
+	return qos_exclude_seq_show_internal(m, v, false);
+}
+
+static int qos_exclude_prefixes_seq_show(struct seq_file *m, void *v)
+{
+	return qos_exclude_seq_show_internal(m, v, true);
+}
+
+static ssize_t qos_exclude_seq_write_internal(struct file *file,
 					      const char __user *buffer,
-					      size_t count, loff_t *off)
+					      size_t count, loff_t *off,
+					      bool is_prefix)
 {
 	struct obd_device *obd;
 	struct lmv_obd *lmv;
@@ -219,12 +237,12 @@ static ssize_t qos_exclude_prefixes_seq_write(struct file *file,
 	char op = 0;
 	char *p;
 	char *name;
-	char namebuf[NAME_MAX + 1];
-	struct qos_exclude_prefix *prefix;
-	struct qos_exclude_prefix *tmp;
+	char namebuf[NAME_MAX + 3];
+	struct qos_exclude_pattern *pat;
+	struct qos_exclude_pattern *tmp;
 	int len;
 	bool pruned = false;
-	int rc;
+	bool again = false;
 
 	/* one extra char to ensure buf ends with '\0' */
 	OBD_ALLOC(buf, count + 1);
@@ -260,60 +278,60 @@ static ssize_t qos_exclude_prefixes_seq_write(struct file *file,
 			OBD_FREE(buf, count + 1);
 			return -ERANGE;
 		}
-
+		strncpy(namebuf, name, len);
+		namebuf[len] = '\0';
+		again = is_prefix;
 		switch (op) {
 		default:
 			if (!pruned) {
 				spin_lock(&lmv->lmv_lock);
-				list_for_each_entry_safe(prefix, tmp,
+				list_for_each_entry_safe(pat, tmp,
 						&lmv->lmv_qos_exclude_list,
 						qep_list) {
-					list_del(&prefix->qep_list);
-					rhashtable_remove_fast(
-						&lmv->lmv_qos_exclude_hash,
-						&prefix->qep_hash,
-						qos_exclude_hash_params);
-					kfree(prefix);
+					list_del(&pat->qep_list);
+					OBD_FREE_PTR(pat);
 				}
 				spin_unlock(&lmv->lmv_lock);
 				pruned = true;
 			}
 			fallthrough;
 		case '+':
-			prefix = kmalloc(sizeof(*prefix), __GFP_ZERO);
-			if (!prefix) {
+again_plus:
+			OBD_ALLOC_PTR(pat);
+			if (!pat) {
 				OBD_FREE(buf, count + 1);
 				return -ENOMEM;
 			}
-			strncpy(prefix->qep_name, name, len);
-			rc = rhashtable_lookup_insert_fast(
-						&lmv->lmv_qos_exclude_hash,
-						&prefix->qep_hash,
-						qos_exclude_hash_params);
-			if (!rc) {
-				spin_lock(&lmv->lmv_lock);
-				list_add_tail(&prefix->qep_list,
-					      &lmv->lmv_qos_exclude_list);
-				spin_unlock(&lmv->lmv_lock);
-			} else {
-				kfree(prefix);
+			strncpy(pat->qep_name, namebuf, len);
+			spin_lock(&lmv->lmv_lock);
+			list_add_tail(&pat->qep_list,
+						&lmv->lmv_qos_exclude_list);
+			spin_unlock(&lmv->lmv_lock);
+			if (again) {
+				again = false;
+				namebuf[len++] = '.';
+				namebuf[len++] = '*';
+				namebuf[len]   = '\0';
+				goto again_plus;
 			}
 			break;
 		case '-':
-			strncpy(namebuf, name, len);
-			namebuf[len] = '\0';
-			prefix = rhashtable_lookup(&lmv->lmv_qos_exclude_hash,
-						   namebuf,
-						   qos_exclude_hash_params);
-			if (prefix) {
-				spin_lock(&lmv->lmv_lock);
-				list_del(&prefix->qep_list);
-				spin_unlock(&lmv->lmv_lock);
-				rhashtable_remove_fast(
-						&lmv->lmv_qos_exclude_hash,
-						&prefix->qep_hash,
-						qos_exclude_hash_params);
-				kfree(prefix);
+again_minus:
+			spin_lock(&lmv->lmv_lock);
+			list_for_each_entry_safe(pat, tmp,
+				&lmv->lmv_qos_exclude_list, qep_list) {
+				if (strcmp(pat->qep_name, namebuf) == 0) {
+					list_del(&pat->qep_list);
+					OBD_FREE_PTR(pat);
+				}
+			}
+			spin_unlock(&lmv->lmv_lock);
+			if (again) {
+				again = false;
+				namebuf[len++] = '.';
+				namebuf[len++] = '*';
+				namebuf[len]   = '\0';
+				goto again_minus;
 			}
 			break;
 		}
@@ -321,10 +339,30 @@ static ssize_t qos_exclude_prefixes_seq_write(struct file *file,
 
 	OBD_FREE(buf, count + 1);
 	return count;
+
+}
+
+static ssize_t qos_exclude_patterns_seq_write(struct file *file,
+					      const char __user *buffer,
+					      size_t count, loff_t *off)
+{
+	return qos_exclude_seq_write_internal(file, buffer, count, off,
+					      false);
+}
+LDEBUGFS_SEQ_FOPS(qos_exclude_patterns);
+
+static ssize_t qos_exclude_prefixes_seq_write(struct file *file,
+					      const char __user *buffer,
+					      size_t count, loff_t *off)
+{
+	return qos_exclude_seq_write_internal(file, buffer, count, off,
+					      true);
 }
 LDEBUGFS_SEQ_FOPS(qos_exclude_prefixes);
 
 static struct ldebugfs_vars ldebugfs_lmv_obd_vars[] = {
+	{ .name =	"qos_exclude_patterns",
+	  .fops =	&qos_exclude_patterns_fops },
 	{ .name =	"qos_exclude_prefixes",
 	  .fops =	&qos_exclude_prefixes_fops },
 	{ NULL }

lustre/tests/sanity.sh
@@ -32211,9 +32211,6 @@ test_413j()
 run_test 413j "set default LMV by setxattr"
 
 test_413k() {
-	(( $MDS1_VERSION >= $(version_code 2.15.60) )) ||
-		skip "Need server version at least 2.15.60"
-
 	local index1
 	local index2
 	local old=$($LCTL get_param -n lmv.*.qos_exclude_prefixes)
@@ -32246,6 +32243,31 @@ test_413k() {
 		error "prefixes count $count2 != $((count + 26))"
 }
 run_test 413k "QoS mkdir exclude prefixes"
+
+test_413l() {
+	local index1
+	local index2
+	local old=$($LCTL get_param -n lmv.*.qos_exclude_patterns)
+	local count=$($LCTL get_param -n lmv.*.qos_exclude_patterns | wc -l)
+	local patterns="????-????:[a-z][0-9]"
+
+	# add patterns
+	stack_trap "$LCTL set_param lmv.*.qos_exclude_patterns=\"$old\""
+	$LCTL set_param lmv.*.qos_exclude_patterns="+$patterns"
+
+	mkdir $DIR/$tdir || error "mkdir $tdir failed"
+	index1=$($LFS getstripe -m $DIR/$tdir)
+	for dname in abcd-1234 b9; do
+		mkdir "$DIR/$tdir/$dname" || error "mkdir $dname failed"
+		index2=$($LFS getstripe -m "$DIR/$tdir/$dname")
+		((index1 == index2)) ||
+			error "$tdir on MDT$index1, $dname on MDT$index2"
+	done
+
+	# remove patterns
+	$LCTL set_param lmv.*.qos_exclude_patterns="-$patterns"
+}
+run_test 413l "QoS mkdir exclude patterns"
 
 test_413z() {
 	local pids=""

commit 3d9e884fed980a75f582a32301a0552b892a7fe6

LU-20495 lmv: fix use-after-free in qos exclude list
lmv_qos_exclude() walked lmv_qos_exclude_list without a lock while
the qos_exclude_{patterns,prefixes} handlers free entries under
lmv_lock, so a mkdir racing "lctl set_param qos_exclude_patterns"
dereferenced a freed entry and panicked the client.

Take lmv_lock around the walk, as the seq_show path already does.

Fixes: 5fe3d1c7b87b ("LU-19268 lmv: Add qos_exclude_patterns")
Assisted-by: ClaudeCode:Opus-4.8
Signed-off-by: Sohei Koyama <skoyama@ddn.com>
Change-Id: Ia222d635587c0f59a09ef85cc3647220850525f6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67395
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lmv/lmv_obd.c
@@ -2221,17 +2221,22 @@ static bool lmv_qos_exclude(struct lmv_obd *lmv, struct md_op_data *op_data)
 {
 	const char *name = op_data->op_name;
 	struct qos_exclude_pattern *pat;
+	bool excluded = false;
 
 	/* skip encrypted files */
 	if (op_data->op_file_encctx)
 		return false;
 
+	spin_lock(&lmv->lmv_lock);
 	list_for_each_entry(pat, &lmv->lmv_qos_exclude_list, qep_list) {
-		if (glob_match(pat->qep_name, name))
-			return true;
+		if (glob_match(pat->qep_name, name)) {
+			excluded = true;
+			break;
+		}
 	}
+	spin_unlock(&lmv->lmv_lock);
 
-	return false;
+	return excluded;
 }
 
 struct lmv_tgt_desc *lmv_locate_tgt_create(struct obd_device *obd,