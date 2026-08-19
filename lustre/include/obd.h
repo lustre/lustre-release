lustre/lustre-release

commit 76626d6c52b19b5cca04007c4b1656cc52a487c1

LU-13344 all: Separate debugfs and procfs handling
Linux 5.6 introduces proc_ops with v5.5-8862-gd56c0d45f0e2
proc: decouple proc from VFS with "struct proc_ops"

Separate debugfs usage and procfs usage to prepare for the divergence
of debugfs using file_operations and procfs using proc_ops

HPE-bug-id: LUS-8589
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I1746e563b55a9e89f90ac01843c304fe6b690d8b
Reviewed-on: https://review.whamcloud.com/37834
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Reviewed-by: Neil Brown <neilb@suse.de>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/include/obd.h
@@ -733,6 +733,7 @@ struct obd_device {
 	struct lprocfs_stats	*obd_svc_stats;
 	const struct attribute	       **obd_attrs;
 	struct lprocfs_vars	*obd_vars;
+	struct ldebugfs_vars	*obd_debugfs_vars;
 	atomic_t		obd_evict_inprogress;
 	wait_queue_head_t	obd_evict_inprogress_waitq;
 	struct list_head	obd_evict_list;	/* protected with pet_lock */

lustre/obdclass/lprocfs_status.c
@@ -112,9 +112,9 @@ struct proc_dir_entry *lprocfs_add_symlink(const char *name,
 }
 EXPORT_SYMBOL(lprocfs_add_symlink);
 
-static const struct file_operations lprocfs_generic_fops = { };
+static const struct file_operations ldebugfs_empty_ops = { };
 
-void ldebugfs_add_vars(struct dentry *parent, struct lprocfs_vars *list,
+void ldebugfs_add_vars(struct dentry *parent, struct ldebugfs_vars *list,
 		       void *data)
 {
 	if (IS_ERR_OR_NULL(parent) || IS_ERR_OR_NULL(list))
@@ -133,11 +133,13 @@ void ldebugfs_add_vars(struct dentry *parent, struct lprocfs_vars *list,
 		}
 		debugfs_create_file(list->name, mode, parent,
 				    list->data ? : data,
-				    list->fops ? : &lprocfs_generic_fops);
+				    list->fops ? : &ldebugfs_empty_ops);
 		list++;
 	}
 }
 EXPORT_SYMBOL_GPL(ldebugfs_add_vars);
+
+static const struct file_operations lprocfs_empty_ops = { };
 
 /**
  * Add /proc entries.
@@ -170,7 +172,7 @@ lprocfs_add_vars(struct proc_dir_entry *root, struct lprocfs_vars *list,
 				mode |= 0200;
 		}
 		proc = proc_create_data(list->name, mode, root,
-					list->fops ?: &lprocfs_generic_fops,
+					list->fops ?: &lprocfs_empty_ops,
 					list->data ?: data);
 		if (!proc)
 			return -ENOMEM;
@@ -1045,7 +1047,7 @@ static void obd_sysfs_release(struct kobject *kobj)
 
 int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only)
 {
-	struct lprocfs_vars *debugfs_vars = NULL;
+	struct ldebugfs_vars *debugfs_vars = NULL;
 	int rc;
 
 	if (!obd || obd->obd_magic != OBD_DEVICE_MAGIC)
@@ -1077,7 +1079,7 @@ int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only)
 	}
 
 	if (!obd->obd_type->typ_procroot)
-		debugfs_vars = obd->obd_vars;
+		debugfs_vars = obd->obd_debugfs_vars;
 	obd->obd_debugfs_entry = debugfs_create_dir(
 		obd->obd_name, obd->obd_type->typ_debugfs_entry);
 	ldebugfs_add_vars(obd->obd_debugfs_entry, debugfs_vars, obd);
@@ -1398,7 +1400,7 @@ static int lprocfs_stats_seq_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-const struct file_operations lprocfs_stats_seq_fops = {
+const struct file_operations ldebugfs_stats_seq_fops = {
 	.owner   = THIS_MODULE,
 	.open    = lprocfs_stats_seq_open,
 	.read    = seq_read,
@@ -1406,7 +1408,16 @@ const struct file_operations lprocfs_stats_seq_fops = {
 	.llseek  = seq_lseek,
 	.release = lprocfs_seq_release,
 };
-EXPORT_SYMBOL(lprocfs_stats_seq_fops);
+EXPORT_SYMBOL(ldebugfs_stats_seq_fops);
+
+static const struct file_operations lprocfs_stats_seq_fops = {
+	.owner   = THIS_MODULE,
+	.open    = lprocfs_stats_seq_open,
+	.read    = seq_read,
+	.write   = lprocfs_stats_seq_write,
+	.llseek  = seq_lseek,
+	.release = lprocfs_seq_release,
+};
 
 int lprocfs_register_stats(struct proc_dir_entry *root, const char *name,
                            struct lprocfs_stats *stats)

lustre/fid/fid_internal.h
@@ -55,7 +55,7 @@ enum {
 
 extern struct lu_context_key seq_thread_key;
 
-extern struct lprocfs_vars seq_server_debugfs_list[];
+extern struct ldebugfs_vars seq_server_debugfs_list[];
 
 /* Store API functions. */
 struct dt_device;
@@ -89,7 +89,7 @@ int seq_client_alloc_super(struct lu_client_seq *seq,
 
 extern struct dentry *seq_debugfs_dir;
 
-extern struct lprocfs_vars seq_client_debugfs_list[];
+extern struct ldebugfs_vars seq_client_debugfs_list[];
 
 extern const struct file_operations seq_fld_debugfs_seq_fops;
 

lustre/fid/lproc_fid.c
@@ -199,7 +199,7 @@ LDEBUGFS_SEQ_FOPS(ldebugfs_server_fid_space);
 LDEBUGFS_SEQ_FOPS(ldebugfs_server_fid_width);
 LDEBUGFS_SEQ_FOPS_RO(ldebugfs_server_fid_server);
 
-struct lprocfs_vars seq_server_debugfs_list[] = {
+struct ldebugfs_vars seq_server_debugfs_list[] = {
 	{ .name	=	"space",
 	  .fops	=	&ldebugfs_server_fid_space_fops	},
 	{ .name	=	"width",
@@ -608,7 +608,7 @@ LDEBUGFS_SEQ_FOPS(ldebugfs_client_fid_width);
 LDEBUGFS_SEQ_FOPS_RO(ldebugfs_client_fid_server);
 LDEBUGFS_SEQ_FOPS_RO(ldebugfs_client_fid_fid);
 
-struct lprocfs_vars seq_client_debugfs_list[] = {
+struct ldebugfs_vars seq_client_debugfs_list[] = {
 	{ .name	=	"space",
 	  .fops	=	&ldebugfs_client_fid_space_fops	},
 	{ .name	=	"width",

lustre/fld/fld_internal.h
@@ -179,7 +179,7 @@ int fld_client_rpc(struct obd_export *exp,
                    struct lu_seq_range *range, __u32 fld_op,
 		   struct ptlrpc_request **reqp);
 
-extern struct lprocfs_vars fld_client_debugfs_list[];
+extern struct ldebugfs_vars fld_client_debugfs_list[];
 
 struct fld_cache *fld_cache_init(const char *name,
                                  int cache_size, int cache_threshold);

lustre/fld/lproc_fld.c
@@ -135,7 +135,7 @@ LDEBUGFS_SEQ_FOPS_RO(fld_debugfs_targets);
 LDEBUGFS_SEQ_FOPS(fld_debugfs_hash);
 LDEBUGFS_FOPS_WR_ONLY(fld, cache_flush);
 
-struct lprocfs_vars fld_client_debugfs_list[] = {
+struct ldebugfs_vars fld_client_debugfs_list[] = {
 	{ .name	=	"targets",
 	  .fops	=	&fld_debugfs_targets_fops	},
 	{ .name	=	"hash",

lustre/include/dt_object.h
@@ -2941,7 +2941,7 @@ static inline struct dt_thread_info *dt_info(const struct lu_env *env)
 int dt_global_init(void);
 void dt_global_fini(void);
 int dt_tunables_init(struct dt_device *dt, struct obd_type *type,
-		     const char *name, struct lprocfs_vars *list);
+		     const char *name, struct ldebugfs_vars *list);
 int dt_tunables_fini(struct dt_device *dt);
 
 # ifdef CONFIG_PROC_FS

lustre/include/lprocfs_status.h
@@ -49,13 +49,25 @@
 #include <libcfs/libcfs.h>
 #include <uapi/linux/lustre/lustre_idl.h>
 
+/*
+ * Liuux 5.6 introduces proc_ops with v5.5-8862-gd56c0d45f0e2
+ * Now that proc and debugfs use separate operation vector types
+ * separate containers are also needed.
+ */
 struct lprocfs_vars {
 	const char			*name;
 	const struct file_operations	*fops;
 	void				*data;
-	/**
-	 * /proc file mode.
-	 */
+	/** /proc file mode. */
+	mode_t				 proc_mode;
+};
+
+/** Provide a debugfs container */
+struct ldebugfs_vars {
+	const char			*name;
+	const struct file_operations	*fops;
+	void				*data;
+	/** debugfs file mode. */
 	mode_t				 proc_mode;
 };
 
@@ -521,13 +533,17 @@ lprocfs_nid_stats_clear_seq_write(struct file *file, const char __user *buffer,
 					size_t count, loff_t *off);
 extern int lprocfs_nid_stats_clear_seq_show(struct seq_file *file, void *data);
 #endif
-extern const struct file_operations lprocfs_stats_seq_fops;
 extern int lprocfs_register_stats(struct proc_dir_entry *root, const char *name,
-                                  struct lprocfs_stats *stats);
+				  struct lprocfs_stats *stats);
+extern const struct file_operations ldebugfs_stats_seq_fops;
 
 /* lprocfs_status.c */
-extern void ldebugfs_add_vars(struct dentry *parent, struct lprocfs_vars *var,
+extern void ldebugfs_add_vars(struct dentry *parent, struct ldebugfs_vars *var,
 			      void *data);
+extern struct dentry *ldebugfs_register(const char *name,
+					struct dentry *parent,
+					struct ldebugfs_vars *list,
+					void *data);
 extern int lprocfs_add_vars(struct proc_dir_entry *root,
 			    struct lprocfs_vars *var, void *data);
 
@@ -582,8 +598,15 @@ ssize_t ping_show(struct kobject *kobj, struct attribute *attr,
 		  char *buffer);
 
 extern ssize_t
+ldebugfs_import_seq_write(struct file *file, const char __user *buffer,
+			  size_t count, loff_t *off);
+static inline ssize_t
 lprocfs_import_seq_write(struct file *file, const char __user *buffer,
-			 size_t count, loff_t *off);
+			 size_t count, loff_t *off)
+{
+	return ldebugfs_import_seq_write(file, buffer, count, off);
+}
+
 extern int lprocfs_pinger_recov_seq_show(struct seq_file *m, void *data);
 extern ssize_t
 lprocfs_pinger_recov_seq_write(struct file *file, const char __user *buffer,
@@ -970,6 +993,10 @@ lprocfs_evict_client_seq_write(struct file *file, const char __user *buffer,
 static inline ssize_t
 lprocfs_ping_seq_write(struct file *file, const char __user *buffer,
 		       size_t count, loff_t *off)
+{ return 0; }
+static inline ssize_t
+ldebugfs_import_seq_write(struct file *file, const char __user *buffer,
+			  size_t count, loff_t *off)
 { return 0; }
 static inline ssize_t
 lprocfs_import_seq_write(struct file *file, const char __user *buffer,

commit 2a2e340d87b75ace248052c2538f28638fe46c7b

LU-16518 obdclass: remove unused obd_vars variable
The last user of this variable was removed during
procfs cleanup.


Fixes: 76626d6c52 ("LU-13344 all: Separate debugfs and procfs handling")
Fixes: 19dc44466a ("LU-8066 mgs: migrate files out of procfs")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I9bd3e001dd8d325a4c79b238d653536d6a6a6964
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67663
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/include/obd.h
@@ -778,7 +778,6 @@ struct obd_device {
 	struct dentry			*obd_svc_debugfs_entry;
 	struct lprocfs_stats		*obd_svc_stats;
 	const struct attribute	       **obd_attrs;
-	struct lprocfs_vars	*obd_vars;
 	struct ldebugfs_vars	*obd_debugfs_vars;
 	struct list_head	obd_evict_list;	/* protected with pet_lock */
 	atomic_t		obd_eviction_count;

lustre/obdclass/lprocfs_status.c
@@ -1343,7 +1343,7 @@ int lprocfs_obd_setup(struct obd_device *obd, bool uuid_only)
 
 	obd->obd_proc_entry = lprocfs_register(obd->obd_name,
 					       obd->obd_type->typ_procroot,
-					       obd->obd_vars, obd);
+					       NULL, obd);
 	if (IS_ERR(obd->obd_proc_entry)) {
 		rc = PTR_ERR(obd->obd_proc_entry);
 		CERROR("error %d setting up lprocfs for %s\n", rc, obd->obd_name);

lustre/osd-ldiskfs/osd_internal.h
@@ -950,7 +950,6 @@ static inline char *osd_oid_name(char *name, size_t name_size,
 
 #ifdef CONFIG_PROC_FS
 /* osd_lproc.c */
-extern struct lprocfs_vars lprocfs_osd_obd_vars[];
 int osd_procfs_init(struct osd_device *osd, const char *name);
 void osd_procfs_fini(struct osd_device *osd);
 void osd_brw_stats_update(struct osd_device *osd, struct osd_iobuf *iobuf);

lustre/osd-zfs/osd_internal.h
@@ -619,8 +619,6 @@ enum {
 
 extern struct kmem_cache *osd_zapit_cachep;
 /* osd_lproc.c */
-extern struct lprocfs_vars lprocfs_osd_obd_vars[];
-
 int osd_procfs_init(struct osd_device *osd, const char *name);
 void osd_procfs_fini(struct osd_device *osd);
 