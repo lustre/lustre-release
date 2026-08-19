lustre/lustre-release

commit e826e8bb77bffce346f570ea5348fa0762c792d6

LU-17961 sec: support supplementary groups from client
The usual way to support more than 2 supplementary groups is to
resort to the server side's identity upcall. This identity upcall
retrieves all user's credentials, including all supplementary groups,
and stores them in cache. But this access to user's credentials from
server side is not always an option.

As an alternative to the server side's identity upcall, we implement
a retry mechanism for intent locking. The client can provide at most
2 supplementary groups in the request sent to the MDS, but sometimes
it does not know which ones are useful for credentials calculation on
server side. For instance in case of lookup, the client does not have
the child inode yet when it sends the intent lock request.
Hopefully, the server can hint at the useful groups, by putting in the
request reply the target inode's GID, and also its ACL. So, in case
the server replies -EACCES, we check the user's credentials against
those, and try again the intent lock request if we find a matching
supplementary group.

On server side, we add an INTERNAL dedicated, separate upcall cache.
This makes it distinct from the regular identity upcall cache that can
be defined to use any upcall including NONE, per an MDT side tuning.
It implements a particular behavior which does not involve an actual
upcall, but instead the cache is filled with supplementary groups read
from the client request, cumulatively at each request.
Dedicated mdt-side tunables are created to configure the entry expiry
time and the acquire expire time for INTERNAL, as well as a tunable to
flush the INTERNAL upcall cache.

A problem with the 'runas' utility was found during testing. If no
supplementary group is provided via the '-G' option, then it needs at
least to set the given GID as a supplementary group. Otherwise the
supplementary groups of the invoking user would be silently inherited.
For instance, if root user calls 'runas -u 500 -g 500 $CMD', we must

lustre/obdclass/upcall_cache.c
@@ -37,6 +37,7 @@
 #include <libcfs/libcfs.h>
 #include <uapi/linux/lnet/lnet-types.h>
 #include <upcall_cache.h>
+#include "upcall_cache_internal.h"
 
 static struct upcall_cache_entry *alloc_entry(struct upcall_cache *cache,
 					      __u64 key, void *args)
@@ -57,19 +58,6 @@ static struct upcall_cache_entry *alloc_entry(struct upcall_cache *cache,
 	if (cache->uc_ops->init_entry)
 		cache->uc_ops->init_entry(entry, args);
 	return entry;
-}
-
-/* protected by cache lock */
-static void free_entry(struct upcall_cache *cache,
-		       struct upcall_cache_entry *entry)
-{
-	if (cache->uc_ops->free_entry)
-		cache->uc_ops->free_entry(cache, entry);
-
-	list_del(&entry->ue_hash);
-	CDEBUG(D_OTHER, "destroy cache entry %p for key %llu\n",
-		entry, entry->ue_key);
-	LIBCFS_FREE(entry, sizeof(*entry));
 }
 
 static inline int upcall_compare(struct upcall_cache *cache,
@@ -96,20 +84,6 @@ static inline int downcall_compare(struct upcall_cache *cache,
 		return cache->uc_ops->downcall_compare(cache, entry, key, args);
 
 	return 0;
-}
-
-static inline void get_entry(struct upcall_cache_entry *entry)
-{
-	atomic_inc(&entry->ue_refcount);
-}
-
-static inline void put_entry(struct upcall_cache *cache,
-			     struct upcall_cache_entry *entry)
-{
-	if (atomic_dec_and_test(&entry->ue_refcount) &&
-	    (UC_CACHE_IS_INVALID(entry) || UC_CACHE_IS_EXPIRED(entry))) {
-		free_entry(cache, entry);
-	}
 }
 
 static inline void write_lock_from_read(rwlock_t *lock, bool *writelock)
@@ -190,7 +164,7 @@ out:
 EXPORT_SYMBOL(upcall_cache_set_upcall);
 
 static inline int refresh_entry(struct upcall_cache *cache,
-			 struct upcall_cache_entry *entry)
+				struct upcall_cache_entry *entry, __u32 fsgid)
 {
 	LASSERT(cache->uc_ops->do_upcall);
 	return cache->uc_ops->do_upcall(cache, entry);
@@ -200,11 +174,13 @@ struct upcall_cache_entry *upcall_cache_get_entry(struct upcall_cache *cache,
 						  __u64 key, void *args)
 {
 	struct upcall_cache_entry *entry = NULL, *new = NULL, *next;
+	gid_t fsgid = (__u32)__kgid_val(INVALID_GID);
+	struct group_info *ginfo = NULL;
 	bool failedacquiring = false;
 	struct list_head *head;
 	wait_queue_entry_t wait;
 	bool writelock;
-	int rc, found;
+	int rc = 0, found;
 
 	ENTRY;
 
@@ -267,13 +243,27 @@ find_with_lock:
 	/* now we hold a write lock */
 	get_entry(entry);
 
+	/* special processing of supp groups for identity upcall */
+	if (strcmp(cache->uc_upcall, IDENTITY_UPCALL_INTERNAL) == 0) {
+		write_unlock(&cache->uc_lock);
+		rc = upcall_cache_get_entry_internal(cache, entry, args,
+						     &fsgid, &ginfo);
+		write_lock(&cache->uc_lock);
+		if (rc)
+			GOTO(out, entry = ERR_PTR(rc));
+	}
+
 	/* acquire for new one */
 	if (UC_CACHE_IS_NEW(entry)) {
-		UC_CACHE_SET_ACQUIRING(entry);
 		UC_CACHE_CLEAR_NEW(entry);
-		write_unlock(&cache->uc_lock);
-		rc = refresh_entry(cache, entry);
-		write_lock(&cache->uc_lock);
+		if (strcmp(cache->uc_upcall, IDENTITY_UPCALL_INTERNAL) == 0) {
+			refresh_entry_internal(cache, entry, fsgid, &ginfo);
+		} else {
+			UC_CACHE_SET_ACQUIRING(entry);
+			write_unlock(&cache->uc_lock);
+			rc = refresh_entry(cache, entry, fsgid);
+			write_lock(&cache->uc_lock);
+		}
 		entry->ue_acquire_expire = ktime_get_seconds() +
 					   cache->uc_acquire_expire;
 		if (rc < 0) {
@@ -360,6 +350,8 @@ out:
 		write_unlock(&cache->uc_lock);
 	else
 		read_unlock(&cache->uc_lock);
+	if (ginfo)
+		groups_free(ginfo);
 	RETURN(entry);
 }
 EXPORT_SYMBOL(upcall_cache_get_entry);

lustre/include/lustre_idmap.h
@@ -43,6 +43,7 @@
 struct lu_ucred;
 
 extern void lustre_groups_from_list(struct group_info *ginfo, gid_t *glist);
+extern void lustre_list_from_groups(gid_t *glist, struct group_info *ginfo);
 extern void lustre_groups_sort(struct group_info *group_info);
 extern int lustre_groups_search(struct group_info *group_info, gid_t grp);
 extern int lustre_in_group_p(struct lu_ucred *mu, gid_t grp);

lustre/include/upcall_cache.h
@@ -20,6 +20,13 @@
 #include <obd.h>
 #include <lustre_sec.h>
 
+/* The special identity_upcall value "INTERNAL" implements a particular behavior
+ * which does not involve an actual upcall. Instead, the cache is filled with
+ * supplementary groups read from the user's credentials provided as input
+ * (usually got from the client request), cumulatively at each request.
+ */
+#define IDENTITY_UPCALL_INTERNAL	"INTERNAL"
+
 /** \defgroup ucache ucache
  *
  * @{

lustre/llite/file.c
@@ -700,8 +700,8 @@ retry:
 
 	CFS_FAIL_TIMEOUT(OBD_FAIL_LLITE_OPEN_DELAY, cfs_fail_val);
 
-	rc = md_intent_lock(sbi->ll_md_exp, op_data, itp, &req,
-			    &ll_md_blocking_ast, 0);
+	rc = ll_intent_lock(sbi->ll_md_exp, op_data, itp, &req,
+			    &ll_md_blocking_ast, 0, true);
 	kfree(name);
 	ll_finish_md_op_data(op_data);
 	if (rc == -ESTALE) {
@@ -1250,7 +1250,7 @@ ll_lease_open(struct inode *inode, struct file *file, fmode_t fmode,
 
 	it.it_open_flags = fmode | open_flags;
 	it.it_open_flags |= MDS_OPEN_LOCK | MDS_OPEN_BY_FID | MDS_OPEN_LEASE;
-	rc = md_intent_lock(sbi->ll_md_exp, op_data, &it, &req,
+	rc = ll_intent_lock(sbi->ll_md_exp, op_data, &it, &req,
 			    &ll_md_blocking_lease_ast,
 	/* LDLM_FL_NO_LRU: To not put the lease lock into LRU list, otherwise
 	 * it can be cancelled which may mislead applications that the lease is
@@ -1259,7 +1259,8 @@ ll_lease_open(struct inode *inode, struct file *file, fmode_t fmode,
 	 * open in ll_md_blocking_ast(). Otherwise as ll_md_blocking_lease_ast
 	 * doesn't deal with openhandle, so normal openhandle will be leaked.
 	 */
-			    LDLM_FL_NO_LRU | LDLM_FL_EXCL);
+			    LDLM_FL_NO_LRU | LDLM_FL_EXCL,
+			    true);
 	ll_finish_md_op_data(op_data);
 	ptlrpc_req_put(req);
 	if (rc < 0)
@@ -6015,7 +6016,8 @@ static int ll_inode_revalidate(struct dentry *dentry, enum ldlm_intent_flags op)
 		RETURN(PTR_ERR(op_data));
 
 	op_data->op_flags |= flags;
-	rc = md_intent_lock(exp, op_data, &oit, &req, &ll_md_blocking_ast, 0);
+	rc = ll_intent_lock(exp, op_data, &oit, &req,
+			    &ll_md_blocking_ast, 0, true);
 	ll_finish_md_op_data(op_data);
 	if (rc < 0) {
 		rc = ll_inode_revalidate_fini(inode, rc);
@@ -6907,8 +6909,8 @@ static int ll_layout_intent(struct inode *inode, struct layout_intent *intent)
 	LDLM_DEBUG_NOLOCK("%s: requeue layout lock for file "DFID"(%p)",
 			  sbi->ll_fsname, PFID(&lli->lli_fid), inode);
 
-	rc = md_intent_lock(sbi->ll_md_exp, op_data, &it, &req,
-			    &ll_md_blocking_ast, 0);
+	rc = ll_intent_lock(sbi->ll_md_exp, op_data, &it, &req,
+			    &ll_md_blocking_ast, 0, true);
 	if (it.it_request != NULL)
 		ptlrpc_req_put(it.it_request);
 	it.it_request = NULL;

lustre/llite/llite_internal.h
@@ -1354,6 +1354,10 @@ int ll_md_blocking_ast(struct ldlm_lock *lock, struct ldlm_lock_desc *ldesc,
 struct dentry *ll_splice_alias(struct inode *inode, struct dentry *de);
 int ll_rmdir_entry(struct inode *dir, char *name, int namelen);
 void ll_update_times(struct ptlrpc_request *request, struct inode *inode);
+int ll_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
+		   struct lookup_intent *it, struct ptlrpc_request **reqp,
+		   ldlm_blocking_callback cb_blocking, __u64 extra_lock_flags,
+		   bool tryagain);
 
 /* llite/rw.c */
 int ll_writepage(struct page *page, struct writeback_control *wbc);

lustre/llite/namei.c
@@ -823,6 +823,145 @@ out:
 	return rc;
 }
 
+static int get_acl_from_req(struct ptlrpc_request *req, struct posix_acl **acl)
+{
+	struct mdt_body	*body;
+	void *buf;
+	int rc;
+
+	body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
+	if (!body->mbo_aclsize) {
+		*acl = NULL;
+		return 0;
+	}
+
+	buf = req_capsule_server_sized_get(&req->rq_pill, &RMF_ACL,
+					   body->mbo_aclsize);
+	if (!buf)
+		return -EPROTO;
+
+	*acl = posix_acl_from_xattr(&init_user_ns, buf, body->mbo_aclsize);
+	if (IS_ERR_OR_NULL(*acl)) {
+		rc = *acl ? PTR_ERR(*acl) : 0;
+		CDEBUG(D_SEC, "convert xattr to acl: %d\n", rc);
+		return rc;
+	}
+
+	rc = posix_acl_valid(&init_user_ns, *acl);
+	if (rc) {
+		CDEBUG(D_SEC, "validate acl: %d\n", rc);
+		posix_acl_release(*acl);
+		return rc;
+	}
+
+	return 0;
+}
+
+static inline int accmode_from_openflags(u64 open_flags)
+{
+	unsigned int may_mask = 0;
+
+	if (open_flags & (FMODE_READ | FMODE_PREAD))
+		may_mask |= MAY_READ;
+	if (open_flags & (FMODE_WRITE | FMODE_PWRITE))
+		may_mask |= MAY_WRITE;
+	if (open_flags & FMODE_EXEC)
+		may_mask = MAY_EXEC;
+
+	return may_mask;
+}
+
+static __u32 get_uc_group_from_acl(const struct posix_acl *acl, int want)
+{
+	const struct posix_acl_entry *pa, *pe;
+
+	FOREACH_ACL_ENTRY(pa, acl, pe) {
+		switch (pa->e_tag) {
+		case ACL_GROUP_OBJ:
+		case ACL_GROUP:
+			if (in_group_p(pa->e_gid) &&
+			    (pa->e_perm & want) == want)
+				return (__u32)from_kgid(&init_user_ns,
+							pa->e_gid);
+			break;
+		default:
+			/* nothing to do */
+			break;
+		}
+	}
+
+	return (__u32)__kgid_val(INVALID_GID);
+}
+
+/* This function implements a retry mechanism on top of md_intent_lock().
+ * This is useful because the client can provide at most 2 supplementary
+ * groups in the request sent to the MDS, but sometimes it does not know
+ * which ones are useful for credentials calculation on server side. For
+ * instance in case of lookup, the client does not have the child inode yet
+ * when it sends the intent lock request.
+ * Hopefully, the server can hint at the useful groups, by putting in the
+ * request reply the target inode's GID, and also its ACL.
+ * So in case the server replies -EACCES, we check the user's credentials
+ * against those, and try again the intent lock request if we find a matching
+ * supplementary group.
+ */
+int ll_intent_lock(struct obd_export *exp, struct md_op_data *op_data,
+		   struct lookup_intent *it, struct ptlrpc_request **reqp,
+		   ldlm_blocking_callback cb_blocking, __u64 extra_lock_flags,
+		   bool tryagain)
+{
+	int rc;
+
+	ENTRY;
+
+intent:
+	rc = md_intent_lock(exp, op_data, it, reqp, cb_blocking,
+			    extra_lock_flags);
+	CDEBUG(D_VFSTRACE,
+	       "intent lock %d on i1 "DFID" suppgids %d %d: rc %d\n",
+	       it->it_op, PFID(&op_data->op_fid1),
+	       op_data->op_suppgids[0], op_data->op_suppgids[1], rc);
+	if (rc == -EACCES && tryagain && it->it_op & IT_OPEN &&
+	    it_disposition(it, DISP_OPEN_DENY) && *reqp) {
+		struct mdt_body *body;
+		__u32 new_suppgid;
+
+		body = req_capsule_server_get(&(*reqp)->rq_pill, &RMF_MDT_BODY);
+		new_suppgid = body->mbo_gid;
+		CDEBUG(D_SEC, "new suppgid from body: %d\n", new_suppgid);
+		if (op_data->op_suppgids[0] == body->mbo_gid ||
+		    op_data->op_suppgids[1] == body->mbo_gid ||
+		    !in_group_p(make_kgid(&init_user_ns, body->mbo_gid))) {
+			int accmode = accmode_from_openflags(it->it_open_flags);
+			struct posix_acl *acl;
+
+			rc = get_acl_from_req(*reqp, &acl);
+			if (rc || !acl)
+				GOTO(out, rc = -EACCES);
+
+			new_suppgid = get_uc_group_from_acl(acl, accmode);
+			posix_acl_release(acl);
+			CDEBUG(D_SEC, "new suppgid from acl: %d\n",
+			       new_suppgid);
+
+			if (new_suppgid == (__u32)__kgid_val(INVALID_GID))
+				GOTO(out, rc = -EACCES);
+		}
+
+		if (!(it->it_open_flags & MDS_OPEN_BY_FID))
+			fid_zero(&op_data->op_fid2);
+		op_data->op_suppgids[1] = new_suppgid;
+		ptlrpc_req_put(*reqp);
+		*reqp = NULL;
+		ll_intent_release(it);
+		tryagain = false;
+		goto intent;
+	}
+
+out:
+	RETURN(rc);
+}
+
 static struct dentry *ll_lookup_it(struct inode *parent, struct dentry *dentry,
 				   struct lookup_intent *it,
 				   void **secctx, __u32 *secctxlen,
@@ -1019,35 +1158,14 @@ inherit:
 		it->it_open_flags |= MDS_OPEN_PCC;
 	}
 
-	rc = md_intent_lock(ll_i2mdexp(parent), op_data, it, &req,
-			    &ll_md_blocking_ast, 0);
 	/* If the MDS allows the client to chgrp (CFS_SETGRP_PERM), but the
 	 * client does not know which suppgid should be sent to the MDS, or
 	 * some other(s) changed the target file's GID after this RPC sent
 	 * to the MDS with the suppgid as the original GID, then we should
 	 * try again with right suppgid.
 	 */
-	if (rc == -EACCES && it->it_op & IT_OPEN &&
-	    it_disposition(it, DISP_OPEN_DENY)) {
-		struct mdt_body *body;
-
-		LASSERT(req != NULL);
-
-		body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
-		if (op_data->op_suppgids[0] == body->mbo_gid ||
-		    op_data->op_suppgids[1] == body->mbo_gid ||
-		    !in_group_p(make_kgid(&init_user_ns, body->mbo_gid)))
-			GOTO(out, retval = ERR_PTR(-EACCES));
-
-		fid_zero(&op_data->op_fid2);
-		op_data->op_suppgids[1] = body->mbo_gid;
-		ptlrpc_req_put(req);
-		req = NULL;
-		ll_intent_release(it);
-		rc = md_intent_lock(ll_i2mdexp(parent), op_data, it, &req,
-				    &ll_md_blocking_ast, 0);
-	}
-
+	rc = ll_intent_lock(ll_i2mdexp(parent), op_data, it, &req,
+			    &ll_md_blocking_ast, 0, true);
 	if (rc < 0)
 		GOTO(out, retval = ERR_PTR(rc));
 

lustre/llite/xattr_cache.c
@@ -401,7 +401,8 @@ static int ll_xattr_find_get_lock(struct inode *inode,
 
 	op_data->op_valid = OBD_MD_FLXATTR | OBD_MD_FLXATTRLS;
 
-	rc = md_intent_lock(exp, op_data, oit, req, &ll_md_blocking_ast, 0);
+	rc = ll_intent_lock(exp, op_data, oit, req,
+			    &ll_md_blocking_ast, 0, true);
 	ll_finish_md_op_data(op_data);
 	*req = oit->it_request;
 

lustre/mdt/mdt_handler.c
@@ -6306,6 +6306,8 @@ static void mdt_fini(const struct lu_env *env, struct mdt_device *m)
 	mdt_tunables_fini(m);
 	upcall_cache_cleanup(m->mdt_identity_cache);
 	m->mdt_identity_cache = NULL;
+	upcall_cache_cleanup(m->mdt_identity_cache_int);
+	m->mdt_identity_cache_int = NULL;
 
 	tgt_fini(env, &m->mdt_lut);
 
@@ -6358,6 +6360,7 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	struct lu_site *s;
 	struct seq_server_site *ss_site;
 	const char *identity_upcall = "NONE";
+	char cache_internal[NAME_MAX + 1] = { 0 };
 	struct md_device *next;
 	struct lu_fid fid;
 	int rc;
@@ -6568,7 +6571,6 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 	 */
 	if (m->mdt_opts.mo_acl)
 		identity_upcall = MDT_IDENTITY_UPCALL_PATH;
-
 	m->mdt_identity_cache = upcall_cache_init(mdt_obd_name(m),
 						identity_upcall,
 						UC_IDCACHE_HASH_SIZE,
@@ -6580,6 +6582,21 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 		rc = PTR_ERR(m->mdt_identity_cache);
 		m->mdt_identity_cache = NULL;
 		GOTO(err_free_hsm, rc);
+	}
+
+	snprintf(cache_internal, sizeof(cache_internal), "%s_int",
+		 mdt_obd_name(m));
+	m->mdt_identity_cache_int = upcall_cache_init(cache_internal,
+						IDENTITY_UPCALL_INTERNAL,
+						UC_IDCACHE_HASH_SIZE,
+						1200, /* entry expire: 20 mn */
+						30, /* acquire expire: 30 s */
+						true, /* acquire can replay */
+						&mdt_identity_upcall_cache_ops);
+	if (IS_ERR(m->mdt_identity_cache_int)) {
+		rc = PTR_ERR(m->mdt_identity_cache_int);
+		m->mdt_identity_cache_int = NULL;
+		GOTO(err_cache, rc);
 	}
 
 	rc = mdt_tunables_init(m, dev);
@@ -6623,6 +6640,9 @@ err_ping_evictor:
 err_procfs:
 	mdt_tunables_fini(m);
 err_recovery:
+	upcall_cache_cleanup(m->mdt_identity_cache_int);
+	m->mdt_identity_cache_int = NULL;
+err_cache:
 	upcall_cache_cleanup(m->mdt_identity_cache);
 	m->mdt_identity_cache = NULL;
 err_free_hsm:

commit 9bef1f3a5cc4d76ab8db06c8486fc190e1b4e471

LU-20530 obdclass: release ref upon error exit
upcall_cache_entry reference must be released
before leaving via error path.

Test-Parameters: kerberos=true testlist=sanity-krb5 env=SANITY_GSS_EXCEPT="10 13 152 200 201"
Test-Parameters: testgroup=review-dne-selinux-ssk-part-2
Fixes: e826e8bb77bf ("LU-17961 sec: support supplementary groups from client")
Signed-off-by: Bruno Faccini <bfaccini@nvidia.com>
Change-Id: I7569887721bdb90a1f33aa14688bb306ea40c5b1
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67617
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/upcall_cache.c
@@ -276,8 +276,12 @@ find_with_lock:
 		rc = upcall_cache_get_entry_internal(cache, entry, args,
 						     &fsgid, &ginfo);
 		write_lock(&cache->uc_lock);
-		if (rc)
+		if (rc) {
+			if (entry == new)
+				UC_CACHE_SET_INVALID(entry);
+			put_entry(cache, entry);
 			GOTO(out, entry = ERR_PTR(rc));
+		}
 	}
 
 	/* acquire for new one */