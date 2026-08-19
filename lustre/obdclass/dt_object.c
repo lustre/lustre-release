lustre/lustre-release

commit 37cd57fac9c10555a5a635bc43489f2accc6ba78

LU-17916 mdt: switch to struct folio order 0
Prefer folio of order 0 instead of struct page. Switch
to struct folio and the folio API.

HPE-bug-id: LUS-12384
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I72ec63d7298dc1dff2a36e1fe07094144acda9a0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/58275
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/obdclass/dt_object.c
@@ -810,9 +810,9 @@ void *rdpg_page_get(const struct lu_rdpg *rdpg, unsigned int index)
 {
 	if (rdpg->rp_npages) {
 		LASSERT(index < rdpg->rp_npages);
-		return kmap(rdpg->rp_pages[index]);
+		return ll_kmap_local_folio(rdpg->rp_folios[index], 0);
 	}
-	LASSERT(index << PAGE_SHIFT < rdpg->rp_count);
+	LASSERT((index << PAGE_SHIFT) < rdpg->rp_count);
 
 	return rdpg->rp_data + (index << PAGE_SHIFT);
 }
@@ -821,7 +821,7 @@ EXPORT_SYMBOL(rdpg_page_get);
 void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index, void *kaddr)
 {
 	if (rdpg->rp_npages)
-		kunmap(kmap_to_page(kaddr));
+		ll_kunmap_local(kaddr);
 }
 EXPORT_SYMBOL(rdpg_page_put);
 
@@ -849,7 +849,7 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 	int rc;
 	ENTRY;
 
-	LASSERT(rdpg->rp_pages != NULL);
+	LASSERT(rdpg->rp_folios != NULL);
 	LASSERT(obj->do_index_ops != NULL);
 
 	if (filler == NULL)
@@ -1019,7 +1019,8 @@ int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 	 * init the header of the remain lu_idxpages.
 	 */
 	if (rc > 0)
-		dt_index_page_adjust(rdpg->rp_pages, rdpg->rp_npages,
+		dt_index_page_adjust(rdpg->rp_folios,
+				     rdpg->rp_npages,
 				     ii->ii_count);
 
 	GOTO(out, rc);
@@ -1036,7 +1037,7 @@ EXPORT_SYMBOL(dt_index_read);
  * Current lu_idxpage read clients are osp_it_next_page(),
  * nodemap_process_idx_pages() and qsd_reint_entries().
  */
-void dt_index_page_adjust(struct page **pages, const u32 npages,
+void dt_index_page_adjust(struct folio **folios, const u32 npages,
 			  const size_t nlupgs)
 {
 	u32 nlupgs_mod = nlupgs % LU_PAGE_COUNT;
@@ -1066,7 +1067,7 @@ void dt_index_page_adjust(struct page **pages, const u32 npages,
 	}
 }
 #else
-void dt_index_page_adjust(struct page **pages, const u32 npages,
+void dt_index_page_adjust(struct folio **folios, const u32 npages,
 			  const size_t nlupgs)
 {
 }

config/lustre-core.m4
@@ -3609,6 +3609,29 @@ AC_DEFUN([LC_HAVE_ILOOKUP5_NOWAIT_ISNEW],[
 ]) # LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
 
 #
+## LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
+#
+# Linux commit v6.18-rc2-1-g7f3779a3ac3e4
+#   mm/filemap: Add NUMA mempolicy support to filemap_alloc_folio()
+#
+AC_DEFUN([LC_SRC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA],[
+	LB2_LINUX_TEST_SRC([filemap_alloc_folio_numa], [
+		#include <linux/pagemap.h>
+	],[
+		struct folio *folio __maybe_unused;
+
+		folio = filemap_alloc_folio_noprof(GFP_KERNEL, 0, NULL);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA],[
+	LB2_MSG_LINUX_TEST_RESULT([if filemap_alloc_folio() takes NUMA mempolicy],
+	[filemap_alloc_folio_numa], [
+		AC_DEFINE(HAVE_FILEMAP_ALLOC_FOLIO_NUMA, 1,
+			  [filemap_alloc_folio() takes NUMA mempolicy])
+	])
+]) # LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
+
+#
 # LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
 #
 # Linux commit v6.19-rc1-31-g6cbfdf89470ef
@@ -3843,6 +3866,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_INODE_STATE_READ
 	LC_SRC_HAVE_VFS_CREATE_DELEGATE
 	LC_SRC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+	LC_SRC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
 
 	# 7.0
 	LC_SRC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER
@@ -4063,6 +4087,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_INODE_STATE_READ
 	LC_HAVE_VFS_CREATE_DELEGATE
 	LC_HAVE_ILOOKUP5_NOWAIT_ISNEW
+	LC_HAVE_FILEMAP_ALLOC_FOLIO_NUMA
 
 	# 7.0
 	LC_HAVE_POSIX_ACL_TO_XATTR_ALLOC_BUFFER

include/lustre_compat/linux/folio.h
@@ -98,6 +98,23 @@ ll_read_cache_folio(struct address_space *mapping, pgoff_t index,
 	bio_add_page((bio), fpgptr((pg)), (sz), (off))
 #endif
 
+#ifndef HAVE_FILEMAP_ALLOC_FOLIO_NUMA
+#ifdef filemap_alloc_folio
+#undef filemap_alloc_folio
+
+#ifdef alloc_hooks
+#define filemap_alloc_folio(gfp, ord, numa) \
+	alloc_hooks(filemap_alloc_folio_noprof((gfp), (ord)))
+#else
+#define filemap_alloc_folio(gfp, ord, numa) \
+	filemap_alloc_folio((gfp), (ord))
+#endif
+#else
+#define filemap_alloc_folio(gfp, ord, numa) \
+	filemap_alloc_folio((gfp), (ord))
+#endif
+#endif /* HAVE_FILEMAP_ALLOC_FOLIO_NUMA */
+
 #else /* !HAVE___FILEMAP_GET_FOLIO */
 #define get_folio_lock(m, i, f, g)	find_lock_page((m), (i))
 #define get_folio_nowait(m, i, f, g)	grab_cache_page_nowait((m), (i))
@@ -180,7 +197,8 @@ do {						\
 #define folio_unlock(p)			unlock_page((p))
 #define folio_pos(p)			page_offset((p))
 #define flush_dcache_folio(p)		flush_dcache_page((p))
-#define filemap_alloc_folio(gfp, ord)	__page_cache_alloc((gfp))
+#define filemap_alloc_folio(gfp, ord, numa) \
+	__page_cache_alloc((gfp))
 #define folio_ref_count(p)		page_count((p))
 #define virt_to_folio(addr)		virt_to_page((addr))
 #define copy_folio_to_iter(f, o, b, i)	\
@@ -413,5 +431,9 @@ static inline int folio_mapcount_page(struct page *page)
 #define folio_mapcount(folio)			page_mapcount(fpgptr(folio))
 #define folio_mapcount_page(pg)			page_mapcount((pg))
 #endif /* HAVE_FOLIO_MAPCOUNT */
+
+#ifndef kvcalloc
+#define kvcalloc(count, size, gfp)		kvzalloc((count) * (size), gfp)
+#endif
 
 #endif /* _LUSTRE_COMPAT_LINUX_FOLIO_H */

lustre/include/dt_object.h
@@ -2278,7 +2278,7 @@ int dt_index_walk(const struct lu_env *env, struct dt_object *obj,
 		  void *arg);
 int dt_index_read(const struct lu_env *env, struct dt_device *dev,
 		  struct idx_info *ii, const struct lu_rdpg *rdpg);
-void dt_index_page_adjust(struct page **pages, const u32 npages,
+void dt_index_page_adjust(struct folio **folios, const u32 npages,
 			  const size_t nlupgs);
 
 static inline struct thandle *dt_trans_create(const struct lu_env *env,

lustre/include/lu_object.h
@@ -22,6 +22,7 @@
 #include <linux/percpu_counter.h>
 #include <linux/rhashtable.h>
 #include <linux/ctype.h>
+#include <lustre_compat.h>
 
 #include <obd_support.h>
 #include <uapi/linux/lustre/lustre_idl.h>
@@ -886,7 +887,7 @@ struct lu_rdpg {
 	__u32                   rp_attrs;
 	/** pointers to pages */
 	union {
-		struct page	**rp_pages;
+		struct folio   **rp_folios;
 		void		*rp_data;
 	};
 };

lustre/include/obd.h
@@ -1338,7 +1338,7 @@ struct md_ops {
 
 	int (*m_read_page)(struct obd_export *exp, struct md_op_data *op_data,
 			   struct md_readdir_info *mrinfo, __u64 hash_offset,
-			   struct page **ppage);
+			   struct folio **pfolio);
 
 	int (*m_unlink)(struct obd_export *exp, struct md_op_data *op_data,
 			struct ptlrpc_request **req);
@@ -1427,7 +1427,7 @@ struct md_ops {
 	int (*m_batch_add)(struct obd_export *exp, struct lu_batch *bh,
 			   struct md_op_item *item);
 	int (*m_dirpage_add)(struct obd_export *exp, struct inode *inode,
-			     struct page **pool,
+			     struct folio **pool,
 			     unsigned int cfs_pgs, unsigned int lu_pgs,
 			     int is_hash64);
 };

lustre/include/obd_class.h
@@ -1633,7 +1633,7 @@ static inline int md_file_resync(struct obd_export *exp,
 static inline int md_read_page(struct obd_export *exp,
 			       struct md_op_data *op_data,
 			       struct md_readdir_info *mrinfo,
-			       __u64  hash_offset, struct page **ppage)
+			       __u64  hash_offset, struct folio **pfolio)
 {
 	int rc;
 
@@ -1647,7 +1647,7 @@ static inline int md_read_page(struct obd_export *exp,
 	return exp->exp_obd->obd_type->typ_md_ops->m_read_page(exp, op_data,
 							       mrinfo,
 							       hash_offset,
-							       ppage);
+							       pfolio);
 }
 
 static inline int md_unlink(struct obd_export *exp, struct md_op_data *op_data,
@@ -1986,7 +1986,7 @@ static inline int md_batch_add(struct obd_export *exp, struct lu_batch *bh,
 
 static inline int md_dirpage_add(struct obd_export *exp,
 				 struct inode *inode,
-				 struct page **pool,
+				 struct folio **pool,
 				 unsigned int cfs_pgs,
 				 unsigned int lu_pgs, int is_hash64)
 {

lustre/llite/dir.c
@@ -40,7 +40,7 @@
 #include "llite_internal.h"
 
 /**
- * ll_get_dir_page() - Get directory page for a given directory inode
+ * ll_get_dir_folio() - Get directory page for a given directory inode
  * @dir: pointer to the directory(inode) for which page is being fetched
  * @op_data: pointer to the md operation structure
  * @offset: Offset within page
@@ -130,52 +130,53 @@
  * * %Success - pointer to the page structure
  * * %Failure - Error pointer (pointed by rc)
  */
-struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
-			     __u64 offset, bool hash64, int *partial_readdir_rc)
+struct folio *ll_get_dir_folio(struct inode *dir, struct md_op_data *op_data,
+			       __u64 offset, bool hash64,
+			       int *partial_readdir_rc)
 {
 	struct md_readdir_info mrinfo = {
 					.mr_blocking_ast = ll_md_blocking_ast };
-	struct page *page;
+	struct folio *folio;
 	unsigned long idx = hash_x_index(offset, hash64);
 	int rc;
 
 	/* check page first */
-	page = find_get_page(dir->i_mapping, idx);
-	if (page) {
-		wait_on_page_locked(page);
-		if (PageUptodate(page))
-			RETURN(page);
-		put_page(page);
+	folio = get_folio_read(dir->i_mapping, idx, 0, 0);
+	if (!IS_ERR_OR_NULL(folio)) {
+		folio_wait_locked(folio);
+		if (folio_test_uptodate(folio))
+			RETURN(folio);
+		folio_put(folio);
 	}
 
-	rc = md_read_page(ll_i2mdexp(dir), op_data, &mrinfo, offset, &page);
+	rc = md_read_page(ll_i2mdexp(dir), op_data, &mrinfo, offset, &folio);
 	if (rc != 0)
 		return ERR_PTR(rc);
 
 	if (partial_readdir_rc && mrinfo.mr_partial_readdir_rc)
 		*partial_readdir_rc = mrinfo.mr_partial_readdir_rc;
 
-	return page;
+	return folio;
 }
 
-void ll_release_page(struct inode *inode, struct page *page,
-		     bool remove)
+void ll_release_dir_folio(struct inode *inode, struct folio *folio,
+			  bool remove)
 {
 	/* Always remove the page for striped dir, because the page is
 	 * built from temporarily in LMV layer
 	 */
 	if (inode && ll_dir_striped(inode)) {
-		__free_page(page);
+		folio_put(folio);
 		return;
 	}
 
 	if (remove) {
-		lock_page(page);
-		if (likely(page->mapping != NULL))
-			cfs_delete_from_page_cache(page);
-		unlock_page(page);
+		folio_lock(folio);
+		if (likely(folio->mapping != NULL))
+			cfs_folio_delete_from_cache(folio);
+		folio_unlock(folio);
 	}
-	put_page(page);
+	folio_put(folio);
 }
 
 int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
@@ -185,7 +186,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 	__u64 pos = *ppos;
 	bool is_api32 = ll_need_32bit_api(sbi);
 	bool is_hash64 = test_bit(LL_SBI_64BIT_HASH, sbi->ll_flags);
-	struct page *page;
+	struct folio *folio;
 	bool done = false;
 	struct llcrypt_str lltr = LLTR_INIT(NULL, 0);
 	int rc = 0;
@@ -198,8 +199,8 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 			RETURN(rc);
 	}
 
-	page = ll_get_dir_page(inode, op_data, pos, is_hash64,
-				partial_readdir_rc);
+	folio = ll_get_dir_folio(inode, op_data, pos, is_hash64,
+				 partial_readdir_rc);
 
 	while (rc == 0 && !done) {
 		void *kaddr = NULL;
@@ -208,13 +209,13 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 		__u64 hash;
 		__u64 next;
 
-		if (IS_ERR(page)) {
-			rc = PTR_ERR(page);
+		if (IS_ERR(folio)) {
+			rc = PTR_ERR(folio);
 			break;
 		}
 
 		hash = MDS_DIR_END_OFF;
-		kaddr = kmap(page);
+		kaddr = kmap(fpgptr(folio));
 		dp = kaddr;
 		for (ent = lu_dirent_start(dp); ent != NULL && !done;
 		     ent = lu_dirent_next(ent)) {
@@ -272,7 +273,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 				kunmap(kmap_to_page(kaddr));
 				kaddr = NULL;
 			}
-			ll_release_page(inode, page, false);
+			ll_release_dir_folio(inode, folio, false);
 			break;
 		}
 
@@ -285,7 +286,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 				kunmap(kmap_to_page(kaddr));
 				kaddr = NULL;
 			}
-			ll_release_page(inode, page, false);
+			ll_release_dir_folio(inode, folio, false);
 		} else {
 			u32 flags = le32_to_cpu(dp->ldp_flags);
 
@@ -294,10 +295,10 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 				kunmap(kmap_to_page(kaddr));
 				kaddr = NULL;
 			}
-			ll_release_page(inode, page, flags & LDF_COLLIDE);
+			ll_release_dir_folio(inode, folio, flags & LDF_COLLIDE);
 			next = pos;
-			page = ll_get_dir_page(inode, op_data, pos,
-					       is_hash64, partial_readdir_rc);
+			folio = ll_get_dir_folio(inode, op_data, pos,
+						 is_hash64, partial_readdir_rc);
 		}
 	}
 	ctx->pos = pos;

commit 5daed0c9689e55e8727e67b44677e6d874a0384d

LU-17916 obdclass: fix dt_index_page_adjust() w/ 64K PAGE_SIZE
It has been reported that builds with 64K PAGE_SIZE fail, as "pages"
in dt_index_page_adjust() is undeclared; fix this by using
kmap_local_folio(), instead of kmap_local_page().

HPE-bug-id: LUS-12384
Fixes: 37cd57fac9 ("LU-17916 mdt: switch to struct folio order 0")
Reported-by: Chris Horn <chorn@ddn.com>
Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Test-Parameters: trivial testlist=sanity clientarch=aarch64 clientdistro=rocky9.5
Change-Id: I1edc56f1ae45a70c0228324ce0576d48c1c1d306
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67900
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Shaun Tancheff <shaun@tancheff.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/obdclass/dt_object.c
@@ -1275,7 +1275,7 @@ void dt_index_page_adjust(struct folio **folios, const u32 npages,
 
 		pgidx = nlupgs / LU_PAGE_COUNT;
 		LASSERT(pgidx < npages);
-		kaddr = kmap_local_page(pages[pgidx]);
+		kaddr = kmap_local_folio(folios[pgidx], 0);
 		lp = kaddr;
 		remain_nlupgs = LU_PAGE_COUNT - nlupgs_mod;
 