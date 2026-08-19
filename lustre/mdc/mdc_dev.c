lustre/lustre-release

commit 8aa231a994683a9224d42c0e7ae48aaebe2f583c

LU-16713 llite: writeback/commit pages under memory pressure
Lustre buffered I/O does not work well with restrictive memcg
control. This may result in OOM when the system is under memroy
pressure.

Lustre has implemented unstable pages support similar to NFS.
But it is disabled by default due to the performance reason.

In Lustre, a client pins the cache pages for writes until the
write transcation is committed on the server (OST) even these
pinned pages have been finished writeback. The server starts
a transaction commit either because the commit interval (5
second, by default) for the backend storage (i.e. OST/ldiskfs)
has been reached or there is not enough room in the journal
for a particular handle to start. Before the write transcation
has been committed and notify the client, these pages are
pinned and not flushable in any way by the kernel.
This means that when a client hits memory pressure there can
be a large number of unfreeable (pinned and uncommitted) pages,
so the application on the client will end up OOM killed because
when asked to free up memory it can not.
This is particularly common with cgroups. Because when cgroups
are in use, the memory limit is generally much lower than the
total system memory limits and it is more likely to reach the
limits.

Linux kernel has matured memory reclaim mechanism to avoid OOM
even with cgroups.
After perform dirtied write for a page, the kernel calls
@balance_dirty_pages(). If the dirtied and uncommitted pages
are over background threshold for the global memory limits or
memory cgroup limits, the writeback threads are woken to perform
some writeout.
When allocate a new page for I/O under memory pressure, the
kernel will try direct reclaim and then allocating. For cgroup,
it will try to reclaim pages from the memory cgroup over soft
limit. The slow page allocation path with direct reclaim will
call @wakeup_flusher_threads() with WB_REASON_VMSCAN to start
writeback dirty pages.

lustre/mdc/mdc_dev.c
@@ -1193,6 +1193,16 @@ static int mdc_io_fsync_start(const struct lu_env *env,
 
 	ENTRY;
 
+	if (fio->fi_mode == CL_FSYNC_RECLAIM) {
+		struct client_obd *cli = osc_cli(osc);
+
+		if (!atomic_long_read(&cli->cl_unstable_count)) {
+			/* Stop flush when there are no unstable pages? */
+			CDEBUG(D_CACHE, "unstable count is zero\n");
+			RETURN(0);
+		}
+	}
+
 	/* a MDC lock always covers whole object, do sync for whole
 	 * possible range despite of supplied start/end values.
 	 */
@@ -1202,19 +1212,25 @@ static int mdc_io_fsync_start(const struct lu_env *env,
 		fio->fi_nr_written += result;
 		result = 0;
 	}
-	if (fio->fi_mode == CL_FSYNC_ALL) {
+	if (fio->fi_mode == CL_FSYNC_ALL || fio->fi_mode == CL_FSYNC_RECLAIM) {
+		struct osc_io *oio = cl2osc_io(env, slice);
+		struct osc_async_cbargs *cbargs = &oio->oi_cbarg;
 		int rc;
 
-		rc = osc_cache_wait_range(env, osc, 0, CL_PAGE_EOF);
-		if (result == 0)
-			result = rc;
+		if (fio->fi_mode == CL_FSYNC_ALL) {
+			rc = osc_cache_wait_range(env, osc, 0, CL_PAGE_EOF);
+			if (result == 0)
+				result = rc;
+		}
 		/* Use OSC sync code because it is asynchronous.
 		 * It is to be added into MDC and avoid the using of
 		 * OST_SYNC at both MDC and MDT.
 		 */
 		rc = osc_fsync_ost(env, osc, fio);
-		if (result == 0)
+		if (result == 0) {
+			cbargs->opc_rpc_sent = 1;
 			result = rc;
+		}
 	}
 
 	RETURN(result);

lustre/osc/osc_io.c
@@ -957,14 +957,25 @@ EXPORT_SYMBOL(osc_fsync_ost);
 static int osc_io_fsync_start(const struct lu_env *env,
 			      const struct cl_io_slice *slice)
 {
-	struct cl_io       *io  = slice->cis_io;
+	struct cl_io *io = slice->cis_io;
 	struct cl_fsync_io *fio = &io->u.ci_fsync;
-	struct cl_object   *obj = slice->cis_obj;
-	struct osc_object  *osc = cl2osc(obj);
-	pgoff_t start  = fio->fi_start >> PAGE_SHIFT;
-	pgoff_t end    = fio->fi_end >> PAGE_SHIFT;
-	int     result = 0;
+	struct cl_object *obj = slice->cis_obj;
+	struct osc_object *osc = cl2osc(obj);
+	pgoff_t start = fio->fi_start >> PAGE_SHIFT;
+	pgoff_t end = fio->fi_end >> PAGE_SHIFT;
+	int result = 0;
+
 	ENTRY;
+
+	if (fio->fi_mode == CL_FSYNC_RECLAIM) {
+		struct client_obd *cli = osc_cli(osc);
+
+		if (!atomic_long_read(&cli->cl_unstable_count)) {
+			/* Stop flush when there are no unstable pages? */
+			CDEBUG(D_CACHE, "unstable count is zero\n");
+			RETURN(0);
+		}
+	}
 
 	if (fio->fi_end == OBD_OBJECT_EOF)
 		end = CL_PAGE_EOF;
@@ -982,20 +993,30 @@ static int osc_io_fsync_start(const struct lu_env *env,
 		fio->fi_nr_written += result;
 		result = 0;
 	}
-	if (fio->fi_mode == CL_FSYNC_ALL) {
+	if (fio->fi_mode == CL_FSYNC_ALL || fio->fi_mode == CL_FSYNC_RECLAIM) {
+		struct osc_io *oio = cl2osc_io(env, slice);
+		struct osc_async_cbargs *cbargs = &oio->oi_cbarg;
 		int rc;
 
 		/* we have to wait for writeback to finish before we can
 		 * send OST_SYNC RPC. This is bad because it causes extents
 		 * to be written osc by osc. However, we usually start
 		 * writeback before CL_FSYNC_ALL so this won't have any real
-		 * problem. */
-		rc = osc_cache_wait_range(env, osc, start, end);
-		if (result == 0)
-			result = rc;
+		 * problem.
+		 * We do not have to wait for waitback to finish in the memory
+		 * reclaim environment.
+		 */
+		if (fio->fi_mode == CL_FSYNC_ALL) {
+			rc = osc_cache_wait_range(env, osc, start, end);
+			if (result == 0)
+				result = rc;
+		}
+
 		rc = osc_fsync_ost(env, osc, fio);
-		if (result == 0)
+		if (result == 0) {
+			cbargs->opc_rpc_sent = 1;
 			result = rc;
+		}
 	}
 
 	RETURN(result);
@@ -1005,16 +1026,17 @@ void osc_io_fsync_end(const struct lu_env *env,
 		      const struct cl_io_slice *slice)
 {
 	struct cl_fsync_io *fio = &slice->cis_io->u.ci_fsync;
-	struct cl_object   *obj = slice->cis_obj;
+	struct cl_object *obj = slice->cis_obj;
+	struct osc_io *oio = cl2osc_io(env, slice);
+	struct osc_async_cbargs *cbargs = &oio->oi_cbarg;
 	pgoff_t start = fio->fi_start >> PAGE_SHIFT;
 	pgoff_t end   = fio->fi_end >> PAGE_SHIFT;
 	int result = 0;
 
 	if (fio->fi_mode == CL_FSYNC_LOCAL) {
 		result = osc_cache_wait_range(env, cl2osc(obj), start, end);
-	} else if (fio->fi_mode == CL_FSYNC_ALL) {
-		struct osc_io           *oio    = cl2osc_io(env, slice);
-		struct osc_async_cbargs *cbargs = &oio->oi_cbarg;
+	} else if (cbargs->opc_rpc_sent && (fio->fi_mode == CL_FSYNC_ALL ||
+					    fio->fi_mode == CL_FSYNC_RECLAIM)) {
 
 		wait_for_completion(&cbargs->opc_sync);
 		if (result == 0)

lustre/autoconf/lustre-core.m4
@@ -3301,6 +3301,28 @@ LB_CHECK_EXPORT([delete_from_page_cache], [mm/filemap.c],
 			[delete_from_page_cache is exported])])
 ]) # LC_EXPORTS_DELETE_FROM_PAGE_CACHE
 
+
+#
+# LC_HAVE_WB_STAT_MOD
+#
+# Kernel 5.16-rc1 bd3488e7b4d61780eb3dfaca1cc6f4026bcffd48
+# mm/writeback: Rename __add_wb_stat() to wb_stat_mod()
+#
+AC_DEFUN([LC_HAVE_WB_STAT_MOD], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if wb_stat_mod() exists],
+wb_stat_mode, [
+	#include <linux/backing-dev.h>
+],[
+	wb_stat_mod(NULL, WB_WRITEBACK, 1);
+],[
+	AC_DEFINE(HAVE_WB_STAT_MOD, 1,
+		[wb_stat_mod() exists])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LC_HAVE_WB_STAT_MOD
+
 #
 # LC_HAVE_INVALIDATE_FOLIO
 #
@@ -4385,6 +4407,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_SECURITY_DENTRY_INIT_WITH_XATTR_NAME_ARG
 	LC_HAVE_KIOCB_COMPLETE_2ARGS
 	LC_EXPORTS_DELETE_FROM_PAGE_CACHE
+	LC_HAVE_WB_STAT_MOD
 
 	# 5.17
 	LC_HAVE_INVALIDATE_FOLIO

lustre/include/cl_object.h
@@ -380,6 +380,14 @@ struct cl_object_operations {
          */
 	int (*coo_attr_update)(const struct lu_env *env, struct cl_object *obj,
 			       const struct cl_attr *attr, unsigned valid);
+	/**
+	 * Mark the inode dirty. By this way, the inode will add into the
+	 * writeback list of the corresponding @bdi_writeback, and then it will
+	 * defer to write out the dirty pages to OSTs via the kernel writeback
+	 * mechanism.
+	 */
+	void (*coo_dirty_for_sync)(const struct lu_env *env,
+				   struct cl_object *obj);
         /**
          * Update object configuration. Called top-to-bottom to modify object
          * configuration.
@@ -1707,14 +1715,16 @@ enum cl_io_lock_dmd {
 
 enum cl_fsync_mode {
 	/** start writeback, do not wait for them to finish */
-	CL_FSYNC_NONE  = 0,
+	CL_FSYNC_NONE		= 0,
 	/** start writeback and wait for them to finish */
-	CL_FSYNC_LOCAL = 1,
+	CL_FSYNC_LOCAL		= 1,
 	/** discard all of dirty pages in a specific file range */
-	CL_FSYNC_DISCARD = 2,
+	CL_FSYNC_DISCARD	= 2,
 	/** start writeback and make sure they have reached storage before
 	 * return. OST_SYNC RPC must be issued and finished */
-	CL_FSYNC_ALL   = 3
+	CL_FSYNC_ALL		= 3,
+	/** start writeback, thus the kernel can reclaim some memory */
+	CL_FSYNC_RECLAIM	= 4,
 };
 
 struct cl_io_rw_common {
@@ -2138,6 +2148,7 @@ int  cl_object_attr_get(const struct lu_env *env, struct cl_object *obj,
 			struct cl_attr *attr);
 int  cl_object_attr_update(const struct lu_env *env, struct cl_object *obj,
                            const struct cl_attr *attr, unsigned valid);
+void cl_object_dirty_for_sync(const struct lu_env *env, struct cl_object *obj);
 int  cl_object_glimpse    (const struct lu_env *env, struct cl_object *obj,
                            struct ost_lvb *lvb);
 int  cl_conf_set          (const struct lu_env *env, struct cl_object *obj,

lustre/include/lustre_compat.h
@@ -663,6 +663,10 @@ static inline bool is_root_inode(struct inode *inode)
 #define ll_access_ok(ptr, len) access_ok(ptr, len)
 #endif
 
+#ifdef HAVE_WB_STAT_MOD
+#define __add_wb_stat(wb, item, amount)		wb_stat_mod(wb, item, amount)
+#endif
+
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG
 #ifndef HAVE_LSMCONTEXT_INIT
 /* Ubuntu 5.19 */

lustre/llite/file.c
@@ -4809,7 +4809,8 @@ int cl_sync_file_range(struct inode *inode, loff_t start, loff_t end,
 	ENTRY;
 
 	if (mode != CL_FSYNC_NONE && mode != CL_FSYNC_LOCAL &&
-	    mode != CL_FSYNC_DISCARD && mode != CL_FSYNC_ALL)
+	    mode != CL_FSYNC_DISCARD && mode != CL_FSYNC_ALL &&
+	    mode != CL_FSYNC_RECLAIM)
 		RETURN(-EINVAL);
 
 	env = cl_env_get(&refcheck);

lustre/llite/llite_lib.c
@@ -1486,8 +1486,8 @@ void ll_put_super(struct super_block *sb)
 	struct ll_sb_info *sbi = ll_s2sbi(sb);
 	char *profilenm = get_profile_name(sb);
 	unsigned long cfg_instance = ll_get_cfg_instance(sb);
-	long ccc_count;
-	int next, force = 1, rc = 0;
+	int next, force = 1;
+
 	ENTRY;
 
 	if (IS_ERR(sbi))
@@ -1508,17 +1508,6 @@ void ll_put_super(struct super_block *sb)
 		if (obd)
 			force = obd->obd_force;
 	}
-
-	/* Wait for unstable pages to be committed to stable storage */
-	if (force == 0) {
-		rc = l_wait_event_abortable(
-			sbi->ll_cache->ccc_unstable_waitq,
-			atomic_long_read(&sbi->ll_cache->ccc_unstable_nr) == 0);
-	}
-
-	ccc_count = atomic_long_read(&sbi->ll_cache->ccc_unstable_nr);
-	if (force == 0 && rc != -ERESTARTSYS)
-		LASSERTF(ccc_count == 0, "count: %li\n", ccc_count);
 
 	/* We need to set force before the lov_disconnect in
 	 * lustre_common_put_super, since l_d cleans up osc's as well.

lustre/llite/rw.c
@@ -1555,6 +1555,7 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 	enum cl_fsync_mode mode;
 	int range_whole = 0;
 	int result;
+
 	ENTRY;
 
 	if (wbc->range_cyclic) {
@@ -1572,6 +1573,37 @@ int ll_writepages(struct address_space *mapping, struct writeback_control *wbc)
 	mode = CL_FSYNC_NONE;
 	if (wbc->sync_mode == WB_SYNC_ALL)
 		mode = CL_FSYNC_LOCAL;
+
+	if (wbc->sync_mode == WB_SYNC_NONE) {
+#ifdef SB_I_CGROUPWB
+		struct bdi_writeback *wb;
+
+		/*
+		 * As it may break full stripe writes on the inode,
+		 * disable periodic kupdate writeback (@wbc->for_kupdate)?
+		 */
+
+		/*
+		 * The system is under memory pressure and it is now reclaiming
+		 * cache pages.
+		 */
+		wb = inode_to_wb(inode);
+		if (wbc->for_background ||
+		    (wb->start_all_reason == WB_REASON_VMSCAN &&
+		     test_bit(WB_start_all, &wb->state)))
+			mode = CL_FSYNC_RECLAIM;
+#else
+		/*
+		 * We have no idea about writeback reason for memory reclaim
+		 * WB_REASON_TRY_TO_FREE_PAGES in the old kernel such as rhel7
+		 * (WB_REASON_VMSCAN in the newer kernel) ...
+		 * Here set mode with CL_FSYNC_RECLAIM forcely on the old
+		 * kernel.
+		 */
+		if (!wbc->for_kupdate)
+			mode = CL_FSYNC_RECLAIM;
+#endif
+	}
 
 	if (ll_i2info(inode)->lli_clob == NULL)
 		RETURN(0);

commit cb3a238c39c171a7a6835808538541ccce375dee

LU-20510 osc: wait for OST_SYNC issued on the fsync error path
osc_io_fsync_start() issues asynchronous OST_SYNC RPCs unconditionally
via osc_fsync_ost(), but only records that it is in flight when no
earlier error has been latched:

	rc = osc_fsync_ost(env, osc, fio);
	if (result == 0) {
		cbargs->opc_rpc_sent = 1;
		result = rc;
	}

osc_io_fsync_end() waits only when opc_rpc_sent is set, so once
osc_cache_writeback_range() or osc_cache_wait_range() has failed, the
io is torn down with the RPC still in flight. Its completion then runs
osc_async_upcall() on &oio->oi_cbarg, which is in struct osc_session,
memory already returned to osc_session_kmem. The resulting freelist
corruption surfaces later, at an unrelated allocation, typically as:

  general protection fault: 0000 [#1] SMP DEBUG_PAGEALLOC
  RIP: 0010:memset_erms+0x9/0x10
   kmem_cache_alloc+0x307/0x430
   osc_session_init+0x31/0x150 [osc]
   cl_env_new+0x223/0x2e0 [obdclass]
   cl_env_get+0xe3/0x3a0 [obdclass]

The window is normally unreachable, since nothing fails writeback on a
healthy client, but it opens on OST I/O errors or client eviction, and
CL_FSYNC_RECLAIM makes it reachable from background reclaim writeback.

Record RPC as in flight whenever it was actually sent, regardless of
previously latched error, so osc_io_fsync_end() always waits for it.

Verified on slab-debug kernel with writeback-error injection. Without
this patch 6/13 runs crashed, with crash occurring in runs that
entered the window; with it, 0 of 10 runs crashed while 5 still
entered the same window.

Fix identical issue added in the same patch in mdc_io_fsync_start().

Fixes: 8aa231a99468 ("LU-16713 llite: writeback/commit pages under memory pressure")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-4.8 llm_code_and_review_tools
Change-Id: If6f5babc0bb7e354826ac5e50da40e892aa1579a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67446

lustre/mdc/mdc_dev.c
@@ -1235,10 +1235,10 @@ static int mdc_io_fsync_start(const struct lu_env *env,
 		 * OST_SYNC at both MDC and MDT.
 		 */
 		rc = osc_fsync_ost(env, osc, fio);
-		if (result == 0) {
+		if (!rc)
 			cbargs->opc_rpc_sent = 1;
+		if (!result)
 			result = rc;
-		}
 	}
 
 	RETURN(result);

lustre/osc/osc_io.c
@@ -1152,10 +1152,17 @@ static int osc_io_fsync_start(const struct lu_env *env,
 		}
 
 		rc = osc_fsync_ost(env, osc, fio);
-		if (result == 0) {
+		/* The RPC is issued unconditionally above, and its completion
+		 * writes into @cbargs, which lives in the session. Record that
+		 * it is in flight whenever it was actually sent -- even if an
+		 * earlier error is already latched in @result -- otherwise
+		 * osc_io_fsync_end() skips the wait and the upcall lands on a
+		 * freed session.
+		 */
+		if (rc == 0)
 			cbargs->opc_rpc_sent = 1;
+		if (result == 0)
 			result = rc;
-		}
 	}
 
 	RETURN(result);