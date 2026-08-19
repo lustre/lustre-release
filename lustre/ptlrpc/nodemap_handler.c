lustre/lustre-release

commit fb7915fa7fb0e9b6d69f957fcd33f8b0c14fb10a

LU-19079 sec: update nodemap membership on flavor change
When a security flavor is updated on an export (e.g. from null to skn)
the client needs to refresh or create a gss context. Now that we have
GSS based nodemaps, it is useful to lookup the nodemap entries when
this happens, to see if the client now belongs to a different nodemap,
based on its gss context.

Add sanity-sec test_105 to exercise this feature.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I8ac737a5bf91a18a499dcdd5e86e8d3fdd8fee5f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/62990
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -805,6 +805,66 @@ out:
 EXPORT_SYMBOL(nodemap_del_member);
 
 /**
+ * nodemap_member_switch() - move an export to a new nodemap
+ * @exp: obd_export structure for the connection that is being moved
+ * @new_nm_name: new nodemap to switch the export to
+ * @gssonly: true if we require the new nodemap to have gssonly_identification
+ *
+ * Move an export to a new nodemap.
+ * This will decrease the refcount on the old nodemap, and increase the refcount
+ * on the new nodemap.
+ *
+ * Return:
+ * * %-EINVAL		export is NULL, or new_nm_name is invalid
+ * * %-ENOENT		nodemap not found
+ * * %-EPERM		nodemap does not have gssonly_identification property
+ */
+int nodemap_member_switch(struct obd_export *exp, char *new_nm_name,
+			  bool gssonly)
+{
+	struct lu_nodemap *old_nodemap = NULL, *new_nodemap;
+	int rc = 0;
+
+	ENTRY;
+
+	if (!new_nm_name || !exp)
+		RETURN(-EINVAL);
+
+	/* Using ac lock to prevent nodemap reclassification while deleting. */
+	mutex_lock(&active_config_lock);
+
+	new_nodemap = nodemap_lookup(new_nm_name);
+	if (IS_ERR(new_nodemap)) {
+		rc = PTR_ERR(new_nodemap);
+		CDEBUG(D_SEC, "%s: nodemap '%s' does not exist: rc = %d\n",
+		       exp->exp_obd->obd_name, new_nm_name, rc);
+		GOTO(out, rc);
+	}
+
+	if (gssonly && !new_nodemap->nmf_gss_identify)
+		GOTO(out, rc = -EPERM);
+
+	/* do nothing if nodemap does not change */
+	old_nodemap = nodemap_get_from_exp(exp);
+	if (new_nodemap == old_nodemap) {
+		nodemap_putref(new_nodemap);
+		GOTO(out, rc = 0);
+	}
+
+	__nodemap_member_switch(exp, new_nodemap, false, false);
+
+out:
+	mutex_unlock(&active_config_lock);
+	/* in case of success, keep the new_nodemap ref from nodemap_lookup */
+	if (rc && !IS_ERR(new_nodemap))
+		nodemap_putref(new_nodemap);
+	if (!IS_ERR_OR_NULL(old_nodemap))
+		nodemap_putref(old_nodemap);
+	RETURN(rc);
+}
+EXPORT_SYMBOL(nodemap_member_switch);
+
+/**
  * nodemap_add_idmap_helper() - add an idmap to the proper nodemap trees
  * @nodemap: nodemap to add idmap to
  * @id_type: NODEMAP_UID or NODEMAP_GID

lustre/ptlrpc/nodemap_member.c
@@ -331,6 +331,63 @@ out_end:
 }
 
 /**
+ * __nodemap_member_switch() - move an export to a new nodemap
+ * @exp: obd_export structure for the connection that is being moved
+ * @new_nodemap: new nodemap to switch the export to
+ * @banned: true if export is in banlist of new_nodemap
+ * @newly_banned: true if export was not banned before change
+ *
+ * Move an export to a new nodemap.
+ * This has to be done 'by hand' because ted_nodemap should never be NULL on
+ * a live export, so nm_member_del() cannot be called.
+ * This needs to be called with the active_config_lock held.
+ *
+ */
+void __nodemap_member_switch(struct obd_export *exp,
+			     struct lu_nodemap *new_nodemap,
+			     bool banned, bool newly_banned)
+{
+	struct lu_nodemap *old_nodemap;
+	bool need_revoke = false;
+
+	/* could deadlock if new_nodemap also reclassifying,
+	 * active_config_lock serializes reclassifies
+	 */
+	mutex_lock(&new_nodemap->nm_member_list_lock);
+
+	list_del_init(&exp->exp_target_data.ted_nodemap_member);
+
+	spin_lock(&exp->exp_target_data.ted_nodemap_lock);
+	old_nodemap = exp->exp_target_data.ted_nodemap;
+	exp->exp_target_data.ted_nodemap = new_nodemap;
+	spin_unlock(&exp->exp_target_data.ted_nodemap_lock);
+	if (old_nodemap)
+		nodemap_putref(old_nodemap);
+
+	list_add(&exp->exp_target_data.ted_nodemap_member,
+		 &new_nodemap->nm_member_list);
+	mutex_unlock(&new_nodemap->nm_member_list_lock);
+
+	nm_register_obd_stats(new_nodemap, exp);
+
+	if (nodemap_active) {
+		if (!old_nodemap) {
+			need_revoke = true;
+		} else {
+			down_read(&old_nodemap->nm_idmap_lock);
+			if (newly_banned ||
+			    nodemap_change_need_update(old_nodemap,
+						       new_nodemap))
+				need_revoke = true;
+			up_read(&old_nodemap->nm_idmap_lock);
+		}
+	}
+
+	if (need_revoke)
+		nm_member_exp_revoke(exp, banned);
+}
+
+/**
  * nm_member_reclassify_nodemap() - Reclassify members of a nodemap
  * @nodemap: nodemap with members to reclassify
  *
@@ -431,36 +488,8 @@ classify:
 		}
 
 		if (new_nodemap != nodemap) {
-			/* could deadlock if new_nodemap also reclassifying,
-			 * active_config_lock serializes reclassifies
-			 */
-			mutex_lock(&new_nodemap->nm_member_list_lock);
-
-			/* don't use member_del because ted_nodemap
-			 * should never be NULL with a live export
-			 */
-			list_del_init(&exp->exp_target_data.ted_nodemap_member);
-
-			/* keep the new_nodemap ref from classify */
-			spin_lock(&exp->exp_target_data.ted_nodemap_lock);
-			exp->exp_target_data.ted_nodemap = new_nodemap;
-			spin_unlock(&exp->exp_target_data.ted_nodemap_lock);
-			nodemap_putref(nodemap);
-
-			list_add(&exp->exp_target_data.ted_nodemap_member,
-				 &new_nodemap->nm_member_list);
-			mutex_unlock(&new_nodemap->nm_member_list_lock);
-
-			nm_register_obd_stats(new_nodemap, exp);
-
-			if (nodemap_active) {
-				down_read(&nodemap->nm_idmap_lock);
-				if (newly_banned ||
-				    nodemap_change_need_update(nodemap,
-							       new_nodemap))
-					nm_member_exp_revoke(exp, banned);
-				up_read(&nodemap->nm_idmap_lock);
-			}
+			__nodemap_member_switch(exp, new_nodemap,
+						banned, newly_banned);
 		} else {
 			nodemap_putref(new_nodemap);
 		}

lustre/include/lustre_nodemap.h
@@ -224,6 +224,8 @@ bool nodemap_has_dynamic_nodemaps(void);
 int nodemap_add_member(struct ptlrpc_svc_ctx *svc_ctx, struct lnet_nid *nid,
 		       struct obd_export *exp);
 void nodemap_del_member(struct obd_export *exp);
+int nodemap_member_switch(struct obd_export *exp, char *new_nm_name,
+			  bool gssonly);
 int nodemap_parse_range(const char *range_string, struct lnet_nid range[2],
 			u8 *netmask);
 int nodemap_parse_idmap(const char *nodemap_name, char *idmap_str,

lustre/ptlrpc/nodemap_internal.h
@@ -219,6 +219,9 @@ int nm_member_add(struct lu_nodemap *nodemap, struct obd_export *exp);
 void nm_member_del(struct lu_nodemap *nodemap, struct obd_export *exp);
 void nm_member_delete_list(struct lu_nodemap *nodemap);
 struct lu_nodemap *nodemap_classify_nid(struct lnet_nid *nid, bool *banned);
+void __nodemap_member_switch(struct obd_export *exp,
+			     struct lu_nodemap *new_nodemap,
+			     bool banned, bool newly_banned);
 void nm_member_reclassify_nodemap(struct lu_nodemap *nodemap);
 void nm_member_revoke_locks(struct lu_nodemap *nodemap);
 void nm_member_revoke_locks_always(struct lu_nodemap *nodemap);

lustre/ptlrpc/sec.c
@@ -2133,7 +2133,8 @@ static int flavor_allowed(struct sptlrpc_flavor *exp,
 int sptlrpc_target_export_check(struct obd_export *exp,
 				struct ptlrpc_request *req)
 {
-	struct sptlrpc_flavor   flavor;
+	struct sptlrpc_flavor flavor;
+	int rc;
 
 	if (exp == NULL)
 		return 0;
@@ -2194,8 +2195,9 @@ int sptlrpc_target_export_check(struct obd_export *exp,
 		exp->exp_flvr_adapt = 0;
 		spin_unlock(&exp->exp_lock);
 
-		return sptlrpc_import_sec_adapt(exp->exp_imp_reverse,
-						req->rq_svc_ctx, &flavor);
+		rc = sptlrpc_import_sec_adapt(exp->exp_imp_reverse,
+					      req->rq_svc_ctx, &flavor);
+		GOTO(nm_switch, rc);
 	}
 
 	/*
@@ -2234,9 +2236,10 @@ int sptlrpc_target_export_check(struct obd_export *exp,
 			flavor = exp->exp_flvr;
 			spin_unlock(&exp->exp_lock);
 
-			return sptlrpc_import_sec_adapt(exp->exp_imp_reverse,
-							req->rq_svc_ctx,
-							&flavor);
+			rc = sptlrpc_import_sec_adapt(exp->exp_imp_reverse,
+						      req->rq_svc_ctx,
+						      &flavor);
+			GOTO(nm_switch, rc);
 		} else {
 			CDEBUG(D_SEC,
 			       "exp %p (%x|%x|%x): is current flavor, install rvs ctx\n",
@@ -2245,8 +2248,10 @@ int sptlrpc_target_export_check(struct obd_export *exp,
 			       exp->exp_flvr_old[1].sf_rpc);
 			spin_unlock(&exp->exp_lock);
 
-			return sptlrpc_svc_install_rvs_ctx(exp->exp_imp_reverse,
-							   req->rq_svc_ctx);
+			rc = sptlrpc_svc_install_rvs_ctx(exp->exp_imp_reverse,
+							 req->rq_svc_ctx);
+
+			GOTO(nm_switch, rc);
 		}
 	}
 
@@ -2319,6 +2324,21 @@ int sptlrpc_target_export_check(struct obd_export *exp,
 	      exp->exp_flvr_expire[1] ?
 	      (s64)(exp->exp_flvr_expire[1] - ktime_get_real_seconds()) : 0);
 	return -EACCES;
+
+nm_switch:
+#ifdef HAVE_SERVER_SUPPORT
+	if (!rc && req->rq_svc_ctx && req->rq_svc_ctx->sc_nodemap) {
+		rc = nodemap_member_switch(exp, req->rq_svc_ctx->sc_nodemap,
+					   true);
+		if (rc) {
+			/* do not fail on issue with nodemap switch */
+			CDEBUG(D_SEC, "%s: could not switch nodemap: rc = %d\n",
+			       exp->exp_obd->obd_name, rc);
+			rc = 0;
+		}
+	}
+#endif
+	return rc;
 }
 EXPORT_SYMBOL(sptlrpc_target_export_check);
 

lustre/tests/sanity-sec.sh
@@ -12432,6 +12432,8 @@ test_102() {
 run_test 102 "SSK automatic key loading from mount point"
 
 cleanup_103() {
+	local client_ip=$(host_nids_address $HOSTNAME $NETTYPE)
+	local client_nid=$(h2nettype $client_ip)
 	local test_key="$SK_PATH/$FSNAME-test103.key"
 
 	# ensure client is unmounted
@@ -12453,6 +12455,11 @@ cleanup_103() {
 	do_nodes $(comma_list $(all_server_nodes)) \
 		"$LGSS_SK -l $SK_PATH/$FSNAME.key -vvv" ||
 		error "failed to reload key on servers"
+
+	# check old exports have been gc'ed
+	wait_update_facet --verbose mds1 \
+		"$LCTL get_param nodemap.default.exports |
+			grep -c $client_nid" 0 120 || true
 
 	# Remount client
 	zconf_mount $HOSTNAME $MOUNT || error "re-mount $MOUNT failed"
@@ -12689,6 +12696,97 @@ test_104() {
 		error "wrong # keys in keyring after removing server key. Expected 0, got $key_cnt (5)"
 }
 run_test 104 "SSK key removal from keyring"
+
+cleanup_105() {
+	local nm_nid=$1
+	local nm_gss=$2
+
+	# ensure client is unmounted
+	umount_client $MOUNT || true
+
+	# remove nodemaps
+	do_facet mgs $LCTL nodemap_del $nm_nid
+	cleanup_local_client_nodemap $nm_gss
+
+	# remount all clients
+	restore_mount $MOUNT
+	if [[ "$MOUNT_2" ]]; then
+		restore_mount $MOUNT2
+	fi
+
+	restore_to_default_flavor
+}
+
+test_105() {
+	local client_ip=$(host_nids_address $HOSTNAME $NETTYPE)
+	local client_nid=$(h2nettype $client_ip)
+	local nm_nid=nm0
+	local nm_gss=c0
+	local exp_cnt
+
+	$SHARED_KEY || skip "Need shared key feature for this test"
+
+	local_mode && skip "in local mode."
+
+	(( MDS1_VERSION >= $(version_code 2.17.50) )) ||
+		skip "Need MDS >= 2.17.50 for sec change nm update"
+
+	stack_trap "cleanup_105 $nm_nid $nm_gss"
+
+	# deactivate SSK for now
+	remove_flavor_all
+	wait_flavor cli2mdt null
+	wait_flavor cli2ost null
+
+	# unmount all clients
+	cleanup_mount $MOUNT
+	if [[ "$MOUNT_2" ]]; then
+		cleanup_mount $MOUNT2
+	fi
+
+	# create c0 nodemap with gssonly, SSK key already created by t-f
+	setup_local_client_nodemap $nm_gss 1 1
+	do_facet mgs $LCTL nodemap_del_range \
+		--name $nm_gss --range $client_nid ||
+		error "del range $client_nid from $nm_gss failed"
+	do_facet mgs $LCTL nodemap_modify --name $nm_gss \
+		--property gssonly_identification --value 1 ||
+		error "setting gssonly_identification on $nm_gss failed"
+	wait_nm_sync $nm_gss gssonly_identification
+
+	# create nm0 nodemap, with regular NID range
+	setup_local_client_nodemap $nm_nid 1 1
+	wait_nm_sync $nm_nid trusted_nodemap
+
+	# remount client to take nodemap into account
+	zconf_mount_clients $HOSTNAME $MOUNT $MOUNT_OPTS ||
+		error "remount failed"
+
+	# check nodemap exports, should be in nm0 because flavor is null
+	do_facet mds1 "$LCTL get_param nodemap.*.exports"
+	exp_cnt=$(do_facet mds1 "$LCTL get_param nodemap.$nm_nid.exports |
+		  grep MDT | grep -c $client_nid")
+	(( exp_cnt > 0 )) ||
+		error "no exports for $client_nid on $nm_nid (1)"
+	exp_cnt=$(do_facet mds1 "$LCTL get_param nodemap.$nm_gss.exports |
+		  grep MDT | grep -c $client_nid")
+	(( exp_cnt == 0 )) ||
+		error "$exp_cnt != 0 exports for $client_nid on $nm_gss (1)"
+
+	restore_to_default_flavor || error "restore $SK_FLAVOR flavor failed"
+
+	# check nodemap exports, should have switched to c0
+	do_facet mds1 "$LCTL get_param nodemap.*.exports"
+	exp_cnt=$(do_facet mds1 "$LCTL get_param nodemap.$nm_nid.exports |
+		  grep MDT | grep -c $client_nid")
+	(( exp_cnt == 0 )) ||
+		error "$exp_cnt != 0 exports for $client_nid on $nm_nid (2)"
+	exp_cnt=$(do_facet mds1 "$LCTL get_param nodemap.$nm_gss.exports |
+		  grep MDT | grep -c $client_nid")
+	(( exp_cnt > 0 )) ||
+		error "no exports for $client_nid on $nm_gss (2)"
+}
+run_test 105 "update nodemap membership on flavor change"
 
 test_300() {
 	local principal="mock_iam_test"

lustre/tests/test-framework.sh
@@ -9967,17 +9967,13 @@ wait_flavor()
 	return 1
 }
 
-restore_to_default_flavor()
-{
+remove_flavor_all() {
 	local proc="mgs.MGS.live.$FSNAME"
-
-	echo "restoring to default flavor..."
-
 	local nrule=$(do_facet mgs lctl get_param -n $proc 2>/dev/null |
-		grep ".srpc.flavor" | wc -l)
+		      grep ".srpc.flavor" | wc -l)
 
 	# remove all existing rules if any
-	if [ $nrule -ne 0 ]; then
+	if (( nrule > 0 )); then
 		echo "$nrule existing rules"
 		for rule in $(do_facet mgs lctl get_param -n $proc 2>/dev/null |
 		    grep ".srpc.flavor."); do
@@ -9990,7 +9986,14 @@ restore_to_default_flavor()
 	# verify no rules left
 	nrule=$(do_facet mgs lctl get_param -n $proc 2>/dev/null |
 		grep ".srpc.flavor." | wc -l)
-	[ $nrule -ne 0 ] && error "still $nrule rules left"
+	(( nrule == 0 )) || error "still $nrule rules left"
+}
+
+restore_to_default_flavor()
+{
+	echo "restoring to default flavor..."
+
+	remove_flavor_all
 
 	# wait for default flavor to be applied
 	if $GSS_SK; then

commit 88d6c2d332117dc1d5320836e66c06960ece9230

LU-19921 nodemap: migrate inactive exports to default nodemap
During nodemap export reclassification, if an export does not have
an active connection, we previously simply skipped it because we
rely on reconnect to reclassify it later. However, this left the
export attached to the old nodemap configuration's member list,
which prevented the old configuration and its associated nodemaps
from ever being released.

Fix this by moving exports without active connections to the
default nodemap of the active configuration. This safely frees
the old configuration while still allowing subsequent reconnects
to properly reclassify the export.

Additionally, improve reference counting safety in
__nodemap_member_switch() by having it acquire a reference on
the new nodemap and release the reference on the old nodemap.
This avoids caller-side reference handover bugs.

Add !exp->exp_connection guard in nm_member_exp_revoke().

Fixes: fb7915fa7fb0 ("LU-19079 sec: update nodemap membership on flavor change")
Signed-off-by: Di Wang <ddiwang@google.com>
Change-Id: Ia1f65f1866d69ab2247b215ae8e32be0868b65cf
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67631
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -871,7 +871,7 @@ EXPORT_SYMBOL(nodemap_del_member);
 int nodemap_member_switch(struct obd_export *exp, char *new_nm_name,
 			  bool gssonly)
 {
-	struct lu_nodemap *old_nodemap = NULL, *new_nodemap;
+	struct lu_nodemap *new_nodemap;
 	int rc = 0;
 
 	ENTRY;
@@ -893,22 +893,12 @@ int nodemap_member_switch(struct obd_export *exp, char *new_nm_name,
 	if (gssonly && !new_nodemap->nmf_gss_identify)
 		GOTO(out, rc = -EPERM);
 
-	/* do nothing if nodemap does not change */
-	old_nodemap = nodemap_get_from_exp(exp);
-	if (new_nodemap == old_nodemap) {
-		nodemap_putref(new_nodemap);
-		GOTO(out, rc = 0);
-	}
-
 	__nodemap_member_switch(exp, new_nodemap, false, false);
 
 out:
 	mutex_unlock(&active_config_lock);
-	/* in case of success, keep the new_nodemap ref from nodemap_lookup */
-	if (rc && !IS_ERR(new_nodemap))
+	if (!IS_ERR(new_nodemap))
 		nodemap_putref(new_nodemap);
-	if (!IS_ERR_OR_NULL(old_nodemap))
-		nodemap_putref(old_nodemap);
 	RETURN(rc);
 }
 EXPORT_SYMBOL(nodemap_member_switch);

lustre/ptlrpc/nodemap_member.c
@@ -186,7 +186,8 @@ static void nm_member_exp_revoke(struct obd_export *exp, bool force_ost)
 		return;
 	if (test_bit(OBDF_RECOVERING, exp->exp_obd->obd_flags))
 		return;
-	if (nid_is_lo0(&exp->exp_connection->c_peer.nid) ||
+	if (!exp->exp_connection ||
+	    nid_is_lo0(&exp->exp_connection->c_peer.nid) ||
 	    is_lwp_on_ost(exp->exp_client_uuid.uuid) ||
 	    is_lwp_on_mdt(exp->exp_client_uuid.uuid))
 		return;
@@ -341,7 +342,8 @@ out_end:
  * This has to be done 'by hand' because ted_nodemap should never be NULL on
  * a live export, so nm_member_del() cannot be called.
  * This needs to be called with the active_config_lock held.
- *
+ * It also drops a ref on the old nodemap, and holds a ref on the new nodemap
+ * on success.
  */
 void __nodemap_member_switch(struct obd_export *exp,
 			     struct lu_nodemap *new_nodemap,
@@ -353,17 +355,19 @@ void __nodemap_member_switch(struct obd_export *exp,
 	/* could deadlock if new_nodemap also reclassifying,
 	 * active_config_lock serializes reclassifies
 	 */
-	mutex_lock(&new_nodemap->nm_member_list_lock);
-
-	list_del_init(&exp->exp_target_data.ted_nodemap_member);
-
 	spin_lock(&exp->exp_target_data.ted_nodemap_lock);
 	old_nodemap = exp->exp_target_data.ted_nodemap;
+	/* do nothing if nodemap does not change */
+	if (old_nodemap == new_nodemap) {
+		spin_unlock(&exp->exp_target_data.ted_nodemap_lock);
+		return;
+	}
+	nodemap_getref(new_nodemap);
 	exp->exp_target_data.ted_nodemap = new_nodemap;
 	spin_unlock(&exp->exp_target_data.ted_nodemap_lock);
-	if (old_nodemap)
-		nodemap_putref(old_nodemap);
 
+	mutex_lock(&new_nodemap->nm_member_list_lock);
+	list_del_init(&exp->exp_target_data.ted_nodemap_member);
 	list_add(&exp->exp_target_data.ted_nodemap_member,
 		 &new_nodemap->nm_member_list);
 	mutex_unlock(&new_nodemap->nm_member_list_lock);
@@ -385,6 +389,10 @@ void __nodemap_member_switch(struct obd_export *exp,
 
 	if (need_revoke)
 		nm_member_exp_revoke(exp, banned);
+
+	/* Drop the ref held by the old list */
+	if (old_nodemap)
+		nodemap_putref(old_nodemap);
 }
 
 /**
@@ -417,12 +425,21 @@ void nm_member_reclassify_nodemap(struct lu_nodemap *nodemap)
 
 		new_nodemap = NULL;
 
-		/* if no conn assigned to this exp, reconnect will reclassify */
 		spin_lock(&exp->exp_lock);
 		if (exp->exp_connection) {
 			nid = &exp->exp_connection->c_peer.nid;
 		} else {
+			/* if no conn assigned to this exp, reconnect will
+			 * reclassify, but it must be moved to the new
+			 * config's default nodemap to prevent stranded
+			 * exports pinning old configs.
+			 */
 			spin_unlock(&exp->exp_lock);
+			if (nodemap == active_config->nmc_default_nodemap)
+				continue;
+			new_nodemap = active_config->nmc_default_nodemap;
+			__nodemap_member_switch(exp, new_nodemap,
+						false, false);
 			continue;
 		}
 		spin_unlock(&exp->exp_lock);
@@ -508,8 +525,7 @@ classify:
 		if (new_nodemap != nodemap)
 			__nodemap_member_switch(exp, new_nodemap,
 						banned, newly_banned);
-		else
-			nodemap_putref(new_nodemap);
+		nodemap_putref(new_nodemap);
 	}
 
 	if (use_nm_cmp_cache) {