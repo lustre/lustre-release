lustre/lustre-release

commit 94470f7eeab5fde0648a14dda36941402c6a3e10

LU-9409 llite: Add tiny write support
If a page is already dirty in the page cache, we can write
to it without a full i/o.  This improves performance for
writes of < 1 page dramatically.

Append writes are a bit tricky, requiring us to take the
range lock (which we can normally avoid), but they are
still much faster than the normal i/o path.

Performance numbers with dd, on a VM with an older Xeon.

All numbers in MiB/s.

                8 bytes 1KiB
Without patch:  .75     75
With patch:     6.5     153

Cray-bug-id: LUS-1705
Signed-off-by: Patrick Farrell <paf@cray.com>
Change-Id: I75cc72ceb5f174a5394af8ffe5df4fe9583f19a3
Reviewed-on: https://review.whamcloud.com/27903
Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/llite/file.c
@@ -1563,6 +1563,101 @@ out:
 	return result;
 }
 
+/**
+ * Similar trick to ll_do_fast_read, this improves write speed for tiny writes.
+ * If a page is already in the page cache and dirty (and some other things -
+ * See ll_tiny_write_begin for the instantiation of these rules), then we can
+ * write to it without doing a full I/O, because Lustre already knows about it
+ * and will write it out.  This saves a lot of processing time.
+ *
+ * All writes here are within one page, so exclusion is handled by the page
+ * lock on the vm page.  Exception is appending, which requires locking the
+ * full file to handle size issues.  We do not do tiny writes for writes which
+ * touch multiple pages because it's very unlikely multiple sequential pages
+ * are already dirty.
+ *
+ * We limit these to < PAGE_SIZE because PAGE_SIZE writes are relatively common
+ * and are unlikely to be to already dirty pages.
+ *
+ * Attribute updates are important here, we do it in ll_tiny_write_end.
+ */
+static ssize_t ll_do_tiny_write(struct kiocb *iocb, struct iov_iter *iter)
+{
+	ssize_t count = iov_iter_count(iter);
+	struct file *file = iocb->ki_filp;
+	struct inode *inode = file_inode(file);
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct range_lock range;
+	ssize_t result = 0;
+	bool append = false;
+
+	ENTRY;
+
+	/* NB: we can't do direct IO for tiny writes because they use the page
+	 * cache, and we can't do sync writes because tiny writes can't flush
+	 * pages.
+	 */
+	if (file->f_flags & (O_DIRECT | O_SYNC))
+		RETURN(0);
+
+	/* It is relatively unlikely we will overwrite a full dirty page, so
+	 * limit tiny writes to < PAGE_SIZE
+	 */
+	if (count >= PAGE_SIZE)
+		RETURN(0);
+
+	/* For append writes, we must take the range lock to protect size
+	 * and also move pos to current size before writing.
+	 */
+	if (file->f_flags & O_APPEND) {
+		struct lu_env *env;
+		__u16 refcheck;
+
+		append = true;
+		range_lock_init(&range, 0, LUSTRE_EOF);
+		result = range_lock(&lli->lli_write_tree, &range);
+		if (result)
+			RETURN(result);
+		env = cl_env_get(&refcheck);
+		if (IS_ERR(env))
+			GOTO(out, result = PTR_ERR(env));
+		ll_merge_attr(env, inode);
+		cl_env_put(env, &refcheck);
+		iocb->ki_pos = i_size_read(inode);
+	}
+
+	/* Does this write touch multiple pages?
+	 *
+	 * This partly duplicates the PAGE_SIZE check above, but must come
+	 * after range locking for append writes because it depends on the
+	 * write position (ki_pos).
+	 */
+	if ((iocb->ki_pos & (PAGE_SIZE-1)) + count > PAGE_SIZE)
+		goto out;
+
+	result = __generic_file_write_iter(iocb, iter);
+
+	/* If the page is not already dirty, ll_tiny_write_begin returns
+	 * -ENODATA.  We continue on to normal write.
+	 */
+	if (result == -ENODATA)
+		result = 0;
+
+	if (result > 0) {
+		ll_stats_ops_tally(ll_i2sbi(inode), LPROC_LL_WRITE_BYTES,
+				   result);
+		ll_file_set_flag(ll_i2info(inode), LLIF_DATA_MODIFIED);
+	}
+
+out:
+	if (append)
+		range_unlock(&lli->lli_write_tree, &range);
+
+	CDEBUG(D_VFSTRACE, "result: %zu, original count %zu\n", result, count);
+
+	RETURN(result);
+}
+
 /*
  * Write to a file (through the page cache).
  */
@@ -1570,8 +1665,18 @@ static ssize_t ll_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct vvp_io_args *args;
 	struct lu_env *env;
-	ssize_t result;
+	ssize_t rc_tiny, rc_normal;
 	__u16 refcheck;
+
+	ENTRY;
+
+	rc_tiny = ll_do_tiny_write(iocb, from);
+
+	/* In case of error, go on and try normal write - Only stop if tiny
+	 * write completed I/O.
+	 */
+	if (iov_iter_count(from) == 0)
+		GOTO(out, rc_normal = rc_tiny);
 
 	env = cl_env_get(&refcheck);
 	if (IS_ERR(env))
@@ -1581,10 +1686,21 @@ static ssize_t ll_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	args->u.normal.via_iter = from;
 	args->u.normal.via_iocb = iocb;
 
-	result = ll_file_io_generic(env, args, iocb->ki_filp, CIT_WRITE,
+	rc_normal = ll_file_io_generic(env, args, iocb->ki_filp, CIT_WRITE,
 				    &iocb->ki_pos, iov_iter_count(from));
+
+	/* On success, combine bytes written. */
+	if (rc_tiny >= 0 && rc_normal > 0)
+		rc_normal += rc_tiny;
+	/* On error, only return error from normal write if tiny write did not
+	 * write any bytes.  Otherwise return bytes written by tiny write.
+	 */
+	else if (rc_tiny > 0)
+		rc_normal = rc_tiny;
+
 	cl_env_put(env, &refcheck);
-	return result;
+out:
+	RETURN(rc_normal);
 }
 
 #ifndef HAVE_FILE_OPERATIONS_READ_WRITE_ITER
@@ -1694,31 +1810,24 @@ static ssize_t ll_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 static ssize_t ll_file_write(struct file *file, const char __user *buf,
 			     size_t count, loff_t *ppos)
 {
-	struct lu_env *env;
 	struct iovec   iov = { .iov_base = (void __user *)buf,
 			       .iov_len = count };
-        struct kiocb  *kiocb;
-        ssize_t        result;
-	__u16          refcheck;
-        ENTRY;
+	struct kiocb   kiocb;
+	ssize_t        result;
 
-        env = cl_env_get(&refcheck);
-        if (IS_ERR(env))
-                RETURN(PTR_ERR(env));
+	ENTRY;
 
-	kiocb = &ll_env_info(env)->lti_kiocb;
-        init_sync_kiocb(kiocb, file);
-        kiocb->ki_pos = *ppos;
+	init_sync_kiocb(&kiocb, file);
+	kiocb.ki_pos = *ppos;
 #ifdef HAVE_KIOCB_KI_LEFT
-	kiocb->ki_left = count;
+	kiocb.ki_left = count;
 #elif defined(HAVE_KI_NBYTES)
-	kiocb->ki_nbytes = count;
+	kiocb.ki_nbytes = count;
 #endif
 
-	result = ll_file_aio_write(kiocb, &iov, 1, kiocb->ki_pos);
-	*ppos = kiocb->ki_pos;
+	result = ll_file_aio_write(&kiocb, &iov, 1, kiocb.ki_pos);
+	*ppos = kiocb.ki_pos;
 
-	cl_env_put(env, &refcheck);
 	RETURN(result);
 }
 #endif /* !HAVE_FILE_OPERATIONS_READ_WRITE_ITER */

lustre/include/cl_object.h
@@ -869,6 +869,13 @@ struct cl_page_operations {
          */
         int (*cpo_is_vmlocked)(const struct lu_env *env,
                                const struct cl_page_slice *slice);
+
+	/**
+	 * Update file attributes when all we have is this page.  Used for tiny
+	 * writes to update attributes when we don't have a full cl_io.
+	 */
+	void (*cpo_page_touch)(const struct lu_env *env,
+			       const struct cl_page_slice *slice, size_t to);
         /**
          * Page destruction.
          */
@@ -2227,6 +2234,8 @@ void    cl_page_discard(const struct lu_env *env, struct cl_io *io,
 void    cl_page_delete(const struct lu_env *env, struct cl_page *pg);
 int     cl_page_is_vmlocked(const struct lu_env *env,
 			    const struct cl_page *pg);
+void	cl_page_touch(const struct lu_env *env, const struct cl_page *pg,
+		      size_t to);
 void    cl_page_export(const struct lu_env *env,
 		       struct cl_page *pg, int uptodate);
 loff_t  cl_offset(const struct cl_object *obj, pgoff_t idx);

lustre/llite/llite_internal.h
@@ -1024,7 +1024,6 @@ struct ll_thread_info {
 	struct iov_iter		lti_iter;
 	struct vvp_io_args	lti_args;
 	struct ra_io_arg	lti_ria;
-	struct kiocb		lti_kiocb;
 	struct ll_cl_context	lti_io_ctx;
 };
 

lustre/llite/rw26.c
@@ -637,13 +637,23 @@ out:
 	return result;
 }
 
+static int ll_tiny_write_begin(struct page *vmpage)
+{
+	/* Page must be present, up to date, dirty, and not in writeback. */
+	if (!vmpage || !PageUptodate(vmpage) || !PageDirty(vmpage) ||
+	    PageWriteback(vmpage))
+		return -ENODATA;
+
+	return 0;
+}
+
 static int ll_write_begin(struct file *file, struct address_space *mapping,
 			  loff_t pos, unsigned len, unsigned flags,
 			  struct page **pagep, void **fsdata)
 {
-	struct ll_cl_context *lcc;
+	struct ll_cl_context *lcc = NULL;
 	const struct lu_env  *env = NULL;
-	struct cl_io   *io;
+	struct cl_io   *io = NULL;
 	struct cl_page *page = NULL;
 
 	struct cl_object *clob = ll_i2info(mapping->host)->lli_clob;
@@ -658,8 +668,9 @@ static int ll_write_begin(struct file *file, struct address_space *mapping,
 
 	lcc = ll_cl_find(file);
 	if (lcc == NULL) {
-		io = NULL;
-		GOTO(out, result = -EIO);
+		vmpage = grab_cache_page_nowait(mapping, index);
+		result = ll_tiny_write_begin(vmpage);
+		GOTO(out, result);
 	}
 
 	env = lcc->lcc_env;
@@ -672,6 +683,7 @@ static int ll_write_begin(struct file *file, struct address_space *mapping,
 		 * problem submitting the I/O. */
 		GOTO(out, result = -EBUSY);
 	}
+
 again:
 	/* To avoid deadlock, try to lock page first. */
 	vmpage = grab_cache_page_nowait(mapping, index);
@@ -733,7 +745,6 @@ again:
 
 				if (result == -EAGAIN)
 					goto again;
-
 				GOTO(out, result);
 			}
 		}
@@ -745,6 +756,7 @@ out:
 			unlock_page(vmpage);
 			put_page(vmpage);
 		}
+		/* On tiny_write failure, page and io are always null. */
 		if (!IS_ERR_OR_NULL(page)) {
 			lu_ref_del(&page->cp_reference, "cl_io", io);
 			cl_page_put(env, page);
@@ -756,6 +768,47 @@ out:
 		*fsdata = lcc;
 	}
 	RETURN(result);
+}
+
+static int ll_tiny_write_end(struct file *file, struct address_space *mapping,
+			     loff_t pos, unsigned int len, unsigned int copied,
+			     struct page *vmpage)
+{
+	struct cl_page *clpage = (struct cl_page *) vmpage->private;
+	loff_t kms = pos+copied;
+	loff_t to = kms & (PAGE_SIZE-1) ? kms & (PAGE_SIZE-1) : PAGE_SIZE;
+	__u16 refcheck;
+	struct lu_env *env = cl_env_get(&refcheck);
+	int rc = 0;
+
+	ENTRY;
+
+	if (IS_ERR(env)) {
+		rc = PTR_ERR(env);
+		goto out;
+	}
+
+	/* This page is dirty in cache, so it should have a cl_page pointer
+	 * set in vmpage->private.
+	 */
+	LASSERT(clpage != NULL);
+
+	if (copied == 0)
+		goto out_env;
+
+	/* Update the underlying size information in the OSC/LOV objects this
+	 * page is part of.
+	 */
+	cl_page_touch(env, clpage, to);
+
+out_env:
+	cl_env_put(env, &refcheck);
+
+out:
+	/* Must return page unlocked. */
+	unlock_page(vmpage);
+
+	RETURN(rc);
 }
 
 static int ll_write_end(struct file *file, struct address_space *mapping,
@@ -773,6 +826,14 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 	ENTRY;
 
 	put_page(vmpage);
+
+	CDEBUG(D_VFSTRACE, "pos %llu, len %u, copied %u\n", pos, len, copied);
+
+	if (lcc == NULL) {
+		result = ll_tiny_write_end(file, mapping, pos, len, copied,
+					   vmpage);
+		GOTO(out, result);
+	}
 
 	LASSERT(lcc != NULL);
 	env  = lcc->lcc_env;
@@ -821,6 +882,9 @@ static int ll_write_end(struct file *file, struct address_space *mapping,
 
 	if (result < 0)
 		io->ci_result = result;
+
+
+out:
 	RETURN(result >= 0 ? copied : result);
 }
 

lustre/obdclass/cl_object.c
@@ -844,13 +844,11 @@ EXPORT_SYMBOL(cl_env_put);
  */
 void cl_attr2lvb(struct ost_lvb *lvb, const struct cl_attr *attr)
 {
-        ENTRY;
         lvb->lvb_size   = attr->cat_size;
         lvb->lvb_mtime  = attr->cat_mtime;
         lvb->lvb_atime  = attr->cat_atime;
         lvb->lvb_ctime  = attr->cat_ctime;
         lvb->lvb_blocks = attr->cat_blocks;
-        EXIT;
 }
 
 /**
@@ -860,13 +858,11 @@ void cl_attr2lvb(struct ost_lvb *lvb, const struct cl_attr *attr)
  */
 void cl_lvb2attr(struct cl_attr *attr, const struct ost_lvb *lvb)
 {
-        ENTRY;
         attr->cat_size   = lvb->lvb_size;
         attr->cat_mtime  = lvb->lvb_mtime;
         attr->cat_atime  = lvb->lvb_atime;
         attr->cat_ctime  = lvb->lvb_ctime;
         attr->cat_blocks = lvb->lvb_blocks;
-        EXIT;
 }
 EXPORT_SYMBOL(cl_lvb2attr);
 

lustre/obdclass/cl_page.c
@@ -804,6 +804,22 @@ int cl_page_is_vmlocked(const struct lu_env *env, const struct cl_page *pg)
 }
 EXPORT_SYMBOL(cl_page_is_vmlocked);
 
+void cl_page_touch(const struct lu_env *env, const struct cl_page *pg,
+		  size_t to)
+{
+	const struct cl_page_slice *slice;
+
+	ENTRY;
+
+	list_for_each_entry(slice, &pg->cp_layers, cpl_linkage) {
+		if (slice->cpl_ops->cpo_page_touch != NULL)
+			(*slice->cpl_ops->cpo_page_touch)(env, slice, to);
+	}
+
+	EXIT;
+}
+EXPORT_SYMBOL(cl_page_touch);
+
 static enum cl_page_state cl_req_type_state(enum cl_req_type crt)
 {
         ENTRY;

lustre/osc/osc_internal.h
@@ -160,6 +160,8 @@ int osc_quotactl(struct obd_device *unused, struct obd_export *exp,
 void osc_inc_unstable_pages(struct ptlrpc_request *req);
 void osc_dec_unstable_pages(struct ptlrpc_request *req);
 bool osc_over_unstable_soft_limit(struct client_obd *cli);
+void osc_page_touch_at(const struct lu_env *env, struct cl_object *obj,
+		       pgoff_t idx, size_t to);
 
 struct ldlm_lock *osc_obj_dlmlock_at_pgoff(const struct lu_env *env,
 					   struct osc_object *obj,

lustre/osc/osc_io.c
@@ -214,34 +214,28 @@ int osc_io_submit(const struct lu_env *env, const struct cl_io_slice *ios,
 EXPORT_SYMBOL(osc_io_submit);
 
 /**
- * This is called when a page is accessed within file in a way that creates
- * new page, if one were missing (i.e., if there were a hole at that place in
- * the file, or accessed page is beyond the current file size).
+ * This is called to update the attributes when modifying a specific page,
+ * both when making new pages and when doing updates to existing cached pages.
  *
  * Expand stripe KMS if necessary.
  */
-static void osc_page_touch_at(const struct lu_env *env,
-			      struct cl_object *obj, pgoff_t idx, size_t to)
+void osc_page_touch_at(const struct lu_env *env, struct cl_object *obj,
+		       pgoff_t idx, size_t to)
 {
-        struct lov_oinfo  *loi  = cl2osc(obj)->oo_oinfo;
-        struct cl_attr    *attr = &osc_env_info(env)->oti_attr;
-        int valid;
-        __u64 kms;
+	struct lov_oinfo  *loi  = cl2osc(obj)->oo_oinfo;
+	struct cl_attr    *attr = &osc_env_info(env)->oti_attr;
+	int valid;
+	__u64 kms;
 
-        /* offset within stripe */
-        kms = cl_offset(obj, idx) + to;
+	ENTRY;
 
-        cl_object_attr_lock(obj);
-        /*
-         * XXX old code used
-         *
-         *         ll_inode_size_lock(inode, 0); lov_stripe_lock(lsm);
-         *
-         * here
-         */
+	/* offset within stripe */
+	kms = cl_offset(obj, idx) + to;
+
+	cl_object_attr_lock(obj);
 	CDEBUG(D_INODE, "stripe KMS %sincreasing %llu->%llu %llu\n",
-               kms > loi->loi_kms ? "" : "not ", loi->loi_kms, kms,
-               loi->loi_lvb.lvb_size);
+	       kms > loi->loi_kms ? "" : "not ", loi->loi_kms, kms,
+	       loi->loi_lvb.lvb_size);
 
 	attr->cat_mtime = attr->cat_ctime = ktime_get_real_seconds();
 	valid = CAT_MTIME | CAT_CTIME;
@@ -255,6 +249,8 @@ static void osc_page_touch_at(const struct lu_env *env,
 	}
 	cl_object_attr_update(env, obj, attr, valid);
 	cl_object_attr_unlock(obj);
+
+	EXIT;
 }
 
 int osc_io_commit_async(const struct lu_env *env,

commit 023776b00454fa07043a0bd3d11103b2851fe1c8

LU-20253 llite: merge attributes after tiny writes
When ll_do_tiny_write() consumes the whole iov,
ll_file_write_iter() returns without calling ll_file_io_generic().
While other writes merge attribute at the end of
ll_file_io_generic(), nothing does this for tiny writes;
ll_tiny_write_end() only updates KMS via cl_page_touch(),
so i_size_read() returns a stale size until a later getattr or
glimpse.

In-kernel i_size_read() users (like overlayfs) observe the stale
size. The wrong size is cached on the overlay inode and is not
refreshed by a later Lustre getattr.

Call ll_merge_attr() from ll_do_tiny_write() once the write has
completed, matching what ll_file_io_generic() does for every other
write. No RPCs are added: cl_object_attr_get() only reads cached
per-stripe attributes.

The merge is done at the end of the write operation rather than in
the per-page ll_tiny_write_end(): that runs with the page lock held,
while ll_merge_attr() sleeps on lli_size_mutex, which
vvp_do_vmtruncate() takes before locking pages in
truncate_pagecache().

A micro-benchmark with fio 8-byte sequential buffered writes
looping over a 64K file (--ioengine=psync --rw=write --bs=8
--size=64k --time_based --runtime=10) show a small performance
regression, which shouldn't appear in real workloads:

  without patch: IOPS=364k, BW=2845KiB/s
  with patch:    IOPS=319k, BW=2492KiB/s

Fixes: 94470f7eeab5 ("LU-9409 llite: Add tiny write support")
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I14fb5b558477fbfdaed456d8d978ff4ac35f288a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67801
Reviewed-by: Keguang Xu <kxu@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Patrick Farrell <patrick@thelustrecollective.com>

lustre/llite/file.c
@@ -2574,7 +2574,8 @@ static ssize_t ll_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
  * We limit these to < PAGE_SIZE because PAGE_SIZE writes are relatively common
  * and are unlikely to be to already dirty pages.
  *
- * Attribute updates are important here, we do them in ll_tiny_write_end.
+ * Attribute updates are important here: ll_tiny_write_end() updates the
+ * OSC-layer attributes, and they are merged into the inode below.
  */
 static ssize_t ll_do_tiny_write(struct kiocb *iocb, struct iov_iter *iter)
 {
@@ -2609,6 +2610,15 @@ static ssize_t ll_do_tiny_write(struct kiocb *iocb, struct iov_iter *iter)
 		result = 0;
 
 	if (result > 0) {
+		struct lu_env *env;
+		__u16 refcheck;
+
+		env = cl_env_get(&refcheck);
+		if (!IS_ERR(env)) {
+			ll_merge_attr(env, inode);
+			cl_env_put(env, &refcheck);
+		}
+
 		ll_heat_add(inode, CIT_WRITE, result);
 		set_bit(LLIF_DATA_MODIFIED, &ll_i2info(inode)->lli_flags);
 	}