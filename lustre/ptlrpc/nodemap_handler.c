lustre/lustre-release

commit 14b519b52d403ebc283e56fd223177c3c885518f

LU-17410 sec: per-nodemap capabilities mask
Add a per-nodemap capabilities mask, used in preference to the global
enable_cap_mask parameter if it is set.
The new nodemap property is named enable_cap_mask, and can be set
thanks to the new lctl command 'nodemap_set_cap'. It is possible to
specify capabilities in hex or with symbolic names, with '+' and '-'
prefixes to respectively add or remove corresponding capabilities.
We support defining 2 types of capabilities, either a "set" so that it
is possible to add capabilities, or a "mask" to reduce capabilities of
the client.
This per-nodemap capabilities mask is available on any nodemap
including the default nodemap.

A dynamic child nodemap is allowed to define only a subset of the
capabilities set on the parent, unless the child_raise_privileges
property has the 'caps' privilege.

sanity-sec test_51 is enhanced to exercise this new nodemap property.

Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
Change-Id: I1ed91c721d869d0596af9c2d7e07a2c411f2b7c2
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57938
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/nodemap_handler.c
@@ -15,6 +15,7 @@
 #include <lustre_net.h>
 #include <lustre_acl.h>
 #include <obd_class.h>
+#include <libcfs/libcfs_caps.h>
 
 #include "nodemap_internal.h"
 #include "ptlrpc_internal.h"
@@ -227,6 +228,7 @@ static bool allow_op_on_nm(struct lu_nodemap *nodemap)
  * - nmf_rbac
  * - nmf_rbac_raise
  * - nmf_forbid_encryption
+ * - nm_capabilities
  * If nmf_raise_privs grants corresponding privilege, any change on these
  * properties is permitted. Otherwise, only lowering privileges is possible,
  * which means:
@@ -237,6 +239,7 @@ static bool allow_op_on_nm(struct lu_nodemap *nodemap)
  * - nmf_rbac to fewer roles
  * - nmf_rbac_raise to fewer roles
  * - nmf_forbid_encryption from 1 (parent) to 0
+ * - nm_capabilities of child is a subset of parent's
  *
  * Return:
  * * %true		if the modification is allowed
@@ -247,6 +250,7 @@ static bool check_privs_for_op(struct lu_nodemap *nodemap,
 	u32 prop_val = (u32)(0xffffffff & val);
 	/* only relevant with priv == NODEMAP_RAISE_PRIV_RAISE */
 	u32 rbac_raise = (u32)(val >> 32);
+	kernel_cap_t *newcaps;
 
 	if (!allow_op_on_nm(nodemap))
 		return false;
@@ -279,6 +283,10 @@ static bool check_privs_for_op(struct lu_nodemap *nodemap,
 	case NODEMAP_RAISE_PRIV_FORBID_ENC:
 		return (nodemap->nm_parent_nm->nmf_forbid_encryption ||
 			!prop_val);
+	case NODEMAP_RAISE_PRIV_CAPS:
+		newcaps = (kernel_cap_t *)&val;
+		return cap_issubset(*newcaps,
+				    nodemap->nm_parent_nm->nm_capabilities);
 	default:
 		return true;
 	}
@@ -1096,6 +1104,8 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 		dst->nm_offset_limit_projid = 0;
 		dst->nm_prim_fileset = NULL;
 		dst->nm_prim_fileset_size = 0;
+		dst->nm_capabilities = CAP_EMPTY_SET;
+		dst->nmf_caps_type = NODEMAP_CAP_OFF;
 	} else {
 		dst->nmf_trust_client_ids = src->nmf_trust_client_ids;
 		dst->nmf_allow_root_access = src->nmf_allow_root_access;
@@ -1138,6 +1148,8 @@ static int nodemap_inherit_properties(struct lu_nodemap *dst,
 			dst->nm_prim_fileset = NULL;
 			dst->nm_prim_fileset_size = 0;
 		}
+		dst->nm_capabilities = src->nm_capabilities;
+		dst->nmf_caps_type = src->nmf_caps_type;
 	}
 
 out:
@@ -1672,6 +1684,102 @@ const char *nodemap_get_sepol(const struct lu_nodemap *nodemap)
 		return (char *)nodemap->nm_sepol;
 }
 EXPORT_SYMBOL(nodemap_get_sepol);
+
+/**
+ * nodemap_set_capabilities() - Define user capabilities on nodemap
+ * @name: name of nodemap
+ * @buffer: capabilities to set
+ *
+ * It is possible to specify capabilities in hex or with symbolic names, with
+ * '+' and '-' prefixes to respectively add or remove corresponding
+ * capabilities. If buffer starts with "set:", the capabilities are set to the
+ * specified ones, making it possible to add capabilities. If buffer starts with
+ * "mask:", the capabilities are filtered through the specified mask. If buffer
+ * is "off", the enable_cap_mask property is cleared.
+ *
+ * Return:
+ * * %0 on success
+ */
+int nodemap_set_capabilities(const char *name, char *buffer)
+{
+	static kernel_cap_t allowed_cap = CAP_EMPTY_SET;
+	struct lu_nodemap *nodemap = NULL;
+	enum nodemap_cap_type type;
+	unsigned long long caps;
+	kernel_cap_t newcaps;
+	bool cap_was_clear;
+	u64 *p_newcaps;
+	u64 cap_tmp;
+	char *caps_str;
+	int i, rc;
+
+	caps_str = strchr(buffer, ':');
+	if (!caps_str)
+		GOTO(out, rc = -EINVAL);
+	*caps_str = '\0';
+	caps_str++;
+
+	for (i = 0; i < ARRAY_SIZE(nodemap_captype_names); i++) {
+		if (strcmp(buffer, nodemap_captype_names[i].ncn_name) == 0) {
+			type = nodemap_captype_names[i].ncn_type;
+			break;
+		}
+	}
+	if (i == ARRAY_SIZE(nodemap_captype_names))
+		GOTO(out, rc = -EINVAL);
+
+	mutex_lock(&active_config_lock);
+	nodemap = nodemap_lookup(name);
+	if (IS_ERR(nodemap)) {
+		mutex_unlock(&active_config_lock);
+		GOTO(out, rc = PTR_ERR(nodemap));
+	}
+
+	rc = kstrtoull(caps_str, 0, &caps);
+	if (rc == -EINVAL) {
+		cap_tmp = libcfs_cap2num(nodemap->nm_capabilities);
+		/* if type is different, capabilities are going to be reset */
+		if (type != nodemap->nmf_caps_type)
+			cap_tmp = libcfs_cap2num(CAP_EMPTY_SET);
+
+		/* the "allmask" is filtered by allowed_cap below */
+		rc = cfs_str2mask(caps_str, libcfs_cap2str, &cap_tmp, 0,
+				  ~0ULL, 0);
+		caps = cap_tmp;
+	}
+	if (rc)
+		GOTO(out_putref, rc);
+
+	/* All of the capabilities that we currently allow/check */
+	if (unlikely(cap_isclear(allowed_cap))) {
+		allowed_cap = CAP_FS_SET;
+		cap_raise(allowed_cap, CAP_SYS_RESOURCE);
+	}
+
+	newcaps = cap_intersect(libcfs_num2cap(caps), allowed_cap);
+	p_newcaps = (u64 *)&newcaps;
+	if (!check_privs_for_op(nodemap, NODEMAP_RAISE_PRIV_CAPS, *p_newcaps))
+		GOTO(out_putref, rc = -EPERM);
+
+	cap_was_clear = cap_isclear(nodemap->nm_capabilities);
+	nodemap->nm_capabilities = newcaps;
+	nodemap->nmf_caps_type = type;
+
+	if (cap_isclear(nodemap->nm_capabilities))
+		rc = nodemap_idx_capabilities_del(nodemap);
+	else if (cap_was_clear)
+		rc = nodemap_idx_capabilities_add(nodemap);
+	else
+		rc = nodemap_idx_capabilities_update(nodemap);
+
+	nm_member_revoke_locks(nodemap);
+
+out_putref:
+	mutex_unlock(&active_config_lock);
+	nodemap_putref(nodemap);
+out:
+	return rc;
+}
 
 /**
  * nodemap_create() - Nodemap constructor
@@ -3277,6 +3385,9 @@ static int cfg_nodemap_cmd(enum lcfg_command_type cmd, const char *nodemap_name,
 	case LCFG_NODEMAP_SET_SEPOL:
 		rc = nodemap_set_sepol(nodemap_name, param, true);
 		break;
+	case LCFG_NODEMAP_SET_CAPS:
+		rc = nodemap_set_capabilities(nodemap_name, param);
+		break;
 	default:
 		rc = -EINVAL;
 	}
@@ -3420,6 +3531,7 @@ int server_iocontrol_nodemap(struct obd_device *obd,
 	case LCFG_NODEMAP_DEL_PROJIDMAP:
 	case LCFG_NODEMAP_SET_FILESET:
 	case LCFG_NODEMAP_SET_SEPOL:
+	case LCFG_NODEMAP_SET_CAPS:
 		if (lcfg->lcfg_bufcount != 3)
 			GOTO(out_lcfg, rc = -EINVAL);
 		nodemap_name = lustre_cfg_string(lcfg, 1);

libcfs/include/libcfs/Makefile.am
@@ -15,4 +15,5 @@ EXTRA_DIST = \
 	libcfs.h \
 	libcfs_debug.h \
 	libcfs_fail.h \
-	libcfs_private.h
+	libcfs_private.h \
+	libcfs_caps.h

libcfs/include/libcfs/libcfs_caps.h
@@ -1,0 +1,97 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (c) 2025 Whamcloud
+ */
+
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Generic capabilities manipulation functions.
+ *
+ */
+
+#ifndef __LIBCFS_CAPS_H__
+#define __LIBCFS_CAPS_H__
+
+static inline const char *libcfs_cap2str(int cap)
+{
+	/* We don't allow using all capabilities, but the fields must exist.
+	 * The supported capabilities are CAP_FS_SET and CAP_NFSD_SET, plus
+	 * CAP_SYS_ADMIN for a bunch of HSM operations (that should be fixed).
+	 */
+	static const char *const capability_names[] = {
+		"cap_chown",			/*  0 */
+		"cap_dac_override",		/*  1 */
+		"cap_dac_read_search",		/*  2 */
+		"cap_fowner",			/*  3 */
+		"cap_fsetid",			/*  4 */
+		NULL,				/*  5 */
+		NULL,				/*  6 */
+		NULL,				/*  7 */
+		NULL,				/*  8 */
+		"cap_linux_immutable",		/*  9 */
+		NULL,				/* 10 */
+		NULL,				/* 11 */
+		NULL,				/* 12 */
+		NULL,				/* 13 */
+		NULL,				/* 14 */
+		NULL,				/* 15 */
+		NULL,				/* 16 */
+		NULL,				/* 17 */
+		NULL,				/* 18 */
+		NULL,				/* 19 */
+		NULL,				/* 20 */
+		/* we should use more precise capabilities than this */
+		"cap_sys_admin",		/* 21 */
+		NULL,				/* 22 */
+		NULL,				/* 23 */
+		"cap_sys_resource",		/* 24 */
+		NULL,				/* 25 */
+		NULL,				/* 26 */
+		"cap_mknod",			/* 27 */
+		NULL,				/* 28 */
+		NULL,				/* 29 */
+		NULL,				/* 30 */
+		NULL,				/* 31 */
+		"cap_mac_override",		/* 32 */
+	};
+
+	if (cap >= ARRAY_SIZE(capability_names))
+		return NULL;
+
+	return capability_names[cap];
+}
+
+/* convert a capability into an integer to print or manage more easily */
+static inline u64 libcfs_cap2num(kernel_cap_t cap)
+{
+#ifdef CAP_FOR_EACH_U32
+	/* kernels before v6.2-13111-gf122a08b197d had a more complex
+	 * kernel_cap_t structure with an array of __u32 values, but this
+	 * was then fixed to have a single __u64 value.  There are accessor
+	 * functions for the old kernel_cap_t but since that is now dead code
+	 * it isn't worthwhile to jump through hoops for compatibility for it.
+	 */
+	return ((u64)cap.cap[1] << 32) | cap.cap[0];
+#else
+	return cap.val;
+#endif
+}
+
+/* convert an integer into a capabilityt */
+static inline kernel_cap_t libcfs_num2cap(u64 num)
+{
+	kernel_cap_t cap;
+
+#ifdef CAP_FOR_EACH_U32
+	cap.cap[0] = num;
+	cap.cap[1] = (num >> 32);
+#else
+	cap.val = num;
+#endif
+
+	return cap;
+}
+
+#endif

libcfs/libcfs/debug.c
@@ -307,7 +307,7 @@ int cfs_mask2str(char *str, int size, u64 mask, const char *(*bit2str)(int bit),
 		str[len++] = '\n';
 	if (len < size)
 		str[len] = '\0';
-	else
+	else if (size)
 		str[size - 1] = '\0';
 
 	return len;

lustre/doc/Makefile.am
@@ -143,6 +143,8 @@ SERVER_MANFILES =				\
 	lctl-nodemap_info.8			\
 	lctl-nodemap-modify.8			\
 	lctl-nodemap_modify.8			\
+	lctl-nodemap-set-cap.8			\
+	lctl-nodemap_set_cap.8			\
 	lctl-nodemap-set-fileset.8		\
 	lctl-nodemap_set_fileset.8		\
 	lctl-nodemap-set-sepol.8		\

lustre/doc/lctl-nodemap-modify.8
@@ -113,6 +113,8 @@ separated) are the nodemap properties names that support privilege raise:
 .br
 - child_raise_privs
 .br
+- caps
+.br
 It is also possible to specify any roles accepted by the
 .B rbac
 property (see below).

lustre/doc/lctl-nodemap-set-cap.8
@@ -1,0 +1,89 @@
+.TH LCTL-NODEMAP_SET_CAP 8 2025-05-27 Lustre "Lustre Configuration Utilities"
+.SH NAME
+lctl-nodemap_set_cap \- set user capabilities
+.SH SYNOPSIS
+.SY "lctl nodemap_set_cap"
+.BI --name " NODEMAP"
+.BI --caps " CAPABILITIES"
+.BI --type " {mask|set|off}"
+.YS
+.SH DESCRIPTION
+.B nodemap_set_cap
+defines capabilities for regular users on the specified
+.IR NODEMAP ,
+as described by
+.IR CAPABILITIES .
+The
+.I CAPABILITIES
+are a mask if the type is
+.B mask
+or a 'set' if type is
+.BR set .
+.SH OPTIONS
+.TP
+.BI --name " NODEMAP"
+The name of the nodemap to define capabilities for.
+.TP
+.BI --caps " CAPABILITIES"
+is the capabilities to apply.
+.TP
+.BI --type " {mask|set|off}"
+is the desired type for capabilities:
+.B mask
+for a capability mask,
+.B set
+to set user capabilities to the specified value,
+.B off
+to disable capabilities filtering.
+.SH EXAMPLES
+Define a capability mask as "cap_chown,cap_dac_read_search" on nodemap 'nm1':
+.EX
+.B # lctl nodemap_set_cap --name nm1 \
+--caps cap_chown,cap_dac_read_search --type mask
+.EE
+.PP
+Add "cap_fowner" to the capability mask on nodemap 'nm1':
+.EX
+.B # lctl nodemap_set_cap --name nm1 \
+--caps +cap_fowner --type mask
+.EE
+.PP
+Remove all capabilities users might have on nodemap 'nm1':
+.EX
+.B # lctl nodemap_set_cap --name nm1 \
+--caps 0 --type mask
+.EE
+.PP
+Set capabilities to "cap_linux_immutable,cap_sys_resource" for regular users on
+nodemap 'nm2':
+.EX
+.B # lctl nodemap_set_cap --name nm2 \
+--caps cap_linux_immutable,cap_sys_resource --type set
+.EE
+.PP
+Add "cap_fsetid" to the capabilities for regular users on nodemap 'nm2':
+.EX
+.B # lctl nodemap_set_cap --name nm2 \
+--caps +cap_fsetid --type set
+.EE
+.PP
+Disable capabilities filtering for regular users on nodemap 'nm2':
+.EX
+.B # lctl nodemap_set_cap --name nm2 --type off
+.EE
+.SH AVAILABILITY
+.B lctl nodemap_set_cap
+is part of the
+.BR lustre (7)
+filesystem package since release 2.17.0
+.\" Added in commit v2.16.99~
+.SH SEE ALSO
+.BR lustre (7),
+.BR lctl-nodemap-activate (8),
+.BR lctl-nodemap-add (8),
+.BR lctl-nodemap-add-idmap (8),
+.BR lctl-nodemap-add-range (8),
+.BR lctl-nodemap-del (8),
+.BR lctl-nodemap-del-idmap (8),
+.BR lctl-nodemap-del-range (8),
+.BR lctl-nodemap-modify (8)

lustre/doc/lctl-nodemap_set_cap.8
@@ -1,0 +1,1 @@
+.so man8/lctl-nodemap-set-cap.8

commit 2c0a5eac28ff192f84557cfc41f7e505562b9275

LU-19921 nodemap: Skip nodemap update and lock revocation
Skip nodemap update and lock revocation if no change
are needed.

When clearing nodemap capabilities, nodemap_idx_capabilities_del()
can return -ENOENT if no capabilities record existed on disk (e.g.,
if inherited from default). Treat -ENOENT as success to avoid
improper rollback.

Fixes: 14b519b52d40 ("LU-17410 sec: per-nodemap capabilities mask")
Signed-off-by: Di Wang <ddiwang@google.com>
Change-Id: I746864998ee28b8c62cb729de468fa0db907c8fc
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67620
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lustre/ptlrpc/nodemap_handler.c
@@ -3574,6 +3574,8 @@ int nodemap_set_capabilities(const char *name, char *buffer)
 	enum nodemap_cap_type type;
 	unsigned long long caps;
 	kernel_cap_t newcaps;
+	enum nodemap_cap_type old_type;
+	kernel_cap_t old_caps;
 	bool cap_was_clear;
 	u64 *p_newcaps;
 	u64 cap_tmp;
@@ -3628,16 +3630,32 @@ int nodemap_set_capabilities(const char *name, char *buffer)
 	if (!check_privs_for_op(nodemap, NODEMAP_RAISE_PRIV_CAPS, *p_newcaps))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (cap_issubset(nodemap->nm_capabilities, newcaps) &&
+	    cap_issubset(newcaps, nodemap->nm_capabilities) &&
+	    nodemap->nmf_caps_type == type)
+		GOTO(out_putref, rc = 0);
+
+	old_caps = nodemap->nm_capabilities;
+	old_type = nodemap->nmf_caps_type;
 	cap_was_clear = cap_isclear(nodemap->nm_capabilities);
 	nodemap->nm_capabilities = newcaps;
 	nodemap->nmf_caps_type = type;
 
-	if (cap_isclear(nodemap->nm_capabilities))
+	if (cap_isclear(nodemap->nm_capabilities)) {
 		rc = nodemap_idx_capabilities_del(nodemap);
-	else if (cap_was_clear)
+		if (rc == -ENOENT)
+			rc = 0;
+	} else if (cap_was_clear) {
 		rc = nodemap_idx_capabilities_add(nodemap);
-	else
+	} else {
 		rc = nodemap_idx_capabilities_update(nodemap);
+	}
+	if (rc) {
+		nodemap->nm_capabilities = old_caps;
+		nodemap->nmf_caps_type = old_type;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
 
@@ -3827,8 +3845,9 @@ out:
  */
 int nodemap_set_deny_unknown(const char *name, bool deny_unknown)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -3839,10 +3858,20 @@ int nodemap_set_deny_unknown(const char *name, bool deny_unknown)
 				deny_unknown))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_deny_unknown == deny_unknown)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_deny_unknown;
 	nodemap->nmf_deny_unknown = deny_unknown;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_deny_unknown = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -3860,8 +3889,9 @@ EXPORT_SYMBOL(nodemap_set_deny_unknown);
  */
 int nodemap_set_allow_root(const char *name, bool allow_root)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -3871,10 +3901,20 @@ int nodemap_set_allow_root(const char *name, bool allow_root)
 	if (!check_privs_for_op(nodemap, NODEMAP_RAISE_PRIV_ADMIN, allow_root))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_allow_root_access == allow_root)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_allow_root_access;
 	nodemap->nmf_allow_root_access = allow_root;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_allow_root_access = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -3893,8 +3933,9 @@ EXPORT_SYMBOL(nodemap_set_allow_root);
  */
 int nodemap_set_trust_client_ids(const char *name, bool trust_client_ids)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -3905,10 +3946,20 @@ int nodemap_set_trust_client_ids(const char *name, bool trust_client_ids)
 				trust_client_ids))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_trust_client_ids == trust_client_ids)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_trust_client_ids;
 	nodemap->nmf_trust_client_ids = trust_client_ids;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_trust_client_ids = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -3919,8 +3970,9 @@ EXPORT_SYMBOL(nodemap_set_trust_client_ids);
 int nodemap_set_mapping_mode(const char *name,
 			     enum nodemap_mapping_modes map_mode)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	enum nodemap_mapping_modes old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -3928,10 +3980,20 @@ int nodemap_set_mapping_mode(const char *name,
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_map_mode == map_mode)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_map_mode;
 	nodemap->nmf_map_mode = map_mode;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_map_mode = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -3998,6 +4060,10 @@ int nodemap_set_rbac(const char *name, enum nodemap_rbac_roles rbac)
 	rc = nodemap_idx_cluster_roles_modify(nodemap, old_rbac,
 					      nodemap->nmf_raise_privs,
 					      nodemap->nmf_rbac_raise);
+	if (rc) {
+		nodemap->nmf_rbac = old_rbac;
+		GOTO(put, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
 put:
@@ -4022,8 +4088,9 @@ EXPORT_SYMBOL(nodemap_set_rbac);
  */
 int nodemap_set_squash_uid(const char *name, uid_t uid)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	uid_t old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4031,10 +4098,20 @@ int nodemap_set_squash_uid(const char *name, uid_t uid)
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nm_squash_uid == uid)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nm_squash_uid;
 	nodemap->nm_squash_uid = uid;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nm_squash_uid = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4057,8 +4134,9 @@ EXPORT_SYMBOL(nodemap_set_squash_uid);
  */
 int nodemap_set_squash_gid(const char *name, gid_t gid)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	gid_t old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4066,10 +4144,20 @@ int nodemap_set_squash_gid(const char *name, gid_t gid)
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nm_squash_gid == gid)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nm_squash_gid;
 	nodemap->nm_squash_gid = gid;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nm_squash_gid = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4093,8 +4181,9 @@ EXPORT_SYMBOL(nodemap_set_squash_gid);
  */
 int nodemap_set_squash_projid(const char *name, projid_t projid)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	projid_t old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4102,10 +4191,20 @@ int nodemap_set_squash_projid(const char *name, projid_t projid)
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nm_squash_projid == projid)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nm_squash_projid;
 	nodemap->nm_squash_projid = projid;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nm_squash_projid = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4207,8 +4306,9 @@ EXPORT_SYMBOL(nodemap_can_setquota);
  */
 int nodemap_set_audit_mode(const char *name, bool enable_audit)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4216,10 +4316,20 @@ int nodemap_set_audit_mode(const char *name, bool enable_audit)
 	if (!allow_op_on_nm(nodemap))
 		GOTO(out_putref, rc = -ENXIO);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_enable_audit == enable_audit)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_enable_audit;
 	nodemap->nmf_enable_audit = enable_audit;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_enable_audit = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4240,6 +4350,7 @@ int nodemap_set_forbid_encryption(const char *name, bool forbid_encryption)
 {
 	struct lu_nodemap *nodemap = NULL;
 	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4250,10 +4361,20 @@ int nodemap_set_forbid_encryption(const char *name, bool forbid_encryption)
 				forbid_encryption))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_forbid_encryption == forbid_encryption)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_forbid_encryption;
 	nodemap->nmf_forbid_encryption = forbid_encryption;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_forbid_encryption = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4300,8 +4421,14 @@ int nodemap_set_raise_privs(const char *name, enum nodemap_raise_privs privs,
 	nodemap->nmf_rbac_raise = rbac_raise;
 	rc = nodemap_idx_cluster_roles_modify(nodemap, nodemap->nmf_rbac,
 					      old_privs, old_rbac_raise);
+	if (rc) {
+		nodemap->nmf_raise_privs = old_privs;
+		nodemap->nmf_rbac_raise = old_rbac_raise;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4319,8 +4446,9 @@ EXPORT_SYMBOL(nodemap_set_raise_privs);
  */
 int nodemap_set_readonly_mount(const char *name, bool readonly_mount)
 {
-	struct lu_nodemap	*nodemap = NULL;
-	int			rc = 0;
+	struct lu_nodemap *nodemap = NULL;
+	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4331,10 +4459,20 @@ int nodemap_set_readonly_mount(const char *name, bool readonly_mount)
 				readonly_mount))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_readonly_mount == readonly_mount)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_readonly_mount;
 	nodemap->nmf_readonly_mount = readonly_mount;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_readonly_mount = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 out:
@@ -4354,6 +4492,7 @@ int nodemap_set_deny_mount(const char *name, bool deny_mount)
 {
 	struct lu_nodemap *nodemap = NULL;
 	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4364,10 +4503,20 @@ int nodemap_set_deny_mount(const char *name, bool deny_mount)
 				deny_mount))
 		GOTO(out_putref, rc = -EPERM);
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_deny_mount == deny_mount)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_deny_mount;
 	nodemap->nmf_deny_mount = deny_mount;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_deny_mount = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
+
 out_putref:
 	nodemap_putref(nodemap);
 	return rc;
@@ -4386,6 +4535,7 @@ int nodemap_set_gss_identify(const char *name, bool gss_identify)
 {
 	struct lu_nodemap *nodemap = NULL;
 	int rc = 0;
+	bool old_val;
 
 	nodemap = nodemap_lookup_unlocked(name);
 	if (IS_ERR(nodemap))
@@ -4411,8 +4561,17 @@ int nodemap_set_gss_identify(const char *name, bool gss_identify)
 		GOTO(out_putref, rc = -EPERM);
 	}
 
+	/* skip update and lock revocation if no change */
+	if (nodemap->nmf_gss_identify == gss_identify)
+		GOTO(out_putref, rc = 0);
+
+	old_val = nodemap->nmf_gss_identify;
 	nodemap->nmf_gss_identify = gss_identify;
 	rc = nodemap_idx_nodemap_update(nodemap);
+	if (rc) {
+		nodemap->nmf_gss_identify = old_val;
+		GOTO(out_putref, rc);
+	}
 
 	nm_member_revoke_locks(nodemap);
 