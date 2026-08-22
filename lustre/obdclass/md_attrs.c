lustre/lustre-release

commit 7d900dee052fa1dc044e58d1ce0f384409f46322

LU-9633 obdclass: Add kernel doc style for obdclass (13)
This patch converts existing functional comments
to kernel doc style comments and removes '/**' for
comments which is not meant to be a kernel-doc comment

Test-Parameters: trivial
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: Idbe923d25f007058cf0f0a287cc9670300f5ac3d
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66942
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/obdclass/md_attrs.c
@@ -18,11 +18,11 @@
 #include <md_object.h>
 
 /**
- * Initialize new \a lma. Only fid is stored.
- *
- * \param lma - is the new LMA structure to be initialized
- * \param fid - is the FID of the object this LMA belongs to
- * \param incompat - features that MDS must understand to access object
+ * lustre_lma_init() - Initialize new @lma.
+ * @lma: is the new LMA structure to be initialized
+ * @fid: is the FID of the object this LMA belongs to
+ * @compat: featores that MDS can ignore
+ * @incompat: features that MDS must understand to access object
  */
 void lustre_lma_init(struct lustre_mdt_attrs *lma, const struct lu_fid *fid,
 		     __u32 compat, __u32 incompat)
@@ -40,9 +40,9 @@ void lustre_lma_init(struct lustre_mdt_attrs *lma, const struct lu_fid *fid,
 EXPORT_SYMBOL(lustre_lma_init);
 
 /**
- * Swab, if needed, LMA structure which is stored on-disk in little-endian order.
- *
- * \param lma - is a pointer to the LMA structure to be swabbed.
+ * lustre_lma_swab() - Swab, if needed, LMA structure which is stored on-disk
+ *                     in little-endian order.
+ * @lma: pointer to the LMA structure to be swabbed.
  */
 void lustre_lma_swab(struct lustre_mdt_attrs *lma)
 {
@@ -65,11 +65,12 @@ void lustre_loa_init(struct lustre_ost_attrs *loa, const struct lu_fid *fid,
 EXPORT_SYMBOL(lustre_loa_init);
 
 /**
- * Swab, if needed, LOA (for OST-object only) structure with LMA EA and PFID EA
- * combined together are stored on-disk in little-endian order.
+ * lustre_loa_swab() - Swab, if needed, LOA (for OST-object only) structure
+ *                     with LMA EA and PFID EA combined together are stored
+ *                     on-disk in little-endian order.
  *
- * \param[in] loa	- the pointer to the LOA structure to be swabbed.
- * \param[in] to_cpu	- to indicate swab for CPU order or not.
+ * @loa: the pointer to the LOA structure to be swabbed.
+ * @to_cpu: to indicate swab for CPU order or not.
  */
 void lustre_loa_swab(struct lustre_ost_attrs *loa, bool to_cpu)
 {
@@ -97,10 +98,9 @@ void lustre_loa_swab(struct lustre_ost_attrs *loa, bool to_cpu)
 EXPORT_SYMBOL(lustre_loa_swab);
 
 /**
- * Swab, if needed, SOM structure which is stored on-disk in little-endian
- * order.
- *
- * \param attrs - is a pointer to the SOM structure to be swabbed.
+ * lustre_som_swab() - Swab, if needed, SOM structure which is stored on-disk
+ *                     in little-endian order.
+ * @attrs: is a pointer to the SOM structure to be swabbed.
  */
 void lustre_som_swab(struct lustre_som_attrs *attrs)
 {
@@ -113,10 +113,9 @@ void lustre_som_swab(struct lustre_som_attrs *attrs)
 EXPORT_SYMBOL(lustre_som_swab);
 
 /**
- * Swab, if needed, HSM structure which is stored on-disk in little-endian
- * order.
- *
- * \param attrs - is a pointer to the HSM structure to be swabbed.
+ * lustre_hsm_swab() - Swab, if needed, HSM structure which is stored on-disk
+ *                     in little-endian order.
+ * @attrs: is a pointer to the HSM structure to be swabbed.
  */
 void lustre_hsm_swab(struct hsm_attrs *attrs)
 {
@@ -128,12 +127,15 @@ void lustre_hsm_swab(struct hsm_attrs *attrs)
 #endif
 }
 
-/*
- * Swab and extract HSM attributes from on-disk xattr.
+/**
+ * lustre_buf2hsm() - Swab and extract HSM attributes from on-disk xattr.
+ * @buf: is a buffer containing the on-disk HSM extended attribute.
+ * @rc: is the size of the HSM xattr stored in @buf
+ * @mh: the md_hsm structure where to extract HSM attributes.
  *
- * \param buf - is a buffer containing the on-disk HSM extended attribute.
- * \param rc  - is the HSM xattr stored in \a buf
- * \param mh  - is the md_hsm structure where to extract HSM attributes.
+ * Return:
+ * * %0 on success
+ * * %negative on failure
  */
 int lustre_buf2hsm(void *buf, int rc, struct md_hsm *mh)
 {
@@ -162,11 +164,10 @@ int lustre_buf2hsm(void *buf, int rc, struct md_hsm *mh)
 }
 EXPORT_SYMBOL(lustre_buf2hsm);
 
-/*
- * Pack HSM attributes.
- *
- * \param buf - is the output buffer where to pack the on-disk HSM xattr.
- * \param mh  - is the md_hsm structure to pack.
+/**
+ * lustre_hsm2buf() - Pack HSM attributes.
+ * @buf: is the output buffer where to pack the on-disk HSM xattr.
+ * @mh: is the md_hsm structure to pack.
  */
 void lustre_hsm2buf(void *buf, const struct md_hsm *mh)
 {

commit c881f8ebcbd130cf6c2b9e8862582fc5d2da5289

LU-9633 obdclass: fix up md_attrs.c kernel doc
The kernel doc conversion of lustre/obdclass/md_attrs.c left
lustre_loa_init() as the only function in the file without a doc
block; add one describing @loa, @fid, @compat and @incompat.

None of these structures are MDT-only, so drop the wording that
says they are.  lustre_lma_init() is called on OST objects as
well - osd-zfs adds LMAC_FID_ON_OST in osd_create() and
osd_obj_create() - and lustre_loa_init() is used for MDT-side
inodes such as agent inodes and OI files via osd_ea_fid_set(),
so both blocks now describe @compat and @incompat as
LMAC_*/LMAI_* rather than as things "MDS" understands.  For the
same reason "(for OST-object only)" is dropped from
lustre_loa_swab(), which osd_get_lma() calls for every ldiskfs
inode; that summary's grammar is fixed while it is touched.

The new Return: section of lustre_buf2hsm() listed only success
and generic failure, but -ENODATA is a normal outcome meaning
the object carries no HSM xattr, and the callers that check the
return value handle it as such rather than as an error
(mdt_attr_get_complex(), mdd_hsm_update_locked() and
mdd_hsm_archive_exists()).  Document it separately from real
errors, and note in @rc that the value passed in may itself be
a negative errno.

A "featores" typo in the lustre_lma_init() doc block is fixed
too.

Comment-only change, no functional change.

Fixes: 7d900dee052f ("LU-9633 obdclass: Add kernel doc style for obdclass (13)")
Test-Parameters: trivial
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Iab1ab3ed2a08819491542c2f31c4ef58839137d2
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/68171
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/obdclass/md_attrs.c
@@ -21,8 +21,8 @@
  * lustre_lma_init() - Initialize new @lma.
  * @lma: is the new LMA structure to be initialized
  * @fid: is the FID of the object this LMA belongs to
- * @compat: featores that MDS can ignore
- * @incompat: features that MDS must understand to access object
+ * @compat: LMAC_* features that can be ignored if not understood
+ * @incompat: LMAI_* features that must be understood to access object
  */
 void lustre_lma_init(struct lustre_mdt_attrs *lma, const struct lu_fid *fid,
 		     __u32 compat, __u32 incompat)
@@ -54,6 +54,16 @@ void lustre_lma_swab(struct lustre_mdt_attrs *lma)
 }
 EXPORT_SYMBOL(lustre_lma_swab);
 
+/**
+ * lustre_loa_init() - Initialize new @loa.
+ * @loa: is the new LOA structure to be initialized
+ * @fid: is the FID of the object this LOA belongs to
+ * @compat: LMAC_* features that can be ignored if not understood
+ * @incompat: LMAI_* features that must be understood to access object
+ *
+ * The embedded LMA is initialized by lustre_lma_init(); the OST-object PFID
+ * EA part (parent FID, stripe and layout component information) is zeroed.
+ */
 void lustre_loa_init(struct lustre_ost_attrs *loa, const struct lu_fid *fid,
 		     __u32 compat, __u32 incompat)
 {
@@ -65,9 +75,9 @@ void lustre_loa_init(struct lustre_ost_attrs *loa, const struct lu_fid *fid,
 EXPORT_SYMBOL(lustre_loa_init);
 
 /**
- * lustre_loa_swab() - Swab, if needed, LOA (for OST-object only) structure
- *                     with LMA EA and PFID EA combined together are stored
- *                     on-disk in little-endian order.
+ * lustre_loa_swab() - Swab, if needed, the LOA structure, which combines the
+ *                     LMA EA with the OST-object PFID EA and is stored on-disk
+ *                     in little-endian order.
  *
  * @loa: the pointer to the LOA structure to be swabbed.
  * @to_cpu: to indicate swab for CPU order or not.
@@ -130,12 +140,17 @@ void lustre_hsm_swab(struct hsm_attrs *attrs)
 /**
  * lustre_buf2hsm() - Swab and extract HSM attributes from on-disk xattr.
  * @buf: is a buffer containing the on-disk HSM extended attribute.
- * @rc: is the size of the HSM xattr stored in @buf
- * @mh: the md_hsm structure where to extract HSM attributes.
+ * @rc: is the size of the HSM xattr stored in @buf, or the negative errno
+ *      from fetching it
+ * @mh: the md_hsm structure where to extract HSM attributes; it is only
+ *      filled in when %0 is returned
  *
  * Return:
  * * %0 on success
- * * %negative on failure
+ * * %-ENODATA if the object has no HSM xattr, i.e. @rc is %0 or %-ENODATA.
+ *   This is a normal outcome rather than an error, and callers are expected
+ *   to handle it as such.
+ * * %negative errno propagated from @rc if fetching the xattr failed
  */
 int lustre_buf2hsm(void *buf, int rc, struct md_hsm *mh)
 {