lustre/lustre-release

commit 5b3eef20c5a0e31d2cd9580f0cbaa0223783905a

LU-6343 lfsck: locate object only when necessary
Currently, for every item to be verified by the LFSCK assistant
thread, the LFSCK assistant thread will try to locate its parent
directory (for namespace LFSCK) or its parent MDT-object (for
layout LFSCK) firstly via the given parent FID. But under most
cases, the system is consistent, such locating parent object is
unnecessary. For example, for namespace LFSCK, the LFSCK main
engine has put the <parent_FID, child_name, child_FID> in the
pre-loaded request, then the assistant thread will locate the
child object with the child_FID and search its linkEA to find
the <parent_FID, child_name> pairs.

Be as some improvement, we will make the LFSCK to locate related
object (especially for parent object) only when really used.

Signed-off-by: Fan Yong <fan.yong@intel.com>
Change-Id: If6a3677a6ad3fa1c694ee06f9688c1f38374f8de
Reviewed-on: http://review.whamcloud.com/13993
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Reviewed-by: Lai Siyao <lai.siyao@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/lfsck/lfsck_namespace.c
@@ -59,6 +59,7 @@ enum lfsck_nameentry_check {
 
 static struct lfsck_namespace_req *
 lfsck_namespace_assistant_req_init(struct lfsck_instance *lfsck,
+				   struct lfsck_assistant_object *lso,
 				   struct lu_dirent *ent, __u16 type)
 {
 	struct lfsck_namespace_req *lnr;
@@ -70,10 +71,9 @@ lfsck_namespace_assistant_req_init(struct lfsck_instance *lfsck,
 		return ERR_PTR(-ENOMEM);
 
 	INIT_LIST_HEAD(&lnr->lnr_lar.lar_list);
-	lnr->lnr_lar.lar_fid = *lfsck_dto2fid(lfsck->li_obj_dir);
+	lnr->lnr_lar.lar_parent = lfsck_assistant_object_get(lso);
 	lnr->lnr_lmv = lfsck_lmv_get(lfsck->li_lmv);
 	lnr->lnr_fid = ent->lde_fid;
-	lnr->lnr_oit_cookie = lfsck->li_pos_current.lp_oit_cookie;
 	lnr->lnr_dir_cookie = ent->lde_hash;
 	lnr->lnr_attr = ent->lde_attrs;
 	lnr->lnr_size = size;
@@ -93,6 +93,7 @@ static void lfsck_namespace_assistant_req_fini(const struct lu_env *env,
 	if (lnr->lnr_lmv != NULL)
 		lfsck_lmv_put(env, lnr->lnr_lmv);
 
+	lfsck_assistant_object_put(env, lar->lar_parent);
 	OBD_FREE(lnr, lnr->lnr_size);
 }
 
@@ -904,7 +905,7 @@ static int lfsck_namespace_insert_orphan(const struct lu_env *env,
 	struct lfsck_thread_info	*info	= lfsck_env_info(env);
 	struct lu_name			*cname	= &info->lti_name;
 	struct dt_insert_rec		*rec	= &info->lti_dt_rec;
-	struct lu_attr			*la	= &info->lti_la3;
+	struct lu_attr			*la	= &info->lti_la2;
 	const struct lu_fid		*cfid	= lfsck_dto2fid(orphan);
 	const struct lu_fid		*pfid;
 	struct lu_fid			 tfid;
@@ -3812,6 +3813,7 @@ static void lfsck_namespace_close_dir(const struct lu_env *env,
 {
 	struct lfsck_namespace		*ns	= com->lc_file_ram;
 	struct lfsck_assistant_data	*lad	= com->lc_data;
+	struct lfsck_assistant_object	*lso	= NULL;
 	struct lfsck_instance		*lfsck	= com->lc_lfsck;
 	struct lfsck_lmv		*llmv	= lfsck->li_lmv;
 	struct lfsck_namespace_req	*lnr;
@@ -3830,13 +3832,21 @@ static void lfsck_namespace_close_dir(const struct lu_env *env,
 		RETURN_EXIT;
 	}
 
+	lso = lfsck_assistant_object_init(env, lfsck_dto2fid(lfsck->li_obj_dir),
+			NULL, lfsck->li_pos_current.lp_oit_cookie, true);
+	if (IS_ERR(lso)) {
+		OBD_FREE(lnr, size);
+		ns->ln_striped_dirs_skipped++;
+
+		RETURN_EXIT;
+	}
+
 	/* Generate a dummy request to indicate that all shards' name entry
 	 * in this striped directory has been scanned for the first time. */
 	INIT_LIST_HEAD(&lnr->lnr_lar.lar_list);
-	lnr->lnr_lar.lar_fid = *lfsck_dto2fid(lfsck->li_obj_dir);
+	lnr->lnr_lar.lar_parent = lso;
 	lnr->lnr_lmv = lfsck_lmv_get(llmv);
 	lnr->lnr_fid = *lfsck_dto2fid(lfsck->li_obj_dir);
-	lnr->lnr_oit_cookie = lfsck->li_pos_current.lp_oit_cookie;
 	lnr->lnr_dir_cookie = MDS_DIR_END_OFF;
 	lnr->lnr_size = size;
 
@@ -4120,6 +4130,7 @@ out:
 
 static int lfsck_namespace_exec_dir(const struct lu_env *env,
 				    struct lfsck_component *com,
+				    struct lfsck_assistant_object *lso,
 				    struct lu_dirent *ent, __u16 type)
 {
 	struct lfsck_assistant_data	*lad	 = com->lc_data;
@@ -4144,7 +4155,7 @@ static int lfsck_namespace_exec_dir(const struct lu_env *env,
 	if (unlikely(lfsck_is_dead_obj(lfsck->li_obj_dir)))
 		return 0;
 
-	lnr = lfsck_namespace_assistant_req_init(com->lc_lfsck, ent, type);
+	lnr = lfsck_namespace_assistant_req_init(com->lc_lfsck, lso, ent, type);
 	if (IS_ERR(lnr)) {
 		struct lfsck_namespace *ns = com->lc_file_ram;
 
@@ -4972,7 +4983,7 @@ log:
 	CDEBUG(D_LFSCK, "%s: namespace LFSCK assistant found dangling "
 	       "reference for: parent "DFID", child "DFID", type %u, "
 	       "name %s. %s: rc = %d\n", lfsck_lfsck2name(lfsck),
-	       PFID(&lnr->lnr_lar.lar_fid), PFID(lfsck_dto2fid(child)),
+	       PFID(lfsck_dto2fid(parent)), PFID(lfsck_dto2fid(child)),
 	       type, cname->ln_name,
 	       create ? "Create the lost OST-object as required" :
 			"Keep the MDT-object there by default", rc);
@@ -5002,7 +5013,8 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 			container_of0(lar, struct lfsck_namespace_req, lnr_lar);
 	struct dt_object	   *dir      = NULL;
 	struct dt_object	   *obj      = NULL;
-	const struct lu_fid	   *pfid;
+	struct lfsck_assistant_object *lso   = lar->lar_parent;
+	const struct lu_fid	   *pfid     = &lso->lso_fid;
 	struct dt_device	   *dev      = NULL;
 	struct lustre_handle	    lh       = { 0 };
 	bool			    repaired = false;
@@ -5017,17 +5029,9 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 	enum lfsck_namespace_inconsistency_type type = LNIT_NONE;
 	ENTRY;
 
-	dir = lfsck_object_find_bottom(env, lfsck, &lar->lar_fid);
-	if (IS_ERR(dir))
-		RETURN(PTR_ERR(dir));
+	if (lso->lso_dead)
+		RETURN(0);
 
-	if (unlikely(lfsck_is_dead_obj(dir)))
-		GOTO(put_dir, rc = 0);
-
-	if (unlikely(!dt_try_as_dir(env, dir)))
-		GOTO(put_dir, rc = -ENOTDIR);
-
-	pfid = lfsck_dto2fid(dir);
 	la->la_nlink = 0;
 	if (lnr->lnr_attr & LUDA_UPGRADE) {
 		ns->ln_flags |= LF_UPGRADE;
@@ -5068,18 +5072,18 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 	}
 
 	if (unlikely(lnr->lnr_dir_cookie == MDS_DIR_END_OFF)) {
-		rc = lfsck_namespace_striped_dir_rescan(env, com, dir, lnr);
+		rc = lfsck_namespace_striped_dir_rescan(env, com, lnr);
 
-		GOTO(put_dir, rc);
+		RETURN(rc);
 	}
 
 	if (fid_seq_is_dot(fid_seq(&lnr->lnr_fid)))
 		GOTO(out, rc = 0);
 
 	if (lnr->lnr_lmv != NULL && lnr->lnr_lmv->ll_lmv_master) {
-		rc = lfsck_namespace_handle_striped_master(env, com, dir, lnr);
+		rc = lfsck_namespace_handle_striped_master(env, com, lnr);
 
-		GOTO(put_dir, rc);
+		RETURN(rc);
 	}
 
 	idx = lfsck_find_mdt_idx_by_fid(env, lfsck, &lnr->lnr_fid);
@@ -5125,6 +5129,15 @@ static int lfsck_namespace_assistant_handler_p1(const struct lu_env *env,
 	if (dt_object_exists(obj) == 0) {
 
 dangling:
+		if (dir == NULL) {
+			dir = lfsck_assistant_object_load(env, lfsck, lso);
+			if (IS_ERR(dir)) {
+				rc = PTR_ERR(dir);
+
+				GOTO(trace, rc == -ENOENT ? 0 : rc);
+			}
+		}
+
 		rc = lfsck_namespace_check_exist(env, dir, obj, lnr->lnr_name);
 		if (rc == 0) {
 			if (!lfsck_is_valid_slave_name_entry(env, lnr->lnr_lmv,
@@ -5169,10 +5182,6 @@ again:
 		dtlocked = true;
 	}
 
-	rc = lfsck_namespace_check_exist(env, dir, obj, lnr->lnr_name);
-	if (rc != 0)
-		GOTO(stop, rc);
-
 	rc = lfsck_links_read(env, obj, &ldata);
 	if (unlikely(rc == -ENOENT)) {
 		if (handle != NULL) {
@@ -5202,13 +5211,12 @@ again:
 			goto stop;
 		}
 
-		ns->ln_flags |= LF_INCONSISTENT;
-
 		/* If the name entry hash does not match the slave striped
 		 * directory, and the name entry does not match also, then
 		 * it is quite possible that name entry is corrupted. */
 		if (!lfsck_is_valid_slave_name_entry(env, lnr->lnr_lmv,
 					lnr->lnr_name, lnr->lnr_namelen)) {
+			ns->ln_flags |= LF_INCONSISTENT;
 			type = LNIT_BAD_DIRENT;
 
 			GOTO(stop, rc = 0);
@@ -5219,6 +5227,7 @@ again:
 		 * not recognize the name entry, then it is quite possible
 		 * that the name entry is corrupted. */
 		if ((lfsck_object_type(obj) & S_IFMT) != lnr->lnr_type) {
+			ns->ln_flags |= LF_INCONSISTENT;
 			type = LNIT_BAD_DIRENT;
 
 			GOTO(stop, rc = 0);
@@ -5241,7 +5250,6 @@ again:
 			type = LNIT_BAD_TYPE;
 
 		count = 1;
-		ns->ln_flags |= LF_INCONSISTENT;
 		/* The magic crashed, we are not sure whether there are more
 		 * corrupt data in the linkea, so remove all linkea entries. */
 		remove = true;
@@ -5252,20 +5260,46 @@ again:
 			type = LNIT_BAD_TYPE;
 
 		count = 1;
-		ns->ln_flags |= LF_UPGRADE;
 		remove = false;
 		newdata = true;
 
 nodata:
 		if (bk->lb_param & LPF_DRYRUN) {
+			if (rc == -ENODATA)
+				ns->ln_flags |= LF_UPGRADE;
+			else
+				ns->ln_flags |= LF_INCONSISTENT;
 			ns->ln_linkea_repaired++;
 			repaired = true;
 			log = true;
 			goto stop;
 		}
 
-		if (!lustre_handle_is_used(&lh))
+		if (!lustre_handle_is_used(&lh)) {
+			remove = false;
+			newdata = false;
+			type = LNIT_NONE;
+
 			goto again;
+		}
+
+		if (dir == NULL) {
+			dir = lfsck_assistant_object_load(env, lfsck, lso);
+			if (IS_ERR(dir)) {
+				rc = PTR_ERR(dir);
+
+				GOTO(stop, rc == -ENOENT ? 0 : rc);
+			}
+		}
+
+		rc = lfsck_namespace_check_exist(env, dir, obj, lnr->lnr_name);
+		if (rc != 0)
+			GOTO(stop, rc);
+
+		if (!remove && newdata)
+			ns->ln_flags |= LF_UPGRADE;
+		else if (remove || !(ns->ln_flags & LF_UPGRADE))
+			ns->ln_flags |= LF_INCONSISTENT;
 
 		if (remove) {
 			LASSERT(newdata);
@@ -5345,6 +5379,15 @@ out:
 		ns->ln_flags |= LF_INCONSISTENT;
 
 		log = false;
+		if (dir == NULL) {
+			dir = lfsck_assistant_object_load(env, lfsck, lso);
+			if (IS_ERR(dir)) {
+				rc = PTR_ERR(dir);
+
+				GOTO(trace, rc == -ENOENT ? 0 : rc);
+			}
+		}
+
 		rc = lfsck_namespace_repair_bad_name_hash(env, com, dir,
 						lnr->lnr_lmv, lnr->lnr_name);
 		if (rc >= 0)
@@ -5352,6 +5395,15 @@ out:
 	}
 
 	if (rc >= 0) {
+		if (type != LNIT_NONE && dir == NULL) {
+			dir = lfsck_assistant_object_load(env, lfsck, lso);
+			if (IS_ERR(dir)) {
+				rc = PTR_ERR(dir);
+
+				GOTO(trace, rc == -ENOENT ? 0 : rc);
+			}
+		}
+
 		switch (type) {
 		case LNIT_BAD_TYPE:
 			log = false;
@@ -5381,12 +5433,12 @@ out:
 			dt_attr_get(env, obj, la);
 	}
 
+trace:
 	down_write(&com->lc_sem);
 	if (rc < 0) {
 		CDEBUG(D_LFSCK, "%s: namespace LFSCK assistant fail to handle "
 		       "the entry: "DFID", parent "DFID", name %.*s: rc = %d\n",
-		       lfsck_lfsck2name(lfsck), PFID(&lnr->lnr_fid),
-		       PFID(lfsck_dto2fid(dir)),
+		       lfsck_lfsck2name(lfsck), PFID(&lnr->lnr_fid), PFID(pfid),
 		       lnr->lnr_namelen, lnr->lnr_name, rc);
 
 		lfsck_namespace_record_failure(env, lfsck, ns);
@@ -5403,8 +5455,7 @@ out:
 			CDEBUG(D_LFSCK, "%s: namespace LFSCK assistant "
 			       "repaired the entry: "DFID", parent "DFID
 			       ", name %.*s\n", lfsck_lfsck2name(lfsck),
-			       PFID(&lnr->lnr_fid),
-			       PFID(lfsck_dto2fid(dir)),
+			       PFID(&lnr->lnr_fid), PFID(pfid),
 			       lnr->lnr_namelen, lnr->lnr_name);
 
 		if (repaired) {
@@ -5456,8 +5507,8 @@ out:
 	if (obj != NULL && !IS_ERR(obj))
 		lfsck_object_put(env, obj);
 
-put_dir:
-	lfsck_object_put(env, dir);
+	if (dir != NULL && !IS_ERR(dir))
+		lfsck_object_put(env, dir);
 
 	return rc;
 }
@@ -5812,6 +5863,7 @@ static int lfsck_namespace_rescan_striped_dir(const struct lu_env *env,
 			(struct lu_dirent *)info->lti_key;
 	struct lfsck_bookmark		*bk	= &lfsck->li_bookmark_ram;
 	struct ptlrpc_thread		*thread = &lfsck->li_thread;
+	struct lfsck_assistant_object	*lso	= NULL;
 	struct lfsck_namespace_req	*lnr;
 	struct lfsck_assistant_req	*lar;
 	int				 rc;
@@ -5846,7 +5898,20 @@ static int lfsck_namespace_rescan_striped_dir(const struct lu_env *env,
 		if (name_is_dot_or_dotdot(ent->lde_name, ent->lde_namelen))
 			goto next;
 
-		lnr = lfsck_namespace_assistant_req_init(lfsck, ent, type);
+		if (lso == NULL) {
+			lso = lfsck_assistant_object_init(env,
+				lfsck_dto2fid(dir), NULL,
+				lfsck->li_pos_current.lp_oit_cookie, true);
+			if (IS_ERR(lso)) {
+				if (bk->lb_param & LPF_FAILOUT)
+					GOTO(out, rc = PTR_ERR(lso));
+
+				lso = NULL;
+				goto next;
+			}
+		}
+
+		lnr = lfsck_namespace_assistant_req_init(lfsck, lso, ent, type);
 		if (IS_ERR(lnr)) {
 			if (bk->lb_param & LPF_FAILOUT)
 				GOTO(out, rc = PTR_ERR(lnr));
@@ -5868,6 +5933,9 @@ next:
 	} while (rc == 0);
 
 out:
+	if (lso != NULL && !IS_ERR(lso))
+		lfsck_assistant_object_put(env, lso);
+
 	lfsck_close_dir(env, lfsck, rc);
 	if (rc <= 0)
 		RETURN(rc);
@@ -6078,9 +6146,9 @@ static void lfsck_namespace_assistant_fill_pos(const struct lu_env *env,
 	lnr = list_entry(lad->lad_req_list.next,
 			 struct lfsck_namespace_req,
 			 lnr_lar.lar_list);
-	pos->lp_oit_cookie = lnr->lnr_oit_cookie;
+	pos->lp_oit_cookie = lnr->lnr_lar.lar_parent->lso_oit_cookie;
 	pos->lp_dir_cookie = lnr->lnr_dir_cookie - 1;
-	pos->lp_dir_parent = lnr->lnr_lar.lar_fid;
+	pos->lp_dir_parent = lnr->lnr_lar.lar_parent->lso_fid;
 }
 
 static int lfsck_namespace_double_scan_result(const struct lu_env *env,

lustre/lfsck/lfsck_engine.c
@@ -608,13 +608,14 @@ out:
 
 static int lfsck_exec_dir(const struct lu_env *env,
 			  struct lfsck_instance *lfsck,
+			  struct lfsck_assistant_object *lso,
 			  struct lu_dirent *ent, __u16 type)
 {
 	struct lfsck_component *com;
 	int			rc;
 
 	list_for_each_entry(com, &lfsck->li_list_scan, lc_link) {
-		rc = com->lc_ops->lfsck_exec_dir(env, com, ent, type);
+		rc = com->lc_ops->lfsck_exec_dir(env, com, lso, ent, type);
 		if (rc != 0)
 			return rc;
 	}
@@ -743,6 +744,7 @@ static int lfsck_master_dir_engine(const struct lu_env *env,
 			(struct lu_dirent *)info->lti_key;
 	struct lfsck_bookmark		*bk	= &lfsck->li_bookmark_ram;
 	struct ptlrpc_thread		*thread = &lfsck->li_thread;
+	struct lfsck_assistant_object	*lso	= NULL;
 	int				 rc;
 	__u16				 type;
 	ENTRY;
@@ -755,7 +757,7 @@ static int lfsck_master_dir_engine(const struct lu_env *env,
 			       lfsck_lfsck2name(lfsck),
 			       PFID(lfsck_dto2fid(dir)), lfsck->li_cookie_dir);
 
-			RETURN(0);
+			GOTO(out, rc = 0);
 		}
 
 		lfsck->li_new_scanned++;
@@ -773,7 +775,7 @@ static int lfsck_master_dir_engine(const struct lu_env *env,
 			       lfsck->li_cookie_dir, rc);
 			lfsck_fail(env, lfsck, true);
 			if (bk->lb_param & LPF_FAILOUT)
-				RETURN(rc);
+				GOTO(out, rc);
 			else
 				goto checkpoint;
 		}
@@ -786,16 +788,29 @@ static int lfsck_master_dir_engine(const struct lu_env *env,
 		if (ent->lde_namelen == 1 && ent->lde_name[0] == '.')
 			goto checkpoint;
 
+		if (lso == NULL) {
+			lso = lfsck_assistant_object_init(env,
+				lfsck_dto2fid(dir), NULL,
+				lfsck->li_pos_current.lp_oit_cookie, true);
+			if (IS_ERR(lso)) {
+				if (bk->lb_param & LPF_FAILOUT)
+					RETURN(PTR_ERR(lso));
+
+				lso = NULL;
+				goto checkpoint;
+			}
+		}
+
 		/* The type in the @ent structure may has been overwritten,
 		 * so we need to pass the @type parameter independently. */
-		rc = lfsck_exec_dir(env, lfsck, ent, type);
+		rc = lfsck_exec_dir(env, lfsck, lso, ent, type);
 		if (rc != 0 && bk->lb_param & LPF_FAILOUT)
-			RETURN(rc);
+			GOTO(out, rc);
 
 checkpoint:
 		rc = lfsck_checkpoint(env, lfsck);
 		if (rc != 0 && bk->lb_param & LPF_FAILOUT)
-			RETURN(rc);
+			GOTO(out, rc);
 
 		/* Rate control. */
 		lfsck_control_speed(lfsck);
@@ -805,14 +820,14 @@ checkpoint:
 			       lfsck_lfsck2name(lfsck),
 			       PFID(lfsck_dto2fid(dir)),
 			       lfsck->li_cookie_dir);
-			RETURN(0);
+			GOTO(out, rc = 0);
 		}
 
 		if (OBD_FAIL_CHECK(OBD_FAIL_LFSCK_FATAL2)) {
 			spin_lock(&lfsck->li_lock);
 			thread_set_flags(thread, SVC_STOPPING);
 			spin_unlock(&lfsck->li_lock);
-			RETURN(-EINVAL);
+			GOTO(out, rc = -EINVAL);
 		}
 
 		rc = iops->next(env, di);
@@ -821,7 +836,13 @@ checkpoint:
 	if (rc > 0 && !lfsck->li_oit_over)
 		lfsck_close_dir(env, lfsck, rc);
 
-	RETURN(rc);
+	GOTO(out, rc);
+
+out:
+	if (lso != NULL)
+		lfsck_assistant_object_put(env, lso);
+
+	return rc;
 }
 
 /**

lustre/lfsck/lfsck_internal.h
@@ -360,6 +360,15 @@ struct lfsck_layout {
 	__u8	ll_ost_bitmap[0];
 };
 
+struct lfsck_assistant_object {
+	struct lu_fid		lso_fid;
+	__u64			lso_oit_cookie;
+	struct lu_attr		lso_attr;
+	atomic_t		lso_ref;
+	unsigned int		lso_dead:1,
+				lso_is_dir:1;
+};
+
 struct lfsck_component;
 struct lfsck_tgt_descs;
 struct lfsck_tgt_desc;
@@ -393,6 +402,7 @@ struct lfsck_operations {
 
 	int (*lfsck_exec_dir)(const struct lu_env *env,
 			      struct lfsck_component *com,
+			      struct lfsck_assistant_object *lso,
 			      struct lu_dirent *ent,
 			      __u16 type);
 
@@ -718,21 +728,27 @@ struct lfsck_thread_args {
 };
 
 struct lfsck_assistant_req {
-	struct list_head	lar_list;
-	struct lu_fid		lar_fid;
+	struct list_head		 lar_list;
+	struct lfsck_assistant_object	*lar_parent;
 };
 
 struct lfsck_namespace_req {
 	struct lfsck_assistant_req	 lnr_lar;
 	struct lfsck_lmv		*lnr_lmv;
 	struct lu_fid			 lnr_fid;
-	__u64				 lnr_oit_cookie;
 	__u64				 lnr_dir_cookie;
 	__u32				 lnr_attr;
 	__u32				 lnr_size;
 	__u16				 lnr_type;
 	__u16				 lnr_namelen;
 	char				 lnr_name[0];
+};
+
+struct lfsck_layout_req {
+	struct lfsck_assistant_req	 llr_lar;
+	struct dt_object		*llr_child;
+	__u32				 llr_ost_idx;
+	__u32				 llr_lov_idx; /* offset in LOV EA */
 };
 
 struct lfsck_assistant_operations {
@@ -821,7 +837,6 @@ struct lfsck_thread_info {
 	struct lu_fid		lti_fid3;
 	struct lu_attr		lti_la;
 	struct lu_attr		lti_la2;
-	struct lu_attr		lti_la3;
 	struct ost_id		lti_oi;
 	union {
 		struct lustre_mdt_attrs lti_lma;
@@ -896,6 +911,14 @@ void lfsck_thread_args_fini(struct lfsck_thread_args *lta);
 struct lfsck_assistant_data *
 lfsck_assistant_data_init(struct lfsck_assistant_operations *lao,
 			  const char *name);
+struct lfsck_assistant_object *
+lfsck_assistant_object_init(const struct lu_env *env, const struct lu_fid *fid,
+			    const struct lu_attr *attr, __u64 cookie,
+			    bool is_dir);
+struct dt_object *
+lfsck_assistant_object_load(const struct lu_env *env,
+			    struct lfsck_instance *lfsck,
+			    struct lfsck_assistant_object *lso);
 int lfsck_async_interpret_common(const struct lu_env *env,
 				 struct ptlrpc_request *req,
 				 void *args, int rc);
@@ -1005,11 +1028,9 @@ int lfsck_namespace_repair_bad_name_hash(const struct lu_env *env,
 					 const char *name);
 int lfsck_namespace_striped_dir_rescan(const struct lu_env *env,
 				       struct lfsck_component *com,
-				       struct dt_object *dir,
 				       struct lfsck_namespace_req *lnr);
 int lfsck_namespace_handle_striped_master(const struct lu_env *env,
 					  struct lfsck_component *com,
-					  struct dt_object *dir,
 					  struct lfsck_namespace_req *lnr);
 
 /* lfsck_layout.c */
@@ -1425,5 +1446,21 @@ static inline void lfsck_lmv_header_cpu_to_le(struct lmv_mds_md_v1 *dst,
 	dst->lmv_master_mdt_index = cpu_to_le32(src->lmv_master_mdt_index);
 	dst->lmv_hash_type = cpu_to_le32(src->lmv_hash_type);
 	dst->lmv_layout_version = cpu_to_le32(src->lmv_layout_version);
+}
+
+static inline struct lfsck_assistant_object *
+lfsck_assistant_object_get(struct lfsck_assistant_object *lso)
+{
+	atomic_inc(&lso->lso_ref);
+
+	return lso;
+}
+
+static inline void
+lfsck_assistant_object_put(const struct lu_env *env,
+			   struct lfsck_assistant_object *lso)
+{
+	if (atomic_dec_and_test(&lso->lso_ref))
+		OBD_FREE_PTR(lso);
 }
 #endif /* _LFSCK_INTERNAL_H */

commit 0c0df54a6504927cfce4422dc12d8c489062f452

LU-17000 lfsck: do not fail a scan for a vanished directory
GOTO() does not assign its second argument, so the ternary in
lfsck_namespace_assistant_handler_p1() was discarded and the target
label still saw the -ENOENT that lfsck_assistant_object_load() returns
when the object is already dead. A directory unlinked while the
namespace LFSCK was running was therefore logged and counted as an
LFSCK failure, and with LPF_FAILOUT it aborted the scan.

One of the four sites jumps to stop:, which falls through to out:.
The reload there is guarded by dir == NULL, so the failed load left an
ERR_PTR in dir that could reach lfsck_namespace_repair_bad_name_hash()
and be dereferenced. Clear dir on that path as well.

Assign the result, spelled as the three working instances of the same
idiom already in this file.

Test-Parameters: trivial testlist=sanity-lfsck
Fixes: 5b3eef20c5a0 ("LU-6343 lfsck: locate object only when necessary")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I32953fd4a110d2c0147a4f8cb2962d5337db8d7a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/68281
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/lfsck/lfsck_namespace.c
@@ -5722,7 +5722,7 @@ dangling:
 			if (IS_ERR(dir)) {
 				rc = PTR_ERR(dir);
 
-				GOTO(trace, rc == -ENOENT ? 0 : rc);
+				GOTO(trace, rc = (rc == -ENOENT ? 0 : rc));
 			}
 		}
 
@@ -5893,8 +5893,12 @@ nodata:
 			dir = lfsck_assistant_object_load(env, lfsck, lso);
 			if (IS_ERR(dir)) {
 				rc = PTR_ERR(dir);
+				/* out: is reached from stop: and only
+				 * reloads dir when it is NULL
+				 */
+				dir = NULL;
 
-				GOTO(stop, rc == -ENOENT ? 0 : rc);
+				GOTO(stop, rc = (rc == -ENOENT ? 0 : rc));
 			}
 		}
 
@@ -5962,7 +5966,7 @@ out:
 			if (IS_ERR(dir)) {
 				rc = PTR_ERR(dir);
 
-				GOTO(trace, rc == -ENOENT ? 0 : rc);
+				GOTO(trace, rc = (rc == -ENOENT ? 0 : rc));
 			}
 		}
 
@@ -5978,7 +5982,7 @@ out:
 			if (IS_ERR(dir)) {
 				rc = PTR_ERR(dir);
 
-				GOTO(trace, rc == -ENOENT ? 0 : rc);
+				GOTO(trace, rc = (rc == -ENOENT ? 0 : rc));
 			}
 		}
 