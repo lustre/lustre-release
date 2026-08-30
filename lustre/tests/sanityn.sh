lustre/lustre-release

commit 234ce16dba60f2e2c2177e5cde21efd75285e4b4

LU-1943 zfs-osd: different fixes for zfs
- echo_create_md_object() to mark la_mode is valid

- ofd_statfs() to set avail/free blocks to 2 when the counters
  are converted to 4K units. otherwise with zfs 2 blocks of size
  128K can be > 0.01% leading to failures in sanity/27*

- zfs-osd to initialize LMA

- zfs-osd's osd_attr_get() to calculate size of directory from
  blocks and do not use cached size (as we don't control it as
  with regular files)

- zfs-osd to track enabled/disabled status for SA and follow it

- take from orion additional locking in osd_object_sa_dirty_add()
  and osd_object_sa_dirty_rele()

- lu_cache_shrink() do not purge objects if __GFP_FS is not set
  this should prevent deadlock when used with zfs backend

- ost_checksum_bulk() to allocate new page to simulate corruption,
  otherwise it corrupts zfs's internal buffers which do not rely
  on PageUptoDate flag

- t-f to modprobe zfs module if osd-zfs is in use

Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Change-Id: I027483162735cd0fb4be9fa8bd12c2f2dc43b44a
Reviewed-on: http://review.whamcloud.com/4141
Tested-by: Hudson
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Mike Pershin <tappro@whamcloud.com>

lustre/osd-zfs/osd_object.c
@@ -125,8 +125,10 @@ osd_object_sa_dirty_add(struct osd_object *obj, struct osd_thandle *oh)
 		return;
 
 	cfs_down(&oh->ot_sa_lock);
+	cfs_write_lock(&obj->oo_attr_lock);
 	if (likely(cfs_list_empty(&obj->oo_sa_linkage)))
 		cfs_list_add(&obj->oo_sa_linkage, &oh->ot_sa_list);
+	cfs_write_unlock(&obj->oo_attr_lock);
 	cfs_up(&oh->ot_sa_lock);
 }
 
@@ -142,7 +144,9 @@ void osd_object_sa_dirty_rele(struct osd_thandle *oh)
 		obj = cfs_list_entry(oh->ot_sa_list.next,
 				     struct osd_object, oo_sa_linkage);
 		sa_spill_rele(obj->oo_sa_hdl);
+		cfs_write_lock(&obj->oo_attr_lock);
 		cfs_list_del_init(&obj->oo_sa_linkage);
+		cfs_write_unlock(&obj->oo_attr_lock);
 	}
 	cfs_up(&oh->ot_sa_lock);
 }
@@ -731,6 +735,10 @@ static int osd_attr_get(const struct lu_env *env,
 	 * from within sa_object_size() can block on a mutex, so
 	 * we can't call sa_object_size() holding rwlock */
 	sa_object_size(obj->oo_sa_hdl, &blksize, &blocks);
+	/* we do not control size of indices, so always calculate
+	 * it from number of blocks reported by DMU */
+	if (S_ISDIR(attr->la_mode))
+		attr->la_size = 512 * blocks;
 	/* Block size may be not set; suggest maximal I/O transfers. */
 	if (blksize == 0)
 		blksize = 1ULL << SPA_MAXBLOCKSHIFT;
@@ -1079,6 +1087,9 @@ static int osd_declare_object_create(const struct lu_env *env,
 
 	dmu_tx_hold_sa_create(oh->ot_tx, ZFS_SA_BASE_ATTR_SIZE);
 
+	__osd_xattr_declare_set(env, obj, sizeof(struct lustre_mdt_attrs),
+				XATTR_NAME_LMA, oh);
+
 	RETURN(osd_declare_quota(env, osd, attr->la_uid, attr->la_gid, 1, oh,
 				 false, NULL, false));
 }
@@ -1361,6 +1372,24 @@ static osd_obj_type_f osd_create_type_f(enum dt_format_type type)
 /*
  * Primitives for directory (i.e. ZAP) handling
  */
+static inline int osd_init_lma(const struct lu_env *env, struct osd_object *obj,
+			       const struct lu_fid *fid, struct osd_thandle *oh)
+{
+	struct osd_thread_info	*info = osd_oti_get(env);
+	struct lustre_mdt_attrs	*lma = &info->oti_mdt_attrs;
+	struct lu_buf		 buf;
+	int rc;
+
+	lustre_lma_init(lma, fid);
+	lustre_lma_swab(lma);
+	buf.lb_buf = lma;
+	buf.lb_len = sizeof(*lma);
+
+	rc = osd_xattr_set_internal(env, obj, &buf, XATTR_NAME_LMA,
+				    LU_XATTR_CREATE, oh, BYPASS_CAPA);
+
+	return rc;
+}
 
 /*
  * Concurrency: @dt is write locked.
@@ -1435,6 +1464,14 @@ static int osd_object_create(const struct lu_env *env, struct dt_object *dt,
 	rc = osd_object_init0(env, obj);
 	LASSERT(ergo(rc == 0, dt_object_exists(dt)));
 	LASSERT(osd_invariant(obj));
+
+	rc = osd_init_lma(env, obj, fid, oh);
+	if (rc) {
+		CERROR("%s: can not set LMA on "DFID": rc = %d\n",
+		       osd->od_svname, PFID(fid), rc);
+		/* ignore errors during LMA initialization */
+		rc = 0;
+	}
 
 out:
 	cfs_up(&obj->oo_guard);

lustre/tests/conf-sanity.sh
@@ -19,6 +19,10 @@ if [ "$FAILURE_MODE" = "HARD" ]; then
 	ALWAYS_EXCEPT="$ALWAYS_EXCEPT $CONFIG_EXCEPTIONS"
 fi
 
+# LU-2059
+ALWAYS_EXCEPT="$ALWAYS_EXCEPT 5d 19b 21b 27a"
+
+
 SRCDIR=`dirname $0`
 PATH=$PWD/$SRCDIR:$SRCDIR:$SRCDIR/../utils:$PATH
 
@@ -558,6 +562,11 @@ is_blkdev () {
 #
 
 test_17() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
         setup
         check_mount || return 41
         cleanup || return $?
@@ -1191,6 +1200,11 @@ cleanup_32() {
 }
 
 test_32a() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	client_only && skip "client only testing" && return 0
 	[ "$NETTYPE" = "tcp" ] || { skip "NETTYPE != tcp" && return 0; }
 	[ -z "$TUNEFS" ] && skip_env "No tunefs" && return 0
@@ -1258,6 +1272,11 @@ test_32a() {
 run_test 32a "Upgrade from 1.8 (not live)"
 
 test_32b() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	client_only && skip "client only testing" && return 0
 	[ "$NETTYPE" = "tcp" ] || { skip "NETTYPE != tcp" && return 0; }
 	[ -z "$TUNEFS" ] && skip_env "No tunefs" && return
@@ -1691,6 +1710,11 @@ test_37() {
 run_test 37 "verify set tunables works for symlink device"
 
 test_38() { # bug 14222
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	setup
 	# like runtests
 	COUNT=10
@@ -2441,6 +2465,11 @@ diff_files_xattrs()
 }
 
 test_52() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	start_mds
 	[ $? -eq 0 ] || { error "Unable to start MDS"; return 1; }
 	start_ost
@@ -2646,6 +2675,11 @@ test_53b() {
 run_test 53b "check MDT thread count params"
 
 test_54a() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
     do_rpc_nodes $(facet_host ost1) run_llverdev $(ostdevname 1) -p
     [ $? -eq 0 ] || error "llverdev failed!"
     reformat_and_config
@@ -2653,6 +2687,11 @@ test_54a() {
 run_test 54a "test llverdev and partial verify of device"
 
 test_54b() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
     setup
     run_llverfs $MOUNT -p
     [ $? -eq 0 ] || error "llverfs failed!"
@@ -2667,6 +2706,11 @@ lov_objid_size()
 }
 
 test_55() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	local mdsdev=$(mdsdevname 1)
 	local mdsvdev=$(mdsvdevname 1)
 
@@ -2742,8 +2786,8 @@ count_osts() {
 }
 
 test_58() { # bug 22658
-	if [ $(facet_fstype mds) == zfs ]; then
-		skip "Does not work with ZFS-based MDTs yet"
+	if [ $(facet_fstype mds) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
 		return
 	fi
 	setup_noconfig
@@ -2890,6 +2934,11 @@ test_61() { # LU-80
 run_test 61 "large xattr"
 
 test_62() {
+	if [ $(facet_fstype $SINGLEMDS) != ldiskfs ]; then
+		skip "Only applicable to ldiskfs-based MDTs"
+		return
+	fi
+
 	# MRP-118
 	local mdsdev=$(mdsdevname 1)
 	local ostdev=$(ostdevname 1)

lustre/obdclass/lu_object.c
@@ -1797,6 +1797,24 @@ static void lu_site_stats_get(cfs_hash_t *hs,
 
 #ifdef __KERNEL__
 
+/*
+ * There exists a potential lock inversion deadlock scenario when using
+ * Lustre on top of ZFS. This occurs between one of ZFS's
+ * buf_hash_table.ht_lock's, and Lustre's lu_sites_guard lock. Essentially,
+ * thread A will take the lu_sites_guard lock and sleep on the ht_lock,
+ * while thread B will take the ht_lock and sleep on the lu_sites_guard
+ * lock. Obviously neither thread will wake and drop their respective hold
+ * on their lock.
+ *
+ * To prevent this from happening we must ensure the lu_sites_guard lock is
+ * not taken while down this code path. ZFS reliably does not set the
+ * __GFP_FS bit in its code paths, so this can be used to determine if it
+ * is safe to take the lu_sites_guard lock.
+ *
+ * Ideally we should accurately return the remaining number of cached
+ * objects without taking the  lu_sites_guard lock, but this is not
+ * possible in the current implementation.
+ */
 static int lu_cache_shrink(SHRINKER_ARGS(sc, nr_to_scan, gfp_mask))
 {
         lu_site_stats_t stats;
@@ -1806,11 +1824,25 @@ static int lu_cache_shrink(SHRINKER_ARGS(sc, nr_to_scan, gfp_mask))
         int remain = shrink_param(sc, nr_to_scan);
         CFS_LIST_HEAD(splice);
 
-        if (remain != 0) {
-                if (!(shrink_param(sc, gfp_mask) & __GFP_FS))
+	if (!(shrink_param(sc, gfp_mask) & __GFP_FS)) {
+		if (remain != 0)
                         return -1;
-                CDEBUG(D_INODE, "Shrink %d objects\n", remain);
+		else
+			/* We must not take the lu_sites_guard lock when
+			 * __GFP_FS is *not* set because of the deadlock
+			 * possibility detailed above. Additionally,
+			 * since we cannot determine the number of
+			 * objects in the cache without taking this
+			 * lock, we're in a particularly tough spot. As
+			 * a result, we'll just lie and say our cache is
+			 * empty. This _should_ be ok, as we can't
+			 * reclaim objects when __GFP_FS is *not* set
+			 * anyways.
+			 */
+			return 0;
         }
+
+	CDEBUG(D_INODE, "Shrink %d objects\n", remain);
 
         cfs_mutex_lock(&lu_sites_guard);
         cfs_list_for_each_entry_safe(s, tmp, &lu_sites, ls_linkage) {

lustre/obdecho/echo_client.c
@@ -1606,7 +1606,7 @@ static int echo_create_md_object(const struct lu_env *env,
         }
 
         ma->ma_attr.la_mode = mode;
-        ma->ma_attr.la_valid = LA_CTIME;
+	ma->ma_attr.la_valid = LA_CTIME | LA_MODE;
         ma->ma_attr.la_ctime = cfs_time_current_64();
 
         if (name != NULL) {

lustre/ofd/ofd_obd.c
@@ -729,10 +729,6 @@ static int ofd_statfs(const struct lu_env *env,  struct obd_export *exp,
 	       osfs->os_blocks, osfs->os_bfree, osfs->os_bavail,
 	       osfs->os_files, osfs->os_ffree, osfs->os_state);
 
-	if (OBD_FAIL_CHECK_VALUE(OBD_FAIL_OST_ENOSPC,
-				 ofd->ofd_lut.lut_lsd.lsd_ost_index))
-		osfs->os_bfree = osfs->os_bavail = 2;
-
 	if (OBD_FAIL_CHECK_VALUE(OBD_FAIL_OST_ENOINO,
 				 ofd->ofd_lut.lut_lsd.lsd_ost_index))
 		osfs->os_ffree = 0;
@@ -752,6 +748,10 @@ static int ofd_statfs(const struct lu_env *env,  struct obd_export *exp,
 		osfs->os_bavail <<= ofd->ofd_blockbits - COMPAT_BSIZE_SHIFT;
 		osfs->os_bsize    = 1 << COMPAT_BSIZE_SHIFT;
 	}
+
+	if (OBD_FAIL_CHECK_VALUE(OBD_FAIL_OST_ENOSPC,
+				 ofd->ofd_lut.lut_lsd.lsd_ost_index))
+		osfs->os_bfree = osfs->os_bavail = 2;
 
 	EXIT;
 out:

lustre/osd-zfs/osd_handler.c
@@ -505,9 +505,17 @@ static int osd_shutdown(const struct lu_env *env, struct osd_device *o)
 	RETURN(0);
 }
 
+static void osd_xattr_changed_cb(void *arg, uint64_t newval)
+{
+	struct osd_device *osd = arg;
+
+	osd->od_xattr_in_sa = (newval == ZFS_XATTR_SA);
+}
+
 static int osd_mount(const struct lu_env *env,
 		     struct osd_device *o, struct lustre_cfg *cfg)
 {
+	struct dsl_dataset *ds;
 	char	  *dev  = lustre_cfg_string(cfg, 1);
 	dmu_buf_t *rootdb;
 	int	   rc;
@@ -528,6 +536,13 @@ static int osd_mount(const struct lu_env *env,
 		CERROR("can't open objset %s: %d\n", o->od_mntdev, rc);
 		RETURN(rc);
 	}
+
+	ds = dmu_objset_ds(o->od_objset.os);
+	LASSERT(ds);
+	rc = dsl_prop_register(ds, "xattr", osd_xattr_changed_cb, o);
+	if (rc)
+		CERROR("%s: cat not register xattr callback, ignore: %d\n",
+		       o->od_svname, rc);
 
 	rc = __osd_obj2dbuf(env, o->od_objset.os, o->od_objset.root,
 				&rootdb, root_tag);
@@ -668,6 +683,7 @@ static struct lu_device *osd_device_fini(const struct lu_env *env,
 					 struct lu_device *d)
 {
 	struct osd_device *o = osd_dev(d);
+	struct dsl_dataset *ds;
 	int		   rc;
 	ENTRY;
 
@@ -675,6 +691,11 @@ static struct lu_device *osd_device_fini(const struct lu_env *env,
 	osd_oi_fini(env, o);
 
 	if (o->od_objset.os) {
+		ds = dmu_objset_ds(o->od_objset.os);
+		rc = dsl_prop_unregister(ds, "xattr", osd_xattr_changed_cb, o);
+		if (rc)
+			CERROR("%s: dsl_prop_unregister xattr error %d\n",
+				o->od_svname, rc);
 		arc_remove_prune_callback(o->arc_prune_cb);
 		o->arc_prune_cb = NULL;
 		osd_sync(env, lu2dt_dev(d));

lustre/osd-zfs/osd_internal.h
@@ -147,6 +147,7 @@ struct osd_thread_info {
 
 	char			 oti_str[64];
 	char			 oti_key[MAXNAMELEN + 1];
+	struct lustre_mdt_attrs oti_mdt_attrs;
 
 	struct lu_attr		 oti_la;
 	struct osa_attr		 oti_osa;
@@ -215,6 +216,7 @@ struct osd_device {
 	uint64_t		 od_ost_compat_grp0;
 
 	unsigned int		 od_rdonly:1,
+				 od_xattr_in_sa:1,
 				 od_quota_iused_est:1;
 	char			 od_mntdev[128];
 	char			 od_svname[128];
@@ -409,6 +411,31 @@ int osd_xattr_del(const struct lu_env *env, struct dt_object *dt,
 		  struct lustre_capa *capa);
 int osd_xattr_list(const struct lu_env *env, struct dt_object *dt,
 		   struct lu_buf *lb, struct lustre_capa *capa);
+void __osd_xattr_declare_set(const struct lu_env *env, struct osd_object *obj,
+			int vallen, const char *name, struct osd_thandle *oh);
+int __osd_sa_xattr_set(const struct lu_env *env, struct osd_object *obj,
+		       const struct lu_buf *buf, const char *name, int fl,
+		       struct osd_thandle *oh);;
+int __osd_xattr_set(const struct lu_env *env, struct osd_object *obj,
+		    const struct lu_buf *buf, const char *name, int fl,
+		    struct osd_thandle *oh);
+static inline int
+osd_xattr_set_internal(const struct lu_env *env, struct osd_object *obj,
+		       const struct lu_buf *buf, const char *name, int fl,
+		       struct osd_thandle *oh, struct lustre_capa *capa)
+{
+	int rc;
+
+	if (osd_obj2dev(obj)->od_xattr_in_sa) {
+		rc = __osd_sa_xattr_set(env, obj, buf, name, fl, oh);
+		if (rc == -EFBIG)
+			rc = __osd_xattr_set(env, obj, buf, name, fl, oh);
+	} else {
+		rc = __osd_xattr_set(env, obj, buf, name, fl, oh);
+	}
+
+	return rc;
+}
 
 #endif
 #endif /* _OSD_INTERNAL_H */

lustre/osd-zfs/osd_xattr.c
@@ -435,7 +435,7 @@ int __osd_sa_xattr_set(const struct lu_env *env, struct osd_object *obj,
 	return rc;
 }
 
-static int
+int
 __osd_xattr_set(const struct lu_env *env, struct osd_object *obj,
 		const struct lu_buf *buf, const char *name, int fl,
 		struct osd_thandle *oh)
@@ -549,8 +549,8 @@ out:
 }
 
 int osd_xattr_set(const struct lu_env *env, struct dt_object *dt,
-		const struct lu_buf *buf, const char *name, int fl,
-		struct thandle *handle, struct lustre_capa *capa)
+		  const struct lu_buf *buf, const char *name, int fl,
+		  struct thandle *handle, struct lustre_capa *capa)
 {
 	struct osd_object  *obj = osd_dt_obj(dt);
 	struct osd_thandle *oh;
@@ -567,10 +567,7 @@ int osd_xattr_set(const struct lu_env *env, struct dt_object *dt,
 	cfs_down(&obj->oo_guard);
 	CDEBUG(D_INODE, "Setting xattr %s with size %d\n",
 		name, (int)buf->lb_len);
-	rc = __osd_sa_xattr_set(env, obj, buf, name, fl, oh);
-	/* place xattr in dnode if SA is full */
-	if (rc == -EFBIG)
-		rc = __osd_xattr_set(env, obj, buf, name, fl, oh);
+	rc = osd_xattr_set_internal(env, obj, buf, name, fl, oh, capa);
 	cfs_up(&obj->oo_guard);
 
 	RETURN(rc);

commit 1be49a3c5d5c6169271370225ec794726ab4b88b

LU-15842 osd-zfs: report a logical size for directories
sanityn test_102 fails intermittently on ZFS, comparing a directory's
stat data taken through two mounts of the same filesystem.
check_fhandle_syscalls.c compares st_dev, st_ino, st_mode, st_mtime and
st_size, and names the field that differed:

  st_size mismatch: open <sampled before the sync>,
                    open_by_handle_at <sampled after it>

The reported failure is on d102.sanityn.remote/subdir, which sanityn
creates with "lfs mkdir -c -1"; the sizes seen for each directory shape
are in the table below.

osd_attr_get() discards a directory's logical size and substitutes DMU
space accounting instead:

	sa_object_size(obj->oo_sa_hdl, &blksize, &blocks);
	if (S_ISDIR(attr->la_mode))
		attr->la_size = 512 * blocks;

sa_object_size() reads dn_phys, the on-disk dnode, whose dn_used is
updated only from syncing context.  A directory's reported size
therefore changes when the txg syncs, and a txg sync performs no
metadata operation:
no MDS_INODELOCK_UPDATE is revoked, so no client cache is invalidated.
A client that sampled before the sync keeps serving the old value under
a lock that is still perfectly valid, while a second mount that samples
afterwards gets a different, equally valid value.  Nothing the client
can do closes that gap, because from the DLM's point of view nothing
changed.

Report the ZAP's logical length instead.  dn_maxblkid is advanced in
open context, in the same tx that adds the dirent, so it moves only
under a lock the client has to reacquire.  la_blocks is left as physical
accounting, which is what st_blocks is meant to be everywhere else.
osd-ldiskfs already reports a logical i_size for directories, which is
why this has always been a ZFS-only failure.

That covers a directory that grows, which is what the reported failure
is.  It is not symmetric: zap_shrink() frees an emptied tail leaf and
dnode_sync_free_range_impl() lowers dn_phys->dn_maxblkid only, from
syncing context, while the in-core dn_maxblkid is never lowered
(dnode_new_blkid() returns early for a smaller blkid).  A directory that
shrinks therefore keeps the larger size until its dnode is evicted and
reloaded, and reports the smaller one afterwards.  Creating and then
unlinking 20000 entries gave 2564096 before and 2465792 after exporting
and importing the pool.  Squeezing the ARC with the filesystem still
mounted did not move it, but that is not a bound on when it can surface:
the dnode stays pinned only while the osd_object is cached, and
lu_site_purge() drops idle objects under memory pressure.  No
client-visible disagreement was produced either way.  Making the size
stable in both directions means maintaining it in the SA as osd-ldiskfs
does, which is a larger change than this one.

Measured on a 4-MDT ZFS DNE cluster with a probe that creates a
directory and stats it through two mounts either side of a "zpool sync"
that touches nothing.  The last column is the pair of sizes the two
mounts reported without the patch:

                        before          after   sizes seen before
  mkdir                 20/20 mismatch  0/20    512 -> 7680
  lfs mkdir -i 1        20/20 mismatch  0/20    1024 -> 7168
  lfs mkdir -c -1       20/20 mismatch  0/20    22528 -> 28672

Over 120 runs of sanityn test_102 on the same clusters, classified by
failure signature, the mismatch appears 10 times without this patch and
0 times with it.

With the patch a directory reports 16384 per stripe and keeps it: 16384
for a plain or remote one, and 65536 for "lfs mkdir -c -1" across four
MDTs, since the client sums the stripes in lmv_merge_attr().  Exporting
and importing all eight pools, which evicts and reloads every dnode from
disk, leaves 30 directories reporting identical sizes.

(dn_maxblkid + 1) * dn_datablksz is exactly what
dmu_object_info_from_dnode() computes for doi_max_offset, in 2.0.0 and
2.4.0.  That helper is deliberately not used: it takes dn_struct_rwlock and dn_mtx and sums dn_nblkptr block pointers for doi_fill_count, so
calling it on every directory getattr serialises operations on a hot
directory through an exclusive mutex.  Both fields are already read
without those locks elsewhere in osd-zfs, and a directory's la_size is
only ever reported by stat(), never used to size a write.

sanityn test_102 only catches this when the two stats happen to straddle
a txg sync, so add sanityn test_122, which stats a plain, a remote and a
striped directory through both mounts either side of a deliberate sync.

While here, only count the directory entries when the caller asks for
them.  osd_attr_get() called zap_count() for every directory getattr,
but la_dirent_count feeds the automatic directory split, which is off by
default, so the work was usually wasted.  osd-ldiskfs already gates it
on LA_DIRENT_CNT; do the same here.  The request has to be latched
before *attr = obj->oo_attr, which overwrites la_valid - osd-ldiskfs is
not exposed to that because osd_inode_getattr() ORs its bits in instead.

conf-sanity's t32_test() verifies an upgraded image by diffing an
"ls -Rni" listing against one recorded when the image was made, and that
listing includes the directory size, so it fails on every ZFS image once
the reported size changes.  The helper is shared by test_32a through
test_32j, so all ten variants are affected.  Nothing else in the listing
moves: of 48 differing lines, 48 are directories and the inodes, modes,
owners, timestamps and file sizes all still match.  Normalise that one
column out of both sides before the comparison, as the test already does
for inode numbers when the image came from an architecture with a
different long.

This does not make st_blocks coherent: it is physical accounting and
still settles at txg sync, as it does on any filesystem.  It is
deliberately not added to check_fhandle_syscalls.

Fixes: 234ce16dba60 ("LU-1943 zfs-osd: different fixes for zfs")

lustre/osd-zfs/osd_object.c
@@ -937,6 +937,10 @@ static int osd_attr_get(const struct lu_env *env, struct dt_object *dt,
 	uint64_t blocks;
 	uint32_t blksize;
 	int rc = 0;
+	/* the copy from oo_attr below overwrites la_valid, so the caller's
+	 * request for the dirent count has to be taken before that
+	 */
+	bool dirent_cnt = attr->la_valid & LA_DIRENT_CNT;
 
 	down_read(&obj->oo_guard);
 
@@ -971,13 +975,27 @@ static int osd_attr_get(const struct lu_env *env, struct dt_object *dt,
 	 * we can't call sa_object_size() holding rwlock
 	 */
 	sa_object_size(obj->oo_sa_hdl, &blksize, &blocks);
-	/* we do not control size of indices, so always calculate
-	 * it from number of blocks reported by DMU
-	 */
 	if (S_ISDIR(attr->la_mode)) {
-		attr->la_size = 512 * blocks;
-		rc = -zap_count(osd->od_os, obj->oo_dn->dn_object,
-				&attr->la_dirent_count);
+		dnode_t *dn = obj->oo_dn;
+
+		/* We keep no logical size for a directory ZAP, so report its
+		 * logical length.  Do not derive it from the DMU block count:
+		 * that comes from the on-disk dnode and only settles when the
+		 * txg syncs, which revokes no DLM lock, so two clients end up
+		 * disagreeing about the size of an idle directory (LU-15842).
+		 * This is what dmu_object_info_from_dnode() computes for
+		 * doi_max_offset, read directly because that helper also takes
+		 * dn_mtx and sums the block pointers on every getattr.
+		 */
+		attr->la_size = (dn->dn_maxblkid + 1) * dn->dn_datablksz;
+		if (dirent_cnt) {
+			rc = -zap_count(osd->od_os, dn->dn_object,
+					&attr->la_dirent_count);
+			if (!rc)
+				attr->la_valid |= LA_DIRENT_CNT;
+		} else {
+			attr->la_dirent_count = LU_DIRENT_COUNT_UNSET;
+		}
 	}
 	/* Block size may be not set; suggest maximal I/O transfers. */
 	if (blksize == 0)

lustre/tests/conf-sanity.sh
@@ -1992,6 +1992,17 @@ t32_verify_quota() {
 	return 0
 }
 
+# osd-zfs reports a directory's size from the ZAP's logical extent, which the
+# size recorded in the image predates (LU-15842), so drop that column before
+# comparing.  t32_check() only restores images matching mds1_FSTYPE, so an
+# ldiskfs run compares ldiskfs against ldiskfs and keeps checking it.  Rebuild
+# every record so the two sides stay comparable if ls repads the size field.
+t32_normalize_list() {
+	[[ $mds1_FSTYPE == zfs ]] || { cat; return; }
+
+	awk '{ if ($2 ~ /^d/) $6 = "DIRSIZE"; $1 = $1; print }'
+}
+
 get_project_quota() {
 	local spec=$4
 	local uuid=$3
@@ -2667,8 +2678,10 @@ t32_test() {
 			else
 				pushd $tmp/mnt/lustre
 			fi
-			$r cat $list_file | sort -k 6 >$tmp/list.orig
-			BLOCKSIZE=1024 ls -Rni --time-style=+%s | sort -k 6 |
+			$r cat $list_file | t32_normalize_list |
+				sort -k 6 >$tmp/list.orig
+			BLOCKSIZE=1024 ls -Rni --time-style=+%s |
+				t32_normalize_list | sort -k 6 |
 				sed 's/\. / /' >$tmp/list || {
 				error_noexit "ls"
 				return 1

lustre/tests/sanityn.sh
@@ -7473,6 +7473,48 @@ test_121() {
 }
 run_test 121 "trunc append race"
 
+# A txg sync performs no metadata operation and so revokes no DLM lock.  If a
+# directory's size is derived from anything that only settles at txg sync, two
+# mounts end up disagreeing about a directory nobody touched - LU-15842
+check_dir_size_same() {
+	local dir=$1
+	local size1 size2
+
+	# no cancel_lru_locks here: this getattr has to reach the MDT before
+	# the sync below, and dropping the locks first only widens the window
+	# for a txg sync to land and settle both stats on the same value
+	size1=$(stat -c %s $DIR/$dir) || error "stat $DIR/$dir failed"
+
+	# force_sync lands in osd_sync(), which waits for the txg, so there is
+	# nothing left to sleep for
+	sync_all_data_mdts
+
+	cancel_lru_locks mdc
+	size2=$(stat -c %s $DIR2/$dir) || error "stat $DIR2/$dir failed"
+
+	(( size1 == size2 )) ||
+		error "$dir size differs across mounts: $size1 != $size2"
+}
+
+test_122() {
+	(( MDS1_VERSION >= $(version_code 2.17.57) )) ||
+		skip "need MDS >= 2.17.57 for a coherent directory size"
+
+	# plain mkdir, not test_mkdir: with MDSCOUNT > 1 that one stripes by
+	# default and the single-MDT case would never run
+	mkdir $DIR/$tdir || error "mkdir $tdir failed"
+	check_dir_size_same $tdir
+
+	(( MDSCOUNT > 1 )) || return 0
+
+	$LFS mkdir -i 1 $DIR/$tdir.remote || error "mkdir $tdir.remote failed"
+	check_dir_size_same $tdir.remote
+
+	$LFS mkdir -c -1 $DIR/$tdir.stripe || error "mkdir $tdir.stripe failed"
+	check_dir_size_same $tdir.stripe
+}
+run_test 122 "directory size is consistent across mounts"
+
 test_200() {
 	remote_ost_nodsh && skip "remote OST with nodsh" && return
 