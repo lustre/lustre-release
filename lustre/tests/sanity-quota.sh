lustre/lustre-release

commit 062456da635c0d901001eb5b6a8917dc9556518a

LU-15277 utils: lfs quota/setquota improvements
Add long options to "lfs quota" for ease of use.  Improve usage
message for "lfs quota" and "lfs setquota" to match current code.

Deprecate the "lfs quota -i MDT_IDX|-I OST_IDX" options to print one
target, since these arguments are backward from other lfs subcommands.
Add "-o" for the OST_IDX and "-m" for the MDT_IDX and long options
"--ost" and "--mdt" to match other lfs subcommands.  We may eventually
be able to liberate "-i" to use the OST_IDX, but not for a while yet.

Fix "lfs setquota" handling of long --times option.  It was being
checked in lfs_setquota_times(), but not in has_times_option().

Sort arguments to be handled (as much as possible) in alphabetical
order for ease of use in the future.

Update lfs-quota.1 and lfs-setquota.1 man pages to describe all
options and add proper argument formatting.

Test-Parameters: trivial testlist=sanity-quota
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I049f22a526469ea1ed1da04beffda6bb683ebbe6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/46615
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/lfs.c
@@ -422,15 +422,15 @@ command_t cmdlist[] = {
 	 "Usage: getname [--help|-h] [--instance|-i] [--fsname|-n] [path ...]"},
 #ifdef HAVE_SYS_QUOTA_H
 	{"setquota", lfs_setquota, 0, "Set filesystem quotas.\n"
-	 "usage: setquota [-t][-D] {-u|-U|-g|-G|-p|-P} {-b|-B|-i|-I LIMIT} [--pool POOL] FILESYSTEM\n"
-	 "	 setquota {-u|-g|-p} --delete FILESYSTEM\n"},
+	 "usage: setquota [-t] {-u|-U|-g|-G|-p|-P ID} {-b|-B|-i|-I LIMIT} [--pool POOL] FILESYSTEM\n"
+	 "       setquota {-u|-g|-p ID} {--default|--delete} FILESYSTEM\n"},
 	{"quota", lfs_quota, 0, "Display disk usage and limits.\n"
 	 "usage: quota [-q] [-v] [-h] [-o OBD_UUID|-i MDT_IDX|-I OST_IDX]\n"
 	 "	       [{-u|-g|-p} UNAME|UID|GNAME|GID|PROJID]\n"
-	 "	       [--pool <OST pool name>] <filesystem>\n"
-	 "	 quota -t <-u|-g|-p> [--pool <OST pool name>] <filesystem>\n"
-	 "	 quota [-q] [-v] [h] {-U|-G|-P} [--pool <OST pool name>] <filesystem>\n"
-	 "	 quota -a {-u|-g|-p} [-s start_qid] [-e end_qid] <filesystem>"},
+	 "             [--pool OST_POOL_NAME] FILESYSTEM\n"
+	 "       quota -t {-u|-g|-p} [--pool OST_POOL_NAME] FILESYSTEM\n"
+	 "       quota [-hqv] {-U|-G|-P} [--pool OST_POOL_NAME] FILESYSTEM\n"
+	 "	 quota -a {-u|-g|-p} [-s START_QID] [-e END_QID] FILESYSTEM"},
 	{"project", lfs_project, 0,
 	 "Change or list project attribute for specified file or directory.\n"
 	 "usage: project [-d|-r] <file|directory...>\n"
@@ -8101,7 +8101,7 @@ static inline int has_times_option(int argc, char **argv)
 	int i;
 
 	for (i = 1; i < argc; i++)
-		if (!strcmp(argv[i], "-t"))
+		if (!strcmp(argv[i], "-t") || !strcmp(argv[i], "--times"))
 			return 1;
 
 	return 0;
@@ -8144,11 +8144,10 @@ static int lfs_setquota_times(int argc, char **argv, struct if_quotactl *qctl)
 	{ .val = 'g',	.name = "group",	.has_arg = no_argument },
 	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
 	{ .val = 'i',	.name = "inode-grace",	.has_arg = required_argument },
+	{ .val = LFS_POOL_OPT, .name = "pool",	.has_arg = required_argument },
 	{ .val = 'p',	.name = "projid",	.has_arg = no_argument },
 	{ .val = 't',	.name = "times",	.has_arg = no_argument },
 	{ .val = 'u',	.name = "user",		.has_arg = no_argument },
-	{ .val = LFS_POOL_OPT,
-			.name = "pool",		.has_arg = required_argument },
 	{ .name = NULL } };
 	int qtype;
 
@@ -8158,23 +8157,6 @@ static int lfs_setquota_times(int argc, char **argv, struct if_quotactl *qctl)
 	while ((c = getopt_long(argc, argv, "b:ghi:ptu",
 				long_opts, NULL)) != -1) {
 		switch (c) {
-		case 'u':
-			qtype = USRQUOTA;
-			goto quota_type;
-		case 'g':
-			qtype = GRPQUOTA;
-			goto quota_type;
-		case 'p':
-			qtype = PRJQUOTA;
-quota_type:
-			if (qctl->qc_type != ALLQUOTA) {
-				fprintf(stderr,
-					"%s: -u/g/p cannot be used more than once\n",
-					progname);
-				return CMD_HELP;
-			}
-			qctl->qc_type = qtype;
-			break;
 		case 'b':
 			if (strncmp(optarg, NOTIFY_GRACE,
 				    strlen(NOTIFY_GRACE)) == 0) {
@@ -8190,6 +8172,9 @@ quota_type:
 			}
 			dqb->dqb_valid |= QIF_BTIME;
 			break;
+		case 'g':
+			qtype = GRPQUOTA;
+			goto quota_type;
 		case 'i':
 			if (strncmp(optarg, NOTIFY_GRACE,
 				    strlen(NOTIFY_GRACE)) == 0) {
@@ -8205,13 +8190,27 @@ quota_type:
 			}
 			dqb->dqb_valid |= QIF_ITIME;
 			break;
-		case 't': /* Yes, of course! */
-			break;
+		case 'p':
+			qtype = PRJQUOTA;
+			goto quota_type;
 		case LFS_POOL_OPT:
 			if (lfs_verify_poolarg(optarg))
 				return -1;
 			strncpy(qctl->qc_poolname, optarg, LOV_MAXPOOLNAME);
 			qctl->qc_cmd  = LUSTRE_Q_SETINFOPOOL;
+			break;
+		case 't': /* Yes, of course! */
+			break;
+		case 'u':
+			qtype = USRQUOTA;
+quota_type:
+			if (qctl->qc_type != ALLQUOTA) {
+				fprintf(stderr,
+					"%s: -u/g/p cannot be used more than once\n",
+					progname);
+				return CMD_HELP;
+			}
+			qctl->qc_type = qtype;
 			break;
 		/* getopt prints error message for us when opterr != 0 */
 		default:
@@ -8510,7 +8509,7 @@ quota_type_def:
 			}
 			qctl->qc_type = qtype;
 			break;
-#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 0, 53, 0)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 22, 53, 0)
 		case 'd':
 			fprintf(stderr,
 				"%s setquota: '-d' deprecated, use '-D' or '--default'\n",
@@ -8999,7 +8998,7 @@ static int tgt_name2index(const char *tgtname, unsigned int *idx)
 	dash += 4;
 
 	*idx = strtoul(dash, &endp, 16);
-	if (*idx > 0xffff) {
+	if (*idx > LOV_V1_INSANE_STRIPE_COUNT) {
 		fprintf(stderr, "wrong index %s\n", tgtname);
 		return -ERANGE;
 	}
@@ -9450,16 +9449,39 @@ static int lfs_quota(int argc, char **argv)
 	int c;
 	char *mnt, *name = NULL;
 	struct if_quotactl *qctl;
-	char *obd_uuid;
+	char *obd_uuid, *endp;
 	int rc = 0, rc1 = 0, verbose = 0, quiet = 0;
-	__u32 valid = QC_GENERAL, idx = 0;
+	__u32 valid = QC_GENERAL;
+	long idx = 0;
 	__u32 start_qid = 0, end_qid = 0;
 	bool human_readable = false;
 	bool show_default = false;
 	int qtype;
 	bool show_pools = false;
 	struct option long_opts[] = {
-	{ .val = LFS_POOL_OPT, .name = "pool", .has_arg = optional_argument },
+	{ .val = 'a',	.name = "all",		.has_arg = required_argument },
+	{ .val = 'e',	.name = "end-qid",	.has_arg = required_argument },
+	{ .val = 'g',	.name = "group",	.has_arg = required_argument },
+	{ .val = 'G',	.name = "default-grp",	.has_arg = no_argument },
+	{ .val = 'h',	.name = "human-readable", .has_arg = no_argument },
+	/* It is unfortunate that '-i' was used for mdt-index, and '-I' for
+	 * ost-index, because '-i' is used for ost-index everywhere else.
+	 * These options have been this way since ancient days, but I suspect
+	 * that they are not often used. Prefer --ost and --mdt instead.
+	 */
+	{ .val = 'm',	.name = "mdt-index",	.has_arg = required_argument },
+	{ .val = 'm',	.name = "mdt",		.has_arg = required_argument },
+	{ .val = 'o',	.name = "ost-index",	.has_arg = required_argument },
+	{ .val = 'o',	.name = "ost",		.has_arg = required_argument },
+	{ .val = LFS_POOL_OPT, .name = "pool",	.has_arg = optional_argument },
+	{ .val = 'p',	.name = "projid",	.has_arg = required_argument },
+	{ .val = 'P',	.name = "default-prj",	.has_arg = no_argument },
+	{ .val = 'q',	.name = "quiet",	.has_arg = no_argument },
+	{ .val = 's',	.name = "start-qid",	.has_arg = required_argument },
+	{ .val = 't',	.name = "times",	.has_arg = no_argument },
+	{ .val = 'u',	.name = "user",		.has_arg = required_argument },
+	{ .val = 'U',	.name = "default-usr",	.has_arg = required_argument },
+	{ .val = 'v',	.name = "verbose",	.has_arg = no_argument },
 	{ .name = NULL } };
 	char **poollist = NULL;
 	char *buf = NULL;
@@ -9473,80 +9495,77 @@ static int lfs_quota(int argc, char **argv)
 	qctl->qc_type = ALLQUOTA;
 	obd_uuid = (char *)qctl->obd_uuid.uuid;
 
-	while ((c = getopt_long(argc, argv, "ae:gGi:I:o:pPqs:tuUvh",
+	while ((c = getopt_long(argc, argv, "ae:gGhi:I:m:o:pPqs:tuUv",
 		long_opts, NULL)) != -1) {
 		switch (c) {
-		case 'U':
-			show_default = true;
-		case 'u':
-			qtype = USRQUOTA;
-			goto quota_type;
-		case 'G':
-			show_default = true;
-		case 'g':
-			qtype = GRPQUOTA;
-			goto quota_type;
-		case 'P':
-			show_default = true;
-		case 'p':
-			qtype = PRJQUOTA;
-quota_type:
-			if (qctl->qc_type != ALLQUOTA) {
-				fprintf(stderr,
-					"%s quota: only one of -u, -g, or -p may be specified\n",
-					progname);
-				rc = CMD_HELP;
-				goto out;
-			}
-			qctl->qc_type = qtype;
-			break;
 		case 'a':
 			qctl->qc_cmd = LUSTRE_Q_ITERQUOTA;
 			break;
-		case 't':
-			qctl->qc_cmd = LUSTRE_Q_GETINFO;
+		case 'e':
+			end_qid = strtoul(optarg, NULL, 0);
 			break;
-		case 'o':
-			valid = qctl->qc_valid = QC_UUID;
-			snprintf(obd_uuid, sizeof(*obd_uuid), "%s", optarg);
+		case 'G':
+			show_default = true;
+			/* fallthrough */
+		case 'g':
+			qtype = GRPQUOTA;
+			goto quota_type;
+		case 'h':
+			human_readable = true;
 			break;
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 22, 53, 0)
 		case 'i':
-			valid = qctl->qc_valid = QC_MDTIDX;
-			idx = qctl->qc_idx = atoi(optarg);
-			if (idx == 0 && *optarg != '0') {
+			fprintf(stderr,
+				"'-i' deprecated, use '--ost' or '--mdt'\n");
+			/* fallthrough */
+#endif
+		case 'm':
+			errno = 0;
+			idx = strtol(optarg, &endp, 0);
+			if (errno != 0 || idx > LOV_V1_INSANE_STRIPE_COUNT ||
+			    idx < 0 || *endp != '\0') {
 				fprintf(stderr,
 					"%s quota: invalid MDT index '%s'\n",
 					progname, optarg);
 				rc = CMD_HELP;
 				goto out;
 			}
+			valid = qctl->qc_valid = QC_MDTIDX;
+			qctl->qc_idx = idx;
 			break;
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 22, 53, 0)
 		case 'I':
-			valid = qctl->qc_valid = QC_OSTIDX;
-			idx = qctl->qc_idx = atoi(optarg);
-			if (idx == 0 && *optarg != '0') {
-				fprintf(stderr,
-					"%s quota: invalid OST index '%s'\n",
-					progname, optarg);
-				rc = CMD_HELP;
-				goto out;
+			fprintf(stderr, "'-I' deprecated, use '--ost'\n");
+			/* fallthrough */
+#endif
+		case 'o':
+			errno = 0;
+			idx = strtol(optarg, &endp, 0);
+			/* simple digit, treat it as a numerical OST index */
+			if (*endp == '\0') {
+				if (idx > LOV_V1_INSANE_STRIPE_COUNT ||
+				    idx < 0) {
+					fprintf(stderr,
+						"%s quota: invalid OST index '%s'\n",
+						progname, optarg);
+					rc = CMD_HELP;
+					goto out;
+				}
+				valid = qctl->qc_valid = QC_OSTIDX;
+				qctl->qc_idx = idx;
+				break;
 			}
+
+			/* need to also handle a UUID for compatibility */
+			valid = qctl->qc_valid = QC_UUID;
+			snprintf(obd_uuid, sizeof(*obd_uuid), "%s", optarg);
 			break;
-		case 's':
-			start_qid = strtoul(optarg, NULL, 0);
-			break;
-		case 'e':
-			end_qid = strtoul(optarg, NULL, 0);
-			break;
-		case 'v':
-			verbose = 1;
-			break;
-		case 'q':
-			quiet = 1;
-			break;
-		case 'h':
-			human_readable = true;
-			break;
+		case 'P':
+			show_default = true;
+			/* fallthrough */
+		case 'p':
+			qtype = PRJQUOTA;
+			goto quota_type;
 		case LFS_POOL_OPT:
 			if ((!optarg) && (argv[optind] != NULL) &&
 				(argv[optind][0] != '-') &&
@@ -9568,6 +9587,33 @@ quota_type:
 			/* optarg is NULL */
 			show_pools = true;
 			qctl->qc_cmd = LUSTRE_Q_GETQUOTAPOOL;
+			break;
+		case 'q':
+			quiet = 1;
+			break;
+		case 's':
+			start_qid = strtoul(optarg, NULL, 0);
+			break;
+		case 't':
+			qctl->qc_cmd = LUSTRE_Q_GETINFO;
+			break;
+		case 'U':
+			show_default = true;
+			/* fallthrough */
+		case 'u':
+			qtype = USRQUOTA;
+quota_type:
+			if (qctl->qc_type != ALLQUOTA) {
+				fprintf(stderr,
+					"%s quota: only one of -u, -g, or -p may be specified\n",
+					progname);
+				rc = CMD_HELP;
+				goto out;
+			}
+			qctl->qc_type = qtype;
+			break;
+		case 'v':
+			verbose = 1;
 			break;
 		default:
 			fprintf(stderr, "%s quota: unrecognized option '%s'\n",

lustre/doc/lfs-quota.1
@@ -1,20 +1,17 @@
-.TH LFS-QUOTA 1 2020-04-06 "Lustre" "Lustre Utilities"
+.TH LFS-QUOTA 1 2022-02-26 "Lustre" "Lustre Utilities"
 .SH NAME
 lfs-quota \- display quota limits and status for users, groups, or projects.
 .SH SYNOPSIS
-.B lfs quota \fR[\fB-hqv\fR]
-       [\fB-o <\fIobd_uuid\fR>|\fB-I <\fIost_idx\fR>|\fB-i <\fImdt_idx\fR>]
-       [\fB-u <\fIuname\fR|\fIuid\fR>|\fB-g <\fIgname\fR|\fIgid\fR>\
-|\fB-p <\fIprojid\fR>]
-       [\fB--pool\fR <\fIpname\fR>]
-       <\fIfilesystem\fR>\fR
+.BR "lfs quota " [ -hqv "] [" --ost " \fIobd_uuid\fR|" --mdt " \fIMDT_IDX\fR]
+       [\fB-u \fIUSER\fR|\fB-g <\fIGROUP\fR>|\fB-p \fIPROJID\fR]
+[\fB--pool\fR \fIPOOL_NAME\fR] \fIFILESYSTEM
 
 .br
-.B lfs quota \fR[\fB-hq\fR] \fR<\fB-U\fR|\fB-G\fR|\fB-P\fR> <\fIfilesystem\fR>
+.BR "lfs quota " [ -hq "] {" -U | -G | -P } " " \fIFILESYSTEM
 .br
 
 .br
-.B lfs quota -t \fR<\fB-u\fR|\fB-g\fR|\fB-p\fR> <\fIfilesystem\fR>
+.BR "lfs quota " -t " {" -u | -g | -p "} " \fIFILESYSTEM
 .br
 
 .br
@@ -24,115 +21,102 @@ lfs-quota \- display quota limits and status for users, groups, or projects.
 .TP
 .SH DESCRIPTION
 .PP
-.B lfs quota <\fIfilesystem\fR>\fR
 .TP
-
-Display disk usage and limits for individual users, groups, and projects. \
-An asterisk is displayed when the quota is exceeded. \
-By default the statistics for the entire filesystem are displayed but \
-individual MDTs and OSTs can be specified. \
-A user or group name or an ID can \
-be specified. If user, group and project are omitted, quotas for \
+.B lfs quota \fIFILESYSTEM
+Display disk usage and limits for individual users, groups, and projects.
+An asterisk is displayed when the quota is exceeded.
+By default the statistics for the entire filesystem are displayed but
+individual MDTs and OSTs can be specified with the
+.B --ost
+or
+.B --mdt
+options.  A user, group, or project ID can be specified.
+If user, group, and project are omitted, quotas for the
 current uid/gid/projid are shown.
 .TP
-.B -h
-Human readable. This will change the formatting of \
-block storage and time values. Without this option block storage values \
-are in kilobytes and times are in seconds. With this option block storage \
+.BR -h | --human-readable
+This will change the formatting of
+block storage and time values. Without this option block storage values
+are in kilobytes and times are in seconds. With this option block storage
 values use the common metric binary suffixes
-.BR k "," M "," G "," T "," P "," E
-which specify units of 2^10, 2^20, 2^30, 2^40, 2^50 and 2^60 \
-respectively.
-Time values use the "XXwXXdXXhXXmXXs" format. This format specifies \
-weeks, days, hours, minutes, seconds.
+.BR K ", " M ", " G ", " T ", " P ", and " E
+which specify units of 2^10, 2^20, 2^30, 2^40, 2^50 and 2^60 bytes,
+respectively.  Time values will use the "XXwXXdXXhXXmXXs" format, which
+specifies weeks, days, hours, minutes, seconds.
 .TP
-.B -q
-Quiet. Display only the line containing the data. \
-The line saying what the data is, and the line with the data
-column headers will not be printed.
+.BR -g | --group " {" \fIGROUP | \fIGID }
+Display quota information for group name \fIGROUP\fR or numeric \fIGID\fR.
 .TP
-.B -v
-Verbose. Display per-MDT and per-OST statistics in addition
+.BR -m | --mdt " " \fIMDT_IDX
+Display quota information for MDT \fIMDT_IDX\fR.
+.TP
+.BR -o | --ost " " \fIOST_IDX
+Display quota information for OST \fIOST_IDX\fR.
+.TP
+.BR --pool " " \fIPOOL_NAME
+Display quota information for OST pool \fIPOOL_NAME\fR.
+.TP
+.BR -p | --projid " " \fIPROJID
+Display quota information for project \fIPROJID\fR.
+.TP
+.BR -q | --quiet
+Display only the line containing the data.
+The line saying what the data is, and the column headers will not be printed.
+.TP
+.BR -u | --user " {" \fIUSER | \fIUID }
+Display quota information for user name \fIUSER\fR or numeric \fIUID\fR.
+.TP
+.BR -v | --verbose
+Display per-MDT and per-OST statistics in addition
 to the usual system wide data. An asterisk near the OST or MDT means that
 the quota is exceeded only for that specific target. The user is over the
 quota only if an asterisk is near the whole filesystem usage.
 Inactive target will also be printed but marked as "inact".
 .TP
-.B -u <\fIuname|uid\fR>
-Display user quota information for \fIuname\fR or \fIuid\fR.
-.TP
-.B -g <\fIgname|gid\fR>
-Display group quota information for name \fIgname\fR or \fIgid\fR.
-.TP
-.B -p <\fIprojid\fR>
-Display project quota information for \fIprojid\fR.
-.TP
-.B -o <\fIobd_uuid\fR>
-Display quota information for \fIobd_uuid\fR.
-.TP
-.B -i <\fImdt_idx\fR>
-Display project quota information for \fImdt_idx\fR.
-.TP
-.B -I <\fIost_idx\fR>
-Display project quota information for \fIost_idx\fR.
-.TP
-.B --pool <\fIpname\fR>
-Display quota information per OST pool \fIpname\fR.
-.TP
-.B lfs quota \fR<\fB-U\fR|\fB-G\fR|\fB-P\fR> <\fIfilesystem\fR>
-.TP
-Display default quota values for users, groups, or projects. \
+.BR "lfs quota " { -U | -G | -P "} " FILESYSTEM
+Display default quota values for users, groups, or projects.
 This command requires super user permissions.
 .TP
-.B -U <\fIuname|uid\fR>
-Display default user quota information for <\fIfilesystem\fR>.
+.BR -G | --default-grp " {" \fIGROUP | \fIGID }
+Display default quota limits for group name \fIGROUP\fR or numeric \fIGID\fR.
 .TP
-.B -G <\fIgname|gid\fR>
-Display default group quota information for name <\fIfilesystem\fR>.
+.BR -P \fIPROJID
+Display default quota limits for project \fIPROJID\fR.
 .TP
-.B -P <\fIprojid\fR>
-Display default project quota information for <\fIfilesystem\fR>.
+.BR -U | --default-usr " {" \fIUSER | UID }
+Display default quota limits for username \fIUSER\fR or numeric \fIUID\fR.
 .TP
-.B lfs quota -t \fR<\fB-u\fR|\fB-g\fR|\fB-p\fR> [\fB--pool\fR <\fIpname\fR>] <\fIfilesystem\fR>
-.TP
-Display grace times for users, groups, or projects. \
-The format is always "XXwXXdXXhXXmXXs".
-.TP
-.B -u <\fIuname|uid\fR>
-Display default user grace times for <\fIfilesystem\fR>.
-.TP
-.B -g <\fIgname|gid\fR>
-Display default group grace times for name <\fIfilesystem\fR>.
-.TP
-.B -p <\fIprojid\fR>
-Display default project grace times for <\fIfilesystem\fR>.
-.TP
-.B --pool <\fIpname\fR>
-Display user, group or project grace times per OST pool \fIpname\fR.
-.TP
-.B lfs quota -a \fR<\fB-u\fR|\fB-g\fR|\fB-p\fR> <\fIfilesystem\fR>
+.BR "lfs quota -a" " {" -u | -g | -p "} " \fIFILESYSTEM
 .TP
 Display all quota setting for all users, groups, or projects.
 .TP
+.BR "lfs quota -t" | --times " {" -u | -g | -p "} [" --pool " \fIPOOL_NAME\fR] " \fIFILESYSTEM
+Display grace times for users, groups, or projects.
+Time values use the "XXwXXdXXhXXmXXs" format, which specifies
+weeks, days, hours, minutes, seconds.
 .SH EXAMPLES
 .TP
 .B $ lfs quota /mnt/lustre
 Display quotas and usage for current user, group, and project
 .TP
-.B $ lfs quota -u bob /mnt/lustre
+.B # lfs quota -u bob /mnt/lustre
 Display quotas and usage for user 'bob'
 .TP
-.B $ lfs quota -U /mnt/lustre
+.B # lfs quota -U /mnt/lustre
 Display default user quotas
 .TP
 .B $ lfs quota -t -u /mnt/lustre
 Display grace times for user quotas on /mnt/lustre
 .TP
-.B $ lfs quota -u ivan --pool flash_pool /mnt/lustre
-Display quotas and usage for user 'ivan' per 'flash_pool"
+.B # lfs quota -u ivan --pool flash_pool /mnt/lustre
+Display quotas and usage for user
+.B ivan
+from the OST pool
+.BR flash_pool .
 .TP
 .B $ lfs quota -t -g --pool flash_pool /mnt/lustre
-Display grace times for group quotas per 'flash_pool"
+Display grace times for group quotas for the OST pool
+.BR flash_pool .
 .TP
 .SH SEE ALSO
 .BR lfs (1),

commit a3351764d69a7b1f08bbc8bc7c56b13df7dd3ab0

LU-20528 utils: Make several options work in lfs quota
1) lfs quota marked --all, --default-usr, --user, --group and
   --projid as required_argument so each of them wrongly consumed
   the following argument. For example, lfs quota --user 1000
   /mnt/lustre failed with invalid id '/mnt/lustre'.
2) d: is missing from the getopt_long() optstring, so lfs quota -d
   is rejected as an unrecognized option.

Also drop the argument from the -U/-G/-P descriptions in OPTIONS,
and add -d to the 'lfs quota' usage and the SYNOPSIS.

Test-Parameters: trivial testlist=sanity-quota
Fixes: 062456da635c ("LU-15277 utils: lfs quota/setquota improvements")
Fixes: 8064b6af1236 ("LU-18386 utils: add lfs quota --delimiter option")
Signed-off-by: Xiao Yang <xyang@ddn.com>
Change-Id: Ia802cda9390968aec1ff42c0561f1c984ce5e278
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67588
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Olaf Faaland <faaland1@llnl.gov>

Documentation/man1/lfs-quota.1
@@ -1,4 +1,4 @@
-.TH LFS-QUOTA 1 2026-03-19 "Lustre" "Lustre User Utilities"
+.TH LFS-QUOTA 1 2026-08-18 "Lustre" "Lustre User Utilities"
 .SH NAME
 lfs-quota \- display quota limits and status for users, groups, or projects.
 .SH SYNOPSIS
@@ -7,7 +7,7 @@ lfs-quota \- display quota limits and status for users, groups, or projects.
 .RB [ --block-softlimit | --bsoftlimit ]
 .RB [ --block-hardlimit | --bhardlimit ]
 .RB [ --block-grace | --bgrace | --btime ]
-.RB [ --delimiter
+.RB [ -d | --delimiter
 .IR DELIMITER ]
 .RB [ --filesystem | --mount-point ]
 .RB [ -h | --human-readable ]
@@ -245,25 +245,14 @@ Display root quota entry.
 By default, it is skipped.
 .SS DEFAULT QUOTA
 .TP
-.BR -G ", " --default-grp " {" \fIGROUP | \fIGID }
-Display default quota limits for group name
-.I GROUP
-or numeric
-.IR GID .
+.BR -G ", " --default-grp
+Display default group quota limit.
 .TP
-.BR -P ", " --default-prj " {" \fIPROJECT | \fIPROJID }
-Display default quota limits for project
-.I PROJID
-or
-.I PROJNAME
-as listed in
-.BR /etc/projid (5).
+.BR -P ", " --default-prj
+Display default project quota limit.
 .TP
-.BR -U ", " --default-usr " {" \fIUSER | UID }
-Display default quota limits for username
-.I USER
-or numeric
-.IR UID .
+.BR -U ", " --default-usr
+Display default user quota limit.
 .TP
 .BI --pool " POOL_NAME"
 Display default quota information for OST pool

lustre/utils/lfs.c
@@ -533,7 +533,7 @@ command_t cmdlist[] = {
 	 "             [--block-softlimit|--bsoftlimit]\n"
 	 "             [--block-hardlimit|--bhardlimit]\n"
 	 "             [--block-grace|--bgrace|--btime]\n"
-	 "             [--delimiter DELIMITER]\n"
+	 "             [-d|--delimiter DELIMITER]\n"
 	 "             [--filesystem|--mount-point]\n"
 	 "             [--inodes|--iusage]\n"
 	 "             [--inode-softlimit|--isoftlimit]\n"
@@ -11860,12 +11860,12 @@ static int lfs_quota(int argc, char **argv)
 	bool all = false;
 
 	struct option long_opts[] = {
-	{ .val = 'a',	.name = "all",		.has_arg = required_argument },
-	{ .val = 'e',	.name = "end-qid",	.has_arg = required_argument },
+	{ .val = 'a',	.name = "all",		.has_arg = no_argument },
 	{ .val = 'd',	.name = "delimiter",	.has_arg = required_argument },
+	{ .val = 'e',	.name = "end-qid",	.has_arg = required_argument },
 	{ .val = LFS_QUOTA_DEFAULT_OPT,
 			.name = "default",	.has_arg = no_argument },
-	{ .val = 'g',	.name = "group",	.has_arg = required_argument },
+	{ .val = 'g',	.name = "group",	.has_arg = no_argument },
 	{ .val = 'G',	.name = "default-grp",	.has_arg = no_argument },
 	{ .val = 'h',	.name = "human-readable", .has_arg = no_argument },
 	/* It is unfortunate that '-i' was used for mdt-index, and '-I' for
@@ -11881,13 +11881,13 @@ static int lfs_quota(int argc, char **argv)
 	{ .val = 'o',	.name = "ost",		.has_arg = required_argument },
 	{ .val = LFS_LQA_OPT, .name = "lqa",	.has_arg = required_argument },
 	{ .val = LFS_POOL_OPT, .name = "pool",	.has_arg = optional_argument },
-	{ .val = 'p',	.name = "projid",	.has_arg = required_argument },
+	{ .val = 'p',	.name = "projid",	.has_arg = no_argument },
 	{ .val = 'P',	.name = "default-prj",	.has_arg = no_argument },
 	{ .val = 'q',	.name = "quiet",	.has_arg = no_argument },
 	{ .val = 's',	.name = "start-qid",	.has_arg = required_argument },
 	{ .val = 't',	.name = "times",	.has_arg = no_argument },
-	{ .val = 'u',	.name = "user",		.has_arg = required_argument },
-	{ .val = 'U',	.name = "default-usr",	.has_arg = required_argument },
+	{ .val = 'u',	.name = "user",		.has_arg = no_argument },
+	{ .val = 'U',	.name = "default-usr",	.has_arg = no_argument },
 	{ .val = 'v',	.name = "verbose",	.has_arg = no_argument },
 	{ .val = LFS_QUOTA_FILESYSTEM_OPT,
 			.name = "filesystem",	.has_arg = no_argument },
@@ -11958,7 +11958,7 @@ static int lfs_quota(int argc, char **argv)
 	qctl->qc_type = ALLQUOTA;
 	obd_uuid = (char *)qctl->obd_uuid.uuid;
 
-	while ((c = getopt_long(argc, argv, "abBe:gGhi:I:m:no:pPqs:tuUv",
+	while ((c = getopt_long(argc, argv, "abBd:e:gGhi:I:m:no:pPqs:tuUv",
 		long_opts, NULL)) != -1) {
 		switch (c) {
 		case 'a':

lustre/tests/sanity-quota.sh
@@ -4748,6 +4748,64 @@ test_49b() {
 }
 run_test 49b "lfs quota -a --blocks has a delimiter"
 
+test_49c() {
+	(( CLIENT_VERSION >= $(version_code 2.17.57) )) ||
+		skip "Need client version >= 2.17.57 for long option fix"
+
+	local nodir=$DIR/nonexistent
+
+	$LFS quota --user $TSTUSR $DIR ||
+		error "'quota --user $TSTUSR $DIR' failed"
+	$LFS quota --group $TSTUSR $DIR ||
+		error "'quota --group $TSTUSR $DIR' failed"
+	$LFS quota --default-usr $DIR ||
+		error "'quota --default-usr $DIR' failed"
+	$LFS quota --default-usr $nodir &&
+		error "'quota --default-usr $nodir' should have failed"
+	$LFS quota --default-grp $DIR ||
+		error "'quota --default-grp $DIR' failed"
+	$LFS quota --default-grp $nodir &&
+		error "'quota --default-grp $nodir' should have failed"
+
+	if is_project_quota_supported; then
+		$LFS quota --projid $TSTPRJID $DIR ||
+			error "'quota --projid $TSTPRJID $DIR' failed"
+		$LFS quota --default-prj $DIR ||
+			error "'quota --default-prj $DIR' failed"
+		$LFS quota --default-prj $nodir &&
+			error "'quota --default-prj $nodir' should have failed"
+	fi
+
+	(( MDS1_VERSION >= $(version_code v2_15_61-145-g3edc718) )) ||
+		skip "Need MDS >= 2.15.61 for 'lfs quota --all'"
+
+	$LFS quota --all -u $DIR || error "'quota --all -u $DIR' failed"
+	$LFS quota --all -g $DIR || error "'quota --all -g $DIR' failed"
+
+	if is_project_quota_supported; then
+		$LFS quota --all -p $DIR || error "'quota --all -p $DIR' failed"
+	fi
+
+	return 0
+}
+run_test 49c "lfs quota long options don't consume an extra argument"
+
+test_49d() {
+	(( CLIENT_VERSION >= $(version_code 2.17.57) )) ||
+		skip "Need client version >= 2.17.57 for 'lfs quota -d' fix"
+
+	local out
+
+	out=$($LFS quota -d "," -u $TSTUSR $DIR) ||
+		error "'quota -d , -u $TSTUSR' failed"
+	[[ "$out" =~ "," ]] || error "no ',' delimiter for -d: '$out'"
+
+	out=$($LFS quota --delimiter="," -u $TSTUSR $DIR) ||
+		error "'quota --delimiter=, -u $TSTUSR' failed"
+	[[ "$out" =~ "," ]] || error "no ',' delimiter for --delimiter: '$out'"
+}
+run_test 49d "lfs quota -d and --delimiter both work"
+
 test_50() {
 	! is_project_quota_supported &&
 		skip "Project quota is not supported"