lustre/lustre-release

commit 294aa9cb666c48e02da1057c222fe5f206ce38fc

LU-1842 quota: add quotactl support on qmt
This patch implements quotactl ({get,set}{quota,info}) support on the
QMT. This involves defining the lqe operations for the qmt as well as
pool operations in order to support per-pool quota in the future.

Signed-off-by: Johann Lombardi <johann@whamcloud.com>
Change-Id: I122e7cb85eb6d1d3460e86217e2a6723763c8cbf
Reviewed-on: http://review.whamcloud.com/4160
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Niu Yawei <niu@whamcloud.com>
Reviewed-by: Mike Pershin <tappro@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/lproc_quota.c
@@ -14,24 +14,18 @@
  * in the LICENSE file that accompanied this code).
  *
  * You should have received a copy of the GNU General Public License
- * version 2 along with this program; If not, see
- * http://www.sun.com/software/products/lustre/docs/GPLv2.pdf
- *
- * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
- * CA 95054 USA or visit www.sun.com if you need additional information or
- * have any questions.
+ * version 2 along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA
  *
  * GPL HEADER END
  */
 /*
- * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012 Intel, Inc.
  * Use is subject to license terms.
  *
- * Copyright (c) 2011, Whamcloud, Inc.
- */
-/*
- * This file is part of Lustre, http://www.lustre.org/
- * Lustre is a trademark of Sun Microsystems, Inc.
+ * Author: Johann Lombardi <johann.lombardi@intel.com>
+ * Author: Niu    Yawei    <yawei.niu@intel.com>
  */
 
 #define DEBUG_SUBSYSTEM S_LQUOTA
@@ -167,20 +161,22 @@ static void *lprocfs_quota_seq_next(struct seq_file *p, void *v, loff_t *pos)
 /*
  * Output example:
  *
- * user_accounting:
+ * usr_accounting:
  * - id:      0
- *   usage:   { inodes:                  209, bytes:             26161152 }
+ *   usage:   { inodes:                  209, kbytes:             2616 }
  * - id:      840000038
- *   usage:   { inodes:                    1, bytes:             10485760 }
+ *   usage:   { inodes:                    1, kbytes:             1048 }
  */
 static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 {
-	struct lquota_procfs	*lqp = p->private;
-	const struct dt_it_ops	*iops;
-	struct dt_it		*it;
-	struct dt_key		*key;
-	struct lquota_acct_rec	 rec;
-	int			 rc;
+	struct lquota_procfs		*lqp = p->private;
+	struct lquota_thread_info	*qti = lquota_info(&lqp->lqp_env);
+	const struct dt_it_ops		*iops;
+	struct dt_it			*it;
+	struct dt_key			*key;
+	struct dt_rec			*rec = (struct dt_rec *)&qti->qti_rec;
+	const struct lu_fid		*fid;
+	int				 rc;
 
 	LASSERT(lqp);
 	if (lqp->lqp_obj == NULL) {
@@ -188,14 +184,26 @@ static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 		return 0;
 	}
 
-	if (v == SEQ_START_TOKEN) {
-		const struct lu_fid *fid = lu_object_fid(&lqp->lqp_obj->do_lu);
+	fid = lu_object_fid(&lqp->lqp_obj->do_lu);
 
-		LASSERT(fid_is_acct(fid));
-		if (fid_oid(fid) == ACCT_USER_OID)
-			seq_printf(p, "user_accounting:\n");
-		else
-			seq_printf(p, "group_accounting:\n");
+	if (v == SEQ_START_TOKEN) {
+		if (fid_is_acct(fid)) {
+			if (fid_oid(fid) == ACCT_USER_OID)
+				seq_printf(p, "usr_accounting:\n");
+			else
+				seq_printf(p, "grp_accounting:\n");
+		} else if (fid_seq(fid) == FID_SEQ_QUOTA_GLB) {
+			int	poolid, rtype, qtype;
+
+			rc = lquota_extract_fid(fid, &poolid, &rtype, &qtype);
+			if (rc)
+				return rc;
+
+			seq_printf(p, "global_pool%d_%s_%s\n", poolid,
+				   RES_NAME(rtype), QTYPE_NAME(qtype));
+		} else {
+			return -ENOTSUPP;
+		}
 		return 0;
 	}
 
@@ -210,7 +218,7 @@ static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 		return PTR_ERR(key);
 	}
 
-	rc = iops->rec(&lqp->lqp_env, it, (struct dt_rec *)&rec, 0);
+	rc = iops->rec(&lqp->lqp_env, it, rec, 0);
 	if (rc) {
 		CERROR("%s: failed to get rec: rc = %d\n",
 		       lqp->lqp_obj->do_lu.lo_dev->ld_obd->obd_name, rc);
@@ -218,8 +226,19 @@ static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 	}
 
 	seq_printf(p, "- %-8s %llu\n", "id:", *((__u64 *)key));
-	seq_printf(p, "  %-8s { inodes: %20"LPF64"u, bytes: %20"LPF64"u }\n",
-		   "usage:", rec.ispace, rec.bspace);
+	if (fid_is_acct(fid))
+		seq_printf(p, "  %-8s { inodes: %20"LPF64"u, kbytes: %20"LPF64
+			   "u }\n", "usage:",
+			   ((struct lquota_acct_rec *)rec)->ispace,
+			   toqb(((struct lquota_acct_rec *)rec)->bspace));
+	else if (fid_seq(fid) == FID_SEQ_QUOTA_GLB)
+		seq_printf(p, "  %-8s { hard: %20"LPF64"u, soft: %20"LPF64
+			   "u, granted: %20"LPF64"u, time: %20"LPF64"u }\n",
+			   "limits:",
+			   ((struct lquota_glb_rec *)rec)->qbr_hardlimit,
+			   ((struct lquota_glb_rec *)rec)->qbr_softlimit,
+			   ((struct lquota_glb_rec *)rec)->qbr_granted,
+			   ((struct lquota_glb_rec *)rec)->qbr_time);
 	return 0;
 }
 

lustre/ldlm/ldlm_resource.c
@@ -936,6 +936,7 @@ void ldlm_namespace_get(struct ldlm_namespace *ns)
 {
         cfs_atomic_inc(&ns->ns_bref);
 }
+EXPORT_SYMBOL(ldlm_namespace_get);
 
 void ldlm_namespace_put(struct ldlm_namespace *ns)
 {
@@ -944,6 +945,7 @@ void ldlm_namespace_put(struct ldlm_namespace *ns)
                 cfs_spin_unlock(&ns->ns_lock);
         }
 }
+EXPORT_SYMBOL(ldlm_namespace_put);
 
 /* Register @ns in the list of namespaces */
 void ldlm_namespace_register(struct ldlm_namespace *ns, ldlm_side_t client)

lustre/quota/Makefile.in
@@ -1,6 +1,6 @@
 MODULES := lquota
 
-qmt-objs := qmt_dev.o qmt_handler.o qmt_lock.o
+qmt-objs := qmt_dev.o qmt_handler.o qmt_lock.o qmt_entry.o qmt_pool.o
 
 quota-objs := lproc_quota.o lquota_lib.o lquota_disk.o lquota_entry.o
 

lustre/quota/lquota_disk.c
@@ -92,18 +92,6 @@ lquota_disk_find_create(const struct lu_env *env, struct dt_device *dev,
 	obj = dt_locate(env, dev, &qti->qti_fid);
 	if (IS_ERR(obj))
 		GOTO(out, obj);
-
-	/* install index operation vector */
-	if (obj->do_index_ops == NULL) {
-		rc = obj->do_ops->do_index_try(env, obj, idx_feat);
-		if (rc) {
-			CERROR("%s: fail to setup index operations for "DFID
-			       " rc:%d\n", dev->dd_lu_dev.ld_obd->obd_name,
-			       PFID(lu_object_fid(&obj->do_lu)), rc);
-			lu_object_put(env, &obj->do_lu);
-			obj = ERR_PTR(rc);
-		}
-	}
 out:
 	local_oid_storage_fini(env, los);
 	RETURN(obj);
@@ -291,10 +279,26 @@ struct dt_object *lquota_disk_glb_find_create(const struct lu_env *env,
 							      idx_feat);
 	}
 
-	if (IS_ERR(glb_idx))
+	if (IS_ERR(glb_idx)) {
 		CERROR("%s: failed to look-up/create idx file "DFID" rc:%ld "
 		       "local:%d\n", dev->dd_lu_dev.ld_obd->obd_name,
 		       PFID(fid), PTR_ERR(glb_idx), local);
+		RETURN(glb_idx);
+	}
+
+	/* install index operation vector */
+	if (glb_idx->do_index_ops == NULL) {
+		int rc;
+
+		rc = glb_idx->do_ops->do_index_try(env, glb_idx, idx_feat);
+		if (rc) {
+			CERROR("%s: failed to setup index operations for "DFID
+			       " rc:%d\n", dev->dd_lu_dev.ld_obd->obd_name,
+			       PFID(lu_object_fid(&glb_idx->do_lu)), rc);
+			lu_object_put(env, &glb_idx->do_lu);
+			glb_idx = ERR_PTR(rc);
+		}
+	}
 
 	RETURN(glb_idx);
 }
@@ -433,6 +437,22 @@ struct dt_object *lquota_disk_slv_find_create(const struct lu_env *env,
 						  &qti->qti_fid,
 						  &dt_quota_slv_features,
 						  qti->qti_buf);
+	}
+
+	if (IS_ERR(slv_idx))
+		RETURN(slv_idx);
+
+	/* install index operation vector */
+	if (slv_idx->do_index_ops == NULL) {
+		rc = slv_idx->do_ops->do_index_try(env, slv_idx,
+						   &dt_quota_slv_features);
+		if (rc) {
+			CERROR("%s: failed to setup index operations for "DFID
+			       " rc:%d\n", dev->dd_lu_dev.ld_obd->obd_name,
+			       PFID(lu_object_fid(&slv_idx->do_lu)), rc);
+			lu_object_put(env, &slv_idx->do_lu);
+			slv_idx = ERR_PTR(rc);
+		}
 	}
 
 	RETURN(slv_idx);

lustre/quota/lquota_entry.c
@@ -41,7 +41,6 @@
 static int hash_lqs_cur_bits = HASH_LQE_CUR_BITS;
 CFS_MODULE_PARM(hash_lqs_cur_bits, "i", int, 0444,
                 "the current bits of lqe hash");
-cfs_mem_cache_t *lqe_kmem;
 
 static unsigned lqe64_hash_hash(cfs_hash_t *hs, const void *key, unsigned mask)
 {

lustre/quota/lquota_internal.h
@@ -32,6 +32,7 @@
 #define _LQUOTA_INTERNAL_H
 
 #define QTYPE_NAME(qtype) ((qtype) == USRQUOTA ? "usr" : "grp")
+#define RES_NAME(res) ((res) == LQUOTA_RES_MD ? "md" : "dt")
 
 #define QIF_IFLAGS (QIF_INODES | QIF_ITIME | QIF_ILIMITS)
 #define QIF_BFLAGS (QIF_SPACE | QIF_BTIME | QIF_BLIMITS)
@@ -254,6 +255,14 @@ static inline void lqe_read_unlock(struct lquota_entry *lqe)
 		cfs_read_unlock(&lqe->lqe_lock);
 }
 
+/*
+ * Helper functions & prototypes
+ */
+
+/* minimum qunit size, 1K inode for metadata pool and 1MB for data pool */
+#define LQUOTA_LEAST_QUNIT(type) \
+	(type == LQUOTA_RES_MD ? (1 << 10) : toqb(PTLRPC_MAX_BRW_SIZE))
+
 /* Common data shared by quota-level handlers. This is allocated per-thread to
  * reduce stack consumption */
 struct lquota_thread_info {
@@ -335,8 +344,9 @@ void lquota_lqe_debug0(struct lquota_entry *lqe,
 struct dt_object *acct_obj_lookup(const struct lu_env *, struct dt_device *,
 				  int);
 void lquota_generate_fid(struct lu_fid *, int, int, int);
-int lquota_extract_fid(struct lu_fid *, int *, int *, int *);
+int lquota_extract_fid(const struct lu_fid *, int *, int *, int *);
 const struct dt_index_features *glb_idx_feature(struct lu_fid *);
+extern cfs_mem_cache_t *lqe_kmem;
 
 /* lquota_entry.c */
 /* site create/destroy */

lustre/quota/lquota_lib.c
@@ -40,6 +40,19 @@
 
 #include "lquota_internal.h"
 
+cfs_mem_cache_t *lqe_kmem;
+
+struct lu_kmem_descr lquota_caches[] = {
+	{
+		.ckd_cache = &lqe_kmem,
+		.ckd_name  = "lqe_kmem",
+		.ckd_size  = sizeof(struct lquota_entry)
+	},
+	{
+		.ckd_cache = NULL
+	}
+};
+
 /* register lquota key */
 LU_KEY_INIT_FINI(lquota, struct lquota_thread_info);
 LU_CONTEXT_KEY_DEFINE(lquota, LCT_MD_THREAD | LCT_DT_THREAD | LCT_LOCAL);
@@ -244,7 +257,7 @@ void lquota_generate_fid(struct lu_fid *fid, int pool_id, int pool_type,
  * Helper routine used to extract pool ID, pool type and quota type from a
  * given FID.
  */
-int lquota_extract_fid(struct lu_fid *fid, int *pool_id, int *pool_type,
+int lquota_extract_fid(const struct lu_fid *fid, int *pool_id, int *pool_type,
 		       int *quota_type)
 {
 	unsigned int	 tmp;
@@ -338,9 +351,13 @@ static int __init init_lquota(void)
 #warning "remove old quota compatibility code"
 #endif
 
-	rc = qmt_glb_init();
+	rc = lu_kmem_init(lquota_caches);
 	if (rc)
 		GOTO(out_key, rc);
+
+	rc = qmt_glb_init();
+	if (rc)
+		GOTO(out_caches, rc);
 
 	rc = qsd_glb_init();
 	if (rc)
@@ -350,6 +367,8 @@ static int __init init_lquota(void)
 
 out_qmt:
 	qmt_glb_fini();
+out_caches:
+	lu_kmem_fini(lquota_caches);
 out_key:
 	lu_context_key_degister(&lquota_thread_key);
 	return rc;
@@ -359,6 +378,7 @@ static void exit_lquota(void)
 {
 	qsd_glb_fini();
 	qmt_glb_fini();
+	lu_kmem_fini(lquota_caches);
 	lu_context_key_degister(&lquota_thread_key);
 }
 

lustre/quota/qmt_dev.c
@@ -99,12 +99,29 @@ static struct lu_device *qmt_device_fini(const struct lu_env *env,
 	LASSERT(qmt != NULL);
 
 	CDEBUG(D_QUOTA, "%s: initiating QMT shutdown\n", qmt->qmt_svname);
+	qmt->qmt_stopping = true;
+
+	/* remove qmt proc entry */
+	if (qmt->qmt_proc != NULL && !IS_ERR(qmt->qmt_proc)) {
+		lprocfs_remove(&qmt->qmt_proc);
+		qmt->qmt_proc = NULL;
+	}
+
+	/* kill pool instances, if any */
+	qmt_pool_fini(env, qmt);
 
 	/* disconnect from OSD */
 	if (qmt->qmt_child_exp != NULL) {
 		obd_disconnect(qmt->qmt_child_exp);
 		qmt->qmt_child_exp = NULL;
-		qmt->qmt_child     = NULL;
+		qmt->qmt_child = NULL;
+	}
+
+	/* release reference on MDT namespace */
+	if (ld->ld_obd->obd_namespace != NULL) {
+		ldlm_namespace_put(ld->ld_obd->obd_namespace);
+		ld->ld_obd->obd_namespace = NULL;
+		qmt->qmt_ns = NULL;
 	}
 
 	RETURN(NULL);
@@ -159,7 +176,7 @@ static int qmt_connect_to_osd(const struct lu_env *env, struct qmt_device *qmt,
 	/* initialize site (although it isn't used anywhere) and lu_device
 	 * pointer to next device */
 	qmt->qmt_child = lu2dt_dev(qmt->qmt_child_exp->exp_obd->obd_lu_dev);
-	ld->ld_site    = qmt->qmt_child_exp->exp_obd->obd_lu_dev->ld_site;
+	ld->ld_site = qmt->qmt_child_exp->exp_obd->obd_lu_dev->ld_site;
 	EXIT;
 out:
 	if (data)
@@ -187,7 +204,8 @@ static int qmt_device_init0(const struct lu_env *env, struct qmt_device *qmt,
 			    struct lu_device_type *ldt, struct lustre_cfg *cfg)
 {
 	struct lu_device	*ld = qmt2lu_dev(qmt);
-	struct obd_device	*obd;
+	struct obd_device	*obd, *mdt_obd;
+	struct obd_type		*type;
 	int			 rc;
 	ENTRY;
 
@@ -204,11 +222,42 @@ static int qmt_device_init0(const struct lu_env *env, struct qmt_device *qmt,
 	obd->obd_lu_dev = ld;
 	ld->ld_obd      = obd;
 
+	/* look-up the parent MDT to steal its ldlm namespace ... */
+	mdt_obd = class_name2obd(lustre_cfg_string(cfg, 2));
+	if (mdt_obd == NULL)
+		RETURN(-ENOENT);
+
+	/* grab reference on MDT namespace. kind of a hack until we have our
+	 * own namespace & service threads */
+	LASSERT(mdt_obd->obd_namespace != NULL);
+	ldlm_namespace_get(mdt_obd->obd_namespace);
+	obd->obd_namespace = mdt_obd->obd_namespace;
+	qmt->qmt_ns = obd->obd_namespace;
+
 	/* connect to backend osd device */
 	rc = qmt_connect_to_osd(env, qmt, cfg);
 	if (rc)
 		GOTO(out, rc);
 
+	/* at the moment there is no linkage between lu_type and obd_type, so
+	 * we lookup obd_type this way */
+	type = class_search_type(LUSTRE_QMT_NAME);
+	LASSERT(type != NULL);
+
+	/* register proc directory associated with this qmt */
+	qmt->qmt_proc = lprocfs_register(qmt->qmt_svname, type->typ_procroot,
+					 NULL, NULL);
+	if (IS_ERR(qmt->qmt_proc)) {
+		rc = PTR_ERR(qmt->qmt_proc);
+		CERROR("%s: failed to create qmt proc entry (%d)\n",
+		       qmt->qmt_svname, rc);
+		GOTO(out, rc);
+	}
+
+	/* initialize pool configuration */
+	rc = qmt_pool_init(env, qmt);
+	if (rc)
+		GOTO(out, rc);
 	EXIT;
 out:
 	if (rc)
@@ -372,7 +421,27 @@ static int qmt_device_prepare(const struct lu_env *env,
 			      struct lu_device *parent,
 			      struct lu_device *ld)
 {
-	return 0;
+	struct qmt_device	*qmt = lu2qmt_dev(ld);
+	struct dt_object	*qmt_root;
+	int			 rc;
+	ENTRY;
+
+	/* initialize quota master root directory where all index files will be
+	 * stored */
+	qmt_root = lquota_disk_dir_find_create(env, qmt->qmt_child, NULL,
+					       QMT_DIR);
+	if (IS_ERR(qmt_root)) {
+		rc = PTR_ERR(qmt_root);
+		CERROR("%s: failed to create master quota directory (%d)\n",
+		       qmt->qmt_svname, rc);
+		RETURN(rc);
+	}
+
+	/* initialize on-disk indexes associated with each pool */
+	rc = qmt_pool_prepare(env, qmt, qmt_root);
+
+	lu_object_put(env, &qmt_root->do_lu);
+	RETURN(rc);
 }
 
 /*

commit 1cf082c4a41f98b478766560362066a007126dcd

LU-8066 quota: fix invalid YAML format
The quota seq file displays information in YAML format but that
format is invalid. It's a simple fix for the first entry to be
a proper YAML mapping.

Test-Parameters: trivial
Fixes: 294aa9cb666c ("LU-1842 quota: add quotactl support on qmt")
Signed-off-by: James Simmons <jsimmons@infradead.org>
Change-Id: Ia10a9a877d6fc21fcb0d43a95ef935f294efd609
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/68016
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/quota/lproc_quota.c
@@ -200,7 +200,7 @@ static int lprocfs_quota_seq_show(struct seq_file *p, void *v)
 			if (rc)
 				return rc;
 
-			seq_printf(p, "global_pool%d_%s_%s\n", 0,
+			seq_printf(p, "global_pool0_%s_%s:\n",
 				   RES_NAME(rtype), qtype_name(qtype));
 		} else if (fid_seq(fid) == FID_SEQ_LOCAL_NAME) {
 			/* global index copy object */