lustre/lustre-release

commit 001b8dbfacb747f1649a2eb047a5f118ce32fdc7

LU-3963 cleanup: C89 and build cleanups
use C99 initialization for a types, remove some platform depended code
and use POSIX compatible instead.
don't build a lhsmtool is none utils allowed.

Signed-off-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
Change-Id: I6e2837953902520240e9ceb251c92329d328d715
Reviewed-on: http://review.whamcloud.com/7803
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Bob Glossman <bob.glossman@intel.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/liblustre/dir.c
@@ -146,8 +146,8 @@ static void *(*memmover)(void *, const void *, size_t) = memmove;
 static int filldir(char *buf, int buflen, const char *name, int namelen,
 		   loff_t offset, ino_t ino, unsigned int d_type, int *filled)
 {
-	struct dirent64 *dirent = (struct dirent64 *)(buf + *filled);
-	struct dirent64  holder;
+	struct intnl_dirent *dirent = (struct intnl_dirent *)(buf + *filled);
+	struct intnl_dirent holder;
 	int reclen = ROUND_UP64(NAME_OFFSET(dirent) + namelen + 1);
 
         /*

libcfs/include/libcfs/libcfs_private.h
@@ -534,7 +534,7 @@ int cfs_percpt_atomic_summary(atomic_t **refs);
  *       value  after  conversion...
  *
  */
-#define CLASSERT(cond) do {switch(42) {case (cond): case 0: break;}} while (0)
+#define CLASSERT(cond) do {switch (1) {case (cond): case 0: break; } } while (0)
 
 /* support decl needed both by kernel and liblustre */
 int		 libcfs_isknown_lnd(int type);

libcfs/include/libcfs/linux/Makefile.am
@@ -1,3 +1,3 @@
 EXTRA_DIST = kp30.h libcfs.h linux-fs.h linux-lock.h linux-mem.h	\
 	linux-prim.h linux-time.h linux-tcpip.h linux-cpu.h		\
-	portals_compat25.h linux-bitops.h linux-crypto.h
+	portals_compat25.h linux-crypto.h

libcfs/include/libcfs/linux/libcfs.h
@@ -54,7 +54,6 @@
 #include <libcfs/linux/linux-lock.h>
 #include <libcfs/linux/linux-fs.h>
 #include <libcfs/linux/linux-tcpip.h>
-#include <libcfs/linux/linux-bitops.h>
 #include <libcfs/linux/kp30.h>
 
 #ifdef HAVE_ASM_TYPES_H
@@ -64,6 +63,7 @@
 #include <asm/timex.h>
 #include <linux/sched.h> /* THREAD_SIZE */
 #include <linux/rbtree.h>
+#include <linux/bitops.h>
 
 #if !defined(__x86_64__)
 # ifdef  __ia64__

libcfs/include/libcfs/linux/portals_compat25.h
@@ -37,10 +37,6 @@
 #ifndef __LIBCFS_LINUX_PORTALS_COMPAT_H__
 #define __LIBCFS_LINUX_PORTALS_COMPAT_H__
 
-#ifndef __user
-#define __user
-#endif
-
 #define LL_PROC_PROTO(name)					\
 	name(struct ctl_table *table, int write,		\
 		void __user *buffer, size_t *lenp, loff_t *ppos)

libcfs/include/libcfs/posix/libcfs.h
@@ -67,6 +67,7 @@
 #include <sys/utsname.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #ifdef HAVE_NETDB_H
 #include <netdb.h>
@@ -186,11 +187,6 @@ typedef unsigned long long cfs_cycles_t;
 #define __swab16s(x)                            do { *(x) = __swab16(*(x)); } while (0)
 #define __swab32s(x)                            do { *(x) = __swab32(*(x)); } while (0)
 #define __swab64s(x)                            do { *(x) = __swab64(*(x)); } while (0)
-#endif
-
-#if !defined(ALIGN)
-#define __ALIGN_MASK(x, mask)	(((x) + (mask)) & ~(mask))
-#define ALIGN(x, a)		__ALIGN_MASK(x, (typeof(x))(a) - 1)
 #endif
 
 # ifndef THREAD_SIZE /* x86_64 linux has THREAD_SIZE in userspace */
@@ -487,5 +483,9 @@ typedef ssize_t (*read_actor_t)();
 #  define IFTODT(type)		(((type) & S_IFMT) >> IFSHIFT)
 #  define DTTOIF(dirtype)	((dirtype) << IFSHIFT)
 # endif
+
+#ifndef ERESTARTSYS
+#define ERESTARTSYS ERESTART
+#endif
 
 #endif

libcfs/include/libcfs/posix/posix-types.h
@@ -42,7 +42,9 @@
 #ifndef _LUSTRE_POSIX_TYPES_H
 #define _LUSTRE_POSIX_TYPES_H
 
-#include <asm/types.h>
+#ifdef HAVE_LINUX_TYPES_H
+# include <linux/types.h>
+#endif
 #include <stdbool.h> /* for bool */
 #ifndef HAVE_UMODE_T
 typedef unsigned short umode_t;

commit 4f56de93baa523c1486e14f304969c2c9014d222

LU-20554 quota: preserve partial OST usage
lov_quotactl() accumulates quota usage from reachable OSTs and stores
the subtotal in dqb_curspace even when querying another target fails.
quotactl_ioctl() currently ignores this subtotal for errors that are
not explicitly handled, so lfs quota may report only MDT usage.

Always copy dqb_curspace returned by lov_quotactl(), but set QIF_SPACE
only when all expected OST quota queries succeed. This reports usage
from reachable OSTs and MDTs while retaining brackets to mark
incomplete results.
For example, with an unavailable OST, lfs quota now reports:

Filesystem     used  bquota  blimit  bgrace ...
   /lusfs/ [105.3G]    200G    200G       - ...

Some errors happened when getting quota info. Some devices may be
not working or deactivated. The data in "[]" is inaccurate.

The brackets make it clear that the displayed usage is incomplete,
while 105.3G includes usage reported by the reachable OSTs.

Remove the obsolete special handling for -EREMOTEIO. Commit
001b8dbfacb7 ("LU-3963 cleanup: C89 and build cleanups") changed
lov_quotactl() to return -ENETDOWN when an expected OST is inactive,
making the special case unreachable.

Add sanity-quota test_16c to verify that usage from a reachable OST is
preserved and marked incomplete while another OST is unavailable.

Fixes: 001b8dbfacb7 ("LU-3963 cleanup: C89 and build cleanups")
Test-Parameters: testlist=sanity-quota env=ONLY=16c
Signed-off-by: Sergey Noskov <sergey.noskov.sn@gmail.com>
Change-Id: I17252ef34ff32dbdd0561c1570ce66f31cacfb87
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67911
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Keguang Xu <kxu@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Marc Vef <mvef@whamcloud.com>

lustre/llite/dir.c
@@ -1796,11 +1796,14 @@ int quotactl_ioctl(struct super_block *sb, struct if_quotactl *qctl)
 			/* collect space usage from OSTs */
 			oqctl_tmp->qc_dqblk.dqb_curspace = 0;
 			rc = obd_quotactl(sbi->ll_dt_exp, oqctl_tmp);
-			if (!rc || rc == -EREMOTEIO) {
-				oqctl->qc_dqblk.dqb_curspace =
-					oqctl_tmp->qc_dqblk.dqb_curspace;
+			/*
+			 * Keep partial OST usage on error, but leave
+			 * QIF_SPACE clear.
+			 */
+			oqctl->qc_dqblk.dqb_curspace =
+				oqctl_tmp->qc_dqblk.dqb_curspace;
+			if (!rc)
 				oqctl->qc_dqblk.dqb_valid |= QIF_SPACE;
-			}
 
 			/* collect space & inode usage from MDTs */
 			oqctl_tmp->qc_cmd = Q_GETOQUOTA;

lustre/tests/sanity-quota.sh
@@ -3175,6 +3175,109 @@ test_16b()
 }
 run_test 16b "lfs quota should skip the nonexistent MDT/OST"
 
+set_mds_osc_state_16c()
+{
+	local facet
+	local index
+	local osc
+	local state
+
+	state=$1
+
+	for index in $(seq $MDSCOUNT); do
+		facet=mds$index
+		osc=$(get_osc_import_name $facet ost1)
+		do_facet $facet $LCTL --device %$osc $state || return 1
+	done
+}
+
+cleanup_16c()
+{
+	local ostdev
+
+	$cleanup_done_16c && return 0
+
+	ostdev=$(ostdevname 1)
+	facet_up ost1 ||
+		start ost1 $ostdev $OST_MOUNT_OPTS || return 1
+	set_mds_osc_state_16c activate || return 1
+	# The remounted client cannot connect until recovery has evicted the
+	# stale export from the previous mount.
+	wait_recovery_complete ost1 || return 1
+	mount_client $MOUNT || return 1
+
+	cleanup_done_16c=true
+}
+
+test_16c()
+{
+	local testfile
+	local full_space
+	local partial_space
+
+	testfile=$DIR/$tdir/$tfile
+
+	(( $CLIENT_VERSION >= $(version_code 2.17.57.44) )) ||
+		skip "need client >= 2.17.57.44 for partial OST usage"
+	(( $OSTCOUNT > 1 )) || skip "needs >= 2 OSTs"
+	[[ $PARALLEL != "yes" ]] || skip "skip parallel run"
+
+	setup_quota_test || error "setup quota failed with $?"
+
+	$LFS setquota -u $TSTUSR -B 500M -I 10K $MOUNT ||
+		error "failed to set quota for user $TSTUSR"
+
+	# Leave OST0000 empty and put all test data on OST0001.
+	$LFS setstripe -c 1 -i 1 $testfile ||
+		error "failed to create $testfile on OST0001"
+	chown $TSTUSR.$TSTUSR $testfile ||
+		error "failed to change ownership of $testfile"
+	$RUNAS $DD of=$testfile count=50 oflag=sync ||
+		quota_error u $TSTUSR "write on OST0001 failed"
+
+	full_space=$($LFS quota -u $TSTUSR --space -q $MOUNT) ||
+		quota_error u $TSTUSR "failed to get complete space usage"
+	full_space=${full_space%%$'\n'*}
+	full_space=$(tr -d '[:space:]' <<<$full_space)
+	[[ $full_space =~ ^[0-9]+$ ]] ||
+		error "invalid complete space usage '$full_space'"
+	(( full_space > 0 )) || error "complete space usage is zero"
+
+	cleanup_done_16c=false
+	stack_trap "cleanup_16c || true" EXIT
+	set_mds_osc_state_16c deactivate ||
+		error "failed to deactivate OST0000 on the MDTs"
+	stop ost1 || error "failed to stop OST0000"
+
+	# Recreate the client import while OST0000 is unavailable.  Locally
+	# deactivating the OSC would also clear ltd_activate and would not
+	# exercise the -ENETDOWN path in lov_quotactl().
+	remount_client $MOUNT
+
+	wait_osc_import_state client ost1 "\(DISCONN\|IDLE\)" ||
+		error "client did not detect stopped OST0000"
+
+	# An incomplete query returns an error, but still prints the subtotal.
+	partial_space=$($LFS quota -u $TSTUSR --space -q $MOUNT 2>/dev/null ||
+			true)
+	partial_space=${partial_space%%$'\n'*}
+	[[ $partial_space == \[*\] ]] ||
+		error "space usage '$partial_space' is not marked incomplete"
+	partial_space=$(tr -d '[][:space:]' <<<$partial_space)
+	[[ $partial_space =~ ^[0-9]+$ ]] ||
+		error "invalid partial space usage '$partial_space'"
+	(( partial_space >= full_space )) ||
+		error "partial usage $partial_space misses active OST usage " \
+		      "$full_space"
+
+	cleanup_16c || error "failed to restore OST0000"
+	wait_osc_import_state mds ost1 FULL ||
+		error "MDTs did not reconnect to OST0000"
+	wait_osc_import_ready client ost1 ||
+		error "client import for OST0000 is not ready"
+}
+run_test 16c "lfs quota should preserve usage with an unavailable OST"
+
 test_17sub() {
 	local err_code=$1
 	local BLKS=1    # 1M less than limit