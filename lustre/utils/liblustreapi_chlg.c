lustre/lustre-release

commit 9c53ded1a70b99de70cc81ffe6a093592ee8c92a

LU-9653 utils: Add kernel doc style for utils (20)
This patch converts existing functional comments
to kernel doc style comments and removes '/**' for
comments which is not meant to be a kernel-doc comment

Test-Parameters: trivial
Signed-off-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Change-Id: I77ec164084a35c81c2d8f4184c7a6538abcd9c2a
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/66122
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/utils/liblustreapi_chlg.c
@@ -46,7 +46,7 @@ static int chlg_dev_path(char *path, size_t path_len, const char *device)
 #define CHANGELOG_PRIV_MAGIC 0xCA8E1080
 #define CHANGELOG_BUFFER_SZ  4096
 
-/**
+/*
  * Record state for efficient changelog consumption.
  * Read chunks of CHANGELOG_BUFFER_SZ bytes.
  */
@@ -68,13 +68,17 @@ struct changelog_private {
 };
 
 /**
- * Start reading from a changelog
+ * llapi_changelog_start() - Start reading from a changelog
+ * @priv: Opaque private control structure
+ * @flags: Start flags (e.g. CHANGELOG_FLAG_JOBID)
+ * @device: Report changes recorded on this MDT
+ * @startrec: Report changes beginning with this record number
  *
- * @param priv      Opaque private control structure
- * @param flags     Start flags (e.g. CHANGELOG_FLAG_JOBID)
- * @param device    Report changes recorded on this MDT
- * @param startrec  Report changes beginning with this record number
- * (just call llapi_changelog_fini when done; don't need an endrec)
+ * Just call llapi_changelog_fini when done; don't need an endrec
+ *
+ * Return:
+ * * %0 on success
+ * * %-errno on failure
  */
 int llapi_changelog_start(void **priv, enum changelog_send_flag flags,
 			  const char *device, long long startrec)
@@ -157,11 +161,12 @@ out_free_cp:
 }
 
 /**
- * Parse user string to extract numeric ID if possible
+ * changelog_parse_username() - Parse user string to extract numeric ID if
+ *                              possible
+ * @username: User name or ID (e.g. "cl1", "cl1-user", "1", or "user")
+ * @user_id: User ID number (0 will let MDT parse the username) [out]
  *
- * @param[in] username	User name or ID (e.g. "cl1", "cl1-user", "1", or "user")
- * @param[out] user_id	User ID number (0 will let MDT parse the username)
- * @return 0 on success, < 0 on error.
+ * Return %0 on success and %negative on error
  */
 static int changelog_parse_username(const char *username, __u32 *user_id)
 {
@@ -188,18 +193,19 @@ static int changelog_parse_username(const char *username, __u32 *user_id)
 }
 
 /**
- * Start reading changelog for a specific user with a specific mask
+ * llapi_changelog_start_user() - Start reading changelog for a specific user
+ *                                with a specific mask
+ * @priv: Opaque private control structure
+ * @flags: Start flags (e.g. CHANGELOG_FLAG_JOBID)
+ * @device: Report changes recorded on this MDT
+ * @username: Changelog user name or ID, e.g. "cl1", "cl1-user", "1", or "user"
+ * @mask: Changelog record mask in numeric form, 0 means the user's
+ *	    registered mask will be used.
+ *	    (Please see llapi_convert_str2mask/mask2str() for converting
+ *	    mask string to numeric form and vice versa.)
+ * @startrec: Report changes beginning with this record number
  *
- * @param priv      Opaque private control structure
- * @param flags     Start flags (e.g. CHANGELOG_FLAG_JOBID)
- * @param device    Report changes recorded on this MDT
- * @param username  Changelog user name or ID, e.g. "cl1", "cl1-user", "1", or
- *		    "user"
- * @param mask      Changelog record mask in numeric form, 0 means the user's
- *		    registered mask will be used.
- *		    (Please see llapi_convert_str2mask/mask2str() for converting
- *		    mask string to numeric form and vice versa.)
- * @param startrec  Report changes beginning with this record number
+ * Return %0 on success and %negative on error
  */
 int llapi_changelog_start_user(void **priv, enum changelog_send_flag flags,
 			       const char *device, const char *username,
@@ -268,10 +274,10 @@ static ssize_t chlg_read_bulk(struct changelog_private *cp)
 }
 
 /**
- * Returns a file descriptor to poll on.
+ * llapi_changelog_get_fd() - Returns a file descriptor to poll on.
+ * @priv:  Opaque changelog reader structure.
  *
- * \@param[in]  priv  Opaque changelog reader structure.
- * @return valid file descriptor on success, negated errno code on failure.
+ * Return valid file descriptor on success, %-errno code on failure.
  */
 int llapi_changelog_get_fd(void *priv)
 {
@@ -284,10 +290,17 @@ int llapi_changelog_get_fd(void *priv)
 }
 
 /**
+ * llapi_changelog_repack_rec() - create new changelog_rec
+ * @rec: changelog_rec sent by the kernel
+ * @crf_wanted: Flags (CLF_*) which should be copied to new changelog_rec
+ * @cref_want: Extra flags (CLFE_*) which should be copied to new changelog_rec
+ *
  * Messages containing changelog records sent by the kernel are collected in
  * llapi_changelog_recv(). Those messages contain everything that the kernel
  * can support. Our user land application might have no interest in many of
  * those extra fields so we then repack the rec with only what we want.
+ *
+ * Returns new changelog_rec or NULL on error
  */
 struct changelog_rec *
 llapi_changelog_repack_rec(const struct changelog_rec *rec,
@@ -511,12 +524,15 @@ out:
 }
 
 /**
- * Set extra flags for reading changelogs
- *
- * @param priv		Opaque private control structure
- * @param extra_flags	Read extra flags (e.g. CHANGELOG_EXTRA_FLAG_UIDGID)
+ * llapi_changelog_set_xflags() - Set extra flags for reading changelogs
+ * @priv: Opaque private control structure
+ * @extra_flags: Read extra flags (e.g. CHANGELOG_EXTRA_FLAG_UIDGID)
  *
  * Just call this function right after llapi_changelog_start().
+ *
+ * Return:
+ * * %0 on success
+ * * %negative on error
  */
 int llapi_changelog_set_xflags(void *priv,
 			       enum changelog_send_extra_flag extra_flags)

commit c2d8b9a0bebad1461f35345a24982d89e10db9c5

LU-9653 utils: fix up changelog kernel-doc
Give every documented function in liblustreapi_chlg.c a "Return:"
section, since kernel-doc only recognises the keyword with a colon,
and convert the leftover doxygen and plain comments on
llapi_changelog_recv(), _fini() and _free().  Use %-errno or the
actual %-EINVAL rather than the non-symbol %negative, and realign the
@mask continuation lines under their new prefix.
changelog_parse_username() has no failure path, so document that it
always returns 0.  DEFAULT_RECORD_FMT moves above the new block
because kernel-doc binds it to the next prototype.

Test-Parameters: trivial
Fixes: 9c53ded1a70b ("LU-9653 utils: Add kernel doc style for utils (20)")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: Ib76bedaa810aadcf00109945395a5def84949f53
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/68235
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Anjus George <georgea@ornl.gov>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>

lustre/utils/liblustreapi_chlg.c
@@ -166,7 +166,10 @@ out_free_cp:
  * @username: User name or ID (e.g. "cl1", "cl1-user", "1", or "user")
  * @user_id: User ID number (0 will let MDT parse the username) [out]
  *
- * Return %0 on success and %negative on error
+ * An unparsable @username is not an error: @user_id is set to %0 and the
+ * MDT is left to resolve the name itself.
+ *
+ * Return: %0 always
  */
 static int changelog_parse_username(const char *username, __u32 *user_id)
 {
@@ -200,12 +203,14 @@ static int changelog_parse_username(const char *username, __u32 *user_id)
  * @device: Report changes recorded on this MDT
  * @username: Changelog user name or ID, e.g. "cl1", "cl1-user", "1", or "user"
  * @mask: Changelog record mask in numeric form, 0 means the user's
- *	    registered mask will be used.
- *	    (Please see llapi_convert_str2mask/mask2str() for converting
- *	    mask string to numeric form and vice versa.)
+ *        registered mask will be used.
+ *        (Please see llapi_convert_str2mask/mask2str() for converting
+ *        mask string to numeric form and vice versa.)
  * @startrec: Report changes beginning with this record number
  *
- * Return %0 on success and %negative on error
+ * Return:
+ * * %0 on success
+ * * %-errno on failure
  */
 int llapi_changelog_start_user(void **priv, enum changelog_send_flag flags,
 			       const char *device, const char *username,
@@ -242,7 +247,14 @@ int llapi_changelog_start_user(void **priv, enum changelog_send_flag flags,
 	return rc < 0 ? -errno : 0;
 }
 
-/** Finish reading from a changelog */
+/**
+ * llapi_changelog_fini() - Finish reading from a changelog
+ * @priv: Opaque private control structure, set to %NULL on success [in, out]
+ *
+ * Return:
+ * * %0 on success
+ * * %-EINVAL if @priv does not point at a changelog reader
+ */
 int llapi_changelog_fini(void **priv)
 {
 	struct changelog_private *cp = *priv;
@@ -277,7 +289,9 @@ static ssize_t chlg_read_bulk(struct changelog_private *cp)
  * llapi_changelog_get_fd() - Returns a file descriptor to poll on.
  * @priv:  Opaque changelog reader structure.
  *
- * Return valid file descriptor on success, %-errno code on failure.
+ * Return:
+ * * valid file descriptor on success
+ * * %-EINVAL if @priv is not a changelog reader
  */
 int llapi_changelog_get_fd(void *priv)
 {
@@ -300,7 +314,9 @@ int llapi_changelog_get_fd(void *priv)
  * can support. Our user land application might have no interest in many of
  * those extra fields so we then repack the rec with only what we want.
  *
- * Returns new changelog_rec or NULL on error
+ * Return:
+ * * new changelog_rec on success
+ * * %NULL on failure
  */
 struct changelog_rec *
 llapi_changelog_repack_rec(const struct changelog_rec *rec,
@@ -387,14 +403,18 @@ no_extras:
 	return new_rec;
 }
 
-/** Read the next changelog entry
- * @param priv Opaque private control structure
- * @param rech Changelog record handle; record will be allocated here
- * @return 0 valid message received; rec is set
- *	 <0 error code
- *	 1 EOF
- */
 #define DEFAULT_RECORD_FMT	(CLF_VERSION | CLF_RENAME)
+
+/**
+ * llapi_changelog_recv() - Read the next changelog entry
+ * @priv: Opaque private control structure
+ * @rech: Changelog record handle; record will be allocated here
+ *
+ * Return:
+ * * %0 on success
+ * * %1 end of the changelog
+ * * %-errno on failure
+ */
 int llapi_changelog_recv(void *priv, struct changelog_rec **rech)
 {
 	struct changelog_private *cp = priv;
@@ -449,7 +469,12 @@ out:
 	return rc;
 }
 
-/** Release the changelog record when done with it. */
+/**
+ * llapi_changelog_free() - Release the changelog record when done with it
+ * @rech: Changelog record handle to release, set to %NULL [in, out]
+ *
+ * Return: %0 always
+ */
 int llapi_changelog_free(struct changelog_rec **rech)
 {
 	free(*rech);
@@ -532,7 +557,7 @@ out:
  *
  * Return:
  * * %0 on success
- * * %negative on error
+ * * %-EINVAL if @priv is not a changelog reader
  */
 int llapi_changelog_set_xflags(void *priv,
 			       enum changelog_send_extra_flag extra_flags)