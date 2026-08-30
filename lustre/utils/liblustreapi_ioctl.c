lustre/lustre-release

commit 594b780062db7e015837faefa75f7352293f54ae

LU-16634 misc: replace obsolete ioctl numbers
Renumber the OBD_IOC_BARRIER and IOC_OSC_SET_ACTIVE ioctl numbers
to use IOC type 'f' instead of 'g' and 'h', respectively.  Add
compatibility code to handle their deprecation in the future.

Move already-deprecated ioctls OBD_IOC_GETNAME_OLD, OBD_GET_VERSION,
IOC_LIBCFS_GET_NI, and IOC_LIBCFS_PING to lustre_ioctl_old.h also.
Remove other already obsolete ioctl number definitions.

Add new llapi_ioctl() and llapi_ioctl_dev() helper functions to
handle fallback of ioctl commands to older versions when needed.

Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: I519f23d34ce6197f3430a3825c5190cedb00cf14
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50390
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Reviewed-by: Vitaliy Kuznetsov <vkuznetsov@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/utils/liblustreapi_ioctl.c
@@ -1,0 +1,222 @@
+/*
+ * LGPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 or (at your discretion) any later version.
+ * (LGPL) version 2.1 accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * LGPL HEADER END
+ */
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * lustre/utils/liblustreapi_ioctl.c
+ *
+ * lustreapi library for packing/unpacking obd_ioctl_data structure to
+ * send commands to different OBD devices.  Mostly for internal use.
+ *
+ * Copyright (c) 2003, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Use is subject to license terms.
+ *
+ * Copyright (c) 2011, 2017, Intel Corporation.
+ *
+ * Copyright (c) 2023, DataDirect Networks Storage.
+ */
+
+#include <string.h>
+#include <stddef.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
+#include <lustre/lustreapi.h>
+#include <libcfs/util/ioctl.h>
+#include <linux/lustre/lustre_ioctl.h>
+#include <lustre_ioctl_old.h>
+#include <linux/lustre/lustre_ver.h>
+
+int llapi_ioctl_pack(struct obd_ioctl_data *data, char **pbuf, int max_len)
+{
+	struct obd_ioctl_data *overlay;
+	char *ptr;
+
+	data->ioc_len = obd_ioctl_packlen(data);
+	data->ioc_version = OBD_IOCTL_VERSION;
+
+	if (*pbuf != NULL && data->ioc_len > max_len) {
+		llapi_error(LLAPI_MSG_ERROR, -EINVAL,
+			    "pbuf = %p, ioc_len = %u, max_len = %d",
+			    *pbuf, data->ioc_len, max_len);
+		return -EINVAL;
+	}
+
+	if (*pbuf == NULL)
+		*pbuf = malloc(data->ioc_len);
+
+	if (*pbuf == NULL)
+		return -ENOMEM;
+
+	overlay = (struct obd_ioctl_data *)*pbuf;
+	memcpy(*pbuf, data, sizeof(*data));
+
+	ptr = overlay->ioc_bulk;
+	if (data->ioc_inlbuf1) {
+		memcpy(ptr, data->ioc_inlbuf1, data->ioc_inllen1);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen1, 8);
+	}
+
+	if (data->ioc_inlbuf2) {
+		memcpy(ptr, data->ioc_inlbuf2, data->ioc_inllen2);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen2, 8);
+	}
+
+	if (data->ioc_inlbuf3) {
+		memcpy(ptr, data->ioc_inlbuf3, data->ioc_inllen3);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen3, 8);
+	}
+
+	if (data->ioc_inlbuf4) {
+		memcpy(ptr, data->ioc_inlbuf4, data->ioc_inllen4);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen4, 8);
+	}
+
+	return 0;
+}
+
+/**
+ * Remap OBD device ioctl cmd to old one in case running with older modules.
+ * Replaces callers that use "l_ioctl(OBD_DEV_ID, ...)".
+ *
+ * \param dev_id	Lustre device number (from 'lctl dl')
+ * \param cmd		ioctl command
+ * \param buf		ioctl data argument, usually obd_ioctl_data
+ */
+int llapi_ioctl_dev(unsigned int dev_id, unsigned int cmd, void *buf)
+{
+	unsigned int oldcmd;
+	int rc;
+
+	/* common case, ioctl works as expected */
+	rc = l_ioctl(dev_id, cmd, buf);
+	if (rc >= 0 || errno != ENOTTY)
+		return rc;
+
+	switch (cmd) {
+	/*
+	 * Use #ifdef instead of version check to minimize the places that a
+	 * version change might cause a compiler error in the future.
+	 *
+	 * Version in comment is to allow finding this code for later removal.
+	 */
+#ifdef OBD_IOC_BARRIER		/* < OBD_OCD_VERSION(2, 19, 53, 0) */
+	case OBD_IOC_BARRIER_V2:
+		oldcmd = OBD_IOC_BARRIER;
+		break;
+#endif
+#ifdef IOC_OSC_SET_ACTIVE	/* < OBD_OCD_VERSION(2, 19, 53, 0) */
+	case OBD_IOC_SET_ACTIVE:
+		oldcmd = IOC_OSC_SET_ACTIVE;
+		break;
+#endif
+	default:
+		oldcmd = 0;
+		break;
+	}
+	if (oldcmd)
+		rc = l_ioctl(dev_id, oldcmd, buf);
+
+	return rc;
+}
+
+/**
+ * Remap regular file ioctl cmd to old one in case running with older modules.
+ * Replaces callers that use "ioctl(fd, ...)".
+ *
+ * \param fd		open file descriptor
+ * \param cmd		ioctl command
+ * \param buf		ioctl data argument
+ */
+int llapi_ioctl(int fd, unsigned int cmd, void *buf)
+{
+	unsigned int oldcmd;
+	int rc;
+
+	if (fd < 0)
+		return -EBADF;
+
+	/* common case, ioctl works as expected */
+	rc = ioctl(fd, cmd, buf);
+	if (rc >= 0 || errno != ENOTTY)
+		return rc;
+
+	switch (cmd) {
+	/*
+	 * Use #ifdef instead of version check to minimize the places that a
+	 * version change might cause a compiler error in the future.
+	 *
+	 * Version in comment is to allow finding this code for later removal.
+	 */
+#ifdef OBD_IOC_GETNAME_OLD	/* < OBD_OCD_VERSION(2, 18, 53, 0) */
+	case OBD_IOC_GETMDNAME:
+		oldcmd = OBD_IOC_GETNAME_OLD;
+		break;
+#endif
+	default:
+		oldcmd = 0;
+		break;
+	}
+	if (oldcmd)
+		rc = ioctl(fd, oldcmd, buf);
+
+	return rc;
+}
+
+int llapi_ioctl_unpack(struct obd_ioctl_data *data, char *pbuf, int max_len)
+{
+	struct obd_ioctl_data *overlay;
+	char *ptr;
+
+	if (pbuf == NULL)
+		return -EINVAL;
+
+	overlay = (struct obd_ioctl_data *)pbuf;
+
+	/* Preserve the caller's buffer pointers */
+	overlay->ioc_inlbuf1 = data->ioc_inlbuf1;
+	overlay->ioc_inlbuf2 = data->ioc_inlbuf2;
+	overlay->ioc_inlbuf3 = data->ioc_inlbuf3;
+	overlay->ioc_inlbuf4 = data->ioc_inlbuf4;
+
+	memcpy(data, pbuf, sizeof(*data));
+
+	ptr = overlay->ioc_bulk;
+	if (data->ioc_inlbuf1) {
+		memcpy(data->ioc_inlbuf1, ptr, data->ioc_inllen1);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen1, 8);
+	}
+
+	if (data->ioc_inlbuf2) {
+		memcpy(data->ioc_inlbuf2, ptr, data->ioc_inllen2);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen2, 8);
+	}
+
+	if (data->ioc_inlbuf3) {
+		memcpy(data->ioc_inlbuf3, ptr, data->ioc_inllen3);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen3, 8);
+	}
+
+	if (data->ioc_inlbuf4) {
+		memcpy(data->ioc_inlbuf4, ptr, data->ioc_inllen4);
+		ptr += __ALIGN_KERNEL(data->ioc_inllen4, 8);
+	}
+
+	return 0;
+}

lnet/include/uapi/linux/lnet/libcfs_ioctl.h
@@ -86,20 +86,19 @@ struct libcfs_ioctl_data {
 #define IOC_LIBCFS_TYPE				      ('e')
 #define IOC_LIBCFS_MIN_NR			      30
 /* libcfs ioctls */
-/* IOC_LIBCFS_PANIC obsolete in 2.8.0, was _IOWR('e', 30, IOCTL_LIBCFS_TYPE) */
+/* XXX: all of these ioctl definitions are broken, the first parameter
+ * should be IOCTL_LIBCFS_TYPE and the last parameter is the struct size.
+ */
 #define IOC_LIBCFS_CLEAR_DEBUG             _IOWR('e', 31, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_MARK_DEBUG              _IOWR('e', 32, IOCTL_LIBCFS_TYPE)
-/* IOC_LIBCFS_MEMHOG obsolete in 2.8.0, was _IOWR('e', 36, IOCTL_LIBCFS_TYPE) */
 /* lnet ioctls */
 #define IOC_LIBCFS_GET_NI		   _IOWR('e', 50, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_FAIL_NID		   _IOWR('e', 51, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_NOTIFY_ROUTER	   _IOWR('e', 55, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_UNCONFIGURE		   _IOWR('e', 56, IOCTL_LIBCFS_TYPE)
-/*	 IOC_LIBCFS_PORTALS_COMPATIBILITY  _IOWR('e', 57, IOCTL_LIBCFS_TYPE) */
 #define IOC_LIBCFS_LNET_DIST		   _IOWR('e', 58, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_CONFIGURE		   _IOWR('e', 59, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_TESTPROTOCOMPAT	   _IOWR('e', 60, IOCTL_LIBCFS_TYPE)
-/* IOC_LIBCFS_PING obsolete in 2.16, was _IOWR('e', 61, IOCTL_LIBCFS_TYPE) */
 #define IOC_LIBCFS_PING_PEER               _IOWR('e', 62, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_LNETST		   _IOWR('e', 63, IOCTL_LIBCFS_TYPE)
 #define IOC_LIBCFS_LNET_FAULT		   _IOWR('e', 64, IOCTL_LIBCFS_TYPE)

lustre/doc/Makefile.am
@@ -143,6 +143,7 @@ LIBMAN = 					\
 	llapi_hsm_copytool_unregister.3		\
 	llapi_hsm_state_get.3			\
 	llapi_hsm_state_set.3			\
+	llapi_ioctl.3				\
 	llapi_ladvise.3				\
 	llapi_layout.7				\
 	llapi_layout_alloc.3			\

lustre/doc/llapi_ioctl.3
@@ -1,0 +1,69 @@
+.TH lustreapi 3 "2023 Mar 22" Lustre user application interface library
+.SH NAME
+llapi_ioctl \- call ioctl() with possible command remapping
+.SH SYNOPSIS
+.nf
+.B #include <lustre/lustreapi.h>
+.sp
+.BI "int llapi_ioctl(int " fd ", unsigned int " cmd ", void *" buf ");"
+.SH DESCRIPTION
+.LP
+.B llapi_ioctl()
+calls the
+.BR ioctl (2)
+system call with the supplied arguments.  However, if the ioctl() returns
+.B ENOTTY
+(no such ioctl) then if
+.I cmd
+has been remapped from an older ioctl command number the ioctl() call will
+be retried with the old ioctl command number.
+.SH RETURN VALUES
+.LP
+.B llapi_ioctl()
+typically returns:
+.TP
+0
+on success in most cases
+.TP
+<0
+a negative value on failure and sets
+.BR errno .
+Due to the variety of ioctl commands, it may return different values
+and errno as appropriate for the
+.IR fd ,
+.I cmd
+number, and argument
+.I buf
+passed.
+.SH "EXAMPLE"
+.nf
+#include <lustre/lustreapi.h>
+
+int main(int argc, char *argv[])
+{
+        unsigned long group = 1234;
+        int rc, fd;
+
+        fd = open(argv[1], O_RDONLY);
+        if (rc < 0) {
+                fprintf(stderr, "error opening file '%s': %s\\n",
+                        argv[1], strerror(errno));
+                return -1;
+        }
+
+        rc = llapi_ioctl(fd, LL_IOC_GROUP_LOCK, &group);
+        if (rc < 0) {
+                fprintf(stderr, "%s: ioctl error: %s\\n",
+                        argv[1], strerror(errno));
+                return rc;
+        }
+
+        printf("%s: locked with group %lu\\n", argv[1], group);
+        return 0;
+}
+.fi
+.SH "SEE ALSO"
+.BR lustre (7),
+.BR ioctl (2),
+.BR lustreapi (7)
+

lustre/include/Makefile.am
@@ -60,6 +60,7 @@ EXTRA_DIST = \
 	lustre_idmap.h \
 	lustre_import.h \
 	lustre_intent.h \
+	lustre_ioctl_old.h \
 	lustre_lfsck.h \
 	lustre_lib.h \
 	lustre_linkea.h \

lustre/include/lustre/lustreapi.h
@@ -1259,6 +1259,8 @@ int llapi_mirror_punch(int fd, unsigned int id, off_t start, size_t length);
 int llapi_heat_get(int fd, struct lu_heat *heat);
 int llapi_heat_set(int fd, __u64 flags);
 
+int llapi_ioctl(int fd, unsigned int cmd, void *buf);
+
 int llapi_layout_sanity(struct llapi_layout *layout, bool incomplete, bool flr);
 void llapi_layout_sanity_perror(int error);
 int llapi_layout_dom_size(struct llapi_layout *layout, uint64_t *size);

lustre/include/lustre_ioctl_old.h
@@ -1,0 +1,84 @@
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * GPL HEADER END
+ */
+/*
+ * This file is part of Lustre, http://www.lustre.org/
+ *
+ * Compatibility for deprecated ioctls that should no longer be used by tools.
+ *
+ * Copyright (c) 2023, DataDirect Networks Storage, all rights reserved.
+ */
+#ifndef __LUSTRE_IOCTL_OLD_H
+#define __LUSTRE_IOCTL_OLD_H
+
+#include <linux/lnet/libcfs_ioctl.h> /* for IOCTL_LIBCFS_TYPE */
+
+/* ioctl command is deprecated after release v1.v2 */
+#define case_OBD_IOC_DEPRECATED(cmd, dev, v1, v2)			\
+	case cmd:							\
+	if (LUSTRE_VERSION_CODE > OBD_OCD_VERSION(v1, v2, 53, 0)) {	\
+		static bool printed;					\
+		obd_ioctl_msg(__FILE__, __func__, __LINE__,		\
+			      printed ? D_IOCTL : D_WARNING, dev, cmd,	\
+			      "deprecated " #cmd " usage", 0);		\
+		printed = true;						\
+	}
+
+#define case_OBD_IOC_DEPRECATED_FT(cmd, dev, v1, v2)			\
+	case_OBD_IOC_DEPRECATED(cmd, dev, v1, v2)			\
+	fallthrough
+
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
+#define OBD_GET_VERSION		_IOWR('f', 144, OBD_IOC_DATA_TYPE) /*< 2.8.55 */
+#endif
+
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 99, 53, 0)
+/* for binary compatibility until 3.0, no more compiling into tools */
+#define OBD_IOC_GETNAME_OLD	_IOWR('f', 131, OBD_IOC_DATA_TYPE) /*< 2.14.52*/
+
+#define IOC_LIBCFS_GET_NI	_IOWR('e', 50, IOCTL_LIBCFS_TYPE)  /*< 2.15.53*/
+#define IOC_LIBCFS_PING		_IOWR('e', 61, IOCTL_LIBCFS_TYPE)  /*< 2.15.53*/
+
+#if LUSTRE_VERSION_CODE >= OBD_OCD_VERSION(2, 19, 53, 0)
+#define OBD_IOC_BARRIER		_IOWR('g', 5, OBD_IOC_DATA_TYPE)   /*< 2.16.55*/
+#define IOC_OSC_SET_ACTIVE	_IOWR('h', 21, void *)		   /*< 2.16.55*/
+#endif
+
+#endif /* OBD_OCD_VERSION(2, 99, 53, 0) */
+
+/* We don't need *_ALLOW() macros for most ioctls, just a few using a bad
+ * _IOC_TYPE value (i.e. not 'f') so that "early exit" type checks work.
+ */
+#define OBD_IOC_CMD_LATE(cmd, name) unlikely(cmd == name)
+#define OBD_IOC_CMD_GONE(cmd, name) (false)
+
+#ifdef OBD_IOC_BARRIER
+#define OBD_IOC_BARRIER_ALLOW(cmd) OBD_IOC_CMD_LATE(cmd, OBD_IOC_BARRIER)
+#else
+#define OBD_IOC_BARRIER_ALLOW(cmd) OBD_IOC_CMD_GONE(cmd)
+#endif
+#ifdef IOC_OSC_SET_ACTIVE
+#define IOC_OSC_SET_ACTIVE_ALLOW(cmd) OBD_IOC_CMD_LATE(cmd, IOC_OSC_SET_ACTIVE)
+#else
+#define IOC_OSC_SET_ACTIVE_ALLOW(cmd) OBD_IOC_CMD_GONE(cmd)
+#endif
+
+#endif /* __LUSTRE_IOCTL_OLD_H */

lustre/include/uapi/linux/lustre/lustre_ioctl.h
@@ -32,6 +32,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/lustre/lustre_idl.h>
+#include <linux/lustre/lustre_ver.h>
 
 /*
  * sparse kernel source annotations
@@ -134,7 +135,14 @@ static inline __u32 obd_ioctl_packlen(struct obd_ioctl_data *data)
 
 #define OBD_IOC_CREATE		_IOWR('f', 101, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_DESTROY		_IOW('f', 104, OBD_IOC_DATA_TYPE)
-/*	OBD_IOC_PREALLOCATE	_IOWR('f', 105, OBD_IOC_DATA_TYPE) */
+#define OBD_IOC_BARRIER_V2	_IOW('f', 105, struct obd_ioctl_data)
+#define OBD_IOC_SET_ACTIVE	_IOW('f', 106, struct obd_ioctl_data)
+#if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 19, 53, 0)
+/* for API compatibility until 2.19.x, but prefer to use new IOC values above */
+/* OBD_IOC_BARRIER wrongly defined as _IOWR('f', 261, OBD_IOC_DATA_TYPE) */
+#define OBD_IOC_BARRIER		_IOWR('g', 5, OBD_IOC_DATA_TYPE) /* < 2.16 */
+#define IOC_OSC_SET_ACTIVE	_IOWR('h', 21, void *) /* deprecated 2.16 */
+#endif
 
 #define OBD_IOC_SETATTR		_IOW('f', 107, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_GETATTR		_IOWR('f', 108, OBD_IOC_DATA_TYPE)
@@ -150,14 +158,9 @@ static inline __u32 obd_ioctl_packlen(struct obd_ioctl_data *data)
 #define OBD_IOC_GETDTNAME	_IOR('f', 127, char[MAX_OBD_NAME])
 /* ioctl codes 128-143 are reserved for fsverity */
 #define OBD_IOC_UUID2DEV	_IOWR('f', 130, OBD_IOC_DATA_TYPE)
-#define OBD_IOC_GETNAME_OLD	_IOWR('f', 131, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_GETMDNAME	_IOR('f', 131, char[MAX_OBD_NAME])
-/*      OBD_IOC_LOV_GET_CONFIG	_IOWR('f', 132, OBD_IOC_DATA_TYPE) until 2.14 */
 #define OBD_IOC_CLIENT_RECOVER	_IOW('f', 133, OBD_IOC_DATA_TYPE)
 /* ioctl codes 128-143 are reserved for fsverity */
-/* FS_IOC_ENABLE_VERITY		_IOW('f', 133, struct fsverity_enable_arg) */
-/* FS_IOC_MEASURE_VERITY	_IOW('f', 134, struct fsverity_digest) */
-/* was	OBD_IOC_NO_TRANSNO	_IOW('f', 140, OBD_IOC_DATA_TYPE) until 2.14 */
 #define OBD_IOC_SET_READONLY	_IOW('f', 141, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_ABORT_RECOVERY	_IOR('f', 142, OBD_IOC_DATA_TYPE)
 enum obd_abort_recovery_flags {
@@ -165,31 +168,15 @@ enum obd_abort_recovery_flags {
 	OBD_FLG_ABORT_RECOV_MDT	= 0x40000, /* LMD_FLG_ABORT_RECOV_MDT */
 };
 /* ioctl codes 128-143 are reserved for fsverity */
-#define OBD_GET_VERSION		_IOWR('f', 144, OBD_IOC_DATA_TYPE)
-/*	OBD_IOC_GSS_SUPPORT	_IOWR('f', 145, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_CLOSE_UUID	_IOWR('f', 147, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_CHANGELOG_SEND	_IOW('f', 148, OBD_IOC_DATA_TYPE) */
 #define OBD_IOC_GETDEVICE	_IOWR('f', 149, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_FID2PATH	_IOWR('f', 150, OBD_IOC_DATA_TYPE)
-/*	lustre/lustre_user.h	151-153 */
-/*	OBD_IOC_LOV_SETSTRIPE	154 LL_IOC_LOV_SETSTRIPE */
-/*	OBD_IOC_LOV_GETSTRIPE	155 LL_IOC_LOV_GETSTRIPE */
-/*	OBD_IOC_LOV_SETEA	156 LL_IOC_LOV_SETEA */
-/*	lustre/lustre_user.h	157-159 */
-/*	OBD_IOC_QUOTACHECK	_IOW('f', 160, int) */
-/*	OBD_IOC_POLL_QUOTACHECK	_IOR('f', 161, struct if_quotacheck *) */
+/*	lustre/lustre_user.h	151-159 */
 #define OBD_IOC_QUOTACTL	_IOWR('f', 162, struct if_quotactl)
 /*	lustre/lustre_user.h	163-176 */
 #define OBD_IOC_CHANGELOG_REG	_IOW('f', 177, struct obd_ioctl_data)
 #define OBD_IOC_CHANGELOG_DEREG	_IOW('f', 178, struct obd_ioctl_data)
 #define OBD_IOC_CHANGELOG_CLEAR	_IOW('f', 179, struct obd_ioctl_data)
-/*	OBD_IOC_RECORD		_IOWR('f', 180, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_ENDRECORD	_IOWR('f', 181, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_PARSE		_IOWR('f', 182, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_DORECORD	_IOWR('f', 183, OBD_IOC_DATA_TYPE) */
 #define OBD_IOC_PROCESS_CFG	_IOWR('f', 184, OBD_IOC_DATA_TYPE)
-/*	OBD_IOC_DUMP_LOG	_IOWR('f', 185, OBD_IOC_DATA_TYPE) */
-/*	OBD_IOC_CLEAR_LOG	_IOWR('f', 186, OBD_IOC_DATA_TYPE) */
 #define OBD_IOC_PARAM		_IOW('f', 187, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_POOL		_IOWR('f', 188, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_REPLACE_NIDS	_IOWR('f', 189, OBD_IOC_DATA_TYPE)
@@ -200,33 +187,18 @@ enum obd_abort_recovery_flags {
 #define OBD_IOC_LLOG_CANCEL	_IOWR('f', 193, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_LLOG_REMOVE	_IOWR('f', 194, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_LLOG_CHECK	_IOWR('f', 195, OBD_IOC_DATA_TYPE)
-/*	OBD_IOC_LLOG_CATINFO	_IOWR('f', 196, OBD_IOC_DATA_TYPE) */
 #define OBD_IOC_NODEMAP		_IOWR('f', 197, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_CLEAR_CONFIGS   _IOWR('f', 198, OBD_IOC_DATA_TYPE)
-
-/*	ECHO_IOC_GET_STRIPE	_IOWR('f', 200, OBD_IOC_DATA_TYPE) */
-/*	ECHO_IOC_SET_STRIPE	_IOWR('f', 201, OBD_IOC_DATA_TYPE) */
-/*	ECHO_IOC_ENQUEUE	_IOWR('f', 202, OBD_IOC_DATA_TYPE) */
-/*	ECHO_IOC_CANCEL		_IOWR('f', 203, OBD_IOC_DATA_TYPE) */
-
 #define OBD_IOC_LCFG_FORK	_IOWR('f', 208, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_LCFG_ERASE	_IOWR('f', 209, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_GET_OBJ_VERSION	_IOR('f', 210, OBD_IOC_DATA_TYPE)
-
 /*	lustre/lustre_user.h	211-220 */
-/* was #define OBD_IOC_GET_MNTOPT	_IOW('f', 220, mntopt_t) until 2.11 */
 #define OBD_IOC_ECHO_MD		_IOR('f', 221, struct obd_ioctl_data)
 #define OBD_IOC_ECHO_ALLOC_SEQ	_IOWR('f', 222, struct obd_ioctl_data)
 #define OBD_IOC_START_LFSCK	_IOWR('f', 230, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_STOP_LFSCK	_IOW('f', 231, OBD_IOC_DATA_TYPE)
 #define OBD_IOC_QUERY_LFSCK	_IOR('f', 232, struct obd_ioctl_data)
 #define OBD_IOC_CHLG_POLL	_IOR('f', 233, long)
-/*	lustre/lustre_user.h	240-249 */
-/* was	LIBCFS_IOC_DEBUG_MASK	_IOWR('f', 250, long) until 2.11 */
-
-/* OBD_IOC_BARRIER wrongly defined as _IOWR('f', 261, OBD_IOC_DATA_TYPE) */
-#define OBD_IOC_BARRIER		_IOWR('g', 5, OBD_IOC_DATA_TYPE)
-
-#define IOC_OSC_SET_ACTIVE	_IOWR('h', 21, void *)
+/*	lustre/lustre_user.h	240-253 */
 
 #endif /* _UAPI_LUSTRE_IOCTL_H */

commit 2b1bdb02dedb4a1b5fcd9a9ed8d3eb31d6e38b90

LU-17000 utils: set errno when llapi_ioctl() rejects a bad fd
llapi_ioctl() short-circuits a negative descriptor with -EBADF but
leaves errno untouched, so its callers' "rc = -errno" idiom turns the
failure into success whenever errno happens to be zero.  Set errno on
that path, as the documented return contract already promises and as
the sibling l_ioctl() helper already does.

Test-Parameters: trivial
Fixes: 594b780062db ("LU-16634 misc: replace obsolete ioctl numbers")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Assisted-by: ClaudeCode:Opus-5 llm_code_and_review_tools
Change-Id: I9f3708e9a8d82005bfcd9f450caf1b50fd7eee5c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/68239
Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/utils/liblustreapi_ioctl.c
@@ -132,8 +132,10 @@ int llapi_ioctl(int fd, unsigned int cmd, void *buf)
 	unsigned int oldcmd;
 	int rc;
 
-	if (fd < 0)
+	if (fd < 0) {
+		errno = EBADF;
 		return -EBADF;
+	}
 
 	/* common case, ioctl works as expected */
 	rc = ioctl(fd, cmd, buf);