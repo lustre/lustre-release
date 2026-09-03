lustre/lustre-release

commit 93a6346f8b73f68cb5bc02a3c826ac0e5b4c236e

LU-2145 server: use unified request handler for MGS
- Unify request handler. It finds target for particular request and
  calls appropriate handler for it. Generic handlers are moved to
  the unified target code. The tgt_session_info is introduced to
  store all request-related data and passed to all handlers.
- Pack reply in llog server functions early and use err_serious()
- remove obsoleted llog_origin_handle_cancel(), it is not used
  anymore
- remove push_ctxt/pop_ctxt from llog server function, it is based
  on OSD now.

Change-Id: Idded90d8112bcab74de9a58e7885fa17dd3340b8
Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
Reviewed-on: http://review.whamcloud.com/4826
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
Tested-by: Hudson
Tested-by: Maloo <whamcloud.maloo@gmail.com>
Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
Reviewed-by: James Simmons <uja.ornl@gmail.com>

lustre/mgs/lproc_mgs.c
@@ -116,30 +116,14 @@ static void seq_show_srpc_rules(struct seq_file *seq, const char *tgtname,
 
 static int mgsself_srpc_seq_show(struct seq_file *seq, void *v)
 {
-        struct obd_device *obd = seq->private;
-	struct mgs_device *mgs;
-        struct fs_db      *fsdb;
-	struct lu_env	   env;
-        int                rc;
+	struct obd_device	*obd = seq->private;
+	struct mgs_device	*mgs = lu2mgs_dev(obd->obd_lu_dev);
+	struct lu_target	*tgt = &mgs->mgs_lut;
 
-	LASSERT(obd != NULL);
-	LASSERT(obd->obd_lu_dev != NULL);
-	mgs = lu2mgs_dev(obd->obd_lu_dev);
+	read_lock(&tgt->lut_sptlrpc_lock);
+	seq_show_srpc_rules(seq, MGSSELF_NAME, &tgt->lut_sptlrpc_rset);
+	read_unlock(&tgt->lut_sptlrpc_lock);
 
-	rc = lu_env_init(&env, LCT_MG_THREAD);
-	if (rc)
-		return rc;
-
-	rc = mgs_find_or_make_fsdb(&env, mgs, MGSSELF_NAME, &fsdb);
-        if (rc)
-		goto out;
-
-	mutex_lock(&fsdb->fsdb_mutex);
-        seq_show_srpc_rules(seq, fsdb->fsdb_name, &fsdb->fsdb_srpc_gen);
-	mutex_unlock(&fsdb->fsdb_mutex);
-
-out:
-	lu_env_fini(&env);
         return 0;
 }
 

lustre/mgs/mgs_internal.h
@@ -173,6 +173,7 @@ struct mgs_device {
 	struct obd_device		*mgs_obd;
 	struct local_oid_storage	*mgs_los;
 	struct mutex			 mgs_mutex;
+	struct lu_target		 mgs_lut;
 };
 
 /* this is a top object */
@@ -290,9 +291,16 @@ extern struct lu_context_key mgs_thread_key;
 
 static inline struct mgs_thread_info *mgs_env_info(const struct lu_env *env)
 {
-	struct mgs_thread_info *info;
+	struct mgs_thread_info	*info;
+	int			rc;
 
 	info = lu_context_key_get(&env->le_ctx, &mgs_thread_key);
+	if (info == NULL) {
+		rc = lu_env_refill((struct lu_env *)env);
+		if (rc != 0)
+			return ERR_PTR(rc);
+		info = lu_context_key_get(&env->le_ctx, &mgs_thread_key);
+	}
 	LASSERT(info != NULL);
 	return info;
 }

lustre/include/lu_target.h
@@ -38,40 +38,184 @@
 #define _LUSTRE_LU_TARGET_H
 
 #include <dt_object.h>
+#include <lustre_export.h>
 #include <lustre_disk.h>
 
 struct lu_target {
-        struct obd_device       *lut_obd;
-        struct dt_device        *lut_bottom;
-        /** last_rcvd file */
-        struct dt_object        *lut_last_rcvd;
-        /* transaction callbacks */
-        struct dt_txn_callback   lut_txn_cb;
-        /** server data in last_rcvd file */
-        struct lr_server_data    lut_lsd;
-        /** Server last transaction number */
-        __u64                    lut_last_transno;
-        /** Lock protecting last transaction number */
+	struct obd_device	*lut_obd;
+	struct dt_device	*lut_bottom;
+
+	/* supported opcodes and handlers for this target */
+	struct tgt_opc_slice	*lut_slice;
+	__u32			 lut_reply_fail_id;
+	__u32			 lut_request_fail_id;
+
+	/* sptlrpc rules */
+	rwlock_t		 lut_sptlrpc_lock;
+	struct sptlrpc_rule_set	 lut_sptlrpc_rset;
+	int			 lut_sec_level;
+	unsigned int		 lut_mds_capa:1,
+				 lut_oss_capa:1;
+
+	/* LAST_RCVD parameters */
+	/** last_rcvd file */
+	struct dt_object	*lut_last_rcvd;
+	/* transaction callbacks */
+	struct dt_txn_callback	 lut_txn_cb;
+	/** server data in last_rcvd file */
+	struct lr_server_data	 lut_lsd;
+	/** Server last transaction number */
+	__u64			 lut_last_transno;
+	/** Lock protecting last transaction number */
 	spinlock_t		 lut_translock;
 	/** Lock protecting client bitmap */
 	spinlock_t		 lut_client_bitmap_lock;
 	/** Bitmap of known clients */
-	unsigned long           *lut_client_bitmap;
+	unsigned long		*lut_client_bitmap;
 };
+
+extern struct lu_context_key tgt_session_key;
+
+struct tgt_session_info {
+	/*
+	 * The following members will be filled explicitly
+	 * with specific data in tgt_ses_init().
+	 */
+	struct req_capsule	*tsi_pill;
+
+	/*
+	 * Lock request for "habeo clavis" operations.
+	 */
+	struct ldlm_request	*tsi_dlm_req;
+
+	/* although we have export in req, there are cases when it is not
+	 * available, e.g. closing files upon export destroy */
+	struct obd_export	*tsi_exp;
+	const struct lu_env	*tsi_env;
+	struct lu_target	*tsi_tgt;
+	/*
+	 * Additional fail id that can be set by handler.
+	 */
+	int			 tsi_reply_fail_id;
+	int			 tsi_request_fail_id;
+};
+
+static inline struct tgt_session_info *tgt_ses_info(const struct lu_env *env)
+{
+	struct tgt_session_info *tsi;
+
+	LASSERT(env->le_ses != NULL);
+	tsi = lu_context_key_get(env->le_ses, &tgt_session_key);
+	LASSERT(tsi);
+	return tsi;
+}
+
+/*
+ * Generic unified target support.
+ */
+enum tgt_handler_flags {
+	/*
+	 * struct *_body is passed in the incoming message, and object
+	 * identified by this fid exists on disk.
+	 *                            *
+	 * "habeo corpus" == "I have a body"
+	 */
+	HABEO_CORPUS = (1 << 0),
+	/*
+	 * struct ldlm_request is passed in the incoming message.
+	 *
+	 * "habeo clavis" == "I have a key"
+	 *                                     */
+	HABEO_CLAVIS = (1 << 1),
+	/*
+	 * this request has fixed reply format, so that reply message can be
+	 * packed by generic code.
+	 *
+	 * "habeo refero" == "I have a reply"
+	 */
+	HABEO_REFERO = (1 << 2),
+	/*
+	 * this request will modify something, so check whether the file system
+	 * is readonly or not, then return -EROFS to client asap if necessary.
+	 *
+	 * "mutabor" == "I shall modify"
+	 */
+	MUTABOR      = (1 << 3)
+};
+
+struct tgt_handler {
+	/* The name of this handler. */
+	const char		*th_name;
+	/* Fail id, check at the beginning */
+	int			 th_fail_id;
+	/* Operation code */
+	__u32			 th_opc;
+	/* Flags in enum tgt_handler_flags */
+	__u32			 th_flags;
+	/* Request version for this opcode */
+	int			 th_version;
+	/* Handler function */
+	int			(*th_act)(struct tgt_session_info *tti);
+	/* Request format for this request */
+	const struct req_format	*th_fmt;
+};
+
+struct tgt_opc_slice {
+	__u32			 tos_opc_start; /* First op code */
+	__u32			 tos_opc_end; /* Last op code */
+	struct tgt_handler	*tos_hs; /* Registered handler */
+};
+
+static inline struct ptlrpc_request *tgt_ses_req(struct tgt_session_info *tsi)
+{
+	return tsi->tsi_pill ? tsi->tsi_pill->rc_req : NULL;
+}
+
+static inline __u64 tgt_conn_flags(struct tgt_session_info *tsi)
+{
+	LASSERT(tsi->tsi_exp);
+	return exp_connect_flags(tsi->tsi_exp);
+}
+
+/* target/tgt_handler.c */
+int tgt_request_handle(struct ptlrpc_request *req);
+char *tgt_name(struct lu_target *tgt);
+void tgt_counter_incr(struct obd_export *exp, int opcode);
+int tgt_connect_check_sptlrpc(struct ptlrpc_request *req,
+			      struct obd_export *exp);
+int tgt_connect(struct tgt_session_info *tsi);
+int tgt_disconnect(struct tgt_session_info *uti);
+int tgt_obd_ping(struct tgt_session_info *tsi);
+int tgt_enqueue(struct tgt_session_info *tsi);
+int tgt_convert(struct tgt_session_info *tsi);
+int tgt_bl_callback(struct tgt_session_info *tsi);
+int tgt_cp_callback(struct tgt_session_info *tsi);
+int tgt_llog_open(struct tgt_session_info *tsi);
+int tgt_llog_close(struct tgt_session_info *tsi);
+int tgt_llog_destroy(struct tgt_session_info *tsi);
+int tgt_llog_read_header(struct tgt_session_info *tsi);
+int tgt_llog_next_block(struct tgt_session_info *tsi);
+int tgt_llog_prev_block(struct tgt_session_info *tsi);
+int tgt_sec_ctx_init(struct tgt_session_info *tsi);
+int tgt_sec_ctx_init_cont(struct tgt_session_info *tsi);
+int tgt_sec_ctx_fini(struct tgt_session_info *tsi);
 
 typedef void (*tgt_cb_t)(struct lu_target *lut, __u64 transno,
-                         void *data, int err);
+			 void *data, int err);
 struct tgt_commit_cb {
-        tgt_cb_t  tgt_cb_func;
-        void     *tgt_cb_data;
+	tgt_cb_t  tgt_cb_func;
+	void     *tgt_cb_data;
 };
 
+/* target/tgt_main.c */
 void tgt_boot_epoch_update(struct lu_target *lut);
 int tgt_last_commit_cb_add(struct thandle *th, struct lu_target *lut,
 			   struct obd_export *exp, __u64 transno);
 int tgt_new_client_cb_add(struct thandle *th, struct obd_export *exp);
 int tgt_init(const struct lu_env *env, struct lu_target *lut,
-	     struct obd_device *obd, struct dt_device *dt);
+	     struct obd_device *obd, struct dt_device *dt,
+	     struct tgt_opc_slice *slice,
+	     int request_fail_id, int reply_fail_id);
 void tgt_fini(const struct lu_env *env, struct lu_target *lut);
 int tgt_client_alloc(struct obd_export *exp);
 void tgt_client_free(struct obd_export *exp);
@@ -87,5 +231,83 @@ int tgt_server_data_write(const struct lu_env *env, struct lu_target *tg,
 			  struct thandle *th);
 int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg, int sync);
 int tgt_truncate_last_rcvd(const struct lu_env *env, struct lu_target *tg, loff_t off);
+
+enum {
+	ESERIOUS = 0x0001000
+};
+
+static inline int err_serious(int rc)
+{
+	LASSERT(rc < 0);
+	return -(-rc | ESERIOUS);
+}
+
+static inline int clear_serious(int rc)
+{
+	if (rc < 0)
+		rc = -(-rc & ~ESERIOUS);
+	return rc;
+}
+
+static inline int is_serious(int rc)
+{
+	return (rc < 0 && -rc & ESERIOUS);
+}
+
+/*
+ * Unified target generic handers macros and generic functions.
+ */
+#define TGT_RPC_HANDLER(base, flags, opc, fn, fmt, version)		\
+[opc - base] = {							\
+	.th_name	= #opc,						\
+	.th_fail_id	= OBD_FAIL_ ## opc ## _NET,			\
+	.th_opc		= opc,						\
+	.th_flags	= flags,					\
+	.th_act		= fn,						\
+	.th_fmt		= fmt,						\
+	.th_version	= version					\
+}
+
+/* MGS request with a format known in advance */
+#define TGT_MGS_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
+			   LUSTRE_MGS_VERSION)
+#define TGT_MGS_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(MGS_FIRST_OPC, flags, name, fn, NULL,	\
+			   LUSTRE_MGS_VERSION)
+
+/*
+ * OBD handler macros and generic functions.
+ */
+#define TGT_OBD_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(OBD_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
+			   LUSTRE_OBD_VERSION)
+
+/*
+ * DLM handler macros and generic functions.
+ */
+#define TGT_DLM_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, NULL,	\
+			   LUSTRE_DLM_VERSION)
+#define TGT_DLM_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(LDLM_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
+			   LUSTRE_DLM_VERSION)
+
+/*
+ * LLOG handler macros and generic functions.
+ */
+#define TGT_LLOG_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, NULL,	\
+			   LUSTRE_LOG_VERSION)
+#define TGT_LLOG_HDL(flags, name, fn)					\
+	TGT_RPC_HANDLER(LLOG_FIRST_OPC, flags, name, fn, &RQF_ ## name,\
+			   LUSTRE_LOG_VERSION)
+
+/*
+ * Sec context handler macros and generic functions.
+ */
+#define TGT_SEC_HDL_VAR(flags, name, fn)				\
+	TGT_RPC_HANDLER(SEC_FIRST_OPC, flags, name, fn, NULL,	\
+			   LUSTRE_OBD_VERSION)
 
 #endif /* __LUSTRE_LU_TARGET_H */

lustre/include/lustre_mdt.h
@@ -43,7 +43,6 @@
 #include <lustre/lustre_idl.h>
 #include <lustre_req_layout.h>
 #include <md_object.h>
-#include <dt_object.h>
 #include <libcfs/libcfs.h>
 
 /*
@@ -55,28 +54,6 @@ struct com_thread_info {
          */
         struct req_capsule *cti_pill;
 };
-
-enum {
-        ESERIOUS = 0x0001000
-};
-
-static inline int err_serious(int rc)
-{
-        LASSERT(rc < 0);
-        return -(-rc | ESERIOUS);
-}
-
-static inline int clear_serious(int rc)
-{
-        if (rc < 0)
-                rc = -(-rc & ~ESERIOUS);
-        return rc;
-}
-
-static inline int is_serious(int rc)
-{
-        return (rc < 0 && -rc & ESERIOUS);
-}
 
 /** @} mdt */
 

lustre/include/lustre_net.h
@@ -3544,7 +3544,6 @@ int llog_origin_handle_prev_block(struct ptlrpc_request *req);
 int llog_origin_handle_next_block(struct ptlrpc_request *req);
 int llog_origin_handle_read_header(struct ptlrpc_request *req);
 int llog_origin_handle_close(struct ptlrpc_request *req);
-int llog_origin_handle_cancel(struct ptlrpc_request *req);
 
 /* ptlrpc/llog_client.c */
 extern struct llog_operations llog_client_ops;

lustre/include/lustre_req_layout.h
@@ -245,6 +245,8 @@ extern struct req_format RQF_LLOG_ORIGIN_HANDLE_PREV_BLOCK;
 extern struct req_format RQF_LLOG_ORIGIN_HANDLE_READ_HEADER;
 extern struct req_format RQF_LLOG_ORIGIN_CONNECT;
 
+extern struct req_format RQF_CONNECT;
+
 extern struct req_msg_field RMF_GENERIC_DATA;
 extern struct req_msg_field RMF_PTLRPC_BODY;
 extern struct req_msg_field RMF_MDT_BODY;

lustre/include/obd_support.h
@@ -424,6 +424,13 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MGC_PAUSE_PROCESS_LOG   0x903
 #define OBD_FAIL_MGS_PAUSE_REQ           0x904
 #define OBD_FAIL_MGS_PAUSE_TARGET_REG    0x905
+#define OBD_FAIL_MGS_CONNECT_NET	 0x906
+#define OBD_FAIL_MGS_DISCONNECT_NET	 0x907
+#define OBD_FAIL_MGS_SET_INFO_NET	 0x908
+#define OBD_FAIL_MGS_EXCEPTION_NET	 0x909
+#define OBD_FAIL_MGS_TARGET_REG_NET	 0x90a
+#define OBD_FAIL_MGS_TARGET_DEL_NET	 0x90b
+#define OBD_FAIL_MGS_CONFIG_READ_NET	 0x90c
 
 #define OBD_FAIL_QUOTA_DQACQ_NET			0xA01
 #define OBD_FAIL_QUOTA_EDQUOT            0xA02

lustre/ldlm/ldlm_lockd.c
@@ -2163,16 +2163,6 @@ static int ldlm_callback_handler(struct ptlrpc_request *req)
                 rc = ldlm_handle_setinfo(req);
                 ldlm_callback_reply(req, rc);
                 RETURN(0);
-        case OBD_LOG_CANCEL: /* remove this eventually - for 1.4.0 compat */
-                CERROR("shouldn't be handling OBD_LOG_CANCEL on DLM thread\n");
-                req_capsule_set(&req->rq_pill, &RQF_LOG_CANCEL);
-                if (OBD_FAIL_CHECK(OBD_FAIL_OBD_LOG_CANCEL_NET))
-                        RETURN(0);
-                rc = llog_origin_handle_cancel(req);
-                if (OBD_FAIL_CHECK(OBD_FAIL_OBD_LOG_CANCEL_REP))
-                        RETURN(0);
-                ldlm_callback_reply(req, rc);
-                RETURN(0);
         case LLOG_ORIGIN_HANDLE_CREATE:
                 req_capsule_set(&req->rq_pill, &RQF_LLOG_ORIGIN_HANDLE_CREATE);
                 if (OBD_FAIL_CHECK(OBD_FAIL_OBD_LOGD_NET))
@@ -2373,15 +2363,6 @@ static int ldlm_cancel_handler(struct ptlrpc_request *req)
                 rc = ldlm_handle_cancel(req);
                 if (rc)
                         break;
-                RETURN(0);
-        case OBD_LOG_CANCEL:
-                req_capsule_set(&req->rq_pill, &RQF_LOG_CANCEL);
-                if (OBD_FAIL_CHECK(OBD_FAIL_OBD_LOG_CANCEL_NET))
-                        RETURN(0);
-                rc = llog_origin_handle_cancel(req);
-                if (OBD_FAIL_CHECK(OBD_FAIL_OBD_LOG_CANCEL_REP))
-                        RETURN(0);
-                ldlm_callback_reply(req, rc);
                 RETURN(0);
         default:
                 CERROR("invalid opcode %d\n",

commit d621287dc820f7895160ca667354c536d62cab88

LU-16518 mgs: remove unused mgs_counter_incr()
... since it is no longer used. We converted the MGS to the
unified request handler and dropped its last callers a long
time ago.

Fixes: 93a6346f8b73 ("LU-2145 server: use unified request handler for MGS")
Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@thelustrecollective.com>
Change-Id: I394073f03876d340b0b2f6f2abf130dd9e3b95e3
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67779
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/mgs/lproc_mgs.c
@@ -344,13 +344,6 @@ void lproc_mgs_cleanup(struct mgs_device *mgs)
 	lprocfs_obd_cleanup(obd);
 }
 
-void mgs_counter_incr(struct obd_export *exp, int opcode)
-{
-	lprocfs_counter_incr(exp->exp_obd->obd_stats, opcode);
-	if (exp->exp_nid_stats && exp->exp_nid_stats->nid_stats != NULL)
-		lprocfs_counter_incr(exp->exp_nid_stats->nid_stats, opcode);
-}
-
 void mgs_stats_counter_init(struct lprocfs_stats *stats)
 {
 	lprocfs_counter_init(stats, LPROC_MGS_CONNECT, 0, "connect");

lustre/mgs/mgs_internal.h
@@ -277,7 +277,6 @@ enum {
 	LPROC_MGS_TARGET_DEL,
 	LPROC_MGS_LAST
 };
-void mgs_counter_incr(struct obd_export *exp, int opcode);
 void mgs_stats_counter_init(struct lprocfs_stats *stats);
 
 struct mgs_thread_info {