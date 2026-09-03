lustre/lustre-release

commit e08fd8c40982453253fb156173bab0e1acfa1216

LU-19437 lnet: re-create missed Lustre peers
LNet peers added by Lustre via LNetAddPeer() can be
deleted by LNet discovery if local and remote peers have
different 'discovery' or MR settings. Such peer is
deleted by LNet and it is expected to be re-created as
simple non-MR peer when Lustre try to access it for
network transfer. However, after commit ee977b9039 Lustre
doesn't try to use such peer.

Patch restores expected behavior and re-creates peer
during call to LNetPeerDiscovered() if peer is missing

Fixes: ee977b9039 ("LU-17906 pltrpc: don't use non-uptodate peer at connect")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: Ib8c08d4925d13005ce250d7f5280426d0eef28ed
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61596
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>

lnet/lnet/peer.c
@@ -1534,20 +1534,26 @@ EXPORT_SYMBOL(LNetPrimaryNID);
 
 int LNetPeerDiscovered(struct lnet_nid *nid)
 {
+	struct lnet_peer_ni *lpni;
+	struct lnet_peer *lp = NULL;
+	int cpt;
 	int rc;
-	struct lnet_peer *lp;
 
 	if (nid_is_lo0(nid))
 		return 1;
 
-	lp = lnet_find_peer(nid);
-	if (!lp) {
-		CDEBUG(D_NET, "No peer for NID %s, can't discover\n",
+	cpt = lnet_net_lock_current();
+	lpni = lnet_peerni_by_nid_locked(nid, NULL, cpt);
+	if (IS_ERR(lpni)) {
+		CDEBUG(D_NET, "No peer for NID %s, skip for now\n",
 		       libcfs_nidstr(nid));
-		return -EHOSTUNREACH;
+		lnet_net_unlock(cpt);
+		return -ENETRESET;
 	}
+	lp = lpni->lpni_peer_net->lpn_peer;
+	lnet_peer_addref_locked(lp);
+	lnet_peer_ni_decref_locked(lpni);
 
-	lnet_net_lock(LNET_LOCK_EX);
 	spin_lock(&lp->lp_lock);
 	if (lp->lp_state & LNET_PEER_NO_DISCOVERY ||
 	    (lp->lp_state & LNET_PEER_DISCOVERED &&
@@ -1561,12 +1567,15 @@ int LNetPeerDiscovered(struct lnet_nid *nid)
 		rc = -EAGAIN;
 	spin_unlock(&lp->lp_lock);
 
-	if (rc == -EAGAIN)
+	if (rc == -EAGAIN) {
+		lnet_net_unlock(cpt);
+		cpt = LNET_LOCK_EX;
+		lnet_net_lock(cpt);
 		lnet_peer_queue_for_discovery(lp);
-
+	}
 	/* Drop refcount from lookup */
 	lnet_peer_decref_locked(lp);
-	lnet_net_unlock(LNET_LOCK_EX);
+	lnet_net_unlock(cpt);
 
 	CDEBUG(D_NET, "Peer NID %s is %sdiscovered: rc = %d\n",
 	       libcfs_nidstr(nid), rc > 0 ? "" : "not ", rc);

commit f50fb2ca3da49326d108e5f125f4a7810f6caa18

LU-19928 ptlrpc: don't sleep inside spinlock
The LNetPeerDiscovered() can sleep being invoked from
import_select_connection() inside spinlock-protected loop.
It was switched to the using of lnet_peerni_by_nid_locked()
which in turn may sleep while taking mutex in slow path.

Meanwhile this function is important to re-create missing
peer, so patch adds 'recreate' param in LNetPeerDiscovered()
to call either safe function without peer recreation or
sleeping one when re-create is demanded explicitly.
If any connection has missing peer status, then it is
refreshed outside spinlock in import_refresh_connections()
added by patch.
Patch also updates conn_uptodate2str() to show -ENETRESET
status as well.

Fixes: e08fd8c409 ("LU-19437 lnet: re-create missed Lustre peers")
Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
Change-Id: I3ebaeb90386e5cb9338d9932904ed89cc1b6ee83
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/67264
Tested-by: Maloo <maloo@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Chris Horn <chorn@ddn.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

include/linux/lnet/api.h
@@ -58,7 +58,7 @@ int LNetDist(struct lnet_nid *nid, struct lnet_nid *srcnid, __u32 *order);
 void LNetPrimaryNID(struct lnet_nid *nid);
 void LNetLocalPrimaryNID(struct lnet_nid *nid);
 bool LNetIsPeerLocal(struct lnet_nid *nid);
-int LNetPeerDiscovered(struct lnet_nid *nid);
+int LNetPeerDiscovered(struct lnet_nid *nid, bool recreate);
 int LNetFetchNIDs(int (*cb)(void *private, struct lnet_nid *nid),
 		  __u32 netid, void *data);
 int LNetHasLocalNet(__u32 net);

lnet/lnet/peer.c
@@ -1636,7 +1636,8 @@ out_unlock:
 }
 EXPORT_SYMBOL(LNetPrimaryNID);
 
-int LNetPeerDiscovered(struct lnet_nid *nid)
+/* NOTE: with @recreate=true function may sleep in slow path */
+int LNetPeerDiscovered(struct lnet_nid *nid, bool recreate)
 {
 	struct lnet_peer_ni *lpni;
 	struct lnet_peer *lp = NULL;
@@ -1647,8 +1648,11 @@ int LNetPeerDiscovered(struct lnet_nid *nid)
 		return 1;
 
 	cpt = lnet_net_lock_current();
-	lpni = lnet_peerni_by_nid_locked(nid, NULL, cpt);
-	if (IS_ERR(lpni)) {
+	if (recreate)
+		lpni = lnet_peerni_by_nid_locked(nid, NULL, cpt);
+	else
+		lpni = lnet_peer_ni_find_locked(nid);
+	if (IS_ERR_OR_NULL(lpni)) {
 		CDEBUG(D_NET, "No peer for NID %s, skip for now\n",
 		       libcfs_nidstr(nid));
 		lnet_net_unlock(cpt);

lustre/obdclass/lprocfs_status.c
@@ -993,6 +993,8 @@ static inline const char *conn_uptodate2str(int status)
 		return "discovering";
 	if (status == -EAGAIN)
 		return "rediscover";
+	if (status == -ENETRESET)
+		return "stale";
 	return "unknown";
 }
 

lustre/ptlrpc/import.c
@@ -468,6 +468,41 @@ int ptlrpc_reconnect_import(struct obd_import *imp)
 }
 EXPORT_SYMBOL(ptlrpc_reconnect_import);
 
+#define REFRESH_NIDS_MAX 16
+/**
+ * import_refresh_connections() - refresh connections with lost peers.
+ * @imp: obd_import whose connections need refreshing
+ *
+ * Function performs an opportunistic refresh of missing peers. Efforts are
+ * capped to a small amount of NIDs to mitigate costly slow-path operations
+ * and lock contention inside LNetPeerDiscovered(). Unprocessed NIDs are
+ * deferred to a subsequent demand-driven call.
+ */
+static void import_refresh_connections(struct obd_import *imp)
+{
+	struct obd_import_conn *conn;
+	struct lnet_nid nids_arr[REFRESH_NIDS_MAX];
+	char nidstr[LNET_NIDSTR_SIZE] = { 0 };
+	int rc, i, nids = 0;
+
+	spin_lock(&imp->imp_lock);
+	list_for_each_entry(conn, &imp->imp_conn_list, oic_item) {
+		if (conn->oic_uptodate != -ENETRESET)
+			continue;
+		nids_arr[nids++] = conn->oic_conn->c_peer.nid;
+		if (nids == REFRESH_NIDS_MAX)
+			break;
+	}
+	spin_unlock(&imp->imp_lock);
+
+	for (i = 0; i < nids; i++) {
+		rc = LNetPeerDiscovered(&nids_arr[i], true);
+		libcfs_nidstr_r(&nids_arr[i], nidstr, LNET_NIDSTR_SIZE);
+		CDEBUG(D_HA, "%s: restore NID %s missing peer: rc = %d\n",
+		       imp->imp_obd->obd_name, nidstr, rc);
+	}
+}
+
 /*
  * Connection on import @imp is changed to another one (if more than one is
  * present). We typically chose connection that we have not tried to connect to
@@ -479,7 +514,7 @@ static int import_select_connection(struct obd_import *imp)
 	struct obd_export *dlmexp;
 	char *target_start;
 	int target_len;
-	bool tried_all = true;
+	bool tried_all = true, to_refresh = false;
 	int rc = 0;
 
 	ENTRY;
@@ -509,7 +544,7 @@ static int import_select_connection(struct obd_import *imp)
 		       libcfs_nidstr(&conn->oic_conn->c_peer.nid),
 		       conn->oic_last_attempt);
 		conn->oic_uptodate =
-			LNetPeerDiscovered(&conn->oic_conn->c_peer.nid);
+			LNetPeerDiscovered(&conn->oic_conn->c_peer.nid, false);
 		/* connection status is changed to good state, try it like
 		 * this is first attempt
 		 */
@@ -518,6 +553,12 @@ static int import_select_connection(struct obd_import *imp)
 			tried_all = false;
 			break;
 		}
+		/* NID has no related LNet peer, need to re-establish it.
+		 * That can't be done under spinlock, so just remember it
+		 * and force refresh outside lock.
+		 */
+		if (conn->oic_uptodate == -ENETRESET)
+			to_refresh = true;
 
 		/* LNET ping failed, skip peer completely */
 		if (conn->oic_uptodate == -EHOSTUNREACH) {
@@ -625,6 +666,9 @@ connect:
 
 out_unlock:
 	spin_unlock(&imp->imp_lock);
+	if (to_refresh)
+		import_refresh_connections(imp);
+
 	RETURN(rc);
 }
 