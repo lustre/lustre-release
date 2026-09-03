lustre/lustre-release

commit 5be466e87304101e582df4c30edd830e528d08fb

LU-10499 pcc: async attach in the background for PCC-RO file
In current PCC, it may have a long delay while the whole file is
being copied into the cache before it can be used. There is a
significant delay for the first file access if the file is large,
which wastes valuable computing time. Being able to shorten this
time to first access may help application efficiency.

In this patch, it adds an tuning parameter "async_threshold",
which means the size threshold to determine doing PCC-RO attach
asynchronously in the background.

When the file size is samller than the threshold, the PCC attach
during open() will be performed in synchronous way.
Otherwise, the client will start a dedicated kernel thread to
copy data from Lustre OSTs to the PCC copy in the background, but
reads could fall back to the normal Lustre I/O path from Lustre
OSTs until the file is fully cached.

This may double the reads to the Lustre filesystem initially if
the file is not read sequentially, but would avoid the high
latency for data access. This may be some cache sharing (avoiding
double reads) if the PCC copy and the application both shared
the filesystem cached pages on the client.

The tuning parameter "llite.*.pcc_async_threshold" is set with
256MiB by default.

EX-3880 pcc: add pcc_async_affinity for async PCC attach

This patch adds a tunable parameter "llite.*.pcc_async_affinity"
that enables or disables the CPT selection in PCC-RO asynchronous
attach for testing.
Was-Change-Id: I1473a7547555a2d6c615d37182b6cc359194aae0

EX-bug-id: EX-2873 EX-3880
Test-Parameters: clientcount=3 testlist=sanity-pcc,sanity-pcc,sanity-pcc
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: Ia80992e9050cc6e4c7f61949fc4013dec303e150
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54379
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lustre/llite/lproc_llite.c
@@ -612,6 +612,67 @@ out_unlock:
 }
 LDEBUGFS_SEQ_FOPS(ll_max_cached_mb);
 
+static ssize_t pcc_async_threshold_show(struct kobject *kobj,
+					struct attribute *attr, char *buffer)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+
+	return scnprintf(buffer, PAGE_SIZE, "%llu\n",
+			 super->pccs_async_threshold);
+}
+
+static ssize_t pcc_async_threshold_store(struct kobject *kobj,
+					 struct attribute *attr,
+					 const char *buffer, size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+	u64 threshold;
+	int rc;
+
+	rc = sysfs_memparse(buffer, count, &threshold, "B");
+	if (rc)
+		return rc;
+
+	super->pccs_async_threshold = threshold;
+
+	return count;
+}
+LUSTRE_RW_ATTR(pcc_async_threshold);
+
+static ssize_t pcc_async_affinity_show(struct kobject *kobj,
+				       struct attribute *attr, char *buffer)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+
+	return scnprintf(buffer, PAGE_SIZE, "%d\n", super->pccs_async_affinity);
+}
+
+static ssize_t pcc_async_affinity_store(struct kobject *kobj,
+					struct attribute *attr,
+					const char *buffer, size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+	bool val;
+	int rc;
+
+	rc = kstrtobool(buffer, &val);
+	if (rc)
+		return rc;
+
+	super->pccs_async_affinity = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(pcc_async_affinity);
+
 static ssize_t checksums_show(struct kobject *kobj, struct attribute *attr,
 			      char *buf)
 {
@@ -2215,6 +2276,8 @@ static struct attribute *llite_attrs[] = {
 #if defined(CONFIG_LL_ENCRYPTION) || defined(HAVE_LUSTRE_CRYPTO)
 	&lustre_attr_filename_enc_use_old_base64.attr,
 #endif
+	&lustre_attr_pcc_async_threshold.attr,
+	&lustre_attr_pcc_async_affinity.attr,
 	NULL,
 };
 

lustre/llite/pcc.c
@@ -129,6 +129,7 @@ int pcc_super_init(struct pcc_super *super)
 	init_rwsem(&super->pccs_rw_sem);
 	INIT_LIST_HEAD(&super->pccs_datasets);
 	super->pccs_generation = 1;
+	super->pccs_async_threshold = PCC_DEFAULT_ASYNC_THRESHOLD;
 
 	return 0;
 }
@@ -1024,6 +1025,8 @@ pcc_dataset_add(struct pcc_super *super, struct pcc_cmd *cmd)
 
 	rc = kern_path(pathname, LOOKUP_DIRECTORY, &dataset->pccd_path);
 	if (unlikely(rc)) {
+		CDEBUG(D_CACHE, "%s: cache path lookup error: rc = %d\n",
+		       pathname, rc);
 		OBD_FREE_PTR(dataset);
 		return rc;
 	}
@@ -1375,6 +1378,8 @@ static int pcc_fid2dataset_path(struct pcc_dataset *dataset, char *buf,
 				(__u32)((fid)->f_oid ^ (fid)->f_seq) & 0XFFFF,
 				PFID(fid));
 	default:
+		CERROR(DFID ": unknown archive format %u: rc = %d\n",
+		       PFID(fid), dataset->pccd_hsmtool_type, -EINVAL);
 		return -EINVAL;
 	}
 }
@@ -1708,8 +1713,159 @@ static int pcc_try_datasets_attach(struct inode *inode, enum pcc_io_type iot,
 	RETURN(rc);
 }
 
-static int pcc_readonly_ioctl_attach(struct file *file, struct inode *inode,
-				     __u32 roid);
+static struct pcc_attach_context *
+pcc_attach_context_alloc(struct file *file, struct inode *inode, __u32 id)
+{
+	struct pcc_attach_context *pccx;
+
+	OBD_ALLOC_PTR(pccx);
+	if (!pccx)
+		RETURN(NULL);
+
+	pccx->pccx_file = get_file(file);
+	pccx->pccx_inode = inode;
+	pccx->pccx_attach_id = id;
+
+	return pccx;
+}
+
+static inline void pcc_attach_context_free(struct pcc_attach_context *pccx)
+{
+	LASSERT(pccx->pccx_file != NULL);
+	fput(pccx->pccx_file);
+	OBD_FREE_PTR(pccx);
+}
+
+static int pcc_attach_check_set(struct inode *inode)
+{
+	struct ll_inode_info *lli = ll_i2info(inode);
+	struct pcc_inode *pcci;
+	int rc = 0;
+
+	ENTRY;
+
+	pcc_inode_lock(inode);
+	if (lli->lli_pcc_state & PCC_STATE_FL_ATTACHING)
+		GOTO(out_unlock, rc = -EINPROGRESS);
+
+	pcci = ll_i2pcci(inode);
+	if (pcci && pcc_inode_has_layout(pcci))
+		GOTO(out_unlock, rc = -EEXIST);
+
+	lli->lli_pcc_state |= PCC_STATE_FL_ATTACHING;
+out_unlock:
+	pcc_inode_unlock(inode);
+	RETURN(rc);
+}
+
+static inline void pcc_readonly_attach_fini(struct inode *inode)
+{
+	pcc_inode_lock(inode);
+	ll_i2info(inode)->lli_pcc_state &= ~PCC_STATE_FL_ATTACHING;
+	pcc_inode_unlock(inode);
+}
+
+static int pcc_readonly_attach(struct file *file, struct inode *inode,
+			       __u32 roid);
+
+static int pcc_readonly_attach_thread(void *arg)
+{
+	struct pcc_attach_context *pccx = (struct pcc_attach_context *)arg;
+	struct file *file = pccx->pccx_file;
+	int rc;
+
+	ENTRY;
+
+	/*
+	 * For asynchronous open attach, it can not reuse the Lustre file
+	 * handle directly when the file is opening for read as the file
+	 * position in the file handle can not be shared by both user thread
+	 * and asynchronous attach thread in kenerl on the background.
+	 * It must reopen the file without O_DIRECT flag and use this new
+	 * file hanlde to do data copy from Lustre OSTs to the PCC copy.
+	 */
+	file = dentry_open(&file->f_path, file->f_flags & ~O_DIRECT,
+			   pcc_super_cred(pccx->pccx_inode->i_sb));
+	if (IS_ERR_OR_NULL(file))
+		GOTO(out, rc = file == NULL ? -EINVAL : PTR_ERR(file));
+
+	rc = pcc_readonly_attach(file, pccx->pccx_inode,
+				 pccx->pccx_attach_id);
+	fput(file);
+out:
+	pcc_readonly_attach_fini(pccx->pccx_inode);
+	CDEBUG(D_CACHE, "PCC-RO attach in background for %pd "DFID" rc = %d\n",
+	       file_dentry(pccx->pccx_file),
+	       PFID(ll_inode2fid(pccx->pccx_inode)), rc);
+	pcc_attach_context_free(pccx);
+	RETURN(rc);
+}
+
+static int pcc_readonly_attach_async(struct file *file,
+				     struct inode *inode, __u32 roid)
+{
+	struct pcc_attach_context *pccx = NULL;
+	struct task_struct *task;
+	int rc;
+
+	ENTRY;
+
+	rc = pcc_attach_check_set(inode);
+	if (rc)
+		RETURN(rc);
+
+	pccx = pcc_attach_context_alloc(file, inode, roid);
+	if (!pccx)
+		GOTO(out, rc = -ENOMEM);
+
+	if (ll_i2pccs(inode)->pccs_async_affinity) {
+		/* Create a attach kthread on the current node. */
+		task = kthread_create(pcc_readonly_attach_thread, pccx,
+				      "ll_pcc_%u", current->pid);
+	} else {
+		int node = cfs_cpt_spread_node(cfs_cpt_tab, CFS_CPT_ANY);
+
+		task = kthread_create_on_node(pcc_readonly_attach_thread, pccx,
+					      node, "ll_pcc_%u", current->pid);
+	}
+
+	if (IS_ERR(task)) {
+		rc = PTR_ERR(task);
+		CERROR("%s: cannot start ll_pcc thread for "DFID": rc = %d\n",
+		       ll_i2sbi(inode)->ll_fsname, PFID(ll_inode2fid(inode)),
+		       rc);
+		GOTO(out, rc);
+	}
+
+	wake_up_process(task);
+	RETURN(0);
+out:
+	if (pccx)
+		pcc_attach_context_free(pccx);
+
+	pcc_readonly_attach_fini(inode);
+	RETURN(rc);
+}
+
+static int pcc_readonly_attach_sync(struct file *file,
+				    struct inode *inode, __u32 roid);
+
+static inline int pcc_do_readonly_attach(struct file *file,
+					 struct inode *inode, __u32 roid)
+{
+	int rc;
+
+	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) >=
+	    ll_i2pccs(inode)->pccs_async_threshold) {
+		rc = pcc_readonly_attach_async(file, inode, roid);
+		if (!rc || rc == -EINPROGRESS)
+			return rc;
+	}
+
+	rc = pcc_readonly_attach_sync(file, inode, roid);
+
+	return rc;
+}
 
 /* Call with pcci_mutex hold */
 static int pcc_try_readonly_open_attach(struct inode *inode, struct file *file,
@@ -1726,6 +1882,9 @@ static int pcc_try_readonly_open_attach(struct inode *inode, struct file *file,
 	if (!((file->f_flags & O_ACCMODE) == O_RDONLY))
 		RETURN(0);
 
+	if (ll_i2info(inode)->lli_pcc_state & PCC_STATE_FL_ATTACHING)
+		RETURN(-EINPROGRESS);
+
 	item.pm_uid = from_kuid(&init_user_ns, current_uid());
 	item.pm_gid = from_kgid(&init_user_ns, current_gid());
 	item.pm_projid = ll_i2info(inode)->lli_projid;
@@ -1739,7 +1898,7 @@ static int pcc_try_readonly_open_attach(struct inode *inode, struct file *file,
 
 	if ((dataset->pccd_flags & PCC_DATASET_PCC_ALL) == PCC_DATASET_PCCRO) {
 		pcc_inode_unlock(inode);
-		rc = pcc_readonly_ioctl_attach(file, inode, dataset->pccd_roid);
+		rc = pcc_do_readonly_attach(file, inode, dataset->pccd_roid);
 		pcc_inode_lock(inode);
 		pcci = ll_i2pcci(inode);
 		if (pcci && pcc_inode_has_layout(pcci))
@@ -3338,28 +3497,6 @@ out_free:
 	RETURN(rc);
 }
 
-static int pcc_attach_allowed_check(struct inode *inode)
-{
-	struct ll_inode_info *lli = ll_i2info(inode);
-	struct pcc_inode *pcci;
-	int rc = 0;
-
-	ENTRY;
-
-	pcc_inode_lock(inode);
-	if (lli->lli_pcc_state & PCC_STATE_FL_ATTACHING)
-		GOTO(out_unlock, rc = -EBUSY);
-
-	pcci = ll_i2pcci(inode);
-	if (pcci && pcc_inode_has_layout(pcci))
-		GOTO(out_unlock, rc = -EEXIST);
-
-	lli->lli_pcc_state |= PCC_STATE_FL_ATTACHING;
-out_unlock:
-	pcc_inode_unlock(inode);
-	RETURN(rc);
-}
-
 static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 				   struct pcc_dataset *dataset,
 				   struct dentry **dentry)
@@ -3447,7 +3584,7 @@ int pcc_readwrite_attach(struct file *file, struct inode *inode,
 
 	ENTRY;
 
-	rc = pcc_attach_allowed_check(inode);
+	rc = pcc_attach_check_set(inode);
 	if (rc)
 		RETURN(rc);
 
@@ -3606,18 +3743,9 @@ repeat:
 	RETURN(rc);
 }
 
-static void pcc_readonly_attach_fini(struct inode *inode)
+static int pcc_readonly_attach(struct file *file,
+			       struct inode *inode, __u32 roid)
 {
-	pcc_inode_lock(inode);
-	ll_i2info(inode)->lli_pcc_state &= ~PCC_STATE_FL_ATTACHING;
-	pcc_inode_unlock(inode);
-}
-
-static int pcc_readonly_ioctl_attach(struct file *file,
-				     struct inode *inode,
-				     __u32 roid)
-{
-	struct ll_sb_info *sbi = ll_i2sbi(inode);
 	struct pcc_super *super = ll_i2pccs(inode);
 	struct ll_inode_info *lli = ll_i2info(inode);
 	const struct cred *old_cred;
@@ -3631,25 +3759,14 @@ static int pcc_readonly_ioctl_attach(struct file *file,
 
 	ENTRY;
 
-	if (!test_bit(LL_SBI_LAYOUT_LOCK, sbi->ll_flags))
-		RETURN(-EOPNOTSUPP);
-
-	rc = pcc_attach_allowed_check(inode);
-	if (rc) {
-		CDEBUG(D_CACHE,
-		       "PCC-RO caching for "DFID" not allowed, rc = %d\n",
-		       PFID(ll_inode2fid(inode)), rc);
-		RETURN(rc);
-	}
-
 	rc = pcc_layout_rdonly_set(inode, &gen);
 	if (rc)
-		GOTO(out_fini, rc);
+		RETURN(rc);
 
 	dataset = pcc_dataset_get(&ll_s2sbi(inode->i_sb)->ll_pcc_super,
 				  LU_PCC_READONLY, roid);
 	if (dataset == NULL)
-		GOTO(out_fini, rc = -ENOENT);
+		RETURN(-ENOENT);
 
 	rc = pcc_attach_data_archive(file, inode, dataset, &dentry);
 	if (rc)
@@ -3703,9 +3820,30 @@ out_put_unlock:
 	mutex_unlock(&lli->lli_layout_mutex);
 out_dataset_put:
 	pcc_dataset_put(dataset);
-out_fini:
-	pcc_readonly_attach_fini(inode);
 
+	RETURN(rc);
+}
+
+static int pcc_readonly_attach_sync(struct file *file,
+				    struct inode *inode, __u32 roid)
+{
+	int rc;
+
+	ENTRY;
+
+	if (!test_bit(LL_SBI_LAYOUT_LOCK, ll_i2sbi(inode)->ll_flags))
+		RETURN(-EOPNOTSUPP);
+
+	rc = pcc_attach_check_set(inode);
+	if (rc) {
+		CDEBUG(D_CACHE,
+		       "PCC-RO caching for "DFID" not allowed, rc = %d\n",
+		       PFID(ll_inode2fid(inode)), rc);
+		RETURN(rc);
+	}
+
+	rc = pcc_readonly_attach(file, inode, roid);
+	pcc_readonly_attach_fini(inode);
 	RETURN(rc);
 }
 
@@ -3721,8 +3859,7 @@ int pcc_ioctl_attach(struct file *file, struct inode *inode,
 		rc = -EOPNOTSUPP;
 		break;
 	case LU_PCC_READONLY:
-		rc = pcc_readonly_ioctl_attach(file, inode,
-					       attach->pcca_id);
+		rc = pcc_readonly_attach_sync(file, inode, attach->pcca_id);
 		break;
 	default:
 		rc = -EINVAL;

lustre/llite/pcc.h
@@ -150,8 +150,10 @@ struct pcc_dataset {
 	struct path		pccd_path;	 /* Root path */
 	struct list_head	pccd_linkage;  /* Linked to pccs_datasets */
 	atomic_t		pccd_refcount; /* Reference count */
-	enum hsmtool_type	pccd_hsmtool_type; /*HSM copytool type */
+	enum hsmtool_type	pccd_hsmtool_type; /* HSM copytool type */
 };
+
+#define PCC_DEFAULT_ASYNC_THRESHOLD	(256 << 20)
 
 struct pcc_super {
 	/* Protect pccs_datasets */
@@ -166,6 +168,9 @@ struct pcc_super {
 	 * parameters for PCC.
 	 */
 	__u64			 pccs_generation;
+	/* Size threshold for asynchrous PCC-RO attach in background. */
+	__u64			 pccs_async_threshold;
+	bool			 pccs_async_affinity;
 };
 
 struct pcc_inode {
@@ -205,6 +210,12 @@ struct pcc_vma {
 	atomic_t				 pccv_refcnt;
 	struct file				*pccv_file;
 	const struct vm_operations_struct	*pccv_vm_ops;
+};
+
+struct pcc_attach_context {
+	struct file		*pccx_file;
+	struct inode		*pccx_inode;
+	__u32			 pccx_attach_id;
 };
 
 enum pcc_io_type {

commit 91063238818f1a10dce09fde02ab1af71f528fc8

LU-10499 pcc: Limit attach queue depth
The existing async attach code does not attempt to limit
the number of async attaches that can be requested at once.
This is a problem because we could theoretically create too
many kthreads and overwhelm the system.

When the attach queue depth is exceeded, we stop allowing
new items to be queued by switching over to sync attach.

Ideally we would rebuild the attach code to generate a
queue of attach requests and have the attach thread code
pull items from the queue until it's exhausted, but that's
a much more substantial change and is left for later.

It's really hard to remember what 'inode' and 'file' mean
when there's more than one in a function, so I've redone
some of the names here.

EX-6399 pcc: add tunable parameter for PCC attach thread

Add a tunable parameter llite.*.pcc_attach_thread_max to
tune the max number of kernel threads doing asynchronous
attach (1024 by default).

Was-Change-Id: Ic59c15af935dd8dff586fa6be3939d4322c136d5

Use variable names that are consistent with parameter name.
Add llite.pcc_attach_thread_max.4 man page for parameter.

Fixes: 5be466e87304 ("LU-10499 pcc: async attach in the background for PCC-RO file")
EX-bug-id: EX-5014 EX-6399
Signed-off-by: Patrick Farrell <pfarrell@whamcloud.com>
Signed-off-by: Qian Yingjin <qian@ddn.com>
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: Ib00dfb67f5245a28b722278d031ee8cdf5e190d6
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54476
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

Documentation/man1/lfs-pcc-attach.1
@@ -71,4 +71,5 @@ filesystem package since release 2.13.0.
 .BR lfs-pcc-pin (1),
 .BR lfs-pcc-state (1),
 .BR llapi_pcc_attach (3),
+.BR llite.pcc_attach_thread_max (4),
 .BR lctl-pcc (8)

Documentation/man4/llite.pcc_attach_thread_max.4
@@ -1,0 +1,71 @@
+.TH LLITE.PCC_ATTACH_THREAD_MAX 4 2026-08-26 "Lustre" "Lustre Kernel Interfaces"
+.SH NAME
+llite.pcc_attach_thread_max \- limit number of PCC copy threads per mount
+.SH SYNOPSIS
+.SY
+.RI "lctl get_param llite." FSNAME *.pcc_attach_thread_max
+.SY
+.RI "lctl set_param llite." FSNAME *.pcc_attach_thread_max= THREAD_COUNT
+.YS
+.SS PROPERTIES
+.TP
+.B Access Permissions
+.br
+.BR 644 " | " -rw-r--r--
+.TP
+.B Scope
+.br
+Per local filesystem mountpoint.
+.TP
+.B Config
+.br
+.B pcc_attach_thread_max
+is always present for client mountpoints.
+.TP
+.B Default
+.br
+.RB pcc_attach_thread_max= 1024
+.TP
+.B Valid Range
+.br
+.RB pcc_attach_thread_max= 1
+.br
+.RB pcc_attach_thread_max= 16384
+.SH DESCRIPTION
+The Persistent Client Cache file attach process starts background file
+copy threads when a cache-eligible file is accessed to perform the
+.BR lfs-pcc-attach (1)
+operation.  The specified
+.I THREAD_COUNT
+is the maximum number of background threads started on each mountpoint.
+The default
+.B 1024
+limit is expected to be sufficient for most uses, but is tunable up or down.
+.SH MODULES
+This parameter is in the following modules:
+.EX
+.B llite.*.pcc_attach_thread_max
+.EE
+.SH EXAMPLES
+Permanently limit the number of background copy threads per mountpoint to 16:
+.RB "mgs# " "lctl set_param -P llite.*.pcc_attach_thread_max=16"
+.EE
+.PP
+Limit the number of background copy threads to 32 for the
+.B testfs
+filesystem on the local
+.BR client :
+.EX
+.RB "client# " "lctl set_param llite.testfs-*.enable_setstripe_gid=32"
+.EE
+.SH AVAILABILITY
+The
+.B llite.pcc_attach_thread_max
+parameter is part of the
+.BR lustre (7)
+filesystem package since release 2.18.0.
+.\" Added in commit 2.17.57-xx
+.SH SEE ALSO
+.BR lfs-pcc-attach (1),
+.BR lctl-get_param (8),
+.BR lctl-set_param (8)

lustre/llite/lproc_llite.c
@@ -881,14 +881,49 @@ static ssize_t pcc_async_affinity_store(struct kobject *kobj,
 }
 LUSTRE_RW_ATTR(pcc_async_affinity);
 
-static ssize_t pcc_mode_show(struct kobject *kobj, struct attribute *attr,
-			      char *buffer)
+static ssize_t
+pcc_attach_thread_max_show(struct kobject *kobj, struct attribute *attr,
+			       char *buffer)
 {
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
 	struct pcc_super *super = &sbi->ll_pcc_super;
 
-	return sprintf(buffer, "0%o\n", super->pccs_mode);
+	return scnprintf(buffer, PAGE_SIZE, "%u\n",
+			 super->pccs_attach_thread_max);
+}
+
+static ssize_t
+pcc_attach_thread_max_store(struct kobject *kobj, struct attribute *attr,
+				const char *buffer, size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 0, &val);
+	if (rc)
+		return rc;
+
+	if (val < 1 || val > PCC_ATTACH_THREAD_MAX_MAX)
+		return -EOVERFLOW;
+
+	super->pccs_attach_thread_max = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(pcc_attach_thread_max);
+
+static ssize_t pcc_mode_show(struct kobject *kobj, struct attribute *attr,
+			     char *buffer)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	struct pcc_super *super = &sbi->ll_pcc_super;
+
+	return scnprintf(buffer, PAGE_SIZE, "0%o\n", super->pccs_mode);
 
 }
 
@@ -2672,6 +2707,7 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_opencache_max_ms.attr,
 	&lustre_attr_parallel_dio.attr,
 	&lustre_attr_pcc_async_threshold.attr,
+	&lustre_attr_pcc_attach_thread_max.attr,
 	&lustre_attr_pcc_mode.attr,
 	&lustre_attr_pcc_async_affinity.attr,
 	&lustre_attr_read_ahead_async_file_threshold_mb.attr,

lustre/llite/pcc.c
@@ -115,6 +115,8 @@ int pcc_super_init(struct pcc_super *super)
 	super->pccs_generation = 1;
 	super->pccs_async_threshold = PCC_DEFAULT_ASYNC_THRESHOLD;
 	super->pccs_mode = 0400;
+	atomic_set(&super->pccs_attach_thread, 0);
+	super->pccs_attach_thread_max = PCC_ATTACH_THREAD_MAX_DEFAULT;
 
 	return 0;
 }
@@ -1780,6 +1782,7 @@ static struct pcc_attach_context *
 pcc_attach_context_alloc(struct file *file, struct inode *inode, __u32 id)
 {
 	struct pcc_attach_context *pccx;
+	struct pcc_super *super = ll_i2pccs(inode);
 
 	OBD_ALLOC_PTR(pccx);
 	if (!pccx)
@@ -1788,12 +1791,16 @@ pcc_attach_context_alloc(struct file *file, struct inode *inode, __u32 id)
 	pccx->pccx_file = get_file(file);
 	pccx->pccx_inode = inode;
 	pccx->pccx_attach_id = id;
+	atomic_inc(&super->pccs_attach_thread);
 
 	return pccx;
 }
 
 static inline void pcc_attach_context_free(struct pcc_attach_context *pccx)
 {
+	struct pcc_super *super = ll_i2pccs(pccx->pccx_inode);
+
+	atomic_dec(&super->pccs_attach_thread);
 	LASSERT(pccx->pccx_file != NULL);
 	fput(pccx->pccx_file);
 	OBD_FREE_PTR(pccx);
@@ -1910,16 +1917,33 @@ out:
 	RETURN(rc);
 }
 
-static int pcc_readonly_attach_sync(struct file *file,
-				    struct inode *inode, __u32 roid);
+static int pcc_readonly_attach_sync(struct file *file, struct inode *inode,
+				    u32 roid);
 
-static inline int pcc_do_readonly_attach(struct file *file,
-					 struct inode *inode, __u32 roid)
+static inline int pcc_do_readonly_attach(struct file *file, struct inode *inode,
+					 u32 roid)
 {
+	struct pcc_super *super = ll_i2pccs(inode);
+	bool async = true;
 	int rc;
 
-	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) >=
-	    ll_i2pccs(inode)->pccs_async_threshold) {
+	/* force sync if we're over the active attach limit, so this thread
+	 * can't keep increasing the thread count.  This lets us exceed the
+	 * number of active threads by $NUMTHREADS, but that should be fine
+	 * as long as we avoid unbounded numbers of kthreads.
+	 */
+	if (atomic_read(&super->pccs_attach_thread) >=
+	    super->pccs_attach_thread_max)
+		async = false;
+	/* if the file size is < the async threshold, don't do async */
+	if (max_t(__u64, ll_i2info(inode)->lli_lazysize, i_size_read(inode)) <
+	    super->pccs_async_threshold) {
+		CDEBUG(D_CACHE, "%s: attach thread limit %u hit, using sync\n",
+		       ll_i2sbi(inode)->ll_fsname, super->pccs_attach_thread_max);
+		async = false;
+	}
+
+	if (async) {
 		rc = pcc_readonly_attach_async(file, inode, roid);
 		if (!rc || rc == -EINPROGRESS)
 			return rc;
@@ -3682,11 +3706,12 @@ static int pcc_filp_write(struct file *filp, const void *buf, ssize_t count,
 
 static ssize_t pcc_copy_data(struct file *src, struct file *dst)
 {
+	size_t buf_len = PCC_COPY_BUFFER_BYTES;
+	struct inode *inode = file_inode(src);
+	loff_t offset = 0;
 	ssize_t rc = 0;
 	ssize_t rc2;
-	loff_t pos, offset = 0;
-	size_t buf_len = 1048576;
-	struct inode *inode = file_inode(src);
+	loff_t pos;
 	void *buf;
 
 	ENTRY;
@@ -3735,10 +3760,10 @@ out_free:
 
 static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 				   struct pcc_dataset *dataset,
-				   struct dentry **dentry)
+				   struct dentry **pcc_dentry)
 {
 	const struct cred *old_cred;
-	struct file *pcc_filp;
+	struct file *pcc_file;
 	bool direct = false;
 	struct path path;
 	ssize_t ret;
@@ -3748,12 +3773,13 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 	ENTRY;
 
 	old_cred = override_creds(pcc_super_cred(inode->i_sb));
-	rc = __pcc_inode_create(dataset, &ll_i2info(inode)->lli_fid, dentry);
+	rc = __pcc_inode_create(dataset, &ll_i2info(inode)->lli_fid,
+				pcc_dentry);
 	if (rc)
 		GOTO(out_cred, rc);
 
 	path.mnt = dataset->pccd_path.mnt;
-	path.dentry = *dentry;
+	path.dentry = *pcc_dentry;
 	/* If the inode is encrypted, we want the PCC file to be synced to the
 	 * storage. This is necessary as we are going to decrypt the page cache
 	 * pages of the PCC inode later in pcc_file_read_iter(), but still we
@@ -3761,18 +3787,18 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 	 */
 	if (IS_ENCRYPTED(inode))
 		flags |= O_SYNC;
-	pcc_filp = dentry_open(&path, flags, current_cred());
-	if (IS_ERR_OR_NULL(pcc_filp)) {
-		rc = pcc_filp == NULL ? -EINVAL : PTR_ERR(pcc_filp);
+	pcc_file = dentry_open(&path, flags, current_cred());
+	if (IS_ERR_OR_NULL(pcc_file)) {
+		rc = pcc_file == NULL ? -EINVAL : PTR_ERR(pcc_file);
 		GOTO(out_dentry, rc);
 	}
 
-	rc = pcc_inode_reset_iattr(inode, *dentry, ATTR_UID | ATTR_GID,
+	rc = pcc_inode_reset_iattr(inode, *pcc_dentry, ATTR_UID | ATTR_GID,
 				   old_cred->uid, old_cred->gid, 0);
 	if (rc)
 		GOTO(out_fput, rc);
 
-	rc = pcc_file_reset_projid(dataset, pcc_filp,
+	rc = pcc_file_reset_projid(dataset, pcc_file,
 				    ll_i2info(inode)->lli_projid);
 	if (rc)
 		GOTO(out_fput, rc);
@@ -3791,26 +3817,29 @@ static int pcc_attach_data_archive(struct file *file, struct inode *inode,
 		direct = true;
 	}
 
-	ret = pcc_copy_data(file, pcc_filp);
+	ret = pcc_copy_data(file, pcc_file);
 	if (direct)
 		file->f_flags |= O_DIRECT;
+
+	CDEBUG(D_CACHE, "Copied data from OSTs to PCC for %pd: rc = %llu\n",
+	       *pcc_dentry, (long long)ret);
+
 	if (ret < 0)
 		GOTO(out_fput, rc = ret);
-
 	/*
 	 * It must to truncate the PCC copy to the same size of the Lustre
 	 * copy after copy data. Otherwise, it may get wrong file size after
 	 * re-attach a file. See LU-13023 for details.
 	 */
-	rc = pcc_inode_reset_iattr(inode, *dentry,
+	rc = pcc_inode_reset_iattr(inode, *pcc_dentry,
 				   ATTR_SIZE | ATTR_MTIME | ATTR_MTIME_SET,
 				   KUIDT_INIT(0), KGIDT_INIT(0), ret);
 out_fput:
-	fput(pcc_filp);
+	fput(pcc_file);
 out_dentry:
 	if (rc) {
-		pcc_inode_remove(inode, *dentry);
-		dput(*dentry);
+		pcc_inode_remove(inode, *pcc_dentry);
+		dput(*pcc_dentry);
 	}
 out_cred:
 	revert_creds(old_cred);

lustre/llite/pcc.h
@@ -136,7 +136,20 @@ struct pcc_dataset {
 };
 
 #define PCC_DEFAULT_ASYNC_THRESHOLD	(256 << 20)
+/* buffer size for kernel buffered I/O for pcc attach
+ *
+ * performance barely varies for buffered I/O once size is more than a few
+ * hundred KiB, so just leave this at 1 MiB
+ */
+#define PCC_COPY_BUFFER_BYTES	(1 << 20) /* 1 MiB */
 
+/* after this many attaches are queued up, fall back to sync attach.  each
+ * attach creates a kthread, so we don't allow too many at once, but sync
+ * attach is very bad for applications, so we try to be generous.
+ */
+#define PCC_ATTACH_THREAD_MAX_DEFAULT	1024
+/* an unreasonably large number of threads, but systems always get larger */
+#define PCC_ATTACH_THREAD_MAX_MAX	16384
 struct pcc_super {
 	/* Protect pccs_datasets */
 	struct rw_semaphore	 pccs_rw_sem;
@@ -154,6 +167,8 @@ struct pcc_super {
 	__u64			 pccs_async_threshold;
 	bool			 pccs_async_affinity;
 	umode_t			 pccs_mode;
+	atomic_t		 pccs_attach_thread;
+	unsigned int		 pccs_attach_thread_max;
 };
 
 struct pcc_inode {